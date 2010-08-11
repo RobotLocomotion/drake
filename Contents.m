ae10d53179f7c3b7a7cd8e23e97a398080608660
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2010-08-11 19:57:58 +0000

    a little more documentation in the root
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib@48 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- Contents.m
+++ Contents.m
@@ -2,6 +2,15 @@
 % MIT Robot Locomotion Group
 % RobotLib Simulators
 %
-% Files
+% 
+% Sub-Directories
+%   simulators  - modules to simulate dynamics
+%   controllers - modules that design and implement controllers
+%   estimators  - modules for state estimation
+%   visualizers - modules for visualization
+%   drivers     - interface code for sensors and actuators
+%   examples    - example code for the model systems
+%
+% Scripts in this folder
 %   configure   - Checks dependencies and sets up matlab path.
 %   runAllTests - Recurses through the robotlib directories, running all test scripts 
