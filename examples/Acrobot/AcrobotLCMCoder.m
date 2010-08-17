6caf03fc537881842e0c2affd83776835cc56ed6
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2010-08-17 19:01:05 +0000

    acrobot lcm files from elena
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib@67 c9849af7-e679-4ec6-a44e-fc146a885bd3

---
+++ examples/Acrobot/AcrobotLCMCoder.m
@@ -0,0 +1,41 @@
+classdef AcrobotLCMCoder < LCMCoder
+% Encodes and Decodes acrobot-specific LCM smessages 
+
+  methods
+    function str = getRobotName(obj)
+      % robot name
+      str = 'Acrobot';
+    end
+    
+    function msg = encodeX(obj,t,x)
+      % encodes the state message
+      msg = robotlib.examples.Acrobot.lcmt_acrobot_x();
+      msg.timestamp = t*1000;
+      msg.theta1 = x(1);
+      msg.theta1Dot = x(3);
+      msg.theta2 = x(2);
+      msg.theta2Dot = x(4);
+    end
+    
+    function [x,t] = decodeX(obj,msg)
+      % decodes the state message
+      msg = robotlib.examples.Acrobot.lcmt_acrobot_x(msg.data);
+      x = [msg.theta; msg.thetaDot];
+      t = msg.timestamp/1000;
+    end
+    
+    function msg = encodeU(obj,t,u)
+      % encodes the input message
+      msg = robotlib.examples.Acrobot.lcmt_acrobot_u();
+      msg.timestamp = t*1000;
+      msg.tau = u(1);
+    end
+    
+    function [u,t] = decodeU(obj,msg)
+      % decodes the input message
+      msg = robotlib.examples.Acrobot.lcmt_acrobot_u(msg.data);
+      u = msg.tau;
+      t = msg.timestamp/1000;
+    end
+  end
+end
