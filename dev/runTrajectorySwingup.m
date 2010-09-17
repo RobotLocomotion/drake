49eb42529fdaa0478c404e714a7a0b2e604b1819
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2010-09-17 03:56:11 +0000

    working dir for russ
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib@148 c9849af7-e679-4ec6-a44e-fc146a885bd3

---
+++ dev/runTrajectorySwingup.m
@@ -0,0 +1,10 @@
+function runTrajectorySwingup
+
+d = AcrobotPlant;
+v = AcrobotVisualizer;
+c = TrajectorySwingup(d);
+
+for i=1:5
+  xtraj = simulate(d,c,[0 6],.01*randn(4,1));
+  playback(v,xtraj);
+end
