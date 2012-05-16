1abe35865e274b747465523d04ce83eaca3d848d
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-05-16 11:10:33 +0000

    working through test case for polynomial feedback / cascade
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/branches/class-hierarchy-cleanup@2981 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Pendulum/test/testFeedback.m
+++ examples/Pendulum/test/testFeedback.m
@@ -17,7 +17,7 @@ try
   sys2=feedback(p2,c);
   typecheck(sys2,'SimulinkModel');
   
-  for i=1:100
+  for i=[];%1:100
     t=rand; x=randn(2,1); u=randn;
     if any(abs(dynamics(sys1,t,x,u) - dynamics(sys2,t,x,u))>1e-10)
       i
@@ -36,6 +36,40 @@ try
       end
     end
   end
+  
+  
+  p = taylorApprox(p,0,[pi;0],0,3);
+  p = setInputLimits(p,-inf,inf);  % todo: another case where I get rid of this
+  sys1=feedback(p,c);
+  typecheck(sys1,'PolynomialSystem');
+  
+  p2 = SimulinkModel(p.getModel());
+  p2 = setInputFrame(p2,p.getInputFrame());
+  p2 = setStateFrame(p2,p.getStateFrame());
+  p2 = setOutputFrame(p2,p.getOutputFrame());
+  sys2=feedback(p2,c);
+  typecheck(sys2,'SimulinkModel');
+  
+  for i=1:100
+    t=rand; x=randn(2,1); u=randn;
+    if any(abs(dynamics(sys1,t,x,u) - dynamics(sys2,t,x,u))>1e-10)
+      i
+      x
+      u
+      xdot_polysys=dynamics(sys1,t,x,u)
+      xdot_simulinkmodel=dynamics(sys2,t,x,u)
+      error('dynamics should match');
+    end
+    
+    if (i<20)  % these are slower
+      y1=simulate(sys1,[0 1],x);
+      y2=simulate(sys2,[0 1],x);
+      if (any(abs(y1.eval(1)-y2.eval(1))>1e-10))
+        error('simulations should match');
+      end
+    end
+  end
+  
 catch ex
   path(oldpath);
   rethrow(ex);
