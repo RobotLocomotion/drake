1720c44bc64a712ee60b2af396532ffcb3465cc7
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-07-03 12:03:51 +0000

    moved static methods back to standalone.  easier for people to use.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/branches/class-hierarchy-cleanup@3180 c9849af7-e679-4ec6-a44e-fc146a885bd3

---
+++ examples/Acrobot/test/lqrTest.m
@@ -0,0 +1,20 @@
+function lqrTest()
+
+%% run the lqr controller from a handful of initial conditions on the
+%% boundary of the estimated ROA and verify that it gets to the top
+
+p = AcrobotPlant;
+p = p.setInputLimits(-inf,inf);  % for now
+[c,V] = balanceLQR(p);
+sys = feedback(p,c);
+n=10;
+x0=[pi;0;0;0];
+y=getLevelSet(V,x0,struct('num_samples',n));
+for i=1:n
+  xtraj=simulate(sys,[0 4],x0 + .99*y(:,i));
+  if (norm(xtraj.eval(4)-x0)>1e-2)
+    error('initial condition from verified ROA didn''t get to the top (in 4 seconds)');
+  end
+end
+
+end
