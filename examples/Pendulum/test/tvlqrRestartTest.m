413164b24503c4984893db3e08fc8e4d00294b05
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-07-07 12:03:24 +0000

    fixed pendulum tvlqr tests
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/branches/class-hierarchy-cleanup@3203 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Pendulum/test/tvlqrRestartTest.m
+++ examples/Pendulum/test/tvlqrRestartTest.m
@@ -5,7 +5,6 @@ oldpath = addpath('..');
 p = PendulumPlant();
 p = setInputLimits(p,-inf,inf);
 
-% OKTOFAIL
 [utraj,xtraj]=swingUpTrajectory(p);
 
 Q=diag([10,1]);R=.1;
@@ -21,9 +20,9 @@ xtraj1 = PPTrajectory(mkpp(breaks(1:isplit+1),coefs(:,1:isplit,:),d));
 xtraj2 = PPTrajectory(mkpp(breaks(isplit+1:end),coefs(:,isplit+1:end,:),d));
 
 [c2,V2] = tvlqr(p,xtraj2,utraj2,Q,R,Q);
-[c1,V1] = tvlqr(p,xtraj1,utraj1,Q,R,V2.eval(breaks(isplit+1)));
+[c1,V1] = tvlqr(p,xtraj1,utraj1,Q,R,V2);
 
-if (any(abs(gets(V.eval(0)) - gets(V1.eval(0)))>1e-4))
+if (any(abs(gets(V.getPoly(0)) - gets(V1.getPoly(0)))>1e-4))
   error('doesn''t match');
 end
 
--- examples/Pendulum/test/tvlqrTest.m
+++ examples/Pendulum/test/tvlqrTest.m
@@ -4,11 +4,10 @@ oldpath = addpath('..');
 
 try 
   p = PendulumPlant();
+
+  [utraj,xtraj]=swingUpTrajectory(p);
+
   p = setInputLimits(p,-inf,inf);
-
-  % OKTOFAIL
-  [utraj,xtraj]=runDircol(p);
-
   Q=diag([10,1]);R=.1;
   [c,V] = tvlqr(p,xtraj,utraj,Q,R,Q);
 
