327dfd3c0fdbb5d94ea7e89e9cb82545c55c1cf4
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-08-10 21:10:04 +0000

    disabling 3D dynamics test for this checkin.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@3489 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Pendulum/test/urdfDynamicsTest.m
+++ examples/Pendulum/test/urdfDynamicsTest.m
@@ -3,7 +3,7 @@ function urdfDynamicsTest
 oldpath=addpath('..');
 p_orig = PendulumPlant;
 p_urdf_2D = PlanarRigidBodyManipulator('../Pendulum.urdf');
-p_urdf_3D = RigidBodyManipulator('../Pendulum.urdf');
+%p_urdf_3D = RigidBodyManipulator('../Pendulum.urdf');
 
 for i=1:25
   t = rand;
@@ -12,7 +12,7 @@ for i=1:25
     
   xdot = p_orig.dynamics(t,x,u);
   valuecheck(p_urdf_2D.dynamics(t,x,u), xdot);
-  valuecheck(p_urdf_3D.dynamics(t,x,u), xdot);
+%  valuecheck(p_urdf_3D.dynamics(t,x,u), xdot);
   
 end
 
