1dc02292caf99e7a854420f1910abe0fd6c9b4c3
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-08-11 01:00:10 +0000

    3D dynamics pass the pendulum and acrobot tests.  yeah!
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@3492 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Acrobot/test/urdfDynamicsTest.m
+++ examples/Acrobot/test/urdfDynamicsTest.m
@@ -2,23 +2,18 @@ function urdfDynamicsTest
 
 oldpath=addpath('..');
 p_orig = AcrobotPlant;
-p_urdf = PlanarRigidBodyManipulator('../Acrobot.urdf');
+p_urdf_2D = PlanarRigidBodyManipulator('../Acrobot.urdf');
+p_urdf_3D = RigidBodyManipulator('../Acrobot.urdf');
 
 for i=1:25
   t = rand;
   x = randn(4,1);
   u = randn;
   
-  [H1,C1,B1]=p_urdf.manipulatorDynamics(x(1:2),x(3:4));
-  [H2,C2,B2]=p_orig.manipulatorDynamics(x(1:2),x(3:4));
-  
-  xdoterr = p_urdf.dynamics(t,x,u) - p_orig.dynamics(t,x,u);
-  if (any(abs(xdoterr)>1e-5))
-    [H1,H2]
-    [C1,C2]
-    [B1,B2]
-    error('dynamics don''t match');
-  end
+  xdot = p_orig.dynamics(t,x,u);
+  valuecheck(p_urdf_2D.dynamics(t,x,u), xdot);
+  valuecheck(p_urdf_3D.dynamics(t,x,u), xdot);
+
 end
 
 path(oldpath);
