924be6184c3dfab67eef97627bf7c77fce8171b9
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-08-15 20:40:44 +0000

    re-implemented loop constraints and brought back the SimpleFourBar example. implemented subs ref for Point. working on support for planar processing of 3D urdfs (setting out of plane joints to fixed).
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@3526 c9849af7-e679-4ec6-a44e-fc146a885bd3

---
+++ systems/plants/PlanarRigidBodyLoop.m
@@ -0,0 +1,39 @@
+classdef PlanarRigidBodyLoop < RigidBodyLoop
+  
+  properties 
+    jcode
+  end
+  
+  methods (Static)
+    
+    function T = parseLink(node,options)
+      xyz=zeros(3,1); rpy=zeros(3,1);
+      origin = node.getElementsByTagName('origin').item(0); 
+      if ~isempty(origin)
+        if origin.hasAttribute('xyz')
+          xyz = reshape(str2num(char(origin.getAttribute('xyz'))),3,1);
+        end
+        if origin.hasAttribute('rpy')
+          rpy = reshape(str2num(char(origin.getAttribute('rpy'))),3,1);
+        end
+      end
+      
+      if any(rpy)
+        rpya=rpy2axis(rpy); rpyangle=rpya(4); rpyaxis=rpya(1:3);
+        if abs(dot(rpyaxis,options.view_axis))<(1-1e-6)
+          error('joints out of the plane are not supported');
+          % note that if they were, it would change the way that I have to 
+          % parse geometries, inertias, etc, for all of the children.
+        elseif dot(rpyaxis,options.view_axis)<0
+          rpyangle=-rpyangle;
+        end
+      else
+        rpyangle=0;
+      end
+      
+      xy = [options.x_axis'; options.y_axis']*xyz;
+      T = [rotmat(rpyangle),xy; 0,0,1];
+    end
+    
+  end
+end
