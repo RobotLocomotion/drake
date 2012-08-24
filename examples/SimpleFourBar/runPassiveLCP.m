26e4f89e87e4b5ed13d681f6182eef97ea989a6a
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-08-24 09:43:08 +0000

    joint limits work well again. tested position constraints (on four bar example) with LCP.  Those work well, too.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@3597 c9849af7-e679-4ec6-a44e-fc146a885bd3

---
+++ examples/SimpleFourBar/runPassiveLCP.m
@@ -0,0 +1,9 @@
+function runPassiveLCP
+
+p = PlanarTimeSteppingRBM('FourBar.urdf',.01);
+v = p.constructVisualizer();
+v.xlim = [-8 8]; v.ylim = [-4 10];
+
+xtraj = p.simulate([0 10]);
+
+v.playback(xtraj);
