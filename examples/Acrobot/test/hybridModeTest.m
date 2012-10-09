4b5bb6bd038a3d0ede1f9b7228b3131b04c3bf60
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-10-09 15:43:17 +0000

    update to pass hybrid mode tests on build server.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@4205 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Acrobot/test/hybridModeTest.m
+++ examples/Acrobot/test/hybridModeTest.m
@@ -6,7 +6,7 @@ m.body(3).joint_limit_min = -pi/2;
 m.body(2).joint_limit_max = pi/2;
 m.body(3).joint_limit_max = pi/2;
 
-p = HybridRigidBodyMode(m,[0; -1],[]);
+p = HybridRigidBodyMode(m,[0; 1],[]);
 
 x0 = resolveConstraints(p,randn(4,1));
 xtraj = simulate(p,[0 4],x0);
