6f9a475487454e688da286eb18a60e246053b269
mpearrow <mpearrow@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-10-23 15:23:53 +0000

    fixed rigidBody -> planarRigidBody
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@4285 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Acrobot/test/hybridModeTest.m
+++ examples/Acrobot/test/hybridModeTest.m
@@ -1,6 +1,6 @@
 function hybridModeTest
 
-m = RigidBodyModel('../Acrobot.urdf');
+m = PlanarRigidBodyModel('../Acrobot.urdf');
 m.body(2).joint_limit_min = -pi/2;
 m.body(3).joint_limit_min = -pi/2;
 m.body(2).joint_limit_max = pi/2;
