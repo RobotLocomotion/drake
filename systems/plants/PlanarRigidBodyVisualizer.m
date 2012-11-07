570b56ba04a5aba3edc2d7efb31b454ee93a8815
mjp <mjp@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-11-07 14:46:30 +0000

    Tweaked the way the geometry is displayed, by reducing to only the convex hull. This also has the side effect of working around a nasty bug in the way alpha (transparency) does not work properly in OpenGL in at least NVIDIA + Ubuntu.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@4453 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/PlanarRigidBodyVisualizer.m
+++ systems/plants/PlanarRigidBodyVisualizer.m
@@ -51,9 +51,13 @@ classdef PlanarRigidBodyVisualizer < Visualizer
           s = size(body.geometry{j}.x); n=prod(s);
           pts = body.T*[reshape(body.geometry{j}.x,1,n); reshape(body.geometry{j}.y,1,n); ones(1,n)];
           xpts = reshape(pts(1,:),s); ypts = reshape(pts(2,:),s);
-
-          patch(xpts,ypts,body.geometry{j}.c,'LineWidth',2); %0*xpts,'FaceColor','flat','FaceVertexCData',body.geometry.c);
-          patch(xpts,ypts,body.geometry{j}.c,'EdgeColor','none'); %0*xpts,'FaceColor','flat','FaceVertexCData',body.geometry.c);
+          
+          % get the convex hull of the geometry, dump superfluous internal
+          % details
+          k=convhull(xpts,ypts);
+          
+          patch(xpts(k),ypts(k),body.geometry{j}.c,'LineWidth',.01); %0*xpts,'FaceColor','flat','FaceVertexCData',body.geometry.c);
+          % patch(xpts,ypts,body.geometry{j}.c,'EdgeColor','none','FaceAlpha',1); %0*xpts,'FaceColor','flat','FaceVertexCData',body.geometry.c);
 
           % for debugging:
           %h(i)=patch(xpts,ypts,co(mod(i-2,size(co,1))+1,:));
