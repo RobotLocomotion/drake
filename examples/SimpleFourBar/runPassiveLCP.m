f4e2da360625ce9994f0b920124434959d791020
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-11-27 20:59:38 +0000

    working commit.  trying to resolve high tolerance in LCP simulation of four-bar
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@4555 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/SimpleFourBar/runPassiveLCP.m
+++ examples/SimpleFourBar/runPassiveLCP.m
@@ -4,11 +4,12 @@ p = TimeSteppingRigidBodyManipulator(PlanarRigidBodyModel('FourBar.urdf'),.01);
 v = p.constructVisualizer();
 v.xlim = [-8 8]; v.ylim = [-4 10];
 
-xtraj = p.simulate([0 10]);
-
-v.playback(xtraj);
+xtraj = p.simulate([0 5]);
 
 x = xtraj.eval(xtraj.getBreaks);
 for i=1:size(x,2)
-  valuecheck(p.stateConstraints(x(:,i)),zeros(4,1),.1);  % note the comically loose tolerance
+  valuecheck(p.stateConstraints(x(:,i)),zeros(4,1));
 end
+
+v.playback(xtraj);
+
