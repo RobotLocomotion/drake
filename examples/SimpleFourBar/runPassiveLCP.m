3320ce478e3f9d16252b243083b472fbb6084b85
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-11-27 20:59:45 +0000

    Revert "working commit.  trying to resolve high tolerance in LCP simulation of four-bar"
    This reverts commit d695149d68d678ea84c9d1a052e9c50a5e75514b.
    
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@4556 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/SimpleFourBar/runPassiveLCP.m
+++ examples/SimpleFourBar/runPassiveLCP.m
@@ -4,12 +4,11 @@ p = TimeSteppingRigidBodyManipulator(PlanarRigidBodyModel('FourBar.urdf'),.01);
 v = p.constructVisualizer();
 v.xlim = [-8 8]; v.ylim = [-4 10];
 
-xtraj = p.simulate([0 5]);
-
-x = xtraj.eval(xtraj.getBreaks);
-for i=1:size(x,2)
-  valuecheck(p.stateConstraints(x(:,i)),zeros(4,1));
-end
+xtraj = p.simulate([0 10]);
 
 v.playback(xtraj);
 
+x = xtraj.eval(xtraj.getBreaks);
+for i=1:size(x,2)
+  valuecheck(p.stateConstraints(x(:,i)),zeros(4,1),.1);  % note the comically loose tolerance
+end
