f949b2178d874c512fe2f7e859ab33e83acd9f1b
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2012-12-19 10:38:24 +0000

    Resolved intermittent behavior in visaulizer.  MessageMonitor needed a notify() to wake sleeping thread.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@4709 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- util/LCMCoder.java
+++ util/LCMCoder.java
@@ -4,6 +4,6 @@ import lcm.lcm.*;
 
 public interface LCMCoder {
   String timestampName();
-  CoordinateFrameData decode(byte[] data);
-  LCMEncodable encode(CoordinateFrameData d);
+  drake.util.CoordinateFrameData decode(byte[] data);
+  LCMEncodable encode(drake.util.CoordinateFrameData d);
 }
