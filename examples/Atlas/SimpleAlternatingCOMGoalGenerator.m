05eb173cc3ebce8f4e780a7b64a527955e2ba6ca
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-01-24 19:40:38 +0000

    atlas demos fix from scott.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@4991 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Atlas/SimpleAlternatingCOMGoalGenerator.m
+++ examples/Atlas/SimpleAlternatingCOMGoalGenerator.m
@@ -16,9 +16,17 @@ classdef SimpleAlternatingCOMGoalGenerator < DrakeSystem
     end
     
     function com_des = getInitialState(obj)
-      com_des = zeros(3,1);
-    end
-        
+      x = obj.manip.getInitialState();
+      q = x(1:obj.manip.getNumDOF());
+      gc = obj.manip.contactPositions(q);
+      cm = obj.manip.getCOM(q);
+      
+      % compute desired COM projection
+      % assumes minimal contact model for now
+      k = convhull(gc(1:2,:)');
+      com_des = [mean(gc(1:2,k),2);cm(3)];
+    end        
+    
     function com_des = update(obj,t,com_des,x)
       nq = getNumDOF(obj.manip);
       q = x(1:nq);
@@ -26,19 +34,20 @@ classdef SimpleAlternatingCOMGoalGenerator < DrakeSystem
       gc = obj.manip.contactPositions(q);
       
       % assumes minimal contact model for now
-      idx = 1:4;
-      if t>18
+      ts = 3;
+      idx = 1:8;
+      if t>3*ts
         idx = 1:8;
-      elseif t>12
+      elseif t>2*ts
         idx = 1:4;
-      elseif t>6
+      elseif t>ts
         idx = 5:8;
       end
       
       gc = gc(:,idx);
       % compute desired COM projection
       k = convhull(gc(1:2,:)');
-      com_des = [mean(gc(1:2,k),2);0];
+      com_des(1:2) = mean(gc(1:2,k),2);
     end
     
     function y = output(obj,t,com_des,x)
