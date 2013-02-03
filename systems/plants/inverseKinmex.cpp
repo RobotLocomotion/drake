6bd3c65561570fa3437d8340cddb99f3e07e8605
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-02-03 12:11:23 +0000

    closer to a mex implementation of inverseKin, but it's not there yet.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@5192 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/inverseKinmex.cpp
+++ systems/plants/inverseKinmex.cpp
@@ -9,16 +9,39 @@ using namespace Eigen;
 using namespace std;
 
 /*
- * A C version of the forwardKin function
+ * A C version of the inverseKin function
  *
- * Piggybacks on HandCpmex.cpp to properly initialize and destroy models
- * Call with doKinematicsmex(q,b_compute_second_derivatives);
  */
+
+void ik( double* q, VectorXd q0, VectorXd q_nom, MatrixXd Q, int narg, int* body_ind, VectorXd* pts, double* f, double** G)
+{
+  /*
+  f = zeros(nF,1); G = zeros(nF,obj.num_q);
+  f(1) = (q-q_nom)'*Q*(q-q_nom);
+  G(1,:) = 2*(q-q_nom)'*Q;
+  kinsol = doKinematics(obj,q,false);
+  i=1;j=2;
+  while i<length(varargin)
+    if (varargin{i}==0)
+      do_rot = 0;
+      [x,J] = getCOM(obj,kinsol);
+    else
+      do_rot = length(varargin{i+1})==6;
+      [x,J] = forwardKin(obj,kinsol,varargin{i},[0;0;0],do_rot);
+    end
+    ind = ~isnan(varargin{i+1});
+    n = sum(ind);
+    f([j:j+n-1]) = x(ind) - varargin{i+1}(ind);
+    G([j:j+n-1],:) = J(ind,:);
+    j=j+n;
+    i=i+2;  
+   */
+}
 
 void mexFunction( int nlhs, mxArray *plhs[],int nrhs, const mxArray *prhs[] ) {
 
-  if (nrhs < 4) {
-    mexErrMsgIdAndTxt("Drake:inverseKinmex:NotEnoughInputs","Usage inverseKinmex(model_ptr,q0,q_nom,Q,body1_ind,body1_pos,body2_ind,body2_pos) for center of mass, or forwardKinmex(model_pts,body_ind,pts,include_rotations)");
+  if ((nrhs<4) || (nrhs%2!=0)) {
+    mexErrMsgIdAndTxt("Drake:inverseKinmex:NotEnoughInputs","Usage inverseKinmex(model_ptr,q0,q_nom,Q,body1_ind,body1_pos,body2_ind,body2_pos,...) ");
   }
 
   Model *model=NULL;
@@ -38,6 +61,21 @@ void mexFunction( int nlhs, mxArray *plhs[],int nrhs, const mxArray *prhs[] ) {
   
   MatrixXd Q = MatrixXd::Zero(nq,nq);
   memcpy(Q.data(),mxGetPr(prhs[3]),sizeof(double)*nq*nq);
+
+  int narg = (nrhs-4)/2;
+  int nF = 1, i,j;
+  int* body_ind = new int[narg];
+  VectorXd* pts = new VectorXd[narg];
+  for (i=0; i<narg; i++) {
+    body_ind[i] = (int) mxGetScalar(prhs[4+2*i]);
+    pts[i] = VectorXd::Zero(mxGetNumberOfElements(prhs[4+2*i+1]));
+    memcpy(pts[i].data(),mxGetPr(prhs[5+i]),sizeof(double)*pts[i].rows());
+    for (j=0; j<pts[i].rows(); j++)
+      if (!isnan(pts[i](j))) nF++; 
+  }
+
+  mexPrintf("nF = %d\n",nF);
   
-  
+  delete[] pts;
+  delete[] body_ind;
 }
