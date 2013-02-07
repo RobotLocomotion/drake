2b2baf42f65f42310ef60a21d8b2198fef925f29
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-02-07 20:36:59 +0000

    started merging hybridrigidbodymode from hongkai's dev branch. (haven't brought over trajectory optimization yet, just simulation).
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@5225 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/test/testHybridMode.m
+++ systems/plants/test/testHybridMode.m
@@ -1,7 +1,7 @@
 function testHybridMode
 
 options.floating = true;
-p = HybridRigidBodyMode('FallingBrick.urdf',zeros(3,1),[1;zeros(3,1)],options);
+p = HybridRigidBodyMode('FallingBrick.urdf',zeros(6,1),[1;zeros(7,1)],options);
 
 xtraj = simulate(p,[0 4]);
 v = p.constructVisualizer();
