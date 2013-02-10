57e1c0f2e859ff1a02a990802c09d0d2140b5273
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-02-10 21:40:24 +0000

    fix state constraints logic in tiler. adds four bar demo from Andres. cute visualizer shows passive vs lqr response.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@5251 c9849af7-e679-4ec6-a44e-fc146a885bd3

---
+++ examples/SimpleFourBar/runLQR.m
@@ -0,0 +1,24 @@
+function runLQR
+
+r = PlanarRigidBodyManipulator('FourBar2.urdf');
+v = r.constructVisualizer();
+
+x0 = [2.861691;.811224;-3.528619;0;0;0]; u0 = 0;
+[x0,u0] = findFixedPoint(r,x0,u0);
+v.draw(0,x0);
+
+Q = diag([10 10 10 1 1 1]);
+R = .001;
+
+c = tilqr(r,x0,u0,Q,R);
+sys = feedback(r,c);
+
+x0 = resolveConstraints(r,double(x0)+.2*randn(6,1));
+xtraj = simulate(sys,[0 10],x0);
+xtrajpassive = simulate(r,[0 10],x0);
+
+vpassive = v;
+vpassive.fade_percent = .5;
+mv = MultiVisualizer({vpassive,v});
+
+mv.playback(vertcat(xtrajpassive,xtraj));
