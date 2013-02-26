c699dc8e9e8441c23ba372c9bd4c263cde5e7b13
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-02-26 04:34:29 +0000

    implements rospack in matlab
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@5417 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- util/rospack.m
+++ util/rospack.m
@@ -1,9 +1,70 @@
 function path = rospack(package)
+% implements 'rospack find package' without requiring ros or rospack to be
+% installed.  this is intended to (at least) help parsing ROS urdf files with
+% package:// tags.
 
-[info,path]=system(['rospack find ',package]);
-if info==0  % success!
-  path = regexprep(path,'\n','');
-else
-  error(path);
+persistent packages package_paths;
+
+if isempty(packages)  % cache packages 
+  package_paths={};
+  package_paths = vertcat(package_paths,searchenvvar('ROS_ROOT'));
+  package_paths = vertcat(package_paths,searchenvvar('ROS_PACKAGE_PATH'));
+  packages=cell(size(package_paths));
+  for i=1:length(package_paths)
+    [~,packages{i}]=fileparts(package_paths{i});
+  end
 end
 
+ind = find(strcmpi(package,packages),1);
+if isempty(ind)
+  error('couldn''t find ROS package %s.\n ROS_ROOT=%s\n ROS_PACKAGE_PATH=%s\n',package,getenv('ROS_ROOT'),getenv('ROS_PACKAGE_PATH'));
+end
+  
+path = package_paths{ind};
+
+% the old version:  actually uses rospack (but libraries paths were an
+% issue)
+%[info,path]=system(['rospack find ',package]);
+%if info==0  % success!
+%  path = regexprep(path,'\n','');
+%else
+%  error(path);
+%end
+
+end
+
+
+function packages = searchenvvar(varname)
+
+packages={};
+path = getenv(varname);
+while ~isempty(path)
+  [token,path]=strtok(path,pathsep);
+  [info,p] = system(['find ',token,' -iname manifest.xml']);
+  if info==0 
+    while ~isempty(p)
+      [pt,p]=strtok(p);
+      pt=fileparts(pt);
+      p = regexprep(p,[pt,'.*\n'],'','dotexceptnewline');
+      packages=vertcat(packages,pt);
+    end
+  else  % if find fails for some reason (windows?), then do it the hard way...
+    packages = vertcat(packages,searchdir(token));
+  end
+end
+
+end
+
+function packages = searchdir(path)
+  if ~isempty(dir(fullfile(path,'manifest.xml')));
+    packages = {path};
+    return;
+  end
+  
+  packages={};
+  d = dir(path);
+  for i=find([d.isdir])
+    if (d(i).name(1)=='.') continue; end
+    packages = vertcat(packages,searchdir(d(i).name));
+  end
+end
