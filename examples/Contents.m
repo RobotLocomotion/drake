79535eba0573116bbe71d90d779fa9a4961fe048
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-02-27 03:44:36 +0000

    no message
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@5441 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Contents.m
+++ examples/Contents.m
@@ -4,13 +4,24 @@
 %   Pendulum
 %   SimpleDoublePendulum
 %
-% Model Underactuated Systems:
+% Model Nonlinear/Underactuated Systems:
+%   DubinsCar
 %   CartPole
 %   Acrobot
+%   FurutaPendulum
+%   PlanarNLink
 %
-% Model Walking Systems:
+% Model Walking/Running Systems:
 %   RimlessWheel
 %   CompassGait
+%   KneedCompassGait
+%   PlanarMonopodHopper
 %
 % Model UAVs:
 %   Glider
+%   Quadrotor2D
+%   Airplane2D
+% 
+% Model Rigid Body Dynamics:
+%   Bouncing Ball
+%  
