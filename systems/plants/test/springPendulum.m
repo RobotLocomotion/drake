b094af8bd73c725dfcdfccc57955172c75a1a3da
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-02-27 12:12:30 +0000

    adding unit tests from benoit
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@5443 c9849af7-e679-4ec6-a44e-fc146a885bd3

---
+++ systems/plants/test/springPendulum.m
@@ -0,0 +1,10 @@
+function springPendulum
+
+r = RigidBodyManipulator('SpringPendulum.urdf');
+x0 = [-pi/2;0];
+run_time = 10;
+xtraj = simulate(r,[0 run_time],x0);
+v = r.constructVisualizer();
+v.playback(xtraj);
+
+end
