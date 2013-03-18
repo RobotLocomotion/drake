846c8dc22f073694ebd5dbc94ae9df131c24f47f
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-03-18 20:51:56 +0000

    merge from scott's branch.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@5564 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/@RigidBodyManipulator/contactPositions.m
+++ systems/plants/@RigidBodyManipulator/contactPositions.m
@@ -1,4 +1,4 @@
-function [p,J,dJ] = contactPositions(obj,kinsol)
+function [contact_pos,J,dJ] = contactPositions(obj,kinsol,body_idx)
 % @retval p(:,i)=[px;py;pz] is the position of the ith contact point 
 % @retval J is dpdq
 % @retval dJ is ddpdqdq
@@ -8,28 +8,32 @@ if ~isstruct(kinsol)
   kinsol = doKinematics(obj,kinsol,nargout>2);
 end
 
+if nargin<3
+  body_idx = 1:length(obj.body);
+end
+
 d=length(obj.gravity);  % 2 for planar, 3 for 3D
-contact_pos = zeros(d,obj.num_contacts); 
+contact_pos = zeros(d,size([obj.body(body_idx).contact_pts],2));  
 if (nargout>1) 
-  J = zeros(d*obj.num_contacts,obj.num_q); 
-  if (nargout>2)
-    dJ = sparse(d*obj.num_contacts,obj.num_q*obj.num_q);
-  end
+  J = zeros(size([obj.body(body_idx).contact_pts],2)*d,obj.num_q); 
+end
+if (nargout>4)
+  dJ = sparse(size([obj.body(body_idx).contact_pts],2)*d,obj.num_q^2);
 end
 
 count=0;
-for i=1:length(obj.body)
-  nC = size(obj.body(i).contact_pts,2);
+for i=1:length(body_idx)
+  nC = size(obj.body(body_idx(i)).contact_pts,2);
   if nC>0
     if (nargout>2)
-      [contact_pos(:,count+(1:nC)),J(d*count+(1:d*nC),:),dJ(d*count+(1:d*nC),:)] = forwardKin(obj,kinsol,i,obj.body(i).contact_pts);
+      [contact_pos(:,count+(1:nC)),J(d*count+(1:d*nC),:),dJ(d*count+(1:d*nC),:)] = forwardKin(obj,kinsol,body_idx(i),obj.body(body_idx(i)).contact_pts);
     elseif (nargout>1)
-      [contact_pos(:,count+(1:nC)),J(d*count+(1:d*nC),:)] = forwardKin(obj,kinsol,i,obj.body(i).contact_pts);
+      [contact_pos(:,count+(1:nC)),J(d*count+(1:d*nC),:)] = forwardKin(obj,kinsol,body_idx(i),obj.body(body_idx(i)).contact_pts);
     else
-      contact_pos(:,count+(1:nC)) = forwardKin(obj,kinsol,i,obj.body(i).contact_pts);
+      contact_pos(:,count+(1:nC)) = forwardKin(obj,kinsol,body_idx(i),obj.body(body_idx(i)).contact_pts);
     end
     count = count + nC;
   end
 end
-p = contact_pos;
+
 end
