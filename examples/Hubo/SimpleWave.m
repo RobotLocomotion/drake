1c613fdf59a43cd4dcc162d30f8592138e4222cb
mjp <mjp@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-03-26 18:15:38 +0000

    cleaning up, making Hubo more like other examples.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@5618 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Hubo/SimpleWave.m
+++ examples/Hubo/SimpleWave.m
@@ -3,7 +3,9 @@ function traj = SimpleWave
 fr = HuboJointCommand;
 traj = ConstantTrajectory(Point(fr));
 t = 0:.1:5;
-traj(8) = PPTrajectory(spline(t,.05*sin(2*pi*t/5)));  % left elbow
+traj(5) = PPTrajectory(spline(t,.05*sin(2*pi*t/5)));  % left elbow
+traj(6) = PPTrajectory(spline(t,.05*sin(2*pi*t/5)));  % left elbow
+
 traj = setOutputFrame(traj,fr);
 
 
