1dbf13fee2157650f2939a6d6eac2227b30f4737
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-04-01 19:53:37 +0000

    bug fixes from sisir.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@5672 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- util/quat2rpy.m
+++ util/quat2rpy.m
@@ -1,12 +1,13 @@
 function rpy = quat2rpy(q)
 % convert quaternion to ROS rpy 
 % from http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
-% note, that this is equivalent to the aerospace toolbox
-% quat2angle(q','XYZ');
 
 w=q(1);x=q(2);y=q(3);z=q(4);
+% rpy = [atan2(2*(w*x + y*z), 1-2*(x*x +y*y)); ...
+%   asin(2*(w*y - z*x)); ...
+%   atan2(2*(w*z + x*y), 1-2*(y*y+z*z))];
 
-rpy = [atan2(2*(w*x + y*z), 1-2*(x*x +y*y)); ...
+% more stable than previous form (old one did not work for quat2rpy(rpy2quat([0,-pi/2,pi])))
+rpy = [atan2(2*(w*x + y*z), w*w + z*z -(x*x +y*y)); ...
   asin(2*(w*y - z*x)); ...
-  atan2(2*(w*z + x*y), 1-2*(y*y+z*z))];
-
+  atan2(2*(w*z + x*y), w*w + x*x-(y*y+z*z))];
