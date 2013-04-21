40f090e3fb1d18cf22e98a15f597d60ab5b1f7e3
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-04-21 21:14:36 +0000

    updated urdf kinematics test to work again.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@5792 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/test/urdf_kin_test.cpp
+++ systems/plants/test/urdf_kin_test.cpp
@@ -1,5 +1,4 @@
 
-#include <fstream>
 #include <iostream>
 #include <cstdlib>
 #include "../urdf.h"
@@ -10,41 +9,23 @@ int main(int argc, char* argv[])
 {
   // todo: pull urdf filename off the command line
   std::string filename("../../../examples/Atlas/urdf/atlas_minimal_contact.urdf");
-  std::string xml_string;
-  std::fstream xml_file(filename.c_str(), std::fstream::in);
-  if (xml_file.is_open())
-  {
-    while ( xml_file.good() )
-    {
-      std::string line;
-      std::getline( xml_file, line);
-      xml_string += (line + "\n");
-    }
-    xml_file.close();
-  }
-  else
-  {
-    std::cerr << "Could not open file ["<<filename.c_str()<<"] for parsing."<< std::endl;
-    return -1;
-  }
-  
-  // parse URDF to get model
-  RigidBodyManipulator* model = parseURDFModel(xml_string);
+  RigidBodyManipulator* model = loadURDF(filename);
   
   // run kinematics with second derivatives 100 times
   double q[34];
   int i;
   
   for (int n=0; n<20; n++) {
-    for (i=0; i<34; i++) q[i]=(double)rand();
+    for (i=0; i<34; i++)  
+      q[i]=(double)rand() / RAND_MAX;
     model->doKinematics(q,true);
   }
   
   Vector4d zero;
   zero << 0,0,0,1; 
   
-  for (int i=0; i<=model->NB; i++) {
-    cout << "forward kin: " << model->bodies[i].linkname << " is at " << model->forwardKin(i,zero,true).transpose() << endl;
+  for (i=0; i<=model->NB; i++) {
+    cout << "forward kin: " << model->bodies[i].linkname << " is at " << model->forwardKin(i,zero,1).transpose() << endl;
   } 
   
   delete model;
