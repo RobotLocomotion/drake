4133f0e5039604899fddbcab45da6a1c360ea24a
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-04-21 21:15:28 +0000

    acrobat visualizes well, but appears to have offset issues.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@5796 c9849af7-e679-4ec6-a44e-fc146a885bd3

---
+++ systems/plants/dev/LibBotVisualizer.m
@@ -0,0 +1,19 @@
+classdef LibBotVisualizer < Visualizer
+
+  methods
+%    constructor (with optional urdf arg and call to addRobotFromURDF) 
+%        opens a new viewer app with unique ID (like the figure num)
+%        specified at the command line
+%        viewer_id should be a shared data handle, which sends a shutdown
+%        command on delete
+%    addRobotFromURDF
+%    draw
+%    loadRenderer
+%    playbackMovie
+%    lcmglwrappers
+  end
+  
+  properties 
+    viewer_id
+  end
+end
