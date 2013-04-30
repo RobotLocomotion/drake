f1c2d4bddd524deea62da510ddedd3a0c460ee4f
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-04-30 11:03:30 +0000

    resolves 1536 - axis2quat documentation (and only documentation) had the the wrong quat convention [x,y,z,w] instead of [w,x,y,z]
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@5848 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- util/axis2quat.m
+++ util/axis2quat.m
@@ -1,5 +1,5 @@
 function q = axis2quat(a)
-% convert from axis angle, a = (x,y,z,theta) to quaternion (x,y,z,w)
+% convert from axis angle, a = (x,y,z,theta) to quaternion (w,x,y,z)
 
 a(1:3) = a(1:3)/norm(a(1:3));
 q = [cos(a(4)/2); a(1:3)*sin(a(4)/2)];
