927f9c5d96ae5d9111cb130b9295454c15fa9594
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-05-19 11:38:56 +0000

    support all lcmgl features except texture2d
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6049 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/test/bullet_collision_test.m
+++ systems/plants/test/bullet_collision_test.m
@@ -5,7 +5,11 @@ r = RigidBodyManipulator();
 for i=1:2
   r = addRobotFromURDF(r,'FallingBrick.urdf',zeros(3,1),zeros(3,1),struct('floating',true));
 end
-v = r.constructVisualizer();
+%v = r.constructVisualizer();
+
+v = BotVisualizer('FallingBrick.urdf',struct('floating',true));
+
+lcmgl = bot_lcmgl_init('contact_points');
 
 for i=1:20
   q = randn(getNumDOF(r),1);
@@ -14,5 +18,15 @@ for i=1:20
   kinsol = doKinematics(r,q);
   
   b = pairwiseContactTest(r,2,3)
+  
+  pts = contactPositions(r,kinsol);
+  
+  bot_lcmgl_color3f(lcmgl,0,0,1); 
+  for j=1:size(pts,2)
+    bot_lcmgl_sphere(lcmgl,pts(:,j),.05,36,36);
+  end
+  bot_lcmgl_color3f(lcmgl,.7,.7,.7); 
+  
+  bot_lcmgl_switch_buffer(lcmgl);
   pause;
 end
