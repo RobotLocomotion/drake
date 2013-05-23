54ad20af9867d67eaf203803db1a678974aba318
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-05-23 23:50:53 +0000

    added analytic zmp planner for 3D model.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6100 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/ZMP/LinearInvertedPendulum.m
+++ examples/ZMP/LinearInvertedPendulum.m
@@ -31,6 +31,8 @@ classdef LinearInvertedPendulum < LinearSystem
       end
              
       obj = obj@LinearSystem([zeros(2),eye(2);zeros(2,4)],[zeros(2);eye(2)],[],[],[eye(4);Czmp],D);
+      obj.h = h;
+      obj.g = g;
       
       cartstateframe = CartTableState;
       obj = setInputFrame(obj,CartTableInput);
@@ -116,8 +118,57 @@ classdef LinearInvertedPendulum < LinearSystem
       comtraj = comtraj(1:2);  % only return position (not velocity)
     end
     
-    function comtraj = ZMPplan(obj,com0,comdot0,dZMP,options)
-      error('not implemented yet');
+    function comtraj = ZMPplan(obj,com0,comf,dZMP,options)
+      % implements analytic method from Harada06
+      %  notice the different arugments (comf instead of comdot0)
+      sizecheck(com0,2);
+      sizecheck(comf,2);
+      typecheck(dZMP,'PPTrajectory');  
+      dZMP = dZMP.inFrame(desiredZMP);
+      if ~isnumeric(obj.h) error('variable height not implemented yet'); end
+      
+      Tc = sqrt(obj.g/obj.h);
+      for ax=1:2
+        ax_zmp = dZMP(ax);
+        [breaks,coefs,l,k,d] = unmkpp(ax_zmp.pp);
+        m = l; %  to match Harada06 eq (1)
+        n = k-1;
+        assert(prod(d)==1);
+      
+        % equations (6) and (7), solved for A(i+1,j)
+        A(n+1,:) = coefs(:,1)';
+        A(n,:) = coefs(:,2)';
+        for i=n-2:-1:0
+          A(i+1,:) = coefs(:,n+1-i)' + (obj.h/obj.g)*(i+1)*(i+2)*A(i+3,:);
+        end
+
+        % equation (12)
+        dt = diff(breaks);
+        dtn = ones(1,m); for i=1:n, dtn(i+1,:) = dt.*dtn(i,:); end  % dtn(a,j) = dt(j)^(a-1)
+        w = [com0(ax) - A(1,1); ...
+          reshape([A(1,2:end) - sum(A(:,1:end-1).*dtn(:,1:end-1)); ...
+          A(2,2:end) - sum(repmat((1:n)',1,m-1).*A(2:end,1:end-1).*dtn(1:end-1,1:end-1))],2*(m-1),1); ...
+          comf(ax) - A(:,end)'*dtn(:,end) ];
+        
+        Z = zeros(2*m);
+        Z(1,1) = 1;
+        % todo: vectorize this.  (should it be a sparse matrix?)
+        for j=1:m-1,
+          Z(2*j+(0:1),2*(j-1) + (1:4)) = [cosh(Tc*dt(j)),    sinh(Tc*dt(j)),   -1, 0; ...
+                                        Tc*sinh(Tc*dt(j)), Tc*cosh(Tc*dt(j)), 0, -Tc];
+        end
+        Z(end,end-1:end) = [ cosh(Tc*dt(m)), sinh(Tc*dt(m)) ];
+      
+        y = reshape((Z\w)',2,m);
+        V = y(1,:); W = y(2,:);
+      
+        % equation (5)
+        % Note: i could return a function handle trajectory which is exact
+        % (with the cosh,sinh terms), but for now I think it's sufficient and more
+        % practical to make a new spline.
+        com_knots(ax,:) = [V+A(1,:),V(end)*cosh(Tc*dt(end)) + W(end)*sinh(Tc*dt(end)) + A(:,end)'*dtn(:,end)];
+      end
+      comtraj = PPTrajectory(spline(breaks,com_knots));
     end
     
     function comtraj = ZMPplanner(obj,com0,comdot0,dZMP,options)
@@ -169,6 +220,11 @@ classdef LinearInvertedPendulum < LinearSystem
       ytraj = sys.simulate(dZMP.tspan,randn(4,1));
       v.playback(ytraj);
     end
+  end
+  
+  properties
+    h
+    g
   end
   
 end
