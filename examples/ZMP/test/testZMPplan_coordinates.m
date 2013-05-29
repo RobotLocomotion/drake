88ff3169d88537eb8e1088a6d7b852d071ac0191
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-05-29 16:23:18 +0000

    halfway through analytic zmp riccati equation derivation / implementation.  s1 is in, but doesn't match tvlqr quite yet.  must still have a typo/bug.  (it's disabled by default).
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6155 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/ZMP/test/testZMPplan_coordinates.m
+++ examples/ZMP/test/testZMPplan_coordinates.m
@@ -9,7 +9,6 @@ oldpath = path;
 x0 = 0; y0 = 0;
 [zmptraj,comtraj] = plan(x0,y0);
 
-
 x0 = 1000;%*randn(); 
 y0 = 1000;%*randn();
 tol = 1e-3;  % note: low tolerance seems potentially reasonable since we're integrating the result of multiplying big numbers
@@ -40,25 +39,25 @@ function [zmptraj,comtraj] = plan(x0,y0,options)
 limp = LinearInvertedPendulum(1.0);
 ts = linspace(0,10,100); 
 zmptraj = setOutputFrame(PPTrajectory(spline(ts,[x0 + 0.5*cos(ts*pi); y0 + sin(ts*pi)])),desiredZMP);
-options.use_mex = true;
-[c_mex,V_mex] = ZMPtracker(limp,zmptraj,options);
-c_mex = c_mex.inInputFrame(limp.getStateFrame);
-c_mex = c_mex.inOutputFrame(limp.getInputFrame);
-V_mex = V_mex.inFrame(limp.getStateFrame);
-options.use_mex = false;
+options.use_tvlqr = true;
+[c_tvlqr,V_tvlqr] = ZMPtracker(limp,zmptraj,options);
+c_tvlqr = c_tvlqr.inInputFrame(limp.getStateFrame);
+c_tvlqr = c_tvlqr.inOutputFrame(limp.getInputFrame);
+V_tvlqr = V_tvlqr.inFrame(limp.getStateFrame);
+options.use_tvlqr = false;
 [c,V] = ZMPtracker(limp,zmptraj,options);
 c = c.inInputFrame(limp.getStateFrame);
 c = c.inOutputFrame(limp.getInputFrame);
 V = V.inFrame(limp.getStateFrame);
 
-tol = .1;
-valuecheck(V_mex.S,V.S,tol,true);
-valuecheck(V_mex.s1,V.s1,tol,true);
-valuecheck(V_mex.s2,V.s2,tol,true);
-valuecheck(c_mex.y0,c.y0,tol,true);
-valuecheck(c_mex.D,c.D,tol,true);
+tol = 1e-6;
+%valuecheck(V.S,V_tvlqr.S,tol,true);
+valuecheck(V.s1,V_tvlqr.s1,tol,true);
+valuecheck(V.s2,V_tvlqr.s2,tol,true);
+valuecheck(c.y0,c_tvlqr.y0,tol,true);
+valuecheck(c.D,c_tvlqr.D,tol,true);
 
-comtraj = COMplanFromTracker(limp,[x0;y0],zeros(2,1),zmptraj.tspan,c_mex);
+comtraj = COMplanFromTracker(limp,[x0;y0],zeros(2,1),zmptraj.tspan,c_tvlqr);
 %comtraj = ZMPplanner(limp,[x0;y0],zeros(2,1),zmptraj);
 
 end
