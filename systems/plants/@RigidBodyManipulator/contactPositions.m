2425b015cf908d7462cfc17126800c7ba0bac49c
scottk <scottk@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-05-29 23:12:15 +0000

    contactConstraints and contactPositions optionally take contact point indexes as arguments
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6163 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/@RigidBodyManipulator/contactPositions.m
+++ systems/plants/@RigidBodyManipulator/contactPositions.m
@@ -1,4 +1,7 @@
-function [contact_pos,J,dJ] = contactPositions(obj,kinsol,body_idx)
+function [contact_pos,J,dJ] = contactPositions(obj,kinsol,body_idx,body_contacts)
+% body_idx is an array of body indexes
+% body_contacts is a cell array of vectors containing contact point indices
+% 
 % @retval p(:,i)=[px;py;pz] is the position of the ith contact point 
 % @retval J is dpdq
 % @retval dJ is ddpdqdq
@@ -12,25 +15,39 @@ if nargin<3
   body_idx = 1:length(obj.body);
 end
 
+if nargin<4
+  n_contact_pts = size([obj.body(body_idx).contact_pts],2);
+  body_contacts = [];
+else
+  n_contact_pts = length(body_idx)*vertcat(body_contacts{:});
+end
+
 d=length(obj.gravity);  % 2 for planar, 3 for 3D
-contact_pos = zeros(d,size([obj.body(body_idx).contact_pts],2))*kinsol.q(1);  
+contact_pos = zeros(d,n_contact_pts)*kinsol.q(1);
+
 if (nargout>1) 
-  J = zeros(size([obj.body(body_idx).contact_pts],2)*d,obj.num_q); 
+  J = zeros(n_contact_pts*d,obj.num_q); 
 end
 if (nargout>2)
-  dJ = sparse(size([obj.body(body_idx).contact_pts],2)*d,obj.num_q^2);
+  dJ = sparse(n_contact_pts*d,obj.num_q^2);
 end
 
 count=0;
 for i=1:length(body_idx)
-  nC = size(obj.body(body_idx(i)).contact_pts,2);
+  if isempty(body_contacts)
+    nC = size(obj.body(body_idx(i)).contact_pts,2);
+    pts_idx = 1:nC;
+  else
+    pts_idx = body_contacts{i};
+    nC = length(pts_idx);
+  end
   if nC>0
     if (nargout>2)
-      [contact_pos(:,count+(1:nC)),J(d*count+(1:d*nC),:),dJ(d*count+(1:d*nC),:)] = forwardKin(obj,kinsol,body_idx(i),obj.body(body_idx(i)).contact_pts);
+      [contact_pos(:,count+(1:nC)),J(d*count+(1:d*nC),:),dJ(d*count+(1:d*nC),:)] = forwardKin(obj,kinsol,body_idx(i),obj.body(body_idx(i)).contact_pts(:,pts_idx));
     elseif (nargout>1)
-      [contact_pos(:,count+(1:nC)),J(d*count+(1:d*nC),:)] = forwardKin(obj,kinsol,body_idx(i),obj.body(body_idx(i)).contact_pts);
+      [contact_pos(:,count+(1:nC)),J(d*count+(1:d*nC),:)] = forwardKin(obj,kinsol,body_idx(i),obj.body(body_idx(i)).contact_pts(:,pts_idx));
     else
-      contact_pos(:,count+(1:nC)) = forwardKin(obj,kinsol,body_idx(i),obj.body(body_idx(i)).contact_pts);
+      contact_pos(:,count+(1:nC)) = forwardKin(obj,kinsol,body_idx(i),obj.body(body_idx(i)).contact_pts(:,pts_idx));
     end
     count = count + nC;
   end
