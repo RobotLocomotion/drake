9dc3ee4ccfd649a0b8af8c6c23976467d3c2dd61
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-06-04 01:14:26 +0000

    added super cool mex debugging tool
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6222 c9849af7-e679-4ec6-a44e-fc146a885bd3

---
+++ util/test/debugMexTest.cpp
@@ -0,0 +1,12 @@
+
+// prints (and returns the input)
+
+#include <mex.h>
+
+void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
+{
+  if (nrhs>0) {
+    double v = mxGetScalar(prhs[0]);
+    mexPrintf("%f\n",v);
+  }
+}
---
+++ util/test/debugMexTester.m
@@ -0,0 +1,5 @@
+function debugMexTester
+
+for i=1:10
+  debugMexEval('debugMexTest',i);
+end
