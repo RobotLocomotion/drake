db362033adbf37351979b2d60b856c307e32d321
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-06-09 23:38:21 +0000

    update from kaess.  (but mex still disabled by default)
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6320 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/trajectories/ExpPlusPPTrajectory.m
+++ systems/trajectories/ExpPlusPPTrajectory.m
@@ -11,7 +11,7 @@ classdef ExpPlusPPTrajectory < Trajectory
     A       % square matrix from the matrix exponential
     alpha   % must be size(A,1) x n
     gamma   % must be dim x n x pporder
-    evalmex % higher speed eval in C++
+    mex_ptr = 0 % higher speed eval in C++
   end    
 
   methods
@@ -30,14 +30,16 @@ classdef ExpPlusPPTrajectory < Trajectory
       obj.gamma = gamma;
       obj.pporder = size(gamma,3);
       
-      gamma_reshaped = reshape(obj.gamma, size(obj.gamma,1)*size(obj.gamma,2), size(obj.gamma,3));
-      obj.evalmex = ExpPlusPPTrajectoryEvalmex(obj.breaks, obj.K, obj.A, obj.alpha, gamma_reshaped);
+      gamma_reshaped = reshape(obj.gamma, ...
+          size(obj.gamma,1)*size(obj.gamma,2), size(obj.gamma,3));
+      obj.mex_ptr = SharedDataHandle(ExpPlusPPTrajectoryEvalmex(...
+          obj.breaks, obj.K, obj.A, obj.alpha, gamma_reshaped));
     end
     
     function [y,jj] = eval(obj,t)
-      if false
+      if false % true && obj.mex_ptr~=0
         % fast C++ version
-        [y,jj] = ExpPlusPPTrajectoryEvalmex(obj.evalmex, t);
+        [y,jj] = ExpPlusPPTrajectoryEvalmex(obj.mex_ptr.getData, t);
       else
         y = zeros(size(obj.K,1),length(t));
         jj = zeros(length(t));
@@ -45,7 +47,8 @@ classdef ExpPlusPPTrajectory < Trajectory
           j = find(t(k)>=obj.breaks(1:end-1),1,'last');
           if isempty(j), j=1; end 
           trel = t(k)-obj.breaks(j);
-          y(:,k) = obj.K*expm(obj.A*trel)*obj.alpha(:,j) + squeeze(obj.gamma(:,j,:))*(trel.^(0:obj.pporder-1)');
+          y(:,k) = obj.K*expm(obj.A*trel)*obj.alpha(:,j) ...
+              + squeeze(obj.gamma(:,j,:))*(trel.^(0:obj.pporder-1)');
           jj(k) = j;
         end
         %if sum (abs(y-y_) > (0.0000001*max(abs(y),abs(y_))+eps)) > 0
