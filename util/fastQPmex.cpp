7f69a669fdbcb2f9d533a9c7ccb4eab5817b1028
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-06-11 01:40:44 +0000

    fastQPmex runs (at least on simple examples) without segfaults.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6328 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- util/fastQPmex.cpp
+++ util/fastQPmex.cpp
@@ -27,6 +27,12 @@ int fastQP(vector< Map<MatrixXd> > QblkDiag, const Map<VectorXd> f, const Map<Ma
   int M = Aeq.rows();
   int N = Aeq.cols();
   
+  if (f.rows() != N) mexErrMsgTxt("size of f doesn't match cols of Aeq");
+  if (beq.rows() !=M) mexErrMsgTxt("size of beq doesn't match rows of Aeq");
+  if (Ain.cols() !=N) mexErrMsgTxt("cols of Ain doesn't match cols of Aeq");
+  if (bin.rows() != Ain.rows()) mexErrMsgTxt("bin rows doesn't match Ain rows");
+  if (x.rows() != N) mexErrMsgTxt("x doesn't match Aeq");
+
   int n_active = active.size();
   MatrixXd Aact = MatrixXd(n_active, N);
   VectorXd bact = VectorXd(n_active);
@@ -47,7 +53,7 @@ int fastQP(vector< Map<MatrixXd> > QblkDiag, const Map<VectorXd> f, const Map<Ma
   		d = numRow*numCol;
       VectorXd Qdiag_mod = *iterQ + VectorXd::Constant(d,1e-8); // regularize
       VectorXd QinvDiag = Qdiag_mod.cwiseInverse();
-      QinvAteq.block(startrow,0,d,N)= QinvDiag.asDiagonal()*Aeq.block(0,startrow,N,d).transpose();  // Aeq.transpose().block(startrow,0,d,N)
+      QinvAteq.block(startrow,0,d,M)= QinvDiag.asDiagonal()*Aeq.block(0,startrow,M,d).transpose();  // Aeq.transpose().block(startrow,0,d,N)
       minusQinvf.segment(startrow,d) = -QinvDiag.cwiseProduct(f.segment(startrow,d));
       Qinv.push_back(QinvDiag);
       startrow=startrow+d;
@@ -55,10 +61,13 @@ int fastQP(vector< Map<MatrixXd> > QblkDiag, const Map<VectorXd> f, const Map<Ma
   		d = numRow;  assert(numRow==numCol);
       MatrixXd Q_mod = *iterQ + 1e-8*MatrixXd::Identity(d,d);
       MatrixXd thisQinv = Q_mod.inverse();
-      QinvAteq.block(startrow,0,d,N)= thisQinv*Aeq.block(0,startrow,N,d).transpose();  // Aeq.transpose().block(startrow,0,d,N)
+      QinvAteq.block(startrow,0,d,M) = thisQinv*Aeq.block(0,startrow,M,d).transpose();  // Aeq.transpose().block(startrow,0,d,N)
       minusQinvf.segment(startrow,d) = -thisQinv*f.segment(startrow,d);
       Qinv.push_back(thisQinv);
       startrow=startrow+d;
+  	}
+  	if (startrow>N) {
+  		mexErrMsgTxt("Q is too big!");
   	}
   }
 
@@ -88,18 +97,20 @@ int fastQP(vector< Map<MatrixXd> > QblkDiag, const Map<VectorXd> f, const Map<Ma
       //Solve H * [x;lam] = [-f;b] using Schur complements
       QinvAt.resize(QinvAteq.rows(), QinvAteq.cols() + Aact.rows());
 
-      int startrow=0;
-      for (vector<MatrixXd>::iterator iterQinv=Qinv.begin(); iterQinv!=Qinv.end(); iterQinv++) {
-      	int numRow = iterQinv->rows();
-      	int numCol = iterQinv->cols();
+      if (n_active>0) {
+				int startrow=0;
+				for (vector<MatrixXd>::iterator iterQinv=Qinv.begin(); iterQinv!=Qinv.end(); iterQinv++) {
+					d = iterQinv->rows();
+					int numCol = iterQinv->cols();
 
-      	if (numRow == 1 || numCol == 1) {  // it's a vector
-      		d = numRow*numCol;
-          QinvAt.block(startrow,0,d,N) << QinvAteq.block(startrow,0,d,N), ((*iterQinv).asDiagonal())*Aact.block(0,startrow,N,d).transpose();
-      	} else { // it's a matrix
-      		d = numRow;
-          QinvAteq.block(startrow,0,d,N) << QinvAteq.block(startrow,0,d,N), (*iterQinv)*Aact.block(0,startrow,N,d).transpose();
-      	}
+					if (numCol == 1) {  // it's a vector
+						QinvAt.block(startrow,0,d,M) << QinvAteq.block(startrow,0,d,M), ((*iterQinv).asDiagonal())*Aact.block(0,startrow,n_active,d).transpose();
+					} else { // it's a matrix
+						QinvAt.block(startrow,0,d,M) << QinvAteq.block(startrow,0,d,M), (*iterQinv)*Aact.block(0,startrow,n_active,d).transpose();
+					}
+				}
+      } else {
+      	QinvAt = QinvAteq;
       }
       
       lam.resize(QinvAt.cols());
@@ -157,157 +168,6 @@ int fastQP(vector< Map<MatrixXd> > QblkDiag, const Map<VectorXd> f, const Map<Ma
   }  
   return fail;
 }
-
-
-
-int fastQP2(const VectorXd Qdiag, const VectorXd f, const MatrixXd Aeq, const VectorXd beq, const MatrixXd Ain, const VectorXd bin, VectorXd* active, VectorXd* x) {
-  int fail = 0;
-  int iterCnt = 0;
-  
-  int M_in = bin.size();
-  int M = Aeq.rows();
-  int N = Aeq.cols();
-  int n_active = (*active).size();
-  
-  MatrixXd Aact = MatrixXd(n_active, N);
-  VectorXd bact = VectorXd(n_active);
-  
-            
-  int i;
-  
-  for (i=0; i<n_active; i++) {
-    Aact.row(i) = Ain.row((*active)(i));
-    bact(i) = bin((*active)(i));
-  }
- 
-  // calculate a bunch of stuff that is constant during each iteration
-  VectorXd Qdiag_mod = Qdiag + VectorXd::Constant(N,1e-8);
-    
-  VectorXd QinvDiag = Qdiag_mod.cwiseInverse();
-    
-  MatrixXd QinvAteq = QinvDiag.asDiagonal()*Aeq.transpose(); 
-  VectorXd minusQinvf = -QinvDiag.cwiseProduct(f);
-  
-  MatrixXd A;
-  VectorXd b;
-  MatrixXd QinvAt;
-  VectorXd lam, lamIneq;
-  VectorXd violated(M_in);
-  VectorXd violation;
-  
-//       cout << "nactive" << n_active << endl;
-//   return 0;
-  
-  while(1) {
-    A.resize(Aeq.rows() + Aact.rows(),N);
-    b.resize(beq.size() + bact.size());
-    A << Aeq,Aact;
-    b << beq,bact;
-    
-//     cout << "dbg: " << A << endl;
-//     cout << "dbg: " << b << endl;
-    
-    if (A.rows() > 0) {
-      //Solve H * [x;lam] = [-f;b] using Schur complements
-      QinvAt.resize(QinvAteq.rows(), QinvAteq.cols() + Aact.rows());
-      QinvAt << QinvAteq, QinvDiag.asDiagonal()*Aact.transpose();
-      
-//     cout << "dbg: " << f.transpose()*QinvAt << endl;
-//         cout << "dbg: " << A*QinvAt << endl;
-//         cout << "dbg: " << b + f.transpose()*QinvAt << endl;
-      
-      lam.resize(QinvAt.cols());
-      lam = -(A*QinvAt).ldlt().solve(b + (f.transpose()*QinvAt).transpose());
-//     cout << "dbgl: " << lam << endl;
-      
-      *x = minusQinvf - QinvAt*lam;
-      lamIneq = lam.tail(lam.size() - M);
-    } else {
-      *x = minusQinvf;
-      lamIneq.resize(0);
-    }
-    
-//     cout << "dbg: " << *x << endl;
-//     return 0;
-    
-    
-    if(Ain.rows() == 0) {
-      (*active).resize(0);
-      break;
-    }
-//     cout << "dbg: " << A << endl;
-    
-    
-    violation = Ain*(*x) - bin;
-    
-//     cout << "violation: " << violation << endl;
-    
-    violated.conservativeResize(M_in);
-    int violCount = 0;
-    for(i=0; i < M_in; i++) {
-      if (violation(i) >= 1e-6) {
-        violated(violCount) = i;
-        violCount++;
-      }
-    }
-    
-//     cout << "lamIneq: " << lamIneq << endl;
-    if (violCount == 0 && lamIneq.all()) {
-      break;
-    }    
-    violated.conservativeResize(violCount);
-//     cout << "violated: " << violated << endl;
-    int prevActiveCount = (*active).size();
-//     cout << "prevActive: " << *active << endl;
-    (*active).conservativeResize(M_in);
-    
-    int newActiveCount = 0;
-    
-    
-    
-    for (i=0; i < prevActiveCount; i++) {
-     if (lamIneq(i) < 0 ) {
-      if(violCount > 0) {
-        violCount--;
-        (*active)(newActiveCount) = violated(violCount);
-        newActiveCount++;
-      }
-     } else {
-      newActiveCount++; 
-     }
-    }
-    
-    while(violCount > 0) {
-      violCount--;
-      (*active)(newActiveCount) = violated(violCount);
-      newActiveCount++;
-    }
-    
-    (*active).conservativeResize(newActiveCount);
-    
-//     cout << "newActive: " << *active << endl;
-    
-    n_active = newActiveCount;
-    Aact.resize(n_active,N);
-    bact.resize(n_active);
-    
-    for (i=0; i<n_active; i++) {
-      Aact.row(i) = Ain.row((*active)(i));
-      bact(i) = bin((*active)(i));
-    }
-    iterCnt++;
-
-    if (iterCnt > MAX_ITER) {
-      //Default to calling this method
-      cout << "FastQP max iter reached." << endl;
-//       mexErrMsgIdAndTxt("Drake:approximateIKmex:Error", "Max iter reached. Problem is likely infeasible");
-      fail = 1;
-      return fail;
-    }
-  }  
-  return fail;
-}
-
 
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
