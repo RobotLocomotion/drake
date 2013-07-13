0c23aaa4fc26c4252ffa38d2d3afac17883663bf
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-07-13 01:49:22 +0000

    fix viewer.  cleanup old dirs
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6608 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Drake Toolbox 
-% Version 4.1.0 (2012b) 27-Feb-2013
+% Version 4.2.0 (2012b) 27-Feb-2013
 % 
 % MIT Robot Locomotion Group
 % Drake Main Directory
@@ -8,10 +8,8 @@
 % Sub-Directories
 %   systems     - core dynamical systems functionality
 %   util        - shared code used by multiple components
-%   drivers     - interface code for sensors and actuators
 %   examples    - dynamics and control examples for a number of model systems
 %
 % Scripts in this folder
-%   configure   - Checks dependencies and sets up matlab path.
-%   make        - Builds all mex files in the directory 
-%   runAllTests - Recurses through the drake directories, running all test scripts 
+%   addpath_drake
+%   rmpath_drake
