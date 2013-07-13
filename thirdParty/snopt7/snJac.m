dc6d393acf9735277a10199f82b4927b732aee57
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-07-13 01:05:58 +0000

    zapping snopt from thirdparty
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6603 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- thirdParty/snopt7/snJac.m
+++
@@ -1,7 +0,0 @@
-function  [A,iAfun,jAvar,iGfun,jGvar] = snJac(usrfun,x0,xlow,xupp,nF)
-%function [A,iAfun,jAvar,iGfun,jGvar] = snJac(usrfun,x0,xlow,xupp,nF)
-%         Finds the coordinate structure for the Jacobian.
-
-findJacOption = 17;
-
-[A,iAfun,jAvar,iGfun,jGvar] = snoptcmex(findJacOption,usrfun,x0,xlow,xupp,nF);
