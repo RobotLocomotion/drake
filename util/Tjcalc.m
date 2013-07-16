282e2bbd851dd06858035f0e985e5f78bf613e19
scottk <scottk@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-07-16 14:36:52 +0000

    corrected comment
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6655 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- util/Tjcalc.m
+++ util/Tjcalc.m
@@ -1,6 +1,6 @@
 function TJ = Tjcalc(pitch,q)
-% single joint kinematic transformation for planar models
-% this is the kinematic analog to jcalcp from the spatial vector library
+% single joint kinematic transformation
+% this is the kinematic analog to jcalc from the spatial vector library
 
 if pitch == 0				% revolute joint
   TJ = [rotz(q),zeros(3,1);zeros(1,3),1];
