d8129612f77d734f420d7978b48a52ba255cb228
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-07-18 19:55:38 +0000

    adds support for control-affine rigidbodyforceelements (for tim's thruster model) i am ironman.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6710 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/RigidBodyForceElement.m
+++ systems/plants/RigidBodyForceElement.m
@@ -1,15 +1,19 @@
 classdef RigidBodyForceElement
   
-  properties
+  properties 
     name
+    direct_feedthrough_flag = false;
   end
   
   methods
     % f_ext is a (potentially sparse) matrix with manip.getNumBodies columns
-    f_ext = computeSpatialForce(obj,manip,q,qd)
+    % B is (nq x nu) matrix which contributes a control-affine term 
+    %      + B(q,qd)*u 
+    % to the manipulator dynamics
+    [f_ext,B] = computeSpatialForce(obj,manip,q,qd)
   end
   
-  methods
+  methods    
     function obj = updateBodyIndices(obj,map_from_old_to_new)
       % intentionally do nothing. overload if necessary
     end
