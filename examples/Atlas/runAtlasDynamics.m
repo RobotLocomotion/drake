b55d3f7a90538b47b0a64a1bea478f56814a38a1
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-07-19 12:13:21 +0000

    updated (and commented) some examples in the atlas directory to for drc tutorial
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6729 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Atlas/runAtlasDynamics.m
+++ examples/Atlas/runAtlasDynamics.m
@@ -1,25 +1,26 @@
 function runAtlasDynamics
+% Simulate the (passive) dynamics of the Atlas model 
 
-% just runs it as a passive system 
-
-% just runs it as a passive system for now
+% Load the model with a floating base
 options.floating = true;
 options.dt = 0.001;
 r = Atlas('urdf/atlas_minimal_contact.urdf',options);
 
+% Initialize the viewer
 v = r.constructVisualizer;
 v.display_dt = 0.01;
 
+% Compute a feasible set of initial conditions for the simulation (e.g. no
+% penetration)
 x0 = Point(r.getStateFrame);
 x0 = resolveConstraints(r,x0);
 
-if (1)
-  % Run simulation, then play it back at realtime speed
+if (1) % Run simulation, then play it back at realtime speed
   tic;
   xtraj = simulate(r,[0 2],x0);
   toc;
   playback(v,xtraj,struct('slider',true));
-else    
+else % View the simulation as it is being computed
   % we are knowingly breaking out to a simulink model with the cascade on the following line.
   warning('off','Drake:DrakeSystem:UnsupportedSampleTime');  
   sys = cascade(r,v);
