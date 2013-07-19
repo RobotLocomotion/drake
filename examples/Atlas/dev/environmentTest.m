54926bab254a3799eb69e597f4e98648f438a6e9
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-07-19 12:13:21 +0000

    updated (and commented) some examples in the atlas directory to for drc tutorial
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6729 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Atlas/dev/environmentTest.m
+++ examples/Atlas/dev/environmentTest.m
@@ -6,15 +6,15 @@ r = TimeSteppingRigidBodyManipulator('',.001);
 %r = RigidBodyManipulator();
 r = addRobotFromURDF(r,'../urdf/atlas_minimal_contact.urdf',[0;0;0],[],struct('floating',true));
 %r = addRobotFromURDF(r,'/Users/russt/drc/software/models/mit_gazebo_models/modified_golf_cart/model3.sdf');
-r = addRobotFromURDF(r,'steering.urdf',[.6;0;0]);
+r = addRobotFromURDF(r,'steering.urdf',[0;0;0]);
 v = r.constructVisualizer();
 
-lhand = findLink(r,'l_hand',0,true);
-rhand = findLink(r,'r_hand',0,true);
-rfoot = findLink(r,'r_foot');
-lfoot = findLink(r,'l_foot');
+lhand = findLinkInd(r,'l_hand',0,true);
+rhand = findLinkInd(r,'r_hand',0,true);
+rfoot = findLinkInd(r,'r_foot');
+lfoot = findLinkInd(r,'l_foot');
 
-wheel = findLink(r,'steering_wheel',0,true);
+wheel = findLinkInd(r,'steering_wheel',0,true);
 
 cost = Point(r.getStateFrame,1);
 cost.base_x = 100;
@@ -37,8 +37,8 @@ v.draw(0,[q0;0*q0]);
 kinsol = doKinematics(r,q0);
 x = forwardKin(r,kinsol,wheel,[[0.1;-.2;-0.05],[0.1;.2;-0.05]]);
 
-rfoot0 = forwardKin(r,kinsol,rfoot,[0;0;0],true);
-lfoot0 = forwardKin(r,kinsol,lfoot,[0;0;0],true);
+rfoot0 = forwardKin(r,kinsol,rfoot,[nan;nan;0],true);
+lfoot0 = forwardKin(r,kinsol,lfoot,[nan;nan;0],true);
 %com = mean([rfoot0(1:3),lfoot0(1:3)],2); com(3)=1;
 com(1:3,1) = nan;
 
