a0df0ae9cb70009e1aef163c166fe12966478c23
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-07-21 03:09:41 +0000

    caught typos in floating=1 derivative calcs. added first derivatives for quaternions.  not tested yet. added test case to testBrickKinematics.  currently fails because quaternion floating base dynamics are not implemented yet.  will do that next.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6745 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/test/testBrickKinematics.m
+++ systems/plants/test/testBrickKinematics.m
@@ -1,21 +1,22 @@
 function testBrickKinematics
 
-options.floating = true;
-m = RigidBodyManipulator('FallingBrick.urdf',options);
-
-nq=getNumDOF(m);
-for i=1:25
-  q = randn(nq,1); qd = randn(nq,1);
-  options.grad_method = {'user','taylorvar'};
-%  [x,J] = geval(1,@contactPositions,m,q,options);
-
-  options.grad_method = 'taylorvar';
-  [x,J,dJ] = geval(1,@contactPositions,m,q,options);
-  kinsol = doKinematics(m,q,false,false,qd);
-  [~,~,Jdot] = contactPositionsJdot(m,kinsol);
-  valuecheck(Jdot,matGradMult(reshape(dJ,3*getNumContacts(m)*nq,nq),qd));
+for fb = {'rpy','RPY','quat'};
+  options.floating = fb{1};
+  m = RigidBodyManipulator('FallingBrick.urdf',options);
+  
+  nq=getNumDOF(m);
+  for i=1:25
+    q = randn(nq,1); qd = randn(nq,1);
+    options.grad_method = {'user','taylorvar'};
+    %  [x,J] = geval(1,@contactPositions,m,q,options);
+    
+    options.grad_method = 'taylorvar';
+    [x,J,dJ] = geval(1,@contactPositions,m,q,options);
+    kinsol = doKinematics(m,q,false,false,qd);
+    [~,~,Jdot] = contactPositionsJdot(m,kinsol);
+    valuecheck(Jdot,matGradMult(reshape(dJ,3*getNumContacts(m)*nq,nq),qd));
+  end
 end
-
 
 
 end
