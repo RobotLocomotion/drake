c96db26e76b28e9f70aaa3c10664a3b8a20dd5d5
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-07-25 01:42:33 +0000

    moving examples out of drake and into their own pod
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6788 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/ZMP/CartTable2DFT.m
+++
@@ -1,8 +0,0 @@
-function fr = CartTable2DFT
-% coordinate frame for the cart-table force/torque sensor 
-   
-coords{1}=['force_x'];
-coords{2}=['force_z'];
-coords{3}='torque';
-
-fr = SingletonCoordinateFrame('CartTable2DFT',3,'f',coords);
