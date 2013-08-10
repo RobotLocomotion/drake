428a172d0abeb3f198e46e661f2aa720e88f5d37
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-08-10 22:22:37 +0000

    updating multicast scripts to work on ubuntu
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@6911 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- util/setup_loopback_multicast.sh
+++ util/setup_loopback_multicast.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Note that you must run this script as superuser (e.g. using sudo) 
 
-loopback_interface=`ifconfig | grep -m 1 -i loopback | cut -d : -f1`;
+loopback_interface=`ifconfig | grep -m 1 -i loopback | cut -d : -f1 | cut -d ' ' -f1`;
+#echo loopback interface is $loopback_interface
 
 if [ `uname -s` == "Darwin" ]; then
   # loopback appears to be multicast by default...
