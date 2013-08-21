eaf7ab68f2e878db2a659f9249b8dcfef227e6d7
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-08-21 15:07:00 +0000

    more robust command line interactions: ok to fail with missing dependency handle vrml_enabled better for nodisplay case.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@7016 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- cmake/add_matlab_unit_tests.pl
+++ cmake/add_matlab_unit_tests.pl
@@ -16,6 +16,6 @@ while (<$in>) {
   $testname = $testdir."/".$test;
   $testname =~ s/\Q$DRAKE_ROOT\///;
 
-  print $ctestfile "ADD_TEST($testname \"matlab\" \"-nosplash\" \"-nodisplay\" \"-r\" \"addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_drake; try, feval('$test'); catch(ex), disp(getReport(ex,'extended')); force_close_system; exit(-1); end; force_close_system; exit(0)\")\n";
+  print $ctestfile "ADD_TEST($testname \"matlab\" \"-nosplash\" \"-nodisplay\" \"-r\" \"addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_drake; try, feval('$test'); catch ex, disp(getReport(ex,'extended')); force_close_system; exit(~strncmp(ex.identifier,'Drake:MissingDependency',23)); end; force_close_system; exit(0)\")\n";
   print $ctestfile "SET_TESTS_PROPERTIES($testname PROPERTIES  WORKING_DIRECTORY \"$testdir\")\n";
 }
