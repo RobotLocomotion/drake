4e419f2f69a8ab7b7fc3ee27c6371e8808bc7931
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-08-21 15:06:47 +0000

    script for adding matlab unit tests (at test time instead of compile time)
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@7015 c9849af7-e679-4ec6-a44e-fc146a885bd3

---
+++ cmake/add_matlab_unit_tests.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+
+# Note: this must be run from the root directory of drake
+
+$CMAKE_INSTALL_PREFIX = `cmake pod-build -L | grep CMAKE_INSTALL_PREFIX | cut -d "=" -f2 | tr -d '[:space:]'`;
+$DRAKE_ROOT = `pwd | tr -d '[:space:]'`;
+
+# write unit tests to .matlab_ctests
+system("matlab -nodisplay -r \"addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_drake; options.gui = false; options.autorun = false; options.test_list_file = 'pod-build/matlab_ctests'; unitTest(options); exit;\"");
+
+open(my $in, 'pod-build/matlab_ctests');
+open(my $ctestfile, '>>', 'pod-build/CTestTestfile.cmake');
+
+while (<$in>) {
+  ($test,$testdir) = split(' ');
+  $testname = $testdir."/".$test;
+  $testname =~ s/\Q$DRAKE_ROOT\///;
+
+  print $ctestfile "ADD_TEST($testname \"matlab\" \"-nosplash\" \"-nodisplay\" \"-r\" \"addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_drake; try, feval('$test'); catch(ex), disp(getReport(ex,'extended')); force_close_system; exit(-1); end; force_close_system; exit(0)\")\n";
+  print $ctestfile "SET_TESTS_PROPERTIES($testname PROPERTIES  WORKING_DIRECTORY \"$testdir\")\n";
+}
