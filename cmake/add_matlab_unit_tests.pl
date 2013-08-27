f66259298d986db13ccf2c6919d206443a46372d
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-08-27 09:48:30 +0000

    adds output with random number generator info to make it easy to recreate a random test.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@7082 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- cmake/add_matlab_unit_tests.pl
+++ cmake/add_matlab_unit_tests.pl
@@ -20,6 +20,6 @@ while (<$in>) {
 #  $failcondition = "1";   # missing dependency => failure
   $failcondition = "~strncmp(ex.identifier,'Drake:MissingDependency',23)";  # missing dependency => pass
 
-  print $ctestfile "ADD_TEST($testname \"matlab\" \"-nosplash\" \"-nodisplay\" \"-r\" \"addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_$POD_NAME; rng('shuffle'); try, feval('$test'); catch ex, disp(getReport(ex,'extended')); force_close_system; exit($failcondition); end; force_close_system; exit(0)\")\n";
+  print $ctestfile "ADD_TEST($testname \"matlab\" \"-nosplash\" \"-nodisplay\" \"-r\" \"addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_$POD_NAME; rng('shuffle'); rng_state=rng; try, feval('$test'); catch ex, disp(getReport(ex,'extended')); fprintf(1,'To reproduce this test use "rng(%d,''%s'')"\n',rng_state.Seed,rng_state.Type); force_close_system; exit($failcondition); end; force_close_system; exit(0)\")\n";
   print $ctestfile "SET_TESTS_PROPERTIES($testname PROPERTIES  WORKING_DIRECTORY \"$testdir\")\n";
 }
