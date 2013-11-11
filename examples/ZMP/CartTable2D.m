fc3f0374a6ea2c7cdbd6e9e5c6ce760516a999ec
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-11-11 12:48:09 +0000

    fix CartTable example (for new planar RBM class)
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@7685 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/ZMP/CartTable2D.m
+++ examples/ZMP/CartTable2D.m
@@ -5,16 +5,16 @@ classdef CartTable2D < TimeSteppingRigidBodyManipulator
       obj = obj@TimeSteppingRigidBodyManipulator('CartTable.urdf',0.005,struct('floating',true,'twoD',true));
       
       obj = addSensor(obj,FullStateFeedbackSensor());
-      s = ContactForceTorqueSensor(obj,findLinkInd(obj,'base'),[0;0],0);
-      s.frame = CartTable2DFT;
+      s = ContactForceTorqueSensor(obj,findLinkInd(obj,'base'),zeros(3,1),zeros(3,1));
+%      s.frame = CartTable2DFT;
       obj = addSensor(obj,s);
       obj = compile(obj);
     end
     
-    function v = constructVisualizer(obj)
-%      v = MultiVisualizer({constructVisualizer@TimeSteppingRigidBodyManipulator(obj),obj.sensor{1}});
-      v = obj.sensor{1};
-    end
+%    function v = constructVisualizer(obj)
+%%      v = MultiVisualizer({constructVisualizer@TimeSteppingRigidBodyManipulator(obj),obj.sensor{1}});
+%      v = obj.sensor{1};
+%    end
   end
 
   methods (Static = true)
