dfb231dc76058222e94ee73d042cd384bb7838c7
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-11-11 12:47:33 +0000

    added backdoor to disable playback during “make test”.  should speed things up considerably.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@7683 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/visualizers/Visualizer.m
+++ systems/visualizers/Visualizer.m
@@ -66,6 +66,11 @@ classdef Visualizer < DrakeSystem
       %   @param options visualizer configuration:
       %                     slider: create playback slider to control time and speed
       
+      global g_disable_playback;
+      if g_disable_playback % evaluates to false if empty
+        return;
+      end
+      
       typecheck(xtraj,'Trajectory');
       if (xtraj.getOutputFrame()~=obj.getInputFrame)
         xtraj = xtraj.inFrame(obj.getInputFrame);  % try to convert it
