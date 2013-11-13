87615c0723d7200e3329ef58aa47c970e416bc58
scottk <scottk@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-11-13 13:28:38 +0000

    adding findJointInd passthrough in TSRBM
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@7702 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/TimeSteppingRigidBodyManipulator.m
+++ systems/plants/TimeSteppingRigidBodyManipulator.m
@@ -673,9 +673,13 @@ classdef TimeSteppingRigidBodyManipulator < DrakeSystem
     function grav = getGravity(obj)
       grav = obj.manip.gravity;
     end
-    
+
+    function joint_ind = findJointInd(model,varargin)
+      joint_ind = findJointInd(model.manip,varargin{:});
+    end
+
     function body_ind = findLinkInd(model,varargin)
-        body_ind = findLinkInd(model.manip,varargin{:});
+      body_ind = findLinkInd(model.manip,varargin{:});
     end
     
     function body = findLink(model,varargin)
