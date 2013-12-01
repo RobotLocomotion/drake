dc9aaa5717310381d92042d56e4215834d492b07
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-12-01 02:02:07 +0000

    resolves occasionally failing testOrientation, by improving the numerics of rotmat2quat
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@7878 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- util/rotmat2quat.m
+++ util/rotmat2quat.m
@@ -3,33 +3,36 @@ function q=rotmat2quat(M)
 % http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/
 % Pay close attention that there are two quaternions for the same rotation
 % matrix!!!, namely quat2rotmat(q) = quat2rotmat(-q).
-traceM = 1+trace(M);
-if(traceM>eps)
-    w = sqrt(traceM)/2;
+
+[val,ind] = max([1 1 1; 1 -1 -1; -1 1 -1; -1 -1 1]*diag(M));
+
+switch(ind)
+  case 1  % val = trace(M)
+    w = sqrt(1+val)/2;
     w4 = w*4;
     x = (M(3,2)-M(2,3))/w4;
     y = (M(1,3)-M(3,1))/w4;
     z = (M(2,1)-M(1,2))/w4;
-else
-    if(M(1,1)>M(2,2)&&M(1,1)>M(3,3))
-        s = 2*sqrt(1+M(1,1)-M(2,2)-M(3,3));
-        w = (M(3,2)-M(2,3))/s;
-        x = 0.25*s;
-        y = (M(1,2)+M(2,1))/s;
-        z = (M(1,3)+M(3,1))/s;
-    elseif(M(2,2)>M(3,3))
-        s = 2*(sqrt(1+M(2,2)-M(1,1)-M(3,3)));
-        w = (M(1,3)-M(3,1))/s;
-        x = (M(1,2)+M(2,1))/s;
-        y = 0.25*s;
-        z = (M(2,3)+M(3,2))/s;
-    else
-        s = 2*(sqrt(1+M(3,3)-M(2,2)-M(1,1)));
-        w = (M(2,1)-M(1,2))/s;
-        x = (M(1,3)+M(3,1))/s;
-        y = (M(2,3)+M(3,2))/s;
-        z = 0.25*s;
-    end
+  case 2 % val = M(1,1) - M(2,2) - M(3,3)
+    s = 2*sqrt(1+val);
+    w = (M(3,2)-M(2,3))/s;
+    x = 0.25*s;
+    y = (M(1,2)+M(2,1))/s;
+    z = (M(1,3)+M(3,1))/s;
+  case 3 % val = M(2,2) - M(1,1) - M(3,3)
+    s = 2*(sqrt(1+val));
+    w = (M(1,3)-M(3,1))/s;
+    x = (M(1,2)+M(2,1))/s;
+    y = 0.25*s;
+    z = (M(2,3)+M(3,2))/s;
+  otherwise % val = M(3,3) - M(2,2) - M(1,1) 
+    s = 2*(sqrt(1+val));
+    w = (M(2,1)-M(1,2))/s;
+    x = (M(1,3)+M(3,1))/s;
+    y = (M(2,3)+M(3,2))/s;
+    z = 0.25*s;
 end
+
 q = [w;x;y;z];
+
 end
