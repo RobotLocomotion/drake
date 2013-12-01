0255d79932030bc4638483e5b59c1a92e370d001
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-12-01 02:38:52 +0000

    resolves occasional unit test error on Pendulum trigPolyTest (and silences the appropriate associated warnings).
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@7880 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Pendulum/test/trigPolyTest.m
+++ examples/Pendulum/test/trigPolyTest.m
@@ -2,7 +2,11 @@ function trigPolyTest
 
 options.replace_output_w_new_state = true;
 
+w = warning('off','Drake:RigidBody:SimplifiedCollisionGeometry');
+warning('off','Drake:RigidBodyManipulator:UnsupportedContactPoints');
 p1 = PlanarRigidBodyManipulator('../Pendulum.urdf');
+warning(w);
+
 tp1 = extractTrigPolySystem(p1,options);
 
 oldpath=addpath('..');
@@ -28,5 +32,8 @@ for i=1:25
   xn = x+dt*xdot;
   xnp = Point(tp1.getStateFrame,xp+dt*xdotp);
   xnp = double(xnp.inFrame(p1.getStateFrame));
+
+  xnp(1) = mod(xnp(1),2*pi);
+  xn(1) = mod(xn(1),2*pi);
   valuecheck(xn,xnp,1e-2);
 end
