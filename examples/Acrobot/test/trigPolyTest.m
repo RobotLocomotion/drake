8f083fda05b8b70e662bafd0dc7c784e30f46b31
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-12-01 22:00:45 +0000

    should resolve occasional failing test trigPolyTest in acrobot directory.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@7890 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Acrobot/test/trigPolyTest.m
+++ examples/Acrobot/test/trigPolyTest.m
@@ -2,7 +2,10 @@ function trigPolyTest
 
 options.replace_output_w_new_state = true;
 
+w = warning('off','Drake:RigidBody:SimplifiedCollisionGeometry');
+warning('off','Drake:RigidBodyManipulator:UnsupportedContactPoints');
 p1 = PlanarRigidBodyManipulator('../Acrobot.urdf');
+warning(w);
 tp1 = extractTrigPolySystem(p1,options);
 
 oldpath=addpath('..');
@@ -12,10 +15,10 @@ path(oldpath);
 tp2 = extractTrigPolySystem(p2,options);
 
 w = warning('off','Drake:DrakeSystem:ConstraintsNotEnforced');
-tf = .5;
+
 % test numerically, because f and e are different (one has the inertial
 % matrix in f, the other has it in e)
-for i=1:5
+for i=1:25
   x = Point(p1.getStateFrame,randn(4,1));
   xp = x.inFrame(tp1.getStateFrame);
   x = double(x); xp=double(xp);
@@ -26,10 +29,14 @@ for i=1:5
   valuecheck(xdot,p2.dynamics(0,x,u));
   valuecheck(xdotp,tp2.dynamics(0,xp,u));
   
-  xtraj = simulate(p1,[0 tf],x);
-  xptraj = simulate(tp1,[0 tf],xp);
-  
-  valuecheck(xtraj.eval(tf),double(Point(tp1.getStateFrame,xptraj.eval(tf)).inFrame(p1.getStateFrame)),1e-2);
+  dt = .1;
+  xn = x+dt*xdot;
+  xnp = Point(tp1.getStateFrame,xp+dt*xdotp);
+  xnp = double(xnp.inFrame(p1.getStateFrame));
+
+  xnp(1:2) = mod(xnp(1:2),2*pi);
+  xn(1:2) = mod(xn(1:2),2*pi);
+  valuecheck(xn,xnp,1e-2);
 end
 
 warning(w);
