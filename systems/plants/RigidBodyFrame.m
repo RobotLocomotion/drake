bb5e6d664a458786ac203d9ca9dc8a68bed657e5
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-12-06 21:17:39 +0000

    added support for rigidbodyframes specified directly in the urdf.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@7927 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/RigidBodyFrame.m
+++ systems/plants/RigidBodyFrame.m
@@ -4,13 +4,17 @@ classdef RigidBodyFrame
   % class provides the common utilities for this
   
   methods
-    function obj = RigidBodyFrame(body_ind,xyz,rpy)
+    function obj = RigidBodyFrame(body_ind,xyz,rpy,name)
       sizecheck(body_ind,1);
       sizecheck(xyz,[3 1]);
       sizecheck(rpy,[3 1]);
       obj.body_ind = body_ind;
       obj.T = [rotz(rpy(3))*roty(rpy(2))*rotx(rpy(1)),xyz; 0,0,0,1];
       obj.Ti = inv(obj.T);
+      if nargin>3
+        typecheck(name,'char');
+        obj.name = name;
+      end
     end
     
     function [x,J] = forwardKin(obj,model,kinsol,pts) % todo: support rotation_type?
@@ -51,6 +55,7 @@ classdef RigidBodyFrame
   end
 
   properties
+    name
     body_ind
     T
     Ti
