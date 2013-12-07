73e56bb67051c184987530474f11caa93aff213f
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-12-07 21:35:28 +0000

    added frame support to forwardJacDot
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@7933 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/@RigidBodyManipulator/forwardJacDot.m
+++ systems/plants/@RigidBodyManipulator/forwardJacDot.m
@@ -1,4 +1,4 @@
-function Jdot = forwardJacDot(obj,kinsol,body_ind,pts,robotnum)
+function Jdot = forwardJacDot(obj,kinsol,body_or_frame_ind,pts,robotnum)
 
 % same input as [x,J] = forwardKin but returns Jdot
 % note: you must have called kinsol = doKinematics with qd passed in as the
@@ -7,7 +7,7 @@ if nargin<5, robotnum=1; end
 if nargin<4, pts=[]; end
 
 % todo: zap this after the transition
-if isa(body_ind,'RigidBody'), error('support for passing in RigidBody objects has been removed.  please pass in the body index'); end
+if isa(body_or_frame_ind,'RigidBody'), error('support for passing in RigidBody objects has been removed.  please pass in the body index'); end
 
 if (kinsol.mex)
   if (obj.mex_model_ptr==0)
@@ -19,10 +19,10 @@ if (kinsol.mex)
   if(body_ind == 0)
     Jdot = forwardKinmex(obj.mex_model_ptr,kinsol.q,0,robotnum,true);
   else
-    Jdot = forwardKinmex(obj.mex_model_ptr,kinsol.q,body_ind,pts,false,true);
+    Jdot = forwardKinmex(obj.mex_model_ptr,kinsol.q,body_or_frame_ind,pts,false,true);
   end
 else
-  if (body_ind == 0) 
+  if (body_or_frame_ind == 0) 
     nq=getNumDOF(obj);
     
     % return center of mass for the entire model
@@ -41,10 +41,19 @@ else
       end
     end
   else
+    if (body_or_frame_ind < 0)
+      frame = obj.frame(-body_or_frame_ind);
+      body_ind = frame.body_ind;
+      Tframe = frame.T;
+    else
+      body_ind = body_or_frame_ind;
+      Tframe=eye(4);
+    end
+  
     m = size(pts,2);
     pts = [pts;ones(1,m)];
     
-    Jdot = reshape(kinsol.dTdqdot{body_ind}*pts,obj.getNumDOF,[])';
+    Jdot = reshape(kinsol.dTdqdot{body_ind}*Tframe*pts,obj.getNumDOF,[])';
   end
   
 end
