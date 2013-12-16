79f17997dc1f82b0cd8fd98663c71ce278205b25
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-12-16 11:34:13 +0000

    support objective, constraints, or objectiveAndConstraints .  clean up orders (always inequality then equality)
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@8008 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- solvers/NonlinearProgram.m
+++ solvers/NonlinearProgram.m
@@ -11,21 +11,103 @@ classdef NonlinearProgram
     num_decision_vars
     num_nonlinear_inequality_constraints
     num_nonlinear_equality_constraints
-    Aeq,beq
     Ain,bin
+    Aeq,beq
     lb,ub
     iGfun,jGvar  % sparsity pattern in objective and nonlinear constraints
     solver
     default_options
     grad_method
   end
+  
+  properties (Access=private)
+    objcon_logic = false;
+  end
 
-  methods (Abstract)
-    % @param x = current value of decision variables
-    % @retval f = [ objective(x); 
-    %               nonlinear_equality_constraints(x);
-    %               nonlinear_inequality_constraints(x)]
-    f = objectiveAndNonlinearConstraints(obj,x);
+  methods % A subset of these MUST be overloaded
+    
+    % PLEASE READ THIS
+    %  some solvers take the objective and constraints as
+    %  separate callbacks.  others (e.g. snopt) takes 
+    %  a single callback which computes both.  i want to 
+    %  support the "computes both" functionality, because
+    %  it can be more efficient for expensive function
+    %  evaluations (e.g. often there is computation that
+    %  is shared between computing the objective value
+    %  and the constraint value).  
+    %  so, i've implemented the simple logic below so 
+    %  that derived classes can implement the objective
+    %  and constraints methods independently, or all 
+    %  in one method.  BUT ALL DERIVED CLASSES MUST
+    %  OVERLOAD ONE OR THE OTHER VERSIONS.
+    
+    function [f,df] = objective(obj,x)
+      if (obj.objcon_logic) % then i'm getting called from objectiveAndNonlinearConstraints
+        error('Drake:NonlinearProgram:AbstractMethod','all derived classes must implement objective or objectiveAndNonlinearConstraints');
+      end
+      
+      % todo: cache the input/output pairs so I only call
+      % this once (in objective OR nonlinearConstraints)
+      obj.objcon_logic = true;
+      if nargout>1
+        [fgh,dfgh] = objectiveAndNonlinearConstraints(obj,x);
+        df = dfgh(1,:);
+      else
+        fgh = objectiveAndNonlinearConstraints(obj,x);
+      end
+      f = fgh(1);
+    end
+    
+    function [g,h,dg,dh] = nonlinearConstraints(obj,x)
+      if (obj.objcon_logic) % then i'm getting called from objectiveAndNonlinearConstraints
+        error('Drake:NonlinearProgram:AbstractMethod','all derived classes must implement objective or objectiveAndNonlinearConstraints');
+      end
+      
+      % todo: cache the input/output pairs so I only call
+      % this once (in objective OR nonlinearConstraints)
+      obj.objcon_logic = true;
+      if nargout>2
+        [fgh,dfgh] = objectiveAndNonlinearConstraints(obj,x);
+        dg = dfgh(1 + (1:obj.num_nonlinear_inequality_constraints),:);
+        dh = dfgh(1+obj.num_nonlinear_inequality_constraints + (1:obj.num_nonlinear_equality_constraints),:);
+      else
+        fgh = objectiveAndNonlinearConstraints(obj,x);
+      end
+      g = fgh(1 + (1:obj.num_nonlinear_inequality_constraints));
+      h = fgh(1+obj.num_nonlinear_inequality_constraints + (1:obj.num_nonlinear_equality_constraints));
+    end
+    
+    function [fgh,dfgh] = objectiveAndNonlinearConstraints(obj,x)
+      % @param x = current value of decision variables
+      % @retval fgh = [ objective(x);
+      %               nonlinear_inequality_constraints(x);
+      %               nonlinear_equality_constraints(x)]
+      
+      if (obj.objcon_logic) % then i'm getting called from objective or nonlinearConstraints
+        error('Drake:NonlinearProgram:AbstractMethod','all derived classes must implement objective or objectiveAndNonlinearConstraints');
+      end
+
+      obj.objcon_logic = true;
+      if nargout>1
+        [f,df] = objective(obj,x);
+        if (obj.num_nonlinear_inequality_constraints + obj.num_nonlinear_equality_constraints)
+          [g,h,dg,dh] = nonlinearConstraints(obj,x);
+          fgh = [f;g;h];
+          dfgh = [df;dg;dh];
+        else
+          fgh = f;
+          dfgh = df;
+        end
+      else
+        f = objective(obj,x);
+        if (obj.num_nonlinear_inequality_constraints + obj.num_nonlinear_equality_constraints)
+          [g,h] = nonlinearConstraints(obj,x);
+          fgh = [f;g;h];
+        else
+          fgh = f;
+        end
+      end
+    end
   end
   
   methods
@@ -75,8 +157,8 @@ classdef NonlinearProgram
       global SNOPT_USERFUN;
       SNOPT_USERFUN = @snopt_userfun;
       
-      A = [obj.Aeq;obj.Ain];
-      b = [obj.beq;obj.bin];
+      A = [obj.Ain;obj.Aeq];
+      b = [obj.bin;obj.beq];
       if isempty(obj.lb) obj.lb = -inf(obj.num_decision_vars,1); end
       if isempty(obj.ub) obj.ub = inf(obj.num_decision_vars,1); end
 
@@ -102,30 +184,38 @@ classdef NonlinearProgram
       function [f,G] = snopt_userfun(x)
         [f,G] = geval(@obj.objectiveAndNonlinearConstraints,x);
         f = [f;0*b];
-        G = G(:);
+        
+        if ~isempty(obj.iGfun) && ~isempty(obj.jGvar)
+          G = G(sub2ind(size(G),obj.iGfun,obj.jGvar));
+        else
+          G = G(:);
+        end
       end      
 
       if isempty(A)
         [x,objval,exitflag] = snopt(x0, ...
           obj.lb,obj.ub, ...
-          [-inf;zeros(obj.num_nonlinear_equality_constraints,1);-inf(obj.num_nonlinear_inequality_constraints,1);obj.beq;-inf(size(obj.bin))],[inf;zeros(obj.num_nonlinear_equality_constraints+obj.num_nonlinear_inequality_constraints,1);obj.beq;obj.bin],...
+          [-inf;-inf(obj.num_nonlinear_inequality_constraints,1);zeros(obj.num_nonlinear_equality_constraints,1);-inf(size(obj.bin));obj.beq],[inf;zeros(obj.num_nonlinear_inequality_constraints+obj.num_nonlinear_equality_constraints,1);obj.bin;obj.beq],...
           'snoptUserfun');
       else
         [iAfun,jAvar,Avals] = find(A);
-        iAfun = iAfun + 1 + obj.num_nonlinear_equality_constraints + obj.num_nonlinear_inequality_constraints;
+        iAfun = iAfun + 1 + obj.num_nonlinear_inequality_constraints + obj.num_nonlinear_equality_constraints;
         
         if isempty(obj.iGfun) || isempty(obj.jGvar)
-          s = [1+obj.num_nonlinear_equality_constraints+obj.num_nonlinear_inequality_constraints,obj.num_decision_vars];
-          [obj.iGfun,obj.jGvar] = ind2sub(s,1:prod(s));
+          s = [1+obj.num_nonlinear_inequality_constraints+obj.num_nonlinear_equality_constraints,obj.num_decision_vars];
+          [iGfun,jGvar] = ind2sub(s,1:prod(s));
+        else
+          iGfun = obj.iGfun;
+          jGvar = obj.jGvar;
         end
           
         [x,objval,exitflag] = snopt(x0, ...
           obj.lb,obj.ub, ...
-          [-inf;zeros(obj.num_nonlinear_equality_constraints,1);-inf(obj.num_nonlinear_inequality_constraints,1);obj.beq;-inf(size(obj.bin))],[inf;zeros(obj.num_nonlinear_equality_constraints+obj.num_nonlinear_inequality_constraints,1);obj.beq;obj.bin],...
+          [-inf;-inf(obj.num_nonlinear_inequality_constraints,1);zeros(obj.num_nonlinear_equality_constraints,1);-inf(size(obj.bin));obj.beq],[inf;zeros(obj.num_nonlinear_inequality_constraints+obj.num_nonlinear_equality_constraints,1);obj.bin;obj.beq],...
           'snoptUserfun',...
           0,1,...
           Avals,iAfun,jAvar,...
-          obj.iGfun,obj.jGvar);
+          iGfun,jGvar);
       end
       
       objval = objval(1);
@@ -136,7 +226,13 @@ classdef NonlinearProgram
       if (obj.num_nonlinear_equality_constraints || obj.num_nonlinear_inequality_constraints)
         error('not implemented yet');
       end
-      [x,objval,exitflag] = fmincon(@obj.objectiveAndNonlinearConstraints,x0,obj.Ain,obj.bin,obj.Aeq,obj.beq,obj.lb,obj.ub,[],obj.default_options.fmincon);
+      if (obj.num_nonlinear_inequality_constraints + obj.num_nonlinear_equality_constraints)
+        nonlinearConstraints = @obj.nonlinearConstraint;
+      else
+        nonlinearConstraints = [];
+      end
+      [x,objval,exitflag] = fmincon(@obj.objective,x0,obj.Ain,obj.bin,obj.Aeq,obj.beq,obj.lb,obj.ub,nonlinearConstraints,obj.default_options.fmincon);
+      objval = full(objval);
     end
   end
   
