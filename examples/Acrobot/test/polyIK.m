18de985e620c88f98f85b21f6968e06dd0624edd
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-12-17 09:27:40 +0000

    fixed a scary bug in TrigPoly
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@8015 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/Acrobot/test/polyIK.m
+++ examples/Acrobot/test/polyIK.m
@@ -11,8 +11,13 @@ q = TrigPoly('q','s','c',2);
 kinsol = doKinematics(r,q);
 x = forwardKin(r,kinsol,hand,zeros(2,1));
 
-v = getTrigPolyBasis(x)
-v0 = eval(v,randn(2,1))
+q0 = [0;.5];  
+% note: general case needs to be more careful about only
+% doing this for the trig variables:
+qerr = sin((q-q0)/2);  
+objective = qerr'*qerr;
+
+v = getTrigPolyBasis([x;objective]);
 
 decision_vars = getmsspoly(v);
 objective = (decision_vars-v0)'*(decision_vars-v0);
