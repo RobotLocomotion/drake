993d60e4445366cda4f1d029265569688cb3fc86
russt <russt@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-12-18 10:24:07 +0000

    added polynomial IK test for Planar N link manipulator.  gloptipoly solves it very quickly for N<=4.  Then gets a LOT slower (not finished running yet) for N=5.
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/trunk@8022 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- examples/PlanarNLink/test/bodyKinTest.m
+++ examples/PlanarNLink/test/bodyKinTest.m
@@ -2,14 +2,14 @@ function bodyKinTest
 
 oldpath=addpath(fullfile(pwd,'..'));
 
-N = 1+ceil(rand);
+N = 1+ceil(randn);
 r = PlanarNLink(N);
 
 kinsol = doKinematics(r,randn(r.getNumDOF,1));
 
 pts = randn(2,20);
-x = forwardKin(r,kinsol,N,pts);
-pts2 = bodyKin(r,kinsol,N,x);
+x = forwardKin(r,kinsol,N+1,pts);
+pts2 = bodyKin(r,kinsol,N+1,x);
 valuecheck(pts2,pts);
 
 path(oldpath);
