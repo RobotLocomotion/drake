1a3623cce6a084a06874435cc1c59182d1300db2
Twan Koolen <tkoolen@mit.edu>
2013-12-19 10:05:45 -0500

    Move twist utility functions to their own files

--- systems/plants/@RigidBodyManipulator/geometricJacobian.m
+++ systems/plants/@RigidBodyManipulator/geometricJacobian.m
@@ -17,51 +17,11 @@ end
 
 transformedMotionSubspaces = cellfun(@transformMotionSubspace, kinsol.T(jointPath), motionSubspaces, num2cell(signs'), 'UniformOutput', false);
 J = cell2mat(transformedMotionSubspaces); % change frame from body to world
-J = adjoint(inv(kinsol.T{expressedIn})) * J; % change frame from world to expressedIn
+J = transformTwists(inv(kinsol.T{expressedIn}), J); % change frame from world to expressedIn
 end
 
 function ret = transformMotionSubspace(H, S, sign)
-% this could be sped up by not explicitly computing adjoint(H) and
-% exploiting its structure
-ret = sign * adjoint(H) * S;
-end
-
-function S = motionSubspace(body, qBody)
-if body.floating == 1 % roll, pitch, yaw
-  rpy = qBody(4 : 6);
-  E = rpydot2angularvelMatrix(rpy);
-  R = rpy2rotmat(rpy);
-  S = [zeros(3, 3), R' * E;
-    R' , zeros(3, 3)];
-elseif body.floating == 2 % quaternion
-  quat = qBody(4 : 7);
-  R = quat2rotmat(quat);
-  S = [zeros(3, 3), R' * quatdot2angularvelMatrix(quat);
-       R', zeros(3, 4)];
-  warning('Drake:RigidBodyManipulator:NotTested','Motion subspace not tested for case of quaternion floating joint.');
-elseif body.floating ~= 0
-  % twistSize = 6;
-  % S = eye(twistSize);
-  % currently not true. This would be the case if we were using twist
-  % as the parameterization of velocity across a 6-DoF joint
-  error('Drake:RigidBodyManipulator:NotImplemented','Motion subspace not implemented for this type of floating joint.');
-else
-  pitch = body.pitch;
-  axis = body.joint_axis;
-  if isinf(pitch) % prismatic joint
-    S = [zeros(3, 1); axis];
-  else % helical or revolute joint
-    S = [axis; pitch * axis];
-  end
-end
-end
-
-function ret = adjoint(H)
-R = H(1 : 3, 1 : 3);
-p = H(1 : 3, 4);
-pHat = vectorToSkewSymmetric(p);
-ret = [R, zeros(3, 3);
-  pHat * R, R];
+ret = sign * transformTwists(H, S);
 end
 
 function vIndices = velocityVectorIndices(bodies, jointIndices)
