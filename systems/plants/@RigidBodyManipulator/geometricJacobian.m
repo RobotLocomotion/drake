34f278a0454880386f61564d9bf552edd4b30cba
Twan Koolen <tkoolen@mit.edu>
2013-12-19 09:11:45 -0500

    geometricJacobian RPY and quat floating base parameterization

--- systems/plants/@RigidBodyManipulator/geometricJacobian.m
+++ systems/plants/@RigidBodyManipulator/geometricJacobian.m
@@ -8,7 +8,13 @@ function [J, vIndices] = geometricJacobian(obj, kinsol, base, endEffector, expre
 [~, jointPath, signs] = obj.findKinematicPath(base, endEffector);
 vIndices = velocityVectorIndices(obj.body, jointPath);
 
-motionSubspaces = arrayfun(@motionSubspace, obj.body(jointPath), 'UniformOutput', false);
+% motionSubspaces = arrayfun(@motionSubspace, obj.body(jointPath), jointpath, 'UniformOutput', false);
+motionSubspaces = cell(1, length(jointPath));
+for i = 1 : length(jointPath)
+  body = obj.body(jointPath(i));
+  motionSubspaces{i} = motionSubspace(body, kinsol.q(body.dofnum));
+end
+
 transformedMotionSubspaces = cellfun(@transformMotionSubspace, kinsol.T(jointPath), motionSubspaces, num2cell(signs'), 'UniformOutput', false);
 J = cell2mat(transformedMotionSubspaces); % change frame from body to world
 J = adjoint(inv(kinsol.T{expressedIn})) * J; % change frame from world to expressedIn
@@ -20,21 +26,33 @@ function ret = transformMotionSubspace(H, S, sign)
 ret = sign * adjoint(H) * S;
 end
 
-function S = motionSubspace(body)
-twistSize = 6;
-if body.floating ~= 0
-    S = eye(twistSize);
-     % currently not true. This would be the case if we were using twist
-     % as the parameterization of velocity across a 6-DoF joint
-    error('Drake:RigidBodyManipulator:NotImplemented','The geometric jacobian is not implemented properly for floating base models yet');
+function S = motionSubspace(body, qBody)
+if body.floating == 1 % roll, pitch, yaw
+  rpy = qBody(4 : 6);
+  E = rpydot2angularvelMatrix(rpy);
+  R = rpy2rotmat(rpy);
+  S = [zeros(3, 3), R' * E;
+    R' , zeros(3, 3)];
+elseif body.floating == 2 % quaternion
+  quat = qBody(4 : 7);
+  R = quat2rotmat(quat);
+  S = [zeros(3, 3), R' * quatdot2angularvelMatrix(quat);
+       R', zeros(3, 4)];
+  warning('Drake:RigidBodyManipulator:NotTested','Motion subspace not tested for case of quaternion floating joint.');
+elseif body.floating ~= 0
+  % twistSize = 6;
+  % S = eye(twistSize);
+  % currently not true. This would be the case if we were using twist
+  % as the parameterization of velocity across a 6-DoF joint
+  error('Drake:RigidBodyManipulator:NotImplemented','Motion subspace not implemented for this type of floating joint.');
 else
-    pitch = body.pitch;
-    axis = body.joint_axis;
-    if isinf(pitch) % prismatic joint
-        S = [zeros(3, 1); axis];
-    else % helical or revolute joint
-        S = [axis; pitch * axis];
-    end
+  pitch = body.pitch;
+  axis = body.joint_axis;
+  if isinf(pitch) % prismatic joint
+    S = [zeros(3, 1); axis];
+  else % helical or revolute joint
+    S = [axis; pitch * axis];
+  end
 end
 end
 
@@ -43,7 +61,7 @@ R = H(1 : 3, 1 : 3);
 p = H(1 : 3, 4);
 pHat = vectorToSkewSymmetric(p);
 ret = [R, zeros(3, 3);
-      pHat * R, R];
+  pHat * R, R];
 end
 
 function vIndices = velocityVectorIndices(bodies, jointIndices)
