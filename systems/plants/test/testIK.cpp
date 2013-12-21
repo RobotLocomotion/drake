98363aceec16375075b9def6a01c8b26205318ba
daih <daih@c9849af7-e679-4ec6-a44e-fc146a885bd3>
2013-12-21 13:14:14 +0000

    testIK.cpp fails for two reasons 1. doKinematics seems not working 2. joint limits in URDFRigidBodyManipulator is different from the MATLAB RigidBodyManipulator
    git-svn-id: https://svn.csail.mit.edu/locomotion/robotlib/branches/hongkaidev@8035 c9849af7-e679-4ec6-a44e-fc146a885bd3

--- systems/plants/test/testIK.cpp
+++ systems/plants/test/testIK.cpp
@@ -19,14 +19,14 @@ int main()
   Vector2d tspan;
   tspan<<0,1;
   VectorXd q0;
-  MATFile *pmat;
-  pmat = matOpen("../../examples/Atlas/data/atlas_fp.mat","r");
-  if(pmat == NULL)
+  MATFile *pnommat;
+  pnommat = matOpen("../../examples/Atlas/data/atlas_fp.mat","r");
+  if(pnommat == NULL)
   {
     printf("Error reading mat file\n");
     return(EXIT_FAILURE);
   }
-  mxArray* pxstar = matGetVariable(pmat,"xstar");
+  mxArray* pxstar = matGetVariable(pnommat,"xstar");
   if(pxstar == NULL)
   {
     printf("no xstar in mat file\n");
@@ -34,9 +34,23 @@ int main()
   }
   q0 = VectorXd(model->num_dof);
   memcpy(q0.data(),mxGetPr(pxstar),sizeof(double)*model->num_dof);
-  Vector3d com_des = Vector3d::Zero();
-  com_des(2) = nan("");
-  WorldCoMConstraint* com_kc = new WorldCoMConstraint(model,com_des,com_des);
+  mxArray* pq0 = mxCreateDoubleMatrix(model->num_dof,1,mxREAL);
+  memcpy(mxGetPr(pq0),q0.data(),sizeof(double)*model->num_dof);
+  model->doKinematics(q0.data());
+  Vector3d com0;
+  model->getCOM(com0);
+  printf("%4.2f %4.2f %4.2f\n",com0(0),com0(1),com0(2));
+  mxArray* pcom0 = mxCreateDoubleMatrix(3,1,mxREAL);
+  memcpy(mxGetPr(pcom0),com0.data(),sizeof(double)*3);
+  mxArray* pjoint_lb = mxCreateDoubleMatrix(model->num_dof,1,mxREAL);
+  mxArray* pjoint_ub = mxCreateDoubleMatrix(model->num_dof,1,mxREAL);
+  memcpy(mxGetPr(pjoint_lb),model->joint_limit_min,sizeof(double)*model->num_dof);
+  memcpy(mxGetPr(pjoint_ub),model->joint_limit_max,sizeof(double)*model->num_dof);
+  Vector3d com_lb = Vector3d::Zero(); 
+  Vector3d com_ub = Vector3d::Zero(); 
+  com_lb(2) = 0.9;
+  com_ub(2) = 1.0;
+  WorldCoMConstraint* com_kc = new WorldCoMConstraint(model,com_lb,com_ub,tspan);
   int num_constraints = 1;
   RigidBodyConstraint** constraint_array = new RigidBodyConstraint*[num_constraints];
   constraint_array[0] = com_kc;
@@ -46,9 +60,19 @@ int main()
   vector<string> infeasible_constraint;
   inverseKin(model,q0,q0,num_constraints,constraint_array,q_sol,info,infeasible_constraint,ikoptions);
   printf("INFO = %d\n",info);
+  MATFile *presultmat;
+  presultmat = matOpen("q_sol.mat","w");
+  mxArray* pqsol = mxCreateDoubleMatrix(model->num_dof,1,mxREAL);
+  memcpy(mxGetPr(pqsol),q_sol.data(),sizeof(double)*model->num_dof);
+  matPutVariable(presultmat,"q_sol",pqsol);
+  matPutVariable(presultmat,"com0",pcom0);
+  matPutVariable(presultmat,"q_nom",pq0);
+  matPutVariable(presultmat,"joint_limit_min",pjoint_lb);
+  matPutVariable(presultmat,"joint_limit_max",pjoint_ub);
+  matClose(presultmat);
   delete com_kc;
   delete[] constraint_array;
-  matClose(pmat);
+  matClose(pnommat);
   return 0;
 }
   
