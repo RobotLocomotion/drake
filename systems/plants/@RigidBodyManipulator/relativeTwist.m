3d3755623519f95bd6bf7b5af59ebeb97f2ac5cc
Twan Koolen <koolen.twan@gmail.com>
2014-01-21 13:37:38 -0500

    relativeTwist documentation

--- systems/plants/@RigidBodyManipulator/relativeTwist.m
+++ systems/plants/@RigidBodyManipulator/relativeTwist.m
@@ -1,4 +1,14 @@
 function twist = relativeTwist(obj, kinsol, base, endEffector, expressedIn)
+% RELATIVETWIST Computes the relative twist between base and endEffector
+% @param kinsol solution structure obtained from doKinematics
+% @param base index of rigid body that will be considered the base
+% @param endEffector index of rigid body that will be considered the end
+% effector
+% @param expressedIn index of rigid body in whose frame the end result will
+% be expressed
+% @retval relative twist of endEffector with respect to base, expressed in
+% expressedIn
+
 % note: need obj for mex pointer info in the future
 
 baseTwist = kinsol.twist{base};
