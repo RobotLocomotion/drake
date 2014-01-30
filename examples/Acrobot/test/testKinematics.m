87d2ac7ff6dc910270a77ea5ce4c7378c9fd9257
Russ Tedrake <russt@mit.edu>
2014-01-30 06:39:07 -0500

    Resolves failing acrobot kinematics unit test (the parameters didn’t match the urdf any more)

--- examples/Acrobot/test/testKinematics.m
+++ examples/Acrobot/test/testKinematics.m
@@ -1,6 +1,9 @@
 function testKinematics
 
+w = warning('off','Drake:RigidBody:SimplifiedCollisionGeometry');
+warning('off','Drake:RigidBodyManipulator:UnsupportedContactPoints');
 m = PlanarRigidBodyManipulator('../Acrobot.urdf');
+warning(w);
 b = getBody(m,1);
 b.contact_pts = randn(3,4);
 m = setBody(m,1,b);
@@ -18,7 +21,7 @@ for i=1:100
   [x,J] = geval(1,@contactPositions,m,q,options);
   
   kinsol = doKinematics(m,q,true,true);
-  hand_frame_xyz = [0;0;-1.1];  % note: this must match the frame specified in the urdf (and rpy must be zero)
+  hand_frame_xyz = [0;0;-2.1];  % note: this must match the frame specified in the urdf (and rpy must be zero)
   valuecheck(forwardKin(m,kinsol,lower_link_id,hand_frame_xyz),forwardKin(m,kinsol,hand_id,zeros(3,1)));
   valuecheck(forwardKin(m,kinsol,lower_link_id,hand_frame_xyz,1),forwardKin(m,kinsol,hand_id,zeros(3,1),1));
   valuecheck(forwardKin(m,kinsol,lower_link_id,hand_frame_xyz,2),forwardKin(m,kinsol,hand_id,zeros(3,1),2));
