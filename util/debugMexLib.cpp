b35d2bee06ab3831b533ee1e0ec3e3786e089e7d
Russ Tedrake <russt@mit.edu>
2014-02-01 11:41:11 -0500

    drake_debug_mex fails with a useful error when mex file tries a mexCallMATLAB.

--- util/debugMexLib.cpp
+++ util/debugMexLib.cpp
@@ -5,13 +5,29 @@
  *      Author: russt
  */
 
+#include <iostream>
 #include <matrix.h>
 #include <mat.h>
 
+using namespace std;
 
 mxArray *mxGetProperty(const mxArray *pa, mwIndex index, const char *propname)
 {
-	return mxGetField(pa,index,propname);
+  return mxGetField(pa,index,propname);
 }
 
+extern "C" {
+int mexCallMATLAB(int nlhs, mxArray *plhs[], int nrhs,
+  mxArray *prhs[], const char *functionName)
+{
+  cerr << "Invalid mex file: Calls to mexCallMATLAB are not supported by drake_debug_mex" << endl;
+  return 0;;
+}
 
+mxArray *mexCallMATLABWithTrap(int nlhs, mxArray *plhs[], int nrhs, mxArray *prhs[], const char *functionName)
+{
+  cerr << "Invalid mex file: Calls to mexCallMATLAB are not supported by drake_debug_mex" << endl;
+  return NULL;
+}
+
+}
