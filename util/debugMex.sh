252207be8f1d98ea00b9d8d0093491c423a20df9
Russ Tedrake <russt@mit.edu>
2014-02-10 06:15:02 -0500

    installs a more robust export script for mex debugging.  (to protect me from myself, and to work on multiple platforms)

--- util/debugMex.sh
+++
@@ -1,4 +0,0 @@
-
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Applications/MATLAB_R2014a.app/bin/maci64
-export DYLD_FORCE_FLAT_NAMESPACE=1
-export DYLD_INSERT_LIBRARIES=/Users/russt/locomotion/drake-distro/drake/pod-build/lib/libdebugMex.dylib 
