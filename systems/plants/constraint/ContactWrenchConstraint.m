6bd98932d7b8d43d8f0685f6b4875f5a95df96f9
Hongkai Dai <daihongkai@gmail.com>
2014-02-17 22:31:12 -0500

    Almost finish FrictionConeWrenchConstraint. Now this constraint supports sparse gradient matrix

--- systems/plants/constraint/ContactWrenchConstraint.m
+++ systems/plants/constraint/ContactWrenchConstraint.m
@@ -1,10 +1,16 @@
 classdef ContactWrenchConstraint < RigidBodyConstraint
   % constrain the contact forces
+  % @param tspan   - A 1 x 2 vector. The time span of the constraint being active
+  % @param robot   - A RigidBodyManipulator or a TimeSteppingRigidBodyManipulator
+  % @param num_constraint   - A scalar. The number of constraints. 
+  % @param mex_ptr          - A DrakeConstraintMexPointer
+  % @param force_size       - A 1 x 2 matrix. The size of the force matrix.
   properties(SetAccess = protected)
     tspan % a 1x2 vector
-    num_constraint
+    num_constraint;
     robot
     mex_ptr
+    force_size;
   end
   
   methods
@@ -49,10 +55,17 @@ classdef ContactWrenchConstraint < RigidBodyConstraint
     
   end
   
+  methods(Access = protected)
+    function flag = checkForceSize(obj,F)
+       F_size = size(F);
+       flag = isnumeric(F) && length(F_size) == 2 && F_size(1) == obj.force_size(1) && F_size(2) == obj.force_size(2);
+    end
+  end
+  
   methods(Abstract)
     [c,dc] = eval(obj,t,F);
-    [w,dw] = wrench(obj,t,kinsol,F); % This function computes the wrench of the contact force
-    flag = checkForceSize(obj,F);
+    [tau,dtau] = torque(obj,t,kinsol,F); % This function computes the torque of the contact force
+    [iFfun,jFvar,m,n] = force(obj,F);
     [lb,ub] = bounds(obj,t);
     name_str = name(obj,t);
   end
