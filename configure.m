d1d23c948c9c077695306ff889d9da27c5d87a28
Russ Tedrake <russt@mit.edu>
2014-02-22 09:50:48 -0500

    zapping deprecated file

--- configure.m
+++
@@ -1,2 +0,0 @@
-
-error('The Drake configure script is deprecated now that the build process uses cmake.  The new sequence is make (at the unix command prompt), then addpath_drake to prepare your current matlab session to use the drake pod.'); 
