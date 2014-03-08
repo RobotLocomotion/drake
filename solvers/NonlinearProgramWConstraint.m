d5d47f07a4e3e60da86dfa720ecd8cf4f90c337c
Hongkai Dai <daihongkai@gmail.com>
2014-03-08 16:02:52 -0500

    NonlinearProgram can handle the objective function

--- solvers/NonlinearProgramWConstraint.m
+++ solvers/NonlinearProgramWConstraint.m
@@ -5,15 +5,24 @@ classdef NonlinearProgramWConstraint < NonlinearProgram
   % @param lcon      -- A cell array of LinearConstraint
   % @param num_nlcon -- The total number of nonlinear constraints
   % @param num_lcon  -- The total number of linear constraints
+  % @param cost      -- A cell array of NonlinearConstraint or LinearConstraint.
   properties(SetAccess = protected)
     nlcon
     lcon
     num_nlcon
     num_lcon
+    cost
   end
   
   properties(Access = protected)
     nlcon_xind % A cell array, nlcon_xind{i} is an int vector recording the indices of x that is used in evaluation the i'th NonlinearConstraint
+    cost_xind_cell % A cell array, cost_xind{i} is an int vector recording the indices of x that is used in evaluating obj.cost{i}
+
+    iCfun; % The row index nonzero entries in the constraint
+    jCvar; % The column index of non-zero entries in the constraint
+    iFfun; % The row index of the non-zero entries in the objective
+    jFvar; % The column index of non-zero entries in the objective
+    
   end
   
   methods
@@ -27,6 +36,12 @@ classdef NonlinearProgramWConstraint < NonlinearProgram
       obj.nlcon_xind = {};
       obj.nlcon_ineq_idx = [];
       obj.nlcon_eq_idx = [];
+      obj.iCfun = [];
+      obj.jCvar = [];
+      obj.iFfun = [];
+      obj.jFvar = [];
+      obj.cost = {};
+      obj.cost_xind_cell = {};
     end
     
     function obj = addNonlinearConstraint(obj,cnstr,xind)
@@ -49,19 +64,21 @@ classdef NonlinearProgramWConstraint < NonlinearProgram
       obj.ceq = [obj.ceq;(cnstr.c_lb(cnstr.ceq_idx)+cnstr.c_ub(cnstr.ceq_idx))/2];
       obj.cin_ub = [obj.cin_ub;cnstr.c_ub(cnstr.cin_idx)];
       obj.cin_lb = [obj.cin_lb;cnstr.c_lb(cnstr.cin_idx)];
-      obj.iGfun = [obj.iGfun;obj.num_nlcon + cnstr.iCfun];
-      obj.jGvar = [obj.jGvar;xind(cnstr.jCvar)];
+      obj.iCfun = [obj.iGfun;obj.num_nlcon + cnstr.iCfun];
+      obj.jCvar = [obj.jCvar;xind(cnstr.jCvar)];
       obj.nlcon_ineq_idx = [obj.nlcon_ineq_idx;obj.num_nlcon+cnstr.cin_idx];
       obj.nlcon_eq_idx = [obj.nlcon_eq_idx;obj.num_nlcon+cnstr.ceq_idx];
       obj.num_nonlinear_inequality_constraints = obj.num_nonlinear_inequality_constraints + length(cnstr.cin_idx);
       obj.num_nonlinear_equality_constraints = obj.num_nonlinear_equality_constraints + length(cnstr.ceq_idx);
       obj.num_nlcon = obj.num_nlcon + cnstr.num_cnstr;
       obj.nlcon_xind = [obj.nlcon_xind,{xind}];
+      obj.iGfun = [obj.iFfun;1+obj.iCfun]; 
+      obj.jGvar = [obj.jFvar;obj.jCvar];
     end
     
     function [g,h,dg,dh] = nonlinearConstraints(obj,x)
       c = zeros(obj.num_nlcon,1);
-      dc_val = zeros(length(obj.iGfun),1);
+      dc_val = zeros(length(obj.iCfun),1);
       nlcon_count = 0;
       dc_count = 0;
       for i = 1:length(obj.nlcon)
@@ -70,7 +87,7 @@ classdef NonlinearProgramWConstraint < NonlinearProgram
         nlcon_count = nlcon_count+obj.nlcon{i}.num_cnstr;
         dc_count = dc_count+obj.nlcon{i}.nnz;
       end
-      dc = sparse(obj.iGfun,obj.jGvar,dc_val,obj.num_nlcon,obj.num_decision_vars);
+      dc = sparse(obj.iCfun,obj.jCvar,dc_val,obj.num_nlcon,obj.num_decision_vars);
       g = c(obj.nlcon_ineq_idx);
       h = c(obj.nlcon_eq_idx);
       dg = dc(obj.nlcon_ineq_idx,:);
@@ -103,9 +120,45 @@ classdef NonlinearProgramWConstraint < NonlinearProgram
       obj = obj.addLinearEqualityConstraints(cnstr_Aeq,cnstr_beq);
     end
     
+    function obj = addCost(obj,cnstr,xind)
+      % Add a cost to the objective function
+      % @param cnstr   -- A NonlinearConstraint or a LinearConstraint
+      % @param xind      -- Optional argument. x(xind) is the decision variables used in
+      % evaluating the cost. Default value is (1:obj.num_vars)
+      if(nargin<3)
+        xind = (1:obj.num_decision_vars);
+      end
+      if(~isa(cnstr,'LinearConstraint') && ~isa(cnstr,'NonlinearConstraint'))
+        error('Drake:NonlinearProgramWConstraint:UnsupportedConstraint','addCost expects a LinearConstraint or NonlinearConstraint object');
+      end
+      if(isa(cnstr,'LinearConstraint'))
+        if(cnstr.num_cnstr ~= 1)
+          error('Drake:NonlinearProgramWConstraint:WrongCost','addCost only accept scalar function');
+        end
+        obj.cost = [obj.cost,{cnstr}];
+        obj.cost_xind_cell = [obj.cost_xind_cell,{xind(cnstr.jAvar)}];
+        obj.jFvar = unique([obj.jFvar;xind(cnstr.jAvar)]);
+      elseif(isa(cnstr,'NonlinearConstraint'))
+        if(cnstr.num_cnstr ~= 1)
+          error('Drake:NonlinearProgramWConstraint:WrongCost','addCost only accept scalar function');
+        end
+        obj.cost = [obj.cost,{cnstr}];
+        obj.cost_xind_cell = [obj.cost_xind_cell,{xind(cnstr.jCvar)}];
+        obj.jFvar = unique([obj.jFvar;xind(cnstr.jCvar)]);
+      end
+      obj.iFfun = ones(length(obj.jFvar),1);
+      obj.iGfun = [obj.iFfun;1+obj.iCfun];
+      obj.jGvar = [obj.jFvar;obj.jCvar];
+    end
+    
     function [f,df] = objective(obj,x)
       f = 0;
       df = zeros(1,obj.num_decision_vars);
+      for i = 1:length(obj.cost)
+        [fi,dfi] = obj.cost{i}.eval(x(obj.cost_xind_cell{i}));
+        f = f+fi;
+        df(obj.cost_xind_cell{i}) = df(obj.cost_xind_cell{i})+dfi;
+      end
     end
   end
 end
