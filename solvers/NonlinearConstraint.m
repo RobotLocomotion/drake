83c072e8015a676e42e00f14b96b147f08ce1d5a
Hongkai Dai <daihongkai@gmail.com>
2014-03-10 16:45:45 -0400

    Made following changes based on the code review 1. Constraint does not have x_lb or x_ub, instead I made a separate class BoundingBoxConstraint to impose the constraints on x directly. 2. No 'relation' property in Constraint 3. Find some bugs in the iGfun,jGvar computation in the previous code. Now I overload the objectiveAndNonlinearConstraints method

--- solvers/NonlinearConstraint.m
+++ solvers/NonlinearConstraint.m
@@ -24,60 +24,51 @@ classdef NonlinearConstraint < Constraint
   end
   
   methods
-    function obj = NonlinearConstraint(c_lb,c_ub,x_lb,x_ub,eval_handle)
-      % @param c_lb    -- The lower bound of the constraint
-      % @param c_ub    -- The upper bound of the constraint
-      % @param x_lb    -- The lower bound of the decision variables
-      % @param x_ub    -- The upper bound of the decision variables
+    function obj = NonlinearConstraint(lb,ub,xdim,eval_handle)
+      % @param lb    -- The lower bound of the constraint
+      % @param ub    -- The upper bound of the constraint
+      % @param xdim  -- An int scalar. x is double vector of xdim x 1
       % @param eval_handle   -- The function handle to evaluate constraint. An optional
       % argument.
-      if(nargin < 5)
+      if(nargin < 4)
         eval_handle = [];
       end
-      obj = obj@Constraint(c_lb,c_ub,x_lb,x_ub,eval_handle);
-      if(~isnumeric(c_lb) || ~isnumeric(c_ub))
-        error('Drake:NonlinearConstraint:BadInputs','NonlinearConstraint c_lb and c_ub should be numeric')
+      obj = obj@Constraint(lb,ub,eval_handle);
+      if(~isnumeric(lb) || ~isnumeric(ub))
+        error('Drake:NonlinearConstraint:BadInputs','NonlinearConstraint lb and ub should be numeric')
       end
-      obj.c_lb = obj.c_lb(:);
-      obj.c_ub = obj.c_ub(:);
-      obj.num_cnstr = numel(obj.c_lb);
-      if(obj.num_cnstr ~= numel(obj.c_ub))
-        error('Drake:NonlinearConstraint:BadInputs','NonlinearConstraint c_lb and c_ub should have same number of elements');
+      obj.lb = obj.lb(:);
+      obj.ub = obj.ub(:);
+      obj.num_cnstr = numel(obj.lb);
+      if(obj.num_cnstr ~= numel(obj.ub))
+        error('Drake:NonlinearConstraint:BadInputs','NonlinearConstraint lb and ub should have same number of elements');
       end
-      if(any(obj.c_lb>obj.c_ub))
-        error('Drake:NonlinearConstraint:BadInputs','NonlinearConstraint c_lb should be no larger than c_ub');
+      if(any(obj.lb>obj.ub))
+        error('Drake:NonlinearConstraint:BadInputs','NonlinearConstraint lb should be no larger than ub');
       end
       c_idx = (1:obj.num_cnstr)';
-      obj.ceq_idx = c_idx(obj.relation == Constraint.equal);
-      obj.cin_idx = c_idx(obj.relation == Constraint.ineq);
-      if(~isnumeric(obj.x_lb) || ~isnumeric(obj.x_ub))
-        error('Drake:NonlinearConstraint:BadInputs','NonlinearConstraint x_lb and x_ub should be numeric');
+      obj.ceq_idx = c_idx(obj.lb == obj.ub);
+      obj.cin_idx = c_idx(obj.lb ~= obj.ub);
+      
+      if(~isnumeric(xdim) || numel(xdim) ~= 1 || xdim<0 || xdim ~= floor(xdim))
+        error('Drake:NonlinearConstraint:BadInputs','NonlinearConstraint xdim should be a non-negative integer');
       end
-      obj.xdim = numel(obj.x_lb);
-      obj.x_lb = obj.x_lb(:);
-      obj.x_ub = obj.x_ub(:);
-      if(obj.xdim ~= numel(obj.x_ub))
-        error('Drake:NonlinearConstraint:BadInputs','NonlinearConstraint x_lb and x_ub should have the same number of elements');
-      end
-      if(any(obj.x_lb>obj.x_ub))
-        error('Drake:NonlinearConstraint:BadInputs','NonlinearConstraint x_lb shoulde be no larger than x_ub');
-      end
+      obj.xdim = xdim;
+      
       obj.iCfun = reshape(bsxfun(@times,(1:obj.num_cnstr)',ones(1,obj.xdim)),[],1);
       obj.jCvar = reshape(bsxfun(@times,1:obj.xdim,ones(obj.num_cnstr,1)),[],1);
       obj.nnz = obj.num_cnstr*obj.xdim;
     end
     
-    function obj = setSparseStructure(obj,iCfun,jCvar,nnz)
+    function obj = setSparseStructure(obj,iCfun,jCvar)
       % set the sparse structure of the 1st order gradient matrix
       % @param iCfun          -- An int vector. The row index of non-zero entries of the
       % gradient matrix
       % @param jCvar          -- An int vector. The column index of the non-zero entries of
       % the gradient matrix
-      % @param nnz            -- An int scalar. The number of non-zero entries in the
-      % gradient matrix
       iCfun = iCfun(:);
       jCvar = jCvar(:);
-      if(numel(iCfun) ~= numel(jCvar) || numel(iCfun) ~= nnz)
+      if(numel(iCfun) ~= numel(jCvar))
         error('Drake:NonlinearConstraint:WrongSparseStructure','NonlinearConstraint iCfun and jCvar should have the same number of elements');
       end
       if(any(iCfun<1) || any(iCfun>obj.num_cnstr) || any(jCvar<1) || any(jCvar>obj.xdim))
@@ -85,7 +76,7 @@ classdef NonlinearConstraint < Constraint
       end
       obj.iCfun = iCfun;
       obj.jCvar = jCvar;
-      obj.nnz = nnz;
+      obj.nnz = numel(iCfun);
     end
     
     function [c,dc_val] = evalSparse(obj,varargin)
