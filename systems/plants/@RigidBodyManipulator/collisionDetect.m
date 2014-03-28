ad718b1542257d25d64b8bedb187e266988da81a
Andrés Valenzuela <avalenzu@mit.edu>
2014-03-27 21:16:58 -0400

    Add signature and comment for new collisionDetect

--- systems/plants/@RigidBodyManipulator/collisionDetect.m
+++ systems/plants/@RigidBodyManipulator/collisionDetect.m
@@ -1,15 +1,25 @@
-function [pos,vel,normal,mu] = collisionDetect(obj,contact_pos)
-% for each column of contact_pos, find the closest point in the world
-% geometry, and it's (absolute) position and velocity, (unit) surface
-% normal, and coefficent of friction.
-% @ingroup Collision
+function [phi,normal,xA,xB,idxA,idxB] = collisionDetect(obj,kinsol, ...
+                                          allow_multiple_contacts, ...
+                                          active_collision_options)
+% @param obj
+% @param kinsol
+% @param allow_multiple_contacts - Logical indicating whether or not the
+%   collision detection algorithm may choose how many contact points to
+%   return.
+% @param active_collision_options - Struct that may the following fields
+%   * body_idx - vector of body indices. Only these bodies will be
+%       considered for collsion detection
+%   * collision_groups - cell array of strings. Only the contact shapes
+%       belonging to these groups will be considered for collision
+%       detection.
+% @retval phi - (m x 1)  Vector of gap function values (typically contact
+%   distance), for m possible contacts
+% @retval normal - (3 x m) Contact normal vector in world coordinates,
+%   points from B to A
+% @retval xA - (3 x m) The closest point on body A to contact with body
+%   B, relative to body A origin and in body A frame
+% @retval xB - (3 x m) The closest point on body B to contact with body
+%   A, relative to body B origin and in body B frame
+% @retval idxA - (m x 1) The index of body A.
+% @retval idxB - (m x 1) The index of body B.
 
-% Note: only implements collisions with the obj.terrain so far
-[z,normal] = getHeight(obj.terrain,contact_pos(1:2,:));
-pos = [contact_pos(1:2,:);z];
-
-n = size(contact_pos,2);
-vel = zeros(3,n); % static world assumption (for now)
-mu = ones(1,n);
-
-end
