b6f81e8fb82bf8f1d86ca58f3d61fb371a54bf40
Andrés Valenzuela <avalenzu@mit.edu>
2014-03-28 11:01:04 -0400

    collisionDetectTest passes with more cases.
    It now considers bricks passing through each other along all axes.

--- systems/plants/test/collisionDetectTest.m
+++ systems/plants/test/collisionDetectTest.m
@@ -1,4 +1,4 @@
-function bullet_collision_closest_points_test(varargin)
+function collisionDetectTest(varargin)
 
 checkDependency('bullet');
 checkDependency('lcmgl');
@@ -16,13 +16,15 @@ lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(),'bullet_collision_c
 for i=1:2
   r = addRobotFromURDF(r,'FallingBrick.urdf',zeros(3,1),zeros(3,1),struct('floating',true));
 end
+r = r.replaceContactShapesWithCHull(1:r.getNumBodies());
+r = r.compile();
 
 v = r.constructVisualizer();
 
 q = zeros(getNumDOF(r),1);
 
 kinsol = doKinematics(r,q);
-pts = contactPositions(r,kinsol);
+pts = r.body(2).contact_shapes{1}.getPoints();
 bnd.xmin=min(pts(1,:));
 bnd.xmax=max(pts(1,:));
 bnd.ymin=min(pts(2,:));
@@ -30,65 +32,75 @@ bnd.ymax=max(pts(2,:));
 bnd.zmin=min(pts(3,:));
 bnd.zmax=max(pts(3,:));
 tol = 1e-6;
-
-for x=linspace(-2*(bnd.xmax-bnd.xmin),-(bnd.xmax-bnd.xmin),50);
+%q(2) = bnd.ymin;
+%q(3) = bnd.zmax;
+for x=linspace(-2*(bnd.xmax-bnd.xmin),2*(bnd.xmax-bnd.xmin),50);
   q(1)=x;
   
   kinsol = doKinematics(r,q);
   
-  [ptA,ptB,normal,JA,JB,dJA,dJB] = pairwiseClosestPoints(r,kinsol,2,3);
+  [phi,normal,xA,xB,idxA,idxB] = collisionDetect(r,kinsol);
 
-  dist_ref = abs(abs(x) - (bnd.xmax-bnd.xmin));
-  debugLCMGL(r,v,kinsol,ptA,ptB,dist_ref,tol);
+  dist_ref = max(abs(x) - (bnd.xmax-bnd.xmin),bnd.ymin-bnd.ymax);
+  debugLCMGL(r,v,kinsol,phi,normal,xA,xB,idxA,idxB,dist_ref,tol);
 
-  valuecheck(norm(ptB-ptA), dist_ref,tol);
+  valuecheck(phi, dist_ref,tol);
 end
 
 q = zeros(getNumDOF(r),1);
-for y=linspace(-2*(bnd.ymax-bnd.ymin),-(bnd.ymax-bnd.ymin),50);
+for y=linspace(-2*(bnd.ymax-bnd.ymin),2*(bnd.ymax-bnd.ymin),50);
   q(2)=y;
   
   kinsol = doKinematics(r,q);
   
-  [ptA,ptB,normal,JA,JB,dJA,dJB] = pairwiseClosestPoints(r,kinsol,2,3);
+  [phi,normal,xA,xB,idxA,idxB] = collisionDetect(r,kinsol);
 
-  dist_ref = abs(abs(y) - (bnd.ymax-bnd.ymin));
-  debugLCMGL(r,v,kinsol,ptA,ptB,dist_ref,tol);
+  dist_ref = max(abs(y) - (bnd.ymax-bnd.ymin),bnd.zmin-bnd.zmax);
+  debugLCMGL(r,v,kinsol,phi,normal,xA,xB,idxA,idxB,dist_ref,tol);
 
-  valuecheck(norm(ptB-ptA), dist_ref,tol);
+  valuecheck(phi, dist_ref,tol);
 end
 
 q = zeros(getNumDOF(r),1);
-for z=linspace(-2*(bnd.zmax-bnd.zmin),-(bnd.zmax-bnd.zmin),50);
+for z=linspace(-2*(bnd.zmax-bnd.zmin),2*(bnd.zmax-bnd.zmin),50);
   q(3)=z;
   
   kinsol = doKinematics(r,q);
   
-  [ptA,ptB,normal,JA,JB,dJA,dJB] = pairwiseClosestPoints(r,kinsol,2,3);
+  [phi,normal,xA,xB,idxA,idxB] = collisionDetect(r,kinsol);
 
-  dist_ref = abs(abs(z) - (bnd.zmax-bnd.zmin));
-  debugLCMGL(r,v,kinsol,ptA,ptB,dist_ref,tol);
+  dist_ref = max(abs(z) - (bnd.zmax-bnd.zmin),bnd.ymin-bnd.ymax);
+  debugLCMGL(r,v,kinsol,phi,normal,xA,xB,idxA,idxB,dist_ref,tol);
 
-  valuecheck(norm(ptB-ptA), dist_ref,tol);
+  valuecheck(phi, dist_ref,tol);
 end
 
-function debugLCMGL(r,v,kinsol,ptA,ptB,dist_ref,tol)
+function debugLCMGL(r,v,kinsol,phi,normal,xA,xB,idxA,idxB,dist_ref,tol)
+
+    xA_in_world = forwardKin(r,kinsol,idxA,xA);
+    xB_in_world = forwardKin(r,kinsol,idxB,xB);
   
     v.draw(0,[q;0*q]);
 
     lcmgl.glColor3f(1,0,0); % red
-    lcmgl.sphere(ptA,.05,20,20);
+    lcmgl.sphere(xA_in_world,.02,20,20);
 
     lcmgl.glColor3f(0,1,0); % green
-    lcmgl.sphere(ptB,.05,20,20);
+    lcmgl.sphere(xB_in_world,.02,20,20);
 
-    dist = norm(ptB-ptA);
-    if (abs(dist_ref-dist)<tol)  
+    lcmgl.glColor3f(0,0,0); % black
+    lcmgl.glLineWidth(5); % black
+    lcmgl.line3(xB_in_world(1),xB_in_world(2),xB_in_world(3), ...
+                xB_in_world(1)+normal(1),xB_in_world(2)+normal(2), ...
+                xB_in_world(3)+normal(3));
+
+    dist = norm(xB_in_world-xA_in_world);
+    if (abs(dist_ref-phi)<tol) && (abs(abs(phi)-dist)<tol)  
       lcmgl.glColor3f(0,0,0); % black
     else
       lcmgl.glColor3f(1,0,0); % red
     end
-    lcmgl.text((ptB+ptA)/2+bnd.zmax*[0;0;1],num2str(dist),0,0);
+    lcmgl.text((xB_in_world+xA_in_world)/2+bnd.zmax*[0;0;1],num2str(phi),0,0);
     
     lcmgl.glColor3f(.7,.7,.7); % gray
     
