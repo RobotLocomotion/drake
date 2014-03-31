a7ac7b1f80261723aa5473578a1b91653e661a27
jacob-izr <jacob.izraelevitz@gmail.com>
2014-03-31 10:39:02 -0400

    Planning on making moveSpatialMassMatrix a static method inside RigidBodyAddedMass, rather than a separate function. Moving the test function to the appropriate place

--- systems/plants/test/moveSpatialMassMatrixTest.m
+++ systems/plants/test/moveSpatialMassMatrixTest.m
@@ -1,7 +1,8 @@
 function moveSpatialMassMatrixTest
 %Checks the moveSpatialMassMatrix function with a number of known cases
 
-disp('Checking moveSpatialMassMatrix function');
+disp('Checking RigidBodyAddedMass.moveSpatialMassMatrix function');
+f = @RigidBodyAddedMass.moveSpatialMassMatrix;
 c = 1;
 a = c/2;
 cm = [c/4;0;0];
@@ -19,17 +20,17 @@ I_move_true = [diag([0 m22 0]) [0 0 0; 0 0 m26; 0 0 0];...
 
 disp('Check1')
 I_move_true = I_move_true([4:6 1:3],[4:6 1:3]);
-I_move = moveSpatialMassMatrix(cm,I_example);
+I_move = f(cm,I_example);
 valuecheck(I_move,I_move_true);
 
 disp('Check2')
 I_magic = magic(6)+magic(6)';
-I_moveThereAndBack = moveSpatialMassMatrix(-cm,moveSpatialMassMatrix(cm,I_magic));
+I_moveThereAndBack = f(-cm,f(cm,I_magic));
 valuecheck(I_magic,I_moveThereAndBack);
 
 disp('Check3')
-I_moveByThree = moveSpatialMassMatrix(3*cm,I_magic);
-I_moveThreeTimes = moveSpatialMassMatrix(cm,moveSpatialMassMatrix(cm,moveSpatialMassMatrix(cm,I_magic)));
+I_moveByThree = f(3*cm,I_magic);
+I_moveThreeTimes = f(cm,f(cm,f(cm,I_magic)));
 valuecheck(I_moveByThree,I_moveThreeTimes);
 
 end
