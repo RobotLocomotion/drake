342ab6bcdea6a831a1b55df02cc7bdc4194c82bc
Andy Barry <abarry@gmail.com>
2014-04-01 20:21:28 +0000

    Changes the text on the front page of the drake documentation to have a few more useful links.

--- doc/drake.m
+++ doc/drake.m
@@ -1,10 +1,21 @@
 
 %> @mainpage
 %>
-%> Welcome to the Drake documentation.  
-%>
+%> 
+%> <h2>Just Getting Started?</h2>
+%> <span style="font-weight:bold; font-size:larger">You probably want to go to the <a href="inherits.html">Class Hierarchy</a></span>.
+%> <br /><br />
 %> <p>
-%> Use the panels above to get started (I recommend checking out the <a href="inherits.html">class
-%> hierarchy</a>)
+%> Other useful pages:
+%> <ul>
+%> <li><a href="class_drake_system.html">DrakeSystem</a></li>
+%> <li><a href="class_rigid_body_manipulator.html">RigidBodyManipulator</a></li>
+%> </ul>
+%> </p>
 %>
+%> Drake's doxygen documentation is automatically updated on every push to
+%> the <a href="https://github.com/RobotLocomotion/drake">github repository</a>, so it
+%> should always be up to date.
+%>
+%> <br /><br /><br /><br />
 %> Brought to you by the MIT Robot Locomotion Group, http://groups.csail.mit.edu/locomotion/
