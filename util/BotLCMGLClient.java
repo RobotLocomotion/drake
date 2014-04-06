065253a1a3fd57ae444f9719dd3d038e1be58e0e
Russ Tedrake <russt@mit.edu>
2014-04-06 10:15:54 -0400

    adds an lcmgl method for plotting points

--- util/BotLCMGLClient.java
+++ util/BotLCMGLClient.java
@@ -380,10 +380,18 @@ public class BotLCMGLClient {
     glEnd();
   }
 
+  public synchronized void points(double[] x, double[] y, double[] z) {
+    glBegin(LCMGL_POINTS);
+    for (int i=0; i<x.length; i++) { 
+      glVertex3d(x[i], y[i], z[i]);
+    }
+    glEnd();
+  }
+  
   public synchronized void plot3(double[] x, double[] y, double[] z) {
     glBegin(LCMGL_LINES);
     for (int i=0; i<x.length; i++) { 
-	glVertex3d(x[i], y[i], z[i]);
+      glVertex3d(x[i], y[i], z[i]);
     }
     glEnd();    
   }
