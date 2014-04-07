7e2acdf0db403efea94ab24a577784542ea1dc51
mposa <mposa@mit.edu>
2014-04-07 13:09:27 -0400

    Fixed pendulum examples.

--- examples/Pendulum/runPassiveWRL.m
+++ examples/Pendulum/runPassiveWRL.m
@@ -1,7 +1,7 @@
 function runPassiveWRL
-
-p = PlanarRigidBodyManipulator('Pendulum.urdf');
+options.terrain = [];
+p = PlanarRigidBodyManipulator('Pendulum.urdf',options);
 x = p.simulate([0 5],randn(2,1));
 
-v = constructVisualizer(p,struct('viewer','RigidBodyWRLVisualizer'));
+v = constructVisualizer(p);
 v.playback(x);
