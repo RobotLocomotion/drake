696e444f9d1f26a1636880e55b25ad5e025e6044
mposa <mposa@mit.edu>
2014-04-07 13:35:58 -0400

    Fixes for unit tests.

--- examples/SimpleFourBar/runLQR.m
+++ examples/SimpleFourBar/runLQR.m
@@ -1,7 +1,7 @@
 function runLQR
-
+options.terrain = [];
 w = warning('off','Drake:RigidBody:SimplifiedCollisionGeometry');
-r = PlanarRigidBodyManipulator('FourBar2.urdf');
+r = PlanarRigidBodyManipulator('FourBar2.urdf',options);
 warning(w);
 v = r.constructVisualizer();
 
