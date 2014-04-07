3bd53136dc6e598336792743e847ef4efba8ce4d
mposa <mposa@mit.edu>
2014-04-07 14:56:15 -0400

    Added a check to ensure that PATH license is up to date.

--- addpath_drake.m
+++ addpath_drake.m
@@ -82,7 +82,15 @@ else
 end
 
 setenv('PATH_LICENSE_STRING', '1926793586&Courtesy&&&USR&54782&7_1_2014&1000&PATH&GEN&31_12_2015&0_0_0&5000&0_0');
-conf.pathlcp_enabled = true;
+  
+try
+  x = pathlcp(speye(500),-ones(500,1));
+  valuecheck(x,ones(500,1));
+  conf.pathlcp_enabled = true;
+catch
+  disp('The cached PATH license is out of date, and PATH will fail to solve larger problems. Please report this bug.');
+  conf.pathlcp_enabled = false;
+end
 
 %conf.pathlcp_enabled = ~isempty(getenv('PATH_LICENSE_STRING'));
 %if (~conf.pathlcp_enabled)
