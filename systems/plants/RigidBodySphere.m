811e07a2f03259b9953bb9b545465a9a184605cd
mposa <mposa@mit.edu>
2014-04-09 18:06:27 -0400

    Updated RigidBody removeCollisionGroups(Except) to work with contact shapes. Fixed runCOMFixedPointSearch unit test. Minor additional bug fixes.

--- systems/plants/RigidBodySphere.m
+++ systems/plants/RigidBodySphere.m
@@ -85,7 +85,7 @@ classdef RigidBodySphere < RigidBodyGeometry
       % @param  obj - RigidBodySphere object
       % @retval pts - 3xm array of points on this geometry (in link frame) that
       %               can collide with the world.
-      if shape.radius == 0
+      if obj.radius == 0
         pts = getPoints(obj);
       end
     end
