c09bb6efb33a8cde6603a7627d1c2e4fc3bd8ff2
Russ Tedrake <russt@mit.edu>
2014-04-21 17:18:54 -0400

    cygwin env works for 2014a now

--- util/mex_cygwin_env.pl
+++ util/mex_cygwin_env.pl
@@ -3,47 +3,67 @@
 # Sets up the required environment variables from the mexopts.bat file (converting the appropriate ones to cygwin format)
 # Usage:
 #   eval `mex_cygwin_env.pl`
-# or 
-#   eval `mex_cygwin_env.pl /cygdrive/c/Users/russt/AppData/Roaming/MathWorks/MATLAB/R2013b/mexopts.bat`
-#
-# todo: handle MATLAB >= R2014a
 
-if (@ARGV > 0) {
-    $MEXOPTS_FILE = $ARGV[0];
-} else {
-    # recover it automatically
-    $MEXOPTS_FILE = `mex.bat -v | grep "Options file"`;
-    $MEXOPTS_FILE =~ s/[^=]*=\s*//; chomp($MEXOPTS_FILE);
-    #print "$MEXOPTS_FILE\n";
-    die "Can't find your mex options file. Make sure mex.bat is in your path (MEX = $MEX).  MATLAB >= R2014a is not supported by this script yet.\n" if ($MEXOPTS_FILE eq "");
-}
+$MEXOPTS = `touch dummy.c && mex.bat -v dummy.c 2> /dev/null`; system("rm dummy.c");
+die "Can't find your mex options file. Make sure mex.bat is in your path (MEX = $MEX).  MATLAB >= R2014a is not supported by this script yet.\n" if ($MEXOPTS eq "");
 
-open(MEXOPTS,$MEXOPTS_FILE);
+if ($MEXOPTS =~ m/Set PATH/) { # Then it's the new mex interface, MATLAB version >= 2014a 
 
-while (<MEXOPTS>) {
-    chomp;
-    if (m/^set/) {
-	s/^set\s*//g;                   # zap set from start of line
-	s/%([^%]+)%/\$\1/g;             # replace %VAR% with $VAR
-	($var,$val) = split /=/;        
-	$val =~ s/\"/\\\"/g;            # " -> \"
-	$val = `echo "$val"`;           # use the shell to evaluate environment variables in the string
+    @lines = split(/\n/,$MEXOPTS);
+
+    @lines = grep(/Set /,@lines);
+    foreach $line (@lines) {
+	$line =~ s/Set //g;
+	($var,$val) = split(/=/,$line);
+	chomp($var); chomp($val);
+	$var =~ s/[^a-zA-Z]//g;
+	$val =~ s/^\s//g;
 	$val =~ s/[^a-zA-Z0-9\\\/ \(\).:;-]//g;  # only let these characters through (had some issue with special characters)
-	$ENV{$var} = "$val";            # set the environment variable (only for this process and children) for future references to that var
+	$ENV{$var} = "$val";
 #	system("echo $var = \$$var\n");
     }
+} else {
+    @lines = split(/\n/,$MEXOPTS);
+    @MEXOPTS = grep(/Options file/,@lines);
+    $MEXOPTS_FILE = $MEXOPTS[0];
+    $MEXOPTS_FILE =~ s/[^=]*=\s*//; chomp($MEXOPTS_FILE);
+
+    open(MEXOPTS,$MEXOPTS_FILE);
+
+    while (<MEXOPTS>) {
+	chomp;
+	if (m/^set/) {
+	    s/^set\s*//g;                   # zap set from start of line
+	    s/%([^%]+)%/\$\1/g;             # replace %VAR% with $VAR
+	    ($var,$val) = split /=/;        
+	    $val =~ s/\"/\\\"/g;            # " -> \"
+	    $val = `echo "$val"`;           # use the shell to evaluate environment variables in the string
+	    $val =~ s/[^a-zA-Z0-9\\\/ \(\).:;-]//g;  # only let these characters through (had some issue with special characters)
+	    $ENV{$var} = "$val";            # set the environment variable (only for this process and children) for future references to that var
+#	system("echo $var = \$$var\n");
+	}
+    }
+
+    close(MEXOPTS)
 }
 
 @vars_to_export_with_cygpath = ('PATH');
-@vars_to_export_with_original_path = ('INCLUDE','LIB');
+@vars_to_export_with_original_path = ('INCLUDE','LIB','LIBPATH');
 foreach $var (@vars_to_export_with_cygpath) {
-    $val = `cygpath \"$ENV{$var}\"`;
-    chomp $val;
-    $val =~ s/;/:/g;
-    print "export $var=\"$val\"\n";
+    @values = split(/;/,$ENV{$var});
+    $valuelist = "";
+    foreach $val (@values) {
+	$val = `cygpath \"$val\"`;
+        chomp $val;
+	if ($valuelist eq "") {
+	    $valuelist = $val;
+	} else {
+	    $valuelist = $valuelist . ":" . $val;
+	}
+    }
+    print "export $var=\"$valuelist\"\n";
 }
 foreach $var (@vars_to_export_with_original_path) {
     print "export $var=\"$ENV{$var}\"\n";
 }
 
-close(MEXOPTS)
