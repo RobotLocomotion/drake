b4baf2f78ed949f3301beea604f47dd64d3d4a7d
Twan Koolen <koolen.twan@gmail.com>
2014-04-24 15:54:57 -0400

    Got rid of the quaternion parameterization test in testMotionSubspaceDotV. In the quaternion floating base parameterization, the joint velocity is now simply the twist across the joint, so the motion subspace is identity and motion subspace dot v is zero. This does not warrant a test. Old test was assuming a quaternion dot velocity parameterization.

--- systems/plants/test/testMotionSubspaceDotV.m
+++ systems/plants/test/testMotionSubspaceDotV.m
@@ -1,7 +1,6 @@
 function testMotionSubspaceDotV()
 
 testRPYVersusSymbolic();
-testQuatVersusSymbolic();
 
 end
 
@@ -28,44 +27,4 @@ SdotVCheck = SDot * vBody;
 SdotV = motionSubspaceDotV(body, qBody, vBody);
 
 valuecheck(simplify(SdotV - SdotVCheck), zeros(6, 1), 0);
-end
-
-function testQuatVersusSymbolic()
-syms x y z q_s q_x q_y q_z real;
-syms xd yd zd q_sd q_xd q_yd q_zd real;
-
-qBody = [x; y; z; q_s; q_x; q_y; q_z];
-vBody = [xd; yd; zd; q_sd; q_xd; q_yd; q_zd];
-
-body = RigidBody();
-body.floating = 2;
-
-S = motionSubspace(body, qBody);
-
-SDot = ...
-  diff(S, x) * xd + ...
-  diff(S, y) * yd + ...
-  diff(S, z) * zd + ...
-  diff(S, q_s) * q_sd + ...
-  diff(S, q_x) * q_xd + ...
-  diff(S, q_y) * q_yd + ...
-  diff(S, q_z) * q_zd;
-SdotVSym = SDot * vBody;
-
-
-for i = 1 : 100
-  quat = randn(4, 1);
-  quat = quat / norm(quat);
-  omega = randn(3, 1);
-  quatd = 1/2 * quatmultiply([0; omega]', quat')';
-  qBodyTest = [randn(3, 1); quat];
-  vBodyTest = [randn(3, 1); quatd];
-
-  SdotV = motionSubspaceDotV(body, qBodyTest, vBodyTest);
-  SdotVCheck = double(subs(SdotVSym, num2cell([qBody; vBody]'), num2cell([qBodyTest; vBodyTest]')));
-  valuecheck(SdotV, SdotVCheck, 1e-8);
-end
-
-
-
 end
