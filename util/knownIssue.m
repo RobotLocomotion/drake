65a167b0e9d93cb7de3bd08568e150a8a8f54f58
Russ Tedrake <russt@mit.edu>
2014-05-26 05:25:15 -0400

    update known issues, and point them to github

--- util/knownIssue.m
+++ util/knownIssue.m
@@ -9,19 +9,19 @@ end
 
 switch testname
   case {'systems/plants/test/fallingBrickLCP', ...
+      'systems/plants/test/fallingCapsulesTest',...
       'systems/plants/test/multiRobotTest', ...
       'systems/plants/test/momentumTest', ...
       'systems/plants/test/terrainTest', ...
       'systems/plants/test/testFloatingBaseDynamics'}
     if strcmp(err_id,'PathLCP:FailedToSolve')
-      bugzilla(1095); return;
+      github(18); return;
     end
-  case {'systems/plants/constraint/test/testKinCnst', ...
-      'systems/plants/constraint/test/testKinCnstWaff', ...
-      'systems/plants/test/testIK',...
-      'systems/plants/test/testIKtraj',...
-      'examples/Atlas/test/testIK'}
-    bugzilla(1897); return;
+  case {'examples/Airplane2D/runDircolWithObs', ...
+      'examples/Airplane2D/runFunnelWithObstacles'}
+    if strcmp(err_id,'Airplane2D:SNOPT:INFO3')
+      github(125); return;
+    end
 end
 
 fprintf('\nThis issue is not listed as known.  Please seriously consider reporting it on <a href="http://groups.csail.mit.edu/locomotion/bugs/">bugzilla</a>.\n\n');
@@ -33,6 +33,5 @@ function bugzilla(bug_id)
 end
 
 function github(issue_id)
-  error('need to insert the right url in here');
-  fprintf('\nThis is a known issue.  See <a href="http://github.com/RobotLocomotion/drake/issues?id=%d">Drake issue %d</a> for more information.\n\n',bug_id,bug_id);
+  fprintf('\nThis is a known issue.  See <a href="https://github.com/RobotLocomotion/drake/issues/%d">Drake issue %d</a> for more information.\n\n',issue_id,issue_id);
 end
