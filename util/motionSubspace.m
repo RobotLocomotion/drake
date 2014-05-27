eb10509f5cd553e7c714c3ca5fc13d5285b8e30b
Twan Koolen <koolen.twan@gmail.com>
2014-05-27 13:51:15 -0400

    Added dSdq.

--- util/motionSubspace.m
+++ util/motionSubspace.m
@@ -1,4 +1,4 @@
-function S = motionSubspace(body, qBody)
+function [S, dSdq] = motionSubspace(body, qBody)
 if body.floating == 1
   % configuration parameterization: origin position in base frame and rpy
   % velocity parameterization: origin velocity in base frame and rpydot
@@ -8,11 +8,37 @@ if body.floating == 1
   R = rpy2rotmat(rpy);
   S = [zeros(3, 3), R' * E;
     R' , zeros(3, 3)];
+  
+  % TODO: clean up
+  if nargout > 1
+    [roll, pitch, yaw] = deal(rpy(1), rpy(2), rpy(3));
+    cr = cos(roll);
+    sr = sin(roll);
+    cp = cos(pitch);
+    sp = sin(pitch);
+    cy = cos(yaw);
+    sy = sin(yaw);
+    
+%     Generated using:
+%     body.floating = 1;
+%     syms r p y real;
+%     syms x y z real;
+%     qBody = [x; y; z; r; p; y];
+%     S = motionSubspace(body, qBody);
+%     dSdq = jacobian(S(:), qBody);
+%     matlabFunction(dSdq)
+    dSdq = reshape([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-cp*sy,-cr*cy-sp*sr*sy,cy*sr-cr*sp*sy,0,0,0,cp*cy,-cr*sy+cy*sp*sr,sr*sy+cr*cy*sp,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,sr*sy+cr*cy*sp,cr*sy-cy*sp*sr,0,0,0,0,-cy*sr+cr*sp*sy,-cr*cy-sp*sr*sy,0,0,0,0,cp*cr,-cp*sr,0,cp*cy*(sr*sy+cr*cy*sp)-cp*sy*(cy*sr-cr*sp*sy)-cp*cr*sp,cp*cy*(cr*sy-cy*sp*sr)-cp*sy*(cr*cy+sp*sr*sy)+cp*sp*sr,0,0,0,0,-cy*(cy*sr-cr*sp*sy)-sy*(sr*sy+cr*cy*sp),-cy*(cr*cy+sp*sr*sy)-sy*(cr*sy-cy*sp*sr),0,0,0,0,cp*cr,-cp*sr,0,0,0,0,0,0,-cy*sp,cp*cy*sr,cp*cr*cy,0,0,0,-sp*sy,cp*sr*sy,cp*cr*sy,0,0,0,-cp,-sp*sr,-cr*sp,cp*sp*2.0-cp*cy.^2*sp*2.0-cp*sp*sy.^2*2.0,-cp.^2*sr+sp.^2*sr+cy*sp*(cr*sy-cy*sp*sr)+cp.^2*cy.^2*sr-sp*sy*(cr*cy+sp*sr*sy)+cp.^2*sr*sy.^2,-cp.^2*cr+cr*sp.^2+cp.^2*cr*cy.^2-cy*sp*(sr*sy+cr*cy*sp)+cp.^2*cr*sy.^2+sp*sy*(cy*sr-cr*sp*sy),0,0,0,0,0,0,0,0,0,-cp,-sp*sr,-cr*sp,0,0,0,0,0,0,-cp*sy,-cr*cy-sp*sr*sy,cy*sr-cr*sp*sy,0,0,0,cp*cy,-cr*sy+cy*sp*sr,sr*sy+cr*cy*sp,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[36,6]);
+  end
+
 elseif body.floating == 2
   % configuration parameterization: origin position in base frame and quat
   % velocity parameterization: twist in body frame
   
   S = eye(6);
+  
+  if nargout > 1
+    dSdq = zeros(numel(S), 7);
+  end
 
   % previously:
 %   % configuration parameterization: origin position in base frame and quat
@@ -33,6 +59,10 @@ else
     S = [zeros(3, 1); axis];
   else % helical or revolute joint
     S = [axis; pitch * axis];
+  end
+  
+  if nargout > 1
+    dSdq = zeros(numel(S), 1);
   end
 end
 end
