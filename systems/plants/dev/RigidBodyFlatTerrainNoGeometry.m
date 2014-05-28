7281a3a66fd825460dc5394ac135fa2ead64b1f5
Andrés Valenzuela <avalenzu@mit.edu>
2014-05-28 15:53:34 -0400

    Move RigidBodyFlatTerrainNoGeometry to dev folder.

--- systems/plants/dev/RigidBodyFlatTerrainNoGeometry.m
+++ systems/plants/dev/RigidBodyFlatTerrainNoGeometry.m
@@ -1,6 +1,8 @@
 classdef RigidBodyFlatTerrainNoGeometry < RigidBodyTerrain
 %  This class provides an implementation of RigidBodyTerrain with z=0
-%  everywhere
+%  everywhere. In contrast to `RigidBodyFlatTerrain`, this class does
+%  not add `RigidBodyGeometry` objects to the manipulator. It is
+%  therefore useful for testing the plumbing for non-flat terrain types.
   
   methods 
     function [z,normal] = getHeight(obj,xy)
