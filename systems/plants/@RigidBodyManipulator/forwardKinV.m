790084e00cea7d3912856412ded0a59f0ba8b24a
Twan Koolen <koolen.twan@gmail.com>
2014-05-29 16:47:02 -0400

    Some cleanup in preparation of doing gradients for forwardKinV.

--- systems/plants/@RigidBodyManipulator/forwardKinV.m
+++ systems/plants/@RigidBodyManipulator/forwardKinV.m
@@ -1,7 +1,7 @@
 function [x, J, Jdot_times_v] = forwardKinV(obj, kinsol, body_or_frame_ind, points, rotation_type, base_ind)
 % computes the position of pts (given in the body frame) in the global
-% frame, as well as the Jacobian Jv that maps the joint velocity vector v
-% to xdot, and d/dt(Jv) * v.
+% frame, as well as the Jacobian J that maps the joint velocity vector v
+% to xdot, and d/dt(J) * v.
 %
 % @param kinsol solution structure obtained from doKinematics
 % @param body_or_frame_ind, an integer ID for a RigidBody or RigidBodyFrame
@@ -13,9 +13,9 @@ function [x, J, Jdot_times_v] = forwardKinV(obj, kinsol, body_or_frame_ind, poin
 % frame. If rotation_type, x is 6-by-num_pts where the final 3
 % components are the roll/pitch/yaw of the body frame (same for all points 
 % on the body) 
-% @retval Jv the Jacobian, that maps the joint velocity vector v to xdot
-% @retval Jvdot_times_v the time derivative of the Jacobian Jv multiplied
-% by the joint velocity vector, d/dt(Jv) * v
+% @retval J the Jacobian, that maps the joint velocity vector v to xdot
+% @retval Jdot_times_v the time derivative of the Jacobian J multiplied
+% by the joint velocity vector, d/dt(J) * v
 %
 % rotation_type  -- 0, no rotation included
 %                -- 1, output Euler angle
@@ -43,43 +43,46 @@ end
 
 expressedIn = base; % todo: pass this in as an argument
 
-computeJvdot_times_v = nargout > 2;
-[JGeometric, vIndices] = geometricJacobian(obj, kinsol, base, endEffector, expressedIn);
-JOmega = JGeometric(1 : 3, :);
-JV = JGeometric(4 : 6, :);
-
 pointSize = size(points, 1);
 nPoints = size(points, 2);
 
 % transform points from end effector frame to base frame
 transform = kinsol.T{base} \ kinsol.T{endEffector} * Tframe;
-points = transform(1:3, :) * [points; ones(1, size(points, 2))];
-if computeJvdot_times_v
+points = transform(1:3, :) * [points; ones(1, nPoints)];
+
+% compute geometric Jacobian
+computeJdot_times_v = nargout > 2;
+[JGeometric, vIndices] = geometricJacobian(obj, kinsol, base, endEffector, expressedIn);
+JOmega = JGeometric(1 : 3, :);
+JV = JGeometric(4 : 6, :);
+
+% compute geometric Jacobian dot times v.
+if computeJdot_times_v
   twist = relativeTwist(kinsol.T, kinsol.twists, base, endEffector, expressedIn);
   JGeometricdotV = geometricJacobianDotV(obj, kinsol, base, endEffector, expressedIn);
 end
 
+% compute position Jacobian
 rHats = zeros(nPoints * pointSize, pointSize);
 for i = 1 : nPoints
   point = points(:, i);
   rHats(pointSize * (i - 1) + 1 : pointSize * i, :) = vectorToSkewSymmetric(point);
 end
+JPos = -rHats * JOmega + repmat(JV, nPoints, 1);
 
-JX = -rHats * JOmega + repmat(JV, nPoints, 1);
-vSize = obj.num_velocities;
-
+% compute orientation Jacobian
 switch (rotation_type)
   case 0 % no rotation included
     x = points;
     Phi = zeros(0, 3);
-    if computeJvdot_times_v
+    if computeJdot_times_v
       Phid = zeros(0, 3);
     end
   case 1 % output rpy
     R = transform(1:3, 1:3);
     rpy = rotmat2rpy(R);
     x = [points; repmat(rpy, 1, nPoints)];
-    if computeJvdot_times_v
+    if computeJdot_times_v
       [Phi, Phid] = angularvel2rpydotMatrix(rpy, twist(1 : 3));
     else
       Phi = angularvel2rpydotMatrix(rpy);
@@ -89,7 +92,7 @@ switch (rotation_type)
     quat = rotmat2quat(R);
     x = [points; repmat(quat, 1, nPoints)];
     Phi = angularvel2quatdotMatrix(quat);
-    if computeJvdot_times_v
+    if computeJdot_times_v
       quatd = Phi * twist(1 : 3);
       Phid = angularvel2quatdotMatrix(quatd);
     end
@@ -98,15 +101,18 @@ switch (rotation_type)
 end
 JRot = Phi * JOmega;
 
+% compute J from JPos and JRot
 xSize = pointSize + size(JRot, 1);
 posRowIndices = repeatVectorIndices(1 : pointSize, xSize, nPoints);
 rotRowIndices = repeatVectorIndices(pointSize + 1 : xSize, xSize, nPoints);
 
+vSize = obj.num_velocities;
 J = zeros(length(posRowIndices) + length(rotRowIndices), vSize);
-J(posRowIndices, vIndices) = JX;
+J(posRowIndices, vIndices) = JPos;
 J(rotRowIndices, vIndices) = repmat(JRot, nPoints, 1);
 
-if computeJvdot_times_v
+% compute Jdot times v
+if computeJdot_times_v
   JRotdot_times_v = Phid * twist(1 : 3) + Phi * JGeometricdotV(1 : 3);
   Jdot_times_v = zeros(length(posRowIndices) + length(rotRowIndices), 1);
   rdots = reshape(-rHats * twist(1 : 3) + repmat(twist(4 : 6), nPoints, 1), pointSize, nPoints);
