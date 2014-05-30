ac6a4d2840650572c1ac0250fabac4ec3e559147
Twan Koolen <koolen.twan@gmail.com>
2014-05-30 16:49:20 -0400

    Cleaned up dinvT.

--- util/dinvT.m
+++ util/dinvT.m
@@ -14,19 +14,10 @@ p = T(1:3, 4);
 dR = getSubMatrixGradient(dT, 1:3, 1:3, size(T));
 dp = getSubMatrixGradient(dT, 1:3, 4, size(T));
 
-dinvT_rot = transposeGrad(dR, size(R));
+dinvT_R = transposeGrad(dR, size(R));
+dinvT_p = -R'*dp - matGradMult(dinvT_R, p, false);
 
-% dinvT_trans = -R'*dp -kron(p', eye(3)) * dinvT_rot;
-% Equivalently:
-dinvT_trans = -R'*dp -p(1) * dinvT_rot(1:3, :) - p(2) * dinvT_rot(4:6, :) - p(3) * dinvT_rot(7:9, :);
-
-% ret = [interleaveRows([3 1], {dinvT_rot, zeros(3, nq)}); dinvT_trans; zeros(1, nq)];
-ret = [dinvT_rot(1:3, :);
-       zeros(1, nq);
-       dinvT_rot(4:6, :);
-       zeros(1, nq);
-       dinvT_rot(7:9, :);
-       zeros(1, nq);
-       dinvT_trans;
-       zeros(1, nq)];
+ret = zeros(numel(T), nq);
+ret = setSubMatrixGradient(ret, dinvT_R, 1:3, 1:3, size(T));
+ret = setSubMatrixGradient(ret, dinvT_p, 1:3, 4, size(T));
 end
