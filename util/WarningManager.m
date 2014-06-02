e32297a88d77b125d5127f7f7385ade414a93e39
Russ Tedrake <russt@mit.edu>
2014-06-02 14:59:13 -0400

    addressing code review comments

--- util/WarningManager.m
+++ util/WarningManager.m
@@ -3,15 +3,31 @@ classdef WarningManager < handle
   % displaying a warning only once (per instance of that class)
   
   properties
-    disabled_msgids={};
+    disabled_msgids=containers.Map;
   end
   
   methods
     function warnOnce(obj,msgid,varargin)
-      if ~any(strcmp(msgid,obj.disabled_msgids))
+      % call this instead of warning to print the warning 
+      % only once.  after calling, the message is 'disabled'
+      % @param msgid as in warning msgid
+      % @param varargin message string + additional info
+      if ~isKey(obj.disabled_msgids,msgid)
         warning(msgid,varargin{:});
-        obj.disabled_msgids{end+1} = msgid;
+        obj.disabled_msgids(msgid) = true;
       end
+    end
+    
+    function tf = isDisabled(obj,msgid)
+      % check if the message is already 'disabled'
+      % @param msgid as in warning msgid
+      tf = isKey(obj.disabled_msgids,msgid);
+    end
+    
+    function disableWarning(obj,msgid)
+      % disable this message type without printing the warning
+      % @param msgid as in warning msgid
+      obj.disabled_msgids(msgid) = true;
     end
   end
   
