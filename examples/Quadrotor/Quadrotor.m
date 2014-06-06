f7b58419344b0967d25bdc38530740bdcc21d75c
Russ Tedrake <russt@mit.edu>
2014-06-06 06:46:14 -0400

    added dircol stub.  it also fails, presumably due to #165.  added random obstacles and collision objects.  its fun to watch

--- examples/Quadrotor/Quadrotor.m
+++ examples/Quadrotor/Quadrotor.m
@@ -1,15 +1,30 @@
-classdef Quadrotor < RigidBodyManipulator
+classdef Quadrotor < TimeSteppingRigidBodyManipulator
   
   methods
     
     function obj = Quadrotor()
       options.floating = true;
-      obj = obj@RigidBodyManipulator('quadrotor.urdf',options);
+      options.terrain = RigidBodyFlatTerrain();
+      obj = obj@TimeSteppingRigidBodyManipulator('quadrotor.urdf',.01,options);
     end
    
     function u0 = nominalThrust(obj)
       % each propellor commands -mg/4
-      u0 = getMass(obj)*norm(obj.gravity)*ones(4,1)/4;
+      u0 = Point(getInputFrame(obj),getMass(obj)*norm(obj.manip.gravity)*ones(4,1)/4);
+    end
+    
+    function obj = addObstacles(obj,number_of_obstacles)
+      if nargin<2, number_of_obstacles = randi(10); end
+      
+      for i=1:number_of_obstacles
+        xy = randn(2,1);
+        while(norm(xy)<1), xy = randn(2,1); end
+        height = .5+rand;
+        s = RigidBodyBox([.2+.8*rand(1,2) height],[xy;height/2],[0;0;randn]);
+        s.c = rand(3,1);
+        obj = addShapeToBody(obj,'world',s);
+        obj = addContactShapeToBody(obj,'world',s);
+      end
     end
   end
   
@@ -17,14 +32,16 @@ classdef Quadrotor < RigidBodyManipulator
   methods (Static)
     function runOpenLoop
       r = Quadrotor();
+      r = addObstacles(r);
+      r = compile(r);
       v = r.constructVisualizer();
 
       x0 = [0;0;.5;zeros(9,1)];
       u0 = nominalThrust(r);
       
-      sys = cascade(setOutputFrame(ConstantTrajectory(u0),getInputFrame(r)),r);
+      sys = cascade(ConstantTrajectory(u0),r);
 
-      xtraj = simulate(sys,[0 1],double(x0)+.1*randn(12,1));
+      xtraj = simulate(sys,[0 2],double(x0)+.1*randn(12,1));
       v.playback(xtraj);
     end
   end
