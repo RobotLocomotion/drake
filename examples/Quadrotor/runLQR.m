1c6029f15d9de2392ff223534c3eb0248d4c7b3f
Russ Tedrake <russt@mit.edu>
2014-06-10 04:31:18 -0400

    fixed lqr example

--- examples/Quadrotor/runLQR.m
+++ examples/Quadrotor/runLQR.m
@@ -6,6 +6,8 @@ v = r.constructVisualizer();
 x0 = [0;0;1;zeros(9,1)];
 u0 = double(nominalThrust(r));
 
+%c = tilqr(r,x0,u0,diag([10*ones(5,1);0;ones(5,1);0]),eye(4));
+
 % the linearized system is not controllable in yaw, so ignore that state
 % when generating the controller
 [A,B] = linearize(r,0,x0,double(u0));
@@ -21,10 +23,9 @@ c = AffineSystem([],[],[],[],[],[],[],-K,u0 + K*x0);
 c = setInputFrame(c,getStateFrame(r));
 c = setOutputFrame(c,getInputFrame(r));
 
-%sys = cascade(setOutputFrame(ConstantTrajectory(u0),getInputFrame(r)),r);
 sys = feedback(r,c);
 
 for i=1:5
-  xtraj = simulate(sys,[0 3],double(x0)+.5*randn(12,1));
+  xtraj = simulate(sys,[0 4],double(x0)+.5*randn(12,1));
   v.playback(xtraj);
 end
