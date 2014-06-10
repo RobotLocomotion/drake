791c1560cf5de747c7ee3e9148b28a9882e843ab
Twan Koolen <koolen.twan@gmail.com>
2014-06-10 17:04:23 -0400

    Second derivative of normalizeVec. Needed for dVdq (gradient output of qdotToV). Geval gradients seem to be the wrong size, but otherwise match the user gradients. Possibly due to old version of geval.

--- util/test/normalizeVecTest.m
+++ util/test/normalizeVecTest.m
@@ -1,5 +1,5 @@
 function normalizeVecTest
-  options.grad_method = {'user','taylorvar'}
-  x = rand(3,1);
-  [x_norm,dx_norm] = geval(@normalizeVec,x,options);
+  options.grad_method = {'user', 'taylorvar'};
+  x = rand(5,1);
+  [x_norm,dx_norm,ddx_norm] = geval(@normalizeVec,x,options);
 end
