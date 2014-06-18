a8d8a00ea06e68e2ee25f371c130e8dd156fd818
Robin Deits <robin.deits@gmail.com>
2014-06-18 14:01:05 +0300

    clean up example

--- examples/Atlas/runAtlasFootstepPlanning.m
+++ examples/Atlas/runAtlasFootstepPlanning.m
@@ -1,20 +1,24 @@
 function plan = runAtlasFootstepPlanning()
 
+% Set up the model
 load('data/atlas_fp.mat', 'xstar');
 r = Atlas('urdf/atlas_minimal_contact.urdf');
 r = r.setInitialState(xstar);
-r = compile(r);
-l_id = r.findFrameId('l_foot_sole');
-r_id = r.findFrameId('r_foot_sole');
-kinsol = doKinematics(r, xstar(1:r.getNumDOF()));
 
-start_pos = struct('right', forwardKin(r, kinsol, r_id, [0;0;0], 1), 'left', forwardKin(r, kinsol, l_id, [0;0;0], 1));
+% Find the initial positions of the feet
+kinsol = doKinematics(r, xstar(1:r.getNumDOF()));
+start_pos = struct('right', forwardKin(r, kinsol, r.foot_frame_id.right, [0;0;0], 1), ...
+                   'left', forwardKin(r, kinsol, r.foot_frame_id.left, [0;0;0], 1));
+                 
+% Plan footsteps to the goal
 goal_pos = struct('right', [1;0;0;0;0;0], 'left', [1;0.26;0;0;0;0]);
 plan = r.planFootsteps(start_pos, goal_pos);
 
+% Show the result
 v = r.constructVisualizer();
 v.draw(0, xstar);
 if isa(v, 'BotVisualizer')
+  checkDependency('lcmgl');
   lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(), 'footstep_plan');
   plan.draw_lcmgl(lcmgl);
 else
