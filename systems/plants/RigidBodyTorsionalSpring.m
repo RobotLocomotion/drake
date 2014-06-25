75605ce58047069778ce5c7c67e0bd9e2f866618
Russ Tedrake <russt@mit.edu>
2014-06-25 16:26:22 -0400

    torsional spring parsing works.  but test case fails

--- systems/plants/RigidBodyTorsionalSpring.m
+++ systems/plants/RigidBodyTorsionalSpring.m
@@ -9,8 +9,9 @@ classdef RigidBodyTorsionalSpring < RigidBodyForceElement
   methods
     function f_ext = computeSpatialForce(obj,manip,q,qd)
       theta = q(manip.body(obj.joint).dofnum);
-      torque = k*(rest_angle - theta);
-      f_ext = manip.body(body_ind).X_joint_to_body'*[zeros(3,1);torque*manip.body(obj.joint).joint_axis];
+      torque = obj.k*(obj.rest_angle - theta);
+      f_ext = sparse(6,getNumBodies(manip));
+      f_ext(:,obj.joint) = manip.body(obj.joint).X_joint_to_body'*[zeros(3,1);torque*manip.body(obj.joint).joint_axis];
     end
     
     function obj = updateBodyIndices(obj,map_from_old_to_new)
@@ -33,14 +34,14 @@ classdef RigidBodyTorsionalSpring < RigidBodyForceElement
       obj.name = name;
       
       if node.hasAttribute('rest_angle')
-        obj.rest_angle = parseParamString(model,robotnum,char(node.getAttribute('rest_length')));
+        obj.rest_angle = parseParamString(model,robotnum,char(node.getAttribute('rest_angle')));
       end
       if node.hasAttribute('stiffness')
         obj.k = parseParamString(model,robotnum,char(node.getAttribute('stiffness')));
       end
       
       jointNode = node.getElementsByTagName('joint').item(0);
-      obj.joint = findJointInd(model,char(jointNode.getAttribute('joint')),robotnum);
+      obj.joint = findJointInd(model,char(jointNode.getAttribute('name')),robotnum);
     end
   end
 end
