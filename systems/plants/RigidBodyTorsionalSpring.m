a310ba396627bbd0922b9491146991f6a5f6d310
Russ Tedrake <russt@mit.edu>
2014-06-26 04:14:46 -0400

    respond to Robin's code review

--- systems/plants/RigidBodyTorsionalSpring.m
+++ systems/plants/RigidBodyTorsionalSpring.m
@@ -1,4 +1,7 @@
 classdef RigidBodyTorsionalSpring < RigidBodyForceElement
+%  Adds torsionals linear springs, with 
+%      torque = k*(rest_angle - current_angle),
+%  to rigid body manipulators.  
   
   properties
     joint 
@@ -11,7 +14,11 @@ classdef RigidBodyTorsionalSpring < RigidBodyForceElement
       theta = q(manip.body(obj.joint).dofnum);
       torque = obj.k*(obj.rest_angle - theta);
       f_ext = sparse(6,getNumBodies(manip));
-      f_ext(:,obj.joint) = [zeros(2,1);torque;zeros(3,1)]; %manip.body(obj.joint).X_joint_to_body'*[zeros(3,1);torque*manip.body(obj.joint).joint_axis];
+
+      % note, because featherstone coordinates do everything about the
+      % z-axis, the below is actually equivalent to
+      % f_ext(:,obj.joint) = manip.body(obj.joint).X_joint_to_body'*[zeros(3,1);torque*manip.body(obj.joint).joint_axis];
+      f_ext(:,obj.joint) = [zeros(2,1);torque;zeros(3,1)]; 
     end
     
     function obj = updateBodyIndices(obj,map_from_old_to_new)
@@ -43,7 +50,7 @@ classdef RigidBodyTorsionalSpring < RigidBodyForceElement
       jointNode = node.getElementsByTagName('joint').item(0);
       obj.joint = findJointInd(model,char(jointNode.getAttribute('name')),robotnum);
       
-      % todo: error if not a pin joint
+      assert(numel(model.body(obj.joint).dofnum)==1 && model.body(obj.joint).pitch==0,'Torsional springs are currnetly only supported for pin joints (aka, continuous and rotational joints)');
     end
   end
 end
