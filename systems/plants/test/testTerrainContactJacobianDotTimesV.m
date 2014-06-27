911ad943bff76f34fcb1903764d6a8e01bf975a7
Twan Koolen <koolen.twan@gmail.com>
2014-06-27 16:29:38 -0400

    Added terrainContactJacobianDotTimesV + test. Test currently fails for quat parameterization. Probably related to forwardJacDotTimesV test failure.

---
+++ systems/plants/test/testTerrainContactJacobianDotTimesV.m
@@ -0,0 +1,48 @@
+function testTerrainContactJacobianDotTimesV()
+floatingBaseParameterizations = {'rpy', 'quat'};
+
+for parameterization = floatingBaseParameterizations
+  robot = createAtlas(parameterization{:});
+  testAgainstJdotFromGradient(robot);
+  testGradients(robot);
+end
+end
+
+function testAgainstJdotFromGradient(robot)
+nq = robot.getNumPositions();
+nv = robot.getNumVelocities();
+
+nTests = 5;
+for i = 1 : nTests
+  q = getRandomConfiguration(robot);
+  v = randn(nv, 1);
+  
+  kinsol = robot.doKinematics(q,false,false, v);
+  Jdot_times_v = robot.terrainContactJacobianDotTimesV(kinsol);
+  
+  kinsol = robot.doKinematics(q,true,false, v);
+  [~, J, dJ] = robot.terrainContactPositions(kinsol);
+  qdot = kinsol.vToqdot * kinsol.v;
+  Jdot = reshape(reshape(dJ, [], nq) * qdot, size(J));
+  Jdot_times_v_check = Jdot * qdot;
+
+  valuecheck(Jdot_times_v_check, Jdot_times_v, 1e-10);
+end
+end
+
+function testGradients(robot)
+nv = robot.getNumVelocities();
+
+nTests = 5;
+for i = 1 : nTests
+  q = getRandomConfiguration(robot);
+  v = randn(nv, 1);
+  
+  kinsol = robot.doKinematics(q,true,false, v);
+  [~, dJdot_times_v] = robot.terrainContactJacobianDotTimesV(kinsol);
+  
+  option.grad_method = 'taylorvar';
+  [~, dJdot_times_v_geval] = geval(1, @(q) robot.terrainContactJacobianDotTimesV(robot.doKinematics(q,false,false, v)), q, option);
+  valuecheck(dJdot_times_v_geval, dJdot_times_v, 1e-10);
+end
+end
