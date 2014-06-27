5b7a61abf119c424cc5fd395b6ead2914b4efb9b
Twan Koolen <koolen.twan@gmail.com>
2014-06-27 17:49:04 -0400

    now using quat parameterization in multiRobotTest.

--- systems/plants/test/multiRobotTest.m
+++ systems/plants/test/multiRobotTest.m
@@ -4,14 +4,15 @@ urdf = 'FallingBrick.urdf';
 options.terrain = RigidBodyFlatTerrain();
 r = TimeSteppingRigidBodyManipulator('',.01,options);
 
-options.floating = true;
+options.floating = 'quat';
 n = 1+randi(4);
 for i=1:n
   r = addRobotFromURDF(r,urdf,zeros(3,1),zeros(3,1),options);
 end
 
-x0 = r.resolveConstraints(randn(12*n,1));
+x0 = r.resolveConstraints([getRandomConfiguration(r); randn(r.getNumVelocities(), 1)]);
 xtraj = simulate(r,[0 3],x0);
 
 v = r.constructVisualizer();
 v.playback(xtraj);
+end
