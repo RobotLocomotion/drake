1e00e2d0c06f5cadc388acecb34c6c3a29ed47a6
Andrés Valenzuela <avalenzu@mit.edu>
2014-07-12 12:24:22 -0400

    Remove reduntant constraints from testRunning

--- solvers/trajectoryOptimization/test/testRunning.m
+++ solvers/trajectoryOptimization/test/testRunning.m
@@ -121,11 +121,15 @@ function [sol,robot_vis,v,cdfkp] = testRunning(seed,stride_length,major_iteratio
   half_periodic_constraint = halfPeriodicConstraint(robot);
   cdfkp = cdfkp.addConstraint(half_periodic_constraint,cdfkp.q_inds(:,[1,end]));
   cdfkp = cdfkp.addConstraint(half_periodic_constraint,cdfkp.v_inds(:,[1,end]));
-  cdfkp = cdfkp.addConstraint(LinearConstraint(zeros(2,1),zeros(2,1),[eye(2),-eye(2)]),cdfkp.com_inds(2:3,[1,end]));
-  cdfkp = cdfkp.addConstraint(LinearConstraint(zeros(3,1),zeros(3,1),[eye(3),diag([-1,1,-1])]),cdfkp.comdot_inds(1:3,[1,end]));
-  cdfkp = cdfkp.addConstraint(LinearConstraint(zeros(3,1),zeros(3,1),[eye(3),diag([-1,1,-1])]),cdfkp.comddot_inds(1:3,[1,end]));
-  cdfkp = cdfkp.addConstraint(LinearConstraint(zeros(3,1),zeros(3,1),[eye(3),diag([1,-1,1])]),cdfkp.H_inds(1:3,[1,end]));
-  cdfkp = cdfkp.addConstraint(LinearConstraint(zeros(3,1),zeros(3,1),[eye(3),diag([1,-1,1])]),cdfkp.Hdot_inds(1:3,[1,end]));
+  %cdfkp = cdfkp.addConstraint(LinearConstraint(zeros(2,1),zeros(2,1),[eye(2),-eye(2)]), ...
+                              %cdfkp.com_inds(2:3,[1,end]));
+  %cdfkp = cdfkp.addConstraint(LinearConstraint(zeros(3,1),zeros(3,1),[eye(3),diag([-1,1,-1])]), ...
+                              %cdfkp.comdot_inds(1:3,[1,end]));
+  %cdfkp = cdfkp.addConstraint(LinearConstraint(zeros(3,1),zeros(3,1),[eye(3),diag([-1,1,-1])]), ...
+                              %cdfkp.comddot_inds(1:3,[1,end]));
+  %cdfkp = cdfkp.addConstraint(LinearConstraint(zeros(3,1),zeros(3,1),[eye(3),diag([1,-1,1])]), ...
+                              %cdfkp.H_inds(1:3,[1,end]));
+  %cdfkp = cdfkp.addConstraint(LinearConstraint(zeros(3,1),zeros(3,1),[eye(3),diag([1,-1,1])]),cdfkp.Hdot_inds(1:3,[1,end]));
 
   % Add stance kinematic constraints
   cdfkp = cdfkp.addRigidBodyConstraint(position_cnstr.toe, ...
