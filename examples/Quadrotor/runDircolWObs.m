4ec79bbb6d7c14e013cc8ebab9cb5d3c363f62bd
Russ Tedrake <russt@mit.edu>
2014-07-13 17:18:56 -0400

    quadrotor obstacle avoidance example works--most of the time. still only checking for collisions at the knot points (so it still clips sometimes), and of course it can get stuck in local minima

--- examples/Quadrotor/runDircolWObs.m
+++ examples/Quadrotor/runDircolWObs.m
@@ -1,42 +1,61 @@
-function [utraj,xtraj,prog] = runDircol
+function runDircolWObs
 
 % simple planning demo which takes the quadrotor from hover at x=0m to a new hover at
 % x=2m with minimal thrust.
 
 r = Quadrotor();
+r = addTrees(r,15);
 
-N = 11;
-prog = DircolTrajectoryOptimization(r,N,[.1 4]);
+N = 21;
+prog = DircolTrajectoryOptimization(r,N,[2 10]);
 
 x0 = Point(getStateFrame(r));  % initial conditions: all-zeros
 x0.base_z = .5;
 u0 = double(nominalThrust(r));
 
+if (nargout<1)
+  v = constructVisualizer(r);%,struct('use_contact_shapes',true));
+  v.draw(0,double(x0));
+
+  prog = addPlanVisualizer(r,prog);
+end
+
 prog = prog.addStateConstraint(ConstantConstraint(double(x0)),1);
 prog = prog.addInputConstraint(ConstantConstraint(u0),1);
 
 xf = x0;                       % final conditions: translated in x
-xf.base_x = 2;
+xf.base_y = 10;
 prog = prog.addStateConstraint(ConstantConstraint(double(xf)),N);
 prog = prog.addInputConstraint(ConstantConstraint(u0),N);
 
 prog = prog.addRunningCost(@cost);
-prog = prog.addFinalCost(@finalCost);
+prog = prog.addFinalCost(@final_cost);
 
-tf0 = 2;                      % initial guess at duration 
+tf0 = 6;                      % initial guess at duration
+
+disp('solving w/o obstacles');
 traj_init.x = PPTrajectory(foh([0,tf0],[double(x0),double(xf)]));
 traj_init.u = ConstantTrajectory(u0);
 
-info=0;
-while (info~=1)
-  tic
-  [xtraj,utraj,z,F,info] = prog.solveTraj(tf0,traj_init);
-  toc
-end
+%traj_opt = setCheckGrad(traj_opt,true);
+
+tic
+[xtraj,utraj,z,F,info] = prog.solveTraj(tf0,traj_init);
+toc
+
+disp('resolving w/ obstacles');
+snprint('snopt.out');
+collision_constraint = generateConstraint(MinDistanceConstraint(r,0.1),0);
+prog = prog.addStateConstraint(collision_constraint{1},1:N,1:getNumPositions(r));
+traj_init.x = xtraj;
+traj_init.u = utraj;
+
+tic
+[xtraj,utraj,z,F,info] = prog.solveTraj(tf0,traj_init);
+toc
 
 if (nargout<1)
-  v = constructVisualizer(r);
-  v.playback(xtraj,struct('slider',true));
+  v.playback(xtraj);%,struct('slider',true));
 end
 
 end
@@ -48,8 +67,8 @@ end
         dg = [zeros(1,1+size(x,1)),2*u'*R];
         %dg = zeros(1, 1 + size(x,1) + size(u,1));
       end
-      
-      function [h,dh] = finalCost(t,x)
+
+      function [h,dh] = final_cost(t,x)
         h = t;
         dh = [1,zeros(1,size(x,1))];
       end
