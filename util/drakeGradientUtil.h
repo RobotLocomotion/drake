ceeea9e8ac405e18fe42c80f3b6c92b840e667db
tkoolen <koolen.twan@gmail.com>
2014-07-18 16:30:03 -0400

    matGradMultMat improvements.

--- util/drakeGradientUtil.h
+++ util/drakeGradientUtil.h
@@ -1,10 +1,9 @@
 #ifndef DRAKEGRADIENTUTIL_H_
 #define DRAKEGRADIENTUTIL_H_
 
-#include <Eigen/Dense>
-#include <Eigen/KroneckerProduct> // unsupported...
+#include <Eigen/Core>
 #include <cmath>
-#include <iostream>
+#include <iostream> // TODO: get rid of this one
 
 /*
  * Methods are templated, which is why declaration and implementation are in the same file
@@ -32,14 +31,34 @@ typename Derived::PlainObject transposeGrad(const Eigen::MatrixBase<Derived>& dX
 }
 
 template <typename DerivedA, typename DerivedB, typename DerivedDA, typename DerivedDB>
-Eigen::Matrix<typename DerivedA::Scalar, Eigen::Dynamic, Eigen::Dynamic> matGradMultMat(
+Eigen::Matrix<
+  typename DerivedA::Scalar,
+  Eigen::Dynamic, // TODO: could figure out compile time size to use
+  DerivedDA::ColsAtCompileTime> matGradMultMat(
     const Eigen::MatrixBase<DerivedA>& A,
     const Eigen::MatrixBase<DerivedB>& B,
     const Eigen::MatrixBase<DerivedDA>& dA,
     const Eigen::MatrixBase<DerivedDB>& dB)
 {
-  return Eigen::kroneckerProduct(Eigen::MatrixXd::Identity(B.cols(), B.cols()), A) * dB +
-      Eigen::kroneckerProduct(B.transpose(), Eigen::MatrixXd::Identity(A.rows(), A.rows())) * dA;
+  const int nq = dA.cols();
+  Eigen::Matrix<typename DerivedA::Scalar, Eigen::Dynamic, DerivedDA::ColsAtCompileTime> ret(A.rows() * B.cols(), nq);
+
+  for (int row = 0; row < B.cols(); row++) {
+    auto block = ret.block(row * A.rows(), 0, A.rows(), nq);
+
+    // A * dB part:
+    block = A * dB.block(row * A.cols(), 0, A.cols(), nq);
+
+    for (int col = 0; col < B.rows(); col++) {
+      // B * dA part:
+      block += (B(col, row) * dA.block(col * A.rows(), 0, A.rows(), nq)).eval();
+    }
+  }
+
+  return ret;
+
+  // much slower and requires unsupported eigen/unsupported:
+//  return Eigen::kroneckerProduct(Eigen::MatrixXd::Identity(B.cols(), B.cols()), A) * dB + Eigen::kroneckerProduct(B.transpose(), Eigen::MatrixXd::Identity(A.rows(), A.rows())) * dA;
 }
 
 #endif /* DRAKEGRADIENTUTIL_H_ */
