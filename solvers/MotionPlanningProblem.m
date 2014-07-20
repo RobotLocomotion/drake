786a75fb08a7ad8ec2db884fb68f74e4312f43cb
Russ Tedrake <russt@mit.edu>
2014-07-19 21:59:01 -0400

    basic rrt implementation

---
+++ solvers/MotionPlanningProblem.m
@@ -0,0 +1,157 @@
+classdef MotionPlanningProblem 
+  % Defines a feasible motion planning problem using Constraint
+  % class objects
+  %
+  % Note: a motion planning problem is *not* a NonlinearProgram
+  % because, for instance, it is not described by a fixed number
+  % of decision variables.  Instead, we require each point in a 
+  % solution to be feasible given all of the constraints, and 
+  % therefore have a NonlinearProgram which tests that feasibility
+  % as a member variable.
+  
+  properties
+    num_vars
+    var_names
+    constraints={};  % a cell array of Constraint objects
+  end
+
+  methods
+    function obj = MotionPlanningProblem(num_vars,var_names)
+      % @num_vars the dimension of the search space (e.g. state or
+      % configuration space)
+      % @var_names optional variable names
+      
+      if(nargin<2)
+        var_names = cellfun(@(i) sprintf('x%d',i),num2cell((1:obj.num_vars)'),'UniformOutput',false);
+      else
+        if(~iscellstr(var_names) || numel(var_names) ~= obj.num_vars)
+          error('Drake:NonlinearProgramWConstraintObjects:InvalidArgument','Argument x_name should be a cell containing %d strings',obj.num_vars);
+        end
+        var_names = var_names(:);
+      end
+      obj.num_vars = num_vars;
+      obj.var_names = var_names;
+      
+    end
+
+    function [xtraj,info] = rrt(obj,x_start,x_goal,tolerance,random_sample_fcn,options)
+      % Simple RRT algorithm
+      % @param x_start
+      % @param x_goal
+      % @param tolerance scalar distance that you must achieve to the goal
+      % @param random_sample_fcn function handle of the form
+      %    xs = random_sample_fcn
+      % which generates a nX-by-1 random sample
+      % @option distance_metric_fcn function handle of the form 
+      %    d = distance_metric_fcn(X,xs)
+      % where X is a nX-by-N list of points, and xs is an nX-by-1 sample.
+      % d should be a 1-by-N list of distances @default euclideanDistance
+      % @option display_after_every draws the tree after this many points
+      % are added
+      % @option max_edge_length a distance by which to trim
+
+      % parameters
+      sizecheck(x_start,[obj.num_vars,1]);
+      sizecheck(x_goal,[obj.num_vars,1]);
+      typecheck(random_sample_fcn,'function_handle');
+      
+      % options
+      if nargin<5, options=struct(); end
+      defaultOptions.distance_metric_fcn = @MotionPlanningProblem.euclideanDistance;
+      defaultOptions.display_after_every = 50;
+      defaultOptions.figure_num = 23;
+      defaultOptions.max_edge_length = inf;  % because i don't have any sense of scale here
+      defaultOptions.max_length_between_constraint_checks = inf;
+      options = applyDefaults(options,defaultOptions);
+      typecheck(options.distance_metric_fcn,'function_handle');
+      
+      
+      N = 10000;  % for pre-allocating memory
+      V = repmat(x_start,1,N);  % vertices
+      parent = nan(1,N-1); % edges
+      
+      figure(options.figure_num); hold on; last_edge=1;
+      plot(x_start(1),x_start(2),'bx',x_goal(1),x_goal(2),'gx','MarkerSize',20,'LineWidth',3);
+
+      n=2;
+      info=2; xtraj=[];  % default return values
+      while n<=N
+        xs = random_sample_fcn();
+        if ~checkConstraints(obj,xs), continue; end
+        
+        d = options.distance_metric_fcn(V(:,1:n-1),xs);
+        [dmin,imin] = min(d);
+
+        if dmin>options.max_edge_length
+          % then linearly interpolate along that distance
+          xs = V(:,imin)+(options.max_edge_length/dmin)*(xs - V(:,imin));
+          dmin = options.max_edge_length;
+        end
+
+        if dmin>options.max_length_between_constraint_checks
+          % then linearly interpolate along that distance
+          num_intermediate_samples = 2+ceil(dmin/options.max_length_between_constraint_checks);
+          xss = linspacevec(V(:,imin),xs,num_intermediate_samples);
+          valid = true;
+          for i=2:num_intermediate_samples-1
+            if ~checkConstraints(obj,xss(:,i)), 
+              valid=false; 
+              break;
+            end
+          end
+          if ~valid, continue; end
+        end
+        
+        V(:,n) = xs;
+        parent(n-1) = imin;
+
+        if (options.distance_metric_fcn(x_goal,xs)<tolerance)
+          path = n;
+          while path(1)>1
+            path = [parent(path(1)-1),path];
+          end
+          figure(options.figure_num);
+          line([V(1,path(1:end-1));V(1,path(2:end))],[V(2,path(1:end-1));V(2,path(2:end))],'Color','b','LineWidth',2);
+          xtraj = PPTrajectory(foh(1:length(path),V(:,path)));
+          info = 1;
+        end
+        
+        if mod(n,options.display_after_every)==0 || info==1
+          figure(options.figure_num);
+          line([V(1,last_edge+1:n);V(1,parent(last_edge:n-1))],[V(2,last_edge+1:n);V(2,parent(last_edge:n-1))],'Color',0.3*[1 1 1]);
+          last_edge = n;
+        end
+        
+        if info==1, return; end
+        n=n+1;
+      end
+    end
+    
+    function obj = addConstraint(obj,constraint,x_inds)
+      typecheck(constraint,'Constraint');
+      if nargin<3, x_inds = 1:obj.num_vars; end
+      sizecheck(x_inds,constraint.xdim);
+      
+      obj.constraints = vertcat(obj.constraints,{constraint});
+    end
+    
+    function valid = checkConstraints(obj,x,varargin)
+      valid = true;
+      for i=1:length(obj.constraints)
+        y = eval(obj.constraints{i},x,varargin{:});
+        if any(y<obj.constraints{i}.lb) || any(y>obj.constraints{i}.ub)
+          valid = false;
+          return;
+        end
+      end
+    end
+    
+  end
+  
+  methods (Static=true)
+    function d = euclideanDistance(X,xs)
+      d = sqrt(sum((X-repmat(xs,1,size(X,2))).^2,1));
+    end
+  end
+  
+end
