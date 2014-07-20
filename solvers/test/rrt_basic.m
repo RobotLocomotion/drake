82981800f4f2a1d3c53bb0a194a1ff661950be62
Russ Tedrake <russt@mit.edu>
2014-07-19 22:37:33 -0400

    clean up plotting

--- solvers/test/rrt_basic.m
+++ solvers/test/rrt_basic.m
@@ -1,16 +1,18 @@
 function rrt_basic
 
-close all;
+figure(1); clf; hold on;
+x_start = rand(2,1);
+x_goal = rand(2,1);
+plot(x_start(1),x_start(2),'bx',x_goal(1),x_goal(2),'gx','MarkerSize',20,'LineWidth',3);
 
 prob = MotionPlanningProblem(2);
-
 options.max_edge_length = .1;
-prob.rrt(rand(2,1),rand(2,1),.05,@uniformSamples,options);
+xtraj = prob.rrt(x_start,x_goal,.05,@uniformSamples,options);
+
+fnplt(xtraj);
 
 end
 
 function xs = uniformSamples
-
 xs = rand(2,1);
-
 end
