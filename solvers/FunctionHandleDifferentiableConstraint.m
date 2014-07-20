617d42e05a97264efa67c1804bcfdbad2c94e9a1
Russ Tedrake <russt@mit.edu>
2014-07-19 20:38:40 -0400

    functionhandleconstraint --> functionhandledifferentiableconstraint and made a real functionhandleconstraint class.

--- solvers/FunctionHandleDifferentiableConstraint.m
+++ solvers/FunctionHandleDifferentiableConstraint.m
@@ -1,6 +1,6 @@
-classdef FunctionHandleConstraint < Constraint
-  %FUNCTIONHANDLECONSTRAINT 
-  % A Constraint implementation where the constraint is given
+classdef FunctionHandleDifferentiableConstraint < DifferentiableConstraint
+  %FUNCTIONHANDLEDIFFERENTIABLECONSTRAINT 
+  % A DifferentiableConstraint implementation where the constraint is given
   % by a function handle.
   
   properties(SetAccess = protected)
@@ -8,17 +8,13 @@ classdef FunctionHandleConstraint < Constraint
   end
   
   methods
-    function obj = FunctionHandleConstraint(lb,ub,eval_handle)
+    function obj = FunctionHandleDifferentiableConstraint(lb,ub,xdim,eval_handle)
       % @param lb    -- The lower bound of the constraint
       % @param ub    -- The upper bound of the constraint
+      % @param xdim  -- An int scalar. x is double vector of xdim x 1
       % @param eval_handle -- A function handle which performs the
       % constraint evaluation
-      
-      % NOTE: if you got a "too many inputs" error, it's probably because
-      % the old FunctionHandleConstraint class is now
-      % FunctionHandleDifferentiableConstraint
-      
-      obj = obj@Constraint(lb,ub);
+      obj = obj@DifferentiableConstraint(lb,ub,xdim);
       obj.eval_handle = eval_handle;
     end
 
@@ -36,7 +32,7 @@ classdef FunctionHandleConstraint < Constraint
       elseif nargout == 3,
         [varargout{1},varargout{2},varargout{3}] = obj.eval_handle(varargin{:});
       else
-        error('Drake:FunctionHandleConstraint:UnsupportedEval','Only support 2nd order gradient at most');
+        error('Drake:FunctionHandleDifferentiableConstraint:UnsupportedEval','Only support 2nd order gradient at most');
       end      
     end
   end
