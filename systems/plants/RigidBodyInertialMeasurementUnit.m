bdd91e4f2b442b290733587fde49eb82079306f8
dgc <dgc@rlg-cbse1.(none)>
2014-07-24 16:43:34 -0400

    Revert "Better collision geometry for the falling brick, and updated some typoes in other files related to RigidBodyInertialMeasurementUnit.m script"
    This reverts commit c0744e02562034bdbf2f7eaf3a3da821d1bbad01.

--- systems/plants/RigidBodyInertialMeasurementUnit.m
+++ systems/plants/RigidBodyInertialMeasurementUnit.m
@@ -30,9 +30,9 @@ classdef RigidBodyInertialMeasurementUnit < RigidBodySensor
       numdof = getNumDOF(manip);
       q = x(1:numdof);
       qd = x(numdof+1:end);
-      qdd = sodynamics(manip,t,q,qd,u);  % todo: this could be much more efficient if I cached qdd
+      qdd = sodynamics(manip,t,x,u);  % todo: this could be much more efficient if I cached qdd
       
-      kinsol = doKinematics(manip,q,false,true,qd);
+      kinsol = doKinematics(manip,q,false,qd);
       
       [x,J] = forwardKin(manip,kinsol,obj.body,obj.xyz,1);
       Jdot = forwardJacDot(manip,kinsol,obj.body,obj.xyz,0,0);
