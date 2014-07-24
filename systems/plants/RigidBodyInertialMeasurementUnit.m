a36a598929838e22680d76c5006d5557709cead0
dgc <dgc@rlg-cbse1.(none)>
2014-07-24 16:04:30 -0400

    Better collision geometry for the falling brick, and updated some typoes in other files related to RigidBodyInertialMeasurementUnit.m script

--- systems/plants/RigidBodyInertialMeasurementUnit.m
+++ systems/plants/RigidBodyInertialMeasurementUnit.m
@@ -30,9 +30,9 @@ classdef RigidBodyInertialMeasurementUnit < RigidBodySensor
       numdof = getNumDOF(manip);
       q = x(1:numdof);
       qd = x(numdof+1:end);
-      qdd = sodynamics(manip,t,x,u);  % todo: this could be much more efficient if I cached qdd
+      qdd = sodynamics(manip,t,q,qd,u);  % todo: this could be much more efficient if I cached qdd
       
-      kinsol = doKinematics(manip,q,false,qd);
+      kinsol = doKinematics(manip,q,false,true,qd);
       
       [x,J] = forwardKin(manip,kinsol,obj.body,obj.xyz,1);   % ask for rpy (because I want to output omega in rpy)
       Jdot = forwardJacDot(manip,kinsol,obj.body,obj.xyz,0,0);
