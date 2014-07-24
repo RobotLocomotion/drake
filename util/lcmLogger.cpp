8de695598a09d547a6e59e7832cbd80aa4d0ff66
Russ Tedrake <russt@mit.edu>
2014-07-24 19:49:00 -0400

    fix gcc compile (again)

--- util/lcmLogger.cpp
+++ util/lcmLogger.cpp
@@ -1,5 +1,5 @@
-/* lcmLogger 
- *   An S-function, with no inputs and no outputs, that will simply 
+/* lcmLogger
+ *   An S-function, with no inputs and no outputs, that will simply
  * collect LCM traffic on the known channel and save it to a workspace
  * variable in a matlab structure.
  *
@@ -26,7 +26,7 @@ class LCMMessage
 public:
   double simtime;
   string channel;
-  mxArray* data = NULL;
+  mxArray* data;
 
   virtual ~LCMMessage(void)
   {
@@ -43,13 +43,13 @@ list<LCMMessage> message_log;
 #define MDL_INITIAL_SIZES
 static void mdlInitializeSizes(SimStruct *S)
 {
-  ssSetNumSFcnParams(S, 2);  // lcm channel regex, 
+  ssSetNumSFcnParams(S, 2);  // lcm channel regex,
                              // workspace variable name to write log into
   ssSetNumContStates(S, 0);
   ssSetNumDiscStates(S, 0);
   ssSetNumInputPorts(S, 0);
   ssSetNumOutputPorts(S, 0);
-  
+
   ssSetNumSampleTimes(S, 1);
 }
 
@@ -73,12 +73,12 @@ static void message_handler (const lcm_recv_buf_t *rbuf, const char *channel, vo
   LCMMessage m;
   m.simtime = simtime;
   m.channel = channel;
-  
+
   mwSize size[2] = {static_cast<mwSize>(rbuf->data_size),1};
   m.data = mxCreateCharArray(2,size);
   memcpy(mxGetData(m.data),rbuf->data,rbuf->data_size);
   mexMakeArrayPersistent(m.data);
-          
+
   message_log.push_back(m);
 }
 
@@ -89,10 +89,10 @@ static void mdlStart(SimStruct *S)
 
   lcm = lcm_create(NULL);
   if (!lcm) ssSetErrorStatus(S, "Failed to create LCM object in lcmLogger s-function.");
-  
+
   char* channel_regex = mxArrayToString(ssGetSFcnParam(S, 0));
   lcm_subscribe(lcm, channel_regex, message_handler, NULL);
-  
+
   mxFree(channel_regex);
 
 }
@@ -102,23 +102,23 @@ static void mdlStart(SimStruct *S)
 static void mdlUpdate(SimStruct *S, int_T tid)
 {
   UNUSED(tid);
-  
+
   simtime = ssGetT(S);
-  
+
   while (true) {
     // setup the LCM file descriptor for waiting.
     int lcm_fd = lcm_get_fileno(lcm);
     fd_set fds;
     FD_ZERO(&fds);
     FD_SET(lcm_fd, &fds);
-    
+
     // wait a limited amount of time for an incoming message
     struct timeval timeout = {
       0,  // seconds
       0   // microseconds
     };
     int status = select(lcm_fd + 1, &fds, 0, 0, &timeout);
-    
+
     if(status==0) {
       break; // no messages
     } else if(FD_ISSET(lcm_fd, &fds)) {
@@ -126,11 +126,11 @@ static void mdlUpdate(SimStruct *S, int_T tid)
       lcm_handle(lcm);
     }
   }
-  
+
 }
 
 #define MDL_OUTPUTS
-static void mdlOutputs(SimStruct *S, int_T tid) 
+static void mdlOutputs(SimStruct *S, int_T tid)
 {
   UNUSED(S);
   UNUSED(tid);
@@ -138,20 +138,20 @@ static void mdlOutputs(SimStruct *S, int_T tid)
 
 
 
-static void mdlTerminate(SimStruct *S) 
+static void mdlTerminate(SimStruct *S)
 {
   UNUSED(S);
   if (!lcm) ssSetErrorStatus(S, "Invalid LCM object.");
-  
+
   lcm_destroy(lcm);
   lcm = NULL;
-  
+
   char* log_variable_name = mxArrayToString(ssGetSFcnParam(S, 1));
 
 //  mexPrintf("Writing LCM log to %s\n",log_variable_name);
 
   const char *fieldnames[] = {"simtime", "channel", "data"};
-  
+
   mxArray* pData = mxCreateStructMatrix(message_log.size(),1,3,fieldnames);
 
   int i=0;
@@ -163,15 +163,15 @@ static void mdlTerminate(SimStruct *S)
   }
 
   mexPutVariable("base",log_variable_name,pData);
-  
+
   message_log.clear();
   mxFree(log_variable_name);
 }
 
-#ifdef MATLAB_MEX_FILE    /* Is this file being compiled as a 
+#ifdef MATLAB_MEX_FILE    /* Is this file being compiled as a
                              MEX-file? */
 #include "simulink.c"     /* MEX-file interface mechanism */
 #else
-#include "cg_sfun.h"      /* Code generation registration 
+#include "cg_sfun.h"      /* Code generation registration
                              function */
 #endif
