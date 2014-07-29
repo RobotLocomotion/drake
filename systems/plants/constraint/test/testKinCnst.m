b500aa9f26ac168d2a38f39bae08ede2113a9147
Andrés Valenzuela <avalenzu@mit.edu>
2014-07-29 13:49:20 -0400

    Add active_collision_options input to ABCDC
    Also make MinDistanceConstraint take active_collision_options before tspan and fix testKinCnst.

--- systems/plants/constraint/test/testKinCnst.m
+++ systems/plants/constraint/test/testKinCnst.m
@@ -192,12 +192,12 @@ testKinCnst_userfun(false,false,t3,q31,q31_aff,RigidBodyConstraint.WorldFixedBod
 
 
 display('Check all-to-all closest-distance constraint');
-testKinCnst_userfun(true,true,t_valid,q,q_aff,RigidBodyConstraint.AllBodiesClosestDistanceConstraintType,robot_collision,robot_collision_aff,0.05,1e1,tspan0);
-testKinCnstInvalidTime_userfun(true,true,t_invalid,q,RigidBodyConstraint.AllBodiesClosestDistanceConstraintType,robot_collision,0.05,1e1,tspan0);
+testKinCnst_userfun(true,true,t_valid,q,q_aff,RigidBodyConstraint.AllBodiesClosestDistanceConstraintType,robot_collision,robot_collision_aff,0.05,1e1,struct(),tspan0);
+testKinCnstInvalidTime_userfun(true,true,t_invalid,q,RigidBodyConstraint.AllBodiesClosestDistanceConstraintType,robot_collision,0.05,1e1,struct(),tspan0);
 
 display('Check minimum distance constraint');
-testKinCnst_userfun(true,true,t_valid,q,q_aff,RigidBodyConstraint.MinDistanceConstraintType,robot_collision,robot_collision_aff,0.05,tspan0);
-testKinCnstInvalidTime_userfun(true,true,t_invalid,q,RigidBodyConstraint.MinDistanceConstraintType,robot_collision,0.05,tspan0);
+testKinCnst_userfun(true,true,t_valid,q,q_aff,RigidBodyConstraint.MinDistanceConstraintType,robot_collision,robot_collision_aff,0.05,[],tspan0);
+testKinCnstInvalidTime_userfun(true,true,t_invalid,q,RigidBodyConstraint.MinDistanceConstraintType,robot_collision,0.05,[],tspan0);
 
 display('Check point to point distance constraint');
 lhand_pts = rand(3,2);
