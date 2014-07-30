5ce2267f32d4d326f3b87e6b5b9a5fc4765663cb
Andrés Valenzuela <avalenzu@mit.edu>
2014-07-29 20:19:24 -0400

    Modify generateInterpolatedMDC to hold magnitude
    Prior to this commit, adding additional interpolation points increased the
    overall magnitude of the returned penalty. Now the returned penalty is the
    average over all of the interpolation points.

--- solvers/trajectoryOptimization/generateInterpolatedMinDistanceConstraint.m
+++ solvers/trajectoryOptimization/generateInterpolatedMinDistanceConstraint.m
@@ -15,15 +15,21 @@ function interpolated_constraint = generateInterpolatedMinDistanceConstraint(con
   % @retval interpolated_constraint -- Cell-array of 
   %                                    FunctionHandleConstraint objects
   [lb,ub] = constraint.bounds(0);
-  interpolated_constraint = cell(size(interpolation_parameter));
-  for i = 1:numel(interpolation_parameter)
-    interpolated_constraint{i} = FunctionHandleConstraint(lb, ub, 2*constraint.robot.getNumPositions(),  ...
-      @(q1,q2)interpEvalWrapper(constraint,interpolation_parameter(i),q1,q2));
-  end
+  interpolated_constraint{1} = FunctionHandleConstraint(lb, ub, 2*constraint.robot.getNumPositions(),  ...
+    @(q1,q2)interpEvalWrapper(constraint,interpolation_parameter,q1,q2));
 end
 
 function [c,dc] = interpEvalWrapper(constraint,interpolation_parameter,q1,q2)
   nq = numel(q1);
-  [c,dc_dq_interp] = constraint.eval(0,(1-interpolation_parameter)*q1+interpolation_parameter*q2);
-  dc = dc_dq_interp*[(1-interpolation_parameter)*eye(nq),interpolation_parameter*eye(nq)];
+  n_interp = numel(interpolation_parameter); 
+  c = 0;
+  dc = zeros(1,2*nq);
+  for i = 1:n_interp
+    [c_i,dc_dq_interp] = constraint.eval(0,(1-interpolation_parameter(i))*q1+interpolation_parameter(i)*q2);
+    dc_i = dc_dq_interp*[(1-interpolation_parameter(i))*eye(nq),interpolation_parameter(i)*eye(nq)];
+    c = c+(1/n_interp)*c_i;
+    dc = dc+(1/n_interp)*dc_i;
+    %c = c+c_i;
+    %dc = dc+:c_i;
+  end
 end
