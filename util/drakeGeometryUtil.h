ea27d940deac8900c299a85c96a56ada0aaffaec
tkoolen <koolen.twan@gmail.com>
2014-08-08 18:43:19 -0400

    Added some rotation representation conversion utility functions to drakeGeometryUtil.

--- util/drakeGeometryUtil.h
+++ util/drakeGeometryUtil.h
@@ -1,8 +1,10 @@
 #ifndef __DRAKE_GEOMETRY_UTIL_H__
 #define __DRAKE_GEOMETRY_UTIL_H__
+
 #include <Eigen/Dense>
 #include <cstring>
 #include <cmath>
+
 Eigen::Vector4d quatConjugate(const Eigen::Vector4d &q);
 Eigen::Matrix4d dquatConjugate();
 Eigen::Vector4d quatProduct(const Eigen::Vector4d &q1, const Eigen::Vector4d &q2);
@@ -13,4 +15,22 @@ Eigen::Vector4d quatDiff(const Eigen::Vector4d &q1, const Eigen::Vector4d &q2);
 Eigen::Matrix<double, 4, 8> dquatDiff(const Eigen::Vector4d &q1, const Eigen::Vector4d &q2);
 double quatDiffAxisInvar(const Eigen::Vector4d &q1, const Eigen::Vector4d &q2, const Eigen::Vector3d &u);
 Eigen::Matrix<double, 1, 11> dquatDiffAxisInvar(const Eigen::Vector4d &q1, const Eigen::Vector4d &q2, const Eigen::Vector3d &u);
+
+
+template <typename Derived>
+Eigen::Vector4d axis2quat(const Eigen::MatrixBase<Derived>& a);
+
+template <typename Derived>
+Eigen::Matrix<typename Derived::Scalar, 3, 3> axis2rotmat(const Eigen::MatrixBase<Derived>& a);
+
+template <typename Derived>
+Eigen::Matrix<typename Derived::Scalar, 3, 1> quat2rpy(const Eigen::MatrixBase<Derived>& q);
+
+template <typename Derived>
+Eigen::Matrix<typename Derived::Scalar, 3, 1> axis2rpy(const Eigen::MatrixBase<Derived>& a);
+
+template <typename Derived>
+Eigen::Matrix<typename Derived::Scalar, 4, 1> quat2axis(const Eigen::MatrixBase<Derived>& q);
+
+
 #endif
