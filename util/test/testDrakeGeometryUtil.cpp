af25b3478532231f998492825d57c91084a09b55
tkoolen <koolen.twan@gmail.com>
2014-08-09 14:03:08 -0400

    Rotation conversion functions, moved angleDiff. Rotation conversion functions tested in testDrakeGeometryUtil.

--- util/test/testDrakeGeometryUtil.cpp
+++ util/test/testDrakeGeometryUtil.cpp
@@ -2,11 +2,12 @@
 #include "drakeGeometryUtil.h"
 #include <exception>
 #include <iostream>
+#include <cmath>
 
 using namespace Eigen;
 using namespace std;
 
-template <typename DerivedA, typename DerivedB>
+template<typename DerivedA, typename DerivedB>
 void valuecheck(const DenseBase<DerivedA>& a, const DenseBase<DerivedB>& b, typename DerivedA::Scalar tolerance = 1e-8)
 {
   if (!a.isApprox(b)) {
@@ -25,22 +26,59 @@ void valuecheck(double a, double b, double tolerance = 1e-8)
   }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   int ntests = 100;
+
+  std::default_random_engine generator;
+
+  // quat2axis, axis2quat
   for (int i = 0; i < ntests; i++) {
-    Vector4d a = Vector4d::Random();
-    a.head<3>().normalize();
-    auto q = axis2quat(a);
-    auto a_back = quat2axis(q);
-    valuecheck(0.0, quatNorm(quatDiff(a, a_back)), 1e-12);
+    Vector4d q = uniformlyRandomQuat(generator);
+    auto a = quat2axis(q);
+    auto q_back = axis2quat(a);
+    valuecheck(acos(abs(q.transpose() * q_back)), 0.0, 1e-6);
   }
 
-//  auto R_from_a = axis2rotmat(a);
-//  auto rpy_from_a = axis2rpy(a);
+  // quat2rotmat, rotmat2quat
+  for (int i = 0; i < ntests; i++) {
+    Vector4d q = uniformlyRandomQuat(generator);
+    Matrix3d R = quat2rotmat(q);
+    Vector4d q_back = rotmat2quat(R);
+    valuecheck(acos(abs(q.transpose() * q_back)), 0.0, 1e-6);
+  }
 
-//  std::cout << "q:" << q << "\n";
+  // quat2rpy, rpy2quat
+  for (int i = 0; i < ntests; i++) {
+    Vector4d q = uniformlyRandomQuat(generator);
+    Vector3d rpy = quat2rpy(q);
+    Vector4d q_back = rpy2quat(rpy);
+    valuecheck(acos(abs(q.transpose() * q_back)), 0.0, 1e-6);
+  }
 
+  // rotmat2axis, axis2rotmat
+  for (int i = 0; i < ntests; i++) {
+    Matrix3d R = uniformlyRandomRotmat(generator);
+    Vector4d a = rotmat2axis(R);
+    Matrix3d R_back = axis2rotmat(a);
+    valuecheck(R, R_back, 1e-6);
+  }
 
+  // rotmat2rpy, rpy2rotmat
+  for (int i = 0; i < ntests; i++) {
+    Matrix3d R = uniformlyRandomRotmat(generator);
+    Vector3d rpy = rotmat2rpy(R);
+    Matrix3d R_back = rpy2rotmat(rpy);
+    valuecheck(R, R_back, 1e-6);
+  }
+
+  // rpy2axis, axis2rpy
+  for (int i = 0; i < ntests; i++) {
+    Vector3d rpy = uniformlyRandomRpy(generator);
+    Vector4d axis = rpy2axis(rpy);
+    Vector3d rpy_back = axis2rpy(axis);
+    valuecheck(rpy, rpy_back, 1e-6);
+  }
 
   return 0;
 }
