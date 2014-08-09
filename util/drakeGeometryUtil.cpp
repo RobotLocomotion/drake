160b6674d7cb0506da6f6925b381243e6c85bb9d
tkoolen <koolen.twan@gmail.com>
2014-08-09 14:52:49 -0400

    4 --> 4.0

--- util/drakeGeometryUtil.cpp
+++ util/drakeGeometryUtil.cpp
@@ -121,7 +121,7 @@ Eigen::Matrix<double, 1, 11> dquatDiffAxisInvar(const Eigen::Vector4d& q1, const
   Matrix<double, 4, 8> dr = dquatDiff(q1, q2);
   Matrix<double, 1, 11> de;
   const auto& rvec = r.tail<3>();
-  de << 4.0 * r(0) * dr.row(0) + 4 * u.transpose() * rvec *u.transpose() * dr.block<3, 8>(1, 0), 4 * u.transpose() * rvec * rvec.transpose();
+  de << 4.0 * r(0) * dr.row(0) + 4.0 * u.transpose() * rvec *u.transpose() * dr.block<3, 8>(1, 0), 4.0 * u.transpose() * rvec * rvec.transpose();
   return de;
 }
 
