4e0491a4a6a4a9863f3ffc07d73e8b130c40cda1
tkoolen <koolen.twan@gmail.com>
2014-08-09 18:54:21 -0400

    testDHomogTransInv.

---
+++ util/test/testDHomogTransInv.m
@@ -0,0 +1,22 @@
+function testDHomogTransInv()
+for i = 1 : 100
+  R = axis2rotmat(uniformlyRandomAxisAngle());
+  p = randn(3, 1);
+  T = [R, p; zeros(1, 3), 1];
+  nq = 15;
+  nv = 7;
+  S = randn(6, nv);
+  qdotToV = randn(nv, nq);
+  dT = dHomogTrans(T, S, qdotToV);
+  dTInv = dHomogTransInv(T, dT);
+  dTInv_check = invMatGrad(T, dT);
+  valuecheck(dTInv_check, dTInv, 1e-10);
+end
+end
+
+function a = uniformlyRandomAxisAngle()
+axis = randn(3, 1);
+axis = axis / norm(axis);
+angle = (rand - 0.5) * 2 * pi;
+a = [axis; angle];
+end
