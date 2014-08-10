cf477a28316a03098e999ec289a5581e1e7bbe41
tkoolen <koolen.twan@gmail.com>
2014-08-10 11:00:25 -0400

    Added comments.

--- util/jointQdotTov.m
+++ util/jointQdotTov.m
@@ -1,10 +1,20 @@
-function [Vq, dVq] = jointQdotTov(body, q_body)
+function [VqJoint, dVqJoint] = jointQdotTov(body, q_body)
+% Computes the matrix that maps from the derivative of the position vector
+% of a joint associated with a RigidBody object to its velocity vector.
+%
+% @param body a RigidBody object
+% @param q_body joint configuration vector for body
+% @retval VqJoint matrix such that v = Vq * qd, where v is the velocity
+% vector for body and qd is the derivative of the configuration vector for
+% body
+% @retval dVqJoint gradient output
+
 compute_gradient = nargout > 1;
 
 if body.floating == 1
-  Vq = eye(6);
+  VqJoint = eye(6);
   if compute_gradient
-    dVq = zeros(numel(Vq), size(Vq, 2));
+    dVqJoint = zeros(numel(VqJoint), size(VqJoint, 2));
   end
 elseif body.floating == 2
   quat = q_body(4 : 7);
@@ -20,7 +30,7 @@ elseif body.floating == 2
   end
   
   RTransposeM = R' * M; % TODO: directly use body frame representation
-  Vq = [...
+  VqJoint = [...
     zeros(3, 3), RTransposeM * dquattildedquat;
     R', zeros(3, 4)];
   
@@ -29,16 +39,16 @@ elseif body.floating == 2
     dRTransposeM = matGradMultMat(R', M, dRTranspose, dM);
     ddquattildedquat = reshape(ddquattildedquat, numel(dquattildedquat), numel(quat));
     dRTransposeMdquattildedquat = matGradMultMat(RTransposeM, dquattildedquat, dRTransposeM, ddquattildedquat);
-    dVq = zeros(numel(Vq), length(q_body)) * q_body(1); % for TaylorVar
-    dVq = setSubMatrixGradient(dVq, dRTranspose, 4:6, 1:3, size(Vq), 4:7);
-    dVq = setSubMatrixGradient(dVq, dRTransposeMdquattildedquat, 1:3, 4:7, size(Vq), 4:7);
+    dVqJoint = zeros(numel(VqJoint), length(q_body)) * q_body(1); % for TaylorVar
+    dVqJoint = setSubMatrixGradient(dVqJoint, dRTranspose, 4:6, 1:3, size(VqJoint), 4:7);
+    dVqJoint = setSubMatrixGradient(dVqJoint, dRTransposeMdquattildedquat, 1:3, 4:7, size(VqJoint), 4:7);
   end
 elseif body.floating ~= 0
   error('case not handled');
 else
-  Vq = 1;
+  VqJoint = 1;
   if compute_gradient
-    dVq = 0;
+    dVqJoint = 0;
   end
 end
 
