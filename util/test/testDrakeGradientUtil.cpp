7a57150f7dd48a512aa662229e377c0e8539daff
tkoolen <koolen.twan@gmail.com>
2014-08-10 15:39:02 -0400

    Extracted out testUtil.h

--- util/test/testDrakeGradientUtil.cpp
+++ util/test/testDrakeGradientUtil.cpp
@@ -1,10 +1,10 @@
 #include "drakeGradientUtil.h"
 #include "drakeGeometryUtil.h"
+#include "testUtil.h"
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 #include <Eigen/KroneckerProduct> // unsupported...
 #include <iostream>
-#include <chrono>
 #include <stdexcept>
 #include <vector>
 #include <array>
@@ -12,23 +12,7 @@
 
 using namespace Eigen;
 
-template<typename TimeT = std::chrono::milliseconds>
-struct measure
-{
-  template<typename F, typename ...Args>
-  static typename TimeT::rep execution(F func, Args&&... args)
-  {
-    auto start = std::chrono::system_clock::now();
 
-    // Now call the function with all the parameters you need.
-    func(std::forward<Args>(args)...);
-
-    auto duration = std::chrono::duration_cast< TimeT>
-    (std::chrono::system_clock::now() - start);
-
-    return duration.count();
-  }
-};
 
 template<typename Derived>
 void setLinearIndices(MatrixBase<Derived>& A)
