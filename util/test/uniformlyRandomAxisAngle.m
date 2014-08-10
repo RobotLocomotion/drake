50c02942ff116f1d8c09f798177791e548a5d91c
tkoolen <koolen.twan@gmail.com>
2014-08-10 00:47:14 -0400

    remove uniformlyRandomAxisAngle copy in util/test

--- util/test/uniformlyRandomAxisAngle.m
+++
@@ -1,8 +0,0 @@
-%NOTEST
-%FLOATINGBASE FIXME delete once geometry util pull request is in
-function a = uniformlyRandomAxisAngle()
-axis = randn(3, 1);
-axis = axis / norm(axis);
-angle = (rand - 0.5) * 2 * pi;
-a = [axis; angle];
-end
