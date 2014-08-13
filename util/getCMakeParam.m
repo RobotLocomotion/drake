fcd7d162adddc5103ddafca2be3c17a496b47603
Russ Tedrake <russt@mit.edu>
2014-08-13 19:57:05 -0400

    matlab-only way to get the cmake params, should be more robust

--- util/getCMakeParam.m
+++ util/getCMakeParam.m
@@ -1,10 +1,24 @@
 function val = getCMakeParam(param)
+% Checks the cmake cache and extracts the stored parameter
 
-[retval,val] = system(['cmake -L -N ',fullfile(getDrakePath,'pod-build'),' | grep ', param,' | cut -d "=" -f2']);
-if (retval)
-  val=[];
-else
-  val = strtrim(val);
+txt = fileread(fullfile(getDrakePath,'pod-build','CMakeCache.txt'));
+if isempty(txt)
+  error('Couldn''t find the cmake cache.  Did you run make?');
 end
+tokens = regexp(txt,[param,':\w+=(.*)'],'tokens','dotexceptnewline');
+if isempty(tokens)
+  error('Couldn''t find the parameter %s in your cmake cache',param);
+end
+val = tokens{1}{1};
+
+% Note: This was the old way of doing it.  But we found it less robust,
+% because the matlab library path might not even allow one to run cmake.
+
+%[retval,val] = system(['cmake -L -N ',fullfile(getDrakePath,'pod-build'),' | grep ', param,' | cut -d "=" -f2']);
+%if (retval)
+%  val=[];
+%else
+%  val = strtrim(val);
+%end
 
 end
