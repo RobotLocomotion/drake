52f2084e938a76498a3744ab82329e76db536c8a
Andrés Valenzuela <avalenzu@mit.edu>
2014-08-13 19:52:57 -0400

    Reimplement Sum as a regular drakeFunction.
    This leaves us with only two drakeFunction.Container classes:
      * Composed
      * Concatenated
    which strikes me as cleaner.

--- +drakeFunction/+test/sumTest.m
+++ +drakeFunction/+test/sumTest.m
@@ -1,9 +1,10 @@
 function sumTest()
+  import drakeFunction.*
   frame = CoordinateFrame('x',5);
   fun1 = drakeFunction.Identity(frame);
 
   % Test with different inputs
-  sum_fun = drakeFunction.Sum({fun1,fun1});
+  sum_fun = Composed(Sum(frame,2),Concatenated({fun1,fun1}));
   x = rand(5,1);
   y = rand(5,1);
   [f,df] = sum_fun.eval([x;y]);
@@ -13,11 +14,12 @@ function sumTest()
   [f,df] = geval(@sum_fun.eval,[x;y],struct('grad_method',{{'user','taylorvar','numerical'}}));
 
   % Test with same inputs
-  sum_fun = drakeFunction.Sum(repmat({fun1},1e1,1),true);
+  N = 10;
+  sum_fun = Composed(Sum(frame,N),Concatenated(repmat({fun1},10,1),true));
   x = rand(5,1);
   [f,df] = sum_fun.eval(x);
-  valuecheck(f,sum_fun.n_contained_functions*x);
-  valuecheck(df,sum_fun.n_contained_functions*eye(frame.dim));
+  valuecheck(f,N*x);
+  valuecheck(df,N*eye(frame.dim));
 
   [f,df] = geval(@sum_fun.eval,x,struct('grad_method',{{'user','taylorvar','numerical'}},'tol',1e-6));
 end
