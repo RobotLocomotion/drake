79ad140786caffae68c556779d4c59c31666b4b1
Andrés Valenzuela <avalenzu@mit.edu>
2014-08-15 09:50:18 -0400

    Make Difference work like diff

--- +drakeFunction/Difference.m
+++ +drakeFunction/Difference.m
@@ -1,7 +1,11 @@
-function fcn = Difference(frame)
+function fcn = Difference(frame,n)
   typecheck(frame,'CoordinateFrame');
-  input_frame = MultiCoordinateFrame({frame,frame});
+  integervaluedcheck(n);
+  assert(n>=2,'Drake:DrakeFunction:Difference:BadInput',...
+    'The number of points to difference, n, must be at least 2');
+  input_frame = MultiCoordinateFrame.constructFrame(repmat({frame},1,n));
   output_frame = frame;
-  A = [eye(frame.dim),-eye(frame.dim)];
+  nx = frame.dim;
+  A = kron(spdiags(ones(nx,1),0,n-1,n),eye(nx)) + kron(spdiags(-ones(nx,1),1,n-1,n),eye(nx));
   fcn = drakeFunction.Linear(input_frame,output_frame,A);
 end
