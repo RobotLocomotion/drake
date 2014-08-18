c4c983122fefcc2f024cccbbf3b687a357c06e24
Andrés Valenzuela <avalenzu@mit.edu>
2014-08-18 07:26:56 -0400

    Add more documentation

--- +drakeFunction/ConstantPower.m
+++ +drakeFunction/ConstantPower.m
@@ -1,9 +1,23 @@
 classdef ConstantPower < drakeFunction.DrakeFunction
   properties (SetAccess = immutable)
-    power
+    power   % Numeric vector or scalar
   end
   methods
     function obj = ConstantPower(varargin)
+      % obj = ConstantPower(input_frame,output_frame,power) returns a
+      %   DrakeFunction that raises its input elementwise to the given
+      %   power
+      %
+      % obj = ConstantPower(frame,value) is the same, but with
+      %   identical input and output frames
+      %
+      % @param input_frame    -- CoordinateFrame of the input
+      % @param output_frame   -- CoordinateFrame of the output. Must
+      %                          have the same dimension as input_frame
+      % @param power          -- Numeric scalar or vector. The output of
+      %                          the returned function is x.^power
+      %
+      % @retval fcn           -- drakeFunction.Linear object
       input_frame = varargin{1};
       if nargin > 2
         output_frame = varargin{2};
