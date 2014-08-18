8fabc06300027bddb55b0f7bba725113ee14d917
Andrés Valenzuela <avalenzu@mit.edu>
2014-08-18 09:48:44 -0400

    Add doc for drakeFunction.Concatenated

--- +drakeFunction/Concatenated.m
+++ +drakeFunction/Concatenated.m
@@ -1,24 +1,65 @@
 classdef Concatenated < drakeFunction.DrakeFunction
   % DrakeFunction representing the concatenation of n functions
-  % Implements
-  %   f  = [f1(x1); f2(x2); ... ; fn(xn)]
-  %   df = [df1(x1), 0      , 0, ..., 0, 0;
-  %         0      , df2(x2), 0, ..., 0, 0;
-  %         .        .        .  .    .  .
-  %         .        .        .   .   .  .
-  %         .        .        .    .  .  .
-  %         0      , 0      , 0, ..., 0, dfn(xn)]
-  % OR
-  %   f = [f1(x);  f2(x);  ... ; fn(x)]
+  %
+  % Implements either
+  % \f[
+  % f(x) = 
+  % \begin{bmatrix}
+  %   f_1(x_1) \\
+  %   f_2(x_2) \\
+  %   \vdots \\
+  %   f_n(x_n)
+  % \end{bmatrix},\;
+  % \frac{df}{dx} = 
+  % \begin{bmatrix}
+  %   \frac{d f_1}{d x_1} &                     &         & 0                  \\
+  %                       & \frac{d f_2}{d x_2} &         &                    \\
+  %                       &                     & \ddots  &                    \\
+  %   0                   &                     &         & \frac{d f_n}{d x_n} 
+  % \end{bmatrix}
+  % \f]
+  %
+  % where \f$x = (x_1, x_2, \dots, x_n)^\prime\f$ or 
+  %
+  % \f[
+  % f(x) = 
+  % \begin{bmatrix}
+  %   f_1(x) \\
+  %   f_2(x) \\
+  %   \vdots \\
+  %   f_n(x)
+  % \end{bmatrix},\;
+  % \frac{df}{dx} = 
+  % \begin{bmatrix}
+  %   \frac{d f_1}{d x} \\
+  %   \frac{d f_2}{d x} \\
+  %   \vdots            \\
+  %   \frac{d f_n}{d x} 
+  % \end{bmatrix}
+  % \f]
   properties (SetAccess = immutable)
-    contained_functions %Cell array of DrakeFunction objects
-    n_contained_functions
-    same_input
+    contained_functions     % Cell array of DrakeFunction objects
+    n_contained_functions   % Number of elements in contained_functions
+    same_input              % Logical scalar indicating whether all of
+                            % the contained function share the same input
+  end
+  properties (Access = private)
     input_map
   end
 
   methods
     function obj = Concatenated(fcns,same_input)
+      % obj = Concatenated(fcns, same_input) returns a DrakeFunction
+      %   representing the concatenation of a given set of
+      %   DrakeFunctions. If same_input = true, the input to the
+      %   concatenated function is passed to all of the component
+      %   functions. Otherwise, the input to the concatenated function
+      %   is split and distributed to the component functions.
+      %
+      % obj = Concatenated(fcns) is the same as Concatenated(fcns,true).
+      %
+      % @param fcns         -- Cell array of DrakeFunction objects
+      % @param same_input   -- Logical scalar
       if nargin < 2, same_input = false; end
       typecheck(fcns,'cell');
       assert(all(cellfun(@(arg)isa(arg,'drakeFunction.DrakeFunction'), fcns)));
@@ -39,7 +80,40 @@ classdef Concatenated < drakeFunction.DrakeFunction
       [f,df] = combineOutputs(obj,f_cell,df_cell);
     end
 
+    function [iCfun, jCvar] = getSparsityPattern(obj)
+      % [iCfun, jCvar] = getSparsityPattern(obj) returns the row and
+      %   column indices of the potentially non-zero elements of this
+      %   function's Jacobian.
+      %
+      % @param obj      -- drakeFunction.Concatenated object 
+      % 
+      % @retval iCfun   -- Vector of row indices of the non-zeros
+      % @retval jCvar   -- Vector of column indices of the non-zeros
+      f_cell = cell(obj.n_contained_functions,1);
+      df_cell = cell(obj.n_contained_functions,1);
+      for i = 1:obj.n_contained_functions
+        f_cell{i} = NaN(obj.contained_functions{i}.getNumOutputs(),1);
+        df_cell{i} = ones(obj.contained_functions{i}.getNumOutputs(), ...
+                          obj.contained_functions{i}.getNumInputs());
+      end
+      [~,df] = combineOutputs(obj,f_cell,df_cell);
+      [iCfun, jCvar] = find(df);
+    end
+  end
+
+  methods (Access = private)
     function [f_cell,df_cell] = evalContainedFunctions(obj,x)
+      % [f_cell,df_cell] = evalContainedFunctions(obj,x) returns the
+      % function values and gradients for each of the component
+      % functions
+      % 
+      % @param obj        -- drakeFunction.Concatenated object
+      % @param x          -- Input vector
+      %
+      % @retval f_cell    -- Cell array of function values for each
+      %                      component function
+      % @retval df_cell   -- Cell array of Jacobians for each component
+      %                      function
       x_cell = cell(1,obj.input_frame.getNumFrames);
       f_cell = cell(size(x_cell));
       df_cell = cell(size(x_cell));
@@ -62,21 +136,9 @@ classdef Concatenated < drakeFunction.DrakeFunction
         df = blkdiag(df_cell{:});
       end
     end
-
-    function [iCfun, jCvar] = getSparsityPattern(obj)
-      f_cell = cell(obj.n_contained_functions,1);
-      df_cell = cell(obj.n_contained_functions,1);
-      for i = 1:obj.n_contained_functions
-        f_cell{i} = NaN(obj.contained_functions{i}.getNumOutputs(),1);
-        df_cell{i} = ones(obj.contained_functions{i}.getNumOutputs(), ...
-                          obj.contained_functions{i}.getNumInputs());
-      end
-      [~,df] = combineOutputs(obj,f_cell,df_cell);
-      [iCfun, jCvar] = find(df);
-    end
   end
 
-  methods (Static)
+  methods (Static, Access = private)
     function [input_frame, input_frame_to_fcn_map] = constructInputFrame(fcns, same_input)
       if nargin < 2, same_input = false; end
       fcn_input_frames = cellfun(@(fcn) fcn.getInputFrame(), ...
