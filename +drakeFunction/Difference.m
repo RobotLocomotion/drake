5c6b931cb1cdaa459fb59f553ea6054fb73080f6
Andrés Valenzuela <avalenzu@mit.edu>
2014-08-18 06:13:36 -0400

    Fix errors in drakeFunction.Difference

--- +drakeFunction/Difference.m
+++ +drakeFunction/Difference.m
@@ -5,8 +5,8 @@ function fcn = Difference(frame,n)
   assert(n>=2,'Drake:DrakeFunction:Difference:BadInput',...
     'The number of points to difference, n, must be at least 2');
   input_frame = MultiCoordinateFrame.constructFrame(repmat({frame},1,n));
-  output_frame = frame;
+  output_frame = MultiCoordinateFrame.constructFrame(repmat({frame},1,n-1));
   nx = frame.dim;
-  A = kron(spdiags(ones(nx,1),0,n-1,n),eye(nx)) + kron(spdiags(-ones(nx,1),1,n-1,n),eye(nx));
+  A = kron(spdiags(ones(n-1,1),0,n-1,n),eye(nx)) + kron(spdiags(-ones(n-1,1),1,n-1,n),eye(nx));
   fcn = drakeFunction.Linear(input_frame,output_frame,A);
 end
