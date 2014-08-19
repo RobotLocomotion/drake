7c14bf00e1dc246bb45e45da4ea274fef0ee6839
mposa <mposa@mit.edu>
2014-08-19 17:49:33 -0400

    Changed doKinematics cache to look for exact matches instead of using a tolerance (was causing "nondeterminism" in simulation). Added tolerances to contactSensorTest.

--- systems/plants/test/contactSensorTest.m
+++ systems/plants/test/contactSensorTest.m
@@ -12,14 +12,16 @@ p = addFrame(p,frame);
 p = addSensor(p,ContactForceTorqueSensor(p,frame));
 p = compile(p);
 
-ytraj = simulate(p,[0 5]);
+T = 2;
+
+[ytraj,xtraj] = simulate(p,[0 T]);
 
 % should find initial conditions for the brick which are resting on the
 % ground. 
-yf = Point(p.getOutputFrame,eval(ytraj,5));
-valuecheck(yf.force_x,0);
-valuecheck(yf.force_z,getMass(p)*norm(getGravity(p)));
-valuecheck(yf.torque,0);
+yf = Point(p.getOutputFrame,eval(ytraj,T));
+valuecheck(yf.force_x,0,1e-6);
+valuecheck(yf.force_z,getMass(p)*norm(getGravity(p)),1e-6);
+valuecheck(yf.torque,0,1e-6);
 
 %v = p.constructVisualizer();
 %v.playback(ytraj);
@@ -34,17 +36,17 @@ p = addFrame(p,frame);
 p = addSensor(p,ContactForceTorqueSensor(p,frame));
 p = compile(p);
 
-ytraj = simulate(p,[0 5]);
+[ytraj,xtraj] = simulate(p,[0 T]);
 
 % should find initial conditions for the brick which are resting on the
 % ground. 
-yf = Point(p.getOutputFrame,eval(ytraj,5));
-valuecheck(yf.force_x,0);
-valuecheck(yf.force_y,0);
-valuecheck(yf.force_z,getMass(p)*norm(getGravity(p)));
-valuecheck(yf.torque_x,0);
-valuecheck(yf.torque_y,0);
-valuecheck(yf.torque_z,0);
+yf = Point(p.getOutputFrame,eval(ytraj,T));
+valuecheck(yf.force_x,0,1e-5);
+valuecheck(yf.force_y,0,1e-5);
+valuecheck(yf.force_z,getMass(p)*norm(getGravity(p)),1e-6);
+valuecheck(yf.torque_x,0,1e-5);
+valuecheck(yf.torque_y,0,1e-5);
+valuecheck(yf.torque_z,0,1e-5);
 warning(S);
 
 %v = p.constructVisualizer();
