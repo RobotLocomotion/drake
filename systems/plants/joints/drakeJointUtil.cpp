b07a211415f0cc45128e7e991bd1bc33fc8bbbef
tkoolen <koolen.twan@gmail.com>
2014-08-20 10:20:52 -0400

    Cleanup.

--- systems/plants/joints/drakeJointUtil.cpp
+++ systems/plants/joints/drakeJointUtil.cpp
@@ -10,27 +10,26 @@
 using namespace Eigen;
 using namespace std;
 
-unique_ptr<DrakeJoint> createJoint(
-    const string& joint_name, const Isometry3d& transform_to_parent_body, int floating, const Vector3d& joint_axis, double pitch)
+unique_ptr<DrakeJoint> createJoint(const string& joint_name, const Isometry3d& transform_to_parent_body, int floating, const Vector3d& joint_axis, double pitch)
 {
   unique_ptr<DrakeJoint> joint;
   switch (floating) {
   case 0: {
     if (pitch == 0.0) {
-      joint = make_unique < RevoluteJoint > (joint_name, transform_to_parent_body, joint_axis);
+      joint = make_unique<RevoluteJoint>(joint_name, transform_to_parent_body, joint_axis);
     } else if (isinf(pitch)) {
-      joint = make_unique < PrismaticJoint > (joint_name, transform_to_parent_body, joint_axis);
+      joint = make_unique<PrismaticJoint>(joint_name, transform_to_parent_body, joint_axis);
     } else {
-      joint = make_unique < HelicalJoint > (joint_name, transform_to_parent_body, joint_axis, pitch);
+      joint = make_unique<HelicalJoint>(joint_name, transform_to_parent_body, joint_axis, pitch);
     }
     break;
   }
   case 1: {
-    joint = make_unique < RollPitchYawFloatingJoint > (joint_name, transform_to_parent_body);
+    joint = make_unique<RollPitchYawFloatingJoint>(joint_name, transform_to_parent_body);
     break;
   }
   case 2: {
-    joint = make_unique < QuaternionFloatingJoint > (joint_name, transform_to_parent_body);
+    joint = make_unique<QuaternionFloatingJoint>(joint_name, transform_to_parent_body);
     break;
   }
   default: {
