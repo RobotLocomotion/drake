f55e94df8635cca727a302ebd95fdf9bc8f736b1
Robin Deits <robin.deits@gmail.com>
2014-08-26 14:10:43 -0400

    fix missing entry

--- examples/Atlas/test/runAtlasWalkingTestMex.m
+++ examples/Atlas/test/runAtlasWalkingTestMex.m
@@ -1,8 +1,8 @@
 function runAtlasWalkingTestMex()
 
 oldpath = addpath(fullfile(getDrakePath,'examples','Atlas'));
+finishup = onCleanup(@() path(oldpath));
 runAtlasWalking(2,0,0,[0.5;0;0;0;0;0]);
-path = oldpath;
 
 end
 
