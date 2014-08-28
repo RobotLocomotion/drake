418ff375429353391ea17e6b399d359f21669f0a
Twan Koolen <tkoolen@csail.mit.edu>
2014-08-28 14:00:04 -0400

    Fixed compile errors on GCC (missing <memory> include and ambiguous call to isinf, see https://bugs.webkit.org/show_bug.cgi?id=59249 for info).

--- systems/plants/joints/drakeJointUtil.cpp
+++ systems/plants/joints/drakeJointUtil.cpp
@@ -6,36 +6,36 @@
 #include "RevoluteJoint.h"
 #include "QuaternionFloatingJoint.h"
 #include "RollPitchYawFloatingJoint.h"
+#include <cmath>
 
 using namespace Eigen;
-using namespace std;
 
-unique_ptr<DrakeJoint> createJoint(const string& joint_name, const Isometry3d& transform_to_parent_body, int floating, const Vector3d& joint_axis, double pitch)
+std::unique_ptr<DrakeJoint> createJoint(const std::string& joint_name, const Isometry3d& transform_to_parent_body, int floating, const Vector3d& joint_axis, double pitch)
 {
-  unique_ptr<DrakeJoint> joint;
+  std::unique_ptr<DrakeJoint> joint;
   switch (floating) {
   case 0: {
     if (pitch == 0.0) {
-      joint = make_unique<RevoluteJoint>(joint_name, transform_to_parent_body, joint_axis);
-    } else if (isinf(pitch)) {
-      joint = make_unique<PrismaticJoint>(joint_name, transform_to_parent_body, joint_axis);
+      joint = std::make_unique<RevoluteJoint>(joint_name, transform_to_parent_body, joint_axis);
+    } else if (std::isinf(pitch)) {
+      joint = std::make_unique<PrismaticJoint>(joint_name, transform_to_parent_body, joint_axis);
     } else {
-      joint = make_unique<HelicalJoint>(joint_name, transform_to_parent_body, joint_axis, pitch);
+      joint = std::make_unique<HelicalJoint>(joint_name, transform_to_parent_body, joint_axis, pitch);
     }
     break;
   }
   case 1: {
-    joint = make_unique<RollPitchYawFloatingJoint>(joint_name, transform_to_parent_body);
+    joint = std::make_unique<RollPitchYawFloatingJoint>(joint_name, transform_to_parent_body);
     break;
   }
   case 2: {
-    joint = make_unique<QuaternionFloatingJoint>(joint_name, transform_to_parent_body);
+    joint = std::make_unique<QuaternionFloatingJoint>(joint_name, transform_to_parent_body);
     break;
   }
   default: {
-    ostringstream stream;
+    std::ostringstream stream;
     stream << "floating type " << floating << " not recognized.";
-    throw runtime_error(stream.str());
+    throw std::runtime_error(stream.str());
     break;
   }
   }
