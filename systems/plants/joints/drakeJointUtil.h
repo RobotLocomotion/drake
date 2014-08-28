210f1f910f12810fe148ab8dedf556d33ca0d83c
Twan Koolen <tkoolen@csail.mit.edu>
2014-08-28 14:00:04 -0400

    Fixed compile errors on GCC (missing <memory> include and ambiguous call to isinf, see https://bugs.webkit.org/show_bug.cgi?id=59249 for info).

--- systems/plants/joints/drakeJointUtil.h
+++ systems/plants/joints/drakeJointUtil.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
+#include <memory>
 
 std::unique_ptr<DrakeJoint> createJoint(
     const std::string& joint_name, const Eigen::Isometry3d& transform_to_parent_body, int floating, const Eigen::Vector3d& joint_axis, double pitch);
