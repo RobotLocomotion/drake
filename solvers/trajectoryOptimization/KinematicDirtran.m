8a7b5099da85d4b9dabb8f93b95d8e3308666ba3
Andrés Valenzuela <avalenzu@mit.edu>
2014-08-29 13:12:50 -0400

    Add solveTraj to KinematicDirtran.

--- solvers/trajectoryOptimization/KinematicDirtran.m
+++ solvers/trajectoryOptimization/KinematicDirtran.m
@@ -13,5 +13,11 @@ classdef KinematicDirtran < KinematicTrajectoryOptimization ...
       %obj.v_inds = obj.u_inds;
       obj.v_inds = obj.x_inds(robot.getNumPositions()+(1:robot.getNumVelocities()),:);
     end
+
+    function [xtraj,z,F,info] = solveTraj(obj,t_init,q_traj_init)
+      traj_init.x = [q_traj_init;q_traj_init.fnder()];      
+      traj_init.u = q_traj_init.fnder().fnder();
+      [xtraj,~,z,F,info] = solveTraj@DirectTrajectoryOptimization(obj,t_init,traj_init);
+    end
   end
 end
