c062e55ec13cfeb74de00bb482dc4b2d74adfb23
Russ Tedrake <russt@mit.edu>
2014-09-10 06:42:39 -0400

    pendulum example runs nicely enough, but have to check for correctness

--- examples/Pendulum/runValueIteration.m
+++ examples/Pendulum/runValueIteration.m
@@ -1,5 +1,24 @@
 function runValueIteration
 
 plant = PendulumPlant;
-mdp = MarkovDecisionProcess.discretizeSystem(plant,{[-pi:.2:pi],[-4:.2:4]},linspace(-1,1,9),1e-2);
+options.dt = 1e-2;
+options.gamma = .95;
+
+xbins = {[0:.2:2*pi],[-4:.2:4]};
+mdp = MarkovDecisionProcess.discretizeSystem(plant,@costfun,xbins,linspace(-1,1,9),options);
+
+function drawfun(J)
+  surf(xbins{1},xbins{2},reshape(J,length(xbins{1}),length(xbins{2}))');
+  xlabel('theta');
+  ylabel('thetadot');
+end
+
+valueIteration(mdp,0.01,@drawfun);
+
+end
+
+function g = costfun(sys,x,u)
+  xd = [pi;0];
+  g = (x-xd)'*(x-xd) + u^2;
+end
 
