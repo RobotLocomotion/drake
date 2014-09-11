9631aa9901235c6eeb714885f918de90f63a13fe
Hongkai Dai <daihongkai@gmail.com>
2014-09-10 20:28:14 -0400

    add the first test case for deleteNonlinearConstraint

--- solvers/test/testNLP.m
+++ solvers/test/testNLP.m
@@ -186,14 +186,15 @@ valuecheck(F{1},F_obj,1e-5);
 
 %%%%%%%%%%%%%%%%%%%%%
 display('test multi-input constraint and cost')
-% x1^2+4*x2^2<=4
-% (x1-2)^2+x2^2<=5
+% min x1*x2
+% -10<=x1^2+4*x2^2<=4
+% -10<=(x1-2)^2+x2^2<=5
 % x1 >= 0
 % 0<=x1+2*x3 <=10
 % x1+3*x3 = 0
 nlp3 = NonlinearProgram(3);
 nlp3 = nlp3.setCheckGrad(true);
-cnstr3 = FunctionHandleConstraint(-inf(2,1),[4;5],2,@cnstr3_userfun);
+cnstr3 = FunctionHandleConstraint([-10;-10],[4;5],2,@cnstr3_userfun);
 cost3 = FunctionHandleObjective(2,@cost3_userfun);
 xind1 = {1;2};
 nlp3 = nlp3.addConstraint(cnstr3,xind1);
@@ -215,6 +216,32 @@ end
 if(x3(1)<-1e-5)
   error('Wrong transcription for SNOPT x_lb');
 end
+
+%%%%%%%%%%%%%%%%%%%
+display('test deleteNonlinearConstraint')
+display('first try to delete all nonlinear constraint');
+nlp4 = nlp3.deleteNonlinearConstraint(1);
+valuecheck(nlp4.num_cin,0);
+valuecheck(nlp4.num_ceq,0);
+if(~isempty(nlp4.cin_lb) || ~isempty(nlp4.cin_ub) )
+  error('The lower and upper bound of cin should be empty');
+end
+[lb,ub] = nlp4.bounds();
+valuecheck(lb,[-inf;-inf;-inf;0]);
+valuecheck(ub,[inf;10;0;0]);
+[iGfun,jGvar] = nlp4.getNonlinearGradientSparsity();
+valuecheck([1 1 1],full(sparse(iGfun,jGvar,ones(size(iGfun)),1+nlp4.num_cin+nlp4.num_ceq,nlp4.num_vars)));
+% x1^2+4*x2^2<=4
+% (x1-2)^2+x2^2<=5
+% x1 >= 0
+% 0<=x1+2*x3 <=10
+% x1+3*x3 = 0
+% x1*x2+x3 = 3
+% 1<=x1+x3^2<=10
+% x1+2*x3*x4 = 5
+
+nlp4 = nlp3.addDecisionVariable(1);
+
 end
 
 function [c,dc] = cnstr1_userfun(x)
@@ -256,6 +283,13 @@ function [c,dc] = cnstr2_userfun(x)
 c = [x(1)*x(2)*x(3);x(2)^2+x(2)*x(3)+2*x(3)^2];
 if(nargout>1)
 dc = [x(2)*x(3) x(1)*x(3) x(1)*x(2); 0 2*x(2)+x(3) x(2)+4*x(3)];
+end
+end
+
+function [c,dc] = cnstr4_userfun(x)
+c = [x(1)*x(2)+x(3);x(1)+x(3)^2;x(1)+2*x(3)*x(4)];
+if(nargout>1)
+  dc = [x(2) x(1) 1 0;1 0 2*x(3) 0;1 0 2*x(4) 2*x(3)];
 end
 end
 
