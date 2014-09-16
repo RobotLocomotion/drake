5d6a615b4f935a8b78031d189349c0d0c0f0d9dc
Russ Tedrake <russt@mit.edu>
2014-09-16 04:53:30 -0400

    adds gradients to barycentric code

--- util/test/baryTest.m
+++ util/test/baryTest.m
@@ -4,7 +4,8 @@ function baryTest
 bins = { linspace(0,5,6), 1, linspace(0,10,11) };
 pts = [5*rand(1,100); ones(1,100); 10*rand(1,100)];
 
-[idx,coef] = barycentricInterpolation(bins,pts);
+[idx,coef,dcoef] = barycentricInterpolation(bins,pts);
+d = size(coef,1);
 
 X = cell(1,numel(bins));
 [X{:}] = ndgrid(bins{:});
@@ -14,4 +15,7 @@ gridpoints = vertcat(X{:});
 for i=1:size(pts,2)
   pt = gridpoints(:,idx(:,i))*coef(:,i);
   valuecheck(pts(:,i),pt);
+
+  dpt = gridpoints(:,idx(:,i))*dcoef(d*(i-1)+(1:d),:);
+  valuecheck(dpt,diag([1 0 1])); % zero because of the singleton dimension
 end
