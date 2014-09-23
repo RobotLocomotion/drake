86e4cc5bb734597da1913daace3539cde39ff14d
Robin Deits <robin.deits@gmail.com>
2014-09-23 12:29:34 -0400

    generalize both environments

--- examples/Quadrotor/runMixedIntegerForest.m
+++ examples/Quadrotor/runMixedIntegerForest.m
@@ -1,8 +1,6 @@
 function runMixedIntegerForest
 
-javaaddpath([getenv('DRC_BASE'), '/software/build/share/java/lcmtypes_eigen-utils.jar']);
 checkDependency('lcmgl');
-lc = lcm.lcm.LCM.getSingleton();
 
 r = Quadrotor();
 % The important trees to create swerving path
@@ -18,93 +16,16 @@ r = addTree(r, [.75,.9,1.75], [2;4.4], -pi/5);
 start = [0;-1.5;.5];
 goal = [1; 10; 0.5];
 % goal = [0;-.5;.5];
-
-lcmgl = LCMGLClient('quad_goal');
-lcmgl.glColor3f(.2,.8,.2);
-lcmgl.sphere(goal, .1, 20, 20);
-lcmgl.switchBuffers();
-
-v = constructVisualizer(r);%,struct('use_contact_shapes',true));
-x0 = double(Point(getStateFrame(r)));  % initial conditions: all-zeros
-x0(1:3) = start;
-v.draw(0,double(x0));
-
-
-b = r.getBody(1);
-obstacles = cell(1, length(b.getContactShapes()));
-for j = 1:length(b.getContactShapes())
-  obstacles{j} = b.getContactShapes{j}.getPoints();
-end
-padded = iris.pad_obstacle_points(obstacles);
-obstacle_pts = cell2mat(reshape(padded, size(padded, 1), [], length(obstacles)));
-
-
-lb = [-3;-2;0];
-ub = [3;13;2];
-A_bounds = [eye(3);-eye(3)];
-b_bounds = [ub; -lb];
-
-figure(1);
-clf
-hold on
-
 seeds = [...
          start';
          goal';
-         ]';
-obs_regions = struct('A', cell(1, size(obstacle_pts, 3)), 'b', cell(1, size(obstacle_pts, 3)));
-for j = 1:size(obstacle_pts, 3)
-  [obs_regions(j).A, obs_regions(j).b] = iris.thirdParty.polytopes.vert2lcon(obstacle_pts(:,:,j)');
-end
-safe_regions = struct('A', {}, 'b', {});
+         ];
+        
 
-% Clear the displayed polytopes
-sendLCMPolytope(0, 0, 0, 0, lc);
-pause
-
-% Draw the bounding box
-sendLCMPolytope(A_bounds, b_bounds, 100, true, lc);
-pause
-
-% Clear the displayed polytopes
-sendLCMPolytope(0, 0, 0, 0, lc);
-
-for j = 1:size(seeds, 2)
-  seed = seeds(:,j);
-  [A, b] = iris.inflate_region(obstacle_pts, A_bounds, b_bounds, seed, struct('require_containment', true, 'error_on_infeas_start', true));
-  safe_regions(end+1) = struct('A', A, 'b', b);
-  sendLCMPolytope(A, b, j, false, lc);
-end
-
-num_cells = 10;
-step_size = (ub - lb) ./ num_cells;
-
-while length(safe_regions) < 9
-  dists = iris.util.obstacle_distance_matrix([obs_regions, safe_regions], lb, ub, num_cells);
-  [~, idx] = max(dists(:));
-  [i,j,k] = ind2sub(size(dists), idx);
-  seed = ([i;j;k] - 1) .* step_size + lb;
-  [A, b] = iris.inflate_region(obstacle_pts, A_bounds, b_bounds, seed, struct('require_containment', true, 'error_on_infeas_start', true));
-  safe_regions(end+1) = struct('A', A, 'b', b);
-  sendLCMPolytope(A, b, length(safe_regions), false, lc);
-end
-
-
-for j = 1:length(safe_regions)
-  A = safe_regions(j).A;
-  b = safe_regions(j).b;
-  V = iris.thirdParty.polytopes.lcon2vert(A,b)';
-  iris.drawing.drawPolyFromVertices(V, 'r');
-end
-drawnow();
-
+lb = [-3;-2;0];
+ub = [3;13;2];
 degree = 3;
 n_segments = 7;
+n_regions = 9;
 
-ytraj = SOSTrajectory(start, goal, {safe_regions}, degree, n_segments, 0.3);
-% save('ytraj.mat', 'ytraj');
-% load('ytraj.mat', 'ytraj');
-ytraj = ytraj.setOutputFrame(DifferentiallyFlatOutputFrame);
-xtraj_from_flat = invertFlatOutputs(r,ytraj);
-v.playback(xtraj_from_flat, struct('slider', true));
-
+runMixedIntegerEnvironment(r, start, goal, lb, ub, seeds, degree, n_segments, n_regions);
--- examples/Quadrotor/runMixedIntegerOffice.m
+++ examples/Quadrotor/runMixedIntegerOffice.m
@@ -1,9 +1,7 @@
 function [r, xtraj, utraj, prog] = runMixedIntegerOffice
 
-javaaddpath([getenv('DRC_BASE'), '/software/build/share/java/lcmtypes_eigen-utils.jar']);
 checkDependency('lcmgl');
 r = Quadrotor();
-lc = lcm.lcm.LCM.getSingleton();
 
 degree = 3;
 n_segments = 7;
@@ -44,95 +42,20 @@ r = addFloatingBox(r, [.6,1.5,.4],[-3.3,-1.8,0.3],0);
 r = addTable(r, [.2,.2,1.0], [-1,-1,.5],2);
 
 
-  v = constructVisualizer(r);%,struct('use_contact_shapes',true));
-  %v.draw(0,double(x0));
-
-
-% bot_bounding_box = RigidBodyBox([.6;.6;.6], [0;0;0], [0;0;0]).getPoints();
-
-b = r.getBody(1);
-obstacles = cell(1, length(b.getContactShapes()));
-for j = 1:length(b.getContactShapes())
-  % obstacles{j} = iris.cspace.minkowski_sum(bot_bounding_box, b.getContactShapes{j}.getPoints());
-  obstacles{j} = b.getContactShapes{j}.getPoints();
-end
-padded = iris.pad_obstacle_points(obstacles);
-obstacle_pts = cell2mat(reshape(padded, size(padded, 1), [], length(obstacles)));
-
-
 lb = [-5;-3.5;0.01];
-ub = [10;3.5;3];
-A_bounds = [eye(3);-eye(3)];
-b_bounds = [ub; -lb];
-
-
-
-
-figure(1);
-clf
-hold on
+ub = [6;3.5;3];
 
 seeds = [...
-         start'; 
-         goal';
+         [4, -1, 1.5];
+         [2.5, .75, 1];
+         [3, 0, 1];
+         start';
+         % goal';
          % [1, 0, .5];
          % [2, 0, .5];
-         [3, 0, 1];
-         [2.5, .75, 1];
-         [4, -1, 1.5];
          % [0, 0, 2];
          % [-2, 0, 1];
          ]';
-obs_regions = struct('A', cell(1, size(obstacle_pts, 3)), 'b', cell(1, size(obstacle_pts, 3)));
-for j = 1:size(obstacle_pts, 3)
-  [obs_regions(j).A, obs_regions(j).b] = iris.thirdParty.polytopes.vert2lcon(obstacle_pts(:,:,j)');
-end
-safe_regions = struct('A', {}, 'b', {});
-for j = 1:size(seeds, 2)
-  seed = seeds(:,j);
-  [A, b] = iris.inflate_region(obstacle_pts, A_bounds, b_bounds, seed, struct('require_containment', true, 'error_on_infeas_start', true));
-  safe_regions(end+1) = struct('A', A, 'b', b);
-  V = iris.thirdParty.polytopes.lcon2vert(A,b)';
-  msg = eigen_utils.eigen_matrixxd_t();
-  msg.rows = size(V, 1);
-  msg.cols = size(V, 2);
-  msg.num_data = msg.rows * msg.cols;
-  msg.data = reshape(V, [], 1);
-  lc.publish('DRAW_POLYTOPE', msg);
-end
+n_regions = 7;
 
-num_cells = 10;
-step_size = (ub - lb) ./ num_cells;
-
-while length(safe_regions) < 7
-  dists = iris.util.obstacle_distance_matrix([obs_regions, safe_regions], lb, ub, num_cells);
-  [~, idx] = max(dists(:));
-  [i,j,k] = ind2sub(size(dists), idx);
-  seed = ([i;j;k] - 1) .* step_size + lb;
-  [A, b] = iris.inflate_region(obstacle_pts, A_bounds, b_bounds, seed, struct('require_containment', true, 'error_on_infeas_start', true));
-  safe_regions(end+1) = struct('A', A, 'b', b);
-  V = iris.thirdParty.polytopes.lcon2vert(A,b)';
-  msg = eigen_utils.eigen_matrixxd_t();
-  msg.rows = size(V, 1);
-  msg.cols = size(V, 2);
-  msg.num_data = msg.rows * msg.cols;
-  msg.data = reshape(V, [], 1);
-  lc.publish('DRAW_POLYTOPE', msg);
-end
-
-
-for j = 1:length(safe_regions)
-  A = safe_regions(j).A;
-  b = safe_regions(j).b;
-  V = iris.thirdParty.polytopes.lcon2vert(A,b)';
-  iris.drawing.drawPolyFromVertices(V, 'r');
-end
-drawnow();
-
-ytraj = SOSTrajectory(start, goal, {safe_regions}, degree, n_segments, 0.6);
-% save('ytraj.mat', 'ytraj');
-% load('ytraj.mat', 'ytraj');
-ytraj = ytraj.setOutputFrame(DifferentiallyFlatOutputFrame);
-xtraj_from_flat = invertFlatOutputs(r,ytraj);
-v.playback(xtraj_from_flat, struct('slider', true));
-
+runMixedIntegerEnvironment(r, start, goal, lb, ub, seeds, degree, n_segments, n_regions);
