78f32bdc98ef5339cf7e8291e5d9d3fb8a920022
Robin Deits <robin.deits@gmail.com>
2014-09-22 23:45:27 -0400

    try auto-seeding regions

--- examples/Quadrotor/runMixedIntegerOffice.m
+++ examples/Quadrotor/runMixedIntegerOffice.m
@@ -5,6 +5,14 @@ checkDependency('lcmgl');
 r = Quadrotor();
 lc = lcm.lcm.LCM.getSingleton();
 
+degree = 3;
+n_segments = 7;
+% start = [5;-1;1.5];
+start = [-2;0;1];
+% goal = [0;0;.5];
+goal = [5;-1;1.5];
+% goal = [6;-1;1.5];
+
 wall_height = 3.0;
 
 
@@ -40,60 +48,88 @@ r = addTable(r, [.2,.2,1.0], [-1,-1,.5],2);
   %v.draw(0,double(x0));
 
 
-bot_bounding_box = RigidBodyBox([.6;.6;.6], [0;0;0], [0;0;0]).getPoints();
+% bot_bounding_box = RigidBodyBox([.6;.6;.6], [0;0;0], [0;0;0]).getPoints();
 
 b = r.getBody(1);
 obstacles = cell(1, length(b.getContactShapes()));
 for j = 1:length(b.getContactShapes())
-  obstacles{j} = iris.cspace.minkowski_sum(bot_bounding_box, b.getContactShapes{j}.getPoints());
+  % obstacles{j} = iris.cspace.minkowski_sum(bot_bounding_box, b.getContactShapes{j}.getPoints());
+  obstacles{j} = b.getContactShapes{j}.getPoints();
 end
 padded = iris.pad_obstacle_points(obstacles);
 obstacle_pts = cell2mat(reshape(padded, size(padded, 1), [], length(obstacles)));
 
-lb = [-10;-10;0];
-ub = [10;10;3];
+
+lb = [-5;-3.5;0.01];
+ub = [10;3.5;3];
 A_bounds = [eye(3);-eye(3)];
 b_bounds = [ub; -lb];
+
+
+
 
 figure(1);
 clf
 hold on
 
 seeds = [...
-         [1, 0, .5];
-         [2, 0, .5];
+         start'; 
+         goal';
+         % [1, 0, .5];
+         % [2, 0, .5];
          [3, 0, 1];
          [2.5, .75, 1];
          [4, -1, 1.5];
-         [0, 0, 2];
-         [-2, 0, 1];
+         % [0, 0, 2];
+         % [-2, 0, 1];
          ]';
+obs_regions = struct('A', cell(1, size(obstacle_pts, 3)), 'b', cell(1, size(obstacle_pts, 3)));
+for j = 1:size(obstacle_pts, 3)
+  [obs_regions(j).A, obs_regions(j).b] = iris.thirdParty.polytopes.vert2lcon(obstacle_pts(:,:,j)');
+end
 safe_regions = struct('A', {}, 'b', {});
 for j = 1:size(seeds, 2)
   seed = seeds(:,j);
   [A, b] = iris.inflate_region(obstacle_pts, A_bounds, b_bounds, seed, struct('require_containment', true, 'error_on_infeas_start', true));
+  safe_regions(end+1) = struct('A', A, 'b', b);
   V = iris.thirdParty.polytopes.lcon2vert(A,b)';
-  iris.drawing.drawPolyFromVertices(V, 'r');
-
   msg = eigen_utils.eigen_matrixxd_t();
   msg.rows = size(V, 1);
   msg.cols = size(V, 2);
   msg.num_data = msg.rows * msg.cols;
   msg.data = reshape(V, [], 1);
   lc.publish('DRAW_POLYTOPE', msg);
+end
+
+num_cells = 10;
+step_size = (ub - lb) ./ num_cells;
+
+while length(safe_regions) < 7
+  dists = iris.util.obstacle_distance_matrix([obs_regions, safe_regions], lb, ub, num_cells);
+  [~, idx] = max(dists(:));
+  [i,j,k] = ind2sub(size(dists), idx);
+  seed = ([i;j;k] - 1) .* step_size + lb;
+  [A, b] = iris.inflate_region(obstacle_pts, A_bounds, b_bounds, seed, struct('require_containment', true, 'error_on_infeas_start', true));
   safe_regions(end+1) = struct('A', A, 'b', b);
+  V = iris.thirdParty.polytopes.lcon2vert(A,b)';
+  msg = eigen_utils.eigen_matrixxd_t();
+  msg.rows = size(V, 1);
+  msg.cols = size(V, 2);
+  msg.num_data = msg.rows * msg.cols;
+  msg.data = reshape(V, [], 1);
+  lc.publish('DRAW_POLYTOPE', msg);
+end
+
+
+for j = 1:length(safe_regions)
+  A = safe_regions(j).A;
+  b = safe_regions(j).b;
+  V = iris.thirdParty.polytopes.lcon2vert(A,b)';
+  iris.drawing.drawPolyFromVertices(V, 'r');
 end
 drawnow();
 
-degree = 3;
-n_segments = 7;
-% start = [5;-1;1.5];
-start = [-2;0;1];
-% goal = [0;0;.5];
-goal = [5;-1;1.5];
-% goal = [6;-1;1.5];
-
-ytraj = SOSTrajectory(start, goal, {safe_regions}, degree, n_segments);
+ytraj = SOSTrajectory(start, goal, {safe_regions}, degree, n_segments, 0.6);
 % save('ytraj.mat', 'ytraj');
 % load('ytraj.mat', 'ytraj');
 ytraj = ytraj.setOutputFrame(DifferentiallyFlatOutputFrame);
