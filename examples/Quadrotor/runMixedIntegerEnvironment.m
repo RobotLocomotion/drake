bcd6dbc5ae9171c10ca559f1da50f8decb3d8e1c
Robin Deits <robin.deits@gmail.com>
2014-09-24 20:08:07 -0400

    move SOS trajectory into drake

--- examples/Quadrotor/runMixedIntegerEnvironment.m
+++ examples/Quadrotor/runMixedIntegerEnvironment.m
@@ -18,68 +18,41 @@ obstacles = cell(1, length(b.getContactShapes()));
 for j = 1:length(b.getContactShapes())
   obstacles{j} = b.getContactShapes{j}.getPoints();
 end
-padded = iris.pad_obstacle_points(obstacles);
-obstacle_pts = cell2mat(reshape(padded, size(padded, 1), [], length(obstacles)));
-
-A_bounds = [eye(3);-eye(3)];
-b_bounds = [ub; -lb];
-
 figure(1);
 clf
 hold on
 
-obs_regions = struct('A', cell(1, size(obstacle_pts, 3)), 'b', cell(1, size(obstacle_pts, 3)));
-for j = 1:size(obstacle_pts, 3)
-  [obs_regions(j).A, obs_regions(j).b] = iris.thirdParty.polytopes.vert2lcon(obstacle_pts(:,:,j)');
-end
-safe_regions = struct('A', {}, 'b', {});
 
 % Clear the displayed polytopes
-sendLCMPolytope(0, 0, 0, 0, lc);
-pause
+drawLCMPolytope(0, 0, 0, 0, lc);
+% pause
 
 % Draw the bounding box
-sendLCMPolytope(A_bounds, b_bounds, 100, true, lc);
-pause
+dim = length(lb);
+A_bounds = [eye(dim);-eye(dim)];
+b_bounds = [ub; -lb];
+drawLCMPolytope(A_bounds, b_bounds, 100, true, lc);
+% pause
 
 % Clear the displayed polytopes
-sendLCMPolytope(0, 0, 0, 0, lc);
+drawLCMPolytope(0, 0, 0, 0, lc);
 
-lcmgl = LCMGLClient('region_seed');
-for j = 1:size(seeds, 2)
-  seed = seeds(:,j);
-  [A, b] = iris.inflate_region(obstacle_pts, A_bounds, b_bounds, seed, struct('require_containment', true, 'error_on_infeas_start', true));
-  safe_regions(end+1) = struct('A', A, 'b', b);
-  sendLCMPolytope(A, b, j, false, lc);
+region_idx = 1;
+function done = drawRegion(r, seed)
+  drawLCMPolytope(r.A, r.b, region_idx, false, lc);
+  region_idx = region_idx + 1;
   lcmgl.glColor3f(.8,.8,.2);
   lcmgl.sphere(seed, 0.06, 20, 20);
   lcmgl.switchBuffers();
-  pause
+  done = false;
 end
 
-num_cells = 10;
-step_size = (ub - lb) ./ num_cells;
+num_steps = 10;
+safe_regions = iris.util.auto_seed_regions(obstacles, lb, ub, seeds, n_regions, num_steps, @drawRegion);
 
-while length(safe_regions) < n_regions
-  dists = iris.util.obstacle_distance_matrix([obs_regions, safe_regions], lb, ub, num_cells);
-  [~, idx] = max(dists(:));
-  [i,j,k] = ind2sub(size(dists), idx);
-  seed = ([i;j;k] - 1) .* step_size + lb;
-  [A, b] = iris.inflate_region(obstacle_pts, A_bounds, b_bounds, seed, struct('require_containment', true, 'error_on_infeas_start', true));
-  safe_regions(end+1) = struct('A', A, 'b', b);
-  sendLCMPolytope(A, b, length(safe_regions), false, lc);
-end
-
-
-for j = 1:length(safe_regions)
-  A = safe_regions(j).A;
-  b = safe_regions(j).b;
-  V = iris.thirdParty.polytopes.lcon2vert(A,b)';
-  iris.drawing.drawPolyFromVertices(V, 'r');
-end
 drawnow();
 
-ytraj = SOSTrajectory(start, goal, {safe_regions}, degree, n_segments, bot_radius);
+ytraj = findSOSTrajectory(start, goal, {safe_regions}, degree, n_segments, bot_radius);
 folder_name = ['~/locomotion/papers/icra-2015-uav-miqp/data/', datestr(now,'yyyy-mm-dd_HH.MM.SS')];
 system(sprintf('mkdir -p %s', folder_name));
 % load('ytraj.mat', 'ytraj');
@@ -87,4 +60,6 @@ ytraj = ytraj.setOutputFrame(DifferentiallyFlatOutputFrame);
 xtraj = invertFlatOutputs(r,ytraj);
 save([folder_name, '/results.mat'], 'ytraj', 'r', 'v', 'xtraj');
 v.playback(xtraj, struct('slider', true));
+
+end
 
