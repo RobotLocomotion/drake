11b897de03163bdb860b589c541d70c874808cb2
Russ Tedrake <russt@mit.edu>
2014-10-11 17:24:12 -0400

    dircol example

--- examples/DoubleIntegrator.m
+++ examples/DoubleIntegrator.m
@@ -111,7 +111,41 @@ classdef DoubleIntegrator < LinearSystem
       breakpoints = linspace(0,2,N);  % initial breakpoints t=0,...,2 seconds
       prog.solveTraj(breakpoints);
     end
-    
+
+    function runDircol
+      % Simple example of Direct Transcription trajectory optimization
+
+      % create the system
+      plant = DoubleIntegrator;
+      
+      % set up a direct transcription problem with N knot points and bounds on
+      % the duration
+      N = 20;
+      prog = DircolTrajectoryOptimization(plant,N,[0.1 10]);
+      
+      % add the initial value constraint
+      x0 = [-2; -2];
+      prog = addStateConstraint(prog,BoundingBoxConstraint(x0,x0),1);
+      
+      % add the final value constraint
+      xf = [0;0];
+      prog = addStateConstraint(prog,BoundingBoxConstraint(xf,xf),N);
+      
+      % add the cost function g(dt,x,u) = 1*dt
+      prog = addRunningCost(prog,@(dt,x,u) dt);
+      
+      % add a display function to draw the trajectory on every iteration
+      function displayStateTrajectory(t,x,u)
+        plot(x(1,:),x(2,:),'b.-','MarkerSize',10);
+        axis([-5,1,-2.5,2.5]); axis equal;
+        drawnow;
+      end
+      prog = addTrajectoryDisplayFunction(prog,@displayStateTrajectory);
+      
+      % solve the optimization problem.
+      breakpoints = linspace(0,2,N);  % initial breakpoints t=0,...,2 seconds
+      prog.solveTraj(breakpoints);
+    end    
   end
   
 end
