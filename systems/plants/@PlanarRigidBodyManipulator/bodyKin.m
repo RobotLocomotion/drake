47e9edca40403054d0b958cbd948ead1e0e47a63
Andrés Valenzuela <avalenzu@mit.edu>
2014-10-13 14:57:57 -0400

    Update PlanarRBM/bodyKin for RBM/bodyKin changes

--- systems/plants/@PlanarRigidBodyManipulator/bodyKin.m
+++ systems/plants/@PlanarRigidBodyManipulator/bodyKin.m
@@ -1,4 +1,4 @@
-function [x,J,P] = bodyKin(obj,kinsol,body_ind,pts)
+function [x,P,J,dP,dJ] = bodyKin(obj,kinsol,body_ind,pts)
 % computes the position of pts (given in the global frame) in the body frame
 % (see documentation for bodyKin in RigidBodyManipulator)
 %
@@ -7,14 +7,18 @@ function [x,J,P] = bodyKin(obj,kinsol,body_ind,pts)
 % returned).  
 
 [n,m] = size(pts); 
-if (n==2) pts = obj.T_2D_to_3D * pts; end
+if (n==2), pts = obj.T_2D_to_3D * pts; end
 
-if (nargout>2)
-  [x,J,P] = bodyKin@RigidBodyManipulator(obj,kinsol,body_ind,pts);
+if (nargout>4)
+  [x,P,J,dP,dJ] = bodyKin@RigidBodyManipulator(obj,kinsol,body_ind,pts);
+elseif (nargout>3)
+  [x,P,J,dP] = bodyKin@RigidBodyManipulator(obj,kinsol,body_ind,pts);
+elseif (nargout>2)
+  [x,P,J] = bodyKin@RigidBodyManipulator(obj,kinsol,body_ind,pts);
 elseif (nargout>1)
-  [x,J] = bodyKin@RigidBodyManipulator(obj,kinsol,body_ind,pts);
+  [x,P] = bodyKin@RigidBodyManipulator(obj,kinsol,body_ind,pts);
 else
   x = bodyKin@RigidBodyManipulator(obj,kinsol,body_ind,pts);
 end
 
-if (n==2) x = obj.T_2D_to_3D'*x; end
+if (n==2), x = obj.T_2D_to_3D'*x; end
