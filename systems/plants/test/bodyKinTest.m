43f8e675d5b12a444ee2293b4ff5d38704f0fb4c
Andrés Valenzuela <avalenzu@mit.edu>
2014-10-13 14:56:54 -0400

    Change output argument order in bodyKin
    P should come before J since it's not a gradient w.r.t. q.

--- systems/plants/test/bodyKinTest.m
+++ systems/plants/test/bodyKinTest.m
@@ -9,6 +9,7 @@ function bodyKinTest()
 
   geval_options.grad_method = {'user','taylorvar'};
   [x,J,dJ] = geval(@(q)bodyKinWrapper(q,r,body_ind,pts),q,geval_options);
+  [P,dP] = geval(@(q)bodyKinPWrapper(q,r,body_ind,pts),q,geval_options);
 end
 
 function [x,J,dJ] = bodyKinWrapper(q,r,body_ind,pts)
@@ -17,8 +18,18 @@ function [x,J,dJ] = bodyKinWrapper(q,r,body_ind,pts)
     case 1
       x = r.bodyKin(kinsol,body_ind,pts);
     case 2
-      [x, J] = r.bodyKin(kinsol,body_ind,pts);
+      [x, ~, J] = r.bodyKin(kinsol,body_ind,pts);
     case 3
-      [x, J, ~, dJ] = r.bodyKin(kinsol,body_ind,pts);
+      [x, ~, J, ~, dJ] = r.bodyKin(kinsol,body_ind,pts);
+  end
+end
+
+function [P,dP] = bodyKinPWrapper(q,r,body_ind,pts)
+  kinsol = r.doKinematics(q,true,false);
+  switch nargout
+    case 1
+      [~, P] = r.bodyKin(kinsol,body_ind,pts);
+    case 2
+      [~, P, ~, dP] = r.bodyKin(kinsol,body_ind,pts);
   end
 end
