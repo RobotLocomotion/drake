4274a44956974904a84a62bda326497466e38f16
Russ Tedrake <russt@mit.edu>
2014-10-20 08:33:31 -0400

    added comment

--- examples/CubicPolynomialExample.m
+++ examples/CubicPolynomialExample.m
@@ -55,6 +55,8 @@ classdef CubicPolynomialExample < PolynomialSystem
       % compute region of attraction       
       % the levelset V<1 is the region of attraction
       V=regionOfAttraction(p,Point(p.getStateFrame,0));
+      
+      % display the polynomial representation of V that results
       display(V.getPoly);
     end
     
