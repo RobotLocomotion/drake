a8c4d8d270ce00bfd42862bb735320ff0883ef77
hongkai <daihongkai@gmail.com>
2014-10-30 01:12:19 -0400

    add the test for GraspFrictionConeWrench. The test for old GraspWrench is broken

--- solvers/trajectoryOptimization/GraspFrictionConeWrench.m
+++ solvers/trajectoryOptimization/GraspFrictionConeWrench.m
@@ -19,6 +19,8 @@ classdef GraspFrictionConeWrench < RigidBodyContactWrench
       if(~isnumeric(grasp_object_idx) || numel(grasp_object_idx) ~= 1)
         error('Drake:GraspFrictionConeWrench: grasp_object_idx should be a scalar');
       end
+      obj.finger_idx = finger_idx;
+      obj.finger_grasp_pt = finger_grasp_pt;
       obj.grasp_object_idx = grasp_object_idx;
       mu_size = size(FC_mu);
       if(length(mu_size) == 2 && mu_size(1) == 1 && mu_size(2) == 1)
@@ -28,7 +30,8 @@ classdef GraspFrictionConeWrench < RigidBodyContactWrench
       if(~isnumeric(FC_mu) || any(FC_mu<0) || length(mu_size) ~= 2 || mu_size(1) ~= 1 || mu_size(2) ~= obj.num_pts)
         error('Drake:FrictionConeWrench: FC_mu should be a non-negative 1 x num_pts array');
       end
-      axis_size = size(grasp_objectFC_axis);
+      obj.FC_mu = FC_mu;
+      axis_size = size(grasp_object_FC_axis);
       if(length(axis_size) == 2 && axis_size(1) == 3 && axis_size(2) == 1)
         grasp_object_FC_axis = bsxfun(@times,grasp_object_FC_axis,ones(1,obj.num_pts));
         axis_size = size(grasp_object_FC_axis);
@@ -55,18 +58,21 @@ classdef GraspFrictionConeWrench < RigidBodyContactWrench
       obj.contact_force_type = RigidBodyContactWrench.GraspFrictionConeType;
       obj.wrench_cnstr_lb = zeros(2*obj.num_pts,1);
       obj.wrench_cnstr_ub = inf(2*obj.num_pts,1);
-      obj.wrench_cnstr_name = cell(obj.num_pts,1);
+      obj.wrench_cnstr_name = cell(2*obj.num_pts,1);
       for i = 1:obj.num_pts
         obj.wrench_cnstr_name{i} = sprintf('%s_pt%d_friction_cone',obj.body_name,i);
         obj.wrench_cnstr_name{i+obj.num_pts} = sprintf('%s_pt%d_friction_cone',obj.body_name,i);
       end
       nq = obj.robot.getNumPositions;
-      obj.wrench_iCfun = repmat([reshape(bsxfun(@times,(1:obj.num_pts)',ones(1,nq)),[],1);reshape(bsxfun(@times,ones(3,1),1:obj.num_pts),[],1)],2,1);
+      obj.wrench_iCfun = [reshape(bsxfun(@times,(1:obj.num_pts)',ones(1,nq)),[],1);reshape(bsxfun(@times,ones(3,1),1:obj.num_pts),[],1)];
+      obj.wrench_iCfun = [obj.wrench_iCfun;obj.wrench_iCfun+obj.num_pts];
       obj.wrench_jCvar = repmat([reshape(bsxfun(@times,ones(obj.num_pts,1),1:nq),[],1);obj.robot.getNumPositions+(1:3*obj.num_pts)'],2,1);
     end
     
     function [c,dc] = evalWrenchConstraint(obj,kinsol,F,slack)
-      [grasp_object_quat,dgrasp_object_quat] = forwardKin(obj.robot,kinsol,obj.grasp_object_idx,2);
+      [grasp_object_pos,dgrasp_object_pos] = forwardKin(obj.robot,kinsol,obj.grasp_object_idx,[0;0;0],2);
+      grasp_object_quat = grasp_object_pos(4:7);
+      dgrasp_object_quat = dgrasp_object_pos(4:7,:);
       [grasp_object_rotmat,dgrasp_object_rotmat_dquat] = quat2rotmat(grasp_object_quat);
       dgrasp_object_rotmat_dq = dgrasp_object_rotmat_dquat*dgrasp_object_quat;
       F_body = grasp_object_rotmat'*F;
@@ -85,7 +91,7 @@ classdef GraspFrictionConeWrench < RigidBodyContactWrench
         dc1dq(i,:) = reshape(obj.grasp_object_FC_axis(:,i)*F(:,i)',1,[])*dgrasp_object_rotmat_transpose_dq;
         dc2dF_body = (2*F_times_axis(i)*obj.grasp_object_FC_axis(:,i)'-obj.cos_theta_square(i)*2*F_body(:,i)');
         dc2dF(i,(i-1)*3+(1:3)) = dc2dF_body*grasp_object_rotmat';
-        dc2dq(i,:) = reshape(dc2dF_body*F(:,i)',1,[])*dgrasp_object_rotmat_transpose_dq;
+        dc2dq(i,:) = reshape(dc2dF_body'*F(:,i)',1,[])*dgrasp_object_rotmat_transpose_dq;
       end
       
       dc = [dc1dq dc1dF;dc2dq dc2dF];
@@ -106,6 +112,12 @@ classdef GraspFrictionConeWrench < RigidBodyContactWrench
       % @retval J       -- A matrix of size prod(size(pos)) x nq. The gradient of pos
       % w.r.t q
       [pos,J] = forwardKin(obj.robot,kinsol,obj.body,obj.body_pts,0);
+    end
+  end
+  
+  methods(Access = protected)
+    function lincon = generateWrenchLincon(obj)
+      lincon = LinearConstraint([],[],zeros(0,3*obj.num_pts));
     end
   end
 end
