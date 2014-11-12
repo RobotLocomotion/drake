2a841d4cf0fce8f57a8f1288fff076d723a5d3cc
hongkai <daihongkai@gmail.com>
2014-11-12 09:00:16 -0500

    one of the test machine does not like to call kron for taylorvar, change to use numerical difference to verify the gradient

--- util/test/testRPYdot2Angularvel.m
+++ util/test/testRPYdot2Angularvel.m
@@ -1,6 +1,7 @@
 function testRPYdot2Angularvel()
 % test both rpydot2angularvel and rpydot2angularvelMatrix
-options.grad_method = {'taylorvar','user'};
+options.grad_method = {'numerical','user'};
+options.tol = 1e-4;
 for i = 1:100
   rpy = uniformlyRandomRPY();
   rpydot = randn(3,1);
