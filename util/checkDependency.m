c8fc3e2cdbaccbe17c91dd4d1da8aab7f4120b16
Andrés Valenzuela <avalenzu@mit.edu>
2014-11-13 09:38:38 -0500

    Fix bad adjustment to checkDependency.

--- util/checkDependency.m
+++ util/checkDependency.m
@@ -56,7 +56,7 @@ else % then try to evaluate the dependency now...
 
         if (conf.lcm_enabled)
           javaaddpathProtectGlobals(fullfile(pods_get_base_path,'share','java','lcmtypes_drake.jar'));
-          [retval,info] = systemWCMakeEnv('util/check_multicast_is_loopback.sh');
+          [retval,info] = systemWCMakeEnv(fullfile(getDrakePath(),'util','check_multicast_is_loopback.sh'));
           if (retval)
             info = strrep(info,'ERROR: ','');
             info = strrep(info,'./',[getDrakePath,'/util/']);
