f057e50c607a3a385381145c68a1ba0ef26d7bbf
peteflorence <pete.florence@gmail.com>
2014-12-02 10:27:16 -0500

    starting runPennDircol

--- examples/Quadrotor/QuadPlantPenn.m
+++ examples/Quadrotor/QuadPlantPenn.m
@@ -1,7 +1,7 @@
 classdef QuadPlantPenn < SecondOrderSystem
-    % Modified from D. Mellinger, N. Michael, and V. Kumar, 
+    % Modified from D. Mellinger, N. Michael, and V. Kumar,
     % "Trajectory generation and control for precise aggressive maneuvers with quadrotors",
-    %  In Proceedings of the 12th International Symposium on Experimental Robotics (ISER 2010), 2010. 
+    %  In Proceedings of the 12th International Symposium on Experimental Robotics (ISER 2010), 2010.
     
     methods
         function obj = QuadPlantPenn()
@@ -11,11 +11,11 @@ classdef QuadPlantPenn < SecondOrderSystem
         end
         
         function m = getMass(obj)
-          m = obj.m
+            m = obj.m
         end
         
         function I = getInertia(obj)
-          I = obj.I;
+            I = obj.I;
         end
         
         function qdd = sodynamics(obj,t,q,qd,u)
@@ -42,11 +42,11 @@ classdef QuadPlantPenn < SecondOrderSystem
             
             % states
             x = [q;qd];
-                        
+            
             phi = x(4);
             theta = x(5);
             psi = x(6);
-
+            
             phidot = x(10);
             thetadot = x(11);
             psidot = x(12);
@@ -66,7 +66,7 @@ classdef QuadPlantPenn < SecondOrderSystem
             F3 = kf*w3;
             F4 = kf*w4;
             
-            km = 0.0245; 
+            km = 0.0245;
             
             M1 = km*w1;
             M2 = km*w2;
@@ -78,25 +78,25 @@ classdef QuadPlantPenn < SecondOrderSystem
             
             pqr = rpydot2angularvel([phi;theta;psi],[phidot;thetadot;psidot]);
             pqr = R'*pqr;
-
+            
             pqr_dot = invI*([L*(F2-F4);L*(F3-F1);(M1-M2+M3-M4)] - cross(pqr,I*pqr));
-  
+            
             % Now, convert pqr_dot to rpy_ddot
             [Phi, dPhi] = angularvel2rpydotMatrix([phi;theta;psi]);
             
             Rdot =  [ 0, sin(phi)*sin(psi) + cos(phi)*cos(psi)*sin(theta),   cos(phi)*sin(psi) - cos(psi)*sin(phi)*sin(theta); ...
-                      0, cos(phi)*sin(psi)*sin(theta) - cos(psi)*sin(phi), - cos(phi)*cos(psi) - sin(phi)*sin(psi)*sin(theta); ...
-                      0,                              cos(phi)*cos(theta),                               -cos(theta)*sin(phi)]*phidot + ...
-                      [ -cos(psi)*sin(theta), cos(psi)*cos(theta)*sin(phi), cos(phi)*cos(psi)*cos(theta); ...
-                        -sin(psi)*sin(theta), cos(theta)*sin(phi)*sin(psi), cos(phi)*cos(theta)*sin(psi); ...
-                                -cos(theta),         -sin(phi)*sin(theta),         -cos(phi)*sin(theta)]*thetadot + ...
-                               [ -cos(theta)*sin(psi), - cos(phi)*cos(psi) - sin(phi)*sin(psi)*sin(theta), cos(psi)*sin(phi) - cos(phi)*sin(psi)*sin(theta); ...
-                                 cos(psi)*cos(theta),   cos(psi)*sin(phi)*sin(theta) - cos(phi)*sin(psi), sin(phi)*sin(psi) + cos(phi)*cos(psi)*sin(theta); ...
-                                                   0,                                                  0,                                                0]*psidot;
-  
+                0, cos(phi)*sin(psi)*sin(theta) - cos(psi)*sin(phi), - cos(phi)*cos(psi) - sin(phi)*sin(psi)*sin(theta); ...
+                0,                              cos(phi)*cos(theta),                               -cos(theta)*sin(phi)]*phidot + ...
+                [ -cos(psi)*sin(theta), cos(psi)*cos(theta)*sin(phi), cos(phi)*cos(psi)*cos(theta); ...
+                -sin(psi)*sin(theta), cos(theta)*sin(phi)*sin(psi), cos(phi)*cos(theta)*sin(psi); ...
+                -cos(theta),         -sin(phi)*sin(theta),         -cos(phi)*sin(theta)]*thetadot + ...
+                [ -cos(theta)*sin(psi), - cos(phi)*cos(psi) - sin(phi)*sin(psi)*sin(theta), cos(psi)*sin(phi) - cos(phi)*sin(psi)*sin(theta); ...
+                cos(psi)*cos(theta),   cos(psi)*sin(phi)*sin(theta) - cos(phi)*sin(psi), sin(phi)*sin(psi) + cos(phi)*cos(psi)*sin(theta); ...
+                0,                                                  0,                                                0]*psidot;
+            
             rpy_ddot = Phi*R*pqr_dot + reshape((dPhi*[phidot;thetadot;psidot]),3,3)*R*pqr + ...
-                       Phi*Rdot*pqr;
-
+                Phi*Rdot*pqr;
+            
             % xdot = [x(7:12);xyz_ddot;rpy_ddot];
             qdd = [xyz_ddot;rpy_ddot];
             
@@ -111,10 +111,42 @@ classdef QuadPlantPenn < SecondOrderSystem
             x = zeros(12,1);
         end
         
+        function obj = addTrees(obj,number_of_obstacles)
+            % Adds a random forest of trees
+            if nargin<2, number_of_obstacles = 5*(randi(5)+2); end
+            for i=1:number_of_obstacles
+                % Populates an area of the forest
+                xy = [20,0;0,12]*(rand(2,1) - [0.5;0]);
+                % Creates a clear path through the middle of the forest
+                while(norm(xy)<1 || (xy(1,1)<=1.5 && xy(1,1)>=-1.5)), xy = randn(2,1); end
+                height = 1+rand;
+                width_param = rand(1,2);
+                yaw = randn;
+                obj = obj.addTree([width_param height],xy,yaw);
+            end
+            obj = compile(obj);
+        end
+        
+        function obj = addTree(obj, lwh, xy, yaw)
+            % Adds a single tree with specified length width height, xy
+            % location, and yaw orientation.
+            height = lwh(1,3);
+            width_param = lwh(1,1:2);
+            treeTrunk = RigidBodyBox([.2+.8*width_param height],...
+                [xy;height/2],[0;0;yaw]);
+            treeTrunk.c = [83,53,10]/255;  % brown
+            obj = addGeometryToBody(obj,'world',treeTrunk);
+            treeLeaves = RigidBodyBox(1.5*[.2+.8*width_param height/4],...
+                [xy;height + height/8],[0;0;yaw]);
+            treeLeaves.c = [0,0.7,0];  % green
+            obj = addGeometryToBody(obj,'world',treeLeaves);
+            obj = compile(obj);
+        end
+        
     end
     properties
-      m = .5;
-      I = diag([0.0023,0.0023,0.004]);
+        m = .5;
+        I = diag([0.0023,0.0023,0.004]);
     end
     
 end
---
+++ examples/Quadrotor/runPennDircol.m
@@ -0,0 +1,80 @@
+function [utraj,xtraj,prog,r] = runPennDircol
+
+% simple planning demo which takes the quadrotor from hover at x=0m to a new hover at
+% x=2m with minimal thrust.
+
+
+%plant = 'russ';
+plant = 'penn';
+
+if plant == 'russ'
+    r = Quadrotor();
+    disp('using russ rotor!')
+elseif plant == 'penn'
+    r = QuadPlantPenn(); % Quadrotor constructor
+    disp('using penn rotor!')
+end
+
+
+N = 11;
+minimum_duration = .1;
+maximum_duration = 4;
+prog = DircolTrajectoryOptimization(r,N,[minimum_duration maximum_duration]);
+
+x0 = Point(getStateFrame(r));  % initial conditions: all-zeros
+
+if plant == 'russ'
+    x0.base_z = .5; % lift the quad off the ground
+elseif plant == 'penn'
+    x0.z = .5; % lift the quad off the ground
+end
+
+
+u0 = double(nominalThrust(r));
+
+prog = prog.addStateConstraint(ConstantConstraint(double(x0)),1); % DirectTrajectoryOptimization method
+prog = prog.addInputConstraint(ConstantConstraint(u0),1); % DirectTrajectoryOptimization method
+
+xf = x0;                       % final conditions: translated in x
+xf.base_x = 2;                 % translate x by 2
+prog = prog.addStateConstraint(ConstantConstraint(double(xf)),N); % DirectTrajectoryOptimization method
+prog = prog.addInputConstraint(ConstantConstraint(u0),N); % DirectTrajectoryOptimization method
+
+prog = prog.addRunningCost(@cost); % note: DirCol not Direct!  DircolTrajectoryOptimization method
+prog = prog.addFinalCost(@finalCost); % DirectTrajectoryOptimization method
+
+tf0 = 2;                      % initial guess at duration
+traj_init.x = PPTrajectory(foh([0,tf0],[double(x0),double(xf)])); % traj.init.x is a PPTrajectory < Trajectory < DrakeSystem
+traj_init.u = ConstantTrajectory(u0); % traj_init.u is a ConstantTrajectory < Trajectory < DrakeSystem
+
+info=0;
+while (info~=1)
+    tic
+    [xtraj,utraj,z,F,info] = prog.solveTraj(tf0,traj_init); % DirectTrajectoryOptimization method
+    toc
+end
+
+if (nargout<1)
+    v = constructVisualizer(r);
+    v.playback(xtraj,struct('slider',true));
+end
+
+end
+
+function [g,dg] = cost(dt,x,u)
+
+R = eye(4);
+g = u'*R*u;
+%g = sum((R*u).*u,1);
+dg = [zeros(1,1+size(x,1)),2*u'*R];
+%dg = zeros(1, 1 + size(x,1) + size(u,1));
+
+end
+
+function [h,dh] = finalCost(t,x)
+
+h = t;
+dh = [1,zeros(1,size(x,1))];
+
+end
+
