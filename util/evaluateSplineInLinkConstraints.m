cf81be78cca4fc1a06496b89876ea5486e7ce833
Robin Deits <robin.deits@gmail.com>
2014-12-04 18:19:57 -0500

    make our cubicSplineCoeffs format compatible with mkpp

--- util/evaluateSplineInLinkConstraints.m
+++ util/evaluateSplineInLinkConstraints.m
@@ -4,16 +4,13 @@ function [y,ydot,yddot] = evaluateSplineInLinkConstraints(t,link_constraints,ind
   tt = t-link_constraints(index).ts(knot_ind);
 
   knot_ind = min(knot_ind,length(link_constraints(index).ts)-1);
-  a0 = link_constraints(index).a0(:,knot_ind);
-  a1 = link_constraints(index).a1(:,knot_ind);
-  a2 = link_constraints(index).a2(:,knot_ind);
-  a3 = link_constraints(index).a3(:,knot_ind);
+  coefs = link_constraints(index).coefs(:,knot_ind,:);
   if nargout < 2
-    y = evalCubicSplineSegment(tt,a0,a1,a2,a3);
+    y = evalCubicSplineSegment(tt,coefs);
   elseif nargout < 3
-    [y,ydot] = evalCubicSplineSegment(tt,a0,a1,a2,a3);
+    [y,ydot] = evalCubicSplineSegment(tt,coefs);
   else
-    [y,ydot,yddot] = evalCubicSplineSegment(tt,a0,a1,a2,a3);
+    [y,ydot,yddot] = evalCubicSplineSegment(tt,coefs);
   end
 end
 
