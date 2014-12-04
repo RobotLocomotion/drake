9a89c4b0d908fabab9f6a93dd39870a2b9941fae
Robin Deits <robin.deits@gmail.com>
2014-12-04 18:20:30 -0500

    generalize dynamic footstep plans

--- systems/robotInterfaces/@Biped/planWalkingZMP.m
+++ systems/robotInterfaces/@Biped/planWalkingZMP.m
@@ -1,20 +1,37 @@
 function walking_plan_data = planWalkingZMP(obj, x0, footstep_plan)
 % Construct a dynamic walking plan based on the ZMP formulation.
 % @param x0 the initial robot state vector
-% @param footstep_plan a FootstepPlan
+% @param footstep_plan a FootstepPlan or DynamicFootstepPlan
 
 path_handle = addpathTemporary(fullfile(getDrakePath(), 'examples', 'ZMP'));
-
-footstep_plan.sanity_check();
-for j = 1:length(footstep_plan.footsteps)
-  footstep_plan.footsteps(j).walking_params = applyDefaults(struct(footstep_plan.footsteps(j).walking_params),...
-    obj.default_walking_params);
-end
 
 nq = getNumPositions(obj);
 q0 = x0(1:nq);
 
-[zmptraj,link_constraints, support_times, supports] = planZMPTraj(obj, q0, footstep_plan.footsteps);
+if isa(footstep_plan, 'FootstepPlan')
+
+  footstep_plan.sanity_check();
+  for j = 1:length(footstep_plan.footsteps)
+    footstep_plan.footsteps(j).walking_params = applyDefaults(struct(footstep_plan.footsteps(j).walking_params),...
+      obj.default_walking_params);
+  end
+
+  dynamic_footstep_plan = planZMPTraj(obj, q0, footstep_plan.footsteps);
+
+  mu = zeros(length(footstep_plan.footsteps), 1);
+  for j = 1:length(footstep_plan.footsteps)
+    mu(j) = footstep_plan.footsteps(j).walking_params.mu;
+  end
+elseif isa(footstep_plan, 'DynamicFootstepPlan')
+  dynamic_footstep_plan = footstep_plan;
+  mu = 1;
+else
+  error('invalid plan type: %s\n', class(footstep_plan));
+end
+
+link_constraints = dynamic_footstep_plan.buildLinkConstraints();
+zmptraj = dynamic_footstep_plan.getZMPTraj();
+
 zmptraj = setOutputFrame(zmptraj,desiredZMP);
 
 kinsol = doKinematics(obj, q0);
@@ -27,9 +44,5 @@ zfeet = mean(foot_pos(3,:));
 options.com0 = com(1:2);
 [c,V,comtraj] = LinearInvertedPendulum.ZMPtrackerClosedForm(com(3)-zfeet,zmptraj,options);
 
-mu = zeros(length(footstep_plan.footsteps), 1);
-for j = 1:length(footstep_plan.footsteps)
-  mu(j) = footstep_plan.footsteps(j).walking_params.mu;
-end
-
-walking_plan_data = WalkingPlanData(q0, support_times, supports, link_constraints, zmptraj, V, c, comtraj, mu);
+[supports, support_times] = dynamic_footstep_plan.getSupports();
+walking_plan_data = WalkingPlanData(x0, support_times, supports, link_constraints, zmptraj, V, c, comtraj, mu);
