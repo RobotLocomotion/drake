2e2d0e88126adb544d8322ed3b0fa1c27dd5ec15
Russ Tedrake <russt@mit.edu>
2014-12-15 08:22:48 -0500

    hopefully fixes (very) low probability failure on unit test ```Matlab
                               < M A T L A B (R) >
                     Copyright 1984-2014 The MathWorks, Inc.
                       R2014a (8.3.0.532) 64-bit (glnxa64)
                                February 11, 2014
    
    To get started, type one of these: helpwin, helpdesk, or demo.
    For product information, visit www.mathworks.com.
    
    To reproduce this test use rng(2042137770,'twister')
    
    [Warning: Disabling autosave for simulink blocks (to avoid generating a lot
    of
    *.mdl.autosave files in your directory.  If you aren't a regular Simulink
    user
    and don't want this disabled, comment out this section in addpath_drake.
    To disable this warning in the future, add
      warning('off','Drake:DisablingSimulinkAutosave')
    to your matlab startup.m]
    [> In addpath_drake at 74
     In addpath_drake at 4]
    gradients generated by user and taylorvar don't match for output 3
    Error using valuecheck (line 78)
    Values don't match.
       Index       Value       Desired Value
      -------     --------    ---------------
        (1,6)  9399.488358     9399.488358
        (3,6)  9399.961561     9399.961561
        (1,8)  9399.488358     9399.488358
        (3,8)  9399.961561     9399.961561
    
    Error in geval (line 79)
               valuecheck(fdf{j},varargout{j},options.tol);
    
    Error in testAngularvel2rpydotMatrix>testGradient (line 14)
       [~, ~, ~] = geval(1, @angularvel2rpydotMatrix, rpy, option);
    
    Error in testAngularvel2rpydotMatrix (line 3)
    testGradient();
    
    Error in fevalPackageSafe (line 21)
     feval(f);
    
    <test_name>util/test/testAngularvel2rpydotMatrix</test_name>
    <error_id>Drake:ValueCheck</error_id> <error_message>Values don't match.
       Index       Value       Desired Value
      -------     --------    ---------------
        (1,6)  9399.488358     9399.488358
        (3,6)  9399.961561     9399.961561
        (1,8)  9399.488358     9399.488358
        (3,8)  9399.961561     9399.961561
    </error_message>
    
    ```

--- util/test/testAngularvel2rpydotMatrix.m
+++ util/test/testAngularvel2rpydotMatrix.m
@@ -5,14 +5,11 @@ testGradient();
 end
 
 function testGradient()
-n_tests = 100;
-test_number = 1;
+
 option.grad_method = {'taylorvar', 'user'};
-while test_number < n_tests
-  rpy = randn(3, 1);
-  if abs(cos(rpy(2))) > 1e-2
-    [~, ~, ~] = geval(1, @angularvel2rpydotMatrix, rpy, option);
-    test_number = test_number + 1;
-  end
+for i=1:100
+  rpy = uniformlyRandomNonsingularRPY();
+  [~, ~, ~] = geval(1, @angularvel2rpydotMatrix, rpy, option);
 end
+
 end
