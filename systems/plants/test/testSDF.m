a477b2591faf754f24d538cc20c6f05e7de42b37
Russ Tedrake <russt@mit.edu>
2014-12-16 08:24:59 -0500

    adds support for parsing models w GAZEBO_MODEL_PATH

--- systems/plants/test/testSDF.m
+++ systems/plants/test/testSDF.m
@@ -1,5 +1,7 @@
 function testSDF
 
+setenv('GAZEBO_MODEL_PATH','/Users/russt/tmp/gazebo_models'); % note: only works here because the path won't have been set somewhere else
+
 r = RigidBodyManipulator('FallingBrickBetterCollisionGeometry.urdf',struct('floating',true));
 r.addRobotFromSDF('/Users/russt/tmp/gazebo_models/cinder_block_2/model.sdf',[2;2;0]);
 
