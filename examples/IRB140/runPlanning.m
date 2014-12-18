db82e051a90c94eaee3b7ecf62e741d8c21f3614
peterkty <peterkty@gmail.com>
2014-12-18 12:41:22 -0500

    update irb140 planning code

--- examples/IRB140/runPlanning.m
+++ examples/IRB140/runPlanning.m
@@ -1,34 +1,29 @@
 function runPlanning()
 
-r=RigidBodyManipulator('irb_140.urdf',struct('floating',true));  
-
+r=RigidBodyManipulator();
+r = addRobotFromURDF(r,'irb_140.urdf',[-0.5, 0, 1.5]',[pi/2, pi/2, 0]');
 v=r.constructVisualizer();
 
 % default starting pose
-q0 = [-0.5, 0, 1.5, pi/2, pi/2, 0,0,0,0,0,0,0]'; 
+q0 = [0,0,0,0,0,0]'; 
 
 %pos_final = [-0.5, 0, 1.015]';
-pos_final = [-1.2, 0.1, 1]';
+pos_final = [-1.212+0.5, -0.1+0.5, 1.025]';
 gripper_idx = findLinkInd(r,'link_6');
 gripper_pt = [-0.04,0,0.1]';
-grasp_orient = angle2quat(0,pi,0)';
+grasp_orient = angle2quat(-1.3,1.1,0.8)';
 
 T = 1;
 N = 2;
 Allcons = cell(0,1);
 
-gripper_cons = WorldPositionConstraint(r, gripper_idx,gripper_pt,pos_final,pos_final,[T,T]);
-
-tol = 0;
-r_gripper_cons_orient = WorldQuatConstraint(r,gripper_idx,grasp_orient,tol,[0.1*T,T]);
-
-Allcons{end+1} = r_gripper_cons_orient;
-
+tol = 0.1;
+gripper_cons = WorldPositionConstraint(r, gripper_idx,gripper_pt,pos_final-tol,pos_final+tol,[T,T]);
 Allcons{end+1} = gripper_cons;
-for i=1:6
-  Allcons{end+1} = PostureChangeConstraint(r,i,0,0); % base
-end
 
+tol = 1;
+r_gripper_cons_orient = WorldQuatConstraint(r,gripper_idx,grasp_orient,tol,[0.1*T,T]);
+Allcons{end+1} = r_gripper_cons_orient;
 
 % compute seeds
 q_start_nom = q0;
@@ -45,7 +40,6 @@ t_vec,qtraj_guess,qtraj_guess,...
 Allcons{:});
 
 q_end = xtraj.eval(xtraj.tspan(end));
-
 % do visualize
 if snopt_info <= 10
   v.playback(xtraj);
