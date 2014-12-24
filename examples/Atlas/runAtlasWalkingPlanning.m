6ca5c9a033d4cafecb68e911cbb0857cb7d68fba
Russ Tedrake <russt@mit.edu>
2014-12-23 21:33:56 -0500

    adds options to atlas walking examples

--- examples/Atlas/runAtlasWalkingPlanning.m
+++ examples/Atlas/runAtlasWalkingPlanning.m
@@ -1,4 +1,4 @@
-function plan = runAtlasWalkingPlanning()
+function plan = runAtlasWalkingPlanning(options)
 % Demonstration of footstep and walking planning on Atlas. First, the footstep planning
 % step generates a set of reachable footstep poses. Next, we use those foot poses to
 % plan a trajectory of the Zero Moment Point (ZMP), from which we can derive a
@@ -6,6 +6,10 @@ function plan = runAtlasWalkingPlanning()
 % trajectory for the robot.
 % @retval plan a WalkingPlanData object including the zmp and com trajectories
 
+if nargin<1, options = struct(); end
+if ~isfield(options,'terrain'), options.terrain = RigidBodyFlatTerrain(); end
+if ~isfield(options,'navgoal'), options.navgoal = [1.5;0;0;0;0;0]; end
+if ~isfield(options,'initial_pose'), options.initial_pose = []; end
 
 checkDependency('lcmgl');
 
@@ -14,12 +18,22 @@ path_handle = addpathTemporary(fullfile(getDrakePath(), 'examples', 'ZMP'));
 % Set up the model
 load('data/atlas_fp.mat', 'xstar');
 x0 = xstar;
-r = Atlas('urdf/atlas_minimal_contact.urdf');
+if ~isfield(options,'initial_pose'), x0(1:6) = options.initial_pose; end
+
+r = Atlas('urdf/atlas_minimal_contact.urdf',options);
+x0 = r.resolveConstraints(x0);
 r = r.setInitialState(x0);
 
-% Plan footsteps to the goal
 q0 = x0(1:r.getNumPositions());
-goal_pos = struct('right', [1;0;0;0;0;0], 'left', [1;0.26;0;0;0;0]);
+
+R=rotz(options.navgoal(6));
+rfoot_navgoal = options.navgoal;
+lfoot_navgoal = options.navgoal;
+rfoot_navgoal(1:3) = rfoot_navgoal(1:3) + R*[0;-0.13;0];
+lfoot_navgoal(1:3) = lfoot_navgoal(1:3) + R*[0;0.13;0];
+
+% Plan footsteps to the goal
+goal_pos = struct('right', rfoot_navgoal, 'left', lfoot_navgoal);
 footstep_plan = r.planFootsteps(q0, goal_pos);
 
 % Show the result
@@ -31,8 +45,8 @@ walking_plan_data = r.planWalkingZMP(x0, footstep_plan);
 if isa(v, 'BotVisualizer')
   lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(), 'footstep_plan');
   footstep_plan.draw_lcmgl(lcmgl);
-  lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(), 'walking_plan');
-  walking_plan_data.draw_lcmgl(lcmgl);
+%  lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(), 'walking_plan');
+%  walking_plan_data.draw_lcmgl(lcmgl);
   lcmgl.switchBuffers();
 else
   figure(25)
--- examples/Atlas/runDRCPracticeTask.m
+++ examples/Atlas/runDRCPracticeTask.m
@@ -5,17 +5,15 @@ task_number=2; % consider taking the task number as an input if/when we load the
 clear gazeboModelPath;
 setenv('GAZEBO_MODEL_PATH',[fullfile(getDrakePath,'examples','Atlas','sdf')]); 
 
-% note: could speed this up by saving the heightmap terrain to disk and
-% loading it directly
-terrain = RigidBodyManipulator(['sdf/drc_practice_task_',num2str(task_number),'.world']);
-options.terrain = RigidBodyHeightMapTerrain.constructHeightMapFromRaycast(terrain,[],-3:.02:10,-3:.02:3,10);
-options.ignore_self_collisions = true;
-%options.use_bullet=false;
-r = Atlas('urdf/atlas_minimal_contact.urdf',options);
+if exist('practice_terrain.mat','file')
+  load practice_terrain.mat;
+else
+  terrain = RigidBodyManipulator(['sdf/drc_practice_task_',num2str(task_number),'.world']);
+  height_map = RigidBodyHeightMapTerrain.constructHeightMapFromRaycast(terrain,[],-3:.02:10,-3:.02:3,10);
+  save practice_terrain.mat height_map;
+end
 
-v = r.constructVisualizer();
-x0 = Point(r.getStateFrame());
-x0.base_x = 3.5;
-x0 = resolveConstraints(r,x0);
-v.draw(0,x0)
-simulate(cascade(r,v),[0 5],x0);
+options.terrain = height_map;  
+options.navgoal = [6.5;0;0;0;0;0];
+runAtlasWalkingPlanning(options);
+
