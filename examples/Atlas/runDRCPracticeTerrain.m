21e5a2bbbf68999ea08f9745dc570abe43440307
Robin Deits <robin.deits@gmail.com>
2015-01-14 09:35:38 -0500

    include terrain in repo

--- examples/Atlas/runDRCPracticeTerrain.m
+++ examples/Atlas/runDRCPracticeTerrain.m
@@ -5,18 +5,19 @@ task_number=2; % consider taking the task number as an input if/when we load the
 clear gazeboModelPath;
 setenv('GAZEBO_MODEL_PATH',[fullfile(getDrakePath,'examples','Atlas','sdf')]); 
 
-if exist('practice_terrain.mat','file')
-  load practice_terrain.mat;
+if exist('data/practice_terrain.mat','file')
+  load data/practice_terrain.mat;
 else
   terrain = RigidBodyManipulator(['sdf/drc_practice_task_',num2str(task_number),'.world']);
   height_map = RigidBodyHeightMapTerrain.constructHeightMapFromRaycast(terrain,[],-3:.02:10,-3:.02:3,10);
-  save practice_terrain.mat height_map;
+  save data/practice_terrain.mat height_map;
 end
 
 options.terrain = height_map;  
 options.navgoal = [6.5;0;0;0;0;0];
-options.initial_pose = [-3;0;0;0;0;0];
+% options.initial_pose = [-3;0;0;0;0;0];
 % options.initial_pose = [0;0;0;0;0;0]
+options.initial_pose = [-1;0;0;0;0;pi/4];
 
 while true
 %   profile on
