1dac665c0f0c0a5fefe4b6cea5d2f4f25e85df5d
Russ Tedrake <russt@mit.edu>
2015-01-17 16:33:37 -0500

    initial conditions that don't bounce off the end of the string.

--- examples/SimplePulleys/ChineseYoYo.m
+++ examples/SimplePulleys/ChineseYoYo.m
@@ -79,7 +79,7 @@ classdef ChineseYoYo < HybridDrakeSystem
       % barf
       x0 = Point(getStateFrame(obj));
       x0.m = 1;
-      x0.load_x = 1.5;
+      x0.load_x = 1;
       x0.load_z = 4.5;
       x0 = double(x0);
       x0(2:end) = resolveConstraints(obj.no_contact,x0(2:end));
