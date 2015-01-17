15d27877b358245e6d8f1726ba5d7df8f56dbb6e
Russ Tedrake <russt@mit.edu>
2015-01-17 16:19:02 -0500

    hybrid tension example (called chinese yo-yo already, even though it's not quite that yet) works.

--- examples/SimplePulleys/ChineseYoYo.m
+++ examples/SimplePulleys/ChineseYoYo.m
@@ -30,9 +30,48 @@ classdef ChineseYoYo < HybridDrakeSystem
       tf = AffineTransform(getOutputFrame(no_contact),getOutputFrame(obj),[zeros(1,nmy);eye(nmy)],[2;zeros(nmy,1)]);
       obj = addMode(obj,cascade(in_contact,tf));
       
+      obj = addTransition(obj,1,@collisionGuard,@collisionDynamics,false,true,2);
+      obj = addTransition(obj,2,@aerialGuard,@collisionDynamics,false,true,1);
+      
       obj.in_contact = in_contact;
       obj.no_contact = no_contact;
       obj = setStateFrame(obj,getOutputFrame(obj));
+    end
+    
+    function [g,dg] = collisionGuard(obj,t,x,u)
+      % todo: generalize this using line segements of the cable constraint?
+      g = x(3) - 4;  % x(3) = load_z
+      dg = [0,0,0,1,0,0,0];
+    end
+    
+    function [g,dg] = aerialGuard(obj,t,x,u)
+      g = 4 - x(3);
+      dg = -[0,0,0,1,0,0,0];
+    end
+    
+    function [xp,mode,status] = collisionDynamics(obj,mode,t,xm,u)
+      if (mode==1)
+        % then i have a collision
+        mode = 2;
+
+        q = xm(1:obj.no_contact.num_positions);
+        v = xm(obj.no_contact.num_positions+1:end);
+        H = manipulatorDynamics(obj.no_contact,q,v);
+        Hinv = inv(H);
+
+        [phi,J] = obj.in_contact.position_constraints{1}.eval(q);
+        lb = obj.in_contact.position_constraints{1}.lb;
+        ub = obj.in_contact.position_constraints{1}.ub;
+        
+        % solve zero post-transition velocities for all active constraints
+        J = J*vToqdot(obj.no_contact, q);
+        vp = (eye(obj.no_contact.num_velocities)-Hinv*J'*pinv(J*Hinv*J')*J)*v;
+        xp = [q;vp];
+      else % mode==2
+        mode = 1;
+        xp = xm;
+      end
+      status = 0;
     end
     
     function x0 = getInitialState(obj)
@@ -40,10 +79,11 @@ classdef ChineseYoYo < HybridDrakeSystem
       % barf
       x0 = Point(getStateFrame(obj));
       x0.m = 1;
-      x0.load_x = 0;
+      x0.load_x = 1.5;
       x0.load_z = 4.5;
       x0 = double(x0);
-   end
+      x0(2:end) = resolveConstraints(obj.no_contact,x0(2:end));
+    end
     
     function v = constructVisualizer(obj)
       v1 = constructVisualizer(obj.no_contact);
@@ -73,8 +113,9 @@ classdef ChineseYoYo < HybridDrakeSystem
       v = r.constructVisualizer();
       
       x0 = getInitialState(r);
-      ytraj = simulate(r,[0 1],x0);
-      v.playback(ytraj);
+      v.drawWrapper(0,x0);
+      ytraj = simulate(r,[0 3],x0);
+      v.playback(ytraj,struct('slider',true));
     end
   end
 end
