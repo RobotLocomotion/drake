27bb4fe174019c6a1b6b70147af46b31c51b9183
Robin Deits <robin.deits@gmail.com>
2015-01-23 16:34:40 -0500

    comments

--- systems/robotInterfaces/@Biped/sampleSwingTerrain.m
+++ systems/robotInterfaces/@Biped/sampleSwingTerrain.m
@@ -1,4 +1,13 @@
 function terrain_pts = sampleSwingTerrain(biped, step1, step2, contact_width, options)
+% Sample the terrain between two poses of the robot's foot and compute a terrain profile.
+% @param biped a Biped
+% @param step1 a Footstep, the original footstep location
+% @param step2 a Footstep, the final footstep location
+% @param contact_width the effective width of the foot, from biped.contactVolume()
+% @option nrho number of points to scan in the direction perpendicular to travel
+% @option nlambda number of points to scan in the direction of travel
+% @retval terrain_pts a 2xN matrix. The first row is distance along the line from step1 to step2;
+%                     the second row is the max terrain height across the sampled width at that distance
 
 if nargin < 5; options = struct(); end
 if ~isfield(options, 'nrho'); options.nrho = 10; end
