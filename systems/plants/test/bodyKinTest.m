4a8ded1a636e1ea7601811deead1d2b0d62a5760
Twan Koolen <koolen.twan@gmail.com>
2015-01-26 21:18:05 -0500

    Work on bodyKin. Passing the original bodyKinTest, but mex still needs to be implemented and tested.

--- systems/plants/test/bodyKinTest.m
+++ systems/plants/test/bodyKinTest.m
@@ -13,23 +13,39 @@ function bodyKinTest()
 end
 
 function [x,J,dJ] = bodyKinWrapper(q,r,body_ind,pts)
-  kinsol = r.doKinematics(q,true,false);
-  switch nargout
-    case 1
-      x = r.bodyKin(kinsol,body_ind,pts);
-    case 2
-      [x, ~, J] = r.bodyKin(kinsol,body_ind,pts);
-    case 3
-      [x, ~, J, ~, dJ] = r.bodyKin(kinsol,body_ind,pts);
+if r.use_new_kinsol
+  kinsol_options.use_mex = false;
+  if nargout > 2
+    kinsol_options.compute_gradients = true;
   end
+  kinsol = r.doKinematics(q, [], [], [], kinsol_options);
+else
+  kinsol = r.doKinematics(q,true,false);
+end
+switch nargout
+  case 1
+    x = r.bodyKin(kinsol,body_ind,pts);
+  case 2
+    [x, ~, J] = r.bodyKin(kinsol,body_ind,pts);
+  case 3
+    [x, ~, J, ~, dJ] = r.bodyKin(kinsol,body_ind,pts);
+end
 end
 
 function [P,dP] = bodyKinPWrapper(q,r,body_ind,pts)
-  kinsol = r.doKinematics(q,true,false);
-  switch nargout
-    case 1
-      [~, P] = r.bodyKin(kinsol,body_ind,pts);
-    case 2
-      [~, P, ~, dP] = r.bodyKin(kinsol,body_ind,pts);
+if r.use_new_kinsol
+  kinsol_options.use_mex = false;
+  if nargout > 1
+    kinsol_options.compute_gradients = true;
   end
+  kinsol = r.doKinematics(q, [], [], [], kinsol_options);
+else
+  kinsol = r.doKinematics(q,true,false);
+end
+switch nargout
+  case 1
+    [~, P] = r.bodyKin(kinsol,body_ind,pts);
+  case 2
+    [~, P, ~, dP] = r.bodyKin(kinsol,body_ind,pts);
+end
 end
