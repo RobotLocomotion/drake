43744c20c337a9865cb099227bab5d4cba807c6a
Robin Deits <robin.deits@gmail.com>
2015-01-27 22:35:05 -0500

    switch fastCross3 back to cross() (which is nearly as fast)

--- util/fastCross3.m
+++
@@ -1,7 +0,0 @@
-function X = fastCross3(U, V)
-% cross() is ludicrously slow. This is about 10x faster for 3-vectors, but does no
-% input checking. Test case in test/testFastCross3.m
-
-X = [U(2) * V(3) - U(3) * V(2);
-     U(3) * V(1) - U(1) * V(3);
-     U(1) * V(2) - U(2) * V(1)];
