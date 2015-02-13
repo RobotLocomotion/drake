1420e63e28ac5334661291f6e56dad6baa164366
hongkai <daihongkai@gmail.com>
2015-02-13 09:35:23 -0500

    remove redundant robotiq_hand_description

--- examples/robotiq_hand_description/meshes/s-model/visual/simplify_obj.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- # echo of all files in a directory
-
-for file in *.obj
-do
-  name=${file%%[.]*}
-  meshlabserver -i $file -o $name'_1.obj' -om vn -s simplify.mlx
-  #echo $name'.obj' 
-done
