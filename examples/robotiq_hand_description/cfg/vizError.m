f9c4e3e327dca9be63c5dc727c5ada2c6f7281ca
hongkai <daihongkai@gmail.com>
2015-02-13 09:35:23 -0500

    remove redundant robotiq_hand_description

--- examples/robotiq_hand_description/cfg/vizError.m
+++
@@ -1,5 +0,0 @@
-% vizualizer closes
-% error not present if 'package://robotiq_hand_description' removed
-
-plant = RigidBodyManipulator('s-model_articulated.urdf');
-viz = plant.constructVisualizer();
