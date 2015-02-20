e925abe688b29c17f7c8603722c385041a2dc0cc
Russ Tedrake <russt@mit.edu>
2015-02-19 21:34:59 -0500

    implemented (most of) parseLink

--- systems/plants/URDFRigidBodyManipulator.cpp
+++ systems/plants/URDFRigidBodyManipulator.cpp
@@ -177,6 +177,8 @@ void poseToTransform(const urdf::Pose& pose, Matrix4d& T)
           0, 0, 0, 1;
 }
 
+
+
 URDFRigidBodyManipulator::URDFRigidBodyManipulator(void)
 : 
   RigidBodyManipulator(0,0,1)
@@ -478,22 +480,176 @@ void setJointLimits(boost::shared_ptr<urdf::ModelInterface> urdf_model, boost::s
   }
 }
 
+void poseAttributesToTransform(TiXmlElement* node, Matrix4d& T)
+{
+  double x=0.0,y=0.0,z=0.0, roll=0.0,pitch=0.0,yaw=0.0;
+
+  const char* attr = node->Attribute("xyz");
+  if (attr) {
+    stringstream s(attr);
+    s >> x >> y >> z;
+  }
+
+  attr = node->Attribute("rpy");
+  if (attr) {
+    stringstream s(attr);
+    s >> roll >> pitch >> yaw;
+  }
+
+  T <<  cos(yaw)*cos(pitch), cos(yaw)*sin(pitch)*sin(roll)-sin(yaw)*cos(roll), cos(yaw)*sin(pitch)*cos(roll)+sin(yaw)*sin(roll), x,
+        sin(yaw)*cos(pitch), sin(yaw)*sin(pitch)*sin(roll)+cos(yaw)*cos(roll), sin(yaw)*sin(pitch)*cos(roll)-cos(yaw)*sin(roll), y,
+        -sin(pitch),         cos(pitch)*sin(roll),                             cos(pitch)*cos(roll),                             z,
+        0, 0, 0, 1;
+}
+
 bool parseInertial(RigidBody* body, TiXmlElement* node, URDFRigidBodyManipulator* model)
 {
+  Isometry3d T = Isometry3d::Identity();
+
+  TiXmlElement* origin = node->FirstChildElement("origin");
+  if (origin) poseAttributesToTransform(origin,T.matrix());
+
+  TiXmlElement* mass = node->FirstChildElement("mass");
+  if (mass) mass->Attribute("value",&(body->mass));
+
+  body->com << T(0,3), T(1,3), T(2,3), 1.0;
+
+  Matrix<double,TWIST_SIZE,TWIST_SIZE> I = Matrix<double,TWIST_SIZE,TWIST_SIZE>::Zero();
+  I.block(3,3,3,3) << body->mass*Matrix3d::Identity();
+
+  TiXmlElement* inertia = node->FirstChildElement("inertia");
+  if (inertia) {
+    inertia->Attribute("ixx",&I(0,0));
+    inertia->Attribute("ixy",&I(0,1)); I(1,0)=I(0,1);
+    inertia->Attribute("ixz",&I(0,2)); I(2,0)=I(0,2);
+    inertia->Attribute("iyy",&I(1,1));
+    inertia->Attribute("iyz",&I(1,2)); I(2,1)=I(1,2);
+    inertia->Attribute("izz",&I(2,2));
+  }
+
+  auto bodyI = transformSpatialInertia(T,static_cast<  Gradient<Isometry3d::MatrixType, Eigen::Dynamic>::type* >(NULL),I);
+  body->I = bodyI.value();
+
+  cout << "I = " << body->I << endl;
+
+  return true;
+}
+
+bool parseVisual(RigidBody* body, TiXmlElement* node, URDFRigidBodyManipulator* model)
+{
+  // todo:  consider implementing this, but I don't need it yet
+  return true;
+}
+
+bool parseCollision(int body_index, TiXmlElement* node, URDFRigidBodyManipulator* model)
+{
+  Isometry3d T = Isometry3d::Identity();
+  TiXmlElement* origin = node->FirstChildElement("origin");
+  if (origin) poseAttributesToTransform(origin,T.matrix());
+
+  bool create_collision_element(true);
+  const char* attr;
+  DrakeCollision::Shape shape = DrakeCollision::Shape::UNKNOWN;
+  vector<double> params;
+
+  TiXmlElement* shape_node;
+  if ((shape_node = node->FirstChildElement("box"))) {
+    shape = DrakeCollision::Shape::BOX;
+    double x=0,y=0,z=0;
+    attr = shape_node->Attribute("size");
+    if (attr) {
+      stringstream s(attr);
+      s >> x >> y >> z;
+    }
+    params.push_back(x);
+    params.push_back(y);
+    params.push_back(z);
+  } else if ((shape_node = node->FirstChildElement("sphere"))) {
+    shape = DrakeCollision::Shape::SPHERE;
+    double r=0;
+    attr = shape_node->Attribute("radius");
+    if (attr) {
+      stringstream s(attr);
+      s >> r;
+    }
+    params.push_back(r);
+  } else if ((shape_node = node->FirstChildElement("cylinder"))) {
+    shape = DrakeCollision::CYLINDER;
+    double r=0,l=0;
+    attr = shape_node->Attribute("radius");
+    if (attr) {
+      stringstream s(attr);
+      s >> r;
+    }
+    attr = shape_node->Attribute("length");
+    if (attr) {
+      stringstream s(attr);
+      s >> l;
+    }
+    params.push_back(r);
+    params.push_back(l);
+  } else if ((shape_node = node->FirstChildElement("mesh"))) {
+    shape = DrakeCollision::Shape::MESH;
+    cerr << "Warning: mesh collision elements will be ignored (until I re-implement the logic below sans boost)" << endl;
+    create_collision_element = false;
+    /*
+    boost::shared_ptr<urdf::Mesh> mesh(boost::dynamic_pointer_cast<urdf::Mesh>(cptr->geometry));
+    boost::filesystem::path mesh_filename(root_dir);
+    boost::regex package(".*package://.*");
+    if (!boost::regex_match(mesh->filename, package)) {
+      mesh_filename /= mesh->filename;
+      readObjFile(mesh_filename,params);
+    } else {
+      create_collision_element = false;
+      if (print_mesh_package_warning) {
+        cerr << "Warning: The robot '" << _urdf_model->getName()
+             << "' contains collision geometries that specify mesh "
+             << "files with the 'package://' syntax, which "
+             << "URDFRigidBodyManipulator does not support. These "
+             << "collision geometries will be ignored." << endl;
+        print_mesh_package_warning = false;
+      }
+    }
+    */
+  } else {
+    cerr << "ERROR: Link " << model->bodies[body_index]->linkname << " has a collision element with an unknown type" << endl;
+    return false;
+  }
+
+  if (create_collision_element){
+    model->addCollisionElement(body_index,T.matrix(),shape,params);
+  }
+
   return true;
 }
 
 bool parseLink(URDFRigidBodyManipulator* model, TiXmlElement* node, int robotnum)
 {
-  if (strcmp(node->Attribute("drake_ignore"),"true")==0) return true;
+  const char* attr = node->Attribute("drake_ignore");
+  if (attr && strcmp(attr,"true")==0) return true;
   RigidBody* body = new RigidBody();
 
-  body->linkname = node->Attribute("name");
+  attr = node->Attribute("name");
+  if (!attr) {
+    cerr << "ERROR: link tag is missing name attribute" << endl;
+    return false;
+  }
+  body->linkname = attr;
 
   TiXmlElement* inertial_node = node->FirstChildElement("inertial");
   if (inertial_node) if (!parseInertial(body,inertial_node,model)) return false;
 
+  for (TiXmlElement* visual_node = node->FirstChildElement("visual"); visual_node; visual_node = visual_node->NextSiblingElement("visual")) {
+    if (!parseVisual(body,visual_node,model)) return false;
+  }
+
   model->bodies.push_back(std::unique_ptr<RigidBody>(body));
+  int body_index = model->bodies.size();
+
+  for (TiXmlElement* collision_node = node->FirstChildElement("collision"); collision_node; collision_node = collision_node->NextSiblingElement("collision")) {
+    if (!parseCollision(body_index,collision_node,model)) return false;
+  }
+
   return true;
 }
 
