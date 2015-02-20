5626d735f5b18ae1f1e6d4cb9a798b3c6f4e1ae0
Russ Tedrake <russt@mit.edu>
2015-02-19 19:37:18 -0500

    rename tests to conform to code style conventions.

--- systems/plants/test/urdfCollisionTest.cpp
+++ systems/plants/test/urdfCollisionTest.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	if (argc<2) {
-		cerr << "Usage: urdf_kin_test urdf_filename" << endl;
+		cerr << "Usage: urdfCollisionTest urdf_filename" << endl;
 		exit(-1);
 	}
   URDFRigidBodyManipulator* model = loadURDFfromFile(argv[1]);
@@ -18,7 +18,7 @@ int main(int argc, char* argv[])
   	cerr << "ERROR: Failed to load model from " << argv[1] << endl;
   	return -1;
   }
-  
+
   // run kinematics with second derivatives 100 times
   VectorXd q = VectorXd::Zero(model->num_dof);
   int i;
@@ -27,25 +27,33 @@ int main(int argc, char* argv[])
   	for (i=0; i<model->num_dof; i++)
   		sscanf(argv[2+i],"%lf",&q(i));
   }
-  
+
 // for (i=0; i<model->num_dof; i++)
 // 	 q(i)=(double)rand() / RAND_MAX;
     model->doKinematics(q,false);
 //  }
-  
-  const Vector4d zero(0,0,0,1);
-//  Vector3d pt;
-  Matrix<double,6,1> pt;
-  
-  for (i=0; i<model->num_bodies; i++) {
-    model->forwardKin(i,zero,1,pt);
-//    cout << i << ": forward kin: " << model->bodies[i].linkname << " is at " << pt.transpose() << endl;
-    cout << model->bodies[i]->linkname << " ";
-    for (int j=0; j<pt.size(); j++)
-    	cout << pt(j) << " ";
-    cout << endl;
-  } 
-  
+
+  VectorXd phi;
+  MatrixXd normal, xA, xB;
+  vector<int> bodyA_idx, bodyB_idx, bodies_idx;
+
+  model->collisionDetect(phi,normal,xA,xB,bodyA_idx,bodyB_idx,bodies_idx);
+
+  for (int j=0; j<phi.rows(); ++j) {
+    cout << phi(j) << " ";
+    for (int i=0; i<3; ++i) {
+      cout << normal(i,j) << " ";
+    }
+    for (int i=0; i<3; ++i) {
+      cout << xA(i,j) << " ";
+    }
+    for (int i=0; i<3; ++i) {
+      cout << xB(i,j) << " ";
+    }
+    cout << model->bodies[bodyA_idx.at(j)]->linkname << " ";
+    cout << model->bodies[bodyB_idx.at(j)]->linkname << endl;
+  }
+
   delete model;
   return 0;
 }
