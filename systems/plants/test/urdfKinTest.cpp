3c1cb46555b697ee0ebdd7b25572117aa8cee240
Russ Tedrake <russt@mit.edu>
2015-02-19 19:37:18 -0500

    rename tests to conform to code style conventions.

--- systems/plants/test/urdfKinTest.cpp
+++ systems/plants/test/urdfKinTest.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	if (argc<2) {
-		cerr << "Usage: urdf_kin_test urdf_filename" << endl;
+		cerr << "Usage: urdfKinTest urdf_filename" << endl;
 		exit(-1);
 	}
   URDFRigidBodyManipulator* model = loadURDFfromFile(argv[1]);
@@ -18,7 +18,7 @@ int main(int argc, char* argv[])
   	cerr << "ERROR: Failed to load model from " << argv[1] << endl;
   	return -1;
   }
-  
+
   // run kinematics with second derivatives 100 times
   VectorXd q = VectorXd::Zero(model->num_dof);
   int i;
@@ -27,16 +27,16 @@ int main(int argc, char* argv[])
   	for (i=0; i<model->num_dof; i++)
   		sscanf(argv[2+i],"%lf",&q(i));
   }
-  
+
 // for (i=0; i<model->num_dof; i++)
 // 	 q(i)=(double)rand() / RAND_MAX;
     model->doKinematics(q.data(),false);
 //  }
-  
+
   const Vector4d zero(0,0,0,1);
 //  Vector3d pt;
   Matrix<double,6,1> pt;
-  
+
   for (i=0; i<model->num_bodies; i++) {
     model->forwardKin(i,zero,1,pt);
 //    cout << i << ": forward kin: " << model->bodies[i].linkname << " is at " << pt.transpose() << endl;
@@ -44,8 +44,8 @@ int main(int argc, char* argv[])
     for (int j=0; j<pt.size(); j++)
     	cout << pt(j) << " ";
     cout << endl;
-  } 
-  
+  }
+
   delete model;
   return 0;
 }
