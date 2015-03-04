a2d9f34a5b8af78010c0202463ab02e08aa22abe
Andrés Valenzuela <avalenzu@mit.edu>
2015-03-04 12:48:02 -0500

    Try removing include guard from DLLEXPORT_drakeCollision

--- systems/plants/collision/DLLEXPORT_drakeCollision.h
+++ systems/plants/collision/DLLEXPORT_drakeCollision.h
@@ -1,6 +1,4 @@
-#ifndef __DLLEXPORT_drakeCollision_H__
-#define __DLLEXPORT_drakeCollision_H__
-
+#undef DLLEXPORT_drakeCollision
 #if defined(WIN32) || defined(WIN64)
   #if defined(drakeCollision_EXPORTS)
     #define DLLEXPORT_drakeCollision __declspec( dllexport )
@@ -9,6 +7,4 @@
   #endif
 #else
     #define DLLEXPORT_drakeCollision
-#endif
-
 #endif
