da6a792c6e01849970f4f8c8cb491e1705c77210
psiorx <psiorx@gmail.com>
2015-03-05 16:09:41 -0500

    removed joint limits, fixed damping threshold

--- examples/Atlas/urdf/robotiq_benchmark.m
+++ examples/Atlas/urdf/robotiq_benchmark.m
@@ -3,7 +3,7 @@ function robotiq_benchmark
 sim_time_full = timeit(@()robotiq_sim('robotiq.urdf'))
 sim_time_limp = timeit(@()robotiq_sim('robotiq_limp.urdf'))
 sim_time_springs = timeit(@()robotiq_sim('robotiq_springs.urdf'))
-sim_time_friction_springs = timeit(@()robotiq_sim('robotiq_friction_springs.urdf'))
+sim_time_friction_springs = timeit(@()robotiq_sim('robotiq_springs_damping.urdf'))
 
 end
 
