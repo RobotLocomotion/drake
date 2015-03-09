f0f576c6cf2735e7269481c19309a71f179732b3
Robin Deits <robin.deits@gmail.com>
2015-03-09 13:50:01 -0400

    don't include megaquit

--- util/megaquit.m
+++
@@ -1,4 +0,0 @@
-%NOTEST
-% Sometimes you will not be able to quit matlab, for example because you have a simulink system running in the background. This script should be enough to actually let you quit. 
-megaclear
-quit
