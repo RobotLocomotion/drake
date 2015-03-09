690832eef3c650cf8e82ad04620d95b2726b00cb
Robin Deits <robin.deits@gmail.com>
2015-03-09 13:13:35 -0400

    kinematic -> positionControl

--- examples/Atlas/+atlasParams/PositionControl.m
+++ examples/Atlas/+atlasParams/PositionControl.m
@@ -1,6 +1,6 @@
-classdef Kinematic < atlasParams.Base
+classdef PositionControl < atlasParams.Base
   methods
-    function obj = Kinematic(r)
+    function obj = PositionControl(r)
       typecheck(r, 'Atlas');
       obj = obj@atlasParams.Base(r);
       obj.whole_body.w_qdd = 0.001*ones(r.getNumVelocities(), 1);
