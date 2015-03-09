46b762c6b60f9bd72c13e934e2f52ada9ca657ae
Robin Deits <robin.deits@gmail.com>
2015-03-09 10:17:33 -0400

    lcmgl off by default

--- examples/Atlas/+atlasControllers/bodyMotionControl.m
+++ examples/Atlas/+atlasControllers/bodyMotionControl.m
@@ -9,15 +9,18 @@ function body_vdot = bodyMotionControl(robot, x, body_id, body_pose_des, body_v_
 % @param body_v_des the desired 6-DOF velocity
 % @param body_vdot_des the desired 6-DOF acceleration
 % @param params an element of the body_motion field of an atlasParams.* object
+DEBUG = false;
 
 nq = robot.getNumPositions();
 q = x(1:nq);
 qd = x(nq+1:end);
 kinsol = doKinematics(robot,q,false,true,qd);
 
-lcmgl = LCMGLClient(sprintf('link_%d_desired', body_id));
-lcmgl.sphere(body_pose_des(1:3), 0.03, 20, 20);
-lcmgl.switchBuffers();
+if DEBUG
+  lcmgl = LCMGLClient(sprintf('link_%d_desired', body_id));
+  lcmgl.sphere(body_pose_des(1:3), 0.03, 20, 20);
+  lcmgl.switchBuffers();
+end
 
 % TODO: this should be updated to use quaternions/spatial velocity
 [p,J] = forwardKin(robot,kinsol,body_id,[0;0;0],1); 
