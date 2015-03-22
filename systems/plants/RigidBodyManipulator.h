d0157f307a0c52d0101b612830a995bdad9c7713
Achim Stein <steinachim@gmx.de>
2015-03-22 19:25:35 +0100

    added missing default parameter

--- systems/plants/RigidBodyManipulator.h
+++ systems/plants/RigidBodyManipulator.h
@@ -264,7 +264,7 @@ public:
   int findLinkId(std::string linkname, int robot = -1);
   //@param robot   the index of the robot. robot = -1 means to look at all the robots
   
-  int findJointId(std::string jointname, int robot);
+  int findJointId(std::string jointname, int robot = -1);
   //@param robot   the index of the robot. robot = -1 means to look at all the robots
 
   std::string getBodyOrFrameName(int body_or_frame_id);
