cb539b55b39518d392ccf8875a294ebfaee35f10
hongkai-dai <daihongkai@gmail.com>
2015-03-26 20:29:45 -0400

    add a test case for Atlas manipulation, it rotate its hand by 180 degrees, this is intended to check the singularity in bodyMotionPD

--- examples/Atlas/+atlasParams/Manip.m
+++ examples/Atlas/+atlasParams/Manip.m
@@ -2,6 +2,8 @@ classdef Manip < atlasParams.Standing
   methods 
     function obj = Manip(r)
       obj = obj@atlasParams.Standing(r);
+      obj.body_motion(r.findLinkId('r_hand')).weight = 0.001;
+      obj.body_motion(r.findLinkId('l_hand')).weight = 0.001;
     end
   end
 end
