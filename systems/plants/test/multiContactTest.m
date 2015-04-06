32c8f96da58d21a61a2a697ff1a58ff2f5b1f662
John Carter <psiorx@gmail.com>
2015-04-06 17:41:47 -0400

    updated unit test to test generation of contact points for fast moving objects

--- systems/plants/test/multiContactTest.m
+++ systems/plants/test/multiContactTest.m
@@ -11,25 +11,27 @@ function multiContactTest
   p = TimeSteppingRigidBodyManipulator([], dt, options);
     
   padding = 0.04;
-  q0 = [0;0;0.5+padding;0;0;0];
-  q1 = [0;0.0;1.5+2*padding;0;0;pi/2];
-  q2 = [0;0.0;2.5+3*padding;0;0;0];
+  q0 = [0;0;0.5+25*padding;0;0;0];
+  q1 = [0;0;1.5+50*padding;0;0;pi/2];
+  q2 = [0;0.75;2.5+100*padding;0;0;0];
+  q3 = [0;-0.75;2.5+100*padding;0;0;0];
   options.floating = true;
   options.terrain = [];
   
-  p = p.addRobotFromURDF('FallingBrick.urdf', [], [], options);
-  p = p.addRobotFromURDF('FallingBrick.urdf', [], [], options);
-  p = p.addRobotFromURDF('FallingBrick.urdf', [], [], options);
+  for i = 1:4
+    p = p.addRobotFromURDF('FallingBrick.urdf', [], [], options);
+  end
   
-  xtraj = p.simulate([0 3], [q0;q1;q2;0*q0;0*q1;0*q2])
+  xtraj = p.simulate([0 3], [q0;q1;q2;q3;0*q0;0*q1;0*q2;0*q3]);
   v = p.constructVisualizer();
   for t = xtraj.getBreaks
     xt = xtraj.eval(t);
-    qt = xt(1:3*size(q0,1));
+    qt = xt(1:4*size(q0,1));
     kinsol = p.doKinematics(qt);
     [phi, normal, xA, xB, idxa, idxb] = p.collisionDetect(kinsol, true);
     v.draw(t,qt);
     debugLCMGL(p, v, kinsol, phi, normal, xA, xB, idxa, idxb)
+    pause(dt)
   end
   
 function debugLCMGL(r,v,kinsol,phi,normal,xA,xB,idxA,idxB)
