17be5a1c0f250b8aebd2028842069ae9171bc19d
Robin Deits <robin.deits@gmail.com>
2015-04-06 22:10:04 -0400

    update solver codegen

--- solvers/qpSpline/matrix_support.c
+++ solvers/qpSpline/matrix_support.c
@@ -1,4 +1,4 @@
-/* Produced by CVXGEN, 2015-04-06 13:38:00 -0400.  */
+/* Produced by CVXGEN, 2015-04-06 22:08:41 -0400.  */
 /* CVXGEN is Copyright (C) 2006-2012 Jacob Mattingley, jem@cvxgen.com. */
 /* The code in this file is Copyright (C) 2006-2012 Jacob Mattingley. */
 /* CVXGEN, or solvers produced by CVXGEN, cannot be used for commercial */
