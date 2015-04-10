d6c52be38732b80526742415536d0f9ff686db23
hongkai-dai <daihongkai@gmail.com>
2015-04-10 10:20:01 -0400

    change the damping ratio in Manip.m for left and right hands

--- examples/Atlas/+atlasParams/Manip.m
+++ examples/Atlas/+atlasParams/Manip.m
@@ -2,8 +2,13 @@ classdef Manip < atlasParams.Standing
   methods 
     function obj = Manip(r)
       obj = obj@atlasParams.Standing(r);
-      obj.body_motion(r.findLinkId('l_hand')).weight = 0.001;
-      obj.body_motion(r.findLinkId('r_hand')).weight = 0.001;
+      l_hand = r.findLinkId('l_hand');
+      r_hand = r.findLinkId('r_hand');
+      obj.body_motion(l_hand).weight = 0.001;
+      obj.body_motion(r_hand).weight = 0.001;
+      obj.body_motion(l_hand).damping_ratio = 0.8;
+      obj.body_motion(r_hand).damping_ratio = 0.8;
+      obj = obj.updateKd();
     end
   end
 end
