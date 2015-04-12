a5074f381d07d1633d4ebc4515d2d20f96af5a25
Russ Tedrake <russt@mit.edu>
2015-04-12 06:28:26 -0400

    resolves failing unit test

--- systems/plants/test/testHybridMode.m
+++ systems/plants/test/testHybridMode.m
@@ -1,13 +1,13 @@
 function testHybridMode
 
 p = RigidBodyManipulator(fullfile(getDrakePath,'examples','Acrobot','Acrobot.urdf'));
-p = setJointLimits(p,[-inf;-1.5],[inf;1.5]);
+p = setJointLimits(p,[-inf;1.5],[inf;1.5]);
 p = compile(p);
 v = p.constructVisualizer();
 p = HybridRigidBodyMode(p);
 % simulates it with both lb and ub on the joint limit set to active (so
 % thetaddot(2) will = 0, but currently the initial velocity will not be dissipated
-x0 = [0;2;0;pi/4;0;5]+3*[zeros(2,1);randn(4,1)];
+x0 = [0;2;0;pi/4;0;0]+3*[zeros(2,1);randn(3,1);0];
 x0 = resolveConstraints(p,x0);
 ytraj = simulate(p,[0,4],x0);
 v.playback(ytraj);
