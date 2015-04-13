6cf775dfc2f0008e57d55733672fd9637ee7bfd9
Robin Deits <robin.deits@gmail.com>
2015-04-13 17:29:26 -0400

    complete model of drc testbed blocks task

--- examples/Atlas/dev/runAtlasWalkingTiltedBlocks.m
+++ examples/Atlas/dev/runAtlasWalkingTiltedBlocks.m
@@ -51,16 +51,23 @@ box_tops = [0.4, 0, 0.2, 0;
             0.4+0.4*2, 0, 0.2, pi;
             0.4+0.4*3, 0, 0.2, -pi/2;
             0.4+0.4*4, 0, 0.35, 0;
+            0.4+0.4*5, 0, 0.35, pi/2;
+            0.4+0.4*6, 0, 0.2, pi;
             0.4, -0.4, 0.2, -pi/2;
             0.4+0.4, -0.4, 0.2, 0;
             0.4+0.4*2, -0.4, 0.2, pi/2;
             0.4+0.4*3, -0.4, 0.35, pi;
-            0.4+0.4*4, -0.4, 0.5, -pi/2]';
+            0.4+0.4*4, -0.4, 0.5, -pi/2;
+            0.4+0.4*5, -0.4, 0.35, 0;
+            0.4+0.4*6, -0.4, 0.2, pi/2]';
 
 safe_regions = iris.TerrainRegion.empty();
 [A, b] = poly2lincon([-1, 0.05, 0.05, -1], [-1, -1, 1, 1]);
 [A, b] = convert_to_cspace(A, b);
 safe_regions(1) = iris.TerrainRegion(A, b, [], [], [0;0;0], [0;0;1]);
+[A, b] = poly2lincon(max(box_tops(1,:))+[0.35, 1, 1, 0.35], [-1, -1, 1, 1]);
+[A, b] = convert_to_cspace(A, b);
+safe_regions(2) = iris.TerrainRegion(A, b, [], [], [0;0;0], [0;0;1]);
 
 for j = 1:size(box_tops, 2)
   rpy = [0;-15*pi/180;box_tops(4,j)];
@@ -74,19 +81,19 @@ for j = 1:size(box_tops, 2)
   safe_regions(end+1) = iris.TerrainRegion(A, b, [], [], box_tops(1:3,j), normal);
 end
 r = r.compile();
-height_map = RigidBodyHeightMapTerrain.constructHeightMapFromRaycast(r,x0(1:nq),-1:.015:3,-1:.015:1,10);
+height_map = RigidBodyHeightMapTerrain.constructHeightMapFromRaycast(r,x0(1:nq),-1:.015:4,-1:.015:1,10);
 r = r.setTerrain(height_map).compile();
 
 v = r.constructVisualizer();
 v.display_dt = 0.01;
 
-footstep_plan = r.planFootsteps(x0(1:nq), struct('right',[max(box_tops(1,:));-0.13;0;0;0;0],...
-                                                 'left', [max(box_tops(1,:));0.13;0;0;0;0]),...
+footstep_plan = r.planFootsteps(x0(1:nq), struct('right',[max(box_tops(1,:))+0.4;-0.13;0;0;0;0],...
+                                                 'left', [max(box_tops(1,:))+0.4;0.13;0;0;0;0]),...
                                 safe_regions,...
-                                struct('step_params', struct('max_forward_step', 0.4,...
+                                struct('step_params', struct('max_forward_step', 0.35,...
                                                              'nom_forward_step', 0.05,...
                                                              'nom_upward_step', 0.3,...
-                                                             'max_num_steps', 14)));
+                                                             'max_num_steps', 20)));
 lcmgl = LCMGLClient('footsteps');
 footstep_plan.draw_lcmgl(lcmgl);
 lcmgl.switchBuffers();
