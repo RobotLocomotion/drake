31423379f806b6e7f758d87a9467a6828017ee1c
Robin Deits <robin.deits@gmail.com>
2015-04-14 18:23:11 -0400

    unit test for turning

---
+++ examples/Atlas/test/runAtlasTurning.m
@@ -0,0 +1,7 @@
+function runAtlasTurning()
+
+path_handle = addpathTemporary(fullfile(getDrakePath,'examples','Atlas'));
+runAtlasWalkingSplit([], struct('initial_pose', [1;0;0;0;0;pi/2],...
+                                'navgoal', [0;-1;0;0;0;-pi/2-pi/16],...
+                                'max_num_steps', 15));
+end
