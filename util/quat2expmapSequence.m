a78098a326eb81c5eee3899150d49742980bc618
hongkai-dai <daihongkai@gmail.com>
2015-04-16 13:13:54 -0400

    turn down the damping gain in Manip params, add a function to unwrap exponential maps

--- util/quat2expmapSequence.m
+++ util/quat2expmapSequence.m
@@ -1,15 +1,19 @@
 function [w,wdot] = quat2expmapSequence(quat,quat_dot)
+% Given a sequence of quaternions, convert them to exponential map, and
+% then unwrap the exponential map such that the adjacent exponential maps
+% are close to each other
 N = size(quat,2);
 assert(all(size(quat_dot)==[4,N]));
 w = zeros(3,N);
 wdot = zeros(3,N);
 for i = 1:N
-  if(i>1)
-    flip = sign(quat(:,i)'*quat(:,i-1));
-    quat(:,i) = flip*quat(:,i);
-    quat_dot(:,i) = flip*quat_dot(:,i);
-  end
   [w(:,i),dw] = quat2expmapmex(quat(:,i));
   wdot(:,i) = dw*quat_dot(:,i);
+  if(i>1)
+    [w(:,i),flip_flag] = unwrapExpmap(w(:,i-1),w(:,i));
+    if(flip_flag)
+      wdot(:,i) = -wdot(:,i);
+    end
+  end
 end
 end
