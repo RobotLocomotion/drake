998116ee6f8b546ad2337c15c3c3a24bc4ae935a
hongkai-dai <daihongkai@gmail.com>
2015-04-22 19:53:03 -0400

    fix the build error in testDrakeGeometryUtil, valuecheck is in testUtil only

--- util/test/testDrakeGeometryUtil.cpp
+++ util/test/testDrakeGeometryUtil.cpp
@@ -1,7 +1,6 @@
 #include <Eigen/Core>
 #include "drakeGeometryUtil.h"
 #include "testUtil.h"
-#include "drakeUtil.h" //Need this only because we need the valuecehck in drakeUtil, which uses isZero rather than isApprox. The long term solution is to move drakeUtil.valuecheck to testUtil
 #include <iostream>
 #include <cmath>
 
@@ -234,7 +233,7 @@ void testExpmap2quat(const Vector4d &quat)
   std::string error_msg;
   Matrix3d expmap_back = expmap.gradient().value()*quat_back.gradient().value();
   Matrix3d identity = Matrix3d::Identity();
-  valuecheck<Eigen::Matrix3d,Eigen::Matrix3d>(expmap_back,identity,1E-10,error_msg);
+  valuecheck(expmap_back,identity,1E-10);
 }
 
 int main(int argc, char **argv)
