f4364d1b037999592605b2c5550960456e361710
Twan Koolen <koolen.twan@gmail.com>
2015-04-22 15:25:28 -0400

    fix valuecheck call.

--- util/test/testDrakeGeometryUtil.cpp
+++ util/test/testDrakeGeometryUtil.cpp
@@ -229,7 +229,7 @@ void testExpmap2quat(const Vector4d &quat)
 {
   auto expmap = quat2expmap(quat,1);
   auto quat_back  = expmap2quat(expmap.value(),2);
-  valuecheck(std::abs(quat.transpose()*quat_back.value()),1);
+  valuecheck(std::abs(quat.transpose() * quat_back.value()), 1.0, 1e-8);
   Matrix3d expmap_back = expmap.gradient().value()*quat_back.gradient().value();
   Matrix3d identity = Matrix3d::Identity();
   valuecheck(expmap_back,identity,1E-10);
