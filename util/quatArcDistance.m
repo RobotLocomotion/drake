eff77a4b817132cf11d65289c3a8f1470f94f762
manuelli <lucas.t.manuelli@gmail.com>
2015-04-29 10:14:35 -0400

    renamed quaternion distance metric on rotations

--- util/quatArcDistance.m
+++ util/quatArcDistance.m
@@ -1,4 +1,4 @@
-function theta = quatRotationDistance(quat1,quat2)
+function theta = quatArcDistance(quat1,quat2)
 	theta = acos(sum(bsxfun(@times,quat1,quat2),1));
   if theta > pi/2
     theta = pi - theta;
