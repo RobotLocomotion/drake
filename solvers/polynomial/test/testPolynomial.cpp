84a0934f584b209f94fb424dec1e9d3809df0e7a
Robin Deits <robin.deits@gmail.com>
2015-05-02 14:36:24 -0400

    remove a spurious difference

--- solvers/polynomial/test/testPolynomial.cpp
+++ solvers/polynomial/test/testPolynomial.cpp
@@ -74,7 +74,7 @@ void testRoots() {
     VectorXd coeffs = VectorXd::Random(int_distribution(generator));
     Polynomial<CoefficientType> poly(coeffs);
     auto roots = poly.roots();
-    valuecheck(roots.rows(), poly.getDegree());
+    valuecheck(roots.rows(), poly.getDegree(), 1e-8);
     for (int i = 0; i < roots.size(); i++) {
       auto value = poly.value(roots[i]);
       valuecheck(std::abs(value), 0.0, 1e-8);
