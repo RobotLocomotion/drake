8a5c5702e75f36fd615724795807035f7b7c2243
Twan Koolen <koolen.twan@gmail.com>
2015-05-01 21:05:29 -0400

    Make fields private in AtlasPlanEval.

--- examples/Atlas/+atlasControllers/AtlasPlanEval.m
+++ examples/Atlas/+atlasControllers/AtlasPlanEval.m
@@ -2,7 +2,7 @@ classdef AtlasPlanEval < PlanEval
 % A PlanEval which includes some Atlas-specific tweaks. Specifically, it
 % provides a cache of useful robot properties which may be expensive to look
 % up at runtime.
-  properties  
+  properties (Access = private)
     robot_property_cache
     robot
   end
