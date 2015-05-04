bb50095287906ed61df6a1748d9215b2496980bc
Twan Koolen <koolen.twan@gmail.com>
2015-05-03 21:09:53 -0400

    update and rename test.

--- examples/Atlas/test/testQPLocomotionPlanCPPWrapper.m
+++ examples/Atlas/test/testQPLocomotionPlanCPPWrapper.m
@@ -1,4 +1,4 @@
-function testQPLocomotionPlanSettings()
+function testQPLocomotionPlanCPPWrapper()
 path = addpathTemporary(fullfile(getDrakePath,'examples','Atlas'));
 
 robot_options = struct(...
@@ -20,14 +20,24 @@ r = Atlas(fullfile(getDrakePath,'examples','Atlas','urdf','atlas_minimal_contact
 load('fromBipedFootAndZMPKnots.mat');
 settings = QPLocomotionPlanSettings.fromBipedFootAndZMPKnots(foot_motion_data, zmp_knots, r, x0, options);
 settings.untracked_joint_inds = 20;
-qp_locomotion_plan_ptr = constructQPLocomotionPlanmex(r.getManipulator().mex_model_ptr, settings, 'qp_controller_input');
 
-t_global = 3;
-q = randn(r.getNumPositions(), 1);
-v = randn(r.getNumVelocities(), 1);
-contact_force_detected = rand(r.getNumBodies(), 1) > 0.5;
-for i = 1 : 100
-  qpLocomotionPlanPublishQPControllerInputmex(qp_locomotion_plan_ptr, t_global, q, v, contact_force_detected);
-  pause(0.01)
+plan = QPLocomotionPlanCPPWrapper(settings);
+valuecheck(plan.start_time, []);
+
+t_globals = linspace(3, 4, 100);
+for t_global = t_globals
+  q = randn(r.getNumPositions(), 1);
+  v = randn(r.getNumVelocities(), 1);
+  contact_force_detected = rand(r.getNumBodies(), 1) > 0.5;
+  x = [q; v];
+  data = plan.getQPControllerInput(t_global, x, contact_force_detected);
+  valuecheck(plan.start_time, t_globals(1), 1e-10);
+  typecheck(data, 'char');
 end
+
+next_plan = plan.getSuccessor();
+valuecheck(next_plan.duration(), inf);
+[~] = plan.getQPControllerInput(t_global, x, contact_force_detected);
+valuecheck(next_plan.start_time, plan.start_time, 1e-10);
+valuecheck(next_plan.duration, plan.duration, 1e-10);
 end
