23392c8aa41d1d10c528050288f6d13bdb6410b6
Robin Deits <robin.deits@gmail.com>
2015-05-15 13:40:10 -0400

    more tests

--- util/test/testConvexHull.cpp
+++ util/test/testConvexHull.cpp
@@ -6,20 +6,20 @@ using namespace Eigen;
 
 int testConvexHull() {
   Matrix<double, 2, Dynamic> pts(2, 4);
-  pts << 1, 2, 3, 2,
-         2, 1, 2, 3;
+  pts << 1.0, 2.0, 3.0, 2.0,
+         2.0, 1.0, 2.0, 3.0;
 
-  if (!inConvexHull(pts, Vector2d(2, 2))) {
-    fprintf(stderr, "2,2 should be in hull\n");
+  if (!inConvexHull(pts, Vector2d(2.0, 2.0))) {
+    fprintf(stderr, "2.0,2.0 should be in hull\n");
     return 1;
   }
 
-  if (!inConvexHull(pts, Vector2d(1.0001, 2))) {
-    fprintf(stderr, "1.0001, 2 should be in hull\n");
+  if (!inConvexHull(pts, Vector2d(1.0001, 2.0))) {
+    fprintf(stderr, "1.0001, 2.0 should be in hull\n");
     return 1;
   }
-  if (inConvexHull(pts, Vector2d(0.9999, 2))) {
-    fprintf(stderr, "0.9999, 2 should not be in hull\n");
+  if (inConvexHull(pts, Vector2d(0.9999, 2.0))) {
+    fprintf(stderr, "0.9999, 2.0 should not be in hull\n");
     return 1;
   }
   if (!inConvexHull(pts, Vector2d(2.49, 2.49))) {
@@ -71,6 +71,19 @@ void testRealData() {
   valuecheck(d, 0.136017, 1e-6);
 }
 
+void testDuplicates() {
+  Matrix<double, 2, Dynamic> pts(2, 8);
+  pts << 0.0, 1.0, 1.0, 0.0, 0.5, 0.0, 0.0, 1.0 - 1e-16, 
+         0.0, 1.0, 0.0, 1.0, 0.5, 0.0, 0.0, 1.0 - 1e-16;
+  double d = signedDistanceInsideConvexHull(pts, Vector2d(0, 0));
+  valuecheck(d, 0.0, 1e-8);
+
+  d = signedDistanceInsideConvexHull(pts, Vector2d(0.5, 0.5));
+  valuecheck(d, 0.5, 1e-8);
+
+  d = signedDistanceInsideConvexHull(pts, Vector2d(-0.5, 0));
+  valuecheck(d, -0.5, 1e-8);
+}
 
 int main() {
   bool failed = false;
@@ -86,9 +99,10 @@ int main() {
 
   testDistanceFromHull();
   std::cout << "testDistanceFromHull passed" << std::endl;
-
   testRealData();
   std::cout << "testRealData passed" << std::endl;
+  testDuplicates();
+  std::cout << "testDuplicates passed" << std::endl;
 
   if (!failed) {
     std::cout << "convexHull tests passed" << std::endl;
