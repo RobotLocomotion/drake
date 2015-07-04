c8a6601971b85ad586386f2f162f58e1ea8dadd9
MotherBrain <aespielberg@ubuntu.(none)>
2015-07-04 12:06:53 -0700

    Committing progress on parameterizing kinematics

---
+++ examples/Acrobot/test_aespielberg/paramEstSyntheticData.m
@@ -0,0 +1,26 @@
+function paramEstSyntheticData
+
+%oldpath = addpath(fullfile(pwd,'..'));
+
+r = AcrobotPlantTest;
+v = AcrobotVisualizer(r);
+
+true_parameters = getParams(r);
+
+%% test on swingup up data
+[utraj,xtraj] = swingUpTrajectory(r);
+
+% Try parameter estimation without any noise
+Ts = .01; breaks=getBreaks(utraj); T0 = breaks(1); Tf = breaks(end);
+data = iddata(eval(xtraj,T0:Ts:Tf)',eval(utraj,T0:Ts:Tf)',Ts,'InputName',r.getInputFrame.coordinates,'OutputName',r.getOutputFrame.coordinates);
+
+estimated_parameters = parameterEstimation(r,data);
+
+
+% Try parameter estimation with noise
+
+
+% Try parameter estimation with noise and known delay
+
+
+path(oldpath);
