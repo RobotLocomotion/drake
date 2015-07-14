661e9482b338d8696c211fb99c75160dd610037b
Michael Posa <mposa@mit.edu>
2015-07-13 20:03:31 -0400

    Added StepTerrain draft.

--- systems/plants/RigidBodyLinearStepTerrain.m
+++ systems/plants/RigidBodyLinearStepTerrain.m
@@ -1,16 +1,13 @@
-classdef RigidBodyStepTerrain < RigidBodyHeightMapTerrain
+classdef RigidBodyLinearStepTerrain < RigidBodyHeightMapTerrain
 %  This class provides an implementation of RigidBodyTerrain with a step
 %  from z = 0.  Step is oriented in the x-axis.
-%  Uses a smooth step function (3t^2 - 2t^3) that has a zero derivative at
-%  x = 0 and x = 1
+%  Uses a linear step up/down.
 %  Currently returns normal with NON-UNIT value. This is because
 %  phi(q) != distance, and the normal is used with forward kinematics to
 %  calculate dphi/dq
-%  WARNING! This class will not work, because it violates assumptions made
-%  bu collision detection. Specifically, dnormal/dq != 0
   
   methods 
-    function obj = RigidBodyStepTerrain(step_height,step_x,smoothing_width)
+    function obj = RigidBodyLinearStepTerrain(step_height,step_x,smoothing_width)
       x = -10:.01:10;
       y = [-5 5];
       [X,Y] = ndgrid(x,y);
@@ -18,7 +15,7 @@ classdef RigidBodyStepTerrain < RigidBodyHeightMapTerrain
       Z = zeros(length(x),length(y));
       t = (X - step_x + smoothing_width)/(2*smoothing_width);
       Z(t >= 1) = step_height;
-      Z(t >0 & t < 1) = step_height*(3*t(I_s).^2 - 2*t(I_s).^3);
+      Z(t >0 & t < 1) = step_height*t(I_s);
       
       obj@RigidBodyHeightMapTerrain(x,y,Z',eye(4));
         
@@ -35,9 +32,9 @@ classdef RigidBodyStepTerrain < RigidBodyHeightMapTerrain
       t = (xy(1,:) - obj.x - obj.smoothing)/(2*obj.smoothing);
       z(t >= 1) = obj.height;
       I_s = find(t > 0 & t < 1);
-      z(I_s) = obj.height*(3*t(I_s).^2 - 2*t(I_s).^3);
+      z(I_s) = obj.height*t(I_s);
       normal = repmat([0;0;1],1,n);
-      normal(I_s,1) = 3*obj.height*(t(I_s) - t(I_s).^2)/obj.smoothing;
+      normal(I_s,1) = obj.height/(2*obj.smoothing);
     end
   end
   
