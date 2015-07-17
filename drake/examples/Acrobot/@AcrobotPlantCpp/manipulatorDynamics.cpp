6f7eccbcbab23166089320bcb7007abf2ab1dc8f
Twan Koolen <koolen.twan@gmail.com>
2015-07-17 17:53:30 -0400

    Got AutoDiffScalar gradients working (TaylorVar input to manipulatorDynamics).

--- drake/examples/Acrobot/@AcrobotPlantCpp/manipulatorDynamics.cpp
+++ drake/examples/Acrobot/@AcrobotPlantCpp/manipulatorDynamics.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <iostream>
 #include "drakeMexUtil.h"
+#include <unsupported/Eigen/AutoDiff>
+#include <drakeGradientUtil.h>
 
 using namespace Eigen;
 using namespace std;
@@ -30,7 +32,7 @@ void manipulatorDynamics(const mxArray* pobj, const MatrixBase<DerivedA> &q, con
   auto c2 = cos(q(1));
   auto s1 = sin(q(0)), s2 = sin(q(1));
   auto s12 = sin(q(0)+q(1));
-      
+
   auto h12 = I2 + m2l1lc2*c2;
   H << I1 + I2 + m2*l1*l1 + 2*m2l1lc2*c2, h12, h12, I2;
 
@@ -46,6 +48,89 @@ void manipulatorDynamics(const mxArray* pobj, const MatrixBase<DerivedA> &q, con
   C(0)+=b1*qd(0); C(1)+=b2*qd(1);
 
   B << 0.0, 1.0;
+}
+
+template <typename Derived>
+Eigen::Matrix<typename Derived::Scalar::Real, Derived::RowsAtCompileTime, Derived::ColsAtCompileTime> autoDiffToValueMatrix(const Eigen::MatrixBase<Derived>& autoDiff) {
+  Eigen::Matrix<typename Derived::Scalar::Real, Derived::RowsAtCompileTime, Derived::ColsAtCompileTime> ret(autoDiff.rows(), autoDiff.cols());
+  for (int i = 0; i < autoDiff.rows(); i++) {
+    for (int j = 0; j < autoDiff.cols(); ++j) {
+      ret(i, j) = autoDiff(i, j).value();
+    }
+  }
+  return ret;
+};
+
+template<typename Derived>
+typename Gradient<Eigen::Matrix<typename Derived::Scalar::Real, Derived::RowsAtCompileTime, Derived::ColsAtCompileTime>, Eigen::Dynamic>::type autoDiffToGradientMatrix(
+        const Eigen::MatrixBase<Derived>& autoDiff, int num_variables = Eigen::Dynamic)
+{
+  int num_variables_from_matrix = 0;
+  for (int i = 0; i < autoDiff.size(); ++i) {
+    num_variables_from_matrix = std::max(num_variables_from_matrix, static_cast<int>(autoDiff(i).derivatives().size()));
+  }
+  if (num_variables == Eigen::Dynamic) {
+    num_variables = num_variables_from_matrix;
+  }
+  else if (num_variables_from_matrix != 0 && num_variables_from_matrix != num_variables) {
+    std::stringstream buf;
+    buf << "Input matrix has derivatives w.r.t " << num_variables_from_matrix << ", variables" << ", whereas num_variables is " << num_variables << ".\n";
+    buf << "Either num_variables_from_matrix should be zero, or it should match num_variables";
+    throw std::runtime_error(buf.str());
+  }
+
+  typename Gradient<Eigen::Matrix<typename Derived::Scalar::Real, Derived::RowsAtCompileTime, Derived::ColsAtCompileTime>, Eigen::Dynamic>::type gradient(autoDiff.size(), num_variables);
+  for (int row = 0; row < autoDiff.rows(); row++) {
+    for (int col = 0; col < autoDiff.cols(); col++) {
+      auto gradient_row = gradient.row(row + col * autoDiff.rows()).transpose();
+      if (autoDiff(row, col).derivatives().size() == 0) {
+        gradient_row.setZero();
+      } else {
+        gradient_row = autoDiff(row, col).derivatives();
+      }
+    }
+  }
+  return gradient;
+}
+
+template<typename DerivedGradient, typename DerivedAutoDiff>
+void gradientMatrixToAutoDiff(const Eigen::MatrixBase<DerivedGradient>& gradient, Eigen::MatrixBase<DerivedAutoDiff>& autoDiff)
+{
+  int nq = gradient.cols();
+  for (size_t row = 0; row < autoDiff.rows(); row++) {
+    for (size_t col = 0; col < autoDiff.cols(); col++) {
+      autoDiff(row, col).derivatives().resize(nq, 1);
+      autoDiff(row, col).derivatives() = gradient.row(row + col * autoDiff.rows()).transpose();
+    }
+  }
+}
+
+template<int Rows, int Cols>
+Eigen::Matrix<AutoDiffScalar<VectorXd>, Rows, Cols> taylorVarToEigen(const mxArray* taylor_var) {
+  auto f = mxGetPropertySafe(taylor_var, "f");
+  auto df = mxGetPropertySafe(taylor_var, "df");
+  if (mxGetNumberOfElements(df) > 1)
+    throw runtime_error("TaylorVars of order higher than 1 currently not supported");
+  auto ret = matlabToEigenMap<Rows, Cols>(f).template cast<AutoDiffScalar<VectorXd>>().eval();
+  typedef Gradient<decltype(ret), Dynamic> GradientType;
+  auto gradient_matrix = matlabToEigenMap<GradientType::type::RowsAtCompileTime, GradientType::type::ColsAtCompileTime>(mxGetCell(df, 0));
+  gradientMatrixToAutoDiff(gradient_matrix, ret);
+
+  return ret;
+}
+
+template <typename Derived>
+mxArray *eigenToTaylorVar(const MatrixBase<Derived>& m, int num_variables = Eigen::Dynamic)
+{
+  const int nrhs = 2;
+  mxArray *prhs[nrhs];
+  prhs[0] = eigenToMatlab(autoDiffToValueMatrix(m));
+  mwSize dims[] = {1};
+  prhs[1] = mxCreateCellArray(1, dims);
+  mxArray *plhs[1];
+  mxSetCell(prhs[1], 0, eigenToMatlab(autoDiffToGradientMatrix(m, num_variables)));
+  mexCallMATLABsafe(1, plhs, nrhs, prhs, "TaylorVar");
+  return plhs[0];
 }
 
 void mexFunction( int nlhs, mxArray *plhs[],int nrhs, const mxArray *prhs[] )
@@ -76,6 +161,21 @@ void mexFunction( int nlhs, mxArray *plhs[],int nrhs, const mxArray *prhs[] )
     plhs[0] = eigenToTrigPoly<2,2>(H);
     plhs[1] = eigenToTrigPoly<2,1>(C);
     plhs[2] = eigenToTrigPoly<2,1>(B);
+  } else if (isa(prhs[1], "TaylorVar") && isa(prhs[2], "TaylorVar")) {
+    typedef AutoDiffScalar<VectorXd> Scalar;
+    auto q = taylorVarToEigen<Dynamic, 1>(prhs[1]);
+    auto qd = taylorVarToEigen<Dynamic, 1>(prhs[2]);
+    Matrix<Scalar, 2, 2> H;
+    Matrix<Scalar, 2, 1> C;
+    Matrix<Scalar, 2, 1> B;
+
+    manipulatorDynamics(pobj, q, qd, H, C, B);
+
+    int nx = q(0).derivatives().size();
+    plhs[0] = eigenToTaylorVar(H, nx);
+    plhs[1] = eigenToTaylorVar(C, nx);
+    plhs[2] = eigenToTaylorVar(B, nx);
+
   } else {
     mexErrMsgIdAndTxt("Drake:AcrobotPLantCpp:UnknownType","don't know how to handle the datatypes passed in for q and/or qd (yet)");
   }
