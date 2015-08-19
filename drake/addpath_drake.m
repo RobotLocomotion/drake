9e84cfd4e2455cd15024023d8a722b634b178d2b
peteflorence <pete.florence@gmail.com>
2015-08-19 18:08:48 -0400

    accessing getProperty, better solution

--- drake/addpath_drake.m
+++ drake/addpath_drake.m
@@ -61,13 +61,16 @@ javaaddpath(fullfile(pods_get_base_path,'share','java','drake.jar'));
 
 % OSX platform-specific: check if reverted to IPv4
 if (computer('arch') == 'maci64')
-  javaoptspath = fileread([matlabroot '/bin/' computer('arch') '/java.opts']);
-  k = strfind(javaoptspath, '-Djava.net.preferIPv4Stack=true');
-  if isempty(k)
+  ipv4_preferred = java.lang.System.getProperty('java.net.preferIPv4Stack');
+  if isempty(ipv4_preferred)
+    ipv4_preferred = 'false';
+  end
+  if (ipv4_preferred ~= 'true')
     display('WARNING: Your JVM may crash if you do not set it to prefer IPv4 over IPv6.')
     display('This may cause any dependencies that involve the JVM (including LCM) to crash at runtime.')
     display('Please see bug report and solution here: https://github.com/RobotLocomotion/drake/issues/558.')
     display('(It just involves adding one line to your java.opts file for Matlab.)')
+    display('Make sure to restart Matlab after editing your java.opts file.')
   end
 end
 
