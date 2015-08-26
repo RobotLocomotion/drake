dd8aacac543fedff3263e1cd050437f931fb90c6
Twan Koolen <koolen.twan@gmail.com>
2015-08-25 21:20:45 -0400

    try to get individual cops working again.

--- drake/systems/controllers/individualCentersOfPressure.m
+++ drake/systems/controllers/individualCentersOfPressure.m
@@ -1,25 +1,26 @@
 function cops = individualCentersOfPressure(r, kinsol, active_supports, normals, B, beta)
 
 n_basis_vectors_per_contact = length(beta) / size(normals, 2);
-n = length(active_supports);
 
 normals_start = 0;
 indexB = 1;
 beta_start = 0;
 
-cops = nan(3, n);
+cops = nan(3, length(active_supports));
 
+% everything in world frame.
 for j = 1:length(active_supports)
   contact_positions = r.getBody(active_supports(j)).getTerrainContactPoints();
-  ncj = size(contact_positions, 2);
-  normalsj = normals(:, normals_start + (1 : ncj));
-  normal = normalsj(:, 1);
-  normals_identical = ~any(any(bsxfun(@minus, normalsj, normal)));
+  contact_positions = r.forwardKin(kinsol, active_supports(j), contact_positions);
+  num_contacts_j = size(contact_positions, 2);
+  normals_j = normals(:, normals_start + (1 : num_contacts_j));
+  normal = normals_j(:, 1);
+  normals_identical = ~any(any(bsxfun(@minus, normals_j, normal) > 1e-8));
   
-  if normals_identical % otherwise computing a COP doesn't make sense
+  if normals_identical
     force = zeros(3, 1);
     torque = zeros(3, 1);
-    for k = 1 : ncj
+    for k = 1 : num_contacts_j
       Bblock = B{indexB};
       betablock = beta(beta_start + (1 : n_basis_vectors_per_contact));
       contact_position = contact_positions(:, k);
@@ -30,12 +31,14 @@ for j = 1:length(active_supports)
       indexB = indexB + 1;
       beta_start = beta_start + n_basis_vectors_per_contact;
     end
+    
     point_on_contact_plane = contact_positions(:, 1);
-    cop_body = resolveCenterOfPressure(torque, force, normal, point_on_contact_plane);
-    cops(:, j) = r.forwardKin(kinsol, active_supports(j), cop_body, 0);
+    cops(:, j) = resolveCenterOfPressure(torque, force, normal, point_on_contact_plane);
+  else
+    error('normals must be identical to compute CoP');  % otherwise computing a COP doesn't make sense
   end
   
-  normals_start = normals_start + ncj;
+  normals_start = normals_start + num_contacts_j;
 end
 
 end
