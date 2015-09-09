60f448d4917d724214b4acf6b0211cf64216a00f
Russ Tedrake <russt@mit.edu>
2015-09-09 08:39:56 -0400

    set data export parameter for 2015b compliance

--- drake/util/getSimulinkStateStructure.m
+++ drake/util/getSimulinkStateStructure.m
@@ -3,6 +3,7 @@ function x_struct = getSimulinkStateStructure(mdl)
 % having it there caused simulink to be loaded ( see 
 % https://github.com/RobotLocomotion/drake/issues/1044 )
 
+set_param(mdl,'SaveFormat','Structure');
 x_struct = Simulink.BlockDiagram.getInitialState(mdl);
 
 
