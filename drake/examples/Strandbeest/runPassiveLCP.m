752cf1dbd72172f4c96e1f4ca2e1b08d51e378ed
Robin Deits <robin.deits@gmail.com>
2015-09-11 03:40:12 -0400

    comments

--- drake/examples/Strandbeest/runPassiveLCP.m
+++ drake/examples/Strandbeest/runPassiveLCP.m
@@ -1,27 +1,33 @@
 function runPassiveLCP
+% To learn more about Stranbeests, created by Theo Jansen, see http://www.strandbeest.com/
+% Leg measurements taken from http://files.tested.com/photos/2015/04/05/74549-leg-measurement.jpg
 
-options.twoD = false;
+% Load our URDF model of the Strandbeest
 options.floating = true;
 w = warning('off','Drake:RigidBody:SimplifiedCollisionGeometry');
 p = TimeSteppingRigidBodyManipulator('Strandbeest.urdf',.01,options);
 warning(w);
-p = p.setTerrain(RigidBodyFlatTerrain()).compile();
-v = p.constructVisualizer();
 
+% Give it something to walk on
+p = p.setTerrain(RigidBodyFlatTerrain()).compile();
+
+% Add a controller which just applies a fixed constant torque to the central axle
 controller = AffineSystem([], [], [], [], [], [], [], zeros(1, p.getNumOutputs()), 300);
 controller = controller.setInputFrame(p.getOutputFrame());
 controller = controller.setOutputFrame(p.getInputFrame());
 sys = feedback(p, controller);
 
 % Add a visualizer
+v = p.constructVisualizer();
 output_select(1).system=1;
 output_select(1).output=1;
 sys = mimoCascade(sys,v,[],[],output_select);
 
+% Simulate the system
 x0 = p.getInitialState();
 x0(4:6) = 0;
 x0(3) = 1.25;
 % v.inspector();
-xtraj = sys.simulate([0 5], x0, struct('gui_control_interface', true));
+xtraj = sys.simulate([0 8], x0, struct('gui_control_interface', true));
 
 v.playback(xtraj, struct('slider', true));
