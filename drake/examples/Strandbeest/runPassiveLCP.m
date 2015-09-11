d87d9312ddb44568cf4e35517eba8c1265e05ca1
Robin Deits <robin.deits@gmail.com>
2015-09-10 23:59:29 -0400

    WIP: overly complex leg assembly

---
+++ drake/examples/Strandbeest/runPassiveLCP.m
@@ -0,0 +1,17 @@
+function runPassiveLCP
+
+options.twoD = false;
+options.floating = false;
+w = warning('off','Drake:RigidBody:SimplifiedCollisionGeometry');
+p = TimeSteppingRigidBodyManipulator('LegAssembly.urdf',.01,options);
+% p = p.setTerrain(RigidBodyFlatTerrain()).compile();
+warning(w);
+v = p.constructVisualizer();
+v.inspector()
+
+% x0 = p.getInitialState();
+% x0(3) = x0(3) + 1;
+
+% xtraj = p.simulate([0 5], x0);
+
+% v.playback(xtraj);
