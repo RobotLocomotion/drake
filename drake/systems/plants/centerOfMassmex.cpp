dc4c189044c415d2fee48d3c74a152372ef53037
Twan Koolen <koolen.twan@gmail.com>
2015-09-28 15:41:36 -0400

    Switched mex functions over to using rigidBodyManipulatorMexFunctions.

--- drake/systems/plants/centerOfMassmex.cpp
+++ drake/systems/plants/centerOfMassmex.cpp
@@ -1,45 +1,5 @@
-#include <mex.h>
-#include <iostream>
-#include <memory>
-#include "drakeMexUtil.h"
-#include "RigidBodyManipulator.h"
-#include "math.h"
+#include "rigidBodyManipulatorMexFunctions.h"
 
-using namespace Eigen;
-using namespace std;
-
-void mexFunction(int nlhs, mxArray *plhs[],int nrhs, const mxArray *prhs[]) {
-
-  std::string usage = "Usage [x, J, dJ] = centerOfMassmex(model_ptr, cache_ptr, robotnum, in_terms_of_qdot)";
-  if (nrhs != 4) {
-    mexErrMsgIdAndTxt("Drake:centerOfMassmex:WrongNumberOfInputs", usage.c_str());
-  }
-
-  if (nlhs > 3) {
-    mexErrMsgIdAndTxt("Drake:centerOfMassmex:WrongNumberOfOutputs", usage.c_str());
-  }
-
-  int arg_num = 0;
-  RigidBodyManipulator *model = static_cast<RigidBodyManipulator*>(getDrakeMexPointer(prhs[arg_num++]));
-  KinematicsCache<double>* cache = static_cast<KinematicsCache<double>*>(getDrakeMexPointer(prhs[arg_num++]));
-
-  set<int> robotnum_set;
-  int num_robot = static_cast<int>(mxGetNumberOfElements(prhs[arg_num]));
-  double* robotnum = mxGetPrSafe(prhs[arg_num]);
-  for (int i = 0; i < num_robot; i++) {
-    robotnum_set.insert((int) robotnum[i] - 1);
-  }
-  arg_num++;
-
-  bool in_terms_of_qdot = (bool) (mxGetLogicals(prhs[arg_num++]))[0];
-  int gradient_order = nlhs - 1;
-
-  auto com = model->centerOfMass<double>(*cache, robotnum_set);
-  plhs[0] = eigenToMatlab(com);
-  if (gradient_order > 0) {
-    auto J_com = model->centerOfMassJacobian<double>(*cache, gradient_order - 1, robotnum_set, in_terms_of_qdot);
-    plhs[1] = eigenToMatlab(J_com.value());
-    if (gradient_order > 1)
-    plhs[2] = eigenToMatlab(J_com.gradient().value());
-  }
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+  centerOfMassmex(nlhs, plhs, nrhs, prhs);
 }
--- drake/systems/plants/forwardKinmex.cpp
+++ drake/systems/plants/forwardKinmex.cpp
@@ -1,35 +1,5 @@
-#include <mex.h>
-#include <iostream>
-#include <memory>
-#include "drakeMexUtil.h"
-#include "RigidBodyManipulator.h"
-#include "drakeGeometryUtil.h"
-#include "math.h"
+#include "rigidBodyManipulatorMexFunctions.h"
 
-using namespace Eigen;
-using namespace std;
-
-
-void mexFunction(int nlhs, mxArray *plhs[],int nrhs, const mxArray *prhs[]) {
-
-  std::string usage = "Usage x = forwardKinmex(model_ptr, cache_ptr, body_or_frame_ind, points, rotation_type, base_or_frame_ind)";
-  if (nrhs != 6) {
-    mexErrMsgIdAndTxt("Drake:forwardKinmex:WrongNumberOfInputs", usage.c_str());
-  }
-
-  if (nlhs != 1) {
-    mexErrMsgIdAndTxt("Drake:forwardKinmex:WrongNumberOfOutputs", usage.c_str());
-  }
-
-  int arg_num = 0;
-  RigidBodyManipulator *model = static_cast<RigidBodyManipulator*>(getDrakeMexPointer(prhs[arg_num++]));
-  KinematicsCache<double>* cache = static_cast<KinematicsCache<double>*>(getDrakeMexPointer(prhs[arg_num++]));
-
-  int body_or_frame_ind = ((int) mxGetScalar(prhs[arg_num++]));
-  auto points = matlabToEigen<SPACE_DIMENSION, Eigen::Dynamic>(prhs[arg_num++]);
-  int rotation_type = (int) mxGetScalar(prhs[arg_num++]);
-  int base_or_frame_ind = ((int) mxGetScalar(prhs[arg_num++]));
-
-  auto x = model->forwardKin(*cache, points, body_or_frame_ind, base_or_frame_ind, rotation_type);
-  plhs[0] = eigenToMatlab(x);
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+  forwardKinmex(nlhs, plhs, nrhs, prhs);
 }
