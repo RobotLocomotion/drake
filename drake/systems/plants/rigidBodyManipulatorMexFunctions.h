130e2998ab6be74f464f3a854f8e58416140a6ba
Twan Koolen <koolen.twan@gmail.com>
2015-09-28 20:59:32 -0400

    Add DLLEXPORT.

--- drake/systems/plants/rigidBodyManipulatorMexFunctions.h
+++ drake/systems/plants/rigidBodyManipulatorMexFunctions.h
@@ -3,20 +3,31 @@
 #ifndef DRAKE_DRAKEMEXFUNCTIONS_H
 #define DRAKE_DRAKEMEXFUNCTIONS_H
 
-void centerOfMassJacobianDotTimesVmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void centerOfMassmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void centerOfMassJacobianmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void centroidalMomentumMatrixDotTimesvmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void centroidalMomentumMatrixmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void doKinematicsmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void findKinematicPathmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void forwardJacDotTimesVmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void forwardKinmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void forwardKinJacobianmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void forwardKinPositionGradientmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void geometricJacobianDotTimesVmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void geometricJacobianmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void massMatrixmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
-void dynamicsBiasTermmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+#undef DLLEXPORT
+#if defined(WIN32) || defined(WIN64)
+#if defined(rbmMexFunctions_EXPORTS)
+    #define DLLEXPORT __declspec( dllexport )
+  #else
+    #define DLLEXPORT __declspec( dllimport )
+  #endif
+#else
+#define DLLEXPORT
+#endif
+
+DLLEXPORT void centerOfMassJacobianDotTimesVmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void centerOfMassmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void centerOfMassJacobianmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void centroidalMomentumMatrixDotTimesvmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void centroidalMomentumMatrixmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void doKinematicsmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void findKinematicPathmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void forwardJacDotTimesVmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void forwardKinmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void forwardKinJacobianmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void forwardKinPositionGradientmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void geometricJacobianDotTimesVmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void geometricJacobianmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void massMatrixmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+DLLEXPORT void dynamicsBiasTermmex(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
 
 #endif //DRAKE_DRAKEMEXFUNCTIONS_H
