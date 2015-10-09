afce95ba6071621c2fc7ea4246d24afb7f79cd23
Maurice Fallon <maurice.fallon@ed.ac.uk>
2015-10-09 14:55:19 +0100

    removes installed include, uses std::vector, removes shared pointer

--- drake/systems/plants/test/testIK.cpp
+++ drake/systems/plants/test/testIK.cpp
@@ -21,15 +21,15 @@ int main()
   Vector3d com_ub = Vector3d::Zero(); 
   com_lb(2) = 0.9;
   com_ub(2) = 1.0;
-  WorldCoMConstraint* com_kc = new WorldCoMConstraint(&model,com_lb,com_ub,tspan);
-  int num_constraints = 1;
-  RigidBodyConstraint** constraint_array = new RigidBodyConstraint*[num_constraints];
-  constraint_array[0] = com_kc;
+  WorldCoMConstraint com_kc(&model,com_lb,com_ub,tspan);
+
+  std::vector<RigidBodyConstraint*> constraint_array;
+  constraint_array.push_back( &com_kc );
   IKoptions ikoptions(&model);
   VectorXd q_sol(model.num_positions);
   int info;
   vector<string> infeasible_constraint;
-  inverseKin(&model,q0,q0,num_constraints,constraint_array,q_sol,info,infeasible_constraint,ikoptions);
+  inverseKin(&model,q0,q0,constraint_array.size(),constraint_array.data(),q_sol,info,infeasible_constraint,ikoptions);
   printf("INFO = %d\n",info);
   if(info != 1)
   {
@@ -44,8 +44,6 @@ int main()
   memcpy(mxGetPrSafe(pqsol),q_sol.data(),sizeof(double)model.num_dof);
   matPutVariable(presultmat,"q_sol",pqsol);
   matClose(presultmat);*/
-  delete com_kc;
-  delete[] constraint_array;
   return 0;
 }
   
