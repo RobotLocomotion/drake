c798c9799ac6c24185abf783d68a528715babd7c
Russ Tedrake <russt@mit.edu>
2015-11-24 06:57:44 -0500

    adds time-domain plotting, too.

--- drake/examples/test/simpleCTParticleFilterDemo.m
+++ drake/examples/test/simpleCTParticleFilterDemo.m
@@ -3,30 +3,41 @@ function simpleCTParticleFilterDemo
 tmppath = addpathTemporary(fullfile(getDrakePath,'examples'));
 
 noise_covariance = .1;  % pretty robust stationary distribution
-%noise_covariance = 2;   % should see some "escapes"
+noise_covariance = 1.2;   % should see some "escape attempts"
+%noise_covariance = 2;   % see definite particle loss nominal system
 
 sys = SimpleCTExample;
 
 % the time-reversed system is interesting, too
-%sys = FunctionHandleSystem(1,0,0,1,false,true,@(t,x,u) x-x.^3,[],@(t,x,u)x);
+sys = FunctionHandleSystem(1,0,0,1,false,true,@(t,x,u) x-x.^3,[],@(t,x,u)x);
 
 sys = DrakeSystemWGaussianNoise(sys,noise_covariance,[],0,.1);
 
 tspan = 0:.1:30;
 
-%figure(1); clf; hold on; axis([tspan(1) tspan(end) -2 2]);
-figure(1); clf; hold on;  xs = -1.5:.1:1.5; plot(xs,2500*dynamics(sys,0,xs,[]),'Color','k','LineWidth',4); % for histogram
-
-particleFilterDemo(sys,randn(1,100000),tspan,@draw_function);
-
+plot_histogram = true;
+if (plot_histogram)
+  num_particles = 100000;
+  figure(1); clf; hold on;  xs = -1.5:.1:1.5; plot(xs,2500*dynamics(sys,0,xs,[]),'Color','k','LineWidth',4); % for histogram
+else % plot time
+  num_particles = 50;
+  figure(1); clf; hold on; axis([tspan(1) tspan(end) -2 2]);
 end
+
+particleFilterDemo(sys,randn(1,num_particles),tspan,@draw_function);
 
 function draw_function(t,x)
 
-% plot(t,x,'b.','MarkerSize',20)
-h = histogram(x,[-1.5:.05:1.5]); axis([-1.5 1.5 -5000 15000]);
-set(h,'FaceColor','r');
-drawnow;
-delete(h)
+if (plot_histogram)
+  h = histogram(x,[-1.5:.05:1.5]); axis([-1.5 1.5 -5000 15000]);
+  set(h,'FaceColor','r');
+  drawnow;
+  delete(h)
+else % plot time
+  plot(t,x,'b.','MarkerSize',20)
+  drawnow;
+end
+
+end
 
 end
