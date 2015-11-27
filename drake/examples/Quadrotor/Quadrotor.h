637ca23aa12ef52464fc0ce7ce7b7579f3c7831e
John Carter <psiorx@gmail.com>
2015-11-27 15:51:14 -0500

    Quadrotor dynamics are now AutoDiffScalar friendly

--- drake/examples/Quadrotor/Quadrotor.h
+++ drake/examples/Quadrotor/Quadrotor.h
@@ -22,14 +22,17 @@ public:
             I(Eigen::Matrix3d::Identity()),
             num_states(getStateFrame().getDim()),
             num_inputs(getInputFrame().getDim())
-  {
-  }
+  {}
   virtual ~Quadrotor(void) {};
 
   virtual Eigen::VectorXd dynamics(double t, const Eigen::VectorXd& x, const Eigen::VectorXd& u) const override {
-    return dynamics_implementation(t,x,u);
+    return dynamics_implementation(t, x, u);
   }
-
+  
+  virtual Drake::TaylorVecX dynamics(Drake::TaylorVarX t, const Drake::TaylorVecX& x, const Drake::TaylorVecX& u) const override {
+    return dynamics_implementation(t, x, u);
+  }
+  
   virtual Eigen::VectorXd output(double t, const Eigen::VectorXd& x, const Eigen::VectorXd& u) const override {
     return x;
   }
@@ -37,12 +40,13 @@ public:
   virtual bool isTimeInvariant() const override { return true; }
   virtual bool isDirectFeedthrough() const override { return false; }
 
-
   DrakeSystemPtr balanceLQR() {
-    Eigen::MatrixXd Q(num_states, num_states);  Q = Eigen::MatrixXd::Identity(num_states, num_states);
-    Eigen::MatrixXd R(num_inputs, 1);  R << 1;
+    const int num_positions = num_states/2;
+    Eigen::MatrixXd Q = Eigen::MatrixXd::Identity(num_states, num_states);
+    Q.block<0, 0>(num_positions, num_positions) = 10.0 * Eigen::MatrixXd::Ones(num_positions, num_positions);
+    Eigen::VectorXd R = Eigen::VectorXd::Ones(num_inputs);
     Eigen::VectorXd xG(num_states);   xG << 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
-    Eigen::VectorXd uG(num_inputs);   uG = Eigen::VectorXd::Ones(4) * m * g * 0.25;
+    Eigen::VectorXd uG = Eigen::VectorXd::Ones(num_inputs) * m * g * 0.25;
 
     return timeInvariantLQR(xG,uG,Q,R);
   }
@@ -54,81 +58,73 @@ public:
 private:
   const unsigned int num_states;
   const unsigned int num_inputs;
+  const double kf = 1;
+  const double km = 0.0245;
 
   template <typename Scalar, typename Vector>
   Vector dynamics_implementation(Scalar t, const Vector& x, const Vector& u) const {
     Vector xdot(num_states);
     xdot = Vector::Zero(num_states);
 
-    double phi = x(3);
-    double theta = x(4);
-    double psi = x(5);
-    double phidot = x(9);
-    double thetadot = x(10);
-    double psidot = x(11);
+    Scalar phi = x(3);
+    Scalar theta = x(4);
+    Scalar psi = x(5);
+    Scalar phidot = x(9);
+    Scalar thetadot = x(10);
+    Scalar psidot = x(11);
 
-    double w1 = u(0);
-    double w2 = u(1);
-    double w3 = u(2);
-    double w4 = u(3);
+    Scalar w1 = u(0);
+    Scalar w2 = u(1);
+    Scalar w3 = u(2);
+    Scalar w4 = u(3);
 
-
-    Eigen::Vector3d rpy;
-    rpy << phi, theta, psi;
-    Eigen::Matrix3d R = rpy2rotmat(rpy);
-
-    const double kf = 1;
-    const double km = 0.0245;
+    Eigen::Matrix<Scalar, 3, 1> rpy(phi, theta, psi);
+    Eigen::Matrix<Scalar, 3, 3> R = rpy2rotmat(rpy);
     
-    double F1 = kf * w1;
-    double F2 = kf * w2;
-    double F3 = kf * w3;
-    double F4 = kf * w4;
+    Scalar F1 = kf * w1;
+    Scalar F2 = kf * w2;
+    Scalar F3 = kf * w3;
+    Scalar F4 = kf * w4;
 
-    double M1 = km * w1;
-    double M2 = km * w2;
-    double M3 = km * w3;
-    double M4 = km * w4;
+    Scalar M1 = km * w1;
+    Scalar M2 = km * w2;
+    Scalar M3 = km * w3;
+    Scalar M4 = km * w4;
 
-    Eigen::Vector3d gvec;
-    gvec << 0, 0, -m * g;
-    Eigen::Vector3d forcevec;
-    forcevec << 0, 0, F1 + F2 + F3 + F4;
-    Eigen::Vector3d xyz_ddot = (1.0 / m) * (gvec + R * forcevec);
+    Eigen::Matrix<Scalar, 3, 1> gvec(0, 0, -m * g);
+    Eigen::Matrix<Scalar, 3, 1> forcevec(0, 0, F1 + F2 + F3 + F4);
+    Eigen::Matrix<Scalar, 3, 1> xyz_ddot = (1.0 / m) * (gvec + R * forcevec);
 
-    Eigen::Vector3d rpydot;
-    rpydot << phidot, thetadot, psidot;
-
-    Eigen::Vector3d pqr;
+    Eigen::Matrix<Scalar, 3, 1> rpydot(phidot, thetadot, psidot);
+    Eigen::Matrix<Scalar, 3, 1> pqr;
     rpydot2angularvel(rpy, rpydot, pqr);
     pqr = R.adjoint() * pqr;
 
-    Eigen::Vector3d pqr_dot_term1;
+    Eigen::Matrix<Scalar, 3, 1> pqr_dot_term1;
     pqr_dot_term1 << L * (F2 - F4), L * (F3 - F1), (M1 - M2 + M3 - M4);
-    Eigen::Vector3d pqr_dot = I.ldlt().solve(pqr_dot_term1 - pqr.cross(I * pqr));
 
-    Eigen::Matrix3d Phi;
-    Gradient<Eigen::Matrix3d, 3>::type dPhi;
-    auto ddPhi = (Gradient<Eigen::Matrix3d, 3, 2>::type*) nullptr;
+    //pqr.cross(I * pqr) won't compile with AutoDiffScalar types?
+    auto a = pqr;
+    auto b = I * pqr;
+    Eigen::Matrix<Scalar, 3, 1> cross(a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b[2], a[0] * b[1] - a[1] * b[0]);
+
+    Eigen::Matrix<Scalar, 3, 1> pqr_dot = I.ldlt().solve(pqr_dot_term1 - cross);
+    Eigen::Matrix<Scalar, 3, 3> Phi;
+    typename Gradient<Eigen::Matrix<Scalar, 3, 3>, 3>::type dPhi;
+    auto ddPhi = (typename Gradient<Eigen::Matrix<Scalar, 3, 3>, 3, 2>::type*) nullptr;
     angularvel2rpydotMatrix(rpy, Phi, &dPhi, ddPhi);
 
-    // This replaces the implementation of Rdot (make a 9x1 vector, reshape it into a 3 x 3)
-    Eigen::Matrix<double, 9, 3> drpy2drotmat = drpy2rotmat(rpy);
-    Eigen::Matrix<double, 9, 1> Rdot_vec;
+    Eigen::Matrix<Scalar, 9, 3> drpy2drotmat = drpy2rotmat(rpy);
+    Eigen::Matrix<Scalar, 9, 1> Rdot_vec;
     Rdot_vec = drpy2drotmat * rpydot;
-    Eigen::Matrix3d Rdot = Eigen::Map<Eigen::MatrixXd>(Rdot_vec.data(), 3, 3);
+    Eigen::Matrix<Scalar, 3, 3> Rdot = Eigen::Map< Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> >(Rdot_vec.data(), 3, 3);
 
-    Eigen::Matrix<double, 9, 1> dPhi_x_rpydot_vec;
+    Eigen::Matrix<Scalar, 9, 1> dPhi_x_rpydot_vec;
     dPhi_x_rpydot_vec = dPhi * rpydot;
-    Eigen::Matrix3d dPhi_x_rpydot = Eigen::Map<Eigen::MatrixXd>(dPhi_x_rpydot_vec.data(), 3, 3);
-    Eigen::Vector3d rpy_ddot = Phi * R * pqr_dot + dPhi_x_rpydot * R * pqr + Phi * Rdot * pqr;
+    Eigen::Matrix<Scalar, 3, 3> dPhi_x_rpydot = Eigen::Map< Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> >(dPhi_x_rpydot_vec.data(), 3, 3);
+    Eigen::Matrix<Scalar, 3, 1> rpy_ddot = Phi * R * pqr_dot + dPhi_x_rpydot * R * pqr + Phi * Rdot * pqr;
 
-    Eigen::Matrix<double, 6, 1> qdd;
-    qdd << xyz_ddot, rpy_ddot;
-    Eigen::Matrix<double, 6, 1> qd;
-    qd = x.template segment<6>(6);
-
-    xdot << qd, qdd;
+    xdot << x.template tail<6>(), xyz_ddot, rpy_ddot;
 
     return xdot;
   }
