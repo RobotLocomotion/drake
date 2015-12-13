9345709f9ed36fb1bfe9964e27fc43dee2214614
Maurice Fallon <maurice.fallon@ed.ac.uk>
2015-12-13 18:46:35 +0000

    minor fixes needed for external perturbation test

--- drake/examples/Valkyrie/+valkyrieFrames/ValkyrieInput.m
+++ drake/examples/Valkyrie/+valkyrieFrames/ValkyrieInput.m
@@ -5,6 +5,9 @@ classdef ValkyrieInput < SingletonCoordinateFrame
       typecheck(r,'TimeSteppingRigidBodyManipulator');
 
       manipInputFrame = r.getManipulator().getInputFrame();
+      if (r.external_force ~= 0)
+        manipInputFrame = manipInputFrame.getFrameByNum(1);
+      end
       input_names = manipInputFrame.getCoordinateNames();
       input_names = regexprep(input_names,'_motor',''); % remove motor suffix
 
--- drake/examples/Valkyrie/+valkyrieFrames/ValkyrieState.m
+++ drake/examples/Valkyrie/+valkyrieFrames/ValkyrieState.m
@@ -5,6 +5,10 @@ classdef ValkyrieState < SingletonCoordinateFrame
       typecheck(r,'TimeSteppingRigidBodyManipulator');
       manipStateFrame = r.getManipulator().getStateFrame();
 
+      % sanity check for stateless hands
+      if (r.external_force ~= 0)
+        manipStateFrame = manipStateFrame.getFrameByNum(1);
+      end
       coordinates = manipStateFrame.getCoordinateNames();
       obj = obj@SingletonCoordinateFrame('valkyrieFrames.ValkyrieState',length(coordinates),'x',coordinates);
       positionFrame = r.getManipulator().getPositionFrame();
