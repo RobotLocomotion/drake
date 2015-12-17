0601d410fa90136d06e72eef45a5c5932f2d1998
Hans Susilo <hanssusilo@gmail.com>
2015-12-16 22:50:20 -0500

    completed parameter estimation for acrobot with manipulator eqns

--- drake/examples/Acrobot/test/paramEstSyntheticData.m
+++ drake/examples/Acrobot/test/paramEstSyntheticData.m
@@ -1,67 +1,99 @@
 function paramEstSyntheticData
-mode = 2;
-options.print_result = 0;
 
-noisestd = sqrt([10, 10, 20, 20]);
+% Synthetic parameter estimation mode
+% 0: Base case parameter estimation - intial params = true params
+% 1: Try parameter estimation without any noise
+% 2: Try parameter estimation with measurement noise
+% 3: Try parameter estimation with noise and unknown delay (Not complete)
+mode = 2;
+
+% Method by which to obtain qdd
+% 0: Use acrobot dynamics to estimate true qdd
+% 1: Take the derivative of qd
+qddmode = 0;
+
+% Option to print from estimator: 0=noprint, 1=print
+% Option 0 will still print output of script
+parameterEstimationOptions.print_result = 0;
+
+% Standard deviation of the data input error
+% In this case, for theta1,theta2,theta1dot,theta2dot,theta1doubledot,theta2doubledot
+noisestd = sqrt([.0005, .0005, .0007, .0007, .0012, .0012,]);
+% Standard deviation of the parameter value percent error
+paramstd = 1/5;
 
 r = AcrobotPlant;
-rtest = AcrobotPlant;
-% v = AcrobotVisualizer(r);
-% vtest = AcrobotVisualizer(rtest);
+rtrue = AcrobotPlant;
 
-% Perturb the original parameter estimates with normally dist. random error
-% with variance 
-errstd = 1/10;
-paramerr = randn(1,10)*errstd;
-paramerr(paramerr>1) = 1;
-paramerr(paramerr<-1) = -1;
-% disp(paramerr)
+if mode ~= 0
+    % Perturb original parameter estimates with random percentage error
+    % normally distributed with standard dev = paramstd, and greater than -1
+    paramerr = randn(1,10)*paramstd;
+    while sum(paramerr<=-1)~=0
+        paramerr(paramerr<-1) = randn(1,sum(paramerr<-1))*paramstd;
+    end
 
-rtest.l1 = rtest.l1 + rtest.l1*paramerr(1); 
-rtest.l2 = rtest.l2 + rtest.l2*paramerr(2); 
-rtest.m1 = rtest.m1 + rtest.m1*paramerr(3); 
-rtest.m2 = rtest.m2 + rtest.m2*paramerr(4);  
-rtest.b1 = rtest.b1 + rtest.b1*paramerr(5);
-rtest.b2 = rtest.b2 + rtest.b2*paramerr(6);
-rtest.lc1 = rtest.lc1 + rtest.lc1*paramerr(7); 
-rtest.lc2 = rtest.lc2 + rtest.lc2*paramerr(8); 
-rtest.Ic1 = rtest.Ic1 + rtest.Ic1*paramerr(9);  
-rtest.Ic2 = rtest.Ic2 + rtest.Ic2*paramerr(10);
+    % rtrue.l1 = rtrue.l1 + rtrue.l1*paramerr(1); 
+    % rtrue.l2 = rtrue.l2 + rtrue.l2*paramerr(2); 
+    % rtrue.m1 = rtrue.m1 + rtrue.m1*paramerr(3); 
+    % rtrue.m2 = rtrue.m2 + rtrue.m2*paramerr(4);
+    rtrue.b1  = rtrue.b1 + rtrue.b1*paramerr(5);
+    rtrue.b2  = rtrue.b2 + rtrue.b2*paramerr(6);
+    rtrue.lc1 = rtrue.lc1 + rtrue.lc1*paramerr(7); 
+    rtrue.lc2 = rtrue.lc2 + rtrue.lc2*paramerr(8); 
+    rtrue.Ic1 = rtrue.Ic1 + rtrue.Ic1*paramerr(9);  
+    rtrue.Ic2 = rtrue.Ic2 + rtrue.Ic2*paramerr(10);
+end
 
-
-%% test on swingup up data
-[utraj,xtraj] = swingUpTrajectory(r);
-
+%% Test on swingup up data
+[utraj,xtraj] = swingUpTrajectory(rtrue);
 Ts = .01; breaks=getBreaks(utraj); T0 = breaks(1); Tf = breaks(end);
+tsamples = T0:Ts:Tf;
+xsamples = eval(xtraj,tsamples)';
+usamples = eval(utraj,tsamples)';
 
-% Try parameter estimation without any noise
-if mode ==1
-    data = iddata(eval(xtraj,T0:Ts:Tf)',eval(utraj,T0:Ts:Tf)',Ts,'InputName',r.getInputFrame.getCoordinateNames(),'OutputName',r.getOutputFrame.getCoordinateNames());
-    estimated_parameters = parameterEstimation(rtest,data,options);
+%% Generate second derivative
+nq = r.num_positions;
+if qddmode == 0
+    qdd = zeros(length(tsamples),nq);
+    for i=1:length(tsamples)
+      [H,C,B] = manipulatorDynamics(rtrue,xsamples(i,1:nq)',xsamples(i,nq+(1:nq))');
+      qdd(i,:) = (H\(B*usamples(i,:)' - C))';
+    end
+else
+    % Differentiating to get the second derivative of the state variables
+    % TODO: Try lowpass filter
+    qdd = deriv(xtraj,T0:Ts:Tf)'; qdd = qdd(:,nq+(1:nq));
 end
+xsamples = [xsamples qdd];
 
-% Try parameter estimation with measurement noise
+%% Add gaussian noise to measurements
+xsamplesfinal = xsamples;
 if mode == 2
-    xsamples = eval(xtraj,T0:Ts:Tf)';
-    xnoisysamples = xsamples + randn(size(xsamples))*diag(noisestd);
-%     disp(xnoisysamples-xsamples);
-    usamples = eval(utraj,T0:Ts:Tf)';
-    data = iddata(xnoisysamples,usamples,Ts,'InputName',r.getInputFrame.getCoordinateNames(),'OutputName',r.getOutputFrame.getCoordinateNames());
-    estimated_parameters = parameterEstimation(rtest,data,options);
+    xsamplesfinal = xsamples+randn(size(xsamples))*diag(noisestd);
 end
 
+% % Debugging purposes
+% v = AcrobotVisualizer(r);
+% vtrue = AcrobotVisualizer(rtrue);
+% vtrue.playback(xtraj);
+% 
+% % plot(xsamples(:,1)); hold on; plot(xsamplesfinal(:,1));
+% plot(xsamples); hold on; plot(xsamplesfinal);
+% % Testing to see if estimate is produced by local minima
+% r = rtrue;
 
-% Try parameter estimation with noise and known delay
+outputFrameNames = [r.getOutputFrame.getCoordinateNames(); 'theta1doubledot'; 'theta2doubledot'];
+data = iddata(xsamplesfinal,usamples,Ts,'InputName',r.getInputFrame.getCoordinateNames(),'OutputName',outputFrameNames);
+[estimated_parameters, estimated_delay] = parameterEstimation(r,rtrue,data,parameterEstimationOptions);
 
-
-
-% print out results
-coords = getCoordinateNames(getParamFrame(r));
-p_orig = double(getParams(r));
+%% Print out results
+coords = getCoordinateNames(r.getParamFrame);
+p_true = double(rtrue.getParams);
+p_init = double(r.getParams);
 fprintf('\nParameter estimation results:\n\n');
-fprintf('  Param  \tOriginal\tEstimated\n');
-fprintf('  -----  \t--------\t---------\n');
+fprintf('  Param  \tTrue    \t Initial\tEstimated\n');
+fprintf('  -----  \t--------\t--------\t---------\n');
 for i=1:length(coords)
-  fprintf('%7s  \t%8.2f\t%8.2f\n',coords{i},p_orig(i),estimated_parameters(i));
+  fprintf('%7s  \t%8.2f\t%8.2f\t%8.2f\n',coords{i},p_true(i),p_init(i),estimated_parameters(i));
 end
-% disp(estimated_parameters);
--- drake/systems/plants/@Manipulator/parameterEstimation.m
+++ drake/systems/plants/@Manipulator/parameterEstimation.m
@@ -1,4 +1,4 @@
-function [phat,estimated_delay] = parameterEstimation(obj,data,varargin)
+function [phat,estimated_delay] = parameterEstimation(obj,obj2,data,varargin)
 %
 % Parameter estimation algorithm for manipulators
 %
@@ -28,7 +28,10 @@ function [phat,estimated_delay] = parameterEstimation(obj,data,varargin)
 % 
 % @option print_result determines if the function will print the results
 % 0 = no print, 1 = print estimated and original, 2 = only print estimated
-%
+% 
+% @option method determines which method to use for estimation
+% 0 = no print, 1 = print estimated and original, 2 = only print estimated
+% 
 
 %% handle options
 if (nargin>2 && isstruct(varargin{1})) options=varargin{1};
@@ -36,21 +39,29 @@ else options=struct(); end
 if (~isfield(options,'print_result')) 
   options.print_result=0; 
 end
+if (~isfield(options,'method')) 
+  options.method=0; 
+end
 
 
-
+%% Initialize
 if (getOutputFrame(obj)~=getStateFrame(obj))
   error('Only full-state feedback is implemented so far');
 end
-
 checkDependency('spotless');
 
 nq = obj.num_positions;
 nu = obj.num_u;
 p_orig = double(getParams(obj));  % probably only for testing
+np = length(p_orig);
+
+[pmin,pmax] = getParamLimits(obj);
+assert(all(pmin>=0));  % otherwise I'll have to subtract it out from the coordinates
+%  not hard, just not implmented yet.
+
 
 %%   Step 1: Extract lumped-parameters 
-
+% Initialize Variables
 q=msspoly('q',nq);
 s=msspoly('s',nq);
 c=msspoly('c',nq);
@@ -58,41 +69,28 @@ qt=TrigPoly(q,s,c);
 qd=msspoly('qd',nq);
 qdd=msspoly('qdd',nq);
 u=msspoly('u',nu);
+
+% Set up known parameters
 p=obj.getParamFrame.getPoly;
 pobj = setParams(obj,p);
+
+% Formulate equations of motion
 [H,C,B] = manipulatorDynamics(pobj,qt,qd);
 err = H*qdd + C - B*u;
 
-[a,b,M]=decomp(getmsspoly(err),[q;s;c;qd;qdd;u]);
-coeff = msspoly(ones(size(b,1),1));
-deg_zero_ind=-1;
-for i=1:size(b,1)
-  % would use prod(a'.^b(i,:)) if msspoly implemented prod (bug 1712)
-  for k=find(b(i,:))
-    coeff(i) = coeff(i).*(a(k)^b(i,k));
-  end
-  if (deg(coeff(i))==0) 
-    if (deg_zero_ind>0) error('should only get a single degree zero term'); end
-    deg_zero_ind = i;
-  end
-end
+[lp,M,Mb,lin_params,beta] = identifiableParameters(getmsspoly(err),p); % posynomial lumped params
+% [lp, M, Mb] = linearParameters(getmsspoly(err),p); % monomial lumped params
 
-if (deg_zero_ind>0)
-  Mb = M(:,deg_zero_ind)*double(coeff(deg_zero_ind));
-  M = M(:,[1:deg_zero_ind-1,deg_zero_ind+1:end]);
-  lp = coeff([1:deg_zero_ind-1,deg_zero_ind+1:end]);
-else
-  Mb = zeros(size(err,1));
-  lp = coeff;
-end
-
-np = length(p_orig);
 nlp = length(lp);
 lp_orig = double(subs(lp,p,p_orig));
-lumped_params = msspoly('lp',nlp);
+lumped_params = msspoly('lp',nlp); 
 % now err=M*lp+Mb and lperr=M*lumped_params+Mb;
 
-%%   Step 2: Least-squares estimation
+
+
+%%   Step 2: Nonlinear least-squares estimation
+% 
+% Perform least squares on observable matrices: M_data*lp_est + Mb_data = 0
 
 if (nargin>1)
   % populate A and b matrices from iddata
@@ -102,25 +100,8 @@ if (nargin>1)
   x_data = get(data,'OutputData')';
   q_data = x_data(1:nq,:);
   qd_data = x_data(nq+(1:nq),:);
+  qdd_data = x_data(2*nq+(1:nq),:); 
   u_data = get(data,'InputData')';
-  
-  qdd_data = diff(qd_data,1,2)/Ts;
-  t_data = t_data(:,1:end-1);
-  q_data = q_data(:,1:end-1);
-  qd_data = qd_data(:,1:end-1);
-  u_data = u_data(:,1:end-1);
-else  % temporary... just for debugging
-  n = 1000;
-  t_data = .01*(0:n-1);
-  q_data = randn(nq,n);
-  qd_data = randn(nq,n);
-  u_data = randn(nu,n);
-end
-
-% just for debugging
-for i=1:length(t_data)
-  [H,C,B] = manipulatorDynamics(obj,q_data(:,i),qd_data(:,i));
-  qdd_data(:,i) = (H\(B*u_data(:,i) - C)) + .01*randn(nq,1);
 end
 
 s_data = sin(q_data);
@@ -130,68 +111,20 @@ ndata = length(t_data);
 M_data = reshape(msubs(M(:),[q;s;c;qd;qdd;u],[q_data;s_data;c_data;qd_data;qdd_data;u_data])',nq*ndata,nlp);
 Mb_data = reshape(msubs(Mb,[q;s;c;qd;qdd;u],[q_data;s_data;c_data;qd_data;qdd_data;u_data])',nq*ndata,1);
 
-lp_est = -M_data\Mb_data;
+
+% Nonlinear least-squares solver
+nonlinfun = @(x) M_data*(msubs(lp,p,x))+Mb_data;
+[x, sqerr_est] = lsqnonlin(nonlinfun,p_orig,pmin,pmax);
+
+% Computing the error rate
 err_orig = M_data*lp_orig + Mb_data;
-
-err_est = M_data*lp_est + Mb_data;
 sqerr_orig = sum(err_orig'*err_orig);
-sqerr_est = sum(err_est'*err_est);
 
-%keyboard;
+phat = Point(getParamFrame(obj),x);
 
 
-%%   Step 3: Geometric program to back out original parameters.
-%
-% Note: Now this is actually solved as a QP
-%
-% min | log(p) - log(p_orig) |_2^2 
-%  s.t. forall i, lp_i = lp_est_i
-%                 pmin <= p <= pman
-%
-% decision variables y_i, p_i = e^y_i, log(p_i) = y_i
-%   allows me to write this as 
-% min | y - log(p_orig) |_2^2 
-%  s.t.   lp_i(p) = lp_est_i, 
-%  s.t.   log(pmin) <= p <= log(pmax)
-%  where lp_i is a monomial in p (with coeff = 1)
-%  and taking the log of this constraint gives the equivalent constraint
-%         A y = log(lp_est_i) where
-%  A(i,j) is the power of the monomial in parameter j
-
-A = sparse(nlp,np);
-for i=1:nlp  % todo:  there must be a better way to do this with spotless
-  [a,b,M] = decomp(lp(i));
-  assert(isscalar(M));
-  assert(M==1);
-  for j=1:length(a)
-    ind = find(match(a(j),p));
-    A(i,ind) = b(j);
-  end
-end
-
-irrelevant_params = find(~any(A));
-if ~isempty(irrelevant_params)
-  for i=1:length(irrelevant_params)
-    warning(['Parameter ',getCoordinateName(getParamFrame(obj),irrelevant_params(i)),' does not impact the dynamics in any way.  Consider removing it from the parameter list']);
-  end
-%  A(:,irrelevant_params) = [];
-%  p_map = find(any(A));
-%else
-%  p_map = 1:np;
-end
-
-[pmin,pmax] = getParamLimits(obj);
-assert(all(pmin>=0));  % otherwise I'll have to subtract it out from the coordinates
-%  not hard, just not implmented yet.
-qpopt = optimset('Display','off');
-[log_p_est,~,exitflag] = quadprog(eye(np),-log(p_orig),[],[],A,log(lp_est),log(pmin),log(pmax),log(p_orig),qpopt);
-
-if (exitflag~=1)
-  warning('quadprog exitflag = %d',exitflag);
-end
-
-phat = Point(getParamFrame(obj),exp(log_p_est));
-
+%%   Step 4: Print Results.
+% 
 
 if options.print_result == 1
     coords = getCoordinateNames(getParamFrame(obj));
@@ -210,6 +143,7 @@ elseif options.print_result == 2
       fprintf('%7s  \t%8.2f\n',coords{i},phat(i));
     end
 end
-
+%TODO: calculate estimated_delay
+estimated_delay = 0;
 
 end
