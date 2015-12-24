14b837a1a3f2f849b87513b0932d1abfefc7724a
Russ Tedrake <russt@mit.edu>
2015-12-24 05:29:21 -0500

    working on command line arguments.

--- drake/systems/plants/urdfLCMNode.cpp
+++ drake/systems/plants/urdfLCMNode.cpp
@@ -2,22 +2,44 @@
 #include "LCMSystem.h"
 #include "RigidBodySystem.h"
 #include "BotVisualizer.h"
+#include "drakeAppUtil.h"
 
 using namespace std;
 using namespace Eigen;
 using namespace Drake;
 
+/** urdfLCMNode Application
+ * @ingroup simulation
+ * @brief Loads a urdf and simulates it, subscribing to LCM inputs and publishing LCM outputs
+ *
+ * This application loads a robot from urdf and runs a simulation, with every input
+ * with an LCM type defined subscribed to the associated LCM channels, and every
+ * output with an LCM type defined publishing on the associate channels.  See @ref lcm_vector_concept.
+ *
+ * Usage:  urdfLCMNode [options] full_path_to_urdf_file
+ *   with (case sensitive) options:
+ *     --base [floating_type]  // can be "FIXED, ROLLPITCHYAW,or QUATERNION" (default: QUATERNION)
+ */
+
 int main(int argc, char* argv[]) {
   if (argc < 2) {
-    std::cerr << "Usage: " << argv[0] << " full_path_to_urdf_file" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " [options] full_path_to_urdf_file" << std::endl;
     return 1;
+  }
+
+  // todo: consider moving this logic into the RigidBodySystem class so it can be reused
+  DrakeJoint::FloatingBaseType floating_base_type = DrakeJoint::QUATERNION;
+  char* floating_base_option = getCommandLineOption(argv,argc+argv,"--base");
+  if (floating_base_option) {
+    if (strcmp(floating_base_option,"FIXED")==0) { floating_base_type = DrakeJoint::FIXED; }
+    else if (strcmp(floating_base_option,"RPY")==0) { floating_base_type = DrakeJoint::ROLLPITCHYAW }
   }
 
   shared_ptr<lcm::LCM> lcm = make_shared<lcm::LCM>();
 
-  auto tree = make_shared<RigidBodyTree>(argv[1],DrakeJoint::FIXED);
+  auto tree = make_shared<RigidBodyTree>(argv[argc-1],floating_base_type);
   auto rigid_body_sys = make_shared<RigidBodySystem>(tree);
-  auto visualizer = make_shared<BotVisualizer<RigidBodySystem::StateVector>>(lcm,argv[1],DrakeJoint::FIXED);
+  auto visualizer = make_shared<BotVisualizer<RigidBodySystem::StateVector>>(lcm,argv[argc-1],floating_base_type);
   auto sys = cascade(rigid_body_sys, visualizer);
 
   VectorXd x0 = VectorXd::Random(tree->num_positions+tree->num_velocities);
