0cc5ddf2e663c0e87ff694a638b036f753779856
Twan Koolen <koolen.twan@gmail.com>
2016-01-11 17:29:24 -0600

    Handle nonstandard inertia matrices.

--- drake/util/drakeGeometryUtil.h
+++ drake/util/drakeGeometryUtil.h
@@ -969,60 +969,86 @@ typename Gradient<DerivedX, DerivedDX::ColsAtCompileTime>::type dTransformSpatia
 };
 
 template<typename DerivedI>
+bool isRegularInertiaMatrix(const Eigen::MatrixBase<DerivedI> &I) {
+  using namespace Eigen;
+  using Scalar = typename DerivedI::Scalar;
+  bool ret = true;
+
+  auto J = I.template topLeftCorner<3, 3>();
+  auto cross_part_1 = I.template topRightCorner<3, 3>();
+  auto cross_part_2 = I.template bottomLeftCorner<3, 3>();
+  const auto &m = I(3, 3);
+  ret = ret && (J - J.transpose()).isZero(); // J symmetric
+  ret = ret && (m * Matrix<Scalar, 3, 3>::Identity() - I.template bottomRightCorner<3, 3>()).isZero(); // mass part is a scalar matrix
+  ret = ret && (cross_part_1 - cross_part_2).isZero(); // cross parts transposes of each other
+  ret = ret && (cross_part_1 + cross_part_1.transpose()).isZero();// cross parts skew symmetric
+
+  return ret;
+}
+
+template<typename DerivedI>
 Eigen::Matrix<typename DerivedI::Scalar, TWIST_SIZE, TWIST_SIZE> transformSpatialInertia(
     const Eigen::Transform<typename DerivedI::Scalar, SPACE_DIMENSION, Eigen::Isometry> &T_current_to_new,
     const Eigen::MatrixBase<DerivedI> &I) {
   using namespace Eigen;
   using Scalar = typename DerivedI::Scalar;
 
-  const auto &R = T_current_to_new.linear();
-  const auto &p = T_current_to_new.translation();
+  if (isRegularInertiaMatrix(I)) {
+    // this check is necessary to support the nonstandard inertia matrices resulting from added masses
+    // TODO: SpatialInertiaMatrix class that keeps track of whether matrix is regular or not
+    const auto &R = T_current_to_new.linear();
+    const auto &p = T_current_to_new.translation();
 
-  auto J = I.template topLeftCorner<3, 3>();
-  Matrix<Scalar, 3, 1> c;
-  c << I(2, 4), I(0, 5), I(1, 3);
-  const auto &m = I(3, 3);
+    auto J = I.template topLeftCorner<3, 3>();
+    Matrix<Scalar, 3, 1> c;
+    c << I(2, 4), I(0, 5), I(1, 3);
+    const auto &m = I(3, 3);
 
-  auto vectorToSkewSymmetricSquared = [](const Matrix<Scalar, 3, 1> &a) {
-    Matrix<Scalar, 3, 3> ret;
-    auto a0_2 = a(0) * a(0);
-    auto a1_2 = a(1) * a(1);
-    auto a2_2 = a(2) * a(2);
+    auto vectorToSkewSymmetricSquared = [](const Matrix<Scalar, 3, 1> &a) {
+      Matrix<Scalar, 3, 3> ret;
+      auto a0_2 = a(0) * a(0);
+      auto a1_2 = a(1) * a(1);
+      auto a2_2 = a(2) * a(2);
 
-    ret(0, 0) = -a1_2 - a2_2;
-    ret(0, 1) = a(0) * a(1);
-    ret(0, 2) = a(0) * a(2);
+      ret(0, 0) = -a1_2 - a2_2;
+      ret(0, 1) = a(0) * a(1);
+      ret(0, 2) = a(0) * a(2);
 
-    ret(1, 0) = ret(0, 1);
-    ret(1, 1) = -a0_2 - a2_2;
-    ret(1, 2) = a(1) * a(2);
+      ret(1, 0) = ret(0, 1);
+      ret(1, 1) = -a0_2 - a2_2;
+      ret(1, 2) = a(1) * a(2);
 
-    ret(2, 0) = ret(0, 2);
-    ret(2, 1) = ret(1, 2);
-    ret(2, 2) = -a0_2 - a1_2;
-    return ret;
-  };
+      ret(2, 0) = ret(0, 2);
+      ret(2, 1) = ret(1, 2);
+      ret(2, 2) = -a0_2 - a1_2;
+      return ret;
+    };
 
-  Matrix<Scalar, TWIST_SIZE, TWIST_SIZE> I_new;
-  auto c_new = (R * c).eval();
-  auto J_new = I_new.template topLeftCorner<3, 3>();
+    Matrix<Scalar, TWIST_SIZE, TWIST_SIZE> I_new;
+    auto c_new = (R * c).eval();
+    auto J_new = I_new.template topLeftCorner<3, 3>();
 
-  if (m > NumTraits<Scalar>::epsilon()) {
-    J_new = vectorToSkewSymmetricSquared(c_new);
-    c_new.noalias() += m * p;
-    J_new -= vectorToSkewSymmetricSquared(c_new);
-    J_new /= m;
+    if (m > NumTraits<Scalar>::epsilon()) {
+      J_new = vectorToSkewSymmetricSquared(c_new);
+      c_new.noalias() += m * p;
+      J_new -= vectorToSkewSymmetricSquared(c_new);
+      J_new /= m;
+    }
+    else {
+      J_new.setZero();
+    }
+    J_new.noalias() += R * J.template selfadjointView<Lower>() * R.transpose();
+
+    I_new.template topRightCorner<3, 3>() = vectorToSkewSymmetric(c_new);
+    I_new.template bottomLeftCorner<3, 3>() = -I_new.template topRightCorner<3, 3>();
+    I_new.template bottomRightCorner<3, 3>() = I.template bottomRightCorner<3, 3>();
+
+    return I_new;
   }
   else {
-    J_new.setZero();
+    auto I_half_transformed = transformSpatialForce(T_current_to_new, I);
+    return transformSpatialForce(T_current_to_new, I_half_transformed.transpose());
   }
-  J_new.noalias() += R * J * R.transpose();
-
-  I_new.template topRightCorner<3, 3>() = vectorToSkewSymmetric(c_new);
-  I_new.template bottomLeftCorner<3, 3>() = -I_new.template topRightCorner<3, 3>();
-  I_new.template bottomRightCorner<3, 3>() = I.template bottomRightCorner<3, 3>();
-
-  return I_new;
 }
 
 template<typename DerivedA, typename DerivedB>
