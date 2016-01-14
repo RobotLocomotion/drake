e076191d8172aae02b990f65e2faaa96c1bbbcfc
Hans Susilo <hanssusilo@gmail.com>
2016-01-14 16:56:03 -0500

    deleted old debugging code

--- drake/examples/Acrobot/test/paramEst_test.m
+++ drake/examples/Acrobot/test/paramEst_test.m
@@ -1,140 +1,103 @@
 function paramEst_test
+global PTRUE
 tmp = addpathTemporary(fullfile(pwd,'..'));
 
-% Synthetic parameter estimation mode
-% 'base':       Base case parameter estimation - intial params = true params
-% 'paramerr':   w/ parameter error but no measurement noise
-% 'measnoise':  w/ parameter error and w/ measurement noise
-% 'delay':      w/ parameter error and w/ measurement noise and delay (Not complete)
-mode = 'paramerr';
+parameterEstimationOptions.method = 'nonlinprog'; % nonlinear least squares
+parameterEstimationOptions.C = eye(4); % The symmetric positive definite cost matrix for simerr
+paramstd = 0.1; % Standard deviation of the parameter value percent error
+noisestd = sqrt([.00025, .00025, .00035, .00035]); % Noise standard deviation for theta1,theta2,theta1dot,theta2dot
 
-% Parameter Estimation model
-% 'dynamic'     = use dynamic model - requires qdd
-% 'energetic'   = use energetic model - doesn't require qdd
-% 'simerr'      = use simulation error - doesn't require qdd
-parameterEstimationOptions.model = 'dynamic';
+numTests = 20;
+models = {'simerr','dynamic','energetic'};
+% models = {'dynamic','energetic'};
+mlen = length(models);
 
-% Method by which to obtain qdd (only used in dynamic model)
-% 'manipul':   Use acrobot manipulator equations to estimate true qdd
-% 'derivative': Take the derivative of qd
-qddmode = 'derivative';
-
-% Parameter Estimation method
-% 'nonlinprog'  = nonlinear least squares (LS) to solve problem
-% 'linprog'       = linear LS on lumped params then nonlinear LS to recover
-%                 original parameters
-% 'lsqnonlin'   = use MATLAB's built-in nonlinear least squares solver (debugging)
-parameterEstimationOptions.method = 'nonlinprog';
-
-% Option to print from estimator
-% 'noprint'     = Do not print output from parameterEstimation.m, but will 
-%                 still print output of paramEstSyntheticData.m script
-% 'printEst'    = only print estimated from parameterEstimation.m
-% 'printAll'	= print estimated and original from parameterEstimation.m
-parameterEstimationOptions.print_result = 'noprint';
-
-% Standard deviation of the data input error
-if strcmp(parameterEstimationOptions.model,'energetic')
-    % In this case, for theta1,theta2,theta1dot,theta2dot
-    noisestd = sqrt([.0005, .0005, .0007, .0007]);
-else
-    % In this case, for theta1,theta2,theta1dot,theta2dot,theta1doubledot,theta2doubledot
-    noisestd = sqrt([.0005, .0005, .0007, .0007, .0012, .0012,]);
-end
-% Standard deviation of the parameter value percent error
-paramstd = 1/5;
-
-r = AcrobotPlant;
 rtrue = AcrobotPlant;
-nq = r.num_positions;
-% The symmetric positive definite cost matrix for simerr
-% Err=sum((xobs(n)-xhat(n))'*C*(xobs(n)-xhat(n)))
-C = eye(2*nq);
-
-if ~strcmp(mode,'base')
-    % Perturb original parameter estimates with random percentage error
-    % normally distributed with standard dev = paramstd, and greater than -1
-    paramerr = randn(1,10)*paramstd;
-    while sum(paramerr<=-1)~=0
-        paramerr(paramerr<-1) = randn(1,sum(paramerr<-1))*paramstd;
-    end
-
-    % rtrue.l1 = rtrue.l1 + rtrue.l1*paramerr(1); 
-    % rtrue.l2 = rtrue.l2 + rtrue.l2*paramerr(2); 
-    % rtrue.m1 = rtrue.m1 + rtrue.m1*paramerr(3); 
-    % rtrue.m2 = rtrue.m2 + rtrue.m2*paramerr(4);
-    rtrue.b1  = rtrue.b1 + rtrue.b1*paramerr(5);
-    rtrue.b2  = rtrue.b2 + rtrue.b2*paramerr(6);
-    rtrue.lc1 = rtrue.lc1 + rtrue.lc1*paramerr(7); 
-    rtrue.lc2 = rtrue.lc2 + rtrue.lc2*paramerr(8); 
-    rtrue.Ic1 = rtrue.Ic1 + rtrue.Ic1*paramerr(9);  
-    rtrue.Ic2 = rtrue.Ic2 + rtrue.Ic2*paramerr(10);
-end
-
-outputFrameNames = r.getOutputFrame.getCoordinateNames();
+p_true = double(rtrue.getParams);
+PTRUE = p_true;
 
 %% Test on swingup up data
 [utraj,xtraj] = swingUpTrajectory(rtrue);
-Ts = .01; breaks=getBreaks(utraj); T0 = breaks(1); Tf = breaks(end);
+Ts = .001; breaks=getBreaks(utraj); T0 = breaks(1); Tf = breaks(end);
 tsamples = T0:Ts:Tf;
 usamples = eval(utraj,tsamples)';
+xsamples = computeTraj(rtrue,eval(xtraj,T0),usamples',tsamples)'; % Produce state traj through forward euler method
 
-% % Produce observations through direct collocation simulation
-% xsamples = eval(xtraj,tsamples)';
-% Produce observations through forward euler method
-xsamples = computeTraj(rtrue,eval(xtraj,T0),usamples',tsamples)';
+r = AcrobotPlant;
+nq = r.num_positions;
+outputFrameNames = [r.getOutputFrame.getCoordinateNames();'theta1doubledot';'theta2doubledot'];
+paramNames = getCoordinateNames(r.getParamFrame);
+pmin = [0.02; 0.02; 0.1; 0.1; 0.05; 0.1;];
+pmax = [1; 1; 2; 2; 1; 1;];
+r = r.setParamLimits(pmin,pmax);
 
-%% Generate second derivative
-if ~strcmp(parameterEstimationOptions.model,'energetic')
-    if strcmp(qddmode,'manipul')
-        qdd = zeros(length(tsamples),nq);
-        for j=1:length(tsamples)
-          [H,C,B] = manipulatorDynamics(rtrue,xsamples(j,1:nq)',xsamples(j,nq+(1:nq))');
-          qdd(j,:) = (H\(B*usamples(j,:)' - C))';
-        end
-    else
-        % Differentiating to get the second derivative of the state variables
-        % TODO: Try lowpass filter
-        qdd = deriv(xtraj,T0:Ts:Tf)'; qdd = qdd(:,nq+(1:nq));
+simerror = zeros(mlen,numTests);
+simtime = zeros(mlen,numTests);
+simflag = zeros(mlen,numTests);
+p_init = zeros(length(p_true),numTests);
+p_est = cell(1,mlen);
+[p_est{:}] = deal(zeros(length(p_true),numTests));
+for j=1:numTests
+    %% Perturb original parameter estimates with random percentage error
+    % normally distributed with standard dev = paramstd, and greater than -1
+    paramerr = randn(1,6)*paramstd;
+    while sum(paramerr<=-1)>0
+        paramerr(paramerr<-1) = randn(1,sum(paramerr<-1))*paramstd;
     end
-    xsamples = [xsamples qdd];
-    outputFrameNames = [outputFrameNames;'theta1doubledot';'theta2doubledot'];
-end
+    r.b1  = r.b1 + r.b1*paramerr(1);
+    r.b2  = r.b2 + r.b2*paramerr(2);
+    r.lc1 = r.lc1 + r.lc1*paramerr(3); 
+    r.lc2 = r.lc2 + r.lc2*paramerr(4); 
+    r.Ic1 = r.Ic1 + r.Ic1*paramerr(5);  
+    r.Ic2 = r.Ic2 + r.Ic2*paramerr(6);
 
-%% Add gaussian noise to measurements
-if strcmp(mode,'measnoise') || strcmp(mode,'delay')
+    p_init(:,j) = double(r.getParams);
+    
+    %% Add gaussian noise to measurements
     measurementNoise = randn(size(xsamples))*diag(noisestd);
-else
-    measurementNoise = 0;
-end
-xsamplesfinal = xsamples+measurementNoise;
-data = iddata(xsamplesfinal,usamples,Ts,'InputName',r.getInputFrame.getCoordinateNames(),'OutputName',outputFrameNames);
+    xsamplesnoisy = xsamples+measurementNoise;
 
+    %% Generate second derivative
+    % Differentiating to get the second derivative of the state variables
+    dtsamples = diff(tsamples)';
+    qdd = diff(xsamplesnoisy(:,nq+(1:nq)),1,1)./repmat(dtsamples,1,nq);
+    xsamplesfinal = [xsamplesnoisy(1:length(qdd),:), qdd];
+    data = iddata(xsamplesfinal,usamples(1:length(qdd),:),Ts,'InputName',r.getInputFrame.getCoordinateNames(),'OutputName',outputFrameNames);
 
-%% Compute results from each model
-coords = getCoordinateNames(r.getParamFrame);
-p_true = double(rtrue.getParams);
-p_init = double(r.getParams);
-
-models = {'dynamic','simerr','energetic'};
-mlen = length(models);
-estimated_parameters = zeros(length(p_true),mlen);
-simerror = zeros(1,mlen);
-simtime = zeros(1,mlen);
-for i=1:mlen
-    parameterEstimationOptions.model = models{i};
-    tic;
-    [estimated_parameters(:,i),simerror(:,i)] = parameterEstimation(r,data,parameterEstimationOptions);
-    simtime(:,i) = toc;
-    %% Print out results
-    fprintf('\nParameter estimation results for %s:\n\n',models{i});
-    fprintf('  Param  \tTrue    \t Initial\tEstimated\n');
-    fprintf('  -----  \t--------\t--------\t---------\n');
-    for j=1:length(coords)
-      fprintf('%7s  \t%8.2f\t%8.2f\t%8.2f\n',coords{j},p_true(j),p_init(j),estimated_parameters(j,i));
+    %% Compute results from each model    
+    for i=1:mlen
+        parameterEstimationOptions.model = models{i};
+        tic;
+        [p_est{i}(:,j),simerror(i,j),~,simflag(i,j)] = parameterEstimation(r,data,parameterEstimationOptions);
+        simtime(i,j) = toc;
+% %         DEBUGGING SNOPT FAILURES
+%         if simflag(i,j)==41
+%             fprintf('parameterEstimation failed\n');
+%             tic;
+%             [p_est{i}(:,j),simerror(i,j),~,simflag(i,j)] = parameterEstimation(r,data,parameterEstimationOptions);
+%             simtime(i,j) = toc;
+%         end
+        
+        fprintf('n: %i \tModel: %10s \tTime: %f \tSim Error: %f\n',j,models{i},simtime(i,j),simerror(i,j));
+%         %% Print out results
+%         fprintf('\nParameter estimation results for %s:\n\n',models{i});
+%         fprintf('  Param  \tTrue    \t Initial\tEstimated\n');
+%         fprintf('  -----  \t--------\t--------\t---------\n');
+%         for k=1:length(paramNames)
+%           fprintf('%7s  \t%8.2f\t%8.2f\t%8.2f\n',paramNames{k},p_true(k),p_init(k),p_est{i}(k,j));
+%         end
     end
 end
-disp(simerror)
+disp(simerror);
+disp(simtime);
+simerrInd = find(strcmp(models,'simerr')==1);
+meanSimerror = mean(simerror,2);
+meanSimerror(simerrInd) = mean(simerror(simerrInd,simflag(simerrInd,:)==1));
+meanSimtime = mean(simtime,2);
+meanSimtime(simerrInd) = mean(simtime(simerrInd,simflag(simerrInd,:)==1));
+disp(meanSimerror);
+disp(meanSimtime);
+% save('data1_001','simerror','simtime','simflag','p_true','p_init','p_est');
 end
 
 function xtraj = computeTraj(obj,x0,utraj,t)
@@ -147,3 +110,41 @@ function xtraj = computeTraj(obj,x0,utraj,t)
         xtraj(:,i+1) = xtraj(:,i)+f*dt(i);
     end
 end
+
+%% DEBUGGING CODE
+% qddTrue = qdd;
+% qdd = deriv(xtraj,T0:Ts:Tf)'; qdd = qdd(:,nq+(1:nq));
+% figure;plot(qddTrue(:,1),'-g'); hold on; plot(qdd(:,1),'-r')
+% title(['q_1 True Acceleration vs. Derivative of Velocity']);
+% xlabel('Sample Time')
+% ylabel({'Acceleration Magnitude' '(m/s)/sample'})
+% legend('True Acceleration','Derivative of Vel');
+
+% v = AcrobotVisualizer(r);
+% vtrue = AcrobotVisualizer(rtrue);
+% vtrue.playback(xtraj);
+% 
+% % plot(xsamples(:,1)); hold on; plot(xsamplesfinal(:,1));
+% plot(xsamples); hold on; plot(xsamplesfinal);
+% % Testing to see if estimate is produced by local minima
+% r = rtrue;
+
+% FOR DEBUGGING TRAJECTORIES
+% vtrue = AcrobotVisualizer(rtrue);
+% vtrue.playback(xtraj);
+% xtraj2pp = PPTrajectory(zoh(tsamples,xtraj2));
+% xtraj2pp = xtraj2pp.setOutputFrame(xtraj.getOutputFrame);
+% vtrue.playback(xtraj2pp);
+% xtraj3 = computeTraj(r,xsamples(1,:)',usamples',tsamples);
+% xtraj3pp = PPTrajectory(zoh(tsamples,xtraj3));
+% xtraj3pp = xtraj3pp.setOutputFrame(xtraj.getOutputFrame);
+% vtrue.playback(xtraj3pp);
+% % figure; comet(xtraj2(1,:),xtraj2(2,:));
+% % hold on; comet(xsamples(:,1)',xsamples(:,2)');
+% figure; plot(xsamples(:,1));
+% figure; plot(xsamples(:,2));
+% figure; plot(xsamples(:,1)',xsamples(:,2)');
+% hold on; plot(xtraj2(1,:),xtraj2(2,:));
+% figure; plot(xsamples(:,1)',xsamples(:,2)');
+% hold on; plot(xtraj2(1,:),xtraj2(2,:));
+% plot(xtraj3(1,:),xtraj3(2,:));
