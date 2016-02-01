36d20ccd2bae8a8ac7bbb9a6edaab97437eecc55
Robin Deits <robin.deits@gmail.com>
2016-02-01 17:52:19 -0500

    remove matlab qpInput implementation

--- drake/systems/controllers/+bipedControllers/BipedPlanEvalAndControlSystem.m
+++ drake/systems/controllers/+bipedControllers/BipedPlanEvalAndControlSystem.m
@@ -71,10 +71,7 @@ classdef BipedPlanEvalAndControlSystem < DrakeSystem
           t0 = tic();
         end
         qp_input_obj = obj.plan_eval.getQPControllerInput(t, x);
-        if isa(qp_input_obj, 'bipedControllers.QPInputConstantHeight')
-          qp_input_msg_data = encodeQPInputLCMMex(qp_input_obj, false);
-          qp_input_msg = drake.lcmt_qp_controller_input(qp_input_msg_data);
-        elseif isnumeric(qp_input_obj)
+        if isnumeric(qp_input_obj)
           qp_input_msg = drake.lcmt_qp_controller_input(qp_input_obj);
         else
           qp_input_msg = qp_input_obj;
