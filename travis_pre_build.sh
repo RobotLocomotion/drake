a0ed27c07d0c9e15d1ea645283e9b7b9ac1f28ea
Pat Marion <james.patrick.marion@gmail.com>
2016-02-03 21:27:59 -0500

    add command to install vtk from a homebrew bottle

--- travis_pre_build.sh
+++ travis_pre_build.sh
@@ -5,6 +5,15 @@ if [ "$CXX" = "g++" ]
 	export CXX="g++-4.8" CC="gcc-4.8"
 fi
 
+install_vtk_homebrew_bottle()
+{
+  wget https://dl.dropboxusercontent.com/s/mtacwrjgmtanfaz/vtk5-5.10.1_2.mavericks.bottle.1.tar.gz
+  brew tap homebrew/science
+  brew install qt
+  brew install vtk5-5.10.1_2.mavericks.bottle.1.tar.gz
+}
+
+
 if [ "$TRAVIS_OS_NAME" = "linux" ]
 	then
 	sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
@@ -20,6 +29,7 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]
 	export CMAKE_FLAGS="-DWITH_SPOTLESS:BOOL=OFF -DWITH_LIBBOT:BOOL=ON -DWITH_DIRECTOR:BOOL=OFF -DWITH_IRIS:BOOL=ON -DWITH_OCTOMAP:BOOL=ON -DWITH_MOSEK:BOOL=ON -DWITH_AVL:BOOL=ON -DWITH_XFOIL:BOOL=ON"
 	mkdir build
 	make download-all > download.log || (cat download.log && exit 1)
+	install_vtk_homebrew_bottle
 	./install_prereqs.sh homebrew
 else
 	echo "WARNING: TRAVIS_OS_NAME: $TRAVIS_OS_NAME is not handled, not installing prereqs"
