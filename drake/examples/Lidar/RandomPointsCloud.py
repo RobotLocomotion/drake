34b6ab248d8eae6971676aeb5ed1d73c43dca7f6
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-02-09 15:05:31 -0500

    Improve lidar support
    Modify lidar messages to include scan angle and direction (the latter is
    needed for bidirectional scanning sensors). Modify simulator to produce
    one message per laser pulse, rather than one message per complete sweep.
    This give us the ability to consume a sensor stream directly (with lower
    latency).

--- drake/examples/Lidar/RandomPointsCloud.py
+++ drake/examples/Lidar/RandomPointsCloud.py
@@ -5,20 +5,26 @@ from drake.lcmt_lidar_data import lcmt_lidar_data as lcm_msg
 if __name__ == '__main__':
     comm = lcm.LCM()
 
+    t = 0
     while True:
+        t = (t + 0.015) % (2.0 * math.pi)
+
         msg = lcm_msg()
-        msg.num_points = random.randint(100, 1000)
-        for k in xrange(msg.num_points):
-            p = lcm_point()
-            r = random.uniform(0, 500)
-            t = random.uniform(0, 2*math.pi)
-            p.x = r * math.sin(t)
-            p.y = r * math.cos(t)
-            p.z = random.uniform(0, 50)
-            p.intensity = random.random()
-            p.scan = k % 16
-            p.echo = random.choice([0,0,0,0,0,1,1,1,2,2,3])
-            msg.points.append(p)
+        for k in xrange(16):
+            for n in range(random.choice([1,1,1,1,1,2,2,2,3,3,4])):
+                p = lcm_point()
+                r = random.uniform(0, 500)
+                p.x = r * math.sin(t)
+                p.y = r * math.cos(t)
+                p.z = r * k / 80
+                p.intensity = random.random()
+                p.scan = k
+                p.echo = n + 1
+                msg.points.append(p)
+
+        msg.timestamp = time.time() * 1e6
+        msg.num_points = len(msg.points)
+        msg.scan_angle = t
+        msg.scan_direction = False
 
         comm.publish("DRAKE_POINTCLOUD_LIDAR_EXAMPLE_RANDOM", msg.encode())
-        time.sleep(0.1)
