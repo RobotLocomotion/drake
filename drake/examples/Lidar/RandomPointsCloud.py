02f90571ea6e8bffda9f016b26834a7c323df131
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-02-11 14:53:13 -0500

    Use dynamic fields for lidar lcm types
    Replace lcmt_lidar_point, having fixed fields, with dynamic fields in
    the lcmt_lidar_data using new lcmt_lidar_field. This makes it easier for
    sensors to provide only relevant data, and to add additional fields in
    the future as needed.

--- drake/examples/Lidar/RandomPointsCloud.py
+++ drake/examples/Lidar/RandomPointsCloud.py
@@ -1,29 +1,65 @@
 import lcm, math, random, sys, time
-from drake.lcmt_lidar_point import lcmt_lidar_point as lcm_point
+from drake.lcmt_lidar_field import lcmt_lidar_field as lcm_field
 from drake.lcmt_lidar_data import lcmt_lidar_data as lcm_msg
+
+#------------------------------------------------------------------------------
+def add_field(msg, field, data):
+    field.values = data;
+    field.num_values = len(data)
+
+    msg.fields.append(field)
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if __name__ == '__main__':
     comm = lcm.LCM()
+
+    position_field = lcm_field()
+    position_field.id = lcm_msg.POSITION
+
+    range_field = lcm_field()
+    range_field.id = lcm_msg.RANGE
+
+    intensity_field = lcm_field()
+    intensity_field.id = lcm_msg.INTENSITY
+
+    scan_field = lcm_field()
+    scan_field.id = lcm_msg.SCAN
+
+    echo_field = lcm_field()
+    echo_field.id = lcm_msg.ECHO
 
     t = 0
     while True:
         t = (t + 0.015) % (2.0 * math.pi)
 
+        range_data = []
+        position_data = []
+        intensity_data = []
+        scan_data = []
+        echo_data = []
+
         msg = lcm_msg()
         for k in xrange(16):
             for n in range(random.choice([1,1,1,1,1,2,2,2,3,3,4])):
-                p = lcm_point()
+                p = k / 40.0
                 r = random.uniform(0, 500)
-                p.x = r * math.sin(t)
-                p.y = r * math.cos(t)
-                p.z = r * k / 80
-                p.intensity = random.random()
-                p.scan = k
-                p.echo = n + 1
-                msg.points.append(p)
+                range_data.append(r)
+                position_data.append(r * math.sin(t) * math.cos(p))
+                position_data.append(r * math.cos(t) * math.cos(p))
+                position_data.append(r * math.sin(p))
+                intensity_data.append(random.random())
+                scan_data.append(k)
+                echo_data.append(n + 1)
+
+        add_field(msg, position_field, position_data)
+        add_field(msg, range_field, range_data)
+        add_field(msg, intensity_field, intensity_data)
+        add_field(msg, scan_field, scan_data)
+        add_field(msg, echo_field, echo_data)
+        msg.num_fields = len(msg.fields)
 
         msg.timestamp = time.time() * 1e6
-        msg.num_points = len(msg.points)
         msg.scan_angle = t
         msg.scan_direction = False
 
