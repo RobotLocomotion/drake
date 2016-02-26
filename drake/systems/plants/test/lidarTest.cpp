eebfa9715525fb98b0d8aefa48a2858948e25326
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-02-26 12:02:07 -0500

    Publish lidar message from test

--- drake/systems/plants/test/lidarTest.cpp
+++ drake/systems/plants/test/lidarTest.cpp
@@ -1,5 +1,6 @@
 
 #include <cmath>
+#include "lcmtypes/bot_core/planar_lidar_t.hpp"
 #include "drake/systems/LCMSystem.h"
 #include "drake/systems/plants/RigidBodySystem.h"
 #include "drake/systems/plants/BotVisualizer.h"
@@ -40,8 +41,21 @@ int main(int argc, char* argv[])
     }
   }
 
-  // render for debugging:
   auto lcm = make_shared<lcm::LCM>();
+
+  bot_core::planar_lidar_t msg;
+  msg.utime = -1;
+  msg.nintensities = 0;
+  msg.rad0 = min_yaw;
+  msg.radstep = (max_yaw - min_yaw) / (distances.size() - 1);
+
+  msg.nranges = distances.size();
+  for (int i=0; i<distances.size(); i++)
+    msg.ranges.push_back(distances(i));
+
+  lcm->publish("DRAKE_POINTCLOUD_LIDAR_TEST", &msg);
+
+  // render for debugging:
   auto visualizer = make_shared<BotVisualizer<RigidBodySystem::StateVector>>(lcm,rigid_body_sys->getRigidBodyTree());
   visualizer->output(t,x,u);
 
