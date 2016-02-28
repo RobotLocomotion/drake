3d6689f3dcdc9511e15edc3f1eddf4df505e0587
psiorx <psiorx@gmail.com>
2016-02-28 14:31:06 -0500

    wrote QuadControl class and lcm decode method

--- drake/examples/Quadrotor/runDynamics.cpp
+++ drake/examples/Quadrotor/runDynamics.cpp
@@ -4,10 +4,46 @@
 #include "BotVisualizer.h"
 #include "RigidBodySystem.h"
 #include "LCMSystem.h"
+#include "lcmtypes/drake/lcmt_quadrotor_control_t.hpp"
 
 using namespace std;
 using namespace Drake;
 using namespace Eigen;
+
+template <typename ScalarType = double>
+class QuadrotorControl {
+public:
+  typedef drake::lcmt_quadrotor_control_t LCMMessageType;
+  static std::string channel() { return "QUAD_CONTROL"; };
+  
+  template <typename Derived>
+  QuadrotorControl(const Eigen::MatrixBase<Derived>& x) : motors(x) {};
+
+  template <typename Derived>
+  QuadrotorControl& operator=(const Eigen::MatrixBase<Derived>& x) {
+    motors = x;
+    return *this;
+  }
+
+  friend Eigen::Vector4d toEigen(const QuadrotorControl<ScalarType>& vec) {    
+    return vec.motors;
+  }
+
+  friend std::string getCoordinateName(const QuadrotorControl<ScalarType>& vec, unsigned int index) {    
+    return index >= 0 && index < RowsAtCompileTime ? std::string("motor") + std::to_string(index) : std::string("error");
+  }
+
+  const static int RowsAtCompileTime = 4;
+
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  Eigen::Matrix<ScalarType, 4, 1> motors;
+};
+
+bool decode(const drake::lcmt_quadrotor_control_t& msg, double& t, QuadrotorControl<double>& x) {
+  t = double(msg.timestamp)/1000.0;
+  x.motors = Eigen::Vector4d(msg.motors);
+  return true;
+}
 
 int main(int argc, char* argv[]) {
   shared_ptr<lcm::LCM> lcm = make_shared<lcm::LCM>();
