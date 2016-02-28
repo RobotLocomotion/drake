5dd6bef5f64689d4a7583480d1c0f2f126a215a2
psiorx <psiorx@gmail.com>
2016-02-28 18:05:17 -0500

    added quadrotor state lcmtype and vector class

--- drake/examples/Quadrotor/runDynamics.cpp
+++ drake/examples/Quadrotor/runDynamics.cpp
@@ -1,11 +1,11 @@
 
 #include <iostream>
-#include "Quadrotor.h"
 #include "BotVisualizer.h"
 #include "RigidBodySystem.h"
 #include "LCMSystem.h"
 #include "LinearSystem.h"
 #include "lcmtypes/drake/lcmt_quadrotor_control_t.hpp"
+#include "lcmtypes/drake/lcmt_quadrotor_state_t.hpp"
 
 using namespace std;
 using namespace Drake;
@@ -18,7 +18,8 @@ public:
   static std::string channel() { return "QUAD_CONTROL"; };
   
   QuadrotorControl(void) {
-    motors.setZero();
+    //motors.setZero();
+    motors << 1.26,1.26,1.26,1.26;
   }
 
   template <typename Derived>
@@ -35,13 +36,52 @@ public:
   }
 
   friend std::string getCoordinateName(const QuadrotorControl<ScalarType>& vec, unsigned int index) {    
-    return index >= 0 && index < RowsAtCompileTime ? std::string("motor") + std::to_string(index) : std::string("error");
+    return index >= 0 && index < RowsAtCompileTime ? std::string("motor") + std::to_string(index + 1) : std::string("error");
   }
 
   const static int RowsAtCompileTime = 4;
 
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
   Eigen::Matrix<ScalarType, 4, 1> motors;
+};
+
+template <typename ScalarType = double>
+class QuadrotorState {
+public:
+  typedef drake::lcmt_quadrotor_state_t LCMMessageType;
+  static std::string channel() { return "QUAD_STATE"; };
+  
+  QuadrotorState(void) {
+    state.setZero();
+  }
+
+  template <typename Derived>
+  QuadrotorState(const Eigen::MatrixBase<Derived>& x) : state(x) {};
+
+  template <typename Derived>
+  QuadrotorState& operator=(const Eigen::MatrixBase<Derived>& x) {
+    state = x;
+    return *this;
+  }
+
+  friend Eigen::Matrix<ScalarType, 13, 1> toEigen(const QuadrotorState<ScalarType>& vec) {    
+    return vec.state;
+  }
+
+  friend std::string getCoordinateName(const QuadrotorState<ScalarType>& vec, unsigned int index) {    
+    static const std::vector<std::string> coordinate_names = 
+    {"x","y","z",
+    "qx","qy","qz","qw",
+    "xdot", "ydot", "zdot",
+    "angvel_x", "angvel_y", "angvel_z"
+    };
+    return index >= 0 && index < RowsAtCompileTime ? coordinate_names[index] : std::string("error");
+  }
+
+  const static int RowsAtCompileTime = 13;
+
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  Eigen::Matrix<ScalarType, 13, 1> state;
 };
 
 bool decode(const drake::lcmt_quadrotor_control_t& msg, double& t, QuadrotorControl<double>& x) {
@@ -59,21 +99,21 @@ int main(int argc, char* argv[]) {
   auto rigid_body_sys = make_shared<RigidBodySystem>(getDrakePath()+"/examples/Quadrotor/quadrotor.urdf",floating_base_type);
   auto const & tree = rigid_body_sys->getRigidBodyTree();
 
-  double box_width = 1000;
-  double box_depth = 10;
-  DrakeShapes::Box geom(Vector3d(box_width, box_width, box_depth));
-  Isometry3d T_element_to_link = Isometry3d::Identity();
-  T_element_to_link.translation() << 0,0,-box_depth/2;  // top of the box is at z=0
-  auto & world = tree->bodies[0];
-  Vector4d color;  color <<  0.9297, 0.7930, 0.6758, 1;  // was hex2dec({'ee','cb','ad'})'/256 in matlab
-  world->addVisualElement(DrakeShapes::VisualElement(geom,T_element_to_link,color));
-  tree->addCollisionElement(RigidBody::CollisionElement(geom,T_element_to_link,world),world,"terrain");
-  tree->updateStaticCollisionElements();
+  // double box_width = 1000;
+  // double box_depth = 10;
+  // DrakeShapes::Box geom(Vector3d(box_width, box_width, box_depth));
+  // Isometry3d T_element_to_link = Isometry3d::Identity();
+  // T_element_to_link.translation() << 0,0,-box_depth/2;  // top of the box is at z=0
+  // auto & world = tree->bodies[0];
+  // Vector4d color;  color <<  0.9297, 0.7930, 0.6758, 1;  // was hex2dec({'ee','cb','ad'})'/256 in matlab
+  // world->addVisualElement(DrakeShapes::VisualElement(geom,T_element_to_link,color));
+  // tree->addCollisionElement(RigidBody::CollisionElement(geom,T_element_to_link,world),world,"terrain");
+  // tree->updateStaticCollisionElements();
 
   auto visualizer = make_shared<BotVisualizer<RigidBodySystem::StateVector>>(lcm,tree);
   
   auto sys_with_lcm_input = cascade(make_shared<Gain<QuadrotorControl, RigidBodySystem::InputVector>>(Eigen::Matrix4d::Identity()), rigid_body_sys);
-  
+
   auto sys_with_vis = cascade(sys_with_lcm_input, visualizer);
 
   SimulationOptions options = default_simulation_options;
@@ -84,7 +124,7 @@ int main(int argc, char* argv[]) {
   x0.head(tree->num_positions) = tree->getZeroConfiguration();
 
   x0(2) = 0.1;
-
+  cout << x0.size() << " states" << endl;
   runLCM(sys_with_vis, lcm,0,std::numeric_limits<double>::infinity(),x0,options);
   
 }
