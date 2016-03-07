21a40e4df3685b3c7c52227327d23e52c83929df
Russ Tedrake <russt@mit.edu>
2016-03-06 21:39:40 -0500

    drake version of crf

--- drake/util/crm.m
+++ drake/util/crm.m
@@ -2,6 +2,6 @@ function vcross = crm(v)
 
 % spatial cross-product operator (motion)
 
-vcross = [ vectorToSkewSymmetric(v(1:3)), zeros(3); 
+vcross = [ vectorToSkewSymmetric(v(1:3)), zeros(3); ...
            vectorToSkewSymmetric(v(4:6)), vectorToSkewSymmetric(v(1:3)) ];
          
