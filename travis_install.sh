00de8fce3de1243b073fb1a516f49dc739fc993f
Jamie Snape <jamie.snape@kitware.com>
2016-03-15 10:27:33 -0400

    Direct "brew update" output to file during Travis CI builds

--- travis_install.sh
+++ travis_install.sh
@@ -1,6 +1,6 @@
 set -e
 mkdir -p $TRAVIS_BUILD_DIR/build
-brew update
+brew update > update.log
 export CMAKE_FLAGS="-DWITH_AVL:BOOL=ON -DWITH_DIRECTOR:BOOL=OFF -DWITH_IRIS:BOOL=ON -DWITH_LIBBOT:BOOL=ON -DWITH_MOSEK:BOOL=ON -DWITH_OCTOMAP:BOOL=ON -DWITH_SPOTLESS:BOOL=OFF -DWITH_XFOIL:BOOL=ON"
 make download-all > download.log || (cat download.log && exit 1)
 ./install_prereqs.sh homebrew
