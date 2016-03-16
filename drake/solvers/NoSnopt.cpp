ae57cd6111ecb7d4023cde15127443ae5a533cfa
Lucy Gibson <lucy.gibson@tri.global>
2016-03-16 11:37:58 -0400

    Was not compiling because of class rename.

--- drake/solvers/NoSnopt.cpp
+++ drake/solvers/NoSnopt.cpp
@@ -2,11 +2,11 @@
 #include <stdexcept>
 #include "SnoptSolver.h"
 
-bool Drake::SNOPTSolver::available() const {
+bool Drake::SnoptSolver::available() const {
   return false;
 }
 
-bool Drake::SNOPTSolver::solve(
+bool Drake::SnoptSolver::solve(
     OptimizationProblem &prog) const {
   throw std::runtime_error(
       "The SNOPT bindings were not compiled.  You'll need to use a different "
