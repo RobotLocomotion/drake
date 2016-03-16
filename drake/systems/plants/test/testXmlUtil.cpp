ba9986ccb63a6b4b84e430a8102ea2bc45cd0802
Sam Creasey <sam.creasey@tri.global>
2016-03-16 13:55:35 -0400

    Use gtest framework for testXmlUtil (see #1743)

--- drake/systems/plants/test/testXmlUtil.cpp
+++ drake/systems/plants/test/testXmlUtil.cpp
@@ -2,25 +2,21 @@
 #include <stdexcept>
 #include <string>
 
+#include "gtest/gtest.h"
+
 #include "drake/systems/plants/xmlUtil.h"
 
 namespace {
 
-void testPopulatePackageMap() {
+TEST(testXmlUtil, testPopulatePackageMap) {
   PackageMap package_map;
   populatePackageMap(package_map);
 
   std::list<std::string> expected_packages = { "drake", "test" };
   for (auto package: expected_packages) {
-    if (!package_map.count(package)) {
-      throw std::runtime_error(
-          std::string("Expected package not found in list: ") + package);
-    }
+    EXPECT_TRUE(package_map.count(package)) <<
+        std::string("Expected package not found in list: ")  << package;
   }
 }
 
-}
-
-int main(int argc, char* argv[]) {
-  testPopulatePackageMap();
 }
