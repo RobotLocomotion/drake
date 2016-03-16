c7cf728fd56477c02ce9050bf24f83eb4cb4adf9
Sam Creasey <sam.creasey@tri.global>
2016-03-16 18:11:31 -0400

    Try running multiple NonlinearProgram solvers in testOptimizationProblem.
    This also has the effect of not trying any if none are available (and
    not needing to conditionally ignore exceptions).
    
    Also add another test of a quadratic objective with linear constraints.
    
    Various style cleanups.

--- drake/solvers/test/testOptimizationProblem.cpp
+++ drake/solvers/test/testOptimizationProblem.cpp
@@ -1,5 +1,8 @@
 #include <typeinfo>
+#include "drake/solvers/MathematicalProgram.h"
+#include "drake/solvers/NloptSolver.h"
 #include "drake/solvers/Optimization.h"
+#include "drake/solvers/SnoptSolver.h"
 #include "drake/util/eigen_matrix_compare.h"
 #include "drake/util/testUtil.h"
 #include "gtest/gtest.h"
@@ -15,15 +18,16 @@ using Eigen::Vector3d;
 using Eigen::Vector4d;
 using Eigen::VectorXd;
 
-using std::numeric_limits;
-
 using Drake::Constraint;
 using Drake::TaylorVecXd;
 using Drake::VecIn;
 using Drake::Vector1d;
 using Drake::VecOut;
+using Drake::MathematicalProgramSolverInterface;
+using Drake::NloptSolver;
 using Drake::OptimizationProblem;
 using Drake::BoundingBoxConstraint;
+using Drake::SnoptSolver;
 using Drake::LinearComplementarityConstraint;
 using drake::util::MatrixCompareType;
 
@@ -61,7 +65,7 @@ struct Unique {
   void eval(VecIn<ScalarType> const&, VecOut<ScalarType>&) const {}
 };
 
-void testAddFunction() {
+TEST(testOptimizationProblem, testAddFunction) {
   OptimizationProblem prog;
   prog.addContinuousVariables(1);
 
@@ -78,7 +82,25 @@ void testAddFunction() {
   prog.addCost(std::unique_ptr<Unique>(new Unique));
 }
 
-void trivialLeastSquares() {
+void runNonlinearProgram(OptimizationProblem& prog,
+                         std::function<void ()> test_func) {
+  NloptSolver nlopt_solver;
+  SnoptSolver snopt_solver;
+
+  std::pair<const char*, MathematicalProgramSolverInterface*> solvers[] = {
+    std::make_pair("SNOPT", &snopt_solver),
+    std::make_pair("NLopt", &nlopt_solver)
+  };
+
+  for (const auto& solver : solvers) {
+    if (!solver.second->available()) { continue; }
+    ASSERT_NO_THROW(solver.second->solve(prog)) <<
+        "Using solver: " << solver.first;
+    EXPECT_NO_THROW(test_func()) << "Using solver: " << solver.first;
+  }
+}
+
+TEST(testOptimizationProblem, trivialLeastSquares) {
   OptimizationProblem prog;
 
   auto const& x = prog.addContinuousVariables(4);
@@ -88,6 +110,7 @@ void trivialLeastSquares() {
 
   Vector4d b = Vector4d::Random();
   auto con = prog.addLinearEqualityConstraint(Matrix4d::Identity(), b, {x});
+
   prog.solve();
   EXPECT_TRUE(
       CompareMatrices(b, x.value(), 1e-10, MatrixCompareType::absolute));
@@ -118,12 +141,54 @@ void trivialLeastSquares() {
   std::shared_ptr<BoundingBoxConstraint> bbcon(new BoundingBoxConstraint(
       MatrixXd::Constant(2, 1, -1000.0), MatrixXd::Constant(2, 1, 1000.0)));
   prog.addBoundingBoxConstraint(bbcon, {x.head(2)});
-  prog.solve();  // now it will solve as a nonlinear program
-  EXPECT_TRUE(CompareMatrices(b.topRows(2) / 2, y.value(), 1e-10,
-                              MatrixCompareType::absolute));
 
-  EXPECT_TRUE(
-      CompareMatrices(b / 3, x.value(), 1e-10, MatrixCompareType::absolute));
+  // Now solve as a nonlinear program.
+  runNonlinearProgram(prog, [&]() {
+      EXPECT_TRUE(CompareMatrices(b.topRows(2) / 2, y.value(), 1e-10,
+                                  MatrixCompareType::absolute));
+      EXPECT_TRUE(
+          CompareMatrices(b / 3, x.value(), 1e-10, MatrixCompareType::absolute));
+    });
+}
+
+// This test comes from Section 2.2 of "Handbook of Test Problems in
+// Local and Global Optimization"
+class TestProblem1Objective {
+ public:
+  static size_t numInputs() { return 5; }
+  static size_t numOutputs() { return 1; }
+
+  template <typename ScalarType>
+  void eval(VecIn<ScalarType> const& x, VecOut<ScalarType>& y) const {
+    assert(x.rows() == numInputs());
+    assert(y.rows() == numOutputs());
+    y(0) =
+        (-50.0 * x(0) * x(0)) + (42 * x(0)) - (50.0 * x(1) * x(1)) +
+        (44 * x(1)) - (50.0 * x(2) * x(2)) + (45 * x(2)) -
+        (50.0 * x(3) * x(3)) + (47 * x(3)) - (50.0 * x(4) * x(4)) +
+        (47.5 * x(4));
+  }
+};
+
+TEST(testOptimizationProblem, testProblem1) {
+  OptimizationProblem prog;
+  auto x = prog.addContinuousVariables(5);
+  prog.addCost(TestProblem1Objective());
+  VectorXd constraint(5);
+  constraint << 20, 12, 11, 7, 4;
+  prog.addLinearConstraint(
+      constraint.transpose(),
+      Drake::Vector1d::Constant(-std::numeric_limits<double>::infinity()),
+      Drake::Vector1d::Constant(40));
+  prog.addBoundingBoxConstraint(
+      MatrixXd::Constant(5, 1, 0), MatrixXd::Constant(5, 1, 1));
+  VectorXd expected(5);
+  expected << 1, 1, 0, 1, 0;
+  prog.setInitialGuess({x}, expected + .2 * VectorXd::Random(5));
+  runNonlinearProgram(prog, [&]() {
+      EXPECT_TRUE(CompareMatrices(x.value(), expected, 1e-10,
+                                  MatrixCompareType::absolute));
+    });
 }
 
 class SixHumpCamelObjective {
@@ -141,20 +206,20 @@ class SixHumpCamelObjective {
   }
 };
 
-void sixHumpCamel() {
+TEST(testOptimizationProblem, sixHumpCamel) {
   OptimizationProblem prog;
   auto x = prog.addContinuousVariables(2);
   auto objective = prog.addCost(SixHumpCamelObjective());
-  prog.solve();
-  prog.printSolution();
 
-  // check (numerically) if it is a local minimum
-  VectorXd ystar, y;
-  objective->eval(x.value(), ystar);
-  for (int i = 0; i < 10; i++) {
-    objective->eval(x.value() + .01 * Eigen::Matrix<double, 2, 1>::Random(), y);
-    if (y(0) < ystar(0)) throw std::runtime_error("not a local minima!");
-  }
+  runNonlinearProgram(prog, [&]() {
+      // check (numerically) if it is a local minimum
+      VectorXd ystar, y;
+      objective->eval(x.value(), ystar);
+      for (int i = 0; i < 10; i++) {
+        objective->eval(x.value() + .01 * Matrix<double, 2, 1>::Random(), y);
+        if (y(0) < ystar(0)) throw std::runtime_error("not a local minima!");
+      }
+    });
 }
 
 class GloptipolyConstrainedExampleObjective {
@@ -176,7 +241,7 @@ class GloptipolyConstrainedExampleConstraint
  public:
   GloptipolyConstrainedExampleConstraint()
       : Constraint(1, Vector1d::Constant(0),
-                   Vector1d::Constant(numeric_limits<double>::infinity())) {}
+                   Vector1d::Constant(std::numeric_limits<double>::infinity())) {}
 
   // for just these two types, implementing this locally is almost cleaner...
   virtual void eval(const Eigen::Ref<const Eigen::VectorXd>& x,
@@ -204,7 +269,7 @@ class GloptipolyConstrainedExampleConstraint
  * Which is from section 3.5 in
  *   Handbook of Test Problems in Local and Global Optimization
  */
-void gloptipolyConstrainedMinimization() {
+TEST(testOptimizationProblem, gloptipolyConstrainedMinimization) {
   OptimizationProblem prog;
   auto x = prog.addContinuousVariables(3);
   prog.addCost(GloptipolyConstrainedExampleObjective());
@@ -213,28 +278,28 @@ void gloptipolyConstrainedMinimization() {
   prog.addGenericConstraint(qp_con, {x});
   prog.addLinearConstraint(
       Vector3d(1, 1, 1).transpose(),
-      Vector1d::Constant(-numeric_limits<double>::infinity()),
+      Vector1d::Constant(-std::numeric_limits<double>::infinity()),
       Vector1d::Constant(4));
   prog.addLinearConstraint(
       Vector3d(0, 3, 1).transpose(),
-      Vector1d::Constant(-numeric_limits<double>::infinity()),
+      Vector1d::Constant(-std::numeric_limits<double>::infinity()),
       Vector1d::Constant(6));
   prog.addBoundingBoxConstraint(
-      Vector3d(0, 0, 0), Vector3d(2, numeric_limits<double>::infinity(), 3));
+      Vector3d(0, 0, 0),
+      Vector3d(2, std::numeric_limits<double>::infinity(), 3));
 
   prog.setInitialGuess({x}, Vector3d(.5, 0, 3) + .1 * Vector3d::Random());
-  prog.solve();
-  prog.printSolution();
-
-  EXPECT_TRUE(CompareMatrices(x.value(), Vector3d(0.5, 0, 3), 1e-4,
-                              MatrixCompareType::absolute));
+  runNonlinearProgram(prog, [&]() {
+      EXPECT_TRUE(CompareMatrices(x.value(), Vector3d(0.5, 0, 3), 1e-4,
+                                  MatrixCompareType::absolute));
+    });
 }
 
 /**
  * Test that the eval() method of LinearComplementarityConstraint correctly
  * returns the slack.
  */
-void simpleLCPConstraintEval() {
+TEST(testOptimizationProblem, simpleLCPConstraintEval) {
   OptimizationProblem prog;
   Eigen::Matrix<double, 2, 2> M;
 
@@ -264,7 +329,7 @@ void simpleLCPConstraintEval() {
  * this case; tests of the correctness of the Moby LCP solver itself live in
  * testMobyLCP.
  */
-void simpleLCP() {
+TEST(testOptimizationProblem, simpleLCP) {
   OptimizationProblem prog;
   Eigen::Matrix<double, 2, 2> M;
 
@@ -278,8 +343,7 @@ void simpleLCP() {
   auto x = prog.addContinuousVariables(2);
 
   prog.addLinearComplementarityConstraint(M, q, {x});
-  prog.solve();
-  prog.printSolution();
+  EXPECT_NO_THROW(prog.solve());
   EXPECT_TRUE(CompareMatrices(x.value(), Vector2d(16, 0), 1e-4,
                               MatrixCompareType::absolute));
 }
@@ -288,7 +352,7 @@ void simpleLCP() {
  * @brief Just two copies of the simpleLCP example, to make sure that the
  * write-through of LCP results to the solution vector works correctly.
  */
-void multiLCP() {
+TEST(testOptimizationProblem, multiLCP) {
   OptimizationProblem prog;
   Eigen::Matrix<double, 2, 2> M;
 
@@ -304,33 +368,13 @@ void multiLCP() {
 
   prog.addLinearComplementarityConstraint(M, q, {x});
   prog.addLinearComplementarityConstraint(M, q, {y});
-  prog.solve();
-  prog.printSolution();
+  EXPECT_NO_THROW(prog.solve());
 
   EXPECT_TRUE(CompareMatrices(x.value(), Vector2d(16, 0), 1e-4,
                               MatrixCompareType::absolute));
 
   EXPECT_TRUE(CompareMatrices(y.value(), Vector2d(16, 0), 1e-4,
                               MatrixCompareType::absolute));
-}
-
-TEST(OptimizationProblemTest, AllTests) {
-  // SNOPT tests
-  try {
-    testAddFunction();
-    trivialLeastSquares();
-    sixHumpCamel();
-    gloptipolyConstrainedMinimization();
-  } catch (const std::exception& e) {
-    // If the exception is SNOPT unavailble, skip the remaining snopt tests
-    // and proceed; if not, reraise it to fail.
-    if (std::string(e.what()) != "SNOPT unavailable") {
-      throw;
-    }
-  }
-  simpleLCPConstraintEval();
-  simpleLCP();
-  multiLCP();
 }
 
 }  // namespace
