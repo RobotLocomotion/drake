0b89836ad6fa59fa153e16653ba8f5ea2a310253
Lucy Gibson <lucy.gibson@tri.global>
2016-03-18 16:45:49 -0400

    nlopt: Correct linker errors, add DRAKEOPTIMIZATION_EXPORT

--- drake/solvers/NloptSolver.h
+++ drake/solvers/NloptSolver.h
@@ -1,11 +1,13 @@
 #ifndef DRAKE_NLOPT_SOLVER_H
 #define DRAKE_NLOPT_SOLVER_H
 
+#include "drake/drakeOptimization_export.h"
+
 #include "MathematicalProgram.h"
 
 namespace Drake {
 
-class NloptSolver :
+class DRAKEOPTIMIZATION_EXPORT NloptSolver :
       public MathematicalProgramSolverInterface  {
  public:
   // This solver is implemented in various pieces depending on if
