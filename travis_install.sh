5a1f6ad0c5e9e98a0be48286381a0dfc093e08f1
Jamie Snape <jamie.snape@kitware.com>
2016-03-23 16:32:25 -0400

    Remove unused dependencies for Travis CI builds

--- travis_install.sh
+++ travis_install.sh
@@ -1,10 +1,7 @@
 set -e
-brew tap homebrew/python > tap.log || cat tap.log
-brew tap homebrew/science > tap.log || cat tap.log
-brew update > update.log || cat update.log
-brew install autoconf automake ccache cmake doxygen gcc glib graphviz gtk+ \
-  jpeg libpng libtool mpfr mpich2 numpy python qt swig valgrind vtk5 \
-  wget > install.log  || (cat install.log && exit 1)
+brew tap homebrew/python > tap.log
+brew update > update.log
+brew install glib graphviz gtk+ numpy python swig > install.log
 mkdir -p $TRAVIS_BUILD_DIR/build
 export CMAKE_FLAGS="-DWITH_AVL:BOOL=ON -DWITH_DIRECTOR:BOOL=OFF -DWITH_IRIS:BOOL=ON -DWITH_LIBBOT:BOOL=ON -DWITH_MOSEK:BOOL=ON -DWITH_OCTOMAP:BOOL=ON -DWITH_SPOTLESS:BOOL=OFF -DWITH_XFOIL:BOOL=ON"
 make download-all > download.log || (cat download.log && exit 1)
