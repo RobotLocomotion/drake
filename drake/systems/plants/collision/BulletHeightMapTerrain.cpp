33a3f0cb29c3466498d4c37a9be3e2763879dc3a
amcastro-tri <alejandro.castro@tri.global>
2016-03-24 11:47:03 -0400

    Removed debug printing from GatherHeightMapAsGridCallBack::processTriangle

--- drake/systems/plants/collision/BulletHeightMapTerrain.cpp
+++ drake/systems/plants/collision/BulletHeightMapTerrain.cpp
@@ -10,14 +10,7 @@ namespace DrakeCollision {
 
 	void GatherHeightMapAsGridCallBack::processTriangle(btVector3* triangle, int i, int j){		
 		int n = m_grid_points.size();
-
-		m_connectivities.push_back(Vector3i(n+1,n+2,n+3));		
-
-		cout << "i,j: " << i << " " << j << ": " << endl;
-		cout << "triangle[0]:" << triangle[0][0] << " " << triangle[0][1] << " " << triangle[0][2] << endl;
-		cout << "triangle[1]:" << triangle[1][0] << " " << triangle[1][1] << " " << triangle[1][2] << endl;
-		cout << "triangle[2]:" << triangle[2][0] << " " << triangle[2][1] << " " << triangle[2][2] << endl;
-
+		m_connectivities.push_back(Vector3i(n+1,n+2,n+3));
 		m_grid_points.push_back(triangle[0]);
 		m_grid_points.push_back(triangle[1]);
 		m_grid_points.push_back(triangle[2]);		
