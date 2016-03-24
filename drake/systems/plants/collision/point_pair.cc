bf89ae500f1245ecf86ea28c631d8be7eb197065
David German <david.german@tri.global>
2016-03-24 11:43:31 -0400

    Remove PointPair lint.

--- drake/systems/plants/collision/point_pair.cc
+++ drake/systems/plants/collision/point_pair.cc
@@ -1,30 +1,32 @@
-#include "PointPair.h"
+#include "drake/systems/plants/collision/point_pair.h"
 
-using namespace std;
-using namespace Eigen;
+using Eigen::Vector3d;
 
 namespace DrakeCollision {
-void PointPair::getResults(Vector3d& ptA, Vector3d& ptB, Vector3d& normal) {
-  ptA = this->ptA;
-  ptB = this->ptB;
-  normal = this->normal;
+
+void PointPair::getResults(Vector3d* ptA, Vector3d* ptB,
+                           Vector3d* normal) const {
+  *ptA = ptA_;
+  *ptB = ptB_;
+  *normal = normal_;
 }
 
-void PointPair::getResults(Vector3d& ptA, Vector3d& ptB, Vector3d& normal,
-                           double& distance) {
+void PointPair::getResults(Vector3d* ptA, Vector3d* ptB, Vector3d* normal,
+                           double* distance) const {
   getResults(ptA, ptB, normal);
-  distance = this->distance;
+  *distance = distance_;
 }
 
-ElementId PointPair::getIdA() { return this->idA; }
+ElementId PointPair::getIdA() const { return idA_; }
 
-ElementId PointPair::getIdB() { return this->idB; }
+ElementId PointPair::getIdB() const { return idB_; }
 
-Vector3d PointPair::getNormal() { return this->normal; }
+Vector3d PointPair::getNormal() const { return normal_; }
 
-double PointPair::getDistance() { return this->distance; }
+double PointPair::getDistance() const { return distance_; }
 
-Vector3d PointPair::getPtA() { return this->ptA; }
+Vector3d PointPair::getPtA() const { return ptA_; }
 
-Vector3d PointPair::getPtB() { return this->ptB; }
-}
+Vector3d PointPair::getPtB() const { return ptB_; }
+
+}  // namespace DrakeCollision
--- drake/systems/plants/collision/point_pair.h
+++ drake/systems/plants/collision/point_pair.h
@@ -1,5 +1,5 @@
-#ifndef __DrakeCollisionPointPair_H__
-#define __DrakeCollisionPointPair_H__
+#ifndef DRAKE_SYSTEMS_PLANTS_COLLISION_POINT_PAIR_H_
+#define DRAKE_SYSTEMS_PLANTS_COLLISION_POINT_PAIR_H_
 
 #include <Eigen/Dense>
 #include <Eigen/StdVector>
@@ -8,56 +8,64 @@
 #include "drake/drakeCollision_export.h"
 
 namespace DrakeCollision {
+
 class DRAKECOLLISION_EXPORT PointPair {
  public:
   PointPair(const ElementId idA, const ElementId idB, const Eigen::Vector3d ptA,
             const Eigen::Vector3d ptB, const Eigen::Vector3d normal,
             double distance)
-      : idA(idA),
-        idB(idB),
-        ptA(ptA),
-        ptB(ptB),
-        normal(normal),
-        distance(distance) {}
+      : idA_(idA),
+        idB_(idB),
+        ptA_(ptA),
+        ptB_(ptB),
+        normal_(normal),
+        distance_(distance) {}
 
-  void getResults(Eigen::Vector3d& ptA, Eigen::Vector3d& ptB,
-                  Eigen::Vector3d& normal);
-  void getResults(Eigen::Vector3d& ptA, Eigen::Vector3d& ptB,
-                  Eigen::Vector3d& normal, double& distance);
-  ElementId getIdA();
-  ElementId getIdB();
-  Eigen::Vector3d getNormal();
-  Eigen::Vector3d getPtA();
-  Eigen::Vector3d getPtB();
-  double getDistance();
+  void getResults(Eigen::Vector3d* ptA, Eigen::Vector3d* ptB,
+                  Eigen::Vector3d* normal) const;
+  void getResults(Eigen::Vector3d* ptA, Eigen::Vector3d* ptB,
+                  Eigen::Vector3d* normal, double* distance) const;
+  ElementId getIdA() const;
+  ElementId getIdB() const;
+  Eigen::Vector3d getNormal() const;
+  Eigen::Vector3d getPtA() const;
+  Eigen::Vector3d getPtB() const;
+  double getDistance() const;
 
-  bool operator<(const PointPair& pt) const { return (distance < pt.distance); }
+  bool operator<(const PointPair& pt) const {
+    return (distance_ < pt.distance_);
+  }
 
   bool operator==(const PointPair& pt) const {
-    return (distance == pt.distance);
+    return (distance_ == pt.distance_);
   }
 
   bool operator!=(const PointPair& pt) const {
-    return (distance != pt.distance);
+    return (distance_ != pt.distance_);
   }
 
   bool operator<=(const PointPair& pt) const {
-    return (distance <= pt.distance);
+    return (distance_ <= pt.distance_);
   }
 
-  bool operator>(const PointPair& pt) const { return (distance > pt.distance); }
+  bool operator>(const PointPair& pt) const {
+    return (distance_ > pt.distance_);
+  }
 
   bool operator>=(const PointPair& pt) const {
-    return (distance >= pt.distance);
+    return (distance_ >= pt.distance_);
   }
 
  protected:
-  ElementId idA;
-  ElementId idB;
-  Eigen::Vector3d ptA;
-  Eigen::Vector3d ptB;
-  Eigen::Vector3d normal;
-  double distance;
+  ElementId idA_;
+  ElementId idB_;
+  Eigen::Vector3d ptA_;
+  Eigen::Vector3d ptB_;
+  Eigen::Vector3d normal_;
+  double distance_;
 };
-}
-#endif
+
+}  // namespace DrakeCollision
+
+#endif  // DRAKE_SYSTEMS_PLANTS_COLLISION_POINT_PAIR_H_
+
