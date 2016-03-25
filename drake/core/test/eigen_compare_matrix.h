7bda976d5cf8a88e10f84b48952a449361a29f6c
Chien-Liang Fok <liang.fok@tri.global>
2016-03-24 22:18:39 -0400

    Got basic mock for comparing Eigen matrices to work. Still need to add logic for handling corner cases like infinity, NaN, and tolerance.

--- drake/core/test/eigen_compare_matrix.h
+++ drake/core/test/eigen_compare_matrix.h
@@ -4,61 +4,71 @@
 #include "gmock/gmock.h"
 #include <Eigen/Dense>
 
-using ::testing::MakeMatcher;
-using ::testing::Matcher;
+using ::testing::MakePolymorphicMatcher;
+using ::testing::PolymorphicMatcher;
 using ::testing::MatcherInterface;
 using ::testing::MatchResultListener;
 
 template<typename Derived>
-class EigenMatrixIsApproximatelyEqualMatcher : public MatcherInterface<Eigen::MatrixBase<Derived>> {
+class EigenMatrixIsApproximatelyEqualMatcher {
  public:
-  explicit EigenMatrixIsApproximatelyEqualMatcher(Eigen::MatrixBase<Derived> mm) 
+  explicit EigenMatrixIsApproximatelyEqualMatcher(Eigen::MatrixBase<Derived> & mm)
       : mm_(mm) { }
 
-  virtual bool MatchAndExplain(Eigen::MatrixBase<Derived> mm, MatchResultListener* listener) const {
-    return false;  // temp!
+  template <typename T>
+  bool MatchAndExplain(T& mm,
+                       MatchResultListener * listener) const {
+
+    bool result = true;
+
+    if (mm.rows() != mm_.rows()) {
+      *listener << "rows do not match: " << mm.rows() << " vs. " << mm_.rows();
+      result = false;
+    }
+
+    if (result && mm.cols() != mm_.cols()) {
+      *listener << "columns do not match: " << mm.cols() << " vs. " << mm_.cols();
+      result = false;
+    }
+
+    for (size_t ii = 0; result && ii < mm.rows(); ii++) {
+      for (size_t jj = 0; result && jj < mm.cols(); jj++) {
+
+        // TODO: Add logic for handling infinity, NaN, and tolerance
+        if (mm(ii,jj) != mm_(ii,jj)) {
+          result = false;
+        }
+
+        if (!result) {
+          *listener << "mismatch in location (" << ii << ", " << jj << "): " << mm(ii, jj) << " vs. " << mm_(ii, jj);
+        }
+      }
+    }
+
+    return result;  // temp!
   }
 
+  /**
+   * Describes the property of a value matching this matcher.
+   */
   virtual void DescribeTo(::std::ostream* os) const {
     *os << "is approximately equal to";
   }
 
+  /**
+   * Describes the property of a value NOT matching this matcher.
+   */
   virtual void DescribeNegationTo(::std::ostream* os) const {
     *os << "is not approximately equal to";
   }
 
  private:
-  Eigen::MatrixBase<Derived> mm_;
+  Eigen::MatrixBase<Derived> & mm_;
 };
 
-template<typename Derived>
-inline Matcher<Eigen::MatrixBase<Derived>> EigenMatrixIsApproximatelyEqual(Eigen::MatrixBase<Derived> mm) {
-  return MakeMatcher(new EigenMatrixIsApproximatelyEqualMatcher<Derived>(mm));
+template <typename Derived>
+inline PolymorphicMatcher<EigenMatrixIsApproximatelyEqualMatcher<Derived>> EigenMatrixIsApproximatelyEqual(Eigen::MatrixBase<Derived>& mm) {
+  return MakePolymorphicMatcher(EigenMatrixIsApproximatelyEqualMatcher<Derived>(mm));
 }
-
-// class EigenMatrixIsApproximatelyEqualMatcher : public MatcherInterface<double> {
-//  public:
-//   explicit EigenMatrixIsApproximatelyEqualMatcher(double mm) 
-//       : mm_(mm) { }
-
-//   virtual bool MatchAndExplain(double mm, MatchResultListener* listener) const {
-//     return mm == mm_;
-//   }
-
-//   virtual void DescribeTo(::std::ostream* os) const {
-//     *os << "is approximately equal to";
-//   }
-
-//   virtual void DescribeNegationTo(::std::ostream* os) const {
-//     *os << "is not approximately equal to";
-//   }
-
-//  private:
-//   double mm_;
-// };
-
-// inline Matcher<double> EigenMatrixIsApproximatelyEqual(double mm) {
-//   return MakeMatcher(new EigenMatrixIsApproximatelyEqualMatcher(mm));
-// }
 
 #endif
