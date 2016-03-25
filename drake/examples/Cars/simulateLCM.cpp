ac604b6ba017b237efdd2f1d4dbd33bd26e2e84e
amcastro-tri <alejandro.castro@tri.global>
2016-03-25 15:27:01 -0400

    Removed by debug wait_for_keypress from Cars example

--- drake/examples/Cars/simulateLCM.cpp
+++ drake/examples/Cars/simulateLCM.cpp
@@ -154,9 +154,7 @@ int main(int argc, char* argv[]) {
       rigid_body_sys->penetration_stiffness / 10.0;
   rigid_body_sys->friction_coefficient = 10.0;  // essentially infinite friction
   options.initial_step_size = 5e-3;
-  options.timeout_seconds = numeric_limits<double>::infinity();
-  options.wait_for_keypress = false;
-  options.rk2 = true;
+  options.timeout_seconds = numeric_limits<double>::infinity();  
 
   VectorXd x0 = VectorXd::Zero(rigid_body_sys->getNumStates());
   x0.head(tree->num_positions) = tree->getZeroConfiguration();
