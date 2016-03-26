d533803345c23b292de0e540a095b6dc582159b5
jwnimmer <jeremy.nimmer@tri.global>
2016-03-25 21:58:16 -0400

    Fix cpplint build/header_guard in core.

--- drake/core/Core.h
+++ drake/core/Core.h
@@ -1,9 +1,9 @@
-#ifndef DRAKE_DRAKECORE_H
-#define DRAKE_DRAKECORE_H
+#ifndef DRAKE_CORE_CORE_H_
+#define DRAKE_CORE_CORE_H_
 
 #include "drake/Path.h"
 #include "Vector.h"
 #include "Function.h"
 #include "Gradient.h"
 
-#endif  // DRAKE_DRAKECORE_H
+#endif  // DRAKE_CORE_CORE_H_
