cd18f0f8d0f816343e9f3f497cac07256b691864
Chien-Liang Fok <liang.fok@tri.global>
2016-04-07 14:35:47 -0400

    Added debug print statements to addRobotFromURDFStringTest.m.

--- drake/systems/plants/test/addRobotFromURDFStringTest.m
+++ drake/systems/plants/test/addRobotFromURDFStringTest.m
@@ -35,9 +35,13 @@ function testGeneratedManipulators(urdf_filename)
       continue
     end
     if ~(isequal(val, r_file_struct.(field)))
-      field
-      r_string_struct.(field)
+      fprintf('Field: %s\n',field);
+      val
       r_file_struct.(field)
+      fprintf('value in r_string_struct:')
+      val{1,1}
+      fprintf('value in r_file_struct:')
+      r_file_struct.(field){1,1}
       error('fields don''t match');
     end
   end
