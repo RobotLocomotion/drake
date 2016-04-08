468491b2696aee291c091ff6071a3b6468177cb1
amcastro-tri <alejandro.castro@tri.global>
2016-04-07 20:23:19 -0400

    Run clang-format on drake/systems/plants/collision/BulletHeightMapTerrain.cpp

--- drake/systems/plants/collision/BulletHeightMapTerrain.cpp
+++ drake/systems/plants/collision/BulletHeightMapTerrain.cpp
@@ -8,12 +8,13 @@ using namespace Eigen;
 
 namespace DrakeCollision {
 
-	void GatherHeightMapAsGridCallBack::processTriangle(btVector3* triangle, int i, int j){
-		int n = m_grid_points.size();
-		m_connectivities.push_back(Vector3i(n+1,n+2,n+3));
-		m_grid_points.push_back(triangle[0]);
-		m_grid_points.push_back(triangle[1]);
-		m_grid_points.push_back(triangle[2]);
-	}
+void GatherHeightMapAsGridCallBack::processTriangle(btVector3* triangle,
+                                                    int i, int j) {
+  int n = m_grid_points.size();
+  m_connectivities.push_back(Vector3i(n + 1, n + 2, n + 3));
+  m_grid_points.push_back(triangle[0]);
+  m_grid_points.push_back(triangle[1]);
+  m_grid_points.push_back(triangle[2]);
+}
 
 }  // namespace DrakeCollision
