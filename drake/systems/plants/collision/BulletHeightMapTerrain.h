1b0510f0d3373d55ea73b1a074926032912f6fe4
amcastro-tri <alejandro.castro@tri.global>
2016-04-07 20:02:49 -0400

    Run clang-format on drake/systems/plants/collision/BulletHeightMapTerrain.h

--- drake/systems/plants/collision/BulletHeightMapTerrain.h
+++ drake/systems/plants/collision/BulletHeightMapTerrain.h
@@ -7,50 +7,40 @@
 
 #include "../shapes/HeightMapTerrain.h"
 
-
 namespace DrakeCollision {
 
-  struct  btTriangle
-  {
-    btVector3       m_vertex0;
-    btVector3       m_vertex1;
-    btVector3       m_vertex2;
-    int     m_partId;
-    int     m_triangleIndex;
-  };
+struct btTriangle {
+  btVector3 m_vertex0;
+  btVector3 m_vertex1;
+  btVector3 m_vertex2;
+  int m_partId;
+  int m_triangleIndex;
+};
 
-  class GatherHeightMapAsGridCallBack : public btTriangleCallback{
-     bool first_triangle;
+class GatherHeightMapAsGridCallBack : public btTriangleCallback {
+  bool first_triangle;
 
-     std::vector<Eigen::Vector3i,Eigen::aligned_allocator<Eigen::Vector3i> > m_connectivities;
-     btAlignedObjectArray<btVector3> m_grid_points;
+  std::vector<Eigen::Vector3i, Eigen::aligned_allocator<Eigen::Vector3i> >
+      m_connectivities;
+  btAlignedObjectArray<btVector3> m_grid_points;
 
-  public:
-    GatherHeightMapAsGridCallBack(): first_triangle(true){}
+ public:
+  GatherHeightMapAsGridCallBack() : first_triangle(true) {}
 
-    virtual void processTriangle(btVector3* triangle, int i, int j);
+  virtual void processTriangle(btVector3* triangle, int i, int j);
 
-    int numTriangles() const
-    {
-      return int(m_connectivities.size());
-    }
+  int numTriangles() const { return int(m_connectivities.size()); }
 
-    int numPoints() const
-    {
-      return int(m_grid_points.size());
-    }
+  int numPoints() const { return int(m_grid_points.size()); }
 
-    const btVector3& getPoint(int i) const{
-      return m_grid_points[i];
-    }
+  const btVector3& getPoint(int i) const { return m_grid_points[i]; }
 
-    const Eigen::Vector3i& getTriangle(int i) const{
-      return m_connectivities[i];
-    }
+  const Eigen::Vector3i& getTriangle(int i) const {
+    return m_connectivities[i];
+  }
+};
 
-  };
-
-}//namespace DrakeCollision
+}  // namespace DrakeCollision
 
 #endif
-//BULLET_HEIGHT_MAP_TERRAIN_H_
+// BULLET_HEIGHT_MAP_TERRAIN_H_
