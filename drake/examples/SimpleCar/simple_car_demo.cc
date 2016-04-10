e93232e5930afac6cec122cec0845dae7fce2621
Rick Poyner <rick.poyner@tri.global>
2016-04-10 15:26:29 -0400

    SimpleCar: support a non-viz car demo.
     * Adapt and clean up steering command driver from Cars/.
     * Fit a car with an LCMSystem wrapper.
    
    Now you can "visualize" car state by staring at bot-spy:
    
     $ $DRAKE_DIST/build/bin/bot-spy &
     $ $DRAKE/pod-build/bin/simpleCarDemo &
     $ ./steering_command_driver.py &

--- drake/examples/SimpleCar/simple_car_demo.cc
+++ drake/examples/SimpleCar/simple_car_demo.cc
@@ -1,9 +1,29 @@
 #include "simple_car.h"
 
-int main(int argc, const char* argv[]) {
-  // TODO rico: build a cascade system:
-  //  * simple car
-  //  * a visual shape (rigid body tree?)
-  //  * bot visualizer
+#include "drake/systems/LCMSystem.h"
+#include "lcmtypes/drake/lcmt_driving_command_t.hpp"
+
+using namespace std;
+using namespace Eigen;
+using namespace Drake;
+using Drake::getDrakePath;
+
+// N.B.: LCMSystem uses this via c++ ADL magic. See also drake issue #1880.
+bool decode(const drake::lcmt_driving_command_t &msg, double &t,
+            DrivingCommand<double> &x) {
+  t = double(msg.timestamp) / 1000.0;
+  x.steering_angle = msg.steering_angle;
+  x.throttle = msg.throttle;
+  x.brake = msg.brake;
+  return true;
+}
+
+int main(int argc, const char *argv[]) {
+  shared_ptr<lcm::LCM> lcm = make_shared<lcm::LCM>();
+
+  auto car = make_shared<Drake::SimpleCar>();
+  Drake::SimpleCarState<double> initial_state;
+  runLCM(car, lcm, 0, numeric_limits<double>::infinity(), initial_state);
+
   return 0;
 }
