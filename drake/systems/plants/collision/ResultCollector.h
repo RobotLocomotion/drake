3045a2ce2fd8061d8f7adbe2c0ab87d29581b872
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-04-11 11:07:49 -0400

    Separate ResultCollector base
    Move the base ResultCollector class into its own file, rather than
    embedding it in the source files for the BulletResultCollector subclass.
    This will make it possible to add other implementations.

---
+++ drake/systems/plants/collision/ResultCollector.h
@@ -0,0 +1,32 @@
+#ifndef DRAKE_SYSTEMS_PLANTS_COLLISION_RESULTCOLLECTOR_H_
+#define DRAKE_SYSTEMS_PLANTS_COLLISION_RESULTCOLLECTOR_H_
+
+#include "drake/systems/plants/collision/DrakeCollision.h"
+
+namespace DrakeCollision {
+Eigen::Vector3d toVector3d(const Eigen::Vector3d& vec);
+
+class ResultCollector {
+ public:
+  virtual ~ResultCollector() {}
+
+  virtual void addPointPairResult(const PointPair& result);
+
+  inline void addSingleResult(const ElementId idA, const ElementId idB,
+                              const Eigen::Vector3d& ptA,
+                              const Eigen::Vector3d& ptB,
+                              const Eigen::Vector3d& normal, double distance) {
+    addPointPairResult(PointPair(idA, idB, ptA, ptB, normal, distance));
+  }
+
+  std::vector<PointPair> getResults() const { return pts; }
+
+  PointPair minDistPoint();
+
+  std::vector<PointPair> pts;
+};
+
+typedef std::shared_ptr<ResultCollector> ResultCollShPtr;
+}
+
+#endif  // DRAKE_SYSTEMS_PLANTS_COLLISION_RESULTCOLLECTOR_H_
