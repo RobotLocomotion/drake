049eb332e3dc9e4ee7ee13046ca48cde95d8358a
David German <david.german@tri.global>
2016-04-12 17:38:59 -0400

    Export MakeRigidBodyFrameFromURDFNode into the DLL.

--- drake/systems/plants/rigid_body_tree_urdf.h
+++ drake/systems/plants/rigid_body_tree_urdf.h
@@ -4,6 +4,7 @@
 #include <memory>
 #include <string>
 
+#include "drake/drakeRBM_export.h"
 #include "drake/systems/plants/RigidBodyFrame.h"
 #include "drake/systems/plants/RigidBodyTree.h"
 #include "drake/thirdParty/tinyxml2/tinyxml2.h"
@@ -14,7 +15,7 @@ namespace systems {
 /// Manufactures a RigidBodyFrame from the given URDF \p link
 /// and \p pose nodes.  The link name must exist in the given
 /// \p model.
-std::shared_ptr<RigidBodyFrame> MakeRigidBodyFrameFromURDFNode(
+DRAKERBM_EXPORT std::shared_ptr<RigidBodyFrame> MakeRigidBodyFrameFromURDFNode(
     const RigidBodyTree& model, const tinyxml2::XMLElement* link,
     const tinyxml2::XMLElement* pose, const std::string& name);
 
