35d33cb169b4f1cac52b57f6fafe0f3f87ed9a77
David German <david.german@tri.global>
2016-04-22 13:48:53 -0400

    Combine Value and AbstractValue in one header. Clarify that T must be a concrete type.  Add tests illustrating how to extend Value<T>.

--- drake/systems/framework/abstract_value.cc
+++
@@ -1,4 +0,0 @@
-// For now, this is an empty .cc file that only serves to confirm
-// abstract_value.h is a stand-alone header.
-
-#include "drake/systems/framework/abstract_value.h"
--- drake/systems/framework/value_test.cc
+++ drake/systems/framework/value_test.cc
@@ -1,8 +1,8 @@
 #include "drake/systems/framework/value.h"
 
 #include <memory>
-
-#include "drake/systems/framework/abstract_value.h"
+#include <sstream>
+#include <string>
 
 #include "gtest/gtest.h"
 
@@ -41,21 +41,35 @@ TEST(ValueTest, Mutation) {
 TEST(ValueTest, BadCast) {
   Value<double> value(4);
   const AbstractValue& erased = value;
-  EXPECT_THROW(erased.GetValue<int>(), std::runtime_error);
+  EXPECT_THROW(erased.GetValue<int>(), std::bad_cast);
 }
 
-// A two-dimensional point, for testing purposes.
-class Point {
+class PrintInterface {
+ public:
+  virtual std::string print() const = 0;
+};
+
+// A trivial class that implements a trivial interface.
+class Point : public PrintInterface {
  public:
   Point(int x, int y) : x_(x), y_(y) {}
+  virtual ~Point() {}
+
   int x() const { return x_; }
   int y() const { return y_; }
   void set_x(int x) { x_ = x; }
+
+  std::string print() const override {
+    std::ostringstream out;
+    out << x_ << "," << y_;
+    return out.str();
+  }
 
  private:
   int x_, y_;
 };
 
+// Tests that classes can be erased in an AbstractValue.
 TEST(ValueTest, ClassType) {
   Point point(1, 2);
   Value<Point> value(point);
@@ -64,6 +78,53 @@ TEST(ValueTest, ClassType) {
   EXPECT_EQ(-1, erased.GetValue<Point>().x());
   EXPECT_EQ(2, erased.GetValue<Point>().y());
 }
+
+// A child class of Value<T> that requires T to satisfy PrintInterface, and
+// exposes some functions using that interface.
+template <typename T>
+class PrintableValue : public Value<T>, public PrintInterface {
+ public:
+  explicit PrintableValue(const T& v) : Value<T>(v) {}
+
+  // PrintableValues are copyable but not moveable.
+  PrintableValue(const PrintableValue<T>& other) = default;
+  PrintableValue& operator=(const PrintableValue<T>& other) = default;
+  PrintableValue(PrintableValue<T>&& other) = delete;
+  PrintableValue& operator=(PrintableValue<T>&& other) = delete;
+
+  virtual std::unique_ptr<AbstractValue> Clone() const override {
+    return std::unique_ptr<PrintableValue<T>>(
+        new PrintableValue<T>(this->get_value()));
+  }
+
+  std::string print() const override {
+    const PrintInterface& print_interface = Value<T>::get_value();
+    return print_interface.print();
+  }
+};
+
+// Tests that AbstractValues can be unerased to interfaces implemented by
+// subclasses of Value<T>.
+TEST(ValueTest, SubclassOfValue) {
+  Point point(3, 4);
+  PrintableValue<Point> printable_value(point);
+  AbstractValue* erased = &printable_value;
+  PrintInterface* printable_erased = dynamic_cast<PrintInterface*>(erased);
+  ASSERT_NE(nullptr, printable_erased);
+  EXPECT_EQ("3,4", printable_erased->print());
+}
+
+TEST(ValueTest, SubclassOfValueSurvivesClone) {
+  Point point(5, 6);
+  PrintableValue<Point> printable_value(point);
+  const AbstractValue& erased = printable_value;
+  std::unique_ptr<AbstractValue> cloned = erased.Clone();
+  PrintInterface* printable_erased = dynamic_cast<PrintInterface*>(
+      cloned.get());
+  ASSERT_NE(nullptr, printable_erased);
+  EXPECT_EQ("5,6", printable_erased->print());
+}
+
 
 }  // namespace
 }  // namespace systems
