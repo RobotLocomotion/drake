5f468b56235203a649be0ed05b3cc7cb58e16d62
amcastro-tri <alejandro.castro@tri.global>
2016-04-28 10:36:25 -0400

    clang-format/cpplint on drake/systems/plants/test/compareParsersmex.cpp

--- drake/systems/plants/test/compareParsersmex.cpp
+++ drake/systems/plants/test/compareParsersmex.cpp
@@ -54,7 +54,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   for (int i = 0; i < cpp_model->bodies.size(); i++) {
     if (cpp_model->bodies[i]->hasParent() &&
         cpp_model->bodies[i]->getJoint().getNumPositions() > 0) {
-      RigidBody* b =
+      RigidBody *b =
           matlab_model->findJoint(cpp_model->bodies[i]->getJoint().getName());
       if (b == nullptr) continue;
       for (int j = 0; j < b->getJoint().getNumPositions(); j++) {
@@ -100,8 +100,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 
     {  // compare H, C, and B
       eigen_aligned_unordered_map<RigidBody const *,
-                                  Matrix<double, TWIST_SIZE, 1> >
-          f_ext;
+                                  Matrix<double, TWIST_SIZE, 1> > f_ext;
 
       auto matlab_H = matlab_model->massMatrix(matlab_cache);
       auto cpp_H = cpp_model->massMatrix(cpp_cache);
