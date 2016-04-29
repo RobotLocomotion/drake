a88828aa14d1d35cfe4ed2e0dc73653a3acf94c1
Matt Marjanovic <maddog@tri.global>
2016-04-29 04:42:22 -0700

    First pass at a multi-car demo --- 2 'sedans' and 1 'bread truck'.

---
+++ drake/examples/MultiCar/xyz_rpy.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <Eigen/Core>
+
+namespace Drake {
+
+template <typename ScalarType>
+class XyzRpy { // models Drake::Vector
+ public:
+  static const int RowsAtCompileTime = 6;
+  typedef Eigen::Matrix<ScalarType, RowsAtCompileTime, 1> EigenType;
+
+  XyzRpy() {}
+
+  XyzRpy(const ScalarType& x, const ScalarType& y, const ScalarType& z,
+         const ScalarType& roll, const ScalarType& pitch,
+         const ScalarType& yaw) {
+    vector_ << x, y, z, roll, pitch, yaw;
+  }
+
+  template <typename Derived>
+  XyzRpy(const Eigen::MatrixBase<Derived>& initial) : vector_(initial) {}
+
+  template <typename Derived>
+  XyzRpy& operator=(const Eigen::MatrixBase<Derived>& rhs) {
+    vector_ = rhs.vector_;
+    return *this;
+  }
+
+  friend EigenType toEigen(const XyzRpy<ScalarType>& xyz_rpy) {
+    return xyz_rpy.vector_;
+  }
+
+ private:
+  EigenType vector_;
+};
+} // namespace Drake
