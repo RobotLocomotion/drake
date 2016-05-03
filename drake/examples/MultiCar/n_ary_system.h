272a2cb7d5bc35dd43523b747f122cf9846f6146
Matt Marjanovic <maddog@tri.global>
2016-05-03 15:03:39 -0700

    In NAryState, keep track of the number of component UnitVectors, provide a constructor that pre-allocates for a given number of units, a set() method to directly set the unit at the given index, and unit-count<->row-count conversion methods that also static-assert that the unit's row-count is known at compile time.
    Make use of these features in NArySystem.
    
    Also, code cleanup.

--- drake/examples/MultiCar/n_ary_system.h
+++ drake/examples/MultiCar/n_ary_system.h
@@ -10,62 +10,73 @@ namespace Drake {
 template <class UnitSystem>
 class NArySystem {
  public:
+  // Required by Drake::System concept.
   template <typename ScalarType>
   using StateVector = NAryState<ScalarType, UnitSystem::template StateVector>;
+  // Required by Drake::System concept.
   template <typename ScalarType>
   using InputVector = NAryState<ScalarType, UnitSystem::template InputVector>;
+  // Required by Drake::System concept.
   template <typename ScalarType>
   using OutputVector = NAryState<ScalarType, UnitSystem::template OutputVector>;
 
-  explicit NArySystem() {}
+  NArySystem() {}
 
   void addSystem(std::shared_ptr<UnitSystem> system) {
     systems_.push_back(system);
   }
 
+  // Required by Drake::System concept.
   template <typename ScalarType>
   StateVector<ScalarType> dynamics(const ScalarType& time,
                                    const StateVector<ScalarType>& state,
                                    const InputVector<ScalarType>& input) const {
-    StateVector<ScalarType> xdot;
+    StateVector<ScalarType> xdot(systems_.size());
     for (std::size_t i = 0; i < systems_.size(); ++i) {
-      xdot.append(systems_[i]->dynamics(time, state.get(i), input.get(i)));
+      xdot.set(i, systems_[i]->dynamics(time, state.get(i), input.get(i)));
     }
     return xdot;
   }
 
+  // Required by Drake::System concept.
   template <typename ScalarType>
   OutputVector<ScalarType> output(const ScalarType& time,
                                   const StateVector<ScalarType>& state,
                                   const InputVector<ScalarType>& input) const {
-    OutputVector<ScalarType> y;
+    OutputVector<ScalarType> y(systems_.size());
     for (std::size_t i = 0; i < systems_.size(); ++i) {
-      y.append(systems_[i]->output(time, state.get(i), input.get(i)));
+      y.set(i, systems_[i]->output(time, state.get(i), input.get(i)));
     }
     return y;
   }
 
+  // Required by Drake::System concept.
   bool isTimeVarying() const {
     return (systems_.size() > 0) && (systems_[0]->isTimeVarying());
   }
 
+  // Required by Drake::System concept.
   bool isDirectFeedthrough() const {
     return (systems_.size() > 0) && (systems_[0]->isDirectFeedthrough());
   }
 
+  // Required by Drake::System concept.
   std::size_t getNumStates() const {
-    return systems_.size() * UnitSystem::template StateVector<double>::RowsAtCompileTime;
+    return StateVector<double>::rowsFromUnitCount(systems_.size());
   }
 
+  // Required by Drake::System concept.
   std::size_t getNumInputs() const {
-    return systems_.size() * UnitSystem::template InputVector<double>::RowsAtCompileTime;
+    return InputVector<double>::rowsFromUnitCount(systems_.size());
   }
 
+  // Required by Drake::System concept.
   std::size_t getNumOutputs() const {
-    return systems_.size() * UnitSystem::template OutputVector<double>::RowsAtCompileTime;
+    return OutputVector<double>::rowsFromUnitCount(systems_.size());
   }
 
  private:
   std::vector<std::shared_ptr<UnitSystem> > systems_;
 };
+
 } // namespace Drake
