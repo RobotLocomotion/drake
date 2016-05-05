8a56a12d9692d3a57001b4def0014674f3adabee
jwnimmer <jeremy.nimmer@tri.global>
2016-05-05 16:34:37 -0400

    Update to use driving_command.

--- drake/examples/Cars/publish_driving_command.cpp
+++ drake/examples/Cars/publish_driving_command.cpp
@@ -4,28 +4,28 @@
 #include <iostream>
 #include <sstream>
 #include <lcm/lcm-cpp.hpp>
-#include "lcmtypes/drake/lcmt_driving_control_cmd_t.hpp"
+#include "lcmtypes/drake/lcmt_driving_command_t.hpp"
 
 using namespace std;
 
 int main(int argc, char** argv) {
   if (argc < 3)
-    cout << "Usage: publishDrivingCommand throttle_value steering_value"
+    cout << "Usage: publishDrivingCommand throttle steering_angle_rad"
          << endl;
 
   lcm::LCM lcm;
   if (!lcm.good()) return 1;
 
-  drake::lcmt_driving_control_cmd_t command;
+  drake::lcmt_driving_command_t command;
   command.timestamp = 0;
 
   stringstream t(argv[1]);
-  t >> command.throttle_value;
+  t >> command.throttle;
+  command.brake = 0.0;
 
   stringstream s(argv[2]);
-  s >> command.steering_angle;
+  s >> command.steering_angle_rad;
 
-  command.brake_value = 0.0;
   lcm.publish("DRIVING_COMMAND", &command);
 
   return 0;
