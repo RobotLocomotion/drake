RobotLocomotion/drake

commit 308a80495191cc2a1de9aa9ce04eda9417649be1

Use of unique_ptr in parseLink and parseSDFLink to avoid memory leaks

drake/systems/plants/RigidBodyTreeSDF.cpp
@@ -208,7 +208,7 @@ bool parseSDFLink(RigidBodyTree* model, std::string model_name,
   const char* attr = node->Attribute("drake_ignore");
   if (attr && strcmp(attr, "true") == 0) return false;
 
-  RigidBody* body = new RigidBody();
+  std::unique_ptr<RigidBody> body(new RigidBody());
   body->model_name = model_name;
 
   attr = node->Attribute("name");
@@ -229,23 +229,22 @@ bool parseSDFLink(RigidBodyTree* model, std::string model_name,
 
   XMLElement* inertial_node = node->FirstChildElement("inertial");
   if (inertial_node)
-    parseSDFInertial(body, inertial_node, model, pose_map, transform_to_model);
+    parseSDFInertial(body.get(), inertial_node, model, pose_map, transform_to_model);
 
   for (XMLElement* visual_node = node->FirstChildElement("visual"); visual_node;
        visual_node = visual_node->NextSiblingElement("visual")) {
-    parseSDFVisual(body, visual_node, model, package_map, root_dir, pose_map,
+    parseSDFVisual(body.get(), visual_node, model, package_map, root_dir, pose_map,
                    transform_to_model);
   }
 
   for (XMLElement* collision_node = node->FirstChildElement("collision");
        collision_node;
        collision_node = collision_node->NextSiblingElement("collision")) {
-    parseSDFCollision(body, collision_node, model, package_map, root_dir,
+    parseSDFCollision(body.get(), collision_node, model, package_map, root_dir,
                       pose_map, transform_to_model);
   }
 
-  // With this call the tree takes ownership of the body.
-  model->add_rigid_body(std::unique_ptr<RigidBody>(body));
+  model->add_rigid_body(std::move(body));
   *index = body->body_index;
   return true;
 }

drake/systems/plants/RigidBodyTreeURDF.cpp
@@ -289,7 +289,7 @@ bool parseLink(RigidBodyTree* model, std::string robot_name, XMLElement* node,
   const char* attr = node->Attribute("drake_ignore");
   if (attr && strcmp(attr, "true") == 0) return false;
 
-  RigidBody* body = new RigidBody();
+  std::unique_ptr<RigidBody> body(new RigidBody());
   body->model_name = robot_name;
 
   attr = node->Attribute("name");
@@ -301,21 +301,20 @@ bool parseLink(RigidBodyTree* model, std::string robot_name, XMLElement* node,
         "ERROR: do not name a link 'world', it is a reserved name");
 
   XMLElement* inertial_node = node->FirstChildElement("inertial");
-  if (inertial_node) parseInertial(body, inertial_node, model);
+  if (inertial_node) parseInertial(body.get(), inertial_node, model);
 
   for (XMLElement* visual_node = node->FirstChildElement("visual"); visual_node;
        visual_node = visual_node->NextSiblingElement("visual")) {
-    parseVisual(body, visual_node, model, materials, package_map, root_dir);
+    parseVisual(body.get(), visual_node, model, materials, package_map, root_dir);
   }
 
   for (XMLElement* collision_node = node->FirstChildElement("collision");
        collision_node;
        collision_node = collision_node->NextSiblingElement("collision")) {
-    parseCollision(body, collision_node, model, package_map, root_dir);
+    parseCollision(body.get(), collision_node, model, package_map, root_dir);
   }
 
-  // With this call the tree takes ownership of the body.
-  model->add_rigid_body(std::unique_ptr<RigidBody>(body));
+  model->add_rigid_body(std::move(body));
   *index = body->body_index;
   return true;
 }

commit b79d557ec71864a7d1777869e9ca31951692af84

Fixes bug introduced in 308a80495191cc2a1de9aa9ce04eda9417649be1

drake/systems/plants/RigidBodyTreeSDF.cpp
@@ -244,8 +244,7 @@ bool parseSDFLink(RigidBodyTree* model, std::string model_name,
                       pose_map, transform_to_model);
   }
 
-  model->add_rigid_body(std::move(body));
-  *index = body->body_index;
+  *index = model->add_rigid_body(std::move(body));
   return true;
 }
 

drake/systems/plants/RigidBodyTreeURDF.cpp
@@ -314,8 +314,7 @@ bool parseLink(RigidBodyTree* model, std::string robot_name, XMLElement* node,
     parseCollision(body.get(), collision_node, model, package_map, root_dir);
   }
 
-  model->add_rigid_body(std::move(body));
-  *index = body->body_index;
+  *index = model->add_rigid_body(std::move(body));
   return true;
 }
 