89361310785a1f6ba40202de0c14cd588f9c2cba
amcastro-tri <alejandro.castro@tri.global>
2016-05-09 11:03:55 -0400

    Moved RigidBody::CollisionElement::body into DrakeCollision::Element::body_

--- drake/systems/plants/collision/Element.h
+++ drake/systems/plants/collision/Element.h
@@ -10,17 +10,23 @@
 #include "drake/drakeCollision_export.h"
 #include "drake/systems/plants/shapes/DrakeShapes.h"
 
+// Forward declaration.
+// RigidBody's interface is never used by the collision engine.
+// It is however useful the concept of having a CollisionElement attached to a
+// Body.
+class RigidBody;
+
 namespace DrakeCollision {
 typedef uintptr_t ElementId;
 
 class DRAKECOLLISION_EXPORT Element : public DrakeShapes::Element {
  public:
   Element(const Eigen::Isometry3d& T_element_to_local =
-              Eigen::Isometry3d::Identity());
+              Eigen::Isometry3d::Identity(), const RigidBody* const body = nullptr);
 
   Element(const DrakeShapes::Geometry& geometry,
           const Eigen::Isometry3d& T_element_to_local =
-              Eigen::Isometry3d::Identity());
+              Eigen::Isometry3d::Identity(), const RigidBody* const body = nullptr);
 
   virtual ~Element() {}
 
@@ -38,6 +44,18 @@ class DRAKECOLLISION_EXPORT Element : public DrakeShapes::Element {
   virtual bool CollidesWith(const Element* other) const { return true; }
 
   /**
+   * @brief Returns a const reference to the body to which this
+   * CollisionElement is attached.
+   *
+   * @throws A std::runtime_error if the CollisionElement is not attached to a
+   * body.
+   */
+  // TODO(amcastro-tri): getBody() -> get_body()
+  const RigidBody& getBody() const;
+
+  bool is_attached_to_body() const { return body_!= nullptr; }
+
+  /**
    * A toString method for this class.
    */
   friend DRAKECOLLISION_EXPORT std::ostream& operator<<(std::ostream&,
@@ -48,6 +66,7 @@ class DRAKECOLLISION_EXPORT Element : public DrakeShapes::Element {
 
  private:
   ElementId id;
+  const RigidBody* const body_{};
 
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
