ee4d38d040f63bd2a2bce9e48a4a91dedc81bb92
Jonathan Owens <jonathan-owens@users.noreply.github.com>
2016-05-10 13:59:59 -0400

    Remove spurious ampersand

--- run_matlab.bat
+++ run_matlab.bat
@@ -1,1 +1,1 @@
-& 'C:/Program Files/MATLAB/R2015b/bin/matlab.exe' -r "disp('testing 123');" -logfile log.out
+'C:/Program Files/MATLAB/R2015b/bin/matlab.exe' -r "disp('testing 123');" -logfile log.out
