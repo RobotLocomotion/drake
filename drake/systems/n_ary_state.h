a4a4bd64a8e730c617c679cb71d87769e5c0885c
Matt Marjanovic <maddog@tri.global>
2016-05-10 12:04:46 -0700

    NAryState fixes:  - change namespace from Drake to drake  - add unit-tests  - more documentation, API fixups

--- drake/systems/n_ary_state.h
+++ drake/systems/n_ary_state.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <stdexcept>
+
 #include <Eigen/Dense>
 
+#include "drake/core/Vector.h"
 
-namespace Drake {
+namespace drake {
+using Drake::toEigen; // TODO maddog@tri.global  ...until Drake-->drake fully.
 
 template <typename ScalarType,
           template <typename> class UnitVector>
@@ -16,20 +20,30 @@ class NAryState {
 
   NAryState() : count_(unitCountFromRows(0)) {}
 
-  NAryState(int count)
+  explicit NAryState(int count)
       : count_(unitCountFromRows(rowsFromUnitCount(count))),
         combined_vector_(EigenType(rowsFromUnitCount(count), 1)) {}
 
+
+  /// Return the count of @param UnitVector units contained.
+  ///
+  /// If UnitVector is a null vector (zero rows), then the count is
+  /// indeterminate and the return value is always < 0.
+  std::ptrdiff_t count() const { return count_; }
+
+  /// Append the @param unit to the end of the list of component
+  /// @param UnitVectors.
   void append(const UnitVector<ScalarType>& unit) {
     const std::size_t unit_size = UnitVector<ScalarType>::RowsAtCompileTime;
-    if (unit_size <= 0) {
+    if (unit_size == 0) {
       // NOP --- in particular, count_ should remain at -1.
+      assert(count_ == -1);
       return;
     }
     // Enlarge combined_vector_ by size of the converted unit.
     combined_vector_.conservativeResize(combined_vector_.rows() + unit_size,
                                         Eigen::NoChange);
-    // Copy unit's eigen-rep into the tail-end of enlarged combined_vector_.
+    // Copy unit's Eigen-rep into the tail-end of enlarged combined_vector_.
     combined_vector_.bottomRows(unit_size) = toEigen(unit);
     // Keep track of the total unit count.
     ++count_;
@@ -37,6 +51,9 @@ class NAryState {
 
   UnitVector<ScalarType> get(std::size_t i) const {
     const std::size_t unit_size = UnitVector<ScalarType>::RowsAtCompileTime;
+    if (!((unit_size == 0) || (i < count_))) {
+      throw std::out_of_range("");
+    }
     const std::size_t row0 = i * unit_size;
     return UnitVector<ScalarType>(combined_vector_.block(row0, 0,
                                                          unit_size, 1));
@@ -44,6 +61,9 @@ class NAryState {
 
   void set(std::size_t i, const UnitVector<ScalarType>& unit) {
     const std::size_t unit_size = UnitVector<ScalarType>::RowsAtCompileTime;
+    if (!((unit_size == 0) || (i < count_))) {
+      throw std::out_of_range("");
+    }
     const std::size_t row0 = i * unit_size;
     combined_vector_.block(row0, 0, unit_size, 1) = toEigen(unit);
   }
@@ -72,29 +92,50 @@ class NAryState {
     return vec.combined_vector_;
   }
 
+  /// Determine how many @param UnitVector units will be decoded from
+  /// an Eigen column matrix with @param rows rows.  @param rows must
+  /// be a multiple of the row count of @param UnitVector.
+  ///
+  /// As a special case, if UnitVector comprises zero rows (i.e., it is
+  /// a NullVector), then the return value is always -1.
+  ///
+  /// @throws std::domain_error if UnitVector is not a null vector and
+  /// rows is not a multiple of UnitVector::RowsAtCompileTime.
   static
-  std::size_t unitCountFromRows(std::size_t rows) {
+  std::ptrdiff_t unitCountFromRows(std::size_t rows) {
     static_assert(UnitVector<ScalarType>::RowsAtCompileTime != Eigen::Dynamic,
                   "Size of component subvector not known at compile time.");
     if (UnitVector<ScalarType>::RowsAtCompileTime > 0) {
-      assert((rows % UnitVector<ScalarType>::RowsAtCompileTime) == 0);
+      if ((rows % UnitVector<ScalarType>::RowsAtCompileTime) != 0) {
+        throw std::domain_error("Row count not a multiple of non-null unit.");
+      }
       return rows / UnitVector<ScalarType>::RowsAtCompileTime;
     }
     return -1;
   }
 
+  /// Determine how many Eigen matrix rows will be needed to represent
+  /// @param count instances of @param UnitVector.
+  ///
+  /// To complement unitCountFromRows(), if @param count is negative,
+  /// the return value is zero.  However, @throws std::domain_error if
+  /// count is negative and UnitVector is not a null vector.
   static
-  std::size_t rowsFromUnitCount(std::size_t count) {
+  std::size_t rowsFromUnitCount(std::ptrdiff_t count) {
     static_assert(UnitVector<ScalarType>::RowsAtCompileTime != Eigen::Dynamic,
                   "Size of component subvector not known at compile time.");
-    if (count > 0) {
+    if (count >= 0) {
       return count * UnitVector<ScalarType>::RowsAtCompileTime;
+    }
+    if (UnitVector<ScalarType>::RowsAtCompileTime != 0) {
+      throw std::domain_error("Negative count for non-null unit.");
     }
     return 0;
   }
 
  private:
-  std::size_t count_;
+  // count_ < 0 indicates "not counted", i.e., UnitVector is a null vector.
+  std::ptrdiff_t count_;
   EigenType combined_vector_;
 };
 
---
+++ drake/systems/test/n_ary_state_test.cc
@@ -0,0 +1,207 @@
+#include "drake/systems/n_ary_state.h"
+
+#include <stdexcept>
+
+#include <Eigen/Dense>
+
+#include "drake/core/Vector.h"
+
+#include "gtest/gtest.h"
+
+namespace {
+
+using Drake::NullVector;
+using Drake::toEigen;
+using drake::NAryState;
+
+// Vector-concept class for exercising composition.
+template <typename ScalarType>
+class VectorQ {
+ public:
+  static const int RowsAtCompileTime = 3;
+  typedef Eigen::Matrix<ScalarType, RowsAtCompileTime, 1> EigenType;
+
+  VectorQ() {}
+
+  template <typename Derived>
+  explicit VectorQ(const Eigen::MatrixBase<Derived>& initial)
+      : v(initial) {}
+
+  template <typename Derived>
+  VectorQ& operator=(const Eigen::MatrixBase<Derived>& rhs) {
+    v = rhs;
+  }
+
+  friend EigenType toEigen(const VectorQ<ScalarType>& vq) {
+    return vq.v;
+  }
+
+  EigenType v;
+};
+
+using VectorQD = VectorQ<double>;
+
+
+GTEST_TEST(TestNAryState, unitCountFromRows) {
+  EXPECT_EQ((NAryState<double, VectorQ>::unitCountFromRows( 0)), 0);
+  EXPECT_EQ((NAryState<double, VectorQ>::unitCountFromRows(15)), 5);
+  EXPECT_THROW((NAryState<double, VectorQ>::unitCountFromRows(17)),
+               std::domain_error);
+
+  EXPECT_EQ((NAryState<double, NullVector>::unitCountFromRows( 0)), -1);
+  EXPECT_EQ((NAryState<double, NullVector>::unitCountFromRows(15)), -1);
+}
+
+
+GTEST_TEST(TestNAryState, rowsFromUnitCount) {
+  EXPECT_EQ((NAryState<double, VectorQ>::rowsFromUnitCount( 0)),  0);
+  EXPECT_EQ((NAryState<double, VectorQ>::rowsFromUnitCount( 1)),  3);
+  EXPECT_EQ((NAryState<double, VectorQ>::rowsFromUnitCount( 5)), 15);
+  EXPECT_THROW((NAryState<double, VectorQ>::rowsFromUnitCount(-1)),
+               std::domain_error);
+
+  EXPECT_EQ((NAryState<double, NullVector>::rowsFromUnitCount( 1)), 0);
+  EXPECT_EQ((NAryState<double, NullVector>::rowsFromUnitCount( 5)), 0);
+  EXPECT_EQ((NAryState<double, NullVector>::rowsFromUnitCount(-1)), 0);
+}
+
+
+GTEST_TEST(TestNAryState, DefaultConstructor) {
+  VectorQD vq0 = VectorQD((VectorQD::EigenType() << 1.0, 2.0, 3.0).finished());
+  VectorQD vq1 = VectorQD((VectorQD::EigenType() << 4.0, 6.0, 8.0).finished());
+
+  // Test default-constructed instance.
+  NAryState<double, VectorQ> dut;
+
+  EXPECT_EQ(dut.count(), 0);
+  EXPECT_EQ(dut.size(), 0);
+  EXPECT_THROW(dut.get(0), std::out_of_range);
+  EXPECT_THROW(dut.set(0, VectorQD()), std::out_of_range);
+  EXPECT_EQ(toEigen(dut), (Eigen::Matrix<double, 0, 1>()));
+
+  dut.append(vq0);
+  EXPECT_EQ(dut.count(), 1);
+  EXPECT_EQ(dut.size(), 3);
+  EXPECT_EQ(dut.get(0).v, vq0.v);
+  EXPECT_EQ(toEigen(dut), vq0.v);
+
+  dut.append(vq1);
+  EXPECT_EQ(dut.count(), 2);
+  EXPECT_EQ(dut.size(), 6);
+  EXPECT_EQ(dut.get(0).v, vq0.v);
+  EXPECT_EQ(dut.get(1).v, vq1.v);
+  EXPECT_EQ(toEigen(dut),
+            (Eigen::VectorXd(6) << vq0.v, vq1.v).finished());
+
+  EXPECT_THROW(dut.get(2), std::out_of_range);
+  EXPECT_THROW(dut.set(2, vq1), std::out_of_range);
+
+  dut.set(0, vq1);
+  dut.set(1, vq0);
+  EXPECT_EQ(dut.get(0).v, vq1.v);
+  EXPECT_EQ(dut.get(1).v, vq0.v);
+  EXPECT_EQ(toEigen(dut),
+            (Eigen::VectorXd(6) << vq1.v, vq0.v).finished());
+}
+
+
+GTEST_TEST(TestNAryState, PreAllocated) {
+  VectorQD vq0 = VectorQD((VectorQD::EigenType() << 1.0, 2.0, 3.0).finished());
+  VectorQD vq1 = VectorQD((VectorQD::EigenType() << 4.0, 6.0, 8.0).finished());
+
+  // Test construction with pre-allocated size.
+  NAryState<double, VectorQ> dut(2);
+  EXPECT_EQ(dut.count(), 2);
+  EXPECT_EQ(dut.size(), 6);
+
+  dut.set(0, vq0);
+  dut.set(1, vq1);
+  EXPECT_EQ(dut.get(0).v, vq0.v);
+  EXPECT_EQ(dut.get(1).v, vq1.v);
+  EXPECT_EQ(toEigen(dut),
+            (Eigen::VectorXd(6) << vq0.v, vq1.v).finished());
+
+  EXPECT_THROW(dut.get(2), std::out_of_range);
+  EXPECT_THROW(dut.set(2, vq1), std::out_of_range);
+}
+
+
+GTEST_TEST(TestNAryState, FromEigen) {
+  VectorQD vq0 = VectorQD((VectorQD::EigenType() << 1.0, 2.0, 3.0).finished());
+  VectorQD vq1 = VectorQD((VectorQD::EigenType() << 4.0, 6.0, 8.0).finished());
+
+  // Test construction from Eigen type.
+  NAryState<double, VectorQ> dut((
+      Eigen::VectorXd(6) << vq0.v, vq1.v).finished());
+
+  EXPECT_EQ(dut.count(), 2);
+  EXPECT_EQ(dut.size(), 6);
+  EXPECT_EQ(dut.get(0).v, vq0.v);
+  EXPECT_EQ(dut.get(1).v, vq1.v);
+  EXPECT_EQ(toEigen(dut),
+            (Eigen::VectorXd(6) << vq0.v, vq1.v).finished());
+
+  EXPECT_THROW(dut.get(2), std::out_of_range);
+  EXPECT_THROW(dut.set(2, vq1), std::out_of_range);
+
+  // Test assignment from Eigen type.
+  dut = vq0.v;
+  EXPECT_EQ(dut.count(), 1);
+  EXPECT_EQ(dut.size(), 3);
+  EXPECT_EQ(dut.get(0).v, vq0.v);
+  EXPECT_EQ(toEigen(dut), vq0.v);
+
+  dut = Eigen::VectorXd(0);
+  EXPECT_EQ(dut.count(), 0);
+  EXPECT_EQ(dut.size(), 0);
+  EXPECT_THROW(dut.get(0), std::out_of_range);
+  EXPECT_EQ(toEigen(dut), Eigen::VectorXd(0));
+}
+
+
+GTEST_TEST(TestNAryState, NullUnitVectors) {
+  NullVector<double> nv;
+
+  // Test default-constructed instance.
+  NAryState<double, NullVector> dut;
+  EXPECT_EQ(dut.count(), -1);
+  EXPECT_EQ(dut.size(), 0);
+  EXPECT_EQ(dut.get(0), nv);
+  EXPECT_EQ(dut.get(1000), nv);
+  dut.set(0, nv);
+  dut.set(1000, nv);
+  EXPECT_EQ(toEigen(dut), Eigen::VectorXd(0));
+
+  // Appending nothing to nothing should not change nothing.
+  dut.append(nv);
+  EXPECT_EQ(dut.count(), -1);
+  EXPECT_EQ(dut.size(), 0);
+  EXPECT_EQ(dut.get(0), nv);
+  EXPECT_EQ(dut.get(1000), nv);
+  dut.set(0, nv);
+  dut.set(1000, nv);
+  EXPECT_EQ(toEigen(dut), Eigen::VectorXd(0));
+
+  // Test assignment from Eigen type.
+  dut = Eigen::Matrix<double, 0, 1>();
+  EXPECT_EQ(dut.count(), -1);
+  EXPECT_EQ(dut.size(), 0);
+  EXPECT_EQ(dut.get(0), nv);
+  EXPECT_EQ(dut.get(1000), nv);
+  dut.set(0, nv);
+  dut.set(1000, nv);
+  EXPECT_EQ(toEigen(dut), Eigen::VectorXd(0));
+
+  // Test construction from Eigen type.
+  NAryState<double, NullVector> dut2(Eigen::VectorXd(0));
+  EXPECT_EQ(dut2.count(), -1);
+  EXPECT_EQ(dut2.size(), 0);
+  EXPECT_EQ(dut2.get(0), nv);
+  EXPECT_EQ(dut2.get(1000), nv);
+  dut2.set(0, nv);
+  dut2.set(1000, nv);
+  EXPECT_EQ(toEigen(dut2), Eigen::VectorXd(0));
+}
+
+
+}  // namespace
