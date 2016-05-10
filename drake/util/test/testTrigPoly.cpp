1fef5082301ee1d58e76c6e2e56abcb3e29b2c62
Grant Gould <ggould@tri.global>
2016-05-10 15:00:40 -0400

    Unit testing for TrigPoly evaluation

--- drake/util/test/testTrigPoly.cpp
+++ drake/util/test/testTrigPoly.cpp
@@ -14,6 +14,9 @@ namespace drake {
 namespace util {
 namespace {
 
+typedef std::map<TrigPolyd::VarType, double> MapType;
+const double kPi = 3.1415926535897;
+
 void test_serialization(TrigPolyd dut, std::string expect) {
   std::stringstream test_stream;
   test_stream << dut;
@@ -38,6 +41,41 @@ TEST(TrigPolyTest, SmokeTest) {
   test_serialization(cos(p), "c1");
   test_serialization(sin(p) * p * p + cos(p), "s1*q1^2+c1");
   test_serialization(sin(p + p), "s1*c1+c1*s1");
+}
+
+TEST(TrigPolyTest, EvaluateMultivariateTest) {
+  const TrigPolyd theta(Polynomiald("th"),
+                        Polynomiald("sth"), Polynomiald("cth"));
+  const TrigPolyd::VarType theta_var =
+      theta.getPolynomial().getSimpleVariable();
+
+  // Check some basic evaluations.
+  EXPECT_EQ(theta.evaluateMultivariate(MapType {{theta_var, 1}}), 1);
+  EXPECT_EQ(sin(theta).evaluateMultivariate(MapType {{theta_var, 0}}), 0);
+  EXPECT_EQ(cos(theta).evaluateMultivariate(MapType {{theta_var, 0}}), 1);
+
+  // Test that the pythagorean theorem is true for various angles.
+  for (const double angle : {0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4}) {
+    EXPECT_NEAR((sin(theta) * sin(theta) + cos(theta) * cos(theta) - 1)
+                .evaluateMultivariate(MapType {{theta_var, angle}}),
+                0,
+                1e-6);
+  }
+
+  // Test an arbitrary multivariate polynomial.
+  const TrigPolyd phi(Polynomiald("phi"),
+                      Polynomiald("sphi"), Polynomiald("cphi"));
+  const TrigPolyd::VarType phi_var = phi.getPolynomial().getSimpleVariable();
+  const TrigPolyd multivariate = theta + phi * cos(theta) + sin(phi + theta);
+  for (const double theta_value : {0., 1., kPi / 4, kPi / 2, -kPi / 4}) {
+    for (const double phi_value : {0., 1., kPi / 4, kPi / 2, -kPi / 4}) {
+      EXPECT_NEAR(multivariate.evaluateMultivariate(
+          MapType {{theta_var, theta_value}, {phi_var, phi_value}}),
+                  (theta_value + (phi_value * std::cos(theta_value)) +
+                   std::sin(phi_value + theta_value)),
+                  1e-6) << "phi: " << phi_value << " theta: " << theta_value;
+    }
+  }
 }
 
 }  // anonymous namespace
