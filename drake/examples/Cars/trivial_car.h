fec4360c3720ecce790e58d12b4a130bbe9b3e6c
Matt Marjanovic <maddog@tri.global>
2016-05-12 11:04:25 -0700

    Some trivial fixes for PR feedback issues.

--- drake/examples/Cars/trivial_car.h
+++ drake/examples/Cars/trivial_car.h
@@ -14,16 +14,21 @@ namespace drake {
 ///
 class TrivialCar {
  public:
+  /// Construct a TrivialCar system that locates the car at position
+  /// (@p x, @p y) with initial heading @p heading.  The car will rotate
+  /// (changing heading) with a period @p period per revolution.
+  TrivialCar(const double x, const double y, const double heading,
+             const double period)
+      : pose_(x, y, heading), period_(period) {}
+
+  /// @name Implement the Drake::System concept.
+  //@{
   template <typename ScalarType>
   using StateVector = Drake::NullVector<ScalarType>;
   template <typename ScalarType>
   using InputVector = Drake::NullVector<ScalarType>;
   template <typename ScalarType>
   using OutputVector = TrivialCarState<ScalarType>;
-
-  TrivialCar(const double x, const double y, const double heading,
-             const double period)
-      : pose_(x, y, heading), period_(period) {}
 
   template <typename ScalarType>
   StateVector<ScalarType> dynamics(const ScalarType& time,
@@ -35,7 +40,7 @@ class TrivialCar {
   template <typename ScalarType>
   OutputVector<ScalarType> output(const ScalarType& time,
                                   const StateVector<ScalarType>& state,
-                                  const InputVector<ScalarType>& input) const {
+                                  const InputVector<ScalarType>&) const {
     typename OutputVector<ScalarType>::EigenType offset;
     offset << 0., 0., (M_PI * 2. * time / period_);
     return toEigen(pose_) + offset;
@@ -43,6 +48,7 @@ class TrivialCar {
 
   bool isTimeVarying() const { return true; }
   bool isDirectFeedthrough() const { return false; }
+  //@}
 
  private:
   const OutputVector<double> pose_;
