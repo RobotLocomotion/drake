cf3f252eb3ad1bdf8ebea9d767f9e2b29820f528
Jamie Snape <jamie.snape@kitware.com>
2016-05-12 16:19:51 -0400

    More MATLAB timeouts

--- drake/systems/plants/test/testForwardKin.m
+++ drake/systems/plants/test/testForwardKin.m
@@ -10,7 +10,7 @@ options.floating = floatingJointType;
 robot = RigidBodyManipulator('FallingBrick.urdf',options);
 robot = robot.addRobotFromURDF('FallingBrick.urdf', [], [], options);
 for rotation_type = 0 : 2
-  checkGradients(robot, rotation_type);  
+  checkGradients(robot, rotation_type);
   checkMex(robot, rotation_type);
   checkJacobianWithAndWithoutGradients(robot, rotation_type);
 end
@@ -21,7 +21,7 @@ if nargin<2, options=struct(); end
 robot = createAtlas(floatingJointType,options);
 
 for rotation_type = 0 : 2
-  checkGradients(robot, rotation_type);  
+  checkGradients(robot, rotation_type);
   checkMex(robot, rotation_type);
   checkJacobianWithAndWithoutGradients(robot, rotation_type);
 end
@@ -36,21 +36,21 @@ test_number = 1;
 while test_number < n_tests
   base = randi(body_range);
   end_effector = randi(body_range);
-  
+
   if base ~= end_effector
     q = getRandomConfiguration(robot);
     nPoints = randi([1, 10]);
     points = randn(3, nPoints);
-    
+
     geval_options.grad_method = 'taylorvar';
     kinsol_options.use_mex = false;
     kinsol_options.compute_gradients = true;
     kinsol_options_geval.use_mex = kinsol_options.use_mex;
     kinsol = robot.doKinematics(q, [], kinsol_options);
-    
+
     forwardKin_options.rotation_type = rotation_type;
     forwardKin_options.base_or_frame_id = base;
-    
+
     [~, J, dJ] = robot.forwardKin(kinsol, end_effector, points, forwardKin_options);
     [~, J_geval, dJ_geval] = geval(1, @(q) robot.forwardKin(robot.doKinematics(q, [], kinsol_options_geval), end_effector, points, forwardKin_options), q, geval_options);
     valuecheck(J_geval, J, 1e-8);
@@ -72,29 +72,29 @@ for i = 1 : n_tests
 
   base = randi(body_range);
   in_terms_of_qdot_options = [true false];
-  
+
   q = getRandomConfiguration(robot);
   nPoints = randi([1, 10]);
   points = randn(3, nPoints);
-  
+
   kinsol_options.use_mex = false;
   kinsol_options.compute_gradients = true;
   kinsol = robot.doKinematics(q, [], kinsol_options);
   kinsol_options.use_mex = true;
   kinsol_mex = robot.doKinematics(q, [], kinsol_options);
-  
+
   for in_terms_of_qdot = in_terms_of_qdot_options
     options.in_terms_of_qdot = in_terms_of_qdot;
     options.base_or_frame_id = base;
     [x, J, dJ] = robot.forwardKin(kinsol, end_effector, points, options);
     [x_mex, J_mex, dJ_mex] = robot.forwardKin(kinsol_mex, end_effector, points, options);
-    
+
     x_pos = x(1 : end - rotationRepresentationSize(rotation_type), :);
     x_pos_mex = x_mex(1 : end - rotationRepresentationSize(rotation_type), :);
-    
+
     x_rot = x(end - rotationRepresentationSize(rotation_type) + 1 : end, :);
     x_rot_mex = x_mex(end - rotationRepresentationSize(rotation_type) + 1 : end, :);
-    
+
     valuecheck(x_pos_mex, x_pos);
     valuecheckRotations(x_rot, x_rot_mex, rotation_type);
     valuecheck(J_mex, J);
@@ -114,7 +114,7 @@ for i = 1 : n_tests
 
   base = randi(body_range);
   in_terms_of_qdot_options = [true false];
-  
+
   q = getRandomConfiguration(robot);
   nPoints = randi([1, 10]);
   points = randn(3, nPoints);
@@ -123,15 +123,15 @@ for i = 1 : n_tests
   for in_terms_of_qdot = in_terms_of_qdot_options
     options.in_terms_of_qdot = in_terms_of_qdot;
     options.base_or_frame_id = base;
-    
+
     kinsol_options.compute_gradients = false;
     kinsol = robot.doKinematics(q, [], kinsol_options);
     [x, J] = robot.forwardKin(kinsol, end_effector, points, options);
-    
+
     kinsol_options.compute_gradients = true;
     kinsol_with_gradients = robot.doKinematics(q, [], kinsol_options);
     [x_with_gradients, J_with_gradients] = robot.forwardKin(kinsol_with_gradients, end_effector, points, options);
-    
+
     valuecheck(x_with_gradients, x);
     valuecheck(J_with_gradients, J);
   end
@@ -161,3 +161,5 @@ switch rotation_type
     error('rotation type not recognized');
 end
 end
+
+% TIMEOUT 1500
