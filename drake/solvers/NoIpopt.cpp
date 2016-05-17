54a3301fab810caca450774821ccbaabe42c7cdb
Sam Creasey <sam.creasey@tri.global>
2016-05-17 16:14:53 -0400

    Framework for ipopt wrapper.  Does most of the setup work but doesn't evaluate anything yet.

---
+++ drake/solvers/NoIpopt.cpp
@@ -0,0 +1,15 @@
+
+#include "drake/solvers/IpoptSolver.h"
+
+#include <stdexcept>
+
+bool drake::solvers::IpoptSolver::available() const {
+  return false;
+}
+
+drake::solvers::SolutionResult drake::solvers::IpoptSolver::Solve(
+    OptimizationProblem &prog) const {
+  throw std::runtime_error(
+      "The IPOPT bindings were not compiled.  You'll need to use a different "
+      "solver.");
+}
