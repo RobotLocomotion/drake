517be1fc7408ff67097e575d8bb1bed8489916b6
David German <david.german@tri.global>
2016-05-17 21:37:08 -0400

    Defer state to a follow-up to PR #2308.

--- drake/systems/framework/continuous_system_interface.h
+++ drake/systems/framework/continuous_system_interface.h
@@ -16,46 +16,7 @@ class ContinuousSystemInterface : public SystemInterface<T> {
  public:
   virtual ~ContinuousSystemInterface() {}
 
-  /// Produces the derivatives of the generalized position q with respect to
-  /// time. The derivatives vector `qdot` will be the same length as
-  /// Context.state.continuous_state.generalized_position.
-  ///
-  /// @param derivatives Must never be nullptr. Implementations of this
-  ///                    interface are not obliged to check, since this is a
-  ///                    performance-sensitive inner loop function.
-  virtual void GetTimeDerivativesOfGeneralizedPosition(
-      const Context<T>& context, VectorInterface<T>* derivatives) const = 0;
-
-  /// Produces the derivatives of the generalized velocity v with respect to
-  /// time. The derivatives vector `vdot` (generalized acceleration) will be
-  /// the same length as Context.state.continuous_state.generalized_velocity.
-  ///
-  /// @param derivatives Must never be nullptr. Implementations of this
-  ///                    interface are not obliged to check, since this is a
-  ///                    performance-sensitive inner loop function.
-  virtual void GetTimeDerivativesOfGeneralizedVelocity(
-      const Context<T>& context, VectorInterface<T>* derivatives) const = 0;
-
-  /// Produces the derivatives of the other continuous state `z` with respect
-  /// to time. The derivatives vector `zdot` will be the same length as
-  /// Context.state.continuous_state.other_continuous_state.
-  ///
-  /// @param derivatives Must never be nullptr. Implementations of this
-  ///                    interface are not obliged to check, since this is a
-  ///                    performance-sensitive inner loop function.
-  virtual void GetTimeDerivativesOfOtherContinuousState(
-      const Context<T>& context, VectorInterface<T>* derivatives) const = 0;
-
-  /// Transforms the given velocity (v) to the derivative of the configuration
-  /// (qdot). The transformation must be linear (qdot = N(q) * v), and it must
-  /// require no more than O(N) time to compute in the number of generalized
-  /// velocity states.
-  ///
-  /// @param derivatives Must never be nullptr. Implementations of this
-  ///                    interface are not obliged to check, since this is a
-  ///                    performance-sensitive inner loop function.
-  virtual void MapVelocityToConfigurationDerivatives(
-      const Context<T>& context, VectorInterface<T>* derivatives) const = 0;
+  // TODO(david-german-tri): Add state derivatives.
 
  protected:
   ContinuousSystemInterface() {}
--- drake/systems/framework/primitives/test/adder_test.cc
+++ drake/systems/framework/primitives/test/adder_test.cc
@@ -28,19 +28,19 @@ class AdderTest : public ::testing::Test {
 
 TEST_F(AdderTest, AddTwoVectors) {
   // Hook up two inputs of the expected size.
-  ASSERT_EQ(2, context_->get_mutable_input()->continuous_ports.size());
+  ASSERT_EQ(2, context_->get_mutable_input()->ports.size());
   BasicVector<double> input0(3 /* length */);
   BasicVector<double> input1(3 /* length */);
   input0.get_mutable_value() << 1, 2, 3;
   input1.get_mutable_value() << 4, 5, 6;
-  context_->get_mutable_input()->continuous_ports[0].input = &input0;
-  context_->get_mutable_input()->continuous_ports[1].input = &input1;
+  context_->get_mutable_input()->ports[0].input = &input0;
+  context_->get_mutable_input()->ports[1].input = &input1;
 
   adder_->Output(*context_, output_.get());
 
-  ASSERT_EQ(1, output_->continuous_ports.size());
+  ASSERT_EQ(1, output_->ports.size());
   BasicVector<double>* output_port = dynamic_cast<BasicVector<double>*>(
-      output_->continuous_ports[0].output.get());
+      output_->ports[0].output.get());
   ASSERT_NE(nullptr, output_port);
   Eigen::Matrix<double, 3, 1> expected;
   expected << 5, 7, 9;
@@ -52,7 +52,7 @@ TEST_F(AdderTest, AddTwoVectors) {
 TEST_F(AdderTest, WrongNumberOfInputPorts) {
   // Hook up just one input.
   BasicVector<double> input0(3 /* length */);
-  context_->get_mutable_input()->continuous_ports[0].input = &input0;
+  context_->get_mutable_input()->ports[0].input = &input0;
 
   EXPECT_THROW(adder_->Output(*context_, output_.get()), std::runtime_error);
 }
@@ -61,21 +61,18 @@ TEST_F(AdderTest, WrongNumberOfInputPorts) {
 // are connected.
 TEST_F(AdderTest, WrongSizeOfInputPorts) {
   // Hook up two inputs, but one of them is the wrong size.
-  ASSERT_EQ(2, context_->get_mutable_input()->continuous_ports.size());
+  ASSERT_EQ(2, context_->get_mutable_input()->ports.size());
   BasicVector<double> input0(3 /* length */);
   BasicVector<double> input1(2 /* length */);
-  context_->get_mutable_input()->continuous_ports[0].input = &input0;
-  context_->get_mutable_input()->continuous_ports[1].input = &input1;
+  context_->get_mutable_input()->ports[0].input = &input0;
+  context_->get_mutable_input()->ports[1].input = &input1;
 
   EXPECT_THROW(adder_->Output(*context_, output_.get()), std::runtime_error);
 }
 
 // Tests that Adder allocates no state variables in the context_.
 TEST_F(AdderTest, AdderIsStateless) {
-  EXPECT_EQ(nullptr,
-            context_->get_state().continuous_state.generalized_velocities);
-  EXPECT_EQ(nullptr,
-            context_->get_state().continuous_state.generalized_position);
+  /// TODO(david-german-tri): Once state exists in Context, assert it's empty.
 }
 
 }  // namespace
