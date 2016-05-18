64c04e08597b285d6e512ffe743ee07ec3a6f688
amcastro-tri <alejandro.castro@tri.global>
2016-05-18 15:00:07 -0400

    Moved doc to Model::

--- drake/systems/plants/collision/bullet_model.h
+++ drake/systems/plants/collision/bullet_model.h
@@ -113,40 +113,10 @@ class BulletModel : public Model {
    */
   std::vector<PointPair> potentialCollisionPoints(bool use_margins) override;
 
-  // @brief Tests if any of the points supplied in input_points collides with
-  // any part of the model within a given threshold.
-  //
-  // In other words, this method tests if any of the spheres of radius
-  // collision_threshold located at input_points[i] collides with any part of
-  // the model. This method returns as soon as any of these spheres collides
-  // with the model.
-  //
-  // @param input_points The list of points to check for collisions against the
-  // model.
-  // @param collision_threshold The radius of a control sphere around each point
-  // used to check for collisions with the model.
-  //
-  // @return `true` if any of the points positively checks for collision.
-  // `false` otherwise.
   bool collidingPointsCheckOnly(
       const std::vector<Eigen::Vector3d>& input_points,
       double collision_threshold) override;
 
-  // @brief Tests which of the points supplied in input_points collides with
-  // any part of the model within a given threshold.
-  //
-  // In other words, this method tests if a sphere of radius
-  // collision_threshold located at input_points[i] collides with any part of
-  // the model. The result is returned as a vector of indexes in input_points
-  // that do collide with the model.
-  //
-  // @param input_points The list of points to check for collisions against the
-  // model.
-  // @param collision_threshold The radius of a control sphere around each point
-  // used to check for collisions with the model.
-  //
-  // @return A vector with indexes in input_points of all those points that do
-  // collide with the model within the specified threshold.
   std::vector<size_t> collidingPoints(
       const std::vector<Eigen::Vector3d>& input_points,
       double collision_threshold) override;
