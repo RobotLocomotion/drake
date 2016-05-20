166a970b297632c13830e3c62669efc139e48ec9
David German <david.german@tri.global>
2016-05-20 16:37:47 -0400

    Introduce ContinuousState, ContinuousSystemInterface, and some supporting infrastructure.  Introduce the SpringMassSystem example.

---
+++ drake/examples/spring_mass/spring_mass_system.cc
@@ -0,0 +1,125 @@
+#include "drake/examples/spring_mass/spring_mass_system.h"
+
+namespace drake {
+
+using systems::BasicVector;
+using systems::Context;
+using systems::ContinuousState;
+using systems::OutputPort;
+using systems::StateVectorInterface;
+using systems::SystemOutput;
+using systems::VectorInterface;
+using systems::VectorX;
+
+namespace examples {
+
+namespace {
+const ptrdiff_t kStateSize = 2;
+}  // namespace
+
+SpringMassStateVector::SpringMassStateVector(double initial_position,
+                                             double initial_velocity)
+    : BasicStateVector<double>(std::unique_ptr<BasicVector<double>>(
+          new BasicVector<double>(kStateSize))) {
+  set_position(initial_position);
+  set_velocity(initial_velocity);
+}
+
+SpringMassStateVector::~SpringMassStateVector() {}
+
+  // Order matters: Position (q) precedes velocity (v) in ContinuousState.
+double SpringMassStateVector::get_position() const { return GetAtIndex(0); }
+double SpringMassStateVector::get_velocity() const { return GetAtIndex(1); }
+void SpringMassStateVector::set_position(double q) { SetAtIndex(0, q); }
+void SpringMassStateVector::set_velocity(double v) { SetAtIndex(1, v); }
+
+SpringMassOutputVector::SpringMassOutputVector()
+    : BasicVector<double>(kStateSize) {}
+
+SpringMassOutputVector::~SpringMassOutputVector() {}
+
+double SpringMassOutputVector::get_position() const { return get_value()[0]; }
+double SpringMassOutputVector::get_velocity() const { return get_value()[1]; }
+
+void SpringMassOutputVector::set_position(double q) {
+  get_mutable_value()[0] = q;
+}
+
+void SpringMassOutputVector::set_velocity(double v) {
+  get_mutable_value()[1] = v;
+}
+
+SpringMassSystem::SpringMassSystem(const std::string& name,
+                                   double spring_constant_N_per_m,
+                                   double mass_kg)
+    : name_(name),
+      spring_constant_N_per_m_(spring_constant_N_per_m),
+      mass_kg_(mass_kg) {}
+
+SpringMassSystem::~SpringMassSystem() {}
+
+std::string SpringMassSystem::get_name() const { return name_; }
+
+// Reserve a context with no input, and a SpringMassStateVector state.
+std::unique_ptr<Context<double>> SpringMassSystem::CreateDefaultContext()
+    const {
+  std::unique_ptr<Context<double>> context(new Context<double>);
+  std::unique_ptr<SpringMassStateVector> state(new SpringMassStateVector(0, 0));
+  context->get_mutable_state()->continuous_state.reset(
+      new ContinuousState<double>(std::move(state), 1 /* size of q */,
+                                  1 /* size of v */, 0 /* size of z */));
+  return context;
+}
+
+std::unique_ptr<SystemOutput<double>> SpringMassSystem::AllocateOutput() const {
+  std::unique_ptr<SystemOutput<double>> output(new SystemOutput<double>);
+  {
+    OutputPort<double> port;
+    port.vector_output.reset(new SpringMassOutputVector());
+    output->ports.push_back(std::move(port));
+  }
+  return output;
+}
+
+std::unique_ptr<StateVectorInterface<double>>
+SpringMassSystem::AllocateStateDerivatives() const {
+  return std::unique_ptr<StateVectorInterface<double>>(
+      new SpringMassStateVector(0, 0));
+}
+
+// Assign the state to the output.
+void SpringMassSystem::Output(const Context<double>& context,
+                              SystemOutput<double>* output) const {
+  // TODO(david-german-tri): Add a cast that is dynamic_cast in Debug mode,
+  // and static_cast in Release mode.
+  // TODO(david-german-tri): Cache the output of this function.
+  const SpringMassStateVector& state =
+      dynamic_cast<const SpringMassStateVector&>(
+          context.get_state().continuous_state->get_state());
+  SpringMassOutputVector* output_vector = dynamic_cast<SpringMassOutputVector*>(
+      output->ports[0].vector_output.get());
+  output_vector->set_position(state.get_position());
+  output_vector->set_velocity(state.get_velocity());
+}
+
+// Compute the actual physics.
+void SpringMassSystem::Dynamics(
+    const Context<double>& context,
+    StateVectorInterface<double>* derivatives) const {
+  // TODO(david-german-tri): Cache the output of this function.
+  const SpringMassStateVector& state =
+      dynamic_cast<const SpringMassStateVector&>(
+          context.get_state().continuous_state->get_state());
+  SpringMassStateVector* derivative_vector =
+      dynamic_cast<SpringMassStateVector*>(derivatives);
+
+  // The derivative of position is velocity.
+  derivative_vector->set_position(state.get_velocity());
+
+  // The derivative of velocity is spring force divided by mass.
+  const double spring_force = -spring_constant_N_per_m_ * state.get_position();
+  derivative_vector->set_velocity(spring_force / mass_kg_);
+}
+
+}  // namespace examples
+}  // namespace drake
---
+++ drake/systems/framework/continuous_system.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+#include "drake/systems/framework/context.h"
+#include "drake/systems/framework/continuous_system_interface.h"
+#include "drake/systems/framework/state_vector_interface.h"
+
+namespace drake {
+namespace systems {
+
+/// An abstract base class template for Systems that have continuous dynamics.
+/// Defines some reasonable default implementations.
+template <typename T>
+class ContinuousSystem : public ContinuousSystemInterface<T> {
+ public:
+  ~ContinuousSystem() override {}
+
+  /// Applies the identity mapping. Throws std::out_of_range if the
+  /// @p generalized_velocity and @p configuration_derivatives are not the
+  /// same size. Child classes should override this function if qdot != v.
+  void MapVelocityToConfigurationDerivatives(
+      const Context<T>& context,
+      const StateVectorInterface<T>& generalized_velocity,
+      StateVectorInterface<T>* configuration_derivatives) const override {
+    if (generalized_velocity.size() != configuration_derivatives->size()) {
+      throw std::out_of_range(
+          "generalized_velocity.size() " +
+          std::to_string(generalized_velocity.size()) +
+          " != configuration_derivatives.size() " +
+          std::to_string(configuration_derivatives->size()) +
+          ". Do you need to override the default implementation of " +
+          "MapVelocityToConfigurationDerivatives?");
+    }
+
+    for (ptrdiff_t i = 0; i < generalized_velocity.size(); ++i) {
+      configuration_derivatives->SetAtIndex(
+          i, generalized_velocity.GetAtIndex(i));
+    }
+  }
+
+ protected:
+  ContinuousSystem() {}
+
+ private:
+  // ContinuousSystem objects are neither copyable nor moveable.
+  ContinuousSystem(const ContinuousSystem<T>& other) = delete;
+  ContinuousSystem& operator=(
+      const ContinuousSystem<T>& other) = delete;
+  ContinuousSystem(ContinuousSystem<T>&& other) = delete;
+  ContinuousSystem& operator=(ContinuousSystem<T>&& other) =
+      delete;
+};
+
+}  // namespace systems
+}  // namespace drake
