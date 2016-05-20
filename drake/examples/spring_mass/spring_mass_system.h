a9430ff10a8d865494b2a8d44965ef8ba60ada2f
David German <david.german@tri.global>
2016-05-20 16:37:47 -0400

    Introduce ContinuousState, ContinuousSystemInterface, and some supporting infrastructure.  Introduce the SpringMassSystem example.

---
+++ drake/examples/spring_mass/spring_mass_system.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "drake/systems/framework/basic_state_vector.h"
+#include "drake/systems/framework/basic_vector.h"
+#include "drake/systems/framework/context.h"
+#include "drake/systems/framework/continuous_system.h"
+#include "drake/systems/framework/state_vector_interface.h"
+#include "drake/systems/framework/system_output.h"
+
+namespace drake {
+namespace examples {
+
+/// The state of a one-dimensional spring-mass system, consisting of the
+/// position and velocity of the mass, in meters.
+class SpringMassStateVector : public systems::BasicStateVector<double> {
+ public:
+  /// @param initial_position The position of the mass in meters.
+  /// @param initial_velocity The velocity of the mass in meters / second.
+  SpringMassStateVector(double initial_position, double initial_velocity);
+  ~SpringMassStateVector() override;
+
+  /// Returns the position of the mass in meters, where zero is the point
+  /// where the spring exerts no force.
+  double get_position() const;
+
+  /// Sets the position of the mass in meters.
+  void set_position(double v);
+
+  /// Returns the velocity of the mass in meters per second.
+  double get_velocity() const;
+
+  /// Sets the velocity of the mass in meters per second.
+  void set_velocity(double v);
+};
+
+/// The output of a one-dimensional spring-mass system, consisting of the
+/// position and velocity of the mass, in meters.
+class SpringMassOutputVector : public systems::BasicVector<double> {
+ public:
+  SpringMassOutputVector();
+  ~SpringMassOutputVector() override;
+
+  /// Returns the position of the mass in meters, where zero is the point
+  /// where the spring exerts no force.
+  double get_position() const;
+
+  /// Sets the position of the mass in meters.
+  void set_position(double v);
+
+  /// Returns the velocity of the mass in meters per second.
+  double get_velocity() const;
+
+  /// Sets the velocity of the mass in meters per second.
+  void set_velocity(double v);
+};
+
+/// A model of a one-dimensional spring-mass system.
+///
+/// @verbatim
+/// |-----\/\/ k /\/\----( m )  +x
+/// @endverbatim
+class SpringMassSystem : public systems::ContinuousSystem<double> {
+ public:
+  SpringMassSystem(const std::string& name, double spring_constant_N_per_m,
+                   double mass_kg);
+
+  ~SpringMassSystem() override;
+
+  std::string get_name() const override;
+
+  /// Allocates a state of type SpringMassStateVector.
+  /// Allocates no input ports.
+  std::unique_ptr<systems::Context<double>> CreateDefaultContext()
+      const override;
+
+  /// Allocates a single output port of type SpringMassStateVector.
+  std::unique_ptr<systems::SystemOutput<double>> AllocateOutput()
+      const override;
+
+  /// Allocates state derivatives of type SpringMassStateVector.
+  std::unique_ptr<systems::StateVectorInterface<double>>
+  AllocateStateDerivatives() const override;
+
+  void Output(const systems::Context<double>& context,
+              systems::SystemOutput<double>* output) const override;
+
+  void Dynamics(
+      const systems::Context<double>& context,
+      systems::StateVectorInterface<double>* derivatives) const override;
+
+ private:
+  const std::string name_;
+  const double spring_constant_N_per_m_;
+  const double mass_kg_;
+};
+
+}  // namespace examples
+}  // namespace drake
---
+++ drake/examples/spring_mass/test/spring_mass_system_test.cc
@@ -0,0 +1,144 @@
+#include "drake/examples/spring_mass/spring_mass_system.h"
+
+#include <memory>
+
+#include <Eigen/Dense>
+#include "gtest/gtest.h"
+
+#include "drake/systems/framework/context.h"
+#include "drake/systems/framework/state.h"
+#include "drake/systems/framework/state_subvector.h"
+#include "drake/systems/framework/state_vector_interface.h"
+#include "drake/systems/framework/system_output.h"
+#include "drake/util/eigen_matrix_compare.h"
+
+namespace drake {
+
+using systems::Context;
+using systems::ContinuousState;
+using systems::StateSubvector;
+using systems::StateVectorInterface;
+using systems::SystemOutput;
+using util::MatrixCompareType;
+
+namespace examples {
+namespace {
+
+const double kSpring = 300.0;  // N/m
+const double kMass = 2.0;      // kg
+
+class SpringMassSystemTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    // Construct the system I/O objects.
+    system_.reset(new SpringMassSystem("test_system", kSpring, kMass));
+    context_ = system_->CreateDefaultContext();
+    system_output_ = system_->AllocateOutput();
+    erased_derivatives_ = system_->AllocateStateDerivatives();
+
+    // Set up some convenience pointers.
+    state_ = dynamic_cast<SpringMassStateVector*>(
+        context_->get_mutable_state()->continuous_state->get_mutable_state());
+    output_ = dynamic_cast<SpringMassOutputVector*>(
+        system_output_->ports[0].vector_output.get());
+    derivatives_ =
+        dynamic_cast<SpringMassStateVector*>(erased_derivatives_.get());
+  }
+
+  void InitializeState(const double position, const double velocity) {
+    state_->set_position(position);
+    state_->set_velocity(velocity);
+  }
+
+ protected:
+  std::unique_ptr<SpringMassSystem> system_;
+  std::unique_ptr<Context<double>> context_;
+  std::unique_ptr<SystemOutput<double>> system_output_;
+  SpringMassStateVector* state_;
+  SpringMassOutputVector* output_;
+  SpringMassStateVector* derivatives_;
+
+ private:
+  std::unique_ptr<StateVectorInterface<double>> erased_derivatives_;
+};
+
+TEST_F(SpringMassSystemTest, Name) {
+  EXPECT_EQ("test_system", system_->get_name());
+}
+
+// Tests that state is passed through to the output.
+TEST_F(SpringMassSystemTest, Output) {
+  InitializeState(0.1, 0.0);  // Displacement 100cm, no velocity.
+  system_->Output(*context_, system_output_.get());
+  ASSERT_EQ(1, system_output_->ports.size());
+
+  // Check the output through the application-specific interface.
+  EXPECT_NEAR(0.1, output_->get_position(), 1e-8);
+  EXPECT_EQ(0.0, output_->get_velocity());
+
+  // Check the output through the VectorInterface API.
+  ASSERT_EQ(2, output_->size());
+  EXPECT_NEAR(0.1, output_->get_value()[0], 1e-8);
+  EXPECT_EQ(0.0, output_->get_value()[1]);
+}
+
+// Tests that second-order structure is exposed in the state.
+TEST_F(SpringMassSystemTest, SecondOrderStructure) {
+  InitializeState(1.2, 3.4);  // Displacement 1.2m, velocity 3.4m/sec.
+  ContinuousState<double>* continuous_state =
+      context_->get_mutable_state()->continuous_state.get();
+  ASSERT_EQ(1, continuous_state->get_generalized_position().size());
+  ASSERT_EQ(1, continuous_state->get_generalized_velocity().size());
+  ASSERT_EQ(0, continuous_state->get_misc_continuous_state().size());
+  EXPECT_NEAR(1.2, continuous_state->get_generalized_position().GetAtIndex(0),
+              1e-8);
+  EXPECT_NEAR(3.4, continuous_state->get_generalized_velocity().GetAtIndex(0),
+              1e-8);
+}
+
+// Tests that second-order structure can be processed in
+// MapVelocityToConfigurationDerivative.
+TEST_F(SpringMassSystemTest, MapVelocityToConfigurationDerivative) {
+  InitializeState(1.2, 3.4);  // Displacement 1.2m, velocity 3.4m/sec.
+  ContinuousState<double>* continuous_state =
+      context_->get_mutable_state()->continuous_state.get();
+
+  // Slice just the configuration derivatives out of the derivative
+  // vector.
+  StateSubvector<double> configuration_derivatives(derivatives_, 0, 1);
+
+  system_->MapVelocityToConfigurationDerivatives(
+      *context_, continuous_state->get_generalized_velocity(),
+      &configuration_derivatives);
+
+  EXPECT_NEAR(3.4, derivatives_->get_position(), 1e-8);
+  EXPECT_NEAR(3.4, configuration_derivatives.GetAtIndex(0), 1e-8);
+}
+
+TEST_F(SpringMassSystemTest, ForcesPositiveDisplacement) {
+  InitializeState(0.1, 0.1);  // Displacement 0.1m, velocity 0.1m/sec.
+  system_->Dynamics(*context_, derivatives_);
+
+  ASSERT_EQ(2, derivatives_->size());
+  // The derivative of position is velocity.
+  EXPECT_NEAR(0.1, derivatives_->get_position(), 1e-8);
+  // The derivative of velocity is force over mass.
+  EXPECT_NEAR(-kSpring * 0.1 / kMass, derivatives_->get_velocity(), 1e-8);
+}
+
+TEST_F(SpringMassSystemTest, ForcesNegativeDisplacement) {
+  InitializeState(-0.1, 0.2);  // Displacement -0.1m, velocity 0.2m/sec.
+  system_->Dynamics(*context_, derivatives_);
+
+  ASSERT_EQ(2, derivatives_->size());
+  // The derivative of position is velocity.
+  EXPECT_NEAR(0.2, derivatives_->get_position(), 1e-8);
+  // The derivative of velocity is force over mass.
+  EXPECT_NEAR(-kSpring * -0.1 / kMass, derivatives_->get_velocity(), 1e-8);
+}
+
+// TODO(david-german-tri, sherm1): Add test cases using integration.
+
+}  // namespace
+}  // namespace examples
+}  // namespace drake
