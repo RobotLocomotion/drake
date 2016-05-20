59167e88cdffc3efab1795c99c4e799f0d5944e6
amcastro-tri <alejandro.castro@tri.global>
2016-05-20 16:21:35 -0400

    Removed parentheses around initialization lists

--- drake/common/test/have_intersection_test.cc
+++ drake/common/test/have_intersection_test.cc
@@ -13,14 +13,13 @@ namespace {
 
 // Test with std::vector specific SortedVectorsHaveIntersection.
 GTEST_TEST(TestHaveIntersection, SortedVectorsHaveIntersection) {
-  // In order for drake::SortedVectorsHaveIntersection to work vectors must:
-  // 1. Not have repeated elements.
-  // 2. Be sorted.
+  // In order for drake::SortedVectorsHaveIntersection to work vectors must be
+  // sorted.
   // An std::vector can have non sorted repeated elements. Therefore the user
   // must ensure that the previous conditions are met.
-  vector<int> set1 = vector<int>({2, 9, 11, 15, 23});
-  vector<int> set2 = vector<int>({9, 11, 13});
-  vector<int> set3 = vector<int>({1, 8, 13});
+  vector<int> set1 = vector<int>{2, 9, 11, 15, 23};
+  vector<int> set2 = vector<int>{9, 11, 13};
+  vector<int> set3 = vector<int>{1, 8, 13};
 
   // set1 intersects set2 (elements 9 and 11 in common).
   EXPECT_TRUE(SortedVectorsHaveIntersection(set1, set2));
@@ -41,8 +40,8 @@ GTEST_TEST(TestHaveIntersection, SortedVectorsHaveIntersection) {
   EXPECT_TRUE(SortedVectorsHaveIntersection(set3, set2));
 
   // Empty sets.
-  vector<int> set4;
-  vector<int> set5;
+  vector<int> set4{};
+  vector<int> set5{};
   // Two empty sets.
   EXPECT_FALSE(SortedVectorsHaveIntersection(set4, set5));
 
@@ -53,7 +52,7 @@ GTEST_TEST(TestHaveIntersection, SortedVectorsHaveIntersection) {
   EXPECT_FALSE(SortedVectorsHaveIntersection(set4, set1));
 
   // Disjoint sets.
-  vector<int> set6 = vector<int>({30, 35, 50});
+  vector<int> set6 = vector<int>{30, 35, 50};
   // set1 entries are all smaller than set6 first entry.
   EXPECT_FALSE(SortedVectorsHaveIntersection(set1, set6));
 
@@ -61,9 +60,9 @@ GTEST_TEST(TestHaveIntersection, SortedVectorsHaveIntersection) {
   EXPECT_FALSE(SortedVectorsHaveIntersection(set6, set1));
 
   // Sets with repeated entries
-  vector<int> set7 = vector<int>({2, 9, 9, 11, 15, 15, 23});
-  vector<int> set8 = vector<int>({9, 11, 11, 13});
-  vector<int> set9 = vector<int>({1, 1, 8, 13});
+  vector<int> set7 = vector<int>{2, 9, 9, 11, 15, 15, 23};
+  vector<int> set8 = vector<int>{9, 11, 11, 13};
+  vector<int> set9 = vector<int>{1, 1, 8, 13};
 
   // set7 intersects set8 (elements 9 and 11 in common).
   EXPECT_TRUE(SortedVectorsHaveIntersection(set7, set8));
