803c9e04b52c19a80e3e5b18c5d3c4873c636d74
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-05-26 12:06:34 -0400

    Add FCLModel to models that can be created
    Add an FCL option to the list of possible model types that can be
    created with DrakeCollision::newModel(TYPE). Make said API public.
    Implement path for AUTO model type.

--- drake/systems/plants/collision/DrakeCollision.h
+++ drake/systems/plants/collision/DrakeCollision.h
@@ -16,6 +16,9 @@ static const int MAX_NUM_COLLISION_FILTER_GROUPS = 128;
 
 namespace DrakeCollision {
 
+enum DRAKECOLLISION_EXPORT ModelType { NONE, AUTO, BULLET, FCL };
+
+DRAKECOLLISION_EXPORT std::unique_ptr<Model> newModel(ModelType);
 DRAKECOLLISION_EXPORT std::unique_ptr<Model> newModel();
 
 typedef std::bitset<MAX_NUM_COLLISION_FILTER_GROUPS> bitmask;
