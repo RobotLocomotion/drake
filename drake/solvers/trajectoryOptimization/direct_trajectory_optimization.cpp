41c1aa684515c4e991a67c3db95d01a2050cb0da
Lucy.tri <lucy.gibson@tri.global>
2016-06-09 10:27:03 -0400

    Port of DirectTrajectoryOptimization from Matlab to C++, WIP

---
+++ drake/solvers/trajectoryOptimization/direct_trajectory_optimization.cpp
@@ -0,0 +1,44 @@
+#include "direct_trajectory_optimization.h"
+
+namespace drake { 
+namespace solvers {
+
+const Drake::DecisionVariableView setupVariables(OptimizationProblem optProblem,
+    size_t N, int numStates, int numInputs) {
+  int nH = N-1;
+  int nX = numStates;
+  int nU = numInputs;
+  int numVars = nH + N * (nX + nU);
+
+  Drake::DecisionVariableView vars =
+    optProblem.AddContinuousVariables(numVars, "h"); 
+    // !!! Looks like we have to create 3 sets of decision vars, for h, x, u: 
+    // !!! Should call AddContinuousVariables 3x. 
+    // Add costs and constraints.
+  return vars;
+}
+
+DirectTrajectoryOptimization::DirectTrajectoryOptimization(
+    const int numInputs, 
+    const int numStates, 
+    const size_t numTimeSamples,
+    const int trajectoryTimeLowerBound,
+    const int trajectoryTimeUpperBound
+    // options TODO
+    ) : numInputs_(numInputs),
+        numStates_(numStates),
+        numTimeSamples_(numTimeSamples) {
+    
+  Drake::DecisionVariableView vars = setupVariables(optProblem_, 
+    numTimeSamples_, numStates_, numInputs_);
+  
+  (void)vars; // !!! temp to avoid warnings.
+  
+  // TODO create LinearConstraint from upper & lower bounds.
+  // AddLinearConstraint 
+  // create and add bounding box constraint.
+  // addDynamicConstraints
+  }
+    
+}  // solvers
+}  // drake
