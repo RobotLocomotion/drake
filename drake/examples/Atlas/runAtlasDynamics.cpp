609779ef8238f7bdafbb68b706f2d0feb39604f8
amcastro-tri <alejandro.castro@tri.global>
2016-06-10 14:23:27 -0400

    Removes global using's + wraps code around try+catch

--- drake/examples/Atlas/runAtlasDynamics.cpp
+++ drake/examples/Atlas/runAtlasDynamics.cpp
@@ -4,34 +4,38 @@
 #include "atlas_system.h"
 #include "drake/systems/plants/BotVisualizer.h"
 
-using namespace std;
-using namespace Drake;
-using namespace Eigen;
-
+using Drake::SimulationOptions;
+using Drake::BotVisualizer;
 using drake::AtlasSystem;
 
 int main(int argc, char* argv[]) {
-  SimulationOptions options;
+  try {
+    SimulationOptions options;
 
-  // Get the final time of the simulation.
-  double final_time =
-      argc >= 2 ? atof(argv[1]) : std::numeric_limits<double>::infinity();
+    // Get the final time of the simulation.
+    double final_time =
+        argc >= 2 ? atof(argv[1]) : std::numeric_limits<double>::infinity();
 
-  shared_ptr<lcm::LCM> lcm = make_shared<lcm::LCM>();
-  if (!lcm->good()) return 1;
+    std::shared_ptr<lcm::LCM> lcm = std::make_shared<lcm::LCM>();
+    if (!lcm->good()) return 1;
 
-  std::shared_ptr<AtlasSystem> atlas_sys = std::make_shared<AtlasSystem>();
+    std::shared_ptr<AtlasSystem> atlas_sys = std::make_shared<AtlasSystem>();
 
-  auto const& tree = atlas_sys->getRigidBodyTree();
+    auto const &tree = atlas_sys->getRigidBodyTree();
 
-  options.initial_step_size = 5e-5;
-  options.realtime_factor = 0.0;
+    options.initial_step_size = 5e-5;
+    options.realtime_factor = 0.0;
 
-  auto visualizer =
-      make_shared<BotVisualizer<RigidBodySystem::StateVector>>(lcm, tree);
+    auto visualizer =
+        std::make_shared<BotVisualizer<AtlasSystem::StateVector>>(lcm, tree);
 
-  auto sys_with_vis = cascade(atlas_sys, visualizer);
+    auto sys_with_vis = cascade(atlas_sys, visualizer);
 
-  runLCM(sys_with_vis, lcm, 0, final_time, atlas_sys->get_initial_state(),
-         options);
+    runLCM(sys_with_vis, lcm, 0, final_time, atlas_sys->get_initial_state(),
+           options);
+  }catch (const std::exception& e) {
+    std::cout << "Error: " << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
 }
