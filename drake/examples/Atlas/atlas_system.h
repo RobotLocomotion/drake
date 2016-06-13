9d02a9b922c46e50c32430964db036143ab6e689
amcastro-tri <alejandro.castro@tri.global>
2016-06-13 08:34:46 -0400

    Addresses reviews

--- drake/examples/Atlas/atlas_system.h
+++ drake/examples/Atlas/atlas_system.h
@@ -8,18 +8,28 @@ using Eigen::VectorXd;
 
 namespace drake {
 
+/** Implements the RigidBodySystem concept for the Atlas robot.
+
+This class is implemented to conveniently place all methods and functionality
+needed to simulate and design controllers for Atlas.
+
+ TODO(amcastro-tri): Future extensions will include the additions of actuators,
+ sensors, controllers and, methods implementing specific capabilities for Atlas.
+ **/
 class AtlasSystem : public Drake::RigidBodySystem {
  public:
+  /** Creates a default instance of the Atlas robot as described by the URDF
+  file in `drake/examples/Atlas/urdf/atlas_convex_hull.urdf`. **/
   AtlasSystem();
 
+  /** Returns an initial state vector describing the configuration of Atlas in a
+  standing position with the knees slightly bent and the arms down. **/
   const VectorXd& get_initial_state() const;
 
  private:
-  RigidBodyTree* tree_;
   VectorXd x0_;  // Atlas's initial configuration.
 
   // Sets the initial pose for Atlas.
-  // Magic numbers are initial conditions used in runAtlasWalking.m.
   void SetInitialConfiguration();
 
   void SetUpTerrain();
