420e6f0bdb12eebd1b6f1ae6f9413e00bd2a996e
Lucy.tri <lucy.gibson@tri.global>
2016-06-14 15:34:46 -0400

    Run cpplint

--- drake/solvers/trajectoryOptimization/direct_trajectory_optimization.h
+++ drake/solvers/trajectoryOptimization/direct_trajectory_optimization.h
@@ -17,18 +17,16 @@ class DRAKEOPTIMIZATION_EXPORT DirectTrajectoryOptimization {
   DirectTrajectoryOptimization(const int numInputs, const int numStates,
                                const size_t numTimeSamples,
                                const int trajectoryTimeLowerBound,
-                               const int trajectoryTimeUpperBound
-                               // options TODO
-                               );
-
+                               const int trajectoryTimeUpperBound);
+  // TODO(tri-lucy) add options params
  private:
   const int numInputs_;
   const int numStates_;
   const size_t numTimeSamples_;  // aka N
 
   Drake::OptimizationProblem optProblem_;
-  // TODO: I'm not sure we actually need these inds, because of how vars are
-  // made in C++ Drake.
+  // TODO(tri-lucy): I'm not sure we actually need these inds, because of how
+  // vars are made in C++ Drake.
   Eigen::VectorXd h_inds_;  // (N-1) x 1 indices for timesteps h so that
                             // z(h_inds(i)) = h(i)
   Eigen::MatrixXd x_inds_;  // n x N indices for state
