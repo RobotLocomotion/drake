f25bce18dc1e5c752d9f4952a58fd7e35efc0ef8
David German <david.german@tri.global>
2016-06-20 17:25:56 -0400

    Add eigen_types.

---
+++ drake/common/eigen_types.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Eigen/Dense>
+#include <Eigen/LU>
+#include <Eigen/StdVector>
+
+using Vector73d = Eigen::Matrix<double, Eigen::Dynamic, 1, 0, 73, 1>;
+using AutoDiffVector73d = Eigen::AutoDiffScalar<LargeVector>;
+using AutoDiffVectorXd = Eigen::AutoDiffScalar<Eigen::VectorXd>;
+
+template <typename Scalar>
+using TwistVector = Eigen::Matrix<Scalar, TWIST_SIZE, 1>;
+
+template <typename Scalar>
+using TwistMatrix = Eigen::Matrix<Scalar, TWIST_SIZE, Eigen::Dynamic>;
+
+template <typename Scalar>
+using Vector3 = Eigen::Matrix<Scalar, 3, 1>;
+
+template <typename Scalar>
+using VectorX = Eigen::Matrix<Scalar, Eigen::Dynamic, 1>;
+
+template <typename Scalar>
+using Matrix3X = Eigen::Matrix<Scalar, 3, Eigen::Dynamic>;
+
+template <typename Scalar>
+using Matrix4X = Eigen::Matrix<Scalar, 4, Eigen::Dynamic>;
+
+template <typename Scalar>
+using MatrixX = Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>;
