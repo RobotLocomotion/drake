99caca17cb36bff027d8bc38a8920327dc556177
jwnimmer <jeremy.nimmer@tri.global>
2016-06-20 21:43:28 -0400

    Fix up per code review suggestions.

--- drake/common/test/drake_assert_test.cc
+++ drake/common/test/drake_assert_test.cc
@@ -7,23 +7,23 @@
 
 namespace {
 
-GTEST_TEST(DrakeAssertTest, AbortTest) {
+GTEST_TEST(DrakeAssertDeathTest, AbortTest) {
   ::testing::FLAGS_gtest_death_test_style = "threadsafe";
   ASSERT_DEATH(
       { DRAKE_ABORT(); },
       "abort: failure at .*drake_assert_test.cc:.. in TestBody");
 }
 
-GTEST_TEST(DrakeAssertTest, AbortUnlessTest) {
+GTEST_TEST(DrakeAssertDeathTest, AbortUnlessTest) {
   ::testing::FLAGS_gtest_death_test_style = "threadsafe";
   ASSERT_DEATH(
       { DRAKE_ABORT_UNLESS(false); },
-      "abort: failure at .*drake_assert_test.cc:.. in TestBody:"
+      "abort: failure at .*drake_assert_test.cc:.. in TestBody..:"
       " assertion 'false' failed");
 }
 
 struct BoolConvertible { operator bool() { return true; } };
-GTEST_TEST(DrakeAssertTest, AssertSyntaxTest) {
+GTEST_TEST(DrakeAssertDeathTest, AssertSyntaxTest) {
   // These should compile.
   DRAKE_ASSERT((2 + 2) == 4);
   DRAKE_ASSERT(BoolConvertible());
@@ -31,12 +31,12 @@ GTEST_TEST(DrakeAssertTest, AssertSyntaxTest) {
 
 // Only run this test if assertions are armed.
 #ifdef DRAKE_ASSERT_IS_ARMED
-GTEST_TEST(DrakeAssertTest, AssertFalseTest) {
+GTEST_TEST(DrakeAssertDeathTest, AssertFalseTest) {
   ::testing::FLAGS_gtest_death_test_style = "threadsafe";
   ASSERT_DEATH(
       { DRAKE_ASSERT((2 + 2) == 5); },
-      "abort: failure at .*drake_assert_test.cc:.. in TestBody: "
-      "assertion '\\(2 \\+ 2\\) == 5' failed");
+      R"(abort: failure at .*drake_assert_test.cc:.. in TestBody\(\): )"
+      R"(assertion '\(2 \+ 2\) == 5' failed)");
 }
 #endif  //  DRAKE_ASSERT_IS_ARMED
 
