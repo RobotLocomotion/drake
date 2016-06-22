703393076cddd110cb95b2ac3a4c128c909e63df
Chien-Liang Fok <liang.fok@tri.global>
2016-06-22 17:11:13 -0400

    Removed debug statements.

--- drake/common/deprecated.h
+++ drake/common/deprecated.h
@@ -11,25 +11,20 @@
 /// A macro is needed since there is no standard way to do this across different
 /// compilers.
 #define DEPRECATED_14(MSG)
-#warning "doxygen version of DEPRECATED_14"
 
 #else  // DRAKE_DOXYGEN_CXX
 
 #ifndef SWIG
   #if __cplusplus >= 201402L
     /* C++14 */
-    #warning "c++14 version of DEPRECATED_14"
     #define DEPRECATED_14(MSG) [[deprecated(MSG)]]
   #elif _MSC_VER
     /* VC++ just says warning C4996 so add "DEPRECATED" to the message. */
-    #warning "VC version of DEPRECATED_14"
     #define DEPRECATED_14(MSG) __declspec(deprecated("DEPRECATED: " MSG))
   #else /* gcc or clang */
-    #warning "gcc/clang version of DEPRECATED_14"
     #define DEPRECATED_14(MSG) __attribute__((deprecated(MSG)))
   #endif
 #else  // SWIG
-  #warning "swig version of DEPRECATED_14"
   #define DEPRECATED_14(MSG)
 #endif  // SWIG
 
