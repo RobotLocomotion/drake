81b73b59cb70ee21069d7532ca84d7774261ec25
Siyuan Feng <siyuan.feng@tri.global>
2016-06-27 11:08:34 -0400

    rearranged a bit

--- drake/examples/sfengQP/test.cpp
+++ drake/examples/sfengQP/test.cpp
@@ -1,11 +1,54 @@
 #include "QPController.h"
 #include "QPEstimator.h"
 
+QPOutput testGravityCompensation(const sfRobotState &rs)
+{
+  // make controller
+  QPController con;
+  QPInput input(*rs.robot);
+  QPOutput output(*rs.robot);
+
+  // setup input
+  assert(input.loadParamFromFile(std::string(VALKYRIE_URDF_PATH) + std::string("/config/qpc_params")));
+
+  input.comdd_d.setZero();
+  input.pelvdd_d.setZero();
+  input.torsodd_d.setZero();
+  input.footdd_d[Side::LEFT].setZero();
+  input.footdd_d[Side::RIGHT].setZero();
+  input.qdd_d.setZero();
+
+  ////////////////////////////////////////////////////////////////////
+  // call QP
+  con.control(rs, input, output);
+
+  // print result
+  output.print();
+
+  return output;
+}
+
+void testQPEstimator(const std::string &urdf, const sfRobotState &rs, const VectorXd &q, const VectorXd &qd, const VectorXd &trq, const Vector6d &ft_l, const Vector6d &ft_r)
+{
+  QPEstimator est(urdf);
+  assert(est.loadParamFromFile(std::string(VALKYRIE_URDF_PATH) + std::string("/config/qpe_params")));
+
+  // make a dummy estimationg, the result should be really close to zero
+  est.init(0, rs.pos, rs.vel, rs.trq, rs.footFT_b[Side::LEFT], rs.footFT_b[Side::RIGHT]);
+  est.estimate(0, q, qd, trq, ft_l, ft_r);
+
+  std::cout << "residual:\n" << est.residual << std::endl;
+  std::cout << "\nvel:\n" << est.vel << std::endl;
+  std::cout << "\ntrq:\n" << est.trq << std::endl;
+  std::cout << "\nwrench:\n" << est.wrench << std::endl;
+ 
+}
+
 int main()
 {
   ////////////////////////////////////////////////////////////////////
   // load model
-  std::string urdf = std::string(VALKYRIE_URDF_PATH) + std::string("/valkyrie_sim_drake.urdf");
+  std::string urdf = std::string(VALKYRIE_URDF_PATH) + std::string("/config/valkyrie_sim_drake.urdf");
   sfRobotState rs(std::unique_ptr<RigidBodyTree>(new RigidBodyTree(urdf, DrakeJoint::ROLLPITCHYAW)));
 
   ////////////////////////////////////////////////////////////////////
@@ -38,40 +81,12 @@ int main()
    
   rs.update(0, q, qd, VectorXd::Zero(rs.robot->number_of_velocities()-6), Vector6d::Zero(), Vector6d::Zero());
   
-  // make controller
-  QPController con;
-  QPInput input(*rs.robot);
-  QPOutput output(*rs.robot);
+  // test QP controller
+  QPOutput output = testGravityCompensation(rs);
 
-  // setup input
-  assert(input.loadParamFromFile(std::string(VALKYRIE_URDF_PATH) + std::string("/config/qpc_params")));
-
-  input.comdd_d.setZero();
-  input.pelvdd_d.setZero();
-  input.torsodd_d.setZero();
-  input.footdd_d[Side::LEFT].setZero();
-  input.footdd_d[Side::RIGHT].setZero();
-  input.qdd_d.setZero();
-
-  // call QP
-  con.control(rs, input, output);
-
-  // print result
-  output.print();
-
-  // make estimator
-  QPEstimator est(urdf);
-  assert(est.loadParamFromFile(std::string(VALKYRIE_URDF_PATH) + std::string("/config/qpe_params")));
-
-  est.init(0, q, qd, output.trq, output.foot_wrench_in_sensor_frame[0], output.foot_wrench_in_sensor_frame[1]);
-  est.estimate(0, q, qd + output.qdd * est.dt, output.trq, output.foot_wrench_in_sensor_frame[0], output.foot_wrench_in_sensor_frame[1]);
-
-  std::cout << "residual:\n" << est.residual << std::endl;
-  std::cout << "\nvel:\n" << est.vel << std::endl;
-  std::cout << "\ntrq:\n" << est.trq << std::endl;
-  std::cout << "\nwrench:\n" << est.wrench << std::endl;
-  /*
-  */
+  ////////////////////////////////////////////////////////////////////
+  // make QP estimator
+  testQPEstimator(urdf, rs, q, qd + output.qdd * 2e-3, output.trq, output.foot_wrench_in_sensor_frame[Side::LEFT], output.foot_wrench_in_sensor_frame[Side::RIGHT]);
 
   return 1;
 }
