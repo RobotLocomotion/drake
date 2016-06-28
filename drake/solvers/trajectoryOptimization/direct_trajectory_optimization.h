42e4fd6d5e0612f9287dfbfdaaef745aaac8cfdd
Lucy Gibson <lucy.gibson@tri.global>
2016-06-28 13:43:21 -0400

    Create and add total time linear constraint.

--- drake/solvers/trajectoryOptimization/direct_trajectory_optimization.h
+++ drake/solvers/trajectoryOptimization/direct_trajectory_optimization.h
@@ -7,6 +7,9 @@
 #include "drake/drakeOptimization_export.h"
 #include "drake/solvers/Optimization.h"
 
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
 namespace drake {
 namespace solvers {
 
@@ -20,15 +23,11 @@ class DRAKEOPTIMIZATION_EXPORT DirectTrajectoryOptimization {
  private:
   const int numInputs_;
   const int numStates_;
-  const size_t N; // Number of time samples
+  const size_t N;  // Number of time samples
 
   OptimizationProblem optProblem_;
-  // TODO(tri-lucy): I'm not sure we actually need these inds, because of how
-  // vars are made in C++ Drake.
-  Eigen::VectorXd h_inds_;  // (N-1) x 1 indices for timesteps h so that
-                            // z(h_inds(i)) = h(i)
-  Eigen::MatrixXd x_inds_;  // n x N indices for state
-  Eigen::MatrixXd u_inds_;  // m x N indices for time
+  DecisionVariableView h_vars_;
 };
+
 }  // solvers
 }  // drake
