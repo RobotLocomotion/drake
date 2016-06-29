a1e5212ab0b7d102e6d68c0bb5f52061b8131043
Sam Creasey <sam.creasey@tri.global>
2016-06-29 14:48:05 -0400

    Create an IK test using the iiwa arm

--- drake/systems/plants/test/testIK.cpp
+++ drake/systems/plants/test/testIK.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cstdlib>
 #include <Eigen/Dense>
 
@@ -13,7 +14,7 @@ using namespace Eigen;
 using drake::util::CompareMatrices;
 using drake::util::MatrixCompareType;
 
-GTEST_TEST(testIK, simpleIK) {
+GTEST_TEST(testIK, atlasIK) {
   RigidBodyTree model("examples/Atlas/urdf/atlas_minimal_contact.urdf");
 
   Vector2d tspan;
@@ -52,4 +53,52 @@ GTEST_TEST(testIK, simpleIK) {
   memcpy(mxGetPrSafe(pqsol), q_sol.data(), sizeof(double)model.num_dof);
   matPutVariable(presultmat,"q_sol", pqsol);
   matClose(presultmat);*/
+}
+
+GTEST_TEST(testIK, iiwaIK) {
+  RigidBodyTree model("examples/kuka_iiwa_arm/urdf/iiwa14.urdf");
+
+  Vector2d tspan;
+  tspan << 0, 1;
+  VectorXd q0 = model.getZeroConfiguration();
+  //q0(1) = 0.8;
+
+  Vector3d pos_end;
+  pos_end << -0.4, 0, 1.1;
+  Vector3d pos_lb = pos_end - Vector3d::Constant(0.1);
+  Vector3d pos_ub = pos_end + Vector3d::Constant(0.1);
+  WorldPositionConstraint wpc(&model, model.FindBodyIndex("iiwa_link_7"),
+                              pos_end, pos_lb, pos_ub, tspan);
+
+  PostureConstraint pc(&model, tspan);
+  Vector3d joint_lb = Vector3d::Zero();
+  joint_lb(0) = 0.9;
+  joint_lb(1) = 0.9;
+  joint_lb(2) = 0.9;
+
+  Vector3d joint_ub = Vector3d::Zero();
+  joint_ub(0) = 1.0;
+  joint_ub(1) = 1.0;
+  joint_ub(2) = 1.0;
+
+  Eigen::VectorXi joint_idx(1);
+  joint_idx(0) = model.findJointId("iiwa_joint_2");
+  pc.setJointLimits(joint_idx, joint_lb, joint_ub);
+
+  std::vector<RigidBodyConstraint*> constraint_array;
+  constraint_array.push_back(&wpc);
+  constraint_array.push_back(&pc);
+  IKoptions ikoptions(&model);
+  VectorXd q_sol(model.number_of_positions());
+  int info;
+  vector<string> infeasible_constraint;
+  inverseKin(&model, q0, q0, constraint_array.size(), constraint_array.data(),
+             ikoptions, &q_sol, &info, &infeasible_constraint);
+  EXPECT_EQ(info, 1);
+
+  VectorXd expected(7);
+  expected << 0.096184, 1.4963, 0.0144598, 0.9,
+      0.0296464, -1.44644, 0.00662504;
+  EXPECT_TRUE(
+      CompareMatrices(q_sol, expected, 1e-5, MatrixCompareType::absolute));
 }
