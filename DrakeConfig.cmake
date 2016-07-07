b3f0364017e674368fc01a37320be1093e091371
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-07-07 14:57:40 -0400

    Refactor and "clean up" superbuild logic
    Refactor the top-level CMakeLists.txt (i.e. 'the superbuild') into
    several files, making use of CMake functions to organize the logic and
    eliminate some minor duplication. In particular, this converts the
    mostly-declarative (and somewhat superfluously repetitive) logic for
    setting up externals into more imperative/procedural logic that is
    hopefully better organized and easier to read.
    
    One particular change of note is that all special case "externals" have
    been removed in favor of new options `ALWAYS` and `LOCAL`, indicating,
    respectively, that an external is always enabled, and that an external
    is local to the repository (does not need download/update steps). This
    allows drake itself to be simply a `LOCAL`, `ALWAYS` subproject rather
    than having a completely separate block for setting up the subproject.
    
    The overall formatting and documentation (comments) of the superbuild
    logic has also been improved. Additionally, logic related to PODS has
    been removed.

---
+++ DrakeConfig.cmake
@@ -0,0 +1,24 @@
+# Check minimum compiler requirements
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9)
+  message(FATAL_ERROR "GCC version must be at least 4.9")
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 7)
+  message(FATAL_ERROR "Apple Clang version must be at least 7")
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.7)
+  message(FATAL_ERROR "Clang version must be at least 3.7")
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 19)
+  message(FATAL_ERROR "MSVC version must be at least 14")
+endif()
+
+# Set default install prefix
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+  set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/install" CACHE STRING
+    "Prefix for installation of sub-packages (note: required during build!)" FORCE)
+endif()
+message(STATUS CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX})
+
+# Require Java at the super-build level since libbot cannot configure without finding Java
+find_package(Java 1.6 REQUIRED)
+include(UseJava)
+
+# Choose your python (major) version
+option(WITH_PYTHON_3 "Force Drake to use python 3 instead of python 2" OFF)
