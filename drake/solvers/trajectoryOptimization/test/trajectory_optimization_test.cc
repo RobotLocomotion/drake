88a8732ba513be41764c23bdf7113a0255c13c33
Lucy Gibson <lucy.gibson@tri.global>
2016-07-11 15:25:44 -0400

    Added GetInitialVars() - WIP.

--- drake/solvers/trajectoryOptimization/test/trajectory_optimization_test.cc
+++ drake/solvers/trajectoryOptimization/test/trajectory_optimization_test.cc
@@ -11,8 +11,10 @@ namespace solvers {
 namespace {
 
 GTEST_TEST(TrajectoryOptimizationTest, DirectTrajectoryOptimizationTest) {
-  // TODO(lgibson) 3rd arg should be 21, using smaller # now for testing.
-  DirectTrajectoryOptimization directTraj(1, 2, 5, 2, 6);
+  // TODO(lgibson) kNumTimeSamples should be 21, using smaller # now for tests.
+  const size_t kNumTimeSamples(5);
+  DirectTrajectoryOptimization directTraj(1, 2, kNumTimeSamples, 2, 6);
+  directTraj.GetInitialVars(4);
 }
 
 }  // anonymous namespace
