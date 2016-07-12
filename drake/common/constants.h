1fef1d76a248be341724950bd015f4bda260c75d
David German <david.german@tri.global>
2016-07-12 10:59:07 -0400

    Deprecate some drakeGeometryUtil constants in favor of drake-namespaced constants.

---
+++ drake/common/constants.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace drake {
+
+constexpr int kQuaternionSize = 4;
+
+constexpr int kSpaceDimension = 3;
+
+/// https://en.wikipedia.org/wiki/Screw_theory#Twist
+constexpr int kTwistSize = 6;
+
+}  // namespace drake
