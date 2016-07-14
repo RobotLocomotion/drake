24138b642bd0c2ee70a367e0bf56144ae15bba24
Lucy Gibson <lucy.gibson@tri.global>
2016-07-14 15:30:11 -0400

    Finish GetInitialVars() and its test.

--- drake/solvers/trajectoryOptimization/test/trajectory_optimization_test.cc
+++ drake/solvers/trajectoryOptimization/test/trajectory_optimization_test.cc
@@ -19,21 +19,27 @@ typedef PiecewisePolynomial<double> PiecewisePolynomialType;
 default_random_engine generator;
 
 GTEST_TEST(TrajectoryOptimizationTest, DirectTrajectoryOptimizationTest) {
-  int num_coefficients = 5;
-  int num_segments = 3;
-  int rows = 3;
-  int cols = 5;
+  const size_t kNumInputs(1);
+  const size_t kNumStates(2);
   // TODO(lgibson) kNumTimeSamples should be 21, using smaller # now for tests.
-  const size_t kNumTimeSamples(5);
-  
-  vector<double> segment_times =
-      PiecewiseFunction::randomSegmentTimes(num_segments, generator);
-  const PiecewisePolynomialType piecewise =
-      PiecewisePolynomialType::random(rows, cols, num_coefficients, 
-                                      segment_times);
-  DirectTrajectoryOptimization directTraj(1, 2, kNumTimeSamples, 2, 6);
-  directTraj.GetInitialVars(4, piecewise, piecewise);
-  
+  const size_t kNumTimeSamples(5);  // aka N. Segment for each time sample.
+  DirectTrajectoryOptimization directTraj(kNumInputs, kNumStates,
+      kNumTimeSamples, 2, 6);
+
+  const size_t t_init_in(7);
+  VectorXd s_times{VectorXd::LinSpaced(kNumTimeSamples, 0, t_init_in)};
+  vector<double> segment_times(s_times.data(), s_times.data() +
+    s_times.rows() * s_times.cols());
+//  cout << "segmentTimes= " << endl;
+//  std::copy(segment_times.begin(), segment_times.end(),
+//    std::ostream_iterator<double>(std::cout, " "));
+
+  const PiecewisePolynomialType inputs_u =
+      PiecewisePolynomialType::random(kNumInputs, 1, 4, segment_times);
+  const PiecewisePolynomialType states_x =
+      PiecewisePolynomialType::random(kNumStates, 1, 6, segment_times);
+
+  directTraj.GetInitialVars(t_init_in, inputs_u, states_x);
 }
 
 }  // anonymous namespace
