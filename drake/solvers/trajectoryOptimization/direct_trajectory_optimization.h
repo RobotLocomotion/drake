412396c233a0d8e87247c46649b4f7308ff9b1d9
Lucy Gibson <lucy.gibson@tri.global>
2016-07-15 15:59:03 -0400

    Added SolveTraj(), call from test, add asserts to test. Added comments.

--- drake/solvers/trajectoryOptimization/direct_trajectory_optimization.h
+++ drake/solvers/trajectoryOptimization/direct_trajectory_optimization.h
@@ -14,17 +14,45 @@ using Eigen::VectorXd;
 namespace drake {
 namespace solvers {
 
+/**
+ * DirectTrajectoryOptimization An abstract class for direct method approaches
+ * to trajectory optimization.
+ *
+ * This class assumes that there are a fixed number (N) time steps/samples, and
+ * that the trajectory is discreteized into timesteps h (N-1 of these), state x
+ * (N of these), and control input u (N of these).
+ *
+ * To maintain nominal sparsity in the optimization programs, this
+ * implementation assumes that all constraints and costs are
+ * time-invariant.
+ */
 class DRAKEOPTIMIZATION_EXPORT DirectTrajectoryOptimization {
  public:
   DirectTrajectoryOptimization(const int num_inputs, const int num_states,
                                const size_t num_time_samples,
                                const int trajectory_time_lower_bound,
                                const int trajectory_time_upper_bound);
+  // TODO(lucy-tri) add param: time steps constant or independent.
+
+  /**
+   *  Evaluate the initial trajectories at the sampled times and construct the
+   *  nominal initial vectors.
+   */
   void GetInitialVars(int t_init_in,
                       const PiecewisePolynomial<double>& traj_init_u,
                       const PiecewisePolynomial<double>& traj_init_x);
+
   void AddStateConstraint(const Constraint& constraint, const int time_index);
-  // TODO(lucy-tri) add options params
+
+  /** 
+   * Solve the nonlinear program and return the resulting trajectory.
+   * @p t_init Initial timespan for solution. 
+   * @p traj_init_u Initial guess for trajectory for control input.
+   * @p traj_init_x Initial guess for trajectory for state input.
+   */
+  SolutionResult SolveTraj(int t_init_in,
+                           const PiecewisePolynomial<double>& traj_init_u,
+                           const PiecewisePolynomial<double>& traj_init_x);
  private:
   const int num_inputs_;
   const int num_states_;
