b28f70c126bab193ababae7a2cc01987118b9d30
Chien-Liang <liangfok@utexas.edu>
2016-07-15 06:34:39 -0400

    Ran clang-format. Addressed reviewer comments.

--- drake/systems/lcm/lcm_subscriber_system.cc
+++ drake/systems/lcm/lcm_subscriber_system.cc
@@ -8,22 +8,22 @@ namespace drake {
 namespace systems {
 namespace lcm {
 
-LcmSubscriberSystem::LcmSubscriberSystem(const std::string& channel,
-                      const LcmBasicVectorTranslator& translator,
-                      LcmReceiveThread* lcm_receive_thread)
+using std::make_unique;
+
+LcmSubscriberSystem::LcmSubscriberSystem(
+    const std::string& channel, const LcmBasicVectorTranslator& translator,
+    LcmReceiveThread* lcm_receive_thread)
     : channel_(channel),
       translator_(translator),
       lcm_receive_thread_(lcm_receive_thread),
       basic_vector_(translator.get_basic_vector_size()) {
   // Initializes the communication layer.
-  ::lcm::Subscription* sub =
-      lcm_receive_thread_->get_lcm()->subscribe(channel_,
-        &LcmSubscriberSystem::HandleMessage, this);
+  ::lcm::Subscription* sub = lcm_receive_thread_->get_lcm()->subscribe(
+      channel_, &LcmSubscriberSystem::HandleMessage, this);
   sub->setQueueCapacity(1);
 }
 
-LcmSubscriberSystem::~LcmSubscriberSystem() {
-}
+LcmSubscriberSystem::~LcmSubscriberSystem() {}
 
 std::string LcmSubscriberSystem::get_name() const {
   return "LcmSubscriberSystem::" + channel_;
@@ -32,19 +32,10 @@ std::string LcmSubscriberSystem::get_name() const {
 std::unique_ptr<Context<double>> LcmSubscriberSystem::CreateDefaultContext()
     const {
   // Creates a new context for this system and sets the number of input ports
-  // to be zero.
+  // to be zero. It leaves the context's state uninitialized since this system
+  // does not use it.
   std::unique_ptr<Context<double>> context(new Context<double>());
   context->SetNumInputPorts(0);
-
-  // Creates a BasicStateVector of size zero for this system.
-  std::unique_ptr<BasicStateVector<double>> state(
-      new BasicStateVector<double>(0));
-
-  // Stores the BasicStateVector in a ContinuousState. Then store the
-  // ContinuousState inside this system's context.
-  context->get_mutable_state()->continuous_state.reset(
-      new ContinuousState<double>(std::move(state), 0 /* size of q */,
-                                  0 /* size of v */, 0 /* size of z */));
 
   // Returns this system's context.
   return context;
@@ -53,12 +44,12 @@ std::unique_ptr<Context<double>> LcmSubscriberSystem::CreateDefaultContext()
 std::unique_ptr<SystemOutput<double>> LcmSubscriberSystem::AllocateOutput()
     const {
   // Instantiates a BasicVector object and stores it in a managed pointer.
-  std::unique_ptr<BasicVector<double>> data(
-      new BasicVector<double>(translator_.get_basic_vector_size()));
+  std::unique_ptr<BasicVector<double>> data =
+      make_unique<BasicVector<double>>(translator_.get_basic_vector_size());
 
   // Instantiates an OutputPort with the above BasicVector as the data type.
-  std::unique_ptr<OutputPort<double>> port(
-      new OutputPort<double>(std::move(data)));
+  std::unique_ptr<OutputPort<double>> port =
+      make_unique<OutputPort<double>>(std::move(data));
 
   // Stores the above-defined OutputPort in this system output.
   std::unique_ptr<SystemOutput<double>> output(new SystemOutput<double>);
@@ -69,7 +60,7 @@ std::unique_ptr<SystemOutput<double>> LcmSubscriberSystem::AllocateOutput()
 }
 
 void LcmSubscriberSystem::EvalOutput(const Context<double>& context,
-                SystemOutput<double>* output) const {
+                                     SystemOutput<double>* output) const {
   BasicVector<double>& output_vector = dynamic_cast<BasicVector<double>&>(
       *output->ports[0]->GetMutableVectorData());
 
@@ -79,7 +70,7 @@ void LcmSubscriberSystem::EvalOutput(const Context<double>& context,
 }
 
 void LcmSubscriberSystem::HandleMessage(const ::lcm::ReceiveBuffer* rbuf,
-                   const std::string& channel) {
+                                        const std::string& channel) {
   if (channel == channel_) {
     data_mutex_.lock();
     translator_.TranslateLcmToBasicVector(rbuf, &basic_vector_);
