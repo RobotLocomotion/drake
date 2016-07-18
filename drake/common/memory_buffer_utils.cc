509906fc45849fd02d8a7c66643b559a997e792c
Chien-Liang Fok <liang.fok@tri.global>
2016-07-18 15:34:12 -0400

    Replaced magic number with a constant variable.

--- drake/common/memory_buffer_utils.cc
+++ drake/common/memory_buffer_utils.cc
@@ -11,6 +11,10 @@ using std::stringstream;
 
 namespace drake {
 
+// Defines the number of bytes to include on a line when creating the string
+// representation of a memory buffer.
+const int kNumValuesPerLine = 10;
+
 std::string MemoryBufferToString(const uint8_t* const buffer,
     int buffer_length) {
   DRAKE_ABORT_UNLESS(buffer);
@@ -18,7 +22,7 @@ std::string MemoryBufferToString(const uint8_t* const buffer,
   for (int ii = 0; ii < buffer_length; ++ii) {
     string_stream << "0x" << std::uppercase << std::setfill('0') << std::setw(2)
                   << std::hex << static_cast<unsigned>(buffer[ii]);
-    if (ii > 0 && (ii + 1) % 10 == 0)
+    if (ii > 0 && (ii + 1) % kNumValuesPerLine == 0)
       string_stream << "\n";
     else
       string_stream << " ";
