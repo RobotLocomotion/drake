539faa7bcb9e682b091f7ea388bca9c2133e2be4
Chien-Liang Fok <liangfok@gmail.com>
2016-07-23 06:17:59 -0400

    Pulled out RigidBodyActuator into its own files. (#2889)
    * Pulled out RigidBodyActuator into its own files.
    
    * Ran clang-format.
    
    * Modified to use forward declaration.
    
    * Added missing #include.

---
+++ drake/systems/plants/rigid_body_actuator.cc
@@ -0,0 +1,12 @@
+#include "drake/systems/plants/rigid_body_actuator.h"
+
+RigidBodyActuator::RigidBodyActuator(const std::string& name_in,
+                                     const RigidBody* body_in,
+                                     double reduction_in,
+                                     double effort_limit_min_in,
+                                     double effort_limit_max_in)
+    : name(name_in),
+      body(body_in),
+      reduction(reduction_in),
+      effort_limit_min(effort_limit_min_in),
+      effort_limit_max(effort_limit_max_in) {}
