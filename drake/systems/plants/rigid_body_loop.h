fe1f61f5b7011b64262f1855b31276b3ca689ac3
Chien-Liang Fok <liang.fok@tri.global>
2016-07-25 15:04:59 -0400

    Refined documentation.

--- drake/systems/plants/rigid_body_loop.h
+++ drake/systems/plants/rigid_body_loop.h
@@ -10,16 +10,17 @@
  * of rigid bodies and their regular joints. The loop joint is specified by two
  * `RigidBodyFrame` objects that must be attached to two different `RigidBody`
  * objects. The coordinate frames defined by the two `RigidBodyFrame` objects
- * are constrained to be in the same in terms of all three position axes and
- * two of the three orientation axes. The unconstrained orientation axis forms
- * the loop joint.
+ * are constrained to have the same origin. The orientations of the two frames
+ * are partially constrained based on the axis of rotation (i.e., the two frames
+ * are only allowed to rotate relative to each other along the axis of
+ * rotation).
  */
 class DRAKERBM_EXPORT RigidBodyLoop {
  public:
   /**
-   * Constructs a kinematic loop by partially constraining the origins and
-   * orientations of two frames. The two frames are defined relative to two
-   * rigid bodies that should be connected via a loop joint.
+   * Constructs a kinematic loop by fully constraining the origins and partially
+   * constraining the orientations of two frames. The two frames are defined
+   * relative to two rigid bodies that should be connected via a loop joint.
    *
    * @param[in] frameA_in A frame defined relative to the loop joint's "parent"
    * rigid body.
@@ -27,9 +28,8 @@ class DRAKERBM_EXPORT RigidBodyLoop {
    * @param[in] frameB_in A frame defined relative to the loop joint's "child"
    * rigid body.
    *
-   * @param[in] axis_in The loop joint's axis of rotation. Note that it does not
-   * matter which frame the axis is specified in since the axes of the two
-   * frames are constrained to be the same.
+   * @param[in] axis_in The loop joint's axis of rotation expressed in the
+   * coordinate frame of @p frameA_in.
    */
   RigidBodyLoop(std::shared_ptr<RigidBodyFrame> frameA_in,
                 std::shared_ptr<RigidBodyFrame> frameB_in,
