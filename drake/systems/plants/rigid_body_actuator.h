ce4a8cdc7ce8673d717e45c68471b208493d19dd
Chien-Liang Fok <liang.fok@tri.global>
2016-07-25 14:50:35 -0400

    Removed "_in" from publicly viewable parameter names.

--- drake/systems/plants/rigid_body_actuator.h
+++ drake/systems/plants/rigid_body_actuator.h
@@ -12,8 +12,8 @@ class DRAKERBM_EXPORT RigidBodyActuator {
   RigidBodyActuator(
       const std::string& name, const RigidBody* body,
       double reduction = 1.0,
-      double effort_limit_min_in = -std::numeric_limits<double>::infinity(),
-      double effort_limit_max_in = std::numeric_limits<double>::infinity());
+      double effort_limit_min = -std::numeric_limits<double>::infinity(),
+      double effort_limit_max = std::numeric_limits<double>::infinity());
 
   const std::string name_;
   const RigidBody* const body_;
