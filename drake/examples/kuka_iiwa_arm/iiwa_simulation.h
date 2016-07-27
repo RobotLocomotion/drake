0b0ec9b217eba94725b7c0c985a2b4038fbc0da2
Chien-Liang Fok <liang.fok@tri.global>
2016-07-27 14:55:13 -0400

    Fixed bug in include.

--- drake/examples/kuka_iiwa_arm/iiwa_simulation.h
+++ drake/examples/kuka_iiwa_arm/iiwa_simulation.h
@@ -4,7 +4,7 @@
 
 #include <Eigen/Geometry>
 
-#include "drake/drakeKukaIIWAArm_export.h"
+#include "drake/drakeKukaIiwaArm_export.h"
 #include "drake/systems/Simulation.h"
 #include "drake/systems/plants/RigidBodySystem.h"
 
