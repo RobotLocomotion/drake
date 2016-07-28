77d014e827ebd5a90e7661e26ae14b722688882c
amcastro-tri <alejandro.castro@tri.global>
2016-07-28 10:17:08 -0400

    Removes dead code from DrakeCollision.cpp

--- drake/systems/plants/collision/DrakeCollision.cpp
+++ drake/systems/plants/collision/DrakeCollision.cpp
@@ -16,34 +16,13 @@ const bitmask ALL_MASK(bitmask(0).set());
 const bitmask NONE_MASK(0);
 const bitmask DEFAULT_GROUP(1);
 
-enum DRAKECOLLISION_EXPORT ModelType { NONE, AUTO, BULLET };
-
-unique_ptr<Model> newModel(ModelType model_type) {
-  switch (model_type) {
-    case NONE:
-      return nullptr;
-      break;
-    case BULLET:
-#ifdef BULLET_COLLISION
-      return unique_ptr<Model>(new BulletModel());
-#else
-      cerr << "Recompile with Bullet enabled (-DBULLET_COLLISION) to use "
-              "Bullet collision models." << endl;
-#endif
-      break;
-    default:
-      cerr << model_type << " is not a recognized collision model type."
-           << endl;
-  }
-  return unique_ptr<Model>();
-}
-
 unique_ptr<Model> newModel() {
 #ifdef BULLET_COLLISION
-  return newModel(BULLET);
+  return unique_ptr<Model>(new BulletModel());
 #else
   DRAKE_ABORT_UNLESS(!"DrakeCollision must be compiled with Bullet.");
-  // This return statement is actually never reached.
+  // This return statement is actually never reached when 
+  // BULLET_COLLISION is defined.
   // It is placed here to avoid a compiler error on not having a return
   // statement.
   return unique_ptr<Model>();
