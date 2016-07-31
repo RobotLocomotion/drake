67aa40fe0532085cca46abc786f17178a2967acf
Chien-Liang Fok <liang.fok@tri.global>
2016-07-31 12:09:58 -0400

    Renamed "instance_name" to be "model_instance_name" for clarity.

--- drake/systems/plants/model_element_id.cc
+++ drake/systems/plants/model_element_id.cc
@@ -4,17 +4,17 @@ namespace drake {
 namespace systems {
 namespace plants {
 
-ModelElementId::ModelElementId(const std::string& instance_name,
+ModelElementId::ModelElementId(const std::string& model_instance_name,
                                const std::string& model_name,
                                const std::string& element_name,
                                int model_instance_id)
-    : instance_name_(instance_name),
+    : model_instance_name_(model_instance_name),
       model_name_(model_name),
       element_name_(element_name),
       model_instance_id_(model_instance_id) {}
 
-const std::string& ModelElementId::get_instance_name() const {
-  return instance_name_;
+const std::string& ModelElementId::get_model_instance_name() const {
+  return model_instance_name_;
 }
 
 const std::string& ModelElementId::get_model_name() const {
@@ -29,7 +29,7 @@ int ModelElementId::get_model_instance_id() const { return model_instance_id_; }
 
 // TODO(liang.fok) Remove this method. See:
 // https://github.com/RobotLocomotion/drake/issues/2990
-void ModelElementId::set_instance_name(const std::string& model_name) {
+void ModelElementId::set_model_instance_name(const std::string& model_name) {
   model_name_ = model_name;
 }
 
@@ -53,8 +53,8 @@ void ModelElementId::set_model_instance_id(int model_instance_id) {
 
 bool operator==(const ModelElementId& left_element,
                 const ModelElementId& right_element) {
-  return left_element.get_instance_name() ==
-             right_element.get_instance_name() &&
+  return left_element.get_model_instance_name() ==
+             right_element.get_model_instance_name() &&
          left_element.get_model_name() == right_element.get_model_name() &&
          left_element.get_element_name() == right_element.get_element_name() &&
          left_element.get_model_instance_id() ==
