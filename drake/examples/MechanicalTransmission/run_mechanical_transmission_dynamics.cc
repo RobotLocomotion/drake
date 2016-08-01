6031ca2c3b9c126510c6488d36e29a6ddcf75ab0
hongkai-dai <daihongkai@gmail.com>
2016-08-01 11:55:52 -0700

    work on the test program for mechanical transmission. Will need to store the simulation results

--- drake/examples/MechanicalTransmission/run_mechanical_transmission_dynamics.cc
+++ drake/examples/MechanicalTransmission/run_mechanical_transmission_dynamics.cc
@@ -24,10 +24,48 @@ int DoMain(int argc, char* argv[]) {
 	rigid_body_system->penetration_damping = 0;
 
 	auto& tree = rigid_body_system->getRigidBodyTree();
-	int joint1 = tree->findJointId(std::string("joint1"));
-	int joint2 = tree->findJointId(std::string("joint2"));
+	int nq = tree->number_of_positions();
+	int joint1 = tree->findJoint("joint1")->get_position_start_index();
+	int joint2 = tree->findJoint("joint2")->get_position_start_index();
 	std::cout<<"joint1:"<<joint1<<" joint2:"<<joint2<<std::endl;
-	//tree->addLinearEqualityPositonConstraint
+	Eigen::Matrix<double,1,Eigen::Dynamic> Aeq_mechanical_transmission(1,nq);
+	Aeq_mechanical_transmission.setZero();
+	Aeq_mechanical_transmission(0,joint1) = 1.0;
+	Aeq_mechanical_transmission(0,joint2) = -0.5;
+	tree->addLinearEqualityPositionConstraint(Aeq_mechanical_transmission,
+				Eigen::Matrix<double,1,1>::Zero());
+
+	drake::SimulationOptions options;
+	options.realtime_factor = 0; // As fast as possible.
+	options.initial_step_size = 0.02;
+
+	// Prevents exception from being thrown when simulation runs slower than real
+  // time, which it most likely will given the small step size.
+  options.warn_real_time_violation = true;
+
+  // Instantates a variable that specifies the duration of the simulation.
+  // The default value is 5 seconds.
+  double duration = 5.0;
+
+  // Searches through the command line looking for a "--duration" flag followed
+  // by a floating point number that specifies a custom duration.
+  for (int ii = 1; ii < argc; ++ii) {
+    if (std::string(argv[ii]) == "--duration") {
+      if (++ii == argc) {
+        throw std::runtime_error(
+            "ERROR: Command line option \"--duration\" is not followed by a "
+            "value!");
+      }
+      duration = atof(argv[ii]);
+    }
+  }
+
+  // Starts the simulation.
+  const double kStartTime = 0.0;
+  VectorXd x0 = VectorXd::Random(rigid_body_system->getNumStates());
+  double stop_time = drake::simulate(*rigid_body_system, kStartTime,
+  																	 duration,x0,options);
+  std::cout<<"stop time:"<<stop_time<<std::endl;
 	return 0;
 }
 }
