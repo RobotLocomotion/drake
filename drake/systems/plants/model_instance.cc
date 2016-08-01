3d49ddc5387f43439f9bcd0afd949aa3c6914ff3
Chien-Liang Fok <liang.fok@tri.global>
2016-08-01 01:30:42 -0400

    Modified ModelInstance to store pointers and not own any of them so we can forward declare all of the modeling elements. This is necessary to avoid circular dependencies with RigidBodySystem.

--- drake/systems/plants/model_instance.cc
+++ drake/systems/plants/model_instance.cc
@@ -18,28 +18,28 @@ void ModelInstance::set_model_name(const std::string& model_name) {
   model_name_ = model_name;
 }
 
-void ModelInstance::add_joint(unique_ptr<DrakeJoint> joint) {
-  joints_.push_back(move(joint));
+void ModelInstance::add_joint(const DrakeJoint* joint) {
+  joints_.push_back(joint);
 }
 
-void ModelInstance::add_body(unique_ptr<RigidBody> body) {
-  bodies_.push_back(move(body));
+void ModelInstance::add_body(const RigidBody* body) {
+  bodies_.push_back(body);
 }
 
-void ModelInstance::add_frame(unique_ptr<RigidBodyFrame> frame) {
-  frames_.push_back(move(frame));
+void ModelInstance::add_frame(const RigidBodyFrame* frame) {
+  frames_.push_back(frame);
 }
 
-void ModelInstance::add_loop(unique_ptr<RigidBodyLoop> loop) {
-  loops_.push_back(move(loop));
+void ModelInstance::add_loop(const RigidBodyLoop* loop) {
+  loops_.push_back(loop);
 }
 
-void ModelInstance::add_actuator(unique_ptr<RigidBodyActuator> actuator) {
-  actuators_.push_back(move(actuator));
+void ModelInstance::add_actuator(const RigidBodyActuator* actuator) {
+  actuators_.push_back(actuator);
 }
 
-void ModelInstance::add_sensor(unique_ptr<RigidBodySensor> sensor) {
-  sensors_.push_back(move(sensor));
+void ModelInstance::add_sensor(const RigidBodySensor* sensor) {
+  sensors_.push_back(sensor);
 }
 
 void ModelInstance::add_model_instance(ModelInstance* model_instance) {
@@ -54,27 +54,27 @@ const std::string& ModelInstance::get_model_name() const {
   return model_name_;
 }
 
-const std::vector<unique_ptr<DrakeJoint>>& ModelInstance::get_joints() const {
+const std::vector<const DrakeJoint*>& ModelInstance::get_joints() const {
   return joints_;
 }
 
-const std::vector<unique_ptr<RigidBody>>& ModelInstance::get_bodies() const {
+const std::vector<const RigidBody*>& ModelInstance::get_bodies() const {
   return bodies_;
 }
 
-const std::vector<unique_ptr<RigidBodyFrame>>& ModelInstance::get_frames() const {
+const std::vector<const RigidBodyFrame*>& ModelInstance::get_frames() const {
   return frames_;
 }
 
-const std::vector<unique_ptr<RigidBodyLoop>>& ModelInstance::get_loops() const {
+const std::vector<const RigidBodyLoop*>& ModelInstance::get_loops() const {
   return loops_;
 }
 
-const std::vector<unique_ptr<RigidBodyActuator>>& ModelInstance::get_actuators() const {
+const std::vector<const RigidBodyActuator*>& ModelInstance::get_actuators() const {
   return actuators_;
 }
 
-const std::vector<unique_ptr<RigidBodySensor>>& ModelInstance::get_sensors() const {
+const std::vector<const RigidBodySensor*>& ModelInstance::get_sensors() const {
   return sensors_;
 }
 
