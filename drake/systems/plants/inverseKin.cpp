75b0453c31cfcdaaeaaacb928789bf7a8c890b36
Sam Creasey <sam.creasey@tri.global>
2016-08-02 13:58:38 -0400

    Fix a couple of line length issues.

--- drake/systems/plants/inverseKin.cpp
+++ drake/systems/plants/inverseKin.cpp
@@ -9,14 +9,15 @@ using Eigen::VectorXd;
 using drake::systems::plants::inverseKinBackend;
 
 template <typename DerivedA, typename DerivedB, typename DerivedC>
-DRAKEIK_EXPORT void inverseKin(RigidBodyTree *model,
-                               const MatrixBase<DerivedA>& q_seed,
-                               const MatrixBase<DerivedB>& q_nom,
-                               const int num_constraints,
-                               RigidBodyConstraint **const constraint_array,
-                               const IKoptions& ikoptions,
-                               MatrixBase<DerivedC>* q_sol, int* info,
-                               std::vector<std::string>* infeasible_constraint) {
+DRAKEIK_EXPORT void inverseKin(
+    RigidBodyTree *model,
+    const MatrixBase<DerivedA>& q_seed,
+    const MatrixBase<DerivedB>& q_nom,
+    const int num_constraints,
+    RigidBodyConstraint **const constraint_array,
+    const IKoptions& ikoptions,
+    MatrixBase<DerivedC>* q_sol, int* info,
+    std::vector<std::string>* infeasible_constraint) {
   double *t = nullptr;
   inverseKinBackend(model, 1, t, q_seed, q_nom, num_constraints,
                     constraint_array, ikoptions, q_sol, info,
