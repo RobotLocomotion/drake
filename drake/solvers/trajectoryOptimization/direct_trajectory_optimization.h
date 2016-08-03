f83bedc5a81c0b60e08e6be31b1e767a2bb34a1e
Sam Creasey <sam.creasey@tri.global>
2016-08-03 17:05:46 -0400

    Add support for adding state and input constraints.
    Also allows for extracting the value of the result by knots or trajectories.

--- drake/solvers/trajectoryOptimization/direct_trajectory_optimization.h
+++ drake/solvers/trajectoryOptimization/direct_trajectory_optimization.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <vector>
+
 #include <Eigen/Core>
 
+#include "drake/common/drake_assert.h"
 #include "drake/drakeTrajectoryOptimization_export.h"
 #include "drake/solvers/optimization.h"
 #include "drake/systems/trajectories/PiecewisePolynomial.h"
@@ -26,8 +29,10 @@ namespace solvers {
 class DRAKETRAJECTORYOPTIMIZATION_EXPORT DirectTrajectoryOptimization {
  public:
   /**
-   * @p trajectory_time_lower_bound Bound on total time for trajectory.
-   * @p trajectory_time_upper_bound Bound on total time for trajectory.
+   * @param trajectory_time_lower_bound Bound on total time for
+   *        trajectory.
+   * @param trajectory_time_upper_bound Bound on total time for
+   *        trajectory.
    */
   DirectTrajectoryOptimization(int num_inputs, int num_states,
                                int num_time_samples,
@@ -36,17 +41,51 @@ class DRAKETRAJECTORYOPTIMIZATION_EXPORT DirectTrajectoryOptimization {
   // TODO(Lucy-tri) add param: time steps constant or independent.
 
   /**
+   * Add a constraint on the input at the specified time indices.
+   *
+   * @param constraint The constraint to be applied.
+   * @param time_indices The (0 offset) time indices to apply the
+   *        constraint.
+   */
+  template <typename ConstraintT>
+  void AddInputConstraint(std::shared_ptr<ConstraintT> constraint,
+                          const std::vector<int>& time_indices) {
+    for (const int i : time_indices) {
+      DRAKE_ASSERT(i < N_);
+      opt_problem_.AddConstraint(
+          constraint, {u_vars_.segment(i * num_inputs_, num_inputs_)});
+    }
+  }
+
+  /**
+   * Add a constraint on the state at the specified time indices.
+   *
+   * @param constraint The constraint to be applied.
+   * @param time_indices The (0 offset) time indices to apply the
+   *        constraint.
+   */
+  template <typename ConstraintT>
+  void AddStateConstraint(std::shared_ptr<ConstraintT> constraint,
+                          const std::vector<int>& time_indices) {
+    for (const int i : time_indices) {
+      DRAKE_ASSERT(i < N_);
+      opt_problem_.AddConstraint(
+          constraint, {x_vars_.segment(i * num_states_, num_states_)});
+    }
+  }
+
+  /**
    * Solve the nonlinear program and return the resulting trajectory.
    *
-   * @p t_init The final time of the solution.
+   * @param t_init The final time of the solution.
    *
-   * @p traj_init_u Initial guess for trajectory for control input. The number
-   * of rows for each segment in @p traj_init_u must be equal to num_inputs
-   * (the first param of the constructor).
+   * @param traj_init_u Initial guess for trajectory for control
+   * input. The number of rows for each segment in @p traj_init_u must
+   * be equal to num_inputs (the first param of the constructor).
    *
-   * @p traj_init_x Initial guess for trajectory for state input. The number
-   * of rows for each segment in @p traj_init_x must be equal to num_states
-   * (the second param of the constructor).
+   * @param traj_init_x Initial guess for trajectory for state
+   * input. The number of rows for each segment in @p traj_init_x must
+   * be equal to num_states (the second param of the constructor).
    */
   SolutionResult SolveTraj(double t_init,
                            const PiecewisePolynomial<double>& traj_init_u,
@@ -59,31 +98,56 @@ class DRAKETRAJECTORYOPTIMIZATION_EXPORT DirectTrajectoryOptimization {
   DirectTrajectoryOptimization& operator=(const DirectTrajectoryOptimization&) =
       delete;
 
+  /**
+   * Extract the result of the trajectory solution as a set of
+   * discrete samples.  Output matrices contain one set of input/state
+   * per column, and the number of columns is equal to the number of
+   * time samples.  @p times will be populated with the times
+   * corresponding to each column.
+   *
+   * @param[out] inputs
+   */
+  void GetResultSamples(Eigen::MatrixXd* inputs, Eigen::MatrixXd* states,
+                        std::vector<double>* times) const;
+
+  /**
+   * Get the input trajectory as a PiecewisePolynomial
+   */
+  PiecewisePolynomial<double> ReconstructInputTrajectory() const;
+
+  /**
+   * Get the state trajectory as a PiecewisePolynomial
+   */
+  PiecewisePolynomial<double> ReconstructStateTrajectory() const;
+
  private:
   /**
    * Evaluate the initial trajectories at the sampled times and construct the
    * nominal initial vectors.
    *
-   * @p t_init The final time of the solution.
+   * @param t_init The final time of the solution.
    *
-   * @p traj_init_u Initial guess for trajectory for control input. The number
-   * of rows for each segment in @p traj_init_u must be equal to num_inputs
-   * (the first param of the constructor).
+   * @param traj_init_u Initial guess for trajectory for control
+   * input. The number of rows for each segment in @p traj_init_u must
+   * be equal to num_inputs (the first param of the constructor).
    *
-   * @p traj_init_x Initial guess for trajectory for state input. The number
-   * of rows for each segment in @p traj_init_x must be equal to num_states
-   * (the second param of the constructor).
+   * @param traj_init_x Initial guess for trajectory for state
+   * input. The number of rows for each segment in @p traj_init_x must
+   * be equal to num_states (the second param of the constructor).
    */
   void GetInitialVars(double t_init_in,
                       const PiecewisePolynomial<double>& traj_init_u,
                       const PiecewisePolynomial<double>& traj_init_x);
+
+  std::vector<double> GetTimeVector() const;
 
   const int num_inputs_;
   const int num_states_;
   const int N_;  // Number of time samples
 
   OptimizationProblem opt_problem_;
-  DecisionVariableView h_vars_;
+  DecisionVariableView h_vars_;  // Time deltas between each
+                                 // input/state sample.
   DecisionVariableView u_vars_;
   DecisionVariableView x_vars_;
 };
--- drake/solvers/trajectoryOptimization/test/trajectory_optimization_test.cc
+++ drake/solvers/trajectoryOptimization/test/trajectory_optimization_test.cc
@@ -1,13 +1,18 @@
-#include <random>
+
 #include <vector>
 
 #include "gtest/gtest.h"
 #include "drake/solvers/trajectoryOptimization/direct_trajectory_optimization.h"
 #include "drake/systems/trajectories/PiecewisePolynomial.h"
+#include "drake/systems/vector.h"
+
+#include "drake/util/eigen_matrix_compare.h"
 
 using std::vector;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+
+using drake::util::MatrixCompareType;
 
 namespace drake {
 namespace solvers {
@@ -37,10 +42,60 @@ GTEST_TEST(TrajectoryOptimizationTest, DirectTrajectoryOptimizationTest) {
   const PiecewisePolynomialType inputs_u(u_vec, times);
   const PiecewisePolynomialType states_x(y_vec, times);
 
+  const Vector1d constrained_input(30);
+  auto input_constraint = std::make_shared<LinearEqualityConstraint>(
+      Vector1d(1), constrained_input);
+  direct_traj.AddInputConstraint(input_constraint,
+                                 {1, kNumTimeSamples - 2});
+
+  const Eigen::Vector2d constrained_state(11, 22);
+  auto state_constraint = std::make_shared<LinearEqualityConstraint>(
+      Eigen::Matrix2d::Identity(), constrained_state);
+  direct_traj.AddStateConstraint(state_constraint,
+                                 {0, kNumTimeSamples - 1});
+
   SolutionResult result = SolutionResult::kUnknownError;
   result =
       direct_traj.SolveTraj(t_init_in, PiecewisePolynomialType(), states_x);
   EXPECT_EQ(result, SolutionResult::kSolutionFound) << "Result is an Error";
+
+  Eigen::MatrixXd inputs;
+  Eigen::MatrixXd states;
+  std::vector<double> times_out;
+
+  direct_traj.GetResultSamples(&inputs, &states, &times_out);
+  PiecewisePolynomial<double> input_traj =
+      direct_traj.ReconstructInputTrajectory();
+
+  EXPECT_TRUE(
+      CompareMatrices(constrained_input, inputs.col(1),
+                      1e-10, MatrixCompareType::absolute));
+  EXPECT_TRUE(
+      CompareMatrices(constrained_input, input_traj.value(times_out[1]),
+                      1e-10, MatrixCompareType::absolute));
+  EXPECT_TRUE(
+      CompareMatrices(constrained_input, inputs.col(kNumTimeSamples - 2),
+                      1e-10, MatrixCompareType::absolute));
+  EXPECT_TRUE(
+      CompareMatrices(constrained_input,
+                      input_traj.value(times_out[kNumTimeSamples - 2]),
+                      1e-10, MatrixCompareType::absolute));
+
+  PiecewisePolynomial<double> state_traj =
+      direct_traj.ReconstructStateTrajectory();
+  EXPECT_TRUE(
+      CompareMatrices(constrained_state, states.col(0),
+                      1e-10, MatrixCompareType::absolute));
+  EXPECT_TRUE(
+      CompareMatrices(constrained_state, state_traj.value(times_out[0]),
+                      1e-10, MatrixCompareType::absolute));
+  EXPECT_TRUE(
+      CompareMatrices(constrained_state, states.col(kNumTimeSamples - 1),
+                      1e-10, MatrixCompareType::absolute));
+  EXPECT_TRUE(
+      CompareMatrices(constrained_state,
+                      state_traj.value(times_out[kNumTimeSamples - 1]),
+                      1e-10, MatrixCompareType::absolute));
 
   result = direct_traj.SolveTraj(t_init_in, inputs_u, states_x);
   EXPECT_EQ(result, SolutionResult::kSolutionFound) << "Result is an Error";
