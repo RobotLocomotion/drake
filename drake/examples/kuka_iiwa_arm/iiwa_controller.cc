61a073f74fb0ccc4497aa588207256c41794ef9f
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-08-04 14:26:02 +0200

    PD controller test WIP

---
+++ drake/examples/kuka_iiwa_arm/iiwa_controller.cc
@@ -0,0 +1,22 @@
+#include "iiwa_controller.h"
+
+
+
+namespace drake {
+namespace examples {
+namespace kuka_iiwa_arm {
+
+//,
+//                 const typename System::template StateVector<double>& x0,
+//                 const typename System::template InputVector<double>& u0)
+
+//std::shared_ptr<AffineSystem<NullVector, System::template StateVector,
+//                             System::template InputVector>>
+//
+
+
+
+
+}  // namespace kuka_iiwa_arm
+}  // namespace examples
+}  // namespace drake
