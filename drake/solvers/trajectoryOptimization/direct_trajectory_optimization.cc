a90a0fa4284348da91512867887ef1b3ea8a1397
Sam Creasey <sam.creasey@tri.global>
2016-08-04 16:23:07 -0400

    Add DircolTrajectoryOptimization
    This allows specifying running costs and dynamic constraints.

--- drake/solvers/trajectoryOptimization/direct_trajectory_optimization.cc
+++ drake/solvers/trajectoryOptimization/direct_trajectory_optimization.cc
@@ -13,7 +13,7 @@ VectorXd VectorDiff(const VectorXd& vec) {
   const int len_minus1 = vec.size() - 1;
   return vec.tail(len_minus1) - vec.head(len_minus1);
 }
-}
+}  // anon namespace
 
 // DirectTrajectoryOptimization
 // For readability of long lines, these single-letter variables names are
@@ -56,9 +56,6 @@ DirectTrajectoryOptimization::DirectTrajectoryOptimization(
   all_inf.fill(std::numeric_limits<double>::infinity());
   opt_problem_.AddBoundingBoxConstraint(MatrixXd::Zero(N_ - 1, 1), all_inf,
                                         {h_vars_});
-
-  // TODO(Lucy-tri) Create constraints for dynamics and add them.
-  // Matlab: obj.addDynamicConstraints();
 }
 
 void DirectTrajectoryOptimization::AddInputBounds(
@@ -121,7 +118,7 @@ class FinalCostWrapper : public Constraint {
   std::shared_ptr<Constraint> constraint_;
 };
 
-}
+}  // anon namespace
 
 // We just use a generic constraint here since we need to mangle the
 // input and output anyway.
@@ -131,7 +128,6 @@ void DirectTrajectoryOptimization::AddFinalCost(
       N_, num_states_, constraint);
   opt_problem_.AddCost(wrapper, {h_vars_, x_vars_.tail(num_states_)});
 }
-
 
 void DirectTrajectoryOptimization::GetInitialVars(
     double timespan_init_in, const PiecewisePolynomial<double>& traj_init_u,
--- drake/solvers/trajectoryOptimization/direct_trajectory_optimization.h
+++ drake/solvers/trajectoryOptimization/direct_trajectory_optimization.h
@@ -24,7 +24,13 @@ namespace solvers {
  * implementation assumes that all constraints and costs are
  * time-invariant.
  *
- * TODO(Lucy-tri) This class is a WIP.
+ * TODO(Lucy-tri) While this class can be instanced as a standalone
+ * object, it's not very useful in that state and should generally be
+ * used as a base class for other optimization approaches which can
+ * define more interesting costs and constraints.  Consider making use
+ * as a base class mandatory (possibly through a protected constructor
+ * or pure virtual implementation of something which every derived
+ * class ends up using, like AddRunningCost or AddDynamicConstraint).
  */
 class DRAKETRAJECTORYOPTIMIZATION_EXPORT DirectTrajectoryOptimization {
  public:
@@ -116,7 +122,6 @@ class DRAKETRAJECTORYOPTIMIZATION_EXPORT DirectTrajectoryOptimization {
     return c;
   }
 
-
   /**
    * Add a cost to the final state and total time.
    *
@@ -202,6 +207,14 @@ class DRAKETRAJECTORYOPTIMIZATION_EXPORT DirectTrajectoryOptimization {
    * Returns a vector containing the state values at each knot point.
    */
   std::vector<Eigen::MatrixXd> GetStateVector() const;
+
+  int num_inputs() const { return num_inputs_; }
+  int num_states() const { return num_states_; }
+  int N() const { return N_; }
+  OptimizationProblem* opt_problem() { return &opt_problem_; }
+  const DecisionVariableView& h_vars() const { return h_vars_; }
+  const DecisionVariableView& u_vars() const { return u_vars_; }
+  const DecisionVariableView& x_vars() const { return x_vars_; }
 
  private:
   /**
