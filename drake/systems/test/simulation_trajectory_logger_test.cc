c96d0c5afa8877606865266fd05b2044fd71b966
hongkai-dai <daihongkai@gmail.com>
2016-08-04 12:36:35 -0400

    rename trajectory logger, get rid of the randomness in the test

--- drake/systems/test/simulation_trajectory_logger_test.cc
+++ drake/systems/test/simulation_trajectory_logger_test.cc
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
 #include "drake/systems/Simulation.h"
-#include "drake/systems/TrajectoryLogger.h"
+#include "drake/systems/trajectory_logger.h"
 #include "drake/systems/test/system_test_util.h"
 
 using Eigen::Dynamic;
@@ -13,13 +13,32 @@ namespace {
 using drake::EigenVector;
 
 int DoMain(int argc, char* argv[]) {
-	auto affine_sys = drake::system_test::CreateRandomAffineSystem<2,2,2>(
-		2, 2, 2);
-  //auto sys1 = AffineSystem<EigenVector<2>::type,EigenVector<2>::type,EigenVector<2>::type>();
-  //TrajectoryLogger<EigenVector<2>::type> logger();
+  const int kXdim = 2;
+  const int kYdim = 1;
+  const int kUdim = 1;
+  // simulate a double-integrator xdot = x +[0;u]; y = x(0);
+  Eigen::Matrix<double,kXdim,kXdim> A;
+  A.setIdentity();
+  Eigen::Matrix<double,kXdim,kUdim> B;
+  B.setZero();
+  B(1,0) = 1.0;
+  Eigen::Matrix<double,kYdim,kXdim> C;
+  C.setZero();
+  C(0,0) = 1.0;
+  Eigen::Matrix<double,kYdim,kUdim> D;
+  D.setZero();
+  Eigen::Matrix<double,kXdim,1> xdot0;
+  xdot0.setZero();
+  Eigen::Matrix<double,kYdim,1> y0;
+  y0.setZero();
+	auto affine_sys = std::make_shared<AffineSystem<
+                                       EigenVector<kXdim>::type,
+                                       EigenVector<kUdim>::type,
+                                       EigenVector<kYdim>::type>
+                                    >(A,B,xdot0,C,D,y0);
 
 	auto traj_logger =
-	     std::make_shared<TrajectoryLogger<EigenVector<2>::type>>(2);
+	     std::make_shared<TrajectoryLogger<EigenVector<kYdim>::type>>(kYdim);
 
 	auto sys = cascade(affine_sys, traj_logger);
 
@@ -52,13 +71,17 @@ int DoMain(int argc, char* argv[]) {
   // Starts the simulation.
 	const double kStartTime = 0;
 	Eigen::VectorXd x0 = Eigen::VectorXd::Zero(affine_sys->getNumStates());
-	x0(0) = 1.0;
+  double init_vel = 1.0;
+	x0(1) = init_vel;
   drake::simulate(*sys.get(), kStartTime, duration, x0, options);
-  auto traj_time = traj_logger->getTrajectoryTime();
-  auto traj_val = traj_logger->getTrajectorySamples();
-  if(traj_time.size() != traj_val.size()) {
+  auto traj = traj_logger->getTrajectory();
+  if(traj.time.size() != traj.val.size()) {
     throw std::runtime_error("Error: number of time samples does not match "
       "that of the trajectory");
+  }
+  for(size_t i = 0; i<traj.time.size(); ++i) {
+    assert(std::abs(traj.val[i](0)-traj.time[i]*init_vel)<=1E-5);
+    assert(std::abs(traj.val[i](1)-init-vel)<=1E-5);
   }
   return 0;
 }
