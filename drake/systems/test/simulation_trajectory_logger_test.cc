1593fcb43efa0060b5178e527ce2f2867fd1c6aa
hongkai-dai <daihongkai@gmail.com>
2016-08-04 16:50:12 -0400

    run clang-format

--- drake/systems/test/simulation_trajectory_logger_test.cc
+++ drake/systems/test/simulation_trajectory_logger_test.cc
@@ -1,7 +1,8 @@
-#include "gtest/gtest.h"
-#include "drake/systems/Simulation.h"
 #include "drake/systems/trajectory_logger.h"
+
+#include "drake/systems/Simulation.h"
 #include "drake/systems/test/system_test_util.h"
+#include "gtest/gtest.h"
 
 using Eigen::Dynamic;
 
@@ -17,32 +18,30 @@ int DoMain(int argc, char* argv[]) {
   const int kYdim = 1;
   const int kUdim = 1;
   // simulate a double-integrator xdot = x +[0;u]; y = x(0);
-  Eigen::Matrix<double,kXdim,kXdim> A;
+  Eigen::Matrix<double, kXdim, kXdim> A;
   A.setIdentity();
-  Eigen::Matrix<double,kXdim,kUdim> B;
+  Eigen::Matrix<double, kXdim, kUdim> B;
   B.setZero();
-  B(1,0) = 1.0;
-  Eigen::Matrix<double,kYdim,kXdim> C;
+  B(1, 0) = 1.0;
+  Eigen::Matrix<double, kYdim, kXdim> C;
   C.setZero();
-  C(0,0) = 1.0;
-  Eigen::Matrix<double,kYdim,kUdim> D;
+  C(0, 0) = 1.0;
+  Eigen::Matrix<double, kYdim, kUdim> D;
   D.setZero();
-  Eigen::Matrix<double,kXdim,1> xdot0;
+  Eigen::Matrix<double, kXdim, 1> xdot0;
   xdot0.setZero();
-  Eigen::Matrix<double,kYdim,1> y0;
+  Eigen::Matrix<double, kYdim, 1> y0;
   y0.setZero();
-	auto affine_sys = std::make_shared<AffineSystem<
-                                       EigenVector<kXdim>::type,
-                                       EigenVector<kUdim>::type,
-                                       EigenVector<kYdim>::type>
-                                    >(A,B,xdot0,C,D,y0);
+  auto affine_sys = std::make_shared<
+      AffineSystem<EigenVector<kXdim>::type, EigenVector<kUdim>::type,
+                   EigenVector<kYdim>::type>>(A, B, xdot0, C, D, y0);
 
-	auto traj_logger =
-	     std::make_shared<TrajectoryLogger<EigenVector<kYdim>::type>>(kYdim);
+  auto traj_logger =
+      std::make_shared<TrajectoryLogger<EigenVector<kYdim>::type>>(kYdim);
 
-	auto sys = cascade(affine_sys, traj_logger);
+  auto sys = cascade(affine_sys, traj_logger);
 
-	// Specifies the simulation options.
+  // Specifies the simulation options.
   drake::SimulationOptions options;
   options.realtime_factor = 0;  // As fast as possible.
   options.initial_step_size = 0.002;
@@ -69,27 +68,28 @@ int DoMain(int argc, char* argv[]) {
   }
 
   // Starts the simulation.
-	const double kStartTime = 0;
-	Eigen::VectorXd x0 = Eigen::VectorXd::Zero(affine_sys->getNumStates());
+  const double kStartTime = 0;
+  Eigen::VectorXd x0 = Eigen::VectorXd::Zero(affine_sys->getNumStates());
   double init_vel = 1.0;
-	x0(1) = init_vel;
+  x0(1) = init_vel;
   drake::simulate(*sys.get(), kStartTime, duration, x0, options);
   auto traj = traj_logger->getTrajectory();
-  if(traj.time.size() != traj.val.size()) {
-    throw std::runtime_error("Error: number of time samples does not match "
-      "that of the trajectory");
+  if (traj.time.size() != traj.val.size()) {
+    throw std::runtime_error(
+        "Error: number of time samples does not match "
+        "that of the trajectory");
   }
-  for(size_t i = 0; i<traj.time.size(); ++i) {
-    assert(std::abs(traj.val[i](0)-traj.time[i]*init_vel)<=1E-5);
-    assert(std::abs(traj.val[i](1)-init-vel)<=1E-5);
+  for (size_t i = 0; i < traj.time.size(); ++i) {
+    assert(std::abs(traj.val[i](0) - traj.time[i] * init_vel) <= 1E-5);
+    assert(std::abs(traj.val[i](1) - init - vel) <= 1E-5);
   }
   return 0;
 }
 
-} // namespace empty
-} // namespace test
-} // namespace systems
-} // namespace drake
+}  // namespace empty
+}  // namespace test
+}  // namespace systems
+}  // namespace drake
 
 int main(int argc, char* argv[]) {
   return drake::systems::test::DoMain(argc, argv);
--- drake/systems/trajectory_logger.h
+++ drake/systems/trajectory_logger.h
@@ -2,61 +2,59 @@
 #include "drake/systems/vector.h"
 
 namespace drake {
-	/* Implements a Drake System (@see drake/system/System.h) that saves the
-	** entire simulated output in memory. This is useful for testing and
-    ** debugging the simulation results
-	*/
-  template<template<typename> class Vector>
-  struct TimeSampleTrajectory {
-    std::vector<double> time;
-    std::vector<Vector<double>,
-      Eigen::aligned_allocator<Vector<double>>> val;
-    TimeSampleTrajectory(): time(0), val(0){};
-  };
+/* Implements a Drake System (@see drake/system/System.h) that saves the
+** entire simulated output in memory. This is useful for testing and
+** debugging the simulation results
+*/
+template <template <typename> class Vector>
+struct TimeSampleTrajectory {
+  std::vector<double> time;
+  std::vector<Vector<double>, Eigen::aligned_allocator<Vector<double>>> val;
+  TimeSampleTrajectory() : time(0), val(0){}
+};
 
-	template <template<typename> class Vector>
-	class TrajectoryLogger {
-	public:
-  	explicit TrajectoryLogger(int traj_dim) : traj_dim_(traj_dim) {};
+template <template <typename> class Vector>
+class TrajectoryLogger {
+ public:
+  explicit TrajectoryLogger(int traj_dim) : traj_dim_(traj_dim) {}
 
-      // Noncopyable
-  	TrajectoryLogger(const TrajectoryLogger&) = delete;
-  	TrajectoryLogger& operator=(const TrajectoryLogger&) = delete;
-    TrajectoryLogger(TrajectoryLogger&& ) = delete;
+  // Noncopyable
+  TrajectoryLogger(const TrajectoryLogger&) = delete;
+  TrajectoryLogger& operator=(const TrajectoryLogger&) = delete;
+  TrajectoryLogger(TrajectoryLogger&&) = delete;
 
-    template <typename ScalarType>
-    using StateVector = NullVector<ScalarType>;
-    template <typename ScalarType>
-    using InputVector = Vector<ScalarType>;
-    template <typename ScalarType>
-    using OutputVector = Vector<ScalarType>;
+  template <typename ScalarType>
+  using StateVector = NullVector<ScalarType>;
+  template <typename ScalarType>
+  using InputVector = Vector<ScalarType>;
+  template <typename ScalarType>
+  using OutputVector = Vector<ScalarType>;
 
-    template <typename ScalarType>
-    StateVector<ScalarType> dynamics(const double& t,
-    	                             const StateVector<ScalarType>& x,
-    	                             const InputVector<ScalarType>& u) const{
-      return StateVector<ScalarType>();
-    }
+  template <typename ScalarType>
+  StateVector<ScalarType> dynamics(const double& t,
+                                   const StateVector<ScalarType>& x,
+                                   const InputVector<ScalarType>& u) const {
+    return StateVector<ScalarType>();
+  }
 
-    template<typename ScalarType>
-    OutputVector<ScalarType> output(const double& t,
-                                    const StateVector<ScalarType>& x,
-    	                            const InputVector<ScalarType>& u) {
-        trajectory_.time.push_back(t);
-        trajectory_.val.push_back(u);
-    	return u;
-    }
-    bool isTimeVarying() const { return false; }
-    bool isDirectFeedthrough() const { return true; }
-    size_t getNumStates() const { return 0u; }
-    size_t getNumInputs() const { return traj_dim_; }
-    size_t getNumOutputs() const { return traj_dim_; }
+  template <typename ScalarType>
+  OutputVector<ScalarType> output(const double& t,
+                                  const StateVector<ScalarType>& x,
+                                  const InputVector<ScalarType>& u) {
+    trajectory_.time.push_back(t);
+    trajectory_.val.push_back(u);
+    return u;
+  }
+  bool isTimeVarying() const { return false; }
+  bool isDirectFeedthrough() const { return true; }
+  size_t getNumStates() const { return 0u; }
+  size_t getNumInputs() const { return traj_dim_; }
+  size_t getNumOutputs() const { return traj_dim_; }
 
-    TimeSampleTrajectory<Vector> getTrajectory() const {
-    	return trajectory_;
-    }
-	private:
-    size_t traj_dim_;
-		TimeSampleTrajectory<Vector> trajectory_;
-	};
+  TimeSampleTrajectory<Vector> getTrajectory() const { return trajectory_; }
+
+ private:
+  size_t traj_dim_;
+  TimeSampleTrajectory<Vector> trajectory_;
+};
 }
