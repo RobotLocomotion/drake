807a333a16408165fa1203366410434df1464d40
hongkai-dai <daihongkai@gmail.com>
2016-08-04 17:30:23 -0400

    use gtest

--- drake/systems/test/simulation_trajectory_logger_test.cc
+++ drake/systems/test/simulation_trajectory_logger_test.cc
@@ -1,10 +1,9 @@
 #include "drake/systems/trajectory_logger.h"
 
-#include "drake/systems/Simulation.h"
-#include "drake/systems/test/system_test_util.h"
 #include "gtest/gtest.h"
 
-using Eigen::Dynamic;
+#include "drake/systems/Simulation.h"
+#include "drake/systems/test/system_test_util.h"
 
 namespace drake {
 namespace systems {
@@ -13,13 +12,14 @@ namespace {
 
 using drake::EigenVector;
 
-int DoMain(int argc, char* argv[]) {
+GTEST_TEST(testTrajectoryLogger, testTrajectoryLogger) {
   const int kXdim = 2;
   const int kYdim = 1;
   const int kUdim = 1;
-  // simulate a double-integrator xdot = x +[0;u]; y = x(0);
+  // simulate a double-integrator xdot = 0*x +[0;u]; y = x(0);
   Eigen::Matrix<double, kXdim, kXdim> A;
-  A.setIdentity();
+  A.setZero();
+  A(0, 1) = 1.0;
   Eigen::Matrix<double, kXdim, kUdim> B;
   B.setZero();
   B(1, 0) = 1.0;
@@ -54,19 +54,6 @@ int DoMain(int argc, char* argv[]) {
   // The default value is 5 seconds.
   double duration = 5.0;
 
-  // Searches through the command line looking for a "--duration" flag followed
-  // by a floating point number that specifies a custom duration.
-  for (int ii = 1; ii < argc; ++ii) {
-    if (std::string(argv[ii]) == "--duration") {
-      if (++ii == argc) {
-        throw std::runtime_error(
-            "ERROR: Command line option \"--duration\" is not followed by a "
-            "value!");
-      }
-      duration = atof(argv[ii]);
-    }
-  }
-
   // Starts the simulation.
   const double kStartTime = 0;
   Eigen::VectorXd x0 = Eigen::VectorXd::Zero(affine_sys->getNumStates());
@@ -74,23 +61,14 @@ int DoMain(int argc, char* argv[]) {
   x0(1) = init_vel;
   drake::simulate(*sys.get(), kStartTime, duration, x0, options);
   auto traj = traj_logger->getTrajectory();
-  if (traj.time.size() != traj.val.size()) {
-    throw std::runtime_error(
-        "Error: number of time samples does not match "
-        "that of the trajectory");
-  }
+  EXPECT_EQ(traj.time.size(), traj.val.size());
+  EXPECT_EQ(traj_logger->getNumOutputs(), static_cast<size_t>(kYdim));
   for (size_t i = 0; i < traj.time.size(); ++i) {
-    assert(std::abs(traj.val[i](0) - traj.time[i] * init_vel) <= 1E-5);
-    assert(std::abs(traj.val[i](1) - init - vel) <= 1E-5);
+    EXPECT_TRUE(std::abs(traj.val[i](0) - traj.time[i] * init_vel) <= 1E-5);
   }
-  return 0;
 }
 
 }  // namespace empty
 }  // namespace test
 }  // namespace systems
 }  // namespace drake
-
-int main(int argc, char* argv[]) {
-  return drake::systems::test::DoMain(argc, argv);
-}
