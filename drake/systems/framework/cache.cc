ebc5c0c462539b8ebb9285b9b84c344011957b73
David German <david.german@tri.global>
2016-08-08 11:01:35 -0400

    Introduce a Cache object that Systems can use (via their context) to store and retrieve computed values, provided their dependencies have not changed.

--- drake/systems/framework/cache.cc
+++ drake/systems/framework/cache.cc
@@ -1,4 +1,73 @@
-// For now, this is an empty .cc file that only serves to confirm
-// cache.h is a stand-alone header.
-
 #include "drake/systems/framework/cache.h"
+
+#include <memory>
+#include <limits>
+
+#include "drake/common/drake_assert.h"
+
+namespace drake {
+namespace systems {
+
+Cache::Cache() {}
+
+Cache::~Cache() {}
+
+CacheTicket Cache::MakeCacheTicket(std::set<CacheTicket> dependencies) {
+  // Create a new ticket.
+  CacheTicket ticket = static_cast<int>(store_.size());
+
+  // Add the ticket to the dependency map. Because dependencies may not be
+  // added after the fact, the dependency map is guaranteed acyclic.
+  for (const CacheTicket& dependency : dependencies) {
+    cache_dependencies_[dependency].insert(ticket);
+  }
+
+  // Reserve a null unique_ptr<AbstractValue> for this ticket, which is
+  // initially not valid.
+  store_.push_back(CacheEntry{false, nullptr});
+  return ticket;
+}
+
+void Cache::Invalidate(CacheTicket ticket) {
+  const std::set<CacheTicket>& to_invalidate{ticket};
+  InvalidateRecursively(to_invalidate);
+}
+
+void Cache::InvalidateRecursively(const std::set<CacheTicket>& to_invalidate) {
+  for (CacheTicket ticket : to_invalidate) {
+    // Delete the ticket's cache value.
+    store_[ticket].value.reset(nullptr);
+    // Visit all the dependents of this ticket.
+    InvalidateRecursively(cache_dependencies_[ticket]);
+  }
+}
+
+AbstractValue* Cache::Set(CacheTicket ticket,
+                          std::unique_ptr<AbstractValue> value) {
+  DRAKE_ABORT_UNLESS(ticket < static_cast<int>(store_.size()));
+  store_[ticket].is_valid = true;
+  store_[ticket].value = std::move(value);
+  return store_[ticket].value.get();
+}
+
+AbstractValue* Cache::Get(CacheTicket ticket) const {
+  DRAKE_ABORT_UNLESS(ticket < static_cast<int>(store_.size()));
+  if (store_[ticket].is_valid) {
+    return store_[ticket].value.get();
+  } else {
+    return nullptr;
+  }
+}
+
+std::unique_ptr<Cache> Cache::Clone() const {
+  auto clone = std::make_unique<Cache>();
+  clone->cache_dependencies_ = cache_dependencies_;
+  for (const auto& element : store_) {
+    clone->store_.push_back(
+        CacheEntry{element.is_valid, element.value->Clone()});
+  }
+  return clone;
+}
+
+}  // namespace systems
+}  // namespace drake
--- drake/systems/framework/cache.h
+++ drake/systems/framework/cache.h
@@ -1,24 +1,65 @@
 #pragma once
 
+#include <cstddef>
+#include <map>
+#include <memory>
+#include <set>
+#include <tuple>
+#include <vector>
+
+#include "drake/drakeSystemFramework_export.h"
+#include "drake/systems/framework/value.h"
+
 namespace drake {
 namespace systems {
 
-// This is a placeholder.
-// TODO(david-german-tri): Add actual functionality.
-template <typename T>
-class Cache {
- public:
-  Cache() {}
-  virtual ~Cache() {}
+typedef int64_t CacheTicket;
 
-  // Cache objects are copyable with the default copy constructor.
-  Cache(const Cache& other) = default;
-  Cache& operator=(const Cache& other) = default;
+typedef struct {
+  bool is_valid;
+  std::unique_ptr<AbstractValue> value;
+} CacheEntry;
+
+/// A key-value cache that can be invalidated based on changes to fields in the
+/// Context.
+///
+/// Invalidated objects are deleted, so as a rule of thumb, you should use the
+/// Cache if the cost of recomputing a cached value is significantly greater
+/// than the cost of allocating space for it.
+///
+/// The cache is not thread-safe.
+class DRAKESYSTEMFRAMEWORK_EXPORT Cache {
+ public:
+  Cache();
+  virtual ~Cache();
+
+  /// Creates a new cache ticket, which will be invalidated whenever any of
+  /// the dependencies are invalidated.
+  CacheTicket MakeCacheTicket(std::set<CacheTicket> dependencies);
+
+  /// Invalidates and deletes the value for @p ticket, and all lines that
+  /// depend on it.
+  void Invalidate(CacheTicket ticket);
+
+  /// Takes ownership of a cached item, and returns a bare pointer to the item.
+  AbstractValue* Set(CacheTicket ticket, std::unique_ptr<AbstractValue> value);
+
+  /// Returns the cached item for the given ticket, or nullptr if the item
+  /// has been invalidated.
+  AbstractValue* Get(CacheTicket ticket) const;
+
+  std::unique_ptr<Cache> Clone() const;
 
  private:
-  // Cache objects are not moveable.
-  Cache(Cache&& other) = delete;
-  Cache& operator=(Cache&& other) = delete;
+  // Invalidates all tickets that depend on the tickets in @p to_invalidate.
+  void InvalidateRecursively(const std::set<CacheTicket>& to_invalidate);
+
+  // For each cache ticket, the set of other cache tickets that are invalidated
+  // when it is invalidated. This graph is directed and acyclic.
+  std::map<CacheTicket, std::set<CacheTicket>> cache_dependencies_;
+
+  // For each cache ticket, the stored value, and its validity bit.
+  std::vector<CacheEntry> store_;
 };
 
 }  // namespace systems
