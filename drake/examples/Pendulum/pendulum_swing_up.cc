fbc1c8c4e582f2059fdbdee8fcc8967d97956d26
Sam Creasey <sam.creasey@tri.global>
2016-08-09 13:37:15 -0400

    Add a trajectory optimization test using the pendulum example.
    Also adds a demo program which can be used to visualize the output of the test (or try some different combinations of parameters and look at the result, etc...)

---
+++ drake/examples/Pendulum/pendulum_swing_up.cc
@@ -0,0 +1,86 @@
+#include <cmath>
+
+#include <Eigen/Core>
+
+#include "drake/common/drake_assert.h"
+#include "drake/common/eigen_types.h"
+#include "drake/examples/Pendulum/pendulum_swing_up.h"
+#include "drake/solvers/Function.h"
+#include "drake/systems/plants/constraint/dynamic_constraint.h"
+
+using drake::solvers::detail::VecIn;
+using drake::solvers::detail::VecOut;
+using drake::solvers::LinearEqualityConstraint;
+
+typedef PiecewisePolynomial<double> PiecewisePolynomialType;
+
+namespace drake {
+namespace examples {
+namespace pendulum {
+namespace {
+
+/**
+ * Define a function to be evaluated as the running cost for a
+ * pendulum trajectory.
+ */
+class PendulumRunningCost {
+ public:
+  static size_t numInputs() { return
+        PendulumState<double>::RowsAtCompileTime + 2; }
+  static size_t numOutputs() { return 1; }
+
+  template <typename ScalarType>
+  void eval(VecIn<ScalarType> const& x, VecOut<ScalarType>& y) const {
+    DRAKE_ASSERT(static_cast<size_t>(x.rows()) == numInputs());
+    DRAKE_ASSERT(static_cast<size_t>(y.rows()) == numOutputs());
+
+    const auto u = x.tail(1);
+    const double R = 10;
+    y = (R * u) * u;
+  }
+};
+
+class PendulumFinalCost {
+ public:
+  static size_t numInputs() { return
+        PendulumState<double>::RowsAtCompileTime + 1; }
+  static size_t numOutputs() { return 1; }
+
+  template <typename ScalarType>
+  void eval(VecIn<ScalarType> const& x, VecOut<ScalarType>& y) const {
+    DRAKE_ASSERT(static_cast<size_t>(x.rows()) == numInputs());
+    DRAKE_ASSERT(static_cast<size_t>(y.rows()) == numOutputs());
+
+    y(0) = x(0);
+  }
+};
+}  // anon namespace
+
+void AddSwingUpTrajectoryParams(
+    std::shared_ptr<Pendulum> pendulum,
+    int num_time_samples,
+    const Eigen::Vector2d& x0, const Eigen::Vector2d& xG,
+    solvers::DircolTrajectoryOptimization* dircol_traj) {
+
+  const int kTorqueLimit = 3;  // Arbitrary, taken from PendulumPlant.m.
+  const drake::Vector1d umin(-kTorqueLimit);
+  const drake::Vector1d umax(kTorqueLimit);
+  dircol_traj->AddInputBounds(umin, umax);
+
+  dircol_traj->AddStateConstraint(
+      std::make_shared<LinearEqualityConstraint>(
+          Eigen::Matrix2d::Identity(), x0), {0});
+  dircol_traj->AddStateConstraint(
+      std::make_shared<LinearEqualityConstraint>(
+          Eigen::Matrix2d::Identity(), xG), {num_time_samples - 1});
+
+  dircol_traj->AddRunningCost(PendulumRunningCost());
+  dircol_traj->AddFinalCost(PendulumFinalCost());
+  dircol_traj->AddDynamicConstraint(
+      std::make_shared<drake::systems::SystemDynamicConstraint<Pendulum>>(
+          pendulum));
+}
+
+}  // pendulum
+}  // examples
+}  // drake
