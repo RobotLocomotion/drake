eb97d66d0f5275d36aca30d65e3a662c60380231
David German <david.german@tri.global>
2016-08-09 18:51:18 -0400

    Add a mechanism for a System to declare its input and output topology independent of the actual input and output values. Provide some convenience infrastructure for allocating Context and Output automatically based on those declarations in leaf Systems.

---
+++ drake/systems/framework/leaf_system.cc
@@ -0,0 +1,4 @@
+// For now, this is an empty .cc file that only serves to confirm
+// leaf_system.h is a stand-alone header.
+
+#include "drake/systems/framework/leaf_system.h"
---
+++ drake/systems/framework/system_port_descriptor.cc
@@ -0,0 +1,4 @@
+// For now, this is an empty .cc file that only serves to confirm
+// system_port_descriptor.h is a stand-alone header.
+
+#include "drake/systems/framework/system_port_descriptor.h"
