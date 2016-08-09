ba64dfb2b67c8f4b3abd6ffe54dd7e10868dff85
amcastro-tri <alejandro.castro@tri.global>
2016-08-09 14:18:32 -0400

    Implements System 2.0 Gain primitive.
    Moves TODO to a more proper location
    
    Fixes lint
    
    Fixes documentation and comments
    
    Adresses reviewers comments
    
    Adresses some simple reviewers comments.
    
    Implements gain-inl.h version and wraps up auto-diff unit test
    
    Fix to CI failure in Windows
    
    Replaces equal by constructor
    
    Will this fix the CI error in Windows?
    
    Windwos fix #3
    
    Addresses reviews
    
    Drake independent dummy_test.cc
    
    Addresses some reviews
    
    Removes getst.h dependency from dummy_test.cc
    
    Replaces Vector3 for VectorX
    
    Uses .eval()
    
    Disables alignment
    
    Addresses reviews
    
    Fixes rendering of doxygen doc
    
    Removes dummy_test.cc
    
    Removes hacky solutions to alignmet issue
    
    Splits unit test for scalar types
    
    Uses System<T>::DeclareInputPort
    
    Adds system methods to ask number of ports and acces specific ports
    
    Provides a solution to the alignment problem
    
    Removes debugging PRINT_VAR
    
    Addresses reviews

---
+++ drake/systems/framework/primitives/gain-inl.h
@@ -0,0 +1,70 @@
+#pragma once
+
+/// @file
+/// Template method implementations for gain.h.
+/// Most users should only include that file, not this one.
+/// For background, see http://drake.mit.edu/cxx_inl.html.
+
+#include "drake/systems/framework/primitives/gain.h"
+
+#include <stdexcept>
+#include <string>
+
+#include "drake/common/drake_assert.h"
+#include "drake/drakeSystemFramework_export.h"
+#include "drake/systems/framework/basic_vector.h"
+#include "drake/systems/framework/context.h"
+
+namespace drake {
+namespace systems {
+
+template <typename T>
+Gain<T>::Gain(const T& k, int length) : gain_(k) {
+  // TODO(amcastro-tri): remove the length parameter from the constructor once
+  // #3109 supporting automatic lengths is resolved.
+  this->DeclareInputPort(kVectorValued, length, kContinuousSampling);
+  this->DeclareOutputPort(kVectorValued, length, kContinuousSampling);
+}
+
+template <typename T>
+void Gain<T>::EvalOutput(const ContextBase<T>& context,
+                          SystemOutput<T>* output) const {
+  // Checks that the single output port has the correct length.
+  // Checks on the output structure are assertions, not exceptions,
+  // since failures would reflect a bug in the Gain implementation, not
+  // user error setting up the system graph. They do not require unit test
+  // coverage, and should not run in release builds.
+
+  DRAKE_ASSERT(output->get_num_ports() == 1);
+  VectorInterface<T>* output_vector =
+      output->get_mutable_port(0)->GetMutableVectorData();
+  DRAKE_ASSERT(output_vector != nullptr);
+  DRAKE_ASSERT(output_vector->get_value().rows() ==
+      this->get_output_port(0).get_size());
+
+  // Check that there are the expected number of input ports.
+  if (context.get_num_input_ports() != 1) {
+    throw std::out_of_range("Expected only one input port, but had " +
+        std::to_string(context.get_num_input_ports()));
+  }
+
+  // There is only one input.
+  // TODO(amcastro-tri): Solve #3140 so that the next line reads:
+  // auto& input_vector = System<T>::get_input_vector(context, 0);
+  // where the return is an Eigen expression.
+  const VectorInterface<T>* input_vector = context.get_vector_input(0);
+
+  // Check the expected length.
+  DRAKE_ASSERT(input_vector != nullptr);
+  DRAKE_ASSERT(input_vector->get_value().rows() ==
+      this->get_input_port(0).get_size());
+
+  // TODO(amcastro-tri): Solve #3140 so that we can readily access the Eigen
+  // vector like so:
+  // auto& output_vector = System<T>::get_output_vector(context, 0);
+  // where the return is an Eigen expression.
+  output_vector->get_mutable_value() = gain_ * input_vector->get_value();
+}
+
+}  // namespace systems
+}  // namespace drake
