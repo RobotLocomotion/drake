5403237d1572d09964620216e1b56795dbe55340
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-08-10 17:14:45 +0200

    WIP cleanup of code and test

--- drake/examples/kuka_iiwa_arm/test/kuka_iiwa_gravity_compensation_test.cc
+++ drake/examples/kuka_iiwa_arm/test/kuka_iiwa_gravity_compensation_test.cc
@@ -4,14 +4,10 @@
 
 #include "drake/common/drake_path.h"
 #include "drake/examples/kuka_iiwa_arm/iiwa_simulation.h"
-#include "drake/examples/kuka_iiwa_arm/robot_state_tap.h"
 #include "drake/systems/LCMSystem.h"
 #include "drake/systems/LinearSystem.h"
-#include "drake/systems/Simulation.h"
-#include "drake/systems/cascade_system.h"
 #include "drake/systems/gravity_compensation_control_system.h"
 #include "drake/systems/plants/BotVisualizer.h"
-#include "drake/systems/plants/RigidBodySystem.h"
 
 using drake::RigidBodySystem;
 using drake::BotVisualizer;
@@ -31,7 +27,7 @@ namespace {
 // (in contrast with a pure PD controller), accurate position control
 // is possible. The test looks for 2 things : if the set-point is
 // the same as the initial pose, the robot maintains its pose with
-// negligeable errors. 
+// negligible errors.
 // The test verifies that the position reached is within some
 // bound with respect to the initial condition.
 GTEST_TEST(testIIWAArm, iiwaArmGravityCompensationControl) {
@@ -56,43 +52,59 @@ GTEST_TEST(testIIWAArm, iiwaArmGravityCompensationControl) {
   // Smaller gains intentionally used for demo.
   const double Kp_common = 50.0;
   const double Kd_common = 0.0;
-  VectorXd Kpdiag = VectorXd::Constant(7, Kp_common);
-  VectorXd Kddiag = VectorXd::Constant(7, Kd_common);
+  VectorXd Kpdiag = VectorXd::Constant(num_dof, Kp_common);
+  VectorXd Kddiag = VectorXd::Constant(num_dof, Kd_common);
   MatrixXd Kp = Kpdiag.asDiagonal();
   MatrixXd Kd = Kddiag.asDiagonal();
-
 
   // Obtains an initial state of the simulation.
   VectorXd x0 = VectorXd::Zero(iiwa_system->getNumStates());
   x0.head(iiwa_tree->number_of_positions()) = iiwa_tree->getZeroConfiguration();
 
-  Eigen::VectorXd random_initial_configuration(7);
+  Eigen::VectorXd random_initial_configuration(num_dof);
   random_initial_configuration << 0.01, -0.01, 0.01, 0.5, 0.01, -0.01, 0.01;
-  x0.head(7) += random_initial_configuration;
+  x0.head(num_dof) += random_initial_configuration;
 
-  // set point is the initial state
+  // set point is the configuration corresponding to all 0 radians.
+  VectorXd set_point_vector = VectorXd::Zero(num_dof);
   auto set_point = std::make_shared<
       AffineSystem<NullVector, NullVector, RigidBodySystem::StateVector>>(
       MatrixXd::Zero(0, 0), MatrixXd::Zero(0, 0), VectorXd::Zero(0),
-      MatrixXd::Zero(7, 0), MatrixXd::Zero(7, 0), VectorXd::Zero(7));
+      MatrixXd::Zero(num_dof, 0), MatrixXd::Zero(num_dof, 0), set_point_vector);
 
   auto controlled_robot =
   std::allocate_shared<GravityCompensatedPDPositionControlSystem<RigidBodySystem>>(
   Eigen::aligned_allocator<GravityCompensatedPDPositionControlSystem<RigidBodySystem>>(),
   iiwa_system, Kp, Kd);
 
-  auto sys = cascade(cascade(cascade(set_point, controlled_robot), visualizer,
+  auto sys = cascade(cascade(cascade(set_point, controlled_robot), visualizer),
                              robot_state_tap);
 
   drake::SimulationOptions options = SetupSimulation();
 
-  // Starts the simulation.
+  // Specifies the start time of the simulation.
   const double kStartTime = 0;
 
-  // Simulation duration in seconds.
-  const double kDuration = 0.5;
+  // Specifies the duration of the simulation.
+  const double kDuration = 2.0;
 
-  drake::simulate(*sys.get(), kStartTime, kDuration, x0, options);
+  EXPECT_NO_THROW(drake::simulate(*sys.get(), kStartTime, kDuration, x0,
+                                  options));
+
+  auto xf = robot_state_tap->get_input_vector();
+
+  // Ensure joint position and velocity limits are not violated.
+  EXPECT_NO_THROW(CheckLimitViolations(iiwa_system, xf));
+
+  // Expect normed joint position difference is below a maximum value.
+  double max_position_norm = 1e-6;
+  EXPECT_TRUE((xf.head(num_dof) - set_point_vector).squaredNorm()
+                  < max_position_norm);
+
+  // Expect final normed joint velocity is larger than a minimum value.
+  // (since this controller wont stay at rest at the set-point).
+  double max_velocity_norm = 1e-6;
+  EXPECT_TRUE(xf.tail(num_dof).squaredNorm() < max_velocity_norm);
 
 }
 
