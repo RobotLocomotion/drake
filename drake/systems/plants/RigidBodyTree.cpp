1ff00ba38e98e53c76e85467f8b3b7838e9b5786
Chien-Liang Fok <liang.fok@tri.global>
2016-08-11 10:10:59 -0400

    Fixed cpplint error.

--- drake/systems/plants/RigidBodyTree.cpp
+++ drake/systems/plants/RigidBodyTree.cpp
@@ -2164,8 +2164,8 @@ void RigidBodyTree::addRobotFromSDF(
     const std::string& sdf_filename,
     const DrakeJoint::FloatingBaseType floating_base_type,
     std::shared_ptr<RigidBodyFrame> weld_to_frame) {
-  drake::parsers::sdf::AddModelInstancesFromSdfFile(sdf_filename, floating_base_type,
-      weld_to_frame, this);
+  drake::parsers::sdf::AddModelInstancesFromSdfFile(sdf_filename,
+      floating_base_type, weld_to_frame, this);
 }
 
 // Explicit template instantiations for massMatrix.
