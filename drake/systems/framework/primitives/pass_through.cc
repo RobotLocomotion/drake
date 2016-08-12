44fca74e3b1e1a6873783b5ec9b3eda0017e6865
amcastro-tri <alejandro.castro@tri.global>
2016-08-12 09:37:26 -0400

    Implements PassThrough system
    Fixes lint
    
    Refactors Buffer --> PassThrough
    
    Refactors file names
    
    Addresses reviews
    
    Splits autodiff scalartype test into its own file
    
    Addresses reviews
    
    Makes use of #3102
    
    Addresses reviews
    
    Addresses reviews
    
    Changes kContinuous sampling to kInheritedSmapling

---
+++ drake/systems/framework/primitives/pass_through.cc
@@ -0,0 +1,9 @@
+#include "drake/systems/framework/primitives/pass_through-inl.h"
+
+namespace drake {
+namespace systems {
+
+template class DRAKESYSTEMFRAMEWORK_EXPORT PassThrough<double>;
+
+}  // namespace systems
+}  // namespace drake
