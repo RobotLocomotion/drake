d88c7497882e429de71e1bd963f56609d27bf5cb
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-08-19 12:33:29 -0400

    Delay setting up options
    Move the logic to set up drake's (external project) options to a macro,
    rather than executing it immediately on inclusion of the file. This is
    necessary for some of the dependent options to be set up properly on an
    initial run (which matters especially for CI), as the variables that
    control them may not be set up yet when the file is included.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -108,162 +108,168 @@ function(drake_optional_external NAME DEFAULT_STATE)
   set(HAVE_${NAME} "${WITH_${NAME}}" PARENT_SCOPE)
 endfunction()
 
-###############################################################################
-# BEGIN "system" dependencies
+#------------------------------------------------------------------------------
+# Set up options
+#------------------------------------------------------------------------------
+macro(drake_setup_options)
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # BEGIN "system" dependencies
 
-# These are packages that we can build, but which we allow the user to use
-# their own copy (usually provided by the system) if preferred. Some of these
-# may be required.
+  # These are packages that we can build, but which we allow the user to use
+  # their own copy (usually provided by the system) if preferred. Some of these
+  # may be required.
 
-drake_system_dependency(
-  EIGEN REQUIRES Eigen3 VERSION 3.2.92
-  "Eigen C++ matrix library")
+  drake_system_dependency(
+    EIGEN REQUIRES Eigen3 VERSION 3.2.92
+    "Eigen C++ matrix library")
 
-drake_system_dependency(
-  GOOGLETEST REQUIRES GTest
-  "Google testing framework")
+  drake_system_dependency(
+    GOOGLETEST REQUIRES GTest
+    "Google testing framework")
 
-drake_system_dependency(
-  GFLAGS REQUIRES gflags
-  "Google command-line flags processing library")
+  drake_system_dependency(
+    GFLAGS REQUIRES gflags
+    "Google command-line flags processing library")
 
-drake_system_dependency(
-  LCM OPTIONAL REQUIRES lcm
-  "Lightweight Communications and Marshaling IPC suite")
+  drake_system_dependency(
+    LCM OPTIONAL REQUIRES lcm
+    "Lightweight Communications and Marshaling IPC suite")
 
-if(WIN32 AND HAVE_LCM)
-  drake_system_dependency(GTK REQUIRES GLib2
-    DEPENDS "WITH_LCM"
-    "GLib toolkit")
-endif()
-
-drake_system_dependency(
-  BOT_CORE_LCMTYPES OPTIONAL REQUIRES bot2-core
-  DEPENDS "HAVE_LCM"
-  "libbot2 robotics suite LCM types")
-
-drake_system_dependency(YAML_CPP OPTIONAL REQUIRES yaml-cpp
-  "C++ library for reading and writing YAML configuration files")
-
-# END "system" dependencies
-###############################################################################
-# BEGIN external projects that are ON by default
-
-drake_optional_external(GOOGLE_STYLEGUIDE ON
-  "Google code style tools for cpplint.py style checking" ON)
-
-drake_optional_external(SPDLOG ON
-  "Fast C++ text logging facility\; disabling will turn off text logging")
-
-drake_optional_external(SWIGMAKE ON
-  "Helper tools to build Python & MATLAB wrappers"
-  "for C++ libraries with Eigen")
-
-drake_optional_external(BULLET ON "Bullet library for collision detection")
-
-if(NOT WIN32)
-  # Not win32 yet; builds, but requires manual installation of VTKk, etc.
-  drake_optional_external(DIRECTOR ON DEPENDS "HAVE_LCM\;HAVE_BOT_CORE_LCMTYPES"
-    "VTK-based visualization tool and robot user interface")
-
-  # Probably not on Windows until lcmgl is split out
-  drake_optional_external(LIBBOT ON
-    "libbot2 robotics suite\;"
-    "used for its simple open-gl visualizer + lcmgl for director")
-
-  drake_optional_external(NLOPT ON "Non-linear optimization solver")
-
-  drake_optional_external(DREAL ON
-    "Non-linear SMT solver and automated reasoning tool")
-
-  # IPOPT is currently disabled on Mac due to MATLAB compatibility issues:
-  # https://github.com/RobotLocomotion/drake/issues/2578
-  if (NOT APPLE)
-    drake_optional_external(IPOPT ON
-      "Interior Point Optimizer, for solving non-linear optimizations")
+  if(WIN32 AND HAVE_LCM)
+    drake_system_dependency(GTK REQUIRES GLib2
+      DEPENDS "WITH_LCM"
+      "GLib toolkit")
   endif()
 
-  drake_optional_external(SWIG_MATLAB ON
-    "A version of SWIG with MATLAB support")
-endif()
+  drake_system_dependency(
+    BOT_CORE_LCMTYPES OPTIONAL REQUIRES bot2-core
+    DEPENDS "HAVE_LCM"
+    "libbot2 robotics suite LCM types")
 
-drake_optional_external(SNOPT_PRECOMPILED ON DEPENDS "NOT WITH_SNOPT"
-  "Sparse Non-linear Optimizer precompiled binaries"
-  "(the source requires a license)")
+  drake_system_dependency(YAML_CPP OPTIONAL REQUIRES yaml-cpp
+    "C++ library for reading and writing YAML configuration files")
 
-# END external projects that are ON by default
-###############################################################################
-# BEGIN external projects that are only needed when MATLAB is in use
+  # END "system" dependencies
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # BEGIN external projects that are ON by default
 
-# The following projects are default ON when MATLAB is present and enabled.
-# Otherwise, they are hidden and default OFF.
-drake_optional_external(SPOTLESS ON
-  DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE"
-  "polynomial optimization front-end for MATLAB")
+  drake_optional_external(GOOGLE_STYLEGUIDE ON
+    "Google code style tools for cpplint.py style checking" ON)
 
-# The following projects are default OFF when MATLAB is present and enabled.
-# Otherwise, they are hidden and default OFF. Some of them may also be hidden
-# on Windows regardless of the status of MATLAB.
-drake_optional_external(BERTINI OFF
-  DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE\;NOT WIN32"
-  "solve polynomial equations\;"
-  "free but pod requires permissions (can't redistribute)")
+  drake_optional_external(SPDLOG ON
+    "Fast C++ text logging facility\; disabling will turn off text logging")
 
-drake_optional_external(GLOPTIPOLY OFF
-  DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE\;NOT WIN32"
-  "free global polynomial optimization tooblox")
+  drake_optional_external(SWIGMAKE ON
+    "Helper tools to build Python & MATLAB wrappers"
+    "for C++ libraries with Eigen")
 
-drake_optional_external(IRIS OFF
-  DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE\;NOT WIN32\;WITH_MOSEK"
-  "fast approximate convex segmentation")
+  drake_optional_external(BULLET ON "Bullet library for collision detection")
 
-drake_optional_external(SEDUMI OFF
-  DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE\;NOT WIN32"
-  "semi-definite programming solver")
+  if(NOT WIN32)
+    # Not win32 yet; builds, but requires manual installation of VTKk, etc.
+    drake_optional_external(DIRECTOR ON
+      DEPENDS "HAVE_LCM\;HAVE_BOT_CORE_LCMTYPES"
+      "VTK-based visualization tool and robot user interface")
 
-drake_optional_external(YALMIP OFF
-  DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE\;NOT WIN32"
-  "free optimization front-end for MATLAB")
+    # Probably not on Windows until lcmgl is split out
+    drake_optional_external(LIBBOT ON
+      "libbot2 robotics suite\;"
+      "used for its simple open-gl visualizer + lcmgl for director")
 
-drake_optional_external(XFOIL OFF
-  DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE\;NOT WIN32"
-  "use w/ XFOIL to compute aerodynamic coefficients for airfoils")
+    drake_optional_external(NLOPT ON "Non-linear optimization solver")
 
-# END external projects that are only needed when MATLAB is in use
-###############################################################################
-# BEGIN external projects that are OFF by default
+    drake_optional_external(DREAL ON
+      "Non-linear SMT solver and automated reasoning tool")
 
-drake_optional_external(SNOPT OFF
-  "Sparse Non-linear Optimizer\;"
-  "requires access to RobotLocomotion/snopt-pod")
+    # IPOPT is currently disabled on Mac due to MATLAB compatibility issues:
+    # https://github.com/RobotLocomotion/drake/issues/2578
+    if (NOT APPLE)
+      drake_optional_external(IPOPT ON
+        "Interior Point Optimizer, for solving non-linear optimizations")
+    endif()
 
-drake_optional_external(SIGNALSCOPE OFF DEPENDS "NOT WIN32\;WITH_DIRECTOR"
-  "Live plotting tool for LCM messages")
+    drake_optional_external(SWIG_MATLAB ON
+      "A version of SWIG with MATLAB support")
+  endif()
 
-# Many of these might work on win32 with little or no work... they just haven't
-# been tried.
-if(NOT WIN32)
-  drake_optional_external(AVL OFF
-    "use w/ AVL to compute aerodynamic coefficients for airfoils")
+  drake_optional_external(SNOPT_PRECOMPILED ON DEPENDS "NOT WITH_SNOPT"
+    "Sparse Non-linear Optimizer precompiled binaries"
+    "(the source requires a license)")
 
-  drake_optional_external(GUROBI OFF
-    "Convex/integer optimization solver\;"
-    "free for academics (will prompt you for login bits)")
+  # END external projects that are ON by default
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # BEGIN external projects that are only needed when MATLAB is in use
 
-  drake_optional_external(MESHCONVERTERS OFF
-    "uses vcglib to convert a few standard filetypes")
+  # The following projects are default ON when MATLAB is present and enabled.
+  # Otherwise, they are hidden and default OFF.
+  drake_optional_external(SPOTLESS ON
+    DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE"
+    "polynomial optimization front-end for MATLAB")
 
-  drake_optional_external(MOSEK OFF
-    "Convex optimization solver\; free for academics")
+  # The following projects are default OFF when MATLAB is present and enabled.
+  # Otherwise, they are hidden and default OFF. Some of them may also be hidden
+  # on Windows regardless of the status of MATLAB.
+  drake_optional_external(BERTINI OFF
+    DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE\;NOT WIN32"
+    "solve polynomial equations\;"
+    "free but pod requires permissions (can't redistribute)")
 
-  drake_optional_external(OCTOMAP OFF
-    "3D occupancy mapping library\; provides oct-tree data structures")
+  drake_optional_external(GLOPTIPOLY OFF
+    DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE\;NOT WIN32"
+    "free global polynomial optimization tooblox")
 
-  # Almost works on windows;  the update step call to git was complaining about
-  # local modifications on drake003
-  drake_optional_external(TEXTBOOK OFF
-    "The Underactuated Robotics textbook and its examples")
-endif()
+  drake_optional_external(IRIS OFF
+    DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE\;NOT WIN32\;WITH_MOSEK"
+    "fast approximate convex segmentation")
 
-# END external projects that are OFF by default
-###############################################################################
+  drake_optional_external(SEDUMI OFF
+    DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE\;NOT WIN32"
+    "semi-definite programming solver")
+
+  drake_optional_external(YALMIP OFF
+    DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE\;NOT WIN32"
+    "free optimization front-end for MATLAB")
+
+  drake_optional_external(XFOIL OFF
+    DEPENDS "NOT DISABLE_MATLAB\;MATLAB_EXECUTABLE\;NOT WIN32"
+    "use w/ XFOIL to compute aerodynamic coefficients for airfoils")
+
+  # END external projects that are only needed when MATLAB is in use
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # BEGIN external projects that are OFF by default
+
+  drake_optional_external(SNOPT OFF
+    "Sparse Non-linear Optimizer\;"
+    "requires access to RobotLocomotion/snopt-pod")
+
+  drake_optional_external(SIGNALSCOPE OFF DEPENDS "NOT WIN32\;WITH_DIRECTOR"
+    "Live plotting tool for LCM messages")
+
+  # Many of these might work on win32 with little or no work... they just
+  # haven't been tried.
+  if(NOT WIN32)
+    drake_optional_external(AVL OFF
+      "use w/ AVL to compute aerodynamic coefficients for airfoils")
+
+    drake_optional_external(GUROBI OFF
+      "Convex/integer optimization solver\;"
+      "free for academics (will prompt you for login bits)")
+
+    drake_optional_external(MESHCONVERTERS OFF
+      "uses vcglib to convert a few standard filetypes")
+
+    drake_optional_external(MOSEK OFF
+      "Convex optimization solver\; free for academics")
+
+    drake_optional_external(OCTOMAP OFF
+      "3D occupancy mapping library\; provides oct-tree data structures")
+
+    # Almost works on windows;  the update step call to git was complaining
+    # about local modifications on drake003
+    drake_optional_external(TEXTBOOK OFF
+      "The Underactuated Robotics textbook and its examples")
+  endif()
+
+  # END external projects that are OFF by default
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+endmacro()
