69c78f59df5fd6bc25bcce8ceae936efd20708c9
Chien-Liang Fok <liang.fok@tri.global>
2016-08-22 16:42:03 -0400

    WIP: Added BotVisualizerSystem.

---
+++ drake/systems/bot_visualizer_system.cc
@@ -0,0 +1,171 @@
+#include "drake/systems/bot_visualizer_system.h"
+
+#include "drake/systems/framework/system_input.h"
+
+namespace drake {
+namespace systems {
+
+namespace {
+const int kNumInputPorts = 1;
+const int kPortIndex = 0;
+}  // namespace
+
+BotVisualizerSystem::BotVisualizerSystem(
+    const RigidBodyTree& tree,
+    ::lcm::LCM* lcm)
+    : tree_(tree), lcm_(lcm) {
+  initialize_drake_visualizer();
+  initialize_draw_message();
+}
+
+BotVisualizerSystem::~BotVisualizerSystem() {}
+
+std::string BotVisualizerSystem::get_name() const {
+  return "BotVisualizerSystem";
+}
+
+std::unique_ptr<ContextBase<double>> BotVisualizerSystem::CreateDefaultContext()
+    const {
+  std::unique_ptr<Context<double>> context(new Context<double>());
+  context->SetNumInputPorts(kNumInputPorts);
+  return std::unique_ptr<ContextBase<double>>(context.release());
+}
+
+std::unique_ptr<SystemOutput<double>> BotVisualizerSystem::AllocateOutput(
+    const ContextBase<double>& context) const {
+  std::unique_ptr<SystemOutput<double>> output(new LeafSystemOutput<double>);
+  return output;
+}
+
+// TODO(liang.fok) Move the contents of this method into another method that's
+// dedicated to publishing middleware messages once it is defined by System. For
+// more information, see:
+//     https://github.com/RobotLocomotion/drake/issues/2836.
+void BotVisualizerSystem::EvalOutput(const ContextBase<double>& context,
+                                    SystemOutput<double>* output) const {
+  // Obtains the input vector.
+  const VectorInterface<double>* input_vector =
+      context.get_vector_input(kPortIndex);
+
+  draw_msg_.timestamp = static_cast<int64_t>(context.get_time() * 1000.0);
+
+  const Eigen::VectorXd q = toEigen(u).head(tree_->number_of_positions());
+  KinematicsCache<double> cache = tree_->doKinematics(q);
+
+  for (size_t i = 0; i < tree_.bodies.size(); ++i) {
+    auto transform = tree_.relativeTransform(cache, 0, i);
+    auto quat = drake::math::rotmat2quat(transform.linear());
+    std::vector<float>& position = draw_msg_.position[i];
+    auto translation = transform.translation();
+    for (int j = 0; j < 3; ++j) {
+      position[j] = static_cast<float>(translation(j));
+    }
+    std::vector<float>& quaternion = draw_msg_.quaternion[i];
+    for (int j = 0; j < 4; ++j) {
+      quaternion[j] = static_cast<float>(quat(j));
+    }
+  }
+
+  lcm_->publish("DRAKE_VIEWER_DRAW", &draw_msg_);
+}
+
+void BotVisualizerSystem::initialize_drake_visualizer() {
+  drake::lcmt_viewer_load_robot message;
+  message.num_links = tree_.bodies.size();
+  for (const auto& body : tree_.bodies) {
+    drake::lcmt_viewer_link_data link;
+    link.name = body->get_name();
+    link.robot_num = body->get_model_instance_id();
+    link.num_geom = body->get_visual_elements().size();
+    for (const auto& v : body->get_visual_elements()) {
+      drake::lcmt_viewer_geometry_data gdata;
+
+      const DrakeShapes::Geometry& geometry = v.getGeometry();
+
+      switch (v.getShape()) {  // would prefer to do this through virtual
+                               // methods, but don't want to introduce any LCM
+                               // dependency on the Geometry classes
+        case DrakeShapes::BOX: {
+          gdata.type = gdata.BOX;
+          gdata.num_float_data = 3;
+          auto b = dynamic_cast<const DrakeShapes::Box&>(geometry);
+          for (int i = 0; i < 3; ++i)
+            gdata.float_data.push_back(static_cast<float>(b.size(i)));
+          break;
+        }
+        case DrakeShapes::SPHERE: {
+          gdata.type = gdata.SPHERE;
+          gdata.num_float_data = 1;
+          auto b = dynamic_cast<const DrakeShapes::Sphere&>(geometry);
+          gdata.float_data.push_back(static_cast<float>(b.radius));
+          break;
+        }
+        case DrakeShapes::CYLINDER: {
+          gdata.type = gdata.CYLINDER;
+          gdata.num_float_data = 2;
+          auto c = dynamic_cast<const DrakeShapes::Cylinder&>(geometry);
+          gdata.float_data.push_back(static_cast<float>(c.radius));
+          gdata.float_data.push_back(static_cast<float>(c.length));
+          break;
+        }
+        case DrakeShapes::MESH: {
+          gdata.type = gdata.MESH;
+          gdata.num_float_data = 3;
+          auto m = dynamic_cast<const DrakeShapes::Mesh&>(geometry);
+          gdata.float_data.push_back(static_cast<float>(m.scale_[0]));
+          gdata.float_data.push_back(static_cast<float>(m.scale_[1]));
+          gdata.float_data.push_back(static_cast<float>(m.scale_[2]));
+
+          if (m.uri_.find("package://") == 0) {
+            gdata.string_data = m.uri_;
+          } else {
+            gdata.string_data = m.resolved_filename_;
+          }
+
+          break;
+        }
+        case DrakeShapes::CAPSULE: {
+          gdata.type = gdata.CAPSULE;
+          gdata.num_float_data = 2;
+          auto c = dynamic_cast<const DrakeShapes::Capsule&>(geometry);
+          gdata.float_data.push_back(static_cast<float>(c.radius));
+          gdata.float_data.push_back(static_cast<float>(c.length));
+          break;
+        }
+        default: {
+          // intentionally do nothing
+          break;
+        }
+      }
+
+      Eigen::Isometry3d T = v.getLocalTransform();
+      Eigen::Map<Eigen::Vector3f> position(gdata.position);
+      position = T.translation().cast<float>();
+      Eigen::Map<Eigen::Vector4f> quaternion(gdata.quaternion);
+      quaternion = drake::math::rotmat2quat(T.rotation()).cast<float>();
+
+      Eigen::Map<Eigen::Vector4f> color(gdata.color);
+      color = v.getMaterial().template cast<float>();
+
+      link.geom.push_back(gdata);
+    }
+    message.link.push_back(link);
+  }
+
+  lcm_->publish("DRAKE_VIEWER_LOAD_ROBOT", &message);
+}
+
+void BotVisualizerSystem::initialize_draw_message() {
+  draw_msg_.num_links = tree_.bodies.size();
+  std::vector<float> position = {0, 0, 0};
+  std::vector<float> quaternion = {0, 0, 0, 1};
+  for (const auto& body : tree_->bodies) {
+    draw_msg_.link_name.push_back(body->get_name());
+    draw_msg_.robot_num.push_back(body->get_model_instance_id());
+    draw_msg_.position.push_back(position);
+    draw_msg_.quaternion.push_back(quaternion);
+  }
+}
+
+}  // namespace systems
+}  // namespace drake
---
+++ drake/systems/bot_visualizer_system.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <lcm/lcm-cpp.hpp>
+
+#include "drake/drakeLCMSystem2_export.h"
+#include "drake/systems/framework/context.h"
+#include "drake/systems/framework/system.h"
+// #include "drake/systems/lcm/lcm_and_vector_interface_translator.h"
+// #include "drake/systems/lcm/lcm_translator_dictionary.h"
+
+namespace drake {
+namespace systems {
+
+/**
+ * This is a Drake System 2.0 block that communicates with the Drake Visualizer
+ * via LCM messages. It does this in two phases: initialization, which runs once
+ * at startup, and run-time, which periodically runs for the duration of the
+ * simulation.
+ *
+ * During initialization, this system block analyzes the RigidBodyTree and
+ * tells Drake Visualizer what it will be visualizing. For example, these
+ * include the number of rigid bodies and their dimensions. The LCM message used
+ * during this phase is called `drake::lcmt_viewer_load_robot` and the channel
+ * name is "DRAKE_VIEWER_LOAD_ROBOT".
+ *
+ * During run-time, this system block takes the current state of the model and
+ * tells Drake Visualizer how to visualize the model. For example, this includes
+ * the position and orientation of each rigid body mentioned during
+ * initialization. The LCM messages used during this phase is
+ * `drake::lcmt_viewer_draw` and the channel name is "DRAKE_VIEWER_DRAW".
+ */
+class DRAKELCMSYSTEM2_EXPORT BotVisualizerSystem : public System<double> {
+ public:
+  /**
+   * A constructor that initializes the Drake visualizer by informing it of all
+   * the bodies within @p tree that need to be visualized.
+   *
+   * @param[in] tree The rigid body tree that's should be visualized. This
+   * reference must remain valid for the lifetime of this object.
+   *
+   * @param[in] lcm A pointer to the LCM subsystem.
+   */
+  BotVisualizerSystem(const RigidBodyTree& tree, ::lcm::LCM* lcm);
+
+
+  ~BotVisualizerSystem() override;
+
+  // Disable copy and assign.
+  BotVisualizerSystem(const BotVisualizerSystem&) = delete;
+  BotVisualizerSystem& operator=(const BotVisualizerSystem&) = delete;
+
+  std::string get_name() const override;
+
+  /**
+   * The default context for this system is one that has one input port and
+   * no state.
+   */
+  std::unique_ptr<ContextBase<double>> CreateDefaultContext() const override;
+
+  /**
+   * The output contains zero ports.
+   */
+  std::unique_ptr<SystemOutput<double>> AllocateOutput(
+      const ContextBase<double>& context) const override;
+
+  /**
+   * Takes the VectorInterface from the input port of the context, computes the
+   * pose of each rigid body in the RigidBodyTree that was provided as an input
+   * argument to the constructor, and publishes draw messages for visualizing
+   * the current state of the model in Drake Visualizer.
+   */
+  void EvalOutput(const ContextBase<double>& context,
+                  SystemOutput<double>* output) const override;
+
+ private:
+
+  // Sends the drake::lcmt_viewer_load_robot messages to the Drake Visualizer
+  // informing it of what needs to be visualized. This is called once by the
+  // constructor.
+  void initialize_drake_visualizer();
+
+  // Initializes member variable draw_msg_.
+  void initialize_draw_message();
+
+  // A const reference to the RigidBodyTree whose rigid bodies are to be
+  // visulized.
+  const RigidBodyTree& tree_;
+
+  // A pointer to the LCM subsystem.
+  ::lcm::LCM* lcm_;
+
+  // The LCM draw message to send to the Drake Visualizer.
+  drake::lcmt_viewer_draw draw_msg_;
+};
+
+}  // namespace systems
+}  // namespace drake
