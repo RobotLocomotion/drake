c3566a215cebd7094e58830380610367c96804cb
David German <david.german@tri.global>
2016-08-22 12:16:44 -0400

    Add cycle breaking based on direct-feedthrough, distinguishing feedback from true algebraic loops.

--- drake/systems/framework/diagram_builder.h
+++ drake/systems/framework/diagram_builder.h
@@ -110,7 +110,10 @@ class DiagramBuilder {
     // Find the systems that have no inputs within the DiagramBuilder.
     std::vector<const System<T>*> nodes_with_in_degree_zero;
     for (const System<T>* system : registered_systems_) {
-      if (dependencies.find(system) == dependencies.end()) {
+      // For the purposes of execution order, a system with no direct
+      // feed-through from input to output is as if it has no inputs.
+      if (dependencies.find(system) == dependencies.end() ||
+          !system->has_any_direct_feedthrough()) {
         nodes_with_in_degree_zero.push_back(system);
       }
     }
@@ -132,9 +135,7 @@ class DiagramBuilder {
     }
 
     if (sorted_systems.size() != systems_.size()) {
-      // TODO(david-german-tri): Attempt to break cycles using
-      // the direct-feedthrough configuration of a System.
-      throw std::logic_error("Cycle detected in DiagramBuilder.");
+      throw std::logic_error("Algebraic loop detected in DiagramBuilder.");
     }
     return sorted_systems;
   }
--- drake/systems/framework/test/diagram_builder_test.cc
+++ drake/systems/framework/test/diagram_builder_test.cc
@@ -5,19 +5,42 @@
 
 #include "drake/systems/framework/diagram.h"
 #include "drake/systems/framework/primitives/adder.h"
+#include "drake/systems/framework/primitives/integrator.h"
 #include "drake/systems/framework/system_port_descriptor.h"
 
 namespace drake {
 namespace systems {
 namespace {
 
-// Tests that an exception is thrown if the diagram contains a cycle.
-GTEST_TEST(DiagramBuilderTest, Cycle) {
+// Tests that an exception is thrown if the diagram contains an algebraic loop.
+GTEST_TEST(DiagramBuilderTest, AlgebraicLoop) {
   DiagramBuilder<double> builder;
   Adder<double> adder(1 /* inputs */, 1 /* length */);
   // Connect the output port to the input port.
   builder.Connect(adder.get_output_port(0), adder.get_input_port(0));
   EXPECT_THROW(builder.Build(), std::logic_error);
+}
+
+// Tests that a cycle which is not an algebraic loop, because one of the
+// components is not direct-feedthrough, can be resolved.
+GTEST_TEST(DiagramBuilderTest, CycleButNoAlgebraicLoop) {
+  DiagramBuilder<double> builder;
+
+  // Create the following diagram:
+  //
+  // input --->| 0       |
+  //           |   Adder +---> Integrator -|
+  //        |->| 1       |                 |---> output
+  //        |------------------------------|
+  Adder<double> adder(2 /* inputs */, 1 /* length */);
+  Integrator<double> integrator(1 /* length */);
+
+  builder.Connect(integrator.get_output_port(0), adder.get_input_port(1));
+  builder.ExportInput(adder.get_input_port(0));
+  builder.ExportOutput(integrator.get_output_port(0));
+
+  // There is no algebraic loop, so we should not throw.
+  EXPECT_NO_THROW(builder.Build());
 }
 
 // Tests that an exception is thrown when building an empty diagram.
