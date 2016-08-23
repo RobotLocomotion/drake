d623fa435ccf3edb21dc6ff7fadc41e75d8434f7
Jeremy Nimmer <jeremy.nimmer@tri.global>
2016-08-22 22:07:08 -0400

    Add new module drake_throw

---
+++ drake/common/drake_throw.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <type_traits>
+
+#include "drake/drakeCommon_export.h"
+
+/// @file
+/// Provides a convenient wrapper to throw an exception when a condition is
+/// unmet.  This is similar to an assertion, but uses exceptions instead of
+/// ::abort(), and cannot be disabled.
+
+namespace drake {
+namespace detail {
+// Throw an error message.
+DRAKECOMMON_EXPORT
+void Throw(const char* condition, const char* func, const char* file, int line);
+}  // namespace detail
+}  // namespace drake
+
+/// Evaluates @p condition and iff the value is false will throw an exception
+/// with a message showing at least the condition text, function name, file,
+/// and line.
+#define DRAKE_THROW_UNLESS(condition)                                   \
+  do {                                                                  \
+    static_assert(                                                      \
+        std::is_convertible<decltype(condition), bool>::value,          \
+        "Throw condition should be bool-convertible.");                 \
+    if (!(condition)) {                                                 \
+      ::drake::detail::Throw(#condition, __func__, __FILE__, __LINE__); \
+    }                                                                   \
+  } while (0)
---
+++ drake/common/test/drake_throw_test.cc
@@ -0,0 +1,15 @@
+#include "drake/common/drake_throw.h"
+
+#include <string>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+namespace {
+
+GTEST_TEST(DrakeThrowTest, BasicTest) {
+  EXPECT_NO_THROW(DRAKE_THROW_UNLESS(true));
+  EXPECT_THROW(DRAKE_THROW_UNLESS(false), std::runtime_error);
+}
+
+}  // namespace
