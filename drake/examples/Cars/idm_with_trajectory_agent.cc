adb0c43642d079dc2e0ba66b810547c5285a1bca
Rick Poyner <rick.poyner@tri.global>
2016-08-25 14:49:54 -0400

    Add draft of idm_with_trajectory_agent block.

---
+++ drake/examples/Cars/idm_with_trajectory_agent.cc
@@ -0,0 +1,12 @@
+#include "drake/examples/Cars/idm_with_trajectory_agent-inl.h"
+
+#include "drake/common/eigen_autodiff_types.h"
+
+namespace drake {
+
+// These instantiations must match the API documentation in
+// idm_with_trajectory_agent.h.
+template class DRAKECARS_EXPORT IdmWithTrajectoryAgent<double>;
+template class DRAKECARS_EXPORT IdmWithTrajectoryAgent<drake::TaylorVarXd>;
+
+}  // namespace drake
