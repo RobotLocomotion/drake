420a7e17503787b4e4bdc6c0e68bd91eda1343c0
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-08-25 10:51:32 +0200

    review comments addressed and small redesign in a test

--- drake/systems/gravity_compensated_system.h
+++ drake/systems/gravity_compensated_system.h
@@ -86,7 +86,7 @@ class GravityCompensatedSystem {
     Eigen::VectorXd vd(num_DoF);
     vd.setZero();
 
-    // The generalised gravity effort is computed by calling inverse dynamics
+    // The generalized gravity effort is computed by calling inverse dynamics
     // with 0 external forces, 0 velocities and 0 accelerations.
     // TODO(naveenoid): Update to use simpler API once issue #3114 is
     // resolved.
