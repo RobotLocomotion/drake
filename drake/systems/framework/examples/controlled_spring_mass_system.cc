62afcef058843b095e74c582d932e560e7ad3eae
amcastro-tri <alejandro.castro@tri.global>
2016-08-27 12:41:56 -0400

    Fills in System<T> methods

--- drake/systems/framework/examples/controlled_spring_mass_system.cc
+++ drake/systems/framework/examples/controlled_spring_mass_system.cc
@@ -12,65 +12,62 @@ template <typename T>
 PidControlledSpringMassSystem<T>::PidControlledSpringMassSystem(
     const T& spring_stiffness, const T& mass,
     const T& Kp, const T& Ki, const T& Kd) : Diagram<T>() {
+  DRAKE_ASSERT(spring_stiffness >= 0);
+  DRAKE_ASSERT(mass >= 0);
   DRAKE_ASSERT(Kp >= 0);
   DRAKE_ASSERT(Ki >= 0);
   DRAKE_ASSERT(Kd >= 0);
-  DRAKE_ASSERT(length > 0);
 
   plant_ = make_unique<SpringMassSystem>(
       spring_stiffness, mass, true /* is forced */);
   controller_ = make_unique<PidController<T>>(Kp, Ki, Kd, 1);
 
+  // A demux is used to split the output from the spring-mass system into two
+  // ports. One port with the mass position and another port with the mass
+  // velocity so that they can be connected to the controller.
+  demux_ = make_unique<Demultiplexer<T>>(2);
 
   DiagramBuilder<T> builder;
   builder.Connect(plant_->get_output_port(0),
-                  controller_->get_error_signal_port());
+                  demux_->get_input_port(0));
 
-  builder.Connect(pass_through_->get_output_port(0),
-                  integrator_->get_input_port(0));
-  builder.Connect(integrator_->get_output_port(0),
-                  integral_gain_->get_input_port(0));
-  builder.Connect(proportional_gain_->get_output_port(0),
-                  adder_->get_input_port(0));
-  builder.Connect(integral_gain_->get_output_port(0),
-                  adder_->get_input_port(1));
-  builder.Connect(derivative_gain_->get_output_port(0),
-                  adder_->get_input_port(2));
-  builder.ExportOutput(adder_->get_output_port(0));
+  // Connects the input error and rate signals to the PID controller.
+  builder.Connect(demux_->get_output_port(0),
+                  controller_->get_error_signal_port());
+  builder.Connect(demux_->get_output_port(1),
+                  controller_->get_error_signal_rate_port());
+
+  // Close the feedback loop.
+  builder.Connect(controller_->get_output_port(0),
+                  plant_->get_input_port(0));
+
+  // The output to this system is the output of the spring-mass system which
+  // consists of a vector with position, velocity and energy.
+  builder.ExportOutput(plant_->get_output_port(0));
   builder.BuildInto(this);
 }
 
 template <typename T>
 bool PidControlledSpringMassSystem<T>::has_any_direct_feedthrough() const {
-  return true;
+  return false;
 }
 
 template <typename T>
-const SystemPortDescriptor<T>& PidControlledSpringMassSystem<T>::get_error_signal_port() const {
-  return Diagram<T>::get_input_port(0);
+void PidControlledSpringMassSystem<T>::SetDefaultState(
+    ContextBase<T>* context) const {
+  ContextBase<T>* controller_context =
+      Diagram<T>::GetMutableSubSystemContext(context, controller_.get());
+  controller_->set_integral_value(controller_context, VectorX<T>::Zero(1));
+
+  ContextBase<T>* plant_context =
+      Diagram<T>::GetMutableSubSystemContext(context, plant_.get());
+  plant_->set_position(plant_context, 0.0);
+  plant_->set_velocity(plant_context, 0.0);
+  plant_->set_conservative_work(plant_context, 0.0);
 }
 
-template <typename T>
-const SystemPortDescriptor<T>&
-PidControlledSpringMassSystem<T>::get_error_signal_rate_port() const {
-  return Diagram<T>::get_input_port(1);
-}
-
-template <typename T>
-void PidControlledSpringMassSystem<T>::SetDefaultState(ContextBase<T>* context) const {
-  const int length = Diagram<T>::get_input_port(0).get_size();
-  set_integral_value(context, VectorX<T>::Zero(length));
-}
-
-template <typename T>
-void PidControlledSpringMassSystem<T>::set_integral_value(
-    ContextBase<T>* context, const Eigen::Ref<const VectorX<T>>& value) const {
-  ContextBase<T>* integrator_context =
-      Diagram<T>::GetMutableSubSystemContext(context, integrator_.get());
-  integrator_->set_integral_value(integrator_context, value);
-}
-
-template class DRAKESYSTEMFRAMEWORK_EXPORT PidControlledSpringMassSystem<double>;
+template class
+DRAKESYSTEMFRAMEWORK_EXPORT PidControlledSpringMassSystem<double>;
 
 }  // namespace systems
 }  // namespace drake
--- drake/systems/framework/examples/controlled_spring_mass_system.h
+++ drake/systems/framework/examples/controlled_spring_mass_system.h
@@ -3,6 +3,7 @@
 #include <memory>
 
 #include "drake/systems/framework/examples/spring_mass_system.h"
+#include "drake/systems/framework/primitives/demultiplexer.h"
 #include "drake/systems/framework/primitives/pid_controller.h"
 
 namespace drake {
@@ -26,34 +27,22 @@ class PidControlledSpringMassSystem : public Diagram<T> {
   /// @param Kp the proportional constant.
   /// @param Ki the integral constant.
   /// @param Kd the derivative constant.
-  /// @param length is the size of the signal to be processed.
-  PidControlledSpringMassSystem(const T& Kp, const T& Ki, const T& Kd, int length);
+  PidControlledSpringMassSystem(const T& spring_stiffness, const T& mass,
+                                const T& Kp, const T& Ki, const T& Kd);
 
   ~PidControlledSpringMassSystem() override {}
 
   // System<T> overrides
   bool has_any_direct_feedthrough() const override;
 
-#if 0
   /// Sets @p context to a default state in which the integral of the
   /// controller is zero.
   void SetDefaultState(ContextBase<T>* context) const;
 
-  /// Sets the integral of the %PidControlledSpringMassSystem to zero.
-  /// @p value must be a column vector of the appropriate size.
-  void set_integral_value(ContextBase<T>* context,
-                          const Eigen::Ref<const VectorX<T>>& value) const;
-
-  /// Returns the input port to the error signal.
-  const SystemPortDescriptor<T>& get_error_signal_port() const;
-
-  /// Returns the input port to the time derivative or rate of the error signal.
-  const SystemPortDescriptor<T>& get_error_signal_rate_port() const;
-#endif
-
  private:
   std::unique_ptr<SpringMassSystem> plant_;
   std::unique_ptr<PidController<T>> controller_;
+  std::unique_ptr<Demultiplexer<T>> demux_;
 };
 
 }  // namespace systems
