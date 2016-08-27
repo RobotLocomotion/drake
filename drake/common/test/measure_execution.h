6c158fc7e3b117b89cd2677436d3bd2980dda422
Jeremy Nimmer <jeremy.nimmer@tri.global>
2016-08-26 21:35:35 -0400

    Rewrite measure::execution, now in common/test The prior version was ripped off from stackoverflow without attribution.

---
+++ drake/common/test/measure_execution.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/// @file
+/// A benchmarking helper.
+
+#include <chrono>
+
+namespace drake {
+namespace common {
+namespace test {
+
+/// Returns the elapsed time of `func(args)`, in seconds.
+template <typename F, typename... Args>
+static double MeasureExecutionTime(F func, Args&&... args) {
+  using clock = std::chrono::steady_clock;
+
+  const clock::time_point start = clock::now();
+  func(std::forward<Args>(args)...);
+  const clock::time_point end = clock::now();
+
+  return std::chrono::duration<double>(end - start).count();
+}
+
+}  // namespace test
+}  // namespace common
+}  // namespace drake
