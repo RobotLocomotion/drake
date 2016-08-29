14dd9d386ba27d048e16d168eeac08961f1fac8a
amcastro-tri <alejandro.castro@tri.global>
2016-08-29 09:57:40 -0400

    Changes to build + shared_ptr to ref

--- drake/systems/framework/primitives/gravity_compensator.cc
+++ drake/systems/framework/primitives/gravity_compensator.cc
@@ -7,16 +7,11 @@
 namespace drake {
 namespace systems {
 
-template class DRAKESYSTEMFRAMEWORK_EXPORT GravityCompensator<double>;
-template class DRAKESYSTEMFRAMEWORK_EXPORT GravityCompensator<AutoDiffXd>;
-
-
 template <typename T>
 GravityCompensator<T>::GravityCompensator(
-    const std::shared_ptr<RigidBodyTree> rigid_body_tree_ptr) :
-    rigid_body_tree_ptr_(rigid_body_tree_ptr) {
+    const RigidBodyTree& world) : mbd_world_(world) {
 
-  int num_DoF  = rigid_body_tree_ptr_->number_of_positions();
+  int num_DoF  = mbd_world_.number_of_positions();
   this->DeclareInputPort(kVectorValued, 2 * num_DoF, kContinuousSampling);
   this->DeclareOutputPort(kVectorValued, num_DoF, kContinuousSampling);
 }
@@ -28,9 +23,9 @@ void GravityCompensator<T>::EvalOutput(const ContextBase<T>& context,
   DRAKE_ASSERT_VOID(System<T>::CheckValidContext(context));
 
   Eigen::VectorXd x = System<T>::get_input_vector(context, 0);
-  int num_DoF = rigid_body_tree_ptr_->number_of_positions();
+  int num_DoF = mbd_world_.number_of_positions();
   KinematicsCache<double> cache =
-      rigid_body_tree_ptr_->doKinematics(
+      mbd_world_.doKinematics(
       x.head(num_DoF), x.tail(num_DoF));
   eigen_aligned_unordered_map<RigidBody const*, drake::TwistVector<double>>
       f_ext;
@@ -38,11 +33,12 @@ void GravityCompensator<T>::EvalOutput(const ContextBase<T>& context,
   Eigen::VectorXd vd(num_DoF);
   vd.setZero();
 
-  Eigen::VectorXd G = rigid_body_tree_ptr_->inverseDynamics(cache, f_ext, vd,
+  Eigen::VectorXd G = mbd_world_.inverseDynamics(cache, f_ext, vd,
                                                             false);
   System<T>::GetMutableOutputVector(output, 0) = G;
-
 }
+
+template class DRAKESYSTEMFRAMEWORK_EXPORT GravityCompensator<double>;
 
 }  // namespace systems
 }  // namespace drake
