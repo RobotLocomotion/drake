2f29ddf1d838992a4d2e259209414fbde70ac209
Chien-Liang Fok <liangfok@gmail.com>
2016-08-28 22:23:27 -0400

    Made LcmPublisherSystem and LcmSubscriberSystem leaf systems. Used new APIs for declaring input and output ports. (#3283)
    * Made LcmPublisherSystem and LcmSubscriberSystem leaf systems. Used new APIs for declaring input and output ports.
    
    * Merge branch 'master' of github.com:RobotLocomotion/drake into feature/update_lcm_system_2_to_be_leaf_system
    
    Conflicts:
    	drake/systems/lcm/lcm_publisher_system.cc
    	drake/systems/lcm/lcm_subscriber_system.cc

--- drake/systems/lcm/lcm_publisher_system.h
+++ drake/systems/lcm/lcm_publisher_system.h
@@ -4,7 +4,7 @@
 
 #include "drake/drakeLCMSystem2_export.h"
 #include "drake/systems/framework/context.h"
-#include "drake/systems/framework/system.h"
+#include "drake/systems/framework/leaf_system.h"
 #include "drake/systems/lcm/lcm_and_vector_base_translator.h"
 #include "drake/systems/lcm/lcm_translator_dictionary.h"
 
@@ -15,7 +15,7 @@ namespace lcm {
 /**
  * Publishes an LCM message containing information from its input port.
  */
-class DRAKELCMSYSTEM2_EXPORT LcmPublisherSystem : public System<double> {
+class DRAKELCMSYSTEM2_EXPORT LcmPublisherSystem : public LeafSystem<double> {
  public:
   /**
    * A constructor.
@@ -55,18 +55,6 @@ class DRAKELCMSYSTEM2_EXPORT LcmPublisherSystem : public System<double> {
   LcmPublisherSystem& operator=(const LcmPublisherSystem&) = delete;
 
   std::string get_name() const override;
-
-  /**
-   * The default context for this system is one that has one input port and
-   * no state.
-   */
-  std::unique_ptr<ContextBase<double>> CreateDefaultContext() const override;
-
-  /**
-   * The output contains zero ports.
-   */
-  std::unique_ptr<SystemOutput<double>> AllocateOutput(
-      const ContextBase<double>& context) const override;
 
   /**
    * Takes the VectorBase from the input port of the context and publishes
