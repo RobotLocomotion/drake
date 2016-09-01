a34aff64d31567c68bd15539904f3f2e00d9bd9a
Bill Hoffman <bill.hoffman@kitware.com>
2016-09-01 13:19:42 -0400

    use correct variable names

--- cmake/modules/3.7/FindMatlab.cmake
+++ cmake/modules/3.7/FindMatlab.cmake
@@ -787,7 +787,7 @@ function(matlab_add_unit_test)
   add_test(NAME ${${prefix}_NAME}
            COMMAND ${CMAKE_COMMAND}
            "-Dtest_name=${${prefix}_NAME}"
-           "-Dadditional_paths=${additional_path_clean}"
+           "-Dadditional_paths=${${prefix}_ADDITIONAL_PATH}}"
            "-Dtest_timeout=${${prefix}_TIMEOUT}"
            "-Doutput_directory=${_matlab_temporary_folder}"
            "-Dworking_directory=${${prefix}_WORKING_DIRECTORY}"
@@ -795,8 +795,8 @@ function(matlab_add_unit_test)
            "-Dno_unittest_framework=${${prefix}_NO_UNITTEST_FRAMEWORK}"
            "-DMatlab_ADDITIONNAL_STARTUP_OPTIONS=${${prefix}_MATLAB_ADDITIONAL_STARTUP_OPTIONS}"
            "-Dunittest_file_to_run=${${prefix}_UNITTEST_FILE}"
-           "-Dcustom_Matlab_test_command=${custom_test_cmd_clean}"
-           "-Dcmd_to_run_before_test=${unittest_precmd_clean}"
+           "-Dcustom_Matlab_test_command=${${prefix}_CUSTOM_TEST_COMMAND}}"
+           "-Dcmd_to_run_before_test=${${prefix}_UNITTEST_PRECOMMAND}"
            -P ${_FindMatlab_SELF_DIR}/MatlabTestsRedirect.cmake
            ${${prefix}_TEST_ARGS}
            ${${prefix}_UNPARSED_ARGUMENTS}
