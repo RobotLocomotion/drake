9136607c00badc104e23241f5413f6851f19c79d
Twan Koolen <koolen.twan@gmail.com>
2016-09-02 16:05:33 -0400

    Add math describing inverse dynamics algorithm and tests.

--- drake/systems/plants/test/rigid_body_tree/rigid_body_tree_inverse_dynamics_test.cc
+++ drake/systems/plants/test/rigid_body_tree/rigid_body_tree_inverse_dynamics_test.cc
@@ -53,7 +53,18 @@ class RigidBodyTreeInverseDynamicsTest : public ::testing::Test {
 // Check that mass matrix derivative minus two times Coriolis matrix is skew
 // symmetric (see e.g. Murray, Li, Sastry. "A Mathematical Introduction to
 // Robotic Manipulation" (1994). Lemma 4.2).
+// http://www.cds.caltech.edu/~murray/books/MLS/pdf/mls94-complete.pdf
+// http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.671
+// .7040&rep=rep1&type=pdf
 // Note: property only holds true if qd = v.
+// The Coriolis matrix is the matrix C(q, qd) when the equations of motion
+// are written as
+//
+//    H(q) qdd + C(q, qd) qd + g(q) = tau
+//
+// with H(q) the mass matrix, g(q) the gravitational terms, and tau the joint
+// torques.
+//
 TEST_F(RigidBodyTreeInverseDynamicsTest, TestSkewSymmetryProperty) {
   int num_velocities = tree_rpy_->number_of_velocities();
 
@@ -105,6 +116,12 @@ TEST_F(RigidBodyTreeInverseDynamicsTest, TestSkewSymmetryProperty) {
 
 // Check that the gradient of the output of inverseDynamics with respect to
 // the joint acceleration vector is the mass matrix.
+// The equations of motion can be written as
+//
+//    H(q) vd + c(q, v) = tau
+//
+// Since inverseDynamics computes tau given q, v, and vd, the gradient of tau
+// with respect to vd should be the mass matrix H(q).
 TEST_F(RigidBodyTreeInverseDynamicsTest, TestAccelerationJacobianIsMassMatrix) {
   std::default_random_engine generator;
 
@@ -137,12 +154,21 @@ TEST_F(RigidBodyTreeInverseDynamicsTest, TestAccelerationJacobianIsMassMatrix) {
   }
 }
 
-// Check that the derivatives of potential energy with respect to q match the
-// vector of generalized gravitational forces for rpy tree.
+// Check that the derivatives of gravitational potential energy with respect
+// to q match the vector of generalized gravitational forces for an rpy tree.
+// When qd = v, the equations of motion can be written as
+//
+//    H(q) qdd + C(q, qd) qd + g(q) = tau
+//
+// When qd = 0 and qdd = 0, tau = g(q), so we can compute the gravitational
+// term using the inverse dynamics algorithm.
+// Now let V(q) denote gravitational potential energy. Then, by definition,
+// g(q) = dV(q)/dq. Assert that that is indeed the case.
 TEST_F(RigidBodyTreeInverseDynamicsTest, TestGeneralizedGravitationalForces) {
   std::default_random_engine generator;
 
-  // Compute vector of generalized gravitational forces.
+  // Compute vector of generalized gravitational forces. Note that
+  // dynamicsBiasTerm simply calls inverseDynamics with vd = 0.
   auto& tree = tree_rpy_;
   auto q = tree->getRandomConfiguration(generator);
   KinematicsCache<double> kinematics_cache(tree->bodies);
@@ -152,7 +178,7 @@ TEST_F(RigidBodyTreeInverseDynamicsTest, TestGeneralizedGravitationalForces) {
   auto gravitational_forces =
       tree->dynamicsBiasTerm(kinematics_cache, f_ext, false);
 
-  // Compute derivatives of potential energy.
+  // Compute the vector gradient of potential energy.
   auto q_autodiff = initializeAutoDiff(q);
   typedef typename decltype(q_autodiff)::Scalar ADScalar;
   KinematicsCache<ADScalar> kinematics_cache_autodiff(tree->bodies);
@@ -172,6 +198,37 @@ TEST_F(RigidBodyTreeInverseDynamicsTest, TestGeneralizedGravitationalForces) {
 
 // Check that momentum rate of change is equal to the sum of all external
 // wrenches exerted upon the mechanism.
+//
+// The total (spatial) momentum of a mechanism can be written as
+//
+//    h(q, v) = A(q) v
+//
+// where A is the momentum matrix (see Orin, Goswami. "Centroidal momentum
+// matrix of a humanoid robot: Structure and properties" (2008)
+// http://www.ambarish
+// .com/paper/20080707_Orin_Goswami_momentum_matrix_IROS2008.pdf
+//
+// The rate of change of momentum, hdot, can be computed using automatic
+// differentiation, given q, v, and vdot.
+//
+// By the Newton-Euler equations, the rate of change of momentum must be
+// equal to the sum of wrenches acting externally on the mechanism:
+//
+//    hdot = W_f + W_g + sum(W_i)
+//
+// where:
+// * W_g is the wrench due to gravity
+// * W_f is the wrench across the floating joint (treating it as having an
+// actuator)
+// * the W_i are other external wrenches acting upon the bodies of the
+// mechanism.
+//
+// Given q, v, vdot, and W_i, the inverse dynamics algorithm can be used to
+// compute the required torques. For a floating mechanism with a
+// QuaternionFloatingJoint, the torque vector associated with the floating
+// joint should exactly be the wrench W_f. All other joint torques are
+// internal to the mechanism.
+// Given hdot, W_f, W_g and the W_i's, assert that Newton-Euler holds.
 TEST_F(RigidBodyTreeInverseDynamicsTest, TestMomentumRateOfChange) {
   std::default_random_engine generator;
   int world_index = 0;
@@ -188,7 +245,9 @@ TEST_F(RigidBodyTreeInverseDynamicsTest, TestMomentumRateOfChange) {
   kinematics_cache.initialize(q, v);
   tree.doKinematics(kinematics_cache, true);
 
-  // Compute gravitational wrench.
+  // Compute gravitational wrench W_g.
+  // Gravitational force can be thought of as acting at center of mass. Change
+  // to world frame.
   auto gravitational_wrench_centroidal = (tree.a_grav * tree.getMass()).eval();
   Eigen::Isometry3d centroidal_to_world;
   centroidal_to_world.setIdentity();
@@ -209,7 +268,7 @@ TEST_F(RigidBodyTreeInverseDynamicsTest, TestMomentumRateOfChange) {
     }
   }
 
-  // Compute wrench across floating joint, add to total wrench.
+  // Compute wrench across floating joint W_f, add to total wrench.
   auto tau = tree.inverseDynamics(kinematics_cache, f_ext, vd);
   auto& floating_body_ptr = tree.bodies[1];
   int floating_joint_start_index =
@@ -222,7 +281,7 @@ TEST_F(RigidBodyTreeInverseDynamicsTest, TestMomentumRateOfChange) {
       transformSpatialForce(body_to_world, floating_joint_wrench_body);
   total_wrench_world += floating_joint_wrench_world;
 
-  // Compute rate of change of momentum.
+  // Compute rate of change of momentum hdot.
   auto identity = MatrixXd::Identity(q.size(), q.size());
   auto v_to_qd =
       kinematics_cache.transformPositionDotMappingToVelocityMapping(identity);
