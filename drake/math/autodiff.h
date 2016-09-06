0a17505ce34e7220a4fba2a8e10922ebdeca611b
Twan Koolen <koolen.twan@gmail.com>
2016-09-06 11:56:56 -0400

    Address comments.

--- drake/math/autodiff.h
+++ drake/math/autodiff.h
@@ -239,48 +239,48 @@ initializeAutoDiffTuple(const Eigen::MatrixBase<Deriveds>&... args) {
   return ret;
 }
 
-/** \brief Computes a matrix of AutoDiffScalars from which both the value and
- * the Jacobian of a function
- * \f[
- * f:\mathbb{R}^{n\times m}\rightarrow\mathbb{R}^{p\times q}
- * \f]
- * can be extracted.
- *
- * The derivative vector for each AutoDiffScalar in the output contains the
- * derivatives with respect to all components of the argument \f$ x \f$.
- *
- * The return type of this function is a matrix with the `best' possible
- * AutoDiffScalar scalar type, in the following sense:
- * * If the number of derivatives can be determined at compile time, the
- *   AutoDiffScalar derivative vector will have that fixed size.
- * * If the maximum number of derivatives can be determined at compile time, the
- *   AutoDiffScalar derivative vector will have that maximum fixed size.
- * * If neither the number, nor the maximum number of derivatives can be
- *   determined at compile time, the output AutoDiffScalar derivative vector
- *   will be dynamically sized.
- *
- * \p f should have a templated call operator that maps an Eigen matrix
- * argument to another Eigen matrix. The scalar type of the output of \f$ f \f$
- * need not match the scalar type of the input (useful in recursive calls to the
- * function to determine higher order derivatives). The easiest way to create an
- * \p f is using a C++14 generic lambda.
- *
- * The algorithm computes the Jacobian in chunks of up to \p MaxChunkSize
- * derivatives at a time. This has three purposes:
- * * It makes it so that derivative vectors can be allocated on the stack,
- *   eliminating dynamic allocations and improving performance if the maximum
- *   number of derivatives cannot be determined at compile time.
- * * It gives control over, and limits the number of required
- *   instantiations of the call operator of f and all the functions it calls.
- * * Excessively large derivative vectors can result in CPU capacity cache
- *   misses; even if the number of derivatives is fixed at compile time, it may
- *   be better to break up into chunks if that means that capacity cache misses
- *   can be prevented.
- *
- * \param f function
- * \param x function argument value at which Jacobian will be evaluated
- * \return AutoDiffScalar matrix corresponding to the Jacobian of f evaluated
- * at x.
+/** Computes a matrix of AutoDiffScalars from which both the value and
+   the Jacobian of a function
+   @f[
+   f:\mathbb{R}^{n\times m}\rightarrow\mathbb{R}^{p\times q}
+   @f]
+   (f: R^n*m -> R^p*q) can be extracted.
+
+   The derivative vector for each AutoDiffScalar in the output contains the
+   derivatives with respect to all components of the argument @f$ x @f$.
+
+   The return type of this function is a matrix with the `best' possible
+   AutoDiffScalar scalar type, in the following sense:
+   - If the number of derivatives can be determined at compile time, the
+     AutoDiffScalar derivative vector will have that fixed size.
+   - If the maximum number of derivatives can be determined at compile time, the
+     AutoDiffScalar derivative vector will have that maximum fixed size.
+   - If neither the number, nor the maximum number of derivatives can be
+     determined at compile time, the output AutoDiffScalar derivative vector
+     will be dynamically sized.
+
+   @p f should have a templated call operator that maps an Eigen matrix
+   argument to another Eigen matrix. The scalar type of the output of @f$ f @f$
+   need not match the scalar type of the input (useful in recursive calls to the
+   function to determine higher order derivatives). The easiest way to create an
+   @p f is using a C++14 generic lambda.
+
+   The algorithm computes the Jacobian in chunks of up to @p MaxChunkSize
+   derivatives at a time. This has three purposes:
+   - It makes it so that derivative vectors can be allocated on the stack,
+     eliminating dynamic allocations and improving performance if the maximum
+     number of derivatives cannot be determined at compile time.
+   - It gives control over, and limits the number of required
+     instantiations of the call operator of f and all the functions it calls.
+   - Excessively large derivative vectors can result in CPU capacity cache
+     misses; even if the number of derivatives is fixed at compile time, it may
+     be better to break up into chunks if that means that capacity cache misses
+     can be prevented.
+
+   @param f function
+   @param x function argument value at which Jacobian will be evaluated
+   @return AutoDiffScalar matrix corresponding to the Jacobian of f evaluated
+   at x.
  */
 template <int MaxChunkSize = 10, class F, class Arg>
 decltype(auto) jacobian(F &&f, Arg &&x) {
@@ -348,13 +348,13 @@ decltype(auto) jacobian(F &&f, Arg &&x) {
 
     // Copy derivatives from chunk to result.
     for (Index i = 0; i < chunk_result.size(); i++) {
-      // intuitive thing to do, but results in problems with non-matching scalar
+      // Intuitive thing to do, but results in problems with non-matching scalar
       // types for recursive jacobian calls:
       // ret(i).derivatives().segment(deriv_num_start, chunk_size) =
       // chunk_result(i).derivatives();
 
-      // instead, assign each element individually, making use of conversion
-      // constructors
+      // Instead, assign each element individually, making use of conversion
+      // constructors.
       for (Index j = 0; j < chunk_size; j++) {
         ret(i).derivatives()(deriv_num_start + j) =
             chunk_result(i).derivatives()(j);
@@ -365,27 +365,27 @@ decltype(auto) jacobian(F &&f, Arg &&x) {
   return ret;
 }
 
-/** \brief Computes a matrix of AutoDiffScalars from which the value, Jacobian,
- * and Hessian of a function
- * \f[
- * f:\mathbb{R}^{n\times m}\rightarrow\mathbb{R}^{p\times q}
- * \f]
- * can be extracted.
- *
- * The output is a matrix of nested AutoDiffScalars, being the result of calling
- * <jacobian>"()" on a function that returns the output of <jacobian>"()",
- * called on \p f.
- *
- * \p MaxChunkSizeOuter and \p MaxChunkSizeInner can be used to control chunk
- * sizes (see <jacobian>"()").
- *
- * See <jacobian>"()" for requirements on the function \p f and the argument
- * \p x.
- *
- * \param f function
- * \param x function argument value at which Hessian will be evaluated
- * \return AutoDiffScalar matrix corresponding to the Hessian of f evaluated at
- * x
+/** Computes a matrix of AutoDiffScalars from which the value, Jacobian,
+   and Hessian of a function
+   @f[
+   f:\mathbb{R}^{n\times m}\rightarrow\mathbb{R}^{p\times q}
+   @f]
+   (f: R^n*m -> R^p*q) can be extracted.
+
+   The output is a matrix of nested AutoDiffScalars, being the result of calling
+   ::jacobian on a function that returns the output of ::jacobian,
+   called on @p f.
+
+   @p MaxChunkSizeOuter and @p MaxChunkSizeInner can be used to control chunk
+   sizes (see <jacobian>"()").
+
+   See ::jacobian for requirements on the function @p f and the argument
+   @p x.
+
+   @param f function
+   @param x function argument value at which Hessian will be evaluated
+   @return AutoDiffScalar matrix corresponding to the Hessian of f evaluated at
+   x
  */
 template <int MaxChunkSizeOuter = 10, int MaxChunkSizeInner = 10, class F,
           class Arg>
