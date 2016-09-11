ada50b32dd54c438800ec23072f00f8ab99368c8
Jeremy Nimmer <jeremy.nimmer@tri.global>
2016-09-11 00:49:16 -0400

    Add eigen_aligned_std_vector

--- drake/common/eigen_stl_types.h
+++ drake/common/eigen_stl_types.h
@@ -7,6 +7,7 @@
 #include <unordered_map>
 
 #include <Eigen/Core>
+#include <Eigen/StdVector>
 
 namespace drake {
 
@@ -16,5 +17,10 @@ template <typename Key, typename T>
 using eigen_aligned_std_unordered_map =
     std::unordered_map<Key, T, std::hash<Key>, std::equal_to<Key>,
                        Eigen::aligned_allocator<std::pair<Key const, T>>>;
+
+/// A std::vector that uses Eigen::aligned_allocator, so that the contained
+/// types may be fixed-size Eigen values.
+template <typename T>
+using eigen_aligned_std_vector = std::vector<T, Eigen::aligned_allocator<T>>;
 
 }  // namespace drake
