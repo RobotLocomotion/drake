bea9b19daa8e410632e1c38f2906c99d217d6692
hongkai-dai <daihongkai@gmail.com>
2016-09-12 09:32:10 -0700

    start from the lorentz cone, instead of the matrix inner product

--- drake/solvers/test/constraint_test.cc
+++ drake/solvers/test/constraint_test.cc
@@ -3,31 +3,40 @@
 #include "gtest/gtest.h"
 
 #include "drake/common/eigen_matrix_compare.h"
-#include "drake/common/eigen_types.h"
+#include "drake/math/autodiff_gradient.h"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using Eigen::Matrix2d;
+using Eigen::Vector2d;
 
 namespace drake {
 namespace solvers {
 namespace {
 
-// Tests that MatrixInnerProductConstraint evaluates correctly
-GTEST_TEST(testConstraint,testMatrixInnerProductConstraint) {
-  // impose the constraint that for a matrix X ,
-  // 1 <= X(0,0) + 2X(0,1) + 3X(1,0) + 4X(1,1) <= 2
-  Matrix2d A;
-  A << 1.0, 2.0, 3.0, 4.0;
-  double lb, ub;
-  lb = 1;
-  ub = 2;
-  MatrixInnerProductConstraint cnstr(A, lb, ub);
-  Matrix2d X;
-  X << 4, 5, 2, 1;
-  drake::Vector1d y;
-  cnstr.Eval(X,y);
-  EXPECT_TRUE(CompareMatrices(y,Vector1d::Constant(24), 1e-10, MatrixCompareType::absolute));
+// Tests if the Lorentz Cone constraint is imposed correctly
+void TestLorentzConeEval(const VectorXd &x_test) {
+  auto cnstr = LorentzConeConstraint();
+  VectorXd y;
+  cnstr.Eval(x_test, y);
+  Vector2d y_expected;
+  y_expected(0) = x_test(0);
+  y_expected(1) = x_test(0)*x_test(0) - x_test.tail(x_test.size()-1).squaredNorm();
+  EXPECT_TRUE(CompareMatrices(y, y_expected, 1E-10, MatrixCompareType::absolute));
+}
+
+GTEST_TEST(testConstraint,testLorentaConeConstraint) {
+// Imposes the Lorentz Cone constraint for a 3 x 1 vector x, that
+// x_1 >= sqrt(x_2^2 + x_3^2)
+  auto cnstr = LorentzConeConstraint();
+  auto lb = cnstr.lower_bound();
+  auto ub = cnstr.upper_bound();
+  EXPECT_TRUE(CompareMatrices(Eigen::Vector2d(0.0, 0.0), lb, 1E-10, MatrixCompareType::absolute));
+  EXPECT_TRUE(CompareMatrices(Eigen::Vector2d::Constant(std::numeric_limits<double>::infinity()), ub, 1e-10, MatrixCompareType::absolute));
+
+  // [3;1;1] satisfy the lorentz cone constraint
+  Eigen::Vector3d x1(3.0, 1.0, 1.0);
+  TestLorentzConeEval(x1);
 }
 } // namespace
 } // namespace solvers
