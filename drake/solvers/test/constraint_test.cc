920d20abf4f2229084265794f9df0c44d0b0828d
hongkai-dai <daihongkai@gmail.com>
2016-09-12 19:32:02 -0700

    WIP: work on lorentz cone

--- drake/solvers/test/constraint_test.cc
+++ drake/solvers/test/constraint_test.cc
@@ -23,6 +23,25 @@ void TestLorentzConeEval(const VectorXd &x_test) {
   y_expected(0) = x_test(0);
   y_expected(1) = x_test(0)*x_test(0) - x_test.tail(x_test.size()-1).squaredNorm();
   EXPECT_TRUE(CompareMatrices(y, y_expected, 1E-10, MatrixCompareType::absolute));
+
+  auto tx = drake::math::initializeAutoDiff(x_test);
+  TaylorVecXd x_taylor = tx;
+  TaylorVecXd y_taylor;
+
+  cnstr.Eval(x_taylor, y_taylor);
+  Eigen::Matrix<double, y.size(), Eigen::Dynamic> y_grad(y.size(), x_test.size());
+  for(int i = 0; i < 2; i++) {
+    for(int j = 0; j < 2; j++) {
+      y_grad(i,j) = static_cast<double>(y_taylor(i).derivatives()(j));
+    }
+  }
+  Eigen::Matrix<double, 2, Eigen::Dynamic> y_grad_expected(2, x_test.size());
+  y_grad_expected.setZero();
+  y_grad_expected(0, 0) = 1.0;
+  y_grad_expected(1, 0) = 2*x_test(0);
+  for(int i = 1;i < x_test.size(); i++) {
+    
+  }
 }
 
 GTEST_TEST(testConstraint,testLorentaConeConstraint) {
