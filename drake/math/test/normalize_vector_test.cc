6f95113bcafc6ac883856ea552f96dfa7021a495
hongkai-dai <daihongkai@gmail.com>
2016-09-13 19:33:48 -0700

    add the C++ test for NormalizeVector

--- drake/math/test/normalize_vector_test.cc
+++ drake/math/test/normalize_vector_test.cc
@@ -1,29 +1,41 @@
-#include "normalize_vector.h"
+#include "drake/math/normalize_vector.h"
 
 #include "gtest/gtest.h"
 
 #include "drake/common/eigen_matrix_compare.h"
-#include "drake/common/eigen_types.h"
+#include "drake/math/autodiff.h"
+#include "drake/math/autodiff_gradient.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
 
 namespace drake {
 namespace math {
 namespace {
 
-void NormalizeVectorTestFun(const Eigen::VecorXd x) {
+void NormalizeVectorTestFun(const Eigen::VectorXd& x) {
+  VectorXd x_normalized(x.size());
+  MatrixXd dx_normalized(x.size(), x.size());
+  MatrixXd ddx_normalized(x.size() * x.size(), x.size());
+  NormalizeVector(x, x_normalized, &dx_normalized, &ddx_normalized);
 
+  // now computes the gradient from autodiff
+  auto x_autodiff = initializeAutoDiff(x);
+  auto x_norm = x_autodiff.norm();
+  auto x_normalized_autodiff = x_autodiff / x_norm;
+
+  EXPECT_TRUE(CompareMatrices(x_normalized,
+                              autoDiffToValueMatrix(x_normalized_autodiff),
+                              1E-10, MatrixCompareType::absolute));
+  EXPECT_TRUE(CompareMatrices(dx_normalized,
+                              autoDiffToGradientMatrix(x_normalized_autodiff),
+                              1E-10, MatrixCompareType::absolute));
 }
+
 GTEST_TEST(NormalizeVectorTest, NormalizeVector) {
-  const int ntests = 1;
-  const int x_rows = 4;
-
-  for (int testnr = 0; testnr < ntests; testnr++) {
-    auto x = Matrix<double, x_rows, 1>::Random().eval();
-    Matrix<double, x_rows, 1> x_norm;
-    Matrix<double, x_rows, x_rows> dx_norm;
-    Matrix<double, x_rows * x_rows, x_rows> ddx_norm;
-    normalizeVec(x, x_norm, &dx_norm, &ddx_norm);
-  }
+  NormalizeVectorTestFun(Eigen::Vector2d(1.0, 0.0));
+  NormalizeVectorTestFun(Eigen::Vector3d(1.0, 2.0, 3.0));
 }
-} // namespace
-} // namespace math
-} // namespace drake
+}  // namespace
+}  // namespace math
+}  // namespace drake
