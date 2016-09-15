4074a5e3c5f8653046d9f01759d580d7b7a87508
hongkai-dai <daihongkai@gmail.com>
2016-09-15 00:01:23 -0700

    WIP: update and add documentation

--- drake/math/normalize_vector.h
+++ drake/math/normalize_vector.h
@@ -7,11 +7,11 @@
 namespace drake {
 namespace math {
 /** Computes the normalized vector, together with its gradient and second derivatives
- * @param x A \p N x 1 vector to be normalized
- * @param x_norm The normalized vector
- * @param dx_norm A \p N x \p N matrix, dx_norm(i,j) is the partial derivative
- * of x_norm(i) w.r.t x(j)
- * @param ddx_norm
+    @param x A \p N x 1 vector to be normalized
+    @param x_norm The normalized vector
+    @param dx_norm A @p N x @p N matrix, @p dx_norm(i,j) is the partial derivative
+    of @p x_norm(i) w.r.t @p x(j)
+    @param ddx_norm
  */
 template <typename Derived>
 void NormalizeVector(
