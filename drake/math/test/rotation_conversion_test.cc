18d8c03a615f6e91889f156526c6ca5e9de9e2a7
hongkai-dai <daihongkai@gmail.com>
2016-09-15 16:12:10 -0700

    WIP: restructure rotation_conversion_test

--- drake/math/test/rotation_conversion_test.cc
+++ drake/math/test/rotation_conversion_test.cc
@@ -26,125 +26,6 @@ using std::cos;
 namespace drake {
 namespace math {
 namespace {
-
-/*
-
-void QuatAxisFun(const Vector4d& quat) {
-  auto a = quat2axis(quat);
-  EXPECT_NEAR(a.head<3>().norm(), 1, 1E-6);
-  auto quat_expected = axis2quat(a);
-  EXPECT_TRUE(CompareMatrices(quat_expected, quat, 1E-10, MatrixCompareType::absolute)
-  || CompareMatrices(quat_expected, -quat, 1E-10, MatrixCompareType::absolute));
-}
-
-GTEST_TEST(RotationConversionTest, QuatAxis) {
-  // First tests the degenerate case, corresponding to no rotation.
-  QuatAxisFun(Vector4d(1.0, 0.0, 0.0, 0.0));
-  QuatAxisFun(Vector4d(-1.0, 0.0, 0.0, 0.0));
-
-  // Second tests another degenerate case, corresponding to 180 degrees of rotation.
-  QuatAxisFun(Vector4d(0.0, 1.0, 0.0, 0.0));
-  QuatAxisFun(Vector4d(0.0, 0.0, 1.0, 0.0));
-  QuatAxisFun(Vector4d(0.0, 0.0, 0.0, 1.0));
-
-  // Tests near-degenerate cases when the angle is close to 0.
-  QuatAxisFun(axis2quat(Vector4d(1, 0, 0, 1E-6*M_PI)));
-  QuatAxisFun(axis2quat(Vector4d(1, 0, 0, -1E-6*M_PI)));
-
-  // Tests near-degenerate cases when the angle is close to 180
-  QuatAxisFun(axis2quat(Vector4d(1, 0, 0, M_PI - 1E-6*M_PI)));
-  QuatAxisFun(axis2quat(Vector4d(0.5, 0.5, 1/sqrt(2), M_PI + 1E-6*M_PI)));
-
-  // Now tests non-degenerate case.
-  QuatAxisFun(Vector4d(0.5, 0.5, 0.5, 0.5));
-  QuatAxisFun(Vector4d(sqrt(2)/2, 0.5, 1/sqrt(6), 1/sqrt(12)));
-}
-
-
-void QuatRotmatFun(const Vector4d& quat) {
-  // Computes the axis-angle representation, then converts to rotation matrix.
-  auto rotmat_expected = axis2rotmat(quat2axis(quat));
-  auto rotmat = quat2rotmat(quat);
-  EXPECT_TRUE(CompareMatrices(rotmat_expected, rotmat, 1E-10, MatrixCompareType::absolute));
-}
-
-GTEST_TEST(RotationConversionTest, QuatRotmat) {
-  // First tests the degenerate case, corresponding to no rotation.
-  QuatRotmatFun(Vector4d(1.0, 0.0, 0.0, 0.0));
-  QuatRotmatFun(Vector4d(-1.0, 0.0, 0.0, 0.0));
-
-  // Now tests non-degenerate cases.
-  QuatRotmatFun(Vector4d(0.5, 0.5, 0.5, 0.5));
-  QuatRotmatFun(Vector4d(1/sqrt(2), 0.4, 1/sqrt(6), 1/sqrt(12)));
-}
-
-void RotmatQuatFun(const Matrix3d &rotmat) {
-  // Knowing that quat2rotmat is correct, I will check if rotmat2quat is the
-  // correct inversion function.
-  auto quat = rotmat2quat(rotmat);
-  auto rotmat_expected = quat2rotmat(quat);
-  EXPECT_TRUE(CompareMatrices(rotmat, rotmat_expected, 1E-10, MatrixCompareType::absolute));
-}
-
-GTEST_TEST(RotationConversionTest, RotmatQuat) {
-  // Tests the degenerate case of no rotation
-  Matrix3d R = Matrix3d::Identity();
-  RotmatQuatFun(R);
-
-  // Tests the degenerate case of 180 degrees rotation.
-  // 180 degrees rotation around z axis
-
-}
-void RotmatAxisFun(const Matrix3d &rotmat) {
-  // Knowing that axis2rotmat is correct, I will check if the rotmat2axis is the
-  // correct inversion function.
-  auto a = rotmat2axis(rotmat);
-  EXPECT_NEAR(a.head<3>().norm(), 1, 1E-6);
-  auto rotmat_expected = axis2rotmat(a);
-  EXPECT_TRUE(CompareMatrices(rotmat, rotmat_expected, 1E-10, MatrixCompareType::absolute));
-  // Computes the angle axis representation by first computing the quaternion,
-  // and then converts the quaternion to angle axis.
-  auto a_expected = quat2axis(rotmat2quat(rotmat));
-  EXPECT_TRUE(CompareMatrices(a, a_expected, 1E-10, MatrixCompareType::absolute)
-    || CompareMatrices(a, -a_expected, 1E-10, MatrixCompareType::absolute));
-}
-
-GTEST_TEST(RotationConversionTest, RotmatAxis) {
-  // Tests the degenerate case of no rotation.
-  Matrix3d R = Matrix3d::Identity();
-  RotmatAxisFun(R);
-
-  // Tests the degenerate case of 180 degrees rotation.
-  // 180 degrees rotation around z axis
-  R.setZero();
-  R(0, 0) = -1;
-  R(1, 1) = -1;
-  R(2, 2) = 1;
-  RotmatAxisFun(R);
-  // 180 degrees rotation around y axis
-  R.setZero();
-  R(0, 0) = -1;
-  R(1, 1) = 1;
-  R(2, 2) = -1;
-  RotmatAxisFun(R);
-  // 180 degrees rotation around z axis
-  R.setZero();
-  R(0, 0) = 1;
-  R(1, 1) = -1;
-  R(2, 2) = -1;
-  RotmatAxisFun(R);
-
-  // Tests the case when the angle is very small, close to degeneration
-  R = axis2rotmat(Vector4d(0.5*sqrt(2), 0.4*sqrt(2), 0.3*sqrt(2), 1E-6*M_PI));
-  RotmatAxisFun(R);
-  R = axis2rotmat(Vector4d(0.5*sqrt(2), 0.4*sqrt(2), 0.3*sqrt(2), -1E-6*M_PI));
-  RotmatAxisFun(R);
-
-  // Tests the non-degenerate case
-  R = axis2rotmat(Vector4d(0.5*sqrt(2), 0.4*sqrt(2), 0.3*sqrt(2), M_PI/6));
-  RotmatAxisFun(R);
-}*/
-
 class RotationConversionTest : public ::testing::Test {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
@@ -192,39 +73,39 @@ class RotationConversionTest : public ::testing::Test {
   }
 
   void initializeQuaternionTestCases() {
-    // Tests conversion from quaternion, tests 9 cases.
-    // 1. Degenerate case, no rotation
-    // 2. Degenerate case, no rotation
-    // 3. Degenerate case, 180 rotation around x axis
-    // 4. Degenerate case, 180 rotation around y axis
-    // 5. Degenerate case, 180 rotation around z axis
-    // 6. Almost degenerate case, small positive rotation angle
-    // 7. Almost degenerate case, small negative rotation angle
-    // 8. Almost degenerate case, close to 180 rotation
-    // 9. Non-degenerate case
+    // Tests conversion from quaternion, tests 9 cases:
+    // 1. Degenerate case, no rotation.
+    // 2. Degenerate case, no rotation.
+    // 3. Degenerate case, 180 rotation around x axis.
+    // 4. Degenerate case, 180 rotation around y axis.
+    // 5. Degenerate case, 180 rotation around z axis.
+    // 6. Almost degenerate case, small positive rotation angle.
+    // 7. Almost degenerate case, small negative rotation angle.
+    // 8. Almost degenerate case, close to 180 rotation.
+    // 9. Non-degenerate case.
 
-    // Case 1. no rotation
+    // Case 1. no rotation.
     q_[0] << 1, 0, 0, 0;
-    // Case 2. no rotation
+    // Case 2. no rotation.
     q_[1] << -1, 0, 0, 0;
-    // Case 3. 180 rotation around x axis
+    // Case 3. 180 rotation around x axis.
     q_[2] << 0, 1, 0, 0;
-    // Case 4. 180 rotation around y axis
+    // Case 4. 180 rotation around y axis.
     q_[3] << 0, 0, 1, 0;
-    // Case 5. 180 rotation around z axis
+    // Case 5. 180 rotation around z axis.
     q_[4] << 0, 0, 0, 1;
-    // Case 6. Almost degenerate case, small positive rotation angle
+    // Case 6. Almost degenerate case, small positive rotation angle.
     q_[5] << cos(1E-6*M_PI), 0.5*sqrt(2)*sin(1E-6*M_PI), 0.4*sqrt(2)*sin(1E-6*M_PI), 0.3*sqrt(2)*sin(1E-6*M_PI);
-    // Case 7. Almost degenerate case, small negative rotation angle
+    // Case 7. Almost degenerate case, small negative rotation angle.
     q_[6] << cos(-1E-6*M_PI), 0.5*sqrt(2)*sin(-1E-6*M_PI), 0.4*sqrt(2)*sin(-1E-6*M_PI), 0.3*sqrt(2)*sin(-1E-6*M_PI);
-    // Case 8. Almost degenerate case, close to 180 degree rotation
+    // Case 8. Almost degenerate case, close to 180 degree rotation.
     q_[7] << cos((1-1E-6)*M_PI), 0.5*sqrt(2)*sin((1-1E-6)*M_PI), 0.4*sqrt(2)*sin((1-1E-6)*M_PI), 0.3*sqrt(2)*sin((1-1E-6)*M_PI);
-    // Case 9. Non-degenerate case
+    // Case 9. Non-degenerate case.
     q_[8] << 1/sqrt(2), 0.5, 1/sqrt(6), 1/sqrt(12);
   }
 
   void initializeRotationMatrixTestCases() {
-    // Tests conversion from rotation matrix, tests at least 7 cases
+    // Tests conversion from rotation matrix, tests 7 cases:
     // 1. Degenerate case, no rotation
     // 2. Degenerate case, 180 degrees rotation around z axis
     // 3. Degenerate case, 180 degrees rotation around y axis
@@ -265,12 +146,25 @@ class RotationConversionTest : public ::testing::Test {
     R_[6] = axis2rotmat(Vector4d(0.5*sqrt(2), 0.4*sqrt(2), 0.3*sqrt(2), M_PI/6));
   }
 
+  void initializeRollPitchYawTestCases() {
+    // Tests conversion from roll pitch yaw angles. Tests cases:
+    // 1. no rotation
+    // 2. Degenerate case, pitch = pi/2;
+    // 3. Non-degenerate case.
+
+    // Case 1, no rotation
+    rpy_[0] <<0, 0, 0;
+    // Case 2, pitch = pi/2;
+    rpy_[1]
+  };
   static const int kNumAxisAngleTestCases = 10;
   static const int kNumQuaternionTestCases = 9;
   static const int kNumRotationMatrixTestCases = 7;
+  static const int kNumRollPitchYawTestCases;
   Vector4d a_[kNumAxisAngleTestCases]; // axis-angle representations to be tested
   Vector4d q_[kNumQuaternionTestCases];  // quaternions to be tested
   Matrix3d R_[kNumRotationMatrixTestCases];  // rotation matrices to be tested
+  Vector3d rpy_[kNumRollPitchYawTestCases]; // roll-pitch-yaw to be tested
 };
 
 TEST_F(RotationConversionTest, AxisQuat) {
@@ -317,27 +211,72 @@ TEST_F(RotationConversionTest, QuatAxis) {
                                MatrixCompareType::absolute));
   }
 }
-/*TEST_F(RotationConversionTest, QuatAxis) {
+
+TEST_F(RotationConversionTest, QuatRotmat) {
   for(int i = 0; i < kNumQuaternionTestCases; i++) {
-    QuatAxisFun(q_[i]);
+    // Computes the axis-angle representation, then converts to rotation matrix.
+    auto rotmat_expected = axis2rotmat(quat2axis(q_[i]));
+    auto rotmat = quat2rotmat(q_[i]);
+    EXPECT_TRUE(CompareMatrices(rotmat_expected, rotmat, 1E-10, MatrixCompareType::absolute));
+    // Compares quat2rotmat(q) and quat2rotmat(-q), the rotation matrices should
+    // be the same.
+    EXPECT_TRUE(CompareMatrices(rotmat, quat2rotmat(-q_[i]), 1E-10, MatrixCompareType::absolute));
   }
 }
 
-TEST_F(RotationConversionTest, QuatRotmat) {
-  Vector4d q_back = rotmat2quat(R_);
-  EXPECT_NEAR(acos(std::abs(q_.transpose() * q_back)), 0.0, 1e-6);
+TEST_F(RotationConversionTest, QuatEigenQuaternion) {
+  for(int i = 0; i < kNumQuaternionTestCases; i++) {
+    Quaterniond eigenQuat = quat2eigenQuaternion(q_[i]);
+    Matrix3d R_expected = quat2rotmat(q_[i]);
+    Matrix3d R_eigen = eigenQuat.matrix();
+    EXPECT_TRUE(
+        CompareMatrices(R_expected,
+                        R_eigen,
+                        1e-6,
+                        MatrixCompareType::absolute));
+  }
+}
+TEST_F(RotationConversionTest, RotmatQuat) {
+  // Knowing that quat2rotmat is correct, I will check if rotmat2quat is the
+  // correct inversion function.
+  for(int i = 0; i < kNumRotationMatrixTestCases; i++) {
+    auto quat = rotmat2quat(R_[i]);
+    auto rotmat_expected = quat2rotmat(quat);
+    EXPECT_TRUE(CompareMatrices(R_[i],
+                                rotmat_expected,
+                                1E-10,
+                                MatrixCompareType::absolute));
+  }
 }
 
+TEST_F(RotationConversionTest, RotmatAxis) {
+  // Knowing that axis2rotmat is correct, I will check if the rotmat2axis is the
+  // correct inversion function.
+  for(int i = 0; i < kNumRotationMatrixTestCases; i++) {
+    auto a = rotmat2axis(R_[i]);
+    EXPECT_NEAR(a.head<3>().norm(), 1, 1E-6);
+    auto rotmat_expected = axis2rotmat(a);
+    EXPECT_TRUE(CompareMatrices(R_[i],
+                                rotmat_expected,
+                                1E-10,
+                                MatrixCompareType::absolute));
+    // Computes the angle axis representation by first computing the quaternion,
+    // and then converts the quaternion to angle axis.
+    auto a_expected = quat2axis(rotmat2quat(R_[i]));
+    EXPECT_TRUE(
+        CompareMatrices(a, a_expected, 1E-10, MatrixCompareType::absolute)
+            || CompareMatrices(a,
+                               -a_expected,
+                               1E-10,
+                               MatrixCompareType::absolute));
+  }
+}
+
+/*
 TEST_F(RotationConversionTest, QuatRPY) {
   Vector3d rpy = quat2rpy(q_);
   Vector4d q_back = rpy2quat(rpy);
   EXPECT_NEAR(acos(std::abs(q_.transpose() * q_back)), 0.0, 1e-6);
-}
-
-TEST_F(RotationConversionTest, RotmatAxis) {
-  Vector4d a = rotmat2axis(R_);
-  Matrix3d R_back = axis2rotmat(a);
-  EXPECT_TRUE(CompareMatrices(R_, R_back, 1e-6, MatrixCompareType::absolute));
 }
 
 TEST_F(RotationConversionTest, RotmatRPY) {
