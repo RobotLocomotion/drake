b17e2c4a64e1223029ea83084a24516368d8a104
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-09-19 15:38:46 +0200

    review comments addressed

--- drake/systems/framework/primitives/gravity_compensator.cc
+++ drake/systems/framework/primitives/gravity_compensator.cc
@@ -10,10 +10,10 @@ namespace systems {
 template <typename T>
 GravityCompensator<T>::GravityCompensator(
     const RigidBodyTree& rigid_body_tree) :
-    rigid_body_tree_(rigid_body_tree) {
+    mdb_world_(rigid_body_tree) {
 
-  int num_of_states  = rigid_body_tree_.number_of_positions() +
-      rigid_body_tree_.number_of_velocities();
+  int num_of_states  = mdb_world_.number_of_positions() +
+      mdb_world_.number_of_velocities();
   this->DeclareInputPort(kVectorValued, num_of_states, kContinuousSampling);
   this->DeclareOutputPort(kVectorValued, num_of_states, kContinuousSampling);
 }
@@ -25,10 +25,10 @@ void GravityCompensator<T>::EvalOutput(const ContextBase<T>& context,
   DRAKE_ASSERT_VOID(System<T>::CheckValidContext(context));
 
   Eigen::VectorXd x = System<T>::get_input_vector(context, 0);
-  int number_of_positions = rigid_body_tree_.number_of_positions();
-  int number_of_velocities = rigid_body_tree_.number_of_velocities();
+  int number_of_positions = mdb_world_.number_of_positions();
+  int number_of_velocities = mdb_world_.number_of_velocities();
   KinematicsCache<double> cache =
-      rigid_body_tree_.doKinematics(x.head(number_of_positions),
+      mdb_world_.doKinematics(x.head(number_of_positions),
                                     x.tail(number_of_velocities));
   eigen_aligned_unordered_map<RigidBody const*, drake::TwistVector<double>>
       f_ext;
@@ -36,7 +36,7 @@ void GravityCompensator<T>::EvalOutput(const ContextBase<T>& context,
   Eigen::VectorXd vd(number_of_velocities);
   vd.setZero();
 
-  Eigen::VectorXd G = rigid_body_tree_.inverseDynamics(cache, f_ext, vd,
+  Eigen::VectorXd G = mdb_world_.inverseDynamics(cache, f_ext, vd,
                                                             false);
   System<T>::GetMutableOutputVector(output, 0) = G;
 
