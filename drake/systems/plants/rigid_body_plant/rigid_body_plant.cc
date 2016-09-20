98a0885da9957bfd2be5f0f22295ae7e8240eed2
amcastro-tri <alejandro.castro@tri.global>
2016-09-20 16:43:32 -0400

    Moves KinematicsResults implementation to cc file

--- drake/systems/plants/rigid_body_plant/rigid_body_plant.cc
+++ drake/systems/plants/rigid_body_plant/rigid_body_plant.cc
@@ -24,6 +24,62 @@ namespace drake {
 namespace systems {
 
 template <typename T>
+KinematicsResults<T>::KinematicsResults(const RigidBodyTree& tree) :
+    tree_(tree), kinematics_cache_(tree_.bodies) {
+}
+
+template <typename T>
+int KinematicsResults<T>::get_num_bodies() const {
+  return tree_.get_number_of_bodies();
+}
+
+template <typename T>
+int KinematicsResults<T>::get_num_positions() const {
+  return kinematics_cache_.getNumPositions();
+}
+
+template <typename T>
+int KinematicsResults<T>::get_num_velocities() const {
+  return kinematics_cache_.getNumVelocities();
+}
+
+template <typename T>
+Quaternion<T> KinematicsResults<T>::get_body_orientation(int body_index) const {
+  Isometry3<T> pose = tree_.relativeTransform(kinematics_cache_, 0, body_index);
+  Vector4<T> quat_vector = drake::math::rotmat2quat(pose.linear());
+  // Note that Eigen quaternion elements are not laid out in memory in the
+  // same way Drake currently aligns them. See issue #3470.
+  return Quaternion<T>(
+      quat_vector[0], quat_vector[1], quat_vector[2], quat_vector[3]);
+}
+
+template <typename T>
+Vector3<T> KinematicsResults<T>::get_body_position(int body_index) const {
+  Isometry3<T> pose = tree_.relativeTransform(kinematics_cache_, 0, body_index);
+  return pose.translation();
+}
+
+template <typename T>
+void KinematicsResults<T>::UpdateFromContext(const Context<T>& context) {
+  // TODO(amcastro-tri): provide nicer accessor to an Eigen representation for
+  // LeafSystems.
+  auto x = dynamic_cast<const BasicVector<T> &>(
+      context.get_state().continuous_state->get_state()).get_value();
+
+  const int nq = tree_.number_of_positions();
+  const int nv = tree_.number_of_velocities();
+
+  // TODO(amcastro-tri): we would like to compile here with `auto` instead of
+  // `VectorX<T>`. However it seems we get some sort of block from a block which
+  // is not instantiated in drakeRBM.
+  VectorX<T> q = x.topRows(nq);
+  VectorX<T> v = x.bottomRows(nv);
+
+  kinematics_cache_.initialize(q, v);
+  tree_.doKinematics(kinematics_cache_, false);
+}
+
+template <typename T>
 RigidBodyPlant<T>::RigidBodyPlant(std::unique_ptr<const RigidBodyTree> tree) :
     tree_(move(tree)) {
   // The input to the system is the generalized forces on the actuators.
@@ -163,42 +219,6 @@ void RigidBodyPlant<T>::EvalOutput(const Context<T>& context,
       output->GetMutableData(1)->
           template GetMutableValue<KinematicsResults<T>>();
   kinematics_results.UpdateFromContext(context);
-
-#if 0
-  // TODO(amcastro-tri): provide nicer accessor to an Eigen representation for
-  // LeafSystems.
-  auto x = dynamic_cast<const BasicVector<T> &>(
-      context.get_state().continuous_state->get_state()).get_value();
-
-  const int nq = get_num_positions();
-  const int nv = get_num_velocities();
-
-  // TODO(amcastro-tri): we would like to compile here with `auto` instead of
-  // `VectorX<T>`. However it seems we get some sort of block from a block which
-  // is not instantiated in drakeRBM.
-  VectorX<T> q = x.topRows(nq);
-  VectorX<T> v = x.bottomRows(nv);
-
-  kinematics_results.initialize(q, v);
-  tree_->doKinematics(kinematics_results, false);
-#endif
-
-#if 0
-  // Evaluates port 1 to be the poses for each rigid body.
-  auto poses_vector =
-      dynamic_cast<VectorOfPoses<T>*>(output->GetMutableVectorData(1));
-  auto cache = InstantiateKinematicsCache(context);
-
-  for (int ibody = 0; ibody < get_num_bodies(); ++ibody) {
-    Isometry3<T> pose = tree_->relativeTransform(cache, 0, ibody);
-    Vector4<T> quat_vector = drake::math::rotmat2quat(pose.linear());
-    Vector3<T> position = pose.translation();
-    poses_vector->set_body_position(ibody, position);
-    poses_vector->set_body_orientation(ibody,
-        Quaternion<T>(
-            quat_vector[0], quat_vector[1], quat_vector[2], quat_vector[3]));
-  }
-#endif
 }
 
 template <typename T>
@@ -437,6 +457,7 @@ void RigidBodyPlant<T>::MapVelocityToConfigurationDerivatives(
 
 // Explicitly instantiates on the most common scalar types.
 template class DRAKE_RBP_EXPORT RigidBodyPlant<double>;
+template class DRAKE_RBP_EXPORT KinematicsResults<double>;
 
 }  // namespace systems
 }  // namespace drake
