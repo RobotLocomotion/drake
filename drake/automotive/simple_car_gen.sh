78bfeff942a49c61d3fa8b2b1c27058d3d6ca909
Jeremy Nimmer <jeremy.nimmer@tri.global>
2016-09-21 12:05:22 -0400

    Turn the SimpleCarConfig into codegen so we get both LCM and non-LCM spellings of it

--- drake/automotive/simple_car_gen.sh
+++ drake/automotive/simple_car_gen.sh
@@ -24,6 +24,7 @@ gen "driving command" steering_angle throttle brake
 gen "euler floating joint state" x y z roll pitch yaw
 gen "idm with trajectory agent state" x_e v_e x_a v_a a_a
 gen "simple car state" x y heading velocity
+gen "simple car config" wheelbase track max_abs_steering_angle max_velocity max_acceleration velocity_lookahead_time velocity_kp
 
 $CLANG_FORMAT --style=file -i \
   $mydir/gen/driving_command.h \
@@ -38,6 +39,10 @@ $CLANG_FORMAT --style=file -i \
   $mydir/gen/idm_with_trajectory_agent_state.cc \
   $mydir/gen/idm_with_trajectory_agent_state_translator.h \
   $mydir/gen/idm_with_trajectory_agent_state_translator.cc \
+  $mydir/gen/simple_car_config.h \
+  $mydir/gen/simple_car_config.cc \
+  $mydir/gen/simple_car_config_translator.h \
+  $mydir/gen/simple_car_config_translator.cc \
   $mydir/gen/simple_car_state.h \
   $mydir/gen/simple_car_state.cc \
   $mydir/gen/simple_car_state_translator.h \
