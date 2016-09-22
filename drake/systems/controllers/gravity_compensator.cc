6fac1d4141793e150156119e37ee8f821d78861b
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-09-22 21:41:39 +0200

    review comments addressed

--- drake/systems/controllers/gravity_compensator.cc
+++ drake/systems/controllers/gravity_compensator.cc
@@ -1,9 +1,8 @@
 #include "./gravity_compensator.h"
 
-#include "drake/systems/plants/RigidBodyTree.h"
 #include "drake/systems/plants/KinematicsCache.h"
+#include "drake/systems/plants/RigidBodyTree.h"
 
-//#include "drake/common/eigen_autodiff_types.h"
 #include "drake/systems/plants/KinematicsCache.h"
 #include "drake/systems/plants/RigidBodySystem.h"
 
@@ -11,11 +10,9 @@ namespace drake {
 namespace systems {
 
 template <typename T>
-GravityCompensator<T>::GravityCompensator(
-    const RigidBodyTree& rigid_body_tree) :
-    mdb_world_(rigid_body_tree) {
-
-  int num_of_inputs  = mdb_world_.number_of_positions();
+GravityCompensator<T>::GravityCompensator(const RigidBodyTree& rigid_body_tree)
+    : mdb_world_(rigid_body_tree) {
+  int num_of_inputs = mdb_world_.number_of_positions();
 
   this->DeclareInputPort(kVectorValued, num_of_inputs, kContinuousSampling);
   this->DeclareOutputPort(kVectorValued, num_of_inputs, kContinuousSampling);
@@ -23,31 +20,28 @@ GravityCompensator<T>::GravityCompensator(
 
 template <typename T>
 void GravityCompensator<T>::EvalOutput(const Context<T>& context,
-                                        SystemOutput<T>* output) const {
+                                       SystemOutput<T>* output) const {
   DRAKE_ASSERT_VOID(System<T>::CheckValidOutput(output));
   DRAKE_ASSERT_VOID(System<T>::CheckValidContext(context));
 
   Eigen::VectorXd x = System<T>::get_input_vector(context, 0);
-  //int number_of_positions = mdb_world_.number_of_positions();
+
   int number_of_velocities = mdb_world_.number_of_velocities();
-  KinematicsCache<double> cache =
-      mdb_world_.doKinematics(x, Eigen::VectorXd::Zero(number_of_velocities));
+
+  Eigen::VectorXd vd = Eigen::VectorXd::Zero(number_of_velocities);
+
+  KinematicsCache<double> cache = mdb_world_.doKinematics(x);
   eigen_aligned_std_unordered_map<RigidBody const*, drake::TwistVector<double>>
       f_ext;
   f_ext.clear();
-  Eigen::VectorXd vd(number_of_velocities);
-  vd.setZero();
 
-  Eigen::VectorXd G = mdb_world_.inverseDynamics(cache, f_ext, vd,
-                                                            false);
+  Eigen::VectorXd G = mdb_world_.inverseDynamics(cache, f_ext, vd, false);
   System<T>::GetMutableOutputVector(output, 0) = G;
-
 }
 
-
-template class GravityCompensator<double>;
+template class DRAKESYSTEM2CONTROLLERS_EXPORT GravityCompensator<double>;
 // TODO(naveenoid) : get the AutoDiff working as in the line below.
-//template class DRAKESYSTEMFRAMEWORK_EXPORT GravityCompensator<AutoDiffXd>;
+// template class DRAKESYSTEMCONTROLLERS_EXPORT GravityCompensator<AutoDiffXd>;
 
 }  // namespace systems
 }  // namespace drake
