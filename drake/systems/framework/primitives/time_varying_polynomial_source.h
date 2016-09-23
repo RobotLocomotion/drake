670c3986af31705a92ee1fff8c268de50562b2b0
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-09-23 17:14:45 +0200

    review comments addressed and test added

--- drake/systems/framework/primitives/time_varying_polynomial_source.h
+++ drake/systems/framework/primitives/time_varying_polynomial_source.h
@@ -1,41 +1,45 @@
-# pragma once
+#pragma once
 
+#include <cstdint>
+#include <memory>
 
-//#include <cstdint>
-//#include <memory>
-//
-//#include "drake/systems/framework/context_base.h"
-//#include "drake/systems/framework/leaf_system.h"
-//#include "drake/systems/framework/system_output.h"
-//#include "drake/systems/trajectories/PiecewisePolynomial.h"
-//
-//namespace drake {
-//namespace systems {
-//
-///// A source block with a which generates the value of  at all times.
-///// @tparam T The vector element type, which must be a valid Eigen scalar.
-/////
-///// Instantiated templates for the following kinds of T's are provided:
-///// - double
-/////
-///// They are already available to link against in libdrakeSystemFramework.
-///// No other values for T are currently supported.
-//template <typename T>
-//class TimeVaryingPolynomialSource : public LeafSystem<T> {
-// public:
-//  /// Constructs a system with a vector output that is constant and equals the
-//  /// supplied @p source_value at all times.
-//  /// @param source_value the constant value of the output so that
-//  /// `y = source_value` at all times.
-//  explicit TimeVaryingPolynomialSource(const PiecewisePolynomialType& pp_traj);
-//
-//  /// Outputs a signal with a fixed value as specified by the user.
-//  void EvalOutput(const ContextBase<T>& context,
-//                  SystemOutput<T>* output) const override;
-//
-// private:
-//   const PiecewisePolynomialType& pp_traj_;
-//};
-//
-//}  // namespace systems
-//}  // namespace drake
+#include "drake/systems/framework/context.h"
+#include "drake/systems/framework/leaf_system.h"
+#include "drake/systems/framework/system_output.h"
+#include "drake/systems/trajectories/PiecewisePolynomial.h"
+
+namespace drake {
+namespace systems {
+
+/// A source block with a which generates the value of a PieceWisePolynomial
+/// at all times.
+///
+/// @tparam T The vector element type, which must be a valid Eigen scalar.
+///
+/// Instantiated templates for the following kinds of T's are provided:
+/// - double
+///
+/// They are already available to link against in libdrakeSystemFramework.
+/// No other values for T are currently supported.
+template <typename T>
+class TimeVaryingPolynomialSource : public LeafSystem<T> {
+ public:
+  /// Constructs a system with a vector output that is time-varying and equals
+  /// the value of the piecewise polynomial evaluated at a each time.
+  /// supplied
+  /// @param pp_traj PiecewisePolynomial used by the system. so that the output
+  /// is `y = pp_traj(t)` at all times.
+  explicit TimeVaryingPolynomialSource(
+      const PiecewisePolynomial<double>& pp_traj);
+
+  /// Outputs a signal with a time-varying polynomial value as specified by the
+  /// user.
+  void EvalOutput(const Context<T>& context,
+                  SystemOutput<T>* output) const override;
+
+ private:
+  const PiecewisePolynomial<double>& pp_traj_;
+};
+
+}  // namespace systems
+}  // namespace drake
