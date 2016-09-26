ecf8cea27264e2bdd937bf1f0099aafa0bca173f
Chien-Liang Fok <liang.fok@tri.global>
2016-09-26 14:05:18 -0400

    Changed drake::ros::WaitForParameter() to use wall clock time instead of ROS simulation time.

--- ros/drake_ros_common/src/parameter_server.cc
+++ ros/drake_ros_common/src/parameter_server.cc
@@ -6,17 +6,19 @@
 namespace drake {
 namespace ros {
 
-typedef std::chrono::system_clock TimeClock;
-typedef std::chrono::duration<double> TimeDuration;
-typedef std::chrono::time_point<TimeClock, TimeDuration> TimePoint;
+typedef std::chrono::high_resolution_clock TimeClock;
+typedef std::chrono::milliseconds TimeDuration;
+typedef std::chrono::time_point<std::chrono::high_resolution_clock> TimePoint;
 
 using std::this_thread::sleep_for;
+using std::chrono::duration_cast;
 
 bool WaitForParameter(const std::string& parameter_name, double max_wait_time) {
-  TimePoint start_wall_time = TimeClock::now();
-  while (::ros::ok() && !::ros::param::has(parameter_name)
-      && (TimeClock::now() - start_wall_time).count() < max_wait_time) {
-    sleep_for(TimeDuration(0.1));  // Sleeps for 1/10 of a second.
+  TimePoint start = TimeClock::now();
+  while (::ros::ok() && !::ros::param::has(parameter_name) &&
+      duration_cast<std::chrono::milliseconds>(TimeClock::now() - start).count()
+      < 1000 * max_wait_time) {
+    sleep_for(TimeDuration(100));  // Sleeps for 100 milliseconds.
   }
   return ::ros::param::has(parameter_name);
 }
