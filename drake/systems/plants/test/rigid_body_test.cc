deec7f0bf076f4ef856eb1980b76a09a51db2f6c
Sean Curtis <sean.curtis@tri.global>
2016-09-26 15:07:03 -0700

    Tests for RigidBody::adjacentTo

--- drake/systems/plants/test/rigid_body_test.cc
+++ drake/systems/plants/test/rigid_body_test.cc
@@ -3,6 +3,8 @@
 #include <gtest/gtest.h>
 
 #include "drake/systems/plants/RigidBody.h"
+#include "drake/systems/plants/joints/FixedJoint.h"
+#include <drake/systems/plants/joints/QuaternionFloatingJoint.h>
 
 namespace drake {
 namespace systems {
@@ -17,6 +19,50 @@ GTEST_TEST(RigidBodyTest, TestGetJointThatIsNotSet) {
   EXPECT_THROW(rigid_body_ptr->getJoint(), std::runtime_error);
 }
 
+// Confirms that the adjacency of bodies is evaluated correctly.  See
+// RigidBody::adjacentTo for details
+GTEST_TEST(RigidBodyTest, TestAdjacency) {
+  // self adjacency
+  std::unique_ptr<RigidBody> rigid_body_ptrA(new RigidBody());
+  EXPECT_FALSE(rigid_body_ptrA->adjacentTo(*rigid_body_ptrA));
+
+  // unconnected rigid bodies
+  std::unique_ptr<RigidBody> rigid_body_ptrB(new RigidBody());
+  EXPECT_FALSE(rigid_body_ptrA->adjacentTo(*rigid_body_ptrB));
+  EXPECT_FALSE(rigid_body_ptrB->adjacentTo(*rigid_body_ptrA));
+
+  // rigid bodies with parent relationship but no link
+  rigid_body_ptrA->set_parent(rigid_body_ptrB.get());
+  EXPECT_TRUE(rigid_body_ptrA->adjacentTo(*rigid_body_ptrB));
+  EXPECT_TRUE(rigid_body_ptrB->adjacentTo(*rigid_body_ptrA));
+
+  Eigen::Isometry3d transform_to_world;
+  std::unique_ptr<DrakeJoint> joint( new FixedJoint("testJoint",
+                                                    transform_to_world));
+  rigid_body_ptrA->setJoint(move(joint));
+
+  // rigid body has joint, but no parent relationship
+  rigid_body_ptrA->set_parent(nullptr);
+  EXPECT_FALSE(rigid_body_ptrA->adjacentTo(*rigid_body_ptrB));
+  EXPECT_FALSE(rigid_body_ptrB->adjacentTo(*rigid_body_ptrA));
+
+  // rigid bodies which *are* properly adjacent
+  rigid_body_ptrA->set_parent(rigid_body_ptrB.get());
+  EXPECT_TRUE(rigid_body_ptrA->adjacentTo(*rigid_body_ptrB));
+  EXPECT_TRUE(rigid_body_ptrB->adjacentTo(*rigid_body_ptrA));
+
+  // connected by floating link
+  std::unique_ptr<RigidBody> rigid_body_ptrC(new RigidBody());
+  std::unique_ptr<RigidBody> rigid_body_ptrD(new RigidBody());
+  rigid_body_ptrC->set_parent(rigid_body_ptrD.get());
+  std::unique_ptr<DrakeJoint> floating_joint(new QuaternionFloatingJoint(
+      "",
+      transform_to_world
+  ));
+  rigid_body_ptrC->setJoint(move(floating_joint));
+  EXPECT_FALSE(rigid_body_ptrC->adjacentTo(*rigid_body_ptrD));
+  EXPECT_FALSE(rigid_body_ptrD->adjacentTo(*rigid_body_ptrC));
+};
 }  // namespace
 }  // namespace test
 }  // namespace plants
