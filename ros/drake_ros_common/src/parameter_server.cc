a44153afd935aa9f12458181797b73ce03f9163e
Chien-Liang Fok <liang.fok@tri.global>
2016-09-26 05:21:43 -0400

    Modified GetRosParameter API to support two versions, one that throws an exception if it fails to obtain the desired parameter, and one that returns a default value.

--- ros/drake_ros_common/src/parameter_server.cc
+++ ros/drake_ros_common/src/parameter_server.cc
@@ -5,15 +5,9 @@ namespace ros {
 
 void WaitForParameter(const std::string& parameter_name, double max_wait_time) {
   ::ros::Time begin_time = ::ros::Time::now();
-
   while (::ros::ok() && !::ros::param::has(parameter_name)
       && (::ros::Time::now() - begin_time).toSec() < max_wait_time) {
-    ::ros::Duration(0.5).sleep();  // Sleeps for half a second.
-  }
-
-  if (!::ros::param::has(parameter_name)) {
-    throw std::runtime_error("ERROR: Failed to obtain parameter \"" +
-        parameter_name + "\" from the ROS parameter server.");
+    ::ros::Duration(0.1).sleep();  // Sleeps for 1/10 of a second.
   }
 }
 
