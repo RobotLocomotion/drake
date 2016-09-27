3dcdd339d23190daffb9d42b44cb646c80d210a2
Chien-Liang Fok <liang.fok@tri.global>
2016-09-27 15:59:02 -0400

    Polished WaitForParameter so boolean logic for terminating while() loop is easier to read.
    Signed-off-by: Chien-Liang Fok <liang.fok@tri.global>

--- ros/drake_ros_common/src/parameter_server.cc
+++ ros/drake_ros_common/src/parameter_server.cc
@@ -6,18 +6,23 @@
 namespace drake {
 namespace ros {
 
-typedef std::chrono::high_resolution_clock TimeClock;
-typedef std::chrono::milliseconds TimeDuration;
-typedef std::chrono::time_point<std::chrono::high_resolution_clock> TimePoint;
-
 using std::this_thread::sleep_for;
 using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::milliseconds;
+using std::chrono::time_point;
+
+typedef high_resolution_clock TimeClock;
+typedef milliseconds TimeDuration;
+typedef time_point<high_resolution_clock> TimePoint;
 
 bool WaitForParameter(const std::string& parameter_name, double max_wait_time) {
   TimePoint start = TimeClock::now();
-  while (::ros::ok() && !::ros::param::has(parameter_name) &&
-      duration_cast<std::chrono::milliseconds>(TimeClock::now() - start).count()
-      < 1000 * max_wait_time) {
+  while (::ros::ok() && !::ros::param::has(parameter_name)) {
+    auto elapsed_time = TimeClock::now() - start;
+    int elapsed_ms = duration_cast<milliseconds>(elapsed_time).count();
+    if (elapsed_ms >= 1000 * max_wait_time)
+      break;
     sleep_for(TimeDuration(100));  // Sleeps for 100 milliseconds.
   }
   return ::ros::param::has(parameter_name);
