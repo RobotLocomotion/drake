48d99930235cc3aefd1894f3a274fc78f066718c
Sean Curtis <sean.curtis@tri.global>
2016-10-03 07:53:09 -0700

    Bring testing classes in line with coding style

--- drake/systems/plants/test/rigid_body_tree/rigid_body_collision_clique_test.cc
+++ drake/systems/plants/test/rigid_body_tree/rigid_body_collision_clique_test.cc
@@ -26,15 +26,15 @@ using std::unique_ptr;
 class RigidBodyTreeCollisionCliqueTest : public ::testing::Test {
  protected:
   void SetUp() override {
-    tree = make_unique<RigidBodyTree>();
+    tree_ = make_unique<RigidBodyTree>();
 
     drake::SquareTwistMatrix<double> I =
         drake::SquareTwistMatrix<double>::Zero();
     I.block(3, 3, 3, 3) << Matrix3d::Identity();
 
     // This body requires a self-collision clique
-    RigidBody * temp_body;
-    tree->add_rigid_body(unique_ptr<RigidBody>(temp_body = new RigidBody()));
+    RigidBody* temp_body;
+    tree_->add_rigid_body(unique_ptr<RigidBody>(temp_body = new RigidBody()));
     temp_body->set_model_name("robot1");
     temp_body->set_name("body1");
     temp_body->set_spatial_inertia(I);
@@ -44,21 +44,21 @@ class RigidBodyTreeCollisionCliqueTest : public ::testing::Test {
     temp_body->AddCollisionElement("default", body1_collision_element_2_.get());
 
     // These next bodies will *not* require self-collision clique
-    tree->add_rigid_body(unique_ptr<RigidBody>(body2_ = new RigidBody()));
+    tree_->add_rigid_body(unique_ptr<RigidBody>(body2_ = new RigidBody()));
     body2_->set_model_name("robot2");
     body2_->set_name("body2");
     body2_->set_spatial_inertia(I);
     body2_collision_element_ = make_unique<Element>();
     body2_->AddCollisionElement("default", body2_collision_element_.get());
 
-    tree->add_rigid_body(unique_ptr<RigidBody>(body3_ = new RigidBody()));
+    tree_->add_rigid_body(unique_ptr<RigidBody>(body3_ = new RigidBody()));
     body3_->set_model_name("robot3");
     body3_->set_name("body3");
     body3_->set_spatial_inertia(I);
   }
 
  protected:
-  std::unique_ptr<RigidBodyTree> tree;
+  std::unique_ptr<RigidBodyTree> tree_;
 
   // Bodies are owned by the tree. These raw pointers allow post-hoc
   // manipulation.
@@ -75,7 +75,7 @@ class RigidBodyTreeCollisionCliqueTest : public ::testing::Test {
 // Confirms that only rigid bodies with multiple collision elements get self-
 // collision cliques.
 TEST_F(RigidBodyTreeCollisionCliqueTest, SelfCollisionClique) {
-  tree->compile();
+  tree_->compile();
   EXPECT_EQ(body1_collision_element_1_->get_num_cliques(), 1);
   EXPECT_EQ(body1_collision_element_2_->get_num_cliques(), 1);
   EXPECT_EQ(body2_collision_element_->get_num_cliques(), 0);
@@ -92,7 +92,7 @@ TEST_F(RigidBodyTreeCollisionCliqueTest, CantCollideClique) {
   body2_->set_parent(body3_);
   body2_->setJoint(move(joint));
 
-  tree->compile();
+  tree_->compile();
 
   // Confirm that collision elements on body 1 are *still* only self-collision
   // cliques, determined by a single clique.
