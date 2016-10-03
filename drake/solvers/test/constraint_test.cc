4b467dfeef2f4db17ea4e690632682f77192de82
hongkai-dai <daihongkai@gmail.com>
2016-10-02 23:15:20 -0700

    Add tests for Lorentz cone. Modify the documentation

--- drake/solvers/test/constraint_test.cc
+++ drake/solvers/test/constraint_test.cc
@@ -16,7 +16,8 @@ namespace solvers {
 namespace {
 
 // Tests if the Lorentz Cone constraint is imposed correctly
-void TestLorentzConeEval(const VectorXd &x_test) {
+template<typename Derived>
+void TestLorentzConeEval(const Derived &x_test, bool is_in_cone) {
   auto cnstr = LorentzConeConstraint();
   VectorXd y;
   cnstr.Eval(x_test, y);
@@ -24,6 +25,9 @@ void TestLorentzConeEval(const VectorXd &x_test) {
   y_expected(0) = x_test(0);
   y_expected(1) = x_test(0)*x_test(0) - x_test.tail(x_test.size()-1).squaredNorm();
   EXPECT_TRUE(CompareMatrices(y, y_expected, 1E-10, MatrixCompareType::absolute));
+
+  bool is_in_cone_expected = (y(0) >= 0) & (y(1) >= 0);
+  EXPECT_TRUE(is_in_cone == is_in_cone_expected);
 
   auto tx = drake::math::initializeAutoDiff(x_test);
   TaylorVecXd x_taylor = tx;
@@ -50,9 +54,21 @@ GTEST_TEST(testConstraint,testLorentaConeConstraint) {
   EXPECT_TRUE(CompareMatrices(Eigen::Vector2d(0.0, 0.0), lb, 1E-10, MatrixCompareType::absolute));
   EXPECT_TRUE(CompareMatrices(Eigen::Vector2d::Constant(std::numeric_limits<double>::infinity()), ub, 1e-10, MatrixCompareType::absolute));
 
-  // [3;1;1] satisfy the lorentz cone constraint
+  // [3;1;1] is in the interior of the Lorentz cone
   Eigen::Vector3d x1(3.0, 1.0, 1.0);
-  TestLorentzConeEval(x1);
+  TestLorentzConeEval(x1, true);
+
+  // [3;2;2;1] is on the boundary of the Lorentz cone
+  Eigen::Vector4d x2(3.0, 2.0, 2.0, 1.0);
+  TestLorentzConeEval(x2, true);
+
+  // [3; 3; 1] is outside of the Lorentz cone
+  Eigen::Vector3d x3(3.0, 3.0, 1.0);
+  TestLorentzConeEval(x3, false);
+
+  // [-3; 1; 1] is outside of the Lorentz cone
+  Eigen::Vector3d x4(-3.0, 1.0, 1.0);
+  TestLorentzConeEval(x4, false);
 }
 } // namespace
 } // namespace solvers
