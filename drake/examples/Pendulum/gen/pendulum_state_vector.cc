e2b1812561b1aef96e8b2534c88d32cc59ec723b
David German <david.german@tri.global>
2016-10-05 14:13:23 -0400

    Generalize lcm_vector_gen, and apply it to Pendulum.

---
+++ drake/examples/Pendulum/gen/pendulum_state_vector.cc
@@ -0,0 +1,16 @@
+#include "drake/examples/Pendulum/gen/pendulum_state_vector.h"
+
+// GENERATED FILE DO NOT EDIT
+// See drake/tools/lcm_vector_gen.py.
+
+namespace drake {
+namespace examples {
+namespace pendulum {
+
+const int PendulumStateVectorIndices::kNumCoordinates;
+const int PendulumStateVectorIndices::kTheta;
+const int PendulumStateVectorIndices::kThetadot;
+
+}  // namespace pendulum
+}  // namespace examples
+}  // namespace drake
