baa344560b4511e6d6b6b1862538f0d57d294194
Soonho Kong <soonho.kong@tri.global>
2016-10-05 16:42:01 -0400

    Fix symbolic classes following META comments in #3669
    See https://reviewable.io/reviews/robotlocomotion/drake/3669

--- drake/common/symbolic_environment.cc
+++ drake/common/symbolic_environment.cc
@@ -2,35 +2,33 @@
 
 #include <iostream>
 #include <sstream>
-
-#include "drake/common/drake_assert.h"
+#include <string>
 
 namespace drake {
 namespace symbolic {
 
 using std::endl;
 using std::ostream;
+using std::string;
 using std::ostringstream;
 
 Environment::Environment(std::initializer_list<value_type> init) : map_(init) {}
 
-void Environment::insert(key_type const & key, mapped_type const & elem) {
+void Environment::insert(const key_type& key, const mapped_type& elem) {
   map_.emplace(key, elem);
 }
 
-ostream& operator<<(ostream& os, symbolic::Environment const& env) {
-  for (auto const& p : env) {
+string Environment::to_string() const {
+  ostringstream oss;
+  oss << *this;
+  return oss.str();
+}
+
+ostream& operator<<(ostream& os, const Environment& env) {
+  for (const auto& p : env) {
     os << p.first << " -> " << p.second << endl;
   }
   return os;
 }
 }  // namespace symbolic
 }  // namespace drake
-
-namespace std {
-string to_string(drake::symbolic::Environment const& env) {
-  ostringstream oss;
-  oss << env;
-  return oss.str();
-}
-}  // namespace std
