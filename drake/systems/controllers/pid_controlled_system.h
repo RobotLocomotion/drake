f1d2099d72d1d664a3287c70613a3306f47d7dbe
Sam Creasey <sam.creasey@tri.global>
2016-10-06 18:35:11 -0400

    Create a PidControlledSystem to simplify use of PidController (which took quite a bit of setup code in the existing examples).

---
+++ drake/systems/controllers/pid_controlled_system.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <memory>
+
+#include "drake/common/drake_export.h"
+#include "drake/systems/framework/diagram.h"
+#include "drake/systems/framework/system.h"
+#include "drake/systems/framework/primitives/adder.h"
+#include "drake/systems/framework/primitives/constant_vector_source.h"
+#include "drake/systems/framework/primitives/demultiplexer.h"
+#include "drake/systems/framework/primitives/gain.h"
+#include "drake/systems/framework/primitives/pid_controller.h"
+
+namespace drake {
+namespace systems {
+
+/// A system which encapsulates a PidController and a controlled
+/// System.
+///
+/// The passed in system must meet the following properties:
+///
+/// * The first input port must be all of the control inputs to the
+/// * system (size N)
+///
+/// * The first output port must be of at least size N*2, where the
+/// * first N elements are the positions of the elements of the
+/// * controlled system, and the second N elements are the velocities.
+/// * Additional elements will be ignored.
+///
+/// @tparam T The vector element type, which must be a valid Eigen scalar.
+///
+/// Instantiated templates for the following kinds of T's are provided:
+/// - double
+/// - AutoDiffXd
+template <typename T>
+class DRAKE_EXPORT PidControlledSystem : public Diagram<T> {
+ public:
+  /// @param[in] system The system to be controlled.
+  /// @param[in] Kp the proportional constant.
+  /// @param[in] Ki the integral constant.
+  /// @param[in] Kd the derivative constant.
+  PidControlledSystem(std::unique_ptr<System<T>> system,
+                      const T& Kp, const T& Ki, const T& Kd);
+  ~PidControlledSystem() override;
+
+  // System<T> overrides
+  bool has_any_direct_feedthrough() const override;
+
+  System<T>* system() { return system_; }
+
+  /// Sets @p context to a default state in which the positions and
+  /// velocities are all zero.  The integral of the controller is also
+  /// set to zero.
+  void SetDefaultState(Context<T>* context) const;
+
+ private:
+  System<T>* system_;
+  PidController<T>* controller_;
+  Demultiplexer<T>* error_demux_;
+  Gain<T>* controller_inverter_;
+  Gain<T>* error_inverter_;
+  Adder<T>* state_minus_target_;
+  Adder<T>* system_input_;
+};
+
+}  // namespace systems
+}  // namespace drake
