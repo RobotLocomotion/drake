b15069270ac12c5fb61ba231546e6f7b3ad163d6
Russ Tedrake <russt@mit.edu>
2016-10-09 15:31:43 -0400

    more cleanup/beautification.

--- drake/examples/simple_continuous_time_system.cc
+++ drake/examples/simple_continuous_time_system.cc
@@ -1,19 +1,23 @@
-
 #include <iostream>
 
 #include "drake/systems/analysis/simulator.h"
 #include "drake/systems/framework/leaf_system.h"
 
+/// Simple Continuous Time System Example
+///
+/// This is meant to be a sort of "hello world" example for the
+/// drake::system classes.  It defines a very simple continous time system,
+/// simulates it from a given initial condition, and plots the result.
+
 namespace drake {
 namespace examples {
 
-/// Simple Continuous Time System Example
+/// Simple Continuous Time System
 /// \begin{gather*}
 ///     \dot{x} = -x + x^3 \\
 ///     y = x
 /// \end{gather*}
-template <typename T>
-class SimpleContinuousTimeSystem : public systems::LeafSystem<T> {
+class SimpleContinuousTimeSystem : public systems::LeafSystem<double> {
  public:
   SimpleContinuousTimeSystem() {
     this->DeclareOutputPort(systems::kVectorValued,
@@ -22,34 +26,28 @@ class SimpleContinuousTimeSystem : public systems::LeafSystem<T> {
   };
   ~SimpleContinuousTimeSystem() override{};
 
-  /// the output does not depend on any of the inputs directly (because there
-  /// are no inputs to this system)
-  bool has_any_direct_feedthrough() const override { return false; }
-
-  /// y = x
-  void EvalOutput(const systems::Context<T>& context,
-                  systems::SystemOutput<T>* output) const override {
-    output->GetMutableVectorData(0)->set_value(
-        context.get_continuous_state()->get_state().CopyToVector());
-  }
-
   // xdot = -x + x^3
   void EvalTimeDerivatives(
-      const systems::Context<T>& context,
-      systems::ContinuousState<T>* derivatives) const override {
-    DRAKE_ASSERT_VOID(systems::System<T>::CheckValidContext(context));
-
-    const T x = context.get_continuous_state()->get_state().GetAtIndex(0);
-    T xdot = -x + std::pow(x, 3.0);
+      const systems::Context<double>& context,
+      systems::ContinuousState<double>* derivatives) const override {
+    double x = context.get_continuous_state()->get_state().GetAtIndex(0);
+    double xdot = -x + std::pow(x, 3.0);
     derivatives->get_mutable_state()->SetAtIndex(0, xdot);
+  }
+
+  /// y = x
+  void EvalOutput(const systems::Context<double>& context,
+                  systems::SystemOutput<double>* output) const override {
+    double x = context.get_continuous_state()->get_state().GetAtIndex(0);
+    output->GetMutableVectorData(0)->SetAtIndex(0, x);
   }
 
  protected:
   // allocate a basic vector of dimension 1
-  virtual std::unique_ptr<systems::ContinuousState<T>> AllocateContinuousState()
-      const override {
-    return std::make_unique<systems::ContinuousState<T>>(
-        std::make_unique<systems::BasicVector<T>>(1));
+  virtual std::unique_ptr<systems::ContinuousState<double>>
+  AllocateContinuousState() const override {
+    return std::make_unique<systems::ContinuousState<double>>(
+        std::make_unique<systems::BasicVector<double>>(1));
   }
 };
 
@@ -58,7 +56,7 @@ class SimpleContinuousTimeSystem : public systems::LeafSystem<T> {
 
 int main(int argc, char* argv[]) {
   // create the simple system
-  auto system = std::make_unique<drake::examples::SimpleContinuousTimeSystem<double>>();
+  auto system = std::make_unique<drake::examples::SimpleContinuousTimeSystem>();
 
   // create the simulator
   drake::systems::Simulator<double> simulator(*system);
