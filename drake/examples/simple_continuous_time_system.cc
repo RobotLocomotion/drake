7b099f887912a80ffaf6a048bf76b0bcb091ba7c
Russ Tedrake <russt@mit.edu>
2016-10-10 17:30:49 -0400

    allocate system on the stack and remove namespaces to make all references explicit

--- drake/examples/simple_continuous_time_system.cc
+++ drake/examples/simple_continuous_time_system.cc
@@ -8,57 +8,52 @@
 #include "drake/systems/analysis/simulator.h"
 #include "drake/systems/framework/leaf_system.h"
 
-namespace drake {
-namespace examples {
-
 /// Simple Continuous Time System
 /// \begin{gather*}
 ///     \dot{x} = -x + x^3 \\
 ///     y = x
 /// \end{gather*}
-class SimpleContinuousTimeSystem : public systems::LeafSystem<double> {
+class SimpleContinuousTimeSystem : public drake::systems::LeafSystem<double> {
  public:
   SimpleContinuousTimeSystem() {
-    this->DeclareOutputPort(systems::kVectorValued,
+    this->DeclareOutputPort(drake::systems::kVectorValued,
                             1,  // dimension of output (y) = 1
-                            systems::kContinuousSampling);
+                            drake::systems::kContinuousSampling);
   }
   ~SimpleContinuousTimeSystem() override{};
 
   // xdot = -x + x^3
   void EvalTimeDerivatives(
-      const systems::Context<double>& context,
-      systems::ContinuousState<double>* derivatives) const override {
+      const drake::systems::Context<double>& context,
+      drake::systems::ContinuousState<double>* derivatives) const override {
     double x = context.get_continuous_state()->get_state().GetAtIndex(0);
     double xdot = -x + std::pow(x, 3.0);
     derivatives->get_mutable_state()->SetAtIndex(0, xdot);
   }
 
   /// y = x
-  void EvalOutput(const systems::Context<double>& context,
-                  systems::SystemOutput<double>* output) const override {
+  void EvalOutput(const drake::systems::Context<double>& context,
+                  drake::systems::SystemOutput<double>* output) const override {
     double x = context.get_continuous_state()->get_state().GetAtIndex(0);
     output->GetMutableVectorData(0)->SetAtIndex(0, x);
   }
 
  protected:
   // allocate a basic vector of dimension 1
-  std::unique_ptr<systems::ContinuousState<double>>
+  std::unique_ptr<drake::systems::ContinuousState<double>>
   AllocateContinuousState() const override {
-    return std::make_unique<systems::ContinuousState<double>>(
-        std::make_unique<systems::BasicVector<double>>(1));
+    return std::make_unique<drake::systems::ContinuousState<double>>(
+        std::make_unique<drake::systems::BasicVector<double>>(1));
   }
 };
 
-}  // namespace examples
-}  // namespace drake
 
 int main(int argc, char* argv[]) {
   // create the simple system
-  auto system = std::make_unique<drake::examples::SimpleContinuousTimeSystem>();
+  SimpleContinuousTimeSystem system;
 
   // create the simulator
-  drake::systems::Simulator<double> simulator(*system);
+  drake::systems::Simulator<double> simulator(system);
 
   // set the initial conditions x(0);
   auto initial_conditions = simulator.get_mutable_context();
