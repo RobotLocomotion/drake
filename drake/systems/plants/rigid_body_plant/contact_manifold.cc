e41e1c38d772c667d33c8ee0be92a26568d1dd6e
Sean Curtis <sean.curtis@tri.global>
2016-10-10 10:42:19 -0700

    Add the base contact manifold
    The base class for all manifolds.

---
+++ drake/systems/plants/rigid_body_plant/contact_manifold.cc
@@ -0,0 +1,9 @@
+#include "drake/systems/plants/rigid_body_plant/contact_manifold.h"
+
+namespace drake {
+namespace systems {
+
+// Explicitly instantiates on the most common scalar types.
+template class ContactManifold<double>;
+}
+}
