1eb0c814a7870aa6af2d40b120128ccbaed6791b
Soonho Kong <soonho.kong@tri.global>
2016-10-10 14:51:45 -0400

    Fix symbolic classes following review comments in #3669 (round3)

--- drake/common/symbolic_environment.cc
+++ drake/common/symbolic_environment.cc
@@ -1,7 +1,9 @@
 #include "drake/common/symbolic_environment.h"
 
+#include <cmath>
 #include <iostream>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 
 namespace drake {
@@ -10,9 +12,19 @@ namespace symbolic {
 using std::endl;
 using std::ostream;
 using std::ostringstream;
+using std::runtime_error;
 using std::string;
 
-Environment::Environment(std::initializer_list<value_type> init) : map_(init) {}
+Environment::Environment(std::initializer_list<value_type> init) : map_(init) {
+  for (const auto& p : init) {
+    if (std::isnan(p.second)) {
+      ostringstream oss;
+      oss << "(" << p.first << ", " << p.second << ")"
+          << " is detected in the initialization of a symbolic environment.";
+      throw runtime_error(oss.str());
+    }
+  }
+}
 
 void Environment::insert(const key_type& key, const mapped_type& elem) {
   map_.emplace(key, elem);
