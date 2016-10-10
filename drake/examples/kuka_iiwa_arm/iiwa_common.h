12ee4b1db11992eb0d792fdd5f5fef126d686ece
Chien-Liang Fok <liang.fok@tri.global>
2016-10-10 13:25:04 -0400

    Updated run_kuka_iiwa_arm_dynamics to use System 2.0.

---
+++ drake/examples/kuka_iiwa_arm/iiwa_common.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <memory>
+
+#include <Eigen/Geometry>
+#include <lcm/lcm-cpp.hpp>
+
+#include "drake/common/drake_export.h"
+#include "drake/systems/plants/RigidBodyTree.h"
+
+namespace drake {
+namespace examples {
+namespace kuka_iiwa_arm {
+
+/**
+ * Adds a ground plane to @p tree.
+ *
+ * @param[in] tree The RigidBodyTree to which to add the ground plane.
+ */
+DRAKE_EXPORT
+void AddGround(RigidBodyTree* tree);
+
+}  // namespace kuka_iiwa_arm
+}  // namespace examples
+}  // namespace drake
