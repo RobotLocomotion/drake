335ee75f3376287f0513c2bc547d58cf370bfbe6
Grant Gould <ggould@tri.global>
2016-10-11 13:30:46 -0400

    Unfactor the joint dof test, to avoid segfaults when indexing the state vector.

--- drake/systems/plants/rigid_body_plant/rigid_body_plant.cc
+++ drake/systems/plants/rigid_body_plant/rigid_body_plant.cc
@@ -222,10 +222,13 @@ void RigidBodyPlant<T>::EvalTimeDerivatives(
     for (auto const& b : tree_->bodies) {
       if (!b->has_parent_body()) continue;
       auto const& joint = b->getJoint();
-      const T limit_force = JointLimitForce(joint,
-                                            q(b->get_position_start_index()),
-                                            v(b->get_velocity_start_index()));
-      right_hand_side(b->get_velocity_start_index()) += limit_force;
+      // Joint limit forces are only implemented for single-axis joints.
+      if (joint.get_num_positions() == 1 && joint.get_num_velocities() == 1) {
+        const T limit_force = JointLimitForce(joint,
+                                              q(b->get_position_start_index()),
+                                              v(b->get_velocity_start_index()));
+        right_hand_side(b->get_velocity_start_index()) += limit_force;
+      }
     }
   }
 
@@ -370,25 +373,22 @@ void RigidBodyPlant<T>::MapVelocityToConfigurationDerivatives(
 template <typename T>
 T RigidBodyPlant<T>::JointLimitForce(const DrakeJoint& joint,
                                      const T& position, const T& velocity) {
-  // Joint limit forces are only implemented for single-axis joints.
-  if (joint.get_num_positions() == 1 && joint.get_num_velocities() == 1) {
-    // Joint stop formula (and definition of "dissipation") from:
-    // https://simtk.org/api_docs/simbody/latest/classSimTK_1_1Force_1_1MobilityLinearStop.html#details
-    const T qmin = joint.getJointLimitMin()(0);
-    const T qmax = joint.getJointLimitMax()(0);
-    const T joint_stiffness = joint.get_joint_limit_stiffness()(0);
-    const T joint_dissipation = joint.get_joint_limit_dissipation()(0);
-    if (position > qmax) {
-      const T violation = position - qmax;
-      const T limit_force = (-joint_stiffness * violation *
-                             (1 + joint_dissipation * velocity));
-      if (limit_force < 0) return limit_force;
-    } else if (position < qmin) {
-      const T violation = position - qmin;
-      const T limit_force = (-joint_stiffness * violation *
-                             (1 - joint_dissipation * velocity));
-      if (limit_force > 0) return limit_force;
-    }
+  // Joint stop formula (and definition of "dissipation") from:
+  // https://simtk.org/api_docs/simbody/latest/classSimTK_1_1Force_1_1MobilityLinearStop.html#details
+  const T qmin = joint.getJointLimitMin()(0);
+  const T qmax = joint.getJointLimitMax()(0);
+  const T joint_stiffness = joint.get_joint_limit_stiffness()(0);
+  const T joint_dissipation = joint.get_joint_limit_dissipation()(0);
+  if (position > qmax) {
+    const T violation = position - qmax;
+    const T limit_force = (-joint_stiffness * violation *
+                           (1 + joint_dissipation * velocity));
+    if (limit_force < 0) return limit_force;
+  } else if (position < qmin) {
+    const T violation = position - qmin;
+    const T limit_force = (-joint_stiffness * violation *
+                           (1 - joint_dissipation * velocity));
+    if (limit_force > 0) return limit_force;
   }
   return 0;
 }
