9bf27bd0bab09c360acc415b02dc7e7fe11d10a4
Sean Curtis <sean.curtis@tri.global>
2016-10-12 13:19:23 -0700

    Make contact detail "copyable"
    It doesn't have an assignment operator but has a clone method and
    copy constructor.

--- drake/systems/plants/rigid_body_plant/contact_detail.h
+++ drake/systems/plants/rigid_body_plant/contact_detail.h
@@ -8,15 +8,17 @@ namespace systems {
 
 /**
   The base class which defines a single point of contact and its corresponding
-  Force (ultimately represented as a wrench).
+  Force (ultimately represented as a wrench)  -- both epxressed in the world
+  frame.
 
-  The point of contact and Force are expressed in the world frame.
+  The intention is for other contact models to create sub-classes that augment
+  a single detail with additional relevant information.
  */
 template <typename T>
 class DRAKE_EXPORT ContactDetail {
  public:
-  /** Default constructor */
-  ContactDetail();
+  /** Copy constructor */
+  ContactDetail(const ContactDetail<T>& other);
 
   /**
    Constructor for a fully specified contact detail: Force and its application
@@ -32,24 +34,32 @@ class DRAKE_EXPORT ContactDetail {
 
   const WrenchVector<T>& get_force() const { return wrench_; };
 
+  virtual ContactDetail* clone() const;
+
  private:
-  /** The point at which the Force is applied -- expressed in the world frame.
-   */
+  /** The point at which the Force is applied - expressed in the world frame. */
   Vector3<T> application_point_{};
 
   /** The contact Force expressed in the world frame. */
   WrenchVector<T> wrench_{};
 };
 
-
 template<typename T>
-ContactDetail<T>::ContactDetail() {}
+ContactDetail<T>::ContactDetail(const ContactDetail<T>& other) :
+  application_point_(other.application_point_),
+  wrench_(other.wrench_)
+{
+}
 
 template<typename T>
 ContactDetail<T>::ContactDetail(const Vector3 <T> &point,
                                 const WrenchVector <T> &wrench)
     : application_point_(point), wrench_(wrench) {}
 
+template<typename T>
+ContactDetail<T>* ContactDetail<T>::clone() const {
+  return new ContactDetail<T>(*this);
+}
 
 extern template class DRAKE_EXPORT ContactDetail<double>;
 }
