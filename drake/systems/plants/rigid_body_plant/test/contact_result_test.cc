bc8fe366bdf1da378eadaf2a84084a6762723d0b
Sean Curtis <sean.curtis@tri.global>
2016-10-12 14:56:06 -0700

    Extend test to include single force in manifold

--- drake/systems/plants/rigid_body_plant/test/contact_result_test.cc
+++ drake/systems/plants/rigid_body_plant/test/contact_result_test.cc
@@ -23,6 +23,13 @@ typedef ContactDetail<double> TestContactDetail;
 typedef Vector3<double> TestVector;
 typedef WrenchVector<double> TestWrench;
 
+template <typename DerivedA, typename DerivedB>
+bool CompareMatrices(const Eigen::MatrixBase<DerivedA>& m1,
+                     const Eigen::MatrixBase<DerivedB>& m2) {
+  return CompareMatrices(m1, m2, 1e-14 /*threshold*/,
+                         MatrixCompareType::absolute);
+}
+
 GTEST_TEST(ContactResultTest, SampleCollisionManifold) {
   TestVector p0 = TestVector::Zero(3, 1);
   TestWrench w0 = TestWrench::Zero(6, 1);
@@ -32,13 +39,24 @@ GTEST_TEST(ContactResultTest, SampleCollisionManifold) {
   EXPECT_EQ(0, manifold.get_num_contacts());
   EXPECT_EQ(nullptr, manifold.get_ith_contact(0));
   auto d_empty = manifold.ComputeNetResponse();
-  EXPECT_TRUE(CompareMatrices(d_empty.get_application_point(),
-                              p0, 1e-14,
-                              MatrixCompareType::absolute));
-  EXPECT_TRUE(CompareMatrices(d_empty.get_force(),
-                              w0, 1e-14,
-                              MatrixCompareType::absolute));
+  EXPECT_TRUE(CompareMatrices(d_empty.get_application_point(), p0));
+  EXPECT_TRUE(CompareMatrices(d_empty.get_force(), w0));
 
+  // Test on single force
+
+  // Zero force
+  auto zeroForce = make_unique<TestContactDetail>(p0, w0);
+  manifold.AddContactDetail(move(zeroForce));
+  EXPECT_EQ(1, manifold.get_num_contacts());
+  auto detail = manifold.get_ith_contact(0);
+  TestVector p = detail->get_application_point();
+  TestWrench w = detail->get_force();
+  EXPECT_TRUE(CompareMatrices(p, p0));
+  EXPECT_TRUE(CompareMatrices(w, w0));
+
+  d_empty = manifold.ComputeNetResponse();
+  EXPECT_TRUE(CompareMatrices(d_empty.get_application_point(), p0));
+  EXPECT_TRUE(CompareMatrices(d_empty.get_force(), w0));
 }
 }  // namespace test
 }  // namespace rigid_body_plant
