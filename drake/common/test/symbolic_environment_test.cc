3403b7ddb003d47959f8f826feeb8af511de38de
Soonho Kong <soonho.kong@tri.global>
2016-10-12 13:45:12 -0400

    Adding test fixtures for testing symbolic classes

--- drake/common/test/symbolic_environment_test.cc
+++ drake/common/test/symbolic_environment_test.cc
@@ -15,30 +15,31 @@ namespace {
 using std::string;
 using std::runtime_error;
 
-GTEST_TEST(SymEnvTest, empty_size) {
-  const Environment env1{};
-  EXPECT_TRUE(env1.empty());
-  EXPECT_EQ(env1.size(), 0u);
-
-  const Variable var_x{"x"};
-  const Variable var_y{"y"};
-  const Variable var_z{"z"};
-  const Environment env2{{var_x, 2}, {var_y, 3}, {var_z, 4}};
-  EXPECT_FALSE(env2.empty());
-  EXPECT_EQ(env2.size(), 3u);
-}
-
-GTEST_TEST(SymEnvTest, init_with_nan) {
-  EXPECT_THROW((Environment{{Variable{"x"}, 10}, {Variable{"y"}, NAN}}),
-               runtime_error);
-}
-
-GTEST_TEST(SymEnvTest, insert_find) {
+// Provides common variables that are used by the following tests.
+class SymbolicEnvironmentTest : public ::testing::Test {
+ protected:
   const Variable var_x{"x"};
   const Variable var_y{"y"};
   const Variable var_z{"z"};
   const Variable var_w{"w"};
   const Variable var_v{"v"};
+};
+
+TEST_F(SymbolicEnvironmentTest, EmptySize) {
+  const Environment env1{};
+  EXPECT_TRUE(env1.empty());
+  EXPECT_EQ(env1.size(), 0u);
+
+  const Environment env2{{var_x, 2}, {var_y, 3}, {var_z, 4}};
+  EXPECT_FALSE(env2.empty());
+  EXPECT_EQ(env2.size(), 3u);
+}
+
+TEST_F(SymbolicEnvironmentTest, InitWithNan) {
+  EXPECT_THROW((Environment{{var_x, 10}, {var_y, NAN}}), runtime_error);
+}
+
+TEST_F(SymbolicEnvironmentTest, insert_find) {
   Environment env1{{var_x, 2}, {var_y, 3}, {var_z, 4}};
   const Environment env2{env1};
 
@@ -57,12 +58,8 @@ GTEST_TEST(SymEnvTest, insert_find) {
   EXPECT_EQ(env1.size(), 5u);
 }
 
-GTEST_TEST(SymEnvTest, output_opreator) {
-  const Variable var_x{"x"};
-  const Variable var_y{"y"};
-  const Variable var_z{"z"};
+TEST_F(SymbolicEnvironmentTest, ToString) {
   const Environment env{{var_x, 2}, {var_y, 3}, {var_z, 3}};
-
   const string out{env.to_string()};
   EXPECT_TRUE(out.find("x -> 2") != string::npos);
   EXPECT_TRUE(out.find("y -> 3") != string::npos);
