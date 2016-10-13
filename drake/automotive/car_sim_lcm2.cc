4118e446578c83ba9f7ab9f86dbdbdbfa0b4f843
Chien-Liang Fok <liang.fok@tri.global>
2016-10-13 13:33:22 -0400

    Added PID controller to car_sim_lcm2.

--- drake/automotive/car_sim_lcm2.cc
+++ drake/automotive/car_sim_lcm2.cc
@@ -8,7 +8,9 @@
 #include "drake/common/drake_path.h"
 // #include "drake/math/roll_pitch_yaw.h"
 #include "drake/lcm/drake_lcm.h"
+
 #include "drake/systems/analysis/simulator.h"
+#include "drake/systems/controllers/pid_controlled_system.h"
 // #include "drake/systems/controllers/gravity_compensator.h"
 #include "drake/systems/framework/diagram.h"
 #include "drake/systems/framework/diagram_builder.h"
@@ -22,6 +24,7 @@
 #include "drake/systems/plants/parser_model_instance_id_table.h"
 #include "drake/systems/plants/rigid_body_plant/rigid_body_plant.h"
 #include "drake/systems/plants/rigid_body_plant/rigid_body_tree_lcm_publisher.h"
+
 
 // TODO(liang.fok) Temporary! Remove once actual input can be provided to
 // RigidBodyPlant!
@@ -54,6 +57,7 @@ using systems::DiagramBuilder;
 // using systems::Gain;
 // using systems::GravityCompensator;
 // using systems::PidController;
+using systems::PidControlledSystem;
 using systems::RigidBodyPlant;
 using systems::RigidBodyTreeLcmPublisher;
 using systems::Simulator;
@@ -63,167 +67,206 @@ using systems::plants::joints::kQuaternion;
 namespace automotive {
 namespace {
 
-// A demo of a single vehicle on a flat terrain.
-template<typename T>
-class CarSimLcm2Demo : public systems::Diagram<T> {
- public:
-  CarSimLcm2Demo() {
-    this->set_name("CarSimLcmDemo");
+// // A demo of a single vehicle on a flat terrain.
+// template<typename T>
+// class CarSimLcm2Demo : public systems::Diagram<T> {
+//  public:
+//   CarSimLcm2Demo() {
+//     this->set_name("CarSimLcmDemo");
 
-    // Instantiates an Multibody Dynamics (MBD) model of the world.
-    auto tree = make_unique<RigidBodyTree>();
-    ModelInstanceIdTable vehicle_instance_id_table =
-        drake::parsers::sdf::AddModelInstancesFromSdfFile(
-            drake::GetDrakePath() +
-            "/automotive/models/prius/prius_with_lidar.sdf",
-            kQuaternion, nullptr /* weld to frame */, tree.get());
+//     // state_minus_target_ = builder.template AddSystem<Adder<T>>
+//     //     (2 /*number of inputs*/, plant_->get_num_states() /* size */);
 
-    AddFlatTerrainToWorld(tree.get());
+//     // // Create and add PID controller.
+//     // // Constants are chosen by trial and error to qualitatively match an
+//     // // experimental run with the same initial conditions and planner.
+//     // // Quantitative comparisons would require torque control and a more careful
+//     // // estimation of the model constants such as friction in the joints.
+//     // const double kp = 2.0;  // proportional constant.
+//     // const double ki = 0.0;  // integral constant.
+//     // const double kd = 1.0;  // derivative constant.
+//     // controller_ = builder.template AddSystem<PidController<T>>(
+//     //     kp, ki, kd, plant_->get_num_positions());
 
-    DiagramBuilder<T> builder;
+//     // gravity_compensator_ = builder.template AddSystem<GravityCompensator<T>>(
+//     //     plant_->get_rigid_body_tree());
+//     // gcomp_minus_pid_ = builder.template AddSystem<Adder<T>>(
+//     //     2 /*number of inputs*/, plant_->get_num_actuators() /* size */);
 
-    // Instantiates a RigidBodyPlant from the MBD model of the world.
-    plant_ = builder.template AddSystem<RigidBodyPlant<T>>(move(tree));
-    plant_->set_contact_parameters(5000.0 /*penetration_stiffness*/,
-      500 /* penetration_damping */, 10 /* friction_coefficient */);
+//     // // Split the input state into two signals one with the positions and one
+//     // // with the velocities.
+//     // // For Kuka:
+//     // // -  get_num_states() = 14
+//     // // -  get_num_positions() = 7
+//     // error_demux_ = builder.template AddSystem<Demultiplexer<T>>(
+//     //     plant_->get_num_states(), plant_->get_num_positions());
+//     // rbp_state_demux_ = builder.template AddSystem<Demultiplexer<T>>(
+//     //     plant_->get_num_states(), plant_->get_num_positions());
 
-    // TODO(liang.fok) Temporary placeholder. Remove when actual inputs can be
-    // wired to RigidBodyPlant.
-    VectorX<T> constant_value(plant_->get_input_size());
-    constant_value.setZero();
-    const_source_ = builder.template AddSystem<ConstantVectorSource<T>>(
-        constant_value);
+//     // controller_inverter_ = builder.template AddSystem<Gain<T>>(
+//     //     -1.0, plant_->get_num_actuators());
+//     // error_inverter_ = builder.template AddSystem<Gain<T>>(
+//     //     -1.0, plant_->get_num_states());
 
-    // state_minus_target_ = builder.template AddSystem<Adder<T>>
-    //     (2 /*number of inputs*/, plant_->get_num_states() /* size */);
+//     // // Creates a plan and wraps it into a source system.
+//     // poly_trajectory_ = MakePlan();
+//     // desired_plan_ = builder.template AddSystem<TimeVaryingPolynomialSource<T>>(
+//     //     *poly_trajectory_);
 
-    // // Create and add PID controller.
-    // // Constants are chosen by trial and error to qualitatively match an
-    // // experimental run with the same initial conditions and planner.
-    // // Quantitative comparisons would require torque control and a more careful
-    // // estimation of the model constants such as friction in the joints.
-    // const double kp = 2.0;  // proportional constant.
-    // const double ki = 0.0;  // integral constant.
-    // const double kd = 1.0;  // derivative constant.
-    // controller_ = builder.template AddSystem<PidController<T>>(
-    //     kp, ki, kd, plant_->get_num_positions());
+//     // Creates and adds LCM publisher for visualization.
+//     viz_publisher_ = builder.template AddSystem<RigidBodyTreeLcmPublisher>(
+//         plant_->get_rigid_body_tree(), &lcm_);
 
-    // gravity_compensator_ = builder.template AddSystem<GravityCompensator<T>>(
-    //     plant_->get_rigid_body_tree());
-    // gcomp_minus_pid_ = builder.template AddSystem<Adder<T>>(
-    //     2 /*number of inputs*/, plant_->get_num_actuators() /* size */);
+//     // // Generates an error signal for the PID controller by subtracting the
+//     // // desired plan state from the RigidBodyPlant's (iiwa arm) state.
+//     // builder.Connect(desired_plan_->get_output_port(0),
+//     //                 error_inverter_->get_input_port());
+//     // builder.Connect(error_inverter_->get_output_port(),
+//     //                 state_minus_target_->get_input_port(0));
+//     // builder.Connect(plant_->get_output_port(0),
+//     //                 state_minus_target_->get_input_port(1));
 
-    // // Split the input state into two signals one with the positions and one
-    // // with the velocities.
-    // // For Kuka:
-    // // -  get_num_states() = 14
-    // // -  get_num_positions() = 7
-    // error_demux_ = builder.template AddSystem<Demultiplexer<T>>(
-    //     plant_->get_num_states(), plant_->get_num_positions());
-    // rbp_state_demux_ = builder.template AddSystem<Demultiplexer<T>>(
-    //     plant_->get_num_states(), plant_->get_num_positions());
+//     // // Splits the error signal into positions and velocities components.
+//     // builder.Connect(state_minus_target_->get_output_port(),
+//     //                 error_demux_->get_input_port(0));
 
-    // controller_inverter_ = builder.template AddSystem<Gain<T>>(
-    //     -1.0, plant_->get_num_actuators());
-    // error_inverter_ = builder.template AddSystem<Gain<T>>(
-    //     -1.0, plant_->get_num_states());
+//     // // Splits the RBP output into positions (q) and velocities (v).
+//     // builder.Connect(plant_->get_output_port(0),
+//     //                 rbp_state_demux_->get_input_port(0));
 
-    // // Creates a plan and wraps it into a source system.
-    // poly_trajectory_ = MakePlan();
-    // desired_plan_ = builder.template AddSystem<TimeVaryingPolynomialSource<T>>(
-    //     *poly_trajectory_);
+//     // // Connects PID controller.
+//     // builder.Connect(error_demux_->get_output_port(0),
+//     //                 controller_->get_error_port());
+//     // builder.Connect(error_demux_->get_output_port(1),
+//     //                 controller_->get_error_derivative_port());
 
-    // Creates and adds LCM publisher for visualization.
-    viz_publisher_ = builder.template AddSystem<RigidBodyTreeLcmPublisher>(
-        plant_->get_rigid_body_tree(), &lcm_);
+//     // // Connects the gravity compensator to the output generalized positions.
+//     // builder.Connect(rbp_state_demux_->get_output_port(0),
+//     //                 gravity_compensator_->get_input_port(0));
+//     // builder.Connect(gravity_compensator_->get_output_port(0),
+//     //                 gcomp_minus_pid_->get_input_port(0));
 
-    // // Generates an error signal for the PID controller by subtracting the
-    // // desired plan state from the RigidBodyPlant's (iiwa arm) state.
-    // builder.Connect(desired_plan_->get_output_port(0),
-    //                 error_inverter_->get_input_port());
-    // builder.Connect(error_inverter_->get_output_port(),
-    //                 state_minus_target_->get_input_port(0));
-    // builder.Connect(plant_->get_output_port(0),
-    //                 state_minus_target_->get_input_port(1));
+//     // // Adds feedback.
+//     // builder.Connect(controller_->get_output_port(0),
+//     //                 controller_inverter_->get_input_port());
+//     // builder.Connect(controller_inverter_->get_output_port(),
+//     //                 gcomp_minus_pid_->get_input_port(1));
 
-    // // Splits the error signal into positions and velocities components.
-    // builder.Connect(state_minus_target_->get_output_port(),
-    //                 error_demux_->get_input_port(0));
+//     // builder.Connect(gcomp_minus_pid_->get_output_port(),
+//     //                 plant_->get_input_port(0));
 
-    // // Splits the RBP output into positions (q) and velocities (v).
-    // builder.Connect(plant_->get_output_port(0),
-    //                 rbp_state_demux_->get_input_port(0));
+//     // TODO(liang.fok) Remove this once actual inputs an be provided to
+//     // RigidBodyPlant.
+//     builder.Connect(const_source_->get_output_port(),
+//                     plant_->get_input_port(0));
 
-    // // Connects PID controller.
-    // builder.Connect(error_demux_->get_output_port(0),
-    //                 controller_->get_error_port());
-    // builder.Connect(error_demux_->get_output_port(1),
-    //                 controller_->get_error_derivative_port());
+//     // Connects to publisher for visualization.
+//     builder.Connect(plant_->get_output_port(0),
+//                     viz_publisher_->get_input_port(0));
 
-    // // Connects the gravity compensator to the output generalized positions.
-    // builder.Connect(rbp_state_demux_->get_output_port(0),
-    //                 gravity_compensator_->get_input_port(0));
-    // builder.Connect(gravity_compensator_->get_output_port(0),
-    //                 gcomp_minus_pid_->get_input_port(0));
+//     builder.ExportOutput(plant_->get_output_port(0));
+//     builder.BuildInto(this);
+//   }
 
-    // // Adds feedback.
-    // builder.Connect(controller_->get_output_port(0),
-    //                 controller_inverter_->get_input_port());
-    // builder.Connect(controller_inverter_->get_output_port(),
-    //                 gcomp_minus_pid_->get_input_port(1));
+//   // const RigidBodyPlant<T>& get_kuka_plant() const { return *plant_; }
 
-    // builder.Connect(gcomp_minus_pid_->get_output_port(),
-    //                 plant_->get_input_port(0));
+//   void SetDefaultState(Context<T>* context) const {
+//     // Context<T>* controller_context =
+//     //     this->GetMutableSubsystemContext(context, controller_);
+//     // controller_->set_integral_value(controller_context, VectorX<T>::Zero(7));
 
-    // TODO(liang.fok) Remove this once actual inputs an be provided to
-    // RigidBodyPlant.
-    builder.Connect(const_source_->get_output_port(),
-                    plant_->get_input_port(0));
+//     Context<T>* plant_context =
+//         this->GetMutableSubsystemContext(context, plant_);
+//     plant_->SetZeroConfiguration(plant_context);
+//   }
 
-    // Connects to publisher for visualization.
-    builder.Connect(plant_->get_output_port(0),
-                    viz_publisher_->get_input_port(0));
+//  private:
+//   RigidBodyPlant<T>* plant_;
+//   // PidController<T>* controller_;
+//   // Demultiplexer<T>* error_demux_;
+//   // Demultiplexer<T>* rbp_state_demux_;
+//   // Gain<T>* controller_inverter_;
+//   // Gain<T>* error_inverter_;
+//   // GravityCompensator<T>* gravity_compensator_;
+//   // Adder<T>* state_minus_target_;
+//   // Adder<T>* gcomp_minus_pid_;
+//   // TimeVaryingPolynomialSource<T>* desired_plan_;
+//   // std::unique_ptr<PiecewisePolynomial<T>> poly_trajectory_;
+//   RigidBodyTreeLcmPublisher* viz_publisher_;
+//   DrakeLcm lcm_;
 
-    builder.ExportOutput(plant_->get_output_port(0));
-    builder.BuildInto(this);
-  }
-
-  // const RigidBodyPlant<T>& get_kuka_plant() const { return *plant_; }
-
-  void SetDefaultState(Context<T>* context) const {
-    // Context<T>* controller_context =
-    //     this->GetMutableSubsystemContext(context, controller_);
-    // controller_->set_integral_value(controller_context, VectorX<T>::Zero(7));
-
-    Context<T>* plant_context =
-        this->GetMutableSubsystemContext(context, plant_);
-    plant_->SetZeroConfiguration(plant_context);
-  }
-
- private:
-  RigidBodyPlant<T>* plant_;
-  // PidController<T>* controller_;
-  // Demultiplexer<T>* error_demux_;
-  // Demultiplexer<T>* rbp_state_demux_;
-  // Gain<T>* controller_inverter_;
-  // Gain<T>* error_inverter_;
-  // GravityCompensator<T>* gravity_compensator_;
-  // Adder<T>* state_minus_target_;
-  // Adder<T>* gcomp_minus_pid_;
-  // TimeVaryingPolynomialSource<T>* desired_plan_;
-  // std::unique_ptr<PiecewisePolynomial<T>> poly_trajectory_;
-  RigidBodyTreeLcmPublisher* viz_publisher_;
-  DrakeLcm lcm_;
-
-  ConstantVectorSource<T>* const_source_;
-};
+//   ConstantVectorSource<T>* const_source_;
+// };
 
 int main() {
-  CarSimLcm2Demo<double> model;
-  Simulator<double> simulator(model);
+  DiagramBuilder<double> builder;
+
+  // Instantiates an Multibody Dynamics (MBD) model of the world.
+  auto tree = make_unique<RigidBodyTree>();
+  ModelInstanceIdTable vehicle_instance_id_table = AddModelInstancesFromSdfFile(
+      drake::GetDrakePath() + "/automotive/models/prius/prius_with_lidar.sdf",
+      kQuaternion, nullptr /* weld to frame */, tree.get());
+
+  AddFlatTerrainToWorld(tree.get());
+
+  // Instantiates a system for visualizing the MBD model.
+  lcm::DrakeLcm lcm;
+  auto publisher = builder.AddSystem<RigidBodyTreeLcmPublisher>(*tree, &lcm);
+
+  // Instantiates a RigidBodyPlant based on the MBD model.
+  auto plant = make_unique<RigidBodyPlant<double>>(move(tree));
+  plant->set_contact_parameters(5000.0 /* penetration_stiffness */,
+      500 /* penetration_damping */, 10 /* friction_coefficient */);
+
+  // Instantiates a Pid controller that controls the RigidBodyPlant.
+  auto controller = builder.AddSystem<systems::PidControlledSystem>(
+      std::move(plant), 0. /* Kp */, 0. /* Ki */, 0. /* Kd */);
+
+  // std::cout << "Size of controller's input port 0: "
+  //           << controller->get_input_port(0).get_size() << std::endl;
+  // std::cout << "Size of controller's input port 1: "
+  //           << controller->get_input_port(1).get_size() << std::endl;
+
+  // TODO(liang.fok) Temporary placeholder. Remove when actual inputs can be
+  // wired to the controller.
+  VectorX<double> constant_value1(controller->get_input_port(0).get_size());
+  constant_value1.setZero();
+  auto constant_source1 =
+      builder.template AddSystem<ConstantVectorSource<double>>(constant_value1);
+
+  VectorX<double> constant_value2(controller->get_input_port(1).get_size());
+  constant_value2.setZero();
+  auto constant_source2 =
+      builder.template AddSystem<ConstantVectorSource<double>>(constant_value2);
+
+  // Connects the feedforward control signal (all zeros).
+  builder.Connect(constant_source1->get_output_port(),
+                  controller->get_input_port(0));
+
+  // Connects the desired state of the system.
+  builder.Connect(constant_source2->get_output_port(),
+                  controller->get_input_port(1));
+  builder.Connect(controller->get_output_port(0),
+                  publisher->get_input_port(0));
+
+  auto diagram = builder.Build();
+
+  Simulator<double> simulator(*diagram);
 
   // Zeroes the state and initializes controller state.
-  model.SetDefaultState(simulator.get_mutable_context());
+  systems::Context<double>* controller_context =
+      diagram->GetMutableSubsystemContext(
+          simulator.get_mutable_context(), controller);
+  controller->SetDefaultState(controller_context);
+
+  // Zeroes the rigid body plant's state. This is necessary because it is by
+  // default initialized to a vector a NaN values.
+  RigidBodyPlant<double>* rigid_body_plant =
+      dynamic_cast<RigidBodyPlant<double>*>(controller->system());
+  Context<double>* plant_context =
+      controller->GetMutableSubsystemContext(controller_context,
+                                             rigid_body_plant);
+  rigid_body_plant->SetZeroConfiguration(plant_context);
 
   // VectorX<double> desired_state = VectorX<double>::Zero(14);
   // model.get_kuka_plant().set_state_vector(
