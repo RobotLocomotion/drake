2177ae240d1e6c7615068da85374ca169b4da79d
Sean Curtis <sean.curtis@tri.global>
2016-10-13 15:40:37 -0700

    Update contact data
    1) ContactResult: Make get ith info const -- so it can be called in a
       const context.
    2) Actually implement ContactResult::AddContactInfo
    3) Modify ContactManifold handle unique_ptr properly
    4) Extend contact info constructor to include manifold

--- drake/systems/plants/rigid_body_plant/contact_results.cc
+++ drake/systems/plants/rigid_body_plant/contact_results.cc
@@ -1,16 +1,21 @@
 #include "drake/systems/plants/rigid_body_plant/contact_results.h"
 
 #include "drake/common/drake_assert.h"
+#include "drake/systems/plants/rigid_body_plant/sampled_contact_manifold.h"
+
+using std::make_unique;
+using std::move;
 
 namespace drake {
 namespace systems {
+
 
 template <typename T>
 size_t ContactResults<T>::get_num_contacts() const { return contacts_.size(); }
 
 template <typename T>
-const ContactInfo<T>& ContactResults<T>::get_contact_info(size_t i) {
-    DRAKE_ASSERT(i < contacts_.size());
+const ContactInfo<T>& ContactResults<T>::get_contact_info(size_t i) const {
+  DRAKE_ASSERT(i < contacts_.size());
   return contacts_[i];
 }
 
@@ -24,8 +29,13 @@ void ContactResults<T>::Clear() {
 
 template <typename T>
 void ContactResults<T>::AddContact( DrakeCollision::ElementId elementA,
-                 DrakeCollision::ElementId elemetnB,
-                 const Vector3<T> & point, const WrenchVector<T> & force) {}
+                 DrakeCollision::ElementId elementB,
+                 const Vector3<T> & point, const WrenchVector<T> & force) {
+  auto manifold = std::make_unique<SampledContactManifold<T>>();
+  auto detail = make_unique<ContactDetail<T>>(point, force);
+  manifold->AddContactDetail(detail);
+  contacts_.emplace_back(elementA, elementB, move(manifold));
+}
 
 
 // explicit instantiation
