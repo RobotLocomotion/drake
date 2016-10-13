fb1f90ef37d4622d638b278ed3651778255a1a6d
Sean Curtis <sean.curtis@tri.global>
2016-10-13 15:40:37 -0700

    Update contact data
    1) ContactResult: Make get ith info const -- so it can be called in a
       const context.
    2) Actually implement ContactResult::AddContactInfo
    3) Modify ContactManifold handle unique_ptr properly
    4) Extend contact info constructor to include manifold

--- drake/systems/plants/rigid_body_plant/contact_info.cc
+++ drake/systems/plants/rigid_body_plant/contact_info.cc
@@ -1,12 +1,17 @@
 #include "drake/systems/plants/rigid_body_plant/contact_info.h"
 
+using std::move;
+
 namespace drake {
 namespace systems {
 
+
 template <typename T>
 ContactInfo<T>::ContactInfo(DrakeCollision::ElementId element1,
-                            DrakeCollision::ElementId element2) :
-    element1_(element1), element2_(element2) {}
+                            DrakeCollision::ElementId element2,
+                            std::unique_ptr<ContactManifold<T>> manifold) :
+    element1_(element1), element2_(element2), contact_manifold_(move(manifold))
+{}
 
 template <typename T>
 ContactInfo<T>::ContactInfo(const ContactInfo<T>& other) :
