93e4619164609132db023bcd16be99fb669c07a2
acsatici <acsatici@gmail.com>
2016-10-12 20:02:34 -0400

    CLQR working and the new hand-tuned controller

--- drake/examples/ChineseYoYo/testSoftPaddleConstrainedLQR.m
+++ drake/examples/ChineseYoYo/testSoftPaddleConstrainedLQR.m
@@ -27,19 +27,20 @@ utrajIC = utraj; %unneccessary
 utrajIC = setOutputFrame(utrajIC, getInputFrame(plant)); %unneccessary
 
 % run lqr
-xdim = p.in_contact.num_positions+ p.in_contact.num_velocities;
-qw = [0,0,1,1];
-Q = 10*diag([qw,qw]);
-R = 1;
+xdim = p.in_contact.num_positions + p.in_contact.num_velocities;
+% Q = 10*diag([1,0.1,10,1,1,0.1,1,1]);
+Q = 0*eye(8);
+Qf = 100*eye(8);
+R = .001;
 options.alpha_1 = 100; % regularization term. Larger means more accurate but stiffer ODE to integrate.
-[c,Ktraj,Straj,Ptraj,Btraj,Ftraj,Straj_full] = constrainedtvlqr(plant,xtrajIC,utrajIC,Q,R,Q,options);
+[c,Ktraj,Straj,Ptraj,Btraj,Ftraj,Straj_full] = constrainedtvlqr(plant,xtrajIC,utrajIC,Q,R,Qf,options);
 
 % build closed loop plant
 sys_cl = plant.feedback(c);
 %%
 
 % get perturbed initial state
-x0 = xtrajIC.eval(0) + 0.03*randn(xdim,1);
+x0 = xtrajIC.eval(0) + [0.025*randn(xdim/4,1); 0.1*randn(xdim/4,1); 0.025*randn(xdim/4,1); 0.1*randn(xdim/4,1)];
 x0 = plant.resolveConstraints(x0);
 
 traj_clIC = sys_cl.simulate(xtrajIC.tspan,x0);
@@ -50,10 +51,24 @@ traj_cl = traj_cl.setOutputFrame(p.getOutputFrame);
 playback(v,traj_cl,struct('slider','true'))
 
 t = linspace(xtrajIC.tspan(1),xtrajIC.tspan(2),1e3);
+xo = xtrajIC.eval(t);
+xc = traj_clIC.eval(t);
 
 %%
-figure(1),clf
-plot(t,xtrajIC.eval(t),t,traj_clIC.eval(t),'--');
+figure(1),clf,
+subplot(2,2,1)
+plot(t,xo(1,:),t,xc(1,:),'--');
+axis('tight')
+subplot(2,2,2)
+plot(t,xo(2,:),t,xc(2,:),'--');
+axis('tight')
+subplot(2,2,3)
+plot(t,xo(3,:),t,xc(3,:),'--');
+axis('tight')
+subplot(2,2,4)
+plot(t,xo(4,:),t,xc(4,:),'--');
+axis('tight')
+
 figure(2),clf
 err = xtrajIC.eval(t)-traj_clIC.eval(t);
 plot(t,err);
