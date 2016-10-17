3870bd958d3c5a75ecbe8f5c20cfb872909ff346
Sean Curtis <sean.curtis@tri.global>
2016-10-17 09:46:19 -0700

    One more pass on style issues

--- drake/systems/plants/rigid_body_plant/sampled_contact_manifold.h
+++ drake/systems/plants/rigid_body_plant/sampled_contact_manifold.h
@@ -30,22 +30,14 @@ class DRAKE_EXPORT SampledContactManifold : public ContactManifold<T> {
   /**
    Computes a single contact detail -- Force and application point -- which
    is equivalent to applying all individual contact forces individually.
-   * @returns The single net Force.
+
+   @returns The single net Force.
    */
   ContactDetail<T> ComputeNetResponse() const override;
 
-  /**
-   Reports the number of distinct contact details for this manifold.
-   */
+  /** Reports the number of distinct contact details for this manifold. */
   size_t get_num_contacts() const override { return contact_details_.size(); }
 
-  /**
-   Access the ith contact detail in this manifold.
-
-   @param[in] i      The index of the requested contact detail.
-   @returns  A pointer to the ith contact detail (or null for invalid index
-             values.
-   */
   const ContactDetail<T>* get_ith_contact(size_t i) const override;
 
   ContactManifold<T>* clone() const override;
@@ -54,10 +46,6 @@ class DRAKE_EXPORT SampledContactManifold : public ContactManifold<T> {
    Add a new contact detail to the manifold.
    @param[in] detail    The contact detail to add.
    */
-  // TODO(SeanCurtis-TRI): Determine if this is the right interface for this.
-  // would it not be better to make sure that this ia a purely read-only
-  // interface?  Although, making sure only const versions of the manifold are
-  // available to the end user would alleviate this issue.
   void AddContactDetail(std::unique_ptr<ContactDetail<T>> detail);
 
  private:
