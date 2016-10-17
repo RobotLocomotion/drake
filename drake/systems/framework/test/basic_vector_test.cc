fb631bd28535af96fe012e8de0abc28586bf2244
Evan Drumwright <drum@tri.global>
2016-10-17 11:04:44 -0700

    Only commit

--- drake/systems/framework/test/basic_vector_test.cc
+++ drake/systems/framework/test/basic_vector_test.cc
@@ -13,6 +13,16 @@ namespace drake {
 namespace systems {
 namespace {
 
+// Tests SetZero functionality.
+GTEST_TEST(BasicVectorTest, SetZero) {
+  BasicVector<int> vec(3);
+  vec.get_mutable_value() << 1, 2, 3;
+  vec.SetZero();
+  Eigen::Vector3i expected;
+  expected << 0, 0, 0;
+  EXPECT_EQ(expected, vec.get_value());
+}
+
 // Tests that the BasicVector<double> is initialized to NaN.
 GTEST_TEST(BasicVectorTest, DoubleInitiallyNaN) {
   BasicVector<double> vec(3);
@@ -88,6 +98,46 @@ GTEST_TEST(BasicVectorTest, ReinitializeInvalid) {
   Eigen::Vector3i next_value;
   next_value << 3, 4, 5;
   EXPECT_THROW(vec.set_value(next_value), std::out_of_range);
+}
+
+// Tests all += * operations for BasicVector.
+GTEST_TEST(BasicVectorTest, PlusEqScaled) {
+  BasicVector<int> ogvec(2), vec1(2), vec2(2), vec3(2), vec4(2), vec5(2);
+  Eigen::Vector2i ans1, ans2, ans3, ans4, ans5;
+  ogvec.SetZero();
+  vec1.get_mutable_value() << 1, 2;
+  vec2.get_mutable_value() << 3, 5;
+  vec3.get_mutable_value() << 7, 11;
+  vec4.get_mutable_value() << 13, 17;
+  vec5.get_mutable_value() << 19, 23;
+  VectorBase<int>& v1 = vec1;
+  VectorBase<int>& v2 = vec2;
+  VectorBase<int>& v3 = vec3;
+  VectorBase<int>& v4 = vec4;
+  VectorBase<int>& v5 = vec5;
+  ogvec.PlusEqScaled(2, v1);
+  ans1 << 2, 4;
+  EXPECT_EQ(ans1, ogvec.get_value());
+
+  ogvec.SetZero();
+  ogvec.PlusEqScaled({{2, v1}, {3, v2}});
+  ans2 << 11, 19;
+  EXPECT_EQ(ans2, ogvec.get_value());
+
+  ogvec.SetZero();
+  ogvec.PlusEqScaled({{2, v1}, {3, v2}, {5, v3}});
+  ans3 << 46, 74;
+  EXPECT_EQ(ans3, ogvec.get_value());
+
+  ogvec.SetZero();
+  ogvec.PlusEqScaled({{2, v1}, {3, v2}, {5, v3}, {7, v4}});
+  ans4 << 137, 193;
+  EXPECT_EQ(ans4, ogvec.get_value());
+
+  ogvec.SetZero();
+  ogvec.PlusEqScaled({{2, v1}, {3, v2}, {5, v3}, {7, v4}, {11, v5}});
+  ans5 << 346, 446;
+  EXPECT_EQ(ans5, ogvec.get_value());
 }
 
 }  // namespace
