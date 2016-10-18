b4ecff74972edb0fa8cecf24457a31743379ff5c
Jamie Snape <jamie.snape@kitware.com>
2016-10-18 12:27:09 -0400

    Do not require CC, CXX, FC, F77 to be constantly set

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -2,7 +2,16 @@ include(ExternalProject)
 
 find_package(Git REQUIRED)
 
-if(${CMAKE_GENERATOR} STREQUAL "Unix Makefiles")
+if(CMAKE_GENERATOR STREQUAL "Ninja")
+  # The Ninja generator does not support Fortran
+  find_program(CMAKE_Fortran_COMPILER "$ENV{FC}" DOC "Fortran compiler")
+  set(CMAKE_Fortran_FLAGS "$ENV{FFLAGS}" CACHE STRING
+    "Flags for Fortran compiler")
+else()
+  enable_language(Fortran)
+endif()
+
+if(CMAKE_GENERATOR STREQUAL "Unix Makefiles")
   set(MAKE_COMMAND "$(MAKE)") # so we can pass through command line arguments
 else()
   find_program(MAKE_COMMAND make)
@@ -159,6 +168,8 @@ macro(drake_add_cmake_external PROJECT)
     CMAKE_C_FLAGS
     CMAKE_CXX_COMPILER
     CMAKE_CXX_FLAGS
+    CMAKE_Fortran_COMPILER
+    CMAKE_Fortran_FLAGS
     CMAKE_EXE_LINKER_FLAGS
     CMAKE_MODULE_LINKER_FLAGS
     CMAKE_SHARED_LINKER_FLAGS
