048fb78d141b51258566db65c0ce0179fc33a9ac
hongkai-dai <daihongkai@gmail.com>
2016-10-18 17:56:52 -0700

    WIP: start to re-write mosek_solver

--- drake/solvers/mosek_solver.cc
+++ drake/solvers/mosek_solver.cc
@@ -2,27 +2,36 @@
 // objective marshalling
 
 #include "drake/solvers/mosek_solver.h"
-#include "drake/solvers/mosek_wrapper.h"
+
+#include <mosek/mosek.h>
 
 #include <Eigen/Core>
 
 namespace drake {
 namespace solvers {
 
+// Add LinearConstraints and LinearEqualityConstraints to the Mosek task.
+MSKrescodee AddLinearConstraints(MSKenv_t &env, MSKtask_t &task, const MathematicalProgram &prog) {
+  for(const auto &binding : prog.linear_equality_constraints()) {
+
+  }
+}
+
 bool MosekSolver::available() const { return true; }
 
 SolutionResult MosekSolver::Solve(MathematicalProgram& prog) const {
-  if (!prog.GetSolverOptionsStr("Mosek").empty()) {
-    if (prog.GetSolverOptionsStr("Mosek").at("problemtype").find("linear")
-            != std::string::npos ||
-        prog.GetSolverOptionsStr("Mosek").at("problemtype").find("quadratic")
-            != std::string::npos ||
-        prog.GetSolverOptionsStr("Mosek").at("problemtype").find("sdp")
-            != std::string::npos) {
-      return MosekWrapper::Solve(prog);
-    }
-  }  // TODO(alexdunyak): add more mosek solution types.
-  return kUnknownError;
+  MSKenv_t env = NULL;
+  MSKtask_t task = NULL;
+  MSKrescodee rescode;
+
+  // Create the Mosek environment.
+  rescode = MSK_makeenv(&env, NULL);
+  if (rescode == MSK_RES_OK) {
+    // Create the optimization task.
+    rescode = MSK_maketask(env, 0, prog.num_vars(), &task);
+  }
+  // Add linear constraints
+  rescode = AddLinearConstraints(env, task, prog);
 }
 
 }
