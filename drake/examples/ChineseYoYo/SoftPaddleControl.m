ae816c53a399af8dd3c7c6a40d217d02bcdd7ba4
acsatici <acsatici@gmail.com>
2016-10-19 16:21:19 -0400

    Current version

--- drake/examples/ChineseYoYo/SoftPaddleControl.m
+++ drake/examples/ChineseYoYo/SoftPaddleControl.m
@@ -32,6 +32,13 @@ classdef SoftPaddleControl < DrakeSystem
       % xtraj and utraj to be defined
       %             c = tvlqr(plant,xtraj,utraj,Q,R,Qf);
       obj.numerically_stable = false;
+      
+      %%%%%%%%%%%%%%%% Part of another way to compute psid %%%%%%%%%%%%%%%%
+%       temp = NLPoincare();
+%       temp = -0.05;
+%       assignin('base','psid',temp);
+%       assignin('base','mlast', 1);
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     end
     
     function u = output(obj,t,~,x)
@@ -56,10 +63,9 @@ classdef SoftPaddleControl < DrakeSystem
       uE = -10*qp(1)*Etilde;
       
       epsilon = 0.5;
-      xFixed = -0.031;
-      xFixed = -0.05;
+%       xFixed = -0.031;
+      xFixed = -0.045;
       zTouchDes = 4;
-      %             psid = -0.001*(q(3)-xFixed)-0.005*qp(3);
       
       psi = q(1);
       R = [cos(psi), sin(psi); -sin(psi), cos(psi)];
@@ -72,89 +78,50 @@ classdef SoftPaddleControl < DrakeSystem
       xTouch = q(3) + qp(3)*tf;
       xpTouch = qp(3);
       
-      %             K = [0.066513, 0.071367, 0.032835, 1.3774e-11];   % original
-      %             K = 0.5*[0.11276, -0.095662, -0.047351, 2.8845e-11];
-      %             K = 0.75*[0.11155, -0.09665, -0.048202, 5.6288e-12];  % Works nicely
-      %K = -[-0.071799     0.042213     0.022939   4.7217e-13];
-      
-      psid = obj.K(1)*(xTouch-xFixed) + obj.K(2)*(zTouch-zTouchDes) + obj.K(3)*xpTouch + obj.K(4)*(zpTouch);
-      %             psid = -psid;
+      psid = -0.001*q(3)-0.005*qp(3);       % Old controller -- from my pocket
+%       psid = obj.K(1)*(xTouch-xFixed) + obj.K(2)*(zTouch-zTouchDes) + obj.K(3)*xpTouch + obj.K(4)*(zpTouch);    % Poincare map controller
       %             psid = 0;
+      
+      %%%%%%%%%%%%%%%%%%%%% Another way to compute psid %%%%%%%%%%%%%%%%%%%
+%       psid = evalin('base','psid');
+%       mlast = evalin('base','mlast');
+%       if m ~= mlast
+%           if m == 1
+%               psid = NLPoincare(x);
+%               assignin('base','psid',psid);
+%           end
+%       end
+%       assignin('base','mlast',m);
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      
       
       u = -obj.kp*(q(1)-psid) - obj.kd*qp(1) + C(1);
       if m == 2
         if(obj.numerically_stable)
           u = Hinvtilde/(Delta)*u + J(1)*((Jp'+2/epsilon*J)*qp+1/(epsilon^2)*phi-J*Hinv*C)/(Delta);
         else
-          %                     psid = -0.0005*(q(3)-xFixed)-0.001*qp(3);
-          %                     if psid > 0.01
-          %                         psid = 0.01;
-          %                     elseif psid < -0.01
-          %                         psid = -0.01;
-          %                     end
           psid = evalin('base','psid');
           u = -obj.kp*(q(1)-psid) - obj.kd*qp(1) + C(1);
           u = Hinvtilde/(Delta)*u + J(1)*(Jp'*qp-J*Hinv*C)/(Delta);
-          %                     if q(1)*qp(1) < 0
-          %                         u = u + uE;
-          %                     end
         end
       end
-      assignin('base','psid', psid)
+%       assignin('base','psid', psid)
       
-      tCutOff1 = 60;
-      tCutOff2 = 100;
-      if t > tCutOff1+50 && t < tCutOff2
-        %             psid = 0;
-        %             psid = -0.0005*q(3)-0.001*qp(3);
-        %K = 0.75*[0.11155, -0.09665, -0.048202, 5.6288e-12];
-        psid = obj.K(1)*(xTouch-xFixed) + obj.K(2)*(zTouch-zTouchDes) + obj.K(3)*xpTouch + obj.K(4)*(zpTouch);
-        u = -obj.kp*(q(1)-psid) - obj.kd*qp(1) + C(1) - 0.1*sign(Etilde*qp(1));
-        %             u = 1000*Etilde*qp(1);
-      end
+      tCutOff1 = 0;
+      tCutOff2 = 500;
       
       if t > tCutOff1 && t < tCutOff2 && m == 2
-        psi = q(1);
-        R = [cos(psi), sin(psi); -sin(psi), cos(psi)];
-        load_in_paddle = -[3; 0] + R'*([q(3); q(4)]-[-3;3]);
-        z = load_in_paddle(2);
-        %                 fprintf(['z = ', num2str(z), '\n'])
-        fprintf(['t = ', num2str(t), '\n'])
-        
-        k1 = 0.1;
-        k2 = 1e+3;
-        lambdad = 1/Hinvtilde*(  k1*J*Hinv*B*sign(Etilde*qp(1)) + J*Hinv*C + Jp'*qp  );
-        
-        epsilon = 1e-2;
-        cf = 1/2 + 1/pi*atan(1/epsilon*(1-z));
-        gammad = cf*lambdad;
-        
-        lambda = max(0,gammad);
-        %                 u = -1/(J*Hinv*B)*(Hinvtilde*lambda - J*Hinv*C - Jp'*qp);
-        %                 u = -cf*k*sign(Etilde*qp(1));
-        %                 u = -cf*k*2/pi*atan(20*Etilde*qp(1)) + C(1);
-        %                 u = -cf*k*Etilde*qp(1) + C(1);
-        %                 psid = evalin('base','psid');
-        %                 if abs(Etilde) > 1
-        %                     u = Hinvtilde/(Delta)*( -obj.kp*(q(1)-psid) - obj.kd*qp(1) ) + J(1)*(Jp'*qp-J*Hinv*C)/(Delta) - k1*2/pi*atan(20*Etilde*qp(1));
-        u = Hinvtilde/(Delta)*( -obj.kp*(q(1)-psid) - obj.kd*qp(1) + C(1) ) + J(1)*(Jp'*qp-J*Hinv*C)/(Delta) - k2*Etilde*qp(1);
-        %                 else
-        %                     u = Hinvtilde/(Delta)*( -obj.kp*(q(1)-psid) - obj.kd*qp(1) + C(1) ) + J(1)*(Jp'*qp-J*Hinv*C)/(Delta);
-        %                 end
-        %                 if t > 40.02
-        %                     keyboard
-        %                 end
+          psid = evalin('base','psid');
+          if rem(t,10) < 0.5
+            fprintf(['t = ', num2str(t), '\n'])
+          end
+          k2 = 1e+2;
+          u = Hinvtilde/(Delta)*( -obj.kp*(q(1)-psid) - obj.kd*qp(1) + C(1) ) + J(1)*(Jp'*qp-J*Hinv*C)/(Delta) - k2*Etilde*qp(1);
       end
-      assignin('base','psid', psid)
-      
-      %% Set
-      %             u = C(1);
-      
+      if m == 1
+        assignin('base','psid', psid)
+      end
     end
-    
-    %         function z = mySign(y, M)
-    %             z = 2/pi*atan(M*y);
-    %         end
   end
   
   methods (Static)
@@ -178,7 +145,7 @@ classdef SoftPaddleControl < DrakeSystem
       x0 = p.getInitialState();
       
       tic
-      [ytraj,xtraj] = simulate(sys,[0 100],x0);
+      [ytraj,xtraj] = simulate(sys,[0 54],x0);
       toc
       
       utraj = ytraj(10);
@@ -271,8 +238,10 @@ classdef SoftPaddleControl < DrakeSystem
         xlabel('$t$ [sec]', 'Interpreter', 'LaTeX', 'FontSize', 15)
         ylabel('$\psi$', 'Interpreter', 'LaTeX', 'FontSize', 15)
         
-        %                 poincareLinearMirrorLawControl
-        
+        if (0)
+            figure(25)
+            v.playbackAVI(xtraj, 'juggling_stabilized_energy_on')
+        end
       end
     end
     
