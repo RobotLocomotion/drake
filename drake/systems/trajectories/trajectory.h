96b8620ba768df557bf3d094316296820b70b1e5
Lucy Gibson <lucy.gibson@tri.global>
2016-10-19 15:32:57 -0400

    Add TimeVaryingPolynomialTrajSrcTest, comment and clean up code.

--- drake/systems/trajectories/trajectory.h
+++ drake/systems/trajectories/trajectory.h
@@ -5,8 +5,15 @@
 
 namespace drake {
 
+/**
+ * A Trajectory represents a path from a start state to a goal state.
+ * Each point in the path of the trajectory is represented by a state (a vector
+ * of doubles).
+ */
 class DRAKE_EXPORT Trajectory {
  public:
+  virtual ~Trajectory() {}
+
   virtual Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> value(
       double t) const = 0;
 };
