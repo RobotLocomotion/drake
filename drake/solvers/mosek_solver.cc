93226f6ad32685aa9b0caf28c6d324c790f6aaad
hongkai-dai <daihongkai@gmail.com>
2016-10-19 18:27:58 -0700

    WIP: work on LP for mosek, the test always says the problem is primal infeasible

--- drake/solvers/mosek_solver.cc
+++ drake/solvers/mosek_solver.cc
@@ -13,17 +13,21 @@ namespace solvers {
 namespace {
 // Add LinearConstraints and LinearEqualityConstraints to the Mosek task.
 template<typename _Binding>
-MSKrescodee AddLinearConstraintsFromBindings(MSKtask_t &task,
+MSKrescodee AddLinearConstraintsFromBindings(MSKtask_t* task,
                                              const std::list<_Binding> &constraint_list,
                                              bool is_equality_constraint) {
   for (const auto &binding : constraint_list) {
     auto constraint = binding.constraint();
     std::vector<int> var_indices = binding.variable_indices();
-    Eigen::MatrixXd A = constraint->A();
-    Eigen::VectorXd lb = constraint->lower_bound();
-    Eigen::VectorXd ub = constraint->upper_bound();
+    const Eigen::MatrixXd& A = constraint->A();
+    const Eigen::VectorXd& lb = constraint->lower_bound();
+    const Eigen::VectorXd& ub = constraint->upper_bound();
     MSKint32t constraint_idx;
-    MSKrescodee rescode = MSK_getnumcon(task, &constraint_idx);
+    MSKrescodee rescode = MSK_getnumcon(*task, &constraint_idx);
+    if (rescode != MSK_RES_OK) {
+      return rescode;
+    }
+    rescode = MSK_appendcons(*task, A.rows());
     if (rescode != MSK_RES_OK) {
       return rescode;
     }
@@ -31,33 +35,29 @@ MSKrescodee AddLinearConstraintsFromBindings(MSKtask_t &task,
     // each constraint. The sense can be equality constraint, less than,
     // greater than, or bounded on both side.
     for (int i = 0; i < A.rows(); ++i) {
-      rescode = MSK_appendcons(task, 1);
-      if (rescode != MSK_RES_OK) {
-        return rescode;
-      }
       if (is_equality_constraint) {
         rescode =
-            MSK_putconbound(task, constraint_idx + i, MSK_BK_FX, lb(i), ub(i));
+            MSK_putconbound(*task, constraint_idx + i, MSK_BK_FX, lb(i), ub(i));
         if (rescode != MSK_RES_OK) {
           return rescode;
         }
       } else {
         if (isinf(lb(i)) && isinf(ub(i))) {
-          continue; // Both lower and upper bounds are infinity, bypass the row.
+          rescode = MSK_putconbound(*task, constraint_idx + i, MSK_BK_FR, -MSK_INFINITY, MSK_INFINITY);
         } else if (isinf(lb(i)) && !isinf(ub(i))) {
-          rescode = MSK_putconbound(task,
+          rescode = MSK_putconbound(*task,
                                     constraint_idx + i,
                                     MSK_BK_UP,
                                     -MSK_INFINITY,
                                     ub(i));
         } else if (!isinf(lb(i)) && isinf(ub(i))) {
-          rescode = MSK_putconbound(task,
+          rescode = MSK_putconbound(*task,
                                     constraint_idx + i,
                                     MSK_BK_LO,
                                     lb(i),
                                     MSK_INFINITY);
         } else {
-          rescode = MSK_putconbound(task,
+          rescode = MSK_putconbound(*task,
                                     constraint_idx + i,
                                     MSK_BK_RA,
                                     lb(i),
@@ -67,8 +67,8 @@ MSKrescodee AddLinearConstraintsFromBindings(MSKtask_t &task,
           return rescode;
         }
       }
-      std::vector<MSKint32t> A_nnz_col_idx(A.rows());
-      std::vector<double> A_nnz_val(A.rows());
+      std::vector<MSKint32t> A_nnz_col_idx;
+      std::vector<double> A_nnz_val;
       A_nnz_col_idx.reserve(A.cols());
       A_nnz_val.reserve(A.cols());
       for (int j = 0; j < A.cols(); ++j) {
@@ -77,7 +77,7 @@ MSKrescodee AddLinearConstraintsFromBindings(MSKtask_t &task,
           A_nnz_val.push_back(A(i, j));
         }
       }
-      rescode = MSK_putarow(task,
+      rescode = MSK_putarow(*task,
                             constraint_idx + i,
                             A_nnz_val.size(),
                             A_nnz_col_idx.data(),
@@ -89,8 +89,8 @@ MSKrescodee AddLinearConstraintsFromBindings(MSKtask_t &task,
   }
   return MSK_RES_OK;
 }
-MSKrescodee AddLinearConstraints(MSKtask_t &task,
-                                 const MathematicalProgram &prog) {
+MSKrescodee AddLinearConstraints(MSKtask_t* task,
+                                 const MathematicalProgram& prog) {
   MSKrescodee rescode = AddLinearConstraintsFromBindings(task,
                                                          prog.linear_equality_constraints(),
                                                          true);
@@ -106,14 +106,54 @@ MSKrescodee AddLinearConstraints(MSKtask_t &task,
   return rescode;
 }
 
-MSKrescodee AddCosts(MSKtask_t &task, const MathematicalProgram &prog) {
+MSKrescodee AddBoundingBoxConstraints(MSKtask_t* task, const MathematicalProgram& prog) {
+  int num_vars = prog.num_vars();
+  std::vector<double> x_lb(num_vars, -std::numeric_limits<double>::infinity());
+  std::vector<double> x_ub(num_vars, std::numeric_limits<double>::infinity());
+  for(const auto& binding : prog.bounding_box_constraints()) {
+    const auto& constraint = binding.constraint();
+    const Eigen::VectorXd& lower_bound = constraint->lower_bound();
+    const Eigen::VectorXd& upper_bound = constraint->upper_bound();
+    int var_count = 0;
+    for(const DecisionVariableView& var : binding.variable_list()) {
+      for(int i = 0; i < static_cast<int>(var.size()); ++i) {
+        int x_idx = var.index() + i;
+        x_lb[x_idx] = std::max(x_lb[x_idx], lower_bound[var_count]);
+        x_ub[x_idx] = std::min(x_ub[x_idx], upper_bound[var_count]);
+        var_count++;
+      }
+    }
+  }
+
+  MSKrescodee rescode= MSK_RES_OK;
+  for(int i = 0; i< num_vars; i++) {
+    if(isinf(x_lb[i]) && isinf(x_ub[i])) {
+      rescode = MSK_putvarbound(*task, i, MSK_BK_FR, -MSK_INFINITY, MSK_INFINITY);
+    }
+    else if (isinf(x_lb[i]) && !isinf(x_ub[i])) {
+      rescode = MSK_putvarbound(*task, i, MSK_BK_UP, -MSK_INFINITY, x_ub[i]);
+    }
+    else if (!isinf(x_lb[i]) && isinf(x_ub[i])) {
+      rescode = MSK_putvarbound(*task, i, MSK_BK_LO, x_lb[i], MSK_INFINITY);
+    }
+    else {
+      rescode = MSK_putvarbound(*task, i, MSK_BK_RA, x_lb[i], x_ub[i]);
+    }
+    if (rescode != MSK_RES_OK) {
+      return rescode;
+    }
+  }
+  return rescode;
+}
+
+MSKrescodee AddCosts(MSKtask_t* task, const MathematicalProgram &prog) {
   // Add the cost in the form 0.5 * x' * Q_all * x + linear_terms' * x
-  MSKrescodee rescode;
+  MSKrescodee rescode = MSK_RES_OK;
   int xDim = prog.num_vars();
   std::vector<Eigen::Triplet<double>> linear_term_triplets;
-  for(const auto &binding : prog.linear_costs()) {
+  for(const auto& binding : prog.linear_costs()) {
     int var_count = 0;
-    Eigen::RowVectorXd c = binding.constraint()->A();
+    const auto& c = binding.constraint()->A();
     for(const DecisionVariableView &var : binding.variable_list()) {
       for(int i = 0; i < static_cast<int>(var.size()); ++i) {
         if(std::abs(c(var_count)) > std::numeric_limits<double>::epsilon()) {
@@ -126,7 +166,7 @@ MSKrescodee AddCosts(MSKtask_t &task, const MathematicalProgram &prog) {
   Eigen::SparseMatrix<double, Eigen::ColMajor> linear_terms(xDim, 1);
   linear_terms.setFromTriplets(linear_term_triplets.begin(), linear_term_triplets.end());
   for(Eigen::SparseMatrix<double, Eigen::ColMajor>::InnerIterator it(linear_terms, 0); it; ++it) {
-    rescode = MSK_putcj(task, it.row(), it.value());
+    rescode = MSK_putcj(*task, static_cast<MSKint32t>(it.row()), it.value());
     if(rescode != MSK_RES_OK) {
       return rescode;
     }
@@ -148,14 +188,22 @@ SolutionResult MosekSolver::Solve(MathematicalProgram& prog) const {
   rescode = MSK_makeenv(&env, NULL);
   if (rescode == MSK_RES_OK) {
     // Create the optimization task.
-    rescode = MSK_maketask(env, 0, prog.num_vars(), &task);
+    rescode = MSK_maketask(env, 0, num_vars, &task);
   }
-  // Add linear constraints
-  if(rescode == MSK_RES_OK) {
-    rescode = AddLinearConstraints(task, prog);
+  if (rescode == MSK_RES_OK) {
+    rescode = MSK_appendvars(task, num_vars);
   }
+  // Add costs
   if(rescode == MSK_RES_OK) {
-    rescode = AddCosts(task, prog);
+    rescode = AddCosts(&task, prog);
+  }
+  // Add bounding box constraints on decision variables.
+  if (rescode == MSK_RES_OK) {
+    rescode = AddBoundingBoxConstraints(&task, prog);
+  }
+  // Add linear constraints.
+  if(rescode == MSK_RES_OK) {
+    rescode = AddLinearConstraints(&task, prog);
   }
 
   SolutionResult result = SolutionResult::kUnknownError;
