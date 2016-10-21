58c39efeedf9a228417df354fa1662dd37e92273
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-10-21 12:05:10 +0200

    review comments

--- drake/systems/framework/primitives/linear_system_plant.h
+++ drake/systems/framework/primitives/linear_system_plant.h
@@ -5,13 +5,13 @@
 namespace drake {
 namespace systems {
 
-/// A linear system that is a specialization of an affine system where the
+/// A continuous Linear system that is a specialization of an affine system where the
 /// inital time derivative of the system state `xDot0` and the initial
 /// system output `y0` are both zero. Given an input signal `u` and a
-/// state `x` the output of this sytem 'y' is
+/// state `x` the output of this sytem 'y' is :
 /// <pre>
-///   \dot{x} = Ax + Bu + \dot{x}_0
-///   y = Cx + Du + y_0
+///   \dot{x} = Ax + Bu
+///   y = Cx + Du
 /// </pre>
 ///
 /// @tparam T The vector element type, which must be a valid Eigen scalar.
