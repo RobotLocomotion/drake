ad58d0a9c7287ff7d9864d892207816309657ebf
Jeremy Nimmer <jeremy.nimmer@tri.global>
2016-10-21 19:31:24 -0400

    Add LcmSystem support for Value<LcmtMessage> abstract ports

---
+++ drake/systems/lcm/serializer.cc
@@ -0,0 +1,12 @@
+#include "drake/systems/lcm/serializer.h"
+
+namespace drake {
+namespace systems {
+namespace lcm {
+
+SerializerInterface::SerializerInterface() {}
+SerializerInterface::~SerializerInterface() {}
+
+}  // namespace lcm
+}  // namespace systems
+}  // namespace drake
