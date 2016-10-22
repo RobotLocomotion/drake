fe969c529fcf3e6b09ecd7530bcbec8afc17f0fa
hongkai-dai <daihongkai@gmail.com>
2016-10-22 11:10:36 -0700

    refactor the tests. Now gurobi and mosek share the same tests

--- drake/solvers/test/mosek_solver_test.cc
+++ drake/solvers/test/mosek_solver_test.cc
@@ -2,8 +2,7 @@
 
 #include "gtest/gtest.h"
 
-#include "drake/common/eigen_matrix_compare.h"
-#include "drake/solvers/test/optimization_program_examples.h"
+#include "drake/solvers/test/optimization_program_examples.cc"
 
 namespace drake {
 namespace solvers {
@@ -12,46 +11,10 @@ GTEST_TEST(testMosek, testLP) {
   MosekSolver mosek_solver;
   testLinearPrograms(mosek_solver);
 }
-/*
- * Test a simple Quadratic Program.
- * The example is taken from
- * http://cvxopt.org/examples/tutorial/qp.html
- * min 2x1^2 + x2^2 + x1x2 + x1 + x2
- * s.t x1 >= 0
- *     x2 >= 0
- *     x1 + x2 = 1
- */
-GTEST_TEST(testMosek, MosekQuadraticProgram1) {
-  MathematicalProgram prog;
-  auto x = prog.AddContinuousVariables(2, "x");
 
-  // Deliberately add the cost as the sum of a quadratic cost
-  // 2x1^2 + x2^2 + x1x2 + x1
-  // add a linear cost
-  // x2
-  Eigen::Matrix2d Q;
-  Q << 4, 2,
-       0, 2;
-  Eigen::Vector2d b(1, 0);
-  prog.AddQuadraticCost(Q, b, {x});
-  prog.AddLinearCost(drake::Vector1d(1.0), {x(1)});
-
-  // Deliberately add two bounding box constraints
-  // (x1, x2) >= (0, 0)
-  // and
-  // x2 >= -1
-  // to test multiple bounding box constraints.
-  prog.AddBoundingBoxConstraint(Eigen::Vector2d(0, 0), Eigen::Vector2d::Constant(std::numeric_limits<double>::infinity()), {x});
-  prog.AddBoundingBoxConstraint(drake::Vector1d(-1), drake::Vector1d(std::numeric_limits<double>::infinity()), {x(1)});
-
-  prog.AddLinearEqualityConstraint(Eigen::RowVector2d(1, 1), drake::Vector1d(1));
-
+GTEST_TEST(testMosek, testQP) {
   MosekSolver mosek_solver;
-  SolutionResult result = mosek_solver.Solve(prog);
-  EXPECT_EQ(result, SolutionResult::kSolutionFound);
-
-  Eigen::Vector2d x_expected(0.25, 0.75);
-  EXPECT_TRUE(CompareMatrices(x.value(), x_expected, 1E-9, MatrixCompareType::absolute));
+  testQuadraticPrograms(mosek_solver);
 }
 
 /*
