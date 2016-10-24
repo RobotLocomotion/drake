767ae08b587c88855a3867cce12f3115d27c9a3b
Sean Curtis <sean.curtis@tri.global>
2016-10-24 12:07:08 -0700

    Refactor badly named method
    SetAllPairsClosestPairs is badly named:
      1) The initial word suggests the verb "set" and not the mathematical
         concept.
      2) "closest pairs" is also wrong.  It's closest points.

--- drake/systems/plants/RigidBodyTree.cpp
+++ drake/systems/plants/RigidBodyTree.cpp
@@ -691,13 +691,13 @@ bool RigidBodyTree::AllPairsClosestPoints(
   for (const auto& body : bodies) {
     body->appendCollisionElementIdsFromThisBody(ids_to_check);
   }
-  return SetAllPairsClosestPairs(cache, ids_to_check, pairs, use_margins);
+  return AllPairsClosestPointsInSet(cache, ids_to_check, pairs, use_margins);
 }
 
-bool RigidBodyTree::SetAllPairsClosestPairs(
-    const KinematicsCache<double>& cache,
-    const vector<DrakeCollision::ElementId>& ids_to_check,
-    std::vector<DrakeCollision::PointPair>* pairs, bool use_margins) {
+bool RigidBodyTree::AllPairsClosestPointsInSet(
+    const KinematicsCache<double> &cache,
+    const vector<DrakeCollision::ElementId> &ids_to_check,
+    std::vector<DrakeCollision::PointPair> *pairs, bool use_margins) {
   updateDynamicCollisionElements(cache);
   return collision_model_->closestPointsAllToAll(ids_to_check, use_margins,
                                                  *pairs);
