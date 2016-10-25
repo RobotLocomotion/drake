003df79374cf52067fefd2ae45a4b4a079227bcd
Chien-Liang Fok <liang.fok@tri.global>
2016-10-25 16:34:14 -0400

    Polished PidControlledSystem by using better terminology and adding documentation comments.

--- drake/systems/controllers/pid_controlled_system.h
+++ drake/systems/controllers/pid_controlled_system.h
@@ -14,29 +14,27 @@
 namespace drake {
 namespace systems {
 
-/// A system which encapsulates a PidController and a controlled
-/// System.
+/// A system that encapsulates a PidController and a controlled System (a.k.a
+/// the "plant").
 ///
-/// The passed in system must meet the following properties:
+/// The passed in plant must meet the following properties:
 ///
-/// * The first input port must be all of the control inputs to the
-/// * system (size N).
+/// * Input port zero must be all of the control inputs (size N). It is
+///   typically the generalized effort (e.g., force or torque) command.
 ///
-/// * The first output port must be of size N * 2, where the first N
-/// * elements are the positions of the elements of the controlled
-/// * system, and the second N elements are the velocities.
+/// * Output port zero must be of size N * 2, where the first N elements are the
+///   position states of the plant, and the second N elements are the velocity
+///   states of the plant.
 ///
-/// The resulting PidControlledSystem has two input ports and one
-/// output port with the following properties:
+/// The resulting PidControlledSystem has two input ports and one output port
+/// with the following properties:
 ///
-/// * The first input port is the feed forward control input to the
-/// * system.
+/// * Input port zero is the feed forward control input to the plant.
 ///
-/// * The second input port is the desired state of the system.
+/// * Input port one is the desired position and velocity state of the plant.
 ///
-/// * The output port is the current state of the system that is being
-/// * controlled (direct passthrough of the controlled system's output
-/// * port).
+/// * The output port is the current state of the plant (it is the direct
+///   passthrough of the plant's output port).
 ///
 /// @tparam T The vector element type, which must be a valid Eigen scalar.
 ///
@@ -48,15 +46,15 @@ namespace systems {
 template <typename T>
 class DRAKE_EXPORT PidControlledSystem : public Diagram<T> {
  public:
-  /// @param[in] system The system to be controlled.
+  /// @param[in] plant The system to be controlled.
   /// @param[in] Kp the proportional constant.
   /// @param[in] Ki the integral constant.
   /// @param[in] Kd the derivative constant.
-  PidControlledSystem(std::unique_ptr<System<T>> system,
+  PidControlledSystem(std::unique_ptr<System<T>> plant,
                       const T& Kp, const T& Ki, const T& Kd);
   ~PidControlledSystem() override;
 
-  System<T>* system() { return system_; }
+  System<T>* plant() { return plant_; }
 
   /// Sets @p context to a default state in which the positions and
   /// velocities are all zero.  The integral of the controller is also
@@ -64,13 +62,29 @@ class DRAKE_EXPORT PidControlledSystem : public Diagram<T> {
   void SetDefaultState(Context<T>* context) const;
 
  private:
-  System<T>* system_;
+  System<T>* plant_;
   PidController<T>* controller_;
+
+  // Takes as input the plant's error state vector and outputs separate position
+  // and velocity state error vectors. These outputs are then inputted into the
+  // PID controller.
   Demultiplexer<T>* error_demux_;
+
+  // Inverts the PID controller's output command. The output of this system is
+  // inputted into plant_input_.
   Gain<T>* controller_inverter_;
+
+  // Inverts the PidControlledSystem input containing the desired state of the
+  // plant. The output is inputted into state_minus_target_.
   Gain<T>* error_inverter_;
+
+  // Subtracts the desired state of the plant from the current state of the
+  // plant. The resulting vector is inputted into error_demux_.
   Adder<T>* state_minus_target_;
-  Adder<T>* system_input_;
+
+  // Adds the PID controller's output command with the provided feed forward
+  // command. The resulting command is then inputted into plant_.
+  Adder<T>* plant_input_;
 };
 
 }  // namespace systems
