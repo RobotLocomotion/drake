8ca26e5a9282f6ef9dc36bd6ee6cfce98871957f
Siyuan Feng <siyuan.feng@tri.global>
2016-10-26 02:07:52 -0400

    update commnets

--- drake/examples/QPInverseDynamicsForHumanoids/valkyrie_balancing_test.cc
+++ drake/examples/QPInverseDynamicsForHumanoids/valkyrie_balancing_test.cc
@@ -13,13 +13,13 @@ namespace qp_inverse_dynamics {
 // In this test, the Valkyrie robot is initialized to a nominal configuration
 // with zero velocities, and the qp controller is setup to track this
 // state. The robot is then perturbed in velocity for the Torso Pitch joint.
-// The test forward simulates the closed loop system for 2 seconds.
+// The test forward simulates the closed loop system for 4 seconds.
 // The simulation does not perform forward dynamics computation, instead, it
 // integrates the computed acceleration from the controller. This dummy
 // simulation should be replaced later with real simulation.
-// The controller should drive the position and velocity close to zero in 2
+// The controller should drive the position and velocity close to zero in 4
 // seconds.
-GTEST_TEST(testQPInverseDynamicsController, testStanding) {
+GTEST_TEST(testQPInverseDynamicsController, testBalancingStanding) {
   // Loads model.
   std::string urdf =
       drake::GetDrakePath() +
@@ -70,9 +70,9 @@ GTEST_TEST(testQPInverseDynamicsController, testStanding) {
   robot_status.Update(0, q, v, Eigen::VectorXd::Zero(robot.actuators.size()),
                       Eigen::Vector6d::Zero(), Eigen::Vector6d::Zero());
 
-  // dt = 4e-3 is picked arbitrarily to ensure the test finishes within a
-  // reasonable amount of time.
-  double dt = 4e-3;
+  // dt = 3e-3 is picked arbitrarily, with Gurobi, this one control call takes
+  // roughly 3ms.
+  double dt = 3e-3;
   double time = 0;
 
   // Feet should be stationary.
@@ -81,6 +81,7 @@ GTEST_TEST(testQPInverseDynamicsController, testStanding) {
 
   int tick_ctr = 0;
   while (time < 4) {
+    // Update weights.
     for (const std::string& joint_name : robot_status.arm_joint_names()) {
       int idx = robot_status.name_to_position_index().at(joint_name);
       input.mutable_desired_joint_motions().mutable_weights()[idx] = -1;
@@ -109,10 +110,8 @@ GTEST_TEST(testQPInverseDynamicsController, testStanding) {
 
     int status = con.Control(robot_status, input, &output);
 
-    if (status) {
-      //std::cout << input;
+    if (status)
       break;
-    }
 
     // Dummy integration.
     // TODO(siyuan.feng@tri.gloabl): replace this with sys2 simulator when it's
@@ -126,18 +125,13 @@ GTEST_TEST(testQPInverseDynamicsController, testStanding) {
     tick_ctr++;
   }
 
-  // Check final state.
+  // Check the final state.
   // Since the feet have equality constraints set to 0 in the qp controller,
   // they should have no velocity after simulation.
   // Thus, the tolerances on feet velocities are smaller than those for the
   // generalized position and velocity.
   EXPECT_TRUE(robot_status.foot(Side::LEFT).velocity().norm() < 1e-6);
   EXPECT_TRUE(robot_status.foot(Side::RIGHT).velocity().norm() < 1e-6);
-
-  for (int i = 0; i < q.size(); i++) {
-    std::cout << "rs.q: " << robot.get_position_name(i) << " "
-              << (q[i] - q_ini[i]) << std::endl;
-  }
   EXPECT_TRUE(drake::CompareMatrices(q, q_ini, 1e-4,
                                      drake::MatrixCompareType::absolute));
   EXPECT_TRUE(drake::CompareMatrices(
