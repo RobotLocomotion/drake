909f6007aca9bc56cbf79c5e2a5790b2b283c98e
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-10-26 19:24:36 +0200

    removed unnecessary vector argument

--- drake/systems/framework/primitives/linear_system.cc
+++ drake/systems/framework/primitives/linear_system.cc
@@ -11,7 +11,7 @@ LinearSystem<T>::LinearSystem(const Eigen::Ref<const MatrixX<T>>& A,
                               const Eigen::Ref<const MatrixX<T>>& B,
                               const Eigen::Ref<const MatrixX<T>>& C,
                               const Eigen::Ref<const MatrixX<T>>& D)
-    : AffineSystem<T>(A, B, VectorX<T>::Zero(A.rows(), 1), C, D,
+    : AffineSystem<T>(A, B, VectorX<T>::Zero(A.rows()), C, D,
                       VectorX<T>::Zero(C.rows(), 1)) {}
 // TODO(naveenoid): Modify constructor to accommodate 0 dimension systems;
 // i.e. in initializing xDot0 and y0 with a zero matrix.
