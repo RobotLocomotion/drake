515e4ea94240f25aff67e794b8b9f535a2331c06
David German <david.german@tri.global>
2016-10-26 18:39:41 -0400

    Overload pow when the base and the exponent are both AutoDiffScalars. Move autodiff overloads into their own file.

--- drake/math/expmap.h
+++ drake/math/expmap.h
@@ -10,6 +10,7 @@
 #include "drake/common/drake_assert.h"
 #include "drake/math/autodiff.h"
 #include "drake/math/autodiff_gradient.h"
+#include "drake/math/autodiff_overloads.h"
 
 namespace drake {
 namespace math {
