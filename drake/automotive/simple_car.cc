d852a66a39e30f3fdd5d69004a02e40227114675
Jeremy Nimmer <jeremy.nimmer@tri.global>
2016-10-26 21:49:18 -0400

    Avoid min/max overload bug in Eigen 3.2

--- drake/automotive/simple_car.cc
+++ drake/automotive/simple_car.cc
@@ -7,7 +7,9 @@ namespace automotive {
 
 // These instantiations must match the API documentation in simple_car.h.
 template class DRAKE_EXPORT SimpleCar<double>;
+#if EIGEN_VERSION_AT_LEAST(3, 2, 93)  // True with Drake superbuild.
 template class DRAKE_EXPORT SimpleCar<drake::TaylorVarXd>;
+#endif
 
 }  // namespace automotive
 }  // namespace drake
