e265501eb06ee83d96813ab2f547c8629b04039c
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-10-27 18:23:06 +0200

    All issues addressed

--- drake/systems/framework/primitives/linear_system.h
+++ drake/systems/framework/primitives/linear_system.h
@@ -6,13 +6,11 @@ namespace drake {
 namespace systems {
 
 /// A continuous linear system that is a specialization of an affine system
-/// where the inital time derivative of the system state `xDot0` and the
-/// initial system output `y0` are both zero. Given an input signal `u` and a
-/// state `x` the output of this sytem 'y' is:
-/// @f[
-///   \dot{x} = Ax + Bu \newline
-///   y = Cx + Du
-/// @f]
+/// where the inital time derivative of the system state `xDot0`
+/// and the initial system output `y0` are both zero. Given an input
+/// signal `u` and a state `x` the output of this sytem 'y' is:
+/// @f[\dot{x} = Ax + Bu @f] \newline
+/// @f[y = Cx + Du @f]
 ///
 /// @tparam T The vector element type, which must be a valid Eigen scalar.
 ///
@@ -26,6 +24,15 @@ namespace systems {
 template <typename T>
 class LinearSystem : public AffineSystem<T> {
  public:
+  /// Constructs a Linear system with a fixed set of coefficient matrices `A`,
+  /// `B`,`C`, and `D`.
+  /// The coefficient matrices must obey the following dimensions :
+  /// | Matrix  | Num Rows    | Num Columns |
+  /// |:-------:|:-----------:|:-----------:|
+  /// | A       | num states  | num states  |
+  /// | B       | num states  | num inputs  |
+  /// | C       | num outputs | num states  |
+  /// | D       | num outputs | num inputs  |
   LinearSystem(const Eigen::Ref<const MatrixX<T>>& A,
                const Eigen::Ref<const MatrixX<T>>& B,
                const Eigen::Ref<const MatrixX<T>>& C,
--- drake/systems/framework/primitives/test/affine_system_test.cc
+++ drake/systems/framework/primitives/test/affine_system_test.cc
@@ -8,32 +8,22 @@ namespace drake {
 namespace systems {
 namespace {
 
-
 class AffineSystemTest : public AffineLinearSystemTest {
  public:
   // Setup an arbitrary AffineSystem.
   AffineSystemTest()
-      : AffineLinearSystemTest(-4.5, 6.5, 3.5, -7.6) {
-  }
-
-  void SetUp() override { Initialize(); }
+      : AffineLinearSystemTest(-4.5, 6.5, 3.5, -7.6) {}
 
   void Initialize() override {
     // Construct the system I/O objects.
-    system_ =
-        make_unique<AffineSystem<double>>(A_, B_, XDot0_, C_, D_, Y0_);
+    system_ =  make_unique<AffineSystem<double>>(A_, B_, xDot0_, C_, D_, y0_);
     system_->set_name("test_affine_system");
     context_ = system_->CreateDefaultContext();
-    system_derivatives_ = system_->AllocateTimeDerivatives();
     input_vector_ = make_unique<BasicVector<double>>(2 /* size */);
-
-    Eigen::Vector2d output_eigen_vector(2);
-    auto output_vector = make_unique<BasicVector<double>>(output_eigen_vector);
-    auto output_value = make_unique<VectorValue<double>>(move(output_vector));
-
-    system_output_.add_port(move(output_value));
+    system_output_ = system_->AllocateOutput(*context_);
+    state_ = context_->get_mutable_continuous_state();
+    derivatives_ = system_->AllocateTimeDerivatives();
   }
-
  protected:
   unique_ptr<AffineSystem<double>> system_;
 };
@@ -46,10 +36,10 @@ TEST_F(AffineSystemTest, Construction) {
   EXPECT_EQ(system_->GetB(), B_);
   EXPECT_EQ(system_->GetC(), C_);
   EXPECT_EQ(system_->GetD(), D_);
-  EXPECT_EQ(system_->GetXDot0(), XDot0_);
-  EXPECT_EQ(system_->GetY0(), Y0_);
-  EXPECT_EQ(1, system_->get_num_output_ports());
-  EXPECT_EQ(1, system_->get_num_input_ports());
+  EXPECT_EQ(system_->GetxDot0(), xDot0_);
+  EXPECT_EQ(system_->Gety0(), y0_);
+  EXPECT_EQ(system_->get_num_output_ports(), 1);
+  EXPECT_EQ(system_->get_num_input_ports(), 1);
 }
 
 // Tests that the derivatives are correctly computed.
@@ -58,14 +48,13 @@ TEST_F(AffineSystemTest, Derivatives) {
   SetInput(u);
 
   Eigen::Vector2d x(0.1, 0.25);
-  SetState(x);
+  state_->SetFromVector(x);
 
-  derivatives_ = system_->AllocateTimeDerivatives();
   EXPECT_NE(derivatives_, nullptr);
   system_->EvalTimeDerivatives(*context_, derivatives_.get());
 
   Eigen::VectorXd expected_derivatives(2);
-  expected_derivatives = A_ * x + B_ * u + XDot0_;
+  expected_derivatives = A_ * x + B_ * u + xDot0_;
 
   EXPECT_EQ(expected_derivatives, derivatives_->get_vector().CopyToVector());
 }
@@ -78,17 +67,17 @@ TEST_F(AffineSystemTest, Output) {
 
   // Sets the state
   Eigen::Vector2d x(0.8, -22.1);
-  SetState(x);
+  state_->SetFromVector(x);
 
-  system_->EvalOutput(*context_, &system_output_);
+  system_->EvalOutput(*context_, system_output_.get());
 
   Eigen::VectorXd expected_output(2);
 
-  expected_output = C_ * x + D_ * u + Y0_;
+  expected_output = C_ * x + D_ * u + y0_;
 
   EXPECT_EQ(
       expected_output,
-      system_output_.get_port(0).get_vector_data<double>()->CopyToVector());
+      system_output_->get_vector_data(0)->get_value());
 }
 
 }  // namespace
