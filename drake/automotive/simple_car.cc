55f9ace4d0af3a54457cd3d4e032154021b3f048
Jeremy Nimmer <jeremy.nimmer@tri.global>
2016-10-26 21:49:18 -0400

    Add symbolic::Expression to automotive examples This replaces MST as the fundamental ScalarType

--- drake/automotive/simple_car.cc
+++ drake/automotive/simple_car.cc
@@ -1,6 +1,10 @@
 #include "drake/automotive/simple_car-inl.h"
 
 #include "drake/common/eigen_autodiff_types.h"
+#include "drake/common/symbolic_expression.h"
+
+// This is used indirectly to allow DRAKE_ASSERT on symbolic::Expression.
+#include "drake/common/symbolic_formula.h"
 
 namespace drake {
 namespace automotive {
@@ -10,6 +14,7 @@ template class DRAKE_EXPORT SimpleCar<double>;
 #if EIGEN_VERSION_AT_LEAST(3, 2, 93)  // True with Drake superbuild.
 template class DRAKE_EXPORT SimpleCar<drake::TaylorVarXd>;
 #endif
+template class DRAKE_EXPORT SimpleCar<drake::symbolic::Expression>;
 
 }  // namespace automotive
 }  // namespace drake
