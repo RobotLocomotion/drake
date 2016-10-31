a1cbfdeacbbef24661d730681057d1358b0ecf22
Sean Curtis <sean.curtis@tri.global>
2016-10-31 13:25:57 -0700

    Added center-of-pressure tests
    Tested the case where the points should successfully produce the correct
    center of pressure -- satisfying the requirements.

--- drake/systems/plants/rigid_body_plant/test/contact_resultant_force_test.cc
+++ drake/systems/plants/rigid_body_plant/test/contact_resultant_force_test.cc
@@ -2,16 +2,38 @@
 
 #include <gtest/gtest.h>
 
+#include "drake/common/eigen_matrix_compare.h"
 #include "drake/systems/plants/rigid_body_plant/contact_force.h"
 
 namespace drake {
 namespace systems {
 namespace {
 
+// Wrapper on CompareMatricies to simplify calls in the tests.
+template <typename DerivedA, typename DerivedB>
+bool AreEquivalent(const Eigen::MatrixBase<DerivedA>& m1,
+                   const Eigen::MatrixBase<DerivedB>& m2) {
+  const double kTolerance = 1e-14;
+  return CompareMatrices(m1, m2, kTolerance, MatrixCompareType::absolute);
+}
+
+// Utility method for asserting the state of the torque component of the wrench.
+template <typename DerivedA, typename DerivedB>
+bool AssertTorque(const WrenchVector<DerivedA>& wrench,
+                  const Eigen::MatrixBase<DerivedB>& torque) {
+  Vector3<DerivedA> test_torque = wrench.template head<3>();
+  return AreEquivalent(test_torque, torque);
+}
+
+// Utility method for asserting the state of the force component of the wrench.
+template <typename DerivedA, typename DerivedB>
+bool AssertForce(const WrenchVector<DerivedA>& wrench,
+                 const Eigen::MatrixBase<DerivedB>& force) {
+  Vector3<DerivedA> test_force = wrench.template tail<3>();
+  return AreEquivalent(test_force, force);
+}
+
 // THe tests to perform
-//  2. Multiple planar forces (i.e., application points on plane with normal
-//      directions perpendicular to that plane) - normal components only.
-//    a. Should produce proper center of pressure (lies on plane), no torque)
 //  3. Multiple planar forces - tangent components only.
 //    a. ???  What gets returned?
 //  4. Multiple planar forces - tangent and normal
@@ -59,7 +81,6 @@ GTEST_TEST(ContactResultantForceTest, ContactForceTests) {
   expected_wrench.template tail<3>() = norm + tan;
   ASSERT_EQ(f2.get_wrench(), expected_wrench);
 }
-
 
 // Tests the various forms for adding forces to the accumulator.  This also
 // implicitly the case where the resultant force is computed for a single
@@ -109,6 +130,94 @@ GTEST_TEST(ContactResultantForceTest, ForceAccumulationTest) {
   }
 }
 
+// Tests the case where multiple contacts forces are provided, but the forces:
+//    1. Are applied at points which all lie on the same plane.
+//    2. Have normal directions perpendicular to the plane.
+//    3. Have no tangential forces.
+// The results should have the following properties:
+//    1. The resultant wrench should have a zero torque component.
+//    2. The resultant force should lie 100% in the normal direction.
+//    3. The min moment point should lie on the plane with the input points.
+GTEST_TEST(ContactResultantForceTest, SimplePlanarContactTest) {
+  // Do *not* change these values. The tests below will become invalid.
+  Vector3<double> pos1, pos2, pos3;
+  Vector3<double> norm, zero;
+  pos1 << 1, 0, 0;
+  pos2 << 2, 0, 0;
+  pos3 << 1, 5, 0;
+  norm << 0, 0, 1;
+  zero = Vector3<double>::Zero();
+  Vector3<double> expected_point;
+
+  // Case 1: Two identical forces -- min. moment point should lie in between
+  // them.
+  {
+    ContactResultantForceCalculator<double> calc;
+    calc.AddForce(pos1, norm, zero, zero);
+    calc.AddForce(pos2, norm, zero, zero);
+    Vector3<double> min_point = calc.ComputeMinimumMomentPoint();
+    WrenchVector<double> wrench = calc.ComputeResultantWrench();
+
+    ASSERT_TRUE(AssertTorque(wrench, zero));
+    ASSERT_TRUE(AssertForce(wrench, norm * 2));
+    expected_point = (pos1 + pos2) * 0.5;
+    ASSERT_TRUE(AreEquivalent(min_point, expected_point));
+  }
+
+  // Case 2: Two forces of unequal magnitudes.  Min. moment point should lie
+  //  between the two points based on ratio of force magnitudes.
+  {
+    ContactResultantForceCalculator<double> calc;
+    calc.AddForce(pos1, norm, zero, zero);
+    calc.AddForce(pos2, 2.0 * norm, zero, zero);
+    Vector3<double> min_point = calc.ComputeMinimumMomentPoint();
+    WrenchVector<double> wrench = calc.ComputeResultantWrench();
+
+    ASSERT_TRUE(AssertTorque(wrench, zero));
+    ASSERT_TRUE(AssertForce(wrench, norm * 3));
+    expected_point = pos1 / 3. + pos2 * 2. / 3.;
+    ASSERT_TRUE(AreEquivalent(min_point, expected_point));
+  }
+
+  // Case 3: Three forces of unequal magnitude.
+  {
+    ContactResultantForceCalculator<double> calc;
+    calc.AddForce(pos1, norm, zero, zero);
+    calc.AddForce(pos2, 2.0 * norm, zero, zero);
+    calc.AddForce(pos3, 3.0 * norm, zero, zero);
+    Vector3<double> min_point = calc.ComputeMinimumMomentPoint();
+    WrenchVector<double> wrench = calc.ComputeResultantWrench();
+
+    ASSERT_TRUE(AssertTorque(wrench, zero));
+    ASSERT_TRUE(AssertForce(wrench, norm * 6));
+    expected_point << 4.0 / 3, 5.0 / 2, 0;
+    ASSERT_TRUE(AreEquivalent(min_point, expected_point));
+  }
+
+  // Case 4: Three forces of unequal magnitude with non-zero tangent components.
+  // The minimum moment point (w.r.t. normal forces) should be the same as if
+  // there were no tangent forces. However, the resultant torque will no longer
+  // be zero.
+  {
+    Vector3<double> tan1, tan2, tan3;
+    tan1 << 0.1, 0.1, 0.0;
+    tan2 << -0.2, 0.1, 0.0;
+    tan3 << 0.1, -0.2, 0.0;
+    ContactResultantForceCalculator<double> calc;
+    calc.AddForce(pos1, norm, tan1, zero);
+    calc.AddForce(pos2, 2.0 * norm, tan2, zero);
+    calc.AddForce(pos3, 3.0 * norm, tan3, zero);
+    Vector3<double> min_point = calc.ComputeMinimumMomentPoint();
+    WrenchVector<double> wrench = calc.ComputeResultantWrench();
+
+    Vector3<double> expected_torque;
+    expected_torque << 0, 0, -0.4;
+    ASSERT_TRUE(AssertTorque(wrench, expected_torque));
+    ASSERT_TRUE(AssertForce(wrench, norm * 6));
+    expected_point << 4.0 / 3, 5.0 / 2, 0;
+    ASSERT_TRUE(AreEquivalent(min_point, expected_point));
+  }
+}
 }  // namespace
 }  // namespace systems
 }  // namespace drake
