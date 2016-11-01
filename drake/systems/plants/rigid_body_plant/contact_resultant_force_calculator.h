bac56bca0736aa94a735976fbe2ae71fa4f6e85f
Sean Curtis <sean.curtis@tri.global>
2016-11-01 15:39:42 -0700

    Update calculator documentation

--- drake/systems/plants/rigid_body_plant/contact_resultant_force_calculator.h
+++ drake/systems/plants/rigid_body_plant/contact_resultant_force_calculator.h
@@ -18,12 +18,13 @@ namespace systems {
  possibly non-zero pure torque component, applied at a single application point.
  The application point for the force will be a minimum moment
  magnitude point.  That is, if the resultant force is applied at this point,
- it will induce the minimum moment. This is a general solution that is well
+ it will induce the minimum possible moment (relative to any other application
+ point). This is a general solution that is well
  defined and can be applied to arbitrary sets of contact forces. It must be
- emphsized, that this calculation considers only the *normal* components of the
+ emphasized, that this calculation considers only the *normal* components of the
  input contact forces in computing the minimum moment point.  The remaining
  tangential components will be included in the final resultant, but will not
- affect the point calculations.
+ affect the calculation of the point.
 
  Center of Pressure
  ==================
@@ -40,8 +41,9 @@ namespace systems {
 
  Usage
  =====
- The class is designed to be exercised by a contact model.  As each pair
- of collision elements are evaluated, the contact model should instantiate a
+ The class is designed to be exercised by a contact response model.  As each
+ pair
+ of collision elements is evaluated, the contact model should instantiate a
  ContactResultantForceCalculator.  As each contact point between the elements
  is processed and a contact force is computed, the details of the contact force
  are provided to the calculator (via calls to AddForce).
@@ -78,29 +80,41 @@ namespace systems {
  minimum moment.  The ContactResultantForceCalculator selects one of
  those points.
 
- We assume that the "ideal" point would be where the line intersects the contact
+ We assume that the "ideal" point would be where the line intersects the
+ (deforemd) contact
  surface. Generally, this can't be solved because it depends on a geometric
  query that is outside the scope of this calculator class.  Furthermore, in many
  cases, it is unnecessary. A point on the surface is good for visualization, but
- in contexts where a mathematically meaningful point is all that is needed, then
+ in contexts where only a mathematically meaningful point is all that is needed,
+ then
  one point is as good as another. That said, the calculator employs a method to
  cheaply approximate the intersection of the line with the contact surface by
  doing the following.
 
  The central axis can be thought of as a line defined by a point and direction.
  The point can be any point on the line.  The direction is defined by the
- direction of the resultant normal force (i.e., the sum of the normal components
+ direction of the resultant normal force (i.e., the vector sum of the normal
+ components
  of all forces.) The direction vector defines "positive" and "negative"
  directions on the line. The force originated from the negative direction and
  accelerates the body in the positive direction.  If we had access to the
  geometry, the point we would be interested in, would be the intersection of the
- line and geometry that is farthest in the "negative" direction (i.e., closest
+ line and (deformed) geometry that is farthest in the "negative" direction
+ (i.e., closest
  to the originating source of the contact).
 
  We will approximate this by finding the contact force application point that
  similarly lies farthest in the negative direction (simply by projecting the
  application points on the line.)  This most-negative projection point will
  serve as the reported minimum moment point.
+
+ This reported minimum moment point
+ can be moved along the central axis by the caller if additional information is
+ available. Movement along the axis preserves its "minimal-moment" property.
+ For example, if the caller had access to the (deformed) geometry, the ray
+ defined by the reported minimum moment point and the resultant ContactForce
+ normal direction can be intersected with the geometry to create an alternate,
+ but equally valid, minimum moment point.
 
  A Zero-Resultant Force
  ======================
