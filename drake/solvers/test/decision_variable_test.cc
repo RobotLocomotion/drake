4670a88f09e6287810f8b0e17dc37481b2acbd22
hongkai-dai <daihongkai@gmail.com>
2016-11-01 13:57:31 -0400

    Use Eigen::map to represent a matrix of decision variable scalars. Run into problem that row() return a differnt type, thus cannot insert it into a list

--- drake/solvers/test/decision_variable_test.cc
+++ drake/solvers/test/decision_variable_test.cc
@@ -9,32 +9,40 @@ GTEST_TEST(TestDecisionVariable, TestConstructor) {
   EXPECT_EQ(static_cast<int>(x.index()), 0);
 
   // Now create a vector of decision variables.
-  std::vector<DecisionVariableScalar> v;
-  v.reserve(10);
+  std::vector<DecisionVariableScalar> v_mutable;
+  std::vector<std::reference_wrapper<const DecisionVariableScalar>> v_immutable;
+  v_mutable.reserve(10);
+  v_immutable.reserve(10);
   for (int i = 0; i < 10; ++i) {
-    v.push_back(DecisionVariableScalar(DecisionVariableScalar::VarType::CONTINUOUS, "x", i));
-    v[i].set_value(2*i);
-    EXPECT_EQ(v[i].value(), 2 * i);
+    DecisionVariableScalar vi(DecisionVariableScalar::VarType::CONTINUOUS, "x", i);
+    v_mutable.push_back(DecisionVariableScalar(DecisionVariableScalar::VarType::CONTINUOUS, "x", i));
+    v_immutable.push_back(std::cref(v_mutable[i]));
+    v_mutable[i].set_value(2*i);
+    EXPECT_EQ(v_immutable[i].get().value(), 2 * i);
   }
-
-  MatrixDecisionVariables X(2, 5, v);
+  // Constructs a variable size decision variable matrix.
+  MatrixXDecisionVariables X(v_immutable.data(), 2, 5);
   for (int j = 0; j < 5; ++j) {
     for (int i = 0; i < 2; ++i) {
-      EXPECT_EQ(X.value(i, j), 2 * (j * 2 + i));
-      EXPECT_EQ(X(i,j).value(0, 0), 2 * (j * 2 + i));
+      EXPECT_EQ(X(i, j).get().value(), 2 * (j * 2 + i));
     }
   }
 
-  /*auto X_row1 = X.row(1);
-  for (int j = 0; j < 5; ++j) {
-    EXPECT_EQ(X_row1.value(0, j), 2 * (j * 2 + 1));
-  }*/
-  // Now modify v
-  v[3].set_value(2);
-  EXPECT_EQ(v[3].value(), 2);
-  EXPECT_EQ(X.value(1, 1), 2);
-  EXPECT_EQ(X(1, 1).value(0, 0), 2);
-  //EXPECT_EQ(X_row1.value(0, 1), 2);
+  v_mutable[2].set_value(5);
+  EXPECT_EQ(v_immutable[2].get().value(), 5);
+  EXPECT_EQ(X(0, 1).get().value(), 5);
+
+  // Constructs a fixed size decision variable matrix.
+  MatrixDecisionVariables<2, 4> X24(v_immutable.data());
+
+  // Take a column of the matrix, check the value.
+  auto X24_col1 = X24.col(1);
+  EXPECT_EQ(X24_col1(0).get().value(), 5);
+
+  v_mutable[2].set_value(-1);
+  EXPECT_EQ(X24_col1(0).get().value(), -1);
+  EXPECT_EQ(X24(0, 1).get().value(), -1);
+
 }
 } // namespace solvers
 } // namespace drake
