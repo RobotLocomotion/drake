60c508d4adf029a0d7e38c3fbf3cfd2581e6fc59
Sean Curtis <sean.curtis@tri.global>
2016-11-02 07:31:56 -0700

    Updated documentation as per review

--- drake/systems/plants/rigid_body_plant/contact_force.h
+++ drake/systems/plants/rigid_body_plant/contact_force.h
@@ -7,20 +7,25 @@ namespace drake {
 namespace systems {
 
 /**
- The data for a single contact force.
+ The data for a single contact force applied to one body of a contacting pair.
 
- Ultimately, a contact force consists of an application point and forces.
- The forces are defined with three pieces of data:
-  - a translational force,
-  - the normal direction of the translational force.  Assumed to be unit length
-    and used to decompose the force into normal and tangential components. The
-    normal is typically defined by the contact normal.
+ Ultimately, a contact force consists of an application point, a spatial
+ force, and a unit vector. The spatial force includes:
+  - a translational force, that is a pure force applied at a point,
   - a "pure torque", the rotational force. This is not the same as the moment
     induced by the translational force.  This is a pure, abstract force (e.g.,
     modeling torsional friction.)
+  The unit normal indicates the normal direction of the translational force.
+  Used to decompose the force into normal and tangential components. The
+  normal is typically defined by the contact normal.
+
+  The application point, normal vector, and force vectors should all be measured
+  and expressed in a common frame.
 
   The ContactForce class provides an interface for accessing the components
   individually as well as combining the forces into a WrenchVector.
+
+  @tparam T The scalar type. Must be a valid Eigen scalar.
  */
 template <typename T>
 class DRAKE_EXPORT ContactForce {
@@ -42,7 +47,6 @@ class DRAKE_EXPORT ContactForce {
    @param application_point         The point at which the wrench is applied.
    @param force                     The translational force.
    @param normal                    The translational force's normal direction.
-   @param pure_torque               The pure torque component
    */
   ContactForce(const Vector3<T>& application_point, const Vector3<T>& force,
                const Vector3<T>& normal);
@@ -74,6 +78,18 @@ class DRAKE_EXPORT ContactForce {
 
   Vector3<T> get_force() const { return force_; }
 
+  /**
+   This is a utility function for returning a compact representation of the
+   spatial force.  It produces a wrench-like data structure containing the
+   spatial force, in that it is a vector in R6 representing a concatentation of
+   a rotational and translational force.
+
+   However, the rotational component is *not* r X F.  Instead, it is the pure
+   torque component of the contact force. To make use of this wrench, it must
+   be associated with the application point to know where the force is appl8ied
+   and compute a moment around an origin.
+   @returns the wrench-like representation of the contact spatial force.
+   */
   WrenchVector<T> get_wrench() const {
     WrenchVector<T> wrench;
     wrench.template head<3>() = pure_torque_;
