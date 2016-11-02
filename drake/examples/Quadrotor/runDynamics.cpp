54162f8ebf65c148b51a113da799a8496de72f66
foehnph <foehnph@ethz.ch>
2016-11-02 12:41:56 +0100

    added warehouse

--- drake/examples/Quadrotor/runDynamics.cpp
+++ drake/examples/Quadrotor/runDynamics.cpp
@@ -11,6 +11,7 @@
 #include "drake/systems/framework/diagram_builder.h"
 #include "drake/systems/framework/primitives/constant_vector_source.h"
 #include "drake/systems/plants/parser_urdf.h"
+#include "drake/systems/plants/parser_sdf.h"
 #include "drake/systems/plants/joints/floating_base_types.h"
 #include "drake/systems/plants/parser_model_instance_id_table.h"
 #include "drake/systems/plants/rigid_body_plant/drake_visualizer.h"
@@ -21,6 +22,8 @@ namespace examples {
 namespace quadrotor {
 namespace {
 
+DEFINE_double(duration, 3, "Total duraation of simulation.");
+
 template<typename T>
 class Quadrotor : public systems::Diagram<T> {
  public:
@@ -28,22 +31,24 @@ class Quadrotor : public systems::Diagram<T> {
     this->set_name("Quadrotor");
 
     auto tree = std::make_unique<RigidBodyTree>();
-    parsers::ModelInstanceIdTable vehicle_instance_id_table =
-        drake::parsers::urdf::AddModelInstanceFromUrdfFile(
-            drake::GetDrakePath() + "/examples/Quadrotor/quadrotor.urdf",
-            systems::plants::joints::kRollPitchYaw,
-            nullptr, tree.get());
+    drake::parsers::urdf::AddModelInstanceFromUrdfFile(
+        drake::GetDrakePath() + "/examples/Quadrotor/quadrotor.urdf",
+        systems::plants::joints::kRollPitchYaw,
+        nullptr, tree.get());
 
+    drake::parsers::sdf::AddModelInstancesFromSdfFile(
+        drake::GetDrakePath() + "/examples/Quadrotor/warehouse.sdf",
+        systems::plants::joints::kFixed,
+        nullptr, tree.get());
 
-
+    AddGround(tree.get());
 
     systems::DiagramBuilder<T> builder;
 
     plant_ = builder.template AddSystem<systems::RigidBodyPlant<T>>(
         std::move(tree));
 
-    Eigen::VectorXd hover_input = Eigen::VectorXd::Constant(4,0.5*9.81/4.0);
-    std::cout << hover_input <<"\n";
+    Eigen::VectorXd hover_input = Eigen::VectorXd::Zero(4);
     source_ = builder.template AddSystem<systems::ConstantVectorSource<T>>(
         hover_input);
 
@@ -56,10 +61,31 @@ class Quadrotor : public systems::Diagram<T> {
     builder.BuildInto(this);
   }
 
+  void AddGround(RigidBodyTree* tree) {
+    double kBoxWidth = 1000;
+    double kBoxDepth = 10;
+    DrakeShapes::Box geom(Eigen::Vector3d(kBoxWidth, kBoxWidth, kBoxDepth));
+    Eigen::Isometry3d T_element_to_link = Eigen::Isometry3d::Identity();
+    T_element_to_link.translation() << 0, 0,
+        -kBoxDepth / 2.0;  // top of the box is at z = 0
+
+    RigidBody& world = tree->world();
+    Eigen::Vector4d color;
+    color << 0.9297, 0.7930, 0.6758, 1;
+    world.AddVisualElement(
+        DrakeShapes::VisualElement(geom, T_element_to_link, color));
+    tree->addCollisionElement(
+        DrakeCollision::Element(geom, T_element_to_link, &world), world,
+        "terrain");
+    tree->updateStaticCollisionElements();
+  }
+
   void SetDefaultState(systems::Context<T>* context) const {
     systems::Context<T>* plant_context =
         this->GetMutableSubsystemContext(context, plant_);
-    plant_->SetZeroConfiguration(plant_context);
+    Eigen::VectorXd x0 = Eigen::VectorXd::Zero(12,1);
+    x0(2) = 0.2;
+    plant_->set_state_vector(plant_context, x0);
   }
 
   const systems::RigidBodyPlant<T>& get_rigid_body_plant() {
@@ -82,14 +108,14 @@ int do_main(int argc, char* argv[]) {
   model.SetDefaultState(simulator.get_mutable_context());
 
   simulator.Initialize();
-  simulator.StepTo(0.5);
+  simulator.StepTo(FLAGS_duration);
   return 0;
 }
 
-}
-}
-}
-}
+}   // namespace
+}   // namespace quadrotor
+}   // namespace examples
+}   // namespace drake
 
 int main(int argc, char* argv[]) {
   return drake::examples::quadrotor::do_main(argc, argv);
