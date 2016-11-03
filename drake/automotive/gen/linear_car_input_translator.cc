ec9495a4440931bf2bf261d8f583fce0eb9247fb
Jon DeCastro <jonathan.decastro@tri.global>
2016-11-03 15:48:40 -0400

    Add diagram composing an ego car, a planner and an agent car

--- drake/automotive/gen/linear_car_input_translator.cc
+++ drake/automotive/gen/linear_car_input_translator.cc
@@ -1,4 +1,4 @@
-#include "drake/automotive/gen/linear_car_state_translator.h"
+#include "drake/automotive/gen/linear_car_input_translator.h"
 
 // GENERATED FILE DO NOT EDIT
 // See drake/tools/lcm_vector_gen.py.
@@ -11,39 +11,37 @@ namespace drake {
 namespace automotive {
 
 std::unique_ptr<systems::BasicVector<double>>
-LinearCarStateTranslator::AllocateOutputVector() const {
-  return std::make_unique<LinearCarState<double>>();
+LinearCarInputTranslator::AllocateOutputVector() const {
+  return std::make_unique<LinearCarInput<double>>();
 }
 
-void LinearCarStateTranslator::Serialize(
+void LinearCarInputTranslator::Serialize(
     double time, const systems::VectorBase<double>& vector_base,
     std::vector<uint8_t>* lcm_message_bytes) const {
   const auto* const vector =
-      dynamic_cast<const LinearCarState<double>*>(&vector_base);
+      dynamic_cast<const LinearCarInput<double>*>(&vector_base);
   DRAKE_DEMAND(vector != nullptr);
-  drake::lcmt_linear_car_state_t message;
+  drake::lcmt_linear_car_input_t message;
   message.timestamp = static_cast<int64_t>(time * 1000);
-  message.x = vector->x();
-  message.v = vector->v();
+  message.vdot = vector->vdot();
   const int lcm_message_length = message.getEncodedSize();
   lcm_message_bytes->resize(lcm_message_length);
   message.encode(lcm_message_bytes->data(), 0, lcm_message_length);
 }
 
-void LinearCarStateTranslator::Deserialize(
+void LinearCarInputTranslator::Deserialize(
     const void* lcm_message_bytes, int lcm_message_length,
     systems::VectorBase<double>* vector_base) const {
   DRAKE_DEMAND(vector_base != nullptr);
-  auto* const my_vector = dynamic_cast<LinearCarState<double>*>(vector_base);
+  auto* const my_vector = dynamic_cast<LinearCarInput<double>*>(vector_base);
   DRAKE_DEMAND(my_vector != nullptr);
 
-  drake::lcmt_linear_car_state_t message;
+  drake::lcmt_linear_car_input_t message;
   int status = message.decode(lcm_message_bytes, 0, lcm_message_length);
   if (status < 0) {
-    throw std::runtime_error("Failed to decode LCM message linear_car_state.");
+    throw std::runtime_error("Failed to decode LCM message linear_car_input.");
   }
-  my_vector->set_x(message.x);
-  my_vector->set_v(message.v);
+  my_vector->set_vdot(message.vdot);
 }
 
 }  // namespace automotive
--- drake/automotive/simple_car_gen.sh
+++ drake/automotive/simple_car_gen.sh
@@ -16,6 +16,10 @@ gen_lcm_and_vector "euler floating joint state" x y z roll pitch yaw
 gen_lcm_and_vector "idm with trajectory agent state" x_e v_e x_a v_a a_a
 gen_lcm_and_vector "simple car state" x y heading velocity
 gen_lcm_and_vector "simple car config" wheelbase track max_abs_steering_angle max_velocity max_acceleration velocity_lookahead_time velocity_kp
+gen_lcm_and_vector "linear car input" vdot
 gen_lcm_and_vector "linear car state" x v
+gen_lcm_and_vector "idm planner input" x v
 
 gen_vector_yaml "idm with trajectory agent parameters" $drake/automotive/idm_with_trajectory_agent_parameters.yaml
+gen_vector_yaml "idm planner parameters" $drake/automotive/idm_planner_parameters.yaml
+
