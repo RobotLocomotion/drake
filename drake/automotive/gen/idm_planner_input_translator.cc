012f61f909dcf0b648dda1f06c1398fdc489a52a
Jon DeCastro <jonathan.decastro@tri.global>
2016-11-03 15:48:40 -0400

    Add diagram composing an ego car, a planner and an agent car

--- drake/automotive/gen/idm_planner_input_translator.cc
+++ drake/automotive/gen/idm_planner_input_translator.cc
@@ -1,4 +1,4 @@
-#include "drake/automotive/gen/linear_car_state_translator.h"
+#include "drake/automotive/gen/idm_planner_input_translator.h"
 
 // GENERATED FILE DO NOT EDIT
 // See drake/tools/lcm_vector_gen.py.
@@ -11,17 +11,17 @@ namespace drake {
 namespace automotive {
 
 std::unique_ptr<systems::BasicVector<double>>
-LinearCarStateTranslator::AllocateOutputVector() const {
-  return std::make_unique<LinearCarState<double>>();
+IdmPlannerInputTranslator::AllocateOutputVector() const {
+  return std::make_unique<IdmPlannerInput<double>>();
 }
 
-void LinearCarStateTranslator::Serialize(
+void IdmPlannerInputTranslator::Serialize(
     double time, const systems::VectorBase<double>& vector_base,
     std::vector<uint8_t>* lcm_message_bytes) const {
   const auto* const vector =
-      dynamic_cast<const LinearCarState<double>*>(&vector_base);
+      dynamic_cast<const IdmPlannerInput<double>*>(&vector_base);
   DRAKE_DEMAND(vector != nullptr);
-  drake::lcmt_linear_car_state_t message;
+  drake::lcmt_idm_planner_input_t message;
   message.timestamp = static_cast<int64_t>(time * 1000);
   message.x = vector->x();
   message.v = vector->v();
@@ -30,17 +30,17 @@ void LinearCarStateTranslator::Serialize(
   message.encode(lcm_message_bytes->data(), 0, lcm_message_length);
 }
 
-void LinearCarStateTranslator::Deserialize(
+void IdmPlannerInputTranslator::Deserialize(
     const void* lcm_message_bytes, int lcm_message_length,
     systems::VectorBase<double>* vector_base) const {
   DRAKE_DEMAND(vector_base != nullptr);
-  auto* const my_vector = dynamic_cast<LinearCarState<double>*>(vector_base);
+  auto* const my_vector = dynamic_cast<IdmPlannerInput<double>*>(vector_base);
   DRAKE_DEMAND(my_vector != nullptr);
 
-  drake::lcmt_linear_car_state_t message;
+  drake::lcmt_idm_planner_input_t message;
   int status = message.decode(lcm_message_bytes, 0, lcm_message_length);
   if (status < 0) {
-    throw std::runtime_error("Failed to decode LCM message linear_car_state.");
+    throw std::runtime_error("Failed to decode LCM message idm_planner_input.");
   }
   my_vector->set_x(message.x);
   my_vector->set_v(message.v);
---
+++ drake/automotive/idm_planner.cc
@@ -0,0 +1,85 @@
+#include "drake/automotive/idm_planner.h"
+
+#include <algorithm>
+#include <cmath>
+
+#include <Eigen/Geometry>
+
+#include "drake/automotive/gen/idm_planner_parameters.h"
+#include "drake/common/drake_assert.h"
+#include "drake/common/drake_export.h"
+
+namespace drake {
+namespace automotive {
+
+template <typename T>
+IdmPlanner<T>::IdmPlanner(const T& v_0) : v_0_(v_0) {
+  this->DeclareInputPort(systems::kVectorValued,
+                          IdmPlannerInputIndices::kNumCoordinates,
+                          systems::kContinuousSampling);
+  this->DeclareInputPort(systems::kVectorValued,
+                          IdmPlannerInputIndices::kNumCoordinates,
+                          systems::kContinuousSampling);
+  this->DeclareOutputPort(systems::kVectorValued,
+                          1,
+                          systems::kContinuousSampling);
+}
+
+template <typename T>
+IdmPlanner<T>::~IdmPlanner() {}
+
+template <typename T>
+void IdmPlanner<T>::EvalOutput(
+    const systems::Context<T>& context,
+    systems::SystemOutput<T>* output) const {
+  DRAKE_ASSERT_VOID(systems::System<T>::CheckValidContext(context));
+  DRAKE_ASSERT_VOID(systems::System<T>::CheckValidOutput(output));
+
+  // Obtain the structures we need to read from and write into.
+  const IdmPlannerInput<T>* const input_ego =
+      dynamic_cast<const IdmPlannerInput<T>*>(
+                                    this->EvalVectorInput(context, 0));
+  const IdmPlannerInput<T>* const input_agent =
+      dynamic_cast<const IdmPlannerInput<T>*>(
+                                    this->EvalVectorInput(context, 1));
+  systems::BasicVector<T>* const output_vector =
+      output->GetMutableVectorData(0);
+  DRAKE_ASSERT(output_vector != nullptr);
+
+  // TODO: bake in David's parameters definition stuff.
+  IdmPlannerParameters<T> params;
+  params.set_a(T(1.0));  // max acceleration.
+  params.set_b(T(3.0));  // comfortable braking deceleration.
+  params.set_s_0(T(1.0));  // minimum desired net distance.
+  params.set_time_headway(T(0.1));  // desired time headway to vehicle in front.
+  params.set_delta(T(4.0));  // recommended choice of free-road exponent.
+  params.set_l_a(T(4.5));  // length of leading car.
+
+  const T& a = params.a();
+  const T& b = params.b();
+  const T& s_0 = params.s_0();
+  const T& time_headway = params.time_headway();
+  const T& delta = params.delta();
+  const T& l_a = params.l_a();
+
+  output_vector->SetAtIndex(0,
+      a * (1.0 - pow(input_ego->v() / v_0_, delta) -
+           pow((s_0 + input_ego->v() * time_headway +
+                input_ego->v() * (input_ego->v() - input_agent->v()) /
+                (2 * sqrt(a * b))) /
+               (input_agent->x() - input_ego->x() - l_a), 2.0)));
+
+  // TODO(david-german-tri): Remove this copy by allowing output ports to be
+  // mere pointers to state variables (or cache lines).
+  output_vector->get_mutable_value() =
+      context.get_continuous_state()->CopyToVector();
+}
+
+// These instantiations must match the API documentation in
+// idm_planner.h.
+template class DRAKE_EXPORT IdmPlanner<double>;
+template class DRAKE_EXPORT IdmPlanner<drake::TaylorVarXd>;
+  //template class DRAKE_EXPORT IdmPlanner<drake::symbolic::Expression>;
+
+}  // namespace automotive
+}  // namespace drake
