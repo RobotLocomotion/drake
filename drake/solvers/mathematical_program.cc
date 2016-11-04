7d7e9f8f9bba08137698b59c43f92663d13d06ed
hongkai-dai <daihongkai@gmail.com>
2016-11-04 11:19:58 -0400

    nlopt works

--- drake/solvers/mathematical_program.cc
+++ drake/solvers/mathematical_program.cc
@@ -64,7 +64,7 @@ MathematicalProgram::MathematicalProgram()
       solver_result_(0),
       required_capabilities_(kNoCapabilities),
       ipopt_solver_(new IpoptSolver()),
-      //nlopt_solver_(new NloptSolver()),
+      nlopt_solver_(new NloptSolver()),
       snopt_solver_(new SnoptSolver()),
       //moby_lcp_solver_(new MobyLCPSolver()),
       linear_system_solver_(new LinearSystemSolver()),
@@ -99,7 +99,7 @@ SolutionResult MathematicalProgram::Solve() {
   } /*else if (is_satisfied(required_capabilities_, kMobyLcpCapabilities) &&
              moby_lcp_solver_->available()) {
     return moby_lcp_solver_->Solve(*this);
-  } else if (is_satisfied(required_capabilities_, kGenericSolverCapabilities) &&
+  }*/ else if (is_satisfied(required_capabilities_, kGenericSolverCapabilities) &&
              snopt_solver_->available()) {
     return snopt_solver_->Solve(*this);
   } else if (is_satisfied(required_capabilities_, kGenericSolverCapabilities) &&
@@ -108,7 +108,7 @@ SolutionResult MathematicalProgram::Solve() {
   } else if (is_satisfied(required_capabilities_, kGenericSolverCapabilities) &&
              nlopt_solver_->available()) {
     return nlopt_solver_->Solve(*this);
-  }*/ else {
+  } else {
     throw std::runtime_error(
         "MathematicalProgram::Solve: "
         "No solver available for the given optimization problem!");
