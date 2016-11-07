9bcca1c9ac18727950fdd6aaaff41a2e1b4b5743
Jeremy Nimmer <jeremy.nimmer@tri.global>
2016-11-07 12:03:49 -0500

    Manually remove uses of DRAKE_EXPORT

--- drake/common/drake_path.h
+++ drake/common/drake_path.h
@@ -2,17 +2,16 @@
 
 #include <string>
 
-#include "drake/common/drake_export.h"
 #include "drake/common/drake_deprecated.h"
 
 namespace drake {
 
 /// Returns the fully-qualified path to the root of the `drake` source tree.
 /// N.B: <em>not</em> the `drake-distro` source tree.
-DRAKE_EXPORT std::string GetDrakePath();
+std::string GetDrakePath();
 
 /// Legacy compatibility alias for GetDrakePath.
 DRAKE_DEPRECATED("Use GetDrakePath instead")
-DRAKE_EXPORT std::string getDrakePath();
+std::string getDrakePath();
 
 }  // namespace drake
