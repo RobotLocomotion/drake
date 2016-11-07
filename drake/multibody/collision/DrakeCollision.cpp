0bdc6cee414a2563a7976fd17dc2072fffc7ba34
amcastro-tri <alejandro.castro@tri.global>
2016-11-07 09:40:43 -0500

    Moves directories collision, joints and shapes into multibody

---
+++ drake/multibody/collision/DrakeCollision.cpp
@@ -0,0 +1,23 @@
+#include "DrakeCollision.h"
+
+#ifdef BULLET_COLLISION
+#include "bullet_model.h"
+#endif
+#include "unusable_model.h"
+
+using std::unique_ptr;
+
+namespace DrakeCollision {
+
+const bitmask ALL_MASK(bitmask(0).set());
+const bitmask NONE_MASK(0);
+const bitmask DEFAULT_GROUP(1);
+
+unique_ptr<Model> newModel() {
+#ifdef BULLET_COLLISION
+  return unique_ptr<Model>(new BulletModel());
+#endif
+  return unique_ptr<Model>(new UnusableModel());
+}
+
+}  // namespace DrakeCollision
