0967eda5ccd1f35322a7b9a0cada51c6c07c8ed8
Evan Drumwright <drum@tri.global>
2016-11-08 14:05:19 -0800

    Workaround for clang related issue?

--- drake/systems/analysis/runge_kutta3_integrator.cc
+++ drake/systems/analysis/runge_kutta3_integrator.cc
@@ -2,7 +2,11 @@
 #include "drake/systems/analysis/runge_kutta3_integrator-inl.h"
 #include <unsupported/Eigen/AutoDiff>
 
-template class DRAKE_EXPORT drake::systems::RungeKutta3Integrator<double>;
+namespace drake {
+namespace systems {
+template class DRAKE_EXPORT RungeKutta3Integrator<double>;
+} // namespace drake
+} // namespace systems
 
 // TODO(edrumwri): correct compile bug from uncommenting line below.
 // template class DRAKE_EXPORT
