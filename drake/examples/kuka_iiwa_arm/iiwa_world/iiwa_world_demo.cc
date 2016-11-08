68a99ce18a753b47c2807a75ce32812e9ddec5fa
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-11-08 14:51:20 +0100

    error in adding to body

--- drake/examples/kuka_iiwa_arm/iiwa_world/iiwa_world_demo.cc
+++ drake/examples/kuka_iiwa_arm/iiwa_world/iiwa_world_demo.cc
@@ -13,23 +13,24 @@ namespace kuka_iiwa_arm {
 namespace {
 
 int main(int argc, char* argv[]) {
-  //  gflags::ParseCommandLineFlags(&argc, &argv, true);
-  //  logging::HandleSpdlogGflags();
-  //
-  //  // TODO(jwnimmer-tri) Allow for multiple simple cars.
-  //  if (FLAGS_num_simple_car > 1) {
-  //    std::cerr << "ERROR: Only one simple car is supported (for now)."
-  //              << std::endl;
-  //    return 1;
-  //  }
 
   auto simulator = std::make_unique<IiwaWorldSimulator<double>>();
   Eigen::Vector3d robot_base(-0.2, -0.2, 0.736);
 
 
-  simulator->AddObjectFixedToTable(robot_base,
-                                  Eigen::Vector3d::Zero() /* rpy */,
-                                  "iiwa");
+  simulator->AddObjectFixedToWorld(Eigen::Vector3d::Zero() /* xyz */,
+                                   Eigen::Vector3d::Zero() /* rpy */,
+                                   "table");
+
+  simulator->AddObjectFixedToWorld(robot_base,
+                                   Eigen::Vector3d::Zero() /* rpy */,
+                                   "iiwa");
+
+//  simulator->AddObjectFixedToTable(robot_base,
+//                                  Eigen::Vector3d::Zero() /* rpy */,
+//                                  "iiwa");
+
+
   Eigen::Vector3d cylinder_base(0.2, 0.2, 0);
   simulator->AddObjectFixedToWorld(cylinder_base,
                                   Eigen::Vector3d::Zero() /* rpy */,
