75e6d86bc022aa1e199684bc5ca65f0309196b85
amcastro-tri <alejandro.castro@tri.global>
2016-11-09 16:10:28 -0500

    Updates ros/drake_ros_systems

--- ros/drake_ros_systems/include/drake/ros/systems/ros_tf_publisher.h
+++ ros/drake_ros_systems/include/drake/ros/systems/ros_tf_publisher.h
@@ -13,7 +13,7 @@
 #include "drake/math/rotation_matrix.h"
 #include "drake/system1/System.h"
 #include "drake/multibody/KinematicsCache.h"
-#include "drake/multibody/RigidBodyTree.h"
+#include "drake/multibody/rigid_body_tree.h"
 #include "drake/multibody/rigid_body_system1/RigidBodySystem.h"
 #include "drake/system1/vector.h"
 
