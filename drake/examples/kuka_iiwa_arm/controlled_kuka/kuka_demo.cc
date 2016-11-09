4f25913c5ccfbeca1a68f3a9b6498adee62ab054
Lucy Gibson <lucy.gibson@tri.global>
2016-11-09 16:46:29 -0500

    Checklisting: better name for vars, Doxygen, etc.

--- drake/examples/kuka_iiwa_arm/controlled_kuka/kuka_demo.cc
+++ drake/examples/kuka_iiwa_arm/controlled_kuka/kuka_demo.cc
@@ -11,7 +11,6 @@
 #include "drake/multibody/parser_urdf.h"
 #include "drake/multibody/rigid_body_plant/drake_visualizer.h"
 #include "drake/multibody/rigid_body_plant/rigid_body_plant.h"
-
 #include "drake/systems/analysis/simulator.h"
 #include "drake/systems/controllers/gravity_compensator.h"
 #include "drake/systems/controllers/pid_controlled_system.h"
@@ -110,10 +109,9 @@ unique_ptr<PiecewisePolynomialTrajectory> MakePlan() {
   PostureConstraint pc3(&tree, Vector2d(6, 8));
   pc3.setJointLimits(joint_position_start_idx, Vector1d(0.7), Vector1d(0.8));
 
-  // TODO(naveenoid): Explain here in a comment why kNumTimesteps = 5.
-  const int kNumTimesteps = 5;
   const std::vector<double> t { 0.0, 2.0, 5.0, 7.0, 9.0 };
-  MatrixXd q0(tree.get_num_positions(), kNumTimesteps);
+  const int kNumTimesteps = t.size();
+  MatrixXd q0(tree.get_num_positions(), t.size());
   for (int i = 0; i < kNumTimesteps; ++i) {
     q0.col(i) = zero_conf;
   }
@@ -125,11 +123,11 @@ unique_ptr<PiecewisePolynomialTrajectory> MakePlan() {
   constraint_array.push_back(&pc3);
   constraint_array.push_back(&wpc2);
   IKoptions ikoptions(&tree);
-  int info[kNumTimesteps];
-  MatrixXd q_sol(tree.get_num_positions(), kNumTimesteps);
+  int info[t.size()];
+  MatrixXd q_sol(tree.get_num_positions(), t.size());
   std::vector<std::string> infeasible_constraint;
 
-  inverseKinPointwise(&tree, kNumTimesteps, &t[0], q0, q0,
+  inverseKinPointwise(&tree, t.size(), &t[0], q0, q0,
                       constraint_array.size(), constraint_array.data(),
                       ikoptions, &q_sol, info, &infeasible_constraint);
   bool info_good = true;
@@ -146,8 +144,7 @@ unique_ptr<PiecewisePolynomialTrajectory> MakePlan() {
         "inverseKinPointwise failed to compute a valid solution.");
   }
 
-  auto pp_traj = make_unique<PiecewisePolynomialTrajectory>(q_sol, t);
-  return pp_traj;
+  return make_unique<PiecewisePolynomialTrajectory>(q_sol, t);
 }
 
 // A model of a Kuka iiwa arm with position control using gravity compensation
--- drake/systems/trajectories/test/piecewise_polynomial_trajectory_test.cc
+++ drake/systems/trajectories/test/piecewise_polynomial_trajectory_test.cc
@@ -4,6 +4,8 @@
 
 #include "drake/systems/trajectories/PiecewisePolynomial.h"
 
+using Eigen::MatrixXd;
+
 namespace drake {
 namespace systems {
 namespace {
@@ -23,47 +25,47 @@ GTEST_TEST(piecewisePolynomialTrajectoryTest, testBasicFunctionality) {
   // Test constructing from a pp vector.
 
   // Create a PiecewisePolynomial from a vector.
-  const PiecewisePolynomialType ppFromVec(p_vec, times);
+  const PiecewisePolynomialType kPpFromVec(p_vec, times);
 
   // Create a PiecewisePolynomialTrajectory from a PP made from a vector.
-  const PiecewisePolynomialTrajectory ppTrajFromVec {ppFromVec};
-  EXPECT_EQ(ppTrajFromVec.rows(), 1);
-  EXPECT_EQ(ppTrajFromVec.cols(), 1);
+  const PiecewisePolynomialTrajectory kPpTrajFromVec {kPpFromVec};
+  EXPECT_EQ(kPpTrajFromVec.rows(), 1);
+  EXPECT_EQ(kPpTrajFromVec.cols(), 1);
   // Test first segment 0 <= t < 3, returning the constant a=3.
-  EXPECT_EQ(ppTrajFromVec.value(0.0).value(), 3);
-  EXPECT_EQ(ppTrajFromVec.value(2).value(), 3);
-  EXPECT_EQ(ppTrajFromVec.value(2.9).value(), 3);
+  EXPECT_EQ(kPpTrajFromVec.value(0.0).value(), 3);
+  EXPECT_EQ(kPpTrajFromVec.value(2).value(), 3);
+  EXPECT_EQ(kPpTrajFromVec.value(2.9).value(), 3);
   // Test second segment, 3 =< t < 7. Must subtract the knot point 3 when
   // evaluating the variable.
-  EXPECT_EQ(ppTrajFromVec.value(3).value(), y.EvaluateUnivariate(3 - 3));
-  EXPECT_EQ(ppTrajFromVec.value(4)(0), y.EvaluateUnivariate(4 - 3));
+  EXPECT_EQ(kPpTrajFromVec.value(3).value(), y.EvaluateUnivariate(3 - 3));
+  EXPECT_EQ(kPpTrajFromVec.value(4)(0), y.EvaluateUnivariate(4 - 3));
   // Test third segment, 7 =< t < 9. Must subtract the knot point 7 when
   // evaluating the variable.
-  EXPECT_EQ(ppTrajFromVec.value(7)(0), y2.EvaluateUnivariate(7 - 7));
-  EXPECT_EQ(ppTrajFromVec.value(8)(0), y2.EvaluateUnivariate(8 - 7));
-  EXPECT_EQ(ppTrajFromVec.value(8.9)(0), y2.EvaluateUnivariate(8.9 - 7));
+  EXPECT_EQ(kPpTrajFromVec.value(7)(0), y2.EvaluateUnivariate(7 - 7));
+  EXPECT_EQ(kPpTrajFromVec.value(8)(0), y2.EvaluateUnivariate(8 - 7));
+  EXPECT_EQ(kPpTrajFromVec.value(8.9)(0), y2.EvaluateUnivariate(8.9 - 7));
 
   // Test constructing from a pp matrix.
 
   // Construct a matrix of polynomials.
-  std::vector<PiecewisePolynomialType::PolynomialMatrix> ppMatrix(1);
-  ppMatrix[0].resize(3, 1);
-  ppMatrix[0](0) = a;
-  ppMatrix[0](1) = y;
-  ppMatrix[0](2) = y2;
+  std::vector<PiecewisePolynomialType::PolynomialMatrix> kPpMatrix(1);
+  kPpMatrix[0].resize(3, 1);
+  kPpMatrix[0](0) = a;
+  kPpMatrix[0](1) = y;
+  kPpMatrix[0](2) = y2;
 
   // Create a PiecewisePolynomial from a matrix.
-  const PiecewisePolynomialType ppFromMatrix(ppMatrix, {0.0, 3});
+  const PiecewisePolynomialType kPpFromMatrix(kPpMatrix, {0.0, 3});
 
   // Create a PiecewisePolynomialTrajectory from a PP made from a matrix.
-  const PiecewisePolynomialTrajectory pp_traj_from_pp_matrix {ppFromMatrix};
-  EXPECT_EQ(pp_traj_from_pp_matrix.rows(), 3);
-  EXPECT_EQ(pp_traj_from_pp_matrix.cols(), 1);
-  EXPECT_EQ(pp_traj_from_pp_matrix.value(2.9)(0), 3);
-  EXPECT_EQ(pp_traj_from_pp_matrix.value(2.9)(1), 2.9);  // y
-  EXPECT_EQ(pp_traj_from_pp_matrix.value(1)(2), 2);  // y2 = 2 * y
-  EXPECT_EQ(pp_traj_from_pp_matrix.value(2)(2), 4);
-  EXPECT_EQ(pp_traj_from_pp_matrix.value(3)(2), 6);
+  const PiecewisePolynomialTrajectory kPpTrajFromPpMatrix {kPpFromMatrix};
+  EXPECT_EQ(kPpTrajFromPpMatrix.rows(), 3);
+  EXPECT_EQ(kPpTrajFromPpMatrix.cols(), 1);
+  EXPECT_EQ(kPpTrajFromPpMatrix.value(2.9)(0), 3);
+  EXPECT_EQ(kPpTrajFromPpMatrix.value(2.9)(1), 2.9);  // y
+  EXPECT_EQ(kPpTrajFromPpMatrix.value(1)(2), 2);  // y2 = 2 * y
+  EXPECT_EQ(kPpTrajFromPpMatrix.value(2)(2), 4);
+  EXPECT_EQ(kPpTrajFromPpMatrix.value(3)(2), 6);
 
   // Test constructing from a matrix.
 
@@ -77,23 +79,23 @@ GTEST_TEST(piecewisePolynomialTrajectoryTest, testBasicFunctionality) {
       7, 6, 4, 2.1;
 
   // Create a PiecewisePolynomialTrajectory from a matrix.
-  const PiecewisePolynomialTrajectory pp_traj_from_matrix {traj_matrix, times};
-  EXPECT_EQ(pp_traj_from_matrix.rows(), kNumJoints);
-  EXPECT_EQ(pp_traj_from_matrix.cols(), 1);
+  const PiecewisePolynomialTrajectory kPpTrajFromMatrix {traj_matrix, times};
+  EXPECT_EQ(kPpTrajFromMatrix.rows(), kNumJoints);
+  EXPECT_EQ(kPpTrajFromMatrix.cols(), 1);
   // There is interpolation between points in the input matrix (traj_matrix),
   // but in this test we test just at the knot points (times).
   // The output from value() is a vector of length = kNumJoints.
-  EXPECT_EQ(pp_traj_from_matrix.value(times[0])(0), traj_matrix(0,0));
-  EXPECT_EQ(pp_traj_from_matrix.value(times[0])(1), traj_matrix(1,0));
-  EXPECT_EQ(pp_traj_from_matrix.value(times[0])(2), traj_matrix(2,0));
+  EXPECT_EQ(kPpTrajFromMatrix.value(times[0])(0), traj_matrix(0,0));
+  EXPECT_EQ(kPpTrajFromMatrix.value(times[0])(1), traj_matrix(1,0));
+  EXPECT_EQ(kPpTrajFromMatrix.value(times[0])(2), traj_matrix(2,0));
 
   // Don't bother testing every knot point; just boundaries.
-  EXPECT_EQ(pp_traj_from_matrix.value(times[1])(2), traj_matrix(2,1));
-  EXPECT_EQ(pp_traj_from_matrix.value(times[2])(2), traj_matrix(2,2));
+  EXPECT_EQ(kPpTrajFromMatrix.value(times[1])(2), traj_matrix(2,1));
+  EXPECT_EQ(kPpTrajFromMatrix.value(times[2])(2), traj_matrix(2,2));
 
-  EXPECT_EQ(pp_traj_from_matrix.value(times[3])(0), traj_matrix(0,3));
-  EXPECT_EQ(pp_traj_from_matrix.value(times[3])(1), traj_matrix(1,3));
-  EXPECT_EQ(pp_traj_from_matrix.value(times[3])(2), traj_matrix(2,3));
+  EXPECT_EQ(kPpTrajFromMatrix.value(times[3])(0), traj_matrix(0,3));
+  EXPECT_EQ(kPpTrajFromMatrix.value(times[3])(1), traj_matrix(1,3));
+  EXPECT_EQ(kPpTrajFromMatrix.value(times[3])(2), traj_matrix(2,3));
 }
 
 }  // namespace
