6f6d8b6cce470ab346c81100cf5eb5328a3da93c
amcastro-tri <alejandro.castro@tri.global>
2016-11-09 16:49:44 -0500

    Updates drake/ros to use kinematics_cache.h

--- ros/drake_ros_systems/include/drake/ros/systems/ros_sensor_publisher_odometry.h
+++ ros/drake_ros_systems/include/drake/ros/systems/ros_sensor_publisher_odometry.h
@@ -10,7 +10,7 @@
 #include "nav_msgs/Odometry.h"
 
 #include "drake/system1/System.h"
-#include "drake/multibody/KinematicsCache.h"
+#include "drake/multibody/kinematics_cache.h"
 #include "drake/multibody/rigid_body_tree.h"
 #include "drake/multibody/rigid_body_system1/RigidBodySystem.h"
 #include "drake/system1/vector.h"
