b0132330449897928f9babc0993829247aab5a93
Sean Curtis <sean.curtis@tri.global>
2016-11-09 13:48:50 -0800

    Missed cpplint error
    Duplicated include

--- drake/multibody/RigidBodyTree.h
+++ drake/multibody/RigidBodyTree.h
@@ -13,7 +13,6 @@
 #include "drake/common/drake_deprecated.h"
 #include "drake/common/drake_export.h"
 #include "drake/common/eigen_stl_types.h"
-#include "drake/common/drake_export.h"
 #include "drake/math/rotation_matrix.h"
 #include "drake/multibody/ForceTorqueMeasurement.h"
 #include "drake/multibody/KinematicPath.h"
