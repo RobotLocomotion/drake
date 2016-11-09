d5948ebe60bba314e7eb2166ba53cf23dcf630e0
Russ Tedrake <russt@mit.edu>
2016-11-08 21:43:24 -0500

    got chars and remote variables working.  added 2d surface plot example.

--- drake/lcm/lcm_call_matlab_client.cc
+++ drake/lcm/lcm_call_matlab_client.cc
@@ -1,61 +1,69 @@
+#include <mex.h>
 #include <cstring>
 #include <map>
-#include <mex.h>
 #include <vector>
 
 #include "lcm/lcm-cpp.hpp"
 
 #include "drake/lcmt_call_matlab.hpp"
 
-
 /// Mex client for matlab feval
 
+void flushMatlabEventBuffer(void) {
+  // an ugly hack.  drawnow used to work, but the internet confirms that it
+  // stopped working around R2015.
+  mexEvalString("pause(.001);");
+}
+
 class Handler {
-  // TODO(russt): Implement a destructor message (or string) for the local vars which calls mxDestroyArray
+  // TODO(russt): Implement a destructor message (or string) for the local vars
+  // which calls mxDestroyArray
 
-
-public:
+ public:
   ~Handler() {}
 
-  void handleMessage(const lcm::ReceiveBuffer* rbuf,
-                   const std::string& chan,
-                   const drake::lcmt_call_matlab* msg) {
+  void handleMessage(const lcm::ReceiveBuffer* rbuf, const std::string& chan,
+                     const drake::lcmt_call_matlab* msg) {
     int i;
-    std::vector<mxArray*> lhs(msg->nlhs), rhs(msg->nrhs);
+    std::vector<mxArray *> lhs(msg->nlhs), rhs(msg->nrhs);
 
     // Create the input arguments
-    for (i=0; i<msg->nrhs; i++) {
+    for (i = 0; i < msg->nrhs; i++) {
       switch (msg->rhs[i].type) {
-        case drake::lcmt_matlab_array::REMOTE_VARIABLE_REFERENCE:
-        {
+        case drake::lcmt_matlab_array::REMOTE_VARIABLE_REFERENCE: {
           int64_t id;
-          memcpy(&id,msg->rhs[i].data.data(),sizeof(int64_t));
+          memcpy(&id, msg->rhs[i].data.data(), sizeof(int64_t));
           if (client_vars_.find(id) == client_vars_.end()) {
-            mexWarnMsgTxt("rhs referenced unknown local variable.  dropping message.");
+            mexWarnMsgTxt(
+                "rhs referenced unknown local variable.  dropping message.");
             return;
           }
-          rhs[i] = client_vars_[id];
+          rhs[i] = client_vars_.at(id);
           break;
         }
-        case drake::lcmt_matlab_array::DOUBLE:
-        {
-          rhs[i] = mxCreateDoubleMatrix(msg->rhs[i].rows, msg->rhs[i].cols, mxREAL);
-          memcpy(mxGetPr(rhs[i]),msg->rhs[i].data.data(),sizeof(double) * msg->rhs[i].rows * msg->rhs[i].cols);
+        case drake::lcmt_matlab_array::DOUBLE: {
+          rhs[i] =
+              mxCreateDoubleMatrix(msg->rhs[i].rows, msg->rhs[i].cols, mxREAL);
+          memcpy(mxGetPr(rhs[i]), msg->rhs[i].data.data(),
+                 sizeof(double) * msg->rhs[i].rows * msg->rhs[i].cols);
           break;
         }
-        case drake::lcmt_matlab_array::CHAR:
-        {
+        case drake::lcmt_matlab_array::CHAR: {
           mwSize dims[2];
           dims[0] = msg->rhs[i].rows;
           dims[1] = msg->rhs[i].cols;
-          rhs[i] = mxCreateCharArray(2,dims);
-          memcpy(mxGetData(rhs[i]),msg->rhs[i].data.data(),sizeof(char) * dims[0] * dims[1]);
+          rhs[i] = mxCreateCharArray(2, dims);
+          // note: sizeof(mxChar) == 2.  doh!
+          mxChar* char_data = static_cast<mxChar*>(mxGetData(rhs[i]));
+          for (int j = 0; j < dims[0] * dims[1]; j++) {
+            char_data[j] = static_cast<mxChar>(msg->rhs[i].data[j]);
+          }
           break;
         }
-        case drake::lcmt_matlab_array::LOGICAL:
-        {
+        case drake::lcmt_matlab_array::LOGICAL: {
           rhs[i] = mxCreateLogicalMatrix(msg->rhs[i].rows, msg->rhs[i].cols);
-          memcpy(mxGetData(rhs[i]),msg->rhs[i].data.data(), msg->rhs[i].num_bytes);
+          memcpy(mxGetData(rhs[i]), msg->rhs[i].data.data(),
+                 msg->rhs[i].num_bytes);
           break;
         }
         default:
@@ -64,29 +72,31 @@ public:
       }
     }
 
-    // Find (or create) any local variables that will be returned by this call.
-    for (i=0; i<msg->nlhs; i++) {
-      if (client_vars_.find(msg->lhs[i]) == client_vars_.end()) { // then construct a new local variable
-        client_vars_[msg->lhs[i]] = nullptr;
-      }
-      lhs[i] = client_vars_[msg->lhs[i]];
-    }
-
     // Make the actual call to MATLAB.
     mexCallMATLAB(static_cast<int>(lhs.size()), lhs.data(),
                   static_cast<int>(rhs.size()), rhs.data(),
                   msg->function_name.c_str());
 
+    // Assign any local variables that were returned by this call.
+    for (i = 0; i < msg->nlhs; i++) {
+      // Note: feels like I'm leaking memory here if msg->lhs[i] exists, but
+      // https://www.mathworks.com/help/matlab/apiref/mxdestroyarray.html
+      // clearly states that you should not call it on left-side arguments
+      // (and in practice I get a segfault when I do).
+      client_vars_[msg->lhs[i]] = lhs[i];
+    }
+
     // Clean up the input argument data.
-    for (i=0; i<rhs.size(); i++) { mxDestroyArray(rhs[i]); }
+    for (i = 0; i < rhs.size(); i++) {
+      mxDestroyArray(rhs[i]);
+    }
   }
 
-private:
-  std::map<int64_t,mxArray*> client_vars_;
+ private:
+  std::map<int64_t, mxArray*> client_vars_;
 };
 
-
-void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
   lcm::LCM lcm;
   if (!lcm.good()) mexErrMsgTxt("Bad LCM reference.");
 
@@ -96,10 +106,15 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   lcm.subscribe(channel, &Handler::handleMessage, &handler_object);
 
   mexPrintf("Listening for messages on LCM_CALL_MATLAB...\n");
+  flushMatlabEventBuffer();
+
   while (1) {
-    if (lcm.handleTimeout(100) < 0) {
+    int n = lcm.handleTimeout(10);
+    if (n < 0) {
       mexErrMsgTxt("Something went wrong in lcm.handle.");
-    }
+    } else if (n == 0) {  // no messages this time, let matlab refresh
+      flushMatlabEventBuffer();
+    }  // else I got a message, so do nothing
   }
 
   mexPrintf("Cleaning up.\n");
--- drake/lcm/test/lcm_call_matlab_test.cc
+++ drake/lcm/test/lcm_call_matlab_test.cc
@@ -4,18 +4,53 @@
 
 #include "drake/lcm/lcm_call_matlab.h"
 
-// Test that linearizing an affine system returns the original A,B,C,D matrices.
+// Note: Unfortunately these really only test whether the code compiles and
+// runs... the actual output must currently be verified by human inspection in
+// the matlab window.
+
+GTEST_TEST(TestLcmCallMatlab, DispEigenMatrix) {
+  Eigen::Matrix2d m;
+  m << 1, 2, 3, 4;
+  lcm_call_matlab("disp", 0, m);
+}
+
+GTEST_TEST(TestLcmCallMatlab, RemoteVarTest) {
+  auto m = lcm_call_matlab("eye", 1, 2);
+  lcm_call_matlab("disp", 0, m[0]);  // should display eye(2)
+}
+
 GTEST_TEST(TestLcmCallMatlab, SimplePlot) {
   int N = 100;
 
   Eigen::VectorXd time(N), val(N);
-  for (int i=0; i<N; i++) {
-    time[i] = 0.01*i;
-    val[i] = sin(2*M_PI*time[i]);
+  for (int i = 0; i < N; i++) {
+    time[i] = 0.01 * i;
+    val[i] = sin(2 * M_PI * time[i]);
   }
 
-  auto h = lcm_call_matlab("plot",1,time,val);
-//  lcm_call_matlab("set",0,h[0],"Color","r");
-  lcm_call_matlab("disp",0,"Hello World!");
-  lcm_call_matlab("drawnow",0);
+  lcm_call_matlab("figure", 0, 1);
+  auto h = lcm_call_matlab("plot", 1, time, val);
+  lcm_call_matlab("set", 0, h[0], "Color", "r");
+}
+
+GTEST_TEST(TestLcmCallMatlab, MeshTest) {
+  const int N = 25, M = 35;
+  Eigen::VectorXd x(N), y(M);
+  Eigen::MatrixXd Z(M, N);
+  for (int i = 0; i < N; i++) {
+    x(i) = -3.0 + 6.0 * i / (N - 1);
+  }
+  for (int i = 0; i < M; i++) {
+    y(i) = -3.0 + 6.0 * i / (M - 1);
+  }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      Z(j, i) = 3 * pow(1 - x(i), 2) * exp(-pow(x(i), 2) - pow(y(j) + 1, 2)) -
+                10 * (x(i) / 5 - pow(x(i), 3) - pow(y(j), 5)) *
+                    exp(-pow(x(i), 2) - pow(y(j), 2)) -
+                1.0 / 3.0 * exp(-pow(x(i) + 1, 2) - pow(y(j), 2));
+    }
+  }
+  lcm_call_matlab("figure", 0, 2);
+  lcm_call_matlab("surf", 0, x, y, Z);
 }
