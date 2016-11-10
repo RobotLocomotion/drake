96c561634a23347a6f19f4392158b313002becd5
Evan Drumwright <drum@tri.global>
2016-11-10 10:26:40 -0800

    Updated documentation for kinematicscache

--- drake/multibody/kinematics_cache.h
+++ drake/multibody/kinematics_cache.h
@@ -15,8 +15,8 @@
 #include "drake/common/constants.h"
 #include "drake/common/drake_assert.h"
 #include "drake/common/eigen_types.h"
-#include "drake/multibody/rigid_body.h"
 #include "drake/multibody/joints/drake_joint.h"
+#include "drake/multibody/rigid_body.h"
 
 template <typename Scalar>
 class KinematicsCacheElement {
@@ -35,10 +35,12 @@ class KinematicsCacheElement {
       motion_subspace_in_world;  // gradient w.r.t. q
   Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, 0,
                 DrakeJoint::MAX_NUM_VELOCITIES,
-                DrakeJoint::MAX_NUM_POSITIONS> qdot_to_v;  // gradient w.r.t. q
+                DrakeJoint::MAX_NUM_POSITIONS>
+      qdot_to_v;  // gradient w.r.t. q
   Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, 0,
                 DrakeJoint::MAX_NUM_POSITIONS,
-                DrakeJoint::MAX_NUM_VELOCITIES> v_to_qdot;  // gradient w.r.t. q
+                DrakeJoint::MAX_NUM_VELOCITIES>
+      v_to_qdot;  // gradient w.r.t. q
   drake::SquareTwistMatrix<Scalar> inertia_in_world;
   drake::SquareTwistMatrix<Scalar> crb_in_world;
 
@@ -77,10 +79,11 @@ class KinematicsCache {
       RigidBodyKCacheElementPair;
   typedef Eigen::aligned_allocator<RigidBodyKCacheElementPair>
       RigidBodyKCacheElementPairAllocator;
-  typedef std::unordered_map<
-      RigidBody const*, KinematicsCacheElementScalar,
-      std::hash<RigidBody const*>, std::equal_to<RigidBody const*>,
-      RigidBodyKCacheElementPairAllocator> RigidBodyToKCacheElementMap;
+  typedef std::unordered_map<RigidBody const*, KinematicsCacheElementScalar,
+                             std::hash<RigidBody const*>,
+                             std::equal_to<RigidBody const*>,
+                             RigidBodyKCacheElementPairAllocator>
+      RigidBodyToKCacheElementMap;
 
   RigidBodyToKCacheElementMap elements;
   std::vector<RigidBody const*> bodies;
@@ -172,10 +175,13 @@ class KinematicsCache {
    * Converts a matrix B, which transforms generalized velocities to an
    * output space X, to a matrix A, which transforms the time
    * derivative of generalized coordinates to the same output X. For example,
-   * B could be a Jacobian that transforms generalized velocities to twists at
-   * the end-effector. This function would allow transforming that Jacobian
-   * so that all partial derivatives would be computed with respect to the
-   * time derivative of the generalized coordinates.
+   * B could be a Jacobian matrix that transforms generalized velocities to
+   * spatial velocities at the end-effector. Formally, this would be the
+   * matrix of partial derivatives of end-effector configuration computed with
+   * respect to quasi-coordinates. This function would allow
+   * transforming that Jacobian so that all partial derivatives would be
+   * computed with respect to the time derivative of the generalized
+   * coordinates.
    * @param A, a `n x nv` sized matrix, where `nv` is the dimension of the
    *      generalized velocities.
    * @returns B a `n x nq` sized matrix, where `nq` is the dimension of the
@@ -187,7 +193,8 @@ class KinematicsCache {
   transformVelocityMappingToPositionDotMapping(
       const Eigen::MatrixBase<Derived>& B) const {
     Eigen::Matrix<typename Derived::Scalar, Derived::RowsAtCompileTime,
-                  Eigen::Dynamic> A(B.rows(), get_num_positions());
+                  Eigen::Dynamic>
+        A(B.rows(), get_num_positions());
     int A_col_start = 0;
     int B_col_start = 0;
     for (auto it = bodies.begin(); it != bodies.end(); ++it) {
@@ -208,7 +215,14 @@ class KinematicsCache {
   /**
    * Converts a matrix A, which transforms the time derivative of generalized
    * coordinates to an output space X, to a matrix B, which transforms
-   * generalized velocities to the same space X.
+   * generalized velocities to the same space X. For example, A could be a
+   * Jacobian matrix that transforms the time derivatives of generalized
+   * coordiantes to spatial velocities at the end effector. Formally, this
+   * would be the matrix of partial derivatives of end-effector configuration
+   * computed with respect to generalized coordinates. This function would
+   * allow the user to transform this Jacobian matrix to the more commonly
+   * used one: the matrix of partial derivatives of end-effector configuration
+   * computed with respect to quasi-coordinates.
    * @param A a `n x nq` sized matrix, where `nq` is the dimension of the
    *      generalized coordinates.
    * @returns B, a `n x nv` sized matrix, where `nv` is the dimension of the
@@ -221,7 +235,8 @@ class KinematicsCache {
   transformPositionDotMappingToVelocityMapping(
       const Eigen::MatrixBase<Derived>& A) const {
     Eigen::Matrix<typename Derived::Scalar, Derived::RowsAtCompileTime,
-                  Eigen::Dynamic> B(A.rows(), get_num_velocities());
+                  Eigen::Dynamic>
+        B(A.rows(), get_num_velocities());
     int B_col_start = 0;
     int A_col_start = 0;
     for (auto it = bodies.begin(); it != bodies.end(); ++it) {
@@ -272,7 +287,7 @@ class KinematicsCache {
 
   int get_num_positions() const { return num_positions; }
 
-  // TODO(liang.fok): Remove this deprecated method prior to Release 1.0.
+// TODO(liang.fok): Remove this deprecated method prior to Release 1.0.
 #ifndef SWIG
   DRAKE_DEPRECATED("Please use get_num_positions().")
 #endif
@@ -280,7 +295,7 @@ class KinematicsCache {
 
   int get_num_velocities() const { return num_velocities; }
 
-  // TODO(liang.fok): Remove this deprecated method prior to Release 1.0.
+// TODO(liang.fok): Remove this deprecated method prior to Release 1.0.
 #ifndef SWIG
   DRAKE_DEPRECATED("Please use get_num_velocities().")
 #endif
