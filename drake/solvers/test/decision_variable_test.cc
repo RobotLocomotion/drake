5325168cfdfca9ea7c83dcf469e71b7f521662e1
hongkai-dai <daihongkai@gmail.com>
2016-11-11 12:17:23 -0800

    decision_variable_test now works

--- drake/solvers/test/decision_variable_test.cc
+++ drake/solvers/test/decision_variable_test.cc
@@ -16,21 +16,35 @@ GTEST_TEST(TestDecisionVariable, TestDecisionVariableValue) {
   EXPECT_EQ(prog.num_vars(), 12);
   auto x1 = prog.AddContinuousVariables(6, "x");
   EXPECT_EQ(prog.num_vars(), 18);
+  std::array<std::string, 6> X_name = {"X", "X", "X", "X", "X", "X"};
+  auto X2 = prog.AddVariables<2, 3>(DecisionVariableScalar::VarType::CONTINUOUS, X_name);
+  EXPECT_EQ(prog.num_vars(), 24);
   Eigen::Matrix<double, 6, 1> x_value;
   x_value << 0, 2, 4, 6, 8, 10;
   Eigen::Matrix<double, 6, 1> s_value;
   s_value << 0, -2, -4, -6, -8, -10;
   Eigen::Matrix<double, 3, 3> S_expected;
   S_expected << 0, -2, -4, -2, -6, -8, -4, -8, -10;
-  Eigen::Matrix<double, 18, 1> var_values;
-  var_values << x_value, s_value, x_value;
+  Eigen::Matrix<double, 24, 1> var_values;
+  var_values << x_value, s_value, x_value, x_value;
   prog.SetDecisionVariableValues(var_values);
   Eigen::MatrixXd X_expected = x_value;
   X_expected.resize(2, 3);
 
+
   DRAKE_ASSERT(CompareMatrices(DecisionVariableMatrixToDoubleMatrix(X1), X_expected, 1E-14, MatrixCompareType::absolute));
   DRAKE_ASSERT(CompareMatrices(DecisionVariableMatrixToDoubleMatrix(S1), S_expected, 1E-14, MatrixCompareType::absolute));
   DRAKE_ASSERT(CompareMatrices(DecisionVariableMatrixToDoubleMatrix(x1), x_value, 1E-14, MatrixCompareType::absolute));
+  DRAKE_ASSERT(CompareMatrices(DecisionVariableMatrixToDoubleMatrix(X2), X_expected, 1E-14, MatrixCompareType::absolute));
+
+  EXPECT_TRUE(VariableVectorContainsColumnVectorsOnly({x1}));
+  EXPECT_FALSE(VariableVectorContainsColumnVectorsOnly({X1, S1}));
+  for (int i = 0; i < 6; ++i) {
+    EXPECT_TRUE(DecisionVariableMatrixCoversIndex(X1, i));
+    EXPECT_TRUE(DecisionVariableMatrixCoversIndex(S1, i + 6));
+    EXPECT_TRUE(DecisionVariableMatrixCoversIndex(x1, i + 12));
+    EXPECT_TRUE(DecisionVariableMatrixCoversIndex(X2, i + 18));
+  }
 }
 }  // namespace solvers
 }  // namespace drake
