b1023d6393ecbd8893fb244c46d77881e6207212
Siyuan Feng <siyuan.feng@tri.global>
2016-11-17 14:26:54 -0500

    Add joint position and velocity getters to KinematicsResults, as well as methods for getting pose and twist with respect to world.
    clang-format.
    
    Make it possible to construct a KinematicsResults object without a RigidBodyPlant.
    
    Add port descriptor getters to RigidBodyPlant.
    
    Add RobotCommandToDesiredEffortConverter.
    
    Add RobotStateEncoder.
    
    Add ActuatorEffortToRigidBodyPlantInputConverter.
    
    Add valkyrie_simulation.
    
    Add RobotStateDecoder.
    
    Add RobotStateEncoder/RobotStateDecoder test.
    
    Add RobotCommandToDesiredEffortConverterTest.
    
    Address comment: add note.
    
    Address comment: handle case where robot_state_t message has state information for joints that RobotStateDecoder doesn't know about.
    
    Address comment: switch order of arguments.
    
    Address comment: remove commented out code.
    
    Address comment: make wrench indices static const members of RobotStateEncoder, use in test.
    
    Address comment: add method for twist in world aligned body frame.

---
+++ drake/examples/Valkyrie/robot_command_to_desired_effort_converter.cc
@@ -0,0 +1,84 @@
+#include "drake/examples/Valkyrie/robot_command_to_desired_effort_converter.h"
+
+#include "lcmtypes/bot_core/atlas_command_t.hpp"
+
+namespace drake {
+namespace systems {
+
+RobotCommandToDesiredEffortConverter::RobotCommandToDesiredEffortConverter(
+    const std::vector<const RigidBodyActuator*>& actuators)
+    : robot_command_port_index_(
+      DeclareAbstractInputPort(kContinuousSampling).get_index()),
+      desired_effort_port_indices_(DeclareDesiredEffortOutputPorts(actuators)),
+      name_to_actuator_(CreateNameToActuatorMap(actuators)) {
+  set_name("RobotCommandToDesiredEffortConverter");
+}
+
+void RobotCommandToDesiredEffortConverter::EvalOutput(
+    const systems::Context<double>& context,
+    systems::SystemOutput<double>* output) const {
+  using bot_core::atlas_command_t;
+
+  const AbstractValue* input =
+      EvalAbstractInput(context, robot_command_port_index_);
+  const atlas_command_t& message = input->GetValue<atlas_command_t>();
+
+  // Note: currently, all RigidBodyActuators are assumed to be one-dimensional.
+  // The following code is written based on that assumption.
+
+  // First, zero all the desired efforts to handle missing values in the
+  // robot_command message. Notably, this happens on the first few ticks of a
+  // simulation, before we've received a command message from the controller,
+  // in which case the subscriber system returns a default-constructed message
+  // (with empty std::vectors for the efforts and such).
+  for (const auto& actuator_and_port_index : desired_effort_port_indices_) {
+    int port_index = actuator_and_port_index.second;
+    output->get_mutable_port(port_index)
+        ->GetMutableVectorData<double>()
+        ->SetAtIndex(0, 0.0);
+  }
+
+  // Copy effort information from LCM message to appropriate output port.
+  for (size_t i = 0; i < message.joint_names.size(); i++) {
+    const std::string& joint_name = message.joint_names[i];
+    const double& effort = message.effort[i];
+    const RigidBodyActuator* actuator = name_to_actuator_.at(joint_name);
+    int port_index = desired_effort_port_indices_.at(actuator);
+    output->get_mutable_port(port_index)
+        ->GetMutableVectorData<double>()
+        ->SetAtIndex(0, effort);
+  }
+}
+
+const SystemPortDescriptor<double>&
+RobotCommandToDesiredEffortConverter::desired_effort_output_port(
+    const RigidBodyActuator& actuator) const {
+  return get_output_port(desired_effort_port_indices_.at(&actuator));
+}
+
+std::map<const RigidBodyActuator*, int>
+RobotCommandToDesiredEffortConverter::DeclareDesiredEffortOutputPorts(
+    const std::vector<const RigidBodyActuator*>& actuators) {
+  // Currently, all RigidBodyActuators are assumed to be one-dimensional.
+  const int desired_effort_length = 1;
+  std::map<const RigidBodyActuator*, int> ret;
+  for (const auto& actuator : actuators) {
+    ret[actuator] = DeclareOutputPort(kVectorValued, desired_effort_length,
+                                      kContinuousSampling)
+                        .get_index();
+  }
+  return ret;
+}
+
+std::map<std::string, const RigidBodyActuator*>
+RobotCommandToDesiredEffortConverter::CreateNameToActuatorMap(
+    const std::vector<const RigidBodyActuator*>& actuators) {
+  std::map<std::string, const RigidBodyActuator*> ret;
+  for (const auto& actuator : actuators) {
+    ret[actuator->name_] = actuator;
+  }
+  return ret;
+}
+
+}  // namespace systems
+}  // namespace drake
