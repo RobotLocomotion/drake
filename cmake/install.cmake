8a87f68adf9dd403eb8d23b48512f0255146038d
Jamie Snape <jamie.snape@kitware.com>
2016-11-18 13:08:03 -0500

    Fix documentation

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -10,23 +10,22 @@ set(DRAKE_LIBRARY_DIR "${CMAKE_INSTALL_PREFIX}/${DRAKE_INSTALL_LIBRARY_DIR}")
 set(DRAKE_PKGCONFIG_DIR "${CMAKE_INSTALL_PREFIX}/${DRAKE_INSTALL_PKGCONFIG_DIR}")
 set(DRAKE_RUNTIME_DIR "${CMAKE_INSTALL_PREFIX}/${DRAKE_INSTALL_RUNTIME_DIR}")
 
-
 #------------------------------------------------------------------------------
 # Install a list of runtime targets to the "bin" installation directory of the
 # drake project.
 #
-#   drake_install_executables(<targets>)
+#   drake_install_executables(<targets...>)
 #
-#   drake_install_executables(TARGETS <targets> [<ARGS>])
+#   drake_install_executables([<args...>] TARGETS <targets...>)
 #
 # Arguments:
-#   TARGETS
+#   TARGETS <targets...>
 #     List of runtime targets. Files installed by this function are given
 #     permissions OWNER_EXECUTE, OWNER_READ, OWNER_WRITE, GROUP_READ,
 #     GROUP_EXECUTE, WORLD_READ, and WORLD_EXECUTE.
-#   <ARGS>
+#
+#   <args...>
 #     Additional arguments to be passed through to install(TARGETS).
-
 #------------------------------------------------------------------------------
 function(drake_install_executables)
   cmake_parse_arguments("" "" "" "TARGETS" ${ARGN})
@@ -41,22 +40,22 @@ function(drake_install_executables)
     ${_UNPARSED_ARGUMENTS})
 endfunction()
 
-
 #------------------------------------------------------------------------------
 # Install a list of header files to the "include" installation directory of the
 # drake project.
 #
-#   drake_install_headers(<files>)
+#   drake_install_headers(<files...>)
 #
-#   drake_install_headers(FILES <files> [<ARGS>])
+#   drake_install_headers([<args...>] FILES <files...>)
 #
 # Arguments:
-#   FILES
+#   FILES <files...>
 #     List of header file names. File names given as relative paths are
 #     interpreted with respect to the current source directory. Files installed
 #     by this function are given permissions OWNER_READ, OWNER_WRITE, GROUP_READ,
 #     and WORLD_READ.
-#   <ARGS>
+#
+#   <args...>
 #     Additional arguments to be passed through to install(FILES).
 #------------------------------------------------------------------------------
 function(drake_install_headers)
@@ -75,39 +74,47 @@ function(drake_install_headers)
     DESTINATION "${DRAKE_INSTALL_INCLUDE_DIR}/drake/${_relative_path}")
 endfunction()
 
-
 #------------------------------------------------------------------------------
 # Generate and install a pkg-config .pc file for a package to the "pkgconfig"
 # subdirectory of the library installation directory ("lib", "lib32", or
 # "lib64") of the drake project.
 #
-# drake_install_pkg_config_file(<NAME> [TARGET <target>]
+# drake_install_pkg_config_file(<name> [TARGET <target>]
 #                               [DESCRIPTION <description>] [URL <url>]
 #                               [VERSION <major.minor.patch>]
-#                               [REQUIRES <packages>] [LIBS <flags>]
-#                               [CFLAGS <flags>] [CLASSPATH <classpath>])
+#                               [REQUIRES <packages...>]
+#                               [LIBS <flags...>] [CFLAGS <flags...>]
+#                               [CLASSPATH <paths...>])
 #
 # Arguments:
-#   <NAME>
+#   <name>
 #     Name of the pkg-config package. The generated pkg-config file will be
-#     named "<NAME>.pc".
-#   TARGET
+#     named "<name>.pc".
+#
+#   TARGET <target>
 #     Target associated with the pkg-config package. Reserved for future use.
-#   DESCRIPTION
+#
+#   DESCRIPTION <description>
 #     Description field of the package.
-#   URL
+#
+#   URL <url>
 #     URL field of the package or "http://drake.mit.edu/" if not specified.
-#   VERSION
-#     Version field of the package or PROJECT_VERSIION if not specified.
-#   REQUIRES
+#
+#   VERSION <major.minor.patch>
+#     Version field of the package or PROJECT_VERSION if not specified.
+#
+#   REQUIRES <packages...>
 #     Requires field of the package.
-#   LIBS
+#
+#   LIBS <flags...>
 #     Libs field of the package. The flag "-L${libdir}" is automatically
 #     prepended to the field.
-#   CFLAGS
+#
+#   CFLAGS <flags...>
 #     Cflags field of the package. The flag "-I${includedir}" automatically
 #     prepended to the field.
-#   CLASSPATH
+#
+#   CLASSPATH <paths...>
 #     Non-standard classpath field of the package.
 #
 # See the documentation of the "GNUInstallDirs" module for the rules that are
@@ -132,7 +139,7 @@ function(drake_install_pkg_config_file _NAME)
   endif()
 
   if(_CLASSPATH)
-    string(REPLACE ";" " " _classpath "${_CLASSPATH}")
+    string(REPLACE ";" ":" _classpath "${_CLASSPATH}")
     set(_classpath "classpath=${_classpath}")
   else()
     set(_classpath)
