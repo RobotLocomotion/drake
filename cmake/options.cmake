30fac3edf281438504cb00990d833ae18e5c9d9a
Jamie Snape <jamie.snape@kitware.com>
2016-11-21 16:59:39 -0500

    Re-alphabetize optional external options

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -214,32 +214,16 @@ macro(drake_setup_options)
 
   drake_optional_external(CCD ON "Convex shape Collision Detection library")
 
+  drake_optional_external(DIRECTOR ON
+    DEPENDS "HAVE_LCM\;HAVE_BOT_CORE_LCMTYPES"
+    "VTK-based visualization tool and robot user interface")
+
   drake_optional_external(GOOGLE_STYLEGUIDE ON
     DEPENDS "NOT DISABLE_PYTHON"
     "Google code style tools for cpplint.py style checking" ON)
 
   drake_optional_external(HSRB_DESCRIPTION ON
     "A ROS package containing a model of Toyota's HSR version B robot.")
-
-  drake_optional_external(SPDLOG ON
-    "Fast C++ text logging facility\; disabling will turn off text logging")
-
-  drake_optional_external(SWIGMAKE ON
-    DEPENDS "NOT DISABLE_MATLAB OR NOT DISABLE_PYTHON"
-    "Helper tools to build Python & MATLAB wrappers"
-    "for C++ libraries with Eigen")
-
-
-  drake_optional_external(DIRECTOR ON
-    DEPENDS "HAVE_LCM\;HAVE_BOT_CORE_LCMTYPES"
-    "VTK-based visualization tool and robot user interface")
-
-  # Probably not on Windows until lcmgl is split out
-  drake_optional_external(LIBBOT ON
-    "libbot2 robotics suite\;"
-    "used for its simple open-gl visualizer + lcmgl for director")
-
-  drake_optional_external(NLOPT ON "Non-linear optimization solver")
 
   # IPOPT is currently disabled on Mac when MATLAB is enabled due to MATLAB
   # compatibility issues:
@@ -248,12 +232,26 @@ macro(drake_setup_options)
     DEPENDS "NOT APPLE OR NOT Matlab_FOUND\;NOT DISABLE_FORTRAN"
     "Interior Point Optimizer, for solving non-linear optimizations")
 
+  drake_optional_external(LIBBOT ON
+    "libbot2 robotics suite\;"
+    "used for its simple open-gl visualizer + lcmgl for director")
+
+  drake_optional_external(NLOPT ON "Non-linear optimization solver")
+
   drake_optional_external(OCTOMAP ON
     "3D occupancy mapping library\; provides oct-tree data structures")
+
+  drake_optional_external(SPDLOG ON
+    "Fast C++ text logging facility\; disabling will turn off text logging")
 
   drake_optional_external(SWIG_MATLAB ON
     DEPENDS "NOT DISABLE_MATLAB OR NOT DISABLE_PYTHON"
     "A version of SWIG with MATLAB support")
+
+  drake_optional_external(SWIGMAKE ON
+    DEPENDS "NOT DISABLE_MATLAB OR NOT DISABLE_PYTHON\;WITH_SWIG_MATLAB"
+    "Helper tools to build Python & MATLAB wrappers"
+    "for C++ libraries with Eigen")
 
   # END external projects that are ON by default
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -284,13 +282,6 @@ macro(drake_setup_options)
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # BEGIN external projects that are OFF by default
 
-  drake_optional_external(SNOPT OFF
-    "Sparse Non-linear Optimizer\;"
-    "requires access to RobotLocomotion/snopt-pod")
-
-  drake_optional_external(SIGNALSCOPE OFF DEPENDS "NOT WIN32\;WITH_DIRECTOR"
-    "Live plotting tool for LCM messages")
-
   drake_optional_external(AVL OFF
     DEPENDS "NOT DISABLE_FORTRAN"
     "use w/ AVL to compute aerodynamic coefficients for airfoils")
@@ -299,8 +290,7 @@ macro(drake_setup_options)
     "Non-linear SMT solver and automated reasoning tool")
 
   drake_optional_external(GUROBI OFF
-    "Convex/integer optimization solver\;"
-    "free for academics (will prompt you for login bits)")
+    "Convex/integer optimization solver\; free for academics")
 
   drake_optional_external(MESHCONVERTERS OFF
     "uses vcglib to convert a few standard filetypes")
@@ -308,8 +298,13 @@ macro(drake_setup_options)
   drake_optional_external(MOSEK OFF
     "Convex optimization solver\; free for academics")
 
-  # Almost works on windows;  the update step call to git was complaining
-  # about local modifications on drake003
+  drake_optional_external(SIGNALSCOPE OFF DEPENDS "WITH_DIRECTOR"
+    "Live plotting tool for LCM messages")
+
+  drake_optional_external(SNOPT OFF
+    "Sparse Non-linear Optimizer\;"
+    "requires access to RobotLocomotion/snopt-pod")
+
   drake_optional_external(TEXTBOOK OFF
     "The Underactuated Robotics textbook and its examples")
 
