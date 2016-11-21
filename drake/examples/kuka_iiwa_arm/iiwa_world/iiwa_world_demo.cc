99bb91fa24e10f11d0b119872cacf0eec4fbbd48
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-11-21 09:20:15 +0100

    review comments addressed

--- drake/examples/kuka_iiwa_arm/iiwa_world/iiwa_world_demo.cc
+++ drake/examples/kuka_iiwa_arm/iiwa_world/iiwa_world_demo.cc
@@ -11,21 +11,28 @@ namespace {
 int main(int argc, char* argv[]) {
   auto iiwa_world = std::make_unique<IiwaWorldSimBuilder<double>>();
 
+  // Adding URDFs
+  iiwa_world->AddObjectUrdf("iiwa", "/examples/kuka_iiwa_arm/urdf/iiwa14.urdf");
+  iiwa_world->AddObjectUrdf(
+      "table",
+      "/examples/kuka_iiwa_arm/models/table/extra_heavy_duty_table.sdf");
+  iiwa_world->AddObjectUrdf(
+      "cylinder",
+      "/examples/kuka_iiwa_arm/models/objects/simple_cylinder.urdf");
+  iiwa_world->AddObjectUrdf(
+      "cuboid", "/examples/kuka_iiwa_arm/models/objects/simple_cuboid.urdf");
+
   iiwa_world->AddObjectFixedToWorld(Eigen::Vector3d::Zero() /* xyz */,
-                                   Eigen::Vector3d::Zero() /* rpy */, "table");
+                                    Eigen::Vector3d::Zero() /* rpy */, "table");
   iiwa_world->AddGroundToTree();
 
   iiwa_world->SetPenetrationContactParameters(4500 /* penetration_stiffness */,
-                                             1.0 /* penetration_damping */,
-                                             1.0 /* contact friction */);
-
+                                              1.0 /* penetration_damping */,
+                                              1.0 /* contact friction */);
   double table_top_z_in_world = 0.736 + 0.057 / 2;
-
   Eigen::Vector3d robot_base(-0.25, -0.75, table_top_z_in_world);
-
   iiwa_world->AddObjectFixedToWorld(robot_base,
-                                   Eigen::Vector3d::Zero() /* rpy */, "iiwa");
-
+                                    Eigen::Vector3d::Zero() /* rpy */, "iiwa");
   Eigen::Vector3d box_base(-0.45, -0.4, table_top_z_in_world + 0.15);
   Eigen::Vector3d cylinder_1_base(-0.5, -0.60, table_top_z_in_world + 0.1);
   Eigen::Vector3d cylinder_2_base(-0.05, -0.75, table_top_z_in_world + 0.1);
@@ -38,11 +45,11 @@ int main(int argc, char* argv[]) {
       box_base, Eigen::Vector3d::Zero() /* rpy */, "cuboid");
 
   auto diagram = iiwa_world->Build();
-//  auto simulator = std::make_unique<systems::Simulator<double>>(*diagram);
-//
-////  iiwa_world->SetZeroConfiguration(simulator.get(), diagram);
-//  simulator->Initialize();
-//  simulator->StepTo(3.5 /* final time */);
+  auto simulator = std::make_unique<systems::Simulator<double>>(*diagram);
+  iiwa_world->SetZeroConfiguration(simulator.get(), diagram.get());
+
+  simulator->Initialize();
+  simulator->StepTo(3.5 /* final time */);
 
   return 0;
 }
