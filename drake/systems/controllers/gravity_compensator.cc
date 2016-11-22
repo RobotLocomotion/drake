c07e7bf7773c0e677553879f64fb9b40b5a5a4f0
Chien-Liang Fok <liang.fok@tri.global>
2016-11-22 16:38:46 -0500

    Modify gravity compensator to throw an exception if the system is underactuated.

--- drake/systems/controllers/gravity_compensator.cc
+++ drake/systems/controllers/gravity_compensator.cc
@@ -8,12 +8,19 @@ namespace systems {
 
 template <typename T>
 GravityCompensator<T>::GravityCompensator(
-    const RigidBodyTree<T>& rigid_body_tree)
-    : rigid_body_tree_(rigid_body_tree) {
-  this->DeclareInputPort(kVectorValued, rigid_body_tree.get_num_positions(),
+    const RigidBodyTree<T>& tree)
+    : tree_(tree) {
+  this->DeclareInputPort(kVectorValued, tree.get_num_positions(),
                          kContinuousSampling);
-  this->DeclareOutputPort(kVectorValued, rigid_body_tree_.get_num_actuators(),
+  this->DeclareOutputPort(kVectorValued, tree_.get_num_actuators(),
                           kContinuousSampling);
+  if (tree.get_num_positions() != tree_.get_num_actuators()) {
+    std::stringstream msg;
+    msg << "ERROR: GravityCompensator: The model is under actuated!\n"
+        << "  - size of gravity vector: " << tree.get_num_positions() << "\n"
+        << "  - number of actuators: " << tree.get_num_actuators();
+    throw std::runtime_error(msg.string());
+  }
 }
 
 template <typename T>
@@ -24,33 +31,16 @@ void GravityCompensator<T>::EvalOutput(const Context<T>& context,
 
   Eigen::VectorXd q = this->EvalEigenVectorInput(context, 0);
 
-  KinematicsCache<T> cache = rigid_body_tree_.doKinematics(q);
+  KinematicsCache<T> cache = tree_.doKinematics(q);
   eigen_aligned_std_unordered_map<RigidBody<T> const*, drake::TwistVector<T>>
       f_ext;
   f_ext.clear();
 
-  Eigen::VectorXd g = rigid_body_tree_.dynamicsBiasTerm(
+  Eigen::VectorXd g = tree_.dynamicsBiasTerm(
       cache, f_ext, false /* include velocity terms */);
 
-  // The size of this system's output vector is equal to the number of
-  // actuators while the size of `g` is equal to the number of DOFs. Thus, we
-  // need to extract from `g` the torque / force commands corresponding to the
-  // actuators that are used.
-  Eigen::VectorXd actuated_g(rigid_body_tree_.get_num_actuators());
-  for (int i = 0; i < rigid_body_tree_.get_num_actuators(); ++i) {
-    // TODO(liang.fok) The assertion below enforces that all actuators are
-    // single DOF. Generalize this method to support multi-DOF actuators
-    // once they exist. See #4153.
-    DRAKE_ASSERT(rigid_body_tree_.actuators.at(i)
-                     .body_->getJoint()
-                     .get_num_positions() == 1);
-    int index_in_g =
-        rigid_body_tree_.actuators.at(i).body_->get_position_start_index();
-    actuated_g[i] = g[index_in_g];
-  }
-  DRAKE_ASSERT(actuated_g.size() ==
-               System<T>::GetMutableOutputVector(output, 0).size());
-  System<T>::GetMutableOutputVector(output, 0) = actuated_g;
+  DRAKE_ASSERT(g.size() == System<T>::GetMutableOutputVector(output, 0).size());
+  System<T>::GetMutableOutputVector(output, 0) = g;
 }
 
 template class GravityCompensator<double>;
--- drake/systems/controllers/test/gravity_compensator_test.cc
+++ drake/systems/controllers/test/gravity_compensator_test.cc
@@ -25,33 +25,14 @@ namespace drake {
 namespace systems {
 namespace {
 
-VectorXd ComputeIiwaGravityTorque(const RigidBodyTree<double>& rigid_body_tree,
+VectorXd ComputeGravityTorque(const RigidBodyTree<double>& rigid_body_tree,
                                   const VectorXd& robot_state) {
   KinematicsCache<double> cache = rigid_body_tree.doKinematics(robot_state);
   eigen_aligned_std_unordered_map<RigidBody<double> const*,
                                   drake::TwistVector<double>> f_ext;
   f_ext.clear();
 
-  Eigen::VectorXd g = rigid_body_tree.dynamicsBiasTerm(cache, f_ext, false);
-
-  // The size of this system's output vector is equal to the number of
-  // actuators while the size of `g` is equal to the number of DOFs. Thus, we
-  // need to extract from `g` the torque / force commands corresponding to the
-  // actuators that are used.
-  Eigen::VectorXd actuated_g(rigid_body_tree.get_num_actuators());
-  for (int i = 0; i < rigid_body_tree.get_num_actuators(); ++i) {
-    // TODO(liang.fok) The assertion below enforces that all actuators are
-    // single DOF. Generalize this method to support multi-DOF actuators
-    // once they exist. See #4153.
-    DRAKE_ASSERT(rigid_body_tree.actuators.at(i)
-                     .body_->getJoint()
-                     .get_num_positions() == 1);
-    int index_in_g =
-        rigid_body_tree.actuators.at(i).body_->get_position_start_index();
-    actuated_g[i] = g[index_in_g];
-  }
-
-  return actuated_g;
+  return rigid_body_tree.dynamicsBiasTerm(cache, f_ext, false);
 }
 
 template <class T>
@@ -99,19 +80,12 @@ class GravityCompensatorTest : public ::testing::Test {
 // value computed by the GravityCompensator for a given joint configuration
 // of the KUKA IIWA Arm are identical.
 TEST_F(GravityCompensatorTest, IiwaOutputTest) {
-  // The following curly brace defines a scope that quarantines local variable
-  // `tree`, which is of type `std::unique_ptr`. Ownership of `tree` is passed
-  // to this unit test's instance of `GravityCompensatorTest`. We quarantine
-  // this local variable to prevent downstream code from seg faulting by trying
-  // to access `tree` after ownership is transferred.
-  {
-    auto tree = std::make_unique<RigidBodyTree<double>>();
-    drake::parsers::urdf::AddModelInstanceFromUrdfFile(
-        drake::GetDrakePath() + "/examples/kuka_iiwa_arm/urdf/iiwa14.urdf",
-        drake::multibody::joints::kFixed, nullptr /* weld to frame */,
-        tree.get());
-    SetUp(std::move(tree));
-  }
+  auto tree = std::make_unique<RigidBodyTree<double>>();
+  drake::parsers::urdf::AddModelInstanceFromUrdfFile(
+      drake::GetDrakePath() + "/examples/kuka_iiwa_arm/urdf/iiwa14.urdf",
+      drake::multibody::joints::kFixed, nullptr /* weld to frame */,
+      tree.get());
+  SetUp(std::move(tree));
 
   // Defines an arbitrary robot position vector.
   Eigen::VectorXd robot_position = Eigen::VectorXd::Zero(7);
@@ -124,7 +98,7 @@ TEST_F(GravityCompensatorTest, IiwaOutputTest) {
   gravity_compensator_->EvalOutput(*context_, output_.get());
 
   VectorXd expected_gravity_vector =
-      ComputeIiwaGravityTorque(*tree_, robot_position);
+      ComputeGravityTorque(*tree_, robot_position);
 
   // Checks the expected and computed gravity torque.
   const BasicVector<double>* output_vector = output_->get_vector_data(0);
@@ -135,45 +109,12 @@ TEST_F(GravityCompensatorTest, IiwaOutputTest) {
 // value computed by the GravityCompensator for a given joint configuration
 // of an underactuated robot are identical.
 TEST_F(GravityCompensatorTest, UnderactuatedOutputTest) {
-  // The following curly brace defines a scope that quarantines local variable
-  // `tree`, which is of type `std::unique_ptr`. Ownership of `tree` is passed
-  // to this unit test's instance of `GravityCompensatorTest`. We quarantine
-  // this local variable to prevent downstream code from seg faulting by trying
-  // to access `tree` after ownership is transferred.
-  {
-    auto tree = std::make_unique<RigidBodyTree<double>>();
-    drake::parsers::sdf::AddModelInstancesFromSdfFile(
-        drake::GetDrakePath() + "/examples/SimpleFourBar/FourBar.sdf",
-        drake::multibody::joints::kFixed, nullptr /* weld to frame */,
-        tree.get());
-    SetUp(std::move(tree));
-  }
-
-  // Verifies that the model is indeed underactuated (it has 3 positions but
-  // only one actuator).
-  ASSERT_EQ(tree_->get_num_positions(), 3);
-  ASSERT_EQ(tree_->get_num_actuators(), 1);
-
-  // Defines an arbitrary robot position vector.
-  Eigen::VectorXd robot_position = Eigen::VectorXd::Zero(3);
-  robot_position << 0.01, -0.02, 0.05;
-
-  input_->get_mutable_value() << robot_position;
-
-  // Hook input of the expected size.
-  context_->SetInputPort(0, MakeInput(std::move(input_)));
-  gravity_compensator_->EvalOutput(*context_, output_.get());
-
-  const BasicVector<double>* output_vector = output_->get_vector_data(0);
-
-  // Checks that the output vector of the gravity compensator is of size 1
-  // (there's only one actuator).
-  ASSERT_EQ(output_vector->size(), 1);
-
-  // Checks the expected and computed gravity torque.
-  VectorXd expected_gravity_vector =
-      ComputeIiwaGravityTorque(*tree_, robot_position);
-  EXPECT_EQ(expected_gravity_vector, output_vector->get_value());
+  auto tree = std::make_unique<RigidBodyTree<double>>();
+  drake::parsers::sdf::AddModelInstancesFromSdfFile(
+      drake::GetDrakePath() + "/examples/SimpleFourBar/FourBar.sdf",
+      drake::multibody::joints::kFixed, nullptr /* weld to frame */,
+      tree.get());
+  EXPECT_THROW(SetUp(std::move(tree)), std::runtime_error);
 }
 
 }  // namespace
