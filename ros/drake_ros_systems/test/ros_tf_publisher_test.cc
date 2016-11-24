d8a3d0d5d6c6e10525803f2fe3830a623c72204c
Chien-Liang Fok <liang.fok@tri.global>
2016-11-24 09:12:12 -0500

    Added unit test for ros_tf_publisher.

--- ros/drake_ros_systems/test/ros_tf_publisher_test.cc
+++ ros/drake_ros_systems/test/ros_tf_publisher_test.cc
@@ -20,6 +20,13 @@ namespace systems {
 namespace test {
 namespace {
 
+// TODO(liang.fok) Unify with identical code in gravity_compensation_test.cc.
+template <class T>
+std::unique_ptr<FreestandingInputPort> MakeInput(
+    std::unique_ptr<BasicVector<T>> data) {
+  return make_unique<FreestandingInputPort>(std::move(data));
+}
+
 // Tests the RostfPublisher by instantiating it, making it publish a transform,
 // and verifying that the expected transform messages were transmitted.
 GTEST_TEST(RosTfPublisherTest, TestRosTfPublisher) {
@@ -34,25 +41,62 @@ GTEST_TEST(RosTfPublisherTest, TestRosTfPublisher) {
   auto input = make_unique<BasicVector<double>>(tree->get_num_positions() +
       tree->get_num_velocities());
 
-  std::cout << "num positions: " << tree->get_num_positions() << std::endl;
-  std::cout << "num velocities: " << tree->get_num_velocities() << std::endl;
-
-  for (int i = 0; i < tree->get_num_positions(); ++i) {
-    std::cout << "position " << i << ": " << tree->get_position_name(i) << std::endl;
-  }
-  for (int i = 0; i < tree->get_num_velocities(); ++i) {
-    std::cout << "velocity " << i << ": " << tree->get_velocity_name(i) << std::endl;
-  }
-
+  EXPECT_EQ(publisher->get_num_output_ports(), 0);
   EXPECT_EQ(input->size(), publisher->get_input_port(0).get_size());
   EXPECT_EQ(input->size(), 4);
-
-  std::cout << "size of input: " << input->size() << std::endl;
 
   Eigen::VectorXd robot_position = Eigen::VectorXd::Zero(input->size());
   robot_position << 1.0, -0.5, 0, 0;
 
   input->get_mutable_value() << robot_position;
+
+  context->SetInputPort(0, MakeInput(std::move(input)));
+  publisher->DoPublish(*context);
+
+  const std::map<std::string, std::unique_ptr<geometry_msgs::TransformStamped>>&
+      messages = publisher->get_transform_messages();
+
+  // Checks the transform message for the first body.
+  EXPECT_NE(messages.find("two_dof_robot0link1"), messages.end());
+  const geometry_msgs::TransformStamped* body1_message =
+      messages.at("two_dof_robot0link1").get();
+  EXPECT_EQ(body1_message->header.frame_id, "world");
+  EXPECT_EQ(body1_message->child_frame_id, "link1");
+  EXPECT_EQ(body1_message->transform.translation.x, 0);
+  EXPECT_EQ(body1_message->transform.translation.y, 0);
+  EXPECT_EQ(body1_message->transform.translation.z, 0);
+  EXPECT_EQ(body1_message->transform.rotation.w, 1);
+  EXPECT_EQ(body1_message->transform.rotation.x, 0);
+  EXPECT_EQ(body1_message->transform.rotation.y, 0);
+  EXPECT_EQ(body1_message->transform.rotation.z, 0);
+
+  // Checks the transform message for the second body.
+  EXPECT_NE(messages.find("two_dof_robot0link2"), messages.end());
+  const geometry_msgs::TransformStamped* body2_message =
+      messages.at("two_dof_robot0link2").get();
+  EXPECT_EQ(body2_message->header.frame_id, "link1");
+  EXPECT_EQ(body2_message->child_frame_id, "link2");
+  EXPECT_EQ(body2_message->transform.translation.x, 0);
+  EXPECT_EQ(body2_message->transform.translation.y, 0);
+  EXPECT_DOUBLE_EQ(body2_message->transform.translation.z, 0.6);
+  EXPECT_DOUBLE_EQ(body2_message->transform.rotation.w, 0.87758256189037265);
+  EXPECT_DOUBLE_EQ(body2_message->transform.rotation.x, 0.47942553860420295);
+  EXPECT_EQ(body2_message->transform.rotation.y, 0);
+  EXPECT_EQ(body2_message->transform.rotation.z, 0);
+
+  // Checks the transform message for the third body.
+  EXPECT_NE(messages.find("two_dof_robot0link3"), messages.end());
+  const geometry_msgs::TransformStamped* body3_message =
+      messages.at("two_dof_robot0link3").get();
+  EXPECT_EQ(body3_message->header.frame_id, "link2");
+  EXPECT_EQ(body3_message->child_frame_id, "link3");
+  EXPECT_EQ(body3_message->transform.translation.x, 0);
+  EXPECT_EQ(body3_message->transform.translation.y, 0);
+  EXPECT_DOUBLE_EQ(body3_message->transform.translation.z, 0.6);
+  EXPECT_DOUBLE_EQ(body3_message->transform.rotation.w, 0.96891242171064473);
+  EXPECT_DOUBLE_EQ(body3_message->transform.rotation.x, -0.24740395925452291);
+  EXPECT_EQ(body3_message->transform.rotation.y, 0);
+  EXPECT_EQ(body3_message->transform.rotation.z, 0);
 }
 
 }  // namespace
