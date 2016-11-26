5daaf1753d788a18d1de083a83ea2030ab9b6935
Chien-Liang Fok <liang.fok@tri.global>
2016-11-26 15:24:11 -0500

    Updated Diagram's unit test.

--- drake/examples/toyota_hsrb/test/hsrb_system_factories_test.cc
+++ drake/examples/toyota_hsrb/test/hsrb_system_factories_test.cc
@@ -83,7 +83,7 @@ GTEST_TEST(ToyotaHsrbTests, TestDiagramFactories) {
     ASSERT_NE(plant, nullptr);
 
     std::vector<const System<double>*> systems = dut1->GetSystems();
-    ASSERT_EQ(systems.size(), 2u);
+    EXPECT_EQ(systems.size(), 2u);
     const RigidBodyPlant<double>* rigid_body_plant =
         dynamic_cast<const RigidBodyPlant<double>*>(systems.at(0));
     const DrakeVisualizer* visualizer =
@@ -91,27 +91,27 @@ GTEST_TEST(ToyotaHsrbTests, TestDiagramFactories) {
     ASSERT_NE(rigid_body_plant, nullptr);
     ASSERT_NE(visualizer, nullptr);
 
-    // Verifies that the plant's output port zero is connected to the
-    // visualizer's sole input port.
     const Diagram<double>::PortIdentifier visualizer_input_port_id{visualizer,
                                                                    0};
     const Diagram<double>::PortIdentifier plant_input_port{rigid_body_plant, 0};
     const Diagram<double>::PortIdentifier plant_output_port_zero{
         rigid_body_plant, 0};
 
+    // Verifies that the plant's output port zero is connected to the
+    // visualizer's sole input port.
     ASSERT_TRUE(dut1->is_connected(visualizer_input_port_id));
-    ASSERT_EQ(dut1->get_connected(visualizer_input_port_id),
+    EXPECT_EQ(dut1->get_connected(visualizer_input_port_id),
               plant_output_port_zero);
 
     // Verifies that the Diagram has one input port and that it's connected to
     // the plant's input port.
-    ASSERT_EQ(dut1->get_num_input_ports(), 1);
-    ASSERT_TRUE(dut1->has_input(plant_input_port));
+    EXPECT_EQ(dut1->get_num_input_ports(), 1);
+    EXPECT_TRUE(dut1->has_input(plant_input_port));
 
     // Verifies that the plant's output port is connected to the Diagram's
     // output port.
-    ASSERT_EQ(dut1->get_num_output_ports(), 1);
-    ASSERT_TRUE(dut1->has_output(plant_output_port_zero));
+    EXPECT_EQ(dut1->get_num_output_ports(), 1);
+    EXPECT_TRUE(dut1->has_output(plant_output_port_zero));
 
     dut2 = CreateConstantSourceToPlantDiagram(*plant, std::move(dut1));
   }
@@ -119,20 +119,20 @@ GTEST_TEST(ToyotaHsrbTests, TestDiagramFactories) {
 
   // The DUT should not have any input ports since the plant's input port is
   // connected to a constant vector source, which has no inputs.
-  ASSERT_EQ(dut2->get_num_input_ports(), 0);
+  EXPECT_EQ(dut2->get_num_input_ports(), 0);
 
   // The DUT should have one output port that's connected to DUT #1's output
   // port zero.
-  ASSERT_EQ(dut2->get_num_output_ports(), 1);
+  EXPECT_EQ(dut2->get_num_output_ports(), 1);
   const Diagram<double>::PortIdentifier dut1_output_port_zero{dut1_ptr, 0};
-  ASSERT_TRUE(dut2->has_output(dut1_output_port_zero));
+  EXPECT_TRUE(dut2->has_output(dut1_output_port_zero));
 
   // 7 floating joint positions + 6 floating joint velocities + 2 joint
   // positions + 2 joint velocities = 17 states.
   const int kNumStates = 17;
 
-  ASSERT_EQ(dut2->get_output_port(0).get_size(), kNumStates);
-  ASSERT_EQ(plant->get_num_states(), kNumStates);
+  EXPECT_EQ(dut2->get_output_port(0).get_size(), kNumStates);
+  EXPECT_EQ(plant->get_num_states(), kNumStates);
 }
 
 }  // namespace
