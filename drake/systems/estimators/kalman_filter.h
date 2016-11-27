6cf6b6509884622b578f4657d040ff8f9a75b2f1
Russ Tedrake <russt@mit.edu>
2016-11-26 22:36:34 -0500

    adds the steady-state kalman filter + unit test

---
+++ drake/systems/estimators/kalman_filter.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <memory>
+
+#include <Eigen/Dense>
+
+#include "drake/systems/estimators/luenberger_observer.h"
+#include "drake/systems/framework/primitives/linear_system.h"
+
+namespace drake {
+namespace systems {
+namespace estimators {
+
+Eigen::MatrixXd
+SteadyStateKalmanFilter(const Eigen::Ref<const Eigen::MatrixXd> &A,
+                        const Eigen::Ref<const Eigen::MatrixXd> &C,
+                        const Eigen::Ref<const Eigen::MatrixXd> &W,
+                        const Eigen::Ref<const Eigen::MatrixXd> &V);
+
+std::unique_ptr<LuenbergerObserver<double>>
+SteadyStateKalmanFilter(std::unique_ptr<LinearSystem<double>> system,
+                        const Eigen::Ref<const Eigen::MatrixXd> &W,
+                        const Eigen::Ref<const Eigen::MatrixXd> &V);
+
+std::unique_ptr<LuenbergerObserver<double>>
+SteadyStateKalmanFilter(std::unique_ptr<System<double>> system,
+                        std::unique_ptr<Context<double>> context,
+                        const Eigen::Ref<const Eigen::MatrixXd> &W,
+                        const Eigen::Ref<const Eigen::MatrixXd> &V);
+
+} // namespace estimators
+} // namespace systems
+} // namespace drake
--- drake/systems/estimators/luenberger_observer.h
+++ drake/systems/estimators/luenberger_observer.h
@@ -5,6 +5,7 @@
 #include <Eigen/Dense>
 
 #include "drake/systems/framework/leaf_system.h"
+#include "drake/systems/framework/primitives/linear_system.h"
 
 namespace drake {
 namespace systems {
@@ -20,9 +21,8 @@ namespace estimators {
 /// The output of the observer system is @f$\hat{x}@f$.
 ///
 /// @ingroup estimator_systems
-template <typename T>
-class LuenbergerObserver : public systems::LeafSystem<T> {
- public:
+template <typename T> class LuenbergerObserver : public systems::LeafSystem<T> {
+public:
   /// @param observed_system  The forward model for the observer.
   /// @param observed_system_context Required because it may contain parameters
   /// which we need to evaluate the system.
@@ -35,27 +35,33 @@ class LuenbergerObserver : public systems::LeafSystem<T> {
   LuenbergerObserver(
       std::unique_ptr<systems::System<T>> observed_system,
       std::unique_ptr<systems::Context<T>> observed_system_context,
-      const Eigen::Ref<const Eigen::MatrixXd>& observer_gain);
+      const Eigen::Ref<const Eigen::MatrixXd> &observer_gain);
 
   /// Non-copyable.
-  LuenbergerObserver(const LuenbergerObserver<T>&) = delete;
-  LuenbergerObserver& operator=(const LuenbergerObserver<T>&) = delete;
+  LuenbergerObserver(const LuenbergerObserver<T> &) = delete;
+  LuenbergerObserver &operator=(const LuenbergerObserver<T> &) = delete;
 
   /// This system is not direct feedthrough.
   bool has_any_direct_feedthrough() const override { return false; }
 
   /// Advance the state estimate using forward dynamics and the observer gains.
-  void EvalTimeDerivatives(
-      const systems::Context<T>& context,
-      systems::ContinuousState<T>* derivatives) const override;
+  void
+  EvalTimeDerivatives(const systems::Context<T> &context,
+                      systems::ContinuousState<T> *derivatives) const override;
 
   /// Outputs the estimated state.
-  void EvalOutput(const systems::Context<T>& context,
-                  systems::SystemOutput<T>* output) const override;
+  void EvalOutput(const systems::Context<T> &context,
+                  systems::SystemOutput<T> *output) const override;
+  
+  /// Access to the observer gain
+  const Eigen::MatrixXd& observer_gain() { return observer_gain_; }
 
- private:
+  /// Provide access via the short-hand name, too.
+  const Eigen::MatrixXd& L() { return observer_gain_; }
+
+private:
   const std::unique_ptr<systems::System<T>> observed_system_;
-  const Eigen::MatrixXd observer_gain_;  // Gain matrix (often called "L").
+  const Eigen::MatrixXd observer_gain_; // Gain matrix (often called "L").
 
   // A (mutable) context is needed to efficiently call the observed system's
   // dynamics and output methods.  Does not add any undeclared state.  This
@@ -65,6 +71,7 @@ class LuenbergerObserver : public systems::LeafSystem<T> {
   const std::unique_ptr<systems::SystemOutput<T>> observed_system_output_;
 };
 
-}  // namespace estimators
-}  // namespace systems
-}  // namespace drake
+
+} // namespace estimators
+} // namespace systems
+} // namespace drake
