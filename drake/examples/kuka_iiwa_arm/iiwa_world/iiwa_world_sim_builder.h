a79d04c23302afc2cebff29c1e311db0f0acd8d2
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-11-28 14:22:08 +0100

    review comments addressed

--- drake/examples/kuka_iiwa_arm/iiwa_world/iiwa_world_sim_builder.h
+++ drake/examples/kuka_iiwa_arm/iiwa_world/iiwa_world_sim_builder.h
@@ -14,7 +14,7 @@ namespace drake {
 namespace examples {
 namespace kuka_iiwa_arm {
 
-/// A helper class to construct and run KUKA iiwa World simulations; i.e.
+/// A helper class to construct and run KUKA iiwa world simulations; i.e.
 /// a Simulation with a KUKA iiwa robot arm and various objects for
 /// it to manipulate.
 /// @tparam T must be a valid Eigen ScalarType.
@@ -29,30 +29,31 @@ class IiwaWorldSimBuilder {
 
   ~IiwaWorldSimBuilder();
 
-  /// Adds a fixed object specified by its name @p object_name to the
+  /// Adds a fixed object specified by its name, @p model_instance_name, to the
   /// `RigidBodyTree` at the pose specified by the position @p xyz and
   /// orientation @p rpy.
   ///
   /// @return model_instance_id of the object that is added.
-  int AddFixedObject(const std::string& object_name, const Eigen::Vector3d& xyz,
+  int AddFixedObject(const std::string& model_instance_name,
+                     const Eigen::Vector3d& xyz,
                      const Eigen::Vector3d& rpy = Eigen::Vector3d::Zero());
 
-  /// Adds a floating object specified by its name @p object_name to the
-  /// `RigidBodyTree` at the pose specified by the position @p xyz and
+  /// Adds a floating object specified by its name, @p model_instance_name, to
+  /// the `RigidBodyTree` at the pose specified by the position @p xyz and
   /// orientation @p rpy.
   ///
   /// @return model_instance_id of the object that is added.
-  int AddFloatingObject(const std::string& object_name,
+  int AddFloatingObject(const std::string& model_instance_name,
                         const Eigen::Vector3d& xyz,
                         const Eigen::Vector3d& rpy = Eigen::Vector3d::Zero());
 
-  /// Adds an object specified by its name @p object_name to the
+  /// Adds an object specified by its name, @p model_instance_name, to the
   /// `RigidBodyTree` at a pose specified by the position @p xyz and
   /// orientation @p rpy.
   ///
   /// @return model_instance_id of the object that is added.
   int AddObjectToFrame(
-      const std::string& object_name, const Eigen::Vector3d& xyz,
+      const std::string& model_instance_name, const Eigen::Vector3d& xyz,
       const Eigen::Vector3d& rpy, std::shared_ptr<RigidBodyFrame> weld_to_frame,
       const drake::multibody::joints::FloatingBaseType floating_base_type =
           drake::multibody::joints::kFixed);
@@ -63,7 +64,8 @@ class IiwaWorldSimBuilder {
   /// Builds a diagram composed of a `RigidBodyPlant` and `DrakeVisualizer`
   /// and returns it. The output of the RigidBodyPlant containing the
   /// plant's generalized state is connected to the input of the
-  /// DrakeVisualizer.
+  /// DrakeVisualizer. Once this method is called, this builder should be
+  /// discarded.
   std::unique_ptr<systems::Diagram<T>> Build();
 
   // TODO(naveenoid): Remove this method once issue #4191 is addressed.
@@ -84,22 +86,26 @@ class IiwaWorldSimBuilder {
 
   /// Allows the addition of objects to the iiwa World described by
   /// @p object_name coupled with URDF/SDF paths in @p urdf_sdf_path.
-  void AddObjectUrdf(const std::string& object_name,
-                     const std::string& urdf_sdf_path);
+  void AddModel(const std::string &object_name,
+                const std::string &model_path);
 
-  /// Returns the size of the input port for the plant being built in the
-  /// diagram.
+  /// Adds a model to the internal model database. Instances of these models
+  /// can then be added to the world via the various `AddFoo()` methods
+  /// provided by this class.
+  ///
+  /// @see AddObjectToFrame
+  /// @see AddFloatingObject
+  /// @see AddFixedObject
   int GetPlantInputSize(void);
 
  private:
-  // For both building and simulation.
   std::unique_ptr<RigidBodyTree<T>> rigid_body_tree_{
       std::make_unique<RigidBodyTree<T>>()};
   lcm::DrakeLcm lcm_;
   systems::RigidBodyPlant<T>* plant_{nullptr};
 
-  // Maps between objects loadable in the simulation and a convenient string
-  // name.
+  // Maps between model instances of which are loadable into the simulation
+  // and a convenient name.
   std::map<std::string, std::string> object_urdf_map_;
   bool built_{false};
 
