0721bfcab625af6132e09360cbf5dffe7ce8e6fa
Matt Marjanovic <maddog@tri.global>
2016-11-29 12:51:23 -0800

    Copy yaml-loading test code (and sample .yaml files) from drake-maliput's maliput/utility directory into maliput/monolane here.

---
+++ drake/automotive/maliput/monolane/test/yaml_load_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""
+"""
+
+import glob
+import subprocess
+import os
+import unittest
+import sys
+
+_THIS_FILE = os.path.abspath(__file__)
+_THIS_DIR = os.path.dirname(_THIS_FILE)
+_DRAKE_PATHS_DIR = os.path.dirname(os.path.dirname(os.path.dirname(_THIS_DIR)))
+
+sys.path.append(_DRAKE_PATHS_DIR)
+
+from drake_paths import DRAKE_DRAKE_BUILD_DIR
+
+
+class TestYamlLoading(unittest.TestCase):
+    def test_yaml_files(self):
+        yaml_dir = os.path.dirname(_THIS_DIR)
+        yaml_files = glob.glob(os.path.join(yaml_dir, '*.yaml'))
+        blacklist = []
+        test_yaml_files = [f for f in yaml_files
+                           if not any([b in f for b in blacklist])]
+        assert len(test_yaml_files) > 0
+        for yf in test_yaml_files:
+            subprocess.check_call([
+                os.path.join(DRAKE_DRAKE_BUILD_DIR,
+                             "automotive/maliput/monolane/test/yaml_load"),
+                "-yaml_file", yf,
+            ])
+
+
+if __name__ == '__main__':
+    unittest.main()
