740f95273054b0dfe3f7bd4d4d06e46c98d36cb1
Chien-Liang Fok <liang.fok@tri.global>
2016-11-29 22:04:34 -0500

    Fixed bug in urdf_parser that resulted in RPY floating joint always being used. (#4248)
    * Fixed bug in urdf_parser that resulted in RPY floating joint always being used.
    
    * Merge branch 'master' of github.com:RobotLocomotion/drake into feature/urdf_parser_bug_fix
    
    * Added unit test to catch the scenario
    
    * Merge branch 'master' of github.com:RobotLocomotion/drake into feature/urdf_parser_bug_fix
    
    * Removed #include <memory>.

--- drake/multibody/test/parser_urdf_test/parser_urdf_test.cc
+++ drake/multibody/test/parser_urdf_test/parser_urdf_test.cc
@@ -1,19 +1,29 @@
+#include "drake/multibody/parser_urdf.h"
+
 #include <iostream>
+#include <fstream>
+#include <memory>
+#include <string>
 
 #include <gtest/gtest.h>
-#include <memory>
 
 #include "drake/common/drake_path.h"
-#include "drake/multibody/parser_urdf.h"
+#include "drake/multibody/joints/floating_base_types.h"
 #include "drake/multibody/rigid_body_tree.h"
 
+using std::endl;
+using std::ifstream;
 using std::make_unique;
+using std::string;
+using std::stringstream;
 using std::unique_ptr;
 
 namespace drake {
 
+using multibody::joints::kQuaternion;
 using parsers::urdf::AddModelInstanceFromUrdfFileWithRpyJointToWorld;
 using parsers::urdf::AddModelInstanceFromUrdfStringWithRpyJointToWorld;
+using parsers::urdf::AddModelInstanceFromUrdfStringSearchingInRosPackages;
 
 namespace systems {
 namespace plants {
@@ -22,7 +32,7 @@ namespace {
 
 GTEST_TEST(URDFParserTest, ParseJointProperties) {
   // Defines a URDF string describing a robot with two links and one joint.
-  std::string urdf_string =
+  string urdf_string =
       "<?xml version=\"1.0\" ?>"
       "<robot name=\"my_robot\" xmlns:xacro=\"http://www.ros.org/wiki/xacro\">"
       "  <link name=\"bar\">"
@@ -74,7 +84,7 @@ GTEST_TEST(URDFParserTest, ParseJointProperties) {
   EXPECT_EQ(foo_joint.get_num_velocities(), 1);
 
   // Obtains a reference to foo_transmission and verifies its parameters.
-  const std::string actuator_name = "foo_motor";
+  const string actuator_name = "foo_motor";
   const RigidBodyActuator& foo_actuator = tree->GetActuator(actuator_name);
 
   EXPECT_EQ(foo_actuator.effort_limit_min_, -123);
@@ -83,15 +93,15 @@ GTEST_TEST(URDFParserTest, ParseJointProperties) {
 }
 
 GTEST_TEST(URDFParserTest, TestParseMaterial) {
-  const std::string file_no_conflict_1 = drake::GetDrakePath() +
+  const string file_no_conflict_1 = drake::GetDrakePath() +
       "/multibody/test/parser_urdf_test/non_conflicting_materials_1.urdf";
-  const std::string file_no_conflict_2 = drake::GetDrakePath() +
+  const string file_no_conflict_2 = drake::GetDrakePath() +
       "/multibody/test/parser_urdf_test/non_conflicting_materials_2.urdf";
-  const std::string file_no_conflict_3 = drake::GetDrakePath() +
+  const string file_no_conflict_3 = drake::GetDrakePath() +
       "/multibody/test/parser_urdf_test/non_conflicting_materials_3.urdf";
-  const std::string file_duplicate = drake::GetDrakePath() +
+  const string file_duplicate = drake::GetDrakePath() +
       "/multibody/test/parser_urdf_test/duplicate_materials.urdf";
-  const std::string file_conflict = drake::GetDrakePath() +
+  const string file_conflict = drake::GetDrakePath() +
       "/multibody/test/parser_urdf_test/conflicting_materials.urdf";
 
   auto tree = make_unique<RigidBodyTree<double>>();
@@ -115,11 +125,42 @@ GTEST_TEST(URDFParserTest, TestParseMaterial) {
       file_conflict, tree.get()), ".*");
 
   // This URDF defines the same color multiple times in different links.
-  const std::string file_same_color_diff_links = drake::GetDrakePath() +
+  const string file_same_color_diff_links = drake::GetDrakePath() +
       "/multibody/test/parser_urdf_test/duplicate_but_same_materials.urdf";
   tree = make_unique<RigidBodyTree<double>>();
   EXPECT_NO_THROW(AddModelInstanceFromUrdfFileWithRpyJointToWorld(
       file_same_color_diff_links, tree.get()));
+}
+
+string ReadTextFile(const string& file) {
+  ifstream text_file(file);
+  DRAKE_DEMAND(text_file.is_open());
+
+  stringstream buffer;
+  string line;
+  while (getline(text_file, line)) {
+    buffer << line << endl;
+  }
+  text_file.close();
+  return buffer.str();
+}
+
+// Tests that a URDF string can be loaded using a quaternion floating joint.
+// This was added as a result of #4248.
+GTEST_TEST(URDFParserTest, TestAddWithQuaternionFloatingDof) {
+  const string model_file = drake::GetDrakePath() +
+      "/multibody/test/parser_urdf_test/zero_dof_robot.urdf";
+
+  const string model_string = ReadTextFile(model_file);
+  PackageMap ros_package_map;
+
+  auto tree = make_unique<RigidBodyTree<double>>();
+  EXPECT_NO_THROW(AddModelInstanceFromUrdfStringSearchingInRosPackages(
+      model_string, ros_package_map, "." /* root_dir */, kQuaternion,
+      nullptr /* weld_to_frame */, tree.get()));
+
+  EXPECT_EQ(tree->get_num_positions(), 7);
+  EXPECT_EQ(tree->get_num_velocities(), 6);
 }
 
 }  // namespace
