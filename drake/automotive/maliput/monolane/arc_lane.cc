6021acdb49753cebfcd360fc9fbc5678e7fae5d7
Matt Marjanovic <maddog@tri.global>
2016-12-01 14:49:27 -0800

    Clean up of LineLane and ArcLane.

--- drake/automotive/maliput/monolane/arc_lane.cc
+++ drake/automotive/maliput/monolane/arc_lane.cc
@@ -8,27 +8,32 @@ namespace drake {
 namespace maliput {
 namespace monolane {
 
-V2 ArcLane::xy_of_p_(const double p) const {
-  const double theta = theta0_ + (p * d_theta_);
+double ArcLane::theta_of_p(const double p) const {
+  return theta0_ + (p * d_theta_);
+}
+
+
+V2 ArcLane::xy_of_p(const double p) const {
+  const double theta = theta_of_p(p);
   return V2(cx_ + (r_ * std::cos(theta)),
             cy_ + (r_ * std::sin(theta)));
 }
 
 
-V2 ArcLane::xy_dot_of_p_(const double p) const {
-  const double theta = theta0_ + (p * d_theta_);
+V2 ArcLane::xy_dot_of_p(const double p) const {
+  const double theta = theta_of_p(p);
   return V2(-r_ * d_theta_ * std::sin(theta),
             r_ * d_theta_ * std::cos(theta));
 }
 
 
-double ArcLane::heading_of_p_(const double p) const {
-  const double theta = theta0_ + (p * d_theta_);
+double ArcLane::heading_of_p(const double p) const {
+  const double theta = theta_of_p(p);
   return theta + std::copysign(M_PI / 2.0, d_theta_);
 }
 
 
-double ArcLane::heading_dot_of_p_(const double p) const {
+double ArcLane::heading_dot_of_p(const double p) const {
   // TODO(maddog)  Does this still hold if r exceeds radius r_, e.g. if r
   //               approaches/crosses the singularity at center of arc?
   return d_theta_;
