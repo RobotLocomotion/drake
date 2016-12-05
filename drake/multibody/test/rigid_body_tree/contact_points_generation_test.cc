381bef2fdd43eb2f64844838270ed9cbfe74050e
Sean Curtis <sean.curtis@tri.global>
2016-12-05 15:08:26 -0800

    Test group filtering of contact points
    Provides a test that confirms the group name filtering works for contact
    points.

--- drake/multibody/test/rigid_body_tree/contact_points_generation_test.cc
+++ drake/multibody/test/rigid_body_tree/contact_points_generation_test.cc
@@ -54,6 +54,8 @@ class RBTContactPointsTest : public ::testing::Test {
   Eigen::Matrix3Xd expected_points_;
 };
 
+// Infrastructure for testing the *types* of geometries which create contact
+// points.
 class ContactGenerationTest : public RBTContactPointsTest {
  public:
   std::string get_urdf_file() const override {
@@ -80,6 +82,53 @@ TEST_F(ContactGenerationTest, GeneratingElementsTest) {
   const auto& contact_points = body->get_contact_points();
   ASSERT_EQ(expected_points_.cols(), contact_points.cols());
   ASSERT_TRUE(expected_points_.isApprox(contact_points));
+}
+
+// Infrastructure for testing the the group name filters for contact points.
+class ContactGroupNameTest : public RBTContactPointsTest {
+ public:
+  std::string get_urdf_file() const override {
+    return "/multibody/test/rigid_body_tree/contact_points_group_name.urdf";
+  }
+
+  void initialize_expected_points() override {
+    const int kPointCount = 4;
+    expected_points_.resize(Eigen::NoChange, kPointCount);
+    Eigen::RowVectorXd cx(kPointCount), cy(kPointCount), cz(kPointCount);
+    // There are four spheres place along the x-axis at 0, 1, 2, & 3,
+    // respectively.  The first is in group "zero", the second in "one", and
+    // the last *two* in "two".
+    cx << 0, 1, 2, 3;
+    cy << 0, 0, 0, 0;
+    cz << 0, 0, 0, 0;
+    expected_points_ << cx, cy, cz;
+  }
+};
+
+// This tests the case where only the right types of collision elements create
+// contact points.
+TEST_F(ContactGroupNameTest, GroupNameContactPointsTest) {
+  RigidBody<double>* body = tree_.FindBody("body");
+
+  // Test the default case; should get three points, one for each small sphere.
+  const auto& contact_points = body->get_contact_points();
+  ASSERT_EQ(expected_points_.cols(), contact_points.cols());
+  EXPECT_TRUE(expected_points_.isApprox(contact_points));
+
+  // Tests for individual groups.
+  Eigen::Matrix3Xd group_points;
+
+  tree_.getTerrainContactPoints(*body, &group_points, "zero");
+  ASSERT_EQ(1, group_points.cols());
+  EXPECT_TRUE(expected_points_.col(0).isApprox(group_points));
+
+  tree_.getTerrainContactPoints(*body, &group_points, "one");
+  ASSERT_EQ(1, group_points.cols());
+  EXPECT_TRUE(expected_points_.col(1).isApprox(group_points));
+
+  tree_.getTerrainContactPoints(*body, &group_points, "two");
+  ASSERT_EQ(2, group_points.cols());
+  EXPECT_TRUE(expected_points_.block(0,2,3,2).isApprox(group_points));
 }
 
 //GTEST_TEST()
