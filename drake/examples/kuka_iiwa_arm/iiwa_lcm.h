ebac19bfed6dc5f917f445152b1a8f6ab9ab5f7c
Sam Creasey <sam.creasey@tri.global>
2016-12-05 15:44:55 -0500

    Move the iiwa lcm handlers into the common library.

---
+++ drake/examples/kuka_iiwa_arm/iiwa_lcm.h
@@ -0,0 +1,50 @@
+#pragma once
+
+/// @file This file contains classes dealing with sending/receiving
+/// LCM messages related to the iiwa arm.
+
+#include "drake/systems/framework/leaf_system.h"
+
+namespace drake {
+namespace examples {
+namespace kuka_iiwa_arm {
+
+/// Handles lcmt_iiwa_command messages from a LcmSubscriberSystem.
+/// Has a single output port which publishes the commanded position
+/// for each joint.
+class IiwaCommandReceiver : public systems::LeafSystem<double> {
+ public:
+  explicit IiwaCommandReceiver(int num_joints);
+
+  void EvalOutput(const systems::Context<double>& context,
+                  systems::SystemOutput<double>* output) const override;
+};
+
+/// Sends lcmt_iiwa_status messages.  This system has two input ports,
+/// one for the current state of the plant and one for the most
+/// recently received command.
+class IiwaStatusSender : public systems::LeafSystem<double> {
+ public:
+  explicit IiwaStatusSender(int num_joints);
+
+  const systems::SystemPortDescriptor<double>& get_command_input_port() const {
+    return this->get_input_port(0);
+  }
+
+  const systems::SystemPortDescriptor<double>& get_state_input_port() const {
+    return this->get_input_port(1);
+  }
+
+  std::unique_ptr<systems::SystemOutput<double>> AllocateOutput(
+      const systems::Context<double>& context) const override;
+
+  void EvalOutput(const systems::Context<double>& context,
+                  systems::SystemOutput<double>* output) const override;
+
+ private:
+  int num_joints_{0};
+};
+
+}  // namespace kuka_iiwa_arm
+}  // namespace examples
+}  // namespace drake
