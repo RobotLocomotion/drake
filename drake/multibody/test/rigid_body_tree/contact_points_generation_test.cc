647991207441b2e2d628f8692c73948244227017
Sean Curtis <sean.curtis@tri.global>
2016-12-05 14:44:54 -0800

    Testing which geometries create contact points
    THe first test confirms that only a two conditions will produce contact
    points.  Very small spheres and boxes (with special testing for evaluating
    the definition of "small".)

--- drake/multibody/test/rigid_body_tree/contact_points_generation_test.cc
+++ drake/multibody/test/rigid_body_tree/contact_points_generation_test.cc
@@ -2,6 +2,7 @@
 
 #include <string>
 
+#include <Eigen/Dense>
 #include <gtest/gtest.h>
 
 #include "drake/common/drake_path.h"
@@ -33,28 +34,52 @@ class RBTContactPointsTest : public ::testing::Test {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
+  // Populates an ordered set of expected contact points.  The number and order
+  // of the values are directly related to the structure of the URDF.  A change
+  // in the URDF *may* require a change in these values.
+  virtual void initialize_expected_points() = 0;
+
   // Returns the drake-relative path to the test urdf file.
   virtual std::string get_urdf_file() const = 0;
+
  protected:
   void SetUp() override {
-    drake::parsers::urdf::AddModelInstanceFromUrdfFile(
+    initialize_expected_points();
+    drake::parsers::urdf::AddModelInstanceFromUrdfFileToWorld(
         drake::GetDrakePath() + get_urdf_file(),
         drake::multibody::joints::kQuaternion, &tree_);
   }
 
   RigidBodyTree<double> tree_;
+  Eigen::Matrix3Xd expected_points_;
 };
 
 class ContactGenerationTest : public RBTContactPointsTest {
  public:
-  std::string get_urdef_file() const override {
+  std::string get_urdf_file() const override {
     return "/multibody/test/rigid_body_tree/contact_points_generation.urdf";
+  }
+
+  void initialize_expected_points() override {
+    const int kPointCount = 9;
+    expected_points_.resize(Eigen::NoChange, kPointCount);
+    Eigen::RowVectorXd cx(kPointCount), cy(kPointCount), cz(kPointCount);
+    // The first eight columns represent the box's corners, the ninth is the
+    // sphere.
+    cx << -1, 1, 1, -1, -1, 1, 1, -1, -1;
+    cy << 1, 1, 1, 1, -1, -1, -1, -1, 0;
+    cz << 1, 1, -1, -1, -1, -1, 1, 1, 0;
+    expected_points_ << cx, cy, cz;
   }
 };
 
 // This tests the case where only the right types of collision elements create
 // contact points.
 TEST_F(ContactGenerationTest, GeneratingElementsTest) {
+  RigidBody<double>* body = tree_.FindBody("body");
+  const auto& contact_points = body->get_contact_points();
+  ASSERT_EQ(expected_points_.cols(), contact_points.cols());
+  ASSERT_TRUE(expected_points_.isApprox(contact_points));
 }
 
 //GTEST_TEST()
@@ -64,5 +89,3 @@ TEST_F(ContactGenerationTest, GeneratingElementsTest) {
 }  // namespace plants
 }  // namespace systems
 }  // namespace drake
-
-}
