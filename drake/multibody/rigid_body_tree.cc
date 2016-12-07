080d871449487dd2c960c10119a4477cc8e74a50
Sean Curtis <sean.curtis@tri.global>
2016-12-06 16:48:19 -0800

    Correct three unit tests
    1) compute_contact_result_test_failed
       - It assumed a fixed ordering in contact results (A, B), now (B, A) can
         be returned.  The code now confirms the state before asserting.
    2) rbt_collision_test
       - Needed to add inertia to the sdf file (otherwise the body gets welded
         to the world.)
       - Sensitivity to the same (A, B)/(B, A) issue.
    3) robot_state_encoder_test
       - One test welds the pelvis to the world.  However, the pelvis geometry
         has non-triangulated mesh which is *not* compatible with the bullet
         mesh representation we're doing.
       - Solution: fall back to convex hull and print warning.

--- drake/multibody/rigid_body_tree.cc
+++ drake/multibody/rigid_body_tree.cc
@@ -301,8 +301,6 @@ void RigidBodyTree<T>::CompileCollisionState() {
       BodyCollisions &elements = pair.second;
       for (auto &collision_item : elements) {
         collision_item.element->set_anchored();
-        collision_item.element->updateWorldTransform(
-            Isometry3d::Identity());
       }
     }
   }
