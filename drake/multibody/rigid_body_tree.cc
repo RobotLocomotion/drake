4e3087212b72f4e698cf036904f5e21b0cf15b90
amcastro-tri <alejandro.castro@tri.global>
2016-12-07 14:41:59 -0500

    Experiments on RigidBodyTree<AutoDiffXd>

--- drake/multibody/rigid_body_tree.cc
+++ drake/multibody/rigid_body_tree.cc
@@ -3168,3 +3168,4 @@ RigidBodyTree<double>::CreateKinematicsCacheWithType<AutoDiffUpTo73d>() const;
 
 // Explicitly instantiates on the most common scalar types.
 template class RigidBodyTree<double>;
+template class RigidBodyTree<AutoDiffXd>;
