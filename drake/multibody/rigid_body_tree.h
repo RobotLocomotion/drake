ab80a700e26cdd49ce5d5b677b84c600d39950fb
Sean Curtis <sean.curtis@tri.global>
2016-12-07 07:01:41 -0800

    Various formatting corrections
    Cleaned up cpplint issues.

--- drake/multibody/rigid_body_tree.h
+++ drake/multibody/rigid_body_tree.h
@@ -1287,11 +1287,12 @@ class RigidBodyTree {
   bool initialized_{false};
 
   int next_available_clique_ = 0;
- public:
 
+ public:
   // Utility class for storing body collision data during RBT instantiation.
   struct BodyCollisionItem {
-    BodyCollisionItem(const std::string& group_name, std::unique_ptr<DrakeCollision::Element> element) {
+    BodyCollisionItem(const std::string& group_name,
+                      std::unique_ptr<DrakeCollision::Element> element) {
       this->group_name = group_name;
       this->element = std::move(element);
     }
@@ -1300,6 +1301,7 @@ class RigidBodyTree {
   };
 
   typedef std::vector<BodyCollisionItem> BodyCollisions;
+
  private:
   // This data structures supports an orderly instantiation of the collision
   // elements.  It is populated during tree construction, exercised during
