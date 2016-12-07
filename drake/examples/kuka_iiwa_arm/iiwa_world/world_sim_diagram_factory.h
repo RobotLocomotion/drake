ba35ce1e0ba1ddf03c3d4e031c61acd910d448d9
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2016-12-07 13:21:15 +0100

    review comments addressed

--- drake/examples/kuka_iiwa_arm/iiwa_world/world_sim_diagram_factory.h
+++ drake/examples/kuka_iiwa_arm/iiwa_world/world_sim_diagram_factory.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "drake/lcm/drake_lcm.h"
+#include "drake/multibody/rigid_body_plant/drake_visualizer.h"
 #include "drake/multibody/rigid_body_plant/rigid_body_plant.h"
 #include "drake/multibody/rigid_body_tree.h"
 #include "drake/systems/analysis/simulator.h"
 #include "drake/systems/framework/diagram.h"
 #include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/framework/primitives/constant_vector_source.h"
 
 namespace drake {
 namespace examples {
@@ -14,30 +16,43 @@ namespace kuka_iiwa_arm {
 // /examples/toyota_hsrb/hsrb_diagram_factories.h and moved to a common
 // library.
 
-/// Builds a diagram composed of a `RigidBodyPlant` and `DrakeVisualizer` from
-/// a `RigidBodyTree` and returns it. The plant's output port zero is connected
-/// to the systems::DrakeVisualizer's input port zero. The returned
-/// systems::Diagram has the same input and output ports as the plant.
-std::unique_ptr<systems::Diagram<double>> BuildPlantAndVisualizerDiagram(
-    std::unique_ptr<RigidBodyTree<double>> rigid_body_tree,
-    double penetration_stiffness, double penetration_damping,
-    double friction_coefficient, lcm::DrakeLcmInterface* lcm);
+/// A custom `Diagram` composed of a `RigidBodyPlant` and a `DrakeVisualizer`.
+/// The diagram's output port zero is connected to the
+/// `DrakeVisualizer`'s input port zero. The resulting diagram
+/// has the same input and output ports as the plant.
+template <typename T>
+class VisualizedPlant : public systems::Diagram<T> {
+ public:
+  /// Builds the VisualizedPlant.
+  /// @p rigid_body_tree the tree to be used within the `RigidBodyPlant`
+  /// @p penetration_stiffness, @p penetration_damping, and
+  /// @p friction_coefficient define the penetration and friction parameters
+  /// of the plant.
+  /// @p lcm is a pointer to an externally created lcm object.
+  VisualizedPlant(std::unique_ptr<RigidBodyTree<T>> rigid_body_tree,
+                  double penetration_stiffness, double penetration_damping,
+                  double friction_coefficient, lcm::DrakeLcmInterface* lcm);
 
-/// Builds and returns a systems::Diagram consisting of a
-/// systems::ConstantVectorSource connected to input port zero of
-/// @p plant_diagram. The returned systems::Diagram has no input ports and the
-/// same output ports as @p plant_diagram. Typically, @p plant_diagram is built
-/// using BuildPlantAndVisualizerDiagram().
-std::unique_ptr<systems::Diagram<double>> BuildConstantSourceToPlantDiagram(
-    std::unique_ptr<systems::Diagram<double>> plant_visualizer_diagram);
+ private:
+  systems::RigidBodyPlant<T>* rigid_body_plant_{nullptr};
+  systems::DrakeVisualizer* drake_visualizer{nullptr};
+};
 
-/// Sets the zero configuration in a `RigidBodyPlant` underlying an assembled
-/// diagram by accessing the context from the `Simulator`.
-/// The parameter @p diagram is to be generated using
-/// BuildConstantSourceToPlantDiagram().
-/// @see BuildConstantSourceToPlantDiagram
-void SetZeroConfiguration(systems::Simulator<double>* simulator,
-                          const systems::Diagram<double>* diagram);
+/// A custom `Diagram` consisting of a `ConstantVectorSource` of 0 magnitude
+/// connected to a `VisualizedPlant`'s input port zero. The resulting diagram
+/// has no input
+/// ports and the same output ports as the `VisualizedPlant`.
+template <typename T>
+class PassiveVisualizedPlant : public systems::Diagram<T> {
+ public:
+  /// Builds the PassiveVisualizedPlant.
+  /// \param visualized_plant a unique pointer to the `VisualizedPlant`.
+  explicit PassiveVisualizedPlant(std::unique_ptr<VisualizedPlant<T>> visualized_plant);
+
+ private:
+  VisualizedPlant<T>* visualized_plant_{nullptr};
+  systems::ConstantVectorSource<T>* constant_vector_source_{nullptr};
+};
 
 }  // namespace kuka_iiwa_arm
 }  // namespace examples
