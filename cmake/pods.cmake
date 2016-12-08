c22fe0509d1b9ef93d293a6baad78fa13d98cb2f
Jamie Snape <jamie.snape@kitware.com>
2016-12-08 18:08:24 -0500

    Remove pods_install_python_packages

--- cmake/pods.cmake
+++ cmake/pods.cmake
@@ -6,38 +6,12 @@
 # Next, any of the following macros can be used.  See the individual macro
 # definitions in this file for individual documentation.
 #
-# Python
-#   pods_install_python_packages(...)
+# General
+#   pods_config_search_paths()
 #
 # ----
 # File: pods.cmake
 # Distributed with pods version: 12.11.14
-
-# pods_install_python_packages(<src_dir1> ...)
-#
-# Install python packages to lib/pythonX.Y/dist-packages, where X.Y refers to
-# the current python version (e.g., 2.6)
-#
-# The __init__.py file in <src_dir> will be installed along with any .py files
-# in subdirectories.
-function(pods_install_python_packages py_src_dir)
-    get_filename_component(py_src_abs_dir ${py_src_dir} ABSOLUTE)
-    get_filename_component(py_module_name ${py_src_abs_dir} NAME)
-
-    # where do we install .py files to?
-    set(python_install_dir
-      "${DRAKE_LIBRARY_DIR}/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/dist-packages")
-
-    file(GLOB_RECURSE module_files ${py_src_abs_dir}/*)
-    foreach(file ${module_files})
-      if(NOT file MATCHES ".*\\.pyc|.*[~#]")
-        file(RELATIVE_PATH __tmp_path ${py_src_abs_dir} ${file})
-        get_filename_component(__tmp_dir ${__tmp_path} PATH)
-        install(FILES ${file}
-          DESTINATION "${python_install_dir}/${py_module_name}/${__tmp_dir}")
-      endif()
-    endforeach()
-endfunction()
 
 # pods_config_search_paths()
 #
