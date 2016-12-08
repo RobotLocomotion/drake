169e9a6752c569d404c31806e042d943d1ec6b3c
Jeremy Nimmer <jeremy.nimmer@tri.global>
2016-12-08 08:53:00 -0500

    Add ExtractDoubleOrThrow and use it in Simulator

--- drake/systems/analysis/test/simulator_test.cc
+++ drake/systems/analysis/test/simulator_test.cc
@@ -495,7 +495,7 @@ GTEST_TEST(SimulatorTest, UpdateThenPublishThenIntegrate) {
 GTEST_TEST(SimulatorTest, AutodiffBasic) {
   SpringMassSystem<AutoDiffXd> spring_mass(1., 1., 0.);
   Simulator<AutoDiffXd> simulator(spring_mass);
-  simulator.StepTo(1.0);
+  simulator.Initialize();
 }
 
 }  // namespace
