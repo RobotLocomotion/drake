4b0deae6f817c7023b9150dcfb5b1557d75001f5
Chien-Liang Fok <liang.fok@tri.global>
2016-12-09 14:35:25 -0500

    WIP

--- drake/multibody/test/package_map_test/package_map_test.cc
+++ drake/multibody/test/package_map_test/package_map_test.cc
@@ -10,23 +10,33 @@ namespace {
 const std::string kRootPath(GetDrakePath() +
 	                        "/multibody/test/package_map_test/");
 
+GTEST_TEST(PackageMapTest, TestManualPopulation) {
+  // TODO
+}
+
 GTEST_TEST(PackageMapTest, TestPopulateMapFromFolder) {
   PackageMap package_map;
   package_map.PopulateFromFolder(kRootPath);
 
-  map<string, string> expected_package_map = {
-    {"parser_common_test_package_a", kRootPath +
-        "parser_common_test_packages/parser_common_test_package_a/"},
-    {"parser_common_test_package_b", kRootPath +
-        "parser_common_test_packages/parser_common_test_package_b/"},
-    {"parser_common_test_package_c", kRootPath +
-        "parser_common_test_packages/parser_common_test_package_set/"
-        "parser_common_test_package_c/"},
-    {"parser_common_test_package_d", kRootPath +
-        "parser_common_test_packages/parser_common_test_package_set/"
-        "parser_common_test_package_d/"}};
+  map<string, string> expected_packages = {
+    {"package_map_test_package_a", kRootPath +
+        "package_map_test_packages/package_map_test_package_a/"},
+    {"package_map_test_package_b", kRootPath +
+        "package_map_test_packages/package_map_test_package_b/"},
+    {"package_map_test_package_c", kRootPath +
+        "package_map_test_packages/package_map_test_package_set/"
+        "package_map_test_package_c/"},
+    {"package_map_test_package_d", kRootPath +
+        "package_map_test_packages/package_map_test_package_set/"
+        "package_map_test_package_d/"}};
 
-  // TODO ...finish
+  for (const auto& path_entry : expected_packages) {
+    const std::string& package_name = path_entry.first;
+    const std::string& package_path = path_entry.second;
+
+    ASSERT_TRUE(package_map.Contains(package_name));
+    EXPECT_EQ(package_map.GetPath(package_name), package_name);
+  }
 }
 
 }  // namespace
