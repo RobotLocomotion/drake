4bc1fbb5acaa7481bf18cc026278552b2c9bfb75
David German <david.german@tri.global>
2016-12-19 10:37:58 -0500

    Add CopyFrom to State and ContinuousState.
    Fixes #4535.

--- drake/systems/framework/state.cc
+++ drake/systems/framework/state.cc
@@ -1,4 +1,13 @@
-// For now, this is an empty .cc file that only serves to confirm
-// state.h is a stand-alone header.
-
 #include "drake/systems/framework/state.h"
+
+#include "drake/common/autodiff_overloads.h"
+#include "drake/common/eigen_autodiff_types.h"
+
+namespace drake {
+namespace systems {
+
+template class State<double>;
+template class State<AutoDiffXd>;
+
+}  // namespace systems
+}  // namespace drake
