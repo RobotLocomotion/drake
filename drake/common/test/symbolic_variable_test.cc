661bcc484da16a1454e3fdd5ea85c47762f9ce64
Soonho Kong <soonho.kong@tri.global>
2016-12-20 15:48:12 -0500

    Add more symbolic tests

--- drake/common/test/symbolic_variable_test.cc
+++ drake/common/test/symbolic_variable_test.cc
@@ -19,54 +19,46 @@ using std::vector;
 // Provides common variables that are used by the following tests.
 class SymbolicVariableTest : public ::testing::Test {
  protected:
+  const Variable w_{"w"};
   const Variable x_{"x"};
   const Variable y_{"y"};
   const Variable z_{"z"};
-  const Variable w_{"w"};
 };
-
-TEST_F(SymbolicVariableTest, GetId) {
-  const Variable x_prime{"x"};
-  EXPECT_NE(x_.get_id(), x_prime.get_id());
-}
 
 TEST_F(SymbolicVariableTest, GetName) {
   const Variable x_prime{"x"};
-  EXPECT_EQ(x_.get_name(), x_prime.get_name());
+  EXPECT_EQ(x_.name(), x_prime.name());
 }
 
-TEST_F(SymbolicVariableTest, MoveCopyPreserveId) {
+TEST_F(SymbolicVariableTest, MoveCopy) {
   Variable x{"x"};
-  const size_t x_id{x.get_id()};
-  const size_t x_hash{x.get_hash()};
+  const size_t x_hash{x.hash()};
   const Variable x_copied{x};
   const Variable x_moved{move(x)};
-  EXPECT_EQ(x_id, x_copied.get_id());
-  EXPECT_EQ(x_hash, x_copied.get_hash());
-  EXPECT_EQ(x_id, x_moved.get_id());
-  EXPECT_EQ(x_hash, x_moved.get_hash());
+  EXPECT_EQ(x_hash, x_copied.hash());
+  EXPECT_EQ(x_hash, x_moved.hash());
 }
 
 TEST_F(SymbolicVariableTest, Lt) {
+  EXPECT_FALSE(w_ < w_);
+  EXPECT_TRUE(w_ < x_);
+  EXPECT_TRUE(w_ < y_);
+  EXPECT_TRUE(w_ < z_);
+
+  EXPECT_FALSE(x_ < w_);
   EXPECT_FALSE(x_ < x_);
   EXPECT_TRUE(x_ < y_);
   EXPECT_TRUE(x_ < z_);
-  EXPECT_TRUE(x_ < w_);
 
+  EXPECT_FALSE(y_ < w_);
   EXPECT_FALSE(y_ < x_);
   EXPECT_FALSE(y_ < y_);
   EXPECT_TRUE(y_ < z_);
-  EXPECT_TRUE(y_ < w_);
 
+  EXPECT_FALSE(z_ < w_);
   EXPECT_FALSE(z_ < x_);
   EXPECT_FALSE(z_ < y_);
   EXPECT_FALSE(z_ < z_);
-  EXPECT_TRUE(z_ < w_);
-
-  EXPECT_FALSE(w_ < x_);
-  EXPECT_FALSE(w_ < y_);
-  EXPECT_FALSE(w_ < z_);
-  EXPECT_FALSE(w_ < w_);
 }
 
 TEST_F(SymbolicVariableTest, Eq) {
