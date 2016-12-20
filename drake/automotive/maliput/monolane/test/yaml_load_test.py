5830322819421b286bd91a26600ee4ed8f574a7f
Matt Marjanovic <maddog@tri.global>
2016-12-20 11:18:50 -0800

    Incorporate patch from @jwnimmer-tri to get the python-based yaml_load_test working in bazel.

--- drake/automotive/maliput/monolane/test/yaml_load_test.py
+++ drake/automotive/maliput/monolane/test/yaml_load_test.py
@@ -16,21 +16,31 @@ _DRAKE_PATHS_DIR = os.path.dirname(os.path.dirname(os.path.dirname(_THIS_DIR)))
 
 sys.path.append(_DRAKE_PATHS_DIR)
 
-from drake_paths import DRAKE_DRAKE_BUILD_DIR
+try:
+    from drake_paths import DRAKE_DRAKE_BUILD_DIR
+except ImportError:
+    # In the case of Bazel.
+    DRAKE_DRAKE_BUILD_DIR = "drake"
 
 
 class TestYamlLoading(unittest.TestCase):
     def test_yaml_files(self):
         yaml_dir = os.path.dirname(_THIS_DIR)
+
+        yaml_load = os.path.join(
+            DRAKE_DRAKE_BUILD_DIR,
+            "automotive/maliput/monolane/yaml_load")
+        self.assertTrue(os.path.exists(yaml_load), yaml_load + " not found")
+
         yaml_files = glob.glob(os.path.join(yaml_dir, '*.yaml'))
         blacklist = []
         test_yaml_files = [f for f in yaml_files
                            if not any([b in f for b in blacklist])]
-        assert len(test_yaml_files) > 0
+        self.assertTrue(len(test_yaml_files) > 0)
+
         for yf in test_yaml_files:
             subprocess.check_call([
-                os.path.join(DRAKE_DRAKE_BUILD_DIR,
-                             "automotive/maliput/monolane/test/yaml_load"),
+                yaml_load,
                 "-yaml_file", yf,
             ])
 
