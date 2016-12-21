0fd71b678efd48bbfb7e49106d46e0b9ba504a02
Jamie Snape <jamie.snape@kitware.com>
2016-12-21 15:52:58 -0500

    Remove another line or two from pods.cmake

--- cmake/pods.cmake
+++ cmake/pods.cmake
@@ -15,24 +15,15 @@
 
 # pods_config_search_paths()
 #
-# Setup include, linker, and pkg-config paths according to the pods core
-# policy.  This macro is automatically invoked, there is no need to do so
-# manually.
+# Setup rpath and install name path according to the pods core policy. This
+# macro is automatically invoked, there is no need to do so manually.
 macro(pods_config_search_paths)
     if(NOT DEFINED __pods_setup)
-      # set where files should be installed to
-      set(LIBRARY_INSTALL_PATH "${DRAKE_LIBRARY_DIR}")
-
-      # add install/lib/pkgconfig to the pkg-config search path so we find
-      # .pc files installed by externals (use both the correct library dir, and
-      # plain 'lib', as some externals may not use the correct library dir)
-      set(ENV{PKG_CONFIG_PATH} "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/pkgconfig:${CMAKE_INSTALL_PREFIX}/lib/pkgconfig:$ENV{PKG_CONFIG_PATH}")
-
       # abuse RPATH
       if(CMAKE_INSTALL_RPATH)
-        set(CMAKE_INSTALL_RPATH ${LIBRARY_INSTALL_PATH}:${CMAKE_INSTALL_RPATH})
+        set(CMAKE_INSTALL_RPATH ${DRAKE_LIBRARY_DIR}:${CMAKE_INSTALL_RPATH})
       else()
-        set(CMAKE_INSTALL_RPATH ${LIBRARY_INSTALL_PATH})
+        set(CMAKE_INSTALL_RPATH ${DRAKE_LIBRARY_DIR})
       endif()
 
       # for OS X, which uses "install name" path rather than rpath
