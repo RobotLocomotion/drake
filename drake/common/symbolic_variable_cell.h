f953f78bd98bef04e75b78549010531024c84390
Soonho Kong <soonho.kong@tri.global>
2016-12-21 10:05:42 -0500

    Add more doxygen comments

--- drake/common/symbolic_variable_cell.h
+++ drake/common/symbolic_variable_cell.h
@@ -7,13 +7,16 @@
 
 namespace drake {
 namespace symbolic {
+
+/** This class is an abstract base-class for symbolic::Indeterminate and
+ * symbolic::DecisionVariableScalar classes. */
 class VariableCell {
  public:
   /** Default constructor. */
   VariableCell() = default;
-  /** Constructs a variable with a string . */
+  /** Constructs a variable with kind and string. */
   VariableCell(VariableKind kind, const std::string& name);
-  /** Constructs a variable with a string . */
+  /** Constructs a variable with kind, string, and hash. */
   VariableCell(VariableKind kind, const std::string& name, size_t hash);
   /** Move-construct a set from an rvalue. */
   VariableCell(VariableCell&& v) = default;
@@ -24,12 +27,18 @@ class VariableCell {
   /** Copy-assign. */
   VariableCell& operator=(const VariableCell& v) = default;
 
+  /** Returns its kind. */
   VariableKind kind() const;
+  /** Returns its name. */
   const std::string& name() const;
+  /** Returns its hash value. */
   size_t hash() const;
 
+  /** Checks equality. */
   virtual bool equal_to(const VariableCell& var) const = 0;
+  /** Checks total ordering. */
   virtual bool less(const VariableCell& var) const = 0;
+  /** Outputs its string representation to @p os. */
   virtual std::ostream& Display(std::ostream& os) const = 0;
 
  private:
