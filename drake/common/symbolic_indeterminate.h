824188ea8293920e98fc668ea2e0ee1d60a7a072
Soonho Kong <soonho.kong@tri.global>
2016-12-21 10:05:42 -0500

    Add more doxygen comments

--- drake/common/symbolic_indeterminate.h
+++ drake/common/symbolic_indeterminate.h
@@ -7,12 +7,17 @@
 namespace drake {
 namespace symbolic {
 
+/** Represents an indeterminate variable. */
 class Indeterminate : public VariableCell {
  public:
+  /** Constructs an indeterminate variable with name. */
   explicit Indeterminate(const std::string& name);
 
+  /** Checks equality. */
   bool equal_to(const VariableCell& var) const override;
+  /** Checks total ordering. */
   bool less(const VariableCell& var) const override;
+  /** Outputs its string representation to @p os. */
   std::ostream& Display(std::ostream& os) const override;
 };
 
