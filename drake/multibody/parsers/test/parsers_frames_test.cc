91fba81dc8495c072f76ca62138342ea4464f51d
amcastro-tri <alejandro.castro@tri.global>
2016-12-27 12:14:53 -0500

    SDF test with L = B

--- drake/multibody/parsers/test/parsers_frames_test.cc
+++ drake/multibody/parsers/test/parsers_frames_test.cc
@@ -1,4 +1,3 @@
-#include "drake/multibody/parsers/urdf_parser.h"
 
 #include <iostream>
 #include <fstream>
@@ -7,8 +6,12 @@
 
 #include <gtest/gtest.h>
 
+#include "spruce.hh"
+
 #include "drake/common/drake_path.h"
 #include "drake/multibody/joints/floating_base_types.h"
+#include "drake/multibody/parsers/urdf_parser.h"
+#include "drake/multibody/parsers/sdf_parser.h"
 #include "drake/multibody/rigid_body_tree.h"
 
 using std::endl;
@@ -28,14 +31,12 @@ using Eigen::VectorXd;
 namespace drake {
 
 using multibody::joints::kQuaternion;
-using parsers::urdf::AddModelInstanceFromUrdfFile;
-using parsers::urdf::AddModelInstanceFromUrdfFileSearchingInRosPackages;
-using parsers::urdf::AddModelInstanceFromUrdfFileToWorld;
-using parsers::urdf::AddModelInstanceFromUrdfFileWithRpyJointToWorld;
-using parsers::urdf::AddModelInstanceFromUrdfStringWithRpyJointToWorld;
-using parsers::urdf::AddModelInstanceFromUrdfString;
 
 namespace parsers {
+
+using urdf::AddModelInstanceFromUrdfFileToWorld;
+using sdf::AddModelInstancesFromSdfFileToWorld;
+
 namespace {
 
 class DoublePendulumFramesTest : public ::testing::Test {
@@ -53,8 +54,20 @@ class DoublePendulumFramesTest : public ::testing::Test {
         "/multibody/parsers/test/" + file_name;
 
     tree_ = std::make_unique<RigidBodyTree<double>>();
-    AddModelInstanceFromUrdfFileToWorld(
-        full_name, drake::multibody::joints::kFixed, tree_.get());
+
+    spruce::path spruce_path(file_name);
+    auto extension = spruce_path.extension();
+    std::transform(
+        extension.begin(), extension.end(), extension.begin(), ::tolower);
+    DRAKE_DEMAND(extension == ".urdf" || extension == ".sdf");
+
+    if(extension == ".urdf") {
+      AddModelInstanceFromUrdfFileToWorld(
+          full_name, drake::multibody::joints::kFixed, tree_.get());
+    } else {
+      AddModelInstancesFromSdfFileToWorld(
+          full_name, drake::multibody::joints::kFixed, tree_.get());
+    }
 
     world_id_ = tree_->FindBody("world")->get_body_index();
     base_id_ = tree_->FindBody("base")->get_body_index();
@@ -112,10 +125,17 @@ class DoublePendulumFramesTest : public ::testing::Test {
       auto Bcm_B = tree_->get_body(i).get_center_of_mass_in_B();
       auto Bcm_W = tree_->transformPoints(cache, Bcm_B, i, 0);
 
+#if 0
       PRINT_VAR(tree_->get_body(i).get_name());
       PRINT_VAR(Bo_W.transpose());
+      PRINT_VAR(expected_Bo_W[i].transpose());
+
       PRINT_VAR(Bcm_B.transpose());
+      PRINT_VAR(expected_Bcm_B[i].transpose());
+
       PRINT_VAR(Bcm_W.transpose());
+      PRINT_VAR(expected_Bcm_W[i].transpose());
+#endif
 
       EXPECT_TRUE(Bo_W.isApprox(expected_Bo_W[i]));
       EXPECT_TRUE(Bcm_B.isApprox(expected_Bcm_B[i]));
@@ -140,6 +160,26 @@ class DoublePendulumFramesTest : public ::testing::Test {
 
 TEST_F(DoublePendulumFramesTest, URDFTest) {
   LoadTreeFrom("simple_pendulum.urdf");
+
+  EXPECT_EQ(tree_->get_num_bodies(), 4);
+  EXPECT_EQ(tree_->get_num_positions(), 2);
+  EXPECT_EQ(tree_->get_num_velocities(), 2);
+
+  // Expected poses for the zero state configuration.
+  RunTest(0.0, 0.0);
+  RunTest(0.0, 45.0);
+  RunTest(45.0, 0.0);
+  RunTest(12.0, -18.0);
+}
+
+// In this case the simple pendulum model is loaded form an SDF file
+// explicitly specifying the link frames L to be located at the mobile frames
+// B (recall B = M in Drake).
+// In this case, <joint> does not need to specify its pose with respect to
+// the parent body, i.e. X_PF is not specified, no <pose> given in <joint>.
+// Therefore the inertial frames I need to be specified as doen in URDF files.
+TEST_F(DoublePendulumFramesTest, SDFTest) {
+  LoadTreeFrom("simple_pendulum_LequalsB.sdf");
 
   EXPECT_EQ(tree_->get_num_bodies(), 4);
   EXPECT_EQ(tree_->get_num_positions(), 2);
