9a3534ddb9f6f8fcf7caa2d7ddb441041ee9170b
Soonho Kong <soonho.kong@tri.global>
2016-12-28 00:29:23 -0500

    Make symbolic::Variable compatible with Eigen::Matrix (#4627)
    * Make symbolic::Variable compatible with Eigen::Matrix

--- drake/common/symbolic_variable.h
+++ drake/common/symbolic_variable.h
@@ -1,10 +1,11 @@
 #pragma once
 
-#include <atomic>
 #include <cstddef>
 #include <functional>
 #include <ostream>
 #include <string>
+
+#include <Eigen/Core>
 
 #include "drake/common/hash.h"
 
@@ -14,8 +15,12 @@ namespace symbolic {
 /** Represents a symbolic variable. */
 class Variable {
  public:
-  /** Default constructor (DELETED). */
-  Variable() = delete;
+  /** Default constructor. This is needed to have Eigen::Matrix<Variable>. The
+      objects created by the default constructor share the same ID, zero. As a
+      result, they all are identified as a single variable by equality operator
+      (==). They all have the same hash value as well.
+   */
+  Variable() : id_{0}, name_{std::string()} {}
 
   /** Constructs a variable with a string . */
   explicit Variable(const std::string& name);
@@ -26,11 +31,11 @@ class Variable {
   /** Copy-construct a set from an lvalue. */
   Variable(const Variable& v) = default;
 
-  /** Move-assign (DELETED). */
-  Variable& operator=(Variable&& v) = delete;
+  /** Move-assign. */
+  Variable& operator=(Variable&& v) = default;
 
-  /** Copy-assign (DELETED). */
-  Variable& operator=(const Variable& v) = delete;
+  /** Copy-assign. */
+  Variable& operator=(const Variable& v) = default;
 
   size_t get_id() const;
   size_t get_hash() const { return std::hash<size_t>{}(id_); }
@@ -42,8 +47,8 @@ class Variable {
  private:
   // Produces a unique ID for a variable.
   static size_t get_next_id();
-  const size_t id_{};       // Unique identifier.
-  const std::string name_;  // Name of variable.
+  size_t id_{};       // Unique identifier.
+  std::string name_;  // Name of variable.
 };
 
 /// Compare two variables based on their ID values
@@ -60,3 +65,14 @@ struct hash_value<symbolic::Variable> {
   size_t operator()(const symbolic::Variable& v) const { return v.get_hash(); }
 };
 }  // namespace drake
+
+#if !defined(DRAKE_DOXYGEN_CXX)
+namespace Eigen {
+// Eigen scalar type traits for Matrix<drake::symbolic::Variable>.
+template <>
+struct NumTraits<drake::symbolic::Variable>
+    : GenericNumTraits<drake::symbolic::Variable> {
+  static inline int digits10() { return 0; }
+};
+}  // namespace Eigen
+#endif  // !defined(DRAKE_DOXYGEN_CXX)
