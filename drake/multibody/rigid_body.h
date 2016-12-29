2fe5b5f5bf6b02d775dadc04cb111e7c90534863
Sean Curtis <sean.curtis@tri.global>
2016-12-29 09:44:01 -0700

    Enforce the invariant the that RigidBodyTree is a proper path (#4632)
    At the conclusion of RigidBodyTree::compile (after all structural changes
    are affected), the RBT evaluates all the bodies and confirms that there
    is a kinetic path from the body to the world.

--- drake/multibody/rigid_body.h
+++ drake/multibody/rigid_body.h
@@ -101,6 +101,11 @@ class RigidBody {
   const DrakeJoint& getJoint() const;
 
   /**
+   * Reports if the body has a parent joint.
+   */
+  bool has_joint() const { return joint_ != nullptr; }
+
+  /**
    * Sets the parent rigid body. This is the rigid body that is connected to
    * this rigid body's joint.
    *
