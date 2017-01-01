81d5c81c98ab45614ee9d169c982125517645ae4
hongkai-dai <daihongkai@gmail.com>
2017-01-01 12:19:23 -0800

    minor modification to make the code more readable

--- drake/solvers/gurobi_solver.cc
+++ drake/solvers/gurobi_solver.cc
@@ -155,20 +155,23 @@ int AddSecondOrderConeConstraints(
     std::vector<int> qcol(num_Q_nonzero);
     std::vector<double> qval(num_Q_nonzero);
     for (int i = 0; i < num_z - 2; ++i) {
-      qrow[i] = variable_indices[num_x + i + 2];
-      qcol[i] = variable_indices[num_x + i + 2];
+      int zi_index = second_order_cone_new_variable_indices[second_order_cone_count][i + 2];
+      qrow[i] = zi_index;
+      qcol[i] = zi_index;
       qval[i] = 1.0;
     }
+    int z0_index = second_order_cone_new_variable_indices[second_order_cone_count][0];
+    int z1_index = second_order_cone_new_variable_indices[second_order_cone_count][1];
     if (is_rotated_cone) {
-      qrow[num_z - 2] = variable_indices[num_x];
-      qcol[num_z - 2] = variable_indices[num_x + 1];
+      qrow[num_z - 2] = z0_index;
+      qcol[num_z - 2] = z1_index;
       qval[num_z - 2] = -1;
     } else {
-      qrow[num_z - 2] = variable_indices[num_x];
-      qcol[num_z - 2] = variable_indices[num_x];
+      qrow[num_z - 2] = z0_index;
+      qcol[num_z - 2] = z0_index;
       qval[num_z - 2] = -1;
-      qrow[num_z - 1] = variable_indices[num_x + 1];
-      qcol[num_z - 1] = variable_indices[num_x + 1];
+      qrow[num_z - 1] = z1_index;
+      qcol[num_z - 1] = z1_index;
       qval[num_z - 1] = 1;
     }
     error =
