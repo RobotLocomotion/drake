832be6e17f3813c1ad13f5035e44107d1601743e
Jamie Snape <jamie.snape@kitware.com>
2017-01-04 13:47:05 -0500

    Fix test timeout multiplier

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -2,8 +2,8 @@ option(CHECK_DEPENDENCY_STRICT
   "Fail the test if a MATLAB dependency check fails" OFF)
 mark_as_advanced(CHECK_DEPENDENCY_STRICT)
 
-set(TEST_TIMEOUT_MULTIPLIER CACHE STRING
-  "Positive integer by which to multiply test timeouts" 1)
+set(TEST_TIMEOUT_MULTIPLIER 1 CACHE STRING
+  "Positive integer by which to multiply test timeouts")
 mark_as_advanced(TEST_TIMEOUT_MULTIPLIER)
 
 
