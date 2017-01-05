b4040413dd81462558ba09460bb6353ec55829e4
nate koenig <natekoenig@gmail.com>
2017-01-05 09:34:13 -0800

    Updated tinyobjloader codebase, and updated cmake

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -129,6 +129,7 @@ macro(drake_find_packages)
   add_definitions(-DEIGEN_MPL2_ONLY)  # Per #4065.
   drake_find_package(gflags CONFIG REQUIRED)
   drake_find_package(GTest MODULE REQUIRED)
+  drake_find_package(tinyobjloader CONFIG REQUIRED)
 
   # END required packages
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
