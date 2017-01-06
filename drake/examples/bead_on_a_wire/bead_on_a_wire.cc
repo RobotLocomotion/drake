b974ba9b9f3e3e9e25334efc770980751e516b29
Evan Drumwright <drum@tri.global>
2017-01-06 09:35:58 -0800

    Added BeadOnAWire.

---
+++ drake/examples/bead_on_a_wire/bead_on_a_wire.cc
@@ -0,0 +1,10 @@
+// NOLINTNEXTLINE(build/include) False positive on inl file.
+#include "drake/examples/bead_on_a_wire/bead_on_a_wire-inl.h"
+
+namespace drake {
+namespace bead_on_a_wire {
+
+template class BeadOnAWire<double>;
+
+}  // namespace bead_on_a_wire
+}  // namespace drake
---
+++ drake/examples/bead_on_a_wire/bead_on_a_wire.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#include "drake/systems/framework/leaf_system.h"
+
+namespace drake {
+namespace bead_on_a_wire {
+
+/// Dynamical system representation of the BeadOnAWire' Paradox problem, taken
+/// from [Stewart 2000]. The BeadOnAWire Paradox consists of a rod contacting
+/// a planar surface *without impact* and subject to sliding Coulomb friction.
+/// The problem is well known to correspond to an *inconsistent rigid contact
+/// configuration*, where non-impulsive forces are necessary to resolve the
+/// problem.
+///
+/// This class uses Drake's `-inl.h` pattern.  When seeing linker errors from
+/// this class, please refer to http://drake.mit.edu/cxx_inl.html.
+///
+/// @tparam T The vector element type, which must be a valid Eigen scalar.
+/// Instantiated templates for the following scalar types @p T are provided:
+/// - double
+///
+/// They are already available to link against in drakeBeadOnAWire.
+///
+/// Inputs: no inputs.
+/// States: planar position (state indices 0 and 1) and orientation (state
+///         index 2), and planar linear velocity (state indices 3 and 4) and
+///         scalar angular velocity (state index 5) in units of m, radians,
+///         m/s, and rad/s, respectively. Orientation is measured counter-
+///         clockwise with respect to the x-axis.
+/// Outputs: same as state.
+template <typename T>
+class BeadOnAWire : public systems::LeafSystem<T> {
+ public:
+  BeadOnAWire();
+
+  void DoCalcOutput(const systems::Context<T>& context,
+                  systems::SystemOutput<T>* output) const override;
+
+  void DoCalcTimeDerivatives(
+      const systems::Context<T>& context,
+      systems::ContinuousState<T>* derivatives) const override;
+
+  /// Models impact using an inelastic impact model with friction.
+  void HandleImpact(
+      const systems::Context<T>& context,
+      systems::ContinuousState<T>* new_state) const;
+
+  /// Sets the acceleration (with respect to the positive y-axis) due to
+  /// gravity (i.e., this number should generally be negative).
+  void set_gravitational_acceleration(double g) { g_ = g; }
+
+  /// Gets the acceleration (with respect to the positive y-axis) due to
+  /// gravity (i.e., this number should generally be negative).
+  double get_gravitational_acceleration() const { return g_; }
+
+  /// Gets the coefficient of dynamic (sliding) Coulomb friction.
+  double get_mu_coulomb() const { return mu_; }
+
+  /// Sets the coefficient of dynamic (sliding) Coulomb friction.
+  void set_mu_coulomb(double mu) { mu_ = mu; }
+
+  /// Gets the mass of the rod.
+  double get_rod_mass() const { return mass_; }
+
+  /// Sets the mass of the rod.
+  void set_rod_mass(double mass) { mass_ = mass; }
+
+  /// Gets the length of the rod.
+  double get_rod_length() const { return rod_length_; }
+
+  /// Sets the length of the rod.
+  void set_rod_length(double rod_length) { rod_length_ = rod_length; }
+
+  /// Gets the rod moment of inertia.
+  double get_rod_moment_of_inertia() const { return J_; }
+
+  /// Sets the rod moment of inertia.
+  void set_rod_moment_of_inertia(double J) { J_ = J; }
+
+  /// Checks whether the system is in an impacting configuration.
+  bool IsImpacting(const systems::Context<T>& context) const;
+
+ protected:
+  void SetDefaultState(const systems::Context<T>& context,
+                       systems::State<T>* state) const override;
+
+ private:
+  Vector2<T> CalcStickingImpactImpulse(const systems::Context<T>& context)
+    const;
+  Vector2<T> CalcFConeImpactImpulse(const systems::Context<T>& context) const;
+  void DoCalcTimeDerivativesTwoContact(const systems::Context<T>& context,
+                                       systems::ContinuousState<T>* derivatives)
+                                         const;
+  void DoCalcTimeDerivativesOneContactNoSliding(
+      const systems::Context<T>& context,
+      systems::ContinuousState<T>* derivatives) const;
+  void SetVelocityDerivatives(const systems::Context<T>& context,
+                              systems::VectorBase<T>* const f,
+                              T fN, T fF, T xc, T yc) const;
+  Vector2<T> CalcStickingContactForces(
+      const systems::Context<T>& context) const;
+
+  double mass_{1.0};        // The mass of the rod.
+  double rod_length_{1.0};  // The length of the rod.
+  double mu_{1000.0};       // The coefficient of friction.
+  double g_{-9.81};         // The acceleration due to gravity.
+  double J_{1.0};           // The moment of the inertia of the rod.
+};
+
+}  // namespace bead_on_a_wire
+}  // namespace drake
