fd53de9db57c680f51611c9dec32d4104010b915
Matt Marjanovic <maddog@tri.global>
2017-01-09 23:34:00 -0800

    Clean up endless-road-car-state converter.

--- drake/automotive/endless_road_car_to_euler_floating_joint.h
+++ drake/automotive/endless_road_car_to_euler_floating_joint.h
@@ -4,21 +4,27 @@
 
 #include "drake/automotive/gen/euler_floating_joint_state.h"
 #include "drake/automotive/gen/endless_road_car_state.h"
-#include "drake/automotive/maliput/api/road_geometry.h"
 #include "drake/automotive/maliput/utility/infinite_circuit_road.h"
 #include "drake/systems/framework/leaf_system.h"
 
 namespace drake {
 namespace automotive {
+// TODO(maddog@tri.global)  EndlessRoadCarState should be generalized to
+//                          RoadCarState, which packs a maliput::api::Lane*
+//                          in addition to (s, r), and this class would
+//                          generalize accordingly.
 
-/// Converts EndlessRoadCarState to a full 6-DOF EulerFloatingJointState,
-/// using the RoadGeometry on which the car is (presumably) driving.
+/// Converts EndlessRoadCarState to a full 6-DOF EulerFloatingJointState
+/// in the world frame, mapping via the RoadGeometry on which the car is
+/// (presumably) driving.
 template <typename T>
 class EndlessRoadCarToEulerFloatingJoint : public systems::LeafSystem<T> {
  public:
-  // TODO(maddog)  Is there a nice way for this to get the RoadGeometry from
-  //               the connected car?  (It will be a moot question once the
-  //               complete ROAD-state including Lane* is used.)
+  /// Constructor.
+  ///
+  /// @param road  the InfiniteCircuitRoad which defines the mapping from
+  ///              LANE-space (s, r) coordinates to world-frame position
+  ///              and orientation.
   EndlessRoadCarToEulerFloatingJoint(
       const maliput::utility::InfiniteCircuitRoad* road)
       : road_(road) {
@@ -28,6 +34,7 @@ class EndlessRoadCarToEulerFloatingJoint : public systems::LeafSystem<T> {
     this->DeclareOutputPort(systems::kVectorValued,
                             EulerFloatingJointStateIndices::kNumCoordinates);
   }
+
 
   void DoCalcOutput(const systems::Context<T>& context,
                     systems::SystemOutput<T>* output) const override {
@@ -54,13 +61,13 @@ class EndlessRoadCarToEulerFloatingJoint : public systems::LeafSystem<T> {
     output_data->set_z(geo.z);
 
     // Simple treatment of orientation:  "car always points in the direction
-    // of its velocity vector", e.g., no slip, oversteeer, etc.
-    // Hence, we express forward-orientation of the car as a composition of
-    // two rotations:  rotation in sr-plane to align with velocity vector,
-    // followed by srh->xyz rotation of LANE-space orientation.
-    maliput::api::Rotation rot = road_->lane()->GetOrientation(lp);
-    // TODO(maddog)  Deal with (sigma_dot < 0).
+    // of its heading", e.g., no slip, oversteeer, etc.  Hence, we express
+    // forward-orientation of the car as a composition of two rotations:
+    //  1) rotation in lane's sr-plane to account for the heading,
     const double theta = input_data->heading();
+    // followed by:
+    //  2) srh->xyz orientation of LANE-space frame w.r.t. world frame.
+    maliput::api::Rotation rot = road_->lane()->GetOrientation(lp);
 
     const double ct = std::cos(theta);
     const double st = std::sin(theta);
@@ -72,13 +79,15 @@ class EndlessRoadCarToEulerFloatingJoint : public systems::LeafSystem<T> {
     const double cg = std::cos(rot.yaw);
     const double sg = std::sin(rot.yaw);
 
+    // We compute the five coefficients of the composite rotation matrix
+    // which we need to extract roll/pitch/yaw.
     const double A = ct*(cb*cg) + st*(-ca*sg + sa*sb*cg);
     const double B = ct*(cb*sg) + st*(ca*cg + sa*sb*sg);
     const double C = -ct*(sb) + st*(sa*cb);
     const double D = st*(sb) + ct*(sa*cb);
     const double E = ca*cb;
-    // TODO(maddog)  yaw and roll are only correct when pitch is in the
-    //               range [-PI/2, PI/2] --- do we care about flipped cars?
+    // NB: Yaw and roll are only correct when pitch is in the range
+    //     [-PI/2, PI/2]; this doesn't handle flipped roads/cars.
     output_data->set_yaw(std::atan2(B, A));
     output_data->set_pitch(std::atan2(-C, std::sqrt(D*D + E*E)));
     output_data->set_roll(std::atan2(D, E));
