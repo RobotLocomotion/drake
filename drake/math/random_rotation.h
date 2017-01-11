1bbb26607039cf0e1b44e2ca078f1c2c6577b354
Russ Tedrake <russt@mit.edu>
2017-01-10 21:09:10 -0800

    Don't require the use of std::default_random_generator for random rotations.  (the default generator is different on different systems, so should not be the default choice, and certainly shouldn't be imposed on the user).

--- drake/math/random_rotation.h
+++ drake/math/random_rotation.h
@@ -6,20 +6,42 @@
 
 #include "drake/common/constants.h"
 #include "drake/common/eigen_types.h"
+#include "drake/math/axis_angle.h"
 
 namespace drake {
 namespace math {
-Eigen::Vector4d UniformlyRandomAxisAngle(
-    // TODO(#2274) Fix NOLINTNEXTLINE(runtime/references).
-    std::default_random_engine& generator);
-Eigen::Vector4d UniformlyRandomQuat(
-    // TODO(#2274) Fix NOLINTNEXTLINE(runtime/references).
-    std::default_random_engine& generator);
-Eigen::Matrix3d UniformlyRandomRotmat(
-    // TODO(#2274) Fix NOLINTNEXTLINE(runtime/references).
-    std::default_random_engine& generator);
-Eigen::Vector3d UniformlyRandomRPY(
-    // TODO(#2274) Fix NOLINTNEXTLINE(runtime/references).
-    std::default_random_engine& generator);
+
+template< class Generator >
+// TODO(#2274) Fix NOLINTNEXTLINE(runtime/references).
+Eigen::Vector4d UniformlyRandomAxisAngle(Generator& generator) {
+  std::normal_distribution<double> normal;
+  std::uniform_real_distribution<double> uniform(-M_PI, M_PI);
+  double angle = uniform(generator);
+  Eigen::Vector3d axis =
+      Eigen::Vector3d(normal(generator), normal(generator), normal(generator));
+  axis.normalize();
+  Eigen::Vector4d a;
+  a << axis, angle;
+  return a;
+}
+
+template< class Generator >
+// TODO(#2274) Fix NOLINTNEXTLINE(runtime/references).
+Eigen::Vector4d UniformlyRandomQuat(Generator& generator) {
+  return axis2quat(UniformlyRandomAxisAngle(generator));
+}
+
+template< class Generator >
+// TODO(#2274) Fix NOLINTNEXTLINE(runtime/references).
+Eigen::Matrix3d UniformlyRandomRotmat(Generator& generator) {
+  return axis2rotmat(UniformlyRandomAxisAngle(generator));
+}
+
+template< class Generator >
+// TODO(#2274) Fix NOLINTNEXTLINE(runtime/references).
+Eigen::Vector3d UniformlyRandomRPY(Generator& generator) {
+  return axis2rpy(UniformlyRandomAxisAngle(generator));
+}
+
 }  // namespace math
 }  // namespace drake
