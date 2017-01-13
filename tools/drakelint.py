00ff94cc640212f95fc8bd3ed7f4fd2303bbfd28
David German <david.german@tri.global>
2017-01-13 18:58:26 -0500

    Add an infrastructure for Drake lint checks.
    Fixes #3150.

---
+++ tools/drakelint.py
@@ -0,0 +1,46 @@
+import sys
+
+# Returns 0 if all of the @p newlines are Unix, and 1 otherwise.
+def _check_invalid_line_endings(newlines):
+    for newline in newlines:
+        if newline != '\n':
+            print("Non-Unix newline characters found.")
+            return 1
+    return 0
+
+# Runs Drake lint checks on each path specified as a command-line argument.
+# Exits 1 if any of the paths are invalid or any lint checks fail.
+# Otherwise exits 0.    
+def main():
+    total_errors = 0
+    for filename in sys.argv[1:]:
+        # Open the file.
+        print("drakelint.py: Linting " + filename)
+        file = open(filename, 'rU')
+        if not file:
+            print("Unable to open " + filename)
+            sys.exit(1)
+            
+        # Read its data.
+        data = file.read()
+        newlines = file.newlines
+        
+        # Run tests and count errors.
+        errors = 0
+        errors += _check_invalid_line_endings(newlines)
+        
+        if errors != 0:
+            print(str(errors) + " errors detected.")
+        
+        # Wrap up.
+        file.close()
+        total_errors += errors
+        
+    if total_errors == 0:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
