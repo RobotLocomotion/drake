17888741c7a195e8c5646ee687f5d421adf29c22
Chien-Liang Fok <liang.fok@tri.global>
2017-01-16 20:34:08 -0500

    Adds a System 2.0-based gyroscope sensor.

---
+++ drake/systems/sensors/gyroscope_output.cc
@@ -0,0 +1,36 @@
+#include "drake/systems/sensors/gyroscope_output.h"
+
+#include <cmath>
+
+#include "drake/systems/sensors/gyroscope.h"
+
+namespace drake {
+namespace systems {
+namespace sensors {
+
+const int GyroscopeOutputConstants::kWx;
+const int GyroscopeOutputConstants::kWy;
+const int GyroscopeOutputConstants::kWz;
+
+template <typename T>
+GyroscopeOutput<T>::GyroscopeOutput() : BasicVector<double>(3) {
+  this->SetFromVector(VectorX<double>::Zero(3));
+}
+
+template <typename T>
+Vector3<T> GyroscopeOutput<T>::get_rotational_velocities() const {
+  // TODO(liang.fok) Optimize the following code by re-interpreting the three
+  // rotational velocity values, which are already sitting sequentially in
+  // BasicVector's memory, as a Vector3<T> with no copying.
+  Vector3<T> rotational_velocity(
+      BasicVector<T>::GetAtIndex(GyroscopeOutputConstants::kWx),
+      BasicVector<T>::GetAtIndex(GyroscopeOutputConstants::kWy),
+      BasicVector<T>::GetAtIndex(GyroscopeOutputConstants::kWz));
+  return rotational_velocity;
+}
+
+template class GyroscopeOutput<double>;
+
+}  // namespace sensors
+}  // namespace systems
+}  // namespace drake
