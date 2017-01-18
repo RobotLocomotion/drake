a998e218cb7d32a4a2ca21a2af8c4ea3637f1069
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-01-18 09:02:01 -0500

    Mark Value<T> as non-copyable
    In the presence of subclassing, only Clone() can be correct.

--- drake/systems/framework/value.h
+++ drake/systems/framework/value.h
@@ -145,14 +145,14 @@ class Value : public AbstractValue {
   explicit Value(const T& v) : value_(v) {}
   ~Value() override {}
 
-  // Values are copyable but not moveable.
-  Value(const Value<T>& other) = default;
-  Value& operator=(const Value<T>& other) = default;
+  // Values are neither copyable nor moveable.
+  Value(const Value<T>& other) = delete;
+  Value& operator=(const Value<T>& other) = delete;
   Value(Value<T>&& other) = delete;
   Value& operator=(Value<T>&& other) = delete;
 
   std::unique_ptr<AbstractValue> Clone() const override {
-    return std::make_unique<Value<T>>(*this);
+    return std::make_unique<Value<T>>(get_value());
   }
 
   void SetFrom(const AbstractValue& other) override {
