465c5e2f49f2221aca14ef3231633ddf30d6fe37
Chris Lalancette <clalancette@osrfoundation.org>
2017-01-17 16:43:51 -0800

    Add in a test to ensure that we are building and linking SDFormat properly.
    Note that this version is linked into the CMake build system.
    Bazel support will be added next.
    
    Signed-off-by: Chris Lalancette <clalancette@osrfoundation.org>

---
+++ drake/multibody/test/sdformat_test.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include <sdf/sdf.hh>
+
+namespace drake {
+namespace systems {
+namespace plants {
+namespace test {
+namespace {
+
+GTEST_TEST(SDFormatTest, TestBasic) {
+  std::string sdfstr("<?xml version='1.0'?><sdf version='1.6'><model name='my_model'><link name='link'/></model></sdf>");
+  sdf::SDFPtr sdfParsed(new sdf::SDF());
+  sdf::init(sdfParsed);
+
+  EXPECT_TRUE(sdf::readString(sdfstr, sdfParsed));
+
+  sdf::ElementPtr model = sdfParsed->Root()->GetElement("model");
+  std::string modelName = model->Get<std::string>("name");
+
+  EXPECT_EQ(modelName, std::string("my_model"));
+
+  std::string linkName = model->GetElement("link")->Get<std::string>("name");
+  EXPECT_EQ(linkName, std::string("link"));
+}
+
+}  // namespace
+}  // namespace test
+}  // namespace plants
+}  // namespace systems
+}  // namespace drake
