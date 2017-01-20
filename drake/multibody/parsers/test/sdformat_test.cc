cb93ad54a4dd60421d87cfe784bef334328142bb
Chris Lalancette <clalancette@osrfoundation.org>
2017-01-19 16:29:10 -0800

    Based on review, make the sdfstr const.
    Signed-off-by: Chris Lalancette <clalancette@osrfoundation.org>

--- drake/multibody/parsers/test/sdformat_test.cc
+++ drake/multibody/parsers/test/sdformat_test.cc
@@ -11,7 +11,7 @@ namespace test {
 namespace {
 
 GTEST_TEST(SDFormatTest, TestBasic) {
-  std::string sdfstr("<?xml version='1.0'?><sdf version='1.6'><model name='my_model'><link name='link'/></model></sdf>");
+  const std::string sdfstr("<?xml version='1.0'?><sdf version='1.6'><model name='my_model'><link name='link'/></model></sdf>");
   sdf::SDFPtr sdfParsed(new sdf::SDF());
   sdf::init(sdfParsed);
 
