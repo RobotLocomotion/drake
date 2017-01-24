48f70382c203d7fdc9556b73f12cdbd8c6904a8f
Scott Kuindersma <kuindersma@gmail.com>
2017-01-24 17:20:24 -0500

    working

--- drake/examples/CompassGait/runDircolCycle.m
+++ drake/examples/CompassGait/runDircolCycle.m
@@ -9,7 +9,7 @@ N = [15;15];
 options.u_const_across_transitions = true;
 options.periodic = true;
 
-traj_opt = HybridTrajectoryOptimization(@DircolTrajectoryOptimization, p,[1;2],N,{[.2 .5],[.2 .5]},options);
+traj_opt = HybridTrajectoryOptimization(@DirtranTrajectoryOptimization, p,[1;2],N,{[.2 .5],[.2 .5]}, options);
 
 x0 = [0;0;2;-.4];
 t1 = .417;
@@ -35,6 +35,13 @@ traj_opt = traj_opt.compile();
 tic
 [xtraj,utraj,z,F,info] = solveTraj(traj_opt,t_init,traj_init);
 toc
+
+xtraj = shiftFlipAppend(xtraj);
+
+%  
+% xtraj2 = xtraj;
+% 
+% xtraj = HybridTrajectory({xtraj, xtraj2});
 if (nargout<1)
   v = CompassGaitVisualizer(p);
   figure(1); clf;
@@ -44,9 +51,11 @@ if (nargout<1)
   fnplt(xtraj,[2 4]);
   fnplt(xtraj,[3 5]);
   
-  playback(v,xtraj);
-  
+  playback(v,xtraj,struct('slider',true));
+
 end
+
+keyboard
 end
 
 function [g,dg] = cost(t,x,u);
@@ -68,3 +77,31 @@ dphidx = [1,1,0,0]; % gradient of foot collision guard 1 w/respect to x
 J=100*abs(dphidx*xdot0)./norm(dphidx)./norm(xdot0);
 end
 
+
+function xtraj_ = flipLeftRight(xtraj)
+
+    tshift = xtraj.traj{2}.tspan(1);
+    mode1_mode_traj = xtraj.traj{2}.trajs{1};
+    mode1_mode_traj = mode1_mode_traj.shiftTime(-tshift);
+    mode1_xtraj = xtraj.traj{1}.trajs{2};
+    mode1_mixed_traj = MixedTrajectory({mode1_mode_traj, mode1_xtraj},xtraj.traj{1}.indices);
+    mode1_mixed_traj = mode1_mixed_traj.setOutputFrame(xtraj.traj{2}.getOutputFrame);
+        
+    mode2_mode_traj = xtraj.traj{1}.trajs{1};
+    mode2_mode_traj = mode2_mode_traj.shiftTime(tshift);
+    mode2_xtraj = xtraj.traj{2}.trajs{2};
+    mode2_mixed_traj = MixedTrajectory({mode2_mode_traj, mode2_xtraj},xtraj.traj{1}.indices);
+    mode2_mixed_traj = mode2_mixed_traj.setOutputFrame(xtraj.traj{1}.getOutputFrame);
+
+    xtraj_ = HybridTrajectory({mode1_mixed_traj,mode2_mixed_traj});
+
+end
+
+function xtraj_ = shiftFlipAppend(xtraj)
+
+    xtraj_flip = flipLeftRight(xtraj);
+    xtraj_flip = xtraj_flip.shiftTime(xtraj.tspan(2));
+    
+    xtraj_ = HybridTrajectory({xtraj.traj{1},xtraj.traj{2},xtraj_flip.traj{1},xtraj_flip.traj{2}});
+    
+end
