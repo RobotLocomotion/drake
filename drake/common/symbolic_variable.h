da47ef0c5752fbce5f87ee45b9fa83271712dd30
Soonho Kong <soonho.kong@tri.global>
2017-01-26 11:09:45 -0500

    Introduce symbolic::Variable::Id (= size_t)

--- drake/common/symbolic_variable.h
+++ drake/common/symbolic_variable.h
@@ -16,6 +16,8 @@ namespace symbolic {
 /** Represents a symbolic variable. */
 class Variable {
  public:
+  typedef size_t Id;
+
   DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(Variable)
 
   /** Default constructor. Constructs a dummy variable. This is needed to have
@@ -35,8 +37,8 @@ class Variable {
   /** Checks if this is a dummy variable (ID = 0) which is created by
    *  the default constructor. */
   bool is_dummy() const { return get_id() == 0; }
-  size_t get_id() const;
-  size_t get_hash() const { return std::hash<size_t>{}(id_); }
+  Id get_id() const;
+  size_t get_hash() const { return std::hash<Id>{}(id_); }
   std::string get_name() const;
   std::string to_string() const;
 
@@ -44,8 +46,8 @@ class Variable {
 
  private:
   // Produces a unique ID for a variable.
-  static size_t get_next_id();
-  size_t id_{};       // Unique identifier.
+  static Id get_next_id();
+  Id id_{};           // Unique identifier.
   std::string name_;  // Name of variable.
 };
 
