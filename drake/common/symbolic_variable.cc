40af7eea39e1b270366b6f5ea48b73a60ea49094
Soonho Kong <soonho.kong@tri.global>
2017-01-26 11:09:45 -0500

    Introduce symbolic::Variable::Id (= size_t)

--- drake/common/symbolic_variable.cc
+++ drake/common/symbolic_variable.cc
@@ -15,18 +15,18 @@ using std::string;
 namespace drake {
 namespace symbolic {
 
-size_t Variable::get_next_id() {
+Variable::Id Variable::get_next_id() {
   // Note that id 0 is reserved for anonymous variable which is created by the
   // default constructor, Variable(). As a result, we have an invariant
   // "get_next_id() > 0".
-  static never_destroyed<atomic<size_t>> next_id(1);
+  static never_destroyed<atomic<Id>> next_id(1);
   return next_id.access()++;
 }
 
 Variable::Variable(const string& name) : id_{get_next_id()}, name_{name} {
   DRAKE_ASSERT(id_ > 0);
 }
-size_t Variable::get_id() const { return id_; }
+Variable::Id Variable::get_id() const { return id_; }
 string Variable::get_name() const { return name_; }
 string Variable::to_string() const {
   ostringstream oss;
