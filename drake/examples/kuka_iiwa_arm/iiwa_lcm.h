1fec122c4fd41e2dbce348834c07c5602c73c1fc
Sam Creasey <sam.creasey@tri.global>
2017-01-26 10:58:32 -0500

    Calculate a target velocity estimate for the arm based on the two most recently received LCM commands.
    This isn't a complete fix for simulation fidelity issues with the arm,
    but it is significantly better (qualitatively) than before.  It does
    not address non-LCM control of the arm where a trajectory is fed
    directly through a diagram.

--- drake/examples/kuka_iiwa_arm/iiwa_lcm.h
+++ drake/examples/kuka_iiwa_arm/iiwa_lcm.h
@@ -5,6 +5,7 @@
 
 #include <memory>
 
+#include "drake/common/eigen_types.h"
 #include "drake/systems/framework/leaf_system.h"
 
 namespace drake {
@@ -13,23 +14,38 @@ namespace kuka_iiwa_arm {
 
 /// Handles lcmt_iiwa_command messages from a LcmSubscriberSystem.
 /// Has a single output port which publishes the commanded position
-/// for each joint.  All values on the output port will be zero if no
-/// LCM message has arrived with a command yet.
+/// for each joint along with an estimate of the commanded velocity
+/// for each joint.
 class IiwaCommandReceiver : public systems::LeafSystem<double> {
  public:
   IiwaCommandReceiver();
 
+  /// Sets the initial position of the controlled iiwa prior to any
+  /// commands being received.  @p x contains the starting position.
+  /// This position will be the commanded position (with zero
+  /// velocity) until a position message is received.  If this
+  /// function is not called, the starting position will be the zero
+  /// configuration.
+  void set_initial_position(
+      systems::Context<double>* context,
+      const Eigen::Ref<const VectorX<double>> x) const;
+
+ protected:
   void DoCalcOutput(const systems::Context<double>& context,
                     systems::SystemOutput<double>* output) const override;
+
+  void DoCalcDiscreteVariableUpdates(
+      const systems::Context<double>& context,
+      systems::DiscreteState<double>* discrete_state) const override;
 };
 
 /// Creates and outputs lcmt_iiwa_status messages.
 ///
-/// This system has two vector-valued input ports, one for the plant's current
-/// state and one for the most recently received command. The input port
-/// containing the plant's state must be twice the size of the input port
-/// containing the most recently received command. This is because there should
-/// be a joint position state and velocity state for each commanded joint.
+/// This system has two vector-valued input ports, one for the plant's
+/// current state and one for the most recently received command.
+/// Both ports contain a position and velocity for each joint
+/// (velocity is unused, this is done to be more readily compatible
+/// with the outputs from IiwaCommandReceiver and RigidBodyPlant).
 ///
 /// This system has one abstract valued output port that contains a
 /// systems::Value object templated on type `lcmt_iiwa_status`. Note that this
