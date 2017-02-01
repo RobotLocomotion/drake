76b22c6d117a9e7dba1c8ba8c555fb90a6641743
Kunimatsu Hashimoto <kunimatsu.hashimoto@tri.global>
2017-02-01 14:49:30 -0500

    sensors::CameraInfo: Add CameraInfo class and the unit test

---
+++ drake/systems/sensors/camera_info.cc
@@ -0,0 +1,15 @@
+#include "drake/systems/sensors/camera_info.h"
+
+namespace drake {
+namespace systems {
+namespace sensors {
+
+CameraInfo::CameraInfo(uint32_t width, uint32_t height, double vertical_fov_rad)
+    : CameraInfo(width, height, width * 0.5 / std::tan(
+          0.5 * width / height * vertical_fov_rad),
+                 height * 0.5 / std::tan(0.5 * vertical_fov_rad),
+                 width * 0.5, height * 0.5) {}
+
+}  // namespace sensors
+}  // namespace systems
+}  // namespace drake
