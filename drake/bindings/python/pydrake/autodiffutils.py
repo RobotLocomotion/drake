f66b7af22b9ec21175a1850239663d45756dc73d
Robin Deits <robin.deits@gmail.com>
2017-02-01 17:38:11 -0500

    comments

--- drake/bindings/python/pydrake/autodiffutils.py
+++ drake/bindings/python/pydrake/autodiffutils.py
@@ -6,6 +6,14 @@ from ._pydrake_autodiffutils import *
 
 
 def wrap(wrapper_type, x):
+    """
+    Construct an instance of the given opaque C++ wrapper type from
+    input x, which should be a numpy ndarray. For example, to wrap
+    a vector of autodiff scalars, you would do:
+
+        wrap(autodiffutils.VectorXAutoDiffXd, x)
+
+    """
     wrapped = wrapper_type(x.shape)
     for i in range(x.size):
         wrapped[i] = x.flat[i]
@@ -13,6 +21,10 @@ def wrap(wrapper_type, x):
 
 
 def unwrap(wrapped):
+    """
+    Given an opaque C++ wrapper type, copy its contents into a new
+    numpy ndarray of wrapped scalars
+    """
     unwrapped = np.empty(wrapped.size(), dtype=AutoDiffXd)
     for i in range(wrapped.size()):
         unwrapped.flat[i] = wrapped[i]
--- drake/bindings/python/pydrake/forwarddiff.py
+++ drake/bindings/python/pydrake/forwarddiff.py
@@ -6,12 +6,22 @@ from .autodiffutils import AutoDiffXd
 
 
 def derivative(function, x):
+    """
+    Compute the derivative of the function evaluated at the scalar input x
+    using Eigen's automatic differentiation.
+    The function should be scalar-input and scalar-output.
+    """
     x_ad = AutoDiffXd(x, np.ones(1))
     y_ad = function(x_ad)
     return y_ad.derivatives()[0]
 
 
 def gradient(function, x):
+    """
+    Compute the gradient of the function evaluated at the vector input x
+    using Eigen's automatic differentiation.
+    The function should be vector-input and scalar-output.
+    """
     x_ad = np.empty(x.shape, dtype=AutoDiffXd)
     for i in range(x.size):
         der = np.zeros(x.size)
@@ -22,6 +32,11 @@ def gradient(function, x):
 
 
 def jacobian(function, x):
+    """
+    Compute the jacobian of the function evaluated at the vector input x
+    using Eigen's automatic differentiation.
+    The function should be vector-input and vector-output.
+    """
     x_ad = np.empty(x.shape, dtype=np.object)
     for i in range(x.size):
         der = np.zeros(x.size)
@@ -30,6 +45,12 @@ def jacobian(function, x):
     y_ad = function(x_ad)
     return np.vstack([y.derivatives() for y in y_ad]).reshape(y_ad.shape + (-1,))
 
+
+# Method overloads:
+# These are obviously not a complete set of mathematical functions for
+# autodiff numbers. Rather, they exist just as a demonstration of *how*
+# to overload individual mathematical functions to work with both
+# AutoDiff and numeric inputs.
 
 def sin(x):
     if isinstance(x, AutoDiffXd):
