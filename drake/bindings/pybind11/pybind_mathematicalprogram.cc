d103ad01cb550e9d76bfc3594d89def62014bc3e
Robin Deits <robin.deits@gmail.com>
2017-01-31 21:53:02 -0500

    support formula constraints in python

--- drake/bindings/pybind11/pybind_mathematicalprogram.cc
+++ drake/bindings/pybind11/pybind_mathematicalprogram.cc
@@ -8,133 +8,261 @@
 
 namespace py = pybind11;
 
+using drake::Variable;
+using drake::symbolic::Expression;
+using drake::symbolic::Formula;
 
 PYBIND11_MAKE_OPAQUE(drake::solvers::VectorXDecisionVariable);
 
 PYBIND11_PLUGIN(_pybind_mathematicalprogram) {
-  py::module m("_pybind_mathematicalprogram", "Drake MathematicalProgram Bindings");
+  py::module m("_pybind_mathematicalprogram",
+               "Drake MathematicalProgram Bindings");
 
   py::class_<drake::solvers::MathematicalProgram>(m, "MathematicalProgram")
     .def(py::init<>())
-    .def("_NewContinuousVariables", (drake::solvers::VectorXDecisionVariable (drake::solvers::MathematicalProgram::*)(
-            size_t,
-            const std::string&)) &drake::solvers::MathematicalProgram::NewContinuousVariables,
+    .def("_NewContinuousVariables", (drake::solvers::VectorXDecisionVariable
+          (drake::solvers::MathematicalProgram::*)(
+          size_t,
+          const std::string&))
+         &drake::solvers::MathematicalProgram::NewContinuousVariables,
          py::arg("rows"),
          py::arg("name") = "x")
-    .def("_NewBinaryVariables", (drake::solvers::VectorXDecisionVariable (drake::solvers::MathematicalProgram::*)(
-            size_t,
-            const std::string&)) &drake::solvers::MathematicalProgram::NewBinaryVariables,
+    .def("_NewBinaryVariables", (drake::solvers::VectorXDecisionVariable
+         (drake::solvers::MathematicalProgram::*)(
+         size_t,
+         const std::string&))
+         &drake::solvers::MathematicalProgram::NewBinaryVariables,
          py::arg("rows"),
          py::arg("name") = "x")
-    .def("AddLinearConstraint", 
+    .def("AddLinearConstraint",
          (drake::solvers::Binding<drake::solvers::LinearConstraint>
           (drake::solvers::MathematicalProgram::*)(
-          const drake::symbolic::Expression&,
-          double, 
-          double)) &drake::solvers::MathematicalProgram::AddLinearConstraint)
+          const Expression&,
+          double,
+          double))
+          &drake::solvers::MathematicalProgram::AddLinearConstraint)
+    .def("AddLinearConstraint",
+         (drake::solvers::Binding<drake::solvers::LinearConstraint>
+          (drake::solvers::MathematicalProgram::*)(
+          const Formula&))
+          &drake::solvers::MathematicalProgram::AddLinearConstraint)
     .def("AddLinearCost",
          (drake::solvers::Binding<drake::solvers::LinearConstraint>
           (drake::solvers::MathematicalProgram::*)(
-          const drake::symbolic::Expression&)) &drake::solvers::MathematicalProgram::AddLinearCost)
+          const Expression&))
+          &drake::solvers::MathematicalProgram::AddLinearCost)
     .def("Solve", &drake::solvers::MathematicalProgram::Solve)
     .def("_GetSolution", [](const drake::solvers::MathematicalProgram& prog,
-                            const drake::symbolic::Variable& var) {
+                            const Variable& var) {
       return prog.GetSolution(var);
     })
-    .def("_GetSolution", [](const drake::solvers::MathematicalProgram& prog,
-                            const drake::solvers::VectorXDecisionVariable& var) {
+    .def("_GetSolution",
+         [](const drake::solvers::MathematicalProgram& prog,
+            const drake::solvers::VectorXDecisionVariable& var) {
       return prog.GetSolution(var);
-    })
-    ;
+    });
 
   py::enum_<drake::solvers::SolutionResult>(m, "SolutionResult")
     .value("kSolutionFound", drake::solvers::SolutionResult::kSolutionFound)
     .value("kInvalidInput", drake::solvers::SolutionResult::kInvalidInput)
-    .value("kInfeasibleConstraints", drake::solvers::SolutionResult::kInfeasibleConstraints)
+    .value("kInfeasibleConstraints",
+           drake::solvers::SolutionResult::kInfeasibleConstraints)
     .value("kUnknownError", drake::solvers::SolutionResult::kUnknownError);
 
-  py::class_<drake::symbolic::Variable>(m, "Variable")
+  py::class_<Variable>(m, "Variable")
     .def(py::init<const std::string&>())
-    .def("__add__", [](const drake::symbolic::Variable& self, const drake::symbolic::Variable& other) {
-      return drake::symbolic::Expression{self + other};
+    .def("__add__", [](const Variable& self,
+                       const Variable& other) {
+      return Expression{self + other};
     })
-    .def("__add__", [](const drake::symbolic::Variable& self, double other) {
-      return drake::symbolic::Expression{self + other};
+    .def("__add__", [](const Variable& self, double other) {
+      return Expression{self + other};
     })
-    .def("__radd__", [](const drake::symbolic::Variable& self, double other) {
-      return drake::symbolic::Expression{other + self};
+    .def("__radd__", [](const Variable& self, double other) {
+      return Expression{other + self};
     })
-    .def("__sub__", [](const drake::symbolic::Variable& self, const drake::symbolic::Variable& other) {
-      return drake::symbolic::Expression{self - other};
+    .def("__sub__", [](const Variable& self,
+                       const Variable& other) {
+      return Expression{self - other};
     })
-    .def("__sub__", [](const drake::symbolic::Variable& self, double other) {
-      return drake::symbolic::Expression{self - other};
+    .def("__sub__", [](const Variable& self, double other) {
+      return Expression{self - other};
     })
-    .def("__rsub__", [](const drake::symbolic::Variable& self, double other) {
-      return drake::symbolic::Expression{other - self};
+    .def("__rsub__", [](const Variable& self, double other) {
+      return Expression{other - self};
     })
-    .def("__mul__", [](const drake::symbolic::Variable& self, const drake::symbolic::Variable& other) {
-      return drake::symbolic::Expression{self * other};
+    .def("__mul__", [](const Variable& self,
+                       const Variable& other) {
+      return Expression{self * other};
     })
-    .def("__mul__", [](const drake::symbolic::Variable& self, double other) {
-      return drake::symbolic::Expression{self * other};
+    .def("__mul__", [](const Variable& self, double other) {
+      return Expression{self * other};
     })
-    .def("__rmul__", [](const drake::symbolic::Variable& self, double other) {
-      return drake::symbolic::Expression{other * self};
+    .def("__rmul__", [](const Variable& self, double other) {
+      return Expression{other * self};
     })
-    .def("__truediv__", [](const drake::symbolic::Variable& self, const drake::symbolic::Variable& other) {
-      return drake::symbolic::Expression{self / other};
+    .def("__truediv__", [](const Variable& self,
+                           const Variable& other) {
+      return Expression{self / other};
     })
-    .def("__truediv__", [](const drake::symbolic::Variable& self, double other) {
-      return drake::symbolic::Expression{self / other};
+    .def("__truediv__", [](const Variable& self,
+                           double other) {
+      return Expression{self / other};
     })
-    .def("__rtruediv__", [](const drake::symbolic::Variable& self, double other) {
-      return drake::symbolic::Expression{other / self};
+    .def("__rtruediv__", [](const Variable& self,
+                            double other) {
+      return Expression{other / self};
     })
-    ;
+    .def("__lt__", [](const Variable& self,
+                      const Variable& other) {
+      return Formula{Expression(self) < Expression(other)};
+    })
+    .def("__lt__", [](const Variable& self,
+                      double other) {
+      return Formula{Expression(self) < Expression(other)};
+    })
+    .def("__le__", [](const Variable& self,
+                      const Variable& other) {
+      return Formula{Expression(self) <= Expression(other)};
+    })
+    .def("__le__", [](const Variable& self,
+                      double other) {
+      return Formula{Expression(self) <= Expression(other)};
+    })
+    .def("__gt__", [](const Variable& self,
+                      const Variable& other) {
+      return Formula{Expression(self) > Expression(other)};
+    })
+    .def("__gt__", [](const Variable& self,
+                      double other) {
+      return Formula{Expression(self) > Expression(other)};
+    })
+    .def("__ge__", [](const Variable& self,
+                      const Variable& other) {
+      return Formula{Expression(self) >= Expression(other)};
+    })
+    .def("__ge__", [](const Variable& self,
+                      double other) {
+      return Formula{Expression(self) >= Expression(other)};
+    })
+    .def("__eq__", [](const Variable& self,
+                      const Variable& other) {
+      return Formula{Expression(self) == Expression(other)};
+    })
+    .def("__eq__", [](const Variable& self,
+                      double other) {
+      return Formula{Expression(self) == Expression(other)};
+    });
 
-  py::class_<drake::symbolic::Expression>(m, "Expression")
+
+  py::class_<Expression>(m, "Expression")
     .def(py::init<>())
-    .def(py::init<const drake::symbolic::Variable&>())
-    .def(py::self                    + py::self)
-    .def(py::self                    + drake::symbolic::Variable())
-    .def(drake::symbolic::Variable() + py::self)
-    .def(py::self                    + double())
-    .def(double()                    + py::self)
-    .def(py::self                    - py::self)
-    .def(py::self                    - drake::symbolic::Variable())
-    .def(drake::symbolic::Variable() - py::self)
-    .def(py::self                    - double())
-    .def(double()                    - py::self)
-    .def(py::self                    * py::self)
-    .def(py::self                    * drake::symbolic::Variable())
-    .def(drake::symbolic::Variable() * py::self)
-    .def(py::self                    * double())
-    .def(double()                    * py::self)
-    .def(py::self                    / py::self)
-    .def(py::self                    / drake::symbolic::Variable())
-    .def(drake::symbolic::Variable() / py::self)
-    .def(py::self                    / double())
-    .def(double()                    / py::self)
-    ;
+    .def(py::init<const Variable&>())
+    .def(py::self   + py::self)
+    .def(py::self   + Variable())
+    .def(Variable() + py::self)
+    .def(py::self   + double())
+    .def(double()   + py::self)
+    .def(py::self   - py::self)
+    .def(py::self   - Variable())
+    .def(Variable() - py::self)
+    .def(py::self   - double())
+    .def(double()   - py::self)
+    .def(py::self   * py::self)
+    .def(py::self   * Variable())
+    .def(Variable() * py::self)
+    .def(py::self   * double())
+    .def(double()   * py::self)
+    .def(py::self   / py::self)
+    .def(py::self   / Variable())
+    .def(Variable() / py::self)
+    .def(py::self   / double())
+    .def(double()   / py::self)
+    .def("__lt__", [](const Expression& self,
+                      const Variable& other) {
+      return Formula{Expression(self) < Expression(other)};
+    })
+    .def("__lt__", [](const Expression& self,
+                      const Expression& other) {
+      return Formula{Expression(self) < Expression(other)};
+    })
+    .def("__lt__", [](const Expression& self,
+                      double other) {
+      return Formula{Expression(self) < Expression(other)};
+    })
+    .def("__le__", [](const Expression& self,
+                      const Variable& other) {
+      return Formula{Expression(self) <= Expression(other)};
+    })
+    .def("__le__", [](const Expression& self,
+                      const Expression& other) {
+      return Formula{Expression(self) <= Expression(other)};
+    })
+    .def("__le__", [](const Expression& self,
+                      double other) {
+      return Formula{Expression(self) <= Expression(other)};
+    })
+    .def("__gt__", [](const Expression& self,
+                      const Variable& other) {
+      return Formula{Expression(self) > Expression(other)};
+    })
+    .def("__gt__", [](const Expression& self,
+                      const Expression& other) {
+      return Formula{Expression(self) > Expression(other)};
+    })
+    .def("__gt__", [](const Expression& self,
+                      double other) {
+      return Formula{Expression(self) > Expression(other)};
+    })
+    .def("__ge__", [](const Expression& self,
+                      const Variable& other) {
+      return Formula{Expression(self) >= Expression(other)};
+    })
+    .def("__ge__", [](const Expression& self,
+                      const Expression& other) {
+      return Formula{Expression(self) >= Expression(other)};
+    })
+    .def("__ge__", [](const Expression& self,
+                      double other) {
+      return Formula{Expression(self) >= Expression(other)};
+    })
+    .def("__eq__", [](const Expression& self,
+                      const Variable& other) {
+      return Formula{Expression(self) == Expression(other)};
+    })
+    .def("__eq__", [](const Expression& self,
+                      const Expression& other) {
+      return Formula{Expression(self) == Expression(other)};
+    })
+    .def("__eq__", [](const Expression& self,
+                      double other) {
+      return Formula{Expression(self) == Expression(other)};
+    });
 
+  py::class_<Formula>(m, "Formula");
 
-  py::class_<drake::solvers::VectorXDecisionVariable>(m, "_VectorXDecisionVariable")
+  py::class_<drake::solvers::VectorXDecisionVariable>(
+    m, "_VectorXDecisionVariable")
     .def(py::init<size_t>())
     .def("__len__", [](const drake::solvers::VectorXDecisionVariable& v) {
       return v.size();
     })
-    .def("__getitem__", [](const drake::solvers::VectorXDecisionVariable& v, size_t i) {
+    .def("__getitem__", [](const drake::solvers::VectorXDecisionVariable& v,
+                           size_t i) {
       return v(i);
     }, py::return_value_policy::reference)
-    .def("__setitem__", [](drake::solvers::VectorXDecisionVariable& v, size_t i, const drake::symbolic::Variable &var) {
+    .def("__setitem__", [](drake::solvers::VectorXDecisionVariable& v,
+                           size_t i, const Variable &var) {
       v(i) = var;
     })
     ;
 
-  py::class_<drake::solvers::LinearConstraint, std::shared_ptr<drake::solvers::LinearConstraint> >(m, "LinearConstraint");
+  py::class_<drake::solvers::LinearConstraint,
+             std::shared_ptr<drake::solvers::LinearConstraint> >(
+    m, "LinearConstraint");
 
-  py::class_<drake::solvers::Binding<drake::solvers::LinearConstraint> >(m, "Binding_LinearConstraint");
+  py::class_<drake::solvers::Binding<drake::solvers::LinearConstraint> >(
+    m, "Binding_LinearConstraint");
 
   return m.ptr();
 }
--- drake/bindings/python/pydrake/test/testMathematicalProgram.py
+++ drake/bindings/python/pydrake/test/testMathematicalProgram.py
@@ -17,7 +17,7 @@ class TestMathematicalProgram(unittest.TestCase):
         c = np.array([-1.0, -1.0, -2.0])
         prog.AddLinearCost(c.dot(x))
         a = np.array([1.0, 2.0, 3.0])
-        prog.AddLinearConstraint(a.dot(x), -np.inf, 4)
+        prog.AddLinearConstraint(a.dot(x) <= 4)
         prog.AddLinearConstraint(x[0] + x[1], 1, np.inf)
         result = prog.Solve()
         self.assertEqual(result, mp.SolutionResult.kSolutionFound)
@@ -29,16 +29,6 @@ class TestMathematicalProgram(unittest.TestCase):
         # Also test by asking for the value of each element of x
         for i in range(3):
             self.assertAlmostEqual(prog.GetSolution(x[i]), x_expected[i])
-
-    def test_expression_constraints(self):
-        prog = mp.MathematicalProgram()
-        x = prog.NewContinuousVariables(2, "x")
-        ex = mp.Expression(x[0]) + x[1]
-        prog.AddLinearConstraint(ex, -1, 1)
-
-
-
-
 
 
 if __name__ == '__main__':
