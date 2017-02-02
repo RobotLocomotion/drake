4144dfb89925374765b8e67ddb799a63796237b2
Matt Marjanovic <maddog@tri.global>
2017-02-02 12:29:07 -0800

    Clean up EndlessRoadCar unit test file a bit.

--- drake/automotive/test/endless_road_car_test.cc
+++ drake/automotive/test/endless_road_car_test.cc
@@ -53,29 +53,17 @@ class BaseEndlessRoadCarTest : public ::testing::Test {
 };
 
 
-
 // Type #1:  'kNone' control_type == no inputs
 class NoneEndlessRoadCarTest : public BaseEndlessRoadCarTest {
  protected:
   NoneEndlessRoadCarTest() {
-    //////////    BaseEndlessRoadCarTest::SetUp();
     dut_ = std::make_unique<EndlessRoadCar<double>>(
         "dut", infinite_road_.get(), EndlessRoadCar<double>::kNone);
 
     context_ = dut_->CreateDefaultContext();
     output_ = dut_->AllocateOutput(*context_);
     derivatives_ = dut_->AllocateTimeDerivatives();
-    /////    SetInputValue(0, 0, 0);
   }
-
-/////  void SetInputValue(double steering_angle, double throttle, double brake) {
-/////    auto value = std::make_unique<DrivingCommand<double>>();
-/////    value->set_steering_angle(steering_angle);
-/////    value->set_throttle(throttle);
-/////    value->set_brake(brake);
-/////    context_->SetInputPort(
-/////        0, std::make_unique<systems::FreestandingInputPort>(std::move(value)));
-/////  }
 
   EndlessRoadCarState<double>* continuous_state() {
     auto result = dynamic_cast<EndlessRoadCarState<double>*>(
@@ -92,9 +80,6 @@ class NoneEndlessRoadCarTest : public BaseEndlessRoadCarTest {
 
 TEST_F(NoneEndlessRoadCarTest, Topology) {
   ASSERT_EQ(0, dut_->get_num_input_ports());
-  ////  const auto& input_descriptor = dut_->get_input_port(0);
-  ////  EXPECT_EQ(systems::kVectorValued, input_descriptor.get_data_type());
-  ////  EXPECT_EQ(DrivingCommandIndices::kNumCoordinates, input_descriptor.size());
 
   ASSERT_EQ(1, dut_->get_num_output_ports());
   const auto& output_descriptor = dut_->get_output_port(0);
@@ -134,19 +119,10 @@ TEST_F(NoneEndlessRoadCarTest, Output) {
   continuous_state()->set_speed(-0.0006);
   dut_->CalcOutput(*context_, output_.get());
   EXPECT_EQ(0., result->speed());
-
-/////  // The input doesn't matter.
-/////  SetInputValue(0.3, 0.5, 0.7);
-/////  dut_->CalcOutput(*context_, output_.get());
-/////  EXPECT_EQ(1.0, result->x());
-/////  EXPECT_EQ(2.0, result->y());
-/////  EXPECT_EQ(3.0, result->heading());
-/////  EXPECT_EQ(4.0, result->velocity());
 }
 
-TEST_F(NoneEndlessRoadCarTest, Derivatives) {
-  ///////  const double kTolerance = 1e-10;
 
+TEST_F(NoneEndlessRoadCarTest, Derivatives) {
   // Grab a pointer to where the EvalTimeDerivatives results end up.
   const EndlessRoadCarState<double>* const result =
       dynamic_cast<const EndlessRoadCarState<double>*>(
@@ -166,61 +142,6 @@ TEST_F(NoneEndlessRoadCarTest, Derivatives) {
   EXPECT_EQ(0.0, result->r());
   EXPECT_EQ(0.0, result->heading());
   EXPECT_EQ(0.0, result->speed());
-
-/////  // Half throttle yields half of the max acceleration.
-/////  const double max_acceleration =
-/////      EndlessRoadCar<double>::get_default_config().max_acceleration();
-/////  SetInputValue(0.0, 0.5, 0.0);
-/////  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-/////  EXPECT_EQ(0.0, result->x());
-/////  EXPECT_EQ(0.0, result->y());
-/////  EXPECT_EQ(0.0, result->heading());
-/////  EXPECT_NEAR(0.5 * max_acceleration, result->velocity(), kTolerance);
-/////
-/////  // Set speed to mid-range, with zero input.
-/////  continuous_state()->set_velocity(10.0);
-/////  SetInputValue(0.0, 0.0, 0.0);
-/////  // At heading 0, we are moving along +x.
-/////  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-/////  EXPECT_NEAR(10.0, result->x(), kTolerance);
-/////  EXPECT_EQ(0.0, result->y());
-/////  EXPECT_EQ(0.0, result->heading());
-/////  EXPECT_EQ(0.0, result->velocity());
-/////
-/////  // A non-zero steering_angle turns in the same direction.  We'd like to turn
-/////  // at 0.1 rad/s at a speed of 10m/s, so we want a curvature of 0.01.
-/////  const double wheelbase = EndlessRoadCar<double>::get_default_config().wheelbase();
-/////  const double steering_angle = std::atan(0.01 * wheelbase);
-/////  SetInputValue(steering_angle, 0.0, 0.0);
-/////  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-/////  EXPECT_NEAR(10.0, result->x(), kTolerance);
-/////  EXPECT_EQ(0.0, result->y());
-/////  EXPECT_NEAR(0.1, result->heading(), kTolerance);
-/////  EXPECT_EQ(0.0, result->velocity());
-/////  SetInputValue(-steering_angle, 0.0, 0.0);
-/////  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-/////  EXPECT_NEAR(10.0, result->x(), kTolerance);
-/////  EXPECT_EQ(0.0, result->y());
-/////  EXPECT_NEAR(-0.1, result->heading(), kTolerance);
-/////  EXPECT_EQ(0.0, result->velocity());
-/////
-/////  // Half brake yields half of the max acceleration.
-/////  SetInputValue(0.0, 0.0, 0.5);
-/////  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-/////  EXPECT_NEAR(10.0, result->x(), kTolerance);
-/////  EXPECT_EQ(0.0, result->y());
-/////  EXPECT_EQ(0.0, result->heading());
-/////  EXPECT_NEAR(-0.5 * EndlessRoadCar<double>::get_default_config().max_acceleration(),
-/////              result->velocity(), kTolerance);
-/////
-/////  // A heading of +90deg points us at +y.
-/////  continuous_state()->set_heading(0.5 * M_PI);
-/////  SetInputValue(0.0, 0.0, 0.0);
-/////  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-/////  EXPECT_NEAR(0.0, result->x(), kTolerance);
-/////  EXPECT_NEAR(10.0, result->y(), kTolerance);
-/////  EXPECT_EQ(0.0, result->heading());
-/////  EXPECT_EQ(0.0, result->velocity());
 }
 
 
@@ -569,8 +490,8 @@ TEST_F(IdmEndlessRoadCarTest, Derivatives) {
   EXPECT_EQ(0.0, result->r());
   EXPECT_EQ(0.0, result->heading());
   EXPECT_NEAR(max_acceleration, result->speed(), kTolerance);
-  // 3)  Current velocity matches IDM desired, but distance is minimal
-  ///    and differential-velocity is large
+  // 4)  Current velocity matches IDM desired, but distance is minimal
+  //     and differential-velocity is large.
   //     Longitudinal acceleration should be negative.
   continuous_state()->set_speed(30.);
   SetInputValue(2.0, 100.);
@@ -580,171 +501,6 @@ TEST_F(IdmEndlessRoadCarTest, Derivatives) {
   EXPECT_EQ(0.0, result->heading());
   EXPECT_GT(0.0, result->speed());
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-#if 0
-
-class EndlessRoadCarTest : public ::testing::Test {
- protected:
-  void SetUp() override {
-    dut_.reset(new EndlessRoadCar<double>);
-    context_ = dut_->CreateDefaultContext();
-    output_ = dut_->AllocateOutput(*context_);
-    derivatives_ = dut_->AllocateTimeDerivatives();
-    SetInputValue(0, 0, 0);
-  }
-
-  void SetInputValue(double steering_angle, double throttle, double brake) {
-    auto value = std::make_unique<DrivingCommand<double>>();
-    value->set_steering_angle(steering_angle);
-    value->set_throttle(throttle);
-    value->set_brake(brake);
-    context_->SetInputPort(
-        0, std::make_unique<systems::FreestandingInputPort>(std::move(value)));
-  }
-
-  EndlessRoadCarState<double>* continuous_state() {
-    auto result = dynamic_cast<EndlessRoadCarState<double>*>(
-        context_->get_mutable_continuous_state_vector());
-    if (result == nullptr) { throw std::bad_cast(); }
-    return result;
-  }
-
-  std::unique_ptr<systems::System<double>> dut_;  //< The device under test.
-  std::unique_ptr<systems::Context<double>> context_;
-  std::unique_ptr<systems::SystemOutput<double>> output_;
-  std::unique_ptr<systems::ContinuousState<double>> derivatives_;
-};
-
-TEST_F(EndlessRoadCarTest, Topology) {
-  ASSERT_EQ(1, dut_->get_num_input_ports());
-  const auto& input_descriptor = dut_->get_input_port(0);
-  EXPECT_EQ(systems::kVectorValued, input_descriptor.get_data_type());
-  EXPECT_EQ(DrivingCommandIndices::kNumCoordinates, input_descriptor.size());
-
-  ASSERT_EQ(1, dut_->get_num_output_ports());
-  const auto& output_descriptor = dut_->get_output_port(0);
-  EXPECT_EQ(systems::kVectorValued, output_descriptor.get_data_type());
-  EXPECT_EQ(EndlessRoadCarStateIndices::kNumCoordinates, output_descriptor.size());
-}
-
-TEST_F(EndlessRoadCarTest, Output) {
-  // Grab a pointer to where the CalcOutput results end up.
-  const EndlessRoadCarState<double>* const result =
-      dynamic_cast<const EndlessRoadCarState<double>*>(output_->get_vector_data(0));
-  ASSERT_NE(nullptr, result);
-
-  // Starting state and output is all zeros.
-  dut_->CalcOutput(*context_, output_.get());
-  EXPECT_EQ(0.0, result->x());
-  EXPECT_EQ(0.0, result->y());
-  EXPECT_EQ(0.0, result->heading());
-  EXPECT_EQ(0.0, result->velocity());
-
-  // New state just propagates through.
-  continuous_state()->set_x(1.0);
-  continuous_state()->set_y(2.0);
-  continuous_state()->set_heading(3.0);
-  continuous_state()->set_velocity(4.0);
-  dut_->CalcOutput(*context_, output_.get());
-  EXPECT_EQ(1.0, result->x());
-  EXPECT_EQ(2.0, result->y());
-  EXPECT_EQ(3.0, result->heading());
-  EXPECT_EQ(4.0, result->velocity());
-
-  // The input doesn't matter.
-  SetInputValue(0.3, 0.5, 0.7);
-  dut_->CalcOutput(*context_, output_.get());
-  EXPECT_EQ(1.0, result->x());
-  EXPECT_EQ(2.0, result->y());
-  EXPECT_EQ(3.0, result->heading());
-  EXPECT_EQ(4.0, result->velocity());
-}
-
-TEST_F(EndlessRoadCarTest, Derivatives) {
-  const double kTolerance = 1e-10;
-
-  // Grab a pointer to where the EvalTimeDerivatives results end up.
-  const EndlessRoadCarState<double>* const result =
-      dynamic_cast<const EndlessRoadCarState<double>*>(
-          derivatives_->get_mutable_vector());
-  ASSERT_NE(nullptr, result);
-
-  // Starting state is all zeros.
-  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-  EXPECT_EQ(0.0, result->x());
-  EXPECT_EQ(0.0, result->y());
-  EXPECT_EQ(0.0, result->heading());
-  EXPECT_EQ(0.0, result->velocity());
-
-  // Half throttle yields half of the max acceleration.
-  const double max_acceleration =
-      EndlessRoadCar<double>::get_default_config().max_acceleration();
-  SetInputValue(0.0, 0.5, 0.0);
-  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-  EXPECT_EQ(0.0, result->x());
-  EXPECT_EQ(0.0, result->y());
-  EXPECT_EQ(0.0, result->heading());
-  EXPECT_NEAR(0.5 * max_acceleration, result->velocity(), kTolerance);
-
-  // Set speed to mid-range, with zero input.
-  continuous_state()->set_velocity(10.0);
-  SetInputValue(0.0, 0.0, 0.0);
-  // At heading 0, we are moving along +x.
-  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-  EXPECT_NEAR(10.0, result->x(), kTolerance);
-  EXPECT_EQ(0.0, result->y());
-  EXPECT_EQ(0.0, result->heading());
-  EXPECT_EQ(0.0, result->velocity());
-
-  // A non-zero steering_angle turns in the same direction.  We'd like to turn
-  // at 0.1 rad/s at a speed of 10m/s, so we want a curvature of 0.01.
-  const double wheelbase = EndlessRoadCar<double>::get_default_config().wheelbase();
-  const double steering_angle = std::atan(0.01 * wheelbase);
-  SetInputValue(steering_angle, 0.0, 0.0);
-  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-  EXPECT_NEAR(10.0, result->x(), kTolerance);
-  EXPECT_EQ(0.0, result->y());
-  EXPECT_NEAR(0.1, result->heading(), kTolerance);
-  EXPECT_EQ(0.0, result->velocity());
-  SetInputValue(-steering_angle, 0.0, 0.0);
-  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-  EXPECT_NEAR(10.0, result->x(), kTolerance);
-  EXPECT_EQ(0.0, result->y());
-  EXPECT_NEAR(-0.1, result->heading(), kTolerance);
-  EXPECT_EQ(0.0, result->velocity());
-
-  // Half brake yields half of the max acceleration.
-  SetInputValue(0.0, 0.0, 0.5);
-  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-  EXPECT_NEAR(10.0, result->x(), kTolerance);
-  EXPECT_EQ(0.0, result->y());
-  EXPECT_EQ(0.0, result->heading());
-  EXPECT_NEAR(-0.5 * EndlessRoadCar<double>::get_default_config().max_acceleration(),
-              result->velocity(), kTolerance);
-
-  // A heading of +90deg points us at +y.
-  continuous_state()->set_heading(0.5 * M_PI);
-  SetInputValue(0.0, 0.0, 0.0);
-  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
-  EXPECT_NEAR(0.0, result->x(), kTolerance);
-  EXPECT_NEAR(10.0, result->y(), kTolerance);
-  EXPECT_EQ(0.0, result->heading());
-  EXPECT_EQ(0.0, result->velocity());
-}
-#endif
-
 
 }  // namespace
 }  // namespace automotive
