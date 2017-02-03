b65e9d69f179211b9b398f56719ae74bd0f98e5d
Evan Drumwright <drum@tri.global>
2017-02-02 16:25:00 -0800

    Fixed syntax error resulting from namespace switch.

--- drake/examples/bead_on_a_wire/bead_on_a_wire.cc
+++ drake/examples/bead_on_a_wire/bead_on_a_wire.cc
@@ -2,9 +2,11 @@
 #include "drake/examples/bead_on_a_wire/bead_on_a_wire-inl.h"
 
 namespace drake {
+namespace examples {
 namespace bead_on_a_wire {
 
 template class BeadOnAWire<double>;
 
 }  // namespace bead_on_a_wire
+}  // namespace examples
 }  // namespace drake
