2016af704b9410ca28ecec95f3b3a73fb1039625
Evan Drumwright <drum@tri.global>
2017-02-02 18:07:49 -0800

    Fixed another namespace-related switch error.

--- drake/examples/painleve/painleve-inl.h
+++ drake/examples/painleve/painleve-inl.h
@@ -16,6 +16,7 @@
 #include "drake/systems/framework/value.h"
 
 namespace drake {
+namespace examples {
 namespace painleve {
 
 template <typename T>
@@ -1022,4 +1023,5 @@ void Painleve<T>::SetDefaultState(const systems::Context<T>& context,
 }
 
 }  // namespace painleve
+}  // namespace examples
 }  // namespace drake
