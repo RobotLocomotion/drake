633b1b073b4314270d4da8c3133d0706df475d62
hongkai-dai <daihongkai@gmail.com>
2017-02-06 14:59:48 -0800

    add Degree(const Variables& vars) to symbolic::Expression

--- drake/common/symbolic_expression.h
+++ drake/common/symbolic_expression.h
@@ -176,6 +176,21 @@ class Expression {
    */
   Polynomial<double> ToPolynomial() const;
 
+ /**
+  * Returns the total degrees of the polynomial w.r.t the variables in
+  * @p vars. For example, the total degree of
+  * e = x^2*y + 2 * x*y*z^3 + x * z^2
+  * w.r.t (x, y) is 3 (from x^2 * y)
+  * w.r.t (x, z) is 4 (from x*y*z^3)
+  * w.r.t (z)    is 3 (from x*y*z^3)
+  * Throws a runtime error if
+  * 1. is_polynomial() is false
+  * 2. vars is not a subset of the variables in GetVariables();
+  * @param vars A set of variables.
+  * @return The total degree
+  */
+  int Degree(const Variables& vars) const;
+
   /** Evaluates under a given environment (by default, an empty environment).
    *  @throws std::runtime_error if NaN is detected during evaluation.
    */
