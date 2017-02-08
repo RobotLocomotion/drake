444a2c1a4feda779407b23fcb61159c0bb82d09c
Robin Deits <robin.deits@gmail.com>
2017-02-08 15:48:12 -0500

    todos

--- drake/bindings/python/pydrake/solvers/ik.py
+++ drake/bindings/python/pydrake/solvers/ik.py
@@ -22,7 +22,11 @@ class WorldPositionConstraint(_WorldPositionConstraint):
     def __init__(self, model, body, pts, lb, ub, tspan=None):
         pts = np.asarray(pts)
         if pts.ndim == 1:
-            # TODO: deprecate this method
+            # TODO: deprecate this method. We should require that pts be a 2-d array
+            # even when the second dimension is of length 1. When that requirement is
+            # made, we can delete this entire class and the renamed _WorldPositionConstraint
+            # import.
+            # Ref: https://github.com/RobotLocomotion/drake/issues/4950
             pts = pts.reshape((pts.size, 1))
         if tspan is None:
             super(WorldPositionConstraint, self).__init__(
@@ -36,7 +40,8 @@ class WorldPositionInFrameConstraint(_WorldPositionInFrameConstraint):
     def __init__(self, model, body, pts, T_world_to_frame, lb, ub, tspan=None):
         pts = np.asarray(pts)
         if pts.ndim == 1:
-            # TODO: deprecate this method
+            # TODO: deprecate this method, as with WorldPositionConstraint
+            # Ref: https://github.com/RobotLocomotion/drake/issues/4950
             pts = pts.reshape((pts.size, 1))
         if tspan is None:
             super(WorldPositionInFrameConstraint, self).__init__(
