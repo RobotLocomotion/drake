8f8905da79723b9a8791f784bab8e49adcce7c1f
Siyuan Feng <siyuan.feng@tri.global>
2017-02-08 16:28:18 -0500

    need to do style guide

--- drake/multibody/rigid_body_plant/visualization/contact_viz.py
+++ drake/multibody/rigid_body_plant/visualization/contact_viz.py
@@ -6,19 +6,23 @@ from director import applogic
 import numpy as np
 import drake as lcmdrakemsg
 
+
 class ContactVisualizer(object):
     def __init__(self):
         self.folderName = 'Contact Results'
         self.name = "Contact Visualizer"
         self.enabled = True
         # Subscribes to the LCM topic
-        lcmUtils.addSubscriber('CONTACT_RESULTS', messageClass=lcmdrakemsg.lcmt_contact_results_for_viz, callback=self.handleMessage)
+        lcmUtils.addSubscriber(
+            'CONTACT_RESULTS',
+            messageClass=lcmdrakemsg.lcmt_contact_results_for_viz,
+            callback=self.handleMessage)
 
     def isEnabled(self):
         return self.enabled
 
     def setEnabled(self, enable):
-        self.enabled = enable;
+        self.enabled = enable
 
     def handleMessage(self, msg):
 
@@ -35,8 +39,12 @@ class ContactVisualizer(object):
         # A map from pair of body names to a list of contact forces
         collision_pair_to_forces = {}
         for contact in msg.contact_info:
-            point = np.array([contact.contact_point[0], contact.contact_point[1], contact.contact_point[2]])
-            force = np.array([contact.contact_force[0], contact.contact_force[1], contact.contact_force[2]])
+            point = np.array([contact.contact_point[0],
+                              contact.contact_point[1],
+                              contact.contact_point[2]])
+            force = np.array([contact.contact_force[0],
+                              contact.contact_force[1],
+                              contact.contact_force[2]])
             mag = np.linalg.norm(force)
             if mag > 1e-4:
                 mag = 0.3 / mag
@@ -45,16 +53,20 @@ class ContactVisualizer(object):
             key2 = (str(contact.body2_name), str(contact.body1_name))
 
             if key1 in collision_pair_to_forces:
-                collision_pair_to_forces[key1].append((point, point + mag * force))
+                collision_pair_to_forces[key1].append((point, point + mag*force))
             elif key2 in collision_pair_to_forces:
-                collision_pair_to_forces[key2].append((point, point + mag * force))
+                collision_pair_to_forces[key2].append((point, point + mag*force))
             else:
-                collision_pair_to_forces[key1] = [(point, point + mag * force)]
+                collision_pair_to_forces[key1] = [(point, point + mag*force)]
 
         for key, list_of_forces in collision_pair_to_forces.iteritems():
             d = DebugData()
             for force_pair in list_of_forces:
-                d.addArrow(start=force_pair[0], end=force_pair[1], tubeRadius=0.005, headRadius=0.01)
+                d.addArrow(start=force_pair[0],
+                           end=force_pair[1],
+                           tubeRadius=0.005,
+                           headRadius=0.01)
+
             vis.showPolyData(d.getPolyData(), str(key), parent=folder, color=[0,1,0])
 
 
@@ -63,7 +75,8 @@ def initVisualizer():
     myVisualizer = ContactVisualizer()
 
     # add to tools menu
-    applogic.MenuActionToggleHelper('Tools', myVisualizer.name, myVisualizer.isEnabled, myVisualizer.setEnabled)
+    applogic.MenuActionToggleHelper('Tools', myVisualizer.name,
+                                    myVisualizer.isEnabled, myVisualizer.setEnabled)
     return myVisualizer
 
 # create visualizer when this script is executed
