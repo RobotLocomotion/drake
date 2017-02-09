cafc050382934f5e408fd816c0c0a2861e58abc9
Jon <jonathan.decastro@tri.global>
2017-02-09 14:54:06 -0500

    Doxygen, explicit instantiations

--- drake/systems/framework/hybrid_automaton_context.cc
+++ drake/systems/framework/hybrid_automaton_context.cc
@@ -1,1 +1,14 @@
+#include "drake/common/eigen_autodiff_types.h"
 #include "drake/systems/framework/hybrid_automaton_context.h"
+
+
+namespace drake {
+namespace systems {
+
+template class ModalSubsystem<double>;
+template class ModalSubsystem<AutoDiffXd>;
+template class ModalSubsystem<symbolic::Expression>;
+
+
+}  // namespace systems
+}  // namespace drake
