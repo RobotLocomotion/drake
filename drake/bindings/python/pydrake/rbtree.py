b42e306bd9b3958ad1c97cbeee6b62a354258600
Robin Deits <robin.deits@gmail.com>
2017-02-09 14:08:41 -0500

    one more unused import

--- drake/bindings/python/pydrake/rbtree.py
+++ drake/bindings/python/pydrake/rbtree.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
-import numpy as np
-
 from ._pydrake_rbtree import *
 
 # TODO: remove these direct imports and leave the constants
