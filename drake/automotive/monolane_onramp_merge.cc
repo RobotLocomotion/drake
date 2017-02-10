9ff9b65b4d7d5409a6d316c8fadb333788660cb9
Jonathan DeCastro <jonathan.decastro@tri.global>
2017-02-10 15:33:00 -0500

    Port a working figure-8 example for testing

--- drake/automotive/monolane_onramp_merge.cc
+++ drake/automotive/monolane_onramp_merge.cc
@@ -1,59 +1,70 @@
-#include "drake/automotive/maliput/api/road_geometry.h"
-#include "drake/automotive/maliput/monolane/builder.h"
+#include "monolane_onramp_merge.h"
 
 #include <cmath>
 #include <iostream>
 
-const double kLaneBoundsLength = 2.;
-const double kDriveableBoundsLength = 4.;
-const double kLinearTolerance = 0.01;
-const double kAngularTolerance = 0.01 * M_PI;
+#include "drake/automotive/maliput/api/road_geometry.h"
+#include "drake/automotive/maliput/monolane/builder.h"
+
+namespace drake {
+namespace maliput {
+namespace monolane {
+
+const double& kLinearTolerance = 0.01;
+const double& kAngularTolerance = 0.01 * M_PI;
 
 template <typename T>
-class MonolaneOnrampMerge {
- public:
-  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(MonolaneOnrampMerge)
+MonolaneOnrampMerge<T>::MonolaneOnrampMerge(const T& lane_length,
+                                            const T& lane_width,
+                                            const T& driveable_width)
+    : lane_bounds_(-lane_width / 2., lane_width / 2.),
+      driveable_bounds_{-driveable_width / 2., driveable_width / 2.} {
+  Build(lane_length);
+}
 
-  const api::RBounds kLaneBounds(-kLaneBoundsLength, kLaneBoundsLength);
-  const api::RBounds kDriveableBounds(-kDriveableBoundsLength,
-                                      kDriveableBoundsLength);
-  Builder b(kLaneBounds, kDriveableBounds, kLinearTolerance, kAngularTolerance);
+template <typename T>
+MonolaneOnrampMerge<T>::MonolaneOnrampMerge(const T& lane_length) {
+  Build(lane_length);
+}
 
-  const EndpointZ kLowFlatZ(0., 0., 0., 0.);
-  const EndpointZ kMidFlatZ(3., 0., 0., 0.);
-  const EndpointZ kMidTiltLeftZ(3., 0., -0.4, 0.);
-  const EndpointZ kMidTiltRightZ(3., 0., 0.4, 0.);
-  const EndpointZ kHighFlatZ(6., 0., 0., 0.);
+template <typename T>
+void MonolaneOnrampMerge<T>::Build(const T& lane_length) {
+  Builder b{lane_bounds_, driveable_bounds_, kLinearTolerance,
+        kAngularTolerance};
 
-  const ArcOffset kCounterClockwiseArc(50., 0.75 * M_PI);  // 135deg, 50m radius
-  const ArcOffset kClockwiseArc(50., -0.75 * M_PI);  // 135deg, 50m radius
+  const EndpointZ& kLowFlatZ{0., 0., 0., 0.};
+  const EndpointZ& kMidFlatZ{3., 0., 0., 0.};
+  const EndpointZ& kMidTiltLeftZ{3., 0., -0.4, 0.};
+  const EndpointZ& kMidTiltRightZ{3., 0., 0.4, 0.};
+  const EndpointZ& kHighFlatZ{6., 0., 0., 0.};
+
+  const ArcOffset& kCounterClockwiseArc{lane_length, 0.75 * M_PI};  // 135deg,
+                                                                    // 50m
+                                                                    // radius
+  const ArcOffset& kClockwiseArc{lane_length, -0.75 * M_PI};  // 135deg, 50m
+                                                              // radius
 
   Endpoint start {{0., 0., -M_PI / 4.}, kLowFlatZ};
 
-  auto c0 = b.Connect("0", start, 50., kMidFlatZ);
+  auto c0 = b.Connect("0", start, lane_length, kMidFlatZ);
 
   auto c1 = b.Connect("1", c0->end(), kCounterClockwiseArc, kMidTiltLeftZ);
   auto c2 = b.Connect("2", c1->end(), kCounterClockwiseArc, kMidFlatZ);
 
-  auto c3 = b.Connect("3", c2->end(), 50., kHighFlatZ);
-  auto c4 = b.Connect("4", c3->end(), 50., kMidFlatZ);
+  auto c3 = b.Connect("3", c2->end(), lane_length, kHighFlatZ);
+  auto c4 = b.Connect("4", c3->end(), lane_length, kMidFlatZ);
 
   auto c5 = b.Connect("5", c4->end(), kClockwiseArc, kMidTiltRightZ);
   auto c6 = b.Connect("6", c5->end(), kClockwiseArc, kMidFlatZ);
 
-  // Tweak ends to check if fuzzy-matching is working.
-  Endpoint c6end = c6->end();
-  c6end = Endpoint(c6end_xy, c6end_z);
-  EndpointZ c0start_z = c0->start().z();
-  c0start_z = EndpointZ(c0start_z.z() - kLinearTolerance * 0.5,
-                        c0start_z.z_dot(),
-                        c0start_z.theta(), c0start_z.theta_dot());
-
-  b.Connect("7", c6end, 50., c0start_z);
+  b.Connect("7", c6->end(), lane_length, c0->start().z());
 
   rg_ = b.Build({"figure-eight"});
-
 }
+
+// These instantiations must match the API documentation in
+// monolane_onramp_merge.h.
+template class MonolaneOnrampMerge<double>;
 
 }  // namespace monolane
 }  // namespace maliput
--- drake/automotive/monolane_onramp_merge.h
+++ drake/automotive/monolane_onramp_merge.h
@@ -1,19 +1,41 @@
-#include "drake/automotive/maliput/api/road_geometry.h"
-#include "drake/automotive/maliput/monolane/builder.h"
+#pragma once
 
 #include <cmath>
 #include <iostream>
+
+#include "drake/automotive/maliput/api/road_geometry.h"
+#include "drake/automotive/maliput/monolane/builder.h"
+
+namespace drake {
+namespace maliput {
+namespace monolane {
+
+const double& kDefaultLaneWidth = 2.;
+const double& kDefaultDriveableWidth = 4.;
 
 template <typename T>
 class MonolaneOnrampMerge {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(MonolaneOnrampMerge)
 
-  MonolaneOnrampMerge();
+  MonolaneOnrampMerge(const T& lane_length);
+
+  MonolaneOnrampMerge(const T& lane_length,
+                      const T& lane_width,
+                      const T& driveable_width);
+
+  void Build(const T& lane_length);
+
+  std::unique_ptr<const api::RoadGeometry> own_road_geometry() {
+    return std::move(rg_);
+  };
 
  private:
+  api::RBounds lane_bounds_{-kDefaultLaneWidth / 2., kDefaultLaneWidth / 2.};
+  api::RBounds driveable_bounds_{-kDefaultDriveableWidth / 2.,
+        kDefaultDriveableWidth / 2.};
   std::unique_ptr<const api::RoadGeometry> rg_;
-}
+};
 
 }  // namespace monolane
 }  // namespace maliput
