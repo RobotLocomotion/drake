c3c5629e3237fad776e6a05d221ac9bb83b4f997
Robin Deits <robin.deits@gmail.com>
2017-02-13 16:16:16 -0500

    refactor symbolic variable bindings into pydrake.symbolic

--- drake/bindings/pybind11/pydrake_mathematicalprogram.cc
+++ drake/bindings/pybind11/pydrake_mathematicalprogram.cc
@@ -1,27 +1,22 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
-#include <pybind11/operators.h>
 #include <pybind11/stl.h>
 
-
+#include "drake/bindings/pybind11/pydrake_symbolic_types.h"
 #include "drake/solvers/mathematical_program.h"
 
 namespace py = pybind11;
-
-
-PYBIND11_NUMPY_OBJECT_DTYPE(drake::symbolic::Variable);
-PYBIND11_NUMPY_OBJECT_DTYPE(drake::symbolic::Expression);
-PYBIND11_NUMPY_OBJECT_DTYPE(drake::symbolic::Formula);
 
 PYBIND11_PLUGIN(_pydrake_mathematicalprogram) {
   using drake::symbolic::Variable;
   using drake::symbolic::Expression;
   using drake::symbolic::Formula;
-
   using drake::solvers::Binding;
   using drake::solvers::MathematicalProgram;
   using drake::solvers::Constraint;
   using drake::solvers::LinearConstraint;
+  using drake::solvers::LinearEqualityConstraint;
+  using drake::solvers::BoundingBoxConstraint;
   using drake::solvers::QuadraticConstraint;
   using drake::solvers::VectorXDecisionVariable;
   using drake::solvers::MatrixXDecisionVariable;
@@ -31,6 +26,13 @@ PYBIND11_PLUGIN(_pydrake_mathematicalprogram) {
 
   py::module m("_pydrake_mathematicalprogram",
                "Drake MathematicalProgram Bindings");
+
+  py::object variable = (py::object)
+    py::module::import("pydrake.symbolic").attr("Variable");
+  py::object expression = (py::object)
+    py::module::import("pydrake.symbolic").attr("Expression");
+  py::object formula = (py::object)
+    py::module::import("pydrake.symbolic").attr("Formula");
 
   py::class_<MathematicalProgramSolverInterface>(
     m, "MathematicalProgramSolverInterface")
@@ -154,204 +156,6 @@ PYBIND11_PLUGIN(_pydrake_mathematicalprogram) {
     .value("kInfeasibleConstraints",
            SolutionResult::kInfeasibleConstraints)
     .value("kUnknownError", SolutionResult::kUnknownError);
-
-  py::class_<Variable>(m, "Variable")
-    .def(py::init<const std::string&>())
-    .def("__repr__", &Variable::to_string)
-    .def("__add__", [](const Variable& self,
-                       const Variable& other) {
-      return Expression{self + other};
-    })
-    .def("__add__", [](const Variable& self, double other) {
-      return Expression{self + other};
-    })
-    .def("__add__", [](const Variable& self, const Expression& other) {
-      return Expression{self + other};
-    })
-    .def("__radd__", [](const Variable& self, double other) {
-      return Expression{other + self};
-    })
-    .def("__sub__", [](const Variable& self,
-                       const Variable& other) {
-      return Expression{self - other};
-    })
-    .def("__sub__", [](const Variable& self, double other) {
-      return Expression{self - other};
-    })
-    .def("__sub__", [](const Variable& self, const Expression& other) {
-      return Expression{self - other};
-    })
-    .def("__rsub__", [](const Variable& self, double other) {
-      return Expression{other - self};
-    })
-    .def("__mul__", [](const Variable& self,
-                       const Variable& other) {
-      return Expression{self * other};
-    })
-    .def("__mul__", [](const Variable& self, double other) {
-      return Expression{self * other};
-    })
-    .def("__mul__", [](const Variable& self, const Expression& other) {
-      return Expression{self * other};
-    })
-    .def("__rmul__", [](const Variable& self, double other) {
-      return Expression{other * self};
-    })
-    .def("__truediv__", [](const Variable& self,
-                           const Variable& other) {
-      return Expression{self / other};
-    })
-    .def("__truediv__", [](const Variable& self,
-                           double other) {
-      return Expression{self / other};
-    })
-    .def("__truediv__", [](const Variable& self, const Expression& other) {
-      return Expression{self / other};
-    })
-    .def("__rtruediv__", [](const Variable& self,
-                            double other) {
-      return Expression{other / self};
-    })
-    .def("__lt__", [](const Variable& self,
-                      const Variable& other) {
-      return Formula{Expression(self) < Expression(other)};
-    })
-    .def("__lt__", [](const Variable& self,
-                      double other) {
-      return Formula{Expression(self) < Expression(other)};
-    })
-    .def("__lt__", [](const Variable& self, const Expression& other) {
-      return Formula{Expression(self) < other};
-    })
-    .def("__le__", [](const Variable& self,
-                      const Variable& other) {
-      return Formula{Expression(self) <= Expression(other)};
-    })
-    .def("__le__", [](const Variable& self,
-                      double other) {
-      return Formula{Expression(self) <= Expression(other)};
-    })
-    .def("__le__", [](const Variable& self, const Expression& other) {
-      return Formula{Expression(self) <= other};
-    })
-    .def("__gt__", [](const Variable& self,
-                      const Variable& other) {
-      return Formula{Expression(self) > Expression(other)};
-    })
-    .def("__gt__", [](const Variable& self,
-                      double other) {
-      return Formula{Expression(self) > Expression(other)};
-    })
-    .def("__gt__", [](const Variable& self, const Expression& other) {
-      return Formula{Expression(self) > other};
-    })
-    .def("__ge__", [](const Variable& self,
-                      const Variable& other) {
-      return Formula{Expression(self) >= Expression(other)};
-    })
-    .def("__ge__", [](const Variable& self,
-                      double other) {
-      return Formula{Expression(self) >= Expression(other)};
-    })
-    .def("__ge__", [](const Variable& self, const Expression& other) {
-      return Formula{Expression(self) >= other};
-    })
-    .def("__eq__", [](const Variable& self,
-                      const Variable& other) {
-      return Formula{Expression(self) == Expression(other)};
-    })
-    .def("__eq__", [](const Variable& self,
-                      double other) {
-      return Formula{Expression(self) == Expression(other)};
-    })
-    .def("__eq__", [](const Variable& self, const Expression& other) {
-      return Formula{Expression(self) == other};
-    });
-
-
-  py::class_<Expression>(m, "Expression")
-    .def(py::init<>())
-    .def(py::init<const Variable&>())
-    .def("__repr__", &Expression::to_string)
-    .def(py::self   + py::self)
-    .def(py::self   + Variable())
-    .def(py::self   + double())
-    .def(double()   + py::self)
-    .def(py::self   - py::self)
-    .def(py::self   - Variable())
-    .def(py::self   - double())
-    .def(double()   - py::self)
-    .def(py::self   * py::self)
-    .def(py::self   * Variable())
-    .def(py::self   * double())
-    .def(double()   * py::self)
-    .def(py::self   / py::self)
-    .def(py::self   / Variable())
-    .def(py::self   / double())
-    .def(double()   / py::self)
-    .def("__lt__", [](const Expression& self,
-                      const Variable& other) {
-      return Formula{Expression(self) < Expression(other)};
-    })
-    .def("__lt__", [](const Expression& self,
-                      const Expression& other) {
-      return Formula{Expression(self) < Expression(other)};
-    })
-    .def("__lt__", [](const Expression& self,
-                      double other) {
-      return Formula{Expression(self) < Expression(other)};
-    })
-    .def("__le__", [](const Expression& self,
-                      const Variable& other) {
-      return Formula{Expression(self) <= Expression(other)};
-    })
-    .def("__le__", [](const Expression& self,
-                      const Expression& other) {
-      return Formula{Expression(self) <= Expression(other)};
-    })
-    .def("__le__", [](const Expression& self,
-                      double other) {
-      return Formula{Expression(self) <= Expression(other)};
-    })
-    .def("__gt__", [](const Expression& self,
-                      const Variable& other) {
-      return Formula{Expression(self) > Expression(other)};
-    })
-    .def("__gt__", [](const Expression& self,
-                      const Expression& other) {
-      return Formula{Expression(self) > Expression(other)};
-    })
-    .def("__gt__", [](const Expression& self,
-                      double other) {
-      return Formula{Expression(self) > Expression(other)};
-    })
-    .def("__ge__", [](const Expression& self,
-                      const Variable& other) {
-      return Formula{Expression(self) >= Expression(other)};
-    })
-    .def("__ge__", [](const Expression& self,
-                      const Expression& other) {
-      return Formula{Expression(self) >= Expression(other)};
-    })
-    .def("__ge__", [](const Expression& self,
-                      double other) {
-      return Formula{Expression(self) >= Expression(other)};
-    })
-    .def("__eq__", [](const Expression& self,
-                      const Variable& other) {
-      return Formula{Expression(self) == Expression(other)};
-    })
-    .def("__eq__", [](const Expression& self,
-                      const Expression& other) {
-      return Formula{Expression(self) == Expression(other)};
-    })
-    .def("__eq__", [](const Expression& self,
-                      double other) {
-      return Formula{Expression(self) == Expression(other)};
-    });
-
-  py::class_<Formula>(m, "Formula")
-    .def("__repr__", &Formula::to_string);
 
   // Assign the wrapped Constraint class to the name 'constraint'
   // so we can use it in this file to indicate that the other constraint
