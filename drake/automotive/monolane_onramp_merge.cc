06174f670144d3f318ad475c2fa38d8f0c40fd3e
Jonathan DeCastro <jonathan.decastro@tri.global>
2017-02-13 17:30:16 -0500

    Spin off utility class, allow for road reversal

--- drake/automotive/monolane_onramp_merge.cc
+++ drake/automotive/monolane_onramp_merge.cc
@@ -1,82 +1,61 @@
-#include "monolane_onramp_merge.h"
+#include "drake/automotive/monolane_onramp_merge.h"
 
 #include <cmath>
 #include <iostream>
 
+#include <Eigen/Geometry>
+
 #include "drake/automotive/maliput/api/road_geometry.h"
 #include "drake/automotive/maliput/monolane/builder.h"
+#include "drake/common/eigen_autodiff_types.h"
+#include "drake/common/symbolic_expression.h"
 
 namespace drake {
 namespace maliput {
 namespace monolane {
 
 template <typename T>
-void RoadSection<T>::AddArcSegment(const T& arc_length, const T& arc_radius,
-                                   const ArcDirection& dir,
-                                   const EndpointZ& end_z) {
-  DRAKE_DEMAND(arc_length < 2 * M_PI * arc_radius);
-  const T& angle = dir == kCCW ? arc_length / arc_radius
-      : -arc_length / arc_radius;
-  const ArcOffset& arc{arc_radius, angle};
-
-  const Connection* connection = b_->Connect(std::to_string(id_++),
-                                             last_endpoint_, arc, end_z);
-  last_endpoint_ = connection->end();
-}
-
-template <typename T>
-void RoadSection<T>::AddArcSegment(const T& arc_length, const T& arc_radius,
-                                   const ArcDirection& dir) {
-  AddArcSegment(arc_length, arc_radius, dir, flat_z_);
-}
-
-template <typename T>
-void RoadSection<T>::AddStraightSegment(const T& lane_length,
-                                        const EndpointZ& end_z) {
-  const Connection* connection = b_->Connect(std::to_string(id_++),
-                                             last_endpoint_, lane_length,
-                                             end_z);
-  last_endpoint_ = connection->end();
-}
-
-template <typename T>
-void RoadSection<T>::AddStraightSegment(const T& lane_length) {
-  AddStraightSegment(lane_length, flat_z_);
-}
-
-template <typename T>
 void MonolaneOnrampMerge<T>::BuildOnramp() {
   Builder b{road_.lane_bounds, road_.driveable_bounds, kLinearTolerance_,
             kAngularTolerance_};
 
-  std::unique_ptr<RoadSection<T>>
-      rs_pre(new RoadSection<T>(std::move(b_), "Pre", road_));
-  rs_pre->AddArcSegment(70., 50., kCW);
-  rs_pre->AddArcSegment(40., 50., kCCW);
+  // Construct the pre-merge road.
+  std::unique_ptr<RoadSectionBuilder<T>> rs_pre(
+      new RoadSectionBuilder<T>(std::move(b_), false));
+  rs_pre->AddArcPrimitive(40., 25., kCW);
+  rs_pre->AddArcPrimitive(20., 25., kCCW);
+  rs_pre->AddArcPrimitive(40., 25., kCW);
+  rs_pre->AddArcPrimitive(20., 25., kCCW);
+  rs_pre->AddArcPrimitive(40., 25., kCW);
+  rs_pre->AddArcPrimitive(20., 25., kCCW);
   const Endpoint endpoint_pre = rs_pre->get_last_endpoint();
   b_ = rs_pre->Finalize();
 
-  std::unique_ptr<RoadSection<T>>
-      rs_post(new RoadSection<T>(std::move(b_), "Post", road_, endpoint_pre));
-  rs_post->AddStraightSegment(50.);
+  // Construct the post-merge road.
+  std::unique_ptr<RoadSectionBuilder<T>> rs_post(
+      new RoadSectionBuilder<T>(std::move(b_), false, endpoint_pre));
+  rs_post->AddLinearPrimitive(50.);
   b_ = rs_post->Finalize();
 
-  std::unique_ptr<RoadSection<T>>
-      rs_onramp(new RoadSection<T>(std::move(b_), "Onramp", road_,
-                                   endpoint_pre.reverse()));
-  rs_onramp->AddArcSegment(50., 30, kCCW);
-  rs_onramp->AddStraightSegment(100.);
+  // Construct the on-ramp.
+  std::unique_ptr<RoadSectionBuilder<T>> rs_onramp(new RoadSectionBuilder<T>(
+      std::move(b_), true, endpoint_pre.reverse()));
+  rs_onramp->AddArcPrimitive(50., 30, kCCW);
+  rs_onramp->AddLinearPrimitive(100.);
   b_ = rs_onramp->Finalize();
 
-  //b.SetDefaultBranch(
+  // b_.SetDefaultBranch(
   //    left1, api::LaneEnd::kStart, left1, api::LaneEnd::kFinish);
 
-  rg_ = b_->Build({"example"});
+  std::cerr << " Building... " << std::endl;
+  rg_ = b_->Build({"monolane-onramp-example"});
 }
 
 // These instantiations must match the API documentation in
 // monolane_onramp_merge.h.
 template class MonolaneOnrampMerge<double>;
+template class MonolaneOnrampMerge<TaylorVarXd>;
+template class MonolaneOnrampMerge<symbolic::Expression>;
 
 }  // namespace monolane
 }  // namespace maliput
