d4e28b50adf841e4030d1a316603e58cc7b32866
hongkai-dai <daihongkai@gmail.com>
2017-02-14 14:45:14 -0800

    Address Soonho's comments

--- drake/common/monomial.cc
+++ drake/common/monomial.cc
@@ -116,23 +116,23 @@ map<Variable::Id, int> ToMonomialPower(const Expression& e) {
   map<Variable::Id, int> powers;
   if (is_one(e)) {
   } else if (is_constant(e)) {
-    throw std::runtime_error(
-        "A constant not equalto to 1, this is not a monomial.");
+    throw runtime_error("A constant not equalto to 1, this is not a monomial.");
   } else if (is_variable(e)) {
     powers.emplace(get_variable(e).get_id(), 1);
   } else if (is_pow(e)) {
     const auto& exponent = get_second_argument(e);
     if (!is_constant(exponent)) {
-      throw std::runtime_error("The exponent is not a constant.");
+      throw runtime_error("The exponent is not a constant.");
     }
     auto exponent_val = get_constant_value(exponent);
     powers = ToMonomialPower(get_first_argument(e));
+    // pow( pow(xᵢ, kᵢ), n) = pow(xᵢ, kᵢ * n)
     for (auto& p : powers) {
       p.second *= exponent_val;
     }
   } else if (is_multiplication(e)) {
     if (!is_one(get_constant_in_multiplication(e))) {
-      throw std::runtime_error("The constant in the multiplication is not 1.");
+      throw runtime_error("The constant in the multiplication is not 1.");
     }
     for (const auto& p : get_base_to_exponent_map_in_multiplication(e)) {
       map<Variable::Id, int> p_powers = ToMonomialPower(pow(p.first, p.second));
@@ -146,8 +146,7 @@ map<Variable::Id, int> ToMonomialPower(const Expression& e) {
       }
     }
   } else {
-    throw std::runtime_error(
-        "This expression cannot be converted to a monomial.");
+    throw runtime_error("This expression cannot be converted to a monomial.");
   }
   return powers;
 }
