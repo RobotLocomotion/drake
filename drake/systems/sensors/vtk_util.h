e8645c7166863aae447d952fa7364081ca578351
Kunimatsu Hashimoto <kunimatsu.hashimoto@tri.global>
2017-02-17 00:32:59 -0500

    Polish comments

--- drake/systems/sensors/vtk_util.h
+++ drake/systems/sensors/vtk_util.h
@@ -17,17 +17,18 @@ class VtkUtil {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(VtkUtil)
 
-  /// Creates a square plane with the given size.  The plane's z axis is its
-  /// normal and is coincident with the z axis of the world coordinate systesm.
-  /// The plane's center is at (0, 0, 0).  The ranges of the plane's x and y are
+  /// Creates a square plane of the given size.  The plane's z axis is its
+  /// normal and is coincident with the z axis of the world coordinate system.
+  /// The plane's center is at (0, 0, 0) of both the world coordinate system and
+  /// plane's coordinate system.  The ranges of the plane's x and y are
   /// `(- size / 2, size / 2)`.
   ///
   /// @param size The size of the plane.
   static vtkSmartPointer<vtkPlaneSource> CreateSquarePlane(double size);
 
-  /// Converts the transformation of Eigen::Isometry3d to that of vtkTransform.
+  /// Converts the provided `Eigen::Isometry3d` to a `vtkTransform`.
   ///
-  /// @param transform The transformation of Eigen::Isometry3d.
+  /// @param transform The transform to convert into a `vtkTransform`.
   static vtkSmartPointer<vtkTransform> ConvertToVtkTransform(
       const Eigen::Isometry3d& transform);
 };
