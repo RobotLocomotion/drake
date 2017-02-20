afc4230c23512e97f9fede2395478c25ce9985d2
Jon <jonathan.decastro@tri.global>
2017-02-20 00:56:34 -0500

    Address first review round

--- drake/systems/framework/hybrid_automaton_context.h
+++ drake/systems/framework/hybrid_automaton_context.h
@@ -51,6 +51,21 @@ using std::unique_ptr;
  * In the context of Drake's System framework, ModalSubsystem captures, for each
  * mode, the System (f, g) for each mode, the Init and Invar for System, and the
  * list of inputs and outputs of the System.
+ *
+ * The invariants and initial conditions for this ModalSubsystem are of
+ * symbolic::Expression type, with each defining a semialgebraic set: a state
+ * assignment is within the set iff it evaluates to a non-negative value.  For
+ * instance, assume we have a System with one state.  Given a
+ * `ModalSubsystem<double>` declared as `mss`, then the following code snippet
+ * defines the semialgebric set `x <= 1`:
+ *
+ * @code
+ *  std::vector<symbolic::Expression>* invariant mss->get_mutable_invariant();
+ *  std::vector<symbolic::Variable> xcs = mss->get_symbolic_continuous_states();
+ *  const symbolic::Expression x0{xcs[0]};  // Symbol for the continuous state.
+ *  (*invariant).push_back({-x0 + 1.})  // The unbounded set x <= 1 defines this
+ *                                      // invariant.
+ * @endcode
  */
 template <typename T>
 class ModalSubsystem {
@@ -60,7 +75,7 @@ class ModalSubsystem {
   typedef int ModeId;
   typedef int PortId;
 
-  /// Constructor.
+  /// ModalSubsystem constructor.
   /// - @p mode_id is an identifier for the mode.
   /// - @p system is a System, the memory of which is shared with the caller.
   /// - @p invariant is a symbolic::Expression containing Invar(·) in terms of
@@ -82,43 +97,40 @@ class ModalSubsystem {
         initial_conditions_(initial_conditions),
         input_port_ids_(input_port_ids),
         output_port_ids_(output_port_ids) {
-    CheckPortsAreValid();
-    CreateSymbolicStatesAndInputs();
+    ThrowIfAnyPortIsInvalid();
+    CreateSymbolicStates();
   }
 
   ModalSubsystem(ModeId mode_id, shared_ptr<System<T>> system,
                  std::vector<PortId> input_port_ids,
                  std::vector<PortId> output_port_ids)
-      : mode_id_(mode_id),
-        system_(move(system)),
-        input_port_ids_(input_port_ids),
-        output_port_ids_(output_port_ids) {
-    ModalSubsystem(mode_id_, system_, {}, {}, input_port_ids_,
-                   output_port_ids_);
-  }
+      : ModalSubsystem(mode_id, system, {}, {}, input_port_ids,
+                       output_port_ids) {}
 
   ModalSubsystem(ModeId mode_id, shared_ptr<System<T>> system)
-      : mode_id_(mode_id), system_(move(system)) {
+      : ModalSubsystem(mode_id, system, {}, {}, {}, {}) {
     // Populates the input and output ports with the full complement of system
     // inputs and outputs.
-    std::vector<PortId> input_port_ids{system_->get_num_input_ports()};
-    std::iota(std::begin(input_port_ids), std::end(input_port_ids), 0);
-    std::vector<PortId> output_port_ids{system_->get_num_output_ports()};
-    std::iota(std::begin(output_port_ids), std::end(output_port_ids), 0);
-
-    ModalSubsystem(mode_id_, system_, {}, {}, input_port_ids, output_port_ids);
+    input_port_ids_.resize(system_->get_num_input_ports());
+    std::iota(std::begin(input_port_ids_), std::end(input_port_ids_), 0);
+    output_port_ids_.resize(system_->get_num_output_ports());
+    std::iota(std::begin(output_port_ids_), std::end(output_port_ids_), 0);
   }
 
-  /// Accessors for the underlying data.
+  /// Immutable accessor to the underlying the ModeId for this ModalSubsystem.
   ModeId get_mode_id() const { return mode_id_; }
-  System<T>* get_system() const { return system_.get(); }
+
+  /// Immutable accessor to the pointer of the underlying System.
+  const System<T>* get_system() const { return system_.get(); }
   const std::vector<PortId> get_input_port_ids() const {
     return input_port_ids_;
   }
+
+  /// Accessors to the PortIds and attributes of the ports to be exposed by this
+  /// ModalSubsystem.
   std::vector<PortId>* get_mutable_input_port_ids() { return &input_port_ids_; }
   PortId get_input_port_id(const int index) const {
-    DRAKE_DEMAND(index >= 0 &&
-                 index < static_cast<int>(input_port_ids_.size()));
+    DRAKE_DEMAND(index >= 0 && index < get_num_input_ports());
     return input_port_ids_[index];
   }
   int get_num_input_ports() const {
@@ -131,18 +143,15 @@ class ModalSubsystem {
     return &output_port_ids_;
   }
   PortId get_output_port_id(const int index) const {
-    DRAKE_DEMAND(index >= 0 &&
-                 index < static_cast<int>(output_port_ids_.size()));
+    DRAKE_DEMAND(index >= 0 && index < get_num_output_ports());
     return output_port_ids_[index];
   }
   int get_num_output_ports() const {
     return static_cast<int>(output_port_ids_.size());
   }
 
-  /// Accessors for the symbolic::Expressions for the invariants and initial
-  /// condition state sets for this ModalSubsystem. Their defining sets are
-  /// semialgebraic: a state assignment is within the set iff it evaluates to a
-  /// non-negative value.
+  /// Accessors to the symbolic::Expressions for the invariants and initial
+  /// condition state sets for this ModalSubsystem.
   const std::vector<symbolic::Expression> get_invariant() const {
     return invariant_;
   }
@@ -181,14 +190,21 @@ class ModalSubsystem {
   }
 
  private:
-  // Checks that we are providing sane port ids.
-  void CheckPortsAreValid() {
-    DRAKE_DEMAND();
+  // Checks the sanity of the provided port ids.
+  void ThrowIfAnyPortIsInvalid() {
+    for (auto inport : input_port_ids_) {
+      DRAKE_THROW_UNLESS(0 <= inport &&
+                         inport < system_->get_num_input_ports());
+    }
+    for (auto outport : output_port_ids_) {
+      DRAKE_THROW_UNLESS(0 <= outport &&
+                         outport < system_->get_num_output_ports());
+    }
   }
 
   // Creates symbolic variables based on the expected context for this
   // subsystem.
-  void CreateSymbolicStatesAndInputs() {
+  void CreateSymbolicStates() {
     // Create a temporary context to extract the needed dimensions.
     unique_ptr<Context<T>> context = system_->AllocateContext();
 
@@ -202,7 +218,7 @@ class ModalSubsystem {
   }
 
   // Creates symbolic variables according to the variable_type key word.
-  void CreateSymbolicVariables(const std::string variable_type,
+  void CreateSymbolicVariables(const std::string& variable_type,
                                const int size) {
     std::vector<std::vector<symbolic::Variable>> sym{};
     if (symbolic_variables_.find(variable_type) != symbolic_variables_.end()) {
@@ -212,8 +228,7 @@ class ModalSubsystem {
     for (int i = 0; i < size; ++i) {
       std::ostringstream key{};
       key << variable_type << i;
-      symbolic::Variable var{key.str()};
-      row.emplace_back(var);
+      row.emplace_back(key.str());
     }
     sym.emplace_back(row);
     symbolic_variables_.insert(std::make_pair(variable_type, sym));
@@ -222,17 +237,35 @@ class ModalSubsystem {
   // An identifier for this mode.
   // TODO(jadecastro): Allow ModeId to take on a descriptor in place of the int.
   ModeId mode_id_;
+
   // The system model.
   shared_ptr<System<T>> system_;
-  // Expression representing the invariant for this mode.
+
+  // Vector of expressions representing the semialgebraic set defining the
+  // invariant for this mode.  Note that a strict coupling between invariant_
+  // and symbolic_variables_ will be enforced within HybridAutomatonContext.
+  // (See the class docstring for details.)
   std::vector<symbolic::Expression> invariant_;
-  // Expression representing the initial conditions for this mode.
+
+  // Vector of expressions representing the semialgebraic set defining
+  // the initial conditions for this mode.  Note that a strict coupling between
+  // initial_conditions_ and symbolic_variables_ will be enforced within
+  // HybridAutomatonContext.
+  // (See the class docstring for details.)
   std::vector<symbolic::Expression> initial_conditions_;
-  // Index set of the input and output ports.
+
+  // Index sets for the input and output ports for this mode.  Each @p PortId
+  // that make up these vectors must correspond to a valid Port Number of the
+  // installed System.  There is otherwise no restriction on repetition,
+  // ordering, or completeness over the complement of System ports.
   std::vector<PortId> input_port_ids_;
   std::vector<PortId> output_port_ids_;
-  // A vector of symbolic variables for each of the continuous states in the
-  // system.
+
+  // A data structure containing the symbolic variables corresponding to the
+  // states of the System.  Continuous and discrete states correspond,
+  // respectively, to the std::string keys "xc" and "xd".  For convenience,
+  // these designations are also prefixes for the labeled keys for each of the
+  // symbolic variables.
   std::map<std::string, std::vector<std::vector<symbolic::Variable>>>
       symbolic_variables_;
 };
--- drake/systems/framework/test/hybrid_automaton_context_test.cc
+++ drake/systems/framework/test/hybrid_automaton_context_test.cc
@@ -16,6 +16,7 @@
 #include "drake/systems/framework/system_input.h"
 #include "drake/systems/framework/system_port_descriptor.h"
 #include "drake/systems/framework/test_utilities/pack_value.h"
+#include "drake/systems/primitives/constant_value_source.h"
 #include "drake/systems/primitives/integrator.h"
 #include "drake/systems/primitives/zero_order_hold.h"
 
@@ -31,26 +32,6 @@ using std::unique_ptr;
 constexpr int kSize{1};
 constexpr double kZohSamplingPeriod{0.1};
 
-class AbstractTestSource : public LeafSystem<double> {
- public:
-  AbstractTestSource() { this->DeclareOutputPort(kVectorValued, 1); }
-
-  unique_ptr<AbstractState> AllocateAbstractState() const override {
-    std::vector<unique_ptr<AbstractValue>> values;
-    values.push_back({PackValue<std::string>("I'm an abstract state.")});
-    return make_unique<AbstractState>(move(values));
-  }
-
-  void DoCalcOutput(const Context<double>& context,
-                    SystemOutput<double>* output) const override {
-    DRAKE_ASSERT_VOID(System<double>::CheckValidOutput(output));
-    DRAKE_ASSERT_VOID(System<double>::CheckValidContext(context));
-
-    System<double>::GetMutableOutputVector(output, 0) =
-        System<double>::CopyContinuousStateVector(context);
-  }
-};
-
 // A system that is an amalgamation of continuous, discrete, and abstract
 // states. Specifically, it is a diagram of an Integrator in series with a
 // ZeroOrderHold element, in parallel with an AbstractTestSource system.
@@ -62,7 +43,11 @@ class ContinuousDiscreteAbstractSystem : public Diagram<double> {
     integrator_ = builder.template AddSystem<Integrator<double>>(kSize);
     zoh_ = builder.template AddSystem<ZeroOrderHold<double>>(kZohSamplingPeriod,
                                                              kSize);
-    abstract_ = builder.template AddSystem<AbstractTestSource>();
+    // abstract_ = builder.template AddSystem<AbstractTestSource>();
+    std::unique_ptr<AbstractValue>
+        value(new Value<std::string>("I'm an abstract state."));
+    abstract_ = builder.template
+        AddSystem<ConstantValueSource<double>>(std::move(value));
 
     builder.Connect(integrator_->get_output_port(0), zoh_->get_input_port(0));
 
@@ -76,27 +61,68 @@ class ContinuousDiscreteAbstractSystem : public Diagram<double> {
  private:
   Integrator<double>* integrator_ = nullptr;
   ZeroOrderHold<double>* zoh_ = nullptr;
-  AbstractTestSource* abstract_ = nullptr;
+  ConstantValueSource<double>* abstract_ = nullptr;
 };
 
-class HybridAutomatonContextTest : public ::testing::Test {
+class ModalSubsystemTest : public ::testing::Test {
  protected:
   void SetUp() override {
     integrator_.reset(new Integrator<double>(kSize));
     integrator_->set_name("Alice");
 
-    // Instantiate a new modal subsystem. Implicitly, one input and one output
-    // are exported as freestanding.
-    const int mode_id = 42;
-    mss_.reset(new ModalSubsystem<double>(mode_id, integrator_));
+    // Instantiate a new modal subsystem. Note that, by default, the
+    // ModalSubsystem mirrors the input and output port ids of the Integrator
+    // system (which, in this case, is SISO).
+    mss_.reset(new ModalSubsystem<double>(mode_id_, integrator_));
   }
 
+  const int mode_id_ = 42;
   unique_ptr<ModalSubsystem<double>> mss_;
-
   shared_ptr<Integrator<double>> integrator_;
 };
 
-class HybridAutomatonContextStateTest : public ::testing::Test {
+// Tests that we can correctly select among exported input and output ports
+// those that are inputs and outputs of the HA.
+TEST_F(ModalSubsystemTest, Attributes) {
+  EXPECT_EQ(mode_id_, mss_->get_mode_id());
+  EXPECT_EQ("Alice", mss_->get_system()->get_name());
+
+  EXPECT_EQ(1, mss_->get_num_input_ports());
+  EXPECT_EQ(1, mss_->get_num_output_ports());
+
+  EXPECT_EQ(0, mss_->get_input_port_ids()[0]);
+  EXPECT_EQ(0, mss_->get_output_port_ids()[0]);
+}
+
+// Tests the validity of a ModalSubsystem constructed with explicit ports.
+GTEST_TEST(ModalSubsystemPortTest, ExplicitPortIds) {
+  shared_ptr<System<double>> example_system(
+      new ContinuousDiscreteAbstractSystem());
+  const ModalSubsystem<double>& mss =
+      ModalSubsystem<double>(0, example_system, {0}, {1, 0});
+
+  // Verify the number and identities of the ports have passed into the object.
+  EXPECT_EQ(1, mss.get_num_input_ports());
+  EXPECT_EQ(2, mss.get_num_output_ports());
+
+  EXPECT_EQ(0, mss.get_input_port_ids()[0]);
+  EXPECT_EQ(1, mss.get_output_port_ids()[0]);
+  EXPECT_EQ(0, mss.get_output_port_ids()[1]);
+}
+
+// Tests that a ModalSubsystem cannot be constructed with invalid ports.
+GTEST_TEST(ModalSubsystemPortTest, InvalidPortIds) {
+  shared_ptr<System<double>> example_system(
+      new ContinuousDiscreteAbstractSystem());
+
+  // Throws if any of the port ids are invalid.
+  EXPECT_THROW(ModalSubsystem<double>(0, example_system,
+    {example_system->get_num_output_ports()}, {0}), std::runtime_error);
+  EXPECT_THROW(ModalSubsystem<double>(0, example_system, {0},
+    {example_system->get_num_output_ports()}), std::runtime_error);
+}
+
+class HybridAutomatonContextTest : public ::testing::Test {
  protected:
   void SetUp() override {
     example_system_.reset(new ContinuousDiscreteAbstractSystem());
@@ -104,75 +130,55 @@ class HybridAutomatonContextStateTest : public ::testing::Test {
 
     // Instantiate a new modal subsystem. Implicitly, one input and one output
     // are exported as freestanding.
-    const int mode_id = 6;
-    mss_.reset(new ModalSubsystem<double>(mode_id, example_system_));
+    mss_.reset(new ModalSubsystem<double>(mode_id_, example_system_));
   }
 
+  const int mode_id_ = 6;
   unique_ptr<ModalSubsystem<double>> mss_;
-
   shared_ptr<ContinuousDiscreteAbstractSystem> example_system_;
 };
 
-// Tests that we can correctly select among exported input and output ports
-// those that are inputs and outputs of the HA.
-TEST_F(HybridAutomatonContextTest, ModalSubsystemPortIds) {
-  EXPECT_EQ(1, mss_->get_num_input_ports());
-  EXPECT_EQ(1, mss_->get_num_output_ports());
-
-  EXPECT_EQ(0, mss_->get_input_port_ids()[0]);
-  EXPECT_EQ(0, mss_->get_output_port_ids()[0]);
-
-  // Explicitly specify the ports for a non-trivial example via the constructor.
-  shared_ptr<System<double>> example_system(
-      new ContinuousDiscreteAbstractSystem());
-  const ModalSubsystem<double>& mss_new =
-      ModalSubsystem<double>(0, example_system, {0}, {1, 0});
-
-  // Verify the number and identities of the ports have passed into the object.
-  EXPECT_EQ(1, mss_new.get_num_input_ports());
-  EXPECT_EQ(2, mss_new.get_num_output_ports());
-
-  EXPECT_EQ(0, mss_new.get_input_port_ids()[0]);
-  EXPECT_EQ(1, mss_new.get_output_port_ids()[0]);
-  EXPECT_EQ(0, mss_new.get_output_port_ids()[1]);
-}
-
 // Tests the ability to specify symbolic expressions and evaluate them.
-TEST_F(HybridAutomatonContextStateTest, ModalSubsystemSymbolicExpressions) {
+TEST_F(HybridAutomatonContextTest, ModalSubsystemSymbolicExpressions) {
   // Fetch the symbolic variables and mutable expressions for this context.
-  symbolic::Variable xc = mss_->get_symbolic_continuous_states()[0];
-  symbolic::Variable xd = mss_->get_symbolic_discrete_states_at(0)[0];
+  std::vector<symbolic::Variable> xcs = mss_->get_symbolic_continuous_states();
+  std::vector<symbolic::Variable> xds =
+      mss_->get_symbolic_discrete_states_at(0);
   std::vector<symbolic::Expression>* example_invariant =
       mss_->get_mutable_invariant();
   std::vector<symbolic::Expression>* example_ic =
       mss_->get_mutable_initial_conditions();
 
-  // Define the symbols @p p (continuous-state) and @p q (discrete-state).
-  const symbolic::Expression p{xc};
-  const symbolic::Expression q{xd};
+  // Define symbols for the continuous state (@p p) and discrete state (@p q)
+  // upon which to construct symbolic expressions.
+  const symbolic::Expression p{xcs[0]};
+  const symbolic::Expression q{xds[0]};
 
-  // Check that the keys are indeed labeled "xc0" and "xd0".
+  // Check that the generated keys are indeed correctly labeled "xc0" and "xd0".
   EXPECT_EQ("xc0", p.to_string());
   EXPECT_EQ("xd0", q.to_string());
 
-  // Create invariants and initial conditions in terms of the symbols @p p and
-  // @p q.
+  // Create examples of invariants and initial conditions in terms of the
+  // symbols @p p and @p q.
+  // N.B. This is a mock-up of how a user would create these expressions.
   (*example_invariant).push_back({pow(p, 2.) - cos(p) + tanh(q)});
   (*example_ic).push_back({pow(p, 3.)});
   (*example_ic).push_back({-p + 3.});
   (*example_ic).push_back({q});
 
-  // Evaluate the expressions for a given assignment.
-  symbolic::Environment x_env{{xc, 3.}, {xd, -2.}};
-  EXPECT_EQ(pow(3., 2.) - cos(3.) + tanh(-2.),
-            (*example_invariant)[0].Evaluate(x_env));
-  EXPECT_EQ(pow(3., 3.), (*example_ic)[0].Evaluate(x_env));
-  EXPECT_EQ(-3. + 3., (*example_ic)[1].Evaluate(x_env));
-  EXPECT_EQ(-2., (*example_ic)[2].Evaluate(x_env));
+  // Check that the symbolic variables obtained from ModalSubsystem are
+  // compatible with the constructed invariants and initial conditions.
+  // N.B. This is a mock-up of code that will exist in HybridAutomatonContext to
+  // process the expressions.
+  symbolic::Environment x_env{{xcs[0], 3.}, {xds[0], -2.}};
+  EXPECT_NO_THROW((*example_invariant)[0].Evaluate(x_env));
+  EXPECT_NO_THROW((*example_ic)[0].Evaluate(x_env));
+  EXPECT_NO_THROW((*example_ic)[1].Evaluate(x_env));
+  EXPECT_NO_THROW((*example_ic)[2].Evaluate(x_env));
 }
 
 // Tests the ability to create a clone of a ModalSubsystem.
-TEST_F(HybridAutomatonContextStateTest, CloneModalSubsystem) {
+TEST_F(HybridAutomatonContextTest, CloneModalSubsystem) {
   // Set fictitious invariants and initial conditions.
   const symbolic::Expression y{symbolic::Variable{"x"}};
   symbolic::Expression expression{y};
@@ -183,7 +189,7 @@ TEST_F(HybridAutomatonContextStateTest, CloneModalSubsystem) {
   unique_ptr<ModalSubsystem<double>> mss_new = mss_->Clone();
 
   // Verify that the data survives the clone.
-  EXPECT_EQ(6, mss_new->get_mode_id());
+  EXPECT_EQ(mode_id_, mss_new->get_mode_id());
   EXPECT_EQ("Bob", mss_new->get_system()->get_name());
   EXPECT_EQ(1, mss_new->get_invariant().size());
   EXPECT_EQ(1, mss_new->get_initial_conditions().size());
