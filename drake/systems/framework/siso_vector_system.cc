3fdb9758972e1525f75de57ceec52f009f2ee02a
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-02-22 09:58:10 -0500

    Add SisoVectorSystem
    Also port a couple primitives to use it as a proof-of-concept.

---
+++ drake/systems/framework/siso_vector_system.cc
@@ -0,0 +1,9 @@
+#include "drake/systems/framework/siso_vector_system.h"
+
+namespace drake {
+namespace systems {
+
+template class SisoVectorSystem<double>;
+
+}  // namespace systems
+}  // namespace drake
