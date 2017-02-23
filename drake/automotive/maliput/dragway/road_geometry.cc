10bd6178ef76c27c9c8ba640a429e28fae0e7e3d
Chien-Liang Fok <liang.fok@tri.global>
2017-02-23 14:05:02 -0500

    Implements the first half of dragway::RoadGeometry::DoToRoadPosition().

--- drake/automotive/maliput/dragway/road_geometry.cc
+++ drake/automotive/maliput/dragway/road_geometry.cc
@@ -5,6 +5,7 @@
 #include "drake/automotive/maliput/dragway/branch_point.h"
 #include "drake/automotive/maliput/dragway/junction.h"
 #include "drake/common/drake_assert.h"
+#include "drake/common/text_logging.h"
 
 using std::make_unique;
 
@@ -48,12 +49,96 @@ const api::BranchPoint* RoadGeometry::do_branch_point(int index) const {
       api::LaneEnd::kStart);
 }
 
+bool RoadGeometry::IsGeoPositionOnDragway(const api::GeoPosition& geo_pos)
+    const {
+  const Lane* lane = dynamic_cast<const Lane*>(junction_.segment(0)->lane(0));
+  DRAKE_ASSERT(lane != nullptr);
+  const double length = lane->length();
+  const api::RBounds lane_driveable_bounds = lane->driveable_bounds(0 /* s */);
+  const double min_y = lane->y_offset() + lane_driveable_bounds.r_min;
+  const double max_y = lane->y_offset() + lane_driveable_bounds.r_max;
+  bool result = true;
+
+  if (geo_pos.x < 0) {
+    drake::log()->info(
+        "dragway::RoadGeometry::IsGeoPositionOnDragway(): The provided geo_pos "
+        "({}, {}) is not on the dragway because its x value is less than zero.",
+        geo_pos.x, geo_pos.y);
+    result = false;
+  }
+  if (geo_pos.x > length) {
+    drake::log()->info(
+        "dragway::RoadGeometry::IsGeoPositionOnDragway(): The provided geo_pos "
+        "({}, {}) is not on the dragway because its x value is greater than "
+        "the length of the dragway ({}).", geo_pos.x, geo_pos.y, length);
+    result = false;
+  }
+  if (geo_pos.y > max_y) {
+    drake::log()->info(
+        "dragway::RoadGeometry::IsGeoPositionOnDragway(): The provided geo_pos "
+        "({}, {}) is not on the dragway because its y value is greater than "
+        "the maximum y coordinate of the dragway ({}).", geo_pos.x, geo_pos.y,
+        max_y);
+    result = false;
+  }
+  if (geo_pos.y < min_y) {
+    drake::log()->info(
+        "dragway::RoadGeometry::IsGeoPositionOnDragway(): The provided geo_pos "
+        "({}, {}) is not on the dragway because its y value is less than "
+        "the minimum y coordinate of the dragway ({}).", geo_pos.x, geo_pos.y,
+        min_y);
+    result = false;
+  }
+  return result;
+}
+
+int RoadGeometry::GetLaneIndex(const api::GeoPosition& geo_pos) const {
+  DRAKE_ASSERT(IsGeoPositionOnDragway(geo_pos));
+  bool lane_found{false};
+  int result{0};
+  for (int i = 0; !lane_found && i < junction_.segment(0)->num_lanes(); ++i) {
+    const Lane* lane = dynamic_cast<const Lane*>(junction_.segment(0)->lane(i));
+    DRAKE_ASSERT(lane != nullptr);
+    if (geo_pos.y <= lane->y_offset() + lane->lane_bounds(0).r_max) {
+      result = i;
+      lane_found = true;
+    }
+  }
+  if (!lane_found) {
+    throw std::runtime_error("dragway::RoadGeometry::GetLaneIndex: Failed to "
+        "find lane for geo_pos (" + std::to_string(geo_pos.x) + ", " +
+        std::to_string(geo_pos.y) + ").");
+  }
+  return result;
+}
+
 api::RoadPosition RoadGeometry::DoToRoadPosition(
-    const api::GeoPosition&,
-    const api::RoadPosition*,
-    api::GeoPosition*,
-    double*) const {
-  DRAKE_ABORT();  // TODO(liang.fok) Implement me.
+    const api::GeoPosition& geo_pos,
+    const api::RoadPosition* hint,
+    api::GeoPosition* nearest_position,
+    double* distance) const {
+  api::LanePosition result_lane_position;
+  result_lane_position.s = geo_pos.x;
+  result_lane_position.h = geo_pos.z;
+  if (IsGeoPositionOnDragway(geo_pos)) {
+    const int lane_index = GetLaneIndex(geo_pos);
+    const Lane* lane =
+        dynamic_cast<const Lane*>(junction_.segment(0)->lane(lane_index));
+    DRAKE_ASSERT(lane != nullptr);
+    const double y_offset = lane->y_offset();
+    result_lane_position.r = geo_pos.y - y_offset;
+    nearest_position->x = geo_pos.x;
+    nearest_position->y = geo_pos.y;
+    nearest_position->z = 0;
+    *distance = geo_pos.z;
+    return api::RoadPosition(lane, result_lane_position);
+  } else {
+    // TODO(liang.fok): Implement this!
+    throw std::runtime_error(
+        "dragway::RoadGeometry::DoToRoadPosition: The ability to determine the "
+        "road position of a GeoPosition that's not on top of the dragway has "
+        "yet to be implemented.");
+  }
 }
 
 }  // namespace dragway
