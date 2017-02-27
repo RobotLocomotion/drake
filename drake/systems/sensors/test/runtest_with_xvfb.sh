1bd572275067702c998e343fa876b72e310b6851
Kunimatsu Hashimoto <kunimatsu.hashimoto@tri.global>
2017-02-27 15:27:08 -0500

    Avoid using Xvfb in the case nvidia libGL is used

--- drake/systems/sensors/test/runtest_with_xvfb.sh
+++ drake/systems/sensors/test/runtest_with_xvfb.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 lspci | grep VGA
 ldd $1
-Xvfb :88 -ac -screen 0 1280x1024x24 &
-# Xorg -noreset +extension GLX +extension RANDR +extension RENDER -config ./xorg.conf :88 &
-sleep 3
-DISPLAY=:88 $1
-test_result=$?
-if [ "$test_result" == "0" ]
+libgl="`ldd $1 | grep libGL.so | grep nvidia`"
+if [ ! -z "$libgl" ]
 then
-  echo "PASS"
+  $1
 else
-  echo "FAIL"
+  Xvfb :88 -ac -screen 0 1280x1024x24 &
+  DISPLAY=:88 $1
+  test_result=$?
+  if [ "$test_result" == "0" ]
+    then
+    echo "PASS"
+  else
+    echo "FAIL"
+  fi
+  xvfb_pid="`pgrep -f "Xvfb :88"`"
+  kill -9 $xvfb_pid
 fi
-xvfb_pid="`pgrep -f "Xvfb :88"`"
-kill -9 $xvfb_pid
