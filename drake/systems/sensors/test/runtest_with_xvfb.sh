33c8b43a99717fae82ca7557a878670a5e4f41a3
Kunimatsu Hashimoto <kunimatsu.hashimoto@tri.global>
2017-02-26 21:47:15 -0500

    Fix test pass / fail report failure

--- drake/systems/sensors/test/runtest_with_xvfb.sh
+++ drake/systems/sensors/test/runtest_with_xvfb.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-glxinfo
+lspci | grep VGA
 Xvfb :88 -ac -screen 0 1280x1024x24 &
+# Xorg -noreset +extension GLX +extension RANDR +extension RENDER -config ./xorg.conf :88 &
 DISPLAY=:88 $1
-DISPLAY=:88 glxinfo
+test_result=$?
+if [ "$test_result" == "0" ]
+then
+  echo "PASS"
+else
+  echo "FAIL"
+fi
 xvfb_pid="`pgrep -f "Xvfb :88"`"
-kill -9 ${xvfb_pid}
-# xvfb-run -a -s "-screen 0 1280x1024x24" $1
+kill -9 $xvfb_pid
