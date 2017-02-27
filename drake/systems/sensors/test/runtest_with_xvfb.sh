3fc81af2554080ad96e32272c2f81bdf16c0f299
Kunimatsu Hashimoto <kunimatsu.hashimoto@tri.global>
2017-02-27 15:51:45 -0500

    Run the unit test with Xvfb

---
+++ drake/systems/sensors/test/runtest_with_xvfb.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+lspci | grep VGA
+ldd $1
+libgl="`ldd $1 | grep libGL.so | grep nvidia`"
+if [ ! -z "$libgl" ]
+then
+  $1
+else
+  Xvfb :88 -ac -screen 0 1280x1024x24 &
+  sleep 3
+  DISPLAY=:88 $1
+  test_result=$?
+  if [ "$test_result" == "0" ]
+    then
+    echo "PASS"
+  else
+    echo "FAIL"
+  fi
+  xvfb_pid="`pgrep -f "Xvfb :88"`"
+  kill -9 $xvfb_pid
+fi
