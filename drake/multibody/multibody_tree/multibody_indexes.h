76faa8257aee9a8adf28de1aa8975ec17eaff96b
amcastro-tri <alejandro.castro@tri.global>
2017-02-28 09:55:37 -0500

    Implements TaggedInt version of indexes

--- drake/multibody/multibody_tree/multibody_indexes.h
+++ drake/multibody/multibody_tree/multibody_indexes.h
@@ -4,74 +4,68 @@
 
 #include <iostream>
 
-template <class Derived>
-class IndexBase {
+template <class Tag>
+class TaggedInt {
  public:
-    DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(IndexBase)
-    IndexBase() : ix(InvalidIndex) {}
-    explicit IndexBase(int i) : ix(i) {DRAKE_ASSERT(i >= 0 || i == InvalidIndex);}
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(TaggedInt)
+  TaggedInt() : ix(InvalidIndex) {}
+  explicit TaggedInt(int i) : ix(i) {DRAKE_ASSERT(i >= 0 || i == InvalidIndex);}
 
-    operator int() const { return ix;}
-    bool is_valid() const { return ix >= 0;}
-    bool is_invalid() const { return ix == InvalidIndex;}
-    void invalidate() { ix = InvalidIndex;}
+  operator int() const { return ix;}
+  bool is_valid() const { return ix >= 0;}
+  bool is_invalid() const { return ix == InvalidIndex;}
+  void invalidate() { ix = InvalidIndex;}
 
-    bool operator==(int  i) const { return ix == i;}                           
-    bool operator!=(int  i) const { return !operator==(i);}
+  bool operator==(int  i) const { return ix == i;}
+  bool operator!=(int  i) const { return !operator==(i);}
 
-    bool operator< (int  i) const { return ix < i;}                            
-    bool operator>=(int  i) const { return !operator<(i);}                     
-                                                                               
-    bool operator> (int  i) const { return ix > i;}                            
-    bool operator<=(int  i) const { return !operator>(i);}
+  bool operator< (int  i) const { return ix < i;}
+  bool operator>=(int  i) const { return !operator<(i);}
 
-    const Derived& operator++() {  /*prefix */
-      DRAKE_ASSERT(is_valid()); ++ix; return get_derived();
-    }
-    Derived operator++(int) {  /*postfix*/
-      DRAKE_ASSERT(is_valid()); ++ix; return Derived(ix-1);
-    }
+  bool operator> (int  i) const { return ix > i;}
+  bool operator<=(int  i) const { return !operator>(i);}
 
-    const Derived& operator--() {  /*prefix */
-      DRAKE_ASSERT(is_valid()); --ix; return get_derived();
-    }
-    Derived operator--(int) {  /*postfix*/
-      DRAKE_ASSERT(is_valid()); --ix; return Derived(ix+1);
-    }
+  const TaggedInt& operator++() {  /*prefix */
+    DRAKE_ASSERT(is_valid());
+    ++ix;
+    return *this;
+  }
+  TaggedInt operator++(int) {  /*postfix*/
+    DRAKE_ASSERT(is_valid()); ++ix; return TaggedInt(ix-1);
+  }
 
-    Derived& operator+=(int i) {
-      DRAKE_ASSERT(is_valid());
-      ix += i;
-      return get_mutable_derived();
-    }
+  const TaggedInt& operator--() {  /*prefix */
+    DRAKE_ASSERT(is_valid()); --ix; return *this;
+  }
+  TaggedInt operator--(int) {  /*postfix*/
+    DRAKE_ASSERT(is_valid()); --ix; return TaggedInt(ix+1);
+  }
 
-    Derived& operator-=(int i) {
-      DRAKE_ASSERT(is_valid());
-      ix -= i;
-      return get_mutable_derived();
-    }
+  TaggedInt& operator+=(int i) {
+    DRAKE_ASSERT(is_valid());
+    ix += i;
+    return *this;
+  }
 
-    static const Derived& Invalid() {
-      static const Derived invalid;
-      return invalid;
-    }
+  TaggedInt& operator-=(int i) {
+    DRAKE_ASSERT(is_valid());
+    ix -= i;
+    return *this;
+  }
+
+  static const TaggedInt& Invalid() {
+    static const TaggedInt invalid;
+    return invalid;
+  }
 
  private:
-  const Derived& get_derived() const {
-    return *static_cast<const Derived*>(this);
-  }
-
-  Derived& get_mutable_derived() {
-    return *static_cast<Derived*>(this);
-  }
-
   int ix;
   static const int InvalidIndex{-1111111111};
 };
 
-template <class IndexType>
+template <class Tag>
 inline std::ostream& operator<<(
-    std::ostream& o, const IndexBase<IndexType>& index) {
+    std::ostream& o, const TaggedInt<Tag>& index) {
   if (index.is_invalid())
     o << "Invalid";
   else
@@ -82,26 +76,9 @@ inline std::ostream& operator<<(
 namespace drake {
 namespace multibody {
 
-class FrameIndex : public IndexBase<FrameIndex> {
- public:
-  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(FrameIndex)
-  FrameIndex() = default;
-  explicit FrameIndex(int i) : IndexBase<FrameIndex>(i) {}
-};
-
-class BodyIndex : public IndexBase<BodyIndex> {
- public:
-  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(BodyIndex)
-  BodyIndex() = default;
-  explicit BodyIndex(int i) : IndexBase<BodyIndex>(i) {}
-};
-
-class MobilizerIndex : public IndexBase<MobilizerIndex> {
- public:
-  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(MobilizerIndex)
-  MobilizerIndex() = default;
-  explicit MobilizerIndex(int i) : IndexBase<MobilizerIndex>(i) {}
-};
+using FrameIndex = TaggedInt<class FrameTag>;
+using BodyIndex = TaggedInt<class BodyTag>;
+using MobilizerIndex = TaggedInt<class MobilizerTag>;
 
 /// For every MultibodyTree<T> the **world** body _always_ has this unique
 /// identifier and it is always zero.
