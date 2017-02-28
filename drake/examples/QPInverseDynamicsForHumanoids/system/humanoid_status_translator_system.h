c5cc653ff094a00d88c76acc28f2d5ea162e403e
Siyuan Feng <siyuan.feng@tri.global>
2017-02-27 21:32:41 -0500

    refactor humanoid status translator, now have differen flavors for robot_state_t message and raw state.

---
+++ drake/examples/QPInverseDynamicsForHumanoids/system/humanoid_status_translator_system.h
@@ -0,0 +1,125 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "drake/common/drake_copyable.h"
+#include "drake/multibody/rigid_body_tree.h"
+#include "drake/systems/framework/leaf_system.h"
+
+namespace drake {
+namespace examples {
+namespace qp_inverse_dynamics {
+
+/**
+ * An abstract base class for systems that translate various types of state
+ * into HumanoidStatus.
+ */
+class HumanoidStatusTranslatorSystem : public systems::LeafSystem<double> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(HumanoidStatusTranslatorSystem)
+
+  /**
+   * Constructor.
+   * @param robot Reference to a RigidBodyTree. The lifespan of @p robot must
+   * be longer than this object.
+   * @param alias_group_path Path to the alias groups file. Used to construct
+   * HumanoidStatus.
+   */
+  HumanoidStatusTranslatorSystem(const RigidBodyTree<double>& robot,
+                                 const std::string& alias_group_path);
+
+  std::unique_ptr<systems::AbstractValue> AllocateOutputAbstract(
+      const systems::OutputPortDescriptor<double>& descriptor) const override;
+
+  /**
+   * Returns the output port for HumanoidStatus.
+   */
+  inline const systems::OutputPortDescriptor<double>&
+  get_output_port_humanoid_status() const {
+    return get_output_port(output_port_index_humanoid_status_);
+  }
+
+ protected:
+  inline int get_output_port_index_humanoid_status() const {
+    return output_port_index_humanoid_status_;
+  }
+
+  inline const RigidBodyTree<double>& get_robot() const { return robot_; }
+
+ private:
+  const RigidBodyTree<double>& robot_;
+  const std::string alias_group_path_;
+
+  int output_port_index_humanoid_status_{0};
+};
+
+/**
+ * Translates a state vector to HumanoidStatus.
+ */
+class StateToHumanoidStatusSystem : public HumanoidStatusTranslatorSystem {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(StateToHumanoidStatusSystem)
+
+  /**
+   * Constructor.
+   * @param robot Reference to a RigidBodyTree. The lifespan of @p robot must
+   * be longer than this object.
+   * @param alias_group_path Path to the alias groups file. Used to construct
+   * HumanoidStatus.
+   */
+  StateToHumanoidStatusSystem(const RigidBodyTree<double>& robot,
+                              const std::string& alias_group_path);
+
+  void DoCalcOutput(const systems::Context<double>& context,
+                    systems::SystemOutput<double>* output) const override;
+
+  /**
+   * Returns the input port for a state vector.
+   */
+  inline const systems::InputPortDescriptor<double>& get_input_port_state()
+      const {
+    return get_input_port(input_port_index_state_);
+  }
+
+ private:
+  int input_port_index_state_{0};
+};
+
+/**
+ * Translates a bot_core::robot_state_t message to HumanoidStatus.
+ */
+class RobotStateMsgToHumanoidStatusSystem
+    : public HumanoidStatusTranslatorSystem {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(RobotStateMsgToHumanoidStatusSystem)
+
+  /**
+   * Constructor.
+   * @param robot Reference to a RigidBodyTree. The lifespan of @p robot
+   * must be longer than this object.
+   * @param alias_group_path Path to the alias groups file. Used to construct
+   * HumanoidStatus.
+   */
+  RobotStateMsgToHumanoidStatusSystem(const RigidBodyTree<double>& robot,
+                                      const std::string& alias_group_path);
+
+  void DoCalcOutput(const systems::Context<double>& context,
+                    systems::SystemOutput<double>* output) const override;
+
+  /**
+   * Returns input port for bot_core::robot_state_t.
+   */
+  inline const systems::InputPortDescriptor<double>&
+  get_input_port_robot_state_msg() const {
+    return get_input_port(input_port_index_lcm_msg_);
+  }
+
+ private:
+  int input_port_index_lcm_msg_{0};
+};
+
+}  // namespace qp_inverse_dynamics
+}  // namespace examples
+}  // namespace drake
--- drake/examples/QPInverseDynamicsForHumanoids/system/valkyrie_balancing_controller_system.cc
+++ drake/examples/QPInverseDynamicsForHumanoids/system/valkyrie_balancing_controller_system.cc
@@ -6,10 +6,10 @@
 #include "bot_core/robot_state_t.hpp"
 #include "drake/common/drake_path.h"
 #include "drake/common/text_logging.h"
+#include "drake/examples/QPInverseDynamicsForHumanoids/system/humanoid_status_translator_system.h"
 #include "drake/examples/QPInverseDynamicsForHumanoids/system/joint_level_controller_system.h"
 #include "drake/examples/QPInverseDynamicsForHumanoids/system/plan_eval_system.h"
 #include "drake/examples/QPInverseDynamicsForHumanoids/system/qp_controller_system.h"
-#include "drake/examples/QPInverseDynamicsForHumanoids/system/robot_state_decoder_system.h"
 #include "drake/examples/Valkyrie/valkyrie_constants.h"
 #include "drake/lcm/drake_lcm.h"
 #include "drake/multibody/joints/floating_base_types.h"
@@ -34,8 +34,10 @@ void controller_loop() {
       drake::GetDrakePath() +
       "/examples/Valkyrie/urdf/urdf/"
       "valkyrie_A_sim_drake_one_neck_dof_wide_ankle_rom.urdf";
+  const std::string kAliasGroupPath = drake::GetDrakePath() +
+                                      "/examples/QPInverseDynamicsForHumanoids/"
+                                      "config/valkyrie.alias_groups";
 
-  // Loads model.
   auto robot = std::make_unique<RigidBodyTree<double>>();
   parsers::urdf::AddModelInstanceFromUrdfFileToWorld(
       kModelFileName, multibody::joints::kRollPitchYaw, robot.get());
@@ -44,8 +46,9 @@ void controller_loop() {
 
   lcm::DrakeLcm lcm;
 
-  RobotStateDecoderSystem* rs_msg_to_rs =
-      builder.AddSystem(std::make_unique<RobotStateDecoderSystem>(*robot));
+  RobotStateMsgToHumanoidStatusSystem* rs_msg_to_rs =
+      builder.AddSystem(std::make_unique<RobotStateMsgToHumanoidStatusSystem>(
+          *robot, kAliasGroupPath));
   PlanEvalSystem* plan_eval =
       builder.AddSystem(std::make_unique<PlanEvalSystem>(*robot));
   QpControllerSystem* qp_con =
