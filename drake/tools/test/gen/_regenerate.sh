4bd281f9ab5e9071fa7dc3cb9d5bd772c623f1a0
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-02-28 23:06:52 -0500

    Add more direct test coverage of lcm_vector_gen

---
+++ drake/tools/test/gen/_regenerate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This program re-generates the source files in this directory.
+# This program is locally invoked by a developer, not `bazel run`.
+# This program's changes to the generated source files should be committed.
+
+set -e
+
+mydir=$(python -c \
+    'import os.path as p; print(p.dirname(p.dirname(p.realpath("'"$0"'"))))')
+drake=$(python -c \
+    'import os.path as p; print(p.realpath("'"$mydir"'/../.."))')
+namespace="drake::tools::test"
+
+source $drake/tools/lcm_vector_gen.sh
+
+# Generate one canonical test vector type.
+gen_lcm_and_vector_from_proto "sample" $mydir/sample.named_vector
+
+# Until CMake stops forcing us to dump all the lcmtypes into one place, the
+# lcm_vector_gen has to place lcmt_sample_t.lcm in a silly place.  Since nobody
+# needs it to be there, we'll move it manually for now.
+mv $drake/lcmtypes/lcmt_sample_t.lcm $mydir/gen/
