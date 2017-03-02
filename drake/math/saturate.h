5a80310c448a66d4ebe1f9c9e152858f034076c8
Chien-Liang Fok <liang.fok@tri.global>
2017-03-02 10:08:29 -0500

    Moves clamp() into a shared location.

---
+++ drake/math/saturate.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "drake/common/cond.h"
+#include "drake/common/drake_assert.h"
+
+namespace drake {
+namespace math {
+
+/// If `value` is within `[low, high]` then return it; else return the boundary.
+template <class T1, class T2, class T3>
+T1 saturate(const T1& value, const T2& low, const T3& high) {
+  DRAKE_ASSERT(low <= high);
+  return cond(
+      value < low, low,
+      value > high, high,
+      value);
+}
+
+}  // namespace math
+}  // namespace drake
