838467466cb15139a2d90887a0aefa4267f5a6b8
Sam Creasey <sam.creasey@tri.global>
2017-03-02 11:43:39 -0500

    Add a drake system which replicates most of the functionality from kuka_plan_runner.

---
+++ drake/examples/kuka_iiwa_arm/iiwa_plan_source.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "drake/systems/framework/context.h"
+#include "drake/systems/framework/leaf_system.h"
+#include "drake/systems/framework/value.h"
+#include "drake/multibody/rigid_body_tree.h"
+
+namespace drake {
+namespace examples {
+namespace kuka_iiwa_arm {
+
+/// This class implements a source of joint positions for an iiwa arm.
+/// It has two input ports, one for robot_plan_t messages containing a
+/// plan to follow, and another port for lcmt_iiwa_status messages.
+///
+/// If no plan has been received, the system will create an initial
+/// plan on the first unrestricted state update which commands the arm
+/// to hold at the measured position.
+///
+/// It is an error to attempt to read from the output port if a valid
+/// status message is not available.
+
+class IiwaPlanSource : public systems::LeafSystem<double> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(IiwaPlanSource)
+
+  explicit IiwaPlanSource(const std::string& model_path);
+  ~IiwaPlanSource() override;
+
+  const systems::InputPortDescriptor<double>& get_plan_input_port() const {
+    return this->get_input_port(plan_input_port_);
+  }
+
+  const systems::InputPortDescriptor<double>& get_status_input_port() const {
+    return this->get_input_port(status_input_port_);
+  }
+
+ protected:
+  std::unique_ptr<systems::AbstractState> AllocateAbstractState()
+      const override;
+
+  void DoCalcOutput(const systems::Context<double>& context,
+                    systems::SystemOutput<double>* output) const override;
+
+  void DoCalcUnrestrictedUpdate(const systems::Context<double>& context,
+                                systems::State<double>* state) const override;
+
+ private:
+  struct PlanData;
+
+  const int plan_input_port_{};
+  const int status_input_port_{};
+  RigidBodyTree<double> tree_;
+};
+
+}  // namespace kuka_iiwa_arm
+}  // namespace examples
+}  // namespace drake
---
+++ drake/examples/kuka_iiwa_arm/test/iiwa_plan_source_test.cc
@@ -0,0 +1,35 @@
+#include "drake/examples/kuka_iiwa_arm/iiwa_plan_source.h"
+
+#include <gtest/gtest.h>
+
+#include "drake/common/drake_path.h"
+
+namespace drake {
+namespace examples {
+namespace kuka_iiwa_arm {
+namespace {
+
+static const int kNumJoints = 7;
+const char* const kIiwaUrdf =
+    "/examples/kuka_iiwa_arm/models/iiwa14/iiwa14_simplified_collision.urdf";
+
+GTEST_TEST(IiwaPlanSourceTest, InstanceTest) {
+  // Test that the constructor works and that the expected ports are
+  // present.
+  IiwaPlanSource dut(GetDrakePath() + kIiwaUrdf);
+  EXPECT_EQ(dut.get_plan_input_port().get_data_type(),
+            systems::kAbstractValued);
+  EXPECT_EQ(dut.get_status_input_port().get_data_type(),
+            systems::kAbstractValued);
+  EXPECT_EQ(dut.get_output_port(0).get_data_type(),
+            systems::kVectorValued);
+  EXPECT_EQ(dut.get_output_port(0).size(), kNumJoints * 2);
+}
+
+// TODO(sam.creasey) Add a more meaningful test.
+
+}  // namespace
+
+}  // namespace kuka_iiwa_arm
+}  // namespace examples
+}  // namespace drake
