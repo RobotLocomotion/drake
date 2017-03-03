a78017dbc81a2231e5b836db8500a841bd4f08fa
David German <david.german@tri.global>
2017-03-03 11:18:55 -0500

    Introduce FrameVelocity, the time derivative of PoseVector.

---
+++ drake/systems/rendering/test/frame_velocity_test.cc
@@ -0,0 +1,45 @@
+#include "drake/systems/rendering/frame_velocity.h"
+
+#include "gtest/gtest.h"
+
+#include "drake/common/eigen_types.h"
+#include "drake/common/eigen_matrix_compare.h"
+
+namespace drake {
+namespace systems {
+namespace rendering {
+namespace {
+
+// Tests that a SpatialVelocity can be assigned to and read from FrameVelocity.
+GTEST_TEST(FrameVelocityTest, AssignAndRead) {
+  // The velocity is initialized to zero.
+  FrameVelocity<double> vec;
+  EXPECT_EQ(Vector6<double>::Zero(), vec.get_value());
+
+  Vector6<double> data;
+  data << 1, 2, 3, 4, 5, 6;
+  multibody::SpatialVelocity<double> vel(data);
+  vec.set_velocity(vel);
+  EXPECT_TRUE(CompareMatrices(Vector3<double>(1, 2, 3),
+                              vec.get_velocity().rotational()));
+  EXPECT_TRUE(CompareMatrices(Vector3<double>(4, 5, 6),
+                              vec.get_velocity().translational()));
+}
+
+GTEST_TEST(FrameVelocityTest, Clone) {
+  FrameVelocity<double> vec;
+  Vector6<double> data;
+  data << 1, 2, 3, 4, 5, 6;
+  multibody::SpatialVelocity<double> vel(data);
+  vec.set_velocity(vel);
+
+  auto clone = vec.Clone();
+  auto typed_clone = dynamic_cast<FrameVelocity<double>*>(clone.get());
+  ASSERT_NE(nullptr, typed_clone);
+  ASSERT_EQ(data, clone->get_value());
+}
+
+};  // namespace
+}  // namespace rendering
+}  // namespace systems
+}  // namespace drake
