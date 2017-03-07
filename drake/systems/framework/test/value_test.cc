56162bc02242155e6d4bd42b3b7c091d7c90ab2b
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-03-07 14:28:48 -0500

    Fix nullptr handling in VectorValue

--- drake/systems/framework/test/value_test.cc
+++ drake/systems/framework/test/value_test.cc
@@ -9,6 +9,7 @@
 
 #include "drake/common/drake_copyable.h"
 #include "drake/systems/framework/basic_vector.h"
+#include "drake/systems/framework/test/my_vector2.h"
 
 namespace drake {
 namespace systems {
@@ -171,11 +172,63 @@ GTEST_TEST(VectorValueTest, CopyConstructor) {
   EXPECT_EQ(3, other_value.get_value()->get_value().z());
 }
 
+GTEST_TEST(VectorValueTest, CopyConstructorSubclass) {
+  VectorValue<int> value(test::MyVector2<int>::Make(1, 2));
+  VectorValue<int> other_value(value);
+  value.get_value()->set_value(Eigen::Vector2i(3, 4));
+
+  const test::MyVector2<int>* const vector =
+      dynamic_cast<const test::MyVector2<int>*>(other_value.get_value());
+  ASSERT_NE(vector, nullptr);
+  EXPECT_EQ(vector->get_value()(0), 1);
+  EXPECT_EQ(vector->get_value()(1), 2);
+}
+
+GTEST_TEST(VectorValueTest, CopyConstructorNull) {
+  VectorValue<int> value{nullptr};
+
+  VectorValue<int> other_value(value);
+  EXPECT_EQ(other_value.get_value(), nullptr);
+}
+
 GTEST_TEST(VectorValueTest, AssignmentOperator) {
   VectorValue<int> value(BasicVector<int>::Make({1, 2, 3}));
   VectorValue<int> other_value(BasicVector<int>::Make({4, 5, 6}));
 
   value = other_value;
+  EXPECT_EQ(4, value.get_value()->get_value().x());
+  EXPECT_EQ(5, value.get_value()->get_value().y());
+  EXPECT_EQ(6, value.get_value()->get_value().z());
+}
+
+GTEST_TEST(VectorValueTest, AssignmentOperatorSubclass) {
+  VectorValue<int> value(test::MyVector2<int>::Make(1, 2));
+  VectorValue<int> other_value(BasicVector<int>::Make({5, 6}));
+  other_value = value;
+  value.get_value()->set_value(Eigen::Vector2i(3, 4));
+
+  const test::MyVector2<int>* const vector =
+      dynamic_cast<const test::MyVector2<int>*>(other_value.get_value());
+  ASSERT_NE(vector, nullptr);
+  EXPECT_EQ(vector->get_value()(0), 1);
+  EXPECT_EQ(vector->get_value()(1), 2);
+}
+
+GTEST_TEST(VectorValueTest, AssignmentOperatorNull) {
+  VectorValue<int> value{nullptr};
+  VectorValue<int> other_value(BasicVector<int>::Make({4, 5, 6}));
+
+  value = other_value;
+  other_value = VectorValue<int>{nullptr};
+  EXPECT_EQ(4, value.get_value()->get_value().x());
+  EXPECT_EQ(5, value.get_value()->get_value().y());
+  EXPECT_EQ(6, value.get_value()->get_value().z());
+  EXPECT_EQ(other_value.get_value(), nullptr);
+}
+
+GTEST_TEST(VectorValueTest, AssignmentOperatorSelf) {
+  VectorValue<int> value(BasicVector<int>::Make({4, 5, 6}));
+  value = value;
   EXPECT_EQ(4, value.get_value()->get_value().x());
   EXPECT_EQ(5, value.get_value()->get_value().y());
   EXPECT_EQ(6, value.get_value()->get_value().z());
