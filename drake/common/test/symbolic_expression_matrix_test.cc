60fd8b04951144029f35884a3c88e596fe065ea0
Soonho Kong <soonho.kong@tri.global>
2017-03-07 16:13:48 -0800

    Add tests for checking equality between symbolic matrices/arrays

--- drake/common/test/symbolic_expression_matrix_test.cc
+++ drake/common/test/symbolic_expression_matrix_test.cc
@@ -1,12 +1,17 @@
 #include "drake/common/symbolic_expression.h"
 
+#include <functional>
+
 #include "gtest/gtest.h"
 
+#include "drake/common/symbolic_formula.h"
 #include "drake/common/symbolic_variable.h"
 
 namespace drake {
 namespace symbolic {
 namespace {
+
+using std::ptr_fun;
 
 class SymbolicExpressionMatrixTest : public ::testing::Test {
  protected:
@@ -127,6 +132,81 @@ TEST_F(SymbolicExpressionMatrixTest, EigenDiv) {
                 (z_ / 2), (pi_ / 2);
   // clang-format on
   EXPECT_EQ(M, M_expected);
+}
+
+TEST_F(SymbolicExpressionMatrixTest, CheckStructuralEquality) {
+  EXPECT_TRUE(CheckStructuralEquality(A_, A_));
+  EXPECT_TRUE(CheckStructuralEquality(B_, B_));
+  EXPECT_TRUE(CheckStructuralEquality(C_, C_));
+
+  EXPECT_FALSE(CheckStructuralEquality(A_, C_));
+  EXPECT_FALSE(CheckStructuralEquality(B_ * A_, B_ * C_));
+}
+
+// Checks if m1.array() == m2.array() returns an array whose (i, j) element is a
+// formula m1(i, j) == m2(i, j).
+bool CheckArrayOperatorEq(const MatrixX<Expression>& m1,
+                          const MatrixX<Expression>& m2) {
+  const auto arr = (m1.array() == m2.array());
+  for (int i = 0; i < arr.rows(); ++i) {
+    for (int j = 0; j < arr.cols(); ++j) {
+      if (!arr(i, j).EqualTo(m1(i, j) == m2(i, j))) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+TEST_F(SymbolicExpressionMatrixTest, ArrayOperatorEq) {
+  const Eigen::Array<Formula, 3, 2> a1{A_.array() == A_.array()};
+  const Eigen::Array<Formula, 2, 3> a2{B_.array() == B_.array()};
+  const Eigen::Array<Formula, 3, 2> a3{C_.array() == C_.array()};
+  EXPECT_TRUE(a1.unaryExpr(ptr_fun(is_true)).all());
+  EXPECT_TRUE(a2.unaryExpr(ptr_fun(is_true)).all());
+  EXPECT_TRUE(a3.unaryExpr(ptr_fun(is_true)).all());
+
+  EXPECT_TRUE(CheckArrayOperatorEq(A_, C_));
+  EXPECT_TRUE(CheckArrayOperatorEq(B_ * A_, B_ * C_));
+}
+
+// Checks if m1 == m2 returns a formula which is a conjunction of
+// m1(i, j) == m2(i, j) for all i and j.
+bool CheckMatrixOperatorEq(const MatrixX<Expression>& m1,
+                           const MatrixX<Expression>& m2) {
+  const Formula f{m1 == m2};
+  Formula f_expected{};  // True
+  for (int i = 0; i < m1.rows(); ++i) {
+    for (int j = 0; j < m1.cols(); ++j) {
+      f_expected = f_expected && (m1(i, j) == m2(i, j));
+    }
+  }
+  return f.EqualTo(f_expected);
+}
+
+TEST_F(SymbolicExpressionMatrixTest, MatrixOperatorEq1) {
+  Eigen::Matrix<Expression, 2, 2> m1;
+  Eigen::Matrix<Expression, 2, 2> m2;
+  m1 << x_, y_, z_, x_;
+  m2 << z_, x_, y_, z_;
+  const Formula f{m1 == m2};
+  EXPECT_EQ(f.to_string(), "((x = z) and (y = x) and (z = y))");
+  ASSERT_TRUE(is_conjunction(f));
+  EXPECT_EQ(get_operands(f).size(), 3);
+  EXPECT_TRUE(CheckMatrixOperatorEq(m1, m2));
+}
+
+TEST_F(SymbolicExpressionMatrixTest, MatrixOperatorEq2) {
+  Eigen::Matrix<Expression, 2, 2> m1;
+  Eigen::Matrix<Expression, 2, 2> m2;
+  m1 << x_, 1.0, z_, x_;
+  m2 << z_, 2.0, y_, z_;
+  const Formula f1{m1 == m2};
+  // Because (1.0 == 2.0) is false, the whole conjunction is reduced
+  // to false.
+  EXPECT_TRUE(is_false(f1));
+  const Formula f2{m1 == m1};
+  EXPECT_TRUE(is_true(f2));
 }
 
 }  // namespace
