3af1b21d2a949d5bb4d27b8257ded33856ba02fd
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-03-09 08:49:05 -0500

    Add explicit instantiation on double
    This helps us fail-fast on compiler errors within method bodies.

--- drake/systems/framework/diagram.cc
+++ drake/systems/framework/diagram.cc
@@ -2,6 +2,15 @@
 
 namespace drake {
 namespace systems {
+namespace internal {
+
+template class DiagramOutput<double>;
+
+template class DiagramTimeDerivatives<double>;
+
+template class DiagramDiscreteVariables<double>;
+
+}  // namespace internal
 
 template class Diagram<double>;
 
--- drake/systems/framework/input_port_evaluator_interface.cc
+++ drake/systems/framework/input_port_evaluator_interface.cc
@@ -1,1 +1,11 @@
 #include "drake/systems/framework/input_port_evaluator_interface.h"
+
+namespace drake {
+namespace systems {
+namespace detail {
+
+template class InputPortEvaluatorInterface<double>;
+
+}  // namespace detail
+}  // namespace systems
+}  // namespace drake
