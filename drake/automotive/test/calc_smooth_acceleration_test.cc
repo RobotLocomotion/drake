75aa9edb3eba932f6c705602dd5cb703ee5f24eb
Chien-Liang Fok <liang.fok@tri.global>
2017-03-09 21:49:07 -0500

    Adds an acceleration command input to MaliputRailcar.

---
+++ drake/automotive/test/calc_smooth_acceleration_test.cc
@@ -0,0 +1,101 @@
+#include "drake/automotive/calc_smooth_acceleration.h"
+
+#include <stdexcept>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "drake/common/eigen_autodiff_types.h"
+#include "drake/common/symbolic_environment.h"
+#include "drake/common/symbolic_expression.h"
+#include "drake/common/symbolic_formula.h"
+#include "drake/common/symbolic_variable.h"
+
+namespace drake {
+
+using symbolic::Expression;
+using symbolic::Environment;
+using symbolic::Variable;
+
+namespace automotive {
+namespace {
+
+template <typename T>
+void do_test() {
+  // A positive desired acceleration when the current velocity is not close to
+  // the max velocity should result in a positive acceleration.
+  EXPECT_GT(
+      calc_smooth_acceleration(
+          T(10.),                     // desired_acceleration
+          T(10.),                     // current_velocity
+          T(50.),                     // max velocity
+          T(4.)),                     // velocity_limit_kp
+      T(0));
+
+  // A positive desired acceleration when the current velocity is equal to the
+  // max velocity should result in zero acceleration.
+  EXPECT_EQ(
+      calc_smooth_acceleration(
+          T(10.),                     // desired_acceleration
+          T(50.),                     // current_velocity
+          T(50.),                     // max velocity
+          T(4.)),                     // velocity_limit_kp
+      T(0));
+
+  // A negative desired acceleration when the current velocity is far from zero
+  // should result in negative acceleration.
+  EXPECT_LT(
+      calc_smooth_acceleration(
+          -10.,                    // desired_acceleration
+          50.,                     // current_velocity
+          50.,                     // max velocity
+          4.),                     // velocity_limit_kp
+      T(0));
+
+  // A desired acceleration of zero result in zero acceleration regardless of
+  // the current velocity.
+  const std::vector<double> test_velocities{0, 10, 20, 30, 40, 50};
+  for (const auto& velocity : test_velocities) {
+    EXPECT_EQ(
+        calc_smooth_acceleration(
+            0.,                      // desired_acceleration
+            velocity,                // current_velocity
+            50.,                     // max velocity
+            4.),                     // velocity_limit_kp
+        T(0));
+  }
+}
+
+GTEST_TEST(SmoothAccelerationFunctionTest, TestDoubleType) {
+  do_test<double>();
+}
+
+GTEST_TEST(SmoothAccelerationFunctionTest, TestAutoDiffXType) {
+  do_test<drake::AutoDiffXd>();
+}
+
+GTEST_TEST(SmoothAccelerationFunctionTest, TestExpressionType) {
+  do_test<Expression>();
+
+  Variable desired_acceleration{"desired_acceleration"};
+  Variable current_velocity{"current_velocity"};
+  Variable max_velocity{"max_velocity"};
+  Variable velocity_limit_kp{"velocity_limit_kp"};
+
+  auto smooth_acceleration_expression = calc_smooth_acceleration(
+      Expression{desired_acceleration},
+      Expression{current_velocity},
+      Expression{max_velocity},
+      Expression{velocity_limit_kp});
+  EXPECT_EQ(
+      smooth_acceleration_expression.Evaluate(
+          Environment{{desired_acceleration, 10},
+                      {current_velocity, 50},
+                      {max_velocity, 50},
+                      {velocity_limit_kp, 4}}),
+      0);
+}
+
+}  // namespace
+}  // namespace automotive
+}  // namespace drake
