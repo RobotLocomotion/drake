43ede6a468caaee92a24e970b0c57495d82feb9e
amcastro-tri <alejandro.castro@tri.global>
2017-03-13 11:50:34 -0400

    Adds spatial inertia files

---
+++ drake/multibody/multibody_tree/spatial_inertia.h
@@ -0,0 +1,242 @@
+#pragma once
+
+#include <memory>
+#include <sstream>
+#include <vector>
+
+#include "drake/common/drake_assert.h"
+#include "drake/common/eigen_types.h"
+#include "drake/math/cross_product.h"
+#include "drake/multibody/multibody_tree/math/spatial_algebra.h"
+#include "drake/multibody/multibody_tree/rotational_inertia.h"
+#include "drake/multibody/multibody_tree/unit_inertia.h"
+
+#include <iostream>
+#include <sstream>
+#define PRINT_VAR(x) std::cout <<  #x ": " << x << std::endl;
+#define PRINT_VARn(x) std::cout <<  #x ":\n" << x << std::endl;
+
+namespace drake {
+namespace multibody {
+
+template <typename T>
+class SpatialInertia {
+ public:
+  /// Default SpatialInertia constructor initializes mass, center of mass and
+  /// rotational inertia to invalid NaN's for a quick detection of
+  /// un-initialized values.
+  SpatialInertia() {}
+
+  /// Constructs a spatial inertia from a given mass, center of mass and
+  /// rotational inertia. The center of mass is measured from an origin Bo, i.e.
+  /// the center of mass is a vector from origin `Bo` to the body's center of
+  /// mass `Bc`, `com = p_BoBc`. The rotational inertia is exptected to be
+  /// computed about the same origin `Bo`.
+  /// Both center of mass `com` and rotational inertia `I` are expected to be
+  /// expressed on a same frame `F`.
+  /// @param[in] mass The mass of the body.
+  /// @param[in] com The center of mass of the body measured from an origin `Bo`
+  /// and expressed in a frame `F`.
+  /// @param[in] I Rotational inertia of the body computed about origin `Bo` and
+  /// expressed in a frame `F`.
+  SpatialInertia(
+      const T& mass, const Vector3<T>& com, const UnitInertia<T>& I) :
+      mass_(mass), p_BoBc_F_(com), I_Bo_F_(mass * I) {
+    //DRAKE_ASSERT(IsPhysicallyValid());
+  }
+
+  /// Returns `true` if any of the elements in this spatial inertia is NaN
+  /// and `false` otherwise.
+  bool IsNaN() const {
+    using std::isnan;
+    if (isnan(mass_)) return true;
+    if (I_Bo_F_.IsNaN()) return true;
+    if (p_BoBc_F_.array().isNaN().any()) return true;
+    return false;
+  }
+
+#if 0
+  // Default copy constructor and copy assignment.
+  SpatialInertia(const SpatialInertia<T>& other) = default;
+  SpatialInertia& operator=(const SpatialInertia<T>& other) = default;
+
+  const T& get_mass() const { return mass_;}
+
+  const Vector3<T>& get_com() const { return p_BoBc_F_;}
+
+  const RotationalInertia<T>& get_rotational_inertia() const { return I_Bo_F_;}
+
+  /// Get a copy to a full Matrix3 representation for this rotational inertia
+  /// including both lower and upper triangular parts.
+  Matrix6<T> CopyToFullMatrix6() const {
+    using math::CrossProductMatrix;
+    Matrix6<T> M;
+    M.template block<3, 3>(0, 0) = I_Bo_F_.CopyToFullMatrix3();
+    M.template block<3, 3>(0, 3) = mass_ * drake::math::VectorToSkewSymmetric(p_BoBc_F_);
+    M.template block<3, 3>(3, 0) = -M.template block<3, 3>(0, 3);
+    M.template block<3, 3>(3, 3) = mass_ * Matrix3<T>::Identity();
+    return M;
+  }
+
+  /// Sets this spatial inertia to have NaN entries. Typically used to quickly
+  /// detect uninitialized values since NaN will trigger a chain of invalid
+  /// computations that then can be tracked to the source.
+  void SetToNaN() {
+    mass_ = nan();
+    p_BoBc_F_.setConstant(nan());
+    I_Bo_F_.SetToNaN();
+  }
+
+  /// Performs a number of checks to verify that this is a physically valid
+  /// spatial inertia.
+  /// The chekcs performed are:
+  /// - No NaN entries.
+  /// - Positive mass.
+  /// - Valid rotational inertia,
+  /// @see RotationalInertia::IsPhysicallyValid().
+  bool IsPhysicallyValid() const {
+    if (isnan(mass_)) return false;
+    if (p_BoBc_F_.array().isNaN().any()) return false;
+    if (mass_ <= T(0)) return false;
+    if (!I_Bo_F_.IsPhysicallyValid()) return false;
+    return true;  // All tests passed.
+  }
+
+  bool IsApprox(const SpatialInertia& M_Bo_F,
+                double tolerance = Eigen::NumTraits<T>::epsilon()) {
+    using std::abs;
+    return
+        abs(mass_ - M_Bo_F.get_mass()) < tolerance &&
+        p_BoBc_F_.isApprox(M_Bo_F.get_com(), tolerance) &&
+        I_Bo_F_.IsApprox(M_Bo_F.get_rotational_inertia(), tolerance);
+  }
+
+  /// Adds spatial inertia @p `M_Bo_F` to this spatial inertia. This operation 
+  /// is only valid if both inertias are computed about the same center `Bo`
+  /// and expressed in the same frame `F`.
+  /// @param[in] M_Bo_F A spatial inertia to be added to this inertia.
+  /// @returns A reference to `this` spatial inetia.
+  SpatialInertia& operator+=(const SpatialInertia<T>& M_Bo_F) {
+    p_BoBc_F_ = get_mass() * get_com() + M_Bo_F.get_mass() * M_Bo_F.get_com();
+    mass_ += M_Bo_F.get_mass();
+    p_BoBc_F_ /= mass_;
+    I_Bo_F_ += M_Bo_F.I_Bo_F_;
+    return *this;
+  }
+
+  /// Computes the product from the right between this spatial inertia with the
+  /// spatial vector @p V. This spatial inertia and spatial vector @p V must be
+  /// expressed in the same frame.
+  /// @param[in] V Spatial vector to multiply from the right.
+  /// @returns The product from the right of `this` inertia with @p V.
+  SpatialVector<T> operator*(const SpatialVector<T>& V) const
+  {
+    const auto& v = V.linear();   // Linear velocity.
+    const auto& w = V.angular();  // Angular velocity.
+    const Vector3<T> mxp = mass_ * p_BoBc_F_;
+    return SpatialVector<T>(
+        I_Bo_F_ * w + mxp.cross(v), /* angular component */
+        mass_ * v - mxp.cross(w));  /* linear component */
+  }
+
+  /// Given this spatial inertia `M_Bo_F` about `Bo` and expressed in frame `F`,
+  /// this method re-expresses the same spatial inertia in another frame `A`.
+  /// This operation is performed in-place modifying the original object.
+  /// @param[in] R_AF Rotation matrix from frame `F` to frame `A`.
+  /// @returns A references to `this` object which now is the same spatial 
+  /// inertia about `Bo` but expressed in frame `A`.
+  SpatialInertia& ReExpressInPlace(const Matrix3<T>& R_AF) {
+    p_BoBc_F_ = R_AF * p_BoBc_F_;
+    I_Bo_F_.ReExpressInPlace(R_AF);
+    return *this;
+  }
+
+  /// Given this spatial inertia `M_Bo_F` about `Bo` and expressed in frame `F`,
+  /// this method computes the same spatial inertia but re-expressed in another
+  /// frame `A`.
+  /// @param[in] R_AF Rotation matrix from frame `F` to frame `A`.
+  /// @returns M_Bo_A The same spatial inertia about `Bo` but expressed in
+  /// frame `A`.
+  SpatialInertia ReExpress(const Matrix3<T>& R_AF) const {
+    return SpatialInertia(*this).ReExpressInPlace(R_AF);
+  }
+
+  /// This methods perfomrs the "parallel axis theorem" for spatial inertias:
+  /// given this spatial inertia `M_Bo_F` about `Bo` and expressed in frame `F`,
+  /// this method modifies this spatial inertia to be computed about a new
+  /// origin Xo. The result still is expressed in frame `F`.
+  /// This operation is performed in-place modifying the original object.
+  /// @see Shift() which does not modify this object.
+  ///
+  /// See Section 2.1.2, p. 20 of A. Jain's book.
+  ///
+  /// @param[in] p_BoXo_F Vector from the original origin `Bo` to the new origin
+  /// `Xo`, expressed in the spatial inertia frame `F`.
+  /// @returns `M_Xo_F` This same spatial inertia but computed about
+  /// origin `Xo`.
+  SpatialInertia& ShiftInPlace(const Vector3<T>& p_BoXo_F) {
+    using math::CrossProductMatrixSquared;
+    const Vector3<T> p_XoBc_F = p_BoBc_F_ - p_BoXo_F;
+    const Matrix3<T> Sp_BoBc_F = CrossProductMatrixSquared(p_BoBc_F_);
+    const Matrix3<T> Sp_XoBc_F = CrossProductMatrixSquared(p_XoBc_F);
+    I_Bo_F_.get_mutable_symmetric_matrix_view() =
+        I_Bo_F_.get_matrix() + mass_ * (Sp_BoBc_F - Sp_XoBc_F);
+    p_BoBc_F_ = p_XoBc_F;
+    return *this;
+  }
+
+  /// This methods perfomrs the "parallel axis theorem" for spatial inertias:
+  /// given this spatial inertia `M_Bo_F` about `Bo` and expressed in frame `F`,
+  /// this method returns this spatial inertia to but computed about a new
+  /// origin Xo. The result still is expressed in frame `F`.
+  /// @see ShiftInPlace() for the in-place operation.
+  ///
+  /// See Section 2.1.2, p. 20 of A. Jain's book.
+  ///
+  /// @param[in] p_BoXo_F Vector from the original origin `Bo` to the new origin
+  /// `Xo`, expressed in the spatial inertia frame `F`.
+  /// @returns `M_Xo_F` This same spatial inertia but computed about
+  /// origin `Xo`.
+  SpatialInertia Shift(const Vector3<T>& p_BoXo_F) const {
+    return SpatialInertia(*this).ShiftInPlace(p_BoXo_F);
+  }
+#endif
+
+ private:
+  // Helper method for NaN initialization.
+  static constexpr T nan() {
+    return std::numeric_limits<
+        typename Eigen::NumTraits<T>::Literal>::quiet_NaN();
+  }
+
+  // Mass of the body.
+  T mass_{nan()};
+  // Center of mass measured in X and expressed in Y. Typically X is the body
+  // frame and Y is the world frame.
+  Vector3<T> p_BoBc_F_{Vector3<T>::Constant(nan())};
+  // Rotational inertia about Xo and expressed in Y.
+  RotationalInertia<T> I_Bo_F_{};  // Defaults to NaN initialized inertia.
+};
+
+#if 0
+template <typename T>
+inline SpatialInertia<T> operator*(
+    const T& s, const SpatialInertia<T>& I_Bo_F) {
+  return SpatialInertia<T>(s * I_Bo_F.get_mass(),
+                           s * I_Bo_F.get_com(),
+                           s * I_Bo_F.get_rotational_inertia());
+}
+
+template <typename T> inline
+std::ostream& operator<<(std::ostream& o,
+                         const SpatialInertia<T>& M) {
+  return o
+      << " mass = " << M.get_mass() << std::endl
+      << " com = [" << M.get_com().transpose() << "]^T" << std::endl
+      << " I = " << std::endl
+      << M.get_rotational_inertia();
+}
+#endif
+
+}  // namespace multibody
+}  // namespace drake
---
+++ drake/multibody/multibody_tree/test/spatial_inertia_tests.cc
@@ -0,0 +1,282 @@
+#include "gtest/gtest.h"
+
+#include "drake/common/eigen_types.h"
+#include "drake/multibody/multibody_tree/rotational_inertia.h"
+#include "drake/multibody/multibody_tree/spatial_inertia.h"
+#include "drake/multibody/multibody_tree/unit_inertia.h"
+
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#define PRINT_VAR(x) std::cout <<  #x ": " << x << std::endl;
+#define PRINT_VARn(x) std::cout <<  #x ":\n" << x << std::endl;
+
+namespace drake {
+namespace multibody {
+namespace math {
+namespace {
+
+using Eigen::AngleAxisd;
+using Eigen::NumTraits;
+using Eigen::Vector3d;
+using std::sort;
+
+// Test default constructor which leaves entries initialized to NaN for a
+// quick detection of un-initialized values.
+GTEST_TEST(UnitInertia, DefaultConstructor) {
+  SpatialInertia<double> I;
+  ASSERT_TRUE(I.IsNaN());
+}
+
+
+
+#if 0
+GTEST_TEST(SpatialInertia, PlusEqualOperator) {
+  const double L = 2.0;
+  // Rod frame R located at the rod's geometric center and oriented along its
+  // principal axes.
+  // Inertia computed about Ro and expressed in R.
+
+  // Spatial inertia computed about the origin for a cube with sides of
+  // length 2.0 centered at x = 1.0. Expressed in world frame.
+  const double mass_right = 1.5;
+  SpatialInertia<double> MRightBox_Wo_W(
+      mass_right,
+      Vector3d::Zero(),
+      UnitInertia<double>::SolidCube(L));
+  MRightBox_Wo_W.ShiftInPlace(-Vector3d::UnitX());
+  // Check if after transformation this still is a physically valid inertia.
+  EXPECT_TRUE(MRightBox_Wo_W.IsPhysicallyValid());
+
+  PRINT_VARn(MRightBox_Wo_W);
+
+  // Spatial inertia computed about the origin for a cube with sides of
+  // length 2.0 centered at x = -1.0. Expressed in world frame.
+  const double mass_left = 0.5;
+  SpatialInertia<double> MLeftBox_Wo_W(
+      mass_left,
+      Vector3d::Zero(),
+      UnitInertia<double>::SolidCube(L));
+  MLeftBox_Wo_W.ShiftInPlace(Vector3d::UnitX());
+  // Check if after transformation this still is a physically valid inertia.
+  EXPECT_TRUE(MLeftBox_Wo_W.IsPhysicallyValid());
+
+  PRINT_VARn(MLeftBox_Wo_W);
+
+  // Spatial inertia of a prism with a squared transverse area of size
+  // 2.0 x 2.0 and length 4.0.
+  // This is computed by adding the above spatial inertias.
+  // Notice that the origina and the expressed-in frame is the same as in the
+  // two individual components.
+  SpatialInertia<double> MPrism_Wo_W(MLeftBox_Wo_W);
+  MPrism_Wo_W += MRightBox_Wo_W;
+  EXPECT_TRUE(MPrism_Wo_W.IsPhysicallyValid());
+
+  PRINT_VARn(MPrism_Wo_W);
+
+  // Check that the compound inertia corresponds to that of a larger box of
+  // length 4.0.
+  const double mass = mass_left + mass_right;
+  const Vector3d com(
+      (mass_left * MLeftBox_Wo_W.get_com() +
+       mass_right * MRightBox_Wo_W.get_com()) / mass);
+  SpatialInertia<double> MExpected_Wo_W(
+      mass,
+      com,
+      UnitInertia<double>::SolidBox(2 * L, L, L));
+  // Check if after transformation this still is a physically valid inertia.
+  EXPECT_TRUE(MExpected_Wo_W.IsPhysicallyValid());
+
+  PRINT_VARn(MExpected_Wo_W);
+
+  EXPECT_TRUE(MPrism_Wo_W.IsApprox(MExpected_Wo_W));
+}
+#endif
+
+#if 0
+GTEST_TEST(SpatialInertia, ReExpress) {
+  // Spatial inertia for a cube of unit mass computed about its center of mass
+  // and expressed in its principal axes frame.
+  const double Lx = 0.2, Ly = 1.0, Lz = 0.5;  // Box's lengths.
+  SpatialInertia<double> M_Bo_B(
+      1.0, Vector3d::UnitX(), UnitInertia<double>::SolidBox(Lx, Ly, Lz));
+
+  // Replace this pring by the respective getters and check values.
+  PRINT_VARn(M_Bo_B);
+
+  // Place B rotated +90 degrees about W's x-axis.
+  Matrix3<double> R_WB =
+      AngleAxisd(M_PI_2, Vector3d::UnitX()).toRotationMatrix();
+  PRINT_VARn(R_WB);
+
+  SpatialInertia<double> M_Bo_W = M_Bo_B.ReExpress(R_WB);
+
+  // Checks for physically correct spatial inertia.
+  EXPECT_TRUE(M_Bo_W.IsPhysicallyValid());
+
+  // Replace this print by the respective checks of the new computed values.
+  // What was in y now is in z.
+  PRINT_VARn(M_Bo_W);
+}
+#endif
+
+#if 0
+GTEST_TEST(SpatialInertia, Shift) {
+  // Place B rotated +90 degrees about W's x-axis.
+  Matrix3<double> R_WB =
+      AngleAxisd(M_PI_2, Vector3d::UnitX()).toRotationMatrix();
+
+  // Spatial inertia for a thin rod of unit mass computed about its center of
+  // mass and expressed in the world frame W.
+  const double mass = 1.2;
+  const double radius = 0.05, length = 1.0;
+  SpatialInertia<double> M_Bo_W(
+      mass, Vector3d::Zero(),
+      UnitInertia<double>::SolidRod(radius, length));
+  M_Bo_W.ReExpressInPlace(R_WB);
+
+  // Replace this print by the respective getters and check values.
+  PRINT_VARn(M_Bo_W);
+
+  // Vector from Bo, in this case Bo = Bc, to the top of the box.
+  Vector3d p_BoXo_W(0, 0.5, 0);
+
+  PRINT_VARn(p_BoXo_W.transpose());
+
+  // Computes spatial inertia about Xo, still expressed in W.
+  SpatialInertia<double> M_Xo_W = M_Bo_W.Shift(p_BoXo_W);
+
+  // Replace this print by the respective checks of the new computed values.
+  // What was in y now is in z.
+  PRINT_VARn(M_Xo_W);
+
+  // Checks for physically correct spatial inertia.
+  EXPECT_TRUE(M_Xo_W.IsPhysicallyValid());
+
+  // Expected moment of inertia for a rod when computed about one of its ends.
+  const double I_end = mass * length / 3.0;
+  const auto& I_Xo_W = M_Xo_W.get_rotational_inertia();
+  EXPECT_NEAR(I_Xo_W(0,0), I_end, Eigen::NumTraits<double>::epsilon());
+  EXPECT_NEAR(I_Xo_W(2,2), I_end, Eigen::NumTraits<double>::epsilon());
+}
+
+// This tests the implementation of two product operators:
+// 1. SpatialVector operator*(const SpatialVector& V) const;
+// 2. SpatialVelocityJacobian operator*(const SpatialVelocityJacobian& J) const;
+//
+// The first operator allows to multiply a SpatialInertia with a SpatialVector
+// from the right.
+// The second operator allows to multiply a SpatialInertia with a
+// SpatialVelocityJacobian. A SpatialVelocityJacobian can be viewed as a matrix
+// where each column is a SpatialVector.
+GTEST_TEST(SpatialInertia, ProductWithSpatialVectors) {
+  const double mass = 2.5;
+  const double radius = 0.1;
+  const double length = 1.0;
+
+  // Spatial inertia of a rod along the z-axis computed about center of mass.
+  SpatialInertia<double> M_Bc_W(
+      mass,
+      Vector3d::Zero(),
+      UnitInertia<double>::SolidRod(radius, length));
+
+  // Spatial inertia about the top end of the bar.
+  SpatialInertia<double> M_Eo_W =
+      M_Bc_W.Shift(length / 2 * Vector3d::UnitZ());
+  EXPECT_TRUE(M_Eo_W.IsPhysicallyValid());
+
+  // Assume a given linear velocity.
+  const Vector3d v_WB(-1, 2, 8);
+
+  // Assume a given angular velocity.
+  const Vector3d w_WB(1, 2, 3);
+
+  // Spatial velocity of its top end frame.
+  SpatialVector<double> V_WB(w_WB, v_WB);
+
+  // Product of a SpatialInertia times a SpatialVector
+  SpatialVector<double> F_WB = M_Bc_W * V_WB;
+
+  SpatialVector<double> F_expected(
+      Vector3d(5.0 / 24.0, 5.0 / 12.0, 0.0375),
+      Vector3d(-2.5, 5.0, 20.0));
+  EXPECT_TRUE(F_WB.IsApprox(F_expected, Eigen::NumTraits<double>::epsilon()));
+
+  // Jacobian with three columns but a maximum up to six columns.
+  SpatialVelocityJacobianUpTo6<double> H(3);
+  H.col(0) = V_WB;
+  H.col(1) = 2. * V_WB;
+  H.col(2) = 3. * V_WB;
+  EXPECT_EQ(H.cols(), 3);  // Verifying the constructor did the right thing.
+
+  // Product of a SpatialInertia times a SpatialVelocityJacobian.
+  SpatialVelocityJacobianUpTo6<double> MxH = M_Bc_W * H;
+  EXPECT_EQ(MxH.rows(), 6);  // Trivial. This is always true.
+  EXPECT_EQ(MxH.cols(), H.cols());  // Depends on the size of H.
+  EXPECT_TRUE(MxH.col(0).IsApprox(F_WB, Eigen::NumTraits<double>::epsilon()));
+  EXPECT_TRUE(MxH.col(1).IsApprox(
+      2.0 * F_WB, Eigen::NumTraits<double>::epsilon()));
+  EXPECT_TRUE(MxH.col(2).IsApprox(
+      3.0 * F_WB, Eigen::NumTraits<double>::epsilon()));
+}
+
+GTEST_TEST(SpatialInertia, ComputeKineticEnergy) {
+  const double mass = 2.5;
+  const double radius = 0.1;
+  const double length = 1.0;
+
+  // Spatial inertia of a rod along the z-axis computed about center of mass.
+  SpatialInertia<double> M_Bc_W(
+      mass,
+      Vector3d::Zero(),
+      UnitInertia<double>::SolidRod(radius, length));
+
+  // Spatial inertia about the top end of the bar.
+  SpatialInertia<double> M_Eo_W =
+      M_Bc_W.Shift(length / 2 * Vector3d::UnitZ());
+  EXPECT_TRUE(M_Eo_W.IsPhysicallyValid());
+
+  // Assume a given angular velocity.
+  const double w0 = 2.5;  // Magnitude of the angular velocity.
+  const Vector3d w_WBc = -w0 * Vector3d::UnitY();
+
+  // Spatial velocity of its top end frame.
+  SpatialVector<double> V_WE(w_WBc, Vector3d::Zero());
+
+  // Spatial velocity of its center of mass.
+  SpatialVector<double> V_WB =
+      /* phi_BE */
+      ShiftOperator<double>(M_Eo_W.get_com()).transpose() * V_WE;
+
+  PRINT_VARn(V_WE);
+  PRINT_VARn(V_WB);
+  PRINT_VARn(M_Bc_W);
+
+  // Computes kinetic energy using spatial operations:
+  // Using spatial algebra about the center of mass:
+  double K_com = 0.5 * V_WB.dot(M_Bc_W * V_WB);
+
+  PRINT_VARn(M_Bc_W * V_WB);
+
+  // Using spatial algebra about the rod's end:
+  double K_end = 0.5 * V_WE.dot(M_Eo_W * V_WE);
+
+  PRINT_VAR(K_com);
+  PRINT_VAR(K_end);
+
+  // Expected kinetic energy of a rod rotation about its end.
+  const double Iend = mass * length * length / 3.0;
+  const double Kexpected = 0.5 * Iend * w0 * w0;
+  PRINT_VAR(Kexpected);
+
+  EXPECT_NEAR(Kexpected, K_com, 4 * NumTraits<double>::epsilon());
+  EXPECT_NEAR(Kexpected, K_end, 4 * NumTraits<double>::epsilon());
+
+}
+#endif
+
+
+}
+}  // math
+}  // multibody
+}  // drake
