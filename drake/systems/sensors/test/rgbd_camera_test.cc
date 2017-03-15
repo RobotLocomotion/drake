78b99e2ae5d8fde92ed20369a0c816ad10b6ec93
Kunimatsu Hashimoto <kunimatsu.hashimoto@tri.global>
2017-03-15 11:26:10 -0400

    Add a tolerance for the RgbdCamera unit test

--- drake/systems/sensors/test/rgbd_camera_test.cc
+++ drake/systems/sensors/test/rgbd_camera_test.cc
@@ -145,13 +145,13 @@ class RenderingSim : public systems::Diagram<double> {
   std::shared_ptr<RigidBodyFrame<double>> rgbd_camera_frame_;
 };
 
-void AssertLe(uint8_t value_a, uint8_t value_b, uint8_t tolerance) {
-  int a = static_cast<int>(value_a);
-  int b = static_cast<int>(value_b);
-  int t = static_cast<int>(tolerance);
-  ASSERT_LE(std::abs(a - b), t);
+template <typename T>
+void AssertNear(T value_a, T value_b, T tolerance) {
+  const double a = static_cast<double>(value_a);
+  const double b = static_cast<double>(value_b);
+  const double t = static_cast<double>(tolerance);
+  ASSERT_NEAR(a, b, t);
 }
-
 
 const std::array<uint8_t, 4> kBackgroundColor{{204u, 229u, 255u, 255u}};
 
@@ -238,7 +238,8 @@ class ImageTest : public ::testing::Test {
     for (int v = 0; v < color_image.height(); v += 20) {
       for (int u = 0; u < color_image.width(); u += 20) {
         for (int ch = 0; ch < 4; ++ch) {
-          AssertLe(color_image.at(u, v)[ch], color[ch], kColorPixelTolerance);
+          AssertNear<uint8_t>(color_image.at(u, v)[ch], color[ch],
+                              kColorPixelTolerance);
         }
         // Assuming depth value provides 0.1 mm precision.
         ASSERT_NEAR(depth_image.at(u, v)[0], depth, 1e-4);
@@ -289,8 +290,8 @@ class ImageTest : public ::testing::Test {
 
     for (const auto& corner : kCorners) {
       for (int ch = 0; ch < color_image.num_channels(); ++ch) {
-        AssertLe(color_image.at(corner.u, corner.v)[ch],
-                 kBackgroundColor[ch], kColorPixelTolerance);
+        AssertNear<uint8_t>(color_image.at(corner.u, corner.v)[ch],
+                            kBackgroundColor[ch], kColorPixelTolerance);
       }
       ASSERT_NEAR(depth_image.at(corner.u, corner.v)[0], 2.f, 1e-4);
     }
@@ -299,8 +300,8 @@ class ImageTest : public ::testing::Test {
     const int kHalfWidth = color_image.width() / 2;
     const int kHalfHeight = color_image.height() / 2;
     for (int ch = 0; ch < color_image.num_channels(); ++ch) {
-      AssertLe(color_image.at(kHalfWidth, kHalfHeight)[ch],
-               255u, kColorPixelTolerance);
+      AssertNear<uint8_t>(color_image.at(kHalfWidth, kHalfHeight)[ch],
+                          255u, kColorPixelTolerance);
     }
     // Verifies the center point's depth.
     ASSERT_NEAR(depth_image.at(kHalfWidth, kHalfHeight)[0], 1.f, 1e-4);
@@ -323,7 +324,8 @@ class ImageTest : public ::testing::Test {
       }
     }
 
-    EXPECT_EQ(expected_horizon, actual_horizon);
+    // We need a tolerance because the result varies depending on the CPU.
+    AssertNear<int>(expected_horizon, actual_horizon, 1);
   }
 
  protected:
