1b4546b724f9a7a88532149118cc3e90b50677e8
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-03-19 11:06:21 -0400

    clang-format-includes on pybind11

--- drake/bindings/pybind11/pydrake_symbolic_types.h
+++ drake/bindings/pybind11/pydrake_symbolic_types.h
@@ -1,9 +1,9 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
 
-#include "drake/common/symbolic_variable.h"
 #include "drake/common/symbolic_expression.h"
 #include "drake/common/symbolic_formula.h"
+#include "drake/common/symbolic_variable.h"
 
 PYBIND11_NUMPY_OBJECT_DTYPE(drake::symbolic::Variable);
 PYBIND11_NUMPY_OBJECT_DTYPE(drake::symbolic::Expression);
