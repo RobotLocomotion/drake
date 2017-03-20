f7c0283650e881dea7b6c0d71053e0c8930ef27e
Chien-Liang Fok <liang.fok@tri.global>
2017-03-20 14:30:07 -0400

    Adds CarVisualizationApplicator.

---
+++ drake/automotive/car_vis_applicator.cc
@@ -0,0 +1,168 @@
+#include "drake/automotive/car_vis_applicator.h"
+
+#include <cmath>
+#include <utility>
+
+#include <Eigen/Geometry>
+
+#include "drake/common/drake_assert.h"
+#include "drake/multibody/shapes/visual_element.h"
+#include "drake/systems/framework/value.h"
+#include "drake/systems/rendering/drake_visualizer_client.h"
+
+namespace drake {
+
+using systems::rendering::MakeGeometryData;
+using systems::rendering::PoseBundle;
+using systems::rendering::PoseVector;
+using systems::Value;
+
+namespace automotive {
+
+template <typename T> const int CarVis<T>::kInvalidIndex;
+
+template <typename T>
+void CarVis<T>::AddVisualizationElements(lcmt_viewer_load_robot* message) {
+  if (starting_index() != kInvalidIndex) {
+    throw std::runtime_error("CarVis::AddVisualizationElements(): Attempted "
+        "to call this method more than once.");
+  }
+  starting_index_ = message->num_links;
+  AddVisualizationElementsImpl(message);
+}
+
+template <typename T>
+void CarVis<T>::AddPoses(const Isometry3<T>& root_pose,
+  systems::rendering::PoseBundle<T>* pose_bundle) const {
+  if (this->starting_index() == -1) {
+    throw std::runtime_error("CarVis::AddPoses(): Please call "
+      "AddVisualizationElements() first.");
+  }
+  AddPosesImpl(root_pose, pose_bundle);
+}
+
+template <typename T>
+void BoxCarVis<T>::AddVisualizationElementsImpl(
+    lcmt_viewer_load_robot* message) {
+  DrakeShapes::VisualElement box_vis(
+      DrakeShapes::Box(Eigen::Vector3d(1.0, 1.0, 1.0)),
+      Eigen::Isometry3d::Identity(),
+      Eigen::Vector4d(0.7, 0.7, 0.7, 1));
+
+  drake::lcmt_viewer_link_data link_data;
+  link_data.name = CarVis<T>::name();
+  link_data.robot_num = CarVis<T>::model_instance_id();
+  link_data.num_geom = 1;
+  link_data.geom.resize(1);
+  link_data.geom[0] = MakeGeometryData(box_vis);
+
+  message->num_links++;
+  message->link.push_back(link_data);
+}
+
+template <typename T>
+void BoxCarVis<T>::AddPosesImpl(const Isometry3<T>& root_pose,
+    PoseBundle<T>* pose_bundle) const {
+  const int index = this->starting_index();
+  DRAKE_ASSERT(index < pose_bundle->get_num_poses());
+  pose_bundle->set_pose(index, root_pose);
+  pose_bundle->set_name(index, this->name());
+  pose_bundle->set_model_instance_id(index, this->model_instance_id());
+}
+
+template <typename T>
+CarVisApplicator<T>::CarVisApplicator() {
+  pose_input_port_index_ =
+      systems::LeafSystem<T>::DeclareAbstractInputPort(
+          Value<PoseBundle<T>>(0)).get_index();
+  output_port_index_ =
+      systems::LeafSystem<T>::DeclareAbstractOutputPort(
+          Value<PoseBundle<T>>(0)).get_index();
+  // These are intialized to zero because they will be increased to the correct
+  // values after AddCarVis() is called the necessary number of times.
+  load_robot_message_.num_links = 0;
+  load_robot_message_.link.resize(0);
+}
+
+template <typename T>
+const systems::InputPortDescriptor<T>&
+CarVisApplicator<T>::get_pose_input_port() const {
+  return systems::System<T>::get_input_port(pose_input_port_index_);
+}
+
+template <typename T>
+const systems::OutputPortDescriptor<T>&
+CarVisApplicator<T>::get_output_port() const {
+  return systems::System<T>::get_output_port(output_port_index_);
+}
+
+template <typename T>
+void CarVisApplicator<T>::AddCarVis(std::unique_ptr<CarVis<T>> vis) {
+  const int id = vis->model_instance_id();
+  if (static_cast<int>(car_visualizers_.size()) != id) {
+    throw std::runtime_error("CarVisApplicator::AddCarVis(): Attempted to add "
+        "CarVis out of model instance ID order. Expected model instance ID " +
+        std::to_string(car_visualizers_.size()) + ", but got " +
+        std::to_string(id) + ".");
+  }
+  vis->AddVisualizationElements(&load_robot_message_);
+  car_visualizers_.push_back(std::move(vis));
+}
+
+template <typename T>
+void CarVisApplicator<T>::DoCalcOutput(
+    const systems::Context<T>& context,
+    systems::SystemOutput<T>* output) const {
+  // Obtains the input and output.
+  const PoseBundle<T>& vehicle_poses =
+      systems::System<T>::EvalAbstractInput(context,
+          pose_input_port_index_)->template GetValue<PoseBundle<T>>();
+  DRAKE_ASSERT(vehicle_poses.get_num_poses() == num_cars());
+  PoseBundle<T>& visualization_poses =
+      output->GetMutableData(output_port_index_)->
+          template GetMutableValue<PoseBundle<T>>();
+  // Resize the output PoseBundle as necessary.
+  if (visualization_poses.get_num_poses() != num_vis_poses()) {
+    visualization_poses = PoseBundle<T>(num_vis_poses());
+  }
+
+  for (int i = 0; i < vehicle_poses.get_num_poses(); ++i) {
+    const int id = vehicle_poses.get_model_instance_id(i);
+    // TODO(liang.fok) Generalize the following to support input PoseVector
+    // objects that are out of order with respect to the model instance ID.
+    if (id != i) {
+      throw std::runtime_error("CarVisApplicator::DoCalcOutput(): Input "
+          "PoseBundle has invalid model instance ID at index " +
+          std::to_string(i) + ". Expected " +
+          std::to_string(i) + " but got " + std::to_string(id));
+    }
+    const std::string& name = vehicle_poses.get_name(i);
+    const std::string& expected_name = car_visualizers_.at(i)->name();
+    if (name != expected_name) {
+      throw std::runtime_error("CarVisApplicator::DoCalcOutput(): Input "
+          "PoseBundle has invalid model name at index " +
+          std::to_string(i) + ". Expected \"" + expected_name +
+          "\" but got \"" + name + "\".");
+    }
+    const Isometry3<T>& root_pose = vehicle_poses.get_pose(i);
+    car_visualizers_.at(i)->AddPoses(root_pose, &visualization_poses);
+  }
+}
+
+template <typename T>
+int CarVisApplicator<T>::num_vis_poses() const {
+  int result{0};
+  for (const auto &v : car_visualizers_) {
+    result += v->num_poses();
+  }
+  return result;
+}
+
+// These instantiations must match the API documentation in
+// car_vis_applicator.h.
+template class CarVisApplicator<double>;
+template class CarVis<double>;
+template class BoxCarVis<double>;
+
+}  // namespace automotive
+}  // namespace drake
---
+++ drake/automotive/car_vis_applicator.h
@@ -0,0 +1,205 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <Eigen/Geometry>
+
+#include "drake/common/drake_copyable.h"
+#include "drake/lcmtypes/drake/lcmt_viewer_load_robot.hpp"
+#include "drake/systems/framework/leaf_system.h"
+#include "drake/systems/rendering/pose_bundle.h"
+#include "drake/systems/rendering/pose_vector.h"
+
+namespace drake {
+namespace automotive {
+
+/// CarVis is a base class that provides visualization geometries and their
+/// poses.
+///
+/// AddVisualizationElements() must be called before AddPoses() because that
+/// determines the starting index at which both the visualization geometries and
+/// their poses are stored. Specifically, AddVisualizationElements() adds the
+/// visual elements to the end of the provided `lcmt_viewer_load_robot` message.
+/// AddPoses saves the poses in the same indices as the visual elements within
+/// the provided PoseBundle.
+///
+/// Instantiated templates for the following kinds of T's are provided:
+/// - double
+///
+/// They are already available to link against in the containing library.
+///
+/// @ingroup automotive_systems
+///
+template <typename T>
+class CarVis {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(CarVis)
+
+  /// The constructor.
+  ///
+  /// @param model_instance_id The model instance ID of the model being
+  /// visualized.
+  ///
+  /// @param name The name of the vehicle being visualized. This can be any
+  /// user-defined value.
+  ///
+  CarVis(int model_instance_id, const std::string& name)
+      : model_instance_id_(model_instance_id), name_(name) {}
+
+  virtual ~CarVis() {}
+
+  /// Adds the visualization elements to @p message. See this class's
+  /// description for more details.
+  ///
+  /// @pre This method was not previously called.
+  void AddVisualizationElements(lcmt_viewer_load_robot* message);
+
+  /// Computes the poses of the bodies that are part of the model instance's
+  /// visualization, and adds them to @p pose_bundle. See this class's
+  /// description for more details.
+  ///
+  /// @pre AddVisualizationElements() was called.
+  void AddPoses(const Isometry3<T>& root_pose,
+    systems::rendering::PoseBundle<T>* pose_bundle) const;
+
+  /// Returns the number of poses in the visualization, which is equal to the
+  /// number of bodies in the visualization.
+  virtual int num_poses() const = 0;
+
+  /// Returns the model instance ID that was supplied to the constructor.
+  int model_instance_id() const { return model_instance_id_; }
+
+  /// Returns the name that was supplied to the constructor.
+  const std::string& name() const { return name_; }
+
+ protected:
+  /// The implementation of AddVisualizationElements() to be defined by
+  /// child classes. Child classes may assume this method is only called once,
+  /// and that starting_index() returns a valid value.
+  virtual void AddVisualizationElementsImpl(lcmt_viewer_load_robot* message)
+      = 0;
+
+  /// The implementation of AddPoses() to be defined by child classes. Child
+  /// classes may assume AddVisualizationElements() was previously called,
+  // meaning starting_index() returns a valid value.
+  virtual void AddPosesImpl(const Isometry3<T>& root_pose,
+    systems::rendering::PoseBundle<T>* pose_bundle) const = 0;
+
+  /// Returns the starting index. This value will only be valid after
+  /// AddVisualizationElements() is called.
+  int starting_index() const { return starting_index_; }
+
+ private:
+  static const int kInvalidIndex{-1};
+  const int model_instance_id_;
+  const std::string name_;
+
+  // The starting index. See documentation for set_starting_index().
+  int starting_index_{kInvalidIndex};
+};
+
+/// BoxCarVis displays a box for each vehicle.
+///
+/// Instantiated templates for the following kinds of T's are provided:
+/// - double
+///
+/// They are already available to link against in the containing library.
+///
+/// @ingroup automotive_systems
+///
+template <typename T>
+class BoxCarVis : public CarVis<T> {
+ public:
+  BoxCarVis(int model_instance_id, const std::string& name)
+      : CarVis<T>(model_instance_id, name) {}
+
+  void AddVisualizationElementsImpl(lcmt_viewer_load_robot* message) override;
+
+  void AddPosesImpl(const Isometry3<T>& root_pose,
+      systems::rendering::PoseBundle<T>* pose_bundle) const override;
+
+  int num_poses() const override { return 1; }
+};
+
+/// CarVisApplicator applies, for each vehicle instance whose pose is described
+/// in a PoseVector, the poses of all bodies belonging to the vehicle instance's
+/// visualization.
+///
+/// Prior to the start of simulation, a CarVis visualizer must be provided for
+/// each vehicle instance using AddCarVis(). The total number of poses defined
+/// among all CarVis visualizers must equal the number of visualization poses
+/// provided to the constructor.
+///
+/// This system is stateless and is direct feed-through.
+///
+/// Input port getters:
+///  - get_pose_input_port() - Contains a PoseBundle of vehicle instance poses.
+///
+/// Output port getters:
+///  - get_output_port() - Contains a PoseBundle of body poses. The bodies
+///    comprise the visualization.
+///
+/// Instantiated templates for the following kinds of T's are provided:
+/// - double
+///
+/// They are already available to link against in the containing library.
+///
+/// @ingroup automotive_systems
+template <typename T>
+class CarVisApplicator : public systems::LeafSystem<T> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(CarVisApplicator)
+
+  /// The constructor.
+  ///
+  /// @param[in] num_cars The total number of cars to visualize. Each car has
+  /// one pose.
+  ///
+  /// @param[in] num_vis_poses The total number of poses across all car
+  /// visualizations.
+  CarVisApplicator();
+  ~CarVisApplicator() override {}
+
+  /// Returns a descriptor of the input port that contains the poses of the
+  /// vehicle instances, in the form of a PoseVector.
+  const systems::InputPortDescriptor<T>& get_pose_input_port() const;
+
+  /// Returns a descriptor of the output port that contains the poses of the
+  /// bodies that represent visualizations of the vehicle instances.
+  const systems::OutputPortDescriptor<T>& get_output_port() const;
+
+  /// Adds a CarVis for a model instance. The ID of the model instance is
+  /// defined by the provided CarVis object using CarVis::model_instance_id().
+  /// A CarVis must be provided for each model instance. A std::runtime_error
+  /// exception is thrown if more than one CarVis is provided for a particular
+  /// model instance ID. CarVis objects must be added in ascending order by
+  /// model instance ID starting at zero.
+  void AddCarVis(std::unique_ptr<CarVis<T>> vis);
+
+  /// Returns an lcmt_viewer_load_robot message containing the geometries of the
+  /// bodies being visualized.
+  const lcmt_viewer_load_robot& get_load_robot_message() const {
+    return load_robot_message_;
+  }
+
+  /// Returns the number vehicles being visualized.
+  int num_cars() const { return static_cast<int>(car_visualizers_.size()); }
+
+  /// Returns the total number of poses of bodies being visualized.
+  int num_vis_poses() const;
+
+ private:
+  void DoCalcOutput(const systems::Context<T>& context,
+                    systems::SystemOutput<T>* output) const override;
+
+  // The index is the model instance ID.
+  std::vector<std::unique_ptr<const CarVis<T>>> car_visualizers_;
+  lcmt_viewer_load_robot load_robot_message_{};
+  int pose_input_port_index_{};
+  int output_port_index_{};
+};
+
+}  // namespace automotive
+}  // namespace drake
