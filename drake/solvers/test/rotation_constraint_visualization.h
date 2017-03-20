a51aa72a06f70470dce60fc96128251cb99fe62b
hongkai-dai <daihongkai@gmail.com>
2017-03-20 09:14:32 -0700

    minor change

--- drake/solvers/test/rotation_constraint_visualization.h
+++ drake/solvers/test/rotation_constraint_visualization.h
@@ -15,7 +15,8 @@ void DrawSphere(const Eigen::RowVector3d& color = Eigen::RowVector3d(0, 0.5,
  * Draw the box bmin <= x <= bmax, where the inequality is elementwise.
  * @param bmin The innermost corner of the box
  * @param bmax The outermost corner of the box
- * @param color The rgb color of the surface to be plotted. @default is [0.5, 0.2, 0.3]
+ * @param color The rgb color of the surface to be plotted.
+ * @default is [0.5, 0.2, 0.3]
  * @pre bmin >= 0 and bmax >= 0. The inequality is elementwise.
  */
 void DrawBox(const Eigen::Vector3d& bmin, const Eigen::Vector3d& bmax,
