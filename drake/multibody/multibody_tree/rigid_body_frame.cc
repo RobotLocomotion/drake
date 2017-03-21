519c7462332bd93c6374a5187ed27a1f955bf05b
amcastro-tri <alejandro.castro@tri.global>
2017-03-21 16:38:56 -0400

    Documents RigidBodyFrame

---
+++ drake/multibody/multibody_tree/rigid_body_frame.cc
@@ -0,0 +1,38 @@
+#include "drake/multibody/multibody_tree/rigid_body_frame.h"
+
+#include <memory>
+
+#include "drake/common/eigen_autodiff_types.h"
+#include "drake/common/eigen_types.h"
+#include "drake/multibody/multibody_tree/body.h"
+#include "drake/multibody/multibody_tree/multibody_tree.h"
+#include "drake/multibody/multibody_tree/multibody_tree_indexes.h"
+#include "drake/multibody/multibody_tree/rigid_body.h"
+
+namespace drake {
+namespace multibody {
+
+template <typename T>
+RigidBodyFrame<T>& RigidBodyFrame<T>::Create(
+    MultibodyTree<T>* tree, const RigidBody<T>& body, const Isometry3<T>& X_BM)
+{
+  // Notice that here we cannot use std::make_unique since constructors are made
+  // private to avoid users creating bodies by other means other than calling
+  // Create().
+  // However we can still create a unique_ptr as below where ownership is clear
+  // and an exception would call the destructor.
+  return *tree->AddMaterialFrame(
+      std::unique_ptr<RigidBodyFrame<T>>(new RigidBodyFrame<T>(body, X_BM)));
+}
+
+template <typename T>
+RigidBodyFrame<T>::RigidBodyFrame(
+    const RigidBody<T>& body, const Isometry3<T>& X_BM) :
+    MaterialFrame<T>(body.get_index()), X_BM_(X_BM) {}
+
+// Explicitly instantiates on the most common scalar types.
+template class RigidBodyFrame<double>;
+template class RigidBodyFrame<AutoDiffXd>;
+
+}  // namespace multibody
+}  // namespace drake
---
+++ drake/multibody/multibody_tree/rigid_body_frame.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "drake/common/eigen_types.h"
+#include "drake/multibody/multibody_tree/material_frame.h"
+
+namespace drake {
+namespace multibody {
+
+// Forward declarations.
+template <class T> class MultibodyTree;
+template <class T> class RigidBody;
+
+/// This class represents a frame `F` with pose `X_BF` measured and expressed in
+/// the body frame `B` of a rigid body.
+///
+/// @tparam T The scalar type. Must be a valid Eigen scalar.
+template <typename T>
+class RigidBodyFrame : public MaterialFrame<T> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(RigidBodyFrame)
+
+  /// Creates a new %RigidBodyFrame and adds it to the MultibodyTree `tree`.
+  /// The input MultibodyTree takes ownership of the newly created frame.
+  ///
+  /// @note This method invalidates the topology of the MultibodyTree `tree`.
+  /// Users must call the Compile() method on `tree` in order to re-compute and
+  /// validate its topology. See the documentation on Compile() for details.
+  ///
+  /// @param[in, out] tree The parent MultibodyTree to which this frame will be
+  ///                      added.
+  /// @returns A constant reference to the newly created rigid body frame.
+  static RigidBodyFrame<T>& Create(
+      MultibodyTree<T>* tree,
+      const RigidBody<T>& body, const Isometry3<T>& X_BM);
+
+  void Compile() final {}
+
+ private:
+  // Users are forced to create new rigid body frames with the Create() factory.
+  RigidBodyFrame(const RigidBody<T>& B, const Isometry3<T>& X_BM);
+
+  Isometry3<T> X_BM_;
+};
+
+}  // namespace multibody
+}  // namespace drake
