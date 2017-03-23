e790e1681c6a583b7a050bddac0c8490d1319c67
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-03-22 22:36:18 -0400

    Manually fix some gtest misquoting

--- drake/examples/QPInverseDynamicsForHumanoids/system/test/manipulator_inverse_dynamics_controller_test.cc
+++ drake/examples/QPInverseDynamicsForHumanoids/system/test/manipulator_inverse_dynamics_controller_test.cc
@@ -1,6 +1,6 @@
 #include "drake/examples/QPInverseDynamicsForHumanoids/system/manipulator_inverse_dynamics_controller.h"
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 #include "drake/common/drake_path.h"
 #include "drake/common/eigen_matrix_compare.h"
