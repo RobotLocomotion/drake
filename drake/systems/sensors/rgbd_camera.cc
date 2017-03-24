5153f402069f2437e7beeb6d3740069c412a5c43
Kunimatsu Hashimoto <kunimatsu.hashimoto@tri.global>
2017-03-24 10:46:24 -0400

    Add a comment

--- drake/systems/sensors/rgbd_camera.cc
+++ drake/systems/sensors/rgbd_camera.cc
@@ -471,6 +471,7 @@ void RgbdCamera::Impl::CreateRenderingWorld() {
       const int body_id = body->get_body_index();
       const auto& color = color_palette_.get_normalized_color(body_id);
       actor_for_label->GetProperty()->SetColor(color.r, color.g, color.b);
+      // This is to disable shadows and to get an object painted a single color.
       actor_for_label->GetProperty()->LightingOff();
 
       // Converts visual's pose in the world to the one in the camera coordinate
