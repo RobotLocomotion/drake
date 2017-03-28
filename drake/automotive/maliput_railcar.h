dd9992d59b24e6714687700b4c61b63d9a1e7611
Jonathan DeCastro <jonathan.decastro@tri.global>
2017-03-28 16:04:46 -0400

    Add RoadPath class and unit tests

--- drake/automotive/maliput_railcar.h
+++ drake/automotive/maliput_railcar.h
@@ -4,6 +4,7 @@
 
 #include "drake/automotive/gen/maliput_railcar_config.h"
 #include "drake/automotive/gen/maliput_railcar_state.h"
+#include "drake/automotive/lane_direction.h"
 #include "drake/automotive/maliput/api/lane.h"
 #include "drake/common/drake_copyable.h"
 #include "drake/systems/framework/leaf_system.h"
@@ -12,28 +13,6 @@
 
 namespace drake {
 namespace automotive {
-
-/// LaneDirection holds the lane that a MaliputRailcar is traversing and the
-/// direction in which it is moving. A MaliputRailcar can either travel in the
-/// increasing-`s` direction or in the decreasing-`s` direction.
-struct LaneDirection {
-  /// Default constructor.
-  LaneDirection() {}
-
-  /// A constructor that sets `with_s` to be `true`.
-  explicit LaneDirection(const maliput::api::Lane* lane_input)
-      : LaneDirection(lane_input, true) {}
-
-  /// Fully parameterized constructor.
-  LaneDirection(const maliput::api::Lane* lane_input, bool with_s_input)
-      : lane(lane_input), with_s(with_s_input) {}
-
-  const maliput::api::Lane* lane{nullptr};
-
-  /// True means that the MaliputRailcar's `s` coordinate increases when the
-  /// vehicle has positive speed. False means the opposite.
-  bool with_s{true};
-};
 
 /// MaliputRailcar models a vehicle that follows a maliput::api::Lane as if it
 /// were on rails and neglecting all physics.
---
+++ drake/automotive/road_path.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <vector>
+
+#include <Eigen/Core>
+
+#include "drake/automotive/lane_direction.h"
+#include "drake/automotive/maliput/api/road_geometry.h"
+#include "drake/common/drake_copyable.h"
+#include "drake/common/eigen_types.h"
+#include "drake/common/trajectories/piecewise_polynomial.h"
+
+namespace drake {
+namespace automotive {
+
+/// RoadPath converts a sequence of Maliput Lanes into a PiecewisePolynomial for
+/// the purpose of generating a path for a car to follow.  The path is created
+/// from the start of a user-specified initial lane and direction of travel, and
+/// proceeds in that direction until either the end of the road is reached
+/// (there exist no ongoing lanes), or the given number of specified breaks have
+/// been traversed.  The resulting path is a cubic spline that exactly matches
+/// the `s = 0` coordinate of the lanes at each specified break point.
+///
+/// This class is explicitly instantiated for the following scalar types. No
+/// other scalar types are supported.
+/// - double
+///
+/// @tparam T The vector element type, which must be a valid Eigen scalar.
+///           Only double is supported.
+template <typename T>
+class RoadPath {
+ public:
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(RoadPath)
+
+  /// Constructor.
+  /// @param road is the RoadGeometry from which a path is generated.
+  /// @param initial_lane_direction contains the initial LaneDirection.  This
+  /// must be a valid @p road Lane.
+  /// @param step_size is the size of each step (in the `s`-direction) between
+  /// knot points.
+  /// @param num_breaks are the number of breaks at which the knot points will
+  /// be evaluated.
+  RoadPath(const maliput::api::RoadGeometry& road,
+           const LaneDirection& initial_lane_direction,
+           const double step_size, int num_breaks);
+  ~RoadPath();
+
+  const PiecewisePolynomial<T>& get_path() const;
+
+  /// Computes the closest path position of an arbitrary point in Cartesian
+  /// space to the path.
+  // TODO(jadecastro): Implement this.
+  const T GetClosestPathPosition(const Vector3<T>& geo_position,
+                                 const double s_guess) const;
+
+ private:
+  // Traverse the road, starting from an initial LaneDirection, and build a
+  // cubic spline PiecewisePolynomial until 1) a given a number of segments has
+  // been traversed, or 2) the end of the road has been reached.
+  //
+  // If a BranchPoint is encountered in which there is more than one ongoing
+  // lane, the zero-index lane is always selected.
+  // TODO(jadecastro): Use DefaultBranch to decide the ongoing Lane.
+  const PiecewisePolynomial<T> MakePiecewisePolynomial(
+      const LaneDirection& initial_lane_direction, const double step_size,
+      int num_breaks);
+
+  PiecewisePolynomial<T> path_{};  // The path representing the mid-curve of the
+                                   // road.
+  PiecewisePolynomial<T> path_prime_{};         // First derivative of path_.
+  PiecewisePolynomial<T> path_double_prime_{};  // Second derivative of path_.
+};
+
+}  // namespace automotive
+}  // namespace drake
