150c9bee58d69e6e8214e9a01cd9c73d72ce3be1
Chien-Liang Fok <liang.fok@tri.global>
2017-03-30 23:20:02 -0400

    Adds MaliputRailcar to AutomotiveSimulator.

--- drake/automotive/automotive_simulator.h
+++ drake/automotive/automotive_simulator.h
@@ -3,6 +3,7 @@
 #include <map>
 #include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "drake/automotive/car_vis_applicator.h"
@@ -11,7 +12,9 @@
 #include "drake/automotive/dev/endless_road_car_to_euler_floating_joint.h"
 #include "drake/automotive/dev/infinite_circuit_road.h"
 #include "drake/automotive/gen/endless_road_car_state.h"
+#include "drake/automotive/gen/maliput_railcar_state.h"
 #include "drake/automotive/maliput/api/road_geometry.h"
+#include "drake/automotive/maliput_railcar.h"
 #include "drake/automotive/simple_car.h"
 #include "drake/automotive/simple_car_to_euler_floating_joint.h"
 #include "drake/automotive/trajectory_car.h"
@@ -113,6 +116,44 @@ class AutomotiveSimulator {
       typename EndlessRoadCar<T>::ControlType control_type,
       const std::string& channel_name);
 
+  /// Adds a MaliputRailcar to this simulation visualized as a Toyota Prius.
+  ///
+  /// @pre Start() has NOT been called.
+  ///
+  /// @pre SetRoadGeometry() was called. Otherwise, a std::runtime_error will be
+  /// thrown.
+  ///
+  /// @param model_name If this is non-empty, the car's model will be labeled
+  /// with this name. It must be unique among all cars.
+  ///
+  /// @param initial_lane_direction The MaliputRailcar's initial lane and
+  /// direction on the lane. The lane in this parameter must be part of the
+  /// maliput::api::RoadGeometry that is added via SetRoadGeometry(). Otherwise
+  /// a std::runtime_error will be thrown.
+  ///
+  /// @param params The MaliputRailcar's parameters.
+  ///
+  /// @param initial_state The MaliputRailcar's initial state.
+  ///
+  /// @return The ID of the car that was just added to the simulation.
+  int AddPriusMaliputRailcar(
+      const std::string& model_name,
+      const LaneDirection& initial_lane_direction,
+      const MaliputRailcarParams<T>& params = MaliputRailcarParams<T>(),
+      const MaliputRailcarState<T>& initial_state = MaliputRailcarState<T>());
+
+  /// Sets the acceleration command of a particular MaliputRailcar.
+  ///
+  /// @param id The ID of the MaliputRailcar. This is the ID that was returned
+  /// by the method that added the MaliputRailcar to the simulation. If no
+  /// MaliputRailcar with such an ID exists, a std::runtime_error is thrown.
+  ///
+  /// @param acceleration The acceleration command to issue to the
+  /// MaliputRailcar.
+  ///
+  /// @pre Start() has been called.
+  void SetMaliputRailcarAccelerationCommand(int id, double acceleration);
+
   /// Sets the RoadGeometry for this simulation.
   ///
   /// The provided RoadGeometry will be wrapped with in an InfiniteCircuitRoad.
@@ -213,6 +254,10 @@ class AutomotiveSimulator {
 
   void SendLoadRobotMessage(const lcmt_viewer_load_robot& message);
 
+  void InitializeSimpleCars();
+  void InitializeEndlessRoadcars();
+  void InitializeMaliputRailcars();
+
   // For both building and simulation.
   std::unique_ptr<lcm::DrakeLcmInterface> lcm_{};
   std::unique_ptr<const maliput::api::RoadGeometry> road_{};
@@ -233,6 +278,13 @@ class AutomotiveSimulator {
   // Holds the desired initial states of each SimpleCar. It is used to
   // initialize the simulation's diagram's state.
   std::map<const SimpleCar<T>*, SimpleCarState<T>> simple_car_initial_states_;
+
+  // Holds the desired initial states of each MaliputRailcar. It is used to
+  // initialize the simulation's diagram's state.
+  std::map<const MaliputRailcar<T>*, std::pair<MaliputRailcarParams<T>,
+                                               MaliputRailcarState<T>>>
+      railcar_configs_;
+
   // === End for building. ===
 
   // Adds the PoseAggregator.
@@ -256,6 +308,9 @@ class AutomotiveSimulator {
   systems::lcm::LcmPublisherSystem* lcm_publisher_{};
 
   int next_vehicle_number_{0};
+
+  // Maps a vehicle id to a pointer to the system that implements the vehicle.
+  std::map<int, systems::System<T>*> vehicles_;
 
   // For simulation.
   std::unique_ptr<systems::Diagram<T>> diagram_{};
