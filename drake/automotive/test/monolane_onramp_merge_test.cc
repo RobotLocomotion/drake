e51a84d82913a952c17b743040ed557ef27b6098
Jon <jonathan.decastro@tri.global>
2017-04-03 16:33:49 -0400

    Fix default branch

--- drake/automotive/test/monolane_onramp_merge_test.cc
+++ drake/automotive/test/monolane_onramp_merge_test.cc
@@ -8,7 +8,18 @@ namespace drake {
 namespace automotive {
 namespace {
 
+namespace api = maliput::api;
 namespace mono = maliput::monolane;
+
+const api::Lane* GetLaneById(const api::RoadGeometry& rg,
+                              const std::string& lane_id) {
+  for (int i = 0; i < rg.num_junctions(); ++i) {
+    if (rg.junction(i)->id().id == lane_id) {
+      return rg.junction(i)->segment(0)->lane(0);
+    }
+  }
+  throw std::runtime_error("No matching junction name in the road network");
+}
 
 // Tests the soundness of the MonolaneOnrampMerge example generator.
 GTEST_TEST(MonolaneOnrampMergeTest, TestDefaultAndNonDefaultAttributes) {
@@ -29,7 +40,25 @@ GTEST_TEST(MonolaneOnrampMergeTest, TestDefaultAndNonDefaultAttributes) {
       rg->junction(0)->segment(0)->lane(0)->driveable_bounds(kSPosition).r_max);
 
   EXPECT_EQ(rg->id().id, "monolane-merge-example");
-  EXPECT_EQ(rg->num_junctions(), 8);
+  EXPECT_EQ(rg->num_junctions(), 9);
+
+  // Verify that there's only one ongoing branch from the `onramp1` lane, and
+  // that it is lane `post0`.
+  const api::Lane* onramp1 = GetLaneById(*rg, "j:onramp1");
+  EXPECT_NO_THROW(onramp1->GetOngoingBranches(api::LaneEnd::kStart));
+  const api::LaneEndSet* lanes_beyond_onramp =
+      onramp1->GetOngoingBranches(api::LaneEnd::kStart);
+  EXPECT_EQ(1, lanes_beyond_onramp->size());
+  EXPECT_EQ("l:post0", lanes_beyond_onramp->get(0).lane->id().id);
+
+  // Verify that there's only one ongoing branch from the `pre0` lane, and
+  // that it is also lane `post0`.
+  const api::Lane* pre0 = GetLaneById(*rg, "j:pre0");
+  EXPECT_NO_THROW(pre0->GetOngoingBranches(api::LaneEnd::kStart));
+  const api::LaneEndSet* lanes_beyond_pre0 =
+      pre0->GetOngoingBranches(api::LaneEnd::kStart);
+  EXPECT_EQ(1, lanes_beyond_pre0->size());
+  EXPECT_EQ("l:post0", lanes_beyond_pre0->get(0).lane->id().id);
 
   // Initialize non-default road characteristics.
   const double kLaneWidth = 6.3;
@@ -53,7 +82,7 @@ GTEST_TEST(MonolaneOnrampMergeTest, TestDefaultAndNonDefaultAttributes) {
                                       .r_max);
 
   EXPECT_EQ(new_rg->id().id, "monolane-merge-example");
-  EXPECT_EQ(new_rg->num_junctions(), 8);
+  EXPECT_EQ(new_rg->num_junctions(), 9);
 }
 
 }  // namespace
