a75ee51bd1a7509758dbac40005409751a5e9e53
David German <david.german@tri.global>
2017-04-05 16:00:11 -0400

    Replace T=int with T=double in various framework tests.
    Fixes #4658.

--- drake/systems/framework/test/value_test.cc
+++ drake/systems/framework/test/value_test.cc
@@ -16,7 +16,7 @@ namespace drake {
 namespace systems {
 namespace {
 
-using MyVector2i = MyVector<2, int>;
+using MyVector2d = MyVector<2, double>;
 
 struct NoDefaultCtor {
   explicit NoDefaultCtor(int i) : data{i} {}
@@ -199,43 +199,43 @@ GTEST_TEST(ValueTest, SubclassOfValueSurvivesClone) {
 }
 
 GTEST_TEST(VectorValueTest, Access) {
-  VectorValue<int> value(BasicVector<int>::Make({1, 2, 3}));
+  VectorValue<double> value(BasicVector<double>::Make({1, 2, 3}));
   EXPECT_EQ(1, value.get_value()->get_value().x());
   EXPECT_EQ(2, value.get_value()->get_value().y());
   EXPECT_EQ(3, value.get_value()->get_value().z());
 }
 
 GTEST_TEST(VectorValueTest, CopyConstructor) {
-  VectorValue<int> value(BasicVector<int>::Make({1, 2, 3}));
+  VectorValue<double> value(BasicVector<double>::Make({1, 2, 3}));
 
-  VectorValue<int> other_value(value);
+  VectorValue<double> other_value(value);
   EXPECT_EQ(1, other_value.get_value()->get_value().x());
   EXPECT_EQ(2, other_value.get_value()->get_value().y());
   EXPECT_EQ(3, other_value.get_value()->get_value().z());
 }
 
 GTEST_TEST(VectorValueTest, CopyConstructorSubclass) {
-  VectorValue<int> value(MyVector2i::Make(1, 2));
-  VectorValue<int> other_value(value);
-  value.get_value()->set_value(Eigen::Vector2i(3, 4));
+  VectorValue<double> value(MyVector2d::Make(1, 2));
+  VectorValue<double> other_value(value);
+  value.get_value()->set_value(Eigen::Vector2d(3, 4));
 
-  const MyVector2i* const vector =
-      dynamic_cast<const MyVector2i*>(other_value.get_value());
+  const MyVector2d* const vector =
+      dynamic_cast<const MyVector2d*>(other_value.get_value());
   ASSERT_NE(vector, nullptr);
   EXPECT_EQ(vector->get_value()(0), 1);
   EXPECT_EQ(vector->get_value()(1), 2);
 }
 
 GTEST_TEST(VectorValueTest, CopyConstructorNull) {
-  VectorValue<int> value{nullptr};
+  VectorValue<double> value{nullptr};
 
-  VectorValue<int> other_value(value);
+  VectorValue<double> other_value(value);
   EXPECT_EQ(other_value.get_value(), nullptr);
 }
 
 GTEST_TEST(VectorValueTest, AssignmentOperator) {
-  VectorValue<int> value(BasicVector<int>::Make({1, 2, 3}));
-  VectorValue<int> other_value(BasicVector<int>::Make({4, 5, 6}));
+  VectorValue<double> value(BasicVector<double>::Make({1, 2, 3}));
+  VectorValue<double> other_value(BasicVector<double>::Make({4, 5, 6}));
 
   value = other_value;
   EXPECT_EQ(4, value.get_value()->get_value().x());
@@ -244,24 +244,24 @@ GTEST_TEST(VectorValueTest, AssignmentOperator) {
 }
 
 GTEST_TEST(VectorValueTest, AssignmentOperatorSubclass) {
-  VectorValue<int> value(MyVector2i::Make(1, 2));
-  VectorValue<int> other_value(BasicVector<int>::Make({5, 6}));
+  VectorValue<double> value(MyVector2d::Make(1, 2));
+  VectorValue<double> other_value(BasicVector<double>::Make({5, 6}));
   other_value = value;
-  value.get_value()->set_value(Eigen::Vector2i(3, 4));
+  value.get_value()->set_value(Eigen::Vector2d(3, 4));
 
-  const MyVector2i* const vector =
-      dynamic_cast<const MyVector2i*>(other_value.get_value());
+  const MyVector2d* const vector =
+      dynamic_cast<const MyVector2d*>(other_value.get_value());
   ASSERT_NE(vector, nullptr);
   EXPECT_EQ(vector->get_value()(0), 1);
   EXPECT_EQ(vector->get_value()(1), 2);
 }
 
 GTEST_TEST(VectorValueTest, AssignmentOperatorNull) {
-  VectorValue<int> value{nullptr};
-  VectorValue<int> other_value(BasicVector<int>::Make({4, 5, 6}));
+  VectorValue<double> value{nullptr};
+  VectorValue<double> other_value(BasicVector<double>::Make({4, 5, 6}));
 
   value = other_value;
-  other_value = VectorValue<int>{nullptr};
+  other_value = VectorValue<double>{nullptr};
   EXPECT_EQ(4, value.get_value()->get_value().x());
   EXPECT_EQ(5, value.get_value()->get_value().y());
   EXPECT_EQ(6, value.get_value()->get_value().z());
@@ -269,7 +269,7 @@ GTEST_TEST(VectorValueTest, AssignmentOperatorNull) {
 }
 
 GTEST_TEST(VectorValueTest, AssignmentOperatorSelf) {
-  VectorValue<int> value(BasicVector<int>::Make({4, 5, 6}));
+  VectorValue<double> value(BasicVector<double>::Make({4, 5, 6}));
   value = value;
   EXPECT_EQ(4, value.get_value()->get_value().x());
   EXPECT_EQ(5, value.get_value()->get_value().y());
