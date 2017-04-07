ca7d5a7f8b7ef80d108776251edb6bd4bfaeb69c
Sean Curtis <sean.curtis@tri.global>
2017-04-07 09:41:36 -0700

    Modifications to is_cloneable
    1) The big change is the definition of cloneable allows a class to return
    a unique_ptr for a *ancestor* class (and not just itself).
    2) In addition, the doxygen has been updated to reflect this and provide
     better usage instruction.
    3) Updated unit tests to reflect this.

--- drake/common/test/drake_cloneable_test.cc
+++ drake/common/test/drake_cloneable_test.cc
@@ -25,14 +25,39 @@ class Base {
   unique_ptr<Base> Clone() const { return make_unique<Base>(); }
 };
 
-// An inherited class with no copy constructor, which inherits the parent's
-// Clone method (but doesn't return the appropriate type).
-// NOT CLONEABLE!
-class InheritClone : public Base {
+GTEST_TEST(IsCloneableTest, FullyCopyable) {
+  EXPECT_TRUE(is_cloneable<Base>::value);
+}
+
+// A class with no copy constructor, but a valid Clone method.
+// Cloneable!
+class CloneOnly {
  public:
-  InheritClone() {}
-  InheritClone(const InheritClone& c) = delete;
+  CloneOnly() {}
+  CloneOnly(const CloneOnly& f) = delete;
+  unique_ptr<CloneOnly> Clone() const { return make_unique<CloneOnly>(); }
 };
+
+GTEST_TEST(IsCloneableTest, OnlyCloneable) {
+  EXPECT_TRUE(is_cloneable<CloneOnly>::value);
+}
+
+// A class with no copy constructor, but has a Clone that returns the parent
+// class pointer. The parent is representative of *any* ancestor in the
+// inheritance tree.
+// Cloneable!
+class CloneToParent : public Base {
+ public:
+  CloneToParent() {}
+  CloneToParent(const CloneToParent& c) = delete;
+  unique_ptr<Base> Clone() const {
+    return make_unique<CloneToParent>();
+  }
+};
+
+GTEST_TEST(IsCloneableTest, CloneToParentFail) {
+  EXPECT_TRUE(is_cloneable<CloneToParent>::value);
+}
 
 // A class with no copy constructor and a Clone method with the wrong return
 // type -- no explicit conversion from * to unique_ptr.
@@ -44,6 +69,10 @@ class BadClone : public Base {
   BadClone* Clone() const { return new BadClone(); }
 };
 
+GTEST_TEST(IsCloneableTest, BadCloneReturnFail) {
+  EXPECT_FALSE(is_cloneable<BadClone>::value);
+}
+
 // A class with no copy constructor and a non-const Clone method.
 // NOT CLONEABLE!
 class NonConstClone : public Base {
@@ -53,6 +82,10 @@ class NonConstClone : public Base {
   unique_ptr<NonConstClone> Clone() { return make_unique<NonConstClone>(); }
 };
 
+GTEST_TEST(IsCloneableTest, NonConstCloneFail) {
+  EXPECT_FALSE(is_cloneable<NonConstClone>::value);
+}
+
 // Class with no copy semantics (no copy constructor, no Clone)
 // NOT CLONEABLE!
 class CantCopy {
@@ -61,12 +94,20 @@ class CantCopy {
   CantCopy(const CantCopy& f) = delete;
 };
 
+GTEST_TEST(IsCloneableTest, UnCopyableFail) {
+  EXPECT_FALSE(is_cloneable<CantCopy>::value);
+}
+
 // A class with a malformed Clone method, but it has a copy constructor.
 // NOT CLONEABLE! (But copyable.)
 class BadCloneCopy {
  public:
   BadCloneCopy* Clone() const { return new BadCloneCopy(); }
 };
+
+GTEST_TEST(IsCloneableTest, CopyableWithBadCloneFail) {
+  EXPECT_FALSE(is_cloneable<BadCloneCopy>::value);
+}
 
 // A class with a copy constructor but no clone method.
 // NOT CLONEABLE! (But copyable.)
@@ -75,43 +116,6 @@ class NoClone {
   NoClone() {}
   NoClone(const NoClone& f) {}
 };
-
-// A class with no copy constructor, but a valid Clone method.
-// Cloneable!
-class CloneOnly {
- public:
-  CloneOnly() {}
-  CloneOnly(const CloneOnly& f) = delete;
-  unique_ptr<CloneOnly> Clone() const { return make_unique<CloneOnly>(); }
-};
-
-GTEST_TEST(IsCloneableTest, FullyCopyable) {
-  EXPECT_TRUE(is_cloneable<Base>::value);
-}
-
-GTEST_TEST(IsCloneableTest, OnlyCloneable) {
-  EXPECT_TRUE(is_cloneable<CloneOnly>::value);
-}
-
-GTEST_TEST(IsCloneableTest, InheritCloneFail) {
-  EXPECT_FALSE(is_cloneable<InheritClone>::value);
-}
-
-GTEST_TEST(IsCloneableTest, BadCloneReturnFail) {
-  EXPECT_FALSE(is_cloneable<BadClone>::value);
-}
-
-GTEST_TEST(IsCloneableTest, NonConstCloneFail) {
-  EXPECT_FALSE(is_cloneable<NonConstClone>::value);
-}
-
-GTEST_TEST(IsCloneableTest, UnCopyableFail) {
-  EXPECT_FALSE(is_cloneable<CantCopy>::value);
-}
-
-GTEST_TEST(IsCloneableTest, CopyableWithBadCloneFail) {
-  EXPECT_FALSE(is_cloneable<BadCloneCopy>::value);
-}
 
 GTEST_TEST(IsCloneableTest, CopyableButNoCloneFail) {
   EXPECT_FALSE(is_cloneable<NoClone>::value);
