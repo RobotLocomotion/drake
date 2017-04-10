9c1c72d7dae18f9f64c1648104474c447fcdadc8
Jonathan DeCastro <jonathan.decastro@tri.global>
2017-04-10 15:01:06 -0400

    Remove DrivingCommand input and replace DrivingCommand output with BasicVector

--- drake/automotive/pure_pursuit.cc
+++ drake/automotive/pure_pursuit.cc
@@ -14,17 +14,16 @@ namespace automotive {
 
 using maliput::api::GeoPosition;
 using maliput::api::Lane;
-using maliput::api::LaneEnd;
 using maliput::api::LanePosition;
 using maliput::api::RoadGeometry;
-using maliput::api::RoadPosition;
 using systems::rendering::PoseVector;
 
 template <typename T>
 T PurePursuit<T>::Evaluate(const PurePursuitParams<T>& pp_params,
-                                 const SimpleCarParams<T>& car_params,
-                                 bool with_s, const RoadGeometry& road,
-                                 const PoseVector<T>& pose) {
+                           const SimpleCarParams<T>& car_params,
+                           const LaneDirection& lane_direction,
+                           const RoadGeometry& road,
+                           const PoseVector<T>& pose) {
   DRAKE_DEMAND(pp_params.IsValid());
   DRAKE_DEMAND(car_params.IsValid());
 
@@ -34,7 +33,7 @@ T PurePursuit<T>::Evaluate(const PurePursuitParams<T>& pp_params,
   using std::sin;
 
   const GeoPosition goal_position =
-      ComputeGoalPoint(pp_params.s_lookahead(), with_s, road, pose);
+      ComputeGoalPoint(pp_params.s_lookahead(), lane_direction, road, pose);
 
   const T x = pose.get_translation().translation().x();
   const T y = pose.get_translation().translation().y();
@@ -49,18 +48,20 @@ T PurePursuit<T>::Evaluate(const PurePursuitParams<T>& pp_params,
 }
 
 template <typename T>
-const GeoPosition PurePursuit<T>::ComputeGoalPoint(const T& s_lookahead,
-                                                   bool with_s,
-                                                   const RoadGeometry& road,
-                                                   const PoseVector<T>& pose) {
-  const RoadPosition position =
-      pose_selector::CalcRoadPosition(road, pose.get_isometry());
+const GeoPosition PurePursuit<T>::ComputeGoalPoint(
+    const T& s_lookahead, const LaneDirection& lane_direction,
+    const RoadGeometry& road, const PoseVector<T>& pose) {
+  const maliput::api::Lane* const lane = lane_direction.lane;
+  const bool with_s = lane_direction.with_s;
+  const LanePosition position =
+      lane->ToLanePosition({pose.get_isometry().translation().x(),
+              pose.get_isometry().translation().y(),
+              pose.get_isometry().translation().z()}, nullptr, nullptr);
   const T s_new =
-      cond(with_s, position.pos.s + s_lookahead, position.pos.s - s_lookahead);
-  const T s_goal = math::saturate(s_new, 0., position.lane->length());
-  // TODO(jadecastro): Add support for locating goal points located in ongoing
-  // lanes.
-  return position.lane->ToGeoPosition(LanePosition(s_goal, 0., 0.));
+      cond(with_s, position.s + s_lookahead, position.s - s_lookahead);
+  const T s_goal = math::saturate(s_new, 0., lane->length());
+  // TODO(jadecastro): Add support for locating goal points in ongoing lanes.
+  return lane->ToGeoPosition({s_goal, 0., position.h});
 }
 
 // These instantiations must match the API documentation in pure_pursuit.h.
