06766248af485d1b92d42cca5f0f0ab4d00e1ef4
Chien-Liang Fok <liang.fok@tri.global>
2017-04-11 09:58:43 -0400

    Fixed TODO comment in lcm_publisher_system.cc. (#5790)

--- drake/systems/lcm/lcm_publisher_system.cc
+++ drake/systems/lcm/lcm_publisher_system.cc
@@ -21,8 +21,8 @@ const int kPortIndex = 0;
 // TODO(jwnimmer-tri) The "serializer xor translator" disjoint implementations
 // within the method bodies below are not ideal, because of the code smell, and
 // because it is likely confusing for users.  We should take further steps to
-// make the Value<LcmMessage> port the primary output port, and find a better
-// phrasing for the vector-valued output port for users.  For now though, this
+// make the Value<LcmMessage> port the primary input port, and find a better
+// phrasing for the vector-valued input port for users.  For now though, this
 // implementation serves as a transition point where we don't have to rewrite
 // the old code yet, but still can supply the AbstractValue port for new code.
 
