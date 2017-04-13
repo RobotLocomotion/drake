8d1b295ad26a212391f59e94aea42dde65643ca7
Mmanu Chaturvedi <mmanu.chaturvedi@kitware.com>
2017-04-13 19:09:19 -0400

    Updating valgrind.sh file for Bazel

--- tools/valgrind.sh
+++ tools/valgrind.sh
@@ -7,4 +7,8 @@ valgrind \
     --leak-check=full \
     --suppressions="tools/valgrind.supp" \
     --error-exitcode=1 \
+    --trace-children=yes \
+    --track-origins=yes \
+    --show-leak-kinds=definite,possible \
+    --gen-suppressions=all \
     "$@"
