3e2fc0e4ea3321825a1449a09bbb523052c75d50
hongkai-dai <daihongkai@gmail.com>
2017-04-16 12:34:51 -0700

    Address Sherm's comments

--- drake/math/evenly_distributed_pts_on_sphere.h
+++ drake/math/evenly_distributed_pts_on_sphere.h
@@ -4,10 +4,10 @@
 namespace drake {
 namespace math {
 /**
- * Deterministically generate approximate evenly distributed points on a unit
+ * Deterministically generates approximate evenly distributed points on a unit
  * sphere. This method uses Fibonacci number. For the detailed math, please
  * refer to http://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere
- * This algorithm works good for large number of samples ( > 50).
+ * This algorithm works well for a large number of samples ( > 50).
  * @param num_samples The number of samples we want on the unit sphere.
  * @return The generated points.
  * @pre num_samples >= 1. Throw std::runtime_error if num_samples < 1
