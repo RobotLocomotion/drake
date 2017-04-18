10206013f3240a0e5efb10cd8a5b507944f359db
Carlos Aguero <caguero@osrfoundation.org>
2017-04-18 10:29:36 -0700

    Add comments to clarify tests.

--- drake/automotive/maliput/rndf/test/ignition_math_test.cc
+++ drake/automotive/maliput/rndf/test/ignition_math_test.cc
@@ -5,6 +5,8 @@ namespace drake {
 namespace maliput {
 namespace rndf {
 
+// Exercise the Bazel infrastructure and ensure that Ignition Math can be
+// built and linked.
 GTEST_TEST(IgnitionMathTest, Test) {
   ignition::math::Angle angle;
   EXPECT_TRUE(ignition::math::equal(0.0, angle.Radian()));
--- drake/automotive/maliput/rndf/test/ignition_rndf_test.cc
+++ drake/automotive/maliput/rndf/test/ignition_rndf_test.cc
@@ -5,6 +5,8 @@ namespace drake {
 namespace maliput {
 namespace rndf {
 
+// Exercise the Bazel infrastructure and ensure that Ignition RNDF can be
+// built and linked.
 GTEST_TEST(IgnitionRNDFTest, Test) {
   ignition::rndf::RNDF rndfLoader;
   EXPECT_FALSE(rndfLoader.Valid());
