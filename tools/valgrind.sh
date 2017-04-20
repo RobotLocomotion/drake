4d2115dce7215ff105a0192f5e383e3aa66b651b
Mmanu Chaturvedi <mmanu.chaturvedi@kitware.com>
2017-04-20 18:39:29 -0400

    Removing Shell Tests From Memchecks

--- tools/valgrind.sh
+++ tools/valgrind.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # Disable GTest death test use of clone() because it confuses valgrind.
+set -x;
 export GTEST_DEATH_TEST_USE_FORK=1
 
+# Only run the test when --no_memcheck is not passed
+for args in "$@"; do
+    if [[ "$args" == "--no_memcheck" ]]; then
+        exit
+    fi
+done
+
 valgrind \
-    --leak-check=full \
-    --suppressions="tools/valgrind.supp" \
-    --error-exitcode=1 \
-    --trace-children=yes \
-    --track-origins=yes \
-    --show-leak-kinds=definite,possible \
-    "$@"
+  --leak-check=full \
+  --suppressions="tools/valgrind.supp" \
+  --error-exitcode=1 \
+  --trace-children=yes \
+  --track-origins=yes \
+  --show-leak-kinds=definite,possible \
+  "$@";
