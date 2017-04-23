99c922f36106c780208b933fd139defe1b76df56
Matt Marjanović <maddog@tri.global>
2017-04-22 21:08:10 -0700

    Control precision/offset of vertex data in maliput OBJ generation. (#5915)
    * Automatically adjust the numerical precision of vertices emitted to disk as a
    function of RoadGeometry::linear_tolerance().
    
    * Provide an "origin" features so that OBJ vertices can be shifted back
    into the realm of single-precision floats.
    
    * Address PR platform review comments.
    
    * Address PR review comments:
     * fix the fixed-point-precision calculation (which was inexplicably wrong).
     * add unit-tests specifically for GenerateObj(), which exercises the 'origin'
       feature explicitly.
    
    Also, move a regression test on GenerateObj() from generate_urdf_test.cc into
    the more appropriate generate_obj_test.cc.
    
    * More PR review tweaks.

--- drake/automotive/maliput/utility/test/generate_urdf_test.cc
+++ drake/automotive/maliput/utility/test/generate_urdf_test.cc
@@ -110,59 +110,6 @@ TEST_F(GenerateUrdfTest, AtLeastRunIt) {
   EXPECT_TRUE(spruce::file::remove(expected_mtl));
 }
 
-
-// Oblique regression test for tripping of an assertion in DrawLaneArrow()
-// during GenerateObj().  See #5745.
-TEST_F(GenerateUrdfTest, DontTickleDrawLaneArrowAssert) {
-  std::string dut_yaml = R"R(# -*- yaml -*-
----
-# distances are meters; angles are degrees.
-maliput_monolane_builder:
-  id: city_1
-  lane_bounds: [-2, 2]
-  driveable_bounds: [-4, 4]
-  position_precision: 0.01
-  orientation_precision: 0.5
-  points:
-    street_9_1_3:
-      xypoint: [92.92893218813452, 307.0710678118655, -45.0]
-      zpoint: [0.0, 0, 0, 0]
-    street_9_1_4:
-      xypoint: [95.05025253169417, 304.9497474683058, -45.0]
-      zpoint: [0.0, 0, 0, 0]
-  connections:
-    street_9_1_3-street_9_1_4: {start: points.street_9_1_3, length: 3.000000000000014,
-      explicit_end: points.street_9_1_4}
-  groups: {}
-)R";
-
-  const std::unique_ptr<const api::RoadGeometry> dut = mono::Load(dut_yaml);
-
-  GenerateUrdfFile(dut.get(), directory_.getStr(), kJunkBasename,
-                   ObjFeatures());
-  // We expect to get three files out of this.
-
-  spruce::path expected_urdf(directory_);
-  expected_urdf.append(kJunkBasename + ".urdf");
-  EXPECT_TRUE(expected_urdf.isFile());
-
-  spruce::path expected_obj(directory_);
-  expected_obj.append(kJunkBasename + ".obj");
-  EXPECT_TRUE(expected_obj.isFile());
-
-  spruce::path expected_mtl(directory_);
-  expected_mtl.append(kJunkBasename + ".mtl");
-  EXPECT_TRUE(expected_mtl.isFile());
-
-  // spruce is retarded: it has no functionality for reading/walking a
-  // directory, so we have to delete all our files individually here where
-  // we know the names.
-  EXPECT_TRUE(spruce::file::remove(expected_urdf));
-  EXPECT_TRUE(spruce::file::remove(expected_obj));
-  EXPECT_TRUE(spruce::file::remove(expected_mtl));
-}
-
-
 }  // namespace utility
 }  // namespace maliput
 }  // namespace drake
