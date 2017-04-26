599d735884731f11074066418394026f3610ecee
Eric Cousineau <eric.cousineau@tri.global>
2017-04-25 22:29:15 -0400

    Add explicit check for is_convertible_workaround for GCC 4.9.[0-3].

--- drake/solvers/test/cost_test.cc
+++ drake/solvers/test/cost_test.cc
@@ -1,5 +1,6 @@
 #include "drake/solvers/cost.h"
 
+#include <iostream>
 #include <memory>
 
 #include <gtest/gtest.h>
@@ -12,19 +13,55 @@
 #include "drake/solvers/decision_variable.h"
 #include "drake/solvers/test/generic_trivial_costs.h"
 
+using std::cout;
+using std::endl;
 using std::make_shared;
 using std::make_unique;
+using std::shared_ptr;
+using std::unique_ptr;
 using std::vector;
 
 using Eigen::Ref;
 using drake::Vector1d;
 using Eigen::Vector2d;
 using Eigen::VectorXd;
+using drake::solvers::detail::is_convertible_workaround;
 using drake::solvers::test::GenericTrivialCost2;
 
 namespace drake {
 namespace solvers {
 namespace {
+
+// Check for the failure of libstdc++ 4.9's std::is_convertible if used to
+// check From = std::unique_ptr<T>, To = std::shared_ptr<U>
+template <typename From, typename To>
+struct check_ptr_convertible {
+  typedef std::unique_ptr<From> FromPtr;
+  typedef std::shared_ptr<To> ToPtr;
+  // static constexpr bool std_value =
+  //    std::is_convertible<FromPtr, ToPtr>::value;
+  static constexpr bool workaround_value =
+      is_convertible_workaround<FromPtr, ToPtr>::value;
+};
+
+struct A {};
+struct B {};
+struct C : B {};
+
+GTEST_TEST(testCost, testIsConvertibleWorkaround) {
+  EXPECT_TRUE((is_convertible_workaround<C*, B*>::value));
+  EXPECT_TRUE((is_convertible_workaround<shared_ptr<C>, shared_ptr<B>>::value));
+  EXPECT_TRUE((is_convertible_workaround<unique_ptr<C>, unique_ptr<B>>::value));
+  EXPECT_TRUE((is_convertible_workaround<Binding<LinearConstraint>,
+                                         Binding<Constraint>>::value));
+  EXPECT_FALSE((is_convertible_workaround<Binding<LinearConstraint>,
+                                          Binding<Cost>>::value));
+
+  // TODO(eric.cousineau): Determine exact conditions for failure in GCC.
+  // Difficult to pinpoint in a multi-platform fashion via __GLIBCXX__ or
+  // __GNUC_* version macros
+  EXPECT_FALSE((check_ptr_convertible<A, B>::workaround_value));
+}
 
 // For a given Constraint, return the equivalent Cost type
 template <typename C>
