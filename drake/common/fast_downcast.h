b933a14f4e19f98674d2296e394841aa4a66bee3
amcastro-tri <alejandro.castro@tri.global>
2017-04-28 11:39:48 -0400

    Adds doc

--- drake/common/fast_downcast.h
+++ drake/common/fast_downcast.h
@@ -2,24 +2,32 @@
 
 #include <type_traits>
 
-/// @file
-/// Provides Drake's assertion implementation.  This is intended to be used
-/// both within Drake and by other software.  Drake's asserts can be armed
-/// and disarmed independently from the system-wide asserts.
-
-
-//#ifdef DRAKE_ASSERT_IS_ARMED
-//#ifdef DRAKE_ASSERT_IS_DISARMED
-
 namespace drake {
-namespace detail {
 
-}  // namespace detail
-
-/// The doc here.
+/// Attempts to donwcast a reference of type `Base` to the requested type
+/// `Derived`. `Derived` must be a reference and can be more cv-qualified than
+/// `from` (i.e. this method can be used to add constness.)
+/// fast_downcast behaves as `dynamic_cast` in Debug builds (i.e. NDEBUG **is
+/// not** defined) while it behaves like `static_cast` in Release builds (i.e.
+/// NDEBUG **is** defined).
+/// It is meant to be used in places where perfomance is paramount and we wish
+/// to avoid the cost of the runtime check, but it is only safe if the program
+/// can guarantee (through some other logic) that the `from` object is
+/// definitely of type `Derived`.
+/// If the cast is successful, it returns the orignal `from` object downcasted
+/// to type `Derived`. If the cast fails, it throws an exception that matches a
+/// handler of type std::bad_cast.
 ///
-// Reference version
+/// @param from The object of type `Base` to be downcasted to type `Derived`.
+/// @returns The `from` object downcasted to type `Derived` when successful.
+///
+/// @throws A std::bad_cast if `from` is cannot be downcasted to `Derived`.
+///
+/// @tparam Base A complete class type where to cast from.
+/// @tparam Derived A reference to a complete class type.
 template<class Derived, class Base>
+// NOLINTNEXTLINE(runtime/references): we allow here to cast from either const
+// NOLINTNEXTLINE(runtime/references): or non-const input references.
 static Derived fast_downcast(Base& from) {
   static_assert(std::is_convertible<Derived, Base&>::value,
                 "The requested type is not a derived class of the input "
@@ -31,7 +39,26 @@ static Derived fast_downcast(Base& from) {
 #endif
 }
 
-// Pointer version.
+/// Attempts to donwcast a pointer of type `Base` to the requested type
+/// `Derived`. `Derived` must be a pointer and can be more cv-qualified than
+/// `from` (i.e. this method can be used to add constness.)
+/// fast_downcast behaves as `dynamic_cast` in Debug builds (i.e. NDEBUG **is
+/// not** defined) while it behaves like `static_cast` in Release builds (i.e.
+/// NDEBUG **is** defined).
+/// It is meant to be used in places where perfomance is paramount and we wish
+/// to avoid the cost of the runtime check, but it is only safe if the program
+/// can guarantee (through some other logic) that the `from` object is
+/// definitely of type `Derived`.
+/// If the cast is successful, it returns the orignal `from` pointer downcasted
+/// to type `Derived`. If the cast fails, it returns `nullptr`.
+///
+/// @param from Pointer to an object of type `Base` to be downcasted to type
+///             `Derived` (which must be a pointer type.)
+/// @returns The `from` poiter downcasted to type `Derived` when successful and
+///          `nullptr` when unsuccessful.
+///
+/// @tparam Base A complete class type where to cast from.
+/// @tparam Derived A pointer to a complete class type.
 template<class Derived, class Base>
 static Derived fast_downcast(Base* from) {
   static_assert(std::is_convertible<Derived, Base*>::value,
