c153c390ab05452d29143873d130d998cd11ec5c
hongkai-dai <daihongkai@gmail.com>
2017-04-30 22:12:50 -0700

    test the feasible postures

--- drake/multibody/dev/test/global_inverse_kinematics_feasible_posture_test.cc
+++ drake/multibody/dev/test/global_inverse_kinematics_feasible_posture_test.cc
@@ -12,13 +12,15 @@ TEST_F(KukaTest, FeasiblePostureTest) {
   // kinematics. global IK should always think the problem is feasible.
 
   // First sample some postures, within the joint limits.
-  const int kNumSamplePerJoint = 2;
+  const int kNumSamplePerJoint = 3;
   Eigen::Matrix<double, 7, kNumSamplePerJoint> q_grid;
   for (int i = 0; i < 7; ++i) {
     q_grid(i, 0) = rigid_body_tree_->joint_limit_min(i) * 0.95
         + rigid_body_tree_->joint_limit_max(i) * 0.05;
     q_grid(i, 1) = rigid_body_tree_->joint_limit_min(i) * 0.5
         + rigid_body_tree_->joint_limit_max(i) * 0.5;
+    q_grid(i, 2) = rigid_body_tree_->joint_limit_min(i) * 0.05
+        + rigid_body_tree_->joint_limit_max(i) * 0.95;
   }
   KinematicsCache<double> cache = rigid_body_tree_->CreateKinematicsCache();
 
@@ -40,12 +42,30 @@ TEST_F(KukaTest, FeasiblePostureTest) {
   }
 
   // Now generate the feasible posture q
-  for (int i = 0; i < static_cast<int>(std::pow<int>(kNumSamplePerJoint, 7));
-       ++i) {
+  // q(i) = q_grid(i, joint_grid_idx(i));
+  Eigen::Matrix<int, 7, 1> joint_grid_idx;
+  joint_grid_idx.setZero();
+  int num_q = std::pow<int>(kNumSamplePerJoint, 7);
+  int q_count = 0;
+  while (q_count < num_q) {
     Eigen::Matrix<double, 7, 1> q;
     for (int joint = 0; joint < 7; ++joint) {
-      q(joint) = (1 << joint) && i ? q_grid(joint, 0) : q_grid(joint, 1);
+      q(joint) = q_grid(joint, joint_grid_idx(joint));
     }
+    // Now increment joint_grid_idx;
+    for (int joint = 0; joint < 7; ++joint) {
+      ++joint_grid_idx(joint);
+      if (joint_grid_idx(joint) >= kNumSamplePerJoint) {
+        joint_grid_idx(joint) -= kNumSamplePerJoint;
+        if (joint < 6) {
+          ++joint_grid_idx(joint + 1);
+        }
+      }
+    }
+    ++q_count;
+
+    // Now compute the forward kinematics for posture q, and then fix the body
+    // position and orientation of the global IK to the body pose of q.
     cache.initialize(q);
     rigid_body_tree_->doKinematics(cache);
     for (int body = 1; body < rigid_body_tree_->get_num_bodies(); ++body) {
@@ -54,21 +74,21 @@ TEST_F(KukaTest, FeasiblePostureTest) {
                                                      rigid_body_tree_->get_body(
                                                          body));
       const Eigen::Vector3d
-          pos_lb = body_pose.translation() - 0.01 * Eigen::Vector3d::Ones();
+          pos_lb = body_pose.translation();
       const Eigen::Vector3d
-          pos_ub = body_pose.translation() + 0.01 * Eigen::Vector3d::Ones();
+          pos_ub = body_pose.translation();
       body_position_constraint[body - 1].constraint()->UpdateLowerBound(pos_lb);
       body_position_constraint[body - 1].constraint()->UpdateUpperBound(pos_ub);
       Eigen::Matrix<double, 9, 1> rotmat_lb_flat;
       Eigen::Matrix<double, 9, 1> rotmat_ub_flat;
       rotmat_lb_flat
-          << body_pose.linear().col(0) - 0.01 * Eigen::Vector3d::Ones(),
-          body_pose.linear().col(1) - 0.01 * Eigen::Vector3d::Ones(),
-          body_pose.linear().col(2) - 0.01 * Eigen::Vector3d::Ones();
+          << body_pose.linear().col(0),
+          body_pose.linear().col(1),
+          body_pose.linear().col(2);
       rotmat_ub_flat
-          << body_pose.linear().col(0) + 0.01 * Eigen::Vector3d::Ones(),
-          body_pose.linear().col(1) + 0.01 * Eigen::Vector3d::Ones(),
-          body_pose.linear().col(2) + 0.01 * Eigen::Vector3d::Ones();
+          << body_pose.linear().col(0),
+          body_pose.linear().col(1),
+          body_pose.linear().col(2);
       body_orientation_constraint[body - 1].constraint()->UpdateLowerBound(
           rotmat_lb_flat);
       body_orientation_constraint[body - 1].constraint()->UpdateUpperBound(
