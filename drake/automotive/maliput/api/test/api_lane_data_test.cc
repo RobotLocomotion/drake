249072ffd915f91bb2453574f83b5729b818bbcc
Matt Marjanovic <maddog@tri.global>
2017-05-01 23:29:14 -0700

    Make unit-tests significantly more compact (through the mighty power of the CPP macro).

--- drake/automotive/maliput/api/test/api_lane_data_test.cc
+++ drake/automotive/maliput/api/test/api_lane_data_test.cc
@@ -2,40 +2,39 @@
 
 #include <gtest/gtest.h>
 
+#include "drake/common/eigen_matrix_compare.h"
+
 namespace drake {
 namespace maliput {
 namespace api {
 
+#define CHECK_ALL_LANE_ACCESSORS(dut, _s, _r, _h)                \
+  do {                                                           \
+    EXPECT_EQ(dut.s(), _s);                                      \
+    EXPECT_EQ(dut.r(), _r);                                      \
+    EXPECT_EQ(dut.h(), _h);                                      \
+    EXPECT_TRUE(CompareMatrices(dut.srh(), Vector3<double>(_s, _r, _h))); \
+  } while (0)
+
+
 GTEST_TEST(LanePositionTest, DefaultConstructor) {
   // Check that default constructor obeys its contract.
   LanePosition dut;
-
-  EXPECT_EQ(dut.s(), 0.);
-  EXPECT_EQ(dut.r(), 0.);
-  EXPECT_EQ(dut.h(), 0.);
-  EXPECT_EQ(dut.srh(), Vector3<double>::Zero());
+  CHECK_ALL_LANE_ACCESSORS(dut, 0., 0., 0.);
 }
 
 
 GTEST_TEST(LanePositionTest, ParameterizedConstructor) {
   // Check the fully-parameterized constructor.
   LanePosition dut(23., 75., 0.567);
-
-  EXPECT_EQ(dut.s(), 23.);
-  EXPECT_EQ(dut.r(), 75.);
-  EXPECT_EQ(dut.h(), 0.567);
-  EXPECT_EQ(dut.srh(), Vector3<double>(23., 75., 0.567));
+  CHECK_ALL_LANE_ACCESSORS(dut, 23., 75., 0.567);
 }
 
 
 GTEST_TEST(LanePositionTest, ConstructionFromVector) {
   // Check the conversion-construction from a 3-vector.
   LanePosition dut = LanePosition::FromSrh(Vector3<double>(23., 75., 0.567));
-
-  EXPECT_EQ(dut.s(), 23.);
-  EXPECT_EQ(dut.r(), 75.);
-  EXPECT_EQ(dut.h(), 0.567);
-  EXPECT_EQ(dut.srh(), Vector3<double>(23., 75., 0.567));
+  CHECK_ALL_LANE_ACCESSORS(dut, 23., 75., 0.567);
 }
 
 
@@ -43,11 +42,7 @@ GTEST_TEST(LanePositionTest, VectorSetter) {
   // Check the vector-based setter.
   LanePosition dut(23., 75., 0.567);
   dut.set_srh(Vector3<double>(9., 7., 8.));
-
-  EXPECT_EQ(dut.s(), 9.);
-  EXPECT_EQ(dut.r(), 7.);
-  EXPECT_EQ(dut.h(), 8.);
-  EXPECT_EQ(dut.srh(), Vector3<double>(9., 7., 8.));
+  CHECK_ALL_LANE_ACCESSORS(dut, 9., 7., 8.);
 }
 
 
@@ -56,16 +51,13 @@ GTEST_TEST(LanePositionTest, ComponentSetters) {
   LanePosition dut(0.1, 0.2, 0.3);
 
   dut.set_s(99.);
-  EXPECT_EQ(dut.s(), 99.);
-  EXPECT_EQ(dut.srh(), Vector3<double>(99., 0.2, 0.3));
+  CHECK_ALL_LANE_ACCESSORS(dut, 99., 0.2, 0.3);
 
   dut.set_r(2.3);
-  EXPECT_EQ(dut.r(), 2.3);
-  EXPECT_EQ(dut.srh(), Vector3<double>(99., 2.3, 0.3));
+  CHECK_ALL_LANE_ACCESSORS(dut, 99., 2.3, 0.3);
 
   dut.set_h(42.);
-  EXPECT_EQ(dut.h(), 42.);
-  EXPECT_EQ(dut.srh(), Vector3<double>(99., 2.3, 42.));
+  CHECK_ALL_LANE_ACCESSORS(dut, 99., 2.3, 42.);
 }
 
 
