9e90502da12c616d4f7f1c2465b8179b11b9dbd2
Siyuan Feng <siyuan.feng@tri.global>
2017-05-10 17:01:23 -0400

    humanoids stuff

--- drake/examples/QPInverseDynamicsForHumanoids/lcm_utils.h
+++ drake/examples/QPInverseDynamicsForHumanoids/lcm_utils.h
@@ -5,7 +5,6 @@
 #include <vector>
 
 #include "bot_core/atlas_command_t.hpp"
-#include "bot_core/robot_state_t.hpp"
 
 #include "drake/common/eigen_types.h"
 #include "drake/examples/QPInverseDynamicsForHumanoids/qp_controller_common.h"
@@ -78,56 +77,6 @@ void DecodeResolvedContact(const RigidBodyTree<double>& robot,
                            ResolvedContact* contact);
 void EncodeResolvedContact(const ResolvedContact& contact,
                            lcmt_resolved_contact* msg);
-
-// TODO(siyuan.feng) Replace these with Twan's similar Encode / Decode methods.
-/**
- * Make a bot_core::robot_state_t lcm message based on given information.
- * The current implementation assumes:
- * 1. \p q and \p qd have the same dimension.
- * 2. Has a RPY floating base joint.
- * 3. All DOF are actuated except the floating base.
- *
- * @param act_joint_names List of ACTUATED joint names (excluding floating base)
- * @param time Time
- * @param q generalized position (including floating base)
- * @param qd generalized velocity (including floating base)
- * @param joint_torque Actuated joint torques
- * @param l_foot_wrench Left foot wrench in the sensor frame
- * @param r_foot_wrench Right foot wrench in the sensor frame
- * @param msg Output lcm message
- */
-void EncodeRobotStateLcmMsg(const std::vector<std::string>& act_joint_names,
-                            double time, const VectorX<double>& q,
-                            const VectorX<double>& qd,
-                            const VectorX<double>& joint_torque,
-                            const Vector6<double>& l_foot_wrench,
-                            const Vector6<double>& r_foot_wrench,
-                            bot_core::robot_state_t* msg);
-
-/**
- * Decode state related information from a bot_core::robot_state_t message.
- * \p msg can contain extra joint information, but we will only decode
- * joints that are listed in \p q_name_to_index.
- * The current implementation assumes:
- * 1. \p q and \p qd have the same dimension.
- * 2. Has a RPY floating base joint.
- * 3. All DOF are actuated except the floating base.
- *
- * @param msg Lcm message
- * @param q_name_to_index A map from coordinate names to index, the joint_name
- * in \p msg is used for lookup.
- * @param time Pointer to decoded time
- * @param q Pointer to decoded generalized position
- * @param qd Pointer to decoded generalized velocity
- * @param joint_torque Pointer to decoded generalized joint torque
- * @param l_foot_wrench Pointer to decoded left foot wrench in the sensor frame
- * @param r_foot_wrench Pointer to decoded right foot wrench in the sensor frame
- */
-void DecodeRobotStateLcmMsg(
-    const bot_core::robot_state_t& msg,
-    const std::unordered_map<std::string, int>& q_name_to_index, double* time,
-    VectorX<double>* q, VectorX<double>* qd, VectorX<double>* joint_torque,
-    Vector6<double>* l_foot_wrench, Vector6<double>* r_foot_wrench);
 
 }  // namespace qp_inverse_dynamics
 }  // namespace examples
