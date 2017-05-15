ef376fa853b3321334f339c480573395f28a013b
Matthew Woehlke <matthew.woehlke@kitware.com>
2017-05-15 11:37:41 -0400

    Generate and install LCM exports
    Update PyCPS, bringing in the ability to generate a version check file.
    Add a script to generate a CPS for LCM (most of the content is
    hard-coded, but the script extracts LCM's version information from
    lcm_version.h). Implement generation of the remaining LCM artifacts.
    Update LCM to pick up support for namespaced targets (which we get from
    the CPS).
    
    As of this change, it should be possible for CMake consumers to build
    against the LCM that our new Bazel install logic installs.

---
+++ tools/lcm-create-cps.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import re
+import sys
+
+def_re = re.compile("#define (LCM_VERSION[^\s]+)\s+([^\s]+)")
+
+defs = {}
+with open(sys.argv[1]) as h:
+    for l in h:
+        m = def_re.match(l)
+        if m is not None:
+            defs[m.group(1)] = m.group(2)
+
+version = "%(LCM_VERSION_MAJOR)s.%(LCM_VERSION_MINOR)s.%(LCM_VERSION_PATCH)s"
+content = """
+{
+  "Cps-Version": "0.8.0",
+  "Name": "lcm",
+  "Description": "Lightweight Communications and Marshaling library",
+  "License": "BSD",
+  "Version":""" + version + """,
+  "Default-Components": [ "lcm" ],
+  "Components": {
+    "lcm-coretypes": {
+      "Type": "interface",
+      "Includes": [ "@prefix@/include" ]
+    },
+    "lcm": {
+      "Type": "dylib",
+      "Location": "@prefix@/lib/liblcm.so",
+      "Requires": [ ":lcm-coretypes" ]
+    },
+    "lcm-gen": {
+      "Type": "exe",
+      "Location": "@prefix@/bin/lcm-gen"
+    },
+    "lcm-java": {
+      "Type": "jar",
+      "Location": "@prefix@/share/java/lcm.jar"
+    }
+  },
+  "X-CMake-Variables": {
+    "LCM_NAMESPACE": "lcm::",
+    "LCM_VERSION": "${lcm_VERSION}",
+    "LCM_USE_FILE": "${CMAKE_CURRENT_LIST_DIR}/lcmUtilities.cmake"
+  }
+}
+""" % defs
+
+print(content[1:])
