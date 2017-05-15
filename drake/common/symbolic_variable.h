b42fe2f02e6c4376a179ad6e918b246f83005421
Soonho Kong <soonho.kong@tri.global>
2017-05-15 11:14:57 -0400

    Add type field to symbolic::Variable class

--- drake/common/symbolic_variable.h
+++ drake/common/symbolic_variable.h
@@ -19,26 +19,34 @@ class Variable {
  public:
   typedef size_t Id;
 
+  /** Supported types of symbolic variables. */
+  enum class Type { CONTINUOUS, INTEGER, BINARY };
+
   DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(Variable)
 
-  /** Default constructor. Constructs a dummy variable. This is needed to have
-   *  Eigen::Matrix<Variable>. The objects created by the default constructor
-   *  share the same ID, zero. As a result, they all are identified as a single
-   *  variable by equality operator (==). They all have the same hash value as
-   *  well.
+  /** Default constructor. Constructs a dummy variable of CONTINUOUS type. This
+   *  is needed to have Eigen::Matrix<Variable>. The objects created by the
+   *  default constructor share the same ID, zero. As a result, they all are
+   *  identified as a single variable by equality operator (==). They all have
+   *  the same hash value as well.
    *
    *  It is allowed to construct a dummy variable but it should not be used to
    *  construct a symbolic expression.
    */
-  Variable() : id_{0}, name_{std::make_shared<std::string>()} {}
+  Variable()
+      : id_{0},
+        type_{Type::CONTINUOUS},
+        name_{std::make_shared<std::string>()} {}
 
-  /** Constructs a variable with a string . */
-  explicit Variable(std::string name);
+  /** Constructs a variable with a string. If not specified, it has CONTINUOUS
+   * type by default.*/
+  explicit Variable(std::string name, Type type = Type::CONTINUOUS);
 
   /** Checks if this is a dummy variable (ID = 0) which is created by
    *  the default constructor. */
   bool is_dummy() const { return get_id() == 0; }
   Id get_id() const;
+  Type get_type() const;
   size_t get_hash() const { return std::hash<Id>{}(id_); }
   std::string get_name() const;
   std::string to_string() const;
@@ -55,6 +63,7 @@ class Variable {
   // Produces a unique ID for a variable.
   static Id get_next_id();
   Id id_{};  // Unique identifier.
+  Type type_{Type::CONTINUOUS};
 
   // Variable class has shared_ptr<string> instead of string to be
   // drake::test::IsMemcpyMovable.
@@ -62,6 +71,9 @@ class Variable {
   // for more information.
   std::shared_ptr<std::string> name_;  // Name of variable.
 };
+
+std::ostream& operator<<(std::ostream& os, Variable::Type type);
+
 }  // namespace symbolic
 
 /** Computes the hash value of a variable. */
