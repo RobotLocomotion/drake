81f9e9ea67088cf4f4021b08aff34bd7db4001bf
JyoPari <play.jyo@gmail.com>
2017-05-28 13:55:23 -0400

    including riccat's function

--- drake/math/test/ricatti_test.cc
+++ drake/math/test/ricatti_test.cc
@@ -1,1 +1,5 @@
 // Unit Test for drake/math/continuous_algebraic_ricatti_equation.cc
+
+#include "drake/drake/math/continuous_algebraic_ricatti_equation.cc" //including ricatti's equation solver for testing
+
+
