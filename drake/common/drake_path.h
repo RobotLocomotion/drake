84041345b382a862055eb7f7ef3c18ace74618ee
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-05-31 08:26:31 -0400

    Rewrite Bazel's GetDrakePath to use FindResource

--- drake/common/drake_path.h
+++ drake/common/drake_path.h
@@ -8,6 +8,9 @@ namespace drake {
 
 /// Returns the fully-qualified path to the root of the `drake` source tree.
 /// N.B: <em>not</em> the `drake-distro` source tree.
+///
+/// This method will eventually become deprecated.
+/// Please use drake::FindResource() instead.
 std::string GetDrakePath();
 
 }  // namespace drake
