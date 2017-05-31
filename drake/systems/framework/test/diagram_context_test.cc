e1715916ac0c48b69ed508e8539fbb0fdc855fb2
Evan Drumwright <drum@tri.global>
2017-05-30 20:03:58 -0700

    Bugfix attempt.

--- drake/systems/framework/test/diagram_context_test.cc
+++ drake/systems/framework/test/diagram_context_test.cc
@@ -314,7 +314,7 @@ TEST_F(DiagramContextTest, Accuracy) {
   const double unity = 1.0;
   context_->set_accuracy(unity);
   std::unique_ptr<Context<double>> clone = context_->Clone();
-  EXPECT_EQ(clone->get_accuracy().value(), unity);
+  EXPECT_EQ(clone->get_accuracy().value_or(999), unity);
 }
 
 }  // namespace
