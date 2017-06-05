a319fa5d129fafd5dbdbf6d9faa531a127074f08
Evan Drumwright <edrumwri@hotmail.com>
2017-06-05 11:00:17 -0700

    Computationally stiff tests (#6228)

--- drake/systems/analysis/test/implicit_euler_integrator_test.cc
+++ drake/systems/analysis/test/implicit_euler_integrator_test.cc
@@ -1,20 +1,57 @@
+#include "drake/systems/plants/spring_mass_system/spring_mass_system.h"
+#include "drake/systems/analysis/test/robertson_system.h"
+#include "drake/systems/analysis/test/spring_mass_damper_system.h"
+#include "drake/systems/analysis/test/stiff_double_mass_spring_system.h"
+#include "drake/systems/analysis/test/discontinuous_spring_mass_damper_system.h"
 #include "drake/systems/analysis/implicit_euler_integrator.h"
 
-#include <cmath>
-
-#include "gtest/gtest.h"
-
-#include "drake/systems/analysis/test/discontinuous_spring_mass_damper_system.h"
-#include "drake/systems/analysis/test/spring_mass_damper_system.h"
-#include "drake/systems/plants/spring_mass_system/spring_mass_system.h"
-
-using drake::systems::implicit_integrator_test::SpringMassDamperSystem;
-using drake::systems::implicit_integrator_test::
-    DiscontinuousSpringMassDamperSystem;
+#include <gtest/gtest.h>
 
 namespace drake {
 namespace systems {
 namespace {
+
+using implicit_integrator_test::SpringMassDamperSystem;
+using implicit_integrator_test::DiscontinuousSpringMassDamperSystem;
+
+// Tests the implicit integrator on Robertson's stiff chemical reaction
+// problem, which has been used to benchmark various implicit integrators.
+// This problem is particularly good at testing large step sizes (since the
+// solution quickly converges) and long simulation times.
+GTEST_TEST(ImplicitEulerIntegratorTest, Robertson) {
+  std::unique_ptr<analysis::test::RobertsonSystem<double>> robertson =
+    std::make_unique<analysis::test::RobertsonSystem<double>>();
+  std::unique_ptr<Context<double>> context = robertson->CreateDefaultContext();
+
+  // Set the initial conditions for Robertson's system.
+  VectorBase<double>* state = context->get_mutable_continuous_state()->
+                                get_mutable_vector();
+  state->SetAtIndex(0, 1);
+  state->SetAtIndex(1, 0);
+  state->SetAtIndex(2, 0);
+
+  const double t_final = robertson->get_end_time();
+  const double tol = 5e-5;
+
+  // Create the integrator.
+  ImplicitEulerIntegrator<double> integrator(*robertson, context.get());
+
+  // Very large step is necessary for this problem since given solution is
+  // at t = 1e11.
+  integrator.set_maximum_step_size(10000000.0);
+  integrator.set_throw_on_minimum_step_size_violation(false);
+  integrator.set_target_accuracy(tol);
+
+  // Integrate the system
+  integrator.Initialize();
+  integrator.IntegrateWithMultipleSteps(t_final);
+
+  // Verify the solution.
+  const Eigen::Vector3d sol = robertson->GetSolution(t_final);
+  EXPECT_NEAR(state->GetAtIndex(0), sol(0), tol);
+  EXPECT_NEAR(state->GetAtIndex(1), sol(1), tol);
+  EXPECT_NEAR(state->GetAtIndex(2), sol(2), tol);
+}
 
 class ImplicitIntegratorTest : public ::testing::Test {
  public:
@@ -30,16 +67,24 @@ class ImplicitIntegratorTest : public ::testing::Test {
         DiscontinuousSpringMassDamperSystem<double>>(stiff_spring_k_,
                                                      damping_b_, mass_,
                                                      constant_force_mag_);
+    stiff_double_system_ =
+        std::make_unique<analysis::test::StiffDoubleMassSpringSystem<double>>();
 
     // One context will be usable for three of the systems.
     context_ = spring_->CreateDefaultContext();
+
+    // Separate context necessary for the double spring mass system.
+    dspring_context_ = stiff_double_system_->CreateDefaultContext();
   }
 
   std::unique_ptr<Context<double>> context_;
+  std::unique_ptr<Context<double>> dspring_context_;
   std::unique_ptr<SpringMassSystem<double>> spring_;
   std::unique_ptr<SpringMassDamperSystem<double>> spring_damper_;
   std::unique_ptr<DiscontinuousSpringMassDamperSystem<double>>
      mod_spring_damper_;
+  std::unique_ptr<analysis::test::StiffDoubleMassSpringSystem<double>>
+     stiff_double_system_;
 
   const double dt_ = 1e-3;                // Default integration step size.
   const double large_dt_ = 1e-1;          // Large integration step size.
@@ -198,6 +243,49 @@ void CheckGeneralStatsValidity(ImplicitEulerIntegrator<double>* integrator) {
   EXPECT_GE(integrator->get_num_step_shrinkages_from_substep_failures(), 0);
   EXPECT_GE(integrator->get_num_step_shrinkages_from_error_control(), 0);
   integrator->ResetStatistics();
+}
+
+// Solve a stiff double spring-mass damper. This system has a very stiff spring
+// and damper connecting two point masses together, and one of the point masses
+// is connected to "the world" using a spring with no damper. The solution of
+// this system should approximate the solution of an undamped spring
+// connected to a mass equal to the sum of both point masses.
+TEST_F(ImplicitIntegratorTest, DoubleSpringMassDamper) {
+  // Clone the spring mass system's state.
+  std::unique_ptr<State<double>> state_copy = dspring_context_->CloneState();
+
+  // Designate the solution tolerance.
+  const double sol_tol = 2e-2;
+
+  // Set integrator parameters.
+  ImplicitEulerIntegrator<double> integrator(*stiff_double_system_,
+                                             dspring_context_.get());
+  integrator.set_maximum_step_size(large_dt_);
+  integrator.request_initial_step_size_target(large_dt_);
+  integrator.set_target_accuracy(1e-5);
+
+  // Get the solution at the target time.
+  const double t_final = 1.0;
+  stiff_double_system_->GetSolution(*dspring_context_, t_final,
+                                    state_copy->get_mutable_continuous_state());
+
+  // Take all the defaults.
+  integrator.Initialize();
+
+  // Integrate.
+  integrator.IntegrateWithMultipleSteps(t_final);
+
+  // Check the solution.
+  const VectorX<double> nsol = dspring_context_->get_continuous_state()->
+      get_generalized_position().CopyToVector();
+  const VectorX<double> sol = state_copy->get_continuous_state()->
+      get_generalized_position().CopyToVector();
+
+  for (int i = 0; i < nsol.size(); ++i)
+    EXPECT_NEAR(sol(i), nsol(i), sol_tol);
+
+  // Verify that integrator statistics are valid.
+  CheckGeneralStatsValidity(&integrator);
 }
 
 // Integrate the mass-spring-damping system using huge stiffness and damping.
@@ -494,9 +582,7 @@ TEST_F(ImplicitIntegratorTest, SpringMassStepAccuracyEffects) {
   integrator.set_maximum_step_size(large_dt_);
   integrator.set_requested_minimum_step_size(small_dt_);
   integrator.set_throw_on_minimum_step_size_violation(false);
-
-  // Turn fixed stepping on.
-  integrator.set_fixed_step_mode(true);
+  integrator.set_target_accuracy(1e-4);
 
   // Setup the initial position and initial velocity.
   const double initial_position = 0.1;
@@ -508,7 +594,7 @@ TEST_F(ImplicitIntegratorTest, SpringMassStepAccuracyEffects) {
 
   // Take all the defaults.
   integrator.Initialize();
-  EXPECT_NEAR(integrator.get_accuracy_in_use(), 1e-1,
+  EXPECT_NEAR(integrator.get_accuracy_in_use(), 1e-4,
               std::numeric_limits<double>::epsilon());
 
   // Get the actual solution.
@@ -517,7 +603,7 @@ TEST_F(ImplicitIntegratorTest, SpringMassStepAccuracyEffects) {
                                     large_dt_, &x_final_true, &v_final_true);
 
   // Integrate exactly one step.
-  integrator.IntegrateWithSingleFixedStep(large_dt_);
+  integrator.IntegrateWithMultipleSteps(large_dt_);
 
   // Get the positional error.
   const double pos_err = std::abs(x_final_true -
@@ -526,9 +612,13 @@ TEST_F(ImplicitIntegratorTest, SpringMassStepAccuracyEffects) {
   // Make the accuracy setting looser, integrate again, and verify that
   // positional error increases.
   integrator.set_target_accuracy(100.0);
+  EXPECT_NEAR(integrator.get_accuracy_in_use(), 100.0,
+              std::numeric_limits<double>::epsilon());
+  integrator.Initialize();
+  context_->set_time(0);
   spring_mass.set_position(context_.get(), initial_position);
   spring_mass.set_velocity(context_.get(), initial_velocity);
-  integrator.IntegrateWithSingleFixedStep(large_dt_);
+  integrator.IntegrateWithMultipleSteps(large_dt_);
   EXPECT_GT(std::abs(x_final_true -
       context_->get_continuous_state_vector().GetAtIndex(0)), pos_err);
 }
