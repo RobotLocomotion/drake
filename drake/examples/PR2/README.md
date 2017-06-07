f9e66e7c0bd3ab2ec9e06da1e531eac57adb2aa7
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-06-06 21:47:27 -0400

    Remove PR2 MATLAB demos
    The model files stay intact, since they are actively used by new python
    bindings.  A new README helps make this clear.

---
+++ drake/examples/PR2/README.md
@@ -0,0 +1,4 @@
+
+This directory contains model files for the PR2.
+
+For example uses, see drake/bindings/python/pydrake/test/testPR2IK.py.
--- drake/examples/PR2/drawPR2.m
+++
@@ -1,5 +0,0 @@
-function drawPR2
-
-options.terrain = RigidBodyFlatTerrain();
-r = RigidBodyManipulator('pr2.urdf',options);
-v = r.constructVisualizer();
