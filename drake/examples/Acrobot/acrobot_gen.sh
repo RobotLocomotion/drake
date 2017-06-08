efbbc2635c135c0a8cfd9c1a1d763f17a75030c7
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-06-08 18:14:17 -0400

    Remove unused acrobot_{input,output}_vector

--- drake/examples/Acrobot/acrobot_gen.sh
+++ drake/examples/Acrobot/acrobot_gen.sh
@@ -11,5 +11,3 @@ namespace="drake::examples::acrobot"
 source $drake/tools/lcm_vector_gen.sh
 
 gen_vector "acrobot state vector" theta1 theta2 theta1dot theta2dot
-gen_vector "acrobot input vector" tau
-gen_vector "acrobot output vector" theta1 theta2
