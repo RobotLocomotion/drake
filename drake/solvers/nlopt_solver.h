a8318e96d035448f1d8947d268b6d1a502f73b94
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-06-09 12:46:29 -0400

    Make MathematicalProgramSolverInterface an interface again

--- drake/solvers/nlopt_solver.h
+++ drake/solvers/nlopt_solver.h
@@ -12,13 +12,18 @@ class NloptSolver : public MathematicalProgramSolverInterface {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(NloptSolver)
 
-  NloptSolver() : MathematicalProgramSolverInterface(SolverType::kNlopt) {}
+  NloptSolver() = default;
+  ~NloptSolver() override = default;
 
   // This solver is implemented in various pieces depending on if
   // NLOpt was available during compilation.
   bool available() const override;
 
   SolutionResult Solve(MathematicalProgram& prog) const override;
+
+  SolverType solver_type() const override { return SolverType::kNlopt; }
+
+  std::string SolverName() const override { return "NLopt"; }
 };
 
 }  // namespace solvers
