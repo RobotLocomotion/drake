c461756cf34b30ec1921f48b23f59065ffa011b6
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-06-09 11:35:09 -0400

    Make MathematicalProgramSolverInterface an interface again

--- drake/solvers/dreal_solver.h
+++ drake/solvers/dreal_solver.h
@@ -12,13 +12,18 @@ class DrealSolver : public MathematicalProgramSolverInterface {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(DrealSolver)
 
-  DrealSolver() : MathematicalProgramSolverInterface(SolverType::kDReal) {}
+  DrealSolver() = default;
+  ~DrealSolver() override = default;
 
   // This solver is implemented in various pieces depending on if
   // Dreal was available during compilation.
   bool available() const override;
 
   SolutionResult Solve(MathematicalProgram& prog) const override;
+
+  SolverType solver_type() const override  { return SolverType::kDReal; }
+
+  std::string SolverName() const override { return "dReal"; }
 };
 
 }  // namespace solvers
--- drake/solvers/snopt_solver.h
+++ drake/solvers/snopt_solver.h
@@ -12,13 +12,18 @@ class SnoptSolver : public MathematicalProgramSolverInterface  {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(SnoptSolver)
 
-  SnoptSolver() : MathematicalProgramSolverInterface(SolverType::kSnopt) {}
+  SnoptSolver() = default;
+  ~SnoptSolver() override = default;
 
   // This solver is implemented in various pieces depending on if
   // SNOPT was available during compilation.
   bool available() const override;
 
   SolutionResult Solve(MathematicalProgram& prog) const override;
+
+  SolverType solver_type() const override { return SolverType::kSnopt; }
+
+  std::string SolverName() const override { return "SNOPT"; }
 };
 
 }  // namespace solvers
