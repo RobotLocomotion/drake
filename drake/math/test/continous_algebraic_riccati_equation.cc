cf161a91c863fd90b1e991b4467279be44a4a191
JyoPari <play.jyo@gmail.com>
2017-06-08 20:35:17 -0400

    creating namespace

---
+++ drake/math/test/continous_algebraic_riccati_equation.cc
@@ -0,0 +1,17 @@
+#include "drake/math/continuous_algebraic_riccati_equation.h"
+#include <gtest/gtest.h>
+#include "drake/common/eigen_matrix_compare.h"
+#include "drake/math/autodiff.h"
+
+using Eigen::MatrixXd;
+
+namespace drake {
+namespace math {
+namespace {
+
+
+
+
+    }
+  }
+}
