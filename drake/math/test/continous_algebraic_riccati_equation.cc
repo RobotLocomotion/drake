418871f00e21fb56ff66073a8d5edb5e07df282f
jyopari <play.jyo@gmail.com>
2017-06-11 09:31:20 -0400

    Create continous_algebraic_riccati_equation.cc
    Checking return values holds true.

--- drake/math/test/continous_algebraic_riccati_equation.cc
+++ drake/math/test/continous_algebraic_riccati_equation.cc
@@ -8,9 +8,29 @@ using Eigen::MatrixXd;
 namespace drake {
 namespace math {
 namespace {
+  
+  void TestCare(const Eigen::Ref<const MatrixXd>& A, // passing matricies and values
+                const Eigen::Ref<const MatrixXd>& B,
+                const Eigen::Ref<const MatrixXd>& Q,
+                const Eigen::Ref<const MatrixXd>& R){
+  MatrixXd X = ContinousAlgebraicRicattiEqation(A,B,Q,R); // setting matrix X as the return from CARE
+  
+  EXPECT_TRUE(CompareMatrices(X, X.transpose(), 1E-10, MatrixCompareType::absolute));
+    int n = X.rows();
+    Eigen::SelfAdjointEigenSolver<MatrixXd> es(X);
+    for (int i = 0; i < n; i++) {
+      EXPECT_GE(es.eigenvalues()[i], 0);
+  }
+  
+  MatrixXd Y = (A.transpose*X)+
+                     (X*A)-(X*B*R.inverse*B.transpose)+
+                     (C.transpose*C); // Solving the Continous-Time AR Equation using A^T*X+X*A-XBR^-1+C^T*c = 0 
 
-
-
+  
+  EXPECT_TRUE(CompareMatrices(Y, MatrixXd::Zero(n, n), 1E-10,
+                              MatrixCompareType::absolute));
+}
+  
 
     }
   }
