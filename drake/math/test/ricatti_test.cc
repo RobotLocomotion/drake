569547b2c9e4dbc158b0590f8c28cf5a1302ebc7
jyopari <play.jyo@gmail.com>
2017-06-11 07:53:08 -0400

    Delete ricatti_test.cc

--- drake/math/test/ricatti_test.cc
+++
@@ -1,5 +0,0 @@
-// Unit Test for drake/math/continuous_algebraic_ricatti_equation.cc
-
-#include "drake/drake/math/continuous_algebraic_ricatti_equation.cc" //including ricatti's equation solver for testing
-
-
