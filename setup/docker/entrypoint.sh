78c3d9dac815d8474d75bbe2e1663cf5c2d8a25f
Brandon D. Northcutt <brandon@northcutt.net>
2017-06-12 10:55:38 -0700

    Single Dockerfile.

--- setup/docker/entrypoint.sh
+++ setup/docker/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -e
+set -e -u
 [[ $# -eq 0 ]] && {
-  cd /drake/build/install/bin
+  #Needed until bazel build produces a drake-visualizer
+  cd /drake-distro/build/install/bin
   ./drake-visualizer &
   sleep 3
-  cd /drake/build/drake/examples/Acrobot
-  ./acrobot_run_passive
+  cd /drake-distro
+  bazel run //drake/examples/Acrobot:acrobot_run_passive
 } || {
   eval "$@"
 }
