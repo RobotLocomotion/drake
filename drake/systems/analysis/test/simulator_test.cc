afce6bc81e8c3c90b008295c21b2769e247ca1cf
Evan Drumwright <drum@tri.global>
2017-06-13 10:01:47 -0700

    Removed no-longer-used logging related stuff in main.

--- drake/systems/analysis/test/simulator_test.cc
+++ drake/systems/analysis/test/simulator_test.cc
@@ -6,14 +6,12 @@
 #include <map>
 
 #include <gtest/gtest.h>
-#include <gflags/gflags.h>
 
 #include <unsupported/Eigen/AutoDiff>
 
 #include "drake/common/drake_assert.h"
 #include "drake/common/drake_copyable.h"
 #include "drake/common/text_logging.h"
-#include "drake/common/text_logging_gflags.h"
 #include "drake/common/test/is_dynamic_castable.h"
 #include "drake/systems/analysis/explicit_euler_integrator.h"
 #include "drake/systems/analysis/implicit_euler_integrator.h"
