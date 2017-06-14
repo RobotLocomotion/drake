6427b923dac1c502970ca3341dc3f23f411cce38
Tristan Thrush <tristant@mit.edu>
2017-06-14 12:31:32 -0700

    work on adding a controller and IK planner to the PR2. Doesnt visualize properly

--- drake/examples/PR2/pr2_simulation.cc
+++ drake/examples/PR2/pr2_simulation.cc
@@ -22,6 +22,7 @@
 #include "drake/multibody/parsers/urdf_parser.h"
 #include "drake/multibody/rigid_body_ik.h"
 
+
 using Eigen::Vector2d;
 using Eigen::Vector3d;
 using Eigen::VectorXd;
@@ -36,14 +37,83 @@ namespace drake {
     namespace examples {
         namespace pr2 {
             namespace {
+/*
+                unique_ptr <PiecewisePolynomialTrajectory> MakePlan(std::unique_ptr <RigidBodyTree<double>> tree) {
 
-                std::unique_ptr <RigidBodyTree<double>> build_pr2_tree(std::vector <ModelInstanceInfo<double>> *pr2) {
+                    //auto tree = make_unique<RigidBodyTree<double>>();
+                    //parsers::urdf::AddModelInstanceFromUrdfFileToWorld(
+                    //GetDrakePath() + "/examples/PR2/pr2_drake_planning.urdf", multibody::joints::kFixed, tree.get());
+                    //kuka_iiwa_arm::CreateTreedFromFixedModelAtPose(kUrdfPath, tree.get());
+
+                    // Creates a basic pointwise IK trajectory for moving the iiwa arm.
+                    // It starts in the zero configuration (straight up).
+                    //std::unique_ptr<RigidBodyTree<double>> tree = make_unique<RigidBodyTree<double>>(plant->get_rigid_body_tree());
+
+                    VectorXd zero_conf = tree->getZeroConfiguration();
+                    VectorXd joint_lb = zero_conf - VectorXd::Constant(7, 0.01);
+                    VectorXd joint_ub = zero_conf + VectorXd::Constant(7, 0.01);
+
+                    // Defines an end effector constraint and makes it active for the time span
+                    // from 1 to 3 seconds.
+                    Vector3d pos_end(0.6, 0, 0.325);
+                    Vector3d pos_lb = pos_end - Vector3d::Constant(0.005);
+                    Vector3d pos_ub = pos_end + Vector3d::Constant(0.005);
+                    WorldPositionConstraint wpc1(tree.get(),
+                                                 tree->FindBodyIndex("iiwa_link_ee"), //"r_wrist_roll_link"),
+                                                 Vector3d::Zero(), pos_lb, pos_ub,
+                                                 Vector2d(1, 20));
+
+                    const std::vector<double> kTimes{0.0, 2.0, 5.0, 7.0, 9.0};
+                    MatrixXd q0(tree->get_num_positions(), kTimes.size());
+                    for (size_t i = 0; i < kTimes.size(); ++i) {
+                        q0.col(i) = zero_conf;
+                    }
+
+                    std::vector < RigidBodyConstraint * > constraint_array;
+                    constraint_array.push_back(&wpc1);
+                    IKoptions ikoptions(tree.get());
+                    std::vector<int> info(kTimes.size(), 0);
+                    MatrixXd q_sol(tree->get_num_positions(), kTimes.size());
+                    std::vector <std::string> infeasible_constraint;
+
+                    inverseKinPointwise(tree.get(), kTimes.size(), kTimes.data(), q0, q0,
+                                        constraint_array.size(), constraint_array.data(),
+                                        ikoptions, &q_sol, info.data(), &infeasible_constraint);
+
+                    //inverseKin(tree.get(), q0, q0, )
+                    bool info_good = true;
+                    for (size_t i = 0; i < kTimes.size(); ++i) {
+                        drake::log()->info("INFO[{}] = {} ", i, info[i]);
+                        if (info[i] != 1) {
+                            info_good = false;
+                        }
+                    }
+                    printf("\n");
+
+                    if (!info_good) {
+                        throw std::runtime_error(
+                                "inverseKinPointwise failed to compute a valid solution.");
+                    }
+
+                    std::vector <MatrixXd> knots(kTimes.size());
+                    for (size_t i = 0; i < kTimes.size(); ++i) {
+                        // We only use column 0 of the matrix in knots (for joint positions),
+                        // so we write a vector.
+                        knots[i] = q_sol.col(i);
+                    }
+
+                    return make_unique<PiecewisePolynomialTrajectory>(
+                            PiecewisePolynomial<double>::FirstOrderHold(kTimes, knots));
+                }
+*/
+                std::unique_ptr <RigidBodyTree<double>>
+                build_pr2_tree(std::vector <ModelInstanceInfo<double>> *pr2, std::string kUrdfPath) {
                     auto tree_builder = std::make_unique < WorldSimTreeBuilder < double >> ();
                     // Adds models to the simulation builder. Instances of these models can be
                     // subsequently added to the world.
-                    tree_builder->StoreModel("pr2", "/examples/PR2/pr2_drake_planning.urdf");
+                    tree_builder->StoreModel("pr2", kUrdfPath);
                     pr2->clear();
-                    int id = tree_builder->AddFloatingModelInstance("pr2", Vector3<double>(0, 0, 0));
+                    int id = tree_builder->AddFixedModelInstance("pr2", Vector3<double>(0, 0, 0));
                     pr2->push_back(tree_builder->get_model_info_for_instance(id));
                     tree_builder->AddGround();
                     return tree_builder->Build();
@@ -53,74 +123,47 @@ namespace drake {
                 void main() {
 
                     //Initialize the robot in the world with the ground, and the visualizer
+                    std::string kUrdfPath = "/examples/PR2/pr2_drake_planning.urdf";//"/examples/Acrobot/Acrobot.urdf";//"/examples/Atlas/urdf/atlas_convex_hull.urdf";/*/"/manipulation/models/iiwa_description/urdf/iiwa14_polytope_collision.urdf";
                     drake::lcm::DrakeLcm lcm;
                     std::vector <ModelInstanceInfo<double>> pr2_info;
                     SimDiagramBuilder<double> builder;
                     systems::DiagramBuilder<double> *diagram_builder = builder.get_mutable_builder();
-                    systems::RigidBodyPlant<double> *plant = builder.AddPlant(build_pr2_tree(&pr2_info));
+                    systems::RigidBodyPlant<double> *plant = builder.AddPlant(build_pr2_tree(&pr2_info, kUrdfPath));
+                    const int num_actuators = plant->actuator_command_input_port().size();
                     builder.AddVisualizer(&lcm);
 
-                    //TODO: add a controller instead of the following line (a start is below)
-                    const int num_actuators = plant->actuator_command_input_port().size();
+                    //Uncontrolled
+                    /*
                     auto constant_zero_source = diagram_builder->template AddSystem<systems::ConstantVectorSource < double>>(VectorX<double>::Zero(num_actuators));
                     diagram_builder->Connect(constant_zero_source->get_output_port(), plant->actuator_command_input_port());
+                    */
 
-                    /*
-                    //Create a controller
+                    //Controller
                     const VectorX<double> kp = VectorX<double>::Constant(num_actuators, 300.0);
                     const VectorX<double> ki = VectorX<double>::Constant(num_actuators, 0.0);
                     const VectorX<double> kd = VectorX<double>::Constant(num_actuators, 5.0);
-                    auto controller = builder.AddController<systems::PidController<double>>(pr2_info[0].instance_id, kp, ki, kd);
+                    auto controller =
+                            builder.AddController < systems::InverseDynamicsController < double >> (
+                                    pr2_info[0].instance_id, plant->get_rigid_body_tree(), kp, ki, kd,
+                                            false);
 
-                    std::unique_ptr<PiecewisePolynomialTrajectory> traj = MakePlan();
+                    //To give the controller zeros
+                    auto constant_zero_source = diagram_builder->template AddSystem<systems::ConstantVectorSource < double>>(VectorX<double>::Zero(controller->get_input_port_desired_state().size()));
+                    diagram_builder->Connect(constant_zero_source->get_output_port(), controller->get_input_port_desired_state());
 
-                    // Adds a trajectory source for desired state.
+                    //To give the controller the trajectory from the ik planner
+                    /*
+                    std::unique_ptr <PiecewisePolynomialTrajectory> traj = MakePlan(
+                            build_pr2_tree(&pr2_info, kUrdfPath));
                     auto traj_src =
-                    diagram_builder->template AddSystem<systems::TrajectorySource<double>>(
+                    diagram_builder->template AddSystem<systems::TrajectorySource < double>>
+                    (
                             *traj, 1);
                     traj_src->set_name("trajectory_source");
 
-                    diagram_builder->Connect(traj_src->get_output_port(),
-                                             controller->get_input_port_desired_state());
-
-                    //Create the command subscriber and status publisher.
-
-                    const RigidBodyTree<double>& tree = plant->get_rigid_body_tree();
-                    const int num_joints = tree.get_num_positions();
-
-                    std::cout << num_actuators << "\n";
-                    std::cout << num_joints << "\n";
-
-                    //TODO: make an lcmt_pr2_command
-                    auto command_subscriber = diagram_builder->AddSystem(systems::lcm::LcmSubscriberSystem::Make<lcmt_pr2_command>("PR2_COMMAND", &lcm));
-                    command_subscriber->set_name("command_subscriber");
-
-                    //TODO: make a Pr2CommandReciever
-                    auto command_receiver = diagram_builder->AddSystem<Pr2CommandReceiver>(num_joints);
-                    command_receiver->set_name("command_receiver");
-
-                    //TODO: make an lcmt_pr2_status
-                    //TODO: make a kPr2LcmStatusPeriod
-                    auto status_publisher = diagram_builder->AddSystem(systems::lcm::LcmPublisherSystem::Make<lcmt_pr2_status>("PR2_STATUS", &lcm));
-                    status_publisher->set_name("status_publisher");
-                    status_publisher->set_publish_period(kIiwaLcmStatusPeriod);
-
-                    //TODO: make a Pr2StatusSender
-                    auto status_sender = diagram_builder->AddSystem<Pr2StatusSender>(num_joints);
-                    status_sender->set_name("status_sender");
-
-                    //Connect everything
-                    diagram_builder->Connect(command_subscriber->get_output_port(0),
-                                          command_receiver->get_input_port(0));
-                    diagram_builder->Connect(command_receiver->get_output_port(0),
-                                          controller->get_input_port_desired_state());
-                    diagram_builder->Connect(plant->get_output_port(0),
-                                          status_sender->get_state_input_port());
-                    diagram_builder->Connect(command_receiver->get_output_port(0),
-                                          status_sender->get_command_input_port());
-                    diagram_builder->Connect(status_sender->get_output_port(0),
-                                          status_publisher->get_input_port(0));
+                    diagram_builder->Connect(traj_src->get_output_port(), controller->get_input_port_desired_state());
                     */
+
                     // Simulates.
                     std::unique_ptr <systems::Diagram<double>> diagram = builder.Build();
                     systems::Simulator<double> simulator(*diagram);
