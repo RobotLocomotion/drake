3d08dfc39eced63db3a133e15efc1a700abb9964
hongkai-dai <daihongkai@gmail.com>
2017-06-14 15:27:46 -0700

    Add a sos test

--- drake/solvers/test/sos_constraint_test.cc
+++ drake/solvers/test/sos_constraint_test.cc
@@ -15,6 +15,25 @@ GTEST_TEST(SosConstraintTest, Univariate1) {
   EXPECT_EQ(result, SolutionResult::kSolutionFound);
 }
 
+GTEST_TEST(SosConstraintTest, Univariate2_Test) {
+  // Find the global optimal of the polynomial
+  // f(x) = (x-2)⁶ + 2 * (x-2)⁴ + 4(x-2)² + 3
+  // The global optimal is obtained at x = 2, with optimal cost 3
+  // We solve this by
+  // max c
+  // s.t f(x) - c is sum-of-squares.
+  MathematicalProgram prog;
+  auto x = prog.NewIndeterminates<1>();
+  auto c = prog.NewContinuousVariables<1>();
+  prog.AddCost(-c(0));
+  symbolic::Polynomial p(pow(x(0) - 2, 6) + 2 * pow(x(0) - 2, 4) + 4 * pow(x(0) - 2, 2) + 3 - c(0), symbolic::Variables({x(0)}));
+  prog.AddSosConstraint(p);
+
+  auto result = prog.Solve();
+  EXPECT_EQ(result, SolutionResult::kSolutionFound);
+  EXPECT_NEAR(prog.GetSolution(c(0)), 3, 1E-4);
+}
+
 GTEST_TEST(SosConstraintTest, Multivariate1) {
   MathematicalProgram prog;
   auto x = prog.NewIndeterminates<2>();
