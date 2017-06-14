d03a39675ccfa6e1647813d760db765be5406991
hongkai-dai <daihongkai@gmail.com>
2017-06-14 13:31:14 -0700

    Add more tests on sos2

--- drake/solvers/test/mixed_integer_optimization_util_test.cc
+++ drake/solvers/test/mixed_integer_optimization_util_test.cc
@@ -51,8 +51,8 @@ GTEST_TEST(TestGrayCode, TestGrayCodeToInteger) {
   EXPECT_EQ(drake::solvers::internal::GrayCodeToInteger(Eigen::Vector3i(1, 0, 1)), 6);
   EXPECT_EQ(drake::solvers::internal::GrayCodeToInteger(Eigen::Vector3i(1, 0, 0)), 7);
 }
-class LogarithmicSOS2Test : public ::testing::TestWithParam
-GTEST_TEST(TestMixedIntegerUtil, TestLogarithmicSOS2) {
+
+void LogarithmicSOS2Test(int num_lambda) {
   // Solve the program
   // min λᵀ * λ
   // s.t sum λ = 1
@@ -60,30 +60,55 @@ GTEST_TEST(TestMixedIntegerUtil, TestLogarithmicSOS2) {
   // We loop over i such that only λ(i) and λ(i+1) can be strictly positive.
   // The optimal cost is λ(i) = λ(i + 1) = 0.5.
   MathematicalProgram prog;
-  auto lambda = prog.NewContinuousVariables<5>("lambda");
+  auto lambda = prog.NewContinuousVariables(num_lambda, "lambda");
   prog.AddLinearConstraint(lambda.cast<symbolic::Expression>().sum() == 1);
-
-  prog.AddCost(lambda.cast<symbolic::Expression>().dot(lambda.cast<symbolic::Expression>()));
-
+  prog.AddCost(lambda.cast<symbolic::Expression>().dot(lambda));
   auto y = AddLogarithmicSOS2Constraint(&prog, lambda.cast<symbolic::Expression>());
-  auto y_assignment0 = prog.AddLinearConstraint(y(0) == 0);
-  auto y_assignment1 = prog.AddLinearConstraint(y(1) == 0);
-  const auto gray_codes = drake::solvers::internal::CalculateReflectedGrayCodes(2);
-  for (int i = 0; i < lambda.rows() - 1; ++i) {
-    y_assignment0.constraint()->UpdateLowerBound(Vector1d(gray_codes(i, 0)));
-    y_assignment0.constraint()->UpdateUpperBound(Vector1d(gray_codes(i, 0)));
-    y_assignment1.constraint()->UpdateLowerBound(Vector1d(gray_codes(i, 1)));
-    y_assignment1.constraint()->UpdateUpperBound(Vector1d(gray_codes(i, 1)));
+  int num_binary_vars = y.rows();
+  int num_intervals = num_lambda - 1;
+  auto y_assignment = prog.AddBoundingBoxConstraint(0, 1, y);
+
+  const auto gray_codes = drake::solvers::internal::CalculateReflectedGrayCodes(num_binary_vars);
+  Eigen::VectorXd y_val(num_binary_vars);
+  for (int i = 0; i < num_intervals; ++i) {
+    y_val.setZero();
+    for (int j = 0; j < num_binary_vars; ++j) {
+      y_val(j) = gray_codes(i, j);
+    }
+    y_assignment.constraint()->UpdateLowerBound(y_val);
+    y_assignment.constraint()->UpdateUpperBound(y_val);
 
     GurobiSolver gurobi_solver;
-    auto result = gurobi_solver.Solve(prog);
-    EXPECT_EQ(result, SolutionResult::kSolutionFound);
-    const auto lambda_val = prog.GetSolution(lambda);
-    Eigen::Matrix<double, 5, 1> lambda_val_expected = Eigen::Matrix<double, 5, 1>::Zero();
-    lambda_val_expected(i) = 0.5;
-    lambda_val_expected(i + 1) = 0.5;
-    EXPECT_TRUE(CompareMatrices(lambda_val, lambda_val_expected, 1E-5, MatrixCompareType::absolute));
+    if (gurobi_solver.available()) {
+      auto result = gurobi_solver.Solve(prog);
+      EXPECT_EQ(result, SolutionResult::kSolutionFound);
+      const auto lambda_val = prog.GetSolution(lambda);
+      Eigen::VectorXd lambda_val_expected = Eigen::VectorXd::Zero(num_lambda);
+      lambda_val_expected(i) = 0.5;
+      lambda_val_expected(i + 1) = 0.5;
+      EXPECT_TRUE(CompareMatrices(lambda_val, lambda_val_expected, 1E-5, MatrixCompareType::absolute));
+    }
   }
+}
+
+GTEST_TEST(TestLogarithmicSOS2, Test4Lambda) {
+  LogarithmicSOS2Test(4);
+}
+
+GTEST_TEST(TestLogarithmicSOS2, Test5Lambda) {
+  LogarithmicSOS2Test(5);
+}
+
+GTEST_TEST(TestLogarithmicSOS2, Test6Lambda) {
+  LogarithmicSOS2Test(6);
+}
+
+GTEST_TEST(TestLogarithmicSOS2, Test7Lambda) {
+  LogarithmicSOS2Test(7);
+}
+
+GTEST_TEST(TestLogarithmicSOS2, Test8Lambda) {
+  LogarithmicSOS2Test(8);
 }
 }  // namespace
 }  // namespace solvers
