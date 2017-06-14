9548d074aa97bced014fac74bac3d595a72e6441
hongkai-dai <daihongkai@gmail.com>
2017-06-13 23:11:30 -0700

    WIP: add logarithmic sos2 constraint

--- drake/solvers/mixed_integer_optimization_util.cc
+++ drake/solvers/mixed_integer_optimization_util.cc
@@ -37,5 +37,25 @@ Eigen::MatrixXi CalculateReflectedGrayCodes(int k) {
 }
 }  // namespace internal
 
+VectorXDecisionVariable AddLogarithmicSOS2Constraint(MathematicalProgram* prog, const Eigen::Ref<const VectorX<symbolic::Expression>>& lambda, const std::string& binary_variable_name) {
+  const int num_lambda = lambda.rows();
+  const int num_interval = num_lambda - 1;
+  const auto gray_codes = internal::CalculateReflectedGrayCodes(num_interval);
+  const int num_binary_vars = CeilLog2(num_interval);
+  auto y = prog->NewBinaryVariables(num_binary_vars, binary_variable_name);
+  for (int j = 0; j < num_binary_vars; ++j) {
+    symbolic::Expression lambda_sum1 = gray_codes(0, j) == 1 ? lambda(0) : 0;
+    symbolic::Expression lambda_sum2 = gray_codes(0, j) == 0 ? lambda(0) : 0;
+    for (int i = 1; i < num_lambda - 1; ++i) {
+      lambda_sum1 += (gray_codes(i - 1, j) == 1 && gray_codes(i, j) == 1) ? lambda(i) : 0;
+      lambda_sum2 += (gray_codes(i - 1, j) == 0 && gray_codes(i, j) == 0) ? lambda(i) : 0;
+    }
+    lambda_sum1 += gray_codes(num_lambda - 1) == 1 ? lambda(num_lambda - 1) : 0;
+    lambda_sum2 += gray_codes(num_lambda - 1) == 0 ? lambda(num_lambda - 1) : 0;
+    prog->AddLinearConstraint(lambda_sum1 <= y(j));
+    prog->AddLinearConstraint(lambda_sum2 <= 1 - y(j));
+  }
+  return y;
+}
 }  // namespace solvers
 }  // namespace drake
--- drake/solvers/test/mixed_integer_optimization_util_test.cc
+++ drake/solvers/test/mixed_integer_optimization_util_test.cc
@@ -2,6 +2,9 @@
 
 #include <gtest/gtest.h>
 
+#include "drake/common/eigen_matrix_compare.h"
+#include "drake/solvers/gurobi_solver.h"
+
 namespace drake {
 namespace solvers {
 namespace {
@@ -12,10 +15,10 @@ GTEST_TEST(TestMixedIntegerUtil, TestCeilLog2) {
   for (int i = 1; i <= kMaxExponent; ++i) {
     two_to_exponent[i] = 2 * two_to_exponent[i - 1];
   }
-  EXPECT_EQ(0, ceil_log2(1));
+  EXPECT_EQ(0, CeilLog2(1));
   for (int i = 0; i < kMaxExponent; ++i) {
     for (int j = two_to_exponent[i] + 1; j <= two_to_exponent[i]; ++j) {
-      EXPECT_EQ(i + 1, ceil_log2(j));
+      EXPECT_EQ(i + 1, CeilLog2(j));
     }
   }
 }
@@ -47,6 +50,42 @@ GTEST_TEST(TestGrayCode, TestGrayCodeToInteger) {
   EXPECT_EQ(drake::solvers::internal::GrayCodeToInteger(Eigen::Vector3i(1, 1, 1)), 5);
   EXPECT_EQ(drake::solvers::internal::GrayCodeToInteger(Eigen::Vector3i(1, 0, 1)), 6);
   EXPECT_EQ(drake::solvers::internal::GrayCodeToInteger(Eigen::Vector3i(1, 0, 0)), 7);
+}
+
+GTEST_TEST(TestMixedIntegerUtil, TestLogarithmicSOS2) {
+  // Solve the program
+  // min λᵀ * λ
+  // s.t sum λ = 1
+  //     λ >= 0
+  //     λ in sos2
+  // We loop over i such that only λ(i) and λ(i+1) can be non-zero.
+  // The optimal cost is λ(i) = λ(i + 1) = 0.5.
+  MathematicalProgram prog;
+  auto lambda = prog.NewContinuousVariables<5>();
+  prog.AddBoundingBoxConstraint(0, 1, lambda);
+  prog.AddLinearConstraint(lambda.cast<symbolic::Expression>().sum() == 1);
+
+  prog.AddCost(lambda.cast<symbolic::Expression>().dot(lambda.cast<symbolic::Expression>()));
+
+  auto y = AddLogarithmicSOS2Constraint(&prog, lambda.cast<symbolic::Expression>());
+  auto y_assignment0 = prog.AddLinearConstraint(y(0) == 0);
+  auto y_assignment1 = prog.AddLinearConstraint(y(1) == 0);
+  const auto gray_codes = drake::solvers::internal::CalculateReflectedGrayCodes(2);
+  for (int i = 0; i < lambda.rows() - 1; ++i) {
+    y_assignment0.constraint()->UpdateLowerBound(Vector1d(gray_codes(i, 0)));
+    y_assignment0.constraint()->UpdateUpperBound(Vector1d(gray_codes(i, 0)));
+    y_assignment1.constraint()->UpdateLowerBound(Vector1d(gray_codes(i, 1)));
+    y_assignment1.constraint()->UpdateUpperBound(Vector1d(gray_codes(i, 1)));
+
+    GurobiSolver gurobi_solver;
+    auto result = gurobi_solver.Solve(prog);
+    EXPECT_EQ(result, SolutionResult::kSolutionFound);
+    const auto lambda_val = prog.GetSolution(lambda);
+    Eigen::Matrix<double, 5, 1> lambda_val_expected = Eigen::Matrix<double, 5, 1>::Zero();
+    lambda_val_expected(i) = 0.5;
+    lambda_val_expected(i + 1) = 0.5;
+    EXPECT_TRUE(CompareMatrices(lambda_val, lambda_val_expected, 1E-5, MatrixCompareType::absolute));
+  }
 }
 }  // namespace
 }  // namespace solvers
