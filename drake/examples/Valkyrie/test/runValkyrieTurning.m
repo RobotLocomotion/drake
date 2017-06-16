1b6fbc02add0c35a024ebc56180b7e1130fe7382
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-06-16 19:50:35 -0400

    Remove MATLAB Valkyrie example

--- drake/examples/Valkyrie/test/runValkyrieTurning.m
+++
@@ -1,9 +0,0 @@
-function runValkyrieTurning()
-
-path_handle = addpathTemporary(fullfile(getDrakePath,'examples','Valkyrie'));
-runValkyrieWalking([], struct('initial_pose', [0;0;0;0;0;3*pi/2],...
-                                'navgoal', [0;0;0;0;0;pi/2-pi/16],...
-                                'max_num_steps', 15));
-end
-
-% TIMEOUT 1500
--- drake/examples/Valkyrie/test/runValkyrieTurning270.m
+++
@@ -1,9 +0,0 @@
-function runValkyrieTurning270()
-
-path_handle = addpathTemporary(fullfile(getDrakePath,'examples','Valkyrie'));
-runValkyrieWalking([], struct('initial_pose', [1;0;0;0;0;pi/2],...
-                                'navgoal', [0;-1;0;0;0;-pi/2-pi/16],...
-                                'max_num_steps', 15));
-end
-
-% TIMEOUT 1500
