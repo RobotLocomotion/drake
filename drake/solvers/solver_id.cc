a433f46828f5a79058144a3a6a779d1730425955
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-06-26 14:05:19 -0400

    Add solver_id component

---
+++ drake/solvers/solver_id.cc
@@ -0,0 +1,42 @@
+#include "drake/solvers/solver_id.h"
+
+#include <atomic>
+#include <utility>
+
+#include "drake/common/never_destroyed.h"
+
+namespace drake {
+namespace solvers {
+namespace {
+
+int get_next_id() {
+  // Note that id 0 is reserved for the SolverId that is created by the default
+  // constructor. As a result, we have an invariant "get_next_id() > 0".
+  static never_destroyed<std::atomic<int>> next_id{1};
+  return next_id.access()++;
+}
+
+}  // namespace
+
+SolverId::SolverId()
+    : id_{0}, name_{"unknown"} {}
+
+SolverId::SolverId(std::string name)
+    : id_{get_next_id()}, name_{std::move(name)} {}
+
+bool operator==(const SolverId& a, const SolverId& b) {
+  return a.id_ == b.id_;
+}
+
+bool operator!=(const SolverId& a, const SolverId& b) {
+  return a.id_ != b.id_;
+}
+
+std::ostream& operator<<(std::ostream& os, const SolverId& self) {
+  // N.B. The ID is _not_ exposed to callers.
+  os << self.name();
+  return os;
+}
+
+}  // namespace solvers
+}  // namespace drake
---
+++ drake/solvers/solver_id.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <string>
+#include <ostream>
+
+#include "drake/common/drake_copyable.h"
+#include "drake/common/reinit_after_move.h"
+
+namespace drake {
+namespace solvers {
+
+/// Identifies a MathematicalProgramSolverInterface implementation.
+///
+/// A moved-from instance is guaranteed to be identical to a
+/// default-constructed instance.
+class SolverId {
+ public:
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(SolverId)
+  ~SolverId() = default;
+
+  /// Constructs a default "unknown" solver type.  All unknown instances are
+  /// considered equal.
+  SolverId();
+
+  /// Constructs a specific, known solver type.  Internally, a hidden
+  /// identifier is allocated and assigned to this instance; all instances that
+  /// share an identifier (including copies of this instance) are considered
+  /// equal.  The solver names are not enforced to be unique, though we
+  /// recommend that they remain so in practice.
+  explicit SolverId(std::string name);
+
+  const std::string& name() const { return name_; }
+
+ private:
+  friend bool operator==(const SolverId&, const SolverId&);
+  friend bool operator!=(const SolverId&, const SolverId&);
+
+  reinit_after_move<int> id_;
+  std::string name_;
+};
+
+bool operator==(const SolverId&, const SolverId&);
+bool operator!=(const SolverId&, const SolverId&);
+std::ostream& operator<<(std::ostream&, const SolverId&);
+
+}  // namespace solvers
+}  // namespace drake
