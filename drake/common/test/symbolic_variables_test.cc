e14787bf2412b0c637934916e1d478a9e8dad40c
Soonho Kong <soonho.kong@tri.global>
2017-06-28 10:58:38 -0400

    Add Variables(const VectorX<Variable>& init)

--- drake/common/test/symbolic_variables_test.cc
+++ drake/common/test/symbolic_variables_test.cc
@@ -2,6 +2,7 @@
 
 #include <gtest/gtest.h>
 
+#include "drake/common/eigen_types.h"
 #include "drake/common/symbolic_variable.h"
 
 namespace drake {
@@ -17,6 +18,23 @@ class VariablesTest : public ::testing::Test {
   const Variable w_{"w"};
   const Variable v_{"v"};
 };
+
+TEST_F(VariablesTest, ConstructFromVariableVector) {
+  const Eigen::Matrix<symbolic::Variable, 3, 1> v1(x_, y_, z_);
+  const Variables vars1(v1);  // vars1 = {x, y_, z}
+  EXPECT_EQ(vars1.size(), 3u);
+  EXPECT_TRUE(vars1.include(x_));
+  EXPECT_TRUE(vars1.include(y_));
+  EXPECT_TRUE(vars1.include(z_));
+
+  VectorX<Variable> v2(3);
+  v2 << x_, x_, y_;
+  const Variables vars2(v2);  // vars2 = {x, y}
+  EXPECT_EQ(vars2.size(), 2u);
+  EXPECT_TRUE(vars2.include(x_));
+  EXPECT_TRUE(vars2.include(y_));
+  EXPECT_FALSE(vars2.include(z_));
+}
 
 TEST_F(VariablesTest, HashEq) {
   const Variables vars1{x_, y_, z_};
