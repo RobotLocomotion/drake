b94552f9bf43b875b004ee46a51664eb8164ca04
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-06-29 20:01:00 -0400

    RunSolver should always run the solver

--- drake/solvers/test/mathematical_program_test_util.cc
+++ drake/solvers/test/mathematical_program_test_util.cc
@@ -1,5 +1,7 @@
 #include "drake/solvers/test/mathematical_program_test_util.h"
 
+#include <stdexcept>
+
 namespace drake {
 namespace solvers {
 namespace test {
@@ -11,13 +13,21 @@ void CheckSolver(const MathematicalProgram& prog,
   EXPECT_EQ(solver_type, desired_solver_type);
 }
 
+// If the solver is absent or does not find a solution, stop immediately.
+// (Were we to continue, testing statements that examine the results would be
+// likely to fail with confusing messages, so best to avoid them entirely.)
 void RunSolver(MathematicalProgram* prog,
                const MathematicalProgramSolverInterface& solver) {
-  if (solver.available()) {
-    SolutionResult result = solver.Solve(*prog);
-    EXPECT_EQ(result, SolutionResult::kSolutionFound)
-        << "Solver " << solver.solver_id().name()
-        << " fails to find the solution." << std::endl;
+  if (!solver.available()) {
+    throw std::runtime_error(
+        "Solver " + solver.solver_id().name() + " is not available");
+  }
+
+  SolutionResult result = solver.Solve(*prog);
+  EXPECT_EQ(result, SolutionResult::kSolutionFound);
+  if (result != SolutionResult::kSolutionFound) {
+    throw std::runtime_error(
+        "Solver " + solver.solver_id().name() + " fails to find the solution");
   }
 }
 }  // namespace test
