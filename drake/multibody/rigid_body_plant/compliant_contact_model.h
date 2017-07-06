374216f51da0da08b713922629a08ae34c1430b9
naveenoid <naveen.kuppuswamy@tri.global>
2017-07-06 12:14:39 -0400

    comment block mods

--- drake/multibody/rigid_body_plant/compliant_contact_model.h
+++ drake/multibody/rigid_body_plant/compliant_contact_model.h
@@ -34,6 +34,9 @@ class CompliantContactModel {
 
   // TODO(SeanCurtis-TRI): Link to documentation explaining these parameters
   // in detail.  To come in a subsequent PR.
+  // TODO(Naveenoid): The following two methods both have argument lists with
+  // the same type. This needs resolution in the future GeometryWorld
+  // architecture.
   /// Sets only the parameters for *normal* contact.  This is a convenience
   /// function to allow for more targeted parameter tuning.
   void set_normal_contact_parameters(double penetration_stiffness,
@@ -56,14 +59,14 @@ class CompliantContactModel {
   // range [0, 1] where the f''(0) = f''(1) = f'(0) = f'(1) = 0.
   static T step5(T x);
 
-  /// Creates a right-handed local basis from a z-axis. Defines an arbitrary x-
-  /// and y-axis such that the basis is orthonormal. The basis is R_WL, where W
-  /// is the frame in which the z-axis is expressed and L is a local basis such
-  /// that v_W = R_WL * v_L.
-  ///
-  /// @param[in] z_axis_W   The vector defining the basis's z-axis expressed
-  ///                       in frame W.
-  /// @retval R_WL          The computed basis.
+  // Creates a right-handed local basis from a z-axis. Defines an arbitrary x-
+  // and y-axis such that the basis is orthonormal. The basis is R_WL, where W
+  // is the frame in which the z-axis is expressed and L is a local basis such
+  // that v_W = R_WL * v_L.
+  //
+  // @param[in] z_axis_W   The vector defining the basis's z-axis expressed
+  //                       in frame W.
+  // @retval R_WL          The computed basis.
   static Matrix3<T> ComputeBasisFromZ(const Vector3<T>& z_axis_W);
 
   // Some parameters defining the contact.
