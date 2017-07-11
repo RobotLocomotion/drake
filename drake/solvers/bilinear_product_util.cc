a0cb6f38b1769a74979a61d7e58d853e27bf5b92
hongkai-dai <daihongkai@gmail.com>
2017-07-11 16:45:01 -0400

    simplify the ReplaceBilinearTerms function, get rid of dependent/independent monomial

--- drake/solvers/bilinear_product_util.cc
+++ drake/solvers/bilinear_product_util.cc
@@ -2,20 +2,27 @@
 
 #include <ostream>
 #include <sstream>
+#include <unordered_map>
+
 namespace drake {
 namespace solvers {
-std::unordered_map<symbolic::Variable::Id, int> ConstructVarToIndexMap(const Eigen::Ref<const VectorXDecisionVariable>& x) {
+/*
+ * Returns the map that maps x(i).get_id() to i.
+ */
+std::unordered_map<symbolic::Variable::Id, int> ConstructVarToIndexMap(
+    const Eigen::Ref<const VectorXDecisionVariable>& x) {
   std::unordered_map<symbolic::Variable::Id, int> map;
   map.reserve(x.rows());
   for (int i = 0; i < x.rows(); ++i) {
     auto it = map.find(x(i).get_id());
     if (it != map.end()) {
-      throw std::runtime_error("Input vector contains duplicate variable " + x(i).get_name());
+      throw std::runtime_error("Input vector contains duplicate variable " +
+                               x(i).get_name());
     }
     map.emplace_hint(it, x(i).get_id(), i);
   }
   return map;
-};
+}
 
 symbolic::Expression ReplaceBilinearTerms(
     const symbolic::Expression& e,
@@ -23,9 +30,12 @@ symbolic::Expression ReplaceBilinearTerms(
     const Eigen::Ref<const VectorXDecisionVariable>& y,
     const Eigen::Ref<const MatrixXDecisionVariable>& W) {
   DRAKE_ASSERT(W.rows() == x.rows() && W.cols() == y.rows());
-  const std::unordered_map<symbolic::Variable::Id, int> x_to_index_map = ConstructVarToIndexMap(x);
-  const std::unordered_map<symbolic::Variable::Id, int> y_to_index_map = ConstructVarToIndexMap(y);
+  const std::unordered_map<symbolic::Variable::Id, int> x_to_index_map =
+      ConstructVarToIndexMap(x);
+  const std::unordered_map<symbolic::Variable::Id, int> y_to_index_map =
+      ConstructVarToIndexMap(y);
 
+  // vars contains all the variables in x and y.
   symbolic::Variables vars{};
   for (int i = 0; i < x.rows(); ++i) {
     vars.insert(x(i));
@@ -39,25 +49,16 @@ symbolic::Expression ReplaceBilinearTerms(
   symbolic::Polynomial::MapType map_replaced;
   map_replaced.reserve(map_bilinear.size());
   for (const auto& p : map_bilinear) {
-    std::map<symbolic::Variable, int> monomial_replace_map;
-    // For each monomial, we decompose it into two parts, the part that contains
-    // variables in `x` or `y`, and the part that does not. We call the first
-    // part as "dependent monomial", the second part as "independent monomial".
-    std::map<symbolic::Variable, int> dependent_monomial_map;
-    std::map<symbolic::Variable, int> independent_monomial_map;
+    std::unordered_map<symbolic::Variable, int, hash_value<symbolic::Variable>> monomial_map;
     int xy_total_degree{0};
     for (const auto& var_power : p.first.get_powers()) {
-      const symbolic::Variable& var{var_power.first};
-      if (x_to_index_map.find(var.get_id()) != x_to_index_map.end() || y_to_index_map.find(var.get_id()) != y_to_index_map.end()) {
-        dependent_monomial_map.emplace(var_power.first, var_power.second);
-        xy_total_degree += var_power.second;
-      } else {
-        independent_monomial_map.emplace(var_power.first, var_power.second);
-      }
+      monomial_map.emplace(var_power.first, var_power.second);
+      xy_total_degree += var_power.second;
     }
     if (xy_total_degree > 2) {
       std::ostringstream oss;
-      oss << "The term " << p.first << " has degree larger than 2 on the variables";
+      oss << "The term " << p.first
+          << " has degree larger than 2 on the variables";
       throw std::runtime_error(oss.str());
     } else if (xy_total_degree < 2) {
       // Only linear or constant terms, do not need to replace the variables.
@@ -71,48 +72,46 @@ symbolic::Expression ReplaceBilinearTerms(
       // This monomial contains bilinear term in x and y.
       std::unordered_map<symbolic::Variable::Id, int>::const_iterator it_x_idx;
       std::unordered_map<symbolic::Variable::Id, int>::const_iterator it_y_idx;
-      if (dependent_monomial_map.size() == 2) {
-        // The dependent monomial is in the form of x * y, namely two different
+      if (monomial_map.size() == 2) {
+        // The monomial is in the form of x * y, namely two different
         // variables multiplying together.
-        auto dependent_monomial_map_it = dependent_monomial_map.begin();
-        const symbolic::Variable &var1{dependent_monomial_map_it->first};
-        ++dependent_monomial_map_it;
-        const symbolic::Variable &var2{dependent_monomial_map_it->first};
+        auto monomial_map_it = monomial_map.begin();
+        const symbolic::Variable &var1{monomial_map_it->first};
+        ++monomial_map_it;
+        const symbolic::Variable &var2{monomial_map_it->first};
         it_x_idx = x_to_index_map.find(var1.get_id());
         if (it_x_idx != x_to_index_map.end()) {
+          // var1 is in x.
           it_y_idx = y_to_index_map.find(var2.get_id());
         } else {
+          // var1 is in y.
           it_x_idx = x_to_index_map.find(var2.get_id());
           it_y_idx = y_to_index_map.find(var1.get_id());
         }
       } else {
-        // The dependent monomial is in the form of x * x, the square of a variable.
-        const symbolic::Variable& squared_var{dependent_monomial_map.begin()->first};
+        // The monomial is in the form of x * x, the square of a variable.
+        const symbolic::Variable& squared_var{
+            monomial_map.begin()->first};
         it_x_idx = x_to_index_map.find(squared_var.get_id());
         it_y_idx = y_to_index_map.find(squared_var.get_id());
       }
-      if (it_x_idx == x_to_index_map.end() || it_y_idx == y_to_index_map.end()) {
+      if (it_x_idx == x_to_index_map.end() ||
+          it_y_idx == y_to_index_map.end()) {
+        // This error would happen, if we ask
+        // ReplaceBilinearTerms(x(i) * x(j), x, y, W).
         std::ostringstream oss;
-        oss << "Term " << p.first << " is bilinear, but x and y does not have the corresponding variables.";
+        oss << "Term " << p.first << " is bilinear, but x and y does not have "
+                                     "the corresponding variables.";
         throw std::runtime_error(oss.str());
       }
       // w_xy is the symbolic variable representing the bilinear term x * y.
       const symbolic::Variable& w_xy{W(it_x_idx->second, it_y_idx->second)};
-      // Multiply w_xy with the independent monomial
-      auto it_w_in_independent_monomial = independent_monomial_map.find(w_xy);
-      if (it_w_in_independent_monomial != independent_monomial_map.end()) {
-        ++(it_w_in_independent_monomial->second);
-      } else {
-        independent_monomial_map.emplace_hint(it_w_in_independent_monomial, w_xy, 1);
-      }
-      map_replaced.emplace(symbolic::Monomial{independent_monomial_map}, p.second);
+      map_replaced.emplace(symbolic::Monomial{w_xy},
+                           p.second);
     }
   }
-  symbolic::Expression poly_replaced{0};
-  for (const auto& p_replaced : map_replaced) {
-    poly_replaced += p_replaced.first.ToExpression() * p_replaced.second;
-  }
-  return poly_replaced;
+  symbolic::Polynomial poly_replaced{map_replaced};
+  return poly_replaced.ToExpression();
 }
 }  // namespace solvers
 }  // namespace drake
--- drake/solvers/test/bilinear_product_util_test.cc
+++ drake/solvers/test/bilinear_product_util_test.cc
@@ -17,13 +17,15 @@ class BilinearProductTest : public ::testing::Test {
         z_{Variable{"z0"}, Variable{"z1"}} {
     for (int i = 0; i < 3; ++i) {
       for (int j = 0; j < 4; ++j) {
-        xy_(i, j) = symbolic::Variable("xy(" + std::to_string(i) + "," + std::to_string(j) + ")");
+        xy_(i, j) = symbolic::Variable("xy(" + std::to_string(i) + "," +
+                                       std::to_string(j) + ")");
       }
     }
 
     for (int i = 0; i < 3; ++i) {
       for (int j = i; j < 3; ++j) {
-        xx_(i, j) = symbolic::Variable("xx(" + std::to_string(i) + "," + std::to_string(j) + ")");
+        xx_(i, j) = symbolic::Variable("xx(" + std::to_string(i) + "," +
+                                       std::to_string(j) + ")");
       }
     }
     for (int i = 0; i < 3; ++i) {
@@ -32,6 +34,7 @@ class BilinearProductTest : public ::testing::Test {
       }
     }
   }
+
  protected:
   VectorDecisionVariable<3> x_;
   VectorDecisionVariable<4> y_;
@@ -100,8 +103,10 @@ TEST_F(BilinearProductTest, QuadraticTerm1) {
   e_expected = 2 * y_(0) * y_(1) * xx_(1, 2);
   EXPECT_PRED2(ExprEqual, e_expected, ReplaceBilinearTerms(e, x_, x_, xx_));
 
-  e = 2 * pow(y_(0), 3) * x_(1) * x_(2) + y_(1) * x_(1) * x_(1) + 2 * x_(0) + 3 + 3 * x_(1);
-  e_expected = 2 * pow(y_(0), 3) * xx_(1, 2) + y_(1) * xx_(1, 1) + 2 * x_(0) + 3 + 3 * x_(1);
+  e = 2 * pow(y_(0), 3) * x_(1) * x_(2) + y_(1) * x_(1) * x_(1) + 2 * x_(0) +
+      3 + 3 * x_(1);
+  e_expected = 2 * pow(y_(0), 3) * xx_(1, 2) + y_(1) * xx_(1, 1) + 2 * x_(0) +
+               3 + 3 * x_(1);
   EXPECT_PRED2(ExprEqual, e_expected, ReplaceBilinearTerms(e, x_, x_, xx_));
 
   e = (2 + y_(0) + y_(0) * y_(1)) * x_(0) * x_(1);
@@ -122,27 +127,38 @@ TEST_F(BilinearProductTest, QuadraticTerm2) {
   e_expected = 2 * z_(0) * z_(1) * z_(1) * xy_(0, 2);
   EXPECT_PRED2(ExprEqual, e_expected, ReplaceBilinearTerms(e, x_, y_, xy_));
 
-  e = 2 * pow(z_(1), 3) * x_(0) * y_(2) + z_(0) * x_(1) * y_(1) + (4 * z_(0) + z_(1)) * x_(2) * y_(3) + 3 * x_(2) + 4;
-  e_expected = 2 * pow(z_(1), 3) * xy_(0, 2) + z_(0) * xy_(1, 1) + (4 * z_(0) + z_(1)) * xy_(2, 3) + 3 * x_(2) + 4;
-  EXPECT_PRED2(ExprEqual, e_expected.Expand(), ReplaceBilinearTerms(e, x_, y_, xy_).Expand());
+  e = 2 * pow(z_(1), 3) * x_(0) * y_(2) + z_(0) * x_(1) * y_(1) +
+      (4 * z_(0) + z_(1)) * x_(2) * y_(3) + 3 * x_(2) + 4;
+  e_expected = 2 * pow(z_(1), 3) * xy_(0, 2) + z_(0) * xy_(1, 1) +
+               (4 * z_(0) + z_(1)) * xy_(2, 3) + 3 * x_(2) + 4;
+  EXPECT_PRED2(ExprEqual, e_expected.Expand(),
+               ReplaceBilinearTerms(e, x_, y_, xy_).Expand());
 }
 
 TEST_F(BilinearProductTest, HigherOrderTest) {
-  EXPECT_THROW(ReplaceBilinearTerms(pow(x_(0), 3), x_, x_, xx_), std::runtime_error);
+  EXPECT_THROW(ReplaceBilinearTerms(pow(x_(0), 3), x_, x_, xx_),
+               std::runtime_error);
 
-  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * x_(0) * x_(1), x_, x_, xx_), std::runtime_error);
+  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * x_(0) * x_(1), x_, x_, xx_),
+               std::runtime_error);
 
-  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * x_(1) * x_(2), x_, x_, xx_), std::runtime_error);
+  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * x_(1) * x_(2), x_, x_, xx_),
+               std::runtime_error);
 
-  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * x_(0), x_, y_, xy_), std::runtime_error);
+  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * x_(0), x_, y_, xy_),
+               std::runtime_error);
 
-  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * x_(1), x_, y_, xy_), std::runtime_error);
+  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * x_(1), x_, y_, xy_),
+               std::runtime_error);
 
-  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * x_(1) * x_(1), x_, y_, xy_), std::runtime_error);
+  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * x_(1) * x_(1), x_, y_, xy_),
+               std::runtime_error);
 
-  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * y_(1) * y_(2), x_, y_, xy_), std::runtime_error);
+  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * y_(1) * y_(2), x_, y_, xy_),
+               std::runtime_error);
 
-  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * y_(2) * y_(2), x_, y_, xy_), std::runtime_error);
+  EXPECT_THROW(ReplaceBilinearTerms(x_(0) * y_(2) * y_(2), x_, y_, xy_),
+               std::runtime_error);
 }
 }  // namespace
 }  // namespace solvers
