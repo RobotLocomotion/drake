49a153d1ef6c1f7258489462119c9beb9e73ea9a
Russ Tedrake <russt@mit.edu>
2017-07-24 06:45:53 -0400

    PR2 => pr2

--- drake/bindings/python/pydrake/test/test_urdf_parser.py
+++ drake/bindings/python/pydrake/test/test_urdf_parser.py
@@ -12,7 +12,7 @@ class TestUrdfParser(unittest.TestCase):
     """
     def testAddModelInstanceFromUrdfStringSearchingInRosPackages(self):
         urdf_file = os.path.join(pydrake.getDrakePath(),
-                                 "examples/PR2/pr2.urdf")
+                                 "examples/pr2/pr2.urdf")
         urdf_string = open(urdf_file).read()
         base_dir = os.path.dirname(urdf_file)
         package_map = pydrake.rbtree.PackageMap()
