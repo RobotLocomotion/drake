268fd9d1a596c9255fa5aca8b20d9708e961735e
Evan Drumwright <drum@tri.global>
2017-07-24 04:41:11 -0700

    Updated rigid contact point definition

--- drake/multibody/rigid_contact/rigid_contact_point.h
+++ drake/multibody/rigid_contact/rigid_contact_point.h
@@ -9,15 +9,14 @@ namespace drake {
 namespace multibody {
 namespace rigid_contact {
 
-/// A tracked point of contact between two rigid bodies that use polytopical
-/// geometries. The point is defined with respect to exactly one of the two
-/// rigid bodies. This point would ideally be used to define constraints only if
-/// the distance from it to the second rigid body were zero; floating point
-/// truncation and rounding errors imply that such distances are unlikely to
-/// ever actually be zero. Instead, we track the point of contact over time and
-/// use ancillary information (e.g., are Lagrange Multipliers zero?) and
-/// store the contact state (sliding, not sliding) to compose necessary
-/// constraint equations.
+/// A tracked point of contact between two rigid bodies. This point, p, would
+/// ideally be used to define constraints at time t if and only if the distance
+/// between the rigid bodies at p(t) were zero; floating point truncation and
+/// rounding errors mean that such distances are unlikely to ever actually be
+/// zero. Incorporating tracked points of contact (as well as the sliding
+/// state- sliding or not sliding) into a System's abstract state and using
+/// ancillary information (e.g., zero Lagrange Multipliers) allows avoiding
+/// these floating point issues.
 struct RigidContactPoint {
   enum class ContactSlidingState {
     /// The bodies are to be considered as sliding at this point of contact.
@@ -30,7 +29,8 @@ struct RigidContactPoint {
   /// Whether the contact is to be treated as sliding or not sliding.
   ContactSlidingState sliding_state;
 
-  /// Abstract data sufficient to uniquely determine a point of contact between
+  /// Abstract data sufficient (in combination with other continuous and/or
+  /// discrete state variables) to uniquely determine a point of contact between
   /// two rigid bodies.
   AbstractValue contact_point_key;
 };
