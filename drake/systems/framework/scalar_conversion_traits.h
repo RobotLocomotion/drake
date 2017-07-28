2eede95e52b7aeb68e07fdba733ced4ac7be71c7
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-07-28 16:30:28 -0400

    Add SystemScalarConverter
    This introduces the concept of a scalar-type converting copy constructor.
    This will eventually replace the prior System design of "override one NVI
    method for each supported target scalar type".
    
    The converter supports converting from any (supported) scalar type to any
    other (supported) scalar type, and a Traits class allows specialization to
    disable unwanted conversions at compile-time (e.g., we can allow AutoDiff
    to Symbolic but disallow the opposite direction).
    
    This commit only introduces the converter infrastructure; it does not yet
    weave it into the System classes.

---
+++ drake/systems/framework/scalar_conversion_traits.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <type_traits>
+
+#include "drake/common/drake_compat.h"
+
+namespace drake {
+namespace systems {
+namespace scalar_conversion {
+
+/// A templated traits class for whether an S<T> can be converted into an S<U>;
+/// the default value is true for all values of S, T, and U.  Particular
+/// scalar-dependent classes may specialize this template to indicate whether
+/// the framework should support conversion for a combination of S, T, and U.
+///
+/// In supported cases, the "scalar-converting copy constructor" for those
+/// types will be used.  That constructor takes the form of, e.g.:
+///
+/// @code
+/// template <typename T>
+/// class Foo {
+///   template <typename U>
+///   explicit Foo(const Foo<U>& other);
+/// };
+/// @endcode
+///
+/// In unsupported cases, the constructor will not even be mentioned by the
+/// framework, so that S need not even compile for certain values of T and U.
+///
+/// @tparam S is the scalar-templated type to copy
+template <template <typename> class S>
+struct Traits {
+  /// @tparam T is the resulting scalar type (to convert into)
+  /// @tparam U is the donor scalar type (to convert from)
+  template <typename T, typename U>
+  using supported = std::true_type;
+};
+
+/// A concrete traits class providing sugar to disable support for symbolic
+/// evaluation (i.e., the symbolic::Expression scalar type).
+///
+/// For example, if MySystem does not support the symbolic expression scalar
+/// type, it could specialize Traits as follows:
+///
+/// @code
+/// namespace drake {
+/// namespace systems {
+/// namespace scalar_conversion {
+/// template <> struct Traits<MySystem> : public NonSymbolicTraits {};
+/// }  // namespace scalar_conversion
+/// }  // namespace systems
+/// }  // namespace drake
+/// @endcode
+struct NonSymbolicTraits {
+  template <typename T, typename U>
+  using supported = typename std::conditional<
+    !std::is_same<T, symbolic::Expression>::value &&
+    !std::is_same<U, symbolic::Expression>::value,
+    std::true_type, std::false_type>::type;
+};
+
+/// A concrete traits class providing sugar to support for converting only from
+/// the `double` scalar type.  For example, if a MySystem<symbolic::Expression>
+/// cannot be converted into a MySystem<double>, it could specialize Traits as
+/// follows:
+///
+/// @code
+/// namespace drake {
+/// namespace systems {
+/// namespace scalar_conversion {
+/// template <> struct Traits<MySystem> : public FromDoubleTraits {};
+/// }  // namespace scalar_conversion
+/// }  // namespace systems
+/// }  // namespace drake
+/// @endcode
+struct FromDoubleTraits {
+  template <typename T, typename U>
+  using supported = typename std::conditional<
+    std::is_same<U, double>::value,
+    std::true_type, std::false_type>::type;
+};
+
+}  // namespace scalar_conversion
+}  // namespace systems
+}  // namespace drake
