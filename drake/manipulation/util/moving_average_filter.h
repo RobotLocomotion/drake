27847e65df2f87574db828792861538d610f66d7
Naveen Kuppuswamy <naveen.sk@gmail.com>
2017-07-28 15:23:56 -0400

    MovingAverageFilter (#6689)

--- drake/manipulation/util/moving_average_filter.h
+++ drake/manipulation/util/moving_average_filter.h
@@ -7,15 +7,14 @@
 #include "drake/common/drake_copyable.h"
 
 namespace drake {
-namespace examples {
-namespace kuka_iiwa_arm {
-namespace tools {
+namespace manipulation {
+namespace util {
 
 /**
  * The implementation of a Moving Average Filter. This discrete time filter
  * outputs the average of the last n samples i.e.
- *  y(k) = 1/n ∑ⱼ x(k-j) ∀ j = 0..n-1, when n<k and,
- *       = 1/k ∑ⱼ x(j) ∀ j = 0..k otherwise;
+ *  y[k] = 1/n ∑ⱼ x[k-j] ∀ j = 0..n-1, when n<k and,
+ *       = 1/k ∑ⱼ x[j] ∀ j = 0..k otherwise;
  * where n is the window size and x being the discrete-time signal that is
  * to be filtered, y is the filtered signal and k is the index of latest
  * element in the signal time-series.
@@ -27,7 +26,7 @@ namespace tools {
  * @tparam T The element type.
  * Instantiated templates for the following kinds of T's are provided:
  *  - double
- *  - Eigen::Array3d
+ *  - VectorX<double>
  */
 template <typename T>
 class MovingAverageFilter {
@@ -35,28 +34,27 @@ class MovingAverageFilter {
   DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(MovingAverageFilter)
   /**
    * Constructs the filter with the specified `window_size`.
-   * @param window_size The size of the window which must be greater than
-   * 0.
+   * @param window_size The size of the window.
    * @throws a std::runtime_error when window_size <= 0.
    */
   explicit MovingAverageFilter(int window_size);
 
   /**
-   * Computes the average filter result. Every call to this method modifies
+   * Updates the average filter result. Every call to this method modifies
    * the internal state of this filter thus resulting in a computation of
+   * the moving average of the data present within the filter window.
    *
    * @param new_data
    * @return
    */
-  T Compute(const T& new_data);
+  T Update(const T& new_data);
 
  private:
   std::queue<T> window_;
-  size_t window_size_{0};
+  int window_size_{0};
   T sum_;
 };
 
-}  // namespace tools
-}  // namespace kuka_iiwa_arm
-}  // namespace examples
+}  // namespace util
+}  // namespace manipulation
 }  // namespace drake
