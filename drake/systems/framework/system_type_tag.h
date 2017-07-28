e78191d945b7fca0e0418745c914208cf2e6c158
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-07-28 09:32:47 -0400

    Add SystemScalarConverter
    This introduces the concept of a scalar-type converting copy constructor.
    This will eventually replace the prior System design of "override one NVI
    method for each supported target scalar type".
    
    The converter supports converting from any (supported) scalar type to any
    other (supported) scalar type, and a Traits class allows specialization to
    disable unwanted conversions at compile-time (e.g., we can allow AutoDiff
    to Symbolic but disallow the opposite direction).
    
    This commit only introduces the converter infrastructure; it does not yet
    weave it into the System classes.

---
+++ drake/systems/framework/system_type_tag.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "drake/systems/framework/system.h"
+
+namespace drake {
+namespace systems {
+
+/// A tag object that denotes a System subclass `S` in function signatures.
+///
+/// For example, `SystemTypeTag<MySystem>{}` will create a dummy object that
+/// can be used to call functions that that look like:
+///
+/// @code
+/// template <template <typename> class S>
+/// const char* get_name(SystemTypeTag<S>) { return S<double>::get_name(); }
+///
+/// int main() {
+///    std::cout << get_name(SystemTypeTag<MySystem>{});
+/// }
+/// @endcode
+///
+/// In this case, we could directly call get_name<MySystem>() by specifying the
+/// template argument, but that is not always possible.  In particular, tag
+/// objects are acutely useful when calling templated constructors, because
+/// there is no other mechanism for the caller to specify the template type.
+template <template <typename> class S>
+struct SystemTypeTag {
+  SystemTypeTag() {
+    static_assert(std::is_base_of<System<double>, S<double>>::value,
+                  "The type argument to SystemTypeTag must be a System");
+  }
+};
+
+}  // namespace systems
+}  // namespace drake
