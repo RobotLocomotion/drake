6d9ebaa256a90656d982e784840901577aaf7013
mitiguy <paul.mitiguy@tri.global>
2017-07-31 18:28:29 -0700

    Update0: Initial effort for inverse dynamics test

--- drake/multibody/benchmarks/kuka_iiwa_robot/test/MG_kuka_iiwa_robot.h
+++ drake/multibody/benchmarks/kuka_iiwa_robot/test/MG_kuka_iiwa_robot.h
@@ -5,6 +5,7 @@
 
 #include "drake/common/drake_copyable.h"
 #include "drake/common/eigen_types.h"
+// #include "drake/multibody/multibody_tree/math/spatial_force.h"
 
 namespace drake {
 namespace multibody {
@@ -13,6 +14,7 @@ namespace kuka_iiwa_robot {
 
 using Eigen::Vector3d;
 using Eigen::Matrix3d;
+typedef SpatialForce<double> SpatialForced;
 
 /// This class is Drake's interface to the MotionGenesis solution for a
 /// 7-DOF KUKA LBR iiwa robot (14 kg payload) which is described at:
@@ -54,6 +56,33 @@ class MGKukaIIwaRobot {
   CalcEndEffectorKinematics(const Eigen::Ref<const VectorX<T>>& q,
                             const Eigen::Ref<const VectorX<T>>& qDt,
                             const Eigen::Ref<const VectorX<T>>& qDDt) const;
+
+  /// This method calculates joint reaction forces for each of the 7 revolute
+  /// motors that connect the rigid bodies in a 7-DOF KUKA LBR iiwa robot.
+  /// Right-handed orthogonal unit vectors Nx, Ny, Nz are fixed in N (Earth)
+  /// with Nz vertically upward.  Ground body (N) is connected to the first
+  /// robot link A.  Similarly, link A connects to link B, B to C, ..., F to G.
+  ///
+  /// @param[in] q robot's joint angles (generalized coordinates).
+  /// @param[in] qDt 1st-time-derivatives of q (q̇).
+  /// @param[in] qDDt 2nd-time-derivatives of q (q̈).
+  ///
+  /// @returns Machine-precision values as defined below.
+  ///
+  /// std::tuple | Description
+  /// -----------|-------------------------------------------------
+  /// F_A_Na     | Spatial force on A from N, expressed in mobilizer frame Na.
+  /// F_B_Ab     | Spatial force on B from A, expressed in mobilizer frame Ab.
+  /// F_C_Bc     | Spatial force on C from B, expressed in mobilizer frame Bc.
+  /// F_D_Cd     | Spatial force on D from C, expressed in mobilizer frame Cd.
+  /// F_E_De     | Spatial force on E from D, expressed in mobilizer frame De.
+  /// F_F_Ef     | Spatial force on F from E, expressed in mobilizer frame Ef.
+  /// F_G_Fg     | Spatial force on G from F, expressed in mobilizer frame Fg.
+  std::tuple<SpatialForced, SpatialForced, SpatialForced, SpatialForced,
+             SpatialForced, SpatialForced, SpatialForced>
+  CalcJointReactionForces(const Eigen::Ref<const VectorX<T>>& q,
+                          const Eigen::Ref<const VectorX<T>>& qDt,
+                          const Eigen::Ref<const VectorX<T>>& qDDt) const;
 };
 
 }  // namespace kuka_iiwa_robot
