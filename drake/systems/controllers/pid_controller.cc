69ce66ee6dd48b80c198f460e7f450e99e238487
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-08-02 10:51:11 -0400

    Port PidController to SystemScalarConverter
    This adds AutoDiffXd support as a side-effect.

--- drake/systems/controllers/pid_controller.cc
+++ drake/systems/controllers/pid_controller.cc
@@ -2,6 +2,10 @@
 
 #include <string>
 
+#include "drake/common/autodiff_overloads.h"
+#include "drake/common/eigen_autodiff_types.h"
+#include "drake/common/symbolic.h"
+
 namespace drake {
 namespace systems {
 namespace controllers {
@@ -18,9 +22,10 @@ PidController<T>::PidController(const MatrixX<double>& state_selector,
                                 const Eigen::VectorXd& kp,
                                 const Eigen::VectorXd& ki,
                                 const Eigen::VectorXd& kd)
-    : kp_(kp),
-      kd_(kd),
+    : LeafSystem<T>(SystemTypeTag<controllers::PidController>{}),
+      kp_(kp),
       ki_(ki),
+      kd_(kd),
       num_controlled_q_(kp.size()),
       num_full_state_(state_selector.cols()),
       state_selector_(state_selector) {
@@ -41,6 +46,15 @@ PidController<T>::PidController(const MatrixX<double>& state_selector,
   input_index_desired_state_ =
       this->DeclareInputPort(kVectorValued, 2 * num_controlled_q_).get_index();
 }
+
+template <typename T>
+template <typename U>
+PidController<T>::PidController(const PidController<U>& other)
+    : PidController(
+          other.state_selector_,
+          other.kp_,
+          other.ki_,
+          other.kd_) {}
 
 template <typename T>
 void PidController<T>::DoCalcTimeDerivatives(
@@ -82,12 +96,6 @@ void PidController<T>::CalcControl(const Context<T>& context,
       (kd_.array() * controlled_state_diff.tail(num_controlled_q_).array())
           .matrix() +
       (ki_.array() * state_block.array()).matrix());
-}
-
-template <typename T>
-PidController<symbolic::Expression>* PidController<T>::DoToSymbolic() const {
-  return new PidController<symbolic::Expression>(state_selector_, kp_, ki_,
-                                                 kd_);
 }
 
 // Adds a simple record-based representation of the PID controller to @p dot.
