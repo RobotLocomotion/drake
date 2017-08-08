b08aaa945e7c74db0d83aaa8250768cdc73fc09a
Soonho Kong <soonho.kong@tri.global>
2017-08-08 11:05:13 -0400

    Add drake/common:symbolic_decompose

---
+++ drake/common/symbolic_decompose.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "drake/common/eigen_types.h"
+#include "drake/common/symbolic.h"
+
+// TODO(soonho-tri): Migrate the functions in drake/solvers:symbolic_extract to
+// this file.
+
+namespace drake {
+namespace symbolic {
+
+/// Decomposes @p expressions into @p M * @p vars.
+///
+/// @throws runtime_error if @p expressions is not linear in @p vars.
+/// @pre M.rows() == expressions.rows() && M.cols() == vars.rows().
+void DecomposeLinearExpressions(
+    const Eigen::Ref<const VectorX<Expression>>& expressions,
+    const Eigen::Ref<const VectorX<Variable>>& vars,
+    Eigen::Ref<Eigen::MatrixXd> M);
+
+/// Decomposes @p expressions into @p M * @p vars + @p v.
+///
+/// @throws runtime_error if @p expressions is not affine in @p vars.
+/// @pre M.rows() == expressions.rows() && M.cols() == vars.rows().
+/// @pre v.rows() == expressions.rows().
+void DecomposeAffineExpressions(
+    const Eigen::Ref<const VectorX<Expression>>& expressions,
+    const Eigen::Ref<const VectorX<Variable>>& vars,
+    Eigen::Ref<Eigen::MatrixXd> M, Eigen::Ref<Eigen::VectorXd> v);
+
+}  // namespace symbolic
+}  // namespace drake
