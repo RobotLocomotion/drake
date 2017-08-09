21e36783e5fab07162588ae217a0a1c9678b90c7
mitiguy <mitiguy@users.noreply.github.com>
2017-08-09 12:14:47 -0700

    Kuka iiwa robot inverse dynamics test  (#6726)
    Created Kuka iiwa robot benchmark test for inverse dynamics,
    using MotionGenesis (MG).

---
+++ drake/multibody/benchmarks/kuka_iiwa_robot/MG/MG_kuka_iiwa_robot.cc
@@ -0,0 +1,118 @@
+#include "drake/multibody/benchmarks/kuka_iiwa_robot/MG/MG_kuka_iiwa_robot.h"
+
+namespace drake {
+namespace multibody {
+namespace benchmarks {
+namespace kuka_iiwa_robot {
+namespace MG {
+
+using Eigen::Map;
+using Eigen::Matrix;
+
+template<typename T>
+void MGKukaIIwaRobot<T>::PrepareMGOutput(
+    const Eigen::Ref<const VectorX<T>>& q,
+    const Eigen::Ref<const VectorX<T>>& qDt,
+    const Eigen::Ref<const VectorX<T>>& qDDt) const {
+  // Form a continuous vector of joint angles and their time-derivatives.
+  const Eigen::Index size = q.size() + qDt.size() + qDDt.size();
+  VectorX<double> joint_angles_and_1st_2nd_derivatives(size);
+  joint_angles_and_1st_2nd_derivatives << q, qDt, qDDt;
+
+  // Calculate MotionGenesis output quantities.
+  MG_kuka_auto_generated_.SetVariablesFromArray(
+        joint_angles_and_1st_2nd_derivatives.data());
+  MG_kuka_auto_generated_.CalculateOutput();
+}
+
+
+template<typename T>
+std::tuple<Matrix3d, Vector3d, Vector3d, Vector3d, Vector3d, Vector3d>
+MGKukaIIwaRobot<T>::CalcEndEffectorKinematics(
+    const Eigen::Ref<const VectorX<T>>& q,
+    const Eigen::Ref<const VectorX<T>>& qDt,
+    const Eigen::Ref<const VectorX<T>>& qDDt) const {
+  // Calculate forward kinematics of end-effector with MotionGenesis.
+  PrepareMGOutput(q, qDt, qDDt);
+
+  // Convert MotionGenesis standard C++ matrix to an Eigen matrix.
+  // Note: Standard C++ stores two-dimensional matrices in row-major form.
+  // By default, Eigen stores two-dimensional matrices in column-major form.
+  // After an Eigen matrix is constructed from the C++ matrix (interpreting the
+  // C++ matrix in row-order), column-major R_NG is populated via operator=.
+  const double* matrix33 = MG_kuka_auto_generated_.R_NG[0];
+  const Matrix3d R_NG = Matrix<double, 3, 3, Eigen::RowMajor>(matrix33);
+
+  const Vector3d p_NoGo_N(MG_kuka_auto_generated_.p_NoGo_N);
+  const Vector3d w_NG_N(MG_kuka_auto_generated_.w_NG_N);
+  const Vector3d v_NGo_N(MG_kuka_auto_generated_.v_NGo_N);
+  const Vector3d alpha_NG_N(MG_kuka_auto_generated_.alpha_NG_N);
+  const Vector3d a_NGo_N(MG_kuka_auto_generated_.a_NGo_N);
+
+  return std::make_tuple(R_NG, p_NoGo_N, w_NG_N, v_NGo_N, alpha_NG_N, a_NGo_N);
+}
+
+
+template<typename T>
+std::tuple<SpatialForced, SpatialForced, SpatialForced, SpatialForced,
+           SpatialForced, SpatialForced, SpatialForced>
+MGKukaIIwaRobot<T>::CalcJointReactionForces(
+    const Eigen::Ref<const VectorX<T>>& q,
+    const Eigen::Ref<const VectorX<T>>& qDt,
+    const Eigen::Ref<const VectorX<T>>& qDDt) const {
+  // Calculate joint reaction torque/forces with MotionGenesis.
+  PrepareMGOutput(q, qDt, qDDt);
+
+  // Convert MotionGenesis standard C++ matrices to Eigen matrices.
+  const SpatialForced F_A_Na(Vector3d(MG_kuka_auto_generated_.tA),
+                             Vector3d(MG_kuka_auto_generated_.fA));
+  const SpatialForced F_B_Ab(Vector3d(MG_kuka_auto_generated_.tB),
+                             Vector3d(MG_kuka_auto_generated_.fB));
+  const SpatialForced F_C_Bc(Vector3d(MG_kuka_auto_generated_.tC),
+                             Vector3d(MG_kuka_auto_generated_.fC));
+  const SpatialForced F_D_Cd(Vector3d(MG_kuka_auto_generated_.tD),
+                             Vector3d(MG_kuka_auto_generated_.fD));
+  const SpatialForced F_E_De(Vector3d(MG_kuka_auto_generated_.tE),
+                             Vector3d(MG_kuka_auto_generated_.fE));
+  const SpatialForced F_F_Ef(Vector3d(MG_kuka_auto_generated_.tF),
+                             Vector3d(MG_kuka_auto_generated_.fF));
+  const SpatialForced F_G_Fg(Vector3d(MG_kuka_auto_generated_.tG),
+                             Vector3d(MG_kuka_auto_generated_.fG));
+
+  return std::make_tuple(F_A_Na, F_B_Ab, F_C_Bc, F_D_Cd,
+                         F_E_De, F_F_Ef, F_G_Fg);
+}
+
+
+template<typename T>
+Vector7d MGKukaIIwaRobot<T>::CalcRevoluteMotorZTorques(
+    const Eigen::Ref<const VectorX<T>>& q,
+    const Eigen::Ref<const VectorX<T>>& qDt,
+    const Eigen::Ref<const VectorX<T>>& qDDt) const {
+  // Calculate joint driving torques with MotionGenesis.
+  PrepareMGOutput(q, qDt, qDDt);
+
+  const double tAz = MG_kuka_auto_generated_.tAz;
+  const double tBz = MG_kuka_auto_generated_.tBz;
+  const double tCz = MG_kuka_auto_generated_.tCz;
+  const double tDz = MG_kuka_auto_generated_.tDz;
+  const double tEz = MG_kuka_auto_generated_.tEz;
+  const double tFz = MG_kuka_auto_generated_.tFz;
+  const double tGz = MG_kuka_auto_generated_.tGz;
+
+  // Assemble the information to return.
+  Vector7d motor_torques;
+  motor_torques << tAz, tBz, tCz, tDz, tEz, tFz, tGz;
+  return motor_torques;
+}
+
+
+// Explicitly instantiates on the most common scalar types.
+template
+class MGKukaIIwaRobot<double>;
+
+}  // namespace MG
+}  // namespace kuka_iiwa_robot
+}  // namespace benchmarks
+}  // namespace multibody
+}  // namespace drake
--- drake/multibody/benchmarks/kuka_iiwa_robot/MG/test/MG_kuka_iiwa_robot_test.cc
+++ drake/multibody/benchmarks/kuka_iiwa_robot/MG/test/MG_kuka_iiwa_robot_test.cc
@@ -1,4 +1,4 @@
-#include "drake/multibody/benchmarks/kuka_iiwa_robot/test/MG_kuka_iiwa_robot.h"
+#include "drake/multibody/benchmarks/kuka_iiwa_robot/MG/MG_kuka_iiwa_robot.h"
 
 #include <cmath>
 #include <limits>
@@ -11,7 +11,10 @@ namespace drake {
 namespace multibody {
 namespace benchmarks {
 namespace kuka_iiwa_robot {
+namespace MG {
 namespace {
+
+using Vector7d = Eigen::Matrix<double, 7, 1>;
 
 // Function to compare Kuka iiwa robot arm end-effector (frame G)'s orientation
 // position, angular velocity, velocity in World (frame N) to expected solution.
@@ -24,15 +27,15 @@ namespace {
 // w_NG_N_expected    |  G's angular velocity in N, expressed in N.
 // v_NGo_N_expected   |  Go's velocity in N, expressed in N.
 void CompareEndEffectorPositionVelocityVsExpectedSolution(
-    const Eigen::Matrix<double, 7, 1>& q,
-    const Eigen::Matrix<double, 7, 1>& qDt,
-    const Eigen::Matrix<double, 7, 1>& qDDt,
-    const Eigen::Matrix3d& R_NG_expected,
-    const Eigen::Vector3d& p_No_Go_N_expected,
-    const Eigen::Vector3d& w_NG_N_expected,
-    const Eigen::Vector3d& v_NGo_N_expected,
-    const Eigen::Vector3d& alpha_NG_N_expected,
-    const Eigen::Vector3d& a_NGo_N_expected) {
+    const Vector7d &q,
+    const Vector7d &qDt,
+    const Vector7d &qDDt,
+    const Eigen::Matrix3d &R_NG_expected,
+    const Eigen::Vector3d &p_No_Go_N_expected,
+    const Eigen::Vector3d &w_NG_N_expected,
+    const Eigen::Vector3d &v_NGo_N_expected,
+    const Eigen::Vector3d &alpha_NG_N_expected,
+    const Eigen::Vector3d &a_NGo_N_expected) {
   MGKukaIIwaRobot<double> MG_kuka_robot;
   // R_NG       | Rotation matrix relating Nx, Ny, Nz to Gx, Gy, Gz.
   // p_NoGo_N   | Go's position from No, expressed in N.
@@ -70,7 +73,7 @@ GTEST_TEST(KukaIIwaRobot, ForwardKinematicsA) {
   const double qG = 0.0, qGDt = 0.0, qGDDt = 0.0;
 
   // Create a state with joint angles (q) and their time-derivatives (qDt).
-  Eigen::Matrix<double, 7, 1> q, q_Dt, q_DDt;
+  Vector7d q, q_Dt, q_DDt;
   q << qA, qB, qC, qD, qE, qF, qG;
   q_Dt << qADt, qBDt, qCDt, qDDt, qEDt, qFDt, qGDt;
   q_DDt << qADDt, qBDDt, qCDDt, qDDDt, qEDDt, qFDDt, qGDDt;
@@ -111,7 +114,7 @@ GTEST_TEST(KukaIIwaRobot, ForwardKinematicsB) {
   const double qG = q60, qGDt = 0.0, qGDDt = 0.0;
 
   // Create a state with joint angles (q) and their time-derivatives (qDt).
-  Eigen::Matrix<double, 7, 1> q, q_Dt, q_DDt;
+  Vector7d q, q_Dt, q_DDt;
   q << qA, qB, qC, qD, qE, qF, qG;
   q_Dt << qADt, qBDt, qCDt, qDDt, qEDt, qFDt, qGDt;
   q_DDt << qADDt, qBDDt, qCDDt, qDDDt, qEDDt, qFDDt, qGDDt;
@@ -139,7 +142,70 @@ GTEST_TEST(KukaIIwaRobot, ForwardKinematicsB) {
                                                        a_NGo_N_expected);
 }
 
+// Test accuracy of calculations for Kuka iiwa robot arm torque motors.
+// when the Kuka arm has all joint angles = 0 rad, all joint rates = 0 rad/sec,
+// and all joint angular accelerations = 0 rad/sec^2.  Then redo test
+// except with qGDDt = 1.0 rad/sec^2 (torque on link G).
+GTEST_TEST(KukaIIwaRobot, TorqueMotorA) {
+  const double qA = 0.0, qADt = 0.0, qADDt = 0.0;
+  const double qB = 0.0, qBDt = 0.0, qBDDt = 0.0;
+  const double qC = 0.0, qCDt = 0.0, qCDDt = 0.0;
+  const double qD = 0.0, qDDt = 0.0, qDDDt = 0.0;
+  const double qE = 0.0, qEDt = 0.0, qEDDt = 0.0;
+  const double qF = 0.0, qFDt = 0.0, qFDDt = 0.0;
+  const double qG = 0.0, qGDt = 0.0, qGDDt = 0.0;
+
+  // Create a state with joint angles (q) and their time-derivatives (qDt).
+  Vector7d q, q_Dt, q_DDt;
+  q << qA, qB, qC, qD, qE, qF, qG;
+  q_Dt << qADt, qBDt, qCDt, qDDt, qEDt, qFDt, qGDt;
+  q_DDt << qADDt, qBDDt, qCDDt, qDDDt, qEDDt, qFDDt, qGDDt;
+
+  // MotionGenesis (MG) solution for the motor torques to hold the robot static.
+  MGKukaIIwaRobot<double> MG_kuka_robot;
+  Vector7d zTorques = MG_kuka_robot.CalcRevoluteMotorZTorques(q, q_Dt, q_DDt);
+
+  // Expected solution for the motor torques to hold the robot static.
+  Vector7d zTorques_expected;
+  zTorques_expected << 0, 0, 0, 0, 0, 0, 0;
+
+  // Compare MG results with expected results.
+  constexpr double kEpsilon = std::numeric_limits<double>::epsilon();
+  EXPECT_TRUE(zTorques.isApprox(zTorques_expected, kEpsilon));
+
+  // Redo test with last motor creating 1000 rad/s^2 angular acceleration on G.
+  q_DDt(6) = 1000;
+  zTorques = MG_kuka_robot.CalcRevoluteMotorZTorques(q, q_Dt, q_DDt);
+  zTorques_expected << 1, 0, 1, 0, 1, 0, 1;
+  EXPECT_TRUE(zTorques.isApprox(zTorques_expected, 40 * kEpsilon));
+
+  // Calculate the joint reaction torques/forces.
+  SpatialForced F_Ao_Na, F_Bo_Ab, F_Co_Bc, F_Do_Cd, F_Eo_De, F_Fo_Ef, F_Go_Fg;
+  std::tie(F_Ao_Na, F_Bo_Ab, F_Co_Bc, F_Do_Cd, F_Eo_De, F_Fo_Ef, F_Go_Fg) =
+      MG_kuka_robot.CalcJointReactionForces(q, q_Dt, q_DDt);
+
+  // Create the expected solution for the joint reaction torque/forces.
+  Eigen::Vector3d zero_vector(0, 0, 0), y_vector(0, 1, 0), z_vector(0, 0, 1);
+  SpatialForced F_Ao_Na_expected(z_vector, zero_vector);
+  SpatialForced F_Bo_Ab_expected(y_vector, zero_vector);
+  SpatialForced F_Co_Bc_expected(z_vector, zero_vector);
+  SpatialForced F_Do_Cd_expected(y_vector, zero_vector);
+  SpatialForced F_Eo_De_expected(z_vector, zero_vector);
+  SpatialForced F_Fo_Ef_expected(y_vector, zero_vector);
+  SpatialForced F_Go_Fg_expected(z_vector, zero_vector);
+
+  // Compare the joint reaction torques/forces with expected results.
+  EXPECT_TRUE(F_Ao_Na.IsApprox(F_Ao_Na_expected, 40 * kEpsilon));
+  EXPECT_TRUE(F_Bo_Ab.IsApprox(F_Bo_Ab_expected, 40 * kEpsilon));
+  EXPECT_TRUE(F_Co_Bc.IsApprox(F_Co_Bc_expected, 40 * kEpsilon));
+  EXPECT_TRUE(F_Do_Cd.IsApprox(F_Do_Cd_expected, 40 * kEpsilon));
+  EXPECT_TRUE(F_Eo_De.IsApprox(F_Eo_De_expected, 40 * kEpsilon));
+  EXPECT_TRUE(F_Fo_Ef.IsApprox(F_Fo_Ef_expected, 40 * kEpsilon));
+  EXPECT_TRUE(F_Go_Fg.IsApprox(F_Go_Fg_expected, 40 * kEpsilon));
+}
+
 }  // namespace
+}  // namespace MG
 }  // namespace kuka_iiwa_robot
 }  // namespace benchmarks
 }  // namespace multibody
