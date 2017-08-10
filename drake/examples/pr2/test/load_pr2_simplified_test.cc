5d3f20f3c03d8632ad138875e06e06d32c045d82
Tristan <tristant@mit.edu>
2017-08-10 15:25:47 -0400

    Responded to second PR pass.

---
+++ drake/examples/pr2/test/load_pr2_simplified_test.cc
@@ -0,0 +1,26 @@
+
+
+#include <gtest/gtest.h>
+
+#include "drake/common/find_resource.h"
+#include "drake/multibody/parsers/urdf_parser.h"
+
+namespace drake {
+namespace examples {
+namespace pr2 {
+
+// Tests if the model from pr2_simplified.urdf loads into a RigidBodyTree with
+// no errors.
+GTEST_TEST(LoadPr2SimplifiedTest, TestIfPr2SimplifiedLoads) {
+  auto tree_ = std::make_unique<RigidBodyTree<double>>();
+  drake::parsers::urdf::AddModelInstanceFromUrdfFile(
+      FindResourceOrThrow("drake/examples/pr2/models/pr2_description/urdf/"
+                          "pr2_simplified.urdf"),
+      multibody::joints::
+          kFixed /* our PR2 model moves with actuators, not a floating base */,
+      nullptr /* weld to frame */, tree_.get());
+}
+
+}  // namespace pr2
+}  // namespace examples
+}  // namespace drake
