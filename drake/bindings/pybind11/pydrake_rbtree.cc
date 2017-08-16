022a7fe2bf5ee5425fd881c7b0295a8e9cc30aaa
Connor Lawson <rconnorlawson@users.noreply.github.com>
2017-08-16 13:22:10 -0400

    Merge pull request #6844 from rconnorlawson/draper-pybind11
    Expand pydrake IK bindings

--- drake/bindings/pybind11/pydrake_rbtree.cc
+++ drake/bindings/pybind11/pydrake_rbtree.cc
@@ -113,6 +113,10 @@ PYBIND11_PLUGIN(_pydrake_rbtree) {
            RigidBodyTreeConstants::default_model_instance_id_set,
          py::arg("in_terms_of_qdot") = false)
     .def("get_num_bodies", &RigidBodyTree<double>::get_num_bodies)
+    .def("get_num_frames", &RigidBodyTree<double>::get_num_frames)
+    .def("getBodyOrFrameName",
+         &RigidBodyTree<double>::getBodyOrFrameName,
+         py::arg("body_or_frame_id"))
     .def("number_of_positions", &RigidBodyTree<double>::get_num_positions)
     .def("get_num_positions", &RigidBodyTree<double>::get_num_positions)
     .def("number_of_velocities", &RigidBodyTree<double>::get_num_velocities)
