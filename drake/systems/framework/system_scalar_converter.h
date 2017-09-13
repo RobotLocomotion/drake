3ac6a35924dbaeada3d9de71d3beb031cd028f17
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-09-12 20:40:55 -0400

    Add LeafSystem constructor with SystemScalarConverter

--- drake/systems/framework/system_scalar_converter.h
+++ drake/systems/framework/system_scalar_converter.h
@@ -65,9 +65,14 @@ class SystemScalarConverter {
   ///
   /// @tparam S is the System type to convert
   ///
+  /// This an implicit conversion constructor (not marked `explicit`), in order
+  /// to make calling code substantially more readable, with relatively little
+  /// risk of an unwanted accidental conversion happening.
+  ///
   /// See @ref system_scalar_conversion for additional overview documentation.
   template <template <typename> class S>
-  explicit SystemScalarConverter(SystemTypeTag<S>) : SystemScalarConverter() {
+  // NOLINTNEXTLINE(runtime/explicit)
+  SystemScalarConverter(SystemTypeTag<S>) : SystemScalarConverter() {
     using Expression = symbolic::Expression;
     // From double to all other types.
     AddIfSupported<S, AutoDiffXd, double>();
