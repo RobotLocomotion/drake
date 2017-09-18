7aa4441624737e3db0518e3b9ed967d16f2b86fc
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-09-18 11:36:52 -0400

    Reindent to use two spaces per styleguide

--- drake/tools/test/lcm_vector_gen_test.sh
+++ drake/tools/test/lcm_vector_gen_test.sh
@@ -8,23 +8,23 @@ find .
 
 # TODO(jwnimmer-tri) De-duplicate this with lcm_vector_gen.sh.
 if [ -z "$CLANG_FORMAT" ]; then
-    CLANG_FORMAT="/usr/bin/clang-format-3.9"  # Preferred choice.
-    if [ ! -x "$CLANG_FORMAT" ]; then
-        CLANG_FORMAT=clang-format
-    fi
+  CLANG_FORMAT="/usr/bin/clang-format-3.9"  # Preferred choice.
+  if [ ! -x "$CLANG_FORMAT" ]; then
+    CLANG_FORMAT=clang-format
+  fi
 fi
 if ! type -p $CLANG_FORMAT > /dev/null ; then
-    cat <<EOF
+  cat <<EOF
 Cannot find $CLANG_FORMAT ; see installation instructions at:
 http://drake.mit.edu/code_style_tools.html
 EOF
-    exit 1
+  exit 1
 fi
 
 # Move the originals (which are symlinks) out of the way.
 tool_outputs="lcmt_sample_t.lcm sample.cc sample.h sample_translator.cc sample_translator.h"
 for item in $tool_outputs; do
-    mv drake/tools/test/gen/"$item"{,.orig}
+  mv drake/tools/test/gen/"$item"{,.orig}
 done
 
 # Run the code generator.
@@ -42,15 +42,15 @@ find drake/tools/test/gen
 
 # Re-format the code.
 for item in $tool_outputs; do
-    if [ $item == "lcmt_sample_t.lcm" ]; then
-        continue
-    fi
-    $CLANG_FORMAT --style=file -i drake/tools/test/gen/"$item"
+  if [ $item == "lcmt_sample_t.lcm" ]; then
+    continue
+  fi
+  $CLANG_FORMAT --style=file -i drake/tools/test/gen/"$item"
 done
 
 # Insist that the generated output matches the current copy in git.
 for item in $tool_outputs; do
-    diff --unified=20 drake/tools/test/gen/"$item"{.orig,}
+  diff --unified=20 drake/tools/test/gen/"$item"{.orig,}
 done
 
 echo "PASS"
