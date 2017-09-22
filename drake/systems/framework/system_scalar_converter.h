b8df9431247c5fb2873fea5efa3056e68c03d201
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-09-22 06:13:48 -0400

    Fix an IWYU

--- drake/systems/framework/system_scalar_converter.h
+++ drake/systems/framework/system_scalar_converter.h
@@ -10,6 +10,7 @@
 #include "drake/common/drake_assert.h"
 #include "drake/common/drake_copyable.h"
 #include "drake/common/eigen_autodiff_types.h"
+#include "drake/common/nice_type_name.h"
 #include "drake/common/symbolic.h"
 #include "drake/systems/framework/scalar_conversion_traits.h"
 #include "drake/systems/framework/system.h"
