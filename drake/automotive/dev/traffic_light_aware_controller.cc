387b036b6696988f38017bcff084fc5b79788cfc
Nikos Arechiga <nikos.arechiga@tri.global>
2017-09-22 16:12:33 -0700

    wip

---
+++ drake/automotive/dev/traffic_light_aware_controller.cc
@@ -0,0 +1,88 @@
+#include "traffic_light_aware_controller.h"
+
+namespace drake {
+using std::pow;
+using systems::BasicVector;
+using systems::Context;
+using systems::System;
+
+namespace automotive {
+
+template <typename T>
+TrafficLightAwareController<T>::TrafficLightAwareController()
+   : car_state_input_index_{ this->DeclareAbstractInputPort().get_index() },
+     acceleration_input_index_{ this->DeclareAbstractInputPort().get_index() },
+     traffic_light_input_index_{ this->DeclareAbstractInputPort().get_index() },
+     output_index_{ this->DeclareVectorOutputPort( BasicVector<T>(1),
+                    &TrafficLightAwareController::DoCalcOutput).get_index() }
+
+ {
+}
+
+template <typename T>
+const systems::InputPortDescriptor<T>& TrafficLightAwareController<T>::car_state() const {
+ return System<T>::get_input_port( car_state_input_index_ );
+}
+
+template <typename T>
+const systems::InputPortDescriptor<T>& TrafficLightAwareController<T>::other_controller_acceleration() const {
+ return System<T>::get_input_port( acceleration_input_index_ );
+}
+
+template <typename T>
+const systems::InputPortDescriptor<T>& TrafficLightAwareController<T>::traffic_light_input() const {
+ return System<T>::get_input_port( traffic_light_input_index_ );
+}
+
+template <typename T>
+const systems::OutputPort<T>& TrafficLightAwareController<T>::acceleration_output() const {
+ return System<T>::get_output_port( output_index_ );
+}
+
+template <typename T>
+void TrafficLightAwareController<T>::DoCalcOutput( const systems::Context<T>& context,
+                                                   systems::BasicVector<T>* output ) const {
+
+  VectorX<T> car_state = ReadInput( context, car_state_input_index_ );
+  VectorX<T> other_acceleration = ReadInput( context, acceleration_input_index_ );
+  VectorX<T> traffic_light_signal = ReadInput( context, traffic_light_input_index_ );
+
+	T car_x = car_state(0);
+  T car_y = car_state(1);
+	T signal_x = traffic_light_signal(0);
+	T signal_y = traffic_light_signal(1);
+  T signal_radius = traffic_light_signal(2);
+
+	if ( (traffic_light_signal(3) == 0)
+       || ( pow(signal_x - car_x, 2) + pow(signal_y - car_y, 2) > pow(signal_radius, 2) )
+  ) {
+		// Either we can go through or we are too far to worry about it, carry on
+    WriteOutput( other_acceleration, output );
+	} else { 
+		// slam on the brakes
+    DrivingCommand<T> driving_command;
+    driving_command.set_steering_angle( other_acceleration(0) );
+    driving_command.set_acceleration( -100 ); // make it stop
+		WriteOutput( driving_command );
+	}
+
+}
+
+template <typename T>
+void TrafficLightAwareController<T>::WriteOutput( const VectorX<T> value,
+                                                  systems::BasicVector<T>* output ) const {
+  output->set_value( value );
+}
+
+template <typename T>
+const VectorX<T> TrafficLightAwareController<T>::ReadInput( const Context<T>& context,
+                                                            int input_index ) const {
+	  const BasicVector<T>* input =
+      this->template EvalVectorInput<BasicVector>(context, input_index );
+  DRAKE_ASSERT((input != nullptr));
+  return input->get_value();
+}
+
+
+} // namespace automotive
+} // namespace drake
---
+++ drake/automotive/dev/traffic_light_aware_controller.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <memory>
+#include <math.h>
+
+#include<Eigen/Geometry>
+
+#include "drake/automotive/gen/driving_command.h"
+#include "drake/common/drake_copyable.h"
+#include "drake/common/eigen_types.h"
+#include "drake/systems/framework/leaf_system.h"
+
+
+namespace drake {
+namespace automotive {
+
+template <typename T>
+class TrafficLightAwareController : public systems::LeafSystem<T> {
+	public:
+	  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(TrafficLightAwareController)
+
+		TrafficLightAwareController();
+
+		// This controller takes as input an acceleration from another controller,
+		// and also looks at the traffic light. If the traffic light is "open",
+		// this controller simply forwards the acceleration command from the other
+		// controller; otherwise it overrides the acceleration with a braking
+		// command
+    const systems::InputPortDescriptor<T>& car_state() const;
+    const systems::InputPortDescriptor<T>& other_controller_acceleration() const;
+		const systems::InputPortDescriptor<T>& traffic_light_input() const;
+		const systems::OutputPort<T>& acceleration_output() const;
+
+	private:
+	  void DoCalcOutput( const systems::Context<T>& context,
+	                   systems::BasicVector<T>* output ) const;
+
+	  // Indices for input / output ports
+	  const int car_state_input_index_{};
+	  const int acceleration_input_index_{};
+	  const int traffic_light_input_index_{};
+	  const int output_index_{};
+
+		VectorX<T> ReadParameters( const systems::Context<T>& context ) const;
+		const VectorX<T> ReadInput( const systems::Context<T>& context, int input_index ) const;
+	  void WriteOutput( const VectorX<T> value,
+	                    systems::BasicVector<T>* output ) const;
+}; 
+
+} // namespace drake
+} // namespace automotive
