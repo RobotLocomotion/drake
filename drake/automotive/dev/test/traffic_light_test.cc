6b5c2d9ec42c84ba50020cebac61f08b943643d9
Nikos Arechiga <nikos.arechiga@tri.global>
2017-09-22 09:47:14 -0700

    Added a preliminary model of a traffic light
    formatting
    
    comments

---
+++ drake/automotive/dev/test/traffic_light_test.cc
@@ -0,0 +1,124 @@
+#include "drake/automotive/dev/traffic_light.h"
+#include "drake/systems/framework/basic_vector.h"
+
+#include <iostream>
+#include <memory>
+#include <stdlib.h>
+
+#include <Eigen/Dense>
+#include <gtest/gtest.h>
+
+namespace drake {
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+
+using std::abs;
+using std::unique_ptr;
+
+using systems::Context;
+using systems::BasicVector;
+using systems::SystemOutput;
+
+namespace automotive {
+namespace {
+
+GTEST_TEST(TrafficLightTest, BasicTest) {
+  TrafficLight<double> traffic_light(0, 0, 1, 6);
+  unique_ptr<Context<double>> context = traffic_light.CreateDefaultContext();
+  unique_ptr<SystemOutput<double>> output =
+      traffic_light.AllocateOutput(*context);
+
+  VectorXd signal_open(4);
+  signal_open(0) = 0;
+  signal_open(1) = 0;
+  signal_open(2) = 1;
+  signal_open(3) = 0;
+
+  VectorXd signal_closed(4);
+  signal_closed(0) = 0;
+  signal_closed(1) = 0;
+  signal_closed(2) = 1;
+  signal_closed(3) = 1;
+
+  // This sequence increases time and watches the signal change.
+  traffic_light.CalcOutput(*context, output.get());
+  VectorXd computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_open);
+
+  context->set_time(1);
+  traffic_light.CalcOutput(*context, output.get());
+  computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_open);
+
+  context->set_time(2);
+  traffic_light.CalcOutput(*context, output.get());
+  computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_open);
+
+  context->set_time(3);
+  traffic_light.CalcOutput(*context, output.get());
+  computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_closed);
+
+  context->set_time(4);
+  traffic_light.CalcOutput(*context, output.get());
+  computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_closed);
+
+  context->set_time(5);
+  traffic_light.CalcOutput(*context, output.get());
+  computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_closed);
+
+  context->set_time(6);
+  traffic_light.CalcOutput(*context, output.get());
+  computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_open);
+
+  context->set_time(7);
+  traffic_light.CalcOutput(*context, output.get());
+  computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_open);
+
+  context->set_time(8);
+  traffic_light.CalcOutput(*context, output.get());
+  computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_open);
+
+  context->set_time(9);
+  traffic_light.CalcOutput(*context, output.get());
+  computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_closed);
+
+  context->set_time(10);
+  traffic_light.CalcOutput(*context, output.get());
+  computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_closed);
+
+  context->set_time(11);
+  traffic_light.CalcOutput(*context, output.get());
+  computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_closed);
+
+  context->set_time(12);
+  traffic_light.CalcOutput(*context, output.get());
+  computed_output =
+      output->get_vector_data(traffic_light.output().get_index())->get_value();
+  EXPECT_EQ(computed_output, signal_open);
+}
+
+}  // namespace
+}  // namespace automotive
+}  // namespace drake
