4822949c299cb336de82f9b087441deb0ed8b2d0
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-09-25 17:23:27 -0400

    Add NONSYMBOLIC to default_scalars

--- drake/common/default_scalars.h
+++ drake/common/default_scalars.h
@@ -36,6 +36,17 @@
 /// See also @ref system_scalar_conversion.
 #define DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS( \
     SomeType) \
-template SomeType<double>; \
-template SomeType<::drake::AutoDiffXd>; \
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS( \
+    SomeType) \
 template SomeType<::drake::symbolic::Expression>;
+
+/// A macro to that defines explicit class template instantiations for Drake's
+/// default set of supported scalar types, excluding all symbolic types.  This
+/// macro should only be used in .cc files, never in .h files.  This is
+/// identical to DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS
+/// except that it does not define support for any drake::symbolic types.
+#define \
+  DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS( \
+      SomeType) \
+template SomeType<double>; \
+template SomeType<::drake::AutoDiffXd>;
