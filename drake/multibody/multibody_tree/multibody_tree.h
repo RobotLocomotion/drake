7fab5ed4e836e40314f897db91a8e080888e30de
amcastro-tri <alejandro.castro@tri.global>
2017-09-26 17:20:35 -0400

    Fixes after merge

--- drake/multibody/multibody_tree/multibody_tree.h
+++ drake/multibody/multibody_tree/multibody_tree.h
@@ -944,54 +944,6 @@ class MultibodyTree {
   void CalcBiasTerm(
       const systems::Context<T>& context, EigenPtr<VectorX<T>> Cv) const;
 
-  /// Computes the combined force contribution of ForceElement objects in the
-  /// model. A ForceElement can apply forcing as a spatial force per body or as
-  /// generalized forces, depending on the ForceElement model. Therefore this
-  /// method provides outputs for both spatial forces per body (with
-  /// `F_Bo_W_array`) and generalized forces (with `tau_array`).
-  /// ForceElement contributions are a function of the state only.
-  /// The output from this method can immediately be used as input to
-  /// CalcInverseDynamics() to include the effect of applied forces by force
-  /// elements.
-  ///
-  /// @param[in] context
-  ///   The context containing the state of the %MultibodyTree model.
-  /// @param[in] pc
-  ///   A position kinematics cache object already updated to be in sync with
-  ///   `context`.
-  /// @param[in] vc
-  ///   A velocity kinematics cache object already updated to be in sync with
-  ///   `context`.
-  /// @param[out] F_Bo_W_array
-  ///   A pointer to a valid, non nullptr, vector of spatial forces
-  ///   containing, for each body B, the total spatial force `F_Bo_W` applied at
-  ///   Bo by the force elements in `this` model, expressed in the world frame
-  ///   W. It must be of size equal to the number of bodies in the
-  ///   MultibodyTree. This method will abort if the the pointer is null or if
-  ///   `F_Bo_W_array` is not of size `get_num_bodies()`.
-  ///   On output, entries will be ordered by BodyNodeIndex.
-  ///   To access a mobilizer's reaction force on given body B in this array,
-  ///   use the index returned by Body::get_node_index().
-  /// @param[out] tau_array
-  ///   On output this array will contain the generalized forces contribution
-  ///   applied by the force elements in `this` model. It must be of size
-  ///   MultibodyTree::get_num_velocities() or this method will abort.
-  ///   Generalized forces for each Mobilizer can be accessed with
-  ///   Mobilizer::get_generalized_forces_from_array().
-  ///
-  /// @pre The position kinematics `pc` must have been previously updated with a
-  /// call to CalcPositionKinematicsCache().
-  /// @pre The velocity kinematics `vc` must have been previously updated with a
-  /// call to CalcVelocityKinematicsCache().
-  ///
-  /// @throws std::bad_cast if `context` is not a `MultibodyTreeContext`.
-  void CalcForceElementsContribution(
-      const systems::Context<T>& context,
-      const PositionKinematicsCache<T>& pc,
-      const VelocityKinematicsCache<T>& vc,
-      std::vector<SpatialForce<T>>* F_Bo_W_array,
-      EigenPtr<VectorX<T>> tau_array) const;
-
   void MapQDotToVelocity(
       const systems::Context<T>& context,
       const Eigen::Ref<const VectorX<T>>& qdot,
