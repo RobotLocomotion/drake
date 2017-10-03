b481d20db6b1c4519c4cf7c64d497a57a0b93060
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-10-03 08:24:09 -0400

    Rename tools to mydir in preparation for relocation

--- tools/asan.sh
+++ tools/asan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 me=$(python -c 'import os; print(os.path.realpath("'"$0"'"))')
-tools=$(dirname "$me")
-export ASAN_OPTIONS="$ASAN_OPTIONS:check_initialization_order=1:detect_stack_use_after_return=1:suppressions=$tools/asan.supp"
+mydir=$(dirname "$me")
+export ASAN_OPTIONS="$ASAN_OPTIONS:check_initialization_order=1:detect_stack_use_after_return=1:suppressions=$mydir/asan.supp"
 "$@"
--- tools/lsan.sh
+++ tools/lsan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 me=$(python -c 'import os; print(os.path.realpath("'"$0"'"))')
-tools=$(dirname "$me")
-export LSAN_OPTIONS="$LSAN_OPTIONS:suppressions=$tools/lsan.supp"
+mydir=$(dirname "$me")
+export LSAN_OPTIONS="$LSAN_OPTIONS:suppressions=$mydir/lsan.supp"
 "$@"
