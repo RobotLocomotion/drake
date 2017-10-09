b9739ada6362771e5995a0e448d5313a52a9e273
Francois Budin <francois.budin@kitware.com>
2017-10-09 11:31:50 -0400

    Improve how to find drake resources
    The function `FindResource()` and all the functions that rely on it search for
    drake resources in the current directory and in a directory that is specified
    by the environment variable `DRAKE_RESOURCE_ROOT`. However, in certain case,
    the caller has an idea of where the resources are and should be able to
    give additional paths to `FindResource()` where to seach. This is for
    example the case when calling `FindResource()` from a python function from
    pydrake such as `getDrakePath()` which is defined in a file installed in drake'
    install directory. Resource paths can be added with the function
    `GetResourceSearchPaths()` which adds the given path in a persistent variable.
    This allows to get Drake's path and Drake's resources path from Python without
    having to set the environment variable `DRAKE_RESOURCE_ROOT`.

--- drake/bindings/python/pydrake/test/testCommon.py
+++ drake/bindings/python/pydrake/test/testCommon.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
+import os
+import shutil
+import subprocess
 import unittest
+import sys
 import pydrake.common
 
 
@@ -27,6 +31,59 @@ class TestCommon(unittest.TestCase):
         pydrake.common.FindResourceOrThrow(
             'drake/examples/atlas/urdf/atlas_convex_hull.urdf'
             )
+
+    def test_install(self):
+        # Install into a temporary directory. The temporary directory does not
+        # need to be removed as bazel tests are run in a scratch space.
+        tmp_folder = "tmp"
+        os.mkdir(tmp_folder)
+        # Install the bindings and its dependencies in scratch space.
+        subprocess.check_call(
+            ["drake/bindings/install",
+             os.path.abspath(tmp_folder),
+             ])
+        # Set Python env
+        # Remove the un-installed copy, so we _know_ it won't be used.
+        shutil.rmtree("drake")
+        os.remove(".drake-resource-sentinel")
+        shutil.rmtree("_solib_k8")
+        shutil.rmtree("external")
+        self.assertEqual(os.listdir("."), [tmp_folder])
+
+        # Set the correct PYTHONPATH and (DY)LD_LIBRARY_PATH to use the
+        # correct pydrake module.
+        env_python_path = "PYTHONPATH"
+        if sys.platform == 'darwin':
+            env_ld_library = "DYLD_LIBRARY_PATH"
+        else:
+            env_ld_library = "LD_LIBRARY_PATH"
+        tool_env = dict(os.environ)
+        tool_env[env_python_path] = os.path.abspath(
+            os.path.join(tmp_folder, "lib", "python2.7", "site-packages")
+        )
+        tool_env[env_ld_library] = os.path.abspath(
+            os.path.join(tmp_folder, "lib")
+        )
+        # Create empty directory since no data is installed. Otherwise
+        # `GetDrakePath()` will throw an exception complaining that no
+        # directory was found. This is safe as:
+        # 1) if the first part of the test (installation fails), an error will
+        # be thrown when trying to create this folder.
+        # 2) This empty folder does not change the behavior of
+        # `GetDrakePath()`; We still expect `GetDrakePath()` to find the
+        # sentinel (which is installed) but simply work around the fact that
+        # no data is installed (which data should be installed anyway?). The
+        # test will fail if the sentinel is not found in the expected location.
+        data_folder = os.path.join(tmp_folder, "share", "drake", "drake")
+        os.mkdir(data_folder)
+        output_path = subprocess.check_output(
+            ["python",
+             "-c", "import pydrake; print(pydrake.getDrakePath())"
+             ],
+            env=tool_env,
+            ).strip()
+        found_install_path = (data_folder in output_path)
+        self.assertEqual(found_install_path, True)
 
 
 if __name__ == '__main__':
--- tools/install/kuka_plan_runner/kuka_plan_runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This file is installed to <prefix>/bin.
-readonly prefix=$(python -c 'import os; print(os.path.realpath(os.path.join("'"$0"'", os.pardir, os.pardir)))')
-
-export DRAKE_RESOURCE_ROOT=${prefix}/share/drake
-exec ${prefix}/bin/kuka_plan_runner "$@"
