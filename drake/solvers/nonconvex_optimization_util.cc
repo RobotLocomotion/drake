bc5e1bf5337a562c539a9c0a8914d56d8ca49910
hongkai-dai <daihongkai@gmail.com>
2017-10-17 10:50:19 -0400

    Add the difference of convex decomposition for nonconvex quadratic forms

---
+++ drake/solvers/nonconvex_optimization_util.cc
@@ -0,0 +1,32 @@
+#include "drake/solvers/nonconvex_optimization_util.h"
+
+#include "drake/solvers/mathematical_program.h"
+
+namespace drake {
+namespace solvers {
+std::pair<Eigen::MatrixXd, Eigen::MatrixXd>
+ConvexDecompositionNonConvexQuadraticForm(
+    const Eigen::Ref<const Eigen::MatrixXd>& Q) {
+  if (Q.rows() != Q.cols()) {
+    throw std::runtime_error("Q is not a square matrix.");
+  }
+  MathematicalProgram prog;
+  auto Q1 = prog.NewSymmetricContinuousVariables(Q.rows(), "Q1");
+  auto Q2 = prog.NewSymmetricContinuousVariables(Q.rows(), "Q2");
+  symbolic::Variable s = prog.NewContinuousVariables<1>("s")(0);
+  prog.AddPositiveSemidefiniteConstraint(Q1);
+  prog.AddPositiveSemidefiniteConstraint(Q2);
+  prog.AddLinearConstraint(Q1.array() - Q2.array() ==
+                           ((Q + Q.transpose()) / 2).array());
+  prog.AddLinearConstraint(symbolic::Expression(s) >=
+                           Q1.cast<symbolic::Expression>().trace());
+  prog.AddLinearConstraint(symbolic::Expression(s) >=
+                           Q2.cast<symbolic::Expression>().trace());
+  prog.AddCost(s);
+  prog.Solve();
+  auto Q1_sol = prog.GetSolution(Q1);
+  auto Q2_sol = prog.GetSolution(Q2);
+  return std::make_pair(Q1_sol, Q2_sol);
+}
+}  // namespace solvers
+}  // namespace drake
---
+++ drake/solvers/nonconvex_optimization_util.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <utility>
+
+#include <Eigen/Core>
+
+namespace drake {
+namespace solvers {
+/**
+ * For a non-convex homogeneous quadratic form xᵀQx, where Q is not necessarily
+ * a positive semidefinite matrix, we decompose it as a difference between two
+ * convex homogeneous quadratic forms
+ * xᵀQx = xᵀQ₁x - xᵀQ₂x,
+ * Q₁, Q₂ are positive semidefinite.
+ * To find the optimal Q₁, Q₂, we aim to solve the following semidefinite
+ * programming problem
+ * min s
+ * s.t s >= trace(Q₁)
+ *     s >= trace(Q₂)
+ *     Q₁ - Q₂ = Q
+ *     Q₁, Q₂ are positive semidefinite
+ * This decomposition Q = Q₁ - Q₂ can be used later, to solve the non-convex
+ * optimization problem involving a quadratic form xᵀQx.
+ * For more information, please refer to the papers on difference of convex
+ * decomposition, for example
+ *   Undominated d.c Decompositions of Quadratic Functions and Applications
+ *   to Branch-and-BOund Approaches
+ *     By I.M.Bomze and M. Locatelli
+ *     Computational Optimization and Applications, 2004
+ *   DC Decomposition of Nonconvex Polynomials with Algebraic Techniques
+ *     By A. A. Ahmadi and G. Hall
+ *     Mathematical Programming, 2015
+ * @param Q A square matrix. Throws a runtime_error if Q is not square.
+ * @return The optimal decomposition (Q₁, Q₂)
+ * TODO(hongkai.dai): templatize this function, to avoid dynamic memory
+ * allocation.
+ */
+std::pair<Eigen::MatrixXd, Eigen::MatrixXd>
+ConvexDecompositionNonConvexQuadraticForm(
+    const Eigen::Ref<const Eigen::MatrixXd>& Q);
+}  // namespace solvers
+}  // namespace drake
