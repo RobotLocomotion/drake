d80f9c9016af49391ab044e9d0384ff3875fe395
Jamie Snape <jamie.snape@kitware.com>
2017-10-18 13:33:22 -0400

    Use Drake build with Bazel for MATLAB bindings

--- drake/matlab/CTestConfig.cmake
+++ drake/matlab/CTestConfig.cmake
@@ -1,7 +1,7 @@
 # -*- mode: cmake -*-
 # vi: set ft=cmake :
 
-set(CTEST_PROJECT_NAME drake)
+set(CTEST_PROJECT_NAME drake_matlab)
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 set(CTEST_DROP_METHOD https)
 set(CTEST_DROP_SITE drake-cdash.csail.mit.edu)
