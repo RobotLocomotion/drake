7fb5c8ecb2d43e095cf52490f7381f5ffaf85146
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-10-18 07:10:20 -0400

    Rename driving_command_fields to driving_command

--- drake/automotive/simple_car_gen.sh
+++ drake/automotive/simple_car_gen.sh
@@ -13,7 +13,7 @@ source $drake/tools/lcm_vector_gen.sh
 
 gen_lcm_and_vector_from_proto "bicycle car parameters" $mydir/bicycle_car_parameters.named_vector
 gen_lcm_and_vector_from_proto "bicycle car state" $mydir/bicycle_car_state.named_vector
-gen_lcm_and_vector_from_proto "driving command" $mydir/driving_command_fields.named_vector
+gen_lcm_and_vector_from_proto "driving command" $mydir/driving_command.named_vector
 gen_vector_proto "idm planner parameters" $mydir/idm_planner_parameters.named_vector
 gen_lcm_and_vector_from_proto "maliput railcar state" $mydir/maliput_railcar_state.named_vector
 gen_lcm_and_vector_from_proto "maliput railcar params" $mydir/maliput_railcar_params.named_vector
