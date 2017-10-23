c312b5de8d4862371a1100c476da44a99e3ec89a
Andrés Valenzuela <andres.valenzuela@tri.global>
2017-10-22 22:22:48 -0400

    Add multi-process version of p&p demo.

--- examples/kuka_iiwa_arm/dev/pick_and_place/lcm_plant.cc
+++ examples/kuka_iiwa_arm/dev/pick_and_place/lcm_plant.cc
@@ -292,10 +292,6 @@ LcmPlant::LcmPlant(
     output_port_wsg_status_.push_back(
         builder.ExportOutput(wsg_status_sender->get_output_port(0)));
 
-    // Export iiwa robot_state_t output port.
-    output_port_iiwa_robot_state_.push_back(builder.ExportOutput(
-        iiwa_and_wsg_plant_->get_output_port_iiwa_robot_state_msg(i)));
-
     // Export WSG command input port.
     input_port_wsg_command_.push_back(
         builder.ExportInput(wsg_controller->get_command_input_port()));
