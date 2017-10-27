39774576f6ae6bac56be1b570a68652808c48f61
Jonathan DeCastro <jonathan.decastro@tri.global>
2017-10-27 15:38:04 -0400

    Introduce a fix for the shadowing error on osx builder (#7354)
    * Introduce a fix for the shadowing error on osx build

--- drake/automotive/road_odometry.h
+++ drake/automotive/road_odometry.h
@@ -18,10 +18,10 @@ struct RoadOdometry {
                const systems::rendering::FrameVelocity<double>& frame_velocity)
       : lane(road_position.lane), pos(road_position.pos), vel(frame_velocity) {}
   /// Fully-parameterized constructor that is T-supported.
-  RoadOdometry(const maliput::api::Lane* lane,
+  RoadOdometry(const maliput::api::Lane* lane_in,
                const maliput::api::LanePositionT<T>& lane_position,
                const systems::rendering::FrameVelocity<T>& frame_velocity)
-      : lane(lane), pos(lane_position), vel(frame_velocity) {}
+      : lane(lane_in), pos(lane_position), vel(frame_velocity) {}
 
   const maliput::api::Lane* lane{};
   maliput::api::LanePositionT<T> pos{};
