RobotLocomotion/drake

commit 655700a7bfa66b0403c815a281a473b9e4e012bf

[Multilane] Connection and Endpoint classes refactor. (#7325)
* multilane::Connection and multilane::Endpoint classes refactor.

- Connection, Endpoint related classes, ArcOffset and Group are moved to connection.cc and connection.h files.
  This change will tidy up builder.h and builder.cc files.
- operator<< is provided for Endpoint related classes.
- GTest comparisons for Endpoint related classes are provided.
- Test cases for Connection, Endpoint related classes and ArcOffset are provided.

drake/automotive/maliput/multilane/builder.cc
@@ -36,8 +36,10 @@ const Connection* Builder::Connect(const std::string& id, int num_lanes,
                  start.xy().y() + (length * std::sin(start.xy().heading())),
                  start.xy().heading()),
       z_end);
-  connections_.push_back(std::make_unique<Connection>(
-      id, start, end, num_lanes, r0, left_shoulder, right_shoulder));
+  const Connection::SegmentParameters segment_parameters {
+      num_lanes, r0, left_shoulder, right_shoulder};
+  connections_.push_back(std::make_unique<Connection>(id, start, end,
+                                                      segment_parameters));
   return connections_.back().get();
 }
 
@@ -50,17 +52,19 @@ const Connection* Builder::Connect(const std::string& id, int num_lanes,
   const double theta0 = alpha - std::copysign(M_PI / 2., arc.d_theta());
   const double theta1 = theta0 + arc.d_theta();
 
-  const double cx = start.xy().x() - (arc.radius() * std::cos(theta0));
-  const double cy = start.xy().y() - (arc.radius() * std::sin(theta0));
-
-  const Endpoint end(EndpointXy(cx + (arc.radius() * std::cos(theta1)),
-                                cy + (arc.radius() * std::sin(theta1)),
-                                alpha + arc.d_theta()),
-                     z_end);
-
+  const Connection::ArcGeometry arc_geometry{
+      start.xy().x() - (arc.radius() * std::cos(theta0)),
+      start.xy().y() - (arc.radius() * std::sin(theta0)),
+      arc.radius(), arc.d_theta()};
+  const Endpoint end(EndpointXy(
+      arc_geometry.cx + (arc.radius() * std::cos(theta1)),
+      arc_geometry.cy + (arc.radius() * std::sin(theta1)),
+      alpha + arc.d_theta()),
+      z_end);
+  const Connection::SegmentParameters segment_parameters{
+      num_lanes, r0, left_shoulder, right_shoulder};
   connections_.push_back(std::make_unique<Connection>(
-      id, start, end, num_lanes, r0, left_shoulder, right_shoulder, cx, cy,
-      arc.radius(), arc.d_theta()));
+      id, start, end, segment_parameters, arc_geometry));
   return connections_.back().get();
 }
 

commit bbeb42c7ed20cfb280fcb170c1fbad10759c8530

Fixes issues introduced in PR #7325.
- Fixes multilane::EndpointZ::reverse method.
- Modifies tolerance treatment in GTest idioms.
- Modified multilane::Group doc strings.

drake/automotive/maliput/monolane/builder.h
@@ -105,7 +105,7 @@ class EndpointZ {
 
   /// Returns an EndpointZ with reversed direction.
   EndpointZ reverse() const {
-    return EndpointZ(z_, -z_dot_, -theta_, -theta_dot_);
+    return EndpointZ(z_, -z_dot_, -theta_, theta_dot_);
   }
 
   double z() const { return z_; }

drake/automotive/maliput/multilane/builder.cc
@@ -36,10 +36,8 @@ const Connection* Builder::Connect(const std::string& id, int num_lanes,
                  start.xy().y() + (length * std::sin(start.xy().heading())),
                  start.xy().heading()),
       z_end);
-  const Connection::SegmentParameters segment_parameters {
-      num_lanes, r0, left_shoulder, right_shoulder};
-  connections_.push_back(std::make_unique<Connection>(id, start, end,
-                                                      segment_parameters));
+  connections_.push_back(std::make_unique<Connection>(
+      id, start, end, num_lanes, r0, left_shoulder, right_shoulder));
   return connections_.back().get();
 }
 
@@ -52,19 +50,16 @@ const Connection* Builder::Connect(const std::string& id, int num_lanes,
   const double theta0 = alpha - std::copysign(M_PI / 2., arc.d_theta());
   const double theta1 = theta0 + arc.d_theta();
 
-  const Connection::ArcGeometry arc_geometry{
-      start.xy().x() - (arc.radius() * std::cos(theta0)),
-      start.xy().y() - (arc.radius() * std::sin(theta0)),
-      arc.radius(), arc.d_theta()};
-  const Endpoint end(EndpointXy(
-      arc_geometry.cx + (arc.radius() * std::cos(theta1)),
-      arc_geometry.cy + (arc.radius() * std::sin(theta1)),
-      alpha + arc.d_theta()),
-      z_end);
-  const Connection::SegmentParameters segment_parameters{
-      num_lanes, r0, left_shoulder, right_shoulder};
+  const double cx = start.xy().x() - (arc.radius() * std::cos(theta0));
+  const double cy = start.xy().y() - (arc.radius() * std::sin(theta0));
+  const Endpoint end(EndpointXy(cx + (arc.radius() * std::cos(theta1)),
+                                cy + (arc.radius() * std::sin(theta1)),
+                                alpha + arc.d_theta()),
+                     z_end);
+
   connections_.push_back(std::make_unique<Connection>(
-      id, start, end, segment_parameters, arc_geometry));
+      id, start, end, num_lanes, r0, left_shoulder, right_shoulder, cx, cy,
+      arc.radius(), arc.d_theta()));
   return connections_.back().get();
 }
 

drake/automotive/maliput/multilane/connection.h
@@ -76,8 +76,12 @@ class EndpointZ {
       : z_(z), z_dot_(z_dot), theta_(theta), theta_dot_(theta_dot) {}
 
   /// Returns an EndpointZ with reversed direction.
+  ///
+  /// Reversing direction is equivalent to rotating s (and along with it, r)
+  /// around the h-axis by 180 degrees, thus flipping the signs of z_dot
+  /// and theta.
   EndpointZ reverse() const {
-    return EndpointZ(z_, -z_dot_, theta_, -theta_dot_);
+    return EndpointZ(z_, -z_dot_, -theta_, theta_dot_);
   }
 
   double z() const { return z_; }
@@ -179,96 +183,68 @@ class Connection {
   /// Possible connection geometries:  line- or arc-segment.
   enum Type { kLine, kArc };
 
-  /// Contains Segment related parameters for later construction.
-  struct SegmentParameters {
-    /// Number of lanes that the Segment will contain.
-    int num_lanes;
-    /// First Lane centerline distance from Segment's reference curve.
-    double r0;
-    /// Extra space at the right of the first Lane in the Segment.
-    double left_shoulder;
-    /// Extra space at the left of the last Lane in the Segment.
-    double right_shoulder;
-  };
-
   /// Constructs a line-segment connection joining `start` to `end`.
   ///
-  /// `segment_parameters` contains the information necessary to later build a
-  /// Segment. It will contain `segment_parameters.num_lanes` lanes, which must
-  /// be greater than zero. First Lane centerline will be placed at
-  /// `segment_parameters.r0` distance from the reference curve.
+  /// Segments will contain `num_lanes` lanes, which must be greater than zero.
+  /// First Lane centerline will be placed at `r0` distance from the reference
+  /// curve.
   ///
-  /// `segment_parameters.left_shoulder` and `segment_parameters.right_shoulder`
-  /// are extra spaces added to the right and left side of the first and last
-  /// lanes of the Segment. They will be added to Segment's bounds and must be
-  /// greater or equal to zero.
+  /// `left_shoulder` and `right_shoulder` are extra spaces added to the right
+  /// and left side of the first and last lanes of the Segment. They will be
+  /// added to Segment's bounds and must be greater or equal to zero.
 
   // TODO(agalbachicar)    `end` should be removed in order to avoid
   //                       inconsistencies when describing the geometry.
   Connection(const std::string& id, const Endpoint& start, const Endpoint& end,
-             const SegmentParameters& segment_parameters)
+             int num_lanes, double r0, double left_shoulder,
+             double right_shoulder)
       : type_(kLine),
         id_(id),
         start_(start),
         end_(end),
-        num_lanes_(segment_parameters.num_lanes),
-        r0_(segment_parameters.r0),
-        left_shoulder_(segment_parameters.left_shoulder),
-        right_shoulder_(segment_parameters.right_shoulder) {
+        num_lanes_(num_lanes),
+        r0_(r0),
+        left_shoulder_(left_shoulder),
+        right_shoulder_(right_shoulder) {
     DRAKE_DEMAND(num_lanes_ > 0);
     DRAKE_DEMAND(left_shoulder_ >= 0);
     DRAKE_DEMAND(right_shoulder_ >= 0);
   }
 
-  /// Contains arc-geometry parameters to build an arc-segment Connection.
-  struct ArcGeometry {
-    /// Arc's center x coordinate.
-    double cx;
-    /// Arc's center y coordinate.
-    double cy;
-    /// Arc's radius.
-    double radius;
-    /// Arc's angle span. Being positive indicates a counter-clockwise arc.
-    double d_theta;
-  };
-
   /// Constructs an arc-segment connection joining `start` to `end`.
   ///
-  /// `segment_parameters` contains the information necessary to later build a
-  /// Segment. It will contain `segment_parameters.num_lanes` lanes, which must
-  /// be greater than zero. First Lane centerline will be placed at
-  /// `segment_parameters.r0` distance from the reference curve.
+  /// `cx`, `cy` specify the center of the arc. `radius` is the radius,
+  /// and `d_theta` is the angle of arc.
   ///
-  /// `segment_parameters.left_shoulder` and `segment_parameters.right_shoulder`
-  /// are extra spaces added to the right and left side of the first and last
-  /// lanes of the Segment. They will be added to Segment's bounds and must be
-  /// greater or equal to zero.
+  /// `radius` must be positive.  `d_theta` > 0 indicates a
+  /// counterclockwise arc from start to end.
   ///
-  /// `arc_geometry` holds information to build the arc-geometry.
-  /// `arc_geometry.cx`, `arc_geometry.cy` specify the center of the arc.
-  /// `arc_geometry.radius` is the radius, and `arc_geometry.d_theta` is the
-  /// angle of arc.
+  /// Segments will contain `num_lanes` lanes, which must be greater than zero.
+  /// First Lane centerline will be placed at `r0` distance from the reference
+  /// curve.
   ///
-  /// `arc_geometry.radius` must be positive.  `arc_geometry.d_theta` > 0
-  /// indicates a counter-clockwise arc from start to end.
+  /// `left_shoulder` and `right_shoulder` are extra spaces added to the right
+  /// and left side of the first and last lanes of the Segment. They will be
+  /// added to Segment's bounds and must be greater or equal to zero.
 
   // TODO(agalbachicar)    `end` should be removed in order to avoid
   //                       inconsistencies when describing the geometry.
   Connection(const std::string& id, const Endpoint& start, const Endpoint& end,
-             const SegmentParameters& segment_parameters,
-             const ArcGeometry& arc_geometry)
+             int num_lanes, double r0, double left_shoulder,
+             double right_shoulder, double cx, double cy, double radius,
+             double d_theta)
       : type_(kArc),
         id_(id),
         start_(start),
         end_(end),
-        num_lanes_(segment_parameters.num_lanes),
-        r0_(segment_parameters.r0),
-        left_shoulder_(segment_parameters.left_shoulder),
-        right_shoulder_(segment_parameters.right_shoulder),
-        cx_(arc_geometry.cx),
-        cy_(arc_geometry.cy),
-        radius_(arc_geometry.radius),
-        d_theta_(arc_geometry.d_theta) {
+        num_lanes_(num_lanes),
+        r0_(r0),
+        left_shoulder_(left_shoulder),
+        right_shoulder_(right_shoulder),
+        cx_(cx),
+        cy_(cy),
+        radius_(radius),
+        d_theta_(d_theta) {
     DRAKE_DEMAND(num_lanes_ > 0);
     DRAKE_DEMAND(left_shoulder_ >= 0);
     DRAKE_DEMAND(right_shoulder_ >= 0);
@@ -350,19 +326,11 @@ class Group {
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(Group)
 
   /// Constructs an empty Group with the specified `id`.
-  ///
-  /// `id` must be unique so as to identify only one Junction.
-
-  // TODO(maddog@tri.global)    Enforce uniqueness of `id`.
   explicit Group(const std::string& id) : id_(id) {}
 
   /// Constructs a Group with `id`, populated by `connections`.
   ///
-  /// `id` must be unique so as to identify only one Junction.
-  ///
   /// `connections` must not contain duplicates.
-
-  // TODO(maddog@tri.global)    Enforce uniqueness of `id`.
   Group(const std::string& id,
         const std::vector<const Connection*>& connections)
       : id_(id) {

drake/automotive/maliput/multilane/test_utilities/multilane_types_compare.cc
@@ -9,10 +9,6 @@ namespace maliput {
 namespace multilane {
 namespace test {
 
-// Scale factor in meters used to convert back and forth from an angle deviation
-// to length deviation.
-const double kLengthScale{1.};
-
 ::testing::AssertionResult IsEndpointXyClose(const EndpointXy& xy1,
                                              const EndpointXy& xy2,
                                              double tolerance) {
@@ -38,16 +34,16 @@ const double kLengthScale{1.};
                     ", diff = " + std::to_string(delta) +
                     ", tolerance = " + std::to_string(tolerance) + "\n";
   }
-  const double delta_angle = std::abs(xy1.heading() - xy2.heading());
-  if ((delta_angle * kLengthScale) > tolerance) {
+  delta = std::abs(xy1.heading() - xy2.heading());
+  if (delta > tolerance) {
     fails = true;
     error_message = error_message +
                     "EndpointXys are different at heading angle. " +
                     "xy1.heading(): " + std::to_string(xy1.heading()) +
                     " vs.  xy2.heading(): " + std::to_string(xy2.heading()) +
-                    ", diff = " + std::to_string(delta_angle) +
+                    ", diff = " + std::to_string(delta) +
                     ", tolerance = " +
-                    std::to_string(tolerance / kLengthScale) + "\n";
+                    std::to_string(tolerance) + "\n";
   }
   if (fails) {
     return ::testing::AssertionFailure() << error_message;
@@ -83,16 +79,16 @@ const double kLengthScale{1.};
                     ", diff = " + std::to_string(delta) +
                     ", tolerance = " + std::to_string(tolerance) + "\n";
   }
-  const double delta_angle = std::abs(z1.theta() - z2.theta());
-  if ((delta_angle * kLengthScale) > tolerance) {
+  delta = std::abs(z1.theta() - z2.theta());
+  if (delta > tolerance) {
     fails = true;
     error_message = error_message +
                     "EndpointZ are different at theta angle. " +
                     "z1.theta(): " + std::to_string(z1.theta()) +
                     " vs.  z2.theta(): " + std::to_string(z2.theta()) +
-                    ", diff = " + std::to_string(delta_angle) +
+                    ", diff = " + std::to_string(delta) +
                     ", tolerance = " +
-                    std::to_string(tolerance / kLengthScale) + "\n";
+                    std::to_string(tolerance) + "\n";
   }
   delta = std::abs(z1.theta_dot() - z2.theta_dot());
   if (delta > tolerance) {

drake/automotive/maliput/multilane/test_utilities/multilane_types_compare.h
@@ -10,39 +10,33 @@ namespace multilane {
 namespace test {
 
 // Compares equality within @p tolerance deviation of two EndpointXy objects.
-//
-// When comparing angles, angle difference in radians is multiplied by a scale
-// factor to convert it into a length and then compared against @p tolerance.
-//
 // @param xy1 An EndpointXy object to compare.
 // @param xy2 An EndpointXy object to compare.
 // @param tolerance An allowable absolute deviation for each EndpointXy's
-// coordinate. It must be expressed in the same unit as length coordinates of
-// EndpointXy.
+// coordinate.
 // @return ::testing::AssertionFailure() When EndpointXy objects are different.
 // @return ::testing::AssertionSuccess() When EndpointXy objects are within
 // the @p tolerance deviation.
+
+// TODO(agalbachicar)    Given that EndpointXy is composed of two lengths and
+//                       one angle, tolerance must be replaced into two distinct
+//                       values to match each magnitude.
 ::testing::AssertionResult IsEndpointXyClose(const EndpointXy& xy1,
                                              const EndpointXy& xy2,
                                              double tolerance);
 
 // Compares equality within @p tolerance deviation of two EndpointZ objects.
-//
-// When comparing angles, angle difference in radians is multiplied by a scale
-// factor to convert it into a length and then compared against @p tolerance.
-//
 // @param z1 An EndpointZ object to compare.
 // @param z2 An EndpointZ object to compare.
 // @param tolerance An allowable absolute deviation for each EndpointZ's
-// coordinate. It must be expressed in the same unit as length coordinates of
-// EndpointZ.
+// coordinate.
 // @return ::testing::AssertionFailure() When EndpointZ objects are different.
 // @return ::testing::AssertionSuccess() When EndpointZ objects are within
 // the @p tolerance deviation.
 
 // TODO(agalbachicar)    Given that EndpointZ is composed of different
-//                       magnitudes, tolerance must be replaced or scaled to
-//                       match each magnitude.
+//                       magnitudes, tolerance must be replaced into distinct
+//                       values to match each magnitude.
 ::testing::AssertionResult IsEndpointZClose(const EndpointZ& z1,
                                             const EndpointZ& z2,
                                             double tolerance);
@@ -51,15 +45,14 @@ namespace test {
 // @param pos1 An Endpoint object to compare.
 // @param pos2 An Endpoint object to compare.
 // @param tolerance An allowable absolute deviation for each Endpoint's
-// coordinate. It must be expressed in the same unit as length coordinates of
-// Endpoint.
+// coordinate.
 // @return ::testing::AssertionFailure() When Endpoint objects are different.
 // @return ::testing::AssertionSuccess() When Endpoint objects are within
 // the @p tolerance deviation.
 
 // TODO(agalbachicar)    Given that EndpointXy and EndpointZ are composed of
-//                       different magnitudes, tolerance must be replaced or
-//                       scaled to match each magnitude.
+//                       different magnitudes, tolerance must be replaced into
+//                       distinct values to match each magnitude.
 ::testing::AssertionResult IsEndpointClose(const Endpoint& pos1,
                                            const Endpoint& pos2,
                                            double tolerance);

drake/automotive/maliput/multilane/test/multilane_builder_test.cc
@@ -16,6 +16,11 @@ namespace drake {
 namespace maliput {
 namespace multilane {
 
+// TODO(maddog-tri)  Test use of Endpoint::reverse() with non-zero theta and
+//                   theta_dot, checking that the orientation of the resulting
+//                   road surface is continuous, off the centerline, at the
+//                   branch-point where two connections connect
+
 GTEST_TEST(MultilaneBuilderTest, Fig8) {
   const double kLaneWidth = 4.;
   const double kLeftShoulder = 2.;

drake/automotive/maliput/multilane/test/multilane_connection_test.cc
@@ -59,7 +59,7 @@ GTEST_TEST(EndpointZTest, Reverse) {
   const EndpointZ dut{1., 2., M_PI / 4., M_PI / 2.};
   const double kZeroTolerance{0.};
   EXPECT_TRUE(test::IsEndpointZClose(
-      dut.reverse(), {1., -2., M_PI / 4., -M_PI / 2.}, kZeroTolerance));
+      dut.reverse(), {1., -2., -M_PI / 4., M_PI / 2.}, kZeroTolerance));
 }
 
 // ArcOffset checks.
@@ -82,8 +82,6 @@ class MultilaneConnectionTest : public ::testing::Test {
   const int kNumLanes{3};
   const double kLeftShoulder{1.};
   const double kRightShoulder{1.5};
-  const Connection::SegmentParameters kSegmentParameters{
-      kNumLanes, kR0, kLeftShoulder, kRightShoulder};
   const EndpointZ kLowFlatZ{0., 0., 0., 0.};
   const double kHeading{-M_PI / 4.};
   const EndpointXy kStartXy{20., 30., kHeading};
@@ -99,8 +97,9 @@ TEST_F(MultilaneConnectionTest, ArcAccessors) {
   const double kDTheta{M_PI / 2.};
   const Endpoint kEndEndpoint{{40., 30., kHeading + kDTheta}, kLowFlatZ};
 
-  const Connection dut(kId, kStartEndpoint, kEndEndpoint, kSegmentParameters,
-                       {kCenterX, kCenterY, kRadius, kDTheta});
+  const Connection dut(kId, kStartEndpoint, kEndEndpoint, kNumLanes, kR0,
+                       kLeftShoulder, kRightShoulder, kCenterX, kCenterY,
+                       kRadius, kDTheta);
   EXPECT_EQ(dut.type(), Connection::Type::kArc);
   EXPECT_EQ(dut.id(), kId);
   EXPECT_TRUE(
@@ -119,8 +118,8 @@ TEST_F(MultilaneConnectionTest, ArcAccessors) {
 TEST_F(MultilaneConnectionTest, LineAccessors) {
   const std::string kId{"line_connection"};
   const Endpoint kEndEndpoint{{50., 0., kHeading}, kLowFlatZ};
-
-  const Connection dut(kId, kStartEndpoint, kEndEndpoint, kSegmentParameters);
+  const Connection dut(kId, kStartEndpoint, kEndEndpoint, kNumLanes, kR0,
+                       kLeftShoulder, kRightShoulder);
   EXPECT_EQ(dut.type(), Connection::Type::kLine);
   EXPECT_EQ(dut.id(), kId);
   EXPECT_TRUE(