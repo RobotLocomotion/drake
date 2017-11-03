190453f8aec3269951cac54c32108c4c64e4148b
Jamie Snape <jamie.snape@kitware.com>
2017-11-03 12:17:37 -0400

    Specify correct minimum versions for CMake packages on Mac

--- matlab/cmake/packages.cmake
+++ matlab/cmake/packages.cmake
@@ -1,9 +1,20 @@
 set(CMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY ON)
 set(CMAKE_FIND_PACKAGE_NO_SYSTEM_PACKAGE_REGISTRY ON)
 
+if(APPLE)
+  set(MINIMUM_MATLAB_VERSION 9.2)
+  # TODO(jamiesnape): Change from 3.1 to 3.4.1 when #7278 merges.
+  set(MINIMUM_PROTOBUF_VERSION 3.1)
+else()
+  # TODO(jamiesnape): Change from 9 to 9.2 when support for R2016a and R2016 is
+  # no longer needed.
+  set(MINIMUM_MATLAB_VERSION 9)
+  set(MINIMUM_PROTOBUF_VERSION 2.6.1)
+endif()
+
 find_package(drake CONFIG REQUIRED)
 find_package(Eigen3 3.3.3 CONFIG REQUIRED)
-find_package(Matlab 9 MODULE REQUIRED COMPONENTS
+find_package(Matlab ${MINIMUM_MATLAB_VERSION} MODULE REQUIRED COMPONENTS
   MAIN_PROGRAM MEX_COMPILER MX_LIBRARY
 )
-find_package(Protobuf 2.6 MODULE REQUIRED)
+find_package(Protobuf ${MINIMUM_PROTOBUF_VERSION} MODULE REQUIRED)
