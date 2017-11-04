8e68d2b5359356f0c13195ae47379cdae5763f56
Rick Cory <rickcory@gmail.com>
2017-11-03 17:00:21 -0700

    Added sine wave primitive system and corresponding unit test.

---
+++ drake/systems/primitives/sine-inl.h
@@ -0,0 +1,117 @@
+#pragma once
+
+/// @file
+/// Template method implementations for sine.h.
+/// Most users should only include that file, not this one.
+/// For background, see http://drake.mit.edu/cxx_inl.html.
+
+#include <sstream>
+
+#include "drake/common/unused.h"
+#include "drake/systems/primitives/sine.h"
+
+namespace drake {
+namespace systems {
+
+template <typename T>
+Sine<T>::Sine(double amplitude, double frequency, double phase, int size,
+              bool is_time_based)
+    : Sine(Eigen::VectorXd::Ones(size) * amplitude,
+           Eigen::VectorXd::Ones(size) * frequency,
+           Eigen::VectorXd::Ones(size) * phase, is_time_based) {}
+
+template <typename T>
+Sine<T>::Sine(const Eigen::VectorXd& amplitudes,
+              const Eigen::VectorXd& frequencies,
+              const Eigen::VectorXd& phases,
+              bool is_time_based)
+    : VectorSystem<T>(SystemTypeTag<systems::Sine>{},
+                      amplitudes.size() * !is_time_based,
+                      amplitudes.size()),
+      amplitude_(amplitudes), frequency_(frequencies), phase_(phases),
+      is_time_based_(is_time_based) {}
+
+template <typename T>
+template <typename U>
+Sine<T>::Sine(const Sine<U>& other)
+    : Sine<T>(other.get_amplitude_vector(), other.get_frequency_vector(),
+              other.get_phase_vector(), other.is_time_based()) {}
+
+template <typename T>
+double Sine<T>::get_amplitude() const {
+  if (!amplitude_.isConstant(amplitude_[0])) {
+    std::stringstream s;
+    s << "The amplitude vector, [" << amplitude_ << "], cannot be represented "
+      << "as a scalar value. Please use "
+      << "drake::systems::Sine::get_amplitude_vector() instead.";
+    throw std::runtime_error(s.str().c_str());
+  }
+  return amplitude_[0];
+}
+
+template <typename T>
+double Sine<T>::get_frequency() const {
+  if (!frequency_.isConstant(frequency_[0])) {
+    std::stringstream s;
+    s << "The frequency vector, [" << frequency_ << "], cannot be represented "
+      << "as a scalar value. Please use "
+      << "drake::systems::Sine::get_frequency_vector() instead.";
+    throw std::runtime_error(s.str().c_str());
+  }
+  return amplitude_[0];
+}
+
+template <typename T>
+double Sine<T>::get_phase() const {
+  if (!phase_.isConstant(phase_[0])) {
+    std::stringstream s;
+    s << "The phase vector, [" << phase_ << "], cannot be represented as a "
+      << "scalar value. Please use "
+      << "drake::systems::Sine::get_phase_vector() instead.";
+    throw std::runtime_error(s.str().c_str());
+  }
+  return amplitude_[0];
+}
+
+template <typename T>
+bool Sine<T>::is_time_based() const {
+  return is_time_based_;
+}
+
+template <typename T>
+const Eigen::VectorXd& Sine<T>::get_amplitude_vector() const {
+  return amplitude_;
+}
+
+template <typename T>
+const Eigen::VectorXd& Sine<T>::get_frequency_vector() const {
+  return frequency_;
+}
+
+template <typename T>
+const Eigen::VectorXd& Sine<T>::get_phase_vector() const {
+  return phase_;
+}
+
+template <typename T>
+void Sine<T>::DoCalcVectorOutput(
+    const Context<T>& context,
+    const Eigen::VectorBlock<const VectorX<T>>& input,
+    const Eigen::VectorBlock<const VectorX<T>>& state,
+    Eigen::VectorBlock<VectorX<T>>* output) const {
+  unused(state);
+
+  VectorX<T> sine_arg;
+  if (is_time_based_) {
+      VectorX<T> time_vec(amplitude_.size());
+      time_vec.fill(context.get_time());
+      sine_arg = frequency_.array() * time_vec.array() + phase_.array();
+  } else {
+      sine_arg = frequency_.array() * input.array() + phase_.array();
+  }
+
+  *output = amplitude_.array() * sine_arg.array().sin();
+}
+
+}  // namespace systems
+}  // namespace drake
---
+++ drake/systems/primitives/test/sine_test.cc
@@ -0,0 +1,214 @@
+#include "drake/systems/primitives/sine.h"
+
+#include <memory>
+
+#include <gtest/gtest.h>
+
+#include "drake/common/eigen_types.h"
+#include "drake/systems/framework/basic_vector.h"
+#include "drake/systems/framework/input_port_value.h"
+#include "drake/systems/framework/test_utilities/scalar_conversion.h"
+
+using Eigen::Vector3d;
+using std::make_unique;
+using std::unique_ptr;
+
+namespace drake {
+namespace systems {
+namespace {
+
+template <typename T>
+void TestSineSystem(const Sine<T>& sine_system,
+                    const Eigen::MatrixXd& input_vectors,
+                    const Eigen::MatrixXd& expected_outputs) {
+  auto context = sine_system.CreateDefaultContext();
+
+  // Verifies that Sine allocates no state variables in the context.
+  EXPECT_EQ(0, context->get_continuous_state().size());
+  auto output = sine_system.AllocateOutput(*context);
+
+  if (sine_system.is_time_based()) {
+    // If the system is time based, the input_vectors Matrix should only contain
+    // a row vector of time instances to test against. In this case, the system
+    // has zero inputs ports.
+    ASSERT_EQ(input_vectors.rows(), 1);
+    ASSERT_EQ(0, sine_system.get_num_input_ports());
+    ASSERT_EQ(0, context->get_num_input_ports());
+
+    for (int i = 0; i < input_vectors.cols(); i++) {
+      // Initialize the time in seconds to be used by the Sine system.
+      context->set_time(input_vectors(0, i));
+
+      // Calculate the Sine system output.
+      sine_system.CalcOutput(*context, output.get());
+
+      // Checks that the number of output ports in the Sine system and the
+      // system output are consistent.
+      ASSERT_EQ(1, output->get_num_ports());
+      ASSERT_EQ(1, sine_system.get_num_output_ports());
+      const BasicVector<double> *output_vector = output->get_vector_data(0);
+      ASSERT_NE(nullptr, output_vector);
+
+      Eigen::VectorXd expected_vector = expected_outputs.col(i);
+      EXPECT_TRUE(expected_vector.isApprox(output_vector->get_value(), 1e-3));
+    }
+  } else {
+    // Loop over the input vectors and check that the Sine system outputs match
+    // the expected outputs.
+    for (int i = 0; i < input_vectors.cols(); i++) {
+      auto input =
+          make_unique<BasicVector<double>>(
+              sine_system.get_amplitude_vector().size());
+      ASSERT_EQ(1, sine_system.get_num_input_ports());
+      ASSERT_EQ(1, context->get_num_input_ports());
+
+      // Confirm the the size of the input port matches the size of the sample
+      // inputs (i.e., each column in the input_vectors matrix represents a
+      // sampling instant. The number of rows in input_vectors should match the
+      // size of the input port).
+      ASSERT_EQ(input->get_mutable_value().size(), input_vectors.rows());
+
+      // Initialize the input and associate it with the context.
+      input->get_mutable_value() << input_vectors.col(i);
+      context->FixInputPort(0, std::move(input));
+
+      // Calculate the Sine system output.
+      sine_system.CalcOutput(*context, output.get());
+
+      // Checks that the number of output ports in the Sine system and the
+      // system output are consistent.
+      ASSERT_EQ(1, output->get_num_ports());
+      ASSERT_EQ(1, sine_system.get_num_output_ports());
+      const BasicVector<double>* output_vector = output->get_vector_data(0);
+      ASSERT_NE(nullptr, output_vector);
+
+      Eigen::VectorXd expected_vector = expected_outputs.col(i);
+      EXPECT_TRUE(expected_vector.isApprox(output_vector->get_value(), 1e-3));
+    }
+  }
+}
+
+// Test with scalar input instances and scalar Sine parameters.
+GTEST_TEST(SineTest, SineScalarTest) {
+  const double kAmp = 1;
+  const double kPhase = 2;
+  const double kFreq = 3;
+  const auto sine_system =
+      make_unique<Sine<double>>(kAmp, kFreq, kPhase, 1, false);
+
+  const Eigen::Vector4d input_vector(M_PI / 6.0, M_PI / 4.0, M_PI / 2.0, M_PI);
+  const Eigen::Vector4d expected_output(-0.4161, -0.9372, 0.4161, -0.9093);
+
+  TestSineSystem(
+      *sine_system, input_vector.transpose(), expected_output.transpose());
+}
+
+// Test with time based instances and scalar Sine parameters.
+GTEST_TEST(SineTest, SineScalarTimeTest) {
+  const double kAmp = 1;
+  const double kPhase = 2;
+  const double kFreq = 3;
+  const auto sine_system =
+      make_unique<Sine<double>>(kAmp, kFreq, kPhase, 1, true);
+
+  const Eigen::Vector4d input_vector(M_PI / 6.0, M_PI / 4.0, M_PI / 2.0, M_PI);
+  const Eigen::Vector4d expected_output(-0.4161, -0.9372, 0.4161, -0.9093);
+
+  TestSineSystem(
+      *sine_system, input_vector.transpose(), expected_output.transpose());
+}
+
+// Test with vector input instances and scalar Sine parameters.
+GTEST_TEST(SineTest, SineVectorTest) {
+  const double kAmp = 1;
+  const double kPhase = 2;
+  const double kFreq = 3;
+  const auto sine_system =
+      make_unique<Sine<double>>(kAmp, kFreq, kPhase, 4, false);
+
+  Eigen::Matrix4d input_vectors;
+  input_vectors << 0.1, 0.2, 0.3, 0.4,
+                   0.5, 0.6, 0.7, 0.8,
+                   0.9, 1.0, 1.1, 1.2,
+                   1.3, 1.4, 1.5, 1.6;
+  Eigen::Matrix4d expected_output;
+  expected_output << 0.7457, 0.5155, 0.2392, -0.0584,
+                     -0.3508, -0.6119, -0.8183, -0.9516,
+                     -0.9999, -0.9589, -0.8323, -0.6313,
+                     -0.3739, -0.0831, 0.2151, 0.4941;
+
+  TestSineSystem(*sine_system, input_vectors, expected_output);
+}
+
+// Test with vector input instances and vector Sine parameters.
+GTEST_TEST(SineTest, SineParameterTest) {
+  Eigen::Vector4d kAmp(1.1, 1.2, 1.3, 1.4);
+  Eigen::Vector4d kFreq(1.5, 1.6, 1.7, 1.8);
+  Eigen::Vector4d kPhase(1.9, 2.0, 2.1, 2.2);
+  const auto sine_system =
+      make_unique<Sine<double>>(kAmp, kFreq, kPhase, false);
+
+  Eigen::Matrix4d input_vectors;
+  input_vectors << 0.1, 0.2, 0.3, 0.4,
+                   0.5, 0.6, 0.7, 0.8,
+                   0.9, 1.0, 1.1, 1.2,
+                   1.3, 1.4, 1.5, 1.6;
+  Eigen::Matrix4d expected_output;
+  expected_output << 0.9761, 0.8893, 0.7826, 0.6583,
+                     0.4020, 0.2167, 0.0259, -0.1656,
+                     -0.6100, -0.7954, -0.9579, -1.0928,
+                     -1.3792, -1.4000, -1.3754, -1.3065;
+
+  TestSineSystem(*sine_system, input_vectors, expected_output);
+}
+
+// Test with time instances, and vector Sine parameters.
+GTEST_TEST(SineTest, SineParameterTimeTest) {
+  Eigen::Vector4d kAmp(1.1, 1.2, 1.3, 1.4);
+  Eigen::Vector4d kFreq(1.5, 1.6, 1.7, 1.8);
+  Eigen::Vector4d kPhase(1.9, 2.0, 2.1, 2.2);
+  const auto sine_system =
+      make_unique<Sine<double>>(kAmp, kFreq, kPhase, true);
+
+  Eigen::Vector4d input_vectors(0.1, 0.6, 1.1, 1.5);
+  Eigen::Matrix4d expected_output;
+  expected_output << 0.9761, 0.3685, -0.4369, -0.9306,
+                     0.9977, 0.2167, -0.6957, -1.1419,
+                     0.9950, 0.0281, -0.9579, -1.2975,
+                     0.9661, -0.1932, -1.2062, -1.3754;
+
+  TestSineSystem(*sine_system, input_vectors.transpose(), expected_output);
+}
+
+GTEST_TEST(SineTest, SineAccessorTest) {
+  Eigen::Vector4d kAmp(1.1, 1.2, 1.3, 1.4);
+  Eigen::Vector4d kFreq(1.5, 1.6, 1.7, 1.8);
+  Eigen::Vector4d kPhase(1.9, 2.0, 2.1, 2.2);
+  const auto sine_system = make_unique<Sine<double>>(kAmp, kFreq, kPhase, true);
+  // Verifies the gain accessors are OK.
+  EXPECT_THROW(sine_system->get_amplitude(), std::runtime_error);
+  EXPECT_THROW(sine_system->get_frequency(), std::runtime_error);
+  EXPECT_THROW(sine_system->get_phase(), std::runtime_error);
+}
+
+GTEST_TEST(SineTest, ToAutoDiff) {
+  Eigen::Vector4d kAmp(1.1, 1.2, 1.3, 1.4);
+  Eigen::Vector4d kFreq(1.5, 1.6, 1.7, 1.8);
+  Eigen::Vector4d kPhase(1.9, 2.0, 2.1, 2.2);
+  const Sine<double> sine_system(kAmp, kFreq, kPhase, true);
+  EXPECT_TRUE(
+      is_autodiffxd_convertible(sine_system, [&](const auto& converted) {
+    EXPECT_EQ(0, converted.get_num_input_ports());
+    EXPECT_EQ(1, converted.get_num_output_ports());
+    EXPECT_EQ(kAmp, converted.get_amplitude_vector());
+  }));
+}
+
+GTEST_TEST(SineTest, ToSymbolic) {
+  const Sine<double> sine(1.0, 2.0, 3.0, true);
+  EXPECT_TRUE(is_symbolic_convertible(sine));
+}
+
+}  // namespace
+}  // namespace systems
+}  // namespace drake
