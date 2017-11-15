75a07dcb38e81c37857b9960a32c51e006f2f16f
Jamie Snape <jamie.snape@kitware.com>
2017-11-15 16:54:37 -0500

    Ask if build --python_path= can be added to .bazelrc on Mac

--- setup/mac/install_prereqs.sh
+++ setup/mac/install_prereqs.sh
@@ -71,3 +71,13 @@ PyYAML
 Sphinx
 EOF
 )
+
+# We require that Bazel uses the Python installed by Homebrew.
+# TODO(jamiesnape): Also support a .bazelrc located in the WORKSPACE.
+if [[ ! -f "${HOME}/.bazelrc" ]] || ! grep -q "build --python_path=" "${HOME}/.bazelrc"; then
+  echo "We need to add 'build --python_path=/usr/local/opt/python/libexec/bin/python' to ~/.bazelrc."
+  read -r -p "Do you want to continue (y/N)? " reply
+  if [[ "${reply}" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+    echo "build --python_path=/usr/local/opt/python/libexec/bin/python" >> "${HOME}/.bazelrc"
+  fi
+fi
