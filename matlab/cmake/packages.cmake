758e7051c3fc42ea22960919ab68398f40694806
Jamie Snape <jamiesnape@users.noreply.github.com>
2017-11-17 16:23:57 -0500

    Various CMake fixes (#7499)
    Do not search for system version of Drake and Eigen.
    Link Protobuf to the correct CMake target.

--- matlab/cmake/packages.cmake
+++ matlab/cmake/packages.cmake
@@ -3,7 +3,7 @@ set(CMAKE_FIND_PACKAGE_NO_SYSTEM_PACKAGE_REGISTRY ON)
 
 if(APPLE)
   set(MINIMUM_MATLAB_VERSION 9.2)
-  # TODO(jamiesnape): Change from 3.1 to 3.4.1 when #7278 merges.
+  # TODO(jamiesnape): Change from 3.1 to 3.5 when #7323 merges.
   set(MINIMUM_PROTOBUF_VERSION 3.1)
 else()
   # TODO(jamiesnape): Change from 9 to 9.2 when support for R2016a and R2016 is
@@ -12,9 +12,13 @@ else()
   set(MINIMUM_PROTOBUF_VERSION 2.6.1)
 endif()
 
-find_package(drake CONFIG REQUIRED)
-find_package(Eigen3 3.3.3 CONFIG REQUIRED)
-find_package(Matlab ${MINIMUM_MATLAB_VERSION} MODULE REQUIRED COMPONENTS
-  MAIN_PROGRAM MEX_COMPILER MX_LIBRARY
+find_package(drake CONFIG REQUIRED
+  NO_CMAKE_SYSTEM_PATH NO_SYSTEM_ENVIRONMENT_PATH
+)
+find_package(Eigen3 3.3.3 CONFIG REQUIRED
+  NO_CMAKE_SYSTEM_PATH NO_SYSTEM_ENVIRONMENT_PATH
+)
+find_package(Matlab ${MINIMUM_MATLAB_VERSION} MODULE REQUIRED
+  COMPONENTS MAIN_PROGRAM MEX_COMPILER MX_LIBRARY
 )
 find_package(Protobuf ${MINIMUM_PROTOBUF_VERSION} MODULE REQUIRED)
