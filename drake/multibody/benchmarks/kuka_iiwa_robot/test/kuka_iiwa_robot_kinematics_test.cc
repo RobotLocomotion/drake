2063cf17f4bdba1061344743079db73180a6e34e
amcastro-tri <alejandro.castro@tri.global>
2017-11-17 15:58:04 -0500

    Implements KukaIIwaRobotKinematics::GeometricJacobian unit test

--- drake/multibody/benchmarks/kuka_iiwa_robot/test/kuka_iiwa_robot_kinematics_test.cc
+++ drake/multibody/benchmarks/kuka_iiwa_robot/test/kuka_iiwa_robot_kinematics_test.cc
@@ -1,5 +1,10 @@
 #include <gtest/gtest.h>
 
+#include "drake/common/autodiff.h"
+#include "drake/common/eigen_types.h"
+#include "drake/common/test_utilities/eigen_matrix_compare.h"
+#include "drake/math/autodiff_gradient.h"
+#include "drake/math/jacobian.h"
 #include "drake/multibody/benchmarks/kuka_iiwa_robot/MG/MG_kuka_iiwa_robot.h"
 #include "drake/multibody/benchmarks/kuka_iiwa_robot/drake_kuka_iiwa_robot.h"
 
@@ -95,6 +100,89 @@ GTEST_TEST(KukaIIwaRobotKinematics, KinematicsTestA) {
 
   // Most general of these three tests non-zero velocity and acceleration.
   TestEndEffectorKinematics(q, qDt, qDDt);
+}
+
+#include <iostream>
+#define PRINT_VARn(a) std::cout << #a":\n" << a << std::endl;
+
+GTEST_TEST(KukaIIwaRobotKinematics, GeometricJacobian) {
+  // Then number of generalized positions in the Kuka arm model.
+  const int kNumPositions = 7;
+
+  // A random set of values for the joint's angles.
+  double q30 = M_PI / 6, q60 = M_PI / 3;
+  double qA = q60;
+  double qB = q30;
+  double qC = q60;
+  double qD = q30;
+  double qE = q60;
+  double qF = q30;
+  double qG = q60;
+
+  // Create a state with joint angles (q) and their time-derivatives (qDt).
+  VectorX<double> qvalue(kNumPositions);
+  qvalue << qA, qB, qC, qD, qE, qF, qG;
+
+  // A random set of values for the joint's velocities.
+  double qAdot = 0.1;
+  double qBdot = 0.2;
+  double qCdot = 0.3;
+  double qDdot = 0.4;
+  double qEdot = 0.5;
+  double qFdot = 0.6;
+  double qGdot = 0.7;
+  VectorX<double> vvalue(kNumPositions);
+  vvalue << qAdot, qBdot, qCdot, qDdot, qEdot, qFdot, qGdot;
+
+  // This helper lambda computes v_NG, the translational velocity of the end
+  // effector frame G in the world (Newtonian) frame N.
+  auto end_effector_velocity = [](const auto& q, const auto& v) {
+    using T = typename std::remove_reference<decltype(q)>::type::Scalar;
+    DrakeKukaIIwaRobot<T> drake_kuka_robot(0);
+    // The value of vdot is arbitrary for the computation of the end effector
+    // velocity.
+    const VectorX<T> vdot = VectorX<T>::Zero(kNumPositions);
+    const SpatialKinematicsPVA<T> drake_kinematics =
+        drake_kuka_robot.CalcEndEffectorKinematics(q, v, vdot);
+    return drake_kinematics.translational_velocity();
+  };
+
+  // Helper lambda to keep positions fixed and provide a functor of v to the
+  // math::jacobian() method below to compute derivatives with respect to v
+  // only.
+  auto end_effector_velocity_fixed_positions = [&](const auto& v) {
+    using T = typename std::remove_reference<decltype(v)>::type::Scalar;
+    Eigen::Matrix<T, kNumPositions, 1> q = qvalue;
+    return end_effector_velocity(q, v);
+  };
+
+  // End effector's velocity.
+  Vector3<double> v_NG = end_effector_velocity_fixed_positions(vvalue);
+
+  // Initialize v to have values vvalue and so that it is the independent
+  // variable of the problem.
+  VectorX<AutoDiffXd> v(kNumPositions);
+  math::initializeAutoDiff(vvalue, v);
+
+  Vector3<AutoDiffXd> v_NG_autodiff = end_effector_velocity_fixed_positions(v);
+  Vector3<double> v_NG_value = math::autoDiffToValueMatrix(v_NG_autodiff);
+  MatrixX<double> v_NG_derivs = math::autoDiffToGradientMatrix(v_NG_autodiff);
+
+  EXPECT_EQ(v_NG_derivs.rows(), 3);
+  EXPECT_EQ(v_NG_derivs.cols(), kNumPositions);
+
+  //auto v_NG_autodiff = math::jacobian(end_effector_velocity_fixed_positions, v);
+  (void)v_NG_autodiff;
+
+  PRINT_VARn(v_NG_autodiff(0).value());
+  PRINT_VARn(v_NG_autodiff(1).value());
+  PRINT_VARn(v_NG_autodiff(2).value());
+
+  PRINT_VARn(v_NG.transpose());
+  PRINT_VARn(v_NG_value.transpose());
+  PRINT_VARn(v_NG_derivs);
+
+  PRINT_VARn(v_NG_derivs * vvalue);
 }
 
 }  // namespace
