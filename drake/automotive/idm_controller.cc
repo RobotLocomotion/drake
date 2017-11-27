3103d5e60d418a6fca0ff900d131579f39eb0e06
Jon <jonathan.decastro@tri.global>
2017-11-27 16:45:42 -0500

    Add multi-branch functionality to PoseSelector

--- drake/automotive/idm_controller.cc
+++ drake/automotive/idm_controller.cc
@@ -27,18 +27,23 @@ static constexpr int kIdmParamsIndex{0};
 
 template <typename T>
 IdmController<T>::IdmController(const RoadGeometry& road)
+    : IdmController(road, false) {}
+
+template <typename T>
+IdmController<T>::IdmController(const RoadGeometry& road, bool check_branches)
     : systems::LeafSystem<T>(
           systems::SystemTypeTag<automotive::IdmController>{}),
       road_(road),
-      ego_pose_index_{
-          this->DeclareVectorInputPort(PoseVector<T>()).get_index()},
-      ego_velocity_index_{
-          this->DeclareVectorInputPort(FrameVelocity<T>()).get_index()},
-      traffic_index_{this->DeclareAbstractInputPort().get_index()},
-      acceleration_index_{
+      check_branches_(check_branches),
+      ego_pose_index_(
+          this->DeclareVectorInputPort(PoseVector<T>()).get_index()),
+      ego_velocity_index_(
+          this->DeclareVectorInputPort(FrameVelocity<T>()).get_index()),
+      traffic_index_(this->DeclareAbstractInputPort().get_index()),
+      acceleration_index_(
           this->DeclareVectorOutputPort(systems::BasicVector<T>(1),
                                         &IdmController::CalcAcceleration)
-              .get_index()} {
+              .get_index()) {
   this->DeclareNumericParameter(IdmPlannerParameters<T>());
 }
 
@@ -115,7 +120,7 @@ void IdmController<T>::ImplCalcAcceleration(
   // Find the single closest car ahead.
   const ClosestPose<T> lead_car_pose = PoseSelector<T>::FindSingleClosestPose(
       ego_position.lane, ego_pose, traffic_poses,
-      idm_params.scan_ahead_distance(), AheadOrBehind::kAhead);
+      idm_params.scan_ahead_distance(), AheadOrBehind::kAhead, check_branches_);
   const T headway_distance = lead_car_pose.distance;
 
   const LanePositionT<T> lane_position(T(ego_position.pos.s()),
