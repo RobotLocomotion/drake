cf9f3c0dca975d0c420d8dfd9d96e4a87cf112a9
Francois Budin <francois.budin@kitware.com>
2017-12-08 16:40:11 -0500

    Replace redundant installation test code with centralized function call

--- tools/workspace/lcm/test/install_test.py
+++ tools/workspace/lcm/test/install_test.py
@@ -1,17 +1,16 @@
 from __future__ import absolute_import, division, print_function
 
 import os
-import shutil
 import subprocess
 import unittest
-import sys
 import install_test_helper
 
 
 class TestCommonInstall(unittest.TestCase):
     def testInstall(self):
         tmp_folder = "tmp"
-        result = install_test_helper.install(tmp_folder, ['lib', 'share'])
+        result = install_test_helper.install(tmp_folder,
+                                             ['bin', 'lib', 'share'])
         self.assertEqual(None, result)
         executable_folder = os.path.join(tmp_folder, "bin")
         subprocess.check_call([os.path.join(executable_folder, "lcm-gen")])
