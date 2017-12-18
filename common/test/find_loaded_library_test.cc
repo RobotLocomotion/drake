089ec14d86c2867593728266e8a72e9ab7699748
Eric Cousineau <eric.cousineau@tri.global>
2017-12-18 11:07:23 -0500

    find_loaded_library_test: Update test to use dummy shared library.

--- common/test/find_loaded_library_test.cc
+++ common/test/find_loaded_library_test.cc
@@ -12,13 +12,7 @@ namespace {
 GTEST_TEST(FindLibraryTest, Library) {
   // Test wether or not `LoadedLibraryPath()` can find the path to a library
   // loaded by the process.
-  optional<string> library_path =
-  // TODO(fbudin69500): Remove this platform-dependency.
-#ifdef __APPLE__
-    LoadedLibraryPath("libgflags.so");
-#else
-    LoadedLibraryPath("libexternal_Scom_Ugithub_Ugflags_Ugflags_Slibgflags.so");
-#endif
+  optional<string> library_path = LoadedLibraryPath("lib_is_real.so");
   EXPECT_TRUE(library_path);
   library_path = LoadedLibraryPath("lib_not_real.so");
   EXPECT_FALSE(library_path);
