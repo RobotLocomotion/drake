cbb98a2f3c17c3d66b9bfbd3ef28ceea90f0df46
Francois Budin <francois.budin@kitware.com>
2018-01-22 18:46:08 -0500

    New bazel macro to test run installed drake.
    `install_test()` is a wrapper macro that creates a Python script that is run
    during the testing phase. The Python script installs `drake` and runs the
    targets that are listed in `install_tests` in install rules.
    The Python script that is created will allow to test multiple targets in
    the install tree without having to install drake multiple times.
    `install_test()` should be called only once, in `//:*`.
    All the install tests that have been created in all the `install()` commands
    in the source tree will be integrated in the Python script that is generated.
    The targets can either be executables that are installed, or any executable
    or scripts that is designed to test drake features in the install tree.
    The Python install_test_helper file (`tools/install/install_test_helper.py`)
    has been improved to easily create Python scripts that run commands from the
    install directory. This is convenient when trying to test an executable in
    the install tree that is run with parameters, or an executable that needs to
    be killed because it never stops running.

--- examples/kuka_iiwa_arm/test/iiwa_wsg_simulation_installed_test.py
+++ examples/kuka_iiwa_arm/test/iiwa_wsg_simulation_installed_test.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 
 import os
+import shutil
 import unittest
-
+import sys
 import install_test_helper
 
 
-class TestKukaSimulation(unittest.TestCase):
+class TestIiwaWsgSimulation(unittest.TestCase):
     def test_install(self):
-        # Get install directory
+        # Get install directory.
         install_dir = install_test_helper.get_install_dir()
         # Make sure the simulation can run without error.  We set cwd="/" to
         # defeat the "search in parent folders" heuristic, so that the "use
         # libdrake.so relative paths" must be successful.
         simulation = os.path.join(
             install_dir,
-            "share/drake/examples/kuka_iiwa_arm/kuka_simulation")
+            "share/drake/examples/kuka_iiwa_arm/iiwa_wsg_simulation")
         self.assertTrue(os.path.exists(simulation), "Can't find " + simulation)
         install_test_helper.check_call([simulation, "--simulation_sec=0.01"])
 
