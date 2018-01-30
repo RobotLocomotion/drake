65c3b414daff9597d2b803007d314c317be191cc
Eric Cousineau <eric.cousineau@tri.global>
2018-01-30 12:52:28 -0500

    framework_py: Add bindings for VectorSystem.

--- bindings/pydrake/systems/test/vector_test.py
+++ bindings/pydrake/systems/test/vector_test.py
@@ -11,28 +11,49 @@ from pydrake.systems.framework import (
     BasicVector,
     )
 
-# TODO(eric.cousineau): Add negative test cases for AutoDiffXd and Symbolic
-# once they are in the bindings.
+
+def pass_through(x):
+    return x
+
+
+# TODO(eric.cousineau): Add negative (or positive) test cases for AutoDiffXd
+# and Symbolic once they are in the bindings.
 
 
 class TestReference(unittest.TestCase):
     def test_basic_vector_double(self):
-        # Ensure that we can get vectors templated on double by reference.
-        init = [1., 2, 3]
-        value_data = BasicVector(init)
-        value = value_data.get_mutable_value()
-        # TODO(eric.cousineau): Determine if there is a way to extract the
-        # pointer referred to by the buffer (e.g. `value.data`).
-        value[:] += 1
-        expected = [2., 3, 4]
-        self.assertTrue(np.allclose(value, expected))
-        self.assertTrue(np.allclose(value_data.get_value(), expected))
-        self.assertTrue(np.allclose(value_data.get_mutable_value(), expected))
-        expected = [5., 6, 7]
-        value_data.SetFromVector(expected)
-        self.assertTrue(np.allclose(value, expected))
-        self.assertTrue(np.allclose(value_data.get_value(), expected))
-        self.assertTrue(np.allclose(value_data.get_mutable_value(), expected))
+        # Test constructing vectors of sizes [0, 1, 2], and ensure that we can
+        # consruct from both lists and `np.array` objects with no ambiguity.
+        for n in range(3):
+            init = range(n)
+            if n > 0:
+                init[0] = float(0)
+            after_add = [x + 1 for x in init]
+            after_set = [x + 10 for x in init]
+
+            for func in [pass_through, np.array]:
+                # Ensure that we can get vectors templated on double by
+                # reference.
+                init = func(init)
+                value_data = BasicVector(init)
+                value = value_data.get_mutable_value()
+                # TODO(eric.cousineau): Determine if there is a way to extract
+                # the pointer referred to by the buffer (e.g. `value.data`).
+                value[:] += 1
+                expected = func(after_add)
+                self.assertTrue(np.allclose(value, expected))
+                self.assertTrue(np.allclose(value_data.get_value(), expected))
+                self.assertTrue(
+                    np.allclose(value_data.get_mutable_value(), expected))
+                expected = func(after_set)
+                value_data.SetFromVector(expected)
+                self.assertTrue(np.allclose(value, expected))
+                self.assertTrue(np.allclose(value_data.get_value(), expected))
+                self.assertTrue(
+                    np.allclose(value_data.get_mutable_value(), expected))
+                # Ensure we can construct from size.
+                value_data = BasicVector(n)
+                self.assertEquals(value_data.size(), n)
 
 
 if __name__ == '__main__':
