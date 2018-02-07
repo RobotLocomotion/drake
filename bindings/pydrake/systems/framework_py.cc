1e34e59383ac85dbed499f1752bf731fbf16b799
Michael Sherman <sherm1@gmail.com>
2018-02-06 18:05:29 -0800

    Update python bindings.

--- bindings/pydrake/systems/framework_py.cc
+++ bindings/pydrake/systems/framework_py.cc
@@ -6,7 +6,7 @@
 
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/bindings/pydrake/systems/systems_pybind.h"
-#include "drake/bindings/pydrake/type_safe_index_py.h"
+#include "drake/bindings/pydrake/type_safe_index_pybind.h"
 #include "drake/bindings/pydrake/util/drake_optional_pybind.h"
 #include "drake/bindings/pydrake/util/eigen_pybind.h"
 #include "drake/systems/framework/abstract_values.h"
@@ -47,21 +47,6 @@ using systems::DiscreteValues;
 
 using pysystems::AddValueInstantiation;
 using pysystems::DefClone;
-
-BindTypeSafeIndex<SubsystemIndex>(m, "SubsystemIndex");
-BindTypeSafeIndex<InputPortIndex>(m, "InputPortIndex");
-BindTypeSafeIndex<OutputPortIndex>(m, "OutputPortIndex");
-BindTypeSafeIndex<DependencyTicket>(m, "DependencyTicket");
-BindTypeSafeIndex<CacheIndex>(m, "CacheIndex");
-BindTypeSafeIndex<DiscreteStateIndex>(m, "DiscreteStateIndex");
-BindTypeSafeIndex<AbstractStateIndex>(m, "AbstractStateIndex");
-BindTypeSafeIndex<NumericParameterIndex>(m, "NumericParameterIndex");
-BindTypeSafeIndex<AbstractParameterIndex>(m, "AbstractParameterIndex");
-
-m.attr("kAutoSize") = kAutoSize;
-py::enum_<PortDataType>(m, "PortDataType")
-  .value("kVectorValued", kVectorValued)
-  .value("kAbstractValued", kAbstractValued);
 
 class PySystem : public py::wrapper<System<T>> {
  public:
@@ -253,11 +238,21 @@ PYBIND11_MODULE(framework, m) {
   // TODO(eric.cousineau): Resolve `str_py` workaround.
   auto str_py = py::eval("str");
 
-  m.attr("kAutoSize") = kAutoSize;
+  BindTypeSafeIndex<DependencyTicket>(m, "DependencyTicket");
+  BindTypeSafeIndex<CacheIndex>(m, "CacheIndex");
+  BindTypeSafeIndex<SubsystemIndex>(m, "SubsystemIndex");
+  BindTypeSafeIndex<InputPortIndex>(m, "InputPortIndex");
+  BindTypeSafeIndex<OutputPortIndex>(m, "OutputPortIndex");
+  BindTypeSafeIndex<DiscreteStateIndex>(m, "DiscreteStateIndex");
+  BindTypeSafeIndex<AbstractStateIndex>(m, "AbstractStateIndex");
+  BindTypeSafeIndex<NumericParameterIndex>(m, "NumericParameterIndex");
+  BindTypeSafeIndex<AbstractParameterIndex>(m, "AbstractParameterIndex");
 
   py::enum_<PortDataType>(m, "PortDataType")
     .value("kVectorValued", kVectorValued)
     .value("kAbstractValued", kAbstractValued);
+
+  m.attr("kAutoSize") = kAutoSize;
 
   // TODO(eric.cousineau): Show constructor, but somehow make sure `pybind11`
   // knows this is abstract?
