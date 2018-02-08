135236a1b3b49321e9be893fa107cb07d8f5fb51
Evan Drumwright <drum@tri.global>
2018-02-07 16:08:00 -0800

    Transferred ball test to bouncing ball test and removed ball test.

--- examples/bouncing_ball/test/bouncing_ball_test.cc
+++ examples/bouncing_ball/test/bouncing_ball_test.cc
@@ -79,11 +79,66 @@ class BouncingBallTest : public ::testing::Test {
     return context_->get_mutable_continuous_state_vector();
   }
 
+  const systems::VectorBase<double>& generalized_position() {
+    return context_->get_continuous_state().get_generalized_position();
+  }
+
+  const systems::VectorBase<double>& generalized_velocity() {
+    return context_->get_continuous_state().get_generalized_velocity();
+  }
+
   std::unique_ptr<BouncingBall<double>> dut_;  //< The device under test.
   std::unique_ptr<systems::Context<double>> context_;
   std::unique_ptr<systems::SystemOutput<double>> output_;
   std::unique_ptr<systems::ContinuousState<double>> derivatives_;
 };
+
+TEST_F(BouncingBallTest, Topology) {
+  ASSERT_EQ(0, dut_->get_num_input_ports());
+
+  ASSERT_EQ(1, dut_->get_num_output_ports());
+  const auto& output_port = dut_->get_output_port(0);
+  EXPECT_EQ(systems::kVectorValued, output_port.get_data_type());
+}
+
+TEST_F(BouncingBallTest, Output) {
+  // Grab a pointer to where the CalcOutput results will be saved.
+  const auto result = output_->get_vector_data(0);
+
+  // Initial state and output.
+  dut_->CalcOutput(*context_, output_.get());
+  EXPECT_EQ(10.0, result->GetAtIndex(0));
+  EXPECT_EQ(0.0, result->GetAtIndex(1));
+
+  // New state just propagates through.
+  continuous_state().SetAtIndex(0, 1.0);
+  continuous_state().SetAtIndex(1, 2.0);
+  dut_->CalcOutput(*context_, output_.get());
+  EXPECT_EQ(1.0, result->GetAtIndex(0));
+  EXPECT_EQ(2.0, result->GetAtIndex(1));
+}
+
+TEST_F(BouncingBallTest, Derivatives) {
+  // Grab a pointer to where the EvalTimeDerivatives results will be saved.
+  const auto& result = derivatives_->get_mutable_vector();
+
+  // Evaluate time derivatives.
+  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
+  EXPECT_EQ(0.0, result.GetAtIndex(0));
+  EXPECT_EQ(-9.81, result.GetAtIndex(1));
+
+  // Test at non-zero velocity.
+  continuous_state().SetAtIndex(1, 5.3);
+  dut_->CalcTimeDerivatives(*context_, derivatives_.get());
+  EXPECT_EQ(5.3, result.GetAtIndex(0));
+  EXPECT_EQ(-9.81, result.GetAtIndex(1));
+}
+
+TEST_F(BouncingBallTest, Accessors) {
+  // Evaluate accessors specific to the second-order system.
+  EXPECT_EQ(10.0, generalized_position().GetAtIndex(0));
+  EXPECT_EQ(0, generalized_velocity().GetAtIndex(0));
+}
 
 TEST_F(BouncingBallTest, Simulate) {
   const double t_final = 10.0;
