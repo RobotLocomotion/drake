dc5c97ec3cd5f6467347e7842764b52ad32cf708
Jon <jonathan.decastro@tri.global>
2018-02-12 14:42:14 -0500

    Add bindings for a small subset of maliput api and dragway backend

---
+++ bindings/pydrake/maliput/dragway_py.cc
@@ -0,0 +1,47 @@
+#include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+
+#include "drake/automotive/maliput/dragway/junction.h"
+#include "drake/automotive/maliput/dragway/lane.h"
+#include "drake/automotive/maliput/dragway/road_geometry.h"
+#include "drake/automotive/maliput/dragway/segment.h"
+#include "drake/bindings/pydrake/pydrake_pybind.h"
+
+namespace drake {
+namespace pydrake {
+
+PYBIND11_MODULE(dragway, m) {
+  // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
+  using namespace drake::maliput;
+
+  m.doc() = "Bindings for the Dragway backend.";
+
+  py::class_<dragway::RoadGeometry>(m, "RoadGeometry")
+      .def(py::init<api::RoadGeometryId, int, double, double, double, double,
+           double, double>(), py::arg("road_id"), py::arg("num_lanes"),
+           py::arg("length"), py::arg("lane_width"), py::arg("shoulder_width"),
+           py::arg("maximum_height"), py::arg("linear_tolerance"),
+           py::arg("angular_tolerance"))
+      .def("junction", &dragway::RoadGeometry::junction, py_reference_internal,
+           // Keep alive, reference: `return` keeps `self` alive.
+           py::keep_alive<0, 1>());
+
+  // TODO(jadecastro) Revisit whether or not the following belong instead as api
+  // bindings.
+  py::class_<dragway::Junction>(m, "Junction")
+      .def("segment", &dragway::Junction::segment, py_reference_internal,
+           // Keep alive, reference: `return` keeps `self` alive.
+           py::keep_alive<0, 1>());
+
+  py::class_<dragway::Segment>(m, "Segment")
+      .def("lane", &dragway::Segment::lane, py_reference_internal,
+           // Keep alive, reference: `return` keeps `self` alive.
+           py::keep_alive<0, 1>());
+
+  py::class_<dragway::Lane>(m, "Lane")
+      .def("ToLanePosition", &dragway::Lane::ToLanePosition)
+      .def("ToGeoPosition", &dragway::Lane::ToGeoPosition);
+}
+
+}  // namespace pydrake
+}  // namespace drake
