c4399b78a008e6c31fdb376a38ebd164faac0403
Jon <jonathan.decastro@tri.global>
2018-02-12 14:42:14 -0500

    Add bindings for a small subset of maliput api and dragway backend

---
+++ bindings/pydrake/maliput/api_py.cc
@@ -0,0 +1,54 @@
+#include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+
+#include "drake/automotive/maliput/api/junction.h"
+#include "drake/automotive/maliput/api/lane.h"
+#include "drake/automotive/maliput/api/lane_data.h"
+#include "drake/automotive/maliput/api/road_geometry.h"
+#include "drake/automotive/maliput/api/segment.h"
+#include "drake/bindings/pydrake/pydrake_pybind.h"
+
+namespace drake {
+namespace pydrake {
+
+PYBIND11_MODULE(api, m) {
+  // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
+  using namespace drake::maliput::api;
+
+  m.doc() = "Bindings for the Maliput API.";
+
+  py::class_<RoadGeometryId>(m, "RoadGeometryId")
+      .def(py::init<std::string>());
+
+  py::class_<GeoPosition> (m, "GeoPosition")
+      .def(py::init<double, double, double>(), py::arg("x"), py::arg("y"),
+           py::arg("z"))
+      .def("xyz", &GeoPosition::xyz);
+
+  py::class_<LanePosition>(m, "LanePosition")
+      .def(py::init<double, double, double>(), py::arg("s"), py::arg("r"),
+           py::arg("h"))
+      .def("srh", &LanePosition::srh);
+
+  py::class_<RoadGeometry>(m, "RoadGeometry")
+      .def("junction", &RoadGeometry::junction, py_reference_internal,
+           // Keep alive, reference: `return` keeps `self` alive.
+           py::keep_alive<0, 1>());
+
+  py::class_<Junction>(m, "Junction")
+      .def("segment", &Junction::segment, py_reference_internal,
+           // Keep alive, reference: `return` keeps `self` alive.
+           py::keep_alive<0, 1>());
+
+  py::class_<Segment>(m, "Segment")
+      .def("lane", &Segment::lane, py_reference_internal,
+           // Keep alive, reference: `return` keeps `self` alive.
+           py::keep_alive<0, 1>());
+
+  py::class_<Lane>(m, "Lane")
+      .def("ToLanePosition", &Lane::ToLanePosition)
+      .def("ToGeoPosition", &Lane::ToGeoPosition);
+}
+
+}  // namespace pydrake
+}  // namespace drake
