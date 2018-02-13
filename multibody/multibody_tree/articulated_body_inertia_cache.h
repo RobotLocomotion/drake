a3845adf7c539010434c3bc5a304e51737dfc55f
Lujing Cen <lujingcen@gmail.com>
2018-02-12 20:51:47 -0800

    address review

--- multibody/multibody_tree/articulated_body_inertia_cache.h
+++ multibody/multibody_tree/articulated_body_inertia_cache.h
@@ -14,10 +14,15 @@ namespace multibody {
 /// results of computations that are used in the recursive implementation of the
 /// articulated body algorithm.
 ///
-/// Articulated body cache entries include:
+/// Articulated body inertia cache entries include:
 /// - Articulated body inertia `Pplus_PB_W` for the articulated body subsystem
 ///   formed by all bodies outboard from body B, projected across its inboard
 ///   mobilizer, about point Bo, and expressed in the world frame W.
+///
+/// @note Another way to view `Pplus_PB_W` is to replace the parent body P with
+///       a massless, inertialess, 0-size body Z that is connected by the same
+///       inboard joint at Bo. The ordinary articulated body inertia `P_ZB_W`
+///       of body Z about point Bo is the same as the quantity `Pplus_PB_W`.
 ///
 /// @tparam T The mathematical type of the context, which must be a valid Eigen
 ///           scalar.
@@ -28,13 +33,13 @@ namespace multibody {
 ///
 /// They are already available to link against in the containing library.
 template<typename T>
-class ArticulatedBodyCache {
+class ArticulatedBodyInertiaCache {
  public:
-  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(ArticulatedBodyCache)
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(ArticulatedBodyInertiaCache)
 
   /// Constructs an articulated body cache entry for the given
   /// MultibodyTreeTopology.
-  explicit ArticulatedBodyCache(const MultibodyTreeTopology& topology) :
+  explicit ArticulatedBodyInertiaCache(const MultibodyTreeTopology& topology) :
       num_nodes_(topology.get_num_bodies()) {
     Allocate();
   }
