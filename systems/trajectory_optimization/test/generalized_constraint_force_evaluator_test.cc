0d34f49f830747385bc10cc8de0297adb72d4ddd
hongkai-dai <daihongkai@gmail.com>
2018-02-12 16:35:51 -0800

    Add a test on joint limit constraint force evaluator.

--- systems/trajectory_optimization/test/generalized_constraint_force_evaluator_test.cc
+++ systems/trajectory_optimization/test/generalized_constraint_force_evaluator_test.cc
@@ -49,6 +49,35 @@ GTEST_TEST(GeneralizedConstraintForceEvaluatorTest, TestEval) {
   EXPECT_TRUE(CompareMatrices(math::autoDiffToValueMatrix(ty), y_expected,
                               1E-10, MatrixCompareType::absolute));
 }
+
+GTEST_TEST(JointLimitConstraintForceEvaluatorTest, TestEval) {
+  auto tree = ConstructFourBarTree();
+
+  // Pretend that the four bar has a joint limit at joint 0.
+  JointLimitConstraintForceEvaluator evaluator(*tree, 0);
+  EXPECT_EQ(evaluator.num_lambda(), 2);
+
+  // Set q to some arbitrary number.
+  const Eigen::VectorXd q =
+      Eigen::VectorXd::LinSpaced(tree->get_num_positions(), 1, 5);
+  // Set lambda to some arbitrary number.
+  const Eigen::VectorXd lambda =
+      Eigen::VectorXd::LinSpaced(evaluator.num_lambda(), -3, 3);
+  const Eigen::VectorXd x = evaluator.CompositeEvalInputVector(q, lambda);
+  const auto tx = math::initializeAutoDiff(x);
+  AutoDiffVecXd ty;
+  evaluator.Eval(tx, ty);
+
+  Eigen::VectorXd y_expected =
+      Eigen::VectorXd::Zero(tree->get_num_velocities());
+  y_expected(0) =
+      lambda(
+          JointLimitConstraintForceEvaluator::LowerLimitForceIndexInLambda()) -
+      lambda(
+          JointLimitConstraintForceEvaluator::UpperLimitForceIndexInLambda());
+  EXPECT_TRUE(CompareMatrices(math::autoDiffToValueMatrix(ty), y_expected,
+                              1E-10, MatrixCompareType::absolute));
+}
 }  // namespace
 }  // namespace trajectory_optimization
 }  // namespace systems
