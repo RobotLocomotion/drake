3c2fb5e768225ee001d47a71d4aecf320eaa0c8c
Russ Tedrake <russt@mit.edu>
2018-02-14 05:12:27 -0500

    adds python bindings to barycentric mesh and dynamic programming algorithms

--- bindings/pydrake/systems/primitives_py.cc
+++ bindings/pydrake/systems/primitives_py.cc
@@ -3,6 +3,7 @@
 
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/systems/primitives/adder.h"
+#include "drake/systems/primitives/barycentric_system.h"
 #include "drake/systems/primitives/constant_value_source.h"
 #include "drake/systems/primitives/constant_vector_source.h"
 #include "drake/systems/primitives/integrator.h"
@@ -21,24 +22,30 @@ PYBIND11_MODULE(primitives, m) {
   using T = double;
 
   py::class_<ConstantVectorSource<T>, LeafSystem<T>>(m, "ConstantVectorSource")
-    .def(py::init<VectorX<T>>());
+      .def(py::init<VectorX<T>>());
 
   py::class_<ConstantValueSource<T>, LeafSystem<T>>(m, "ConstantValueSource");
 
-  py::class_<Adder<T>, LeafSystem<T>>(m, "Adder")
-    .def(py::init<int, int>());
+  py::class_<Adder<T>, LeafSystem<T>>(m, "Adder").def(py::init<int, int>());
 
   py::class_<Integrator<T>, LeafSystem<T>>(m, "Integrator")
-    .def(py::init<int>());
+      .def(py::init<int>());
 
   py::class_<ZeroOrderHold<T>, LeafSystem<T>>(m, "ZeroOrderHold")
-    .def(py::init<double, int>());
+      .def(py::init<double, int>());
 
   py::class_<SignalLogger<T>, LeafSystem<T>>(m, "SignalLogger")
-    .def(py::init<int>())
-    .def(py::init<int, int>())
-    .def("sample_times", &SignalLogger<T>::sample_times)
-    .def("data", &SignalLogger<T>::data);
+      .def(py::init<int>())
+      .def(py::init<int, int>())
+      .def("sample_times", &SignalLogger<T>::sample_times)
+      .def("data", &SignalLogger<T>::data);
+
+  py::class_<BarycentricMeshSystem<T>, LeafSystem<T>>(m,
+                                                      "BarycentricMeshSystem")
+      .def(py::init<math::BarycentricMesh<T>,
+                    const Eigen::Ref<const MatrixX<T>>&>())
+      .def("get_mesh", &BarycentricMeshSystem<T>::get_mesh)
+      .def("get_output_values", &BarycentricMeshSystem<T>::get_output_values);
 
   // TODO(eric.cousineau): Add more systems as needed.
 }
