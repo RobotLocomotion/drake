30c74d911c7146f7ee9c973c2dab59dc67922874
Andrés Valenzuela <andres.valenzuela@tri.global>
2018-02-14 08:10:42 -0500

    SchunkWsgPlainController implements StateFeedbackControllerInterface.

--- manipulation/schunk_wsg/schunk_wsg_plain_controller.h
+++ manipulation/schunk_wsg/schunk_wsg_plain_controller.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "drake/common/drake_copyable.h"
+#include "drake/systems/controllers/state_feedback_controller_interface.h"
 #include "drake/systems/framework/diagram.h"
 
 namespace drake {
@@ -16,7 +17,9 @@ enum class ControlMode {
 /// This class implements a controller for a Schunk WSG gripper as a
 /// `systems::Diagram`. The composition of this diagram is determined by the
 /// ControlMode specified for the controller.
-class SchunkWsgPlainController : public systems::Diagram<double> {
+class SchunkWsgPlainController
+    : public systems::Diagram<double>,
+      public systems::controllers::StateFeedbackControllerInterface<double> {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(SchunkWsgPlainController)
   explicit SchunkWsgPlainController(
@@ -39,6 +42,21 @@ class SchunkWsgPlainController : public systems::Diagram<double> {
 
   const systems::InputPortDescriptor<double>& get_max_force_input_port() const {
     return this->get_input_port(max_force_input_port_);
+  }
+
+  // Implement StateFeedbackControllerInterface
+  virtual const systems::InputPortDescriptor<double>&
+  get_input_port_estimated_state() const override {
+    return get_state_input_port();
+  }
+
+  virtual const systems::InputPortDescriptor<double>&
+  get_input_port_desired_state() const override {
+    return get_desired_state_input_port();
+  }
+
+  virtual const systems::OutputPort<double>& get_output_port_control() const override {
+    return get_output_port(0);
   }
 
  private:
