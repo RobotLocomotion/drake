eccab961b231a1a93fbd43a23646f5360df14234
Eric Cousineau <eric.cousineau@tri.global>
2018-02-14 15:54:59 -0500

    libdrake: Fix build_componets_refresh

--- tools/install/libdrake/build_components_refresh.py
+++ tools/install/libdrake/build_components_refresh.py
@@ -20,15 +20,16 @@ def _remove_dev(components):
 
 
 def _find_libdrake_components():
-    query_string = ' '.join([
-        'kind("cc_library",'
-        'visible("//tools/install/libdrake:libdrake.so",'
-        '"//drake/..."))',
-        'except("//drake/examples/...")'
-        'except(attr("testonly", "1", "//drake/..."))'
-        'except("//drake/lcmtypes/...")'
-        'except("//drake:*")'
-    ])
+    query_string = """
+kind("cc_library", visible("//tools/install/libdrake:libdrake.so", "//..."))
+    except(attr("testonly", "1", "//..."))
+    except("//:*")
+    except("//common:text_logging_gflags")
+    except("//common/proto:protobuf_ubsan_fixup")
+    except("//examples/...")
+    except("//lcmtypes/...")
+    except("//tools/install/libdrake:*")
+"""
     command = ["bazel", "query", query_string]
     components = [x for x in subprocess.check_output(command).split('\n') if x]
     components = _remove_dev(components)
