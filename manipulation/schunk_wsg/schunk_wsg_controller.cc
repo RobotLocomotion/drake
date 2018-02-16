b518f561fea1cd82f672706fa29b8977732a3cf9
Andrés Valenzuela <andres.valenzuela@tri.global>
2018-02-16 16:08:11 -0500

    More glass-boxing

--- manipulation/schunk_wsg/schunk_wsg_controller.cc
+++ manipulation/schunk_wsg/schunk_wsg_controller.cc
@@ -2,6 +2,7 @@
 
 #include <vector>
 
+#include "drake/common/default_scalars.h"
 #include "drake/manipulation/schunk_wsg/schunk_wsg_low_level_controller.h"
 #include "drake/manipulation/schunk_wsg/schunk_wsg_position_controller.h"
 #include "drake/systems/controllers/pid_controller.h"
@@ -17,35 +18,38 @@ namespace drake {
 namespace manipulation {
 namespace schunk_wsg {
 
-SchunkWsgController::SchunkWsgController() {
-  systems::DiagramBuilder<double> builder;
+template <typename T>
+SchunkWsgController<T>::SchunkWsgController() {
+  systems::DiagramBuilder<T> builder;
 
   // Add low-level controller. This makes the gripper behave as though it had
   // only one degree of freedom.
-  auto low_level_controller = builder.AddSystem<SchunkWsgLowLevelController>();
+  auto low_level_controller =
+      builder.template AddSystem<SchunkWsgLowLevelController<T>>();
 
   // Add position controller. This controls the separation of the fingers.
-  auto position_controller = builder.AddSystem<SchunkWsgPositionController>();
+  auto position_controller =
+      builder.template AddSystem<SchunkWsgPositionController<T>>();
 
   // Export the inputs.
   estimated_state_input_port_ = builder.ExportInput(
-      low_level_controller->get_estimated_joint_state_input_port());
+      low_level_controller->get_input_port_estimated_joint_state());
   desired_state_input_port_ =
       builder.ExportInput(position_controller->get_input_port_desired_state());
   max_force_input_port_ =
-      builder.ExportInput(position_controller->get_max_force_input_port());
+      builder.ExportInput(position_controller->get_input_port_max_force());
 
   // Export the outputs.
   builder.ExportOutput(
-      low_level_controller->get_comanded_joint_force_output_port());
+      low_level_controller->get_output_port_commanded_joint_force());
 
   // Connect the subsystems.
   // position_controller -> low_level_controller
   builder.Connect(position_controller->get_output_port_control(),
-                  low_level_controller->get_commanded_grip_force_input_port());
+                  low_level_controller->get_input_port_commanded_grip_force());
 
   // low_level_controller -> position_controller
-  builder.Connect(low_level_controller->get_estimated_grip_state_output_port(),
+  builder.Connect(low_level_controller->get_output_port_estimated_grip_state(),
                   position_controller->get_input_port_estimated_state());
 
   builder.BuildInto(this);
@@ -54,3 +58,6 @@ SchunkWsgController::SchunkWsgController() {
 }  // namespace schunk_wsg
 }  // namespace manipulation
 }  // namespace drake
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
+    class ::drake::manipulation::schunk_wsg::SchunkWsgController);
--- manipulation/schunk_wsg/schunk_wsg_controller.h
+++ manipulation/schunk_wsg/schunk_wsg_controller.h
@@ -8,44 +8,60 @@ namespace drake {
 namespace manipulation {
 namespace schunk_wsg {
 
-enum class ControlMode {
-  kPosition = 0,
-  kForce = 1,
-  kPositionAndForce = 2,
-};
-
-/// This class implements a controller for a Schunk WSG gripper as a
-/// `systems::Diagram`. The composition of this diagram is determined by the
-/// ControlMode specified for the controller.
+/* This class implements a PID controller for the distance between the fingers
+ * of a parallel-jaw gripper.
+ * ```
+ *                 ┌─────────────────────────────────────────────────┐
+ *                 │                                                 │
+ *                 │    ┌──────────┐                                 │
+ *                 └───▶│          │commanded                        │
+ *                      │Schunk    │grip force                       │
+ *       max force ────▶│Position  ├──────────┐                grip  │
+ *                      │Controller│          │   ┌──────────┐ state │
+ *   desired state ────▶│          │          └──▶│Schunk    ├───────┘
+ *                      └──────────┘              │LowLevel  │
+ * estimated state ──────────────────────────────▶│Controller├─────────▶ control
+ *                                                └──────────┘
+ * ```
+ * Instantiated templates for the following kinds of T's are provided:
+ * - double
+ * - AutoDiffXd
+ * - symbolic::Expression
+ *
+ * They are already available to link against in the containing library.
+ * No other values for T are currently supported.
+ */
+template <typename T>
 class SchunkWsgController
-    : public systems::Diagram<double>,
-      public systems::controllers::StateFeedbackControllerInterface<double> {
+    : public systems::Diagram<T>,
+      public systems::controllers::StateFeedbackControllerInterface<T> {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(SchunkWsgController)
   explicit SchunkWsgController();
 
-  const systems::InputPortDescriptor<double>& get_desired_state_input_port()
+  const systems::InputPortDescriptor<T>& get_desired_state_input_port()
       const {
     return this->get_input_port(desired_state_input_port_);
   }
 
-  const systems::InputPortDescriptor<double>& get_max_force_input_port() const {
+  const systems::InputPortDescriptor<T>& get_max_force_input_port() const {
     return this->get_input_port(max_force_input_port_);
   }
 
   // Implement StateFeedbackControllerInterface
-  virtual const systems::InputPortDescriptor<double>&
+  virtual const systems::InputPortDescriptor<T>&
   get_input_port_estimated_state() const override {
     return this->get_input_port(estimated_state_input_port_);
   }
 
-  virtual const systems::InputPortDescriptor<double>&
+  virtual const systems::InputPortDescriptor<T>&
   get_input_port_desired_state() const override {
     return this->get_input_port(desired_state_input_port_);
   }
 
-  virtual const systems::OutputPort<double>& get_output_port_control() const override {
-    return get_output_port(0);
+  virtual const systems::OutputPort<T>& get_output_port_control()
+      const override {
+    return this->get_output_port(0);
   }
 
  private:
