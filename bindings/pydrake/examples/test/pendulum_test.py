eeb72f8534a9b79f0115e6129b86d994a277c157
Russ Tedrake <russt@mit.edu>
2018-02-17 04:22:35 -0500

    bind pendulum inputs, states, and params

--- bindings/pydrake/examples/test/pendulum_test.py
+++ bindings/pydrake/examples/test/pendulum_test.py
@@ -8,7 +8,7 @@ import numpy as np
 
 import pydrake.systems.framework as framework
 from pydrake.examples.pendulum import (
-    PendulumPlant,
+    PendulumInput, PendulumParams, PendulumPlant, PendulumState
     )
 from pydrake.systems.analysis import (
     Simulator
@@ -19,31 +19,51 @@ from pydrake.systems.primitives import (
 
 
 class TestPendulum(unittest.TestCase):
+    def test_input(self):
+        input = PendulumInput()
+        input.set_tau(1.)
+        self.assertEqual(input.tau(), 1.)
+
+    def test_params(self):
+        params = PendulumParams()
+        params.set_mass(1.)
+        params.set_length(2.)
+        params.set_damping(3.)
+        params.set_gravity(4.)
+        self.assertEqual(params.mass(), 1.)
+        self.assertEqual(params.length(), 2.)
+        self.assertEqual(params.damping(), 3.)
+        self.assertEqual(params.gravity(), 4.)
+
+    def test_state(self):
+        state = PendulumState()
+        state.set_theta(1.)
+        state.set_thetadot(2.)
+        self.assertEqual(state.theta(), 1.)
+        self.assertEqual(state.thetadot(), 2.)
+
     def test_simulation(self):
         # Basic constant-torque pendulum simulation.
+        pendulum = PendulumPlant()
 
-        builder = framework.DiagramBuilder()
+        # Create the simulator.
+        simulator = Simulator(pendulum)
+        context = simulator.get_mutable_context()
 
-        pendulum = builder.AddSystem(PendulumPlant())
-        source = builder.AddSystem(ConstantVectorSource([1.0]))
+        # Set an input torque.
+        input = PendulumInput()
+        input.set_tau(1.)
+        context.FixInputPort(0, input)
 
-        builder.Connect(source.get_output_port(0),
-                        pendulum.get_input_port(0))
+        # Set the initial state.
+        state = context.get_mutable_continuous_state_vector()
+        state.set_theta(1.)
+        state.set_thetadot(0.)
 
-        diagram = builder.Build()
-        simulator = Simulator(diagram)
-        simulator.Initialize()
-
-        # TODO(russt): Clean up state vector access below.
-        state = simulator.get_mutable_context().get_mutable_state()\
-                         .get_mutable_continuous_state().get_mutable_vector()
-
-        initial_state = np.array([1.0, 0.0])
-        state.SetFromVector(initial_state)
-
+        # Simulate (and make sure the state actually changes).
+        initial_state = state.CopyToVector()
         simulator.StepTo(1.0)
-
-        self.assertFalse((state.CopyToVector() == initial_state).all())
+        self.assertFalse((state.CopyToVector() == initial_state).any())
 
 
 if __name__ == '__main__':
