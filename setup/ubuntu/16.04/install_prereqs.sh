5f60cdf3783f9645db250bdd666539cca84755a8
Jamie Snape <jamie.snape@kitware.com>
2018-02-20 11:26:57 -0500

    Get apt packages to install from separate text files
    Also remove cmake-curses-gui which is not needed.

--- setup/ubuntu/16.04/install_prereqs.sh
+++ setup/ubuntu/16.04/install_prereqs.sh
@@ -17,6 +17,10 @@ trap at_exit EXIT
 # Dependencies that are installed by the following sourced script that are
 # needed when developing with binary distributions are also needed when
 # developing with source distributions.
+#
+# Note that the list of packages in binary_distribution/packages.txt is used to
+# generate the dependencies of the drake .deb package, so does not include
+# development dependencies such as build-essential and cmake.
 
 source "${BASH_SOURCE%/*}/binary_distribution/install_prereqs.sh"
 
