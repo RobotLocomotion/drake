1dfbf1471a05fa51e33c891828dabf1c54256a3f
Matt Marjanovic <maddog@tri.global>
2018-02-23 11:57:48 -0800

    Create SimpleRulebook, a simple implementation of RoadRulebook.
    Also:
     * Add a tolerance parameter to RoadRulebook::FindRules().
     * Make rule-specific test-helpers publicly available as test_utilities.

--- automotive/maliput/api/test/rules_speed_limit_test.cc
+++ automotive/maliput/api/test/rules_speed_limit_test.cc
@@ -6,41 +6,13 @@
 #include <gtest/gtest.h>
 
 #include "drake/automotive/maliput/api/rules/regions.h"
+#include "drake/automotive/maliput/api/test_utilities/rules_speed_limit_compare.h"
 #include "drake/automotive/maliput/api/test_utilities/rules_test_utilities.h"
 
 namespace drake {
 namespace maliput {
 namespace api {
 namespace rules {
-
-namespace test {
-
-/// Predicate-formatter which tests equality of SpeedLimitRule::Severity.
-// TODO(maddog@tri.global)  This should be replaced by a generic predicate
-//                          which handles anything with operator==.
-::testing::AssertionResult IsEqual(const char* a_expression,
-                                   const char* b_expression,
-                                   rules::SpeedLimitRule::Severity a,
-                                   rules::SpeedLimitRule::Severity b) {
-  return ::testing::internal::CmpHelperEQ(a_expression, b_expression, a, b);
-}
-
-/// Predicate-formatter which tests equality of SpeedLimitRule.
-::testing::AssertionResult IsEqual(const char* a_expression,
-                                   const char* b_expression,
-                                   const rules::SpeedLimitRule& a,
-                                   const rules::SpeedLimitRule& b) {
-  AssertionResultCollector c;
-  MALIPUT_ADD_RESULT(c, MALIPUT_IS_EQUAL(a.id(), b.id()));
-  MALIPUT_ADD_RESULT(c, MALIPUT_IS_EQUAL(a.zone(), b.zone()));
-  MALIPUT_ADD_RESULT(c, MALIPUT_IS_EQUAL(a.severity(), b.severity()));
-  MALIPUT_ADD_RESULT(c, MALIPUT_IS_EQUAL(a.max(), b.max()));
-  MALIPUT_ADD_RESULT(c, MALIPUT_IS_EQUAL(a.min(), b.min()));
-  return c.result();
-}
-
-}  // namespace test
-
 namespace {
 
 const LaneSRange kZone(LaneId("the_lane"), SRange(13., 15.));
