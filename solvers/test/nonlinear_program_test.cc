97df618a485bbb68a63192ef62052543c6234342
Hongkai Dai <daihongkai@gmail.com>
2018-02-24 20:49:36 -0800

    Add a unit length constraint program for testing (#8158)
    * Add a unit length constraint program for testing
    
    * Address Eric's comments.
    
    * Address Sherm's comments.

--- solvers/test/nonlinear_program_test.cc
+++ solvers/test/nonlinear_program_test.cc
@@ -382,6 +382,18 @@ GTEST_TEST(testNonlinearProgram, ConvexCubicProgramExample) {
   prob.SetInitialGuessForAllVariables(Vector1d(1));
   RunNonlinearProgram(&prob, [&]() {prob.CheckSolution();});
 }
+
+GTEST_TEST(testNonlinearProgram, UnitLengthConstraint) {
+  UnitLengthProgramExample prob;
+
+  prob.SetInitialGuessForAllVariables(Vector4d(1, 0, 0, 0));
+  RunNonlinearProgram(&prob, [&prob]() {prob.CheckSolution(1E-8);});
+
+  // Try a different initial guess, that doesn't satisfy the unit length
+  // constraint.
+  prob.SetInitialGuessForAllVariables(Vector4d(1, 2, 3, 4));
+  RunNonlinearProgram(&prob, [&prob]() {prob.CheckSolution(1E-8);});
+}
 }  // namespace test
 }  // namespace solvers
 }  // namespace drake
