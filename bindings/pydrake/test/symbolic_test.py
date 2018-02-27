cdaa25ca6b32906217ea6760d3674b6d95a7deb4
Soonho Kong <soonho.kong@tri.global>
2018-02-27 14:33:09 -0500

    Merge symbolic_{monomial, variables}_test to symbolic_test in pydrake

--- bindings/pydrake/test/symbolic_test.py
+++ bindings/pydrake/test/symbolic_test.py
@@ -10,9 +10,12 @@ import pydrake.symbolic as sym
 x = sym.Variable("x")
 y = sym.Variable("y")
 z = sym.Variable("z")
+w = sym.Variable("w")
 a = sym.Variable("a")
 b = sym.Variable("b")
 c = sym.Variable("c")
+e_x = sym.Expression(x)
+e_y = sym.Expression(y)
 
 
 class TestSymbolicVariables(unittest.TestCase):
@@ -84,6 +87,106 @@ class TestSymbolicVariables(unittest.TestCase):
                          "((y = 2) and (x >= 1) and (x <= 2))")
         self.assertEqual(str(sym.logical_or(x >= 1, x <= 2, y == 2)),
                          "((y = 2) or (x >= 1) or (x <= 2))")
+
+
+class TestSymbolicVariables(unittest.TestCase):
+    def test_default_constructor(self):
+        vars = sym.Variables()
+        self.assertEqual(vars.size(), 0)
+        self.assertTrue(vars.empty())
+
+    def test_constructor_list(self):
+        vars = sym.Variables([x, y, z])
+        self.assertEqual(vars.size(), 3)
+
+    def test_insert1(self):
+        vars = sym.Variables()
+        vars.insert(x)
+        self.assertEqual(vars.size(), 1)
+
+    def test_insert2(self):
+        vars = sym.Variables([x])
+        vars.insert(sym.Variables([y, z]))
+        self.assertEqual(vars.size(), 3)
+
+    def test_erase1(self):
+        vars = sym.Variables([x, y, z])
+        count = vars.erase(x)
+        self.assertEqual(count, 1)
+
+    def test_erase2(self):
+        vars1 = sym.Variables([x, y, z])
+        vars2 = sym.Variables([w, z])
+        count = vars1.erase(vars2)
+        self.assertEqual(count, 1)
+        self.assertEqual(vars1.size(), 2)
+
+    def test_include(self):
+        vars = sym.Variables([x, y, z])
+        self.assertTrue(vars.include(y))
+
+    def test_to_string(self):
+        vars = sym.Variables()
+        vars.insert(x)
+        vars.insert(y)
+        vars.insert(z)
+        self.assertEqual(vars.to_string(), "{x, y, z}")
+
+    def test_subset_properties(self):
+        vars1 = sym.Variables([x, y, z])
+        vars2 = sym.Variables([x, y])
+        self.assertFalse(vars1.IsSubsetOf(vars2))
+        self.assertFalse(vars1.IsStrictSubsetOf(vars2))
+        self.assertTrue(vars1.IsSupersetOf(vars2))
+        self.assertTrue(vars1.IsStrictSupersetOf(vars2))
+
+    def test_eq(self):
+        vars1 = sym.Variables([x, y, z])
+        vars2 = sym.Variables([x, y])
+        self.assertFalse(vars1 == vars2)
+
+    def test_lt(self):
+        vars1 = sym.Variables([x, y])
+        vars2 = sym.Variables([x, y, z])
+        self.assertTrue(vars1 < vars2)
+
+    def test_add(self):
+        vars1 = sym.Variables([x, y])
+        vars2 = sym.Variables([y, z])
+        vars3 = vars1 + vars2  # [x, y, z]
+        self.assertEqual(vars3.size(), 3)
+        vars4 = vars1 + z  # [x, y, z]
+        self.assertEqual(vars4.size(), 3)
+        vars5 = x + vars1  # [x, y]
+        self.assertEqual(vars5.size(), 2)
+
+    def test_add_assignment(self):
+        vars = sym.Variables([x])
+        vars += y
+        self.assertEqual(vars.size(), 2)
+        vars += sym.Variables([x, z])
+        self.assertEqual(vars.size(), 3)
+
+    def test_sub(self):
+        vars1 = sym.Variables([x, y])
+        vars2 = sym.Variables([y, z])
+        vars3 = vars1 - vars2  # [x]
+        self.assertEqual(vars3, sym.Variables([x]))
+        vars4 = vars1 - y  # [x]
+        self.assertEqual(vars4, sym.Variables([x]))
+
+    def test_sub_assignment(self):
+        vars = sym.Variables([x, y, z])
+        vars -= y  # = [x, z]
+        self.assertEqual(vars, sym.Variables([x, z]))
+        vars -= sym.Variables([x])  # = [z]
+        self.assertEqual(vars, sym.Variables([z]))
+
+    def test_intersect(self):
+        vars1 = sym.Variables([x, y, z])
+        vars2 = sym.Variables([y, w])
+        vars3 = sym.intersect(vars1, vars2)  # = [y]
+        self.assertEqual(vars3, sym.Variables([y]))
 
 
 class TestSymbolicExpression(unittest.TestCase):
@@ -174,6 +277,117 @@ class TestSymbolicExpression(unittest.TestCase):
         self.assertEqual(J[0, 1], - x * sym.sin(y))
         self.assertEqual(J[1, 1], x * sym.cos(y))
         self.assertEqual(J[2, 1], 0)
+
+
+class TestSymbolicMonomial(unittest.TestCase):
+    def test_constructor_variable(self):
+        m = sym.Monomial(x)  # m = x¹
+        self.assertEqual(m.degree(x), 1)
+        self.assertEqual(m.total_degree(), 1)
+
+    def test_constructor_variable_int(self):
+        m = sym.Monomial(x, 2)  # m = x²
+        self.assertEqual(m.degree(x), 2)
+        self.assertEqual(m.total_degree(), 2)
+
+    def test_constructor_map(self):
+        powers_in = {x: 2, y: 3, z: 4}
+        m = sym.Monomial(powers_in)
+        powers_out = m.get_powers()
+        self.assertEqual(powers_out[x], 2)
+        self.assertEqual(powers_out[y], 3)
+        self.assertEqual(powers_out[z], 4)
+
+    def test_comparison(self):
+        # m1 = m2 = x²
+        m1 = sym.Monomial(x, 2)
+        m2 = sym.Monomial(x, 2)
+        m3 = sym.Monomial(x, 1)
+        m4 = sym.Monomial(y, 2)
+        # Test operator==
+        self.assertTrue(m1 == m2)
+        self.assertFalse(m1 == m3)
+        self.assertFalse(m1 == m4)
+        self.assertFalse(m2 == m3)
+        self.assertFalse(m2 == m4)
+        self.assertFalse(m3 == m4)
+        # Test operator!=
+        self.assertFalse(m1 != m2)
+        self.assertTrue(m1 != m3)
+        self.assertTrue(m1 != m4)
+        self.assertTrue(m2 != m3)
+        self.assertTrue(m2 != m4)
+        self.assertTrue(m3 != m4)
+
+    def test_str(self):
+        m1 = sym.Monomial(x, 2)
+        self.assertEqual(str(m1), "x^2")
+        m2 = m1 * sym.Monomial(y)
+        self.assertEqual(str(m2), "x^2 * y")
+
+    def test_multiplication1(self):
+        m1 = sym.Monomial(x, 2)
+        m2 = sym.Monomial(y, 3)
+        m3 = m1 * m2
+        self.assertEqual(m3.degree(x), 2)
+        self.assertEqual(m3.degree(y), 3)
+
+    def test_multiplication2(self):
+        m1 = sym.Monomial(x, 2)
+        m2 = m1 * sym.Monomial(y)
+        m3 = sym.Monomial(y) * m1
+        self.assertEqual(m2.degree(x), 2)
+        self.assertEqual(m2.degree(y), 1)
+        self.assertEqual(m2, m3)
+
+    def test_multiplication_assignment1(self):
+        m = sym.Monomial(x, 2)
+        m *= sym.Monomial(y, 3)
+        self.assertEqual(m.degree(x), 2)
+        self.assertEqual(m.degree(y), 3)
+
+    def test_multiplication_assignment2(self):
+        m = sym.Monomial(x, 2)
+        m *= sym.Monomial(y)
+        self.assertEqual(m.degree(x), 2)
+        self.assertEqual(m.degree(y), 1)
+
+    def test_pow(self):
+        m1 = sym.Monomial(x, 2) * sym.Monomial(y)  # m1 = x²y
+        m2 = m1 ** 2                 # m2 = x⁴y²
+        self.assertEqual(m2.degree(x), 4)
+        self.assertEqual(m2.degree(y), 2)
+
+    def test_pow_in_place(self):
+        m1 = sym.Monomial(x, 2) * sym.Monomial(y)  # m1 = x²y
+        m2 = m1.pow_in_place(2)      # m1 = m2 = x⁴y²
+        self.assertEqual(m1.degree(x), 4)
+        self.assertEqual(m1.degree(y), 2)
+        self.assertEqual(m2.degree(x), 4)
+        self.assertEqual(m2.degree(y), 2)
+
+    def test_get_powers(self):
+        m = sym.Monomial(x, 2) * sym.Monomial(y)  # m = x²y
+        powers = m.get_powers()
+        self.assertEqual(powers[x], 2)
+        self.assertEqual(powers[y], 1)
+
+    def test_to_expression(self):
+        m = sym.Monomial(x, 3) * sym.Monomial(y)  # m = x³y
+        e = m.ToExpression()
+        self.assertEqual(str(e), "(pow(x, 3) * y)")
+
+    def test_get_variables(self):
+        m = sym.Monomial(x, 3) * sym.Monomial(y)  # m = x³y
+        vars = m.GetVariables()  # = [x, y]
+        self.assertEqual(vars.size(), 2)
+
+    def test_monomial_basis(self):
+        vars = sym.Variables([x, y, z])
+        basis1 = sym.MonomialBasis(vars, 3)
+        basis2 = sym.MonomialBasis([x, y, z], 3)
+        self.assertEqual(basis1.size, 20)
+        self.assertEqual(basis2.size, 20)
 
 
 class TestSymbolicPolynomial(unittest.TestCase):
