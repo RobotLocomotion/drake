f99e9151eb96c393cc8a3b66b1fb24a96e4d5620
mitiguy <paul.mitiguy@tri.global>
2018-03-01 13:32:50 -0800

    Reorganize files in preparation for moving file  drake/multibody/multibody_tree/math/rotation_matrix.h  to  drake/math/rotation_matrix.h

--- math/rotation_matrix_utilities.cc
+++ math/rotation_matrix_utilities.cc
@@ -1,4 +1,4 @@
 // For now, this is an empty .cc file that only serves to confirm
 // rotation_matrix.h is a stand-alone header.
 
-#include "drake/math/rotation_matrix.h"
+#include "drake/math/rotation_matrix_utilities.h"
