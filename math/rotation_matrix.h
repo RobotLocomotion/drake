39f70994794c21d2df1620a20d523eba0f682cf6
mitiguy <paul.mitiguy@tri.global>
2018-03-02 12:16:14 -0800

    Update to respond to reviewer comments

--- math/rotation_matrix.h
+++ math/rotation_matrix.h
@@ -1,3 +1,20 @@
 #pragma once
 
+// TODO(mitiguy) Merge Rotation class from
+// drake/multibody/multibody_tree/math/rotation.h into
+// drake/math/rotation_matrix.h.
+// The goal of PR #8217 is a first step to replace the "old" rotation_matrix.h
+// file with the "new" rotation_matrix.h file which contains the new
+// RotationMatrix class.  The process being employed will be able to make this
+// replacement without changing #include drake/math/rotation_matrix.h statements
+// in the multiplicity of its uses in currently existing Drake code.
+// For PR #8217, the process is as follows:
+// * Rename drake/math/rotation_matrix.h to
+//   drake/math/rotation_matrix_utilities.h.
+// * Rename drake/math/rotation_matrix.cc to
+//   drake/math/rotation_matrix_utilities.cc.
+// * Create new (mostly-empty) files:
+//   drake/math/rotation_matrix.h
+//   drake/math/rotation_matrix.cc.
+// * Update build files and #include statements accordingly.
 #include "drake/math/rotation_matrix_utilities.h"
--- math/rotation_matrix_utilities.cc
+++ math/rotation_matrix_utilities.cc
@@ -1,4 +1,4 @@
 // For now, this is an empty .cc file that only serves to confirm
-// rotation_matrix.h is a stand-alone header.
+// rotation_matrix_utilities.h is a stand-alone header.
 
 #include "drake/math/rotation_matrix_utilities.h"
