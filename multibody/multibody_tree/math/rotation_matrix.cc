2076fa53f07cdbb2e81ad11ec21b26c94f239862
mitiguy <paul.mitiguy@tri.global>
2018-03-01 17:14:41 -0800

    Add mostly empty placeholder files

---
+++ multibody/multibody_tree/math/rotation_matrix.cc
@@ -0,0 +1,5 @@
+// For now, this is an empty .cc file that only serves to confirm
+// rotation_matrix.h is a stand-alone header.
+
+#include "drake/math/rotation_matrix.h"
+
---
+++ multibody/multibody_tree/math/rotation_matrix.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// TODO(mitiguy) Merge Rotation class from 
+// drake/multibody/multibody_tree/math/rotation.h into drake/math/rotation.h.
+#include "drake/math/rotation_matrix_utilities.h"
+
