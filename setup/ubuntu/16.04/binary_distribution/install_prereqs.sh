d9d56dcaf3532ad4493a85d7087807c2e9bde56c
Jeremy Nimmer <jeremy.nimmer@tri.global>
2018-03-01 20:48:30 -0500

    Use dreal and ibex from workspace (not apt) on Ubuntu
    When downstream projects want to bump around which commit they are
    using, its a pain to have to touch anything to do with apt packages.
    Having Bazel fetch what's needed is an ergonomics improvement.
    
    In the future, this PR might also enable us to remove the Drake apt
    source from our developer setup instructions.

--- setup/ubuntu/16.04/binary_distribution/install_prereqs.sh
+++ setup/ubuntu/16.04/binary_distribution/install_prereqs.sh
@@ -24,6 +24,38 @@ if [[ "$(lsb_release -sc)" != 'xenial' ]]; then
   exit 2
 fi
 
+# This script used to install some custom packages directly; here, we'll set
+# them to `auto` mode so that Ubuntu will suggest to remove them if nothing
+# else is using them.
+mark_auto() {
+  package="$1"
+  shift
+
+  installed=$(dpkg-query --showformat='${Version}\n' --show "${package}" 2>/dev/null || true)
+  if [[ -z "${installed}" ]]; then
+    return
+  fi
+
+  for version in "$@"; do
+    if [[ "${version}" == "${installed}" ]]; then
+      apt-mark auto "${package}"
+      return
+    fi
+  done
+}
+mark_auto \
+  dreal \
+  4.17.12.2 \
+  4.17.12.3 \
+  4.18.01.3 \
+  4.18.02.2 \
+  4.18.02.4
+mark_auto \
+  libibex-dev \
+  2.6.3 \
+  2.6.5.20180123154310.gitf618c7b296182f90a84d54936d144b87df0747b9~16.04 \
+  2.6.5.20180211084215.gitd1419538b4d818ed1cf21a01896bc5eaae5d1d57~16.04
+
 wget -O - https://drake-apt.csail.mit.edu/drake.pub.gpg | apt-key add
 echo 'deb [arch=amd64] https://drake-apt.csail.mit.edu xenial main' > /etc/apt/sources.list.d/drake.list
 
