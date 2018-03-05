d822f9da1d243742ed035cd2ddf47ece78262d16
mitiguy <paul.mitiguy@tri.global>
2018-03-05 13:47:24 -0800

    Rename rotation_matrix_utilities*  to  rotation_matrix_deprecates*

--- math/rotation_matrix.h
+++ math/rotation_matrix.h
@@ -3,4 +3,4 @@
 // TODO(mitiguy) Merge Rotation class from
 // drake/multibody/multibody_tree/math/rotation.h into
 // drake/math/rotation_matrix.h.
-#include "drake/math/rotation_matrix_utilities.h"
+#include "drake/math/rotation_matrix_deprecated.h"
