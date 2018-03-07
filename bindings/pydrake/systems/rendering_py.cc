25c0b72c23affed967bb48aba0634f9eb31d7185
Jon <jonathan.decastro@tri.global>
2018-03-06 21:22:19 -0500

    Bind PoseBundle, FrameVelocity, PoseAggregator

--- bindings/pydrake/systems/rendering_py.cc
+++ bindings/pydrake/systems/rendering_py.cc
@@ -2,7 +2,11 @@
 #include "pybind11/pybind11.h"
 
 #include "drake/bindings/pydrake/pydrake_pybind.h"
+#include "drake/bindings/pydrake/systems/systems_pybind.h"
 #include "drake/bindings/pydrake/util/eigen_geometry_pybind.h"
+#include "drake/multibody/multibody_tree/math/spatial_velocity.h"
+#include "drake/systems/rendering/frame_velocity.h"
+#include "drake/systems/rendering/pose_aggregator.h"
 #include "drake/systems/rendering/pose_vector.h"
 
 namespace drake {
@@ -20,6 +24,28 @@ PYBIND11_MODULE(rendering, m) {
 
   using T = double;
 
+  py::class_<PoseAggregator<T>, LeafSystem<T>>(m, "PoseAggregator")
+    .def(py::init<>())
+    .def("AddSingleInput", &PoseAggregator<T>::AddSingleInput,
+         py_reference_internal)
+    .def("AddSinglePoseAndVelocityInput",
+         &PoseAggregator<T>::AddSinglePoseAndVelocityInput)
+    .def("AddBundleInput", &PoseAggregator<T>::AddBundleInput,
+         py_reference_internal);
+
+  py::class_<PoseBundle<T>>(m, "PoseBundle")
+    .def(py::init<int>())
+    .def("get_num_poses", &PoseBundle<T>::get_num_poses)
+    .def("get_pose", &PoseBundle<T>::get_pose)
+    .def("set_pose", &PoseBundle<T>::set_pose)
+    .def("get_velocity", &PoseBundle<T>::get_velocity)
+    .def("set_velocity", &PoseBundle<T>::set_velocity)
+    .def("get_name", &PoseBundle<T>::get_name)
+    .def("set_name", &PoseBundle<T>::set_name)
+    .def("get_model_instance_id", &PoseBundle<T>::get_model_instance_id)
+    .def("set_model_instance_id", &PoseBundle<T>::set_model_instance_id);
+  pysystems::AddValueInstantiation<PoseBundle<T>>(m);
+
   py::class_<PoseVector<T>, BasicVector<T>> pose_vector(m, "PoseVector");
   pose_vector
     .def(py::init())
@@ -30,6 +56,25 @@ PYBIND11_MODULE(rendering, m) {
     .def("set_rotation", &PoseVector<T>::set_rotation);
 
   pose_vector.attr("kSize") = int{PoseVector<T>::kSize};
+
+  // N.B. We simplify the get/set signatures below to temporarily sidestep
+  // dependence on multibody.
+  // TODO(jadecastro) Revert this once the bindings for SpatialVelocity exist.
+  py::class_<FrameVelocity<T>, BasicVector<T>> frame_velocity(
+      m, "FrameVelocity");
+  frame_velocity
+    .def(py::init())
+    .def("get_velocity",
+         [](const FrameVelocity<T>* self) {
+           multibody::SpatialVelocity<T> result = self->get_velocity();
+           return std::make_pair(result.rotational(), result.translational());
+         })
+    .def("set_velocity",
+         [](FrameVelocity<T>* self, const Vector3<T>& w, const Vector3<T>& v) {
+           self->set_velocity(multibody::SpatialVelocity<T>(w, v));
+         }, py::arg("w"), py::arg("v"));
+
+  frame_velocity.attr("kSize") = int{FrameVelocity<T>::kSize};
 
   // TODO(eric.cousineau): Add more systems as needed.
 }
--- bindings/pydrake/systems/test/rendering_test.py
+++ bindings/pydrake/systems/test/rendering_test.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from pydrake.systems.rendering import (
+    FrameVelocity,
+    PoseBundle,
+    PoseAggregator,
     PoseVector,
 )
 
@@ -9,6 +12,7 @@ import unittest
 import numpy as np
 
 from pydrake.systems.framework import (
+    AbstractValue,
     BasicVector,
 )
 from pydrake.util.eigen_geometry import (
@@ -22,6 +26,41 @@ def normalized(x):
 
 
 class TestRendering(unittest.TestCase):
+    def test_pose_bundle(self):
+        num_poses = 7
+        bundle = PoseBundle(num_poses)
+        # - Accessors.
+        self.assertEqual(bundle.get_num_poses(), num_poses)
+        self.assertTrue(isinstance(bundle.get_pose(0), Isometry3))
+        self.assertTrue(isinstance(bundle.get_velocity(0), FrameVelocity))
+        # - - Value.
+        self.assertTrue(np.allclose(bundle.get_pose(0).translation(), 3 * [0]))
+        w_actual, v_actual = bundle.get_velocity(0).get_velocity()
+        self.assertTrue(np.allclose(w_actual, 3 * [0]))
+        self.assertTrue(np.allclose(v_actual, 3 * [0]))
+        # - Mutators.
+        kIndex = 5
+        p = [0, 1, 2]
+        q = Quaternion(wxyz=normalized([0.1, 0.3, 0.7, 0.9]))
+        bundle.set_pose(kIndex, Isometry3(q, p))
+        frame_velocity = FrameVelocity()
+        w = [0.1, 0.3, 0.5]
+        v = [0., 1., 2.]
+        frame_velocity.set_velocity(w=w, v=v)
+        bundle.set_velocity(kIndex, frame_velocity)
+        self.assertTrue(np.allclose(bundle.get_pose(kIndex).matrix(),
+                                    Isometry3(q, p).matrix()))
+        w_actual, v_actual = bundle.get_velocity(kIndex).get_velocity()
+        self.assertTrue(np.allclose(w_actual, w))
+        self.assertTrue(np.allclose(v_actual, v))
+        name = "Alice"
+        bundle.set_name(kIndex, name)
+        self.assertEqual(bundle.get_name(kIndex), name)
+        instance_id = 42
+        bundle.set_model_instance_id(kIndex, instance_id)
+        self.assertEqual(bundle.get_model_instance_id(kIndex), instance_id)
+
+
     def test_pose_vector(self):
         value = PoseVector()
         self.assertTrue(isinstance(value, BasicVector))
@@ -50,3 +89,83 @@ class TestRendering(unittest.TestCase):
         vector_expected = np.hstack((p, q.wxyz()))
         vector_actual = value.get_value()
         self.assertTrue(np.allclose(vector_actual, vector_expected))
+
+
+    def test_frame_velocity(self):
+        frame_velocity = FrameVelocity()
+        self.assertTrue(isinstance(frame_velocity, BasicVector))
+        self.assertTrue(isinstance(copy.copy(frame_velocity), FrameVelocity))
+        self.assertTrue(isinstance(frame_velocity.Clone(), FrameVelocity))
+        self.assertEquals(frame_velocity.size(), FrameVelocity.kSize)
+        # - Accessors.
+        w_actual, v_actual = frame_velocity.get_velocity()
+        self.assertTrue(isinstance(w_actual, np.ndarray))
+        self.assertTrue(isinstance(v_actual, np.ndarray))
+        self.assertEqual(w_actual.shape, (3,))
+        self.assertEqual(v_actual.shape, (3,))
+        # - Value.
+        self.assertTrue(np.allclose(w_actual, 3 * [0]))
+        self.assertTrue(np.allclose(v_actual, 3 * [0]))
+        # - Mutators.
+        w = [0.1, 0.3, 0.5]
+        v = [0., 1., 2.]
+        frame_velocity.set_velocity(w=w, v=v)
+        w_actual, v_actual = frame_velocity.get_velocity()
+        self.assertTrue(np.allclose(w_actual, w))
+        self.assertTrue(np.allclose(v_actual, v))
+        # - Ensure ordering is ((wx, wy, wz), (vx, vy, vz))
+        vector_expected = np.hstack((w, v))
+        vector_actual = frame_velocity.get_value()
+        self.assertTrue(np.allclose(vector_actual, vector_expected))
+
+
+    def test_pose_aggregator(self):
+        aggregator = PoseAggregator()
+        # - Set-up.
+        id1 = 5
+        port1 = aggregator.AddSingleInput("pose_only", id1)
+        self.assertEqual(port1.size(), PoseVector.kSize)
+        id2 = 42
+        ports2 = aggregator.AddSinglePoseAndVelocityInput("pose_and_velocity",
+                                                          id2)
+        self.assertEqual(ports2[0].size(), PoseVector.kSize)
+        self.assertEqual(ports2[1].size(), FrameVelocity.kSize)
+        num_poses = 1
+        port3 = aggregator.AddBundleInput("pose_bundle", num_poses)
+        self.assertEqual(port3.size(), 0)  # it's an abstract input.
+
+        # - CalcOutput
+        context = aggregator.CreateDefaultContext()
+        output = aggregator.AllocateOutput(context)
+
+        p1 = [0, 1, 2]
+        pose1 = PoseVector()
+        pose1.set_translation(p1)
+        p2 = [5, 7, 9]
+        pose2 = PoseVector()
+        pose2.set_translation(p2)
+        w = [0.3, 0.4, 0.5]
+        v = [0.5, 0.6, 0.7]
+        velocity = FrameVelocity()
+        velocity.set_velocity(w=w, v=v)
+        p3 = [50, 70, 90]
+        q3 = Quaternion(wxyz=normalized([0.1, 0.3, 0.7, 0.9]))
+        bundle = PoseBundle(num_poses)
+        bundle.set_pose(0, Isometry3(q3, p3))
+        bundle_value = AbstractValue.Make(bundle)
+
+        context.FixInputPort(0, pose1)
+        context.FixInputPort(1, pose2)
+        context.FixInputPort(2, velocity)
+        context.FixInputPort(3, bundle_value)
+
+        aggregator.CalcOutput(context, output)
+
+        value = output.get_data(0).get_value()
+        self.assertEqual(value.get_num_poses(), 3)
+        self.assertTrue(np.allclose(value.get_pose(0).translation(), p1))
+        self.assertTrue(np.allclose(value.get_pose(1).translation(), p2))
+        w_actual, v_actual = value.get_velocity(1).get_velocity()
+        self.assertTrue(np.allclose(w_actual, w))
+        self.assertTrue(np.allclose(v_actual, v))
+        self.assertTrue(np.allclose(value.get_pose(2).translation(), p3))
