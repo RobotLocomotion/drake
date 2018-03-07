1c4550c96ce7bed5e525df6768bd3116e405b85b
Jeremy Nimmer <jeremy.nimmer@tri.global>
2018-03-07 06:53:41 -0500

    Fix Wshadow warnings from GCC (systems)
    This removes potential confusion between member fields and local
    variables.  We are careful to leave the Doxygen spellings alone
    (not infected with -in suffixes), by buring constructors with the
    respelled arguments into the cc file.  None of these constructors
    appear to be performance-critical.

--- systems/primitives/time_varying_data.h
+++ systems/primitives/time_varying_data.h
@@ -67,60 +67,20 @@ struct TimeVaryingData {
   ///
   /// @param time_period The time period from which to construct the time
   /// vector.  time_period must be greater than zero.
-  TimeVaryingData(const std::vector<Eigen::MatrixXd>& Ain,
-                  const std::vector<Eigen::MatrixXd>& Bin,
-                  const std::vector<Eigen::MatrixXd>& f0in,
-                  const std::vector<Eigen::MatrixXd>& Cin,
-                  const std::vector<Eigen::MatrixXd>& Din,
-                  const std::vector<Eigen::MatrixXd>& y0in, double time_period)
-      : TimeVaryingData(
-            PiecewisePolynomial<double>::FirstOrderHold(
-                internal::vector_iota(Ain.size(), time_period), Ain),
-            PiecewisePolynomial<double>::FirstOrderHold(
-                internal::vector_iota(Bin.size(), time_period), Bin),
-            PiecewisePolynomial<double>::FirstOrderHold(
-                internal::vector_iota(f0in.size(), time_period), f0in),
-            PiecewisePolynomial<double>::FirstOrderHold(
-                internal::vector_iota(Cin.size(), time_period), Cin),
-            PiecewisePolynomial<double>::FirstOrderHold(
-                internal::vector_iota(Din.size(), time_period), Din),
-            PiecewisePolynomial<double>::FirstOrderHold(
-                internal::vector_iota(y0in.size(), time_period), y0in)) {
-    DRAKE_DEMAND(time_period > 0.);
-  }
+  TimeVaryingData(const std::vector<Eigen::MatrixXd>& A,
+                  const std::vector<Eigen::MatrixXd>& B,
+                  const std::vector<Eigen::MatrixXd>& f0,
+                  const std::vector<Eigen::MatrixXd>& C,
+                  const std::vector<Eigen::MatrixXd>& D,
+                  const std::vector<Eigen::MatrixXd>& y0, double time_period);
 
   /// Fully-parameterized constructor of PiecewisePolynomials.
-  TimeVaryingData(const PiecewisePolynomial<double>& Ain,
-                  const PiecewisePolynomial<double>& Bin,
-                  const PiecewisePolynomial<double>& f0in,
-                  const PiecewisePolynomial<double>& Cin,
-                  const PiecewisePolynomial<double>& Din,
-                  const PiecewisePolynomial<double>& y0in)
-      : A(PiecewisePolynomialTrajectory(Ain)),
-        B(PiecewisePolynomialTrajectory(Bin)),
-        f0(PiecewisePolynomialTrajectory(f0in)),
-        C(PiecewisePolynomialTrajectory(Cin)),
-        D(PiecewisePolynomialTrajectory(Din)),
-        y0(PiecewisePolynomialTrajectory(y0in)) {
-    DRAKE_DEMAND(A.get_piecewise_polynomial().getNumberOfSegments() ==
-                 B.get_piecewise_polynomial().getNumberOfSegments());
-    DRAKE_DEMAND(A.get_piecewise_polynomial().getNumberOfSegments() ==
-                 C.get_piecewise_polynomial().getNumberOfSegments());
-    DRAKE_DEMAND(A.get_piecewise_polynomial().getNumberOfSegments() ==
-                 D.get_piecewise_polynomial().getNumberOfSegments());
-    DRAKE_DEMAND(A.rows() == A.cols());
-    DRAKE_DEMAND(B.rows() == A.cols());
-    DRAKE_DEMAND(C.cols() == A.cols());
-    DRAKE_DEMAND(D.rows() == C.rows());
-    DRAKE_DEMAND(D.cols() == B.cols());
-
-    DRAKE_DEMAND(f0.get_piecewise_polynomial().getNumberOfSegments() ==
-                 A.get_piecewise_polynomial().getNumberOfSegments());
-    DRAKE_DEMAND(f0.rows() == A.cols());
-    DRAKE_DEMAND(y0.get_piecewise_polynomial().getNumberOfSegments() ==
-                 A.get_piecewise_polynomial().getNumberOfSegments());
-    DRAKE_DEMAND(y0.rows() == C.rows());
-  }
+  TimeVaryingData(const PiecewisePolynomial<double>& A,
+                  const PiecewisePolynomial<double>& B,
+                  const PiecewisePolynomial<double>& f0,
+                  const PiecewisePolynomial<double>& C,
+                  const PiecewisePolynomial<double>& D,
+                  const PiecewisePolynomial<double>& y0);
 
   PiecewisePolynomialTrajectory A{PiecewisePolynomial<double>()};
   PiecewisePolynomialTrajectory B{PiecewisePolynomial<double>()};
@@ -157,18 +117,13 @@ struct LinearTimeVaryingData : TimeVaryingData {
                         const std::vector<Eigen::MatrixXd>& B,
                         const std::vector<Eigen::MatrixXd>& C,
                         const std::vector<Eigen::MatrixXd>& D,
-                        double time_period)
-      : TimeVaryingData(
-            A, B, internal::eigen_vector_zeros(A.size(), A[0].rows()), C, D,
-            internal::eigen_vector_zeros(C.size(), C[0].rows()), time_period) {}
+                        double time_period);
 
   /// Fully-parameterized constructor of PiecewisePolynomials.
   LinearTimeVaryingData(const PiecewisePolynomial<double>& A,
                         const PiecewisePolynomial<double>& B,
                         const PiecewisePolynomial<double>& C,
-                        const PiecewisePolynomial<double>& D)
-      : TimeVaryingData(A, B, internal::MakeZeroedPiecewisePolynomial(A), C, D,
-                        internal::MakeZeroedPiecewisePolynomial(C)) {}
+                        const PiecewisePolynomial<double>& D);
 };
 
 }  // namespace systems
