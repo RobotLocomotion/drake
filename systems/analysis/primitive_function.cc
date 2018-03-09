10a13f0826944453be235d49e0e85bfd2b040c32
Michel Hidalgo <hid.michel@gmail.com>
2018-03-09 14:09:19 -0300

    Added PrimitiveFunction class implementation.
    A thin wrapper of the ScalarInitialValueProblem class to ease quadrature.

---
+++ systems/analysis/primitive_function.cc
@@ -0,0 +1,9 @@
+#include "drake/systems/analysis/primitive_function.h"
+
+namespace drake {
+namespace systems {
+
+template class PrimitiveFunction<double>;
+
+}  // namespace systems
+}  // namespace drake
