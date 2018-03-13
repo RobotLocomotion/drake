6ee2ab96495538cb0e2087ab4d4e8e210936b24b
Jonathan DeCastro <jonathan.decastro@tri.global>
2018-03-13 13:48:06 -0400

    PoseAggregator's AddSinglePoseAndVelocityInput() returns a struct (#8347)
    * PoseAggregator's AddSinglePoseAndVelocityInput() returns a struct

--- bindings/pydrake/systems/rendering_py.cc
+++ bindings/pydrake/systems/rendering_py.cc
@@ -62,6 +62,21 @@ PYBIND11_MODULE(rendering, m) {
     .def("set_model_instance_id", &PoseBundle<T>::set_model_instance_id);
   pysystems::AddValueInstantiation<PoseBundle<T>>(m);
 
+  py::class_<PoseVelocityInputPortDescriptors<T>>(
+      m, "PoseVelocityInputPortDescriptors")
+      // N.B. We use lambdas below since we cannot use `def_readonly` with
+      // reference members.
+      .def_property_readonly("pose_descriptor",
+           [](PoseVelocityInputPortDescriptors<T>* self) ->
+           const InputPortDescriptor<T>& {
+             return self->pose_descriptor;
+           })
+      .def_property_readonly("velocity_descriptor",
+           [](PoseVelocityInputPortDescriptors<T>* self) ->
+           const InputPortDescriptor<T>& {
+             return self->velocity_descriptor;
+           });
+
   py::class_<PoseAggregator<T>, LeafSystem<T>>(m, "PoseAggregator")
     .def(py::init<>())
     .def("AddSingleInput", &PoseAggregator<T>::AddSingleInput,
