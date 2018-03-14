6d9bb99a25b068ed3d4192aa1bd0cbfcfa4b7069
Russ Tedrake <russt@mit.edu>
2018-03-14 06:50:08 -0400

    Provide Eigen versions of the piecewise polynomial generation methods (addresses an existing TODO) and use them from python.
    The std::vector versions of the methods were hard to use from Python
    because passing in a 2d array would result in the automatic type
    conversion to a std::vector<Matrix<T>> with a single matrix element,
    instead of a list of elements, one per column, which is almost always
    what I want.

--- bindings/pydrake/trajectories_py.cc
+++ bindings/pydrake/trajectories_py.cc
@@ -33,34 +33,37 @@ PYBIND11_MODULE(trajectories, m) {
       m, "PiecewisePolynomial")
       .def(py::init<>())
       .def(py::init<const Eigen::Ref<const MatrixX<T>>&>())
-      .def_static("ZeroOrderHold", &PiecewisePolynomial<T>::ZeroOrderHold)
-      .def_static("FirstOrderHold", &PiecewisePolynomial<T>::FirstOrderHold)
-      .def_static("Pchip", &PiecewisePolynomial<T>::Pchip)
-      .def_static(
-          "Cubic",
-          py::overload_cast<
-              const std::vector<double>&,
-              const std::vector<PiecewisePolynomial<T>::CoefficientMatrix>&,
-              const PiecewisePolynomial<T>::CoefficientMatrix&,
-              const PiecewisePolynomial<T>::CoefficientMatrix&>(
-              &PiecewisePolynomial<T>::Cubic),
-          py::arg("breaks"), py::arg("knots"), py::arg("knot_dot_start"),
-          py::arg("knot_dot_end"))
-      .def_static(
-          "Cubic",
-          py::overload_cast<
-              const std::vector<double>&,
-              const std::vector<PiecewisePolynomial<T>::CoefficientMatrix>&,
-              const std::vector<PiecewisePolynomial<T>::CoefficientMatrix>&>(
-              &PiecewisePolynomial<T>::Cubic),
-          py::arg("breaks"), py::arg("knots"), py::arg("knots_dot"))
-      .def_static(
-          "Cubic",
-          py::overload_cast<
-              const std::vector<double>&,
-              const std::vector<PiecewisePolynomial<T>::CoefficientMatrix>&>(
-              &PiecewisePolynomial<T>::Cubic),
-          py::arg("breaks"), py::arg("knots"))
+      .def_static("ZeroOrderHold",
+                  py::overload_cast<const Eigen::Ref<const Eigen::VectorXd>&,
+                                    const Eigen::Ref<const MatrixX<T>>&>(
+                      &PiecewisePolynomial<T>::ZeroOrderHold))
+      .def_static("FirstOrderHold",
+                  py::overload_cast<const Eigen::Ref<const Eigen::VectorXd>&,
+                                    const Eigen::Ref<const MatrixX<T>>&>(
+                      &PiecewisePolynomial<T>::FirstOrderHold))
+      .def_static("Pchip",
+                  py::overload_cast<const Eigen::Ref<const Eigen::VectorXd>&,
+                                    const Eigen::Ref<const MatrixX<T>>&, bool>(
+                      &PiecewisePolynomial<T>::Pchip))
+      .def_static("Cubic",
+                  py::overload_cast<const Eigen::Ref<const Eigen::VectorXd>&,
+                                    const Eigen::Ref<const MatrixX<T>>&,
+                                    const Eigen::Ref<const VectorX<T>>&,
+                                    const Eigen::Ref<const VectorX<T>>&>(
+                      &PiecewisePolynomial<T>::Cubic),
+                  py::arg("breaks"), py::arg("knots"),
+                  py::arg("knot_dot_start"), py::arg("knot_dot_end"))
+      .def_static("Cubic",
+                  py::overload_cast<const Eigen::Ref<const Eigen::VectorXd>&,
+                                    const Eigen::Ref<const MatrixX<T>>&,
+                                    const Eigen::Ref<const MatrixX<T>>&>(
+                      &PiecewisePolynomial<T>::Cubic),
+                  py::arg("breaks"), py::arg("knots"), py::arg("knots_dot"))
+      .def_static("Cubic",
+                  py::overload_cast<const Eigen::Ref<const Eigen::VectorXd>&,
+                                    const Eigen::Ref<const MatrixX<T>>&>(
+                      &PiecewisePolynomial<T>::Cubic),
+                  py::arg("breaks"), py::arg("knots"))
       .def("value", &PiecewisePolynomial<T>::value)
       .def("rows", &PiecewisePolynomial<T>::rows)
       .def("cols", &PiecewisePolynomial<T>::cols);
