cffc8cb796ab410d2e17ec1535c39d6031d82e53
hongkai-dai <daihongkai@gmail.com>
2018-03-13 20:52:55 -0700

    Add the demo

--- manipulation/dev/remote_tree_viewer_wrapper.h
+++ manipulation/dev/remote_tree_viewer_wrapper.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 #include "drake/common/eigen_types.h"
 #include "drake/lcm/drake_lcm.h"
 #include "drake/multibody/rigid_body_tree.h"
@@ -8,38 +11,41 @@ namespace drake {
 namespace manipulation {
 namespace dev {
 class RemoteTreeViewerWrapper {
-  public:
-    RemoteTreeViewerWrapper();
-    void publishPointCloud(
-            const Eigen::Matrix3Xd& pts,
-            const std::vector<std::string>& path,
-            const std::vector<std::vector<double>>& color = {{1.0, 0.0, 1.0}});
-    void publishLine(
-            const Eigen::Matrix3Xd& pts,
-            const std::vector<std::string>& path);
-    void publishRawMesh(
-            const Eigen::Matrix3Xd& verts,
-            const std::vector<Eigen::Vector3i>& tris,
-            const std::vector<std::string>& path);
-    void publishRigidBodyTree(
-            const RigidBodyTree<double>& tree,
-            const Eigen::VectorXd& q,
-            const Eigen::Vector4d& color,
-            const std::vector<std::string>& path,
-            bool visual = true);
-    void publishRigidBody(
-            const RigidBody<double>& body,
-            const Eigen::Affine3d& tf,
-            const Eigen::Vector4d& color,
-            const std::vector<std::string>& path);
-    void publishGeometry(
-            const DrakeShapes::Geometry& geometry,
-            const Eigen::Affine3d& tf,
-            const Eigen::Vector4d& color,
-            const std::vector<std::string>& path);
+ public:
+  RemoteTreeViewerWrapper();
 
-  private:
-    drake::lcm::DrakeLcm lcm_;
+  ~RemoteTreeViewerWrapper() = default;
+
+  void publishPointCloud(
+          const Eigen::Matrix3Xd& pts,
+          const std::vector<std::string>& path,
+          const std::vector<std::vector<double>>& color = {{1.0, 0.0, 1.0}});
+  void publishLine(
+          const Eigen::Matrix3Xd& pts,
+          const std::vector<std::string>& path);
+  void publishRawMesh(
+          const Eigen::Matrix3Xd& verts,
+          const std::vector<Eigen::Vector3i>& tris,
+          const std::vector<std::string>& path);
+  void publishRigidBodyTree(
+          const RigidBodyTree<double>& tree,
+          const Eigen::VectorXd& q,
+          const Eigen::Vector4d& color,
+          const std::vector<std::string>& path,
+          bool visual = true);
+  void publishRigidBody(
+          const RigidBody<double>& body,
+          const Eigen::Affine3d& tf,
+          const Eigen::Vector4d& color,
+          const std::vector<std::string>& path);
+  void publishGeometry(
+          const DrakeShapes::Geometry& geometry,
+          const Eigen::Affine3d& tf,
+          const Eigen::Vector4d& color,
+          const std::vector<std::string>& path);
+
+ private:
+  drake::lcm::DrakeLcm lcm_;
 };
 }  // namespace dev
 }  // namespace manipulation
---
+++ manipulation/dev/test/remote_tree_viewer_wrapper_demo.cc
@@ -0,0 +1,78 @@
+#include "drake/manipulation/dev/remote_tree_viewer_wrapper.h"
+
+#include "drake/common/find_resource.h"
+#include "drake/multibody/parsers/urdf_parser.h"
+#include "drake/multibody/rigid_body_tree.h"
+
+namespace drake {
+namespace manipulation {
+namespace dev {
+static inline double randrange(double min, double max) {
+  return (static_cast<double>(rand()) / RAND_MAX) * (max - min) + min;
+}
+
+int DoMain() {
+  RemoteTreeViewerWrapper rm;
+
+  int n_points = 1000;
+  double x_size = 3.0;
+  double y_size = 2.0;
+  double z_size = 1.0;
+  Eigen::Matrix3Xd pts(3, n_points);
+  for (int i = 0; i < n_points; i++) {
+    Eigen::Vector3d xyz(randrange(-1.0, 1.0), randrange(-1.0, 1.0),
+                        randrange(-1.0, 1.0));
+    xyz /= xyz.norm();
+    xyz[0] *= x_size;
+    xyz[1] *= y_size;
+    xyz[2] *= z_size;
+    pts.col(i) = xyz;
+  }
+  rm.publishPointCloud(pts, {"test_pc"});
+  rm.publishLine(pts, {"test_line"});
+
+  const Eigen::Vector4d color_gray(0.7, 0.7, 0.7, 0.9);
+  const Eigen::Vector4d color_blue(0.3, 0.3, 1.0, 0.9);
+
+  Eigen::Affine3d tf_box;
+  tf_box.setIdentity();
+  tf_box.translation()[1] = y_size * 2;
+  rm.publishGeometry(DrakeShapes::Box({x_size, y_size, z_size}), tf_box,
+                     color_gray, {"test_box"});
+
+  Eigen::Affine3d tf_sphere;
+  tf_sphere.setIdentity();
+  tf_sphere.translation()[1] = y_size * 2 * 2;
+  rm.publishGeometry(DrakeShapes::Sphere(y_size), tf_sphere, color_gray,
+                     {"test_sphere"});
+
+  Eigen::Affine3d tf_cylinder;
+  tf_cylinder.setIdentity();
+  tf_cylinder.translation()[1] = y_size * 2 * 3;
+  rm.publishGeometry(DrakeShapes::Cylinder(y_size, z_size), tf_cylinder,
+                     color_gray, {"test_cylinder"});
+
+  Eigen::Affine3d tf_capsule;
+  tf_capsule.setIdentity();
+  tf_capsule.translation()[1] = y_size * 2 * 4;
+  rm.publishGeometry(DrakeShapes::Capsule(y_size, z_size), tf_capsule,
+                     color_gray, {"test_capsule"});
+
+  RigidBodyTree<double> tree;
+  const auto iiwa_path = drake::FindResourceOrThrow(
+      "drake/examples/atlas/urdf/atlas_minimal_contact.urdf");
+  parsers::urdf::AddModelInstanceFromUrdfFileWithRpyJointToWorld(iiwa_path,
+                                                                 &tree);
+  Eigen::Affine3d tf_robot;
+  tf_robot.setIdentity();
+  tf_robot.translation()[1] = -y_size * 2;
+  rm.publishRigidBodyTree(tree, Eigen::VectorXd::Zero(tree.get_num_positions()),
+                          color_blue, {"test_robot"});
+
+  return 0;
+}
+}  // namespace dev
+}  // namespace manipulation
+}  // namespace drake
+
+int main() { return drake::manipulation::dev::DoMain(); }
