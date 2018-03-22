b9de5ecfd2c15a84a0b45837abae7c2b1eee44c1
Eric Cousineau <eric.cousineau@tri.global>
2018-03-22 00:08:46 -0400

    pydrake autodiff: Use wrap_pybind to define aliases for use with NumPy broadcasting

--- bindings/pydrake/util/wrap_pybind.h
+++ bindings/pydrake/util/wrap_pybind.h
@@ -14,33 +14,22 @@ namespace pydrake {
 // Defines a function in module `m`, and mirrors to module `mirror` for
 // backwards compatibility. Throws an error if any mirrored methods already
 // exist and do not match the original value.
+template <typename From, typename To>
 class MirrorDef {
  public:
-  MirrorDef(py::module m, py::module mirror)
+  MirrorDef(From* m, To* mirror)
       : m_(m), mirror_(mirror) {}
 
   template <typename... Args>
   MirrorDef& def(const char* name, Args&&... args) {
-    m_.def(name, std::forward<Args>(args)...);
-    do_mirror(name);
+    m_->def(name, std::forward<Args>(args)...);
+    mirror_->def(name, std::forward<Args>(args)...);
     return *this;
   }
 
  private:
-  void do_mirror(const char* name) {
-    py::object value = m_.attr(name);
-    // Ensure we won't shadow anything in the mirror; if an attribute of this
-    // name exists and is not exactly the same, throw an error.
-    py::object mirror_value = py::getattr(mirror_, name, py::none());
-    if (!mirror_value.is_none() && !mirror_value.is(value))
-      throw py::cast_error(py::str(
-          "Mirroring: '{}' from {} already exists in {} and will be shadowed")
-              .format(name, m_, mirror_).cast<std::string>());
-    mirror_.attr(name) = value;
-  }
-
-  py::module m_;
-  py::module mirror_;
+  From* m_{};
+  To* mirror_{};
 };
 
 }  // namespace pydrake
