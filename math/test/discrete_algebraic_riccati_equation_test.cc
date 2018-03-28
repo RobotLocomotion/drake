44de86f5bab81e6617d5875efc7991ef640730e7
Weiqiao <weiqiaoh@mit.edu>
2018-03-27 22:42:54 -0400

    fix a bug in DARE solver

--- math/test/discrete_algebraic_riccati_equation_test.cc
+++ math/test/discrete_algebraic_riccati_equation_test.cc
@@ -51,6 +51,14 @@ GTEST_TEST(DARE, SolveDAREandVerify) {
   Q2 << 1, 0, 0, 0;
   R2 << 0.3;
   SolveDAREandVerify(A2, B2, Q2, R2);
+  // Test 3: the first generalized eigenvalue of (S,T) is stable
+  int n3 = 2, m3 = 1;
+  MatrixXd A3(n3, n3), B3(n3, m3), Q3(n3, n3), R3(m3, m3);
+  A3 << 0, 1, 0, 0;
+  B3 << 0, 1;
+  Q3 << 1, 0, 0, 1;
+  R3 << 1;
+  SolveDAREandVerify(A3, B3, Q3, R3);
 }
 }  // namespace
 }  // namespace math
