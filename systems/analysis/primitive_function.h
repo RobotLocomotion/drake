02a0ab4cf15787b7690a70ad8000e6e1fbd4adea
Michel Hidalgo <hid.michel@gmail.com>
2018-03-29 12:30:43 -0300

    Addressed PrimitiveFunction class implementation feature review first pass comments.

--- systems/analysis/primitive_function.h
+++ systems/analysis/primitive_function.h
@@ -12,9 +12,11 @@
 namespace drake {
 namespace systems {
 
-/// A thin wrapper of the ScalarInitialValueProblem class to provide a simple
-/// interface when performing quadrature on an arbitrary scalar function i.e.
-/// when evaluating a primitive function F(u; 𝐤), such that
+
+/// A thin wrapper of the ScalarInitialValueProblem class that, in concert with
+/// Drake's ODE initial value problem solvers ("integrators"), provides the
+/// ability to perform quadrature on an arbitrary scalar function. That is, it
+/// allows the evaluation of a primitive function F(u; 𝐤), such that
 /// F(u; 𝐤) =∫ᵥᵘ f(x; 𝐤) dx where f : ℝ  →  ℝ , u ∈ ℝ, v ∈ ℝ, 𝐤 ∈ ℝᵐ. The
 /// parameter vector 𝐤 allows for generic function definitions, which can later
 /// be evaluated for any instance of said vector.
@@ -22,15 +24,18 @@ namespace systems {
 /// For further insight into its use, consider the following examples.
 ///
 /// - Solving the elliptic integral of the first kind
-///   F(φ; k) = ∫ᵠ √(1 - k² sin² θ)⁻¹ dθ becomes straightforward by defining
-///   f(θ; 𝐤) ≜ √(1 - 𝐤₁² sin² θ)⁻¹ with 𝐤 ≜ [k] and evaluating at u = φ.
+///   F(φ; ξ) = ∫ᵠ √(1 - ξ² sin² θ)⁻¹ dθ becomes straightforward by defining
+///   f(θ; 𝐤) ≜ √(1 - k₁² sin² θ)⁻¹ with 𝐤 ≜ [ξ] and evaluating F(u; 𝐤) at
+///   u = φ.
 ///
 /// - As the bearings in a rotating machine age over time, these are more likely
 ///   to fail. Let γ be a random variable describing the time to first bearing
 ///   failure, described by a family of probability density functions fᵧ(y; l)
 ///   parameterized by bearing load l. In this context, the probability of a
 ///   bearing under a load l₁ to fail in the first N months becomes
-///   P(0 < γ ≤ N mo.; l₁) = Fᵧ(N mo.; l₁) - Fᵧ(0; l₁), where
+///   P(0 < γ ≤ N mo.; l₁) = Fᵧ(N mo.; l₁) - Fᵧ(0; l₁), where Fᵧ(y; l) is the
+///   associated family of cumulative probability density functions (i.e. the
+///   probability of γ taking a value less than or equal to its argument y) and
 ///   F'ᵧ(y; l) = fᵧ(y; l). Therefore, defining f ≜ fᵧ with 𝐤 ≜ [l] and
 ///   evaluating at u = N yields the result.
 ///
@@ -51,14 +56,14 @@ class PrimitiveFunction {
   /// @return The integrand value f(@p x; @p k).
   typedef std::function<T(const T& x, const VectorX<T>& k)> IntegrandFunction;
 
-  /// A collection of values i.e. lower integration bound v
-  /// and parameter vector 𝐤 to specify the integral.
+  /// The set of values that, along with the integrand, fully specify the
+  /// definite integral i.e. the lower integration bound v and the parameter
+  /// vector 𝐤.
   struct SpecifiedValues {
-    /// Default constructor, leaving all values unspecified.
+    /// Default constructor that leaves all values unspecified.
     SpecifiedValues() = default;
 
-    /// Constructor specifying all values.
-    ///
+    /// Constructor that specifies all values.
     /// @param v_in Specified lower integration bound v.
     /// @param k_in Specified parameter vector 𝐤.
     SpecifiedValues(const optional<T>& v_in,
@@ -71,16 +76,15 @@ class PrimitiveFunction {
 
   /// Constructs the primitive function of the given @p integrand_function,
   /// using @p default_values.v as lower integration bound if given (0 if
-  /// not) and parameterized with @p default_values.k by default.
+  /// not) and parameterized with @p default_values.k if given (an empty vector
+  /// if not) by default.
   ///
-  /// @param integrand_function The function f(x; 𝐤) under the integral sign.
+  /// @param integrand_function The function f(x; 𝐤) being integrated.
   /// @param default_values The values specified by default for this function,
   ///                       i.e. default lower integration bound v ∈ ℝ  and
   ///                       default parameter vector 𝐤 ∈ ℝᵐ.
-  /// @pre A parameter vector @p default_values.k is provided.
-  /// @throw std::logic_error if preconditions are not met.
   PrimitiveFunction(const IntegrandFunction& integrand_function,
-                    const SpecifiedValues& default_values) {
+                    const SpecifiedValues& default_values = {}) {
     // Expresses the scalar integral to be solved as an ODE.
     typename ScalarInitialValueProblem<T>::ScalarODEFunction
         scalar_ode_function = [integrand_function](
@@ -89,28 +93,31 @@ class PrimitiveFunction {
       return integrand_function(t, k);
     };
 
-    // Default initial conditions for the scalar ODE form are
-    // both, time and state, set to 0.
     typename ScalarInitialValueProblem<T>::SpecifiedValues
         scalar_ivp_default_values;
+    // Default initial time for the scalar ODE form falls back
+    // to 0 if no lower integration bound is specified.
     scalar_ivp_default_values.t0 = default_values.v.value_or(
         static_cast<T>(0.0));
+    // Default initial state for the scalar ODE form is set to 0.
     scalar_ivp_default_values.x0 = static_cast<T>(0.0);
-    scalar_ivp_default_values.k = default_values.k;
+    // Default parameter vector for the scalar ODE falls back to
+    // the empty vector if none is given.
+    scalar_ivp_default_values.k = default_values.k.value_or(VectorX<T>());
 
     // Instantiates the scalar initial value problem.
     scalar_ivp_ = std::make_unique<ScalarInitialValueProblem<T>>(
         scalar_ode_function, scalar_ivp_default_values);
   }
 
-  /// Evaluates the function at @p u using the lower integration bound v and
-  /// parameter vector 𝐤 present in @p values, falling back to the ones
-  /// given on construction if not given.
+  /// Evaluates the definite integral over the lower integration bound v to
+  /// @p u using the parameter vector 𝐤 present in @p values, falling back
+  /// to the ones given on construction if not given.
   ///
-  /// @param u The function argument (i.e. the upper integration bound).
+  /// @param u The upper integration bound.
   /// @param values The specified values for the function.
-  /// @return The function value (i.e. the integration result).
-  /// @pre Given upper integration bound @p u must be larger than or equal
+  /// @return The value of the definite integral.
+  /// @pre The given upper integration bound @p u must be larger than or equal
   ///      to the lower integration bound v.
   /// @pre If given, the dimension of the parameter vector @p values.k
   ///      must match that of the parameter vector 𝐤 in the default specified
