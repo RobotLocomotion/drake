571d29d0c17be58a24e0e084371c59096076b7b4
Sean Curtis <sean.curtis@tri.global>
2018-04-02 06:56:04 -0700

    Add visualization material to geometry
    1. Adds a definition of a *visualization* material (used for simple viz)
    2. Modifies declaration of GeometryInstance to include a material.
    3. Modifies the solar system example to have colors.
    4. Updates LCM generation to account for materials.
    5. Updates the RigidBodyPlantBridge to extract visual materials from RBT
       and pass them onto GeometrySystem.
    6. Adds a unit test to the geometry_visualization functionality; confirms
       message contains expected contents.

--- geometry/geometry_instance.cc
+++ geometry/geometry_instance.cc
@@ -7,7 +7,15 @@ namespace geometry {
 
 GeometryInstance::GeometryInstance(const Isometry3<double>& X_PG,
                                    std::unique_ptr<Shape> shape)
-    : id_(GeometryId::get_new_id()), X_PG_(X_PG), shape_(std::move(shape)) {}
+    : GeometryInstance(X_PG, std::move(shape), VisualMaterial()) {}
+
+GeometryInstance::GeometryInstance(const Isometry3<double>& X_PG,
+                                   std::unique_ptr<Shape> shape,
+                                   const VisualMaterial& vis_material)
+    : id_(GeometryId::get_new_id()),
+      X_PG_(X_PG),
+      shape_(std::move(shape)),
+      visual_material_(vis_material) {}
 
 }  // namespace geometry
 }  // namespace drake
