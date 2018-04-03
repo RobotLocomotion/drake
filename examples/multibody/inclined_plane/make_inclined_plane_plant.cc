abed326d584b1fa870a70f4b3c56b39376a0949f
amcastro-tri <alejandro.castro@tri.global>
2018-04-03 12:34:09 -0300

    Create inlinced plane example

---
+++ examples/multibody/inclined_plane/make_inclined_plane_plant.cc
@@ -0,0 +1,68 @@
+#include "drake/examples/multibody/inclined_plane/make_inclined_plane_plant.h"
+
+#include "drake/multibody/multibody_tree/uniform_gravity_field_element.h"
+
+namespace drake {
+namespace examples {
+namespace multibody {
+namespace bouncing_ball {
+
+using geometry::GeometrySystem;
+using geometry::Sphere;
+using geometry::HalfSpace;
+using drake::multibody::multibody_plant::MultibodyPlant;
+using drake::multibody::RigidBody;
+using drake::multibody::SpatialInertia;
+using drake::multibody::UniformGravityFieldElement;
+using drake::multibody::UnitInertia;
+using Eigen::AngleAxisd;
+
+std::unique_ptr<drake::multibody::multibody_plant::MultibodyPlant<double>>
+MakeInclinedPlanePlant(double radius, double mass, double slope, double gravity,
+                       geometry::GeometrySystem<double>* geometry_system) {
+  DRAKE_DEMAND(geometry_system != nullptr);
+
+  auto plant = std::make_unique<MultibodyPlant<double>>();
+
+  UnitInertia<double> G_Bcm = UnitInertia<double>::SolidSphere(radius);
+  SpatialInertia<double> M_Bcm(mass, Vector3<double>::Zero(), G_Bcm);
+
+  const RigidBody<double>& ball = plant->AddRigidBody("Ball", M_Bcm);
+
+  plant->RegisterAsSourceForGeometrySystem(geometry_system);
+
+  // Orientation of a plane frame P with its z axis normal to the plane and its
+  // x axis pointing down the plane.
+  const Matrix3<double> R_WP(AngleAxisd(slope, Vector3<double>::UnitY()));
+
+  const Vector3<double> normal_W = R_WP.col(2);
+  // Offset the plane so that with the rolling object's center at x = (0, 0, 0)
+  // (the default initial condition) there is a contact point at point_W.
+  const Vector3<double> point_W = -normal_W * radius;
+
+  // A half-space for the inclined plane geometry.
+  plant->RegisterCollisionGeometry(
+      plant->world_body(),
+      HalfSpace::MakePose(normal_W, point_W), HalfSpace(), geometry_system);
+
+  // Add sphere geometry for the ball.
+  plant->RegisterCollisionGeometry(
+      ball,
+      /* Pose X_BG of the geometry frame G in the ball frame B. */
+      Isometry3<double>::Identity(),
+      Sphere(radius), geometry_system);
+
+  // Gravity acting in the -z direction.
+  plant->AddForceElement<UniformGravityFieldElement>(
+      -gravity * Vector3<double>::UnitZ());
+
+  // We are done creating the plant.
+  plant->Finalize();
+
+  return plant;
+}
+
+}  // namespace bouncing_ball
+}  // namespace multibody
+}  // namespace examples
+}  // namespace drake
