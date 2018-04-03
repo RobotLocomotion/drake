7f7f040f856995328b94c52530eaf8adc0d98d22
Jamie Snape <jamie.snape@kitware.com>
2018-04-03 13:19:30 -0400

    Use mkdtemp when creating temp directories except for call_{matlab|python}

--- common/temp_directory.h
+++ common/temp_directory.h
@@ -6,9 +6,12 @@ namespace drake {
 
 /// Returns a directory location suitable for temporary files.
 /// @return The value of the environment variable TEST_TMPDIR if defined or
-/// otherwise /tmp. Any trailing / will be stripped from the output.
-/// @throws std::runtime_error If the path referred to by TEST_TMPDIR or /tmp
-/// does not exist or is not a directory.
+/// otherwise ${TMPDIR:-/tmp}/robotlocomotion_drake_XXXXXX where each X is
+/// replaced by a character from the portable filename character set. Any
+/// trailing / will be stripped from the output.
+/// @throws std::runtime_error If the path referred to by TEST_TMPDIR or
+/// ${TMPDIR:-/tmp}/robotlocomotion_drake_XXXXXX cannot be created, does not
+/// exist, or is not a directory.
 std::string temp_directory();
 
 }  // namespace drake
