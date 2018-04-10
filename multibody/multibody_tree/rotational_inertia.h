b5dcb6d7adec59637fd16fccf644eb89e2c2d8ae
amcastro-tri <alejandro.castro@tri.global>
2018-04-10 11:00:13 -0300

    Fixes BUILD file

--- multibody/multibody_tree/rotational_inertia.h
+++ multibody/multibody_tree/rotational_inertia.h
@@ -11,7 +11,6 @@
 
 #include <Eigen/Eigenvalues>
 
-#include "drake/common/autodiff.h"
 #include "drake/common/drake_assert.h"
 #include "drake/common/drake_bool.h"
 #include "drake/common/drake_copyable.h"
