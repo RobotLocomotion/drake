25e3b2e5d9ca74b23e53fc0fdb1501eb3e86ffde
Russ Tedrake <russt@mit.edu>
2018-04-16 05:22:10 +0800

    adds compass gait example

---
+++ examples/compass_gait/compass_gait.h
@@ -0,0 +1,156 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "drake/examples/compass_gait/gen/compass_gait_continuous_state.h"
+#include "drake/examples/compass_gait/gen/compass_gait_params.h"
+#include "drake/systems/framework/event.h"
+#include "drake/systems/framework/leaf_system.h"
+#include "drake/systems/framework/scalar_conversion_traits.h"
+#include "drake/systems/framework/witness_function.h"
+
+namespace drake {
+namespace examples {
+namespace compass_gait {
+
+/// Dynamical representation of the idealized hybrid dynamics of a "compass
+/// gait", as described in
+///   http://underactuated.mit.edu/underactuated.html?chapter=simple_legs
+/// In addition, this model has two additional (discrete) state variables that
+/// are not required in the mathematical model:
+///   - the position of the stance toe along the ramp (helpful for outputting
+///     a floating-base model coordinate, e.g. for visualization),
+///   - a boolean indicator for "left support" (true when the stance leg is
+///     the left leg).
+///
+/// Inputs: None.
+/// Continuous States: stance, swing, stancedot, swingdot.
+/// Discrete States: stance toe position, left support indicator.
+/// Outputs:
+///   0) continuous (minimal coordinates) state output
+///   1) floating-base state output
+///
+/// @tparam T The vector element type, which must be a valid Eigen scalar.
+/// Instantiated templates for the following scalar types @p T are provided:
+/// - double
+/// - AutoDiffXd
+/// - symbolic::Expression
+template <typename T>
+class CompassGait final : public systems::LeafSystem<T> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(CompassGait);
+
+  /// Constructs the plant.
+  CompassGait();
+
+  /// Scalar-converting copy constructor.  See @ref system_scalar_conversion.
+  template <typename U>
+  explicit CompassGait(const CompassGait<U>&) : CompassGait<T>() {}
+
+  /// Return reference to the output port that publishes only
+  /// [theta_stance, theta_swing, thetatdot_stance, thetadot_swing].
+  const systems::OutputPort<T>& get_minimal_state_output_port() {
+    return this->get_output_port(0);
+  }
+
+  /// Returns reference to the output port that provides a the state output
+  /// for CompassGait.urdf (as instantiated with
+  /// FloatingBaseType::kRollPitchYaw).
+  const systems::OutputPort<T>& get_floating_base_state_output_port() {
+    return this->get_output_port(1);
+  }
+
+  /// Access the CompassGaitContinuousState.
+  static const CompassGaitContinuousState<T>& get_continuous_state(
+      const systems::ContinuousState<T>& cstate) {
+    return dynamic_cast<const CompassGaitContinuousState<T>&>(
+        cstate.get_vector());
+  }
+
+  /// Access the CompassGaitContinuousState.
+  static const CompassGaitContinuousState<T>& get_continuous_state(
+      const systems::Context<T>& context) {
+    return get_continuous_state(context.get_continuous_state());
+  }
+
+  /// Access the mutable CompassGaitContinuousState.
+  static CompassGaitContinuousState<T>& get_mutable_continuous_state(
+      systems::ContinuousState<T>* cstate) {
+    return dynamic_cast<CompassGaitContinuousState<T>&>(
+        cstate->get_mutable_vector());
+  }
+
+  /// Access the mutable CompassGaitContinuousState.
+  static CompassGaitContinuousState<T>& get_mutable_continuous_state(
+      systems::Context<T>* context) {
+    return get_mutable_continuous_state(
+        &context->get_mutable_continuous_state());
+  }
+
+  static const T& get_toe_position(const systems::Context<T>& context) {
+    return context.get_discrete_state(0).GetAtIndex(0);
+  }
+
+  static T& get_mutable_toe_position(systems::State<T>* state) {
+    return state->get_mutable_discrete_state().get_mutable_vector(0).GetAtIndex(
+        0);
+  }
+
+  static bool get_left_stance(const systems::Context<T>& context) {
+    return context.template get_abstract_state<bool>(0);
+  }
+
+  static bool& get_mutable_left_stance(systems::State<T>* state) {
+    return state->template get_mutable_abstract_state<bool>(0);
+  }
+
+  /// Access the CompassGaitParams.
+  const CompassGaitParams<T>& get_parameters(
+      const systems::Context<T>& context) const {
+    return this->template GetNumericParameter<CompassGaitParams>(context, 0);
+  }
+
+  ///@{
+  /// Manipulator equation of CompassGait: M(q)q̈ + bias(q,q̇̇) = B*u.
+  /// - M[2x2] is the mass matrix.
+  /// - bias[2x1] includes the Coriolis term and gravity term,
+  ///     i.e. bias[2x1] = C(q,v)*v - τ_g(q).
+  Vector2<T> DynamicsBiasTerm(const systems::Context<T> &context) const;
+  Matrix2<T> MassMatrix(const systems::Context<T> &context) const;
+  ///@}
+
+ private:
+  T DoCalcKineticEnergy(const systems::Context<T>& context) const override;
+  T DoCalcPotentialEnergy(const systems::Context<T>& context) const override;
+
+  // Check when the foot hits the ramp (with a sufficiently large step length).
+  T FootCollision(const systems::Context<T>& context) const;
+
+  // Handles the impact dynamics, including reseting the stance and swing legs.
+  void CollisionDynamics(const systems::Context<T> &context,
+                         const systems::UnrestrictedUpdateEvent<T> &,
+                         systems::State<T> *state) const;
+
+  void MinimalStateOut(const systems::Context<T>& context,
+                       CompassGaitContinuousState<T>* output) const;
+
+  void FloatingBaseStateOut(const systems::Context<T>& context,
+                            systems::BasicVector<T>* output) const;
+
+  // Implements the simple double pendulum dynamics during stance.
+  void DoCalcTimeDerivatives(
+      const systems::Context<T>& context,
+      systems::ContinuousState<T>* derivatives) const override;
+
+  void DoGetWitnessFunctions(const systems::Context<T>&,
+                             std::vector<const systems::WitnessFunction<T>*>*
+                                 witnesses) const override;
+
+  // The system stores its witness function internally.
+  std::unique_ptr<systems::WitnessFunction<T>> foot_collision_;
+};
+
+}  // namespace compass_gait
+}  // namespace examples
+}  // namespace drake
---
+++ examples/compass_gait/test/compass_gait_test.cc
@@ -0,0 +1,176 @@
+#include "drake/examples/compass_gait/compass_gait.h"
+
+#include <gtest/gtest.h>
+
+#include "drake/common/symbolic.h"
+#include "drake/systems/framework/test_utilities/scalar_conversion.h"
+
+namespace drake {
+namespace examples {
+namespace compass_gait {
+
+using std::cos;
+using std::sin;
+using std::sqrt;
+using symbolic::Expression;
+using symbolic::Variable;
+
+namespace {
+
+GTEST_TEST(CompassGaitTest, ScalarTypeTests) {
+  CompassGait<double> cg;
+  EXPECT_TRUE(is_autodiffxd_convertible(cg));
+  EXPECT_TRUE(is_symbolic_convertible(cg));
+}
+
+GTEST_TEST(CompassGaitTest, TestEnergyConservedInSwing) {
+  CompassGait<Expression> cg;
+
+  auto context = cg.CreateDefaultContext();
+  CompassGaitContinuousState<Expression>& state =
+      cg.get_mutable_continuous_state(context.get());
+  auto derivatives = cg.AllocateTimeDerivatives();
+
+  // Set the state vector to be symbolic variables.
+  state.SetToNamedVariables();
+
+  // Compute the time-derivative of the energy symbolically.
+  const Expression energy =
+      cg.CalcKineticEnergy(*context) + cg.CalcPotentialEnergy(*context);
+  cg.CalcTimeDerivatives(*context, derivatives.get());
+  const Expression energy_dot =
+      energy.Jacobian(get_variable_vector(state.CopyToVector())) *
+      derivatives->CopyToVector();
+
+  // Evaluate the time-derivative of energy at a random state, it should be
+  // zero.
+  symbolic::Environment e;
+  e.insert(get_variable(state.stance()), 1.);
+  e.insert(get_variable(state.swing()), 2.);
+  e.insert(get_variable(state.stancedot()), 3.);
+  e.insert(get_variable(state.swingdot()), 4.);
+
+  EXPECT_NEAR(energy_dot.Evaluate(e), 0.0, 1e-12);
+}
+
+GTEST_TEST(CompassGaitTest, TestCollisionGuard) {
+  const CompassGait<double> cg;
+  auto context = cg.CreateDefaultContext();
+  CompassGaitContinuousState<double>& state =
+      cg.get_mutable_continuous_state(context.get());
+  const CompassGaitParams<double>& params = cg.get_parameters(*context);
+
+  const systems::WitnessFunction<double>* foot_collision;
+  {
+    std::vector<const systems::WitnessFunction<double>*> witness_functions;
+    cg.GetWitnessFunctions(*context, &witness_functions);
+    EXPECT_EQ(witness_functions.size(), 1);
+    foot_collision = witness_functions[0];
+  }
+
+  // Foot is above the ground.
+  state.set_stance(.1);
+  state.set_swing(-.1);
+  EXPECT_GT(cg.CalcWitnessValue(*context, *foot_collision), 0.);
+
+  // Foot is below the ground, but stepping backward.
+  state.set_stance(-.1);
+  state.set_swing(.1);
+  EXPECT_GT(cg.CalcWitnessValue(*context, *foot_collision), 0.);
+
+  // Foot is exactly touching the ground.
+  state.set_stance(.1 + params.slope());
+  state.set_swing(-.1 + params.slope());
+  EXPECT_NEAR(cg.CalcWitnessValue(*context, *foot_collision), 0., 1e-12);
+}
+
+double CalcAngularMomentum(const CompassGait<double>& cg,
+                           const systems::Context<double>& context,
+                           bool about_stance_foot) {
+  const CompassGaitContinuousState<double>& state =
+      cg.get_continuous_state(context);
+  const CompassGaitParams<double>& params = cg.get_parameters(context);
+  const double m = params.mass_leg();
+  const double mh = params.mass_hip();
+  const double a = params.length_leg() - params.center_of_mass_leg();
+  const double b = params.center_of_mass_leg();
+  const double l = params.length_leg();
+  const double cst = cos(state.stance());
+  const double csw = cos(state.swing());
+  const double sst = sin(state.stance());
+  const double ssw = sin(state.swing());
+  const double vst = state.stancedot();
+  const double vsw = state.swingdot();
+
+  // Position and velocity of the mass in the stance leg.
+  const Eigen::Vector2d p_m_stance{a * sst, a * cst};
+  const Eigen::Vector2d v_m_stance{a * cst * vst, -a * sst * vst};
+
+  // Position and velocity of the mass in the hip.
+  const Eigen::Vector2d p_mh{l * sst, l * cst};
+  const Eigen::Vector2d v_mh{l * cst * vst, -l * sst * vst};
+
+  // Position and velocity of the mass in the stance leg.
+  const Eigen::Vector2d p_m_swing = p_mh - Eigen::Vector2d{b * ssw, b * csw};
+  const Eigen::Vector2d v_m_swing =
+      v_mh - Eigen::Vector2d{b * csw * vsw, -b * ssw * vsw};
+
+  Eigen::Vector2d origin = Eigen::Vector2d::Zero();
+  if (!about_stance_foot) {
+    origin = p_mh - Eigen::Vector2d{l * ssw, l * csw};
+  }
+
+  // Compute the out-of-plane component of the cross product of these planar
+  // vectors.  (inputs are in x and z, output is y axis).
+  const auto my_cross = [](const Eigen::Vector2d& a, const Eigen::Vector2d&
+  b) {
+    return a(1) * b(0) - a(0) * b(1);
+  };
+
+  // ∑ r × mv.
+  return my_cross(p_m_stance - origin, m * v_m_stance) +
+         my_cross(p_mh - origin, mh * v_mh) +
+         my_cross(p_m_swing - origin, m * v_m_swing);
+}
+
+GTEST_TEST(CompassGaitTest, TestCollisionDynamics) {
+  // Check that angular momentum is conserved about the impact.
+  // (This fact was not used explicitly in the derivation).
+  const CompassGait<double> cg;
+  auto context = cg.CreateDefaultContext();
+  CompassGaitContinuousState<double>& state =
+      cg.get_mutable_continuous_state(context.get());
+  const CompassGaitParams<double>& params = cg.get_parameters(*context);
+
+  const systems::WitnessFunction<double>* foot_collision;
+  {
+    std::vector<const systems::WitnessFunction<double>*> witness_functions;
+    cg.GetWitnessFunctions(*context, &witness_functions);
+    EXPECT_EQ(witness_functions.size(), 1);
+    foot_collision = witness_functions[0];
+  }
+
+  // Foot is touching the ground.
+  state.set_stance(.2 + params.slope());
+  state.set_swing(-.2 + params.slope());
+  state.set_stancedot(2.);
+  state.set_swingdot(-1.);
+
+  const double angular_momentum_before =
+      CalcAngularMomentum(cg, *context, false);
+
+  // Evaluate collision dynamics.
+  auto next_context = cg.CreateDefaultContext();
+  dynamic_cast<const systems::UnrestrictedUpdateEvent<double>*>(
+      foot_collision->get_event())
+      ->handle(*context, &next_context->get_mutable_state());
+
+  const double angular_momentum_after =
+      CalcAngularMomentum(cg, *next_context, true);
+  EXPECT_NEAR(angular_momentum_before, angular_momentum_after, 1e-8);
+}
+
+}  // namespace
+}  // namespace compass_gait
+}  // namespace examples
+}  // namespace drake
