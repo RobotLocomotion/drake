0c78baca87cfe466fb31274c45d747f14f70adcf
Michael Sherman <sherm1@gmail.com>
2018-04-18 22:02:55 -0700

    Rework System input port evaluators to have good error reporting and unit tests.

--- systems/framework/system.cc
+++ systems/framework/system.cc
@@ -4,11 +4,61 @@
 #include <ios>
 #include <regex>
 
+#include <fmt/format.h>
+
 #include "drake/common/autodiff.h"
 #include "drake/common/default_scalars.h"
 
+namespace {
+
+// Output a string like "System::EvalInput()".
+std::string FmtFunc(const char* func) {
+  return fmt::format("System::{}()", func);
+}
+
+}
+
 namespace drake {
 namespace systems {
+
+template <typename T>
+void System<T>::ThrowNegativeInputPortIndex(const char* func,
+                                            int port_index) const {
+  DRAKE_DEMAND(port_index < 0);
+  throw std::out_of_range(
+      fmt::format("{}: negative port index {} is illegal. (Subsystem {})",
+                  FmtFunc(func), port_index, GetSystemPathname()));
+}
+
+template <typename T>
+void System<T>::ThrowInputPortIndexOutOfRange(const char* func,
+                                              InputPortIndex port,
+                                              int num_input_ports) const {
+  DRAKE_DEMAND(num_input_ports >= 0);
+  throw std::out_of_range(
+      fmt::format("{}: there is no input port with index {} because there "
+                  "are only {} input ports in subsystem {}.",
+                  FmtFunc(func), port, num_input_ports, GetSystemPathname()));
+}
+
+template <typename T>
+void System<T>::ThrowInputPortHasWrongType(
+    const char* func, InputPortIndex port, const std::string& expected_type,
+    const std::string& actual_type) const {
+  throw std::logic_error(fmt::format(
+      "{}: expected value of type {} for input port[{}] "
+      "but the actual type was {}. (Subsystem {})",
+      FmtFunc(func), expected_type, port, actual_type, GetSystemPathname()));
+}
+
+template <typename T>
+void System<T>::ThrowCantEvaluateInputPort(const char* func,
+                                           InputPortIndex port) const {
+  throw std::logic_error(
+      fmt::format("{}: input port[{}] is neither connected nor freestanding so "
+                  "cannot be evaluated. (Subsystem {})",
+                  FmtFunc(func), port, GetSystemPathname()));
+}
 
 std::string SystemImpl::GetMemoryObjectName(
     const std::string& nice_type_name, int64_t address) {
