a27b15b20f0531eb2ab5ec81c9c80d15175a2208
Jamie Snape <jamie.snape@kitware.com>
2018-04-20 15:33:09 -0400

    Ensure pybind11 CMake targets match upstream

--- tools/workspace/pybind11/package-create-cps.py
+++ tools/workspace/pybind11/package-create-cps.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python2
 
+import sys
+
 from drake.tools.install.cpsutils import read_defs
 
+# Targets correct as of pybind11/pybind11@6d0b470.
+
 defs = read_defs("#define PYBIND11_(VERSION[^\s]+)\s+([^\s]+)")
+
+if sys.platform.startswith('darwin'):
+    defs["MODULE_LINK_FLAGS"] = '"Link-Flags": ["-undefined dynamic_lookup"],'
+else:
+    defs["MODULE_LINK_FLAGS"] = ""
 
 content = """
 {
@@ -11,12 +20,32 @@ content = """
   "Description": "Seamless operability between C++11 and Python",
   "License": "BSD-3-Clause",
   "Version": "%(VERSION_MAJOR)s.%(VERSION_MINOR)s.%(VERSION_PATCH)s",
-  "Default-Components": [":module"],
+  "Requires": {
+    "PythonLibsNew": {
+      "X-CMake-Find-Args": ["MODULE"]
+    }
+  },
+  "Default-Components": [":pybind11"],
   "Components": {
+    "embed": {
+      "Type": "interface",
+      "Requires": [
+        ":pybind11",
+        "${PYTHON_LIBRARIES}"
+      ]
+    },
     "module": {
       "Type": "interface",
-      "Includes": ["@prefix@/include/pybind11"],
-      "Compile-Features": ["c++11"]
+      %(MODULE_LINK_FLAGS)s
+      "Requires": [":pybind11"]
+    },
+    "pybind11": {
+      "Type": "interface",
+      "Includes": [
+        "@prefix@/include/pybind11",
+        "${PYTHON_INCLUDE_DIRS}"
+      ],
+      "Compile-Features": ["c++14"]
     }
   },
   "X-CMake-Includes": ["${CMAKE_CURRENT_LIST_DIR}/pybind11Tools.cmake"],
