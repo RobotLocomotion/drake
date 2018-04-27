079faab11612b86b41efd69abe5880b3123c096d
Eric Cousineau <eric.cousineau@tri.global>
2018-04-27 18:42:50 -0400

    pydrake wrap_pybind: Generalize pointer-wrapping for callback arguments

--- bindings/pydrake/systems/controllers_py.cc
+++ bindings/pydrake/systems/controllers_py.cc
@@ -5,34 +5,12 @@
 
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/bindings/pydrake/symbolic_types_pybind.h"
-#include "drake/bindings/pydrake/util/wrap_function.h"
+#include "drake/bindings/pydrake/util/wrap_pybind.h"
 #include "drake/systems/controllers/dynamic_programming.h"
 #include "drake/systems/controllers/linear_quadratic_regulator.h"
 
 namespace drake {
 namespace pydrake {
-
-namespace {
-
-template <typename T, typename = void>
-struct wrap_ptr : public wrap_arg_default<T> {};
-
-template <typename T>
-struct wrap_ptr<const systems::Context<T>&> {
-  using Type = systems::Context<T>;
-  static auto wrap(const Type& arg) { return &arg; }
-  static auto unwrap(const Type* arg_wrapped) { return *arg_wrapped; }
-};
-
-// Ensures that `const Context<T>&` is wrapped with `const Context<T>*`.
-// TODO(eric.cousineau): Replace this with general wrappper, place in
-// `pydrake_pybind` or somewhere related.
-template <typename Func>
-auto WrapPtr(Func&& func) {
-  return WrapFunction<wrap_ptr>(std::forward<Func>(func));
-}
-
-}  // namespace
 
 PYBIND11_MODULE(controllers, m) {
   // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
@@ -58,10 +36,10 @@ PYBIND11_MODULE(controllers, m) {
       .def_readwrite("visualization_callback",
                      &DynamicProgrammingOptions::visualization_callback);
 
-  m.def("FittedValueIteration", WrapPtr(&FittedValueIteration));
+  m.def("FittedValueIteration", WrapCallbacks(&FittedValueIteration));
 
   m.def("LinearProgrammingApproximateDynamicProgramming",
-        WrapPtr(&LinearProgrammingApproximateDynamicProgramming));
+        WrapCallbacks(&LinearProgrammingApproximateDynamicProgramming));
 
   m.def("LinearQuadraticRegulator",
         [](const Eigen::Ref<const Eigen::MatrixXd>& A,
--- bindings/pydrake/util/wrap_pybind.h
+++ bindings/pydrake/util/wrap_pybind.h
@@ -8,6 +8,8 @@
 
 #include "pybind11/pybind11.h"
 
+#include "drake/bindings/pydrake/util/wrap_function.h"
+
 namespace drake {
 namespace pydrake {
 
@@ -35,6 +37,49 @@ class MirrorDef {
   A* const a_{};
   B* const b_{};
 };
+
+namespace detail {
+
+template <typename T, typename = void>
+struct wrap_ref_ptr : public wrap_arg_default<T> {};
+
+template <typename T>
+using is_generic_pybind =
+  std::is_base_of<py::detail::type_caster_generic, py::detail::make_caster<T>>;
+
+// TODO(eric.cousineau): Only use this on non-primitive types (e.g. types whose
+// `type_caster`s are generic).
+template <typename T>
+struct wrap_ref_ptr<T&, std::enable_if_t<is_generic_pybind<T>::value>> {
+  // NOLINTNEXTLINE[runtime/references]: Intentional.
+  static T* wrap(T& arg) { return &arg; }
+  static T& unwrap(T* arg_wrapped) { return *arg_wrapped; }
+};
+
+template <typename T, typename = void>
+struct wrap_callback : public wrap_arg_default<T> {};
+
+template <typename Signature>
+struct wrap_callback<const std::function<Signature>&>
+    : public wrap_arg_function<wrap_ref_ptr, Signature> {};
+
+template <typename Signature>
+struct wrap_callback<std::function<Signature>>
+    : public wrap_callback<const std::function<Signature>&> {};
+
+}  // namespace detail
+
+/// Ensures that any `std::function<>` arguments are wrapped such that any `T&`
+/// (which can infer for `T = const U`) is wrapped as `U*` (and conversely
+/// unwrapped when returned).
+/// Arguments with const or mutable references to objects that may only exist
+/// in C++, and not yet in Python, may cause `pybind11` to trigger a copy, when
+/// it's only valid to use a reference.
+/// For more information, see: https://github.com/pybind/pybind11/issues/1241
+template <typename Func>
+auto WrapCallbacks(Func&& func) {
+  return WrapFunction<detail::wrap_callback, false>(std::forward<Func>(func));
+}
 
 }  // namespace pydrake
 }  // namespace drake
