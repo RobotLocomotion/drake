2490ecae023eb80877e0d484240f3c603ed127cf
hongkai-dai <daihongkai@gmail.com>
2018-04-30 16:44:35 -0700

    Add the part to compute the box-sphere halfspace. Start to add the mixed-integer constraint for bilinear McCormick.

--- solvers/mixed_integer_rotation_constraint.cc
+++ solvers/mixed_integer_rotation_constraint.cc
@@ -1,6 +1,129 @@
 #include "drake/solvers/mixed_integer_rotation_constraint.h"
 
+#include "drake/solvers/mixed_integer_rotation_constraint_internal.h"
+
 namespace drake {
 namespace solvers {
+template <MixedIntegerRotationConstraintType ConstraintType>
+MixedIntegerRotationConstraintGenerator<ConstraintType>::
+    MixedIntegerRotationConstraintGenerator(int num_intervals_per_half_axis,
+                                            IntervalBinning interval_binning)
+    : num_intervals_per_half_axis_(num_intervals_per_half_axis),
+      interval_binning_(interval_binning),
+      phi_nonnegative_{
+          Eigen::VectorXd::LinSpaced(0, 1, num_intervals_per_half_axis_ + 1)} {
+  phi_.resize(2 * num_intervals_per_half_axis_ + 1);
+  phi_(num_intervals_per_half_axis_) = 0;
+  for (int i = 0; i < num_intervals_per_half_axis_; ++i) {
+    phi_(num_intervals_per_half_axis_ - i - 1) = -phi_nonnegative_(i);
+    phi_(num_intervals_per_half_axis_ + i) = phi_nonnegative_(i);
+  }
+
+  // If we consider the box-sphere intersection, then we need to compute the
+  // halfspace nᵀx≥ d, as the tightest halfspace for each intersection region.
+  if (ConstraintType ==
+          MixedIntegerRotationConstraintType::kBoxSphereIntersection ||
+      ConstraintType == MixedIntegerRotationConstraintType::kBoth) {
+    const double kEpsilon = std::numeric_limits<double>::epsilon();
+
+    box_sphere_intersection_vertices_.resize(num_intervals_per_half_axis_);
+    box_sphere_intersection_halfspace_.resize(num_intervals_per_half_axis_);
+    for (int xi = 0; xi < num_intervals_per_half_axis_; ++xi) {
+      box_sphere_intersection_vertices_[xi].resize(
+          num_intervals_per_half_axis_);
+      box_sphere_intersection_halfspace_[xi].resize(
+          num_intervals_per_half_axis_);
+      for (int yi = 0; yi < num_intervals_per_half_axis_; ++yi) {
+        box_sphere_intersection_vertices_[xi][yi].resize(
+            num_intervals_per_half_axis_);
+        box_sphere_intersection_halfspace_[xi][yi].resize(
+            num_intervals_per_half_axis_);
+        for (int zi = 0; zi < num_intervals_per_half_axis_; ++zi) {
+          const Eigen::Vector3d box_min(
+              phi_nonnegative_(xi), phi_nonnegative_(yi), phi_nonnegative_(zi));
+          const Eigen::Vector3d box_max(phi_nonnegative_(xi + 1),
+                                        phi_nonnegative_(yi + 1),
+                                        phi_nonnegative_(zi + 1));
+          const double box_min_norm = box_min.lpNorm<2>();
+          const double box_max_norm = box_max.lpNorm<2>();
+          if (box_min_norm <= 1.0 - 2 * kEpsilon &&
+              box_max_norm >= 1.0 + 2 * kEpsilon) {
+            // box_min is strictly inside the sphere, box_max is strictly
+            // outside of the sphere.
+            box_sphere_intersection_vertices_[xi][yi][zi] =
+                internal::ComputeBoxEdgesAndSphereIntersection(box_min,
+                                                               box_max);
+            DRAKE_DEMAND(box_sphere_intersection_vertices_[xi][yi][zi].size() >=
+                         3);
+
+            Eigen::Vector3d normal{};
+            internal::ComputeHalfSpaceRelaxationForBoxSphereIntersection(
+                box_sphere_intersection_vertices_[xi][yi][zi],
+                &(box_sphere_intersection_halfspace_[xi][yi][zi].first),
+                &(box_sphere_intersection_vertices_[xi][yi][zi].second));
+          } else if (std::abs(box_min_norm - 1) < 2 * kEpsilon) {
+            // box_min is on the surface. This is the unique intersection point
+            // between the sphere surface and the box.
+            box_sphere_intersection_vertices_[xi][yi][zi].push_back(
+                box_min / box_min_norm);
+          } else if (std::abs(box_max_norm - 1) < 2 * kEpsilon) {
+            // box_max is on the surface. This is the unique intersection point
+            // between the sphere surface and the box.
+            box_sphere_intersection_vertices_[xi][yi][zi].push_back(
+                box_max / box_max_norm);
+          }
+        }
+      }
+    }
+  }
+}
+
+template <MixedIntegerRotationConstraintType ConstraintType>
+typename AddMixedIntegerRotationConstraintReturn<ConstraintType>::Type
+MixedIntegerRotationConstraintGenerator<ConstraintType>::AddToProgram(
+    MathematicalProgram* prog,
+    const Eigen::Ref<const MatrixDecisionVariable<3, 3>>& R) const {}
+
+template <>
+AddMixedIntegerRotationConstraintReturn<
+    MixedIntegerRotationConstraintType::kBilinearMcCormick>::Type
+MixedIntegerRotationConstraintGenerator<
+    MixedIntegerRotationConstraintType::kBilinearMcCormick>::
+    AddToProgram(
+        MathematicalProgram* prog,
+        const Eigen::Ref<const MatrixDecisionVariable<3, 3>>& R) const {
+  std::array<std::array<VectorXDecisionVariable, 3>, 3> lambda;
+  AddMixedIntegerRotationConstraintReturn<
+      MixedIntegerRotationConstraintType::kBilinearMcCormick>::Type B;
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      const std::string lambda_name =
+          "lambda[" + std::to_string(i) + "][" + std::to_string(j) + "]";
+      lambda[i][j] = prog->NewContinuousVariables(
+          2 * num_intervals_per_half_axis_ + 1, lambda_name);
+      // R(i, j) = φᵀ * λ[i][j]
+      prog->AddLinearConstraint(
+          R(i, j) -
+              phi_.dot(lambda[i][j].template cast<symbolic::Expression>()) ==
+          0);
+      switch (interval_binning_) {
+        case IntervalBinning::kLogarithmic: {
+          B[i][j] = AddLogarithmicSos2Constraint(
+              prog, lambda[i][j].cast<symbolic::Expression>());
+          break;
+        }
+        case IntervalBinning::kLinear: {
+          B[i][j] = prog->NewBinaryVariables(
+              2 * num_intervals_per_half_axis_,
+              "B[" + std::to_string(i) + "][" + std::to_string(j) + "]");
+          AddSos2Constraint(prog, lambda[i][j].cast<symbolic::Expression>(),
+                            B[i][j].cast<symbolic::Expression>());
+          break;
+        }
+      }
+    }
+  }
+  return B;
+}
 }  // namespace solvers
 }  // namespace drake
--- solvers/mixed_integer_rotation_constraint.h
+++ solvers/mixed_integer_rotation_constraint.h
@@ -3,61 +3,120 @@
 #include "drake/solvers/mathematical_program.h"
 #include "drake/solvers/mixed_integer_optimization_util.h"
 
+namespace drake {
+namespace solvers {
+enum class MixedIntegerRotationConstraintType {
+  kBoxSphereIntersection,  ///< Relax SO(3) constraint by considering the
+                           // intersection between boxes and the unit sphere
+                           // surface.
+  kBilinearMcCormick,      ///< Relax SO(3) constraint by considering the
+                           // McCormick envelope on the bilinear product.
+  kBoth,                   ///< Relax SO(3) constraint by considering both the
+                           // intersection between boxes and the unit sphere
+                           // surface, and the McCormick envelope on the
+                           // bilinear product.
+};
+
 /**
- * @file
+ * The return type of adding mixed integer relaxation for SO(3) constraint, if
+ * the approach is to consider the intersection between the box and the unit
+ * sphere surface.
+ */
+struct MixedIntegerRotationConstraintBoxSphereIntersectionReturn {
+  std::vector<Matrix3<symbolic::Expression>> CRpos;
+  std::vector<Matrix3<symbolic::Expression>> CRneg;
+  std::vector<Matrix3<symbolic::Variable>> BRpos;
+  std::vector<Matrix3<symbolic::Variable>> BRneg;
+};
+
+/**
+ * The return type of adding mixed integer relaxation for SO(3) constraint, if
+ * the approach is to consider both the intersection between the box and the
+ * unit sphere surface, and the McCormick envelope of bilinear products.
+ */
+struct MixedIntegerRotationConstraintBothReturn {
+  std::array<std::array<VectorXDecisionVariable, 3>, 3> B;
+  std::vector<Matrix3<symbolic::Expression>> CRpos;
+  std::vector<Matrix3<symbolic::Expression>> CRneg;
+  std::vector<Matrix3<symbolic::Variable>> BRpos;
+  std::vector<Matrix3<symbolic::Variable>> BRneg;
+};
+
+/**
+ * The return type for adding mixed integer relaxation for SO(3) constraint.
+ * We will specialize the return type based on different constraint types.
+ */
+template <MixedIntegerRotationConstraintType ConstraintType>
+struct AddMixedIntegerRotationConstraintReturn {
+  typedef void Type;
+};
+
+template <>
+struct AddMixedIntegerRotationConstraintReturn<
+    MixedIntegerRotationConstraintType::kBoxSphereIntersection> {
+  typedef MixedIntegerRotationConstraintBoxSphereIntersectionReturn Type;
+};
+
+template <>
+struct AddMixedIntegerRotationConstraintReturn<
+    MixedIntegerRotationConstraintType::kBilinearMcCormick> {
+  typedef std::array<std::array<VectorXDecisionVariable, 3>, 3> Type;
+};
+
+template <>
+struct AddMixedIntegerRotationConstraintReturn<
+    MixedIntegerRotationConstraintType::kBoth> {
+  typedef MixedIntegerRotationConstraintBothReturn Type;
+};
+
+/**
  * We will relax the non-convex SO(3) constraint on rotation matrix R to
  * mixed-integer linear constraints. The formulation of these constraints are
  * described in
  * Global Inverse Kinematics via Mixed-integer Convex Optimization
  * by Hongkai Dai, Gregory Izatt and Russ Tedrake, ISRR, 2017
  */
-
-namespace drake {
-namespace solvers {
-/**
- * Relax the non-convex SO(3) constraint on rotation matrix, by considering the
- * intersection between boxes and unit length sphere surface. The boxes are
- * obtained by cutting the x, y and z axes into small intervals, and each cell
- * in 3D space is a box.
- * The formulation of these constraints are described in
- * Global Inverse Kinematics via Mixed-integer Convex Optimization
- * by Hongkai Dai, Gregory Izatt and Russ Tedrake, ISRR, 2017
- */
-class MixedIntegerRotationConstraintByBoxSphereIntersectionGenerator {
+template <MixedIntegerRotationConstraintType ConstraintType>
+class MixedIntegerRotationConstraintGenerator {
  public:
   DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(
-      MixedIntegerRotationConstraintByBoxSphereIntersectionGenerator)
+      MixedIntegerRotationConstraintGenerator)
 
-  MixedIntegerRotationConstraintByBoxSphereIntersectionGenerator(
-      int num_intervals_per_half_axis, IntervalBinning interval_binning);
+  MixedIntegerRotationConstraintGenerator(int num_intervals_per_half_axis,
+                                          IntervalBinning interval_binning);
 
-  struct ReturnType {
-    std::vector<Matrix3<symbolic::Expression>> CRpos;
-    std::vector<Matrix3<symbolic::Expression>> CRneg;
-    std::vector<Matrix3<symbolic::Variable>> BRpos;
-    std::vector<Matrix3<symbolic::Variable>> BRneg;
-  };
-
-  ReturnType AddToProgram(MathematicalProgram* prog,
-               const Eigen::Ref<const MatrixDecisionVariable<3, 3>>& R);
+  /**
+   * Add the mixed-integer linear constraints to the optimization program, as
+   * an relaxation of SO(3) constraint on the rotation matrix `R`.
+   * @param prog The optimization program to which the mixed-integer constraints
+   * (and additional variables) are added.
+   * @param R The rotation matrix on which the SO(3) constraint is imposed.
+   */
+  typename AddMixedIntegerRotationConstraintReturn<ConstraintType>::Type
+  AddToProgram(MathematicalProgram* prog,
+               const Eigen::Ref<const MatrixDecisionVariable<3, 3>>& R) const;
 
  private:
   const int num_intervals_per_half_axis_;
-  const RelaxationType relaxation_type_;
+  const IntervalBinning interval_binning_;
   // φ(i) = -1 + 1 / num_intervals_per_half_axis_ * i
   Eigen::VectorXd phi_;
   // φ₊(i) = 1 / num_intervals_per_half_axis_ * i
   Eigen::VectorXd phi_nonnegative_;
 
   // When considering the intersection between the box and the sphere surface,
-  // we will find one halfspace nᵀx ≥ d, such that all points on the
-  // intersection surface satisfy this halfspace constraint. For intersection
-  // region between the box [φ₊(xi), φ₊(xi+1)] x [φ₊(yi), φ₊(yi+1)] x
-  // [φ₊(zi), φ₊(zi+1)] and the sphere surface, this halfspace is
-  // (intersection_halfspace_[xi][yi][zi].first)ᵀ x ≥
-  // intersection_halfspace[xi][yi][zi].second
+  // we will compute the vertices of the intersection region, and find one tight
+  // halfspace nᵀx ≥ d, such that all points on the intersection surface satisfy
+  // this halfspace constraint. For intersection region between the box
+  // [φ₊(xi), φ₊(xi+1)] x [φ₊(yi), φ₊(yi+1)] x [φ₊(zi), φ₊(zi+1)] and the sphere
+  // surface, the vertices of the intersection region is in
+  // box_sphere_intersection_vertices_[xi][yi][zi], and the halfspace is
+  // (box_sphere_intersection_halfspace_[xi][yi][zi].first)ᵀ x ≥
+  // box_sphere_intersection_halfspace[xi][yi][zi].second
+  std::vector<std::vector<std::vector<std::vector<Eigen::Vector3d>>>>
+      box_sphere_intersection_vertices_;
   std::vector<std::vector<std::vector<std::pair<Eigen::Vector3d, double>>>>
-      intersection_halfspace_
+      box_sphere_intersection_halfspace_;
 };
 }  // namespace solvers
 }  // namespace drake
