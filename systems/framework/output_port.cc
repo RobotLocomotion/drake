a9f633f9dafb23b44f16e689ac157bb4aa6087d0
Jeremy Nimmer <jeremy.nimmer@tri.global>
2018-05-11 11:54:10 -0400

    Add userscalar_acceptance_test

--- systems/framework/output_port.cc
+++ systems/framework/output_port.cc
@@ -2,16 +2,5 @@
 
 #include "drake/common/default_scalars.h"
 
-namespace drake {
-namespace systems {
-
-// The Vector2/3 instantiations here are for the benefit of some
-// older unit tests but are not otherwise advertised.
-template class OutputPort<Eigen::AutoDiffScalar<Eigen::Vector2d>>;
-template class OutputPort<Eigen::AutoDiffScalar<Eigen::Vector3d>>;
-
-}  // namespace systems
-}  // namespace drake
-
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
     class ::drake::systems::OutputPort)
