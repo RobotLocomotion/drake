2f4c46090b04a79324712691e3c9ba88cb4e578f
amcastro-tri <alejandro.castro@tri.global>
2018-05-16 11:54:07 -0400

    Updates MobImpl to use default_scalars.h expl instantiation

--- multibody/multibody_tree/mobilizer_impl.cc
+++ multibody/multibody_tree/mobilizer_impl.cc
@@ -2,7 +2,7 @@
 
 #include <memory>
 
-#include "drake/common/autodiff.h"
+#include "drake/common/default_scalars.h"
 #include "drake/multibody/multibody_tree/body_node_impl.h"
 
 namespace drake {
@@ -16,17 +16,30 @@ std::unique_ptr<internal::BodyNode<T>> MobilizerImpl<T, nq, nv>::CreateBodyNode(
                                                              body, mobilizer);
 }
 
-// Macro used to explicitly instantiate implementations on all sizes needed.
-#define EXPLICITLY_INSTANTIATE_IMPLS(T) \
-template class MobilizerImpl<T, 0, 0>; \
-template class MobilizerImpl<T, 1, 1>; \
-template class MobilizerImpl<T, 2, 2>; \
-template class MobilizerImpl<T, 3, 3>; \
-template class MobilizerImpl<T, 7, 6>;
-
-// Explicitly instantiates on the most common scalar types.
-EXPLICITLY_INSTANTIATE_IMPLS(double);
-EXPLICITLY_INSTANTIATE_IMPLS(AutoDiffXd);
+// Helper classes to aid the explicit instantiation with macro
+// DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(), which
+// expects a template class as an argument.
+template <typename T>
+class MobilizerImpl0x0 : public MobilizerImpl<T, 0, 0> {};
+template <typename T>
+class MobilizerImpl1x1 : public MobilizerImpl<T, 1, 1> {};
+template <typename T>
+class MobilizerImpl2x2 : public MobilizerImpl<T, 2, 2> {};
+template <typename T>
+class MobilizerImpl3x3 : public MobilizerImpl<T, 3, 3> {};
+template <typename T>
+class MobilizerImpl7x6 : public MobilizerImpl<T, 7, 6> {};
 
 }  // namespace multibody
 }  // namespace drake
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
+    class ::drake::multibody::MobilizerImpl0x0)
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
+    class ::drake::multibody::MobilizerImpl1x1)
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
+    class ::drake::multibody::MobilizerImpl2x2)
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
+    class ::drake::multibody::MobilizerImpl3x3)
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
+    class ::drake::multibody::MobilizerImpl7x6)
