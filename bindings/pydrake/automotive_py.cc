6e57764a498fb6c5002226a2c5c2b06af9266330
Jonathan DeCastro <jonathan.decastro@tri.global>
2018-05-22 09:49:57 -0400

    Bind automotive PoseSelector (#8809)
    * Bind automotive PoseSelector

--- bindings/pydrake/automotive_py.cc
+++ bindings/pydrake/automotive_py.cc
@@ -4,11 +4,15 @@
 #include "drake/automotive/calc_ongoing_road_position.h"
 #include "drake/automotive/gen/driving_command.h"
 #include "drake/automotive/idm_controller.h"
+#include "drake/automotive/maliput/api/lane_data.h"
 #include "drake/automotive/maliput/api/road_geometry.h"
+#include "drake/automotive/pose_selector.h"
 #include "drake/automotive/pure_pursuit_controller.h"
+#include "drake/automotive/road_odometry.h"
 #include "drake/automotive/simple_car.h"
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/bindings/pydrake/systems/systems_pybind.h"
+#include "drake/bindings/pydrake/util/wrap_pybind.h"
 #include "drake/systems/framework/basic_vector.h"
 
 namespace drake {
@@ -27,6 +31,10 @@ PYBIND11_MODULE(automotive, m) {
 
   using T = double;
 
+  py::enum_<AheadOrBehind>(m, "AheadOrBehind")
+      .value("kAhead", AheadOrBehind::kAhead)
+      .value("kBehind", AheadOrBehind::kBehind);
+
   py::enum_<RoadPositionStrategy>(m, "RoadPositionStrategy")
       .value("kCache", RoadPositionStrategy::kCache)
       .value("kExhaustiveSearch", RoadPositionStrategy::kExhaustiveSearch);
@@ -35,10 +43,40 @@ PYBIND11_MODULE(automotive, m) {
       .value("kPath", ScanStrategy::kPath)
       .value("kBranches", ScanStrategy::kBranches);
 
+  py::class_<ClosestPose<T>>(m, "ClosestPose")
+      .def(py::init<>())
+      .def(py::init<const RoadOdometry<T>&, const T&>(), py::arg("odom"),
+           py::arg("dist"),
+           // Keep alive, transitive: `self` keeps `RoadOdometry` pointer
+           // members alive.
+           py::keep_alive<1, 2>())
+      .def_readwrite("odometry", &ClosestPose<T>::odometry,
+                     py_reference_internal)
+      .def_readwrite("distance", &ClosestPose<T>::distance);
+
+  py::class_<RoadOdometry<T>> road_odometry(m, "RoadOdometry");
+  road_odometry
+      .def(py::init<>())
+      .def(py::init<const maliput::api::RoadPosition&,
+                    const systems::rendering::FrameVelocity<T>&>(),
+           py::arg("road_position"), py::arg("frame_velocity"),
+           // Keep alive, transitive: `self` keeps `RoadPosition` pointer
+           // members alive.
+           py::keep_alive<1, 2>())
+      .def(py::init<const maliput::api::Lane*,
+                    const maliput::api::LanePositionT<T>&,
+                    const systems::rendering::FrameVelocity<T>&>(),
+           py::arg("lane"), py::arg("lane_position"), py::arg("frame_velocity"),
+           // Keep alive, reference: `self` keeps `Lane*` alive.
+           py::keep_alive<1, 2>())
+      .def_readwrite("pos", &RoadOdometry<T>::pos)
+      .def_readwrite("vel", &RoadOdometry<T>::vel);
+  DefReadWriteKeepAlive(&road_odometry, "lane", &RoadOdometry<T>::lane);
+
   py::class_<LaneDirection>(m, "LaneDirection")
       .def(py::init<const maliput::api::Lane*, bool>(), py::arg("lane"),
            py::arg("with_s"))
-      .def_readwrite("lane", &LaneDirection::lane)
+      .def_readwrite("lane", &LaneDirection::lane, py_reference_internal)
       .def_readwrite("with_s", &LaneDirection::with_s);
   pysystems::AddValueInstantiation<LaneDirection>(m);
 
@@ -51,10 +89,10 @@ PYBIND11_MODULE(automotive, m) {
       .def("set_acceleration", &DrivingCommand<T>::set_acceleration);
 
   py::class_<IdmController<T>, LeafSystem<T>>(m, "IdmController")
-      .def(py::init<const maliput::api::RoadGeometry&,
-           ScanStrategy, RoadPositionStrategy, double>(), py::arg("road"),
-           py::arg("path_or_branches"), py::arg("road_position_strategy"),
-           py::arg("period_sec"))
+      .def(py::init<const maliput::api::RoadGeometry&, ScanStrategy,
+                    RoadPositionStrategy, double>(),
+           py::arg("road"), py::arg("path_or_branches"),
+           py::arg("road_position_strategy"), py::arg("period_sec"))
       .def("ego_pose_input", &IdmController<T>::ego_pose_input,
            py_reference_internal)
       .def("ego_velocity_input", &IdmController<T>::ego_velocity_input,
@@ -64,8 +102,35 @@ PYBIND11_MODULE(automotive, m) {
       .def("acceleration_output", &IdmController<T>::acceleration_output,
            py_reference_internal);
 
-  py::class_<PurePursuitController<T>, LeafSystem<T>>(
-      m, "PurePursuitController")
+  py::class_<PoseSelector<T>>(m, "PoseSelector")
+      .def_static(
+          "FindClosestPair",
+          [](const maliput::api::Lane* lane,
+             const systems::rendering::PoseVector<T>& ego_pose,
+             const systems::rendering::PoseBundle<T>& traffic_poses,
+             const T& scan_distance, ScanStrategy path_or_branches) {
+            return PoseSelector<T>::FindClosestPair(
+                lane, ego_pose, traffic_poses, scan_distance, path_or_branches);
+          },
+          py::arg("lane"), py::arg("ego_pose"), py::arg("traffic_poses"),
+          py::arg("scan_distance"), py::arg("path_or_branches"))
+      .def_static("FindSingleClosestPose",
+                  [](const maliput::api::Lane* lane,
+                     const systems::rendering::PoseVector<T>& ego_pose,
+                     const systems::rendering::PoseBundle<T>& traffic_poses,
+                     const T& scan_distance, const AheadOrBehind side,
+                     ScanStrategy path_or_branches) {
+                    return PoseSelector<T>::FindSingleClosestPose(
+                        lane, ego_pose, traffic_poses, scan_distance, side,
+                        path_or_branches);
+                  },
+                  py::arg("lane"), py::arg("ego_pose"),
+                  py::arg("traffic_poses"), py::arg("scan_distance"),
+                  py::arg("side"), py::arg("path_or_branches"))
+      .def_static("GetSigmaVelocity", &PoseSelector<T>::GetSigmaVelocity);
+
+  py::class_<PurePursuitController<T>, LeafSystem<T>>(m,
+                                                      "PurePursuitController")
       .def(py::init<>())
       .def("ego_pose_input", &PurePursuitController<T>::ego_pose_input,
            py_reference_internal)
