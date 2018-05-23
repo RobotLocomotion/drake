1b70dca12ac0ebf595f2c68a5ee1c2f9e6d75739
Michael Sherman <sherm1@gmail.com>
2018-05-22 20:22:42 -0700

    Pull DiagramOutputPort into its own file and untangle from Diagram to parallel LeafOutputPort.

--- systems/framework/diagram.cc
+++ systems/framework/diagram.cc
@@ -3,9 +3,6 @@
 #include "drake/common/default_scalars.h"
 
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
-    class ::drake::systems::internal::DiagramOutputPort)
-
-DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
     class ::drake::systems::internal::DiagramOutput)
 
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
