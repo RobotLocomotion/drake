e4b358696e2c54e93cd7099d1e77819c708a9dd2
Sam Creasey <sam.creasey@tri.global>
2018-05-29 14:42:24 -0400

    Modify the optitrack sim to take FramePoseVector.
    This is needed for MultibodyPlant support.

--- systems/sensors/optitrack_sender.h
+++ systems/sensors/optitrack_sender.h
@@ -5,31 +5,47 @@
 /// publishing over LCM, and may support other messaging protocols in the
 /// future.
 
+#include <map>
+#include <string>
+#include <utility>
+
 #include "optitrack/optitrack_frame_t.hpp"
 
+#include "drake/common/drake_assert.h"
 #include "drake/common/drake_copyable.h"
 #include "drake/common/eigen_types.h"
+#include "drake/geometry/geometry_ids.h"
 #include "drake/systems/framework/leaf_system.h"
 
 namespace drake {
 namespace systems {
 namespace sensors {
 
+// TODO(sam.creasey) Support publising data description messages.
 
-/// The system has one abstract-valued input port and one abstract-valued
-/// output port. The abstract input port is templated on a std::vector of type
-/// manipulation::perception::TrackedBody, and the abstract output port value
-/// is templated on type `optitrack_frame_t`.
+/// The system has one abstract-valued input port and one
+/// abstract-valued output port. The one abstract input port is
+/// templated on a geometry::FramePoseVector.  The abstract output
+/// port value is templated on type `optitrack_frame_t`.
 ///
 /// Note that this system does not actually send this message on an LCM channel.
 /// To send the message, the output of this system should be connected to an
-/// input port of a systems::Value objecte templated on type
+/// input port of a systems::Value object templated on type
 /// `optitrack_frame_t`.
-class OptitrackLCMFrameSender : public systems::LeafSystem<double> {
+class OptitrackLcmFrameSender : public systems::LeafSystem<double> {
  public:
-  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(OptitrackLCMFrameSender)
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(OptitrackLcmFrameSender)
 
-  explicit OptitrackLCMFrameSender(int num_rigid_bodies);
+  /// Create an OptitrackLcmFrameSender using the FramePoseVector
+  /// input port.
+  ///
+  /// @param frame_map contains a map of geometry::FrameId to the
+  /// rigid body name and id to be published in the resulting lcm
+  /// messages.
+  explicit OptitrackLcmFrameSender(
+      const std::map<geometry::FrameId, std::pair<std::string, int>>&
+      frame_map);
+
 
   const systems::InputPortDescriptor<double>& get_optitrack_input_port() const {
     return get_input_port(0);
@@ -40,14 +56,15 @@ class OptitrackLCMFrameSender : public systems::LeafSystem<double> {
   }
 
  private:
-  // This is the method to use for the output port allocator.
   optitrack::optitrack_frame_t CreateNewMessage() const;
 
-  // This is the calculator method for the output port.
-  void PopulateMessage(const systems::Context<double>& context,
-                       optitrack::optitrack_frame_t* output) const;
+  void PopulatePoseMessage(
+      const systems::Context<double>& context,
+      optitrack::optitrack_frame_t* output) const;
 
-  const int num_rigid_bodies_;
+  const int num_rigid_bodies_{-1};
+  const std::map<geometry::FrameId,
+                 std::pair<std::string, int>> frame_map_;
 };
 
 }  // namespace sensors
