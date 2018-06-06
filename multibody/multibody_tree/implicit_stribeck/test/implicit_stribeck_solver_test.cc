af06386416c83f255bc8afc9f28e94dc976a908b
amcastro-tri <alejandro.castro@tri.global>
2018-06-05 21:19:54 -0400

    Implements first crossing by zero test

--- multibody/multibody_tree/implicit_stribeck/test/implicit_stribeck_solver_test.cc
+++ multibody/multibody_tree/implicit_stribeck/test/implicit_stribeck_solver_test.cc
@@ -4,283 +4,48 @@
 
 #include <gtest/gtest.h>
 
+#include "drake/common/test_utilities/eigen_matrix_compare.h"
+
+#include <iostream>
+#define PRINT_VAR(a) std::cout << #a": " << a << std::endl;
+
 namespace drake {
 namespace multibody {
 namespace implicit_stribeck {
 namespace {
 
-/* Top view of the pizza saver:
-
-  ^ y               C
-  |                 ◯
-  |                /\
-  ----> x         /  \
-               b /    \ a
-                /      \
-               /        \
-              ◯----------◯
-              A     c    B
-
-It is modeled as an equilateral triangle with a contact point at each of the
-legs. The total mass of the pizza saver is m and its rotational inertia about
-the triangle's barycenter is I.
-If h is the height of the triangle from any of its sides, the distance from
-any point to the triangle's center is 2h/3. The height h relates to the size
-a of a side by h = 3/2/sqrt(3) a.
-The generalized positions vector for this case is q = [x, y, theta], with
-theta = 0 for the triangle in the configuration shown in the schematic. */
-class PizzaSaver : public ::testing::Test {
+class DirectionLimiter : public ::testing::Test {
  public:
   void SetUp() override {
-    // Now we'll set up each term in the equation:
-    //   Mv̇ = τ + Dᵀ⋅fₜ
-    // where τ =[Fx, Fy, Mz] contains the external force in x, the external
-    // force in y and the external moment about z (out of plane).
-    M_ << m_,  0,  0,
-           0, m_,  0,
-           0,  0,  I_;
-  }
-
-  MatrixX<double> ComputeTangentialJacobian(double theta) {
-    MatrixX<double> D(2 * nc_, nv_);
-    const double c = cos(theta);
-    const double s = sin(theta);
-
-    // 2D rotation matrix of the body frame B in the world frame W.
-    Matrix2<double> R_WB;
-    R_WB <<  c, s,
-            -s, c;
-
-    // Position of each contact point in the body frame B.
-    const Vector2<double> p_BoA(-sqrt(3) / 2.0, -0.5);
-    const Vector2<double> p_BoB(sqrt(3) / 2.0, -0.5);
-    const Vector2<double> p_BoC(0.0, 1.0);
-
-    // Position of each contact point in the world frame W.
-    const Vector2<double> p_BoA_W = R_WB * p_BoA;
-    const Vector2<double> p_BoB_W = R_WB * p_BoB;
-    const Vector2<double> p_BoC_W = R_WB * p_BoC;
-
-    // Point A
-    D.block(0, 0, 2, nv_) << 1, 0, -p_BoA_W.y(),
-                             0, 1,  p_BoA_W.x();
-
-    // Point B
-    D.block(2, 0, 2, nv_) << 1, 0, -p_BoB_W.y(),
-                             0, 1,  p_BoB_W.x();
-
-    // Point C
-    D.block(4, 0, 2, nv_) << 1, 0, -p_BoC_W.y(),
-                             0, 1,  p_BoC_W.x();
-
-    return D;
-  }
-
-  void SetProblem(const Vector3<double>& v0, const Vector3<double>& tau,
-                  double mu, double theta, double dt) {
-    // Next time step generalized momentum if there are no friction forces.
-    p_star_ = M_ * v0 + dt * tau;
-
-    // Normal forces. Assume they are equally distributed.
-    fn_ = m_ * g_ / 3.0 * Vector3<double>::Ones();
-
-    // All contact points have the same friction for this case.
-    mu_ = mu * Vector3<double>::Ones();
-
-    D_ = ComputeTangentialJacobian(theta);
-
-    solver_.SetProblemData(&M_, &D_, &p_star_, &fn_, &mu_);
   }
 
  protected:
-  // Problem parameters.
-  const double m_{1.0};   // Mass of the pizza saver.
-  const double R_{1.0};   // Distance from COM to any contact point.
-  const double g_{10.0};  // Acceleration of gravity.
-  // The radius of the circumscribed circle R is the distance from each
-  // contact point to the triangle's center.
-  // If we model the pizza saver as three point masses m/3 at each contact
-  // point, the moment of inertia is I = 3 * (m/3 R²):
-  const double I_{R_ * R_ * m_};  // = 1.0 in this case.
-
-  // Problem sizes.
-  const int nv_{3};  // number of generalized velocities.
-  const int nc_{3};  // number of contact points.
-
-  // Mass matrix.
-  MatrixX<double> M_{nv_, nv_};
-
-  // Tangential velocities Jacobian.
-  MatrixX<double> D_{2 * nc_, nv_};
-
-  // The implicit Stribeck solver for this problem.
-  ImplicitStribeckSolver<double> solver_{nv_};
-
-  // Additional solver data that must outlive solver_ during solution.
-  Vector3<double> p_star_;  // Generalized momentum.
-  Vector3<double> fn_;      // Normal forces at each contact point.
-  Vector3<double> mu_;      // Friction coefficient at each contact point.
+  // Limiter parameters. See LimitDirectionChange for further details.
+  const double v_stribeck = 1.0e-4;  // m/s
+  const double theta_max = M_PI / 6.0;  // radians.
+  const double cos_min = std::cos(theta_max);
+  const double tolerance = 0.01;  // Dimensionless. A factor of v_stribeck.
+  // Tolerance to perform comparisons close to machine precission.
+  const double kTolerance = 10 * std::numeric_limits<double>::epsilon();
 };
 
-// This test the solver when we apply a moment Mz about COM to the pizza saver.
-// If Mz < mu * m * g * R, the saver should be in stiction (within the Stribeck
-// approximation). Otherwise the saver will be sliding.
-// For this setup the transition occurs at M_transition = mu * m * g * R = 5.0
-TEST_F(PizzaSaver, SmallAppliedMoment) {
-  const double kTolerance = 10 * std::numeric_limits<double>::epsilon();
+TEST_F(DirectionLimiter, StraightCrossThroughZero) {
+  const Vector2<double> vt(0.1, 0.05);
+  const Vector2<double> dvt(-0.3, -0.15);  // dvt = -3 * vt.
 
-  const double dt = 1.0e-3;  // time step in seconds.
+  const double alpha = internal::LimitDirectionChange<double>::run(
+      vt, dvt, cos_min, v_stribeck, tolerance);
 
-  const double mu = 0.5;
+  PRINT_VAR(alpha);
 
-  // Some arbitrary orientation. This particular case has symmetry of
-  // revolution.
-  const double theta = M_PI / 5;
+  // Since the change crosses zero exactly, we expect
+  // v_alpha = v + alpha * dv = v/‖v‖⋅vₛ/2.
+  const Vector2<double> vt_alpha_expected = vt.normalized() * v_stribeck / 2.0;
 
-  // External forcing.
-  const double Mz = 3.0;  // M_transition = 5.0
-  const Vector3<double> tau(0.0, 0.0, Mz);
+  const Vector2<double> vt_alpha = vt + alpha * dvt;
 
-  // Initial velocity.
-  const Vector3<double> v0 = Vector3<double>::Zero();
-
-  SetProblem(v0, tau, mu, theta, dt);
-
-  Parameters parameters;  // Default parameters.
-  parameters.stiction_tolerance = 1.0e-6;
-  solver_.set_solver_parameters(parameters);
-
-  ComputationInfo info = solver_.SolveWithGuess(dt, v0);
-  ASSERT_EQ(info, ComputationInfo::Success);
-
-  VectorX<double> tau_f = solver_.get_generalized_forces();
-
-  const IterationStats& stats = solver_.get_iteration_statistics();
-
-  const double vt_tolerance =
-      /* Dimensionless relative (to the stiction tolerance) tolerance */
-      solver_.get_solver_parameters().tolerance *
-          solver_.get_solver_parameters().stiction_tolerance;
-  EXPECT_TRUE(stats.vt_residual < vt_tolerance);
-
-  // For this problem we expect the x and y components of the forces due to
-  // friction to be zero to machine epsilon.
-  EXPECT_NEAR(tau_f(0), 0.0, kTolerance);
-  EXPECT_NEAR(tau_f(1), 0.0, kTolerance);
-
-  // The moment due to friction should balance the applied Mz. There is though
-  // an error introduced by the non-zero stiction tolerance.
-  EXPECT_NEAR(tau_f(2), Mz, 5.0e-4);
-
-  const VectorX<double>& vt = solver_.get_tangential_velocities();
-  ASSERT_EQ(vt.size(), 2 * nc_);
-
-  // The problem has symmetry of revolution. Thus, for any rotation theta,
-  // the three tangential velocities should have the same magnitude.
-  const double v_slipA = vt.segment<2>(0).norm();
-  const double v_slipB = vt.segment<2>(2).norm();
-  const double v_slipC = vt.segment<2>(4).norm();
-  EXPECT_NEAR(v_slipA, v_slipB, kTolerance);
-  EXPECT_NEAR(v_slipA, v_slipC, kTolerance);
-  EXPECT_NEAR(v_slipC, v_slipB, kTolerance);
-
-  // For this case where Mz < M_transition, we expect stiction (within the
-  // Stribeck's stiction tolerance)
-  EXPECT_LT(v_slipA, parameters.stiction_tolerance);
-  EXPECT_LT(v_slipB, parameters.stiction_tolerance);
-  EXPECT_LT(v_slipC, parameters.stiction_tolerance);
-
-  const VectorX<double>& v = solver_.get_generalized_velocities();
-  ASSERT_EQ(v.size(), nv_);
-
-  // For this problem we expect the translational velocities to be zero within
-  // machine epsilon.
-  EXPECT_NEAR(v(0), 0.0, kTolerance);
-  EXPECT_NEAR(v(1), 0.0, kTolerance);
-}
-
-// Exactly the same problem as in PizzaSaver::SmallAppliedMoment but with an
-// applied moment Mz = 6.0 > M_transition = 5.0. In this case the pizza saver
-// transitions to sliding with a net moment of Mz - M_transition during a
-// period (time stepping interval) dt. Therefore we expect a change of angular
-// velocity given by Δω = dt⋅(Mz - Mtransition) / I.
-TEST_F(PizzaSaver, LargeAppliedMoment) {
-  const double kTolerance = 10 * std::numeric_limits<double>::epsilon();
-
-  const double dt = 1.0e-3;  // time step in seconds.
-  const double mu = 0.5;  // Friction coefficient.
-
-  // Some arbitrary orientation. This particular case has symmetry of
-  // revolution.
-  const double theta = M_PI / 5;
-
-  // External forcing.
-  const double M_transition = 5.0;
-  const double Mz = 6.0;
-  const Vector3<double> tau(0.0, 0.0, Mz);
-
-  // Initial velocity.
-  const Vector3<double> v0 = Vector3<double>::Zero();
-
-  SetProblem(v0, tau, mu, theta, dt);
-
-  Parameters parameters;  // Default parameters.
-  parameters.stiction_tolerance = 1.0e-6;
-  solver_.set_solver_parameters(parameters);
-
-  ComputationInfo info = solver_.SolveWithGuess(dt, v0);
-  ASSERT_EQ(info, ComputationInfo::Success);
-
-  VectorX<double> tau_f = solver_.get_generalized_forces();
-
-  const IterationStats& stats = solver_.get_iteration_statistics();
-
-  const double vt_tolerance =
-      /* Dimensionless relative (to the stiction tolerance) tolerance */
-      solver_.get_solver_parameters().tolerance *
-          solver_.get_solver_parameters().stiction_tolerance;
-  EXPECT_TRUE(stats.vt_residual < vt_tolerance);
-
-  // For this problem we expect the x and y components of the forces due to
-  // friction to be zero to machine epsilon.
-  EXPECT_NEAR(tau_f(0), 0.0, kTolerance);
-  EXPECT_NEAR(tau_f(1), 0.0, kTolerance);
-  // Since we are sliding, the total moment should match M_transition.
-  EXPECT_NEAR(tau_f(2), M_transition, 1.0e-13);
-
-  const VectorX<double>& vt = solver_.get_tangential_velocities();
-  ASSERT_EQ(vt.size(), 2 * nc_);
-
-  // The problem has symmetry of revolution. Thus, for any rotation theta,
-  // the three tangential velocities should have the same magnitude.
-  const double v_slipA = vt.segment<2>(0).norm();
-  const double v_slipB = vt.segment<2>(2).norm();
-  const double v_slipC = vt.segment<2>(4).norm();
-  EXPECT_NEAR(v_slipA, v_slipB, kTolerance);
-  EXPECT_NEAR(v_slipA, v_slipC, kTolerance);
-  EXPECT_NEAR(v_slipC, v_slipB, kTolerance);
-
-  // For this case where Mz > M_transition, we expect sliding, so that expected
-  // velocities are larger than the stiction tolerance.
-  EXPECT_GT(v_slipA, parameters.stiction_tolerance);
-  EXPECT_GT(v_slipB, parameters.stiction_tolerance);
-  EXPECT_GT(v_slipC, parameters.stiction_tolerance);
-
-  const VectorX<double>& v = solver_.get_generalized_velocities();
-  ASSERT_EQ(v.size(), nv_);
-
-  // For this problem we expect the translational velocities to be zero within
-  // machine epsilon.
-  EXPECT_NEAR(v(0), 0.0, kTolerance);
-  EXPECT_NEAR(v(1), 0.0, kTolerance);
-
-  const double omega = dt * (Mz - 5.0) / I_;
-  EXPECT_NEAR(v(2), omega, kTolerance);
-
-  // Slip velocities should only be due to rotation.
-  EXPECT_NEAR(v_slipA, R_ * omega, kTolerance);
-  EXPECT_NEAR(v_slipB, R_ * omega, kTolerance);
-  EXPECT_NEAR(v_slipC, R_ * omega, kTolerance);
+  EXPECT_TRUE(CompareMatrices(
+      vt_alpha, vt_alpha_expected, kTolerance, MatrixCompareType::relative));
 }
 
 }  // namespace
