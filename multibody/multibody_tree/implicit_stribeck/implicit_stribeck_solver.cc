dd30f51a947036792e62568038ce0c194b7dc5b0
amcastro-tri <alejandro.castro@tri.global>
2018-06-11 11:14:13 -0400

    Quickly adds limiter

--- multibody/multibody_tree/implicit_stribeck/implicit_stribeck_solver.cc
+++ multibody/multibody_tree/implicit_stribeck/implicit_stribeck_solver.cc
@@ -17,6 +17,242 @@ namespace drake {
 namespace multibody {
 namespace implicit_stribeck {
 
+namespace internal {
+template <typename T>
+T DirectionChangeLimiter<T>::CalcAlpha(
+    const Eigen::Ref<const Vector2<T>>& v,
+    const Eigen::Ref<const Vector2<T>>& dv,
+    double cos_theta_max, double v_stiction, double tolerance) {
+  DRAKE_ASSERT(v_stiction > 0);
+  DRAKE_ASSERT(tolerance > 0);
+  DRAKE_ASSERT(dv.size() == v.size());
+
+  using std::abs;
+  using std::max;
+  using std::min;
+  using std::sqrt;
+
+  // εᵥ is used to determine when a velocity is close to zero.
+  const double epsilon_v = v_stiction * tolerance;
+  const double epsilon_v2 = epsilon_v * epsilon_v;
+
+  const Vector2<T> v1 = v + dv;  // v_alpha = v + dv, when alpha = 1.
+
+  // Case I: Quick exit for small changes in v.
+  const T dv_norm2 = dv.squaredNorm();
+  if (dv_norm2 < epsilon_v2) {
+    return 1.0;
+  }
+
+  const T v_norm = v.norm();
+  const T v1_norm = v1.norm();
+  const T x = v_norm / v_stiction;    // Dimensionless slip v and v1.
+  const T x1 = v1_norm / v_stiction;
+  // From Case I, we know dv_norm > epsilon_v.
+  const T dv_norm = sqrt(dv_norm2);
+
+  // Case II: limit transition from stiction to sliding when x << 1.0 and
+  // gradients might be close to zero (due to the "soft norms").
+  if (x < tolerance && x1 > 1.0) {
+    // we know v1 != 0  since x1 > 1.0.
+    // With v_alpha = v + alpha * dv, we make |v_alpha| = v_stiction / 2.
+    // For this case dv ≈ v1 (v ≈ 0). Therefore:
+    // alpha ≈ v_stiction / dv_norm / 2.
+    return v_stiction / dv_norm / 2.0;
+  }
+
+  // Case III: Transition to an almost exact stiction from sliding.
+  // We want to avoid v1 landing in a region of zero gradients so that we force
+  // it to land within the circle of radius v_stiction, at v_stribeck/2 in the
+  // direction of v.
+  if (x > 1.0 && x1 < tolerance) {
+    // In this case x1 is negligible compared to x. That is dv ≈ -v. For this
+    // case we'll limit v + αdv = vₛ/2⋅v/‖v‖. Using v ≈ -dv, we arrive to
+    // dv(α-1) = -vₛ/2⋅dv/‖dv‖ or:
+    return 1.0 - v_stiction / 2.0 / dv_norm;
+  }
+
+  if (x < 1.0) {
+    // Another quick exit. Two possibilities (both of which yield the same
+    // action):
+    // x1 < 1: we go from within the Stribeck circle back into it. Since this
+    //         region has strong gradients, we allow it. i.e. alpha = 1.0
+    // x1 > 1: If we go from a region of strong gradients (x < 1) to sliding
+    //         (x1 > 1), we allow it. Notice that the case from weak gradients
+    //         (close to zero) when x < tolerance, was covered by Case II.
+    return 1.0;
+  } else {  // x > 1.0
+    if (x1 < 1.0) {
+      // Case IV:
+      // From Case III we know that x1 > tolerance, i.e x1 falls in a region of
+      // strong gradients and thus we allow it.
+      return 1.0;
+    }
+
+    // Case V:
+    // Since v_stiction is so small, the next case very seldom happens. However,
+    // it is a very common case for 1D-like problems for which tangential
+    // velocities change in sign and the zero crossing might be missed.
+    // Notice that since we reached this point, we know that:
+    //  - x > 1.0 (we are within the scope of an if statement for x > 1)
+    //  - x1 > 1.0 (we went through Case IV)
+    //  - dv_norm > epsilon_v (we went through Case I, i.e. non-zero)
+    // Here we are checking for the case when the line connecting v and v1
+    // intersects the Stribeck circle. For this case we compute alpha so that
+    // the update corresponds to the velocity closest to the origin.
+    T alpha;
+    const T v_dot_dv = v.dot(dv);
+    if (CrossesTheStictionRegion(
+        v, dv, v_dot_dv, dv_norm, dv_norm2, epsilon_v, v_stiction, &alpha)) {
+      return alpha;
+    }
+
+    // If we are here we know:
+    //  - x > 1.0
+    //  - x1 > 1.0
+    //  - dv_norm > epsilon_v
+    //  - line connecting v with v1 never goes through the Stribeck circle.
+    //
+    // Case VI:
+    // Therefore we know changes happen entirely outside the circle of radius
+    // v_stiction. To avoid large jumps in the direction of v (typically during
+    // strong impacts), we limit the maximum angle change between v to v1.
+    // To this end, we find a scalar 0 < alpha < 1 such that
+    // cos(θₘₐₓ) = v⋅(v+αdv)/(‖v‖‖v+αdv‖), see [Uchida et al., 2015].
+
+    // First we compute the angle change when alpha = 1, between v1 and v.
+    const T cos1 = v.dot(v1) / v_norm / v1_norm;
+
+    // We allow angle changes theta < theta_max, and we take alpha = 1.0.
+    // In particular, when v1 is exactly aligned with v (but we know it does not
+    // cross through zero, i.e. cos(theta) > 0).
+    if (cos1 > cos_theta_max) {
+      return 1.0;
+    } else {
+      // we limit the angle change to theta_max so that:
+      // cos(θₘₐₓ) = v⋅(v+αdv)/(‖v‖‖v+αdv‖)
+      // if we square both sides of this equation, we arrive at a quadratic
+      // equation with coefficients a, b, c, for α. The math below simply is the
+      // algebra to compute coefficients a, b, c and solve the quadratic
+      // equation.
+
+      // All terms are made non-dimensional using v_stiction as the reference
+      // scale.
+      const T x_dot_dx = v_dot_dv / (v_stiction * v_stiction);
+      const T dx = dv.norm() / v_stiction;
+      const T x2 = x * x;
+      const T dx4 = x2 * x2;
+      const T cos_theta_max2 = cos_theta_max * cos_theta_max;
+
+      // Form the terms of the quadratic equation aα² + bα + c = 0.
+      const T a = x2 * dx * dx * cos_theta_max2 - x_dot_dx * x_dot_dx;
+      const T b = 2 * x2 * x_dot_dx * (cos_theta_max2 - 1.0);
+      const T c = dx4 * (cos_theta_max2 - 1.0);
+
+      // Solve quadratic equation. We know, from the geometry of the problem,
+      // that the roots to this problem are real. Thus, the discriminant of the
+      // quadratic equation (Δ = b² - 4ac) must be positive.
+      // We use a very specialized quadratic solver for this case where we know
+      // there must exist a positive (i.e. real) root.
+      alpha = SolveQuadraticForTheSmallestPositiveRoot(a, b, c);
+
+      // The geometry of the problem tells us that α ≤ 1.0
+      DRAKE_ASSERT(alpha <= 1.0);
+      return alpha;
+    }
+  }
+
+  // We should never reach this point.
+  throw std::logic_error("Bug detected. An angle change case was missed.");
+}
+
+template <typename T>
+bool DirectionChangeLimiter<T>::CrossesTheStictionRegion(
+    const Eigen::Ref<const Vector2<T>>& v,
+    const Eigen::Ref<const Vector2<T>>& dv,
+    const T& v_dot_dv,  const T& dv_norm, const T& dv_norm2,
+    double epsilon_v, double v_stiction, T* alpha_out) {
+  DRAKE_ASSERT(dv_norm > 0);
+  DRAKE_ASSERT(dv_norm2 > 0);
+  DRAKE_ASSERT(epsilon_v > 0);
+  DRAKE_ASSERT(v_stiction > 0);
+  T& alpha = *alpha_out;
+  if (v_dot_dv < 0.0) {  // Moving towards the origin.
+    alpha = -v_dot_dv / dv_norm2;  // alpha > 0
+    if (alpha < 1.0) {  // The update might be crossing the stiction region.
+      const Vector2<T> v_alpha = v + alpha * dv;  // Note: v_alpha.dot(dv) = 0.
+      const T v_alpha_norm = v_alpha.norm();
+      if (v_alpha_norm < epsilon_v) {
+        // v_alpha is almost zero.
+        // This situation happens when dv ≈ -a v with a > 0. Therefore we cap
+        // v_alpha to v_alpha = v / ‖v‖⋅vₛ/2. To do this, we "move" v_alpha
+        // in the direction opposite of dv/‖dv‖ by magnitude vs/2, or similarly,
+        // we substract ‖dv‖vs/2 from the previously computed alpha.
+        alpha -= v_stiction / 2.0 / dv_norm;
+        DRAKE_ASSERT(0 < alpha && alpha <= 1);
+        return true;  // Crosses the stiction region.
+      } else if (v_alpha_norm < v_stiction) {
+        // v_alpha falls within the Stribeck circle but its magnitude is
+        // larger than epsilon_v.
+        return true;  // Crosses the stiction region.
+      }
+    }
+  }
+  return false;
+}
+
+template <typename T>
+T DirectionChangeLimiter<T>::SolveQuadraticForTheSmallestPositiveRoot(
+    const T& a, const T& b, const T& c) {
+  using std::abs;
+  using std::max;
+  using std::min;
+  using std::sqrt;
+  T alpha;
+  // First determine if a = 0 (to machine epsilon). This comparison is fair
+  // since a is dimensionless.
+  if (abs(a) < std::numeric_limits<double>::epsilon()) {
+    // There is only a single root to the, now linear, equation bα + c = 0.
+    alpha = -c / b;
+    // Note: a = 0, α > 0 => x_dot_dx = x * dx * cmin ≠ 0 => b ≠ 0
+    // We assert this even though within the scope this method is called it is
+    // not possible for b to be zero.
+    DRAKE_ASSERT(abs(b) > std::numeric_limits<double>::epsilon());
+  } else {
+    // The determinant, Δ = b² - 4ac, of the quadratic equation.
+    const T Delta = b * b - 4 * a * c;  // Uppercase, as in Δ.
+    // Geometry tell us that a real solution does exist i.e. Delta > 0.
+    DRAKE_ASSERT(Delta > 0);
+    const T sqrt_Delta = sqrt(Delta);
+
+    // To avoid loss of significance, when 4ac is relatively small compared
+    // to b² (i.e. the square root of the discriminant is close to b), we use
+    // Vieta's formula (α₁α₂ = c / a) to compute the second root given we
+    // computed the first root without precision lost. This guarantees the
+    // numerical stability of the method.
+    const T numerator = -0.5 * (b + (b > 0.0 ? sqrt_Delta : -sqrt_Delta));
+    const T alpha1 = numerator / a;
+    const T alpha2 = c / numerator;
+
+    // The geometry of the problem tells us that at least one must be
+    // positive.
+    DRAKE_ASSERT(alpha2 > 0 || alpha1 > 0);
+
+    if (alpha2 > 0 && alpha1 > 0) {
+      // This branch is triggered for large angle changes (typically close
+      // to 180 degrees) between v1 and vt.
+      alpha = min(alpha2, alpha1);
+    } else {
+      // This branch is triggered for small angles changes (typically
+      // smaller than 90 degrees) between v1 and vt.
+      alpha = max(alpha2, alpha1);
+    }
+  }
+  return alpha;
+}
+
+}  // namespace internal
+
 template <typename T>
 ImplicitStribeckSolver<T>::ImplicitStribeckSolver(int nv) :
     nv_(nv),
@@ -460,7 +696,21 @@ ComputationInfo ImplicitStribeckSolver<T>::SolveWithGuess(
     // We'll do so by computing a coefficient 0 < α < 1 so t_hat if the
     // generalized velocities are updated as vᵏ⁺¹ = vᵏ + α Δvᵏ then θ < θₘₐₓ
     // for all contact points.
-    T alpha = 1.0;  // We set α = 1 for now.
+    T alpha = 1.0;
+    const int nc = nc_;
+    const double cos_theta_max = std::cos(parameters_.theta_max);
+    double v_stribeck = parameters_.stiction_tolerance;
+    for (int ic = 0; ic < nc; ++ic) {
+      const int ik = 2 * ic;
+      auto vt_ic = vt.template segment<2>(ik);
+      const auto dvt_ic = Delta_vt.template segment<2>(ik);
+      alpha = min(
+          alpha,
+          internal::DirectionChangeLimiter<T>::CalcAlpha(
+              vt_ic, dvt_ic,
+              cos_theta_max, v_stribeck, parameters_.tolerance));
+    }
+
     v = v + alpha * Delta_v;
     vt = Jt * v;
 
--- multibody/multibody_tree/implicit_stribeck/implicit_stribeck_solver.h
+++ multibody/multibody_tree/implicit_stribeck/implicit_stribeck_solver.h
@@ -9,6 +9,156 @@
 namespace drake {
 namespace multibody {
 namespace implicit_stribeck {
+namespace internal {
+/// This struct implements an internal (thus within `internal::`) detail of the
+/// implicit Stribeck solver. The implicit Stribeck solver performs a
+/// Newton-Raphson iteration, and at each k-th iteration, it computes a
+/// tangential velocity update Δvₜᵏ. A standard Newton-Raphson strategy would
+/// compute the tangential velocity at the next iteration (k+1) as
+/// vₜᵏ⁺¹ = vₜᵏ + αΔvₜᵏ, where 0 < α < 1, is a coefficient typically obtained
+/// with a line search algorithm to improve the convergence rate. Line search
+/// works very well for smooth problems. However, even though the implicit
+/// Stribeck is solving the root of a continuous function, this function has
+/// very steep gradients only within the very small regions close to where the
+/// tangential velocities are zero. These regions are circles in ℝ² of radius
+/// equal to the stiction tolerance of the solver vₛ. We refer to
+/// these circular regions as the "stiction regions" and to their boundaries as
+/// the "stiction circles". We refer to the region of ℝ² outside the stiction
+/// region around the origin as the "sliding region".
+/// The implicit Stribeck solver uses the following modified Stribeck function
+/// describing the functional dependence of the Stribeck coefficient of friction
+/// μₛ with slip speed: <pre>
+///     μₛ(x) = ⌈ μ⋅x⋅(2.0 - x),  x  < 1
+///             ⌊ μ            ,  x >= 1
+/// </pre>
+/// where x corresponds to the dimensionless slip speed x = ‖vₜ‖ / vₛ and
+/// μ is the Coulomb's law coefficient of friction. The implicit Stribeck solver
+/// makes no distinction between static and dynamic coefficients of friction and
+/// therefore a single coefficient μ needs to be specified.
+/// Since the Stribeck function used for the friction coefficient has very
+/// steep gradients only within the stiction region and zero outside of it,
+/// gradients (that is the Jacobian of the residual) in the Newton-Raphson
+/// iteration performed by the implicit Stribeck solver are large within
+/// these small circular regions (stiction) and small outside them (slip).
+/// We say that gradients within these stiction circles are "strong" and
+/// gradients outside these regions are "weak".
+/// These regions are so small compared to the velocity scales dealt with
+/// by the implicit Stribeck solver, that effectively, the Newton-Raphson
+/// iteration would only "see" a fixed dynamic coefficient of friction and it
+/// would never be able to predict stiction. That is, if search direction Δvₜᵏ
+/// computed by the Newton-Raphson algorithm is not limited in some way, the
+/// iteration would never fall within the stiction regions where gradients
+/// are "strong" to guide the convergence of the solution, to either stiction
+/// or sliding.
+///
+/// The remedy to this situation is to limit changes in the tangential
+/// velocities at each iteration. The situation described above, in which an
+/// update  Δvₜᵏ "misses" the stiction circle can be described in purely
+/// geometric terms. We exploit this fact to devise a strategy that is
+/// appropriate for this particular problem. We use the methodology outlined in
+/// [Uchida et al., 2015] and describe particulars to our implementation below.
+///
+/// LimitDirectionChange implements a specific strategy with knowledge of the
+/// implicit Stribeck iteration procedure. It is important to note that the
+/// implicit Stribeck uses "soft norms" to avoid divisions by zero. That is,
+/// friction forces are computed according to: <pre>
+///   fₜ(vₜ) = -μ(‖vₜ‖ₛ) vₜ/‖vₜ‖ₛ
+/// </pre>
+/// where, to avoid the singularity at zero velocity, we use a "soft norm"
+/// ‖vₜ‖ₛ = sqrt(vₜᵀvₜ + εᵥ²), with εᵥ a small fraction of vₛ. Due to the
+/// use of soft norms, the gradient of fₜ with vₜ is now well defined,
+/// but it goes to zero as vₜ approaches the origin. Therefore, gradients
+/// are also "weak" in the neighborhood of ‖vₜ‖ₛ ≲ εᵥ.
+/// Due to this, external forcing (either from applied forces or from coupling
+/// with other friction forces) has the potential to, mistakenly, force a
+/// transition from stiction to sliding. The solver will most likely recover
+/// from this, but this will result in a larger number of iterations.
+/// LimitDirectionChange considers any tangential velocity vₜ (or change Δvₜ)
+/// to be approximately zero if x = ‖vₜ‖/vₛ is smaller than `tolerance`
+/// (see docs below, this is a dimensionless number << 1). We define
+/// `εᵥ = tolerance⋅vₛ` (with units of m/s).
+///
+/// In what follows we list a number of special scenarios dealt with by
+/// LimitDirectionChange. We use the observations made above.
+///  - LimitDirectionChange first deals with the case ‖vₜ‖ < εᵥ to avoid
+///    divisions by zero in the subsequent cases. It essentially clips vₜᵏ⁺¹
+///    to have magnitude vₛ/2 when the update Δvₜᵏ ≠ 0. For small updates
+///    Δvₜᵏ leading to vₜᵏ⁺¹ within the stiction region, we take  α = 1.
+///    See implementation notes for CalcAlpha() for further details.
+///  - Transition from ‖vₜ‖ < εᵥ (stiction) to ‖vₜ‖/vₛ > 1 (sliding). Since
+///    we are in a region of "weak" gradients (due to "norm softening",
+///    see discussion above), we limit the update to vₜᵏ⁺¹ = vₜᵏ/‖vₜᵏ‖⋅vₛ/2.
+///    In other words, if the speed would grow too fast, we cap it at vₛ/2
+///    so that at least two Newton iterations are required to go from near-0
+///    sticking to sliding.
+///  - Transition from sliding ‖vₜᵏ‖/vₛ > 1 to an almost perfect stiction with
+///    ‖vₜᵏ⁺¹‖ < εᵥ. In an attempt to avoid weak gradients for the next
+///    iteration, we impose the limit vₜᵏ⁺¹ = vₜᵏ/‖vₜᵏ‖⋅vₛ/2, placing the
+///    velocity "in the same direction where it came from", within the stiction
+///    region, but where gradients are strong.
+///  - Velocity change Δvₜᵏ intersects the stiction circle. To be more precise,
+///    the line connecting vₜᵏ and vₜᵏ + Δvₜᵏ crosses the stiction region.
+///    This situation implies that most likely a stiction transition could
+///    happen but the pure Newton-Raphson would miss it. This situation is
+///    outlined in [Uchida et al., 2015]. In this case LimitDirectionChange
+///    computes α so that vₜᵏ⁺¹ =  vₜᵏ + αΔvₜᵏ is the closest vector to the
+///    origin. This corresponds to the geometric condition
+///    dot(vₜᵏ⁺¹, Δvₜᵏ) = 0.
+///  - Velocity change Δvₜᵏ does not intersect the stiction circle, i.e.
+///    changes happen in a region away from stiction (within the sliding
+///    region). However, large angular changes (measured by the angle
+///    θ = acos(vₜᵏ⁺¹⋅vₜᵏ/(‖vₜᵏ⁺¹‖‖vₜᵏ‖)) between vₜᵏ⁺¹ and vₜᵏ)
+///    might indicate a solution that is attempting to reach a stiction region.
+///    In order to aid convergence, we limit the angle change to θₘₐₓ, and
+///    therefore (see [Uchida et al., 2015]) we compute α so that
+///    θₘₐₓ = acos(vₜᵏ⁺¹⋅vₜᵏ/(‖vₜᵏ⁺¹‖‖vₜᵏ‖)).
+///
+/// Uchida, T.K., Sherman, M.A. and Delp, S.L., 2015.
+///   Making a meaningful impact: modelling simultaneous frictional collisions
+///   in spatial multibody systems. Proc. R. Soc. A, 471(2177), p.20140859.
+///
+/// %LimitDirectionChange implements the algorithm described above. We place it
+/// inside a struct so that we can use Eigen::Ref arguments allowing different
+/// scalar types T.
+template <typename T>
+struct DirectionChangeLimiter {
+  /// Implements the limiting algorithm described in the documentation above.
+  /// @param[in] v the k-th iteration tangential velocity vₜᵏ, in m/s.
+  /// @param[in] dv the k-th iteration tangential velocity update Δvₜᵏ, in m/s.
+  /// @param[in] cos_theta_max precomputed value of cos(θₘₐₓ).
+  /// @param[in] v_stiction the stiction tolerance vₛ, in m/s.
+  /// @param[in] tolerance a value << 1 used to determine when ‖vₜ‖ ≈ 0.
+  /// Typical values lie withing the 10⁻³ - 10⁻² range. This allows us to
+  /// compute `εᵥ = tolerance⋅vₛ` (in m/s) which defines a "small tangential
+  /// velocity scale". This value is used to compute "soft norms" (see class's
+  /// documentation) and to detect values close to zero, ‖vₜ‖ < εᵥ.
+  /// @retval α the limit in [0, 1] so that vₜᵏ⁺¹ = vₜᵏ + αΔvₜᵏ.
+  static T CalcAlpha(const Eigen::Ref<const Vector2<T>>& v,
+                     const Eigen::Ref<const Vector2<T>>& dv,
+                     double cos_theta_max, double v_stiction, double tolerance);
+
+  /// Helper method for detecting when the line connecting v with v1 = v + dv
+  /// crosses the stiction region, a circle of radius `v_stiction`.
+  /// All other input arguments are quantities already precomputed by
+  /// CalcAlpha() and thus we reuse them.
+  /// @param alpha when this method returns `true` (zero crossing), a
+  /// coefficient in `(0, 1]` so that `v_alpha = v + alpha * dv` is the closest
+  /// vector to the origin. It is not set when the method returns `false`.
+  /// @returns `true` if the line connecting v with v1 = v + dv crosses the
+  /// stiction region.
+  static bool CrossesTheStictionRegion(
+      const Eigen::Ref<const Vector2<T>>& v,
+      const Eigen::Ref<const Vector2<T>>& dv,
+      const T& v_dot_dv, const T& dv_norm, const T& dv_norm2,
+      double epsilon_v, double v_stiction, T* alpha);
+
+  /// Helper method to solve the quadratic equation aα² + bα + c = 0 for the
+  /// very particular case we know we have real roots (Δ = b² - 4ac > 0) and we
+  /// are interested in the smallest positive root.
+  static T SolveQuadraticForTheSmallestPositiveRoot(
+      const T& a, const T& b, const T& c);
+};
+}  // namespace internal
 
 /// The result from ImplicitStribeckSolver::SolveWithGuess() used to report the
 /// success or failure of the solver.
@@ -57,6 +207,8 @@ struct Parameters {
   /// stiction_tolerance).
   /// Typical value is about 1%.
   double tolerance{1.0e-2};
+
+  double theta_max{M_PI / 3.0};
 };
 
 /// Struct used to store information about the iteration process performed by
