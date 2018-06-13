9dba4e5947f745784deb870ba03ea12f9216038e
Frank <frank.permenter@tri.global>
2018-06-13 07:30:48 -0400

    Added test cases

---
+++ solvers/test/integer_lattice_test.cc
@@ -0,0 +1,109 @@
+/* clang-format off to disable clang-format-includes */
+/* clang-format on */
+
+#include "drake/solvers/integer_lattice.h"
+
+#include <gtest/gtest.h>
+namespace drake {
+namespace solvers {
+namespace {
+
+struct VectorLexCompare {
+  bool operator() (Eigen::VectorXi v, Eigen::VectorXi w) const {
+    for (int i = 0; i < v.size(); ++i) {
+      if (v(i) < w(i)) return true;
+      if (v(i) > w(i)) return false;
+    }
+    return false;
+  }
+};
+
+typedef std::set<Eigen::VectorXi, VectorLexCompare>  IntegerSet;
+
+IntegerSet MatrixToSet(Eigen::MatrixXi M) {
+  IntegerSet M_set;
+  for (int i = 0; i < M.rows(); i++) {
+    M_set.insert(M.row(i));
+  }
+  return M_set;
+}
+
+
+
+class IntegerLatticeTest : public ::testing::Test {
+ public:
+  void CheckEnumeration(IntegerSet ref) {
+      auto y = EnumerateIntegerSolutions(A_, b_, lower_bound_, upper_bound_);
+      auto y_set = MatrixToSet(y);
+      EXPECT_EQ(y_set, ref);
+  }
+  void SetDimensions(int num_ineq, int num_var) {
+    int m = num_ineq; int n = num_var;
+    A_.resize(m, n);
+    b_.resize(m);
+    lower_bound_.resize(n);
+    upper_bound_.resize(n);
+  }
+ protected:
+  Eigen::MatrixXi A_;
+  Eigen::VectorXi b_;
+  Eigen::VectorXi lower_bound_;
+  Eigen::VectorXi upper_bound_;
+};
+
+
+TEST_F(IntegerLatticeTest, EqualComponents) {
+  SetDimensions(2, 2);
+  A_ << 1, -1,
+       -1, 1;
+
+  b_ << 0, 0;
+
+  lower_bound_ << 0, 0;
+  upper_bound_ << 2, 2;
+  IntegerSet ref;
+
+  ref.insert(Eigen::VectorXi::Constant(2, 0));
+  ref.insert(Eigen::VectorXi::Constant(2, 1));
+  ref.insert(Eigen::VectorXi::Constant(2, 2));
+  CheckEnumeration(ref);
+}
+
+TEST_F(IntegerLatticeTest, SumToConstant) {
+  SetDimensions(2, 2);
+  A_ << 1, 1,
+      -1, -1;
+  b_ << 2,
+       -2;
+
+  lower_bound_ << 0, 0;
+  upper_bound_ << 2, 2;
+  Eigen::MatrixXi ref_mat(3, 2);
+  ref_mat << 0, 2,
+             2, 0,
+             1, 1;
+
+  CheckEnumeration(MatrixToSet(ref_mat));
+}
+
+
+TEST_F(IntegerLatticeTest, Empty) {
+  SetDimensions(1, 2);
+  A_ << 1, 0;
+  b_ << 0;
+
+  lower_bound_ << 1, 0;
+  upper_bound_ << 2, 0;
+  IntegerSet ref;
+
+  CheckEnumeration(ref);
+}
+
+
+
+}  // namespace
+}  // namespace solvers
+}  // namespace drake
+
+
+
---
+++ solvers/test/sos_utils_test.cc
@@ -0,0 +1,95 @@
+/* clang-format off to disable clang-format-includes */
+#include "drake/solvers/mathematical_program.h"
+#include "drake/solvers/sos_utils.h"
+#include "drake/common/symbolic_monomial_util.h"
+/* clang-format on */
+
+#include <gtest/gtest.h>
+
+#include "drake/common/symbolic.h"
+
+namespace drake {
+namespace solvers {
+namespace {
+
+using drake::symbolic::Expression;
+using drake::symbolic::Variable;
+using drake::symbolic::Variables;
+using drake::symbolic::Monomial;
+
+typedef std::set<Monomial,
+                drake::symbolic::GradedReverseLexOrder<std::less<Variable>>>
+                MonomialSet;
+
+MonomialSet VectorToSet(MonomialVector x) {
+  MonomialSet x_set;
+  for (int i = 0; i < x.size(); i++) {
+    x_set.insert(x(i));
+  }
+  return x_set;
+}
+
+class SosUtilsTest : public ::testing::Test {
+ public:
+  void CheckMonomialBasis(symbolic::Polynomial poly, MonomialSet basis_ref) {
+    MonomialVector basis = ConstructMonomialBasis(poly);
+    MonomialSet basis_set = VectorToSet(basis);
+    EXPECT_EQ(basis_set, basis_ref);
+  }
+
+ protected:
+  void SetUp() override {
+    x_ = prog_.NewIndeterminates<3>();
+  }
+
+  MathematicalProgram prog_;
+  VectorIndeterminate<3> x_;
+};
+
+TEST_F(SosUtilsTest, MotzkinPoly) {
+  const symbolic::Polynomial poly{
+    pow(x_(0), 2)*pow(x_(1), 4)  +
+    pow(x_(0), 4)*pow(x_(1), 2)  +
+    pow(x_(0), 2)*pow(x_(1), 2)  +
+    + 1
+  };
+  MonomialSet basis_ref;
+  basis_ref.insert(Monomial());
+  basis_ref.insert(Monomial(pow(x_(0), 1)*pow(x_(1), 1)));
+  basis_ref.insert(Monomial(pow(x_(0), 2)*pow(x_(1), 1)));
+  basis_ref.insert(Monomial(pow(x_(0), 1)*pow(x_(1), 2)));
+  CheckMonomialBasis(poly, basis_ref);
+}
+
+TEST_F(SosUtilsTest, Univariate) {
+  const symbolic::Polynomial poly{
+    1 + pow(x_(0), 1) + pow(x_(0), 2) + pow(x_(0), 8)};
+  MonomialSet basis_ref;
+  basis_ref.insert(Monomial());
+  basis_ref.insert(Monomial(pow(x_(0), 1)));
+  basis_ref.insert(Monomial(pow(x_(0), 2)));
+  basis_ref.insert(Monomial(pow(x_(0), 3)));
+  basis_ref.insert(Monomial(pow(x_(0), 4)));
+  CheckMonomialBasis(poly, basis_ref);
+}
+
+
+TEST_F(SosUtilsTest, Empty) {
+  const symbolic::Polynomial poly{ pow(x_(0), 3) };
+  MonomialSet basis_ref;
+  CheckMonomialBasis(poly, basis_ref);
+}
+
+TEST_F(SosUtilsTest, Singleton) {
+  const symbolic::Polynomial poly{1 + pow(x_(0), 1) };
+  MonomialSet basis_ref;
+  basis_ref.insert(Monomial());
+  CheckMonomialBasis(poly, basis_ref);
+}
+
+}  // namespace
+}  // namespace solvers
+}  // namespace drake
+
+
+
