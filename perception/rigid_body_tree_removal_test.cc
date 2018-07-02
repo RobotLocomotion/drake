5037a24c7ea339645e3248ec6177f7243955d499
andreas-tenpas <andreas.tenpas@tri.global>
2018-07-02 19:41:48 -0400

    connect to schunk wsg hand

--- perception/rigid_body_tree_removal_test.cc
+++ perception/rigid_body_tree_removal_test.cc
@@ -1,15 +1,21 @@
 #include <gflags/gflags.h>
 
+#include "drake/lcmt_schunk_wsg_command.hpp"
 #include "drake/examples/kuka_iiwa_arm/iiwa_lcm.h"
 #include "drake/examples/kuka_iiwa_arm/iiwa_world/iiwa_wsg_diagram_factory.h"
 #include "drake/lcm/drake_lcm.h"
+#include "drake/manipulation/schunk_wsg/schunk_wsg_constants.h"
+#include "drake/manipulation/schunk_wsg/schunk_wsg_controller.h"
+#include "drake/manipulation/schunk_wsg/schunk_wsg_lcm.h"
 #include "drake/manipulation/util/world_sim_tree_builder.h"
 #include "drake/multibody/rigid_body_plant/drake_visualizer.h"
 #include "drake/multibody/rigid_body_plant/rigid_body_plant.h"
 #include "drake/perception/rigid_body_tree_removal.h"
 #include "drake/systems/analysis/simulator.h"
 #include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/lcm/lcm_subscriber_system.h"
 #include "drake/systems/lcm/lcm_publisher_system.h"
+#include "drake/systems/primitives/constant_vector_source.h"
 #include "drake/systems/sensors/image_to_lcm_image_array_t.h"
 #include "drake/systems/sensors/rgbd_camera.h"
 
@@ -18,9 +24,16 @@ namespace perception {
 namespace filtering {
 
 using examples::kuka_iiwa_arm::IiwaAndWsgPlantWithStateEstimator;
+using examples::kuka_iiwa_arm::kIiwaLcmStatusPeriod;
+using examples::kuka_iiwa_arm::IiwaCommandReceiver;
+using examples::kuka_iiwa_arm::IiwaStatusSender;
+using examples::kuka_iiwa_arm::IiwaContactResultsToExternalTorque;
+using manipulation::schunk_wsg::SchunkWsgController;
+using manipulation::schunk_wsg::SchunkWsgStatusSender;
 using manipulation::util::ModelInstanceInfo;
 using manipulation::util::WorldSimTreeBuilder;
 using systems::lcm::LcmPublisherSystem;
+using systems::lcm::LcmSubscriberSystem;
 using systems::RigidBodyPlant;
 using systems::sensors::ImageToLcmImageArrayT;
 using systems::sensors::RgbdCamera;
@@ -263,15 +276,118 @@ int do_main() {
 //  builder.Connect(camera_info_msg_source->get_output_port(0),
 //                  camera_info_pub->get_input_port());
 
+  // Create the command subscriber and status publisher.
+    auto iiwa_command_sub = builder.AddSystem(
+        LcmSubscriberSystem::Make<lcmt_iiwa_command>("IIWA_COMMAND",
+                                                                   &lcm));
+    iiwa_command_sub->set_name("iiwa_command_subscriber");
+    auto iiwa_command_receiver = builder.AddSystem<IiwaCommandReceiver>();
+    iiwa_command_receiver->set_name("iwwa_command_receiver");
+
+    auto iiwa_status_pub = builder.AddSystem(
+        LcmPublisherSystem::Make<lcmt_iiwa_status>("IIWA_STATUS",
+                                                                 &lcm));
+    iiwa_status_pub->set_name("iiwa_status_publisher");
+    iiwa_status_pub->set_publish_period(kIiwaLcmStatusPeriod);
+    auto iiwa_status_sender = builder.AddSystem<IiwaStatusSender>();
+    iiwa_status_sender->set_name("iiwa_status_sender");
+
+    std::vector<int> instance_ids = {iiwa_instance.instance_id};
+    auto external_torque_converter =
+        builder.AddSystem<IiwaContactResultsToExternalTorque>(
+            tree, instance_ids);
+
+    // TODO(siyuan): Connect this to kuka_planner runner once it generates
+    // reference acceleration.
+    auto iiwa_zero_acceleration_source =
+        builder.template AddSystem<systems::ConstantVectorSource<double>>(
+            Eigen::VectorXd::Zero(7));
+    iiwa_zero_acceleration_source->set_name("zero_acceleration");
+
+    builder.Connect(iiwa_command_sub->get_output_port(),
+                    iiwa_command_receiver->get_input_port(0));
+    builder.Connect(iiwa_command_receiver->get_commanded_state_output_port(),
+                    model->get_input_port_iiwa_state_command());
+    builder.Connect(iiwa_zero_acceleration_source->get_output_port(),
+                    model->get_input_port_iiwa_acceleration_command());
+
+    builder.Connect(model->get_output_port_iiwa_state(),
+                    iiwa_status_sender->get_state_input_port());
+    builder.Connect(iiwa_command_receiver->get_output_port(0),
+                    iiwa_status_sender->get_command_input_port());
+    builder.Connect(model->get_output_port_computed_torque(),
+                    iiwa_status_sender->get_commanded_torque_input_port());
+    builder.Connect(model->get_output_port_iiwa_measured_torque(),
+                    iiwa_status_sender->get_measured_torque_input_port());
+    builder.Connect(model->get_output_port_contact_results(),
+                    external_torque_converter->get_input_port(0));
+    builder.Connect(external_torque_converter->get_output_port(0),
+                    iiwa_status_sender->get_external_torque_input_port());
+    builder.Connect(iiwa_status_sender->get_output_port(0),
+                    iiwa_status_pub->get_input_port());
+
+    auto wsg_command_sub = builder.AddSystem(
+        LcmSubscriberSystem::Make<lcmt_schunk_wsg_command>(
+            "SCHUNK_WSG_COMMAND", &lcm));
+    wsg_command_sub->set_name("wsg_command_subscriber");
+    auto wsg_controller = builder.AddSystem<SchunkWsgController>();
+
+    auto wsg_status_pub = builder.AddSystem(
+        LcmPublisherSystem::Make<lcmt_schunk_wsg_status>(
+            "SCHUNK_WSG_STATUS", &lcm));
+    wsg_status_pub->set_name("wsg_status_publisher");
+    wsg_status_pub->set_publish_period(
+        manipulation::schunk_wsg::kSchunkWsgLcmStatusPeriod);
+
+    auto wsg_status_sender = builder.AddSystem<SchunkWsgStatusSender>(
+        model->get_output_port_wsg_state().size(),
+        model->get_output_port_wsg_measured_torque().size(),
+        manipulation::schunk_wsg::kSchunkWsgPositionIndex,
+        manipulation::schunk_wsg::kSchunkWsgVelocityIndex);
+    wsg_status_sender->set_name("wsg_status_sender");
+
+    builder.Connect(wsg_command_sub->get_output_port(),
+                    wsg_controller->get_command_input_port());
+    builder.Connect(wsg_controller->get_output_port(0),
+                    model->get_input_port_wsg_command());
+    builder.Connect(model->get_output_port_wsg_state(),
+                    wsg_status_sender->get_input_port_wsg_state());
+    builder.Connect(model->get_output_port_wsg_measured_torque(),
+                    wsg_status_sender->get_input_port_measured_torque());
+    builder.Connect(model->get_output_port_wsg_state(),
+                    wsg_controller->get_state_input_port());
+    builder.Connect(*wsg_status_sender, *wsg_status_pub);
+
+    auto iiwa_state_pub = builder.AddSystem(
+        systems::lcm::LcmPublisherSystem::Make<bot_core::robot_state_t>(
+            "EST_ROBOT_STATE", &lcm));
+    iiwa_state_pub->set_name("iiwa_state_publisher");
+    iiwa_state_pub->set_publish_period(kIiwaLcmStatusPeriod);
+
+    builder.Connect(model->get_output_port_iiwa_robot_state_msg(),
+                    iiwa_state_pub->get_input_port());
+    iiwa_state_pub->set_publish_period(kIiwaLcmStatusPeriod);
+
+    auto box_state_pub = builder.AddSystem(
+        systems::lcm::LcmPublisherSystem::Make<bot_core::robot_state_t>(
+            "OBJECT_STATE_EST", &lcm));
+    box_state_pub->set_name("box_state_publisher");
+    box_state_pub->set_publish_period(kIiwaLcmStatusPeriod);
+
+    builder.Connect(model->get_output_port_object_robot_state_msg(),
+                    box_state_pub->get_input_port());
+    box_state_pub->set_publish_period(kIiwaLcmStatusPeriod);
+
   auto sys = builder.Build();
   systems::Simulator<double> simulator(*sys);
 
   lcm.StartReceiveThread();
   simulator.Initialize();
   simulator.set_publish_every_time_step(false);
-//  double simulation_sec = 32.5;
-//  simulator.StepTo(simulation_sec);
+  double simulation_sec = 2.0;
+  simulator.StepTo(simulation_sec);
 
+  // TODO: fix segmentation fault
   const systems::AbstractValue& output = camera->depth_image_output_port().EvalAbstract(simulator.get_context());
   const auto& output_image = output.GetValue<systems::sensors::ImageDepth32F>();
   drake::log()->info(
