e5324b578adf455a2ff681ba27d4e56b9e34f02c
andreas-tenpas <andreas.tenpas@tri.global>
2018-07-12 10:02:57 -0400

    fix merge conflicts

---
+++ perception/rigid_body_tree_removal.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "drake/common/drake_copyable.h"
+#include "drake/multibody/rigid_body_tree.h"
+#include "drake/perception/point_cloud.h"
+#include "drake/systems/framework/context.h"
+#include "drake/systems/framework/leaf_system.h"
+
+namespace drake {
+namespace perception {
+
+/// A filtering algorithm for point clouds that removes known geometries.
+/// Given a RigidBodyTree object, the algorithm takes a point cloud and a vector
+/// of tree positions as input, and outputs a filtered point cloud from which
+/// all points
+/// expected to belong to the rigid bodies comprising the RigidBodyTree have
+/// been removed.
+class RigidBodyTreeRemoval final : public systems::LeafSystem<double> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(RigidBodyTreeRemoval)
+
+  /// Constructs the filter given a RigidBodyTree.
+  ///
+  /// @param[in] tree The RigidBodyTree containing the geometric configuration
+  /// of the world.
+  explicit RigidBodyTreeRemoval(std::unique_ptr<RigidBodyTree<double>> tree);
+
+  /// Returns the RigidBodyTree for which known geometries are calculated.
+  //  const RigidBodyTree<double>& tree() const { return tree_; }
+
+  /// Returns a descriptor of the input port containing the generalized
+  /// positions of
+  /// the RigidBodyTree.
+  const systems::InputPortDescriptor<double>&
+  get_rigid_body_tree_positions_input_port() const {
+    return this->get_input_port(input_port_index_tree_positions_);
+  }
+
+  /// Returns a descriptor of the input port containing the point cloud.
+  const systems::InputPortDescriptor<double>& get_point_cloud_input_port()
+      const {
+    return this->get_input_port(input_port_index_point_cloud_);
+  }
+
+ private:
+  PointCloud MakeOutputPointCloud() const;
+
+  void FilterPointCloud(const systems::Context<double>& context,
+                        PointCloud* output) const;
+
+  std::unique_ptr<RigidBodyTree<double>> tree_;
+
+  int input_port_index_point_cloud_{0};
+  int input_port_index_tree_positions_{1};
+
+  double collision_threshold_;
+};
+
+}  // namespace perception
+}  // namespace drake
---
+++ perception/rigid_body_tree_removal_test.cc
@@ -0,0 +1,302 @@
+#include <gflags/gflags.h>
+
+#include "drake/examples/kuka_iiwa_arm/iiwa_lcm.h"
+#include "drake/examples/kuka_iiwa_arm/iiwa_world/iiwa_wsg_diagram_factory.h"
+#include "drake/lcm/drake_lcm.h"
+#include "drake/manipulation/util/world_sim_tree_builder.h"
+#include "drake/multibody/rigid_body_plant/drake_visualizer.h"
+#include "drake/multibody/rigid_body_plant/rigid_body_plant.h"
+#include "drake/perception/rigid_body_tree_removal.h"
+#include "drake/systems/analysis/simulator.h"
+#include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/lcm/lcm_publisher_system.h"
+#include "drake/systems/sensors/image_to_lcm_image_array_t.h"
+#include "drake/systems/sensors/rgbd_camera.h"
+
+namespace drake {
+namespace perception {
+namespace filtering {
+
+using examples::kuka_iiwa_arm::IiwaAndWsgPlantWithStateEstimator;
+using manipulation::util::ModelInstanceInfo;
+using manipulation::util::WorldSimTreeBuilder;
+using systems::lcm::LcmPublisherSystem;
+using systems::RigidBodyPlant;
+using systems::sensors::ImageToLcmImageArrayT;
+using systems::sensors::RgbdCamera;
+using systems::sensors::RgbdCameraDiscrete;
+
+// Information for constructing a simulated physical camera in a scene.
+template <typename T>
+struct PhysicalCamera {
+  std::string name;
+  std::shared_ptr<RigidBodyFrame<T>> fixture_frame;
+  std::shared_ptr<RigidBodyFrame<T>> sensor_frame;
+  Eigen::Isometry3d extrinsics;
+};
+
+void AddCamera(WorldSimTreeBuilder<double>* tree_builder,
+               PhysicalCamera<double>* physical_camera,
+               const Eigen::Vector3d& base, const Eigen::Vector3d& rotation) {
+  const char* const kWsgFixtureUrdf =
+        "drake/manipulation/models/xtion_description/urdf/"
+        "xtion_wsg_fixture.urdf";
+  tree_builder->StoreDrakeModel("xtion_wsg_fixture", kWsgFixtureUrdf);
+
+  int fixture_id =
+      tree_builder->AddFixedModelInstance("xtion_wsg_fixture", base, rotation);
+  physical_camera->fixture_frame =
+      tree_builder->tree().findFrame("xtion_wsg_fixture", fixture_id);
+
+  drake::log()->info(" fixture_id: {}", fixture_id);
+  drake::log()->info(
+      "  fixture_frame:   {}",
+      physical_camera->fixture_frame->get_transform_to_body().matrix());
+
+  tree_builder->StoreDrakeModel(
+          physical_camera->name,
+          "drake/manipulation/models/xtion_description/urdf/xtion.urdf");
+  int xtion_id = tree_builder->AddModelInstanceToFrame(
+      "xtion", physical_camera->fixture_frame,
+      drake::multibody::joints::kFixed);
+  physical_camera->sensor_frame =
+      tree_builder->tree().findFrame("rgbd_camera_frame", xtion_id);
+
+  drake::log()->info(
+      "  fixture_frame:   {}",
+      physical_camera->fixture_frame->get_transform_to_body().matrix());
+}
+
+template <typename T>
+std::unique_ptr<RigidBodyPlant<T>> BuildCombinedPlant(
+    ModelInstanceInfo<T>* iiwa_instance, ModelInstanceInfo<T>* wsg_instance,
+    ModelInstanceInfo<T>* box_instance, PhysicalCamera<T>* physical_camera) {
+  auto tree_builder = std::make_unique<WorldSimTreeBuilder<double>>();
+
+  // Adds models to the simulation builder. Instances of these models can be
+  // subsequently added to the world.
+  const char* const kIiwaUrdf =
+      "drake/manipulation/models/iiwa_description/urdf/"
+      "iiwa14_polytope_collision.urdf";
+  tree_builder->StoreDrakeModel("iiwa", kIiwaUrdf);
+  tree_builder->StoreDrakeModel(
+      "table",
+      "drake/examples/kuka_iiwa_arm/models/table/"
+      "extra_heavy_duty_table_surface_only_collision.sdf");
+  tree_builder->StoreDrakeModel(
+      "box",
+      "drake/examples/kuka_iiwa_arm/models/objects/"
+      "block_for_pick_and_place.urdf");
+  tree_builder->StoreDrakeModel(
+      "wsg",
+      "drake/manipulation/models/wsg_50_description"
+      "/sdf/schunk_wsg_50_ball_contact.sdf");
+
+  // Build a world with two fixed tables.  A box is placed one on
+  // table, and the iiwa arm is fixed to the other.
+  tree_builder->AddFixedModelInstance("table",
+                                      Eigen::Vector3d::Zero() /* xyz */,
+                                      Eigen::Vector3d::Zero() /* rpy */);
+  tree_builder->AddFixedModelInstance("table",
+                                      Eigen::Vector3d(0.8, 0, 0) /* xyz */,
+                                      Eigen::Vector3d::Zero() /* rpy */);
+  tree_builder->AddFixedModelInstance("table",
+                                      Eigen::Vector3d(0, 0.85, 0) /* xyz */,
+                                      Eigen::Vector3d::Zero() /* rpy */);
+
+  tree_builder->AddGround();
+
+  // The `z` coordinate of the top of the table in the world frame.
+  // The quantity 0.736 is the `z` coordinate of the frame associated with the
+  // 'surface' collision element in the SDF. This element uses a box of height
+  // 0.057m thus giving the surface height (`z`) in world coordinates as
+  // 0.736 + 0.057 / 2.
+  const double kTableTopZInWorld = 0.736 + 0.057 / 2;
+
+  // Coordinates for kRobotBase originally from iiwa_world_demo.cc.
+  // The intention is to center the robot on the table.
+  const Eigen::Vector3d kRobotBase(0, 0, kTableTopZInWorld);
+  // Start the box slightly above the table.  If we place it at
+  // the table top exactly, it may start colliding the table (which is
+  // not good, as it will likely shoot off into space).
+  const Eigen::Vector3d kBoxBase(0.8, 0., kTableTopZInWorld + 0.1);
+
+  int id = tree_builder->AddFixedModelInstance("iiwa", kRobotBase);
+  *iiwa_instance = tree_builder->get_model_info_for_instance(id);
+  id = tree_builder->AddFloatingModelInstance("box", kBoxBase,
+                                              Vector3<double>(0, 0, 0));
+  *box_instance = tree_builder->get_model_info_for_instance(id);
+  id = tree_builder->AddModelInstanceToFrame(
+      "wsg", tree_builder->tree().findFrame("iiwa_frame_ee"),
+      drake::multibody::joints::kFixed);
+  *wsg_instance = tree_builder->get_model_info_for_instance(id);
+
+  // Add the camera.
+  const Eigen::Vector3d kCamBase(0, 0, 3.0);
+  const Eigen::Vector3d kCamRotation(-M_PI_2, 0, 0);
+  AddCamera(tree_builder.get(), physical_camera, kCamBase, kCamRotation);
+
+  auto plant = std::make_unique<RigidBodyPlant<T>>(tree_builder->Build());
+
+  return plant;
+}
+
+int do_main() {
+  std::unique_ptr<PhysicalCamera<double>> physical_camera;
+  physical_camera.reset(new PhysicalCamera<double>());
+  physical_camera->name = "xtion";
+
+  ModelInstanceInfo<double> iiwa_instance, wsg_instance, box_instance;
+  std::unique_ptr<systems::RigidBodyPlant<double>> model_ptr =
+      BuildCombinedPlant<double>(&iiwa_instance, &wsg_instance, &box_instance, physical_camera.get());
+  model_ptr->set_name("plant");
+
+  systems::DiagramBuilder<double> builder;
+  auto model =
+        builder.template AddSystem<IiwaAndWsgPlantWithStateEstimator<double>>(
+            std::move(model_ptr), iiwa_instance, wsg_instance, box_instance);
+  model->set_name("plant_with_state_estimator");
+
+  const RigidBodyTree<double>& tree = model->get_plant().get_rigid_body_tree();
+
+  // Add visualizer.
+  drake::lcm::DrakeLcm lcm;
+  systems::DrakeVisualizer* visualizer = builder.AddSystem<systems::DrakeVisualizer>(tree, &lcm);
+  visualizer->set_name("visualizer");
+  builder.Connect(model->get_output_port_plant_state(),
+                  visualizer->get_input_port(0));
+  visualizer->set_publish_period(examples::kuka_iiwa_arm::kIiwaLcmStatusPeriod);
+
+  const double depth_range_near = 0.2;
+  const double depth_range_far = 1.5;
+  const double fov_y = M_PI_4;
+  const double period = 1. / 300;  // Should this be 60FPS?
+  const double render_label_image = false;
+
+  auto* camera = builder.AddSystem<RgbdCameraDiscrete>(
+      std::make_unique<RgbdCamera>(physical_camera->name, model->get_tree(),
+                                   *physical_camera->sensor_frame,
+                                   depth_range_near, depth_range_far, fov_y),
+      period, render_label_image);
+  camera->set_name(physical_camera->name + "_rgbd_camera");
+
+  builder.Connect(model->get_output_port_plant_state(),
+                  camera->state_input_port());
+
+  const std::string camera_name = "simulated_xtion";
+  const std::string lcm_image_channel =
+      "DRAKE_RGBD_CAMERA_IMAGES_" + camera_name;
+
+//  // Image to LCM.
+//  auto* image_to_lcm_message =
+//      builder.AddSystem<ImageToLcmImageArrayT>("color", "depth", "label");
+//  image_to_lcm_message->set_name(physical_camera->name + "_lcm_converter");
+//
+//  builder.Connect(camera->color_image_output_port(),
+//                  image_to_lcm_message->color_image_input_port());
+//
+//  builder.Connect(camera->depth_image_output_port(),
+//                  image_to_lcm_message->depth_image_input_port());
+//
+//  if (render_label_image) {
+//    builder.Connect(camera->label_image_output_port(),
+//                    image_to_lcm_message->label_image_input_port());
+//  }
+//
+//  // Camera image publisher.
+//  auto* image_lcm_pub = builder.AddSystem(
+//      LcmPublisherSystem::Make<robotlocomotion::image_array_t>(
+//          lcm_image_channel, &lcm));
+//  image_lcm_pub->set_name(physical_camera->name + "_lcm_publisher");
+//  image_lcm_pub->set_publish_period(period);
+//
+//  builder.Connect(image_to_lcm_message->image_array_t_msg_output_port(),
+//                  image_lcm_pub->get_input_port());
+//
+//  // Make the camera description message.
+//  anzu::camera_description_t desc{};
+//  desc.camera_name = camera_name;
+//  desc.lcm_channel_name = lcm_image_channel;
+//  desc.num_image_types = 2;
+//
+//  auto& rgb_info = camera->camera().color_camera_info();
+//  auto& depth_info = camera->camera().depth_camera_info();
+//
+//  std::vector<rgbd_bridge::ImageType> types = {
+//      rgbd_bridge::ImageType::RGB, rgbd_bridge::ImageType::DEPTH};
+//  std::map<rgbd_bridge::ImageType, rgbd_bridge::Intrinsics> intrinsics;
+//  // Color
+//  intrinsics[rgbd_bridge::ImageType::RGB] = rgbd_bridge::Intrinsics(
+//      rgb_info.width(), rgb_info.height(), rgb_info.focal_x(),
+//      rgb_info.focal_y(), rgb_info.center_x(), rgb_info.center_y());
+//  // Depth
+//  intrinsics[rgbd_bridge::ImageType::DEPTH] = rgbd_bridge::Intrinsics(
+//      depth_info.width(), depth_info.height(), depth_info.focal_x(),
+//      depth_info.focal_y(), depth_info.center_x(), depth_info.center_y());
+//
+//  for (rgbd_bridge::ImageType type : types) {
+//    anzu::image_description_t image_desc{};
+//    image_desc.frame_name = rgbd_bridge::ImageTypeToFrameName(type);
+//    image_desc.type = rgbd_bridge::ImageTypeToDescriptionType(type);
+//
+//    image_desc.intrinsics =
+//        rgbd_bridge::SerializeIntrinsics(intrinsics.at(type));
+//
+//    for (rgbd_bridge::ImageType to_type : types) {
+//      image_desc.extrinsics.push_back(rgbd_bridge::SerializeExtrinsics(
+//          type, to_type, Eigen::Isometry3f::Identity()));
+//    }
+//    image_desc.num_extrinsics = image_desc.extrinsics.size();
+//    desc.image_types.push_back(image_desc);
+//  }
+//
+//  // make a publisher
+//  auto camera_info_pub = builder.AddSystem(
+//      drake::systems::lcm::LcmPublisherSystem::Make<
+//          anzu::camera_description_t>("DRAKE_RGBD_CAMERAS", &lcm));
+//  camera_info_pub->set_publish_period(period);
+//
+//  auto camera_info_msg_source = builder.template AddSystem<
+//      drake::systems::ConstantValueSource<double>>(
+//      drake::systems::AbstractValue::Make<anzu::camera_description_t>(
+//          desc));
+//  builder.Connect(camera_info_msg_source->get_output_port(0),
+//                  camera_info_pub->get_input_port());
+
+  auto sys = builder.Build();
+  systems::Simulator<double> simulator(*sys);
+
+  lcm.StartReceiveThread();
+  simulator.Initialize();
+  simulator.set_publish_every_time_step(false);
+//  double simulation_sec = 32.5;
+//  simulator.StepTo(simulation_sec);
+
+  const systems::AbstractValue& output = camera->depth_image_output_port().EvalAbstract(simulator.get_context());
+  const auto& output_image = output.GetValue<systems::sensors::ImageDepth32F>();
+  drake::log()->info(
+        "  depth image:   {}",
+        output_image.width());
+
+//  const auto& depth_image = camera->depth_image_output_port<double>().Eval(simulator.get_context());
+//  auto depth_image = camera->ConvertDepthImageToPointCloud(const ImageDepth32F& depth_image,
+//                                              const CameraInfo& camera_info,
+//                                              Eigen::Matrix3Xf* point_cloud)
+
+  return 0;
+}
+
+}  // namespace filtering
+}  // namespace perception
+}  // namespace drake
+
+
+int main(int argc, char* argv[]) {
+//  gflags::SetUsageMessage(
+//      "A simple acrobot demo using Drake's MultibodyTree,"
+//      "with SceneGraph visualization. "
+//      "Launch drake-visualizer before running this example.");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+//  drake::logging::HandleSpdlogGflags();
+  return drake::perception::filtering::do_main();
+}
