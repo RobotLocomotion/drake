9c2b1cee65b5e5c4aa1f17eab2682ead14b79468
amcastro-tri <alejandro.castro@tri.global>
2018-07-16 13:26:07 -0300

    More tests for spring force

--- multibody/multibody_tree/test/spring_damper_test.cc
+++ multibody/multibody_tree/test/spring_damper_test.cc
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 
 #include "drake/common/eigen_types.h"
+#include "drake/common/test_utilities/eigen_matrix_compare.h"
 #include "drake/multibody/multibody_tree/multibody_tree.h"
 #include "drake/multibody/multibody_tree/rigid_body.h"
 #include "drake/multibody/multibody_tree/position_kinematics_cache.h"
@@ -22,6 +23,8 @@ using systems::Context;
 namespace multibody {
 namespace multibody_tree {
 namespace {
+
+constexpr double kTolerance = 10 * std::numeric_limits<double>::epsilon();
 
 class SpringDamperTester : public ::testing::Test {
  public:
@@ -51,6 +54,7 @@ class SpringDamperTester : public ::testing::Test {
         model_.get_topology());
     vc_ = std::make_unique<VelocityKinematicsCache<double>>(
         model_.get_topology());
+    forces_ = std::make_unique<MultibodyForces<double>>(model_);
   }
 
   void SetSliderPosition(double position) {
@@ -58,6 +62,19 @@ class SpringDamperTester : public ::testing::Test {
     // Update the kinematics cache.
     model_.CalcPositionKinematicsCache(*context_, pc_.get());
     model_.CalcVelocityKinematicsCache(*context_, *pc_, vc_.get());
+  }
+
+  void CalcSpringDamperForces() const {
+    spring_damper_->CalcAndAddForceContribution(
+        *mbt_context_, *pc_, *vc_, forces_.get());
+  }
+
+  const SpatialForce<double>& GetSpatialForceOnBodyA() const {
+    return forces_->body_forces().at(bodyA_->node_index());
+  }
+
+  const SpatialForce<double>& GetSpatialForceOnBodyB() const {
+    return forces_->body_forces().at(bodyB_->node_index());
   }
 
  protected:
@@ -70,17 +87,16 @@ class SpringDamperTester : public ::testing::Test {
   const RigidBody<double>* bodyB_{nullptr};
   const PrismaticJoint<double>* slider_{nullptr};
   const SpringDamper<double>* spring_damper_{nullptr};
+  std::unique_ptr<MultibodyForces<double>> forces_;
   
   // Parameters of the case.
-  const Vector3<double> p_AP_{1, 0, 0};
-  const Vector3<double> p_BQ_{0, 0, 0};
-
   const double rest_length_ = 1.0;  // [m]
-  const double stiffness_ = 2.5;    // [N/m]
+  const double stiffness_ = 2.0;    // [N/m]
   const double damping_ = 0.5;      // [Ns/m]
+  const double torque_arm_length_{1.0};
+  const Vector3<double> p_AP_{0, torque_arm_length_, 0};
+  const Vector3<double> p_BQ_{0, torque_arm_length_, 0};
 };
-
-//constexpr double kEpsilon = std::numeric_limits<double>::epsilon();
 
 TEST_F(SpringDamperTester, ConstructionAndAccessors) {
   EXPECT_EQ(spring_damper_->bodyA().index(), bodyA_->index());
@@ -92,18 +108,60 @@ TEST_F(SpringDamperTester, ConstructionAndAccessors) {
   EXPECT_EQ(spring_damper_->point_on_bodyB(), p_BQ_);
 }
 
-// Test rigid body constructor.
-TEST_F(SpringDamperTester, LengthLongerThanResingLength) {
-  SetSliderPosition(3.0);
+// Verify the spring applies no forces when the spearation length equals the
+// rest length.
+TEST_F(SpringDamperTester, RestLength) {
+  SetSliderPosition(1.0);
+  CalcSpringDamperForces();
+  const SpatialForce<double>& F_A_W = GetSpatialForceOnBodyA();
+  const SpatialForce<double>& F_B_W = GetSpatialForceOnBodyB();
+  EXPECT_EQ(F_A_W.get_coeffs(), SpatialForce<double>::Zero().get_coeffs());
+  EXPECT_EQ(F_B_W.get_coeffs(), SpatialForce<double>::Zero().get_coeffs());
+}
 
-  MultibodyForces<double> forces(model_);
-  spring_damper_->CalcAndAddForceContribution(
-      *mbt_context_, *pc_, *vc_, &forces);
+// Verify forces computation when the spring length is larger than its rest
+// length.
+TEST_F(SpringDamperTester, LengthLargerThanRestLength) {
+  const double length = 2.0;
+  SetSliderPosition(length);
+  CalcSpringDamperForces();
+  const SpatialForce<double>& F_A_W = GetSpatialForceOnBodyA();
+  const SpatialForce<double>& F_B_W = GetSpatialForceOnBodyB();
+  const double expected_force_magnitude = stiffness_ * (length - rest_length_);
+  const double expected_torque_magnitude =
+      expected_force_magnitude * torque_arm_length_;
+  const SpatialForce<double> F_A_W_expected(
+      Vector3<double>(0, 0, -expected_torque_magnitude),
+      Vector3<double>(expected_force_magnitude, 0, 0));
+  EXPECT_TRUE(CompareMatrices(
+      F_A_W.get_coeffs(), F_A_W_expected.get_coeffs(),
+      kTolerance, MatrixCompareType::relative));
+  EXPECT_TRUE(CompareMatrices(
+      F_B_W.get_coeffs(), -F_A_W_expected.get_coeffs(),
+      kTolerance, MatrixCompareType::relative));
+}
 
-  const SpatialForce<double>& F_A_W =
-      forces.body_forces().at(bodyA_->node_index());
-  const SpatialForce<double>& F_B_W =
-      forces.body_forces().at(bodyB_->node_index());
+// Verify forces computation when the spring length is smaller than its rest
+// length.
+TEST_F(SpringDamperTester, LengthSmallerThanRestLength) {
+  const double length = 0.5;
+  SetSliderPosition(length);
+  CalcSpringDamperForces();
+  const SpatialForce<double>& F_A_W = GetSpatialForceOnBodyA();
+  const SpatialForce<double>& F_B_W = GetSpatialForceOnBodyB();
+  const double expected_force_magnitude = stiffness_ * (length - rest_length_);
+  const double expected_torque_magnitude =
+      expected_force_magnitude * torque_arm_length_;
+  const SpatialForce<double> F_A_W_expected(
+      Vector3<double>(0, 0, -expected_torque_magnitude),
+      Vector3<double>(expected_force_magnitude, 0, 0));
+  EXPECT_TRUE(CompareMatrices(
+      F_A_W.get_coeffs(), F_A_W_expected.get_coeffs(),
+      kTolerance, MatrixCompareType::relative));
+  EXPECT_TRUE(CompareMatrices(
+      F_B_W.get_coeffs(), -F_A_W_expected.get_coeffs(),
+      kTolerance, MatrixCompareType::relative));
+
 
   PRINT_VAR(F_A_W);
   PRINT_VAR(F_B_W);
