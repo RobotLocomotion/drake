12c0e1329b01ce8a0759a3ed0a76037b0d32c97c
andreas-tenpas <andreas.tenpas@tri.global>
2018-07-17 15:26:28 -0400

    test for collidingPoints()

--- perception/rigid_body_tree_removal.cc
+++ perception/rigid_body_tree_removal.cc
@@ -3,8 +3,9 @@
 namespace drake {
 namespace perception {
 
-RigidBodyTreeRemoval::RigidBodyTreeRemoval(const RigidBodyTree<double>& tree)
-    : tree_(tree) {
+RigidBodyTreeRemoval::RigidBodyTreeRemoval(const RigidBodyTree<double>& tree,
+                                           double collision_threshold)
+    : tree_(tree), collision_threshold_(collision_threshold) {
   /// input port for point cloud
   point_cloud_input_port_index_ = this->DeclareAbstractInputPort().get_index();
 
@@ -39,6 +40,9 @@ void RigidBodyTreeRemoval::FilterPointCloud(
     points[i] = input_cloud.xyz(i).cast<double>();
   }
 
+  points.resize(1);
+  points[0] = Eigen::Vector3d(0.,0.,0.);
+
   // 2. Extract the indices of the points in collision.
   Eigen::VectorXd q =
       this->EvalEigenVectorInput(context, state_input_port_index_)
@@ -48,10 +52,27 @@ void RigidBodyTreeRemoval::FilterPointCloud(
       const_cast<RigidBodyTree<double>&>(tree_).collidingPoints(
           kinematics_cache, points, collision_threshold_);
 
+  log()->info("points: {}, filtered_point_indices: {}", points.size(),
+              filtered_point_indices.size());
+
+  // for(size_t i=0; i < points.size(); i++)
+  //  log()->info("{}", points[i].transpose());
+
   // 3. Create a new point cloud without the colliding points.
-  output->resize(filtered_point_indices.size());
-  for (size_t i = 0; i < filtered_point_indices.size(); i++) {
-    output->mutable_xyz(i) = input_cloud.xyz(filtered_point_indices[i]);
+  output->resize(points.size() - filtered_point_indices.size());
+  int k = 0;
+  for (size_t i = 0; i < points.size(); i++) {
+    bool keep = true;
+    for (size_t j = 0; j < filtered_point_indices.size(); j++) {
+      if (i == filtered_point_indices[j]) {
+        keep = false;
+        break;
+      }
+    }
+    if (keep) {
+      output->mutable_xyz(k) = points[i].cast<float>();
+      k++;
+    }
   }
 }
 
