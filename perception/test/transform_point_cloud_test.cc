39dbed074dd8ece0033326963d252ce147054e76
andreas-tenpas <andreas.tenpas@tri.global>
2018-07-25 20:37:13 -0400

    add new test case

--- perception/test/transform_point_cloud_test.cc
+++ perception/test/transform_point_cloud_test.cc
@@ -20,6 +20,11 @@ const Vector3<double> kFrameToBodyP(-0.3, 5.4, -2.7);
 const Vector3<double> kWorldToBodyRpy(M_PI_2, -0.236, M_PI_4);
 const Vector3<double> kWorldToBodyP(-1.4, 0.3, 2.8);
 
+const int kWorldFrameIndex = 0;
+const Vector3<float> kMin(-10.0, -20.0, -30.0);
+const Vector3<float> kMax(10.0, 20.0, 30.0);
+const int kNumPoints = 5;
+
 Matrix3X<float> GenerateBoundedSample(const Vector3<float>& min,
                                       const Vector3<float>& max, int num_cols) {
   Matrix3X<float> return_matrix = Matrix3X<float>::Zero(3, num_cols);
@@ -32,21 +37,24 @@ Matrix3X<float> GenerateBoundedSample(const Vector3<float>& min,
   return return_matrix;
 }
 
-// Verifies that the system applies the transform correctly to the point cloud.
-GTEST_TEST(TransformPointCloudTest, TransformToWorldFrame) {
-  const int kWorldFrameIndex = 0;
-  const Vector3<float> kMin(-10.0, -20.0, -30.0);
-  const Vector3<float> kMax(10.0, 20.0, 30.0);
-  const int kNumPoints = 5;
+Matrix4X<float> CalcExpectedOutput(const Isometry3<double>& isom,
+    const MatrixX<float>& data) {
+  // The rigid transform below uses `float` because the point cloud uses
+  // `float` as the numerical representation.
+  math::RigidTransform<double> transform_d(isom);
+  math::RigidTransform<float> transform(transform_d.cast<float>());
+  Matrix4X<float> data_homogeneous(4, data.cols());
+  data_homogeneous.block(0, 0, 3, data.cols()) = data;
+  data_homogeneous.row(3) = VectorX<float>::Ones(kNumPoints);
+  Matrix4X<float> expected_output = transform.GetAsMatrix4() * data_homogeneous;
+  return expected_output;
+}
 
-  std::unique_ptr<RigidBodyTree<double>> tree =
-      std::make_unique<RigidBodyTree<double>>();
-
-  std::unique_ptr<RigidBody<double>> body =
-      std::make_unique<RigidBody<double>>();
-  body->set_name("body");
-  body->set_mass(1.0);
-  body->set_spatial_inertia(Matrix6<double>::Identity());
+RigidBody<double> AddBodyToTree(RigidBodyTree<double>* tree) {
+  RigidBody<double> body;
+  body.set_name("body");
+  body.set_mass(1.0);
+  body.set_spatial_inertia(Matrix6<double>::Identity());
 
   Isometry3<double> body_transform;
   body_transform =
@@ -54,15 +62,24 @@ GTEST_TEST(TransformPointCloudTest, TransformToWorldFrame) {
       (Eigen::AngleAxisd(kWorldToBodyRpy(0), Eigen::Vector3d::UnitX()) *
        Eigen::AngleAxisd(kWorldToBodyRpy(1), Eigen::Vector3d::UnitY()) *
        Eigen::AngleAxisd(kWorldToBodyRpy(2), Eigen::Vector3d::UnitZ()));
-  body->add_joint(&tree->world(), std::make_unique<RollPitchYawFloatingJoint>(
+  body.add_joint(&tree->world(), std::make_unique<RollPitchYawFloatingJoint>(
                                       "base", body_transform));
 
+  return body;
+}
+
+// Verifies that the system applies the transform correctly to the point cloud.
+GTEST_TEST(TransformPointCloudTest, ApplyTransform) {
+  std::unique_ptr<RigidBodyTree<double>> tree =
+      std::make_unique<RigidBodyTree<double>>();
+
+  std::unique_ptr<RigidBody<double>> body =
+      std::make_unique<RigidBody<double>>(AddBodyToTree(tree.get()));
   RigidBody<double>* b = tree->add_rigid_body(std::move(body));
 
   std::shared_ptr<RigidBodyFrame<double>> frame =
       std::make_shared<RigidBodyFrame<double>>("frame", b, kFrameToBodyP,
                                                kFrameToBodyRpy);
-  // RigidBodyFrame<double> frame("frame", b, kFrameToBodyP, kFrameToBodyRpy);
   tree->addFrame(frame);
 
   tree->compile();
@@ -86,7 +103,7 @@ GTEST_TEST(TransformPointCloudTest, TransformToWorldFrame) {
   context->FixInputPort(1, state);
 
   transformer->point_cloud_output_port().Calc(*context, output.get());
-  auto output_cloud = output->GetValueOrThrow<PointCloud>();
+  PointCloud output_cloud = output->GetValueOrThrow<PointCloud>();
 
   // The rigid transform below uses `float` because the point cloud uses
   // `float` as the numerical representation.
@@ -94,19 +111,69 @@ GTEST_TEST(TransformPointCloudTest, TransformToWorldFrame) {
       tree->doKinematics(state.head(tree->get_num_positions()));
   const Isometry3<double> isom =
       tree->relativeTransform(cache, 0, frame->get_frame_index());
-  math::RigidTransform<double> transform_d(isom);
-  math::RigidTransform<float> transform(transform_d.cast<float>());
-  Matrix4X<float> test_data_homogeneous(4, kNumPoints);
-  test_data_homogeneous.block(0, 0, 3, kNumPoints) = test_data;
-  test_data_homogeneous.row(3) = VectorX<float>::Ones(kNumPoints);
-  Matrix4X<float> expected_output =
-      transform.GetAsMatrix4() * test_data_homogeneous;
+
+  Matrix4X<float> expected_output = CalcExpectedOutput(isom, test_data);
 
   // The tolerance used here has this value because the point cloud uses
   // `float` as the numerical representation.
   EXPECT_TRUE(CompareMatrices(output_cloud.xyzs(),
                               expected_output.block(0, 0, 3, kNumPoints),
-                              16.0f * std::numeric_limits<float>::epsilon()));
+                              10.0f * std::numeric_limits<float>::epsilon()));
+}
+
+// Verifies that the system transforms a point cloud correctly to the world
+// frame. The test differs from the previous test in which constructor is
+// called for TransformPointCloud.
+GTEST_TEST(TransformPointCloudTest, TransformToWorldFrame) {
+  std::unique_ptr<RigidBodyTree<double>> tree =
+      std::make_unique<RigidBodyTree<double>>();
+
+  std::unique_ptr<RigidBody<double>> body =
+      std::make_unique<RigidBody<double>>(AddBodyToTree(tree.get()));
+  RigidBody<double>* b = tree->add_rigid_body(std::move(body));
+
+  std::shared_ptr<RigidBodyFrame<double>> frame =
+      std::make_shared<RigidBodyFrame<double>>("frame", b, kFrameToBodyP,
+                                               kFrameToBodyRpy);
+  tree->addFrame(frame);
+
+  tree->compile();
+
+  std::unique_ptr<TransformPointCloud> transformer =
+      std::make_unique<TransformPointCloud>(*tree.get(),
+          frame->get_frame_index());
+  std::unique_ptr<systems::Context<double>> context =
+      transformer->CreateDefaultContext();
+  std::unique_ptr<systems::AbstractValue> output =
+      transformer->point_cloud_output_port().Allocate();
+
+  VectorX<double> state(transformer->state_input_port().size());
+  state << 0.3, -0.4, 2.3, 0, 0, 0, 1;
+
+  MatrixX<float> test_data = GenerateBoundedSample(kMin, kMax, kNumPoints);
+  PointCloud cloud(kNumPoints);
+  cloud.mutable_xyzs() = test_data;
+
+  context->FixInputPort(0, systems::AbstractValue::Make<PointCloud>(cloud));
+  context->FixInputPort(1, state);
+
+  transformer->point_cloud_output_port().Calc(*context, output.get());
+  PointCloud output_cloud = output->GetValueOrThrow<PointCloud>();
+
+  // The rigid transform below uses `float` because the point cloud uses
+  // `float` as the numerical representation.
+  const KinematicsCache<double> cache =
+      tree->doKinematics(state.head(tree->get_num_positions()));
+  const Isometry3<double> isom =
+      tree->relativeTransform(cache, 0, frame->get_frame_index());
+
+  Matrix4X<float> expected_output = CalcExpectedOutput(isom, test_data);
+
+  // The tolerance used here has this value because the point cloud uses
+  // `float` as the numerical representation.
+  EXPECT_TRUE(CompareMatrices(output_cloud.xyzs(),
+                              expected_output.block(0, 0, 3, kNumPoints),
+                              10.0f * std::numeric_limits<float>::epsilon()));
 }
 
 }  // namespace
