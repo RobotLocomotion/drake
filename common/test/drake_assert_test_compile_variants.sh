51a4ee89c36cde25fb74d98263125a5dee2b0c61
Jamie Snape <jamie.snape@kitware.com>
2018-07-27 14:16:21 -0400

    Move C++14 flags from CROSSTOOL to bazel.rc

--- common/test/drake_assert_test_compile_variants.sh
+++ common/test/drake_assert_test_compile_variants.sh
@@ -19,7 +19,7 @@ if [[ ! -e ./drake ]]; then
 fi
 
 # Confirm that it compiles successfully, whether or not assertions are enabled.
-TESTING_CXXFLAGS="-std=c++1y -I . \
+TESTING_CXXFLAGS="-std=c++14 -I . \
     -c $drake_assert_test_compile_cc \
     -o /dev/null"
 "$BAZEL_CC" $TESTING_CXXFLAGS
