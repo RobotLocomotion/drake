11262e0321106ef7649dfe7eb426cbaef38c1223
Eric Cousineau <eric.cousineau@tri.global>
2018-08-03 11:34:38 -0400

    parsers: Permit using `tree.findFrame("world")`, which contains an actual body

--- multibody/parsers/parser_common.cc
+++ multibody/parsers/parser_common.cc
@@ -52,10 +52,14 @@ int AddFloatingJoint(
     // the robot at the desired location in the world.
     if (weld_to_frame->get_name() ==
         string(RigidBodyTreeConstants::kWorldName)) {
-      if (!weld_to_frame->has_as_rigid_body(nullptr)) {
+      // N.B. We compare based on name because at some point, the world
+      // `RigidBody` gets copied and thus invalidates pointer comparison.
+      if (!weld_to_frame->has_as_rigid_body(nullptr) &&
+          weld_to_frame->get_rigid_body().get_name() !=
+              RigidBodyTreeConstants::kWorldName) {
         throw runtime_error(
             "AddFloatingJoint: Attempted to weld robot to the world while "
-            "specifying a body link!");
+            "specifying a non-world body link!");
       }
       weld_to_body = tree->get_bodies()[0].get();  // the world's body
       floating_joint_name = FloatingJointConstants::kFloatingJointName;
