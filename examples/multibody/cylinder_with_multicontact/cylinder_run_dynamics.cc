f785d81b838d04cf676d74220b7da0e1a92be518
amcastro-tri <alejandro.castro@tri.global>
2018-08-09 10:07:20 -0400

    Moves to cylinder_with_multicontact/

---
+++ examples/multibody/cylinder_with_multicontact/cylinder_run_dynamics.cc
@@ -0,0 +1,291 @@
+#include <memory>
+
+#include <gflags/gflags.h>
+
+#include "drake/common/drake_assert.h"
+#include "drake/common/text_logging_gflags.h"
+#include "drake/examples/multibody/cylinder_with_multicontact/make_cylinder_plant.h"
+#include "drake/geometry/geometry_visualization.h"
+#include "drake/geometry/scene_graph.h"
+#include "drake/lcm/drake_lcm.h"
+#include "drake/lcmt_viewer_draw.hpp"
+#include "drake/math/random_rotation.h"
+#include "drake/multibody/multibody_tree/quaternion_floating_mobilizer.h"
+#include "drake/systems/analysis/implicit_euler_integrator.h"
+#include "drake/systems/analysis/runge_kutta2_integrator.h"
+#include "drake/systems/analysis/runge_kutta3_integrator.h"
+#include "drake/systems/analysis/semi_explicit_euler_integrator.h"
+#include "drake/systems/analysis/simulator.h"
+#include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/lcm/lcm_publisher_system.h"
+#include "drake/systems/lcm/serializer.h"
+#include "drake/systems/rendering/pose_bundle_to_draw_message.h"
+
+#include <iostream>
+#define PRINT_VAR(a) std::cout << #a": " << a << std::endl;
+#define PRINT_VARn(a) std::cout << #a":\n" << a << std::endl;
+
+namespace drake {
+namespace examples {
+namespace multibody {
+namespace bouncing_ball {
+namespace {
+
+DEFINE_double(target_realtime_rate, 0.5,
+              "Desired rate relative to real time.  See documentation for "
+              "Simulator::set_target_realtime_rate() for details.");
+
+DEFINE_string(integration_scheme, "runge_kutta2",
+              "Integration scheme to be used. Available options are: "
+              "'semi_explicit_euler','runge_kutta2','runge_kutta3',"
+              "'implicit_euler'");
+
+DEFINE_double(simulation_time, 10.0,
+              "Desired duration of the simulation in seconds.");
+
+DEFINE_double(z0, 0.5,
+              "The initial height, in z, of the ball, m.");
+
+DEFINE_double(vx0, 1.0,
+              "The initial x-velocity of the ball, m/s.");
+
+DEFINE_double(wx0, 0.1,
+              "The initial x-angular velocity of the ball, rad/s.");
+
+DEFINE_double(friction_coefficient, 0.5,
+              "The friction coefficient of both the sphere and the ground.");
+
+DEFINE_double(penetration_allowance, 1.0e-3,
+              "Penetration allowance. [m]. "
+              "See MultibodyPlant::set_penetration_allowance().");
+
+DEFINE_double(stiction_tolerance, 1.0e-4,
+              "The maximum slipping speed allowed during stiction. [m/s]");
+
+DEFINE_double(time_step, 1.0e-3,
+              "If zero, the plant is modeled as a continuous system. "
+              "If positive, the period (in seconds) of the discrete updates "
+              "for the plant modeled as a discrete system."
+              "This parameter must be non-negative.");
+
+using Eigen::AngleAxisd;
+using Eigen::Isometry3d;
+using Eigen::Matrix3d;
+using Eigen::Vector3d;
+using geometry::SceneGraph;
+using geometry::SourceId;
+using lcm::DrakeLcm;
+using drake::multibody::multibody_plant::CoulombFriction;
+using drake::multibody::multibody_plant::ContactResults;
+using drake::multibody::multibody_plant::MultibodyPlant;
+using drake::multibody::multibody_plant::PointPairContactInfo;
+using drake::multibody::MultibodyTree;
+using drake::multibody::QuaternionFloatingMobilizer;
+using drake::multibody::SpatialVelocity;
+using drake::systems::AbstractValue;
+using drake::systems::ImplicitEulerIntegrator;
+using drake::systems::lcm::LcmPublisherSystem;
+using drake::systems::lcm::Serializer;
+using drake::systems::rendering::PoseBundleToDrawMessage;
+using drake::systems::RungeKutta2Integrator;
+using drake::systems::RungeKutta3Integrator;
+using drake::systems::SemiExplicitEulerIntegrator;
+
+
+int do_main() {
+  systems::DiagramBuilder<double> builder;
+
+  SceneGraph<double>& scene_graph = *builder.AddSystem<SceneGraph>();
+  scene_graph.set_name("scene_graph");
+
+  // The target accuracy determines the size of the actual time steps taken
+  // whenever a variable time step integrator is used.
+  const double target_accuracy = 0.001;
+
+  // Plant's parameters.
+  const double radius = 0.05;   // m
+  const double mass = 0.1;      // kg
+  const double g = 9.81;        // m/s^2
+  const CoulombFriction<double> coulomb_friction(
+      FLAGS_friction_coefficient /* static friction */,
+      FLAGS_friction_coefficient /* dynamic friction */);
+
+  MultibodyPlant<double>& plant = *builder.AddSystem(MakeBouncingBallPlant(
+      8,
+      radius, mass, coulomb_friction, -g * Vector3d::UnitZ(), FLAGS_time_step,
+      &scene_graph));
+  const MultibodyTree<double>& model = plant.model();
+  // Set how much penetration (in meters) we are willing to accept.
+  plant.set_penetration_allowance(FLAGS_penetration_allowance);
+  plant.set_stiction_tolerance(FLAGS_stiction_tolerance);
+
+  // Hint the integrator's time step based on the contact time scale.
+  // A fraction of this time scale is used which is chosen so that the fixed
+  // time step integrators are stable.
+  const double max_time_step =
+      plant.get_contact_penalty_method_time_scale() / 30;
+
+  DRAKE_DEMAND(plant.num_velocities() == 6);
+  DRAKE_DEMAND(plant.num_positions() == 7);
+
+  // Boilerplate used to connect the plant to a SceneGraph for
+  // visualization.
+  DrakeLcm lcm;
+  const PoseBundleToDrawMessage& converter =
+      *builder.template AddSystem<PoseBundleToDrawMessage>();
+  LcmPublisherSystem& publisher =
+      *builder.template AddSystem<LcmPublisherSystem>(
+          "DRAKE_VIEWER_DRAW",
+          std::make_unique<Serializer<drake::lcmt_viewer_draw>>(), &lcm);
+  publisher.set_publish_period(1 / 60.0);
+
+  // Sanity check on the availability of the optional source id before using it.
+  DRAKE_DEMAND(!!plant.get_source_id());
+
+  builder.Connect(
+      plant.get_geometry_poses_output_port(),
+      scene_graph.get_source_pose_port(plant.get_source_id().value()));
+  builder.Connect(scene_graph.get_query_output_port(),
+                  plant.get_geometry_query_input_port());
+
+  builder.Connect(scene_graph.get_pose_bundle_output_port(),
+                  converter.get_input_port(0));
+  builder.Connect(converter, publisher);
+
+  // Last thing before building the diagram; dispatch the message to load
+  // geometry.
+  geometry::DispatchLoadMessage(scene_graph);
+
+  // And build the Diagram:
+  std::unique_ptr<systems::Diagram<double>> diagram = builder.Build();
+
+  // Create a context for this system:
+  std::unique_ptr<systems::Context<double>> diagram_context =
+      diagram->CreateDefaultContext();
+  diagram->SetDefaultContext(diagram_context.get());
+  systems::Context<double>& plant_context =
+      diagram->GetMutableSubsystemContext(plant, diagram_context.get());
+
+  // Set at height z0 with random orientation.
+  std::mt19937 generator(41);
+  std::uniform_real_distribution<double> uniform(-1.0, 1.0);
+  model.SetDefaultContext(&plant_context);
+  //Matrix3d R_WB = math::UniformlyRandomRotationMatrix(&generator).matrix();
+  Isometry3d X_WB = Isometry3d::Identity();
+  //X_WB.linear() = R_WB;
+  X_WB.translation() = Vector3d(0.0, 0.0, FLAGS_z0);
+  const auto& ball = model.GetBodyByName("Ball");
+  model.SetFreeBodyPoseOrThrow(
+      ball, X_WB, &plant_context);
+  model.SetFreeBodySpatialVelocityOrThrow(
+      ball,
+      SpatialVelocity<double>(
+          Vector3<double>(FLAGS_wx0, 0.0, 0.0),
+          Vector3<double>(FLAGS_vx0, 0.0, 0.0)), &plant_context);
+
+  systems::Simulator<double> simulator(*diagram, std::move(diagram_context));
+
+  systems::IntegratorBase<double>* integrator{nullptr};
+  if (FLAGS_integration_scheme == "implicit_euler") {
+    integrator =
+        simulator.reset_integrator<ImplicitEulerIntegrator<double>>(
+            *diagram, &simulator.get_mutable_context());
+  } else if (FLAGS_integration_scheme == "runge_kutta2") {
+    integrator =
+        simulator.reset_integrator<RungeKutta2Integrator<double>>(
+            *diagram, max_time_step, &simulator.get_mutable_context());
+  } else if (FLAGS_integration_scheme == "runge_kutta3") {
+    integrator =
+        simulator.reset_integrator<RungeKutta3Integrator<double>>(
+            *diagram, &simulator.get_mutable_context());
+  } else if (FLAGS_integration_scheme == "semi_explicit_euler") {
+    integrator =
+        simulator.reset_integrator<SemiExplicitEulerIntegrator<double>>(
+            *diagram, max_time_step, &simulator.get_mutable_context());
+  } else {
+    throw std::runtime_error(
+        "Integration scheme '" + FLAGS_integration_scheme +
+        "' not supported for this example.");
+  }
+  integrator->set_maximum_step_size(max_time_step);
+
+  // Error control is only supported for variable time step integrators.
+  if (!integrator->get_fixed_step_mode())
+    integrator->set_target_accuracy(target_accuracy);
+
+  simulator.set_publish_every_time_step(true);
+  simulator.set_target_realtime_rate(FLAGS_target_realtime_rate);
+  simulator.Initialize();
+  simulator.StepTo(FLAGS_simulation_time);
+
+  // Some sanity checks:
+  if (FLAGS_integration_scheme == "semi_explicit_euler") {
+    DRAKE_DEMAND(integrator->get_fixed_step_mode() == true);
+  }
+
+  // Checks for variable time step integrators.
+  if (!integrator->get_fixed_step_mode()) {
+    // From IntegratorBase::set_maximum_step_size():
+    // "The integrator may stretch the maximum step size by as much as 1% to
+    // reach discrete event." Thus the 1.01 factor in this DRAKE_DEMAND.
+    DRAKE_DEMAND(
+        integrator->get_largest_step_size_taken() <= 1.01 * max_time_step);
+    DRAKE_DEMAND(integrator->get_smallest_adapted_step_size_taken() <=
+        integrator->get_largest_step_size_taken());
+    DRAKE_DEMAND(integrator->get_num_steps_taken() >=
+        FLAGS_simulation_time / max_time_step);
+  }
+
+  // Checks for fixed time step integrators.
+  if (integrator->get_fixed_step_mode()) {
+    const int kNumEvaluationsPerStep =
+        FLAGS_integration_scheme == "runge_kutta2"? 2 : 1;
+    DRAKE_DEMAND(integrator->get_num_derivative_evaluations() ==
+        integrator->get_num_steps_taken() * kNumEvaluationsPerStep);
+    DRAKE_DEMAND(
+        integrator->get_num_step_shrinkages_from_error_control() == 0);
+  }
+
+  // We made a good guess for max_time_step and therefore we expect no
+  // failures when taking a time step.
+  DRAKE_DEMAND(integrator->get_num_substep_failures() == 0);
+  DRAKE_DEMAND(
+      integrator->get_num_step_shrinkages_from_substep_failures() == 0);
+
+  std::unique_ptr<AbstractValue> contact_results_value =
+      plant.get_contact_results_output_port().Allocate();
+  const ContactResults<double>& contact_results =
+      contact_results_value->GetValueOrThrow<ContactResults<double>>();
+  // Compute the poses for each geometry in the model.
+  plant.get_contact_results_output_port().Calc(
+      plant_context, contact_results_value.get());
+
+  //DRAKE_DEMAND(contact_results.num_contacts() == 1);  // only one contact pair.
+  //const PointPairContactInfo<double>& contact_info =
+    //  contact_results.contact_info(0);
+
+  for (int i(0); i < contact_results.num_contacts();++i) {
+    const auto& contact_info = contact_results.contact_info(i);
+    PRINT_VAR(contact_info.point_pair().depth);
+    PRINT_VAR(contact_info.separation_speed());
+    PRINT_VAR(contact_info.slip_speed());
+  }
+
+  return 0;
+}
+
+}  // namespace
+}  // namespace bouncing_ball
+}  // namespace multibody
+}  // namespace examples
+}  // namespace drake
+
+int main(int argc, char* argv[]) {
+  gflags::SetUsageMessage(
+      "A simple acrobot demo using Drake's MultibodyTree,"
+      "with SceneGraph visualization. "
+      "Launch drake-visualizer before running this example.");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  drake::logging::HandleSpdlogGflags();
+  return drake::examples::multibody::bouncing_ball::do_main();
+}
---
+++ examples/multibody/cylinder_with_multicontact/make_cylinder_plant.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <memory>
+
+#include "drake/geometry/scene_graph.h"
+#include "drake/multibody/multibody_tree/multibody_plant/multibody_plant.h"
+
+namespace drake {
+namespace examples {
+namespace multibody {
+namespace bouncing_ball {
+
+/// This method makes a MultibodyPlant model of a ball falling into a plane.
+/// MultibodyPlant models the contact of the ball with the ground as a perfectly
+/// inelastic collision (zero coefficient of restitution), i.e. energy is lost
+/// due to the collision.
+///
+/// @param[in] radius
+///   The radius of the ball.
+/// @param[in] mass
+///   The mass of the ball.
+/// @param[in] surface_friction
+///   The Coulomb's law coefficients of friction.
+/// @param[in] gravity_W
+///   The acceleration of gravity vector, expressed in the world frame W.
+/// @param scene_graph
+///   If a SceneGraph is provided with this argument, this factory method
+///   will register the new multibody plant to be a source for that geometry
+///   system and it will also register geometry for collision.
+///   If this argument is omitted, no geometry will be registered.
+std::unique_ptr<drake::multibody::multibody_plant::MultibodyPlant<double>>
+MakeBouncingBallPlant(int nspheres,
+    double radius, double mass,
+    const drake::multibody::multibody_plant::CoulombFriction<double>&
+    surface_friction, const Vector3<double>& gravity_W,
+    double dt,
+    geometry::SceneGraph<double>* scene_graph = nullptr);
+
+}  // namespace bouncing_ball
+}  // namespace multibody
+}  // namespace examples
+}  // namespace drake
