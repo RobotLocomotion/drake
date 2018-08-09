edadbc8e4be34951ee6b347dffd5e5103e4d167a
amcastro-tri <alejandro.castro@tri.global>
2018-08-09 10:54:42 -0400

    Simplify example since it only runs time-stepping

--- examples/multibody/cylinder_with_multicontact/cylinder_run_dynamics.cc
+++ examples/multibody/cylinder_with_multicontact/cylinder_run_dynamics.cc
@@ -35,11 +35,6 @@ DEFINE_double(target_realtime_rate, 0.5,
               "Desired rate relative to real time.  See documentation for "
               "Simulator::set_target_realtime_rate() for details.");
 
-DEFINE_string(integration_scheme, "runge_kutta2",
-              "Integration scheme to be used. Available options are: "
-              "'semi_explicit_euler','runge_kutta2','runge_kutta3',"
-              "'implicit_euler'");
-
 DEFINE_double(simulation_time, 10.0,
               "Desired duration of the simulation in seconds.");
 
@@ -98,10 +93,6 @@ int do_main() {
   SceneGraph<double>& scene_graph = *builder.AddSystem<SceneGraph>();
   scene_graph.set_name("scene_graph");
 
-  // The target accuracy determines the size of the actual time steps taken
-  // whenever a variable time step integrator is used.
-  const double target_accuracy = 0.001;
-
   // Plant's parameters.
   const double radius = 0.05;   // m
   const double mass = 0.1;      // kg
@@ -117,12 +108,6 @@ int do_main() {
   // Set how much penetration (in meters) we are willing to accept.
   plant.set_penetration_allowance(FLAGS_penetration_allowance);
   plant.set_stiction_tolerance(FLAGS_stiction_tolerance);
-
-  // Hint the integrator's time step based on the contact time scale.
-  // A fraction of this time scale is used which is chosen so that the fixed
-  // time step integrators are stable.
-  const double max_time_step =
-      plant.get_contact_penalty_method_time_scale() / 30;
 
   DRAKE_DEMAND(plant.num_velocities() == 6);
   DRAKE_DEMAND(plant.num_positions() == 7);
@@ -184,91 +169,10 @@ int do_main() {
 
   systems::Simulator<double> simulator(*diagram, std::move(diagram_context));
 
-  systems::IntegratorBase<double>* integrator{nullptr};
-  if (FLAGS_integration_scheme == "implicit_euler") {
-    integrator =
-        simulator.reset_integrator<ImplicitEulerIntegrator<double>>(
-            *diagram, &simulator.get_mutable_context());
-  } else if (FLAGS_integration_scheme == "runge_kutta2") {
-    integrator =
-        simulator.reset_integrator<RungeKutta2Integrator<double>>(
-            *diagram, max_time_step, &simulator.get_mutable_context());
-  } else if (FLAGS_integration_scheme == "runge_kutta3") {
-    integrator =
-        simulator.reset_integrator<RungeKutta3Integrator<double>>(
-            *diagram, &simulator.get_mutable_context());
-  } else if (FLAGS_integration_scheme == "semi_explicit_euler") {
-    integrator =
-        simulator.reset_integrator<SemiExplicitEulerIntegrator<double>>(
-            *diagram, max_time_step, &simulator.get_mutable_context());
-  } else {
-    throw std::runtime_error(
-        "Integration scheme '" + FLAGS_integration_scheme +
-        "' not supported for this example.");
-  }
-  integrator->set_maximum_step_size(max_time_step);
-
-  // Error control is only supported for variable time step integrators.
-  if (!integrator->get_fixed_step_mode())
-    integrator->set_target_accuracy(target_accuracy);
-
   simulator.set_publish_every_time_step(true);
   simulator.set_target_realtime_rate(FLAGS_target_realtime_rate);
   simulator.Initialize();
   simulator.StepTo(FLAGS_simulation_time);
-
-  // Some sanity checks:
-  if (FLAGS_integration_scheme == "semi_explicit_euler") {
-    DRAKE_DEMAND(integrator->get_fixed_step_mode() == true);
-  }
-
-  // Checks for variable time step integrators.
-  if (!integrator->get_fixed_step_mode()) {
-    // From IntegratorBase::set_maximum_step_size():
-    // "The integrator may stretch the maximum step size by as much as 1% to
-    // reach discrete event." Thus the 1.01 factor in this DRAKE_DEMAND.
-    DRAKE_DEMAND(
-        integrator->get_largest_step_size_taken() <= 1.01 * max_time_step);
-    DRAKE_DEMAND(integrator->get_smallest_adapted_step_size_taken() <=
-        integrator->get_largest_step_size_taken());
-    DRAKE_DEMAND(integrator->get_num_steps_taken() >=
-        FLAGS_simulation_time / max_time_step);
-  }
-
-  // Checks for fixed time step integrators.
-  if (integrator->get_fixed_step_mode()) {
-    const int kNumEvaluationsPerStep =
-        FLAGS_integration_scheme == "runge_kutta2"? 2 : 1;
-    DRAKE_DEMAND(integrator->get_num_derivative_evaluations() ==
-        integrator->get_num_steps_taken() * kNumEvaluationsPerStep);
-    DRAKE_DEMAND(
-        integrator->get_num_step_shrinkages_from_error_control() == 0);
-  }
-
-  // We made a good guess for max_time_step and therefore we expect no
-  // failures when taking a time step.
-  DRAKE_DEMAND(integrator->get_num_substep_failures() == 0);
-  DRAKE_DEMAND(
-      integrator->get_num_step_shrinkages_from_substep_failures() == 0);
-
-  std::unique_ptr<AbstractValue> contact_results_value =
-      plant.get_contact_results_output_port().Allocate();
-  const ContactResults<double>& contact_results =
-      contact_results_value->GetValueOrThrow<ContactResults<double>>();
-  // Compute the poses for each geometry in the model.
-  plant.get_contact_results_output_port().Calc(
-      plant_context, contact_results_value.get());
-
-  //DRAKE_DEMAND(contact_results.num_contacts() == 1);  // only one contact pair.
-  //const PointPairContactInfo<double>& contact_info =
-    //  contact_results.contact_info(0);
-
-  for (int i(0); i < contact_results.num_contacts();++i) {
-    const auto& contact_info = contact_results.contact_info(i);
-    PRINT_VAR(contact_info.point_pair().depth);
-    PRINT_VAR(contact_info.separation_speed());
-    PRINT_VAR(contact_info.slip_speed());
-  }
 
   return 0;
 }
