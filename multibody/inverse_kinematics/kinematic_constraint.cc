e0e14be00b21cec72852f023da6e345957e8b8a1
hongkai-dai <daihongkai@gmail.com>
2018-08-27 13:18:34 -0700

    WIP: work on multibody IK constraint

---
+++ multibody/inverse_kinematics/kinematic_constraint.cc
@@ -0,0 +1,8 @@
+#include "drake/multibody/inverse_kinematics/kinematic_constraint.h"
+
+namespace drake {
+namespace multibody {
+namespace inverse_kinematics {
+}  // namespace inverse_kinematics
+}  // namespace multibody
+}  // namespace drake
