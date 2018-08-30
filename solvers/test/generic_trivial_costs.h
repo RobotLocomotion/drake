86a15eb1b8d42dd3b2d3bb366b6c73505dcb2135
Jeremy Nimmer <jeremy.nimmer@tri.global>
2018-08-30 14:50:36 -0400

    math, solvers: Remove unused include

--- solvers/test/generic_trivial_costs.h
+++ solvers/test/generic_trivial_costs.h
@@ -7,7 +7,6 @@
 #include "drake/common/drake_assert.h"
 #include "drake/common/drake_copyable.h"
 #include "drake/common/eigen_types.h"
-#include "drake/common/number_traits.h"
 #include "drake/math/autodiff.h"
 #include "drake/solvers/cost.h"
 #include "drake/solvers/function.h"
