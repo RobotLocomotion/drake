b140a6229582dcbcae5efbba048978e4938c97a0
hongkai-dai <daihongkai@gmail.com>
2018-08-30 21:02:30 -0700

    Add PositionConstraint

--- multibody/inverse_kinematics/kinematic_constraint.cc
+++ multibody/inverse_kinematics/kinematic_constraint.cc
@@ -2,7 +2,47 @@
 
 namespace drake {
 namespace multibody {
-namespace inverse_kinematics {
-}  // namespace inverse_kinematics
+namespace {
+// Check if the generalized positions in @p mbt_context is the same as @p q.
+// If they are not the same, then reset @p mbt_context's generalized positions
+// to q. Otherwise, leave @p mbt_context unchanged.
+template <typename T>
+void UpdateContextConfiguration(const Eigen::Ref<const VectorX<T>>& q,
+                                MultibodyTreeContext<T>* mbt_context) {
+  if (mbt_context->get_positions() != q) {
+    mbt_context->get_mutable_positions() = q;
+  }
+}
+}  // namespace
+PositionConstraint::PositionConstraint(
+    const multibody::MultibodyTree<AutoDiffXd>& tree, FrameIndex frameB_idx,
+    const Eigen::Ref<const Eigen::Vector3d>& p_BQ, FrameIndex frameA_idx,
+    const Eigen::Ref<const Eigen::Vector3d>& p_AQ_lower,
+    const Eigen::Ref<const Eigen::Vector3d>& p_AQ_upper,
+    MultibodyTreeContext<AutoDiffXd>* context)
+    : solvers::Constraint(3, tree.num_positions(), p_AQ_lower, p_AQ_upper),
+      tree_(tree),
+      frameB_idx_{frameB_idx},
+      frameA_idx_{frameA_idx},
+      p_BQ_{p_BQ},
+      context_{context} {
+  DRAKE_DEMAND(context);
+}
+
+void PositionConstraint::DoEval(const Eigen::Ref<const Eigen::VectorXd>& x,
+                                Eigen::VectorXd* y) const {
+  AutoDiffVecXd y_t;
+  Eval(math::initializeAutoDiff(x), &y_t);
+  *y = math::autoDiffToValueMatrix(y_t);
+}
+
+void PositionConstraint::DoEval(const Eigen::Ref<const AutoDiffVecXd>& x,
+                                AutoDiffVecXd* y) const {
+  y->resize(3);
+  UpdateContextConfiguration(x, context_);
+  tree_.CalcPointsPositions(*context_, tree_.get_frame(frameB_idx_),
+                            p_BQ_.cast<AutoDiffXd>(),
+                            tree_.get_frame(frameA_idx_), y);
+}
 }  // namespace multibody
 }  // namespace drake
--- multibody/inverse_kinematics/kinematic_constraint.h
+++ multibody/inverse_kinematics/kinematic_constraint.h
@@ -5,14 +5,58 @@
 
 namespace drake {
 namespace multibody {
-namespace inverse_kinematics {
-// Constrains the position of a point Q, rigidly attached to a frame A, to be within a bounding box measured and expressed in frame B. Namely lb <= p_BQ <= ub.
-class PositionConstraint : solvers::Constraint {
+// Constrains the position of a point Q, rigidly attached to a frame B, to be
+// within a bounding box measured and expressed in frame A. Namely
+// p_AQ_lower <= p_AQ <= p_AQ_upper.
+class PositionConstraint : public solvers::Constraint {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(PositionConstraint)
 
-  PositionConstraint(const multibody_tree::MultibodyTree& tree, int frameA_idx
+  /**
+   * @param tree The multibody tree on which the constraint is imposed. @p tree
+   * should be alive during the whole lifetime of this constraint.
+   * @param frameB_idx The index of frame B.
+   * @param p_BQ The position of the point Q, rigidly attached to frame B,
+   * measured and expressed in frame A.
+   * @param frameA_idx The index of frame A.
+   * @param p_AQ_lower The lower bound on the position of point Q, measured and
+   * expressed in frame A.
+   * @param p_AQ_upper The upper bound on the position of point Q, measured and
+   * expressed in frame A.
+   * @param context The Context that has been allocated for this @p tree. We
+   * will update the context when evaluating the constraint. @p context should
+   * be alive during the lifetime of this constraint.
+   */
+  PositionConstraint(const multibody::MultibodyTree<AutoDiffXd>& tree,
+                     FrameIndex frameB_idx,
+                     const Eigen::Ref<const Eigen::Vector3d>& p_BQ,
+                     FrameIndex frameA_idx,
+                     const Eigen::Ref<const Eigen::Vector3d>& p_AQ_lower,
+                     const Eigen::Ref<const Eigen::Vector3d>& p_AQ_upper,
+                     MultibodyTreeContext<AutoDiffXd>* context);
+
+  ~PositionConstraint() override {}
+
+ private:
+  void DoEval(const Eigen::Ref<const Eigen::VectorXd>& x,
+              Eigen::VectorXd* y) const override;
+
+  void DoEval(const Eigen::Ref<const AutoDiffVecXd>& x,
+              AutoDiffVecXd* y) const override;
+
+  void DoEval(const Eigen::Ref<const VectorX<symbolic::Variable>>& x,
+              VectorX<symbolic::Expression>* y) const override {
+    unused(x);
+    unused(y);
+    throw std::logic_error(
+        "PositionConstraint::DoEval() does not work for symbolic variables.");
+  }
+
+  const MultibodyTree<AutoDiffXd>& tree_;
+  const FrameIndex frameB_idx_;
+  const FrameIndex frameA_idx_;
+  const Eigen::Vector3d p_BQ_;
+  MultibodyTreeContext<AutoDiffXd>* const context_;
 };
-}  // namespace inverse_kinematics
 }  // namespace multibody
 }  // namespace drake
