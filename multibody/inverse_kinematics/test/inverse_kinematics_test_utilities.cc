a51132d5b9680435d0423d43d1e324b0dc234f21
hongkai-dai <daihongkai@gmail.com>
2018-09-01 22:54:49 -0700

    Add two free bodies to test kinematic constraints.

--- multibody/inverse_kinematics/test/inverse_kinematics_test_utilities.cc
+++ multibody/inverse_kinematics/test/inverse_kinematics_test_utilities.cc
@@ -1,7 +1,5 @@
 #include "drake/multibody/inverse_kinematics/test/inverse_kinematics_test_utilities.h"
 
-#include "drake/multibody/joints/quaternion_ball_joint.h"
-
 namespace drake {
 namespace multibody {
 namespace test {
@@ -12,17 +10,21 @@ std::unique_ptr<MultibodyTree<T>> ConstructTwoFreeBodies() {
   const double mass{1};
   const Eigen::Vector3d p_AoAcm_A(0, 0, 0);
   const RotationalInertia<double> I_AAcm_A{0.001, 0.001, 0.001};
-  const SpatialInertia<double> M_AAo_A = SpatialInertia<double>::MakeFromCentralInertia(mass, p_AoAcm_A, I_AAcm_A);
+  const SpatialInertia<double> M_AAo_A =
+      SpatialInertia<double>::MakeFromCentralInertia(mass, p_AoAcm_A, I_AAcm_A);
 
-  const RigidBody<T>& link1 = model->AddRigidBody("body1", M_AAo_A);
-  const RigidBody<T>& link2 = model->AddRigidBody("body2", M_AAo_A);
+  model->AddRigidBody("body1", M_AAo_A);
+  model->AddRigidBody("body2", M_AAo_A);
 
-  model->template AddJoint<QuaternionBallJoint>("joint1", model->world_body(), Eigen::Isometry3d::Identity(), link1, Eigen::Isometry3d::Identity());
+  model->Finalize();
+
   return model;
 }
 
-template std::unique_ptr<MultibodyTree<double>> ConstructTwoFreeBodies<double>();
-template std::unique_ptr<MultibodyTree<AutoDiffXd>> ConstructTwoFreeBodies<AutoDiffXd>();
+template std::unique_ptr<MultibodyTree<double>>
+ConstructTwoFreeBodies<double>();
+template std::unique_ptr<MultibodyTree<AutoDiffXd>>
+ConstructTwoFreeBodies<AutoDiffXd>();
 }  // namespace test
 }  // namespace multibody
 }  // namespace drake
--- multibody/inverse_kinematics/test/kinematic_constraint_test.cc
+++ multibody/inverse_kinematics/test/kinematic_constraint_test.cc
@@ -5,6 +5,7 @@
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
 #include "drake/math/autodiff_gradient.h"
 #include "drake/multibody/benchmarks/kuka_iiwa_robot/make_kuka_iiwa_model.h"
+#include "drake/multibody/inverse_kinematics/test/inverse_kinematics_test_utilities.h"
 
 namespace drake {
 namespace multibody {
@@ -45,16 +46,23 @@ class IiwaKinematicConstraintTest : public ::testing::Test {
   std::unique_ptr<systems::LeafContext<double>> context_double_;
 };
 
-template <typename T>
-unique_ptr<MultibodyTree<T>>
 // Test kinematic constraints on two free floating bodies.
-class TwoBodiesConstraintTest : public ::testing::Test {
+class TwoFreeBodiesConstraintTest : public ::testing::Test {
  public:
-  DRAKE_NO_COPY_NO_MOVE_NO-ASSIGN(TwoBodiesConstraintTest)
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(TwoFreeBodiesConstraintTest)
 
- public:
-   std::unique_ptr<MultibodyTree<AutoDiffXd>> two_bodies_;
-}
+  TwoFreeBodiesConstraintTest()
+      : two_bodies_(test::ConstructTwoFreeBodies<AutoDiffXd>()),
+        body1_index_(two_bodies_->GetBodyByName("body1").body_frame().index()),
+        body2_index_(two_bodies_->GetBodyByName("body2").body_frame().index()),
+        context_autodiff_(two_bodies_->CreateDefaultContext()) {}
+
+ protected:
+  std::unique_ptr<MultibodyTree<AutoDiffXd>> two_bodies_;
+  FrameIndex body1_index_;
+  FrameIndex body2_index_;
+  std::unique_ptr<systems::LeafContext<AutoDiffXd>> context_autodiff_;
+};
 
 TEST_F(IiwaKinematicConstraintTest, PositionConstraint) {
   const Eigen::Vector3d p_BQ(0.1, 0.2, 0.3);
@@ -101,6 +109,10 @@ TEST_F(IiwaKinematicConstraintTest, PositionConstraint) {
   CompareAutoDiffVectors(y_autodiff, y_autodiff_expected, tol);
 }
 
+Eigen::Vector4d QuaternionToVector4(const Eigen::Quaterniond& q) {
+  return Eigen::Vector4d(q.w(), q.x(), q.y(), q.z());
+}
+
 TEST_F(IiwaKinematicConstraintTest, OrientationConstraint) {
   const double angle_bound{0.1 * M_PI};
   OrientationConstraint constraint(
@@ -111,8 +123,8 @@ TEST_F(IiwaKinematicConstraintTest, OrientationConstraint) {
   EXPECT_EQ(constraint.num_constraints(), 1);
   EXPECT_EQ(constraint.num_vars(), iiwa_autodiff_->num_positions());
   EXPECT_TRUE(CompareMatrices(constraint.lower_bound(),
-                              Vector1d(2 * std::cos(angle_bound) - 1)));
-  EXPECT_TRUE(CompareMatrices(constraint.upper_bound(), Vector1d(1)));
+                              Vector1d(2 * std::cos(angle_bound) + 1)));
+  EXPECT_TRUE(CompareMatrices(constraint.upper_bound(), Vector1d(3)));
 
   Eigen::VectorXd q(iiwa_autodiff_->num_positions());
   // Arbitrary joint angles.
@@ -131,6 +143,38 @@ TEST_F(IiwaKinematicConstraintTest, OrientationConstraint) {
           .linear()
           .trace();
   CompareAutoDiffVectors(y_autodiff, y_autodiff_expected, 1E-12);
+}
+
+TEST_F(TwoFreeBodiesConstraintTest, OrientationConstraint) {
+  // Check if the orientation constraints are satisfied for two bodies with
+  // given orientations
+  OrientationConstraint constraint(
+      *two_bodies_, body1_index_, body2_index_, 0.1 * M_PI,
+      dynamic_cast<MultibodyTreeContext<AutoDiffXd>*>(context_autodiff_.get()));
+
+  // With q_satisfied, the angle between body1 and body2 is 0.09 * M_PI, it
+  // should satisfy the orientation constraint.
+  Eigen::Matrix<double, 14, 1> q_satisfied;
+  // a is an arbitrary rotation axis.
+  const Eigen::Vector3d a(1.0 / 3, 2.0 / 3, -2.0 / 3);
+  const double theta_satisfied = 0.09 * M_PI;
+  const Eigen::Quaterniond body1_quaternion(0.5, -0.5, 0.1, 0.7);
+  Eigen::Quaterniond body2_quaternion =
+      body1_quaternion * Eigen::AngleAxisd(theta_satisfied, a);
+  q_satisfied << QuaternionToVector4(body1_quaternion), Eigen::Vector3d::Zero(),
+      QuaternionToVector4(body2_quaternion), Eigen::Vector3d(0.2, 0.3, 0.4);
+
+  EXPECT_TRUE(constraint.CheckSatisfied(q_satisfied));
+
+  // With q_unsatisfied, the angle between body1 and body2 is 0.101 * M_PI, it
+  // should not satisfy the orientation constraint.
+  const double theta_unsatisfied = 0.101 * M_PI;
+  body2_quaternion = body1_quaternion * Eigen::AngleAxisd(theta_unsatisfied, a);
+  Eigen::Matrix<double, 14, 1> q_unsatisfied;
+  q_unsatisfied << QuaternionToVector4(body1_quaternion),
+      Eigen::Vector3d::Zero(), QuaternionToVector4(body2_quaternion),
+      Eigen::Vector3d::Zero();
+  EXPECT_FALSE(constraint.CheckSatisfied(q_unsatisfied));
 }
 
 TEST_F(IiwaKinematicConstraintTest, OrientationConstraintConstructionError) {
@@ -201,9 +245,8 @@ TEST_F(IiwaKinematicConstraintTest, GazeTargetConstraintConstructorError) {
                    *iiwa_autodiff_, iiwa_link_frame_indices_[2], p_AS,
                    Eigen::Vector3d::Ones(), iiwa_link_frame_indices_[3], p_BT,
                    -0.1, dynamic_cast<MultibodyTreeContext<AutoDiffXd>*>(
-                            context_autodiff_.get())),
+                             context_autodiff_.get())),
                std::logic_error);
-
 }
 }  // namespace multibody
 }  // namespace drake
