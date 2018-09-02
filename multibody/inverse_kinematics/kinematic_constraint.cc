fe859fb8f592fedff9d5e99f000c17ebbd4a9562
hongkai-dai <daihongkai@gmail.com>
2018-09-01 19:04:50 -0700

    WIP: add AngleBetweenVectorsConstraint

--- multibody/inverse_kinematics/kinematic_constraint.cc
+++ multibody/inverse_kinematics/kinematic_constraint.cc
@@ -37,8 +37,8 @@ PositionConstraint::PositionConstraint(
     MultibodyTreeContext<AutoDiffXd>* context)
     : solvers::Constraint(3, tree.num_positions(), p_AQ_lower, p_AQ_upper),
       tree_(tree),
-      frameB_idx_{frameB_idx},
-      frameA_idx_{frameA_idx},
+      frameB_{tree_.get_frame(frameB_idx)},
+      frameA_{tree_.get_frame(frameA_idx)},
       p_BQ_{p_BQ},
       context_{context} {
   DRAKE_DEMAND(context);
@@ -55,9 +55,8 @@ void PositionConstraint::DoEval(const Eigen::Ref<const AutoDiffVecXd>& x,
                                 AutoDiffVecXd* y) const {
   y->resize(3);
   UpdateContextConfiguration(x, context_);
-  tree_.CalcPointsPositions(*context_, tree_.get_frame(frameB_idx_),
-                            p_BQ_.cast<AutoDiffXd>(),
-                            tree_.get_frame(frameA_idx_), y);
+  tree_.CalcPointsPositions(*context_, frameB_, p_BQ_.cast<AutoDiffXd>(),
+                            frameA_, y);
 }
 
 OrientationConstraint::OrientationConstraint(
@@ -67,8 +66,8 @@ OrientationConstraint::OrientationConstraint(
     : solvers::Constraint(1, tree.num_positions(),
                           Vector1d(2 * std::cos(angle_bound) - 1), Vector1d(1)),
       tree_{tree},
-      frameA_idx_{frameA_idx},
-      frameB_idx_{frameB_idx},
+      frameA_{tree.get_frame(frameA_idx)},
+      frameB_{tree.get_frame(frameB_idx)},
       context_(context) {
   DRAKE_DEMAND(context);
   if (angle_bound < 0) {
@@ -88,11 +87,8 @@ void OrientationConstraint::DoEval(const Eigen::Ref<const AutoDiffVecXd>& x,
                                    AutoDiffVecXd* y) const {
   y->resize(1);
   UpdateContextConfiguration(x, context_);
-  (*y)(0) = tree_
-                .CalcRelativeTransform(*context_, tree_.get_frame(frameA_idx_),
-                                       tree_.get_frame(frameB_idx_))
-                .linear()
-                .trace();
+  (*y)(0) =
+      tree_.CalcRelativeTransform(*context_, frameA_, frameB_).linear().trace();
 }
 
 GazeTargetConstraint::GazeTargetConstraint(
@@ -138,6 +134,44 @@ void GazeTargetConstraint::DoEval(const Eigen::Ref<const AutoDiffVecXd>& x,
   (*y)(0) = p_ST_A.dot(n_A_);
   (*y)(1) = (*y)(0) * (*y)(0) -
             std::pow(cos_cone_half_angle_, 2) * p_ST_A.dot(p_ST_A);
+}
+
+AngleBetweenVectorsConstraint::AngleBetweenVectorsConstraint(
+    const MultibodyTree<AutoDiffXd>& tree, const FrameIndex& frameA_idx,
+    const Eigen::Ref<const Eigen::Vector3d>& n_A, const FrameIndex& frameB_idx,
+    const Eigen::Ref<const Eigen::Vector3d>& n_B, double angle_lower,
+    double angle_upper, MultibodyTreeContext<AutoDiffXd>* context)
+    : solvers::Constraint(1, tree.num_positions(),
+                          Vector1d(std::cos(angle_upper)),
+                          Vector1d(std::cos(angle_lower))),
+      tree_(tree),
+      frameA_(tree_.get_frame(frameA_idx)),
+      n_A_A_(NormalizeVector(n_A)),
+      frameB_(tree_.get_frame(frameB_idx)),
+      n_B_B_(NormalizeVector(n_B)),
+      context_(context) {
+  if (!(angle_lower >= 0 && angle_upper >= angle_lower &&
+        angle_upper <= M_PI)) {
+    throw std::logic_error(
+        "AngleBetweenVectorsConstraint: should satisfy 0 <= angle_lower <= "
+        "angle_upper <= pi");
+  }
+}
+
+void AngleBetweenVectorsConstraint::DoEval(
+    const Eigen::Ref<const Eigen::VectorXd>& x, Eigen::VectorXd* y) const {
+  AutoDiffVecXd y_t;
+  Eval(math::initializeAutoDiff(x), &y_t);
+  *y = math::autoDiffToValueMatrix(y_t);
+}
+
+void AngleBetweenVectorsConstraint::DoEval(
+    const Eigen::Ref<const AutoDiffVecXd>& x, AutoDiffVecXd* y) const {
+  y->resize(1);
+  UpdateContextConfiguration(x, context_);
+  (*y)(0) = n_A_A_.dot(
+      tree_.CalcRelativeTransform(*context_, frameA_, frameB_).linear() *
+      n_B_B_);
 }
 }  // namespace multibody
 }  // namespace drake
--- multibody/inverse_kinematics/kinematic_constraint.h
+++ multibody/inverse_kinematics/kinematic_constraint.h
@@ -55,8 +55,8 @@ class PositionConstraint : public solvers::Constraint {
   }
 
   const MultibodyTree<AutoDiffXd>& tree_;
-  const FrameIndex frameB_idx_;
-  const FrameIndex frameA_idx_;
+  const Frame<AutoDiffXd>& frameB_;
+  const Frame<AutoDiffXd>& frameA_;
   const Eigen::Vector3d p_BQ_;
   MultibodyTreeContext<AutoDiffXd>* const context_;
 };
@@ -114,12 +114,13 @@ class OrientationConstraint : public solvers::Constraint {
     unused(x);
     unused(y);
     throw std::logic_error(
-        "PositionConstraint::DoEval() does not work for symbolic variables.");
+        "OrientationConstraint::DoEval() does not work for symbolic "
+        "variables.");
   }
 
   const MultibodyTree<AutoDiffXd>& tree_;
-  const FrameIndex frameA_idx_;
-  const FrameIndex frameB_idx_;
+  const Frame<AutoDiffXd>& frameA_;
+  const Frame<AutoDiffXd>& frameB_;
   MultibodyTreeContext<AutoDiffXd>* const context_;
 };
 
@@ -169,7 +170,7 @@ class GazeTargetConstraint : public solvers::Constraint {
                        double cone_half_angle,
                        MultibodyTreeContext<AutoDiffXd>* context);
 
-  ~GazeTargetConstraint() override {};
+  ~GazeTargetConstraint() override{};
 
  private:
   void DoEval(const Eigen::Ref<const Eigen::VectorXd>& x,
@@ -183,7 +184,7 @@ class GazeTargetConstraint : public solvers::Constraint {
     unused(x);
     unused(y);
     throw std::logic_error(
-        "PositionConstraint::DoEval() does not work for symbolic variables.");
+        "GazeTargetConstraint::DoEval() does not work for symbolic variables.");
   }
   const MultibodyTree<AutoDiffXd>& tree_;
   const Frame<AutoDiffXd>& frameA_;
@@ -194,6 +195,71 @@ class GazeTargetConstraint : public solvers::Constraint {
   const double cone_half_angle_;
   const double cos_cone_half_angle_;
   MultibodyTreeContext<AutoDiffXd>* const context_;
+};
+
+/**
+ * Constrains that the angle between a vector n_A and another vector n_B is
+ * between [θ_lower, θ_upper]. n_A is fixed to a frame A, while n_B is fixed to
+ * a frame B.
+ * Mathematically, if we denote n_A_A as n_A measured and expressed in frame A
+ * after normalization (n_A_A has unit length), and n_B_B as n_B measured and
+ * expressed in frame B after normalization, the constraint is
+ * cos(θ_upper) ≤ n_A_Aᵀ * R_AB * n_B_B ≤ cos(θ_lower)
+ */
+class AngleBetweenVectorsConstraint : public solvers::Constraint {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(AngleBetweenVectorsConstraint)
+
+  /**
+   * @param tree The MultibodyTree on which the constraint is imposed. @p tree
+   * should be alive during the lifetime of this constraint.
+   * @param frameA_idx The index of frame A.
+   * @param n_A The vector n_A fixed to frame A, measured and expressed in frame
+   * A. @pre n_A should be a non-zero vector. @throw logic error if n_A is
+   * close to zero.
+   * @param frameB_idx The index of frame B.
+   * @param n_B The vector n_A fixed to frame B, measured and expressed in frame
+   * B. @pre n_B should be a non-zero vector. @throw logic error if n_B is
+   * close to zero.
+   * @param angle_lower The lower bound on the angle between n_A and n_B. It is
+   * denoted as θ_lower in the class documentation. @pre angle_lower >= 0.
+   * @throw a logic error if angle_lower is negative.
+   * @param angle_upper The upper bound on the angle between n_A and n_B. it is
+   * denoted as θ_upper in the class documentation. @pre angle_lower <=
+   * angle_upper <= pi. @throw a logic error if angle_upper is outside the
+   * bounds.
+   */
+  AngleBetweenVectorsConstraint(const MultibodyTree<AutoDiffXd>& tree,
+                                const FrameIndex& frameA_idx,
+                                const Eigen::Ref<const Eigen::Vector3d>& n_A,
+                                const FrameIndex& frameB_idx,
+                                const Eigen::Ref<const Eigen::Vector3d>& n_B,
+                                double angle_lower, double angle_upper,
+                                MultibodyTreeContext<AutoDiffXd>* context);
+
+  ~AngleBetweenVectorsConstraint() override {}
+
+ private:
+  void DoEval(const Eigen::Ref<const Eigen::VectorXd>& x,
+              Eigen::VectorXd* y) const override;
+
+  void DoEval(const Eigen::Ref<const AutoDiffVecXd>& x,
+              AutoDiffVecXd* y) const override;
+
+  void DoEval(const Eigen::Ref<const VectorX<symbolic::Variable>>& x,
+              VectorX<symbolic::Expression>* y) const override {
+    unused(x);
+    unused(y);
+    throw std::logic_error(
+        "AngleBetweenVectorsConstraint::DoEval() does not work for symbolic "
+        "variables.");
+  }
+  const MultibodyTree<AutoDiffXd>& tree_;
+  const Frame<AutoDiffXd>& frameA_;
+  const Eigen::Vector3d n_A_A_;
+  const Frame<AutoDiffXd>& frameB_;
+  const Eigen::Vector3d n_B_B_;
+  MultibodyTreeContext<AutoDiffXd>* context_;
 };
 }  // namespace multibody
 }  // namespace drake
