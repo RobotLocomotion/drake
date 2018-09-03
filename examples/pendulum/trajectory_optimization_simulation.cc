1d786a4c1fc8720acbe9c7b81c494a06f246604e
Russ Tedrake <russt@mit.edu>
2018-09-02 21:44:00 -0400

    convert pendulum example visualization from RBT to SceneGraph

--- examples/pendulum/trajectory_optimization_simulation.cc
+++ examples/pendulum/trajectory_optimization_simulation.cc
@@ -6,10 +6,8 @@
 #include "drake/common/find_resource.h"
 #include "drake/common/is_approx_equal_abstol.h"
 #include "drake/examples/pendulum/pendulum_plant.h"
+#include "drake/geometry/geometry_visualization.h"
 #include "drake/lcm/drake_lcm.h"
-#include "drake/multibody/joints/floating_base_types.h"
-#include "drake/multibody/parsers/urdf_parser.h"
-#include "drake/multibody/rigid_body_plant/drake_visualizer.h"
 #include "drake/systems/analysis/simulator.h"
 #include "drake/systems/controllers/pid_controlled_system.h"
 #include "drake/systems/framework/diagram.h"
@@ -89,6 +87,14 @@ int DoMain() {
       builder.AddSystem<systems::TrajectorySource>(pp_xtraj);
   state_trajectory->set_name("state trajectory");
 
+  auto scene_graph = builder.AddSystem<geometry::SceneGraph>();
+  pendulum->RegisterGeometry(pendulum->get_parameters(*context),
+                             scene_graph);
+  const geometry::SourceId source_id = pendulum->source_id();
+  const int state_port_index = pendulum->get_state_output_port().get_index();
+  const int geometry_port_index =
+      pendulum->get_geometry_pose_output_port().get_index();
+
   // The choices of PidController constants here are fairly arbitrary,
   // but seem to effectively swing up the pendulum and hold it.
   const double Kp = 10;
@@ -96,24 +102,21 @@ int DoMain() {
   const double Kd = 1;
   auto pid_controlled_pendulum =
       builder.AddSystem<systems::controllers::PidControlledSystem<double>>(
-          std::move(pendulum), Kp, Ki, Kd);
+          std::move(pendulum), Kp, Ki, Kd, state_port_index);
   pid_controlled_pendulum->set_name("PID Controlled Pendulum");
-
-  lcm::DrakeLcm lcm;
-  auto tree = std::make_unique<RigidBodyTree<double>>();
-  parsers::urdf::AddModelInstanceFromUrdfFileToWorld(
-      FindResourceOrThrow("drake/examples/pendulum/Pendulum.urdf"),
-      multibody::joints::kFixed, tree.get());
-
-  auto publisher = builder.AddSystem<systems::DrakeVisualizer>(*tree, &lcm);
-  publisher->set_name("publisher");
 
   builder.Connect(input_trajectory->get_output_port(),
                   pid_controlled_pendulum->get_control_input_port());
   builder.Connect(state_trajectory->get_output_port(),
                   pid_controlled_pendulum->get_state_input_port());
-  builder.Connect(pid_controlled_pendulum->get_state_output_port(),
-                  publisher->get_input_port(0));
+
+  builder.Connect(pid_controlled_pendulum->get_output_port(geometry_port_index),
+                  scene_graph->get_source_pose_port(source_id));
+
+  lcm::DrakeLcm lcm;
+  geometry::ConnectVisualization(*scene_graph, &builder, &lcm);
+  geometry::DispatchLoadMessage(*scene_graph, &lcm);
+
 
   auto diagram = builder.Build();
 
