5da3352d8c5d3aecba7a43f191483ec7b897c199
hongkai-dai <daihongkai@gmail.com>
2018-09-06 16:37:16 -0700

    Cannot run the test with squaredNorm constraint

--- bindings/pydrake/multibody/inverse_kinematics_py.cc
+++ bindings/pydrake/multibody/inverse_kinematics_py.cc
@@ -3,6 +3,7 @@
 #include "pybind11/pybind11.h"
 
 #include "drake/bindings/pydrake/pydrake_pybind.h"
+#include "drake/bindings/pydrake/symbolic_types_pybind.h"
 #include "drake/multibody/inverse_kinematics/inverse_kinematics.h"
 
 namespace drake {
@@ -17,8 +18,6 @@ using multibody::Frame;
 
 void init_module(py::module m) {
   m.doc() = "InverseKinematics module";
-
-  py::object variable = py::module::import("pydrake.symbolic").attr("Variable");
 
   py::class_<InverseKinematics> ik_cls(m, "InverseKinematics");
   ik_cls.def(py::init<const MultibodyPlant<double>&>(), py::arg("plant"))
@@ -35,13 +34,29 @@ void init_module(py::module m) {
                const Frame<double>&, const Frame<double>&, double)>(
                &InverseKinematics::AddOrientationConstraint),
            py::arg("frameA"), py::arg("frameB"), py::arg("theta_bound"))
+      .def("AddGazeTargetConstraint",
+           static_cast<Binding<Constraint> (InverseKinematics::*)(
+               const Frame<double>&, const Eigen::Ref<const Eigen::Vector3d>&,
+               const Eigen::Ref<const Eigen::Vector3d>&, const Frame<double>&,
+               const Eigen::Ref<const Eigen::Vector3d>&, double)>(
+               &InverseKinematics::AddGazeTargetConstraint),
+           py::arg("frameA"), py::arg("p_AS"), py::arg("n_A"),
+           py::arg("frameB"), py::arg("p_BT"), py::arg("cone_half_angle"))
+      .def("AddAngleBetweenVectorsConstraint",
+           static_cast<Binding<Constraint> (InverseKinematics::*)(
+               const Frame<double>&, const Eigen::Ref<const Eigen::Vector3d>&,
+               const Frame<double>&, const Eigen::Ref<const Eigen::Vector3d>&,
+               double, double)>(
+               &InverseKinematics::AddAngleBetweenVectorsConstraint),
+           py::arg("frameA"), py::arg("na_A"), py::arg("frameB"),
+           py::arg("nb_B"), py::arg("angle_lower"), py::arg("angle_upper"))
+      .def("q", static_cast<const solvers::VectorXDecisionVariable& (
+                    InverseKinematics::*)() const>(&InverseKinematics::q))
       .def("prog", static_cast<const MathematicalProgram& (
                        InverseKinematics::*)() const>(&InverseKinematics::prog))
       .def("get_mutable_prog",
            static_cast<MathematicalProgram* (InverseKinematics::*)() const>(
-               &InverseKinematics::get_mutable_prog))
-      .def("q", static_cast<const solvers::VectorXDecisionVariable& (
-                    InverseKinematics::*)() const>(&InverseKinematics::q));
+               &InverseKinematics::get_mutable_prog));
 }
 
 void init_all(py::module m) {
--- bindings/pydrake/multibody/test/inverse_kinematics_test.py
+++ bindings/pydrake/multibody/test/inverse_kinematics_test.py
@@ -1,4 +1,7 @@
 from pydrake.multibody import inverse_kinematics as ik
+
+import math
+
 from pydrake.multibody.multibody_tree.multibody_plant import MultibodyPlant 
 from pydrake.multibody.multibody_tree.parsing import (
     AddModelFromSdfFile,
@@ -13,22 +16,29 @@ from pydrake.common import FindResourceOrThrow
 class TestInverseKinematics(unittest.TestCase):
     def setUp(self):
         file_name = FindResourceOrThrow(
-            "drake/multibody/benchmarks/acrobot/acrobot.sdf")
+            "drake/bindings/pydrake/multibody/test/two_bodies.sdf")
         self.plant = MultibodyPlant(time_step=0.01)
         model_instance = AddModelFromSdfFile(
             file_name=file_name, plant=self.plant, scene_graph=None)
         self.plant.Finalize()
-        self.link1_frame = self.plant.GetBodyByName("Link1").body_frame()
-        self.link2_frame = self.plant.GetBodyByName("Link2").body_frame()
+        self.link1_frame = self.plant.GetBodyByName("body1").body_frame()
+        self.link2_frame = self.plant.GetBodyByName("body2").body_frame()
+        self.ik_two_bodies= ik.InverseKinematics(self.plant)
+
+        def squaredNorm(x):
+            return np.array([x[0] * x[0] + x[1] * x[1] + x[2] * x[2] + x[3] * x[3]])
+
+        self.ik_two_bodies.get_mutable_prog().AddConstraint(squaredNorm, [1], [1], self.ik_two_bodies.q()[:4])
+        self.ik_two_bodies.get_mutable_prog().AddConstraint(squaredNorm, [1], [1], self.ik_two_bodies.q()[7:11])
 
     def test_AddPositionConstraint(self):
-        ik_acrobot = ik.InverseKinematics(self.plant)
-        ik_acrobot.AddPositionConstraint(self.link1_frame, [0, 0.1, 0.2], self.link2_frame, [-1, -1, -1], [1, 1, 1])
-        result = ik_acrobot.get_mutable_prog().Solve()
+        self.ik_two_bodies.AddPositionConstraint(self.link1_frame, [0, 0.1, 0.2], self.link2_frame, [-1, -1, -1], [1, 1, 1])
+        result = self.ik_two_bodies.get_mutable_prog().Solve()
         self.assertEqual(result, mp.SolutionResult.kSolutionFound)
+        q_val = self.ik_two_bodies.prog().GetSolution(self.ik_two_bodies.q())
+        print q_val
 
     def test_AddOrientationConstraint(self):
-        ik_acrobot = ik.InverseKinematics(self.plant)
-        ik_acrobot.AddOrientationConstraint(self.link1_frame, self.link2_frame, 0.5 * math.pi)
-        result = ik_acrobot.get_mutable_prog().Solve()
+        self.ik_two_bodies.AddOrientationConstraint(self.link1_frame, self.link2_frame, 0.5 * math.pi)
+        result = self.ik_two_bodies.get_mutable_prog().Solve()
         self.assertEqual(result, mp.SolutionResult.kSolutionFound)
