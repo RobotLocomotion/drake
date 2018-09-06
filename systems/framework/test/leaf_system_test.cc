5fc3556c0ffa0b45f3d850255bd10b9ccc8bc757
damrongguoy <Damrong.Guoy@tri.global>
2018-09-05 18:00:38 -0700

    Step 4 of System::get_num_continuous_states(): unit test of leaf_system

--- systems/framework/test/leaf_system_test.cc
+++ systems/framework/test/leaf_system_test.cc
@@ -569,6 +569,9 @@ TEST_F(LeafSystemTest, AbstractParameters) {
 // Tests that the leaf system reserved the declared misc continuous state.
 TEST_F(LeafSystemTest, DeclareVanillaMiscContinuousState) {
   system_.DeclareContinuousState(2);
+
+  EXPECT_EQ(2, system_.get_num_continuous_states()); // test it before creating Context
+
   std::unique_ptr<Context<double>> context = system_.CreateDefaultContext();
   const ContinuousState<double>& xc = context->get_continuous_state();
   EXPECT_EQ(2, xc.size());
@@ -581,6 +584,9 @@ TEST_F(LeafSystemTest, DeclareVanillaMiscContinuousState) {
 // interesting custom type.
 TEST_F(LeafSystemTest, DeclareTypedMiscContinuousState) {
   system_.DeclareContinuousState(MyVector2d());
+
+  EXPECT_EQ(2, system_.get_num_continuous_states()); // test it before creating Context
+
   std::unique_ptr<Context<double>> context = system_.CreateDefaultContext();
   const ContinuousState<double>& xc = context->get_continuous_state();
   // Check that type was preserved.
@@ -596,6 +602,9 @@ TEST_F(LeafSystemTest, DeclareTypedMiscContinuousState) {
 // second-order structure.
 TEST_F(LeafSystemTest, DeclareVanillaContinuousState) {
   system_.DeclareContinuousState(4, 3, 2);
+
+  EXPECT_EQ(4 + 3 + 2, system_.get_num_continuous_states()); // test it before creating Context
+
   std::unique_ptr<Context<double>> context = system_.CreateDefaultContext();
   const ContinuousState<double>& xc = context->get_continuous_state();
   EXPECT_EQ(4 + 3 + 2, xc.size());
@@ -609,6 +618,9 @@ TEST_F(LeafSystemTest, DeclareVanillaContinuousState) {
 TEST_F(LeafSystemTest, DeclareTypedContinuousState) {
   using MyVector9d = MyVector<4 + 3 + 2, double>;
   system_.DeclareContinuousState(MyVector9d(), 4, 3, 2);
+
+  EXPECT_EQ(4 + 3 + 2, system_.get_num_continuous_states()); // test it before creating Context
+
   std::unique_ptr<Context<double>> context = system_.CreateDefaultContext();
   const ContinuousState<double>& xc = context->get_continuous_state();
   // Check that type was preserved.
