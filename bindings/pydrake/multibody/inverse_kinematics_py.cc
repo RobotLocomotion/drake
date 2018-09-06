fb52feec82c0d13a87fbf2d1c9c45a3f9f0b2844
hongkai-dai <daihongkai@gmail.com>
2018-09-06 15:47:11 -0700

    Run into a problem to bind InverseKinematics::q()

--- bindings/pydrake/multibody/inverse_kinematics_py.cc
+++ bindings/pydrake/multibody/inverse_kinematics_py.cc
@@ -10,12 +10,15 @@ namespace pydrake {
 
 using solvers::Binding;
 using solvers::Constraint;
+using solvers::MathematicalProgram;
 using multibody::InverseKinematics;
 using multibody::multibody_plant::MultibodyPlant;
 using multibody::Frame;
 
 void init_module(py::module m) {
   m.doc() = "InverseKinematics module";
+
+  py::object variable = py::module::import("pydrake.symbolic").attr("Variable");
 
   py::class_<InverseKinematics> ik_cls(m, "InverseKinematics");
   ik_cls.def(py::init<const MultibodyPlant<double>&>(), py::arg("plant"))
@@ -26,11 +29,30 @@ void init_module(py::module m) {
                const Eigen::Ref<const Eigen::Vector3d>&)>(
                &InverseKinematics::AddPositionConstraint),
            py::arg("frameB"), py::arg("p_BQ"), py::arg("frameA"),
-           py::arg("p_AQ_lower"), py::arg("p_AQ_upper"));
+           py::arg("p_AQ_lower"), py::arg("p_AQ_upper"))
+      .def("AddOrientationConstraint",
+           static_cast<Binding<Constraint> (InverseKinematics::*)(
+               const Frame<double>&, const Frame<double>&, double)>(
+               &InverseKinematics::AddOrientationConstraint),
+           py::arg("frameA"), py::arg("frameB"), py::arg("theta_bound"))
+      .def("prog", static_cast<const MathematicalProgram& (
+                       InverseKinematics::*)() const>(&InverseKinematics::prog))
+      .def("get_mutable_prog",
+           static_cast<MathematicalProgram* (InverseKinematics::*)() const>(
+               &InverseKinematics::get_mutable_prog))
+      .def("q", static_cast<const solvers::VectorXDecisionVariable& (
+                    InverseKinematics::*)() const>(&InverseKinematics::q));
+}
+
+void init_all(py::module m) {
+  py::dict vars = m.attr("__dict__");
+  py::exec("import pydrake.solvers.mathematicalprogram \n", py::globals(),
+           vars);
 }
 
 PYBIND11_MODULE(inverse_kinematics, m) {
   init_module(m);
+  init_all(m);
 }
 
 }  // namespace pydrake
--- bindings/pydrake/multibody/test/inverse_kinematics_test.py
+++ bindings/pydrake/multibody/test/inverse_kinematics_test.py
@@ -1,15 +1,13 @@
-from pydrake.multibody.multibody_tree.multibody_plant import (
-    MultibodyPlant,
-)
 from pydrake.multibody import inverse_kinematics as ik
+from pydrake.multibody.multibody_tree.multibody_plant import MultibodyPlant 
 from pydrake.multibody.multibody_tree.parsing import (
     AddModelFromSdfFile,
 )
 from pydrake.multibody.benchmarks.acrobot import (
     MakeAcrobotPlant,
 )
-
 import unittest
+import pydrake.solvers.mathematicalprogram as mp
 
 from pydrake.common import FindResourceOrThrow
 class TestInverseKinematics(unittest.TestCase):
@@ -24,6 +22,13 @@ class TestInverseKinematics(unittest.TestCase):
         self.link2_frame = self.plant.GetBodyByName("Link2").body_frame()
 
     def test_AddPositionConstraint(self):
-        ik_two_bodies = ik.InverseKinematics(self.plant)
-        ik_two_bodies.AddPositionConstraint(self.link1_frame, [0, 0.1, 0.2], self.link2_frame, [-1, -1, -1], [1, 1, 1])
+        ik_acrobot = ik.InverseKinematics(self.plant)
+        ik_acrobot.AddPositionConstraint(self.link1_frame, [0, 0.1, 0.2], self.link2_frame, [-1, -1, -1], [1, 1, 1])
+        result = ik_acrobot.get_mutable_prog().Solve()
+        self.assertEqual(result, mp.SolutionResult.kSolutionFound)
 
+    def test_AddOrientationConstraint(self):
+        ik_acrobot = ik.InverseKinematics(self.plant)
+        ik_acrobot.AddOrientationConstraint(self.link1_frame, self.link2_frame, 0.5 * math.pi)
+        result = ik_acrobot.get_mutable_prog().Solve()
+        self.assertEqual(result, mp.SolutionResult.kSolutionFound)
