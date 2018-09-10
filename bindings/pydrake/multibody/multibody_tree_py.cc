50a603ae575d7557771a48bdfbddb8f1d5bb3d56
amcastro-tri <alejandro.castro@tri.global>
2018-09-10 09:48:31 -0400

    Provides multibody state access in Python

--- bindings/pydrake/multibody/multibody_tree_py.cc
+++ bindings/pydrake/multibody/multibody_tree_py.cc
@@ -184,6 +184,18 @@ void init_module(py::module m) {
     py::class_<Class>(m, "MultibodyTree")
         .def("CalcRelativeTransform", &Class::CalcRelativeTransform,
              py::arg("context"), py::arg("frame_A"), py::arg("frame_B"))
+        .def("get_multibody_state_vector",
+             [](const MultibodyTree<T>* self,
+                const Context<T>& context) -> Eigen::Ref<const VectorX<T>> {
+               return self->get_multibody_state_vector(context);
+             }, py_reference_internal,
+             py::arg("context"))
+        .def("get_mutable_multibody_state_vector",
+             [](const MultibodyTree<T>* self,
+                Context<T>* context) -> Eigen::Ref<VectorX<T>> {
+               return self->get_mutable_multibody_state_vector(context);
+             }, py_reference_internal,
+             py::arg("context"))
         .def(
             "CalcPointsPositions",
             [](
