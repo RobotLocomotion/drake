e7f77a1190a0088b7c9251d8e734116c46f7880d
Soonho Kong <soonho.kong@tri.global>
2018-09-12 07:21:49 -0400

    symbolic uninterpreted_function takes a vector of expressions instead of a set of variables

--- common/symbolic_expression_cell.h
+++ common/symbolic_expression_cell.h
@@ -17,6 +17,7 @@
 #include <memory>
 #include <ostream>
 #include <string>
+#include <vector>
 
 #include <Eigen/Core>
 
@@ -789,10 +790,11 @@ class ExpressionIfThenElse : public ExpressionCell {
 /** Symbolic expression representing an uninterpreted function. */
 class ExpressionUninterpretedFunction : public ExpressionCell {
  public:
-  /** Constructs an uninterpreted-function expression from @p name and @p vars.
+  /** Constructs an uninterpreted-function expression from @p name and @p
+   * arguments.
    */
-  ExpressionUninterpretedFunction(const std::string& name,
-                                  const Variables& vars);
+  ExpressionUninterpretedFunction(std::string name,
+                                  std::vector<Expression> arguments);
   void HashAppendDetail(DelegatingHasher*) const override;
   Variables GetVariables() const override;
   bool EqualTo(const ExpressionCell& e) const override;
@@ -807,9 +809,12 @@ class ExpressionUninterpretedFunction : public ExpressionCell {
   /** Returns the name of this expression. */
   const std::string& get_name() const { return name_; }
 
+  /** Returns the arguments of this expression. */
+  const std::vector<Expression>& get_arguments() const { return arguments_; }
+
  private:
   const std::string name_;
-  const Variables variables_;
+  const std::vector<Expression> arguments_;
 };
 
 /** Checks if @p c is a constant expression. */
