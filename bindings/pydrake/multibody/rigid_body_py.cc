b004ad96a7c9acbc270c5a4e45aa73688d82dfeb
Jamie Snape <jamie.snape@kitware.com>
2018-09-26 12:11:45 -0400

    Add documentation for several pydrake multibody APIs
    Co-authored-by: Mmanu Chaturvedi <mmanu.chaturvedi@kitware.com>

--- bindings/pydrake/multibody/rigid_body_py.cc
+++ bindings/pydrake/multibody/rigid_body_py.cc
@@ -2,10 +2,13 @@
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
 
+#include "drake/bindings/pydrake/documentation_pybind.h"
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/multibody/joints/prismatic_joint.h"
 #include "drake/multibody/joints/revolute_joint.h"
 #include "drake/multibody/rigid_body.h"
+
+#define D(...) DOC(__VA_ARGS__)
 
 namespace drake {
 namespace pydrake {
@@ -15,28 +18,38 @@ PYBIND11_MODULE(rigid_body, m) {
 
   py::module::import("pydrake.multibody.joints");
 
-  py::class_<RigidBody<double> >(m, "RigidBody")
-    .def(py::init())
-    .def("get_body_index", &RigidBody<double>::get_body_index)
-    .def("get_center_of_mass", &RigidBody<double>::get_center_of_mass)
-    .def("get_name", &RigidBody<double>::get_name)
-    .def("set_name", &RigidBody<double>::set_name)
+  py::class_<RigidBody<double> >(m, "RigidBody", D(RigidBody))
+    .def(py::init(), D(RigidBody, RigidBody))
+    .def("get_body_index", &RigidBody<double>::get_body_index,
+         D(RigidBody, get_body_index))
+    .def("get_center_of_mass", &RigidBody<double>::get_center_of_mass,
+         D(RigidBody, get_center_of_mass))
+    .def("get_name", &RigidBody<double>::get_name, D(RigidBody, get_name))
+    .def("set_name", &RigidBody<double>::set_name, D(RigidBody, set_name))
     .def("get_position_start_index",
-         &RigidBody<double>::get_position_start_index)
-    .def("get_spatial_inertia", &RigidBody<double>::get_spatial_inertia)
-    .def("set_spatial_inertia", &RigidBody<double>::set_spatial_inertia)
+         &RigidBody<double>::get_position_start_index,
+         D(RigidBody, get_position_start_index))
+    .def("get_spatial_inertia", &RigidBody<double>::get_spatial_inertia,
+         D(RigidBody, get_spatial_inertia))
+    .def("set_spatial_inertia", &RigidBody<double>::set_spatial_inertia,
+         D(RigidBody, set_spatial_inertia))
 
-    .def("add_joint", &RigidBody<double>::add_joint<DrakeJoint>)
-    .def("has_joint", &RigidBody<double>::has_joint)
+    .def("add_joint", &RigidBody<double>::add_joint<DrakeJoint>,
+         D(RigidBody, add_joint))
+    .def("has_joint", &RigidBody<double>::has_joint, D(RigidBody, has_joint))
     .def("getJoint", &RigidBody<double>::getJoint,
-         py::return_value_policy::reference_internal)
+         py::return_value_policy::reference_internal, D(RigidBody, getJoint))
 
-    .def("get_visual_elements", &RigidBody<double>::get_visual_elements)
-    .def("AddVisualElement", &RigidBody<double>::AddVisualElement)
+    .def("get_visual_elements", &RigidBody<double>::get_visual_elements,
+         D(RigidBody, get_visual_elements))
+    .def("AddVisualElement", &RigidBody<double>::AddVisualElement,
+         D(RigidBody, AddVisualElement))
     .def("get_num_collision_elements",
-         &RigidBody<double>::get_num_collision_elements)
+         &RigidBody<double>::get_num_collision_elements,
+         D(RigidBody, get_num_collision_elements))
     .def("get_collision_element_ids",
-         &RigidBody<double>::get_collision_element_ids);
+         &RigidBody<double>::get_collision_element_ids,
+         D(RigidBody, get_collision_element_ids));
 }
 
 }  // namespace pydrake
