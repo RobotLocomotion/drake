ac54fd7a875a968208d3d8c7e37c7330bee53e20
Jamie Snape <jamie.snape@kitware.com>
2018-10-03 09:29:11 -0400

    Add documentation to pydrake.multibody.* modules
    Co-authored-by: Mmanu Chaturvedi <mmanu.chaturvedi@kitware.com>

--- bindings/pydrake/multibody/joints_py.cc
+++ bindings/pydrake/multibody/joints_py.cc
@@ -2,11 +2,14 @@
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
 
+#include "drake/bindings/pydrake/documentation_pybind.h"
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/multibody/joints/drake_joint.h"
 #include "drake/multibody/joints/fixed_axis_one_dof_joint.h"
 #include "drake/multibody/joints/prismatic_joint.h"
 #include "drake/multibody/joints/revolute_joint.h"
+
+auto& doc = pydrake_doc;
 
 namespace drake {
 namespace pydrake {
@@ -14,25 +17,30 @@ namespace pydrake {
 PYBIND11_MODULE(joints, m) {
   m.doc() = "Joint types supported by Drake.";
 
-  py::class_<DrakeJoint>(m, "DrakeJoint")
-    .def("get_num_positions", &DrakeJoint::get_num_positions)
-    .def("get_name", &DrakeJoint::get_name);
+  py::class_<DrakeJoint>(m, "DrakeJoint", doc.DrakeJoint.doc)
+    .def("get_num_positions", &DrakeJoint::get_num_positions,
+         doc.DrakeJoint.get_num_positions.doc)
+    .def("get_name", &DrakeJoint::get_name, doc.DrakeJoint.get_name.doc);
 
-  py::class_<PrismaticJoint, DrakeJoint>(m, "PrismaticJoint")
+  py::class_<PrismaticJoint, DrakeJoint>(m, "PrismaticJoint",
+                                         doc.PrismaticJoint.doc)
     .def(py::init<const std::string&,
                   const Eigen::Isometry3d&,
                   const Eigen::Vector3d&>(),
          py::arg("name"),
          py::arg("transform_to_parent_body"),
-         py::arg("translation_axis"));
+         py::arg("translation_axis"),
+         doc.PrismaticJoint.ctor.doc);
 
-  py::class_<RevoluteJoint, DrakeJoint>(m, "RevoluteJoint")
+  py::class_<RevoluteJoint, DrakeJoint>(
+      m, "RevoluteJoint", doc.RevoluteJoint.doc)
     .def(py::init<const std::string&,
                   const Eigen::Isometry3d&,
                   const Eigen::Vector3d&>(),
          py::arg("name"),
          py::arg("transform_to_parent_body"),
-         py::arg("rotation_axis"));
+         py::arg("rotation_axis"),
+         doc.RevoluteJoint.ctor.doc);
 }
 
 }  // namespace pydrake
