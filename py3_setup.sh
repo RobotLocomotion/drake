253627ea9ca663c6a73539280e6a785b52dae13e
Eric Cousineau <eric.cousineau@tri.global>
2018-10-16 22:30:04 -0400

    Simplify

--- py3_setup.sh
+++ py3_setup.sh
@@ -16,17 +16,17 @@ if [[ ! -f build/py3/bin/python3 ]]; then
 fi
 
 source build/py3/bin/activate
+_python_bin=$(which python3)
 
-_py_arg="--python_path=$(which python3) --action_env=PYTHON_BIN_PATH=$(which python3)"
-bazel-build() { (
+bazel-py3() { (
+    set -eu
+    cmd=${1}
+    shift
     set -x
-    bazel build ${_py_arg} "$@"
+    bazel ${cmd} \
+        --python_path=${_python_bin} \
+        --action_env=PYTHON_BIN_PATH=${_python_bin} \
+        "$@"
 ) }
-bazel-run() { (
-    set -x
-    bazel run ${_py_arg} "$@"
-) }
-bazel-test() { (
-    set -x
-    bazel test ${_py_arg} "$@"
-) }
+
+complete -F _bazel__complete -o nospace "bazel-py3"
