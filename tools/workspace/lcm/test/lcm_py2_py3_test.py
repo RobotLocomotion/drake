c1f1feda6aba20b2ce6f091bcc3f0e163d2a6bec
Eric Cousineau <eric.cousineau@tri.global>
2018-10-17 18:48:14 -0400

    Add explicit LCM unittest

---
+++ tools/workspace/lcm/test/lcm_py2_py3_test.py
@@ -0,0 +1,11 @@
+from subprocess import check_call
+import unittest
+
+# This will check the Bazel Python version (2 or 3).
+from lcm import LCM
+
+
+class TestLcmPython2and3(unittest.TestCase):
+    def test_python2(self):
+        # This explicitly checks Python2.
+        check_call(["python2", "-c", "from lcm import LCM"])
