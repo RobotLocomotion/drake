e6570dc2312ec7cfd180fcbf9e6878f8f4ed7e58
Russ Tedrake <russt@mit.edu>
2018-10-18 08:13:54 -0400

    Updated to support more robust discrete- continuous-time interactions

--- systems/primitives/discrete_derivative.cc
+++ systems/primitives/discrete_derivative.cc
@@ -1,6 +1,68 @@
 #include "drake/systems/primitives/discrete_derivative.h"
 
+#include <memory>
+#include <vector>
+
 #include "drake/common/default_scalars.h"
+
+namespace drake {
+namespace systems {
+
+using Eigen::MatrixXd;
+
+template <typename T>
+DiscreteDerivative<T>::DiscreteDerivative(int num_inputs, double time_step)
+    : LeafSystem<T>(SystemTypeTag<systems::DiscreteDerivative>{}),
+      n_(num_inputs),
+      time_step_(time_step) {
+  DRAKE_DEMAND(n_ > 0);
+  DRAKE_DEMAND(time_step_ > 0.0);
+
+  this->DeclareVectorInputPort("u", systems::BasicVector<T>(n_));
+  this->DeclareVectorOutputPort("dudt", systems::BasicVector<T>(n_),
+                                &DiscreteDerivative<T>::CalcOutput);
+
+  // TODO(sherm): Prefer two state vectors of size n_ upon resolution of #9705.
+  this->DeclareDiscreteState(2 * n_);
+  this->DeclarePeriodicDiscreteUpdate(time_step_);
+}
+
+template <typename T>
+void DiscreteDerivative<T>::set_state(
+    const Eigen::Ref<const drake::VectorX<T>>& u,
+    drake::systems::Context<T>* context) const {
+  DRAKE_DEMAND(u.size() == n_);
+
+  context->get_mutable_discrete_state_vector().get_mutable_value() << u, u;
+}
+
+template <typename T>
+void DiscreteDerivative<T>::DoCalcDiscreteVariableUpdates(
+    const drake::systems::Context<T>& context,
+    const std::vector<const drake::systems::DiscreteUpdateEvent<T>*>&,
+    drake::systems::DiscreteValues<T>* discrete_state) const {
+  // x₀[n+1] = u[n].
+  discrete_state->get_mutable_vector().get_mutable_value().head(n_) =
+      this->EvalEigenVectorInput(context, 0);
+
+  // x₁[n+1] = x₀[n].
+  discrete_state->get_mutable_vector().get_mutable_value().tail(n_) =
+      context.get_discrete_state(0).get_value().head(n_);
+}
+
+template <typename T>
+void DiscreteDerivative<T>::CalcOutput(
+    const drake::systems::Context<T>& context,
+    drake::systems::BasicVector<T>* output_vector) const {
+  const auto x0 = context.get_discrete_state(0).get_value().head(n_);
+  const auto x1 = context.get_discrete_state(0).get_value().tail(n_);
+
+  // y(t) = (x₀[n]-x₁[n])/h.
+  output_vector->SetFromVector((x0 - x1) / time_step_);
+}
+
+}  // namespace systems
+}  // namespace drake
 
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
     class ::drake::systems::DiscreteDerivative)
--- systems/primitives/discrete_derivative.h
+++ systems/primitives/discrete_derivative.h
@@ -1,23 +1,37 @@
 #pragma once
 
-#include "drake/systems/primitives/linear_system.h"
+#include <memory>
+#include <vector>
 
-using Eigen::MatrixXd;
+#include "drake/systems/framework/leaf_system.h"
 
 namespace drake {
 namespace systems {
 
-/// System that outputs the discrete-time derivative of the input:
-///   @f$ y[n] = (u[n] - u[n-1])/h @f$
+/// System that outputs the discrete-time derivative of its input:
+///   y(t) = (u[n] - u[n-1])/h,
+/// where n = floor(t/h), where h is the time period.
 ///
 /// This is implemented as the linear system
-///   @f[ x[n+1] = u[n], @f]
-///   @f[ y[n] = (u[n]-x[n])/h, @f]
-/// where @f$ h @f$ is the time period.
+/// <pre>
+///   x₀[n+1] = u[n],
+///   x₁[n+1] = x₀[n],
+///   y(t) = (x₀[n]-x₁[n])/h.
+///   x₀[0] and x₁[0] are initialized in the Context (default is zeros).
+/// </pre>
 ///
-/// @system{ DiscreteDerivative, @input_port{u(t)}, @output_port{du(t)/dt} }
+/// Note: The reason for this specific implementation is a little subtle.
+/// Since drake systems do not control the evaluation time of their output
+/// ports, a downstream system may ask for the output at any continuous time.
+/// The estimate y(t) = (u(t) - u[n])/(t-n*h), which is a more
+/// continuous-time derivative, would require fewer state variables, and
+/// introduce less delay.  But the inconsistent time interval (which could be
+/// arbitrarily close to zero) makes it numerically unreliable.  Prefer the
+/// discrete-time derivative implemented here.
 ///
-/// @tparam T The type of mathematical object being added.
+/// @system{ DiscreteDerivative, @input_port{u}, @output_port{dudt} }
+///
+/// @tparam T The underlying scalar type. Must be a valid Eigen scalar.
 ///
 /// Instantiated templates for the following kinds of T's are provided:
 /// - double
@@ -26,25 +40,48 @@ namespace systems {
 ///
 /// @ingroup primitive_systems
 template <class T>
-class DiscreteDerivative final : public LinearSystem<T> {
+class DiscreteDerivative final : public LeafSystem<T> {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(DiscreteDerivative)
 
-  DiscreteDerivative(int num_inputs, double time_period)
-      : LinearSystem<T>(
-            SystemTypeTag<systems::DiscreteDerivative>{},
-            MatrixXd::Zero(num_inputs, num_inputs),
-            MatrixXd::Identity(num_inputs, num_inputs),
-            -MatrixXd::Identity(num_inputs, num_inputs) / time_period,
-            MatrixXd::Identity(num_inputs, num_inputs) / time_period,
-            time_period) {
-    DRAKE_DEMAND(time_period > 0.0);
-  }
+  /// Constructor taking @p num_inputs, the size of the vector to be
+  /// differentiated, and @p time_step, the sampling interval.
+  DiscreteDerivative(int num_inputs, double time_step);
 
   /// Scalar-converting copy constructor.  See @ref system_scalar_conversion.
   template <typename U>
   explicit DiscreteDerivative(const DiscreteDerivative<U>& other)
-      : DiscreteDerivative<T>(other.A().cols(), 1. / other.D()(0, 0)) {}
+      : DiscreteDerivative<T>(other.get_input_port().size(),
+                              other.time_step()) {}
+
+  /// Convenience method to set the entire input history to a constant
+  /// vector value (x₀ = x₁ = u,resulting in a derivative = 0).  This is
+  /// useful during initialization to avoid large derivative outputs if
+  /// u[0] ≠ 0.  @p u must be the same size as the input/output ports.
+  void set_state(const Eigen::Ref<const VectorX<T>>& u,
+                   systems::Context<T>* context) const;
+
+  const systems::InputPort<T>& get_input_port() const {
+    return System<T>::get_input_port(0);
+  }
+
+  const systems::OutputPort<T>& get_output_port() const {
+    return System<T>::get_output_port(0);
+  }
+
+  double time_step() const { return time_step_; }
+
+ private:
+  void DoCalcDiscreteVariableUpdates(
+      const Context<T>& context,
+      const std::vector<const DiscreteUpdateEvent<T>*>&,
+      DiscreteValues<T>* discrete_state) const final;
+
+  void CalcOutput(const Context<T>& context,
+                  BasicVector<T>* output_vector) const;
+
+  const int n_;  // The size of the input (and output) ports.
+  const double time_step_;
 };
 
 }  // namespace systems
