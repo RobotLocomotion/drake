0cb6623a4df9f2de451f1f75f90090ea2158647e
Jamie Snape <jamie.snape@kitware.com>
2018-10-19 11:06:02 -0400

    Update and expand mkdoc.py unit test

--- tools/workspace/pybind11/test/sample_header.h
+++ tools/workspace/pybind11/test/sample_header.h
@@ -1,34 +1,69 @@
 #pragma once
 
+/// @file
+/// Sit amet nisl purus in mollis nunc sed id semper. Risus nec feugiat in
+/// fermentum posuere urna nec tincidunt praesent. Suscipit tellus mauris a
+/// diam.
+
 #include <vector>
 
 #include "drake/common/drake_copyable.h"
+#include "drake/common/unused.h"
 
-/// Root-level symbol.
+/// Root-level symbol. Magna fermentum iaculis eu non diam phasellus
+/// vestibulum.
 struct RootLevelSymbol {};
 
 namespace drake {
 
 /**
- * Mid-level symbol.
- * Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
- * aliquip ex ea commodo consequat.
+ * Mid-level symbol. Ut enim ad minim veniam, quis nostrud exercitation
+ * ullamco laboris nisi ut aliquip ex ea commodo consequat.
+ *
+ * 1. Begin first ordered list element. Rutrum quisque non tellus orci ac
+ *    auctor. End first ordered list element.
+ * 2. Begin second ordered list element. Ipsum faucibus vitae aliquet nec.
+ *    Ligula ullamcorper malesuada proin libero. End second ordered list
+ *    element.
+ * 3. Begin third ordered list element. Dictum sit amet justo donec enim.
+ *    Pharetra convallis posuere morbi leo urna molestie. End third ordered
+ *    list element.
+ *
+ * @version 1.0.
  */
 struct MidLevelSymbol {};
 
 namespace mkdoc_test {
 
-/// Function.
+/** Function. Mi sit amet mauris commodo quis. */
 void func();
-/// Function, overload 1.
-void func(int);
-/// Function, template overload.
+/// Function, overload 1. Velit ut tortor pretium viverra suspendisse potenti
+/// nullam ac tortor.
+/// @throws std::exception Begin raises. Morbi tincidunt augue interdum velit
+/// euismod. Justo nec ultrices dui sapien eget mi proin sed libero. End
+/// raises.
+void func(int* param);
+/// Function, template overload. Pellentesque diam volutpat commodo sed egestas
+/// egestas fringilla phasellus faucibus.
+/// @param[in,out] param Begin input/output parameter. Morbi enim nunc faucibus
+/// a pellentesque sit. End input/output parameter.
 template <typename T>
 void func(T);
 
 /// Class.
 /// Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
 /// tempor incididunt ut labore et dolore magna aliqua.
+///
+/// * Begin first unordered list element. Volutpat blandit aliquam etiam erat
+///   velit scelerisque. End first unordered list element.
+/// * Begin second unordered list element. Eget est lorem ipsum dolor sit amet.
+///   Ipsum dolor sit amet consectetur adipiscing. End second unordered list
+///   element.
+/// * Begin third unordered list element. Hac habitasse platea dictumst quisque
+///   sagittis purus sit. End third unordered list element.
+///
+/// Quisque sagittis purus sit amet volutpat.
+/// @sa Struct
 class Class {
  public:
   DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(Class)
@@ -36,79 +71,155 @@ class Class {
   // TODO(eric.cousineau): Make `mkdoc` recognize documentation for aliases. At
   // present, neither `UsingAlias` nor `TypedefAlias` show up.
 
-  /// Using alias.
+  /// Using alias. @a Italics. Sit amet nisl purus in mollis nunc sed id
+  /// semper.
   using UsingAlias = std::vector<Class>;
 
-  /// Typedef alias.
+  /// Typedef alias. @b Bold. Risus nec feugiat in fermentum posuere urna nec
+  /// tincidunt praesent.
   typedef std::vector<Class> TypedefAlias;
 
   /// Custom constructor 1.
   Class() {}
 
-  /// Custom constructor 2.
-  explicit Class(int) {}
+  /// Custom constructor 2. @e Italics. Ut tristique et egestas quis ipsum
+  /// suspendisse ultrices gravida. @c Typewriter. Suscipit tellus mauris a
+  /// diam. Maecenas accumsan lacus vel facilisis volutpat est.
+  /// @param param Begin parameter. Dignissim diam quis enim lobortis
+  /// scelerisque fermentum dui faucibus. End parameter.
+  ///
+  /// Ut consequat semper viverra nam libero.
+  /// @code{.cpp}
+  /// Class class();
+  /// class.PublicMethod();
+  /// @endcode
+  explicit Class(int param) {
+    unused(param);
+  }
 
-  /// Custom constructor 3.
-  explicit Class(int, std::vector<int>) {}
+  /// Custom constructor 3. @em Italics. Integer quis auctor elit sed vulputate
+  /// mi sit.
+  /// @param[in] param1 Begin first input parameter. Mollis nunc sed id semper
+  /// risus in hendrerit gravida rutrum. End first input parameter.
+  /// @param[in] param2 Begin second input parameter. Tristique senectus et
+  /// netus et malesuada fames ac turpis. End second input parameter.
+  explicit Class(int param1, std::vector<int> param2) {
+    unused(param1);
+    unused(param2);
+  }
 
-  /// Public method.
+  /// Public method. @f$ A = \pi r^2 @f$. Condimentum id venenatis a
+  /// condimentum vitae sapien pellentesque habitant morbi.
+  /// @cond
+  /// Private documentation. Tortor vitae purus faucibus ornare suspendisse.
+  /// Orci dapibus ultrices in iaculis.
+  /// @endcond
   void PublicMethod() {}
 
-  /// Public template method.
+  // Private documentation.
+  /// Public template method. @f[ a^2 + b^2 = c^2. @f] Sagittis id consectetur
+  /// purus ut faucibus pulvinar.
+  /// @tparam T Begin template parameter. acilisi etiam dignissim diam quis. Ut
+  /// pharetra sit amet aliquam. End template parameter.
   template <typename T>
   void PublicTemplateMethod() {}
 
-  /// Static method.
+  /// Static method. @p Typewriter. Sed faucibus turpis in eu mi bibendum neque
+  /// egestas.
+  /// @invariant Begin invariant. Odio euismod lacinia at quis risus sed
+  /// vulputate odio. End invariant.
+  /// @pre Begin precondition. Cras fermentum odio eu feugiat pretium nibh.
+  /// Ornare suspendisse sed nisi lacus sed viverra tellus. End precondition.
+  /// @post Begin postcondition. Tortor id aliquet lectus proin nibh nisl
+  /// condimentum id. End postcondition.
   static void PublicStatic() {}
 
  protected:
-  /// Protected method.
-  void ProtectedMethod() {}
+  /// Protected method. Nibh sed pulvinar proin gravida hendrerit. Orci
+  /// phasellus egestas tellus rutrum tellus pellentesque eu.
+  /// @returns Begin returns. Faucibus interdum posuere lorem ipsum dolor.
+  /// Malesuada proin libero nunc consequat interdum varius sit amet. End
+  /// returns.
+  int ProtectedMethod() {
+    return 0;
+  }
 
-  /// Protected nested class.
+  /// Protected nested class. Sed turpis tincidunt id aliquet. Egestas sed sed
+  /// risus pretium.
+  /// @bug Begin bug report. Cras pulvinar mattis nunc sed blandit libero. Eget
+  /// est lorem ipsum dolor sit amet consectetur. End bug report.
+  /// @test Begin test case. Sem integer vitae justo eget magna fermentum.
+  /// Convallis posuere morbi leo urna molestie at elementum eu facilisis. End
+  /// test case.
   class Nested {};
 
-  /// Protected member.
+  /// Protected member. Porttitor eget dolor morbi non arcu risus quis varius
+  /// quam.
   int protected_member_{};
 
  private:
-  /// Private method, public documentation.
+  /// Private method, public documentation. Senectus et netus et malesuada
+  /// fames ac.
+  /// @todo Begin TODO. Ut tellus elementum sagittis vitae et leo duis ut diam.
+  /// Et malesuada fames ac turpis. End TODO.
   void PrivateMethod() {}
 
   // Private member, private documentation.
   int private_member_{};
 };
 
-/// Struct.
+/// Struct. Sed elementum tempus egestas sed sed risus pretium. Vel pharetra
+/// vel turpis nunc.
+/// @deprecated Begin deprecated. Est pellentesque elit ullamcorper dignissim
+/// cras tincidunt lobortis. End deprecated.
+/// @see Class
 struct Struct {
-  /// Field 1.
+  /// Field 1. Sit amet cursus sit amet dictum sit amet. Id leo in vitae turpis
+  /// massa sed elementum tempus.
+  /// @attention Begin attention. Ultricies lacus sed turpis tincidunt id
+  /// aliquet risus feugiat.  End attention.
   int field_1;
-  /// Field 2.
+  /// Field 2. Consectetur libero id faucibus nisl tincidunt eget nullam non
+  /// nisi.
+  /// @note Begin note. Consectetur a erat nam at lectus. Consequat ac felis
+  /// donec et odio pellentesque diam. End note.
   std::vector<int> field_2;
 };
 
-/// Template class.
+/// Template class. Mauris pharetra et ultrices neque ornare aenean euismod
+/// elementum.
+/// @tparam T Begin template parameter. Feugiat scelerisque varius morbi enim
+/// nunc faucibus a. End template parameter.
 template <typename T>
 class TemplateClass {
  public:
-  /// Default constructor.
+  /// Default constructor. Condimentum mattis pellentesque id nibh tortor id.
+  /// Nisl rhoncus mattis rhoncus urna neque.
+  /// @remarks Begin remarks. Ante metus dictum at tempor commodo. Nec feugiat
+  /// in fermentum posuere urna nec. End remarks.
   TemplateClass() {}
 };
 
-/// Specialize.
+/// Specialize. Nisl pretium fusce id velit ut tortor pretium viverra. Quis
+/// ipsum suspendisse ultrices gravida dictum fusce ut.
+/// @since Begin since. Nullam eget felis eget nunc lobortis mattis aliquam
+/// faucibus. End since.
 template <>
 class TemplateClass<int> {
 };
 
-/// Enumeration.
+/// Enumeration. Feugiat scelerisque varius morbi enim.  Facilisis leo vel
+/// fringilla est ullamcorper eget nulla facilisi.
 enum Enum {
   /// Enumeration constant.
   EnumConstant,
 };
 
-/// Enumeration class.
+/// Enumeration class. Malesuada fames ac turpis egestas integer eget aliquet
+/// nibh praesent.
 enum EnumClass {
-  /// Enumeration class constant.
+  /// Enumeration class constant. Vestibulum mattis ullamcorper velit sed
+  /// ullamcorper.
   EnumClassConstant,
 };
 
--- tools/workspace/pybind11/test/sample_header_documentation.expected.h
+++ tools/workspace/pybind11/test/sample_header_documentation.expected.h
@@ -15,135 +15,306 @@
 constexpr struct /* sample_header_doc */ {
   // Symbol: RootLevelSymbol
   struct /* RootLevelSymbol */ {
-    // Source: drake/tools/workspace/pybind11/test/sample_header.h:8
-    const char* doc = R"""(Root-level symbol.)""";
+    // Source: drake/tools/workspace/pybind11/test/sample_header.h:15
+    const char* doc =
+R"""(Root-level symbol. Magna fermentum iaculis eu non diam phasellus
+vestibulum.)""";
   } RootLevelSymbol;
   // Symbol: drake
   struct /* drake */ {
     // Symbol: drake::MidLevelSymbol
     struct /* MidLevelSymbol */ {
-      // Source: drake/tools/workspace/pybind11/test/sample_header.h:17
+      // Source: drake/tools/workspace/pybind11/test/sample_header.h:34
       const char* doc =
 R"""(Mid-level symbol. Ut enim ad minim veniam, quis nostrud exercitation
-ullamco laboris nisi ut aliquip ex ea commodo consequat.)""";
+ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+1. Begin first ordered list element. Rutrum quisque non tellus orci ac
+   auctor. End first ordered list element.
+2. Begin second ordered list element. Ipsum faucibus vitae aliquet nec.
+   Ligula ullamcorper malesuada proin libero. End second ordered list
+   element.
+3. Begin third ordered list element. Dictum sit amet justo donec enim.
+   Pharetra convallis posuere morbi leo urna molestie. End third ordered
+   list element.
+
+Version:
+    1.0.)""";
     } MidLevelSymbol;
     // Symbol: drake::mkdoc_test
     struct /* mkdoc_test */ {
       // Symbol: drake::mkdoc_test::AnonymousConstant
       struct /* AnonymousConstant */ {
-        // Source: drake/tools/workspace/pybind11/test/sample_header.h:118
+        // Source: drake/tools/workspace/pybind11/test/sample_header.h:229
         const char* doc = R"""(Anonymous enum's constant.)""";
       } AnonymousConstant;
       // Symbol: drake::mkdoc_test::Class
       struct /* Class */ {
-        // Source: drake/tools/workspace/pybind11/test/sample_header.h:32
+        // Source: drake/tools/workspace/pybind11/test/sample_header.h:67
         const char* doc =
 R"""(Class. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-eiusmod tempor incididunt ut labore et dolore magna aliqua.)""";
+eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+* Begin first unordered list element. Volutpat blandit aliquam etiam erat
+  velit scelerisque. End first unordered list element.
+* Begin second unordered list element. Eget est lorem ipsum dolor sit amet.
+  Ipsum dolor sit amet consectetur adipiscing. End second unordered list
+  element.
+* Begin third unordered list element. Hac habitasse platea dictumst quisque
+  sagittis purus sit. End third unordered list element.
+
+Quisque sagittis purus sit amet volutpat.
+
+See also:
+    Struct)""";
         // Symbol: drake::mkdoc_test::Class::Class
         struct /* ctor */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:34
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:69
           const char* doc = R"""()""";
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:34
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:69
           const char* doc_2 = R"""()""";
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:46
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:83
           const char* doc_3 = R"""(Custom constructor 1.)""";
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:49
-          const char* doc_4 = R"""(Custom constructor 2.)""";
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:52
-          const char* doc_5 = R"""(Custom constructor 3.)""";
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:96
+          const char* doc_4 =
+R"""(Custom constructor 2. *Italics*. Ut tristique et egestas quis ipsum
+suspendisse ultrices gravida. ``Typewriter``. Suscipit tellus mauris a
+diam. Maecenas accumsan lacus vel facilisis volutpat est.
+
+Parameter ``param``:
+    Begin parameter. Dignissim diam quis enim lobortis scelerisque
+    fermentum dui faucibus. End parameter.
+
+Ut consequat semper viverra nam libero.
+
+
+::
+
+    Class class();
+    class.PublicMethod();)""";
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:106
+          const char* doc_5 =
+R"""(Custom constructor 3. *Italics*. Integer quis auctor elit sed
+vulputate mi sit.
+
+Parameter ``param1``:
+    Begin first input parameter. Mollis nunc sed id semper risus in
+    hendrerit gravida rutrum. End first input parameter.
+
+Parameter ``param2``:
+    Begin second input parameter. Tristique senectus et netus et
+    malesuada fames ac turpis. End second input parameter.)""";
         } ctor;
         // Symbol: drake::mkdoc_test::Class::DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE
         struct /* DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:34
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:69
           const char* doc = R"""()""";
         } DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE;
         // Symbol: drake::mkdoc_test::Class::Nested
         struct /* Nested */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:69
-          const char* doc = R"""(Protected nested class.)""";
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:154
+          const char* doc =
+R"""(Protected nested class. Sed turpis tincidunt id aliquet. Egestas sed
+sed risus pretium.
+
+Bug report:
+    Begin bug report. Cras pulvinar mattis nunc sed blandit libero.
+    Eget est lorem ipsum dolor sit amet consectetur. End bug report.
+
+Test case:
+    Begin test case. Sem integer vitae justo eget magna fermentum.
+    Convallis posuere morbi leo urna molestie at elementum eu
+    facilisis. End test case.)""";
         } Nested;
         // Symbol: drake::mkdoc_test::Class::ProtectedMethod
         struct /* ProtectedMethod */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:66
-          const char* doc = R"""(Protected method.)""";
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:143
+          const char* doc =
+R"""(Protected method. Nibh sed pulvinar proin gravida hendrerit. Orci
+phasellus egestas tellus rutrum tellus pellentesque eu.
+
+Returns:
+    Begin returns. Faucibus interdum posuere lorem ipsum dolor.
+    Malesuada proin libero nunc consequat interdum varius sit amet.
+    End returns.)""";
         } ProtectedMethod;
         // Symbol: drake::mkdoc_test::Class::PublicMethod
         struct /* PublicMethod */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:55
-          const char* doc = R"""(Public method.)""";
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:117
+          const char* doc =
+R"""(Public method. :math:`A = \pi r^2`. Condimentum id venenatis a
+condimentum vitae sapien pellentesque habitant morbi.)""";
         } PublicMethod;
         // Symbol: drake::mkdoc_test::Class::PublicStatic
         struct /* PublicStatic */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:62
-          const char* doc = R"""(Static method.)""";
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:135
+          const char* doc =
+R"""(Static method. ``Typewriter``. Sed faucibus turpis in eu mi bibendum
+neque egestas.
+
+Invariant:
+    Begin invariant. Odio euismod lacinia at quis risus sed vulputate
+    odio. End invariant.
+
+Precondition:
+    Begin precondition. Cras fermentum odio eu feugiat pretium nibh.
+    Ornare suspendisse sed nisi lacus sed viverra tellus. End
+    precondition.
+
+Postcondition:
+    Begin postcondition. Tortor id aliquet lectus proin nibh nisl
+    condimentum id. End postcondition.)""";
         } PublicStatic;
         // Symbol: drake::mkdoc_test::Class::PublicTemplateMethod
         struct /* PublicTemplateMethod */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:59
-          const char* doc = R"""(Public template method.)""";
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:125
+          const char* doc =
+R"""(Public template method.
+
+.. math:: a^2 + b^2 = c^2.
+
+Sagittis id consectetur purus ut faucibus pulvinar.
+
+Template parameter ``T``:
+    Begin template parameter. acilisi etiam dignissim diam quis. Ut
+    pharetra sit amet aliquam. End template parameter.)""";
         } PublicTemplateMethod;
+        // Symbol: drake::mkdoc_test::Class::TypedefAlias
+        struct /* TypedefAlias */ {
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:80
+          const char* doc =
+R"""(Typedef alias. **Bold**. Risus nec feugiat in fermentum posuere urna
+nec tincidunt praesent.)""";
+        } TypedefAlias;
+        // Symbol: drake::mkdoc_test::Class::UsingAlias
+        struct /* UsingAlias */ {
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:76
+          const char* doc =
+R"""(Using alias. *Italics*. Sit amet nisl purus in mollis nunc sed id
+semper.)""";
+        } UsingAlias;
         // Symbol: drake::mkdoc_test::Class::protected_member_
         struct /* protected_member_ */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:72
-          const char* doc = R"""(Protected member.)""";
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:158
+          const char* doc =
+R"""(Protected member. Porttitor eget dolor morbi non arcu risus quis
+varius quam.)""";
         } protected_member_;
       } Class;
       // Symbol: drake::mkdoc_test::Enum
       struct /* Enum */ {
-        // Source: drake/tools/workspace/pybind11/test/sample_header.h:104
-        const char* doc = R"""(Enumeration.)""";
+        // Source: drake/tools/workspace/pybind11/test/sample_header.h:213
+        const char* doc =
+R"""(Enumeration. Feugiat scelerisque varius morbi enim. Facilisis leo vel
+fringilla est ullamcorper eget nulla facilisi.)""";
         // Symbol: drake::mkdoc_test::Enum::EnumConstant
         struct /* EnumConstant */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:106
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:215
           const char* doc = R"""(Enumeration constant.)""";
         } EnumConstant;
       } Enum;
       // Symbol: drake::mkdoc_test::EnumClass
       struct /* EnumClass */ {
-        // Source: drake/tools/workspace/pybind11/test/sample_header.h:110
-        const char* doc = R"""(Enumeration class.)""";
+        // Source: drake/tools/workspace/pybind11/test/sample_header.h:220
+        const char* doc =
+R"""(Enumeration class. Malesuada fames ac turpis egestas integer eget
+aliquet nibh praesent.)""";
         // Symbol: drake::mkdoc_test::EnumClass::EnumClassConstant
         struct /* EnumClassConstant */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:112
-          const char* doc = R"""(Enumeration class constant.)""";
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:223
+          const char* doc =
+R"""(Enumeration class constant. Vestibulum mattis ullamcorper velit sed
+ullamcorper.)""";
         } EnumClassConstant;
       } EnumClass;
       // Symbol: drake::mkdoc_test::Struct
       struct /* Struct */ {
-        // Source: drake/tools/workspace/pybind11/test/sample_header.h:83
-        const char* doc = R"""(Struct.)""";
+        // Source: drake/tools/workspace/pybind11/test/sample_header.h:176
+        const char* doc =
+R"""(Struct. Sed elementum tempus egestas sed sed risus pretium. Vel
+pharetra vel turpis nunc.
+
+Deprecated:
+    Begin deprecated. Est pellentesque elit ullamcorper dignissim cras
+    tincidunt lobortis. End deprecated.
+
+See also:
+    Class)""";
         // Symbol: drake::mkdoc_test::Struct::field_1
         struct /* field_1 */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:85
-          const char* doc = R"""(Field 1.)""";
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:181
+          const char* doc =
+R"""(Field 1. Sit amet cursus sit amet dictum sit amet. Id leo in vitae
+turpis massa sed elementum tempus.
+
+Attention:
+    Begin attention. Ultricies lacus sed turpis tincidunt id aliquet
+    risus feugiat. End attention.)""";
         } field_1;
         // Symbol: drake::mkdoc_test::Struct::field_2
         struct /* field_2 */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:87
-          const char* doc = R"""(Field 2.)""";
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:186
+          const char* doc =
+R"""(Field 2. Consectetur libero id faucibus nisl tincidunt eget nullam non
+nisi.
+
+Note:
+    Begin note. Consectetur a erat nam at lectus. Consequat ac felis
+    donec et odio pellentesque diam. End note.)""";
         } field_2;
       } Struct;
       // Symbol: drake::mkdoc_test::TemplateClass
       struct /* TemplateClass */ {
-        // Source: drake/tools/workspace/pybind11/test/sample_header.h:92
-        const char* doc = R"""(Template class.)""";
-        // Source: drake/tools/workspace/pybind11/test/sample_header.h:100
-        const char* doc_2 = R"""(Specialize.)""";
+        // Source: drake/tools/workspace/pybind11/test/sample_header.h:194
+        const char* doc =
+R"""(Template class. Mauris pharetra et ultrices neque ornare aenean
+euismod elementum.
+
+Template parameter ``T``:
+    Begin template parameter. Feugiat scelerisque varius morbi enim
+    nunc faucibus a. End template parameter.)""";
+        // Source: drake/tools/workspace/pybind11/test/sample_header.h:208
+        const char* doc_2 =
+R"""(Specialize. Nisl pretium fusce id velit ut tortor pretium viverra.
+Quis ipsum suspendisse ultrices gravida dictum fusce ut.
+
+Since:
+    Begin since. Nullam eget felis eget nunc lobortis mattis aliquam
+    faucibus. End since.)""";
         // Symbol: drake::mkdoc_test::TemplateClass::TemplateClass<T>
         struct /* ctor */ {
-          // Source: drake/tools/workspace/pybind11/test/sample_header.h:95
-          const char* doc = R"""(Default constructor.)""";
+          // Source: drake/tools/workspace/pybind11/test/sample_header.h:200
+          const char* doc =
+R"""(Default constructor. Condimentum mattis pellentesque id nibh tortor
+id. Nisl rhoncus mattis rhoncus urna neque.
+
+Remark:
+    Begin remarks. Ante metus dictum at tempor commodo. Nec feugiat in
+    fermentum posuere urna nec. End remarks.)""";
         } ctor;
       } TemplateClass;
       // Symbol: drake::mkdoc_test::func
       struct /* func */ {
-        // Source: drake/tools/workspace/pybind11/test/sample_header.h:22
-        const char* doc = R"""(Function.)""";
-        // Source: drake/tools/workspace/pybind11/test/sample_header.h:24
-        const char* doc_2 = R"""(Function, overload 1.)""";
-        // Source: drake/tools/workspace/pybind11/test/sample_header.h:27
-        const char* doc_3 = R"""(Function, template overload.)""";
+        // Source: drake/tools/workspace/pybind11/test/sample_header.h:39
+        const char* doc =
+R"""(Function. Mi sit amet mauris commodo quis.)""";
+        // Source: drake/tools/workspace/pybind11/test/sample_header.h:45
+        const char* doc_2 =
+R"""(Function, overload 1. Velit ut tortor pretium viverra suspendisse
+potenti nullam ac tortor.
+
+Raises:
+    RuntimeError Begin raises. Morbi tincidunt augue interdum velit
+    euismod. Justo nec ultrices dui sapien eget mi proin sed libero.
+    End raises.)""";
+        // Source: drake/tools/workspace/pybind11/test/sample_header.h:51
+        const char* doc_3 =
+R"""(Function, template overload. Pellentesque diam volutpat commodo sed
+egestas egestas fringilla phasellus faucibus.
+
+Parameter ``param``:
+    Begin input/output parameter. Morbi enim nunc faucibus a
+    pellentesque sit. End input/output parameter.)""";
       } func;
     } mkdoc_test;
   } drake;
