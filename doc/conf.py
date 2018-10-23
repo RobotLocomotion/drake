f201707eb5ff02c7732244b6137e66b4168bd328
Jamie Snape <jamie.snape@kitware.com>
2018-10-23 09:21:38 -0400

    Add favicon to HTML documentation

--- doc/conf.py
+++ doc/conf.py
@@ -118,7 +118,7 @@ html_title = u'Drake Documentation'
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'images/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
