d189d6a84a36d659f3190c94a4ca5244592a3fa5
Russ Tedrake <russt@mit.edu>
2018-10-23 15:15:48 -0400

    Render multiple camera images in drake visualizer

--- systems/sensors/image_to_lcm_image_array_t.h
+++ systems/sensors/image_to_lcm_image_array_t.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include "robotlocomotion/image_array_t.hpp"
 
 #include "drake/common/drake_copyable.h"
 #include "drake/systems/framework/leaf_system.h"
+#include "drake/systems/sensors/pixel_types.h"
 
 namespace drake {
 namespace systems {
@@ -26,7 +28,8 @@ class ImageToLcmImageArrayT : public systems::LeafSystem<double> {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(ImageToLcmImageArrayT)
 
-  /// An %ImageToLcmImageArrayT constructor.
+  /// An %ImageToLcmImageArrayT constructor.  Declares three input ports --
+  /// one color image, one depth image, and one label image.
   ///
   /// @param color_frame_name The frame name used for color image.
   /// @param depth_frame_name The frame name used for depth image.
@@ -39,31 +42,40 @@ class ImageToLcmImageArrayT : public systems::LeafSystem<double> {
                         bool do_compress = false);
 
   /// Returns the input port containing a color image.
+  /// Note: Only valid if the color/depth/label constructor is used.
   const InputPort<double>& color_image_input_port() const;
 
   /// Returns the input port containing a depth image.
+  /// Note: Only valid if the color/depth/label constructor is used.
   const InputPort<double>& depth_image_input_port() const;
 
   /// Returns the input port containing a label image.
+  /// Note: Only valid if the color/depth/label constructor is used.
   const InputPort<double>& label_image_input_port() const;
 
   /// Returns the abstract valued output port that contains a
   /// `Value<robotlocomotion::image_array_t>`.
   const OutputPort<double>& image_array_t_msg_output_port() const;
 
+
+  /// Default constructor doesn't declare any ports.  Use the Add*Input()
+  /// methods to declare them.
+  explicit ImageToLcmImageArrayT(bool do_compress = false);
+
+  template <PixelType kPixelType>
+  const InputPort<double>& DeclareImageInputPort(const std::string& name);
+
  private:
   void CalcImageArray(const systems::Context<double>& context,
                       robotlocomotion::image_array_t* msg) const;
+
 
   int color_image_input_port_index_{-1};
   int depth_image_input_port_index_{-1};
   int label_image_input_port_index_{-1};
   int image_array_t_msg_output_port_index_{-1};
 
-  const std::string color_frame_name_;
-  const std::string depth_frame_name_;
-  const std::string label_frame_name_;
-
+  std::vector<PixelType> input_port_pixel_type_{};
   const bool do_compress_;
 };
 
