942f4f2ebe011bb0e024e72dcc033d59a8e92aef
Eric Cousineau <eric.cousineau@tri.global>
2018-10-24 22:07:27 -0400

    drake_visualizer: Enable --use_builtin_scripts

---
+++ tools/workspace/drake_visualizer/plugin/use_builtin_scripts.py
@@ -0,0 +1,54 @@
+"""Amalgam of visualizer scripts."""
+
+from __future__ import print_function
+
+from collections import OrderedDict
+import os
+import sys
+
+from drake.tools.workspace.drake_visualizer.plugin import (
+    contact,
+    frame,
+    image,
+    time,
+    scoped_singleton_func,
+)
+
+
+@scoped_singleton_func
+def init_visualizer():
+    available = OrderedDict((
+        ("contact", contact.init_visualizer),
+        ("frame", frame.init_visualizer),
+        ("time", time.init_visualizer),
+        ("image", image.init_visualizer),
+    ))
+    print("")
+    print("Drake Scripts:")
+    scripts_raw = os.environ["_DRAKE_VISUALIZER_BUILTIN_SCRIPTS"]
+    print("  Specified: --use_builtin_scripts={}".format(scripts_raw))
+    if scripts_raw == "all":
+        scripts = available.keys()
+    else:
+        scripts = []
+        for script in scripts_raw.split(","):
+            script = script.strip()
+            if not script:
+                continue
+            if script not in available:
+                print("\nWARNING: Invalid script: {}\n".format(script))
+                continue
+            scripts.append(script)
+    print("  Available: --use_builtin_scripts={}".format(
+        ",".join(available.keys())))
+    print("")
+    results = OrderedDict()
+    for script in scripts:
+        results[script] = available[script]()
+    return results
+
+
+# Activate the plugin if this script is run directly; store the results to keep
+# the plugin objects in scope.
+if __name__ == "__main__":
+    drake_builtin_scripts = init_visualizer()
