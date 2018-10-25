e426bf38c1d960e318ec350b33259abe67fdfb9a
Sean Curtis <sean.curtis@tri.global>
2018-10-25 13:52:54 -0700

    Add basis for geometry roles
    Roles are defined by role-related properties. This includes the underlying
    class that supports geometry properties (and tests) and the declaration of
    specific roles (currently only proximity and illustration).
    
    The roles are not incorporated anywhere and do not affect functionality or
    API. It is merely the basis for defining roles which will *eventually*
    enter the API.

---
+++ geometry/geometry_properties.cc
@@ -0,0 +1,26 @@
+#include "drake/geometry/geometry_properties.h"
+
+namespace drake {
+namespace geometry {
+
+std::ostream& operator<<(std::ostream& out, const GeometryProperties& props) {
+  const size_t count = props.values_.size();
+  size_t i = 0;
+  for (const auto& group_pair : props.values_) {
+    const std::string& group_name = group_pair.first;
+    const GeometryProperties::Group& group_properties =
+        group_pair.second;
+    out << "[" << group_name << "]";
+    for (const auto& property_pair : group_properties) {
+      const std::string& property_name = property_pair.first;
+      out << "\n  " << property_name;
+      // TODO(SeanCurtis-TRI): How do I print the value in an AbstractValue?
+    }
+    if (i < count - 1) out << "\n";
+    ++i;
+  }
+  return out;
+}
+
+}  // namespace geometry
+}  // namespace drake
