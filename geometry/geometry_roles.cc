7dad9109ecf886ebc9f5509070a8e70272355849
Sean Curtis <sean.curtis@tri.global>
2018-10-26 11:25:37 -0700

    Add basis for geometry roles
    Roles are defined by role-related properties. This includes the underlying
    class that supports geometry properties (and tests) and the declaration of
    specific roles (currently only proximity and illustration).
    
    The roles are not incorporated anywhere and do not affect functionality or
    API. It is merely the basis for defining roles which will *eventually*
    enter the API.

---
+++ geometry/geometry_roles.cc
@@ -0,0 +1,27 @@
+#include "drake/geometry/geometry_roles.h"
+
+#include <string>
+
+namespace drake {
+namespace geometry {
+
+std::string to_string(const Role& role) {
+  switch (role) {
+    case Role::kProximity:
+      return "proximity";
+    case Role::kIllustration:
+      return "illustration";
+    case Role::kUnassigned:
+      return "unassigned";
+    default:
+      return "unknown";
+  }
+}
+
+std::ostream& operator<<(std::ostream& out, const Role& role) {
+  out << to_string(role);
+  return out;
+}
+
+}  // namespace geometry
+}  // namespace drake
