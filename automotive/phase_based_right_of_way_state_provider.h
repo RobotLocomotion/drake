46179c760322635f8fff55e39675d72a509ea752
Chien-Liang Fok <liang.fok@tri.global>
2018-11-13 13:56:27 -0500

    Adds PhaseBasedRightOfWayStateProvider This enables ado cars to determine RightOfWayRule states based on RightOfWayPhase states.

---
+++ automotive/phase_based_right_of_way_state_provider.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "drake/automotive/maliput/api/rules/right_of_way_phase_book.h"
+#include "drake/automotive/maliput/api/rules/right_of_way_phase_provider.h"
+#include "drake/automotive/maliput/api/rules/right_of_way_rule.h"
+#include "drake/common/drake_copyable.h"
+#include "drake/common/drake_optional.h"
+
+namespace drake {
+namespace automotive {
+
+/// A maliput::api::rules::RightOfWayStateProvider that relies on
+/// maliput::api::rules::RightOfWayPhase instances to determine
+/// maliput::api::rules::RightOfWayRule state.
+class PhaseBasedRightOfWayStateProvider final
+    : public maliput::api::rules::RightOfWayStateProvider {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(PhaseBasedRightOfWayStateProvider)
+
+  /// Constructs a PhaseBasedRightOfWayStateProvider.
+  ///
+  /// All reference parameters are aliased; their lifespans must exceed that of
+  /// this instance.
+  PhaseBasedRightOfWayStateProvider(
+      const maliput::api::rules::RightOfWayPhaseBook& phase_book,
+      const maliput::api::rules::RightOfWayPhaseProvider& phase_provider);
+
+  ~PhaseBasedRightOfWayStateProvider() final = default;
+
+  inline const maliput::api::rules::RightOfWayPhaseBook& phase_book() const {
+    return phase_book_;
+  }
+
+  inline const maliput::api::rules::RightOfWayPhaseProvider& phase_provider()
+      const {
+    return phase_provider_;
+  }
+
+ private:
+  optional<maliput::api::rules::RightOfWayStateProvider::Result>
+      DoGetState(const maliput::api::rules::RightOfWayRule::Id& id) const final;
+
+  const maliput::api::rules::RightOfWayPhaseBook& phase_book_;
+  const maliput::api::rules::RightOfWayPhaseProvider& phase_provider_;
+};
+
+}  // namespace automotive
+}  // namespace drake
