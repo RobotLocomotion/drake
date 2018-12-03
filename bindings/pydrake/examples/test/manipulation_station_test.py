fbd23ccf6793e5770cfe196ebf5bdc63fca9d985
Siyuan Feng <siyuan.feng@tri.global>
2018-12-03 10:35:07 -0500

    added a py test for loading iiwa and wsg explicitly

--- bindings/pydrake/examples/test/manipulation_station_test.py
+++ bindings/pydrake/examples/test/manipulation_station_test.py
@@ -2,12 +2,15 @@
 import unittest
 import numpy as np
 
+from pydrake.common import FindResourceOrThrow
+from pydrake.common.eigen_geometry import Isometry3
 from pydrake.examples.manipulation_station import (
     IiwaCollisionModel,
     ManipulationStation,
     ManipulationStationHardwareInterface
 )
 from pydrake.multibody.multibody_tree.multibody_plant import MultibodyPlant
+from pydrake.multibody.multibody_tree.parsing import AddModelFromSdfFile
 
 
 class TestManipulationStation(unittest.TestCase):
@@ -40,6 +43,39 @@ class TestManipulationStation(unittest.TestCase):
 
         station.get_camera_poses_in_world()["0"]
         self.assertEqual(len(station.get_camera_names()), 3)
+
+    def test_manipulation_station_add_iiwa_and_wsg_explicitly(self):
+        station = ManipulationStation()
+        plant = station.get_mutable_multibody_plant()
+
+        # Add models for iiwa and wsg
+        iiwa_model_file = FindResourceOrThrow(
+            "drake/manipulation/models/iiwa_description/iiwa7/iiwa7_no_collision.sdf")
+        iiwa = AddModelFromSdfFile(iiwa_model_file, "iiwa", plant)
+        X_WI = Isometry3.Identity()
+        plant.WeldFrames(
+            plant.world_frame(), plant.GetFrameByName("iiwa_link_0", iiwa), X_WI)
+
+        wsg_model_file = FindResourceOrThrow(
+            "drake/manipulation/models/wsg_50_description/sdf/schunk_wsg_50.sdf")
+        wsg = AddModelFromSdfFile(wsg_model_file, "gripper", plant)
+        X_7G = Isometry3.Identity()
+        plant.WeldFrames(
+            plant.GetFrameByName("iiwa_link_7", iiwa),
+            plant.GetFrameByName("body", wsg), X_7G)
+
+        # Register models for the controller.
+        station.RegisterIiwaControllerModel(
+            iiwa_model_file, "iiwa", plant.world_frame().name(), "iiwa_link_0", X_WI)
+        station.RegisterWsgControllerModel(
+            wsg_model_file, "gripper", "iiwa_link_7", "body", X_7G)
+
+        # Finalize
+        station.Finalize()
+
+        # This WSG gripper model has 2 independent dof, and the IIWA model has 7.
+        self.assertEqual(plant.num_positions(), 9)
+        self.assertEqual(plant.num_velocities(), 9)
 
     def test_iiwa_collision_model(self):
         # Check that all of the elements of the enum were spelled correctly.
