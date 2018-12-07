3e231535a48ced5b482f107d10000b0dc93692aa
Andrés Valenzuela <andres.valenzuela@tri.global>
2018-12-07 13:51:39 -0500

    Switch comments to Doxygen style.

--- multibody/inverse_kinematics/angle_between_vectors_constraint.h
+++ multibody/inverse_kinematics/angle_between_vectors_constraint.h
@@ -8,38 +8,43 @@
 namespace drake {
 namespace multibody {
 namespace internal {
-// Constrains that the angle between a vector na and another vector nb is
-// between [θ_lower, θ_upper]. na is fixed to a frame A, while nb is fixed
-// to a frame B.
-// Mathematically, if we denote na_unit_A as na expressed in frame A after
-// normalization (na_unit_A has unit length), and nb_unit_B as nb expressed in
-// frame B after normalization, the constraint is
-// cos(θ_upper) ≤ na_unit_Aᵀ * R_AB * nb_unit_B ≤ cos(θ_lower)
+/**
+ * Constrains that the angle between a vector na and another vector nb is
+ * between [θ_lower, θ_upper]. na is fixed to a frame A, while nb is fixed
+ * to a frame B.
+ * Mathematically, if we denote na_unit_A as na expressed in frame A after
+ * normalization (na_unit_A has unit length), and nb_unit_B as nb expressed in
+ * frame B after normalization, the constraint is
+ * cos(θ_upper) ≤ na_unit_Aᵀ * R_AB * nb_unit_B ≤ cos(θ_lower)
+ */
 class AngleBetweenVectorsConstraint : public solvers::Constraint {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(AngleBetweenVectorsConstraint)
 
-  // @param tree The MultibodyTree on which the constraint is imposed. @p tree
-  // should be alive during the lifetime of this constraint.
-  // @param frameA_idx The index of frame A.
-  // @param na_A The vector na_A fixed to frame A, expressed in frame A.
-  // @pre na_A should be a non-zero vector.
-  // @throws std::logic_error if na_A is close to zero.
-  // @param frameB_idx The index of frame B.
-  // @param nb_B The vector nb fixed to frame B, expressed in frameB.
-  // @pre nb_B should be a non-zero vector.
-  // @throws std::logic_error if nb_B is close to zero.
-  // @param angle_lower The lower bound on the angle between na and nb. It is
-  // denoted as θ_lower in the class documentation.
-  // @pre angle_lower >= 0.
-  // @throws std::invalid_argument error if angle_lower is negative.
-  // @param angle_upper The upper bound on the angle between na and nb. it is
-  // denoted as θ_upper in the class documentation.
-  // @pre angle_lower <= angle_upper <= pi.
-  // @throws std::invalid_argument if angle_upper is outside the bounds.
-  // @param context The Context that has been allocated for this @p tree. We
-  // will update the context when evaluating the constraint. @p context should
-  // be alive during the lifetime of this constraint.
+  /**
+   * Constructs an AngleBetweenVectorsConstraint.
+   * @param tree The MultibodyTree on which the constraint is imposed. @p tree
+   * should be alive during the lifetime of this constraint.
+   * @param frameA_idx The index of frame A.
+   * @param na_A The vector na_A fixed to frame A, expressed in frame A.
+   * @pre na_A should be a non-zero vector.
+   * @throws std::logic_error if na_A is close to zero.
+   * @param frameB_idx The index of frame B.
+   * @param nb_B The vector nb fixed to frame B, expressed in frameB.
+   * @pre nb_B should be a non-zero vector.
+   * @throws std::logic_error if nb_B is close to zero.
+   * @param angle_lower The lower bound on the angle between na and nb. It is
+   * denoted as θ_lower in the class documentation.
+   * @pre angle_lower >= 0.
+   * @throws std::invalid_argument error if angle_lower is negative.
+   * @param angle_upper The upper bound on the angle between na and nb. it is
+   * denoted as θ_upper in the class documentation.
+   * @pre angle_lower <= angle_upper <= pi.
+   * @throws std::invalid_argument if angle_upper is outside the bounds.
+   * @param context The Context that has been allocated for this @p tree. We
+   * will update the context when evaluating the constraint. @p context should
+   * be alive during the lifetime of this constraint.
+   */
   AngleBetweenVectorsConstraint(
       const multibody_plant::MultibodyPlant<double>& plant,
       const Frame<double>& frameA,
--- multibody/inverse_kinematics/orientation_constraint.h
+++ multibody/inverse_kinematics/orientation_constraint.h
@@ -9,46 +9,51 @@
 namespace drake {
 namespace multibody {
 namespace internal {
-// Constrains that the angle difference θ between the orientation of frame A
-// and the orientation of frame B to satisfy θ ≤ θ_bound. The angle
-// difference between frame A's orientation R_WA and B's orientation R_WB is θ
-// (θ ∈ [0, π]), if there exists a rotation axis a, such that rotating frame
-// A by angle θ about axis a aligns it with frame B. Namely
-// R_AB = I + sinθ â + (1-cosθ)â²   (1)
-// where R_AB is the orientation of frame B expressed in frame A. â is the skew
-// symmetric matrix of the rotation axis a. Equation (1) is the Rodrigues
-// formula that computes the rotation matrix froma rotation axis a and an angle
-// θ, https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
-// If the users want frame A and frame B to align perfectly, they can set
-// θ_bound = 0.
-// Mathematically, this constraint is imposed as
-// trace(R_AB) ≥ 2cos(θ_bound) + 1   (1)
-// To derive (1), using Rodrigues formula
-// R_AB = I + sinθ â + (1-cosθ)â²
-// where
-// trace(R_AB) = 2cos(θ) + 1 ≥ 2cos(θ_bound) + 1
+/**
+ * Constrains that the angle difference θ between the orientation of frame A
+ * and the orientation of frame B to satisfy θ ≤ θ_bound. The angle
+ * difference between frame A's orientation R_WA and B's orientation R_WB is θ
+ *   (θ ∈ [0, π]), if there exists a rotation axis a, such that rotating frame
+ * A by angle θ about axis a aligns it with frame B. Namely
+ *   R_AB = I + sinθ â + (1-cosθ)â²   (1)
+ * where R_AB is the orientation of frame B expressed in frame A. â is the skew
+ * symmetric matrix of the rotation axis a. Equation (1) is the Rodrigues
+ * formula that computes the rotation matrix froma rotation axis a and an angle
+ * θ, https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
+ * If the users want frame A and frame B to align perfectly, they can set
+ *   θ_bound = 0.
+ * Mathematically, this constraint is imposed as
+ *   trace(R_AB) ≥ 2cos(θ_bound) + 1   (1)
+ * To derive (1), using Rodrigues formula
+ *   R_AB = I + sinθ â + (1-cosθ)â²
+ * where
+ *   trace(R_AB) = 2cos(θ) + 1 ≥ 2cos(θ_bound) + 1
+ */
 class OrientationConstraint : public solvers::Constraint {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(OrientationConstraint)
 
-  // The frame A is fixed to a frame A̅, with orientatation R_AbarA measured
-  // in frame A̅. The frame B is fixed to a frame B̅, with orientation R_BbarB
-  // measured in frame B. We constrain the angle between frame A and B to be
-  // less than theta_bound.
-  // @param tree The MultibodyTree on which the constraint is imposed. @p tree
-  // should be alive during the lifetime of this constraint.
-  // @param frameAbar_idx The index of frame A̅.
-  // @param R_AbarA The orientation of frame A measured in frame A̅.
-  // @param frameBbar_idx The index of frame B̅.
-  // @param R_BbarB The orientation of frame B measured in frame B̅.
-  // @param theta_bound The bound on the angle difference between frame A's
-  // orientation and frame B's orientation. It is denoted as θ_bound in the
-  // class documentation. @p theta_bound is in radians.
-  // @pre angle_bound >= 0.
-  // @throws std::invalid_argument if angle_bound < 0.
-  // @param context The Context that has been allocated for this @p tree. We
-  // will update the context when evaluating the constraint. @p context should
-  // be alive during the lifetime of this constraint.
+  /**
+   * Constructs an OrientationConstraint object.
+   * The frame A is fixed to a frame A̅, with orientatation R_AbarA measured
+   * in frame A̅. The frame B is fixed to a frame B̅, with orientation R_BbarB
+   * measured in frame B. We constrain the angle between frame A and B to be
+   * less than theta_bound.
+   * @param tree The MultibodyTree on which the constraint is imposed. @p tree
+   *   should be alive during the lifetime of this constraint.
+   * @param frameAbar_idx The index of frame A̅.
+   * @param R_AbarA The orientation of frame A measured in frame A̅.
+   * @param frameBbar_idx The index of frame B̅.
+   * @param R_BbarB The orientation of frame B measured in frame B̅.
+   * @param theta_bound The bound on the angle difference between frame A's
+   *   orientation and frame B's orientation. It is denoted as θ_bound in the
+   *   class documentation. @p theta_bound is in radians.
+   * @pre angle_bound >= 0.
+   * @throws std::invalid_argument if angle_bound < 0.
+   * @param context The Context that has been allocated for this @p tree. We
+   *   will update the context when evaluating the constraint. @p context should
+   *   be alive during the lifetime of this constraint.
+   */
   OrientationConstraint(const multibody_plant::MultibodyPlant<double>& plant,
                         const Frame<double>& frameAbar,
                         const math::RotationMatrix<double>& R_AbarA,
