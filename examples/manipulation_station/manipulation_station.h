8830160dcd670b0abaa770ffaae4c81bcb0402d1
Siyuan Feng <siyuan.feng@tri.global>
2018-12-14 15:44:35 -0500

    - expose the scene graph for rendering camera images. - fixed a bug in num_x querries for dev scene graph inspector.

--- examples/manipulation_station/manipulation_station.h
+++ examples/manipulation_station/manipulation_station.h
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include "drake/common/eigen_types.h"
+#include "drake/geometry/dev/scene_graph.h"
 #include "drake/geometry/scene_graph.h"
 #include "drake/math/rigid_transform.h"
 #include "drake/multibody/plant/multibody_plant.h"
@@ -258,6 +259,28 @@ class ManipulationStation : public systems::Diagram<T> {
   /// add additional elements into the world before calling Finalize().
   geometry::SceneGraph<T>& get_mutable_scene_graph() { return *scene_graph_; }
 
+  /// Returns a const pointer to the SceneGraph that's being used to render
+  /// camera images. Since the SceneGraph for rendering is constructed in
+  /// Finalize(), this returns nullptr before Finalize() is called.
+  /// Note: the current implementation of the manipulation station uses a
+  /// separate development version of SceneGraph for rendering (as opposed to
+  /// the one returned by get_scene_graph() used for contact detection and
+  /// visualization). This method will be deprecated soon.
+  const geometry::dev::SceneGraph<T>* get_render_scene_graph() const {
+    return render_scene_graph_;
+  }
+
+  /// Returns a mutable pointer to the SceneGraph that's being used to render
+  /// camera images. Since the SceneGraph for rendering is constructed in
+  /// Finalize(), this returns nullptr before Finalize() is called.
+  /// Note: the current implementation of the manipulation station uses a
+  /// separate development version of SceneGraph for rendering (as opposed to
+  /// the one returned by get_scene_graph() used for contact detection and
+  /// visualization). This method will be deprecated soon.
+  geometry::dev::SceneGraph<T>* get_mutable_render_scene_graph() {
+    return render_scene_graph_;
+  }
+
   /// Return a reference to the plant used by the inverse dynamics controller
   /// (which contains only a model of the iiwa + equivalent mass of the
   /// gripper).
@@ -371,6 +394,8 @@ class ManipulationStation : public systems::Diagram<T> {
   std::unique_ptr<multibody::MultibodyPlant<T>> owned_controller_plant_;
   multibody::MultibodyPlant<T>* plant_;
   geometry::SceneGraph<T>* scene_graph_;
+  // This is made in Finalize().
+  geometry::dev::SceneGraph<T>* render_scene_graph_{};
 
   // Populated by RegisterIiwaControllerModel() and
   // RegisterWsgControllerModel().
