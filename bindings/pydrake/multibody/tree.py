41a1e0c60c6ac80ecb0a5355bd9895c20d2bab87
Eric Cousineau <eric.cousineau@tri.global>
2018-12-17 10:46:37 -0500

    py multibody: Add shims for updated spelling

---
+++ bindings/pydrake/multibody/tree.py
@@ -0,0 +1,14 @@
+"""
+Bindings for multibody tree components.
+
+Note:
+    The documentation is not yet populated with symbols, but will be soon.
+"""
+# TODO(eric.cousineau): Replace with actual bindings.
+
+from pydrake.multibody.multibody_tree import *
+
+# Do not import soon-to-be deprecated symbols (#9366).
+del BodyNodeIndex
+del MobilizerIndex
+del MultibodyTree
