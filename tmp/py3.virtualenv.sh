096f5bc8f44f8b2f933d92950cf940edc01709eb
Eric Cousineau <eric.cousineau@tri.global>
2018-12-16 21:09:44 -0500

    Update

--- tmp/py3.virtualenv.sh
+++ tmp/py3.virtualenv.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-_dir=$(dirname ${BASH_SOURCE})/..
+_dir=$(dirname ${BASH_SOURCE})
 
-if [[ ! -f ${_dir}/build/py3/bin/python3 ]]; then
+if [[ ! -f ${_dir}/virtualenv/bin/python3 ]]; then
 (
     set -x
     cd ${_dir}
-    python3 -m virtualenv --python python3 build/py3
+    python3 -m virtualenv --python python3 virtualenv
     set +x
-    source build/py3/bin/activate
+    source virtualenv/bin/activate
     # Install some (if not all) needed dependencies.
     pip install -I \
         pyyaml protobuf==3.6.0 sphinx==1.8.1 sphinx_rtd_theme \
@@ -16,10 +16,10 @@ if [[ ! -f ${_dir}/build/py3/bin/python3 ]]; then
     set -x
     # Reflect system Python; make `python` and `python-config` fall through to
     # system, and use `python{major}-config`.
-    rm build/py3/bin/python
-    mv build/py3/bin/{python-config,python3-config}
-    sed -i 's#py3/bin/python\b#py3/bin/python3#g' build/py3/bin/python3-config
+    rm virtualenv/bin/python
+    mv virtualenv/bin/{python-config,python3-config}
+    sed -i 's#py3/bin/python\b#py3/bin/python3#g' virtualenv/bin/python3-config
 )
 fi
 
-source ${_dir}/build/py3/bin/activate
+source ${_dir}/virtualenv/bin/activate
