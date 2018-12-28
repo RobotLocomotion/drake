3a1847f6078cee31185f15e0dda78e5ca428b7cc
Soonho Kong <soonho.kong@tri.global>
2018-12-27 21:07:05 -0500

    Rename MakeFunctionCode to MakeScalarFunctionCode

--- common/test/symbolic_codegen_test.cc
+++ common/test/symbolic_codegen_test.cc
@@ -13,7 +13,8 @@ using std::string;
 
 // Helper function to combine the function name @p function_name and an
 // expression @p e with the proper function header and footer.
-string MakeFunctionCode(const string& function_name, int n, const string& e) {
+string MakeScalarFunctionCode(const string& function_name, const int n,
+                              const string& e) {
   // Note that fmtlib requires to escape "{'" and "}" using "{{" and "}}".
   return fmt::format(
       R"""(double {}(const double* p) {{
@@ -34,123 +35,125 @@ class SymbolicCodeGenTest : public ::testing::Test {
 };
 
 TEST_F(SymbolicCodeGenTest, Variable) {
-  EXPECT_EQ(CodeGen("f", {x_, y_}, x_), MakeFunctionCode("f", 2, "p[0]"));
-  EXPECT_EQ(CodeGen("f", {x_, y_, z_}, z_), MakeFunctionCode("f", 3, "p[2]"));
+  EXPECT_EQ(CodeGen("f", {x_, y_}, x_), MakeScalarFunctionCode("f", 2, "p[0]"));
+  EXPECT_EQ(CodeGen("f", {x_, y_, z_}, z_),
+            MakeScalarFunctionCode("f", 3, "p[2]"));
 }
 
 TEST_F(SymbolicCodeGenTest, Constant) {
-  EXPECT_EQ(CodeGen("f", {}, 3.141592), MakeFunctionCode("f", 0, "3.141592"));
+  EXPECT_EQ(CodeGen("f", {}, 3.141592),
+            MakeScalarFunctionCode("f", 0, "3.141592"));
 }
 
 TEST_F(SymbolicCodeGenTest, Addition) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, 2.0 + 3.0 * x_ - 7.0 * y_),
-            MakeFunctionCode("f", 2, "(2 + (3 * p[0]) + (-7 * p[1]))"));
+            MakeScalarFunctionCode("f", 2, "(2 + (3 * p[0]) + (-7 * p[1]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Multiplication) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, 2.0 * 3.0 * x_ * x_ * -7.0 * y_ * y_ * y_),
-            MakeFunctionCode(
+            MakeScalarFunctionCode(
                 "f", 2, "(-42 * pow(p[0], 2.000000) * pow(p[1], 3.000000))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Pow) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, pow(2 + x_, 3 * y_)),
-            MakeFunctionCode("f", 2, "pow((2 + p[0]), (3 * p[1]))"));
+            MakeScalarFunctionCode("f", 2, "pow((2 + p[0]), (3 * p[1]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Division) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, (2 + x_) / (3 * y_)),
-            MakeFunctionCode("f", 2, "((2 + p[0]) / (3 * p[1]))"));
+            MakeScalarFunctionCode("f", 2, "((2 + p[0]) / (3 * p[1]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Abs) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, abs(2 + x_)),
-            MakeFunctionCode("f", 2, "fabs((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "fabs((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Log) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, log(2 + x_)),
-            MakeFunctionCode("f", 2, "log((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "log((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Exp) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, exp(2 + x_)),
-            MakeFunctionCode("f", 2, "exp((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "exp((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Sqrt) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, sqrt(2 + x_)),
-            MakeFunctionCode("f", 2, "sqrt((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "sqrt((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Sin) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, sin(2 + x_)),
-            MakeFunctionCode("f", 2, "sin((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "sin((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Cos) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, cos(2 + x_)),
-            MakeFunctionCode("f", 2, "cos((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "cos((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Tan) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, tan(2 + x_)),
-            MakeFunctionCode("f", 2, "tan((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "tan((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Asin) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, asin(2 + x_)),
-            MakeFunctionCode("f", 2, "asin((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "asin((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Acos) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, acos(2 + x_)),
-            MakeFunctionCode("f", 2, "acos((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "acos((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Atan) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, atan(2 + x_)),
-            MakeFunctionCode("f", 2, "atan((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "atan((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Atan2) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, atan2(2 + x_, 3 + y_)),
-            MakeFunctionCode("f", 2, "atan2((2 + p[0]), (3 + p[1]))"));
+            MakeScalarFunctionCode("f", 2, "atan2((2 + p[0]), (3 + p[1]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Sinh) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, sinh(2 + x_)),
-            MakeFunctionCode("f", 2, "sinh((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "sinh((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Cosh) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, cosh(2 + x_)),
-            MakeFunctionCode("f", 2, "cosh((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "cosh((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Tanh) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, tanh(2 + x_)),
-            MakeFunctionCode("f", 2, "tanh((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "tanh((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Min) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, min(2 + x_, 3 + y_)),
-            MakeFunctionCode("f", 2, "fmin((2 + p[0]), (3 + p[1]))"));
+            MakeScalarFunctionCode("f", 2, "fmin((2 + p[0]), (3 + p[1]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Max) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, max(2 + x_, 3 + y_)),
-            MakeFunctionCode("f", 2, "fmax((2 + p[0]), (3 + p[1]))"));
+            MakeScalarFunctionCode("f", 2, "fmax((2 + p[0]), (3 + p[1]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Ceil) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, ceil(2 + x_)),
-            MakeFunctionCode("f", 2, "ceil((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "ceil((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, Floor) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, floor(2 + x_)),
-            MakeFunctionCode("f", 2, "floor((2 + p[0]))"));
+            MakeScalarFunctionCode("f", 2, "floor((2 + p[0]))"));
 }
 
 TEST_F(SymbolicCodeGenTest, IfThenElse) {
@@ -163,9 +166,9 @@ TEST_F(SymbolicCodeGenTest, UninterpretedFunction) {
   EXPECT_THROW(CodeGen("f", {x_, y_}, e), std::runtime_error);
 }
 
-TEST_F(SymbolicCodeGenTest, ExampleInDocumentation) {
+TEST_F(SymbolicCodeGenTest, ScalarExampleInDocumentation) {
   EXPECT_EQ(CodeGen("f", {x_, y_}, 1 + sin(x_) + cos(y_)),
-            MakeFunctionCode("f", 2, "(1 + sin(p[0]) + cos(p[1]))"));
+            MakeScalarFunctionCode("f", 2, "(1 + sin(p[0]) + cos(p[1]))"));
 }
 
 }  // namespace
