be9167f418bc8a132d76aaf78ab29ebb80c625ce
Soonho Kong <soonho.kong@tri.global>
2018-12-27 21:07:05 -0500

    Add CodeGen for a symbolic dense matrix

--- common/symbolic_codegen.cc
+++ common/symbolic_codegen.cc
@@ -6,6 +6,7 @@
 namespace drake {
 namespace symbolic {
 
+using std::ostream;
 using std::ostringstream;
 using std::runtime_error;
 using std::string;
@@ -196,6 +197,43 @@ string CodeGen(const string& function_name, const vector<Variable>& parameters,
       << parameters.size() << "}}; }\n";
   return oss.str();
 }
+
+namespace internal {
+void CodeGenData(const string& function_name,
+                 const vector<Variable>& parameters,
+                 const Expression* const data, const int size,
+                 ostream* const os) {
+  // Add header for the main function.
+  (*os) << "void " << function_name << "(const double* p, double* m) {\n";
+  // Build a map from Variable::Id to index (in parameters).
+  CodeGenVisitor::IdToIndexMap id_to_idx_map;
+  for (vector<Variable>::size_type i = 0; i < parameters.size(); ++i) {
+    id_to_idx_map.emplace(parameters[i].get_id(), i);
+  }
+  const CodeGenVisitor visitor{id_to_idx_map};
+  for (int i = 0; i < size; ++i) {
+    (*os) << "    "
+          << "m[" << i << "] = " << visitor.CodeGen(data[i]) << ";\n";
+  }
+  // Add footer for the main function.
+  (*os) << "}\n";
+}
+
+void CodeGenMeta(const string& function_name, const int parameter_size,
+                 const int rows, const int cols, ostream* const os) {
+  // <function_name>_meta_t type.
+  (*os) << "typedef struct {\n"
+           "    /* p: input, vector */\n"
+           "    struct { int size; } p;\n"
+           "    /* m: output, matrix */\n"
+           "    struct { int rows; int cols; } m;\n"
+           "} "
+        << function_name << "_meta_t;\n";
+  // <function_name>_meta().
+  (*os) << function_name << "_meta_t " << function_name << "_meta() { return {{"
+        << parameter_size << "}, {" << rows << ", " << cols << "}}; }\n";
+}
+}  // namespace internal
 
 }  // namespace symbolic
 }  // namespace drake
