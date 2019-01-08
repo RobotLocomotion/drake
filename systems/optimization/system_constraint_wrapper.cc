ca26fd1ebaf954e6d5281a8d581fb6f813447fe8
hongkai-dai <daihongkai@gmail.com>
2019-01-08 14:42:01 -0800

    Add SystemConstraintWrapper class.

--- systems/optimization/system_constraint_wrapper.cc
+++ systems/optimization/system_constraint_wrapper.cc
@@ -14,9 +14,31 @@ SystemConstraintWrapper::SystemConstraintWrapper(
                           system_double->get_constraint(index).description()),
       system_double_{system_double},
       system_autodiff_{system_autodiff},
+      index_{index},
       context_double_{context.Clone()},
       context_autodiff_{system_autodiff_->CreateDefaultContext()},
       selector_double_(selector_double),
-      selector_autodiff_(selector_autodiff) {}
+      selector_autodiff_(selector_autodiff) {
+  context_autodiff_->SetTimeStateAndParametersFrom(*context_double_);
+}
+
+void SystemConstraintWrapper::DoEval(const Eigen::Ref<const Eigen::VectorXd>& x,
+                                     Eigen::VectorXd* y) const {
+  selector_double_(*system_double_, x, context_double_.get());
+  system_double_->get_constraint(index_).Calc(*context_double_, y);
+}
+
+void SystemConstraintWrapper::DoEval(const Eigen::Ref<const AutoDiffVecXd>& x,
+                                     AutoDiffVecXd* y) const {
+  selector_autodiff_(*system_autodiff_, x, context_autodiff_.get());
+  system_autodiff_->get_constraint(index_).Calc(*context_autodiff_, y);
+}
+
+void SystemConstraintWrapper::DoEval(
+    const Eigen::Ref<const VectorX<symbolic::Variable>>&,
+    VectorX<symbolic::Expression>*) const {
+  throw std::runtime_error(
+      "SystemConstraintWrapper::DoEval do not support symbolic computation.");
+}
 }  // namespace systems
 }  // namespace drake
