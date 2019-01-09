374b88d2a6d0e9ce03e3401fadf7d3b6b9c4c25f
Jeremy Nimmer <jeremy.nimmer@tri.global>
2019-01-08 20:00:44 -0500

    logging: Allow the user access to the default sink (#10356)
    This permits Drake users to redirect Drake's text logs.

--- common/text_logging.cc
+++ common/text_logging.cc
@@ -2,8 +2,10 @@
 
 #include <memory>
 #include <mutex>
+#include <utility>
 
 #ifdef HAVE_SPDLOG
+#include <spdlog/sinks/dist_sink.h>
 #include <spdlog/sinks/stdout_sinks.h>
 #endif
 
@@ -21,10 +23,14 @@ std::shared_ptr<logging::logger> onetime_create_log() {
   // will just return it; if not, we'll create our own default one.
   std::shared_ptr<logging::logger> result(spdlog::get("console"));
   if (!result) {
-    // We use the logger_mt (instead of logger_st) so more than one thread can
-    // use this logger and have their messages be staggered by line, instead of
-    // co-mingling their character bytes.
-    result = spdlog::stderr_logger_mt("console");
+    // We wrap our stderr sink in a dist_sink so that users can atomically swap
+    // out the sinks used by all Drake logging, via dist_sink_mt's APIs.
+    auto wrapper = std::make_shared<spdlog::sinks::dist_sink_mt>();
+    // We use the stderr_sink_mt (instead of stderr_sink_st) so more than one
+    // thread can use this logger and have their messages be staggered by line,
+    // instead of co-mingling their character bytes.
+    wrapper->add_sink(std::make_shared<spdlog::sinks::stderr_sink_mt>());
+    result = std::make_shared<logging::logger>("console", std::move(wrapper));
   }
   return result;
 }
@@ -36,14 +42,34 @@ logging::logger* log() {
   return g_logger.access().get();
 }
 
+logging::sink* logging::get_dist_sink() {
+  // Extract the dist_sink_mt from Drake's logger instance.
+  auto* sink = log()->sinks().empty() ? nullptr : log()->sinks().front().get();
+  auto* result = dynamic_cast<spdlog::sinks::dist_sink_mt*>(sink);
+  if (result == nullptr) {
+    throw std::logic_error(
+        "drake::logging::get_sink(): error: the spdlog sink configuration has"
+        "unexpectedly changed.");
+  }
+  return result;
+}
+
 #else  // HAVE_SPDLOG
 
 logging::logger::logger() {}
+
+logging::sink::sink() {}
 
 logging::logger* log() {
   // A do-nothing logger instance.
   static logging::logger g_logger;
   return &g_logger;
+}
+
+logging::sink* logging::get_dist_sink() {
+  // An empty sink instance.
+  static logging::sink g_sink;
+  return &g_sink;
 }
 
 #endif  // HAVE_SPDLOG
