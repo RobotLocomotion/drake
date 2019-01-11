54bb17272cf55038079b61c5593a9c0137274565
Eric Cousineau <eric.cousineau@tri.global>
2019-01-11 13:19:32 -0500

    drake_visualizer: Deprecate old scripts

--- multibody/rigid_body_plant/visualization/contact_viz.py
+++ multibody/rigid_body_plant/visualization/contact_viz.py
@@ -1,4 +1,7 @@
-from drake.tools.workspace.drake_visualizer.plugin import show_contact
+from drake.tools.workspace.drake_visualizer.plugin import (
+    show_contact, warn_old_script)
+
+warn_old_script(new_module=show_contact)
 
 # Activate the plugin if this script is run directly; store the results to keep
 # the plugin objects in scope.
