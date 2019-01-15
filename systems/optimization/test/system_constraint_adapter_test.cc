fcaf8cf463985469473698e5699f29e86abeff05
hongkai-dai <daihongkai@gmail.com>
2019-01-14 16:01:27 -0800

    Add example on using generic lambda as the selector.

--- systems/optimization/test/system_constraint_adapter_test.cc
+++ systems/optimization/test/system_constraint_adapter_test.cc
@@ -15,8 +15,7 @@ const double kEps = std::numeric_limits<double>::epsilon();
 // Assumes that vars = [p; x(0); x(1); x(2)].
 struct DummySystemSelector1 {
   template <typename T>
-  void operator()(const System<T>&,
-                  const Eigen::Ref<const VectorX<T>>& vars,
+  void operator()(const System<T>&, const Eigen::Ref<const VectorX<T>>& vars,
                   Context<T>* context) {
     context->get_mutable_continuous_state_vector().SetFromVector(
         vars.template tail<3>());
@@ -31,24 +30,43 @@ GTEST_TEST(SystemConstraintAdapter, CreateSystemConstraintWrapper) {
 
   auto context = system.CreateDefaultContext();
 
-  auto constraint = adapter.Create(system.constraint_index(), *context,
-                                   DummySystemSelector1{}, 4);
+  auto constraint1 = adapter.Create(system.constraint_index(), *context,
+                                    DummySystemSelector1{}, 4);
   EXPECT_TRUE(
-      CompareMatrices(constraint->lower_bound(), Eigen::Vector2d(2, 0)));
+      CompareMatrices(constraint1->lower_bound(), Eigen::Vector2d(2, 0)));
   EXPECT_TRUE(
-      CompareMatrices(constraint->upper_bound(), Eigen::Vector2d(kInf, kInf)));
+      CompareMatrices(constraint1->upper_bound(), Eigen::Vector2d(kInf, kInf)));
 
-  EXPECT_EQ(constraint->get_description(), "dummy_system_constraint");
+  EXPECT_EQ(constraint1->get_description(), "dummy_system_constraint");
 
   const Eigen::Vector4d var(2, 3, 4, 5);
   Eigen::VectorXd y;
-  constraint->Eval(var, &y);
+  constraint1->Eval(var, &y);
 
   DummySystemSelector1 selector;
   selector.operator()<double>(system, var, context.get());
   Eigen::VectorXd y_expected;
   DummySystemConstraintCalc<double>(*context, &y_expected);
   EXPECT_TRUE(CompareMatrices(y, y_expected, 3 * kEps));
+
+  // Use a lambda function as the selector.
+  // This selector's vars = [x(0); x(1); x(2)]
+  auto selector2 = [](const auto&, const auto& vars, auto* m_context) {
+    m_context->get_mutable_continuous_state_vector().SetFromVector(vars);
+  };
+  const double p_val = 2;
+  context->get_mutable_numeric_parameter(0).SetAtIndex(0, p_val);
+  auto constraint2 =
+      adapter.Create(system.constraint_index(), *context, selector2, 3);
+  const Eigen::Vector3d x_val2(5, 6, 7);
+  Eigen::VectorXd y_val2;
+  constraint2->Eval(x_val2, &y_val2);
+  // Calling constraint2->Eval doesn't change the parameter in the context.
+  EXPECT_EQ(context->get_numeric_parameter(0).GetAtIndex(0), p_val);
+  selector2(system, x_val2, context.get());
+  Eigen::VectorXd y_val2_expected;
+  DummySystemConstraintCalc<double>(*context, &y_val2_expected);
+  EXPECT_TRUE(CompareMatrices(y_val2, y_val2_expected, 3 * kEps));
 }
 
 GTEST_TEST(SystemConstraintAdapter, SolveDummySystemConstraint) {
