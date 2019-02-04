180c4b83185ed53a39a3c852bccb9e81ff1b539e
Evan Drumwright <drum@tri.global>
2019-02-04 10:32:27 -0800

    Fixed build error.

--- multibody/plant/externally_applied_force.cc
+++ multibody/plant/externally_applied_force.cc
@@ -2,4 +2,4 @@
 
 // Explicitly instantiates on the most common scalar types.
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
-    class ::drake::multibody::ExternallyAppliedForce)
+    struct ::drake::multibody::ExternallyAppliedForce)
