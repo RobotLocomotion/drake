2409295eb947d7dfedc9cb0ef72b93af57ecb979
Michael Sherman <sherm1@gmail.com>
2019-02-25 22:23:10 -0800

    Add FixValue methods to InputPort.

--- systems/framework/input_port.h
+++ systems/framework/input_port.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <memory>
 #include <string>
 #include <type_traits>
 #include <utility>
@@ -96,9 +97,102 @@ class InputPort final : public InputPortBase {
   }
 #endif  // DRAKE_DOXYGEN_CXX
 
-  /** Returns true iff this port is connected.  Beware that at the moment, this
-  could be an expensive operation, because the value is brought up-to-date as
-  part of this operation. */
+#ifdef DRAKE_DOXYGEN_CXX
+  /** Provides a fixed value for this %InputPort in the given Context. If the
+  port is already connected, this value will override the connected source
+  value. (By "connected" we mean that the port appeared in a
+  DiagramBuilder::Connect() call.)
+
+  If any Eigen vector object is provided, it is copied to a BasicVector object
+  and this must be a vector port. A BasicVector-derived object may also be
+  supplied and its concrete type will be preserved. Any other ValueType will
+  be converted to an AbstractValue whose underlying type is `Value<ValueType>`
+  and this must be an abstract port.
+
+  The returned FixedInputPortValue reference may be used to modify the input
+  port's value subsequently using the appropriate FixedInputPortValue method,
+  which will ensure that cache invalidation notifications are delivered.
+
+  @tparam ValueType The type of the supplied `value` object. This will be
+      inferred so no template argument need be specified. The type must be
+      copy constructible or have a public `Clone()` method.
+
+  @param[in,out] context A Context that is compatible with the System that
+                         owns this port.
+  @param[in]     value   The fixed value for this port. Must be convertible
+                         to the input port's data type.
+  @returns a reference to the the FixedInputPortValue object in the Context
+           that contains this port's value.
+
+  @pre `context` must be compatible with the System that owns this %InputPort.
+  @pre `value` must be compatible with this %InputPort's data type. */
+  template <typename ValueType>
+  FixedInputPortValue& FixValue(Context<T>* context,
+                                const ValueType& value) const;
+#else
+  // The FixValue() methods here all use the ContextBase FixInputPort()
+  // method that takes a unique_ptr<AbstractValue>, rather than the many
+  // variants in Context<T>, in case we can deprecate those.
+
+  // This signature is used for any Eigen vector type, but the argument is
+  // copied to a BasicVector for the input port value.
+  FixedInputPortValue& FixValue(
+      Context<T>* context, const Eigen::Ref<const VectorX<T>>& vector) const {
+    return context->FixInputPort(
+        get_index(), std::make_unique<Value<BasicVector<T>>>(vector));
+  }
+
+  // BasicVector must be special-cased because we permit use of a concrete
+  // BasicVector subtype that does not have its own Clone() method. That does
+  // not meet the requirements of drake::is_cloneable.
+  FixedInputPortValue& FixValue(Context<T>* context,
+                                const BasicVector<T>& vector) const {
+    return context->FixInputPort(
+        get_index(), std::make_unique<Value<BasicVector<T>>>(vector.Clone()));
+  }
+
+  // This signature is used for AbstractValue or Value<U> arguments.
+  FixedInputPortValue& FixValue(Context<T>* context,
+                                const AbstractValue& abstract_value) const {
+    return context->FixInputPort(get_index(), abstract_value.Clone());
+  }
+
+  // Special case char* to std::string to avoid ugly compilation messages
+  // for this case, where the user's intent is obvious.
+  FixedInputPortValue& FixValue(Context<T>* context,
+                                const char* c_string) const {
+    return FixValue(&*context, std::string(c_string));
+  }
+
+  // Returns true if ValueType is compatible with Eigen::Ref.
+  template <typename ValueType>
+  static constexpr bool is_eigen_refable() {
+    return is_eigen_refable_helper<ValueType>(1);  // Any int will do here.
+  }
+
+  // This method won't instantiate if any of the non-templatized signatures
+  // above can be used (after possible conversions). If we allowed this to
+  // instantiate it would be chosen instead of performing those conversions.
+  template <typename ValueType,
+            typename = std::enable_if_t<
+                !(is_eigen_refable<ValueType>() ||
+                  std::is_base_of<AbstractValue, ValueType>::value ||
+                  std::is_base_of<BasicVector<T>, ValueType>::value)>>
+  FixedInputPortValue& FixValue(Context<T>* context,
+                                const ValueType& value) const {
+    static_assert(
+        std::is_copy_constructible<ValueType>::value ||
+            drake::is_cloneable<ValueType>::value,
+        "FixValue(): an input port value type must be copy constructible or "
+        "have a public Clone() method.");
+    return FixValueHelper(&*context, value, 1, 1);
+  }
+#endif
+
+  /** Returns true iff this port is connected or has had a fixed value provided
+  in the given Context.  Beware that at the moment, this could be an expensive
+  operation, because the value is brought up-to-date as part of this
+  operation. */
   bool HasValue(const Context<T>& context) const {
     DRAKE_ASSERT_VOID(get_system_base().ThrowIfContextNotCompatible(context));
     return DoEvalOptional(context);
@@ -137,6 +231,53 @@ class InputPort final : public InputPortBase {
         system_(*system) {
     DRAKE_DEMAND(system != nullptr);
     DRAKE_DEMAND(static_cast<const void*>(system) == system_base);
+  }
+
+  // This overload is chosen for the int argument if the Ref type exists,
+  // otherwise there is an SFINAE failure here.
+  template <typename ValueType>
+  static constexpr bool is_eigen_refable_helper(
+      decltype(std::declval<Eigen::Ref<ValueType>>(), int())) {
+    return true;
+  }
+
+  // When the above method can't be instantiated, the int argument converts to
+  // a char and this method is invoked instead.
+  template <typename ValueType>
+  static constexpr bool is_eigen_refable_helper(char) {
+    return false;
+  }
+
+  // This is instantiated if ValueType has a copy constructor. If it is also
+  // cloneable, this method still gets invoked; we prefer use of the the copy
+  // constructor.
+  template <
+      typename ValueType,
+      typename = std::enable_if_t<std::is_copy_constructible<ValueType>::value>>
+  FixedInputPortValue& FixValueHelper(Context<T>* context,
+                                      const ValueType& value, int, int) const {
+    return context->FixInputPort(get_index(),
+                                 std::make_unique<Value<ValueType>>(value));
+  }
+
+  // This is available if ValueType is cloneable, but is dispreferred if there
+  // is also a copy constructor because the `int, int` args above are a better
+  // match than the `int, ...` args here.
+  template <typename ValueType,
+            typename = std::enable_if_t<drake::is_cloneable<ValueType>::value>>
+  FixedInputPortValue& FixValueHelper(Context<T>* context,
+                                      const ValueType& value, int, ...) const {
+    return context->FixInputPort(
+        get_index(), std::make_unique<Value<ValueType>>(value.Clone()));
+  }
+
+  // This signature exists for non-copyable, non-cloneable ValueType just to
+  // avoid spurious compilation errors. A static_assert above will have provided
+  // a good message already; this method just needs to compile peacefully.
+  template <typename ValueType>
+  FixedInputPortValue& FixValueHelper(Context<T>*, const ValueType&,
+                                      ...) const {
+    DRAKE_UNREACHABLE();
   }
 
   const System<T>& system_;
--- systems/framework/test/input_port_test.cc
+++ systems/framework/test/input_port_test.cc
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 
 #include "drake/common/test_utilities/expect_throws_message.h"
+#include "drake/systems/framework/basic_vector.h"
 #include "drake/systems/framework/leaf_system.h"
 #include "drake/systems/framework/test_utilities/my_vector.h"
 
@@ -143,6 +144,148 @@ GTEST_TEST(InputPortTest, AbstractTest) {
       "InputPort::Eval..: wrong value type int specified; "
       "actual type was std::string "
       "for InputPort.*2.*of.*dummy.*DummySystem.*");
+}
+
+// This struct is for testing the FixValue() variants.
+struct SystemWithInputPorts final : public LeafSystem<double> {
+ public:
+  SystemWithInputPorts()
+      : basic_vec_port{DeclareVectorInputPort("basic_vec_port",
+                                              BasicVector<double>(3))},
+        derived_vec_port{DeclareVectorInputPort(
+            "derived_vec_port",
+            MyVector3d(Eigen::Vector3d(1., 2., 3.)))},
+        int_port{DeclareAbstractInputPort("int_port", Value<int>(5))},
+        double_port{
+            DeclareAbstractInputPort("double_port", Value<double>(1.25))},
+        string_port{DeclareAbstractInputPort("string_port",
+                                             Value<std::string>("hello"))} {}
+  const InputPort<double>& basic_vec_port;
+  const InputPort<double>& derived_vec_port;
+  const InputPort<double>& int_port;
+  const InputPort<double>& double_port;
+  const InputPort<double>& string_port;
+};
+
+// Test each of the five FixValue() methods. For reference:
+// (1) FixValue(Eigen::Ref) should convert to BasicVector
+// (2) FixValue(BasicVector) special-cased to make concrete vector types work
+// (3) FixValue(AbstractValue) must match the port type
+// (4) FixValue(const char*) special-cased shorthand for std::string
+// (5) FixValue<V>(V) creates Value<V> for an abstract port
+//
+// Also for sanity, make sure the returned FixedInputPortValue object works,
+// although its API is so awful no one should use it.
+GTEST_TEST(InputPortTest, FixValueTests) {
+  SystemWithInputPorts dut;
+  std::unique_ptr<Context<double>> context = dut.CreateDefaultContext();
+
+  // None of the ports should have a value initially.
+  for (int i = 0; i < dut.get_num_input_ports(); ++i) {
+    EXPECT_FALSE(dut.get_input_port(i).HasValue(*context));
+  }
+
+  // First check that the API that accepts an Eigen::Ref works. This is
+  // supposed to copy the Eigen object to a BasicVector *not* create
+  // a generic abstract object.
+  const Eigen::Vector3d expected_vec(10., 20., 30.);
+  dut.basic_vec_port.FixValue(&*context, expected_vec);  // (1)
+  EXPECT_EQ(dut.basic_vec_port.Eval(*context), expected_vec);
+  EXPECT_EQ(
+      dut.basic_vec_port.Eval<BasicVector<double>>(*context).CopyToVector(),
+      expected_vec);
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      dut.basic_vec_port.Eval<Eigen::Vector3d>(*context), std::logic_error,
+      ".*wrong value type.*Eigen.*actual type.*BasicVector.*");
+
+  // Pass a more complicated Eigen object; should still work.
+  const Eigen::Vector4d long_vec(.25, .5, .75, 1.);
+  dut.basic_vec_port.FixValue(&*context, long_vec.tail(3));
+  EXPECT_EQ(dut.basic_vec_port.Eval(*context), Eigen::Vector3d(.5, .75, 1.));
+
+  // Should return a runtime error if the size is wrong.
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      dut.basic_vec_port.FixValue(&*context, long_vec.segment<2>(1)),
+      std::logic_error, ".*expected.*size=3.*actual.*size=2.*");
+
+  const MyVector3d my_vector(expected_vec);
+  dut.basic_vec_port.FixValue(&*context, my_vector);  // (2)
+  dut.derived_vec_port.FixValue(&*context, my_vector);
+
+  EXPECT_EQ(dut.basic_vec_port.Eval<MyVector3d>(*context).CopyToVector(),
+            expected_vec);
+  EXPECT_EQ(dut.derived_vec_port.Eval<MyVector3d>(*context).CopyToVector(),
+            expected_vec);
+
+  const BasicVector<double> basic_vector3({7., 8., 9.});
+  dut.basic_vec_port.FixValue(&*context, basic_vector3);  // (2)
+  dut.derived_vec_port.FixValue(&*context, basic_vector3);
+
+  // This is the right type, wrong size for the vector ports.
+  const BasicVector<double> basic_vector2({19., 20.});
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      dut.basic_vec_port.FixValue(&*context, basic_vector2), std::logic_error,
+      ".*expected.*size=3.*actual.*size=2.*");
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      dut.derived_vec_port.FixValue(&*context, basic_vector2), std::logic_error,
+      ".*expected.*size=3.*actual.*size=2.*");
+
+  dut.int_port.FixValue(&*context, 17);  // (5)
+  EXPECT_EQ(dut.int_port.Eval<int>(*context), 17);
+
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      dut.int_port.FixValue(&*context, 1.25), std::logic_error,
+      ".*expected value of type int.*actual type was double.*");
+
+  dut.double_port.FixValue(&*context, 1.25);  // (5)
+  EXPECT_EQ(dut.double_port.Eval<double>(*context), 1.25);
+
+  // Without an explicit template argument, API (5) won't do numerical
+  // conversions.
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      dut.double_port.FixValue(&*context, 4),  // (5)
+      std::logic_error,
+      ".*expected value of type double.*actual type was int.*");
+
+  // But an explicit template argument can serve as a workaround.
+  dut.double_port.FixValue<double>(&*context, 4);  // (5)
+  EXPECT_EQ(dut.double_port.Eval<double>(*context), 4.0);
+
+  // Use the string port for a variety of tests:
+  // - the port value can be set as a string or char* constant
+  // - the generic AbstractValue API works
+  // - we can use the returned FixedInputPortValue object to change the value
+
+  // Check the basics.
+  dut.string_port.FixValue(&*context, std::string("dummy"));  // (5)
+  EXPECT_EQ(dut.string_port.Eval<std::string>(*context), "dummy");
+
+  // Test special case API for C string constant, treated as an std::string.
+  dut.string_port.FixValue(&*context, "a c string");  // (4)
+  EXPECT_EQ(dut.string_port.Eval<std::string>(*context), "a c string");
+
+  // Test API (3) that takes an AbstractValue directly.
+
+  // We should only accept the right kind of abstract value.
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      dut.string_port.FixValue(&*context,
+                               *AbstractValue::Make<int>(42)),  // (3)
+      std::logic_error, ".*expected.*type std::string.*actual type was int.*");
+
+  auto& fixed_value = dut.string_port.FixValue(
+      &*context, *AbstractValue::Make<std::string>("abstract"));  // (3)
+  EXPECT_EQ(dut.string_port.Eval<std::string>(*context), "abstract");
+
+  // FixedInputPortValue has a very clunky interface, but let's make sure we
+  // at least got the right one.
+  fixed_value.GetMutableData()->GetMutableValue<std::string>() =
+      "replacement string";
+  EXPECT_EQ(dut.string_port.Eval<std::string>(*context), "replacement string");
+
+  // All of the ports should have values by now.
+  for (int i = 0; i < dut.get_num_input_ports(); ++i) {
+    EXPECT_TRUE(dut.get_input_port(i).HasValue(*context));
+  }
 }
 
 }  // namespace
