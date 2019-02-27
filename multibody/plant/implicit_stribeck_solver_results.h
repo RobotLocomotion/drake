952944dbfe9b289c9d52f2edb7c3e28ab1e7d0d4
amcastro-tri <alejandro.castro@tri.global>
2019-02-27 12:53:58 -0500

    Documents ImplicitStribeckSolverResults

--- multibody/plant/implicit_stribeck_solver_results.h
+++ multibody/plant/implicit_stribeck_solver_results.h
@@ -7,17 +7,30 @@
 namespace drake {
 namespace multibody {
 
+/// This struct stores the results from a computation performed with
+/// ImplicitStribeckSolver. See the ImplicitStribeckSolver class's documentation
+/// for further details.
+/// We denote with `nv` the size of the vector of generalized velocities and
+/// with `nc` the number of contact points.
 template <class T>
 struct ImplicitStribeckSolverResults {
-  /// Next time step generalized velocities.
+  /// Next time step vector of generalized velocities.
   VectorX<T> v_next;
 
+  /// Vector storing the normal force (positive) at each contact point, of size
+  /// `nc`.
   VectorX<T> fn;
+
+  /// Vector of tangential velocities in the contact frame, of size `2⋅nc`.
   VectorX<T> ft;
 
+  /// Vector of normal separation speeds, of size `nc`.
   VectorX<T> vn;
+
+  /// Vector of tangential relative velocities, of size `2⋅nc`.
   VectorX<T> vt;
 
+  /// Vector of generalized forces due to contact, of size `nv`.
   VectorX<T> tau_contact;
 };
 
