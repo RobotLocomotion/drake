fc9663a410496cf6ee068f2c89184baecfd22dd8
Michael Sherman <sherm1@gmail.com>
2019-03-07 18:08:58 -0800

    Implement policy variants for numerical vector vs. abstract objects.

--- systems/framework/test/value_to_abstract_value_test.cc
+++ systems/framework/test/value_to_abstract_value_test.cc
@@ -1,6 +1,7 @@
 #include "drake/systems/framework/value_to_abstract_value.h"
 
 #include <memory>
+#include <type_traits>
 
 #include <gtest/gtest.h>
 
@@ -80,7 +81,7 @@ class BaseClassCloneable : public JustCloneable {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(BaseClassCloneable)
   BaseClassCloneable(std::string name, double value)
-      : JustCloneable(value), name_(name) {}
+      : JustCloneable(value), name_(std::move(name)) {}
   std::string name() const { return name_; }
 
  private:
@@ -97,7 +98,7 @@ class DerivedClassCloneable : public JustCloneable {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(DerivedClassCloneable)
   DerivedClassCloneable(std::string name, double value)
-      : JustCloneable(value), name_(name) {}
+      : JustCloneable(value), name_(std::move(name)) {}
   std::unique_ptr<DerivedClassCloneable> Clone() const {
     return std::make_unique<DerivedClassCloneable>(name_, value());
   }
@@ -112,9 +113,10 @@ class DerivedClassCloneable : public JustCloneable {
 };
 
 // Test each of the five ToAbstract() methods. For reference:
-// (1) ToAbstract(Eigen::Ref) should convert to BasicVector
-// (2) ToAbstract(BasicVector) special-cased to make concrete vector types work
-//                             regardless of whether they overload Clone()
+// (1) [VectorPolicy only] ToAbstract(Eigen::Ref) should convert to BasicVector
+// (2) [VectorPolicy only] ToAbstract(BasicVector) special-cased to make
+//                         concrete vector types work regardless of whether they
+//                         overload Clone()
 // (3) ToAbstract(AbstractValue) cloned directly without wrapping in Value<>
 // (4) ToAbstract(const char*) special-cased shorthand for std::string
 // (5) ToAbstract<V>(V) creates Value<V> for copyable or cloneable types V,
@@ -125,39 +127,45 @@ class DerivedClassCloneable : public JustCloneable {
 GTEST_TEST(ValueToAbstractValue, GenericObjects) {
   // All methods return std::unique_ptr<AbstractValue>.
 
-  const auto int_val = ValueToAbstractValue<double>::ToAbstract(5);  // (5)
+  // Both policies are the same for non-vector objects. We'll just mix them
+  // up below but not attempt to test every combination separately.
+  using VectorPolicy = ValueToAbstractValue<double, true>;
+  using AbstractPolicy = ValueToAbstractValue<double, false>;
+
+  const auto int_val = VectorPolicy::ToAbstract(5);  // (5)
   EXPECT_EQ(int_val->GetValue<int>(), 5);
 
   const auto str_val =
-      ValueToAbstractValue<double>::ToAbstract(std::string("string"));  // (5)
+      AbstractPolicy::ToAbstract(std::string("string"));  // (5)
   EXPECT_EQ(str_val->GetValue<std::string>(), "string");
 
   // char* values are stored in std::string AbstractValues.
-  const auto char_ptr_val = ValueToAbstractValue<double>::ToAbstract("char*");
+  const auto char_ptr_val = VectorPolicy::ToAbstract("char*");
   EXPECT_EQ(char_ptr_val->GetValue<std::string>(), "char*");  // (4)
 
-  const auto copyable_val = ValueToAbstractValue<double>::ToAbstract(
-      CopyConstructible(1.25));  // (5)
+  const auto copyable_val =
+      AbstractPolicy::ToAbstract(CopyConstructible(1.25));  // (5)
   EXPECT_EQ(copyable_val->GetValue<CopyConstructible>().value(), 1.25);
 
-  const auto cloneable_val = ValueToAbstractValue<double>::ToAbstract(
-      JustCloneable(0.75));  // (5)
+  const auto cloneable_val =
+      VectorPolicy::ToAbstract(JustCloneable(0.75));  // (5)
   EXPECT_EQ(cloneable_val->GetValue<JustCloneable>().value(), 0.75);
 
   // Here ToAbstract() has to discover that the available Clone() method
   // returns a base class-typed object, rather than the concrete type we give
   // it here, so we have to store it in a Value<BaseType>.
-  const auto base_cloneable_val = ValueToAbstractValue<double>::ToAbstract(
+  const auto base_cloneable_val = AbstractPolicy::ToAbstract(
       BaseClassCloneable("base_cloneable", 1.5));  // (5)
   EXPECT_EQ(base_cloneable_val->GetValue<JustCloneable>().value(), 1.5);
   EXPECT_EQ(dynamic_cast<const BaseClassCloneable&>(
-                base_cloneable_val->GetValue<JustCloneable>()).name(),
+                base_cloneable_val->GetValue<JustCloneable>())
+                .name(),
             "base_cloneable");
 
   // Here ToAbstract() should discover that the available Clone() method
   // returns the derived class-typed object, so we can store that directly
   // in a Value<DerivedType>.
-  const auto derived_cloneable_val = ValueToAbstractValue<double>::ToAbstract(
+  const auto derived_cloneable_val = VectorPolicy::ToAbstract(
       DerivedClassCloneable("derived_cloneable", 0.25));  // (5)
   EXPECT_EQ(derived_cloneable_val->GetValue<DerivedClassCloneable>().value(),
             0.25);
@@ -171,7 +179,7 @@ GTEST_TEST(ValueToAbstractValue, GenericObjects) {
   EXPECT_FALSE(cc_value.was_copy_constructed());
 
   const auto cc_val_abstract_copy =
-      ValueToAbstractValue<double>::ToAbstract(cc_value);  // (5)
+      AbstractPolicy::ToAbstract(cc_value);  // (5)
   const auto& cc_val_copy =
       cc_val_abstract_copy->GetValue<CopyableAndCloneable>();
   EXPECT_EQ(cc_val_copy.value(), 2.25);
@@ -179,58 +187,90 @@ GTEST_TEST(ValueToAbstractValue, GenericObjects) {
   EXPECT_TRUE(cc_val_copy.was_copy_constructed());
   EXPECT_EQ(cc_val_copy.num_clones(), 0);
 
-  // This is also (5) but should trigger a nice static_assert message if
+  // These are also (5) but should trigger a nice static_assert message if
   // uncommented, similar to:
   //   static assertion failed: ValueToAbstractValue(): value type must be
   //   copy constructible or have an accessible Clone() method that returns
   //   std::unique_ptr.
-  // ValueToAbstractValue<double>::ToAbstract(NotCopyableOrCloneable(2.));
+  // VectorPolicy::ToAbstract(NotCopyableOrCloneable(2.));
+  // AbstractPolicy::ToAbstract(NotCopyableOrCloneable(2.));
 }
 
-// Test variants (1) and (2) which deal with Eigen vectors and BasicVectors,
-// respectively.
+// Test signature (1) and (2) which deal with Eigen vectors and BasicVectors,
+// respectively, under VectorPolicy. Under AbstractPolicy, signature (5) gets
+// used for everything here as in the previous test.
 GTEST_TEST(ValueToAbstractValue, Vectors) {
   const Eigen::Vector3d expected_vec(10., 20., 30.);
 
-  // Any Eigen vector type should be stored as a BasicVector.
-  const auto vec3_val =
-      ValueToAbstractValue<double>::ToAbstract(expected_vec);  // (1)
+  // Handling of Eigen vector and BasicVector-derived objects V differs between
+  // the two policies. The VectorPolicy will always use Value<BasicVector> to
+  // hold either kind of object, while the AbstractPolicy will use Value<X>
+  // where X is V if V is copy constructible, otherwise the return type
+  // of V::Clone(), which may be V or one of V's base classes.
+  //
+  // Handling of plain BasicVector objects is identical in both policies.
+  using VectorPolicy = ValueToAbstractValue<double, true>;
+  using AbstractPolicy = ValueToAbstractValue<double, false>;
+
+  // Any Eigen vector type should be stored as a BasicVector under VectorPolicy,
+  // otherwise as itself.
+  auto vec3_val = VectorPolicy::ToAbstract(expected_vec);  // (1)
   EXPECT_EQ(vec3_val->GetValue<BasicVector<double>>().get_value(),
-      expected_vec);
+            expected_vec);
+
+  vec3_val = AbstractPolicy::ToAbstract(expected_vec);  // (5)
+  EXPECT_EQ(vec3_val->GetValue<Eigen::Vector3d>(), expected_vec);
 
   // Pass a more complicated Eigen object; should still work.
   const Eigen::Vector4d long_vec(.25, .5, .75, 1.);
-  const auto tail3_val =
-      ValueToAbstractValue<double>::ToAbstract(long_vec.tail(3));  // (1)
+  const auto last3 = long_vec.tail(3).eval();
+  auto tail3_val = VectorPolicy::ToAbstract(last3);  // (1)
   EXPECT_EQ(tail3_val->GetValue<BasicVector<double>>().get_value(),
             Eigen::Vector3d(.5, .75, 1.));
 
-  const auto segment2_val =
-      ValueToAbstractValue<double>::ToAbstract(long_vec.segment<2>(1));  // (1)
+  tail3_val = AbstractPolicy::ToAbstract(last3);  // (5)
+  EXPECT_EQ(tail3_val->GetValue<std::remove_const_t<decltype(last3)>>(),
+            Eigen::Vector3d(.5, .75, 1.));
+
+  auto segment2_val = VectorPolicy::ToAbstract(long_vec.segment<2>(1));  // (1)
   EXPECT_EQ(segment2_val->GetValue<BasicVector<double>>().get_value(),
             Eigen::Vector2d(.5, .75));
+  segment2_val =
+      AbstractPolicy::ToAbstract(long_vec.segment<2>(1).eval());  // (5)
+  EXPECT_EQ(segment2_val->GetValue<Eigen::Vector2d>(),
+            Eigen::Vector2d(.5, .75));
 
-  // A plain BasicVector is stored as a Value<BasicVector>, but is still
-  // processed by the BasicVector overload (2) of ToAbstract.
+  // A plain BasicVector is stored as a Value<BasicVector>. It is handled
+  // by the BasicVector overload (2) for the VectorPolicy and the fallback
+  // overload (5) for the AbstractPolicy, but with identical results.
   const BasicVector<double> basic_vector3({7., 8., 9.});
-  const auto basic_vector_val =
-      ValueToAbstractValue<double>::ToAbstract(basic_vector3);  // (2)
+  const auto basic_vector_val = VectorPolicy::ToAbstract(basic_vector3);  // (2)
   EXPECT_EQ(basic_vector_val->GetValue<BasicVector<double>>().get_value(),
+            Eigen::Vector3d(7., 8., 9.));
+  const auto basic_vector_val2 =
+      AbstractPolicy::ToAbstract(basic_vector3);  // (5)
+  EXPECT_EQ(basic_vector_val2->GetValue<BasicVector<double>>().get_value(),
             Eigen::Vector3d(7., 8., 9.));
 
   // MyVector derives from BasicVector and has its own Clone() method
   // that returns unique_ptr<MyVector>. *Despite that*, and unlike treatment
-  // of other cloneable classes, it should be stored as a Value<BasicVector>,
-  // which is why variant (2) of ToAbstract exists. However, the concrete
+  // of other cloneable classes, under the VectorPolicy it should be stored as a
+  // Value<BasicVector>, which is why signature (2) exists for that policy.
+  // Under the AbstractPolicy, this is handled like any other cloneable object
+  // by signature (5) and stored as Value<MyVector>. Either way, the concrete
   // type must be preserved.
   const MyVector3d my_vector3(expected_vec);
-  const auto my_vector3_val =
-      ValueToAbstractValue<double>::ToAbstract(my_vector3);  // (2)
+  auto my_vector3_val = VectorPolicy::ToAbstract(my_vector3);  // (2)
   const BasicVector<double>& basic_vector =
       my_vector3_val->GetValue<BasicVector<double>>();
   EXPECT_EQ(basic_vector.get_value(), expected_vec);
   EXPECT_EQ(dynamic_cast<const MyVector3d&>(basic_vector).get_value(),
             expected_vec);
+
+  my_vector3_val = AbstractPolicy::ToAbstract(my_vector3);  // (5)
+  const MyVector3d& my_vector3_from_abstract =
+      my_vector3_val->GetValue<MyVector3d>();
+  EXPECT_EQ(my_vector3_from_abstract.get_value(), expected_vec);
 }
 
 // Test variant (3) of ToAbstract() that deals with AbstractValues.
@@ -239,16 +279,18 @@ GTEST_TEST(ValueToAbstractValue, AbstractValue) {
   std::unique_ptr<AbstractValue> absval_str =
       AbstractValue::Make<std::string>("hello");
 
-  const auto absval_str_copy =
-      ValueToAbstractValue<double>::ToAbstract(*absval_str);  // (3)
+  // The two policies are identical for AbstractValue and Value<T> objects.
+  using VectorPolicy = ValueToAbstractValue<double, true>;
+  using AbstractPolicy = ValueToAbstractValue<double, false>;
+
+  const auto absval_str_copy = VectorPolicy::ToAbstract(*absval_str);  // (3)
   EXPECT_EQ(absval_str_copy->GetValue<std::string>(), "hello");
   // Make sure we're not just looking at the same object.
   absval_str->GetMutableValue<std::string>() = "goodbye";
   EXPECT_EQ(absval_str_copy->GetValue<std::string>(), "hello");  // no change
 
   // Pass an object of AbstractValue-derived type.
-  const auto val_int =
-      ValueToAbstractValue<double>::ToAbstract(Value<int>(10));  // (3)
+  const auto val_int = AbstractPolicy::ToAbstract(Value<int>(10));  // (3)
   EXPECT_EQ(val_int->GetValue<int>(), 10);
 }
 
--- systems/framework/value_to_abstract_value.h
+++ systems/framework/value_to_abstract_value.h
@@ -14,12 +14,21 @@ namespace drake {
 namespace systems {
 namespace internal {
 /**
-Implements Drake policy for taking an object of a concrete type and
-storing it in an AbstractValue. The policy enforced here by these five
-overloads is:
- 1. Any Eigen vector type is copied into a `Value<BasicVector<T>>`.
- 2. Any BasicVector or type derived from BasicVector is cloned into a
-    `Value<BasicVector<T>>` that preserves the concrete type.
+Implements Drake policies for taking an object of a concrete type and
+storing it in an AbstractValue. There are subtly different policies regarding
+the handling of Eigen vector and BasicVector objects depending on whether the
+intended destination is a Drake numerical vector object (e.g., a vector port) or
+an abstract object. Call these "VectorPolicy" and "AbstractPolicy", resp.
+
+There are five overloads for the VectorPolicy and three for the AbstractPolicy.
+Signatures (1) and (2) are not present for the AbstractPolicy. The policies
+enforced by each of these methods are:
+
+ 1. [VectorPolicy only] Any Eigen vector type is copied into a
+    `Value<BasicVector<T>>`.
+ 2. [VectorPolicy only] Any BasicVector or type derived from BasicVector is
+    cloned into a `Value<BasicVector<T>>` although the stored object still has
+    the most-derived type.
  3. Any AbstractValue object is simply cloned.
  4. A `char *` type is copied into a Value<std::string>.
  5. For any other type V
@@ -30,34 +39,52 @@ overloads is:
       where B is a base class of T, it is cloned into a `Value<B>`;
     - otherwise, compilation fails with a `static_assert` message.
 
+For the AbstractPolicy, overload (5) is used for Eigen vectors and BasicVectors
+which are treated in that case exactly like any other copyable or cloneable
+types.
+
+@warning Eigen expressions typically don't have simple Vector types. That
+doesn't matter under the VectorPolicy. However, under the AbstractPolicy you
+will get Value<V> for whatever odd Eigen operator type you provided. To avoid
+trouble, you should always pass a simple Eigen-typed variable, or use `.eval()`
+at the end of your Eigen expression to turn it into a simple type.
+
+<!-- TODO(sherm1) Figure out how to make the `.eval()` effect happen
+                  automatically. -->
+
 @tparam T The scalar type in use for vector objects.
+@tparam use_vector_policy Whether to use VectorPolicy or AbstractPolicy.
 */
-template <typename T>
+template <typename T, bool use_vector_policy>
 class ValueToAbstractValue {
  public:
-  // This signature is used for any Eigen vector type, but the argument is
-  // copied to a BasicVector for the returned abstract value.
+  // Signature (1) (VectorPolicy only): used for any Eigen vector type, but the
+  // argument is copied to a BasicVector for the returned abstract value.
+  template <bool keep_this = use_vector_policy,
+      typename = std::enable_if_t<keep_this>>
   static std::unique_ptr<AbstractValue> ToAbstract(
       const Eigen::Ref<const VectorX<T>>& vector) {
     return std::make_unique<Value<BasicVector<T>>>(vector);
   }
 
-  // BasicVector must be special-cased so that we use a Value<BasicVector>
-  // to store a copy of the given object, even if it is from a subclass of
-  // BasicVector, and even if it has its own Clone() method. The actual type
-  // is preserved regardless.
+  // Signature (2) (VectorPolicy only): BasicVector must be
+  // special-cased so that we use a Value<BasicVector> to store a copy of the
+  // given object, even if it is from a subclass of BasicVector, and even if it
+  // has its own Clone() method. The actual type is preserved regardless.
+  template <bool keep_this = use_vector_policy,
+            typename = std::enable_if_t<keep_this>>
   static std::unique_ptr<AbstractValue> ToAbstract(
       const BasicVector<T>& vector) {
     return std::make_unique<Value<BasicVector<T>>>(vector.Clone());
   }
 
-  // This signature is used for AbstractValue or Value<U> arguments.
+  // Signature (3): used for AbstractValue or Value<U> arguments.
   static std::unique_ptr<AbstractValue> ToAbstract(const AbstractValue& value) {
     return value.Clone();
   }
 
-  // Special case char* to std::string to avoid ugly compilation messages
-  // for this case, where the user's intent is obvious.
+  // Signature (4): special case char* to std::string to avoid ugly compilation
+  // messages for this case, where the user's intent is obvious.
   static std::unique_ptr<AbstractValue> ToAbstract(const char* c_string) {
     return std::make_unique<Value<std::string>>(c_string);
   }
@@ -79,18 +106,21 @@ class ValueToAbstractValue {
     return has_accessible_clone_helper<ValueType>(1);  // Any int will do here.
   }
 
-  // This method won't instantiate if any of the non-templatized signatures
-  // above can be used (after possible conversions). If we allowed this to
-  // instantiate it would be chosen instead of performing those conversions.
+  // Signature (5): This signature won't instantiate if any of the
+  // non-templatized signatures above can be used (after possible conversions).
+  // If we allowed this to instantiate it would be chosen instead of performing
+  // those conversions. Note that this, rather than signature (2), will be used
+  // for BasicVector ValueTypes for abstract destinations but not for numerical
+  // vector destinations.
   template <typename ValueType,
             typename = std::enable_if_t<
-                !(is_eigen_refable<ValueType>() ||
-                  std::is_base_of<BasicVector<T>, ValueType>::value ||
-                  std::is_base_of<AbstractValue, ValueType>::value)>>
+                !(std::is_base_of<AbstractValue, ValueType>::value ||
+                  (use_vector_policy &&
+                   (is_eigen_refable<ValueType>() ||
+                    std::is_base_of<BasicVector<T>, ValueType>::value)))>>
   static std::unique_ptr<AbstractValue> ToAbstract(const ValueType& value) {
-    static_assert(
-        std::is_copy_constructible<ValueType>::value ||
-            has_accessible_clone<ValueType>(),
+    static_assert(std::is_copy_constructible<ValueType>::value ||
+                      has_accessible_clone<ValueType>(),
         "ValueToAbstractValue(): value type must be copy constructible or "
         "have an accessible Clone() method that returns std::unique_ptr.");
     return ValueHelper(value, 1, 1);
