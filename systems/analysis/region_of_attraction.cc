7eedae34bdce23bb8ddf2fd3d1263e82e1a02ea5
Russ Tedrake <russt@mit.edu>
2019-03-11 06:51:04 -0400

    First implementation of general region of attraction code. Implements the convex formulation of searching for a maximal level set of a Lyapunov candidate. Working towards #10863

---
+++ systems/analysis/region_of_attraction.cc
@@ -0,0 +1,157 @@
+#include "drake/systems/analysis/region_of_attraction.h"
+
+#include "drake/math/continuous_lyapunov_equation.h"
+#include "drake/math/matrix_util.h"
+#include "drake/solvers/mathematical_program.h"
+#include "drake/solvers/solve.h"
+
+namespace drake {
+namespace systems {
+namespace analysis {
+
+using solvers::MathematicalProgram;
+using solvers::Solve;
+
+using symbolic::Environment;
+using symbolic::Expression;
+using symbolic::Polynomial;
+using symbolic::Substitution;
+using symbolic::Variable;
+using symbolic::Variables;
+
+// Assumes V>0 and the Hessian of Vdot<0 at the origin.
+// Use Vdot = 0 => V >= rho (or x=0) via
+//   maximize   rho
+//   subject to (V-rho)*x'*x - Lambda*Vdot is SOS.
+symbolic::Expression FixedLyapunovConvex(const solvers::VectorXIndeterminate& x,
+                                         const Expression& V,
+                                         const Expression& Vdot) {
+  MathematicalProgram prog;
+  prog.AddIndeterminates(x);
+
+  // TODO(russt): Add this as an argument once I have an example that needs it.
+  // This is a reasonable guess.
+  const int V_degree = Polynomial(V).TotalDegree();
+  const int Vdot_degree = Polynomial(Vdot).TotalDegree();
+  const int lambda_degree = Vdot_degree;
+  const auto lambda =
+      prog.NewFreePolynomial(Variables(x), lambda_degree).ToExpression();
+
+  const auto rho = prog.NewContinuousVariables<1>("rho")[0];
+  // Want (V-rho)(x'x)^d and Lambda*Vdot to be the same degree.
+  const int d = std::floor((lambda_degree + Vdot_degree - V_degree) / 2);
+  prog.AddSosConstraint((V - rho) * pow((x.transpose() * x)[0], d) -
+                        lambda * Vdot);
+
+  prog.AddCost(-rho);
+  const auto result = Solve(prog);
+
+  DRAKE_THROW_UNLESS(result.is_success());
+
+  DRAKE_THROW_UNLESS(result.GetSolution(rho) > 0.0);
+  return V / result.GetSolution(rho);
+}
+
+Expression RegionOfAttraction(const System<double>& system,
+                              const Context<double>& context,
+                              const RegionOfAttractionOptions& options) {
+  system.ValidateContext(context);
+  DRAKE_THROW_UNLESS(context.has_only_continuous_state());
+
+  const int num_states = context.num_continuous_states();
+  VectorX<double> x0 = context.get_continuous_state_vector().CopyToVector();
+
+  // Check that x0 is a fixed point.
+  VectorX<double> xdot0 =
+      system.EvalTimeDerivatives(context).get_vector().CopyToVector();
+  DRAKE_THROW_UNLESS(xdot0.template lpNorm<Eigen::Infinity>() <= 1e-14);
+
+  const auto symbolic_system = system.ToSymbolic();
+  const auto symbolic_context = symbolic_system->CreateDefaultContext();
+  // Time and parameters should just be doubles (not Variables).
+  symbolic_context->SetTime(0.0);
+  symbolic_context->get_mutable_parameters().SetFrom(context.get_parameters());
+
+  // Subroutines should create their own programs to avoid incidental
+  // sharing of costs or constraints.  However, we pass x and expect that
+  // sub-programs will use AddIndeterminates(x).
+  MathematicalProgram prog;
+  const auto x = prog.NewIndeterminates(num_states, "x");
+
+  Environment x0env;
+  for (int i = 0; i < num_states; i++) {
+    x0env.insert(x(i), x0(i));
+  }
+
+  // Evaluate the dynamics (in relative coordinates).
+  symbolic_context->SetContinuousState(x0 + x);
+  const auto f = symbolic_system->EvalTimeDerivatives(*symbolic_context)
+                     .get_vector()
+                     .CopyToVector();
+
+  Expression V = options.lyapunov_candidate;
+
+  if (V.EqualTo(Expression::Zero())) {
+    // Then the caller did NOT pass in a candidate Lyapunov function.
+    // Solve a Lyapunov equation to find a candidate.
+    const auto A = Evaluate(Jacobian(f, x), x0env);
+    const auto Q = Eigen::MatrixXd::Identity(num_states, num_states);
+    const auto P = math::RealContinuousLyapunovEquation(A, Q);
+    V = x.dot(P * x);
+  } else {
+    // The caller did pass in a candidate Lyapunov function.
+    // If necessary, replace the state variables.
+    if (options.state_variables.rows() > 0) {
+      Substitution subs;
+      subs.reserve(num_states);
+      for (int i = 0; i < num_states; i++) {
+        subs.emplace(options.state_variables(i), x(i));
+      }
+      V = V.Substitute(subs);
+    }
+
+    // Check that V has the right Variables.
+    DRAKE_THROW_UNLESS(V.GetVariables().IsSubsetOf(Variables(x)));
+
+    // Check that V is positive definite.
+    prog.AddSosConstraint(V);
+    const auto result = Solve(prog);
+    DRAKE_THROW_UNLESS(result.is_success());
+  }
+
+  const auto Vdot = V.Jacobian(x).dot(f);
+
+  if (!options.lyapunov_candidate.EqualTo(Expression::Zero())) {
+    // Check that the Hessian of Vdot is negative definite.
+    const Eigen::MatrixXd H =
+        symbolic::Evaluate(symbolic::Jacobian(Vdot.Jacobian(x), x), x0env);
+    DRAKE_THROW_UNLESS(math::IsPositiveDefinite(-H, 1e-8));
+  }
+
+  // TODO(russt): implement numerical "balancing" from matlab version.
+  // https://github.com/RobotLocomotion/drake/blob
+  //        /last_sha_with_original_matlab/drake/matlab/systems
+  //        /%40PolynomialSystem/regionOfAttraction.m
+
+  V = FixedLyapunovConvex(x, V, Vdot);
+
+  // Put V back into global coordinates.
+  Substitution subs;
+  subs.reserve(num_states);
+  if (options.state_variables.rows() > 0) {
+    for (int i = 0; i < num_states; i++) {
+      subs.emplace(x(i), options.state_variables(i) - x0(i));
+    }
+  } else {
+    for (int i = 0; i < num_states; i++) {
+      subs.emplace(x(i), x(i) - x0(i));
+    }
+  }
+  V = V.Substitute(subs);
+
+  return V;
+}
+
+}  // namespace analysis
+}  // namespace systems
+}  // namespace drake
