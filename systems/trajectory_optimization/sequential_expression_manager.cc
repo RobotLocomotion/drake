b05013b1cf88ae0beac1f375d7719b3330b5d009
Andrés Valenzuela <andres.valenzuela@tri.global>
2019-03-12 11:00:33 -0400

    Add MultipleShooting::NewSequentialContinuousVariable().
    This method returns a vector of placeholder variables that can be used with the
    newly public `ConstructPlaceholderVariableSubstitution()`. The actual decision
    variables for a particular time index can be retrieved with
    `GetSequentialVariablesByName()`.

---
+++ systems/trajectory_optimization/sequential_expression_manager.cc
@@ -0,0 +1,66 @@
+#include "drake/systems/trajectory_optimization/sequential_expression_manager.h"
+
+#include <fmt/format.h>
+
+namespace drake {
+namespace systems {
+namespace trajectory_optimization {
+namespace internal {
+
+using symbolic::Substitution;
+using symbolic::Expression;
+using symbolic::Variable;
+using symbolic::Variables;
+
+SequentialExpressionManager::SequentialExpressionManager(int num_samples)
+    : num_samples_(num_samples) {
+  DRAKE_DEMAND(num_samples_ > 0);
+}
+
+VectorX<Variable> SequentialExpressionManager::RegisterSequentialExpressions(
+    const Eigen::Ref<const MatrixX<Expression>>& sequential_expressions,
+    const std::string& name) {
+  const int rows = sequential_expressions.rows();
+  DRAKE_DEMAND(sequential_expressions.cols() == num_samples_);
+  VectorX<Variable> placeholders{rows};
+  for (int i = 0; i < rows; ++i) {
+    placeholders(i) = Variable(fmt::format("{}{}", name, i));
+  }
+  const auto pair = name_to_placeholders_and_sequential_expressions_.insert(
+      {name, {placeholders, sequential_expressions}});
+  DRAKE_DEMAND(pair.second);
+  return placeholders;
+}
+
+void SequentialExpressionManager::AddPlaceholderVariableSubstitutionsForIndex(
+    int index, Substitution* substitution) const {
+  DRAKE_DEMAND(substitution);
+  DRAKE_DEMAND(0 <= index && index < num_samples_);
+  for (const auto& pair : name_to_placeholders_and_sequential_expressions_) {
+    // pair.first is the name, which we don't need here.
+    const VectorX<Variable>& placeholders = pair.second.first;
+    const MatrixX<Expression>& sequential_expressions = pair.second.second;
+    const int rows = placeholders.rows();
+    for (int row = 0; row < rows; ++row) {
+      substitution->emplace(placeholders(row),
+                            sequential_expressions(row, index));
+    }
+  }
+}
+
+VectorX<Expression>
+SequentialExpressionManager::GetSequentialExpressionsByName(
+    const std::string& name, int index) const {
+  DRAKE_DEMAND(0 <= index && index < num_samples_);
+  const auto it = name_to_placeholders_and_sequential_expressions_.find(name);
+  DRAKE_DEMAND(it != name_to_placeholders_and_sequential_expressions_.end());
+  const MatrixX<Expression>& sequential_expressions = it->second.second;
+  return sequential_expressions.col(index);
+}
+
+int SequentialExpressionManager::num_samples() const { return num_samples_; }
+
+}  // namespace internal
+}  // namespace trajectory_optimization
+}  // namespace systems
+}  // namespace drake
---
+++ systems/trajectory_optimization/test/sequential_expression_manager_test.cc
@@ -0,0 +1,117 @@
+#include "drake/systems/trajectory_optimization/sequential_expression_manager.h"
+
+#include <fmt/format.h>
+#include <gtest/gtest.h>
+
+#include "drake/common/eigen_types.h"
+#include "drake/common/symbolic.h"
+
+namespace drake {
+namespace systems {
+namespace trajectory_optimization {
+namespace internal {
+
+using symbolic::Expression;
+using symbolic::Variable;
+
+class SequentialExpressionManagerTests : public testing::Test {
+ public:
+  MatrixX<Variable> MakeVariableMatrix(int rows, int cols) const {
+    MatrixX<Variable> variable{rows, cols};
+    for (int i = 0; i < rows; ++i) {
+      for (int j = 0; j < cols; ++j) {
+        variable(i, j) = Variable(fmt::format("x_{}_{}", i, j));
+      }
+    }
+    return variable;
+  }
+
+ protected:
+  const int num_variables{3};
+  const int num_samples{5};
+  SequentialExpressionManager dut{num_samples};
+};
+
+// Verify that num_samples() works as expected.
+TEST_F(SequentialExpressionManagerTests, NumSamplesTest) {
+  EXPECT_EQ(dut.num_samples(), num_samples);
+}
+
+// Test with a matrix of Expressions.
+TEST_F(SequentialExpressionManagerTests, RegisterAndSubstituteExpressionTest) {
+  MatrixX<Expression> x_sequential =
+      MakeVariableMatrix(num_variables, num_samples);
+  x_sequential.cwiseSqrt();
+  VectorX<Variable> x_placeholder =
+      dut.RegisterSequentialExpressions(x_sequential.cast<Expression>(), "x");
+  MatrixX<Expression> placeholder_expression =
+      x_placeholder * x_placeholder.transpose();
+  for (int j = 0; j < num_samples; ++j) {
+    MatrixX<Expression> expected_expression =
+        x_sequential.col(j) * x_sequential.col(j).transpose();
+    symbolic::Substitution subs;
+    dut.AddPlaceholderVariableSubstitutionsForIndex(j, &subs);
+    EXPECT_EQ(symbolic::Substitute(placeholder_expression, subs),
+              expected_expression);
+  }
+}
+
+// Test with an Eigen::Map applied to a vector of Expressions.
+TEST_F(SequentialExpressionManagerTests, RegisterAndSubstituteMapTest) {
+  VectorX<Expression> x_sequential =
+      MakeVariableMatrix(num_variables * num_samples, 1);
+  VectorX<Variable> x_placeholder = dut.RegisterSequentialExpressions(
+      Eigen::Map<MatrixX<Expression>>(x_sequential.data(), num_variables,
+                                      num_samples),
+      "x");
+  MatrixX<Expression> placeholder_expression =
+      x_placeholder * x_placeholder.transpose();
+  for (int j = 0; j < num_samples; ++j) {
+    MatrixX<Expression> expected_expression =
+        x_sequential.segment(j * num_variables, num_variables) *
+        x_sequential.segment(j * num_variables, num_variables).transpose();
+    symbolic::Substitution subs;
+    dut.AddPlaceholderVariableSubstitutionsForIndex(j, &subs);
+    EXPECT_EQ(symbolic::Substitute(placeholder_expression, subs),
+              expected_expression);
+  }
+}
+
+// Test with an Eigen::Map and cast() applied to a vector of Variables.
+TEST_F(SequentialExpressionManagerTests, RegisterAndSubstituteMapCastTest) {
+  VectorX<Variable> x_sequential =
+      MakeVariableMatrix(num_variables * num_samples, 1);
+  VectorX<Variable> x_placeholder = dut.RegisterSequentialExpressions(
+      Eigen::Map<MatrixX<Variable>>(x_sequential.data(), num_variables,
+                                    num_samples)
+          .cast<Expression>(),
+      "x");
+  MatrixX<Expression> placeholder_expression =
+      x_placeholder * x_placeholder.transpose();
+  for (int j = 0; j < num_samples; ++j) {
+    MatrixX<Expression> expected_expression =
+        x_sequential.segment(j * num_variables, num_variables) *
+        x_sequential.segment(j * num_variables, num_variables).transpose();
+    symbolic::Substitution subs;
+    dut.AddPlaceholderVariableSubstitutionsForIndex(j, &subs);
+    EXPECT_EQ(symbolic::Substitute(placeholder_expression, subs),
+              expected_expression);
+  }
+}
+
+// Verify that GetSequentialExpressionsByName behaves as expected.
+TEST_F(SequentialExpressionManagerTests, GetSequentialExpressionsByNameTest) {
+  MatrixX<Expression> x_sequential =
+      MakeVariableMatrix(num_variables, num_samples);
+  x_sequential.cwiseSqrt();
+  VectorX<Variable> x_placeholder =
+      dut.RegisterSequentialExpressions(x_sequential.cast<Expression>(), "x");
+  for (int j = 0; j < num_samples; ++j) {
+    VectorX<Expression> expected_expression = x_sequential.col(j);
+    EXPECT_EQ(dut.GetSequentialExpressionsByName("x", j), expected_expression);
+  }
+}
+}  // namespace internal
+}  // namespace trajectory_optimization
+}  // namespace systems
+}  // namespace drake
