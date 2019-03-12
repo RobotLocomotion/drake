65fb12f406e2be1e02cd3fb9805b01836fc14596
Rick Cory <rick.cory@tri.global>
2019-03-12 10:54:39 -0700

    Updates DirectTranscription to support multiple inputs.

--- systems/trajectory_optimization/direct_transcription.h
+++ systems/trajectory_optimization/direct_transcription.h
@@ -38,8 +38,29 @@ class DirectTranscription : public MultipleShooting {
   ///    context after calling this method will NOT impact the trajectory
   ///    optimization.
   /// @param num_time_samples The number of knot points in the trajectory.
+  /// @param input_port_index A valid input port index or valid
+  /// InputPortSelection for @p system.  All other inputs on the system will be
+  /// left disconnected (if they are disconnected in @p context) or will be set
+  /// to their current values (if they are connected/fixed in @p context).
+  /// @default kUseFirstInputIfItExists.
+  DirectTranscription(
+      const System<double>* system, const Context<double>& context,
+      int num_time_samples,
+      variant<InputPortSelection, InputPortIndex> input_port_index =
+          InputPortSelection::kUseFirstInputIfItExists);
+
+#ifndef DRAKE_DOXYGEN_CXX
+  /// @exclude_from_pydrake_mkdoc{This constructor disambiguates the simple
+  /// discrete-time System constructor from the continuous-time System
+  /// constructor when an InputPortIndex type is passed in place of a
+  /// variant<InputPortSelection, InputPortIndex> type.}
   DirectTranscription(const System<double>* system,
-                      const Context<double>& context, int num_time_samples);
+                      const Context<double>& context, int num_time_samples,
+                      InputPortIndex input_port_index)
+      : DirectTranscription(
+            system, context, num_time_samples,
+            variant<InputPortSelection, InputPortIndex>(input_port_index)) {}
+#endif
 
   // TODO(russt): Generalize the symbolic short-cutting to handle this case,
   //  and remove the special-purpose constructor (unless we want it for
@@ -58,13 +79,21 @@ class DirectTranscription : public MultipleShooting {
   ///    context after calling this method will NOT impact the trajectory
   ///    optimization.
   /// @param num_time_samples The number of knot points in the trajectory.
+  /// @param input_port_index A valid input port index or valid
+  /// InputPortSelection for @p system.  All other inputs on the system will be
+  /// left disconnected (if they are disconnected in @p context) or will be set
+  /// to their current values (if they are connected/fixed in @p context).
+  /// @default kUseFirstInputIfItExists.
   ///
   /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.  When
   /// we do bind it, we should probably rename `system` to tv_linear_system` or
   /// similar, so that kwargs determine which overload is suggested, instead of
   /// hoping that type checking does the right thing.}
-  DirectTranscription(const TimeVaryingLinearSystem<double>* system,
-                      const Context<double>& context, int num_time_samples);
+  DirectTranscription(
+      const TimeVaryingLinearSystem<double>* system,
+      const Context<double>& context, int num_time_samples,
+      variant<InputPortSelection, InputPortIndex> input_port_index =
+          InputPortSelection::kUseFirstInputIfItExists);
 
   // TODO(russt): Support more than just forward Euler integration
   //  (perhaps by taking IntegratorBase as an optional parameter?).
@@ -82,8 +111,16 @@ class DirectTranscription : public MultipleShooting {
   ///    optimization.
   /// @param num_time_samples The number of knot points in the trajectory.
   /// @param fixed_timestep The spacing between sample times.
-  DirectTranscription(const System<double>* system, const Context<double>&
-      context, int num_time_samples, double fixed_timestep);
+  /// @param input_port_index A valid input port index or valid
+  /// InputPortSelection for @p system.  All other inputs on the system will be
+  /// left disconnected (if they are disconnected in @p context) or will be set
+  /// to their current values (if they are connected/fixed in @p context).
+  /// @default kUseFirstInputIfItExists.
+  DirectTranscription(
+      const System<double>* system, const Context<double>& context,
+      int num_time_samples, double fixed_timestep,
+      variant<InputPortSelection, InputPortIndex> input_port_index =
+          InputPortSelection::kUseFirstInputIfItExists);
 
   // TODO(russt):  Implement constructor for continuous time systems with
   // time as a decision variable; and perhaps add support for mixed
@@ -130,8 +167,9 @@ class DirectTranscription : public MultipleShooting {
   // Attempts to create an autodiff version of the plant, and to impose
   // the generic (nonlinear) constraints to impose the dynamics.
   // Aborts if the conversion ToAutoDiffXd fails.
-  void AddAutodiffDynamicConstraints(const System<double>* system,
-                                     const Context<double>& context);
+  void AddAutodiffDynamicConstraints(
+      const System<double>* system, const Context<double>& context,
+      variant<InputPortSelection, InputPortIndex> input_port_index);
 
   // Constrain the final input to match the penultimate, otherwise the final
   // input is unconstrained.
@@ -146,13 +184,17 @@ class DirectTranscription : public MultipleShooting {
   // provided @p system and @p context have only one group of discrete states
   // and only one (possibly multidimensional) input.
   void ValidateSystem(const System<double>& system,
-                      const Context<double>& context);
+                      const Context<double>& context,
+                      variant<InputPortSelection, InputPortIndex>
+                      input_port_index =
+                      InputPortSelection::kUseFirstInputIfItExists);
 
   // AutoDiff versions of the System components (for the constraints).
   // These values are allocated iff the dynamic constraints are allocated
   // as DiscreteTimeSystemConstraints, otherwise they are nullptr.
   std::unique_ptr<const System<AutoDiffXd>> system_;
   std::unique_ptr<Context<AutoDiffXd>> context_;
+  const InputPort<AutoDiffXd>* input_port_{nullptr};
   FixedInputPortValue* input_port_value_{nullptr};  // Owned by the context.
 
   const bool discrete_time_system_{false};
