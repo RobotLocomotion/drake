3774ea4f97aeb7c7d58d86bb89d6f360dc3b562c
mpetersen94 <markpetersen@g.harvard.edu>
2019-03-19 12:19:21 -0400

    Add AbstractValue support to DiscreteTimeDelay system

--- systems/primitives/discrete_time_delay.h
+++ systems/primitives/discrete_time_delay.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <memory>
+
 #include "drake/common/drake_copyable.h"
+#include "drake/common/value.h"
+#include "drake/systems/framework/abstract_values.h"
 #include "drake/systems/framework/basic_vector.h"
 #include "drake/systems/framework/context.h"
 #include "drake/systems/framework/leaf_system.h"
@@ -9,14 +13,16 @@ namespace drake {
 namespace systems {
 
 /// A discrete time delay block with input u, which is vector-valued (discrete
-/// or continuous), and output delayed_u which is previously received input,
-/// delayed by the given amount.  The initial output will be a vector of zeros
+/// or continuous) or abstract, and output delayed_u which is previously
+/// received input, delayed by the given amount.  The initial output will be a
+/// vector of zeros for vector-valued or a given value for abstract-valued
 /// until the delay time has passed.
 ///
 /// @ingroup primitive_systems
 /// @system{DiscreteTimeDelay, @input_port{u}, @output_port{delayed_u}}
 ///
 /// Let t,z ∈ ℕ be the number of delay time steps and the input vector size.
+/// For abstract-valued %DiscreteTimeDelay, z is 1.
 /// The state x ∈ ℝ⁽ᵗ⁺¹⁾ᶻ is partitioned into t+1 blocks x[0] x[1] ... x[t],
 /// each of size z. The input and output are u,y ∈ ℝᶻ.
 /// The discrete state space dynamics of %DiscreteTimeDelay is:
@@ -25,7 +31,8 @@ namespace systems {
 ///   yₙ = xₙ[0]                       // output
 ///   x₀ = xᵢₙᵢₜ                       // initialize
 /// ```
-/// where xᵢₙᵢₜ = 0 for vector-valued %DiscreteTimeDelay.
+/// where xᵢₙᵢₜ = 0 for vector-valued %DiscreteTimeDelay and xᵢₙᵢₜ is a
+/// given value for abstract-valued %DiscreteTimeDelay.
 ///
 /// See @ref discrete_systems "Discrete Systems" for general information about
 /// discrete systems in Drake, including how they interact with continuous
@@ -43,7 +50,16 @@ class DiscreteTimeDelay final : public LeafSystem<T> {
   /// Constructs a DiscreteTimeDelay system updating every `update_sec` and
   /// delaying a vector-valued input of size `vector_size` for
   /// `delay_timesteps` number of updates.
-  DiscreteTimeDelay(double update_sec, int delay_timesteps, int vector_size);
+  DiscreteTimeDelay(double update_sec, int delay_timesteps, int vector_size)
+      : DiscreteTimeDelay(update_sec, delay_timesteps, vector_size, nullptr) {}
+
+  /// Constructs a DiscreteTimeDelay system updating every `update_sec` and
+  /// delaying an abstract-valued input of type `abstract_model_value` for
+  /// `delay_timesteps` number of updates.
+  DiscreteTimeDelay(double update_sec, int delay_timesteps,
+                    const AbstractValue& abstract_model_value)
+      : DiscreteTimeDelay(update_sec, delay_timesteps, -1,
+                          abstract_model_value.Clone()) {}
 
   /// Scalar-type converting copy constructor.
   /// See @ref system_scalar_conversion.
@@ -66,12 +82,20 @@ class DiscreteTimeDelay final : public LeafSystem<T> {
   /// block, sliding the delay buffer forward and placing the sampled input at
   /// the end. This emulates an update event and is mostly useful for testing.
   void SaveInputToBuffer(Context<T>* context) const {
-    SaveInputVectorToBuffer(*context, &context->get_mutable_discrete_state());
+    if (is_abstract()) {
+      SaveInputAbstractValueToBuffer(*context, &context->get_mutable_state());
+    } else {
+      SaveInputVectorToBuffer(*context, &context->get_mutable_discrete_state());
+    }
   }
 
  private:
   // Allow different specializations to access each other's private data.
   template <typename U> friend class DiscreteTimeDelay;
+
+  // All of the other constructors delegate here.
+  DiscreteTimeDelay(double update_sec, int delay_timesteps, int vector_size,
+                    std::unique_ptr<const AbstractValue> model_value);
 
   // Override feedthrough detection to avoid the need for `DoToSymbolic()`.
   optional<bool> DoHasDirectFeedthrough(int input_port,
@@ -85,9 +109,20 @@ class DiscreteTimeDelay final : public LeafSystem<T> {
   void SaveInputVectorToBuffer(const Context<T>& context,
                                DiscreteValues<T>* discrete_state) const;
 
+  // Sets the output port value to the properly delayed abstract value.
+  void CopyDelayedAbstractValue(const Context<T>& context,
+                                AbstractValue* output) const;
+
+  // Saves the input port into the abstract-valued state,
+  void SaveInputAbstractValueToBuffer(const Context<T>& context,
+                                      State<T>* state) const;
+
+  bool is_abstract() const { return abstract_model_value_ != nullptr; }
+
   const double update_sec_{};
   const int delay_buffer_size_{};
   const int vector_size_{};
+  const std::unique_ptr<const AbstractValue> abstract_model_value_;
 };
 
 }  // namespace systems
