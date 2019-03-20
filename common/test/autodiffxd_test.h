cce04d4b3a4e24dde41fc671b80f0179e87ebb1d
amcastro-tri <alejandro.castro@tri.global>
2019-03-20 17:26:17 -0400

    Stop using make_coherent

--- common/test/autodiffxd_test.h
+++ common/test/autodiffxd_test.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <algorithm>
+#include <limits>
+
 #include <gtest/gtest.h>
 
 #include "drake/common/autodiff.h"
@@ -17,23 +20,28 @@ class AutoDiffXdTest : public ::testing::Test {
   template <typename F>
   ::testing::AssertionResult Check(const F& f) {
     // AutoDiffXd constants -- x and y.
-    const AutoDiffXd x_xd_{0.4};
-    AutoDiffXd y_xd_{0.3};
+    const AutoDiffXd x_xd{0.4};
+    AutoDiffXd y_xd{0.3};
     // AutoDiffd<3> constants -- x and y.
-    const AutoDiffd<3> x_3d_{x_xd_.value()};
-    AutoDiffd<3> y_3d_{y_xd_.value()};
+    const AutoDiffd<3> x_3d{x_xd.value()};
+    AutoDiffd<3> y_3d{y_xd.value()};
     // We only set the derivatives of y and leave x's uninitialized.
-    y_xd_.derivatives() = Eigen::VectorXd::Ones(3);
-    y_3d_.derivatives() = Eigen::Vector3d::Ones();
+    y_xd.derivatives() = Eigen::VectorXd::Ones(3);
+    y_3d.derivatives() = Eigen::Vector3d::Ones();
 
-    const AutoDiffXd e1{f(x_xd_, y_xd_)};
-    const AutoDiffd<3> e2{f(x_3d_, y_3d_)};
+    const AutoDiffXd e1{f(x_xd, y_xd)};
+    const AutoDiffd<3> e2{f(x_3d, y_3d)};
 
     if (std::isnan(e1.value()) && std::isnan(e2.value())) {
       // Both values are NaN.
       return ::testing::AssertionSuccess();
     }
-    if (e1.value() != e2.value()) {
+    // Slightly different execution branches might lead to results that differ
+    // by a machine precision order of magnitude.
+    const double kEpsilon = std::numeric_limits<double>::epsilon();
+    const double kValueTolerance =
+        10 * kEpsilon * std::max(std::abs(e1.value()), std::abs(e2.value()));
+    if (std::abs(e1.value() - e2.value()) > kValueTolerance) {
       return ::testing::AssertionFailure()
              << "Values do not match: " << e1.value() << " and " << e2.value();
     }
@@ -42,7 +50,14 @@ class AutoDiffXdTest : public ::testing::Test {
       // Both derivatives are NaN.
       return ::testing::AssertionSuccess();
     }
-    if (e1.derivatives() != e2.derivatives()) {
+    const double kDerivativeTolerance = 10 * kEpsilon;  // relative tolerance.
+    const bool derivatives_match =
+        // Zero derivatives.
+        (e1.derivatives().size() == 0 &&
+         e2.derivatives().isZero(kDerivativeTolerance)) ||
+        // Non-zero derivatives
+        e1.derivatives().isApprox(e2.derivatives(), kDerivativeTolerance);
+    if (!derivatives_match) {
       return ::testing::AssertionFailure() << "Derivatives do not match:\n"
                                            << e1.derivatives() << "\n----\n"
                                            << e2.derivatives() << "\n";
