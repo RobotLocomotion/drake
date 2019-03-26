39d388a5be33e84b63d96cc010284ce23f77c802
Eric Cousineau <eric.cousineau@tri.global>
2019-03-26 09:30:48 -0400

    py: Use --test_env to permit easier usage of matplotlib in tests

--- examples/van_der_pol/plot_limit_cycle.py
+++ examples/van_der_pol/plot_limit_cycle.py
@@ -1,14 +1,6 @@
-import argparse
 import matplotlib.pyplot as plt
 
 from pydrake.examples.van_der_pol import VanDerPolOscillator
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--test",
-                    action='store_true',
-                    help="Run without blocking for user input.",
-                    default=False)
-args = parser.parse_args()
 
 x = VanDerPolOscillator.CalcLimitCycle()
 
@@ -19,5 +11,4 @@ ax.set_ylim([-3, 3])
 ax.set_xlabel('q')
 ax.set_ylabel('qdot')
 
-if not args.test:
-    plt.show()
+plt.show()
