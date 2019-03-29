3ff1fd51ffdff2388188c978b3311b6b531328aa
Michael Sherman <sherm@tri.global>
2019-03-29 11:23:21 -0700

    Enable caching by default (#10979)
    * Enable caching by default.
    
    * Cache entry forgot a dependency.
    
    * A test case held a mutable reference and wrote through it more than once.

--- manipulation/schunk_wsg/test/schunk_wsg_position_controller_test.cc
+++ manipulation/schunk_wsg/test/schunk_wsg_position_controller_test.cc
@@ -98,15 +98,14 @@ GTEST_TEST(SchunkWsgPositionControllerTest, SimTest) {
                 Eval(controller_context)[0],
             -force_limit);
 
-  auto wsg_state = wsg->GetMutablePositionsAndVelocities(&wsg_context);
-  wsg_state = Vector4d::Zero();
+  wsg->SetPositionsAndVelocities(&wsg_context, Vector4d::Zero());
   simulator.AdvanceTo(1.0);
 
   const double kTolerance = 1e-12;
 
   // Check that we achieved the desired position.
   EXPECT_TRUE(CompareMatrices(
-      wsg_state,
+      wsg->GetPositionsAndVelocities(wsg_context),
       Vector4d(-desired_position / 2, desired_position / 2, 0.0, 0.0),
       kTolerance));
   // The steady-state force should be near zero.
@@ -121,7 +120,7 @@ GTEST_TEST(SchunkWsgPositionControllerTest, SimTest) {
                       &controller_context);
   simulator.AdvanceTo(2.0);
   EXPECT_TRUE(CompareMatrices(
-      wsg_state,
+      wsg->GetPositionsAndVelocities(wsg_context),
       Vector4d(-desired_position / 2, desired_position / 2, 0.0, 0.0),
       kTolerance));
   // The steady-state force should be near zero.
@@ -140,7 +139,8 @@ GTEST_TEST(SchunkWsgPositionControllerTest, SimTest) {
               force_limit, kTolerance);
 
   // Set the position to the target and observe zero force.
-  wsg_state = Vector4d(-desired_position / 2, desired_position / 2, 0.0, 0.0);
+  wsg->SetPositionsAndVelocities(&wsg_context,
+      Vector4d(-desired_position / 2, desired_position / 2, 0.0, 0.0));
 
   EXPECT_EQ(controller->get_grip_force_output_port().
                 Eval(controller_context)[0],
