dfc5ce69c74aab83a0eb67920755ed59d7b26794
Russ Tedrake <russt@mit.edu>
2019-03-31 21:30:21 -0400

    Provide GetInfeasibleConstraints
    This is a long-missed feature by former developers of mathematical programs in the Matlab versions of drake.
    Resolves #3130

--- solvers/solve.cc
+++ solvers/solve.cc
@@ -2,6 +2,7 @@
 
 #include <memory>
 
+#include "drake/common/nice_type_name.h"
 #include "drake/solvers/choose_best_solver.h"
 #include "drake/solvers/solver_interface.h"
 
@@ -26,6 +27,39 @@ MathematicalProgramResult Solve(
 
 MathematicalProgramResult Solve(const MathematicalProgram& prog) {
   return Solve(prog, {}, {});
+}
+
+std::vector<std::string> GetInfeasibleConstraints(
+    const MathematicalProgram& prog, const MathematicalProgramResult&
+    result, optional<double> tolerance) {
+  std::vector<std::string> descriptions;
+
+  if (!tolerance) {
+    // TODO(russt): Extract the constraint tolerance from the solver.  This
+    // value was used successfully for some time in MATLAB Drake, so I've
+    // ported it as the default here.
+    tolerance = 1e-4;
+  }
+
+  for (const auto& binding : prog.GetAllConstraints()) {
+    const Eigen::VectorXd val = result.EvalBinding(binding);
+    const std::shared_ptr<Constraint>& constraint = binding.evaluator();
+    std::string d = constraint->get_description();
+    if (d.empty()) {
+      d = NiceTypeName::Get(*constraint);
+    }
+    for (int i = 0; i < val.rows(); i++) {
+      if (val[i] < constraint->lower_bound()[i] - *tolerance ||
+          val[i] > constraint->upper_bound()[i] + *tolerance) {
+        descriptions.push_back(
+            d + "[" + std::to_string(i) +
+            "]: " + std::to_string(constraint->lower_bound()[i]) +
+            " <= " + std::to_string(val[i]) +
+            " <= " + std::to_string(constraint->upper_bound()[i]));
+      }
+    }
+  }
+  return descriptions;
 }
 }  // namespace solvers
 }  // namespace drake
