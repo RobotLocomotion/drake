d526a57adeb208ee67d1a228a6574315bfe6180e
Chien-Liang Fok <liang.fok@tri.global>
2019-04-09 16:45:48 -0400

    Applies clang-format to phase_ring_test.cc (#11209)
    * Applies clang-format to phase_ring_test.cc
    No functional changes.

--- automotive/maliput/api/test/phase_ring_test.cc
+++ automotive/maliput/api/test/phase_ring_test.cc
@@ -38,16 +38,14 @@ Phase CreateFullPhase(const Phase::Id& id) {
          BulbState::kOn}}}};
 }
 
-Phase CreatePhaseWithMissingRuleStates(
-    const Phase::Id& id) {
+Phase CreatePhaseWithMissingRuleStates(const Phase::Id& id) {
   const Phase mock_phase = CreateFullPhase(id);
   RuleStates rule_states = mock_phase.rule_states();
   rule_states.erase(rule_states.begin());
   return Phase(id, rule_states, mock_phase.bulb_states());
 }
 
-Phase CreatePhaseWithMissingBulbStates(
-    const Phase::Id& id) {
+Phase CreatePhaseWithMissingBulbStates(const Phase::Id& id) {
   const Phase mock_phase = CreateFullPhase(id);
   BulbStates bulb_states = *mock_phase.bulb_states();
   bulb_states.erase(bulb_states.begin());
@@ -98,8 +96,8 @@ TEST_F(PhaseRingTest, Constructor) {
 
   // Next phases defines an unknown phase.
   const std::unordered_map<Phase::Id, std::vector<PhaseRing::NextPhase>>
-      unknown_next_phases = {{Phase::Id("unknown"),
-                              std::vector<PhaseRing::NextPhase>()}};
+      unknown_next_phases = {
+          {Phase::Id("unknown"), std::vector<PhaseRing::NextPhase>()}};
   EXPECT_THROW(
       PhaseRing(id_,
                 {CreateFullPhase(phase_id_1_), CreateFullPhase(phase_id_2_)},
@@ -120,8 +118,7 @@ TEST_F(PhaseRingTest, Accessors) {
 TEST_F(PhaseRingTest, NextPhases) {
   const double kDuration1{30};
   const double kDuration2{60};
-  std::unordered_map<Phase::Id, std::vector<PhaseRing::NextPhase>>
-      next_phases;
+  std::unordered_map<Phase::Id, std::vector<PhaseRing::NextPhase>> next_phases;
   next_phases.emplace(std::make_pair(
       phase_id_1_,
       std::vector<PhaseRing::NextPhase>{{phase_id_2_, kDuration1}}));
