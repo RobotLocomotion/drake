b0362995dfda2530035390be064760948ceec55a
Sean Curtis <sean.curtis@tri.global>
2019-04-09 13:25:53 -0700

    Make ComputeSignedDistanceToPoint autodiffable
    This entailed several pieces of work.
    
    1. Refactoring:
       -  Extract code from proximity_engine.cc into its own file
          (proximity/distance_to_point.h) and make it appropriately templated.
       - Steal functionality out of the soon-to-be-defunct
         distance_to_point_with_gradient.* to put into proximity_utilities.h
    2. proximity_engine.h/cc:
       - Reformulate queries in proximity_engine.cc to make use of the
         refactored code in proximity_utilites.h and distance_to_point.h
       - Reformulate queries to separate broadphase in double with narrowphase
         in T.
       - Change signature of function is question to be tempalted on T instead
         of double.
    3. Extend the conversion utilities in geometry/utilities.h to include
       convenience casting for Vector3<T> -> Vector3<double>
    4. Push proximity engine changes through GeometryState and QueryObject.
    5. Testing
       - New (limited) unit tests on distance_to_point.h. Specifically, spot
         checking of autodiff compatibility.
       - smoke test on ProximityEngine to confirm broadphase vs narrowphase
         funcionality.
    6. Update documentation in QueryObject to pave the road for partial
       AutoDiff support.

--- geometry/proximity/distance_to_point_with_gradient.h
+++ geometry/proximity/distance_to_point_with_gradient.h
@@ -6,6 +6,7 @@
 #include <fcl/geometry/shape/sphere.h>
 
 #include "drake/geometry/geometry_ids.h"
+#include "drake/geometry/proximity/proximity_utilities.h"
 #include "drake/geometry/query_results/signed_distance_to_point_with_gradient.h"
 #include "drake/math/autodiff.h"
 #include "drake/math/autodiff_gradient.h"
@@ -14,80 +15,6 @@
 namespace drake {
 namespace geometry {
 namespace internal {
-/**
- * Calculates an absolute tolerance value conditioned to a problem's
- * characteristic `size`. The tolerance is sufficient to account for round-off
- * error which arises due to transformations.
- */
-double DistanceToPointRelativeTolerance(double size);
-
-/**
- * @name ComputeDistanceToPrimitive
- * Computes the signed distance from a point Q to a primitive.
- * Refer to QueryObject::ComputeSignedDistanceToPoint for more details.
- * @param X_WG The pose of the primitive geometry G in the world frame.
- * @param p_WQ The position of the point Q in the world frame.
- * @param p_GN[out] The position of the witness point N on the primitive,
- * expressed in the primitive geometry frame G.
- * @param distance[out] The signed distance from the point Q to the primitive.
- * @param grad_W[out] The gradient of the distance function w.r.t p_GQ (the
- * position of the point Q in the primitiveframe). This gradient vector is
- * expressed in the world frame.
- * @{
- */
-
-/** Overload of ComputeDistanceToPrimitive() for sphere primitive. */
-template <typename T>
-void ComputeDistanceToPrimitive(const fcl::Sphered& sphere,
-                                const math::RigidTransform<T>& X_WG,
-                                const Vector3<T>& p_WQ, Vector3<T>* p_GN,
-                                T* distance, Vector3<T>* grad_W) {
-  // TODO(DamrongGuoy): Move most code of this function into FCL.
-  const double radius = sphere.radius;
-  const Vector3<T> p_GQ_G = X_WG.inverse() * p_WQ;
-  const T dist_GQ = p_GQ_G.norm();
-
-  // The gradient is always in the direction from the center of the sphere to
-  // the query point Q, regardless of whether the point Q is outside or inside
-  // the sphere G.  The gradient is undefined if the query point Q is at the
-  // center of the sphere G.
-  //
-  // If the query point Q is near the center of the sphere G within a
-  // tolerance, we arbitrarily set the gradient vector as documented in
-  // query_object.h (QueryObject::ComputeSignedDistanceToPoint).
-  const double tolerance = DistanceToPointRelativeTolerance(radius);
-  // Unit vector in x-direction of G's frame.
-  const Vector3<T> Gx = Vector3<T>::UnitX();
-  // Gradient vector expressed in G's frame.
-  const Vector3<T> grad_G = (dist_GQ > tolerance) ? p_GQ_G / dist_GQ : Gx;
-
-  // Do not compute distance as |p_GQ|, as the gradient of |p_GQ| w.r.t p_GQ is
-  // p_GQᵀ/|p_GQ|, not well defined at p_GQ = 0. Instead, compute the distance
-  // as p_GQ.dot(grad_G).
-  *distance = p_GQ_G.dot(grad_G) - T(radius);
-
-  // Position vector of the nearest point N on G's surface from the query
-  // point Q, expressed in G's frame.
-  *p_GN = radius * grad_G;
-  // Gradient vector expressed in World frame.
-  *grad_W = X_WG.rotation() * grad_G;
-}
-
-/** Overload of ComputeDistanceToPrimitive() for halfspace primitive. */
-template <typename T>
-void ComputeDistanceToPrimitive(const fcl::Halfspaced& halfspace,
-                                const math::RigidTransform<T>& X_WG,
-                                const Vector3<T>& p_WQ, Vector3<T>* p_GN,
-                                T* distance, Vector3<T>* grad_W) {
-  // FCL stores the halfspace as {x | nᵀ * x <= d}, with n being a unit length
-  // normal vector. Both n and x are expressed in the halfspace frame.
-  const Vector3<T> n_G = halfspace.n.cast<T>();
-  const Vector3<T> p_GQ = X_WG.inverse() * p_WQ;
-  *distance = n_G.dot(p_GQ) - T(halfspace.d);
-  *p_GN = p_GQ - *distance * n_G;
-  *grad_W = X_WG.rotation() * n_G;
-}
-//@}
 
 /**
  * An internal functor to support the call back function in proximity_engine.cc.
---
+++ geometry/proximity/proximity_utilities.h
@@ -0,0 +1,116 @@
+#pragma once
+
+// Exclude internal classes from doxygen.
+#if !defined(DRAKE_DOXYGEN_CXX)
+
+#include <algorithm>
+#include <cstdint>
+#include <vector>
+
+#include <fcl/fcl.h>
+#include <fmt/format.h>
+
+#include "drake/geometry/geometry_ids.h"
+#include "drake/geometry/geometry_index.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+
+// TODO(SeanCurtis-TRI): Snake case this name.
+/** Calculates an absolute tolerance value conditioned to a problem's
+ characteristic, positive-valued `size`. The tolerance is sufficient to account
+ for round-off error which arises due to transformations.
+ @pre size > 0.  */
+constexpr double DistanceToPointRelativeTolerance(double size) {
+  return 1e-14 * std::max(1.0, size);
+}
+
+/** Class for coordinating the collision objects stored in the proximity engine
+ with the geometries stored in SceneGraph. The two are related in that the
+ collision object stores the GeometryIndex of the corresponding SceneGraph
+ geometry as well as a bit to mark whether it is anchored or dynamic.
+
+ It stores this data by compactly "encoding" them. The encoding packs a geometry
+ index value with a bit indicating if the index refers to dynamic or anchored
+ geometry (proximity engine segregates them). The highest-order bit indicates
+ dynamic (1) or anchored (0). The remaining lower bits store the index. The data
+ is stored in a pointer-sized integer. This integer is, in turn, stored directly
+ into fcl::CollisionObject's void* user data member.  */
+class EncodedData {
+ public:
+  /** Constructs encoded data directly from the index and the known
+   anchored/dynamic characterization.  */
+  EncodedData(GeometryIndex index, bool is_dynamic)
+      : data_(static_cast<uintptr_t>(index)) {
+    if (is_dynamic) set_dynamic();
+    // NOTE: data is encoded as anchored by default. So, an action only needs to
+    // be taken in the dynamic case.
+  }
+
+  /** Constructs encoded data by extracting it from the given collision object.
+   */
+  explicit EncodedData(const fcl::CollisionObject<double>& fcl_object)
+      : data_(reinterpret_cast<uintptr_t>(fcl_object.getUserData())) {}
+
+  /** Constucts encoded data for the given index identified as dynamic.  */
+  static EncodedData encode_dynamic(GeometryIndex index) {
+    return EncodedData(index, true);
+  }
+
+  /** Constucts encoded data for the given index identified as anchored.  */
+  static EncodedData encode_anchored(GeometryIndex index) {
+    return EncodedData(index, false);
+  }
+
+  /** Sets the encoded data to be dynamic.  */
+  void set_dynamic() { data_ |= kIsDynamicMask; }
+
+  /** Sets the encoded data to be anchored.  */
+  void set_anchored() { data_ &= ~kIsDynamicMask; }
+
+  /** Writes the encoded data into the collision object's user data.  */
+  void write_to(fcl::CollisionObject<double>* object) {
+    object->setUserData(reinterpret_cast<void*>(data_));
+  }
+
+  /** Reports true if the encoded data is marked as dynamic. False if anchored.
+   */
+  bool is_dynamic() const { return (data_ & kIsDynamicMask) != 0; }
+
+  /** Reports the stored index.  */
+  GeometryIndex index() const {
+    return static_cast<GeometryIndex>(data_ & ~kIsDynamicMask);
+  }
+
+  /** Given a map from GeometryIndex to ids of the SceneGraph geometries,
+   reports the geometry id for the encoded data.  */
+  GeometryId id(const std::vector<GeometryId>& geometry_map) const {
+    return geometry_map[index()];
+  }
+
+  /** Reports the encoded data.  */
+  uintptr_t encoded_data() const { return data_; }
+
+ private:
+  // Note: This sets a mask to be 1000...0 based on the size of a pointer.
+  // C++ guarantees that uintptr_t can hold a pointer and cast to a pointer,
+  // but it doesn't guarantee it's the *same size* as a pointer. So, we set
+  // the mask value based on pointer size. This is important because we're
+  // storing it in the void* of the collision object.
+  static const uintptr_t kIsDynamicMask = uintptr_t{1}
+      << (sizeof(void*) * 8 - 1);
+
+  // The encoded data - index and mobility type.
+  // We're using an unsigned value here because:
+  //   - Bitmasking games are typically more intuitive with unsigned values
+  //     (think of bit shifting as an example here).
+  //   - This unsigned integer doesn't bleed out into the API at all.
+  uintptr_t data_{};
+};
+
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
+
+#endif  // !defined(DRAKE_DOXYGEN_CXX)
