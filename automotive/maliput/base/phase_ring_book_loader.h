20560351f8e687540779bd5cd3b62f77e06c0695
Chien-Liang Fok <liang.fok@tri.global>
2019-04-10 15:30:16 -0400

    Updates PhaseRingBookLoader to load traffic light states

--- automotive/maliput/base/phase_ring_book_loader.h
+++ automotive/maliput/base/phase_ring_book_loader.h
@@ -5,6 +5,7 @@
 
 #include "drake/automotive/maliput/api/rules/phase_ring_book.h"
 #include "drake/automotive/maliput/api/rules/road_rulebook.h"
+#include "drake/automotive/maliput/api/rules/traffic_light_book.h"
 
 namespace drake {
 namespace maliput {
@@ -12,24 +13,32 @@ namespace maliput {
 /// Instantiates and returns an api::rules::PhaseRingBook instance based on the
 /// specified @p rulebook, and @p input document.
 ///
-/// @param rulebook Contains the rules that apply to @p road_geometry.
+/// @param rulebook Contains the rules.
+///
+/// @param traffic_light_book Contains the traffic lights.
 ///
 /// @param input The YAML PhaseRings document.
 ///
 /// @return The newly created api::rules::PhaseRingBook instance.
 std::unique_ptr<api::rules::PhaseRingBook> LoadPhaseRingBook(
-    const api::rules::RoadRulebook* rulebook, const std::string& input);
+    const api::rules::RoadRulebook* rulebook,
+    const api::rules::TrafficLightBook* traffic_light_book,
+    const std::string& input);
 
 /// Instantiates and returns an api::rules::PhaseRingBook instance based on the
 /// specified @p rulebook, and @p filename.
 ///
-/// @param rulebook Contains the rules that apply to @p road_geometry.
+/// @param rulebook Contains the rules.
+///
+/// @param traffic_light_book Contains the traffic lights.
 ///
 /// @param filename The YAML file that contains a PhaseRings document.
 ///
 /// @return The newly created api::rules::PhaseRingBook instance.
 std::unique_ptr<api::rules::PhaseRingBook> LoadPhaseRingBookFromFile(
-    const api::rules::RoadRulebook* rulebook, const std::string& filename);
+    const api::rules::RoadRulebook* rulebook,
+    const api::rules::TrafficLightBook* traffic_light_book,
+    const std::string& filename);
 
 }  // namespace maliput
 }  // namespace drake
