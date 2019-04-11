eedf300ef0e50b52c9971273797ad0f9772fdd8e
Andrew Best <andrew.best@tri.global>
2019-04-11 10:49:28 -0700

    Adds YAML support for DirectionUsageRule.
    Resolves #10917.

--- automotive/maliput/base/test/road_rulebook_loader_test.cc
+++ automotive/maliput/base/test/road_rulebook_loader_test.cc
@@ -1,5 +1,6 @@
 #include "drake/automotive/maliput/base/road_rulebook_loader.h"
 
+#include <iostream>
 #include <iterator>
 #include <memory>
 #include <string>
@@ -11,6 +12,7 @@
 #include "drake/automotive/maliput/api/road_geometry.h"
 #include "drake/automotive/maliput/api/rules/regions.h"
 #include "drake/automotive/maliput/api/rules/right_of_way_rule.h"
+#include "drake/automotive/maliput/api/test_utilities/rules_direction_usage_compare.h"
 #include "drake/automotive/maliput/api/test_utilities/rules_right_of_way_compare.h"
 #include "drake/automotive/maliput/api/test_utilities/rules_test_utilities.h"
 #include "drake/automotive/maliput/multilane/builder.h"
@@ -23,6 +25,7 @@ namespace maliput {
 namespace {
 
 using drake::maliput::api::LaneId;
+using drake::maliput::api::rules::DirectionUsageRule;
 using drake::maliput::api::rules::LaneSRange;
 using drake::maliput::api::rules::LaneSRoute;
 using drake::maliput::api::rules::RightOfWayRule;
@@ -36,7 +39,7 @@ class TestLoading2x2IntersectionRules : public ::testing::Test {
         road_geometry_(
             multilane::LoadFile(multilane::BuilderFactory(), filepath_)) {}
 
-  std::vector<RightOfWayRule> CreateStraightThroughRules() const {
+  std::vector<RightOfWayRule> CreateStraightThroughRightOfWayRules() const {
     struct TestCase {
       std::string rule_name;
       std::string lane_name;
@@ -62,7 +65,7 @@ class TestLoading2x2IntersectionRules : public ::testing::Test {
     return result;
   }
 
-  std::vector<RightOfWayRule> CreateRightTurnRules() const {
+  std::vector<RightOfWayRule> CreateRightTurnRightOfWayRules() const {
     struct TestCase {
       std::string rule_name;
       std::string lane_name;
@@ -97,7 +100,7 @@ class TestLoading2x2IntersectionRules : public ::testing::Test {
     return result;
   }
 
-  std::vector<RightOfWayRule> CreateLeftTurnRules() const {
+  std::vector<RightOfWayRule> CreateLeftTurnRightOfWayRules() const {
     struct TestCase {
       std::string rule_name;
       std::string lane_name;
@@ -125,29 +128,132 @@ class TestLoading2x2IntersectionRules : public ::testing::Test {
     return result;
   }
 
+  std::vector<DirectionUsageRule> CreateDirectionUsageRules() const {
+    struct TestCase {
+      std::string rule_name;
+      std::string lane_name;
+      std::string state_name;
+      DirectionUsageRule::State::Type rule_type;
+      DirectionUsageRule::State::Severity rule_severity;
+    };
+    const std::vector<TestCase> test_cases = {
+        // Turn cases.
+        {"l:north_right_turn_segment_0", "l:north_right_turn_segment_0",
+         "WithS", DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kStrict},
+        {"l:north_left_turn_segment_0", "l:north_left_turn_segment_0", "WithS",
+         DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kStrict},
+        {"l:south_right_turn_segment_0", "l:south_right_turn_segment_0",
+         "WithS", DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kStrict},
+        {"l:south_left_turn_segment_0", "l:south_left_turn_segment_0", "WithS",
+         DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kStrict},
+        {"l:east_right_turn_segment_0", "l:east_right_turn_segment_0", "WithS",
+         DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kStrict},
+        {"l:east_left_turn_segment_0", "l:east_left_turn_segment_0", "WithS",
+         DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kStrict},
+        {"l:west_left_turn_segment_0", "l:west_left_turn_segment_0", "WithS",
+         DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kStrict},
+        {"l:west_right_turn_segment_0", "l:west_right_turn_segment_0", "WithS",
+         DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kStrict},
+
+        {"l:n_segment_0", "l:n_segment_0", "WithS",
+         DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kPreferred},
+        {"l:n_segment_1", "l:n_segment_1", "AgainstS",
+         DirectionUsageRule::State::Type::kAgainstS,
+         DirectionUsageRule::State::Severity::kPreferred},
+        {"l:s_segment_0", "l:s_segment_0", "WithS",
+         DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kPreferred},
+        {"l:s_segment_1", "l:s_segment_1", "AgainstS",
+         DirectionUsageRule::State::Type::kAgainstS,
+         DirectionUsageRule::State::Severity::kPreferred},
+        {"l:e_segment_0", "l:e_segment_0", "WithS",
+         DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kPreferred},
+        {"l:e_segment_1", "l:e_segment_1", "AgainstS",
+         DirectionUsageRule::State::Type::kAgainstS,
+         DirectionUsageRule::State::Severity::kPreferred},
+        {"l:w_segment_0", "l:w_segment_0", "WithS",
+         DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kPreferred},
+        {"l:w_segment_1", "l:w_segment_1", "AgainstS",
+         DirectionUsageRule::State::Type::kAgainstS,
+         DirectionUsageRule::State::Severity::kPreferred},
+
+        // Intersection straight segments travel with the same +S direction as
+        // the connected segments.
+        {"l:ns_intersection_segment_0", "l:ns_intersection_segment_0", "WithS",
+         DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kStrict},
+        {"l:ns_intersection_segment_1", "l:ns_intersection_segment_1",
+         "AgainstS", DirectionUsageRule::State::Type::kAgainstS,
+         DirectionUsageRule::State::Severity::kStrict},
+        {"l:ew_intersection_segment_0", "l:ew_intersection_segment_0", "WithS",
+         DirectionUsageRule::State::Type::kWithS,
+         DirectionUsageRule::State::Severity::kStrict},
+        {"l:ew_intersection_segment_1", "l:ew_intersection_segment_1",
+         "AgainstS", DirectionUsageRule::State::Type::kAgainstS,
+         DirectionUsageRule::State::Severity::kStrict}};
+
+    std::vector<DirectionUsageRule> result;
+    for (const auto& test_case : test_cases) {
+      const auto lane =
+          road_geometry_->ById().GetLane(LaneId(test_case.lane_name));
+      result.push_back(DirectionUsageRule(
+          DirectionUsageRule::Id(test_case.rule_name),
+          LaneSRange(LaneId(test_case.lane_name), SRange(0, lane->length())),
+          {DirectionUsageRule::State(
+              DirectionUsageRule::State::Id(test_case.state_name),
+              test_case.rule_type, test_case.rule_severity)}));
+    }
+    return result;
+  }
+
   const std::string filepath_;
   const std::unique_ptr<const api::RoadGeometry> road_geometry_;
 };
 
 TEST_F(TestLoading2x2IntersectionRules, LoadFromFile) {
-  const auto straight_cases = CreateStraightThroughRules();
-  const auto right_turn_cases = CreateRightTurnRules();
-  const auto left_turn_cases = CreateLeftTurnRules();
-
-  std::vector<RightOfWayRule> test_cases;
-  test_cases.insert(test_cases.end(), std::begin(straight_cases),
-                    std::end(straight_cases));
-  test_cases.insert(test_cases.end(), std::begin(right_turn_cases),
-                    std::end(right_turn_cases));
-  test_cases.insert(test_cases.end(), std::begin(left_turn_cases),
-                    std::end(left_turn_cases));
-
   const std::unique_ptr<api::rules::RoadRulebook> rulebook =
       LoadRoadRulebookFromFile(road_geometry_.get(), filepath_);
   EXPECT_NE(rulebook, nullptr);
-  for (const auto& test_case : test_cases) {
-    const RightOfWayRule rule = rulebook->GetRule(test_case.id());
-    EXPECT_TRUE(MALIPUT_IS_EQUAL(rule, test_case));
+
+  // RightOfWayRules testing.
+  {
+    const auto straight_cases = CreateStraightThroughRightOfWayRules();
+    const auto right_turn_cases = CreateRightTurnRightOfWayRules();
+    const auto left_turn_cases = CreateLeftTurnRightOfWayRules();
+
+    std::vector<RightOfWayRule> test_cases;
+    test_cases.insert(test_cases.end(), std::begin(straight_cases),
+                      std::end(straight_cases));
+    test_cases.insert(test_cases.end(), std::begin(right_turn_cases),
+                      std::end(right_turn_cases));
+    test_cases.insert(test_cases.end(), std::begin(left_turn_cases),
+                      std::end(left_turn_cases));
+
+    for (const auto& test_case : test_cases) {
+      const RightOfWayRule rule = rulebook->GetRule(test_case.id());
+      EXPECT_TRUE(MALIPUT_IS_EQUAL(rule, test_case));
+    }
+  }
+
+  // DirectionUsageRules testing.
+  {
+    std::vector<DirectionUsageRule> direction_usage_cases =
+        CreateDirectionUsageRules();
+    for (const auto& test_case : direction_usage_cases) {
+      const DirectionUsageRule rule = rulebook->GetRule(test_case.id());
+      EXPECT_TRUE(MALIPUT_IS_EQUAL(rule, test_case));
+    }
   }
 }
 
