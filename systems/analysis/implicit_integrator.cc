86dd199b79a7329a5f580b1358259bd40d35cbc0
Evan Drumwright <edrumwri@hotmail.com>
2019-04-16 15:09:05 -0700

    Implicit Euler Integrator refactor (#11182)

---
+++ systems/analysis/implicit_integrator.cc
@@ -0,0 +1,7 @@
+#include "drake/systems/analysis/implicit_integrator.h"
+
+#include "drake/common/autodiff.h"
+#include "drake/common/default_scalars.h"
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
+    class ::drake::systems::ImplicitIntegrator)
