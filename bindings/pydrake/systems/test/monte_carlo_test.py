995661968490cb2bb6e7fa1c3efdb7be60b31be4
Gregory Izatt <gregory.izatt@gmail.com>
2019-04-16 13:44:28 -0400

    Verify monte carlo basin with analytical basin

--- bindings/pydrake/systems/test/monte_carlo_test.py
+++ bindings/pydrake/systems/test/monte_carlo_test.py
@@ -41,7 +41,7 @@ from pydrake.multibody.tree import (
     UniformGravityFieldElement
 )
 from pydrake.multibody.plant import (
-    MultibodyPlant    
+    MultibodyPlant
 )
 from pydrake.multibody.parsing import Parser
 
@@ -88,12 +88,12 @@ class TestMonteCarlo(unittest.TestCase):
         pendulum.WeldFrames(pendulum.world_frame(),
                             pendulum.GetFrameByName("base_part2"))
         pendulum.Finalize()
-        
+
         elbow = pendulum.GetMutableJointByName("theta")
         upright_theta = np.pi
         theta_expression = Variable(
             name="theta",
-            type=Variable.Type.RANDOM_GAUSSIAN)*0.25 + upright_theta
+            type=Variable.Type.RANDOM_UNIFORM)*2*np.pi
         elbow.set_random_angle_distribution(theta_expression)
 
         # LQR configuration
@@ -103,7 +103,6 @@ class TestMonteCarlo(unittest.TestCase):
         linearize_context.SetContinuousState(
             np.array([upright_theta, 0.]))
 
-
         actuation_port_index = pendulum.get_actuation_input_port().get_index()
         linearize_context.FixInputPort(
             actuation_port_index, np.zeros(1))
@@ -112,13 +111,17 @@ class TestMonteCarlo(unittest.TestCase):
                 pendulum, linearize_context, Q, R,
                 np.zeros(0), actuation_port_index))
 
+        torque_limit = 1.0
         torque_limiter = builder.AddSystem(
-            Saturation(min_value=np.array([-1.0]),
-                       max_value=np.array([1.0])))
+            Saturation(min_value=np.array([-torque_limit]),
+                       max_value=np.array([torque_limit])))
 
-        builder.Connect(controller.get_output_port(0), torque_limiter.get_input_port(0))
-        builder.Connect(torque_limiter.get_output_port(0), pendulum.get_actuation_input_port())
-        builder.Connect(pendulum.get_output_port(0), controller.get_input_port(0))
+        builder.Connect(controller.get_output_port(0),
+                        torque_limiter.get_input_port(0))
+        builder.Connect(torque_limiter.get_output_port(0),
+                        pendulum.get_actuation_input_port())
+        builder.Connect(pendulum.get_output_port(0),
+                        controller.get_input_port(0))
 
         diagram = builder.Build()
 
@@ -131,17 +134,31 @@ class TestMonteCarlo(unittest.TestCase):
         def calc_output(system, context):
             state = diagram.GetSubsystemContext(
                 pendulum, context).get_continuous_state_vector()
-            return (state.GetAtIndex(0) - upright_theta + np.pi) % (2*np.pi) - np.pi
+            error = state.GetAtIndex(0) - upright_theta + np.pi
+            # Wrap error to [-pi, pi].
+            return (error) % (2*np.pi) - np.pi
 
         results = MonteCarloSimulation(
             make_simulator=make_simulator, output=calc_output,
-            final_time=1.0, num_samples=100, generator=RandomGenerator())
+            final_time=1.0, num_samples=50, generator=RandomGenerator())
 
-        num_passing = sum([abs(res.output) < 0.05 for res in results])
-        passing_ratio = float(num_passing) / len(results)
-        # Empirically...
-        self.assertTrue(0.3 <= passing_ratio <= 0.8)
-        print("Fraction of cases that converged: %f" % passing_ratio)
+        binary_results = np.array([abs(res.output) < 0.05 for res in results])
+        passing_ratio = float(sum(binary_results)) / len(results)
+        # Normal approximation of 95% confidence interval for binomial
+        # prob passing_ratio.
+        passing_ratio_var = 1.96 * np.sqrt(
+            passing_ratio*(1. - passing_ratio)/len(results))
 
+        # Calculate where the torque exceeds the torque limit to find
+        # the expected ROA.
+        arm_radius = 0.5
+        arm_mass = 1.0
+        # torque = r x f = r * (m * 9.81 * sin(theta))
+        # theta = asin(torque / (r * m))
+        roa_half_width = np.arcsin(torque_limit /
+                                   (arm_radius * arm_mass * 9.81))
+        roa_as_fraction_of_state_space = roa_half_width / np.pi
 
-
+        self.assertTrue(passing_ratio - passing_ratio_var <=
+                        roa_as_fraction_of_state_space <=
+                        passing_ratio + passing_ratio_var)
