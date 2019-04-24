a928a4cba3cf7878d7b09415a8f64c7a8b0a3733
Jeremy Nimmer <jeremy.nimmer@tri.global>
2019-04-23 21:40:47 -0400

    geometry: Make FrameKinematicsVector avoid allocations
    This should ideally be replaced with a flat_unordered_map or similar in
    the future, but we don't have access to one of those yet.

--- geometry/frame_kinematics_vector.cc
+++ geometry/frame_kinematics_vector.cc
@@ -29,40 +29,50 @@ template <typename KinematicsValue>
 FrameKinematicsVector<KinematicsValue>::FrameKinematicsVector(
     SourceId source_id, const std::vector<FrameId>& ids)
     : source_id_(source_id), values_(0) {
-  KinematicsValue default_value;
-  InitializeKinematicsValue(&default_value);
+  optional<KinematicsValue> default_value{KinematicsValue{}};
+  InitializeKinematicsValue(&default_value.value());
   for (FrameId id : ids) {
     bool is_unique = values_.emplace(id, default_value).second;
     if (!is_unique) {
       throw std::runtime_error(
           fmt::format("At least one frame id appears multiple times: {}", id));
     }
+    ++size_;
   }
 }
 
 template <typename KinematicsValue>
 FrameKinematicsVector<KinematicsValue>::FrameKinematicsVector(
     std::initializer_list<std::pair<const FrameId, KinematicsValue>> init) {
-  values_ = init;
+  values_.insert(init.begin(), init.end());
+  size_ = init.size();
 }
 
 template <typename KinematicsValue>
 FrameKinematicsVector<KinematicsValue>&
 FrameKinematicsVector<KinematicsValue>::operator=(
     std::initializer_list<std::pair<const FrameId, KinematicsValue>> init) {
-  values_ = init;
+  clear();
+  for (const auto& item : init) {
+    set_value(item.first, item.second);
+  }
   return *this;
 }
 
 template <typename KinematicsValue>
 void FrameKinematicsVector<KinematicsValue>::clear() {
-  values_.clear();
+  for (auto& item : values_) {
+    item.second = nullopt;
+  }
+  size_ = 0;
 }
 
 template <typename KinematicsValue>
 void FrameKinematicsVector<KinematicsValue>::set_value(
     FrameId id, const KinematicsValue& value) {
-  values_[id] = value;
+  auto& map_value = values_[id];
+  if (!map_value.has_value()) { ++size_; }
+  map_value = value;
 }
 
 template <typename KinematicsValue>
@@ -70,10 +80,32 @@ const KinematicsValue& FrameKinematicsVector<KinematicsValue>::value(
     FrameId id) const {
   using std::to_string;
   auto iter = values_.find(id);
-  if (iter == values_.end()) {
-    throw std::runtime_error("No such FrameId " + to_string(id) + ".");
+  if (iter != values_.end()) {
+    const optional<KinematicsValue>& map_value = iter->second;
+    if (map_value.has_value()) {
+      return *map_value;
+    }
   }
-  return iter->second;
+  throw std::runtime_error("No such FrameId " + to_string(id) + ".");
+}
+
+template <typename KinematicsValue>
+bool FrameKinematicsVector<KinematicsValue>::has_id(FrameId id) const {
+  auto iter = values_.find(id);
+  return (iter != values_.end()) && iter->second.has_value();
+}
+
+template <typename KinematicsValue>
+std::vector<FrameId>
+FrameKinematicsVector<KinematicsValue>::frame_ids() const {
+  std::vector<FrameId> result;
+  result.reserve(size_);
+  for (const auto& item : values_) {
+    if (item.second.has_value()) {
+      result.emplace_back(item.first);
+    }
+  }
+  return result;
 }
 
 // Explicitly instantiates on the most common scalar types.
