6732239c88e59ff923414be9e5d5815c7269d593
Jeremy Nimmer <jeremy.nimmer@tri.global>
2019-04-25 11:54:20 -0400

    Add more lazy-if test cases

--- common/test/symbolic_expression_test2.cc
+++ common/test/symbolic_expression_test2.cc
@@ -1,7 +1,9 @@
+#include <iostream>
 #include <utility>
 #include <vector>
 
 #include <gtest/gtest.h>
+#include <fmt/format.h>
 
 #include "drake/common/symbolic.h"
 #include "drake/common/symbolic_cond_literals.h"
@@ -25,60 +27,102 @@ class SymbolicExpressionTest : public ::testing::Test {
   const Expression z_{var_z_};
 };
 
-template <typename T>
-std::pair<T, T> min_max(const T& a, const T& b) {
-  using namespace drake::symbolic::branching_literals;
-  T less;
-  T more;
-  lazy_assign(&less, &more) =
-  lazy_if (a < b) ^[&]() {
-    less = a;
-    more = b;
-  } || lazy_elif (boolean<T>{false}) ^[&]() {
-    // nothing
-  } || lazy_else ^[&]() {
-    less = b;
-    more = a;
-  };
-  return std::make_pair(less, more);
-}
+// TODO(jwnimmer-tri) Test use of true/false literals as conditions.
 
+// Tests conditionally assigning to a result.
+template <typename T>
+std::pair<T, T> MinMax(const T& a, const T& b) {
+  using namespace drake::symbolic::branching_literals;
+  std::pair<T, T> result{};
+  lazy_assign(&result.first, &result.second) =
+  lazy_if (a < b) ^[&]() {
+    result = std::make_pair(a, b);
+  } || lazy_else ^[&]() {
+    result = std::make_pair(b, a);
+  };
+  return result;
+}
 TEST_F(SymbolicExpressionTest, MinMax) {
-  auto result = min_max(x_, y_);
+  EXPECT_EQ(MinMax(2.0, 1.0), std::make_pair(1.0, 2.0));
+  auto result = MinMax(x_, y_);
   EXPECT_PRED2(ExprEqual, result.first, if_then_else(x_ < y_, x_, y_));
   EXPECT_PRED2(ExprEqual, result.second, if_then_else(x_ < y_, y_, x_));
 }
 
-GTEST_TEST(DoubleTest, ShortCircuit1) {
+// Tests conditionally raising an exception.
+template <typename T>
+T CheckedSqrt(const T& a) {
+  using std::sqrt;
   using namespace drake::symbolic::branching_literals;
-  using T = double;
-  T a{0.0};
-  lazy_assign(&a) =
-  lazy_if (a >= 0) ^[&]() {
-    a -= 1.0;
-    std::cout << "Side effect!\n";
+  T result{};
+  lazy_assign(&result) =
+  lazy_if (a < 0) ^[&]() {
+    throw std::logic_error("CheckedSqrt requires a positive value");
   } || lazy_else ^[&]() {
-    // This will be skipped, because `a` was >= 0.
-    DRAKE_DEMAND(false);
+    result = sqrt(a);
   };
-  EXPECT_EQ(a, -1.0);
+  return result;
+}
+TEST_F(SymbolicExpressionTest, CheckedSqrt) {
+  EXPECT_EQ(CheckedSqrt(4.0), 2.0);
+  EXPECT_THROW(CheckedSqrt(x_), std::exception);
+#if 0  // We want this to pass, but it doesn't yet.
+  EXPECT_PRED2(ExprEqual, CheckedSqrt(Expression{4.0}), 2.0);
+#endif
 }
 
-bool NeverCalled() {
-  DRAKE_DEMAND(false);
-  return false;
+// Tests that elif predicates remain unevaluated when a prior condition has
+// already matched.
+template <typename T>
+boolean<T> ReturnsBoolButAlwaysThrows() {
+  throw std::logic_error("ReturnsBoolButAlwaysThrows was invoked");
+  return boolean<T>(false);
 }
-
-GTEST_TEST(DoubleTest, ShortCircuit2) {
+template <typename T>
+T Identity(const T& a) {
   using namespace drake::symbolic::branching_literals;
-  using T = double;
-  T a{0.0};
-  lazy_assign(&a) =
-  lazy_if (a >= 0) ^[&]() {
-  } || lazy_elif (NeverCalled()) ^[&]() {
-    DRAKE_DEMAND(false);
+  T result{};
+  lazy_assign(&result) =
+  lazy_if (a == a) ^[&]() {
+    result = a;
+  } || lazy_elif (ReturnsBoolButAlwaysThrows<T>()) ^[&]() {
+    throw std::logic_error("Wrong branch in Identity");
   };
+  return result;
 }
+TEST_F(SymbolicExpressionTest, Identity) {
+  EXPECT_EQ(Identity(1.0), 1.0);
+#if 0  // We want this to pass, but it doesn't yet.
+  EXPECT_PRED2(ExprEqual, Identity(Expression{1.0}), 1.0);
+  EXPECT_PRED2(ExprEqual, Identity(x_), x_);
+#endif
+}
+
+// Tests side effects that aren't scalar assignments.
+template <typename T>
+T AbsLogWhenNegative(const T& a, std::ostream& out) {
+  using namespace drake::symbolic::branching_literals;
+  T result{};
+  lazy_assign(&result) =
+  lazy_if (a >= 0) ^[&]() {
+    result = a;
+  } || lazy_else ^[&]() {
+    out << fmt::format("a = '{}' was negative", a);
+    result = -a;
+  };
+  return result;
+}
+TEST_F(SymbolicExpressionTest, AbsLogWhenNegative) {
+  std::stringstream out;
+  EXPECT_EQ(AbsLogWhenNegative(1.0, out), 1.0);
+  EXPECT_EQ(out.str(), "");
+  EXPECT_EQ(AbsLogWhenNegative(-1.0, out), 1.0);
+  EXPECT_EQ(out.str(), "a = '-1' was negative");
+  // TODO Add Expression tests.
+}
+
+// Tests nested conditional assignments.
+// TODO Write me.
 
 }  // namespace
 }  // namespace symbolic
