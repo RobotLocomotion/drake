723df8e385e94ac5d6214855a79ddca26a8e8676
Pang Tao <pangtao@mit.edu>
2019-04-26 21:07:17 -0400

    add abstract port of type PlanData to controller systems

--- manipulation/robot_plan_runner/controller_systems.cc
+++ manipulation/robot_plan_runner/controller_systems.cc
@@ -8,17 +8,18 @@ namespace robot_plan_runner {
 using systems::BasicVector;
 using systems::kVectorValued;
 
-RobotController::RobotController(PlanTypes plan_type) : num_positions_(7) {
-  switch(plan_type) {
-    case PlanTypes::kJointSpaceController:
+RobotController::RobotController(PlanType plan_type) : num_positions_(7) {
+  switch (plan_type) {
+    case PlanType::kJointSpaceController:
       this->set_name("JointSpaceController");
       plan_ = std::make_unique<JointSpacePlan>();
       break;
-    case PlanTypes::kTaskSpaceController:
+    case PlanType::kTaskSpaceController:
       this->set_name("TaskSpaceController");
       break;
   }
 
+  // declare input and output ports
   input_port_idx_q_ =
       this->DeclareInputPort("q", kVectorValued, num_positions_).get_index();
   input_port_idx_v_ =
@@ -26,26 +27,36 @@ RobotController::RobotController(PlanTypes plan_type) : num_positions_(7) {
   input_port_idx_tau_ext_ =
       this->DeclareInputPort("tau_ext", kVectorValued, num_positions_)
           .get_index();
-  output_port_idx_cmd_ = this->DeclareVectorOutputPort(
-                                   "q_cmd", BasicVector<double>(num_positions_),
-                                   &RobotController::CalcCommands)
-                               .get_index();
+  input_port_idx_plan_data =
+      this->DeclareAbstractInputPort("plan_data", Value<PlanData>())
+          .get_index();
+  this->DeclareVectorOutputPort("q_cmd", BasicVector<double>(num_positions_),
+                                &RobotController::CalcCommands)
+      .get_index();
 
+  // resize data members
   q_cmd_.resize(num_positions_);
   tau_cmd_.resize(num_positions_);
 };
 
-
-void RobotController::CalcCommands(
-    const systems::Context<double>& context,
-    BasicVector<double>* q_tau_cmd) const {
+void RobotController::CalcCommands(const systems::Context<double>& context,
+                                   BasicVector<double>* q_tau_cmd) const {
   Eigen::VectorBlock<VectorX<double>> q_tau_vector =
       q_tau_cmd->get_mutable_value();
 
-  auto q = this->get_input_port(input_port_idx_q_).Eval(context);
-  auto v = this->get_input_port(input_port_idx_v_).Eval(context);
-  auto tau_ext = this->get_input_port(input_port_idx_tau_ext_).Eval(context);
+  const auto& q = this->get_input_port(input_port_idx_q_).Eval(context);
+  const auto& v = this->get_input_port(input_port_idx_v_).Eval(context);
+  const auto& tau_ext =
+      this->get_input_port(input_port_idx_tau_ext_).Eval(context);
+  const AbstractValue* plan_data_ptr =
+      this->EvalAbstractInput(context, input_port_idx_plan_data);
+  const auto& plan_data_value = plan_data_ptr->get_value<PlanData>();
 
+  // check if the plan from input port is new.
+  if(plan_data_value.plan_index > plan_index_current_) {
+    plan_->UpdatePlan(plan_data_value);
+    plan_index_current_ = plan_data_value.plan_index;
+  }
   // TODO: record plan start time and take the difference.
   double t = context.get_time();
 
--- manipulation/robot_plan_runner/robot_plans.h
+++ manipulation/robot_plan_runner/robot_plans.h
@@ -4,41 +4,55 @@
 
 #include <Eigen/Dense>
 
-#include "drake/multibody/plant/multibody_plant.h"
 #include "drake/common/trajectories/piecewise_polynomial.h"
+#include "drake/common/trajectories/piecewise_quaternion.h"
+#include "drake/multibody/plant/multibody_plant.h"
 
 namespace drake {
 namespace manipulation {
 namespace robot_plan_runner {
 
-enum class PlanTypes { kJointSpaceController, kTaskSpaceController };
+enum class PlanType { kJointSpaceController, kTaskSpaceController };
+
+struct PlanData {
+  PlanType plan_type;
+  int plan_index;
+  optional<trajectories::PiecewisePolynomial<double>> joint_traj;
+  struct EeData {
+    trajectories::PiecewisePolynomial<double> ee_xyz_traj;
+    trajectories::PiecewiseQuaternionSlerp<double> ee_quat_traj;
+  };
+  optional<EeData> ee_traj;
+};
 
 class PlanBase {
  public:
   PlanBase() = default;
-  virtual ~PlanBase() {};
+  virtual ~PlanBase(){};
 
   virtual void Step(const Eigen::Ref<const Eigen::VectorXd>& q,
                     const Eigen::Ref<const Eigen::VectorXd>& v,
                     const Eigen::Ref<const Eigen::VectorXd>& tau_external,
                     double t, Eigen::VectorXd* const q_commanded,
                     Eigen::VectorXd* const tau_commanded) const = 0;
+  virtual void UpdatePlan(const PlanData& plan_data) = 0;
+
 };
 
 class JointSpacePlan : public PlanBase {
  public:
-  JointSpacePlan() : num_positions_(7) {};
+  JointSpacePlan() : num_positions_(7){};
 
-  void UpdatePlan(
-      std::unique_ptr<trajectories::PiecewisePolynomial<double>> q_traj_new);
+  void UpdatePlan(const PlanData& plan_data) override;
 
   void Step(const Eigen::Ref<const Eigen::VectorXd>& q,
             const Eigen::Ref<const Eigen::VectorXd>& v,
             const Eigen::Ref<const Eigen::VectorXd>& tau_external, double t,
             Eigen::VectorXd* const q_cmd,
             Eigen::VectorXd* const tau_cmd) const override;
+
  private:
-  std::unique_ptr<trajectories::PiecewisePolynomial<double>> q_traj_;
+  std::unique_ptr<const trajectories::PiecewisePolynomial<double>> q_traj_;
   const int num_positions_;
 };
 
