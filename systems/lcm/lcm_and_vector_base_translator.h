af541e594f1553b785bb50124d97128c155e4dbd
Jeremy Nimmer <jeremy.nimmer@tri.global>
2019-04-30 22:38:18 -0400

    systems/lcm: Deprecate LcmAndVectorBaseTranslator

--- systems/lcm/lcm_and_vector_base_translator.h
+++ systems/lcm/lcm_and_vector_base_translator.h
@@ -5,18 +5,20 @@
 #include <vector>
 
 #include "drake/common/drake_copyable.h"
+#include "drake/common/drake_deprecated.h"
 #include "drake/systems/framework/basic_vector.h"
 
 namespace drake {
 namespace systems {
 namespace lcm {
 
-// TODO(jwnimmer-tri) We should deprecated and then remove this base class.
 /**
  * Defines an abstract parent class of all translators that convert between
  * LCM message bytes and `drake::systems::VectorBase` objects.
  */
-class LcmAndVectorBaseTranslator {
+class DRAKE_DEPRECATED("2019-08-01",
+    "LcmAndVectorBaseTranslator is deprecated, with no replacement.")
+LcmAndVectorBaseTranslator {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(LcmAndVectorBaseTranslator)
 
@@ -25,6 +27,8 @@ class LcmAndVectorBaseTranslator {
    *
    * @param[in] size The size of the vector in the `VectorBase`.
    */
+  DRAKE_DEPRECATED("2019-08-01",
+      "LcmAndVectorBaseTranslator is deprecated, with no replacement.")
   explicit LcmAndVectorBaseTranslator(int size);
   virtual ~LcmAndVectorBaseTranslator();
 
@@ -32,6 +36,8 @@ class LcmAndVectorBaseTranslator {
    * Returns the size of the vector in the `drake::systems::VectorBase`
    * object.
    */
+  DRAKE_DEPRECATED("2019-08-01",
+      "LcmAndVectorBaseTranslator is deprecated, with no replacement.")
   int get_vector_size() const;
 
   /**
@@ -43,6 +49,8 @@ class LcmAndVectorBaseTranslator {
    * The default implementation in this class returns nullptr.  Subclasses that
    * require custom VectorBase subtypes should override it.
    */
+  DRAKE_DEPRECATED("2019-08-01",
+      "LcmAndVectorBaseTranslator is deprecated, with no replacement.")
   virtual std::unique_ptr<BasicVector<double>> AllocateOutputVector() const;
 
   /**
@@ -62,6 +70,8 @@ class LcmAndVectorBaseTranslator {
    * This often occurs when the size of the @p vector_base does not equal
    * or is incompatible with the size of the decoded LCM message.
    */
+  DRAKE_DEPRECATED("2019-08-01",
+      "LcmAndVectorBaseTranslator is deprecated, with no replacement.")
   virtual void Deserialize(
       const void* lcm_message_bytes, int lcm_message_length,
       VectorBase<double>* vector_base) const = 0;
@@ -77,6 +87,8 @@ class LcmAndVectorBaseTranslator {
    * @param[out] lcm_message_bytes The LCM message bytes.
    * This pointer must not be `nullptr`.
    */
+  DRAKE_DEPRECATED("2019-08-01",
+      "LcmAndVectorBaseTranslator is deprecated, with no replacement.")
   virtual void Serialize(double time,
       const VectorBase<double>& vector_base,
       std::vector<uint8_t>* lcm_message_bytes) const = 0;
