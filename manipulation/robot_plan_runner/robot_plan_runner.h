e819c501d7199864ab52978b8e0ea269160427bc
Pang Tao <pangtao@mit.edu>
2019-05-03 10:48:54 -0400

    Add documentation.

--- manipulation/robot_plan_runner/robot_plan_runner.h
+++ manipulation/robot_plan_runner/robot_plan_runner.h
@@ -9,6 +9,10 @@ namespace robot_plan_runner {
 class RobotPlanRunner : public systems::Diagram<double> {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(RobotPlanRunner);
+  /*
+   * If control_period_sec is non-zero, A ZOH with period control_period_sec is
+   * added.  Otherwise the system is stateless. 
+   */
   explicit RobotPlanRunner(double control_period_sec = 0.005);
 };
 
