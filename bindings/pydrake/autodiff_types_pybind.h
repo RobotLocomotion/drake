e544d84b05b9c6e2209de595a7b7d4f4b881296c
Eric Cousineau <eric.cousineau@tri.global>
2019-05-05 20:25:48 -0400

    py math: Disentangle .eigen_geometry, .math, .autodiff, and .symbolic

--- bindings/pydrake/autodiff_types_pybind.h
+++ bindings/pydrake/autodiff_types_pybind.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+
 #include <Eigen/Core>
 #include "pybind11/eigen.h"
 #include "pybind11/pybind11.h"
@@ -11,3 +13,45 @@
 // `pybind11::detail`, so we should not place these in `drake::pydrake`.
 
 PYBIND11_NUMPY_OBJECT_DTYPE(drake::AutoDiffXd);
+
+namespace drake {
+namespace pydrake {
+namespace internal {
+
+// TODO(eric.cousineau): Deprecate these methods once we support proper NumPy
+// UFuncs.
+
+template <typename PyObject>
+void BindAutoDiffOverloads(PyObject* obj) {
+  // TODO(m-chaturvedi) Add Pybind11 documentation.
+  (*obj)  // BR
+      .def("log", [](const AutoDiffXd& x) { return log(x); })
+      .def("abs", [](const AutoDiffXd& x) { return abs(x); })
+      .def("exp", [](const AutoDiffXd& x) { return exp(x); })
+      .def("sqrt", [](const AutoDiffXd& x) { return sqrt(x); })
+      .def("pow", [](const AutoDiffXd& x, double y) { return pow(x, y); })
+      .def("sin", [](const AutoDiffXd& x) { return sin(x); })
+      .def("cos", [](const AutoDiffXd& x) { return cos(x); })
+      .def("tan", [](const AutoDiffXd& x) { return tan(x); })
+      .def("asin", [](const AutoDiffXd& x) { return asin(x); })
+      .def("acos", [](const AutoDiffXd& x) { return acos(x); })
+      .def("atan2",
+          [](const AutoDiffXd& y, const AutoDiffXd& x) { return atan2(y, x); })
+      .def("sinh", [](const AutoDiffXd& x) { return sinh(x); })
+      .def("cosh", [](const AutoDiffXd& x) { return cosh(x); })
+      .def("tanh", [](const AutoDiffXd& x) { return tanh(x); })
+      .def("min",
+          [](const AutoDiffXd& x, const AutoDiffXd& y) { return min(x, y); })
+      .def("max",
+          [](const AutoDiffXd& x, const AutoDiffXd& y) { return max(x, y); })
+      .def("ceil", [](const AutoDiffXd& x) { return ceil(x); })
+      .def("floor", [](const AutoDiffXd& x) { return floor(x); })
+      // Matrix
+      .def("inv", [](const MatrixX<AutoDiffXd>& X) -> MatrixX<AutoDiffXd> {
+        return X.inverse();
+      });
+}
+
+}  // namespace internal
+}  // namespace pydrake
+}  // namespace drake
--- bindings/pydrake/symbolic_types_pybind.h
+++ bindings/pydrake/symbolic_types_pybind.h
@@ -17,3 +17,44 @@ PYBIND11_NUMPY_OBJECT_DTYPE(drake::symbolic::Formula);
 PYBIND11_NUMPY_OBJECT_DTYPE(drake::symbolic::Monomial);
 PYBIND11_NUMPY_OBJECT_DTYPE(drake::symbolic::Polynomial);
 PYBIND11_NUMPY_OBJECT_DTYPE(drake::symbolic::Variable);
+
+namespace drake {
+namespace pydrake {
+namespace internal {
+
+// TODO(eric.cousineau): Deprecate these methods once we support proper NumPy
+// UFuncs.
+
+template <typename PyObject>
+void BindSymbolicOverloads(PyObject obj) {
+  using symbolic::Expression;
+  (*obj)  // BR
+      .def("log", &symbolic::log)
+      .def("abs", &symbolic::abs)
+      .def("exp", &symbolic::exp)
+      .def("sqrt", &symbolic::sqrt)
+      .def("pow", py::overload_cast<const Expression&, const Expression&>(
+                      &symbolic::pow))
+      .def("sin", &symbolic::sin)
+      .def("cos", &symbolic::cos)
+      .def("tan", &symbolic::tan)
+      .def("asin", &symbolic::asin)
+      .def("acos", &symbolic::acos)
+      .def("atan", &symbolic::atan)
+      .def("atan2", &symbolic::atan2)
+      .def("sinh", &symbolic::sinh)
+      .def("cosh", &symbolic::cosh)
+      .def("tanh", &symbolic::tanh)
+      .def("min", &symbolic::min)
+      .def("max", &symbolic::max)
+      .def("ceil", &symbolic::ceil)
+      .def("floor", &symbolic::floor)
+      // Matrix overloads.
+      .def("inv", [](const MatrixX<Expression>& X) -> MatrixX<Expression> {
+        return X.inverse();
+      });
+}
+
+}  // namespace internal
+}  // namespace pydrake
+}  // namespace drake
