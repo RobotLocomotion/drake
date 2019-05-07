ce25ab941ab0c943f3fdce447169a47c0dd46752
hongkai-dai <daihongkai@gmail.com>
2019-05-07 10:15:20 -0700

    minor change.

--- geometry/proximity/proximity_utilities.cc
+++ geometry/proximity/proximity_utilities.cc
@@ -40,8 +40,6 @@ std::string GetGeometryName(const fcl::CollisionObjectd& object) {
       return "Octtree";
     case fcl::NODE_COUNT:
       return "Unsupported";
-    default:
-      throw std::runtime_error("Should not reach here");
   }
   DRAKE_UNREACHABLE();
 }
