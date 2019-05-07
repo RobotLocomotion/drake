51568bea0961e79ddd4187edc3fea04644c3be19
Eric Cousineau <eric.cousineau@tri.global>
2019-05-06 22:09:22 -0400

    Remove redundant `math_overloads_*` tests
    Module loading order is no longer an issue

--- bindings/pydrake/test/math_overloads_ad_test.py
+++
@@ -1,9 +0,0 @@
-"""
-See `math_overloads_test_utilities.py` for information.
-"""
-import pydrake.test.math_overloads_test_utilities as mtest
-
-
-class TestMathOverloads(mtest.MathOverloadsBase):
-    def test_overloads(self):
-        self.check_overload(mtest.AutoDiffOverloads())
