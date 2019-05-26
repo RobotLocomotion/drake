43403ccd83dcd5d25323f767908bca7d5a09cce1
Katy Muhlrad <kmuhlrad@gmail.com>
2019-05-26 14:54:00 -0400

    remove TODO

--- examples/manipulation_station/manipulation_station.h
+++ examples/manipulation_station/manipulation_station.h
@@ -152,7 +152,6 @@ class ManipulationStation : public systems::Diagram<T> {
       const optional<const math::RigidTransformd>& X_WCameraBody = {},
       IiwaCollisionModel collision_model = IiwaCollisionModel::kNoCollision);
 
-  // TODO(kmuhlrad): Rename SetupMITClassStation.
   /// Adds a default iiwa, wsg, cupboard, and 80/20 frame for the MIT
   /// Intelligent Robot Manipulation class, then calls
   /// RegisterIiwaControllerModel() and RegisterWsgControllerModel() with
