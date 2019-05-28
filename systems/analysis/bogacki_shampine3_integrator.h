5e6eba467e494de8e1df8fd612028248a768674b
Evan Drumwright <drum@tri.global>
2019-05-28 09:30:12 -0700

    Checkpoint.

--- systems/analysis/bogacki_shampine3_integrator.h
+++ systems/analysis/bogacki_shampine3_integrator.h
@@ -11,35 +11,32 @@ namespace drake {
 namespace systems {
 
 /**
- * A third-order, four-stage, first-same-as-last (FSAL) Runge Kutta integrator
- * with a second order error estimate.
- * @tparam T A double or autodiff type.
- *
- * Instantiated templates for the following kinds of T's are provided:
- *
- * - double
- * - AutoDiffXd
- *
- * For a discussion of this Runge-Kutta method, see [Hairer, 1993].
- *
- * The Butcher tableaux for this integrator follows:
- * <pre>
- * 0    |
- * 1/2  | 1/2
- * 3/4  | 0           3/4
- * 1    | 2/9         1/3      4/9
- * -----------------------------------------------------------------------------
-          2/9         1/3      4/9     0
-          7/24        1/4      1/3     1/8
+ A third-order, four-stage, first-same-as-last (FSAL) Runge Kutta integrator
+ with a second order error estimate.
 
- * </pre>
- * where the second to last row is the 3rd-order (propagated) solution and
- * the last row gives a 2nd-order accurate solution which, when subtracted
- * from the propagated solution, yields the 2nd-order error estimate of the
- * error.
- *
- * - [Bogacki, 1989] P. Bogacki and L. Shampine. "A 3(2) pair of Runge–Kutta
- *   formulas", Appl. Math. Letters, 2 (4): 321–325, 1989.
+ @tparam T A double or autodiff type.
+
+ Instantiated templates for the following kinds of T's are provided:
+ - double
+ - AutoDiffXd
+
+ For a discussion of this Runge-Kutta method, see [Hairer, 1993].
+ The Butcher tableaux for this integrator follows:
+ <pre>
+ 0    |
+ 1/2  | 1/2
+ 3/4  | 0           3/4
+ 1    | 2/9         1/3      4/9
+ -----------------------------------------------------------------------------
+        2/9         1/3      4/9     0
+        7/24        1/4      1/3     1/8
+</pre>
+ where the second to last row is the 3rd-order (propagated) solution and
+ the last row gives a 2nd-order accurate solution which, when subtracted
+ from the propagated solution, yields the 2nd-order error estimate of the
+ error.
+  - [Bogacki, 1989] P. Bogacki and L. Shampine. "A 3(2) pair of Runge–Kutta
+    formulas", Appl. Math. Letters, 2 (4): 321–325, 1989.
  */
 template <class T>
 class BogackiShampine3Integrator final : public IntegratorBase<T> {
