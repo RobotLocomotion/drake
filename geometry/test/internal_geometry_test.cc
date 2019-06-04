5d5ea6ee325b5fafe6f39d76b8435c2650da8759
Sean Curtis <sean.curtis@tri.global>
2019-06-04 10:48:49 -0700

    GeometryState adds role removal API.
    This provides the ability to remove one or more roles from a geometry or
    from the geometries attached to a frame. It also provides the explicit
    functions for removing a geometry from a particular renderer (as opposed
    to removing it from all renderers by virtue of removing the perception
    role).
    
    Note: some GeometryState documentation refers to implementations of
    SceneGraph methods that won't exist until the *next* PR.

--- geometry/test/internal_geometry_test.cc
+++ geometry/test/internal_geometry_test.cc
@@ -50,6 +50,86 @@ GTEST_TEST(InternalGeometryTest, RedundantPropertyAssignment) {
   EXPECT_TRUE(geometry.has_perception_role());
 }
 
+// Tests the removal of proximity and illustration roles -- the removal is
+// identical for both. Perception requires special treatment (see below).
+GTEST_TEST(InternalGeometryTest, RemoveRole_NonPerception) {
+  // Configure a geometry with all roles; we assume from previous unit tests
+  // that the geometry's state is correct.
+  InternalGeometry geometry;
+  geometry.SetRole(ProximityProperties());
+  geometry.SetRole(IllustrationProperties());
+
+  // Case: Remove proximity, illustration persists.
+  EXPECT_NO_THROW(geometry.RemoveProximityRole());
+  EXPECT_FALSE(geometry.has_role(Role::kProximity));
+  EXPECT_TRUE(geometry.has_role(Role::kIllustration));
+
+  // Case: Redundant removal of role is a no-op.
+  EXPECT_NO_THROW(geometry.RemoveProximityRole());
+  EXPECT_FALSE(geometry.has_role(Role::kProximity));
+  EXPECT_TRUE(geometry.has_role(Role::kIllustration));
+
+  // Case: Remove illustration, no roles exist.
+  EXPECT_NO_THROW(geometry.RemoveIllustrationRole());
+  EXPECT_FALSE(geometry.has_role(Role::kProximity));
+  EXPECT_FALSE(geometry.has_role(Role::kIllustration));
+
+  // Case: Redundant removal of role is a no-op.
+  EXPECT_NO_THROW(geometry.RemoveIllustrationRole());
+  EXPECT_FALSE(geometry.has_role(Role::kProximity));
+  EXPECT_FALSE(geometry.has_role(Role::kIllustration));
+}
+
+// Test the perception role removal. This is its own unique test because it has
+// special, per-renderer logic that doesn't apply to either proximity or
+// illustration.
+GTEST_TEST(InternalGeometryTest, RemovePerceptionRole) {
+  const std::string renderer1("renderer1");
+  const std::string renderer2("renderer2");
+
+  // Configure a geometry with all roles; we assume from previous unit tests
+  // that the geometry's state is correct.
+  InternalGeometry geometry;
+  const RenderIndex index1(10);
+  const RenderIndex index2(20);
+  geometry.set_render_index(renderer1, index1);
+  geometry.set_render_index(renderer2, index2);
+  geometry.SetRole(PerceptionProperties());
+
+  // Case: Remove render index for a non-existent render engine; old index is
+  // still valid and there are still perception properties.
+  EXPECT_NO_THROW(geometry.ClearRenderIndex("invalid"));
+  EXPECT_EQ(geometry.render_index(renderer1), index1);
+  EXPECT_EQ(geometry.render_index(renderer2), index2);
+  EXPECT_TRUE(geometry.has_role(Role::kPerception));
+
+  // Case: Remove render index for valid render engine; no index exists and it
+  // still has perception properties.
+  EXPECT_NO_THROW(geometry.ClearRenderIndex(renderer1));
+  EXPECT_EQ(geometry.render_index(renderer1), nullopt);
+  EXPECT_EQ(geometry.render_index(renderer2), index2);
+  EXPECT_TRUE(geometry.has_role(Role::kPerception));
+
+  // Case: Remove perception properties while there is still a valid render
+  // index. All render indices are cleared.
+  EXPECT_NO_THROW(geometry.RemovePerceptionRole());
+  EXPECT_EQ(geometry.render_index(renderer1), nullopt);
+  EXPECT_EQ(geometry.render_index(renderer2), nullopt);
+  EXPECT_FALSE(geometry.has_role(Role::kPerception));
+
+  // Case: Removing render index leaves the geometry with *no* render indices,
+  // but it *still* has perception properties.
+  geometry.set_render_index(renderer1, index1);
+  geometry.SetRole(PerceptionProperties());
+  // Confirm it's wired up for renderer1.
+  EXPECT_EQ(geometry.render_index(renderer1), index1);
+  EXPECT_TRUE(geometry.has_role(Role::kPerception));
+
+  EXPECT_NO_THROW(geometry.ClearRenderIndex(renderer1));
+  EXPECT_EQ(geometry.render_index(renderer1), nullopt);
+  EXPECT_TRUE(geometry.has_role(Role::kPerception));
+}
+
 }  // namespace
 }  // namespace internal
 }  // namespace geometry
