c3fcf668682045f38c83197d503b225bc0c8b72a
Evan Drumwright <drum@tri.global>
2019-06-04 13:49:46 -0700

    Updates from feature review.

--- multibody/plant/hydroelastic_traction_calculator.cc
+++ multibody/plant/hydroelastic_traction_calculator.cc
@@ -18,13 +18,40 @@ using drake::systems::Context;
 namespace drake {
 namespace multibody {
 
-template <class T>
+template <typename T>
+HydroelasticTractionCalculatorData<T>::HydroelasticTractionCalculatorData(
+    const systems::Context<T>& context,
+    const geometry::ContactSurface<T>& surface,
+    const MultibodyPlant<T>& plant) :
+    context_(context), surface_(surface), plant_(plant) {
+  // Get the transformation of the geometry for M to the world frame.
+  X_WM_ = GetGeometryTransformationToWorld(context_, surface.id_M());
+
+  // Get the bodies that the two geometries are attached to. We'll call these
+  // A and B.
+  BodyIndex bodyA_index = plant.GetBodyIndexFromRegisteredGeometryId(
+      surface.id_M());
+  BodyIndex bodyB_index = plant.GetBodyIndexFromRegisteredGeometryId(
+      surface.id_N());
+  const Body<T>& bodyA = plant.get_body(bodyA_index);
+  const Body<T>& bodyB = plant.get_body(bodyB_index);
+
+  // Get the transformation of the two bodies to the world frame.
+  X_WA_ = plant.EvalBodyPoseInWorld(context, bodyA);
+  X_WB_ = plant.EvalBodyPoseInWorld(context, bodyB);
+
+  // Get the spatial velocities for the two bodies (at the body frames).
+  V_WA_ = plant.EvalBodySpatialVelocityInWorld(context, bodyA);
+  V_WB_ = plant.EvalBodySpatialVelocityInWorld(context, bodyB);
+}
+
+template <typename T>
 const RigidTransform<T>
-HydroelasticTractionCalculator<T>::GetTransformFromGeometry(
+HydroelasticTractionCalculatorData<T>::GetGeometryTransformationToWorld(
     const Context<T>& context, geometry::GeometryId geometry_id) const {
   // Need the SceneGraphInspector to query the pose relating the geometry's pose
   // to the pose of the frame it is attached to.
-  const auto& query_object = plant_->get_geometry_query_input_port().
+  const auto& query_object = plant_.get_geometry_query_input_port().
       template Eval<geometry::QueryObject<T>>(context);
   const geometry::SceneGraphInspector<T>& inspector = query_object.inspector();
   // TODO(edrumwri): Replace this to use a RigidTransform reference when
@@ -33,36 +60,36 @@ HydroelasticTractionCalculator<T>::GetTransformFromGeometry(
       RigidTransform<double>(inspector.X_PG(geometry_id)).template cast<T>();
 
   // Compute the pose relating the geometry's pose to the world frame.
-  BodyIndex body_index = plant_->GetBodyIndexFromRegisteredGeometryId(
+  BodyIndex body_index = plant_.GetBodyIndexFromRegisteredGeometryId(
       geometry_id);
-  const Body<T>& body = plant_->get_body(body_index);
-  return plant_->EvalBodyPoseInWorld(context, body) * X_PG;
+  const Body<T>& body = plant_.get_body(body_index);
+  return plant_.EvalBodyPoseInWorld(context, body) * X_PG;
 }
 
-template <class T>
+template <typename T>
 void HydroelasticTractionCalculator<T>::
 ComputeSpatialForcesAtBodyOriginsFromTraction(
+    const HydroelasticTractionCalculatorData<T>& data,
     const Vector3<T>& p_WQ,
     const Vector3<T>& traction_Q_W,
-    const RigidTransform<T>& X_WM, const RigidTransform<T>& X_WN,
     SpatialForce<T>* F_Mo_W, SpatialForce<T>* F_No_W) const {
   // Set the two vectors from the contact point to the two body frames, all
   // expressed in the world frame.
-  const Vector3<T> p_QMo_W = X_WM.translation() - p_WQ;
-  const Vector3<T> p_QNo_W = X_WN.translation() - p_WQ;
+  const Vector3<T> p_QAo_W = data.X_WA().translation() - p_WQ;
+  const Vector3<T> p_QBo_W = data.X_WB().translation() - p_WQ;
 
   // Convert the traction to a momentless-spatial force (i.e., without
   // changing the point of application). This force will be applied to one
   // body and the (negated) reaction force will be applied to the other.
   SpatialForce<T> F_Q_W(Vector3<T>(0, 0, 0), traction_Q_W);
-  *F_Mo_W = F_Q_W.Shift(p_QMo_W);
-  *F_No_W = (-F_Q_W).Shift(p_QNo_W);
+  *F_Mo_W = F_Q_W.Shift(p_QAo_W);
+  *F_No_W = (-F_Q_W).Shift(p_QBo_W);
 }
 
 // Determines the point of contact corresponding to the given barycentric
 // coordinates. Returns an offset vector from the world frame to the point of
 // contact (Q), expressed in the world frame.
-template <class T>
+template <typename T>
 Vector3<T> HydroelasticTractionCalculator<T>::CalcContactPoint(
     const ContactSurface<T>& surface,
     geometry::SurfaceFaceIndex face_index,
@@ -76,60 +103,48 @@ Vector3<T> HydroelasticTractionCalculator<T>::CalcContactPoint(
   const auto& vc = mesh.vertex(mesh.element(face_index).vertex(2));
   const Vector3<T> r_MQ = va.r_MV() * Q_barycentric_M[0] +
       vb.r_MV() * Q_barycentric_M[1] + vc.r_MV() * Q_barycentric_M[2];
-  return X_WM.inverse() * r_MQ;
+  return X_WM * r_MQ;
 }
 
-template <class T>
+template <typename T>
 Vector3<T> HydroelasticTractionCalculator<T>::CalcTractionAtPoint(
-    const Context<T>& context, const ContactSurface<T>& surface,
+    const HydroelasticTractionCalculatorData<T>& data,
     geometry::SurfaceFaceIndex face_index,
     const typename geometry::SurfaceMesh<T>::Barycentric&
         Q_barycentric_M, double dissipation, double mu_coulomb,
-    const RigidTransform<T>& X_WM, const RigidTransform<T>& X_WN,
     Vector3<T>* p_WQ) const {
   // Compute the point of contact in the world frame.
-  *p_WQ = CalcContactPoint(surface, face_index, Q_barycentric_M, X_WM);
-
-  // Get the bodies that the two geometries are attached to.
-  BodyIndex bodyM_index = plant_->GetBodyIndexFromRegisteredGeometryId(
-      surface.id_M());
-  BodyIndex bodyN_index = plant_->GetBodyIndexFromRegisteredGeometryId(
-      surface.id_N());
-  const Body<T>& bodyM = plant_->get_body(bodyM_index);
-  const Body<T>& bodyN = plant_->get_body(bodyN_index);
+  *p_WQ = CalcContactPoint(
+      data.surface(), face_index, Q_barycentric_M, data.X_WM());
 
   // Get the "hydroelastic pressure" at the point (in Newtons).
-  const T e_mn = surface.EvaluateE_MN(face_index, Q_barycentric_M);
+  const T e_mn = data.surface().EvaluateE_MN(face_index, Q_barycentric_M);
 
   // Get the normal from M to N, expressed in the global frame, to the contact
   // surface at Q.
-  const Vector3<T> h_MN_M = surface.EvaluateGrad_h_MN_M(
+  const Vector3<T> h_MN_M = data.surface().EvaluateGrad_h_MN_M(
       face_index, Q_barycentric_M);
   const Vector3<T> nhat_MN_M = h_MN_M.normalized();
-  const Vector3<T> nhat_MN_W = X_WM.rotation().transpose() * nhat_MN_M;
+  const Vector3<T> nhat_MN_W = data.X_WM().rotation() * nhat_MN_M;
 
   // Get the relative spatial velocity at the point Q between the
-  // two bodies, by subtracting the spatial velocity of a point (Nq)
-  // coincident with p_WQ on Body N from the spatial velocity of a point (Mq)
-  // coincident with p_WQ on Body M.
+  // two bodies, by subtracting the spatial velocity of a point (Bq)
+  // coincident with p_WQ on Body B from the spatial velocity of a point (Aq)
+  // coincident with p_WQ on Body A.
 
-  // First compute the spatial velocity of Body M at Mq.
-  const SpatialVelocity<T>& V_WM = plant_->EvalBodySpatialVelocityInWorld(
-    context, bodyM);
-  const Vector3<T> p_MoMq_W = *p_WQ - X_WM.translation();
-  const SpatialVelocity<T> V_WMq = V_WM.Shift(p_MoMq_W);
+  // First compute the spatial velocity of Body A at Aq.
+  const Vector3<T> p_AoAq_W = *p_WQ - data.X_WA().translation();
+  const SpatialVelocity<T> V_WAq = data.V_WA().Shift(p_AoAq_W);
 
-  // Next compute the spatial velocity of Body N at Nq.
-  const SpatialVelocity<T>& V_WN = plant_->EvalBodySpatialVelocityInWorld(
-    context, bodyN);
-  const Vector3<T> p_NoNq_W = *p_WQ - X_WN.translation();
-  const SpatialVelocity<T> V_WNq = V_WN.Shift(p_NoNq_W);
+  // Next compute the spatial velocity of Body B at Bq.
+  const Vector3<T> p_BoBq_W = *p_WQ - data.X_WB().translation();
+  const SpatialVelocity<T> V_WBq = data.V_WB().Shift(p_BoBq_W);
 
-  // Finally compute the relative velocity of Frame Mq relative to Frame Nq,
+  // Finally compute the relative velocity of Frame Aq relative to Frame Bq,
   // expressed in the world frame, and then the translational component of this
   // velocity.
-  const SpatialVelocity<T> V_NqMq_W = V_WMq - V_WNq;
-  const Vector3<T>& Qdot_NM_W = V_NqMq_W.translational();
+  const SpatialVelocity<T> V_BqAq_W = V_WAq - V_WBq;
+  const Vector3<T>& Qdot_NM_W = V_BqAq_W.translational();
 
   // Get the velocity along the normal to the contact surface. Note that a
   // positive value indicates that bodies are separating at Q while a negative
@@ -168,7 +183,9 @@ Vector3<T> HydroelasticTractionCalculator<T>::CalcTractionAtPoint(
 }  // namespace multibody
 }  // namespace drake
 
-// TODO(edrumwri) instantiate on SymbolicExpression when it no longer causes a
-// linker error complaining about an unresolved symbol in SceneGraph.
+// TODO(edrumwri) instantiate these on SymbolicExpression when they no longer
+// causes a linker error complaining about an unresolved symbol in SceneGraph.
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
+    class drake::multibody::HydroelasticTractionCalculatorData)
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
     class drake::multibody::HydroelasticTractionCalculator)
--- multibody/plant/test/hydroelastic_traction_test.cc
+++ multibody/plant/test/hydroelastic_traction_test.cc
@@ -43,7 +43,8 @@ GeometryId FindGeometry(
 
 }  // namespace internal
 
-class MultibodyPlantHydroelasticTractionTests : public ::testing::Test {
+class MultibodyPlantHydroelasticTractionTests :
+public ::testing::TestWithParam<math::RigidTransform<double>> {
  public:
   const MultibodyPlant<double>& plant() const { return *plant_; }
   Context<double>& plant_context() { return *plant_context_; }
@@ -53,8 +54,14 @@ class MultibodyPlantHydroelasticTractionTests : public ::testing::Test {
   const HydroelasticTractionCalculator<double>& traction_calculator() const {
     return *traction_calculator_;
   }
-  math::RigidTransform<double> GetTransformFromGeometry(GeometryId g) const {
-    return traction_calculator_->GetTransformFromGeometry(*plant_context_, g);
+  const HydroelasticTractionCalculatorData<double>& traction_calculator_data()
+      const {
+    return *traction_calculator_data_;
+  }
+  math::RigidTransform<double> GetGeometryTransformationToWorld(
+      GeometryId g) const {
+    return traction_calculator_data_->GetGeometryTransformationToWorld(
+        *plant_context_, g);
   }
 
   // Returns the default numerical tolerance.
@@ -64,25 +71,37 @@ class MultibodyPlantHydroelasticTractionTests : public ::testing::Test {
   void ComputeSpatialForcesAtBodyOriginsFromHydroelasticModel(
       double dissipation, double mu_coulomb,
       SpatialForce<double>* F_Mo_W, SpatialForce<double>* F_No_W) {
-    // Get the two transforms.
-    const auto X_WM = GetTransformFromGeometry(contact_surface().id_M());
-    const auto X_WN = GetTransformFromGeometry(contact_surface().id_N());
+    // Instantiate the traction calculator data.
+    traction_calculator_data_ =
+        std::make_unique<HydroelasticTractionCalculatorData<double>>(
+            *plant_context_, *contact_surface_, plant());
 
-    // First compute the traction.
+    // First compute the traction, expressed in the world frame.
     Vector3<double> p_WQ;
     const Vector3<double> traction_Q_W = traction_calculator().
         CalcTractionAtPoint(
-            plant_context(), contact_surface(), SurfaceFaceIndex(0),
+            traction_calculator_data(), SurfaceFaceIndex(0),
             SurfaceMesh<double>::Barycentric(1.0, 0.0, 0.0),
-            dissipation, mu_coulomb, X_WM, X_WN, &p_WQ);
+            dissipation, mu_coulomb, &p_WQ);
+
+    // Compute the expected point of contact in the world frame. Recall that
+    // the parameter to this test transforms both bodies to a pose Y in the
+    // world.
+    const Vector3<double> p_WQ_expected = GetParam() *
+        Vector3<double>(0.5, 0.5, -0.5);
 
     // Verify the point of contact.
-    ASSERT_NEAR(p_WQ[0], 0.5, eps_);
-    ASSERT_NEAR(p_WQ[1], 0.5, eps_);
-    ASSERT_NEAR(p_WQ[2], -0.5, eps_);
+    for (int i = 0; i < 3; ++i)
+      ASSERT_NEAR(p_WQ[i], p_WQ_expected[i], eps());
 
     traction_calculator().ComputeSpatialForcesAtBodyOriginsFromTraction(
-        p_WQ, traction_Q_W, X_WM, X_WN, F_Mo_W, F_No_W);
+        traction_calculator_data(), p_WQ, traction_Q_W, F_Mo_W, F_No_W);
+  }
+
+  void SetBoxTranslationalVelocity(const Vector3<double>& v) {
+    plant_->SetFreeBodySpatialVelocity(plant_context_,
+        plant_->GetBodyByName("box"),
+        SpatialVelocity<double>(Vector3<double>::Zero(), v));
   }
 
  private:
@@ -103,6 +122,9 @@ class MultibodyPlantHydroelasticTractionTests : public ::testing::Test {
     plant.Finalize();
     diagram_ = builder.Build();
 
+    ASSERT_EQ(plant.num_velocities(), 12);
+    ASSERT_EQ(plant.num_positions(), 14);
+
     // Create a context for this system.
     context_ = diagram_->CreateDefaultContext();
     plant_context_ =
@@ -110,11 +132,11 @@ class MultibodyPlantHydroelasticTractionTests : public ::testing::Test {
 
     contact_surface_ = CreateContactSurface();
 
-    // Set the pose and velocity for the box.
-    ASSERT_EQ(plant.num_velocities(), 6);
-    ASSERT_EQ(plant.num_positions(), 7);
+    // Set the poses for the two bodies.
     plant.SetFreeBodyPose(plant_context_,
-        plant.GetBodyByName("box"), math::RigidTransform<double>());
+        plant.GetBodyByName("box"), GetParam());
+    plant.SetFreeBodyPose(plant_context_,
+        plant.GetBodyByName("ground"), GetParam());
 
     // Instantiate the traction calculator.
     traction_calculator_ =
@@ -178,10 +200,12 @@ class MultibodyPlantHydroelasticTractionTests : public ::testing::Test {
   systems::Context<double>* plant_context_;
   std::unique_ptr<ContactSurface<double>> contact_surface_;
   std::unique_ptr<HydroelasticTractionCalculator<double>> traction_calculator_;
+  std::unique_ptr<HydroelasticTractionCalculatorData<double>>
+      traction_calculator_data_;
 };
 
 // Tests the traction calculation without any frictional or dissipation forces.
-TEST_F(MultibodyPlantHydroelasticTractionTests, VanillaTraction) {
+TEST_P(MultibodyPlantHydroelasticTractionTests, VanillaTraction) {
   const double dissipation = 0.0;
   const double mu_coulomb = 0.0;
 
@@ -190,20 +214,26 @@ TEST_F(MultibodyPlantHydroelasticTractionTests, VanillaTraction) {
   ComputeSpatialForcesAtBodyOriginsFromHydroelasticModel(
       dissipation, mu_coulomb, &F_Mo_W, &F_No_W);
 
+  // Re-express the spatial forces in Y's frame for easy interpretability.
+  const math::RotationMatrix<double>& R_WY = GetParam().rotation();
+  const Vector3<double> f_No_Y = R_WY.transpose() * F_No_W.translational();
+  const Vector3<double> tau_No_Y = R_WY.transpose() * F_No_W.rotational();
+
   // Check the spatial force at p. We know that geometry M is the halfspace,
   // so we'll check the spatial force for geometry N instead. Note that the
   // tangential components are zero.
-  EXPECT_NEAR(F_No_W.translational()[0], 0.0, eps());
-  EXPECT_NEAR(F_No_W.translational()[1], 0.0, eps());
-  EXPECT_NEAR(F_No_W.translational()[2], 0.5, eps());
+  EXPECT_NEAR(f_No_Y[0], 0.0, eps());
+  EXPECT_NEAR(f_No_Y[1], 0.0, eps());
+  EXPECT_NEAR(f_No_Y[2], 0.5, eps());
 
   // A moment on the tet will be generated due to the normal traction. The
-  // origin of the tet frame is located at (0,0,0) in the world frame.
-  // The moment arm at the point will be (.5, .5, -.5). Crossing this vector
-  // with the traction at that point (0, 0, 0.5) yields the following.
-  EXPECT_NEAR(F_No_W.rotational()[0], 0.25, eps());
-  EXPECT_NEAR(F_No_W.rotational()[1], -0.25, eps());
-  EXPECT_NEAR(F_No_W.rotational()[2], 0, eps());
+  // origin of the tet frame is located at (0,0,0) in the Y frame.
+  // The moment arm at the point will be (.5, .5, -.5), again in the Y frame.
+  // Crossing this vector with the traction at that point (0, 0, 0.5) yields the
+  // following.
+  EXPECT_NEAR(tau_No_Y[0], 0.25, eps());
+  EXPECT_NEAR(tau_No_Y[1], -0.25, eps());
+  EXPECT_NEAR(tau_No_Y[2], 0, eps());
 
   // The translational components of the two wrenches should be equal and
   // opposite.
@@ -212,21 +242,23 @@ TEST_F(MultibodyPlantHydroelasticTractionTests, VanillaTraction) {
 }
 
 // Tests the traction calculation with friction but without dissipation forces.
-TEST_F(MultibodyPlantHydroelasticTractionTests, TractionWithFraction) {
+TEST_P(MultibodyPlantHydroelasticTractionTests, TractionWithFraction) {
   const double dissipation = 0.0;
   const double mu_coulomb = 1.0;
 
-  // Give the tet an initial (horizontal) velocity along the +x axis.
-  const int num_velocities = plant().num_velocities();
-  ASSERT_EQ(num_velocities, 6);
-  VectorX<double> tet_velocity(num_velocities);
-  tet_velocity << 0, 0, 0, 1, 0, 0;
-  plant().SetVelocities(&plant_context(), tet_velocity);
+  // Give the tet an initial (vertical) velocity along the +x axis in the Y
+  // frame.
+  const math::RotationMatrix<double>& R_WY = GetParam().rotation();
+  SetBoxTranslationalVelocity(R_WY * Vector3<double>(1, 0, 0));
 
   // Compute the spatial forces at the origins of the body frames.
   multibody::SpatialForce<double> F_Mo_W, F_No_W;
   ComputeSpatialForcesAtBodyOriginsFromHydroelasticModel(
       dissipation, mu_coulomb, &F_Mo_W, &F_No_W);
+
+  // Re-express the spatial forces in Y's frame for easy interpretability.
+  const Vector3<double> f_No_Y = R_WY.transpose() * F_No_W.translational();
+  const Vector3<double> tau_No_Y = R_WY.transpose() * F_No_W.rotational();
 
   // Check the spatial force at p. We know that geometry M is the halfspace,
   // so we'll check the spatial force for geometry N instead. The coefficient
@@ -238,17 +270,17 @@ TEST_F(MultibodyPlantHydroelasticTractionTests, TractionWithFraction) {
   // has proven useful.
   const double regularization_scalar =
       traction_calculator().regularization_scalar();
-  EXPECT_NEAR(F_No_W.translational()[0], -0.5, regularization_scalar);
-  EXPECT_NEAR(F_No_W.translational()[1], 0.0, eps());
-  EXPECT_NEAR(F_No_W.translational()[2], 0.5, eps());
+  EXPECT_NEAR(f_No_Y[0], -0.5, regularization_scalar);
+  EXPECT_NEAR(f_No_Y[1], 0.0, eps());
+  EXPECT_NEAR(f_No_Y[2], 0.5, eps());
 
   // A moment on the tet will be generated due to the traction. The
-  // origin of the tet frame is located at (0,0,0) in the world frame.
+  // origin of the tet frame is located at (0,0,0) in the Y frame.
   // The moment arm at the point will be (.5, .5, -.5). Crossing this vector
   // with the traction at that point (-.5, 0, 0.5) yields the following.
-  EXPECT_NEAR(F_No_W.rotational()[0], 0.25, eps());
-  EXPECT_NEAR(F_No_W.rotational()[1], 0.0, regularization_scalar);
-  EXPECT_NEAR(F_No_W.rotational()[2], 0.25, regularization_scalar);
+  EXPECT_NEAR(tau_No_Y[0], 0.25, eps());
+  EXPECT_NEAR(tau_No_Y[1], 0.0, regularization_scalar);
+  EXPECT_NEAR(tau_No_Y[2], 0.25, regularization_scalar);
 
   // The translational components of the two wrenches should be equal and
   // opposite.
@@ -257,17 +289,16 @@ TEST_F(MultibodyPlantHydroelasticTractionTests, TractionWithFraction) {
 }
 
 // Tests the traction calculation with dissipation forces but without friction.
-TEST_F(MultibodyPlantHydroelasticTractionTests, TractionWithDissipation) {
+TEST_P(MultibodyPlantHydroelasticTractionTests, TractionWithDissipation) {
   const double dissipation = 1.0;
   const double mu_coulomb = 0.0;
 
-  // Give the tet an initial (vertical) velocity along the -z axis.
-  const int num_velocities = plant().num_velocities();
-  ASSERT_EQ(num_velocities, 6);
-  VectorX<double> tet_velocity(num_velocities);
+  // Give the tet an initial (vertical) velocity along the -z axis in the Y
+  // frame.
+  const math::RotationMatrix<double>& R_WY = GetParam().rotation();
   const double separating_velocity = -1.0;
-  tet_velocity << 0, 0, 0, 0, 0, separating_velocity;
-  plant().SetVelocities(&plant_context(), tet_velocity);
+  SetBoxTranslationalVelocity(R_WY *
+      Vector3<double>(0, 0, separating_velocity));
 
   // Compute the magnitude of the normal traction. Note that the damping
   // constant at each point will be field value * dissipation coefficient.
@@ -282,22 +313,26 @@ TEST_F(MultibodyPlantHydroelasticTractionTests, TractionWithDissipation) {
   ComputeSpatialForcesAtBodyOriginsFromHydroelasticModel(
       dissipation, mu_coulomb, &F_Mo_W, &F_No_W);
 
+  // Re-express the spatial forces in Y's frame for easy interpretability.
+  const Vector3<double> f_No_Y = R_WY.transpose() * F_No_W.translational();
+  const Vector3<double> tau_No_Y = R_WY.transpose() * F_No_W.rotational();
+
   // Check the spatial force at p. We know that geometry M is the halfspace,
   // so we'll check the spatial force for geometry N instead. The coefficient
   // of friction is unity, so the total frictional traction will have the same
   // magnitude as the normal traction.
-  EXPECT_NEAR(F_No_W.translational()[0], 0.0, eps());
-  EXPECT_NEAR(F_No_W.translational()[1], 0.0, eps());
-  EXPECT_NEAR(F_No_W.translational()[2], normal_traction_magnitude, eps());
+  EXPECT_NEAR(f_No_Y[0], 0.0, eps());
+  EXPECT_NEAR(f_No_Y[1], 0.0, eps());
+  EXPECT_NEAR(f_No_Y[2], normal_traction_magnitude, eps());
 
   // A moment on the tet will be generated due to the traction. The
   // origin of the tet frame is located at (0,0,0) in the world frame.
   // The moment arm at the point will be (.5, .5, -.5). Crossing this vector
   // with the traction at that point (0, 0, normal_traction_magnitude) yields
   // the following.
-  EXPECT_NEAR(F_No_W.rotational()[0], 0.5 * normal_traction_magnitude, eps());
-  EXPECT_NEAR(F_No_W.rotational()[1], -0.5 * normal_traction_magnitude, eps());
-  EXPECT_NEAR(F_No_W.rotational()[2], 0.0, eps());
+  EXPECT_NEAR(tau_No_Y[0], 0.5 * normal_traction_magnitude, eps());
+  EXPECT_NEAR(tau_No_Y[1], -0.5 * normal_traction_magnitude, eps());
+  EXPECT_NEAR(tau_No_Y[2], 0.0, eps());
 
   // The translational components of the two wrenches should be equal and
   // opposite.
@@ -305,5 +340,17 @@ TEST_F(MultibodyPlantHydroelasticTractionTests, TractionWithDissipation) {
       0, eps());
 }
 
+const math::RigidTransform<double> poses[] = {
+    math::RigidTransform<double>::Identity(),
+    math::RigidTransform<double>(
+        math::RotationMatrix<double>::MakeYRotation(M_PI_4),
+        drake::Vector3<double>(1, 2, 3))
+};
+
+INSTANTIATE_TEST_CASE_P(PoseInstantiations,
+                        MultibodyPlantHydroelasticTractionTests,
+                        ::testing::ValuesIn(poses));
+
 }  // namespace multibody
 }  // namespace drake
+
