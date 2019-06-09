790a9d526c77aa7abe6f7da8ac06b7adc76b9cb3
hongkai-dai <daihongkai@gmail.com>
2019-06-09 11:53:27 -0700

    Add python binding for symbolic::Polynomial::EvalPartial

--- bindings/pydrake/test/symbolic_test.py
+++ bindings/pydrake/test/symbolic_test.py
@@ -1044,3 +1044,15 @@ class TestSymbolicPolynomial(unittest.TestCase):
         env = {x: float('nan')}
         with self.assertRaises(RuntimeError):
             p.Evaluate(env)
+
+    def test_polynomial_evaluate_partial(self):
+        p = sym.Polynomial(a * x * x + b * x + c, [x])
+        env = {a: 2.0,
+               b: 3.0,
+               c: 5.0}
+        self.assertTrue(p.EvaluatePartial(env).EqualTo(
+            sym.Polynomial(env[a] * x * x + env[b] * x + env[c], [x])))
+
+        self.assertTrue(p.EvaluatePartial(a, 2).EqualTo(
+            sym.Polynomial(2 * x * x + b * x + c, [x])))
+
