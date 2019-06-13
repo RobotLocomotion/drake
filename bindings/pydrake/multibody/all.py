7dfdbaa184e7dd3bc5445c40f930bc9aab81cdb7
Eric Cousineau <eric.cousineau@tri.global>
2019-06-13 16:57:58 -0400

    pydrake: Remove catching of deprecation errors in `.all` modules
    Document sharp edges of the interply between deprecation and `.all`

--- bindings/pydrake/multibody/all.py
+++ bindings/pydrake/multibody/all.py
@@ -1,19 +1,5 @@
 import warnings
 
-# Deprecated symbols.
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore", DeprecationWarning)
-    # - Attic (#9314)
-    from .collision import *
-    from .joints import *
-    from .parsers import *
-    from .rigid_body_plant import *
-    from .rigid_body_tree import *
-    from .rigid_body import *
-    from .shapes import *
-    # - Shuffle (#9314. #9366)
-    from .multibody_tree.all import *  # noqa
-
 # Normal symbols.
 from .inverse_kinematics import *  # noqa
 from .math import *  # noqa
