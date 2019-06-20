539a4ee0568eee208ff09829f0ccf13b1be85a60
Jamie Snape <jamiesnape@users.noreply.github.com>
2019-06-20 09:07:43 -0400

    Set G_DEBUG and G_SLICE environment variables when running valgrind (#11679)

--- tools/dynamic_analysis/valgrind.sh
+++ tools/dynamic_analysis/valgrind.sh
@@ -2,6 +2,15 @@
 me=$(python -c 'import os; print(os.path.realpath("'"$0"'"))')
 mydir=$(dirname "$me")
 
+# Ensure that newly allocated memory that is not directly initialized by GLib
+# and memory being freed by GLib are reset to zero.
+export G_DEBUG=gc-friendly
+
+# Ensure that all GLib memory slices that are allocated through g_slice_alloc()
+# and released by g_slice_free1() are actually allocated via direct calls to
+# g_malloc() and g_free().
+export G_SLICE=always-malloc
+
 # Disable GTest death test use of clone() because it confuses valgrind.
 export GTEST_DEATH_TEST_USE_FORK=1
 
