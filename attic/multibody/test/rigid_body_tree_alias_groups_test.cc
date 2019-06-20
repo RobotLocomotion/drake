ae388a16f7904049b5adb42007a1f9e98c1d1610
Jeremy Nimmer <jeremy.nimmer@tri.global>
2019-06-20 10:24:45 -0400

    attic: Deprecate qp id, plan_eval, and alias_groups
    This improves build times and removes one of the only remaining C++ uses
    of protobuf.

--- attic/multibody/test/rigid_body_tree_alias_groups_test.cc
+++ attic/multibody/test/rigid_body_tree_alias_groups_test.cc
@@ -12,14 +12,18 @@ namespace {
 void TestFullConfig(multibody::joints::FloatingBaseType type) {
   std::string urdf = FindResourceOrThrow(
       "drake/multibody/test/rigid_body_tree/two_dof_robot.urdf");
-  std::string config = FindResourceOrThrow(
-      "drake/multibody/test/test.alias_groups");
 
   auto robot = std::make_unique<RigidBodyTree<double>>();
   parsers::urdf::AddModelInstanceFromUrdfFileToWorld(urdf, type, robot.get());
 
   RigidBodyTreeAliasGroups<double> alias(robot.get());
-  alias.LoadFromFile(config);
+
+  alias.AddBodyGroup("b_group1", {});
+  alias.AddBodyGroup("b_group2", {"link1"});
+  alias.AddBodyGroup("b_group2", {"link3"});
+  alias.AddBodyGroup("b_group3", {"world"});
+  alias.AddJointGroup("j_group1", {});
+  alias.AddJointGroup("j_group2", {"base", "joint1"});
 
   EXPECT_TRUE(alias.has_position_group("j_group1"));
   EXPECT_TRUE(alias.has_position_group("j_group2"));
---
+++ examples/qp_inverse_dynamics/README.md
@@ -0,0 +1,2 @@
+The examples/qp_inverse_dynamics folder is deprecated and will be removed from
+Drake on 2019-10-01.
