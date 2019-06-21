0c280c96195ed53adea32373c391cca82461997e
Hongkai Dai <daihongkai@gmail.com>
2019-06-20 21:23:47 -0700

    CSDP solves problem with no free variables. (#11658)
    CSDP solves problem with no free variables.

--- solvers/csdp_solver.cc
+++ solvers/csdp_solver.cc
@@ -1,19 +1,164 @@
 #include "drake/solvers/csdp_solver.h"
 
+#include <algorithm>
+#include <limits>
 #include <stdexcept>
+#include <unordered_map>
+#include <vector>
+
+#include <Eigen/Core>
+#include <Eigen/Sparse>
+#include <Eigen/SparseQR>
 
 #include "drake/solvers/csdp_solver_internal.h"
 
 namespace drake {
 namespace solvers {
-
-void CsdpSolver::DoSolve(const MathematicalProgram&, const Eigen::VectorXd&,
-                         const SolverOptions&,
-                         MathematicalProgramResult*) const {
-  // TODO(hongkai.dai): support DoSolve in subsequent PRs.
-  throw std::runtime_error("CsdpSolver::DoSolve() not supported yet");
+namespace {
+using internal::ConvertCsdpBlockMatrixtoEigen;
+using internal::SdpaFreeFormat;
+using internal::DecisionVariableInSdpaX;
+using internal::CsdpMatrixIndex;
+void SetCsdpSolverDetails(int csdp_ret, double pobj, double dobj, int y_size,
+                          double* y, const csdp::blockmatrix& Z,
+                          CsdpSolverDetails* solver_details) {
+  solver_details->return_code = csdp_ret;
+  solver_details->primal_objective = pobj;
+  solver_details->dual_objective = dobj;
+  solver_details->y_val.resize(y_size);
+  for (int i = 0; i < y_size; ++i) {
+    // CSDP uses Fortran 1-index array so the array y points to will be of size
+    // y_size + 1 with the zero entry ignored.
+    solver_details->y_val(i) = y[i + 1];
+  }
+  ConvertCsdpBlockMatrixtoEigen(Z, &(solver_details->Z_val));
 }
 
-bool CsdpSolver::is_available() { return false; }
+SolutionResult ConvertCsdpReturnToSolutionResult(int csdp_ret) {
+  switch (csdp_ret) {
+    case 0:
+    case 3:
+      return SolutionResult::kSolutionFound;
+    case 1:
+      return SolutionResult::kInfeasibleConstraints;
+    case 2:
+      return SolutionResult::kDualInfeasible;
+    case 4:
+      return SolutionResult::kIterationLimit;
+    default:
+      return SolutionResult::kUnknownError;
+  }
+}
+
+void SetProgramSolution(const SdpaFreeFormat& sdpa_free_format,
+                        const csdp::blockmatrix& X,
+                        const Eigen::VectorXd& s_val,
+                        Eigen::VectorXd* prog_sol) {
+  for (int i = 0;
+       i < static_cast<int>(sdpa_free_format.prog_var_in_sdpa().size()); ++i) {
+    if (holds_alternative<DecisionVariableInSdpaX>(
+            sdpa_free_format.prog_var_in_sdpa()[i])) {
+      const auto& decision_var_in_X = get<DecisionVariableInSdpaX>(
+          sdpa_free_format.prog_var_in_sdpa()[i]);
+
+      double X_entry_val{};
+      switch (X.blocks[decision_var_in_X.entry_in_X.block_index + 1]
+                  .blockcategory) {
+        case csdp::MATRIX: {
+          X_entry_val =
+              X.blocks[decision_var_in_X.entry_in_X.block_index + 1]
+                  .data.mat[CsdpMatrixIndex(
+                      decision_var_in_X.entry_in_X.row_index_in_block,
+                      decision_var_in_X.entry_in_X.column_index_in_block,
+                      X.blocks[decision_var_in_X.entry_in_X.block_index + 1]
+                          .blocksize)];
+          break;
+        }
+        case csdp::DIAG: {
+          X_entry_val =
+              X.blocks[decision_var_in_X.entry_in_X.block_index + 1]
+                  .data
+                  .vec[decision_var_in_X.entry_in_X.row_index_in_block + 1];
+          break;
+        }
+        default: {
+          throw std::runtime_error(
+              "SetProgramSolution(): unknown X block type.");
+        }
+      }
+      (*prog_sol)(i) =
+          decision_var_in_X.offset +
+          (decision_var_in_X.coeff_sign == internal::Sign::kPositive
+               ? X_entry_val
+               : -X_entry_val);
+    } else if (holds_alternative<double>(
+                   sdpa_free_format.prog_var_in_sdpa()[i])) {
+      (*prog_sol)(i) = get<double>(sdpa_free_format.prog_var_in_sdpa()[i]);
+    } else if (holds_alternative<SdpaFreeFormat::FreeVariableIndex>(
+                   sdpa_free_format.prog_var_in_sdpa()[i])) {
+      (*prog_sol)(i) = s_val(get<SdpaFreeFormat::FreeVariableIndex>(
+          sdpa_free_format.prog_var_in_sdpa()[i]));
+    }
+  }
+}
+
+void SolveProgramWithNoFreeVariables(
+    const MathematicalProgram& prog,
+    const SdpaFreeFormat& sdpa_free_format,
+    MathematicalProgramResult* result) {
+  DRAKE_ASSERT(sdpa_free_format.num_free_variables() == 0);
+
+  csdp::blockmatrix C;
+  double* rhs;
+  csdp::constraintmatrix* constraints{nullptr};
+  GenerateCsdpProblemDataWithoutFreeVariables(sdpa_free_format, &C, &rhs,
+                                              &constraints);
+
+  struct csdp::blockmatrix X, Z;
+  double* y;
+  csdp::initsoln(sdpa_free_format.num_X_rows(), sdpa_free_format.g().rows(), C,
+                 rhs, constraints, &X, &y, &Z);
+  double pobj, dobj;
+  const int ret = csdp::easy_sdp(
+      sdpa_free_format.num_X_rows(), sdpa_free_format.g().rows(), C, rhs,
+      constraints, -sdpa_free_format.constant_min_cost_term(), &X, &y, &Z,
+      &pobj, &dobj);
+
+  // Set solver details.
+  CsdpSolverDetails& solver_details =
+      result->SetSolverDetailsType<CsdpSolverDetails>();
+  SetCsdpSolverDetails(ret, pobj, dobj, sdpa_free_format.g().rows(), y, Z,
+                       &solver_details);
+  // Retrieve the information back to result.
+  // Since CSDP solves a maximization problem max -cost, where "cost" is the
+  // minimization cost in MathematicalProgram, we need to negate the cost.
+  result->set_optimal_cost(
+      ret == 1 /* primal infeasible */ ? MathematicalProgram::
+                                             kGlobalInfeasibleCost
+                                       : -pobj);
+  result->set_solution_result(ConvertCsdpReturnToSolutionResult(ret));
+  Eigen::VectorXd prog_sol(prog.num_vars());
+  SetProgramSolution(sdpa_free_format, X, Eigen::VectorXd::Zero(0), &prog_sol);
+  result->set_x_val(prog_sol);
+
+  csdp::free_prob(sdpa_free_format.num_X_rows(), sdpa_free_format.g().rows(), C,
+                  rhs, constraints, X, y, Z);
+}
+}  // namespace
+
+void CsdpSolver::DoSolve(const MathematicalProgram& prog,
+                         const Eigen::VectorXd&, const SolverOptions&,
+                         MathematicalProgramResult* result) const {
+  result->set_solver_id(CsdpSolver::id());
+  const SdpaFreeFormat sdpa_free_format(prog);
+  if (sdpa_free_format.num_free_variables() == 0) {
+    SolveProgramWithNoFreeVariables(prog, sdpa_free_format, result);
+  } else {
+    throw std::runtime_error(
+        "CsdpSolver::Solve(): do not support problem with free variables yet.");
+  }
+}
+
+bool CsdpSolver::is_available() { return true; }
 }  // namespace solvers
 }  // namespace drake
---
+++ solvers/test/csdp_solver_internal_test.cc
@@ -0,0 +1,298 @@
+#include "drake/solvers/csdp_solver_internal.h"
+
+#include <limits>
+#include <utility>
+
+#include <gtest/gtest.h>
+
+#include "drake/common/test_utilities/eigen_matrix_compare.h"
+#include "drake/solvers/test/csdp_test_examples.h"
+
+namespace drake {
+namespace solvers {
+namespace internal {
+
+void CompareBlockrec(const csdp::blockrec& block, csdp::blockcat blockcategory,
+                     int blocksize, const std::vector<double>& value,
+                     double tol) {
+  EXPECT_EQ(block.blockcategory, blockcategory);
+  EXPECT_EQ(block.blocksize, blocksize);
+  if (blockcategory == csdp::blockcat::MATRIX) {
+    for (int j = 0; j < blocksize; ++j) {
+      for (int i = 0; i < blocksize; ++i) {
+        EXPECT_NEAR(block.data.mat[ijtok(i + 1, j + 1, blocksize)],
+                    value[j * blocksize + i], tol);
+      }
+    }
+  } else if (blockcategory == csdp::blockcat::DIAG) {
+    for (int i = 0; i < blocksize; ++i) {
+      EXPECT_NEAR(block.data.vec[i + 1], value[i], tol);
+    }
+  } else {
+    throw std::invalid_argument("Unknown block category.");
+  }
+}
+
+void CheckSparseblock(const csdp::sparseblock& block,
+                      const std::vector<Eigen::Triplet<double>>& block_entries,
+                      int blocknum, int blocksize, int constraintnum) {
+  for (int i = 0; i < static_cast<int>(block_entries.size()); ++i) {
+    EXPECT_EQ(block.entries[i + 1], block_entries[i].value());
+    EXPECT_EQ(block.iindices[i + 1], block_entries[i].row() + 1);
+    EXPECT_EQ(block.jindices[i + 1], block_entries[i].col() + 1);
+    EXPECT_EQ(block.numentries, static_cast<int>(block_entries.size()));
+    EXPECT_EQ(block.blocknum, blocknum);
+    EXPECT_EQ(block.blocksize, blocksize);
+    EXPECT_EQ(block.constraintnum, constraintnum);
+  }
+}
+
+TEST_F(SDPwithOverlappingVariables,
+       GenerateCsdpProblemDataWithoutFreeVariables) {
+  const SdpaFreeFormat dut(*prog_);
+  struct csdp::blockmatrix C_csdp;
+  double* rhs_csdp;
+  struct csdp::constraintmatrix* constraints{nullptr};
+  GenerateCsdpProblemDataWithoutFreeVariables(dut, &C_csdp, &rhs_csdp,
+                                              &constraints);
+
+  // Check the cost min 2 * x0 + x2
+  EXPECT_EQ(C_csdp.nblocks, 2);
+  CompareBlockrec(C_csdp.blocks[1], csdp::MATRIX, 2, {-2, 0, 0, 0}, 0);
+  CompareBlockrec(C_csdp.blocks[2], csdp::MATRIX, 2, {0, -0.5, -0.5, 0}, 0);
+
+  // Check the equality constraint from
+  // [x0 x1] is psd
+  // [x1 x0]
+  struct csdp::sparseblock* blockptr = constraints[1].blocks;
+  std::vector<Eigen::Triplet<double>> block_entries;
+  block_entries.emplace_back(0, 0, 1);
+  block_entries.emplace_back(1, 1, -1);
+  CheckSparseblock(*blockptr, block_entries, 1, 2, 1);
+  EXPECT_EQ(blockptr->next, nullptr);
+  EXPECT_EQ(rhs_csdp[1], 0);
+
+  // Check the equality constraint from
+  // [x0 x2] is psd
+  // [x2 x0]
+  // The equality constraint X(0, 0) - X(2, 2) = 0
+  blockptr = constraints[2].blocks;
+  block_entries.clear();
+  block_entries.emplace_back(0, 0, 1);
+  CheckSparseblock(*blockptr, block_entries, 1, 2, 2);
+  blockptr = blockptr->next;
+  block_entries.clear();
+  block_entries.emplace_back(0, 0, -1);
+  CheckSparseblock(*blockptr, block_entries, 2, 2, 2);
+  EXPECT_EQ(blockptr->next, nullptr);
+  EXPECT_EQ(rhs_csdp[2], 0);
+
+  // The equality constraint X(0, 0) - X(3, 3) = 0
+  blockptr = constraints[3].blocks;
+  block_entries.clear();
+  block_entries.emplace_back(0, 0, 1);
+  CheckSparseblock(*blockptr, block_entries, 1, 2, 3);
+  blockptr = blockptr->next;
+  block_entries.clear();
+  block_entries.emplace_back(1, 1, -1);
+  CheckSparseblock(*blockptr, block_entries, 2, 2, 3);
+  EXPECT_EQ(blockptr->next, nullptr);
+  EXPECT_EQ(rhs_csdp[3], 0);
+
+  // The equality constraint x1 = 1
+  blockptr = constraints[4].blocks;
+  block_entries.clear();
+  block_entries.emplace_back(0, 1, 0.5);
+  CheckSparseblock(*blockptr, block_entries, 1, 2, 4);
+  EXPECT_EQ(blockptr->next, nullptr);
+  EXPECT_EQ(rhs_csdp[4], 1);
+
+  FreeCsdpProblemData(4, C_csdp, rhs_csdp, constraints);
+}
+
+TEST_F(CsdpDocExample, GenerateCsdpProblemDataWithoutFreeVariables) {
+  const SdpaFreeFormat dut(*prog_);
+  struct csdp::blockmatrix C_csdp;
+  double* rhs_csdp;
+  struct csdp::constraintmatrix* constraints{nullptr};
+  GenerateCsdpProblemDataWithoutFreeVariables(dut, &C_csdp, &rhs_csdp,
+                                              &constraints);
+
+  // Check the cost.
+  EXPECT_EQ(C_csdp.nblocks, 3);
+  CompareBlockrec(C_csdp.blocks[1], csdp::MATRIX, 2, {2, 1, 1, 2}, 0);
+  CompareBlockrec(C_csdp.blocks[2], csdp::MATRIX, 3,
+                  {3, 0, 1, 0, 2, 0, 1, 0, 3}, 0);
+  CompareBlockrec(C_csdp.blocks[3], csdp::DIAG, 2, {0, 0}, 0);
+
+  // Check constraints.
+  // Constraint 1.
+  struct csdp::sparseblock* blockptr = constraints[1].blocks;
+  std::vector<Eigen::Triplet<double>> block_entries;
+  block_entries.emplace_back(0, 0, 3);
+  block_entries.emplace_back(0, 1, 1);
+  block_entries.emplace_back(1, 1, 3);
+  CheckSparseblock(*blockptr, block_entries, 1, 2, 1);
+  blockptr = blockptr->next;
+  block_entries.clear();
+  block_entries.emplace_back(0, 0, 1);
+  CheckSparseblock(*blockptr, block_entries, 3, 2, 1);
+  EXPECT_EQ(blockptr->next, nullptr);
+  EXPECT_EQ(rhs_csdp[1], 1);
+
+  // Constraint 2.
+  blockptr = constraints[2].blocks;
+  block_entries.clear();
+  block_entries.emplace_back(0, 0, 3);
+  block_entries.emplace_back(1, 1, 4);
+  block_entries.emplace_back(0, 2, 1);
+  block_entries.emplace_back(2, 2, 5);
+  CheckSparseblock(*blockptr, block_entries, 2, 3, 2);
+  blockptr = blockptr->next;
+  block_entries.clear();
+  CheckSparseblock(*blockptr, block_entries, 3, 2, 2);
+  EXPECT_EQ(blockptr->next, nullptr);
+  block_entries.emplace_back(1, 1, 1);
+  EXPECT_EQ(rhs_csdp[2], 2);
+
+  FreeCsdpProblemData(2, C_csdp, rhs_csdp, constraints);
+}
+
+TEST_F(TrivialSDP1, GenerateCsdpProblemDataWithoutFreeVariables) {
+  const SdpaFreeFormat dut(*prog_);
+  struct csdp::blockmatrix C_csdp;
+  double* rhs_csdp;
+  struct csdp::constraintmatrix* constraints{nullptr};
+  GenerateCsdpProblemDataWithoutFreeVariables(dut, &C_csdp, &rhs_csdp,
+                                              &constraints);
+  /**
+   * A trivial SDP
+   * max X1(0, 1) + X1(1, 2)
+   * s.t X1 ∈ ℝ³ˣ³ is psd
+   *     X1(0, 0) + X1(1, 1) + X1(2, 2) = 1
+   *     X1(0, 1) + X1(1, 2) - 2 * X1(0, 2) <= 0
+   */
+  // Check the cost
+  EXPECT_EQ(C_csdp.nblocks, 2);
+  CompareBlockrec(C_csdp.blocks[1], csdp::MATRIX, 3,
+                  {0, 0.5, 0, 0.5, 0, 0.5, 0, 0.5, 0}, 0);
+  CompareBlockrec(C_csdp.blocks[2], csdp::DIAG, 1, {0}, 0);
+  // Check the constraint X1(0, 0) + X1(1, 1) + X1(2, 2) = 1
+  struct csdp::sparseblock* blockptr = constraints[1].blocks;
+  std::vector<Eigen::Triplet<double>> block_entries;
+  block_entries.emplace_back(0, 0, 1);
+  block_entries.emplace_back(1, 1, 1);
+  block_entries.emplace_back(2, 2, 1);
+  CheckSparseblock(*blockptr, block_entries, 1, 3, 1);
+  EXPECT_EQ(blockptr->next, nullptr);
+  EXPECT_EQ(rhs_csdp[1], 1);
+
+  // Check the constraint X1(0, 1) + X1(1, 2) - 2 * X1(0, 2) <= 0
+  blockptr = constraints[2].blocks;
+  block_entries.clear();
+  block_entries.emplace_back(0, 1, 0.5);
+  block_entries.emplace_back(0, 2, -1);
+  block_entries.emplace_back(1, 2, 0.5);
+  CheckSparseblock(*blockptr, block_entries, 1, 3, 2);
+  blockptr = blockptr->next;
+  block_entries.clear();
+  block_entries.emplace_back(0, 0, 1);
+  CheckSparseblock(*blockptr, block_entries, 2, 1, 2);
+  EXPECT_EQ(blockptr->next, nullptr);
+  EXPECT_EQ(rhs_csdp[2], 0);
+
+  FreeCsdpProblemData(2, C_csdp, rhs_csdp, constraints);
+}
+
+TEST_F(SDPwithOverlappingVariables, Solve) {
+  const SdpaFreeFormat dut(*prog_);
+  csdp::blockmatrix C;
+  double* rhs;
+  csdp::constraintmatrix* constraints{nullptr};
+  GenerateCsdpProblemDataWithoutFreeVariables(dut, &C, &rhs, &constraints);
+
+  struct csdp::blockmatrix X, Z;
+  double* y;
+  csdp::initsoln(dut.num_X_rows(), dut.g().rows(), C, rhs, constraints, &X, &y,
+                 &Z);
+  double pobj, dobj;
+  const int ret =
+      csdp::easy_sdp(dut.num_X_rows(), dut.g().rows(), C, rhs, constraints,
+                     -dut.constant_min_cost_term(), &X, &y, &Z, &pobj, &dobj);
+  EXPECT_EQ(ret, 0 /* 0 is for success */);
+  const double tol = 1E-7;
+  EXPECT_NEAR(pobj, -1, tol);
+  EXPECT_NEAR(dobj, -1, tol);
+  // Check the value of X
+  EXPECT_EQ(X.nblocks, 2);
+  CompareBlockrec(X.blocks[1], csdp::MATRIX, 2, {1, 1, 1, 1}, tol);
+  CompareBlockrec(X.blocks[2], csdp::MATRIX, 2, {1, -1, -1, 1}, tol);
+
+  csdp::free_prob(dut.num_X_rows(), dut.g().rows(), C, rhs, constraints, X, y,
+                  Z);
+}
+
+TEST_F(CsdpDocExample, Solve) {
+  const SdpaFreeFormat dut(*prog_);
+  csdp::blockmatrix C;
+  double* rhs;
+  csdp::constraintmatrix* constraints{nullptr};
+  GenerateCsdpProblemDataWithoutFreeVariables(dut, &C, &rhs, &constraints);
+
+  struct csdp::blockmatrix X, Z;
+  double* y;
+  csdp::initsoln(dut.num_X_rows(), dut.g().rows(), C, rhs, constraints, &X, &y,
+                 &Z);
+  double pobj, dobj;
+  const int ret =
+      csdp::easy_sdp(dut.num_X_rows(), dut.g().rows(), C, rhs, constraints,
+                     -dut.constant_min_cost_term(), &X, &y, &Z, &pobj, &dobj);
+  EXPECT_EQ(ret, 0 /* 0 is for success */);
+
+  const double tol = 5E-7;
+  EXPECT_NEAR(pobj, 2.75, tol);
+  EXPECT_NEAR(y[1], 0.75, tol);
+  EXPECT_NEAR(y[2], 1, tol);
+
+  EXPECT_EQ(X.nblocks, 3);
+  CompareBlockrec(X.blocks[1], csdp::MATRIX, 2, {0.125, 0.125, 0.125, 0.125},
+                  tol);
+  CompareBlockrec(X.blocks[2], csdp::MATRIX, 3,
+                  {2.0 / 3, 0, 0, 0, 0, 0, 0, 0, 0}, tol);
+  CompareBlockrec(X.blocks[3], csdp::DIAG, 2, {0, 0}, tol);
+
+  EXPECT_EQ(Z.nblocks, 3);
+  CompareBlockrec(Z.blocks[1], csdp::MATRIX, 2, {0.25, -0.25, -0.25, 0.25},
+                  tol);
+  CompareBlockrec(Z.blocks[2], csdp::MATRIX, 3, {0, 0, 0, 0, 2, 0, 0, 0, 2},
+                  tol);
+  CompareBlockrec(Z.blocks[3], csdp::DIAG, 2, {0.75, 1}, tol);
+
+  csdp::free_prob(dut.num_X_rows(), dut.g().rows(), C, rhs, constraints, X, y,
+                  Z);
+}
+
+TEST_F(TrivialSDP1, Solve) {
+  const SdpaFreeFormat dut(*prog_);
+  csdp::blockmatrix C;
+  double* rhs;
+  csdp::constraintmatrix* constraints{nullptr};
+  GenerateCsdpProblemDataWithoutFreeVariables(dut, &C, &rhs, &constraints);
+
+  struct csdp::blockmatrix X, Z;
+  double* y;
+  csdp::initsoln(dut.num_X_rows(), dut.g().rows(), C, rhs, constraints, &X, &y,
+                 &Z);
+  double pobj, dobj;
+  const int ret =
+      csdp::easy_sdp(dut.num_X_rows(), dut.g().rows(), C, rhs, constraints,
+                     -dut.constant_min_cost_term(), &X, &y, &Z, &pobj, &dobj);
+  EXPECT_EQ(ret, 0 /* 0 is for success */);
+
+  csdp::free_prob(dut.num_X_rows(), dut.g().rows(), C, rhs, constraints, X, y,
+                  Z);
+}
+
+}  // namespace internal
+}  // namespace solvers
+}  // namespace drake
