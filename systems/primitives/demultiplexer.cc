7df91eb1d3a2806afd785bac8f2278cb1a08a407
huihua.zhao <huihua.zhao@tri.global>
2019-06-21 10:43:25 -0700

    To allow the output sizes of the demultiplexer to be specified by a vector
    The original demultiplexer is limited that the output ports have to be
    the same size. This PR tries to expand the functionality to allow output
    ports with arbitrary sizes.

--- systems/primitives/demultiplexer.cc
+++ systems/primitives/demultiplexer.cc
@@ -8,19 +8,24 @@ namespace drake {
 namespace systems {
 
 template <typename T>
-Demultiplexer<T>::Demultiplexer(int size, int output_ports_sizes)
-    : LeafSystem<T>(SystemTypeTag<systems::Demultiplexer>{}) {
-  // The size must be a multiple of output_ports_sizes.
-  DRAKE_DEMAND(size % output_ports_sizes == 0);
-  const int num_output_ports = size / output_ports_sizes;
+Demultiplexer<T>::Demultiplexer(const std::vector<int>& output_ports_sizes)
+    : LeafSystem<T>(SystemTypeTag<systems::Demultiplexer>{}),
+      output_ports_sizes_(output_ports_sizes),
+      output_ports_start_(CalcOutputPortsStart(output_ports_sizes)) {
+  const int size =
+      std::accumulate(output_ports_sizes.begin(), output_ports_sizes.end(), 0);
 
-  // TODO(amcastro-tri): remove the size parameter from the constructor once
-  // #3109 supporting automatic sizes is resolved.
   this->DeclareInputPort(kVectorValued, size);
+
   // TODO(david-german-tri): Provide a way to infer the type.
+  const int num_output_ports = output_ports_sizes.size();
+  DRAKE_THROW_UNLESS(num_output_ports >= 1);
   for (int i = 0; i < num_output_ports; ++i) {
+    const int output_port_size = output_ports_sizes[i];
+    // Require no zero size output port.
+    DRAKE_THROW_UNLESS(output_port_size >= 1);
     this->DeclareVectorOutputPort(
-        BasicVector<T>(output_ports_sizes),
+        BasicVector<T>(output_port_size),
         [this, i](const Context<T>& context, BasicVector<T>* vector) {
           this->CopyToOutput(context, OutputPortIndex(i), vector);
         });
@@ -28,23 +33,26 @@ Demultiplexer<T>::Demultiplexer(int size, int output_ports_sizes)
 }
 
 template <typename T>
+Demultiplexer<T>::Demultiplexer(int size, int output_ports_size)
+    : Demultiplexer(CalcOutputPortsSizes(size, output_ports_size)) {}
+
+template <typename T>
 template <typename U>
 Demultiplexer<T>::Demultiplexer(const Demultiplexer<U>& other)
-    : Demultiplexer<T>(other.get_input_port(0).size(),
-                       other.get_output_port(0).size()) {}
+    : Demultiplexer<T>(other.get_output_ports_sizes()) {}
 
 template <typename T>
 void Demultiplexer<T>::CopyToOutput(const Context<T>& context,
                                     OutputPortIndex port_index,
                                     BasicVector<T>* output) const {
-  // All output ports have the same size as defined in the constructor.
-  const int out_size = this->get_output_port(0).size();
+  const int out_size = this->get_output_port(port_index).size();
+  const int out_start_index = output_ports_start_[port_index];
 
   // TODO(amcastro-tri): the output should simply reference the input port's
   // value to avoid copy.
   auto in_vector = this->get_input_port(0).Eval(context);
   auto out_vector = output->get_mutable_value();
-  out_vector = in_vector.segment(port_index * out_size, out_size);
+  out_vector = in_vector.segment(out_start_index, out_size);
 }
 
 }  // namespace systems
