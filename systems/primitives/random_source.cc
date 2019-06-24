f0fa1eb5fd523d1ced06f3f0105156f449819cc7
Jeremy Nimmer <jeremy.nimmer@tri.global>
2019-06-24 13:49:16 -0400

    primitives: Remove templates from RandomSource APIs (#11670)
    Templates make compilation times long and error messages bad.

--- systems/primitives/random_source.cc
+++ systems/primitives/random_source.cc
@@ -1,21 +1,122 @@
 #include "drake/systems/primitives/random_source.h"
 
+#include <atomic>
+#include <random>
+
 #include "drake/common/never_destroyed.h"
 
 namespace drake {
 namespace systems {
+namespace {
 
-namespace internal {
-template<typename Generator>
-typename Generator::result_type generate_unique_seed() {
-  static never_destroyed<typename Generator::result_type> seed(
-      Generator::default_seed);
+using Seed = RandomSource::Seed;
+
+// Stores exactly one of the three supported distribution objects.  Note that
+// the distribution objects hold computational state; they are not just pure
+// mathematical functions.
+using DistributionVariant = variant<
+    std::uniform_real_distribution<double>,
+    std::normal_distribution<double>,
+    std::exponential_distribution<double>>;
+
+// Creates a distribution object from the distribution enumeration.
+DistributionVariant MakeDistributionVariant(RandomDistribution which) {
+  switch (which) {
+    case RandomDistribution::kUniform:
+      return std::uniform_real_distribution<double>();
+    case RandomDistribution::kGaussian:
+      return std::normal_distribution<double>();
+    case RandomDistribution::kExponential:
+      return std::exponential_distribution<double>();
+  }
+  DRAKE_UNREACHABLE();
+}
+
+// Generates real-valued (i.e., `double`) samples from some distribution.  This
+// serves as the abstract state of a RandomSource, which encompasses all of the
+// source's state *except* for the currently-sampled output values which are
+// stored as discrete state.
+class SampleGenerator {
+ public:
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(SampleGenerator)
+
+  SampleGenerator() = default;
+  SampleGenerator(Seed seed, RandomDistribution which)
+      : generator_(seed), distribution_(MakeDistributionVariant(which)) {}
+
+  double GenerateNext() {
+    switch (distribution_.index()) {
+      case 0: return get<0>(distribution_)(generator_);
+      case 1: return get<1>(distribution_)(generator_);
+      case 2: return get<2>(distribution_)(generator_);
+    }
+    DRAKE_UNREACHABLE();
+  }
+
+ private:
+  RandomGenerator generator_;
+  DistributionVariant distribution_;
+};
+
+// Returns a monotonically increasing integer on each call.
+Seed get_next_seed() {
+  static never_destroyed<std::atomic<Seed>> seed(
+      RandomGenerator::default_seed);
   return seed.access()++;
 }
 
-template RandomGenerator::result_type generate_unique_seed<RandomGenerator>();
+}  // namespace
 
-}  // namespace internal
+RandomSource::RandomSource(
+    RandomDistribution distribution, int num_outputs,
+    double sampling_interval_sec)
+    : distribution_(distribution), seed_(get_next_seed()) {
+  this->DeclareDiscreteState(num_outputs);
+  this->DeclareAbstractState(Value<SampleGenerator>().Clone());
+  this->DeclarePeriodicUnrestrictedUpdateEvent(
+      sampling_interval_sec, 0., &RandomSource::UpdateSamples);
+  this->DeclareVectorOutputPort(
+      "output", BasicVector<double>(num_outputs),
+      [](const Context<double>& context, BasicVector<double>* output) {
+        const auto& values = context.get_discrete_state(0);
+        output->SetFrom(values);
+      });
+}
+
+RandomSource::~RandomSource() {}
+
+void RandomSource::SetDefaultState(
+    const Context<double>& context, State<double>* state) const {
+  SetSeed(seed_, context, state);
+}
+
+void RandomSource::SetRandomState(
+    const Context<double>& context, State<double>* state,
+    RandomGenerator* seed_generator) const {
+  const Seed fresh_seed = (*seed_generator)();
+  SetSeed(fresh_seed, context, state);
+}
+
+// Writes the given seed into abstract state (replacing the existing
+// SampleGenerator) and then does `UpdateSamples`.
+void RandomSource::SetSeed(
+    Seed seed, const Context<double>& context, State<double>* state) const {
+  state->template get_mutable_abstract_state<SampleGenerator>(0) =
+      SampleGenerator(seed, distribution_);
+  UpdateSamples(context, state);
+}
+
+// Samples random values into the discrete state, using the SampleGenerator
+// from the abstract state.  (Note that the generator's abstract state is also
+// mutated as a side effect of this method.)
+void RandomSource::UpdateSamples(
+    const Context<double>&, State<double>* state) const {
+  auto& source = state->template get_mutable_abstract_state<SampleGenerator>(0);
+  auto& samples = state->get_mutable_discrete_state(0);
+  for (int i = 0; i < samples.size(); ++i) {
+    samples[i] = source.GenerateNext();
+  }
+}
 
 int AddRandomInputs(double sampling_interval_sec,
                     DiagramBuilder<double>* builder) {
@@ -24,14 +125,13 @@ int AddRandomInputs(double sampling_interval_sec,
   // there is (currently) no builder->GetSystems() method.
   for (const auto* system : builder->GetMutableSystems()) {
     for (int i = 0; i < system->num_input_ports(); i++) {
-      const systems::InputPort<double>& port =
-          system->get_input_port(i);
+      const systems::InputPort<double>& port = system->get_input_port(i);
       // Check for the random label.
       if (!port.is_random()) {
         continue;
       }
 
-      typedef typename Diagram<double>::InputPortLocator InputPortLocator;
+      using InputPortLocator = Diagram<double>::InputPortLocator;
       // Check if the input is already wired up.
       InputPortLocator id{&port.get_system(), port.get_index()};
       if (builder->connection_map_.count(id) > 0 ||
@@ -39,28 +139,10 @@ int AddRandomInputs(double sampling_interval_sec,
         continue;
       }
 
-      count++;
-      switch (port.get_random_type().value()) {
-        case RandomDistribution::kUniform: {
-          const auto* uniform = builder->AddSystem<UniformRandomSource>(
-              port.size(), sampling_interval_sec);
-          builder->Connect(uniform->get_output_port(0), port);
-          continue;
-        }
-        case RandomDistribution::kGaussian: {
-          const auto* gaussian = builder->AddSystem<GaussianRandomSource>(
-              port.size(), sampling_interval_sec);
-          builder->Connect(gaussian->get_output_port(0), port);
-          continue;
-        }
-        case RandomDistribution::kExponential: {
-          const auto* exponential = builder->AddSystem<ExponentialRandomSource>(
-              port.size(), sampling_interval_sec);
-          builder->Connect(exponential->get_output_port(0), port);
-          continue;
-        }
-      }
-      DRAKE_UNREACHABLE();
+      const auto* const source = builder->AddSystem<RandomSource>(
+          port.get_random_type().value(), port.size(), sampling_interval_sec);
+      builder->Connect(source->get_output_port(0), port);
+      ++count;
     }
   }
   return count;
@@ -68,4 +150,3 @@ int AddRandomInputs(double sampling_interval_sec,
 
 }  // namespace systems
 }  // namespace drake
-
