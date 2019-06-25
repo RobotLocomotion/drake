c5a373abfad05e2cac0de76b797ce5c3fa8a4750
Alejandro Castro <alejandro.castro@tri.global>
2019-06-25 12:24:41 -0400

    Introduces broad-phase API. (#11702)

--- geometry/query_object.cc
+++ geometry/query_object.cc
@@ -82,6 +82,16 @@ QueryObject<T>::ComputePointPairPenetration() const {
 }
 
 template <typename T>
+std::vector<SortedPair<GeometryId>> QueryObject<T>::FindCollisionCandidates()
+    const {
+  ThrowIfNotCallable();
+  // TODO(amcastro-tri): Modify this when the cache system is in place.
+  FullPoseUpdate();
+  const GeometryState<T>& state = geometry_state();
+  return state.FindCollisionCandidates();
+}
+
+template <typename T>
 std::vector<ContactSurface<T>>
 QueryObject<T>::ComputeContactSurfaces() const {
   ThrowIfNotCallable();
