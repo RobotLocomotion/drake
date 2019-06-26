1d14c9f6e05396eaa5df9d044d8d6094d18354ee
Jamie Snape <jamie.snape@kitware.com>
2019-06-26 12:58:15 -0400

    Ensure sanitizer defect reports include line numbers

--- tools/dynamic_analysis/asan.sh
+++ tools/dynamic_analysis/asan.sh
@@ -5,4 +5,6 @@ export ASAN_OPTIONS="$ASAN_OPTIONS:check_initialization_order=1:detect_stack_use
 # LSan is run with ASan by default, ASAN_OPTIONS can't be used to suppress LSan
 # errors
 export LSAN_OPTIONS="$LSAN_OPTIONS:suppressions=$mydir/lsan.supp"
+# Ensure executable named llvm-symbolizer is on the PATH.
+export PATH="$PATH:/usr/lib/llvm-6.0/bin:/usr/local/opt/llvm/bin"
 "$@"
