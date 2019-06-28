dc6da7df227eb3d85ace3b0020d92f99c311ec01
Jeremy Nimmer <jeremy.nimmer@tri.global>
2019-06-28 19:56:51 -0400

    primitives: Add get/set seed support to RandomSource (#11676)

--- systems/primitives/random_source.h
+++ systems/primitives/random_source.h
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "drake/common/drake_copyable.h"
+#include "drake/common/drake_optional.h"
 #include "drake/common/random.h"
 #include "drake/systems/framework/diagram_builder.h"
 #include "drake/systems/framework/leaf_system.h"
@@ -17,6 +18,38 @@ namespace systems {
 /// Ts is the sampling interval.
 ///
 /// @system{RandomSource,,@output_port{output}}
+///
+/// This system exposes a parameter named `seed` for the pseudo-random number
+/// generator that determines the noise output.  The `seed` parameter behaves
+/// as follows:
+///
+/// 1. Each newly-created RandomSource chooses a new `per_instance_seed` member
+/// field value for itself.  The value will be unique within the current
+/// executable process.
+///
+/// 2. By default, `source.CreateDefaultContext()` will set the returned
+/// context's `seed` parameter to `per_instance_seed`.  Therefore, for a given
+/// instance of this system, the parameters, state, and outputs will be
+/// identical for all simulations that start from a default context.
+///
+/// 3. By default, `source.SetRandomContext()` will choose a new, arbitrary
+/// value for the context's `seed` parameter, which means that the system's
+/// parameters, state, and outputs will (almost certainly) differ from their
+/// defaults.
+///
+/// 4. The user may call `source.set_fixed_seed(new_seed)` on this system.
+/// When a `new_seed` value is provided, it is used by both
+/// `CreateDefaultContext` and `SetRandomContext`.  Therefore, the system's
+/// parameters, state, and outputs will be identical to any other instances
+/// that share the same `new_seed` value for their `seed` context parameter.
+/// Note that `set_fixed_seed` affects subsequently-created contexts; any
+/// pre-existing contexts are unaffected.  The user may call
+/// `source.set_fixed_seed(nullopt)` to revert to the default the behaviors
+/// described in #2 and #3 again.
+///
+/// 5. The context returned by `source.AllocateContext()` does not contain a
+/// valid `seed` parameter; that context should not be used until its values
+/// are populated via another Context.
 ///
 /// @note This system is only defined for the double scalar type.
 ///
@@ -48,6 +81,20 @@ class RandomSource : public LeafSystem<double> {
 
   ~RandomSource() override;
 
+  /// Returns the `distribution` given at construction.
+  RandomDistribution get_distribution() const { return distribution_; }
+
+  /// Returns the value of the `seed` parameter in the given context.
+  Seed get_seed(const Context<double>& context) const;
+
+  /// Gets this system's fixed random seed (or else nullopt when the seed is
+  /// not fixed).  Refer to the class overview documentation for details.
+  optional<Seed> get_fixed_seed() const { return fixed_seed_; }
+
+  /// Sets (or clears) this system's fixed random seed.  Refer to the class
+  /// overview documentation for details.
+  void set_fixed_seed(const optional<Seed>& seed) { fixed_seed_ = seed; }
+
  private:
   void SetDefaultState(const Context<double>&, State<double>*) const final;
   void SetRandomState(const Context<double>&, State<double>*,
@@ -56,7 +103,8 @@ class RandomSource : public LeafSystem<double> {
   void UpdateSamples(const Context<double>&, State<double>*) const;
 
   const RandomDistribution distribution_;
-  const Seed seed_;
+  const Seed instance_seed_;
+  optional<Seed> fixed_seed_;
 };
 
 /// For each subsystem input port in @p builder that is (a) not yet connected
--- systems/primitives/test/random_source_test.cc
+++ systems/primitives/test/random_source_test.cc
@@ -76,6 +76,8 @@ void CheckStatistics(
 GTEST_TEST(RandomSourceTest, UniformWhiteNoise) {
   auto random_source = std::make_unique<RandomSource>(
       RandomDistribution::kUniform, 2, 0.0025);
+  EXPECT_EQ(random_source->get_distribution(), RandomDistribution::kUniform);
+  EXPECT_EQ(random_source->get_fixed_seed(), nullopt);
 
   // Cumulative distribution function of the uniform distribution.
   auto Phi = [](double z) { return z; };
@@ -91,6 +93,8 @@ GTEST_TEST(RandomSourceTest, UniformWhiteNoise) {
 GTEST_TEST(RandomSourceTest, GaussianWhiteNoise) {
   auto random_source = std::make_unique<RandomSource>(
       RandomDistribution::kGaussian, 2, 0.0025);
+  EXPECT_EQ(random_source->get_distribution(), RandomDistribution::kGaussian);
+  EXPECT_EQ(random_source->get_fixed_seed(), nullopt);
 
   // Cumulative distribution function of the standard normal distribution.
   auto Phi = [](double z) { return 0.5 * std::erfc(-z / std::sqrt(2.0)); };
@@ -106,6 +110,9 @@ GTEST_TEST(RandomSourceTest, GaussianWhiteNoise) {
 GTEST_TEST(RandomSourceTest, ExponentialWhiteNoise) {
   auto random_source = std::make_unique<RandomSource>(
       RandomDistribution::kExponential, 2, 0.0025);
+  EXPECT_EQ(random_source->get_distribution(),
+            RandomDistribution::kExponential);
+  EXPECT_EQ(random_source->get_fixed_seed(), nullopt);
 
   // Cumulative distribution function of the exponential distribution with λ=1,
   // (note: only valid for z>=0).
@@ -230,6 +237,91 @@ GTEST_TEST(RandomSourceTest, CorrelationTest) {
     // using the same seed will lead to numbers ≊ 1.
     EXPECT_LE(xcorr / N, 0.1);
   }
+}
+
+// Check the invariants of the seed lifecycle.
+GTEST_TEST(RandomSourceTest, SeedTest) {
+  RandomSource dut(RandomDistribution::kUniform, 1, 0.0025);
+  const auto& port = dut.get_output_port(0);
+
+  // The source does not have a fixed seed by default.
+  EXPECT_EQ(dut.get_fixed_seed(), nullopt);
+
+  // A given instance of a RandomSource always defaults to the same seed.
+  using Seed = RandomSource::Seed;
+  const Seed default_seed = dut.get_seed(*dut.CreateDefaultContext());
+  EXPECT_EQ(dut.get_seed(*dut.CreateDefaultContext()), default_seed);
+  EXPECT_EQ(dut.get_seed(*dut.CreateDefaultContext()), default_seed);
+  EXPECT_EQ(dut.get_seed(*dut.CreateDefaultContext()), default_seed);
+
+  // A given instance of a RandomSource always defaults to the same output.
+  const double default_output = port.Eval(*dut.CreateDefaultContext())[0];
+  EXPECT_EQ(port.Eval(*dut.CreateDefaultContext())[0], default_output);
+  EXPECT_EQ(port.Eval(*dut.CreateDefaultContext())[0], default_output);
+  EXPECT_EQ(port.Eval(*dut.CreateDefaultContext())[0], default_output);
+
+  // When asked to create a random seed, a fresh seed and output appears.  (The
+  // checks below have a small chance of ending up spuriously false.)
+  struct SeedAndOutput { Seed seed{}; double output{}; };
+  RandomGenerator new_seed_generator;
+  auto make_new_seed_and_output = [&dut, &new_seed_generator]() {
+    auto random_context = dut.CreateDefaultContext();
+    dut.SetRandomContext(random_context.get(), &new_seed_generator);
+    return SeedAndOutput{
+        dut.get_seed(*random_context),
+        dut.get_output_port(0).Eval(*random_context)[0]};
+  };
+  const auto new1 = make_new_seed_and_output();
+  const auto new2 = make_new_seed_and_output();
+  EXPECT_NE(new1.seed, default_seed);
+  EXPECT_NE(new2.seed, default_seed);
+  EXPECT_NE(new1.seed, new2.seed);
+  EXPECT_NE(new1.output, default_output);
+  EXPECT_NE(new2.output, default_output);
+  EXPECT_NE(new1.output, new2.output);
+
+  // We can re-create those same identical random seeds and values again,
+  // starting from the same (original) generator.
+  new_seed_generator = {};
+  const auto new1b = make_new_seed_and_output();
+  const auto new2b = make_new_seed_and_output();
+  EXPECT_EQ(new1b.seed, new1.seed);
+  EXPECT_EQ(new2b.seed, new2.seed);
+  EXPECT_EQ(new1b.output, new1.output);
+  EXPECT_EQ(new2b.output, new2.output);
+
+  // Now let's spin up a different RandomSource.
+  RandomSource other(dut.get_distribution(), 1, 0.0025);
+  const auto& other_port = other.get_output_port(0);
+
+  // The output defaults to something fresh.
+  const double other_default_output =
+      other_port.Eval(*other.CreateDefaultContext())[0];
+  EXPECT_NE(other_default_output, default_output);
+
+  // Even on a different RandomSource, we can re-create the identical values of
+  // the original dut if we start from identical constructor arguments the same
+  // fixed seed.
+  other.set_fixed_seed(new1.seed);
+  EXPECT_EQ(other.get_fixed_seed().value_or(0), new1.seed);
+  auto other_context = other.CreateDefaultContext();
+  EXPECT_EQ(other.get_seed(*other_context), new1.seed);
+  EXPECT_EQ(other_port.Eval(*other_context)[0], new1.output);
+
+  // Both the default context and the random context use the fixed seed.
+  RandomGenerator ignored_seed_generator;
+  other.SetRandomContext(other_context.get(), &ignored_seed_generator);
+  EXPECT_EQ(other.get_seed(*other_context), new1.seed);
+  EXPECT_EQ(other_port.Eval(*other_context)[0], new1.output);
+  other.SetRandomContext(other_context.get(), &ignored_seed_generator);
+  EXPECT_EQ(other.get_seed(*other_context), new1.seed);
+  EXPECT_EQ(other_port.Eval(*other_context)[0], new1.output);
+
+  // If we remove the fixed seed, it returns to normal.
+  other.set_fixed_seed(nullopt);
+  EXPECT_EQ(other.get_fixed_seed(), nullopt);
+  EXPECT_EQ(other_port.Eval(*other.CreateDefaultContext())[0],
+            other_default_output);
 }
 
 // Make sure that calling SetRandomContext changes the output, and
