affa36f9074061e02d4a8a447f81889bcd4a0bed
hongkai-dai <daihongkai@gmail.com>
2019-07-08 15:30:28 -0700

    Address Jeremy's comments.

--- solvers/evaluator_base.cc
+++ solvers/evaluator_base.cc
@@ -15,25 +15,23 @@ namespace {
 void CheckGradientSparsityPattern(
     const std::vector<std::pair<int, int>>& gradient_sparsity_pattern, int rows,
     int cols) {
-  if (kDrakeAssertIsArmed) {
-    std::set<std::pair<int, int>> nonzero_entries;
-    for (const auto& nonzero_entry : gradient_sparsity_pattern) {
-      if (nonzero_entry.first < 0 || nonzero_entry.first >= rows) {
-        throw std::invalid_argument(
-            "Constraint::SetSparsityPattern(): row index out of range.");
-      }
-      if (nonzero_entry.second < 0 || nonzero_entry.second >= cols) {
-        throw std::invalid_argument(
-            "Constraint::SetSparsityPattern(): column index out of range.");
-      }
-      auto it = nonzero_entries.find(nonzero_entry);
-      if (it != nonzero_entries.end()) {
-        throw std::invalid_argument(
-            "Constraint::SetSparsityPatten(): was given entries with repeated "
-            "values.");
-      }
-      nonzero_entries.insert(it, nonzero_entry);
+  std::set<std::pair<int, int>> nonzero_entries;
+  for (const auto& nonzero_entry : gradient_sparsity_pattern) {
+    if (nonzero_entry.first < 0 || nonzero_entry.first >= rows) {
+      throw std::invalid_argument(
+          "Constraint::SetSparsityPattern(): row index out of range.");
     }
+    if (nonzero_entry.second < 0 || nonzero_entry.second >= cols) {
+      throw std::invalid_argument(
+          "Constraint::SetSparsityPattern(): column index out of range.");
+    }
+    auto it = nonzero_entries.find(nonzero_entry);
+    if (it != nonzero_entries.end()) {
+      throw std::invalid_argument(
+          "Constraint::SetSparsityPatten(): was given entries with repeated "
+          "values.");
+    }
+    nonzero_entries.insert(it, nonzero_entry);
   }
 }
 }  // namespace
