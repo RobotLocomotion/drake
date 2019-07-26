d110c10d2d9320cb517586df6f70f43657003b3d
Sean Curtis <sean.curtis@tri.global>
2019-07-26 12:34:59 -0700

    Add utility for mapping Shape type to shape *name*
    This is different from ShapeToString in that it doesn't provide any of the
    properties; just the type name. The purpose is to be able to blindly print
    the name of a shape in a context in which I wouldn't typically have at
    compile time.

--- geometry/proximity/test/proximity_utilities_test.cc
+++ geometry/proximity/test/proximity_utilities_test.cc
@@ -1,6 +1,7 @@
 #include "drake/geometry/proximity/proximity_utilities.h"
 
 #include <memory>
+#include <sstream>
 
 #include <fcl/fcl.h>
 #include <gtest/gtest.h>
@@ -58,6 +59,47 @@ GTEST_TEST(EncodedData, ConstructionFromFclObject) {
     EXPECT_FALSE(data_read.is_dynamic());
     EXPECT_EQ(data_read.id(), data_A.id());
   }
+}
+
+GTEST_TEST(ShapeName, SimpleReification) {
+  ShapeName name;
+
+  EXPECT_EQ(name.string(), "");
+
+  Sphere(0.5).Reify(&name);
+  EXPECT_EQ(name.string(), "Sphere");
+
+  Cylinder(1, 2).Reify(&name);
+  EXPECT_EQ(name.string(), "Cylinder");
+
+  Box(1, 2, 3).Reify(&name);
+  EXPECT_EQ(name.string(), "Box");
+
+  HalfSpace().Reify(&name);
+  EXPECT_EQ(name.string(), "HalfSpace");
+
+  Mesh("filepath", 1.0).Reify(&name);
+  EXPECT_EQ(name.string(), "Mesh");
+
+  Convex("filepath", 1.0).Reify(&name);
+  EXPECT_EQ(name.string(), "Convex");
+}
+
+GTEST_TEST(ShapeName, ReifyOnConstruction) {
+  EXPECT_EQ(ShapeName(Sphere(0.5)).string(), "Sphere");
+  EXPECT_EQ(ShapeName(Cylinder(1, 2)).string(), "Cylinder");
+  EXPECT_EQ(ShapeName(Box(1, 2, 3)).string(), "Box");
+  EXPECT_EQ(ShapeName(HalfSpace()).string(), "HalfSpace");
+  EXPECT_EQ(ShapeName(Mesh("filepath", 1.0)).string(), "Mesh");
+  EXPECT_EQ(ShapeName(Convex("filepath", 1.0)).string(), "Convex");
+}
+
+GTEST_TEST(ShapeName, Stremaing) {
+  ShapeName name(Sphere(0.5));
+  std::stringstream ss;
+  ss << name;
+  EXPECT_EQ(name.string(), "Sphere");
+  EXPECT_EQ(ss.str(), name.string());
 }
 
 }  // namespace
