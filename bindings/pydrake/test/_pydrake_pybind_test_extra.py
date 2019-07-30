6a895a0ed01e9a022bd9f96659f23a23f6838b19
Eric Cousineau <eric.cousineau@tri.global>
2019-07-30 10:47:11 -0400

    pydrake: Add `ParamInit` and add a focused test for `pydrake_pybind`

---
+++ bindings/pydrake/test/_pydrake_pybind_test_extra.py
@@ -0,0 +1,8 @@
+import copy
+
+
+def check_copy(copy_function, obj):
+    """Checks `copy_function` to ensure `obj` is equal to its copy, and that
+    it is not the same instance."""
+    obj_copy = copy_function(obj)
+    return obj == obj_copy and obj is not obj_copy
