6baf94d992b5f0ea3818630624728a85e62d8872
Rick Cory <rick.cory@tri.global>
2019-08-01 13:54:10 -0700

    Addressing platform review comments.

--- examples/planar_gripper/planar_gripper_common.cc
+++ examples/planar_gripper/planar_gripper_common.cc
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <map>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "drake/common/drake_assert.h"
@@ -15,67 +16,101 @@ namespace drake {
 namespace examples {
 namespace planar_gripper {
 
-using drake::math::RigidTransform;
-using drake::math::RollPitchYaw;
+using drake::math::RigidTransformd;
+using drake::math::RollPitchYawd;
 using drake::multibody::MultibodyPlant;
+using Eigen::Vector3d;
 
 template <typename T>
 void WeldGripperFrames(MultibodyPlant<T>* plant) {
-  const double outer_radius = 0.19;
-  const double f1_angle = M_PI / 3;
-  const math::RigidTransformd XT(math::RollPitchYaw<double>(0, 0, 0),
-                                 Eigen::Vector3d(0, 0, outer_radius));
+  // The finger base links are all welded a fixed distance from the world
+  // origin, on the Y-Z plane.
+  const double kOriginToBaseDistance = 0.19;
 
-  // Weld the first finger.
-  math::RigidTransformd X_PC1(math::RollPitchYaw<double>(f1_angle, 0, 0),
-                              Eigen::Vector3d::Zero());
-  X_PC1 = X_PC1 * XT;
+  // Weld the first finger. Finger base links are arranged equidistant along the
+  // perimeter of a circle. The first finger is welded 60 degrees from the
+  // +Z-axis. Frames F1, F2, F3 correspond to the base link finger frames.
+  RigidTransformd X_WF1(RollPitchYawd(M_PI / 3, 0, 0), Vector3d::Zero());
+  X_WF1 = X_WF1 * RigidTransformd(Vector3d(0, 0, kOriginToBaseDistance));
   const multibody::Frame<T>& finger1_base_frame =
       plant->GetFrameByName("finger1_base");
-  plant->WeldFrames(plant->world_frame(), finger1_base_frame, X_PC1);
+  plant->WeldFrames(plant->world_frame(), finger1_base_frame, X_WF1);
 
-  // Weld the second finger.
-  const math::RigidTransformd X_PC2 =
-      math::RigidTransformd(math::RollPitchYawd(M_PI / 3 * 2, 0, 0),
-                            Eigen::Vector3d::Zero()) *
-      X_PC1;
+  // Weld the second finger. The second finger is 120 degrees from the first
+  // finger (i.e., the arc from finger 1's base to finger 2's base is 120
+  // degrees).
+  const RigidTransformd X_WF2 =
+      RigidTransformd(RollPitchYawd(2 * M_PI / 3, 0, 0),
+                      Vector3d::Zero()) *
+      X_WF1;
   const multibody::Frame<T>& finger2_base_frame =
       plant->GetFrameByName("finger2_base");
-  plant->WeldFrames(plant->world_frame(), finger2_base_frame, X_PC2);
+  plant->WeldFrames(plant->world_frame(), finger2_base_frame, X_WF2);
 
-  // Weld the 3rd finger.
-  const math::RigidTransformd X_PC3 =
-      math::RigidTransformd(math::RollPitchYawd(M_PI / 3 * 2, 0, 0),
-                            Eigen::Vector3d::Zero()) *
-      X_PC2;
+  // Weld the 3rd finger. The third finger is 120 degrees from the second
+  // finger.
+  const RigidTransformd X_WF3 =
+      RigidTransformd(RollPitchYawd(2 * M_PI / 3, 0, 0),
+                      Vector3d::Zero()) *
+      X_WF2;
   const multibody::Frame<T>& finger3_base_frame =
       plant->GetFrameByName("finger3_base");
-  plant->WeldFrames(plant->world_frame(), finger3_base_frame, X_PC3);
+  plant->WeldFrames(plant->world_frame(), finger3_base_frame, X_WF3);
 }
 
 // Explicit instantiations.
 template void WeldGripperFrames(MultibodyPlant<double>* plant);
 
-MatrixX<double> ParseKeyframes(const std::string& name,
-                               EigenPtr<Vector3<double>> brick_initial_pose) {
-  const std::string keyframe_path = FindResourceOrThrow(name);
-  std::fstream fs;
-  fs.open(keyframe_path, std::fstream::in);
-  DRAKE_DEMAND(fs.is_open());
+// Build a keyframe matrix for joints in joint_ordering by extracting the
+// appropriate columns from all_keyframes. The interpretation of columns in
+// joint_keyframes are ordered as in joint_ordering.
+MatrixX<double> MakeKeyframes(
+    MatrixX<double> all_keyframes, std::vector<std::string> joint_ordering,
+    std::vector<std::string> headers) {
+  // First find the columns in the keyframe data for just the joints in
+  // joint_ordering.
+  std::map<std::string, int> joint_name_to_col_index_map;
+  for (const auto& header_name : joint_ordering) {
+    auto match_it = std::find(headers.begin(), headers.end(), header_name);
+    DRAKE_DEMAND(match_it != headers.end());
+    joint_name_to_col_index_map[header_name] =
+        match_it - headers.begin();
+  }
+  // Now create the keyframe matrix.
+  const int kKeyframeCount = all_keyframes.rows();
+  const int kNumFingerJoints = joint_ordering.size();
+  MatrixX<double> joint_keyframes(kKeyframeCount, kNumFingerJoints);
+  for (int i = 0; i < kNumFingerJoints; ++i) {
+    const std::string& joint_name = joint_ordering[i];
+    const int all_keyframe_col_index =
+        joint_name_to_col_index_map[joint_name];
+    joint_keyframes.block(0, i, kKeyframeCount, 1) =
+        all_keyframes.block(0, all_keyframe_col_index, kKeyframeCount, 1);
+  }
+  return joint_keyframes;
+}
 
-  // Count the number of keyframes.
+std::pair<MatrixX<double>, std::map<std::string, int>> ParseKeyframes(
+    const std::string& name,
+    EigenPtr<Vector3<double>> brick_initial_pose) {
+  const std::string keyframe_path = FindResourceOrThrow(name);
+  std::fstream file;
+  file.open(keyframe_path, std::fstream::in);
+  DRAKE_DEMAND(file.is_open());
+
+  // Count the number of lines in the file.
   std::string line;
   int line_count = 0;
-  while (std::getline(fs, line)) {
-    if (line.size() == 0) break;
+  while (!std::getline(file, line).eof()) {
     line_count++;
   }
   const int keyframe_count = line_count - 1;
   drake::log()->info("Found {} keyframes", keyframe_count);
 
   // Get the file headers.
-  fs.seekg(0);
-  std::getline(fs, line);
+  file.clear();
+  file.seekg(0);
+  std::getline(file, line);
   std::stringstream sstream(line);
   std::vector<std::string> headers;
   std::string token;
@@ -83,69 +118,57 @@ MatrixX<double> ParseKeyframes(const std::string& name,
     headers.push_back(token);
   }
 
+  // Make sure we read the correct number of headers.
+  const int kNumHeaders = 9;
+  if (headers.size() != kNumHeaders) {
+    throw std::runtime_error(
+        "Unexpected number of headers found in keyframe input file.");
+  }
+
   // Extract all keyframes (finger and brick)
   MatrixX<double> all_keyframes(keyframe_count, headers.size());
+  int header_count;
   for (int i = 0; i < all_keyframes.rows(); ++i) {
-    for (int j = 0; j < all_keyframes.cols(); ++j) {
-      fs >> all_keyframes(i, j);
+    for (header_count = 0; header_count < all_keyframes.cols();
+         ++header_count) {
+      file >> all_keyframes(i, header_count);
     }
+    DRAKE_DEMAND(header_count == kNumHeaders);
   }
 
-  // Find the columns for just the finger joints.
-  std::vector<std::string> finger_joint_name_ordering = {
+  // Find the columns in the keyframe data for just the finger joints and
+  // create the corresponding keyframe matrix.
+  std::vector<std::string> finger_joint_ordering = {
       "finger1_BaseJoint", "finger2_BaseJoint", "finger3_BaseJoint",
       "finger1_MidJoint",    "finger2_MidJoint",    "finger3_MidJoint"};
-  std::map<std::string, int> finger_joint_name_to_col_index_map;
-  for (auto it = finger_joint_name_ordering.begin();
-       it != finger_joint_name_ordering.end(); it++) {
-    auto name_it = std::find(headers.begin(), headers.end(), *it);
-    DRAKE_DEMAND(name_it != headers.end());
-    finger_joint_name_to_col_index_map[*it] = name_it - headers.begin();
-  }
+  MatrixX<double> finger_joint_keyframes =
+      MakeKeyframes(all_keyframes, finger_joint_ordering, headers);
 
-  // Create the matrix for just the finger joint keyframes.
-  const int kNumFingerJoints = 6;
-  MatrixX<double> finger_joint_keyframes(keyframe_count, kNumFingerJoints);
-  for (auto it = finger_joint_name_ordering.begin();
-       it != finger_joint_name_ordering.end(); ++it) {
-    // Create the indexes.
-    int all_keyframe_col_index = finger_joint_name_to_col_index_map[*it];
-    auto joint_only_col_index = it - finger_joint_name_ordering.begin();
-
-    finger_joint_keyframes.block(0, joint_only_col_index, keyframe_count, 1) =
-        all_keyframes.block(0, all_keyframe_col_index, keyframe_count, 1);
-  }
-
-  // Create a matrix for the just the brick's keyframes;
-  std::vector<std::string> brick_joint_name_ordering = {
+  // Find the columns in the keyframe data for just the brick joints and create
+  // the corresponding keyframe matrix. Note: Only the first keyframe is used to
+  // set the brick's initial position. All other brick keyframe data is unused.
+  std::vector<std::string> brick_joint_ordering = {
       "brick_translate_y_joint", "brick_translate_z_joint",
       "brick_revolute_x_joint"};
   std::map<std::string, int> brick_joint_name_to_col_index_map;
-  for (auto it = brick_joint_name_ordering.begin();
-       it != brick_joint_name_ordering.end(); it++) {
-    auto name_it = std::find(headers.begin(), headers.end(), *it);
-    DRAKE_DEMAND(name_it != headers.end());
-    brick_joint_name_to_col_index_map[*it] = name_it - headers.begin();
-  }
+  MatrixX<double> brick_joint_keyframes =
+      MakeKeyframes(all_keyframes, brick_joint_ordering, headers);
 
-  const int kNumBrickJoints = 3;
-  MatrixX<double> brick_joint_keyframes(keyframe_count, kNumBrickJoints);
-  for (auto it = brick_joint_name_ordering.begin();
-       it != brick_joint_name_ordering.end(); ++it) {
-    // Create the indexes.
-    int all_keyframe_col_index = brick_joint_name_to_col_index_map[*it];
-    auto joint_only_col_index = it - brick_joint_name_ordering.begin();
-
-    brick_joint_keyframes.block(0, joint_only_col_index, keyframe_count, 1) =
-        all_keyframes.block(0, all_keyframe_col_index, keyframe_count, 1);
-  }
-
-  // Find the brick's initial conditions.
+  // Set the brick's initial positions.
   (*brick_initial_pose)(0) = brick_joint_keyframes(0, 0);  // y-translate
   (*brick_initial_pose)(1) = brick_joint_keyframes(0, 1);  // z-translate
   (*brick_initial_pose)(2) = brick_joint_keyframes(0, 2);  // x-revolute
 
-  return finger_joint_keyframes;
+  finger_joint_keyframes.transposeInPlace();
+
+  // Create the finger joint name to column index map.
+  std::map<std::string, int> finger_joint_name_to_col_index_map;
+  for (size_t i = 0; i < finger_joint_ordering.size(); i++) {
+    finger_joint_name_to_col_index_map[finger_joint_ordering[i]] = i;
+  }
+
+  return std::make_pair(finger_joint_keyframes,
+                        finger_joint_name_to_col_index_map);
 }
 
 }  // namespace planar_gripper
--- examples/planar_gripper/planar_gripper_common.h
+++ examples/planar_gripper/planar_gripper_common.h
@@ -1,8 +1,8 @@
 #pragma once
 
+#include <map>
 #include <string>
-
-#include "robotlocomotion/robot_plan_t.hpp"
+#include <utility>
 
 #include "drake/multibody/plant/multibody_plant.h"
 
@@ -12,13 +12,39 @@ namespace planar_gripper {
 
 using drake::multibody::MultibodyPlant;
 
+/**
+ * Welds each finger's base frame to the world. The planar-gripper is made up of
+ * three 2-DOF fingers, whose bases are fixed equidistant along the perimeter of
+ * a circle. All "fingertips" point inwards (towards the world origin) when all
+ * joint angles are zero, and all motion lies in the Y-Z plane.
+ * @tparam T The scalar type. Currently only supports double.
+ * @param plant The plant that will contain the planar-gripper.
+ */
 template <typename T>
 void WeldGripperFrames(MultibodyPlant<T>* plant);
 
-void PublishRobotPlan(const robotlocomotion::robot_plan_t& plan);
-
-MatrixX<double> ParseKeyframes(const std::string& name,
-                               EigenPtr<Vector3<double>> brick_ics);
+/**
+ * Parses a text file containing keyframe joint positions for the planar gripper
+ * and the planar brick (the object being manipulated).
+ * @param[in] name The file name to parse.
+ * @param[out] brick_initial_pose A vector containing the initial brick pose.
+ * @return A std::pair containing a matrix of finger joint position keyframes
+ * (each matrix row represents a single keyframe) and a std::map containing the
+ * mapping between each finger joint name and the corresponding column index in
+ * the keyframe matrix containing the data for that joint.
+ * @note The file should begin with a header row that indicates the joint
+ * ordering for keyframes. Each row (keyframe) following the header should
+ * contain the same number of joints as indicated in the header. All entries
+ * should be tab separated and the file should end in a newline character.
+ * Header names should consist of three finger base joints, three finger mid
+ * joints, and three brick joints (9 total): {finger1_BaseJoint,
+ * finger2_BaseJoint, finger3_BaseJoint, finger1_MidJoint, finger2_MidJoint,
+ * finger3_MindJoint, brick_translate_y_joint, brick_translate_z_joint,
+ * brick_revolute_x_joint}. Names may appear in any order. The behavior of
+ * parsing is undefined if these conditions are not met.
+ */
+std::pair<MatrixX<double>, std::map<std::string, int>> ParseKeyframes(
+    const std::string& name, EigenPtr<Vector3<double>> brick_initial_pose);
 
 }  // namespace planar_gripper
 }  // namespace examples
