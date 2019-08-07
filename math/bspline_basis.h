a2738581db6f121f3e72c5a2b1533d2b19b1a1ae
Andrés K. Valenzuela <andres.valenzuela@tri.global>
2019-08-07 15:54:06 -0400

    math: Add BsplineBasis and BsplineTrajectory classes

---
+++ math/bspline_basis.h
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <vector>
+
+#include "drake/common/drake_bool.h"
+#include "drake/common/drake_copyable.h"
+#include "drake/math/knot_vector_type.h"
+
+namespace drake {
+namespace math {
+template <typename T>
+class BsplineBasis final {
+ public:
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(BsplineBasis);
+
+  /** Constructs a B-spline basis with the specified `order` and `knots`.
+  @pre `knots` is sorted in ascending order.
+  @throws std::invalid_argument if knots.size() < 2 * order. */
+  BsplineBasis(int order, std::vector<T> knots);
+
+  /** Constructs a B-spline basis with the specified `order` and
+  `num_control_points` with an auto-generated knot vector. If `type ==
+  KnotVectorType::kClampedUniform` the knot vector is a clamped uniform knot
+  vector from 0 to 1. If `type == KnotVectorType::kUniform`, the knot vector
+  is [-(order - 1), ..., num_control_points].
+  @throws std::invalid_argument if num_control_points < order. */
+  BsplineBasis(int order, int num_control_points,
+               KnotVectorType type = KnotVectorType::kClampedUniform);
+
+  int order() const { return order_; }
+
+  int num_control_points() const { return num_control_points_; }
+
+  const std::vector<T>& knots() const { return knots_; }
+
+  T initial_parameter_value() const { return knots()[order() - 1]; }
+
+  T final_parameter_value() const { return knots()[num_control_points()]; }
+
+  bool IsControlPointActive(int control_point_index,
+                            const std::array<T, 2>& parameter_interval) const;
+
+  bool IsControlPointActive(int control_point_index,
+                            const T& parameter_value) const;
+
+  std::vector<int> ComputeActiveControlPointIndices(
+      const std::array<T, 2>& parameter_interval) const;
+
+  std::vector<int> ComputeActiveControlPointIndices(
+      const T& parameter_value) const;
+
+  /** Implements the de Boor algorithm. Uses the notation from Patrikalakis et
+  al. [1]. Since the depth of recursion is known a-priori, the algorithm is
+  flattened along the lines described in [2] to avoid duplicate computations.
+
+   [1] https://web.mit.edu/hyperbook/Patrikalakis-Maekawa-Cho/node18.html
+   [2] De Boor, Carl. "On calculating with B-splines." Journal of
+       Approximation theory 6.1 (1972): 50-62.
+  @param control_points Control points of a B-spline curve. In the notation
+  of [1] this is p.
+  @param parameter_value Parameter value at which to evaluate the B-spline
+  curve defined by `this` and `control_points`. In the notation of [1] this
+  is \bar{t}. */
+  template <typename T_control_point>
+  T_control_point DeBoor(const std::vector<T_control_point>& control_points,
+                         T parameter_value) const {
+    // NOTE: The implementation of this method is included in the header so that
+    // it can be used with custom values of T_control_point.
+    using std::max;
+    using std::min;
+    // Define short names to match notation in [1].
+    const std::vector<T>& t = knots();
+    const T t_bar = min(max(parameter_value, initial_parameter_value()),
+                        final_parameter_value());
+    const int& k = order();
+
+    // Find the knot index 𝑙 (ell in code) such that t[𝑙] ≤ t_bar < t[𝑙 + 1].
+    // Special handling is required for the case where
+    // t_bar == this->final_parameter_value().
+    const int ell = std::distance(
+        t.begin(),
+        std::prev(t_bar < final_parameter_value()
+                      ? std::upper_bound(t.begin(), t.end(), t_bar)
+                      : std::lower_bound(t.begin(), t.end(), t_bar)));
+    // The vector that stores the intermediate de Boor points (the
+    // pᵢʲ in [1]).
+    std::vector<T_control_point> p(order());
+    // For j = 0, i goes from ell down to ell - (k - 1). Define r such that
+    // i = ell - r.
+    for (int r = 0; r < k; ++r) {
+      const int i = ell - r;
+      p.at(r) = control_points.at(i);
+    }
+    // For j = 1, ..., k - 1, i goes from ell down to ell - (k - j - 1). Again,
+    // i = ell - r.
+    for (int j = 1; j < k; ++j) {
+      for (int r = 0; r < k - j; ++r) {
+        const int i = ell - r;
+        // α = (t_bar - t[i]) / (t[i + k - j] - t[i]);
+        T alpha = (t_bar - t.at(i)) / (t.at(i + order() - j) - t.at(i));
+        p.at(r) = (1.0 - alpha) * p.at(r + 1) + alpha * p.at(r);
+      }
+    }
+    return p.front();
+  }
+
+  T BasisFunctionValue(int index, T parameter_value) const;
+
+  bool operator==(const BsplineBasis& other) const;
+
+ private:
+  int order_;
+  int num_control_points_;
+  std::vector<T> knots_;
+};
+}  // namespace math
+}  // namespace drake
---
+++ math/test/bspline_basis_test.cc
@@ -0,0 +1,213 @@
+#include "drake/math/bspline_basis.h"
+
+#include <gtest/gtest.h>
+
+#include "drake/common/test_utilities/expect_throws_message.h"
+
+namespace drake {
+namespace math {
+
+// Verifies that the constructors work as expected.
+GTEST_TEST(BsplineBasisTests, ConstructorTest) {
+  const int expected_order = 4;
+  const int expected_num_control_points = 11;
+  const std::vector<double> expected_knots_0{
+      0, 0, 0, 0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1, 1, 1, 1};
+  const std::vector<double> expected_knots_1{-3, -2, -1, 0, 1, 2,  3, 4,
+                                             5,  6,  7,  8, 9, 10, 11};
+  // Check the order and num_control_points constructor.
+  BsplineBasis<double> bspline_basis_0{expected_order,
+                                       expected_num_control_points};
+  EXPECT_EQ(bspline_basis_0.order(), expected_order);
+  EXPECT_EQ(bspline_basis_0.num_control_points(), expected_num_control_points);
+  EXPECT_EQ(bspline_basis_0.knots(), expected_knots_0);
+  BsplineBasis<double> bspline_basis_1{
+      expected_order, expected_num_control_points, KnotVectorType::kUniform};
+  EXPECT_EQ(bspline_basis_1.order(), expected_order);
+  EXPECT_EQ(bspline_basis_1.num_control_points(), expected_num_control_points);
+  EXPECT_EQ(bspline_basis_1.knots(), expected_knots_1);
+
+  // Check the order and knots constructor.
+  BsplineBasis<double> bspline_basis_2{expected_order, expected_knots_0};
+  EXPECT_EQ(bspline_basis_2.order(), expected_order);
+  EXPECT_EQ(bspline_basis_2.num_control_points(), expected_num_control_points);
+  EXPECT_EQ(bspline_basis_2.knots(), expected_knots_0);
+}
+
+GTEST_TEST(BsplineBasisTests, MinNumControlPoints) {
+  const int order = 4;
+  const char* expected_message_0 =
+      "The number of control points (.*) should be greater than or equal to "
+      "the order (.*).";
+  DRAKE_EXPECT_THROWS_MESSAGE(BsplineBasis<double>(order, 3),
+                              std::invalid_argument, expected_message_0);
+  const char* expected_message_1 =
+      "The number of knots (.*) should be greater than or equal to twice the "
+      "order (.*).";
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      BsplineBasis<double>(order, {0, 1, 2, 3, 4, 5, 6}), std::invalid_argument,
+      expected_message_1);
+}
+
+// Verifies that ComputeActiveControlPointIndices() returns the correct values
+// for selected inputs.
+GTEST_TEST(BsplineBasisTests, ComputeActiveControlPointIndicesTest) {
+  const int expected_order = 5;
+  const int expected_num_control_points = 14;
+  BsplineBasis<double> bspline_basis_0{expected_order,
+                                       expected_num_control_points};
+
+  const std::array<double, 2> plan_interval_0{0.9, 1.0};
+  const std::vector<int> expected_active_control_point_indices_0{9, 10, 11, 12,
+                                                                 13};
+  std::vector<int> active_control_point_indices_0{
+      bspline_basis_0.ComputeActiveControlPointIndices(plan_interval_0)};
+  EXPECT_EQ(active_control_point_indices_0,
+            expected_active_control_point_indices_0);
+
+  const std::array<double, 2> plan_interval_1{0.95, 1.0};
+  const std::vector<int> expected_active_control_point_indices_1{9, 10, 11, 12,
+                                                                 13};
+  std::vector<int> active_control_point_indices_1{
+      bspline_basis_0.ComputeActiveControlPointIndices(plan_interval_1)};
+  EXPECT_EQ(active_control_point_indices_1,
+            expected_active_control_point_indices_1);
+
+  const std::array<double, 2> plan_interval_2{0.85, 1.0};
+  const std::vector<int> expected_active_control_point_indices_2{8,  9,  10,
+                                                                 11, 12, 13};
+  std::vector<int> active_control_point_indices_2{
+      bspline_basis_0.ComputeActiveControlPointIndices(plan_interval_2)};
+  EXPECT_EQ(active_control_point_indices_2,
+            expected_active_control_point_indices_2);
+
+  const std::array<double, 2> plan_interval_3{0.0, 0.0};
+  const std::vector<int> expected_active_control_point_indices_3{0, 1, 2, 3, 4};
+  std::vector<int> active_control_point_indices_3{
+      bspline_basis_0.ComputeActiveControlPointIndices(plan_interval_3)};
+  EXPECT_EQ(active_control_point_indices_3,
+            expected_active_control_point_indices_3);
+
+  const std::array<double, 2> plan_interval_4{1.0, 1.0};
+  const std::vector<int> expected_active_control_point_indices_4{9, 10, 11, 12,
+                                                                 13};
+  std::vector<int> active_control_point_indices_4{
+      bspline_basis_0.ComputeActiveControlPointIndices(plan_interval_4)};
+  EXPECT_EQ(active_control_point_indices_4,
+            expected_active_control_point_indices_4);
+
+  BsplineBasis<double> bspline_basis_1{1, expected_num_control_points};
+  const std::array<double, 2> plan_interval_5{0.0, 0.0};
+  const std::vector<int> expected_active_control_point_indices_5{0};
+  std::vector<int> active_control_point_indices_5{
+      bspline_basis_1.ComputeActiveControlPointIndices(plan_interval_5)};
+  EXPECT_EQ(active_control_point_indices_5,
+            expected_active_control_point_indices_5);
+}
+
+// Compares values returned by BsplineBasis::BasisFunctionValue() to those
+// generated by scipy.interpolate.Bspline.basis_element():
+//
+//     import numpy as np
+//     from scipy.interpolate import BSpline
+//     knots = [0, 0, 0, 0, 0.25, 0.5, 0.75, 1, 1, 1, 1]
+//     order = 4
+//     num_basis_functions = len(knots) - order
+//     for i in range(0, num_basis_functions):
+//         bspline = BSpline.basis_element(knots[i:(i+order+1)],
+//                                         extrapolate=False)
+//         u = np.union1d(np.linspace(bspline.t[order - 1],
+//                                    bspline.t[-order], 7), knots)
+//         print("parameter_values.push_back(std::vector<double>{{{}}});"
+//               .format(", ".join("{:.17f}".format(n) for n in u)))
+//         print("expected_basis_function_values.push_back" +
+//               "(std::vector<double>{{{}}});"
+//               .format(", ".join("{:.17f}".format(0.0 if np.isnan(n) else n)
+//                                 for n in bspline(u))))
+//
+// NOTE(avalenzu): Scipy gives B[6](1.0) = 0.0, which is wrong. That value has
+// been replaced with the correct one (1.0).
+GTEST_TEST(BsplineBasisTests, BasisFunctionValueScipyComparison) {
+  const int order = 4;
+  const int num_control_points = 7;
+  BsplineBasis<double> basis{order, num_control_points};
+  std::vector<std::vector<double>> parameter_values{};
+  std::vector<std::vector<double>> expected_basis_function_values{};
+  parameter_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.04166666666666666, 0.08333333333333333,
+      0.12500000000000000, 0.16666666666666666, 0.20833333333333331,
+      0.25000000000000000, 0.50000000000000000, 0.75000000000000000,
+      1.00000000000000000});
+  expected_basis_function_values.push_back(std::vector<double>{
+      1.00000000000000000, 0.57870370370370383, 0.29629629629629639,
+      0.12500000000000000, 0.03703703703703705, 0.00462962962962964,
+      0.00000000000000000, 0.00000000000000000, 0.00000000000000000,
+      0.00000000000000000});
+  parameter_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.08333333333333333, 0.16666666666666666,
+      0.25000000000000000, 0.33333333333333331, 0.41666666666666663,
+      0.50000000000000000, 0.75000000000000000, 1.00000000000000000});
+  expected_basis_function_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.56481481481481488, 0.51851851851851860,
+      0.25000000000000000, 0.07407407407407410, 0.00925925925925927,
+      0.00000000000000000, 0.00000000000000000, 0.00000000000000000});
+  parameter_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.12500000000000000, 0.25000000000000000,
+      0.37500000000000000, 0.50000000000000000, 0.62500000000000000,
+      0.75000000000000000, 1.00000000000000000});
+  expected_basis_function_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.26041666666666663, 0.58333333333333326,
+      0.46875000000000000, 0.16666666666666666, 0.02083333333333333,
+      0.00000000000000000, 0.00000000000000000});
+  parameter_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.16666666666666666, 0.25000000000000000,
+      0.33333333333333331, 0.50000000000000000, 0.66666666666666663,
+      0.75000000000000000, 0.83333333333333326, 1.00000000000000000});
+  expected_basis_function_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.04938271604938271, 0.16666666666666666,
+      0.37037037037037035, 0.66666666666666663, 0.37037037037037052,
+      0.16666666666666666, 0.04938271604938278, 0.00000000000000000});
+  parameter_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.25000000000000000, 0.37500000000000000,
+      0.50000000000000000, 0.62500000000000000, 0.75000000000000000,
+      0.87500000000000000, 1.00000000000000000});
+  expected_basis_function_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.00000000000000000, 0.02083333333333333,
+      0.16666666666666666, 0.46875000000000000, 0.58333333333333326,
+      0.26041666666666663, 0.00000000000000000});
+  parameter_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.25000000000000000, 0.50000000000000000,
+      0.58333333333333337, 0.66666666666666663, 0.75000000000000000,
+      0.83333333333333326, 0.91666666666666663, 1.00000000000000000});
+  expected_basis_function_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.00000000000000000, 0.00000000000000000,
+      0.00925925925925927, 0.07407407407407403, 0.25000000000000000,
+      0.51851851851851827, 0.56481481481481488, 0.00000000000000000});
+  parameter_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.25000000000000000, 0.50000000000000000,
+      0.75000000000000000, 0.79166666666666663, 0.83333333333333337,
+      0.87500000000000000, 0.91666666666666663, 0.95833333333333326,
+      1.00000000000000000});
+  expected_basis_function_values.push_back(std::vector<double>{
+      0.00000000000000000, 0.00000000000000000, 0.00000000000000000,
+      0.00000000000000000, 0.00462962962962962, 0.03703703703703709,
+      0.12500000000000000, 0.29629629629629611, 0.57870370370370305,
+      1.00000000000000000});
+  ASSERT_EQ(static_cast<int>(parameter_values.size()), num_control_points);
+  ASSERT_EQ(static_cast<int>(expected_basis_function_values.size()),
+            num_control_points);
+  for (int i = 0; i < num_control_points; ++i) {
+    const int num_parameter_values =
+        static_cast<int>(parameter_values[i].size());
+    ASSERT_EQ(static_cast<int>(expected_basis_function_values[i].size()),
+              num_parameter_values);
+    for (int j = 0; j < num_parameter_values; ++j) {
+      EXPECT_NEAR(basis.BasisFunctionValue(i, parameter_values[i][j]),
+                  expected_basis_function_values[i][j],
+                  std::numeric_limits<double>::epsilon());
+    }
+  }
+}
+
+}  // namespace math
+}  // namespace drake
