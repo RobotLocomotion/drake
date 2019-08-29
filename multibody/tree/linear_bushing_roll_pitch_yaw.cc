ed5357c2244024f52ed8486d1f131fe8b0394a17
Mitiguy <mitiguy@StanfordAlumni.org>
2019-08-28 17:26:53 -0700

    Uncomment the clone methods

--- multibody/tree/linear_bushing_roll_pitch_yaw.cc
+++ multibody/tree/linear_bushing_roll_pitch_yaw.cc
@@ -17,10 +17,10 @@ template <typename T>
 LinearBushingRollPitchYaw<T>::LinearBushingRollPitchYaw(
                           const Frame<T>& frameAb,
                           const Frame<T>& frameBa,
-                          const Vector3<T>& torque_stiffness_constants,
-                          const Vector3<T>& torque_damping_constants,
-                          const Vector3<T>& force_stiffness_constants,
-                          const Vector3<T>& force_damping_constants) :
+                          const Vector3<double>& torque_stiffness_constants,
+                          const Vector3<double>& torque_damping_constants,
+                          const Vector3<double>& force_stiffness_constants,
+                          const Vector3<double>& force_damping_constants) :
     ForceElement<T>(frameAb.body().model_instance()),
     frameAb_(frameAb),
     frameBa_(frameBa),
@@ -85,8 +85,8 @@ T LinearBushingRollPitchYaw<T>::CalcPotentialEnergy(
     const internal::PositionKinematicsCache<T>& /* pc */) const {
   // Calculate the roll-pitch-yaw angles q₀, q₁, q₂ and use them together with
   // torsional stiffness to form the torque potential energy.
-  const math::RollPitchYaw<T> rpy = CalcBushingRollPitchYawAngles(context);
-  const Vector3<T> q012Squared = rpy.vector().cwiseProduct(rpy.vector());
+  const Vector3<T> ypr = CalcBushingBodyZYXAngleSequenceYawRollPitch(context);
+  const Vector3<T> q012Squared = ypr.cwiseProduct(ypr);
   const T torque_potential_energy =
       0.5 * torque_stiffness_constants().dot(q012Squared);
 
@@ -140,24 +140,24 @@ T LinearBushingRollPitchYaw<T>::CalcNonConservativePower(
   return Pn_torque + Pn_force;
 }
 
-#if 0
 template <typename T>
 template <typename ToScalar>
 std::unique_ptr<ForceElement<ToScalar>>
 LinearBushingRollPitchYaw<T>::TemplatedDoCloneToScalar(
     const internal::MultibodyTree<ToScalar>& tree_clone) const {
-  const Body<ToScalar>& bodyA_clone =
-      tree_clone.get_body(bodyA().index());
-  const Body<ToScalar>& bodyB_clone =
-      tree_clone.get_body(bodyB().index());
+  const Frame<ToScalar>& frameAb_clone = tree_clone.get_variant(frameAb());
+  const Frame<ToScalar>& frameBa_clone = tree_clone.get_variant(frameBa());
+  const Vector3<double>& k012 = torque_stiffness_constants();
+  const Vector3<double>& b012 = torque_damping_constants();
+  const Vector3<double>& kxyz = force_stiffness_constants();
+  const Vector3<double>& bxyz = force_damping_constants();
 
   // Make the Joint<T> clone.
-  auto spring_damper_clone =
+  auto bushing_clone =
       std::make_unique<LinearBushingRollPitchYaw<ToScalar>>(
-          bodyA_clone, p_AP(), bodyB_clone, p_BQ(), free_length(), stiffness(),
-          damping());
+          frameAb_clone, frameBa_clone, k012, b012, kxyz, bxyz);
 
-  return std::move(spring_damper_clone);
+  return std::move(bushing_clone);
 }
 
 template <typename T>
@@ -180,58 +180,6 @@ LinearBushingRollPitchYaw<T>::DoCloneToScalar(
     const internal::MultibodyTree<symbolic::Expression>& tree_clone) const {
   return TemplatedDoCloneToScalar(tree_clone);
 }
-
-template <typename T>
-T LinearBushingRollPitchYaw<T>::SafeSoftNorm(const Vector3<T> &x) const {
-  using std::sqrt;
-  const double epsilon_length =
-      std::numeric_limits<double>::epsilon() * free_length();
-  const double epsilon_length_squared = epsilon_length * epsilon_length;
-  const T x2 = x.squaredNorm();
-  if (scalar_predicate<T>::is_bool && (x2 < epsilon_length_squared)) {
-    throw std::runtime_error("The length of the spring became nearly zero. "
-                                 "Revisit your model to avoid this situation.");
-  }
-  return sqrt(x2 + epsilon_length_squared);
-}
-
-template <typename T>
-T LinearBushingRollPitchYaw<T>::CalcLengthTimeDerivative(
-    const internal::PositionKinematicsCache<T>& pc,
-    const internal::VelocityKinematicsCache<T>& vc) const {
-  const math::RigidTransform<T>& X_WA = pc.get_X_WB(bodyA().node_index());
-  const math::RigidTransform<T>& X_WB = pc.get_X_WB(bodyB().node_index());
-
-  const Vector3<T> p_WP = X_WA * p_AP_.template cast<T>();
-  const Vector3<T> p_WQ = X_WB * p_BQ_.template cast<T>();
-
-  // Vector from P to Q. It's length is the current length of the spring.
-  const Vector3<T> p_PQ_W = p_WQ - p_WP;
-
-  // Using a "soft" norm we define a "soft length" as ℓₛ = ‖p_PQ‖ₛ.
-  const T length_soft = SafeSoftNorm(p_PQ_W);
-
-  const Vector3<T> r_PQ_W = p_PQ_W / length_soft;
-
-  // Compute the velocities of P and Q so that we can add the damping force.
-  // p_PAo = p_WAo - p_WP
-  const Vector3<T> p_PAo_W = X_WA.translation() - p_WP;
-  const SpatialVelocity<T>& V_WP =
-      vc.get_V_WB(bodyA().node_index()).Shift(-p_PAo_W);
-
-  // p_QBo = p_WBo - p_WQ
-  const Vector3<T> p_QBo_W = X_WB.translation() - p_WQ;
-  const SpatialVelocity<T>& V_WQ =
-      vc.get_V_WB(bodyB().node_index()).Shift(-p_QBo_W);
-
-  // Relative velocity of P in Q, expressed in the world frame.
-  const Vector3<T> v_PQ_W = V_WQ.translational() - V_WP.translational();
-  // The rate at which the length of the spring changes.
-  const T length_dot = v_PQ_W.dot(r_PQ_W);
-
-  return length_dot;
-}
-#endif
 
 }  // namespace multibody
 }  // namespace drake
