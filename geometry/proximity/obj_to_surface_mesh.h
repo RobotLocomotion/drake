c65409b5065569d5521a5cefc16ea7d08e86cd44
DamrongGuoy <Damrong.Guoy@tri.global>
2019-08-30 13:43:23 -0700

    [geometry] Read Wavefront obj file into SurfaceMesh.

---
+++ geometry/proximity/obj_to_surface_mesh.h
@@ -0,0 +1,155 @@
+#pragma once
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <tiny_obj_loader.h>
+
+#include "drake/geometry/proximity/surface_mesh.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+
+#ifndef DRAKE_DOXYGEN_CXX
+
+/**
+ Converts vertices of tinyobj to vertices of SurfaceMesh.
+ @param tinyobj_vertices
+     Vertices from tinyobj represented as `std::vector` of floating-point
+     numbers.
+ @param scale
+     An optional scale to coordinates.
+ @return
+     Vertices for SurfaceMesh.
+ @pre
+     The size of `tinyobj_vertices` is divisible by three.
+ @tparam T
+     The underlying scalar type for coordinates, e.g., double or AutoDiffXd.
+     Must be a valid Eigen scalar.
+ */
+template <typename T>
+std::vector<SurfaceVertex<T>> TinyObjToSurfaceVertices(
+    const std::vector<tinyobj::real_t>& tinyobj_vertices, const double scale) {
+  // Vertices from tinyobj are in a vector of floating-point numbers like this:
+  //     tinyobj_vertices = {c₀,c₁,c₂, c₃,c₄,c₅, c₆,c₇,c₈,...}
+  //                      = {x, y, z,  x, y, z,  x, y, z,...}
+  // We will convert to a vector of Vector3<T> like this:
+  //     vertices = {{c₀,c₁,c₂}, {c₃,c₄,c₅}, {c₆,c₇,c₈},...}
+  //              = {    v0,         v1,         v2,...}
+  const int num_coords = tinyobj_vertices.size();
+  DRAKE_DEMAND(num_coords % 3 == 0);
+  std::vector<SurfaceVertex<T>> vertices;
+  vertices.reserve(num_coords / 3);
+
+  auto iter = tinyobj_vertices.begin();
+  while (iter != tinyobj_vertices.end()) {
+    double x = *(iter++) * scale;
+    double y = *(iter++) * scale;
+    double z = *(iter++) * scale;
+    vertices.emplace_back(Vector3<T>(x, y, z));
+  }
+  return vertices;
+}
+
+/**
+ Converts faces of tinyobj::mesh_t to faces of SurfaceMesh.
+ @param mesh
+     The mesh from tinyobj.
+ @return
+     The triangular faces for SurfaceMesh.
+ @pre
+     Every face is a triangle.
+ */
+std::vector<SurfaceFace> TinyObjToSurfaceFaces(const tinyobj::mesh_t& mesh) {
+  //
+  // In general, tinyobj::mesh_t::num_face_vertices is a list of number
+  // of vertices of each polygonal face like this:
+  //     mesh.num_face_vertices = {n₀, n₁, n₂,...},
+  // where faceᵢ has nᵢ vertices. In this function, we assume every face is a
+  // triangle, so every nᵢ is 3 like this:
+  //     mesh.num_face_vertices = {3, 3, 3,...}.
+  //
+  // In general, tinyobj::mesh_t::indices concatenates tinyobj::index_t of
+  // vertices of each face like this:
+  //     mesh.indices = {v0₀,v0₁,...,v0ₙ₀₋₁, v1₀,v1₁,...,v1ₙ₁₋₁, ...}.
+  //                     -------face0------  -------face1------
+  // In this function, we assume each face is a triangle, so it looks like this:
+  //     mesh.indices = {v0₀,v0₁,v0₂, v1₀,v1₁,v1₂, ...}
+  //                     ---face0---  ---face1---
+  //
+  // Each tinyobj::index_t consists of vertex_index, normal_index, and
+  // texcoord_index. In this function, we use only vertex_index.
+  //
+  const int num_faces = mesh.num_face_vertices.size();
+  const int num_indices = mesh.indices.size();
+  DRAKE_DEMAND(3 * num_faces == num_indices);
+  std::vector<SurfaceFace> faces;
+  faces.reserve(num_faces);
+  for (int face = 0; face < num_faces; ++face) {
+    DRAKE_DEMAND(mesh.num_face_vertices[face] == 3);
+    const int vertex_indices[3] = {mesh.indices[3 * face].vertex_index,
+                                   mesh.indices[3 * face + 1].vertex_index,
+                                   mesh.indices[3 * face + 2].vertex_index};
+    faces.emplace_back(vertex_indices);
+  }
+  return faces;
+}
+
+#endif  // #ifndef DRAKE_DOXYGEN_CXX
+
+/**
+ Constructs a surface mesh from a Wavefront .obj file located at the given
+ _absolute_ file path and optionally scales coordinates by the given scale
+ factor.
+ @param absolute_filename
+     The file name with absolute path. We only support an .obj file with only
+     one object.
+ @param scale
+     An optional scale to coordinates.
+ @throws std::runtime_error if the .obj file doesn't define a single object.
+     This can happen if it is empty, if there are multiple object-name
+     statements (e.g., "o object name"), or if there are faces defined
+     outside a single object-name statement.
+ @return surface mesh
+ @tparam T
+     The underlying scalar type for coordinates, e.g., double or AutoDiffXd.
+     Must be a valid Eigen scalar.
+ */
+template <typename T>
+SurfaceMesh<T> ReadObjToSurfaceMesh(const std::string& absolute_filename,
+                                    const double scale = 1.0) {
+  tinyobj::attrib_t attrib;
+  std::vector<tinyobj::shape_t> shapes;
+  std::vector<tinyobj::material_t> materials;
+  std::string err;
+  bool do_tinyobj_triangulation = true;  // triangulate non-triangle faces.
+
+  // Provide the base directory for the material-library file.
+  const size_t pos = absolute_filename.find_last_of('/');
+  const std::string mtl_basedir = absolute_filename.substr(0, pos + 1);
+
+  bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err,
+                              absolute_filename.c_str(), mtl_basedir.c_str(),
+                              do_tinyobj_triangulation);
+  if (!ret || !err.empty()) {
+    throw std::runtime_error("Error parsing file '" + absolute_filename +
+                             "' : " + err);
+  }
+  if (shapes.size() != 1) {
+    throw std::runtime_error("The Wavefront .obj file '" + absolute_filename +
+                             "' must have one and only one object defined in "
+                             "it.");
+  }
+  std::vector<SurfaceVertex<T>> vertices =
+      TinyObjToSurfaceVertices<T>(attrib.vertices, scale);
+
+  std::vector<SurfaceFace> faces = TinyObjToSurfaceFaces(shapes[0].mesh);
+
+  return SurfaceMesh<T>(std::move(faces), std::move(vertices));
+}
+
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
---
+++ geometry/proximity/test/obj_to_surface_mesh_test.cc
@@ -0,0 +1,154 @@
+#include "drake/geometry/proximity/obj_to_surface_mesh.h"
+
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "drake/common/eigen_types.h"
+#include "drake/common/find_resource.h"
+#include "drake/common/test_utilities/expect_throws_message.h"
+#include "drake/geometry/proximity/surface_mesh.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+namespace {
+
+GTEST_TEST(ObjToSurfaceMeshTest, TinyObjToSurfaceVertices) {
+  // clang-format off
+  const std::vector<tinyobj::real_t> tinyobj_vertices {
+      1.0, -1.0, -1.0,  // first vertex.
+      1.0, -1.0,  1.0   // second vertex.
+  };
+  // clang-format on
+
+  const double scale = 2.0;
+  const std::vector<SurfaceVertex<double>> surface_vertices =
+      TinyObjToSurfaceVertices<double>(tinyobj_vertices, scale);
+
+  EXPECT_EQ(2, surface_vertices.size());
+  // clang-format off
+  const std::vector<Vector3<double>> expect_vertices {
+      {2.0, -2.0, -2.0},  // first vertex scaled by 2.0.
+      {2.0, -2.0,  2.0}   // second vertex scaled by 2.0.
+  };
+  // clang-format on
+
+  for (int i = 0; i < 2; ++i) {
+    EXPECT_EQ(expect_vertices[i], surface_vertices[i].r_MV());
+  }
+}
+
+GTEST_TEST(ObjToSurfaceMeshTest, TinyObjToSurfaceFaces) {
+  // clang-format off
+  const tinyobj::mesh_t tinyobj_mesh {
+      // indices
+      //     Each entry consists of vertex_index, normal_index, and
+      //     texcoord_index with -1 meaning not used. Here we use only
+      //     vertex_index and set normal_index and texcoord_index to -1.
+      //     Every three entries correspond to a triangle.
+      {{0, -1, -1}, {1, -1, -1}, {2, -1, -1},  // triangle 0 1 2
+       {0, -1, -1}, {2, -1, -1}, {3, -1, -1}   // triangle 0 2 3
+      },
+      // num_face_vertices
+      //     Here each face is a triangle, so we have the number of vertices
+      //     of each face equals 3.
+      {3, 3},
+      // material_ids
+      //     Per-face material ID for rendering. It's not used in this test.
+      {},
+      // tags
+      //     It's not used in this test.
+      {}
+  };
+  // clang-format on
+
+  const std::vector<SurfaceFace> surface_faces =
+      TinyObjToSurfaceFaces(tinyobj_mesh);
+
+  EXPECT_EQ(2, surface_faces.size());
+  const int expect_faces[2][3]{{0, 1, 2}, {0, 2, 3}};
+  auto face_equal = [](const SurfaceFace& f, const SurfaceFace& g) -> bool {
+    return std::make_tuple(f.vertex(0), f.vertex(1), f.vertex(2)) ==
+           std::make_tuple(g.vertex(0), g.vertex(1), g.vertex(2));
+  };
+  for (int i = 0; i < 2; ++i) {
+    EXPECT_TRUE(face_equal(SurfaceFace(expect_faces[i]), surface_faces[i]));
+  }
+}
+
+GTEST_TEST(ObjToSurfaceMeshTest, ReadObjToSurfaceMesh) {
+  const std::string filename =
+      FindResourceOrThrow("drake/geometry/proximity/test/quad_cube.obj");
+  SurfaceMesh<double> surface = ReadObjToSurfaceMesh<double>(filename);
+
+  ASSERT_EQ(surface.num_vertices(), 8);
+  ASSERT_EQ(surface.num_faces(), 12);
+
+  // This test relies on the specific content of the file quad_cube.obj.
+  // These coordinates came from the first section of quad_cube.obj.
+  // clang-format off
+  std::vector<Vector3<double>> expect_vertices {
+      { 1.000000, -1.000000, -1.000000},
+      { 1.000000, -1.000000,  1.000000},
+      {-1.000000, -1.000000,  1.000000},
+      {-1.000000, -1.000000, -1.000000},
+      { 1.000000,  1.000000, -1.000000},
+      { 1.000000,  1.000000,  1.000001},
+      {-1.000000,  1.000000,  1.000000},
+      {-1.000000,  1.000000, -1.000000}
+  };
+  // clang-format on
+
+  for (int i = 0; i < 8; ++i) {
+    EXPECT_EQ(expect_vertices[i], surface.vertex(SurfaceVertexIndex(i)).r_MV());
+  }
+
+  // This test relies on the specific content of the file quad_cube.obj. The
+  // last section of quad_cube.obj describes the six square faces of the cube.
+  // We expect that each square is subdivided into two triangles. Note that
+  // vertex indices in the file quad_cube.obj are from 1 to 8, but tinyobj
+  // has vertex indices from 0 to 7.
+  // clang-format off
+  int expect_faces[12][3] {
+      {0, 1, 2}, {0, 2, 3},  // face 1 2 3 4 in quad_cube.obj
+      {4, 7, 6}, {4, 6, 5},  // face 5 8 7 6 in quad_cube.obj
+      {0, 4, 5}, {0, 5, 1},  // face 1 5 6 2 in quad_cube.obj
+      {1, 5, 6}, {1, 6, 2},  // face 2 6 7 3 in quad_cube.obj
+      {2, 6, 7}, {2, 7, 3},  // face 3 7 8 4 in quad_cube.obj
+      {4, 0, 3}, {4, 3, 7}   // face 5 1 4 8 in quad_cube.obj
+  };
+  // clang-format on
+
+  auto face_equal = [](const SurfaceFace& f, const SurfaceFace& g) -> bool {
+    return std::make_tuple(f.vertex(0), f.vertex(1), f.vertex(2)) ==
+           std::make_tuple(g.vertex(0), g.vertex(1), g.vertex(2));
+  };
+  for (int i = 0; i < 12; ++i) {
+    EXPECT_TRUE(face_equal(SurfaceFace(expect_faces[i]),
+                           surface.element(SurfaceFaceIndex(i))));
+  }
+}
+
+GTEST_TEST(ObjToSurfaceMeshTest, ThrowExceptionForTwoObjects) {
+  const std::string filename = FindResourceOrThrow(
+      "drake/geometry/proximity/test/forbidden_two_cubes.obj");
+  DRAKE_EXPECT_THROWS_MESSAGE(ReadObjToSurfaceMesh<double>(filename),
+                              std::runtime_error,
+                              ".*must have one and only one object defined.*");
+}
+
+GTEST_TEST(ObjToSurfaceMeshTest, SmokeTestAutoDiffXd) {
+  const std::string filename =
+      FindResourceOrThrow("drake/geometry/proximity/test/quad_cube.obj");
+  SurfaceMesh<AutoDiffXd> surface = ReadObjToSurfaceMesh<AutoDiffXd>(filename);
+  EXPECT_EQ(surface.num_vertices(), 8);
+  EXPECT_EQ(surface.num_faces(), 12);
+}
+
+}  // namespace
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
