4f8f6cd6def586c94f42bd17bddef9f6ef04b82d
DamrongGuoy <Damrong.Guoy@tri.global>
2019-08-30 13:43:23 -0700

    [geometry] Read Wavefront obj file into SurfaceMesh.

---
+++ geometry/proximity/obj_to_surface_mesh.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <istream>
+#include <string>
+#include <vector>
+
+#include "drake/geometry/proximity/surface_mesh.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+
+/**
+ Constructs a surface mesh from a Wavefront .obj file and optionally scales
+ coordinates by the given scale factor. Polygons will be triangulated if they
+ are not triangles already. All objects in the .obj file will be merged into
+ the surface mesh. See https://en.wikipedia.org/wiki/Wavefront_.obj_file for
+ the file format.
+ @param filename
+     A valid file name with absolute path or relative path.
+ @param scale
+     An optional scale to coordinates.
+ @throws std::runtime_error if `filename` doesn't have a valid file path, or the
+     file has no faces.
+ @return surface mesh
+ */
+SurfaceMesh<double> ReadObjToSurfaceMesh(const std::string& filename,
+                                         double scale = 1.0);
+
+/**
+ Overload of @ref ReadObjToSurfaceMesh(const std::string&, double) with the
+ Wavefront .obj file given in std::istream.
+ */
+SurfaceMesh<double> ReadObjToSurfaceMesh(std::istream* input_stream,
+                                         double scale = 1.0);
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
