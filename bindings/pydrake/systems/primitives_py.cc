264cd5f5056833db87ed16d62433ee226d0aa330
Russ Tedrake <russt@mit.edu>
2019-09-11 21:23:19 -0400

    Adds simple tutorial on the systems framework (#12009)
    * Adds simple tutorial on the systems framework
    
    ported (and updated) from the underactuated notes appendix.

--- bindings/pydrake/systems/primitives_py.cc
+++ bindings/pydrake/systems/primitives_py.cc
@@ -83,8 +83,25 @@ PYBIND11_MODULE(primitives, m) {
         .def("y0",
             overload_cast_explicit<const VectorXd&>(&AffineSystem<T>::y0),
             doc.AffineSystem.y0.doc)
+        // Wrap a few methods from the TimeVaryingAffineSystem parent class.
+        // TODO(russt): Move to TimeVaryingAffineSystem if/when that class is
+        // wrapped.
+        .def("get_input_port", &TimeVaryingAffineSystem<T>::get_input_port,
+            py_reference_internal,
+            doc.TimeVaryingAffineSystem.get_input_port.doc)
+        .def("get_output_port", &TimeVaryingAffineSystem<T>::get_output_port,
+            py_reference_internal,
+            doc.TimeVaryingAffineSystem.get_output_port.doc)
         .def("time_period", &AffineSystem<T>::time_period,
-            doc.TimeVaryingAffineSystem.time_period.doc);
+            doc.TimeVaryingAffineSystem.time_period.doc)
+        // Need to specifically redeclare the System to have both overloads
+        // available.
+        .def("get_input_port", &System<T>::get_input_port,
+            py_reference_internal, py::arg("port_index"),
+            pydrake_doc.drake.systems.System.get_input_port.doc)
+        .def("get_output_port", &System<T>::get_output_port,
+            py_reference_internal, py::arg("port_index"),
+            pydrake_doc.drake.systems.System.get_output_port.doc);
 
     DefineTemplateClassWithDefault<ConstantValueSource<T>, LeafSystem<T>>(
         m, "ConstantValueSource", GetPyParam<T>(), doc.ConstantValueSource.doc)
