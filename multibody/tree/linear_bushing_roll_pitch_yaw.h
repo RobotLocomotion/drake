61896d71c70c379cd7ce53d03a1d877ae24e2558
Mitiguy <mitiguy@StanfordAlumni.org>
2019-09-13 19:07:25 -0700

    Clean up SpaceXYZ roll-pitch-yaw conventions and add more tests

--- multibody/tree/linear_bushing_roll_pitch_yaw.h
+++ multibody/tree/linear_bushing_roll_pitch_yaw.h
@@ -17,13 +17,13 @@ template <typename T> class Body;
 /// body A to a frame Bᴀ of a body B.  The bushing has both rotational and
 /// translational stiffness and damping properties.
 ///
-/// The bushing's rotational stiffness depends on the yaw-pitch-roll angles
+/// The bushing's rotational stiffness depends on the roll-pitch-yaw angles
 /// q₀, q₁, q₂ and its rotational damping depends on q̇₀, q̇₁, q̇₂.  The angles
 /// q₀, q₁, q₂ are calculated from the orientation between frames Aʙ and Bᴀ and
-/// have the range `-π < q₀ <= π`, `-π/2 < q₁ <= π/2`, `-π < q₂ <= π`.
+/// have the range `-π < q₀ <= π`, `-π/2 <= q₁ <= π/2`, `-π < q₂ <= π`.
 ///
 /// The bushing's translational stiffness is linear in displacements x, y, z and
-/// the translational damping is linear in ẋ, ẏ, ż.  The displacements x, y, z
+/// its translational damping is linear in ẋ, ẏ, ż.  The displacements x, y, z
 /// are defined so the position vector from Aʙₒ (frame Aʙ's origin) to Bᴀₒ
 /// (frame Bᴀ's origin) expressed in frame Aʙ is `[x, y, z]`.
 ///
@@ -31,22 +31,23 @@ template <typename T> class Body;
 /// on frame Aʙ and a force f applied to point Aʙₒ, as
 /// <pre>
 /// f = (kx x + bx ẋ) Aʙx  +  (ky y + by ẏ) Aʙy + (kz z + bz ż) Aʙz
-/// τ = (k₀q₀ + b₀q̇₀) Aʙz  +  (k₁q₁ + b₁q̇₁) Iy  + (k₂q₂ + b₂q̇₂) Bᴀx
+/// τ = (k₀q₀ + b₀q̇₀) Bᴀx  +  (k₁q₁ + b₁q̇₁) Iy  + (k₂q₂ + b₂q̇₂) Aʙz
 /// </pre>
 /// where kx, ky, kz and bx, by, bz are force stiffness and damping constants,
 /// k₀, k₁, k₂ and b₀, b₁, b₂, are torque stiffness and damping constants,
 /// Aʙx, Aʙy, Aʙz are orthogonal unit vectors fixed in frame Aʙ,
 /// Bᴀx, Bᴀy, Bᴀz are orthogonal unit vectors fixed in frame Bᴀ, and
-/// Iy = -sin(q₀)*Aʙx + cos(q₀)*Aʙy.  Above, τ is expressed in a non-orthogonal
-/// basis related to a RollPitchYaw sequence - which is a Body-fixed (intrinsic)
-/// Z-Y-X rotation by angles `[yaw = q₀, pitch = q₁, roll = q₂]`.
+/// Iy = -sin(q₂)*Aʙx + cos(q₂)*Aʙy.  Note: The previous expression for τ is
+/// expressed in a non-orthogonal basis related to a Space-fixed XYZ (intrinsic)
+/// RollPitchYaw sequence with angles `[roll = q₀, pitch = q₁, yaw = q₂]`.
 ///
-/// The set of forces on body B from the bushing is equivalent to a torque -τ
+/// Using action/reaction (or applying Newtons 2ⁿᵈ law to the massless bushing),
+/// the set of forces on body B from the bushing is equivalent to a torque -τ
 /// on frame Bᴀ and a force -f applied to the point of B coincident with Aʙₒ.
 ///
 /// @tparam T The underlying scalar type. Must be a valid Eigen scalar.
 ///
-/// @see @ref RollPitchYaw for definitions of yaw, pitch, roll = `[q₀, q₁, q₂]`.
+/// @see @ref RollPitchYaw for definitions of roll, pitch, yaw = `[q₀, q₁, q₂]`.
 template <typename T>
 class LinearBushingRollPitchYaw final : public ForceElement<T> {
  public:
@@ -65,7 +66,6 @@ class LinearBushingRollPitchYaw final : public ForceElement<T> {
   /// `[bx, by, bz]` associated with translational rates `[ẋ, ẏ, ż]`.
   /// @note Refer to this class's documentation for details about τ, f, q₀, etc.
   /// @note The stiffness and damping parameters are usually non-negative.
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
   LinearBushingRollPitchYaw(const Frame<T>& frameAb,
                             const Frame<T>& frameBa,
                             const Vector3<double>& torque_stiffness_constants,
@@ -73,49 +73,40 @@ class LinearBushingRollPitchYaw final : public ForceElement<T> {
                             const Vector3<double>& force_stiffness_constants,
                             const Vector3<double>& force_damping_constants);
 
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
   const Body<T>& bodyA() const { return frameAb_.body(); }
-
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
   const Body<T>& bodyB() const { return frameBa_.body(); }
-
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
   const Frame<T>& frameAb() const { return frameAb_; }
-
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
   const Frame<T>& frameBa() const { return frameBa_; }
 
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
+  // Returns the torque stiffness constants [k₀, k₁, k₂].
   const Vector3<double>& torque_stiffness_constants() const {
     return torque_stiffness_constants_;
   }
 
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
+  // Returns the torque damping constants [b₀, b₁, b₂].
   const Vector3<double>& torque_damping_constants() const {
     return torque_damping_constants_;
   }
 
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
+  // Returns the force stiffness constants [kx, ky, kz].
   const Vector3<double>& force_stiffness_constants() const {
     return force_stiffness_constants_;
   }
 
+  // Returns the force damping constants [bx, by, bz].
   const Vector3<double>& force_damping_constants() const {
     return force_damping_constants_;
   }
 
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
   T CalcPotentialEnergy(
       const systems::Context<T>& context,
       const internal::PositionKinematicsCache<T>& pc) const override;
 
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
   T CalcConservativePower(
       const systems::Context<T>& context,
       const internal::PositionKinematicsCache<T>& pc,
       const internal::VelocityKinematicsCache<T>& vc) const override;
 
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
   T CalcNonConservativePower(
       const systems::Context<T>& context,
       const internal::PositionKinematicsCache<T>& pc,
@@ -126,19 +117,26 @@ class LinearBushingRollPitchYaw final : public ForceElement<T> {
   /// @note: `X_AʙBᴀ.rotation()` is the rotation matrix R_AʙBᴀ that relates
   /// frames Aʙ and Bᴀ whereas `X_AʙBᴀ.translation()` is `p_AʙBᴀ = [x, y, z]`
   /// (the position from Aʙₒ to Bᴀₒ expressed in Aʙ).
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
   math::RigidTransform<T> CalcBushingRigidTransform(
       const systems::Context<T>& context) const {
     return frameBa().CalcPose(context, frameAb());
   }
 
   /// Calculates the bushing's RollPitchYaw from the rotation matrix R_AʙBᴀ that
-  /// relates frames Aʙ and Bᴀ.  Note: `[q₀, q₁, q₂]` is `[yaw, pitch, roll]`.
+  /// relates frames Aʙ and Bᴀ.  Note: `[roll, pitch, yaw]` = `[q₀, q₁, q₂]`.
   /// @param[in] context The state of the multibody system.
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
   math::RollPitchYaw<T> CalcBushingRollPitchYawAngles(
       const systems::Context<T>& context) const {
     return math::RollPitchYaw<T>(CalcBushingRigidTransform(context).rotation());
+  }
+
+  /// Calculates the bushing's RollPitchYaw rates for frame Bᴀ in frame Aʙ.
+  /// @param[in] context The state of the multibody system.
+  /// @retval [ṙoll, ṗitch, ẏaw] = [q̇₀, q̇₁, q̇₂].
+  Vector3<T> CalcBushingRollPitchYawAngleRates(
+      const systems::Context<T>& context) const {
+    const math::RollPitchYaw<T>& rpy = CalcBushingRollPitchYawAngles(context);
+    return CalcBushingRollPitchYawAngleRates(context, rpy);
   }
 
   /// Returns V_AʙBᴀ, frame Bᴀ's spatial velocity in frame Aʙ expressed in Aʙ.
@@ -146,8 +144,8 @@ class LinearBushingRollPitchYaw final : public ForceElement<T> {
   /// @note: `V_AʙBᴀ.rotational()` is w_AʙBᴀ (Bᴀ's angular velocity in frame Aʙ
   /// expressed in Aʙ) whereas `V_AʙBᴀ.translational()` is `v_AʙBᴀₒ = [ẋ, ẏ, ż]`
   /// (point Bᴀₒ's translational velocity in Aʙ, expressed in frame Aʙ).
-  /// Note: w_AʙBᴀ is not equal to either `[q̇₀, q̇₁, q̇₂]` or `[q̇₂, q̇₁, q̇₀]`.
-  /// @exclude_from_pydrake_mkdoc{This overload is not bound in pydrake.}
+  /// Note: w_AʙBᴀ is not equal to `[q̇₀, q̇₁, q̇₂]`.
+  /// @see CalcBushingRollPitchYawAngleRates() for `[q̇₀, q̇₁, q̇₂]`.
   SpatialVelocity<T> CalcBushingSpatialVelocity(
       const systems::Context<T>& context) const {
     return frameBa().CalcSpatialVelocity(context, frameAb(), frameAb());
@@ -155,9 +153,10 @@ class LinearBushingRollPitchYaw final : public ForceElement<T> {
 
   /// Returns F_Ab_Ab, the spatial force on frame Aʙ expressed in frame Aʙ.
   /// @param[in] context The state of the multibody system.
-  /// @note The set of forces exerted on body A by the bushing are replaced by
-  /// a force f_Aʙₒ equal to the set's resultant applied to point Aʙₒ and
-  /// a torque t_Aʙ Aequal to the moment of the set about point Aʙₒ.
+  /// @note F_Ab_Ab is a force/torque representation of the set of all bushing
+  /// forces exerted on body A. In other words, the bushing forces on body A
+  /// are replaced by the set's resultant force f_Aʙₒ applied to point Aʙₒ
+  /// and a torque t_Aʙ equal to the moment of the set about point Aʙₒ.
   /// F_Ab_Ab.rotational() is t_Aʙ whereas F_Ab_Ab.translational() is f_Aʙₒ.
   SpatialForce<T> CalcBushingResultantSpatialForceOnAbo(
       const systems::Context<T>& context) const {
@@ -192,115 +191,84 @@ class LinearBushingRollPitchYaw final : public ForceElement<T> {
   // Calculates the bushing's RollPitchYaw rates for frame Bᴀ in frame Aʙ.
   // @param[in] context The state of the multibody system.
   // @param[in] rpy RollPitchYaw orientation relating frames Aʙ and Bᴀ.
-  // @param[in] is_return_in_yaw_pitch_roll_order is `true` if this method
-  // is to return [q̇₀, q̇₁, q̇₂] = [ẏaw, ṗitch, ṙoll] whereas `false` returns
-  // [q̇₂, q̇₁, q̇₀] = [ṙoll, ṗitch, ẏaw]
+  // @retval `[ṙoll, ṗitch, ẏaw]` = `[q̇₀, q̇₁, q̇₂]`.
   Vector3<T> CalcBushingRollPitchYawAngleRates(
       const systems::Context<T>& context,
-      const math::RollPitchYaw<T>& rpy,
-      const bool is_return_in_yaw_pitch_roll_order) const {
+      const math::RollPitchYaw<T>& rpy) const {
     const Vector3<T> w_AbBa = CalcBushingSpatialVelocity(context).rotational();
-    const Vector3<T> rpyDt = rpy.CalcRpyDtFromAngularVelocityInParent(w_AbBa);
-    return is_return_in_yaw_pitch_roll_order == false ? rpyDt :
-           Vector3<T>(rpyDt(2), rpyDt(1), rpyDt(0));
-  }
-
-  // Calculates the bushing's RollPitchYaw rates for frame Bᴀ in frame Aʙ.
-  // @see related CalcBushingRollPitchYawAngleRates() method for information.
-  Vector3<T> CalcBushingRollPitchYawAngleRates(
-      const systems::Context<T>& context,
-      const bool is_return_in_yaw_pitch_roll_order) const {
-    const math::RollPitchYaw<T>& rpy = CalcBushingRollPitchYawAngles(context);
-    return CalcBushingRollPitchYawAngleRates(context, rpy,
-                                             is_return_in_yaw_pitch_roll_order);
+    return rpy.CalcRpyDtFromAngularVelocityInParent(w_AbBa);
   }
 
   // Convert a torque τ from a non-orthogonal basis related to RollPitchYaw of
-  // τ = τ₀ Aʙz + τ₁ Iy  + τ₂ Bᴀx  to an orthogonal basis of the form
+  // τ = τ₀ Bᴀx + τ₁  Iy + τ₂ Aʙz  to an orthogonal basis of the form
   // τ = τx Aʙx + τy Aʙy + τz Aʙz  where
   // Aʙx, Aʙy, Aʙz are orthogonal unit vectors fixed in frame Aʙ,
   // Bᴀx, Bᴀy, Bᴀz are orthogonal unit vectors fixed in frame Bᴀ, and
-  // Iy = -sin(q₀)*Aʙx + cos(q₀)*Aʙy.  Note: The non-orthogonal basis is related
-  // to a RollPitchYaw rotation sequence - which is a Body-fixed (intrinsic)
-  // Z-Y-X rotation by angles `[yaw = q₀, pitch = q₁, roll = q₂]`.
+  // Iy = -sin(q₂)*Aʙx + cos(q₂)*Aʙy.  Note: The non-orthogonal basis is related
+  // to a Space-fixed XYZ (intrinsic) RollPitchYaw sequence with angles
+  // `[roll = q₀, pitch = q₁, yaw = q₂]`.
   Vector3<T> ConvertTorqueFromNonOrthogonalBasisToFrameAb(
       const Vector3<T>& t_nonOrthogonal_basis,
       const math::RollPitchYaw<T>& rpy) const {
     const T t0 = t_nonOrthogonal_basis(0);
     const T t1 = t_nonOrthogonal_basis(1);
     const T t2 = t_nonOrthogonal_basis(2);
-    const T q0 = rpy.yaw_angle();
     const T q1 = rpy.pitch_angle();
-    const T cos0 = cos(q0), sin0 = sin(q0);
+    const T q2 = rpy.yaw_angle();
     const T cos1 = cos(q1), sin1 = sin(q1);
-    const T tx = cos0 * cos1 * t2 - sin0 * t1;
-    const T ty = cos0 * t1 + sin0 * cos1 * t2;
-    const T tz = t0 - sin1 * t2;
+    const T cos2 = cos(q2), sin2 = sin(q2);
+    const T tx = cos1 * cos2 * t0 - sin2 * t1;
+    const T ty = cos2 * t1 + sin2 * cos1 * t0;
+    const T tz = t2 - sin1 * t0;
     return Vector3<T>(tx, ty, tz);
   }
 
   // Use the bushing's torque stiffness constants and its roll-pitch-yaw angles
   // to form the bushing's stiffness torque τₛ on frame Aʙ.
   // @param[in] context The state of the multibody system.
-  // @param[in] is_express_in_frame_Ab is `true` if this method is to return
-  // τₛ expressed in frame Aʙ whereas `false` returns `[k₀q₀, k₁q₁, k₂q₂]`.
-  // Note: `false` means τₛ is expressed in a non-orthogonal basis as
-  // τₛ = k₀q₀ Aʙz  +  k₁q₁ Iy  + k₂q₂ Bᴀx,
-  // where k₀, k₁, k₂ are torque stiffness constants,
-  // Aʙx, Aʙy, Aʙz are orthogonal unit vectors fixed in frame Aʙ,
-  // Bᴀx, Bᴀy, Bᴀz are orthogonal unit vectors fixed in frame Bᴀ, and
-  // Iy = -sin(q₀)*Aʙx + cos(q₀)*Aʙy.  The non-orthogonal basis Aʙz, Iy, Bᴀx
-  // arises from the RollPitchYaw sequence - which is a Body-fixed Z-Y-X
-  // rotation by angles `[yaw = q₀, pitch = q₁, roll = q₂]`
+  // param[in] is_express_in_frameAb is `true` if this method is to return τₛ
+  // expressed in frame Aʙ whereas `false` returns `[k₀q₀, k₁q₁, k₂q₂]`.
+  // @see ConvertTorqueFromNonOrthogonalBasisToFrameAb() for more information.
   Vector3<T> CalcBushingTorqueStiffnessOnAb(
       const systems::Context<T>& context,
-      const bool is_express_results_in_frame_Ab) const {
+      const bool is_express_in_frameAb) const {
     const math::RollPitchYaw<T> rpy = CalcBushingRollPitchYawAngles(context);
-    const Vector3<T> ypr = rpy.ConvertAnglesToSequenceBodyZYXYawRollPitch();
-    const Vector3<T> torque_kiqi =
-        torque_stiffness_constants().cwiseProduct(ypr);
-    return is_express_results_in_frame_Ab == false ? torque_kiqi :
-           ConvertTorqueFromNonOrthogonalBasisToFrameAb(torque_kiqi, rpy);
+    const Vector3<T> ki_qi =
+        torque_stiffness_constants().cwiseProduct(rpy.vector());
+    return is_express_in_frameAb == false ? ki_qi :
+           ConvertTorqueFromNonOrthogonalBasisToFrameAb(ki_qi, rpy);
   }
 
   // Use the bushing's torque damping constants and its roll-pitch-yaw rates
   // to form the bushing's damping torque τᵣ on frame Aʙ.
   // @param[in] context The state of the multibody system.
-  // @param[in] is_express_in_frame_Ab is `true` if this method is to return
-  // τᵣ expressed in frame Aʙ whereas `false` returns `[b₀q̇₀, b₁q̇₁, b₂q̇₂]`.
-  // @see CalcBushingTorqueStiffnessOnAb() for non-orthogonal basis information.
+  // param[in] is_express_in_frameAb is `true` if this method is to return τᵣ
+  // expressed in frame Aʙ whereas `false` returns `[b₀q̇₀, b₁q̇₁, b₂q̇₂]`.
+  // @see ConvertTorqueFromNonOrthogonalBasisToFrameAb() for more information.
   Vector3<T> CalcBushingTorqueDampingOnAb(
       const systems::Context<T>& context,
-      const bool is_express_results_in_frame_Ab) const {
+      const bool is_express_in_frameAb) const {
     const math::RollPitchYaw<T>& rpy = CalcBushingRollPitchYawAngles(context);
-    const Vector3<T> yprDt =
-        CalcBushingRollPitchYawAngleRates(context, rpy, true);
-    const Vector3<T> torque_biqiDt =
-        torque_damping_constants().cwiseProduct(yprDt);
-    return is_express_results_in_frame_Ab == false ? torque_biqiDt :
-           ConvertTorqueFromNonOrthogonalBasisToFrameAb(torque_biqiDt, rpy);
+    const Vector3<T> rpyDt = CalcBushingRollPitchYawAngleRates(context, rpy);
+    const Vector3<T> bi_qiDt = torque_damping_constants().cwiseProduct(rpyDt);
+    return is_express_in_frameAb == false ? bi_qiDt :
+           ConvertTorqueFromNonOrthogonalBasisToFrameAb(bi_qiDt, rpy);
   }
 
-  // Calculates [kx*x, ky*y, kz*z], the bushing's stiffness force on point Aʙₒ
+  // Returns [kx*x, ky*y, kz*z], the bushing's stiffness force on point Aʙₒ
   // expressed in frame Aʙ.
   // @param[in] context The state of the multibody system.
   Vector3<T> CalcBushingForceStiffnessOnAbo(
       const systems::Context<T>& context) const {
-    // Use the bushing's force stiffness constants [kx, ky, kz] and
-    // `p_AʙBᴀ = [x, y, z]` (the position vector from point Aʙₒ to point Bᴀₒ
-    // expressed in frame Aʙ) to form [kx*x, ky*y, kz*z].
     const Vector3<T> xyz = CalcBushingRigidTransform(context).translation();
     return force_stiffness_constants().cwiseProduct(xyz);
   }
 
-  // Calculates [bx*ẋ, by*ẏ, bz*ż], the bushing's damping force on point Aʙₒ
+  // Returns [bx*ẋ, by*ẏ, bz*ż], the bushing's damping force on point Aʙₒ
   // expressed in frame Aʙ.
   // @param[in] context The state of the multibody system.
   Vector3<T> CalcBushingForceDampingOnAbo(
       const systems::Context<T>& context) const {
-    // Use the bushing's force damping constants [bx, by, bz] and
-    // `v_AʙBᴀₒ = [ẋ, ẏ, ż]` (Bᴀₒ's translational velocity in frame Aʙ
-    // expressed in frame Aʙ) to form [bx*ẋ, by*ẏ, bz*ż].
     const Vector3<T> xyzDt =
         CalcBushingSpatialVelocity(context).translational();
     return force_damping_constants().cwiseProduct(xyzDt);
