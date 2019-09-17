48476b3a4e08603cf82e162f1a21bf4069bc6d30
amcastro-tri <alejandro.castro@tri.global>
2019-09-17 18:11:10 -0400

    Adds ContactResults for continuous MBP. Fixes #10529.

--- multibody/plant/multibody_plant.cc
+++ multibody/plant/multibody_plant.cc
@@ -1088,7 +1088,111 @@ void MultibodyPlant<T>::CopyContactResultsOutput(
 }
 
 template <typename T>
-void MultibodyPlant<T>::CalcContactResults(
+void MultibodyPlant<T>::CalcContactResultsContinuous(
+    const systems::Context<T>& context,
+    ContactResults<T>* contact_results) const {
+  DRAKE_DEMAND(contact_results != nullptr);
+  if (num_collision_geometries() == 0) return;
+
+  const std::vector<PenetrationAsPointPair<T>>& point_pairs =
+      EvalPointPairPenetrations(context);
+
+  std::vector<CoulombFriction<double>> combined_friction_pairs =
+      CalcCombinedFrictionCoefficients(point_pairs);
+
+  const internal::PositionKinematicsCache<T>& pc =
+      EvalPositionKinematics(context);
+  const internal::VelocityKinematicsCache<T>& vc =
+      EvalVelocityKinematics(context);
+
+  contact_results->Clear();
+  for (size_t icontact = 0; icontact < point_pairs.size(); ++icontact) {
+    const auto& pair = point_pairs[icontact];
+    const GeometryId geometryA_id = pair.id_A;
+    const GeometryId geometryB_id = pair.id_B;
+
+    BodyIndex bodyA_index = geometry_id_to_body_index_.at(geometryA_id);
+    BodyIndex bodyB_index = geometry_id_to_body_index_.at(geometryB_id);
+
+    internal::BodyNodeIndex bodyA_node_index =
+        internal_tree().get_body(bodyA_index).node_index();
+    internal::BodyNodeIndex bodyB_node_index =
+        internal_tree().get_body(bodyB_index).node_index();
+
+    // Penetration depth, > 0 during pair.
+    const T& x = pair.depth;
+    DRAKE_ASSERT(x >= 0);
+    const Vector3<T>& nhat_BA_W = pair.nhat_BA_W;
+    const Vector3<T>& p_WCa = pair.p_WCa;
+    const Vector3<T>& p_WCb = pair.p_WCb;
+
+    // Contact point C.
+    const Vector3<T> p_WC = 0.5 * (p_WCa + p_WCb);
+
+    // Contact point position on body A.
+    const Vector3<T>& p_WAo = pc.get_X_WB(bodyA_node_index).translation();
+    const Vector3<T>& p_CoAo_W = p_WAo - p_WC;
+
+    // Contact point position on body B.
+    const Vector3<T>& p_WBo = pc.get_X_WB(bodyB_node_index).translation();
+    const Vector3<T>& p_CoBo_W = p_WBo - p_WC;
+
+    // Separation velocity, > 0  if objects separate.
+    const Vector3<T> v_WAc =
+        vc.get_V_WB(bodyA_node_index).Shift(-p_CoAo_W).translational();
+    const Vector3<T> v_WBc =
+        vc.get_V_WB(bodyB_node_index).Shift(-p_CoBo_W).translational();
+    const Vector3<T> v_AcBc_W = v_WBc - v_WAc;
+
+    // if xdot = vn > 0 ==> they are getting closer.
+    const T vn = v_AcBc_W.dot(nhat_BA_W);
+
+    // Magnitude of the normal force on body A at contact point C.
+    const T k = penalty_method_contact_parameters_.stiffness;
+    const T d = penalty_method_contact_parameters_.damping;
+    const T fn_AC = k * x * (1.0 + d * vn);
+
+    if (fn_AC > 0) {
+      // Normal force on body A, at C, expressed in W.
+      const Vector3<T> fn_AC_W = fn_AC * nhat_BA_W;
+
+      // Compute tangential velocity, that is, v_AcBc projected onto the tangent
+      // plane with normal nhat_BA:
+      const Vector3<T> vt_AcBc_W = v_AcBc_W - vn * nhat_BA_W;
+      // Tangential speed (squared):
+      const T vt_squared = vt_AcBc_W.squaredNorm();
+
+      // Consider a value indistinguishable from zero if it is smaller
+      // then 1e-14 and test against that value squared.
+      const T kNonZeroSqd = 1e-14 * 1e-14;
+      // Tangential friction force on A at C, expressed in W.
+      Vector3<T> ft_AC_W = Vector3<T>::Zero();
+      T slip_velocity = 0;
+      if (vt_squared > kNonZeroSqd) {
+        slip_velocity = sqrt(vt_squared);
+        // Stribeck friction coefficient.
+        const T mu_stribeck = stribeck_model_.ComputeFrictionCoefficient(
+            slip_velocity, combined_friction_pairs[icontact]);
+        // Tangential direction.
+        const Vector3<T> that_W = vt_AcBc_W / slip_velocity;
+
+        // Magnitude of the friction force on A at C.
+        const T ft_AC = mu_stribeck * fn_AC;
+        ft_AC_W = ft_AC * that_W;
+      }
+
+      // Spatial force on body A at C, expressed in the world frame W.
+      const SpatialForce<T> F_AC_W(Vector3<T>::Zero(), fn_AC_W + ft_AC_W);
+
+      const Vector3<T> f_Bc_W = -F_AC_W.translational();
+      contact_results->AddContactInfo(
+          {bodyA_index, bodyB_index, f_Bc_W, p_WC, vn, slip_velocity, pair});
+    }
+  }
+}
+
+template <typename T>
+void MultibodyPlant<T>::CalcContactResultsDiscrete(
     const systems::Context<T>& context,
     ContactResults<T>* contact_results) const {
   DRAKE_DEMAND(contact_results != nullptr);
@@ -1127,8 +1231,8 @@ void MultibodyPlant<T>::CalcContactResults(
     const RotationMatrix<T>& R_WC = R_WC_set[icontact];
 
     // Contact forces applied on B at contact point C.
-    const Vector3<T> f_Bc_C(-ft(2 * icontact), -ft(2 * icontact + 1),
-                            fn(icontact));
+    const Vector3<T> f_Bc_C(ft(2 * icontact), ft(2 * icontact + 1),
+                            -fn(icontact));
     const Vector3<T> f_Bc_W = R_WC * f_Bc_C;
 
     // Slip velocity.
@@ -1143,20 +1247,24 @@ void MultibodyPlant<T>::CalcContactResults(
   }
 }
 
-template<typename T>
+template <typename T>
 void MultibodyPlant<T>::CalcAndAddContactForcesByPenaltyMethod(
-    const systems::Context<T>&,
-    const internal::PositionKinematicsCache<T>& pc,
-    const internal::VelocityKinematicsCache<T>& vc,
-    const std::vector<PenetrationAsPointPair<T>>& point_pairs,
+    const systems::Context<T>& context,
     std::vector<SpatialForce<T>>* F_BBo_W_array) const {
+  DRAKE_DEMAND(F_BBo_W_array != nullptr);
   if (num_collision_geometries() == 0) return;
 
-  std::vector<CoulombFriction<double>> combined_friction_pairs =
-      CalcCombinedFrictionCoefficients(point_pairs);
+  const ContactResults<T>& contact_results = EvalContactResults(context);
 
-  for (size_t icontact = 0; icontact < point_pairs.size(); ++icontact) {
-    const auto& pair = point_pairs[icontact];
+  const internal::PositionKinematicsCache<T>& pc =
+      EvalPositionKinematics(context);
+
+  for (int pair_index = 0;
+       pair_index < contact_results.num_point_pair_contacts(); ++pair_index) {
+    const PointPairContactInfo<T>& contact_info =
+        contact_results.point_pair_contact_info(pair_index);
+    const PenetrationAsPointPair<T>& pair = contact_info.point_pair();
+
     const GeometryId geometryA_id = pair.id_A;
     const GeometryId geometryB_id = pair.id_B;
 
@@ -1168,85 +1276,31 @@ void MultibodyPlant<T>::CalcAndAddContactForcesByPenaltyMethod(
     internal::BodyNodeIndex bodyB_node_index =
         internal_tree().get_body(bodyB_index).node_index();
 
-    // Penetration depth, > 0 during pair.
-    const T& x = pair.depth;
-    DRAKE_ASSERT(x >= 0);
-    const Vector3<T>& nhat_BA_W = pair.nhat_BA_W;
-    const Vector3<T>& p_WCa = pair.p_WCa;
-    const Vector3<T>& p_WCb = pair.p_WCb;
-
     // Contact point C.
-    const Vector3<T> p_WC = 0.5 * (p_WCa + p_WCb);
+    const Vector3<T> p_WC = contact_info.contact_point();
 
     // Contact point position on body A.
-    const Vector3<T>& p_WAo =
-        pc.get_X_WB(bodyA_node_index).translation();
+    const Vector3<T>& p_WAo = pc.get_X_WB(bodyA_node_index).translation();
     const Vector3<T>& p_CoAo_W = p_WAo - p_WC;
 
     // Contact point position on body B.
-    const Vector3<T>& p_WBo =
-        pc.get_X_WB(bodyB_node_index).translation();
+    const Vector3<T>& p_WBo = pc.get_X_WB(bodyB_node_index).translation();
     const Vector3<T>& p_CoBo_W = p_WBo - p_WC;
 
-    // Separation velocity, > 0  if objects separate.
-    const Vector3<T> v_WAc =
-        vc.get_V_WB(bodyA_node_index).Shift(-p_CoAo_W).translational();
-    const Vector3<T> v_WBc =
-        vc.get_V_WB(bodyB_node_index).Shift(-p_CoBo_W).translational();
-    const Vector3<T> v_AcBc_W = v_WBc - v_WAc;
+    const Vector3<T> f_Bc_W = contact_info.contact_force();
+    const SpatialForce<T> F_AC_W(Vector3<T>::Zero(), -f_Bc_W);
 
-    // if xdot = vn > 0 ==> they are getting closer.
-    const T vn = v_AcBc_W.dot(nhat_BA_W);
-
-    // Magnitude of the normal force on body A at contact point C.
-    const T k = penalty_method_contact_parameters_.stiffness;
-    const T d = penalty_method_contact_parameters_.damping;
-    const T fn_AC = k * x * (1.0 + d * vn);
-
-    if (fn_AC > 0) {
-      // Normal force on body A, at C, expressed in W.
-      const Vector3<T> fn_AC_W = fn_AC * nhat_BA_W;
-
-      // Compute tangential velocity, that is, v_AcBc projected onto the tangent
-      // plane with normal nhat_BA:
-      const Vector3<T> vt_AcBc_W = v_AcBc_W - vn * nhat_BA_W;
-      // Tangential speed (squared):
-      const T vt_squared = vt_AcBc_W.squaredNorm();
-
-      // Consider a value indistinguishable from zero if it is smaller
-      // then 1e-14 and test against that value squared.
-      const T kNonZeroSqd = 1e-14 * 1e-14;
-      // Tangential friction force on A at C, expressed in W.
-      Vector3<T> ft_AC_W = Vector3<T>::Zero();
-      if (vt_squared > kNonZeroSqd) {
-        const T vt = sqrt(vt_squared);
-        // Stribeck friction coefficient.
-        const T mu_stribeck = stribeck_model_.ComputeFrictionCoefficient(
-            vt, combined_friction_pairs[icontact]);
-        // Tangential direction.
-        const Vector3<T> that_W = vt_AcBc_W / vt;
-
-        // Magnitude of the friction force on A at C.
-        const T ft_AC = mu_stribeck * fn_AC;
-        ft_AC_W = ft_AC * that_W;
+    if (F_BBo_W_array != nullptr) {
+      if (bodyA_index != world_index()) {
+        // Spatial force on body A at Ao, expressed in W.
+        const SpatialForce<T> F_AAo_W = F_AC_W.Shift(p_CoAo_W);
+        F_BBo_W_array->at(bodyA_node_index) += F_AAo_W;
       }
 
-      // Spatial force on body A at C, expressed in the world frame W.
-      const SpatialForce<T> F_AC_W(Vector3<T>::Zero(),
-                                   fn_AC_W + ft_AC_W);
-
-      if (F_BBo_W_array != nullptr) {
-        if (bodyA_index != world_index()) {
-          // Spatial force on body A at Ao, expressed in W.
-          const SpatialForce<T> F_AAo_W = F_AC_W.Shift(p_CoAo_W);
-          F_BBo_W_array->at(bodyA_node_index) += F_AAo_W;
-        }
-
-        if (bodyB_index != world_index()) {
-          // Spatial force on body B at Bo, expressed in W.
-          const SpatialForce<T> F_BBo_W = -F_AC_W.Shift(p_CoBo_W);
-          F_BBo_W_array->at(bodyB_node_index) += F_BBo_W;
-        }
+      if (bodyB_index != world_index()) {
+        // Spatial force on body B at Bo, expressed in W.
+        const SpatialForce<T> F_BBo_W = -F_AC_W.Shift(p_CoBo_W);
+        F_BBo_W_array->at(bodyB_node_index) += F_BBo_W;
       }
     }
   }
@@ -1438,10 +1492,7 @@ void MultibodyPlant<T>::DoCalcTimeDerivatives(
 
   // Compute contact forces on each body by penalty method.
   if (num_collision_geometries() > 0) {
-    const std::vector<PenetrationAsPointPair<T>>& point_pairs =
-        EvalPointPairPenetrations(context);
-    CalcAndAddContactForcesByPenaltyMethod(
-        context, pc, vc, point_pairs, &F_BBo_W_array);
+    CalcAndAddContactForcesByPenaltyMethod(context, &F_BBo_W_array);
   }
 
   internal_tree().CalcInverseDynamics(
@@ -1652,7 +1703,6 @@ void MultibodyPlant<T>::CalcImplicitStribeckResults(
       implicit_stribeck_solver_->get_generalized_contact_forces();
 }
 
-// TODO(amcastro-tri): Consider splitting this method into smaller pieces.
 template<typename T>
 void MultibodyPlant<T>::DoCalcDiscreteVariableUpdates(
     const drake::systems::Context<T>& context0,
@@ -1914,6 +1964,13 @@ void MultibodyPlant<T>::DeclareCacheEntries() {
       implicit_stribeck_solver_cache_entry.cache_index();
 
   // Cache contact results.
+  // In discrete mode contact forces computation requires to advance the system
+  // from step n to n+1. Therefore they are a function of state and input.
+  // In continuous mode contact forces are simply a function of state.
+  const systems::DependencyTicket& dependency_ticket =
+      is_discrete() ? this->cache_entry_ticket(
+                          cache_indexes_.implicit_stribeck_solver_results)
+                    : this->kinematics_ticket();
   auto& contact_results_cache_entry = this->DeclareCacheEntry(
       std::string("Contact results."),
       []() { return AbstractValue::Make(ContactResults<T>()); },
@@ -1922,12 +1979,13 @@ void MultibodyPlant<T>::DeclareCacheEntries() {
         auto& context = dynamic_cast<const Context<T>&>(context_base);
         auto& contact_results_cache =
             cache_value->get_mutable_value<ContactResults<T>>();
-        this->CalcContactResults(context, &contact_results_cache);
+        if (is_discrete()) {
+          this->CalcContactResultsDiscrete(context, &contact_results_cache);
+        } else {
+          this->CalcContactResultsContinuous(context, &contact_results_cache);
+        }
       },
-      // We explicitly declare the dependence on the implicit Stribeck solver
-      // even though the Eval() above does the evaluation.
-      {this->cache_entry_ticket(
-          cache_indexes_.implicit_stribeck_solver_results)});
+      {dependency_ticket});
   cache_indexes_.contact_results = contact_results_cache_entry.cache_index();
 }
 
