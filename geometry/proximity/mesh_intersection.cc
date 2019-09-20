d7855e049404a46895a47513f36e934d00e58b98
Sean Curtis <sean.curtis@tri.global>
2019-09-20 08:47:50 -0700

    Handle AutoDiffXd for contact surface between rigid and soft meshes
    - Adds documentation for why we *can't* do this.
    - Add some compilation shields against that failure.
    - Add a test confirming compilation and throwing
      - incidental clean up on test.

--- geometry/proximity/mesh_intersection.cc
+++ geometry/proximity/mesh_intersection.cc
@@ -4,6 +4,16 @@ namespace drake {
 namespace geometry {
 namespace mesh_intersection {
 
+std::unique_ptr<ContactSurface<AutoDiffXd>>
+ComputeContactSurfaceFromSoftVolumeRigidSurface(
+    const GeometryId, const VolumeMeshField<double, double>&,
+    const math::RigidTransform<AutoDiffXd>&, const GeometryId,
+    const SurfaceMesh<double>&, const math::RigidTransform<AutoDiffXd>&) {
+  throw std::logic_error(
+      "ContactSurface for AutoDiff-valued Compliant-Rigid meshes are not "
+      "currently supported with AutoDiffXd");
+}
+
 }  // namespace mesh_intersection
 }  // namespace geometry
 }  // namespace drake
