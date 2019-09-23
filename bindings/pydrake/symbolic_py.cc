1b4549b1741f1f5fd25cded0da2d3f7950ece434
Soonho Kong <soonho.kong@tri.global>
2019-09-23 13:06:09 -0400

    Suppress "self-assign-overloaded" warnings for Apple LLVM >= 10.0.1

--- bindings/pydrake/symbolic_py.cc
+++ bindings/pydrake/symbolic_py.cc
@@ -18,7 +18,8 @@
 // that is using py::self (example: `def(py::self + py::self)`).
 // Here, we suppress the warning using `#pragma diagnostic`.
 #if (__APPLE__) && (__clang__) && (__clang_major__ >= 10) && \
-    (__clang_minor__ >= 0) && (__clang_patchlevel__ >= 1)
+    !((__clang_major__ == 10) && (__clang_minor__ == 0) &&   \
+        (__clang_patchlevel__ == 0))
 #pragma GCC diagnostic ignored "-Wself-assign-overloaded"
 #endif
 
