e75c83ae693c58c51508dd1b466d4bf70b4c4bac
Jamie Snape <jamie.snape@kitware.com>
2019-10-21 15:59:17 -0400

    Fix tests that call the compiler directly on macOS

--- tools/cc_toolchain/print_host_settings.sh
+++ tools/cc_toolchain/print_host_settings.sh
@@ -2,6 +2,12 @@
 # This should only be invoked via Bazel.
 set -eux
 
+# TODO(jamiesnape): Determine this information from Bazel.
+if [[ "$(uname -s)" == Darwin ]]; then
+  export DEVELOPER_DIR="$(xcode-select --print-path)"
+  export SDKROOT="$(xcrun --show-sdk-path)"
+fi
+
 capture_cc_env="$1"
 source "$capture_cc_env"
 
