bd61488d529e144846784ebabcf3670572cc9aa8
Jamie Snape <jamie.snape@kitware.com>
2019-10-25 13:03:47 -0400

    Remove support for C++14 and require C++17

--- attic/multibody/rigid_body_tree_alias_groups.cc
+++ attic/multibody/rigid_body_tree_alias_groups.cc
@@ -2,13 +2,6 @@
 
 #include <set>
 
-#if  __cplusplus < 201703L
-template <typename T>
-constexpr char RigidBodyTreeAliasGroups<T>::kBodyGroupsKeyword[];
-template <typename T>
-constexpr char RigidBodyTreeAliasGroups<T>::kJointGroupsKeyword[];
-#endif
-
 namespace {
 // Inserts @p vec into @p mapping if @p key does not exist, or appends @p vec
 // to the existing vector in @p map. This function also guarantees the newly
