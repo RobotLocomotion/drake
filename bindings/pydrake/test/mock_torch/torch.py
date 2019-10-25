c8ef1c85340cee8adf90915ad56a8ece8567163a
Jamie Snape <jamie.snape@kitware.com>
2019-10-25 19:52:16 -0400

    Remove most usage of six

--- bindings/pydrake/test/mock_torch/torch.py
+++ bindings/pydrake/test/mock_torch/torch.py
@@ -2,16 +2,11 @@
 # simulating the following line:
 # https://github.com/pytorch/pytorch/blob/v1.0.0/torch/__init__.py#L75
 
+import os
 import sys
-
-import six
-if six.PY2:
-    import ctypes as _dl_flags
-else:
-    import os as _dl_flags
 
 
 # Make the check in `pydrake/__init__.py` pass, but then undo the change.
 _old_flags = sys.getdlopenflags()
-sys.setdlopenflags(_dl_flags.RTLD_GLOBAL)
+sys.setdlopenflags(os.RTLD_GLOBAL)
 sys.setdlopenflags(_old_flags)
