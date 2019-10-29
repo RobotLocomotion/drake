4371dca5af474a2e70b102c424e53bff543a2beb
Jamie Snape <jamie.snape@kitware.com>
2019-10-29 16:56:40 -0400

    Deprecate drake_optional.h header

--- common/drake_optional.h
+++ common/drake_optional.h
@@ -1,14 +1,20 @@
 #pragma once
 
+// NOLINTNEXTLINE(whitespace/line_length)
+#warning "drake/common/drake_optional.h" and drake::optional are deprecated and will be removed on or after 2020-02-01. Please use <optional> and std::optional instead.
+
 /// @file
-/// Provides drake::optional as an alias for std::optional.
+/// Provides drake::optional as a deprecated alias for std::optional.
 
 #include <optional>
+
+#include "drake/common/drake_deprecated.h"
 
 namespace drake {
 
 template <typename T>
 using optional = std::optional<T>;
+DRAKE_DEPRECATED("2020-02-01", "Please use std::nullopt instead.")
 constexpr auto nullopt = std::nullopt;
 using nullopt_t = std::nullopt_t;
 
