d6db06961fad18351d2e19fa0d6b2a64a5570a15
Belinda Teh <belinda.teh@tri.global>
2019-10-30 14:39:13 -0700

    Render capsule in vtk and ospray engines
    Represented using a cylinder and two spheres

---
+++ geometry/render/render_engine_vtk_base.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <vtkAppendPolyData.h>
+#include <vtkCylinderSource.h>
+#include <vtkSmartPointer.h>
+#include <vtkSphereSource.h>
+#include <vtkTransform.h>
+#include <vtkTransformPolyDataFilter.h>
+
+namespace drake {
+namespace geometry {
+namespace render {
+
+// Creates a capsule by appending a cylinder and two spheres.
+void CreateVtkCapsule(vtkSmartPointer<vtkAppendPolyData> append_filter,
+                      double radius, double length);
+
+// Sets common sphere options such as its dimensions and resolution.
+void SetSphereOptions(vtkSphereSource* vtk_sphere, double radius);
+
+// Sets common cylinder options such as its dimensions and resolution.
+void SetCylinderOptions(vtkCylinderSource* vtk_cylinder, double height,
+                        double radius);
+
+// Transforms a vtk cylinder that is y-axis aligned to be z-axis aligned, which
+// is what Drake uses.
+void TransformToDrakeCylinder(vtkTransform* transform,
+                              vtkTransformPolyDataFilter* transform_filter,
+                              vtkCylinderSource* vtk_cylinder);
+
+}  // namespace render
+}  // namespace geometry
+}  // namespace drake
