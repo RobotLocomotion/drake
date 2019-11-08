962fa5f9512c6eca81f58e6329320104d2961ad4
Belinda Teh <belinda.teh@tri.global>
2019-11-08 14:21:39 -0800

    Add BoundingVolumeHierarchy class
    - add RigidMesh struct layer to hold the bvh, similar to the SoftMesh struct layer
    - create AABB tree with first pass on naive splitting, optimisations to come

---
+++ geometry/proximity/bounding_volume_hierarchy.h
@@ -0,0 +1,200 @@
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "drake/common/drake_assert.h"
+#include "drake/common/drake_copyable.h"
+#include "drake/common/drake_nodiscard.h"
+#include "drake/common/eigen_types.h"
+#include "drake/common/reset_on_copy.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+
+/** Axis-aligned bounding box used in BoundingVolumeHierarchy.  */
+class AABB {
+ public:
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(AABB)
+
+  /**
+   @pre halfwidth.x(), halfwidth.y(), halfwidth.z() are not negative.
+  */
+  AABB(const Vector3<double> center, const Vector3<double> halfwidth)
+      : center_(std::move(center)), halfwidth_(std::move(halfwidth)) {
+    DRAKE_DEMAND(halfwidth.x() >= 0.0);
+    DRAKE_DEMAND(halfwidth.y() >= 0.0);
+    DRAKE_DEMAND(halfwidth.z() >= 0.0);
+  }
+
+  /** Returns the center. */
+  const Vector3<double>& center() const { return center_; }
+
+  /** Returns the halfwidth. */
+  const Vector3<double>& halfwidth() const { return halfwidth_; }
+
+  /** @return Volume of the bounding box.  */
+  double CalcVolume() {
+    return halfwidth_[0] * 2 * halfwidth_[1] * 2 * halfwidth_[2] * 2;
+  }
+
+ private:
+  /** Center point of the box.  */
+  Vector3<double> center_;
+  /** Halfwidth extents along each axes.  */
+  Vector3<double> halfwidth_;
+};
+
+/** Node of the tree structure representing the BoundingVolumeHierarchy.  */
+template <class MeshType>
+struct BVNode {
+  AABB aabb;
+  std::unique_ptr<BVNode<MeshType>> left;
+  std::unique_ptr<BVNode<MeshType>> right;
+  /** Leaf node's link to the actual mesh element.  */
+  typename MeshType::ElementIndex e;
+};
+
+/** BoundingVolumeHierarchy groups mesh elements within bounding volumes to
+ support broad-phase operations. The underlying structure is a binary tree of
+ axis-aligned bounding boxes (AABB). Leaf nodes contain an index into elements
+ of the mesh. The BVH needs a reference to the mesh in order to build the tree,
+ but does not own the mesh.
+ @pre    Assumes that the mesh remains static and is not mutable.
+ @tparam MeshType SurfaceMesh<double> or VolumeMesh<double> (Exotic types like
+         SurfaceMesh<AutoDiffXd> are not supported).
+*/
+template <class MeshType>
+class BoundingVolumeHierarchy {
+ public:
+  using CentroidPair =
+      std::pair<typename MeshType::ElementIndex, Vector3<double>>;
+
+  BoundingVolumeHierarchy(const BoundingVolumeHierarchy& bvh) { *this = bvh; }
+
+  BoundingVolumeHierarchy& operator=(const BoundingVolumeHierarchy& bvh) {
+    if (&bvh == this) return *this;
+
+    num_vertices_per_element_ = bvh.num_vertices_per_element_;
+    // TODO(tehbelinda): Decide on copy semantics such as deep copy support.
+    mesh_ = nullptr;
+    bv_tree_ = nullptr;
+
+    return *this;
+  }
+
+  BoundingVolumeHierarchy(BoundingVolumeHierarchy&&) = default;
+  BoundingVolumeHierarchy& operator=(BoundingVolumeHierarchy&&) = default;
+
+  explicit BoundingVolumeHierarchy(const MeshType* mesh)
+      : mesh_(mesh), num_vertices_per_element_(MeshType::kDim + 1) {
+    DRAKE_DEMAND(mesh_ != nullptr);
+
+    // Generate element indices and corresponding centroids. These are used
+    // for calculating the split point of the volumes.
+    const int num_elements = mesh_->num_elements();
+    std::vector<CentroidPair> element_centroids;
+    for (typename MeshType::ElementIndex i(0); i < num_elements; ++i) {
+      Vector3<double> centroid = ComputeCentroid(i);
+      element_centroids.push_back(std::pair(i, centroid));
+    }
+
+    bv_tree_ = BuildBVTree(element_centroids.begin(), element_centroids.end());
+  }
+
+  const MeshType& mesh() const { return *mesh_; }
+
+ private:
+  // Convenience class for testing.
+  friend class BVHTester;
+
+  std::unique_ptr<BVNode<MeshType>> BuildBVTree(
+      const typename std::vector<CentroidPair>::iterator start,
+      const typename std::vector<CentroidPair>::iterator end) {
+    // Generate bounding volume.
+    const AABB aabb = ComputeBoundingVolume(start, end);
+
+    auto node = std::make_unique<BVNode<MeshType>>(BVNode<MeshType>({aabb}));
+    const int num_elements = end - start;
+    if (num_elements == 1) {
+      // Store element index in this leaf node.
+      node->e = start->first;
+    } else {
+      // Sort by centroid along the x-axis for now.
+      // TODO(tehbelinda): Use a better method for splitting, e.g. checking all
+      // axes / surface area / etc. Right now the sorting is redundant since
+      // we are sorting with the same parameters at every depth. This will soon
+      // change with the improvements, for example, sorting by different axes
+      // at different depths if we choose the axis with the greatest spread.
+      std::sort(start, end, [](const CentroidPair& a, const CentroidPair& b) {
+        return a.second[0] < b.second[0];
+      });
+
+      // Continue with the next branches.
+      const typename std::vector<CentroidPair>::iterator mid =
+          start + num_elements / 2;
+      node->left = BuildBVTree(start, mid);
+      node->right = BuildBVTree(mid, end);
+    }
+    return node;
+  }
+
+  const AABB ComputeBoundingVolume(
+      const typename std::vector<CentroidPair>::iterator start,
+      const typename std::vector<CentroidPair>::iterator end) const {
+    // Keep track of the min/max bounds to create the bounding box.
+    Vector3<double> max_bounds =
+        std::numeric_limits<double>::lowest() * Vector3<double>::Ones();
+    Vector3<double> min_bounds =
+        std::numeric_limits<double>::max() * Vector3<double>::Ones();
+
+    // Check each mesh element in the given range.
+    for (auto pair = start; pair < end; ++pair) {
+      const auto& element = mesh_->element(pair->first);
+      // Check each vertex in the element.
+      for (int v = 0; v < num_vertices_per_element_; ++v) {
+        const auto& vertex = mesh_->vertex(element.vertex(v)).r_MV();
+        // Compare its extent along each of the 3 axes.
+        min_bounds = min_bounds.cwiseMin(vertex);
+        max_bounds = max_bounds.cwiseMax(vertex);
+      }
+    }
+    const Vector3<double> center = (min_bounds + max_bounds) / 2;
+    const Vector3<double> halfwidth = max_bounds - center;
+    return AABB(center, halfwidth);
+  }
+
+  // TODO(tehbelinda): Move this funtion into SurfaceMesh/VolumeMesh directly
+  // and rename to CalcElementCentroid(ElementIndex).
+  Vector3<double> ComputeCentroid(
+      const typename MeshType::ElementIndex i) const {
+    Vector3<double> centroid{0, 0, 0};
+    const auto& element = mesh_->element(i);
+    // Calculate average from all vertices.
+    for (int v = 0; v < num_vertices_per_element_; ++v) {
+      const auto& vertex = mesh_->vertex(element.vertex(v)).r_MV();
+      centroid += vertex;
+    }
+    centroid /= num_vertices_per_element_;
+    return centroid;
+  }
+
+  // We use `reset_on_copy` so that the default copy constructor resets
+  // the pointer to null when a BoundingVolumeHierarchy is copied. This is
+  // currently redundant with the explicit null in the current copy constructor
+  // (via assignment operator) while we are still working on copy semantics.
+  reset_on_copy<const MeshType*> mesh_;
+
+  int num_vertices_per_element_;
+
+  std::unique_ptr<BVNode<MeshType>> bv_tree_;
+};
+
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
---
+++ geometry/proximity/test/bounding_volume_hierarchy_test.cc
@@ -0,0 +1,172 @@
+#include "drake/geometry/proximity/bounding_volume_hierarchy.h"
+
+#include <memory>
+
+#include <gtest/gtest.h>
+
+#include "drake/geometry/proximity/make_sphere_mesh.h"
+#include "drake/geometry/proximity/surface_mesh.h"
+#include "drake/geometry/shape_specification.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+
+// Friend class for accessing BoundingVolumeHierarchy's protected/private
+// functionality.
+class BVHTester {
+ public:
+  BVHTester() = delete;
+
+  template <class MeshType>
+  static Vector3<double> ComputeCentroid(
+      const BoundingVolumeHierarchy<MeshType>& bvh,
+      const typename MeshType::ElementIndex i) {
+    return bvh.ComputeCentroid(i);
+  }
+
+  template <class MeshType>
+  static AABB ComputeBoundingVolume(
+      const BoundingVolumeHierarchy<MeshType>& bvh,
+      const typename std::vector<
+          typename BoundingVolumeHierarchy<MeshType>::CentroidPair>::iterator
+          start,
+      const typename std::vector<
+          typename BoundingVolumeHierarchy<MeshType>::CentroidPair>::iterator
+          end) {
+    return bvh.ComputeBoundingVolume(start, end);
+  }
+
+  template <class MeshType>
+  static BVNode<MeshType>* GetBVTree(
+      const BoundingVolumeHierarchy<MeshType>& bvh) {
+    return bvh.bv_tree_.get();
+  }
+
+  template <class MeshType>
+  static int GetNumVerticesPerElement(
+      const BoundingVolumeHierarchy<MeshType>& bvh) {
+    return bvh.num_vertices_per_element_;
+  }
+};
+
+namespace {
+
+// Test fixture class for testing BoundingVolumeHierarchy. Uses a coarse
+// sphere, i.e. an octahedron, as the underlying mesh.
+class BVHTest : public ::testing::Test {
+ public:
+  BVHTest()
+      : ::testing::Test(),
+        mesh_(MakeSphereSurfaceMesh<double>(Sphere(1.5), 3)),
+        bvh_(BoundingVolumeHierarchy<SurfaceMesh<double>>(&mesh_)) {}
+
+ protected:
+  SurfaceMesh<double> mesh_;
+  BoundingVolumeHierarchy<SurfaceMesh<double>> bvh_;
+};
+
+// Tests computing the centroid of an element.
+TEST_F(BVHTest, TestComputeCentroid) {
+  Vector3<double> centroid = BVHTester::ComputeCentroid<SurfaceMesh<double>>(
+      bvh_, SurfaceFaceIndex(0));
+  // The first face of our octahedron with radius 1.5 is a triangle with
+  // vertices at 1.5 along each axis, so its centroid should average out to
+  // 1.5 / 3 = 0.5.
+  EXPECT_EQ(centroid, Vector3<double>(0.5, 0.5, 0.5));
+}
+
+// Tests computing the bounding volume of elements.
+TEST_F(BVHTest, TestComputeBoundingVolume) {
+  std::vector<BoundingVolumeHierarchy<SurfaceMesh<double>>::CentroidPair>
+      element_centroids{
+          std::pair(SurfaceFaceIndex(0), Vector3<double>(0.5, 0.5, 0.5))};
+  AABB aabb = BVHTester::ComputeBoundingVolume<SurfaceMesh<double>>(
+      bvh_, element_centroids.begin(), element_centroids.end());
+  // The first face of our octahedron with radius 1.5 is a triangle with
+  // vertices at 1.5 along each axis, so the bounding box should cover a volume
+  // of 0 to 1.5. Thus it should have a center of 1.5 / 2 = 0.75 and halfwidths
+  // of 0.75.
+  EXPECT_EQ(aabb.center(), Vector3<double>(0.75, 0.75, 0.75));
+  EXPECT_EQ(aabb.halfwidth(), Vector3<double>(0.75, 0.75, 0.75));
+
+  // Add all the elements from the octahedron to test multiple elements. The
+  // centroid values are irrelevant for this test, but the bounding box should
+  // now encompass the whole sphere with a center of 0 and halfwidth of 1.5.
+  for (int i = 1; i < bvh_.mesh().num_elements(); ++i) {
+    element_centroids.push_back(
+        std::pair(SurfaceFaceIndex(i), Vector3<double>(0.5, 0.5, 0.5)));
+  }
+  aabb = BVHTester::ComputeBoundingVolume<SurfaceMesh<double>>(
+      bvh_, element_centroids.begin(), element_centroids.end());
+  EXPECT_EQ(aabb.center(), Vector3<double>(0., 0., 0.));
+  EXPECT_EQ(aabb.halfwidth(), Vector3<double>(1.5, 1.5, 1.5));
+}
+
+// Tests properties from building the bounding volume tree.
+TEST_F(BVHTest, TestBuildBVTree) {
+  // Since its a binary tree with a single element at each leaf node, the tree
+  // depth can be found using 2^d = num_elements. The octahedron has 8 elements
+  // so we should end up with a balanced tree of depth 3 where each level's
+  // volume is less than its parent's volume.
+  BVNode<SurfaceMesh<double>>* bv_tree = BVHTester::GetBVTree(bvh_);
+  int num_elements = mesh_.num_elements();
+  std::function<void(BVNode<SurfaceMesh<double>>*, int, double)> check_node;
+  check_node = [&check_node, num_elements](BVNode<SurfaceMesh<double>>* node,
+                                           int depth, double volume) -> void {
+    if (depth < 3) {
+      EXPECT_NE(node->left, nullptr);
+      EXPECT_NE(node->right, nullptr);
+
+      // Check each of the branches at increasing depth.
+      double left_volume = node->left->aabb.CalcVolume();
+      EXPECT_LE(left_volume, volume);
+      check_node(node->left.get(), depth + 1, left_volume);
+      double right_volume = node->right->aabb.CalcVolume();
+      EXPECT_LE(right_volume, volume);
+      check_node(node->right.get(), depth + 1, right_volume);
+    } else {
+      // At depth 3, we should reach the leaf node with a valid element instead
+      // of more branches.
+      EXPECT_EQ(node->left, nullptr);
+      EXPECT_EQ(node->right, nullptr);
+      EXPECT_GE(node->e, 0);
+      EXPECT_LT(node->e, num_elements);
+    }
+  };
+  check_node(bv_tree, 0, bv_tree->aabb.CalcVolume());
+}
+
+// Tests copy constructor.
+GTEST_TEST(BoundingVolumeHierarchyTest, TestCopy) {
+  const Sphere sphere(1.5);
+  const double edge_length = 3 * sphere.get_radius();
+  auto mesh = std::make_unique<SurfaceMesh<double>>(
+      MakeSphereSurfaceMesh<double>(sphere, edge_length));
+  BoundingVolumeHierarchy<SurfaceMesh<double>> original(mesh.get());
+
+  // Copy constructor.
+  BoundingVolumeHierarchy<SurfaceMesh<double>> copy(original);
+
+  // Confirm that it's a copy.
+  // TODO(tehbelinda): Update test expectations pending copy semantics.
+  EXPECT_NE(&original.mesh(), &copy.mesh());
+  EXPECT_EQ(&copy.mesh(), nullptr);
+  EXPECT_NE(BVHTester::GetBVTree(original), BVHTester::GetBVTree(copy));
+  EXPECT_EQ(BVHTester::GetBVTree(copy), nullptr);
+  EXPECT_EQ(BVHTester::GetNumVerticesPerElement(original),
+            BVHTester::GetNumVerticesPerElement(copy));
+}
+
+// Tests calculating the bounding box volume.
+GTEST_TEST(AABBTest, TestVolume) {
+  AABB aabb = AABB(Vector3<double>(-1, 2, 1), Vector3<double>(2, 0.5, 2.7));
+  EXPECT_EQ(aabb.CalcVolume(), 21.6);
+  AABB zero_aabb = AABB(Vector3<double>(0, 0, 0), Vector3<double>(0, 0, 0));
+  EXPECT_EQ(zero_aabb.CalcVolume(), 0);
+}
+
+}  // namespace
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
