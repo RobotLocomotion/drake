78b5f2706dd6063cf459fba3db740ad795dbeeeb
Evan Drumwright <edrumwri@gmail.com>
2019-11-15 14:22:15 -0600

    Final update for PR.

--- systems/framework/diagram.h
+++ systems/framework/diagram.h
@@ -810,8 +810,8 @@ class Diagram : public System<T>, internal::SystemParentServiceInterface {
       // possible that the system implements DoMapVelocityToQDot() even when it
       // has no velocity variables; this can be the case when the system can be
       // run in both discrete and continuous modes. So we skip this operation
-      // when there are no position variables to allow MapVelocityToQdot() to be
-      // used in both discrete and continuous modes.
+      // when there are no position variables so that we may allow
+      // MapVelocityToQdot() to be used in both discrete and continuous modes.
       const int num_v = sub_xc.get_generalized_velocity().size();
       if (num_v == 0) continue;
       const Eigen::Ref<const VectorX<T>>& v_slice =
@@ -863,8 +863,8 @@ class Diagram : public System<T>, internal::SystemParentServiceInterface {
       // the system implements DoMapQDotToVelocity() even when it has no
       // position variables; this can be the case when the system can be run
       // in both discrete and continuous modes. So we skip this operation when
-      // there are no position variables to allow MapQDotToVelocity() to be used
-      // in both discrete and continuous modes.
+      // there are no position variables so that we may allow
+      // MapQDotToVelocity() to be used in both discrete and continuous modes.
       const int num_q = sub_xc.get_generalized_position().size();
       if (num_q == 0) continue;
       const Eigen::Ref<const VectorX<T>>& dq_slice =
