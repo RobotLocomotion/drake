8a88f7830bfc7d4ef55ee550c4f6743ba15c1d87
Mmanu Chaturvedi <mmanu.chaturvedi@gmail.com>
2019-11-18 17:02:48 -0500

    Fix whitespace and remove binary dep

--- tools/pybind11_coverage/get_pybind_coverage.py
+++ tools/pybind11_coverage/get_pybind_coverage.py
@@ -21,7 +21,7 @@ def main():
     args = parse_arguments()
     pybind_strings = \
         libclang_parser.get_docstring_for_bindings(args.pybind_source_files)
-    
+
     class_coverage = parse_pybind_xml_docstrings.ClassCoverage(
             args.xml_docstrings, pybind_strings, args.class_coverage)
     class_coverage.get_coverage()
