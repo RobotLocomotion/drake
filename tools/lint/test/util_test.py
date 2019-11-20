bcbc9d1c34a0b4eb66453b86a27433f05d5addd6
Jeremy Nimmer <jeremy.nimmer@tri.global>
2019-11-20 11:11:50 -0500

    lint: Make find_all_sources more paranoid

--- tools/lint/test/util_test.py
+++ tools/lint/test/util_test.py
@@ -6,22 +6,21 @@ from drake.tools.lint.util import find_all_sources
 
 class UtilTest(unittest.TestCase):
 
-    @unittest.skipIf(
-        os.path.exists("/.dockerenv"),
-        "Path lengths are often shorter than 10 in Docker containers")
     def test_find(self):
         workspace_dir, relpaths = find_all_sources("drake")
 
         # Sanity-check workspace_dir.
-        self.assertGreater(len(workspace_dir), 10)
+        self.assertTrue(workspace_dir.startswith("/"), workspace_dir)
+        self.assertTrue(os.path.isdir(workspace_dir), workspace_dir)
         workspace = os.path.join(workspace_dir, "WORKSPACE")
-        self.assertTrue(os.path.exists(workspace))
+        self.assertTrue(os.path.exists(workspace), workspace)
         with open(workspace, "r") as workspace_contents:
             workspace_lines = workspace_contents.readlines()
         self.assertTrue('workspace(name = "drake")\n' in workspace_lines)
 
         # Sanity-check relpaths.
-        self.assertGreater(len(relpaths), 1000)
+        self.assertGreater(len(relpaths), 1_000)
+        self.assertLess(len(relpaths), 10_000)
         self.assertTrue('.bazelproject' in relpaths)
         self.assertTrue('setup/ubuntu/install_prereqs.sh' in relpaths)
         THIRD_PARTY_SOURCES_ALLOWED_TO_BE_FOUND = [
