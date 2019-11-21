3f1547f426e3b0d8ed6094bf6c36b6c57a711927
Eric Cousineau <eric.cousineau@tri.global>
2019-11-21 17:39:12 -0500

    geometry_properties: Permit type erasure (for Python)

--- geometry/geometry_properties.cc
+++ geometry/geometry_properties.cc
@@ -23,14 +23,67 @@ std::set<std::string> GeometryProperties::GetGroupNames() const {
   return group_names;
 }
 
+void GeometryProperties::AddPropertyAbstract(
+    const std::string& group_name, const std::string& name,
+    const AbstractValue& value) {
+  auto iter = values_.find(group_name);
+  if (iter == values_.end()) {
+    auto result = values_.insert({group_name, Group{}});
+    DRAKE_DEMAND(result.second);
+    iter = result.first;
+  }
+
+  Group& group = iter->second;
+  auto value_iter = group.find(name);
+  if (value_iter == group.end()) {
+    group[name] = value.Clone();
+    return;
+  }
+  throw std::logic_error(fmt::format(
+      "AddProperty(): Trying to add property '{}' to group '{}'; "
+      "a property with that name already exists",
+      name, group_name));
+}
+
 bool GeometryProperties::HasProperty(const std::string& group_name,
                                      const std::string& name) const {
-  const auto iter = values_.find(group_name);
-  if (iter != values_.end()) {
-    const Group& group = iter->second;
-    return group.count(name) > 0;
+  return GetPropertyAbstractMaybe(group_name, name, false) != nullptr;
+}
+
+const AbstractValue& GeometryProperties::GetPropertyAbstract(
+    const std::string& group_name, const std::string& name) const {
+  const AbstractValue* abstract = GetPropertyAbstractMaybe(
+      group_name, name, true);
+  if (!abstract) {
+    throw std::logic_error(
+        fmt::format("GetProperty(): There is no property '{}' in group '{}'.",
+                    name, group_name));
   }
-  return false;
+  return *abstract;
+}
+
+const AbstractValue* GeometryProperties::GetPropertyAbstractMaybe(
+    const std::string& group_name, const std::string& name,
+    bool throw_for_bad_group) const {
+  const auto iter = values_.find(group_name);
+  if (iter == values_.end()) {
+    if (throw_for_bad_group) {
+      throw std::logic_error(
+          fmt::format(
+              "GetProperty(): Trying to read property '{}' from group "
+              "'{}'. But the group does not exist.",
+              name, group_name));
+    } else {
+      return nullptr;
+    }
+  }
+  const Group& group = iter->second;
+  const auto value_iter = group.find(name);
+  if (value_iter != group.end()) {
+    return value_iter->second.get();
+  } else {
+    return nullptr;
+  }
 }
 
 std::ostream& operator<<(std::ostream& out, const GeometryProperties& props) {
