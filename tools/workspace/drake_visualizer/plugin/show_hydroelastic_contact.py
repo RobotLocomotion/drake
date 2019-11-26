14db0d7617d711e5d16c0d3a8ad3424afeff84ca
Damrong Guoy <42557859+DamrongGuoy@users.noreply.github.com>
2019-11-26 15:34:03 -0800

    tools: fix show_hydroelastic_contact plugin of drake_visualizer (#12417)

--- tools/workspace/drake_visualizer/plugin/show_hydroelastic_contact.py
+++ tools/workspace/drake_visualizer/plugin/show_hydroelastic_contact.py
@@ -12,8 +12,8 @@ from PythonQt import QtCore, QtGui
 import drake as lcmdrakemsg
 
 from drake.tools.workspace.drake_visualizer.plugin import scoped_singleton_func
-from show_point_pair_contact import ContactVisModes
-
+from drake.tools.workspace.drake_visualizer.plugin.show_point_pair_contact \
+     import ContactVisModes
 
 # TODO(seancurtis-TRI) Make the dialog box for scaling force arrows in
 # show_point_pair_contact.py accessible to this plugin too.
