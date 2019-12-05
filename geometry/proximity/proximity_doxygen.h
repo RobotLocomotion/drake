cf49e812a8aa1e8e1c15ee7d713986b9476f4241
Belinda Teh <belinda.teh@tri.global>
2019-12-04 16:43:57 -0800

    Benchmark vtk and ospray render engines
    - Add API for manually configuring light position
    - Rework geometry documentation and add benchmark comparison data
    
    Co-authored-by: Sean Curtis <sean.curtis@tri.global>

---
+++ geometry/proximity/proximity_doxygen.h
@@ -0,0 +1,21 @@
+namespace drake {
+namespace geometry {
+namespace proximity {
+
+/** @addtogroup proximity_queries
+
+ Proximity queries span a range of types, including:
+
+   - penetration
+   - distance
+   - ray-intersection
+
+ Not all shape queries or scalar types are fully supported.
+
+ @note This page is still under construction.
+
+ Next topic: @ref render_engines  */
+
+}  // namespace proximity
+}  // namespace geometry
+}  // namespace drake
