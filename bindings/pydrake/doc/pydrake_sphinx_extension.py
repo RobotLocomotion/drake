e0a54c9ba123a04ba2eee4d2705248045e0a3933
Mmanu Chaturvedi <mmanu.chaturvedi@gmail.com>
2019-12-05 13:48:48 -0500

    'Fix' sphinx to get __init__ documentation.

--- bindings/pydrake/doc/pydrake_sphinx_extension.py
+++ bindings/pydrake/doc/pydrake_sphinx_extension.py
@@ -246,6 +246,8 @@ def autodoc_skip_member(app, what, name, obj, skip, options):
     # did not work. Revisit this at some point.
     if "__del__" in name:
         return True
+    if "__init__" in name:
+        return False
     return None
 
 
