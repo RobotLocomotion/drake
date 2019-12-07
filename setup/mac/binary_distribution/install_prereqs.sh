80ab816661b21335a2de1b42fb0b8d1f827e7a74
Soonho Kong <soonho.kong@tri.global>
2019-12-07 10:42:57 -0500

    Pass --no-lock to brew bundle
    https://github.com/Homebrew/homebrew-bundle/pull/552 introduces
    "Brewfile.lock.json" which provides a list of successfully installed packages
    and their metainfo.
    
    For now, git detects those new added "Brewfile.lock.json" as untracked
    files. Passing `--no-lock` is one solution to this problem. An alternative is to
    add the pattern in `.gitignore` but I think passing `--no-lock` is a cleaner
    solution.

--- setup/mac/binary_distribution/install_prereqs.sh
+++ setup/mac/binary_distribution/install_prereqs.sh
@@ -24,7 +24,7 @@ fi
 # TODO(jamiesnape): Remove two lines uninstalling dreal on or after 2020-02-01.
 brew uninstall --force dreal
 brew untap dreal/dreal &>/dev/null || true
-/usr/local/bin/brew bundle --file="${BASH_SOURCE%/*}/Brewfile"
+/usr/local/bin/brew bundle --file="${BASH_SOURCE%/*}/Brewfile" --no-lock
 
 if ! command -v /usr/local/bin/pip3 &>/dev/null; then
   echo 'ERROR: pip3 is NOT installed. The post-install step for the python formula may have failed.' >&2
