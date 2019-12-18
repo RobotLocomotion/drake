884736c401b7bdd0a8a15784de73947ef6ec56f4
Sean Curtis <sean.curtis@tri.global>
2019-12-18 13:58:33 -0800

    Correct SymbolicVectorSystem "container" population
    When evaluating various expressions (output, dynamics, or discrete updates),
    SymbolicVectorSystem builds an Environment or Substitution in order to
    map inputs to results.
    
    The original implementation would conservatively attempt to populate the
    container with every defined variable, even if the expression being
    evaluated didn't need it.
    
    This meant if there were *any* input variables, the input port would be
    evaluated. In small plant-controller loops, this quickly leads to spurious
    algebraic loops as input invokes output and vice versa forever.
    
    This changes the population algorithm to strategically evaluate the input
    ports based on a *known* need.

--- systems/primitives/test/symbolic_vector_system_test.cc
+++ systems/primitives/test/symbolic_vector_system_test.cc
@@ -1,11 +1,14 @@
 #include "drake/systems/primitives/symbolic_vector_system.h"
 
+#include <memory>
+
 #include <gtest/gtest.h>
 
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
 #include "drake/common/test_utilities/symbolic_test_util.h"
 #include "drake/math/autodiff.h"
 #include "drake/math/autodiff_gradient.h"
+#include "drake/systems/framework/diagram_builder.h"
 #include "drake/systems/framework/test_utilities/scalar_conversion.h"
 
 namespace drake {
@@ -218,6 +221,100 @@ TEST_F(SymbolicVectorSystemTest, ScalarPassThrough) {
                               Vector1d{0.12}));
 }
 
+// A simple class whose sole purpose it to report when it's output gets
+// calculated. This is a one-shot system. Once the output has been calculated,
+// it will always report "calculated". Throw it out and create a new one as
+// necessary.
+// Trying to make it reusable requires coordinating the hidden mutable state in
+// the system with the context -- the cache entry associated with the output
+// would have to be marked "out of date". For this testing context, it's simpler
+// to use it, evaluate it, and then throw it out.
+template <typename T>
+class CalcRecorder final : public LeafSystem<T> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(CalcRecorder);
+
+  explicit CalcRecorder(int output_size = 1) {
+    this->DeclareVectorOutputPort(BasicVector<T>(output_size),
+        &CalcRecorder::CalcOutput);
+  }
+
+  bool calculated() const { return calculated_; }
+
+ private:
+  void CalcOutput(const Context<T>& context,
+                  BasicVector<T>* output_vector) const {
+    calculated_ = true;
+    for (int i = 0; i < output_vector->size(); ++i) {
+      output_vector->SetAtIndex(i, T(i * 0.5));
+    }
+  }
+
+  mutable bool calculated_{false};
+};
+
+// Simply confirm the utility class does what it's supposed to do.
+GTEST_TEST(SymbolicVectorSystemTestUtil, CalcRecorder) {
+  CalcRecorder<double> recorder;
+  ASSERT_FALSE(recorder.calculated());
+  auto context = recorder.AllocateContext();
+  recorder.get_output_port(0).Eval(*context);
+  ASSERT_TRUE(recorder.calculated());
+}
+
+template <typename T>
+class InputEvaluationTest : public ::testing::Test {};
+
+// We want to confirm that the inputs are only evaluated for when required
+// for *every* scalar type -- AutoDiffXd takes a different path than the other
+// scalars.
+using InputEvalTypes = ::testing::Types<double, AutoDiffXd, Expression>;
+TYPED_TEST_SUITE(InputEvaluationTest, InputEvalTypes);
+
+// This confirms that the input is only evaluated when it is *explicitly*
+// required.
+TYPED_TEST(InputEvaluationTest, EvaluateOnlyWhenNecessary) {
+  using T = TypeParam;
+  Variable u0("u0");
+  Variable x0("x0");
+  auto input_evaluated_for_output = [u0,
+                                     x0](const Vector1<Expression>& output) {
+    DiagramBuilder<T> builder;
+    auto& recorder = *builder.template AddSystem<CalcRecorder<T>>();
+    Vector1<Expression> dynamics{x0};
+    auto& dut = *builder.template AddSystem<SymbolicVectorSystem<T>>(
+        std::nullopt, Vector1<Variable>{x0}, Vector1<Variable>{u0}, dynamics,
+        output);
+
+    builder.Connect(recorder, dut);
+
+    auto diagram = builder.Build();
+
+    auto context = diagram->AllocateContext();
+    const auto& dut_context = diagram->GetSubsystemContext(dut, *context);
+    dut.get_output_port().Eval(dut_context);
+    return recorder.calculated();
+  };
+
+  // Case: output simply doesn't reference the input.
+  {
+    Vector1<Expression> output{x0};
+    ASSERT_FALSE(input_evaluated_for_output(output));
+  }
+
+  // Case: output references input.
+  {
+    Vector1<Expression> output{x0 + u0};
+    ASSERT_TRUE(input_evaluated_for_output(output));
+  }
+
+  // Case: output references input in mathematically meaningless way.
+  {
+    Vector1<Expression> output{x0 + u0 * 0};
+    ASSERT_FALSE(input_evaluated_for_output(output));
+  }
+}
+
 TEST_F(SymbolicVectorSystemTest, VectorPassThrough) {
   // y = u.
   SymbolicVectorSystem<double> system({}, Vector0<Variable>{}, u_,
@@ -228,6 +325,7 @@ TEST_F(SymbolicVectorSystemTest, VectorPassThrough) {
   EXPECT_TRUE(context->is_stateless());
   EXPECT_EQ(system.get_input_port().size(), 2);
   EXPECT_EQ(system.get_output_port().size(), 2);
+  EXPECT_TRUE(system.HasDirectFeedthrough(0, 0));
 
   context->FixInputPort(0, Vector2d{0.12, 0.34});
   EXPECT_TRUE(CompareMatrices(system.get_output_port()
