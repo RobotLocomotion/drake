25150ee1cdc064d2d5a6a0d3b08ede3694c60a1d
Ante Qu <antequ@cs.stanford.edu>
2020-01-03 20:52:18 -0500

    Velocity-Implicit Euler Fixed Steps. Simplified code and added descriptions.

---
+++ systems/analysis/velocity_implicit_euler_integrator.cc
@@ -0,0 +1,7 @@
+#include "drake/systems/analysis/velocity_implicit_euler_integrator.h"
+
+#include "drake/common/autodiff.h"
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
+    class ::drake::systems::VelocityImplicitEulerIntegrator)
+
