5c4db85f27d70e98af917db501956438c32e77c1
Evan Drumwright <edrumwri@gmail.com>
2020-01-10 16:12:46 -0800

    Adds ability to construct MultibodyForces before constructing a MultibodyPlant (#12556)
    Adds ability to construct MultibodyForces without a MultibodyPlant..

--- multibody/tree/multibody_forces.cc
+++ multibody/tree/multibody_forces.cc
@@ -6,15 +6,20 @@ namespace drake {
 namespace multibody {
 
 template <typename T>
-MultibodyForces<T>::MultibodyForces(const internal::MultibodyTree<T>& model) {
+MultibodyForces<T>::MultibodyForces(const internal::MultibodyTree<T>& model)
+    : MultibodyForces(model.num_bodies(), model.num_velocities()) {
   DRAKE_DEMAND(model.topology_is_valid());
-  F_B_W_.resize(model.num_bodies(), SpatialForce<T>::Zero());
-  tau_ = VectorX<T>::Zero(model.num_velocities());
 }
 
 template <typename T>
 MultibodyForces<T>::MultibodyForces(const MultibodyPlantSurrogate<T>& plant)
     : MultibodyForces(internal::GetInternalTree(plant)) {}
+
+template <typename T>
+MultibodyForces<T>::MultibodyForces(int nb, int nv) {
+  F_B_W_.resize(nb, SpatialForce<T>::Zero());
+  tau_ = VectorX<T>::Zero(nv);
+}
 
 template <typename T>
 MultibodyForces<T>& MultibodyForces<T>::SetZero() {
@@ -32,9 +37,8 @@ bool MultibodyForces<T>::CheckHasRightSizeForModel(
 template <typename T>
 bool MultibodyForces<T>::CheckHasRightSizeForModel(
     const internal::MultibodyTree<T>& model) const {
-  return
-      model.num_velocities() == num_velocities() &&
-      model.num_bodies() == num_bodies();
+  return model.num_velocities() == num_velocities() &&
+         model.num_bodies() == num_bodies();
 }
 
 template <typename T>
@@ -43,8 +47,7 @@ void MultibodyForces<T>::AddInForces(const MultibodyForces<T>& addend) {
   DRAKE_DEMAND(this->num_velocities() == addend.num_velocities());
   // Add in body forces:
   auto Faddend = addend.body_forces().cbegin();
-  for (auto& F : F_B_W_)
-    F += *Faddend++;
+  for (auto& F : F_B_W_) F += *Faddend++;
   // Add in generalized forces:
   tau_ += addend.generalized_forces();
 }
