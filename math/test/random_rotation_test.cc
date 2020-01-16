718727a8262e7f700e9648393f8c7c97e21bc566
hongkai-dai <daihongkai@gmail.com>
2020-01-16 15:09:44 -0800

    Use Hopf coordinate to compute a measure of the grid in SO(3), test the uniform random quaternion using this measure.

--- math/test/random_rotation_test.cc
+++ math/test/random_rotation_test.cc
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 
 #include "drake/common/symbolic.h"
+#include "drake/math/hopf_coordinate.h"
 
 namespace drake {
 namespace math {
@@ -51,6 +52,66 @@ void CheckUniformAngleAxes(const std::vector<Orientation>& orientations,
   }
 }
 
+// We use Hopf coordinates to divide the range of [0, π] x [0, 2π] x [-π, π]
+// into grids. According to equation 5 in "Generating Uniform Incremental Grids
+// on SO(3) Using the Hopf Fibration" by A Yershova, S. LaValle and J. Mitchell,
+// the surface value of the Hopf coordinate (θ, φ, ψ) is dV = sinθdθdφdψ. So we
+// can compute the measure of each cell in the grid. Using basic confidence
+// interval statistics.  See, for instance,
+//    Simulation and the Monte Carlo Method (Third Edition)
+//      by Rubinstein and Kroese, 2017
+//    page 108,
+// where I've used Y as the indicator function of the Hopf coordinate in a given
+// cell. E[Y] = measure of that cell, Var(Y) = E[Y]-E[Y]², since E[Y²]=E[Y],
+// We expect a perfect sampler to obtain a value
+//    count/N = E[Y] ± 1.645 √(Var(Y)/N)
+// with 95% confidence.  Checks that our pseudo-random sampler
+// accomplishes this bound within a factor of fudge_factor.
+void CheckUniformHopfCoordinate(
+    const std::vector<Eigen::Vector3d>& hopf_coordinates, int num_intervals,
+    double fudge_factor) {
+  const Eigen::VectorXd theta_all =
+      Eigen::VectorXd::LinSpaced(num_intervals + 1, 0, M_PI);
+  const Eigen::VectorXd phi_all =
+      Eigen::VectorXd::LinSpaced(num_intervals + 1, 0, 2 * M_PI);
+  const Eigen::VectorXd psi_all =
+      Eigen::VectorXd::LinSpaced(num_intervals + 1, -M_PI, M_PI);
+  std::vector<std::vector<std::vector<int>>> counts(num_intervals);
+  for (int i = 0; i < num_intervals; ++i) {
+    counts[i].resize(num_intervals);
+    for (int j = 0; j < num_intervals; ++j) {
+      counts[i][j].resize(num_intervals);
+      for (int k = 0; k < num_intervals; ++k) {
+        counts[i][j][k] = 0;
+      }
+    }
+  }
+  for (const auto& hopf : hopf_coordinates) {
+    const int theta_idx = std::floor(hopf[0] / (M_PI / num_intervals));
+    const int phi_idx = std::floor(hopf[1] / (2 * M_PI / num_intervals));
+    const int psi_idx =
+        std::floor((hopf[2] + M_PI) / (2 * M_PI / num_intervals));
+    counts[theta_idx][phi_idx][psi_idx]++;
+  }
+  const int N = static_cast<int>(hopf_coordinates.size());
+
+  // The total measure of SO(3) is ∫dV=∫∫∫sinθdθdφdψ=8π²
+  // Now compute the measure of each cell.
+  for (int i = 0; i < num_intervals; ++i) {
+    for (int j = 0; j < num_intervals; ++j) {
+      for (int k = 0; k < num_intervals; ++k) {
+        const double cell_measure =
+            (std::cos(theta_all[i]) - std::cos(theta_all[i + 1])) *
+            (phi_all[j + 1] - phi_all[j]) * (psi_all[k + 1] - psi_all[k]);
+        const double EY = cell_measure / (8 * std::pow(M_PI, 2));
+        const double VarY = EY - EY * EY;
+        EXPECT_NEAR(static_cast<double>(counts[i][j][k]) / N, EY,
+                    fudge_factor * 1.645 * std::sqrt(VarY / N));
+      }
+    }
+  }
+}
+
 GTEST_TEST(RandomRotationTest, UniformlyRandomRotation) {
   RandomGenerator generator;
 
@@ -58,17 +119,20 @@ GTEST_TEST(RandomRotationTest, UniformlyRandomRotation) {
   std::vector<Eigen::Quaterniond> quaternions(num_samples);
   std::vector<Eigen::AngleAxisd> angle_axes(num_samples);
   std::vector<Eigen::Matrix3d> rotation_matrices(num_samples);
+  std::vector<Eigen::Vector3d> hopf_coordinates(num_samples);
   for (int i = 0; i < num_samples; ++i) {
     quaternions[i] = UniformlyRandomQuaternion<double>(&generator);
     angle_axes[i] = UniformlyRandomAngleAxis<double>(&generator);
     rotation_matrices[i] =
         UniformlyRandomRotationMatrix<double>(&generator).matrix();
+    hopf_coordinates[i] = QuaternionToHopfCoordinate(quaternions[i]);
   }
   const int num_intervals = 20;
   const double fudge_factor = 2;
   CheckUniformAngleAxes(quaternions, num_intervals, fudge_factor);
   CheckUniformAngleAxes(angle_axes, num_intervals, fudge_factor);
   CheckUniformAngleAxes(rotation_matrices, num_intervals, fudge_factor);
+  CheckUniformHopfCoordinate(hopf_coordinates, 10, fudge_factor);
 }
 
 GTEST_TEST(RandomRotationTest, Symbolic) {
