5be3f6f5b5ae31eaf68c3fd3e585354033066ac7
Ante Qu <antequ@cs.stanford.edu>
2020-01-24 17:35:59 -0800

    changes

--- systems/analysis/velocity_implicit_euler_integrator.h
+++ systems/analysis/velocity_implicit_euler_integrator.h
@@ -427,6 +427,7 @@ void VelocityImplicitEulerIntegrator<T>::ComputeForwardDiffVelocityJacobian(
   // Define a y' to perturb
   VectorX<T> y_prime = y;
 
+  // Define the lambda l_of_y to evaluate l(y)
   auto l_of_y = [&ny, &nq, &qk, &t, &qt0, &h, this](const VectorX<T>& y_state)
       -> VectorX<T> {
     VectorX<T> x_state(ny + nq);
