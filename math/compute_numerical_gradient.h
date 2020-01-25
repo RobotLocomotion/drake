8cef14f221d0d82e4e7bc793b1f08c18a50e0c0d
amcastro-tri <alejandro.castro@tri.global>
2020-01-25 10:11:02 -0500

    Enable math::ComputeNumericalGradient() for scalar types T != double

--- math/compute_numerical_gradient.h
+++ math/compute_numerical_gradient.h
@@ -50,13 +50,15 @@ class NumericalGradientOption {
 /**
  * Compute the gradient of a function f(x) through numerical difference.
  * @param calc_fun calc_fun(x, &y) computes the value of f(x), and stores the
- * value in y.
+ * value in y. `calc_fun` is responsible for properly resizing the output `y`
+ * when it consists of an Eigen vector of Eigen::Dynamic size.
+ *
  * @param x The point at which the numerical gradient is computed.
  * @param option The options for computing numerical gradient.
- * @tparam DerivedX an Eigen column vector of double.
- * @tparam DerivedY an Eigen column vector of double.
+ * @tparam DerivedX an Eigen column vector.
+ * @tparam DerivedY an Eigen column vector.
  * @tparam DerivedCalcX The type of x in the calc_fun. Must be an Eigen column
- * vector of double. It is possible to have DerivedCalcX being different from
+ * vector. It is possible to have DerivedCalcX being different from
  * DerivedX, for example, `calc_fun` could be solvers::EvaluatorBase(const
  * Eigen::Ref<const Eigen::VectorXd>&, Eigen::VectorXd*), but `x` could be of
  * type Eigen::VectorXd.
@@ -87,50 +89,64 @@ class NumericalGradientOption {
  * @endcode
  */
 template <typename DerivedX, typename DerivedY, typename DerivedCalcX>
-typename std::enable_if<is_eigen_vector_of<DerivedX, double>::value &&
-                            is_eigen_vector_of<DerivedY, double>::value &&
-                            is_eigen_vector_of<DerivedCalcX, double>::value,
-                        Eigen::Matrix<double, DerivedY::RowsAtCompileTime,
-                                      DerivedX::RowsAtCompileTime>>::type
+Eigen::Matrix<typename DerivedX::Scalar, DerivedY::RowsAtCompileTime,
+              DerivedX::RowsAtCompileTime>
 ComputeNumericalGradient(
     std::function<void(const DerivedCalcX&, DerivedY* y)> calc_fun,
     const DerivedX& x,
     const NumericalGradientOption& option = NumericalGradientOption{
         NumericalGradientMethod::kForward}) {
-  // First evaluate f(x).
-  Eigen::Matrix<double, DerivedY::RowsAtCompileTime, 1> y;
-  calc_fun(x, &y);
+  // All input Eigen types must be vectors templated on the same scalar type.
+  typedef typename DerivedX::Scalar T;
+  static_assert(
+      is_eigen_vector_of<DerivedY, T>::value,
+      "DerivedY must be templated on the same scalar type as DerivedX.");
+  static_assert(
+      is_eigen_vector_of<DerivedCalcX, T>::value,
+      "DerivedCalcX must be templated on the same scalar type as DerivedX.");
+
+  using std::abs;
+  using std::max;
+
+  // We need to evaluate f(x), only for the forward and backward schemes.
+  Eigen::Matrix<T, DerivedY::RowsAtCompileTime, 1> y;
+  if (option.method() != NumericalGradientMethod::kCentral)
+    calc_fun(x, &y);
 
   // Now evaluate f(x + Δx) and f(x - Δx) along each dimension.
-  Eigen::Matrix<double, DerivedX::RowsAtCompileTime, 1> x_plus = x;
-  Eigen::Matrix<double, DerivedX::RowsAtCompileTime, 1> x_minus = x;
-  Eigen::Matrix<double, DerivedY::RowsAtCompileTime, 1> y_plus, y_minus;
-  Eigen::Matrix<double, DerivedY::RowsAtCompileTime,
-                DerivedX::RowsAtCompileTime>
-      J(y.rows(), x.rows());
+  Eigen::Matrix<T, DerivedX::RowsAtCompileTime, 1> x_plus = x;
+  Eigen::Matrix<T, DerivedX::RowsAtCompileTime, 1> x_minus = x;
+  Eigen::Matrix<T, DerivedY::RowsAtCompileTime, 1> y_plus, y_minus;
+  Eigen::Matrix<T, DerivedY::RowsAtCompileTime, DerivedX::RowsAtCompileTime> J;
   for (int i = 0; i < x.rows(); ++i) {
     if (option.method() == NumericalGradientMethod::kForward ||
         option.method() == NumericalGradientMethod::kCentral) {
-      x_plus(i) += std::max(option.perturbation_size(),
-                            std::abs(x(i)) * option.perturbation_size());
+      x_plus(i) += max(option.perturbation_size(),
+                       abs(x(i)) * option.perturbation_size());
       calc_fun(x_plus, &y_plus);
     }
     if (option.method() == NumericalGradientMethod::kBackward ||
         option.method() == NumericalGradientMethod::kCentral) {
-      x_minus(i) -= std::max(option.perturbation_size(),
-                             std::abs(x(i)) * option.perturbation_size());
+      x_minus(i) -= max(option.perturbation_size(),
+                        abs(x(i)) * option.perturbation_size());
       calc_fun(x_minus, &y_minus);
     }
     switch (option.method()) {
       case NumericalGradientMethod::kForward: {
+        // Resize if we haven't yet.
+        if (J.size() == 0) J.resize(y.rows(), x.rows());
         J.col(i) = (y_plus - y) / (x_plus(i) - x(i));
         break;
       }
       case NumericalGradientMethod::kBackward: {
+        // Resize if we haven't yet.
+        if (J.size() == 0) J.resize(y.rows(), x.rows());
         J.col(i) = (y - y_minus) / (x(i) - x_minus(i));
         break;
       }
       case NumericalGradientMethod::kCentral: {
+        // Resize if we haven't yet.
+        if (J.size() == 0) J.resize(y_plus.rows(), x.rows());
         J.col(i) = (y_plus - y_minus) / (x_plus(i) - x_minus(i));
         break;
       }
