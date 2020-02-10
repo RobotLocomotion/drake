234478ae8254588a5533a58bce28d05c397addef
Eric Cousineau <eric.cousineau@tri.global>
2020-02-10 18:54:11 -0500

    affine_system: Do not add D*u to output if D==0
    Add test in linear_system

--- systems/primitives/affine_system.h
+++ systems/primitives/affine_system.h
@@ -289,6 +289,7 @@ class AffineSystem : public TimeVaryingAffineSystem<T> {
   const Eigen::MatrixXd C_;
   const Eigen::MatrixXd D_;
   const Eigen::VectorXd y0_;
+  const bool has_meaningful_D_{};
 };
 
 }  // namespace systems
