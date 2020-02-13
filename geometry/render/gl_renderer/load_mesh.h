f9cb21fdbdb322ca485b29756d7a238971bc4059
Belinda Teh <belinda.teh@tri.global>
2020-02-13 14:57:29 -0800

    Port RenderEngineGl implementation in openGL for Drake

--- geometry/render/gl_renderer/load_mesh.h
+++ geometry/render/gl_renderer/load_mesh.h
@@ -5,18 +5,25 @@
 
 #include <Eigen/Dense>
 
-#include "perception/gl_renderer/opengl_includes.h"
+#include "drake/geometry/render/gl_renderer/opengl_includes.h"
 
-namespace anzu {
-namespace gl_renderer {
+namespace drake {
+namespace geometry {
+namespace render {
+namespace gl {
 
-using VertexBuffer =
-    Eigen::Matrix<GLfloat, Eigen::Dynamic, 3, Eigen::RowMajor>;
+using VertexBuffer = Eigen::Matrix<GLfloat, Eigen::Dynamic, 3, Eigen::RowMajor>;
 using IndexBuffer = Eigen::Matrix<GLuint, Eigen::Dynamic, 3, Eigen::RowMajor>;
 
-/// Loads a mesh's vertices and indices (faces). Does not load textures.
+/** Loads a mesh's vertices and indices (faces). Does not load textures.
+ Note that while this functionality seems similar to ReadObjToSurfaceMesh,
+ RenderEngineGl cannot use SurfaceMesh. Rendering requires normals and texture
+ coordinates; SurfaceMesh was not designed with those quantities in mind.
+ */
 std::pair<VertexBuffer, IndexBuffer> LoadMeshFromObj(
     const std::string& filename);
 
-}  // namespace gl_renderer
-}  // namespace anzu
+}  // namespace gl
+}  // namespace render
+}  // namespace geometry
+}  // namespace drake
