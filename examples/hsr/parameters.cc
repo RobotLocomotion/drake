e612180f8ab0fab931c203f7ad1785134d2721dd
huihua <huihua.zhao@tri.global>
2020-02-20 13:31:05 -0800

    Add simulation parameters

---
+++ examples/hsr/parameters.cc
@@ -0,0 +1,66 @@
+#include "drake/examples/hsr/parameters.h"
+
+#include <limits>
+
+#include <gflags/gflags.h>
+
+#include "drake/common/never_destroyed.h"
+
+DEFINE_double(simulation_time, 20,
+              "Desired duration of the simulation in seconds");
+
+DEFINE_double(time_step, 2.5e-3,
+              "Simulation time step used for the discrete systems.");
+
+DEFINE_double(gravity, 9.8, "Value of gravity in the direction of -z.");
+
+DEFINE_double(target_realtime_rate, 1,
+              "Desired rate relative to real time.  See documentation for "
+              "Simulator::set_target_realtime_rate() for details.");
+
+DEFINE_double(integration_accuracy, 1.0E-6,
+              "When time_step = 0 (plant is modeled as a continuous system), "
+              "this is the desired integration accuracy.  This value is not "
+              "used if time_step > 0 (fixed-time step).");
+DEFINE_double(penetration_allowance, 5.0E-3, "Allowable penetration (meters).");
+DEFINE_double(v_stiction_tolerance, 1.0E-2,
+              "The maximum slipping speed allowed during stiction. [m/s]");
+DEFINE_double(inclined_plane_coef_static_friction, 0.6,
+              "Inclined plane's coefficient of static friction (no units).");
+DEFINE_double(inclined_plane_coef_kinetic_friction, 0.6,
+              "Inclined plane's coefficient of kinetic friction (no units).  "
+              "When time_step > 0, this value is ignored.  Only the "
+              "coefficient of static friction is used in fixed-time step.");
+
+namespace drake {
+namespace examples {
+namespace hsr {
+
+namespace {
+Parameters CreateParameters() {
+  Parameters result;
+  result.simulation_time = FLAGS_simulation_time;
+  result.time_step = FLAGS_time_step;
+  result.gravity = FLAGS_gravity;
+  result.integration_accuracy = FLAGS_integration_accuracy;
+  result.v_stiction_tolerance = FLAGS_v_stiction_tolerance;
+  result.target_realtime_rate = FLAGS_target_realtime_rate;
+  result.penetration_allowance = FLAGS_penetration_allowance;
+  result.inclined_plane_coef_static_friction =
+      FLAGS_inclined_plane_coef_static_friction;
+  result.inclined_plane_coef_kinetic_friction =
+      FLAGS_inclined_plane_coef_kinetic_friction;
+  return result;
+}
+
+}  // namespace
+
+const Parameters& hsr_sim_flags() {
+  static const drake::never_destroyed<Parameters> global(
+      CreateParameters());
+  return global.access();
+}
+
+}  // namespace hsr
+}  // namespace examples
+}  // namespace drake
---
+++ examples/hsr/parameters.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+
+namespace drake {
+namespace examples {
+namespace hsr {
+
+/// A structure to hold values that are constant at runtime after
+/// command-line processing.  Access the sole instance via `flags()`.
+struct Parameters {
+  double gravity{};
+  double target_realtime_rate{};
+  double integration_accuracy{};
+  double penetration_allowance{};
+  double v_stiction_tolerance{};
+  double inclined_plane_coef_static_friction{};
+  double inclined_plane_coef_kinetic_friction{};
+  double simulation_time{};
+  double time_step{};
+};
+
+/// @return a constref to the singleton `Parameters` instance.
+const Parameters& hsr_sim_flags();
+
+}  // namespace hsr
+}  // namespace examples
+}  // namespace drake
