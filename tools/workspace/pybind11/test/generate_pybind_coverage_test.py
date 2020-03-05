426a52a9be5f8347cae6f14e67b95dec9075ebf6
Eric Cousineau <eric.cousineau@tri.global>
2020-03-05 00:33:17 -0500

    Generate pybind coverage stats (#12040)
    Co-Authored-By: Eric Cousineau <eric.cousineau@tri.global>

---
+++ tools/workspace/pybind11/test/generate_pybind_coverage_test.py
@@ -0,0 +1,35 @@
+import filecmp
+import os
+import unittest
+
+
+def _read(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+class TestLibclangParser(unittest.TestCase):
+    def assert_file_equal(self, actual_file, expected_file):
+        actual = _read(actual_file)
+        expected = _read(expected_file)
+        self.assertMultiLineEqual(actual, expected, actual_file)
+
+    def get_test_file(self, relpath):
+        return os.path.join("tools/workspace/pybind11", relpath)
+
+    def test_parser(self):
+        self.assert_file_equal(
+            self.get_test_file("file_coverage_test.csv"),
+            self.get_test_file(
+                "test/sample_header_file_coverage_expected.csv"),
+        )
+        self.assert_file_equal(
+            self.get_test_file("class_coverage_test.csv"),
+            self.get_test_file(
+                "test/sample_header_class_coverage_expected.csv"),
+        )
+        self.assert_file_equal(
+            self.get_test_file("sample_header_documentation_test.xml"),
+            self.get_test_file(
+                "test/sample_header_documentation_pybind_expected.xml"),
+        )
