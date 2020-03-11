374ee6335994b59456a308d6a942c892c5d35737
huihua <huihua.zhao@tri.global>
2020-03-11 11:23:37 -0700

    Add more thorough unit tests

--- multibody/tree/test/door_hinge_test.cc
+++ multibody/tree/test/door_hinge_test.cc
@@ -29,6 +29,10 @@ class DoorHingeTester {
 
   const DoorHinge<double>& door_hinge() const { return door_hinge_; }
 
+  const DoorHingeConfig& door_hinge_config() const {
+    return door_hinge_.config_;
+  }
+
  private:
   const DoorHinge<double>& door_hinge_;
 };
@@ -38,18 +42,27 @@ namespace {
 constexpr double kPositionLowerLimit = -1.0;
 constexpr double kPositionUpperLimit = 1.0;
 constexpr double kDamping = 1.0;
+constexpr double kAngle = 0.5;
+constexpr double kAngularRate = 1.0;
+constexpr double kIntegrationTimeStep = 1e-6;
 
 class DoorHingeTest : public ::testing::Test {
  public:
-  DoorHingeTest() {
+  DoorHingeTest() {}
+
+  void ConfigTest(const DoorHingeConfig& config) {
     // Create an empty model.
     auto model = std::make_unique<internal::MultibodyTree<double>>();
 
     auto& body1 = model->AddBody<RigidBody>(SpatialInertia<double>());
-    hinge_joint_ = &model->AddJoint<RevoluteJoint>(
+    revolute_joint_ = &model->AddJoint<RevoluteJoint>(
         "Joint1", model->world_body(), std::nullopt, body1, std::nullopt,
         Eigen::Vector3d::UnitZ(), kPositionLowerLimit, kPositionUpperLimit,
         kDamping);
+
+    door_hinge_ = &model->AddForceElement<DoorHinge>(*revolute_joint_, config);
+
+    door_hinge_tester_ = std::make_unique<DoorHingeTester>(*door_hinge_);
 
     // Transfer tree to system and get a Context.
     system_ = std::make_unique<internal::MultibodyTreeSystem<double>>(
@@ -58,8 +71,8 @@ class DoorHingeTest : public ::testing::Test {
   }
 
   void SetHingeJointState(double angle, double angular_rate) {
-    hinge_joint_->set_angle(context_.get(), angle);
-    hinge_joint_->set_angular_rate(context_.get(), angular_rate);
+    revolute_joint_->set_angle(context_.get(), angle);
+    revolute_joint_->set_angular_rate(context_.get(), angular_rate);
   }
 
   const internal::MultibodyTree<double>& tree() const {
@@ -71,21 +84,17 @@ class DoorHingeTest : public ::testing::Test {
         tree().get_joint(JointIndex(0)));
   }
 
-  DoorHingeTester CreateDoorHingeTester(const DoorHingeConfig& config) {
-    DoorHinge<double> door_hinge(joint(), config);
-    return DoorHingeTester(door_hinge);
-  }
+  const DoorHingeTester& door_hinge_tester() { return *door_hinge_tester_; }
 
  protected:
   std::unique_ptr<internal::MultibodyTreeSystem<double>> system_;
   std::unique_ptr<systems::Context<double>> context_;
 
-  const RevoluteJoint<double>* hinge_joint_{nullptr};
+  const RevoluteJoint<double>* revolute_joint_{nullptr};
+  const DoorHinge<double>* door_hinge_{nullptr};
+  std::unique_ptr<DoorHingeTester> door_hinge_tester_;
 };
 
-// We assume that the MultibodyPlant boilerplate is adequately tested in our
-// various regression tests; the tests here mostly just check that the math has
-// the correct signs and rough magnitudes.
 DoorHingeConfig no_forces_config() {
   DoorHingeConfig config;
   config.spring_zero_angle_rad = 0;
@@ -98,9 +107,77 @@ DoorHingeConfig no_forces_config() {
   return config;
 }
 
+// With the condition that there is only frictional torque, this function
+// confirms that a) the potential energy and conservative power should be zero;
+// b) on-conservative power equals to the corresponding torque times velocity.
+void TestFrictionOnlyEnergyAndPower(const DoorHingeTester& dut,
+                                    const systems::Context<double>& context,
+                                    const internal::MultibodyTree<double>& tree,
+                                    double angular_rate) {
+  const double potential_energy_half_qc = dut.door_hinge().CalcPotentialEnergy(
+      context, tree.EvalPositionKinematics(context));
+  EXPECT_EQ(potential_energy_half_qc, 0.0);
+
+  const double conserv_power = dut.door_hinge().CalcConservativePower(
+      context, tree.EvalPositionKinematics(context),
+      tree.EvalVelocityKinematics(context));
+  EXPECT_EQ(conserv_power, 0.0);
+
+  // Verify the non-conservative power
+  const double non_conserv_power = dut.door_hinge().CalcNonConservativePower(
+      context, tree.EvalPositionKinematics(context),
+      tree.EvalVelocityKinematics(context));
+  EXPECT_EQ(non_conserv_power, dut.CalcHingeFrictionalTorque(
+                                   angular_rate, dut.door_hinge_config()) *
+                                   angular_rate);
+}
+
+// With the condition that there is only spring related torque, this function
+// confirms that a) the potential energy equals to the corresponding torque
+// times velocity; b) on-conservative power should be zero.
+void TestSpringTorqueOnlyPower(const DoorHingeTester& dut,
+                               const systems::Context<double>& context,
+                               const internal::MultibodyTree<double>& tree,
+                               double angle, double angular_rate) {
+  auto spring_power = [&dut](double q, double v) {
+    return dut.CalcHingeSpringTorque(q, dut.door_hinge_config()) * v;
+  };
+
+  const double conserv_power = dut.door_hinge().CalcConservativePower(
+      context, tree.EvalPositionKinematics(context),
+      tree.EvalVelocityKinematics(context));
+  EXPECT_EQ(conserv_power, spring_power(angle, angular_rate));
+
+  const double non_conserv_power = dut.door_hinge().CalcNonConservativePower(
+      context, tree.EvalPositionKinematics(context),
+      tree.EvalVelocityKinematics(context));
+  EXPECT_EQ(non_conserv_power, 0.0);
+}
+
+// This function integrates the conservative power (P) to get the corresponding
+// energy (PE), i.e., PE = -∫Pdt. We assume the hinge joint moves from zero to
+// the input `angle` with a constant `angular_rate`.
+double IntegrateConservativePower(const DoorHingeTester& dut,
+                                  double target_angle, double angular_rate) {
+  auto conserv_power = [&dut](double q, double v) {
+    return dut.CalcHingeSpringTorque(q, dut.door_hinge_config()) * v;
+  };
+
+  double angle = 0.0;
+  double pe_integrated = 0.0;
+  while (angle < target_angle) {
+    pe_integrated -= conserv_power(angle, angular_rate) * kIntegrationTimeStep;
+    angle += angular_rate * kIntegrationTimeStep;
+  }
+  return pe_integrated;
+}
+
+// Verify the torques and the energy should be zero when the config parameters
+// are all zero.
 TEST_F(DoorHingeTest, ZeroTest) {
   DoorHingeConfig config = no_forces_config();
-  DoorHingeTester dut = CreateDoorHingeTester(config);
+  ConfigTest(config);
+  DoorHingeTester dut = door_hinge_tester();
 
   // If no frictions, springs, etc. are applied, our torques should be 0.
   EXPECT_EQ(dut.CalcHingeFrictionalTorque(0., config), 0);
@@ -120,10 +197,13 @@ TEST_F(DoorHingeTest, ZeroTest) {
   EXPECT_EQ(potential_energy_2, 0.0);
 }
 
+// Test the case with only the torional spring torque, the corresponding energy
+// and power are computed correctly at different states.
 TEST_F(DoorHingeTest, SpringTest) {
   DoorHingeConfig config = no_forces_config();
   config.spring_constant = 1;
-  DoorHingeTester dut = CreateDoorHingeTester(config);
+  ConfigTest(config);
+  DoorHingeTester dut = door_hinge_tester();
 
   // Springs make spring torque (but not friction).
   EXPECT_EQ(dut.CalcHingeFrictionalTorque(0., config), 0);
@@ -131,32 +211,41 @@ TEST_F(DoorHingeTest, SpringTest) {
   EXPECT_LT(dut.CalcHingeSpringTorque(1., config), 0);  // Pulls toward zero.
   EXPECT_EQ(dut.CalcHingeSpringTorque(0., config), 0);
 
-  // Test potential energy with only a spring.
+  // Test potential energy at non-zero angle.
+  SetHingeJointState(kAngle, kAngularRate);
+  const double integrated_potential_energy =
+      IntegrateConservativePower(dut, kAngle, kAngularRate);
   const double potential_energy = dut.door_hinge().CalcPotentialEnergy(
       *context_, tree().EvalPositionKinematics(*context_));
+  EXPECT_NEAR(potential_energy, integrated_potential_energy,
+              kIntegrationTimeStep);
 
-  const double calculated_potential_energy =
-      0.5 * config.spring_constant * (0 - config.spring_zero_angle_rad) *
-      (0 - config.spring_zero_angle_rad);
-  EXPECT_EQ(potential_energy, calculated_potential_energy);
+  // Test the powers are computed correctly.
+  TestSpringTorqueOnlyPower(dut, *context_, tree(), kAngle, kAngularRate);
 }
 
+// Test the case with only the catch spring torque, the corresponding
+// energy and power are computed correctly at different states.
 TEST_F(DoorHingeTest, CatchTest) {
   DoorHingeConfig config = no_forces_config();
-  config.catch_width = 1;
-  config.catch_torque = 1;
-  DoorHingeTester dut = CreateDoorHingeTester(config);
+  config.catch_width = 2 * kAngle;
+  config.catch_torque = 1.0;
+  ConfigTest(config);
+  DoorHingeTester dut = door_hinge_tester();
 
   // The catch makes spring torque (but not friction).
   EXPECT_EQ(dut.CalcHingeFrictionalTorque(0., config), 0);
   EXPECT_EQ(dut.CalcHingeFrictionalTorque(1., config), 0);
 
-  EXPECT_GT(dut.CalcHingeSpringTorque(1., config), 0);   // Resists closing.
-  EXPECT_EQ(dut.CalcHingeSpringTorque(0.5, config), 0);  // Tipover point.
-  EXPECT_LT(dut.CalcHingeSpringTorque(0., config), 0);   // Detent pulls in.
+  // Resists closing.
+  EXPECT_GT(dut.CalcHingeSpringTorque(config.catch_width, config), 0);
+  // Tipover point.
+  EXPECT_EQ(dut.CalcHingeSpringTorque(config.catch_width / 2, config), 0);
+  // Detent pulls in.
+  EXPECT_LT(dut.CalcHingeSpringTorque(0., config), 0);
 
-  // The potential energy with only the catch torque at zero position and the
-  // catch_width position should be zero.
+  // Verify that the potential energy with only the catch spring torque at zero
+  // position and the catch_width position should be 0.
   const double potential_energy_q0 = dut.door_hinge().CalcPotentialEnergy(
       *context_, tree().EvalPositionKinematics(*context_));
   EXPECT_EQ(potential_energy_q0, 0.0);
@@ -164,27 +253,27 @@ TEST_F(DoorHingeTest, CatchTest) {
   SetHingeJointState(config.catch_width, 0.0);
   const double potential_energy_qc = dut.door_hinge().CalcPotentialEnergy(
       *context_, tree().EvalPositionKinematics(*context_));
-  EXPECT_EQ(potential_energy_q0, potential_energy_qc);
+  EXPECT_EQ(potential_energy_qc, 0.0);
 
-  // Compute the maximum energy when the door opens at the half center
-  // position.
-  auto singlet = [](double t, double x) {
-    return 1 - tanh(x / t) * tanh(x / t);
-  };
-  const double catch_center = config.catch_width / 2;
-  const double computed_potential_energy =
-      config.catch_torque * catch_center *
-      (singlet(catch_center, 0.0) - singlet(catch_center, -catch_center));
-  SetHingeJointState(catch_center, 0.0);
-  const double potential_energy_half_qc = dut.door_hinge().CalcPotentialEnergy(
+  // Test the energy from power integration.
+  SetHingeJointState(kAngle, kAngularRate);
+  const double integrated_potential_energy =
+      IntegrateConservativePower(dut, kAngle, kAngularRate);
+  const double potential_energy = dut.door_hinge().CalcPotentialEnergy(
       *context_, tree().EvalPositionKinematics(*context_));
-  EXPECT_EQ(potential_energy_half_qc, computed_potential_energy);
+  EXPECT_NEAR(potential_energy, integrated_potential_energy,
+              kIntegrationTimeStep);
+
+  // Verify the power terms are computed correctly.
+  // Test the powers are computed correctly
+  TestSpringTorqueOnlyPower(dut, *context_, tree(), kAngle, kAngularRate);
 }
 
 TEST_F(DoorHingeTest, StaticFrictionTest) {
   DoorHingeConfig config = no_forces_config();
   config.static_friction_torque = 1;
-  DoorHingeTester dut = CreateDoorHingeTester(config);
+  ConfigTest(config);
+  DoorHingeTester dut = door_hinge_tester();
 
   // Friction opposes tiny motion, but falls away with substantial motion.
   EXPECT_EQ(dut.CalcHingeFrictionalTorque(0., config), 0);
@@ -195,12 +284,17 @@ TEST_F(DoorHingeTest, StaticFrictionTest) {
   // No spring torque.
   EXPECT_EQ(dut.CalcHingeSpringTorque(0., config), 0);
   EXPECT_EQ(dut.CalcHingeSpringTorque(1., config), 0);
+
+  // Test the energy and power with a given state.
+  SetHingeJointState(kAngle, kAngularRate);
+  TestFrictionOnlyEnergyAndPower(dut, *context_, tree(), kAngularRate);
 }
 
 TEST_F(DoorHingeTest, DynamicFrictionTest) {
   DoorHingeConfig config = no_forces_config();
   config.dynamic_friction_torque = 1;
-  DoorHingeTester dut = CreateDoorHingeTester(config);
+  ConfigTest(config);
+  DoorHingeTester dut = door_hinge_tester();
 
   // Friction opposes any motion, even tiny motion.
   EXPECT_EQ(dut.CalcHingeFrictionalTorque(0., config), 0);
@@ -211,12 +305,17 @@ TEST_F(DoorHingeTest, DynamicFrictionTest) {
   // No spring torque.
   EXPECT_EQ(dut.CalcHingeSpringTorque(0., config), 0);
   EXPECT_EQ(dut.CalcHingeSpringTorque(1., config), 0);
+
+  // Test the energy and power with a given state.
+  SetHingeJointState(kAngle, kAngularRate);
+  TestFrictionOnlyEnergyAndPower(dut, *context_, tree(), kAngularRate);
 }
 
 TEST_F(DoorHingeTest, ViscousFrictionTest) {
   DoorHingeConfig config = no_forces_config();
   config.viscous_friction = 1;
-  DoorHingeTester dut = CreateDoorHingeTester(config);
+  ConfigTest(config);
+  DoorHingeTester dut = door_hinge_tester();
 
   // Friction opposes motion proprotionally.
   EXPECT_EQ(dut.CalcHingeFrictionalTorque(0., config), 0);
@@ -227,6 +326,10 @@ TEST_F(DoorHingeTest, ViscousFrictionTest) {
   // No spring torque.
   EXPECT_EQ(dut.CalcHingeSpringTorque(0., config), 0);
   EXPECT_EQ(dut.CalcHingeSpringTorque(1., config), 0);
+
+  // Test the energy and power with a given state.
+  SetHingeJointState(kAngle, kAngularRate);
+  TestFrictionOnlyEnergyAndPower(dut, *context_, tree(), kAngularRate);
 }
 
 }  // namespace
