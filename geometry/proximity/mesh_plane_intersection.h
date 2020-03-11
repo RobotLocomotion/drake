a73a9467188da8df4219a92a21ffdda6ae023b1f
Sean Curtis <sean.curtis@tri.global>
2020-03-11 13:48:43 -0700

    Enable use of rigid half space in hydroelastic contact
    Previous commits introduced the mathematical basis for intersecting a
    half space boundary plane with a tetrahedron. This builds on that
    atomic operation:
    
      - Allow instantiation of hydroelastic representation of rigid half
        space.
      - Define internal representation of rigid half space.
      - Evaluation of ContactSurface from intersection of VolumeMesh with
        rigid half space.
      - restore the rolling sphere demo to use a rigid half space as ground
        instead of the box previously used.

--- geometry/proximity/mesh_plane_intersection.h
+++ geometry/proximity/mesh_plane_intersection.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <memory>
 #include <unordered_map>
 #include <vector>
 
 #include "drake/common/sorted_pair.h"
+#include "drake/geometry/geometry_ids.h"
+#include "drake/geometry/proximity/bounding_volume_hierarchy.h"
 #include "drake/geometry/proximity/plane.h"
 #include "drake/geometry/proximity/volume_mesh_field.h"
+#include "drake/geometry/query_results/contact_surface.h"
 #include "drake/math/rigid_transform.h"
 
 namespace drake {
@@ -46,9 +50,11 @@ namespace internal {
 
  @param[in] tet_index       The index of the tetrahedron to attempt to
                             intersect.
- @param[in] field_M         The volume mesh field (and mesh) containing the
-                            tetrahedra to intersect. The vertex positions are
-                            all measured and expressed in Frame M.
+ @param[in] field_M         The _linear_ volume mesh field (and mesh) containing
+                            the tetrahedra to intersect. The vertex positions
+                            are all measured and expressed in Frame M. The
+                            linearity of the field is a strict requirement of
+                            the algorithm.
  @param[in] plane_M         The definition of a plane measured and expressed
                             in Frame M.
  @param[in] X_WM            The relative pose between the mesh frame M and the
@@ -65,13 +71,84 @@ namespace internal {
  @pre `tet_index` lies in the range `[0, field_M.mesh().num_elements())`.
  */
 template <typename T>
-void SliceTetWithPlane(
-    VolumeElementIndex tet_index,
-    const VolumeMeshField<double, double>& field_M, const Plane<T>& plane_M,
-    const math::RigidTransform<T>& X_WM, std::vector<SurfaceFace>* faces,
-    std::vector<SurfaceVertex<T>>* vertices_W, std::vector<T>* surface_e,
-    std::unordered_map<SortedPair<VolumeVertexIndex>, SurfaceVertexIndex>*
-        cut_edges);
+void SliceTetWithPlane(VolumeElementIndex tet_index,
+                       const VolumeMeshFieldLinear<double, double>& field_M,
+                       const Plane<T>& plane_M,
+                       const math::RigidTransform<T>& X_WM,
+                       std::vector<SurfaceFace>* faces,
+                       std::vector<SurfaceVertex<T>>* vertices_W,
+                       std::vector<T>* surface_e,
+                       std::unordered_map<SortedPair<VolumeVertexIndex>,
+                                          SurfaceVertexIndex>* cut_edges);
+
+/* Computes a ContactSurface by intersecting a plane with a set of tetrahedra
+ drawn from the given volume mesh (and its pressure field). The indicated
+ tetrahedra would typically be the result of broadphase culling.
+
+ @param[in] mesh_id         The id associated with the volume mesh.
+ @param[in] mesh_field_M    The _linear_ mesh field (and corresponding mesh,
+                            mesh_M) from which we compute the ContactSurface.
+                            The field (and the mesh vertices) are measured and
+                            expressed in Frame M. The linearity of the field is
+                            a strict requirement of the underlying algorithm.
+ @param[in] plane_id        The id associated with the plane.
+ @param[in] plane_M         The plane to intersect against the tetrahedra;
+                            measured and expressed in the same frame M.
+ @param[in] tet_indces      Indices for the tetrahedra in mesh_M to test against
+                            the plane.
+ @param[in] X_WM            The relative pose between the mesh frame M and the
+                            world frame W. Used to guarantee that the contact
+                            surface is measured and expressed in the world
+                            frame.
+ @returns `nullptr` if there is no intersection, otherwise the appropriate
+           ContactSurface. The normals of the contact surface mesh will all
+           be parallel with the plane normal.
+ @pre  `i ∈ [0, N) ∀ i ∈ tet_indices`, where N is the number of tetrahedra in
+       mesh_M.
+*/
+template <typename T>
+std::unique_ptr<ContactSurface<T>> ComputeContactSurface(
+    GeometryId mesh_id,
+    const VolumeMeshFieldLinear<double, double>& mesh_field_M,
+    GeometryId plane_id, const Plane<T>& plane_M,
+    const std::vector<VolumeElementIndex> tet_indices,
+    const math::RigidTransform<T>& X_WM);
+
+// TODO(SeanCurtis-TRI): This is, in some sense, the "public" api. It refers to
+//  half spaces. Does it belong in this file? Does it belong elsewhere? At the
+//  end of the day, where does surface mesh-soft half space go?
+/* Computes the ContactSurface formed by a rigid half space and a given
+ soft mesh.
+
+ The definition of the half space is implicit in the call -- it is the type
+ defined by the HalfSpace class, thus, only its id and its pose in a common
+ frame (the world frame) is necessary.
+
+ @param[in] id_S        The id of the soft mesh.
+ @param[in] field_S     The _linear_ mesh field (and its corresponding mesh
+                        `mesh_S`) for the soft mesh. The field and mesh vertices
+                        are measured and expressed in Frame S. The linearity of
+                        the field is a strict requirement of the underlying
+                        algorithm.
+ @param[in] bvh_S       The bounding-volume hierarchy of the soft volume mesh
+                        measured and expressed in Frame S.
+ @param[in] X_WS        The relative pose of Frame S and the world frame W.
+ @param[in] id_R        The id of the rigid half space.
+ @param[in] X_WR        The relative pose between Frame R -- the frame the half
+                        space is defined in -- and the world frame W.
+ @returns `nullptr` if there is no collision, otherwise the ContactSurface
+          between geometries S and R. The normals of the contact surface mesh
+          will all be parallel with the plane normal.
+ @tparam T The underlying scalar type. Must be a valid Eigen scalar.
+ */
+template <typename T>
+std::unique_ptr<ContactSurface<T>>
+ComputeContactSurfaceFromSoftVolumeRigidHalfSpace(
+    const GeometryId id_S, const VolumeMeshFieldLinear<double, double>& field_S,
+    const BoundingVolumeHierarchy<VolumeMesh<double>>& bvh_S,
+    const math::RigidTransform<T>& X_WS, const GeometryId id_R,
+    const math::RigidTransform<T>& X_WR);
+
 
 }  // namespace internal
 }  // namespace geometry
