71f8c9b7789d031a6afe29465f3dcf4fee037580
Jeremy Nimmer <jeremy.nimmer@tri.global>
2020-03-20 17:10:59 -0400

    framework: Break WitnessFunction dependency cycle
    System should depend on its constituent elements (ports, constraints,
    cache entries, witness functions, etc.), never the other way around.
    
    This changes the WitnessFunction constructor without a deprecation
    period, but users should not be using this constructor in the first
    place -- WitnessFunctions should always be constructed via the
    LeafSystem::MakeWitnessFunction methods.
    
    Also clean up a few oddities (dead code, etc.) in WitnessFunction
    along the way.

--- systems/framework/witness_function.cc
+++ systems/framework/witness_function.cc
@@ -1,6 +1,4 @@
 #include "drake/systems/framework/witness_function.h"
 
-#include "drake/systems/framework/system.h"
-
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
     class ::drake::systems::WitnessFunction)
