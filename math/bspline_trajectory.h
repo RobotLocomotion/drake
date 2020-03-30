5ea410f1cc8470dbb9abb5e02e55a1ba2ec44449
Andrés K. Valenzuela <andres.valenzuela@tri.global>
2020-03-30 16:23:51 -0400

    Doc fixes for reviewer comments

--- math/bspline_trajectory.h
+++ math/bspline_trajectory.h
@@ -10,13 +10,17 @@
 
 namespace drake {
 namespace math {
+/** Represents a B-spline curve using a given `basis` with `control_points` in
+ℝʳᵒʷˢ ˣ ᶜᵒˡˢ.
+@see BsplineBasis */
 template <typename T>
 class BsplineTrajectory final : public trajectories::Trajectory<T> {
  public:
   DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(BsplineTrajectory);
 
   /** Constructs a B-spline trajectory with the given `basis` and
-  `control_points`.*/
+  `control_points`.
+  @pre control_points.size() == basis.num_basis_functions() */
   BsplineTrajectory(BsplineBasis<double> basis,
                     std::vector<MatrixX<T>> control_points);
 
@@ -25,6 +29,14 @@ class BsplineTrajectory final : public trajectories::Trajectory<T> {
   // Required methods for trajectories::Trajectory interface.
   std::unique_ptr<trajectories::Trajectory<T>> Clone() const override;
 
+  /** Evaluates the BsplineTrajectory at the given time t.
+  @param t The time at which to evaluate the %PiecewisePolynomial.
+  @return The matrix of evaluated values.
+  @warning If t does not lie in the range [start_time(), end_time], the 
+           trajectory will silently be evaluated at the closest
+           point to t. For example, `value(-1)` will return `value(0)` for
+           a trajectory defined over [0, 1].
+   */
   MatrixX<T> value(double time) const override;
 
   std::unique_ptr<trajectories::Trajectory<T>> MakeDerivative(
@@ -39,8 +51,10 @@ class BsplineTrajectory final : public trajectories::Trajectory<T> {
   double end_time() const override { return knots()[num_control_points()]; }
 
   // Other methods
+  /** Returns the number of control points in this curve. */
   int num_control_points() const { return basis_.num_basis_functions(); }
 
+  /** Returns the control points of this curve. */
   const std::vector<MatrixX<T>>& control_points() const {
     return control_points_;
   }
@@ -57,27 +71,40 @@ class BsplineTrajectory final : public trajectories::Trajectory<T> {
 
   int degree() const { return order() - 1; }
 
+  /** Returns the basis of this curve. */
   const BsplineBasis<double>& basis() const { return basis_; }
 
-  /** Adds new knots at the specified `times`. The control points of the
-  trajectory are adjusted such that it produces the same value for any valid
-  time before and after this method is called.
-  @pre this->knots.front() <= t <= this->knots().back() for all t in `times` */
+  /** Adds new knots at the specified `times` without changing the behavior of
+  the trajectory. The basis and control points of the trajectory are adjusted
+  such that it produces the same value for any valid time before and after this
+  method is called. If adding the new knots increaces the multiplicity of any
+  existing knots, the modified trajectory will maintain the same level of
+  continuity at that point. However, BsplineTrajectory objects constructed with
+  the modified basis and different control points may have lower levels of
+  continuity. Note that `times` need not be sorted.
+  @pre this->start_time() <= t <= this->end_time() for all t in `times` */
   void InsertKnots(const std::vector<double>& times);
 
-  bool operator==(const BsplineTrajectory<T>& other) const;
-
+  /** Returns a new BsplineTrajectory that uses the same basis as `this`, and
+  whose control points are the result of calling `select(point)` on each `point`
+  in `this->control_points()`.*/
   math::BsplineTrajectory<T> CopyWithSelector(
       const std::function<MatrixX<T>(const MatrixX<T>&)>& select) const;
 
+  /** Returns a new BsplineTrajectory that uses the same basis as `this`, and
+  whose control points are the result of calling
+  `point.block(start_row, start_col, block_rows, block_cols)` on each `point`
+  in `this->control_points()`.*/
   math::BsplineTrajectory<T> CopyBlock(int start_row, int start_col,
                                        int block_rows, int block_cols) const;
 
-  /** Creates a math::BsplineTrajectory consisting of the first `n` elements of
-  `original`.
-  @returns the newly created math::BsplineTrajectory.
-  @pre original.cols() == 1 */
+  /** Returns a new BsplineTrajectory that uses the same basis as `this`, and
+  whose control points are the result of calling `point.head(n)` on each `point`
+  in `this->control_points()`.
+  @pre this->cols() == 1 */
   math::BsplineTrajectory<T> CopyHead(int n) const;
+
+  bool operator==(const BsplineTrajectory<T>& other) const;
 
  private:
   BsplineBasis<double> basis_;
