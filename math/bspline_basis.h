7675d62ba5062f8610311d8b55986114bd819c50
Andrés K. Valenzuela <andres.valenzuela@tri.global>
2020-03-30 12:03:41 -0400

    Doc tweaks

--- math/bspline_basis.h
+++ math/bspline_basis.h
@@ -14,13 +14,13 @@ namespace math {
 /** Given a set of non-descending breakpoints t₀ ≤ t₁ ≤ ⋅⋅⋅ ≤ tₘ, a B-spline
 basis of order k is a set of n + 1 (where n = m - k) piecewise polynomials of
 degree k - 1 defined over those breakpoints. The elements of this set are
-called "B-splines". The vector T = (t₀, t₁, ..., tₘ)' is referred to as
-the "knot vector" of the basis and its elements are refered to as "knots".
+called "B-splines". The vector (t₀, t₁, ..., tₘ)' is referred to as
+the "knot vector" of the basis and its elements are referred to as "knots".
 
 A B-spline curve using a B-spline basis B, is a parametric curve mapping
-parameter values in [tₖ₋₁, tₙ] to a vector space V. For t ∈ [tₖ₋₁, tₙ] the value
-of the curve is given by the linear combination of n control points, pᵢ ∈ V,
-with the elements of B evaluated at t.
+parameter values in [tₖ₋₁, tₙ₊₁] to a vector space V. For t ∈ [tₖ₋₁, tₙ₊₁] the
+value of the curve is given by the linear combination of n + 1 control points,
+pᵢ ∈ V, with the elements of B evaluated at t.
 
 For more information on B-splines and their uses, see (for example)
 Patrikalakis et al. [1].
@@ -48,30 +48,36 @@ class BsplineBasis final {
   /** The order of this B-spline basis (k in the class description). */
   int order() const { return order_; }
 
-  /** The number of basis functions in this B-spline basis (n in the class
+  /** The number of basis functions in this B-spline basis (n + 1 in the class
   description). */
   int num_basis_functions() const { return num_basis_functions_; }
 
-  /** The knot vector of this B-spline basis (T in the class description). */
+  /** The knot vector of this B-spline basis (the vector (t₀, t₁, ..., tₘ)' in
+  the class description). */
   const std::vector<T>& knots() const { return knots_; }
 
   /** The minimum allowable parameter value for B-spline curves using this
-  basis*/
+  basis (tₖ₋₁ in the class description). */
   const T& initial_parameter_value() const { return knots()[order() - 1]; }
 
   /** The maximum allowable parameter value for B-spline curves using this
-  basis*/
+  basis (tₙ₊₁ in the class description). */
   const T& final_parameter_value() const {
     return knots()[num_basis_functions()];
   }
 
-  /** Returns the indices of the basis functions which are non-zero for some
-  parameter value in `parameter_interval`. */
+  /** Returns the indices of the basis functions which evaluate to non-zero
+  values for some parameter value in `parameter_interval`.
+  @pre parameter_interval[0] ≤ parameter_interval[1]
+  @pre parameter_interval[0] ≥ this->initial_parameter_value()
+  @pre parameter_interval[1] ≤ this->final_parameter_value() */
   std::vector<int> ComputeActiveBasisFunctionIndices(
       const std::array<T, 2>& parameter_interval) const;
 
-  /** Returns the indices of the basis functions which are non-zero when
-  evaluated at `parameter_value`. */
+  /** Returns the indices of the basis functions which evaluate to non-zero
+  values for `parameter_value`.
+  @pre parameter_value ≥ this->initial_parameter_value()
+  @pre parameter_value ≤ this->final_parameter_value() */
   std::vector<int> ComputeActiveBasisFunctionIndices(
       const T& parameter_value) const;
 
@@ -82,6 +88,9 @@ class BsplineBasis final {
 
   /** Evaluates the B-spline curve defined by `this` and `control_points` at the
   given `parameter_value`.
+  @param control_points Control points of the B-spline curve.
+  @param parameter_value Parameter value at which to evaluate the B-spline
+  curve defined by `this` and `control_points`.
   @pre control_points.size() == this->num_basis_functions()
   @pre parameter_value ≥ this->initial_parameter_value()
   @pre parameter_value ≤ this->final_parameter_value() */
@@ -89,22 +98,17 @@ class BsplineBasis final {
   T_control_point EvaluateCurve(
       const std::vector<T_control_point>& control_points,
       T parameter_value) const {
-    // This function implements the de Boor algorithm. It uses the notation
-    // from Patrikalakis et al. [1]. Since the depth of recursion is known
-    // a-priori, the algorithm is flattened along the lines described in [2] to
-    // avoid duplicate computations.
+    /* This function implements the de Boor algorithm. It uses the notation
+    from Patrikalakis et al. [1]. Since the depth of recursion is known
+    a-priori, the algorithm is flattened along the lines described in [2] to
+    avoid duplicate computations.
 
-    //  [1] https://web.mit.edu/hyperbook/Patrikalakis-Maekawa-Cho/node18.html
-    //  [2] De Boor, Carl. "On calculating with B-splines." Journal of
-    //      Approximation theory 6.1 (1972): 50-62.
-    // @param control_points Control points of a B-spline curve. In the notation
-    // of [1] this is p.
-    // @param parameter_value Parameter value at which to evaluate the B-spline
-    // curve defined by `this` and `control_points`. In the notation of [1] this
-    // is \bar{t}.
+     [1] https://web.mit.edu/hyperbook/Patrikalakis-Maekawa-Cho/node18.html
+     [2] De Boor, Carl. "On calculating with B-splines." Journal of
+         Approximation theory 6.1 (1972): 50-62.
 
-    // NOTE: The implementation of this method is included in the header so that
-    // it can be used with custom values of T_control_point.
+    NOTE: The implementation of this method is included in the header so that
+    it can be used with custom values of T_control_point. */
     DRAKE_DEMAND(static_cast<int>(control_points.size()) ==
                  num_basis_functions());
 
