b755f73ae20b0b608bfbd80049dbdda10eddb223
Andrés K. Valenzuela <andres.valenzuela@tri.global>
2020-03-30 16:23:51 -0400

    Doc fixes for reviewer comments

--- math/knot_vector_type.h
+++ math/knot_vector_type.h
@@ -5,7 +5,7 @@ namespace math {
 
 /**
 Enum representing types of knot vectors. "Uniform" refers to the spacing
-between the knots. "Clamped" indicates that the first an last knots have
+between the knots. "Clamped" indicates that the first and last knots have
 multiplicity equal to the order of the spline.
 
 Reference:
