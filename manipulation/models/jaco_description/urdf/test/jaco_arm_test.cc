e6ef10be7bf5b27da2ee6b16e2068a4e28150a26
Sam Creasey <sam.creasey@tri.global>
2020-03-31 15:49:48 -0400

    Add hand/arm only jaco 7dof models, along with sphere collision versions

--- manipulation/models/jaco_description/urdf/test/jaco_arm_test.cc
+++ manipulation/models/jaco_description/urdf/test/jaco_arm_test.cc
@@ -10,9 +10,8 @@ namespace drake {
 namespace manipulation {
 namespace {
 
-// Tests that j2n6s300.urdf can be loaded into a RigidBodyTree. This
-// unit test also verifies that the URDF can be parsed by the URDF
-// parser.
+// Tests that j2n6s300.urdf can be loaded into a MultibodyPlant. This unit
+// test also verifies that the URDF can be parsed by the URDF parser.
 GTEST_TEST(JacoArmTest, TestLoad6DofTree) {
   const std::string kPath(FindResourceOrThrow(
       "drake/manipulation/models/jaco_description/urdf/"
@@ -33,9 +32,8 @@ GTEST_TEST(JacoArmTest, TestLoad6DofTree) {
   EXPECT_EQ(plant.num_bodies(), 16);
 }
 
-// Tests that j2s7s300.urdf can be loaded into a RigidBodyTree. This
-// unit test also verifies that the URDF can be parsed by the URDF
-// parser.
+// Tests that the urdf can be loaded into a MultibodyPlant. This unit
+// test also verifies that the URDF can be parsed by the URDF parser.
 GTEST_TEST(JacoArmTest, TestLoad7DofTree) {
   const std::string kPath(FindResourceOrThrow(
       "drake/manipulation/models/jaco_description/urdf/"
@@ -54,6 +52,106 @@ GTEST_TEST(JacoArmTest, TestLoad7DofTree) {
   // the robot: world, root, and base. Hence, there should be a total
   // of 7 + 1 + 6 + 3 = 16 bodies in the tree.
   EXPECT_EQ(plant.num_bodies(), 17);
+}
+
+// Tests that the urdf can be loaded into a MultibodyPlant. This unit
+// test also verifies that the URDF can be parsed by the URDF parser.
+GTEST_TEST(JacoArmTest, TestLoad7DofSphereCollisionTree) {
+  const std::string kPath(FindResourceOrThrow(
+      "drake/manipulation/models/jaco_description/urdf/"
+      "j2s7s300_sphere_collision.urdf"));
+
+  multibody::MultibodyPlant<double> plant(0.0);
+  multibody::Parser parser(&plant);
+  parser.AddModelFromFile(kPath);
+
+  // There should be actuators for all 7 degrees of freedom and 3
+  // fingers.
+  EXPECT_EQ(plant.num_actuators(), 10);
+
+  // Each robot has 7 bodies, an end effector, and with 6 finger
+  // bodies.  In addition, there are three bodies that are not part of
+  // the robot: world, root, and base. Hence, there should be a total
+  // of 7 + 1 + 6 + 3 = 16 bodies in the tree.
+  EXPECT_EQ(plant.num_bodies(), 17);
+}
+
+// Tests that the urdf can be loaded into a MultibodyPlant. This unit
+// test also verifies that the URDF can be parsed by the URDF parser.
+GTEST_TEST(JacoArmTest, TestLoad7DofTreeArmOnly) {
+  const std::string kPath(FindResourceOrThrow(
+      "drake/manipulation/models/jaco_description/urdf/"
+      "j2s7s300_arm.urdf"));
+
+  multibody::MultibodyPlant<double> plant(0.0);
+  multibody::Parser parser(&plant);
+  parser.AddModelFromFile(kPath);
+
+  EXPECT_EQ(plant.num_actuators(), 7);
+
+  // Each arm has 7 bodies. In addition, there are three bodies that are not
+  // part of the robot: world, root, and base. Hence, there should be a total
+  // of 7 + 3 = 10 bodies in the tree.
+  EXPECT_EQ(plant.num_bodies(), 10);
+}
+
+// Tests that the urdf can be loaded into a MultibodyPlant. This unit
+// test also verifies that the URDF can be parsed by the URDF parser.
+GTEST_TEST(JacoArmTest, TestLoad7DofTreeSphereCollisionArmOnly) {
+  const std::string kPath(FindResourceOrThrow(
+      "drake/manipulation/models/jaco_description/urdf/"
+      "j2s7s300_arm_sphere_collision.urdf"));
+
+  multibody::MultibodyPlant<double> plant(0.0);
+  multibody::Parser parser(&plant);
+  parser.AddModelFromFile(kPath);
+
+  EXPECT_EQ(plant.num_actuators(), 7);
+
+  // Each arm has 7 bodies. In addition, there are three bodies that are not
+  // part of the robot: world, root, and base. Hence, there should be a total
+  // of 7 + 3 = 10 bodies in the tree.
+  EXPECT_EQ(plant.num_bodies(), 10);
+}
+
+// Tests that the urdf can be loaded into a MultibodyPlant. This unit
+// test also verifies that the URDF can be parsed by the URDF parser.
+GTEST_TEST(JacoArmTest, TestLoad7DofTreeHandOnly) {
+  const std::string kPath(FindResourceOrThrow(
+      "drake/manipulation/models/jaco_description/urdf/"
+      "j2s7s300_hand.urdf"));
+
+  multibody::MultibodyPlant<double> plant(0.0);
+  multibody::Parser parser(&plant);
+  parser.AddModelFromFile(kPath);
+
+  // There should be actuators for all 3 fingers.
+  EXPECT_EQ(plant.num_actuators(), 3);
+
+  // Each hand has a base, an end effector, and 6 finger bodies.  In addition,
+  // there is one world body.  Hence, there should be a total of 1 + 6 + 2 =
+  // 9 bodies in the tree.
+  EXPECT_EQ(plant.num_bodies(), 9);
+}
+
+// Tests that the urdf can be loaded into a MultibodyPlant. This unit
+// test also verifies that the URDF can be parsed by the URDF parser.
+GTEST_TEST(JacoArmTest, TestLoad7DofTreeSphereCollisionHandOnly) {
+  const std::string kPath(FindResourceOrThrow(
+      "drake/manipulation/models/jaco_description/urdf/"
+      "j2s7s300_hand_sphere_collision.urdf"));
+
+  multibody::MultibodyPlant<double> plant(0.0);
+  multibody::Parser parser(&plant);
+  parser.AddModelFromFile(kPath);
+
+  // There should be actuators for all 3 fingers.
+  EXPECT_EQ(plant.num_actuators(), 3);
+
+  // Each hand has a base, an end effector, and 6 finger bodies.  In addition,
+  // there is one world body.  Hence, there should be a total of 1 + 6 + 2 =
+  // 9 bodies in the tree.
+  EXPECT_EQ(plant.num_bodies(), 9);
 }
 
 }  // namespace
