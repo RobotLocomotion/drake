6d287022bcd0b649f58e28a2a8be0e6253fab297
Sam Creasey <sam.creasey@tri.global>
2020-03-31 15:49:48 -0400

    Add hand/arm only jaco 7dof models, along with sphere collision versions

--- manipulation/models/jaco_description/urdf/test/jaco_arm_test.cc
+++ manipulation/models/jaco_description/urdf/test/jaco_arm_test.cc
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 
@@ -10,51 +11,67 @@ namespace drake {
 namespace manipulation {
 namespace {
 
-// Tests that j2n6s300.urdf can be loaded into a RigidBodyTree. This
-// unit test also verifies that the URDF can be parsed by the URDF
-// parser.
-GTEST_TEST(JacoArmTest, TestLoad6DofTree) {
-  const std::string kPath(FindResourceOrThrow(
-      "drake/manipulation/models/jaco_description/urdf/"
-      "j2n6s300.urdf"));
+struct TestCase {
+  std::string urdf;
+  int num_actuators{};
+  int num_bodies{};
+};
+
+class JacoArmParamsTest
+    : public ::testing::TestWithParam<TestCase> {};
+
+// Tests that a model can be loaded into a MultibodyPlant. This unit
+// test also verifies that the URDF can be parsed by the URDF parser.
+TEST_P(JacoArmParamsTest, HasExpected) {
+  const TestCase param = GetParam();
+  const std::string urdf_path =
+      "drake/manipulation/models/jaco_description/urdf/";
+  const std::string urdf(FindResourceOrThrow(urdf_path + param.urdf));
 
   multibody::MultibodyPlant<double> plant(0.0);
   multibody::Parser parser(&plant);
-  parser.AddModelFromFile(kPath);
+  parser.AddModelFromFile(urdf);
 
-  // There should be actuators for all 6 degrees of freedom and 3
-  // fingers.
-  EXPECT_EQ(plant.num_actuators(), 9);
-
-  // Each robot has 6 bodies, an end effector, and with 6 finger
-  // bodies.  In addition, there are three bodies that are not part of
-  // the robot: world, root, and base. Hence, there should be a total
-  // of 6 + 1 + 6 + 3 = 16 bodies in the tree.
-  EXPECT_EQ(plant.num_bodies(), 16);
+  EXPECT_EQ(plant.num_actuators(), param.num_actuators);
+  EXPECT_EQ(plant.num_bodies(), param.num_bodies);
 }
 
-// Tests that j2s7s300.urdf can be loaded into a RigidBodyTree. This
-// unit test also verifies that the URDF can be parsed by the URDF
-// parser.
-GTEST_TEST(JacoArmTest, TestLoad7DofTree) {
-  const std::string kPath(FindResourceOrThrow(
-      "drake/manipulation/models/jaco_description/urdf/"
-      "j2s7s300.urdf"));
-
-  multibody::MultibodyPlant<double> plant(0.0);
-  multibody::Parser parser(&plant);
-  parser.AddModelFromFile(kPath);
-
-  // There should be actuators for all 7 degrees of freedom and 3
-  // fingers.
-  EXPECT_EQ(plant.num_actuators(), 10);
-
-  // Each robot has 7 bodies, an end effector, and with 6 finger
-  // bodies.  In addition, there are three bodies that are not part of
-  // the robot: world, root, and base. Hence, there should be a total
-  // of 7 + 1 + 6 + 3 = 16 bodies in the tree.
-  EXPECT_EQ(plant.num_bodies(), 17);
+std::vector<TestCase> GenerateTestCases() {
+  return std::vector<TestCase>{
+    {"j2n6s300.urdf", 9,
+          6 /* robot */ +
+          1 /* end effector */ +
+          3 * 2 /* two per finger */ +
+          3 /* world, root, base */ },
+    {"j2s7s300.urdf", 10,
+          7 /* robot */ +
+          1 /* end effector */ +
+          3 * 2 /* two per finger */ +
+          3 /* world, root, base */ },
+    {"j2s7s300_sphere_collision.urdf", 10,
+          7 /* robot */ +
+          1 /* end effector */ +
+          3 * 2 /* two per finger */ +
+          3 /* world, root, base */ },
+    {"j2s7s300_arm.urdf", 7,
+          7 /* robot */ +
+          3 /* world, root, base */ },
+    {"j2s7s300_arm_sphere_collision.urdf", 7,
+          7 /* robot */ +
+          3 /* world, root, base */ },
+    {"j2s7s300_hand_sphere_collision.urdf", 3,
+          1 /* end effector */ +
+          3 * 2 /* two per finger */ +
+          2 /* world, base */ },
+    {"j2s7s300_hand.urdf", 3,
+          1 /* end effector */ +
+          3 * 2 /* two per finger */ +
+          2 /* world, base */ },
+        };
 }
+
+INSTANTIATE_TEST_SUITE_P(JacoArmTest, JacoArmParamsTest,
+                         testing::ValuesIn(GenerateTestCases()));
 
 }  // namespace
 }  // namespace manipulation
