42bffa563fef662f7cf8ad137f662b32289280a9
Sean Curtis <sean.curtis@tri.global>
2020-04-01 09:53:40 -0700

    Soft half space - rigid mesh contact must cull backfaces
    For the same reason we cull faces in the contact surface mesh between soft
    and rigid meshes, the potential contact surface between rigid mesh and soft
    half space requires the same functionality. This commit:
    
      - Refactors the core culling logic for reuse.
      - Adds the culling to mesh - half space intersection
      - Adds a test showing culling is happening.

--- geometry/proximity/mesh_half_space_intersection.cc
+++ geometry/proximity/mesh_half_space_intersection.cc
@@ -341,8 +341,15 @@ ComputeContactSurfaceFromSoftHalfSpaceRigidMesh(
     const math::RigidTransform<T>& X_WR) {
   std::vector<SurfaceFaceIndex> tri_indices;
   tri_indices.reserve(mesh_R.num_elements());
-  auto bvh_callback = [&tri_indices](SurfaceFaceIndex tri_index) {
-    tri_indices.push_back(tri_index);
+  const math::RotationMatrixd& R_WR = convert_to_double(X_WR).rotation();
+  auto bvh_callback = [&tri_indices, &mesh_R, &R_WS = X_WS.rotation(),
+                       &R_WR](SurfaceFaceIndex tri_index) {
+    // The gradient of the half space pressure field lies in the _opposite_
+    // direction as its normal. Its normal is Sz. So, grad_p_W = - Sz_W.
+    const Eigen::Vector3d& grad_p_W = -R_WS.col(2);
+    if (IsFaceNormalInNormalDirection(grad_p_W, mesh_R, tri_index, R_WR)) {
+      tri_indices.push_back(tri_index);
+    }
     return BvttCallbackResult::Continue;
   };
   const math::RigidTransform<T> X_RW = X_WR.inverse();
