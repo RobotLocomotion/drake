a02f1f9509af58e68cfe58602ba829f658f86838
Russ Tedrake <russt@mit.edu>
2020-04-02 21:33:25 -0400

    trajectories: support default scalars in PiecewisePolynomial
    No virtual methods nor type-specific shenanigans this time.  Just straight up double => T (or const T&, ...).
    I've deprecated a typedef that was used only sometimes and imho obscured readability rather than helped it.

--- common/trajectories/test/piecewise_polynomial_test.cc
+++ common/trajectories/test/piecewise_polynomial_test.cc
@@ -400,6 +400,27 @@ GTEST_TEST(testPiecewisePolynomial, ReverseAndScaleTimeTest) {
   TestScaling(spline, 4.3);
 }
 
+template <typename T>
+void TestScalarType() {
+  Eigen::VectorXd breaks(3);
+  breaks << 0, .5, 1.;
+  Eigen::MatrixXd samples(2, 3);
+  samples << 1, 1, 2, 2, 0, 3;
+
+  const PiecewisePolynomial<double> spline =
+      PiecewisePolynomial<double>::CubicWithContinuousSecondDerivatives(
+          breaks, samples);
+
+  const MatrixX<T> value = spline.value(0.5);
+  EXPECT_NEAR(ExtractDoubleOrThrow(value(0)), samples(0, 1), 1e-14);
+  EXPECT_NEAR(ExtractDoubleOrThrow(value(1)), samples(1, 1), 1e-14);
+}
+
+GTEST_TEST(PiecewiseTrajectoryTest, ScalarTypes) {
+  TestScalarType<AutoDiffXd>();
+  TestScalarType<symbolic::Expression>();
+}
+
 }  // namespace
 }  // namespace trajectories
 }  // namespace drake
