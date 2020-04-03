009871872c31f46c242c9b69695f7bfafb7406f0
Ante Qu <antequ@cs.stanford.edu>
2020-04-03 12:41:09 -0700

    Add missing implicit integration tests for error control and autodiff
    This commit:
    1. Adds a QuadraticSystemAccuracy test to verify that the implicit Euler and velocity-implicit Euler integrators have second-order error estimation (by halving the step size and comparing the error estimate) and that they achieve the exact right error on a quadratic system.
    2. Adds a few lines to verify that the implicit integrators support error estimation.
    3. Tests the StationarySystem with an Autodiff'd Jacobian Computation scheme, to make sure AutoDiff Jacobians function properly when the derivative does not depend on the state.
    4. Fixes a bug in the AutoDiff Jacobian Computation where if the derivative does not depend on the state, implicit integrators segfault because the iteration matrix adds a Jacobian matrix with 0 width to an identity square matrix.

--- systems/analysis/test/velocity_implicit_euler_integrator_test.cc
+++ systems/analysis/test/velocity_implicit_euler_integrator_test.cc
@@ -3,10 +3,77 @@
 #include <gtest/gtest.h>
 
 #include "drake/systems/analysis/test_utilities/implicit_integrator_test.h"
+#include "drake/systems/analysis/test_utilities/quadratic_scalar_system.h"
 
 namespace drake {
 namespace systems {
 namespace analysis_test {
+
+// Tests accuracy for integrating the quadratic system (with the state at time t
+// corresponding to f(t) ≡ 4t² + 4t + C, where C is the initial state) over
+// t ∈ [0, 1]. Since the error estimate has a Taylor series that is
+// accurate to O(h²) and since we have no terms beyond this order,
+// halving the step size should improve the error estimate by a factor of 4.
+// Furthemore, we test that the error estimate gives the exact error, with
+// both the correct sign and magnitude.
+GTEST_TEST(VelocityImplicitIntegratorErrorEstimatorTest,
+           QuadraticSystemAccuracy) {
+  QuadraticScalarSystem quadratic(7);
+  auto quadratic_context = quadratic.CreateDefaultContext();
+  const double C = quadratic.Evaluate(0);
+  quadratic_context->SetTime(0.0);
+  quadratic_context->get_mutable_continuous_state_vector()[0] = C;
+
+  VelocityImplicitEulerIntegrator<double> vie(quadratic,
+                                              quadratic_context.get());
+
+  // Ensure that the velocity-implicit Euler integrator supports error
+  // estimation.
+  ASSERT_EQ(vie.supports_error_estimation(), true);
+
+  // Per the description in IntegratorBase::get_error_estimate_order(), this
+  // should return "2", in accordance with the order of the polynomial in the
+  // Big-Oh term.
+  ASSERT_EQ(vie.get_error_estimate_order(), 2);
+
+  const double t_final = 1.5;
+  vie.set_maximum_step_size(t_final);
+  vie.set_fixed_step_mode(true);
+  vie.Initialize();
+  ASSERT_TRUE(vie.IntegrateWithSingleFixedStepToTime(t_final));
+
+  const double err_est_h = vie.get_error_estimate()->get_vector().GetAtIndex(0);
+  const double expected_answer = quadratic.Evaluate(t_final);
+  const double actual_answer =
+      quadratic_context->get_continuous_state_vector()[0];
+
+  // Verify that the error estimate gets the exact error value. Note the tight
+  // tolerance used.
+  EXPECT_NEAR(err_est_h, actual_answer - expected_answer,
+              10 * std::numeric_limits<double>::epsilon());
+
+  // Now obtain the error estimate corresponding to two half-sized "steps" of
+  // size h/2, to verify the error estimate order. To clarify, this means that
+  // the velocity-implicit Euler integrator would take four small steps and two
+  // large steps here.
+  quadratic_context->SetTime(0.0);
+  quadratic_context->get_mutable_continuous_state_vector()[0] = C;
+  vie.Initialize();
+  ASSERT_TRUE(vie.IntegrateWithSingleFixedStepToTime(t_final / 2));
+  ASSERT_TRUE(vie.IntegrateWithSingleFixedStepToTime(t_final));
+
+  const double err_est_2h_2 =
+      vie.get_error_estimate()->get_vector().GetAtIndex(0);
+
+  // Since the error estimate is second order, the estimate from half-steps
+  // should be a quarter the size for a quadratic system.
+  EXPECT_NEAR(err_est_2h_2, 1.0 / 4 * err_est_h,
+              10 * std::numeric_limits<double>::epsilon());
+
+  // Verify the validity of general statistics.
+  ImplicitIntegratorTest<
+      VelocityImplicitEulerIntegrator<double>>::CheckGeneralStatsValidity(&vie);
+}
 
 // Test Velocity-Implicit Euler integrator on common implicit tests.
 typedef ::testing::Types<VelocityImplicitEulerIntegrator<double>> MyTypes;
---
+++ systems/analysis/test_utilities/stationary_system.cc
@@ -0,0 +1,5 @@
+#include "drake/systems/analysis/test_utilities/stationary_system.h"
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
+    class drake::systems::analysis_test::StationarySystem)
+
