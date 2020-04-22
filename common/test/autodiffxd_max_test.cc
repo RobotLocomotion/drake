79a2c8dce67d5bae93e1ff3a9c20629f6f5c5e5a
Andres Valenzuela <andres.valenzuela@tri.global>
2020-04-22 08:47:03 -0400

    common: Add implementations of {min,max}(AutoDiffXd, AutoDiffXd) (#13112)
    Resolves #13082.

--- common/test/autodiffxd_max_test.cc
+++ common/test/autodiffxd_max_test.cc
@@ -12,16 +12,28 @@ TEST_F(AutoDiffXdTest, Max) {
   CHECK_BINARY_FUNCTION_ADS_ADS(max, y, x, -0.4);
 }
 
-TEST_F(AutoDiffXdTest, TieBreakingCheck) {
-  // Given `max(v1, v2)`, Eigen autodiff's implementation of the max function
-  // and our overload return the first argument `v1` when `v1 == v2` holds. In
+TEST_F(AutoDiffXdTest, TieBreakingCheckMaxBothNonEmpty) {
+  // Given `max(v1, v2)`, our overload returns the first argument `v1` when
+  // `v1 == v2` holds if both `v1` and `v2` have non-empty derivatives. In
   // Drake, we rely on this implementation-detail. This test checks if the
   // property holds so that we can detect a possible change in future.
-  const AutoDiffXd v1{1.0, Vector1<double>(1.)};
+  const AutoDiffXd v1{1.0, Vector1<double>(3.)};
   const AutoDiffXd v2{1.0, Vector1<double>(2.)};
-  EXPECT_EQ(max(v1, v2).derivatives()[0], 1.0);  // Returns v1, not v2.
+  EXPECT_EQ(max(v1, v2).derivatives()[0], 3.0);  // Returns v1, not v2.
 }
 
+TEST_F(AutoDiffXdTest, TieBreakingCheckMaxOneNonEmpty) {
+  // Given `max(v1, v2)`, our overload returns whichever argument has non-empty
+  // derivatives in the case where only one has non-empty derivatives. In
+  // Drake, we rely on this implementation-detail. This test checks if the
+  // property holds so that we can detect a possible change in future.
+  const AutoDiffXd v1{1.0};
+  const AutoDiffXd v2{1.0, Vector1<double>(2.)};
+  EXPECT_TRUE(CompareMatrices(min(v1, v2).derivatives(),
+                              Vector1<double>(2.)));  // Returns v2, not v1.
+  EXPECT_TRUE(CompareMatrices(min(v2, v1).derivatives(),
+                              Vector1<double>(2.)));  // Returns v2, not v1.
+}
 }  // namespace
 }  // namespace test
 }  // namespace drake
