00831317f057dbc744dc3f571c1d3bc9fd234866
Sean Curtis <sean.curtis@tri.global>
2020-04-30 13:56:44 -0700

    Move mesh generation out of RenderEngenGl (and put under test)
    This:
    
      1. Renames load_mesh.* to shape_meshes.*.
      2. It moves the logic for generating a mesh *out* of RenderEngineGl and
         into load_mesh.
         - Note: this is the first of several PRs, only the sphere is moved.

--- geometry/render/gl_renderer/dev/shape_meshes.cc
+++ geometry/render/gl_renderer/dev/shape_meshes.cc
@@ -1,6 +1,7 @@
 #include "drake/geometry/render/gl_renderer/dev/shape_meshes.h"
 
 #include <algorithm>
+#include <cmath>
 #include <fstream>
 #include <limits>
 #include <string>
@@ -16,11 +17,12 @@ namespace geometry {
 namespace render {
 namespace internal {
 
+using std::make_pair;
+using std::pair;
 using std::string;
 using std::vector;
 
-std::pair<VertexBuffer, IndexBuffer> LoadMeshFromObj(
-    std::istream* input_stream) {
+pair<VertexBuffer, IndexBuffer> LoadMeshFromObj(std::istream* input_stream) {
   tinyobj::attrib_t attrib;
   vector<tinyobj::shape_t> shapes;
   vector<tinyobj::material_t> materials;
@@ -83,16 +85,166 @@ std::pair<VertexBuffer, IndexBuffer> LoadMeshFromObj(
       ++tri_index;
     }
   }
-  return std::make_pair(vertices, indices);
+  return make_pair(vertices, indices);
 }
 
-std::pair<VertexBuffer, IndexBuffer> LoadMeshFromObj(const string& filename) {
+pair<VertexBuffer, IndexBuffer> LoadMeshFromObj(const string& filename) {
   std::ifstream input_stream(filename);
   if (!input_stream.is_open()) {
     throw std::runtime_error(
         fmt::format("Cannot load the obj file '{}'", filename));
   }
   return LoadMeshFromObj(&input_stream);
+}
+
+pair<VertexBuffer, IndexBuffer> MakeLongLatUnitSphere(int longitude_bands,
+                                                      int latitude_bands) {
+  /*
+   For notational convenience:
+    T = number of latitude bands
+    G = number of longitude bands.
+
+       G Longitudinal bands
+
+  t_0        *******
+           **╱  │  ╲**         <-- polar band
+  t_1    **─┼───┼───┼─**
+        *  │    │    │  *
+  t_2   *──┼────┼────┼──*      <-- medial bands       T Latiduinal bands
+        *  │    │    │  *
+  t_3    **─┼───┼───┼─**
+           **╲  │  ╱**         <--  polar band
+  t_4        *******
+
+  Vertex count:
+  For T latitude bands, there are T + 1 "rings" of vertices. Two rings have
+  radius 0 and a single vertex (the north and south pole). The other rings
+  all have G vertices. Total number of vertices = (T - 1) * G + 2.
+
+  Triangle count:
+  Every medial latitudinal band produces G quads or 2G triangles. There are
+  T - 2 medial bands. The two polar bands produce G triangles. So, total
+  number of triangles = (T - 2) * 2G + 2 * G
+                      = (2(T - 2) + 2) * G
+                      = (2T - 2) * G
+
+  We enumerate the latitude rings from 0 to T. Latitude rings t_0 and t_T are at
+  the poles. The height of the ring centers from the sphere center are not
+  uniformly spaced along the Cz axis (this would lead to ill-aspected
+  triangles). Instead, we define z_i (the height of the ith ring center) so that
+  the _longitudinal_ circle is uniformly sampled. Every interior latitude ring
+  t_i has radius r_i = sqrt(R² - z_i * z_i), R = 1 for the unit sphere.
+
+  The algorithm starts at the north pole and works to the south pole, tracking
+  which longitude ring we're on. For 0 < t_i < T, we also generate the triangles
+  spanning rings t_i and t_i-1. The poles are treated as a special cases.
+  */
+  // Minimum values that create a sphere approximation with volume.
+  DRAKE_DEMAND(longitude_bands >= 3);
+  DRAKE_DEMAND(latitude_bands >= 2);
+
+  const int vert_count = (latitude_bands - 1) * longitude_bands + 2;
+  const int tri_count = (2 * latitude_bands - 2) * longitude_bands;
+
+  // Angle separating latitudinal rings measured in the longitudinal direction
+  // (defines the height of rings).
+  const GLfloat delta_phi = static_cast<GLfloat>(M_PI / latitude_bands);
+  // Angle separating longitudinal lines measured in the latitudinal direction
+  // (defines distribution of vertices on the ring).
+  const GLfloat delta_theta = static_cast<GLfloat>(2 * M_PI / longitude_bands);
+
+  VertexBuffer vertices{vert_count, 3};
+  IndexBuffer indices{tri_count, 3};
+
+  // Insertion points into vertices and indices for each new vertex and tri.
+  int v_index = 0;
+  int t_index = 0;
+
+  // Add the single vertex for the north pole.
+  vertices.block<1, 3>(v_index, 0) << 0, 0, 1;
+  ++v_index;
+
+  // Index of the ring we're working on, its height and radius.
+  int t_i = 1;
+  GLfloat z_i = cosf(t_i * delta_phi);
+  GLfloat r_i = sqrtf(1.0 - z_i * z_i);
+
+  // North pole fan betwen ring t_1 and t_0 (north pole vertex).
+
+  // The for loop adds vertex v_j. p_j is the index of the vertex that precedes
+  // v_j in the same latitudinal ring. This pattern is repeated multiple times.
+  // In a given for loop, v_index iterates through the indices of the vertices
+  // being added for the new ring: e.g., [11, 12, 13, 14, ... 20]. p would then
+  // iterate throug the values [20, 11, 12, ..., 19]. In each case, it is the
+  // index of the vertex in the same ring that "precedes" the vertex indicated
+  // by v_index -- but p < v_index is not strictly true; the vertex that
+  // "precedes" the first vertex in the ring is the last vertex in the ring.
+  int p = v_index + longitude_bands - 1;
+  for (int v_j = 0; v_j < longitude_bands; ++v_j) {
+    const GLfloat theta = v_j * delta_theta;
+    const GLfloat v_x = r_i * cosf(theta);
+    const GLfloat v_y = r_i * sinf(theta);
+    vertices.block<1, 3>(v_index, 0) << v_x, v_y, z_i;
+    indices.block<1, 3>(t_index++, 0) << 0, p, v_index;
+    p = v_index++;
+  }
+
+  // Latitudinal bands
+  for (t_i = 2; t_i < latitude_bands; ++t_i) {
+    z_i = cosf(t_i * delta_phi);
+    r_i = sqrtf(1.0f - z_i * z_i);
+    /*
+     We add all the vertices for t_i and build triangles between t_i and t_i-1.
+     The triangles are formed with the following vertices:
+
+         │ c    │ b
+     ────•──────•────                      <-- circle for t_i-1
+         │ ╲    │
+         │   ╲  │
+         │ p   ╲│ v
+     ────•──────•────                      <-- circle for t_i
+         │      │
+
+     The vertices are labeled by their _indices_ v, p, b, and c.
+      v: the vertex we just added for longitude line v_j.
+      p: the previous vertex to v in the same latitudinal ring (with periodic
+         conditions).
+      b = v - G
+      c = p - G
+    */
+    p = v_index + longitude_bands - 1;
+    for (int v_j = 0; v_j < longitude_bands; ++v_j) {
+      const GLfloat theta = v_j * delta_theta;
+      const GLfloat v_x = r_i * cosf(theta);
+      const GLfloat v_y = r_i * sinf(theta);
+      vertices.block<1, 3>(v_index, 0) << v_x, v_y, z_i;
+      const int b = v_index - longitude_bands;
+      const int c = p - longitude_bands;
+      indices.block<1, 3>(t_index++, 0) << v_index, c, p;
+      indices.block<1, 3>(t_index++, 0) << v_index, b, c;
+      p = v_index++;
+    }
+  }
+
+  // South pole fan. Ring t_T-1 has already been added, just add the vertex
+  // at the south pole and connect into triangles.
+  vertices.block<1, 3>(v_index, 0) << 0.f, 0.f, -1.f;
+  const int prev_ring_start = v_index - longitude_bands;
+  // Post-increment v_index so its value represents the total number of
+  // vertices added.
+  const int south_pole = v_index++;
+  p = south_pole - 1;
+  for (int v_j = 0; v_j < longitude_bands; ++v_j) {
+    const int v = prev_ring_start + v_j;
+    indices.block<1, 3>(t_index++, 0) << v, p, south_pole;
+    p = v;
+  }
+
+  // The process of building should match our predicted counts.
+  DRAKE_DEMAND(v_index == vert_count);
+  DRAKE_DEMAND(t_index == tri_count);
+
+  return make_pair(vertices, indices);
 }
 
 }  // namespace internal
--- geometry/render/gl_renderer/dev/test/shape_meshes_test.cc
+++ geometry/render/gl_renderer/dev/test/shape_meshes_test.cc
@@ -1,9 +1,11 @@
 #include "drake/geometry/render/gl_renderer/dev/shape_meshes.h"
 
+#include <limits>
 #include <sstream>
 
 #include <gtest/gtest.h>
 
+#include "drake/common/eigen_types.h"
 #include "drake/common/find_resource.h"
 #include "drake/common/test_utilities/expect_throws_message.h"
 
@@ -47,7 +49,7 @@ GTEST_TEST(LoadMeshFromObjTest, ReadingFile) {
 
 // Confirms that non-triangular faces get triangulated.
 GTEST_TEST(LoadMeshFromObjTest, TriangulatePolygons) {
-/*
+  /*
              o 4
             ╱  ╲            A five-sided polygon and a four-sided polygon
            ╱    ╲           should be triangulated into three and two
@@ -61,17 +63,17 @@ GTEST_TEST(LoadMeshFromObjTest, TriangulatePolygons) {
           │      │
           o──────o
           6      7
-*/
+  */
   std::stringstream in_stream(R"_(
-v -1 -1 0
-v 1 -1 0
-v 2 1 0
-v 0 2 0
-v -2 1 0
-v -1 -2 0
-v 1 -2 0
-f 1 2 3 4 5
-f 6 7 2 1
+  v -1 -1 0
+  v 1 -1 0
+  v 2 1 0
+  v 0 2 0
+  v -2 1 0
+  v -1 -2 0
+  v 1 -2 0
+  f 1 2 3 4 5
+  f 6 7 2 1
   )_");
   auto [vertices, indices] = LoadMeshFromObj(&in_stream);
   EXPECT_EQ(vertices.rows(), 7);
@@ -80,7 +82,7 @@ f 6 7 2 1
 
 // Geometry already triangulated gets preserved.
 GTEST_TEST(LoadMeshFromObjTest, PreserveTriangulation) {
-/*
+  /*
              o 4
             ╱  ╲
            ╱    ╲
@@ -94,20 +96,20 @@ GTEST_TEST(LoadMeshFromObjTest, PreserveTriangulation) {
           │      │         Connect 1 to 7 to form two triangles.
           o──────o
           6      7
-*/
+  */
   std::stringstream in_stream(R"_(
-v -1 -1 0
-v 1 -1 0
-v 2 1 0
-v 0 2 0
-v -2 1 0
-v -1 -2 0
-v 1 -2 0
-f 1 2 5
-f 2 3 5
-f 3 4 5
-f 1 6 7
-f 1 7 2
+  v -1 -1 0
+  v 1 -1 0
+  v 2 1 0
+  v 0 2 0
+  v -2 1 0
+  v -1 -2 0
+  v 1 -2 0
+  f 1 2 5
+  f 2 3 5
+  f 3 4 5
+  f 1 6 7
+  f 1 7 2
   )_");
   auto [vertices, indices] = LoadMeshFromObj(&in_stream);
   EXPECT_EQ(vertices.rows(), 7);
@@ -117,7 +119,7 @@ f 1 7 2
 // Confirms that no mesh optimization takes place, e.g., unreferenced vertices
 // are still included and duplicate vertices are not merged.
 GTEST_TEST(LoadMeshFromObjTest, NoMeshOptimization) {
-/*
+  /*
 
         4 o───────o 3
           │     ╱ │
@@ -125,20 +127,117 @@ GTEST_TEST(LoadMeshFromObjTest, NoMeshOptimization) {
           │ ╱     │
     1, 6  o───────o 2
 
-*/
+  */
   std::stringstream in_stream(R"_(
-v -1 -1 0  # First four corners form a box around the origin.
-v 1 -1 0
-v 1 1 0
-v -1 1 0
-v 3 0 0    # Unreferenced vertex to the right of the box
-v -1 -1 0  # Duplpicate of vertex 1
-f 1 2 3
-f 6 3 4
+  v -1 -1 0  # First four corners form a box around the origin.
+  v 1 -1 0
+  v 1 1 0
+  v -1 1 0
+  v 3 0 0    # Unreferenced vertex to the right of the box
+  v -1 -1 0  # Duplpicate of vertex 1
+  f 1 2 3
+  f 6 3 4
   )_");
   auto [vertices, indices] = LoadMeshFromObj(&in_stream);
   EXPECT_EQ(vertices.rows(), 6);
   EXPECT_EQ(indices.rows(), 2);
+}
+
+// Computes the normal to the indicated triangle whose magnitude is twice the
+// triangle's area. I.e., for triangle (A, B, C), computes: (B - A) X (C - A).
+Vector3<GLfloat> normal(const VertexBuffer& vertices, const IndexBuffer& tris,
+                        int tri_index) {
+  const auto& a = vertices.block<1, 3>(tris(tri_index, 0), 0);
+  const auto& b = vertices.block<1, 3>(tris(tri_index, 1), 0);
+  const auto& c = vertices.block<1, 3>(tris(tri_index, 2), 0);
+  return (b - a).cross(c - a);
+}
+
+// Computes the area of the indicated triangle.
+GLfloat area(const VertexBuffer& vertices, const IndexBuffer& tris,
+             int tri_index) {
+  return normal(vertices, tris, tri_index).norm() * 0.5;
+}
+
+// Computes the normal for the triangle (implied by its winding).
+Vector3<GLfloat> unit_normal(const VertexBuffer& vertices,
+                             const IndexBuffer& tris, int tri_index) {
+  return normal(vertices, tris, tri_index).normalized();
+}
+
+// Computes the centroid of the indicated triangle.
+Vector3<GLfloat> centroid(const VertexBuffer& vertices, const IndexBuffer& tris,
+                          int tri_index) {
+  return (vertices.block<1, 3>(tris(tri_index, 0), 0) +
+          vertices.block<1, 3>(tris(tri_index, 1), 0) +
+          vertices.block<1, 3>(tris(tri_index, 2), 0)) /
+         3.f;
+}
+
+// Simply confirm that the utility functions above report good values.
+GTEST_TEST(ShapeMeshesTest, ConfirmUtilities) {
+  VertexBuffer vertices(3, 3);
+  /*
+            │╲ (0, 1.5, 0)        Right isoceles triangle with legs of length
+            │ ╲                   1.5, lying on the z = 0 plane.
+            │  ╲                     area = 1.5 * 1.5 / 2 = 2.25 / 2 = 1.125
+            │   ╲                    unit normal = <0, 0, 1>
+            │    ╲                   normal = <0, 0, 2.25>
+   (0, 0, 0)└─────  (1.5, 0, 0)      centroid = (0.5, 0.5, 0)
+  */
+  vertices.block<1, 3>(0, 0) << 0, 0, 0;
+  vertices.block<1, 3>(1, 0) << 1.5, 0, 0;
+  vertices.block<1, 3>(2, 0) << 0, 1.5, 0;
+  IndexBuffer indices(1, 3);
+  indices.block<1, 3>(0, 0) << 0, 1, 2;
+  EXPECT_EQ(normal(vertices, indices, 0), Vector3<GLfloat>(0, 0, 2.25));
+  EXPECT_EQ(unit_normal(vertices, indices, 0), Vector3<GLfloat>(0, 0, 1));
+  EXPECT_EQ(area(vertices, indices, 0), 1.125);
+  EXPECT_EQ(centroid(vertices, indices, 0), Vector3<GLfloat>(0.5f, 0.5f, 0));
+}
+
+GTEST_TEST(MakeLongLatUnitSphere, ConfirmSphere) {
+  const GLfloat kEps = std::numeric_limits<GLfloat>::epsilon();
+
+  // Closed form solution for surface area of unit sphere.
+  const GLfloat kIdealArea = static_cast<GLfloat>(4 * M_PI);  // 4πR², R = 1.
+
+  float prev_area = 0;
+  for (int resolution : {3, 10, 20, 40}) {
+    auto [vertices, indices] = MakeLongLatUnitSphere(resolution, resolution);
+
+    // Confirm area converges towards (but not above) ideal area.
+    float total_area = 0;
+    for (int t = 0; t < indices.rows(); ++t) {
+      const auto a = area(vertices, indices, t);
+      total_area += a;
+    }
+    EXPECT_GT(total_area, prev_area) << "for resolution " << resolution;
+    EXPECT_LE(total_area, kIdealArea) << "for resolution " << resolution;
+    prev_area = total_area;
+
+    // All vertices lie on the unit sphere.
+    for (int v_i = 0; v_i < vertices.rows(); ++v_i) {
+      const auto& v = vertices.block<1, 3>(v_i, 0);
+      EXPECT_NEAR(v.norm(), 1.f, kEps)
+          << "for resolution: " << resolution << " and vertex " << v_i;
+    }
+
+    // All face normals point outward
+    for (int t = 0; t < indices.rows(); ++t) {
+      Vector3<GLfloat> c = centroid(vertices, indices, t);
+      Vector3<GLfloat> n = normal(vertices, indices, t);
+      // If the winding were backwards, this dot product would be negative.
+      EXPECT_GT(n.dot(c), 0)
+          << "for resolution: " << resolution << " and triangle " << t;
+    }
+  }
+
+  /* These tests are merely suggestive; they don't guarantee a correct sphere
+   mesh. We *might* consider confirming there are no holes in the mesh. In
+   practice that may not be necessary; those kinds of artifacts would be
+   immediately apparent in rendered images. Defer those heavyweight types of
+   tests until there's a proven bug. */
 }
 
 }  // namespace
