ee8361264b98c5119f4083904da32f7dc3f5ae0d
Jeremy Nimmer <jeremy.nimmer@tri.global>
2020-04-30 20:08:01 -0400

    attic,examples: Remove deprecated code 2020-05

--- examples/kuka_iiwa_arm/iiwa_common.h
+++ examples/kuka_iiwa_arm/iiwa_common.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <memory>
 #include <string>
 #include <vector>
 
@@ -21,35 +20,10 @@ namespace kuka_iiwa_arm {
 using manipulation::kuka_iiwa::kIiwaArmNumJoints;
 using manipulation::kuka_iiwa::get_iiwa_max_joint_velocities;
 
-/// Computes the lumped inertia parameters of the gripper and the end effector
-/// link expressed in the end effector frame.
-/// @param world_tree The RigidBodyTree that contains the arm and the gripper
-/// models.
-/// @param iiwa_instance Identifier for the arm in @p world_tree.
-/// @param end_effector_link_name Link name of the end effector.
-/// @param wsg_instance Identifier for the gripper in @p world_tree.
-/// @return Lumped inertia parameters.
-template <typename T>
-DRAKE_DEPRECATED("2020-05-01", "This function is being removed.")
-Matrix6<T> ComputeLumpedGripperInertiaInEndEffectorFrame(
-    const RigidBodyTree<T>& world_tree,
-    int iiwa_instance, const std::string& end_effector_link_name,
-    int wsg_instance);
-
 /// Verifies that @p tree matches assumptions about joint indices.
 /// Aborts if the tree isn't as expected.
 DRAKE_DEPRECATED("2020-06-01", "This function is being removed.")
 void VerifyIiwaTree(const RigidBodyTree<double>& tree);
-
-/// Builds a RigidBodyTree at the specified @position and @orientation from
-/// the model specified by @model_file_name.
-/// This method is a convenience wrapper over `AddModelInstanceFromUrdfFile`.
-/// @see drake::parsers::urdf::AddModelInstanceFromUrdfFile
-DRAKE_DEPRECATED("2020-05-01", "This function is being removed.")
-void CreateTreedFromFixedModelAtPose(
-    const std::string& model_file_name, RigidBodyTreed* tree,
-    const Eigen::Vector3d& position = Eigen::Vector3d::Zero(),
-    const Eigen::Vector3d& orientation = Eigen::Vector3d::Zero());
 
 /// Used to set the feedback gains for the simulated position controlled KUKA.
 void SetPositionControlledIiwaGains(Eigen::VectorXd* Kp,
