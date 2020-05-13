585b9c259dd2a282e7b3f0061733b6d4bf458b2c
Eric Cousineau <eric.cousineau@tri.global>
2020-05-12 21:28:22 -0400

    py mbp: Bind set_stiction_tolerance, get_contact_penalty_method_time_scale

--- bindings/pydrake/multibody/test/plant_test.py
+++ bindings/pydrake/multibody/test/plant_test.py
@@ -1290,7 +1290,10 @@ class TestPlant(unittest.TestCase):
         plant = MultibodyPlant_[float](0.0)
         Parser(plant).AddModelFromFile(file_name)
         plant.Finalize()
-        plant.set_penetration_allowance(0.0001)
+        plant.set_penetration_allowance(penetration_allowance=0.0001)
+        plant.set_stiction_tolerance(v_stiction=0.001)
+        self.assertIsInstance(
+            plant.get_contact_penalty_method_time_scale(), float)
         contact_results_to_lcm = ContactResultsToLcmSystem(plant)
         context = contact_results_to_lcm.CreateDefaultContext()
         contact_results_to_lcm.get_input_port(0).FixValue(
