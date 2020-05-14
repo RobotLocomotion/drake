24214fb9eb5bbd2725d17e25187aa9d1bf3180c5
Russ Tedrake <russt@mit.edu>
2020-05-13 21:05:30 -0400

    systems: Luenberger observer uses cache entries
    instead of mutable member variables.
    related to #13131

--- systems/estimators/test/luenberger_observer_test.cc
+++ systems/estimators/test/luenberger_observer_test.cc
@@ -9,9 +9,12 @@
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
 #include "drake/common/test_utilities/is_dynamic_castable.h"
 #include "drake/examples/pendulum/pendulum_plant.h"
+#include "drake/systems/framework/test_utilities/scalar_conversion.h"
 #include "drake/systems/primitives/linear_system.h"
 
 namespace drake {
+namespace systems {
+namespace estimators {
 namespace {
 
 // Test the estimator dynamics observing a linear system.
@@ -39,41 +42,71 @@ GTEST_TEST(LuenbergerObserverTest, ErrorDynamics) {
        27., 28., 29.;
   // clang-format on
 
-  auto plant = std::make_unique<systems::LinearSystem<double>>(A, B, C, D);
-  auto plant_context = plant->CreateDefaultContext();
-  auto observer =
-      std::make_unique<systems::estimators::LuenbergerObserver<double>>(
-          std::move(plant), std::move(plant_context), L);
+  // Run the test using all three constructors.
+  for (int i = 0; i < 3; ++i) {
+    auto plant = std::make_unique<systems::LinearSystem<double>>(A, B, C, D);
+    auto plant_context = plant->CreateDefaultContext();
+    std::unique_ptr<systems::estimators::LuenbergerObserver<double>> observer{
+        nullptr};
+    switch (i) {
+      case 0:  // pass system and context by reference.
+        observer =
+            std::make_unique<systems::estimators::LuenbergerObserver<double>>(
+                *plant, *plant_context, L);
+        break;
+      case 1:  // owned system version.
+        observer =
+            std::make_unique<systems::estimators::LuenbergerObserver<double>>(
+                std::move(plant), *plant_context, L);
+        break;
+      case 2:  // deprecated constructor (passes both system and context
+               // for ownership).
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+        // NOTE: make_unique actually swallows the deprecation warnings anyhow,
+        // so the pragma is not actually needed.  But I'm leaving it here for
+        // easy removal once the deprecation period expires.
+        observer =
+            std::make_unique<systems::estimators::LuenbergerObserver<double>>(
+                std::move(plant), std::move(plant_context), L);
+#pragma GCC diagnostic pop
+        break;
+    }
 
-  auto context = observer->CreateDefaultContext();
-  auto derivatives = observer->AllocateTimeDerivatives();
-  auto output = observer->AllocateOutput();
+    auto context = observer->CreateDefaultContext();
+    auto derivatives = observer->AllocateTimeDerivatives();
+    auto output = observer->AllocateOutput();
 
-  EXPECT_FALSE(observer->HasAnyDirectFeedthrough());
+    EXPECT_FALSE(observer->HasAnyDirectFeedthrough());
 
-  // The expected dynamics are:
-  //  xhatdot = Axhat + Bu + L(y-yhat)
-  //  y = xhat
+    // The expected dynamics are:
+    //  xhatdot = Axhat + Bu + L(y-yhat)
+    //  y = xhat
 
-  Eigen::Vector3d xhat(1.0, 2.0, 3.0);
-  Vector1d u(4.0);
+    Eigen::Vector3d xhat(1.0, 2.0, 3.0);
+    Vector1d u(4.0);
 
-  Eigen::Vector2d y(5.0, 6.0);
+    Eigen::Vector2d y(5.0, 6.0);
 
-  Eigen::Vector3d xhatdot = A * xhat + B * u + L * (y - C * xhat - D * u);
+    Eigen::Vector3d xhatdot = A * xhat + B * u + L * (y - C * xhat - D * u);
 
-  observer->get_input_port(0).FixValue(context.get(), y);
-  observer->get_input_port(1).FixValue(context.get(), u);
-  context->get_mutable_continuous_state_vector().SetFromVector(xhat);
+    observer->get_input_port(0).FixValue(context.get(), y);
+    observer->get_input_port(1).FixValue(context.get(), u);
+    context->get_mutable_continuous_state_vector().SetFromVector(xhat);
 
-  observer->CalcTimeDerivatives(*context, derivatives.get());
-  observer->CalcOutput(*context, output.get());
+    observer->CalcTimeDerivatives(*context, derivatives.get());
+    observer->CalcOutput(*context, output.get());
 
-  double tol = 1e-10;
+    double tol = 1e-10;
 
-  EXPECT_TRUE(CompareMatrices(xhatdot, derivatives->CopyToVector(), tol));
-  EXPECT_TRUE(CompareMatrices(
-      xhat, output->GetMutableVectorData(0)->CopyToVector(), tol));
+    EXPECT_TRUE(CompareMatrices(xhatdot, derivatives->CopyToVector(), tol));
+    EXPECT_TRUE(CompareMatrices(
+        xhat, output->GetMutableVectorData(0)->CopyToVector(), tol));
+
+    // TODO(russt): Support scalar conversion.
+    EXPECT_FALSE(is_autodiffxd_convertible(*observer));
+    EXPECT_FALSE(is_symbolic_convertible(*observer));
+  }
 }
 
 // Check that the observer inherits the dynamic types of the pendulum.
@@ -100,5 +133,21 @@ GTEST_TEST(LuenbergerObserverTest, DerivedTypes) {
   // is not that way yet (see #6998).
 }
 
+GTEST_TEST(LuenbergerObserverTest, InputOutputPorts) {
+  examples::pendulum::PendulumPlant<double> plant;
+  auto context = plant.CreateDefaultContext();
+  const Eigen::Matrix2d L = Eigen::Matrix2d::Identity();
+  LuenbergerObserver<double> observer(plant, *context, L);
+
+  EXPECT_EQ(observer.GetInputPort("observed_system_input").get_index(),
+            observer.get_observed_system_input_input_port().get_index());
+  EXPECT_EQ(observer.GetInputPort("observed_system_output").get_index(),
+            observer.get_observed_system_output_input_port().get_index());
+  EXPECT_EQ(observer.GetOutputPort("estimated_state").get_index(),
+            observer.get_estimated_state_output_port().get_index());
+}
+
 }  // namespace
+}  // namespace estimators
+}  // namespace systems
 }  // namespace drake
