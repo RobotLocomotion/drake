94235787e94df966ef5e7d5c939a72806968fb68
hongkai-dai <daihongkai@gmail.com>
2020-05-20 10:28:01 -0700

    Interpret dual variable solution as the shadow price.

--- solvers/test/osqp_solver_test.cc
+++ solvers/test/osqp_solver_test.cc
@@ -128,13 +128,25 @@ GTEST_TEST(OsqpSolverTest, DualSolution1) {
     // x[1] + 4 * x[2] == 3
     // Solving the KKT condition, we get the dual solution as
     // dual solution for x[0] >= 0 is 0
-    // dual solution for x[1] + 4 * x[2] == 3 is -0.363636
+    // dual solution for x[1] + 4 * x[2] == 3 is 0.363636
     EXPECT_TRUE(
         CompareMatrices(result.GetDualSolution(constraint1), Vector1d(0.)));
+    const double dual_solution_expected = 0.363636;
     EXPECT_TRUE(CompareMatrices(result.GetDualSolution(constraint2),
-                                Vector1d(-0.36363636), 1e-6));
+                                Vector1d(dual_solution_expected), 1e-6));
     EXPECT_TRUE(CompareMatrices(result.GetDualSolution(constraint3),
                                 Eigen::Vector3d::Zero()));
+
+    // Now update the equality constraint right-hand side with a small amount,
+    // check the optimal cost of the updated QP. The change in the optimal cost
+    // should be equal to dual variable solution.
+    const double delta = 1e-5;
+    constraint2.evaluator()->set_bounds(Vector1d(3 + delta),
+                                        Vector1d(3 + delta));
+    const MathematicalProgramResult result_updated = osqp_solver.Solve(prog);
+    EXPECT_NEAR(
+        (result_updated.get_optimal_cost() - result.get_optimal_cost()) / delta,
+        dual_solution_expected, 1e-5);
   }
 }
 
@@ -156,14 +168,23 @@ GTEST_TEST(OsqpSolverTest, DualSolution2) {
     // 2 * x[0] + 4 * x[1] >= 0
     // x[1] - 4 * x[2] == -2
     // Solving the KKT condition, we get the dual solution as
-    // dual solution for 2 * x[0] + 4 * x[1] >= 0 is -0.34285714
-    // dual solution for x[1] - 4 * x[2] == -2 is 1.14285714
+    // dual solution for 2 * x[0] + 4 * x[1] >= 0 is 0.34285714
+    // dual solution for x[1] - 4 * x[2] == -2 is -1.14285714
+    const double dual_solution_expected = 0.34285714;
     EXPECT_TRUE(CompareMatrices(result.GetDualSolution(constraint1),
-                                Vector1d(-0.34285714), 1e-6));
+                                Vector1d(dual_solution_expected), 1e-6));
     EXPECT_TRUE(CompareMatrices(result.GetDualSolution(constraint2),
-                                Vector1d(1.14285714), 1e-6));
+                                Vector1d(-1.14285714), 1e-6));
     EXPECT_TRUE(CompareMatrices(result.GetDualSolution(constraint3),
                                 Eigen::Vector3d::Zero(), 1e-6));
+    // Now perturb the lower bound of the inequality by a little bit, the change
+    // of the optimal cost should be equal to the dual solution.
+    const double delta = 1e-5;
+    constraint1.evaluator()->UpdateLowerBound(Vector1d(delta));
+    const MathematicalProgramResult result_updated = osqp_solver.Solve(prog);
+    EXPECT_NEAR(
+        (result_updated.get_optimal_cost() - result.get_optimal_cost()) / delta,
+        dual_solution_expected, 1e-5);
   }
 }
 
@@ -185,10 +206,34 @@ GTEST_TEST(OsqpSolverTest, DualSolution3) {
     // x[0] <= 2
     // x[1] >= -1
     // Solving the KKT condition, we get the dual solution as
-    // dual solution for x[0] <= 2 is 6
-    // dual solution for x[1] >= -1 is -6
+    // dual solution for x[0] <= 2 is -6
+    // dual solution for x[1] >= -1 is 6
     EXPECT_TRUE(CompareMatrices(result.GetDualSolution(constraint),
-                                Eigen::Vector2d(6, -6), 1e-6));
+                                Eigen::Vector2d(-6, 6), 1e-6));
+    // Now perturb the bounds a bit, the change of the optimal cost should match
+    // with the dual solution.
+    const double delta = 1e-5;
+    constraint.evaluator()->UpdateUpperBound(Eigen::Vector2d(2 + delta, 2));
+    const MathematicalProgramResult result1 = osqp_solver.Solve(prog);
+    EXPECT_NEAR(
+        (result1.get_optimal_cost() - result.get_optimal_cost()) / delta, -6,
+        2e-5);
+    constraint.evaluator()->UpdateUpperBound(Eigen::Vector2d(2, 2 + delta));
+    const MathematicalProgramResult result2 = osqp_solver.Solve(prog);
+    // The dual solution for x[1] <= 2 is 0.
+    EXPECT_NEAR(result2.get_optimal_cost(), result.get_optimal_cost(), 2e-5);
+
+    constraint.evaluator()->set_bounds(Eigen::Vector2d(-1 + delta, -1),
+                                       Eigen::Vector2d(2, 2));
+    const MathematicalProgramResult result3 = osqp_solver.Solve(prog);
+    // The dual solution for x[0] >= -1 is 0.
+    EXPECT_NEAR(result3.get_optimal_cost(), result.get_optimal_cost(), 2e-5);
+
+    constraint.evaluator()->UpdateLowerBound(Eigen::Vector2d(-1, -1 + delta));
+    const MathematicalProgramResult result4 = osqp_solver.Solve(prog);
+    EXPECT_NEAR(
+        (result4.get_optimal_cost() - result.get_optimal_cost()) / delta, 6,
+        2e-5);
   }
 }
 
