07c00c601325a228771eb8d130d7847947a18035
ggould-tri <ggould@tri.global>
2020-05-21 14:04:30 -0400

    Verbatim copy of TRI monte carlo acrobot demo
    Co-authored-by: Grant Gould <ggould@tri.global>
    Co-authored-by: Sam Creasey <sam.creasey@tri.global>
    Co-authored-by: Jeremy Nimmer <jeremy.nimmer@tri.global>
    Co-authored-by: Andrés K. Valenzuela <andres.valenzuela@tri.global>

---
+++ examples/acrobot/dev/test/spong_sim_main_test.py
@@ -0,0 +1,71 @@
+import os
+import subprocess
+import sys
+import unittest
+
+from anzu.common.runfiles import Rlocation
+
+from anzu.sim.acrobot.acrobot_io import load_output, load_scenario
+
+
+_backend = "py"
+
+
+class TestRunSpongControlledAcrobot(unittest.TestCase):
+
+    def setUp(self):
+        self.dut = Rlocation(
+            f"anzu/sim/acrobot/spong_sim_main_{_backend}")
+        # 4 states x 30 seconds of samples at 20 Hz per sample_scenario.
+        self.nominal_x_tape_shape = (4, 601)
+
+    def test_help(self):
+        result = subprocess.run(
+            [self.dut, "--help"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            encoding="utf-8")
+        self.assertEqual(result.stderr, "")
+        self.assertIn("spong-controlled acrobot", result.stdout)
+        self.assertEqual(result.returncode, 0 if _backend == "py" else 1)
+
+    def test_sample_scenario(self):
+        scenario = Rlocation("anzu/sim/acrobot/test/sample_scenario.yaml")
+        output = os.path.join(os.environ["TEST_TMPDIR"], "output.yaml")
+        subprocess.check_call([
+            self.dut, "--scenario", scenario, "--output", output])
+        x_tape = load_output(filename=output)
+        self.assertEqual(x_tape.shape, self.nominal_x_tape_shape)
+
+    def test_stochastic_scenario(self):
+        if _backend == "py":
+            return  # Python backend does not support stochastic scenarios.
+
+        scenario = Rlocation(
+            "anzu/sim/acrobot/test/sample_stochastic_scenario.yaml")
+        output = os.path.join(os.environ["TEST_TMPDIR"], "output.yaml")
+        dump_file = os.path.join(os.environ["TEST_TMPDIR"],
+                                 "scenario_out.yaml")
+        subprocess.check_call([
+            self.dut, "--scenario", scenario, "--output", output,
+            "--dump_scenario", dump_file])
+        x_tape = load_output(filename=output)
+        # 4 states x 30 seconds of samples at 20 Hz per sample_scenario.
+        self.assertEqual(x_tape.shape, (4, 601))
+
+        # Load the scenario dump; don't bother checking exact distribution
+        # semantics; just check that we have the right data shape and type.
+        dump = load_scenario(filename=dump_file)
+        self.assertEqual(len(dump["controller_params"]), 4)
+        self.assertTrue(all(type(x) == float
+                            for x in dump["controller_params"]))
+        self.assertEqual(len(dump["initial_state"]), 4)
+        self.assertTrue(all(type(x) == float
+                            for x in dump["initial_state"]))
+
+
+if __name__ == "__main__":
+    if "--cc" in sys.argv:
+        sys.argv.remove("--cc")
+        _backend = "cc"
+    unittest.main()
