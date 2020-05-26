80d28764f30de301054ad984a45edf24a3241f2a
Eric Cousineau <eric.cousineau@tri.global>
2020-05-26 15:52:49 -0400

    py value: Allow binding Value<vector<T>> to Value[List[T]]

--- bindings/pydrake/common/test/value_test_util_py.cc
+++ bindings/pydrake/common/test/value_test_util_py.cc
@@ -1,3 +1,4 @@
+#include "pybind11/operators.h"
 #include "pybind11/pybind11.h"
 
 #include "drake/bindings/pydrake/common/value_pybind.h"
@@ -6,6 +7,11 @@
 namespace drake {
 namespace pydrake {
 namespace {
+
+class CustomType {
+ public:
+  bool operator==(const CustomType&) const { return true; }
+};
 
 class MoveOnlyType {
  public:
@@ -38,8 +44,14 @@ PYBIND11_MODULE(value_test_util, m) {
       .def(py::init<int>())
       .def("x", &MoveOnlyType::x)
       .def("set_x", &MoveOnlyType::set_x);
-  // Define `Value` instantiation.
+  // Define `Value[MoveOnlyType]` instantiation.
   AddValueInstantiation<MoveOnlyType>(m);
+
+  py::class_<CustomType>(m, "CustomType")
+      .def(py::init())
+      .def(py::self == py::self);
+  // Define `Value[List[CustomType]]` instantiation.
+  AddValueInstantiation<std::vector<CustomType>>(m);
 
   m.def("make_abstract_value_cc_type_unregistered",
       []() { return AbstractValue::Make(UnregisteredType{}); });
