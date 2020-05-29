eec0243919b0caeb512bc234e13cf232d2719951
Eric Cousineau <eric.cousineau@tri.global>
2020-05-29 01:21:56 -0400

    drake_visualizer: Add `limit_clipping_range` script

---
+++ tools/workspace/drake_visualizer/_drake_visualizer_builtin_scripts/limit_clipping_range.py
@@ -0,0 +1,29 @@
+"""
+Limits clipping range as mentioned here:
+https://github.com/RobotLocomotion/drake/issues/13446#issuecomment-635997871
+
+For background, see:
+https://www.khronos.org/opengl/wiki/Depth_Buffer_Precision
+"""
+
+# N.B. Depending on the choice of these numbers, it may be necessary to only
+# limit one, or both. For simplicity, we will always limit both.
+MAX_NEAR = 0.05  # 5cm
+MAX_FAR = 1e3  # 1km
+
+
+def activate(view):
+
+    def on_render(render_window, event):
+        near, far = view.camera().GetClippingRange()
+        near = min(MAX_NEAR, near)
+        far = min(MAX_FAR, far)
+        assert far > near
+        view.camera().SetClippingRange(near, far)
+
+    view.renderWindow().AddObserver('StartEvent', on_render)
+
+
+# Activate the plugin if this script is run directly.
+if __name__ == "__main__":
+    activate()
