d930eda4ae915f41c56eb1c6e167c4ba83af7732
Andres Valenzuela <andres.valenzuela@tri.global>
2020-06-01 13:42:49 -0400

    systems: Use Eigen's formatting in VectorBase::operator<<() output
    This restores PR #13365
    This reverts commit fca17492ebb64e1d634d3422e376769e99238a0d (#13384)

--- systems/framework/test/basic_vector_test.cc
+++ systems/framework/test/basic_vector_test.cc
@@ -189,21 +189,34 @@ GTEST_TEST(BasicVectorTest, PlusEqScaled) {
   EXPECT_EQ(ans5, ogvec.get_value());
 }
 
+template <typename T>
+class TypedBasicVectorTest : public ::testing::Test {};
+
+using DefaultScalars =
+    ::testing::Types<double, AutoDiffXd, symbolic::Expression>;
+TYPED_TEST_SUITE(TypedBasicVectorTest, DefaultScalars);
+
 // Tests ability to stream a BasicVector into a string.
-GTEST_TEST(BasicVectorTest, StringStream) {
-  BasicVector<double> vec(3);
+TYPED_TEST(TypedBasicVectorTest, StringStream) {
+  using T = TypeParam;
+  BasicVector<T> vec(3);
   vec.get_mutable_value() << 1.0, 2.2, 3.3;
   std::stringstream s;
   s << "hello " << vec << " world";
-  EXPECT_EQ(s.str(), "hello [1, 2.2, 3.3] world");
+  std::stringstream s_expected;
+  s_expected << "hello " << vec.get_value().transpose() << " world";
+  EXPECT_EQ(s.str(), s_expected.str());
 }
 
 // Tests ability to stream a BasicVector of size zero into a string.
-GTEST_TEST(BasicVectorTest, ZeroLengthStringStream) {
-  BasicVector<double> vec(0);
+TYPED_TEST(TypedBasicVectorTest, ZeroLengthStringStream) {
+  using T = TypeParam;
+  BasicVector<T> vec(0);
   std::stringstream s;
-  s << "foo " << vec << " bar";
-  EXPECT_EQ(s.str(), "foo [] bar");
+  s << "foo [" << vec << "] bar";
+  std::stringstream s_expected;
+  s_expected << "foo [" << vec.get_value().transpose() << "] bar";
+  EXPECT_EQ(s.str(), s_expected.str());
 }
 
 // Tests the default set of bounds (empty).
