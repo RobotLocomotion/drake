8e68c3d881c7589e2d336ccc83c458db4ccd4eb4
hongkai-dai <daihongkai@gmail.com>
2020-06-02 08:28:00 -0700

    add dual solution for Gurobi QCP.

--- bindings/pydrake/solvers/test/gurobi_solver_test.py
+++ bindings/pydrake/solvers/test/gurobi_solver_test.py
@@ -23,6 +23,18 @@ class TestMathematicalProgram(unittest.TestCase):
         self.assertEqual(result.get_solver_details().optimization_status, 2)
         self.assertTrue(np.isnan(result.get_solver_details().objective_bound))
 
+    def test_gurobi_socp_dual(self):
+        prog = mp.MathematicalProgram()
+        x = prog.NewContinuousVariables(2, "x")
+        constraint = prog.AddLorentzConeConstraint([2., 2*x[0], 3 * x[1] + 1])
+        prog.AddLinearCost(x[1])
+        solver = GurobiSolver()
+        options = mp.SolverOptions()
+        options.SetOption(solver.solver_id(), "QCPDual", 1)
+        result = solver.Solve(prog, None, options)
+        np.testing.assert_allclose(
+            result.GetDualSolution(constraint), np.array([-1./12]), atol=1e-7)
+
     def test_gurobi_license(self):
         # Nominal use case.
         with GurobiSolver.AcquireLicense():
