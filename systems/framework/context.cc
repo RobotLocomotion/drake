5a79cc67421f62c9ecb2aada4b21b0c1b3ccd1b1
Michael Sherman <sherm1@gmail.com>
2020-06-02 08:48:46 -0700

    Make CalcNextUpdateTime() "right now" returns work during initialization (#13438)

--- systems/framework/context.cc
+++ systems/framework/context.cc
@@ -1,7 +1,4 @@
 #include "drake/systems/framework/context.h"
 
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
-    struct ::drake::systems::StepInfo)
-
-DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
     class ::drake::systems::Context)
