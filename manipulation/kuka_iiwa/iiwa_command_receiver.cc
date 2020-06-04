0eff544e061d273c8407e4052353f6ba07b06897
Siyuan Feng <siyuan.feng@tri.global>
2020-06-04 16:18:38 +0800

    add latching for iiwa_cmd_recv

--- manipulation/kuka_iiwa/iiwa_command_receiver.cc
+++ manipulation/kuka_iiwa/iiwa_command_receiver.cc
@@ -1,5 +1,7 @@
 #include "drake/manipulation/kuka_iiwa/iiwa_command_receiver.h"
 
+#include <limits>
+
 #include "drake/common/drake_assert.h"
 #include "drake/common/drake_throw.h"
 #include "drake/common/text_logging.h"
@@ -29,11 +31,18 @@ IiwaCommandReceiver::IiwaCommandReceiver(int num_joints)
       DeclareNumericParameter(default_position)};
   DRAKE_DEMAND(param == 0);  // We're depending on that elsewhere.
 
-  DeclareAbstractInputPort("lcmt_iiwa_command", *MakeCommandMessage());
+  const auto& msg_input_id =
+      DeclareAbstractInputPort("lcmt_iiwa_command", *MakeCommandMessage());
+  const auto flag_id = DeclareDiscreteState(Eigen::VectorXd::Zero(1));
+  const auto latched_q_id =
+      DeclareDiscreteState(Eigen::VectorXd::Zero(num_joints));
+
   DeclareInputPort("position_measured", systems::kVectorValued, num_joints_);
   groomed_input_ = &DeclareCacheEntry(
       "groomed_input", &IiwaCommandReceiver::CalcInput,
-      {all_input_ports_ticket(), numeric_parameter_ticket(param)});
+      {discrete_state_ticket(flag_id), discrete_state_ticket(latched_q_id),
+       input_port_ticket(msg_input_id.get_index()),
+       numeric_parameter_ticket(param)});
 
   DeclareVectorOutputPort(
       "position", BasicVector<double>(num_joints),
@@ -68,6 +77,41 @@ void IiwaCommandReceiver::set_initial_position(
   context->get_mutable_numeric_parameter(0).SetFromVector(q);
 }
 
+void IiwaCommandReceiver::DoCalcNextUpdateTime(
+    const systems::Context<double>& context,
+    systems::CompositeEventCollection<double>* events, double* time) const {
+  // We do not support events other than our own message timing events.
+  LeafSystem<double>::DoCalcNextUpdateTime(context, events, time);
+  DRAKE_THROW_UNLESS(events->HasEvents() == false);
+  DRAKE_THROW_UNLESS(std::isinf(*time));
+
+  if (context.get_discrete_state(0).get_value()[0] == 0) {
+    // Schedule a discrete update event at now to set the position command.
+    *time = context.get_time();
+    auto& discrete_events = events->get_mutable_discrete_update_events();
+
+    systems::DiscreteUpdateEvent<double>::DiscreteUpdateCallback callback =
+        [this](const systems::Context<double>& c,
+               const systems::DiscreteUpdateEvent<double>&,
+               systems::DiscreteValues<double>* new_discrete_values) {
+          drake::log()->info("Latching position command at t = {}",
+                             c.get_time());
+          const VectorXd positions =
+              get_position_measured_input_port().HasValue(c)
+                  ? get_position_measured_input_port().Eval(c)
+                  : c.get_numeric_parameter(0).get_value();
+          new_discrete_values->get_mutable_vector(0).get_mutable_value()[0] = 1;
+          new_discrete_values->get_mutable_vector(1).get_mutable_value() =
+              positions;
+        };
+    discrete_events.add_event(
+        std::make_unique<systems::DiscreteUpdateEvent<double>>(callback));
+  } else {
+    // Already latched, no-op.
+    *time = std::numeric_limits<double>::infinity();
+  }
+}
+
 // Returns (in "result") the command message input, or if a message has not
 // been received yet returns the a fallback value.  The result always has
 // num_joints_ positions and torques.
@@ -80,14 +124,9 @@ void IiwaCommandReceiver::CalcInput(
   // Copies the input value into our tentative result.
   *result = get_message_input_port().Eval<lcmt_iiwa_command>(context);
 
-  // If we haven't received a non-default message yet, use nominal values
-  // instead.  N.B. This works due to lcm::Serializer<>::CreateDefaultValue()
-  // using value-initialization.
   if (lcm::AreLcmMessagesEqual(*result, lcmt_iiwa_command{})) {
-    const VectorXd positions =
-        get_position_measured_input_port().HasValue(context) ?
-            get_position_measured_input_port().Eval(context) :
-            context.get_numeric_parameter(0).get_value();
+    // Haven't received a valid lcm command yet, use the fallback value.
+    const VectorXd positions = context.get_discrete_state(1).get_value();
     result->num_joints = positions.size();
     result->joint_position =
         {positions.data(), positions.data() + positions.size()};
