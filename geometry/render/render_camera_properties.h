7056677e30007408062da80ff6748c2d75ba048e
Sean Curtis <sean.curtis@tri.global>
2020-06-16 14:25:28 -0700

    Address reviewer comments

--- geometry/render/render_camera_properties.h
+++ geometry/render/render_camera_properties.h
@@ -22,6 +22,18 @@ class RenderCameraProperties {
   /** Constructs properties for the given render engine. The clipping planes
    can be omitted to use the indicated default values.
 
+   The default values for the clipping planes are motivated by applications of
+   common human-scaled manipulation tasks. As such, these clipping values cover
+   a range appropriate for viewing small objects up close within a large
+   domestic environments.
+
+   Do keep in mind the service range of your depth sensor. It is important that
+   the clipping planes span the range of your depth sensor, otherwise the depth
+   returns will be clipped. For example, the default far clipping plane
+   coincides with the maximum range reported for the
+   <a href="https://www.intelrealsense.com/depth-camera-d415/">
+   Intel RealSense D415 camera</a>.
+
    @pre `near_clipping > 0` and `far_clipping > 0`.
    @pre `far_clipping > near_clipping`.  */
   RenderCameraProperties(std::string render_engine_name,
