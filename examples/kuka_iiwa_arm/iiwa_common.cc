3ce25c11cb2017656eaa14987f557dfdcc7316be
Jeremy Nimmer <jeremy.nimmer@tri.global>
2020-06-30 23:15:43 -0400

    examples: Remove deprecated methods 2020-07-01

--- examples/kuka_iiwa_arm/iiwa_common.cc
+++ examples/kuka_iiwa_arm/iiwa_common.cc
@@ -1,17 +1,6 @@
 #include "drake/examples/kuka_iiwa_arm/iiwa_common.h"
 
-#include <map>
-#include <string>
-#include <utility>
-#include <vector>
-
-#include "drake/common/drake_assert.h"
-#include "drake/common/eigen_types.h"
-#include "drake/common/text_logging.h"
-#include "drake/manipulation/util/robot_plan_utils.h"
-
-using std::string;
-using std::vector;
+#include <cmath>
 
 namespace drake {
 namespace examples {
@@ -46,64 +35,6 @@ void SetTorqueControlledIiwaGains(Eigen::VectorXd* stiffness,
   // A dimensionless damping ratio. See KukaTorqueController for details.
   damping_ratio->resize(stiffness->size());
   damping_ratio->setConstant(1.0);
-}
-
-void ApplyJointVelocityLimits(const MatrixX<double>& keyframes,
-                              std::vector<double>* time) {
-  DRAKE_DEMAND(keyframes.cols() == static_cast<int>(time->size()));
-
-  const int num_time_steps = keyframes.cols();
-
-  // Calculate a matrix of velocities between each timestep.  We'll
-  // use this later to determine by how much the plan exceeds the
-  // joint velocity limits.
-  Eigen::MatrixXd velocities(keyframes.rows(), num_time_steps - 1);
-  for (int i = 0; i < velocities.rows(); i++) {
-    for (int j = 0; j < velocities.cols(); j++) {
-      DRAKE_ASSERT((*time)[j + 1] > (*time)[j]);
-      velocities(i, j) =
-          std::abs((keyframes(i, j + 1) - keyframes(i, j)) /
-                   ((*time)[j + 1] - (*time)[j]));
-    }
-  }
-
-  DRAKE_ASSERT(velocities.rows() == kIiwaArmNumJoints);
-
-  Eigen::VectorXd velocity_ratios(velocities.rows());
-
-  const VectorX<double> iiwa_max_joint_velocities =
-      get_iiwa_max_joint_velocities();
-  for (int i = 0; i < velocities.rows(); i++) {
-    const double max_plan_velocity = velocities.row(i).maxCoeff();
-    // Maybe don't try max velocity at first...
-    velocity_ratios(i) =
-        max_plan_velocity / (iiwa_max_joint_velocities[i] * 0.9);
-  }
-
-  const double max_velocity_ratio = velocity_ratios.maxCoeff();
-  if (max_velocity_ratio > 1) {
-    // The code below slows the entire plan such that the fastest step
-    // meets the limits.  If that step is much faster than the others,
-    // the whole plan becomes very slow.
-    drake::log()->debug("Slowing plan by {}", max_velocity_ratio);
-    for (int j = 0; j < num_time_steps; j++) {
-      (*time)[j] *= max_velocity_ratio;
-    }
-  }
-}
-
-robotlocomotion::robot_plan_t EncodeKeyFrames(
-    const std::vector<std::string>& joint_names,
-    const std::vector<double>& time,
-    const std::vector<int>& info,
-    const MatrixX<double>& keyframes) {
-  std::vector<Eigen::VectorXd> waypoints;
-  for (int i = 0; i < keyframes.cols(); ++i) {
-    waypoints.push_back(keyframes.col(i));
-  }
-
-  return manipulation::util::EncodeKeyFrames(
-      joint_names, time, info, waypoints);
 }
 
 }  // namespace kuka_iiwa_arm
