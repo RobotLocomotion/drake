132ae201cc386c2bea5ef1bd8b1cab9c22b90428
Rufus Wong <rcywongaa@gmail.com>
2020-07-10 23:49:28 +0800

    Resolve conflict

--- bindings/pydrake/multibody/test/plant_test.py
+++ bindings/pydrake/multibody/test/plant_test.py
@@ -587,6 +587,13 @@ class TestPlant(unittest.TestCase):
             self.assert_sane(Js_v_AB_E)
             self.assertEqual(Js_v_AB_E.shape, (3, nw))
 
+            Js_v_AB_E = plant.CalcJacobianTranslationalVelocity(
+                context=context, with_respect_to=wrt, frame_B=base_frame,
+                p_BoBi_B=np.zeros((3, 3)), frame_A=world_frame,
+                frame_E=world_frame)
+            self.assert_sane(Js_v_AB_E)
+            self.assertEqual(Js_v_AB_E.shape, (9, nw))
+
         AsBias_ABp_E = plant.CalcBiasSpatialAcceleration(
             context=context, with_respect_to=JacobianWrtVariable.kV,
             frame_B=base_frame, p_BoBp_B=np.zeros(3), frame_A=world_frame,
@@ -599,13 +606,6 @@ class TestPlant(unittest.TestCase):
             frame_E=world_frame)
         self.assert_sane(asBias_ABi_E, nonzero=False)
         self.assertEqual(asBias_ABi_E.shape, (3, 1))
-
-            Js_v_AB_E = plant.CalcJacobianTranslationalVelocity(
-                context=context, with_respect_to=wrt, frame_B=base_frame,
-                p_BoBi_B=np.zeros((3, 3)), frame_A=world_frame,
-                frame_E=world_frame)
-            self.assert_sane(Js_v_AB_E)
-            self.assertEqual(Js_v_AB_E.shape, (9, nw))
 
         # Compute body pose.
         X_WBase = plant.EvalBodyPoseInWorld(context, base)
