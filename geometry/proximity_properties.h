141eda1462ab103879971ab980f32a4e56391122
Sean Curtis <sean.curtis@tri.global>
2020-07-17 16:02:54 -0700

    ProximityProperties has canonical properties (and validates them)
     - Introduce the concept of Drake "canonical" properties, using
       ProximityProperties as the initial implementation.
       - Provide example for documenting it.
     - moved internal::HydroelasticType into its own header so that
       geometry_roles could make use of it.
     - Added printing to CoulombFriction.
     - hydroelastic_internal no longer had to validate type/value of expected
       properties (only presence). This simplified that code and its tests.

--- geometry/proximity_properties.h
+++ geometry/proximity_properties.h
@@ -11,6 +11,7 @@
 #include <ostream>
 
 #include "drake/geometry/geometry_roles.h"
+#include "drake/geometry/proximity/hydroelastic_type.h"
 #include "drake/multibody/plant/coulomb_friction.h"
 
 namespace drake {
@@ -68,23 +69,6 @@ extern const char* const kSlabThickness;    ///< Slab thickness property name
                                             ///< (for half spaces).
 
 //@}
-
-// TODO(SeanCurtis-TRI): Update this to have an additional classification: kBoth
-//  when we have the need from the algorithm. For example: when we have two
-//  very stiff objects, we'd want to process them as soft. But when one
-//  very stiff and one very soft object interact, it might make sense to
-//  consider the stiff object as effectively rigid and simplify the computation.
-//  In this case, the object would get two representations.
-/* Classification of the type of representation a shape has for the
- hydroelastic contact model: rigid or soft.  */
-enum class HydroelasticType {
-  kUndefined,
-  kRigid,
-  kSoft
-};
-
-/* Streaming operator for writing hydroelastic type to output stream.  */
-std::ostream& operator<<(std::ostream& out, const HydroelasticType& type);
 
 }  // namespace internal
 
