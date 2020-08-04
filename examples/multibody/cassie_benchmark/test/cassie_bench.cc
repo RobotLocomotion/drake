997f49ebca3d6b6ab891c678cd78e19ed84bc192
Rick Poyner <rick.poyner@tri.global>
2020-08-04 17:40:55 -0400

    wip: respond to comments

--- examples/multibody/cassie_benchmark/test/cassie_bench.cc
+++ examples/multibody/cassie_benchmark/test/cassie_bench.cc
@@ -25,6 +25,16 @@ namespace drake {
 namespace examples {
 namespace {
 
+// According to research from @sherm1, the caching system has some
+// debug-only asserts that allocate memory. Hence there are two different
+// ceilings for allocations until that is addressed.
+#ifdef NDEBUG
+#define RELEASE_LIMIT_MALLOC(x) \
+  drake::test::LimitMalloc guard({.max_num_allocations = (x)});
+#else
+#define RELEASE_LIMIT_MALLOC(x) /* empty */
+#endif
+
 class DoubleFixture : public benchmark::Fixture {
  public:
   // This apparently futile using statement works around "overloaded virtual"
@@ -44,10 +54,12 @@ class DoubleFixture : public benchmark::Fixture {
     nv_ = plant_->num_velocities();
     nu_ = plant_->num_actuators();
 
-    x_ = VectorXd::Zero(nq_ + nv_);
+    context_ = plant_->CreateDefaultContext();
+
     u_ = VectorXd::Zero(nu_);
 
-    context_ = plant_->CreateDefaultContext();
+    // Use default state to avoid problems with all-zero quaternions.
+    x_ = context_->get_continuous_state_vector().CopyToVector();
   }
 
   systems::DiagramBuilder<double> builder_;
@@ -56,54 +68,46 @@ class DoubleFixture : public benchmark::Fixture {
   int nq_{};
   int nv_{};
   int nu_{};
-  VectorXd x_{};
   VectorXd u_{};
+  VectorXd x_{};
 };
 
 // NOLINTNEXTLINE(runtime/references) cpplint disapproves of gbench choices.
 BENCHMARK_F(DoubleFixture, DoubleMassMatrix)(benchmark::State& state) {
   MatrixXd M(nv_, nv_);
-  int i = 0;
   for (auto _ : state) {
-    x_(0) = i;
+    RELEASE_LIMIT_MALLOC(175);
+    context_->NoteContinuousStateChange();
     plant_->SetPositionsAndVelocities(context_.get(), x_);
     plant_->CalcMassMatrix(*context_, &M);
-    i++;
   }
 }
 
 // NOLINTNEXTLINE(runtime/references) cpplint disapproves of gbench choices.
 BENCHMARK_F(DoubleFixture, DoubleInverseDynamics)(benchmark::State& state) {
-  VectorXd desired_vdot;
-  VectorXd x = VectorXd::Zero(nq_ + nv_);
+  VectorXd desired_vdot = VectorXd::Zero(nv_);
   multibody::MultibodyForces<double> external_forces(*plant_);
-  int i = 0;
   for (auto _ : state) {
-    x = VectorXd::Constant(nq_ + nv_, i);
-    desired_vdot = VectorXd::Constant(nv_, i);
-    plant_->SetPositionsAndVelocities(context_.get(), x);
+    RELEASE_LIMIT_MALLOC(3);
+    context_->NoteContinuousStateChange();
+    plant_->SetPositionsAndVelocities(context_.get(), x_);
     plant_->CalcInverseDynamics(*context_, desired_vdot, external_forces);
-    i++;
   }
 }
 
 // NOLINTNEXTLINE(runtime/references) cpplint disapproves of gbench choices.
 BENCHMARK_F(DoubleFixture, DoubleForwardDynamics)(benchmark::State& state) {
-  VectorXd x = VectorXd::Zero(nq_ + nv_);
-  VectorXd u = VectorXd::Zero(nu_);
   auto derivatives = plant_->AllocateTimeDerivatives();
-  int i = 0;
+  auto& port_value =
+      plant_->get_actuation_input_port().FixValue(context_.get(), u_);
   for (auto _ : state) {
-    // Add one to i here to avoid the 0-inertia singularity.
-    x = VectorXd::Constant(nq_ + nv_, i + 1);
-    u = VectorXd::Constant(nu_, i);
-    context_->FixInputPort(plant_->get_actuation_input_port().get_index(), u);
-    plant_->SetPositionsAndVelocities(context_.get(), x);
+    RELEASE_LIMIT_MALLOC(22);
+    context_->NoteContinuousStateChange();
+    port_value.GetMutableData();
+    plant_->SetPositionsAndVelocities(context_.get(), x_);
     plant_->CalcTimeDerivatives(*context_, derivatives.get());
-    i++;
   }
 }
-
 
 class AutodiffFixture : public DoubleFixture {
  public:
@@ -118,70 +122,51 @@ class AutodiffFixture : public DoubleFixture {
   std::unique_ptr<Context<AutoDiffXd>> context_autodiff_;
 };
 
-// According to research from @sherm1, the caching system has some
-// debug-only asserts that allocate memory. Hence there are two different
-// ceilings for allocations until that is addressed.
-#ifdef NDEBUG
-#define RELEASE_LIMIT_MALLOC(x) \
-  drake::test::LimitMalloc guard({.max_num_allocations = (x)});
-#else
-#define RELEASE_LIMIT_MALLOC(x) /* empty */
-#endif
-
 // NOLINTNEXTLINE(runtime/references) cpplint disapproves of gbench choices.
 BENCHMARK_F(AutodiffFixture, AutodiffMassMatrix)(benchmark::State& state) {
   MatrixX<AutoDiffXd> M_autodiff(nv_, nv_);
-  int i = 0;
+  auto x_autodiff = math::initializeAutoDiff(x_);
   for (auto _ : state) {
-    RELEASE_LIMIT_MALLOC(62567);
-    x_(0) = i;
-    plant_autodiff_->SetPositionsAndVelocities(
-        context_autodiff_.get(), math::initializeAutoDiff(x_));
+    RELEASE_LIMIT_MALLOC(62476);
+    context_autodiff_->NoteContinuousStateChange();
+    plant_autodiff_->SetPositionsAndVelocities(context_autodiff_.get(),
+                                               x_autodiff);
     plant_autodiff_->CalcMassMatrix(*context_autodiff_, &M_autodiff);
-    i++;
   }
 }
 
 // NOLINTNEXTLINE(runtime/references) cpplint disapproves of gbench choices.
 BENCHMARK_F(AutodiffFixture, AutodiffInverseDynamics)(benchmark::State& state) {
-  VectorXd desired_vdot;
-  VectorXd x = VectorXd::Zero(nq_ + nv_);
+  VectorXd desired_vdot = VectorXd::Zero(nv_);
   multibody::MultibodyForces<AutoDiffXd> external_forces_autodiff(
       *plant_autodiff_);
-  int i = 0;
+  auto x_autodiff = math::initializeAutoDiff(x_, nq_ + 2 * nv_);
+  auto vdot_autodiff =
+      math::initializeAutoDiff(desired_vdot, nq_ + 2 * nv_, nq_ + nv_);
   for (auto _ : state) {
-    RELEASE_LIMIT_MALLOC(70438);
-    x = VectorXd::Constant(nq_ + nv_, i);
-    desired_vdot = VectorXd::Constant(nv_, i);
-    plant_autodiff_->SetPositionsAndVelocities(
-        context_autodiff_.get(),
-        math::initializeAutoDiff(x, nq_ + 2 * nv_));
-    plant_autodiff_->CalcInverseDynamics(
-        *context_autodiff_,
-        math::initializeAutoDiff(desired_vdot, nq_ + 2 * nv_, nq_ + nv_),
-        external_forces_autodiff);
-    i++;
+    RELEASE_LIMIT_MALLOC(70301);
+    context_autodiff_->NoteContinuousStateChange();
+    plant_autodiff_->SetPositionsAndVelocities(context_autodiff_.get(),
+                                               x_autodiff);
+    plant_autodiff_->CalcInverseDynamics(*context_autodiff_,
+                                         vdot_autodiff,
+                                         external_forces_autodiff);
   }
 }
 
 // NOLINTNEXTLINE(runtime/references) cpplint disapproves of gbench choices.
 BENCHMARK_F(AutodiffFixture, AutodiffForwardDynamics)(benchmark::State& state) {
-  VectorXd x = VectorXd::Zero(nq_ + nv_);
-  VectorXd u = VectorXd::Zero(nu_);
   auto derivatives_autodiff = plant_autodiff_->AllocateTimeDerivatives();
-  int i = 0;
+  auto u_autodiff = math::initializeAutoDiff(u_, nq_ + nv_ + nu_, nq_ + nv_);
+  auto& port_value = plant_autodiff_->get_actuation_input_port().FixValue(
+      context_autodiff_.get(), u_autodiff);
+  auto x_autodiff = math::initializeAutoDiff(x_, nq_ + nv_ + nu_);
   for (auto _ : state) {
-    RELEASE_LIMIT_MALLOC(105834);
-    i++;
-    x = VectorXd::Constant(nq_ + nv_, i);
-    u = VectorXd::Constant(nu_, i);
-
-    context_autodiff_->FixInputPort(
-        plant_autodiff_->get_actuation_input_port().get_index(),
-        math::initializeAutoDiff(u, nq_ + nv_ + nu_, nq_ + nv_));
-    plant_autodiff_->SetPositionsAndVelocities(
-        context_autodiff_.get(),
-        math::initializeAutoDiff(x, nq_ + nv_ + nu_));
+    RELEASE_LIMIT_MALLOC(105675);
+    context_autodiff_->NoteContinuousStateChange();
+    port_value.GetMutableData();
+    plant_autodiff_->SetPositionsAndVelocities(context_autodiff_.get(),
+                                               x_autodiff);
     plant_autodiff_->CalcTimeDerivatives(*context_autodiff_,
                                          derivatives_autodiff.get());
   }
