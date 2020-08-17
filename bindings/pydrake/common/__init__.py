cb86fa433bcda812e44643cb61a97a46da988ff7
Eric Cousineau <eric.cousineau@tri.global>
2020-08-17 10:34:40 -0400

    pydrake math: Ensure that .multiply preserves input shape

--- bindings/pydrake/common/__init__.py
+++ bindings/pydrake/common/__init__.py
@@ -1,1 +1,31 @@
+import collections
+import functools
+import inspect
+
+import numpy as np
+
 from ._module_py import *
+
+
+def _wrap_to_match_input_shape(f):
+    # See docstring for `WrapToMatchInputShape` in `eigen_pybind.h` for more
+    # details.
+    # N.B. We cannot use `inspect.Signature` due to the fact that pybind11's
+    # instance method is not inspectable for overloads.
+    assert callable(f), f
+
+    @functools.wraps(f)
+    def wrapper(self, *args, **kwargs):
+        # Call the function first to permit it to raise the appropriate
+        # TypeError from pybind11 if the inputs are not correctly formatted.
+        out = f(self, *args, **kwargs)
+        if isinstance(out, np.ndarray):
+            arg_list = tuple(args) + tuple(kwargs.values())
+            assert len(arg_list) == 1
+            (arg,) = arg_list
+            in_shape = np.asarray(arg).shape
+            return out.reshape(in_shape)
+        else:
+            return out
+
+    return wrapper
