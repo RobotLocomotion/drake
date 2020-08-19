e70c130261ac316bb82afd34b78786bf118081fd
Jeremy Nimmer <jeremy.nimmer@tri.global>
2020-08-19 13:49:04 -0400

    framework: Mark subvector and supervector final
    This will make the h/cc split clearer in the future, as well as
    allow us to improve the implementations and inheritance tree of
    the VectorBase family.
    
    This is a breaking change, in the unlikely event that anyone was
    inheriting from these classes.
    
    Also deprecate a worthless constructor of Subvector.

--- systems/framework/continuous_state.h
+++ systems/framework/continuous_state.h
@@ -6,7 +6,6 @@
 #include "drake/common/drake_assert.h"
 #include "drake/common/drake_copyable.h"
 #include "drake/common/drake_throw.h"
-#include "drake/systems/framework/basic_vector.h"
 #include "drake/systems/framework/framework_common.h"
 #include "drake/systems/framework/scalar_conversion_traits.h"
 #include "drake/systems/framework/vector_base.h"
