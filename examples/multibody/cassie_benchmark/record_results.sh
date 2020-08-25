914488f3d3d0b05ebb1d268446ebd07a98e67f97
Rick Poyner <rick.poyner@tri.global>
2020-08-25 18:49:41 -0400

    cassie bench: Add processor affinity control for Linux
    Relevant to #13902.
    
    On linux only, invoke the benchmark under the `taskset` command. Add a
    bit of documentation.

--- examples/multibody/cassie_benchmark/record_results.sh
+++ examples/multibody/cassie_benchmark/record_results.sh
@@ -7,10 +7,17 @@ set -e -u -o pipefail
 
 uname -a > ${TEST_UNDECLARED_OUTPUTS_DIR}/kernel.txt || true
 
+# Fill this in with a platform-specific command to control processor affinity,
+# if any.
+AFFINITY_COMMAND=""
+
 (
 case $(uname) in
     Linux)
         lsb_release -idrc
+        # Choosing processor #0 is arbitrary. It is up to experimenters
+        # to ensure it is reliably idle during experiments.
+        AFFINITY_COMMAND="taskset 0x1"
         ;;
     Darwin)
         sw_vers
@@ -24,6 +31,7 @@ esac
 ${TEST_SRCDIR}/drake/tools/workspace/cc/identify_compiler \
  > ${TEST_UNDECLARED_OUTPUTS_DIR}/compiler.txt
 
+${AFFINITY_COMMAND} \
 ${TEST_SRCDIR}/drake/examples/multibody/cassie_benchmark/cassie_bench \
     --benchmark_display_aggregates_only=true \
     --benchmark_repetitions=9 \
