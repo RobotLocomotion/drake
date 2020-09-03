883439824ec638802bf9b3f9b8ed3843493f1741
Russ Tedrake <russt@mit.edu>
2020-09-02 21:55:09 -0400

    DifferentialIKSystem: Add (optional) discrete state to "warn once"
    This significantly improves the robustness of the teleoperation demo on colab.  Currently, if a user drives the robot into singularity (and it is relatively common mistake), then the diff IK spews so many warnings that the sliders disappear (and so does the "stop simulation" button).  This quiets that behavior significantly.

--- manipulation/planner/test/differential_inverse_kinematics_integrator_test.cc
+++ manipulation/planner/test/differential_inverse_kinematics_integrator_test.cc
@@ -43,6 +43,11 @@ GTEST_TEST(DifferentialInverseKinematicsIntegatorTest, BasicTest) {
       *robot, frame_E, time_step, params);
   auto diff_ik_context = diff_ik.CreateDefaultContext();
 
+  // Set the results status state to a failure state.
+  diff_ik_context->get_mutable_discrete_state(1).SetAtIndex(
+      0, static_cast<double>(
+             DifferentialInverseKinematicsStatus::kNoSolutionFound));
+
   Eigen::VectorXd q(7);
   q << 0.1, 0.2, -.15, 0.43, -0.32, -0.21, 0.11;
 
@@ -60,6 +65,11 @@ GTEST_TEST(DifferentialInverseKinematicsIntegatorTest, BasicTest) {
   auto discrete_state = diff_ik.AllocateDiscreteVariables();
   diff_ik.CalcDiscreteVariableUpdates(*diff_ik_context, discrete_state.get());
 
+  // Confirm that the result status state was updated properly.
+  EXPECT_EQ(
+      discrete_state->get_vector(1).GetAtIndex(0),
+      static_cast<double>(DifferentialInverseKinematicsStatus::kSolutionFound));
+
   params.set_timestep(time_step);  // intentionally set this after diff_ik call
   DifferentialInverseKinematicsResult result = DoDifferentialInverseKinematics(
       *robot, *robot_context, X_WE_desired.GetAsIsometry3(), frame_E, params);
@@ -67,6 +77,20 @@ GTEST_TEST(DifferentialInverseKinematicsIntegatorTest, BasicTest) {
   EXPECT_EQ(result.status, DifferentialInverseKinematicsStatus::kSolutionFound);
   EXPECT_TRUE(CompareMatrices(q + time_step * result.joint_velocities.value(),
                               discrete_state->get_vector(0).get_value()));
+
+  // Add infeasible constraints, and confirm the result state.
+  // clang-format off
+  const auto A = (MatrixX<double>(2, 7) <<
+      1, -1,  0, 0, 0, 0, 0,
+      0,  1, -1, 0, 0, 0, 0).finished();
+  // clang-format on
+  const auto b = Eigen::VectorXd::Zero(A.rows());
+  diff_ik.get_mutable_parameters().AddLinearVelocityConstraint(
+      std::make_shared<solvers::LinearConstraint>(A, b, b));
+  diff_ik.CalcDiscreteVariableUpdates(*diff_ik_context, discrete_state.get());
+  EXPECT_EQ(discrete_state->get_vector(1).GetAtIndex(0),
+            static_cast<double>(
+                DifferentialInverseKinematicsStatus::kStuck));
 }
 
 GTEST_TEST(DifferentialInverseKinematicsIntegatorTest, ParametersTest) {
@@ -84,6 +108,29 @@ GTEST_TEST(DifferentialInverseKinematicsIntegatorTest, ParametersTest) {
   EXPECT_EQ(diff_ik.get_parameters().get_timestep(), time_step);
   diff_ik.get_mutable_parameters().set_timestep(0.2);
   EXPECT_EQ(diff_ik.get_parameters().get_timestep(), 0.2);
+}
+
+// Confirm that we can act like a difference equation system when the warning
+// logic is disabled.
+GTEST_TEST(DifferentialInverseKinematicsIntegatorTest,
+           DifferenceEquationSystemTest) {
+  auto robot = MakeIiwa();
+  auto robot_context = robot->CreateDefaultContext();
+  const multibody::Frame<double>& frame_E =
+      robot->GetFrameByName("iiwa_link_7");
+
+  DifferentialInverseKinematicsParameters params(robot->num_positions(),
+                                                 robot->num_velocities());
+  const double time_step = 0.1;
+
+  DifferentialInverseKinematicsIntegrator diff_ik_with_status_state(
+      *robot, frame_E, time_step, params);
+  EXPECT_EQ(diff_ik_with_status_state.num_discrete_state_groups(), 2);
+
+  DifferentialInverseKinematicsIntegrator diff_ik_without_status_state(
+      *robot, frame_E, time_step, params, nullptr, false);
+  EXPECT_EQ(diff_ik_without_status_state.num_discrete_state_groups(), 1);
+  EXPECT_TRUE(diff_ik_without_status_state.IsDifferenceEquationSystem());
 }
 
 }  // namespace
