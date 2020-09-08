de3bb85f31e7fbb49664fbaf22b22e7a3b6e609f
hongkai-dai <daihongkai@gmail.com>
2020-09-08 13:54:34 -0700

    Construct a generic polynomial from expression.

--- common/test_utilities/symbolic_test_util.h
+++ common/test_utilities/symbolic_test_util.h
@@ -76,6 +76,27 @@ template <typename BasisElement>
   return p1.EqualTo(p2);
 }
 
+template <typename BasisElement>
+[[nodiscard]] inline bool GenericPolyNotEqual(
+    const GenericPolynomial<BasisElement>& p1,
+    const GenericPolynomial<BasisElement>& p2) {
+  return !p1.EqualTo(p2);
+}
+
+template <typename BasisElement>
+[[nodiscard]] inline bool GenericPolyEqualAfterExpansion(
+    const GenericPolynomial<BasisElement>& p1,
+    const GenericPolynomial<BasisElement>& p2) {
+  return p1.EqualToAfterExpansion(p2);
+}
+
+template <typename BasisElement>
+[[nodiscard]] inline bool GenericPolyNotEqualAfterExpansion(
+    const GenericPolynomial<BasisElement>& p1,
+    const GenericPolynomial<BasisElement>& p2) {
+  return !p1.EqualToAfterExpansion(p2);
+}
+
 [[nodiscard]] inline bool PolyEqual(const Polynomial& p1,
                                       const Polynomial& p2) {
   return p1.EqualTo(p2);
