d667deae8a6d6504576e37742d0afdf019f80b01
Eric Cousineau <eric.cousineau@tri.global>
2020-09-17 17:49:31 -0400

    py mbp: Make SpatialVector's default constructor initialize to NaN

--- bindings/pydrake/multibody/math_py.cc
+++ bindings/pydrake/multibody/math_py.cc
@@ -38,6 +38,18 @@ void BindSpatialVectorMixin(PyClass* pcls) {
   using Class = typename PyClass::type;
   auto& cls = *pcls;
   cls  // BR
+      .def(py::init([]() {
+        Class out;
+        out.SetNaN();
+        return out;
+      }),
+          R"""(
+      Constructs to all NaNs.
+
+      Note:
+          This is different from C++, which in Release builds may leave memory
+          uninitialized. For more details, see issue #14086.
+      )""")
       .def(
           "rotational",
           [](const Class* self) -> const Vector3<T> {
@@ -98,7 +110,6 @@ void DoScalarDependentDefinitions(py::module m, T) {
         m, "SpatialVelocity", param, cls_doc.doc);
     BindSpatialVectorMixin<T>(&cls);
     cls  // BR
-        .def(py::init(), cls_doc.ctor.doc_0args)
         .def(py::init<const Eigen::Ref<const Vector3<T>>&,
                  const Eigen::Ref<const Vector3<T>>&>(),
             py::arg("w"), py::arg("v"), cls_doc.ctor.doc_2args)
@@ -128,7 +139,6 @@ void DoScalarDependentDefinitions(py::module m, T) {
         m, "SpatialMomentum", param, cls_doc.doc);
     BindSpatialVectorMixin<T>(&cls);
     cls  // BR
-        .def(py::init(), cls_doc.ctor.doc_0args)
         .def(py::init<const Eigen::Ref<const Vector3<T>>&,
                  const Eigen::Ref<const Vector3<T>>&>(),
             py::arg("h"), py::arg("l"), cls_doc.ctor.doc_2args)
@@ -145,7 +155,6 @@ void DoScalarDependentDefinitions(py::module m, T) {
         m, "SpatialAcceleration", param, cls_doc.doc);
     BindSpatialVectorMixin<T>(&cls);
     cls  // BR
-        .def(py::init(), cls_doc.ctor.doc_0args)
         .def(py::init<const Eigen::Ref<const Vector3<T>>&,
                  const Eigen::Ref<const Vector3<T>>&>(),
             py::arg("alpha"), py::arg("a"), cls_doc.ctor.doc_2args)
@@ -162,7 +171,6 @@ void DoScalarDependentDefinitions(py::module m, T) {
         m, "SpatialForce", param, cls_doc.doc);
     BindSpatialVectorMixin<T>(&cls);
     cls  // BR
-        .def(py::init(), cls_doc.ctor.doc_0args)
         .def(py::init<const Eigen::Ref<const Vector3<T>>&,
                  const Eigen::Ref<const Vector3<T>>&>(),
             py::arg("tau"), py::arg("f"), cls_doc.ctor.doc_2args)
