c95433266f8456e1915f60e7c6deea08cd4813f1
Eric Cousineau <eric.cousineau@tri.global>
2020-09-19 12:19:25 -0400

    py mbp: Bind operators for SpatialVector; bind shifting for velocity and force

--- bindings/pydrake/multibody/math_py.cc
+++ bindings/pydrake/multibody/math_py.cc
@@ -1,4 +1,5 @@
 #include "pybind11/eigen.h"
+#include "pybind11/operators.h"
 #include "pybind11/pybind11.h"
 
 #include "drake/bindings/pydrake/common/cpp_template_pybind.h"
@@ -8,12 +9,27 @@
 #include "drake/bindings/pydrake/documentation_pybind.h"
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/multibody/math/spatial_acceleration.h"
+// N.B. We need this to instantiate definitions of `::dot` (#14097).
+#include "drake/multibody/math/spatial_algebra.h"
 #include "drake/multibody/math/spatial_force.h"
 #include "drake/multibody/math/spatial_momentum.h"
 #include "drake/multibody/math/spatial_velocity.h"
 
+#pragma GCC diagnostic push
+// Similar to `symbolic_py.cc`, we must suppress `-Wself-assign-overloaded` to
+/// use operators for Apple's clang>=10. However, different than
+// `symbolic_py.cc`, we must also enable this for clang 9 on Bionic, as is
+// triggered on `py::self -= py::self` for some reason.
+// It is fine to use this at a file-wide scope since in practice we only
+// encounter these warnings in bindings due to pybind11's operators.
+#if (__clang__) && (__clang_major__ >= 9)
+#pragma GCC diagnostic ignored "-Wself-assign-overloaded"
+#endif
+
 namespace drake {
 namespace pydrake {
+
+using math::RotationMatrix;
 
 // Binds any child classes of the `SpatialVector` mixin.
 template <typename T, typename PyClass>
@@ -38,7 +54,31 @@ void BindSpatialVectorMixin(PyClass* pcls) {
       .def(
           "get_coeffs", [](const Class& self) { return self.get_coeffs(); },
           doc.SpatialVector.get_coeffs.doc_0args_const)
-      .def_static("Zero", &Class::Zero, doc.SpatialVector.Zero.doc);
+      .def_static("Zero", &Class::Zero, doc.SpatialVector.Zero.doc)
+      .def(-py::self)
+      .def(py::self += py::self)
+      .def(py::self -= py::self)
+      .def(py::self *= T{})
+      .def(py::self + py::self)
+      .def(py::self - py::self)
+      .def(py::self * T{})
+      .def(T{} * py::self)
+      .def(
+          "Rotate",
+          [](const Class& self, RotationMatrix<T>& R_FE) {
+            return R_FE * self;
+          },
+          py::arg("R_FE"),
+          R"""(
+          Provides a Python-only implementation of rotating / re-expressing) a
+          spatial vector.
+
+          Note:
+              This is done because defining ``__rmatmul__`` on this class does
+              not disambiguate against the definitions of
+              ``RotationMatrix.__matmul__``.
+          )""");
+  DefCopyAndDeepCopy(&cls);
 }
 
 namespace {
@@ -62,8 +102,21 @@ void DoScalarDependentDefinitions(py::module m, T) {
         .def(py::init<const Eigen::Ref<const Vector3<T>>&,
                  const Eigen::Ref<const Vector3<T>>&>(),
             py::arg("w"), py::arg("v"), cls_doc.ctor.doc_2args)
-        .def(py::init<const Vector6<T>&>(), py::arg("V"),
-            cls_doc.ctor.doc_1args);
+        .def(
+            py::init<const Vector6<T>&>(), py::arg("V"), cls_doc.ctor.doc_1args)
+        .def("ShiftInPlace", &Class::ShiftInPlace, py::arg("p_BpBq_E"),
+            py_rvp::reference, cls_doc.ShiftInPlace.doc)
+        .def("Shift", &Class::Shift, py::arg("p_BpBq_E"), cls_doc.Shift.doc)
+        .def("ComposeWithMovingFrameVelocity",
+            &Class::ComposeWithMovingFrameVelocity, py::arg("p_PoBo_E"),
+            py::arg("V_PB_E"), cls_doc.ComposeWithMovingFrameVelocity.doc)
+        .def("dot",
+            overload_cast_explicit<T, const SpatialForce<T>&>(&Class::dot),
+            py::arg("F_Q_E"), cls_doc.dot.doc_1args_F)
+        .def("dot",
+            overload_cast_explicit<T, const SpatialMomentum<T>&>(&Class::dot),
+            py::arg("L_NBp_E"), cls_doc.dot.doc_1args_L);
+    cls.attr("__matmul__") = cls.attr("dot");
     AddValueInstantiation<Class>(m);
     // Some ports need `Value<std::vector<Class>>`.
     AddValueInstantiation<std::vector<Class>>(m);
@@ -113,8 +166,20 @@ void DoScalarDependentDefinitions(py::module m, T) {
         .def(py::init<const Eigen::Ref<const Vector3<T>>&,
                  const Eigen::Ref<const Vector3<T>>&>(),
             py::arg("tau"), py::arg("f"), cls_doc.ctor.doc_2args)
-        .def(py::init<const Vector6<T>&>(), py::arg("F"),
-            cls_doc.ctor.doc_1args);
+        .def(
+            py::init<const Vector6<T>&>(), py::arg("F"), cls_doc.ctor.doc_1args)
+        .def("ShiftInPlace",
+            overload_cast_explicit<Class&, const Vector3<T>&>(
+                &Class::ShiftInPlace),
+            py::arg("p_BpBq_E"), py_rvp::reference,
+            cls_doc.ShiftInPlace.doc_1args)
+        .def("Shift",
+            overload_cast_explicit<Class, const Vector3<T>&>(&Class::Shift),
+            py::arg("p_BpBq_E"), cls_doc.Shift.doc_1args)
+        .def("dot",
+            overload_cast_explicit<T, const SpatialVelocity<T>&>(&Class::dot),
+            py::arg("V_IBp_E"), cls_doc.dot.doc);
+    cls.attr("__matmul__") = cls.attr("dot");
     AddValueInstantiation<Class>(m);
     // Some ports need `Value<std::vector<Class>>`.
     AddValueInstantiation<std::vector<Class>>(m);
@@ -127,6 +192,7 @@ PYBIND11_MODULE(math, m) {
   using namespace drake::multibody;
 
   py::module::import("pydrake.autodiffutils");
+  py::module::import("pydrake.math");
   py::module::import("pydrake.symbolic");
 
   m.doc() = "Bindings for multibody math.";
@@ -136,3 +202,5 @@ PYBIND11_MODULE(math, m) {
 
 }  // namespace pydrake
 }  // namespace drake
+
+#pragma GCC diagnostic pop
