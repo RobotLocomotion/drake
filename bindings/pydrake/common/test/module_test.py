4af6e3ea41e3dd5c79214a160419fc7302ef514e
Russ Tedrake <russt@mit.edu>
2020-09-20 23:22:37 -0400

    python: document RandomGenerator use with numpy
    Resolves #12632

--- bindings/pydrake/common/test/module_test.py
+++ bindings/pydrake/common/test/module_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os
 import unittest
 
@@ -54,6 +55,16 @@ class TestCommon(unittest.TestCase):
         self.assertEqual(g1(), 3499211612)
         g2 = mut.RandomGenerator(10)
         self.assertEqual(g2(), 3312796937)
+
+    def test_random_numpy_coordination(self):
+        # Verify that multiple numpy generators can be seeded from
+        # a single RandomGenerator without duplicating values (as
+        # discussed in #12632).
+        g = mut.RandomGenerator(123)
+        rs1 = np.random.RandomState(seed=g())
+        rs1.randint(100)  # discard one
+        rs2 = np.random.RandomState(seed=g())
+        self.assertNotEqual(rs1.randint(100), rs2.randint(100))
 
     def test_assert_is_armed(self):
         self.assertIsInstance(mut.kDrakeAssertIsArmed, bool)
