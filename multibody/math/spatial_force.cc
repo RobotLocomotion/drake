ffdb4e1a14e1d6e401d83e06621686f7939d5f74
Eric Cousineau <eric.cousineau@tri.global>
2020-09-22 20:23:49 -0400

    multibody/math: Merge all targets into :spatial_algebra
    Encapsulates linking artifacts due to dependency cycle

--- multibody/math/spatial_force.cc
+++ multibody/math/spatial_force.cc
@@ -1,6 +1,8 @@
 #include "drake/multibody/math/spatial_force.h"
 
 #include "drake/common/default_scalars.h"
+// Necessary to instantiate dot(). See #14097 for more details.
+#include "drake/multibody/math/spatial_algebra.h"
 
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
     class ::drake::multibody::SpatialForce)
