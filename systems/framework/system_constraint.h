7ed34fe07c5c8c890fb6a25df4a600d3e1a094b4
Soonho Kong <soonho.kong@tri.global>
2020-10-02 11:54:25 -0400

    Make compatible with Eigen-3.3.8 (#14159)
    Eigen introduces Eigen::all[1], so we need this patch to avoid name conflicts with drake::all.
    
    [1]: http://eigen.tuxfamily.org/dox-devel/group__Core__Module.html#ga790ab6c4226ef5f678b9eb532a3eab14

--- systems/framework/system_constraint.h
+++ systems/framework/system_constraint.h
@@ -220,19 +220,19 @@ class SystemConstraint final {
     // elegant, and so that double evaluation is as fast as possible.
     if (type() == SystemConstraintType::kEquality) {
       if (tol == 0.0) {
-        return all(value.array() == 0.0);
+        return drake::all(value.array() == 0.0);
       } else {
-        return all(value.cwiseAbs().array() <= tol);
+        return drake::all(value.cwiseAbs().array() <= tol);
       }
     } else {
       DRAKE_ASSERT(type() == SystemConstraintType::kInequality);
       // TODO(hongkai.dai): ignore the bounds that are infinite.
       if (tol == 0.0) {
-        return all(value.array() >= lower_bound().array()) &&
-               all(value.array() <= upper_bound().array());
+        return drake::all(value.array() >= lower_bound().array()) &&
+               drake::all(value.array() <= upper_bound().array());
       } else {
-        return all((value - lower_bound()).array() >= -tol) &&
-               all((upper_bound() - value).array() >= -tol);
+        return drake::all((value - lower_bound()).array() >= -tol) &&
+               drake::all((upper_bound() - value).array() >= -tol);
       }
     }
   }
