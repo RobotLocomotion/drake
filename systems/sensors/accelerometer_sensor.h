7dc5a1edcb793389f1c71255f600c71971de2867
Sean Curtis <sean.curtis@tri.global>
2020-10-08 10:57:04 -0700

    Add back deprecated foo_sensor.h files
    The "restored" header files simply emit deprecation warnings and include
    `foo.h`. The restored headers are added to the build file.

---
+++ systems/sensors/accelerometer_sensor.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// NOLINTNEXTLINE(whitespace/line_length)
+#warning DRAKE DEPRECATED: Please use accelerometer.h instead. The deprecated code will be removed from Drake on or after 2021-02-01.
+
+#include "drake/systems/sensors/accelerometer.h"
