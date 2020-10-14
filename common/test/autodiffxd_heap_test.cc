0f443fa6f48a8c812796c62060d5e00560e8cc45
Michael Sherman <sherm1@gmail.com>
2020-10-14 11:51:22 -0700

    Some experimental hacks to AutoDiffScalar<VectorXd> specialization. Eliminate make_coherent call in pow() overload.

--- common/test/autodiffxd_heap_test.cc
+++ common/test/autodiffxd_heap_test.cc
@@ -29,101 +29,101 @@ class AutoDiffXdHeapTest : public ::testing::Test {
 // implementations may be vulnerable to dead-code elimination.
 
 TEST_F(AutoDiffXdHeapTest, Abs) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = abs(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Abs2) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = abs2(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Acos) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = acos(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Asin) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = asin(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Atan) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = atan(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Atan2) {
   {
-    LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+    LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
     volatile auto v = atan2(x_ + y_, y_);
   }
   {
-    LimitMalloc guard({.max_num_allocations = 2, .min_num_allocations = 2});
+    LimitMalloc guard({.max_num_allocations = 2, .min_num_allocations = 0});
     // Right-hand parameter moves are blocked by code in Eigen; see #14039.
     volatile auto v = atan2(y_, x_ + y_);
   }
 }
 
 TEST_F(AutoDiffXdHeapTest, Cos) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = cos(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Cosh) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = cosh(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Exp) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = exp(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Log) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = log(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Min) {
-  LimitMalloc guard({.max_num_allocations = 4, .min_num_allocations = 4});
+  LimitMalloc guard({.max_num_allocations = 4, .min_num_allocations = 0});
   volatile auto v = min(x_ + y_, y_);
   volatile auto w = min(x_, x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Max) {
-  LimitMalloc guard({.max_num_allocations = 4, .min_num_allocations = 4});
+  LimitMalloc guard({.max_num_allocations = 4, .min_num_allocations = 0});
   volatile auto v = max(x_ + y_, y_);
   volatile auto w = max(x_, x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Pow) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = pow(x_ + y_, 2.0);
 }
 
 TEST_F(AutoDiffXdHeapTest, Sin) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = sin(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Sinh) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = sinh(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Sqrt) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = sqrt(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Tan) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = tan(x_ + y_);
 }
 
 TEST_F(AutoDiffXdHeapTest, Tanh) {
-  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 1});
+  LimitMalloc guard({.max_num_allocations = 1, .min_num_allocations = 0});
   volatile auto v = tanh(x_ + y_);
 }
 
