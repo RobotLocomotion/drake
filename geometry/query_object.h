05ff822d2a5995cabe974e953c51abd66cbd2dfe
Sean Curtis <sean.curtis@tri.global>
2020-11-16 15:28:53 -0800

    Implement Mesh for proximity via its convex hull.
    ** This is a breaking change. Anyone with a mesh registered with a
    proximity role will start observing proximity query results where there
    were none before.  **
    
    This provides an initial implementation of the Mesh shape for proximity
    queries. It does so by using a very simple and very slow representation
    of the mesh's convex hull.
    
    1. Refactor the OBJ --> fcl::Convex code to support Mesh. This basically
       *moved* the functions so they'd be available more generally.
    2. Remove the all artifacts of special mesh treatment:
       - MeshIdentifier reifier.
       - special dynamic_mesh_tree_, anchored_mesh_tree_, and X_MeshBs_
         (no longer necessary because the meshes don't have to be segregated).
    3. Reimplement ImplementGeometry(Mesh) to make use of fcl::Convex.
    4. Update unit tests accordingly.
    5. Update documentation.

--- geometry/query_object.h
+++ geometry/query_object.h
@@ -182,8 +182,8 @@ class QueryObject {
             point pairs. The ordering of the results is guaranteed to be
             consistent -- for fixed geometry poses, the results will remain
             the same.
-   @warning This silently ignores Mesh geometries (but Convex mesh geometries
-            are included).
+    @warning For Mesh shapes, their convex hulls are used in this query. It is
+            *not* computationally efficient or particularly accurate.
    @throws if T = AutoDiffXd and object actually collides. */
   std::vector<PenetrationAsPointPair<T>> ComputePointPairPenetration() const;
 
@@ -207,9 +207,9 @@ class QueryObject {
        | Box       |  yes  |  yes  |
        | Capsule   |  no   |  no   |
        | Ellipsoid |  yes  |  yes  |
-       | HalfSpace |  no   |  no   |
+       | HalfSpace |  yes  |  yes  |
        | Mesh      |  no   |  yes  |
-       | Convex    |  no   |  no   |
+       | Convex    |  no   |  yes  |
 
      - One geometry must be soft, and the other must be rigid. There is no
        support for soft-soft collision or rigid-rigid collision. If such
@@ -275,15 +275,13 @@ class QueryObject {
 
    @returns A vector populated with collision pair candidates (the order will
             remain constant for a fixed population but can change as geometry
-            ids are added/removed).
-   @warning This silently ignores Mesh geometries (but Convex mesh geometries
-            are included). */
+            ids are added/removed).  */
   std::vector<SortedPair<GeometryId>> FindCollisionCandidates() const;
 
   /** Reports true if there are _any_ collisions between unfiltered pairs in the
    world.
-   @warning This silently ignores Mesh geometries (but Convex mesh geometries
-            are included). */
+   @warning For Mesh shapes, their convex hulls are used in this query. It is
+            *not* computationally efficient or particularly accurate.  */
   bool HasCollisions() const;
 
   //@}
@@ -369,7 +367,9 @@ class QueryObject {
 
    @returns The signed distance (and supporting data) for all unfiltered
             geometry pairs whose distance is less than or equal to
-            `max_distance`.  */
+            `max_distance`.
+   @warning For Mesh shapes, their convex hulls are used in this query. It is
+            *not* computationally efficient or particularly accurate.  */
   std::vector<SignedDistancePair<T>> ComputeSignedDistancePairwiseClosestPoints(
       const double max_distance =
           std::numeric_limits<double>::infinity()) const;
@@ -380,7 +380,9 @@ class QueryObject {
    as ComputeSignedDistancePairwiseClosestPoints().
 
    @throws std::exception if either geometry id is invalid, or if the pair
-                          (A, B) has been marked as filtered.  */
+                          (A, B) has been marked as filtered.
+   @warning For Mesh shapes, their convex hulls are used in this query. It is
+            *not* computationally efficient or particularly accurate.  */
   SignedDistancePair<T> ComputeSignedDistancePairClosestPoints(
       GeometryId geometry_id_A, GeometryId geometry_id_B) const;
 
