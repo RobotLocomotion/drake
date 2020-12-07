f7020f9b76e276feeeb8051c5d036ee45180dbfb
Jamie Snape <jamie.snape@kitware.com>
2020-12-07 13:07:55 -0500

    setup: use formulae dependent on python 3.8 on macOS

--- setup/mac/binary_distribution/install_prereqs.sh
+++ setup/mac/binary_distribution/install_prereqs.sh
@@ -19,6 +19,16 @@ if ! command -v /usr/local/bin/brew &>/dev/null; then
 fi
 
 /usr/local/bin/brew update
+
+# TODO(jamiesnape): Remove lines uninstalling vtk@8.2, ospray@1.8, and
+# embree@3.5 on or after 2021-02-01.
+/usr/local/bin/brew uninstall --force $(cat <<EOF
+robotlocomotion/director/vtk@8.2
+robotlocomotion/director/ospray@1.8
+robotlocomotion/director/embree@3.5
+EOF
+)
+
 /usr/local/bin/brew bundle --file="${BASH_SOURCE%/*}/Brewfile" --no-lock
 
 if ! command -v /usr/local/opt/python@3.8/bin/pip3 &>/dev/null; then
