477a5ce4a1b6a2121f9f739029b698790e501271
DamrongGuoy <Damrong.Guoy@tri.global>
2020-12-07 19:47:12 -0800

    [geometry] Prepare for PR "Make Cylinder mesh with medial axis." Utilities for mesh generation: SplitTriangularPrismToTetrahedra() and SplitPyramidToTetrahedra().

---
+++ geometry/proximity/meshing_utilities.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <vector>
+
+#include "drake/geometry/proximity/volume_mesh.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+
+/* Subdivides a triangular prism into three tetrahedra such that they share the
+ diagonal v0,v5 (see ordering below) of the rectangular face v0,v2,v5,v3. The
+ other two face diagonals are v0,v4 and v1,v5.
+
+ We assume the input vertex indices (represented as VolumeVertexIndex) are in
+ this ordering:
+   1. Three vertices v0,v1,v2 of the "bottom" face are in counterclockwise
+      order when look from "above" the prism.
+   2. Three vertices v3,v4,v5 of the "top" face match v0,v1,v2, respectively.
+
+                       v3
+                      /|
+                     / |\
+                    /  | \
+                   /   |  \
+                  /    v0  \
+                 /    /\    \
+                v4-----------v5
+                |   /    \   |
+                |  /      \  |
+                | /        \ |
+                |/          \|
+                v1-----------v2
+ */
+std::vector<VolumeElement> SplitTriangularPrismToTetrahedra(
+    VolumeVertexIndex v0, VolumeVertexIndex v1, VolumeVertexIndex v2,
+    VolumeVertexIndex v3, VolumeVertexIndex v4, VolumeVertexIndex v5);
+
+/* Subdivide a pyramid into two tetrahedra sharing the diagonal v0,v2.
+
+                       v4
+                   ／  /\ ＼
+                v0-----------v1
+                |   /    \   |
+                |  /      \  |
+                | /        \ |
+                |/          \|
+                v3-----------v2
+ */
+std::vector<VolumeElement> SplitPyramidToTetrahedra(VolumeVertexIndex v0,
+                                                    VolumeVertexIndex v1,
+                                                    VolumeVertexIndex v2,
+                                                    VolumeVertexIndex v3,
+                                                    VolumeVertexIndex v4);
+
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
