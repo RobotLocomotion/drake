55615ef6c21f96d2bd2684ba5cfa47c6d6148cdb
Rick Poyner (rico) <rick.poyner@tri.global>
2020-12-10 20:14:27 -0500

    examples/acrobot: Add an autodiff benchmark (#14432)
    Relevant to: #8482
    
    This patch rewrites Hongkai's original program (from an old branch) to use
    google benchmark, removes some obsolete measurements (RigidBodyTree,
    AutoDiffUpTo73d), and adds some new ones (MBP vanilla CalcMassMatrix()).
    
    This benchmark set is nice because is captures the small-problem (only four
    derivatives!) end of the autodiff problem space.
    
    A possible plan would be to wrap this program with controlled-experiment
    scripts, similar to those in examples/multibody/cassie_benchmark, and use it to
    help drive further autodiff optimization work.

---
+++ examples/acrobot/test/benchmark_autodiff_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Placeholder sanity check of benchmark_autodiff program.
+# Later work may expand benchmark uses and tests; see #8482.
+
+${TEST_SRCDIR}/drake/examples/acrobot/benchmark_autodiff
