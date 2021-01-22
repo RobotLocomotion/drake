5a5f18bf75b1c7436aa9099dcfa351bbae3d748c
Xuchen Han <xuchen.han@tri.global>
2021-01-22 13:25:46 -0800

    Fix Autodiff test for LHS

--- multibody/fixed_fem/dev/test/eigen_conjugate_gradient_solver_test.cc
+++ multibody/fixed_fem/dev/test/eigen_conjugate_gradient_solver_test.cc
@@ -17,8 +17,8 @@ constexpr int kD = 4;
 /* Tolerance for the CG solver. */
 const double kTol = 1e-14;
 
-/* Generate an arbitrary symmetric positive definite (SPD) matrix. */
-Eigen::Matrix<double, kD, kD> MakeSpdMatrix() {
+/* Generate an arbitrary kD-by-kD matrix. */
+Eigen::Matrix<double, kD, kD> MakeMatrix() {
   Eigen::Matrix<double, kD, kD> B;
   // clang-format off
   B << 31.705908, 62.927869, 93.446683, 65.319711,
@@ -26,6 +26,12 @@ Eigen::Matrix<double, kD, kD> MakeSpdMatrix() {
        53.688842, 12.454311, 33.192683, 85.237341,
        14.656643, 42.836689, 13.362649, 85.326259;
   // clang-format on
+  return B;
+}
+
+/* Generate an arbitrary symmetric positive definite (SPD) matrix. */
+Eigen::Matrix<double, kD, kD> MakeSpdMatrix() {
+  Eigen::Matrix<double, kD, kD> B = MakeMatrix();
   /* A = Bᵀ * B + ε * I₄ is guaranteed to be symmetric positive definite. */
   return B.transpose() * B + 0.1 * Eigen::Matrix<double, kD, kD>::Identity();
 }
@@ -102,46 +108,63 @@ GTEST_TEST(EigenConjugateGradientSolverTest, AutoDiffSolverTestRhs) {
   }
 }
 
-/* A test that demonstrates that it may not be advisable to differentiate
- through the left hand side of a linear system in an iterative solver with
- Eigen::AutoDiffScalar. */
+/* Tests the derivatives of the solution with respect to the left hand side
+ matches the expected values. */
 GTEST_TEST(EigenConjugateGradientSolverTest, AutoDiffSolverTestLhs) {
-  using T = AutoDiffXd;
+  /* We take care to make sure the derivatives of A is compatible with the fact
+  that A remains SPD. In particular, we cannot initialize the derivatives with
+  following code block:
+  ```
   const Eigen::Matrix<double, kD, kD> A = MakeSpdMatrix();
   Eigen::Matrix<T, kD, kD> A_autodiff;
   math::initializeAutoDiff(A, A_autodiff);
+  ```
+  because then dA is not guaranteed to be symmetric. */
+  using T = AutoDiffXd;
+  const Eigen::Matrix<double, kD, kD> B = MakeMatrix();
+  Eigen::Matrix<T, kD, kD> B_autodiff;
+  math::initializeAutoDiff(B, B_autodiff);
+  /* A = Bᵀ * B + ε * I₄ is guaranteed to be symmetric positive definite. */
+  Eigen::Matrix<T, kD, kD> A_autodiff =
+      B_autodiff.transpose() * B_autodiff +
+      Eigen::Matrix<T, kD, kD>::Identity() * 0.1;
+  /* Set up solver and rhs and solve for the solution, x_cg. */
+  const Eigen::SparseMatrix<T> A_sparse = A_autodiff.sparseView();
+  contact_solvers::internal::SparseLinearOperator<T> A_op("A", &A_sparse);
+  const EigenConjugateGradientSolver<T> solver(A_op, kTol);
   const Vector<double, kD> b = MakeRhs();
-  /* The Eigen::CG solver under test. */
-  const Eigen::ConjugateGradient<Eigen::Matrix<T, kD, kD>> cg(A_autodiff);
-  /* Use LU decomposition to get expected values for A⁻¹ and x. */
+  Vector<T, kD> x_cg;
+  solver.Solve(b.cast<T>(), &x_cg);
+
+  /* Use LU decomposition to get expected values for A⁻¹, A⁻¹Bᵀ and x. */
   const Eigen::FullPivLU<Eigen::Matrix<T, kD, kD>> lu(A_autodiff);
   const Eigen::Matrix<T, kD, kD> A_inverse =
       lu.solve(Eigen::Matrix<T, kD, kD>::Identity());
-  Vector<T, kD> x_lu = lu.solve(b.cast<T>());
-  Vector<T, kD> x_cg = cg.solve(b.cast<T>());
-  EXPECT_TRUE(CompareMatrices(x_lu, x_cg, kTol));
+  const Eigen::Matrix<T, kD, kD> A_inverse_BT =
+      lu.solve(B_autodiff.transpose());
+  Vector<T, kD> x = lu.solve(b.cast<T>());
+  const Vector<T, kD> Bx = B_autodiff * x;
+  EXPECT_TRUE(CompareMatrices(x, x_cg, kTol));
 
   for (int i = 0; i < kD; ++i) {
     const Eigen::Matrix<double, kD, kD> xi_derivatives_cg =
         Eigen::Map<const Eigen::Matrix<double, kD, kD>>(
             x_cg(i).derivatives().data(), kD, kD);
-    const Eigen::Matrix<double, kD, kD> xi_derivatives_lu =
-        Eigen::Map<const Eigen::Matrix<double, kD, kD>>(
-            x_lu(i).derivatives().data(), kD, kD);
-    /* x = f(A) = A⁻¹b.
-       ∂xᵢ/∂Aⱼₖ = ∂A⁻¹ᵢₗ/∂Aⱼₖbₗ = −A⁻¹ᵢₚ * ∂A⁻¹ₚₛ/∂Aⱼₖ  * A⁻¹ₛₗbₗ
-                = -A⁻¹ᵢⱼ * A⁻¹ₖₗbₗ = -A⁻¹ᵢⱼ * xₖ. */
+    /* Build the expected derivatives of x(i) w.r.t. B:
+     x = f(B) = A⁻¹b.
+     ∂xᵢ/∂Bⱼₖ = ∂A⁻¹ᵢₗ/∂Bⱼₖbₗ = −A⁻¹ᵢₚ * ∂Aₚₛ/∂Bⱼₖ  * A⁻¹ₛₗbₗ (1).
+     Note that A = Bᵀ * B + ε * I₄ and thus  ∂Aₚₛ/∂Bⱼₖ = δₖₚBⱼₛ+δₖₛBⱼₚ.
+     Plugging it into (1) and after some algebra we get
+     ∂xᵢ/∂Bⱼₖ = -A⁻¹ᵢₖ(Bx)ⱼ - (A⁻¹B)ᵢⱼxₖ. */
     Eigen::Matrix<double, kD, kD> expected_derivatives;
     for (int j = 0; j < kD; ++j) {
       for (int k = 0; k < kD; ++k) {
-        expected_derivatives(j, k) = -(A_inverse(i, j) * x_lu(k)).value();
+        expected_derivatives(j, k) =
+            -(A_inverse(i, k) * Bx(j) + A_inverse_BT(i, j) * x(k)).value();
       }
     }
-    /* The derivatives from CG does not match expected value. */
-    EXPECT_FALSE(
-        CompareMatrices(xi_derivatives_cg, expected_derivatives, 1e-4));
-    /* The derivatives from LU matches expected value. */
-    EXPECT_TRUE(CompareMatrices(xi_derivatives_lu, expected_derivatives, kTol));
+    /* The derivatives matches expected value. */
+    EXPECT_TRUE(CompareMatrices(xi_derivatives_cg, expected_derivatives, kTol));
   }
 }
 }  // namespace
