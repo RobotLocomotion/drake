ce36a0fba54442d425c8a800638e71355f2e68e4
Rick Poyner (rico) <rick.poyner@tri.global>
2021-01-28 18:38:47 -0500

    tools/performance: Start gathering sharable infrastructure (#14505)
    * tools/performance: Split useful tooling out from cassie_bench
    
    Relevant to: #14464
    
    Gather the sharable bits of experiment execution scripting and port the
    cassie benchmark to use it.
    
    This is a first pass at factoring out the common stuff from the
    cassie_benchmark tree, for use by other benchmarks. It hoists the basic
    techniques for data collection and variance reduction. As this gathers
    more uses, it can be polished more.
    
    Also missing from this patch is any documentation or tooling to help
    with using the compare.py script from the googlebenchmark package.

--- examples/multibody/cassie_benchmark/record_results.sh
+++ examples/multibody/cassie_benchmark/record_results.sh
@@ -1,42 +1,6 @@
 #!/bin/bash
 # Collect context information for a benchmark experiment.
-# TODO(rpoyner-tri) find a robust way of recording source code version
-# information.
 
-set -e -u -o pipefail
-
-uname -a > ${TEST_UNDECLARED_OUTPUTS_DIR}/kernel.txt || true
-
-# Fill this in with a platform-specific command to control processor affinity,
-# if any.
-AFFINITY_COMMAND=""
-
-case $(uname) in
-    Linux)
-        lsb_release -idrc
-        # Choosing processor #0 is arbitrary. It is up to experimenters
-        # to ensure it is reliably idle during experiments.
-        AFFINITY_COMMAND="taskset 0x1"
-        ;;
-    Darwin)
-        sw_vers
-        ;;
-    *)
-        echo unknown
-        ;;
-esac > ${TEST_UNDECLARED_OUTPUTS_DIR}/os.txt
-
-${TEST_SRCDIR}/drake/tools/workspace/cc/identify_compiler \
- > ${TEST_UNDECLARED_OUTPUTS_DIR}/compiler.txt
-
-${AFFINITY_COMMAND} \
-${TEST_SRCDIR}/drake/examples/multibody/cassie_benchmark/cassie_bench \
-    --benchmark_display_aggregates_only=true \
-    --benchmark_repetitions=9 \
-    --benchmark_out_format=json \
-    --benchmark_out=${TEST_UNDECLARED_OUTPUTS_DIR}/results.json \
-    "$@" \
-    >& ${TEST_UNDECLARED_OUTPUTS_DIR}/summary.txt
-
-echo Full results are in:
-echo ${TEST_UNDECLARED_OUTPUTS_DIR}/
+${TEST_SRCDIR}/drake/tools/performance/record_results \
+    ${TEST_SRCDIR}/drake/examples/multibody/cassie_benchmark/cassie_bench \
+    "$@"
