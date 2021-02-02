1848d2f775cfca153d7ac20f5b8f921d804909da
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-02-02 08:04:40 -0800

    Remove deprecated code 2021-02 (#14607)
    * Remove deprecated code 2021-02

--- systems/sensors/accelerometer_sensor.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// NOLINTNEXTLINE(whitespace/line_length)
-#warning DRAKE DEPRECATED: Please use accelerometer.h instead. The deprecated code will be removed from Drake on or after 2021-02-01.
-
-#include "drake/systems/sensors/accelerometer.h"
