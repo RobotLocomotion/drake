736707dc2c372d656dfec4d788f2c662cb480406
Jamie Snape <jamie.snape@kitware.com>
2021-02-03 15:04:56 -0500

    Add retry to apt-get/brew update calls and add flag to skip update altogether
    --without-update avoids redundant potentially flaky downloads for people who have already updated recently.

--- setup/mac/binary_distribution/install_prereqs.sh
+++ setup/mac/binary_distribution/install_prereqs.sh
@@ -5,6 +5,21 @@
 
 set -euxo pipefail
 
+with_update=1
+
+while [ "${1:-}" != "" ]; do
+  case "$1" in
+    # Do NOT call brew update during execution of this script.
+    --without-update)
+      with_update=0
+      ;;
+    *)
+      echo 'Invalid command line argument' >&2
+      exit 5
+  esac
+  shift
+done
+
 if [[ "${EUID}" -eq 0 ]]; then
   echo 'ERROR: This script must NOT be run as root' >&2
   exit 1
@@ -19,6 +34,10 @@ if ! command -v brew &>/dev/null; then
     "$(/usr/bin/curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# Pass --retry 2 when invoking the curl command line tool during execution of
+# this script to retry twice the download of a bottle.
+[[ -z "${HOMEBREW_CURL_RETRIES:-}" ]] || export HOMEBREW_CURL_RETRIES=2
+
 # Do not automatically update before running brew install, brew upgrade, or brew
 # tap during execution of this script. We manually update where necessary and
 # retry if the update fails.
@@ -26,10 +45,20 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 
 # Do not automatically cleanup installed, upgraded, and/or reinstalled formulae
 # during execution of this script. Manually run brew cleanup after the script
-# completes or wait for the next automatic cleanup if necessary
+# completes or wait for the next automatic cleanup if necessary.
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 
-brew update
+binary_distribution_called_update=0
+
+if [[ "${with_update}" -eq 1 ]]; then
+  # Note that brew update uses git, so HOMEBREW_CURL_RETRIES does not take
+  # effect.
+  brew update || (sleep 30; brew update)
+
+  # Do NOT call brew update again when installing prerequisites for source
+  # distributions.
+  binary_distribution_called_update=1
+fi
 
 # TODO(jamiesnape): Remove lines tapping robotlocomotion/director and
 # uninstalling ipopt@3.11, vtk@8.2, ospray@1.8, and embree@3.5 on or after
