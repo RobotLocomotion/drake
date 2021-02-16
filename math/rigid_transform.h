0d4794817b678ae62761f41493739e19736cced6
mitiguy <paul.mitiguy@tri.global>
2021-02-16 12:49:43 -0800

    Shorten one of the comments.

--- math/rigid_transform.h
+++ math/rigid_transform.h
@@ -606,11 +606,9 @@ class RigidTransform {
 // packed into 12 consecutive doubles, with a 3x3 RotationMatrix<double> in
 // the first 9 doubles and the translation vector in the last 3 doubles.
 // A unit test verifies this memory layout for a RigidTransform<double>.
-// Note: The C++ standard guarantees that the members of a class appear in
-// memory in the same order as they are declared.  Nonstatic data members of a
-// (non-union) class are allocated so that later members have higher addresses
-// within a class object. Implementation alignment requirements might cause
-// two adjacent members not to be allocated immediately after each other.
+// Note: The C++ standard guarantees that the non-static members of a class
+// appear in memory in the same order as they are declared.  Implementation
+// alignment requirements might cause a gap in memory between adjacent members.
 static_assert(sizeof(RigidTransform<double>) == 12 * sizeof(double),
     "Low-level optimizations depend on RigidTransform<double> being "
     "stored as 12 sequential doubles in memory.");
