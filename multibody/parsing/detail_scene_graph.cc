750e6653f09ffb59fb15bfcb753e9c073c542bfe
Eric Cousineau <eric.cousineau@tri.global>
2021-02-25 17:59:56 -0500

    Upgrade sdformat to 10.3.0
    Deprecate `@tinyxml` external
    Ignore new `/sdf//heightmap` elements
    models: Fix now-invalid usages of `/sdf//pose`
    
    Co-Authored-By: Addisu Z. Taddese <addisu@openrobotics.org>

--- multibody/parsing/detail_scene_graph.cc
+++ multibody/parsing/detail_scene_graph.cc
@@ -166,6 +166,9 @@ std::unique_ptr<geometry::Shape> MakeShapeFromSdfGeometry(
         return make_unique<geometry::Mesh>(file_name, scale);
       }
     }
+    case sdf::GeometryType::HEIGHTMAP: {
+      return std::unique_ptr<geometry::Shape>(nullptr);
+    }
   }
 
   DRAKE_UNREACHABLE();
@@ -200,6 +203,7 @@ std::unique_ptr<GeometryInstance> MakeGeometryInstanceFromSdfVisual(
   // other geometry types whenever X_LC != X_LG.
   switch (sdf_geometry.Type()) {
     case sdf::GeometryType::EMPTY:  // Also includes custom geometries.
+    case sdf::GeometryType::HEIGHTMAP:
     case sdf::GeometryType::BOX:
     case sdf::GeometryType::CYLINDER:
     case sdf::GeometryType::MESH:
@@ -227,9 +231,12 @@ std::unique_ptr<GeometryInstance> MakeGeometryInstanceFromSdfVisual(
     }
   }
 
-  auto instance = make_unique<GeometryInstance>(
-      X_LC, MakeShapeFromSdfGeometry(sdf_geometry, resolve_filename),
-      sdf_visual.Name());
+  auto shape = MakeShapeFromSdfGeometry(sdf_geometry, resolve_filename);
+  if (shape == nullptr) {
+    return nullptr;
+  }
+  auto instance =
+      make_unique<GeometryInstance>(X_LC, move(shape), sdf_visual.Name());
   instance->set_illustration_properties(
       MakeVisualPropertiesFromSdfVisual(sdf_visual, resolve_filename));
   return instance;
@@ -345,6 +352,7 @@ RigidTransformd MakeGeometryPoseFromSdfCollision(
   const sdf::Geometry& sdf_geometry = *sdf_collision.Geom();
   switch (sdf_geometry.Type()) {
     case sdf::GeometryType::EMPTY:
+    case sdf::GeometryType::HEIGHTMAP:
     case sdf::GeometryType::BOX:
     case sdf::GeometryType::CYLINDER:
     case sdf::GeometryType::MESH:
