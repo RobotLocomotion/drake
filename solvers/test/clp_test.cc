a0dcb1c945e7319fb6d4469c11d0067c4284bd0e
hongkai-dai <daihongkai@gmail.com>
2021-03-02 22:21:15 -0800

    Add CLP to drake build.

---
+++ solvers/test/clp_test.cc
@@ -0,0 +1,6 @@
+#include "ClpSimplex.hpp"
+#include <gtest/gtest.h>
+
+// TODO(hongkai.dai): remove this test file when we add ClpSolver class to
+// Drake.
+GTEST_TEST(ClpTest, Test) { ClpSimplex model; }
