f98ed3d0ad7ec445ab9e26a2d474e4584da33492
Evan Drumwright <edrumwri@gmail.com>
2021-03-06 07:09:28 -0600

    Updates from review.

---
+++ multibody/tree/string_view_map_key.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <optional>
+#include <string_view>
+#include <string>
+
+namespace drake {
+namespace multibody {
+namespace internal {
+
+// This class exists because unorderded_map in C++17 does not allow using
+// different keys for storage and lookup. This class serves two purposes:
+// 1. It stores a string and provides a view into it. This is necessary
+//    for insertion into the hashtable because string_view acts only as
+//    a pointer.
+// 2. It stores a string_view only. This permits querying the hashtable
+//    with a string_view without copying the underlying string. Note that
+//    the underlying memory pointed to by the string_view must outlive
+//    `this` and all copies in this mode.
+class StringViewMapKey {
+ public:
+  StringViewMapKey() = default;
+
+  // Default move constructor may not respect short-string optimization.
+  // See note in move assignment operator below.
+  StringViewMapKey(StringViewMapKey&& s) { operator=(s); }
+  StringViewMapKey(const StringViewMapKey& s) { operator=(s); }
+
+  StringViewMapKey& operator=(const StringViewMapKey& s) {
+    storage_ = s.storage_;
+    if (storage_.has_value()) {
+      // s is used for Mode 1.
+      view_ = storage_.value();
+    } else {
+      // s is used for Mode 2.
+      view_ = s.view();
+    }
+    return *this;
+  }
+
+  // Note: default move assignment may fail when short-string-optimization
+  // (SSO) is used by the underlying std::string because the moved view would
+  // still point to the array in the source.
+  StringViewMapKey& operator=(StringViewMapKey&& s) {
+    storage_ = std::move(s.storage_);
+    view_ = (storage_.has_value()) ? storage_.value() : s.view();
+    return *this;
+  }
+
+  // Mode 1 constructor with implicit conversion.
+  // NOLINTNEXTLINE
+  StringViewMapKey(std::string&& s)
+      : storage_(std::move(s)),
+        view_(storage_.value()) {}
+
+  // Mode 1 constructor with implicit conversion.
+  // NOLINTNEXTLINE
+  StringViewMapKey(const std::string& s) : storage_(s), view_(storage_.value()) {}
+
+  // Mode 2 constructor with implicit conversion.
+  // NOLINTNEXTLINE
+  StringViewMapKey(std::string_view view) : view_(view) {}
+
+  std::string_view view() const { return view_; }
+
+  // Underlying optional storage, exposed for testing.
+  const std::optional<std::string>& storage() const { return storage_; }
+
+ private:
+  std::optional<std::string> storage_;
+  std::string_view view_;
+
+  friend bool operator==(const StringViewMapKey& a, const StringViewMapKey& b) {
+    return a.view_ == b.view_;
+  }
+};
+
+}  // namespace internal
+}  // namespace multibody
+}  // namespace drake
+
+namespace std {
+template<> struct hash<drake::multibody::internal::StringViewMapKey> {
+std::size_t operator()(
+  const drake::multibody::internal::StringViewMapKey& s) const noexcept {
+      return std::hash<std::string_view>{}(s.view());
+}
+};
+}  // namespace std
---
+++ multibody/tree/test/string_view_map_key_test.cc
@@ -0,0 +1,86 @@
+#include "drake/multibody/tree/string_view_map_key.h"
+
+#include <gtest/gtest.h>
+
+namespace drake {
+namespace multibody {
+namespace internal {
+namespace {
+
+// Short string for validation of SSO (short-string optimization) strings.
+const std::string kShortString = "abcde";
+
+// Long string for validation of non-SSO strings.
+const std::string kLongString = "abcdefghijklmnopqrstuvwxyz";
+
+// For "reference" keys.
+const std::string_view kReferenceView = "reference";
+
+class StringViewMapKeyTest : public ::testing::Test {
+ public:
+  void ValidateStorageKey(const StringViewMapKey& key) {
+    // Validate that the string_view is associated with the storage when the
+    // optional value is not set.
+    ASSERT_EQ(key.storage().has_value(), true);
+    EXPECT_EQ(key.storage().value().c_str(), key.view().data());
+  }
+
+  void ValidateReferenceKey(const StringViewMapKey& key) {
+    ASSERT_EQ(key.storage().has_value(), false);
+    EXPECT_EQ(key.view().data(), kReferenceView.data());
+  }
+
+ protected:
+  void SetUp() {}
+
+  StringViewMapKey kShortKey{kShortString};
+  StringViewMapKey kLongKey{kLongString};
+  StringViewMapKey kReferenceKey{kReferenceView};
+};
+
+// Validate constructed objects.
+TEST_F(StringViewMapKeyTest, Construction) {
+  ValidateStorageKey(kShortKey);
+  ValidateStorageKey(kLongKey);
+  ValidateReferenceKey(kReferenceKey);
+}
+
+// Validate copy constructed objects.
+TEST_F(StringViewMapKeyTest, CopyConstruction) {
+  ValidateStorageKey(StringViewMapKey(kShortKey));
+  ValidateStorageKey(StringViewMapKey(kLongKey));
+  ValidateReferenceKey(StringViewMapKey(kReferenceKey));
+}
+
+// Validate move constructed objects.
+TEST_F(StringViewMapKeyTest, MoveConstruction) {
+  ValidateStorageKey(StringViewMapKey(std::move(kShortKey)));
+  ValidateStorageKey(StringViewMapKey(std::move(kLongKey)));
+  ValidateReferenceKey(StringViewMapKey(std::move(kReferenceKey)));
+}
+
+// Validate copy assigned objects.
+TEST_F(StringViewMapKeyTest, CopyAssignment) {
+  StringViewMapKey short_copy;
+  StringViewMapKey long_copy;
+  StringViewMapKey reference_copy;
+  ValidateStorageKey(short_copy = kShortKey);
+  ValidateStorageKey(long_copy = kLongKey);
+  ValidateReferenceKey(reference_copy = kReferenceKey);
+}
+
+// Validate move assigned objects.
+TEST_F(StringViewMapKeyTest, MoveAssignment) {
+  StringViewMapKey short_move;
+  StringViewMapKey long_move;
+  StringViewMapKey reference_move;
+  ValidateStorageKey(short_move = std::move(kShortKey));
+  ValidateStorageKey(long_move = std::move(kLongKey));
+  ValidateReferenceKey(reference_move = std::move(kReferenceKey));
+}
+
+
+}  // namespace
+}  // namespace internal
+}  // namespace multibody
+}  // namespace drake
