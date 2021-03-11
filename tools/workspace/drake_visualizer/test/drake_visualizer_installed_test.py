4f0f0dca1588fa63213d3a30188790e4bc429b6b
Jamie Snape <jamie.snape@kitware.com>
2021-03-11 11:42:37 -0500

    Update and re-enable drake-visualizer for macOS

--- tools/workspace/drake_visualizer/test/drake_visualizer_installed_test.py
+++ tools/workspace/drake_visualizer/test/drake_visualizer_installed_test.py
@@ -6,14 +6,6 @@ import install_test_helper
 
 
 class TestDrakeVisualizerInstalled(unittest.TestCase):
-    # Drake Visualizer is disabled for at least as long as it takes to
-    # transition Drake and VTK 8.2 to Python 3.9 or some alternative solution
-    # is found, see https://github.com/RobotLocomotion/drake/issues/14650 for
-    # updates.
-    @unittest.skipIf(
-        sys.platform == "darwin",
-        "Drake Visualizer is disabled on macOS, see https://git.io/Jt1BG "
-        "for updates")
     def test_help(self):
         """Ensures we can call `./bin/drake-visualizer --help` from install."""
         # Get install directory.
