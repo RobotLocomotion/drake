85368b42ca21e5688e1fb761d5ce1b06fc81e55b
Eric Cousineau <eric.cousineau@tri.global>
2021-03-19 10:48:28 -0400

    simplify flags a bit more; fixup python running code

--- tools/autopybind/generate.py
+++ tools/autopybind/generate.py
@@ -39,20 +39,9 @@ def extract_archive_tempdir(archive, *, dir=None, prefix=None, debug=False):
     """
     if dir is None:
         dir = os.environ.get("TEST_TMPDIR")
-    with TemporaryDirectory(dir=dir, prefix=prefix) as tmp_dir:
-        with tarfile.open(archive, "r") as tar:
-            tar.extractall(path=tmp_dir)
-            yield str(tmp_dir)
-
-
-def run_autopybind11(output_dir):
     with debuggable_temporary_directory(dir=dir, prefix=prefix, debug=debug) as tmp_dir:  # noqa
         with tarfile.open(archive, "r") as tar:
             tar.extractall(path=tmp_dir)
-            if debug:
-                eprint(f"Extracted:")
-                eprint(f"  archive: {archive}")
-                eprint(f"  tmp_dir: {tmp_dir}")
             yield str(tmp_dir)
 
 
@@ -66,46 +55,16 @@ def eprint(s):
     print(s, file=sys.stderr)
 
 
-def run_autopybind11(output_dir, debug):
-    castxml_bin = find_data("external/castxml/castxml_bin")
-    bindings_to_generate = find_data(
-        "tools/autopybind/bindings_to_generate.yaml")
-    customization_file = find_data(
-        "tools/autopybind/autopybind11_customization.yaml")
-    response_file = find_data("tools/autopybind/bindings_to_generate.rsp")
-    headers_tar = find_data(
-        "tools/autopybind/bindings_to_generate_headers.tar")
-
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir)
-
-    argv = [
-        sys.executable,
-        "-m",
-        "autopybind11",
-        "--stage=2",
-        "--module_name=pydrake",
-        "--castxml",
-        castxml_bin,
-        "-o",
-        output_dir,
-        "-y",
-        bindings_to_generate,
-        "-rs",
-        response_file,
-        "-cg",
-        customization_file,
-    ]
-
+def run_autopybind11_in_bazel(argv, headers_tar, debug):
     # Since we're connecting a genrule with a proper binary, we must "simulate"
     # the genfiles directory.
     print("Running autopybind11...")
     cmd = shlex_join(argv)
     if debug:
         eprint(f"+ {cmd}")
-    # Since we're connecting a genrule with a proper binary, we must "simulate"
-    # the genfiles directory.
+
     dt_autopybind11 = float("nan")
+
     with extract_archive_tempdir(headers_tar, debug=debug) as headers_dir:
         t_start = time.time()
         result = run(
@@ -127,6 +86,39 @@ def run_autopybind11(output_dir, debug):
     if result.returncode != 0:
         print(f"ERROR: Failed for: {cmd}", file=sys.stderr)
         sys.exit(1)
+
+
+def run_autopybind11_for_pydrake(output_dir, input_yaml, debug):
+    castxml_bin = find_data("external/castxml/castxml_bin")
+    if input_yaml is None:
+        input_yaml = find_data(
+            "tools/autopybind/bindings_to_generate.yaml")
+    customization_file = find_data(
+        "tools/autopybind/autopybind11_customization.yaml")
+    compile_info_file = find_data("tools/autopybind/libdrake_compile_info.rsp")
+    headers_tar = find_data("tools/autopybind/libdrake_headers.tar")
+
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
+    argv = [
+        sys.executable,
+        "-m",
+        "autopybind11",
+        "--stage=2",
+        "--module_name=pydrake",
+        "--castxml",
+        castxml_bin,
+        "-o",
+        output_dir,
+        "-y",
+        input_yaml,
+        "-rs",
+        compile_info_file,
+        "-cg",
+        customization_file,
+    ]
+    run_autopybind11_in_bazel(argv, headers_tar, debug)
     print(f"Wrote files to: {output_dir}")
 
 
@@ -137,10 +129,15 @@ def main():
         help="Outputs all generated artifcats this directory. Creates the "
              "directory if it does not exist.")
     parser.add_argument(
+        "--input_yaml", type=str, default=None,
+        help="Input YAML describing symbols to generate bindings for. "
+             "If not specified, will use version-controlled configuration "
+             "file.")
+    parser.add_argument(
         "--debug", action="store_true",
         help="Include verbose output and keep intermediate articats")
     args = parser.parse_args()
-    run_autopybind11(args.output_dir, args.debug)
+    run_autopybind11_for_pydrake(args.output_dir, args.input_yaml, args.debug)
 
 
 if __name__ == "__main__":
--- tools/autopybind/test/generate_test.py
+++ tools/autopybind/test/generate_test.py
@@ -1,6 +1,21 @@
 import os
 from subprocess import run
+import sys
 import unittest
+
+
+def get_isolated_env():
+    home = "/do_not_read_or_write"
+    if sys.platform == "darwin":
+        path = "/usr/local/bin:/usr/bin:/bin"
+    else:
+        path = "/usr/bin:/bin"
+    env = {
+        "HOME": home,
+        "LANG": "en_US.UTF-8",
+        "PATH": path,
+    }
+    return env
 
 
 class TestGenerate(unittest.TestCase):
@@ -8,8 +23,11 @@ class TestGenerate(unittest.TestCase):
         generate_binary = "tools/autopybind/generate"
         output_dir = os.path.join(os.environ["TEST_TMPDIR"], "output")
 
-        print(os.getcwd())
-        run([generate_binary, "--output_dir", output_dir], check=True)
+        run(
+            [generate_binary, "--output_dir", output_dir],
+            env=get_isolated_env(),
+            check=True,
+        )
         self.assertTrue(os.path.isdir(output_dir), output_dir)
 
         # TODO(eric.cousineau): Add some quick text checks on the generated
