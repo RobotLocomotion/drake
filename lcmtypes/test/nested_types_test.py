a03389ef5bc6503fcbc85fc557edc1c4141309da
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-04-05 06:32:14 -0700

    lcmtypes: Remove deprecated messages 2021-04 (#14842)

--- lcmtypes/test/nested_types_test.py
+++ lcmtypes/test/nested_types_test.py
@@ -2,9 +2,9 @@
 # code-gen'd correctly, and can be imported at will.  If something isn't
 # working, it will raise an exception during instantiation or construction.
 
-import drake.lcmt_polynomial
-import drake.lcmt_polynomial_matrix
+from drake import lcmt_robot_plan, lcmt_robot_state
 
-mat = drake.lcmt_polynomial_matrix()
-mat.polynomials.append(drake.lcmt_polynomial())
-mat.encode()
+outer = lcmt_robot_plan()
+outer.num_states = 1
+outer.plan.append(lcmt_robot_state())
+outer.encode()
