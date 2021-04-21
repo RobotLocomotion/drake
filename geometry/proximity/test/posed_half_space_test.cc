6d8fc082cd122622a0c01195a417ba7d73b4179a
Sean Curtis <sean.curtis@tri.global>
2021-04-21 08:19:17 -0700

    Allow Plane and PoseHalfSpace to do mixed-scalar computation
    Plane::CalcHeight() and PosedHalfSpace::CalcSignedDistance() can now
    support mixed numerical scalars (double and AutoDiffXd).

--- geometry/proximity/test/posed_half_space_test.cc
+++ geometry/proximity/test/posed_half_space_test.cc
@@ -9,6 +9,7 @@
 #include "drake/common/eigen_types.h"
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
 #include "drake/common/test_utilities/expect_throws_message.h"
+#include "drake/math/autodiff.h"
 
 namespace drake {
 namespace geometry {
@@ -55,8 +56,63 @@ GTEST_TEST(PosedHalfSpaceTest, CalcSignedDistance) {
 
   const PosedHalfSpace<double> hs_F(nhat_F, p_FP);
 
-  EXPECT_NEAR(hs_F.CalcSignedDistance(p_FP + 0.5 * nhat_F), 0.5, kEps);
-  EXPECT_NEAR(hs_F.CalcSignedDistance(p_FP - 0.5 * nhat_F), -0.5, kEps);
+  EXPECT_NEAR(hs_F.CalcSignedDistance(Vector3d{p_FP + 0.5 * nhat_F}), 0.5,
+              kEps);
+  EXPECT_NEAR(hs_F.CalcSignedDistance(Vector3d{p_FP - 0.5 * nhat_F}), -0.5,
+              kEps);
+}
+
+// Confirm that mixed-scalar computation "works". In the case of this test,
+// "works" means that the query point scalar type and half space scalar types
+// are independent, the return type is the promoted type, and derivatives
+// propagate through.
+GTEST_TEST(PosedHalfSpaceTest, MixedScalar) {
+  // Arbitrary Normal and origins.
+  const Vector3d nhat_F_d = Vector3d{-1, 0.25, 1.3}.normalized();
+  const Vector3d p_FB_d{0.25, 0.5, -1};
+  PosedHalfSpace<double> hs_F_d(nhat_F_d, p_FB_d);
+
+  const Vector3<AutoDiffXd> p_FB_ad = math::initializeAutoDiff(p_FB_d);
+  PosedHalfSpace<AutoDiffXd> hs_F_ad(nhat_F_d.cast<AutoDiffXd>(), p_FB_ad);
+
+  const double expected_distance = 0.125;
+  const Vector3d p_FQ_d = p_FB_d + expected_distance * nhat_F_d;
+  const Vector3<AutoDiffXd> p_FQ_ad_derivs = math::initializeAutoDiff(p_FQ_d);
+  const Vector3<AutoDiffXd> p_FQ_ad_no_derivs = p_FQ_d.cast<AutoDiffXd>();
+
+  {
+    // Double-valued plane and double-valued point -> double valued result.
+    const double distance = hs_F_d.CalcSignedDistance(p_FQ_d);
+    EXPECT_EQ(distance, expected_distance);
+  }
+
+  {
+    // Double-valued plane and AutoDiff-valued point -> AutoDiff valued result
+    // with the same number of derivatives as the point.
+    const AutoDiffXd distance1 = hs_F_d.CalcSignedDistance(p_FQ_ad_derivs);
+    EXPECT_EQ(distance1.value(), expected_distance);
+    EXPECT_EQ(distance1.derivatives().size(), 3);
+
+    const AutoDiffXd distance2 = hs_F_d.CalcSignedDistance(p_FQ_ad_no_derivs);
+    EXPECT_EQ(distance2.value(), expected_distance);
+    EXPECT_EQ(distance2.derivatives().size(), 0);
+  }
+
+  {
+    // AutoDiff-valued plane and double-valued point -> AutoDiff valued result
+    // with the same number of derivatives as the plane.
+    const AutoDiffXd distance = hs_F_ad.CalcSignedDistance(p_FQ_d);
+    EXPECT_EQ(distance.value(), expected_distance);
+    EXPECT_EQ(distance.derivatives().size(), 3);
+  }
+
+  {
+    // AutoDiff-valued plane and AutoDiff-valued point -> AutoDiff valued result
+    // with the same number of derivatives as the plane (and point).
+    const AutoDiffXd distance = hs_F_ad.CalcSignedDistance(p_FQ_ad_derivs);
+    EXPECT_EQ(distance.value(), expected_distance);
+    EXPECT_EQ(distance.derivatives().size(), 3);
+  }
 }
 
 }  // namespace
