4c4c9dbb9129401af955833bcd37d4cb247fac9c
DamrongGuoy <damrong.guoy@tri.global>
2021-04-29 13:36:54 -0700

    [geometry] Utility to add one representative triangle for each contact polygon. It will help speed up discrete hydroelastic contact model.

--- geometry/proximity/contact_surface_utility.h
+++ geometry/proximity/contact_surface_utility.h
@@ -116,6 +116,58 @@ void AddPolygonToMeshData(
     std::vector<SurfaceFace>* faces,
     std::vector<SurfaceVertex<T>>* vertices_F);
 
+// Any polygon with area less than this threshold is considered having
+// near-zero area in AddPolygonToMeshDataAsOneTriangle() below.
+constexpr double kMinimumPolygonArea = 1e-13;
+
+// TODO(14579) The following AddPolygonToMeshDataAsOneTriangle() is expected to
+//  simply go away when we implement the final support for discrete
+//  hydroelastics. If it persists (for whatever reason), find a better way to
+//  deal with a polygon with zero or near-zero area. Perhaps we should
+//  allow users to specify criteria to classify such polygons instead of
+//  relying on the threshold kMinimumPolygonArea above.
+
+/* Adds a representative triangle of a convex polygon to the given set of
+ `faces` and `vertices`. The triangle has the same centroid, area, and normal
+ vector as the polygon. The three vertices of the representative triangle are
+ introduced into `vertices`.
+
+ The exact choice of the representative triangle is arbitrary subject to the
+ constraints in the previous paragraph.
+
+ In debug builds, this function will do _expensive_ validation of its
+ parameters.
+
+ @param[in] polygon_F
+     The input polygon is represented by position vectors of its vertices,
+     measured and expressed in frame F. This polygon is _not_ in `faces` or
+     `vertices`.
+ @param[in] nhat_F
+     The unit normal vector to the polygon, expressed in frame F.
+ @param[out] faces
+     The new triangle is added into `faces` with the same orientation (same
+     normal vector) as the input polygon.
+ @param[out] vertices_F
+     The set of vertex positions to be extended; each vertex is measured and
+     expressed in frame F. Three vertices will be added.
+
+ @pre `faces` and `vertices_F` are not `nullptr`.
+ @pre The polygon is simple (does not intersect itself and has no holes).
+ @pre The winding of `polygon_F` is consistent with the direction of `nhat_F`.
+      They must respect the right-hand rule.
+
+ @note There are two reasons for skipping a polygon with zero or near-zero area.
+       1. Such a polygon contributes negligibly to contact force and moment.
+       2. For the scalar type AutoDiffXd, such a polygon may cause unstable
+          calculation of derivatives of the representative triangle.
+
+ @tparam T  The computational scalar type. Only supports double and AutoDiffXd.
+ */
+template <typename T>
+void AddPolygonToMeshDataAsOneTriangle(
+    const std::vector<Vector3<T>>& polygon_F, const Vector3<T>& nhat_F,
+    std::vector<SurfaceFace>* faces, std::vector<SurfaceVertex<T>>* vertices_F);
+
 /* Determines if the indicated triangle has a face normal that is "in the
  direction" of the given normal.
 
