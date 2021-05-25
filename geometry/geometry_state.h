437867c7ca3a8f690e4205368921f794c266669a
Sean Curtis <sean.curtis@tri.global>
2021-05-25 11:21:02 -0700

    Adding collision filter manager API
    In anticipation of expanding the functionality relating to working
    with collision filters, we're changing the public API. This introduces
    a *parallel* API and labels the old API as "legacy". The legacy API
    will be deprecated in a follow up PR.
    
    The purpose of the API is to reduce SceneGraph's knowledge of working
    with collision filters. SceneGraph produces a CollisionFilterManager,
    but all manipulation of collision filter configuration now belongs to
    that manager (reducing the churn on SceneGraph's already large API).
    
    To achieve this end:
    
      - Introduce new classes: CollisionFilterManager and
        CollisionFilterDeclaration.
        - The unit tests for CollisionFilterManager are copied from
          scene_graph_test.cc (as the manager can only be constructed by
          SceneGraph). The source test code is marked as Legacy for future
          deprecation.
      - Add new API: SceneGraph::collision_filter_manager().
      - GeometryState updates API to support new manager/declaration API.
    
    This does *not* include bindings. Those will be included when we
    deprecate the legacy API.

--- geometry/geometry_state.h
+++ geometry/geometry_state.h
@@ -11,6 +11,7 @@
 
 #include "drake/common/autodiff.h"
 #include "drake/common/drake_copyable.h"
+#include "drake/geometry/collision_filter_declaration.h"
 #include "drake/geometry/frame_kinematics_vector.h"
 #include "drake/geometry/geometry_ids.h"
 #include "drake/geometry/geometry_index.h"
@@ -418,6 +419,9 @@ class GeometryState {
   }
 
   //@}
+
+  /** Implementation of CollisionFilterManager::ApplyFilterDeclaration(). */
+  void ApplyFilterDeclaration(const CollisionFilterDeclaration& declaration);
 
   /** @name               Proximity filters
 
