540b983899b27fe7fd0b6600fac9da1a9040124a
Sean Curtis <sean.curtis@tri.global>
2021-05-25 09:28:25 -0700

    Make a deformable volume mesh
     - Introduce the generic MeshDeformer abstract.
      - It is compatible with both SurfaceMesh and VolumeMesh. Each have
        been modified to support the deformer.
     - Add the DeformableVolumeMesh
      - Wraps a VolumeMesh with a corresponding MeshDeformer and provides
        an API for updating vertex positions.
    
    In the near future, a "deformable" bvh will be added as well.

---
+++ geometry/proximity/deformable_volume_mesh.cc
@@ -0,0 +1,18 @@
+#include "drake/geometry/proximity/deformable_volume_mesh.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+
+template <typename T>
+void DeformableVolumeMesh<T>::UpdateVertexPositions(
+    const Eigen::Ref<const VectorX<T>>& q) {
+  deformer_.SetAllPositions(q);
+}
+
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
+    class ::drake::geometry::internal::DeformableVolumeMesh)
