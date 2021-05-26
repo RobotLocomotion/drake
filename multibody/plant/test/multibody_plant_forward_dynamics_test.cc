f9555e4347c1dc41519754a67158cea28b8b7fcc
Sean Curtis <sean.curtis@tri.global>
2021-05-26 06:23:05 -0700

    [hotfix] Fix a mac ci build - test tolerance was too tight (#15094)
    * Fix a mac ci build - test tolerance was too tight
    
    The mac (in debug) needed a bit more tolerance in the test.

--- multibody/plant/test/multibody_plant_forward_dynamics_test.cc
+++ multibody/plant/test/multibody_plant_forward_dynamics_test.cc
@@ -181,10 +181,10 @@ GTEST_TEST(MultibodyPlantForwardDynamics, AtlasRobot) {
   // Verify that the implicit dynamics match the continuous ones.
   Eigen::VectorXd residual = plant.AllocateImplicitTimeDerivativesResidual();
   plant.CalcImplicitTimeDerivativesResidual(*context, *derivatives, &residual);
-  // Note the slightly looser tolerance of 1e-13 which was required for this
+  // Note the slightly looser tolerance of 2e-13 which was required for this
   // test.
   EXPECT_TRUE(CompareMatrices(
-      residual, Eigen::VectorXd::Zero(plant.num_multibody_states()), 1e-13));
+      residual, Eigen::VectorXd::Zero(plant.num_multibody_states()), 2e-13));
 }
 
 // TODO(amcastro-tri): Include test with non-zero actuation and external forces.
