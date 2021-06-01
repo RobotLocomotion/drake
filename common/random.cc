71fdba659b4a1f844477a3210e37768820e97103
hongkai-dai <daihongkai@gmail.com>
2021-06-01 11:38:11 -0700

    add LinearTransformDensity.

--- common/random.cc
+++ common/random.cc
@@ -1,4 +1,37 @@
 #include "drake/common/random.h"
 
-// For now, this is an empty .cc file that only serves to confirm
-// random.h is a stand-alone header.
+namespace drake {
+template <typename T>
+T CalcProbabilityDensity(RandomDistribution distribution,
+                         const Eigen::Ref<const VectorX<T>>& x) {
+  switch (distribution) {
+    case RandomDistribution::kUniform: {
+      for (int i = 0; i < x.rows(); ++i) {
+        if (x(i) < 0.0 || x(i) > 1.0) {
+          return T(0.);
+        }
+      }
+      return T(1.);
+    }
+    case RandomDistribution::kGaussian: {
+      return ((-0.5 * x.array() * x.array()).exp() / std::sqrt(2 * M_PI))
+          .prod();
+    }
+    case RandomDistribution::kExponential: {
+      for (int i = 0; i < x.rows(); ++i) {
+        if (x(i) < 0.0) {
+          return T(0.);
+        }
+      }
+      return (-x.array()).exp().prod();
+    }
+  }
+  DRAKE_UNREACHABLE();
+}
+
+template double CalcProbabilityDensity<double>(
+    RandomDistribution, const Eigen::Ref<const VectorX<double>>&);
+template AutoDiffXd CalcProbabilityDensity<AutoDiffXd>(
+    RandomDistribution, const Eigen::Ref<const VectorX<AutoDiffXd>>&);
+}  // namespace drake
+
