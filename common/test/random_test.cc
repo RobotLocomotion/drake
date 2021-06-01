e6726e2ef317cca8497473556c0979863a7af524
hongkai-dai <daihongkai@gmail.com>
2021-06-01 11:38:11 -0700

    add LinearTransformDensity.

--- common/test/random_test.cc
+++ common/test/random_test.cc
@@ -1,9 +1,13 @@
 #include "drake/common/random.h"
 
+#include <limits>
+
 #include <gtest/gtest.h>
 
 namespace drake {
 namespace {
+
+const double kEps = std::numeric_limits<double>::epsilon();
 
 GTEST_TEST(RandomTest, CompareWith19337) {
   std::mt19937 oracle;
@@ -27,6 +31,83 @@ GTEST_TEST(RandomTest, Seed) {
   for (int i = 0; i < 100; ++i) {
     EXPECT_EQ(dut1(), dut2());
   }
+}
+
+template <typename T>
+void CheckCalcProbabilityDensityUniform() {
+  // Sample with non-zero probability.
+  for (const auto& x :
+       {Vector2<T>(0.1, 0.5), Vector2<T>(0., 0.5), Vector2<T>(0.1, 1.)}) {
+    T density = CalcProbabilityDensity<T>(RandomDistribution::kUniform, x);
+    EXPECT_EQ(ExtractDoubleOrThrow(density), 1.);
+  }
+
+  // Sample with zero probability.
+  for (const auto& x_zero_prob :
+       {Vector2<T>(-0.1, 0.5), Vector2<T>(0.2, 1.5), Vector2<T>(-0.1, 1.2)}) {
+    T density =
+        CalcProbabilityDensity<T>(RandomDistribution::kUniform, x_zero_prob);
+    EXPECT_EQ(ExtractDoubleOrThrow(density), 0.);
+  }
+}
+
+GTEST_TEST(RandomTest, CalcProbabilityDensityUniform) {
+  CheckCalcProbabilityDensityUniform<double>();
+  CheckCalcProbabilityDensityUniform<AutoDiffXd>();
+}
+
+template <typename T>
+void CheckCalcProbabilityDensityGaussian() {
+  for (const auto& x :
+       {Vector3<T>(0.5, 1.2, 3.5), Vector3<T>(-0.2, -1., 2.1)}) {
+    // Compute the pdf of each variable separately, and then multiply their
+    // pdf together since the variables are independent.
+    T density_expected = T(1);
+    for (int i = 0; i < 3; ++i) {
+      using std::exp;
+      using std::sqrt;
+      density_expected *= exp(-0.5 * x(i) * x(i)) / sqrt(2 * M_PI);
+    }
+    const T density =
+        CalcProbabilityDensity<T>(RandomDistribution::kGaussian, x);
+    EXPECT_NEAR(ExtractDoubleOrThrow(density),
+                ExtractDoubleOrThrow(density_expected), 10 * kEps);
+  }
+}
+
+GTEST_TEST(RandomTest, CalcProbabilityDensityGaussian) {
+  CheckCalcProbabilityDensityGaussian<double>();
+  CheckCalcProbabilityDensityGaussian<AutoDiffXd>();
+}
+
+template <typename T>
+void CheckCalcProbabilityDensityExponential() {
+  // Check samples with non-zero probability.
+  for (const auto& x : {Vector3<T>(0.1, 0.5, 0.), Vector3<T>(0.1, 0.9, 1.5)}) {
+    // Compute the pdf of each variable separately, and then multiply them
+    // together.
+    T density_expected(1.);
+    for (int i = 0; i < 3; ++i) {
+      using std::exp;
+      density_expected *= exp(-x(i));
+    }
+    EXPECT_NEAR(ExtractDoubleOrThrow(CalcProbabilityDensity<T>(
+                    RandomDistribution::kExponential, x)),
+                ExtractDoubleOrThrow(density_expected), 10 * kEps);
+  }
+
+  // Check samples with zero probability.
+  for (const Vector3<T>& x_zero_prob :
+       {Vector3<T>(-0.5, 1., 2.), Vector3<T>(-0.01, 0.01, 1)}) {
+    EXPECT_EQ(ExtractDoubleOrThrow(CalcProbabilityDensity<T>(
+                  RandomDistribution::kExponential, x_zero_prob)),
+              0.);
+  }
+}
+
+GTEST_TEST(RandomTest, CalcProbabilityDensityExponential) {
+  CheckCalcProbabilityDensityExponential<double>();
+  CheckCalcProbabilityDensityExponential<AutoDiffXd>();
 }
 
 }  // namespace
---
+++ systems/primitives/linear_transform_density.h
@@ -0,0 +1,133 @@
+#pragma once
+
+#include "drake/common/drake_copyable.h"
+#include "drake/common/random.h"
+#include "drake/systems/framework/leaf_system.h"
+
+namespace drake {
+namespace systems {
+/**
+ * Performs linear transformation on the random signal w_in as
+ * w_out = A*w_in + b.
+ * The user can obtain the probability density of w_out. When the class
+ * is instantiated with autodiff scalar, the user can also obtain the gradient
+ * of the probability density of w_out.
+ *
+ * @system
+ * name: LinearTransformDensity
+ * input_ports:
+ * - w_in (random signal)
+ * - A (matrix)
+ * - b (vector, optional)
+ * output_ports:
+ * - w_out
+ * @endsystem
+ *
+ * The `b` port can remain disconnected, in which case it defaults to zero.
+ *
+ * `A` should be a matrix using a column-major order.
+ *
+ * The user should make sure that the input port `w_in` is connected from the
+ * output port of a RandomSource with the same distribution. A recommended way
+ * is to use `AddRandomInputs()`.
+ *
+ * @warning The code cannot verify that the distribution type of w_in matches
+ * between where w_in comes from and the w_in input port of this sytem. This
+ * class will quitely produce incorrect behavior if the distribution types don't
+ * match.
+ *
+ * @see @ref stochastic_systems
+ *
+ * @ingroup primitive_systems
+ *
+ * @tparam_nonsymbolic_scalar
+ */
+template <typename T>
+class LinearTransformDensity final : public LeafSystem<T> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(LinearTransformDensity)
+
+  /**
+   * @param distribution The random input w_in should satisfy this distribution.
+   * @param input_size The dimension of the input w_in.
+   * @param output_size The dimension of the output w_out.
+   * @note The matrix A will have `output_size` columns and `input_size` rows.
+   * The vector b will have `output_size` columns.
+   */
+  LinearTransformDensity(RandomDistribution distribution, int input_size,
+                         int output_size);
+
+  /** Scalar-converting copy constructor. See @ref system_scalar_conversion. */
+  template <typename U>
+  explicit LinearTransformDensity(const LinearTransformDensity<U>&);
+
+  /** Gets the input port for w_in. */
+  const InputPort<T>& get_input_port_w_in() const {
+    return this->get_input_port(w_in_port_id_);
+  }
+
+  /** Gets the input port for A. */
+  const InputPort<T>& get_input_port_A() const {
+    return this->get_input_port(A_port_id_);
+  }
+
+  /** Gets the input port for b. */
+  const InputPort<T>& get_input_port_b() const {
+    return this->get_input_port(b_port_id_);
+  }
+
+  RandomDistribution get_distribution() const { return distribution_; }
+
+  /**
+   * Fix the input port `A` to a constant value in a given context.
+   * @param context The context into which A's value is set.
+   * @param A The value to which the port is fixed. The matrix A has num_output
+   * rows and num_input columns, note that A is column-majored.
+   */
+  FixedInputPortValue& FixConstantA(
+      Context<T>* context, const Eigen::Ref<const MatrixX<T>>& A) const;
+
+  /**
+   * Fix the input port `b` to a constant value in a given context.
+   * @param context The context into which b's value is set.
+   * @param b The value to which the port is fixed. The vector b has num_output
+   * rows.
+   */
+  FixedInputPortValue& FixConstantB(
+      Context<T>* context, const Eigen::Ref<const VectorX<T>>& b) const;
+
+  /**
+   * Compute the density (pdf) of a sampled output w_out.
+   *
+   * When T=AutoDiffXd, this function computes the gradient of the function
+   * density(w_out_sample). Namely given an output sample, we want to know
+   * how the probability of drawing this sample would change, when the
+   * parameters of the distribution (like A and b) change. Such information is
+   * locally expressed in the gradient. Note this is different from computing
+   * the density of the input.
+   *
+   * @throw std::exception if A is not an invertible matrix.
+   */
+  T CalcDensity(const Context<T>& context) const;
+
+ private:
+  void CalcOutput(const Context<T>& context, BasicVector<T>* w_out) const;
+
+  Eigen::Map<const MatrixX<T>> GetA(const Context<T>& context) const;
+
+  const RandomDistribution distribution_;
+  const int input_size_{};
+  const int output_size_{};
+  InputPortIndex w_in_port_id_;
+  InputPortIndex A_port_id_;
+  InputPortIndex b_port_id_;
+};
+
+// Exclude symbolic::Expression from the scalartype conversion of
+// LinearTransformDensity
+namespace scalar_conversion {
+template <>
+struct Traits<LinearTransformDensity> : public NonSymbolicTraits {};
+}  // namespace scalar_conversion
+}  // namespace systems
+}  // namespace drake
