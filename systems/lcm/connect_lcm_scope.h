03eaa693957007abfef10d8b8b97b21fc0bf2829
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-06-30 19:43:04 -0700

    [lcm] Deprecate ConnectLcmScope

--- systems/lcm/connect_lcm_scope.h
+++ systems/lcm/connect_lcm_scope.h
@@ -9,31 +9,9 @@ namespace drake {
 namespace systems {
 namespace lcm {
 
-// TODO(jwnimmer-tri) Add DRAKE_DEPRECATED onto here as of 2021-01-01 or so,
-// with the removal date the usual +3 months after the marker gets added.
-/**
- (To be deprecated.) Prefer to use LcmScopeSystem::AddToBuilder instead of
- this function; the LcmScopeSystem provides more detailed timestamps.
-
- Provides the ability to publish any vector-valued output port to the LCM
- @p channel, using the drake::lcmt_drake_signal LCM message type, by adding
- an appropriate LcmPublisherSystem to the @p builder.  If @p lcm is
- null, then an LCM instance will be created automatically (but this is
- expensive, and creating multiple LCM instances in a single process should be
- avoided).
-
- The intention is to enable logging and debugging in complex diagrams
- using external tools like `lcm-spy`.
-
- The optional @p publish_period specifies how often messages will be published.
- If the period is zero (the default), then the underlying LcmPublisherSystem
- will publish every step.
-
- @pre @p src must be an OutputPort of a system that has already been added to
- the @p builder.
-
- @ingroup message_passing
- */
+DRAKE_DEPRECATED("2021-11-01",
+    "Prefer to use LcmScopeSystem::AddToBuilder instead of this function;"
+    " the LcmScopeSystem provides more detailed timestamps.")
 LcmPublisherSystem* ConnectLcmScope(const OutputPort<double>& src,
                                     const std::string& channel,
                                     systems::DiagramBuilder<double>* builder,
