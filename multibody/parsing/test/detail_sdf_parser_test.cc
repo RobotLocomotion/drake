fc8fce8889b257e3fd0d871adbd20dffb74e92da
Eric Cousineau <eric.cousineau@tri.global>
2021-07-01 15:09:30 -0400

    detail_sdf_parser_test: Make ParseTestString use string, not file
    This helps to avoid spurious Mac CI failures

--- multibody/parsing/test/detail_sdf_parser_test.cc
+++ multibody/parsing/test/detail_sdf_parser_test.cc
@@ -57,18 +57,27 @@ ModelInstanceIndex AddModelFromSdfFile(
     const PackageMap& package_map,
     MultibodyPlant<double>* plant,
     geometry::SceneGraph<double>* scene_graph = nullptr) {
+  const DataSource data_source{&file_name, {}};
   return AddModelFromSdf(
-      { .file_name = &file_name },
-      model_name, package_map, plant, scene_graph);
+      data_source, model_name, package_map, plant, scene_graph);
 }
 std::vector<ModelInstanceIndex> AddModelsFromSdfFile(
     const std::string& file_name,
     const PackageMap& package_map,
     MultibodyPlant<double>* plant,
     geometry::SceneGraph<double>* scene_graph = nullptr) {
+  const DataSource data_source{&file_name, {}};
   return AddModelsFromSdf(
-      { .file_name = &file_name },
-      package_map, plant, scene_graph);
+      data_source, package_map, plant, scene_graph);
+}
+std::vector<ModelInstanceIndex> AddModelsFromSdfString(
+    const std::string& file_contents,
+    const PackageMap& package_map,
+    MultibodyPlant<double>* plant,
+    geometry::SceneGraph<double>* scene_graph = nullptr) {
+  const DataSource data_source{{}, &file_contents};
+  return AddModelsFromSdf(
+      data_source, package_map, plant, scene_graph);
 }
 
 const Frame<double>& GetModelFrameByName(const MultibodyPlant<double>& plant,
@@ -267,17 +276,15 @@ struct PlantAndSceneGraph {
 
 PlantAndSceneGraph ParseTestString(const std::string& inner,
                                    const std::string& sdf_version = "1.6") {
-  const std::string filename = temp_directory() + "/test_string.sdf";
-  std::ofstream file(filename);
-  file << "<sdf version='" << sdf_version << "'>" << inner << "\n</sdf>\n";
-  file.close();
+  const std::string file_contents =
+      "<sdf version='" + sdf_version + "'>" + inner + "\n</sdf>\n";
   PlantAndSceneGraph pair;
   pair.plant = std::make_unique<MultibodyPlant<double>>(0.0);
   pair.scene_graph = std::make_unique<SceneGraph<double>>();
   PackageMap package_map;
   pair.plant->RegisterAsSourceForSceneGraph(pair.scene_graph.get());
   drake::log()->debug("inner: {}", inner);
-  AddModelsFromSdfFile(filename, package_map, pair.plant.get());
+  AddModelsFromSdfString(file_contents, package_map, pair.plant.get());
   return pair;
 }
 
