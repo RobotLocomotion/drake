0b6ac772e1bc472921ca324e535505139b1aae61
Russ Tedrake <russt@mit.edu>
2021-07-12 13:17:40 -0400

    [math] more helpful error message in DecomposePSDmatrixIntoXtransposeTimesX (#15365)
    * [math] more helpful error message in DecomposePSDmatrixIntoXtransposeTimesX

--- math/test/quadratic_form_test.cc
+++ math/test/quadratic_form_test.cc
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
+#include "drake/common/test_utilities/expect_throws_message.h"
 #include "drake/math/matrix_util.h"
 #include "drake/math/rotation_matrix.h"
 
@@ -78,9 +79,11 @@ GTEST_TEST(TestDecomposePSDmatrixIntoXtransposeTimesX, Test5) {
 
 GTEST_TEST(TestDecomposePSDmatrixIntoXtransposeTimesX, negativeY) {
   // Y is a negative definite matrix.
-  EXPECT_THROW(
+  DRAKE_EXPECT_THROWS_MESSAGE(
       DecomposePSDmatrixIntoXtransposeTimesX(-Eigen::Matrix3d::Identity(), 0),
-      std::runtime_error);
+      std::runtime_error,
+      "Y is not positive definite. It has an eigenvalue -1.* that is more "
+      "negative than the tolerance 0.*.");
 }
 
 GTEST_TEST(TestDecomposePSDmatrixIntoXtransposeTimesX, indefiniteY) {
