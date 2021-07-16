808aec811fe5d09b5db932ed1ea7aedc761b6a3c
Sean Curtis <sean.curtis@tri.global>
2021-07-16 08:20:00 -0700

    Introduce "transient" filter modifications
    This introduces the concept of a history of collision filter modifications
    and the ability to edit that history.
    
     - Introduce new id type: FilterId.
     - CollisionFilterManager introduces the concept and provides an API for
       exercising it.
     - CollisionFilter implements the plumbing.
     - Python bindings

--- geometry/collision_filter_manager.h
+++ geometry/collision_filter_manager.h
@@ -73,7 +73,97 @@ class GeometryState;
    If a proximity role is subsequently assigned, those geometries will _still_
    not be part of any user-declared collision filters.
  - In general, adding collisions and assigning proximity roles should
-   happen prior to collision filter configuration. */
+   happen prior to collision filter configuration.
+
+ <h3>Transient vs Persistent changes</h3>
+
+ Collision filtering is all about defining which geometry pairs are in the set
+ C. The simplest way to modify the set C is through *persistent* modifications
+ to a "base configuration" (via calls to Apply()). However, declarations can
+ also be applied in a *transient* manner. Transient declarations form a history.
+ The current definition of the set C is the result of applying the history
+ of collision filter declarations to the persistent base configuration in order.
+ That history can be modified:
+
+   - New transient declarations can be appended.
+   - Arbitrary transient declarations can be removed from the sequence.
+   - The current configuration (based on a history with an arbitrary number
+     of transient declarations) can be "flattened" into the persistent base
+     (with no history).
+
+ The table below illustrates a sequence of operations and their effect on C. We
+ define C's initial configuration as `C = {P₁, P₂, ..., Pₙ}` (for `n` geometry
+ pairs). Each action is described as either persistent or transient.
+
+  | Line |  C                  |  Action |
+  | :--: | :------------------ | :-------------------------------------------------- |
+  |  1   | `{P₁, P₂, ..., Pₙ}` | Initial condition of the persistent base            |
+  |  2   | `{P₂, ..., Pₙ}`     | Remove P₁ from the persistent base                  |
+  |  3   | `{P₂, ..., Pₙ₋₁}`   | Remove Pₙ from the persistent base                  |
+  |  4   | `{P₂}`              | Remove all pairs except P₂ from the persistent base |
+  |  5   | `{P₂, P₄, P₅}`      | Transient declaration #1 puts P₄ and P₅ into C      |
+  |  6   | `{P₂, P₃, P₄, P₅}`  | Transient declaration #2 puts P₃ and P₄ into C      |
+  |  7   | `{P₂, P₃, P₄}`      | Remove declaration #1.                              |
+  |  8   | `{P₂, P₃, P₄}`      | Configuration flattened; #2 no longer exists        |
+  __Table 1__: An example sequence of operations on collision filters.
+
+ Notes:
+   - lines 2 - 4 represent a series of *persistent* operations, filtering pairs
+     of geometry (aka removing them from C by calling Apply()).
+   - line 5: the first transient filter declaration which is assigned the id
+     value #1 (via a call to ApplyTransient()).
+   - line 6: Adds a new transient filter declaration to the sequence (assigned
+     id #2). Note, that it redundantly declares that P₄ is a member of C just
+     as declaration #1 did. This redundancy is fine. In fact, it may be very
+     important (see below).
+   - line 7: We remove declaration #1. Although #1 added both P₄ and P₅ to C,
+     the result of removing this declaration from the sequence is that P₅ is no
+     longer a member of C but P₄ *is*. This is because #2's declaration that P₄
+     *is* in C preserves its membership.
+   - line 8: the current configuration is "flattened" into the persistent base.
+     All history is thrown out and any filter identifiers are rendered invalid.
+
+ This example workflow above illustrates some key ideas in using transient
+ declarations.
+
+   - __The persistent configuration can only be modified when there is no
+     transient history.__ Applying a filter declaration should have the effect
+     of realizing that declaration. If a pair is *declared* to be in C, the
+     result of the declaration is that the pair is in C (assuming that the pair
+     *can* be in C). If we allowed modifying the persistent configuration with
+     an active transient history, there might be no discernable change in the
+     resultant configuration state because a subsequent transient declaration
+     may supplant it. This would lead to inscrutable bugs. Therefore, it's
+     simply not allowed.
+   - __When defining a transient declaration, the declaration should include all
+     critical pairs *explicitly*.__ This includes those pairs that should and
+     should not be in C. Any pair *not* explicitly accounted for should be one
+     whose filter status is immaterial.
+     It *might* seems desirable (from an optimization perspective) to examine
+     the *current* configuration of C and apply the minimum change to put it
+     into a desired state (i.e., if a pair I need filtered is already filtered,
+     I would omit it from the declaration). This approach is fraught with peril.
+     If the current configuration is the result of previous transient
+     declarations, the removal of any of those declarations could invalidate the
+     attempted difference calculation and my declaration would no longer be
+     sufficient to guarantee the set C required.
+   - __Anyone at any time can add to the history.__ Some code can modify C to
+     suit its needs. However, it can also make subsequent calls into code that
+     also modifies C with no guarantees that the called code will undo those
+     changes. Code that modifies collision filters configuration should document
+     that it does so. It should also have a clear protocol for cleaning up its
+     own changes. Finally, code that applies declarations should not take for
+     granted that its transient declarations are necessarily the last
+     declarations in the history.
+
+ <h4>Making transient filter declarations</h4>
+
+ There is a custom API for applying a filter declaration as a *transient*
+ declaration. It returns the id for the transient API (used to remove the
+ declaration from the history sequence).
+
+ Attempting to change the persistent configuration when there are active
+ transient declarations in the history will throw an exception.   */
 class CollisionFilterManager {
  public:
   DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(CollisionFilterManager)
@@ -90,7 +180,8 @@ class CollisionFilterManager {
      - Attempts to "allow" collision between a pair that is strictly excluded
        (e.g., between two anchored geometries) will be ignored.
 
-   @throws std::exception if the `declaration` references invalid ids. */
+   @throws std::exception if the `declaration` references invalid ids or there
+                          is an active history. */
   void Apply(const CollisionFilterDeclaration& declaration) {
     /* Modifications made via this API are by the user. Only the internals can
      declare filters to be "invariant". */
@@ -110,6 +201,34 @@ class CollisionFilterManager {
   //  If those can be resolved, it would make more sense to make the
   //  CollisionFilterManager the one-stop shop for all things collision filter
   //  rather than having it split over two classes.
+
+  /** Applies the declaration as the newest *transient* modification to the
+   collision filter configuration. The declaration must be considered "valid",
+   as defined for Apply(). */
+  FilterId ApplyTransient(const CollisionFilterDeclaration& declaration) {
+    return filter_->ApplyTransient(declaration, extract_ids_);
+  }
+
+  /** Attempts to remove the transient declaration from the history for the
+   declaration associated with the given `filter_id`.
+   @param filter_id   The id of the filter declaration to remove.
+   @returns `true` iff `is_active(filter_id)` returns `true` before calling this
+            method (i.e., `filter_id` refers to an existent filter that has
+            successfully been removed). */
+  bool RemoveDeclaration(FilterId filter_id) {
+    return filter_->RemoveDeclaration(filter_id);
+  }
+
+  /** Reports if there are any active transient filter declarations. */
+  bool has_transient_history() const {
+    return filter_->has_transient_history();
+  }
+
+  /** Reports if the transient collision filter declaration indicated by the
+   given `filter_id` is part of the history. */
+  bool IsActive(FilterId filter_id) const {
+    return filter_->IsActive(filter_id);
+  }
 
  private:
   /* Only GeometryState can construct a collision filter manager. */
