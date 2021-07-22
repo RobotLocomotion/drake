b128153ecc0b96cf6e61997ef4860871069480fc
Damrong Guoy <42557859+DamrongGuoy@users.noreply.github.com>
2021-07-22 14:12:46 -0700

    [geometry] QueryObject's new queries for hydroelastics. (#15210)
    Adds two new queries to QueryObject -- the tentative API to
    access the "polygonal" contact surface. This API is a place
    holder; it's return type will change. Implementing this now
    is merely a convenience so end users can benefit from
    performance improvements without waiting for the new
    infrastructure.

--- geometry/geometry_state.h
+++ geometry/geometry_state.h
@@ -398,6 +398,11 @@ class GeometryState {
     return geometry_engine_->ComputeContactSurfaces(X_WGs_);
   }
 
+  /** Implementation of QueryObject::ComputePolygonalContactSurfaces().  */
+  std::vector<ContactSurface<T>> ComputePolygonalContactSurfaces() const {
+    return geometry_engine_->ComputePolygonalContactSurfaces(X_WGs_);
+  }
+
   /** Implementation of QueryObject::ComputeContactSurfacesWithFallback().  */
   void ComputeContactSurfacesWithFallback(
       std::vector<ContactSurface<T>>* surfaces,
@@ -405,6 +410,17 @@ class GeometryState {
     DRAKE_DEMAND(surfaces);
     DRAKE_DEMAND(point_pairs);
     return geometry_engine_->ComputeContactSurfacesWithFallback(
+        X_WGs_, surfaces, point_pairs);
+  }
+
+  /** Implementation of
+   QueryObject::ComputePolygonalContactSurfacesWithFallback().  */
+  void ComputePolygonalContactSurfacesWithFallback(
+      std::vector<ContactSurface<T>>* surfaces,
+      std::vector<PenetrationAsPointPair<T>>* point_pairs) const {
+    DRAKE_DEMAND(surfaces);
+    DRAKE_DEMAND(point_pairs);
+    return geometry_engine_->ComputePolygonalContactSurfacesWithFallback(
         X_WGs_, surfaces, point_pairs);
   }
 
