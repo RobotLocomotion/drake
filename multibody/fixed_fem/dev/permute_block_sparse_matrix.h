0cf01d4c2225c3acf119ae771022712d33be66f1
Xuchen Han <xuchen.han@tri.global>
2021-07-30 11:49:58 -0700

    [fem] Add PermuteBlockVector()
    This matrix utility method facilitates mapping velocities/contact forces
    in the original order of the deformable vertices to the order where
    all participating vertex quantities come first and vice versa.
    
    Fix typo in documentation of PermuteBlockSparseMatrix().

--- multibody/fixed_fem/dev/permute_block_sparse_matrix.h
+++ multibody/fixed_fem/dev/permute_block_sparse_matrix.h
@@ -8,11 +8,11 @@ namespace drake {
 namespace multibody {
 namespace fem {
 namespace internal {
-
-/* Given an Eigen::SparseMatrix with block structure with 3x3 block entries Bᵢⱼ
-where i, j ∈ V = {0,...,num_vertices-1} and a permutation P on V, this method
-builds the permuted matrix with 3x3 block entries Cᵢⱼ such that Cᵢⱼ =
-B_{P(i),P(j)}.
+// TODO(xuchenhan-tri): Consider moving this method to matrix_utilities.h/cc.
+/* Given a 3N-by-3N Eigen::SparseMatrix with block structure with 3x3 block
+entries Bᵢⱼ where i, j ∈ V = {0, ..., N-1} and a permutation P on V, this method
+builds the permuted matrix with 3x3 block entries C's such that Cₚ₍ᵢ₎ₚ₍ⱼ₎ =
+Bᵢⱼ.
 
 For example, suppose the input `matrix` is given by
        a a a b b b
@@ -31,8 +31,8 @@ permutation will be:
        b b b a a a
 
 @param[in] matrix             The original block sparse matrix to be permuted.
-@param[in] block_permutation  block_permutation[i] gives the permuted index
-                              of the block whose original index is `i`.
+@param[in] block_permutation  block_permutation[i] gives the index of the
+                              permuted block whose original index is `i`.
 @pre matrix.rows() == matrix.cols().
 @pre matrix.rows() % 3 == 0.
 @pre block_permutation is a permutation of {0, 1, ..., matrix.rows()/3-1}. */
