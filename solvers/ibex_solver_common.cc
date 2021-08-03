e3ccc3ece5c4610ad1e4580d6013a491d27160fd
Soonho Kong <soonho.kong@tri.global>
2021-08-03 14:26:48 -0400

    [solvers] Add IbexSolver

---
+++ solvers/ibex_solver_common.cc
@@ -0,0 +1,43 @@
+/* clang-format off to disable clang-format-includes */
+#include "drake/solvers/ibex_solver.h"
+/* clang-format on */
+
+#include "drake/common/never_destroyed.h"
+#include "drake/solvers/mathematical_program.h"
+
+// This file contains implementations that are common to both the available and
+// unavailable flavor of this class.
+
+namespace drake {
+namespace solvers {
+
+IbexSolver::IbexSolver()
+    : SolverBase(&id, &is_available, &is_enabled,
+                 &ProgramAttributesSatisfied) {}
+
+IbexSolver::~IbexSolver() = default;
+
+SolverId IbexSolver::id() {
+  static const never_destroyed<SolverId> singleton{"dReal"};
+  return singleton.access();
+}
+
+bool IbexSolver::is_enabled() { return true; }
+
+bool IbexSolver::ProgramAttributesSatisfied(const MathematicalProgram& prog) {
+  static const never_destroyed<ProgramAttributes> solver_capabilities(
+      std::initializer_list<ProgramAttribute>{
+          ProgramAttribute::kGenericConstraint,
+          ProgramAttribute::kLinearEqualityConstraint,
+          ProgramAttribute::kLinearConstraint,
+          ProgramAttribute::kLorentzConeConstraint,
+          ProgramAttribute::kRotatedLorentzConeConstraint,
+          ProgramAttribute::kLinearComplementarityConstraint,
+          ProgramAttribute::kLinearCost, ProgramAttribute::kQuadraticCost,
+          ProgramAttribute::kBinaryVariable});
+  return AreRequiredAttributesSupported(prog.required_capabilities(),
+                                        solver_capabilities.access());
+}
+
+}  // namespace solvers
+}  // namespace drake
