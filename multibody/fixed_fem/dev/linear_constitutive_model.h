35e16dd30db25aa6ceda5e83035cbb0c581dce47
Xuchen Han <xuchen.han@tri.global>
2021-08-02 21:20:54 -0700

    [fem] Prepare to move ConstitutiveModel out of dev
    Misc improvements including:
    * Moving function definitions to .cc files.
    * Remove non-essential traits entries.
    * Remove unused forward declarations.
    * Provide default `DoCalc` methods for the base class that provides more
    readable error messages.

--- multibody/fixed_fem/dev/linear_constitutive_model.h
+++ multibody/fixed_fem/dev/linear_constitutive_model.h
@@ -1,12 +1,9 @@
 #pragma once
 
 #include <array>
-#include <utility>
 
 #include "drake/common/eigen_types.h"
-#include "drake/common/unused.h"
 #include "drake/multibody/fixed_fem/dev/constitutive_model.h"
-#include "drake/multibody/fixed_fem/dev/constitutive_model_utilities.h"
 #include "drake/multibody/fixed_fem/dev/linear_constitutive_model_data.h"
 
 namespace drake {
@@ -14,18 +11,11 @@ namespace multibody {
 namespace fem {
 namespace internal {
 
-/* Forward declare the model to be referred to in the traits class. */
-template <typename T, int num_locations>
-class LinearConstitutiveModel;
-
 /* Traits for LinearConstitutiveModel. */
 template <typename T, int num_locations>
 struct LinearConstitutiveModelTraits {
   using Scalar = T;
-  using ModelType = LinearConstitutiveModel<T, num_locations>;
-  using DeformationGradientDataType =
-      LinearConstitutiveModelData<T, num_locations>;
-  static constexpr int kNumLocations = num_locations;
+  using Data = LinearConstitutiveModelData<T, num_locations>;
 };
 
 /* Implements the infinitesimal-strain linear elasticity constitutive model as
@@ -41,10 +31,7 @@ class LinearConstitutiveModel final
           LinearConstitutiveModelTraits<T, num_locations>> {
  public:
   using Traits = LinearConstitutiveModelTraits<T, num_locations>;
-  using ModelType = typename Traits::ModelType;
-  using DeformationGradientDataType =
-      typename Traits::DeformationGradientDataType;
-  using Base = ConstitutiveModel<ModelType, Traits>;
+  using Data = typename Traits::Data;
 
   DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(LinearConstitutiveModel)
 
@@ -52,39 +39,9 @@ class LinearConstitutiveModel final
    prescribed Young's modulus and Poisson ratio.
    @param youngs_modulus Young's modulus of the model, with unit N/m²
    @param poisson_ratio Poisson ratio of the model, unitless.
-   @pre youngs_modulus must be non-negative.
-   @pre poisson_ratio must be strictly greater than -1 and strictly smaller than
-   0.5. */
-  LinearConstitutiveModel(const T& youngs_modulus, const T& poisson_ratio)
-      : E_(youngs_modulus), nu_(poisson_ratio) {
-    std::tie(lambda_, mu_) = CalcLameParameters(E_, nu_);
-    /* Recall that
-          Pᵢⱼ = 2μ * εᵢⱼ + λ * εₐₐ * δᵢⱼ,
-      So,
-          ∂Pᵢⱼ/∂Fₖₗ = 2μ * ∂εᵢⱼ/∂Fₖₗ + λ * ∂εₐₐ/∂Fₖₗ * δᵢⱼ,
-      Since
-          ∂εᵢⱼ/∂Fₖₗ = 0.5 * δᵢₖ δⱼₗ  + 0.5 * δᵢₗ δₖⱼ.
-      Plugging in, we get:
-          ∂Pᵢⱼ/∂Fₖₗ = μ * (δᵢₖδⱼₗ + δᵢₗ δⱼₖ) +  λ * δₖₗ * δᵢⱼ.
-      Keep in mind that the indices are laid out such that the ik-th entry in
-      the jl-th block corresponds to the value dPᵢⱼ/dFₖₗ.  */
-    // First term.
-    dPdF_ = mu_ * Eigen::Matrix<T, 9, 9>::Identity();
-    for (int k = 0; k < 3; ++k) {
-      // Second term.
-      for (int l = 0; l < 3; ++l) {
-        const int i = l;
-        const int j = k;
-        dPdF_(3 * j + i, 3 * l + k) += mu_;
-      }
-      // Third term.
-      for (int i = 0; i < 3; ++i) {
-        const int l = k;
-        const int j = i;
-        dPdF_(3 * j + i, 3 * l + k) += lambda_;
-      }
-    }
-  }
+   @pre youngs_modulus >= 0.
+   @pre -1 < poisson_ratio < 0.5. */
+  LinearConstitutiveModel(const T& youngs_modulus, const T& poisson_ratio);
 
   const T& youngs_modulus() const { return E_; }
 
@@ -95,41 +52,26 @@ class LinearConstitutiveModel final
   const T& lame_first_parameter() const { return lambda_; }
 
  private:
+  using Base =
+      ConstitutiveModel<LinearConstitutiveModel<T, num_locations>, Traits>;
   friend Base;
 
-  /* Implements ConstitutiveModel::CalcElasticEnergyDensity() in the CRTP base
-   class. */
+  /* Shadows ConstitutiveModel::DoCalcElasticEnergyDensity() as required by the
+   CRTP base class. */
   void DoCalcElasticEnergyDensity(
       const LinearConstitutiveModelData<T, num_locations>& data,
-      std::array<T, num_locations>* Psi) const {
-    for (int i = 0; i < num_locations; ++i) {
-      const auto& strain = data.strain()[i];
-      const auto& trace_strain = data.trace_strain()[i];
-      (*Psi)[i] = mu_ * strain.squaredNorm() +
-                  0.5 * lambda_ * trace_strain * trace_strain;
-    }
-  }
+      std::array<T, num_locations>* Psi) const;
 
-  /* Implements ConstitutiveModel::CalcFirstPiolaStress() in the CRTP base
-   class. */
-  void DoCalcFirstPiolaStress(const DeformationGradientDataType& data,
-                              std::array<Matrix3<T>, num_locations>* P) const {
-    for (int i = 0; i < num_locations; ++i) {
-      const auto& strain = data.strain()[i];
-      const auto& trace_strain = data.trace_strain()[i];
-      (*P)[i] =
-          2.0 * mu_ * strain + lambda_ * trace_strain * Matrix3<T>::Identity();
-    }
-  }
-
-  /* Implements ConstitutiveModel::CalcFirstPiolaStressDerivative() in the CRTP
+  /* Shadows ConstitutiveModel::DoCalcFirstPiolaStress() as required by the CRTP
    base class. */
+  void DoCalcFirstPiolaStress(const Data& data,
+                              std::array<Matrix3<T>, num_locations>* P) const;
+
+  /* Shadows ConstitutiveModel::DoCalcFirstPiolaStressDerivative() as required
+   by the CRTP base class. */
   void DoCalcFirstPiolaStressDerivative(
-      const DeformationGradientDataType& data,
-      std::array<Eigen::Matrix<T, 9, 9>, num_locations>* dPdF) const {
-    unused(data);
-    std::fill(dPdF->begin(), dPdF->end(), dPdF_);
-  }
+      const Data& data,
+      std::array<Eigen::Matrix<T, 9, 9>, num_locations>* dPdF) const;
 
   T E_;       // Young's modulus, N/m².
   T nu_;      // Poisson ratio.
