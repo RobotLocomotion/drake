5b812adfcbbc10f8b4e87631f204bbb62beb3661
Xuchen Han <xuchen.han@tri.global>
2021-08-04 11:28:24 -0700

    [fem] Move LinearConstitutiveModelData out of dev

--- multibody/fem/linear_constitutive_model_data.h
+++ multibody/fem/linear_constitutive_model_data.h
@@ -21,16 +21,10 @@ class LinearConstitutiveModelData
     : public DeformationGradientData<
           LinearConstitutiveModelData<T, num_locations>> {
  public:
-  using Base =
-      DeformationGradientData<LinearConstitutiveModelData<T, num_locations>>;
-
   DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(LinearConstitutiveModelData);
 
   /* Constructs a LinearConstitutiveModelData with zero strain. */
-  LinearConstitutiveModelData() {
-    strain_.fill(Matrix3<T>::Zero());
-    trace_strain_.fill(0.0);
-  }
+  LinearConstitutiveModelData();
 
   /* Returns the infinitesimal strains evaluated at the quadrature locations
    for the associated element. */
@@ -45,18 +39,14 @@ class LinearConstitutiveModelData
   }
 
  private:
+  using Base =
+      DeformationGradientData<LinearConstitutiveModelData<T, num_locations>>;
+
   friend Base;
 
   /* Shadows DeformationGradientData::UpdateFromDeformationGradient() as
    required by the CRTP base class. */
-  void UpdateFromDeformationGradient() {
-    const std::array<Matrix3<T>, num_locations>& F =
-        this->deformation_gradient();
-    for (int i = 0; i < num_locations; ++i) {
-      strain_[i] = 0.5 * (F[i] + F[i].transpose()) - Matrix3<T>::Identity();
-      trace_strain_[i] = strain_[i].trace();
-    }
-  }
+  void UpdateFromDeformationGradient();
 
   /* Infinitesimal strain = 0.5 * (F + Fᵀ) - I. */
   std::array<Matrix3<T>, num_locations> strain_;
