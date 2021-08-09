f5c666a030407d20ca295e631082920ae8e53938
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-08-08 18:40:13 -0700

    fixup! Hand roll the warning to avoid drake.common native code

--- __init__.py
+++ __init__.py
@@ -24,10 +24,13 @@ except ImportError:
 # If yes, then alias pydrake.lcmtypes.lcmt_foo as drake.lcmt_foo.
 if _USES_DEPRECATED_LCMTYPES:
     from pydrake.lcmtypes import *
-    from pydrake.common.deprecation import _warn_deprecated
-    _warn_deprecated(
+
+    # Warn the user about the pending removal.
+    import warnings as _warnings
+    _warnings.warn(
         "Drake's lcmtypes for Python are now named pydrake.lcmtypes.lcmt_foo"
-        " instead of drake.lcmt_foo. The 'drake' package name is deprecated."
+        " instead of drake.lcmt_foo. The 'drake' module name is deprecated."
         " Please adjust your import statements to match the new name, and"
-        " remove the BUILD dependency on @drake//lcmtypes:lcmtypes_drake_py.",
-        date="2021-12-01")
+        " remove the BUILD dependency on @drake//lcmtypes:lcmtypes_drake_py."
+        " The old module will be removed from Drake on or after 2021-12-01.",
+        category=DeprecationWarning)
