f6dbe2ecee758ef59af9a7ee80ba38b2cf08e950
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-08-09 14:24:59 -0700

    [pydrake] Add lcmtypes under a new package name
    Alias drake.lcmt_foo to pydrake.lcmtypes.lcmt_foo.
    Deprecatate the drake module spelling for lcmtypes.

---
+++ lcmtypes/drake/__init__.py
@@ -0,0 +1,11 @@
+# This file is only used by the installed copy of Drake, to provide a
+# compatiblity alias for "from drake import lcmt_foo".
+
+from pydrake.common.deprecation import _warn_deprecated
+from pydrake.lcmtypes import *
+
+_warn_deprecated(
+    "Drake's lcmtypes for Python are now named pydrake.lcmtypes.lcmt_foo"
+    " instead of drake.lcmt_foo. The 'drake' package name is deprecated."
+    " Please adjust your import statements to match the new name.",
+    date="2021-12-01")
