17ac4f2234b95408617880a23e15853d81c6a12c
Russ Tedrake <russt@mit.edu>
2021-08-20 08:54:08 -0400

    [geometry] Establish Meshcat in C++ (step 3)
    Implements the most essential elements of the Meshcat API:
    - Added SceneTree data structure.
    - Added stduuid (to create the uuid's required by meshcat)
    - Added SetObject from Shape, Rgba
      - Supports all shapes that meshcat_visualizer.py supports (more, in fact)
      - Does not support texture maps yet.
    - Added SetTransform
    - Added Delete
    - Added SetProperty<double>
    - Added HasPath, GetPacked* methods, primarily for testing.
    
    This PR is > 500 lines, but a significant portion of this commit is relatively boilerplate management of the msgpack data structures used to communicate with the javascript client.
    
    Updated the meshcat_manual_test to step the user through the checks.
    Updated meschat_test to hammer on the new API.

--- geometry/meshcat.h
+++ geometry/meshcat.h
@@ -4,21 +4,76 @@
 #include <string>
 
 #include "drake/common/drake_copyable.h"
+#include "drake/geometry/rgba.h"
+#include "drake/geometry/shape_specification.h"
+#include "drake/math/rigid_transform.h"
 
 namespace drake {
 namespace geometry {
 
-/** Provides an interface to https://github.com/rdeits/meshcat.
+/** Provides an interface to %Meshcat (https://github.com/rdeits/meshcat).
 
 Each instance of this class spawns a thread which runs an http/websocket server.
 Users can navigate their browser to the hosted URL to visualize the Meshcat
 scene.  Note that, unlike many visualizers, one cannot open the visualizer until
 this server is running.
 
-Note: This code is currently a skeleton implementation; it only allows you to
-set (boolean) properties as a minimal demonstration of sending data from C++ to
-the viewer.  It is the result of the second PR in a train of PRs that will
-establish the full Meshcat functionality. See #13038.
+In the current implementation, Meshcat methods must be called from the same
+thread where the class instance was constructed.  For example, running multiple
+simulations in parallel using the same Meshcat instance is not yet supported. We
+may generalize this in the future.
+
+@section meshcat_path Meshcat paths and the scene tree
+
+https://github.com/rdeits/meshcat#api provides a nice introduction to the
+websocket API that we wrap with this class.  One of the core concepts is the
+"scene tree" -- a directory-like structure of objects, transforms, and
+properties. The scene tree is viewable in the browser by clicking on "Open
+Controls" in the top right corner.
+
+Elements of the tree are referenced programmatically by a "/"-delimited string
+indicating the object's **path** in the scene tree. An object at path "/foo/bar"
+is a child of an object at path "/foo", so setting the transform of (or
+deleting) "/foo" will also affect its children.
+
+The string path arguments to the methods in this class use the following
+semantics:
+- A path that begins with "/" is treated as an absolute path, and is used
+without modification.
+- A path that *does not* begin with "/" is treated as a relative path to the
+default working directory.
+- The "working directory" is fixed to be "/drake" in the current implementation.
+So any relative path "foo" will be treated as "/drake/foo".
+- Delete("/foo") will remove all objects, transforms, and properties in "/foo"
+and its children from the scene tree.
+- Delete() is equivalent to Delete("") or Delete("/drake/").
+- SetObject("/foo", ...) places the object at "/foo/<object>", where "<object>"
+is a hard-coded suffix used for all objects.  You can use the
+Delete("/foo/<object>") to delete the object only (and not the children of
+"/foo") and must use SetProperty("/foo/<object>", ...) to change object-specific
+properties.
+
+The root directory contains a number of elements that are set up automatically
+in the browser.  These include "/Background", "/Lights", "/Grid", and
+"/Cameras".  To find more details please see the @link
+https://github.com/rdeits/meshcat#api meshcat documentation @endlink and the
+@link https://threejs.org/docs/index.html three.js documentation @endlink.
+- You can modify these elements, create new lights/cameras, and even delete
+these elements (one at a time).
+- Delete("/") is not allowed.  It will be silently ignored.
+
+@section meshcat_recommended_workflow Recommended workflow
+
+For convenience, Meshcat fosters a work flow in which all user-created objects
+created in Drake are contained in the "/drake" folder. Objects added with a
+relative path are placed in the "/drake" folder for you. The benefits are:
+- It's simple to distinguish between user objects and "infrastructure" objects
+in the visualizer.
+- All user objects can easily be cleared by a single, parameter-free call to
+Delete(). You are welcome to use absolute paths to organize your data, but the
+burden on tracking and cleaning them up lie on you.
+
+
 */
 class Meshcat {
  public:
@@ -37,13 +92,94 @@ class Meshcat {
   interface.  Most users should connect via a browser opened to web_url(). */
   std::string ws_url() const;
 
-  /** Forwards a set_property(...) message to the meshcat viewers. For example,
+  /** Sets the 3D object at a given `path` in the scene tree.  Note that
+  `path`="/foo" will always set an object in the tree at "/foo/<object>".  See
+  @ref meshcat_path.  Any objects previously set at this `path` will be
+  replaced.
+  @param path a "/"-delimited string indicating the path in the scene tree.
+              See @ref meshcat_path for the semantics.
+  @param shape a Shape that specifies the geometry of the object.
+  @param rgba an Rgba that specifies the (solid) color of the object.
+  */
+  void SetObject(std::string_view path, const Shape& shape,
+                 const Rgba& rgba = Rgba(.9, .9, .9, 1.));
+
+  // TODO(russt): SetObject with texture map.
+
+  /** Set the RigidTransform for a given path in the scene tree. An object's
+  pose is the concatenation of all of the transforms along its path, so setting
+  the transform of "/foo" will move the objects at "/foo/box1" and
+  "/foo/robots/HAL9000".
+  @param path a "/"-delimited string indicating the path in the scene tree.
+              See @ref meshcat_path for the semantics.
+  @param X_ParentPath the relative transform from the path to its immediate
+  parent.
+  */
+  void SetTransform(std::string_view path,
+                    const math::RigidTransformd& X_ParentPath);
+
+  /** Deletes the object at the given `path` as well as all of its children.
+  See @ref meshcat_path for the detailed semantics of deletion. */
+  void Delete(std::string_view path = "");
+
+  /** Sets a single named property of the object at the given path. For example,
   @verbatim
   meshcat.SetProperty("/Background", "visible", false);
   @endverbatim
-  will turn off the background. */
-  void SetProperty(std::string_view path, std::string_view property,
-                   bool value);
+  will turn off the background. See @ref meshcat_path for more details about
+  these properties and how to address them.
+
+  @param path a "/"-delimited string indicating the path in the scene tree.
+              See @ref meshcat_path for the semantics.
+  @param property the string name of the property to set
+  @param value the new value.
+  */
+  void SetProperty(std::string_view path, std::string property, bool value);
+
+  /** Sets a single named property of the object at the given path. For example,
+  @verbatim
+  meshcat.SetProperty("/Cameras/default/rotated/<object>", "zoom", 2.0);
+  meshcat.SetProperty("/Lights/DirectionalLight/<object>", "intensity", 1.0);
+  @endverbatim
+  See @ref meshcat_path for more details about these properties and how to
+  address them.
+
+  @param path a "/"-delimited string indicating the path in the scene tree.
+              See @ref meshcat_path for the semantics.
+  @param property the string name of the property to set
+  @param value the new value.
+  */
+  void SetProperty(std::string_view path, std::string property, double value);
+
+  // TODO(russt): Implement SetAnimation().
+  // TODO(russt): Implement SetButton() and SetSlider() as wrappers on
+  // set_control.
+
+  /* These remaining public methods are intended to primarily for testing. These
+  calls must safely acquire the data from the websocket thread and will block
+  execution waiting for that data to be acquired. They are intentionally
+  excluded from doxygen. */
+
+  /* Returns true iff `path` exists in the current internal (server) tree.
+  Note that the javascript client maintains its own tree, which may contain
+  elements that are not in the server tree. */
+  bool HasPath(std::string_view path) const;
+
+  /* Returns the msgpack representation of the object stored in the internal
+  (server) tree at `path`, or the empty string if `path` does not exist or no
+  object has been set at `path`. */
+  std::string GetPackedObject(std::string_view path) const;
+
+  /* Returns the msgpack representation of the transform stored in the internal
+  (server) tree at `path`, or the empty string if `path` does not exist or no
+  transform has been set at `path`. */
+  std::string GetPackedTransform(std::string_view path) const;
+
+  /* Returns the msgpack representation of the `property` stored in the
+  internal (server) tree at `path`, or the empty string if `path` does not
+  exist or `property` has not been set at `path`. */
+  std::string GetPackedProperty(std::string_view path,
+                                std::string property) const;
 
  private:
   // Provides PIMPL encapsulation of websocket types.
--- geometry/test/meshcat_test.cc
+++ geometry/test/meshcat_test.cc
@@ -5,13 +5,18 @@
 #include <fmt/format.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <msgpack.hpp>
 
 #include "drake/common/find_resource.h"
+#include "drake/common/test_utilities/eigen_matrix_compare.h"
+#include "drake/geometry/meshcat_types.h"
 
 namespace drake {
 namespace geometry {
 namespace {
 
+using Eigen::Vector3d;
+using math::RigidTransformd;
 using ::testing::HasSubstr;
 
 // A small wrapper around std::system to ensure correct argument passing.
@@ -26,7 +31,7 @@ int SystemCall(const std::vector<std::string>& argv) {
 }
 
 GTEST_TEST(MeshcatTest, TestHttp) {
-  drake::geometry::Meshcat meshcat;
+  Meshcat meshcat;
   // Note: The server doesn't respect all requests; unfortunately we can't use
   // curl --head and wget --spider nor curl --range to avoid downloading the
   // full file.
@@ -42,8 +47,8 @@ GTEST_TEST(MeshcatTest, TestHttp) {
 }
 
 GTEST_TEST(MeshcatTest, ConstructMultiple) {
-  drake::geometry::Meshcat meshcat;
-  drake::geometry::Meshcat meshcat2;
+  Meshcat meshcat;
+  Meshcat meshcat2;
 
   EXPECT_THAT(meshcat.web_url(), HasSubstr("http://localhost:"));
   EXPECT_THAT(meshcat.ws_url(), HasSubstr("ws://localhost:"));
@@ -52,8 +57,178 @@ GTEST_TEST(MeshcatTest, ConstructMultiple) {
   EXPECT_NE(meshcat.web_url(), meshcat2.web_url());
 }
 
-void CheckCommand(const drake::geometry::Meshcat& meshcat, int message_num,
-                  const std::string& desired_command_json) {
+// The correctness of this is established with meshcat_manual_test.  Here we
+// simply aim to provide code coverage for CI (e.g., no segfaults).
+GTEST_TEST(MeshcatTest, SetObjectWithShape) {
+  Meshcat meshcat;
+  EXPECT_TRUE(meshcat.GetPackedObject("sphere").empty());
+  meshcat.SetObject("sphere", Sphere(.25), Rgba(1.0, 0, 0, 1));
+  EXPECT_FALSE(meshcat.GetPackedObject("sphere").empty());
+  meshcat.SetObject("cylinder", Cylinder(.25, .5), Rgba(0.0, 1.0, 0, 1));
+  EXPECT_FALSE(meshcat.GetPackedObject("cylinder").empty());
+  // HalfSpaces are not supported yet; this should only log a warning.
+  meshcat.SetObject("halfspace", HalfSpace());
+  EXPECT_TRUE(meshcat.GetPackedObject("halfspace").empty());
+  meshcat.SetObject("box", Box(.25, .25, .5), Rgba(0, 0, 1, 1));
+  EXPECT_FALSE(meshcat.GetPackedObject("box").empty());
+  meshcat.SetObject("ellipsoid", Ellipsoid(.25, .25, .5), Rgba(1., 0, 1, 1));
+  EXPECT_FALSE(meshcat.GetPackedObject("ellipsoid").empty());
+  // Capsules are not supported yet; this should only log a warning.
+  meshcat.SetObject("capsule", Capsule(.25, .5));
+  EXPECT_TRUE(meshcat.GetPackedObject("capsule").empty());
+  meshcat.SetObject(
+      "mesh", Mesh(FindResourceOrThrow(
+                       "drake/systems/sensors/test/models/meshes/box.obj"),
+                   .25));
+  EXPECT_FALSE(meshcat.GetPackedObject("mesh").empty());
+  meshcat.SetObject(
+      "convex", Convex(FindResourceOrThrow(
+                           "drake/systems/sensors/test/models/meshes/box.obj"),
+                       .25));
+  EXPECT_FALSE(meshcat.GetPackedObject("convex").empty());
+  // Bad filename (no extension).  Should only log a warning.
+  meshcat.SetObject("bad", Mesh("test"));
+  EXPECT_TRUE(meshcat.GetPackedObject("bad").empty());
+  // Bad filename (file doesn't exist).  Should only log a warning.
+  meshcat.SetObject("bad", Mesh("test.obj"));
+  EXPECT_TRUE(meshcat.GetPackedObject("bad").empty());
+}
+
+GTEST_TEST(MeshcatTest, SetTransform) {
+  Meshcat meshcat;
+  EXPECT_FALSE(meshcat.HasPath("frame"));
+  EXPECT_TRUE(meshcat.GetPackedTransform("frame").empty());
+  const RigidTransformd X_ParentPath{math::RollPitchYawd(.5, .26, -3),
+                                     Vector3d{.9, -2., .12}};
+  meshcat.SetTransform("frame", X_ParentPath);
+
+  std::string transform = meshcat.GetPackedTransform("frame");
+  msgpack::object_handle oh =
+      msgpack::unpack(transform.data(), transform.size());
+  auto data = oh.get().as<internal::SetTransformData>();
+  EXPECT_EQ(data.type, "set_transform");
+  EXPECT_EQ(data.path, "/drake/frame");
+  Eigen::Map<Eigen::Matrix4d> matrix(data.matrix);
+  EXPECT_TRUE(CompareMatrices(matrix, X_ParentPath.GetAsMatrix4()));
+}
+
+GTEST_TEST(MeshcatTest, Delete) {
+  Meshcat meshcat;
+  // Ok to delete an empty tree.
+  meshcat.Delete();
+  EXPECT_FALSE(meshcat.HasPath(""));
+  EXPECT_FALSE(meshcat.HasPath("frame"));
+  meshcat.SetTransform("frame", RigidTransformd{});
+  EXPECT_TRUE(meshcat.HasPath(""));
+  EXPECT_TRUE(meshcat.HasPath("frame"));
+  EXPECT_TRUE(meshcat.HasPath("/drake/frame"));
+  // Deleting a random string does nothing.
+  meshcat.Delete("bad");
+  EXPECT_TRUE(meshcat.HasPath("frame"));
+  meshcat.Delete("frame");
+  EXPECT_FALSE(meshcat.HasPath("frame"));
+
+  // Deleting a parent directory deletes all children.
+  meshcat.SetTransform("test/frame", RigidTransformd{});
+  meshcat.SetTransform("test/frame2", RigidTransformd{});
+  meshcat.SetTransform("test/another/frame", RigidTransformd{});
+  EXPECT_TRUE(meshcat.HasPath("test/frame"));
+  EXPECT_TRUE(meshcat.HasPath("test/frame2"));
+  EXPECT_TRUE(meshcat.HasPath("test/another/frame"));
+  meshcat.Delete("test");
+  EXPECT_FALSE(meshcat.HasPath("test/frame"));
+  EXPECT_FALSE(meshcat.HasPath("test/frame2"));
+  EXPECT_FALSE(meshcat.HasPath("test/another/frame"));
+  EXPECT_TRUE(meshcat.HasPath("/drake"));
+
+  // Deleting the empty string deletes the prefix.
+  meshcat.SetTransform("test/frame", RigidTransformd{});
+  meshcat.SetTransform("test/frame2", RigidTransformd{});
+  meshcat.SetTransform("test/another/frame", RigidTransformd{});
+  EXPECT_TRUE(meshcat.HasPath("test/frame"));
+  EXPECT_TRUE(meshcat.HasPath("test/frame2"));
+  EXPECT_TRUE(meshcat.HasPath("test/another/frame"));
+  meshcat.Delete();
+  EXPECT_FALSE(meshcat.HasPath("test/frame"));
+  EXPECT_FALSE(meshcat.HasPath("test/frame2"));
+  EXPECT_FALSE(meshcat.HasPath("test/another/frame"));
+  EXPECT_FALSE(meshcat.HasPath("/drake"));
+}
+
+// Tests three methods of SceneTreeElement:
+// - SceneTreeElement::operator[]() is used in Meshcat::Set*().  We'll use
+// SetTransform() here.
+// - SceneTreeElement::Find() is used in Meshcat::HasPath() and
+// Meshcat::GetPacked*().  We'll use HasPath() to test.
+// - SceneTreeElement::Delete() is used in Meshat::Delete().
+// All of them also run through WebSocketPublisher::FullPath().
+GTEST_TEST(MeshcatTest, Paths) {
+  Meshcat meshcat;
+  // Absolute paths.
+  meshcat.SetTransform("/foo/frame", RigidTransformd{});
+  EXPECT_TRUE(meshcat.HasPath("/foo/frame"));
+  meshcat.Delete("/foo/frame");
+  EXPECT_FALSE(meshcat.HasPath("/foo/frame"));
+
+  // Absolute paths with strange spellings.
+  meshcat.SetTransform("///bar///frame///", RigidTransformd{});
+  EXPECT_TRUE(meshcat.HasPath("//bar//frame//"));
+  EXPECT_TRUE(meshcat.HasPath("/bar/frame"));
+  meshcat.Delete("////bar//frame///");
+  EXPECT_FALSE(meshcat.HasPath("/bar/frame"));
+
+  // Relative paths.
+  meshcat.SetTransform("frame", RigidTransformd{});
+  EXPECT_TRUE(meshcat.HasPath("frame"));
+  EXPECT_TRUE(meshcat.HasPath("/drake/frame"));
+
+  // Relative paths with strange spellings.
+  meshcat.SetTransform("bar///frame///", RigidTransformd{});
+  EXPECT_TRUE(meshcat.HasPath("bar//frame//"));
+  EXPECT_TRUE(meshcat.HasPath("/drake/bar/frame"));
+  meshcat.Delete("bar//frame//");
+  EXPECT_FALSE(meshcat.HasPath("bar/frame"));
+  EXPECT_FALSE(meshcat.HasPath("/drake/bar/frame"));
+}
+
+GTEST_TEST(MeshcatTest, SetPropertyBool) {
+  Meshcat meshcat;
+  EXPECT_FALSE(meshcat.HasPath("/Grid"));
+  EXPECT_TRUE(meshcat.GetPackedProperty("/Grid", "visible").empty());
+  meshcat.SetProperty("/Grid", "visible", false);
+  EXPECT_TRUE(meshcat.HasPath("/Grid"));
+
+  std::string property = meshcat.GetPackedProperty("/Grid", "visible");
+  msgpack::object_handle oh = msgpack::unpack(property.data(), property.size());
+  auto data = oh.get().as<internal::SetPropertyData<bool>>();
+  EXPECT_EQ(data.type, "set_property");
+  EXPECT_EQ(data.path, "/Grid");
+  EXPECT_EQ(data.property, "visible");
+  EXPECT_FALSE(data.value);
+}
+
+GTEST_TEST(MeshcatTest, SetPropertyDouble) {
+  Meshcat meshcat;
+  EXPECT_FALSE(meshcat.HasPath("/Cameras/default/rotated/<object>"));
+  EXPECT_TRUE(
+      meshcat.GetPackedProperty("/Cameras/default/rotated/<object>", "zoom")
+          .empty());
+  meshcat.SetProperty("/Cameras/default/rotated/<object>", "zoom", 2.0);
+  EXPECT_TRUE(meshcat.HasPath("/Cameras/default/rotated/<object>"));
+
+  std::string property =
+      meshcat.GetPackedProperty("/Cameras/default/rotated/<object>", "zoom");
+  msgpack::object_handle oh = msgpack::unpack(property.data(), property.size());
+  auto data = oh.get().as<internal::SetPropertyData<double>>();
+  EXPECT_EQ(data.type, "set_property");
+  EXPECT_EQ(data.path, "/Cameras/default/rotated/<object>");
+  EXPECT_EQ(data.property, "zoom");
+  EXPECT_EQ(data.value, 2.0);
+}
+
+void CheckWebsocketCommand(const drake::geometry::Meshcat& meshcat,
+                           int message_num,
+                           const std::string& desired_command_json) {
   EXPECT_EQ(SystemCall(
                 {FindResourceOrThrow("drake/geometry/meshcat_websocket_client"),
                  meshcat.ws_url(), std::to_string(message_num),
@@ -61,30 +236,30 @@ void CheckCommand(const drake::geometry::Meshcat& meshcat, int message_num,
             0);
 }
 
-GTEST_TEST(MeshcatTest, SetProperty) {
-  drake::geometry::Meshcat meshcat;
+GTEST_TEST(MeshcatTest, SetPropertyWebSocket) {
+  Meshcat meshcat;
   meshcat.SetProperty("/Background", "visible", false);
-  CheckCommand(meshcat, 1, R"""({
-              "property": "visible",
-              "value": false,
-              "path": "/Background",
-              "type": "set_property"
-              })""");
+  CheckWebsocketCommand(meshcat, 1, R"""({
+      "type": "set_property",
+      "path": "/Background",
+      "property": "visible",
+      "value": false
+    })""");
   meshcat.SetProperty("/Grid", "visible", false);
   // Note: The order of the messages is due to "/Background" < "/Grid" in the
   // std::map, not due to the order that SetProperty was called.
-  CheckCommand(meshcat, 1, R"""({
-              "property": "visible",
-              "value": false,
-              "path": "/Background",
-              "type": "set_property"
-              })""");
-  CheckCommand(meshcat, 2, R"""({
-              "property": "visible",
-              "value": false,
-              "path": "/Grid",
-              "type": "set_property"
-              })""");
+  CheckWebsocketCommand(meshcat, 1, R"""({
+      "type": "set_property",
+      "path": "/Background",
+      "property": "visible",
+      "value": false
+    })""");
+  CheckWebsocketCommand(meshcat, 2, R"""({
+      "type": "set_property",
+      "path": "/Grid",
+      "property": "visible",
+      "value": false
+    })""");
 }
 
 }  // namespace
