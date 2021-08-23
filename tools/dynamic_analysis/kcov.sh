087601e3925a2e9237a2d60a7625b91ea40c06d9
Rick Poyner <rick.poyner@tri.global>
2021-08-23 11:19:45 -0400

    kcov support: Add explicit failure for kcov:#339 situations

--- tools/dynamic_analysis/kcov.sh
+++ tools/dynamic_analysis/kcov.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 me=$(python3 -c 'import os; print(os.path.realpath("'"$0"'"))')
 WORKSPACE=$(dirname $(dirname $(dirname "${me}")))
 
@@ -11,6 +10,25 @@ if [ ! -f "${WORKSPACE}/WORKSPACE" ]; then
 fi
 
 export PATH="/opt/kcov/35/bin:${PATH}"
+
+# ELF binaries with 'sh' in their first 80 bytes are mishandled by kcov,
+# leading to segfault. See https://github.com/SimonKagstrom/kcov/issues/339
+is_kcov_339_vulnerable () {
+    file -b $(readlink -f "$1") | grep -q ELF \
+        && dd bs=80 count=1 if="$1" 2>/dev/null | grep -q sh
+}
+
+read -r -d '' ERROR_339 <<- eof
+Error: $1 is vulnerable to kcov issue #339; coverage analysis would
+misinterpret the binary as a shell script and then segfault.
+eof
+
+# Avoid tripping kcov issue #339 and give an explicit error. This check can be
+# removed once kcov version 40 is in use.
+if  is_kcov_339_vulnerable "$1"; then
+    echo "$ERROR_339"
+    exit 1
+fi
 
 kcov \
     "--include-path=${WORKSPACE}" \
