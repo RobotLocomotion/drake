6aaae4812b209c42c426a0e305c45e4934dee5ae
Gregory Izatt <gregory.izatt@gmail.com>
2021-08-26 22:06:12 -0400

    Bindings for IntervalBinning and MixedIntegerRotationConstraintGenerator.

--- bindings/pydrake/solvers/mixed_integer_optimization_util_py.cc
+++ bindings/pydrake/solvers/mixed_integer_optimization_util_py.cc
@@ -5,6 +5,7 @@
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/bindings/pydrake/symbolic_types_pybind.h"
 #include "drake/solvers/mixed_integer_optimization_util.h"
+#include "drake/solvers/mixed_integer_rotation_constraint.h"
 
 namespace drake {
 namespace pydrake {
@@ -39,6 +40,42 @@ PYBIND11_MODULE(mixed_integer_optimization_util, m) {
       },
       py::arg("prog"), py::arg("num_lambda"),
       doc.AddLogarithmicSos1Constraint.doc_2args);
-}
+
+    {
+      py::enum_<IntervalBinning>(m, "IntervalBinning", doc.IntervalBinning.doc)
+        .value("kLogarithmic", IntervalBinning::kLogarithmic)
+        .value("kLinear", IntervalBinning::kLinear);
+    }
+    
+    {
+        using Class = MixedIntegerRotationConstraintGenerator;
+        constexpr auto& cls_doc = doc.MixedIntegerRotationConstraintGenerator;
+        py::class_<Class> cls(m, "MixedIntegerRotationConstraintGenerator", cls_doc.doc);
+        
+        using Enum = Class::Approach;
+        constexpr auto& enum_doc = cls_doc.Approach;
+        py::enum_<Class::Approach>(cls, "Approach", enum_doc.doc)
+            .value("kBoxSphereIntersection", Enum::kBoxSphereIntersection,
+                enum_doc.kBoxSphereIntersection.doc)
+            .value("kBilinearMcCormick",
+                Enum::kBilinearMcCormick,
+                enum_doc.kBilinearMcCormick.doc)
+            .value("kBoth", Enum::kBoth,
+                enum_doc.kBoth.doc);
+       
+        using Struct = Class::ReturnType;
+        constexpr auto& struct_doc = cls_doc.ReturnType;
+        py::class_<Struct>(cls, "ReturnType", struct_doc.doc);
+        
+        cls
+            .def(py::init<Class::Approach, int, IntervalBinning>(),
+                 py::arg("approach"), py::arg("num_intervals_per_half_axis"),
+                 py::arg("interval_binning"),
+                 cls_doc.ctor.doc)
+            .def("AddToProgram", &Class::AddToProgram,
+                 py::arg("R"), py::arg("prog"), cls_doc.AddToProgram.doc) // keep-alive for prog ptr?
+        ;
+    }
+  }
 }  // namespace pydrake
 }  // namespace drake
