dcac6bed34ec3c3c6559dd11396590d7a8d01ba8
Greg Izatt <gizatt@mit.edu>
2021-08-31 15:55:59 -0400

    Add rotation constraint python bindings to solvers.all (#15687)
    * Add rotation constraint python bindings to solvers.all

--- bindings/pydrake/solvers/all.py
+++ bindings/pydrake/solvers/all.py
@@ -9,6 +9,7 @@ from .dreal import *
 from .gurobi import *  # noqa
 from .ipopt import *  # noqa
 from .mixed_integer_optimization_util import *  # noqa
+from .mixed_integer_rotation_constraint import *  # noqa
 from .mosek import *  # noqa
 from .osqp import *  # noqa
 from .sdpa_free_format import *  # noqa
