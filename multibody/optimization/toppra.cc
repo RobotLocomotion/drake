55d865dff4875198045be6b0d14fa2ab4b0825c5
mpetersen94 <markpetersen@g.harvard.edu>
2021-09-01 10:36:10 -0400

    Hacking toppra

---
+++ multibody/optimization/toppra.cc
@@ -0,0 +1,206 @@
+#include "drake/multibody/optimization/toppra.h"
+
+Eigen::VectorXd CalcGridpts(const PiecewisePolynomial<double>& path,
+                            double max_err, int max_iter, double max_seg_length,
+                            int min_points) {
+  std::vector<double> gridpts{path.start_time(), path.end_time()};
+  for (int iter = 0; iter < max_iter; iter++) {
+    bool add_points{false};
+    for (int ii = 0; ii < gridpts.size() - 1; ii++) {
+      double mid_pt{0.5 * (gridpts[ii] + gridpts[ii + 1])};
+      double dist{gridpts[ii + 1] - gridpts[ii]};
+      if (dist > max_seg_length) {
+        add_points = true;
+        gridpts.emplace(gridpts.begin() + ii + 1, mid_pt);
+        ii++;
+        continue;
+      }
+
+      double error{0.5 * std::pow(dist, 2) *
+                   path.EvalDerivative(mid_pt, 2).cwiseAbs().maxCoeff()};
+      if (error > max_err) {
+        add_points = true;
+        gridpts.emplace(gridpts.begin() + ii + 1, mid_pt);
+        ii++;
+        continue;
+      }
+    }
+    if (!add_points) {
+      break;
+    }
+  }
+  while (gridpts.size() < min_points) {
+    for (int ii{}; ii < gridpts.size() - 1; ii += 2) {
+      double mid_pt{0.5 * (gridpts[ii] + gridpts[ii + 1])};
+      gridpts.emplace(gridpts.begin() + ii + 1, mid_pt);
+    }
+  }
+  return Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(gridpts.data(),
+                                                       gridpts.size());
+}
+
+Toppra::Toppra(const PiecewisePolynomial<double>& path,
+               const MultibodyPlant<double>& plant,
+               const Eigen::VectorXd& gridpoints)
+    : backward_prog_{new solvers::MathematicalProgram()},
+      forward_prog_{new solvers::MathematicalProgram()},
+      // Variables for the backward pass are [x, u]
+      backward_vars_{backward_prog_.NewContinuousVariables(2)},
+      // Variables for the forward pass are u
+      forward_vars_{forward_prog_.NewContinuousVariables(1, "u")},
+      path_{path},
+      plant_{plant},
+      gridpoints_{gridpoints} {
+  DRAKE_DEMAND(gridpoints(0) == path.start_time());
+  DRAKE_DEMAND(gridpoints(Eigen::last) == path.end_time());
+}
+
+ToppraBoundingBoxConstraint& Toppra::AddJointVelocityLimit(
+    const Eigen::VectorXd& lower_limit, const Eigen::VectorXd upper_limit) {
+  const int N = gridpoints_.size() - 1;
+  const int n_dof = path_.rows();
+  DRAKE_DEMAND(lower_limit.size() == n_dof);
+  DRAKE_DEMAND(upper_limit.size() == n_dof);
+  Eigen::VectorXd x_lower_bound(N);
+  Eigen::VectorXd x_upper_bound(N);
+
+  for (int knot = 0; knot < N; knot++) {
+    Eigen::VectorXd qs_dot = path_.EvalDerivative(gridpoints_(knot), 1);
+
+    double sd_max = std::numeric_limits<double>::infinity();
+    double sd_min = -std::numeric_limits<double>::infinity();
+    for (int dof = 0; dof < n_dof; dof++) {
+      if (qs_dot(dof) > 0) {
+        sd_max = std::min(sd_max, upper_limit(dof) / qs_dot(dof));
+        sd_min = std::max(sd_min, lower_limit(dof) / qs_dot(dof));
+      }
+      elif (qs_dot(dof) < 0) {
+        sd_max = std::min(sd_max, lower_limit(dof) / qs_dot(dof));
+        sd_min = std::max(sd_min, upper_limit(dof) / qs_dot(dof));
+      }
+    }
+    x_lower_bound(knot) = std::pow(std::max(sd_min, 0), 2);
+    x_upper_bound(knot) = std::pow(sd_max, 2);
+  }
+  bb_constraints_.emplace_back(x_lower_bound, x_upper_bound);
+  return bb_constraints_.back();
+}
+
+ToppraLinearConstraint& ToppraAddJointAccelerationLimit(
+    const Eigen::VectorXd& lower_limit, const Eigen::VectorXd upper_limit,
+    ToppraDiscretization discretization) {
+  const int N = gridpoints_.size() - 1;
+  const int n_dof = path_.rows();
+  DRAKE_DEMAND(lower_limit.size() == n_dof);
+  DRAKE_DEMAND(upper_limit.size() == n_dof);
+  const int n_con = discretization == ToppraDiscretization::kInterpolation
+                        ? 2 * n_dof
+                        : n_dof;
+  Eigen::MatrixXd con_A(n_con, 2 * N);
+  Eigen::MatrixXd con_lb(n_con, N);
+  Eigen::MatrixXd con_ub(n_con, N);
+
+  for (size_t knot{}; knot < N; knot++) {
+    Eigen::VectorXd qs_dot = path_.EvalDerivative(gridpoints_(knot), 1);
+    Eigen::VectorXd qs_ddot = path_.EvalDerivative(gridpoints_(knot), 2);
+
+    con_A.block(0, 2 * knot, n_dof, 1) << qs_ddot;
+    con_A.block(0, 2 * knot + 1, n_dof, 1) << qs_dot;
+    con_lb.block(0, knot, n_dof, 1) << lower_limit;
+    con_ub.block(0, knot, n_dof, 1) << upper_limit;
+
+    if (discretization == ToppraDiscretization::kInterpolation) {
+      double delta = gridpts(knot + 1) - gridpts(knot);
+      Eigen::VectorXd qs_dot_next = path.EvalDerivative(gridpts(knot + 1), 1);
+      Eigen::VectorXd qs_ddot_next = path.EvalDerivative(gridpts(knot + 1), 2);
+
+      con_A.block(n_dof, 2 * knot, n_dof, 1) << qs_ddot_next;
+      con_A.block(n_dof, 2 * knot + 1, n_dof, 1)
+          << qs_dot_next + 2 * delta * qs_ddot_next;
+      con_lb.block(n_dof, knot, n_dof, 1) << lower_limit;
+      con_ub.block(n_dof, knot, n_dof, 1) << upper_limit;
+    }
+  }
+  lin_constraints_.emplace_back(con_A, con_lb, con_ub);
+  return lin_constraints_.back();
+}
+
+// ToppraLinearConstraint Toppra::AddJointTorqueLimit(
+//     const Eigen::VectorXd& lower_limit, const Eigen::VectorXd upper_limit);
+
+// ToppraBoundingBoxConstraint Toppra::AddFrameVelocityLimit(
+//     const Frame<double>& constraint_frame, const Eigen::VectorXd&
+//     lower_limit, const Eigen::VectorXd upper_limit);
+
+// ToppraLinearConstraint Toppra::AddFrameAccelerationLimit(
+//     const Frame<double>& constraint_frame, const Eigen::VectorXd&
+//     lower_limit, const Eigen::VectorXd upper_limit);
+
+Eigen::MatrixXd Toppra::ComputeBackwardPass(double s_dot_0, double s_dot_N) {
+  DRAKE_DEMAND(s_dot_0 >= 0);
+  DRAKE_DEMAND(s_dot_N >= 0);
+  Eigen::Vector2d min_cost_A(1, 1e-9);
+  Eigen::Vector2d max_cost_A(-1, -1e-9);
+  const int N = gridpoints_.size() - 1;
+
+  // Compute controllable set.
+  Eigen::MatrixXd K(N + 1, 2);
+  K.row(N) << std::pow(s_dot_N, 2), std::pow(s_dot_N, 2);
+  // Setup and solve sequence of one-step problems
+  for (int knot N - 1; knot > -1; knot--) {
+    // Setup constraints for both max & min
+    linear_con_lb(0, knot) = K(knot + 1, 0);
+    linear_con_ub(0, knot) = K(knot + 1, 1);
+    for (int bb_con = 0; bb_con < bb_constraint_coeff_.size(); bb_con++) {
+      Eigen::VectorXd lb, ub;
+      std::tie(lb, ub) = bb_constraint_coeff_[bb_con];
+      bb_constraint_[bb_con].evaluator()->set_bounds(lb.segment(knot, 1),
+                                                     ub.segment(knot, 1));
+    }
+    for (int lin_con = 0; lin_con < lin_constraint_coeff_.size(); lin_con++) {
+      Eigen::MatrixXd A, lb, ub;
+      std::tie(A, lb, ub) = lin_constraint_coeff_[lin_con];
+      backward_lin_constraint_[lin_con].evaluator()->UpdateCoefficients(
+          A.block(0, 2 * knot, A.rows(), 2), lb.col(knot), ub.col(knot));
+    }
+    // Solve minimum
+    {
+      backpass_cost_.evaluator()->UpdateCoefficients(min_cost_A);
+      auto result = solver_.Solve(backward_prog_);
+      K(knot, 0) = result.GetSolution()(0);
+    }
+    // Solve maximum
+    {
+      backpass_cost_.evaluator()->UpdateCoefficients(max_cost_A);
+      auto result = solver_.Solve(backward_prog_);
+      K(knot, 1) = result.GetSolution()(0);
+    }
+
+    if (K.row(knot).hasNaN()) {
+      throw std::runtime_error(
+          fmt::format("Toppra hit numerical issues. Controllable set "
+                      "at knot {}/{} "
+                      "can't be computed.",
+                      knot, N));
+    }
+    if (K(knot, 0) < 0) {
+      K(knot, 0) = 0;
+    }
+  }
+
+  // Check problem is controllable
+  double x_start = std::pow(s_dot_0, 2);
+  double eps = 1e-8;
+  if (x_start < K(0, 0) - eps || x_start > K(0, 1) + eps) {
+    throw std::runtime_error(fmt::format(
+        "ToppraSolver: Initial velocity not controllable. {} not in "
+        "({}, {}).",
+        x_start, K(0, 0), K(0, 1)));
+  }
+
+  return K;
+}
+
+Eigen::VectorXd Toppra::ComputeForwardPass(double s_dot_0, Eigen::MatrixXd& K);
+
+PiecewisePolynomial<double> Toppra::Solve() {}
--- multibody/optimization/toppra.h
+++ multibody/optimization/toppra.h
@@ -1,16 +1,20 @@
 #pragma once
 
-#include <memory>
-
 #include "drake/common/trajectories/piecewise_polynomial.h"
-// #include "drake/common/sorted_pair.h"
 #include "drake/multibody/plant/multibody_plant.h"
 #include "drake/solvers/mathematical_program.h"
 
 using common::trajectories::PiecewisePolynomial;
+using solvers::Binding;
+using solvers::BoundingBoxConstraint;
+using solvers::LinearCost;
+using solvers::LinearConstraint;
 
 namespace drake {
 namespace multibody {
+
+enum class ToppraDiscretization { kCollocation, kInterpolation };
+
 /**
  * Solves a Time Optimal Path Parameterization based on Reachability Analysis
  * (TOPPRA) to find the fastest traversal of a given path, satisfying the given
@@ -25,6 +29,11 @@ class Toppra {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(Toppra)
 
+  typedef std::tuple<Eigen::VectorXd, Eigen::VectorXd>
+      ToppraBoundingBoxConstraint;
+  typedef std::tuple<Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd>
+      ToppraLinearConstraint;
+
   ~Toppra() {}
 
   /**
@@ -32,10 +41,55 @@ class Toppra {
    * This constructor will create and own a context for @param plant.
    * @param path The trajectory on which the toppra problem will be solved.
    * @param plant The robot that will follow the solved trajectory.  Used for
-   * enforcing torque and frame specific constraints.
+   *              enforcing torque and frame specific constraints.
+   * @param gridpoints The points along the path to discretize the problem and
+   *                   enforce constraints at.
    */
   explicit Toppra(const PiecewisePolynomial<double>& path,
-                  const MultibodyPlant<double>& plant);
+                  const MultibodyPlant<double>& plant,
+                  const Eigen::VectorXd& gridpoints);
+
+  /**
+   * Takes a path and generates a sequence of gridpoints selected to control
+   * the interpolation error of the optimization. The distance between
+   * gridpoints must be below `max_seg_length`, there must be at least
+   * `min_points` number of gridpoints and the interpolation error, estimated
+   * with the equation
+   * <pre>
+   *   err_{est} = 0.5 * \max(|q̈ * Δ_{segment}^2|),
+   * </pre>
+   * must be less than `max_err`.  Any segments that are too long or have too
+   * much error are split in half.  This results in more points in parts of the
+   * path with higher curvature. All grid points will lie between
+   * path.start_time() and path.end_time().
+   */
+  static Eigen::VectorXd CalcGridpts(const PiecewisePolynomial<double>& path,
+                                     double max_err = 1e-3, int max_iter = 100,
+                                     double max_seg_length = 0.05,
+                                     int min_points = 100);
+
+  PiecewisePolynomial<double> Solve();
+
+  ToppraBoundingBoxConstraint& AddJointVelocityLimit(
+      const Eigen::VectorXd& lower_limit, const Eigen::VectorXd upper_limit);
+
+  ToppraLinearConstraint& AddJointAccelerationLimit(
+      const Eigen::VectorXd& lower_limit, const Eigen::VectorXd upper_limit,
+      ToppraDiscretization discretization =
+          ToppraDiscretization::kInterpolation);
+
+  // ToppraLinearConstraint& AddJointTorqueLimit(const Eigen::VectorXd&
+  // lower_limit,
+  //                                            const Eigen::VectorXd
+  //                                            upper_limit);
+
+  // ToppraBoundingBoxConstraint& AddFrameVelocityLimit(
+  //     const Frame<double>& constraint_frame, const Eigen::VectorXd&
+  //     lower_limit, const Eigen::VectorXd upper_limit);
+
+  // ToppraLinearConstraint& AddFrameAccelerationLimit(
+  //     const Frame<double>& constraint_frame, const Eigen::VectorXd&
+  //     lower_limit, const Eigen::VectorXd upper_limit);
 
   /** Getter for the optimization program constructed by Toppra. */
   const solvers::MathematicalProgram& prog() const { return *prog_; }
@@ -44,11 +98,34 @@ class Toppra {
   solvers::MathematicalProgram* get_mutable_prog() const { return prog_.get(); }
 
  private:
+  /**
+   * Performs the backward pass step of TOPPRA, computing the controllable set
+   * at each gridpoint.
+   */
+  Eigen::MatrixXd ComputeBackwardPass(double s_dot_0, double s_dot_N);
+
+  /**
+   * Performs the forward pass step of TOPPRA, computing the greediest
+   * acceleration at each gridpoint that remains within the controllable set.
+   */
+  Eigen::VectorXd ComputeForwardPass(double s_dot_0, Eigen::MatrixXd& K);
+
   std::unique_ptr<solvers::MathematicalProgram> backward_prog_;
-  solvers::VectorXDecisionVariable backward_vars_;
   std::unique_ptr<solvers::MathematicalProgram> forward_prog_;
+  solvers::VectorXDecisionVariable backward_vars_;
   solvers::VectorXDecisionVariable forward_vars_;
+  Bindings<LinearCost> backward_cost_;
+  Bindings<LinearCost> forward_cost_;
+  Bindings<LinearConstraint> backward_continuity_con_;
+  Bindings<LinearConstraint> forward_continuity_con_;
+  std::vector<Binding<BoundingBoxConstraint>> bb_constraint_;
+  std::vector<Binding<LinearConstraint>> backward_lin_constraint_;
+  std::vector<Binding<LinearConstraint>> forward_lin_constraint_;
+  std::vector<ToppraBoundingBoxConstraint> bb_constraint_coeff_;
+  std::vector<ToppraLinearConstraint> lin_constraint_coeff_;
+  const PiecewisePolynomial<double>& path_;
   const MultibodyPlant<double>& plant_;
+  Eigen::VectorXd gridpoints_;
 };
 }  // namespace multibody
 }  // namespace drake
