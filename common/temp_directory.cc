1e8d845f667aa1e5751581473a48ac29ec6766c2
Sean Curtis <sean.curtis@tri.global>
2021-09-01 13:22:34 -0700

    Modify temp_directory so the caller must clean up
    This cleans up the ambiguity about who is responsible for cleaning up
    the calling directory. The caller is now *always* responsible.
    
    TODO:
     - unit tests

--- common/temp_directory.cc
+++ common/temp_directory.cc
@@ -12,23 +12,18 @@ namespace drake {
 std::string temp_directory() {
   filesystem::path path;
 
-  const char* test_tmpdir = std::getenv("TEST_TMPDIR");
+  const char* tmpdir = nullptr;
+  (tmpdir = std::getenv("TEST_TMPDIR")) || (tmpdir = std::getenv("TMPDIR")) ||
+      (tmpdir = "/tmp");
 
-  if (test_tmpdir == nullptr) {
-    const char* tmpdir = nullptr;
-    (tmpdir = std::getenv("TMPDIR")) || (tmpdir = "/tmp");
+  filesystem::path path_template(tmpdir);
+  path_template.append("robotlocomotion_drake_XXXXXX");
 
-    filesystem::path path_template(tmpdir);
-    path_template.append("robotlocomotion_drake_XXXXXX");
+  std::string path_template_str = path_template.string();
+  const char* dtemp = ::mkdtemp(&path_template_str[0]);
+  DRAKE_THROW_UNLESS(dtemp != nullptr);
 
-    std::string path_template_str = path_template.string();
-    const char* dtemp = ::mkdtemp(&path_template_str[0]);
-    DRAKE_THROW_UNLESS(dtemp != nullptr);
-
-    path = dtemp;
-  } else {
-    path = test_tmpdir;
-  }
+  path = dtemp;
 
   DRAKE_THROW_UNLESS(filesystem::is_directory(path));
 
