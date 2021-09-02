9977cca24fa9e1eabc99965da0b40181bc8a1a28
Sean Curtis <sean.curtis@tri.global>
2021-09-02 13:29:47 -0700

    [common] Always allow caller clean up directory from temp_directory() (#15698)
    * Modify temp_directory so the caller can always clean up.
    
    For code that creates a temporary working directory, this cleans up the
    ambiguity of whether or not it is able to delete that directory. Now it
    can *always* clean up (modulo catastrophic system failures).
    
    Updates the tests that confirm the behavior when TEST_TMPDIR is set.

--- common/temp_directory.cc
+++ common/temp_directory.cc
@@ -12,32 +12,24 @@ namespace drake {
 std::string temp_directory() {
   filesystem::path path;
 
-  const char* test_tmpdir = std::getenv("TEST_TMPDIR");
+  const char* tmpdir = nullptr;
+  (tmpdir = std::getenv("TEST_TMPDIR")) || (tmpdir = std::getenv("TMPDIR")) ||
+      (tmpdir = "/tmp");
 
-  if (test_tmpdir == nullptr) {
-    const char* tmpdir = nullptr;
-    (tmpdir = std::getenv("TMPDIR")) || (tmpdir = "/tmp");
+  filesystem::path path_template(tmpdir);
+  path_template.append("robotlocomotion_drake_XXXXXX");
 
-    filesystem::path path_template(tmpdir);
-    path_template.append("robotlocomotion_drake_XXXXXX");
+  std::string path_template_str = path_template.string();
+  const char* dtemp = ::mkdtemp(&path_template_str[0]);
+  DRAKE_THROW_UNLESS(dtemp != nullptr);
 
-    std::string path_template_str = path_template.string();
-    const char* dtemp = ::mkdtemp(&path_template_str[0]);
-    DRAKE_THROW_UNLESS(dtemp != nullptr);
-
-    path = dtemp;
-  } else {
-    path = test_tmpdir;
-  }
+  path = dtemp;
 
   DRAKE_THROW_UNLESS(filesystem::is_directory(path));
+  std::string path_string = path.string();
+  DRAKE_DEMAND(path_string.back() != '/');
 
-  // Strip any trailing /.
-  std::string result = path.string();
-  if (result.back() == '/') {
-    result.pop_back();
-  }
-  return result;
+  return path_string;
 }
 
 }  // namespace drake
