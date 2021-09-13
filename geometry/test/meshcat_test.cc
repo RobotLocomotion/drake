c557101ffa83c1eb7e5b575b836b0298f80f080e
Russ Tedrake <russt@mit.edu>
2021-09-13 13:01:45 -0400

    [geometry:meshcat] Add support for ports and public_urls (#15727)
    * [geometry:meshcat] Add support for ports and public_urls
    
    - Adds an option for taking the port in the Meshcat constructor
    - Moves the throw std::runtime_error from the websocket thread to the main thread so that one can catch failures to open a requested port.

--- geometry/test/meshcat_test.cc
+++ geometry/test/meshcat_test.cc
@@ -9,6 +9,7 @@
 
 #include "drake/common/find_resource.h"
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
+#include "drake/common/test_utilities/expect_throws_message.h"
 #include "drake/geometry/meshcat_types.h"
 
 namespace drake {
@@ -55,6 +56,20 @@ GTEST_TEST(MeshcatTest, ConstructMultiple) {
   EXPECT_THAT(meshcat2.web_url(), HasSubstr("http://localhost:"));
   EXPECT_THAT(meshcat2.ws_url(), HasSubstr("ws://localhost:"));
   EXPECT_NE(meshcat.web_url(), meshcat2.web_url());
+}
+
+GTEST_TEST(MeshcatTest, Ports) {
+  Meshcat meshcat(7050);
+  EXPECT_EQ(meshcat.port(), 7050);
+
+  // Can't open the same port twice.
+  DRAKE_EXPECT_THROWS_MESSAGE(Meshcat m2(7050),
+                              "Meshcat failed to open a websocket port.");
+
+  // The default constructor gets a default port.
+  Meshcat m3;
+  EXPECT_GE(m3.port(), 7000);
+  EXPECT_LE(m3.port(), 7099);
 }
 
 // The correctness of this is established with meshcat_manual_test.  Here we
