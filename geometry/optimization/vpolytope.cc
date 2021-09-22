7ccb4429ba1f82e6fcb569ecff036ff748a723a1
Russ Tedrake <russt@mit.edu>
2021-09-22 08:41:14 -0400

    [geometry:optimization] Add VPolytope from HPolyhedron
    - Brings in qhull as an external.
    - Adds an implementation for generating VPolytope from HPolyhedron using qhull
    
    Co-authored-by: Mark Petersen <markpetersen@g.harvard.edu>
    Co-authored-by: Tobia Marcucci <tobiam@mit.edu>

--- geometry/optimization/vpolytope.cc
+++ geometry/optimization/vpolytope.cc
@@ -3,6 +3,9 @@
 #include <limits>
 #include <memory>
 
+#include "libqhullcpp/Qhull.h"
+#include "libqhullcpp/QhullVertexSet.h"
+
 #include "drake/common/is_approx_equal_abstol.h"
 #include "drake/solvers/solve.h"
 
@@ -38,6 +41,48 @@ VPolytope::VPolytope(const QueryObject<double>& query_object,
   const RigidTransformd& X_WG = query_object.GetPoseInWorld(geometry_id);
   const RigidTransformd X_EG = X_WE.InvertAndCompose(X_WG);
   vertices_ = X_EG * vertices;
+}
+
+VPolytope::VPolytope(const HPolyhedron& hpoly)
+    : ConvexSet(&ConvexSetCloner<VPolytope>, hpoly.ambient_dimension()) {
+  DRAKE_THROW_UNLESS(hpoly.IsBounded());
+
+  Eigen::MatrixXd coeffs(hpoly.A().rows(), hpoly.A().cols() + 1);
+  coeffs.leftCols(hpoly.A().cols()) = hpoly.A();
+  coeffs.col(hpoly.A().cols()) = -hpoly.b();
+
+  Eigen::MatrixXd coeffs_t = coeffs.transpose();
+  std::vector<double> flat_coeffs;
+  flat_coeffs.resize(coeffs_t.size());
+  Eigen::VectorXd::Map(&flat_coeffs[0], coeffs_t.size()) = coeffs_t;
+
+  Eigen::VectorXd eigen_center = hpoly.ChebyshevCenter();
+  std::vector<double> center;
+  center.resize(eigen_center.size());
+  Eigen::VectorXd::Map(&center[0], eigen_center.size()) = eigen_center;
+
+  orgQhull::Qhull qhull;
+  qhull.setFeasiblePoint(orgQhull::Coordinates(center));
+  qhull.runQhull("", hpoly.A().cols() + 1, hpoly.A().rows(), flat_coeffs.data(),
+                 "H");
+
+  vertices_.resize(hpoly.ambient_dimension(), qhull.facetCount());
+  int ii = 0;
+  for (const auto& facet : qhull.facetList()) {
+    auto incident_hyperplanes = facet.vertices();
+    Eigen::MatrixXd vertex_A(incident_hyperplanes.count(),
+                             hpoly.ambient_dimension());
+    for (int jj = 0; jj < incident_hyperplanes.count(); jj++) {
+      std::vector<double> hyperplane =
+          incident_hyperplanes.at(jj).point().toStdVector();
+      vertex_A.row(jj) = Eigen::Map<Eigen::RowVectorXd, Eigen::Unaligned>(
+          hyperplane.data(), hyperplane.size());
+    }
+    vertices_.col(ii) = vertex_A.partialPivLu().solve(Eigen::VectorXd::Ones(
+                            incident_hyperplanes.count())) +
+                        eigen_center;
+    ii++;
+  }
 }
 
 VPolytope::~VPolytope() = default;
