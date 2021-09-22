ce7b311adf4342feabf8c19fd48a3610147db34d
DamrongGuoy <Damrong.Guoy@tri.global>
2021-09-22 12:15:59 -0700

    Remove unused files. Simplify the PR.

--- multibody/plant/hydroelastic_poly_contact_info.cc
+++
@@ -1,4 +0,0 @@
-#include "drake/multibody/plant/hydroelastic_poly_contact_info.h"
-
-DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
-    class ::drake::multibody::HydroelasticPolyContactInfo)
