3ad8bd7e4b1b4262fe12a9885aa1c80ebc23faaa
mpetersen94 <markpetersen@g.harvard.edu>
2021-09-23 12:06:25 -0400

    Add bindings for toppra

--- bindings/pydrake/multibody/optimization_py.cc
+++ bindings/pydrake/multibody/optimization_py.cc
@@ -8,6 +8,7 @@
 #include "drake/multibody/optimization/centroidal_momentum_constraint.h"
 #include "drake/multibody/optimization/quaternion_integration_constraint.h"
 #include "drake/multibody/optimization/static_equilibrium_problem.h"
+#include "drake/multibody/optimization/toppra.h"
 
 namespace drake {
 namespace pydrake {
@@ -23,6 +24,19 @@ PYBIND11_MODULE(optimization, m) {
   py::module::import("pydrake.math");
   py::module::import("pydrake.multibody.plant");
   py::module::import("pydrake.solvers.mathematicalprogram");
+
+  {
+    using Class = CalcGridPointsOptions;
+    constexpr auto& cls_doc = doc.CalcGridPointsOptions;
+    py::class_<Class>(m, "CalcGridPointsOptions", cls_doc.doc)
+        .def(ParamInit<Class>())
+        .def_readwrite("max_err", &Class::max_err, cls_doc.max_err.doc)
+        .def_readwrite("max_iter", &Class::max_iter, cls_doc.max_iter.doc)
+        .def_readwrite("max_seg_length", &Class::max_seg_length,
+            cls_doc.max_seg_length.doc)
+        .def_readwrite(
+            "min_points", &Class::min_points, cls_doc.min_points.doc);
+  }
 
   {
     using Class = CentroidalMomentumConstraint;
@@ -60,6 +74,16 @@ PYBIND11_MODULE(optimization, m) {
   }
 
   {
+    using Class = QuaternionEulerIntegrationConstraint;
+    constexpr auto& cls_doc = doc.QuaternionEulerIntegrationConstraint;
+    using Ptr = std::shared_ptr<Class>;
+    py::class_<Class, solvers::Constraint, Ptr>(
+        m, "QuaternionEulerIntegrationConstraint", cls_doc.doc)
+        .def(py::init<bool>(), py::arg("allow_quaternion_negation"),
+            cls_doc.ctor.doc);
+  }
+
+  {
     using Class = StaticEquilibriumProblem;
     constexpr auto& cls_doc = doc.StaticEquilibriumProblem;
     py::class_<Class>(m, "StaticEquilibriumProblem", cls_doc.doc)
@@ -84,15 +108,49 @@ PYBIND11_MODULE(optimization, m) {
   }
 
   {
-    using Class = QuaternionEulerIntegrationConstraint;
-    constexpr auto& cls_doc = doc.QuaternionEulerIntegrationConstraint;
-    using Ptr = std::shared_ptr<Class>;
-    py::class_<Class, solvers::Constraint, Ptr>(
-        m, "QuaternionEulerIntegrationConstraint", cls_doc.doc)
-        .def(py::init([](bool allow_quaternion_negation) {
-          return std::make_unique<Class>(allow_quaternion_negation);
-        }),
-            py::arg("allow_quaternion_negation"), cls_doc.ctor.doc);
+    using Class = ToppraDiscretization;
+    constexpr auto& cls_doc = doc.ToppraDiscretization;
+    py::enum_<Class>(m, "ToppraDiscretization", cls_doc.doc)
+        .value("kCollocation", Class::kCollocation, cls_doc.kCollocation.doc)
+        .value("kInterpolation", Class::kInterpolation,
+            cls_doc.kInterpolation.doc);
+  }
+
+  {
+    using Class = Toppra;
+    constexpr auto& cls_doc = doc.Toppra;
+    py::class_<Class>(m, "Toppra", cls_doc.doc)
+        .def(py::init<const Trajectory<double>&, const MultibodyPlant<double>&,
+                 const Eigen::Ref<const Eigen::VectorXd>&>(),
+            py::arg("path"), py::arg("plant"), py::arg("gridpoints"),
+            cls_doc.ctor.doc)
+        .def_static("CalcGridPoints", &Class::CalcGridPoints, py::arg("path"),
+            py::arg("options"), cls_doc.CalcGridPoints.doc)
+        .def("SolvePathParameterization", &Class::SolvePathParameterization,
+            cls_doc.SolvePathParameterization.doc)
+        .def("AddJointVelocityLimit", &Class::AddJointVelocityLimit,
+            py::arg("lower_limit"), py::arg("upper_limit"),
+            cls_doc.AddJointVelocityLimit.doc)
+        .def("AddJointAccelerationLimit", &Class::AddJointAccelerationLimit,
+            py::arg("lower_limit"), py::arg("upper_limit"),
+            py::arg("discretization") = ToppraDiscretization::kInterpolation,
+            cls_doc.AddJointAccelerationLimit.doc)
+        .def("AddJointTorqueLimit", &Class::AddJointTorqueLimit,
+            py::arg("lower_limit"), py::arg("upper_limit"),
+            py::arg("discretization") = ToppraDiscretization::kInterpolation,
+            cls_doc.AddJointTorqueLimit.doc)
+        .def("AddFrameVelocityLimit", &Class::AddFrameVelocityLimit,
+            py::arg("constraint_frame"), py::arg("lower_limit"),
+            py::arg("upper_limit"), cls_doc.AddFrameVelocityLimit.doc)
+        .def("AddFrameTranslationalSpeedLimit",
+            &Class::AddFrameTranslationalSpeedLimit,
+            py::arg("constraint_frame"), py::arg("upper_limit"),
+            cls_doc.AddFrameTranslationalSpeedLimit.doc)
+        .def("AddFrameAccelerationLimit", &Class::AddFrameAccelerationLimit,
+            py::arg("constraint_frame"), py::arg("lower_limit"),
+            py::arg("upper_limit"),
+            py::arg("discretization") = ToppraDiscretization::kInterpolation,
+            cls_doc.AddFrameAccelerationLimit.doc);
   }
 }
 }  // namespace
