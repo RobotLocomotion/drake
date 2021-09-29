861e86a156f641ec6393de9f152541c9e34eb5e5
Sean Curtis <sean.curtis@tri.global>
2021-09-29 08:45:43 -0700

    Add unit test that illustrates Identifier ambiguity
    In this case, I'm accessing FrameIds (but I'm confident it is reflected
    in all bound identifiers). Depending on what code is accessing it, it
    may get a different static backing of the symbol.
    
    To see the problem:
    
        bazel test bindings/pydrake:py/sg_fid_test
    
    This will do two things:
    
      1. Spew a mountain of messages announcing invocation of get_new_id() on
         various identifier types.
      2. Report failure in (most likely) test one with an error that says:
         AssertionError: 1 == 1
    
    The cause of this appears that
      1. SceneGraph instances are getting access to a different FrameId than
         GeometryFrame.
           - Note: SceneGraph is actually invoking an unbound static method:
             InternalFrame::world_frame_id(). Bound GeometryFrame is directly
             calling FrameId::get_new_id().
           - Parsing through the spew, it is clear that GeometryFrame is
             accessing the same instance that you'd get by calling
             pydrake.geometry.FrameId.get_new_id().

---
+++ bindings/pydrake/test/sg_fid_test.py
@@ -0,0 +1,51 @@
+import pydrake.geometry as mut
+
+import unittest
+
+
+class TestFoo(unittest.TestCase):
+    def test_one(self):
+        """
+        Every acquired FrameId must be unique. If this test executes first, it
+        will fail. scene_graph's world_frame_id() will match the id of the
+        newly added geometry frame. If, however, test_two executes first, this
+        test will pass.
+
+        This is because both SceneGraph and GeometryFrame call
+        FrameId::get_new_id(), but they are somehow accessing *different*
+        FrameId classes -- they are drawing from different static sources.
+        """
+        scene_graph = mut.SceneGraph()
+        f = mut.GeometryFrame("frame")
+        f_id = mut.FrameId.get_new_id()
+        self.assertNotEqual(scene_graph.world_frame_id().get_value(),
+                            f.id().get_value())
+        self.assertNotEqual(scene_graph.world_frame_id().get_value(), f_id)
+        self.assertNotEqual(f.id().get_value(), f_id)
+
+
+    def test_two(self):
+        """
+        Same exact test as test_one, fails if executed first, passes if
+        executed second.
+        """
+        scene_graph = mut.SceneGraph()
+        f = mut.GeometryFrame("frame")
+        f_id = mut.FrameId.get_new_id()
+        self.assertNotEqual(scene_graph.world_frame_id().get_value(),
+                            f.id().get_value())
+        self.assertNotEqual(scene_graph.world_frame_id().get_value(), f_id)
+        self.assertNotEqual(f.id().get_value(), f_id)
+
+    def test_three(self):
+        """
+        This test also fails under the conditions and the reasons outlined
+        above.
+        """
+        scene_graph = mut.SceneGraph_[float]()
+        f = mut.GeometryFrame("frame")
+        f_id = mut.FrameId.get_new_id()
+        self.assertNotEqual(scene_graph.world_frame_id().get_value(),
+                            f.id().get_value())
+        self.assertNotEqual(scene_graph.world_frame_id().get_value(), f_id)
+        self.assertNotEqual(f.id().get_value(), f_id)
