ad563206888b8d63bc595a1e58cbe5f0a107c282
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-09-29 16:27:54 -0700

    [multibody] Promote MultibodyPlantConfig out of dev (#15826)
    Refactor MultibodyPlant to obtain its default constructor values from
    the Config struct in most cases.

--- multibody/plant/test/multibody_plant_config_functions_test.cc
+++ multibody/plant/test/multibody_plant_config_functions_test.cc
@@ -1,13 +1,16 @@
-#include "drake/multibody/plant/dev/multibody_plant_config_functions.h"
+#include "drake/multibody/plant/multibody_plant_config_functions.h"
 
 #include <gtest/gtest.h>
 
 #include "drake/common/test_utilities/expect_throws_message.h"
+#include "drake/common/yaml/yaml_read_archive.h"
 
 namespace drake {
 namespace multibody {
 namespace internal {
 namespace {
+
+using yaml::YamlReadArchive;
 
 GTEST_TEST(MultibodyPlantConfigFunctionsTest, BasicTest) {
   MultibodyPlantConfig config;
@@ -15,6 +18,26 @@ GTEST_TEST(MultibodyPlantConfigFunctionsTest, BasicTest) {
   config.penetration_allowance = 0.003;
   config.stiction_tolerance = 0.004;
   config.contact_model = "hydroelastic";
+
+  drake::systems::DiagramBuilder<double> builder;
+  auto result = AddMultibodyPlant(config, &builder);
+  EXPECT_EQ(result.plant.time_step(), 0.002);
+  EXPECT_EQ(result.plant.get_contact_model(),
+            ContactModel::kHydroelasticsOnly);
+  // There is no getter for penetration_allowance nor stiction_tolerance, so we
+  // can't test them.
+}
+
+const char* const kExampleConfig = R"""(
+time_step: 0.002
+penetration_allowance: 0.003
+stiction_tolerance: 0.004
+contact_model: hydroelastic
+)""";
+
+GTEST_TEST(MultibodyPlantConfigFunctionsTest, YamlTest) {
+  MultibodyPlantConfig config;
+  YamlReadArchive(YAML::Load(kExampleConfig)).Accept(&config);
 
   drake::systems::DiagramBuilder<double> builder;
   auto result = AddMultibodyPlant(config, &builder);
@@ -35,7 +58,7 @@ GTEST_TEST(MultibodyPlantConfigFunctionsTest, ContactModelTest) {
 
   for (const auto& [name, value] : known_values) {
     EXPECT_EQ(GetContactModelFromString(name), value);
-    EXPECT_EQ(ContactModelToString(value), name);
+    EXPECT_EQ(GetStringFromContactModel(value), name);
   }
 
   DRAKE_EXPECT_THROWS_MESSAGE(GetContactModelFromString("foobar"),
