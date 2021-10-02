35c7ead6bcf32db6c77c154751f47ba85639a2b5
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-10-02 09:16:00 -0700

    [workspace] Add new_release tooling for automatic upgrades

--- tools/workspace/metadata.py
+++ tools/workspace/metadata.py
@@ -13,8 +13,10 @@ def _check_output(args):
     return subprocess.check_output(args).decode("utf8")
 
 
-def read_repository_metadata():
-    """Returns a dict."""
+def read_repository_metadata(repositories=None):
+    """If no repositories are given, returns data for all repositories.
+    Returns a dict of {repository_name => details_dict}.
+    """
     result = {}
 
     # Ask where the repository rules write their output.
@@ -22,19 +24,20 @@ def read_repository_metadata():
         ["bazel", "info", "output_base"]).strip()
     assert os.path.isdir(output_base), output_base
 
-    # Obtain a list of known repositories.
-    package_lines = _check_output(
-        ["bazel", "query", "deps(//...)", "--output", "package"])
-    repositories = set()
-    for line in package_lines.split("\n"):
-        if not line.startswith("@"):
-            continue
-        name = line[1:].split("/")[0]
-        repositories.add(name)
+    if not repositories:
+        # Obtain a list of known repositories.
+        package_lines = _check_output(
+            ["bazel", "query", "deps(//...)", "--output", "package"])
+        repositories = set()
+        for line in package_lines.split("\n"):
+            if not line.startswith("@"):
+                continue
+            name = line[1:].split("/")[0]
+            repositories.add(name)
 
-    # These are starlark deps, so don't show up in the query.
-    repositories.add("bazel_skylib")
-    repositories.add("rules_pkg")
+        # These are starlark deps, so don't show up in the query.
+        repositories.add("bazel_skylib")
+        repositories.add("rules_pkg")
 
     # Make sure all of the repository_rule results are up-to-date.
     subprocess.check_call(["bazel", "fetch", "//..."])
