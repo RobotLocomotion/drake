218b0b0b8df0c12863276fdfda09e9dd3f38aee2
Russ Tedrake <russt@mit.edu>
2021-10-02 07:06:02 -0400

    [geometry:meshcat] Adds SetAnimation
    Provides a MeshcatAnimation class and Meshcat::SetAnimation.
    
    Includes an upgrade to my test script which prints more narrow diffs;
    this proved invaluable for debugging this complicated message type.
    
    Resolves #15800.

--- geometry/test/meshcat_websocket_client.py
+++ geometry/test/meshcat_websocket_client.py
@@ -8,6 +8,32 @@ import umsgpack
 import websockets
 
 
+def recursive_compare(d1, d2, level='root'):
+    if isinstance(d1, dict) and isinstance(d2, dict):
+        if d1.keys() != d2.keys():
+            s1 = set(d1.keys())
+            s2 = set(d2.keys())
+            print('{:<20} + {} - {}'.format(level, s1-s2, s2-s1))
+            common_keys = s1 & s2
+        else:
+            common_keys = set(d1.keys())
+
+        for k in common_keys:
+            recursive_compare(d1[k], d2[k], level='{}.{}'.format(level, k))
+
+    elif isinstance(d1, list) and isinstance(d2, list):
+        if len(d1) != len(d2):
+            print('{:<20} len1={}; len2={}'.format(level, len(d1), len(d2)))
+        common_len = min(len(d1), len(d2))
+
+        for i in range(common_len):
+            recursive_compare(d1[i], d2[i], level='{}[{}]'.format(level, i))
+
+    else:
+        if d1 != d2:
+            print('{:<20} {} != {}'.format(level, d1, d2))
+
+
 async def check_command(ws_url, message_number, desired_command):
     async with websockets.connect(ws_url) as websocket:
         message = ''
@@ -18,6 +44,7 @@ async def check_command(ws_url, message_number, desired_command):
             print("FAILED")
             print(f"Expected: {desired_command}")
             print(f"Received: {command}")
+            recursive_compare(desired_command, command)
             sys.exit(1)
 
 
