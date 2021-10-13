f94ee1ddcbdbe85be50e3b924e9a2ccdc81b10b5
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-10-13 10:25:55 -0700

    [common] DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN detects mis-uses
    Add unit test advice to complement the new built-in checking.
    
    This commit also contains a demonstration of related changes:
    * Add (positive) unit test for this macro.
    * Fix name_value to correctly document its (pre-existing) non-copyable behavior.
    * Remove useless unit test for a defaulted copy constructor.

---
+++ common/test/drake_copyable_test.cc
@@ -0,0 +1,34 @@
+#include "drake/common/drake_copyable.h"
+
+#include <gtest/gtest.h>
+
+namespace drake {
+namespace {
+
+// When a class has a user-defined destructor, the implicit generation of the
+// copy constructor and copy-assignment operator is deprecated as of C++11.
+//
+// Under Drake's Clang configuration, relying on the deprecated operator
+// produces an error under -Werror=deprecated.
+//
+// Therefore, if DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN failed to default those
+// operations, then this unit test would no longer compile (under Clang).
+class Example {
+ public:
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(Example);
+  Example() = default;
+  ~Example() {}
+};
+
+GTEST_TEST(DrakeCopyableTest, CopyConstruct) {
+  Example foo;
+  Example bar(foo);
+}
+
+GTEST_TEST(DrakeCopyableTest, CopyAssign) {
+  Example foo, bar;
+  bar = foo;
+}
+
+}  // namespace
+}  // namespace drake
