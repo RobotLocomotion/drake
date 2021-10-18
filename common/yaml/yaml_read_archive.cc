2170da8a106890ad7aa0a2f5f04fea8cfac21b67
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-10-18 10:10:16 -0700

    [yaml] Bury most yaml-cpp use into cc file for yaml_read_archive
    The header-file uses of yamp-cpp that will require deprecation annotations
    remain intact for now.
    
    With the changes here, we lose a little specificity in error messages when
    using YAML merge keys, but that's an acceptable loss.

--- common/yaml/yaml_read_archive.cc
+++ common/yaml/yaml_read_archive.cc
@@ -9,22 +9,6 @@
 
 namespace drake {
 namespace yaml {
-
-YamlReadArchive::YamlReadArchive(const YAML::Node& root)
-    : YamlReadArchive(root, Options{}) {}
-
-YamlReadArchive::YamlReadArchive(const YAML::Node& root, const Options& options)
-    : owned_root_(root),
-      root_(&owned_root_),
-      mapish_item_key_(nullptr),
-      mapish_item_value_(nullptr),
-      options_(options),
-      parent_(nullptr) {
-  // Reprocess the owned_root for merge keys only after all member fields are
-  // initialized; otherwise, the method might access invalid member data.
-  RewriteMergeKeys(const_cast<YAML::Node*>(&owned_root_));
-}
-
 namespace {
 
 // The source and destination are both of type Map.  Copy the key-value pairs
@@ -44,9 +28,20 @@ void CopyWithMergeKeySemantics(
   }
 }
 
-}  // namespace
-
-void YamlReadArchive::RewriteMergeKeys(YAML::Node* node) const {
+// If the given `node` (of type Map) has a YAML merge key defined, then mutates
+// the `node` in place to replace the merge key entry with the merged values.
+//
+// See https://yaml.org/type/merge.html for details on syntax and semantics.
+//
+// If the YAML merge key syntax is violated, then this function throws an
+// exception.  In the future, it might be nice to use the ReportError helper
+// instead of throwing, but that is currently too awkward.
+//
+// The `parent` is only used to provide context during error reporting.
+//
+// If yaml-cpp adds native support for merge keys during its own parsing, then
+// we should be able to remove this helper.
+void RewriteMergeKeys(const YAML::Node& parent, YAML::Node* node) {
   DRAKE_DEMAND(node != nullptr);
   DRAKE_DEMAND(node->Type() == YAML::NodeType::Map);
   const YAML::Node& merge_key = (*node)["<<"];
@@ -54,6 +49,8 @@ void YamlReadArchive::RewriteMergeKeys(YAML::Node* node) const {
     return;
   }
   (*node).remove("<<");
+  const char* error_message = nullptr;
+  YAML::Node error_locus;
   switch (merge_key.Type()) {
     case YAML::NodeType::Map: {
       // Merge `merge_key` Map into `node` Map.
@@ -64,68 +61,179 @@ void YamlReadArchive::RewriteMergeKeys(YAML::Node* node) const {
       // Merge each Map in `merge_key` Sequence-of-Maps into the `node` Map.
       for (const YAML::Node& merge_key_item : merge_key) {
         if (merge_key_item.Type() != YAML::NodeType::Map) {
-          ReportError(
-              "has invalid merge key type (Sequence-of-non-Mapping) within");
+          error_message =
+              "has invalid merge key type (Sequence-of-non-Mapping).";
+          error_locus = *node;
+          break;
         }
         CopyWithMergeKeySemantics(merge_key_item, node);
+      }
+      if (error_message != nullptr) {
+        break;
       }
       return;
     }
     case YAML::NodeType::Scalar: {
-      ReportError("has invalid merge key type (Scalar) within");
-      return;
+      error_message = "has invalid merge key type (Scalar).";
+      error_locus = parent;
+      break;
     }
     case YAML::NodeType::Null: {
-      ReportError("has invalid merge key type (Null) within");
-      return;
+      error_message = "has invalid merge key type (Null).";
+      error_locus = parent;
+      break;
     }
     case YAML::NodeType::Undefined: {
       // We should never reach here due to the "if (!merge_key)" guard above.
       DRAKE_UNREACHABLE();
     }
   }
+  DRAKE_DEMAND(error_message != nullptr);
+  DRAKE_DEMAND(error_locus.IsDefined());
+  // Sort the keys so that our error message is deterministic.
+  std::vector<std::string> keys;
+  for (const auto& map_pair : error_locus) {
+    keys.push_back(map_pair.first.as<std::string>());
+  }
+  std::sort(keys.begin(), keys.end());
+  throw std::runtime_error(fmt::format(
+      "YAML node of type Mapping (with size {} and keys {{{}}}) {}",
+      keys.size(), fmt::join(keys, ", "), error_message));
+}
+
+// Convert a jbeder/yaml-cpp YAML::Node `node` to a Drake yaml::internal::Node.
+// See https://github.com/jbeder/yaml-cpp/wiki/Tutorial for a jbeder reference.
+// The `parent` is only used to provide context during error reporting.
+internal::Node ConvertJbederYamlNodeToDrakeYamlNode(
+    const YAML::Node& parent, const YAML::Node& node) {
+  switch (node.Type()) {
+    case YAML::NodeType::Undefined: {
+      throw std::runtime_error("A yaml-cpp node was unexpectedly Undefined");
+    }
+    case YAML::NodeType::Null: {
+      return internal::Node::MakeNull();
+    }
+    case YAML::NodeType::Scalar: {
+      auto result = internal::Node::MakeScalar(node.Scalar());
+      result.SetTag(node.Tag());
+      return result;
+    }
+    case YAML::NodeType::Sequence: {
+      auto result = internal::Node::MakeSequence();
+      result.SetTag(node.Tag());
+      for (size_t i = 0; i < node.size(); ++i) {
+        result.Add(ConvertJbederYamlNodeToDrakeYamlNode(node, node[i]));
+      }
+      return result;
+    }
+    case YAML::NodeType::Map: {
+      YAML::Node merged_node(node);
+      RewriteMergeKeys(parent, &merged_node);
+      auto result = internal::Node::MakeMapping();
+      result.SetTag(merged_node.Tag());
+      for (const auto& key_value : merged_node) {
+        const YAML::Node& key = key_value.first;
+        const YAML::Node& value = key_value.second;
+        result.Add(key.Scalar(), ConvertJbederYamlNodeToDrakeYamlNode(
+            node, value));
+      }
+      return result;
+    }
+  }
   DRAKE_UNREACHABLE();
 }
 
-bool YamlReadArchive::has_root() const {
-  if (mapish_item_key_ != nullptr) {
-    return true;
-  }
-  DRAKE_DEMAND(root_ != nullptr);
-  return !(root_->IsNull());
+}  // namespace
+
+YamlReadArchive::YamlReadArchive(const YAML::Node& root)
+    : YamlReadArchive(root, Options{}) {}
+
+YamlReadArchive::YamlReadArchive(const YAML::Node& root, const Options& options)
+    : owned_root_(ConvertJbederYamlNodeToDrakeYamlNode({}, root)),
+      root_(&owned_root_.value()),
+      mapish_item_key_(nullptr),
+      mapish_item_value_(nullptr),
+      options_(options),
+      parent_(nullptr) {}
+
+void YamlReadArchive::ParseScalar(const std::string& value, bool* result) {
+  ParseScalar<bool>(value, result);
 }
 
-YAML::Node YamlReadArchive::MaybeGetSubNode(const char* name) const {
+void YamlReadArchive::ParseScalar(const std::string& value, double* result) {
+  ParseScalar<double>(value, result);
+}
+
+void YamlReadArchive::ParseScalar(const std::string& value, int* result) {
+  ParseScalar<int>(value, result);
+}
+
+void YamlReadArchive::ParseScalar(
+    const std::string& value, std::string* result) {
+  DRAKE_DEMAND(result != nullptr);
+  *result = value;
+}
+
+const internal::Node* YamlReadArchive::MaybeGetSubNode(const char* name) const {
   DRAKE_DEMAND(name != nullptr);
   if (mapish_item_key_ != nullptr) {
     DRAKE_DEMAND(mapish_item_value_ != nullptr);
     if (std::strcmp(mapish_item_key_, name) == 0) {
-      return *mapish_item_value_;
+      return mapish_item_value_;
     }
-    return YAML::Node(YAML::NodeType::Undefined);
+    return nullptr;
   }
   DRAKE_DEMAND(root_ != nullptr);
-  return (*root_)[name];
+  DRAKE_DEMAND(root_->IsMapping());
+  const auto& map = root_->GetMapping();
+  auto iter = map.find(name);
+  if (iter == map.end()) {
+    return nullptr;
+  }
+  return &(iter->second);
 }
 
-YAML::Node YamlReadArchive::GetSubNode(
-    const char* name, YAML::NodeType::value expected_type) const {
-  YAML::Node result = MaybeGetSubNode(name);
-  if (!result) {
+const internal::Node* YamlReadArchive::GetSubNodeScalar(
+    const char* name) const {
+  const internal::Node* result = GetSubNodeAny(
+      name, internal::NodeType::kScalar);
+  if ((result != nullptr) && (result->GetTag() == internal::Node::kTagNull)) {
+    ReportError("has non-Scalar (Null)");
+    result = nullptr;
+  }
+  return result;
+}
+
+const internal::Node* YamlReadArchive::GetSubNodeSequence(
+    const char* name) const {
+  return GetSubNodeAny(name, internal::NodeType::kSequence);
+}
+
+const internal::Node* YamlReadArchive::GetSubNodeMapping(
+    const char* name) const {
+  return GetSubNodeAny(name, internal::NodeType::kMapping);
+}
+
+const internal::Node* YamlReadArchive::GetSubNodeAny(
+    const char* name, internal::NodeType expected_type) const {
+  const internal::Node* result = MaybeGetSubNode(name);
+  if (result == nullptr) {
     if (!options_.allow_cpp_with_no_yaml) {
       ReportError("is missing");
     }
     return result;
   }
-  const auto& actual_type = result.Type();
+  internal::NodeType actual_type = result->GetType();
   if (actual_type != expected_type) {
+    std::string_view expected_type_string =
+        internal::Node::GetTypeString(expected_type);
+    std::string_view actual_type_string = result->GetTypeString();
+    if (result->GetTag() == internal::Node::kTagNull) {
+      actual_type_string = "Null";
+    }
     ReportError(fmt::format(
-        "has non-{} ({})", to_string(expected_type), to_string(actual_type)));
-    result = YAML::Node(YAML::NodeType::Undefined);
-    return result;
-  }
-  if (expected_type == YAML::NodeType::Map) {
-    RewriteMergeKeys(&result);
+        "has non-{} ({})", expected_type_string, actual_type_string));
+    result = nullptr;
   }
   return result;
 }
@@ -135,15 +243,15 @@ void YamlReadArchive::CheckAllAccepted() const {
   // have a real Mapping node).  Calling it with a map-ish key (i.e., while
   // parsing a sequence) would mean that YamlReadArchive went off the rails.
   DRAKE_DEMAND(mapish_item_key_ == nullptr);
-  DRAKE_DEMAND(root_->Type() == YAML::NodeType::Map);
+  DRAKE_DEMAND(root_->IsMapping());
   if (options_.allow_yaml_with_no_cpp) {
     return;
   }
-  for (const auto& map_pair : *root_) {
-    const std::string& name = map_pair.first.as<std::string>();
-    if (visited_names_.count(name) == 0) {
+  for (const auto& [key, value] : root_->GetMapping()) {
+    unused(value);
+    if (visited_names_.count(key) == 0) {
       ReportError(fmt::format(
-          "key {} did not match any visited value", name));
+          "key {} did not match any visited value", key));
     }
   }
 }
@@ -166,45 +274,29 @@ void YamlReadArchive::ReportError(const std::string& note) const {
 }
 
 void YamlReadArchive::PrintNodeSummary(std::ostream& s) const {
-  YAML::Node to_print;
   if (mapish_item_key_ != nullptr) {
-    DRAKE_DEMAND(mapish_item_value_ != nullptr);
-    to_print[mapish_item_key_] = *mapish_item_value_;
-  } else {
-    DRAKE_DEMAND(root_ != nullptr);
-    to_print = *root_;
+    fmt::print(s, " (with size 1 and keys {{{}}})", mapish_item_key_);
+    return;
   }
-  fmt::print(s, "YAML node of type {}", to_string(to_print.Type()));
-  switch (to_print.Type()) {
-    case YAML::NodeType::Undefined:
-    case YAML::NodeType::Null:
-    case YAML::NodeType::Scalar:
-    case YAML::NodeType::Sequence: {
-      // Don't log any details of these.
-      break;
-    }
-    case YAML::NodeType::Map: {
-      const size_t size = to_print.size();
-      fmt::print(s, " (with size {} and keys {{", size);
-      // Sort the keys so that our error message is deterministic.
-      std::vector<std::string> keys;
-      keys.reserve(size);
-      for (const auto& map_pair : to_print) {
-        keys.emplace_back(map_pair.first.as<std::string>());
-      }
-      std::sort(keys.begin(), keys.end());
-      DRAKE_DEMAND(keys.size() == size);
-      for (size_t i = 0; i < size; ++i) {
-        if (i == 0) {
-          fmt::print(s, "{}", keys[i]);
-        } else {
-          fmt::print(s, ", {}", keys[i]);
-        }
-      }
-      fmt::print(s, "}})");
-      break;
-    }
+
+  DRAKE_DEMAND(root_ != nullptr);
+  fmt::print(s, "YAML node of type {}", root_->GetTypeString());
+  if (!root_->IsMapping()) {
+    // Don't log any additional details for non-Mappings.
+    return;
   }
+
+  // Grab the mapping's keys.  (It's a std::map, so the ordering here is
+  // fully deterministic.)
+  std::vector<std::string_view> keys;
+  for (const auto& [key, value] : root_->GetMapping()) {
+    unused(value);
+    keys.push_back(key);
+  }
+
+  // Output the details of the keys.
+  fmt::print(s, " (with size {} and keys {{{}}})",
+             keys.size(), fmt::join(keys, ", "));
 }
 
 void YamlReadArchive::PrintVisitNameType(std::ostream& s) const {
@@ -217,17 +309,6 @@ void YamlReadArchive::PrintVisitNameType(std::ostream& s) const {
   fmt::print(s, "{} {}",
              drake::NiceTypeName::Get(*debug_visit_type_),
              debug_visit_name_);
-}
-
-const char* YamlReadArchive::to_string(YAML::NodeType::value x) {
-  switch (x) {
-    case YAML::NodeType::Undefined: return "Undefined";
-    case YAML::NodeType::Null: return "Null";
-    case YAML::NodeType::Scalar: return "Scalar";
-    case YAML::NodeType::Sequence: return "Sequence";
-    case YAML::NodeType::Map: return "Mapping";
-  }
-  return "UNKNOWN";
 }
 
 std::ostream& operator<<(std::ostream& os, const YamlReadArchive::Options& x) {
