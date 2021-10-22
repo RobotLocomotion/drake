325e1e64fad5a030ce17083b80bf8859c1ad1a63
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-10-22 10:05:04 -0700

    [solvers] Log the chosen solver

--- solvers/solve.cc
+++ solvers/solve.cc
@@ -3,6 +3,7 @@
 #include <memory>
 
 #include "drake/common/nice_type_name.h"
+#include "drake/common/text_logging.h"
 #include "drake/solvers/choose_best_solver.h"
 #include "drake/solvers/solver_interface.h"
 
@@ -13,6 +14,7 @@ MathematicalProgramResult Solve(
     const std::optional<Eigen::VectorXd>& initial_guess,
     const std::optional<SolverOptions>& solver_options) {
   const SolverId solver_id = ChooseBestSolver(prog);
+  drake::log()->debug("solvers::Solve will use {}", solver_id);
   std::unique_ptr<SolverInterface> solver = MakeSolver(solver_id);
   MathematicalProgramResult result{};
   solver->Solve(prog, initial_guess, solver_options, &result);
