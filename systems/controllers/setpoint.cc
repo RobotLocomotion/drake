f5c8f96117c25d9f77d2a56f394bc044dc244c5f
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-11-01 10:26:05 -0700

    [controllers] Deprecate CartesianSetpoint and VectorSetpoint (#16006)

--- systems/controllers/setpoint.cc
+++ systems/controllers/setpoint.cc
@@ -4,8 +4,11 @@ namespace drake {
 namespace systems {
 namespace controllers {
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 template class CartesianSetpoint<double>;
 template class VectorSetpoint<double>;
+#pragma GCC diagnostic pop
 
 }  // namespace controllers
 }  // namespace systems
