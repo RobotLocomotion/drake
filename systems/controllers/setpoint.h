839b743abf896bcd8c780432f588330f17b3132d
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-11-01 10:44:01 -0700

    Hotfix for deprecation build errors

--- systems/controllers/setpoint.h
+++ systems/controllers/setpoint.h
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "drake/common/drake_copyable.h"
+#include "drake/common/drake_deprecated.h"
 #include "drake/common/eigen_types.h"
 #include "drake/math/rigid_transform.h"
 #include "drake/math/rotation_matrix.h"
