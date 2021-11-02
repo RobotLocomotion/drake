2dbac5c5edfd8884ab58a39c5591cfe72f57df65
Matthew Woehlke <matthew.woehlke@kitware.com>
2021-11-02 14:54:25 -0400

    Don't use shebangs to set shell options

--- tools/wheel/dev/test/test-wheel.sh
+++ tools/wheel/dev/test/test-wheel.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 # This shell script tests a Drake wheel. It is intended to be run inside either
 # a "pristine" Ubuntu container, or one which has been provisioned by the
@@ -6,6 +6,8 @@
 # container, and the container's path to the wheel should be given as an
 # argument to the script. If no path is specified, "drake" from PyPI will be
 # tested.
+
+set -e
 
 if [ ! -d /opt/python ]; then
     test_root="$(readlink -f "$(dirname "${BASH_SOURCE}")")"
