42b8c19f18ebffc8ae46cb8bacb0eee80bd31be8
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-11-02 20:39:44 -0700

    [yaml] Add sugar to read serialized files

--- common/yaml/yaml_doxygen.h
+++ common/yaml/yaml_doxygen.h
@@ -4,6 +4,63 @@
 TODO(jwnimmer-tri) This section is still a work in progress, and needs
 better overview text and organization.
 
+<h2>Reading YAML files</h2>
+
+Use LoadYamlFile() or LoadYamlString() to de-serialize YAML-formatted string
+data into C++ structure.
+
+Sample data:
+@code{yaml}
+foo: 1.0
+bar: [2.0, 3.0]
+@endcode
+
+Sample code:
+@code{cpp}
+struct MyData {
+  double foo{NAN};
+  std::vector<double> bar;
+
+  template <typename Archive>
+  void Serialize(Archive* a) {
+    a->Visit(DRAKE_NVP(foo));
+    a->Visit(DRAKE_NVP(bar));
+  }
+};
+
+MyData LoadData(const std::string& filename) {
+  return LoadYamlFile<MyData>(filename);
+}
+@endcode
+
+There is also an option to load from a top-level child in the document:
+
+@code{yaml}
+data_1:
+  foo: 1.0
+  bar: [2.0, 3.0]
+data_2:
+  foo: 4.0
+  bar: [6.0, 6.0]
+@endcode
+
+@code{cpp}
+MyData LoadData(const std::string& filename) {
+  return LoadYamlFile<MyData>(filename, "data_1");
+}
+@endcode
+
+Structures can be arbitrarily nested, as long as each `struct` has a
+`Serialize` method.  Many common built-in types (int, double, std::string,
+std::vector, std::array, std::map, std::unordered_map, std::optional,
+std::variant, Eigen::Matrix) may also be used.
+
+YAML's "merge keys" (https://yaml.org/type/merge.html) are supported.
+TODO(jwnimmer-tri) Give an example of merge keys.
+
+For inspiration and background, see:
+https://www.boost.org/doc/libs/release/libs/serialization/doc/tutorial.html
+
 <h2>Writing YAML files</h2>
 
 Use SaveYamlFile() or SaveYamlString() to output a YAML-formatted serialization
