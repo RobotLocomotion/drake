80695f4990d54c80cab0d43f552dec45ee01fdd0
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-11-04 09:49:03 -0700

    [pydrake] Add symbolic.Expression.Unapply

--- bindings/pydrake/test/symbolic_test.py
+++ bindings/pydrake/test/symbolic_test.py
@@ -1447,3 +1447,113 @@ class TestDecomposeLumpedParameters(unittest.TestCase):
                                     [sym.Expression(0), x*x]])
         numpy_compare.assert_equal(alpha, [sym.Expression(a), a*a])
         numpy_compare.assert_equal(w0, [sym.Expression(x), sym.Expression(0)])
+
+
+class TestUnapply(unittest.TestCase):
+    def setUp(self):
+        # For these kinds of expressions, the unapplied args should only ever
+        # be of type Expression or float.
+        self._composite_expressions = [
+            # Abs
+            sym.abs(e_x),
+            # Acos
+            sym.acos(e_x),
+            # Add
+            1.0 + (2.0 * x) + (3.0 * y * z),
+            # Asin
+            sym.asin(e_x),
+            # Atan
+            sym.atan(e_x),
+            # Atan2
+            sym.atan2(e_x, e_y),
+            # Ceil
+            sym.ceil(e_x),
+            # Constant
+            sym.Expression(1.0),
+            # Cos
+            sym.cos(e_x),
+            # Cosh
+            sym.cosh(e_x),
+            # Div
+            e_x / e_y,
+            # Exp
+            sym.exp(e_x),
+            # Floor
+            sym.floor(e_x),
+            # Log
+            sym.log(e_x),
+            # Max
+            sym.max(e_x, e_y),
+            # Min
+            sym.min(e_x, e_y),
+            # Mul
+            2.0 * x * sym.pow(y, z),
+            # NaN
+            sym.Expression(np.nan),
+            # Pow
+            sym.pow(e_x, e_y),
+            # Sin
+            sym.sin(e_x),
+            # Sinh
+            sym.sinh(e_x),
+            # Sqrt
+            sym.sqrt(e_x),
+            # Tan
+            sym.tan(e_x),
+            # Tanh
+            sym.tanh(e_x),
+        ]
+        # For these kinds of expressions, at least one of the unapplied args
+        # will not be an Expression nor float.
+        self._other_expressions = [
+            # IfThenElse
+            sym.if_then_else(e_x < e_y, e_x, e_y),
+            # UninterpretedFunction
+            sym.uninterpreted_function("name", [e_x, e_y]),
+            # Var
+            e_x,
+        ]
+
+    def test_round_trip(self):
+        """Focused unit test to check each kind of Expression at least once."""
+        for e in self._composite_expressions + self._other_expressions:
+            with self.subTest(e=e):
+                self._check_one_round_trip(e)
+
+    def _check_one_round_trip(self, e):
+        ctor, args = e.Unapply()
+        result = ctor(*args)
+        self.assertTrue(e.EqualTo(result), msg=repr(result))
+
+    def test_is_composite(self):
+        """Confirms that some kinds of expressions have only expressions or
+        floats as children.
+        """
+        for e in self._composite_expressions:
+            with self.subTest(e=e):
+                self._check_one_composite(e)
+
+    def _check_one_composite(self, e):
+        ctor, args = e.Unapply()
+        self.assertGreater(len(args), 0)
+        for arg in args:
+            self.assertIsInstance(arg, (sym.Expression, float))
+
+    def test_replace(self):
+        """Acceptance test that shows how to perform substitutions."""
+        e1 = x * sym.sin(y) + 2.0 * sym.exp(sym.sin(y))
+        sy = sym.Variable("sy")
+        e2 = self._replace(e1, sym.sin(y), sy).Expand()
+        self.assertEqual(str(e2), "((x * sy) + 2 * exp(sy))")
+
+    def _replace(self, expr, old_subexpr, new_subexpr):
+        if not isinstance(expr, sym.Expression):
+            return expr
+        if expr.EqualTo(old_subexpr):
+            return new_subexpr
+        ctor, old_args = expr.Unapply()
+        new_args = [
+            self._replace(arg, old_subexpr, new_subexpr)
+            for arg in old_args
+        ]
+        return ctor(*new_args)
