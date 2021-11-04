a778574445c53c8c6009fda6986bf42e1351cb37
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-11-04 09:14:22 -0700

    [pydrake] Add symbolic.Expression.Unapply

--- bindings/pydrake/_symbolic_extra.py
+++ bindings/pydrake/_symbolic_extra.py
@@ -2,6 +2,7 @@
 # rationale.
 
 import functools
+import operator
 
 
 def logical_and(*formulas):
@@ -12,3 +13,11 @@ def logical_and(*formulas):
 def logical_or(*formulas):
     assert len(formulas) >= 1, "Must supply at least one operand"
     return functools.reduce(__logical_or, formulas)
+
+
+def _reduce_add(*args):
+    return functools.reduce(operator.add, args)
+
+
+def _reduce_mul(*args):
+    return functools.reduce(operator.mul, args)
