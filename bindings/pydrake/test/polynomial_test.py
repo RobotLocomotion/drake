9b3851ce5964b376e4fa32e09f7d15589debe869
Mark Petersen <31107801+mpetersen94@users.noreply.github.com>
2021-11-09 10:24:50 -0500

    Bind all scalar templates of Polynomial and Trajectory + subclasses (#16067)

--- bindings/pydrake/test/polynomial_test.py
+++ bindings/pydrake/test/polynomial_test.py
@@ -3,39 +3,60 @@ import unittest
 
 from pydrake.common import ToleranceType
 from pydrake.common.test_utilities import numpy_compare
-from pydrake.polynomial import Polynomial
+from pydrake.polynomial import Polynomial_
 
 
 class TestPolynomial(unittest.TestCase):
-    def test_constructors(self):
+    @numpy_compare.check_all_types
+    def test_constructors(self, T):
+        Polynomial = Polynomial_[T]
+
         p1 = Polynomial()
         p2 = Polynomial(5)
         p3 = Polynomial([1, 2, 3])
 
-    def test_analysis_methods(self):
+    @numpy_compare.check_all_types
+    def test_analysis_methods(self, T):
+        Polynomial = Polynomial_[T]
+
         p = Polynomial([1, 2, 3])
         self.assertEqual(p.GetNumberOfCoefficients(), 3)
         self.assertEqual(p.GetDegree(), 2)
         self.assertFalse(p.IsAffine())
-        np.testing.assert_equal(p.GetCoefficients(), np.array([1, 2, 3]))
+        numpy_compare.assert_float_equal(p.GetCoefficients(),
+                                         np.array([1., 2., 3.]))
         p_d = p.Derivative(derivative_order=1)
         self.assertEqual(p_d.GetDegree(), 1)
         p_i = p.Integral(integration_constant=0)
         self.assertEqual(p_i.GetDegree(), 3)
-        self.assertTrue(
-            p.CoefficientsAlmostEqual(p, 1e-14, ToleranceType.kRelative))
+        numpy_compare.assert_equal(
+            p.CoefficientsAlmostEqual(p, 1e-14, ToleranceType.kRelative), True)
 
-    def test_arithmetic(self):
+    @numpy_compare.check_all_types
+    def test_arithmetic(self, T):
+        Polynomial = Polynomial_[T]
+
         p = Polynomial([0, 1])
-        np.testing.assert_equal((-p).GetCoefficients(), np.array([0, -1]))
-        np.testing.assert_equal((p+p).GetCoefficients(), np.array([0, 2]))
-        np.testing.assert_equal((p+5).GetCoefficients(), np.array([5, 1]))
-        np.testing.assert_equal((5+p).GetCoefficients(), np.array([5, 1]))
-        np.testing.assert_equal((p-p).GetCoefficients(), np.array([0]))
-        np.testing.assert_equal((p-5).GetCoefficients(), np.array([-5, 1]))
-        np.testing.assert_equal((5-p).GetCoefficients(), np.array([5, -1]))
-        np.testing.assert_equal((p*p).GetCoefficients(), np.array([0, 0, 1]))
-        np.testing.assert_equal((p*5).GetCoefficients(), np.array([0, 5]))
-        np.testing.assert_equal((5*p).GetCoefficients(), np.array([0, 5]))
-        np.testing.assert_equal((p/5).GetCoefficients(), np.array([0, 0.2]))
+        numpy_compare.assert_float_equal((-p).GetCoefficients(),
+                                         np.array([0., -1.]))
+        numpy_compare.assert_float_equal((p+p).GetCoefficients(),
+                                         np.array([0., 2.]))
+        numpy_compare.assert_float_equal((p+5).GetCoefficients(),
+                                         np.array([5., 1.]))
+        numpy_compare.assert_float_equal((5+p).GetCoefficients(),
+                                         np.array([5., 1.]))
+        numpy_compare.assert_float_equal((p-p).GetCoefficients(),
+                                         np.array([0.]))
+        numpy_compare.assert_float_equal((p-5).GetCoefficients(),
+                                         np.array([-5., 1.]))
+        numpy_compare.assert_float_equal((5-p).GetCoefficients(),
+                                         np.array([5., -1.]))
+        numpy_compare.assert_float_equal((p*p).GetCoefficients(),
+                                         np.array([0., 0., 1.]))
+        numpy_compare.assert_float_equal((p*5).GetCoefficients(),
+                                         np.array([0., 5.]))
+        numpy_compare.assert_float_equal((5*p).GetCoefficients(),
+                                         np.array([0., 5.]))
+        numpy_compare.assert_float_equal((p/5).GetCoefficients(),
+                                         np.array([0., 0.2]))
         self.assertTrue(p == p)
