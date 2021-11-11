98856102701a21244c16aec826a03a6e7946ac98
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-11-11 10:19:24 -0800

    [lcm] Deprecate DrakeMockLcm

--- lcm/drake_mock_lcm.h
+++ lcm/drake_mock_lcm.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "drake/common/drake_copyable.h"
+#include "drake/common/drake_deprecated.h"
 #include "drake/lcm/drake_lcm.h"
 
 namespace drake {
@@ -12,7 +13,8 @@ memory, not on the wire. Other than the class name, it is identical to a
 href="https://lcm-proj.github.io/group__LcmC__lcm__t.html#gaf29963ef43edadf45296d5ad82c18d4b">memq
 provider</a>.
 */
-class DrakeMockLcm : public DrakeLcm {
+class DRAKE_DEPRECATED("2022-03-01", "Use DrakeLcm(\"memq://\") instead.")
+DrakeMockLcm : public DrakeLcm {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(DrakeMockLcm);
 
