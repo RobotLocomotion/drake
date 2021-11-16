b6fcaead53e7ebac738ad82b20011821635e6cc0
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-11-16 10:00:36 -0800

    WIP paths

--- multibody/fixed_fem/dev/petsc_benchmark/run_petsc_benchmark.sh
+++ multibody/fixed_fem/dev/petsc_benchmark/run_petsc_benchmark.sh
@@ -1,11 +1,14 @@
+set -ex
+PETSC=/home/jwnimmer/jwnimmer-tri/drake/multibody/fixed_fem/dev/tmp/petsc
+GBENCH=/home/jwnimmer/jwnimmer-tri/drake/multibody/fixed_fem/dev/tmp/benchmark/install
 g++ -fPIC -Wall -Wwrite-strings -Wno-strict-aliasing -Wno-unknown-pragma -fstack-protector -fvisibility=hidden -O2 -march=haswell \
--I/dir/to/petsc/include \
--I/dir/to/petsc/arch-linux-c-opt/include \
+-I${PETSC}/include \
+-I${PETSC}/arch-linux-c-opt/include \
 -I/usr/include/eigen3  \
 petsc_benchmark.cc \
--Wl,-rpath,/dir/to/petsc/arch-linux-c-opt/lib -L/dir/to/petsc/arch-linux-c-opt/lib \
+-Wl,-rpath,${PETSC}/arch-linux-c-opt/lib -L${PETSC}/arch-linux-c-opt/lib \
 -Wl,-rpath,/usr/lib/gcc/x86_64-linux-gnu/7 -L/usr/lib/gcc/x86_64-linux-gnu/7  \
 -lpetsc -llapack -lblas \
--isystem /dir/to/google_benchmark/include -L/dir/to/google_benchmark/build/src -lbenchmark -lpthread \
+-isystem ${GBENCH}/include -L${GBENCH}/lib -lbenchmark -lpthread \
 -o petsc_benchmark  \
 && ./petsc_benchmark
