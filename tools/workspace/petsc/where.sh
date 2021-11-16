df942549997eaa521a0193157434da498a6c2463
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-11-16 11:10:57 -0800

    where finds .text also

--- tools/workspace/petsc/where.sh
+++ tools/workspace/petsc/where.sh
@@ -4,4 +4,9 @@ set -e
 
 [[ -n "$1" ]]
 cd ~/jwnimmer-tri/drake/multibody/fixed_fem/dev/tmp/petsc/arch-linux-c-opt/obj
-find . -name '*.o' | xargs -t -n1 objdump -t 2>&1 | egrep '(objdump|bss.*'"$1"')' | fgrep -B 1 bss | head -n 1 | sed -e 's#.*\./#src/#; s#\.o#.c#;'
+find . -name '*.o' |
+  xargs -t -n1 objdump -w -t 2>&1 |
+  egrep '(^objdump|bss.*'"$1"'|text.*'"$1"')' |
+  egrep -B 1 '\.(bss|text)' |
+  head -n 1 |
+  sed -e 's#.*\./#src/#; s#\.o#.c#;'
