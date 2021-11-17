decc5d7ecb00131d0ec8aba94625047aaffc114e
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-11-16 17:26:01 -0800

    speedy where

--- tools/workspace/petsc/where.sh
+++ tools/workspace/petsc/where.sh
@@ -4,8 +4,15 @@ set -e
 
 [[ -n "$1" ]]
 cd ~/jwnimmer-tri/drake/multibody/fixed_fem/dev/tmp/petsc/arch-linux-c-opt/obj
-find . -name '*.o' |
-  xargs -t -n1 objdump -w -t 2>&1 |
+
+database=/home/jwnimmer/jwnimmer-tri/drake/multibody/fixed_fem/dev/tmp/petsc/objdump-database.txt
+if [[ ! -e ${database} ]]; then
+  find . -name '*.o' |
+    xargs -t -n1 objdump -w -t 2>&1 |
+    cat > ${database}
+fi
+
+cat ${database} |
   egrep '(^objdump|bss.*'"$1"'|text.*'"$1"'$)' |
   egrep -B 1 '\.(bss|text)' |
   head -n 1 |
