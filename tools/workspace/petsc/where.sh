a3e31afc370876a11ebc1b36aa22259d832299c6
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-11-16 17:14:48 -0800

    anchor the names

--- tools/workspace/petsc/where.sh
+++ tools/workspace/petsc/where.sh
@@ -6,7 +6,7 @@ set -e
 cd ~/jwnimmer-tri/drake/multibody/fixed_fem/dev/tmp/petsc/arch-linux-c-opt/obj
 find . -name '*.o' |
   xargs -t -n1 objdump -w -t 2>&1 |
-  egrep '(^objdump|bss.*'"$1"'|text.*'"$1"')' |
+  egrep '(^objdump|bss.*'"$1"'|text.*'"$1"'$)' |
   egrep -B 1 '\.(bss|text)' |
   head -n 1 |
   sed -e 's#.*\./#src/#; s#\.o#.c#;'
