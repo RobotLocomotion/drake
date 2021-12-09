0c126bd870fdfd0f28ffb23d548cd24d529617db
Stephen McDowell <stephen.mcdowell@kitware.com>
2021-12-09 12:30:26 -0500

    wheel: avoid gcc-7 ICE by reducing bazel resource usage

--- tools/wheel/dev/image/build-drake.sh
+++ tools/wheel/dev/image/build-drake.sh
@@ -8,7 +8,11 @@ git apply < /image/pip-drake.patch
 
 export SNOPT_PATH=git
 
+# TODO(svenevs) Bionic continues to ICE with gcc 7 and Eigen code, reducing
+# --local_{cpu,ram}_resources is only solution.  Remove with Bionic support.
 bazel run \
+    --local_cpu_resources=HOST_CPUS*0.5 \
+    --local_ram_resources=HOST_RAM*0.5 \
     --disk_cache=/var/cache/bazel/disk_cache \
     --repository_cache=/var/cache/bazel/repository_cache \
     --repo_env=DRAKE_OS=manylinux \
