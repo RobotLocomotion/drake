8a63aac6166b3b583f0ee5b7d9789bef9d5ed6c3
Stephen McDowell <stephen.mcdowell@kitware.com>
2021-12-09 12:22:47 -0500

    pin builds to cmake==3.16.3
    newer cmake required for bionic and vtk-9

--- tools/wheel/dev/image/provision-base.sh
+++ tools/wheel/dev/image/provision-base.sh
@@ -19,13 +19,30 @@ apt-get -y install --no-install-recommends \
     libtool libltdl-dev \
     gcc g++ gfortran libgfortran-7-dev \
     libclang-9-dev clang-format-9 \
-    git cmake ninja-build pkg-config \
+    git ninja-build pkg-config \
     yasm file wget unzip zip ssh
 
 apt-get -y install --no-install-recommends \
     libglib2.0-dev libnlopt-dev \
     libgl1-mesa-dev libxt-dev \
     opencl-headers ocl-icd-opencl-dev
+
+# To build vtk-9 on bionic we need an updated CMake.
+# See: https://apt.kitware.com/
+# TODO(svenevs) Revert this when we drop support for bionic
+apt-get -y install --no-install-recommends gpg lsb-release
+kw_asc="https://apt.kitware.com/keys/kitware-archive-latest.asc"
+wget -O - "$kw_asc" 2>/dev/null | \
+    gpg --dearmor - | \
+    tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] " \
+    "https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main" | \
+    tee /etc/apt/sources.list.d/kitware.list >/dev/null
+apt-get -y update
+rm /usr/share/keyrings/kitware-archive-keyring.gpg
+apt-get -y install --no-install-recommends kitware-archive-keyring
+apt-get -y install --no-install-recommends \
+    "cmake-data=3.16.3-*" "cmake=3.16.3-*"
 
 # Install Bazel.
 cd /tmp
