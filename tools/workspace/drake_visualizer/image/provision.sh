58b7b7723301f664647ec0b6f72b2f6f611b40fd
Stephen McDowell <stephen.mcdowell@kitware.com>
2021-12-22 19:24:44 -0500

    unify drake_visualizer build with wheel and vtk
    - pin cmake and bazel versions
    - minor artifact cleanup change

--- tools/workspace/drake_visualizer/image/provision.sh
+++ tools/workspace/drake_visualizer/image/provision.sh
@@ -4,35 +4,39 @@ set -eu -o pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
+readonly BAZEL_VERSION=4.2.1
+readonly BAZEL_ROOT=https://github.com/bazelbuild/bazel/releases/download
+
 # Install prerequisites.
 apt-get -y update
 apt-get -y upgrade
 
 xargs -d$'\n' apt-get -y install --no-install-recommends < /image/prereqs
 
-rm -rf /var/lib/apt/lists/*
-
-# Install CMake and Bazel.
-# Note that we need a more recent version than is available on Bionic to ensure
-# that we get the updated FindOpenGL and therefore link the correct flavor of
-# the OpenGL library (GLVND). With the version of CMake from the Ubuntu Bionic
-# distribution, we would need even more drastic patching of drake-visualizer.
+# Install CMake.
+# To find the right OpenGL library (GLVND) on bionic we need an updated CMake.
+# See: https://apt.kitware.com/
 # TODO(svenevs) Use distro version of CMake when we drop Bionic support.
-export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
-
-apt-key --keyring /etc/apt/trusted.gpg.d/bazel.gpg adv --quiet \
-    --fetch-keys https://bazel.build/bazel-release.pub.gpg
-apt-key --keyring /etc/apt/trusted.gpg.d/kitware.gpg adv --quiet \
-    --fetch-keys https://apt.kitware.com/keys/kitware-archive-latest.asc
-echo 'deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8' \
-    > /etc/apt/sources.list.d/bazel.list
-echo "deb https://apt.kitware.com/ubuntu $(lsb_release --codename --short) main" \
-    > /etc/apt/sources.list.d/kitware.list
-
+apt-get -y install --no-install-recommends gpg lsb-release wget
+kw_asc="https://apt.kitware.com/keys/kitware-archive-latest.asc"
+wget -O - "$kw_asc" 2>/dev/null | \
+    gpg --dearmor - | \
+    tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] " \
+    "https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main" | \
+    tee /etc/apt/sources.list.d/kitware.list >/dev/null
 apt-get -y update
-apt-get -y install --no-install-recommends bazel cmake
+rm /usr/share/keyrings/kitware-archive-keyring.gpg
+apt-get -y install --no-install-recommends kitware-archive-keyring
+apt-get -y install --no-install-recommends \
+    "cmake-data=3.16.3-*" "cmake=3.16.3-*"
 
-rm -rf /var/lib/apt/lists/*
+# Install Bazel.
+apt-get -y install --no-install-recommends unzip
+cd /tmp
+wget ${BAZEL_ROOT}/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
+bash /tmp/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
+rm /tmp/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
 
 # Note that we symlink /usr/bin/python since the package names to install an
 # executable named as such are not the same in different Ubuntu packages.
