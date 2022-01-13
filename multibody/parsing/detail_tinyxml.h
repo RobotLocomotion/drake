53c5441b6ab800276ab1ab4f07fc9462ecada607
Russ Tedrake <russt@mit.edu>
2022-01-13 12:19:10 -0500

    [multibody] Parse mujoco geometry (but not meshes)
    Towards #16369

--- multibody/parsing/detail_tinyxml.h
+++ multibody/parsing/detail_tinyxml.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include <Eigen/Dense>
 #include <tinyxml2.h>
@@ -10,6 +11,10 @@
 namespace drake {
 namespace multibody {
 namespace internal {
+
+// Parses a string containing double values in the XML attribute format into a
+// vector of doubles.
+std::vector<double> ConvertToDoubles(const std::string& str);
 
 // Parses a string attribute of @p node named @p attribute_name into @p val.
 // If the attribute is not present, @p val will be cleared.
@@ -27,27 +32,32 @@ bool ParseStringAttribute(const tinyxml2::XMLElement* node,
 bool ParseScalarAttribute(const tinyxml2::XMLElement* node,
                           const char* attribute_name, double* val);
 
-// Parses an attribute of @p node named @p attribute_name consisting of 3
-// scalar values into @p val.
+// Parses an attribute of @p node named @p attribute_name consisting of scalar
+// values into @p val.
 //
-// @returns false if the attribute is not present
+// @returns false if the attribute is not present, and the output is left
+// untouched.
+// @tparam rows is indicates the size of the vector `val`.
 //
-// @throws std::exception if the attribute doesn't contain
-// three numeric values.
+// @throws std::exception if the attribute doesn't contain `rows` numeric
+// values.
+template <int rows>
 bool ParseVectorAttribute(const tinyxml2::XMLElement* node,
                           const char* attribute_name,
-                          Eigen::Vector3d* val);
-
-// Parses an attribute of @p node named @p attribute_name consisting of 4
-// scalar values into @p val.
-//
-// @returns false if the attribute is not present
-//
-// @throws std::exception if the attribute doesn't contain
-// four numeric values.
-bool ParseVectorAttribute(const tinyxml2::XMLElement* node,
-                          const char* attribute_name,
-                          Eigen::Vector4d* val);
+                          Eigen::Matrix<double, rows, 1>* val) {
+  const char* attr = node->Attribute(attribute_name);
+  if (attr) {
+    std::vector<double> vals = ConvertToDoubles(attr);
+    if (vals.size() != rows) {
+      throw std::invalid_argument(
+          fmt::format("Expected {} values for attribute {} got {}", rows,
+                      attribute_name, attr));
+    }
+    *val = Eigen::Matrix<double, rows, 1>(vals.data());
+    return true;
+  }
+  return false;
+}
 
 // Parses "xyz" and "rpy" attributes from @p node and returns a
 // RigidTransformd created from them.  If either the "xyz" or "rpy"
@@ -60,7 +70,7 @@ math::RigidTransformd OriginAttributesToTransform(
 
 // Parses a three vector value from parameter @p node, which is an
 // XML node. The value is specified by an attribute within the XML
-// whose name is apecified by parameter @p attribute_name.  This
+// whose name is specified by parameter @p attribute_name.  This
 // method also supports a three vector specified by a single scalar
 // value, which it automatically converts into a three vector by
 // using the same scalar value for all three dimensions.
