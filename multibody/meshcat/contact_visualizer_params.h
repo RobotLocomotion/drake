85beb5d1064b25254cdce2261ad8bad3781eaad7
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-01-26 07:53:55 -0800

    [multibody] Move ContactResultsToMeshcat into meshcat directory
    ... and rename it to ContactVisualizer for simplicity.
    
    We should not contaminate the MultibodyPlant library with an
    unconditional dependency on MeshCat.

--- multibody/meshcat/contact_visualizer_params.h
+++ multibody/meshcat/contact_visualizer_params.h
@@ -6,9 +6,10 @@
 
 namespace drake {
 namespace multibody {
+namespace meshcat {
 
-/** The set of parameters for configuring ContactResultsToMeshcat. */
-struct ContactResultsToMeshcatParams {
+/** The set of parameters for configuring ContactVisualizer. */
+struct ContactVisualizerParams {
   /** The duration (in simulation seconds) between attempts to update poses in
    the visualizer. (To help avoid small simulation timesteps, we use a default
    period that has an exact representation in binary floating point; see
@@ -19,7 +20,7 @@ struct ContactResultsToMeshcatParams {
   geometry::Rgba color{0, 1, 0, 1};
 
   /** A prefix to add to the path for all objects and transforms curated by the
-   ContactResultsToMeshcat. It can be an absolute path or relative path.
+   ContactVisualizer. It can be an absolute path or relative path.
    If relative, this `prefix` will be appended to the geometry::Meshcat `prefix`
    based on the standard path semantics in Meshcat. See @ref meshcat_path
    "Meshcat paths" for details. */
@@ -32,7 +33,7 @@ struct ContactResultsToMeshcatParams {
   bool delete_on_initialization_event{true};
 
   /** The threshold below which forces will no longer be drawn.  The
-   ContactResultsToMeshcat constructor enforces that this must be strictly
+   ContactVisualizer constructor enforces that this must be strictly
    positive; zero forces do not have a meaningful direction and cannot be
    visualized. */
   double force_threshold{0.01};
@@ -44,5 +45,6 @@ struct ContactResultsToMeshcatParams {
   double radius{0.01};
 };
 
+}  // namespace meshcat
 }  // namespace multibody
 }  // namespace drake
