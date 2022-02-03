564f81c1cf1116dcf4b09937fcaee4cbc7c7e9d5
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-02-03 13:18:45 -0800

    [doc] Add experimental keyword to mark unstable APIs (#16503)
    * [doc] Add @experimental keyword to mark unstable APIs

--- tools/workspace/pybind11/mkdoc_comment.py
+++ tools/workspace/pybind11/mkdoc_comment.py
@@ -285,6 +285,7 @@ def process_doxygen_commands(s):
     # Drake-specific Doxygen aliases.
     # TODO (betsymcphail): Not tested
     s = re.sub(r'[@\\]default\s+', r'\n$*Default:* ', s)
+    s = re.sub(r'[@\\]experimental\s+', '\n\n$Warning:\n\nThis feature is considered to be **experimental** and may change or be removed at any time, without any deprecation notice ahead of time.\n\n', s)  # noqa
 
     # Omit tparam scalar type boilerplate; the python documentation already
     # presents this information in a more useful manner.
