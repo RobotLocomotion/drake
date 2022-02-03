652a2a20c65e395ff708c912221d2e3e1db9308e
amcastro-tri <alejandro.castro@tri.global>
2022-02-03 09:16:55 -0500

    Documents SapContactProblem's constructor

--- multibody/contact_solvers/sap/sap_contact_problem.h
+++ multibody/contact_solvers/sap/sap_contact_problem.h
@@ -198,14 +198,35 @@ class SapCouplerConstraint final : public SapConstraint<T> {
 // SAP Problem defined by:
 //   - A⋅(v−v*) = Jᵀ⋅γ
 //   - Constraints.
+// See design document for more details.
 template <typename T>
 class SapContactProblem {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(SapContactProblem);
 
+  // Constructs a SAP contact problem where the linearized dynamics read:
+  //  A⋅(v−v*) = Jᵀ⋅γ
+  // where A is the linearized discrete momentum matrix and v* are the
+  // free-motion generalized velocities. The contact Jacobian J is specified as
+  // constraints are added with AddConstraint().
+  // 
+  // @param time_step The time step used by the discrete SAP formulation.
+  // @param A Linearized dynamics momentum matrix. It is block diagonal. Each
+  // block corresponds to a group of DOFs or "clique". The number of cliques is
+  // A.size() and the number of DOFs in the c-th clique is A[c].rows() (or
+  // A[c].cols() since each block is square). The total number of generalized
+  // velocities is nv = ∑A[c].rows().
+  // @param v_star Free-motion velocities, of size nv. DOFs order must
+  // correspond to those referenced in A.
+  // 
+  // TODO: I found useful to keep a reference to the original data in the scope
+  // where the problem is built (the manager). Therefore it might be more
+  // convenient for the constructor to keep references to data rather than
+  // moving copies of the original data.
   SapContactProblem(const T& time_step, std::vector<MatrixX<T>>&& A,
                     VectorX<T>&& v_star);
 
+  // TODO: I believe this is not needed. Remove.
   SapContactProblem(
       std::vector<MatrixX<T>>&& A, VectorX<T>&& v_star,
       std::vector<std::unique_ptr<SapConstraint<T>>>&& constraints);
