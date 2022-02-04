962f76f3144a74cbbefea44937cd071589e2e12f
Betsy McPhail <betsy.mcphail@kitware.com>
2022-02-04 16:49:07 -0500

    setup: Upgrade recommended Bazel version to 5.0

--- tools/workspace/drake_visualizer/image/provision.sh
+++ tools/workspace/drake_visualizer/image/provision.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
-readonly BAZEL_VERSION=4.2.1
+readonly BAZEL_VERSION=5.0.0
 readonly BAZEL_ROOT=https://github.com/bazelbuild/bazel/releases/download
 
 # Install prerequisites.
