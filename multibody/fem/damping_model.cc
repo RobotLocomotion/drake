8fdef42f76075b3e6574be75519eb1fb08708981
xuchenhan-tri <68254008+xuchenhan-tri@users.noreply.github.com>
2022-02-03 17:25:30 -0800

    [fem] Add Rayleigh damping model (#16496)

---
+++ multibody/fem/damping_model.cc
@@ -0,0 +1,23 @@
+#include "drake/multibody/fem/damping_model.h"
+
+#include "drake/common/drake_throw.h"
+
+namespace drake {
+namespace multibody {
+namespace fem {
+
+template <typename T>
+DampingModel<T>::DampingModel(const T& mass_coeff_alpha,
+                              const T& stiffness_coeff_beta)
+    : mass_coeff_alpha_(mass_coeff_alpha),
+      stiffness_coeff_beta_(stiffness_coeff_beta) {
+  DRAKE_THROW_UNLESS(mass_coeff_alpha >= 0.0);
+  DRAKE_THROW_UNLESS(stiffness_coeff_beta >= 0.0);
+}
+
+}  // namespace fem
+}  // namespace multibody
+}  // namespace drake
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
+    class ::drake::multibody::fem::DampingModel);
