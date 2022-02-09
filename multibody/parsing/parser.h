34dc17065573dc182074f3b29492ca0b45e31290
Rick Poyner <rick.poyner@tri.global>
2022-02-09 18:06:50 -0500

    [parsing] Draft of diagnostics control, used with URDF parsing
    Relevant to: #16229

--- multibody/parsing/parser.h
+++ multibody/parsing/parser.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 
+#include "drake/common/diagnostic_policy.h"
 #include "drake/multibody/parsing/package_map.h"
 #include "drake/multibody/plant/multibody_plant.h"
 #include "drake/multibody/tree/multibody_tree_indexes.h"
@@ -32,6 +33,10 @@ class Parser final {
 
   /// Gets a mutable reference to the PackageMap used by this parser.
   PackageMap& package_map() { return package_map_; }
+
+  /// Cause all subsequent Add*Model*() operations to use strict parsing;
+  /// warnings will be treated as errors.
+  void SetStrictParsing() { is_strict_ = true; }
 
   /// Parses the SDF or URDF file named in @p file_name and adds all of its
   /// model(s) to @p plant.
@@ -87,7 +92,9 @@ class Parser final {
       const std::string& model_name = {});
 
  private:
+  bool is_strict_{false};
   PackageMap package_map_;
+  drake::internal::DiagnosticPolicy diagnostic_policy_;
   MultibodyPlant<double>* const plant_;
 };
 
