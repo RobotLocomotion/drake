8d5ac6c85a8e0bc4d661901453094781105fdcee
amcastro-tri <alejandro.castro@tri.global>
2022-02-15 10:36:16 -0500

    Promotes ConstraintCluster into a class

--- multibody/contact_solvers/sap/contact_problem_graph.cc
+++ multibody/contact_solvers/sap/contact_problem_graph.cc
@@ -7,24 +7,34 @@ namespace multibody {
 namespace contact_solvers {
 namespace internal {
 
+ContactProblemGraph::ConstraintCluster::ConstraintCluster(
+    SortedPair<int> cliques)
+    : cliques_(std::move(cliques)) {
+  DRAKE_THROW_UNLESS(cliques_.first() >= 0 && cliques_.second() >= 0);
+}
+
+ContactProblemGraph::ConstraintCluster&
+ContactProblemGraph::ConstraintCluster::AddConstraint(
+    int constraint_index, int num_constraint_equations) {
+  DRAKE_THROW_UNLESS(constraint_index >= 0 && num_constraint_equations >= 0);
+  num_constraint_equations_ += num_constraint_equations;
+  constraint_index_.push_back(constraint_index);
+  constraint_num_equations_.push_back(num_constraint_equations);
+  return *this;
+}
+
 ContactProblemGraph::ContactProblemGraph(int num_cliques)
     : num_cliques_(num_cliques) {}
 
-int ContactProblemGraph::AddConstraint(const SortedPair<int>& cliques_pair,
+int ContactProblemGraph::AddConstraint(SortedPair<int> cliques_pair,
                                        int num_constraint_equations) {
-  const int cluster_index = clusters_.size();
-
   auto [iterator, new_cluster] = pair_to_cluster_index_.insert(
-      std::make_pair(cliques_pair, cluster_index));
-  if (new_cluster) clusters_.push_back(ConstraintCluster());
-
-  ConstraintCluster& cluster = clusters_[iterator->second];
-  cluster.cliques = cliques_pair;
-  cluster.num_constraint_equations += num_constraint_equations;
-  cluster.constraint_num_equations.push_back(num_constraint_equations);
-  const int constraint_index = num_constraints_;
-  cluster.constraint_index.push_back(constraint_index);
-  ++num_constraints_;
+      std::make_pair(cliques_pair, clusters_.size()));
+  if (new_cluster) clusters_.emplace_back(std::move(cliques_pair));
+  const int cluster_index = iterator->second;
+  const int constraint_index = num_constraints_++;
+  ConstraintCluster& cluster = clusters_[cluster_index];
+  cluster.AddConstraint(constraint_index, num_constraint_equations);
   num_constraint_equations_ += num_constraint_equations;
   return constraint_index;
 }
@@ -33,8 +43,8 @@ int ContactProblemGraph::AddConstraint(int clique,
                                        int num_constraint_equations) {
   DRAKE_THROW_UNLESS(0 <= clique && clique < num_cliques());
   DRAKE_THROW_UNLESS(num_constraint_equations >= 0);
-  const auto cliques_pair = drake::MakeSortedPair(clique, clique);
-  return AddConstraint(cliques_pair, num_constraint_equations);
+  return AddConstraint(drake::MakeSortedPair(clique, clique),
+                       num_constraint_equations);
 }
 
 int ContactProblemGraph::AddConstraint(int first_clique, int second_clique,
--- multibody/contact_solvers/sap/contact_problem_graph.h
+++ multibody/contact_solvers/sap/contact_problem_graph.h
@@ -12,19 +12,18 @@ namespace contact_solvers {
 namespace internal {
 
 /* This class represents the graph for a SapContactProblem where each node in
-the graph corresponds to a clique and each edge in the graph corresponds to a
-"constraint cluster", see ConstraintCluster. A cluster of constraints or
-"constraint cluster" is defined as the set of constraints between one or two
-cliques. This concept is described in detail with an example in [Castro et al.,
-2021].
+ the graph corresponds to a clique and each edge in the graph corresponds to a
+ "constraint cluster", see ConstraintCluster. A constraint cluster is the set of
+ all constraints acting on the same clique, or on the same pair of cliques. This
+ concept is described in detail with an example in [Castro et al., 2021].
 
-NOTE: This code in Drake is an extension to the ideas in [Castro et al., 2021].
-While [Castro et al., 2021] refers to "trees" in the multibody system, this idea
-is extended to the more general concept of "cliques" for any mechanical system.
-Similarly, while [Castro et al., 2021] refers to as "patches" to the the set of
-constact constraints between one or two trees, this concept has been extended to
-"constraint cluster" to refer to a set of arbitrary constraints (not necessarily
-contact constraints.)
+ NOTE: This code in Drake is an extension of the ideas in [Castro et al., 2021].
+ While [Castro et al., 2021] refers to "trees" in the multibody system, this
+ idea is extended to the more general concept of "cliques" for any mechanical
+ system. Similarly, while [Castro et al., 2021] refers to the set of contact
+ constraints between one or two trees as "patches", this concept has been
+ extended to "constraint cluster" to refer to a set of arbitrary constraints
+ (not necessarily contact constraints.)
 
  Clusters are created in the order constraints are added to the problem, see
  AddConstraint(). The first time a constraint between one or two cliques is
@@ -40,37 +39,65 @@ class ContactProblemGraph {
   cliques are stored as a SortedPair. Along with the cliques pair and the set of
   constraints, this struct stores additional metadata related to problem size
   useful when building a mathematical model of the problem. */
-  struct ConstraintCluster {
+  class ConstraintCluster {
+   public:
+    DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(ConstraintCluster);
+
+    /* Constructor for a cluster of constraints between the cliques specified by
+     `cliques_pair`. Indexes in `cliques_pair` must be non-negative. or an
+     exception is thrown.*/
+    explicit ConstraintCluster(SortedPair<int> cliques);
+
+    /* Adds a constraint with index `constraint_index` and
+     `num_constraint_equations` constraint equations into this cluster. Both
+     `constraint_index` and `num_constraint_equations` must be non-negative or
+     an exception is thrown. */
+    ConstraintCluster& AddConstraint(int constraint_index,
+                                     int num_constraint_equations);
+
+    const drake::SortedPair<int>& cliques() const { return cliques_; }
+    int num_constraints() const {
+      return static_cast<int>(constraint_index_.size());
+    }
+    int num_constraint_equations() const { return num_constraint_equations_; }
+    const std::vector<int>& constraint_index() const {
+      return constraint_index_;
+    }
+    const std::vector<int>& constraint_num_equations() const {
+      return constraint_num_equations_;
+    }
+
+   private:
     /* The one or two cliques connected by the set of constraints in this
      cluster. We allow cliques.first() and cliques.second() to be the same
      clique to identify a constraint within the same clique. This corresponds to
      a graph edge that connects the clique's node with itself. */
-    drake::SortedPair<int> cliques;
+    drake::SortedPair<int> cliques_;
 
     /* Total number of constraint equations contributed by this cluster. */
-    int num_constraint_equations{0};
+    int num_constraint_equations_{0};
 
     /* Given the i-th constraint in this cluster, constraint_index[i] returns
      the constraint index assigned by ContactProblemGraph::AddConstraint(). */
-    std::vector<int> constraint_index;
+    std::vector<int> constraint_index_;
 
     /* Given the i-th constraint in this cluster, constraint_num_equations[i]
      returns the number of constraint equations contributed by the constraint
      with index constraint_index[i]. */
-    std::vector<int> constraint_num_equations;
+    std::vector<int> constraint_num_equations_;
   };
 
   /* Constructor for a graph with `num_cliques`. */
   explicit ContactProblemGraph(int num_cliques);
 
   /* Add constraint within a single clique with `num_constraint_equations`. An
-   exception is thrown if `clique` is not in (0, num_cliques()) or if
+   exception is thrown if `clique` is not in [0, num_cliques()) or if
    `num_constraint_equations` is negative. Returns the index to the newly added
    constraint. */
   int AddConstraint(int clique, int num_constraint_equations);
 
   /* Add constraint between two cliques with `num_constraint_equations`. An
-   exception is thrown if either `first_clique` or `second_clique` is not in (0,
+   exception is thrown if either `first_clique` or `second_clique` is not in [0,
    num_cliques()) or if `num_constraint_equations` is negative. Returns the
    index to the newly added constraint. */
   int AddConstraint(int first_clique, int second_clique,
@@ -100,23 +127,22 @@ class ContactProblemGraph {
   }
 
  private:
+  /* Helper to add a constraint between a pair of cliques. */
+  int AddConstraint(SortedPair<int> cliques_pair, int num_constrained_dofs);
+
   int num_cliques_{0};
   int num_constraints_{0};
   int num_constraint_equations_{0};
-  // Vector of all clusters in the graph. Clusters are created in the order
-  // constraints are added to the problem. The first time a constraint between
-  // two cliques is added, a cluster is created. Subsequent addition of more
-  // constraints between the same two cliques will be associated with the
-  // already existing cluster in the problem.
-  // Given a SortedPair of cliques, an index into this array can be obtained
-  // with the map pair_to_cluster_index_.
+  /* Vector of all clusters in the graph. Clusters are created in the order
+   constraints are added to the problem. The first time a constraint between
+   two cliques is added, a cluster is created. Subsequent addition of more
+   constraints between the same two cliques will be associated with the
+   already existing cluster in the problem.
+   Given a SortedPair of cliques, an index into this array can be obtained
+   with the map pair_to_cluster_index_. */
   std::vector<ConstraintCluster> clusters_;
   // Map cliques pair to cluster index.
   std::unordered_map<SortedPair<int>, int> pair_to_cluster_index_;
-
-  /* Helper to add a constraint between a pair of cliques. */
-  int AddConstraint(const SortedPair<int>& cliques_pair,
-                    int num_constrained_dofs);
 };
 
 }  // namespace internal
