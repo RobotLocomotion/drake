eacd781e763b2afadaa90ee49b467e2a57e4e1f2
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-02-16 21:41:39 -0800

    [performance] Rewrite benchmark_tool infrastructure
    Get rid of all of the small, user-visible helper programs, now
    replaced by two bazel macros (plus some internal helpers).
    
    One macro declares a googlebench binary. The other macro wraps it up
    into a controlled experiment runner.
    
    Update READMEs to match.

--- examples/multibody/cassie_benchmark/record_results.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Collect context information for a benchmark experiment.
-
-${TEST_SRCDIR}/drake/tools/performance/record_results \
-    ${TEST_SRCDIR}/drake/examples/multibody/cassie_benchmark/cassie_bench \
-    "$@"
