8935715ce4609513dff8bd6e4120583184f82b51
mpetersen94 <markpetersen@g.harvard.edu>
2022-02-19 00:56:41 -0500

    Add Intersection Convex Set

---
+++ geometry/optimization/intersection.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "drake/geometry/optimization/convex_set.h"
+
+namespace drake {
+namespace geometry {
+namespace optimization {
+
+/** Implements a rotated second order cone convex set where \f$ z=Ax+b \f$ lies
+within rotated Lorentz cone.  Note: This set is unbounded.
+@ingroup geometry_optimization
+*/
+class Intersection final : public ConvexSet {
+ public:
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(Intersection)
+
+  /** Constructs the intersection from a vector of convex sets. */
+  explicit Intersection(const ConvexSets& sets);
+
+  /** Constructs the intersection from a pair of convex sets. */
+  Intersection(const ConvexSet& setA, const ConvexSet& setB);
+
+  ~Intersection() final;
+
+  /** The number of factors (or sets) used in the intersection. */
+  int num_factors() const { return sets_.size(); }
+
+  /** Returns a reference to the ConvexSet defining the @p index factor in the
+  intersection. */
+  const ConvexSet& factor(int i) const;
+
+ private:
+  bool DoIsBounded() const final;
+
+  bool DoPointInSet(const Eigen::Ref<const Eigen::VectorXd>& x,
+                    double tol) const final;
+
+  void DoAddPointInSetConstraints(
+      solvers::MathematicalProgram* prog,
+      const Eigen::Ref<const solvers::VectorXDecisionVariable>& vars)
+      const final;
+
+  std::vector<solvers::Binding<solvers::Constraint>>
+  DoAddPointInNonnegativeScalingConstraints(
+      solvers::MathematicalProgram* prog,
+      const Eigen::Ref<const solvers::VectorXDecisionVariable>& x,
+      const symbolic::Variable& t) const final;
+
+  std::vector<solvers::Binding<solvers::Constraint>>
+  DoAddPointInNonnegativeScalingConstraints(
+      solvers::MathematicalProgram* prog,
+      const Eigen::Ref<const Eigen::MatrixXd>& A,
+      const Eigen::Ref<const Eigen::VectorXd>& b,
+      const Eigen::Ref<const Eigen::VectorXd>& c, double d,
+      const Eigen::Ref<const solvers::VectorXDecisionVariable>& x,
+      const Eigen::Ref<const solvers::VectorXDecisionVariable>& t) const final;
+
+  // TODO(mpetersen94): Implement DoToShapeWithPose.
+  std::pair<std::unique_ptr<Shape>, math::RigidTransformd> DoToShapeWithPose()
+      const final;
+
+  ConvexSets sets_{};  // Not marked const to support move semantics.
+};
+
+}  // namespace optimization
+}  // namespace geometry
+}  // namespace drake
