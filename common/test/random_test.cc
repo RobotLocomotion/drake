7e50c5d9c51e4c7373d393916857e8f278c678f2
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-02-21 06:23:04 -0800

    [common] Give RandomGenerator efficient default and move constructors (#16633)
    Copying 5000 bytes of storage during a move is wasteful; better to just
    swap a heap pointer.
    
    Calculating 624 random integers in the default constructor is wasteful,
    if we're only trying to allocate a storage for generator that will be
    moved into later (e.g., when resizing a collection of generators).
    
    While we're here, tidy up some of its API documentation for completeness.

--- common/test/random_test.cc
+++ common/test/random_test.cc
@@ -1,37 +1,165 @@
 #include "drake/common/random.h"
 
 #include <limits>
+#include <type_traits>
 
 #include <gtest/gtest.h>
 
 #include "drake/common/autodiff.h"
+#include "drake/common/test_utilities/limit_malloc.h"
 
 namespace drake {
 namespace {
 
-const double kEps = std::numeric_limits<double>::epsilon();
+constexpr double kEps = std::numeric_limits<double>::epsilon();
 
-GTEST_TEST(RandomTest, CompareWith19337) {
+// The mt19937 inside our RandomGenerator implementation has a large block of
+// pre-computed internal state that is only updated (in bulk) after a certain
+// number of random outputs. Extracting this many outputs is sufficient to
+// ensure that we trigger the internal update, in case it might be relevant
+// to our memory-safety tests in the below.
+//
+// For details, refer to state_size (which is n == 624 in our case) here:
+// https://en.cppreference.com/w/cpp/numeric/random/mersenne_twister_engine
+constexpr int kNumSteps = 700;
+
+GTEST_TEST(RandomGeneratorTest, Traits) {
+  EXPECT_TRUE(std::is_default_constructible_v<RandomGenerator>);
+  EXPECT_TRUE(std::is_copy_constructible_v<RandomGenerator>);
+  EXPECT_TRUE(std::is_copy_assignable_v<RandomGenerator>);
+  EXPECT_TRUE(std::is_move_constructible_v<RandomGenerator>);
+  EXPECT_TRUE(std::is_move_assignable_v<RandomGenerator>);
+}
+
+GTEST_TEST(RandomGeneratorTest, Efficiency) {
+  // Check an insanity upper bound, to demonstrate that we don't store large
+  // random state on the stack.
+  EXPECT_LE(sizeof(RandomGenerator), 64);
+
+  {
+    // Default construction is heap-free.
+    drake::test::LimitMalloc guard;
+    RandomGenerator dut;
+  }
+}
+
+// Copying a defaulted generator produces identical outputs.
+GTEST_TEST(RandomGeneratorTest, Copy0) {
+  RandomGenerator foo;
+  RandomGenerator bar(foo);
+  for (int i = 0; i < kNumSteps; ++i) {
+    ASSERT_EQ(foo(), bar()) << "with i = " << i;
+  }
+}
+
+// Copying a seeded generator produces identical outputs.
+GTEST_TEST(RandomGeneratorTest, Copy1) {
+  RandomGenerator foo(123);
+  RandomGenerator bar(foo);
+  for (int i = 0; i < kNumSteps; ++i) {
+    ASSERT_EQ(foo(), bar()) << "with i = " << i;
+  }
+}
+
+// Copy-assigning a defaulted generator produces identical outputs.
+GTEST_TEST(RandomGeneratorTest, CopyAssign0) {
+  RandomGenerator foo;
+  RandomGenerator bar;
+  bar = foo;
+  for (int i = 0; i < kNumSteps; ++i) {
+    ASSERT_EQ(foo(), bar()) << "with i = " << i;
+  }
+}
+
+// Copy-assigning a seeded generator produces identical outputs.
+GTEST_TEST(RandomGeneratorTest, CopyAssign1) {
+  RandomGenerator foo(123);
+  RandomGenerator bar;
+  bar = foo;
+  for (int i = 0; i < kNumSteps; ++i) {
+    ASSERT_EQ(foo(), bar()) << "with i = " << i;
+  }
+}
+
+// Moving from a defaulted generator produces identical outputs.
+GTEST_TEST(RandomGeneratorTest, Move0) {
+  RandomGenerator foo;
+  RandomGenerator temp;
+  RandomGenerator bar(std::move(temp));
+  for (int i = 0; i < kNumSteps; ++i) {
+    ASSERT_EQ(foo(), bar()) << "with i = " << i;
+  }
+}
+
+// Moving from a seeded generator produces identical outputs.
+GTEST_TEST(RandomGeneratorTest, Move1) {
+  RandomGenerator foo(123);
+  RandomGenerator temp(123);
+  RandomGenerator bar(std::move(temp));
+  for (int i = 0; i < kNumSteps; ++i) {
+    ASSERT_EQ(foo(), bar()) << "with i = " << i;
+  }
+}
+
+// Move-assigning from a defaulted generator produces identical outputs.
+GTEST_TEST(RandomGeneratorTest, MoveAssign0) {
+  RandomGenerator foo;
+  RandomGenerator bar;
+  bar = RandomGenerator();
+  for (int i = 0; i < kNumSteps; ++i) {
+    ASSERT_EQ(foo(), bar()) << "with i = " << i;
+  }
+}
+
+// Move-assigning from a seeded generator produces identical outputs.
+GTEST_TEST(RandomGeneratorTest, MoveAssign1) {
+  RandomGenerator foo(123);
+  RandomGenerator bar;
+  bar = RandomGenerator(123);
+  for (int i = 0; i < kNumSteps; ++i) {
+    ASSERT_EQ(foo(), bar()) << "with i = " << i;
+  }
+}
+
+// Using a moved-from defaulted generator does not segfault.
+GTEST_TEST(RandomGeneratorTest, MovedFrom0) {
+  RandomGenerator foo;
+  RandomGenerator bar = std::move(foo);
+  for (int i = 0; i < kNumSteps; ++i) {
+    EXPECT_NO_THROW(foo());
+    EXPECT_NO_THROW(bar());
+  }
+}
+
+// Using a moved-from seeded generator does not segfault.
+GTEST_TEST(RandomGeneratorTest, MovedFrom1) {
+  RandomGenerator foo(123);
+  RandomGenerator bar = std::move(foo);
+  for (int i = 0; i < kNumSteps; ++i) {
+    EXPECT_NO_THROW(foo());
+    EXPECT_NO_THROW(bar());
+  }
+}
+
+GTEST_TEST(RandomGeneratorTest, CompareWith19337) {
   std::mt19937 oracle;
 
   RandomGenerator dut;
   EXPECT_EQ(dut.min(), oracle.min());
   EXPECT_EQ(dut.max(), oracle.max());
 
-  RandomGenerator::result_type first = dut();
-  auto oracle_first = oracle();
-  EXPECT_EQ(first, oracle_first);
-
-  for (int i = 0; i < 100; ++i) {
-    EXPECT_EQ(dut(), oracle());
+  for (int i = 0; i < kNumSteps; ++i) {
+    ASSERT_EQ(dut(), oracle()) << "with i = " << i;
   }
 }
 
-GTEST_TEST(RandomTest, Seed) {
-  RandomGenerator dut1;
-  RandomGenerator dut2(RandomGenerator::default_seed);
-  for (int i = 0; i < 100; ++i) {
-    EXPECT_EQ(dut1(), dut2());
+// The sequence from a default-constructed generator is the same as explicitly
+// passing in the default seed.
+GTEST_TEST(RandomGeneratorTest, DefaultMatchesSeedConstant) {
+  RandomGenerator foo;
+  RandomGenerator bar(RandomGenerator::default_seed);
+  for (int i = 0; i < kNumSteps; ++i) {
+    ASSERT_EQ(foo(), bar()) << "with i = " << i;
   }
 }
 
