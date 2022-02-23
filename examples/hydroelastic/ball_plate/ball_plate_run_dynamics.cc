6a428c669210e3ff10370a85b6c09c2939a06c18
DamrongGuoy <Damrong.Guoy@tri.global>
2022-02-23 10:41:56 -0800

    Platform Reviewable.

--- examples/hydroelastic/ball_plate/ball_plate_run_dynamics.cc
+++ examples/hydroelastic/ball_plate/ball_plate_run_dynamics.cc
@@ -26,11 +26,11 @@ DEFINE_string(contact_surface_representation, "polygon",
               "Contact-surface representation for hydroelastics. "
               "Options are: 'triangle' or 'polygon'. Default is 'polygon'.");
 DEFINE_double(hydroelastic_modulus, 3.0e4,
-              "Hydroelastic modulus of both the ball, [Pa].");
+              "Hydroelastic modulus of the ball, [Pa].");
 DEFINE_double(resolution_hint_factor, 0.3,
               "This scaling factor, [unitless], multiplied by the radius of "
               "the ball gives the target edge length of the mesh of the ball "
-              "on the surface its hydroelastic representation. The smaller "
+              "on the surface of its hydroelastic representation. The smaller "
               "number gives a finer mesh with more tetrahedral elements.");
 DEFINE_double(dissipation, 3.0,
               "Hunt & Crossley dissipation, [s/m], for the ball");
@@ -50,11 +50,11 @@ DEFINE_double(vy, 0.0,
 DEFINE_double(vz, -7.0,
               "Ball's initial translational velocity in the z-axis in m/s.");
 DEFINE_double(wx, 0.0,
-              "Ball's initial angular velocity in the y-axis in degrees/s.");
+              "Ball's initial angular velocity in the x-axis in degrees/s.");
 DEFINE_double(wy, -10.0,
               "Ball's initial angular velocity in the y-axis in degrees/s.");
 DEFINE_double(wz, 0.0,
-              "Ball's initial angular velocity in the y-axis in degrees/s.");
+              "Ball's initial angular velocity in the z-axis in degrees/s.");
 
 // Ball's initial pose.
 DEFINE_double(z0, 0.15, "Ball's initial position in the z-axis.");
@@ -82,7 +82,7 @@ int do_main() {
   config.contact_surface_representation = FLAGS_contact_surface_representation;
   auto [plant, scene_graph] = AddMultibodyPlant(config, &builder);
 
-  // Plant's parameters.
+  // Ball's parameters.
   const double radius = 0.05;   // m
   const double mass = 0.1;      // kg
   AddBallPlateBodies(
@@ -112,8 +112,9 @@ int do_main() {
       math::RigidTransformd{Vector3d(FLAGS_x0, 0.0, FLAGS_z0)});
   plant.SetFreeBodySpatialVelocity(
       &plant_context, plant.GetBodyByName("Ball"),
-      SpatialVelocity<double>{Vector3d(FLAGS_wx, FLAGS_wy, FLAGS_wz),
-                              Vector3d(FLAGS_vx, FLAGS_vy, FLAGS_vz)});
+      SpatialVelocity<double>{
+          M_PI / 180.0 * Vector3d(FLAGS_wx, FLAGS_wy, FLAGS_wz),
+          Vector3d(FLAGS_vx, FLAGS_vy, FLAGS_vz)});
 
   simulator->AdvanceTo(FLAGS_simulation_time);
   systems::PrintSimulatorStatistics(*simulator);
@@ -136,8 +137,6 @@ int main(int argc, char* argv[]) {
       "non-convex shapes accurately without resorting to their convex\n"
       "hulls. Launch drake-visualizer before running this example.\n"
       "See the README.md file for more information.\n");
-  FLAGS_simulator_accuracy = 0.05;
-  FLAGS_simulator_max_time_step = 0.1;
   FLAGS_simulator_publish_every_time_step = true;
   FLAGS_simulator_target_realtime_rate = 0.1;
   gflags::ParseCommandLineFlags(&argc, &argv, true);
