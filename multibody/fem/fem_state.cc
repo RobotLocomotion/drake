e884af834e2663bb3619e45ec050596d57b55517
Xuchen Han <xuchen.han@tri.global>
2022-02-24 11:41:10 -0800

    Address more Alejandro's feedback

--- multibody/fem/fem_state.cc
+++ multibody/fem/fem_state.cc
@@ -7,38 +7,52 @@ namespace fem {
 template <typename T>
 FemState<T>::FemState(const internal::FemStateManager<T>* manager)
     : manager_(manager) {
-  context_ = manager_->CreateDefaultContext();
+  DRAKE_DEMAND(manager != nullptr);
+  owned_context_ = manager_->CreateDefaultContext();
+}
+
+template <typename T>
+FemState<T>::FemState(const internal::FemStateManager<T>* manager,
+                      const systems::Context<T>* context)
+    : manager_(manager), context_(context) {
+  DRAKE_DEMAND(manager != nullptr);
+  DRAKE_DEMAND(context != nullptr);
 }
 
 template <typename T>
 const VectorX<T>& FemState<T>::GetPositions() const {
-  return context_->get_discrete_state(manager_->fem_position_index()).value();
+  return get_context()
+      .get_discrete_state(manager_->fem_position_index())
+      .value();
 }
 
 template <typename T>
 const VectorX<T>& FemState<T>::GetVelocities() const {
-  return context_->get_discrete_state(manager_->fem_velocity_index()).value();
+  return get_context()
+      .get_discrete_state(manager_->fem_velocity_index())
+      .value();
 }
 
 template <typename T>
 const VectorX<T>& FemState<T>::GetAccelerations() const {
-  return context_->get_discrete_state(manager_->fem_acceleration_index())
+  return get_context()
+      .get_discrete_state(manager_->fem_acceleration_index())
       .value();
 }
 
 template <typename T>
 void FemState<T>::SetPositions(const Eigen::Ref<const VectorX<T>>& q) {
-  context_->SetDiscreteState(manager_->fem_position_index(), q);
+  get_mutable_context().SetDiscreteState(manager_->fem_position_index(), q);
 }
 
 template <typename T>
 void FemState<T>::SetVelocities(const Eigen::Ref<const VectorX<T>>& v) {
-  context_->SetDiscreteState(manager_->fem_velocity_index(), v);
+  get_mutable_context().SetDiscreteState(manager_->fem_velocity_index(), v);
 }
 
 template <typename T>
 void FemState<T>::SetAccelerations(const Eigen::Ref<const VectorX<T>>& a) {
-  context_->SetDiscreteState(manager_->fem_acceleration_index(), a);
+  get_mutable_context().SetDiscreteState(manager_->fem_acceleration_index(), a);
 }
 
 }  // namespace fem
--- multibody/fem/fem_state_manager.h
+++ multibody/fem/fem_state_manager.h
@@ -20,43 +20,7 @@ class FemStateManager : public systems::LeafSystem<T> {
   FemStateManager(const VectorX<T>& model_q, const VectorX<T>& model_v,
                   const VectorX<T>& model_a);
 
-  /* Declares per-element data that depends on all states in this manager.
-   @param[in] num_elements       The number of elements/element data.
-   @param[in] calc_element_data  A callback that evaluates element data in each
-                                 FEM element using the given states.
-   @pre num_elements > 0.
-   @tparam Data  The data type of the per-element data. */
-  template <typename Data>
-  void DeclareElementData(
-      int num_elements,
-      std::function<void(const VectorX<T>& q, const VectorX<T>& v,
-                         const VectorX<T>& a, std::vector<Data>* element_data)>
-          calc_element_data) {
-    DRAKE_THROW_UNLESS(num_elements > 0);
-    element_data_index_ =
-        this->DeclareCacheEntry(
-                "FEM element data",
-                systems::ValueProducer(
-                    std::function<void(const systems::Context<T>&,
-                                       std::vector<Data>*)>{
-                        [this, num_elements, calc_element_data](
-                            const systems::Context<T>& context,
-                            std::vector<Data>* element_data) {
-                          DRAKE_DEMAND(element_data != nullptr);
-                          element_data->resize(num_elements);
-                          const VectorX<T>& q =
-                              context.get_discrete_state(q_index_).value();
-                          const VectorX<T>& v =
-                              context.get_discrete_state(v_index_).value();
-                          const VectorX<T>& a =
-                              context.get_discrete_state(a_index_).value();
-                          calc_element_data(q, v, a, element_data);
-                        }}),
-                {this->discrete_state_ticket(q_index_),
-                 this->discrete_state_ticket(v_index_),
-                 this->discrete_state_ticket(a_index_)})
-            .cache_index();
-  }
+  using systems::SystemBase::DeclareCacheEntry;
 
   /* Returns the discrete state index. */
   systems::DiscreteStateIndex fem_position_index() const { return q_index_; }
@@ -65,18 +29,10 @@ class FemStateManager : public systems::LeafSystem<T> {
     return a_index_;
   }
 
-  /* Returns the element data cache index.
-   @throws std::exception if DeclareElementData hasn't been called. */
-  systems::CacheIndex element_data_index() const {
-    DRAKE_THROW_UNLESS(element_data_index_.is_valid());
-    return element_data_index_;
-  }
-
  private:
   systems::DiscreteStateIndex q_index_;
   systems::DiscreteStateIndex v_index_;
   systems::DiscreteStateIndex a_index_;
-  systems::CacheIndex element_data_index_;
 };
 
 }  // namespace internal
