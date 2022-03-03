af571984db741ae3fd70e9e38ea04a311b3086b1
amcastro-tri <alejandro.castro@tri.global>
2022-03-03 16:39:02 -0500

    [SAP] Implements SapModel.

---
+++ multibody/contact_solvers/sap/sap_model.cc
@@ -0,0 +1,224 @@
+#include "drake/multibody/contact_solvers/sap/sap_model.h"
+
+#include <memory>
+#include <utility>
+
+#include "drake/common/default_scalars.h"
+#include "drake/multibody/contact_solvers/block_sparse_matrix.h"
+#include "drake/multibody/contact_solvers/sap/contact_problem_graph.h"
+
+namespace drake {
+namespace multibody {
+namespace contact_solvers {
+namespace internal {
+
+using systems::Context;
+
+template <typename T>
+SapModel<T>::SapModel(const SapContactProblem<T>* problem_ptr)
+    : problem_(problem_ptr) {
+  // Graph to the original contact problem, including all cliques
+  // (participating and non-participating).
+  const ContactProblemGraph& graph = problem().graph();
+
+  // Permutations to map indexes from participating cliques/dofs to the original
+  // set of cliques/dofs.
+  const PartialPermutation& cliques_permutation = graph.participating_cliques();
+  velocities_permutation_ = MakeParticipatingVelocitiesPermutation(problem());
+
+  // Extract momentum matrix for participating DOFs only.
+  const int num_participating_cliques =
+      cliques_permutation.permuted_domain_size();
+  A_.resize(num_participating_cliques);
+  cliques_permutation.Apply(problem().dynamics_matrix(), &A_);
+
+  // Get v* and p* for participating DOFs only.
+  const int nv_participating = velocities_permutation_.permuted_domain_size();
+  p_star_.resize(nv_participating);
+  v_star_.resize(nv_participating);
+  velocities_permutation_.Apply(problem().v_star(), &v_star_);
+  MultiplyByDynamicsMatrix(v_star_, &p_star_);
+
+  // TODO(amcastro-tri): Compute diagonal approximation of the Delassus
+  // operator. For now we set it to NaN so that if used by accident it'll
+  // trigger an easy trail to follow.
+  const VectorX<T> delassus_diagonal =
+      VectorX<T>::Constant(num_constraints(), NAN);
+
+  // Create constraints bundle.
+  constraints_bundle_ =
+      std::make_unique<SapConstraintBundle<T>>(&problem(), delassus_diagonal);
+
+  system_ = std::make_unique<SapModelSystem>(num_velocities());
+  DeclareCacheEntries();
+}
+
+template <typename T>
+void SapModel<T>::DeclareCacheEntries() {
+  const auto& constraint_velocities_cache_entry = system_->DeclareCacheEntry(
+      "Constraint velocities, vc = J⋅v.",
+      systems::ValueProducer(this, &SapModel<T>::CalcConstraintVelocities),
+      {systems::System<T>::xd_ticket()});
+  system_->mutable_cache_indexes().constraint_velocities =
+      constraint_velocities_cache_entry.cache_index();
+
+  const auto& momentum_gain_cache_entry = system_->DeclareCacheEntry(
+      "Momentum gain, momentum_gain = A⋅(v-v*).",
+      systems::ValueProducer(this, &SapModel<T>::CalcMomentumGain),
+      {systems::System<T>::xd_ticket()});
+  system_->mutable_cache_indexes().momentum_gain =
+      momentum_gain_cache_entry.cache_index();
+
+  // This cache entry is not costly to compute. Having this computation in the
+  // cache however elimianates heap allocation for temporaries.
+  const auto& velocity_gain_cache_entry = system_->DeclareCacheEntry(
+      "Velocity gain, velocity_gain = v-v*.",
+      systems::ValueProducer(this, &SapModel<T>::CalcVelocityGain),
+      {systems::System<T>::xd_ticket()});
+  system_->mutable_cache_indexes().velocity_gain =
+      velocity_gain_cache_entry.cache_index();
+
+  const auto& momentum_cost_cache_entry = system_->DeclareCacheEntry(
+      "Momentum cost, momentum_cost = 1/2⋅(v-v*)ᵀ⋅A⋅(v-v*).",
+      systems::ValueProducer(this, &SapModel<T>::CalcMomentumCost),
+      {system_->cache_entry_ticket(
+           system_->mutable_cache_indexes().velocity_gain),
+       system_->cache_entry_ticket(
+           system_->mutable_cache_indexes().momentum_gain)});
+  system_->mutable_cache_indexes().momentum_cost =
+      momentum_cost_cache_entry.cache_index();
+}
+
+template <typename T>
+std::unique_ptr<systems::Context<T>> SapModel<T>::MakeContext() const {
+  return system_->CreateDefaultContext();
+}
+
+template <typename T>
+const VectorX<T>& SapModel<T>::GetVelocities(const Context<T>& context) const {
+  system_->ValidateContext(context);
+  return context.get_discrete_state(system_->velocities_index()).value();
+}
+
+template <typename T>
+void SapModel<T>::SetVelocities(const VectorX<T>& v,
+                                Context<T>* context) const {
+  DRAKE_DEMAND(v.size() == num_velocities());
+  system_->ValidateContext(*context);
+  context->SetDiscreteState(system_->velocities_index(), v);
+}
+
+template <typename T>
+void SapModel<T>::CalcConstraintVelocities(const Context<T>& context,
+                                           VectorX<T>* vc) const {
+  system_->ValidateContext(context);
+  vc->resize(num_constraint_equations());
+  const VectorX<T>& v = GetVelocities(context);
+  constraints_bundle_->J().Multiply(v, vc);
+}
+
+template <typename T>
+void SapModel<T>::CalcVelocityGain(const Context<T>& context,
+                                   VectorX<T>* velocity_gain) const {
+  system_->ValidateContext(context);
+  velocity_gain->resize(num_velocities());
+  *velocity_gain = GetVelocities(context) - v_star();
+}
+
+template <typename T>
+void SapModel<T>::CalcMomentumGain(const Context<T>& context,
+                                   VectorX<T>* momentum_gain) const {
+  system_->ValidateContext(context);
+  momentum_gain->resize(num_velocities());
+  const VectorX<T>& velocity_gain = EvalVelocityGain(context);
+  MultiplyByDynamicsMatrix(velocity_gain, momentum_gain);
+}
+
+template <typename T>
+void SapModel<T>::CalcMomentumCost(const Context<T>& context,
+                                   T* momentum_cost) const {
+  system_->ValidateContext(context);
+  const VectorX<T>& velocity_gain = EvalVelocityGain(context);
+  const VectorX<T>& momentum_gain = EvalMomentumGain(context);
+  (*momentum_cost) = 0.5 * velocity_gain.dot(momentum_gain);
+}
+
+template <typename T>
+int SapModel<T>::num_cliques() const {
+  return problem().graph().participating_cliques().permuted_domain_size();
+}
+
+template <typename T>
+int SapModel<T>::num_velocities() const {
+  return velocities_permutation_.permuted_domain_size();
+}
+
+template <typename T>
+int SapModel<T>::num_constraints() const {
+  return problem().num_constraints();
+}
+
+template <typename T>
+int SapModel<T>::num_constraint_equations() const {
+  return problem().num_constraint_equations();
+}
+
+template <typename T>
+const std::vector<MatrixX<T>>& SapModel<T>::dynamics_matrix() const {
+  return A_;
+}
+
+template <typename T>
+const VectorX<T>& SapModel<T>::v_star() const {
+  return v_star_;
+}
+
+template <typename T>
+const VectorX<T>& SapModel<T>::p_star() const {
+  return p_star_;
+}
+
+template <typename T>
+PartialPermutation SapModel<T>::MakeParticipatingVelocitiesPermutation(
+    const SapContactProblem<T>& problem) const {
+  const PartialPermutation& cliques_permutation =
+      problem.graph().participating_cliques();
+  int v_first = 0;  // first velocity for a given clique.
+  int num_participating_velocities = 0;
+  std::vector<int> participating_velocities(problem.num_velocities(), -1);
+  for (int c = 0; c < problem.num_cliques(); ++c) {
+    const int nv = problem.num_velocities(c);
+    if (cliques_permutation.participates(c)) {
+      // Add participating dofs to the list.
+      for (int i = 0; i < nv; ++i) {
+        const int v = v_first + i;
+        const int v_participating = num_participating_velocities++;
+        participating_velocities[v] = v_participating;
+      }
+    }
+    v_first += nv;
+  }
+  return PartialPermutation(std::move(participating_velocities));
+}
+
+template <typename T>
+void SapModel<T>::MultiplyByDynamicsMatrix(const VectorX<T>& v,
+                                           VectorX<T>* p) const {
+  DRAKE_DEMAND(v.size() == num_velocities());
+  DRAKE_DEMAND(p->size() == num_velocities());
+  int clique_start = 0;
+  for (const auto& Ab : A_) {
+    const int clique_size = Ab.rows();
+    p->segment(clique_start, clique_size) =
+        Ab * v.segment(clique_start, clique_size);
+    clique_start += clique_size;
+  }
+}
+
+}  // namespace internal
+}  // namespace contact_solvers
+}  // namespace multibody
+}  // namespace drake
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
+    class ::drake::multibody::contact_solvers::internal::SapModel)
---
+++ multibody/contact_solvers/sap/sap_model.h
@@ -0,0 +1,184 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "drake/common/drake_copyable.h"
+#include "drake/multibody/contact_solvers/sap/partial_permutation.h"
+#include "drake/multibody/contact_solvers/sap/sap_constraint_bundle.h"
+#include "drake/multibody/contact_solvers/sap/sap_contact_problem.h"
+#include "drake/systems/framework/context.h"
+#include "drake/systems/framework/leaf_system.h"
+
+namespace drake {
+namespace multibody {
+namespace contact_solvers {
+namespace internal {
+
+/* This class represents the underlying computational model built by the SAP
+ solver given a SapContactProblem.
+ The model re-arranges constraints to exploit the block sparse structure of the
+ problem and it only includes participating cliques, that is, cliques that
+ connect to a cluster (edge) in the contact graph. The solution for
+ non-participating cliques is trivial (v=v*) and therefore they are excluded
+ from the main (expensive) SAP computation.
+
+ [Castro et al., 2021] Castro A., Permenter F. and Han X., 2021. An
+ Unconstrained Convex Formulation of Compliant Contact. Available at
+ https://arxiv.org/abs/2110.10107
+*/
+template <typename T>
+class SapModel {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(SapModel);
+
+  /* Constructs a model of `problem` optimized to be used by the SAP solver.
+   The input `problem` must outlive `this` model. */
+  explicit SapModel(const SapContactProblem<T>* problem);
+
+  /* Returns a reference to the contact problem being modeled by this class. */
+  const SapContactProblem<T>& problem() const {
+    DRAKE_ASSERT(problem_ != nullptr);
+    return *problem_;
+  }
+
+  /* Returns the number of (participating) cliques. */
+  int num_cliques() const;
+
+  /* Returns the number or (participating) generalized velocities.*/
+  int num_velocities() const;
+
+  int num_constraints() const;
+  int num_constraint_equations() const;
+
+  /* The time step of problem(). */
+  const T& time_step() const { return problem_->time_step(); }
+
+  /* Returns the system dynamics matrix A for participating velocities only. See
+   velocities_permutation(). */
+  const std::vector<MatrixX<T>>& dynamics_matrix() const;
+
+  /* Returns free-motion velocities v*, Participating velocities only. */
+  const VectorX<T>& v_star() const;
+
+  /* Returns free-motion generalized momenta. Participating momenta only. */
+  const VectorX<T>& p_star() const;
+
+  /* Makes a context to be used on queries with this model. */
+  std::unique_ptr<systems::Context<T>> MakeContext() const;
+
+  /* Retrieves the generalized velocities stored in `context`. Of size
+   num_velocities(). */
+  const VectorX<T>& GetVelocities(const systems::Context<T>& context) const;
+
+  /* Stores generalized velocities `v` in `context`.
+   @throws exception if v.size() is not num_velocities(). */
+  void SetVelocities(const VectorX<T>& v, systems::Context<T>* context) const;
+
+  /* Evaluates the constraint velocities vc = J⋅v. */
+  const VectorX<T>& EvalConstraintVelocities(
+      const systems::Context<T>& context) const {
+    return system_
+        ->get_cache_entry(system_->cache_indexes().constraint_velocities)
+        .template Eval<VectorX<T>>(context);
+  }
+
+  /* Evaluates the velocity gain defined as velocity_gain = v - v*. */
+  const VectorX<T>& EvalVelocityGain(const systems::Context<T>& context) const {
+    return system_->get_cache_entry(system_->cache_indexes().velocity_gain)
+        .template Eval<VectorX<T>>(context);
+  }
+
+  /* Evaluates the momentum gain defined as momentum_gain = A⋅(v - v*). */
+  const VectorX<T>& EvalMomentumGain(const systems::Context<T>& context) const {
+    return system_->get_cache_entry(system_->cache_indexes().momentum_gain)
+        .template Eval<VectorX<T>>(context);
+  }
+
+  /* Evaluates the momentum cost defined as
+   momentum_cost = 1/2⋅(v-v*)ᵀ⋅A⋅(v-v*). */
+  const T& EvalMomentumCost(const systems::Context<T>& context) const {
+    return system_->get_cache_entry(system_->cache_indexes().momentum_cost)
+        .template Eval<T>(context);
+  }
+
+ private:
+  /* System used to manage context resources. */
+  class SapModelSystem : public systems::LeafSystem<T> {
+   public:
+    /* Struct used to conglomerate the indexes of all cache entries declared by
+     the model. */
+    struct CacheIndexes {
+      systems::CacheIndex constraint_velocities;
+      systems::CacheIndex momentum_cost;
+      systems::CacheIndex momentum_gain;
+      systems::CacheIndex velocity_gain;
+    };
+
+    /* Constructs a system that declares a discrete state of size
+     num_velocities. */
+    explicit SapModelSystem(int num_velocities) {
+      velocities_index_ = DeclareDiscreteState(num_velocities);
+    }
+
+    /* Promote system methods so that SapModel can use them to declare state and
+     cache entries. */
+    using systems::LeafSystem<T>::DeclareDiscreteState;
+    using systems::SystemBase::DeclareCacheEntry;
+
+    /* Index of the discrete state declared by this system. */
+    systems::DiscreteStateIndex velocities_index() const {
+      return velocities_index_;
+    }
+
+    /* Accessors to cache indexes. */
+    const CacheIndexes& cache_indexes() const { return cache_indexes_; }
+    CacheIndexes& mutable_cache_indexes() { return cache_indexes_; }
+
+   private:
+    systems::DiscreteStateIndex velocities_index_;
+    CacheIndexes cache_indexes_;
+  };
+
+  void DeclareCacheEntries();
+
+  /* Makes a permutation to go back and forth between dofs in the original
+   contact problem and "participating dofs" in this model. */
+  PartialPermutation MakeParticipatingVelocitiesPermutation(
+      const SapContactProblem<T>& problem) const;
+
+  /* Performs multiplication p = A * v. Only participating velocities are
+   considered.
+   @pre p must be a valid pointer.
+   @pre both v and p must be of size num_participating_velocities(). */
+  void MultiplyByDynamicsMatrix(const VectorX<T>& v, VectorX<T>* p) const;
+
+  /* Calc methods to update cache entries. See the documentation for the
+  corresponding Eval methods for details. */
+  void CalcConstraintVelocities(const systems::Context<T>& context,
+                                VectorX<T>* vc) const;
+  void CalcVelocityGain(const systems::Context<T>& context,
+                        VectorX<T>* velocity_gain) const;
+  void CalcMomentumGain(const systems::Context<T>& context,
+                        VectorX<T>* momentum_gain) const;
+  void CalcMomentumCost(const systems::Context<T>& context,
+                        T* momentum_cost) const;
+
+  const SapContactProblem<T>* problem_{nullptr};
+  /* Permutation to map back and forth between DOFs in problem_ and DOFs in this
+   model. */
+  PartialPermutation velocities_permutation_;
+  /* Per-clique blocks of the momentum matrix. Only participating cliques. */
+  std::vector<MatrixX<T>> A_;
+  VectorX<T> v_star_;  // Free motion generalized velocity v*.
+  VectorX<T> p_star_;  // Free motion generalized impulse, i.e. p* = A⋅v*.
+  std::unique_ptr<SapConstraintBundle<T>> constraints_bundle_;
+
+  // System used to manage context resources.
+  std::unique_ptr<SapModelSystem> system_;
+};
+
+}  // namespace internal
+}  // namespace contact_solvers
+}  // namespace multibody
+}  // namespace drake
