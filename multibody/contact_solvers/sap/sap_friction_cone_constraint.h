7c0bda1ac8772ee4b4d776e1fecb0152a05330a4
amcastro-tri <alejandro.castro@tri.global>
2022-03-04 13:37:44 -0500

    Document the constraint class

--- multibody/contact_solvers/sap/sap_friction_cone_constraint.h
+++ multibody/contact_solvers/sap/sap_friction_cone_constraint.h
@@ -9,15 +9,63 @@ namespace multibody {
 namespace contact_solvers {
 namespace internal {
 
+/* Implements contact constraints for the SAP solver.
+ Here we provide a brief description of the contact forces modeled by this
+ constraint, enough to introduce notation and the constraint's parameters.
+ Please refer to [Castro et al., 2021] for an in-depth discussion.
+
+ Normal Compliance:
+  Contact constraints in the SAP formulation model a compliant normal impulse γₙ
+  according to:
+    γₙ/δt = (−k⋅ϕ−d⋅vₙ)₊
+  where δt is the time step used in the formulation, k is the contact stiffness
+  (in N/m), d is the dissipation (in N⋅s/m) and (x)₊ = max(0, x). ϕ and vₙ are
+  the next time step values of the signed distance function and normal velocity
+  respectively. ϕ is defined to be negative when bodies overlap and positive
+  otherwise. vₙ is defined to be positive when bodies move away from each other
+  and negative when they move towards each other. Dissipation is parameterized
+  as d = tau_d⋅k, where tau_d is the "dissipation time scale".
+
+ Regularized Friction:
+  SAP contact constraints regularize friction. That is, in stiction the
+  tangential contact forces obeys:
+    γₜ = −vₜ/Rₜ
+  where vₜ is the tangential velocity and Rₜ is the regularization parameter for
+  the tangential direction.
+  During sliding, the friction force obeys:
+    γₜ = −μγₙt̂
+  where t̂ = vₜ/‖vₜ‖.
+  Notice that:
+    1. The friction force always opposes the tangential velocity, satisfying the
+       principle of maximum dissipation.
+    2. It obeys Coulomb's law of friction, i.e. ‖γₜ‖ ≤ μγₙ.
+
+  Regularization of friction means that during stiction there might be a
+  residual non-zero slip velocity, even if small and negligible for a particular
+  application. [Castro et al., 2021] estimate this slip velocity vₛ to be in the
+  order of vₛ ≈ σ⋅δt⋅g, where g is the acceleration of gravity (≈9.81m/s² on
+  planet Earth) and σ is a dimensionless parameter used to parameterize the
+  regularization introduced by the constraint in a scale independent manner.
+  Typical values reside in the range σ ∈ (10⁻⁴,10⁻²).
+
+ [Castro et al., 2021] Castro A., Permenter F. and Han X., 2021. An
+   Unconstrained Convex Formulation of Compliant Contact. Available at
+   https://arxiv.org/abs/2110.10107
+
+ @tparam_nonsymbolic_scalar */
 template <typename T>
 class SapFrictionConeConstraint final : public SapConstraint<T> {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(SapFrictionConeConstraint);
 
-  // TODO: Simplify, get rid of struct Parameters.
+  /* Numerical parameters that define the constraint. Refer to this class's
+   documention for details. */
   struct Parameters {
+    /* Coefficient of friction, dimensionless. It must be non-negative. */
     T mu{0.0};
+    /* Contact stiffness, in N/m. It must be non-negative. */
     T stiffness{0.0};
+    /* Dissipation time scale, in seconds. It must be non-negative. */
     T dissipation_time_scale{0.0};
     // Rigid approximation constant: Rₙ = β²/(4π²)⋅w when the contact frequency
     // ωₙ is below the limit ωₙ⋅δt ≤ 2π. That is, the period is Tₙ = β⋅δt. w
