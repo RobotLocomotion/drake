82d46eec8e6427a6dc442128e80e3e4f3f5ce35e
Russ Tedrake <russt@mit.edu>
2022-03-09 09:10:10 -0500

    [systems/analysis] Deprecate InitialValueProblem OdeContext
    Previously, InitialValueProblem and its wrappers
    (ScalarInitialValueProblem, AntiderivativeFunction) all invented new
    structures called __Context to accept their arguments.  But these
    structures do not provide the Context API.  Furthermore, they are
    trivially small wrappers which aren't needed and required a bunch of
    error checking.
    
    This has always bugged me, but I decided to fix it now before adding
    python bindings for InitialValueProblem and friends -- I'd rather not
    let the OdeContext, etc, sneak into the pydrake API if I planned to
    deprecate it eventually.
    
    Discussed previously in #12857.

--- systems/analysis/scalar_initial_value_problem.cc
+++ systems/analysis/scalar_initial_value_problem.cc
@@ -1,4 +1,43 @@
 #include "drake/systems/analysis/scalar_initial_value_problem.h"
 
+namespace drake {
+namespace systems {
+
+template <typename T>
+ScalarInitialValueProblem<T>::ScalarInitialValueProblem(
+    const ScalarOdeFunction& scalar_ode_function, const T& x0,
+    const Eigen::Ref<const VectorX<T>>& k) {
+  // Wraps the given scalar ODE function as a vector ODE function.
+  typename InitialValueProblem<T>::OdeFunction ode_function =
+      [scalar_ode_function](const T& t, const VectorX<T>& x,
+                            const VectorX<T>& params) -> VectorX<T> {
+    return VectorX<T>::Constant(1, scalar_ode_function(t, x[0], params));
+  };
+  // Instantiates the vector initial value problem.
+  vector_ivp_ =
+      std::make_unique<InitialValueProblem<T>>(ode_function, Vector1<T>{x0}, k);
+}
+
+template <typename T>
+T ScalarInitialValueProblem<T>::Solve(const T& t0, const T& tf) const {
+  return this->vector_ivp_->Solve(t0, tf)[0];
+}
+
+template <typename T>
+std::unique_ptr<ScalarDenseOutput<T>> ScalarInitialValueProblem<T>::DenseSolve(
+    const T& t0, const T& tf) const {
+  // Delegates request to the vector form of this IVP by putting
+  // specified values in vector form and the resulting dense output
+  // back into scalar form.
+  const int kDimension = 0;
+  std::unique_ptr<DenseOutput<T>> vector_dense_output =
+      this->vector_ivp_->DenseSolve(t0, tf);
+  return std::make_unique<ScalarViewDenseOutput<T>>(
+      std::move(vector_dense_output), kDimension);
+}
+
+}  // namespace systems
+}  // namespace drake
+
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
     class drake::systems::ScalarInitialValueProblem)
--- systems/analysis/test/initial_value_problem_test.cc
+++ systems/analysis/test/initial_value_problem_test.cc
@@ -23,6 +23,227 @@ GTEST_TEST(InitialValueProblemTest, SolutionUsingMultipleIntegrators) {
   const double kAccuracy = 1e-2;
 
   // The initial time t₀, for IVP definition.
+  const double kInitialTime = 0.0;
+  // The initial state 𝐱₀, for IVP definition.
+  const VectorX<double> kInitialState = VectorX<double>::Zero(2);
+  // The default parameters 𝐤₀, for IVP definition.
+  const VectorX<double> kParameters = VectorX<double>::Constant(2, 1.0);
+
+  // Instantiates a generic IVP for test purposes only,
+  // using a generic ODE d𝐱/dt = -𝐱 + 𝐤, that does not
+  // model (nor attempts to model) any physical process.
+  InitialValueProblem<double> ivp(
+      [](const double& t, const VectorX<double>& x,
+         const VectorX<double>& k) -> VectorX<double> {
+        unused(t);
+        return -x + k;
+      }, kInitialState, kParameters);
+
+  // Testing against closed form solution of above's IVP, which can be written
+  // as 𝐱(t; 𝐤) = 𝐤 + (𝐱₀ - 𝐤) * e^(-(t - t₀)).
+  const double t0 = kInitialTime;
+  const VectorX<double>& x0 = kInitialState;
+  const VectorX<double>& k1 = kParameters;
+
+  const double t1 = kInitialTime + 1.0;
+  EXPECT_TRUE(CompareMatrices(
+      ivp.Solve(t0, t1), k1 + (x0 - k1) * std::exp(-(t1 - t0)), kAccuracy));
+
+  // Replaces default integrator.
+  const double kMaximumStep = 0.1;
+  const IntegratorBase<double>& default_integrator = ivp.get_integrator();
+  IntegratorBase<double>* configured_integrator =
+      ivp.reset_integrator<RungeKutta2Integrator<double>>(kMaximumStep);
+  EXPECT_NE(configured_integrator, &default_integrator);
+  EXPECT_EQ(configured_integrator, &ivp.get_integrator());
+
+  const double t2 = kInitialTime + 0.3;
+  // Testing against closed form solution of above's IVP, which can be written
+  // as 𝐱(t; 𝐤) = 𝐤 + (𝐱₀ - 𝐤) * e^(-(t - t₀)).
+  EXPECT_TRUE(CompareMatrices(
+      ivp.Solve(t0, t2), k1 + (x0 - k1) * std::exp(-(t2 - t0)), kAccuracy));
+}
+
+// Parameterized fixture for testing accuracy of IVP solutions.
+class InitialValueProblemAccuracyTest
+    : public ::testing::TestWithParam<double> {
+ protected:
+  void SetUp() {
+    integration_accuracy_ = GetParam();
+  }
+
+  // Expected accuracy for numerical integral
+  // evaluation in the relative tolerance sense.
+  double integration_accuracy_{0.};
+};
+
+// Accuracy test of the solution for the momentum 𝐩 of a particle
+// with mass m travelling through a gas with dynamic viscosity μ,
+// where d𝐩/dt = -μ * 𝐩/m and 𝐩(t₀; [m, μ]) = 𝐩₀.
+TEST_P(InitialValueProblemAccuracyTest, ParticleInAGasMomentum) {
+  // The initial time t₀.
+  const double kInitialTime = 0.0;
+  // The initial momentum 𝐩₀ of the particle at time t₀.
+  const VectorX<double> kInitialParticleMomentum = (
+      VectorX<double>(3) << -3.0, 1.0, 2.0).finished();
+
+  const double kLowestGasViscosity = 1.0;
+  const double kHighestGasViscosity = 10.0;
+  const double kGasViscosityStep = 1.0;
+
+  const double kLowestParticleMass = 1.0;
+  const double kHighestParticleMass = 10.0;
+  const double kParticleMassStep = 1.0;
+
+  const double kTotalTime = 1.0;
+  const double kTimeStep = 0.1;
+
+  const double t0 = kInitialTime;
+  const double tf = kTotalTime;
+  const VectorX<double>& p0 = kInitialParticleMomentum;
+  for (double mu = kLowestGasViscosity; mu <= kHighestGasViscosity;
+       mu += kGasViscosityStep) {
+    for (double m = kLowestParticleMass; m <= kHighestParticleMass;
+         m += kParticleMassStep) {
+      const Eigen::Vector2d kParameters{mu, m};
+      // Instantiates the particle momentum IVP.
+      InitialValueProblem<double> particle_momentum_ivp(
+          [](const double& t, const VectorX<double>& p,
+             const VectorX<double>& k) -> VectorX<double> {
+            const double mu_ = k[0];
+            const double m_ = k[1];
+            return -mu_ * p / m_;
+          },
+          kInitialParticleMomentum, kParameters);
+
+      IntegratorBase<double>& inner_integrator =
+          particle_momentum_ivp.get_mutable_integrator();
+      inner_integrator.set_target_accuracy(integration_accuracy_);
+
+      const std::unique_ptr<DenseOutput<double>> particle_momentum_approx =
+          particle_momentum_ivp.DenseSolve(t0, tf);
+
+      for (double t = kInitialTime; t <= kTotalTime; t += kTimeStep) {
+        // Tests are performed against the closed form
+        // solution for the IVP described above, which is
+        // 𝐩(t; [μ, m]) = 𝐩₀ * e^(-μ * (t - t₀) / m).
+        const VectorX<double> solution = p0 * std::exp(-mu * (t - t0) / m);
+
+        EXPECT_TRUE(CompareMatrices(particle_momentum_ivp.Solve(t0, t),
+                                    solution, integration_accuracy_))
+            << "Failure solving d𝐩/dt = -μ * 𝐩/m"
+            << " using 𝐩(" << t0 << "; [μ, m]) = " << p0 << " for t = " << t
+            << ", μ = " << mu << " and m = " << m << " to an accuracy of "
+            << integration_accuracy_;
+
+        EXPECT_TRUE(CompareMatrices(particle_momentum_approx->Evaluate(t),
+                                    solution, integration_accuracy_))
+            << "Failure approximating the solution for d𝐩/dt = -μ * 𝐩/m"
+            << " using 𝐩(" << t0 << "; [μ, m]) = " << p0 << " for t = " << t
+            << ", μ = " << mu << " and m = " << m << " to an accuracy of "
+            << integration_accuracy_ << " with solver's continuous extension.";
+      }
+    }
+  }
+}
+
+// Accuracy test of the solution for the velocity 𝐯 of a particle
+// with mass m travelling through a gas with dynamic viscosity μ
+// and being pushed by a constant force 𝐅, where
+// d𝐯/dt = (𝐅 - μ * 𝐯) / m and 𝐯(t₀; [m, μ]) = 𝐯₀.
+TEST_P(InitialValueProblemAccuracyTest, ParticleInAGasForcedVelocity) {
+  // The initial time t₀.
+  const double kInitialTime = 0.0;
+  // The initial velocity 𝐯₀ of the particle at time t₀.
+  const VectorX<double> kInitialParticleVelocity = VectorX<double>::Unit(3, 0);
+  // The force 𝐅 pushing the particle.
+  const VectorX<double> kPushingForce = VectorX<double>::Unit(3, 1);
+
+  // The mass m of the particle and the dynamic viscosity μ of the gas.
+  const double kLowestGasViscosity = 1.0;
+  const double kHighestGasViscosity = 10.0;
+  const double kGasViscosityStep = 1.0;
+
+  const double kLowestParticleMass = 1.0;
+  const double kHighestParticleMass = 10.0;
+  const double kParticleMassStep = 1.0;
+
+  const double kTotalTime = 1.0;
+  const double kTimeStep = 0.1;
+
+  const double t0 = kInitialTime;
+  const double tf = kTotalTime;
+
+  const VectorX<double>& F = kPushingForce;
+  const VectorX<double>& v0 = kInitialParticleVelocity;
+
+  for (double mu = kLowestGasViscosity; mu <= kHighestGasViscosity;
+       mu += kGasViscosityStep) {
+    for (double m = kLowestParticleMass; m <= kHighestParticleMass;
+         m += kParticleMassStep) {
+      const Eigen::Vector2d kParameters{mu, m};
+
+      // Instantiates the particle velocity IVP.
+      InitialValueProblem<double> particle_velocity_ivp(
+          [&kPushingForce](const double& t, const VectorX<double>& v,
+                           const VectorX<double>& k) -> VectorX<double> {
+            const double mu_ = k[0];
+            const double m_ = k[1];
+            const VectorX<double>& F_ = kPushingForce;
+            return (F_ - mu_ * v) / m_;
+          },
+          kInitialParticleVelocity, kParameters);
+
+      IntegratorBase<double>& inner_integrator =
+          particle_velocity_ivp.get_mutable_integrator();
+      inner_integrator.set_target_accuracy(integration_accuracy_);
+
+      const std::unique_ptr<DenseOutput<double>> particle_velocity_approx =
+          particle_velocity_ivp.DenseSolve(t0, tf);
+
+      for (double t = kInitialTime; t <= kTotalTime; t += kTimeStep) {
+        // Tests are performed against the closed form
+        // solution for the IVP described above, which is
+        // 𝐯(t; [μ, m]) = 𝐯₀ * e^(-μ * (t - t₀) / m) +
+        //                𝐅 / μ * (1 - e^(-μ * (t - t₀) / m))
+        // with 𝐅 = (0., 1., 0.).
+        const VectorX<double> solution =
+            v0 * std::exp(-mu * (t - t0) / m) +
+            F / mu * (1. - std::exp(-mu * (t - t0) / m));
+        EXPECT_TRUE(CompareMatrices(particle_velocity_ivp.Solve(t0, t),
+                                    solution, integration_accuracy_))
+            << "Failure solving d𝐯/dt = (-μ * 𝐯 + 𝐅) / m"
+            << " using 𝐯(" << t0 << "; [μ, m]) = " << v0 << " for t = " << t
+            << ", μ = " << mu << ", m = " << m << "and 𝐅 = " << F
+            << " to an accuracy of " << integration_accuracy_;
+
+        EXPECT_TRUE(CompareMatrices(particle_velocity_approx->Evaluate(t),
+                                    solution, integration_accuracy_))
+            << "Failure approximating the solution for "
+            << "d𝐯/dt = (-μ * 𝐯 + 𝐅) / m using 𝐯(" << t0 << "; [μ, m]) = " << v0
+            << " for t = " << t << ", μ = " << mu << ", m = " << m
+            << "and 𝐅 = " << F << " to an accuracy of " << integration_accuracy_
+            << " with solver's continuous extension.";
+      }
+    }
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(IncreasingAccuracyInitialValueProblemTests,
+                         InitialValueProblemAccuracyTest,
+                         ::testing::Values(1e-1, 1e-2, 1e-3, 1e-4, 1e-5));
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
+// Checks IVP solver usage with multiple integrators.
+GTEST_TEST(DeprecatedInitialValueProblemTest,
+           SolutionUsingMultipleIntegrators) {
+  // Accuracy upper bound, as not all the integrators used below support
+  // error control.
+  const double kAccuracy = 1e-2;
+
+  // The initial time t₀, for IVP definition.
   const double kDefaultInitialTime = 0.0;
   // The initial state 𝐱₀, for IVP definition.
   const VectorX<double> kDefaultInitialState = VectorX<double>::Zero(2);
@@ -74,7 +295,8 @@ GTEST_TEST(InitialValueProblemTest, SolutionUsingMultipleIntegrators) {
 }
 
 // Validates preconditions when constructing any given IVP.
-GTEST_TEST(InitialValueProblemTest, ConstructionPreconditionsValidation) {
+GTEST_TEST(DeprecatedInitialValueProblemTest,
+           ConstructionPreconditionsValidation) {
   // Defines a generic ODE d𝐱/dt = -𝐱 + 𝐤, that does not
   // model (nor attempts to model) any physical process.
   const InitialValueProblem<double>::OdeFunction dummy_ode_function =
@@ -120,7 +342,8 @@ GTEST_TEST(InitialValueProblemTest, ConstructionPreconditionsValidation) {
 }
 
 // Validates preconditions when solving any given IVP.
-GTEST_TEST(InitialValueProblemTest, ComputationPreconditionsValidation) {
+GTEST_TEST(DeprecatedInitialValueProblemTest,
+           ComputationPreconditionsValidation) {
   // The initial time t₀, for IVP definition.
   const double kDefaultInitialTime = 0.0;
   // The initial state 𝐱₀, for IVP definition.
@@ -225,7 +448,7 @@ GTEST_TEST(InitialValueProblemTest, ComputationPreconditionsValidation) {
 }
 
 // Parameterized fixture for testing accuracy of IVP solutions.
-class InitialValueProblemAccuracyTest
+class DeprecatedInitialValueProblemAccuracyTest
     : public ::testing::TestWithParam<double> {
  protected:
   void SetUp() {
@@ -240,7 +463,7 @@ class InitialValueProblemAccuracyTest
 // Accuracy test of the solution for the momentum 𝐩 of a particle
 // with mass m travelling through a gas with dynamic viscosity μ,
 // where d𝐩/dt = -μ * 𝐩/m and 𝐩(t₀; [m, μ]) = 𝐩₀.
-TEST_P(InitialValueProblemAccuracyTest, ParticleInAGasMomentum) {
+TEST_P(DeprecatedInitialValueProblemAccuracyTest, ParticleInAGasMomentum) {
   // The initial time t₀.
   const double kInitialTime = 0.0;
   // The initial momentum 𝐩₀ of the particle at time t₀.
@@ -324,7 +547,8 @@ TEST_P(InitialValueProblemAccuracyTest, ParticleInAGasMomentum) {
 // with mass m travelling through a gas with dynamic viscosity μ
 // and being pushed by a constant force 𝐅, where
 // d𝐯/dt = (𝐅 - μ * 𝐯) / m and 𝐯(t₀; [m, μ]) = 𝐯₀.
-TEST_P(InitialValueProblemAccuracyTest, ParticleInAGasForcedVelocity) {
+TEST_P(DeprecatedInitialValueProblemAccuracyTest,
+       ParticleInAGasForcedVelocity) {
   // The initial time t₀.
   const double kInitialTime = 0.0;
   // The initial velocity 𝐯₀ of the particle at time t₀.
@@ -394,9 +618,8 @@ TEST_P(InitialValueProblemAccuracyTest, ParticleInAGasForcedVelocity) {
         EXPECT_TRUE(CompareMatrices(particle_velocity_ivp.Solve(t, values),
                                     solution, integration_accuracy_))
             << "Failure solving d𝐯/dt = (-μ * 𝐯 + 𝐅) / m"
-            << " using 𝐯(" << t0 << "; [μ, m]) = " << v0
-            << " for t = " << t << ", μ = " << mu
-            << ", m = " << m << "and 𝐅 = " << F
+            << " using 𝐯(" << t0 << "; [μ, m]) = " << v0 << " for t = " << t
+            << ", μ = " << mu << ", m = " << m << "and 𝐅 = " << F
             << " to an accuracy of " << integration_accuracy_;
 
         EXPECT_TRUE(CompareMatrices(particle_velocity_approx->Evaluate(t),
@@ -411,9 +634,11 @@ TEST_P(InitialValueProblemAccuracyTest, ParticleInAGasForcedVelocity) {
   }
 }
 
-INSTANTIATE_TEST_SUITE_P(IncreasingAccuracyInitialValueProblemTests,
-                        InitialValueProblemAccuracyTest,
-                        ::testing::Values(1e-1, 1e-2, 1e-3, 1e-4, 1e-5));
+INSTANTIATE_TEST_SUITE_P(DeprecatedInitialValueProblemAccuracyTest,
+                         DeprecatedInitialValueProblemAccuracyTest,
+                         ::testing::Values(1e-1, 1e-2, 1e-3, 1e-4, 1e-5));
+
+#pragma GCC diagnostic pop
 
 }  // namespace
 }  // namespace analysis
