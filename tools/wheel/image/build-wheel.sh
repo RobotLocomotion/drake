b7ea64a05cf24c2c35805b04ff72e432f98757c8
Matthew Woehlke <matthew.woehlke@kitware.com>
2022-03-24 10:20:53 -0400

    Add macOS wheel build scripts
    Create a script to build a wheel on macOS. Update various Bazel logic to
    support the 'macos_wheel' platform. Add scripts to mangle library load
    paths on macOS. Update inner wheel build script to support macOS as well
    as manylinux. Update various inner build scripts to support a build root
    other than '/', since macOS does not permit creation of arbitrary
    top-level directories.
    
    Note that the wheel build requires that the user running the scripts is
    able to use 'brew', and has write access to '/opt'.

--- tools/wheel/image/build-wheel.sh
+++ tools/wheel/image/build-wheel.sh
@@ -1,41 +1,59 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -eu -o pipefail
 
+# Use GNU 'cp' on macOS so we have a consistent CLI.
+if [ "$(uname)" == "Darwin" ]; then
+    cp()
+    {
+        gcp "$@"
+    }
+fi
+
+# Helper function to change the RPATH of libraries. The first argument is the
+# (origin-relative) new RPATH to be added. Remaining arguments are libraries to
+# be modified. Note that existing RPATH(s) are removed.
 chrpath()
 {
     rpath=$1
     shift 1
 
     for lib in "$@"; do
-        patchelf --remove-rpath "$lib"
-        patchelf --set-rpath "$rpath" "$lib"
+        if [ "$(uname)" == "Linux" ]; then
+            patchelf --remove-rpath "$lib"
+            patchelf --set-rpath "\$ORIGIN/$rpath" "$lib"
+        else
+            strip_rpath "$lib"
+            install_name_tool -add_rpath "@loader_path/$rpath" "$lib"
+        fi
     done
 }
 
-# TODO(mwoehlke-kitware) Most of this should move to Bazel.
-mkdir -p /wheel/drake
-mkdir -p /wheel/pydrake/lib
-mkdir -p /wheel/pydrake/share/drake
-cd /wheel
+###############################################################################
 
-cp -r -t /wheel/drake \
+# TODO(mwoehlke-kitware) Most of this should move to Bazel.
+mkdir -p ${DRAKE_WHEELBUILD_PREFIX}/wheel/drake
+mkdir -p ${DRAKE_WHEELBUILD_PREFIX}/wheel/pydrake/lib
+mkdir -p ${DRAKE_WHEELBUILD_PREFIX}/wheel/pydrake/share/drake
+cd ${DRAKE_WHEELBUILD_PREFIX}/wheel
+
+cp -r -t ${DRAKE_WHEELBUILD_PREFIX}/wheel/drake \
     /opt/drake/lib/python*/site-packages/drake/*
 
-cp -r -t /wheel/pydrake \
+cp -r -t ${DRAKE_WHEELBUILD_PREFIX}/wheel/pydrake \
     /opt/drake/share/doc \
     /opt/drake/lib/python*/site-packages/pydrake/*
 
-cp -r -t /wheel/pydrake/lib \
+cp -r -t ${DRAKE_WHEELBUILD_PREFIX}/wheel/pydrake/lib \
     /opt/drake/lib/libdrake*.so
 
 # NOTE: build-vtk.sh also puts licenses in /opt/drake-dependencies/licenses.
-cp -r -t /wheel/pydrake/doc \
+cp -r -t ${DRAKE_WHEELBUILD_PREFIX}/wheel/pydrake/doc \
     /opt/drake-dependencies/licenses/*
 
 # TODO(mwoehlke-kitware) We need a different way of shipping non-arch files
 # (examples, models).
-cp -r -t /wheel/pydrake/share/drake \
+cp -r -t ${DRAKE_WHEELBUILD_PREFIX}/wheel/pydrake/share/drake \
     /opt/drake/share/drake/.drake-find_resource-sentinel \
     /opt/drake/share/drake/package.xml \
     /opt/drake/share/drake/examples \
@@ -43,23 +61,35 @@ cp -r -t /wheel/pydrake/share/drake \
     /opt/drake/share/drake/manipulation \
     /opt/drake/share/drake/tutorials
 
-mkdir -p /wheel/pydrake/share/drake/setup
-cp -r -t /wheel/pydrake/share/drake/setup \
-    /opt/drake/share/drake/setup/deepnote
+if [ "$(uname)" == "Linux" ]; then
+    mkdir -p ${DRAKE_WHEELBUILD_PREFIX}/wheel/pydrake/share/drake/setup
+    cp -r -t ${DRAKE_WHEELBUILD_PREFIX}/wheel/pydrake/share/drake/setup \
+        /opt/drake/share/drake/setup/deepnote
+fi
 
 # TODO(mwoehlke-kitware) We need to remove these to keep the wheel from being
 # too large, but (per above), the whole of share/drake shouldn't be in the
 # wheel.
-rm /wheel/pydrake/share/drake/manipulation/models/ycb/meshes/*.png
-rm -r /wheel/pydrake/share/drake/examples/atlas
+rm -rf \
+    ${DRAKE_WHEELBUILD_PREFIX}/wheel/pydrake/share/drake/manipulation/models/ycb/meshes/*.png \
+    ${DRAKE_WHEELBUILD_PREFIX}/wheel/pydrake/share/drake/examples/atlas
 
-export LD_LIBRARY_PATH=/wheel/pydrake/lib:/opt/drake-dependencies/lib
+if [ "$(uname)" == "Linux" ]; then
+    export LD_LIBRARY_PATH=${DRAKE_WHEELBUILD_PREFIX}/wheel/pydrake/lib:/opt/drake-dependencies/lib
+fi
 
-chrpath '$ORIGIN/lib' pydrake/*.so
-chrpath '$ORIGIN/../lib' pydrake/*/*.so
+chrpath lib pydrake/*.so
+chrpath ../lib pydrake/*/*.so
+
+if [ "$(uname)" == "Darwin" ]; then
+    change_lpath '@loader_path/../../../' '@rpath/' pydrake/*.so
+    change_lpath '@loader_path/../../../../' '@rpath/' pydrake/*/*.so
+fi
 
 python setup.py bdist_wheel
 
-GLIBC_VERSION=$(ldd --version | sed -n '1{s/.* //;s/[.]/_/p}')
+if [ "$(uname)" == "Linux" ]; then
+    GLIBC_VERSION=$(ldd --version | sed -n '1{s/.* //;s/[.]/_/p}')
 
-auditwheel repair --plat manylinux_${GLIBC_VERSION}_x86_64 dist/drake*.whl
+    auditwheel repair --plat manylinux_${GLIBC_VERSION}_x86_64 dist/drake*.whl
+fi
