53dea10d7c5250aa95ba9097f0942df8964ddbc1
Rick Poyner <rick.poyner@tri.global>
2022-03-31 17:23:25 -0400

    [parsing] Plumb the last URDF error to diagnostic policy
    Relevant to: #16782
    
    Based on my possibly flawed static analysis, this patch converts the
    last message reachable from `AddModelFromUrdf()` via bad input.
    
    Since this one routine is used many places, a few small changes require
    a lot of new test cases.

--- multibody/parsing/detail_tinyxml.h
+++ multibody/parsing/detail_tinyxml.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <optional>
 #include <string>
 #include <vector>
 
 #include <Eigen/Dense>
 #include <tinyxml2.h>
 
+#include "drake/common/diagnostic_policy.h"
 #include "drake/math/rigid_transform.h"
 
 namespace drake {
@@ -25,12 +27,20 @@ bool ParseStringAttribute(const tinyxml2::XMLElement* node,
 
 // Parses a scalar attribute of @p node named @p attribute_name into @p val.
 //
-// @returns false if the attribute is not present
+// @returns false if the attribute is not present, or if no numeric values were
+// found.
 //
-// @throws std::exception if the attribute doesn't contain a
-// single numeric value.
-bool ParseScalarAttribute(const tinyxml2::XMLElement* node,
-                          const char* attribute_name, double* val);
+// Emits an error on @p policy if the attribute doesn't contain exactly one
+// numeric value. If the error policy does not throw, then the function will
+// return true and set @p val to the first value obtained, if present.
+//
+// If @p policy is empty, then the default policy will be used, which results
+// in a `throw` on error.
+bool ParseScalarAttribute(
+    const tinyxml2::XMLElement* node,
+    const char* attribute_name, double* val,
+    std::optional<const drake::internal::DiagnosticPolicy> policy =
+    std::nullopt);
 
 // Parses an attribute of @p node named @p attribute_name consisting of scalar
 // values into @p val.
