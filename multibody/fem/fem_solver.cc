16ebcf2a133849ca0062a52e94a6ed0d6600f15c
Xuchen Han <xuchen.han@tri.global>
2022-03-31 17:34:19 -0700

    [fem] Add FemSolver

---
+++ multibody/fem/fem_solver.cc
@@ -0,0 +1,99 @@
+#include "drake/multibody/fem/fem_solver.h"
+
+#include <algorithm>
+
+namespace drake {
+namespace multibody {
+namespace fem {
+namespace internal {
+
+template <typename T>
+FemSolver<T>::FemSolver(const FemModel<T>* model,
+                        const DiscreteTimeIntegrator<T>* integrator)
+    : model_(model), integrator_(integrator), scratch_data_(*model_) {
+  DRAKE_DEMAND(model_ != nullptr);
+  DRAKE_DEMAND(integrator_ != nullptr);
+}
+
+template <typename T>
+int FemSolver<T>::AdvanceOneTimeStep(const FemState<T>& prev_state,
+                                     FemState<T>* next_state) const {
+  DRAKE_DEMAND(next_state != nullptr);
+  model_->ThrowIfModelStateIncompatible(__func__, prev_state);
+  model_->ThrowIfModelStateIncompatible(__func__, *next_state);
+  const VectorX<T>& unknown_variable = integrator_->GetUnknowns(prev_state);
+  integrator_->AdvanceOneTimeStep(prev_state, unknown_variable, next_state);
+  /* Run Newton-Raphson iterations. */
+  return SolveWithInitialGuess(next_state);
+}
+
+template <typename T>
+void FemSolver<T>::set_linear_solve_tolerance(const T& residual_norm) const {
+  /* The relative tolerance when solving for A * dz = -b, where A is the tangent
+   matrix. We set it to be on the order of the residual norm to achieve local
+   second order convergence [Nocedal and Wright, section 7.1]. We set it to be
+   smaller than or equal to the relative tolerance to ensure that linear models
+   converge in exact one Newton iteration.
+
+   [Nocedal and Wright] Nocedal, J., & Wright, S. (2006). Numerical
+   optimization. Springer Science & Business Media. */
+  double linear_solve_tolerance =
+      std::min({0.5, relative_tolerance_, ExtractDoubleOrThrow(residual_norm)});
+  DRAKE_DEMAND(scratch_data_.tangent_matrix != nullptr);
+  scratch_data_.tangent_matrix->set_relative_tolerance(linear_solve_tolerance);
+}
+
+template <typename T>
+int FemSolver<T>::SolveWithInitialGuess(FemState<T>* state) const {
+  /* Make sure the scratch quantities are of the correct sizes. */
+  ResetScratchDataIfNecessary();
+  model_->ApplyBoundaryCondition(state);
+  model_->CalcResidual(*state, &scratch_data_.b);
+  T residual_norm = scratch_data_.b.norm();
+  T initial_residual_norm = residual_norm;
+  int iter = 0;
+  /* Newton-Raphson iterations. We iterate until any of the following is true:
+   1. The max number of allowed iterations is reached;
+   2. The norm of the residual is smaller than the absolute tolerance.
+   3. The relative error (the norm of the residual divided by the norm of the
+      initial residual) is smaller than the unitless relative tolerance. */
+  do {
+    set_linear_solve_tolerance(residual_norm);
+    model_->CalcTangentMatrix(*state, integrator_->GetWeights(),
+                              scratch_data_.tangent_matrix.get());
+    scratch_data_.tangent_matrix->AssembleIfNecessary();
+    /* Solve for A * dz = -b, where A is the tangent matrix. */
+    // TODO(xuchenhan-tri): PetscSymmetricBlockSparseMatrix should have a way of
+    // communicating solver failure.
+    scratch_data_.dz = scratch_data_.tangent_matrix->Solve(
+        internal::PetscSymmetricBlockSparseMatrix::SolverType::
+            kConjugateGradient,
+        internal::PetscSymmetricBlockSparseMatrix::PreconditionerType::
+            kIncompleteCholesky,
+        -scratch_data_.b);
+    integrator_->UpdateStateFromChangeInUnknowns(scratch_data_.dz, state);
+    model_->CalcResidual(*state, &scratch_data_.b);
+    residual_norm = scratch_data_.b.norm();
+    ++iter;
+  } while (iter < kMaxIterations_ &&
+           residual_norm > relative_tolerance_ * initial_residual_norm &&
+           residual_norm > absolute_tolerance_);
+  if (iter == kMaxIterations_) {
+    return -1;
+  }
+  return iter;
+}
+
+template <typename T>
+void FemSolver<T>::ResetScratchDataIfNecessary() const {
+  DRAKE_DEMAND(model_ != nullptr);
+  if (scratch_data_.num_dofs() != model_->num_dofs())
+    scratch_data_.Resize(*model_);
+}
+
+}  // namespace internal
+}  // namespace fem
+}  // namespace multibody
+}  // namespace drake
+
+template class drake::multibody::fem::internal::FemSolver<double>;
---
+++ multibody/fem/fem_solver.h
@@ -0,0 +1,144 @@
+#pragma once
+
+#include <memory>
+
+#include "drake/common/eigen_types.h"
+#include "drake/multibody/fem/discrete_time_integrator.h"
+#include "drake/multibody/fem/fem_model.h"
+#include "drake/multibody/fem/fem_state.h"
+
+namespace drake {
+namespace multibody {
+namespace fem {
+namespace internal {
+
+/* FemSolver solves discrete dynamic elasticity problems. The governing PDE of
+ the dynamics is spatially discretized in FemModel and temporally discretized by
+ DiscreteTimeIntegrator. FemSolver provides the `AdvanceOneTimeStep()` function
+ that advances the free-motion states (i.e. without considering contacts or
+ constraints) of the spatially discretized FEM model by one time step according
+ to the prescribed discrete time integration scheme using a Newton-Raphson
+ solver.
+ @tparam_double_only */
+template <typename T>
+class FemSolver {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(FemSolver);
+
+  /* Constructs a new FemSolver that solves the given `model` with the
+   `integrator` provided to advance time.
+   @note The `model` and `integrator` pointers persist in `this` FemSolver and
+   thus the model and the integrator must outlive this solver.
+   @pre model != nullptr.
+   @pre integrator != nullptr.*/
+  FemSolver(const FemModel<T>* model,
+            const DiscreteTimeIntegrator<T>* integrator);
+
+  // TODO(xuchenhan-tri): The existance of the mutable scratch_data makes
+  // AdvanceOneTimeStep() not thread-safe. As a result FemSolver can't currently
+  // be placed in contact managers.
+  /* Advances the state of the FEM model by one time step with the integrator
+   prescribed at construction.
+   @param[in] prev_state   The state of the FEM model evaluated at the previous
+                           time step.
+   @param[out] next_state  The state of the FEM model evaluated at the next time
+                           step.
+   @returns the number of Newton-Raphson iterations the solver takes to
+   converge if the solver converges or -1 if the solver fails to converge.
+   @pre next_state != nullptr.
+   @throws std::exception if the input `prev_state` or `next_state` is
+   incompatible with the FEM model solved by this solver. */
+  int AdvanceOneTimeStep(const FemState<T>& prev_state,
+                         FemState<T>* next_state) const;
+
+  /* Returns the FEM model that this solver solves for. */
+  const FemModel<T>& model() const { return *model_; }
+
+  /* Returns the discrete time integrator that this solver uses. */
+  const DiscreteTimeIntegrator<T>& integrator() const { return *integrator_; }
+
+  /* Sets the relative tolerance, unitless. The Newton-Raphson iterations are
+   considered as converged if the norm of the residual,  is smaller than the
+   relative tolerance times the norm of the residual at the start of the
+   Newton-Raphson iterations. In other words, the iterations are considered as
+   converged if ‖r‖ < εᵣ * ‖r₀‖, where r and r₀ are the residual and the
+   initial residual respectively. The default value is 1e-4. */
+  void set_relative_tolerance(double tolerance) {
+    relative_tolerance_ = tolerance;
+  }
+
+  double relative_tolerance() const { return relative_tolerance_; }
+
+  /* Sets the absolute tolerance with unit Newton. The Newton-Raphson iterations
+   are considered as converged if the norm of the residual is smaller than the
+   absolute tolerance. In other words, the iterations are considered as
+   converged if ‖r‖ < εₐ, where r is the residual. The default value is 1e-6. */
+  void set_absolute_tolerance(double tolerance) {
+    absolute_tolerance_ = tolerance;
+  }
+
+  double absolute_tolerance() const { return absolute_tolerance_; }
+
+ private:
+  /* Holds the scratch data used in the solver to avoid unnecessary
+   reallocation. */
+  struct SolverScratchData {
+    /* Constructs a scratch data that is compatible with the given model. */
+    explicit SolverScratchData(const FemModel<T>& model) { Resize(model); }
+
+    /* Resizes scratch data to have sizes compatible with the given `model`. */
+    void Resize(const FemModel<T>& model) {
+      b.resize(model.num_dofs());
+      dz.resize(model.num_dofs());
+      tangent_matrix = model.MakePetscSymmetricBlockSparseTangentMatrix();
+    }
+
+    int num_dofs() const { return b.size(); }
+
+    std::unique_ptr<internal::PetscSymmetricBlockSparseMatrix> tangent_matrix;
+    /* Stores the residual of the model. */
+    VectorX<T> b;
+    /* Stores the solution to A * dz = -b, where A is the tangent matrix. */
+    VectorX<T> dz;
+  };
+
+  /* Uses a Newton-Raphson solver to solve for the unknown z such that the
+   residual is zero, i.e. b(z) = 0, up to the specified tolerances. See
+   set_relative_tolerance() and set_absolute_tolerance() for convergence
+   criteria. The input FEM state is non-null and is guaranteed to be compatible
+   with the FEM model.
+   @param[in, out] state  As input, `state` provides an initial guess of
+   the solution. As output, `state` reports the equilibrium state.
+   @returns the number of iterations it takes for the solver to converge or -1
+   if the solver fails to converge. */
+  int SolveWithInitialGuess(FemState<T>* state) const;
+
+  /* Updates the relative tolerance for the linear solver used in the
+   Newton-Raphson iterations based on the residual norm if the linear solver
+   is iterative. More specifically, it is set to the minimum of 0.5, the
+   relative tolerance of the NewtonSolver, and the given `residual_norm`. No-op
+   if the linear solver is direct. */
+  void set_linear_solve_tolerance(const T& residual_norm) const;
+
+  /* Resets the scratch data to be consistent with the model if the model has
+   been modified. */
+  void ResetScratchDataIfNecessary() const;
+
+  /* The FEM model being solved by `this` solver. */
+  const FemModel<T>* model_{nullptr};
+  /* The discrete time integrator the solver uses. */
+  const DiscreteTimeIntegrator<T>* integrator_{nullptr};
+  /* Tolerance for convergence. */
+  double relative_tolerance_{1e-4};  // unitless.
+  double absolute_tolerance_{1e-6};  // unit N.
+  /* Max number of Newton-Raphson iterations the solver takes before it gives
+   up. */
+  int kMaxIterations_{100};
+  /* Scratch data used in the solver iteration. */
+  mutable SolverScratchData scratch_data_;
+};
+
+}  // namespace internal
+}  // namespace fem
+}  // namespace multibody
+}  // namespace drake
