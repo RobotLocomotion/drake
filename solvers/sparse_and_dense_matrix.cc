fe74e228cd8ca9db432e5dda1833a8ad0214d863
hongkai-dai <daihongkai@gmail.com>
2022-04-03 19:11:04 -0700

    Add SparseAndDenseMatrix class. Use to hold a sparse matrix and an optional dense matrix.

---
+++ solvers/sparse_and_dense_matrix.cc
@@ -0,0 +1,31 @@
+#include "drake/solvers/sparse_and_dense_matrix.h"
+
+#include <thread>
+
+namespace drake {
+namespace solvers {
+namespace internal {
+SparseAndDenseMatrix::SparseAndDenseMatrix(Eigen::SparseMatrix<double> sparse)
+    : sparse_(std::move(sparse)), dense_(0, 0) {}
+
+SparseAndDenseMatrix::SparseAndDenseMatrix(
+    const Eigen::Ref<const Eigen::MatrixXd>& dense)
+    : sparse_(dense.sparseView()), dense_(dense) {}
+
+const Eigen::SparseMatrix<double>& SparseAndDenseMatrix::get_as_sparse() const {
+  return sparse_;
+}
+
+const Eigen::MatrixXd& SparseAndDenseMatrix::GetAsDense() const {
+  if (dense_.size() != 0) {
+    return dense_;
+  } else {
+    // Modifies the dense_ field. For thread-safety, use a std::mutex.
+    std::lock_guard<std::mutex> guard(mutex_);
+    dense_ = sparse_.toDense();
+    return dense_;
+  }
+}
+}  // namespace internal
+}  // namespace solvers
+}  // namespace drake
---
+++ solvers/sparse_and_dense_matrix.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <mutex>
+
+#include <Eigen/Core>
+#include <Eigen/SparseCore>
+
+#include "drake/common/drake_copyable.h"
+
+namespace drake {
+namespace solvers {
+namespace internal {
+/**
+ * This class is typically used in cost and constraint class to store a sparse
+ * matrix and an optional dense matrix. It also provides setter/getter for the
+ * matrix.
+ */
+class SparseAndDenseMatrix {
+ public:
+  // Delete these constructors for thread safety.
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(SparseAndDenseMatrix)
+  SparseAndDenseMatrix(Eigen::SparseMatrix<double> sparse);
+
+  SparseAndDenseMatrix(const Eigen::Ref<const Eigen::MatrixXd>& dense);
+
+  const Eigen::SparseMatrix<double>& get_as_sparse() const;
+
+  // Getter for the dense matrix. If the class is constructed from a dense
+  // matrix, or GetAsDense() function has been called before, then this is a
+  // no-op. Otherwise this function will internally create a dense matrix.
+  const Eigen::MatrixXd& GetAsDense() const;
+
+  ~SparseAndDenseMatrix() {}
+
+ private:
+  Eigen::SparseMatrix<double> sparse_;
+  // If dense_.size() == 0, then we only use sparse_;
+  mutable Eigen::MatrixXd dense_;
+  mutable std::mutex mutex_;
+};
+}  // namespace internal
+}  // namespace solvers
+}  // namespace drake
