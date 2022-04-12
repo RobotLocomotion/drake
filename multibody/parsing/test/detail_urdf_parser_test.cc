9598c3242a1f2330b628104d0d2027c152ab2ea6
Rick Poyner <rick.poyner@tri.global>
2022-04-12 15:20:16 -0400

    [parsing] Warn about unsupported URDF features
    Closes: #16783
    Closes: #16181
    
    Based on study of the ROS documentation and urdfdom module sources, add
    warnings for ignored URDF features. Implementation is similar to the
    in-progress mujoco parser, with an eye to future integration of diagnostics
    there.
    
    Adjust tests for existing files that emit new warnings, and add explicit
    tests for each of the added warning types.

--- multibody/parsing/test/detail_urdf_parser_test.cc
+++ multibody/parsing/test/detail_urdf_parser_test.cc
@@ -533,7 +533,16 @@ TEST_F(UrdfParserTest, TestAtlasMinimalContact) {
   std::string full_name = FindResourceOrThrow(
       "drake/examples/atlas/urdf/atlas_minimal_contact.urdf");
   AddModelFromUrdfFile(full_name, "");
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*gazebo.*ignored.*"));
+  for (int k = 0; k < 30; k++) {
+    EXPECT_THAT(TakeWarning(), MatchesRegex(".*safety_controller.*ignored.*"));
+  }
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*mechanicalReduction.*ignored.*"));
   EXPECT_THAT(TakeWarning(), MatchesRegex(".*attached to a fixed joint.*"));
+  for (int k = 0; k < 30; k++) {
+    EXPECT_THAT(TakeWarning(),
+                MatchesRegex(".*mechanicalReduction.*ignored.*"));
+  }
   plant_.Finalize();
 
   EXPECT_EQ(plant_.num_positions(), 37);
@@ -563,7 +572,9 @@ TEST_F(UrdfParserTest, TestRegisteredSceneGraph) {
       "drake/examples/atlas/urdf/atlas_minimal_contact.urdf");
   // Test that registration with scene graph results in visual geometries.
   AddModelFromUrdfFile(full_name, "");
-  EXPECT_THAT(TakeWarning(), MatchesRegex(".*attached to a fixed joint.*"));
+  // Mostly ignore warnings here; they are tested in detail elsewhere.
+  EXPECT_GT(warning_records_.size(), 60);
+  warning_records_.clear();
   plant_.Finalize();
   EXPECT_NE(plant_.num_visual_geometries(), 0);
 }
@@ -573,7 +584,18 @@ TEST_F(UrdfParserTest, JointParsingTest) {
       "drake/multibody/parsing/test/urdf_parser_test/"
       "joint_parsing_test.urdf");
   AddModelFromUrdfFile(full_name, "");
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*mechanicalReduction.*ignored.*"));
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*mechanicalReduction.*ignored.*"));
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
+
   EXPECT_THAT(TakeWarning(), MatchesRegex(".*has a zero effort limit.*"));
+
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*mechanicalReduction.*ignored.*"));
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
   plant_.Finalize();
 
   // Revolute joint
@@ -1231,38 +1253,45 @@ TEST_F(ReflectedInertiaTest, Both) {
   VerifyParameters("<drake:rotor_inertia value='1.5' />",
                    "<drake:gear_ratio value='300.0' />",
                    1.5, 300);
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
 }
 
 TEST_F(ReflectedInertiaTest, DefaultGearRatio) {
   // Test successful parsing of rotor_inertia and default value for gear_ratio.
   VerifyParameters("<drake:rotor_inertia value='1.5' />", "", 1.5, 1.0);
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
 }
 
 TEST_F(ReflectedInertiaTest, RotorInertiaNoValue) {
   ProvokeError("<drake:rotor_inertia />", "",
                ".*joint actuator revolute_AB's drake:rotor_inertia does not"
                " have a \"value\" attribute!");
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
 }
 
 TEST_F(ReflectedInertiaTest, RotorInertiaManyValues) {
   ProvokeError("<drake:rotor_inertia value='1 2 3'/>", "",
                ".*Expected single value.*value.*");
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
 }
 
 TEST_F(ReflectedInertiaTest, DefaultRotorInertia) {
   // Test successful parsing of gear_ratio and default value for rotor_inertia.
   VerifyParameters("", "<drake:gear_ratio value='300.0' />", 0.0, 300.0);
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
 }
 
 TEST_F(ReflectedInertiaTest, GearRatioNoValue) {
   ProvokeError("", "<drake:gear_ratio />",
                ".*joint actuator revolute_AB's drake:gear_ratio does not have"
                " a \"value\" attribute!");
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
 }
 
 TEST_F(ReflectedInertiaTest, GearRatioManyValues) {
   ProvokeError("<drake:gear_ratio value='1 2 3'/>", "",
                ".*Expected single value.*value.*");
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
 }
 
 // TODO(SeanCurtis-TRI) The logic testing for collision filter group parsing
@@ -1370,6 +1399,118 @@ TEST_F(UrdfParserTest, IgnoredCollisionFilterGroupMissingName) {
   EXPECT_THAT(TakeError(), MatchesRegex(
                   ".*The tag <drake:ignored_collision_filter_group> does not"
                   " specify the required attribute \"name\"."));
+}
+
+// Here follow tests to verify that Drake issues a warning when it ignores
+// something thought to be a documented URDF element or attribute.
+
+TEST_F(UrdfParserTest, UnsupportedGazeboIgnored) {
+  EXPECT_NE(AddModelFromUrdfString(R"""(
+    <robot name='robot'>
+      <link name='a'/>
+      <gazebo/>
+    </robot>)""", ""), std::nullopt);
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*gazebo.*ignored.*"));
+}
+
+TEST_F(UrdfParserTest, UnsupportedVersionIgnored) {
+  EXPECT_NE(AddModelFromUrdfString(R"""(
+    <robot name='robot' version='0.99'>
+      <link name='a'/>
+    </robot>)""", ""), std::nullopt);
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*version.*ignored.*"));
+}
+
+TEST_F(UrdfParserTest, UnsupportedLinkTypeIgnored) {
+  EXPECT_NE(AddModelFromUrdfString(R"""(
+    <robot name='robot'>
+      <link name='a' type='unknown'/>
+    </robot>)""", ""), std::nullopt);
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*type.*link.*ignored.*"));
+}
+
+TEST_F(UrdfParserTest, UnsupportedJointStuffIgnored) {
+  std::array<std::string, 3> tags{"calibration", "mimic", "safety_controller"};
+  for (const auto& tag : tags) {
+    EXPECT_NE(AddModelFromUrdfString(fmt::format(R"""(
+    <robot>
+      <link name='parent'/>
+      <link name='child'/>
+      <joint name='a' type='revolute'>
+        <parent link='parent'/>
+        <child link='child'/>
+        <{}/>
+      </joint>
+    </robot>)""", tag), tag), std::nullopt);
+    EXPECT_THAT(TakeWarning(),
+                MatchesRegex(fmt::format(".*{}.*ignored.*", tag)));
+  }
+}
+
+TEST_F(UrdfParserTest, UnsupportedTransmissionStuffIgnored) {
+  std::array<std::string, 8> tags{
+    "leftActuator", "rightActuator", "flexJoint", "rollJoint", "gap_joint",
+    "passive_joint", "use_simulated_gripper_joint", "mechanicalReduction"};
+  for (const auto& tag : tags) {
+    EXPECT_NE(AddModelFromUrdfString(fmt::format(R"""(
+    <robot>
+      <link name='parent'/>
+      <link name='child'/>
+      <joint name='a' type='revolute'>
+        <parent link='parent'/>
+        <child link='child'/>
+      </joint>
+      <transmission type='SimpleTransmission'>
+        <actuator name='a'/>
+        <joint name='a'/>
+        <{}/>
+      </transmission>
+    </robot>)""", tag), tag), std::nullopt);
+    EXPECT_THAT(TakeWarning(),
+                MatchesRegex(fmt::format(".*{}.*ignored.*", tag)));
+  }
+}
+TEST_F(UrdfParserTest, UnsupportedTransmissionActuatorStuffIgnored) {
+  std::array<std::string, 2> tags{
+    "hardwareInterface", "mechanicalReduction"};
+  for (const auto& tag : tags) {
+    EXPECT_NE(AddModelFromUrdfString(fmt::format(R"""(
+    <robot>
+      <link name='parent'/>
+      <link name='child'/>
+      <joint name='a' type='revolute'>
+        <parent link='parent'/>
+        <child link='child'/>
+      </joint>
+      <transmission type='SimpleTransmission'>
+        <actuator name='a'>
+          <{}/>
+        </actuator>
+        <joint name='a'/>
+      </transmission>
+    </robot>)""", tag), tag), std::nullopt);
+    EXPECT_THAT(TakeWarning(),
+                MatchesRegex(fmt::format(".*{}.*ignored.*", tag)));
+  }
+}
+
+TEST_F(UrdfParserTest, UnsupportedTransmissionJointStuffIgnored) {
+  EXPECT_NE(AddModelFromUrdfString(R"""(
+    <robot name ='a'>
+      <link name='parent'/>
+      <link name='child'/>
+      <joint name='a' type='revolute'>
+        <parent link='parent'/>
+        <child link='child'/>
+      </joint>
+      <transmission type='SimpleTransmission'>
+        <actuator name='a'/>
+        <joint name='a'>
+          <hardwareInterface/>
+        </joint>
+      </transmission>
+    </robot>)""", ""), std::nullopt);
+  EXPECT_THAT(TakeWarning(), MatchesRegex(".*hardwareInterface.*ignored.*"));
 }
 
 }  // namespace
