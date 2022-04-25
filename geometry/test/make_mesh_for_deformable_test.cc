e00137bb72eabd0c614b800ae638f78f7529b6b8
Xuchen Han <xuchen.han@tri.global>
2022-04-25 15:08:44 -0700

    [geometry] InternalGeometry supports deformable representation

--- geometry/test/make_mesh_for_deformable_test.cc
+++ geometry/test/make_mesh_for_deformable_test.cc
@@ -12,7 +12,56 @@ namespace {
 
 constexpr double kRezHint = 0.5;
 
-GTEST_TEST(DeformableMeshBuilderTest, Sphere) {
+class MeshBuilderForDeformableTest : public ::testing::Test {
+ protected:
+  MeshBuilderForDeformable builder_;
+};
+
+TEST_F(MeshBuilderForDeformableTest, Sphere) {
+  const Sphere sphere(1.0);
+  std::unique_ptr<VolumeMesh<double>> mesh = builder_.Build(sphere, kRezHint);
+  const VolumeMesh<double> expected_mesh =
+      MakeMeshForDeformable(sphere, kRezHint);
+  EXPECT_TRUE(mesh->Equal(expected_mesh));
+}
+
+TEST_F(MeshBuilderForDeformableTest, Cylinder) {
+  const Cylinder c(1.25, 2.5);
+  DRAKE_EXPECT_THROWS_MESSAGE(builder_.Build(c, kRezHint),
+                              "Cylinder.*not supported.*");
+}
+
+TEST_F(MeshBuilderForDeformableTest, Halfspace) {
+  const HalfSpace h;
+  DRAKE_EXPECT_THROWS_MESSAGE(builder_.Build(h, kRezHint),
+                              "Half space.*not supported.*");
+}
+
+TEST_F(MeshBuilderForDeformableTest, Box) {
+  const Box b(1.5, 2.5, 3.5);
+  DRAKE_EXPECT_THROWS_MESSAGE(builder_.Build(b, kRezHint),
+                              "Box.*not supported.*");
+}
+
+TEST_F(MeshBuilderForDeformableTest, Capsule) {
+  const Capsule c(1.25, 2.5);
+  DRAKE_EXPECT_THROWS_MESSAGE(builder_.Build(c, kRezHint),
+                              "Capsule.*not supported.*");
+}
+
+TEST_F(MeshBuilderForDeformableTest, Mesh) {
+  const Mesh m("path/to/file", 1.5);
+  DRAKE_EXPECT_THROWS_MESSAGE(builder_.Build(m, kRezHint),
+                              "Mesh.*not supported.*");
+}
+
+TEST_F(MeshBuilderForDeformableTest, Convex) {
+  const Convex m("path/to/file", 1.5);
+  DRAKE_EXPECT_THROWS_MESSAGE(builder_.Build(m, kRezHint),
+                              "Convex.*not supported.*");
+}
+
+GTEST_TEST(MakeMeshForDeformable, Sphere) {
   const Sphere sphere(1.0);
   const VolumeMesh<double> mesh = MakeMeshForDeformable(sphere, kRezHint);
   const VolumeMesh<double> expected_mesh = MakeSphereVolumeMesh<double>(
@@ -20,37 +69,37 @@ GTEST_TEST(DeformableMeshBuilderTest, Sphere) {
   EXPECT_TRUE(mesh.Equal(expected_mesh));
 }
 
-GTEST_TEST(DeformableMeshBuilderTest, Cylinder) {
+GTEST_TEST(MakeMeshForDeformable, Cylinder) {
   const Cylinder c(1.25, 2.5);
   DRAKE_EXPECT_THROWS_MESSAGE(MakeMeshForDeformable(c, kRezHint),
                               "Cylinder.*not supported.*");
 }
 
-GTEST_TEST(DeformableMeshBuilderTest, Halfspace) {
+GTEST_TEST(MakeMeshForDeformable, Halfspace) {
   const HalfSpace h;
   DRAKE_EXPECT_THROWS_MESSAGE(MakeMeshForDeformable(h, kRezHint),
                               "Half space.*not supported.*");
 }
 
-GTEST_TEST(DeformableMeshBuilderTest, Box) {
+GTEST_TEST(MakeMeshForDeformable, Box) {
   const Box b(1.5, 2.5, 3.5);
   DRAKE_EXPECT_THROWS_MESSAGE(MakeMeshForDeformable(b, kRezHint),
                               "Box.*not supported.*");
 }
 
-GTEST_TEST(DeformableMeshBuilderTest, Capsule) {
+GTEST_TEST(MakeMeshForDeformable, Capsule) {
   const Capsule c(1.25, 2.5);
   DRAKE_EXPECT_THROWS_MESSAGE(MakeMeshForDeformable(c, kRezHint),
                               "Capsule.*not supported.*");
 }
 
-GTEST_TEST(DeformableMeshBuilderTest, Mesh) {
+GTEST_TEST(MakeMeshForDeformable, Mesh) {
   const Mesh m("path/to/file", 1.5);
   DRAKE_EXPECT_THROWS_MESSAGE(MakeMeshForDeformable(m, kRezHint),
                               "Mesh.*not supported.*");
 }
 
-GTEST_TEST(DeformableMeshBuilderTest, Convex) {
+GTEST_TEST(MakeMeshForDeformable, Convex) {
   const Convex m("path/to/file", 1.5);
   DRAKE_EXPECT_THROWS_MESSAGE(MakeMeshForDeformable(m, kRezHint),
                               "Convex.*not supported.*");
