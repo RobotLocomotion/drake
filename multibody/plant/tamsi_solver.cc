fbd179e3fe8bf420cbbb9cf103daae45364e86fb
Russ Tedrake <russt@mit.edu>
2022-04-25 21:26:56 -0400

    [multibody] Replace M.ldlt() in Tamsi no-contact path with LinearSolver
    Resolves the immediate failure identified in #17037.

--- multibody/plant/tamsi_solver.cc
+++ multibody/plant/tamsi_solver.cc
@@ -7,6 +7,7 @@
 #include <vector>
 
 #include "drake/common/extract_double.h"
+#include "drake/math/linear_solve.h"
 
 namespace drake {
 namespace multibody {
@@ -621,7 +622,8 @@ TamsiSolverResult TamsiSolver<T>::SolveWithGuess(
     auto& v = fixed_size_workspace_.mutable_v();
     // With no friction forces Eq. (3) in the documentation reduces to
     // M vˢ⁺¹ = p*.
-    v = M.ldlt().solve(p_star);
+    math::LinearSolver<Eigen::LDLT, MatrixX<T>> M_ldlt(M.eval());
+    v = M_ldlt.Solve(p_star);
     // "One iteration" with exactly "zero" vt_error.
     statistics_.Update(0.0);
     return TamsiSolverResult::kSuccess;
