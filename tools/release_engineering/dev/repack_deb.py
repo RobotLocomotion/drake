f99dad63d33941e9d0f56271ad081320705022bb
Stephen McDowell <stephen.mcdowell@kitware.com>
2022-04-27 20:11:56 -0400

    [tools/dev] apt release packaging improvements
    - Add debian/compat (9), alien generated compat file gives warnings.
    - debian/control => debian/control.in to configure dependencies by
      reading packages-{codename}.txt from the tarball.
        - Section: alien unrecognized, changed to libdevel to match current.
        - Priority: extra is outdated and should now be optional.
        - Package: drake-dev not drake.
    - VTK 8 C++ / Python libraries have rpath manually fixed.
    - Remove shebang from director scripts to avoid lintian warnings.
    - Fix directory and file permissions for large sections of the drake
      install tree.

--- tools/release_engineering/dev/repack_deb.py
+++ tools/release_engineering/dev/repack_deb.py
@@ -15,9 +15,13 @@
 
 import argparse
 import email.utils
+import itertools
+import lsb_release
 import os
+from pathlib import Path
 import shutil
 import subprocess
+import sys
 import tarfile
 import tempfile
 
@@ -36,19 +40,35 @@ def _run(args):
     """Implements all steps for repacking tgz => deb.
     """
     # Find our runfiles.
-    deb_control = _rlocation('debian/control')
+    deb_compat = _rlocation('debian/compat')
+    deb_control_in = _rlocation('debian/control.in')
     deb_copyright = _rlocation('debian/copyright')
     deb_changelog_in = _rlocation('debian/changelog.in')
 
-    # Discern the version badging to use.
+    # Discern the version badging to use, get the dependencies for drake.
+    codename = lsb_release.get_os_release()['CODENAME']
     with tarfile.open(args.tgz) as archive:
         version = archive.getmember('drake/share/doc/drake/VERSION.TXT')
         version_txt = archive.extractfile(version).read().decode('utf-8')
         version_mtime = version.mtime
+
+        packages = archive.getmember(
+            f'drake/share/drake/setup/packages-{codename}.txt')
+        packages_txt = archive.extractfile(packages).read().decode('utf-8')
+
     version_tokens = version_txt.split()
     assert len(version_tokens) == 2, version_txt
     yyyymmddhhmmss, git_sha = version_tokens
     debian_version = f'0.0.{yyyymmddhhmmss}'
+
+    # Compute the new control.  The `packages_txt` will have one package-name
+    # per line, we add a couple more dependencies and then transform this to
+    # be an indented and comma separated list.
+    packages_txt += '${shlibs:Depends}\n${misc:Depends}'
+    packages_txt = packages_txt.replace('\n', ',\n         ')
+    with open(deb_control_in, encoding='utf-8') as f:
+        deb_control_contents = f.read().format(
+            depends=packages_txt)
 
     # Compute the new changelog.
     with open(deb_changelog_in, encoding='utf-8') as f:
@@ -62,6 +82,7 @@ def _run(args):
     cwd = f'{args.tempdir}/alien'
     os.mkdir(cwd)
     alien = [
+        'fakeroot',
         '/usr/bin/alien',
         '--to-deb',
         '--single',
@@ -80,10 +101,69 @@ def _run(args):
               f'{package_dir}/opt/drake')
 
     # Overwrite some metadata.
-    shutil.copy(deb_control, f'{package_dir}/debian/')
+    shutil.copy(deb_compat, f'{package_dir}/debian/')
+    with open(f'{package_dir}/debian/control', 'w', encoding='utf-8') as f:
+        f.write(deb_control_contents)
     shutil.copy(deb_copyright, f'{package_dir}/debian/')
     with open(f'{package_dir}/debian/changelog', 'w', encoding='utf-8') as f:
         f.write(deb_changelog_contents)
+
+    # Patch RPATH entries for drake-visualizer.
+    lib_dir = Path(package_dir) / 'opt' / 'drake' / 'lib'
+    # VTK C++ libraries need to be able to find their C++ counterparts.
+    for vtk_cxx_lib in lib_dir.glob('libvtk*-8.2.so*'):
+        subprocess.check_call([
+            'patchelf',
+            '--set-rpath',
+            '$ORIGIN',
+            str(vtk_cxx_lib),
+        ])
+    # VTK Python libraries and director/vtkDRCFiltersPython.so need to be able
+    # to find their C++ counterparts.  They live in equitable directory
+    # locations to be able to patch the same rpath.
+    py_ver = f'python{".".join([str(d) for d in sys.version_info[0:2]])}'
+    site_packages = lib_dir / py_ver / 'site-packages'
+    vtk_py_libs = list((site_packages / 'vtkmodules').glob('vtk*.so'))
+    director_lib = [site_packages / 'director' / 'vtkDRCFiltersPython.so']
+    # The director director/vtkDRCFiltersPython.so liv
+    for py_lib in itertools.chain(vtk_py_libs, director_lib):
+        subprocess.check_call([
+            'patchelf',
+            '--set-rpath',
+            '$ORIGIN/../../..',
+            str(py_lib),
+        ])
+
+    # Remove shebang for scripts in director (drake-visualizer).
+    for script in {'min_bounding_rect.py', 'qhull_2d.py'}:
+        path = site_packages / 'director' / 'thirdparty' / script
+        with open(path, 'r', encoding='utf-8') as f:
+            contents = f.read()
+            contents = contents.replace('#!/usr/bin/python', '#', 1)
+        with open(path, 'w', encoding='utf-8') as f:
+            f.write(contents)
+
+    # Fix directory permissions for all of drake.
+    for root, dirs, files in os.walk(package_dir + '/opt'):
+        for d in dirs:
+            os.chmod(os.path.join(root, d), 0o755)
+
+    # Fix file permissions for e.g., header files, license files, many others
+    # where typically 664 but needs 644.  Also fixes a handful of .so files
+    # that have their executable bits set (e.g., VTK).
+    for d in {'/opt/drake/include', '/opt/drake/lib', '/opt/drake/share'}:
+        for root, dirs, files in os.walk(package_dir + d):
+            for f in files:
+                os.chmod(os.path.join(root, f), 0o644)
+
+    # non-standard-executable-perm: 0775 != 0755
+    non_standard_perm = ['/opt/drake/bin/optitrack_client']
+    script_not_executable = [
+        '/opt/drake/share/drake/setup/deepnote/install_nginx',
+        '/opt/drake/share/drake/setup/install_prereqs',
+    ]
+    for script in itertools.chain(non_standard_perm, script_not_executable):
+        os.chmod(package_dir + script, 0o755)
 
     # Create the deb.
     subprocess.check_call(['fakeroot', 'debian/rules', 'binary'],
