c36ae87e14e6ef15d5631705616443ecf9b46592
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-04-29 14:11:26 -0700

    [pydrake] Changing Python logging level also changes C++ level
    Deprecate the confusing and now-superfluous pydrake set_log_level function.
    
    When using nicely formatted logging, emit "TRACE" not "Level 5".

--- bindings/pydrake/common/__init__.py
+++ bindings/pydrake/common/__init__.py
@@ -6,10 +6,69 @@ import logging as _logging
 import numpy as np
 
 from ._module_py import *
+from ._module_py import _set_log_level
 
 # When running from python, turn DRAKE_ASSERT and DRAKE_DEMAND failures into
 # SystemExit, instead of process aborts.  See RobotLocomotion/drake#5268.
 set_assertion_failure_to_throw_exception()
+
+_root_logger = _logging.getLogger()
+_drake_logger = _logging.getLogger("drake")
+
+
+def _python_level_to_spdlog(level: int):
+    """Returns the spdlog string level that enables C++ logging at the given
+    Python logging level, for use with pydrake.common._set_log_level.
+    """
+    if level >= _logging.CRITICAL:
+        return "critical"
+    if level >= _logging.ERROR:
+        return "err"
+    if level >= _logging.WARNING:
+        return "warn"
+    if level >= _logging.INFO:
+        return "info"
+    if level >= _logging.DEBUG:
+        return "debug"
+    return "trace"
+
+
+def _sync_spdlog_level():
+    """Updates Drake's C++ spdlog level threshold to match the effective level
+    of Drake's Python logger. This will either be the Drake Logger's level or
+    else the root Logger's level in case Drake Logger's level is NOTSET.
+
+    This only syncs the "effective" level, not the "enabled for" level. The
+    "enabled for" level would also incorporate the global "logging.disable"
+    setting, but we do not reflect that back into C++ spdlog at the moment.
+    """
+    level = _drake_logger.level or _root_logger.level
+    _set_log_level(_python_level_to_spdlog(level))
+
+
+def _monkey_patch_logger_level_events():
+    """Adds a hook into Drake's Python Logger object such that changes to
+    Python logging levels are reflected back into Drake's C++ spdlog level.
+    """
+    # The only notification from the root logger to its child loggers that
+    # the root level has changed is it's call to `logger._cache.clear()`.
+    # That notification is triggered for any logger level change, including
+    # Drake's own logger. Therefore, hooking that notification to sync the
+    # C++ logging level is sufficient to cover all cases.
+    #
+    # This relies on the implementation details of CPython logging, but since
+    # all of pydrake is native code that only supports CPython and we must
+    # recompile pydrake for every new minor Python version, this should be
+    # sufficiently robust given our Drake CI testing of this feature.
+    class spdlog_syncing_dict(dict):
+        def clear(self):
+            super().clear()
+            _sync_spdlog_level()
+
+    _drake_logger._cache = spdlog_syncing_dict()
+
+
+_monkey_patch_logger_level_events()
 
 
 def configure_logging():
@@ -19,13 +78,11 @@ def configure_logging():
     We offer it as a convenience only because Python's logging defaults are
     more spartan than Drake's C++ logging format (e.g., Python does not show
     message timestamps by default).
-
-    See also:
-       :py:func:`pydrake.common.set_log_level`
     """
     format = "[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s"
     _logging.basicConfig(level=_logging.INFO, format=format)
-    _logging.getLogger("drake").setLevel(_logging.NOTSET)
+    _drake_logger.setLevel(_logging.NOTSET)
+    _logging.addLevelName(5, "TRACE")
 
 
 def _wrap_to_match_input_shape(f):
