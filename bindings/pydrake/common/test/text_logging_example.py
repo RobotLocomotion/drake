4026da867f8ddd127c0af998fe5c335c13b270fb
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-04-30 17:33:16 -0700

    [pydrake] Changing Python logging level also changes C++ level
    Deprecate the confusing and now-superfluous pydrake set_log_level function.
    
    When using nicely formatted logging, emit "TRACE" not "Level 5".

--- bindings/pydrake/common/test/text_logging_example.py
+++ bindings/pydrake/common/test/text_logging_example.py
@@ -1,7 +1,7 @@
 import argparse
 import logging
 
-from pydrake.common import configure_logging, set_log_level
+from pydrake.common import configure_logging
 from pydrake.common.test.text_logging_test import do_log_test
 
 
@@ -9,33 +9,23 @@ def main():
     # Parse our arguments.
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--python_level", metavar="INT", type=int, required=True,
-        help="Set Python Drake logger to this level, or use 0 as a no-op.")
-    parser.add_argument(
-        "--spdlog_level_name", metavar="STR", type=str, required=True,
-        help="Set C++ Drake to this log level, or use 'unchanged' as a no-op."
-             " Refer to set_log_level in drake/common for valid level names.")
-    parser.add_argument(
         "--use_nice_format", metavar="1|0", type=int, required=True,
         help="Switch on (or off) nicely formatted Python output.")
+    parser.add_argument(
+        "--root_level", metavar="INT", type=int, required=True,
+        help="Set Python root logger to this level, or use -1 for a no-op.")
+    parser.add_argument(
+        "--drake_level", metavar="INT", type=int, required=True,
+        help="Set Python Drake logger to this level, or use -1 for a no-op.")
     args = parser.parse_args()
 
-    # Configure Python logging (if requested).
-    if args.python_level != logging.NOTSET:
-        if args.use_nice_format:
-            configure_logging()
-            logging.getLogger().setLevel(args.python_level)
-        else:
-            logging.basicConfig(level=args.python_level)
-            logging.getLogger("drake").setLevel(logging.NOTSET)
-    else:
-        # Trying to use the "nice format" makes no sense when we're not even
-        # configuring Python logging in the first place.
-        assert args.use_nice_format == 0
-
-    # Configure C++ logging (if requested).
-    if args.spdlog_level_name != "unchanged":
-        set_log_level(args.spdlog_level_name)
+    # Configure logging as instructed.
+    if args.use_nice_format:
+        configure_logging()
+    if args.root_level >= 0:
+        logging.getLogger().setLevel(args.root_level)
+    if args.drake_level >= 0:
+        logging.getLogger("drake").setLevel(args.drake_level)
 
     # Emit a bunch of log messages from C++. Depending on the level filtering,
     # not all of them will be printed.
