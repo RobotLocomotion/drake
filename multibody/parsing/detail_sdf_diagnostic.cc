d2ce07e753434dc421411445a3a4c247a4df9ea1
Sam Creasey <sam.creasey@tri.global>
2022-05-04 16:15:39 -0400

    SDF parsing: Warn about unsupported elements
    Also cleans up some models which had been incorporated into drake with
    unsupported elements.
    
    Fixes #16785

---
+++ multibody/parsing/detail_sdf_diagnostic.cc
@@ -0,0 +1,76 @@
+#include "drake/multibody/parsing/detail_sdf_diagnostic.h"
+
+namespace drake {
+namespace multibody {
+namespace internal {
+
+using drake::internal::DiagnosticDetail;
+using drake::internal::DiagnosticPolicy;
+
+void CheckSupportedElements(
+    const DiagnosticPolicy& diagnostic,
+    sdf::ElementPtr root_element,
+    const std::set<std::string>& supported_elements) {
+  CheckSupportedElements(diagnostic, root_element.get(), supported_elements);
+}
+
+void CheckSupportedElements(
+    const DiagnosticPolicy& diagnostic,
+    const sdf::Element* root_element,
+    const std::set<std::string>& supported_elements) {
+
+  sdf::ElementPtr element = root_element->GetFirstElement();
+  while (element) {
+    const std::string& element_name = element->GetName();
+    if ((supported_elements.find(element_name) == supported_elements.end()) &&
+        element->GetExplicitlySetInFile()) {
+      internal::DiagnosticDetail detail;
+      if (!element->FilePath().empty()) {
+        detail.filename = element->FilePath();
+      }
+      detail.line = element->LineNumber();
+      detail.message =
+          std::string("Unsupported SDFormat element in ") +
+          root_element->GetName() + std::string(": ") + element_name;
+      // Unsupported elements in the drake namespace are errors.
+      if (element_name.find("drake:") == 0) {
+        diagnostic.Error(detail);
+      } else {
+        diagnostic.Warning(detail);
+      }
+    }
+    element = element->GetNextElement();
+  }
+}
+
+void CheckSupportedElementValue(
+    const drake::internal::DiagnosticPolicy& diagnostic,
+    sdf::ElementPtr root_element,
+    const std::string& element_name,
+    const std::string& expected) {
+  if (!root_element->HasElement(element_name)) {
+    return;
+  }
+
+  sdf::ElementPtr element = root_element->GetElement(element_name);
+  if (!element->GetExplicitlySetInFile()) {
+    return;
+  }
+
+  sdf::ParamPtr value = element->GetValue();
+  if (value->GetAsString() != expected) {
+    internal::DiagnosticDetail detail;
+    if (!element->FilePath().empty()) {
+      detail.filename = element->FilePath();
+    }
+    detail.line = element->LineNumber();
+    detail.message =
+        std::string("Unsupported value for SDFormat element ") +
+        element->GetName() + std::string(": ") + value->GetAsString();
+    diagnostic.Warning(detail);
+  }
+}
+
+}  // namespace internal
+}  // namespace multibody
+}  // namespace drake
