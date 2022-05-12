8ec94da947a04813fd9800e28fafc23e06db0c18
Rick Poyner <rick.poyner@tri.global>
2022-05-12 13:32:03 -0400

    [parsing] Enable multi-robot collision filter groups
    Relevant to: #14785
    
    This patch extends the parsing of collision filter groups so that collision
    filters can be defined across multiple robots by using SDFormat model
    composition.
    
    Summary of changes:
     * add CollisionFilterGroupResolver, with unit test
     * port existing parser modules to use it
       * modernize a few sdf_parser errors
     * demonstrate multi-robot filters in sdf_parser_test

--- multibody/parsing/scoped_names.h
+++ multibody/parsing/scoped_names.h
@@ -11,6 +11,11 @@
 namespace drake {
 namespace multibody {
 namespace parsing {
+namespace internal {
+// Expose the delimeter string for reference, especially in tests.
+constexpr char kScopedNameDelim[] = "::";
+}  // namespace internal
+
 
 /// Finds an optionally model-scoped frame, using the naming rules of
 /// `ParseScopedName`.
