f23b263c6e5e1c35129dceebf15af119bbf92690
Rick Poyner <rick.poyner@tri.global>
2022-05-12 13:32:03 -0400

    [parsing] Enable multi-robot collision filter groups
    Relevant to: #14785
    
    This patch extends the parsing of collision filter groups so that collision
    filters can be defined across multiple robots by using SDFormat model
    composition.
    
    Summary of changes:
     * add CollisionFilterGroupResolver, with unit test
     * port existing parser modules to use it
       * modernize a few sdf_parser errors
     * demonstrate multi-robot filters in sdf_parser_test

--- multibody/parsing/detail_parsing_workspace.h
+++ multibody/parsing/detail_parsing_workspace.h
@@ -3,6 +3,7 @@
 #include "drake/common/diagnostic_policy.h"
 #include "drake/common/drake_assert.h"
 #include "drake/common/drake_copyable.h"
+#include "drake/multibody/parsing/detail_collision_filter_group_resolver.h"
 #include "drake/multibody/parsing/package_map.h"
 #include "drake/multibody/plant/multibody_plant.h"
 
@@ -15,29 +16,31 @@ namespace internal {
 // elsewhere.
 //
 // Note that code using this struct may pass it via const-ref, but the
-// indicated plant will still be mutable; only its pointer within the struct is
-// const.
+// indicated plant and resolver objects will still be mutable; only the pointer
+// values within the struct are const.
 struct ParsingWorkspace {
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(ParsingWorkspace)
 
   // All parameters are aliased; they must have a lifetime greater than that of
   // this struct.
-  ParsingWorkspace(
-    const PackageMap& package_map_in,
-    const drake::internal::DiagnosticPolicy& diagnostic_in,
-    MultibodyPlant<double>* plant_in)
+  ParsingWorkspace(const PackageMap& package_map_in,
+                   const drake::internal::DiagnosticPolicy& diagnostic_in,
+                   MultibodyPlant<double>* plant_in,
+                   internal::CollisionFilterGroupResolver* resolver_in)
       : package_map(package_map_in),
         diagnostic(diagnostic_in),
-        plant(plant_in) {
+        plant(plant_in),
+        resolver(resolver_in) {
     DRAKE_DEMAND(plant != nullptr);
+    DRAKE_DEMAND(resolver != nullptr);
   }
 
   const PackageMap& package_map;
   const drake::internal::DiagnosticPolicy& diagnostic;
   MultibodyPlant<double>* const plant;
+  internal::CollisionFilterGroupResolver* const resolver;
 };
 
 }  // namespace internal
 }  // namespace multibody
 }  // namespace drake
-
