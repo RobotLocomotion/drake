af86606a7192e937647d05ae78a4200b4d37ea39
damrongguoy <damrong.guoy@tri.global>
2022-05-16 14:52:39 -0700

    [geometry] Compute contact surfaces for deformables
    Phase 1.
    - Reuse SurfaceVolumeIntersector<> for deformables.
    - Template parameter MeshBuilder now
      specifies the kinds of output (MeshType and FieldType) for both
      hydroelastics and deformables.
    - Another template parameter BvType to specify Obb for tetrahedral
      meshes in hydroelastics and Aabb for deformable meshes.
    - Give ContactSurface (similar to hydroelastics) plus
      tetrahedron indices of contact polygons and barycentric coordinates
      of polygons' centroids.
    
    Phase 2.
    - Towards new DeformableSurfaceVolumeIntersector instead of DeformableContactBuilder.
    - Factor out CalcContactPolygon() from SampleVolumeFieldOnSurface().
    - Finish CalcContactPolygon() of DeformableSurfaceVolumeIntersector.
    - Remove DeformableContactBuilder.
    - Remove an extra parameter of {Tri,Poly}MeshBuilder::AddPolygon()
      introduced in Phase 1.

---
+++ geometry/proximity/deformable_mesh_intersection.cc
@@ -0,0 +1,147 @@
+#include "drake/geometry/proximity/deformable_mesh_intersection.h"
+
+#include <utility>
+#include <vector>
+
+#include "drake/geometry/proximity/contact_surface_utility.h"
+#include "drake/geometry/proximity/deformable_volume_mesh.h"
+#include "drake/geometry/proximity/mesh_intersection.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+
+// TODO(DamrongGuoy) Declare DeformableSurfaceVolumeIntersector in the header
+//  file to test it directly and for future code re-use.
+class DeformableSurfaceVolumeIntersector : public
+    SurfaceVolumeIntersector<PolyMeshBuilder<double>, Aabb> {
+ public:
+  void SampleVolumeFieldOnSurface(
+      const VolumeMeshFieldLinear<double, double>& volume_field_W,
+      const Bvh<Aabb, VolumeMesh<double>>& bvh_W,
+      const TriangleSurfaceMesh<double>& surface_N,
+      const Bvh<Obb, TriangleSurfaceMesh<double>>& bvh_N,
+      const math::RigidTransform<T>& X_WN) {
+    PolyMeshBuilder<double> builder;
+    const bool filter_face_normal_along_field_gradient = false;
+    SurfaceVolumeIntersector<PolyMeshBuilder<double>, Aabb>::
+        SampleVolumeFieldOnSurface(volume_field_W, bvh_W,
+                                   surface_N, bvh_N, X_WN,
+                                   &builder,
+                                   filter_face_normal_along_field_gradient);
+  }
+
+  /* Returns indices of tetrahedra containing the contact polygons.
+   @pre Call it after the rigid-deformable mesh intersection finished.
+  */
+  const std::vector<int>& tetrahedron_index_of_polygons() {
+    return tetrahedron_index_of_polygons_;
+  }
+
+  /* Returns barycentric coordinates of the centroids of the contact polygons.
+   @pre Call it after the rigid-deformable mesh intersection finished.
+   */
+  const std::vector<VolumeMesh<double>::Barycentric<double>>&
+  barycentric_centroids() {
+    return barycentric_centroids_;
+  }
+
+ protected:
+  void CalcContactPolygon(
+      const VolumeMeshFieldLinear<double, double>& volume_field_M,
+      const TriangleSurfaceMesh<double>& surface_N,
+      const math::RigidTransform<T>& X_MN,
+      const math::RigidTransform<double>& X_MN_d,
+      PolyMeshBuilder<double>* builder_M,
+      const bool filter_face_normal_along_field_gradient,
+      const int tet_index, const int tri_index) override {
+    const int num_vertices_before = builder_M->num_vertices();
+    const int num_polygons_before = builder_M->num_faces();
+    SurfaceVolumeIntersector<PolyMeshBuilder<double>, Aabb>::CalcContactPolygon(
+        volume_field_M, surface_N, X_MN, X_MN_d, builder_M,
+        filter_face_normal_along_field_gradient, tet_index, tri_index);
+    const int num_vertices_after = builder_M->num_vertices();
+    const int num_polygons_after = builder_M->num_faces();
+
+    if (num_polygons_after == num_polygons_before) {
+      return;
+    }
+    DRAKE_DEMAND(num_polygons_after == num_polygons_before + 1);
+
+    tetrahedron_index_of_polygons_.push_back(tet_index);
+
+    std::vector<int> polygon(num_vertices_after - num_vertices_before);
+    std::iota(polygon.begin(), polygon.end(), num_vertices_before);
+
+    barycentric_centroids_.push_back(volume_field_M.mesh().CalcBarycentric(
+        CalcPolygonCentroid(
+            polygon, X_MN_d.rotation() * surface_N.face_normal(tri_index),
+            builder_M->vertices()),
+        tet_index));
+  }
+
+ private:
+  std::vector<int> tetrahedron_index_of_polygons_{};
+  std::vector<VolumeMesh<double>::Barycentric<double>> barycentric_centroids_{};
+};
+
+std::unique_ptr<ContactSurface<double>>
+ComputeContactSurfaceFromDeformableVolumeRigidSurface(
+    const GeometryId deformable_id,
+    const deformable::DeformableGeometry& deformable_W,
+    const GeometryId rigid_id, const TriangleSurfaceMesh<double>& rigid_mesh_R,
+    const Bvh<Obb, TriangleSurfaceMesh<double>>& rigid_bvh_R,
+    const math::RigidTransform<double>& X_WR,
+    std::vector<int>* tetrahedron_index_of_polygons,
+    std::vector<VolumeMesh<double>::Barycentric<double>>*
+        barycentric_centroids) {
+  DRAKE_DEMAND(tetrahedron_index_of_polygons != nullptr);
+  DRAKE_DEMAND(barycentric_centroids != nullptr);
+  tetrahedron_index_of_polygons->clear();
+  barycentric_centroids->clear();
+
+  // TODO(DamrongGuoy) Is there a better way than creating a new
+  //  VolumeMeshFieldLinear here? We do it here, so we can reuse
+  //  SurfaceVolumeIntersector. These are some ideas that Xuchen and Damrong
+  //  consider for future refactoring:
+  //  1. Change the type parameter MeshBuilder of SurfaceVolumeIntersector<>
+  //     to provide the tetrahedral mesh. This includes TriMeshBuilder and
+  //     PolyMeshBuilder.
+  //  Or 2. Allow VolumeMeshFieldLinear to switch to a different tetrahedral
+  //        mesh while keeping the same field values at vertices. Assume that
+  //        the new mesh has the same connectivity.
+  //  Or 3. Pass an additional parameter for the tetrahedral mesh to
+  //        SampleVolumeFieldOnSurface(). Right now it uses the mesh of the
+  //        given VolumeMeshFieldLinear.
+  VolumeMeshFieldLinear<double, double> field_W(
+      std::vector<double>(deformable_W.signed_distance_field().values()),
+      &deformable_W.deformable_mesh().mesh(), true /*calculate gradient*/);
+
+  DeformableSurfaceVolumeIntersector intersect;
+  intersect.SampleVolumeFieldOnSurface(
+      field_W, deformable_W.deformable_mesh().bvh(),
+      rigid_mesh_R, rigid_bvh_R, X_WR);
+
+  if (!intersect.has_intersection()) {
+    return {};
+  }
+
+  *tetrahedron_index_of_polygons = intersect.tetrahedron_index_of_polygons();
+  *barycentric_centroids = intersect.barycentric_centroids();
+  // The contact surface is documented as having the normals pointing *out*
+  // of the second surface and into the first. This mesh intersection
+  // creates a surface mesh with normals pointing out of the rigid surface,
+  // so we make sure the ids are ordered so that the rigid is the second id.
+  auto contact_surface = std::make_unique<ContactSurface<double>>(
+      deformable_id, rigid_id, intersect.release_mesh(),
+      intersect.release_field(),
+      std::make_unique<std::vector<Vector3<double>>>(
+          std::move(intersect.mutable_grad_eM_M())),
+      nullptr);
+
+  return contact_surface;
+}
+
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
---
+++ geometry/proximity/deformable_mesh_intersection.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "drake/geometry/proximity/bvh.h"
+#include "drake/geometry/proximity/deformable_contact_geometries.h"
+#include "drake/geometry/proximity/triangle_surface_mesh.h"
+#include "drake/geometry/query_results/contact_surface.h"
+#include "drake/math/rigid_transform.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+
+// TODO(DamrongGuoy): Redefine DeformableContactSurface to have
+//  {ContactSurface, tetrahedron indices of polygons, and
+//  barycentric coordinates of centroids of polygons}. Then, return the
+//  DeformableContactSurface instead of returning ContactSurfac with two
+//  output parameters.
+
+/* Computes the contact surface between a deformable geometry and a rigid
+ geometry.
+ @param[in] deformable_id
+     Id of the deformable geometry
+ @param[in] deformable_W
+     A deformable geometry expressed in World frame. It provides the
+     deformed tetrahedral mesh and the approximated signed distance field.
+ @param[in] rigid_id
+     Id of the rigid geometry
+ @param[in] rigid_mesh_R
+     The rigid geometry is represented as a surface mesh, whose vertex
+     positions are in frame R. We assume that triangles are oriented
+     outward.
+ @param[in] rigid_bvh_R
+     A bounding volume hierarchy built on the geometry contained in
+     `rigid_mesh_R`.
+ @param[in] X_WR
+     The pose of the rigid geometry's frame R in World frame.
+ @param[out] tetrahedron_index_of_polygons
+     Tetrahedron index of each contact polygon. The sequence of tetrahedron
+     indices is parallel to the sequence of contact polygons. If there is no
+     contact, it becomes an empty sequence.
+ @param[out] barycentric_centroids
+     Barycentric coordinates of centroids of contact polygons with respect to
+     their containing tetrahedra.
+
+ @return
+     The contact surface expressed in World frame with surface normal pointing
+     in the direction described in ContactSurface according to deformable_id
+     and rigid_id.
+
+ @pre  tetrahedron_index_of_polygons is not a nullptr.   */
+std::unique_ptr<ContactSurface<double>>
+ComputeContactSurfaceFromDeformableVolumeRigidSurface(
+    const GeometryId deformable_id,
+    const deformable::DeformableGeometry& deformable_W,
+    const GeometryId rigid_id,
+    const TriangleSurfaceMesh<double>& rigid_mesh_R,
+    const Bvh<Obb, TriangleSurfaceMesh<double>>& rigid_bvh_R,
+    const math::RigidTransform<double>& X_WR,
+    std::vector<int>* tetrahedron_index_of_polygons,
+    std::vector<VolumeMesh<double>::Barycentric<double>>*
+        barycentric_centroids);
+
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
