8ef8c7a54fa2f3def5a8b4cefddf455d8d24fb7a
mitiguy <paul.mitiguy@tri.global>
2022-05-17 17:13:30 -0700

    Get rid of the change in planar_mobilizer.h

--- multibody/tree/planar_mobilizer.h
+++ multibody/tree/planar_mobilizer.h
@@ -50,10 +50,6 @@ class PlanarMobilizer final : public MobilizerImpl<T, 3, 3> {
   std::string position_suffix(int position_index_in_mobilizer) const final;
   std::string velocity_suffix(int velocity_index_in_mobilizer) const final;
 
-  // Overloads that define whether this mobilizer can rotate and/or translate.
-  bool can_rotate() const final    { return true; }
-  bool can_translate() const final { return true; }
-
   /* Retrieves from `context` the two translations (x, y) which describe the
    position for `this` mobilizer as documented in this class's documentation.
 
