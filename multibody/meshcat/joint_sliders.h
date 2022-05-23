da56702c80b2a9911a7152b514b108b322c62ea5
Stephen McDowell <stephen.mcdowell@kitware.com>
2022-05-23 12:34:11 -0400

    [multibody] Add meshcat JointSliders::SetPositions
    - Enable the sliders to be updated using a vector of positions
      the same length as the MultibodyPlant::num_positions(), indices
      that correspond to joint sliders will update the meshcat UI.
    - Add corresponding python bindings and related tests.

--- multibody/meshcat/joint_sliders.h
+++ multibody/meshcat/joint_sliders.h
@@ -111,13 +111,27 @@ class JointSliders final : public systems::LeafSystem<T> {
       const systems::Diagram<T>& diagram,
       std::optional<double> timeout = std::nullopt) const;
 
+  /** Set all robot positions (corresponding to joint positions and potentially
+  positions not associated with any joint) to the values in `q`.  The meshcat
+  sliders associated with any joint positions described by `q` will have their
+  value updated.  Additionally, the "initial state" vector of positions tracked
+  by this instance will be updated to the values in `q`.  This "initial state"
+  vector update will persist even if sliders are removed (e.g., via Delete).
+
+  @param q A vector whose length is equal to the associated
+  MultibodyPlant::num_positions().
+  */
+  void SetPositions(const Eigen::VectorXd& q);
+
  private:
+  // TODO(svenevs): remove friendship after diagnosing test problem.
+  template <typename U> friend class JointSlidersTester;
   void CalcOutput(const systems::Context<T>&, systems::BasicVector<T>*) const;
 
   std::shared_ptr<geometry::Meshcat> meshcat_;
   const MultibodyPlant<T>* const plant_;
   const std::map<int, std::string> position_names_;
-  const Eigen::VectorXd initial_value_;
+  Eigen::VectorXd initial_value_;
   std::atomic<bool> is_registered_;
 };
 
