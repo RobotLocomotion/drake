2cfa3babc17b02f08a38c32c6f640b7e6d1c1cb7
Matthew Woehlke <matthew.woehlke@kitware.com>
2022-05-23 14:41:09 -0400

    Add macOS wheel build scripts
    Create a script to build a wheel on macOS. Update various Bazel logic to
    support the 'macos_wheel' platform. Add scripts to mangle library load
    paths on macOS. Update inner wheel build script to support macOS as well
    as manylinux. Update various inner build scripts to support a build root
    other than '/', since macOS does not permit creation of arbitrary
    top-level directories.
    
    Note that the wheel build requires that the user running the scripts is
    able to use 'brew', and has write access to '/opt'.

--- tools/wheel/image/build-dependencies.sh
+++ tools/wheel/image/build-dependencies.sh
@@ -1,14 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Internal script to build dependencies for a Drake wheel.
 
 set -eu -o pipefail
 
-mkdir -p /dependencies/build
-cd /dependencies/build
+mkdir -p /opt/drake-wheel-build/dependencies/build
+cd /opt/drake-wheel-build/dependencies/build
 
 cmake -G Ninja \
-  -DCMAKE_INSTALL_PREFIX=/opt/drake-dependencies \
-  /dependencies/src
+    -DCMAKE_INSTALL_PREFIX=/opt/drake-dependencies \
+    /opt/drake-wheel-build/dependencies/src
 
 ninja
 
-ln -s /opt/drake-dependencies/bin/patchelf /usr/local/bin/patchelf
+if [ "$(uname)" == "Linux" ]; then
+    ln -s /opt/drake-dependencies/bin/patchelf /usr/local/bin/patchelf
+fi
--- tools/wheel/image/build-vtk.sh
+++ tools/wheel/image/build-vtk.sh
@@ -1,25 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Internal script to build VTK for use in a Drake wheel.
+#
 # NOTE: If you need to update this file, please refer to
 #       tools/workspace/vtk/README for important details.
 
 set -eu -o pipefail
 
+readonly VTK_DIR=/opt/drake-wheel-build/vtk
 readonly VTK_VERSION=9.1.0
 
-mkdir -p /vtk
-cd /vtk
+mkdir -p ${VTK_DIR}
+cd ${VTK_DIR}
 
 git clone \
     --branch v${VTK_VERSION} --depth 1 \
     https://gitlab.kitware.com/vtk/vtk.git src
 
-mkdir -p /vtk/build
-cd /vtk/build
+mkdir -p ${VTK_DIR}/build
+cd ${VTK_DIR}/build
 
-mapfile -t VTK_CMAKE_ARGS < <(sed -e '/^#/d' -e 's/^/-D/' < /vtk/vtk-args)
+mapfile -t VTK_CMAKE_ARGS < \
+    <(sed -e '/^#/d' -e 's/^/-D/' < ${VTK_DIR}/vtk-args)
 
-cmake "${VTK_CMAKE_ARGS[@]}" -GNinja -Wno-dev /vtk/src
+cmake "${VTK_CMAKE_ARGS[@]}" -GNinja -Wno-dev ${VTK_DIR}/src
 
 ninja install/strip
 
