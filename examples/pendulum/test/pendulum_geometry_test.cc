81353644ee084b7809c4aac5e313e5c52c66580c
Xuchen Han <xuchen.han@tri.global>
2022-05-25 17:51:15 -0700

    [geometry] Improve KinematicsVector
    Change the implementation of KinematicsVector to use a absl::flat_hash_map.
    
    Rename KinematicsVector::ids() to KinematicsVector::GetAllIds() without
    deprecation (since ids() was introduced in between releases).
    
    Co-authored-by: Jeremy Nimmer <jeremy.nimmer@tri.global>

--- examples/pendulum/test/pendulum_geometry_test.cc
+++ examples/pendulum/test/pendulum_geometry_test.cc
@@ -23,7 +23,7 @@ using math::RigidTransformd;
 math::RigidTransformd ExtractSinglePose(
     const geometry::FramePoseVector<double>& pose_vector) {
   DRAKE_THROW_UNLESS(pose_vector.size() == 1);
-  for (const auto& id : pose_vector.ids()) {
+  for (const geometry::FrameId& id : pose_vector.GetAllIds()) {
     return pose_vector.value(id);
   }
   DRAKE_UNREACHABLE();
