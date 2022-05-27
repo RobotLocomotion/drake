989011e266c22e7e32403a3dee0d41953e143b05
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-05-27 07:49:03 -0700

    PR [workspace] Add reusable macro and documentation for vendor_cxx
    Update yaml_cpp_internal to follow the new style.

--- common/yaml/yaml_write_archive.cc
+++ common/yaml/yaml_write_archive.cc
@@ -4,8 +4,8 @@
 #include <utility>
 #include <vector>
 
-#include <drake-yaml-cpp/emitfromevents.h>
-#include <drake-yaml-cpp/yaml.h>
+#include <drake_vendor/yaml-cpp/emitfromevents.h>
+#include <drake_vendor/yaml-cpp/yaml.h>
 
 #include "drake/common/drake_assert.h"
 #include "drake/common/unused.h"
