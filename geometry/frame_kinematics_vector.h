28f6a3e56450e0fe846406f4070dc024b6f28dcd
Xuchen Han <xuchen.han@tri.global>
2022-06-06 16:08:24 -0700

    [geometry] Add stub for frame_kinematics_vector.h

---
+++ geometry/frame_kinematics_vector.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "drake/geometry/kinematics_vector.h"
+
+#warning DRAKE DEPRECATED: The header path \
+  "drake/geometry/frame_kinematics_vector.h" is deprecated. It will be removed \
+  around 2022-10-01. Include "drake/geometry/kinematics_vector.h" instead.
