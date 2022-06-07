3de9fd63dbb345efcdfcd3fe0788b23b4ff584af
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-06-07 10:21:15 -0700

    [workspace] Build NLopt from source
    Compared to the Ubuntu and macOS packages, this removes the LGPL'd
    "luksan" solver from the build, which means we can link the library
    statically (and eventually, hidden). This might mean that NLopt is
    no longer able to solve certain kinds of programs, but that is a
    fair trade-off given all of the problems caused by LGPL linking.
    
    The package.BUILD.bazel is modeled on nlopt.BUILD.bazel in a10ebb7.
    
    Deprecate the now-unused current (host OS) nlopt external.

---
+++ tools/workspace/nlopt_internal/test/enum_test.py
@@ -0,0 +1,42 @@
+import re
+import unittest
+
+from bazel_tools.tools.python.runfiles import runfiles
+
+
+class TestEnum(unittest.TestCase):
+
+    def setUp(self):
+        self.maxDiff = None
+
+    def test_enum_cross_check(self):
+        """Checks that the Drake-created flavor of nlopt.cpp (via a patch file)
+        is consistent with the upstream-generated flavor of same (via CMake).
+
+        If this test fails during an NLopt version pin upgrade, you will need
+        to update patches/gen_enums.patch with the reported differences.
+        """
+        # Load both input files.
+        # "actual" refers to the the Drake-created flavor (via a patch file).
+        # "expected" refers to the upstream-generated flavor (via CMake).
+        manifest = runfiles.Create()
+        actual_file = manifest.Rlocation(
+            "nlopt_internal/genrule/nlopt.hpp")
+        with open(actual_file) as f:
+            actual = f.read()
+        expected_file = manifest.Rlocation(
+            "drake/tools/workspace/nlopt_internal/test/nlopt-upstream.hpp")
+        with open(expected_file) as f:
+            expected = f.read()
+
+        # When CMake is processing the header file, it removes blank lines.
+        # We will do the same to our actual file to prep for comparison.
+        actual = actual.replace("\n\n", "\n")
+
+        # CMake also does something inexplicable to tab-spaced macro line
+        # endings. Canonicalize those in both files for comparison.
+        actual = re.sub(r'\s+\\', r' \\', actual)
+        expected = re.sub(r'\s+\\', r' \\', expected)
+
+        # Compare.
+        self.assertMultiLineEqual(expected, actual)
