e95b82cc2293bf42261f73843db4c05ebee1863e
Matthew Woehlke <matthew.woehlke@kitware.com>
2022-06-07 14:42:56 -0400

    Add MUMPS to IPOPT when building wheels
    Point our wheel build of IPOPT at MUMPS. Because MUMPS is copyleft, we
    must consume it as a shared library. On Ubuntu, this means we can just
    use the distro-provided package. On macOS, we must build it from source,
    as it is not packaged separately in Homebrew. (Homebrew normally builds
    it as part of the IPOPT build.)

--- tools/wheel/image/dependencies/projects/ipopt.cmake
+++ tools/wheel/image/dependencies/projects/ipopt.cmake
@@ -1,8 +1,24 @@
+if(APPLE)
+    set(ipopt_extra_dependencies mumps)
+    set(ipopt_mumps_configure_args
+        --with-mumps-lib=-ldmumps\ -lmumps_common\ -lpord\ -lmpiseq
+        --with-mumps-incdir=${CMAKE_INSTALL_PREFIX}/include
+        CPPFLAGS=-I${CMAKE_INSTALL_PREFIX}/include/mumps_seq
+    )
+else()
+    set(ipopt_extra_dependencies)
+    set(ipopt_mumps_configure_args
+        --with-mumps-lib=-ldmumps_seq
+        --with-mumps-incdir=/usr/include
+        CPPFLAGS=-I/usr/include/mumps_seq
+    )
+endif()
+
 ExternalProject_Add(ipopt
     URL ${ipopt_url}
     URL_MD5 ${ipopt_md5}
     DOWNLOAD_NAME ${ipopt_dlname}
-    DEPENDS lapack
+    DEPENDS lapack ${ipopt_extra_dependencies}
     ${COMMON_EP_ARGS}
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ./configure
@@ -12,6 +28,7 @@ ExternalProject_Add(ipopt
         CFLAGS=-fPIC
         CXXFLAGS=-fPIC
         LDFLAGS=-L${CMAKE_INSTALL_PREFIX}/lib
+        ${ipopt_mumps_configure_args}
     BUILD_COMMAND make
     INSTALL_COMMAND make install
     )
