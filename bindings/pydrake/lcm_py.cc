3089354e16867440d4c17c0a5c290c683e2d5af4
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-06-10 07:31:22 -0700

    [lcm] Add params struct and option for a channel suffix
    Deprecate the two-argument constructor.

--- bindings/pydrake/lcm_py.cc
+++ bindings/pydrake/lcm_py.cc
@@ -4,6 +4,7 @@
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
 
+#include "drake/bindings/pydrake/common/deprecation_pybind.h"
 #include "drake/bindings/pydrake/documentation_pybind.h"
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/lcm/drake_lcm.h"
@@ -47,14 +48,36 @@ PYBIND11_MODULE(lcm, m) {
   }
 
   {
+    using Class = DrakeLcmParams;
+    constexpr auto& cls_doc = doc.DrakeLcmParams;
+    py::class_<Class>(m, "DrakeLcmParams", cls_doc.doc)
+        .def(ParamInit<Class>())
+        .def_readwrite("lcm_url", &Class::lcm_url, cls_doc.lcm_url.doc)
+        .def_readwrite("channel_suffix", &Class::channel_suffix,
+            cls_doc.channel_suffix.doc)
+        .def_readwrite("defer_initialization", &Class::defer_initialization,
+            cls_doc.defer_initialization.doc)
+        .def("__repr__", [](const Class& self) {
+          return py::str(
+              "DrakeLcmParams("
+              "lcm_url={}, "
+              "channel_suffix={}, "
+              "defer_initialization={})")
+              .format(
+                  self.lcm_url, self.channel_suffix, self.defer_initialization);
+        });
+  }
+
+  {
     using Class = DrakeLcm;
     constexpr auto& cls_doc = doc.DrakeLcm;
-    py::class_<Class, DrakeLcmInterface>(m, "DrakeLcm", cls_doc.doc)
+    py::class_<Class, DrakeLcmInterface> cls(m, "DrakeLcm", cls_doc.doc);
+    cls  // BR
         .def(py::init<>(), cls_doc.ctor.doc_0args)
-        .def(
-            py::init<std::string>(), py::arg("lcm_url"), cls_doc.ctor.doc_1args)
-        .def(py::init<std::string, bool>(), py::arg("lcm_url"),
-            py::arg("defer_initialization"), cls_doc.ctor.doc_2args)
+        .def(py::init<std::string>(), py::arg("lcm_url"),
+            cls_doc.ctor.doc_1args_lcm_url)
+        .def(py::init<DrakeLcmParams>(), py::arg("params"),
+            cls_doc.ctor.doc_1args_params)
         .def(
             "Subscribe",
             [](Class* self, const std::string& channel,
@@ -69,6 +92,14 @@ PYBIND11_MODULE(lcm, m) {
             },
             py::arg("channel"), py::arg("handler"), cls_doc.Subscribe.doc);
     // TODO(eric.cousineau): Add remaining methods.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    const char* const doc_deprecated =
+        cls_doc.ctor
+            .doc_deprecated_deprecated_2args_lcm_url_defer_initialization;
+    cls.def(py_init_deprecated<Class, std::string, bool>(doc_deprecated),
+        py::arg("lcm_url"), py::arg("defer_initialization"), doc_deprecated);
+#pragma GCC diagnostic pop
   }
 
   ExecuteExtraPythonCode(m);
---
+++ lcm/drake_lcm_params.cc
@@ -0,0 +1,9 @@
+#include "drake/lcm/drake_lcm_params.h"
+
+namespace drake {
+namespace lcm {
+
+DrakeLcmParams::~DrakeLcmParams() = default;
+
+}  // namespace lcm
+}  // namespace drake
