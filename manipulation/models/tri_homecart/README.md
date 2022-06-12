14421cfa7fd3990daa617d0566c79834144b1ff8
Russ Tedrake <russt@mit.edu>
2022-06-12 08:54:24 -0400

    [manipulation] Add UR3e model and TRI Homecart
    This PR only contains a test that verifies that the model can be
    parsed correctly (through model directives, etc). But in a follow-up
    PR I will use this as a Drake example of bimanual motion planning with
    Graph of Convex Sets.
    
    These models were derived from the Anzu versions; with a few changes:
    
    - Anzu has "left/right" variants of the ur3e models, which set tighter
    joint limits for the homecart configuration.  I felt that it was a
    little inelegant to put those in the ur3e directory (they should be
    homecart specific).  We can add them later if need be.
    
    - I've used the simple wsg gripper models that were already in Drake
    instead of the fancier models in anzu.
    
    - I've named the directory `tri_homecart` instead of `homecart`.
    
    - I've manually removed the installed .obj and .png files from the pip
    wheels; otherwise it would cause our wheels to exceed the 100MB limit.
    
    - The model files have been optimized by Cody to get the filesize for the homecart-specific model files down to ~ 2MB.

---
+++ manipulation/models/tri_homecart/README.md
@@ -0,0 +1,7 @@
+# TRI HomeCart
+
+The HomeCart is a bimanual manipulation station developed at the Toyota
+Research Institute (TRI).  The model files contained here are all developed by TRI
+and distributed under Drake's license.  They represent a slightly simplified
+version of the model files used internally at TRI (most notably they are using
+a simplified version of the grippers). 
---
+++ manipulation/models/ur3e/test/parse_test.cc
@@ -0,0 +1,41 @@
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "drake/common/find_resource.h"
+#include "drake/multibody/parsing/parser.h"
+#include "drake/multibody/plant/multibody_plant.h"
+
+namespace drake {
+namespace manipulation {
+namespace {
+
+GTEST_TEST(ParseTest, SpheresCollision) {
+  multibody::MultibodyPlant<double> plant(0.0);
+  geometry::SceneGraph<double> scene_graph;
+  plant.RegisterAsSourceForSceneGraph(&scene_graph);
+  multibody::Parser parser(&plant);
+  parser.AddModelFromFile(FindResourceOrThrow(
+          "drake/manipulation/models/ur3e/ur3e_spheres_collision.urdf"));
+  plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("ur_base_link"));
+  plant.Finalize();
+
+  EXPECT_EQ(plant.num_positions(), 6);
+}
+
+GTEST_TEST(ParseTest, CylindersCollision) {
+  multibody::MultibodyPlant<double> plant(0.0);
+  geometry::SceneGraph<double> scene_graph;
+  plant.RegisterAsSourceForSceneGraph(&scene_graph);
+  multibody::Parser parser(&plant);
+  parser.AddModelFromFile(FindResourceOrThrow(
+          "drake/manipulation/models/ur3e/ur3e_cylinders_collision.urdf"));
+  plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("ur_base_link"));
+  plant.Finalize();
+
+  EXPECT_EQ(plant.num_positions(), 6);
+}
+
+}  // namespace
+}  // namespace manipulation
+}  // namespace drake
