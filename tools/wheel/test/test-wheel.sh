20fee2c0061728b2b780643732798e41ad2fc5fe
Matthew Woehlke <matthew.woehlke@kitware.com>
2022-06-15 17:58:32 -0400

    Add a check of the wheel size (#17403)
    * Add a check of the wheel size
    
    Add an additional test to the wheel test script which checks if the
    wheel size is greater than PyPI's per-wheel size limit. This will give
    us earlier warning if the wheel grows too large (which it is, as of
    writing).

--- tools/wheel/test/test-wheel.sh
+++ tools/wheel/test/test-wheel.sh
@@ -44,3 +44,15 @@ prog.AddQuadraticCost(numpy.eye(2), numpy.zeros(2), x)
 solver = pydrake.all.IpoptSolver()
 assert solver.Solve(prog, None, None).is_success(), "IPOPT is not usable"
 EOF
+
+python - "$1" << EOF
+import os
+import sys
+
+# Check that wheel size is within PyPI's per-wheel size quota.
+wheel_size = os.path.getsize(sys.argv[1])
+wheel_size_in_mib = wheel_size / float(1 << 20)
+pypi_wheel_max_size = 100 << 20  # 100 MiB
+fail_message = f"Wheel is too large ({wheel_size_in_mib:.2f} MiB) for PyPI"
+assert wheel_size < pypi_wheel_max_size, fail_message
+EOF
