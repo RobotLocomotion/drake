2ec7e7ee92c7c367c7008d0745a946d2b7f0e7cf
Zach Fang <zach.fang@tri.global>
2022-06-20 12:54:30 -0400

    [geometry] Promote RenderEngineGltfClient and its tests
    Move RenderEngineGltfClient to //geometry/render_gltf_client
    folder and separate factory.{h, cc} as its own cc_library.

--- geometry/render_gltf_client/internal_render_engine_gltf_client.h
+++ geometry/render_gltf_client/internal_render_engine_gltf_client.h
@@ -3,7 +3,6 @@
 #include <memory>
 #include <string>
 
-#include "drake/geometry/render/dev/render_gltf_client/factory.h"
 #include "drake/geometry/render_gltf_client/internal_render_client.h"
 #include "drake/geometry/render_vtk/internal_render_engine_vtk.h"
 
@@ -12,9 +11,9 @@ namespace geometry {
 namespace render_gltf_client {
 namespace internal {
 
-/* A RenderClient that exports
+/* A RenderEngine that exports
  <a href="https://www.khronos.org/registry/glTF/specs/2.0/glTF-2.0.html">glTF
- </a> scenes to upload to a render server. */
+ </a> scenes, uploads to a render server, and retrieves the renderings back. */
 class RenderEngineGltfClient : public geometry::render::RenderEngineVtk {
  public:
   /* @name Does not allow copy, move, or assignment  */
@@ -29,14 +28,22 @@ class RenderEngineGltfClient : public geometry::render::RenderEngineVtk {
   RenderEngineGltfClient& operator=(RenderEngineGltfClient&&) = delete;
   //@}}
 
-  /* Constructs the render engine from the given `parameters`.  By default the
-   %RenderEngineGltfClient will communicate with a local server.
+  /* Constructs the render engine from the given `parameters`.  By default,
+   RenderEngineGltfClient will communicate with a local server.
    @sa RenderEngineGltfClientParams */
   RenderEngineGltfClient(const RenderEngineGltfClientParams& parameters =
                              RenderEngineGltfClientParams());
 
-  // TODO(svenevs): remove when VTK is updated, see implementation for details.
+  // TODO(svenevs): Remove when VTK is updated, see implementation for details.
   void UpdateViewpoint(const math::RigidTransformd& X_WC) override;
+
+  const RenderEngineGltfClientParams& get_params() const {
+    return render_client_->get_params();
+  }
+
+  const std::string& temp_directory() const {
+    return render_client_->temp_directory();
+  }
 
  protected:
   /* Copy constructor for the purpose of cloning. */
@@ -61,23 +68,12 @@ class RenderEngineGltfClient : public geometry::render::RenderEngineVtk {
       const geometry::render::ColorRenderCamera& camera,
       systems::sensors::ImageLabel16I* label_image_out) const override;
 
-  /* Returns the path to export a glTF scene file to for the specified
-  `image_type` and `scene_id`.  The returned path is constructed as
-  `{RenderClient::temp_directory()}/{scene_id}-{image_type}.gltf`. */
-  std::string ExportPathFor(geometry::render::internal::ImageType image_type,
-                            int64_t scene_id) const;
-
   /* Exports the `RenderEngineVtk::pipelines_[image_type]` VTK scene to a
-  glTF file, returning the path to the newly exported file. */
-  std::string ExportScene(geometry::render::internal::ImageType image_type,
-                          int64_t scene_id) const;
+   glTF file given `export_path`. */
+  void ExportScene(const std::string& export_path,
+                   geometry::render::internal::ImageType image_type) const;
 
-  /* Deletes the files at the paths `scene_path` and `image_path`.  Should only
-   be called when `!no_cleanup()`. */
-  void CleanupFrame(const std::string& scene_path,
-                    const std::string& image_path) const;
-
-  /* Helper access method for testing UpdateViewpoint matrix inversion for the
+  /* Helper access function for testing UpdateViewpoint matrix inversion for the
    specified image_type.  Only used for testing. */
   Eigen::Matrix4d CameraModelViewTransformMatrix(
       geometry::render::internal::ImageType image_type) const;
