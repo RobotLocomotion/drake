17e1e7d61ed807446587b2b15a79ec5dedc981cb
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-06-22 15:52:08 -0700

    Lock down the MPLBACKEND setting for a few demo programs

--- bindings/pydrake/math_example.py
+++ bindings/pydrake/math_example.py
@@ -1,10 +1,28 @@
-"""Shows examples using math utilities."""
+"""Shows examples using math utilities.
 
+To see this interactively:
+  bazel run //bindings/pydrake:py/math_example
+"""
+
+# To be a meaningful unit test, we must render the figure somehow.
+# The Agg backend (creating a PNG image) is suitably cross-platform.
+# Users should feel free to use a different back-end in their own code.
+import os
+os.environ['MPLBACKEND'] = 'Agg'  # noqa
+
+# Now that the environment is set up, it's safe to import matplotlib, etc.
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import webbrowser
 
 from pydrake.math import BarycentricMesh
+
+# If running under `bazel run`, output to cwd so the user can find it.
+# If running under `bazel test`, avoid polluting the test's cwd.
+for env_name in ['BUILD_WORKING_DIRECTORY', 'TEST_TMPDIR']:
+    if env_name in os.environ:
+        os.chdir(os.environ[env_name])
 
 # Plot a surface using BarycentricMesh.
 mesh = BarycentricMesh([{0, 1}, {0, 1}])
@@ -20,4 +38,10 @@ Z = np.reshape(values, X.shape)
 ax.plot_surface(X, Y, Z)
 ax.set_xlabel('x')
 ax.set_ylabel('y')
-plt.show()
+
+plt.savefig('math_example.png')
+assert os.path.exists('math_example.png')
+
+# Show the figure (but not when testing).
+if 'TEST_TMPDIR' not in os.environ:
+    webbrowser.open_new_tab(url='math_example.png')
