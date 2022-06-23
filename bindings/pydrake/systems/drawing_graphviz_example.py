8fb9dd01ce68817eba2a61360139cf60083a4d30
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-06-22 17:13:33 -0700

    Lock down the MPLBACKEND setting for a few demo programs (#17437)

--- bindings/pydrake/systems/drawing_graphviz_example.py
+++ bindings/pydrake/systems/drawing_graphviz_example.py
@@ -1,4 +1,18 @@
+"""Shows a quick demo of pydrake.systems.drawing.
+
+To see this interactively:
+  bazel run //bindings/pydrake/systems:drawing_graphviz_example
+"""
+
+# We must render the figures somehow.
+# The Agg backend (creating a PNG image) is suitably cross-platform.
+# Users should feel free to use a different back-end in their own code.
+import os
+os.environ['MPLBACKEND'] = 'Agg'  # noqa
+
+# Now that the environment is set up, it's safe to import matplotlib, etc.
 import matplotlib.pyplot as plt
+import webbrowser
 
 from pydrake.common import FindResourceOrThrow
 from pydrake.geometry import DrakeVisualizer, SceneGraph
@@ -8,6 +22,12 @@ from pydrake.multibody.plant import MultibodyPlant
 from pydrake.systems.drawing import plot_graphviz, plot_system_graphviz
 from pydrake.systems.framework import DiagramBuilder
 
+# If running under `bazel run`, output to cwd to the user can find it.
+# If running under `bazel test` avoid polluting the test's cwd.
+for env_name in ['BUILD_WORKING_DIRECTORY', 'TEST_TMPDIR']:
+    if env_name in os.environ:
+        os.chdir(os.environ[env_name])
+
 file_name = FindResourceOrThrow(
     "drake/examples/multibody/cart_pole/cart_pole.sdf")
 builder = DiagramBuilder()
@@ -16,8 +36,10 @@ cart_pole = builder.AddSystem(MultibodyPlant(0.0))
 cart_pole.RegisterAsSourceForSceneGraph(scene_graph)
 Parser(plant=cart_pole).AddModelFromFile(file_name)
 
-plt.figure()
+plt.figure(figsize=(11, 8.5), dpi=300)
 plot_graphviz(cart_pole.GetTopologyGraphvizString())
+plt.savefig('cart_pole_topology.png')
+assert os.path.exists('cart_pole_topology.png')
 
 cart_pole.Finalize()
 assert cart_pole.geometry_source_is_registered()
@@ -35,7 +57,12 @@ DrakeVisualizer.AddToBuilder(builder=builder, scene_graph=scene_graph)
 diagram = builder.Build()
 diagram.set_name("graphviz_example")
 
-plt.figure()
+plt.figure(figsize=(11, 8.5), dpi=300)
 plot_system_graphviz(diagram, max_depth=2)
+plt.savefig('drawing_graphviz_example.png')
+assert os.path.exists('drawing_graphviz_example.png')
 
-plt.show()
+# Show the figures (but not when testing).
+if 'TEST_TMPDIR' not in os.environ:
+    webbrowser.open_new_tab(url='cart_pole_topology.png')
+    webbrowser.open_new_tab(url='drawing_graphviz_example.png')
