c2a2956afecb73537c96808af064e8bea28cd42d
Grant Gould <ggould@alum.mit.edu>
2022-06-27 10:54:09 -0400

    Clarify `channel_suffix` semantics for `SubscribeAllChannels`
    * The previous documentation said the channel name is "unchanged"
      but this could mean either "unchanged by the suffix mechanism"
      or "unchanged from its suffixed form".
    * Clarify that the callback does receive the suffix.
    * Nail this down with a test.

--- lcm/test/drake_lcm_test.cc
+++ lcm/test/drake_lcm_test.cc
@@ -380,6 +380,18 @@ TEST_F(DrakeLcmTest, Suffix) {
     Publish(dut_.get(), "SuffixDrakeLcmTest", message_);
     native_lcm->handleTimeout(50 /* millis */);
   });
+
+  // SubscribeAll using Drake LCM, expecting to see the fully qualified
+  // channel name.
+  received_drake = lcmt_drake_signal();
+  subscription = dut_->SubscribeAllChannels([&received_drake](
+      std::string_view channel_name, const void*, int) {
+    EXPECT_EQ(channel_name, "SuffixDrakeLcmTest_SUFFIX");
+  });
+  LoopUntilDone(&received_drake, 20 /* retries */, [&]() {
+    Publish(dut_.get(), "SuffixDrakeLcmTest", message_);
+    native_lcm->handleTimeout(50 /* millis */);
+  });
 }
 
 }  // namespace
