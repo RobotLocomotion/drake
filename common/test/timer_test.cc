72e4db39fed74e2f1d1ba58f0e71c5610d1b6925
hammerpa <hammerpa@amazon.com>
2022-07-07 13:20:20 -0700

    make timer_test more robust.

--- common/test/timer_test.cc
+++ common/test/timer_test.cc
@@ -7,27 +7,23 @@
 namespace drake {
 
 GTEST_TEST(TimeTest, Everything) {
-  using std::chrono::duration;
-  const duration<double> kLongInterval = std::chrono::milliseconds(100);
-  const duration<double> kShortInterval = std::chrono::milliseconds(10);
-
   SteadyTimer timer;
 
   // Clock starts upon construction. Allow some time to pass and confirm
   // positive measurement.
-  std::this_thread::sleep_for(kLongInterval);
-  const double T1 = timer.Tick();
-  EXPECT_GT(T1, 0.0);
+  EXPECT_GT(timer.Tick(), 0.0);
 
-  // Start resets the clock. Allow a smaller time to pass and confirm positive
-  // measurement that is less than T1.
+  // Start resets the clock. Allow a small time to pass and confirm timer
+  // measures at least that amount.
   timer.Start();
-  std::this_thread::sleep_for(kShortInterval);
-  const double T2 = timer.Tick();
-  EXPECT_GT(T2, 0.0);
-  EXPECT_LT(T2, T1);
+  using std::chrono::duration;
+  const duration<double> kShortInterval = std::chrono::milliseconds(10);
+  std::this_thread::sleep_for(kShortInterval);  // sleep for at least 10ms
+  const double t = timer.Tick();
+  EXPECT_GT(t, 0.0);
+  EXPECT_GE(t, std::chrono::duration<double>(kShortInterval).count());
 
   // As time progresses, the tick value monotonically increases.
-  EXPECT_GT(timer.Tick(), T2);
+  EXPECT_GT(timer.Tick(), t);
 }
 }  // namespace drake
