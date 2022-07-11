f54acba40d618276a9f237adf0bd25afc0c1c36d
Rick Poyner <rick.poyner@tri.global>
2022-07-11 19:06:10 -0400

    [visualization] Repair meldis --host option
    Broken by #16981.

--- bindings/pydrake/visualization/meldis.py
+++ bindings/pydrake/visualization/meldis.py
@@ -237,7 +237,8 @@ class Meldis:
         lcm_url = self._lcm.get_lcm_url()
         _logger.info(f"Meldis is listening for LCM messages at {lcm_url}")
 
-        params = MeshcatParams(host="localhost", port=meshcat_port,
+        params = MeshcatParams(host=meshcat_host or "localhost",
+                               port=meshcat_port,
                                show_stats_plot=False)
         self.meshcat = Meshcat(params=params)
 
