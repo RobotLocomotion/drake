9ec61cb5e2460ac46cf65df4caec29c95c100947
Alexander Karatarakis <alex@karatarakis.com>
2022-07-16 19:53:02 -0700

    Fix missing internal::enable_if. Replace with std::enable_if
    This was an internal type_trait from Eigen, to support compilations
    below C++11. Eigen is dropping support for old C++ version, and
    removed this type_trait:
    https://gitlab.com/libeigen/eigen/-/merge_requests/732
    
    Per https://drake.mit.edu/from_source.html
    Drake requires C++17/20 and already uses std::enable_if(_t)
    in other places.

--- common/autodiffxd.h
+++ common/autodiffxd.h
@@ -106,7 +106,7 @@ class AutoDiffScalar<VectorXd>
       const AutoDiffScalar<OtherDerType>& other
 #ifndef EIGEN_PARSED_BY_DOXYGEN
       ,
-      typename internal::enable_if<
+      typename std::enable_if<
           internal::is_same<
               Scalar, typename internal::traits<typename internal::remove_all<
                           OtherDerType>::type>::Scalar>::value,
