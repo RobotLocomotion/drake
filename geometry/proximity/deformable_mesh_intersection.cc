d9a9d9fe9549bc906467b6209073da7ebd93ccfc
Xuchen Han <xuchen.han@tri.global>
2022-07-18 17:41:43 -0700

    [geometry] Build deformable sdf from distance to surface mesh
    Remove the ReferenceDeformableGeometry class and instead stores the
    approximate signed distance field directly in deformable geometry. With
    this, we unlock arbitrary volume meshes (with potentially non-convex
    surface) as deformable geomtries.
    
    While doing that, we ensure that the mesh inside the SDF points to the
    deformable mesh held by DeformableGeometry so that we don't have to
    create local copies in deformable_mesh_intersection.cc.

--- geometry/proximity/deformable_mesh_intersection.cc
+++ geometry/proximity/deformable_mesh_intersection.cc
@@ -81,28 +81,12 @@ void AppendDeformableRigidContact(
     const math::RigidTransform<double>& X_DR,
     DeformableRigidContact<double>* deformable_rigid_contact) {
   DRAKE_DEMAND(deformable_rigid_contact != nullptr);
-  const VolumeMesh<double>& mesh = deformable_D.deformable_mesh().mesh();
-  // TODO(DamrongGuoy) Is there a better way than creating a new
-  //  VolumeMeshFieldLinear here? We do it here, so we can reuse
-  //  SurfaceVolumeIntersector. These are some ideas that Xuchen and Damrong
-  //  consider for future refactoring:
-  //  1. Change the type parameter MeshBuilder of SurfaceVolumeIntersector<>
-  //     to provide the tetrahedral mesh. This includes TriMeshBuilder and
-  //     PolyMeshBuilder.
-  //  Or 2. Allow VolumeMeshFieldLinear to switch to a different tetrahedral
-  //        mesh while keeping the same field values at vertices. Assume that
-  //        the new mesh has the same connectivity.
-  //  Or 3. Pass an additional parameter for the tetrahedral mesh to
-  //        SampleVolumeFieldOnSurface(). Right now it uses the mesh of the
-  //        given VolumeMeshFieldLinear.
-  VolumeMeshFieldLinear<double, double> field_D(
-      std::vector<double>(deformable_D.signed_distance_field().values()), &mesh,
-      true /*calculate gradient*/);
 
   DeformableSurfaceVolumeIntersector intersect;
   intersect.SampleVolumeFieldOnSurface(
-      field_D, deformable_D.deformable_mesh().bvh(), rigid_mesh_R, rigid_bvh_R,
-      X_DR, false /* don't filter face normal along field gradient */);
+      deformable_D.signed_distance_field(),
+      deformable_D.deformable_mesh().bvh(), rigid_mesh_R, rigid_bvh_R, X_DR,
+      false /* don't filter face normal along field gradient */);
 
   if (intersect.has_intersection()) {
     std::unique_ptr<PolygonSurfaceMesh<double>> contact_mesh_W =
@@ -120,6 +104,7 @@ void AppendDeformableRigidContact(
           i, contact_mesh_W->element_centroid(i));
     }
 
+    const VolumeMesh<double>& mesh = deformable_D.deformable_mesh().mesh();
     std::vector<int>& participating_tetrahedra =
         intersect.mutable_tetrahedron_index_of_polygons();
     std::unordered_set<int> participating_vertices;
