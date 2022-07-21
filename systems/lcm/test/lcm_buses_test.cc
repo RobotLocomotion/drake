869e8fbf038ff8c319f81af62c03f3848eb9656c
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-07-21 13:48:23 -0700

    [lcm] Add LcmBuses files copied from Anzu
    Co-authored-by: zachfang <zach.fang@tri.global>

---
+++ systems/lcm/test/lcm_buses_test.cc
@@ -0,0 +1,38 @@
+#include "sim/common/lcm_buses.h"
+
+#include <gtest/gtest.h>
+
+#include "drake/lcm/drake_lcm.h"
+
+using drake::lcm::DrakeLcm;
+using drake::lcm::DrakeLcmInterface;
+
+namespace anzu {
+namespace sim {
+namespace {
+
+// A basic acceptance test.
+GTEST_TEST(LcmBuses, Basic) {
+  // Start with an empty map.
+  LcmBuses dut;
+  EXPECT_EQ(dut.size(), 0);
+  EXPECT_THROW(dut.Find("foo", "foo"), std::exception);
+  EXPECT_EQ(dut.GetAllBusNames().size(), 0);
+
+  // Add a single bus.
+  DrakeLcm lcm;
+  dut.Add("default", &lcm);
+  EXPECT_EQ(dut.size(), 1);
+  EXPECT_NO_THROW(dut.Find("Basic test", "default"));
+  EXPECT_EQ(dut.GetAllBusNames(), std::vector<std::string_view>{"default"});
+
+  // Re-adding the same name will throw.
+  EXPECT_THROW(dut.Add("default", &lcm), std::exception);
+
+  // Adding nullptr will throw.
+  EXPECT_THROW(dut.Add("nullptr", nullptr), std::exception);
+}
+
+}  // namespace
+}  // namespace sim
+}  // namespace anzu
