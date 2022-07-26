25fb7b24139094d7894f219c8b7e3e7a27041aa8
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-07-26 11:34:08 -0700

    [ad] Add AutoDiff replacement class for AutoDiffXd
    This is private for now; it is not used outside of its unit tests.

---
+++ common/ad/test/auto_diff_basic_test.cc
@@ -0,0 +1,54 @@
+#include <gtest/gtest.h>
+
+#include "drake/common/ad/auto_diff.h"
+#include "drake/common/eigen_types.h"
+
+// This file contains only the very most basic unit tests for AutoDiffXd, e.g.,
+// construction and assignment. Tests for the derivative math are elsewhere.
+
+namespace drake {
+namespace ad {
+namespace {
+
+using Eigen::VectorXd;
+
+GTEST_TEST(AutodiffBasicTest, DefaultCtor) {
+  const AutoDiff dut;
+  EXPECT_EQ(dut.value(), 0.0);
+  EXPECT_EQ(dut.derivatives().size(), 0);
+}
+
+GTEST_TEST(AutodiffBasicTest, ConvertingCtor) {
+  const AutoDiff dut{1.0};
+  EXPECT_EQ(dut.value(), 1.0);
+  EXPECT_EQ(dut.derivatives().size(), 0);
+}
+
+GTEST_TEST(AutodiffBasicTest, UnitCtor) {
+  const AutoDiff dut{1.0, 4, 2};
+  EXPECT_EQ(dut.value(), 1.0);
+  EXPECT_EQ(dut.derivatives().size(), 4);
+  EXPECT_EQ(dut.derivatives()[2], 1.0);
+}
+
+GTEST_TEST(AutodiffBasicTest, VectorCtor) {
+  const Eigen::VectorXd derivs = Eigen::VectorXd::LinSpaced(3, 1.0, 3.0);
+  const AutoDiff dut{1.0, derivs};
+  EXPECT_EQ(dut.value(), 1.0);
+  EXPECT_EQ(dut.derivatives().size(), 3);
+  EXPECT_EQ(dut.derivatives(), derivs);
+}
+
+GTEST_TEST(AutodiffBasicTest, AssignConstant) {
+  AutoDiff dut{1.0, 4, 2};
+  EXPECT_EQ(dut.value(), 1.0);
+  EXPECT_EQ(dut.derivatives().size(), 4);
+  dut = -1.0;
+  EXPECT_EQ(dut.value(), -1.0);
+  EXPECT_EQ(dut.derivatives().size(), 4);
+  EXPECT_TRUE(dut.derivatives().isZero(0.0));
+}
+
+}  // namespace
+}  // namespace ad
+}  // namespace drake
