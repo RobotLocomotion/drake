bb9dc7087eb4e99c2ed891c786fb4ba1ebdfb670
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-07-26 19:03:04 -0700

    PR [ad] Add support for arithmetic operations

---
+++ common/ad/test/standard_operations_add_test.cc
@@ -0,0 +1,33 @@
+#include "drake/common/ad/auto_diff.h"
+#include "drake/common/ad/test/standard_operations_test.h"
+
+namespace drake {
+namespace test {
+namespace {
+
+TEST_F(StandardOperationsTest, Addition) {
+  CHECK_BINARY_OP(+, x, y, 1.0);
+  CHECK_BINARY_OP(+, x, y, -1.0);
+  CHECK_BINARY_OP(+, y, x, 1.0);
+  CHECK_BINARY_OP(+, y, x, -1.0);
+}
+
+namespace {
+
+// We need to wrap the operator under test, to give it a name.
+// Eigen doesn't provide unary operator+, so we'll no-op instead.
+AutoDiffDut unary_add(const AutoDiffDut& x) { return +x; }
+AutoDiff3 unary_add(const AutoDiff3& x) { return x; }
+
+}  // namespace
+
+TEST_F(StandardOperationsTest, UnaryAddition) {
+  CHECK_UNARY_FUNCTION(unary_add, x, y, 1.0);
+  CHECK_UNARY_FUNCTION(unary_add, x, y, -1.0);
+  CHECK_UNARY_FUNCTION(unary_add, y, x, 1.0);
+  CHECK_UNARY_FUNCTION(unary_add, y, x, -1.0);
+}
+
+}  // namespace
+}  // namespace test
+}  // namespace drake
