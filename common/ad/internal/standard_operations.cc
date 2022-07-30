5db25b8e6d9bb8d4968b68dc5d844be9ee822002
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-07-30 08:56:01 -0700

    [ad] Add support for trig and hyperbolic operations

--- common/ad/internal/standard_operations.cc
+++ common/ad/internal/standard_operations.cc
@@ -13,6 +13,100 @@
 namespace drake {
 namespace ad {
 
+AutoDiff sin(AutoDiff x) {
+  // ∂/∂x sin(x) = cos(x)
+  x.partials().Mul(std::cos(x.value()));
+  x.value() = std::sin(x.value());
+  return x;
+}
+
+AutoDiff cos(AutoDiff x) {
+  // ∂/∂x cos(x) = -sin(x)
+  x.partials().Mul(-std::sin(x.value()));
+  x.value() = std::cos(x.value());
+  return x;
+}
+
+AutoDiff tan(AutoDiff x) {
+  // ∂/∂x tan(x) = sec²(x) = cos⁻²(x)
+  const double cos_x = std::cos(x.value());
+  x.partials().Div(cos_x * cos_x);
+  x.value() = std::tan(x.value());
+  return x;
+}
+
+AutoDiff asin(AutoDiff x) {
+  // ∂/∂x acos(x) = 1 / sqrt(1 - x²)
+  const double x2 = x.value() * x.value();
+  x.partials().Div(std::sqrt(1 - x2));
+  x.value() = std::asin(x.value());
+  return x;
+}
+
+AutoDiff acos(AutoDiff x) {
+  // ∂/∂x acos(x) = -1 / sqrt(1 - x²)
+  const double x2 = x.value() * x.value();
+  x.partials().Div(-std::sqrt(1 - x2));
+  x.value() = std::acos(x.value());
+  return x;
+}
+
+AutoDiff atan(AutoDiff x) {
+  // ∂/∂x atan(x) = 1 / (1 + x²)
+  const double x2 = x.value() * x.value();
+  x.partials().Div(1 + x2);
+  x.value() = std::atan(x.value());
+  return x;
+}
+
+AutoDiff atan2(AutoDiff a, const AutoDiff& b) {
+  // ∂/∂x atan2(a, b) = (ba' - ab')/(a² + b²)
+  const double norm = a.value() * a.value() + b.value() * b.value();
+  a.partials().Mul(b.value());
+  a.partials().AddScaled(-a.value(), b.partials());
+  a.partials().Div(norm);
+  a.value() = std::atan2(a.value(), b.value());
+  return a;
+}
+
+AutoDiff atan2(AutoDiff a, double b) {
+  // ∂/∂x atan2(a, b) = (ba' - ab')/(a² + b²) = ba'/(a² + b²)
+  const double norm = a.value() * a.value() + b * b;
+  a.partials().Mul(b / norm);
+  a.value() = std::atan2(a.value(), b);
+  return a;
+}
+
+AutoDiff atan2(double a, AutoDiff b) {
+  // ∂/∂x atan2(a, b) = (ba' - ab')/(a² + b²) = -ab'/(a² + b²)
+  const double norm = a * a + b.value() * b.value();
+  b.partials().Mul(-a / norm);
+  b.value() = std::atan2(a, b.value());
+  return b;
+}
+
+AutoDiff sinh(AutoDiff x) {
+  // ∂/∂x sinh(x) = cosh(x)
+  x.partials().Mul(std::cosh(x.value()));
+  x.value() = std::sinh(x.value());
+  return x;
+}
+
+AutoDiff cosh(AutoDiff x) {
+  // ∂/∂x cosh(x) = sinh(x)
+  x.partials().Mul(std::sinh(x.value()));
+  x.value() = std::cosh(x.value());
+  return x;
+}
+
+AutoDiff tanh(AutoDiff x) {
+  // ∂/∂x tanh(x) = 1 - tanh²(x)
+  const double new_value = std::tanh(x.value());
+  x.partials().Mul(1 - (new_value * new_value));
+  x.value() = new_value;
+  return x;
+}
+
 std::ostream& operator<<(std::ostream& s, const AutoDiff& x) {
   return s << fmt::format("{}", x.value());
 }
--- common/ad/test/standard_operations_test.h
+++ common/ad/test/standard_operations_test.h
@@ -79,7 +79,7 @@ class StandardOperationsTest : public ::testing::Test {
 
     return CompareMatrices(
         value_and_der_x, value_and_der_3,
-        2.0 * std::numeric_limits<double>::epsilon(),
+        4.0 * std::numeric_limits<double>::epsilon(),
         MatrixCompareType::relative)
       << "\n(where xd.size() = " << e_xd.derivatives().size() << ")";
   }
@@ -106,10 +106,6 @@ class StandardOperationsTest : public ::testing::Test {
   CHECK_EXPR(x bop(c bop y));         \
   CHECK_EXPR(c bop(x bop y));
 
-// The multiplicative factor 0.9 < 1.0 let us call function such as asin, acos,
-// etc. whose arguments must be in (-1, 1).
-// The additive factor 5.0 let us call functions whose arguments must be
-// positive.
 #define CHECK_UNARY_FUNCTION(f, x, y, c) \
   CHECK_EXPR(f(x + x) + (y + y));        \
   CHECK_EXPR(f(x + y) + (x + y));        \
@@ -152,23 +148,31 @@ class StandardOperationsTest : public ::testing::Test {
   CHECK_EXPR(f(c* x, c* x) + y);                  \
   CHECK_EXPR(f(-x, -y) + y)
 
-// The additive factor 5.0 let us call functions whose arguments must be
-// positive in order to have well defined derivatives. Eg.: sqrt, pow.
 #define CHECK_BINARY_FUNCTION_ADS_SCALAR(f, x, y, c) \
   CHECK_EXPR(f(x, c) + y);                           \
   CHECK_EXPR(f(x + x, c) + y);                       \
   CHECK_EXPR(f(x + y, c) + y);                       \
   CHECK_EXPR(f(x - x + 5.0, c) - y);                 \
-  CHECK_EXPR(f(x - x + 5.0, c) - y);                 \
-  CHECK_EXPR(f(x* x, c) * y);                        \
-  CHECK_EXPR(f(x* x, c) * y);                        \
-  CHECK_EXPR(f(x / x, c) / y);                       \
+  CHECK_EXPR(f(x * x, c) * y);                       \
   CHECK_EXPR(f(x / x, c) / y);                       \
   CHECK_EXPR(f(x + c, c) + y);                       \
   CHECK_EXPR(f(c + x, c) + y);                       \
-  CHECK_EXPR(f(x* c, c) + y);                        \
-  CHECK_EXPR(f(c* x, c) + y);                        \
+  CHECK_EXPR(f(x * c, c) + y);                       \
+  CHECK_EXPR(f(c * x, c) + y);                       \
   CHECK_EXPR(f(-x, c) + y);
+
+#define CHECK_BINARY_FUNCTION_SCALAR_ADS(f, x, y, c) \
+  CHECK_EXPR(f(c, x) + y);                           \
+  CHECK_EXPR(f(c, x + x) + y);                       \
+  CHECK_EXPR(f(c, x + y) + y);                       \
+  CHECK_EXPR(f(c, x - x + 5.0) - y);                 \
+  CHECK_EXPR(f(c, x * x) * y);                       \
+  CHECK_EXPR(f(c, x / x) / y);                       \
+  CHECK_EXPR(f(c, x + c) + y);                       \
+  CHECK_EXPR(f(c, c + x) + y);                       \
+  CHECK_EXPR(f(c, x * c) + y);                       \
+  CHECK_EXPR(f(c, c * x) + y);                       \
+  CHECK_EXPR(f(c, -x) + y);
 
 }  // namespace test
 }  // namespace drake
