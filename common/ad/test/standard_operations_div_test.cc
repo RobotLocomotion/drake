5caf1a0f93ce0128e9e87e1dab7102826debf95b
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-07-30 09:36:58 -0700

    [ad] Fix gradients of in-place x*x and x/x

--- common/ad/test/standard_operations_div_test.cc
+++ common/ad/test/standard_operations_div_test.cc
@@ -12,6 +12,15 @@ TEST_F(StandardOperationsTest, Division) {
   CHECK_BINARY_OP(/, y, x, -1.0);
 }
 
+// The CHECK_BINARY_OP typically copies the first argument, so does not test
+// aliasing between the two arguments. We'll do that here specifically.
+TEST_F(StandardOperationsTest, InPlaceDivision) {
+  AutoDiffDut x{0.5, 3, 0};
+  x /= x;
+  EXPECT_EQ(x.value(), 1.0);
+  EXPECT_EQ(x.derivatives(), Eigen::Vector3d::Zero());
+}
+
 }  // namespace
 }  // namespace test
 }  // namespace drake
