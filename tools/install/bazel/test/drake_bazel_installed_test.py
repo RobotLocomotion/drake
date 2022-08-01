8a53b0c909e76a0367902979aed16defde3b5733
Matthew Woehlke <matthew.woehlke@kitware.com>
2022-08-01 13:51:59 -0400

    [workspace] Upgrade rules_python to latest release 0.10.2

--- tools/install/bazel/test/drake_bazel_installed_test.py
+++ tools/install/bazel/test/drake_bazel_installed_test.py
@@ -16,9 +16,9 @@ def main():
 
     # The commit (version) here should be identical to the commit listed in
     # drake/tools/workspace/rules_python/repository.bzl.
-    rules_python_commit = "0.10.0"
+    rules_python_commit = "0.10.2"
     rules_python_url = f"https://github.com/bazelbuild/rules_python/archive/{rules_python_commit}.tar.gz"  # noqa
-    rules_python_sha256 = "56dc7569e5dd149e576941bdb67a57e19cd2a7a63cc352b62ac047732008d7e1"  # noqa
+    rules_python_sha256 = "a3a6e99f497be089f81ec082882e40246bfd435f52f4e82f37e89449b04573f6"  # noqa
 
     with open(join(scratch_dir, "WORKSPACE"), "w") as f:
         f.write(f"""
