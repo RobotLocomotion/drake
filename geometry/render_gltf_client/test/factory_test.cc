1fbc917e8c3bfb736c2a5d9a7c3667f0e970fda4
Zach Fang <zach.fang@tri.global>
2022-08-02 16:06:19 -0400

    [geometry] Move MakeRenderEngineGltfClient() from the dev folder

---
+++ geometry/render_gltf_client/test/factory_test.cc
@@ -0,0 +1,51 @@
+#include "drake/geometry/render_gltf_client/factory.h"
+
+#include <gtest/gtest.h>
+
+#include "drake/geometry/render/render_engine.h"
+#include "drake/geometry/render_gltf_client/internal_render_engine_gltf_client.h"
+#include "drake/geometry/render_gltf_client/render_engine_gltf_client_params.h"
+
+namespace drake {
+namespace geometry {
+namespace {
+
+using render::RenderEngine;
+using render_gltf_client::internal::RenderEngineGltfClient;
+
+GTEST_TEST(FactoryTest, ConstructWithDefaultParams) {
+  const std::unique_ptr<RenderEngine> render_engine =
+      MakeRenderEngineGltfClient(RenderEngineGltfClientParams{});
+  const RenderEngineGltfClient* render_engine_gltf_client =
+      dynamic_cast<RenderEngineGltfClient*>(render_engine.get());
+
+  EXPECT_NE(render_engine_gltf_client, nullptr);
+  // Check the default RenderEngineGltfClientParams is propagated properly.
+  EXPECT_EQ(render_engine_gltf_client->get_params().GetUrl(),
+            "http://127.0.0.1:8000/render");
+  EXPECT_EQ(render_engine_gltf_client->get_params().verbose, false);
+  EXPECT_EQ(render_engine_gltf_client->get_params().cleanup, true);
+}
+
+GTEST_TEST(FactoryTest, ConstructWithNonDefaultParams) {
+  const RenderEngineGltfClientParams params{.base_url = "http://127.0.0.1:1234",
+                                            .render_endpoint = "testing",
+                                            .verbose = true,
+                                            .cleanup = false};
+
+  const std::unique_ptr<RenderEngine> render_engine =
+      MakeRenderEngineGltfClient(params);
+  const RenderEngineGltfClient* render_engine_gltf_client =
+      dynamic_cast<RenderEngineGltfClient*>(render_engine.get());
+
+  EXPECT_NE(render_engine_gltf_client, nullptr);
+  // Check the non-default RenderEngineGltfClientParams is propagated properly.
+  EXPECT_EQ(render_engine_gltf_client->get_params().GetUrl(),
+            params.base_url + "/" + params.render_endpoint);
+  EXPECT_EQ(render_engine_gltf_client->get_params().verbose, params.verbose);
+  EXPECT_EQ(render_engine_gltf_client->get_params().cleanup, params.cleanup);
+}
+
+}  // namespace
+}  // namespace geometry
+}  // namespace drake
