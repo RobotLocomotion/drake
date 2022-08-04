5cc1d2656eb97727cf43375c15cf956ed49fbfc6
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-08-04 12:28:10 -0700

    [manipulation] Add ZeroForceDriver for hardware_sim

--- manipulation/util/zero_force_driver_functions.cc
+++ manipulation/util/zero_force_driver_functions.cc
@@ -1,43 +1,37 @@
-#include "sim/common/zero_force_driver_functions.h"
+#include "drake/manipulation/util/zero_force_driver_functions.h"
 
 #include "drake/common/eigen_types.h"
 #include "drake/systems/primitives/constant_vector_source.h"
 
-namespace anzu {
-namespace sim {
+namespace drake {
+namespace manipulation {
 
-using drake::multibody::MultibodyPlant;
-using drake::multibody::ModelInstanceIndex;
-using drake::multibody::parsing::ModelInstanceInfo;
-using drake::systems::DiagramBuilder;
-using drake::systems::lcm::LcmBuses;
+using multibody::MultibodyPlant;
+using multibody::ModelInstanceIndex;
+using multibody::parsing::ModelInstanceInfo;
+using systems::ConstantVectorSource;
+using systems::DiagramBuilder;
+using systems::lcm::LcmBuses;
 
 void ApplyDriverConfig(
-    const ZeroForceDriver& driver_config,
+    const ZeroForceDriver&,
     const std::string& model_instance_name,
     const MultibodyPlant<double>& sim_plant,
-    const std::map<std::string, ModelInstanceInfo>& models_from_directives,
-    const LcmBuses& /* lcms */,
+    const std::map<std::string, ModelInstanceInfo>&,
+    const LcmBuses&,
     DiagramBuilder<double>* builder) {
   DRAKE_THROW_UNLESS(builder != nullptr);
-  if (models_from_directives.count(model_instance_name) == 0) {
-    throw std::runtime_error(fmt::format(
-        "ZeroForceDriver could not find model directive '{}' to actuate",
-        model_instance_name));
-  }
-  const ModelInstanceInfo& model_info =
-      models_from_directives.at(model_instance_name);
-  const ModelInstanceIndex& model_instance = model_info.model_instance;
+  const ModelInstanceIndex& model_instance =
+      sim_plant.GetModelInstanceByName(model_instance_name);
   const int num_dofs = sim_plant.num_actuated_dofs(model_instance);
   // Fail-fast if we try to actuate an inert model.
   DRAKE_THROW_UNLESS(num_dofs > 0);
-  auto actuation =
-      builder->AddSystem<drake::systems::ConstantVectorSource<double>>(
-          drake::VectorX<double>::Zero(num_dofs));
+  auto actuation = builder->AddSystem<ConstantVectorSource<double>>(
+      VectorX<double>::Zero(num_dofs));
   actuation->set_name("zero_force_source_for_" + model_instance_name);
   builder->Connect(actuation->get_output_port(),
                    sim_plant.get_actuation_input_port(model_instance));
 }
 
-}  // namespace sim
-}  // namespace anzu
+}  // namespace manipulation
+}  // namespace drake
--- manipulation/util/zero_force_driver_functions.h
+++ manipulation/util/zero_force_driver_functions.h
@@ -3,25 +3,26 @@
 #include <map>
 #include <string>
 
-#include "drake/multibody/parsing/process_model_directives.h"
+#include "drake/manipulation/util/zero_force_driver.h"
+#include "drake/multibody/parsing/model_instance_info.h"
 #include "drake/multibody/plant/multibody_plant.h"
 #include "drake/systems/framework/diagram_builder.h"
 #include "drake/systems/lcm/lcm_buses.h"
-#include "sim/common/zero_force_driver.h"
 
-namespace anzu {
-namespace sim {
+namespace drake {
+namespace manipulation {
 
 /** Applies zero actuation to every joint of a model mainly for debugging and
-testing purposes. */
+testing purposes.
+@pre The sim_plant.is_finalized() is true. */
 void ApplyDriverConfig(
     const ZeroForceDriver& driver_config,
     const std::string& model_instance_name,
-    const drake::multibody::MultibodyPlant<double>& sim_plant,
-    const std::map<std::string, drake::multibody::parsing::ModelInstanceInfo>&
+    const multibody::MultibodyPlant<double>& sim_plant,
+    const std::map<std::string, multibody::parsing::ModelInstanceInfo>&
         models_from_directives,
-    const drake::systems::lcm::LcmBuses& /* lcms */,
-    drake::systems::DiagramBuilder<double>* builder);
+    const systems::lcm::LcmBuses& lcms,
+    systems::DiagramBuilder<double>* builder);
 
-}  // namespace sim
-}  // namespace anzu
+}  // namespace manipulation
+}  // namespace drake
