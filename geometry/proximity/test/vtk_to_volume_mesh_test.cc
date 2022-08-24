3e10f7f814d3612fede93551f4495e986d5e686d
Stephen McDowell <stephen.mcdowell@kitware.com>
2022-08-24 10:39:25 -0400

    [geometry] ReadVtkToVolumeMesh()
    - Use VTK library to import a tetrahedral mesh.
    - Expose vtkUnstructuredGridReader and supported classes to bazel.

---
+++ geometry/proximity/test/vtk_to_volume_mesh_test.cc
@@ -0,0 +1,42 @@
+#include "drake/geometry/proximity/vtk_to_volume_mesh.h"
+
+#include <gtest/gtest.h>
+
+#include "drake/common/find_resource.h"
+#include "drake/common/test_utilities/expect_throws_message.h"
+
+namespace drake {
+namespace geometry {
+namespace {
+
+using Eigen::Vector3d;
+
+GTEST_TEST(VtkToVolumeMeshTest, OneTetrahedronFile) {
+  const std::string test_file =
+      FindResourceOrThrow("drake/geometry/test/one_tetrahedron.vtk");
+  VolumeMesh<double> volume_mesh = internal::ReadVtkToVolumeMesh(test_file);
+
+  const VolumeMesh<double> expected_mesh{
+      {{0, 1, 2, 3}},
+      {Vector3d::Zero(), Vector3d::UnitX(), Vector3d::UnitY(),
+       Vector3d::UnitZ()}};
+  EXPECT_TRUE(volume_mesh.Equal(expected_mesh));
+}
+
+// With an additional field variable, we can still read the mesh.
+GTEST_TEST(VtkToVolumeMeshTest, KeepMeshIgnoreFieldVariables) {
+  const std::string test_file = FindResourceOrThrow(
+      "drake/geometry/test/two_tetrahedra_with_field_variable.vtk");
+  VolumeMesh<double> volume_mesh =
+      internal::ReadVtkToVolumeMesh(test_file);
+
+  const VolumeMesh<double> expected_mesh{
+      {{0, 1, 2, 3}, {0, 2, 1, 4}},
+      {Vector3d::Zero(), Vector3d::UnitX(), Vector3d::UnitY(),
+       Vector3d::UnitZ(), -Vector3d::UnitZ()}};
+  EXPECT_TRUE(volume_mesh.Equal(expected_mesh));
+}
+
+}  // namespace
+}  // namespace geometry
+}  // namespace drake
