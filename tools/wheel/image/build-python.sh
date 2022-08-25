467abd6e0207a5aa275ab1a084a0e34526ef057e
Matthew Woehlke <matthew.woehlke@kitware.com>
2022-08-25 14:16:22 -0400

    Build wheels with self-built Python 3.10
    Add ability to build Python during wheel build environment provisioning,
    rather than relying on system packages. Add ability to have separate
    build and test platforms for wheels. Add a 3.10 wheel, built on Focal
    using Python 3.10.6 built from source and tested on Jammy.
    
    Co-authored-by: Jeremy Nimmer <jeremy.nimmer@tri.global>

---
+++ tools/wheel/image/build-python.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Internal script to build Python.
+# Docker (Linux) only.
+
+set -eu -o pipefail
+
+readonly VERSION=$1
+readonly PREFIX=$2
+readonly SHA=$3
+
+apt-get -y update
+apt-get -y install --no-install-recommends \
+    build-essential \
+    libbz2-dev \
+    libc6-dev \
+    libssl-dev
+
+readonly ARCHIVE=Python-$VERSION.tar.xz
+readonly URL=https://www.python.org/ftp/python/$VERSION/$ARCHIVE
+readonly SRC_DIR=/opt/drake-wheel-build/python
+
+mkdir -p $SRC_DIR
+cd $SRC_DIR
+
+wget $URL
+echo "$SHA  $ARCHIVE" | sha256sum -c
+
+tar --strip-components=1 -xf $ARCHIVE
+rm $ARCHIVE
+
+./configure --prefix=$PREFIX
+make -j install
