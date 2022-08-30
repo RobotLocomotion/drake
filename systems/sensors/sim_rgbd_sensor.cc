946eb3474d0712141b367c589e5238bcec821cd2
Sean Curtis <sean.curtis@tri.global>
2022-08-30 07:36:34 -0700

    Initial copy of anzu assets

---
+++ systems/sensors/sim_rgbd_sensor.cc
@@ -0,0 +1,121 @@
+#include "sim/common/sim_rgbd_sensor.h"
+
+#include <memory>
+#include <optional>
+#include <regex>
+
+#include "drake/systems/lcm/lcm_publisher_system.h"
+#include "drake/systems/sensors/image_to_lcm_image_array_t.h"
+#include "sim/common/sim_camera_description_publisher.h"
+
+namespace anzu {
+namespace sim {
+
+using drake::geometry::render::ColorRenderCamera;
+using drake::geometry::render::DepthRenderCamera;
+using drake::math::RigidTransformd;
+using drake::systems::sensors::RgbdSensor;
+
+RgbdSensor* AddSimRgbdSensor(
+    const drake::geometry::SceneGraph<double>& scene_graph,
+    const drake::multibody::MultibodyPlant<double>& plant,
+    const SimRgbdSensor& sim_rgbd_sensor,
+    drake::systems::DiagramBuilder<double>* builder) {
+  DRAKE_DEMAND(builder != nullptr);
+  // TODO(eric.cousineau): Simplify this if drake#10247 is resolved.
+
+  // 'A' is the body for the parent sensor frame.
+  // `P` is the parent of the sensor frame.
+  // `B` is the sensor frame.
+
+  const auto& frame_P = sim_rgbd_sensor.frame();
+  const auto& body_A = frame_P.body();
+  const std::optional<drake::geometry::FrameId> body_A_id =
+      plant.GetBodyFrameIdIfExists(body_A.index());
+  DRAKE_THROW_UNLESS(body_A_id.has_value());
+
+  // We must include the offest of the body to ensure that we posture the camera
+  // appropriately.
+  const RigidTransformd X_AP = frame_P.GetFixedPoseInBodyFrame();
+  const RigidTransformd X_AB = X_AP * sim_rgbd_sensor.X_PB();
+
+  auto* rgbd_sensor_sys = builder->AddSystem<RgbdSensor>(
+      *body_A_id, X_AB, sim_rgbd_sensor.color_properties(),
+      sim_rgbd_sensor.depth_properties());
+  // `set_name` is only used for debugging.
+  rgbd_sensor_sys->set_name("rgbd_sensor_" + sim_rgbd_sensor.serial());
+  builder->Connect(scene_graph.get_query_output_port(),
+                   rgbd_sensor_sys->query_object_input_port());
+  return rgbd_sensor_sys;
+}
+
+void DrakeAddSimRgbdSensorLcmPublisher(
+    const SimRgbdSensor& sim_rgbd_sensor,
+    const drake::systems::OutputPort<double>* rgb_port,
+    const drake::systems::OutputPort<double>* depth_16u_port,
+    bool do_compress,
+    drake::systems::DiagramBuilder<double>* builder,
+    drake::lcm::DrakeLcmInterface* lcm) {
+  DRAKE_DEMAND(builder != nullptr);
+  DRAKE_DEMAND(lcm != nullptr);
+  if (!rgb_port && !depth_16u_port) return;
+
+  auto image_to_lcm_image_array =
+      builder->AddSystem<drake::systems::sensors::ImageToLcmImageArrayT>(
+          do_compress);
+  image_to_lcm_image_array->set_name(
+      "image_to_lcm_" + sim_rgbd_sensor.serial());
+  if (depth_16u_port) {
+    const auto& lcm_depth_port =
+        image_to_lcm_image_array->DeclareImageInputPort<
+            drake::systems::sensors::PixelType::kDepth16U>("depth");
+    builder->Connect(*depth_16u_port, lcm_depth_port);
+  }
+  if (rgb_port) {
+    const auto& lcm_rgb_port = image_to_lcm_image_array->DeclareImageInputPort<
+        drake::systems::sensors::PixelType::kRgba8U>("rgb");
+    builder->Connect(*rgb_port, lcm_rgb_port);
+  }
+  auto image_array_lcm_publisher =
+      builder->AddSystem(drake::systems::lcm::LcmPublisherSystem::Make<
+                         drake::lcmt_image_array>(
+          "DRAKE_RGBD_CAMERA_IMAGES_" + sim_rgbd_sensor.serial(), lcm,
+          1. / sim_rgbd_sensor.rate_hz()));
+  builder->Connect(image_to_lcm_image_array->image_array_t_msg_output_port(),
+                   image_array_lcm_publisher->get_input_port());
+}
+
+void AddSimRgbdSensorDescriptionLcmPublisher(
+    const std::string serial,
+    const ColorRenderCamera& color_properties,
+    const DepthRenderCamera& depth_properties,
+    drake::systems::DiagramBuilder<double>* builder,
+    drake::lcm::DrakeLcmInterface* lcm) {
+  auto* publisher =
+      builder->AddSystem(std::make_unique<SimCameraDescriptionPublisher>(
+          serial, "DRAKE_RGBD_CAMERAS", "DRAKE_RGBD_CAMERA_IMAGES_" + serial,
+          color_properties, depth_properties, lcm));
+  publisher->set_name("camera_description_" + serial);
+}
+
+void AddSimRgbdSensorLcmPublishers(
+    const SimRgbdSensor& sim_rgbd_sensor,
+    const drake::systems::OutputPort<double>* rgb_port,
+    const drake::systems::OutputPort<double>* depth_16u_port,
+    bool do_compress,
+    drake::systems::DiagramBuilder<double>* builder,
+    drake::lcm::DrakeLcmInterface* lcm) {
+  const size_t old_system_count = builder->GetSystems().size();
+  DrakeAddSimRgbdSensorLcmPublisher(sim_rgbd_sensor, rgb_port, depth_16u_port,
+                                    do_compress, builder, lcm);
+
+  if (builder->GetSystems().size() != old_system_count) {
+    // Only add the description publisher if we've added to the builder.
+    AddSimRgbdSensorDescriptionLcmPublisher(
+        sim_rgbd_sensor.serial(), sim_rgbd_sensor.color_properties(),
+        sim_rgbd_sensor.depth_properties(), builder, lcm);
+  }
+}
+
+}  // namespace sim
+}  // namespace anzu
