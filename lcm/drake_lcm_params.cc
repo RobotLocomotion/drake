RobotLocomotion/drake

commit a3a2ae53129794e0e7ccfe64c663b8d76a50e6ce

[lcm] Add option for a channel name suffix (#17337)
* [lcm] Add params struct and option for a channel suffix

Deprecate the two-argument constructor.

lcm/BUILD.bazel
@@ -21,6 +21,7 @@ drake_cc_package_library(
     visibility = ["//visibility:public"],
     deps = [
         ":drake_lcm",
+        ":drake_lcm_params",
         ":interface",
         ":lcm_log",
         ":lcm_messages",
@@ -48,10 +49,21 @@ drake_cc_library(
 )
 
 drake_cc_library(
+    name = "drake_lcm_params",
+    srcs = ["drake_lcm_params.cc"],
+    hdrs = ["drake_lcm_params.h"],
+    deps = [
+        "//common:essential",
+        "//common:name_value",
+    ],
+)
+
+drake_cc_library(
     name = "drake_lcm",
     srcs = ["drake_lcm.cc"],
     hdrs = ["drake_lcm.h"],
     deps = [
+        ":drake_lcm_params",
         ":interface",
         "//common:essential",
         "//common:scope_exit",

lcm/drake_lcm_params.cc
@@ -1,0 +1,9 @@
+#include "drake/lcm/drake_lcm_params.h"
+
+namespace drake {
+namespace lcm {
+
+DrakeLcmParams::~DrakeLcmParams() = default;
+
+}  // namespace lcm
+}  // namespace drake

lcm/drake_lcm_params.h
@@ -1,0 +1,47 @@
+#pragma once
+
+#include <string>
+
+#include "drake/common/drake_copyable.h"
+#include "drake/common/name_value.h"
+
+namespace drake {
+namespace lcm {
+
+/** The set of parameters for configuring DrakeLcm.  */
+struct DrakeLcmParams {
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(DrakeLcmParams)
+  DrakeLcmParams() = default;
+  ~DrakeLcmParams();
+
+  /** The URL for DrakeLcm communication. If empty, DrakeLcm will use the
+  default URL per the DrakeLcm::DrakeLcm() no-argument constructor. */
+  std::string lcm_url;
+
+  /** The custom LCM channel name suffix for this DrakeLcm instance (optional).
+
+  When provided, calls to DrakeLcm::Publish() or DrakeLcm::Subscribe() will
+  append this string to the `channel` name requested for publish or subscribe.
+
+  This has no effect on DrakeLcm::SubscribeAllChannels(); the channel names
+  reported there will remain unmodified.
+  */
+  std::string channel_suffix;
+
+  /** (Advanced) Controls whether or not LCM's background receive thread will
+  be launched immediately during the constructor (when false) or deferred until
+  the first time it's needed (when true). This can be useful if the scheduling
+  configuration for new threads varies between the construction time and first
+  use. */
+  bool defer_initialization{false};
+
+  template <typename Archive>
+  void Serialize(Archive* a) {
+    a->Visit(DRAKE_NVP(lcm_url));
+    a->Visit(DRAKE_NVP(channel_suffix));
+    a->Visit(DRAKE_NVP(defer_initialization));
+  }
+};
+
+}  // namespace lcm
+}  // namespace drake

bindings/pydrake/BUILD.bazel
@@ -156,6 +156,7 @@ drake_pybind_library(
     name = "lcm_py",
     cc_deps = [
         "//bindings/pydrake:documentation_pybind",
+        "//bindings/pydrake/common:deprecation_pybind",
     ],
     cc_srcs = ["lcm_py.cc"],
     package_info = PACKAGE_INFO,
@@ -580,6 +581,7 @@ drake_py_unittest(
     name = "lcm_test",
     deps = [
         ":lcm_py",
+        "//bindings/pydrake/common/test_utilities:deprecation_py",
         "//lcmtypes:lcmtypes_drake_py",
     ],
 )

systems/lcm/BUILD.bazel
@@ -73,9 +73,12 @@ drake_cc_library(
     name = "lcm_interface_system",
     srcs = ["lcm_interface_system.cc"],
     hdrs = ["lcm_interface_system.h"],
+    interface_deps = [
+        "//lcm:drake_lcm_params",
+        "//systems/framework:leaf_system",
+    ],
     deps = [
         "//lcm:drake_lcm",
-        "//systems/framework:leaf_system",
     ],
 )
 

bindings/pydrake/lcm_py.cc
@@ -4,6 +4,7 @@
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
 
+#include "drake/bindings/pydrake/common/deprecation_pybind.h"
 #include "drake/bindings/pydrake/documentation_pybind.h"
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/lcm/drake_lcm.h"
@@ -47,14 +48,36 @@ PYBIND11_MODULE(lcm, m) {
   }
 
   {
+    using Class = DrakeLcmParams;
+    constexpr auto& cls_doc = doc.DrakeLcmParams;
+    py::class_<Class>(m, "DrakeLcmParams", cls_doc.doc)
+        .def(ParamInit<Class>())
+        .def_readwrite("lcm_url", &Class::lcm_url, cls_doc.lcm_url.doc)
+        .def_readwrite("channel_suffix", &Class::channel_suffix,
+            cls_doc.channel_suffix.doc)
+        .def_readwrite("defer_initialization", &Class::defer_initialization,
+            cls_doc.defer_initialization.doc)
+        .def("__repr__", [](const Class& self) {
+          return py::str(
+              "DrakeLcmParams("
+              "lcm_url={}, "
+              "channel_suffix={}, "
+              "defer_initialization={})")
+              .format(
+                  self.lcm_url, self.channel_suffix, self.defer_initialization);
+        });
+  }
+
+  {
     using Class = DrakeLcm;
     constexpr auto& cls_doc = doc.DrakeLcm;
-    py::class_<Class, DrakeLcmInterface>(m, "DrakeLcm", cls_doc.doc)
+    py::class_<Class, DrakeLcmInterface> cls(m, "DrakeLcm", cls_doc.doc);
+    cls  // BR
         .def(py::init<>(), cls_doc.ctor.doc_0args)
-        .def(
-            py::init<std::string>(), py::arg("lcm_url"), cls_doc.ctor.doc_1args)
-        .def(py::init<std::string, bool>(), py::arg("lcm_url"),
-            py::arg("defer_initialization"), cls_doc.ctor.doc_2args)
+        .def(py::init<std::string>(), py::arg("lcm_url"),
+            cls_doc.ctor.doc_1args_lcm_url)
+        .def(py::init<DrakeLcmParams>(), py::arg("params"),
+            cls_doc.ctor.doc_1args_params)
         .def(
             "Subscribe",
             [](Class* self, const std::string& channel,
@@ -69,6 +92,14 @@ PYBIND11_MODULE(lcm, m) {
             },
             py::arg("channel"), py::arg("handler"), cls_doc.Subscribe.doc);
     // TODO(eric.cousineau): Add remaining methods.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    const char* const doc_deprecated =
+        cls_doc.ctor
+            .doc_deprecated_deprecated_2args_lcm_url_defer_initialization;
+    cls.def(py_init_deprecated<Class, std::string, bool>(doc_deprecated),
+        py::arg("lcm_url"), py::arg("defer_initialization"), doc_deprecated);
+#pragma GCC diagnostic pop
   }
 
   ExecuteExtraPythonCode(m);

bindings/pydrake/test/lcm_test.py
@@ -1,6 +1,7 @@
 import unittest
 
-from pydrake.lcm import DrakeLcm, DrakeLcmInterface, Subscriber
+from pydrake.common.test_utilities.deprecation import catch_drake_warnings
+from pydrake.lcm import DrakeLcm, DrakeLcmInterface, DrakeLcmParams, Subscriber
 
 from drake import lcmt_quaternion
 
@@ -19,10 +20,21 @@ class TestLcm(unittest.TestCase):
         self.assertIsInstance(dut, DrakeLcmInterface)
         dut.get_lcm_url()
         DrakeLcm(lcm_url="")
-        DrakeLcm(lcm_url="", defer_initialization=True)
         # Test virtual function names.
         dut.Publish
         dut.HandleSubscriptions
+
+    def test_params(self):
+        dut = DrakeLcmParams()
+        dut.lcm_url = "memq://123"
+        dut.channel_suffix = "_foo"
+        dut.defer_initialization = True
+        instance = DrakeLcm(params=dut)
+        self.assertTrue(instance.get_lcm_url(), "memq://123")
+
+    def test_deprecated(self):
+        with catch_drake_warnings(expected_count=1):
+            DrakeLcm(lcm_url="", defer_initialization=True)
 
     def _handler(self, raw):
         quat = lcmt_quaternion.decode(raw)

examples/planar_gripper/planar_gripper_common.cc
@@ -8,9 +8,7 @@
 
 #include "drake/common/drake_assert.h"
 #include "drake/common/find_resource.h"
-#include "drake/lcm/drake_lcm.h"
 #include "drake/multibody/plant/multibody_plant.h"
-#include "drake/systems/lcm/lcm_interface_system.h"
 
 namespace drake {
 namespace examples {

commit 9c6b01c9956993f0730e36aa4ae46c6d8c04442e

Make DrakeLcmParams an aggregate to fix relevant compilation errors
This type is constructed as an aggregate in drake_lcm.cc

```C++
DrakeLcm::DrakeLcm(std::string lcm_url)
    : DrakeLcm(DrakeLcmParams{.lcm_url = std::move(lcm_url)}) {}
```

```C++
DrakeLcm::DrakeLcm(std::string lcm_url, bool defer_initialization)
    : DrakeLcm(DrakeLcmParams{
          .lcm_url = std::move(lcm_url),
          .channel_suffix = {},
          .defer_initialization = defer_initialization}) {}
```

However, because there are constructors, the type is not
actually an aggregate, causing:

```
drake/lcm/drake_lcm.cc:75:16: error: no matching constructor for initialization of 'drake::lcm::DrakeLcmParams'
    : DrakeLcm(DrakeLcmParams{.lcm_url = std::move(lcm_url)}) {}
               ^             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
drake/lcm/drake_lcm.cc:89:16: error: no matching constructor for initialization of 'drake::lcm::DrakeLcmParams'
    : DrakeLcm(DrakeLcmParams{
               ^             ~
```

MinRepro example:
```C++
struct MyInt
{
    MyInt() = default;
    MyInt(const MyInt&) = default;
    MyInt(MyInt&&) = default;
    MyInt& operator=(const MyInt&) = default;
    MyInt& operator=(MyInt&&) = default;

    int a;
};

int main()
{
    MyInt a{.a = 5}
    (void)a;
    return 0;
}

```

```
error: designated initializers cannot be used with a non-aggregate type ‘MyInt’
   27 |     MyInt a{.a = 5};
      |                   ^
```


Caused by #17337

lcm/BUILD.bazel
@@ -50,7 +50,6 @@ drake_cc_library(
 
 drake_cc_library(
     name = "drake_lcm_params",
-    srcs = ["drake_lcm_params.cc"],
     hdrs = ["drake_lcm_params.h"],
     deps = [
         "//common:essential",

lcm/drake_lcm_params.cc
@@ -1,9 +1,0 @@
-#include "drake/lcm/drake_lcm_params.h"
-
-namespace drake {
-namespace lcm {
-
-DrakeLcmParams::~DrakeLcmParams() = default;
-
-}  // namespace lcm
-}  // namespace drake

lcm/drake_lcm_params.h
@@ -10,10 +10,6 @@ namespace lcm {
 
 /** The set of parameters for configuring DrakeLcm.  */
 struct DrakeLcmParams {
-  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(DrakeLcmParams)
-  DrakeLcmParams() = default;
-  ~DrakeLcmParams();
-
   /** Passes this object to an Archive.
   Refer to @ref yaml_serialization "YAML Serialization" for background. */
   template <typename Archive>