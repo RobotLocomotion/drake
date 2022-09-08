27e20f339c8d68c11ae6cb1ab350c277c9cfaf1e
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-09-07 17:16:19 -0700

    [geometry] Make VolumeMesh from VTK specified in geometry::Mesh

---
+++ geometry/proximity/make_mesh_from_vtk.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "drake/geometry/proximity/volume_mesh.h"
+#include "drake/geometry/shape_specification.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+
+/* Creates a VolumeMesh of a possibly non-convex object from a VTK file
+ containing its tetrahedral mesh. It complements MakeConvexVolumeMesh().
+
+ @param[in] mesh  The mesh specification containing VTK file name.
+ @retval  volume_mesh
+ @tparam_nonsymbolic_scalar
+
+ @throw std::exception if a tetrahedral element has non-positive volume.
+        std::exception if the `mesh` specification refers to non-VTK file.
+        std::exception if the VTK file does not contain a tetrahedral mesh.
+        For example, it will throw if `mesh` refers to Obj file instead of
+        VTK file, or the VTK file has a triangle mesh or a quadrilateral mesh
+        instead of a tetrahedral mesh.
+ */
+template <typename T>
+VolumeMesh<T> MakeVolumeMeshFromVtk(const Mesh& mesh);
+
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
