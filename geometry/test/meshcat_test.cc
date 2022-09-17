a8a130a613dbec2e2323b10dacca1ad157e7466d
Russ Tedrake <russt@mit.edu>
2022-09-17 13:49:58 -0400

    [meshcat] Allow keyboard events to move sliders and click buttons.
    This will enable, for instance, keyboard teleop through Meshcat. It
    will make the Deepnote teleop demos much more ergonomic.
    
    Upgrades meshcat to the latest commit to get this feature.
    
    To see the new features, `bazel run
    //geometry:meshcat_manual_test`. The last prompt (starting with "I've
    added two buttons..." will now ask you to press keys in addition to
    using the gui button / slider for the interaction.

--- geometry/test/meshcat_test.cc
+++ geometry/test/meshcat_test.cc
@@ -613,6 +613,39 @@ GTEST_TEST(MeshcatTest, Buttons) {
   DRAKE_EXPECT_THROWS_MESSAGE(
       meshcat.GetButtonClicks("bob"),
       "Meshcat does not have any button named bob.");
+
+  // Adding a button with the keycode.
+  meshcat.AddButton("alice", "KeyT");
+  CheckWebsocketCommand(meshcat, R"""({
+      "type": "button",
+      "name": "alice"
+    })""", {}, {});
+  EXPECT_EQ(meshcat.GetButtonClicks("alice"), 1);
+  // Adding with the same keycode still resets.
+  meshcat.AddButton("alice", "KeyT");
+  EXPECT_EQ(meshcat.GetButtonClicks("alice"), 0);
+  // Adding the same button with an empty keycode throws.
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      meshcat.AddButton("alice"),
+      ".*does not match the current keycode.*");
+  // Adding the same button with a different keycode throws.
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      meshcat.AddButton("alice", "KeyR"),
+      ".*does not match the current keycode.*");
+  meshcat.DeleteButton("alice");
+
+  // Adding a button with the keycode empty, then populated works.
+  meshcat.AddButton("alice");
+  meshcat.AddButton("alice", "KeyT");
+
+  // Adding a button named the same as a slider throws.
+  meshcat.AddSlider("slider", 0.2, 1.5, 0.1, 0.5);
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      meshcat.AddButton("slider"),
+      "Meshcat already has a slider named slider.");
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      meshcat.AddButton("slider", "KeyR"),
+      "Meshcat already has a slider named slider.");
 }
 
 GTEST_TEST(MeshcatTest, Sliders) {
