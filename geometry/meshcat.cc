3ba7a52f7027543dda15f08592b9a8f112935bfb
Russ Tedrake <russt@mit.edu>
2022-09-17 13:49:58 -0400

    [meshcat] Allow keyboard events to move sliders and click buttons.
    This will enable, for instance, keyboard teleop through Meshcat. It
    will make the Deepnote teleop demos much more ergonomic.
    
    Upgrades meshcat to the latest commit to get this feature.
    
    To see the new features, `bazel run
    //geometry:meshcat_manual_test`. The last prompt (starting with "I've
    added two buttons..." will now ask you to press keys in addition to
    using the gui button / slider for the interaction.

--- geometry/meshcat.cc
+++ geometry/meshcat.cc
@@ -1201,7 +1201,7 @@ class Meshcat::Impl {
   }
 
   // This function is public via the PIMPL.
-  void AddButton(std::string name) {
+  void AddButton(std::string name, std::string keycode) {
     DRAKE_DEMAND(IsThread(main_thread_id_));
 
     internal::SetButtonControl data;
@@ -1211,19 +1211,24 @@ class Meshcat::Impl {
   'type': 'button',
   'name': '{}'
 }})))""", data.name);
+    data.keycode1 = std::move(keycode);
 
     {
       std::lock_guard<std::mutex> lock(controls_mutex_);
-      auto iter = buttons_.find(data.name);
-      if (iter != buttons_.end()) {
-        iter->second.num_clicks = 0;
-        return;
-      }
       if (sliders_.find(data.name) != sliders_.end()) {
         throw std::logic_error(
             fmt::format("Meshcat already has a slider named {}.", data.name));
       }
-      controls_.emplace_back(data.name);
+      auto iter = buttons_.find(data.name);
+      if (iter != buttons_.end()) {
+        iter->second.num_clicks = 0;
+        if (iter->second.keycode1 == data.keycode1) {
+          // No need to publish to meshcat.
+          return;
+        }
+      } else {
+        controls_.emplace_back(data.name);
+      }
       buttons_[data.name] = data;
       DRAKE_DEMAND(controls_.size() == (buttons_.size() + sliders_.size()));
     }
@@ -1278,8 +1283,9 @@ class Meshcat::Impl {
   }
 
   // This function is public via the PIMPL.
-  void AddSlider(std::string name, double min, double max,
-                               double step, double value) {
+  void AddSlider(std::string name, double min, double max, double step,
+                 double value, std::string decrement_keycode,
+                 std::string increment_keycode) {
     DRAKE_DEMAND(IsThread(main_thread_id_));
 
     internal::SetSliderControl data;
@@ -1297,8 +1303,10 @@ class Meshcat::Impl {
     // https://github.com/dataarts/dat.gui/blob/f720c729deca5d5c79da8464f8a05500d38b140c/src/dat/controllers/NumberController.js#L62
     value = std::max(value, min);
     value = std::min(value, max);
-    value = std::round(value/step)*step;
+    value = std::round(value / step) * step;
     data.value = value;
+    data.keycode1 = std::move(decrement_keycode);
+    data.keycode2 = std::move(increment_keycode);
 
     {
       std::lock_guard<std::mutex> lock(controls_mutex_);
@@ -2121,8 +2129,8 @@ void Meshcat::ResetRenderMode() {
   impl().ResetRenderMode();
 }
 
-void Meshcat::AddButton(std::string name) {
-  impl().AddButton(std::move(name));
+void Meshcat::AddButton(std::string name, std::string keycode) {
+  impl().AddButton(std::move(name), std::move(keycode));
 }
 
 int Meshcat::GetButtonClicks(std::string_view name) {
@@ -2133,9 +2141,11 @@ void Meshcat::DeleteButton(std::string name) {
   impl().DeleteButton(std::move(name));
 }
 
-void Meshcat::AddSlider(std::string name, double min, double max,
-                               double step, double value) {
-  impl().AddSlider(std::move(name), min, max, step, value);
+void Meshcat::AddSlider(std::string name, double min, double max, double step,
+                        double value, std::string decrement_keycode,
+                        std::string increment_keycode) {
+  impl().AddSlider(std::move(name), min, max, step, value,
+                   std::move(decrement_keycode), std::move(increment_keycode));
 }
 
 void Meshcat::SetSliderValue(std::string name, double value) {
