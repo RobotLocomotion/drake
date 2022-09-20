6003b18f88c3d0883430334638d41b9e95cec7c5
Xuchen Han <xuchen.han@tri.global>
2022-09-20 10:31:14 -0700

    [multibody] Support deformable free motion calculation

--- multibody/plant/deformable_driver.h
+++ multibody/plant/deformable_driver.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <memory>
 #include <vector>
 
 #include "drake/common/default_scalars.h"
 #include "drake/common/drake_copyable.h"
 #include "drake/common/eigen_types.h"
+#include "drake/multibody/fem/discrete_time_integrator.h"
 #include "drake/multibody/plant/deformable_model.h"
 #include "drake/multibody/plant/discrete_update_manager.h"
 #include "drake/systems/framework/context.h"
@@ -57,6 +59,9 @@ class DeformableDriver : public ScalarConvertibleComponent<T> {
   struct CacheIndexes {
     /* Per body cache entries indexed by DeformableBodyIndex. */
     std::vector<systems::CacheIndex> fem_states;
+    std::vector<systems::CacheIndex> free_motion_fem_states;
+    std::vector<systems::CacheIndex> next_fem_states;
+    std::vector<systems::CacheIndex> fem_solver_scratches;
   };
 
   /* Copies the state of the deformable body with `id` in the given `context`
@@ -72,10 +77,40 @@ class DeformableDriver : public ScalarConvertibleComponent<T> {
   const fem::FemState<T>& EvalFemState(const systems::Context<T>& context,
                                        DeformableBodyIndex index) const;
 
+  /* Given the state of the deformable body with `index` in the given `context`,
+   computes its "free motion" state (the state the body would have at the next
+   time step in the absense of contact or constraints).
+   @pre fem_state_star != nullptr and is compatible with the state of the
+   deformable body with the given `index`. */
+  void CalcFreeMotionFemState(const systems::Context<T>& context,
+                              DeformableBodyIndex index,
+                              fem::FemState<T>* fem_state_star) const;
+
+  /* Eval version of CalcFreeMotionFemState(). */
+  const fem::FemState<T>& EvalFreeMotionFemState(
+      const systems::Context<T>& context, DeformableBodyIndex index) const;
+
+  /* Given the state of the deformable body with `index` in the given `context`,
+   computes the state of the deformable body at the next time step.
+   @note The state of the deformable body will the same as the "free motion"
+         state in the absense of contact or constraints.
+   @pre next_fem_state != nullptr and is compatible with the state of
+        the deformable body with the given `index`. */
+  void CalcNextFemState(const systems::Context<T>& context,
+                        DeformableBodyIndex index,
+                        fem::FemState<T>* next_fem_state) const;
+
+  /* Eval version of CalcNextFemState(). */
+  const fem::FemState<T>& EvalNextFemState(const systems::Context<T>& context,
+                                           DeformableBodyIndex index) const;
+
   CacheIndexes cache_indexes_;
   /* Modeling information about all deformable bodies. */
   const DeformableModel<T>* const deformable_model_;
   const DiscreteUpdateManager<T>* const  manager_;
+  /* The integrator used to advance deformable body free motion states in time.
+   */
+  std::unique_ptr<fem::internal::DiscreteTimeIntegrator<T>> integrator_;
 };
 
 }  // namespace internal
