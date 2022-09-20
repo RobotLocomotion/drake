c12db4f434058639f2028b7dce9dbd870179d3e1
Xuchen Han <xuchen.han@tri.global>
2022-09-20 10:31:14 -0700

    [multibody] Support deformable free motion calculation

--- multibody/plant/test/deformable_driver_test.cc
+++ multibody/plant/test/deformable_driver_test.cc
@@ -2,6 +2,7 @@
 
 #include <gtest/gtest.h>
 
+#include "drake/common/test_utilities/eigen_matrix_compare.h"
 #include "drake/common/test_utilities/expect_throws_message.h"
 #include "drake/multibody/plant/compliant_contact_manager.h"
 #include "drake/systems/framework/diagram_builder.h"
@@ -33,8 +34,9 @@ class CompliantContactManagerTest {
 
 class DeformableDriverTest : public ::testing::Test {
  protected:
+  static constexpr double kDt = 0.01;
+
   void SetUp() override {
-    constexpr double kDt = 0.01;
     std::tie(plant_, scene_graph_) =
         AddMultibodyPlantSceneGraph(&builder_, kDt);
     auto deformable_model = make_unique<DeformableModel<double>>(plant_);
@@ -50,12 +52,27 @@ class DeformableDriverTest : public ::testing::Test {
     context_ = plant_->CreateDefaultContext();
   }
 
-  /* Calls private member function in DeformableDriver with the same name. */
-  const fem::FemState<double>& EvalFemState(
+  /* Forwarding calls to private member functions in DeformableDriver with the
+   same name.
+   @{ */
+  const FemState<double>& EvalFemState(
       const systems::Context<double>& context,
       DeformableBodyIndex index) const {
     return driver_->EvalFemState(context, index);
   }
+
+  const FemState<double>& EvalFreeMotionFemState(
+      const systems::Context<double>& context,
+      DeformableBodyIndex index) const {
+    return driver_->EvalFreeMotionFemState(context, index);
+  }
+
+  const FemState<double>& EvalNextFemState(
+      const systems::Context<double>& context,
+      DeformableBodyIndex index) const {
+    return driver_->EvalNextFemState(context, index);
+  }
+  /* @} */
 
   systems::DiagramBuilder<double> builder_;
   MultibodyPlant<double>* plant_{nullptr};
@@ -71,9 +88,10 @@ class DeformableDriverTest : public ::testing::Test {
                                   double resolution_hint) {
     auto geometry = make_unique<GeometryInstance>(
         RigidTransformd(), make_unique<Sphere>(1), "sphere");
-    const fem::DeformableBodyConfig<double> default_body_config;
+    fem::DeformableBodyConfig<double> body_config;
+    body_config.set_youngs_modulus(1e6);
     DeformableBodyId body_id = model->RegisterDeformableBody(
-        move(geometry), default_body_config, resolution_hint);
+        move(geometry), body_config, resolution_hint);
     return body_id;
   }
 };
@@ -104,6 +122,47 @@ TEST_F(DeformableDriverTest, FemState) {
   EXPECT_EQ(fem_state.GetPositions(), q);
   EXPECT_EQ(fem_state.GetVelocities(), v);
   EXPECT_EQ(fem_state.GetAccelerations(), a);
+}
+
+TEST_F(DeformableDriverTest, FreeMotionFemState) {
+  const VectorX<double> q = model_->GetReferencePositions(body_id_);
+  const int num_dofs = q.size();
+  const VectorX<double> v = VectorX<double>::Zero(num_dofs);
+  const VectorX<double> a = VectorX<double>::Zero(num_dofs);
+  const FemState<double>& free_motion_fem_state =
+      EvalFreeMotionFemState(*context_, DeformableBodyIndex(0));
+  const Vector3<double> kGravity(0, 0, -9.81);
+  VectorX<double> next_a = a;
+  for (int dof = 0; dof < num_dofs; dof += 3) {
+    next_a.segment<3>(dof) += kGravity;
+  }
+  // We use the clear-box knowledge that we use midpoint rule for time
+  // integration.
+  const VectorX<double> next_v = v + next_a * kDt;
+  const VectorX<double> next_q = q + 0.5 * (v + next_v) * kDt;
+  // Tolerance has unit of velocity.
+  const double kTol = 1e-4;
+  EXPECT_TRUE(CompareMatrices(free_motion_fem_state.GetPositions(), next_q,
+                              kTol * kDt));
+  EXPECT_TRUE(
+      CompareMatrices(free_motion_fem_state.GetVelocities(), next_v, kTol));
+  EXPECT_TRUE(CompareMatrices(free_motion_fem_state.GetAccelerations(), next_a,
+                              kTol / kDt));
+}
+
+TEST_F(DeformableDriverTest, NextFemState) {
+  const FemState<double>& free_motion_fem_state =
+      EvalFreeMotionFemState(*context_, DeformableBodyIndex(0));
+  const FemState<double>& next_fem_state =
+      EvalNextFemState(*context_, DeformableBodyIndex(0));
+  /* The next state should be the same as the free motion state in the absence
+   * of contact or constraints. */
+  EXPECT_EQ(free_motion_fem_state.GetPositions(),
+            next_fem_state.GetPositions());
+  EXPECT_EQ(free_motion_fem_state.GetVelocities(),
+            next_fem_state.GetVelocities());
+  EXPECT_EQ(free_motion_fem_state.GetAccelerations(),
+            next_fem_state.GetAccelerations());
 }
 
 }  // namespace
