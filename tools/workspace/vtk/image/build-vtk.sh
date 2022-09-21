b07450a6b70897c7d25f0981e1faee2a1a19a665
Stephen McDowell <stephen.mcdowell@kitware.com>
2022-09-21 17:06:01 -0400

    [workspace] Use VTK inline namespace

--- tools/workspace/vtk/image/build-vtk.sh
+++ tools/workspace/vtk/image/build-vtk.sh
@@ -2,14 +2,14 @@
 
 set -eu -o pipefail
 
-readonly VTK_VERSION=9.1.0
+# Can be a branch, tag, or commit (e.g., 'v9.1.0').
+readonly VTK_VERSION="d706250a1422ae1e7ece0fa09a510186769a5fec"
 
 mkdir -p /vtk
 cd /vtk
 
-git clone \
-    --branch v${VTK_VERSION} --depth 1 \
-    https://gitlab.kitware.com/vtk/vtk.git src
+git clone https://gitlab.kitware.com/vtk/vtk.git src
+git -C src checkout "${VTK_VERSION}"
 
 mkdir -p /vtk/build
 cd /vtk/build
