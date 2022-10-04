dc42933341d68376f55618ff764a6458e80c2d4c
Russ Tedrake <russt@mit.edu>
2022-10-04 17:24:08 -0400

    [pydrake/multibody] Add bindings for MassDamperSpringAnalyticalSolution

--- bindings/pydrake/multibody/benchmarks_py.cc
+++ bindings/pydrake/multibody/benchmarks_py.cc
@@ -4,6 +4,7 @@
 #include "drake/bindings/pydrake/documentation_pybind.h"
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/multibody/benchmarks/acrobot/make_acrobot_plant.h"
+#include "drake/multibody/benchmarks/mass_damper_spring/mass_damper_spring_analytical_solution.h"
 
 namespace drake {
 namespace pydrake {
@@ -42,6 +43,22 @@ void init_all(py::module m) {
 PYBIND11_MODULE(benchmarks, m) {
   init_acrobot(m.def_submodule("acrobot"));
   init_all(m.def_submodule("all"));
+
+  constexpr auto& doc = pydrake_doc.drake.multibody.benchmarks;
+
+  {
+    using Class = multibody::benchmarks::MassDamperSpringAnalyticalSolution<T>;
+    constexpr auto& cls_doc = doc.MassDamperSpringAnalyticalSolution;
+    py::class_<Class>(m, "MassDamperSpringAnalyticalSolution", cls_doc.doc)
+        .def(py::init<const T&, const T&, const T&>(), py::arg("mass"),
+            py::arg("b"), py::arg("k"), cls_doc.ctor.doc)
+        .def("SetInitialValue", &Class::SetInitialValue, py::arg("x0"),
+            py::arg("xDt0"), cls_doc.SetInitialValue.doc)
+        .def("get_x", &Class::get_x, py::arg("t"), cls_doc.get_x.doc)
+        .def("get_xDt", &Class::get_xDt, py::arg("t"), cls_doc.get_xDt.doc)
+        .def("get_xDtDt", &Class::get_xDtDt, py::arg("t"),
+            cls_doc.get_xDtDt.doc);
+  }
 }
 
 }  // namespace pydrake
