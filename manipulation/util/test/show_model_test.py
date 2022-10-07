8f1ad0bc2dc2546515da40e002b47dc51f249122
Todd Rowell <todd.rowell@tri.global>
2022-10-07 13:13:26 -0400

    Remove manipulation/util/geometry_inspector
    All the functionality of this tool is now in
    manipulation/util/show_model.

--- manipulation/util/test/show_model_test.py
+++ manipulation/util/test/show_model_test.py
@@ -5,9 +5,17 @@ from bazel_tools.tools.python.runfiles import runfiles
 
 
 class TestShowModel(unittest.TestCase):
+    def setUp(self):
+        self.manifest = runfiles.Create()
+        self.dut = self.manifest.Rlocation(
+            "drake/manipulation/util/show_model")
+
+    def model_file(self, model_runpath):
+        """Get a model file from a runpath."""
+        return self.manifest.Rlocation(model_runpath)
+
     def test_show(self):
         """Test that show_model doesn't crash."""
-        manifest = runfiles.Create()
         model_runpaths = [
             # Simple SDFormat file.
             "drake/multibody/benchmarks/acrobot/acrobot.sdf",
@@ -18,8 +26,27 @@ class TestShowModel(unittest.TestCase):
             # SDFormat world file with multiple models.
             "drake/manipulation/util/test/simple_world_with_two_models.sdf",
         ]
-        bin = manifest.Rlocation("drake/manipulation/util/show_model")
         for model_runpath in model_runpaths:
             print(model_runpath)
-            model_file = manifest.Rlocation(model_runpath)
-            subprocess.check_call([bin, model_file, "--loop_once"])
+            subprocess.check_call([self.dut, self.model_file(model_runpath),
+                                   "--loop_once"])
+
+    def test_pyplot(self):
+        """Test that pyplot doesn't crash."""
+        model_runpath = ("drake/manipulation/models/iiwa_description/sdf/"
+                         "iiwa14_no_collision.sdf")
+        subprocess.check_call([self.dut, self.model_file(model_runpath),
+                               "--loop_once", "--pyplot"])
+
+    def test_set_position(self):
+        """Test that the --position option doesn't crash."""
+        model_runpaths = [
+            # Simple SDFormat file.
+            "drake/multibody/benchmarks/acrobot/acrobot.sdf",
+            # Simple URDF file.
+            "drake/multibody/benchmarks/acrobot/acrobot.urdf",
+        ]
+        for model_runpath in model_runpaths:
+            print(model_runpath)
+            subprocess.check_call([self.dut, self.model_file(model_runpath),
+                                   "--loop_once", "--position", "0.1", "0.2"])
