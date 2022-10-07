71dd431b1b0181d11d11f49efa4b4d863c1d336f
Todd Rowell <todd.rowell@tri.global>
2022-10-07 13:13:26 -0400

    Remove manipulation/util/geometry_inspector
    All the functionality of this tool is now in
    manipulation/util/show_model.

--- manipulation/models/jaco_description/README.md
+++ manipulation/models/jaco_description/README.md
@@ -12,8 +12,8 @@ hand only.
 
 The versions with the `_sphere_collision` suffix use collision
 geometry defined entirely by sphere primitives.  These models were
-created by hand editing the urdf and verifiying the results using
-`//manipulation/util:geometry_inspector`.
+created by hand editing the urdf and verifiying the results using a
+visualization tool such as `//manipulation/util:show_model`.
 
 In addition, some tags unsupported by Drake have been removed, to reduce the
 burden of warning output. For URDF support details, see:
--- manipulation/util/test/show_model_test.py
+++ manipulation/util/test/show_model_test.py
@@ -23,3 +23,28 @@ class TestShowModel(unittest.TestCase):
             print(model_runpath)
             model_file = manifest.Rlocation(model_runpath)
             subprocess.check_call([bin, model_file, "--loop_once"])
+
+    def test_pyplot(self):
+        """Test that pyplot doesn't crash."""
+        manifest = runfiles.Create()
+        bin = manifest.Rlocation("drake/manipulation/util/show_model")
+        model_runpath = ("drake/manipulation/models/iiwa_description/sdf/"
+                         "iiwa14_no_collision.sdf")
+        model_file = manifest.Rlocation(model_runpath)
+        subprocess.check_call([bin, model_file, "--loop_once", "--pyplot"])
+
+    def test_set_position(self):
+        """Test that the --position option doesn't crash."""
+        manifest = runfiles.Create()
+        model_runpaths = [
+            # Simple SDFormat file.
+            "drake/multibody/benchmarks/acrobot/acrobot.sdf",
+            # Simple URDF file.
+            "drake/multibody/benchmarks/acrobot/acrobot.urdf",
+        ]
+        bin = manifest.Rlocation("drake/manipulation/util/show_model")
+        for model_runpath in model_runpaths:
+            print(model_runpath)
+            model_file = manifest.Rlocation(model_runpath)
+            subprocess.check_call([bin, model_file, "--loop_once",
+                                   "--position", "0.1", "0.2"])
