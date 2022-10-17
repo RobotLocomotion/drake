35903480823edb4b3c5be1c12b433a82a5618517
Matthew Woehlke <matthew.woehlke@kitware.com>
2022-10-17 16:25:22 -0400

    Refactor wheel tests into individual tests
    Refactor how we run wheel tests so that each test is a separate entity,
    rather than just having all tests crammed into test-wheel.sh. In
    particular, refactor Linux testing to create a temporary Docker image
    that has the wheel installed, so that we will (on Linux) be able to
    create tests that need to manipulate the test environment without such
    manipulation affecting other tests.

---
+++ tools/wheel/test/install-wheel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This shell script installs a Drake wheel. It must be run inside a container
+# (Ubuntu) or environment (macOS) which has been properly provisioned, e.g. by
+# the accompanying Dockerfile (Ubuntu) or by macos/test-wheel.sh (macOS). In
+# particular, /opt/drake-wheel-test/python must contain a Python virtual
+# environment which will be used to run the tests. The path to the wheel to be
+# installed must be given as an argument to the script. On Ubuntu, the wheel
+# must be accessible to the container, and the path must be the container's
+# path to the wheel (rather than the host's path).
+#
+# In general, it is not recommended to attempt to run this script directly;
+# use //tools/wheel:builder instead.
+
+set -eu -o pipefail
+
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 <wheel>" >&2
+    exit 1
+fi
+
+. /opt/drake-wheel-test/python/bin/activate
+
+pip install "$1"
