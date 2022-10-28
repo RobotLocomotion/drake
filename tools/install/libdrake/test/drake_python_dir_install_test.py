f28f85d21268485bf864a7007f9aad3b28d7376f
Scott K Logan <logans@cottsay.net>
2022-10-28 20:33:52 +0000

    PR feedback

--- tools/install/libdrake/test/drake_python_dir_install_test.py
+++ tools/install/libdrake/test/drake_python_dir_install_test.py
@@ -9,7 +9,8 @@ import install_test_helper
 
 class DrakePythonDirInstallTest(unittest.TestCase):
     def test_drake_python_dir(self):
-        cmake_source_dir = install_test_helper.create_temporary_dir("pydir_src")
+        cmake_source_dir = install_test_helper.create_temporary_dir(
+            "pydir_src")
 
         cmake_prefix_path = install_test_helper.get_install_dir()
 
@@ -19,20 +20,30 @@ class DrakePythonDirInstallTest(unittest.TestCase):
             set(CMAKE_PREFIX_PATH {cmake_prefix_path})
             find_package(drake CONFIG REQUIRED)
 
-            # Sanity check
+            # PYTHON_VERSION check
+            if(NOT "{py_major}.{py_minor}" EQUAL ${{drake_PYTHON_VERSION}})
+              message(FATAL_ERROR "Python version does not match")
+            else()
+              message(
+                STATUS "Found expected Drake Python "
+                "version: ${{drake_PYTHON_VERSION}}")
+            endif()
+
+            # PYTHON_DIR Sanity check
             execute_process(
               COMMAND ${{CMAKE_COMMAND}} -E env PYTHONPATH=""
-                {python_exe} -c "import pydrake"
+                {python_exe} -c "import pydrake.all"
               ERROR_QUIET
               RESULT_VARIABLE _IMPORT_RESULT)
             if(0 EQUAL _IMPORT_RESULT)
               message(FATAL_ERROR "Import of pydrake should have failed")
             endif()
 
-            # Actual test
+            # PYTHON_DIR Actual test
             execute_process(
-              COMMAND ${{CMAKE_COMMAND}} -E env PYTHONPATH=${{drake_PYTHON_DIR}}
-                {python_exe} -c "import pydrake"
+              COMMAND ${{CMAKE_COMMAND}} -E env
+                PYTHONPATH=${{drake_PYTHON_DIR}}
+                {python_exe} -c "import pydrake.all"
               RESULT_VARIABLE _IMPORT_RESULT)
             if(NOT 0 EQUAL _IMPORT_RESULT)
               message(FATAL_ERROR "Import of pydrake failed")
@@ -40,14 +51,16 @@ class DrakePythonDirInstallTest(unittest.TestCase):
               message(STATUS "Import of pydrake works as expected")
             endif()
         """.format(
-            cmake_prefix_path=cmake_prefix_path, python_exe=sys.executable)
+            cmake_prefix_path=cmake_prefix_path, python_exe=sys.executable,
+            py_major=sys.version_info.major, py_minor=sys.version_info.minor)
 
         cmake_filename = os.path.join(cmake_source_dir, "CMakeLists.txt")
 
         with open(cmake_filename, "w") as f:
             f.write(textwrap.dedent(cmake_content))
 
-        cmake_binary_dir = install_test_helper.create_temporary_dir("pydir_build")
+        cmake_binary_dir = install_test_helper.create_temporary_dir(
+            "pydir_build")
 
         subprocess.check_call(["cmake", cmake_source_dir],
                               cwd=cmake_binary_dir)
