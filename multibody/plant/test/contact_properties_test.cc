e40398a17564730bcc0c3e407818f3a7a5204b6c
Alejandro Castro <alejandro.castro@tri.global>
2022-10-28 10:40:11 -0400

    Implements additional helper methods to retrieve contact properties. (#18188)

--- multibody/plant/test/contact_properties_test.cc
+++ multibody/plant/test/contact_properties_test.cc
@@ -32,8 +32,15 @@ constexpr double kKC = 2e4;
 constexpr double kKDefault = 3e4;
 constexpr double kTauA = 0.1;
 constexpr double kTauB = 0.2;
-constexpr double kTauD = -0.2;
+constexpr double kNegativeTauD = -0.2;
 constexpr double kTauDefault = 0.3;
+constexpr double kHydroModulusA = 1e6;
+constexpr double kHydroModulusB = 1e8;
+constexpr double kHydroModulusC = 1e10;
+constexpr double kHydroModulusDefault = 3e6;
+constexpr double kHuntCrossleyDissipationA = 0.1;
+constexpr double kHuntCrossleyDissipationDefault = 0.1;
+constexpr double kInfinity = std::numeric_limits<double>::infinity();
 
 class ContactPropertiesTest : public ::testing::Test {
  public:
@@ -41,14 +48,16 @@ class ContactPropertiesTest : public ::testing::Test {
     const SourceId s_id = scene_graph_.RegisterSource("source");
     const FrameId f_id =
         scene_graph_.RegisterFrame(s_id, GeometryFrame("frame"));
-    unique_ptr<GeometryInstance> geometry_A =
-        MakeGeometryInstance("A", kMuA, kKA, kTauA);
-    unique_ptr<GeometryInstance> geometry_B =
-        MakeGeometryInstance("B", kMuB, std::nullopt, kTauB);
-    unique_ptr<GeometryInstance> geometry_C =
-        MakeGeometryInstance("C", kMuC, kKC, std::nullopt);
-    unique_ptr<GeometryInstance> geometry_D =
-        MakeGeometryInstance("D", kMuD, std::nullopt, kTauD);
+    unique_ptr<GeometryInstance> geometry_A = MakeGeometryInstance(
+        "A", kMuA, kKA, kTauA, kHuntCrossleyDissipationA, kHydroModulusA);
+    unique_ptr<GeometryInstance> geometry_B = MakeGeometryInstance(
+        "B", kMuB, std::nullopt, kTauB, std::nullopt, kHydroModulusB,
+        geometry::internal::HydroelasticType::kRigid);
+    unique_ptr<GeometryInstance> geometry_C = MakeGeometryInstance(
+        "C", kMuC, kKC, std::nullopt, std::nullopt, kHydroModulusC,
+        geometry::internal::HydroelasticType::kSoft);
+    unique_ptr<GeometryInstance> geometry_D = MakeGeometryInstance(
+        "D", kMuD, std::nullopt, kNegativeTauD, std::nullopt);
 
     g_A_ = scene_graph_.RegisterGeometry(s_id, f_id, move(geometry_A));
     g_B_ = scene_graph_.RegisterGeometry(s_id, f_id, move(geometry_B));
@@ -56,10 +65,15 @@ class ContactPropertiesTest : public ::testing::Test {
     g_D_ = scene_graph_.RegisterGeometry(s_id, f_id, move(geometry_D));
   }
 
-  GeometryId g_A_;  // Has mu, k, tau.
-  GeometryId g_B_;  // Has mu, tau.
-  GeometryId g_C_;  // Has mu, k.
-  GeometryId g_D_;  // Has mu, invalid tau.
+  // mu  = Coulomb friction
+  // k   = stiffness
+  // tau = relaxation time
+  // Eh  = hydroelastic modulus
+  // d   = dissipation
+  GeometryId g_A_;  // Has mu, k, tau, dA, Eh_A.
+  GeometryId g_B_;  // Has mu,    tau,     Eh_B and "rigid hydroelastic".
+  GeometryId g_C_;  // Has mu, k,          Eh_C and "compliant hydroelastic".
+  GeometryId g_D_;  // Has mu,    invalid tau.
 
   const SceneGraphInspector<double>& inspector() const {
     return scene_graph_.model_inspector();
@@ -67,15 +81,33 @@ class ContactPropertiesTest : public ::testing::Test {
 
   unique_ptr<GeometryInstance> MakeGeometryInstance(
       const std::string& name, const CoulombFriction<double>& mu,
-      const std::optional<double>& k, const std::optional<double>& tau) {
+      const std::optional<double>& k, const std::optional<double>& tau,
+      const std::optional<double>& hunt_crossley_dissipation,
+      const std::optional<double>& hydro_modulus = std::nullopt,
+      const std::optional<geometry::internal::HydroelasticType>&
+          compliance_type = std::nullopt) {
     auto sphere = make_unique<Sphere>(1.0);
     auto result =
         make_unique<GeometryInstance>(RigidTransformd(), move(sphere), name);
     geometry::ProximityProperties props;
-    geometry::AddContactMaterial({}, k, mu, &props);
+    geometry::AddContactMaterial(hunt_crossley_dissipation, k, mu, &props);
     if (tau.has_value()) {
       props.AddProperty(geometry::internal::kMaterialGroup,
                         geometry::internal::kRelaxationTime, *tau);
+    }
+
+    // Hydroelastic contact requires the resolution hint, even if not used for
+    // these tests.
+    constexpr double kRezHint = 1.0;
+    props.AddProperty(geometry::internal::kHydroGroup,
+                      geometry::internal::kRezHint, kRezHint);
+    if (hydro_modulus.has_value()) {
+      props.AddProperty(geometry::internal::kHydroGroup,
+                        geometry::internal::kElastic, *hydro_modulus);
+    }
+    if (compliance_type.has_value()) {
+      props.AddProperty(geometry::internal::kHydroGroup,
+                        geometry::internal::kComplianceType, *compliance_type);
     }
     result->set_proximity_properties(move(props));
     return result;
@@ -89,6 +121,48 @@ TEST_F(ContactPropertiesTest, GetPointContactStiffness) {
   EXPECT_EQ(GetPointContactStiffness(g_A_, kKDefault, inspector()), kKA);
   EXPECT_EQ(GetPointContactStiffness(g_B_, kKDefault, inspector()), kKDefault);
   EXPECT_EQ(GetPointContactStiffness(g_C_, kKDefault, inspector()), kKC);
+}
+
+TEST_F(ContactPropertiesTest, GetHydroelasticModulus) {
+  EXPECT_EQ(GetHydroelasticModulus(g_A_, kHydroModulusDefault, inspector()),
+            kHydroModulusA);
+  EXPECT_EQ(GetHydroelasticModulus(g_B_, kHydroModulusDefault, inspector()),
+            kInfinity);
+  EXPECT_EQ(GetHydroelasticModulus(g_C_, kHydroModulusDefault, inspector()),
+            kHydroModulusC);
+  EXPECT_EQ(GetHydroelasticModulus(g_D_, kHydroModulusDefault, inspector()),
+            kHydroModulusDefault);
+}
+
+TEST_F(ContactPropertiesTest, GetHuntCrossleyDissipation) {
+  EXPECT_EQ(GetHuntCrossleyDissipation(g_A_, kHuntCrossleyDissipationDefault,
+                                       inspector()),
+            kHuntCrossleyDissipationA);
+  EXPECT_EQ(GetHuntCrossleyDissipation(g_B_, kHuntCrossleyDissipationDefault,
+                                       inspector()),
+            kHuntCrossleyDissipationDefault);
+  EXPECT_EQ(GetHuntCrossleyDissipation(g_C_, kHuntCrossleyDissipationDefault,
+                                       inspector()),
+            kHuntCrossleyDissipationDefault);
+}
+
+TEST_F(ContactPropertiesTest, GetCombinedHuntCrossleyDissipation) {
+  EXPECT_EQ(GetCombinedHuntCrossleyDissipation(
+                g_A_, g_B_, kHydroModulusA, kInfinity,
+                kHuntCrossleyDissipationDefault, inspector()),
+            kHuntCrossleyDissipationA);
+  EXPECT_EQ(GetCombinedHuntCrossleyDissipation(
+                g_B_, g_A_, kInfinity, kHydroModulusA,
+                kHuntCrossleyDissipationDefault, inspector()),
+            kHuntCrossleyDissipationA);
+  const double dAC = (kHydroModulusC / (kHydroModulusA + kHydroModulusC)) *
+                         kHuntCrossleyDissipationA +
+                     (kHydroModulusA / (kHydroModulusA + kHydroModulusC)) *
+                         kHuntCrossleyDissipationDefault;
+  EXPECT_EQ(GetCombinedHuntCrossleyDissipation(
+                g_A_, g_C_, kHydroModulusA, kHydroModulusDefault,
+                kHuntCrossleyDissipationDefault, inspector()),
+            dAC);
 }
 
 TEST_F(ContactPropertiesTest, GetDissipationTimeConstant) {
