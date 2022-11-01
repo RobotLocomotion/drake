87098257794cda86d2c248eefd5d29987d517895
Stephen McDowell <stephen.mcdowell@kitware.com>
2022-11-01 14:31:37 -0400

    [wheel] Enable mac arm64 wheel builds
    - Update libjpeg-turbo version, use CMake to build not autotools.
    - Update libpng version to include macOS arm64 specific fixes.

--- tools/wheel/image/dependencies/projects/libjpeg-turbo.cmake
+++ tools/wheel/image/dependencies/projects/libjpeg-turbo.cmake
@@ -1,23 +1,13 @@
-if(APPLE)
-    set(_libjpeg-turbo_ARGS_APPLE --host x86_64-apple-darwin )
-endif()
-
 ExternalProject_Add(libjpeg-turbo
     URL ${libjpeg-turbo_url}
     URL_MD5 ${libjpeg-turbo_md5}
     ${COMMON_EP_ARGS}
-    BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND ./configure
-        --prefix=${CMAKE_INSTALL_PREFIX}
-        --disable-shared
-        ${_libjpeg-turbo_ARGS_APPLE}
-        NASM=yasm
-        CFLAGS=-fPIC
-        CXXFLAGS=-fPIC
-    BUILD_COMMAND make
-    INSTALL_COMMAND make install
-    )
+    ${COMMON_CMAKE_EP_ARGS}
+    CMAKE_ARGS
+        ${COMMON_CMAKE_ARGS}
+        -DCMAKE_ASM_NASM_COMPILER=yasm
+)
 
 extract_license(libjpeg-turbo
-    README
+    LICENSE.md
 )
