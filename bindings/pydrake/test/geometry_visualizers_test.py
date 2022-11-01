792b28d23ceb7ea42a2fa5a4a1d59f24a858afae
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-11-01 07:04:15 -0700

    [pydrake] Remove deprecated MeshcatVisualizerCpp 2022-11
    Likewise remove MeshcatPointCloudVisualizerCpp.

--- bindings/pydrake/test/geometry_visualizers_test.py
+++ bindings/pydrake/test/geometry_visualizers_test.py
@@ -9,7 +9,6 @@ from drake import lcmt_viewer_load_robot, lcmt_viewer_draw
 from pydrake.autodiffutils import AutoDiffXd
 from pydrake.common.value import AbstractValue
 from pydrake.common.test_utilities import numpy_compare
-from pydrake.common.test_utilities.deprecation import catch_drake_warnings
 from pydrake.lcm import DrakeLcm, Subscriber
 from pydrake.math import RigidTransform
 from pydrake.perception import PointCloud
@@ -268,26 +267,6 @@ class TestGeometryVisualizers(unittest.TestCase):
         self.assertIsInstance(vis_autodiff,
                               mut.MeshcatVisualizer_[AutoDiffXd])
 
-    def test_deprecated_meshcat_visualizer_cpp_add(self):
-        """This checks a deprecated API spelling; remove this on 2022-11-01."""
-        T = float
-        builder = DiagramBuilder_[T]()
-        scene_graph = builder.AddSystem(mut.SceneGraph_[T]())
-        meshcat = mut.Meshcat()
-        with catch_drake_warnings(expected_count=1):
-            added = mut.MeshcatVisualizerCpp.AddToBuilder(
-                builder=builder, scene_graph=scene_graph, meshcat=meshcat)
-        self.assertIsInstance(added, mut.MeshcatVisualizer)
-
-    def test_deprecated_meshcat_visualizer_cpp_scalar_conversion(self):
-        """This checks a deprecated API spelling; remove this on 2022-11-01."""
-        meshcat = mut.Meshcat()
-        with catch_drake_warnings(expected_count=1):
-            vis = mut.MeshcatVisualizerCpp(meshcat)
-        vis_autodiff = vis.ToAutoDiffXd()
-        self.assertIsInstance(vis_autodiff,
-                              mut.MeshcatVisualizerCpp_[AutoDiffXd])
-
     @numpy_compare.check_nonsymbolic_types
     def test_meshcat_point_cloud_visualizer(self, T):
         meshcat = mut.Meshcat()
@@ -307,17 +286,6 @@ class TestGeometryVisualizers(unittest.TestCase):
             ad_visualizer = visualizer.ToAutoDiffXd()
             self.assertIsInstance(
                 ad_visualizer, mut.MeshcatPointCloudVisualizer_[AutoDiffXd])
-
-    def test_deprecated_meshcat_point_cloud_visualizer_cpp(self):
-        """This checks a deprecated API spelling; remove this on 2022-11-01."""
-        meshcat = mut.Meshcat()
-        with catch_drake_warnings(expected_count=1):
-            visualizer = mut.MeshcatPointCloudVisualizerCpp(
-                meshcat=meshcat, path="cloud", publish_period=1/12.0)
-        visualizer.set_point_size(0.1)
-        ad_visualizer = visualizer.ToAutoDiffXd()
-        self.assertIsInstance(
-            ad_visualizer, mut.MeshcatPointCloudVisualizerCpp_[AutoDiffXd])
 
     def test_start_meshcat(self):
         # StartMeshcat only performs interesting work on cloud notebook hosts.
