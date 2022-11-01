7220276c062401ffbcd487d3e64f1811bfa23cc2
hongkai-dai <daihongkai@gmail.com>
2022-10-31 21:57:35 -0700

    Compute monomial basis with the degree for each variable <= 1.

--- bindings/pydrake/test/symbolic_test.py
+++ bindings/pydrake/test/symbolic_test.py
@@ -1152,6 +1152,11 @@ class TestSymbolicMonomial(unittest.TestCase):
         basis = sym.OddDegreeMonomialBasis(vars, 3)
         self.assertEqual(basis.size, 6)
 
+    def test_calc_monomial_basis_order_up_to_one(self):
+        basis = sym.CalcMonomialBasisOrderUpToOne(
+            x=sym.Variables([x, y, z]), sort_monomial=False)
+        self.assertEqual(basis.size, 8)
+
     def test_evaluate(self):
         m = sym.Monomial(x, 3) * sym.Monomial(y)  # m = x³y
         env = {x: 2.0,
--- common/symbolic/monomial_util.cc
+++ common/symbolic/monomial_util.cc
@@ -1,5 +1,8 @@
 #include "drake/common/symbolic/monomial_util.h"
 
+#include <algorithm>
+#include <vector>
+
 #include "drake/common/drake_assert.h"
 
 namespace drake {
@@ -20,6 +23,62 @@ Eigen::Matrix<Monomial, Eigen::Dynamic, 1> OddDegreeMonomialBasis(
     const Variables& vars, int degree) {
   return internal::ComputeMonomialBasis<Eigen::Dynamic>(
       vars, degree, internal::DegreeType::kOdd);
+}
+
+namespace {
+// Generates all the monomials with degree up to 1 for each variable, for all
+// the variables in x[start], x[start+1], ..., x.back(). Append these monomials
+// to `monomial_basis`.
+// @param[in/out] monomial_basis We assume that as the input, monomial_basis
+// doesn't contain x[start] in its variables. As an output, the new monomials
+// that contain x[start] will be appended to monomial_basis.
+void CalcMonomialBasisOrderUpToOneHelper(
+    const std::vector<Variable>& x, int start,
+    std::vector<Monomial>* monomial_basis) {
+  DRAKE_DEMAND(start >= 0 && start < static_cast<int>(x.size()));
+  if (start == static_cast<int>(x.size() - 1)) {
+    // Generate the monomial for a single variable, which is 1 and the variable
+    // itself.
+    monomial_basis->emplace_back();
+    monomial_basis->emplace_back(x[start], 1);
+    return;
+  } else {
+    // First generate the monomials using all the variables x[start + 1] ...
+    // x[x.size() - 1]
+    CalcMonomialBasisOrderUpToOneHelper(x, start + 1, monomial_basis);
+    const int monomial_count = monomial_basis->size();
+    // Construct the monomial x[start].
+    const Monomial x_start(x[start], 1);
+    // Multiply x_start to each monomial already in monomial_set, and append the
+    // multiplication result to monomial_set.
+    for (int i = 0; i < monomial_count; ++i) {
+      monomial_basis->push_back(x_start * (*monomial_basis)[i]);
+    }
+    return;
+  }
+}
+}  // namespace
+
+VectorX<Monomial> CalcMonomialBasisOrderUpToOne(const Variables& x,
+                                                bool sort_monomial) {
+  std::vector<Variable> x_vec;
+  x_vec.reserve(x.size());
+  for (const auto& var : x) {
+    x_vec.push_back(var);
+  }
+  std::vector<Monomial> monomial_basis;
+  // There will be pow(2, x.size()) monomials in the basis.
+  monomial_basis.reserve(1 << static_cast<int>(x.size()));
+  CalcMonomialBasisOrderUpToOneHelper(x_vec, 0, &monomial_basis);
+  VectorX<Monomial> ret(monomial_basis.size());
+  if (sort_monomial) {
+    std::sort(monomial_basis.begin(), monomial_basis.end(),
+              GradedReverseLexOrder<std::less<Variable>>());
+  }
+  for (int i = 0; i < ret.rows(); ++i) {
+    ret(i) = monomial_basis[i];
+  }
+  return ret;
 }
 }  // namespace symbolic
 }  // namespace drake
