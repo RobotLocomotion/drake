3e88ec3635af0d7aed6435acb047b6cb6becc85c
Rick Poyner <rick.poyner@tri.global>
2022-12-05 12:48:10 -0500

    [planning] Add RobotCollisionType files copied from Anzu
    Co-authored-by: calderpg-tri <32444241+calderpg-tri@users.noreply.github.com>
    Co-authored-by: Jeremy Nimmer <jeremy.nimmer@tri.global>
    Co-authored-by: Sean Curtis <sean.curtis@tri.global>

---
+++ planning/test/robot_collision_type_test.cc
@@ -0,0 +1,74 @@
+#include "planning/robot_collision_type.h"
+
+#include <fmt/format.h>
+#include <gtest/gtest.h>
+
+namespace anzu {
+namespace planning {
+namespace {
+
+constexpr RobotCollisionType kNoCollision = RobotCollisionType::kNoCollision;
+constexpr RobotCollisionType kEnvironmentCollision =
+    RobotCollisionType::kEnvironmentCollision;
+constexpr RobotCollisionType kSelfCollision =
+    RobotCollisionType::kSelfCollision;
+constexpr RobotCollisionType kEnvironmentAndSelfCollision =
+    RobotCollisionType::kEnvironmentAndSelfCollision;
+
+struct Case {
+  RobotCollisionType input;
+  bool in_collision;
+  RobotCollisionType output;
+
+  std::string to_string() const {
+    return fmt::format("input {}, in_collision {}, output {}",
+                       static_cast<int>(input), in_collision,
+                       static_cast<int>(output));
+  }
+};
+
+GTEST_TEST(CollisionCheckerTest, EnvCollisionBits) {
+  Case cases[] = {
+      {kNoCollision, false, kNoCollision},
+      {kNoCollision, true, kEnvironmentCollision},
+
+      {kEnvironmentCollision, false, kNoCollision},
+      {kEnvironmentCollision, true, kEnvironmentCollision},
+
+      {kSelfCollision, false, kSelfCollision},
+      {kSelfCollision, true, kEnvironmentAndSelfCollision},
+
+      {kEnvironmentAndSelfCollision, false, kSelfCollision},
+      {kEnvironmentAndSelfCollision, true, kEnvironmentAndSelfCollision},
+  };
+  for (const auto& a_case : cases) {
+    SCOPED_TRACE(a_case.to_string());
+    EXPECT_EQ(SetInEnvironmentCollision(a_case.input, a_case.in_collision),
+              a_case.output);
+  }
+}
+
+GTEST_TEST(CollisionCheckerTest, SelfCollisionBits) {
+  Case cases[] = {
+      {kNoCollision, false, kNoCollision},
+      {kNoCollision, true, kSelfCollision},
+
+      {kEnvironmentCollision, false, kEnvironmentCollision},
+      {kEnvironmentCollision, true, kEnvironmentAndSelfCollision},
+
+      {kSelfCollision, false, kNoCollision},
+      {kSelfCollision, true, kSelfCollision},
+
+      {kEnvironmentAndSelfCollision, false, kEnvironmentCollision},
+      {kEnvironmentAndSelfCollision, true, kEnvironmentAndSelfCollision},
+  };
+  for (const auto& a_case : cases) {
+    SCOPED_TRACE(a_case.to_string());
+    EXPECT_EQ(SetInSelfCollision(a_case.input, a_case.in_collision),
+              a_case.output);
+  }
+}
+
+}  // namespace
+}  // namespace planning
+}  // namespace anzu
