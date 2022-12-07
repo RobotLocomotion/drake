1c0ac7cf58c3421f6e1c174e743a1bb3079d9923
Sean Curtis <sean.curtis@tri.global>
2022-12-07 13:47:43 -0800

    Add RobotClearance files copied from Anzu
      Co-authored-by: calderpg-tri <32444241+calderpg-tri@users.noreply.github.com>
      Co-authored-by: Jeremy Nimmer <jeremy.nimmer@tri.global>
      Co-authored-by: Sean Curtis <sean.curtis@tri.global>

---
+++ planning/robot_clearance.cc
@@ -0,0 +1,35 @@
+#include "planning/robot_clearance.h"
+
+#include "drake/common/drake_throw.h"
+
+namespace anzu {
+namespace planning {
+
+using drake::multibody::BodyIndex;
+
+RobotClearance::~RobotClearance() = default;
+
+void RobotClearance::Reserve(int size) {
+  robot_indices_.reserve(size);
+  other_indices_.reserve(size);
+  collision_types_.reserve(size);
+  distances_.reserve(size);
+  jacobians_.reserve(size * nq_);
+}
+
+void RobotClearance::Append(
+    BodyIndex robot_index, BodyIndex other_index,
+    RobotCollisionType collision_type, double distance,
+    const Eigen::Ref<const Eigen::RowVectorXd>& jacobian) {
+  DRAKE_THROW_UNLESS(jacobian.cols() == nq_);
+  robot_indices_.push_back(robot_index);
+  other_indices_.push_back(other_index);
+  collision_types_.push_back(collision_type);
+  distances_.push_back(distance);
+  for (int j = 0; j < nq_; ++j) {
+    jacobians_.push_back(jacobian[j]);
+  }
+}
+
+}  // namespace planning
+}  // namespace anzu
