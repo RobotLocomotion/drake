05ea834e477d54161e011016ae750f49cd6cadbe
Rick Poyner <rick.poyner@tri.global>
2022-12-08 14:49:54 -0500

    [planning] Add BodyShapeDescription
    This commit adds the copied code to the BUILD and fixes its namespaces
    to match up with Drake instead of Anzu.
    
    Also improves the implementation as noted during Drake code reviews.

--- planning/body_shape_description.cc
+++ planning/body_shape_description.cc
@@ -1,15 +1,15 @@
 #include "planning/body_shape_description.h"
 
-#include <Eigen/Geometry>
-
 #include "drake/common/drake_throw.h"
+#include "drake/geometry/query_object.h"
+#include "drake/geometry/scene_graph.h"
 
-namespace anzu {
+namespace drake {
 namespace planning {
 
 BodyShapeDescription::BodyShapeDescription(
-    const drake::geometry::Shape& shape,
-    const drake::math::RigidTransformd& X_BS,
+    const geometry::Shape& shape,
+    const math::RigidTransformd& X_BS,
     std::string model_instance_name,
     std::string body_name)
     : shape_(shape.Clone()),
@@ -18,21 +18,29 @@ BodyShapeDescription::BodyShapeDescription(
       body_name_(std::move(body_name)) {}
 
 BodyShapeDescription MakeBodyShapeDescription(
-    const drake::multibody::MultibodyPlant<double>& plant,
-    const drake::systems::Context<double>& plant_context,
-    const drake::geometry::GeometryId& geometry_id) {
+    const multibody::MultibodyPlant<double>& plant,
+    const systems::Context<double>& plant_context,
+    const geometry::GeometryId& geometry_id) {
+  DRAKE_DEMAND(plant.geometry_source_is_registered());
+  plant.ValidateContext(plant_context);
+  DRAKE_DEMAND(geometry_id.is_valid());
+
   // Grab query object to get the inspector.
   const auto& query_object =
       plant.get_geometry_query_input_port()
-          .template Eval<drake::geometry::QueryObject<double>>(plant_context);
+          .template Eval<geometry::QueryObject<double>>(plant_context);
   const auto& inspector = query_object.inspector();
+
+  // Make sure the geometry_id is associated with the passed plant.
+  DRAKE_DEMAND(inspector.BelongsToSource(geometry_id, *plant.get_source_id()));
+
   const auto frame_id = inspector.GetFrameId(geometry_id);
-  // inspector gives us the shape's pose w.r.t. the parent frame F. We rely on
-  // MbP registering the geometry frame F to the body B as X_BF = I.
-  const drake::math::RigidTransformd& X_BS =
+  // inspector gives us the shape's pose w.r.t. the parent geometry frame F. We
+  // rely on MbP registering the geometry frame F to the body B as X_BF = I.
+  const math::RigidTransformd& X_BS =
       inspector.GetPoseInFrame(geometry_id);
   const auto body = plant.GetBodyFromFrameId(frame_id);
-  DRAKE_THROW_UNLESS(body != nullptr);
+  DRAKE_DEMAND(body != nullptr);
   const std::string& model_instance_name =
       plant.GetModelInstanceName(body->model_instance());
   return BodyShapeDescription(inspector.GetShape(geometry_id), X_BS,
@@ -40,4 +48,4 @@ BodyShapeDescription MakeBodyShapeDescription(
 }
 
 }  // namespace planning
-}  // namespace anzu
+}  // namespace drake
--- planning/test/body_shape_description_test.cc
+++ planning/test/body_shape_description_test.cc
@@ -2,24 +2,19 @@
 
 #include <gtest/gtest.h>
 
-#include "drake/common/test_utilities/expect_throws_message.h"
 #include "drake/multibody/parsing/parser.h"
 #include "drake/multibody/plant/multibody_plant.h"
 #include "drake/planning/robot_diagram_builder.h"
-#include "drake/systems/framework/diagram_builder.h"
 
-namespace anzu {
+namespace drake {
 namespace planning {
 namespace {
 
-using drake::geometry::GeometryId;
-using drake::geometry::SceneGraph;
-using drake::geometry::Sphere;
-using drake::math::RigidTransformd;
-using drake::math::RollPitchYaw;
-using drake::multibody::AddMultibodyPlantSceneGraph;
-using drake::multibody::MultibodyPlant;
-using drake::multibody::Parser;
+using geometry::GeometryId;
+using geometry::Sphere;
+using math::RigidTransformd;
+using math::RollPitchYaw;
+using multibody::MultibodyPlant;
 
 GTEST_TEST(BodyShapeDescriptionTest, Arbitrary) {
   const Sphere sphere{1.23};
@@ -36,7 +31,7 @@ GTEST_TEST(BodyShapeDescriptionTest, Arbitrary) {
 }
 
 GTEST_TEST(BodyShapeDescriptionTest, FromPlant) {
-  drake::planning::RobotDiagramBuilder<double> builder;
+  planning::RobotDiagramBuilder<double> builder;
   const std::string model = R"""(
 <?xml version='1.0'?>
 <sdf version='1.7'>
@@ -66,32 +61,8 @@ GTEST_TEST(BodyShapeDescriptionTest, FromPlant) {
   EXPECT_TRUE(dut.pose_in_body().IsExactlyIdentity());
   EXPECT_EQ(dut.model_instance_name(), "my_model");
   EXPECT_EQ(dut.body_name(), "link");
-
-  // Wrong context throws.
-  EXPECT_THROW(MakeBodyShapeDescription(plant, *diagram_context, geometries[0]),
-               std::exception);
-  // Invalid GeometryId throws.
-  EXPECT_THROW(MakeBodyShapeDescription(plant, plant_context, {}),
-               std::exception);
-  // GeometryId for nonexistent geometry throws.
-  EXPECT_THROW(
-      MakeBodyShapeDescription(plant, plant_context, GeometryId::get_new_id()),
-      std::exception);
-}
-
-GTEST_TEST(BodyShapeDescriptionTest, NoSceneGraphThrows) {
-  drake::systems::DiagramBuilder<double> builder;
-  auto plant = builder.AddSystem<MultibodyPlant<double>>(0.0);
-  plant->Finalize();
-  auto diagram = builder.Build();
-  auto diagram_context = diagram->CreateDefaultContext();
-  auto& plant_context = plant->GetMyContextFromRoot(*diagram_context);
-
-  DRAKE_EXPECT_THROWS_MESSAGE(
-      MakeBodyShapeDescription(*plant, plant_context, {}),
-      ".*Port.*geometry_query.*not connected.*");
 }
 
 }  // namespace
 }  // namespace planning
-}  // namespace anzu
+}  // namespace drake
