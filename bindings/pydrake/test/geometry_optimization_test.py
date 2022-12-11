d768e8a8c8309286ffaffb9b376c533cbc936349
hongkai-dai <daihongkai@gmail.com>
2022-12-10 17:03:56 -0800

    Add HPolyhedron::FindRedundant

--- bindings/pydrake/test/geometry_optimization_test.py
+++ bindings/pydrake/test/geometry_optimization_test.py
@@ -137,6 +137,10 @@ class TestGeometryOptimization(unittest.TestCase):
             other=h_unit_box,
             check_for_redundancy=False)
         # Check that the ReduceInequalities binding works.
+        redundant_indices = h_half_box_intersect_unit_box.FindRedundant(
+            tol=1E-9)
+        # Check FindRedundant with default tol.
+        h_half_box_intersect_unit_box.FindRedundant()
         h_half_box3 = h_half_box_intersect_unit_box.ReduceInequalities(
             tol=1E-9)
 
