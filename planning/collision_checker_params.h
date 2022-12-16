973ee73de6bfff41c13de505ea101e03d6602eb2
Rick Poyner <rick.poyner@tri.global>
2022-12-16 16:03:04 -0500

    [planning] Add CollisionCheckerParams
    This commit adds the copied code to the BUILD and fixes its namespaces
    and includes to match up with Drake instead of Anzu.

--- planning/collision_checker_params.h
+++ planning/collision_checker_params.h
@@ -7,31 +7,33 @@
 #include <Eigen/Core>
 
 #include "drake/multibody/tree/multibody_tree_indexes.h"
-#include "planning/robot_diagram.h"
+#include "drake/planning/robot_diagram.h"
 
-namespace anzu {
+namespace drake {
 namespace planning {
 
 /** Configuration distance takes two configurations of the robot, q1 and q2,
 both as Eigen::VectorXd, and returns (potentially weighted) C-space distance
-as a double.
+as a double. The returned distance will be strictly non-negative.
 
 To be valid, the function must satisfy the following condition:
 
- - F(q, q) ≡ 0
+ - dist(q, q) ≡ 0
 */
 using ConfigurationDistanceFunction =
     std::function<double(const Eigen::VectorXd&, const Eigen::VectorXd&)>;
 
+// TODO(SeanCurtis-TRI): Move the default interpolator into the params.
 /** Configuration interpolation function takes two configurations of the robot,
-q1, and q2, both as Eigen::VectorXd, plus a ratio in [0, 1] and returns the
-interpolated configuration.
+q1, and q2, both as Eigen::VectorXd, plus a ratio, r, in [0, 1] and returns the
+interpolated configuration. Behavior of the function for values of r outside of
+the range [0,1] is undefined.
 
 To be valid, the function must satisfy the following conditions:
 
- - F(q1, q2, 0) ≡ q1
- - F(q1, q2, 1) ≡ q2
- - F(q, q, for all r in [0, 1]) ≡ q
+ - interpolate(q1, q2, 0) ≡ q1
+ - interpolate(q1, q2, 1) ≡ q2
+ - interpolate(q, q, r) ≡ q, for all r in [0, 1]
 */
 using ConfigurationInterpolationFunction =
     std::function<Eigen::VectorXd(
@@ -45,29 +47,34 @@ struct CollisionCheckerParams {
   nullptr. */
   std::unique_ptr<RobotDiagram<double>> model;
 
+  // TODO(SeanCurtis-TRI): add doc hyperlinks to edge checking doc.
   /** A vector of model instance indices that identify which model instances
   belong to the robot. The list must be non-empty and must not include the
   world model instance. */
   std::vector<drake::multibody::ModelInstanceIndex> robot_model_instances;
 
+  // TODO(SeanCurtis-TRI): add doc hyperlinks to edge checking doc.
   /** Configuration (probably weighted) distance function.
   @note the `configuration_distance_function` object will be copied and retained
   by a collision checker, so if the function has any lambda-captured data then
   that data must outlive the collision checker. */
   ConfigurationDistanceFunction configuration_distance_function;
 
+  // TODO(SeanCurtis-TRI): add doc hyperlinks to edge checking doc.
   /** Step size for edge checking; in units compatible with the configuration
-   distance function..
+  distance function.
   Collision checking of edges q1->q2 is performed by interpolating from q1 to q2
   at edge_step_size steps and checking the interpolated configuration for
   collision. The value must be positive. */
   double edge_step_size{};
 
+  // TODO(SeanCurtis-TRI): add doc hyperlinks to edge checking doc.
   /** Additional padding to apply to all robot-environment collision queries. If
   distance between robot and environment is less than padding, the checker
   reports a collision. */
   double env_collision_padding{};
 
+  // TODO(SeanCurtis-TRI): add doc hyperlinks to edge checking doc.
   /** Additional padding to apply to all robot-robot self collision queries. If
   distance between robot and itself is less than padding, the checker reports a
   collision. */
@@ -75,4 +82,4 @@ struct CollisionCheckerParams {
 };
 
 }  // namespace planning
-}  // namespace anzu
+}  // namespace drake
