8191fe88bb9b63931286dcb8d5eeaf8176d567ac
Bassam ul Haq <bassamul.haq.ctr@tri.global>
2022-12-22 12:21:22 -0500

    Add VTK textured capsule source.
    Replace the vtkCapsuleSource with vtkTexturedCapsuleSource. This
    implementation contains the texture coordinates. It also changes the wireframe
    construction of the capsule. Now a single sphere is used where the poles are
    parallel to the cylinder body. The vtkCapsuleSource has two spheres with their
    poles parallel to the normal of the cylinder body. This change enables
    adding texture to the capsule body.

---
+++ third_party/com_gitlab_vtk/vtk_textured_capsule_source.h
@@ -0,0 +1,116 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkTexturedCapsuleSource.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/**
+ * @class   vtkTexturedCapsuleSource
+ * @brief   Generate a capsule centered at the origin
+ *
+ * vtkTexturedCapsuleSource creates a capsule (represented by polygons) of
+ * specified radius centered at the origin.  The resolution (polygonal
+ * discretization) in both the latitude (phi) and longitude (theta) directions
+ * can be specified as well as the length of the capsule cylinder
+ * (CylinderLength).
+ */
+
+#pragma once
+
+#include "vtkPolyDataAlgorithm.h"
+
+namespace com_gitlab_vtk {
+
+class vtkTexturedCapsuleSource : public vtkPolyDataAlgorithm  {
+ public:
+  vtkTypeMacro(vtkTexturedCapsuleSource, vtkPolyDataAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent) override;
+
+  /**
+   * Construct a capsule with radius 0.5 and resolution 8 in both the Phi and
+   * Theta directions and a cylinder of length 1.0.
+   */
+  static vtkTexturedCapsuleSource* New();
+
+  ///@{
+  /**
+   * Set/get the radius of the capsule.  The initial value is 0.5.
+   */
+  vtkSetClampMacro(Radius, double, 0.0, VTK_DOUBLE_MAX);
+  vtkGetMacro(Radius, double);
+  ///@}
+
+  ///@{
+  /**
+   * Set/get the center of the capsule.  The initial value is (0.0, 0.0, 0.0).
+   */
+  vtkSetVector3Macro(Center, double);
+  vtkGetVectorMacro(Center, double, 3);
+  ///@}
+
+  ///@{
+  /**
+   * Set/get the length of the cylinder.  The initial value is 1.0.
+   */
+  vtkSetClampMacro(CylinderLength, double, 0.0, VTK_DOUBLE_MAX);
+  vtkGetMacro(CylinderLength, double);
+  ///@}
+
+  ///@{
+  /**
+   * Set/get the number of points in the longitude direction for the spheres.
+   * The initial value is 8.
+   */
+  vtkSetClampMacro(ThetaResolution, int, 8, VTK_INT_MAX);
+  vtkGetMacro(ThetaResolution, int);
+  ///@}
+
+  ///@{
+  /**
+   * Set/get the number of points in the latitude direction for the spheres.
+   * The initial value is 8.
+   */
+  vtkSetClampMacro(PhiResolution, int, 8, VTK_INT_MAX);
+  vtkGetMacro(PhiResolution, int);
+  ///@}
+
+  ///@{
+  /**
+   * Set/get the desired precision for the output points.
+   * vtkAlgorithm::SINGLE_PRECISION - Output single-precision floating point.
+   * vtkAlgorithm::DOUBLE_PRECISION - Output double-precision floating point.
+   */
+  vtkSetMacro(OutputPointsPrecision, int);
+  vtkGetMacro(OutputPointsPrecision, int);
+  ///@}
+
+ protected:
+  explicit vtkTexturedCapsuleSource(int res = 8);
+  ~vtkTexturedCapsuleSource() override = default;
+
+  int RequestData(vtkInformation*, vtkInformationVector**,
+                  vtkInformationVector*) override;
+  int RequestInformation(vtkInformation*, vtkInformationVector**,
+                         vtkInformationVector*) override;
+
+  double Radius;
+  double Center[3];
+  int ThetaResolution;
+  int PhiResolution;
+  double CylinderLength;
+  int OutputPointsPrecision;
+
+ private:
+  vtkTexturedCapsuleSource(const vtkTexturedCapsuleSource&) = delete;
+  void operator=(const vtkTexturedCapsuleSource&) = delete;
+};
+
+}  // namespace com_gitlab_vtk
