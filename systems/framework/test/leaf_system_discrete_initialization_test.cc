a7744780185e46ab4fb518de444e1a6167535a39
Eric Cousineau <eric.cousineau@tri.global>
2023-01-07 20:42:48 -0500

    switch to init event; add zoh cascade

--- systems/framework/test/leaf_system_discrete_initialization_test.cc
+++ systems/framework/test/leaf_system_discrete_initialization_test.cc
@@ -15,6 +15,7 @@ class Dut : public LeafSystem<double> {
  public:
   Dut() {
     initialized_index_ = DeclareDiscreteState(VectorXd::Zero(1));
+    DeclareInitializationDiscreteUpdateEvent(&Dut::Initialize);
     DeclareVectorOutputPort(
         "initialized",
         BasicVector<double>(1),
@@ -30,28 +31,11 @@ class Dut : public LeafSystem<double> {
   }
 
  private:
-  void DoCalcNextUpdateTime(
+  EventStatus Initialize(
       const Context<double>& context,
-      CompositeEventCollection<double>* events,
-      double* time) const override {
-    LeafSystem<double>::DoCalcNextUpdateTime(context, events, time);
-    DRAKE_DEMAND(!events->HasEvents());
-    DRAKE_DEMAND(std::isinf(*time));
-    if (IsInitialized(context)) {
-      return;
-    }
-    // Schedule for now.
-    // TODO(eric.cousineau): Should this instead just be an initialization
-    // event?
-    *time = context.get_time();
-    auto initialize = [this](
-        const Context<double>& event_context,
-        const DiscreteUpdateEvent<double>&,
-        DiscreteValues<double>* result) {
-      result->get_mutable_value(initialized_index_)[0] = 1.0;
-    };
-    events->get_mutable_discrete_update_events().AddEvent(
-      DiscreteUpdateEvent<double>(initialize));
+      DiscreteValues<double>* state) const {
+    state->get_mutable_value(initialized_index_)[0] = 1.0;
+    return EventStatus::Succeeded();
   }
 
   DiscreteStateIndex initialized_index_;
@@ -62,46 +46,67 @@ GTEST_TEST(LeafSystemDiscreteInitializationTest, Behavior) {
 
   auto* dut = builder.AddSystem<Dut>();
   const double zoh_period_sec = 1.0;
-  auto* zoh = builder.AddSystem<ZeroOrderHold>(zoh_period_sec, 1);
-  builder.Connect(dut->get_output_port(), zoh->get_input_port());
+  auto* zoh_1 = builder.AddSystem<ZeroOrderHold>(zoh_period_sec, 1);
+  builder.Connect(dut->get_output_port(), zoh_1->get_input_port());
+  // Add cascade of ZOH.
+  auto* zoh_2 = builder.AddSystem<ZeroOrderHold>(zoh_period_sec, 1);
+  builder.Connect(zoh_1->get_output_port(), zoh_2->get_input_port());
+  auto* zoh_3 = builder.AddSystem<ZeroOrderHold>(zoh_period_sec, 1);
+  builder.Connect(zoh_2->get_output_port(), zoh_3->get_input_port());
 
   auto diagram = builder.Build();
   Simulator<double> simulator(*diagram);
 
   const auto& diagram_context = simulator.get_context();
   const auto& dut_context = dut->GetMyContextFromRoot(diagram_context);
-  const auto& zoh_context = zoh->GetMyContextFromRoot(diagram_context);
 
-  auto zoh_shows_initialized = [&]() {
-    return zoh_context.get_discrete_state(0)[0] != 0.0;
+  auto zoh_shows_initialized = [&](const System<double>* zoh) {
+    const auto& zoh_context = zoh->GetMyContextFromRoot(diagram_context);
+    return zoh_context.get_discrete_state(0)[0] == 1.0;
   };
 
   // Default context is as expected.
   EXPECT_FALSE(dut->IsInitialized(dut_context));
-  EXPECT_FALSE(zoh_shows_initialized());
+  EXPECT_FALSE(zoh_shows_initialized(zoh_1));
+  EXPECT_FALSE(zoh_shows_initialized(zoh_2));
+  EXPECT_FALSE(zoh_shows_initialized(zoh_3));
 
   // Initialize.
   simulator.Initialize();
-  // Note: Initialization() is a *specific* kind of event. Our manually
-  // scheduled pseudo-initialization event is not detectable beyond nominal
-  // event queuing.
-  EXPECT_FALSE(dut->IsInitialized(dut_context));
-  EXPECT_FALSE(zoh_shows_initialized());
+  // Our Dut initalization was processed. Note that the ZOH discrete update at
+  // t=0 is *not* an initialization event, thus was not processed.
+  EXPECT_TRUE(dut->IsInitialized(dut_context));
+  EXPECT_FALSE(zoh_shows_initialized(zoh_1));
+  EXPECT_FALSE(zoh_shows_initialized(zoh_2));
+  EXPECT_FALSE(zoh_shows_initialized(zoh_3));
 
   // Advance.
   simulator.AdvanceTo(0.0);
-  // Now initialized as we have processed the t=0 pseudo-initialization event.
+  // As before, we have processed the initialization event.
   EXPECT_TRUE(dut->IsInitialized(dut_context));
-  // The ZOH updated, but was operating on dut's y⁻(0) (not initialized), thus
-  // indicates no initialization.
-  EXPECT_FALSE(zoh_shows_initialized());
+  // The ZOH updated, and the dut's y⁻(0) was initialized, thus
+  // indicates initialization.
+  EXPECT_TRUE(zoh_shows_initialized(zoh_1));
+  // Subsequent ZOH values are not yet updated.
+  EXPECT_FALSE(zoh_shows_initialized(zoh_2));
+  EXPECT_FALSE(zoh_shows_initialized(zoh_3));
 
   // Advance to exactly the boundary. Process all events with
-  // AdvancePendingEvents().
+  // AdvancePendingEvents(). No change should occur.
   simulator.AdvanceTo(zoh_period_sec);
   simulator.AdvancePendingEvents();
   EXPECT_TRUE(dut->IsInitialized(dut_context));
-  EXPECT_TRUE(zoh_shows_initialized());
+  EXPECT_TRUE(zoh_shows_initialized(zoh_1));
+  EXPECT_TRUE(zoh_shows_initialized(zoh_2));
+  EXPECT_FALSE(zoh_shows_initialized(zoh_3));
+
+  // One more time, so that all are updated.
+  simulator.AdvanceTo(zoh_period_sec * 2);
+  simulator.AdvancePendingEvents();
+  EXPECT_TRUE(dut->IsInitialized(dut_context));
+  EXPECT_TRUE(zoh_shows_initialized(zoh_1));
+  EXPECT_TRUE(zoh_shows_initialized(zoh_2));
+  EXPECT_TRUE(zoh_shows_initialized(zoh_3));
 }
 
 }  // namespace
