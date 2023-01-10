d318b4d54be993ef2165f0ac7106b8d2eb86299e
hongkai-dai <daihongkai@gmail.com>
2023-01-09 23:07:51 -0800

    Add bounding box or rotated lorentz cone constraint for small sos polynomials.
    When calling NewSosPolynomial, if the gram matrix has size 1x1, add a bounding box constraint. if the gram matrix has size 2x2, add a rotated lorentz cone constraint.

--- solvers/mathematical_program.cc
+++ solvers/mathematical_program.cc
@@ -209,23 +209,56 @@ symbolic::Polynomial ComputePolynomialFromMonomialBasisAndGramMatrix(
   }
   return p;
 }
+
+bool IsSymmetricMatrix(const Eigen::Ref<const MatrixX<symbolic::Variable>>& X) {
+  if (X.rows() != X.cols()) {
+    return false;
+  }
+  for (int j = 0; j < X.cols(); ++j) {
+    for (int i = j + 1; i < X.rows(); ++i) {
+      if (!(X(i, j).equal_to(X(j, i)))) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 }  // namespace
 
 symbolic::Polynomial MathematicalProgram::NewSosPolynomial(
     const Eigen::Ref<const MatrixX<symbolic::Variable>>& gramian,
     const Eigen::Ref<const VectorX<symbolic::Monomial>>& monomial_basis,
     NonnegativePolynomial type) {
-  DRAKE_ASSERT(gramian.rows() == gramian.cols());
+  DRAKE_ASSERT(IsSymmetricMatrix(gramian));
   DRAKE_ASSERT(gramian.rows() == monomial_basis.rows());
   const symbolic::Polynomial p =
       ComputePolynomialFromMonomialBasisAndGramMatrix(monomial_basis, gramian);
   switch (type) {
     case MathematicalProgram::NonnegativePolynomial::kSos: {
-      AddPositiveSemidefiniteConstraint(gramian);
+      if (gramian.rows() == 1) {
+        // A 1x1 matrix being PSD is equivalent to its entry being non-negative.
+        AddBoundingBoxConstraint(0, kInf, gramian(0, 0));
+      } else if (gramian.rows() == 2) {
+        // A 2x2 matrix
+        // ⌈X(0, 0) X(0, 1)⌉
+        // ⌊X(0, 1) X(1, 1)⌋
+        // being psd is equivalent to [X(0, 0), X(1, 1), X(0, 1)] in the rotated
+        // lorentz cone.
+        AddRotatedLorentzConeConstraint(Vector3<symbolic::Variable>(
+            gramian(0, 0), gramian(1, 1), gramian(0, 1)));
+      } else {
+        AddPositiveSemidefiniteConstraint(gramian);
+      }
       return p;
     }
     case MathematicalProgram::NonnegativePolynomial::kSdsos: {
-      AddScaledDiagonallyDominantMatrixConstraint(gramian);
+      if (gramian.rows() == 1) {
+        // A 1x1 matrix being scaled diagonally dominant is equivalent to its
+        // entry being non-negative.
+        AddBoundingBoxConstraint(0, kInf, gramian(0, 0));
+      } else {
+        AddScaledDiagonallyDominantMatrixConstraint(gramian);
+      }
       return p;
     }
     case MathematicalProgram::NonnegativePolynomial::kDsos: {
