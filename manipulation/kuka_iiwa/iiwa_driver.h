afe3b47c7618c4595d3266d3582c431788851537
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-01-15 17:18:20 -0800

    PR [pydrake] Fix docstring formatting for IiwaDriver.control_mode
    The prior docstring leads to a malformed property, which shows up
    wrong on the website and breaks Mypy.

--- manipulation/kuka_iiwa/iiwa_driver.h
+++ manipulation/kuka_iiwa/iiwa_driver.h
@@ -24,10 +24,10 @@ struct IiwaDriver {
   /** Per BuildIiwaControl. */
   double ext_joint_filter_tau{0.01};
 
-  /// Per ParseIiwaControlMode. Valid options are:
-  /// - "position_only"
-  /// - "position_and_torque" (default)
-  /// - "torque_only"
+  /** The driver's control mode. Valid options (per ParseIiwaControlMode) are:
+  - "position_only"
+  - "position_and_torque" (default)
+  - "torque_only" */
   std::string control_mode{"position_and_torque"};
 
   std::string lcm_bus{"default"};
