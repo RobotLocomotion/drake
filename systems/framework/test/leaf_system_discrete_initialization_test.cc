9819741ae7c6bdc6a8375e16ad087f3ca0deefde
Eric Cousineau <eric.cousineau@tri.global>
2023-01-16 12:34:03 -0500

    split up and expand testing

--- systems/framework/test/leaf_system_discrete_initialization_test.cc
+++ systems/framework/test/leaf_system_discrete_initialization_test.cc
@@ -10,17 +10,19 @@ namespace systems {
 namespace {
 
 using Eigen::VectorXd;
-const double kEps = std::numeric_limits<double>::epsilon();
+
 class Dut : public LeafSystem<double> {
  public:
-  explicit Dut(double period_sec) {
+  explicit Dut(double period_sec_) {
     // Initialization.
     initialized_index_ = DeclareDiscreteState(VectorXd::Zero(1));
+    initialize_counter_index_ = DeclareDiscreteState(VectorXd::Zero(1));
     DeclareInitializationDiscreteUpdateEvent(&Dut::Initialize);
     DeclareStateOutputPort("initialized", initialized_index_);
     // Discrete counter.
-    counter_index_ = DeclareDiscreteState(VectorXd::Zero(1));
-    DeclarePeriodicDiscreteUpdateEvent(period_sec, 0.0, &Dut::UpdateCounter);
+    periodic_counter_index_ = DeclareDiscreteState(VectorXd::Zero(1));
+    DeclarePeriodicDiscreteUpdateEvent(
+        period_sec_, 0.0, &Dut::UpdatePeriodicCounter);
   }
 
   bool IsInitialized(const Context<double>& context) const {
@@ -29,9 +31,15 @@ class Dut : public LeafSystem<double> {
     return initialized == 1.0;
   }
 
-  int GetCounter(const Context<double>& context) const {
+  int GetInitializeCounter(const Context<double>& context) const {
     const double counter =
-        context.get_discrete_state(counter_index_)[0];
+        context.get_discrete_state(initialize_counter_index_)[0];
+    return static_cast<int>(counter);
+  }
+
+  int GetPeriodicCounter(const Context<double>& context) const {
+    const double counter =
+        context.get_discrete_state(periodic_counter_index_)[0];
     return static_cast<int>(counter);
   }
 
@@ -40,124 +48,186 @@ class Dut : public LeafSystem<double> {
       const Context<double>& context,
       DiscreteValues<double>* state) const {
     state->get_mutable_value(initialized_index_)[0] = 1.0;
+    state->get_mutable_value(initialize_counter_index_)[0] += 1.0;
     return EventStatus::Succeeded();
   }
 
-  void UpdateCounter(
+  void UpdatePeriodicCounter(
       const Context<double>& context,
       DiscreteValues<double>* state) const {
-    state->get_mutable_value(counter_index_)[0] += 1.0;
+    state->get_mutable_value(periodic_counter_index_)[0] += 1.0;
   }
 
   DiscreteStateIndex initialized_index_;
-  DiscreteStateIndex counter_index_;
+  DiscreteStateIndex initialize_counter_index_;
+  DiscreteStateIndex periodic_counter_index_;
 };
 
-GTEST_TEST(LeafSystemDiscreteInitializationTest, Behavior) {
-  DiagramBuilder<double> builder;
+class LeafSystemDiscreteInitializationTest : public ::testing::Test {
+ public:
+  LeafSystemDiscreteInitializationTest() { }
 
-  const double period_sec = 1.0;
-  // Add Dut.
-  auto* dut = builder.AddSystem<Dut>(period_sec);
-  // Add cascade of ZOH.
-  // N.B. Initializing has no effect on this sequence of events.
-  const bool initialize = true;
-  auto* zoh_1 =
-      builder.AddSystem<ZeroOrderHold>(period_sec, 1, initialize);
-  auto* zoh_2 =
-      builder.AddSystem<ZeroOrderHold>(period_sec, 1, initialize);
-  auto* zoh_3 =
-      builder.AddSystem<ZeroOrderHold>(period_sec, 1, initialize);
-  // Connect 3 ZOH in series.
+ protected:
+  void BuildDiagram(bool zoh_initialize) {
+    DiagramBuilder<double> builder;
+
+    // Add Dut.
+    dut_ = builder.AddSystem<Dut>(period_sec_);
+    // Add cascade of ZOH.
+    zoh_1_ = builder.AddSystem<ZeroOrderHold>(period_sec_, 1, zoh_initialize);
+    zoh_2_ = builder.AddSystem<ZeroOrderHold>(period_sec_, 1, zoh_initialize);
+    zoh_3_ = builder.AddSystem<ZeroOrderHold>(period_sec_, 1, zoh_initialize);
+    // Connect 3 ZOH in series.
+    builder.Connect(dut_->get_output_port(), zoh_1_->get_input_port());
+    builder.Connect(zoh_1_->get_output_port(), zoh_2_->get_input_port());
+    builder.Connect(zoh_2_->get_output_port(), zoh_3_->get_input_port());
+
+    diagram_ = builder.Build();
+    simulator_ = std::make_unique<Simulator<double>>(*diagram_);
+    diagram_context_ = &simulator_->get_mutable_context();
+    dut_context_ = &dut_->GetMyContextFromRoot(*diagram_context_);
+  }
+
+  void CheckDefaultContext() const {
+    // Default context is as expected.
+    EXPECT_FALSE(dut_->IsInitialized(*dut_context_));
+    EXPECT_EQ(dut_->GetInitializeCounter(*dut_context_), 0);
+    EXPECT_EQ(dut_->GetPeriodicCounter(*dut_context_), 0);
+    EXPECT_FALSE(IsZohInitialized(zoh_1_));
+    EXPECT_FALSE(IsZohInitialized(zoh_2_));
+    EXPECT_FALSE(IsZohInitialized(zoh_3_));
+  }
+
+  bool IsZohInitialized(const System<double>* zoh) const {
+    const auto& zoh_context = zoh->GetMyContextFromRoot(*diagram_context_);
+    return zoh_context.get_discrete_state(0)[0] == 1.0;
+  }
+
+  const double period_sec_ = 1.0;
+
+  const Dut* dut_{};
+  const ZeroOrderHold<double>* zoh_1_{};
+  const ZeroOrderHold<double>* zoh_2_{};
+  const ZeroOrderHold<double>* zoh_3_{};
+  std::unique_ptr<Diagram<double>> diagram_;
+  std::unique_ptr<Simulator<double>> simulator_;
+  const Context<double>* diagram_context_{};
+  const Context<double>* dut_context_{};
+};
+
+TEST_F(LeafSystemDiscreteInitializationTest, Nominal) {
+  for (bool zoh_initialize : {false, true}) {
+    SCOPED_TRACE(fmt::format("zoh_initialize = {}", zoh_initialize));
+    BuildDiagram(zoh_initialize);
+    CheckDefaultContext();
+
+    // Initialize.
+    simulator_->Initialize();
+    // Our Dut initalization was processed. Note that the ZOH discrete update at
+    // t=0 is *not* an initialization event, thus was not processed.
+    EXPECT_TRUE(dut_->IsInitialized(*dut_context_));
+    EXPECT_EQ(dut_->GetInitializeCounter(*dut_context_), 1);
+    EXPECT_EQ(dut_->GetPeriodicCounter(*dut_context_), 0);
+    EXPECT_FALSE(IsZohInitialized(zoh_1_));
+    EXPECT_FALSE(IsZohInitialized(zoh_2_));
+    EXPECT_FALSE(IsZohInitialized(zoh_3_));
+
+    // Advance.
+    simulator_->AdvanceTo(0.0);
+    // As before, we have processed the initialization event.
+    EXPECT_TRUE(dut_->IsInitialized(*dut_context_));
+    EXPECT_EQ(dut_->GetInitializeCounter(*dut_context_), 1);
+    // We have also processed one discrete update.
+    EXPECT_EQ(dut_->GetPeriodicCounter(*dut_context_), 1);
+    // The ZOH updated, and the dut_'s y⁻(0) was initialized, thus
+    // indicates initialization.
+    EXPECT_TRUE(IsZohInitialized(zoh_1_));
+    // Subsequent ZOH values are not yet updated.
+    EXPECT_FALSE(IsZohInitialized(zoh_2_));
+    EXPECT_FALSE(IsZohInitialized(zoh_3_));
+
+    // Advance to exactly the boundary. Process all events with
+    // AdvancePendingEvents(). No change should occur.
+    simulator_->AdvanceTo(period_sec_);
+    simulator_->AdvancePendingEvents();
+    EXPECT_TRUE(dut_->IsInitialized(*dut_context_));
+    EXPECT_EQ(dut_->GetInitializeCounter(*dut_context_), 1);
+    EXPECT_EQ(dut_->GetPeriodicCounter(*dut_context_), 2);
+    EXPECT_TRUE(IsZohInitialized(zoh_1_));
+    EXPECT_TRUE(IsZohInitialized(zoh_2_));
+    EXPECT_FALSE(IsZohInitialized(zoh_3_));
+
+    // One more time, so that all are updated.
+    simulator_->AdvanceTo(period_sec_ * 2);
+    simulator_->AdvancePendingEvents();
+    EXPECT_TRUE(dut_->IsInitialized(*dut_context_));
+    EXPECT_EQ(dut_->GetInitializeCounter(*dut_context_), 1);
+    EXPECT_EQ(dut_->GetPeriodicCounter(*dut_context_), 3);
+    EXPECT_TRUE(IsZohInitialized(zoh_1_));
+    EXPECT_TRUE(IsZohInitialized(zoh_2_));
+    EXPECT_TRUE(IsZohInitialized(zoh_3_));
+  }
+}
+
+TEST_F(LeafSystemDiscreteInitializationTest, RepeatedInitialize) {
+  // Show that simultaneous discrete updates can be handled by precisely
+  // calling `simulator_->Initialize()` by order of cascade through showing
+  // converged behavior for discrete initialization events.
+
+  // Note: Given the use of the initialization counter, we don't have a fixed
+  // point the strictest of senses. However, this is only meant for testing /
+  // inspection.
+  // Users wanting to use this trick should ideally:
+  // - Not encode "non-fixed-point beahvior" for discrete intialialization
+  //   events
+  // - Avoid any external effects in those events. Instead, defer them to
+  //   (periodic) publish events.
+
+  // Discrete cascade order is one more due to our dut_'s initialization event.
   const int zoh_cascade_order = 3;
-  builder.Connect(dut->get_output_port(), zoh_1->get_input_port());
-  builder.Connect(zoh_1->get_output_port(), zoh_2->get_input_port());
-  builder.Connect(zoh_2->get_output_port(), zoh_3->get_input_port());
-  // Discrete cascade order is one more due to our dut's initialization event.
   const int discrete_initialization_cascade_order = zoh_cascade_order + 1;
 
-  auto diagram = builder.Build();
-  Simulator<double> simulator(*diagram);
+  // Show that this does *not* work for zoh_initialize = false.
+  {
+    SCOPED_TRACE("zoh_initialize = false");
+    const bool zoh_initialize = false;
+    BuildDiagram(zoh_initialize);
+    CheckDefaultContext();
 
-  auto& diagram_context = simulator.get_mutable_context();
-  const auto diagram_context_init = diagram_context.Clone();
-  const auto& dut_context = dut->GetMyContextFromRoot(diagram_context);
+    for (int i = 0; i < discrete_initialization_cascade_order; ++i) {
+      simulator_->Initialize();
+    }
 
-  auto zoh_shows_initialized = [&](const System<double>* zoh) {
-    const auto& zoh_context = zoh->GetMyContextFromRoot(diagram_context);
-    return zoh_context.get_discrete_state(0)[0] == 1.0;
-  };
-
-  // Default context is as expected.
-  EXPECT_FALSE(dut->IsInitialized(dut_context));
-  EXPECT_EQ(dut->GetCounter(dut_context), 0);
-  EXPECT_FALSE(zoh_shows_initialized(zoh_1));
-  EXPECT_FALSE(zoh_shows_initialized(zoh_2));
-  EXPECT_FALSE(zoh_shows_initialized(zoh_3));
-
-  // Initialize.
-  simulator.Initialize();
-  // Our Dut initalization was processed. Note that the ZOH discrete update at
-  // t=0 is *not* an initialization event, thus was not processed.
-  EXPECT_TRUE(dut->IsInitialized(dut_context));
-  EXPECT_EQ(dut->GetCounter(dut_context), 0);
-  EXPECT_FALSE(zoh_shows_initialized(zoh_1));
-  EXPECT_FALSE(zoh_shows_initialized(zoh_2));
-  EXPECT_FALSE(zoh_shows_initialized(zoh_3));
-
-  // Advance.
-  simulator.AdvanceTo(0.0);
-  // As before, we have processed the initialization event.
-  EXPECT_TRUE(dut->IsInitialized(dut_context));
-  // We have also processed one discrete update.
-  EXPECT_EQ(dut->GetCounter(dut_context), 1);
-  // The ZOH updated, and the dut's y⁻(0) was initialized, thus
-  // indicates initialization.
-  EXPECT_TRUE(zoh_shows_initialized(zoh_1));
-  // Subsequent ZOH values are not yet updated.
-  EXPECT_FALSE(zoh_shows_initialized(zoh_2));
-  EXPECT_FALSE(zoh_shows_initialized(zoh_3));
-
-  // Advance to exactly the boundary. Process all events with
-  // AdvancePendingEvents(). No change should occur.
-  simulator.AdvanceTo(period_sec);
-  simulator.AdvancePendingEvents();
-  EXPECT_TRUE(dut->IsInitialized(dut_context));
-  EXPECT_EQ(dut->GetCounter(dut_context), 2);
-  EXPECT_TRUE(zoh_shows_initialized(zoh_1));
-  EXPECT_TRUE(zoh_shows_initialized(zoh_2));
-  EXPECT_FALSE(zoh_shows_initialized(zoh_3));
-
-  // One more time, so that all are updated.
-  simulator.AdvanceTo(period_sec * 2);
-  simulator.AdvancePendingEvents();
-  EXPECT_TRUE(dut->IsInitialized(dut_context));
-  EXPECT_EQ(dut->GetCounter(dut_context), 3);
-  EXPECT_TRUE(zoh_shows_initialized(zoh_1));
-  EXPECT_TRUE(zoh_shows_initialized(zoh_2));
-  EXPECT_TRUE(zoh_shows_initialized(zoh_3));
-
-  // Reset and show we're back to initial state.
-  diagram_context.SetTimeStateAndParametersFrom(*diagram_context_init);
-  EXPECT_FALSE(dut->IsInitialized(dut_context));
-  EXPECT_EQ(dut->GetCounter(dut_context), 0);
-  EXPECT_FALSE(zoh_shows_initialized(zoh_1));
-  EXPECT_FALSE(zoh_shows_initialized(zoh_2));
-  EXPECT_FALSE(zoh_shows_initialized(zoh_3));
-
-  // Show that simultaneous discrete updates can be handled by precisely
-  // calling `simulator.Initialize)` by a value related to degree of cascade by
-  // showing ultimate consistency.
-  // WARNING: This will only work if ZeroOrderHold(..., initialize=True).
-  // TODO(eric.cousineau): Make explicit test case for this.
-  for (int i = 0; i < discrete_initialization_cascade_order; ++i) {
-    simulator.Initialize();
+    EXPECT_TRUE(dut_->IsInitialized(*dut_context_));
+    EXPECT_EQ(dut_->GetPeriodicCounter(*dut_context_), 0);
+    EXPECT_EQ(
+        dut_->GetInitializeCounter(*dut_context_),
+        discrete_initialization_cascade_order);
+    EXPECT_FALSE(IsZohInitialized(zoh_1_));
+    EXPECT_FALSE(IsZohInitialized(zoh_2_));
+    EXPECT_FALSE(IsZohInitialized(zoh_3_));
   }
-  EXPECT_TRUE(dut->IsInitialized(dut_context));
-  EXPECT_EQ(dut->GetCounter(dut_context), 0);
-  EXPECT_TRUE(zoh_shows_initialized(zoh_1));
-  EXPECT_TRUE(zoh_shows_initialized(zoh_2));
-  EXPECT_TRUE(zoh_shows_initialized(zoh_3));
+
+  // Show that it does work for zoh_initialize = true.
+  {
+    SCOPED_TRACE("zoh_initialize = true");
+    const bool zoh_initialize = true;
+    BuildDiagram(zoh_initialize);
+    CheckDefaultContext();
+
+    for (int i = 0; i < discrete_initialization_cascade_order; ++i) {
+      simulator_->Initialize();
+    }
+
+    EXPECT_TRUE(dut_->IsInitialized(*dut_context_));
+    EXPECT_EQ(
+        dut_->GetInitializeCounter(*dut_context_),
+        discrete_initialization_cascade_order);
+    EXPECT_EQ(dut_->GetPeriodicCounter(*dut_context_), 0);
+    EXPECT_TRUE(IsZohInitialized(zoh_1_));
+    EXPECT_TRUE(IsZohInitialized(zoh_2_));
+    EXPECT_TRUE(IsZohInitialized(zoh_3_));
+  }
 }
 
 }  // namespace
