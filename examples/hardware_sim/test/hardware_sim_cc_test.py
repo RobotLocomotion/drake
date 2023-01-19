f6064d0a7dc87d6e48014bbd42924de1c2170d3f
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-01-19 11:25:09 -0800

    [examples] Add main and yaml parsing to python hardware_sim (#18566)

--- examples/hardware_sim/test/hardware_sim_cc_test.py
+++ examples/hardware_sim/test/hardware_sim_cc_test.py
@@ -5,12 +5,12 @@ from drake.examples.hardware_sim.test.hardware_sim_test_common import (
 )
 
 
-class HardwareSimPyTest(HardwareSimTest, unittest.TestCase):
-    """Smoke test for our hardware_sim_py program.
+class HardwareSimCcTest(HardwareSimTest, unittest.TestCase):
+    """Smoke test for our hardware_sim_cc program.
 
     All of the actual test cases are defined in our base class.
     """
 
     def __init__(self, *args, **kwargs):
-        self._sim_lang = "py"
+        self._sim_lang = "cc"
         super().__init__(*args, **kwargs)
