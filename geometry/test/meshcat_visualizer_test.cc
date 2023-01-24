731f7b398fedf0306707dac03459b0c06666a311
Rick Poyner <rick.poyner@tri.global>
2023-01-24 11:31:35 -0500

    Port to parser.AddModels, part 7

--- geometry/test/meshcat_visualizer_test.cc
+++ geometry/test/meshcat_visualizer_test.cc
@@ -316,10 +316,10 @@ GTEST_TEST(MeshcatVisualizerTest, MultipleModels) {
   std::string urdf = FindResourceOrThrow(
       "drake/manipulation/models/iiwa_description/urdf/"
       "iiwa14_no_collision.urdf");
-  auto iiwa0 = multibody::Parser(&plant).AddModelFromFile(urdf);
+  auto iiwa0 = multibody::Parser(&plant).AddModels(urdf).at(0);
   plant.WeldFrames(plant.world_frame(),
                    plant.GetBodyByName("base", iiwa0).body_frame());
-  auto iiwa1 = multibody::Parser(&plant).AddModelFromFile(urdf, "second_iiwa");
+  auto iiwa1 = multibody::Parser(&plant, "second").AddModels(urdf).at(0);
   plant.WeldFrames(plant.world_frame(),
                    plant.GetBodyByName("base", iiwa1).body_frame());
   plant.Finalize();
@@ -339,18 +339,18 @@ GTEST_TEST(MeshcatVisualizerTest, MultipleModels) {
   auto context = diagram->CreateDefaultContext();
 
   EXPECT_FALSE(meshcat->HasPath("/drake/visualizer/iiwa14"));
-  EXPECT_FALSE(meshcat->HasPath("/drake/visualizer/second_iiwa"));
+  EXPECT_FALSE(meshcat->HasPath("/drake/visualizer/second/iiwa14"));
 
   diagram->ForcedPublish(*context);
 
   EXPECT_TRUE(meshcat->HasPath("/drake/visualizer/iiwa14"));
-  EXPECT_TRUE(meshcat->HasPath("/drake/visualizer/second_iiwa"));
+  EXPECT_TRUE(meshcat->HasPath("/drake/visualizer/second/iiwa14"));
   for (int link = 0; link < 8; link++) {
     EXPECT_NE(meshcat->GetPackedTransform(
                   fmt::format("/drake/visualizer/iiwa14/iiwa_link_{}", link)),
               "");
     EXPECT_NE(meshcat->GetPackedTransform(fmt::format(
-                  "/drake/visualizer/second_iiwa/iiwa_link_{}", link)),
+                  "/drake/visualizer/second/iiwa14/iiwa_link_{}", link)),
               "");
   }
 }
