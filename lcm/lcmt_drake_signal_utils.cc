5487cfa0d2906198acff19a0cf6f937b573289ba
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-01-30 12:20:39 -0800

    [lcm,visualization] Opt-in to clang-format linting

--- lcm/lcmt_drake_signal_utils.cc
+++ lcm/lcmt_drake_signal_utils.cc
@@ -14,8 +14,8 @@ bool CompareLcmtDrakeSignalMessages(const lcmt_drake_signal& actual_message,
 
   if (n != expected_message.dim) {
     drake::log()->trace(
-        "CompareLcmtDrakeSignalMessages: Dimensions mismatch ({} vs. {}).",
-        n, expected_message.dim);
+        "CompareLcmtDrakeSignalMessages: Dimensions mismatch ({} vs. {}).", n,
+        expected_message.dim);
     result = false;
   }
 
@@ -37,14 +37,14 @@ bool CompareLcmtDrakeSignalMessages(const lcmt_drake_signal& actual_message,
   for (int i = 0; i < n && result; ++i) {
     if (actual_message.val[i] != expected_message.val[i]) {
       drake::log()->trace(
-          "CompareLcmtDrakeSignalMessages: val {} mismatch ({} vs. {}).",
-          i, actual_message.val[i], expected_message.val[i]);
+          "CompareLcmtDrakeSignalMessages: val {} mismatch ({} vs. {}).", i,
+          actual_message.val[i], expected_message.val[i]);
       result = false;
     }
     if (actual_message.coord[i] != expected_message.coord[i]) {
       drake::log()->trace(
-          "CompareLcmtDrakeSignalMessages: coord {} mismatch ({} vs. {}).",
-          i, actual_message.coord[i], expected_message.coord[i]);
+          "CompareLcmtDrakeSignalMessages: coord {} mismatch ({} vs. {}).", i,
+          actual_message.coord[i], expected_message.coord[i]);
       result = false;
     }
   }
