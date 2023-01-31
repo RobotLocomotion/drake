1f66a7d9b0c620b93e5178a1dbcf1d083d9ba4ce
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-01-31 12:02:37 -0800

    [py planning] Remove RobotBuilder const vs non-const distinction
    In Python, only the mutable getters make any sense when using the
    builder pattern.

--- bindings/pydrake/planning/test/robot_diagram_test.py
+++ bindings/pydrake/planning/test/robot_diagram_test.py
@@ -18,24 +18,17 @@ class TestRobotDiagram(unittest.TestCase):
         Class = mut.RobotDiagramBuilder_[T]
         dut = Class(time_step=0.0)
         if T == float:
-            # Most users would use the mutable_parser function.
-            dut.mutable_parser().AddModels(FindResourceOrThrow(
+            dut.parser().AddModels(FindResourceOrThrow(
                 "drake/manipulation/models/iiwa_description/urdf/"
                 "iiwa14_spheres_dense_collision.urdf"))
-            # Sanity check that this is bound. There are currently no const
-            # functions defined by Parser, so it's somewehat useless.
-            self.assertIsInstance(dut.parser(), Parser)
         else:
-            # TODO(jwnimmer-tri) Use dut.mutable_plant() to manually add some
+            # TODO(jwnimmer-tri) Use dut.plant() to manually add some
             # models, bodies, and geometries here.
             pass
 
         # Sanity check all of the accessors.
-        self.assertIsInstance(dut.mutable_builder(), DiagramBuilder_[T])
         self.assertIsInstance(dut.builder(), DiagramBuilder_[T])
-        self.assertIsInstance(dut.mutable_plant(), MultibodyPlant_[T])
         self.assertIsInstance(dut.plant(), MultibodyPlant_[T])
-        self.assertIsInstance(dut.mutable_scene_graph(), SceneGraph_[T])
         self.assertIsInstance(dut.scene_graph(), SceneGraph_[T])
 
         # Finalize.
