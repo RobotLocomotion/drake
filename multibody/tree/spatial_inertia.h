14654ddd123ad8b19e27c50b19dff76297b4f742
mitiguy <paul.mitiguy@tri.global>
2023-02-23 17:35:20 -0800

    Add SpatialInertial::HollowSphereWithDensity().

--- multibody/tree/spatial_inertia.h
+++ multibody/tree/spatial_inertia.h
@@ -191,6 +191,20 @@ class SpatialInertia {
   /// @throws std::exception if r is zero or negative.
   static SpatialInertia<T> SolidSphereWithDensity(const T& density, const T& r);
 
+  /// Creates a spatial inertia for a uniform density thin hollow sphere B about
+  /// its geometric center Bo (which is coincident with B's center of mass Bcm).
+  /// @param[in] density mass per unit area (kg/m²).
+  /// @param[in] r sphere's radius (the hollow sphere is regarded as an
+  /// infinitesimally thin shell of uniform density).
+  /// @retval M_BBo B's spatial inertia about Bo. Since B's rotational inertia
+  ///   is triaxially symmetric, M_BBo_B = M_BBo_E, i.e., M_BBo expressed in
+  ///   frame B is equal to M_BBo expressed in an arbitrary frame E.
+  /// @note B's rotational inertia about Bo is triaxially symmetric, meaning
+  ///   B's has an equal moment of inertia about any line passing through Bo.
+  /// @throws std::exception if r is zero or negative.
+  static SpatialInertia<T> HollowSphereWithDensity(
+      const T& density, const T& r);
+
   /// Creates a spatial inertia for a uniform density solid tetrahedron B about
   /// a point A, from which position vectors to B's 4 vertices B0, B1, B2, B3
   /// are measured (position vectors are all expressed in a common frame E).
