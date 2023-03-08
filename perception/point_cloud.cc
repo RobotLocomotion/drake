8e60c0cee719ea31505892ef81f6b02165cd7d64
Zach Fang <zach.fang@tri.global>
2023-03-07 22:12:36 -0500

    [perception] PointCloud can morph its fields after construction
    Prior to this PR, the fields of a PointCloud object is determined
    at construction time, and doesn't allow any morphing after that.
    An exception will be thrown if one attempts to copy/move-assign a
    PointCloud with a different `Fields` to another. However, the same
    PointCloud can be used to copy/move-construct another Pointcloud.
    This PR tries to make the behavior consistent and avoid confusions
    for users.

--- perception/point_cloud.cc
+++ perception/point_cloud.cc
@@ -83,7 +83,10 @@ class PointCloud::Storage {
     normals_.conservativeResize(NoChange,
                                 f.contains(pc_flags::kNormals) ? size_ : 0);
     rgbs_.conservativeResize(NoChange, f.contains(pc_flags::kRGBs) ? size_ : 0);
-    descriptors_.conservativeResize(NoChange, f.has_descriptor() ? size_ : 0);
+    // Note: The row number can change depends on whether 'f' contains a
+    // descriptor field and the type of the descriptor.
+    descriptors_.conservativeResize(f.descriptor_type().size(),
+                                    f.has_descriptor() ? size_ : 0);
     fields_ = f;
     CheckInvariants();
   }
@@ -132,23 +135,21 @@ pc_flags::Fields ResolveFields(
   }
 }
 
-// Resolves the fields from a pair of point clouds and desired fields.
-// Implements the resolution rules in `SetFrom`.
-// @pre Valid point clouds `a` and `b`.
-// @returns Fields that both point clouds have.
-pc_flags::Fields ResolvePairFields(
-    const PointCloud& a,
-    const PointCloud& b,
-    pc_flags::Fields fields) {
-  if (fields == pc_flags::kInherit) {
-    // If we do not permit a subset, expect the exact same fields.
-    a.RequireExactFields(b.fields());
-    return a.fields();
-  } else {
-    a.RequireFields(fields);
-    b.RequireFields(fields);
-    return fields;
+// Finds the added fields from `new_fields` w.r.t. `old_fields`.
+pc_flags::Fields FindAddedFields(pc_flags::Fields old_fields,
+                                 pc_flags::Fields new_fields) {
+  pc_flags::Fields added_fields(pc_flags::kNone);
+  for (const auto field :
+       {pc_flags::kXYZs, pc_flags::kNormals, pc_flags::kRGBs}) {
+    if (!old_fields.contains(field) && new_fields.contains(field))
+      added_fields |= field;
   }
+
+  if (new_fields.has_descriptor() &&
+      old_fields.descriptor_type() != new_fields.descriptor_type()) {
+    added_fields |= new_fields.descriptor_type();
+  }
+  return added_fields;
 }
 
 }  // namespace
@@ -172,7 +173,7 @@ PointCloud::PointCloud(const PointCloud& other,
 }
 
 PointCloud::PointCloud(PointCloud&& other)
-    : PointCloud(0, other.fields(), true) {
+    : PointCloud(0, other.storage_->fields(), true) {
   // This has zero size. Directly swap storages.
   storage_.swap(other.storage_);
 }
@@ -183,8 +184,6 @@ PointCloud& PointCloud::operator=(const PointCloud& other) {
 }
 
 PointCloud& PointCloud::operator=(PointCloud&& other) {
-  // We may only take rvalue references if the fields match exactly.
-  RequireExactFields(other.fields());
   // Swap storages.
   storage_.swap(other.storage_);
   // Empty out the other cloud, but let it remain being a valid point cloud
@@ -207,11 +206,33 @@ int PointCloud::size() const {
 void PointCloud::resize(int new_size, bool skip_initialization) {
   DRAKE_DEMAND(new_size >= 0);
   const int old_size = size();
+  if (old_size == new_size)
+    return;
   storage_->resize(new_size);
   DRAKE_DEMAND(storage_->size() == new_size);
   if (new_size > old_size && !skip_initialization) {
     const int size_diff = new_size - old_size;
     SetDefault(old_size, size_diff);
+  }
+}
+
+void PointCloud::SetFields(pc_flags::Fields new_fields, bool skip_initialize) {
+  const pc_flags::Fields old_fields = storage_->fields();
+  if (old_fields == new_fields)
+    return;
+  storage_->UpdateFields(new_fields);
+
+  if (!skip_initialize) {
+    // Default-initialize containers for newly added fields.
+    pc_flags::Fields added_fields = FindAddedFields(old_fields, new_fields);
+    if (added_fields.contains(pc_flags::kXYZs))
+      mutable_xyzs().middleCols(0, size()).setConstant(kDefaultValue);
+    if (added_fields.contains(pc_flags::kNormals))
+      mutable_normals().middleCols(0, size()).setConstant(kDefaultValue);
+    if (added_fields.contains(pc_flags::kRGBs))
+      mutable_rgbs().middleCols(0, size()).setConstant(kDefaultColor);
+    if (added_fields.has_descriptor())
+      mutable_descriptors().middleCols(0, size()).setConstant(kDefaultValue);
   }
 }
 
@@ -236,6 +257,17 @@ void PointCloud::SetDefault(int start, int num) {
 void PointCloud::SetFrom(const PointCloud& other,
                          pc_flags::Fields fields_in,
                          bool allow_resize) {
+  // Update or check the fields of the point cloud(s) if necessary.
+  pc_flags::Fields fields_to_copy = fields_in;
+  if (fields_in == pc_flags::kInherit) {
+    fields_to_copy = other.storage_->fields();
+    SetFields(other.storage_->fields());
+  } else {
+    this->RequireFields(fields_to_copy);
+    other.RequireFields(fields_to_copy);
+  }
+
+  // Update the size of this point cloud if necessary.
   int old_size = size();
   int new_size = other.size();
   if (allow_resize) {
@@ -244,18 +276,18 @@ void PointCloud::SetFrom(const PointCloud& other,
     throw std::runtime_error(
         fmt::format("SetFrom: {} != {}", new_size, old_size));
   }
-  pc_flags::Fields fields_resolved =
-      ResolvePairFields(*this, other, fields_in);
-  if (fields_resolved.contains(pc_flags::kXYZs)) {
+
+  // Populate data from `other` to this point cloud.
+  if (fields_to_copy.contains(pc_flags::kXYZs)) {
     mutable_xyzs() = other.xyzs();
   }
-  if (fields_resolved.contains(pc_flags::kNormals)) {
+  if (fields_to_copy.contains(pc_flags::kNormals)) {
     mutable_normals() = other.normals();
   }
-  if (fields_resolved.contains(pc_flags::kRGBs)) {
+  if (fields_to_copy.contains(pc_flags::kRGBs)) {
     mutable_rgbs() = other.rgbs();
   }
-  if (fields_resolved.has_descriptor()) {
+  if (fields_to_copy.has_descriptor()) {
     mutable_descriptors() = other.descriptors();
   }
 }
