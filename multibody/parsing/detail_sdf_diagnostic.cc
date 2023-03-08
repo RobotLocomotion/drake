43d0905ae690524926a688bae94561d229a62a7d
Marco A. Gutierrez <marco@openrobotics.org>
2023-03-08 04:46:19 +0000

    [parsing] make sdf errors use diagnostic mechanism

--- multibody/parsing/detail_sdf_diagnostic.cc
+++ multibody/parsing/detail_sdf_diagnostic.cc
@@ -1,5 +1,7 @@
 #include "drake/multibody/parsing/detail_sdf_diagnostic.h"
 
+#include <utility>
+
 #include "drake/common/drake_assert.h"
 
 namespace drake {
@@ -9,40 +11,135 @@ namespace internal {
 using drake::internal::DiagnosticDetail;
 using drake::internal::DiagnosticPolicy;
 
+SDFormatDiagnostic::SDFormatDiagnostic(
+    const drake::internal::DiagnosticPolicy* diagnostic,
+    const drake::multibody::internal::DataSource* data_source,
+    const std::string& file_extension)
+    : diagnostic_(diagnostic), data_source_(data_source),
+      file_extension_(file_extension) {
+  DRAKE_DEMAND(diagnostic != nullptr);
+  DRAKE_DEMAND(data_source != nullptr);
+}
+
+DiagnosticDetail SDFormatDiagnostic::MakeDetail(
+    const sdf::Element& element, const std::string& message) const {
+  DiagnosticDetail detail;
+  if (!element.FilePath().empty()) {
+        detail.filename = element.FilePath();
+  } else {
+    if (data_source_->IsFilename()) {
+      detail.filename = data_source_->GetAbsolutePath();
+    } else {
+      detail.filename = data_source_->GetStem() + "." + file_extension_;
+    }
+  }
+  detail.line = element.LineNumber();
+  detail.message = message;
+  return detail;
+}
+
+void SDFormatDiagnostic::Warning(
+    sdf::ElementConstPtr element, const std::string& message) const {
+  diagnostic_->Warning(MakeDetail(*element, message));
+}
+
+void SDFormatDiagnostic::Error(
+    sdf::ElementConstPtr element, const std::string& message) const {
+  diagnostic_->Error(MakeDetail(*element, message));
+}
+
+DiagnosticPolicy SDFormatDiagnostic::MakePolicyForNode(
+    const sdf::Element& element) const {
+  DiagnosticPolicy result;
+  result.SetActionForWarnings(
+      [this, &element](const DiagnosticDetail& detail) {
+        diagnostic_->Warning(MakeDetail(element, detail.message));
+      });
+  result.SetActionForErrors(
+      [this, &element](const DiagnosticDetail& detail) {
+        diagnostic_->Error(MakeDetail(element, detail.message));
+      });
+  return result;
+}
+
+bool SDFormatDiagnostic::PropagateErrors(
+    const sdf::Errors& errors) const {
+  bool result = false;
+  for (const auto& e : errors) {
+    DiagnosticDetail detail;
+    detail.filename = e.FilePath();
+    detail.line = e.LineNumber();
+    if (e.XmlPath().has_value()) {
+      detail.message = fmt::format(
+          "At XML path {}: {}", e.XmlPath().value(), e.Message());
+    } else {
+      detail.message = e.Message();
+    }
+    if (IsError(e)) {
+      diagnostic_->Error(detail);
+      result = true;
+    } else {
+      diagnostic_->Warning(detail);
+    }
+  }
+  return result;
+}
+
+bool IsError(const sdf::Error& report) {
+  switch (report.Code()) {
+    case sdf::ErrorCode::ELEMENT_DEPRECATED:
+    case sdf::ErrorCode::VERSION_DEPRECATED:
+    case sdf::ErrorCode::NONE: {
+      return false;
+    }
+    default: {
+      return true;
+    }
+  }
+}
+
+bool PropagateErrors(
+    sdf::Errors&& input_errors,
+    sdf::Errors* output_errors) {
+  bool result = false;
+  for (auto& e : input_errors) {
+    if (IsError(e)) {
+      result = true;
+    }
+    output_errors->push_back(std::move(e));
+  }
+  return result;
+}
+
 void CheckSupportedElements(
-    const DiagnosticPolicy& diagnostic,
-    sdf::ElementPtr root_element,
+    const SDFormatDiagnostic& diagnostic,
+    sdf::ElementConstPtr root_element,
     const std::set<std::string>& supported_elements) {
   CheckSupportedElements(diagnostic, root_element.get(), supported_elements);
 }
 
 void CheckSupportedElements(
-    const DiagnosticPolicy& diagnostic,
+    const SDFormatDiagnostic& diagnostic,
     const sdf::Element* root_element,
     const std::set<std::string>& supported_elements) {
   DRAKE_DEMAND(root_element != nullptr);
 
-  sdf::ElementPtr element = root_element->GetFirstElement();
+  sdf::ElementConstPtr element = root_element->GetFirstElement();
   while (element) {
     const std::string& element_name = element->GetName();
     if ((supported_elements.find(element_name) == supported_elements.end()) &&
         element->GetExplicitlySetInFile()) {
-      internal::DiagnosticDetail detail;
-      if (!element->FilePath().empty()) {
-        detail.filename = element->FilePath();
-      }
-      detail.line = element->LineNumber();
       // Unsupported elements in the drake namespace are errors.
       if (element_name.find("drake:") == 0) {
-        detail.message =
+        std::string message =
             std::string("Unsupported SDFormat element in ") +
             root_element->GetName() + std::string(": ") + element_name;
-        diagnostic.Error(detail);
+        diagnostic.Error(element, std::move(message));
       } else {
-        detail.message =
+        std::string message =
             std::string("Ignoring unsupported SDFormat element in ") +
             root_element->GetName() + std::string(": ") + element_name;
-        diagnostic.Warning(detail);
+        diagnostic.Warning(element, std::move(message));
       }
     }
     element = element->GetNextElement();
@@ -50,8 +147,8 @@ void CheckSupportedElements(
 }
 
 void CheckSupportedElementValue(
-    const drake::internal::DiagnosticPolicy& diagnostic,
-    sdf::ElementPtr root_element,
+    const SDFormatDiagnostic& diagnostic,
+    sdf::ElementConstPtr root_element,
     const std::string& element_name,
     const std::string& expected) {
   DRAKE_DEMAND(root_element != nullptr);
@@ -60,22 +157,17 @@ void CheckSupportedElementValue(
     return;
   }
 
-  sdf::ElementPtr element = root_element->GetElement(element_name);
+  sdf::ElementConstPtr element = root_element->FindElement(element_name);
   if (!element->GetExplicitlySetInFile()) {
     return;
   }
 
   sdf::ParamPtr value = element->GetValue();
   if (value->GetAsString() != expected) {
-    internal::DiagnosticDetail detail;
-    if (!element->FilePath().empty()) {
-      detail.filename = element->FilePath();
-    }
-    detail.line = element->LineNumber();
-    detail.message =
-        std::string("Unsupported value for SDFormat element ") +
+    std::string message =
+      std::string("Unsupported value for SDFormat element ") +
         element->GetName() + std::string(": ") + value->GetAsString();
-    diagnostic.Warning(detail);
+    diagnostic.Warning(element, message);
   }
 }
 
--- multibody/parsing/detail_sdf_diagnostic.h
+++ multibody/parsing/detail_sdf_diagnostic.h
@@ -6,33 +6,96 @@
 #include <drake_vendor/sdf/Element.hh>
 
 #include "drake/common/diagnostic_policy.h"
+#include "drake/multibody/parsing/detail_common.h"
 
 namespace drake {
 namespace multibody {
 namespace internal {
 
-/// Checks that all child elements of @p root_element are in the set of @p
-/// supported_elements, and logs warnings/errors using @p diagnostic.
-/// Unsupported elements in the `drake:` namespace are errors, all others are
-/// warnings.  (see https://github.com/RobotLocomotion/drake/issues/16785 for
-/// some discussion of this rationale)
+// Helper class to format diagnostic messages for a SDFormat data source.
+class SDFormatDiagnostic {
+ public:
+  // Both @p diagnostic and @p data_source are aliased; their lifetime must
+  // exceed that of this object.  @p file_extension is only used to indicate
+  // the type of source when @p data_source is a literal string. It is copied
+  // internally, so places no restrictions on the lifetime of the passed
+  // parameter.
+  // @pre diagnostic cannot be nullptr.
+  // @pre data_source cannot be nullptr.
+  SDFormatDiagnostic(
+      const drake::internal::DiagnosticPolicy* diagnostic,
+      const DataSource* data_source,
+      const std::string& file_extension = "sdf");
+
+  // Issues a warning for an ElementConstPtr.
+  void Warning(const sdf::ElementConstPtr element,
+               const std::string& message) const;
+
+  // Issues an error for an ElementConstPtr.
+  void Error(const sdf::ElementConstPtr element,
+             const std::string& message) const;
+
+  // Make a temporary policy that can be passed to a node-unaware parsing
+  // function. The lifetime of this object, and the @p element should be
+  // greater than the lifetime of the returned policy.
+  drake::internal::DiagnosticPolicy MakePolicyForNode(
+      const sdf::Element& element) const;
+
+  // Warn about spec-documented elements ignored by Drake.
+  void WarnUnsupportedElement(const sdf::ElementConstPtr element,
+                              const std::string& tag) const;
+
+  // Warn about spec-documented attributes ignored by Drake.
+  void WarnUnsupportedAttribute(const sdf::ElementConstPtr element,
+                                const std::string& attribute) const;
+
+  // Copies all `errors` into this Diagnostic object.
+  // Returns true if there were any errors.
+  bool PropagateErrors(const sdf::Errors& errors) const;
+
+ private:
+  // Makes a diagnostic detail record based on an Element.
+  drake::internal::DiagnosticDetail MakeDetail(
+      const sdf::Element& element,
+      const std::string& message) const;
+
+  const drake::internal::DiagnosticPolicy* diagnostic_{};
+  const DataSource* data_source_{};
+  const std::string file_extension_;
+};
+
+// Checks that all child elements of @p root_element are in the set of @p
+// supported_elements, and logs warnings/errors using @p diagnostic.
+// Unsupported elements in the `drake:` namespace are errors, all others are
+// warnings.  (see https://github.com/RobotLocomotion/drake/issues/16785 for
+// some discussion of this rationale)
 void CheckSupportedElements(
-    const drake::internal::DiagnosticPolicy& diagnostic,
-    sdf::ElementPtr root_element,
+    const SDFormatDiagnostic& diagnostic,
+    sdf::ElementConstPtr root_element,
     const std::set<std::string>& supported_elements);
 
 void CheckSupportedElements(
-    const drake::internal::DiagnosticPolicy& diagnostic,
+    const SDFormatDiagnostic& diagnostic,
     const sdf::Element* root_element,
     const std::set<std::string>& supported_elements);
 
-/// Checks, for elements where there is only one supported value, that
-/// the element matches that value if it's present.
+// Checks, for elements where there is only one supported value, that
+// the element matches that value if it's present.
 void CheckSupportedElementValue(
-    const drake::internal::DiagnosticPolicy& diagnostic,
-    sdf::ElementPtr root_element,
+    const SDFormatDiagnostic& diagnostic,
+    sdf::ElementConstPtr root_element,
     const std::string& element_name,
     const std::string& expected);
+
+// Move-appends all `input_errors` onto `output_errors`.
+// Returns true if `input_errors` contains any error.
+bool PropagateErrors(
+    sdf::Errors&& input_errors,
+    sdf::Errors* output_errors);
+
+// Returns true iff the given report indicates an error, or false
+// for warnings.
+bool IsError(const sdf::Error& report);
 
 }  // namespace internal
 }  // namespace multibody
