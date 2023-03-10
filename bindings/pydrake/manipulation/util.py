df60bf94a412aba02f33e9518fd5af85c7c7cc01
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-03-10 12:30:17 -0800

    [py manipulation] Condense into a single module

---
+++ bindings/pydrake/manipulation/util.py
@@ -0,0 +1,12 @@
+"""Shim module that provides vestigial names for pydrake.manipulation.
+
+Prefer not to use this import path in new code; all of the code in
+this module can be imported from pydrake.manipulation directly.
+
+This module will be deprecated at some point in the future.
+"""
+
+from pydrake.manipulation import (
+    ApplyDriverConfig,
+    ZeroForceDriver,
+)
