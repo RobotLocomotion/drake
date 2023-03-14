196669e975f604a733afb4cb5568ed286486f579
xuchenhan-tri <xuchen.han@tri.global>
2023-03-14 11:00:42 -0700

    [multibody] Remove BlockSparseLinearOperator (#18986)
    BlockSparseLinearOperator is the LinearOperator version of
    BlockSparseMatrix. We anticipated that it might be useful when
    developing new contact solvers but it has since become unused.

--- multibody/contact_solvers/sap/test/sap_solver_test.cc
+++ multibody/contact_solvers/sap/test/sap_solver_test.cc
@@ -6,13 +6,11 @@
 
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
 #include "drake/common/test_utilities/expect_throws_message.h"
-#include "drake/multibody/contact_solvers/block_sparse_linear_operator.h"
 #include "drake/multibody/contact_solvers/block_sparse_matrix.h"
 #include "drake/multibody/contact_solvers/contact_solver_utils.h"
 #include "drake/multibody/contact_solvers/sap/sap_friction_cone_constraint.h"
 #include "drake/multibody/contact_solvers/sap/sap_solver_results.h"
 #include "drake/multibody/contact_solvers/supernodal_solver.h"
-#include "drake/multibody/contact_solvers/system_dynamics_data.h"
 #include "drake/systems/framework/context.h"
 
 using drake::systems::Context;
