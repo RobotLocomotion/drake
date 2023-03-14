3bb2a4d48cf5f42af01314ab6975c4549cf412cd
amice <amice@mit.edu>
2023-03-14 09:36:29 -0400

    tmp

--- geometry/optimization/dev/test/polynomial_positive_on_path_test.cc
+++ geometry/optimization/dev/test/polynomial_positive_on_path_test.cc
@@ -1,5 +1,6 @@
 #include "drake/geometry/optimization/dev/polynomial_positive_on_path.h"
 
+#include <iostream>
 #include <optional>
 
 #include <gtest/gtest.h>
@@ -10,38 +11,101 @@ namespace drake {
 namespace geometry {
 namespace optimization {
 
-GTEST_TEST(ParametrizedPolyPositive, DegreeZero) {
-  const symbolic::Variable mu{"mu"};
-  const symbolic::Variables parameters{};
-  const symbolic::
+using solvers::MathematicalProgram;
+using symbolic::Polynomial;
+using symbolic::Variable;
+using symbolic::Variables;
 
+GTEST_TEST(ParametrizedPolyPositive, CtorDegreeZero) {
+  const Variable interval_variable{"mu"};
+  const Variable coeff{"coeff"};
+  const Variables parameters{coeff};
+  // A polynomial with no indeterminates.
+  const Polynomial poly{coeff, symbolic::Variables()};
+
+  const ParametrizedPolynomialPositiveOnUnitInterval poly_prog{
+      poly, interval_variable, parameters};
+
+  const Polynomial lambda{poly_prog.get_lambda()};
+  const Polynomial nu{poly_prog.get_nu()};
+  const Polynomial p{poly_prog.get_p()};
+
+  EXPECT_EQ(p.TotalDegree(), 0);
+  EXPECT_TRUE(p.EqualTo(poly - lambda));
+  EXPECT_TRUE(nu.EqualTo(symbolic::Polynomial(0)));
+
+  const MathematicalProgram* psatz_variables_and_constraints =
+      poly_prog.get_psatz_variables_and_psd_constraints();
+
+  EXPECT_EQ(psatz_variables_and_constraints->indeterminates().size(), 1);
+  EXPECT_TRUE(psatz_variables_and_constraints->indeterminate(0).equal_to(
+      interval_variable));
+  for (const auto& var : p.decision_variables()) {
+    if (parameters.include(var)) {
+      // Parameters should not be in the decision variables.
+      EXPECT_THROW(
+          unused(
+              psatz_variables_and_constraints->FindDecisionVariableIndex(var)),
+          std::runtime_error);
+    } else {
+      // Ensure that all the decision variables of p are in the program.
+      EXPECT_NO_THROW(unused(
+          psatz_variables_and_constraints->FindDecisionVariableIndex(var)));
+    }
+  }
+}
+
+GTEST_TEST(ParametrizedPolyPositive, CtorScalarDegreeOdd) {
+  const int deg{3};
+  const Variable interval_variable{"mu"};
+  const Variable coeff{"coeff"};
+  const Variables parameters{coeff};
+  // A polynomial with no indeterminates.
+  const Polynomial poly{coeff, symbolic::Variables()};
+
+  const ParametrizedPolynomialPositiveOnUnitInterval poly_prog{
+      poly, interval_variable, parameters};
+
+  const Polynomial lambda{poly_prog.get_lambda()};
+  const Polynomial nu{poly_prog.get_nu()};
+  const Polynomial p{poly_prog.get_p()};
+
+  EXPECT_EQ(p.TotalDegree(), 0);
+  EXPECT_TRUE(p.EqualTo(poly - lambda));
+  EXPECT_TRUE(nu.EqualTo(symbolic::Polynomial(0)));
+
+  const MathematicalProgram* psatz_variables_and_constraints =
+      poly_prog.get_psatz_variables_and_psd_constraints();
+
+  EXPECT_EQ(psatz_variables_and_constraints->indeterminates().size(), 1);
+  EXPECT_TRUE(psatz_variables_and_constraints->indeterminate(0).equal_to(
+      interval_variable));
+  for (const auto& var : p.decision_variables()) {
+    if (parameters.include(var)) {
+      // Parameters should not be in the decision variables.
+      EXPECT_THROW(
+          unused(
+              psatz_variables_and_constraints->FindDecisionVariableIndex(var)),
+          std::runtime_error);
+    } else {
+      // Ensure that all the decision variables of p are in the program.
+      EXPECT_NO_THROW(unused(
+          psatz_variables_and_constraints->FindDecisionVariableIndex(var)));
+    }
+  }
 
 }
 
-GTEST_TEST(ParametrizedPolyPositive, ScalarDegreeOdd) {
+GTEST_TEST(ParametrizedPolyPositive, CtorScalarDegreeEven) {}
 
-}
+GTEST_TEST(ParametrizedPolyPositive, CtorMatrix2DegreeOdd) {}
 
-GTEST_TEST(ParametrizedPolyPositive, ScalarDegreeEven) {
+GTEST_TEST(ParametrizedPolyPositive, CtorMatrix2DegreeEven) {}
 
-}
+GTEST_TEST(ParametrizedPolyPositive, CtorMatrix3DegreeOdd) {}
 
-GTEST_TEST(ParametrizedPolyPositive, Matrix2DegreeOdd) {
-
-}
-
-GTEST_TEST(ParametrizedPolyPositive, Matrix2DegreeEven) {
-
-}
-
-GTEST_TEST(ParametrizedPolyPositive, Matrix3DegreeOdd) {
-
-}
-
-GTEST_TEST(ParametrizedPolyPositive, Matrix3DegreeEven) {
-
-}
+GTEST_TEST(ParametrizedPolyPositive, CtorMatrix3DegreeEven) {}
 
 }  // namespace optimization
 }  // namespace geometry
-}  // namespace drakes
+}  // namespace drake
