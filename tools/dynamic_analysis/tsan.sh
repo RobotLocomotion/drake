b41878e5a5812b10b3c18808723ca61541857d15
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-03-27 09:20:57 -0700

    [setup] Upgrade sanitizer builds to clang-14 on Ubuntu

--- tools/dynamic_analysis/tsan.sh
+++ tools/dynamic_analysis/tsan.sh
@@ -3,5 +3,5 @@ me=$(python3 -c 'import os; print(os.path.realpath("'"$0"'"))')
 mydir=$(dirname "$me")
 export TSAN_OPTIONS="$TSAN_OPTIONS:detect_deadlocks=1:second_deadlock_stack=1:strip_path_prefix=/proc/self/cwd/:suppressions=$mydir/tsan.supp"
 # Ensure executable named llvm-symbolizer is on the PATH.
-export PATH="$PATH:/usr/lib/llvm-12/bin:/usr/local/opt/llvm/bin"
+export PATH="$PATH:/usr/lib/llvm-14/bin:/usr/lib/llvm-12/bin:/usr/local/opt/llvm/bin"
 "$@"
