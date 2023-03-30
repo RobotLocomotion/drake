12f80fc1779e598af4c86abd1c937f7968f9f246
Alexandre Amice <43739209+AlexandreAmice@users.noreply.github.com>
2023-03-30 12:06:22 -0600

    [optimization] [dev] Add polynomial_positive_on_path module (#18995)
    Also perform minor cleanups on cspace_free_path.

---
+++ geometry/optimization/dev/polynomial_positive_on_path.cc
@@ -0,0 +1,119 @@
+#include "drake/geometry/optimization/dev/polynomial_positive_on_path.h"
+
+#include <limits>
+#include <utility>
+
+#include "drake/common/symbolic/monomial_util.h"
+namespace drake {
+namespace geometry {
+namespace optimization {
+
+ParametrizedPolynomialPositiveOnUnitInterval::
+    ParametrizedPolynomialPositiveOnUnitInterval(
+        const symbolic::Polynomial& poly,
+        const symbolic::Variable& interval_variable,
+        const symbolic::Variables& parameters)
+    : mu_(interval_variable),
+      p_(poly),
+      parameters_(parameters),
+      psatz_variables_and_psd_constraints_() {
+  psatz_variables_and_psd_constraints_.AddIndeterminates(poly.indeterminates());
+  for (const auto& var : poly.decision_variables()) {
+    // Add the decision variables of poly which are not parameters.
+    if (!parameters_.include(var)) {
+      psatz_variables_and_psd_constraints_.AddDecisionVariables(
+          solvers::VectorDecisionVariable<1>(var));
+    }
+  }
+
+  const int deg = poly.Degree(mu_);
+  if (poly.TotalDegree() == 0) {
+    // If poly is of degree 0, then it is a scalar, and we just need to
+    // constraint that p_ >= 0.
+    const solvers::VectorXDecisionVariable lambda{
+        psatz_variables_and_psd_constraints_.NewContinuousVariables(1, "Sl")};
+    psatz_variables_and_psd_constraints_.AddBoundingBoxConstraint(
+        0, std::numeric_limits<double>::infinity(), lambda);
+    lambda_ = symbolic::Polynomial{lambda(0), symbolic::Variables()};
+    p_ -= lambda(0);
+  } else {
+    const int d = static_cast<int>(std::floor(deg / 2));
+    const solvers::MathematicalProgram::NonnegativePolynomial type =
+        solvers::MathematicalProgram::NonnegativePolynomial::kSos;
+
+    // This basis is [μᵈ, ... μ, 1, y₁, ..., yₙ]
+    const int num_y_indets =
+        poly.indeterminates().size() - (poly.Degree(mu_) > 0 ? 1 : 0);
+
+    VectorX<symbolic::Monomial> multiplier_basis_d{d + 1 + num_y_indets};
+
+    multiplier_basis_d.head(d + 1) = symbolic::MonomialBasis({mu_}, d);
+    int i = d + 1;
+    for (const auto& var : poly.indeterminates()) {
+      if (!var.equal_to(mu_)) {
+        DRAKE_DEMAND(poly.Degree(var) <= 2);
+        multiplier_basis_d(i) = symbolic::Monomial(var);
+        ++i;
+      }
+    }
+
+    // Constructs the multiplier polynomials and their associated Gram matrices
+    // as well as the polynomial p_. Recall that p_ has already been initialized
+    // to poly(μ,y).
+    auto [lambda, Q_lambda] =
+        psatz_variables_and_psd_constraints_.NewSosPolynomial(
+            multiplier_basis_d, type, "Sl");
+    lambda_ = std::move(lambda);
+    if (deg == 0) {
+      p_ -= lambda_;  // interval variable doesn't exist in the program, so we
+                      // can ignore it.
+    } else if (deg % 2 == 0) {
+      auto [nu, Q_nu] = psatz_variables_and_psd_constraints_.NewSosPolynomial(
+          multiplier_basis_d.tail(multiplier_basis_d.size() -
+                                  1),  // exclude μᵈ monomial
+          type, "Sv");
+      nu_ = std::move(nu);
+      p_ -= lambda_ + nu_ * mu_ * (symbolic::Polynomial(1, {mu_}) - mu_);
+    } else {
+      auto [nu, Q_nu] = psatz_variables_and_psd_constraints_.NewSosPolynomial(
+          multiplier_basis_d, type, "Sv");
+      nu_ = std::move(nu);
+      p_ -= lambda_ * mu_ + nu_ * (symbolic::Polynomial(1, {mu_}) - mu_);
+    }
+  }
+}
+
+void ParametrizedPolynomialPositiveOnUnitInterval::
+    AddPositivityConstraintToProgram(const symbolic::Environment& env,
+                                     solvers::MathematicalProgram* prog) const {
+  DRAKE_DEMAND(env.size() == parameters_.size());
+  for (const auto& parameter : parameters_) {
+    DRAKE_DEMAND(env.find(parameter) != env.cend());
+  }
+  for (int i = 0;
+       i < psatz_variables_and_psd_constraints_.indeterminates().size(); ++i) {
+    // Check that prog contains the indeterminates of this program.
+    DRAKE_DEMAND(
+        prog->indeterminates_index().count(
+            psatz_variables_and_psd_constraints_.indeterminates()(i).get_id()) >
+        0);
+  }
+
+  prog->AddDecisionVariables(
+      psatz_variables_and_psd_constraints_.decision_variables());
+  for (const auto& binding :
+       psatz_variables_and_psd_constraints_.GetAllConstraints()) {
+    prog->AddConstraint(binding);
+  }
+  // Add the p_ == 0 constraint after evaluation. Do this manually to avoid a
+  // call to Reparse that occurs in AddEqualityConstraintBetweenPolynomials.
+  const symbolic::Polynomial p_evaled{p_.EvaluatePartial(env)};
+
+  for (const auto& item : p_evaled.monomial_to_coefficient_map()) {
+    prog->AddLinearEqualityConstraint(item.second, 0);
+  }
+}
+
+}  // namespace optimization
+}  // namespace geometry
+}  // namespace drake
---
+++ geometry/optimization/dev/polynomial_positive_on_path.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <optional>
+
+#include "drake/solvers/mathematical_program.h"
+
+namespace drake {
+namespace geometry {
+namespace optimization {
+
+/**
+ * Certifying a path as collision free will require certifying that many matrix
+ * SOS conditions [1;y]ᵀP(μ;s)[1;y] ≥ 0 for μ ∈ [0,1] where the s is a
+ * multivariate parameter which must be evaluated before the optimization
+ * program is solved. This class contains the information for adding the
+ * constraint that a polynomial be positive on the unit interval. The polynomial
+ * is parametrized as some of its indeterminates needing to be evaluated before
+ * being added to the program.
+ *
+ * @param poly The parametrized polynomial which we will enforce positivity of.
+ * This polynomial can be arbitrary degree in the interval variable μ and must
+ * be quadratic in all other indeterminates y.
+ * @param interval_variable The variable μ associated to the unit interval.
+ * @param parameters The parameters which must be evaluated before enforcing the
+ * positivity of poly. This is a subset of the polynomial's decision variables.
+ */
+class ParametrizedPolynomialPositiveOnUnitInterval {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(ParametrizedPolynomialPositiveOnUnitInterval)
+
+  ParametrizedPolynomialPositiveOnUnitInterval(
+      const symbolic::Polynomial& poly,
+      const symbolic::Variable& interval_variable,
+      const symbolic::Variables& parameters);
+
+  // Add the constraint that this parametrized polynomial is positive on the
+  // unit interval. The Environment env must contain an evaluation for all the
+  // parameters in parameters_. The MathematicalProgram prog must already
+  // contain the indeterminates of psatz_variables_and_psd_constraints_.
+  void AddPositivityConstraintToProgram(
+      const symbolic::Environment& env,
+      solvers::MathematicalProgram* prog) const;
+
+  const symbolic::Variable& get_mu() const { return mu_; }
+  const symbolic::Polynomial& get_p() const { return p_; }
+  const symbolic::Polynomial& get_lambda() const { return lambda_; }
+  const symbolic::Polynomial& get_nu() const { return nu_; }
+  const symbolic::Variables& get_parameters() const { return parameters_; }
+
+  const solvers::MathematicalProgram& get_psatz_variables_and_psd_constraints()
+      const {
+    return psatz_variables_and_psd_constraints_;
+  }
+
+ private:
+  // TODO(Alexandre.Amice) make all members const.
+  // The variable parametrizing the path. We will enforce that a certain
+  // polynomial is positive for μ ∈ [0,1].
+  const symbolic::Variable mu_;
+
+  // A polynomial q(μ,y) = [1;y]ᵀP(μ;s)[1;y] (where μ is univariate and y is
+  // multivariate) is positive on the interval μ ∈ [0,1] if and only if there
+  // exists biforms λ(μ,y) = ∑ ϕᵢ²(μ,y) and ν(μ,y) = ∑ ψᵢ²(μ,y) such that
+  // q(μ,y) = λ(μ,y) + ν(μ,y)*μ*(1-μ) if deg(q, μ) = 2d or q(μ,y) = λ(μ,y)*μ +
+  // ν(μ,y)*(1-μ) if deg(q, μ) = 2d + 1. Moreover, in both cases deg(ϕᵢ, μ) ≤ d,
+  // deg(ϕᵢ, y) = 1, and and deg(ψᵢ, y) = 1. In the first case
+  // deg(ψᵢ, μ) ≤ d - 1, and in the second deg(ψᵢ, μ) ≤ d. If deg(poly,μ) > 0 we
+  // construct the polynomial p_(μ,y) = poly(μ,y)-q(μ,y) which we will later
+  // constrain to be equal to 0. If deg(poly,μ) = 0, then q is either
+  // constrained to be a positive scalar if it has no other indeterminates, or
+  // it is constrained to be a sum-of-squares if it is a quadratic form in y.
+  symbolic::Polynomial p_;
+
+  // The subset of the decision variables in p_ which must be evaluated before
+  // we enforce it's positivity in a Mathematical Program.
+  const symbolic::Variables parameters_;
+
+  // The λ(μ,y) in the documentation of p_
+  symbolic::Polynomial lambda_{0};
+  // The ν(μ,y) in the documentation of p_
+  symbolic::Polynomial nu_{0};
+
+  // A program which stores the psd variables and constraints associated to λ
+  // and ν. See the description of p_. The use of this member is idiosyncratic,
+  // so it is not recommended to provide an accessor to it.
+  solvers::MathematicalProgram psatz_variables_and_psd_constraints_;
+};
+
+}  // namespace optimization
+}  // namespace geometry
+}  // namespace drake
