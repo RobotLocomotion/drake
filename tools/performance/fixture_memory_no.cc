e5411da3f88f9c868dc65d970ebd0187a43e2b76
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-04-05 07:38:08 -0700

    [performance] Add memory statistics to benchmarks by default

---
+++ tools/performance/fixture_memory_no.cc
@@ -0,0 +1,13 @@
+#include "drake/tools/performance/fixture_memory.h"
+
+namespace drake {
+namespace tools {
+namespace performance {
+
+void EnableMemoryManager() {}
+
+void TareMemoryManager() {}
+
+}  // namespace performance
+}  // namespace tools
+}  // namespace drake
