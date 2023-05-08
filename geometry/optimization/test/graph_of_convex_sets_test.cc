cb6bb54e9946cd91d400086edf6168f9588decf2
Sean Curtis <sean.curtis@tri.global>
2023-05-08 14:31:23 -0700

    Make the test more direct (if incomplete)

--- geometry/optimization/test/graph_of_convex_sets_test.cc
+++ geometry/optimization/test/graph_of_convex_sets_test.cc
@@ -1256,14 +1256,28 @@ TEST_F(PreprocessShortestPathTest, CheckResults) {
   }
 }
 
-// Confirm that the preprocessor uses the solver passed in through the options
-// structure.
-TEST_F(PreprocessShortestPathTest, Solver) {
-  // Intentionally choose a solver that cannot run the preprocessing.
+// PreprocessShortestPath() is supposed to consume the solver options. More
+// particularly, those options must be passed to it from SolveShortestPath().
+// We'll exploit friend access to confirm PreporocessShortestPath() makes use
+// of the options. Confirming the options are *passed* is trickier. This is
+// because both PreprocessShortestPath() and SolveShortestPath() invoke Solve
+// with the same options. It is difficult to pass in a set of options such that
+// we can discen the exercise in PreprocessShortestPath strictly from looking
+// at the result. For example, passing an incompatible solver (as we do below)
+// will throw an exception no matter what, even if SolveShortestPath() skips
+// calling PreprocessShortestPath(). So, for now, we'll directly test that
+// PreprocessShortestPath() depends on the options and leave the confirmation of
+// SolveShortestPath() correctly passing those options as a future exercise.
+TEST_F(PreprocessShortestPathTest, DependsOnOptions) {
+  // Intentionally choose a solver that cannot run the preprocessing. Throwing
+  // an exception is proof that the function relied on the options.
   solvers::LinearSystemSolver solver;
   options_.solver = &solver;
-  DRAKE_EXPECT_THROWS_MESSAGE(g_.SolveShortestPath(vid_[0], vid_[5], options_),
+  DRAKE_EXPECT_THROWS_MESSAGE(PreprocessShortestPath(vid_[0], vid_[5]),
                               ".*LinearSystemSolver is unable to solve.*");
+
+  // TODO(SeanCurtis-TRI): Figure out a way to tell that SolveShortestPath
+  // invokes PreporcessShortestPath with the given options as documented above.
 }
 
 /* This test rounds the shortest path on a graph with two paths around an
