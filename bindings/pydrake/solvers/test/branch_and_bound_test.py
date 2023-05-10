ec2ee07bae462a5ef64fba1a12361e691c33f17f
Hongkai Dai <daihongkai@gmail.com>
2023-05-10 23:24:39 +0200

    [solvers] Add max_explored_nodes for branch_and_bound. Early terminate the branch and bound process if the explored nodes in the tree exceeds this number. (#19366)

--- bindings/pydrake/solvers/test/branch_and_bound_test.py
+++ bindings/pydrake/solvers/test/branch_and_bound_test.py
@@ -21,11 +21,18 @@ class TestMixedIntegerBranchAndBound(unittest.TestCase):
         prog.AddLinearConstraint(b[1] + 1.2 * x[1] - b[0] <= 5)
         prog.AddQuadraticCost(x[0] * x[0])
 
-        dut = MixedIntegerBranchAndBound(prog, OsqpSolver().solver_id())
-        solution_result = dut.Solve()
+        dut1 = MixedIntegerBranchAndBound(prog, OsqpSolver().solver_id())
+        solution_result = dut1.Solve()
         self.assertEqual(solution_result, SolutionResult.kSolutionFound)
-        self.assertAlmostEqual(dut.GetOptimalCost(), 1.)
-        self.assertAlmostEqual(dut.GetSubOptimalCost(0), 1.)
-        self.assertAlmostEqual(dut.GetSolution(x[0], 0), 1.)
-        self.assertAlmostEqual(dut.GetSolution(x[0], 1), 1.)
-        np.testing.assert_allclose(dut.GetSolution(x, 0), [1., 0.], atol=1e-12)
+        self.assertAlmostEqual(dut1.GetOptimalCost(), 1.)
+        self.assertAlmostEqual(dut1.GetSubOptimalCost(0), 1.)
+        self.assertAlmostEqual(dut1.GetSolution(x[0], 0), 1.)
+        self.assertAlmostEqual(dut1.GetSolution(x[0], 1), 1.)
+        np.testing.assert_allclose(
+            dut1.GetSolution(x, 0), [1., 0.], atol=1e-12)
+
+        options = MixedIntegerBranchAndBound.Options()
+        options.max_explored_nodes = 1
+        dut2 = MixedIntegerBranchAndBound(
+            prog=prog, solver_id=OsqpSolver().solver_id(), options=options)
+        solution_result = dut2.Solve()
