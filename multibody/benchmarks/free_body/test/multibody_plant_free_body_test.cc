58bb3695f5c8d89d9b6f3ca80c8946198c693c8d
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-05-15 13:20:43 -0700

    [multibody] Bump tolerance for free_body_test (#19400)
    On Mac Ventura Arm, this comparison was slightly beyond tolerance.

--- multibody/benchmarks/free_body/test/multibody_plant_free_body_test.cc
+++ multibody/benchmarks/free_body/test/multibody_plant_free_body_test.cc
@@ -442,7 +442,7 @@ void TestKaneExactSolution(FreeBody torque_free_cylinder_kane,
 
   // Ensure Kane's quaternion time-derivative matches expected angular velocity.
   EXPECT_TRUE(math::IsQuaternionAndQuaternionDtEqualAngularVelocityExpressedInB(
-    quat_kane, quatDt_kane, w_expected, 8 * kEpsilon));
+    quat_kane, quatDt_kane, w_expected, 12 * kEpsilon));
 
   // Ensure expected quaternion time-derivative matches Kane's angular velocity.
   EXPECT_TRUE(math::IsQuaternionAndQuaternionDtEqualAngularVelocityExpressedInB(
