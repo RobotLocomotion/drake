90e5f55387cdf02ae26ce6867f1c88811f391796
Rick Poyner <rick.poyner@tri.global>
2023-05-19 18:49:41 -0400

    wip: more reorg; time to start contemplating some tests

--- tmp/inertia_fixer.cc
+++ tmp/inertia_fixer.cc
@@ -23,6 +23,16 @@ using tinyxml2::XMLPrinter;
 
 // XXX Does tinyxml2 quote-swapping ('' to "") break any strings?
 
+// Nope: it does entity substitution. So things may look worse in source, but
+// information will be preserved.
+/*
+For Input:
+  <material name='silver-"ish"'>
+
+We get Output:
+  <material name="silver-&quot;ish&quot;">
+*/
+
 // We jump through hoops here, just to get 2-space indented output.
 class XmlPrinter final : public XMLPrinter {
  public:
@@ -34,6 +44,7 @@ class XmlPrinter final : public XMLPrinter {
   }
 };
 
+// Create a child element if it does not already exist.
 XMLElement* EnsureChildElement(XMLElement* el, const char* child) {
   XMLElement* kid = el->FirstChildElement(child);
   if (!kid) {
@@ -42,10 +53,13 @@ XMLElement* EnsureChildElement(XMLElement* el, const char* child) {
   return kid;
 }
 
+// Format a double in shortest round-trip representation.
 std::string roundtrip(double x) {
   return fmt::format("{}", x);
 }
 
+// Recursively find all of the descendant elements of 'el` named "link", and
+// accumulate them in an externally supplied vector.
 void FindLinks(XMLElement* el, std::vector<XMLElement*>* links) {
   if (std::string(el->Name()) == "link") {
     links->push_back(el);
@@ -58,97 +72,44 @@ void FindLinks(XMLElement* el, std::vector<XMLElement*>* links) {
   }
 }
 
-class InertiaProcessor {
+
+class XmlInertiaMapper {
  public:
-  InertiaProcessor(
+  using Mapping = std::unordered_map<BodyIndex, XMLElement*>;
+  virtual ~XmlInertiaMapper() {}
+  virtual const Mapping& mapping() const = 0;
+  virtual void UpdateInertia(XMLElement*, const SpatialInertia<double>&) = 0;
+};
+
+class UrdfInertiaMapper final : public XmlInertiaMapper {
+ public:
+  UrdfInertiaMapper(
       const MultibodyPlant<double>& plant,
-      const SceneGraph<double>& scene_graph,
+      const std::vector<ModelInstanceIndex>& models,
       XMLDocument* doc)
-      : plant_(plant), scene_graph_(scene_graph), doc_(doc) {
-    XMLElement* root = doc->RootElement();
-    std::string root_name(root->Name());
-    if (root_name == "sdf") {
-      update_inertia_ =
-          std::bind(&InertiaProcessor::UpdateInertiaSdf, this,
-                    std::placeholders::_1, std::placeholders::_2);
-    } else if (root_name == "robot") {
-      update_inertia_ =
-          std::bind(&InertiaProcessor::UpdateInertiaUrdf, this,
-                    std::placeholders::_1, std::placeholders::_2);
-    } else {
-      drake::log()->error("Unknown file type: root element was {}", root_name);
-      ::exit(EXIT_FAILURE);
+      : plant_(plant) {
+    DRAKE_ASSERT(models.size() == 1);
+    std::vector<XMLElement*> links;
+    FindLinks(doc->RootElement(), &links);
+    DRAKE_ASSERT(drake::ssize(links) == plant_.num_bodies() - 1);
+    for (int k = 1; k < plant_.num_bodies(); ++k) {
+      const BodyIndex body_index{k};
+      mapping_[body_index] = links[k - 1];
+      if (kDrakeAssertIsArmed) {
+        const auto& body_name = plant_.get_body(body_index).name();
+        const XMLElement* link = links[k - 1];
+        const char* name = link->Attribute("name");
+        DRAKE_ASSERT(name != nullptr);
+        DRAKE_ASSERT(std::string(name) == body_name);
+      }
     }
-
-    // Crawl through the document, finding all of the 'link' elements,
-    // regardless of their path back to the root. We can interrogate their
-    // parents and deduce model names to ensure unique mappings.
-    FindLinks(root, &links_);
   }
 
-  std::optional<SpatialInertia<double>> MaybeFixBodyInertia(
-      BodyIndex body_index) {
-    // * Leverage the maybe-damaged model that got parsed by the parser:
-    //   * for geometries
-    //   * for mass/density parameters
-    // * Build proper inertias from geometries:
-    //   * cf. mujoco parser's techniques
-    //   * figure out how to combine inertias for multiple geometries?
-    const auto maybe_frame_id = plant_.GetBodyFrameIdIfExists(body_index);
-    if (!maybe_frame_id.has_value()) {
-      return {};
-    }
-    const auto& body_name = plant_.get_body(body_index).name();
-    const auto& rigid_body = plant_.GetRigidBodyByName(body_name);
-    const auto& old_inertia = rigid_body.default_spatial_inertia();
-    if (FLAGS_invalid_only && old_inertia.IsPhysicallyValid()) {
-      return {};
-    }
-    const double mass = old_inertia.get_mass();
-    const auto& inspector = scene_graph_.model_inspector();
-    const auto geoms = inspector.GetGeometries(*maybe_frame_id,
-                                               Role::kProximity);
-    if (geoms.empty()) {
-      // XXX look at visuals instead??
-      return {};
-    }
-    SpatialInertia<double> M_BBo_B(0, {0, 0, 0}, {0, 0, 0});
-    for (const auto& geom : geoms) {
-      const auto M_GG_G_one = CalcSpatialInertia(inspector.GetShape(geom), 1.0);
-      SpatialInertia<double> M_GG_G(mass, M_GG_G_one.get_com(),
-                                    M_GG_G_one.get_unit_inertia());
-      const auto& X_BG = inspector.GetPoseInFrame(geom);
-      const auto M_GBo_B = M_GG_G.ReExpress(X_BG.rotation())
-                           .Shift(-X_BG.translation());
-      // XXX presumably this accumulates CoM. How do we accumulate/understand
-      // the inertia frame rotation?
-      M_BBo_B += M_GBo_B;
-    }
-    return M_BBo_B;
-  }
+  const Mapping& mapping() const final { return mapping_; }
 
-  void UpdateInertiaXml(
-      BodyIndex body_index,
-      const SpatialInertia<double>& inertia) {
-    // * Edit fixed-up inertias back into doc.
-    update_inertia_(body_index, inertia);
-  }
-
- private:
-  void UpdateInertiaUrdf(
-      BodyIndex body_index,
-      const SpatialInertia<double>& inertia) {
-    const auto& body_name = plant_.get_body(body_index).name();
-    XMLElement* found_link{};
-    for (XMLElement* link : links_) {
-      const char* name = link->Attribute("name");
-      if (!name) { continue; }
-      if (std::string(name) != body_name) { continue; }
-      found_link = link;
-      break;
-    }
-    DRAKE_DEMAND(found_link != nullptr);  // Named element was found.
-    XMLElement* inertial = EnsureChildElement(found_link, "inertial");
+  void UpdateInertia(XMLElement* el,
+                     const SpatialInertia<double>& inertia) final {
+    XMLElement* inertial = EnsureChildElement(el, "inertial");
     XMLElement* origin = EnsureChildElement(inertial, "origin");
     origin->SetAttribute("xyz", "0 0 0");
     origin->SetAttribute("rpy", "0 0 0");
@@ -166,34 +127,46 @@ class InertiaProcessor {
     coeffs->SetAttribute("iyz", roundtrip(prod(2)).c_str());
   }
 
-  void UpdateInertiaSdf(
-      BodyIndex body_index,
-      const SpatialInertia<double>& inertia) {
-    // We may well be limited to a naive implementation of "write back to sdf"
-    // here. Because sdf has nested models and (multiple kinds of?) inclusion,
-    // it may be arbitrarily difficult to complete the job of writing back all
-    // of the inertias in a multi-file model. It's probably possible to fix the
-    // inertias in the primary document we loaded, but that's it.
-    const auto& body_name = plant_.get_body(body_index).name();
-    XMLElement* found_link{};
-    for (XMLElement* link : links_) {
-      // Find the one we want.
-      const char* name = link->Attribute("name");
-      if (!name) { continue; }
-      if (std::string(name) != body_name) { continue; }
-      // XXX use model instance names if needed. Maybe all this name-mapping
-      // gets done up-front?
-      found_link = link;
-      break;
+ private:
+  const MultibodyPlant<double>& plant_;
+  Mapping mapping_;
+};
+
+
+class SdfInertiaMapper final : public XmlInertiaMapper {
+ public:
+  SdfInertiaMapper(
+      const MultibodyPlant<double>& plant,
+      const std::vector<ModelInstanceIndex>& models,
+      XMLDocument* doc)
+      : plant_(plant) {
+    DRAKE_ASSERT(models.size() >= 1);
+    std::vector<XMLElement*> links;
+    FindLinks(doc->RootElement(), &links);
+    // XXX This assumption is likely violated by files that use include tags.
+    DRAKE_ASSERT(drake::ssize(links) == plant_.num_bodies() - 1);
+    for (int k = 1; k < plant_.num_bodies(); ++k) {
+      const BodyIndex body_index{k};
+      mapping_[body_index] = links[k - 1];
+      if (kDrakeAssertIsArmed) {
+        const auto& body_name = plant_.get_body(body_index).name();
+        const XMLElement* link = links[k - 1];
+        const char* name = link->Attribute("name");
+        DRAKE_ASSERT(name != nullptr);
+        DRAKE_ASSERT(std::string(name) == body_name);
+      }
     }
-    DRAKE_DEMAND(found_link != nullptr);
+  }
+  const Mapping& mapping() const final { return mapping_; }
+  void UpdateInertia(XMLElement* el,
+                     const SpatialInertia<double>& inertia) final {
     // Fill out the inertial properties.
-    XMLElement* inertial = EnsureChildElement(found_link, "inertial");
+    XMLElement* inertial = EnsureChildElement(el, "inertial");
     XMLElement* pose = EnsureChildElement(inertial, "pose");
     pose->SetText("0 0 0 0 0 0");
     auto write_child = [&](XMLElement* parent, const char* name, double value) {
-      XMLElement* el = EnsureChildElement(parent, name);
-      el->SetText(roundtrip(value).c_str());
+      XMLElement* kid = EnsureChildElement(parent, name);
+      kid->SetText(roundtrip(value).c_str());
     };
     write_child(inertial, "mass", inertia.get_mass());
     XMLElement* coeffs = EnsureChildElement(inertial, "inertia");
@@ -208,15 +181,91 @@ class InertiaProcessor {
     write_child(coeffs, "iyz", prod(2));
   }
 
+ private:
+  const MultibodyPlant<double>& plant_;
+  Mapping mapping_;
+};
+
+
+class InertiaProcessor {
+ public:
+  InertiaProcessor(
+      const MultibodyPlant<double>& plant,
+      const SceneGraph<double>& scene_graph,
+      const std::vector<ModelInstanceIndex>& models,
+      XMLDocument* doc)
+      : plant_(plant), scene_graph_(scene_graph) {
+    XMLElement* root = doc->RootElement();
+    std::string root_name(root->Name());
+    if (root_name == "sdf") {
+      mapper_ = std::make_unique<SdfInertiaMapper>(plant, models, doc);
+    } else if (root_name == "robot") {
+      mapper_ = std::make_unique<UrdfInertiaMapper>(plant, models, doc);
+    } else {
+      drake::log()->error("Unknown file type: root element was {}", root_name);
+      ::exit(EXIT_FAILURE);
+    }
+  }
+
+  void Process() {
+    for (const auto& pair : mapper_->mapping()) {
+      const auto& [body_index, el] = pair;
+      const auto maybe_inertia = MaybeFixBodyInertia(body_index);
+      if (maybe_inertia.has_value()) {
+        mapper_->UpdateInertia(el, *maybe_inertia);
+      }
+    }
+  }
+
+ private:
+  std::optional<SpatialInertia<double>> MaybeFixBodyInertia(
+      BodyIndex body_index) {
+    const auto& body = plant_.get_body(body_index);
+    const auto* rigid_body = dynamic_cast<const RigidBody<double>*>(&body);
+    if (!rigid_body) {
+      // Only rigid bodies have constant inertia, for which model file fixups
+      // make sense.
+      return {};
+    }
+    const auto maybe_frame_id = plant_.GetBodyFrameIdIfExists(body_index);
+    if (!maybe_frame_id.has_value()) {
+      // No geometry to fix inertia from.
+      return {};
+    }
+    const auto& old_inertia = rigid_body->default_spatial_inertia();
+    if (FLAGS_invalid_only && old_inertia.IsPhysicallyValid()) {
+      // Skip valid inertias by user preference.
+      return {};
+    }
+    const auto& inspector = scene_graph_.model_inspector();
+    const auto geoms = inspector.GetGeometries(*maybe_frame_id,
+                                               Role::kProximity);
+    if (geoms.empty()) {
+      // No geometry to fix inertia from.
+      return {};
+    }
+    SpatialInertia<double> M_BBo_B(0, {0, 0, 0}, {0, 0, 0});
+    // XXX mass calculations are all wrong for the case of multiple geometries.
+    const double mass = old_inertia.get_mass();
+    for (const auto& geom : geoms) {
+      const auto M_GG_G_one = CalcSpatialInertia(inspector.GetShape(geom), 1.0);
+      SpatialInertia<double> M_GG_G(mass, M_GG_G_one.get_com(),
+                                    M_GG_G_one.get_unit_inertia());
+      const auto& X_BG = inspector.GetPoseInFrame(geom);
+      const auto M_GBo_B = M_GG_G.ReExpress(X_BG.rotation())
+                           .Shift(-X_BG.translation());
+      M_BBo_B += M_GBo_B;
+    }
+    return M_BBo_B;
+  }
+
   const MultibodyPlant<double>& plant_;
   const SceneGraph<double>& scene_graph_;
-  XMLDocument* doc_{};
-  std::vector<XMLElement*> links_;
-  std::function<void(BodyIndex, const SpatialInertia<double>&)> update_inertia_;
+  std::unique_ptr<XmlInertiaMapper> mapper_;
 };
 
 int do_main(int argc, char* argv[]) {
-  gflags::SetUsageMessage("[INPUT-FILE-OR-URL]\n"
+  gflags::SetUsageMessage("[INPUT-FILE] [OUTPUT-FILE]\n"
                           "Rewrite URDF/SDFormat with fixed-up inertias");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   if (argc < 2) {
@@ -240,15 +289,20 @@ int do_main(int argc, char* argv[]) {
   Parser parser{&plant};
   parser.package_map().PopulateFromRosPackagePath();
 
-  parser.AddModels(argv[1]);
-
-  InertiaProcessor processor(plant, scene_graph, &xml_doc);
-  for (BodyIndex k(1); k < plant.num_bodies(); ++k) {
-    const auto maybe_inertia = processor.MaybeFixBodyInertia(k);
-    if (maybe_inertia.has_value()) {
-      processor.UpdateInertiaXml(k, *maybe_inertia);
-    }
+  // Chances are good that parsing may end in an error (implemented as throw).
+  // For purposes of this tool, we should make its message usable. To do that,
+  // catch it and display it via logging. Yes, this violates the coding
+  // standard; in this case it makes the tool significantly more useful.
+  std::vector<ModelInstanceIndex> models;
+  try {
+    models = parser.AddModels(argv[1]);
+  } catch (const std::exception& e) {
+    drake::log()->error(e.what());
+    ::exit(EXIT_FAILURE);
   }
+
+  InertiaProcessor processor(plant, scene_graph, models, &xml_doc);
+  processor.Process();
 
   // Use our custom XmlPrinter to "print to memory", then dump that to file.
   XmlPrinter printer;
