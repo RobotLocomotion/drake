e70d3cde1bb0ea7aac7a4f85c88baf863bd090ee
Joseph Masterjohn <joemasterjohn@gmail.com>
2023-05-24 15:21:49 -0400

    [multibody] Reduce SapContactProblem to selected/exluded dofs.

--- multibody/plant/slicing_and_indexing.cc
+++ multibody/plant/slicing_and_indexing.cc
@@ -1,6 +1,7 @@
 #include "drake/multibody/plant/slicing_and_indexing.h"
 
 #include <set>
+#include <utility>
 
 #include "drake/common/default_scalars.h"
 #include "drake/common/eigen_types.h"
@@ -47,6 +48,29 @@ MatrixX<T> SelectRowsCols(const MatrixX<T>& M,
 }
 
 template <typename T>
+MatrixX<T> ExcludeRowsCols(const MatrixX<T>& M,
+                           const std::vector<int>& indices) {
+  DRAKE_DEMAND(M.rows() == M.cols());
+  DRAKE_ASSERT_VOID(DemandIndicesValid(indices, M.rows()));
+  if (indices.size() == 0) {
+    return M;
+  }
+  std::vector<int> selected_indices;
+  selected_indices.reserve(M.rows() - indices.size());
+
+  int excluded_index = 0;
+  for (int i = 0; i < M.rows(); ++i) {
+    if (i == indices[excluded_index]) {
+      ++excluded_index;
+    } else {
+      selected_indices.push_back(i);
+    }
+  }
+  DRAKE_ASSERT(selected_indices.size() == M.rows() - indices.size());
+  return SelectRowsCols(M, selected_indices);
+}
+
+template <typename T>
 MatrixX<T> SelectCols(const MatrixX<T>& M, const std::vector<int>& indices) {
   DRAKE_ASSERT_VOID(DemandIndicesValid(indices, M.cols()));
   const int selected_count = indices.size();
@@ -62,6 +86,53 @@ MatrixX<T> SelectCols(const MatrixX<T>& M, const std::vector<int>& indices) {
 }
 
 template <typename T>
+contact_solvers::internal::MatrixBlock<T> SelectCols(
+    const contact_solvers::internal::MatrixBlock<T>& M,
+    const std::vector<int>& indices) {
+  if (M.is_dense()) {
+    return contact_solvers::internal::MatrixBlock<T>(
+        std::move(SelectCols(M.MakeDenseMatrix(), indices)));
+  } else {
+    throw std::runtime_error(
+        "SelectCols only supports dense MatrixBlock arguments.");
+  }
+}
+
+template <typename T>
+MatrixX<T> ExcludeCols(const MatrixX<T>& M, const std::vector<int>& indices) {
+  DRAKE_ASSERT_VOID(DemandIndicesValid(indices, M.cols()));
+  if (indices.size() == 0) {
+    return M;
+  }
+  std::vector<int> selected_indices;
+  selected_indices.reserve(M.cols() - indices.size());
+
+  int excluded_index = 0;
+  for (int i = 0; i < M.cols(); ++i) {
+    if (i == indices[excluded_index]) {
+      ++excluded_index;
+    } else {
+      selected_indices.push_back(i);
+    }
+  }
+  DRAKE_ASSERT(selected_indices.size() == M.cols() - indices.size());
+  return SelectCols(M, selected_indices);
+}
+
+template <typename T>
+contact_solvers::internal::MatrixBlock<T> ExcludeCols(
+    const contact_solvers::internal::MatrixBlock<T>& M,
+    const std::vector<int>& indices) {
+  if (M.is_dense()) {
+    return contact_solvers::internal::MatrixBlock<T>(
+        std::move(ExcludeCols(M.MakeDenseMatrix(), indices)));
+  } else {
+    throw std::runtime_error(
+        "ExcludeColsCols only supports dense MatrixBlock arguments.");
+  }
+}
+
+template <typename T>
 VectorX<T> SelectRows(const VectorX<T>& v, const std::vector<int>& indices) {
   DRAKE_ASSERT_VOID(DemandIndicesValid(indices, v.size()));
   const int selected_count = indices.size();
@@ -72,6 +143,28 @@ VectorX<T> SelectRows(const VectorX<T>& v, const std::vector<int>& indices) {
 
   for (int i = 0; i < result.rows(); ++i) {
     result(i) = v(indices[i]);
+  }
+  return result;
+}
+
+template <typename T>
+VectorX<T> ExcludeRows(const VectorX<T>& v, const std::vector<int>& indices) {
+  DRAKE_ASSERT_VOID(DemandIndicesValid(indices, v.size()));
+  const int selected_count = indices.size();
+  if (selected_count == 0) {
+    return v;
+  }
+  VectorX<T> result(v.rows() - selected_count);
+
+  int r_index = 0;
+  int exclude_index = 0;
+  for (int i = 0; i < v.rows(); ++i) {
+    if (i < indices[exclude_index]) {
+      result(r_index) = v(i);
+      ++r_index;
+    } else {
+      ++exclude_index;
+    }
   }
   return result;
 }
@@ -99,8 +192,45 @@ VectorX<T> ExpandRows(const VectorX<T>& v, int rows_out,
   return result;
 }
 
+template <typename T>
+VectorX<T> ExpandRows(const Eigen::VectorBlock<const VectorX<T>>& v,
+                      int rows_out, const std::vector<int>& indices) {
+  DRAKE_ASSERT(static_cast<int>(indices.size()) == v.rows());
+  DRAKE_ASSERT(rows_out >= v.rows());
+  DRAKE_ASSERT_VOID(DemandIndicesValid(indices, rows_out));
+  if (rows_out == v.rows()) {
+    return v;
+  }
+  VectorX<T> result(rows_out);
+
+  int index_cursor = 0;
+  for (int i = 0; i < result.rows(); ++i) {
+    if (index_cursor >= v.rows() || i < indices[index_cursor]) {
+      result(i) = 0.;
+    } else {
+      result(indices[index_cursor]) = v(index_cursor);
+      ++index_cursor;
+    }
+  }
+  return result;
+}
+
 DRAKE_DEFINE_FUNCTION_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
-    (&SelectRowsCols<T>, &SelectRows<T>, &SelectCols<T>, &ExpandRows<T>))
+    (&SelectRowsCols<T>, &ExcludeRowsCols<T>, &SelectRows<T>, &ExcludeRows<T>,
+     static_cast<MatrixX<T> (*)(const MatrixX<T>&, const std::vector<int>&)>(
+         &SelectCols),
+     static_cast<contact_solvers::internal::MatrixBlock<T> (*)(
+         const contact_solvers::internal::MatrixBlock<T>&,
+         const std::vector<int>&)>(&SelectCols),
+     static_cast<MatrixX<T> (*)(const MatrixX<T>&, const std::vector<int>&)>(
+         &ExcludeCols),
+     static_cast<contact_solvers::internal::MatrixBlock<T> (*)(
+         const contact_solvers::internal::MatrixBlock<T>&,
+         const std::vector<int>&)>(&ExcludeCols),
+     static_cast<VectorX<T> (*)(const Eigen::VectorBlock<const VectorX<T>>&,
+                                int, const std::vector<int>&)>(&ExpandRows),
+     static_cast<VectorX<T> (*)(const VectorX<T>&, int,
+                                const std::vector<int>&)>(&ExpandRows)))
 
 }  // namespace internal
 }  // namespace multibody
