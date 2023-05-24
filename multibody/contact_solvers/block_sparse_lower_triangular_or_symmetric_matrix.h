d758122ac6a49d68fd3c51716942f65e6d144b0c
Xuchen Han <xuchen.han@tri.global>
2023-05-24 11:22:51 -0700

    Address Alejandro's comment

--- multibody/contact_solvers/block_sparse_lower_triangular_or_symmetric_matrix.h
+++ multibody/contact_solvers/block_sparse_lower_triangular_or_symmetric_matrix.h
@@ -33,7 +33,8 @@ class BlockSparsityPattern {
    @pre Each node j in neighbor[i] satisfies j >= i, i.e, if an edge exists
         between i and j, j exists in neighbor[i], but i doesn't exist in
         neighbor[j].
-   @pre i exists in neighbors[i]*/
+   @pre i exists in neighbors[i] and the minimum size of `neighbors` is one
+        (when only the diagonal block exists). */
   BlockSparsityPattern(std::vector<int> block_sizes,
                        std::vector<std::vector<int>> neighbors)
       : block_sizes_(std::move(block_sizes)), neighbors_(std::move(neighbors)) {
@@ -55,12 +56,12 @@ class BlockSparsityPattern {
   const std::vector<int>& block_sizes() const { return block_sizes_; }
 
   /* The connectivity pattern of the sparsity. More specifically,
-   `neighbors()[c][i]` gives the block row index of the i-th non-zero block in
-   the c-th block column. `neighbors[c]` is sorted for each block column c and
-   that each entry in `neighbors[c]` is greater than or equal to c. In other
-   words, only the lower triangular part of the sparsity pattern is specified.
-   As a result, we have `neighbors[c][0] = c` for each c because all diagonal
-   blocks are nonzero. */
+   `neighbors()[j][k]` gives the block row index of the k-th non-zero block in
+   the j-th block column. `neighbors()[j]` is sorted for each block column j and
+   each entry in `neighbors()[j]` is greater than or equal to j. In other words,
+   only the lower triangular part of the sparsity pattern is specified. As a
+   result, we have `neighbors[j][0] = j` for each j because all diagonal blocks
+   are nonzero. */
   const std::vector<std::vector<int>>& neighbors() const { return neighbors_; }
 
  private:
@@ -79,7 +80,7 @@ class BlockSparsityPattern {
      sparsity pattern and mutable data.
 
  @tparam MatrixType   The matrix type of each block in block sparse matrix,
-                      Matrix3<double> or Matrix<X<double>.
+                      Matrix3<double> or MatrixX<double>.
  @tparam is_symmetric Determines whether the matrix is symmetric or lower
                       triangular.
  @warn Data is ALWAYS retrieved/set through the lower triangular part of the
@@ -96,7 +97,7 @@ class BlockSparseLowerTriangularOrSymmetricMatrix {
    block sparsity pattern.
    @param sparsity_pattern  The block sparsity pattern of lower triangular part
                             of the matrix. */
-  BlockSparseLowerTriangularOrSymmetricMatrix(
+  explicit BlockSparseLowerTriangularOrSymmetricMatrix(
       BlockSparsityPattern sparsity_pattern);
 
   int rows() const { return cols_; }
@@ -106,7 +107,8 @@ class BlockSparseLowerTriangularOrSymmetricMatrix {
 
   /* Adds Aij to the ij-th block of this matrix.
    @pre Aij = Aij.transpose() if i==j and `this` matrix is symmetric.
-   @pre HasBlock(i, j) == true.
+   @pre HasBlock(i, j) == true (note that HasBlock(i, j) is always false when
+   j > i and `this` matrix is not symmetric).
    @pre The size of Aij is compatible to the size of ij-th block implied by the
    sparsity pattern at construction. */
   void AddToBlock(int i, int j, const Eigen::Ref<const MatrixType>& Aij) {
@@ -118,9 +120,10 @@ class BlockSparseLowerTriangularOrSymmetricMatrix {
     old_value += Aij;
   }
 
-  /* Similar to AddToBlock, but overwrites instead of accumulates.
+  /* Overwrites the ij-th block with `Aij`.
    @pre Aij = Aij.transpose() if i==j and `this` matrix is symmetric.
-   @pre HasBlock(i, j) == true.
+   @pre HasBlock(i, j) == true (note that HasBlock(i, j) is always false when
+   j > i and `this` matrix is not symmetric).
    @pre The size of Aij is compatible to the size of ij-th block implied by the
    sparsity pattern at construction. */
   void SetBlock(int i, int j, MatrixType Aij) {
@@ -134,17 +137,21 @@ class BlockSparseLowerTriangularOrSymmetricMatrix {
 
   /* (Advanced) Similar to SetBlock, but uses flat indices instead of block row
    indices. This is convenient when one has the flat index of block readily
-   available. */
+   available (e.g. while looping over a column when the number of nonzero blocks
+   in the column is known).
+   @pre 0 <= j < block_cols().
+   @pre The j-th block column has at least `flat+1` nonzero entries. */
   void SetBlockFlat(int flat, int j, MatrixType Aij) {
+    DRAKE_ASSERT(0 <= j && j < block_cols_);
     DRAKE_ASSERT(flat >= 0 && flat < ssize(blocks_[j]));
     blocks_[j][flat] = std::move(Aij);
   }
 
-  /* Sets the numerical values of all nonzero blocks to zero without
-   changing the sparsity pattern. */
+  /* Sets the numerical values of all nonzero blocks to zero without changing
+   the sparsity pattern. */
   void SetZero();
 
-  /* Makes a dense representation of the matrix. Useful for debugging purpose.
+  /* Makes a dense representation of the matrix. Useful for debugging purposes.
    */
   MatrixX<double> MakeDenseMatrix() const;
 
@@ -173,30 +180,38 @@ class BlockSparseLowerTriangularOrSymmetricMatrix {
     return blocks_[j][block_row_to_flat_[j][i]];
   }
 
-  /* Returns the i-th diagonal block. */
-  const MatrixType& diagonal_block(int i) const {
-    DRAKE_ASSERT(0 <= i && i < block_cols_);
+  /* Returns the j-th diagonal block.
+   @pre 0 <= j < block_cols(). */
+  const MatrixType& diagonal_block(int j) const {
+    DRAKE_ASSERT(0 <= j && j < block_cols_);
     /* Since block_rows are sorted with in each block column, the first entry is
      necessarily the diagonal. */
-    return blocks_[i][0];
+    return blocks_[j][0];
   }
 
   /* (Advanced) Similar to `block`, but returns matrix blocks based on flat
-   indices instead of block row indices. */
+   indices instead of block row indices.
+   @pre 0 <= j < block_cols().
+   @pre The j-th block column has at least `flat+1` nonzero entries. */
   const MatrixType& block_flat(int flat, int j) {
     DRAKE_ASSERT(0 <= j && j < block_cols_);
+    DRAKE_ASSERT(flat >= 0 && flat < ssize(blocks_[j]));
     return blocks_[j][flat];
   }
 
   /* (Advanced) Similar to `mutable_block`, but returns matrix blocks based on
-   flat indices instead of block row indices. */
+   flat indices instead of block row indices.
+   @pre 0 <= j < block_cols().
+   @pre The j-th block column has at least `flat+1` nonzero entries. */
   MatrixType& mutable_block_flat(int flat, int j) {
     DRAKE_ASSERT(0 <= j && j < block_cols_);
+    DRAKE_ASSERT(flat >= 0 && flat < ssize(blocks_[j]));
     return blocks_[j][flat];
   }
 
   /* Returns the sorted block row indices in the lower triangular part of the
-   j-th block column. */
+   j-th block column.
+   @pre 0 <= j < block_cols(). */
   const std::vector<int>& block_row_indices(int j) const {
     DRAKE_ASSERT(0 <= j && j < block_cols_);
     return sparsity_pattern_.neighbors()[j];
@@ -218,15 +233,20 @@ class BlockSparseLowerTriangularOrSymmetricMatrix {
   int block_cols_{};
   /* The number of columns. */
   int cols_{};
-  /* See getter. */
-  std::vector<int> starting_cols_;
   /* Dense blocks stored in a 2d vector. The first index is the block column
    index and the second index is a flat index that can be retrieved from
    block_row_to_flat_. */
   std::vector<std::vector<MatrixType>> blocks_;
+  // TODO(xuchenhan-tri): consider putting the following index book-keeping into
+  // the BlockSparsityPattern class.
+  /* See getter. */
+  std::vector<int> starting_cols_;
   /* Mapping from block row index to flat index for each column; i.e.,
-   blocks_[c][block_row_to_flat_[c][r]] gives the (r,c) block.
-   block_row_to_flat_[c][r] == -1 if the implied block is empty. */
+   blocks_[j][block_row_to_flat_[j][i]] gives the (i,j) block.
+   block_row_to_flat_[j][i] == -1 if the implied block is empty. */
+  // TODO(xuchenhan-tri): consider using
+  // std::vector<std::vector<unordered_map<int, int>>> to accomodate large
+  // matrices.
   std::vector<std::vector<int>> block_row_to_flat_;
 };
 
