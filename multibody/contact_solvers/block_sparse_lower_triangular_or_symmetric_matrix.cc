484cb13aa6bc5e83edb5a0d4442acad8b7a0db45
Xuchen Han <xuchen.han@tri.global>
2023-06-01 11:16:58 -0700

    Address Grant's comment

--- multibody/contact_solvers/block_sparse_lower_triangular_or_symmetric_matrix.cc
+++ multibody/contact_solvers/block_sparse_lower_triangular_or_symmetric_matrix.cc
@@ -2,6 +2,8 @@
 
 #include <algorithm>
 
+#include <fmt/format.h>
+
 namespace drake {
 namespace multibody {
 namespace contact_solvers {
@@ -67,6 +69,30 @@ MatrixX<double> BlockSparseLowerTriangularOrSymmetricMatrix<
     }
   }
   return result;
+}
+
+template <typename MatrixType, bool is_symmetric>
+void BlockSparseLowerTriangularOrSymmetricMatrix<
+    MatrixType, is_symmetric>::AssertValid(int i, int j,
+                                           const std::optional<MatrixType>& Aij,
+                                           const char* source) const {
+  if (!(0 <= j && j <= i && i <= block_rows())) {
+    throw std::runtime_error(fmt::format(
+        "{}: block indices out of bound. It is required that 0 <= j && j <= i "
+        "&& i < block_rows(). Instead, i = {}, j = {}, block_rows() = {}.",
+        source, i, j, block_rows()));
+  }
+  if (!HasBlock(i, j)) {
+    throw std::runtime_error(fmt::format(
+        "{}: The requested {},{}-th block doesn't exist.", source, i, j));
+  }
+  if (is_symmetric && i == j && Aij.has_value() &&
+      !((*Aij - Aij->transpose()).norm() < 1e-12 * Aij->norm())) {
+    throw std::runtime_error(
+        fmt::format("{}: The {}-th diagonal block must be symmetric for a "
+                    "symmetric matrix.",
+                    source, i));
+  }
 }
 
 template class BlockSparseLowerTriangularOrSymmetricMatrix<MatrixX<double>,
