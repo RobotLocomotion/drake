f7c7f43686ab81f77a4503359ebfce8e91ca2afc
Russ Tedrake <russt@mit.edu>
2023-06-03 21:34:19 -0400

    Add support for num_positions != num_velocities in DifferentialIK
    Resolves a question from StackOverflow:
    https://stackoverflow.com/questions/76391333/differential-inverse-kinematics-integrator-does-not-work-with-a-floating-plant

--- bindings/pydrake/multibody/inverse_kinematics_py_differential.cc
+++ bindings/pydrake/multibody/inverse_kinematics_py_differential.cc
@@ -118,14 +118,15 @@ void DefineIkDifferential(py::module m) {
       "DoDifferentialInverseKinematics",
       [](const Eigen::VectorXd& q_current, const Eigen::VectorXd& v_current,
           const Eigen::VectorXd& V, const Eigen::MatrixXd& J,
-          const DifferentialInverseKinematicsParameters& parameters) {
+          const DifferentialInverseKinematicsParameters& parameters,
+          const std::optional<Eigen::Ref<const MatrixX<double>>>& Nplus) {
         return DoDifferentialInverseKinematics(
-            q_current, v_current, V, J, parameters);
+            q_current, v_current, V, J, parameters, Nplus);
       },
       py::arg("q_current"), py::arg("v_current"), py::arg("V"), py::arg("J"),
-      py::arg("parameters"),
+      py::arg("parameters"), py::arg("Nplus") = std::nullopt,
       doc.DoDifferentialInverseKinematics
-          .doc_5args_q_current_v_current_V_J_parameters);
+          .doc_6args_q_current_v_current_V_J_parameters_Nplus);
 
   m.def(
       "DoDifferentialInverseKinematics",
--- multibody/inverse_kinematics/differential_inverse_kinematics.h
+++ multibody/inverse_kinematics/differential_inverse_kinematics.h
@@ -64,9 +64,6 @@ class DifferentialInverseKinematicsParameters {
    * @param num_positions Number of generalized positions.
    * @param num_velocities Number of generalized velocities (by default it will
    * be set to num_positions).
-   *
-   * @note Currently DoDifferentialInverseKinematics requires that
-   * num_positions == num_velocities.
    */
   DifferentialInverseKinematicsParameters(
       int num_positions, std::optional<int> num_velocities = std::nullopt);
@@ -158,7 +155,7 @@ class DifferentialInverseKinematicsParameters {
   /**
    * Sets the joint centering gain, K, so that the joint centering command is
    * v_next = K * (q_nominal - q_current).
-   * @pre K must be num_velocities x num_positions.
+   * @pre K must be num_positions x num_positions.
    */
   void set_joint_centering_gain(const MatrixX<double>& K) {
     DRAKE_DEMAND(K.rows() == num_velocities_);
@@ -288,12 +285,12 @@ class DifferentialInverseKinematicsParameters {
  *
  * ```
  *   min_{v_next,alpha}  -100 * alpha
- *                         + | P * (v_next - K * (q_nominal - q_current)) |^2
+ *                         + |P * (v_next - N⁺*K*(q_nominal - q_current))|^2
  *
  *   s.t.
  *     J * v_next = alpha * V, // J * v_next has the same direction as V
  *     0 <= alpha <= 1,        // Never go faster than V
- *     joint_lim_min <= q_current + v_next*dt <= joint_lim_max,
+ *     joint_lim_min <= q_current + N*v_next*dt <= joint_lim_max,
  *     joint_vel_lim_min <= v_next <= joint_vel_lim_max,
  *     joint_accel_lim_min <= (v_next - v_current)/dt <= joint_accel_lim_max,
  *     and additional linear velocity constraints,
@@ -334,6 +331,10 @@ class DifferentialInverseKinematicsParameters {
  * velocity as @p V.
  * @param parameters Collection of various problem specific constraints and
  * constants.
+ * @param Nplus (optional) matrix which maps v = N⁺(q)⋅q̇. See
+ * MultibodyPlant::MapQDotToVelocity() for more details. By default, it is
+ * taken to be the identity matrix. If dim(q) != dim(v), then you *must*
+ * provide Nplus.
  * @return If the solver successfully finds a solution, joint_velocities will
  * be set to v, otherwise it will be nullopt.
  *
@@ -344,7 +345,9 @@ DifferentialInverseKinematicsResult DoDifferentialInverseKinematics(
     const Eigen::Ref<const VectorX<double>>& v_current,
     const Eigen::Ref<const VectorX<double>>& V,
     const Eigen::Ref<const MatrixX<double>>& J,
-    const DifferentialInverseKinematicsParameters& parameters);
+    const DifferentialInverseKinematicsParameters& parameters,
+    const std::optional<Eigen::Ref<const MatrixX<double>>>& Nplus =
+        std::nullopt);
 
 // TODO(russt): V_WE_desired should be of type SpatialVelocity.
 /**
@@ -400,12 +403,14 @@ DifferentialInverseKinematicsResult DoDifferentialInverseKinematics(
 #ifndef DRAKE_DOXYGEN_CXX
 namespace internal {
 DifferentialInverseKinematicsResult DoDifferentialInverseKinematics(
-    const Eigen::Ref<const VectorX<double>>&,
-    const Eigen::Ref<const VectorX<double>>&,
-    const math::RigidTransform<double>&,
-    const Eigen::Ref<const Matrix6X<double>>&,
-    const SpatialVelocity<double>&,
-    const DifferentialInverseKinematicsParameters&);
+    const Eigen::Ref<const VectorX<double>>& q_current,
+    const Eigen::Ref<const VectorX<double>>& v_current,
+    const math::RigidTransform<double>& X_WE,
+    const Eigen::Ref<const Matrix6X<double>>& J_WE_W,
+    const SpatialVelocity<double>& V_WE_desired,
+    const DifferentialInverseKinematicsParameters& parameters,
+    const std::optional<Eigen::Ref<const MatrixX<double>>>& Nplus =
+        std::nullopt);
 }  // namespace internal
 #endif
 
