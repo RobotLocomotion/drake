49be3f31ca571b7d1d39950ba15d6df2d766422f
Sean Curtis <sean.curtis@tri.global>
2023-06-08 10:58:13 -0700

    Make RenderEngineGl intelligently threadsafe (#19426)
    * Make RenderEngineGl intelligently threadsafe
    
    When a RenderEngineGl is cloned, instead of *sharing* an instance of
    OpenGlContext with its clones, each engine clone has a unique clone of the
    original engine's OpenGlContext. The cloned contexts are unique in that
    they can be bound to different threads, but they share the same underlying
    OpenGl objects, stored on the GPU.
    
    Because RenderEngineGl no longer shares the exact same OpenGl context with
    the original, some additional work needs to be done to the clone to make
    sure it is fully prepared:
    
      - Its OpenGl state needs to be configured (back face culling, etc.)
      - Vertex array objects need to be recreated from the vertex buffers
        shared in the OpenGl context -- by design, vertex array objects don't
        get shared.
        - Additional infrastructure has been added so the recreation of array
          objects from buffers is possible.
      - Each cloned instance has to have its own unique namespace for shade
        program uniforms. This is accomplished by recreating the OpenGl
        program for instance (using the same compiled shaders).
    
    This means, the private copy constructor for RenderEngineGl is, itself,
    insufficient to create a working copy. Only the public Clone() method
    should be used.
    
    A new test has been added to test the thread safety invariants of both
    OpenGlContext and RenderEngineGl.

--- geometry/render_gl/test/internal_opengl_context_test.cc
+++ geometry/render_gl/test/internal_opengl_context_test.cc
@@ -1,5 +1,7 @@
 #include "drake/geometry/render_gl/internal_opengl_context.h"
 
+#include <memory>
+
 #include <gtest/gtest.h>
 
 namespace drake {
@@ -8,23 +10,40 @@ namespace render_gl {
 namespace internal {
 namespace {
 
-// Tests that an OpenGL context can be obtained.
-GTEST_TEST(OpenGlContext, GetContext) {
-  OpenGlContext opengl_context{};
-  EXPECT_FALSE(glIsEnabled(GL_DEBUG_OUTPUT));
-  opengl_context.MakeCurrent();
-  // Test switching contexts and enabling debug mode.
-  OpenGlContext debug_context{true};
-  EXPECT_TRUE(glIsEnabled(GL_DEBUG_OUTPUT));
-  debug_context.MakeCurrent();
-  // Enable something on the current context.
-  EXPECT_FALSE(glIsEnabled(GL_BLEND));
-  glEnable(GL_BLEND);
-  EXPECT_TRUE(glIsEnabled(GL_BLEND));
-  // Back to original context.
-  opengl_context.MakeCurrent();
-  // The previous enable should have no effect on this context.
-  EXPECT_FALSE(glIsEnabled(GL_BLEND));
+// Tests the context binding APIs: IsCurrent(), MakeCurrent(), and
+// ClearCurrent().
+//
+//   - A created context isn't bound.
+//   - IsCurrent() correctly reports a context's status *for the current
+//     thread*.
+//   - MakeCurrent() binds the current thread.
+//     - Replacing any previously bound context.
+//   - ClearCurrent() clears any bound context.
+GTEST_TEST(OpenGlContextTest, BindingContexts) {
+  const OpenGlContext c1;
+  EXPECT_FALSE(c1.IsCurrent());
+  const OpenGlContext c2;
+  EXPECT_FALSE(c2.IsCurrent());
+
+  // Binding one means it is the only one that reports as bound.
+  c1.MakeCurrent();
+  EXPECT_TRUE(c1.IsCurrent());
+  EXPECT_FALSE(c2.IsCurrent());
+
+  // Binding another replaces the first.
+  c2.MakeCurrent();
+  EXPECT_FALSE(c1.IsCurrent());
+  EXPECT_TRUE(c2.IsCurrent());
+
+  // Calling ClearCurrent() clears them both.
+  OpenGlContext::ClearCurrent();
+  EXPECT_FALSE(c1.IsCurrent());
+  EXPECT_FALSE(c2.IsCurrent());
+
+  // Calling ClearCurrent() with nothing bound has no effect.
+  OpenGlContext::ClearCurrent();
+  EXPECT_FALSE(c1.IsCurrent());
+  EXPECT_FALSE(c2.IsCurrent());
 }
 
 GTEST_TEST(OpenGlContext, WindowVisibility) {
@@ -48,6 +67,79 @@ GTEST_TEST(OpenGlContext, WindowVisibility) {
   // Calling it again doesn't have any effect.
   opengl_context.HideWindow();
   EXPECT_FALSE(opengl_context.IsWindowViewable());
+}
+
+// Confirm that a context clone shares objects with its source.
+GTEST_TEST(OpenGlContextTest, CloneShares) {
+  const OpenGlContext source;
+  source.MakeCurrent();
+
+  // Create a buffer in source's context.
+  GLuint buffer;
+  glCreateFramebuffers(1, &buffer);
+  ASSERT_TRUE(glIsFramebuffer(buffer));
+  // Reject the null hypothesis -- not everything is a frame buffer.
+  ASSERT_FALSE(glIsFramebuffer(buffer + 1));
+
+  // Clone shares the frame buffer.
+  const OpenGlContext clone(source);
+  clone.MakeCurrent();
+  // It's still a frame buffer for the clone of the source context.
+  EXPECT_TRUE(glIsFramebuffer(buffer));
+
+  // However, a newly created OpenGlContext does *not* have the frame buffer.
+  const OpenGlContext independent;
+  independent.MakeCurrent();
+  ASSERT_FALSE(glIsFramebuffer(buffer));
+}
+
+GTEST_TEST(OpenGlContextTest, SourceSurvivesCloneDeath) {
+  const OpenGlContext source;
+  source.MakeCurrent();
+
+  // Create a buffer in source's context.
+  GLuint buffer;
+  glCreateFramebuffers(1, &buffer);
+
+  {
+    // Create and destroy the clone; the source isn't harmed.
+    const OpenGlContext clone(source);
+    clone.MakeCurrent();
+    // It's still a frame buffer with the clone of the source context.
+    ASSERT_TRUE(glIsFramebuffer(buffer));
+    // We'll destroy this context as we leave. Making sure the context isn't
+    // bound when the destructor is called will lead to its immediate
+    // destruction.
+    OpenGlContext::ClearCurrent();
+  }
+
+  // Even after the destruction of the sharing context, the buffer still exists
+  // in the original.
+  source.MakeCurrent();
+  ASSERT_TRUE(glIsFramebuffer(buffer));
+}
+
+GTEST_TEST(OpenGlContextTest, CloneSurvivesSourceDeath) {
+  // This will get defined from a source that gets destroyed.
+  std::unique_ptr<OpenGlContext> clone = nullptr;
+  GLuint buffer;
+
+  {
+    const OpenGlContext source;
+    source.MakeCurrent();
+
+    // Create a buffer in source's context.
+    glCreateFramebuffers(1, &buffer);
+
+    clone = std::make_unique<OpenGlContext>(source);
+
+    // Make sure the source isn't bound so its OpenGl objects get destroyed
+    // immediately.
+    OpenGlContext::ClearCurrent();
+  }
+
+  clone->MakeCurrent();
+  ASSERT_TRUE(glIsFramebuffer(buffer));
 }
 
 }  // namespace
