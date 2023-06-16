f083890a1f898208ea84ebc95ad92c57b851f2d3
mitiguy <paul.mitiguy@tri.global>
2023-06-15 17:00:59 -0700

    Enhance spatial inertia shift efficiency/significant digits.

--- multibody/plant/test/multibody_plant_forward_dynamics_test.cc
+++ multibody/plant/test/multibody_plant_forward_dynamics_test.cc
@@ -197,10 +197,9 @@ GTEST_TEST(MultibodyPlantForwardDynamics, AtlasRobot) {
   // Verify that the implicit dynamics match the continuous ones.
   Eigen::VectorXd residual = plant.AllocateImplicitTimeDerivativesResidual();
   plant.CalcImplicitTimeDerivativesResidual(*context, *derivatives, &residual);
-  // Note the slightly looser tolerance of 4e-13 which was required for this
-  // test.
+  // A looser tolerance of 4e-13 was required for this to pass on the Macintosh.
   EXPECT_TRUE(CompareMatrices(
-      residual, Eigen::VectorXd::Zero(plant.num_multibody_states()), 6e-13));
+      residual, Eigen::VectorXd::Zero(plant.num_multibody_states()), 4e-13));
 }
 
 // Verifies we can do forward dynamics on a model with a zero-sized state.
