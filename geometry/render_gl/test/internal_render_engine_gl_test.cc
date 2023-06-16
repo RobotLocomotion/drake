09aea37c6948954edaa2e9e2b5ee53a2dc0c2289
Sean Curtis <sean.curtis@tri.global>
2023-06-16 15:01:44 +0900

    Handle geometry::Mesh("foo.gltf", 1) throughout Drake
    If we allow a user to specify a Mesh using a .gltf (instead of an .obj)
    then every system that can't reify a .gltf file needs to respond.
    
    1. geometry::Mesh and geometry::Convex conveniently report their extension.
    2. Updating the following shape reifiers:
       - DrakeVisualizer warns and ignores non-objs.
       - Meshcat warns and ignores unsupported geometry types.
         - The two primary unsupported geometry types (STL and GLTF) are in
           flux.
       - Hydroelastic internal
         - MakeRigidRepresentation
           - Mesh already throws for unsupported mesh type.
           - Convex extended to likewise throw for unsupported mesh type.
         - MakeSoftRepresentation
           - Mesh calls into MakeVolumeMeshFromVtk; we defer to that to know
             if it has the expected vtk.
           - Convex calls into MakeConvexVolumeMesh; we defer to that to know
             if it has the expected obj.
       - RenderEngineGl warns and ignores non-objs.
       - RenderEngineVtk
    3. Skipping the following shape reifiers:
       - MeshBuilderForDeformable. It currently only supports vtk files with
         tetrahedral meshes. Presumably, it already handles the case of a
         non-vtk file provided (e.g., obj). We'll rely on that logic to handle
         a mesh with the .gltf extension as well.
       - ProximityEngine. It already has the protocol to throw on any non-obj.
       - ShapeToString. It doesn't care about extensions.
       - deformable_contact_internal (Geometries). This actually defers to
         hydroleastic_internal (see above).
       - ShapeConfigurations and MakeFclShape (in characterization_utilities.h)
         - They are hard coded to use a box mesh and ignore the filename.

--- geometry/render_gl/test/internal_render_engine_gl_test.cc
+++ geometry/render_gl/test/internal_render_engine_gl_test.cc
@@ -48,7 +48,16 @@ class RenderEngineGlTester {
   }
 
   const internal::OpenGlGeometry GetMesh(const std::string& filename) const {
-    const int index = const_cast<RenderEngineGl&>(engine_).GetMesh(filename);
+    // A dummy registration data; we'll learn if the filename was accepted
+    // by examining the data.
+    RenderEngineGl::RegistrationData data{GeometryId::get_new_id(), {}, {}};
+    const int index =
+        const_cast<RenderEngineGl&>(engine_).GetMesh(filename, &data);
+    // We should have non-negative and accepted or negative and not accepted.
+    if ((index >= 0) != data.accepted) {
+      throw std::runtime_error(
+          "Mesh acceptance state doesn't match the returned geometry index");
+    }
     return engine_.geometries_[index];
   }
 
@@ -998,6 +1007,24 @@ TEST_F(RenderEngineGlTest, TextureMeshTest) {
     EXPECT_NE(gl_engine, nullptr);
     PerformCenterShapeTest(gl_engine);
   }
+}
+
+// Confirms that meshes/convex referencing a file with an unsupported extension
+// are ignored. (There's also an untested one-time warning.)
+TEST_F(RenderEngineGlTest, UnsupportedMeshConvex) {
+  Init(X_WR_, false);
+  const PerceptionProperties material = simple_material();
+  const GeometryId id = GeometryId::get_new_id();
+
+  const Mesh mesh("invalid.fbx");
+  EXPECT_FALSE(renderer_->RegisterVisual(id, mesh, material,
+                                         RigidTransformd::Identity(),
+                                         false /* needs update */));
+
+  const Convex convex("invalid.fbx");
+  EXPECT_FALSE(renderer_->RegisterVisual(id, convex, material,
+                                         RigidTransformd::Identity(),
+                                         false /* needs update */));
 }
 
 // TODO(SeanCurtis-TRI) Enable this test when I support the default logic for
