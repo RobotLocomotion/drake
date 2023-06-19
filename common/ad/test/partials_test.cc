26cef0c743b4d59645ce77243395242c1f639793
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-06-18 19:37:31 -0700

    [ad] Opt-in to clang-format lint

--- common/ad/test/partials_test.cc
+++ common/ad/test/partials_test.cc
@@ -11,9 +11,9 @@ namespace ad {
 namespace internal {
 namespace {
 
-using Eigen::VectorXd;
 using Eigen::Vector2d;
 using Eigen::Vector4d;
+using Eigen::VectorXd;
 
 // An empty fixture for later expansion.
 class PartialsTest : public ::testing::Test {};
@@ -56,8 +56,8 @@ TEST_F(PartialsTest, UnitCtorMisuse) {
 TEST_F(PartialsTest, FullCtor) {
   const Partials dut{Vector4d::LinSpaced(10.0, 13.0)};
   EXPECT_EQ(dut.size(), 4);
-  EXPECT_TRUE(CompareMatrices(
-      dut.make_const_xpr(), Vector4d::LinSpaced(10.0, 13.0)));
+  EXPECT_TRUE(
+      CompareMatrices(dut.make_const_xpr(), Vector4d::LinSpaced(10.0, 13.0)));
 }
 
 TEST_F(PartialsTest, FullCtorEmpty) {
@@ -111,8 +111,7 @@ TEST_F(PartialsTest, MatchSizeOf) {
 
   // Mismatched sizes.
   const Partials wrong{5, 0};
-  DRAKE_EXPECT_THROWS_MESSAGE(foo.MatchSizeOf(wrong),
-      ".*different sizes.*");
+  DRAKE_EXPECT_THROWS_MESSAGE(foo.MatchSizeOf(wrong), ".*different sizes.*");
 }
 
 TEST_F(PartialsTest, Mul) {
@@ -147,8 +146,8 @@ TEST_F(PartialsTest, DivZero) {
   dut.Div(0.0);
   const double kInf = std::numeric_limits<double>::infinity();
   const double kNaN = std::numeric_limits<double>::quiet_NaN();
-  EXPECT_TRUE(CompareMatrices(dut.make_const_xpr(),
-                              Vector4d(kNaN, kNaN, kInf, kNaN)));
+  EXPECT_TRUE(
+      CompareMatrices(dut.make_const_xpr(), Vector4d(kNaN, kNaN, kInf, kNaN)));
 }
 
 TEST_F(PartialsTest, Add) {
@@ -164,8 +163,8 @@ TEST_F(PartialsTest, AddScaled) {
   dut.AddScaled(2.0, Partials(4, 1));
   dut.AddScaled(3.0, Partials(4, 2));
   dut.AddScaled(4.0, Partials(4, 3));
-  EXPECT_TRUE(CompareMatrices(
-      dut.make_const_xpr(), Vector4d::LinSpaced(1.0, 4.0)));
+  EXPECT_TRUE(
+      CompareMatrices(dut.make_const_xpr(), Vector4d::LinSpaced(1.0, 4.0)));
 }
 
 TEST_F(PartialsTest, AddRhsEmpty) {
@@ -206,14 +205,13 @@ TEST_F(PartialsTest, AddScaledBothEmpty) {
 
 TEST_F(PartialsTest, AddDifferentSizes) {
   Partials dut{4, 2};
-  DRAKE_EXPECT_THROWS_MESSAGE(dut.Add(Partials(2, 0)),
-      ".*different sizes.*");
+  DRAKE_EXPECT_THROWS_MESSAGE(dut.Add(Partials(2, 0)), ".*different sizes.*");
 }
 
 TEST_F(PartialsTest, AddScaledDifferentSizes) {
   Partials dut{4, 2};
   DRAKE_EXPECT_THROWS_MESSAGE(dut.AddScaled(-1.0, Partials(2, 0)),
-      ".*different sizes.*");
+                              ".*different sizes.*");
 }
 
 TEST_F(PartialsTest, DefaultCtorMutableGetter) {
