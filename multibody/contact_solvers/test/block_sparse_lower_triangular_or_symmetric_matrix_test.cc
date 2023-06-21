cb29c6398eee51961685aa02d1b7eb76e6de88a6
xuchenhan-tri <xuchen.han@tri.global>
2023-06-21 16:20:54 -0700

    Allow zero matrix on the diagonal for block sparse matrix (#19647)
    Fix a bug that incorrectly rejects putting zero matrix on the diagonal
    block of a block sparse symmetric matrix.

--- multibody/contact_solvers/test/block_sparse_lower_triangular_or_symmetric_matrix_test.cc
+++ multibody/contact_solvers/test/block_sparse_lower_triangular_or_symmetric_matrix_test.cc
@@ -229,7 +229,9 @@ GTEST_TEST(TriangularBlockSparseMatrixTest, InvalidOperations) {
     /* Non-symmetric diagonal block. */
     DRAKE_ASSERT_THROWS_MESSAGE_IF_ARMED(
         A_symmetric.AddToBlock(2, 2, non_symmetric_matrix),
-        ".*must be symmetric.*");
+        ".*must be symmetric[^]*");
+    /* No false error when zero matrix is added to the diagonal block. */
+    EXPECT_NO_THROW(A_symmetric.AddToBlock(2, 2, MatrixXd::Zero(4, 4)));
 
     /* Evidence that other functions do the same checks. */
     ASSERT_THROW(A_triangular.block(0, 1), std::exception);
