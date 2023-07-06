02bcd4eaf86617f96dcb5b138313fbc5549870c9
Russ Tedrake <russt@mit.edu>
2023-07-05 20:16:13 -0400

    Implements MakeSemidefiniteRelaxation (#19632)
    Given a (typically nonconvex) MathematicalProgram, this method
    constructs a new MathematicalProgram which implements the SDP
    relaxation of the original program.
    
    This initial implementation supports the case of quadratic objectives
    and constraints; it's an extremely important (and quite general) case.
    
    Also touches up a few of the matching lines in the Spectrahedron class
    with feedback from code review for this method.

--- geometry/optimization/spectrahedron.cc
+++ geometry/optimization/spectrahedron.cc
@@ -31,7 +31,7 @@ const double kInf = std::numeric_limits<double>::infinity();
 // TODO(russt): This can be replaced by vars(indices) once we have Eigen 3.4.
 VectorXDecisionVariable GetVariablesByIndex(
     const Eigen::Ref<const VectorXDecisionVariable>& vars,
-    std::vector<int> indices) {
+    const std::vector<int>& indices) {
   VectorXDecisionVariable new_vars(indices.size());
   for (int i = 0; i < ssize(indices); ++i) {
     new_vars[i] = vars[indices[i]];
@@ -126,7 +126,7 @@ Spectrahedron::DoAddPointInNonnegativeScalingConstraints(
     VectorXDecisionVariable vars = stack_xt(binding.variables());
     MatrixXd Ab(binding.evaluator()->num_constraints(), vars.size());
     Ab.leftCols(binding.evaluator()->num_vars()) =
-        binding.evaluator()->GetDenseA();
+        binding.evaluator()->get_sparse_A();
     Ab.rightCols<1>() = -binding.evaluator()->lower_bound();
     constraints.emplace_back(prog->AddLinearEqualityConstraint(Ab, 0, vars));
   }
@@ -143,7 +143,7 @@ Spectrahedron::DoAddPointInNonnegativeScalingConstraints(
     VectorXDecisionVariable vars = stack_xt(binding.variables());
     MatrixXd Ab(binding.evaluator()->num_constraints(), vars.size());
     Ab.leftCols(binding.evaluator()->num_vars()) =
-        binding.evaluator()->GetDenseA();
+        binding.evaluator()->get_sparse_A();
     if (binding.evaluator()->lower_bound().array().isFinite().any()) {
       Ab.rightCols<1>() = -binding.evaluator()->lower_bound();
       constraints.emplace_back(prog->AddLinearConstraint(Ab, 0, kInf, vars));
@@ -203,11 +203,11 @@ Spectrahedron::DoAddPointInNonnegativeScalingConstraints(
     const int num_constraints = binding.evaluator()->num_constraints();
     std::tie(this_A, this_b) = binding_Ab(binding.variables());
     MatrixXd A_bar(num_constraints, x.size() + t.size());
-    A_bar.leftCols(x.size()) = binding.evaluator()->GetDenseA() * this_A;
+    A_bar.leftCols(x.size()) = binding.evaluator()->get_sparse_A() * this_A;
     A_bar.rightCols(t.size()) =
         -binding.evaluator()->upper_bound() * c.transpose();
     VectorXd b_bar = d * binding.evaluator()->upper_bound() -
-                     binding.evaluator()->GetDenseA() * this_b;
+                     binding.evaluator()->get_sparse_A() * this_b;
     constraints.emplace_back(
         prog->AddLinearEqualityConstraint(A_bar, b_bar, {x, t}));
   }
@@ -225,9 +225,9 @@ Spectrahedron::DoAddPointInNonnegativeScalingConstraints(
     const int num_constraints = binding.evaluator()->num_constraints();
     std::tie(this_A, this_b) = binding_Ab(binding.variables());
     MatrixXd A_bar(num_constraints, x.size() + t.size());
-    A_bar.leftCols(x.size()) = binding.evaluator()->GetDenseA() * this_A;
+    A_bar.leftCols(x.size()) = binding.evaluator()->get_sparse_A() * this_A;
     VectorXd b_bar = d * binding.evaluator()->lower_bound() -
-                     binding.evaluator()->GetDenseA() * this_b;
+                     binding.evaluator()->get_sparse_A() * this_b;
     if (b_bar.array().isFinite().any()) {
       A_bar.rightCols(t.size()) =
           -binding.evaluator()->lower_bound() * c.transpose();
@@ -239,7 +239,7 @@ Spectrahedron::DoAddPointInNonnegativeScalingConstraints(
     // [A_c * A, -b_c * c'][x;t] ≤ d * b_c - A_c * b
     // A̅ [x;t] ≤ b̅,
     b_bar = d * binding.evaluator()->upper_bound() -
-            binding.evaluator()->GetDenseA() * this_b;
+            binding.evaluator()->get_sparse_A() * this_b;
     if (b_bar.array().isFinite().any()) {
       A_bar.rightCols(t.size()) =
           -binding.evaluator()->upper_bound() * c.transpose();
