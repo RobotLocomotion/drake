58e5b8847f3cf9e53d13535b2b9f21ea98d2f4d1
Xuchen Han <xuchen.han@tri.global>
2023-07-10 13:31:41 -0700

    Address review comments

--- multibody/contact_solvers/block_sparse_cholesky_solver.cc
+++ multibody/contact_solvers/block_sparse_cholesky_solver.cc
@@ -1,7 +1,6 @@
 #include "drake/multibody/contact_solvers/block_sparse_cholesky_solver.h"
 
 #include <algorithm>
-#include <numeric>
 #include <utility>
 #include <vector>
 
@@ -11,6 +10,9 @@ namespace drake {
 namespace multibody {
 namespace contact_solvers {
 namespace internal {
+
+template <typename BlockType>
+BlockSparseCholeskySolver<BlockType>::~BlockSparseCholeskySolver() = default;
 
 template <typename BlockType>
 void BlockSparseCholeskySolver<BlockType>::SetMatrix(const SymmetricMatrix& A) {
@@ -27,28 +29,22 @@ template <typename BlockType>
 void BlockSparseCholeskySolver<BlockType>::UpdateMatrix(
     const SymmetricMatrix& A) {
   PermuteAndCopyToL(A);
-  matrix_set_ = true;
-  is_factored_ = false;
+  solver_mode_ = SolverMode::kUnfactored;
 }
 
 template <typename BlockType>
 bool BlockSparseCholeskySolver<BlockType>::Factor() {
-  DRAKE_THROW_UNLESS(matrix_set_);
-  matrix_set_ = false;
+  DRAKE_THROW_UNLESS(solver_mode() == SolverMode::kUnfactored);
   for (int j = 0; j < L_->block_cols(); ++j) {
     /* Update diagonal. */
     const BlockType& Ajj = L_->diagonal_block(j);
     L_diag_[j].compute(Ajj);
     if (L_diag_[j].info() != Eigen::Success) {
-      is_factored_ = false;
+      solver_mode_ = SolverMode::kEmpty;
       return false;
     }
-    /* Technically, we should have
-      L_->SetBlockFlat(0, j, L_diag_[j].matrixL());
-    for a complete L_ matrix, but we omit it here since we don't ever touch the
-    diagonal block of L_. Instead, we directly invoke L_diag_. */
-
-    /* Update column.
+    L_->SetBlockFlat(0, j, L_diag_[j].matrixL());
+    /* Update L₂₁ column.
      | a₁₁  *  | = | λ₁₁  0 | * | λ₁₁ᵀ L₂₁ᵀ |
      | a₂₁ a₂₂ |   | L₂₁ L₂₂|   |  0   L₂₂ᵀ |
      So we have
@@ -62,51 +58,52 @@ bool BlockSparseCholeskySolver<BlockType>::Factor() {
       BlockType Lij = Ljj.solve(Aij.transpose()).transpose();
       L_->SetBlockFlat(flat, j, std::move(Lij));
     }
+    /* Update L₂₂ according to L₂₂ = a₂₂ - L₂₁⋅L₂₁ᵀ. */
     RightLookingSymmetricRank1Update(j);
   }
-  is_factored_ = true;
+  solver_mode_ = SolverMode::kFactored;
   return true;
 }
 
 template <typename BlockType>
 VectorX<double> BlockSparseCholeskySolver<BlockType>::Solve(
-    const Eigen::Ref<const VectorX<double>>& y) const {
-  VectorX<double> x(y);
+    const Eigen::Ref<const VectorX<double>>& b) const {
+  VectorX<double> x(b);
   SolveInPlace(&x);
   return x;
 }
 
 template <typename BlockType>
 void BlockSparseCholeskySolver<BlockType>::SolveInPlace(
-    VectorX<double>* y) const {
-  DRAKE_THROW_UNLESS(is_factored_);
-  DRAKE_DEMAND(y != nullptr);
-  DRAKE_DEMAND(y->size() == L_->cols());
-  VectorX<double> permuted_y(*y);
-  scalar_permutation_.Apply(*y, &permuted_y);
+    VectorX<double>* b) const {
+  DRAKE_THROW_UNLESS(solver_mode() == SolverMode::kFactored);
+  DRAKE_THROW_UNLESS(b != nullptr);
+  DRAKE_THROW_UNLESS(b->size() == L_->cols());
+  VectorX<double> permuted_b(*b);
+  scalar_permutation_.Apply(*b, &permuted_b);
 
   const BlockSparsityPattern& block_sparsity_pattern = L_->sparsity_pattern();
   const std::vector<int>& block_sizes = block_sparsity_pattern.block_sizes();
   const std::vector<int>& starting_cols = L_->starting_cols();
 
-  /* Solve Lz = y in place. */
+  /* Solve Lz = b in place. */
   for (int j = 0; j < L_->block_cols(); ++j) {
     const int block_size = block_sizes[j];
     const int offset = starting_cols[j];
     /* Solve for the j-th block entry. */
-    const VectorX<double> yj =
-        L_diag_[j].matrixL().solve(permuted_y.segment(offset, block_size));
-    permuted_y.segment(offset, block_size) = yj;
+    const VectorX<double> bj =
+        L_diag_[j].matrixL().solve(permuted_b.segment(offset, block_size));
+    permuted_b.segment(offset, block_size) = bj;
     /* Eliminate for the j-th block entry from the system. */
     const auto& blocks_in_col_j = L_->block_row_indices(j);
     for (int flat = 1; flat < ssize(blocks_in_col_j); ++flat) {
       const int i = blocks_in_col_j[flat];
-      permuted_y.segment(starting_cols[i], block_sizes[i]).noalias() -=
-          L_->block_flat(flat, j) * yj;
+      permuted_b.segment(starting_cols[i], block_sizes[i]).noalias() -=
+          L_->block_flat(flat, j) * bj;
     }
   }
 
-  VectorX<double>& permuted_z = permuted_y;
+  VectorX<double>& permuted_z = permuted_b;
   /* Solve Lᵀx = z in place. */
   for (int j = L_->block_cols() - 1; j >= 0; --j) {
     /* Eliminate all solved variables. */
@@ -122,7 +119,22 @@ void BlockSparseCholeskySolver<BlockType>::SolveInPlace(
         permuted_z.segment(starting_cols[j], block_sizes[j]));
     permuted_z.segment(starting_cols[j], block_sizes[j]) = zj;
   }
-  scalar_permutation_.ApplyInverse(permuted_z, y);
+  scalar_permutation_.ApplyInverse(permuted_z, b);
+}
+
+template <typename BlockType>
+MatrixX<double> BlockSparseCholeskySolver<BlockType>::L() const {
+  DRAKE_THROW_UNLESS(solver_mode() == SolverMode::kFactored);
+  return L_->MakeDenseMatrix();
+}
+
+template <typename BlockType>
+Eigen::PermutationMatrix<Eigen::Dynamic>
+BlockSparseCholeskySolver<BlockType>::CalcPermutationMatrix() const {
+  DRAKE_THROW_UNLESS(solver_mode() != SolverMode::kEmpty);
+  const std::vector<int>& p = scalar_permutation_.permutation();
+  return Eigen::PermutationMatrix<Eigen::Dynamic>(
+      Eigen::Map<const VectorX<int>>(p.data(), p.size()));
 }
 
 template <typename BlockType>
@@ -151,26 +163,29 @@ void BlockSparseCholeskySolver<BlockType>::SetScalarPermutation(
     const SymmetricMatrix& A, const std::vector<int>& elimination_ordering) {
   /* It's easier to build the scalar elimination ordering first from block
    elimination ordering and then convert it to the scalar permutation (the
-   inverse of the scalar elimination ordering). The scalar permutation maps the
-   right hand side of A * x = b to the right handside of
-   L * L^T * x_permuted = b_permuted. */
+   inverse of the scalar elimination ordering) that induces the permutation P
+   such that L⋅Lᵀ = P⋅A⋅Pᵀ. See CalcPermutationMatrix(). */
   std::vector<int> scalar_elimination_ordering(A.cols());
-  const BlockSparsityPattern& A_block_pattern = A.sparsity_pattern();
-  const std::vector<int>& A_block_sizes = A_block_pattern.block_sizes();
-  const std::vector<int>& starting_indices = A.starting_cols();
-  int scalar_order = 0;
-  for (int i = 0; i < ssize(elimination_ordering); ++i) {
-    const int block = elimination_ordering[i];
-    const int start = starting_indices[block];
-    const int size = A_block_sizes[block];
-    for (int s = start; s < start + size; ++s) {
-      scalar_elimination_ordering[scalar_order++] = s;
+  {
+    const BlockSparsityPattern& A_block_pattern = A.sparsity_pattern();
+    const std::vector<int>& A_block_sizes = A_block_pattern.block_sizes();
+    const std::vector<int>& starting_indices = A.starting_cols();
+    int i_permuted = 0;
+    for (int block_permuted = 0; block_permuted < ssize(elimination_ordering);
+         ++block_permuted) {
+      const int block = elimination_ordering[block_permuted];
+      const int start = starting_indices[block];
+      const int size = A_block_sizes[block];
+      for (int i = start; i < start + size; ++i) {
+        scalar_elimination_ordering[i_permuted++] = i;
+      }
     }
   }
   /* Invert the elimination ordering to get the permutation. */
   std::vector<int> scalar_permutation(scalar_elimination_ordering.size());
-  for (int i = 0; i < ssize(scalar_permutation); ++i) {
-    scalar_permutation[scalar_elimination_ordering[i]] = i;
+  for (int i_permuted = 0; i_permuted < ssize(scalar_permutation);
+       ++i_permuted) {
+    scalar_permutation[scalar_elimination_ordering[i_permuted]] = i_permuted;
   }
   scalar_permutation_ = PartialPermutation(std::move(scalar_permutation));
 }
@@ -179,14 +194,14 @@ template <typename BlockType>
 BlockSparsityPattern BlockSparseCholeskySolver<BlockType>::SymbolicFactor(
     const SymmetricMatrix& A, const std::vector<int>& elimination_ordering) {
   /* 1. Compute the block permutation as well as the scalar permutation. */
-  const int N = elimination_ordering.size();
+  const int n = elimination_ordering.size();
   /* Construct the inverse of the elimination ordering, which permutes the
    original indices to new indices. */
-  std::vector<int> permutation(N);
-  for (int i = 0; i < N; ++i) {
+  std::vector<int> permutation(n);
+  for (int i = 0; i < n; ++i) {
     permutation[elimination_ordering[i]] = i;
   }
-  PartialPermutation block_permutation(std::move(permutation));
+  const PartialPermutation block_permutation(std::move(permutation));
 
   /* Find the sparsity pattern of the permuted A (under the permutation induced
    by the elimination ordering). */
@@ -197,9 +212,9 @@ BlockSparsityPattern BlockSparseCholeskySolver<BlockType>::SymbolicFactor(
   std::vector<std::vector<int>> permuted_sparsity_pattern(
       sparsity_pattern.size());
   for (int i = 0; i < ssize(sparsity_pattern); ++i) {
-    const int pi = block_permutation.push(i);
+    const int pi = block_permutation.permuted_index(i);
     for (int j : sparsity_pattern[i]) {
-      const int pj = block_permutation.push(j);
+      const int pj = block_permutation.permuted_index(j);
       permuted_sparsity_pattern[std::min(pi, pj)].emplace_back(
           std::max(pi, pj));
     }
@@ -217,12 +232,12 @@ template <typename BlockType>
 void BlockSparseCholeskySolver<BlockType>::RightLookingSymmetricRank1Update(
     int j) {
   const std::vector<int>& blocks_in_col_j = L_->block_row_indices(j);
-  const int N = blocks_in_col_j.size();
+  const int n = blocks_in_col_j.size();
   /* We start from k = 1 here to skip the j,j diagonal entry. */
-  for (int k = 1; k < N; ++k) {
+  for (int k = 1; k < n; ++k) {
     const int col = blocks_in_col_j[k];
     const BlockType& B = L_->block_flat(k, j);
-    for (int l = k; l < N; ++l) {
+    for (int l = k; l < n; ++l) {
       const int row = blocks_in_col_j[l];
       const BlockType& A = L_->block_flat(l, j);
       L_->AddToBlock(row, col, -A * B.transpose());
@@ -233,11 +248,11 @@ void BlockSparseCholeskySolver<BlockType>::RightLookingSymmetricRank1Update(
 template <typename BlockType>
 void BlockSparseCholeskySolver<BlockType>::PermuteAndCopyToL(
     const SymmetricMatrix& A) {
-  const int N = A.block_cols();
-  DRAKE_DEMAND(N == block_permutation_.domain_size());
-  DRAKE_DEMAND(N == block_permutation_.permuted_domain_size());
+  const int n = A.block_cols();
+  DRAKE_DEMAND(n == block_permutation_.domain_size());
+  DRAKE_DEMAND(n == block_permutation_.permuted_domain_size());
   L_->SetZero();
-  for (int j = 0; j < N; ++j) {
+  for (int j = 0; j < n; ++j) {
     const std::vector<int>& row_indices = A.block_row_indices(j);
     for (int i : row_indices) {
       const BlockType& block = A.block(i, j);
