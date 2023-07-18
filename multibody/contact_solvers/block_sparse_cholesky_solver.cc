3b3dd3f868f394d8c7432977bebfefe6a0899732
Xuchen Han <xuchen.han@tri.global>
2023-07-18 16:55:42 -0700

    BlockSparseCholeskySolver computes Schur complement

--- multibody/contact_solvers/block_sparse_cholesky_solver.cc
+++ multibody/contact_solvers/block_sparse_cholesky_solver.cc
@@ -34,35 +34,137 @@ void BlockSparseCholeskySolver<BlockType>::UpdateMatrix(
 
 template <typename BlockType>
 bool BlockSparseCholeskySolver<BlockType>::Factor() {
-  DRAKE_THROW_UNLESS(solver_mode() == SolverMode::kAnalyzed);
-  for (int j = 0; j < L_->block_cols(); ++j) {
-    /* Update diagonal. */
-    const BlockType& Ajj = L_->diagonal_block(j);
-    L_diag_[j].compute(Ajj);
-    if (L_diag_[j].info() != Eigen::Success) {
-      solver_mode_ = SolverMode::kEmpty;
-      return false;
-    }
-    L_->SetBlockFlat(0, j, L_diag_[j].matrixL());
-    /* Update L₂₁ column.
-     | a₁₁  *  | = | λ₁₁  0 | * | λ₁₁ᵀ L₂₁ᵀ |
-     | a₂₁ a₂₂ |   | L₂₁ L₂₂|   |  0   L₂₂ᵀ |
-     So we have
-      L₂₁λ₁₁ᵀ = a₂₁, and thus
-      λ₁₁L₂₁ᵀ = a₂₁ᵀ */
-    const std::vector<int>& row_blocks = L_->block_row_indices(j);
-    const auto Ljj = L_diag_[j].matrixL();
-    /* We start from flat = 1 here to skip the j,j diagonal entry. */
-    for (int flat = 1; flat < ssize(row_blocks); ++flat) {
-      const BlockType& Aij = L_->block_flat(flat, j);
-      BlockType Lij = Ljj.solve(Aij.transpose()).transpose();
-      L_->SetBlockFlat(flat, j, std::move(Lij));
-    }
-    /* Update L₂₂ according to L₂₂ = a₂₂ - L₂₁⋅L₂₁ᵀ. */
-    RightLookingSymmetricRank1Update(j);
+  DRAKE_THROW_UNLESS(solver_mode_ == SolverMode::kAnalyzed);
+  const bool success = CalcPartialFactorization(0, L_->block_cols());
+  solver_mode_ = success ? SolverMode::kFactored : SolverMode::kEmpty;
+  return success;
+}
+
+template <typename BlockType>
+std::optional<MatrixX<double>>
+BlockSparseCholeskySolver<BlockType>::FactorAndCalcSchurComplement(
+    const SymmetricMatrix& A,
+    const std::unordered_set<int>& eliminated_blocks) {
+  const int num_eliminated_blocks = ssize(eliminated_blocks);
+  const int num_total_blocks = A.block_cols();
+  const int num_remaining_blocks = num_total_blocks - num_eliminated_blocks;
+  DRAKE_DEMAND(num_remaining_blocks >= 0);
+  if (num_remaining_blocks == 0) {
+    SetMatrix(A);
+    const bool success = Factor();
+    return success ? std::optional<MatrixX<double>>(MatrixX<double>::Zero(0, 0))
+                   : std::nullopt;
   }
-  solver_mode_ = SolverMode::kFactored;
-  return true;
+
+  /* The Schur complement of the `eliminated_blocks` appear on the bottom
+   right corner of the matrix under Cholesky factorization when the
+   `eliminated_blocks` have been factorized first with a right-looking
+   Cholesky. So we need an elimination ordering that ensures that the blocks
+   associated with `eliminated_blocks` are eliminated first. There are
+   many ordering that satisfy this requirement, and we look for one that reduces
+   fill-in. */
+  const std::vector<int> elimination_ordering =
+      ComputeMinimumDegreeOrdering(A.sparsity_pattern(), eliminated_blocks);
+  SetMatrixImpl(A, elimination_ordering,
+                SymbolicFactor(A, elimination_ordering));
+
+  /* Reset solver mode and exit if factorization of the eliminated blocks fails.
+   */
+  if (!CalcPartialFactorization(0, num_eliminated_blocks)) {
+    solver_mode_ = SolverMode::kEmpty;
+    return std::nullopt;
+  }
+
+  const MatrixX<double> permuted_S =
+      L_->MakeDenseBottomRightCorner(num_remaining_blocks)
+          .template selfadjointView<Eigen::Lower>();
+
+  /* Finish the factorization. If the factorization fails, set the solver mode
+   and exit. */
+  bool success =
+      CalcPartialFactorization(num_eliminated_blocks, L_->block_cols());
+  solver_mode_ = success ? SolverMode::kFactored : SolverMode::kEmpty;
+  if (!success) {
+    return std::nullopt;
+  }
+
+  /* The Schur complement computed here is permuted according to the elimination
+   ordering. We need to invert this permutation to obtain the original Schur
+   complement. To do that, we need to find the _scalar_ permutation for the
+   remaining blocks, and for that we need the _block_ permutation of the
+   remaining blocks as well as the sizes of those blocks. */
+
+  /* The block elimination ordering implies a permutation of the remaining
+   blocks. For example, suppose the elimination ordering is
+   [0, 5, 2, 4, 6, 3, 1], eliminated blocks are {0, 2, 5}, and the remaining
+   blocks are {1, 3, 4, 6} (notice how the eliminated blocks come before
+   remaining blocks). Then the induced elimination ordering on the remaining
+   blocks is [2, 3, 1, 0]. To see that, note that 1, 3, 4, 6 are the 0th, 1st,
+   2nd, 3rd elements in the remaining group; the order that they are eliminated
+   is
+
+     4 (2nd element in remaining blocks).
+     6 (3rd element in remaining blocks).
+     3 (1st element in remaining blocks).
+     1 (0th element in remaining blocks).
+
+   We name this elimination ordering on the remaining blocks
+   `remaining_ordering`. We also store the mapping from global indices to
+   remaining indices in `global_to_remaining` where a value of -1 indicates that
+   the element is eliminated.
+
+   In particular, in the above example, `remaining ordering` is [2, 3, 1, 0] and
+   `global_to_remaining` is [-1, 0, -1, 1, 2, -1, 3]. Note that
+   `global_to_remaining` assigns the set {1, 3, 4, 6} indices in ascending
+   order. */
+  std::vector<int> global_to_remaining(num_total_blocks, -1);
+  const std::vector<int>& block_sizes = A.sparsity_pattern().block_sizes();
+  std::vector<int> remaining_block_sizes;
+  remaining_block_sizes.reserve(num_remaining_blocks);
+  int remaining_index = 0;
+  for (int i = 0; i < num_total_blocks; ++i) {
+    if (eliminated_blocks.count(i) == 0) {
+      global_to_remaining[i] = remaining_index++;
+      remaining_block_sizes.emplace_back(block_sizes[i]);
+    }
+  }
+  /* The scalar index of the first scalar in each remaining block. */
+  std::vector<int> remaining_starting_indices(num_remaining_blocks);
+  remaining_starting_indices[0] = 0;
+  for (int i = 1; i < num_remaining_blocks; ++i) {
+    remaining_starting_indices[i] =
+        remaining_starting_indices[i - 1] + remaining_block_sizes[i - 1];
+  }
+  /* The block elimination ordering within remaining blocks. */
+  std::vector<int> remaining_ordering;
+  remaining_ordering.reserve(num_remaining_blocks);
+  /* We start with permuted_i = ssize(eliminated_blocks) to skip all
+   eliminated blocks. */
+  for (int permuted_i = ssize(eliminated_blocks);
+       permuted_i < ssize(elimination_ordering); ++permuted_i) {
+    const int i = elimination_ordering[permuted_i];
+    DRAKE_DEMAND(global_to_remaining[i] >= 0);
+    remaining_ordering.emplace_back(global_to_remaining[i]);
+  }
+  const int num_remaining_scalars =
+      remaining_starting_indices.back() + remaining_block_sizes.back();
+  /* Build the scalar elimination ordering (for scalars in the remaining blocks)
+   from the block elimination ordering. */
+  VectorX<int> scalar_remaining_ordering(num_remaining_scalars);
+  int new_scalar_index = 0;
+  for (int i = 0; i < ssize(remaining_ordering); ++i) {
+    const int block = remaining_ordering[i];
+    const int start = remaining_starting_indices[block];
+    const int size = remaining_block_sizes[block];
+    for (int s = start; s < start + size; ++s) {
+      scalar_remaining_ordering(new_scalar_index++) = s;
+    }
+  }
+  /* The elimination ordering is the inverse of the permutation. */
+  const auto& inverse_scalar_permutation = scalar_remaining_ordering;
+  Eigen::PermutationMatrix<Eigen::Dynamic> P_inv(inverse_scalar_permutation);
+  const auto P = P_inv.transpose();
+  return P_inv * permuted_S * P;
 }
 
 template <typename BlockType>
@@ -230,6 +332,41 @@ BlockSparsityPattern BlockSparseCholeskySolver<BlockType>::SymbolicFactor(
    new ordering. */
   return contact_solvers::internal::SymbolicCholeskyFactor(
       BlockSparsityPattern(permuted_block_sizes, permuted_sparsity_pattern));
+}
+
+template <typename BlockType>
+bool BlockSparseCholeskySolver<BlockType>::CalcPartialFactorization(
+    int starting_col_block, int ending_col_block) {
+  DRAKE_THROW_UNLESS(solver_mode() == SolverMode::kAnalyzed);
+  DRAKE_DEMAND(starting_col_block >= 0 &&
+               starting_col_block <= L_->block_cols());
+  DRAKE_DEMAND(ending_col_block >= 0 && ending_col_block <= L_->block_cols());
+  for (int j = starting_col_block; j < ending_col_block; ++j) {
+    /* Update diagonal. */
+    const BlockType& Ajj = L_->diagonal_block(j);
+    L_diag_[j].compute(Ajj);
+    if (L_diag_[j].info() != Eigen::Success) {
+      return false;
+    }
+    L_->SetBlockFlat(0, j, L_diag_[j].matrixL());
+    /* Update L₂₁ column.
+     | a₁₁  *  | = | λ₁₁  0 | * | λ₁₁ᵀ L₂₁ᵀ |
+     | a₂₁ a₂₂ |   | L₂₁ L₂₂|   |  0   L₂₂ᵀ |
+     So we have
+      L₂₁λ₁₁ᵀ = a₂₁, and thus
+      λ₁₁L₂₁ᵀ = a₂₁ᵀ */
+    const std::vector<int>& row_blocks = L_->block_row_indices(j);
+    const auto Ljj = L_diag_[j].matrixL();
+    /* We start from flat = 1 here to skip the j,j diagonal entry. */
+    for (int flat = 1; flat < ssize(row_blocks); ++flat) {
+      const BlockType& Aij = L_->block_flat(flat, j);
+      BlockType Lij = Ljj.solve(Aij.transpose()).transpose();
+      L_->SetBlockFlat(flat, j, std::move(Lij));
+    }
+    /* Update L₂₂ according to L₂₂ = a₂₂ - L₂₁⋅L₂₁ᵀ. */
+    RightLookingSymmetricRank1Update(j);
+  }
+  return true;
 }
 
 template <typename BlockType>
