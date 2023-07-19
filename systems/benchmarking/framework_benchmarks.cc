c77b0d9fa74233617089e057131dd282933e53c9
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-07-19 10:29:52 -0700

    [framework] Add DiagramBuilder::Build benchmark

--- systems/benchmarking/framework_benchmarks.cc
+++ systems/benchmarking/framework_benchmarks.cc
@@ -1,6 +1,7 @@
 #include <benchmark/benchmark.h>
 
 #include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/primitives/adder.h"
 #include "drake/systems/primitives/pass_through.h"
 #include "drake/tools/performance/fixture_common.h"
 
@@ -55,6 +56,66 @@ BENCHMARK_F(BasicFixture, PassThrough3)(benchmark::State& state) {
     output.Eval(*context_);
   }
 }
+
+// Helper function for the DiagramBuild benchmark. Creates a diagram containing
+// num_systems subsystems.  When depth==0, each subsystem is an Adder, otherwise
+// each subsystem is a recursive self-call with the next smaller depth.
+std::unique_ptr<DiagramBuilder<double>> MakeDiagramBuilder(int num_systems,
+                                                           int depth) {
+  DRAKE_DEMAND(num_systems > 0);
+  DRAKE_DEMAND(depth >= 0);
+  auto builder = std::make_unique<DiagramBuilder<double>>();
+  std::vector<System<double>*> children;
+  for (int i = 0; i < num_systems; ++i) {
+    if (depth == 0) {
+      children.push_back(builder->AddSystem<Adder>(num_systems, 1));
+    } else {
+      children.push_back(builder->AddSystem(
+          MakeDiagramBuilder(num_systems, depth - 1)->Build()));
+    }
+    System<double>* child = children.back();
+    // The child's input ports are connected either to the other childrens'
+    // output ports (when possible), or else the diagram's input ports.
+    for (int j = 0; j < num_systems; ++j) {
+      const InputPort<double>& input = child->get_input_port(j);
+      if (j < i) {
+        builder->Connect(children.at(j)->get_output_port(), input);
+      } else {
+        if (i == 0) {
+          builder->ExportInput(input, fmt::to_string(j));
+        } else {
+          builder->ConnectInput(fmt::to_string(j), input);
+        }
+      }
+    }
+  }
+  builder->ExportOutput(children.back()->get_output_port());
+  return builder;
+}
+
+void DiagramBuild(benchmark::State& state) {  // NOLINT
+  const int num_systems = state.range(0);
+  const int depth = state.range(1);
+  std::unique_ptr<DiagramBuilder<double>> builder;
+  std::unique_ptr<Diagram<double>> diagram;
+  for (auto _ : state) {
+    // Create a DiagramBuilder, sans timekeeping.
+    state.PauseTiming();
+    diagram.reset();
+    builder = MakeDiagramBuilder(num_systems, depth);
+    state.ResumeTiming();
+
+    // Time the Build operation.
+    diagram = builder->Build();
+  }
+}
+
+BENCHMARK(DiagramBuild)
+    ->Unit(benchmark::kMillisecond)
+    ->Args({3, 0})
+    ->Args({30, 0})
+    ->Args({3, 1})
+    ->Args({3, 2});
 
 }  // namespace
 }  // namespace systems
