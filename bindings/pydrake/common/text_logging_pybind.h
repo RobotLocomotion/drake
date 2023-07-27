74ba41031d950ae7c80165fc128585b983756562
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-07-27 13:32:55 -0700

    [py common] Add use_native_cpp_logging feature

--- bindings/pydrake/common/text_logging_pybind.h
+++ bindings/pydrake/common/text_logging_pybind.h
@@ -4,8 +4,14 @@ namespace drake {
 namespace pydrake {
 namespace internal {
 
-/* If possible, redirects Drake's C++ logs to Python's `logging` module. */
+/* If possible, redirects Drake's C++ logs to Python's `logging` module. This
+function is not thread-safe; no other threads should be running when it is
+called. */
 void MaybeRedirectPythonLogging();
+
+/* Implements pydrake.common.use_native_cpp_logging(). This function is not
+thread-safe; no other threads should be running when it is called. */
+void UseNativeCppLogging();
 
 }  // namespace internal
 }  // namespace pydrake
