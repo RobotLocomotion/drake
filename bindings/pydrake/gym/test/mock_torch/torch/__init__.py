47959d8e46ed9a83332da94cc6901d4d82323d58
Jose Barreiros <jose.barreiros@tri.global>
2023-07-27 21:08:06 -0400

    add comments and remove connection patch

--- bindings/pydrake/gym/test/mock_torch/torch/__init__.py
+++ bindings/pydrake/gym/test/mock_torch/torch/__init__.py
@@ -1,4 +1,7 @@
-# This is a mock version of torch ...
+# This is a mock version of torch needed for
+# stable_baselines3_internal which is used
+# for testing.
+# This does not affect the functionality of env_checker.check_env()
 
 from . import distributions
 from . import nn
