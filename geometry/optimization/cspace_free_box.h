d7d8e2829c74d848467a1709f12df93af436b60d
Hongkai Dai <daihongkai@gmail.com>
2023-07-31 20:37:29 -0700

    Search C-space box given Lagrangians.

--- geometry/optimization/cspace_free_box.h
+++ geometry/optimization/cspace_free_box.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <memory>
 #include <optional>
 #include <unordered_map>
 #include <vector>
@@ -140,6 +141,29 @@ class CspaceFreeBox : public CspaceFreePolytopeBase {
     SeparationCertificate certificate;
   };
 
+  struct FindBoxGivenLagrangianOptions {
+    /** When we solve an optimization problem with a cost function, after we
+     find the optimal solution, we will "back off" a bit to find a strictly
+     feasible solution. backoff_scale=0 means no back off. backoff_scale should
+     be between [0, 1]. We recommend using a small backoff_scale value during
+     bilinear alternation. Without backoff, the optimal solution is only
+     marginally feasible, which could make the next iteration of bilinear
+     alternation infeasible.
+     */
+    std::optional<double> backoff_scale{std::nullopt};
+
+    /** ID for the solver */
+    solvers::SolverId solver_id{solvers::MosekSolver::id()};
+
+    /** options for solving the MathematicalProgram */
+    std::optional<solvers::SolverOptions> solver_options{std::nullopt};
+
+    /** We can constrain the C-space box {q | q_box_lower<=q<=q_box_upper}
+     to contain some sampled q. Each column of q_inner_pts is a sample of q.
+     */
+    std::optional<Eigen::MatrixXd> q_inner_pts;
+  };
+
   /**
    @param plant The plant for which we compute the C-space free boxes. It
    must outlive this CspaceFreeBox object.
@@ -163,6 +187,10 @@ class CspaceFreeBox : public CspaceFreePolytopeBase {
    * @param q_box_upper The upper bound of the C-space box.
    * @param ignored_collision_pairs We ignore the pair of geometries in
    * `ignored_collision_pairs`.
+   * @param[out] q_star The tangent-configuration variable s is defined as s =
+   * tan((q - q_star)/2) for the revolute joint, where it depends on q_star.
+   * Note that q_star depends on the box parameter q_box_lower and q_box_upper,
+   * we set q_star to the center of the C-space box.
    * @param[out] certificates Contains the certificate we successfully found for
    * each pair of geometries. Notice that depending on `options`, the program
    * could search for the certificate for each geometry pair in parallel, and
@@ -177,13 +205,45 @@ class CspaceFreeBox : public CspaceFreePolytopeBase {
       const Eigen::Ref<const Eigen::VectorXd>& q_box_lower,
       const Eigen::Ref<const Eigen::VectorXd>& q_box_upper,
       const IgnoredCollisionPairs& ignored_collision_pairs,
-      const FindSeparationCertificateOptions& options,
+      const FindSeparationCertificateOptions& options, Eigen::VectorXd* q_star,
       std::unordered_map<SortedPair<geometry::GeometryId>,
                          SeparationCertificateResult>* certificates) const;
+
+  /**
+   Given certificates certifying the C-space free box {s | s_box_lower <= s <=
+   s_box_upper}, this method constructs a program to search for a new C-space
+   box {s | s_box_lower' <= s <= s_box_upper'} such that this box is collision
+   free. This program treats s_box_lower and s_box_upper as decision variables,
+   and searches for the separating planes between each pair of geometries. Note
+   that this program doesn't contain any cost yet.
+   @param certificates The return of
+   FindSeparationCertificateGivenBox().
+   @param[out] s_box_lower The C-space box is parameterized as {s | s_box_lower
+   <= s <= s_box_upper}.
+   @param[out] s_box_upper The C-space box is parameterized as {s | s_box_lower
+   <= s <= s_box_upper}.
+   */
+  [[nodiscard]] std::unique_ptr<solvers::MathematicalProgram>
+  InitializeBoxSearchProgram(
+      const IgnoredCollisionPairs& ignored_collision_pairs,
+      const Eigen::Ref<const Eigen::VectorXd>& q_star,
+      const std::unordered_map<SortedPair<geometry::GeometryId>,
+                               SeparationCertificateResult>& certificates,
+      VectorX<symbolic::Variable>* s_box_lower,
+      VectorX<symbolic::Variable>* s_box_upper) const;
 
  private:
   // Forward declare the tester class that will test the private members.
   friend class CspaceFreeBoxTester;
+
+  struct FindBoxGivenLagrangianResult {
+    Eigen::VectorXd s_box_lower;
+    Eigen::VectorXd s_box_upper;
+    // a[i].dot(x) + b[i] = 0 is the separation plane for
+    // this->separating_planes()[i].
+    std::unordered_map<int, Vector3<symbolic::Polynomial>> a;
+    std::unordered_map<int, symbolic::Polynomial> b;
+  };
 
   /*
    Computes the range of s from the box q_box_lower <= q <= q_box_upper. We also
@@ -226,6 +286,16 @@ class CspaceFreeBox : public CspaceFreePolytopeBase {
       PolynomialsToCertify* certify_polynomials) const;
 
   /*
+   Generates all the PlaneSeparatesGeometries structs that we need to verify.
+   @param plane_geometries_vec[out] contain the rational function that needs to
+   be non-negative within the C-space box.
+   */
+  void GeneratePlaneGeometriesVec(
+      const Eigen::Ref<const Eigen::VectorXd>& q_star,
+      const IgnoredCollisionPairs& ignored_collision_pairs,
+      std::vector<PlaneSeparatesGeometries>* plane_geometries_vec) const;
+
+  /*
    Constructs the program which searches for the plane separating a pair of
    geometries, for all configuration in the box {q | q_box_lower <= q <=
    q_box_upper}.
@@ -254,12 +324,11 @@ class CspaceFreeBox : public CspaceFreePolytopeBase {
    The geometry pair which certificates[i] certifies is given by
    separating_planes()[certificates[i].plane_index].geometry_pair().
    */
-  [[nodiscard]] std::vector<std::optional<SeparationCertificateResult>>
-  FindSeparationCertificateGivenBox(
-      const IgnoredCollisionPairs& ignored_collision_pairs,
-      const Eigen::Ref<const Eigen::VectorXd>& q_box_lower,
-      const Eigen::Ref<const Eigen::VectorXd>& q_box_upper,
-      const FindSeparationCertificateOptions& options) const;
+  void FindSeparationCertificateGivenBoxImpl(
+      const PolynomialsToCertify& polynomials_to_certify,
+      const FindSeparationCertificateOptions& options,
+      std::vector<std::optional<SeparationCertificateResult>>* certificates_vec)
+      const;
 
   /*
    Adds the constraint that each column of s_inner_pts is in the box s_box_lower
@@ -269,7 +338,91 @@ class CspaceFreeBox : public CspaceFreePolytopeBase {
                                const VectorX<symbolic::Variable>& s_box_lower,
                                const VectorX<symbolic::Variable>& s_box_upper,
                                const Eigen::MatrixXd& s_inner_pts) const;
+
+  /* When we fix the Lagrangian multipliers and search for the C-space box
+   { s | s_box_lower <= s <= s_box_upper}, we count the total size of all Gram
+   matrices in the SOS program.
+   */
+  [[nodiscard]] int GetGramVarSizeForBoxSearchProgram(
+      const std::vector<PlaneSeparatesGeometries>& plane_geometries_vec) const;
+
+  /*
+   Overload InitializeBoxSearchProgram.
+   This overloaded function use input arguments that are constructed in other
+   private functions. Some of these input arguments can be re-used if we call
+   this InitializeBoxSearchProgram repeatedly (for example in bilinear search).
+   @param polynoials_to_certify Check the output argument of
+   GeneratePolynomialsToCertify.
+   @param certificates_vec This is the output of
+   FindSeparationCertificateGivenBox. It contains the Lagrangian multipliers
+   which will be fixed when we search for the C-space box.
+   @param s_box_lower The decision variables for the box lower limits.
+   @param s_box_upper The decision variables for the box upper limits.
+   @param s_minus_s_box_lower The polynomial representing s - s_box_lower.
+   @param s_box_upper_minus_s The polynomial representing s_box_upper - s.
+   @param gram_total_size The output of GetGramVarSizeForBoxSearchProgram.
+   */
+  [[nodiscard]] std::unique_ptr<solvers::MathematicalProgram>
+  InitializeBoxSearchProgram(
+      const Eigen::Ref<const Eigen::VectorXd>& q_star,
+      const std::vector<PlaneSeparatesGeometries>& plane_geometries_vec,
+      const std::vector<std::optional<SeparationCertificateResult>>&
+          certificates_vec,
+      const Eigen::Ref<const VectorX<symbolic::Variable>>& s_box_lower,
+      const Eigen::Ref<const VectorX<symbolic::Variable>>& s_box_upper,
+      const Eigen::Ref<const VectorX<symbolic::Polynomial>>&
+          s_minus_s_box_lower,
+      const Eigen::Ref<const VectorX<symbolic::Polynomial>>&
+          s_box_upper_minus_s,
+      int gram_total_size) const;
+
+  /*
+   Adds the constraint s_joint_limit_lower <= s_box_lower <= s_box_upper <=
+   s_joint_limit_upper.
+   */
+  void AddBoxInJointLimitConstraint(
+      solvers::MathematicalProgram* prog,
+      const Eigen::Ref<const Eigen::VectorXd>& q_star,
+      const VectorX<symbolic::Variable>& s_box_lower,
+      const VectorX<symbolic::Variable>& s_box_upper) const;
+
+  /*
+   @param box_volume_delta Adds the cost -power(∏ᵢ(s_box_upper(i) -
+   s_box_lower(i) + δ(i)), 1/n) where n is the dimensionality of s. When δ = 0,
+   this cost is the volume of the box { s | s_box_lower <= s <= s_box_upper}. If
+   δ(i) is larger than δ(j), then the cost will try to focus more on enlarging
+   s_box_upper(j) - s_box_lower(j), compared to s_box_upper(i) - s_box_lower(i).
+   δ should be elementwise non-negative.
+   */
+  [[nodiscard]] std::optional<FindBoxGivenLagrangianResult>
+  FindBoxGivenLagrangian(
+      const Eigen::Ref<const Eigen::VectorXd>& q_star,
+      const std::vector<PlaneSeparatesGeometries>& plane_geometries_vec,
+      const std::vector<std::optional<SeparationCertificateResult>>&
+          certificates_vec,
+      const Eigen::Ref<const VectorX<symbolic::Variable>>& s_box_lower,
+      const Eigen::Ref<const VectorX<symbolic::Variable>>& s_box_upper,
+      const Eigen::Ref<const VectorX<symbolic::Polynomial>>&
+          s_minus_s_box_lower,
+      const Eigen::Ref<const VectorX<symbolic::Polynomial>>&
+          s_box_upper_minus_s,
+      int gram_total_size, const Eigen::VectorXd& box_volume_delta,
+      const FindBoxGivenLagrangianOptions& options) const;
 };
+
+/*
+ Adds the cost -power(∏ᵢ(s_box_upper(i) - s_box_lower(i) + δ(i)), 1/n) where n
+ is the dimensionality of s. When δ = 0, this cost is the volume of the box {
+ s | s_box_lower <= s <= s_box_upper}.
+ @param delta δ in the documentation above. If δ(i) is larger than δ(j), then
+ the cost will try to focus more on enlarging s_box_upper(j) -
+ s_box_lower(j), compared to s_box_upper(i) - s_box_lower(i).
+ δ should be elementwise non-negative.
+ */
+void AddMaximizeBoxVolumeCost(solvers::MathematicalProgram* prog,
+                              const VectorX<symbolic::Variable>& s_box_lower,
+                              const VectorX<symbolic::Variable>& s_box_upper,
+                              const Eigen::VectorXd& delta);
 }  // namespace optimization
 }  // namespace geometry
 }  // namespace drake
