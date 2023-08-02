42dd2ceebc163753e995d14de299568693196fa9
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-08-02 10:30:35 -0700

    Clean up some lingering remnants of unsugared visualization (#19910)

--- examples/kuka_iiwa_arm/kuka_simulation.cc
+++ examples/kuka_iiwa_arm/kuka_simulation.cc
@@ -14,7 +14,6 @@
 #include "drake/examples/kuka_iiwa_arm/iiwa_common.h"
 #include "drake/examples/kuka_iiwa_arm/iiwa_lcm.h"
 #include "drake/examples/kuka_iiwa_arm/kuka_torque_controller.h"
-#include "drake/geometry/drake_visualizer.h"
 #include "drake/geometry/scene_graph.h"
 #include "drake/lcmt_iiwa_command.hpp"
 #include "drake/lcmt_iiwa_status.hpp"
@@ -30,6 +29,7 @@
 #include "drake/systems/lcm/lcm_subscriber_system.h"
 #include "drake/systems/primitives/demultiplexer.h"
 #include "drake/systems/primitives/discrete_derivative.h"
+#include "drake/visualization/visualization_config_functions.h"
 
 DEFINE_double(simulation_sec, std::numeric_limits<double>::infinity(),
               "Number of seconds to simulate.");
@@ -74,7 +74,8 @@ int DoMain() {
 
   // Creates and adds LCM publisher for visualization.
   auto lcm = builder.AddSystem<systems::lcm::LcmInterfaceSystem>();
-  geometry::DrakeVisualizerd::AddToBuilder(&builder, scene_graph, lcm);
+  visualization::ApplyVisualizationConfig({}, &builder, nullptr, nullptr,
+                                          nullptr, nullptr, lcm);
 
   // Since we welded the model to the world above, the only remaining joints
   // should be those in the arm.
