d717790fdea19c4e7cdf473fc15522be2762201b
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-08-07 10:53:49 -0700

    [yaml] Fix round-trip bugs with variant<string,...> (#19954)

--- systems/sensors/test/camera_config_test.cc
+++ systems/sensors/test/camera_config_test.cc
@@ -268,6 +268,12 @@ GTEST_TEST(CameraConfigTest, SerializeBackgroundRgba) {
   EXPECT_EQ(yaml, "background:\n  rgba: [0.1, 0.2, 0.3, 0.4]\n");
 }
 
+GTEST_TEST(CameraConfigTest, SerializationDefaultRoundTrip) {
+  const CameraConfig original;
+  const std::string yaml = SaveYamlString<CameraConfig>(original);
+  EXPECT_NO_THROW(LoadYamlString<CameraConfig>(yaml)) << "with yaml:\n" << yaml;
+}
+
 // Various tests to support the documented examples in the documentation. The
 // number of each parsed config should correspond to the number in the docs for
 // the renderer_class field.
