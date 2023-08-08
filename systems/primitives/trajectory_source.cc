ecc99b7e5700425ca88ee4126d5e38fbe3466080
Russ Tedrake <russt@mit.edu>
2023-08-08 18:21:48 -0400

    Support default scalars for TrajectorySource

--- systems/primitives/trajectory_source.cc
+++ systems/primitives/trajectory_source.cc
@@ -1,6 +1,10 @@
 #include "drake/systems/primitives/trajectory_source.h"
 
+#include <limits>
+
 #include "drake/common/drake_assert.h"
+#include "drake/common/ssize.h"
+#include "drake/math/autodiff_gradient.h"
 
 namespace drake {
 namespace systems {
@@ -11,8 +15,9 @@ template <typename T>
 TrajectorySource<T>::TrajectorySource(const Trajectory<T>& trajectory,
                                       int output_derivative_order,
                                       bool zero_derivatives_beyond_limits)
-    : SingleOutputVectorSource<T>(trajectory.rows() *
-                                  (1 + output_derivative_order)),
+    : SingleOutputVectorSource<T>(
+          SystemTypeTag<TrajectorySource>{},
+          trajectory.rows() * (1 + output_derivative_order)),
       // Make a copy of the input trajectory.
       trajectory_(trajectory.Clone()),
       clamp_derivatives_(zero_derivatives_beyond_limits) {
@@ -30,9 +35,29 @@ TrajectorySource<T>::TrajectorySource(const Trajectory<T>& trajectory,
 }
 
 template <typename T>
+template <typename U>
+TrajectorySource<T>::TrajectorySource(const TrajectorySource<U>& other)
+    : SingleOutputVectorSource<T>(
+          SystemTypeTag<TrajectorySource>{},
+          other.trajectory_->rows() * (1 + ssize(other.derivatives_))),
+      clamp_derivatives_{other.clamp_derivatives_} {
+  if constexpr (std::is_same_v<U, double>) {
+    failsafe_trajectory_ = other.trajectory_->Clone();
+    for (int i = 0; i < ssize(other.derivatives_); ++i) {
+      failsafe_derivatives_.emplace_back(other.derivatives_[i]->Clone());
+    }
+  } else {
+    throw std::runtime_error(
+        "TrajectorySource only supports scalar conversion from double.");
+  }
+}
+
+template <typename T>
 void TrajectorySource<T>::UpdateTrajectory(
     const trajectories::Trajectory<T>& trajectory) {
-  DRAKE_DEMAND(trajectory.rows() == trajectory_->rows());
+  DRAKE_DEMAND(trajectory_ != nullptr || failsafe_trajectory_ != nullptr);
+  int rows = trajectory_ ? trajectory_->rows() : failsafe_trajectory_->rows();
+  DRAKE_DEMAND(trajectory.rows() == rows);
   DRAKE_DEMAND(trajectory.cols() == 1);
 
   trajectory_ = trajectory.Clone();
@@ -42,29 +67,59 @@ void TrajectorySource<T>::UpdateTrajectory(
     else
       derivatives_[i] = derivatives_[i - 1]->MakeDerivative();
   }
+  failsafe_trajectory_ = nullptr;
+  failsafe_derivatives_.clear();
 }
 
 template <typename T>
 void TrajectorySource<T>::DoCalcVectorOutput(
     const Context<T>& context, Eigen::VectorBlock<VectorX<T>>* output) const {
-  int len = trajectory_->rows();
-  output->head(len) = trajectory_->value(context.get_time());
+  DRAKE_DEMAND(trajectory_ != nullptr || failsafe_trajectory_ != nullptr);
+  int len = trajectory_ ? trajectory_->rows() : failsafe_trajectory_->rows();
+  T time = context.get_time();
+  double maybe_time_as_double{std::numeric_limits<double>::quiet_NaN()};
+  if (trajectory_ != nullptr) {
+    output->head(len) = trajectory_->value(time);
+  } else {
+    // TODO(russt): We are missing DiscardZeroGradient for scalars, and/or
+    // a version of ExtractDoubleOrThrow which throws on non-zero gradients.
+    if constexpr (std::is_same_v<T, AutoDiffXd>) {
+      if (time.derivatives().size() != 0 && !time.derivatives().isZero()) {
+        throw std::runtime_error(
+            "Cannot take gradients w.r.t. time with this TrajectorySource. You "
+            "must call UpdateTrajectory() to provide a "
+            "Trajectory<AutoDiffXd>.");
+      }
+    }
+    maybe_time_as_double = ExtractDoubleOrThrow(context.get_time());
+    output->head(len) = failsafe_trajectory_->value(maybe_time_as_double);
+  }
 
-  double time = context.get_time();
-  bool set_zero = clamp_derivatives_ && (time > trajectory_->end_time() ||
-                                         time < trajectory_->start_time());
+  T start_time = trajectory_ ? trajectory_->start_time()
+                             : failsafe_trajectory_->start_time();
+  T end_time =
+      trajectory_ ? trajectory_->end_time() : failsafe_trajectory_->end_time();
+  boolean<T> set_zero =
+      boolean<T>(clamp_derivatives_) && (time > end_time || time < start_time);
 
-  for (size_t i = 0; i < derivatives_.size(); ++i) {
-    if (set_zero) {
-      output->segment(len * (i + 1), len).setZero();
-    } else {
-      output->segment(len * (i + 1), len) =
-          derivatives_[i]->value(context.get_time());
+  DRAKE_DEMAND(derivatives_.size() == 0 || failsafe_derivatives_.size() == 0);
+  for (int i = 0; i < ssize(derivatives_); ++i) {
+    VectorX<T> value = derivatives_[i]->value(context.get_time());
+    for (int j = 0; j < len; ++j) {
+      (*output)[len * (i + 1) + j] = if_then_else(set_zero, T(0), value[j]);
+    }
+  }
+  for (int i = 0; i < ssize(failsafe_derivatives_); ++i) {
+    DRAKE_DEMAND(!std::isnan(maybe_time_as_double));
+    VectorX<T> value = failsafe_derivatives_[i]->value(maybe_time_as_double);
+    for (int j = 0; j < len; ++j) {
+      (*output)[len * (i + 1) + j] = if_then_else(set_zero, T(0), value[j]);
     }
   }
 }
 
-template class TrajectorySource<double>;
-
 }  // namespace systems
 }  // namespace drake
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
+    class ::drake::systems::TrajectorySource)
--- systems/primitives/trajectory_source.h
+++ systems/primitives/trajectory_source.h
@@ -26,8 +26,12 @@ namespace systems {
 /// - y0
 /// @endsystem
 ///
-/// @tparam_double_only
-/// @ingroup primitive_systems
+/// Note: Scalar conversion is supported, but the stored Trajectory is not
+/// automatically scalar converted. You must call UpdateTrajectory() with an
+/// updated Trajectory<T> in order to fully-enable scalar-type support on the
+/// trajectory parameters/values.
+///
+/// @tparam_default_scalar @ingroup primitive_systems
 template <typename T>
 class TrajectorySource final : public SingleOutputVectorSource<T> {
  public:
@@ -37,12 +41,17 @@ class TrajectorySource final : public SingleOutputVectorSource<T> {
   /// @param output_derivative_order The number of times to take the derivative.
   /// Must be greater than or equal to zero.
   /// @param zero_derivatives_beyond_limits All derivatives will be zero before
-  /// the start time or after the end time of @p trajectory.
+  /// the start time or after the end time of @p trajectory. However, this
+  /// clamping is ignored for T=Expression.
   /// @pre The value of `trajectory` is a column vector. More precisely,
   /// trajectory.cols() == 1.
   explicit TrajectorySource(const trajectories::Trajectory<T>& trajectory,
                             int output_derivative_order = 0,
                             bool zero_derivatives_beyond_limits = true);
+
+  // Scalar-converting copy constructor.  See @ref system_scalar_conversion.
+  template <typename U>
+  explicit TrajectorySource(const TrajectorySource<U>& other);
 
   ~TrajectorySource() final = default;
 
@@ -51,6 +60,10 @@ class TrajectorySource final : public SingleOutputVectorSource<T> {
   void UpdateTrajectory(const trajectories::Trajectory<T>& trajectory);
 
  private:
+  // TrajectorySource of one scalar type is friends with all other scalar types.
+  template <typename>
+  friend class TrajectorySource;
+
   // Outputs a vector of values evaluated at the context time of the trajectory
   // and up to its Nth derivatives, where the trajectory and N are passed to
   // the constructor. The size of the vector is:
@@ -59,10 +72,23 @@ class TrajectorySource final : public SingleOutputVectorSource<T> {
   void DoCalcVectorOutput(const Context<T>& context,
                           Eigen::VectorBlock<VectorX<T>>* output) const final;
 
-  std::unique_ptr<trajectories::Trajectory<T>> trajectory_;
+  std::unique_ptr<trajectories::Trajectory<T>> trajectory_{};
   const bool clamp_derivatives_;
-  std::vector<std::unique_ptr<trajectories::Trajectory<T>>> derivatives_;
+  std::vector<std::unique_ptr<trajectories::Trajectory<T>>> derivatives_{};
+
+  // These are used when trajectory_ and derivatives_ are nullptr, e.g.,
+  // after a scalar conversion. Scalar converting double => non-double ends
+  // up cloning the T=double trajectory here, until UpdateTrajectory is used
+  // to replace it.
+  // TODO(russt): Remove these if/when we support automatic scalar conversion
+  // of Trajectory classes.
+  std::unique_ptr<trajectories::Trajectory<double>> failsafe_trajectory_{};
+  std::vector<std::unique_ptr<trajectories::Trajectory<double>>>
+      failsafe_derivatives_{};
 };
 
 }  // namespace systems
 }  // namespace drake
+
+DRAKE_DECLARE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
+    class ::drake::systems::TrajectorySource)
