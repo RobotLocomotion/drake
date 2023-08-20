4a50300785de95232237e722fd8c976a28ac5308
Rick Poyner <rick.poyner@tri.global>
2023-08-20 17:04:23 -0400

    [common] Fix hangs with MaybePauseForUser

--- common/test_utilities/maybe_pause_for_user.h
+++ common/test_utilities/maybe_pause_for_user.h
@@ -6,7 +6,19 @@ namespace common {
 /** Pauses execution for the user to examine visualization output, for use
  within bazel unit tests. This function will not pause execution when running
  as a bazel test, but when running as a command-line executable, it will enable
- users to see the visualization outputs. */
+ users to see the visualization outputs.
+
+ The complete behavior is somewhat more subtle, depending on the bazel rules
+ used to build the program, and the way it is invoked:
+
+ - built with a bazel test rule
+   - invoked with "bazel test" -- does not pause
+   - invoked with "bazel run" -- does not pause
+   - invoked directly ("bazel/bin/[PROGRAM]") -- does pause
+ - built with a bazel binary rule
+   - invoked with "bazel run" -- does pause
+   - invoked directly ("bazel/bin/[PROGRAM]") -- does pause
+*/
 void MaybePauseForUser();
 
 }  // namespace common
