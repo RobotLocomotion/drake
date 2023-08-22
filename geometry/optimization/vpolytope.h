3df9922e5891fecb848219a41d4ac5d7495e8802
Thomas Cohn <cohnt@umich.edu>
2023-08-21 21:36:14 -0400

    Handle converting HPolyhedron to VPolytope if it is not full dimensional. (#20001)

--- geometry/optimization/vpolytope.h
+++ geometry/optimization/vpolytope.h
@@ -6,6 +6,7 @@
 #include <utility>
 #include <vector>
 
+#include "drake/geometry/optimization/affine_subspace.h"
 #include "drake/geometry/optimization/convex_set.h"
 #include "drake/geometry/optimization/hpolyhedron.h"
 #include "drake/math/rigid_transform.h"
@@ -44,11 +45,16 @@ class VPolytope final : public ConvexSet {
   @pydrake_mkdoc_identifier{vertices} */
   explicit VPolytope(const Eigen::Ref<const Eigen::MatrixXd>& vertices);
 
-  /** Constructs the polytope from a bounded polyhedron (using Qhull).
+  /** Constructs the polytope from a bounded polyhedron (using Qhull). If the
+  HPolyhedron is not full-dimensional, we perform computations in a coordinate
+  system of its affine hull. `tol` specifies the numerical tolerance used in the
+  computation of the affine hull. See the documentation of AffineSubspace for
+  more details. A loose tolerance is necessary for the built-in solvers, but a
+  tighter tolerance can be used with commercial solvers (e.g. Gurobi and Mosek).
   @throws std::runtime_error if H is unbounded or if Qhull terminates with an
   error.
   @pydrake_mkdoc_identifier{hpolyhedron} */
-  explicit VPolytope(const HPolyhedron& H);
+  explicit VPolytope(const HPolyhedron& H, const double tol = 1e-9);
 
   /** Constructs the polytope from a SceneGraph geometry.
   @pydrake_mkdoc_identifier{scenegraph} */
