57c4ccbed6500d169af0862848b37307f1ae9dc7
Rick Poyner <rick.poyner@tri.global>
2023-08-24 13:45:07 -0400

    wip: first draft, with trivial test

--- geometry/hydroelastize.h
+++ geometry/hydroelastize.h
@@ -7,6 +7,7 @@ namespace geometry {
 
 /** Iterate over all of the geometries of a scene graph, ensuring that
     all geometries have hydroelastic properties.
+    @pre scene_graph is not nullptr.
  */
 template <typename T>
 void Hydroelastize(SceneGraph<T>* scene_graph);
