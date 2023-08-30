d5b65935a91c9d694f68cf1a2a9adcc3720e4593
Rick Poyner <rick.poyner@tri.global>
2023-08-30 16:13:37 -0400

    wip: clean up hydroelastize for internal use

--- geometry/hydroelastize.h
+++ geometry/hydroelastize.h
@@ -4,21 +4,17 @@
 
 namespace drake {
 namespace geometry {
+namespace internal {
 
 /** Iterate over all of the geometries of a scene graph, ensuring that
     all geometries have hydroelastic properties.
     @pre scene_graph is not nullptr.
  */
-// template <typename T>
-// void Hydroelastize(SceneGraph<T>* scene_graph);
-
-namespace internal {
 template <typename T>
 void Hydroelastize(GeometryState<T>* geometry_state);
-}  // namespace internal
-
 // TODO(rpoyner-tri): gain access to configuration values for hydro properties
 // TODO(rpoyner-tri): ignore "tiny bubbles"
 
+}  // namespace internal
 }  // namespace geometry
 }  // namespace drake
