26e15fe49d5a278f953a9067416670bb988be031
Hongkai Dai <daihongkai@gmail.com>
2023-08-31 21:36:33 -0700

    Add InCollisionConstraint.

--- multibody/inverse_kinematics/minimum_distance_constraint.h
+++ multibody/inverse_kinematics/minimum_distance_constraint.h
@@ -205,6 +205,10 @@ class MinimumDistanceConstraint final : public solvers::Constraint {
     return minimum_value_constraint_->influence_value();
   }
 
+  const solvers::MinimumValueConstraint& minimum_value_constraint() const {
+    return *minimum_value_constraint_;
+  }
+
  private:
   void DoEval(const Eigen::Ref<const Eigen::VectorXd>& x,
               Eigen::VectorXd* y) const override;
@@ -249,6 +253,84 @@ class MinimumDistanceConstraint final : public solvers::Constraint {
 
   const planning::CollisionChecker* collision_checker_;
   planning::CollisionCheckerContext* collision_checker_context_;
+};
+
+/** Constraint min(d) <= ub. Namely at least one of the signed distance between
+ all candidate pairs of geometries is below ub. When ub=0, it means the robot
+ is in collision.
+ */
+class InCollisionConstraint final : public solvers::Constraint {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(InCollisionConstraint);
+
+  /**
+   @param minimum_distance_upper The upper bound on the minimum distance.
+   @param normalizer A positive scalar. This normalizer should be roughly in the
+   same magnitude as the range of distances between all pairs of geometries. It
+   doesn't need to be precise.
+   */
+  InCollisionConstraint(const multibody::MultibodyPlant<double>* const plant,
+                        double minimum_distance_upper, double normalizer,
+                        systems::Context<double>* plant_context);
+
+  /** Overloaded constructor with AutoDiff scalar. */
+  InCollisionConstraint(
+      const multibody::MultibodyPlant<AutoDiffXd>* const plant,
+      double minimum_distance_upper, double normalizer,
+      systems::Context<AutoDiffXd>* plant_context);
+
+  /** Overloaded constructor with CollisionChecker. */
+  InCollisionConstraint(
+      const planning::CollisionChecker* collision_checker,
+      double minimum_distance_upper, double normalizer,
+      planning::CollisionCheckerContext* collision_checker_context);
+
+  ~InCollisionConstraint() override {}
+
+  [[nodiscard]] double minimum_distance_upper() const {
+    return minimum_distance_upper_;
+  }
+
+  [[nodiscard]] double normalizer() const { return normalizer_; }
+
+ private:
+  void DoEval(const Eigen::Ref<const Eigen::VectorXd>& x,
+              Eigen::VectorXd* y) const override;
+
+  void DoEval(const Eigen::Ref<const AutoDiffVecXd>& x,
+              AutoDiffVecXd* y) const override;
+
+  void DoEval(const Eigen::Ref<const VectorX<symbolic::Variable>>&,
+              VectorX<symbolic::Expression>*) const override {
+    throw std::logic_error(
+        "InCollisionConstraint::DoEval() does not work for symbolic "
+        "variables.");
+  }
+
+  template <typename T>
+  void DoEvalGeneric(const Eigen::Ref<const VectorX<T>>& x,
+                     VectorX<T>* y) const;
+
+  template <typename T>
+  void Initialize(const MultibodyPlant<T>& plant,
+                  systems::Context<T>* plant_context,
+                  double minimum_distance_upper, double nomalizer);
+
+  void Initialize(const planning::CollisionChecker& collision_checker,
+                  double minimum_distance_upper, double normalizer);
+
+  const multibody::MultibodyPlant<double>* const plant_double_;
+  systems::Context<double>* const plant_context_double_;
+  std::unique_ptr<solvers::MinimumValueUpperBoundConstraint>
+      minimum_value_constraint_;
+
+  const multibody::MultibodyPlant<AutoDiffXd>* const plant_autodiff_;
+  systems::Context<AutoDiffXd>* const plant_context_autodiff_;
+
+  const planning::CollisionChecker* collision_checker_;
+  planning::CollisionCheckerContext* collision_checker_context_;
+  double minimum_distance_upper_;
+  double normalizer_;
 };
 }  // namespace multibody
 }  // namespace drake
--- multibody/inverse_kinematics/test/minimum_distance_constraint_test.cc
+++ multibody/inverse_kinematics/test/minimum_distance_constraint_test.cc
@@ -604,5 +604,101 @@ TEST_F(SpheresAndWallsTest, TestWithCollisionChecker) {
         Eigen::Vector3d(1.1 * minimum_distance + 2 * radius_, 0, 0), true);
   }
 }
+
+TEST_F(TwoFreeSpheresTest, InCollisionConstraint) {
+  // Test InCollision constraint with MBP.
+  double minimum_distance_upper = 0.5;
+  double normalizer = 1;
+  auto diagram_context_double = diagram_double_->CreateDefaultContext();
+  auto plant_context_double = &(
+      plant_double_->GetMyMutableContextFromRoot(diagram_context_double.get()));
+  InCollisionConstraint dut_double(plant_double_, minimum_distance_upper,
+                                   normalizer, plant_context_double);
+  Eigen::VectorXd q_double_good = Eigen::VectorXd::Zero(14);
+  q_double_good.head<4>() << 1, 0, 0, 0;
+  q_double_good.segment<4>(7) << 1, 0, 0, 0;
+  q_double_good.tail<3>() << radius1_ + radius2_ + minimum_distance_upper - 0.1,
+      0, 0;
+
+  EXPECT_TRUE(dut_double.CheckSatisfied(q_double_good));
+
+  Eigen::VectorXd q_double_bad = q_double_good;
+  q_double_bad.tail<3>() << radius1_ + radius2_ + minimum_distance_upper + 0.1,
+      0, 0;
+  EXPECT_FALSE(dut_double.CheckSatisfied(q_double_bad));
+
+  // Now construct InCollisionConstraint with AutoDiffXd. Make sure the
+  // evaluation is the same as the constraint constructed with double.
+  auto diagram_context_autodiff = diagram_autodiff_->CreateDefaultContext();
+  auto plant_context_ad = &(plant_autodiff_->GetMyMutableContextFromRoot(
+      diagram_context_autodiff.get()));
+  InCollisionConstraint dut_ad(plant_autodiff_, minimum_distance_upper,
+                               normalizer, plant_context_ad);
+
+  // Use arbitrary gradient.
+  Eigen::MatrixXd q_gradient =
+      Eigen::MatrixXd(plant_double_->num_positions(), 2);
+  q_gradient.col(0) = Eigen::VectorXd::LinSpaced(q_gradient.rows(), 0, 2);
+  q_gradient.col(1) = Eigen::VectorXd::LinSpaced(q_gradient.rows(), 3, -1);
+  const auto q_ad_good = math::InitializeAutoDiff(q_double_good, q_gradient);
+  const auto q_ad_bad = math::InitializeAutoDiff(q_double_bad, q_gradient);
+
+  Eigen::VectorXd y_double;
+  Eigen::VectorXd y_double_expected;
+  dut_double.Eval(q_double_good, &y_double);
+  dut_ad.Eval(q_double_good, &y_double_expected);
+  EXPECT_TRUE(CompareMatrices(y_double, y_double_expected));
+  AutoDiffVecXd y_ad;
+  AutoDiffVecXd y_ad_expected;
+  dut_double.Eval(q_ad_good, &y_ad);
+  dut_ad.Eval(q_ad_good, &y_ad_expected);
+  const double tol = 1E-12;
+  EXPECT_TRUE(CompareMatrices(math::ExtractValue(y_ad), y_double));
+  EXPECT_TRUE(CompareMatrices(math::ExtractValue(y_ad),
+                              math::ExtractValue(y_ad_expected), tol));
+  EXPECT_TRUE(CompareMatrices(math::ExtractGradient(y_ad),
+                              math::ExtractGradient(y_ad_expected), tol));
+}
+
+TEST_F(SpheresAndWallsTest, InCollisionConstraintCollisionChecker) {
+  // Test InCollisionConstraint with CollisionChecker.
+  planning::CollisionCheckerParams params;
+  params.model = builder_.Build();
+  params.robot_model_instances.push_back(multibody::default_model_instance());
+  auto quaternion_difference = [](const Eigen::Ref<const Eigen::Vector4d>& q1,
+                                  const Eigen::Ref<const Eigen::Vector4d>& q2) {
+    // Compute 1-cos(theta/2) where theta is the angle between the two
+    // quaternions.
+    return 1 - (Eigen::Quaterniond(q1(0), q1(1), q1(2), q1(3)).conjugate() *
+                Eigen::Quaterniond(q2(0), q2(1), q2(2), q2(3)))
+                   .w();
+  };
+  params.configuration_distance_function = [quaternion_difference](
+                                               const Eigen::VectorXd& q1,
+                                               const Eigen::VectorXd& q2) {
+    return quaternion_difference(q1.head<4>(), q2.head<4>()) +
+           quaternion_difference(q1.segment<4>(7), q2.segment<4>(7)) +
+           (q1.segment<3>(4) - q2.segment<3>(4)).norm() +
+           (q1.segment<3>(11) - q2.segment<3>(11)).norm();
+  };
+  params.edge_step_size = 0.05;
+  planning::SceneGraphCollisionChecker collision_checker(std::move(params));
+  auto collision_checker_context =
+      collision_checker.MakeStandaloneModelContext();
+
+  double minimum_distance_upper = 0.1;
+  double normalizer = 2;
+  InCollisionConstraint dut(&collision_checker, minimum_distance_upper,
+                            normalizer, collision_checker_context.get());
+
+  Eigen::VectorXd q =
+      Eigen::VectorXd::Zero(collision_checker.plant().num_positions());
+  q.head<4>() << 1, 0, 0, 0;
+  q.segment<4>(7) << 1, 0, 0, 0;
+  q.tail<3>() << wall_length_ / 2 + minimum_distance_upper + 0.5, 0, 0;
+  EXPECT_FALSE(dut.CheckSatisfied(q));
+  q.tail<3>() << radius_ * 2 + minimum_distance_upper - 0.1, 0, 0;
+  EXPECT_TRUE(dut.CheckSatisfied(q));
+}
 }  // namespace multibody
 }  // namespace drake
