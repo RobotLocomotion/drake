abc7b248cff674a90cc80cd0e0210340fa87bc2c
Rick Poyner <rick.poyner@tri.global>
2023-09-05 13:31:26 -0400

    wip: mitigate todo's a bit

--- geometry/scene_graph_config.h
+++ geometry/scene_graph_config.h
@@ -19,6 +19,8 @@ struct SceneGraphConfig {
     a->Visit(DRAKE_NVP(hydroelastize_default_hydroelastic_modulus));
     a->Visit(DRAKE_NVP(hydroelastize_default_resolution_hint));
     a->Visit(DRAKE_NVP(hydroelastize_default_slab_thickness));
+    a->Visit(DRAKE_NVP(hydroelastize_default_hunt_crossley_dissipation));
+    a->Visit(DRAKE_NVP(hydroelastize_default_dynamic_friction));
   }
 
   /// Configures the SceneGraph::set_hydroelastize().
@@ -29,13 +31,19 @@ struct SceneGraphConfig {
 
   /// Configures the default hydroelastic modulus to use when hydroelastize is
   /// active.
-  double hydroelastize_default_hydroelastic_modulus{1e8};
+  double hydroelastize_default_hydroelastic_modulus{1e7};
   /// Configures the default resolution hint to use when hydroelastize is
   /// active.
   double hydroelastize_default_resolution_hint{0.01};
   /// Configures the default slab thickness to use when hydroelastize is
   /// active.
-  double hydroelastize_default_slab_thickness{1e3};
+  double hydroelastize_default_slab_thickness{10.0};
+  /// Configures the default dissipation to use when hydroelastize is
+  /// active.
+  double hydroelastize_default_hunt_crossley_dissipation{1.25};
+  /// Configures the default dynamic friction to use when hydroelastize is
+  /// active.
+  double hydroelastize_default_dynamic_friction{0.5};
 };
 
 }  // namespace geometry
