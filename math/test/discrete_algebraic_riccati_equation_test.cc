2fbfa1f6ca8d50fb3d79cca42de4cd557efa6ab1
Tyler Veness <calcmogul@gmail.com>
2023-09-05 15:48:51 -0700

    [math] Rewrite the DARE solver to use a faster algorithm (#20104)
    Implements the SDA algorithm on page 5 of the following paper:
    
      E. K.-W. Chu, H.-Y. Fan, W.-W. Lin & C.-S. Wang "Structure-Preserving
      Algorithms for Periodic Discrete-Time Algebraic Riccati Equations",
      International Journal of Control, 77:8, 767-788, 2004.
      DOI: 10.1080/00207170410001714988
    
    Here's Google Benchmark results for the old DARE solver using
    https://github.com/calcmogul/DAREBench/:
    ```
    Running ./build/DAREBench
    Run on (8 X 3595.49 MHz CPU s)
    CPU Caches:
      L1 Data 32 KiB (x4)
      L1 Instruction 32 KiB (x4)
      L2 Unified 256 KiB (x4)
      L3 Unified 6144 KiB (x1)
    Load Average: 1.59, 4.28, 3.08
    -------------------------------------------------------------------------------
    Benchmark                                     Time             CPU   Iterations
    -------------------------------------------------------------------------------
    DARE_WPIMath_Dynamic                       28.2 us         28.1 us        24704
    DARE_WPIMath_NoPrecondChecks_Dynamic       21.3 us         21.1 us        33349
    DARE_WPIMath_Static                        13.1 us         13.1 us        52985
    DARE_WPIMath_NoPrecondChecks_Static        8.78 us         8.77 us        79851
    DARE_SLICOT                                41.4 us         41.3 us        16967
    DARE_Drake                                 95.6 us         95.3 us         7597
    ```
    
    Here's Google Benchmark results for the new DARE solver using
    https://github.com/calcmogul/DAREBench/tree/new-drake-dare-solver:
    ```
    Running ./build/DAREBench
    Run on (8 X 3600.03 MHz CPU s)
    CPU Caches:
      L1 Data 32 KiB (x4)
      L1 Instruction 32 KiB (x4)
      L2 Unified 256 KiB (x4)
      L3 Unified 6144 KiB (x1)
    Load Average: 0.65, 0.41, 0.42
    -------------------------------------------------------------------------------
    Benchmark                                     Time             CPU   Iterations
    -------------------------------------------------------------------------------
    DARE_WPIMath_Dynamic                       28.1 us         28.1 us        25070
    DARE_WPIMath_NoPrecondChecks_Dynamic       21.2 us         21.2 us        32991
    DARE_WPIMath_Static                        13.1 us         13.1 us        52666
    DARE_WPIMath_NoPrecondChecks_Static        8.79 us         8.79 us        79544
    DARE_SLICOT                                41.3 us         41.3 us        16893
    DARE_Drake                                 28.1 us         28.1 us        25109
    ```
    
    "WPIMath" is a previous implementation I wrote of the new algorithm.
    Benchmarking has shown that using statically sized matrices is twice as
    fast as dynamically sized matrices, and the precondition checks are very
    expensive relative to the main solve.
    
    WPIMath refactored the precondition checks out so users could opt out of
    precondition checking via separate DARE overloads, and so the ABQRN
    overload doesn't do an extra LLT decomposition of R. I decided to leave
    Drake's API as is though.

--- math/test/discrete_algebraic_riccati_equation_test.cc
+++ math/test/discrete_algebraic_riccati_equation_test.cc
@@ -1,123 +1,263 @@
 #include "drake/math/discrete_algebraic_riccati_equation.h"
 
+#include <exception>
+
+#include <Eigen/LU>  // for inverse()
 #include <gtest/gtest.h>
 
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
-#include "drake/math/autodiff.h"
-
-using Eigen::MatrixXd;
 
 namespace drake {
 namespace math {
 namespace {
-void SolveDAREandVerify(const Eigen::Ref<const MatrixXd>& A,
-                        const Eigen::Ref<const MatrixXd>& B,
-                        const Eigen::Ref<const MatrixXd>& Q,
-                        const Eigen::Ref<const MatrixXd>& R) {
-  MatrixXd X = DiscreteAlgebraicRiccatiEquation(A, B, Q, R);
-  // Check that X is positive semi-definite.
+
+void SolveDAREandVerify(const Eigen::Ref<const Eigen::MatrixXd>& A,
+                        const Eigen::Ref<const Eigen::MatrixXd>& B,
+                        const Eigen::Ref<const Eigen::MatrixXd>& Q,
+                        const Eigen::Ref<const Eigen::MatrixXd>& R) {
+  int states = A.rows();
+
+  Eigen::MatrixXd X = DiscreteAlgebraicRiccatiEquation(A, B, Q, R);
+
+  // Check that X is symmetric
   EXPECT_TRUE(
-      CompareMatrices(X, X.transpose(), 1E-10, MatrixCompareType::absolute));
-  int n = X.rows();
-  Eigen::SelfAdjointEigenSolver<MatrixXd> es(X);
-  for (int i = 0; i < n; i++) {
+      CompareMatrices(X, X.transpose(), 1e-10, MatrixCompareType::absolute));
+
+  // Check that X is positive semidefinite
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es{X, Eigen::EigenvaluesOnly};
+  for (int i = 0; i < states; ++i) {
     EXPECT_GE(es.eigenvalues()[i], 0);
   }
-  // Check that X is the solution to the discrete time ARE.
+
+  // Check that X is the solution to the discrete time ARE
   // clang-format off
-  MatrixXd Y =
+  Eigen::MatrixXd Y =
       A.transpose() * X * A
       - X
       - (A.transpose() * X * B * (B.transpose() * X * B + R).inverse()
         * B.transpose() * X * A)
       + Q;
   // clang-format on
-  EXPECT_TRUE(CompareMatrices(Y, MatrixXd::Zero(n, n), 1E-10,
+  EXPECT_TRUE(CompareMatrices(Y, Eigen::MatrixXd::Zero(states, states), 1e-10,
                               MatrixCompareType::absolute));
 }
 
-void SolveDAREandVerify(const Eigen::Ref<const MatrixXd>& A,
-                        const Eigen::Ref<const MatrixXd>& B,
-                        const Eigen::Ref<const MatrixXd>& Q,
-                        const Eigen::Ref<const MatrixXd>& R,
-                        const Eigen::Ref<const MatrixXd>& N) {
-  MatrixXd X = DiscreteAlgebraicRiccatiEquation(A, B, Q, R, N);
-  // Check that X is positive semi-definite.
+void SolveDAREandVerify(const Eigen::Ref<const Eigen::MatrixXd>& A,
+                        const Eigen::Ref<const Eigen::MatrixXd>& B,
+                        const Eigen::Ref<const Eigen::MatrixXd>& Q,
+                        const Eigen::Ref<const Eigen::MatrixXd>& R,
+                        const Eigen::Ref<const Eigen::MatrixXd>& N) {
+  int states = A.rows();
+
+  Eigen::MatrixXd X = DiscreteAlgebraicRiccatiEquation(A, B, Q, R, N);
+
+  // Check that X is symmetric
   EXPECT_TRUE(
-      CompareMatrices(X, X.transpose(), 1E-10, MatrixCompareType::absolute));
-  int n = X.rows();
-  Eigen::SelfAdjointEigenSolver<MatrixXd> es(X);
-  for (int i = 0; i < n; i++) {
+      CompareMatrices(X, X.transpose(), 1e-10, MatrixCompareType::absolute));
+
+  // Check that X is positive semidefinite
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es{X, Eigen::EigenvaluesOnly};
+  for (int i = 0; i < states; ++i) {
     EXPECT_GE(es.eigenvalues()[i], 0);
   }
-  // Check that X is the solution to the discrete time ARE.
+
+  // Check that X is the solution to the discrete time ARE
   // clang-format off
-  MatrixXd Y =
+  Eigen::MatrixXd Y =
       A.transpose() * X * A
       - X
       - ((A.transpose() * X * B + N) * (B.transpose() * X * B + R).inverse()
         * (B.transpose() * X * A + N.transpose()))
       + Q;
   // clang-format on
-  EXPECT_TRUE(CompareMatrices(Y, MatrixXd::Zero(n, n), 1E-10,
+  EXPECT_TRUE(CompareMatrices(Y, Eigen::MatrixXd::Zero(states, states), 1e-10,
                               MatrixCompareType::absolute));
 }
 
-GTEST_TEST(DARE, SolveDAREandVerify) {
+GTEST_TEST(DARE, NonInvertibleA) {
   // Test 1: non-invertible A
   // Example 2 of "On the Numerical Solution of the Discrete-Time Algebraic
   // Riccati Equation"
-  int n1 = 4, m1 = 1;
-  MatrixXd A1(n1, n1), B1(n1, m1), Q1(n1, n1), R1(m1, m1);
-  A1 << 0.5, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0;
-  B1 << 0, 0, 0, 1;
-  Q1 << 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
-  R1 << 0.25;
-  SolveDAREandVerify(A1, B1, Q1, R1);
+  Eigen::MatrixXd A{4, 4};
+  A << 0.5, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0;
+  Eigen::MatrixXd B{4, 1};
+  B << 0, 0, 0, 1;
+  Eigen::MatrixXd Q{4, 4};
+  Q << 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+  Eigen::MatrixXd R{1, 1};
+  R << 0.25;
+  SolveDAREandVerify(A, B, Q, R);
 
-  MatrixXd Aref1(n1, n1);
-  Aref1 << 0.25, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0;
-  SolveDAREandVerify(A1, B1, (A1 - Aref1).transpose() * Q1 * (A1 - Aref1),
-      B1.transpose() * Q1 * B1 + R1, (A1 - Aref1).transpose() * Q1 * B1);
-
-  // Test 2: invertible A
-  int n2 = 2, m2 = 1;
-  MatrixXd A2(n2, n2), B2(n2, m2), Q2(n2, n2), R2(m2, m2);
-  A2 << 1, 1, 0, 1;
-  B2 << 0, 1;
-  Q2 << 1, 0, 0, 0;
-  R2 << 0.3;
-  SolveDAREandVerify(A2, B2, Q2, R2);
-
-  MatrixXd Aref2(n2, n2);
-  Aref2 << 0.5, 1, 0, 1;
-  SolveDAREandVerify(A2, B2, (A2 - Aref2).transpose() * Q2 * (A2 - Aref2),
-      B2.transpose() * Q2 * B2 + R2, (A2 - Aref2).transpose() * Q2 * B2);
-
-  // Test 3: the first generalized eigenvalue of (S,T) is stable
-  int n3 = 2, m3 = 1;
-  MatrixXd A3(n3, n3), B3(n3, m3), Q3(n3, n3), R3(m3, m3);
-  A3 << 0, 1, 0, 0;
-  B3 << 0, 1;
-  Q3 << 1, 0, 0, 1;
-  R3 << 1;
-  SolveDAREandVerify(A3, B3, Q3, R3);
-
-  MatrixXd Aref3(n3, n3);
-  Aref3 << 0, 0.5, 0, 0;
-  SolveDAREandVerify(A3, B3, (A3 - Aref3).transpose() * Q3 * (A3 - Aref3),
-      B3.transpose() * Q3 * B3 + R3, (A3 - Aref3).transpose() * Q3 * B3);
-
-  // Test 4: A = B = Q = R = I_2 (2-by-2 identity matrix)
-  const Eigen::MatrixXd A4{Eigen::Matrix2d::Identity()};
-  const Eigen::MatrixXd B4{Eigen::Matrix2d::Identity()};
-  const Eigen::MatrixXd Q4{Eigen::Matrix2d::Identity()};
-  const Eigen::MatrixXd R4{Eigen::Matrix2d::Identity()};
-  SolveDAREandVerify(A4, B4, Q4, R4);
-
-  const Eigen::MatrixXd N4{Eigen::Matrix2d::Identity()};
-  SolveDAREandVerify(A4, B4, Q4, R4, N4);
+  Eigen::MatrixXd Aref{4, 4};
+  Aref << 0.25, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0;
+  SolveDAREandVerify(A, B, (A - Aref).transpose() * Q * (A - Aref),
+                     B.transpose() * Q * B + R, (A - Aref).transpose() * Q * B);
 }
+
+GTEST_TEST(DARE, InvertibleA) {
+  // Test 2: invertible A
+  Eigen::MatrixXd A{2, 2};
+  A << 1, 1, 0, 1;
+  Eigen::MatrixXd B{2, 1};
+  B << 0, 1;
+  Eigen::MatrixXd Q{2, 2};
+  Q << 1, 0, 0, 0;
+  Eigen::MatrixXd R{1, 1};
+  R << 0.3;
+  SolveDAREandVerify(A, B, Q, R);
+
+  Eigen::MatrixXd Aref{2, 2};
+  Aref << 0.5, 1, 0, 1;
+  SolveDAREandVerify(A, B, (A - Aref).transpose() * Q * (A - Aref),
+                     B.transpose() * Q * B + R, (A - Aref).transpose() * Q * B);
+}
+
+GTEST_TEST(DARE, FirstGeneralizedEigenvalueOfSTIsStable) {
+  // Test 3: the first generalized eigenvalue of (S, T) is stable
+  Eigen::MatrixXd A{2, 2};
+  A << 0, 1, 0, 0;
+  Eigen::MatrixXd B{2, 1};
+  B << 0, 1;
+  Eigen::MatrixXd Q{2, 2};
+  Q << 1, 0, 0, 1;
+  Eigen::MatrixXd R{1, 1};
+  R << 1;
+  SolveDAREandVerify(A, B, Q, R);
+
+  Eigen::MatrixXd Aref{2, 2};
+  Aref << 0, 0.5, 0, 0;
+  SolveDAREandVerify(A, B, (A - Aref).transpose() * Q * (A - Aref),
+                     B.transpose() * Q * B + R, (A - Aref).transpose() * Q * B);
+}
+
+GTEST_TEST(DARE, IdentitySystem) {
+  // Test 4: A = B = Q = R = I₂ₓ₂
+  const Eigen::MatrixXd A{Eigen::Matrix2d::Identity()};
+  const Eigen::MatrixXd B{Eigen::Matrix2d::Identity()};
+  const Eigen::MatrixXd Q{Eigen::Matrix2d::Identity()};
+  const Eigen::MatrixXd R{Eigen::Matrix2d::Identity()};
+  SolveDAREandVerify(A, B, Q, R);
+
+  const Eigen::MatrixXd N{Eigen::Matrix2d::Identity()};
+  SolveDAREandVerify(A, B, Q, R, N);
+}
+
+GTEST_TEST(DARE, MoreInputsThanStates) {
+  // Test 5: More inputs than states
+  const Eigen::Matrix2d A{Eigen::Matrix2d::Identity()};
+  Eigen::MatrixXd B{2, 3};
+  B << 1, 0, 0, 0, 0.5, 0.3;
+  const Eigen::Matrix2d Q{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix3d R{Eigen::Matrix3d::Identity()};
+  SolveDAREandVerify(A, B, Q, R);
+
+  Eigen::MatrixXd N{2, 3};
+  N << 1, 0, 0, 0, 1, 0;
+  SolveDAREandVerify(A, B, Q, R, N);
+}
+
+GTEST_TEST(DARE, QNotSymmetricPositiveSemidefinite_ABQR) {
+  const Eigen::Matrix2d A{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d B{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d Q{-Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d R{Eigen::Matrix2d::Identity()};
+
+  EXPECT_THROW(SolveDAREandVerify(A, B, Q, R), std::exception);
+}
+
+GTEST_TEST(DARE, QNotSymmetricPositiveSemidefinite_ABQRN) {
+  const Eigen::Matrix2d A{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d B{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d Q{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d R{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d N{2.0 * Eigen::Matrix2d::Identity()};
+
+  EXPECT_THROW(SolveDAREandVerify(A, B, Q, R, N), std::exception);
+}
+
+GTEST_TEST(DARE, RNotSymmetricPositiveDefinite_ABQR) {
+  const Eigen::Matrix2d A{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d B{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d Q{Eigen::Matrix2d::Identity()};
+
+  const Eigen::Matrix2d R1{Eigen::Matrix2d::Zero()};
+  EXPECT_THROW(SolveDAREandVerify(A, B, Q, R1), std::exception);
+
+  const Eigen::Matrix2d R2{-Eigen::Matrix2d::Identity()};
+  EXPECT_THROW(SolveDAREandVerify(A, B, Q, R2), std::exception);
+}
+
+GTEST_TEST(DARE, RNotSymmetricPositiveDefinite_ABQRN) {
+  const Eigen::Matrix2d A{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d B{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d Q{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d N{Eigen::Matrix2d::Identity()};
+
+  const Eigen::Matrix2d R1{Eigen::Matrix2d::Zero()};
+  EXPECT_THROW(SolveDAREandVerify(A, B, Q, R1, N), std::exception);
+
+  const Eigen::Matrix2d R2{-Eigen::Matrix2d::Identity()};
+  EXPECT_THROW(SolveDAREandVerify(A, B, Q, R2, N), std::exception);
+}
+
+GTEST_TEST(DARE, ABNotStabilizable_ABQR) {
+  const Eigen::Matrix2d A{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d B{Eigen::Matrix2d::Zero()};
+  const Eigen::Matrix2d Q{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d R{Eigen::Matrix2d::Identity()};
+
+  EXPECT_THROW(SolveDAREandVerify(A, B, Q, R), std::exception);
+}
+
+GTEST_TEST(DARETest, ABNotStabilizable_ABQRN) {
+  const Eigen::Matrix2d A{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d B{Eigen::Matrix2d::Zero()};
+  const Eigen::Matrix2d Q{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d R{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d N{Eigen::Matrix2d::Identity()};
+
+  EXPECT_THROW(SolveDAREandVerify(A, B, Q, R, N), std::exception);
+}
+
+GTEST_TEST(DARETest, ACNotDetectable_ABQR) {
+  const Eigen::Matrix2d A{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d B{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d Q{Eigen::Matrix2d::Zero()};
+  const Eigen::Matrix2d R{Eigen::Matrix2d::Identity()};
+
+  EXPECT_THROW(SolveDAREandVerify(A, B, Q, R), std::exception);
+}
+
+GTEST_TEST(DARETest, ACNotDetectable_ABQRN) {
+  const Eigen::Matrix2d A{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d B{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d Q{Eigen::Matrix2d::Zero()};
+  const Eigen::Matrix2d R{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d N{Eigen::Matrix2d::Zero()};
+
+  EXPECT_THROW(SolveDAREandVerify(A, B, Q, R, N), std::exception);
+}
+
+GTEST_TEST(DARETest, QDecomposition) {
+  // Ensures the decomposition of Q into CᵀC is correct
+
+  Eigen::Matrix2d A{2, 2};
+  A << 1.0, 0.0, 0.0, 0.0;
+  const Eigen::Matrix2d B{Eigen::Matrix2d::Identity()};
+  const Eigen::Matrix2d R{Eigen::Matrix2d::Identity()};
+
+  // (A, C₁) should be detectable pair
+  Eigen::Matrix2d C_1{2, 2};
+  C_1 << 0.0, 0.0, 1.0, 0.0;
+  const Eigen::Matrix2d Q_1 = C_1.transpose() * C_1;
+  EXPECT_NO_THROW(SolveDAREandVerify(A, B, Q_1, R));
+
+  // (A, C₂) shouldn't be detectable pair
+  const Eigen::Matrix2d C_2 = C_1.transpose();
+  const Eigen::Matrix2d Q_2 = C_2.transpose() * C_2;
+  EXPECT_THROW(SolveDAREandVerify(A, B, Q_2, R), std::exception);
+}
+
 }  // namespace
 }  // namespace math
 }  // namespace drake
