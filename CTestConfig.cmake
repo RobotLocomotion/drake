d15028360bff8b49f17e4f8405e65f7230d2500a
betsy.mcphail <betsy.mcphail@kitware.com>
2023-09-21 14:12:43 -0400

    Test new CDash server

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,7 +3,7 @@
 
 set(CTEST_PROJECT_NAME drake)
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
-set(CTEST_DROP_METHOD https)
-set(CTEST_DROP_SITE drake-cdash.csail.mit.edu)
+set(CTEST_DROP_METHOD http)
+set(CTEST_DROP_SITE ec2-34-203-13-209.compute-1.amazonaws.com/cdash)
 set(CTEST_DROP_LOCATION "/submit.php?project=${CTEST_PROJECT_NAME}")
 set(CTEST_DROP_SITE_CDASH ON)
