ab844ec90ec9b45f13cdf5c870be7c3e23e202f0
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-09-20 17:44:29 -0700

    WIP [systems] Improve Meshcat visualization in Diagrams

--- multibody/meshcat/contact_visualizer.cc
+++ multibody/meshcat/contact_visualizer.cc
@@ -324,6 +324,23 @@ EventStatus ContactVisualizer<T>::OnInitialization(const Context<T>&) const {
   return EventStatus::Succeeded();
 }
 
+template <typename T>
+typename systems::LeafSystem<T>::GraphvizFragment
+ContactVisualizer<T>::DoGetGraphvizFragment(
+    const typename systems::LeafSystem<T>::GraphvizFragmentParams& params)
+    const {
+  typename systems::LeafSystem<T>::GraphvizFragmentParams new_params{params};
+  new_params.header_lines.push_back(
+      fmt::format("path=/drake/{}", params_.prefix));
+  typename systems::LeafSystem<T>::GraphvizFragment result =
+      systems::LeafSystem<T>::DoGetGraphvizFragment(new_params);
+  result.fragments.push_back(
+      fmt::format("Meshcat [color=magenta];\n"
+                  "{}:e -> Meshcat [style=dashed, color=magenta]\n",
+                  new_params.node_id));
+  return result;
+}
+
 }  // namespace meshcat
 }  // namespace multibody
 }  // namespace drake
