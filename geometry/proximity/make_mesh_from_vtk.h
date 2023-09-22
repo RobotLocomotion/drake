36fc0773307f4d94f86fd478c2a1342e0e5a708d
Rick Poyner <rick.poyner@tri.global>
2023-09-22 13:10:50 -0400

    [geometry] Allow some sub-optimal uses for vtk volume mesh files
    Accept vtk volume mesh files in some cases where only the surface mesh
    or its convex hull might actually be used. This is intended to ease
    step-wise refinement of models to optimize them for use with
    hydroelastic contact.
    
    Also, make some improvements to error messages in a few places.

--- geometry/proximity/make_mesh_from_vtk.h
+++ geometry/proximity/make_mesh_from_vtk.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <filesystem>
+#include <string>
+
 #include "drake/geometry/proximity/volume_mesh.h"
 #include "drake/geometry/shape_specification.h"
 
@@ -10,7 +13,8 @@ namespace internal {
 /* Creates a VolumeMesh of a possibly non-convex object from a VTK file
  containing its tetrahedral mesh. It complements MakeConvexVolumeMesh().
 
- @param[in] mesh  The mesh specification containing VTK file name.
+ @param[in] filename  VTK file name.
+ @param[in] scale  scale parameter.
  @retval  volume_mesh
  @tparam_nonsymbolic_scalar
 
@@ -22,7 +26,16 @@ namespace internal {
         instead of a tetrahedral mesh.
  */
 template <typename T>
-VolumeMesh<T> MakeVolumeMeshFromVtk(const Mesh& mesh);
+VolumeMesh<T> MakeVolumeMeshFromVtk(const std::filesystem::path& filename,
+                                    double scale);
+
+/* Overload the 2-argument MakeVolumeMeshFromVtk() to take filename and scale
+   from either Mesh or Convex shape specifications.
+ */
+template <typename T, typename Shape>
+VolumeMesh<T> MakeVolumeMeshFromVtk(const Shape& shape) {
+  return MakeVolumeMeshFromVtk<T>(shape.filename(), shape.scale());
+}
 
 }  // namespace internal
 }  // namespace geometry
