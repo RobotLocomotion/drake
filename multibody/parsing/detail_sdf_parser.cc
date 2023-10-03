18cc3ab124c41aa3044dc805836682fd8be6e4a5
Rick Poyner <rick.poyner@tri.global>
2023-10-03 12:52:41 -0400

    patch an sdf deprecated item

--- multibody/parsing/detail_sdf_parser.cc
+++ multibody/parsing/detail_sdf_parser.cc
@@ -147,7 +147,7 @@ std::string GetRelativeBodyName(
     // nested_model_absolute_name. Otherwise the nested model is not a
     // descendent of the model relative to which we are computing the name.
     const std::string required_prefix =
-        relative_to_model_absolute_name + sdf::kSdfScopeDelimiter;
+        relative_to_model_absolute_name + std::string(sdf::kScopeDelimiter);
     DRAKE_DEMAND(StartsWith(nested_model_absolute_name, required_prefix));
 
     const std::string nested_model_relative_name =
