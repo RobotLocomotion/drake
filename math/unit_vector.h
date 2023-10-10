828ce5e575e0bd9da3a1d2decb758b48e950fa78
mitiguy <paul.mitiguy@tri.global>
2023-10-10 15:20:38 -0700

    Respond to Xue Chen's comments.

--- math/unit_vector.h
+++ math/unit_vector.h
@@ -8,48 +8,53 @@ namespace drake {
 namespace math {
 namespace internal {
 
-/// Throws unless ‖unit_vector‖ is within 2 bits (= 4ε ≈ 8.88E-16) of 1.0, where
-/// 2 bits = 2²ε = 4ε and ε = std::numeric_limits<double>::epsilon().
-/// @param[in] unit_vector a vector which is allegedly a unit vector.
-/// @param[in] function_name name of the function that is to appear in the
-/// exception message (if an exception is thrown).
-/// @throws std::exception if ‖unit_vector‖ is not within 4ε of 1.0.
-/// @retval ‖unit_vector‖² which is exactly 1.0 for a perfect unit_vector.
-/// @note: When type T is symbolic::Expression, this function is a no-op that
-/// does not throw an exception and returns 1.0.
-/// @note Frequently ‖unit_vector‖ is exactly 1.0 (e.g., when unit_vector is an
-/// x or y or z coordinate axis direction). If not, the calling function should
-/// consider normalizing unit_vector even if it passes this test so that
-/// ‖unit_vector‖ is less than ε ≈ 2.22E-16 of 1.0, e.g., as
-/// @code{.cpp}
-///  using std::sqrt;
-///  const T mag_squared = math::ThrowIfNotUnitVector(unit_vector, __func__);
-///  const Vector3<T> uvec =
-///      (mag_squared == 1.0) ? unit_vector : unit_vector / sqrt(mag_squared);
-/// @endcode
+// Throws unless ‖unit_vector‖ is within 2 bits (= 4ε ≈ 8.88E-16) of 1.0, where
+// 2 bits = 2²ε = 4ε and ε = std::numeric_limits<double>::epsilon().
+// @param[in] unit_vector a vector which is allegedly a unit vector.
+// @param[in] function_name name of the function that is to appear in the
+// exception message (if an exception is thrown).
+// @throws std::exception if ‖unit_vector‖ is not within 4ε of 1.0.
+// @retval ‖unit_vector‖² which is exactly 1.0 for a perfect unit_vector.
+// @note: When type T is symbolic::Expression, this function is a no-op that
+// does not throw an exception and returns 1.0.
+// @note Frequently ‖unit_vector‖ is exactly 1.0 (e.g., when unit_vector is an
+// x or y or z axis direction). If the retval ‖unit_vector‖² is not exactly 1.0,
+// the calling function might consider normalizing unit_vector even when it
+// passes this test so ‖unit_vector‖ is within ε ≈ 2.22E-16 of 1.0, e.g., as
+// @code{.cpp}
+//  using std::sqrt;
+//  const T mag_squared =
+//      math::internal::ThrowIfNotUnitVector(unit_vector, __func__);
+//  const Vector3<T> uvec =
+//      (mag_squared == 1.0) ? unit_vector : unit_vector / sqrt(mag_squared);
+// @endcode
 template <typename T>
 T ThrowIfNotUnitVector(const Vector3<T>& unit_vector,
                        std::string_view function_name);
 
-/// If ‖unit_vector‖ is not within 2 bits (= 4ε ≈ 8.88E-16) of 1.0,
-/// writes a warning to the log file (only writes one warning per process).
-/// Note: 2 bits = 2²ε = 4ε and ε = std::numeric_limits<double>::epsilon().
-/// @param[in] unit_vector a vector which is allegedly a unit vector.
-/// @param[in] function_name name of the function that appears in the message
-/// written to the log file (if ‖unit_vector‖ is not within tolerance of 1.0).
-/// @retval ‖unit_vector‖² which is exactly 1.0 for a perfect unit_vector.
-/// @note: When type T is symbolic::Expression, this function is a no-op that
-/// does not write to the log file and returns 1.0.
-/// @note Frequently ‖unit_vector‖ is exactly 1.0 (e.g., when unit_vector is an
-/// x or y or z coordinate axis direction). If not, the calling function should
-/// consider normalizing unit_vector even if it passes this test so that
-/// ‖unit_vector‖ is less than ε ≈ 2.22E-16 of 1.0, e.g., as
-/// @code{.cpp}
-///  using std::sqrt;
-///  const T mag_squared = math::ThrowIfNotUnitVector(unit_vector, __func__);
-///  const Vector3<T> uvec =
-///      (mag_squared == 1.0) ? unit_vector : unit_vector / sqrt(mag_squared);
-/// @endcode
+// If ‖unit_vector‖ is not within 2 bits (= 4ε ≈ 8.88E-16) of 1.0,
+// writes a warning to the log file (only writes one warning per process).
+// Note: 2 bits = 2²ε = 4ε and ε = std::numeric_limits<double>::epsilon().
+// @param[in] unit_vector a vector which is allegedly a unit vector.
+// @param[in] function_name name of the function that appears in the message
+// written to the log file (if ‖unit_vector‖ is not within tolerance of 1.0).
+// @retval ‖unit_vector‖² which is exactly 1.0 for a perfect unit_vector.
+// @note: When type T is symbolic::Expression, this function is a no-op that
+// does not write to the log file and returns 1.0.
+// @note Frequently ‖unit_vector‖ is exactly 1.0 (e.g., when unit_vector is an
+// x or y or z axis direction). If the retval ‖unit_vector‖² is not exactly 1.0,
+// the calling function might consider normalizing unit_vector even when it
+// passes this test so ‖unit_vector‖ is within ε ≈ 2.22E-16 of 1.0, e.g., as
+// @code{.cpp}
+//  using std::sqrt;
+//  const T mag_squared =
+//      math::internal::ThrowIfNotUnitVector(unit_vector, __func__);
+//  const Vector3<T> uvec =
+//      (mag_squared == 1.0) ? unit_vector : unit_vector / sqrt(mag_squared);
+// @endcode
+// TODO(Mitiguy) Change calls to this function to ThrowIfNotUnitVector() on the
+//  date specified in the message written to the log file. The date is specified
+//  in the implementation of WarnIfNotUnitVector() in unit_vector.cc.
 template <typename T>
 T WarnIfNotUnitVector(const Vector3<T>& unit_vector,
                       std::string_view function_name);
