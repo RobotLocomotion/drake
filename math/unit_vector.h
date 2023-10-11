961451f205306bf3ff16647fc6185a7b54502b40
mitiguy <paul.mitiguy@tri.global>
2023-10-11 15:13:56 -0700

    Partial code update.

--- math/unit_vector.h
+++ math/unit_vector.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <limits>
 #include <string>
 
 #include "drake/common/eigen_types.h"
@@ -8,33 +9,38 @@ namespace drake {
 namespace math {
 namespace internal {
 
-// Throws unless ‖unit_vector‖ is within 2 bits (= 4ε ≈ 8.88E-16) of 1.0, where
-// 2 bits = 2²ε = 4ε and ε = std::numeric_limits<double>::epsilon().
+// Tolerance for ‖unit_vector‖ is 3 bits (= 8ε ≈ 1.78E-15) of 1.0, where
+// 3 bits = 2³ε = 8ε and ε = std::numeric_limits<double>::epsilon().
+// @note The use of 2 bits was determined empirically, is well within the
+// tolerance achieved by normalizing a vast range of non-zero vectors, and
+// seems to provide a valid RotationMatrix() (see RotationMatrix::IsValid()).
+constexpr double kTolerance_unit_vector_norm =
+    8 * std::numeric_limits<double>::epsilon();
+
+// Throws unless ‖unit_vector‖ is within kTolerance_unit_vector_norm of 1.0.
 // @param[in] unit_vector a vector which is allegedly a unit vector.
 // @param[in] function_name name of the function that is to appear in the
 // exception message (if an exception is thrown).
-// @throws std::exception if ‖unit_vector‖ is not within 4ε of 1.0.
+// @throws std::exception if ‖unit_vector‖ is not within tolerance of 1.0.
 // @retval ‖unit_vector‖² which is exactly 1.0 for a perfect unit_vector.
 // @note: When type T is symbolic::Expression, this function is a no-op that
 // does not throw an exception and returns 1.0.
 // @note Frequently ‖unit_vector‖ is exactly 1.0 (e.g., when unit_vector is an
 // x or y or z axis direction). If the retval ‖unit_vector‖² is not exactly 1.0,
-// the calling function might consider normalizing unit_vector even when it
+// the calling function should consider normalizing unit_vector even when it
 // passes this test so ‖unit_vector‖ is within ε ≈ 2.22E-16 of 1.0, e.g., as
 // @code{.cpp}
 //  using std::sqrt;
 //  const T mag_squared =
 //      math::internal::ThrowIfNotUnitVector(unit_vector, __func__);
-//  const Vector3<T> uvec =
-//      (mag_squared == 1.0) ? unit_vector : unit_vector / sqrt(mag_squared);
+//  if (mag_squared != 1.0) unit_vector /= sqrt(mag_squared);
 // @endcode
 template <typename T>
 T ThrowIfNotUnitVector(const Vector3<T>& unit_vector,
                        std::string_view function_name);
 
-// If ‖unit_vector‖ is not within 2 bits (= 4ε ≈ 8.88E-16) of 1.0,
+// If ‖unit_vector‖ is not within kTolerance_unit_vector_norm of 1.0,
 // writes a warning to the log file (only writes one warning per process).
-// Note: 2 bits = 2²ε = 4ε and ε = std::numeric_limits<double>::epsilon().
 // @param[in] unit_vector a vector which is allegedly a unit vector.
 // @param[in] function_name name of the function that appears in the message
 // written to the log file (if ‖unit_vector‖ is not within tolerance of 1.0).
@@ -43,18 +49,15 @@ T ThrowIfNotUnitVector(const Vector3<T>& unit_vector,
 // does not write to the log file and returns 1.0.
 // @note Frequently ‖unit_vector‖ is exactly 1.0 (e.g., when unit_vector is an
 // x or y or z axis direction). If the retval ‖unit_vector‖² is not exactly 1.0,
-// the calling function might consider normalizing unit_vector even when it
+// the calling function should consider normalizing unit_vector even when it
 // passes this test so ‖unit_vector‖ is within ε ≈ 2.22E-16 of 1.0, e.g., as
 // @code{.cpp}
 //  using std::sqrt;
 //  const T mag_squared =
 //      math::internal::ThrowIfNotUnitVector(unit_vector, __func__);
-//  const Vector3<T> uvec =
-//      (mag_squared == 1.0) ? unit_vector : unit_vector / sqrt(mag_squared);
+//  if (mag_squared != 1.0) unit_vector /= sqrt(mag_squared);
 // @endcode
-// TODO(Mitiguy) Change calls to this function to ThrowIfNotUnitVector() on the
-//  date specified in the message written to the log file. The date is specified
-//  in the implementation of WarnIfNotUnitVector() in unit_vector.cc.
+// TODO(2023-12-01) Change calls to this function to ThrowIfNotUnitVector().
 template <typename T>
 T WarnIfNotUnitVector(const Vector3<T>& unit_vector,
                       std::string_view function_name);
