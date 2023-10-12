d37d9e0736c83a5314c7ca452780e0d3306b4fed
Hongkai Dai <daihongkai@gmail.com>
2023-10-12 15:44:15 -0700

    Add ClarabelSolver.
    ClarabelSolver supports quadratic/linear costs and
    linear/second-order/exponential-cone constraints.
    It doesn't support PSD cone yet, and ChooseBestSolver doesn't select
    ClarabelSolver yet.

---
+++ solvers/no_clarabel.cc
@@ -0,0 +1,21 @@
+/* clang-format off to disable clang-format-includes */
+#include "drake/solvers/clarabel_solver.h"
+/* clang-format on */
+
+#include <stdexcept>
+
+namespace drake {
+namespace solvers {
+bool ClarabelSolver::is_available() {
+  return false;
+}
+
+void ClarabelSolver::DoSolve(const MathematicalProgram&, const Eigen::VectorXd&,
+                             const SolverOptions&,
+                             MathematicalProgramResult*) const {
+  throw std::runtime_error(
+      "The Clarabel bindings were not compiled. You'll need to use a "
+      "different solver.");
+}
+}  // namespace solvers
+}  // namespace drake
---
+++ solvers/test/clarabel_solver_test.cc
@@ -0,0 +1,296 @@
+#include "drake/solvers/clarabel_solver.h"
+
+#include <gtest/gtest.h>
+
+#include "drake/common/test_utilities/eigen_matrix_compare.h"
+#include "drake/solvers/mathematical_program.h"
+#include "drake/solvers/test/exponential_cone_program_examples.h"
+#include "drake/solvers/test/linear_program_examples.h"
+#include "drake/solvers/test/mathematical_program_test_util.h"
+#include "drake/solvers/test/quadratic_program_examples.h"
+#include "drake/solvers/test/second_order_cone_program_examples.h"
+
+namespace drake {
+namespace solvers {
+namespace test {
+const double kTol = 1E-5;
+GTEST_TEST(LinearProgramTest, TestGeneralLP) {
+  // Test a linear program with only equality constraint.
+  // min x(0) + 2 * x(1)
+  // s.t x(0) + x(1) = 2
+  // The problem is unbounded.
+  MathematicalProgram prog;
+  const auto x = prog.NewContinuousVariables<2>("x");
+  prog.AddLinearCost(x(0) + 2 * x(1));
+  prog.AddLinearConstraint(x(0) + x(1) == 2);
+  ClarabelSolver solver;
+  if (solver.available()) {
+    auto result = solver.Solve(prog, {}, {});
+    EXPECT_EQ(result.get_solution_result(), SolutionResult::kDualInfeasible);
+  }
+
+  // Now add the constraint x(1) <= 1. The problem is
+  // min x(0) + 2 * x(1)
+  // s.t x(0) + x(1) = 2
+  //     x(1) <= 1
+  // the problem should still be unbounded.
+  prog.AddBoundingBoxConstraint(-std::numeric_limits<double>::infinity(), 1,
+                                x(1));
+  if (solver.available()) {
+    auto result = solver.Solve(prog, {}, {});
+    EXPECT_EQ(result.get_solution_result(), SolutionResult::kDualInfeasible);
+  }
+
+  // Now add the constraint x(0) <= 5. The problem is
+  // min x(0) + 2x(1)
+  // s.t x(0) + x(1) = 2
+  //     x(1) <= 1
+  //     x(0) <= 5
+  // the problem should be feasible. The optimal cost is -1, with x = (5, -3)
+  prog.AddBoundingBoxConstraint(-std::numeric_limits<double>::infinity(), 5,
+                                x(0));
+  if (solver.available()) {
+    auto result = solver.Solve(prog, {}, {});
+    EXPECT_TRUE(result.is_success());
+    EXPECT_NEAR(result.get_optimal_cost(), -1, kTol);
+    const Eigen::Vector2d x_expected(5, -3);
+    EXPECT_TRUE(CompareMatrices(result.GetSolution(x), x_expected, kTol,
+                                MatrixCompareType::absolute));
+  }
+
+  // Now change the cost to 3x(0) - x(1) + 5, and add the constraint 2 <= x(0)
+  // The problem is
+  // min 3x(0) - x(1) + 5
+  // s.t x(0) + x(1) = 2
+  //     2 <= x(0) <= 5
+  //          x(1) <= 1
+  // The optimal cost is 11, the optimal solution is x = (2, 0)
+  prog.AddLinearCost(2 * x(0) - 3 * x(1) + 5);
+  prog.AddBoundingBoxConstraint(
+      2, 6 /* this upper bound = 6 is intentionally redundant.*/, x(0));
+  if (solver.available()) {
+    auto result = solver.Solve(prog, {}, {});
+    EXPECT_TRUE(result.is_success());
+    EXPECT_NEAR(result.get_optimal_cost(), 11, kTol);
+    const Eigen::Vector2d x_expected(2, 0);
+    EXPECT_TRUE(CompareMatrices(result.GetSolution(x), x_expected, kTol,
+                                MatrixCompareType::absolute));
+  }
+}
+
+GTEST_TEST(LinearProgramTest, TestInfeasibleEqualityOnlyLP) {
+  // Test a linear program with only equality constraints
+  // min x(0) + 2 * x(1)
+  // s.t x(0) + x(1) = 1
+  //     2x(0) + x(1) = 2
+  //     x(0) - 2x(1) = 3
+  // This problem is infeasible.
+  MathematicalProgram prog;
+  const auto x = prog.NewContinuousVariables<2>("x");
+  prog.AddLinearCost(x(0) + 2 * x(1));
+  prog.AddLinearEqualityConstraint(x(0) + x(1) == 1 && 2 * x(0) + x(1) == 2);
+  prog.AddLinearEqualityConstraint(x(0) - 2 * x(1) == 3);
+  ClarabelSolver clarabel_solver;
+  if (clarabel_solver.available()) {
+    auto result = clarabel_solver.Solve(prog, {}, {});
+    EXPECT_EQ(result.get_solution_result(),
+              SolutionResult::kInfeasibleConstraints);
+  }
+}
+
+GTEST_TEST(LinearProgramTest, TestAllLPConstraintTypes) {
+  // Test a linear program with bounding box, linear equality and inequality
+  // constraints
+  // min x(0) + 2 * x(1) + 3 * x(2) + 2
+  // s.t  x(0) + x(1) = 2
+  //      x(0) + 2x(2) = 3
+  //      -2 <= x(0) + 4x(1) <= 10
+  //      -5 <= x(1) + 2x(2) <= 9
+  //      -x(0) + 2x(2) <= 7
+  //      -x(1) + 3x(2) >= -10
+  //       x(0) <= 10
+  //      1 <= x(2) <= 9
+  // The optimal cost is 8, with x = (1, 1, 1)
+  MathematicalProgram prog;
+  const auto x = prog.NewContinuousVariables<3>();
+  prog.AddLinearCost(x(0) + 2 * x(1) + 3 * x(2) + 2);
+  prog.AddLinearEqualityConstraint(x(0) + x(1) == 2 && x(0) + 2 * x(2) == 3);
+  Eigen::Matrix<double, 3, 3> A;
+  // clang-format off
+  A << 1, 4, 0,
+       0, 1, 2,
+       -1, 0, 2;
+  // clang-format on
+  prog.AddLinearConstraint(
+      A, Eigen::Vector3d(-2, -5, -std::numeric_limits<double>::infinity()),
+      Eigen::Vector3d(10, 9, 7), x);
+  prog.AddLinearConstraint(-x(1) + 3 * x(2) >= -10);
+  prog.AddBoundingBoxConstraint(-std::numeric_limits<double>::infinity(), 10,
+                                x(0));
+  prog.AddBoundingBoxConstraint(1, 9, x(2));
+
+  ClarabelSolver clarabel_solver;
+  if (clarabel_solver.available()) {
+    auto result = clarabel_solver.Solve(prog, {}, {});
+    EXPECT_TRUE(result.is_success());
+    EXPECT_NEAR(result.get_optimal_cost(), 8, kTol);
+    EXPECT_TRUE(CompareMatrices(result.GetSolution(x), Eigen::Vector3d(1, 1, 1),
+                                kTol, MatrixCompareType::absolute));
+  }
+}
+
+TEST_P(LinearProgramTest, TestLP) {
+  ClarabelSolver solver;
+  prob()->RunProblem(&solver);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ClarabelTest, LinearProgramTest,
+    ::testing::Combine(::testing::ValuesIn(linear_cost_form()),
+                       ::testing::ValuesIn(linear_constraint_form()),
+                       ::testing::ValuesIn(linear_problems())));
+
+TEST_F(InfeasibleLinearProgramTest0, TestInfeasible) {
+  ClarabelSolver solver;
+  if (solver.available()) {
+    auto result = solver.Solve(*prog_, {}, {});
+    EXPECT_EQ(result.get_solution_result(),
+              SolutionResult::kInfeasibleConstraints);
+    EXPECT_EQ(result.get_optimal_cost(),
+              MathematicalProgram::kGlobalInfeasibleCost);
+  }
+}
+
+TEST_F(UnboundedLinearProgramTest0, TestUnbounded) {
+  ClarabelSolver solver;
+  if (solver.available()) {
+    auto result = solver.Solve(*prog_, {}, {});
+    EXPECT_EQ(result.get_solution_result(), SolutionResult::kDualInfeasible);
+    EXPECT_EQ(result.get_optimal_cost(), MathematicalProgram::kUnboundedCost);
+    // Make sure that we have written Clarabel's variable value into `result`.
+    EXPECT_TRUE(result.GetSolution(prog_->decision_variables())
+                    .array()
+                    .isFinite()
+                    .all());
+  }
+}
+
+TEST_F(DuplicatedVariableLinearProgramTest1, Test) {
+  ClarabelSolver solver;
+  if (solver.is_available()) {
+    CheckSolution(solver, std::nullopt, kTol);
+  }
+}
+
+TEST_P(QuadraticProgramTest, TestQP) {
+  ClarabelSolver solver;
+  if (solver.available()) {
+    prob()->RunProblem(&solver);
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ClarabelTest, QuadraticProgramTest,
+    ::testing::Combine(::testing::ValuesIn(quadratic_cost_form()),
+                       ::testing::ValuesIn(linear_constraint_form()),
+                       ::testing::ValuesIn(quadratic_problems())));
+
+GTEST_TEST(QPtest, TestUnitBallExample) {
+  ClarabelSolver solver;
+  if (solver.available()) {
+    TestQPonUnitBallExample(solver);
+  }
+}
+
+GTEST_TEST(TestDuplicatedVariableQuadraticProgram, Test) {
+  ClarabelSolver solver;
+  if (solver.available()) {
+    TestDuplicatedVariableQuadraticProgram(solver, 1E-5);
+  }
+}
+
+TEST_P(TestEllipsoidsSeparation, TestSOCP) {
+  ClarabelSolver clarabel_solver;
+  if (clarabel_solver.available()) {
+    SolveAndCheckSolution(clarabel_solver, {}, kTol);
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ClarabelTest, TestEllipsoidsSeparation,
+    ::testing::ValuesIn(GetEllipsoidsSeparationProblems()));
+
+TEST_P(TestQPasSOCP, TestSOCP) {
+  ClarabelSolver clarabel_solver;
+  if (clarabel_solver.available()) {
+    SolveAndCheckSolution(clarabel_solver, kTol);
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(ClarabelTest, TestQPasSOCP,
+                         ::testing::ValuesIn(GetQPasSOCPProblems()));
+
+TEST_P(TestFindSpringEquilibrium, TestSOCP) {
+  ClarabelSolver clarabel_solver;
+  if (clarabel_solver.available()) {
+    SolveAndCheckSolution(clarabel_solver, {}, 3E-4);
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ClarabelTest, TestFindSpringEquilibrium,
+    ::testing::ValuesIn(GetFindSpringEquilibriumProblems()));
+
+GTEST_TEST(TestSOCP, MaximizeGeometricMeanTrivialProblem1) {
+  MaximizeGeometricMeanTrivialProblem1 prob;
+  ClarabelSolver solver;
+  if (solver.available()) {
+    const auto result = solver.Solve(prob.prog(), {}, {});
+    // Practically I observe Clarabel requires looser tolerance for this test. I
+    // don't know why.
+    prob.CheckSolution(result, 3 * kTol);
+  }
+}
+
+GTEST_TEST(TestSOCP, MaximizeGeometricMeanTrivialProblem2) {
+  MaximizeGeometricMeanTrivialProblem2 prob;
+  ClarabelSolver solver;
+  if (solver.available()) {
+    const auto result = solver.Solve(prob.prog(), {}, {});
+    prob.CheckSolution(result, kTol);
+  }
+}
+
+GTEST_TEST(TestSOCP, SmallestEllipsoidCoveringProblem) {
+  ClarabelSolver solver;
+  SolveAndCheckSmallestEllipsoidCoveringProblems(solver, {}, kTol);
+}
+
+GTEST_TEST(TestExponentialConeProgram, ExponentialConeTrivialExample) {
+  ClarabelSolver solver;
+  if (solver.available()) {
+    // Currently we don't support retrieving dual solution for exponential cone
+    // constraints from Clarabel yet.
+    ExponentialConeTrivialExample(solver, 2E-4, false);
+  }
+}
+
+GTEST_TEST(TestExponentialConeProgram, MinimizeKLDivengence) {
+  ClarabelSolver clarabel_solver;
+  if (clarabel_solver.available()) {
+    MinimizeKLDivergence(clarabel_solver, 1E-4);
+  }
+}
+
+// TODO(hongkai.dai): Enable this test when ClarabelSolver supports PSD
+// constraints.
+// GTEST_TEST(TestExponentialConeProgram, MinimalEllipsoidConveringPoints) {
+//  ClarabelSolver clarabel_solver;
+//  if (clarabel_solver.available()) {
+//    MinimalEllipsoidCoveringPoints(clarabel_solver, 1E-4);
+//  }
+//}
+
+}  // namespace test
+}  // namespace solvers
+}  // namespace drake
