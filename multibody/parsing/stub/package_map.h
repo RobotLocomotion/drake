94b40808211e71160d7124f82d87fa2247441a51
Sean Curtis <sean.curtis@tri.global>
2023-10-17 09:35:33 -0700

    Add multibody::PackageMap stub

---
+++ multibody/parsing/stub/package_map.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "drake/common/parsing/package_map.h"
+
+namespace drake {
+namespace multibody {
+
+using PackageMap = drake::PackageMap;
+
+}  // namespace multibody
+}  // namespace drake
