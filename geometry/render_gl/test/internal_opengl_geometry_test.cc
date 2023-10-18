a99d654244f9229be05be1fac3de08e21add23c6
Sean Curtis <sean.curtis@tri.global>
2023-10-18 11:32:27 -0700

    Generalize RenderEngineGl's geometries and instances
    Previously, RenderEngineGl would assume a great deal about the geometries
    and how they get posed in the world. This changes those assumptions:
    
    - Geometry primitives are no longer assumed by RenderEngineGl::RenderAt().
      - Each OpenGlGeometry records the nature of its primitives and that is
        used at render time.
    - The vertex attributes can be organized more generally in a dat array. I.e,
      rather than the previous hard-coded grouping, a source could choose to
      present them interleaved (this can happen in future glTF parsing).
      - Each OpenGlGeometry stores the interepretation of its vertex data as
        stored in the associated buffer object (to facilitate cloning).
    - We no longer assume the OpenGlGeometry data is measured and expressed in
      a frame that only differs from Drake's geometry frame by an optional
      scale factor.
       - A single Drake "geometry" can be comprised of multiple OpenGlInstances
         each with their own poses. And the underlying geometry can also
         have the vertex information expressed in yet another frame.
       - OpenGlGeometry and OpenGlInstance pick up new frame semantics to
         track this more general articulation of geometry.
       - This led to a redundancy in RenderEngineGl::Part, and that struct has
         been removed.
       - Shader programs have been updated to accept the various model
         transforms directly, rather than attempting to compute them on the
         fly.

--- geometry/render_gl/test/internal_opengl_geometry_test.cc
+++ geometry/render_gl/test/internal_opengl_geometry_test.cc
@@ -12,60 +12,116 @@ namespace render_gl {
 namespace internal {
 namespace {
 
+using Eigen::DiagonalMatrix;
+using Eigen::Matrix3f;
+using Eigen::Matrix4f;
 using Eigen::Vector3d;
+using Eigen::Vector3f;
+using Eigen::Vector4f;
 using math::RigidTransformd;
 using render::RenderLabel;
 
 // Note: All values in these tests are effectively garbage. They are not really
 // names of OpenGL objects. These tests merely exercise the functionality of the
-// OpenGlGeometry class which does not actually depend on the object names to
-// actually refer to objects in an OpenGL context.
+// OpenGlGeometry class which does not depend on the object names to actually
+// refer to objects in an OpenGL context.
 
 GTEST_TEST(OpenGlGeometryTest, Construction) {
-  const OpenGlGeometry default_geometry;
-  EXPECT_EQ(default_geometry.vertex_array, OpenGlGeometry::kInvalid);
-  EXPECT_EQ(default_geometry.vertex_buffer, OpenGlGeometry::kInvalid);
-  EXPECT_EQ(default_geometry.index_buffer, OpenGlGeometry::kInvalid);
-  EXPECT_EQ(default_geometry.index_buffer_size, 0);
-  EXPECT_EQ(default_geometry.v_count, 0);
-
-  const OpenGlGeometry geometry{1, 2, 3, 4, 7};
+  // clang-format off
+  const Matrix4f T_MN = (Matrix4f() << 1,  2,  3,  4,
+                                       5,  6,  7,  8,
+                                       9,  10, 11, 12,
+                                       13, 14, 15, 16).finished();
+  const Matrix3f N_MN = (Matrix3f() << 21, 22, 23,
+                                       24, 25, 26,
+                                       27, 28, 29).finished();
+  // clang-format on
+  const OpenGlGeometry geometry{.vertex_array = 1,
+                                .vertex_buffer = 2,
+                                .index_buffer = 3,
+                                .v_count = 4,
+                                .index_count = 5,
+                                .type = 17,
+                                .mode = 18,
+                                .T_MN = T_MN,
+                                .N_MN = N_MN};
   EXPECT_EQ(geometry.vertex_array, 1);
   EXPECT_EQ(geometry.vertex_buffer, 2);
   EXPECT_EQ(geometry.index_buffer, 3);
-  EXPECT_EQ(geometry.index_buffer_size, 4);
-  EXPECT_EQ(geometry.v_count, 7);
-
-  DRAKE_EXPECT_THROWS_MESSAGE(OpenGlGeometry(1, 2, 3, -1, 1),
-                              "Index buffer size must be non-negative");
+  EXPECT_EQ(geometry.v_count, 4);
+  EXPECT_EQ(geometry.index_count, 5);
+  EXPECT_EQ(geometry.type, 17);
+  EXPECT_EQ(geometry.mode, 18);
+  EXPECT_TRUE(CompareMatrices(geometry.T_MN, T_MN));
+  EXPECT_TRUE(CompareMatrices(geometry.N_MN, N_MN));
 }
 
 GTEST_TEST(OpenGlGeometryTest, IsDefined) {
   const GLuint kInvalid = OpenGlGeometry::kInvalid;
 
-  EXPECT_TRUE(OpenGlGeometry(1, 2, 3, 4, 1).is_defined());
-  EXPECT_FALSE(OpenGlGeometry(kInvalid, 2, 3, 4, 1).is_defined());
-  EXPECT_FALSE(OpenGlGeometry(1, kInvalid, 3, 4, 1).is_defined());
-  EXPECT_FALSE(OpenGlGeometry(1, 2, kInvalid, 4, 1).is_defined());
-  EXPECT_FALSE(OpenGlGeometry(kInvalid, kInvalid, 3, 4, 1).is_defined());
-  EXPECT_FALSE(OpenGlGeometry(kInvalid, 2, kInvalid, 4, 1).is_defined());
-  EXPECT_FALSE(OpenGlGeometry(1, kInvalid, kInvalid, 4, 1).is_defined());
-  EXPECT_FALSE(OpenGlGeometry(kInvalid, kInvalid, kInvalid, 4, 1).is_defined());
+  auto make_geo = [](GLuint a, GLuint b, GLuint c, int d, int e) {
+    return OpenGlGeometry{.vertex_array = a,
+                          .vertex_buffer = b,
+                          .index_buffer = c,
+                          .v_count = d,
+                          .index_count = e};
+  };
+
+  EXPECT_TRUE(make_geo(1, 2, 3, 4, 5).is_defined());
+  EXPECT_FALSE(make_geo(kInvalid, 2, 3, 4, 5).is_defined());
+  EXPECT_FALSE(make_geo(1, kInvalid, 3, 4, 5).is_defined());
+  EXPECT_FALSE(make_geo(1, 2, kInvalid, 4, 5).is_defined());
+  EXPECT_FALSE(make_geo(1, 2, 3, kInvalid, 5).is_defined());
+  EXPECT_FALSE(make_geo(1, 2, 3, 4, kInvalid).is_defined());
+  EXPECT_FALSE(make_geo(kInvalid, kInvalid, 3, 4, 5).is_defined());
+  EXPECT_FALSE(make_geo(kInvalid, 2, kInvalid, 4, 5).is_defined());
+  EXPECT_FALSE(make_geo(1, kInvalid, kInvalid, 4, 5).is_defined());
+  EXPECT_FALSE(make_geo(kInvalid, kInvalid, kInvalid, 4, 5).is_defined());
 }
 
 GTEST_TEST(OpenGlGeometryTest, ThrowIfUndefined) {
-  const OpenGlGeometry valid{1, 2, 3, 4, 1};
+  const OpenGlGeometry valid{.vertex_array = 1,
+                             .vertex_buffer = 2,
+                             .index_buffer = 3,
+                             .v_count = 4,
+                             .index_count = 5};
   EXPECT_NO_THROW(valid.throw_if_undefined("test message"));
   DRAKE_EXPECT_THROWS_MESSAGE(
-      OpenGlGeometry().throw_if_undefined("default is undefined"),
+      OpenGlGeometry{.vertex_array = 1}.throw_if_undefined(
+          "default is undefined"),
       "default is undefined");
 }
 
-GTEST_TEST(OpenGlInstanceTest, Construction) {
+// We don't explicitly test construction as it is intended to simply used
+// designated initializers. But we do want to test the validation.
+GTEST_TEST(OpenGlInstanceTest, Validity) {
+  // We need a geometry to reference, but we don't need valid transforms.
+  // clang-format off
+  const Matrix4f T_MN = (Matrix4f() << 1,  2,  3,  4,
+                                       5,  6,  7,  8,
+                                       9,  10, 11, 12,
+                                       13, 14, 15, 16).finished();
+  const Matrix3f N_MN = (Matrix3f() << 21, 22, 23,
+                                       24, 25, 26,
+                                       27, 28, 29).finished();
+  // clang-format on
+  const OpenGlGeometry geometry{.vertex_array = 1,
+                                .vertex_buffer = 2,
+                                .index_buffer = 3,
+                                .v_count = 4,
+                                .index_count = 7,
+                                .type = 17,
+                                .mode = 18,
+                                .T_MN = T_MN,
+                                .N_MN = N_MN};
+  const Vector4f scale(10, 20, 30, 1.0);
+  const Matrix4f T_GN_expected = DiagonalMatrix<float, 4>(scale) * T_MN;
+  const Vector3f scale_inv(1.0 / scale.x(), 1.0 / scale.y(), 1.0 / scale.z());
+  const Matrix3f N_GN_expected = DiagonalMatrix<float, 3>(scale_inv) * N_MN;
+
   // This is a dummy index that doesn't reference into any particular geometry.
   const int geometry_index = 2;
   const RigidTransformd X_WG{Vector3d{-1, -2, 3}};
-  const Vector3d scale{0.25, 0.5, 0.75};
   // We'll create a pretend block of depth data; simply a double value with no
   // real meaning.
   const ShaderProgramData depth_data{ShaderId::get_new_id(),
@@ -75,13 +131,12 @@ GTEST_TEST(OpenGlInstanceTest, Construction) {
   const ShaderProgramData color_data(ShaderId::get_new_id(),
                                      AbstractValue::Make(33));
 
-  const OpenGlInstance instance{geometry_index, X_WG,       scale,
-                                color_data,     depth_data, label_data};
+  const OpenGlInstance instance(geometry_index, scale.head<3>(), geometry,
+                                color_data, depth_data, label_data);
 
   EXPECT_EQ(instance.geometry, geometry_index);
-  EXPECT_TRUE(
-      CompareMatrices(instance.X_WG.GetAsMatrix34(), X_WG.GetAsMatrix34()));
-  EXPECT_TRUE(CompareMatrices(instance.scale, scale));
+  EXPECT_TRUE(CompareMatrices(instance.T_GN, T_GN_expected));
+  EXPECT_TRUE(CompareMatrices(instance.N_GN, N_GN_expected));
 
   EXPECT_EQ(
       instance.shader_data[RenderType::kDepth].value().get_value<double>(),
