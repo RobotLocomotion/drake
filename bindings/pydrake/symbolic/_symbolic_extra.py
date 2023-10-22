5e10d357d36a7e34994bef5d5fd47c83e472d1d6
Drewbot <contact@drewhamilton.net>
2023-10-22 17:11:37 -0400

    Update _symbolic_extra.py
    float to integer rounding to clean up 16-decimal place floats with all zeros.

--- bindings/pydrake/symbolic/_symbolic_extra.py
+++ bindings/pydrake/symbolic/_symbolic_extra.py
@@ -30,10 +30,11 @@ _symbolic_sympy_defer = None
 
 
 def to_sympy(
-    x: typing.Union[float, bool, Variable, Expression, Formula],
+    x: typing.Union[float, int, bool, Variable, Expression, Formula],
     *,
-    memo: typing.Dict = None
-) -> typing.Union[float, bool, "sympy.Expr"]:
+    memo: typing.Dict = None,
+    round_ints: bool = False
+) -> typing.Union[float, int, bool, "sympy.Expr"]:
     """Converts a pydrake object to the corresponding SymPy Expr.
 
     Certain expressions are not supported and will raise NotImplementedError.
@@ -59,14 +60,15 @@ def to_sympy(
         from pydrake.symbolic import _symbolic_sympy as _symbolic_sympy_defer
     if memo is None:
         memo = dict()
-    return _symbolic_sympy_defer._to_sympy(x, memo=memo)
+    return _symbolic_sympy_defer._to_sympy(x, memo=memo, round_ints=round_ints)
 
 
 def from_sympy(
-    x: typing.Union[float, bool, "sympy.Expr"],
+    x: typing.Union[float, int, bool, "sympy.Expr"],
     *,
-    memo: typing.Dict = None
-) -> typing.Union[float, bool, Variable, Expression, Formula]:
+    memo: typing.Dict = None,
+    round_ints: bool = False
+) -> typing.Union[float, int, bool, Variable, Expression, Formula]:
     """Converts a SymPy Expr to the corresponding pydrake object.
 
     Certain expressions are not supported and will raise NotImplementedError.
@@ -91,4 +93,4 @@ def from_sympy(
         from pydrake.symbolic import _symbolic_sympy as _symbolic_sympy_defer
     if memo is None:
         memo = dict()
-    return _symbolic_sympy_defer._from_sympy(x, memo=memo)
+    return _symbolic_sympy_defer._from_sympy(x, memo=memo, round_ints=round_ints)
