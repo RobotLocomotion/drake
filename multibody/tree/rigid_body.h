2df60c04d76a0af3721ab950b4f483a2ac1f053d
Michael Sherman <sherm1@gmail.com>
2023-10-25 17:01:26 -0700

    Eliminate mention of flexible body support from MultibodyTree

--- multibody/tree/rigid_body.h
+++ multibody/tree/rigid_body.h
@@ -20,6 +20,10 @@
 namespace drake {
 namespace multibody {
 
+// TODO(sherm1) Since there are now only rigid bodies, this functionality should
+//  move to the Body base class with RigidBody removed or left as an alias for
+//  Body.
+
 /// The term **rigid body** implies that the deformations of the body under
 /// consideration are so small that they have no significant effect on the
 /// overall motions of the body and therefore deformations can be neglected.
@@ -75,16 +79,6 @@ class RigidBody : public Body<T> {
   RigidBody(const std::string& body_name,
             ModelInstanceIndex model_instance,
             const SpatialInertia<double>& M_BBo_B);
-
-  /// There are no flexible degrees of freedom associated with a rigid body and
-  /// therefore this method returns zero. By definition, a rigid body has no
-  /// state associated with flexible deformations.
-  int get_num_flexible_positions() const final { return 0; }
-
-  /// There are no flexible degrees of freedom associated with a rigid body and
-  /// therefore this method returns zero. By definition, a rigid body has no
-  /// state associated with flexible deformations.
-  int get_num_flexible_velocities() const final { return 0; }
 
   /// Returns this rigid body's default mass, which is initially supplied at
   /// construction when specifying this rigid body's SpatialInertia.
@@ -173,7 +167,7 @@ class RigidBody : public Body<T> {
   }
 
   // TODO(joemasterjohn): Speed this up when we can store a reference to a
-  // SpatialInertia<T> as an abstract parameter.
+  //  SpatialInertia<T> as an abstract parameter.
 
   /// Gets this body's spatial inertia about its origin from the given context.
   /// @param[in] context contains the state of the multibody system.
@@ -271,9 +265,6 @@ class RigidBody : public Body<T> {
   /// kinematics).
   /// @param[in] pc position kinematics cache.
   /// @retval X_WB pose of rigid body B in world frame W.
-  // TODO(amcastro-tri) When cache entries are in the context, replace this
-  // method by Body<T>::get_pose_in_world(const Context<T>&).
-  //----------------------------------------------------------------------------
   const math::RigidTransform<T>& get_pose_in_world(
       const internal::PositionKinematicsCache<T>& pc) const {
     return pc.get_X_WB(this->node_index());
@@ -309,9 +300,6 @@ class RigidBody : public Body<T> {
   /// @param[in] vc velocity kinematics cache.
   /// @retval V_WB_W `this` rigid body B's spatial velocity in the world
   /// frame W, expressed in W (for point Bo, the body frame's origin).
-  // TODO(amcastro-tri) When cache entries are in the context, replace this
-  //  method by Body<T>::get_spatial_velocity_in_world(const Context<T>&).
-  //----------------------------------------------------------------------------
   const SpatialVelocity<T>& get_spatial_velocity_in_world(
       const internal::VelocityKinematicsCache<T>& vc) const {
     return vc.get_V_WB(this->node_index());
@@ -346,9 +334,6 @@ class RigidBody : public Body<T> {
   /// @param[in] ac acceleration kinematics cache.
   /// @retval A_WB_W `this` rigid body B's spatial acceleration in the world
   /// frame W, expressed in W (for point Bo, the body frame's origin).
-  // TODO(amcastro-tri) When cache entries are in the context, replace this
-  // method by Body<T>::get_spatial_acceleration_in_world(const Context<T>&).
-  //----------------------------------------------------------------------------
   const SpatialAcceleration<T>& get_spatial_acceleration_in_world(
       const internal::AccelerationKinematicsCache<T>& ac) const {
     return ac.get_A_WB(this->node_index());
