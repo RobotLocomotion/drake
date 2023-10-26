0f5b1c13d5222f793f605eeaac1a9945856e99fb
Sean Curtis <sean.curtis@tri.global>
2023-10-26 13:42:23 -0700

    Meshcat can report "current" meshcat camera pose
    - model_visualizer enables camera tracking and makes use of the pose.
      - As the render view is no longer independent of the meshcat camera, we
        eliminate the visualization of the frustum
      - Speed up the maximum render rate (however, we don't blindly render).
    - Simplifies the dmd.yaml and sdf ecosystem to support the camera.

--- geometry/meshcat.h
+++ geometry/meshcat.h
@@ -505,6 +505,32 @@ class Meshcat {
   void SetCameraPose(const Eigen::Vector3d& camera_in_world,
                      const Eigen::Vector3d& target_in_world);
 
+  /** If camera tracking is enabled, the %Meshcat instance will track the
+   position and orientation of the in-browser camera and make it available
+   from GetTrackedCameraPose().
+
+   This comes with some caveats:
+
+     - If there are multiple browsers connected to the same meshcat server,
+       they will fight each other; the last client to move its camera will
+       be the reported pose. If tracking the camera, it is best to use only a
+       single browser window.
+     - This setting is *not* persisted in a static html version of the session.
+     - It will only work with perspective cameras; if the camera is
+       orthographic, GetTrackedCameraPose() will persist in broadcasting the
+       last known perspective camera's transform.
+  */
+  void SetCameraTracking(bool on);
+
+  /** Returns the most recent tracked camera position. The identity transform is
+   returned if:
+
+     - SetCameraTracking() has been disabled, or
+     - SetCameraTracking() has been enabled, but we haven't received pose
+       information from meshcat yet.
+   */
+  math::RigidTransformd GetTrackedCameraPose() const;
+
   // TODO(SeanCurtis-TRI): Consider the API:
   //  void SetCameraPose(const RigidTransformd& X_WC, bool target_distance = 1);
   // We'll have to confirm that picking arbitrary rotations R_WC doesn't
--- tools/workspace/meshcat/repository.bzl
+++ tools/workspace/meshcat/repository.bzl
@@ -5,13 +5,13 @@ def meshcat_repository(
         mirrors = None):
     github_archive(
         name = name,
-        repository = "meshcat-dev/meshcat",
+        repository = "SeanCurtis-TRI/meshcat",
         upgrade_advice = """
         Updating this commit requires local testing; see
         drake/tools/workspace/meshcat/README.md for details.
         """,
-        commit = "fa29aecebd5f1712844c3b98967a7d21707df5a0",
-        sha256 = "bdd505e90ab4e46958714a909f0a45d42d905eee0fa87637b97092035a4df7a3",  # noqa
+        commit = "b48a04c7dba9e041e2f46476bd426981b59467b2",
+        sha256 = "6287c1deb1eae8a9802984e3d1da0a5ce20008fd9c374322e29e9af201c65c38",  # noqa
         build_file = ":package.BUILD.bazel",
         mirrors = mirrors,
     )
