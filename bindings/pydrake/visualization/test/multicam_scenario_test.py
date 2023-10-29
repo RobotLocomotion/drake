e49c90439bb393d7b02d2ffe17a0ff28f3b0648d
Zach Fang <zach.fang@tri.global>
2023-10-28 21:11:13 -0400

    [visualization] add multi-cam scenario for testing

---
+++ bindings/pydrake/visualization/test/multicam_scenario_test.py
@@ -0,0 +1,32 @@
+import os.path
+import subprocess
+import unittest
+
+from bazel_tools.tools.python.runfiles.runfiles import Create as CreateRunfiles
+import yaml
+
+
+class TestMulticamScenario(unittest.TestCase):
+    def _find_resource(self, respath):
+        runfiles = CreateRunfiles()
+        result = runfiles.Rlocation(respath)
+        self.assertTrue(result, respath)
+        self.assertTrue(os.path.exists(result), respath)
+        return result
+
+    def test_smoke(self):
+        """Runs `multicam_scenario.yaml` and checks it doesn't crash."""
+        simulator = self._find_resource(
+            "drake/examples/hardware_sim/hardware_sim_py"
+        )
+        test_scenarios = self._find_resource(
+            "drake/bindings/pydrake/visualization/test/multicam_scenario.yaml"
+        )
+        run_args = [
+            simulator,
+            f"--scenario_file={test_scenarios}",
+            "--scenario_name=Multicam",
+            # For the smoke test, exit fairly quickly.
+            "--scenario_text={simulation_duration: 0.0625}",
+        ]
+        subprocess.run(run_args, check=True)
