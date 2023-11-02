3850501481ef2eb4d5873719280629fefeb4cc76
Xuchen Han <xuchen.han@tri.global>
2023-11-01 17:24:01 -0700

    Rework deformable torus example to demo external force
    Add PointSourceForceField as an example of a concrete ForceDensityField.
    
    Add a `gripper` flag so that one can toggle between a parallel gripper
    and a suction gripper to pick up the deformable torus.
    
    Refactor the controller into separate files to reduce the lines of code
    in the example .cc file.
    
    Change the parallel fingers to capsule shape to avoid sticking resulting
    from the sharp change in contact normal from boxes.

---
+++ examples/multibody/deformable_torus/parallel_gripper_controller.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "drake/systems/framework/leaf_system.h"
+
+namespace drake {
+namespace examples {
+namespace deformable_torus {
+
+/* We create a leaf system that outputs the desired state of a parallel jaw
+ gripper to follow a close-lift-open motion sequence. The desired position is
+ 2-dimensional with the first element corresponding to the wrist degree of
+ freedom and the second element corresponding to the left finger degree of
+ freedom. This control is a time-based state machine, where desired state
+ changes based on the context time. There are four states, executed in the
+ following order:
+
+  0. The fingers are open in the initial state.
+  1. The fingers are closed to secure a grasp.
+  2. The gripper is lifted to a prescribed final height.
+  3. The fingers are open to loosen the grasp.
+
+ The desired state is interpolated between these states. */
+class ParallelGripperController : public systems::LeafSystem<double> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(ParallelGripperController);
+
+  /* Constructs a ParallelGripperController system with the given parameters.
+   @param[in] open_width   The width between fingers in the open state. (meters)
+   @param[in] closed_width The width between fingers in the closed state.
+                           (meters)
+   @param[in] height       The height of the gripper in the lifted state.
+                           (meters) */
+  ParallelGripperController(double open_width, double closed_width,
+                            double height);
+
+ private:
+  /* Computes the output desired state of the parallel gripper. */
+  void CalcDesiredState(const systems::Context<double>& context,
+                        systems::BasicVector<double>* output) const;
+
+  /* The time at which the fingers reach the desired closed state. */
+  const double fingers_closed_time_{1.5};
+  /* The time at which the gripper reaches the desired "lifted" state. */
+  const double gripper_lifted_time_{3.0};
+  const double hold_time_{5.5};
+  /* The time at which the fingers reach the desired open state. */
+  const double fingers_open_time_{7.0};
+  Eigen::Vector2d initial_configuration_;
+  Eigen::Vector2d closed_configuration_;
+  Eigen::Vector2d lifted_configuration_;
+  Eigen::Vector2d open_configuration_;
+};
+
+}  // namespace deformable_torus
+}  // namespace examples
+}  // namespace drake
---
+++ examples/multibody/deformable_torus/suction_cup_controller.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "drake/multibody/plant/multibody_plant.h"
+
+namespace drake {
+namespace examples {
+namespace deformable_torus {
+
+/* We create a leaf system that outputs the desire state of a suction cup
+ gripper mounted on a prismatic joint to the world. This control is a time-based
+ state machine, where the desired state changes based on the context time. The
+ desired state is described through two output ports, one containing the desired
+ position and velocity of the prismatic joint to the world, the other containing
+ the maximum suction force density. The suction cup is first lowered to approach
+ the object, then the suction force is turned on to pick up the object. Then the
+ suction cup is raised to the initial height and the suction force is turned off
+ to release the object. This is strictly for demo purposes and is not intended
+ as a generalized model for suction grippers. */
+class SuctionCupController : public systems::LeafSystem<double> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(SuctionCupController);
+
+  /* Constructs a SuctionCupController system with the given parameters. The
+   trajectory is characterized by 4 key time stamps and 2 key positions. The 4
+   key time stamps are `approach_time`, `start_suction_time`,
+   `retrieve_time`, and `release_suction_time`. They must be in increasing
+   order. The 2 key positions are `initial_height` and `object_height`.
+   @param[in] initial_height        Initial height of the gripper.
+   @param[in] object_height         The estimated height of the object. We turn
+                                    suction on when the gripper first reach this
+                                    height.
+   @param[in] approach_time         The time at which the gripper starts to
+                                    approach the manipuland.
+   @param[in] start_suction_time    The time at which suction force turns on.
+   @param[in] retrieve_time         The time at which to start lifting the
+                                    gripper.
+   @param[in] release_suction_time  The time at which suction turns off. */
+  SuctionCupController(double initial_height, double object_height,
+                       double approach_time, double start_suction_time,
+                       double retrieve_time, double release_suction_time);
+
+  const systems::OutputPort<double>& desired_state_output_port() const {
+    return get_output_port(desired_state_port_index_);
+  }
+
+  const systems::OutputPort<double>& maximum_force_density_port() const {
+    return get_output_port(maximum_force_density_port_index_);
+  }
+
+ private:
+  /* Computes the output desired state of the suction cup. */
+  void CalcDesiredState(const systems::Context<double>& context,
+                        systems::BasicVector<double>* desired_state) const;
+
+  /* Computes the maximum suction force in Newtons based on time. */
+  void CalcMaxForceDensity(
+      const systems::Context<double>& context,
+      systems::BasicVector<double>* max_force_density) const;
+
+  double initial_height_{};
+  double object_height_{};
+  double approach_time_{};
+  double start_suction_time_{};
+  double retrieve_time_{};
+  double release_suction_time_{};
+
+  int desired_state_port_index_{};
+  int maximum_force_density_port_index_{};
+};
+
+}  // namespace deformable_torus
+}  // namespace examples
+}  // namespace drake
