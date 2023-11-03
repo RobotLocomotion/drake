07a46ba8a7d73b86b9440644c6ca4d70e8212d42
Brian-Acosta <bjacosta@seas.upenn.edu>
2023-11-03 16:06:06 -0400

    clean up benchmark

--- perception/test/downsample_benchmark.cc
+++ perception/test/downsample_benchmark.cc
@@ -2,14 +2,19 @@
 #include <iostream>
 #include "drake/perception/point_cloud.h"
 
-namespace drake::perception {
-
-int do_main() {
-  std::srand(1234);
+namespace drake {
+namespace perception {
+namespace {
+int DoMain() {
+  std::srand(5432);
   auto pc_xyz = PointCloud(1000000, pc_flags::kXYZs);
   pc_xyz.mutable_xyzs().setRandom();
+
   auto pc_maximal = PointCloud(1000000, pc_flags::kXYZs | pc_flags::kRGBs |
       pc_flags::kNormals | pc_flags::kDescriptorCurvature);
+
+  // make the spatial coordinates the same to avoid time differences due to
+  // spatial hashing
   pc_maximal.mutable_xyzs() = pc_xyz.xyzs();
   pc_maximal.mutable_rgbs().setRandom();
   pc_maximal.mutable_normals().setRandom();
@@ -23,18 +28,19 @@ int do_main() {
 
   double duration_xyz = static_cast<std::chrono::duration<double>>(
       post_xyz - start
-    ).count();
-
+  ).count();
   double duration_max = static_cast<std::chrono::duration<double>>(
       post_max - start
   ).count();
 
-  std::cout << "xyz only time " << duration_xyz <<
-                " s\nmaximal fields time " << duration_max << std::endl;
+  std::cout << "xyz only time " << duration_xyz << std::endl
+            << "maximal fields time " << duration_max << std::endl;
   return 0;
+}
+}
 }
 }
 
 int main(int argc, char *argv[]) {
-  drake::perception::do_main();
+  drake::perception::DoMain();
 }
