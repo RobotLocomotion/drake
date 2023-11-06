e2dd88f4e738781af76b47ec3a5572aa1746034a
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-11-06 12:07:47 -0800

    move benchmark to 'benchmarking' folder and clang-format it

--- perception/benchmarking/downsample_benchmark.cc
+++ perception/benchmarking/downsample_benchmark.cc
@@ -12,7 +12,8 @@ int DoMain() {
   pc_xyz.mutable_xyzs().setRandom();
 
   auto pc_maximal = PointCloud(1000000, pc_flags::kXYZs | pc_flags::kRGBs |
-      pc_flags::kNormals | pc_flags::kDescriptorCurvature);
+                                            pc_flags::kNormals |
+                                            pc_flags::kDescriptorCurvature);
 
   // make the spatial coordinates the same to avoid time differences due to
   // spatial hashing
@@ -27,10 +28,10 @@ int DoMain() {
   auto pc_down_max = pc_maximal.VoxelizedDownSample(0.02, false);
   auto post_max = std::chrono::high_resolution_clock::now();
 
-  double duration_xyz = static_cast<std::chrono::duration<double>>(
-      post_xyz - start).count();
-  double duration_max = static_cast<std::chrono::duration<double>>(
-      post_max - start).count();
+  double duration_xyz =
+      static_cast<std::chrono::duration<double>>(post_xyz - start).count();
+  double duration_max =
+      static_cast<std::chrono::duration<double>>(post_max - start).count();
 
   std::cout << "xyz only time " << duration_xyz << std::endl
             << "maximal fields time " << duration_max << std::endl;
