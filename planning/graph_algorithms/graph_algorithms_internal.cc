eca8f3b4c53abfe9027dbb5b150560f0574bd6c1
amice <amice@mit.edu>
2023-11-06 10:41:44 -0500

    wip

--- planning/graph_algorithms/graph_algorithms_internal.cc
+++ planning/graph_algorithms/graph_algorithms_internal.cc
@@ -6,9 +6,6 @@ namespace planning {
 namespace graph_algorithms {
 namespace internal {
 
-// Given the adjacency matrix of a graph G, return the complement graph H. The
-// complement graph H has the same vertex set as G, with vertices (i,j) forming
-// an edge in H if and only if they do not form an edge in G.
 Eigen::SparseMatrix<bool> ComplementaryAdjacencyMatrix(
     const Eigen::Ref<const Eigen::SparseMatrix<bool>>& adjacency_matrix) {
   const int n = adjacency_matrix.rows();
@@ -30,8 +27,6 @@ Eigen::SparseMatrix<bool> ComplementaryAdjacencyMatrix(
   return ret;
 }
 
-// Given a list of triplets, add the symmetric component to the triplet list.
-// This is useful when constructing adjacency matrices.
 void SymmetrizeTripletList(
     std::vector<Eigen::Triplet<bool>>* expected_entries) {
   expected_entries->reserve(static_cast<int>(2 * expected_entries->size()));
--- planning/graph_algorithms/graph_algorithms_internal.h
+++ planning/graph_algorithms/graph_algorithms_internal.h
@@ -12,7 +12,9 @@ namespace internal {
 
 // Given the adjacency matrix of a graph G, return the complement graph H. The
 // complement graph H has the same vertex set as G, with vertices (i,j) forming
-// an edge in H if and only if they do not form an edge in G.
+// an edge in H if and only if they do not form an edge in G. Note that we
+// always exclude self-loops i.e. the diagonal of the returned matrix is always
+// false.
 Eigen::SparseMatrix<bool> ComplementaryAdjacencyMatrix(
     const Eigen::Ref<const Eigen ::SparseMatrix<bool>>& adjacency_matrix);
 
