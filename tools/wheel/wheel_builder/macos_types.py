28490ec3404aa6eba994ca7fa94859f236828a8f
Matthew Woehlke <matthew.woehlke@kitware.com>
2023-12-14 15:23:46 -0500

    fixup

---
+++ tools/wheel/wheel_builder/macos_types.py
@@ -0,0 +1,14 @@
+# This file contains data types used by the macOS-specific build logic. See
+# //tools/wheel:builder for the user interface.
+
+from dataclasses import dataclass
+from typing import Tuple
+
+
+class PythonTarget:
+    def __init__(self, *version_parts):
+        pv_parts = tuple(map(str, version_parts))
+        self.version_tuple = tuple(version_parts)
+        self.version_full = '.'.join(pv_parts)
+        self.version = '.'.join(pv_parts[:2])
+        self.tag = ''.join(pv_parts[:2])
