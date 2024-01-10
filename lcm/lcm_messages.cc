09ed2eeb8de4f22030290f9bca2dd75ce2c4383e
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-01-10 11:57:32 -0800

    [lcm] Notice errors during message encoding

--- lcm/lcm_messages.cc
+++ lcm/lcm_messages.cc
@@ -1,1 +1,20 @@
 #include "drake/lcm/lcm_messages.h"
+
+#include <stdexcept>
+
+#include <fmt/format.h>
+
+namespace drake {
+namespace lcm {
+namespace internal {
+
+void ThrowLcmEncodeDecodeError(const char* operation,
+                               const std::type_info& message_type) {
+  throw std::runtime_error(fmt::format("Error {} message of type '{}'",
+                                       operation,
+                                       NiceTypeName::Get(message_type)));
+}
+
+}  // namespace internal
+}  // namespace lcm
+}  // namespace drake
