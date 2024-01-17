91eabf7dd68a6e85fc2c8c09c96bd317521c2c72
Xuchen Han <xuchen.han@tri.global>
2024-01-17 14:42:11 -0800

    [render] Make RenderMesh from TriangleSurfaceMesh

--- geometry/render/render_mesh.h
+++ geometry/render/render_mesh.h
@@ -7,6 +7,7 @@
 
 #include "drake/common/diagnostic_policy.h"
 #include "drake/geometry/geometry_properties.h"
+#include "drake/geometry/proximity/triangle_surface_mesh.h"
 #include "drake/geometry/render/render_material.h"
 #include "drake/geometry/rgba.h"
 
@@ -90,6 +91,17 @@ struct RenderMesh {
 std::vector<RenderMesh> LoadRenderMeshesFromObj(
     const std::filesystem::path& obj_path, const GeometryProperties& properties,
     const Rgba& default_diffuse,
+    const drake::internal::DiagnosticPolicy& policy = {});
+
+/* Constructs a render mesh from a triangle surface mesh. The vertex normals are
+ computed using area-weighted face normals incident to the vertex. UvState
+ will be set to UvState::kNone and all uv coordinates are set to zero. The
+ material of the resulting render mesh is created using the protocol in
+ MakeMeshFallbackMaterial() with the given `properties` and `default_diffuse`.
+ */
+RenderMesh MakeRenderMeshFromTriangleSurfaceMesh(
+    const TriangleSurfaceMesh<double>& mesh,
+    const GeometryProperties& properties, const Rgba& default_diffuse,
     const drake::internal::DiagnosticPolicy& policy = {});
 
 }  // namespace internal
