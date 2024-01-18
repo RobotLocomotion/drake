9dafe5d7b450e4e26e5378d8be69f33c0e08e78d
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-01-18 13:49:01 -0800

    [geometry] Serve the environment map over http
    The behavioral change here is a small one: when setting an environment
    map instead of it being copied into the property message as an inline
    data URI, instead the property message only contains the URL and image
    is served over http.
    
    Primarily, this allows the browser to avoid re-requesting the image in
    case it's already downloaded it previously. It also has side benefits:
    the http image becomes easier to debug -- in the developer console it
    shows up as a separate request, including an inline image preview.  In
    theory it should also be faster to skip the base64 encoding in C++ and
    decoding in the browser, though that's not really measurable for just
    the environment map.
    
    The bigger picture is that the CAS technology introduced here can also
    eventually be used for resources beyond just the environment map, such
    as texture files, geometry files, object mesh files, etc.
    
    As part of this change, we also fix the StaticHtml concatenation to
    use a rope instead of iterated string concatenation, in order to avoid
    quadratic time-complexity.

--- geometry/meshcat_internal.h
+++ geometry/meshcat_internal.h
@@ -1,8 +1,14 @@
 #pragma once
 
-#include <filesystem>
+#include <memory>
+#include <mutex>
 #include <optional>
 #include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "drake/common/drake_copyable.h"
+#include "drake/common/sha256.h"
 
 namespace drake {
 namespace geometry {
@@ -18,6 +24,81 @@ invalid. The valid static resource URLs are:
 - `/stats.min.js` */
 std::optional<std::string_view> GetMeshcatStaticResource(
     std::string_view url_path);
+
+/* FileStorage maintains an in-memory database with the contents of files, keyed
+by their checksum.
+
+For large assets (e.g., images) sometimes we use the msgpack message to transmit
+the URL of the asset, instead of the asset as inline data. In that case, we need
+to store the asset so that we can serve it over HTTP GET when a browser client
+needs it. Our storage must checkpoint the asset in memory -- we can't just keep
+around the fs::path and re-read it again later because the file might have
+disappeared or changed in the meantime.
+
+This storage is built around shared_ptr and weak_ptr so that users of this class
+are responsible for managing the lifetime of the assets. The FileStorage class
+only holds weak pointers. The Insert() function returns a shared_ptr to the
+caller, who is responsible for keeping that shared_ptr around exactly as long as
+the asset should remain in the database. Inserting the same asset again will
+bump the use_count (by returning a copy of the same shared_ptr).
+
+Because this class is accessed by both the Meshcat main thread (to insert
+assets) and the websocket worker thread (to serve assets), this class is
+thread-safe: all functions other than the constructor and destructor take a
+mutex lock. */
+class FileStorage final {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(FileStorage);
+
+  /* The record type contained in this database, as returned by Insert() or
+  Find(). It is the responsibility of users of FileStorage to hold the `content`
+  pointer (or the entire Handle) as long as the database entry should remain
+  findable in our storage. */
+  struct Handle {
+    Sha256 sha256;
+    std::shared_ptr<const std::string> content;
+  };
+
+  /* Constructs an empty database. */
+  FileStorage();
+
+  /* Clears the database; it does not destroy any reference-counted "content"
+  strings or `Handle` objects still held by users outside of this class. You can
+  think of it like only the database's index is destroyed. */
+  ~FileStorage();
+
+  /* Adds the given `content` to the database. The `content` argument will be
+  empty after this call (you can use the return value, instead). If possible,
+  this function re-uses an existing entry, i.e., this function will "intern" the
+  string. */
+  Handle Insert(std::string&& content);
+
+  /* Returns the database content with the given checksum, or when not found in
+  returns default-constructed a Handle (with content == nullptr). */
+  [[nodiscard]] Handle Find(const Sha256& sha256) const;
+
+  /* Returns the entire database. */
+  std::vector<Handle> DumpEverything() const;
+
+  /* Reclaims unused memory.
+  Returns the number of reclamations (for unit testing). */
+  int ShrinkToFit();
+
+ private:
+  struct Item {
+    std::weak_ptr<const std::string> content;
+
+    // TODO(jwnimmer-tri) In the future, we might want to track additional
+    // metadata in the Item record, e.g., the original filename, mime type, etc.
+  };
+
+  /* The implementation of Find(). Assumes that `mutex_` is already held. */
+  [[nodiscard]] Handle FindWhileLocked(const Sha256& sha256) const;
+
+  // The mutex_ guards all other member fields.
+  mutable std::mutex mutex_;
+  std::unordered_map<Sha256, Item> map_;
+};
 
 }  // namespace internal
 }  // namespace geometry
