92c22cb49b84a84a94d5268b6e4b1b66d93a4a94
Michael Sherman <sherm1@gmail.com>
2024-01-22 08:52:28 -0800

    Adds LinkJointGraph skeleton
    Not functional yet.

---
+++ multibody/topology/test/link_joint_graph_test.cc
@@ -0,0 +1,324 @@
+/* clang-format off to disable clang-format-includes */
+#include "drake/multibody/topology/graph.h"
+/* clang-format on */
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "drake/common/test_utilities/expect_throws_message.h"
+#include "drake/multibody/topology/forest.h"
+
+namespace drake {
+namespace multibody {
+namespace internal {
+
+// This class is a friend of subclasses that have private constructors and
+// member functions so that we can test those APIs standalone.
+class LinkJointGraphTester {
+ public:
+  static LinkJointGraph::Link MakeLink(BodyIndex index, std::string name,
+                                       ModelInstanceIndex model_instance,
+                                       LinkFlags flags) {
+    return LinkJointGraph::Link(index, std::move(name), model_instance, flags);
+  }
+
+  static LinkJointGraph::Joint MakeJoint(JointIndex index, std::string name,
+                                         ModelInstanceIndex model_instance,
+                                         JointTypeIndex joint_type_index,
+                                         BodyIndex parent_link_index,
+                                         BodyIndex child_link_index,
+                                         JointFlags flags) {
+    return LinkJointGraph::Joint(index, std::move(name), model_instance,
+                                 joint_type_index, parent_link_index,
+                                 child_link_index, flags);
+  }
+
+  static LinkFlags set_link_flags(LinkFlags to_set,
+                                  LinkJointGraph::Link* link) {
+    return link->set_flags(to_set);
+  }
+
+  static JointFlags set_joint_flags(JointFlags to_set,
+                                    LinkJointGraph::Joint* joint) {
+    return joint->set_flags(to_set);
+  }
+};
+
+namespace {
+
+// Verify that the enums in link_joint_graph_defs.h work properly as bitmaps.
+GTEST_TEST(LinkJointGraph, FlagsAndOptions) {
+  const auto is_static = LinkFlags::kStatic;
+  const auto massless = LinkFlags::kTreatAsMassless;
+
+  // Or-ed flags still have LinkFlags type.
+  auto link_flags = is_static | massless;
+  static_assert(std::is_same_v<decltype(link_flags), LinkFlags>);
+
+  // And is bitwise but still returns LinkFlags, convertible to bool.
+  EXPECT_EQ(link_flags & is_static, LinkFlags::kStatic);
+  EXPECT_EQ(link_flags & massless, LinkFlags::kTreatAsMassless);
+  EXPECT_FALSE(static_cast<bool>(link_flags & LinkFlags::kMustBeBaseBody));
+  EXPECT_EQ(link_flags & LinkFlags::kMustBeBaseBody, LinkFlags::kDefault);
+
+  // Repeat for Modeling Options.
+  const auto use_fixed_base = ModelingOptions::kUseFixedBase;
+  const auto combine_links = ModelingOptions::kCombineLinkComposites;
+  auto modeling_options = use_fixed_base | combine_links;
+  static_assert(std::is_same_v<decltype(modeling_options), ModelingOptions>);
+  EXPECT_EQ(modeling_options & use_fixed_base, ModelingOptions::kUseFixedBase);
+  EXPECT_EQ(modeling_options & combine_links,
+            ModelingOptions::kCombineLinkComposites);
+  EXPECT_FALSE(static_cast<bool>(modeling_options &
+                                 ModelingOptions::kUseRpyFloatingJoints));
+  EXPECT_EQ(modeling_options & ModelingOptions::kUseRpyFloatingJoints,
+            ModelingOptions::kDefault);
+
+  // Only one option in JointFlags currently.
+  auto joint_flags = JointFlags::kMustBeModeled;
+  static_assert(std::is_same_v<decltype(joint_flags), JointFlags>);
+  EXPECT_EQ(joint_flags & JointFlags::kMustBeModeled,
+            JointFlags::kMustBeModeled);
+  EXPECT_EQ(joint_flags | JointFlags::kMustBeModeled,
+            JointFlags::kMustBeModeled);
+}
+
+// LinkJointGraph supports copy/move/assign, including the already-built
+// SpanningForest. That takes some sleight-of-hand to get back pointers right;
+// make sure it works. Note that the actual copying and moving of data members
+// is implemented using the corresponding compiler-generated defaults for the
+// Data structs in LinkJointGraph and SpanningForest. We can assume those are
+// working correctly so don't need to check every member.
+GTEST_TEST(LinkJointGraph, CopyMoveAssignTest) {
+  LinkJointGraph graph;
+  LinkJointGraph* graph_ptr = &graph;  // To avoid warnings below.
+  const SpanningForest* original_graph_forest_ptr = &graph.BuildForest();
+
+  // These first checks don't use copy/move/assign but are here to make it
+  // clear what we have before we start with those.
+  EXPECT_EQ(&graph.forest().graph(), &graph);
+  EXPECT_TRUE(graph.forest_is_valid());
+  graph.AddLink("link1", ModelInstanceIndex(19));
+  EXPECT_FALSE(graph.forest_is_valid());
+
+  // Remember the memory address of link1 so we can see if we're re-using
+  // the same memory when we expect to be.
+  const LinkJointGraph::Link& original_link1 = graph.links()[1];
+
+  // Self copy-assign and self move-assign should be no-ops. It's hard to verify
+  // that no self-copying has occurred but we can at least see that we're still
+  // using the same memory afterwards.
+  graph = *graph_ptr;                  // Self copy-assign.
+  EXPECT_EQ(ssize(graph.links()), 2);  // World + link1.
+  EXPECT_EQ(&graph.forest(), original_graph_forest_ptr);
+  EXPECT_EQ(&graph.links()[1], &original_link1);
+
+  graph = std::move(*graph_ptr);       // Self move-assign.
+  EXPECT_EQ(ssize(graph.links()), 2);  // World + link1.
+  EXPECT_EQ(&graph.forest(), original_graph_forest_ptr);
+  EXPECT_EQ(&graph.links()[1], &original_link1);
+
+  LinkJointGraph graph_copy(graph);  // Copy constructor.
+  const SpanningForest* graph_copy_forest = &graph_copy.forest();
+  EXPECT_TRUE(graph_copy.HasLinkNamed("link1", ModelInstanceIndex(19)));
+  EXPECT_FALSE(graph_copy.forest_is_valid());
+  EXPECT_NE(graph_copy_forest, original_graph_forest_ptr);
+  EXPECT_EQ(&graph_copy.forest().graph(), &graph_copy);
+  EXPECT_NE(&graph_copy.links()[1], &original_link1);
+
+  LinkJointGraph graph_assign;
+  graph_assign = graph;  // Copy assignment.
+  EXPECT_TRUE(graph_assign.HasLinkNamed("link1", ModelInstanceIndex(19)));
+  EXPECT_FALSE(graph_assign.forest_is_valid());
+  EXPECT_NE(&graph_assign.forest(), &graph.forest());
+  EXPECT_EQ(&graph_assign.forest().graph(), &graph_assign);
+  EXPECT_NE(&graph_assign.links()[1], &original_link1);
+
+  graph.BuildForest();  // Mark forest valid again.
+  EXPECT_TRUE(graph.forest_is_valid());
+  EXPECT_EQ(ssize(graph.links()), 2);  // World + link1
+
+  LinkJointGraph graph_move(std::move(graph));  // Move constructor.
+  EXPECT_EQ(&graph_move.forest(), original_graph_forest_ptr);  // Stolen!
+  EXPECT_TRUE(graph_move.forest_is_valid());
+  EXPECT_TRUE(graph_move.HasLinkNamed("link1", ModelInstanceIndex(19)));
+  EXPECT_EQ(ssize(graph_move.links()), 2);             // World + link1
+  EXPECT_EQ(&graph_move.links()[1], &original_link1);  // Stolen!
+  EXPECT_EQ(ssize(graph.links()), 1);                  // Back to default state.
+  EXPECT_NE(&graph.forest(), nullptr);  // Should have a new Forest.
+  EXPECT_NE(&graph.forest(), original_graph_forest_ptr);
+
+  LinkJointGraph graph_move_assign;
+  const LinkJointGraph::Link& copy_link1 = graph_copy.links()[1];
+  graph_move_assign = std::move(graph_copy);                  // Move assignent.
+  EXPECT_EQ(&graph_move_assign.forest(), graph_copy_forest);  // Stolen!
+  EXPECT_FALSE(graph_move_assign.forest_is_valid());
+  EXPECT_TRUE(graph_move_assign.HasLinkNamed("link1", ModelInstanceIndex(19)));
+  EXPECT_EQ(ssize(graph_move_assign.links()), 2);         // World + link1
+  EXPECT_EQ(&graph_move_assign.links()[1], &copy_link1);  // Stolen!
+  EXPECT_EQ(ssize(graph_copy.links()), 1);   // Back to default state.
+  EXPECT_NE(&graph_copy.forest(), nullptr);  // Should have a new Forest.
+  EXPECT_NE(&graph_copy.forest(), graph_copy_forest);
+}
+
+// A default-constructed LinkJointGraph should contain a predefined World
+// Link, predefined joint types, and be suitable for generating a matching
+// SpanningForest.
+GTEST_TEST(LinkJointGraph, EmptyTest) {
+  LinkJointGraph graph;
+
+  // World is predefined.
+  EXPECT_EQ(ssize(graph.links()), 1);
+  EXPECT_TRUE(graph.joints().empty());
+  EXPECT_EQ(graph.world_link().name(), "world");
+  EXPECT_EQ(graph.world_link().model_instance(), world_model_instance());
+  EXPECT_EQ(graph.world_link().index(), BodyIndex(0));
+
+  // Topologically important joint types are predefined.
+  EXPECT_EQ(ssize(graph.joint_types()), 3);
+  const LinkJointGraph::JointType& weld_type =
+      graph.joint_types(LinkJointGraph::weld_joint_type_index());
+  EXPECT_EQ(weld_type.type_name, "weld");
+  EXPECT_EQ(weld_type.nq, 0);
+  EXPECT_EQ(weld_type.nv, 0);
+  EXPECT_EQ(weld_type.has_quaternion, false);
+  const LinkJointGraph::JointType& quaternion_floating_type =
+      graph.joint_types(LinkJointGraph::quaternion_floating_joint_type_index());
+  EXPECT_EQ(quaternion_floating_type.type_name, "quaternion_floating");
+  EXPECT_EQ(quaternion_floating_type.nq, 7);
+  EXPECT_EQ(quaternion_floating_type.nv, 6);
+  EXPECT_EQ(quaternion_floating_type.has_quaternion, true);
+  const LinkJointGraph::JointType& rpy_floating_type =
+      graph.joint_types(LinkJointGraph::rpy_floating_joint_type_index());
+  EXPECT_EQ(rpy_floating_type.type_name, "rpy_floating");
+  EXPECT_EQ(rpy_floating_type.nq, 6);
+  EXPECT_EQ(rpy_floating_type.nv, 6);
+  EXPECT_EQ(rpy_floating_type.has_quaternion, false);
+
+  EXPECT_FALSE(graph.forest_is_valid());
+
+  const SpanningForest& forest = graph.BuildForest();
+  EXPECT_EQ(&forest.graph(), &graph);
+  EXPECT_TRUE(graph.forest_is_valid());
+
+  // Check that Clear() puts the graph back to default-constructed condition.
+  // First add some junk to the graph.
+  graph.RegisterJointType("revolute", 1, 1);
+  const BodyIndex dummy_index =
+      graph.AddLink("dummy", default_model_instance());
+  EXPECT_TRUE(graph.HasLinkNamed("dummy", default_model_instance()));
+  EXPECT_EQ(dummy_index, BodyIndex(1));
+  EXPECT_EQ(ssize(graph.links()), 2);
+  EXPECT_EQ(ssize(graph.joints()), 0);
+  EXPECT_EQ(ssize(graph.joint_types()), 4);
+  // Now get rid of that junk.
+  graph.Clear();
+  EXPECT_EQ(ssize(graph.links()), 1);  // World
+  EXPECT_TRUE(graph.joints().empty());
+  EXPECT_EQ(ssize(graph.joint_types()), 3);  // Predefineds
+  EXPECT_FALSE(graph.forest_is_valid());
+
+  // Make sure Clear() saved the existing forest.
+  const SpanningForest& same_forest = graph.BuildForest();
+  EXPECT_EQ(&same_forest, &forest);
+  EXPECT_EQ(&forest.graph(), &graph);
+  EXPECT_TRUE(graph.forest_is_valid());
+}
+
+// Make sure AddLink() rejects obvious errors.
+GTEST_TEST(LinkJointGraph, AddLinkErrors) {
+  LinkJointGraph graph;
+
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      graph.AddLink("not_world", world_model_instance()),
+      "AddLink.*Model instance.*0.*reserved for.*World.*");
+
+  graph.AddLink("link1", ModelInstanceIndex(3));
+  graph.AddLink("link1", ModelInstanceIndex(4));  // OK, different instance.
+
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      graph.AddLink("link1", ModelInstanceIndex(3)),
+      "AddLink.*already a link named.*link1.*model instance.*3.*");
+
+  // Addlink accepts flags, but not Shadow which is set internally only.
+  const BodyIndex link2_index =
+      graph.AddLink("link2", ModelInstanceIndex(3),
+                    LinkFlags::kTreatAsMassless | LinkFlags::kMustBeBaseBody);
+  const LinkJointGraph::Link& link2 = graph.links(link2_index);
+  EXPECT_TRUE(link2.treat_as_massless() && link2.must_be_base_body());
+  EXPECT_FALSE(link2.is_static() || link2.is_shadow());
+
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      graph.AddLink("link3", ModelInstanceIndex(3), LinkFlags::kShadow),
+      "AddLink.*Can't add.*link3.*Shadow flag.*");
+}
+
+// Check operation of the public members of the Link subclass.
+GTEST_TEST(LinkJoinGraph, LinkTest) {
+  LinkJointGraph::Link link5 = LinkJointGraphTester::MakeLink(
+      BodyIndex(5), "link5", ModelInstanceIndex(7), LinkFlags::kMustBeBaseBody);
+  EXPECT_EQ(link5.index(), BodyIndex(5));
+  EXPECT_EQ(link5.model_instance(), ModelInstanceIndex(7));
+  EXPECT_EQ(link5.name(), "link5");
+
+  // Check flags.
+  EXPECT_FALSE(link5.is_world());
+  EXPECT_FALSE(link5.is_anchored());
+  EXPECT_FALSE(link5.is_static());
+  EXPECT_FALSE(link5.treat_as_massless());
+  EXPECT_FALSE(link5.is_shadow());
+  EXPECT_TRUE(link5.must_be_base_body());
+  LinkJointGraphTester::set_link_flags(LinkFlags::kStatic, &link5);
+  EXPECT_TRUE(link5.is_static());
+  EXPECT_TRUE(link5.is_anchored());  // Static links are anchored to World.
+  EXPECT_TRUE(link5.must_be_base_body());   // Unchanged.
+  EXPECT_FALSE(link5.treat_as_massless());  // Unchanged.
+
+  // Only LinkJointGraph sets these; no public interface.
+  EXPECT_TRUE(link5.joints().empty());
+  EXPECT_TRUE(link5.joints_as_parent().empty());
+  EXPECT_TRUE(link5.joints_as_child().empty());
+  EXPECT_EQ(link5.num_shadows(), 0);
+  EXPECT_FALSE(link5.primary_link().is_valid());
+  EXPECT_FALSE(link5.mobod_index().is_valid());
+  EXPECT_FALSE(link5.inboard_joint_index().is_valid());
+  EXPECT_FALSE(link5.composite().is_valid());
+}
+
+// Check operation of the public members of the Joint subclass.
+GTEST_TEST(LinkJointGraph, JointTest) {
+  const BodyIndex parent_index(1);
+  const BodyIndex child_index(2);
+  const BodyIndex other_body_index(3);  // Not connected by joint3.
+  LinkJointGraph::Joint joint3 = LinkJointGraphTester::MakeJoint(
+      JointIndex(3), "joint3", ModelInstanceIndex(9),
+      LinkJointGraph::rpy_floating_joint_type_index(), parent_index,
+      child_index, JointFlags::kMustBeModeled);
+  EXPECT_EQ(joint3.index(), JointIndex(3));
+  EXPECT_EQ(joint3.model_instance(), ModelInstanceIndex(9));
+  EXPECT_EQ(joint3.name(), "joint3");
+  EXPECT_EQ(joint3.parent_link(), parent_index);
+  EXPECT_EQ(joint3.child_link(), child_index);
+  EXPECT_FALSE(joint3.is_weld());
+  EXPECT_EQ(joint3.type_index(),
+            LinkJointGraph::rpy_floating_joint_type_index());
+  EXPECT_TRUE(joint3.connects(parent_index));
+  EXPECT_TRUE(joint3.connects(child_index));
+  EXPECT_FALSE(joint3.connects(other_body_index));
+  EXPECT_TRUE(joint3.connects(parent_index, child_index));
+  EXPECT_TRUE(joint3.connects(child_index, parent_index));
+  EXPECT_FALSE(joint3.connects(parent_index, other_body_index));
+  EXPECT_TRUE(joint3.must_be_modeled());
+  EXPECT_FALSE(joint3.mobod_index().is_valid());
+  EXPECT_FALSE(joint3.has_been_processed());
+  EXPECT_EQ(joint3.other_link_index(parent_index), child_index);
+  EXPECT_EQ(joint3.other_link_index(child_index), parent_index);
+  LinkJointGraphTester::set_joint_flags(JointFlags::kDefault, &joint3);
+  EXPECT_FALSE(joint3.must_be_modeled());
+}
+
+}  // namespace
+}  // namespace internal
+}  // namespace multibody
+}  // namespace drake
