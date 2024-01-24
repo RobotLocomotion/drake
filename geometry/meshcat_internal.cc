895c0c39e65c0bb9e0335a089cf437736195adc4
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-01-23 18:20:16 -0800

    [geometry] Serve the environment map over http
    The behavioral change here is a small one: when setting an environment
    map instead of it being copied into the property message as an inline
    data URI, the property message only contains the URL and image data is
    served over http.
    
    Primarily, this allows the browser to avoid re-requesting the image in
    case it's already downloaded it previously. It also has side benefits:
    the http image becomes easier to debug -- in the developer console it
    shows up as a separate request, including an inline image preview.  In
    theory it should also be faster to skip the base64 encoding in C++ and
    decoding in the browser, though that's not really measurable for just
    the environment map.
    
    The bigger picture is that the CAS technology introduced here can also
    eventually be used for resources beyond just the environment map, such
    as texture files, geometry files, object mesh files, etc.
    
    Forr testing the changes to meshcat.cc, the pre-existing unit and
    manual tests already have sufficient coverage.
    
    XXX The meshcat_manual_test can lie now, pulling from disk cache.
    
    XXX Figure out where to call ShrinkToFit.

--- geometry/meshcat_internal.cc
+++ geometry/meshcat_internal.cc
@@ -1,5 +1,6 @@
 #include "drake/geometry/meshcat_internal.h"
 
+#include <algorithm>
 #include <stdexcept>
 #include <utility>
 
@@ -68,6 +69,99 @@ UuidGenerator::~UuidGenerator() = default;
 
 std::string UuidGenerator::GenerateRandom() {
   return uuids::to_string(impl_->uuid_());
+}
+
+FileStorage::FileStorage() = default;
+
+FileStorage::~FileStorage() = default;
+
+FileStorage::Handle FileStorage::Insert(std::string&& content,
+                                        std::string&& filename_hint) {
+  // Do the checksum outside of the critical section.
+  const Sha256 sha256 = Sha256::Checksum(content);
+
+  // Remove any newlines from the filename_hint.
+  for (auto& ch : filename_hint) {
+    if (ch == '\n' || ch == '\r') {
+      ch = ' ';
+    }
+  }
+
+  // Try to re-use an existing entry if possible.
+  std::lock_guard guard(mutex_);
+  Handle result = FindWhileLocked(sha256);
+  if (result.content != nullptr) {
+    // Found the content; no need to insert a new copy -- but for consistency we
+    // should still dispose of the user-provided strings.
+    content.clear();
+    filename_hint.clear();
+  } else {
+    // Not found; we need to insert the content.
+    auto content_ptr = std::make_shared<std::string>(std::move(content));
+    map_[sha256] = Item{.filename_hint = filename_hint, .content = content_ptr};
+    result.sha256 = sha256;
+    result.filename_hint = std::move(filename_hint);
+    result.content = std::move(content_ptr);
+  }
+  return result;
+}
+
+FileStorage::Handle FileStorage::Find(const Sha256& sha256) const {
+  std::lock_guard guard(mutex_);
+  return FindWhileLocked(sha256);
+}
+
+FileStorage::Handle FileStorage::FindWhileLocked(const Sha256& sha256) const {
+  auto iter = map_.find(sha256);
+  if (iter != map_.end()) {
+    const Item& item = iter->second;
+    return item.ToHandle(sha256);
+  }
+  return {};
+}
+
+FileStorage::Handle FileStorage::Item::ToHandle(const Sha256& sha256) const {
+  std::shared_ptr<const std::string> strong = content.lock();
+  return (strong != nullptr)
+             ? FileStorage::Handle{.sha256 = sha256,
+                                   .filename_hint = filename_hint,
+                                   .content = std::move(strong)}
+             : FileStorage::Handle{};
+}
+
+std::vector<FileStorage::Handle> FileStorage::DumpEverything() const {
+  std::vector<Handle> result;
+  {
+    // Use a critical section to copy out the map.
+    std::lock_guard guard(mutex_);
+    result.reserve(map_.size());
+    for (const auto& [sha256, item] : map_) {
+      Handle handle = item.ToHandle(sha256);
+      if (handle.content != nullptr) {
+        result.push_back(std::move(handle));
+      }
+    }
+  }
+  // For determinism, sort the result (but outside of the critical section).
+  std::sort(result.begin(), result.end(), [](const auto& a, const auto& b) {
+    return a.sha256 < b.sha256;
+  });
+  return result;
+}
+
+int FileStorage::ShrinkToFit() {
+  int result = 0;
+  std::lock_guard guard(mutex_);
+  for (auto iter = map_.begin(); iter != map_.end();) {
+    Item& item = iter->second;
+    if (item.content.expired()) {
+      iter = map_.erase(iter);
+      ++result;
+    } else {
+      ++iter;
+    }
+  }
+  return result;
 }
 
 }  // namespace internal
--- geometry/test/meshcat_internal_test.cc
+++ geometry/test/meshcat_internal_test.cc
@@ -39,6 +39,111 @@ GTEST_TEST(MeshcatInternalTest, UuidGenerator) {
   EXPECT_THAT(bar, testing::MatchesRegex(pattern));
 }
 
+GTEST_TEST(MeshcatInternalTest, FileStorage) {
+  using Handle = FileStorage::Handle;
+  FileStorage dut;
+
+  // Start out empty.
+  EXPECT_FALSE(dut.Find(Sha256{}).content);
+  EXPECT_EQ(dut.DumpEverything().size(), 0);
+  EXPECT_EQ(dut.ShrinkToFit(), 0);
+
+  // Add one item.
+  std::string one_content{"content_1"};
+  std::string one_hint{"hint_1"};
+  Handle one = dut.Insert(std::move(one_content), std::move(one_hint));
+  EXPECT_EQ(one_content, "");
+  EXPECT_EQ(one_hint, "");
+  EXPECT_NE(one.sha256, Sha256{});
+  EXPECT_EQ(one.filename_hint, "hint_1");
+  EXPECT_THAT(one.content, testing::Pointee(testing::Eq("content_1")));
+  EXPECT_EQ(one.content.use_count(), 1);
+  EXPECT_EQ(dut.ShrinkToFit(), 0);
+
+  // Add a second item.
+  // Newlines in the filename are replaced with blanks.
+  Handle two = dut.Insert("content_2", "hint\r\n2");
+  EXPECT_NE(two.sha256, one.sha256);
+  EXPECT_EQ(two.filename_hint, "hint  2");
+  EXPECT_THAT(two.content, testing::Pointee(testing::Eq("content_2")));
+  EXPECT_EQ(two.content.use_count(), 1);
+  EXPECT_EQ(dut.ShrinkToFit(), 0);
+
+  // Add the first item again.
+  std::string repeat_content{"content_1"};
+  std::string repeat_hint{"will_be_ignored"};
+  Handle repeat = dut.Insert(std::move(repeat_content), std::move(repeat_hint));
+  EXPECT_EQ(repeat_content, "");
+  EXPECT_EQ(repeat_hint, "");
+  EXPECT_EQ(repeat.sha256, one.sha256);
+  EXPECT_EQ(repeat.filename_hint, "hint_1");
+  EXPECT_EQ(repeat.content.get(), one.content.get());
+  EXPECT_EQ(repeat.content.use_count(), 2);
+  EXPECT_EQ(one.content.use_count(), 2);
+  EXPECT_EQ(dut.ShrinkToFit(), 0);
+
+  // Look up the first item.
+  Handle find_one = dut.Find(one.sha256);
+  EXPECT_EQ(find_one.filename_hint, "hint_1");
+  EXPECT_THAT(find_one.content, testing::Pointee(testing::Eq("content_1")));
+  EXPECT_EQ(find_one.content.use_count(), 3);
+  EXPECT_EQ(dut.ShrinkToFit(), 0);
+
+  // Dump everything. We don't care what order the assets come out in,
+  // as long as it's deterministic -- we enforce that by hard-coding
+  // an assumed order here.
+  std::vector<Handle> everything = dut.DumpEverything();
+  ASSERT_EQ(everything.size(), 2);
+  EXPECT_EQ(everything.at(0).filename_hint, "hint_1");
+  EXPECT_EQ(everything.at(1).filename_hint, "hint  2");
+  everything = {};
+
+  // Reset up all handles to the first item, so that it will be
+  // removed from storage.
+  one = {};
+  repeat = {};
+  find_one = {};
+  EXPECT_FALSE(dut.Find(one.sha256).content);
+  EXPECT_EQ(dut.ShrinkToFit(), 1);
+
+  // Look up the second item.
+  Handle find_two = dut.Find(two.sha256);
+  EXPECT_EQ(find_two.filename_hint, "hint  2");
+  EXPECT_THAT(find_two.content, testing::Pointee(testing::Eq("content_2")));
+  EXPECT_EQ(find_two.content.use_count(), 2);
+  EXPECT_EQ(dut.ShrinkToFit(), 0);
+
+  // Reset the original handle, but keeping the Find() handle.
+  two = {};
+  EXPECT_THAT(find_two.content, testing::Pointee(testing::Eq("content_2")));
+  EXPECT_EQ(find_two.content.use_count(), 1);
+  EXPECT_EQ(dut.ShrinkToFit(), 0);
+
+  // Reset the last remaining handle.
+  find_two = {};
+  EXPECT_FALSE(dut.Find(two.sha256).content);
+  EXPECT_EQ(dut.ShrinkToFit(), 1);
+}
+
+GTEST_TEST(MeshcatInternalTest, FileStorageInsertExpired) {
+  using Handle = FileStorage::Handle;
+  FileStorage dut;
+
+  // Insert content and then immediately expire it.
+  Handle one = dut.Insert("content", "old_hint");
+  one = {};
+
+  // Re-insert the same content again and make sure it's findable.
+  one = dut.Insert("content", "new_hint");
+  EXPECT_EQ(one.filename_hint, "new_hint");
+  EXPECT_THAT(one.content, testing::Pointee(testing::Eq("content")));
+  EXPECT_EQ(one.content.use_count(), 1);
+  Handle find_one = dut.Find(one.sha256);
+  EXPECT_EQ(find_one.filename_hint, "new_hint");
+  EXPECT_THAT(find_one.content, testing::Pointee(testing::Eq("content")));
+  EXPECT_EQ(dut.ShrinkToFit(), 0);
+}
+
 }  // namespace
 }  // namespace internal
 }  // namespace geometry
