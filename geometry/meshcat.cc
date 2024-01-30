692a4a9c247693367dd1df99b51897b274b24906
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-01-30 07:39:34 -0800

    When using visit with overloaded, prescribe the return type
    This helps avoid boilerplate and improve clarity.

--- geometry/meshcat.cc
+++ geometry/meshcat.cc
@@ -414,7 +414,7 @@ class MeshcatShapeReifier : public ShapeReifier {
     }
 
     // Set the scale.
-    std::visit(
+    std::visit<void>(
         overloaded{[](std::monostate) {},
                    [scale](auto& lumped_object) {
                      Eigen::Map<Eigen::Matrix4d> matrix(lumped_object.matrix);
