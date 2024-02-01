561ed79d180e9f13f6193c112636b33d658c8e45
Damrong Guoy <42557859+DamrongGuoy@users.noreply.github.com>
2024-01-31 16:57:10 -0800

    [geometry] Fast initialize compliant-hydroelastic nonconvex meshes and deformables (#20798)
    - Use BVH in CalcDistanceToSurfaceMesh().
    - Skip calling CalcDistanceToSurfaceMesh() on boundary vertices.

--- geometry/proximity/calc_distance_to_surface_mesh.cc
+++ geometry/proximity/calc_distance_to_surface_mesh.cc
@@ -4,11 +4,16 @@
 #include <limits>
 #include <vector>
 
+#include "drake/geometry/proximity/distance_to_point_callback.h"
 #include "drake/math/rotation_matrix.h"
 
 namespace drake {
 namespace geometry {
 namespace internal {
+
+using Eigen::Vector3d;
+using math::RigidTransformd;
+
 namespace {
 
 /* Struct to hold vertex positions of a triangle. */
@@ -112,23 +117,90 @@ double CalcSquaredDistanceToTriangle(const Vector3<double>& p_WQ,
   return std::min({d_AB_squared, d_AC_squared, d_BC_squared});
 }
 
+// TODO(xuchenhan-tri): Consider using the algorithm presented in chapter 5.1.5
+//  of "Real Time Collision Detection" (Christer Ericson) with fewer flops if
+//  performance is an issue for CalcSquaredDistanceToTriangle().
+
+class BvhVisitor {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(BvhVisitor);
+
+  static double CalcMinDistance(
+      const Vector3d& p_WQ, const TriangleSurfaceMesh<double>& mesh_W,
+      const Bvh<Obb, TriangleSurfaceMesh<double>>::NodeType& node_W) {
+    BvhVisitor visitor{p_WQ, mesh_W};
+    visitor.Visit(node_W);
+    return std::sqrt(visitor.min_squared_);
+  }
+
+ private:
+  BvhVisitor(const Vector3d& p_WQ, const TriangleSurfaceMesh<double>& mesh_W)
+      : p_WQ_{p_WQ}, mesh_W_{mesh_W} {}
+
+  void Visit(const Bvh<Obb, TriangleSurfaceMesh<double>>::NodeType& node_W) {
+    if (node_W.is_leaf()) {
+      for (int i = 0; i < node_W.num_element_indices(); ++i) {
+        const SurfaceTriangle& t = mesh_W_.triangles()[node_W.element_index(i)];
+        min_squared_ = std::min(min_squared_,
+                                CalcSquaredDistanceToTriangle(
+                                    p_WQ_, {mesh_W_.vertices()[t.vertex(0)],
+                                            mesh_W_.vertices()[t.vertex(1)],
+                                            mesh_W_.vertices()[t.vertex(2)]}));
+      }
+      return;
+    }
+
+    // The rest of this function is for the subtree of the internal node rooted
+    // at node_W.
+
+    const Vector3d box_half_width_B = node_W.bv().half_width();
+    const RigidTransformd& X_WB = node_W.bv().pose();
+    const Vector3d p_BQ = X_WB.inverse() * p_WQ_;
+
+    const auto [closest_point_on_box_boundary_B, grad_signed_distance_B, _] =
+        point_distance::DistanceToPoint<double>::ComputeDistanceToBox<3>(
+            box_half_width_B, p_BQ);
+    // Mathematically we can calculate the signed distance phi from the gradient
+    // grad_phi, the query point Q and its closest point C on the box's
+    // boundary as:
+    //      phi = grad_phi.dot(Q - C)
+    // because:
+    // 1. The unsigned distance is clearly |Q-C|. We just need to set the
+    //    correct sign depending on the location of Q relative to the box.
+    // 2. grad_phi is a unit vector pointing outward from the box.  It is
+    //    parallel to Q-C (or antiparallel if Q is inside the box).
+    const double signed_distance =
+        grad_signed_distance_B.dot(p_BQ - closest_point_on_box_boundary_B);
+
+    // Check for possible pruning.
+    if (signed_distance > 0) {
+      // The query point is outside, so we can get the lower bound.
+      const double squared_distance_from_this_node_is_at_least =
+          signed_distance * signed_distance;
+      // Use the lower bound to possibly prune this subtree.
+      if (squared_distance_from_this_node_is_at_least > min_squared_) {
+        return;
+      }
+    }
+
+    // We couldn't prune the subtree, recursively search both children.
+    this->Visit(node_W.left());
+    this->Visit(node_W.right());
+  }
+
+ private:
+  const Vector3<double>& p_WQ_;
+  const TriangleSurfaceMesh<double>& mesh_W_;
+
+  double min_squared_{std::numeric_limits<double>::infinity()};
+};
+
 }  // namespace
 
-// TODO(xuchenhan-tri): Consider using the algorithm presented in chapter 5.1.5
-// of "Real Time Collision Detection" (Christer Ericson) with fewer flops if
-// performance is an issue.
-double CalcDistanceToSurfaceMesh(const Vector3<double>& p_WQ,
-                                 const TriangleSurfaceMesh<double>& mesh_W) {
-  double distance_squared = std::numeric_limits<double>::infinity();
-  const std::vector<Vector3<double>>& vertices = mesh_W.vertices();
-  for (const SurfaceTriangle& t : mesh_W.triangles()) {
-    distance_squared =
-        std::min(distance_squared,
-                 CalcSquaredDistanceToTriangle(
-                     p_WQ, {vertices[t.vertex(0)], vertices[t.vertex(1)],
-                            vertices[t.vertex(2)]}));
-  }
-  return std::sqrt(distance_squared);
+double CalcDistanceToSurfaceMesh(
+    const Vector3<double>& p_WQ, const TriangleSurfaceMesh<double>& mesh_W,
+    const Bvh<Obb, TriangleSurfaceMesh<double>>& bvh_W) {
+  return BvhVisitor::CalcMinDistance(p_WQ, mesh_W, bvh_W.root_node());
 }
 
 }  // namespace internal
