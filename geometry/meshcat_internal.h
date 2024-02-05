c3f3d43ff772838f1ff45091aa22186d5e439b07
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-02-05 13:43:57 -0800

    [meshcat] Unbundle glTFs before sending over msgpack
    This substantially improves loading times for large files.

--- geometry/meshcat_internal.h
+++ geometry/meshcat_internal.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <filesystem>
 #include <memory>
 #include <optional>
 #include <string>
+#include <vector>
 
 #include "drake/common/drake_copyable.h"
+#include "drake/geometry/meshcat_file_storage_internal.h"
 
 namespace drake {
 namespace geometry {
@@ -44,6 +47,25 @@ class UuidGenerator final {
   struct Impl;
   std::unique_ptr<Impl> impl_;
 };
+
+/* Rewrites glTF data in memory so that all of its URIs refer to assets in
+FileStorage, instead of their original location.
+
+When a glTF file has bundled assets (i.e., data URIs) this decodes and unbundles
+them. Meshcat is MUCH slower at loading bundled assets compared to unbundled.
+
+When glTF file has relative path URIs (i.e., unbundled files on disk), this
+loads the files FileStorage so that we can serve them later, even if the
+original file has disappeared in the meantime.
+
+@param[in] gltf_filename The glTF filename, used to calculate relative paths.
+@param[in,out] gltf_contents The glTF data already loaded from `gltf_filename`.
+  It will be edited in place to replace the URIs.
+@param[in,out] storage The database where assets should be stored.
+@returns The handles for all assets cited by `gltf_contents`. */
+[[nodiscard]] std::vector<std::shared_ptr<const FileStorage::Handle>>
+UnbundleGltfAssets(const std::filesystem::path& gltf_filename,
+                   std::string* gltf_contents, FileStorage* storage);
 
 }  // namespace internal
 }  // namespace geometry
