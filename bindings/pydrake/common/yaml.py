cba2b99f930643f0d4822373997d9716fcb01d5b
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-02-11 09:40:31 -0800

    [py yaml] yaml_load_typed uses schema=type(defaults) by default (#20922)

--- bindings/pydrake/common/yaml.py
+++ bindings/pydrake/common/yaml.py
@@ -454,7 +454,7 @@ def _merge_yaml_dict_into_target(*, options, yaml_dict,
             target=target, value_schema=sub_schema)
 
 
-def yaml_load_typed(*, schema,
+def yaml_load_typed(*, schema=None,
                     data=None,
                     filename=None,
                     child_name=None,
@@ -472,7 +472,9 @@ def yaml_load_typed(*, schema,
         schema: The type to load as. This either must be a ``dataclass``, a C++
             class bound using pybind11 and ``DefAttributesUsingSerialize``, or
             a ``Mapping[str, ...]`` where the mapping's value type is one of
-            those two categories.
+            those two categories. If a non-None ``defaults`` is provided, then
+            ``schema`` can be ``None`` and will use ``type(defaults)`` in that
+            case.
         data: The string of YAML data to be loaded. Exactly one of either
             ``data`` or ``filename`` must be provided.
         filename: The filename of YAML data to be loaded. Exactly one of either
@@ -497,6 +499,14 @@ def yaml_load_typed(*, schema,
            schema can have default values that are left intact unless the YAML
            data provides a value *for that specific key*.
     """
+
+    # Infer the schema when possible.
+    if schema is None:
+        if defaults is None:
+            raise ValueError(
+                "At least one of schema= and defaults= must be provided")
+        schema = type(defaults)
+
     # Choose the allow/retain setting in case none were provided.
     options = _LoadYamlOptions(
         allow_yaml_with_no_schema=allow_yaml_with_no_schema,
