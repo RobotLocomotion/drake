48f8a0c8723344497bded54c66e8a6ce1276cee9
Michael Sherman <sherm1@gmail.com>
2024-02-20 09:35:02 -0800

    Adds LinkJointGraph skeleton (#20806)
    Not functional yet.

---
+++ multibody/topology/graph.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/** @file
+This is the file to #include to use LinkJointGraph. It includes
+subsidiary headers for nested class definitions to keep file sizes
+reasonable. */
+
+// TODO(sherm1) Not used yet by MultibodyPlant; see PR #20225.
+
+#define DRAKE_MULTIBODY_TOPOLOGY_GRAPH_INCLUDED
+
+// Don't alpha-sort these internal includes; the order matters.
+// clang-format off
+#include "drake/multibody/topology/link_joint_graph.h"
+#include "drake/multibody/topology/link_joint_graph_link.h"
+#include "drake/multibody/topology/link_joint_graph_joint.h"
+#include "drake/multibody/topology/link_joint_graph_inlines.h"
+// clang-format on
+
+#undef DRAKE_MULTIBODY_TOPOLOGY_GRAPH_INCLUDED
