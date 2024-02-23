875d72ea27a69258bdc189bf8190237075869474
Russ Tedrake <russt@mit.edu>
2024-02-23 11:44:39 -0500

    Return bindings from KinematicTrajectoryOptimization Add* methods (#21014)
    Resolves 20894.

--- planning/trajectory_optimization/kinematic_trajectory_optimization.h
+++ planning/trajectory_optimization/kinematic_trajectory_optimization.h
@@ -105,23 +105,23 @@ class KinematicTrajectoryOptimization {
 
   /** Adds a linear constraint on the value of the path, `lb` ≤ r(s) ≤ `ub`.
   @pre 0 <= `s` <= 1. */
-  void AddPathPositionConstraint(const Eigen::Ref<const Eigen::VectorXd>& lb,
-                                 const Eigen::Ref<const Eigen::VectorXd>& ub,
-                                 double s);
+  solvers::Binding<solvers::LinearConstraint> AddPathPositionConstraint(
+      const Eigen::Ref<const Eigen::VectorXd>& lb,
+      const Eigen::Ref<const Eigen::VectorXd>& ub, double s);
 
   /** Adds a (generic) constraint on path. The constraint will be evaluated
   as if it is bound with variables corresponding to `r(s)`.
   @pre constraint.num_vars() == num_positions()
   @pre 0 <= `s` <= 1. */
-  void AddPathPositionConstraint(
+  solvers::Binding<solvers::Constraint> AddPathPositionConstraint(
       const std::shared_ptr<solvers::Constraint>& constraint, double s);
 
   /** Adds a linear constraint on the derivative of the path, `lb` ≤ ṙ(s) ≤
   `ub`. Note that this does NOT directly constrain q̇(t).
   @pre 0 <= `s` <= 1. */
-  void AddPathVelocityConstraint(const Eigen::Ref<const Eigen::VectorXd>& lb,
-                                 const Eigen::Ref<const Eigen::VectorXd>& ub,
-                                 double s);
+  solvers::Binding<solvers::LinearConstraint> AddPathVelocityConstraint(
+      const Eigen::Ref<const Eigen::VectorXd>& lb,
+      const Eigen::Ref<const Eigen::VectorXd>& ub, double s);
 
   /** Adds a (generic) constraint on trajectory velocity `q̇(t)`, evaluated at
   `s`. The constraint will be evaluated as if it is bound with variables
@@ -133,55 +133,79 @@ class KinematicTrajectoryOptimization {
   AddVelocityConstraint(constraint, s=1).
   @pre constraint.num_vars() == num_positions()
   @pre 0 <= `s` <= 1. */
-  void AddVelocityConstraintAtNormalizedTime(
+  solvers::Binding<solvers::Constraint> AddVelocityConstraintAtNormalizedTime(
       const std::shared_ptr<solvers::Constraint>& constraint, double s);
 
   /** Adds a linear constraint on the second derivative of the path,
   `lb` ≤ r̈(s) ≤ `ub`. Note that this does NOT directly constrain q̈(t).
   @pre 0 <= `s` <= 1. */
-  void AddPathAccelerationConstraint(
+  solvers::Binding<solvers::LinearConstraint> AddPathAccelerationConstraint(
       const Eigen::Ref<const Eigen::VectorXd>& lb,
       const Eigen::Ref<const Eigen::VectorXd>& ub, double s);
 
-  /** Adds upper and lower bounds on the duration of the trajectory. */
-  void AddDurationConstraint(std::optional<double> lb,
-                             std::optional<double> ub);
+  /** Adds bounding box constraints for upper and lower bounds on the duration
+   * of the trajectory. */
+  solvers::Binding<solvers::BoundingBoxConstraint> AddDurationConstraint(
+      std::optional<double> lb, std::optional<double> ub);
 
   /** Adds bounding box constraints to enforce upper and lower bounds on the
   positions trajectory, q(t). These bounds will be respected at all times,
   t∈[0,T]. This also implies the constraints are satisfied for r(s), for all
-  s∈[0,1]. */
-  void AddPositionBounds(const Eigen::Ref<const Eigen::VectorXd>& lb,
-                         const Eigen::Ref<const Eigen::VectorXd>& ub);
+  s∈[0,1].
+
+  @returns A vector of bindings with the ith element adding a constraint to the
+  ith control point.
+  */
+  std::vector<solvers::Binding<solvers::BoundingBoxConstraint>>
+  AddPositionBounds(const Eigen::Ref<const Eigen::VectorXd>& lb,
+                    const Eigen::Ref<const Eigen::VectorXd>& ub);
 
   /** Adds linear constraints to enforce upper and lower bounds on the velocity
   trajectory, q̇(t). These bounds will be respected at all times. Note this
-  does NOT directly constrain ṙ(s). */
-  void AddVelocityBounds(const Eigen::Ref<const Eigen::VectorXd>& lb,
-                         const Eigen::Ref<const Eigen::VectorXd>& ub);
+  does NOT directly constrain ṙ(s).
+
+  @returns A vector of bindings with the ith element adding a constraint to the
+  ith control point of the derivative trajectory. */
+  std::vector<solvers::Binding<solvers::LinearConstraint>> AddVelocityBounds(
+      const Eigen::Ref<const Eigen::VectorXd>& lb,
+      const Eigen::Ref<const Eigen::VectorXd>& ub);
 
   /** Adds generic (nonlinear) constraints to enforce the upper and lower
   bounds to the acceleration trajectory, q̈(t).  These constraints will be
-  respected at all times.  Note that this does NOT directly constrain r̈(s). */
-  void AddAccelerationBounds(const Eigen::Ref<const Eigen::VectorXd>& lb,
-                             const Eigen::Ref<const Eigen::VectorXd>& ub);
+  respected at all times.  Note that this does NOT directly constrain r̈(s).
 
-  /** Adds generic (nonlinear) constraints to enforce the upper and lower bounds
-  to the jerk trajectory, d³qdt³(t).  These constraints will be respected at all
-  times.  Note that this does NOT directly constrain d³rds³(s). */
-  void AddJerkBounds(const Eigen::Ref<const Eigen::VectorXd>& lb,
-                     const Eigen::Ref<const Eigen::VectorXd>& ub);
+  @returns A vector of bindings with the ith element is itself a vector of
+  constraints (one per dof) adding a constraint to the ith control point of the
+  acceleration trajectory. */
+  std::vector<std::vector<solvers::Binding<solvers::Constraint>>>
+  AddAccelerationBounds(const Eigen::Ref<const Eigen::VectorXd>& lb,
+                        const Eigen::Ref<const Eigen::VectorXd>& ub);
+
+  /** Adds generic (nonlinear) constraints to enforce the upper and lower
+  bounds to the jerk trajectory, d³qdt³(t).  These constraints will be
+  respected at all times.  Note that this does NOT directly constrain
+  d³rds³(s).
+
+  @returns A vector of bindings with the ith element is itself a vector of
+  constraints (one per dof) adding a constraint to the ith control point of the
+  jerk trajectory. */
+  std::vector<std::vector<solvers::Binding<solvers::Constraint>>> AddJerkBounds(
+      const Eigen::Ref<const Eigen::VectorXd>& lb,
+      const Eigen::Ref<const Eigen::VectorXd>& ub);
 
   /** Adds a linear cost on the duration of the trajectory. */
-  void AddDurationCost(double weight = 1.0);
+  solvers::Binding<solvers::LinearCost> AddDurationCost(double weight = 1.0);
 
   /** Adds a cost on an upper bound on length of the path, ∫₀ᵀ |q̇(t)|₂ dt, by
   summing the distance between the path control points. If `use_conic_constraint
   = false`, then costs are added via MathematicalProgram::AddL2NormCost;
   otherwise they are added via
-  MathematicalProgram::AddL2NormCostUsingConicConstraint. */
-  void AddPathLengthCost(double weight = 1.0,
-                         bool use_conic_constraint = false);
+  MathematicalProgram::AddL2NormCostUsingConicConstraint.
+
+  @returns A vector of bindings with the ith element adding a cost to the
+  ith control point of the velocity trajectory. */
+  std::vector<solvers::Binding<solvers::Cost>> AddPathLengthCost(
+      double weight = 1.0, bool use_conic_constraint = false);
 
   /* TODO(russt):
   - Support a cost on an upper bound on "energy" of the path, ∫₀ᵀ |q̇(t)|₂² dt.
