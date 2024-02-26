c0827c44e6ac880ae54751b7ef16b9279a2ca5f9
amice <amice@mit.edu>
2024-02-26 08:51:12 -0500

    add python bindings

--- bindings/pydrake/solvers/test/semidefinite_relaxation_test.py
+++ bindings/pydrake/solvers/test/semidefinite_relaxation_test.py
@@ -5,6 +5,7 @@ from pydrake.solvers import (
     MakeSemidefiniteRelaxation,
     MathematicalProgram,
 )
+from pydrake.symbolic import Variables
 
 
 class TestSemidefiniteRelaxation(unittest.TestCase):
@@ -20,5 +21,26 @@ class TestSemidefiniteRelaxation(unittest.TestCase):
         self.assertEqual(relaxation.num_vars(), 6)
         self.assertEqual(len(relaxation.positive_semidefinite_constraints()),
                          1)
-        self.assertEqual(len(relaxation.bounding_box_constraints()), 1)
+        self.assertEqual(len(relaxation.linear_equality_constraints()), 1)
         self.assertEqual(len(relaxation.linear_constraints()), 2)
+
+    def test_MakeSemidefiniteRelaxationWithGroups(self):
+        prog = MathematicalProgram()
+        x = prog.NewContinuousVariables(2, "x")
+        y = prog.NewContinuousVariables(3, "y")
+
+        x_vars = Variables(x)
+        y_vars = Variables(y)
+
+        A = np.array([[0.5, 0.7], [-.2, 0.4], [-2.3, -4.5]])
+        lb = np.array([1.3, -.24, 0.25])
+        ub = np.array([5.6, 0.1, 1.4])
+        prog.AddLinearConstraint(A, lb, ub, x)
+        prog.AddQuadraticConstraint(A, lb, 0, 5, y)
+        relaxation = MakeSemidefiniteRelaxation(prog, [x_vars, y_vars])
+
+        self.assertEqual(relaxation.num_vars(), 6 + 10)
+        self.assertEqual(len(relaxation.positive_semidefinite_constraints()),
+                         2)
+        self.assertEqual(len(relaxation.linear_equality_constraints()), 2)
+        self.assertEqual(len(relaxation.linear_constraints()), 2+1)
