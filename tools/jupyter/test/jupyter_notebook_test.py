c9c22234f2f54e040c3d7289c2a7f1099f2ef3d0
Stephen McDowell <stephen.mcdowell@kitware.com>
2024-03-01 17:01:58 -0500

    [workspace] Remove vestigial jupyter package
    - https://jupyter.org/install#jupyter-notebook
    - https://pypi.org/project/jupyter/ (very old...)
    - https://pypi.org/project/notebook/ (what we want...)
    
    Note that `notebook` is already installed via
    binary_distribution/requirements.txt.
    
    Also update to call the `notebook` python command from the same
    interpreter.

--- tools/jupyter/test/jupyter_notebook_test.py
+++ tools/jupyter/test/jupyter_notebook_test.py
@@ -1,9 +1,11 @@
 import subprocess
+import sys
 import unittest
 
 
 class TestJupyterNotebook(unittest.TestCase):
     def test_help(self):
         """Ensures that `jupyter notebook` is installed (#12042)."""
-        status = subprocess.call(args=["jupyter", "notebook", "--help"])
+        status = subprocess.call(
+            args=[sys.executable, "-m", "notebook", "--help"])
         self.assertEqual(status, 0)
