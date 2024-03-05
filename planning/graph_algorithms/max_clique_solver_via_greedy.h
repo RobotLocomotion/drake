70668ecf81d9f6d913676831314121000b42c36b
Peter <wernerpe@mit.edu>
2024-03-05 15:31:24 -0500

    docs updated

--- planning/graph_algorithms/max_clique_solver_via_greedy.h
+++ planning/graph_algorithms/max_clique_solver_via_greedy.h
@@ -9,9 +9,15 @@ namespace planning {
 namespace graph_algorithms {
 
 /**
- * Produces approximate solution to the maximum clique problem via a greedy
- * heuristic.
- * TODO(wernerpe): complete docs
+ * Approximately solves the maximum clique problem via a greedy
+ * heuristic. Vertices are greedily added to the clique based on their degree of
+ * connectivity to the remaining candidate vertices. Candidate vertices are
+ * non-clique members that share an edge with every clique member. The algorithm
+ * initializes the clique with an empty set and makes every vertex a candidate,
+ * then the degree of connectivity of every vertex is computed and candidate
+ * vertex with the highest degree is added to the clique. Afterwards, the
+ * adjacency matrix and new candidate list are updated and the previous two
+ * steps are repeated.
  */
 class MaxCliqueSolverViaGreedy final : public MaxCliqueSolverBase {
  public:
