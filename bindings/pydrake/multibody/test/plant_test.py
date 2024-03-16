eec20faba4dc8086c04d67e9c2fa2377961a4b5c
Nicholas Pfaff <53228351+nepfaff@users.noreply.github.com>
2024-03-15 20:48:50 -0400

    Bind CalcPrincipalSemiDiametersAndPoseForSolidEllipsoid (#21149)
    Co-authored-by: Jeremy Nimmer <jeremy.nimmer@tri.global>

--- bindings/pydrake/multibody/test/plant_test.py
+++ bindings/pydrake/multibody/test/plant_test.py
@@ -838,6 +838,10 @@ class TestPlant(unittest.TestCase):
         # work.
         if T != Expression:
             self.assertTrue(spatial_inertia.IsPhysicallyValid())
+        (semi_diameters, transform) = spatial_inertia \
+            .CalcPrincipalSemiDiametersAndPoseForSolidEllipsoid()
+        self.assertIsInstance(semi_diameters, np.ndarray)
+        self.assertIsInstance(transform, RigidTransform)
         self.assertIsInstance(spatial_inertia.CopyToFullMatrix6(), np.ndarray)
         self.assertIsInstance(
             spatial_inertia.ReExpress(R_AE=RotationMatrix()), SpatialInertia)
