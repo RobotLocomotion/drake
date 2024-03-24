34a08c4d05870db4815a9605d79a1d7c57ff7dbe
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-03-24 11:03:50 -0700

    [render_vtk] Silence gltf basisu extension warnings by default
    Add RenderVtkParams configuration for what to silence.
    
    Fix serialize_pybind to admit the full vector and map template signatures.

--- bindings/pydrake/common/serialize_pybind.h
+++ bindings/pydrake/common/serialize_pybind.h
@@ -168,15 +168,15 @@ class DefAttributesArchive {
   }
 
   // Partial specialization for List.
-  template <typename U>
-  static py::object CalcSchemaType(const std::vector<U>*) {
+  template <typename U, typename A>
+  static py::object CalcSchemaType(const std::vector<U, A>*) {
     auto u_type = CalcSchemaType(static_cast<U*>(nullptr));
     return GetTemplateClass("List")[u_type];
   }
 
   // Partial specialization for Dict.
-  template <typename U, typename V>
-  static py::object CalcSchemaType(const std::map<U, V>*) {
+  template <typename U, typename V, typename C, typename A>
+  static py::object CalcSchemaType(const std::map<U, V, C, A>*) {
     auto u_type = CalcSchemaType(static_cast<U*>(nullptr));
     auto v_type = CalcSchemaType(static_cast<V*>(nullptr));
     auto inner_types = py::make_tuple(u_type, v_type);
