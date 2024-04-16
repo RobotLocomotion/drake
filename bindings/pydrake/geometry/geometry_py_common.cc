8475f0fe20f04623c885eb378f9592bfccfeb3ce
Rick Poyner <rick.poyner@tri.global>
2024-04-16 15:32:32 -0400

    [geometry, multibody] Integrate scene graph configuration
    This patch integrates SceneGraphConfig into scene graph, and completes the
    implementation of easy, no-annotation hydroelastic configuration.

--- bindings/pydrake/geometry/geometry_py_common.cc
+++ bindings/pydrake/geometry/geometry_py_common.cc
@@ -128,6 +128,7 @@ void DoScalarIndependentDefinitions(py::module m) {
         .def("HasGroup", &Class::HasGroup, py::arg("group_name"),
             cls_doc.HasGroup.doc)
         .def("num_groups", &Class::num_groups, cls_doc.num_groups.doc)
+        .def("IsEmpty", &Class::IsEmpty, cls_doc.IsEmpty.doc)
         .def(
             "GetPropertiesInGroup",
             [](const Class& self, const std::string& group_name) {
