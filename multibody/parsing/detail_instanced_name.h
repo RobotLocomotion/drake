79d6093b15cd3b476f96de0a67bc5512b30e1d61
Rick Poyner <rick.poyner@tri.global>
2024-04-16 18:38:31 -0400

    [parsing] Fix name clash of collision filter groups
    This patch fixes a name clash of collision filter groups when loading the same
    model multiple times using auto-renaming and plant.RenameModel().

---
+++ multibody/parsing/detail_instanced_name.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <compare>
+#include <optional>
+#include <string>
+
+#include "drake/common/fmt.h"
+#include "drake/multibody/tree/multibody_tree_indexes.h"
+
+namespace drake {
+namespace multibody {
+namespace internal {
+
+// Describes a name as found during parsing, together with index of the model
+// where it was found. It purposely does not record the model by name, since
+// model renaming via the plant could invalidate such names.
+struct InstancedName {
+  std::optional<ModelInstanceIndex> index;  // Empty means global name.
+  std::string name;
+
+  bool operator==(const InstancedName&) const = default;
+  // TODO(rpoyner-tri): replace explicit implementation with compiler
+  // default once macOS Ventura support ends.
+  std::strong_ordering operator<=>(const InstancedName& that) const;
+
+  std::string to_string() const;
+};
+
+}  // namespace internal
+}  // namespace multibody
+}  // namespace drake
+
+DRAKE_FORMATTER_AS(, drake::multibody::internal, InstancedName, x,
+                   x.to_string())
