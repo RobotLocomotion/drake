78a2461644e7bc8c25e4ee32062b58767e1221a7
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-04-28 15:16:19 -0700

    [geometry] Factor recording implementation out of Meshcat
    The user-facing API is unchanged.
    
    This provides for better encapsulation, less brittleness, and easier
    unit testing -- all of which will be critically important as we start
    to add support for timed SetObject calls.
    
    This also fixes an implementation inconsistency where the *data type*
    of the property being set affected whether or not during a recording
    with set_visualizations_in_recording=false an untimed SetProperty
    would be shown live or not.

---
+++ geometry/test/meshcat_recording_internal_test.cc
@@ -0,0 +1,17 @@
+#include "drake/geometry/meshcat_recording_internal.h"
+
+#include <gtest/gtest.h>
+
+namespace drake {
+namespace geometry {
+namespace internal {
+namespace {
+
+GTEST_TEST(MeshcatRecordingInternalTest, Basic) {
+  MeshcatRecording dut;
+}
+
+}  // namespace
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
