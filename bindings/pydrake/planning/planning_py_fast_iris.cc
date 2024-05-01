f60e180fae9442e676cdf282223a219ed6b5d44a
Peter <wernerpe@mit.edu>
2024-05-01 14:34:05 -0400

    fast iris first commit

---
+++ bindings/pydrake/planning/planning_py_fast_iris.cc
@@ -0,0 +1,122 @@
+#include "drake/bindings/pydrake/documentation_pybind.h"
+#include "drake/bindings/pydrake/pydrake_pybind.h"
+#include "drake/planning/fast_iris.h"
+
+namespace drake {
+namespace pydrake {
+namespace internal {
+
+void DefinePlanningFastIris(py::module m) {
+  // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
+  using namespace drake::planning;
+  constexpr auto& doc = pydrake_doc.drake.planning;
+
+  // FastIrisOptions  
+  const auto& cls_doc = doc.FastIrisOptions;
+  py::class_<FastIrisOptions>(m, "FastIrisOptions", cls_doc.doc)
+    .def(py::init<>())
+    .def_readwrite("num_particles",
+        &FastIrisOptions::num_particles,
+        cls_doc.num_particles.doc)
+    .def_readwrite("tau",
+        &FastIrisOptions::tau,
+        cls_doc.tau.doc)
+    .def_readwrite("delta",
+        &FastIrisOptions::delta,
+        cls_doc.delta.doc)
+    .def_readwrite("admissible_proportion_in_collision",
+        &FastIrisOptions::admissible_proportion_in_collision,
+        cls_doc.admissible_proportion_in_collision.doc)
+    .def_readwrite("containment_points",
+        &FastIrisOptions::containment_points,
+        cls_doc.containment_points.doc)
+    .def_readwrite("force_containment_points",
+        &FastIrisOptions::force_containment_points,
+        cls_doc.force_containment_points.doc)
+    .def_readwrite("max_iterations",
+        &FastIrisOptions::max_iterations,
+        cls_doc.max_iterations.doc)
+    .def_readwrite("max_iterations_separating_planes",
+        &FastIrisOptions::max_iterations_separating_planes,
+        cls_doc.max_iterations_separating_planes.doc)
+    .def_readwrite("max_separating_planes_per_iteration",
+        &FastIrisOptions::max_separating_planes_per_iteration,
+        cls_doc.max_separating_planes_per_iteration.doc)
+    .def_readwrite("bisection_steps",
+        &FastIrisOptions::bisection_steps,
+        cls_doc.bisection_steps.doc)
+    .def_readwrite("parallelize",
+        &FastIrisOptions::parallelize,
+        cls_doc.parallelize.doc)
+    .def_readwrite("verbose",
+        &FastIrisOptions::verbose,
+        cls_doc.verbose.doc)
+    .def_readwrite("require_sample_point_is_contained",
+        &FastIrisOptions::require_sample_point_is_contained,
+        cls_doc.require_sample_point_is_contained.doc)
+    .def_readwrite("configuration_space_margin",
+        &FastIrisOptions::configuration_space_margin,
+        cls_doc.configuration_space_margin.doc)
+    .def_readwrite("termination_threshold",
+        &FastIrisOptions::termination_threshold,
+        cls_doc.termination_threshold.doc)
+    .def_readwrite("relative_termination_threshold",
+        &FastIrisOptions::relative_termination_threshold,
+        cls_doc.relative_termination_threshold.doc)
+    .def_readwrite("random_seed",
+        &FastIrisOptions::random_seed,
+        cls_doc.random_seed.doc)
+    .def("__repr__", [](const FastIrisOptions& self) {
+        return py::str(
+            "FastIrisOptions("
+            "num_particles={}, "
+            "tau={}, "
+            "delta={}, "
+            "admissible_proportion_in_collision={}, "
+            "force_containment_points={}, "
+            "num_consecutive_failures={}, "
+            "max_iterations={}, "
+            "max_iterations_separating_planes={}, "
+            "max_separating_planes_per_iteration={}, "
+            "bisection_steps={}, "
+            "parallelize={}, "
+            "verbose={}, "
+            "require_sample_point_is_contained={}, "
+            "configuration_space_margin={}, "
+            "termination_threshold={}, "
+            "relative_termination_threshold={}, "
+            "random_seed={} "
+            ")")
+            .format(self.num_particles,
+                self.tau,
+                self.delta,
+                self.admissible_proportion_in_collision,
+                self.containment_points,
+                self.force_containment_points,
+                self.max_iterations,
+                self.max_iterations_separating_planes,
+                self.max_separating_planes_per_iteration,
+                self.bisection_steps,
+                self.parallelize,
+                self.verbose,
+                self.require_sample_point_is_contained,
+                self.configuration_space_margin,
+                self.termination_threshold,
+                self.relative_termination_threshold,
+                self.random_seed);
+    });
+  
+
+  m.def("FastIris", 
+      &FastIris,
+      py::arg("checker"), 
+      py::arg("starting_ellipsoid"), 
+      py::arg("domain"),
+      py::arg("options") = FastIrisOptions(),
+      doc.FastIris.doc);
+
+}
+
+}  // namespace internal
+}  // namespace pydrake
+}  // namespace drake
---
+++ planning/fast_iris.h
@@ -0,0 +1,140 @@
+#pragma once
+
+#include <filesystem>
+#include <memory>
+#include <optional>
+#include <utility>
+#include <vector>
+
+#include <Eigen/Dense>
+
+#include "drake/common/parallelism.h"
+#include "drake/geometry/meshcat.h"
+// #include "drake/geometry/optimization/convex_set.h"
+#include "drake/geometry/optimization/hpolyhedron.h"
+#include "drake/geometry/optimization/hyperellipsoid.h"
+#include "drake/planning/collision_checker.h"
+
+namespace drake {
+namespace planning {
+
+using geometry::optimization::HPolyhedron;
+using geometry::optimization::Hyperellipsoid;
+
+struct FastIrisOptions {
+  /** Passes this object to an Archive.
+  Refer to @ref yaml_serialization "YAML Serialization" for background.
+  Note: This only serializes options that are YAML built-in types. */
+  template <typename Archive>
+  void Serialize(Archive* a) {
+    a->Visit(DRAKE_NVP(num_particles));
+    a->Visit(DRAKE_NVP(tau));
+    a->Visit(DRAKE_NVP(delta));
+    a->Visit(DRAKE_NVP(admissible_proportion_in_collision));
+    a->Visit(DRAKE_NVP(containment_points));
+    a->Visit(DRAKE_NVP(force_containment_points));
+    a->Visit(DRAKE_NVP(max_iterations));
+    a->Visit(DRAKE_NVP(max_iterations_separating_planes));
+    a->Visit(DRAKE_NVP(max_separating_planes_per_iteration));
+    a->Visit(DRAKE_NVP(bisection_steps));
+    a->Visit(DRAKE_NVP(parallelize));
+    a->Visit(DRAKE_NVP(verbose));
+    a->Visit(DRAKE_NVP(require_sample_point_is_contained));
+    a->Visit(DRAKE_NVP(configuration_space_margin));
+    a->Visit(DRAKE_NVP(termination_threshold));
+    a->Visit(DRAKE_NVP(relative_termination_threshold));
+    a->Visit(DRAKE_NVP(random_seed));
+  }
+
+  FastIrisOptions() = default;
+
+  /** Number of particles used to estimate the closest collision*/
+  int num_particles = 1e3;
+
+  double tau = 0.5;
+
+  double delta = 5e-2;
+
+  double admissible_proportion_in_collision = 1e-2;
+
+  /** Points that are guaranteed to be contained in the final region
+   * provided their convex hull is collision free.*/
+  Eigen::MatrixXd containment_points;
+
+  /** If true, sets faces tangent to the sublevelsets of dist(C), where
+   * c is the convex hull of the points passed in `containment_points`.
+   */
+  bool force_containment_points{false};
+
+  /** Number of resampling steps for the gradient updates*/
+  // int num_resampling_steps = 1;
+
+  /** Number Iris Iterations*/
+  int max_iterations{2};
+
+  /** Maximum number of rounds of adding faces to the polytope*/
+  int max_iterations_separating_planes{20};
+
+  /** Maximum number of faces to add per round of samples*/
+  int max_separating_planes_per_iteration{-1};
+
+  /** Maximum number of bisection steps per gradient step*/
+  int bisection_steps{10};
+
+  /** Parallelize the updates of the particles*/
+  bool parallelize{true};
+
+  /* Enables print statements indicating the progress of fast iris**/
+  bool verbose{false};
+
+  /** The initial polytope is guaranteed to contain the point if that point is
+  collision-free. However, the IRIS alternation objectives do not include (and
+  can not easily include) a constraint that the original sample point is
+  contained. Therefore, the IRIS paper recommends that if containment is a
+  requirement, then the algorithm should simply terminate early if alternations
+  would ever cause the set to not contain the point. */
+  bool require_sample_point_is_contained{true};
+
+  /** For IRIS in configuration space, we retreat by this margin from each
+  C-space obstacle in order to avoid the possibility of requiring an infinite
+  number of faces to approximate a curved boundary.
+  */
+  double configuration_space_margin{1e-2};
+
+  /** IRIS will terminate if the change in the *volume* of the hyperellipsoid
+  between iterations is less that this threshold. This termination condition can
+  be disabled by setting to a negative value. */
+  double termination_threshold{2e-2};
+
+  /** IRIS will terminate if the change in the *volume* of the hyperellipsoid
+  between iterations is less that this percent of the previous best volume.
+  This termination condition can be disabled by setting to a negative value. */
+  double relative_termination_threshold{1e-3};  // from rdeits/iris-distro.
+
+  /** For IRIS in configuration space, it can be beneficial to not only specify
+  task-space obstacles (passed in through the plant) but also obstacles that are
+  defined by convex sets in the configuration space. This option can be used to
+  pass in such configuration space obstacles. */
+  // ConvexSets configuration_obstacles{};
+
+  /** The only randomization in IRIS is the random sampling done to find
+  counter-examples for the additional constraints using in
+  IrisInConfigurationSpace. Use this option to set the initial seed. */
+  int random_seed{1234};
+
+  /** Passing a meshcat instance may enable debugging visualizations; this
+  currently and when the
+  configuration space is <= 3 dimensional.*/
+  std::shared_ptr<geometry::Meshcat> meshcat{};
+};
+
+/** Given a seed point and an initial ellipsoidal metric, use sampling based
+optimization to find a collision free polytope in cspace.*/
+
+HPolyhedron FastIris(const CollisionChecker& checker,
+                     const Hyperellipsoid& starting_ellipsoid,
+                     const HPolyhedron& domain,
+                     const FastIrisOptions& options = FastIrisOptions());
+
+}  // namespace planning
+}  // namespace drake
