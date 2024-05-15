1b22d941ed001ac0d7202f9fe4428829f608f251
Christian Barcelo <cbarcelo@theaiinstitute.com>
2024-05-15 16:33:42 +0000

    Reorder SpatialVelocity::Shift to avoid issues with ARM

--- multibody/math/spatial_velocity.h
+++ multibody/math/spatial_velocity.h
@@ -109,7 +109,9 @@ class SpatialVelocity : public SpatialVector<SpatialVelocity, T> {
   /// `this` whereas ShiftInPlace() does modify `this`.
   /// @see ShiftInPlace() for more information and how V_MC_E is calculated.
   SpatialVelocity<T> Shift(const Vector3<T>& offset) const {
-    return SpatialVelocity<T>(*this).ShiftInPlace(offset);
+    auto ret = SpatialVelocity<T>(*this);
+    ret.ShiftInPlace(offset);
+    return ret;
   }
 
   /// Compose `this` spatial velocity (measured in some frame M) with the
