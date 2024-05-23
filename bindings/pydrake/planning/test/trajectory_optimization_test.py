de81568ba9ea410f810eaffb906ddf142dcd65f5
David von Wrangel <49842806+wrangelvid@users.noreply.github.com>
2024-05-23 13:19:39 -0400

    add nonlinear derivative bounds (#21459)

--- bindings/pydrake/planning/test/trajectory_optimization_test.py
+++ bindings/pydrake/planning/test/trajectory_optimization_test.py
@@ -585,6 +585,32 @@ class TestTrajectoryOptimization(unittest.TestCase):
                                   precision=3,
                                   scientific=False), str)
 
+        # In the follwoing, we test adding the bindings for nonlinear
+        # constraints.
+        max_acceleration = np.ones((2, 1))
+        max_jerk = 7.5 * np.ones((2, 1))
+
+        # Add global acceleration bounds.
+        gcs.AddNonlinearDerivativeBounds(
+            lb=-max_acceleration, ub=max_acceleration, derivative_order=2)
+
+        # Add the jerk bounds to each subgraph.
+        main1.AddNonlinearDerivativeBounds(
+            lb=-max_jerk, ub=max_jerk, derivative_order=3)
+        main2.AddNonlinearDerivativeBounds(
+            lb=-max_jerk, ub=max_jerk, derivative_order=3)
+
+        # Add half of the maximum acceleration and jerk bounds at the subspace
+        # point and region.
+        main1_to_main2_pt.AddVelocityBounds(
+            lb=-max_acceleration / 2, ub=max_acceleration / 2)
+        main1_to_main2_region.AddVelocityBounds(
+            lb=-max_acceleration / 2, ub=max_acceleration / 2)
+
+        main1_to_main2_pt.AddVelocityBounds(lb=-max_jerk / 2, ub=max_jerk / 2)
+        main1_to_main2_region.AddVelocityBounds(
+            lb=-max_jerk / 2, ub=max_jerk / 2)
+
     def test_gcs_trajectory_optimization_wraparound(self):
         gcs_wraparound = GcsTrajectoryOptimization(
             num_positions=1, continuous_revolute_joints=[0])
