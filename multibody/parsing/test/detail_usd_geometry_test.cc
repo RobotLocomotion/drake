287598b0fc2a36d4a12136063c8289f47c84b443
Hong Wang <hongw@nvidia.com>
2024-05-29 11:49:20 -0700

    Implement GetEllipsoidDimensionTest

--- multibody/parsing/test/detail_usd_geometry_test.cc
+++ multibody/parsing/test/detail_usd_geometry_test.cc
@@ -2,6 +2,7 @@
 
 #include "pxr/usd/usd/stage.h"
 #include "pxr/usd/usdGeom/cube.h"
+#include "pxr/usd/usdGeom/sphere.h"
 #include <gtest/gtest.h>
 
 #include "drake/common/test_utilities/diagnostic_policy_test_base.h"
@@ -45,8 +46,36 @@ TEST_F(UsdGeometryTest, GetBoxDimensionTest) {
   std::optional<Eigen::Vector3d> dimension = GetBoxDimension(
     cube.GetPrim(), meters_per_unit_, diagnostic_policy_);
   EXPECT_TRUE(dimension.has_value());
-  double final_size = size * scale_factor * meters_per_unit_;
-  EXPECT_EQ(dimension, Eigen::Vector3d(final_size, final_size, final_size));
+
+  double correct_size = size * scale_factor * meters_per_unit_;
+  Eigen::Vector3d correct_dimension =
+    Eigen::Vector3d(correct_size, correct_size, correct_size);
+  EXPECT_EQ(dimension.value(), correct_dimension);
+}
+
+TEST_F(UsdGeometryTest, GetEllipsoidDimensionTest) {
+  pxr::UsdGeomSphere sphere = pxr::UsdGeomSphere::Define(
+    stage_, pxr::SdfPath("/Sphere"));
+
+  double radius = 17.0;
+  EXPECT_TRUE(sphere.CreateRadiusAttr().Set(radius));
+
+  pxr::VtVec3fArray extent;
+  EXPECT_TRUE(pxr::UsdGeomSphere::ComputeExtent(radius, &extent));
+  EXPECT_TRUE(sphere.CreateExtentAttr().Set(extent));
+
+  pxr::GfVec3d scale_factor = pxr::GfVec3d(0.6, 1.1, 2.9);
+  pxr::GfMatrix4d transform = pxr::GfMatrix4d();
+  transform.SetScale(scale_factor);
+  EXPECT_TRUE(sphere.AddTransformOp().Set(transform));
+
+  std::optional<Eigen::Vector3d> dimension = GetEllipsoidDimension(
+    sphere.GetPrim(), meters_per_unit_, diagnostic_policy_);
+  EXPECT_TRUE(dimension.has_value());
+
+  Eigen::Vector3d correct_dimension =
+    UsdVec3dToEigen(scale_factor) * radius * meters_per_unit_;
+  EXPECT_EQ(dimension.value(), correct_dimension);
 }
 
 }  // namespace
