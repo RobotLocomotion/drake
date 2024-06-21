c9cbb1d5626ea9d15a5a5491d6f8fb2b806b2593
Rick Poyner (rico) <rick.poyner@tri.global>
2024-06-21 09:19:09 -0400

    [parsing] Fix name clash of collision filter groups (#21529)
    This patch fixes a name clash of collision filter groups when loading the same
    model multiple times using auto-renaming and plant.RenameModel().

--- multibody/parsing/parser.cc
+++ multibody/parsing/parser.cc
@@ -2,10 +2,13 @@
 
 #include <optional>
 #include <set>
+#include <utility>
 
 #include "drake/multibody/parsing/detail_collision_filter_group_resolver.h"
+#include "drake/multibody/parsing/detail_collision_filter_groups_impl.h"
 #include "drake/multibody/parsing/detail_common.h"
 #include "drake/multibody/parsing/detail_composite_parse.h"
+#include "drake/multibody/parsing/detail_instanced_name.h"
 #include "drake/multibody/parsing/detail_parsing_workspace.h"
 #include "drake/multibody/parsing/detail_path_utils.h"
 #include "drake/multibody/parsing/detail_select_parser.h"
@@ -16,10 +19,20 @@ namespace multibody {
 using drake::internal::DiagnosticDetail;
 using drake::internal::DiagnosticPolicy;
 using internal::CollisionFilterGroupResolver;
+using internal::CollisionFilterGroupsImpl;
 using internal::DataSource;
+using internal::InstancedName;
 using internal::ParserInterface;
 using internal::ParsingWorkspace;
 using internal::SelectParser;
+
+// Storage for internals that need to have the same lifetime as a Parser
+// instance, but avoid adding internal namespace details to parser.h.
+struct Parser::ParserInternalData {
+  // Collision filter groups that use InstancedName. This representation is
+  // invariant with respect to model renaming via the plant.
+  CollisionFilterGroupsImpl<InstancedName> collision_filter_groups_storage_;
+};
 
 Parser::Parser(MultibodyPlant<double>* plant,
                geometry::SceneGraph<double>* scene_graph)
@@ -32,7 +45,7 @@ Parser::Parser(MultibodyPlant<double>* plant,
 Parser::Parser(MultibodyPlant<double>* plant,
                geometry::SceneGraph<double>* scene_graph,
                std::string_view model_name_prefix)
-    : plant_(plant) {
+    : plant_(plant), data_(new Parser::ParserInternalData) {
   DRAKE_THROW_UNLESS(plant != nullptr);
 
   if (!model_name_prefix.empty()) {
@@ -54,14 +67,23 @@ Parser::Parser(MultibodyPlant<double>* plant,
   diagnostic_policy_.SetActionForWarnings(warnings_maybe_strict);
 }
 
+Parser::~Parser() {}
+
+CollisionFilterGroups Parser::GetCollisionFilterGroups() const {
+  return CollisionFilterGroups(ConvertInstancedNamesToStrings(
+      data_->collision_filter_groups_storage_, *plant_));
+}
+
 std::vector<ModelInstanceIndex> Parser::AddModels(
     const std::filesystem::path& file_name) {
   const std::string filename_string{file_name.string()};
   DataSource data_source(DataSource::kFilename, &filename_string);
   ParserInterface& parser = SelectParser(diagnostic_policy_, file_name);
   auto composite = internal::CompositeParse::MakeCompositeParse(this);
-  return parser.AddAllModels(data_source, model_name_prefix_,
-                             composite->workspace());
+  auto result = parser.AddAllModels(data_source, model_name_prefix_,
+                                    composite->workspace());
+  composite->Finish();
+  return result;
 }
 
 std::vector<ModelInstanceIndex> Parser::AddModelsFromUrl(
@@ -80,8 +102,25 @@ std::vector<ModelInstanceIndex> Parser::AddModelsFromString(
   const std::string pseudo_name(data_source.GetStem() + "." + file_type);
   ParserInterface& parser = SelectParser(diagnostic_policy_, pseudo_name);
   auto composite = internal::CompositeParse::MakeCompositeParse(this);
-  return parser.AddAllModels(data_source, model_name_prefix_,
-                             composite->workspace());
+  auto result = parser.AddAllModels(data_source, model_name_prefix_,
+                                    composite->workspace());
+  composite->Finish();
+  return result;
+}
+
+void Parser::ResolveCollisionFilterGroupsFromCompositeParse(
+    internal::CollisionFilterGroupResolver* resolver) {
+  DRAKE_DEMAND(resolver != nullptr);
+
+  const CollisionFilterGroupsImpl<internal::InstancedName> groups =
+      resolver->Resolve(diagnostic_policy_);
+
+  // Merge the groups found during a composite parse into the accumulated groups
+  // held by this parser.
+  data_->collision_filter_groups_storage_.Merge(groups);
+
+  // Deprecated 2024-10-01: copy the merged groups to the legacy data object.
+  collision_filter_groups_ = GetCollisionFilterGroups();
 }
 
 }  // namespace multibody
