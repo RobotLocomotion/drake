3da55950ce51120a3646e2d15dc666e4dd6d3691
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-06-24 21:33:12 -0700

    [multibody] GeometryContactData takes over deformable caching

--- multibody/plant/geometry_contact_data.h
+++ multibody/plant/geometry_contact_data.h
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "drake/geometry/query_results/contact_surface.h"
+#include "drake/geometry/query_results/deformable_contact.h"
 #include "drake/geometry/query_results/penetration_as_point_pair.h"
 
 namespace drake {
@@ -14,13 +15,41 @@ consumed by MultibodyPlant. Depending on MbP's contact model and the proximity
 properties in the scene graph, one or the other vector might be guaranteed to be
 empty; e.g., even in kPoint only mode we still have a field named `surfaces` but
 it's always empty.
+
+When T != double, the class is specialized to omit member data that is
+incompatible with such scalars.
+
 @tparam_default_scalar */
 template <typename T>
 struct GeometryContactData {
   std::vector<geometry::PenetrationAsPointPair<T>> point_pairs;
   std::vector<geometry::ContactSurface<T>> surfaces;
-  // TODO(jwnimmer-tri) It seems to me like DeformableContact<T> should be
-  // computed and cached here as well.
+  geometry::internal::DeformableContact<T> deformable;
+};
+
+/* Full specialization of HydroelasticContactInfo for T = AutoDiffXd. */
+template <>
+class GeometryContactData<AutoDiffXd> {
+ public:
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(GeometryContactData);
+  GeometryContactData() = default;
+
+  using T = AutoDiffXd;
+  std::vector<geometry::PenetrationAsPointPair<T>> point_pairs;
+  std::vector<geometry::ContactSurface<T>> surfaces;
+};
+
+/* Full specialization of HydroelasticContactInfo for T = Expression. */
+template <>
+class GeometryContactData<symbolic::Expression> {
+ public:
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(GeometryContactData);
+  GeometryContactData() = default;
+
+  using T = symbolic::Expression;
+  std::vector<geometry::PenetrationAsPointPair<T>> point_pairs;
+  // TODO(jwnimmer-tri) ContactSurface here is wrong and we should remove it.
+  std::vector<geometry::ContactSurface<T>> surfaces;
 };
 
 }  // namespace internal
