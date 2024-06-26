8ec94804ee39994b9decc6f8df8e7aaadae2f517
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-06-25 21:09:57 -0700

    [math] Use dynamic dispatch for highway SIMD

--- math/fast_pose_composition_functions.h
+++ math/fast_pose_composition_functions.h
@@ -1,30 +1,9 @@
 #pragma once
 
-/** @file
-Internal use only. */
-
-/* Declarations for fast, low-level functions for handling objects stored in
-small matrices with known memory layouts. Ideally these are implemented using
-platform-specific SIMD instructions for speed; however, we always provide a
-straightforward portable fallback. */
-
-/* N.B. Do not include code from drake/common here because this file will be
-included by a compilation unit that may have a different opinion about whether
-SIMD instructions are enabled than Eigen does in the rest of Drake. */
-
 namespace drake {
 namespace math {
 
-/* We do not have access to the declarations for RotationMatrix and
-RigidTransform here. Instead, the functions below depend on knowledge of the
-internal storage format of these classes, which is guaranteed and enforced by
-the class declarations:
- - Drake RotationMatrix objects are stored as 3x3 column-ordered matrices in
-   nine consecutive doubles.
- - Drake RigidTransform objects are stored as 3x4 column-ordered matrices in
-   twelve consecutive doubles. The first nine elements comprise a 3x3
-   RotationMatrix and the last three are the translation vector. */
-
+// We must forward-declare these classes to avoid a circular dependency.
 #ifndef DRAKE_DOXYGEN_CXX
 template <typename>
 class RotationMatrix;
@@ -34,7 +13,12 @@ class RigidTransform;
 
 namespace internal {
 
-/* Composes two drake::math::RotationMatrix<double> objects as quickly as
+/* Declarations for fast, low-level functions for handling objects stored in
+small matrices with known memory layouts. Ideally these are implemented using
+platform-specific SIMD instructions for speed; however, we always provide a
+straightforward portable fallback. */
+
+/* Composes two RotationMatrix<double> objects as quickly as
 possible, resulting in a new RotationMatrix.
 
 Here we calculate `R_AC = R_AB * R_BC`. It is OK for R_AC to overlap
@@ -43,66 +27,44 @@ void ComposeRR(const RotationMatrix<double>& R_AB,
                const RotationMatrix<double>& R_BC,
                RotationMatrix<double>* R_AC);
 
-/* Composes the inverse of a drake::math::RotationMatrix<double> object with
-another (non-inverted) drake::math::RotationMatrix<double> as quickly as
-possible, resulting in a new RotationMatrix.
+/* Composes the inverse of a RotationMatrix<double> object with another
+(non-inverted) RotationMatrix<double> as quickly as possible, resulting in a new
+RotationMatrix.
 
 @note A valid RotationMatrix is orthonormal, and the inverse of an orthonormal
 matrix is just its transpose. This function assumes orthonormality and hence
 simply multiplies the transpose of its first argument by the second.
 
-Here we calculate `R_AC = R_BA⁻¹ * R_BC`. It is OK for R_AC to overlap
-with one or both inputs. */
+Here we calculate `R_AC = R_BA⁻¹ * R_BC`. It is OK for R_AC to overlap with one
+or both inputs. */
 void ComposeRinvR(const RotationMatrix<double>& R_BA,
                   const RotationMatrix<double>& R_BC,
                   RotationMatrix<double>* R_AC);
 
-/** Composes two drake::math::RigidTransform<double> objects as quickly as
-possible, resulting in a new RigidTransform.
+/* Composes two RigidTransform<double> objects as quickly as possible, resulting
+in a new RigidTransform.
 
-@note This function is specialized for RigidTransforms and is not just a
-matrix multiply.
+@note This function is specialized for RigidTransforms and is not just a matrix
+multiply.
 
-Here we calculate `X_AC = X_AB * X_BC`. It is OK for X_AC to overlap
-with one or both inputs. */
+Here we calculate `X_AC = X_AB * X_BC`. It is OK for X_AC to overlap with one or
+both inputs. */
 void ComposeXX(const RigidTransform<double>& X_AB,
                const RigidTransform<double>& X_BC,
                RigidTransform<double>* X_AC);
 
-/** Composes the inverse of a drake::math::RigidTransform<double> object with
-another (non-inverted) drake::math::RigidTransform<double> as quickly as
-possible, resulting in a new RigidTransform.
+/* Composes the inverse of a RigidTransform<double> object with another
+(non-inverted) RigidTransform<double> as quickly as possible, resulting in a new
+RigidTransform.
 
-@note This function is specialized for RigidTransforms and is not just a
-matrix multiply.
+@note This function is specialized for RigidTransforms and is not just a matrix
+multiply.
 
-Here we calculate `X_AC = X_BA⁻¹ * X_BC`. It is OK for X_AC to overlap
-with one or both inputs. */
+Here we calculate `X_AC = X_BA⁻¹ * X_BC`. It is OK for X_AC to overlap with one
+or both inputs. */
 void ComposeXinvX(const RigidTransform<double>& X_BA,
                   const RigidTransform<double>& X_BC,
                   RigidTransform<double>* X_AC);
-
-/* Returns `true` if we are using the portable fallback implementations for
-the above functions. */
-bool IsUsingPortableCompositionFunctions();
-
-/* These portable implementations are exposed so they can be unit tested.
-Call IsUsingPortableCompositionFunctions() to determine whether these are being
-used to implement the above functions. */
-
-void ComposeRRPortable(const RotationMatrix<double>& R_AB,
-                       const RotationMatrix<double>& R_BC,
-                       RotationMatrix<double>* R_AC);
-void ComposeRinvRPortable(const RotationMatrix<double>& R_BA,
-                          const RotationMatrix<double>& R_BC,
-                          RotationMatrix<double>* R_AC);
-
-void ComposeXXPortable(const RigidTransform<double>& X_AB,
-                       const RigidTransform<double>& X_BC,
-                       RigidTransform<double>* X_AC);
-void ComposeXinvXPortable(const RigidTransform<double>& X_BA,
-                          const RigidTransform<double>& X_BC,
-                          RigidTransform<double>* X_AC);
 
 }  // namespace internal
 }  // namespace math
--- math/test/fast_pose_composition_functions_test.cc
+++ math/test/fast_pose_composition_functions_test.cc
@@ -1,215 +1,177 @@
 #include "drake/math/fast_pose_composition_functions.h"
 
-#include <limits>
+#include <tuple>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#include "hwy/tests/hwy_gtest.h"
+#pragma GCC diagnostic pop
 
 #include <Eigen/Dense>
 #include <gtest/gtest.h>
 
+#include "drake/common/hwy_dynamic.h"
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
-#include "drake/math/fast_pose_composition_functions_avx2_fma.h"
+#include "drake/math/rigid_transform.h"
+#include "drake/math/rotation_matrix.h"
 
 namespace drake {
 namespace math {
+namespace internal {
 namespace {
 using Eigen::Matrix3d;
 using Eigen::Matrix4d;
 using Matrix34d = Eigen::Matrix<double, 3, 4>;
 
-#if defined(__APPLE__)
-constexpr bool kApple = true;
-#else
-constexpr bool kApple = false;
-#endif
+// Note that A's and B's rotation matrices are not legitimate. We are just
+// testing that the correct matrix operations are performed, and using integer
+// values is a lot easier for exact testing and debugging than floating point.
+RigidTransformd MakeA() {
+  Matrix34d value;
+  // clang-format off
+  value << 1, 4, 7, 10,
+           2, 5, 8, 11,
+           3, 6, 9, 12;
+  // clang-format on
+  return RigidTransformd::MakeUnchecked(value);
+}
 
-// Checks that our logic for detecting AVX at runtime works as expected.
-GTEST_TEST(TestFastPoseCompositionFunctions, AvxDetection) {
-  if (!kApple) {
-    // On Drake's Ubuntu CI we should always have AVX available (Drake CI only
-    // covers x64_64 builds, no arm64 yet). If we ever do add arm64 in CI, then
-    // we'll need to amend this condition.
-    EXPECT_EQ(internal::AvxSupported(), true);
+RigidTransformd MakeAinv() {
+  Matrix34d A = MakeA().GetAsMatrix34();
+  Matrix3d Rt = A.leftCols(3).transpose();
+  Matrix34d value;
+  value.block<3, 3>(0, 0) = Rt;
+  value.block<3, 1>(0, 3) = -Rt * A.col(3);
+  return RigidTransformd::MakeUnchecked(value);
+}
+
+RigidTransformd MakeB() {
+  Matrix34d value;
+  // clang-format off
+  value << 13, 16, 19, 22,
+           14, 17, 20, 23,
+           15, 18, 21, 24;
+  // clang-format on
+  return RigidTransformd::MakeUnchecked(value);
+}
+
+struct Param {
+  /* The 'args' string encodes a particular argument order to some particular
+  function under test (e.g., ComposeRR). For example if 'args' is "ABO" then the
+  test will call `ComposeRR(A, B, O)` -- each test case has local variables A,
+  B, and O. */
+  std::string args;
+
+  /* When this is true, the test checks ComposeRinvR instead of ComposeRR, or
+  ComposeXinvX instead of ComposeXX. */
+  bool inverse{};
+
+  /* A printer so that test names are helpful. */
+  friend std::ostream& operator<<(std::ostream& out, const Param& param) {
+    out << param.args;
+    if (param.inverse) {
+      out << "inv";
+    }
+    return out;
   }
+};
+
+/* This hwy-infused test fixture replicates every test case to be run against
+every target architecture variant (e.g., SSE4, AVX2, AVX512VL, etc). When run,
+it filters the suite to only run tests that the current CPU can handle. */
+class FastPoseCompositionFunctions
+    : public hwy::TestWithParamTargetAndT<Param> {
+ protected:
+  void SetUp() override {
+    // Reset Drake's dispatcher, to be sure that we run all of the target
+    // architectures.
+    drake::internal::HwyDynamicReset();
+    hwy::TestWithParamTargetAndT<Param>::SetUp();
+  }
+};
+
+/* All possible permutations of argument aliasing. */
+auto MakePermutations() {
+  return ::testing::Values(Param{.args = "ABO", .inverse = false},
+                           Param{.args = "ABA", .inverse = false},
+                           Param{.args = "ABB", .inverse = false},
+                           Param{.args = "AAA", .inverse = false},
+                           Param{.args = "ABO", .inverse = true},
+                           Param{.args = "ABA", .inverse = true},
+                           Param{.args = "ABB", .inverse = true},
+                           Param{.args = "AAA", .inverse = true});
 }
 
-// The "using portable" and "has avx" should be opposites so long as AVX is the
-// only accelerated option.
-GTEST_TEST(TestFastPoseCompositionFunctions, UsingPortable) {
-  EXPECT_NE(internal::IsUsingPortableCompositionFunctions(),
-            internal::AvxSupported());
+HWY_TARGET_INSTANTIATE_TEST_SUITE_P_T(FastPoseCompositionFunctions,
+                                      MakePermutations());
+
+TEST_P(FastPoseCompositionFunctions, ComposeRR) {
+  const Param& param = GetParam();
+
+  // We have three options for input / output objects.
+  RotationMatrixd A = MakeA().rotation();
+  RotationMatrixd B = MakeB().rotation();
+  RotationMatrixd O;
+
+  // Choose which arguments we want to pass in this instance.
+  auto lookup = [&A, &B, &O](char abo) -> RotationMatrixd* {
+    if (abo == 'A') return &A;
+    if (abo == 'B') return &B;
+    if (abo == 'O') return &O;
+    DRAKE_UNREACHABLE();
+  };
+  const RotationMatrixd* const arg1 = lookup(param.args.at(0));
+  const RotationMatrixd* const arg2 = lookup(param.args.at(1));
+  RotationMatrixd* const arg3 = lookup(param.args.at(2));
+
+  // We must prepare the answer prior to calling the function. Some calls will
+  // alias their input and output.
+  DRAKE_DEMAND(param.args.at(0) == 'A');
+  const Matrix3d expected =
+      (param.inverse ? MakeAinv() : MakeA()).rotation().matrix() *
+      arg2->matrix();
+
+  // Call the function under test.
+  (param.inverse ? ComposeRinvR : ComposeRR)(*arg1, *arg2, arg3);
+
+  // Check the answer.
+  EXPECT_TRUE(CompareMatrices(arg3->matrix(), expected));
 }
 
-// Test the given RotationMatrix composition function for correct functionality
-// (just matrix multiply or matrix transpose multiply here) and that it still
-// works when the output overlaps in memory with the inputs.
-//
-// To avoid circular dependencies we have to test this functionality without
-// access to the declaration of RotationMatrix (we have only a forward
-// reference). We are guaranteed, however, that a RotationMatrix is just an
-// array of nine consecutive doubles (by a static_assert in rotation_matrix.h)
-// and in column order (by Eigen's default storage order for matrices).
-void RawTestRxR(
-    std::function<void(const double*, const double*, double*)> compose_RxR,
-    bool invert_first_matrix) {
-  // Note that M and N are not legitimate RotationMatrix values. We are just
-  // testing that the correct matrix operations are performed.
-  Matrix3d M, N;
-  // clang-format off
-  M << 1, 5, 9,
-       2, 6, 10,
-       3, 7, 11;
-  N << 13, 17, 21,
-       14, 18, 22,
-       15, 19, 23;
-  // clang-format on
-  // Inverse is just transpose for a RotationMatrix.
-  const Matrix3d Mx = invert_first_matrix ? M.transpose() : M;
-  const Matrix3d MxM_expected = Mx * M;
-  const Matrix3d MxN_expected = Mx * N;
+TEST_P(FastPoseCompositionFunctions, ComposeXX) {
+  const Param& param = GetParam();
 
-  Matrix3d MxN;
-  compose_RxR(M.data(), N.data(), MxN.data());
+  // We have three options for input / output objects.
+  RigidTransformd A = MakeA();
+  RigidTransformd B = MakeB();
+  RigidTransformd O;
 
-  // Should be a perfect match with integer elements.
-  EXPECT_TRUE(CompareMatrices(MxN, MxN_expected, 0));
+  // Choose which arguments we want to pass in this instance.
+  auto lookup = [&A, &B, &O](char abo) -> RigidTransformd* {
+    if (abo == 'A') return &A;
+    if (abo == 'B') return &B;
+    if (abo == 'O') return &O;
+    DRAKE_UNREACHABLE();
+  };
+  const RigidTransformd* const arg1 = lookup(param.args.at(0));
+  const RigidTransformd* const arg2 = lookup(param.args.at(1));
+  RigidTransformd* const arg3 = lookup(param.args.at(2));
 
-  // Now test in-place compositions.
-  Matrix3d Mwork = M, Nwork = N;  // Copies to overwrite.
+  // We must prepare the answer prior to calling the function. Some calls will
+  // alias their input and output.
+  DRAKE_DEMAND(param.args.at(0) == 'A');
+  const Matrix4d expected =
+      (param.inverse ? MakeAinv() : MakeA()).GetAsMatrix4() *
+      arg2->GetAsMatrix4();
 
-  // Results should be perfect match with integer elements.
-  compose_RxR(Mwork.data(), Nwork.data(), Mwork.data());  // Mwork=Mx*N
-  EXPECT_TRUE(CompareMatrices(Mwork, MxN_expected, 0));
-  Mwork = M;                                              // Restore value.
-  compose_RxR(Mwork.data(), Nwork.data(), Nwork.data());  // Nwork=Mx*N
-  EXPECT_TRUE(CompareMatrices(Nwork, MxN_expected, 0));
-  compose_RxR(Mwork.data(), Mwork.data(), Mwork.data());  // Mwork=Mx*M
-  EXPECT_TRUE(CompareMatrices(Mwork, MxM_expected, 0));
-}
+  // Call the function under test.
+  (param.inverse ? ComposeXinvX : ComposeXX)(*arg1, *arg2, arg3);
 
-// Wraps the given composition function in one that can work directly with
-// arrays of doubles for testing.
-void TestRxR(std::function<void(const RotationMatrix<double>& R1,
-                                const RotationMatrix<double>& R2,
-                                RotationMatrix<double>* ROut)>
-                 compose_RxR,
-             bool invert_first_matrix) {
-  RawTestRxR(
-      [&](const double* R_1, const double* R_2, double* R_out) {
-        compose_RxR(reinterpret_cast<const RotationMatrix<double>&>(*R_1),
-                    reinterpret_cast<const RotationMatrix<double>&>(*R_2),
-                    reinterpret_cast<RotationMatrix<double>*>(R_out));
-      },
-      invert_first_matrix);
-}
-
-// Test the given RigidTransform composition function for correct functionality
-// and that it still works when the output overlaps in memory with the inputs.
-void RawTestXxX(
-    std::function<void(const double*, const double*, double*)> compose_XxX,
-    bool invert_first_matrix) {
-  // Note that M and N are not legitimate RigidTransform values. We are just
-  // testing that the correct matrix operations are performed.
-  Matrix34d M, N;
-  // clang-format off
-  M << 1, 4, 7, 10,
-       2, 5, 8, 11,
-       3, 6, 9, 12;
-  N << 13, 16, 19, 22,
-       14, 17, 20, 23,
-       15, 18, 21, 24;
-  // clang-format on
-
-  // Append 0001 row so we can use straight matrix operations to get the
-  // result.
-  Matrix4d M0, N0;
-  M0.topRows(3) = M;
-  M0.row(3) << 0, 0, 0, 1;
-  N0.topRows(3) = N;
-  N0.row(3) << 0, 0, 0, 1;
-
-  const Matrix3d Rt = M.leftCols(3).transpose();  // RotationMatrix inverse
-  Matrix4d M0inv;
-  M0inv.block<3, 3>(0, 0) = Rt;
-  M0inv.block<3, 1>(0, 3) = -Rt * M.col(3);
-  M0inv.row(3) << 0, 0, 0, 1;
-
-  const Matrix4d M0x = invert_first_matrix ? M0inv : M0;
-  const Matrix34d MxN_expected = (M0x * N0).topRows(3);
-  const Matrix34d MxM_expected = (M0x * M0).topRows(3);
-
-  Matrix34d MxN;
-  compose_XxX(M.data(), N.data(), MxN.data());
-
-  // Should be a perfect match with integer elements.
-  EXPECT_TRUE(CompareMatrices(MxN, MxN_expected, 0));
-
-  // Now test in-place compositions.
-  Matrix34d Mwork = M, Nwork = N;
-  compose_XxX(Mwork.data(), Nwork.data(), Mwork.data());  // Mwork=Mx*N
-  EXPECT_TRUE(CompareMatrices(Mwork, MxN_expected, 0));
-  Mwork = M;                                              // Restore value.
-  compose_XxX(Mwork.data(), Nwork.data(), Nwork.data());  // Nwork=Mx*N
-  EXPECT_TRUE(CompareMatrices(Nwork, MxN_expected, 0));
-  compose_XxX(Mwork.data(), Mwork.data(), Mwork.data());  // Mwork=Mx*M
-  EXPECT_TRUE(CompareMatrices(Mwork, MxM_expected, 0));
-}
-
-// Wraps the given composition function in one that can work directly with
-// arrays of doubles for testing.
-void TestXxX(std::function<void(const RigidTransform<double>& X1,
-                                const RigidTransform<double>& X2,
-                                RigidTransform<double>* XOut)>
-                 compose_XxX,
-             bool invert_first_matrix) {
-  RawTestXxX(
-      [&](const double* X_1, const double* X_2, double* X_out) {
-        compose_XxX(reinterpret_cast<const RigidTransform<double>&>(*X_1),
-                    reinterpret_cast<const RigidTransform<double>&>(*X_2),
-                    reinterpret_cast<RigidTransform<double>*>(X_out));
-      },
-      invert_first_matrix);
-}
-
-/* Test the user-callable methods first. Those are ideally implemented
-with SIMD instructions, however they may just punt to the plain C++ fallback
-methods, which we'll test separately below. */
-
-GTEST_TEST(TestFastPoseCompositionFunctions, TestRR) {
-  SCOPED_TRACE("testing ComposeRR()");
-  TestRxR(internal::ComposeRR, false);
-}
-GTEST_TEST(TestFastPoseCompositionFunctions, TestRinvR) {
-  SCOPED_TRACE("testing ComposeRinvR()");
-  TestRxR(internal::ComposeRinvR, true);
-}
-GTEST_TEST(TestFastPoseCompositionFunctions, TestXX) {
-  SCOPED_TRACE("testing ComposeXX()");
-  TestXxX(internal::ComposeXX, false);
-}
-GTEST_TEST(TestFastPoseCompositionFunctions, TestXinvX) {
-  SCOPED_TRACE("testing ComposeXinvX()");
-  TestXxX(internal::ComposeXinvX, true);
-}
-
-/* Now repeat these tests for the portable methods. */
-
-GTEST_TEST(TestFastPoseCompositionFunctions, TestRRPortable) {
-  SCOPED_TRACE("testing internal::ComposeRRPortable()");
-  TestRxR(internal::ComposeRRPortable, false);
-}
-GTEST_TEST(TestFastPoseCompositionFunctions, TestRinvRPortable) {
-  SCOPED_TRACE("testing internal::ComposeRinvRPortable()");
-  TestRxR(internal::ComposeRinvRPortable, true);
-}
-GTEST_TEST(TestFastPoseCompositionFunctions, TestXXPortable) {
-  SCOPED_TRACE("testing internal::ComposeXXPortable()");
-  TestXxX(internal::ComposeXXPortable, false);
-}
-GTEST_TEST(TestFastPoseCompositionFunctions, TestXinvXPortable) {
-  SCOPED_TRACE("testing internal::ComposeXinvXPortable()");
-  TestXxX(internal::ComposeXinvXPortable, true);
+  // Check the answer.
+  EXPECT_TRUE(CompareMatrices(arg3->GetAsMatrix4(), expected));
 }
 
 }  // namespace
-
+}  // namespace internal
 }  // namespace math
 }  // namespace drake
