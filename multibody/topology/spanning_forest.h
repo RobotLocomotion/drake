2be012383277d86bb44c6d46579f5024b1485c0d
Michael Sherman <sherm1@gmail.com>
2024-07-08 15:12:06 -0700

    Supports RemoveJoint
    Distinguish index from ordinal for links and joints
    
    Tracks quaternion location

--- multibody/topology/spanning_forest.h
+++ multibody/topology/spanning_forest.h
@@ -21,6 +21,12 @@ namespace multibody {
 // TODO(sherm1) Promote from internal once API has stabilized: issue #11307.
 namespace internal {
 
+// TODO(sherm1) During the PR train leading up to MbP using this code in Drake
+//  master, I'm using Doxygen comments /** despite the fact that this is
+//  currently just internal. That allows me to validate Doxygen syntax in
+//  anticipation of the API becoming public later. Change these to /* in the
+//  final PR in this train to satisfy the styleguide.
+
 using WeldedMobodsIndex = TypeSafeIndex<class WeldedMobodsTag>;
 
 // TODO(sherm1) The following describes the aspirational SpanningForest but
@@ -168,24 +174,36 @@ class SpanningForest {
   explaining why. Otherwise returns the empty string. */
   const std::string& why_no_dynamics() const { return data_.why_no_dynamics; }
 
-  /** Provides convenient access to the owning graph's links. */
+  /** Provides convenient access to the owning graph's links, contiguous
+  and accessed by LinkOrdinal. */
   const std::vector<Link>& links() const { return graph().links(); }
 
   /** Provides convenient access to one of the owning graph's links. Requires
-  a BodyIndex, not a plain integer.
-  @pre link_index is in range */
-  const Link& links(BodyIndex link_index) const {
-    return graph().links(link_index);
+  a LinkOrdinal, not a plain integer.
+  @pre link_ordinal is in range */
+  const Link& links(LinkOrdinal link_ordinal) const {
+    return graph().links(link_ordinal);
   }
 
-  /** Provides convenient access to the owning graph's joints. */
+  // TODO(sherm1) Make this unchecked; maybe private?
+  const Link& link_by_index(BodyIndex link_index) const {
+    return graph().link_by_index(link_index);
+  }
+
+  /** Provides convenient access to the owning graph's joints, contiguous
+  and accessed by JointOrdinal. */
   const std::vector<Joint>& joints() const { return graph().joints(); }
 
   /** Provides convenient access to one of the owning graph's joints. Requires
-  a JointIndex, not a plain integer.
-  @pre joint_index is in range */
-  const Joint& joints(JointIndex joint_index) const {
-    return graph().joints(joint_index);
+  a JointOrdinal, not a plain integer.
+  @pre joint_ordinal is in range */
+  const Joint& joints(JointOrdinal joint_ordinal) const {
+    return graph().joints(joint_ordinal);
+  }
+
+  // TODO(sherm1) Make this unchecked; maybe private?
+  const Joint& joint_by_index(JointIndex joint_index) const {
+    return graph().joint_by_index(joint_index);
   }
 
   /** All the mobilized bodies, in depth-first order. World comes first,
@@ -279,13 +297,13 @@ class SpanningForest {
   "active" Link, that is, the one whose mobilizer is used to move the whole
   Composite. Cost is O(1) and very fast.
   @pre mobod_index is in range */
-  inline BodyIndex mobod_to_link(MobodIndex mobod_index) const;
+  inline LinkOrdinal mobod_to_link_ordinal(MobodIndex mobod_index) const;
 
   /** Returns all the Links mobilized by this Mobod. The "active" Link returned
   by mobod_to_link() comes first, then any other Links in the same Composite.
   O(1), very fast.
   @pre mobod_index is in range  */
-  inline const std::vector<BodyIndex>& mobod_to_links(
+  inline const std::vector<LinkOrdinal>& mobod_to_link_ordinals(
       MobodIndex mobod_index) const;
 
   /** Returns the total number of generalized position coordinates q used by
@@ -295,6 +313,13 @@ class SpanningForest {
   /** Returns the total number of generalized velocity coordinates v used by
   this model. O(1), very fast. */
   int num_velocities() const { return ssize(data_.v_to_mobod); }
+
+  /** Returns the indexes of all quaternions within the generalized position
+  coordinates q. Each quaternion begins at the given index with its scalar
+  element w, followed immediately by its vector part xyz. */
+  const std::vector<int>& quaternion_starts() const {
+    return data_.quaternion_starts;
+  }
 
   /** Returns the Mobod to which a given position coordinate q belongs.
   O(1), very fast.
@@ -417,8 +442,8 @@ class SpanningForest {
   //  - updates maps of link-to-mobod and joint-to-mobod
   //  - if joint type is Weld, we are creating or joining a WeldedMobods group
   //    and LinkComposite; if welded to World the Mobod is "anchored". */
-  const Mobod& AddNewMobod(BodyIndex outboard_link_index,
-                           JointIndex joint_index,
+  const Mobod& AddNewMobod(LinkOrdinal outboard_link_ordinal,
+                           JointOrdinal joint_ordinal,
                            MobodIndex inboard_mobod_index, bool is_reversed);
 
   // Given a list of Static or MustBeBaseBody Links, adds a weld or floating
@@ -437,14 +462,14 @@ class SpanningForest {
   //  - if both are massless we have an invalid forest
   //  - either or both Links may be composites; it is the mass properties
   //    of the whole composite that determines masslessness.
-  void HandleLoopClosure(JointIndex loop_joint_index);
+  void HandleLoopClosure(JointOrdinal loop_joint_ordinal);
 
   // Adds a shadow Link of the given primary and mobilizes the shadow with
   // the given joint which was originally connected to the primary. Adds a
   // weld constraint to reattach the shadow to the primary. The shadow and
   // weld are added to the graph as ephemeral elements.
-  const Mobod& AddShadowMobod(BodyIndex primary_link_index,
-                              JointIndex shadow_joint_index);
+  const Mobod& AddShadowMobod(LinkOrdinal primary_link_ordinal,
+                              JointOrdinal shadow_joint_ordinal);
 
   bool model_instance_is_static(ModelInstanceIndex index) const {
     return static_cast<bool>(options(index) & ForestBuildingOptions::kStatic);
@@ -473,8 +498,8 @@ class SpanningForest {
                : LinkJointGraph::quaternion_floating_joint_traits_index();
   }
 
-  bool link_is_already_in_forest(BodyIndex link_index) const {
-    return graph().link_to_mobod(link_index).is_valid();
+  bool link_is_already_in_forest(LinkOrdinal link_ordinal) const {
+    return links(link_ordinal).mobod_index().is_valid();
   }
 
   LinkJointGraph& mutable_graph() {
@@ -525,10 +550,14 @@ class SpanningForest {
     std::vector<MobodIndex> q_to_mobod;  // size is nq (total number of q's)
     std::vector<MobodIndex> v_to_mobod;  // size is nv (total number of v's)
 
+    // Indexes of quaternion starts within the q vector, in increasing order.
+    std::vector<int> quaternion_starts;
+
     // This policy is expressed as a "less than" comparator of the type used by
     // std::priority_queue. It should return true if the left argument is a
     // worse choice than the right argument, according to the policy.
-    std::function<bool(const BodyIndex&, const BodyIndex&)> base_body_policy;
+    std::function<bool(const LinkOrdinal&, const LinkOrdinal&)>
+        base_body_policy;
 
     // Set to false if we had to end a branch with a massless body.
     bool dynamics_ok{true};
