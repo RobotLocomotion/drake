b128c7da9e064f1db3c08b0397ad44ca3278b4ca
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-07-08 09:50:43 -0700

    [multibody] ContactResults no longer reports quadrature data

--- multibody/plant/multibody_plant.cc
+++ multibody/plant/multibody_plant.cc
@@ -2257,10 +2257,9 @@ void MultibodyPlant<T>::CalcHydroelasticContactForcesContinuous(
         geometryM_id, geometryN_id, inspector);
 
     // Integrate the hydroelastic traction field over the contact surface.
-    std::vector<HydroelasticQuadraturePointData<T>> traction_output;
     SpatialForce<T> F_Ac_W;
     traction_calculator.ComputeSpatialForcesAtCentroidFromHydroelasticModel(
-        data, dissipation, dynamic_friction, &traction_output, &F_Ac_W);
+        data, dissipation, dynamic_friction, &F_Ac_W);
 
     // Shift the traction at the centroid to tractions at the body origins.
     SpatialForce<T> F_Ao_W, F_Bo_W;
@@ -2276,7 +2275,7 @@ void MultibodyPlant<T>::CalcHydroelasticContactForcesContinuous(
     }
 
     // Add the information for contact reporting.
-    contact_info.emplace_back(&surface, F_Ac_W, std::move(traction_output));
+    contact_info.emplace_back(&surface, F_Ac_W);
   }
 }
 
--- multibody/plant/test/hydroelastic_contact_info_test.cc
+++ multibody/plant/test/hydroelastic_contact_info_test.cc
@@ -46,30 +46,15 @@ SpatialForce<double> MakeSpatialForce() {
                               Vector3<double>(4, 5, 6));
 }
 
-// Returns a distinct vector (containing a single element) of quadrature point
-// data.
-std::vector<HydroelasticQuadraturePointData<double>> GetQuadraturePointData() {
-  HydroelasticQuadraturePointData<double> data;
-  data.p_WQ = Vector3<double>(3.0, 5.0, 7.0);
-  data.face_index = 1;
-  data.vt_BqAq_W = Vector3<double>(11.0, 13.0, 17.0);
-  data.traction_Aq_W = Vector3<double>(19.0, 23.0, 29.0);
-  return {data};
-}
-
 HydroelasticContactInfo<double> CreateContactInfo(
     std::unique_ptr<ContactSurface<double>>* contact_surface,
     std::unique_ptr<HydroelasticContactInfo<double>>* contact_info) {
   // Create an arbitrary contact surface.
   *contact_surface = MakeContactSurface();
 
-  // Create the HydroelasticContactInfo using particular spatial force and
-  // quadrature point data.
-  std::vector<HydroelasticQuadraturePointData<double>> quadrature_point_data =
-      GetQuadraturePointData();
+  // Create the HydroelasticContactInfo using particular spatial force.
   return HydroelasticContactInfo<double>(contact_surface->get(),
-                                         MakeSpatialForce(),
-                                         std::move(quadrature_point_data));
+                                         MakeSpatialForce());
 }
 
 // Verifies that the HydroelasticContactInfo structure uses the raw pointer
@@ -90,9 +75,6 @@ GTEST_TEST(HydroelasticContactInfo, CopyConstruction) {
   // Verify that the spatial force was copied.
   EXPECT_EQ(copy.F_Ac_W().translational(), MakeSpatialForce().translational());
   EXPECT_EQ(copy.F_Ac_W().rotational(), MakeSpatialForce().rotational());
-
-  // Verify that the quadrature point data was copied.
-  EXPECT_EQ(copy.quadrature_point_data(), GetQuadraturePointData());
 }
 
 // Verifies that the HydroelasticContactInfo structure transfers ownership of
@@ -111,9 +93,6 @@ GTEST_TEST(HydroelasticContactInfo, MoveConstruction) {
   EXPECT_EQ(moved_copy.F_Ac_W().translational(),
             MakeSpatialForce().translational());
   EXPECT_EQ(moved_copy.F_Ac_W().rotational(), MakeSpatialForce().rotational());
-
-  // Verify that the quadrature point data was copied.
-  EXPECT_EQ(moved_copy.quadrature_point_data(), GetQuadraturePointData());
 }
 
 }  // namespace
