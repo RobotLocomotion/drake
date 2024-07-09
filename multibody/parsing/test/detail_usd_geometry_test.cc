22435453cb0bfc24b8ccf8c0c4a383cbc3fcbf73
Hong Wang <hongw@nvidia.com>
2024-07-08 18:21:39 -0700

    Remove unnecessary ASSERTs and fix dynamic_cast

--- multibody/parsing/test/detail_usd_geometry_test.cc
+++ multibody/parsing/test/detail_usd_geometry_test.cc
@@ -48,36 +48,33 @@ TEST_F(UsdGeometryTest, BoxParsingTest) {
 
   const double size = 49.0;
   pxr::UsdAttribute size_attribute = box.CreateSizeAttr();
-  EXPECT_TRUE(size_attribute.Set(size));
+  size_attribute.Set(size);
 
   const pxr::GfVec3d scale_factor = pxr::GfVec3d(0.4, 0.5, 0.6);
   pxr::UsdGeomXformOp scale_op = box.AddScaleOp(
     pxr::UsdGeomXformOp::PrecisionDouble);
-  EXPECT_TRUE(scale_op.Set(scale_factor));
-
-  std::optional<Eigen::Vector3d> parsed_dimension = internal::GetBoxDimension(
-    box.GetPrim(), meters_per_unit_, diagnostic_policy_);
-  EXPECT_TRUE(parsed_dimension.has_value());
+  scale_op.Set(scale_factor);
 
   const Eigen::Vector3d correct_dimension =
     internal::UsdVec3dToEigen(scale_factor) * size * meters_per_unit_;
+  std::optional<Eigen::Vector3d> parsed_dimension = internal::GetBoxDimension(
+    box.GetPrim(), meters_per_unit_, diagnostic_policy_);
   EXPECT_EQ(parsed_dimension.value(), correct_dimension);
 
   std::unique_ptr<geometry::Shape> shape = internal::CreateGeometryBox(
     box.GetPrim(), meters_per_unit_, diagnostic_policy_);
-  EXPECT_TRUE(shape != nullptr);
-  geometry::Box* shape_box = dynamic_cast<geometry::Box*>(shape.get());
-  EXPECT_EQ(shape_box->size(), correct_dimension);
+  ASSERT_TRUE(shape != nullptr);
+  geometry::Box& shape_box = dynamic_cast<geometry::Box&>(*shape);
+  EXPECT_EQ(shape_box.size(), correct_dimension);
 
   const float mass = 2.71;
   pxr::UsdPhysicsMassAPI mass_api = pxr::UsdPhysicsMassAPI::Apply(
     box.GetPrim());
   pxr::UsdAttribute mass_attribute = mass_api.CreateMassAttr();
-  EXPECT_TRUE(mass_attribute.Set(mass));
+  mass_attribute.Set(mass);
   std::optional<SpatialInertia<double>> inertia =
     internal::CreateSpatialInertiaForBox(box.GetPrim(), meters_per_unit_,
       diagnostic_policy_);
-  EXPECT_TRUE(inertia.has_value());
   EXPECT_EQ(mass, static_cast<float>(inertia.value().get_mass()));
 
   // Case: Input Prim is not an UsdGeomCube.
@@ -90,7 +87,7 @@ TEST_F(UsdGeometryTest, BoxParsingTest) {
     ".*Failed to cast the Prim at .* into an UsdGeomCube.*"));
 
   // Case: Input Prim is missing the size attribute.
-  EXPECT_TRUE(box.GetPrim().RemoveProperty(size_attribute.GetName()));
+  box.GetPrim().RemoveProperty(size_attribute.GetName());
   parsed_dimension = internal::GetBoxDimension(box.GetPrim(), meters_per_unit_,
     diagnostic_policy_);
   EXPECT_FALSE(parsed_dimension.has_value());
@@ -105,40 +102,37 @@ TEST_F(UsdGeometryTest, EllipsoidParsingTest) {
 
   const double radius = 17.0;
   pxr::UsdAttribute radius_attribute = ellipsoid.CreateRadiusAttr();
-  EXPECT_TRUE(radius_attribute.Set(radius));
+  radius_attribute.Set(radius);
 
   const pxr::GfVec3d scale_factor = pxr::GfVec3d(0.6, 1.1, 2.9);
   pxr::UsdGeomXformOp scale_op = ellipsoid.AddScaleOp(
     pxr::UsdGeomXformOp::PrecisionDouble);
-  EXPECT_TRUE(scale_op.Set(scale_factor));
-
-  std::optional<Eigen::Vector3d> parsed_dimension =
-    internal::GetEllipsoidDimension(ellipsoid.GetPrim(), meters_per_unit_,
-      diagnostic_policy_);
-  EXPECT_TRUE(parsed_dimension.has_value());
+  scale_op.Set(scale_factor);
 
   const Eigen::Vector3d correct_dimension =
     internal::UsdVec3dToEigen(scale_factor) * radius * meters_per_unit_;
+  std::optional<Eigen::Vector3d> parsed_dimension =
+    internal::GetEllipsoidDimension(ellipsoid.GetPrim(), meters_per_unit_,
+      diagnostic_policy_);
   EXPECT_EQ(parsed_dimension.value(), correct_dimension);
 
   std::unique_ptr<geometry::Shape> shape = internal::CreateGeometryEllipsoid(
     ellipsoid.GetPrim(), meters_per_unit_, diagnostic_policy_);
-  EXPECT_TRUE(shape != nullptr);
-  geometry::Ellipsoid* shape_ellipsoid =
-    dynamic_cast<geometry::Ellipsoid*>(shape.get());
+  ASSERT_TRUE(shape != nullptr);
+  geometry::Ellipsoid& shape_ellipsoid =
+    dynamic_cast<geometry::Ellipsoid&>(*shape);
   const Eigen::Vector3d shape_dimension = Eigen::Vector3d(
-    shape_ellipsoid->a(), shape_ellipsoid->b(), shape_ellipsoid->c());
+    shape_ellipsoid.a(), shape_ellipsoid.b(), shape_ellipsoid.c());
   EXPECT_EQ(shape_dimension, correct_dimension);
 
   const float mass = 77.241;
   pxr::UsdPhysicsMassAPI mass_api = pxr::UsdPhysicsMassAPI::Apply(
     ellipsoid.GetPrim());
   pxr::UsdAttribute mass_attribute = mass_api.CreateMassAttr();
-  EXPECT_TRUE(mass_attribute.Set(mass));
+  mass_attribute.Set(mass);
   std::optional<SpatialInertia<double>> inertia =
     internal::CreateSpatialInertiaForEllipsoid(ellipsoid.GetPrim(),
       meters_per_unit_, diagnostic_policy_);
-  EXPECT_TRUE(inertia.has_value());
   EXPECT_EQ(mass, static_cast<float>(inertia.value().get_mass()));
 
   // Case: Input Prim is not an UsdGeomSphere.
@@ -151,7 +145,7 @@ TEST_F(UsdGeometryTest, EllipsoidParsingTest) {
     ".*Failed to cast the Prim at .* into an UsdGeomSphere.*"));
 
   // Case: Input Prim is missing the radius attribute.
-  EXPECT_TRUE(ellipsoid.GetPrim().RemoveProperty(radius_attribute.GetName()));
+  ellipsoid.GetPrim().RemoveProperty(radius_attribute.GetName());
   parsed_dimension = internal::GetEllipsoidDimension(ellipsoid.GetPrim(),
     meters_per_unit_, diagnostic_policy_);
   EXPECT_FALSE(parsed_dimension.has_value());
@@ -170,44 +164,41 @@ TEST_F(UsdGeometryTest, CylinderParsingTest) {
   pxr::UsdAttribute radius_attribute = cylinder.CreateRadiusAttr();
   pxr::UsdAttribute height_attribute = cylinder.CreateHeightAttr();
   pxr::UsdAttribute axis_attribute = cylinder.CreateAxisAttr();
-  EXPECT_TRUE(radius_attribute.Set(radius));
-  EXPECT_TRUE(height_attribute.Set(height));
-  EXPECT_TRUE(axis_attribute.Set(axis));
+  radius_attribute.Set(radius);
+  height_attribute.Set(height);
+  axis_attribute.Set(axis);
 
   const pxr::GfVec3d scale_factor = pxr::GfVec3d(0.7, 0.7, 0.9);
   pxr::UsdGeomXformOp scale_op = cylinder.AddScaleOp(
     pxr::UsdGeomXformOp::PrecisionDouble);
-  EXPECT_TRUE(scale_op.Set(scale_factor));
-
-  std::optional<Eigen::Vector2d> parsed_dimension =
-    internal::GetCylinderDimension(cylinder.GetPrim(), meters_per_unit_,
-      stage_up_axis_, diagnostic_policy_);
-  EXPECT_TRUE(parsed_dimension.has_value());
+  scale_op.Set(scale_factor);
 
   const double correct_radius = scale_factor[0] * radius * meters_per_unit_;
   const double correct_height = scale_factor[2] * height * meters_per_unit_;
   const Eigen::Vector2d correct_dimension = Eigen::Vector2d(correct_radius,
     correct_height);
+  std::optional<Eigen::Vector2d> parsed_dimension =
+    internal::GetCylinderDimension(cylinder.GetPrim(), meters_per_unit_,
+      stage_up_axis_, diagnostic_policy_);
   EXPECT_EQ(parsed_dimension.value(), correct_dimension);
 
   std::unique_ptr<geometry::Shape> shape = internal::CreateGeometryCylinder(
     cylinder.GetPrim(), meters_per_unit_, stage_up_axis_, diagnostic_policy_);
-  EXPECT_TRUE(shape != nullptr);
-  geometry::Cylinder* shape_cylinder =
-    dynamic_cast<geometry::Cylinder*>(shape.get());
+  ASSERT_TRUE(shape != nullptr);
+  geometry::Cylinder& shape_cylinder =
+    dynamic_cast<geometry::Cylinder&>(*shape);
   Eigen::Vector2d shape_dimension = Eigen::Vector2d(
-    shape_cylinder->radius(), shape_cylinder->length());
+    shape_cylinder.radius(), shape_cylinder.length());
   EXPECT_EQ(shape_dimension, correct_dimension);
 
   const float mass = 152.0;
   pxr::UsdPhysicsMassAPI mass_api = pxr::UsdPhysicsMassAPI::Apply(
     cylinder.GetPrim());
   pxr::UsdAttribute mass_attribute = mass_api.CreateMassAttr();
-  EXPECT_TRUE(mass_attribute.Set(mass));
+  mass_attribute.Set(mass);
   std::optional<SpatialInertia<double>> inertia =
     internal::CreateSpatialInertiaForCylinder(cylinder.GetPrim(),
       meters_per_unit_, stage_up_axis_, diagnostic_policy_);
-  EXPECT_TRUE(inertia.has_value());
   EXPECT_EQ(mass, static_cast<float>(inertia.value().get_mass()));
 
   // Case: Input Prim is not an UsdGeomCylinder.
@@ -220,7 +211,7 @@ TEST_F(UsdGeometryTest, CylinderParsingTest) {
     ".*Failed to cast the Prim at .* into an UsdGeomCylinder.*"));
 
   // Case: Input Prim is missing the height attribute.
-  EXPECT_TRUE(cylinder.GetPrim().RemoveProperty(height_attribute.GetName()));
+  cylinder.GetPrim().RemoveProperty(height_attribute.GetName());
   parsed_dimension = internal::GetCylinderDimension(cylinder.GetPrim(),
     meters_per_unit_, stage_up_axis_, diagnostic_policy_);
   EXPECT_FALSE(parsed_dimension.has_value());
@@ -228,7 +219,7 @@ TEST_F(UsdGeometryTest, CylinderParsingTest) {
     ".*Failed to read the 'height' attribute of the Prim at.*"));
 
   // Case: Input Prim is missing the radius attribute.
-  EXPECT_TRUE(cylinder.GetPrim().RemoveProperty(radius_attribute.GetName()));
+  cylinder.GetPrim().RemoveProperty(radius_attribute.GetName());
   parsed_dimension = internal::GetCylinderDimension(cylinder.GetPrim(),
     meters_per_unit_, stage_up_axis_, diagnostic_policy_);
   EXPECT_FALSE(parsed_dimension.has_value());
@@ -254,7 +245,7 @@ TEST_F(UsdGeometryTest, CylinderParsingTest) {
     ".*The axis of the geometry at .* is invalid.*"));
 
   // Case: The axis attribute of the cylinder does not exist.
-  EXPECT_TRUE(cylinder.GetPrim().RemoveProperty(axis_attribute.GetName()));
+  cylinder.GetPrim().RemoveProperty(axis_attribute.GetName());
   parsed_dimension = internal::GetCylinderDimension(cylinder.GetPrim(),
     meters_per_unit_, stage_up_axis_, diagnostic_policy_);
   EXPECT_FALSE(parsed_dimension.has_value());
@@ -273,44 +264,40 @@ TEST_F(UsdGeometryTest, CapsuleParsingTest) {
   pxr::UsdAttribute radius_attribute = capsule.CreateRadiusAttr();
   pxr::UsdAttribute height_attribute = capsule.CreateHeightAttr();
   pxr::UsdAttribute axis_attribute = capsule.CreateAxisAttr();
-  EXPECT_TRUE(radius_attribute.Set(radius));
-  EXPECT_TRUE(height_attribute.Set(height));
-  EXPECT_TRUE(axis_attribute.Set(axis));
+  radius_attribute.Set(radius);
+  height_attribute.Set(height);
+  axis_attribute.Set(axis);
 
   const pxr::GfVec3d scale_factor = pxr::GfVec3d(0.7, 0.7, 0.9);
   pxr::UsdGeomXformOp scale_op = capsule.AddScaleOp(
     pxr::UsdGeomXformOp::PrecisionDouble);
-  EXPECT_TRUE(scale_op.Set(scale_factor));
-
-  std::optional<Eigen::Vector2d> parsed_dimension =
-    internal::GetCapsuleDimension(capsule.GetPrim(), meters_per_unit_,
-      stage_up_axis_, diagnostic_policy_);
-  EXPECT_TRUE(parsed_dimension.has_value());
+  scale_op.Set(scale_factor);
 
   const double correct_radius = scale_factor[0] * radius * meters_per_unit_;
   const double correct_height = scale_factor[2] * height * meters_per_unit_;
   const Eigen::Vector2d correct_dimension = Eigen::Vector2d(correct_radius,
     correct_height);
+  std::optional<Eigen::Vector2d> parsed_dimension =
+    internal::GetCapsuleDimension(capsule.GetPrim(), meters_per_unit_,
+      stage_up_axis_, diagnostic_policy_);
   EXPECT_EQ(parsed_dimension.value(), correct_dimension);
 
   std::unique_ptr<geometry::Shape> shape = internal::CreateGeometryCapsule(
     capsule.GetPrim(), meters_per_unit_, stage_up_axis_, diagnostic_policy_);
-  EXPECT_TRUE(shape != nullptr);
-  geometry::Capsule* shape_capsule =
-    dynamic_cast<geometry::Capsule*>(shape.get());
+  ASSERT_TRUE(shape != nullptr);
+  geometry::Capsule& shape_capsule = dynamic_cast<geometry::Capsule&>(*shape);
   const Eigen::Vector2d shape_dimension = Eigen::Vector2d(
-    shape_capsule->radius(), shape_capsule->length());
+    shape_capsule.radius(), shape_capsule.length());
   EXPECT_EQ(shape_dimension, correct_dimension);
 
   const float mass = 152.0;
   pxr::UsdPhysicsMassAPI mass_api = pxr::UsdPhysicsMassAPI::Apply(
     capsule.GetPrim());
   pxr::UsdAttribute mass_attribute = mass_api.CreateMassAttr();
-  EXPECT_TRUE(mass_attribute.Set(mass));
+  mass_attribute.Set(mass);
   std::optional<SpatialInertia<double>> inertia =
     internal::CreateSpatialInertiaForCapsule(capsule.GetPrim(),
       meters_per_unit_, stage_up_axis_, diagnostic_policy_);
-  EXPECT_TRUE(inertia.has_value());
   EXPECT_EQ(mass, static_cast<float>(inertia.value().get_mass()));
 
   // Case: Input Prim is not an UsdGeomCapsule.
@@ -323,7 +310,7 @@ TEST_F(UsdGeometryTest, CapsuleParsingTest) {
     ".*Failed to cast the Prim at .* into an UsdGeomCapsule.*"));
 
   // Case: Input Prim is missing the height attribute.
-  EXPECT_TRUE(capsule.GetPrim().RemoveProperty(height_attribute.GetName()));
+  capsule.GetPrim().RemoveProperty(height_attribute.GetName());
   parsed_dimension = internal::GetCapsuleDimension(capsule.GetPrim(),
     meters_per_unit_, stage_up_axis_, diagnostic_policy_);
   EXPECT_FALSE(parsed_dimension.has_value());
@@ -331,7 +318,7 @@ TEST_F(UsdGeometryTest, CapsuleParsingTest) {
     ".*Failed to read the 'height' attribute of the Prim at.*"));
 
   // Case: Input Prim is missing the radius attribute.
-  EXPECT_TRUE(capsule.GetPrim().RemoveProperty(radius_attribute.GetName()));
+  capsule.GetPrim().RemoveProperty(radius_attribute.GetName());
   parsed_dimension = internal::GetCapsuleDimension(capsule.GetPrim(),
     meters_per_unit_, stage_up_axis_, diagnostic_policy_);
   EXPECT_FALSE(parsed_dimension.has_value());
@@ -374,24 +361,23 @@ TEST_F(UsdGeometryTest, MeshParsingTest) {
   pxr::UsdAttribute points_attribute = mesh.CreatePointsAttr();
   pxr::UsdAttribute face_counts_attribute = mesh.CreateFaceVertexCountsAttr();
   pxr::UsdAttribute indices_attribute = mesh.CreateFaceVertexIndicesAttr();
-  EXPECT_TRUE(points_attribute.Set(vertices));
-  EXPECT_TRUE(face_counts_attribute.Set(face_vertex_counts));
-  EXPECT_TRUE(indices_attribute.Set(face_vertex_indices));
+  points_attribute.Set(vertices);
+  face_counts_attribute.Set(face_vertex_counts);
+  indices_attribute.Set(face_vertex_indices);
   pxr::UsdGeomXformOp scale_op = mesh.AddScaleOp(
     pxr::UsdGeomXformOp::PrecisionDouble);
 
-  EXPECT_TRUE(scale_op.Set(
-    pxr::GfVec3d(scale_factor, scale_factor, scale_factor)));
+  scale_op.Set(pxr::GfVec3d(scale_factor, scale_factor, scale_factor));
   std::unique_ptr<geometry::Shape> shape = internal::CreateGeometryMesh(
     "octahedron.obj", mesh.GetPrim(), meters_per_unit_, diagnostic_policy_);
-  EXPECT_TRUE(shape != nullptr);
-  geometry::Mesh* shape_mesh = dynamic_cast<geometry::Mesh*>(shape.get());
-  EXPECT_EQ(shape_mesh->scale(), scale_factor);
+  ASSERT_TRUE(shape != nullptr);
+  geometry::Mesh& shape_mesh = dynamic_cast<geometry::Mesh&>(*shape);
+  EXPECT_EQ(shape_mesh.scale(), scale_factor);
 
   // Check whether Drake can sucessfully parse that file by computing the
   // convex hull of the octahedron mesh.
   const geometry::PolygonSurfaceMesh<double>& convex_hull =
-    shape_mesh->GetConvexHull();
+    shape_mesh.GetConvexHull();
   EXPECT_EQ(convex_hull.num_faces(), 8);
 
   // Case: Input Prim is not an UsdGeomMesh.
@@ -404,7 +390,7 @@ TEST_F(UsdGeometryTest, MeshParsingTest) {
     ".*Failed to cast the Prim at .* into an UsdGeomMesh.*"));
 
   // Case: The UsdGeomMesh Prim does not have indices attribute.
-  EXPECT_TRUE(mesh.GetPrim().RemoveProperty(indices_attribute.GetName()));
+  mesh.GetPrim().RemoveProperty(indices_attribute.GetName());
   shape = internal::CreateGeometryMesh("no_indices.obj", mesh.GetPrim(),
     meters_per_unit_, diagnostic_policy_);
   EXPECT_TRUE(shape == nullptr);
@@ -412,7 +398,7 @@ TEST_F(UsdGeometryTest, MeshParsingTest) {
     ".*Failed to read the 'faceVertexIndices' attribute of the Prim at.*"));
 
   // Case: The UsdGeomMesh Prim does not have points attribute.
-  EXPECT_TRUE(mesh.GetPrim().RemoveProperty(points_attribute.GetName()));
+  mesh.GetPrim().RemoveProperty(points_attribute.GetName());
   shape = internal::CreateGeometryMesh("no_points.obj", mesh.GetPrim(),
     meters_per_unit_, diagnostic_policy_);
   EXPECT_TRUE(shape == nullptr);
@@ -421,7 +407,7 @@ TEST_F(UsdGeometryTest, MeshParsingTest) {
 
   // Case: The face count attribute of the Prim contains elements other than 3.
   face_vertex_counts[0] = 4;
-  EXPECT_TRUE(face_counts_attribute.Set(face_vertex_counts));
+  face_counts_attribute.Set(face_vertex_counts);
   shape = internal::CreateGeometryMesh("quad_mesh.obj", mesh.GetPrim(),
     meters_per_unit_, diagnostic_policy_);
   EXPECT_TRUE(shape == nullptr);
@@ -429,7 +415,7 @@ TEST_F(UsdGeometryTest, MeshParsingTest) {
     ".*The mesh at .* is not a triangle mesh..*"));
 
   // Case: The UsdGeomMesh Prim does not have face counts attribute.
-  EXPECT_TRUE(mesh.GetPrim().RemoveProperty(face_counts_attribute.GetName()));
+  mesh.GetPrim().RemoveProperty(face_counts_attribute.GetName());
   shape = internal::CreateGeometryMesh("no_face_counts.obj", mesh.GetPrim(),
     meters_per_unit_, diagnostic_policy_);
   EXPECT_TRUE(shape == nullptr);
@@ -437,7 +423,7 @@ TEST_F(UsdGeometryTest, MeshParsingTest) {
     ".*Failed to read the 'faceVertexCounts' attribute of the Prim at.*"));
 
   // Case: The UsdGeomMesh Prim has invalid (non-isotropic) scaling.
-  EXPECT_TRUE(scale_op.Set(pxr::GfVec3d(1.0, 2.0, 1.0)));
+  scale_op.Set(pxr::GfVec3d(1.0, 2.0, 1.0));
   shape = internal::CreateGeometryMesh("invalid_scaling.obj", mesh.GetPrim(),
     meters_per_unit_, diagnostic_policy_);
   EXPECT_TRUE(shape == nullptr);
@@ -456,13 +442,12 @@ TEST_F(UsdGeometryTest, GetRigidTransformTest) {
     pxr::UsdGeomXformOp::PrecisionDouble);
   pxr::UsdGeomXformOp rotate_op = xform.AddRotateXYZOp(
     pxr::UsdGeomXformOp::PrecisionDouble);
-  EXPECT_TRUE(translate_op.Set(translation));
-  EXPECT_TRUE(rotate_op.Set(rotation_xyz));
+  translate_op.Set(translation);
+  rotate_op.Set(rotation_xyz);
 
   std::optional<math::RigidTransform<double>> transform =
     internal::GetPrimRigidTransform(xform.GetPrim(), meters_per_unit_,
       diagnostic_policy_);
-  EXPECT_TRUE(transform.has_value());
   const Eigen::Vector3d parsed_translation = transform.value().translation();
   const Eigen::Vector3d parsed_rotation_xyz =
     transform.value().rotation().ToRollPitchYaw().vector();
@@ -504,23 +489,23 @@ TEST_F(UsdGeometryTest, GetPrimColorTest) {
     pxr::TfToken(""));
   std::optional<Eigen::Vector4d> color = internal::GetGeomPrimColor(empty_prim,
     diagnostic_policy_);
-  EXPECT_EQ(color, std::nullopt);
+  EXPECT_FALSE(color.has_value());
 
   // Case: Prim does not contain color attribute.
   pxr::UsdGeomCube box = pxr::UsdGeomCube::Define(stage_,
     pxr::SdfPath("/Box"));
   color = internal::GetGeomPrimColor(box.GetPrim(), diagnostic_policy_);
-  EXPECT_EQ(color, std::nullopt);
+  EXPECT_FALSE(color.has_value());
 
   // Case: All inputs are valid.
   pxr::UsdGeomGprim box_gprim = pxr::UsdGeomGprim(box);
   const pxr::VtArray<pxr::GfVec3f> input_color = {
     pxr::GfVec3f(0.1, 0.2, 0.0) };
-  EXPECT_TRUE(box_gprim.CreateDisplayColorAttr().Set(input_color));
+  box_gprim.CreateDisplayColorAttr().Set(input_color);
   color = internal::GetGeomPrimColor(box.GetPrim(), diagnostic_policy_);
-  EXPECT_TRUE(color.has_value());
+  ASSERT_TRUE(color.has_value());
   const pxr::GfVec3f output_color = pxr::GfVec3f(
-    color.value()[0], color.value()[1], color.value()[2]);
+    (*color)[0], (*color)[1], (*color)[2]);
   EXPECT_EQ(input_color[0], output_color);
 }
 
@@ -544,7 +529,7 @@ TEST_F(UsdGeometryTest, InvalidMassTest) {
       double size = 1
     })""";
 
-  EXPECT_TRUE(stage_->GetRootLayer()->ImportFromString(file));
+  ASSERT_TRUE(stage_->GetRootLayer()->ImportFromString(file));
   const double mass = internal::GetPrimMass(
     stage_->GetPrimAtPath(pxr::SdfPath("/Box")), diagnostic_policy_);
   EXPECT_EQ(1.0, mass);
