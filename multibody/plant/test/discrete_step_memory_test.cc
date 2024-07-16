b73b98fe7acc68d693cd14eaae5dacf2efc35afa
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-07-16 15:22:15 -0700

    [multibody] Discrete MultibodyPlant uses sampled output ports
    This is a major change to the output ports of a discrete-time
    MultibodyPlant for dynamics outputs (forces, accelerations, contacts,
    etc.). The kinematics outputs (positions, velocities, poses, etc.)
    are unchanged. Refer to the new plant API documentation titled
    "Discrete system dynamics" and "Output port sampling" for details.
    
    A quick summary of major impacts:
    
    (1) By default, a discrete-time MultibodyPlant now has extra abstract
    State in the Context in support the sampling. If you need a minimal-
    state representation for motion planning, optimization, etc. then you
    can set the config option use_sampled_output_ports=false, or use a
    continuous-time plant instead.
    
    (2) By default, setting the positions of a discrete-time plant in the
    Context will not have any effect on the dynamics-related output ports,
    e.g., the contact results will not change. If you need to see changes
    to outputs without stepping the plant, then you can set the config
    option use_sampled_output_ports=false, or use a continuous-time plant
    instead. Or, you can call ExecuteForcedEvents() to force a dynamics
    step and then the outputs will change.
    
    (3) Simulations that use the dynamics output ports (e.g., that
    visualize contact results, or report external torques) will run
    substantially faster.
    
    (4) The port semantics now have a solid mathematical foundation.
    Previously, they were pseudo-sampled in a way that traded off
    correctness for speed. This new implementation is both faster
    and more correct.

---
+++ multibody/plant/test/discrete_step_memory_test.cc
@@ -0,0 +1,46 @@
+#include "drake/multibody/plant/discrete_step_memory.h"
+
+#include <gtest/gtest.h>
+
+#include "drake/multibody/plant/multibody_plant.h"
+
+namespace drake {
+namespace multibody {
+class MultibodyPlantTester {
+ public:
+  MultibodyPlantTester() = delete;
+  static const internal::MultibodyTree<double>& internal_tree(
+      const MultibodyPlant<double>& plant) {
+    return plant.internal_tree();
+  }
+};
+namespace internal {
+namespace {
+
+GTEST_TEST(DiscreteStepMemoryTest, Lifecycle) {
+  MultibodyPlant<double> plant{0.01};
+  const internal::MultibodyTree<double>& tree =
+      MultibodyPlantTester::internal_tree(plant);
+  plant.Finalize();
+
+  // Initially empty.
+  DiscreteStepMemory dut;
+  EXPECT_EQ(dut.template get<double>(), nullptr);
+
+  // Allocation matches get().
+  const auto& data = dut.template Allocate<double>(tree.get_topology());
+  EXPECT_EQ(dut.template get<double>(), &data);
+
+  // Access the wrong type yields empty.
+  EXPECT_EQ(dut.template get<AutoDiffXd>(), nullptr);
+
+  // Moved from values are also empty, even with the right type.
+  DiscreteStepMemory moved_to{std::move(dut)};
+  EXPECT_EQ(moved_to.template get<double>(), &data);
+  EXPECT_EQ(dut.template get<double>(), nullptr);
+}
+
+}  // namespace
+}  // namespace internal
+}  // namespace multibody
+}  // namespace drake
