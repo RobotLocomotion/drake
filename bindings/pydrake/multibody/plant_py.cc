cbce6ea267fb87575fa21f84bdaaf949e9260bf5
Mitiguy <mitiguy@users.noreply.github.com>
2024-07-22 08:28:17 -0700

    Add bindings for MultibodyPlant::CalcCenterOfMassTranslationalVelocity/Acceleration() (#21730)
    Co-Authored-By: mitiguy <paul.mitiguy@tri.global>

--- bindings/pydrake/multibody/plant_py.cc
+++ bindings/pydrake/multibody/plant_py.cc
@@ -335,6 +335,28 @@ void DoScalarDependentDefinitions(py::module m, T) {
                 &Class::CalcCenterOfMassPositionInWorld),
             py::arg("context"), py::arg("model_instances"),
             cls_doc.CalcCenterOfMassPositionInWorld.doc_2args)
+        .def("CalcCenterOfMassTranslationalVelocityInWorld",
+            overload_cast_explicit<Vector3<T>, const Context<T>&>(
+                &Class::CalcCenterOfMassTranslationalVelocityInWorld),
+            py::arg("context"),
+            cls_doc.CalcCenterOfMassTranslationalVelocityInWorld.doc_1args)
+        .def("CalcCenterOfMassTranslationalVelocityInWorld",
+            overload_cast_explicit<Vector3<T>, const Context<T>&,
+                const std::vector<ModelInstanceIndex>&>(
+                &Class::CalcCenterOfMassTranslationalVelocityInWorld),
+            py::arg("context"), py::arg("model_instances"),
+            cls_doc.CalcCenterOfMassTranslationalVelocityInWorld.doc_2args)
+        .def("CalcCenterOfMassTranslationalAccelerationInWorld",
+            overload_cast_explicit<Vector3<T>, const Context<T>&>(
+                &Class::CalcCenterOfMassTranslationalAccelerationInWorld),
+            py::arg("context"),
+            cls_doc.CalcCenterOfMassTranslationalAccelerationInWorld.doc_1args)
+        .def("CalcCenterOfMassTranslationalAccelerationInWorld",
+            overload_cast_explicit<Vector3<T>, const Context<T>&,
+                const std::vector<ModelInstanceIndex>&>(
+                &Class::CalcCenterOfMassTranslationalAccelerationInWorld),
+            py::arg("context"), py::arg("model_instances"),
+            cls_doc.CalcCenterOfMassTranslationalAccelerationInWorld.doc_2args)
         .def(
             "CalcSpatialInertia",
             [](const Class* self, const Context<T>& context,
