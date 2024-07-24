cc29512af6ddb9bea9b768631a6f983ec7a4f9b9
Joseph Masterjohn <joemasterjohn@gmail.com>
2024-07-24 18:28:49 -0400

    [proximity] Adds VolumeMeshTopology

---
+++ geometry/proximity/volume_mesh_topology.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <array>
+#include <vector>
+
+#include "drake/geometry/proximity/sorted_triplet.h"
+#include "drake/geometry/proximity/volume_mesh.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+
+/** %VolumeMeshTopology represents the topology of a tetrahedral volume mesh.
+ */
+class VolumeMeshTopology {
+ public:
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(VolumeMeshTopology);
+
+  template <typename T>
+  explicit VolumeMeshTopology(const VolumeMesh<T>& mesh) {
+    tetrahedra_neighbors_.resize(mesh.num_elements(),
+                                 std::array<int, 4>{-1, -1, -1, -1});
+
+    CalcAdjacentTetrahedraNeighbors(mesh.tetrahedra());
+  }
+
+  /**
+   Returns the index of `i`-th neighbor of tet `e` (i.e. the tet across from
+   vertex `i`). If tet `e` does not have a neighbor across from `i` (i.e. face
+   `i` is a boundary face), returns -1.
+   @param e The index of the element.
+   @param i The index of the neighbor
+   @pre `e ∈ [0, mesh().num_elements())`.
+   @pre `i ∈ [0, 3]`.
+  */
+  int neighbor(int e, int i) const {
+    DRAKE_DEMAND(0 <= e && e < ssize(tetrahedra_neighbors_));
+    DRAKE_DEMAND(0 <= i && i < 4);
+    return tetrahedra_neighbors_[e][i];
+  }
+
+ private:
+  // Calculate the adjacency information for all tetrahedra in `elements`.
+  void CalcAdjacentTetrahedraNeighbors(
+      const std::vector<VolumeElement>& elements);
+
+  // Stores the index of the neighboring tetrahedra of the element at index i.
+  // The index stored at index j is the neighbor across for vertex j, or in
+  // other terms the tet that shares face {0, 1, 2, 3} / {i}. -1 is used to
+  // represent the absence of a neighbor on a face (i.e. a boundary face).
+  std::vector<std::array<int, 4>> tetrahedra_neighbors_;
+};
+
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
