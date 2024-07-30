0ba9225a00f8eea809fca7f4d0eb0218168b6a9e
Xuchen Han <xuchen.han@tri.global>
2024-07-30 15:05:03 -0700

    [fem] Remove quadrature template from constitutive models

--- multibody/fem/deformation_gradient_data.h
+++ multibody/fem/deformation_gradient_data.h
@@ -22,11 +22,10 @@ class DeformationGradientData;
  model (`Foo`) there should be a corresponding `FooData` derived from
  DeformationGradientData.
 
- It stores the deformation gradients calculated at the prescribed "locations"
- (the number of which could depend on varying criteria, e.g., quadrature points,
- etc.). A derived class will further store derived quantities which solely
- depend on the stored deformation gradients (e.g., strain) that facilitate
- calculations of energy density, stress, and stress derivatives.
+ It stores the deformation gradients calculated at the prescribed location. A
+ derived class will further store derived quantities which solely depend on the
+ stored deformation gradients (e.g., strain) that facilitate calculations of
+ energy density, stress, and stress derivatives.
 
  As part of a derivation, the child `FooData` class must implement the method:
 
@@ -35,42 +34,30 @@ class DeformationGradientData;
  Note that this is not a virtual method but failure to implement the method
  correctly will lead to an exception about "undefined" methods when
  `UpdateData()` is invoked.
- @tparam_nonsymbolic_scalar T.
- @tparam num_locations_at_compile_time Number of locations at which the data are
- evaluated. */
-template <template <typename, int> class DerivedDeformationGradientData,
-          typename T, int num_locations_at_compile_time>
-class DeformationGradientData<
-    DerivedDeformationGradientData<T, num_locations_at_compile_time>> {
+ @tparam_nonsymbolic_scalar T. */
+template <template <typename> class DerivedDeformationGradientData, typename T>
+class DeformationGradientData<DerivedDeformationGradientData<T>> {
  public:
-  using Derived =
-      DerivedDeformationGradientData<T, num_locations_at_compile_time>;
-
-  /* The number of locations at which the data needs to be evaluated. */
-  static constexpr int num_locations = num_locations_at_compile_time;
+  using Derived = DerivedDeformationGradientData<T>;
 
   /* Updates the data with the given deformation gradients. The deformation
    gradient dependent quantities are also updated with the given
-   `deformation_gradient`.
-   @param deformation_gradient The up-to-date deformation gradients evaluated at
-   the prescribed locations. */
-  void UpdateData(std::array<Matrix3<T>, num_locations> deformation_gradient,
-                  std::array<Matrix3<T>, num_locations>
-                      previous_step_deformation_gradient) {
-    deformation_gradient_ = std::move(deformation_gradient);
-    previous_step_deformation_gradient_ =
-        std::move(previous_step_deformation_gradient);
+   deformation gradients evaluated at the current time step and the previous
+   time step. */
+  void UpdateData(const Matrix3<T>& deformation_gradient,
+                  const Matrix3<T>& previous_step_deformation_gradient) {
+    deformation_gradient_ = deformation_gradient;
+    previous_step_deformation_gradient_ = previous_step_deformation_gradient;
     static_cast<Derived*>(this)->UpdateFromDeformationGradient();
   }
 
-  const std::array<Matrix3<T>, num_locations>& deformation_gradient() const {
+  const Matrix3<T>& deformation_gradient() const {
     return deformation_gradient_;
   }
 
   /* Returns the deformation gradient evaluated at all quadrature points at the
    previous time step t₀. */
-  const std::array<Matrix3<T>, num_locations>&
-  previous_step_deformation_gradient() const {
+  const Matrix3<T>& previous_step_deformation_gradient() const {
     return previous_step_deformation_gradient_;
   }
 
@@ -79,10 +66,9 @@ class DeformationGradientData<
 
   /* Constructs a DeformationGradientData with identity deformation gradients.
    */
-  DeformationGradientData() {
-    deformation_gradient_.fill(Matrix3<T>::Identity());
-    previous_step_deformation_gradient_.fill(Matrix3<T>::Identity());
-  }
+  DeformationGradientData()
+      : deformation_gradient_(Matrix3<T>::Identity()),
+        previous_step_deformation_gradient_(Matrix3<T>::Identity()) {}
 
   /* Derived classes *must* shadow this method to compute quantities derived
    from deformation gradients. `deformation_gradient()` will be up to date
@@ -95,8 +81,8 @@ class DeformationGradientData<
   }
 
  private:
-  std::array<Matrix3<T>, num_locations> deformation_gradient_;
-  std::array<Matrix3<T>, num_locations> previous_step_deformation_gradient_;
+  Matrix3<T> deformation_gradient_;
+  Matrix3<T> previous_step_deformation_gradient_;
 };
 
 }  // namespace internal
