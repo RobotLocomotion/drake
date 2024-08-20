624b1a9e0f8c26239e6677ee7d269127758146a7
cohnt <cohnt@umich.edu>
2024-08-20 13:48:31 -0400

    Getters for the A and b arguments in CartesianProduct.

--- geometry/optimization/cartesian_product.h
+++ geometry/optimization/cartesian_product.h
@@ -73,6 +73,14 @@ class CartesianProduct final : public ConvexSet {
   product. */
   const ConvexSet& factor(int i) const;
 
+  /** Returns a pointer to the matrix A if it has been set, or nullptr
+  otherwise. */
+  std::optional<Eigen::MatrixXd> A() const { return A_; }
+
+  /** Returns a pointer to the vector b if it has been set, or nullptr
+  otherwise. */
+  std::optional<Eigen::VectorXd> b() const { return b_; }
+
   /** Returns true if each subvector is in its corresponding set with tolerance
   `tol`.  Note: Tolerance support for this query varies in the different convex
   set implementations. */
