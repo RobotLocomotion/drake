49974e8a879ec808e8f545904496e1de8b256c8e
Sean Curtis <sean.curtis@tri.global>
2024-08-21 13:05:47 -0700

    Introduce InMemoryMesh and MeshSource
    These provide the foundation of in-memory mesh specification.
    InMemoryMesh tracks a MemoryFile for a mesh file (and an optional set of
    supporting files in the near future).
    
    MeshSource acts as a sugared-up variant between a filesystem::path and
    an InMemoryMesh. This will, ultimately, serve as the storage for Mesh
    and Convex specifications.
    
    While it is not integrated with Mesh or Convex yet, we exercise the new
    APIs in vtk_to_volume_mesh -- which consumes the MeshSource and creates
    the VolumeMesh from both in-memory as well as on-disk .vtk files.

---
+++ geometry/test/in_memory_mesh_test.cc
@@ -0,0 +1,78 @@
+#include "drake/geometry/in_memory_mesh.h"
+
+#include <gtest/gtest.h>
+
+#include "drake/common/test_utilities/expect_throws_message.h"
+#include "drake/common/unused.h"
+
+namespace drake {
+namespace geometry {
+namespace {
+
+GTEST_TEST(InMemoryMeshTest, Constructor) {
+  const InMemoryMesh empty;
+  EXPECT_TRUE(empty.empty());
+
+  const InMemoryMesh just_mesh(MemoryFile("body", ".ext", "hint"));
+  ASSERT_FALSE(just_mesh.empty());
+  EXPECT_EQ(just_mesh.mesh_file().contents(), "body");
+}
+
+GTEST_TEST(MeshSourceTest, PathConstructor) {
+  const MeshSource s(std::filesystem::path("path"));
+  EXPECT_EQ(s.description(), "path");
+  ASSERT_TRUE(s.is_path());
+  EXPECT_FALSE(s.is_in_memory());
+  EXPECT_EQ(s.path(), "path");
+}
+
+GTEST_TEST(MeshSourceTest, InMemoryMeshConstructor) {
+  const MeshSource s(InMemoryMesh(MemoryFile("contents", ".ext", "hint")));
+  EXPECT_EQ(s.description(), "hint");
+  EXPECT_FALSE(s.is_path());
+  ASSERT_TRUE(s.is_in_memory());
+  EXPECT_EQ(s.in_memory().mesh_file().contents(), "contents");
+}
+
+GTEST_TEST(MeshSourceTest, StringConstructor) {
+  const char c_ptr[] = "path1";
+  const std::string_view view("path2");
+  const std::string str("path3");
+
+  /* C-style string. */
+  {
+    const MeshSource s(c_ptr);
+    EXPECT_TRUE(s.is_path());
+    EXPECT_FALSE(s.is_in_memory());
+  }
+
+  /* std::string_view. */
+  {
+    const MeshSource s(view);
+    EXPECT_TRUE(s.is_path());
+    EXPECT_FALSE(s.is_in_memory());
+  }
+
+  /* std::string. */
+  {
+    const MeshSource s(str);
+    EXPECT_TRUE(s.is_path());
+    EXPECT_FALSE(s.is_in_memory());
+  }
+}
+
+/* Dummy function for testing implicit construction. */
+void ConsumeMeshSource(const MeshSource&) {}
+
+/* We allow implicit conversion from a filename/path and in-memory mesh to a
+ MeshSource. Successful compilation implies test success. */
+GTEST_TEST(MeshSourceTest, ImplicitConversion) {
+  ConsumeMeshSource("char_pointer");
+  ConsumeMeshSource(std::string("string"));
+  ConsumeMeshSource(std::string_view("string_view"));
+  ConsumeMeshSource(std::filesystem::path("path"));
+  ConsumeMeshSource(InMemoryMesh(MemoryFile("contents", ".ext", "hint")));
+}
+}  // namespace
+}  // namespace geometry
+}  // namespace drake
