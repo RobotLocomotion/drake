923a09a63333af58447fa95f1f2463c2fcf4d4ae
Michael Sherman <sherm1@gmail.com>
2024-08-22 09:58:30 -0700

    Replace Doxygen /** with /* for internal API (#21840)

--- multibody/topology/spanning_forest_loop_constraint.h
+++ multibody/topology/spanning_forest_loop_constraint.h
@@ -11,13 +11,13 @@ namespace multibody {
 // TODO(sherm1) Promote from internal once API has stabilized: issue #11307.
 namespace internal {
 
-/** Weld constraints added during modeling to close loops. */
+/* Weld constraints added during modeling to close loops. */
 class SpanningForest::LoopConstraint {
  public:
-  /** (Internal use only) Copy/Move constructor & assignment. */
+  /* (Internal use only) Copy/Move constructor & assignment. */
   DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(LoopConstraint);
 
-  /** (Internal use only) Created in SpanningForest::BuildForest() when loops
+  /* (Internal use only) Created in SpanningForest::BuildForest() when loops
   are cut. */
   LoopConstraint(LoopConstraintIndex loop_constraint_index,
                  MobodIndex primary_mobod_index, MobodIndex shadow_mobod_index)
@@ -30,14 +30,14 @@ class SpanningForest::LoopConstraint {
     DRAKE_DEMAND(primary_mobod_index != shadow_mobod_index);
   }
 
-  /** Returns the index assigned to this LoopConstraint. */
+  /* Returns the index assigned to this LoopConstraint. */
   LoopConstraintIndex index() const { return loop_constraint_index_; }
 
-  /** Returns the primary Mobod associated with the cut Link. This will
+  /* Returns the primary Mobod associated with the cut Link. This will
   be the parent of the implementing weld constraint. */
   MobodIndex primary_mobod() const { return primary_mobod_index_; }
 
-  /** Returns the shadow Mobod associated with the cut Link. This will
+  /* Returns the shadow Mobod associated with the cut Link. This will
   be the child of the implementing weld constraint. */
   MobodIndex shadow_mobod() const { return shadow_mobod_index_; }
 
