63543b1c1e633b0d86da769444fcda8238d96e50
Matthew Woehlke <matthew.woehlke@kitware.com>
2024-08-23 15:57:41 -0400

    Use venv for Python dependencies (macOS)
    Create a series of scripts to set up and manage a Python virtual
    environment that can be used to build (and test) Drake. Remove Python
    packages from macOS setup. Add dependencies to the virtual environment
    as needed.

--- setup/mac/source_distribution/install_prereqs.sh
+++ setup/mac/source_distribution/install_prereqs.sh
@@ -49,13 +49,7 @@ if [[ "${with_test_only}" -eq 1 ]]; then
   brew bundle --file="${BASH_SOURCE%/*}/Brewfile-test-only" --no-lock
 fi
 
-if ! command -v pip3.12 &>/dev/null; then
-  echo 'ERROR: pip3.12 is NOT installed. The post-install step for the python@3.12 formula may have failed.' >&2
+if ! command -v python3.12 &>/dev/null; then
+  echo 'ERROR: python3.12 is NOT installed. The post-install step for the python@3.12 formula may have failed.' >&2
   exit 2
-fi
-
-pip3.12 install --break-system-packages -r "${BASH_SOURCE%/*}/requirements.txt"
-
-if [[ "${with_test_only}" -eq 1 ]]; then
-  pip3.12 install --break-system-packages -r "${BASH_SOURCE%/*}/requirements-test-only.txt"
 fi
