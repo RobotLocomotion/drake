91e8464bdc4a1a181cb17b88734a3481008660bb
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-08-28 18:16:49 -0700

    Remove deprecated code 2024-09

--- examples/pendulum/gen/pendulum_input.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "drake/examples/pendulum/pendulum_input.h"
-
-// clang-format off
-// NOLINTNEXTLINE
-#warning "DRAKE_DEPRECATED: This include path is deprecated and will be removed on or after 2024-09-01. Remove the /gen/ part of your code's include statement."
-// clang-format on
