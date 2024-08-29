7694671886306691594de38b9e361dd1bcc57dfe
cohnt <cohnt@umich.edu>
2024-08-29 16:41:26 -0400

    Implement and test for GcsTrajectoryOptimization.

--- planning/trajectory_optimization/gcs_trajectory_optimization.h
+++ planning/trajectory_optimization/gcs_trajectory_optimization.h
@@ -996,7 +996,8 @@ class GcsTrajectoryOptimization final {
 
 /** Returns a list of indices in the plant's generalized positions which
 correspond to a continuous revolute joint (a revolute joint with no joint
-limits). This includes the revolute component of a planar joint */
+limits). This includes the revolute component of PlanarJoint and
+RpyFloatingJoint. */
 std::vector<int> GetContinuousRevoluteJointIndices(
     const multibody::MultibodyPlant<double>& plant);
 
