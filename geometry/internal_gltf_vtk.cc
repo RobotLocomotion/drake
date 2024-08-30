db41288bbdc99e745ad3263d1494279e440e2b7d
Sean Curtis <sean.curtis@tri.global>
2024-08-30 11:56:03 -0700

    Extend convex hull computation to include in-memory glTF files
    We use VTK to parse the glTF files. This moves from vtkGLTFImporter to
    vtkGLTFDocumentLoader. This latter class supports parsing from a stream.
    However, we do patch it so we can silence it regarding missing supporting
    files that we simply don't care about (e.g., texture images).
    
    To be able to parse from memory, we need a custom vtkURILoader to resolve
    the file URI for a glTF's .bin file. We create one that knows about
    MeshSource so that it can serve files to VTK appropriately.
    
    To properly test a glTF file, we clean up rainbox_box.gltf, so that its
    transforms more fully reflect the intended value (they'd been written out
    with imprecision which was irrelevant to its visual appearance, but messed
    up the convex hull).
    
    In support of all of the above, we also do the following:
    
    - Introduce vtk-based helper provides utilities to work with glTF files in
      VTK.
    - Patching VTK
      - Add VTK::IOCore to our visible VTK packages.
      - The aforementioned patch.
    - Make the VtkDiagnosticEventObserver visible to the convex hull
      computation.

---
+++ geometry/internal_gltf_vtk.cc
@@ -0,0 +1,98 @@
+#include "drake/geometry/internal_gltf_vtk.h"
+
+#include <algorithm>
+#include <utility>
+
+#include <fmt/format.h>
+#include <vtkFileResourceStream.h>    // vtkIOCore
+#include <vtkMemoryResourceStream.h>  // vtkIOCore
+
+#include "drake/common/drake_assert.h"
+#include "drake/common/memory_file.h"
+#include "drake/common/overloaded.h"
+
+namespace drake {
+namespace geometry {
+namespace internal {
+
+using Eigen::Vector3d;
+
+namespace {
+
+constexpr char kBaseUriPrefix[] = "/MeshMemoryLoader/";
+constexpr int kBaseUriPrefxLength = 18;
+}
+
+MeshMemoryLoader::MeshMemoryLoader(const InMemoryMesh* mesh)
+    : mesh_(*mesh) {
+  DRAKE_DEMAND(mesh != nullptr);
+  base_uri_ = vtkURI::Make("file", vtkURIComponent(), kBaseUriPrefix);
+  SetBaseURI(base_uri_);
+}
+
+vtkSmartPointer<vtkResourceStream> MeshMemoryLoader::DoLoad(const vtkURI& uri) {
+  // Case insensitive
+  std::string scheme = uri.GetScheme().GetValue();
+  std::transform(scheme.begin(), scheme.end(), scheme.begin(),
+                 [](unsigned char c) {
+                   return std::tolower(c);
+                 });
+  if (scheme == "file") {
+    // Populate the stream with the data.
+    const std::string& path = uri.GetPath().GetValue();
+    // Note: If the glTF file were to reference a file in a *higher* directory
+    // (e.g., ../foo.bin), then this trick will not work. But the glTF spec
+    // explicitly disallows that.
+    auto pos = path.find(kBaseUriPrefix);
+    if (pos == std::string::npos) {
+      throw std::runtime_error(fmt::format(
+          "Error with in-memory glTF file. The glTF file has an unrecognizable "
+          "resource file URI: '{}'. All URIs should use relative file "
+          "positions and be included in the in-memory mesh's supporting files.",
+          uri.ToString()));
+    }
+    const std::string_view name(path.begin() + pos + kBaseUriPrefxLength,
+                                path.end());
+    const auto file_source_iter = mesh_.supporting_files.find(name);
+    if (file_source_iter == mesh_.supporting_files.end()) {
+      // If the glTF file refers to a file that isn't in our set of
+      // supporting files, we won't immediately throw. We'll let the parser yell
+      // about it later, when it's actually needed.
+      return nullptr;
+    }
+
+    return std::visit<vtkSmartPointer<vtkResourceStream>>(
+        overloaded{[](const std::filesystem::path& source_path) {
+                     vtkNew<vtkFileResourceStream> stream;
+                     if (!stream->Open(source_path.c_str())) {
+                       throw std::runtime_error("Unable to open file");
+                     }
+                     return stream;
+                   },
+                   [](const MemoryFile& file) {
+                     vtkNew<vtkMemoryResourceStream> stream;
+                     stream->SetBuffer(file.contents().c_str(),
+                                       file.contents().size(),
+                                       /* copy= */ false);
+                     return stream;
+                   }},
+        file_source_iter->second);
+  } else if (scheme == "data") {
+    // For data URIs, we'll let VTK's infrastructure handle it.
+    return this->LoadData(uri);
+  }
+
+  vtkErrorMacro("Unknown URI scheme for \"" << uri.ToString() << "\"");
+  return nullptr;
+}
+
+vtkSmartPointer<vtkResourceStream> MakeStreamForString(
+    const std::string* source) {
+  vtkNew<vtkMemoryResourceStream> stream;
+  stream->SetBuffer(source->c_str(), source->size(), /* copy= */ false);
+  return stream;
+}
+
+}  // namespace internal
+}  // namespace geometry
+}  // namespace drake
