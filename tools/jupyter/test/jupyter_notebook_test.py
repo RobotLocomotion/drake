4696fb14aa34ff05add1aa81cb795c4df0e3a126
Matthew Woehlke <matthew.woehlke@kitware.com>
2024-09-03 13:28:19 -0400

    not working

--- tools/jupyter/test/jupyter_notebook_test.py
+++ tools/jupyter/test/jupyter_notebook_test.py
@@ -1,9 +1,11 @@
 import subprocess
 import unittest
 
+from python.runfiles import Rlocation
 
 class TestJupyterNotebook(unittest.TestCase):
     def test_help(self):
+        print(Rlocation)
         """Ensures that `jupyter notebook` is installed (#12042)."""
         status = subprocess.call(args=["jupyter", "notebook", "--help"])
         self.assertEqual(status, 0)
