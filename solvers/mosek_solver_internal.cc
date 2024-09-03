9e541d59b36cb1b7a6550531ec8919374d057220
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-09-03 15:26:59 -0700

    fix mosek build errors

--- solvers/mosek_solver_internal.cc
+++ solvers/mosek_solver_internal.cc
@@ -1499,12 +1499,10 @@ MSKrescodee MosekSolverProgram::UpdateOptions(
   MSKrescodee rescode{MSK_RES_OK};
   // Set the maximum number of threads used by Mosek via the CommonSolverOptions
   // first, so that solver-specific options can overwrite this later.
-  if (merged_options.max_num_threads() > 0) {
-    rescode = MSK_putintparam(task_, "MSK_IPAR_NUM_THREADS",
-                              merged_options.max_num_threads());
-    ThrowForInvalidOption(rescode, "MSK_IPAR_NUM_THREADS",
-                          merged_options.max_num_threads());
-  }
+  rescode = MSK_putnaintparam(task_, "MSK_IPAR_NUM_THREADS",
+                              merged_options.get_max_threads());
+  ThrowForInvalidOption(rescode, "MSK_IPAR_NUM_THREADS",
+                        merged_options.get_max_threads());
   for (const auto& double_options : merged_options.GetOptionsDouble(mosek_id)) {
     if (rescode == MSK_RES_OK) {
       rescode = MSK_putnadouparam(task_, double_options.first.c_str(),
