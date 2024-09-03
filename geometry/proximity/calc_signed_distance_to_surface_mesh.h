0febe5a98df6ac1b773c425dfe5d081160871b53
damrongguoy <damrong.guoy@tri.global>
2024-09-03 16:16:01 -0700

    Clean up and refactor the .h file with downstream changes in .cc and _test.cc.

--- geometry/proximity/calc_signed_distance_to_surface_mesh.h
+++ geometry/proximity/calc_signed_distance_to_surface_mesh.h
@@ -2,6 +2,7 @@
 
 #include <limits>
 #include <map>
+#include <variant>
 #include <vector>
 
 #include "drake/common/drake_copyable.h"
@@ -19,24 +20,23 @@ namespace internal {
 // TODO(DamrongGuoy) Consider moving FeatureNormalSet into its own file if it
 //  is useful for other applications.
 
-// %FeatureNormalSet provides certain kinds of outward normal vectors at
-// vertices and edges of a triangle surface mesh. The normal at a vertex is
-// the angle-weighted average of face normals of triangles sharing the vertex.
-// The normal at an edge is the equal-weight average of face normals of two
-// triangles sharing the edge.
-//
-// The following paper shows that this kind of normal is suitable for the
-// inside-outside test of a point closest to a vertex or an edge, and other
-// kinds of normal vectors may give incorrect result.
-//
-// J.A. Baerentzen; H. Aanaes. Signed distance computation using the angle
-// weighted pseudonormal. IEEE Transactions on Visualization and Computer
-// Graphics (Volume: 11, Issue: 3, May-June 2005).
+/* %FeatureNormalSet provides outward normal vectors at vertices and edges of a
+ triangle surface mesh. The normal at a vertex is the angle-weighted average of
+ the face normals of the triangles sharing the vertex. The normal at an edge is
+ the equal-weight average of the face normals of the two triangles sharing the
+ edge.
+
+ The following paper shows that normals computed in this way are most suitable
+ for the inside-outside test of a point closest to a vertex or an edge.
+
+ J.A. Baerentzen; H. Aanaes. Signed distance computation using the angle
+ weighted pseudonormal. IEEE Transactions on Visualization and Computer
+ Graphics (Volume: 11, Issue: 3, May-June 2005).  */
 class FeatureNormalSet {
  public:
   DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(FeatureNormalSet);
 
-  // Computes and stores the normals at vertices and edges of the given
+  // Computes and stores the normals at the vertices and the edges of the given
   // surface mesh expressed in frame M.
   //
   // @pre  The mesh is watertight and a closed manifold. Otherwise, the
@@ -51,9 +51,8 @@ class FeatureNormalSet {
   //
   // @param v  the vertex index into the mesh
   //
-  // @pre 0 <= v < number of vertices in the mesh
-  // @throws std::exception if `v` is invalid.
-  Vector3<double> vertex_normal(int v) const {
+  // @throws std::exception if v < 0 or v >= number of vertices in the mesh
+  const Vector3<double>& vertex_normal(int v) const {
     DRAKE_THROW_UNLESS(0 <= v && v < ssize(vertex_normals_));
     return vertex_normals_[v];
   }
@@ -63,10 +62,9 @@ class FeatureNormalSet {
   //
   // The returned normal vector is expressed in the mesh's frame.
   //
-  // @param uv   the SortedPair<int>{u, v} of the edge between vertex
-  //             index u and vertex index v
+  // @param uv   the edge between vertices u and v, as represented by the
+  //             sorted indices of vertices.
   //
-  // @pre The edge `uv` is in the mesh.
   // @throws std::exception if the edge `uv` is not in the mesh.
   Vector3<double> edge_normal(const SortedPair<int>& uv) const {
     auto it = edge_normals_.find(uv);
@@ -79,157 +77,94 @@ class FeatureNormalSet {
   std::map<SortedPair<int>, Vector3<double>> edge_normals_{};
 };
 
-// %SquaredDistanceToTriangle stores information about squared distance from
-// a query point Q to a triangle for the inside-outside test.
+/* %SquaredDistanceToTriangle stores information about squared distance from
+ a query point Q to a triangle for the inside-outside test.  */
 struct SquaredDistanceToTriangle {
   double squared_distance{std::numeric_limits<double>::infinity()};
   // The point in the triangle closest to the query point Q.
   Vector3<double> closest_point{};
-
-  // Classification of the projection Q' of the query point Q onto the plane of
-  // the triangle. We use the following definitions:
-  //
-  // - The _interior_ of a triangle consists of points in the triangle that
-  //   are neither in edges nor at vertices.
-  // - The _interior_ of an edge consists of points in the edge that are not
-  //   at vertices.
-  enum class Projection {
-    // Q' is in the interior of the triangle. The `closest_point` is Q'.
-    // We can use the face normal of the triangle for the inside-outside test.
-    //
-    //                                  A
-    //                                🮠│
-    //                             🮠   │
-    //                          🮠      │
-    //                       🮠   Q'    │
-    //                   B ┄ ┄ ┄ ┄ ┄ ┄ ┄C
-    kInside,
-
-    // Q' is outside the interior of the triangle, and it is nearest to the
-    // interior of an edge (Q' could be in the interior of an edge).
-    // The `closest_point` is the projection of Q' onto that edge.
-    // We can use the edge normal (the equal-weight average of face normals of
-    // two triangles sharing the edge) for the inside-outside test. In the
-    // picture below, the area between the two rays is the region of such
-    // Q' closest to edge BC.
-    //
-    //                                  A
-    //                                🮠│
-    //                             🮠   │
-    //                          🮠      │
-    //                       🮠         │
-    //                   B ┄┄┄┄┄┄┄┄┄┄┄┄┄C
-    //                   ↓              ↓
-    //                   ↓       Q'     ↓
-    //                   ↓              ↓
-    kOutsideNearEdge,
-
-    // Q' is outside the interior of the triangle nearest to a vertex
-    // (Q' could be at a vertex). The `closest_point` is that vertex. We can
-    // use the vertex normal (the angle-weighted average of face normals of
-    // triangles sharing the vertex) for the inside-outside test. In the
-    // picture below, the area between the two rays is the region of such Q'
-    // closest to vertex B.
-    //
-    //                                   A
-    //                                🮠│
-    //         ↖                   🮠   │
-    //            ↖             🮠      │
-    //               ↖       🮠         │
-    //                   B ┄┄┄┄┄┄┄┄┄┄┄┄┄C
-    //       Q'          ↓
-    //                   ↓
-    //                   ↓
-    kOutsideNearVertex
-  } location{};
-
-  // The meaning of v depends on `location`:
-  // - kInside: v is zero. The closest point is in the triangle.
-  // - kOutsideNearEdge: The closest point is in the edge between the
-  //                     triangle's v-th vertex and (v+1)%3-th vertex;
-  //                     0 <= v < 3.
-  // - kOutsideNearVertex: The closest point is at the triangle's v-th vertex;
-  //                       0 <= v < 3.
-  int v{};
+  // The normal at the closest feature (vertex, edge, or triangle).
+  Vector3<double> feature_normal{};
 
   auto operator<=>(const SquaredDistanceToTriangle&) const = default;
 };
 
-// TODO(DamrongGuoy): Consider consolidating this function with the one in
-//  the anonymous namespace of calc_distance_to_surface_mesh.cc. That one
-//  gives the squared distance without the closest point and the classification
-//  of the projection of the query point.
-
+/* Calculates the squared distance and the closest point from the query point
+ Q to a triangle in a mesh. It also provides the normal at the closest point
+ suitable for the inside-outside test (see FeatureNormalSet).  */
 SquaredDistanceToTriangle CalcSquaredDistanceToTriangle(
     const Vector3<double>& p_MQ, int triangle_index,
-    const TriangleSurfaceMesh<double>& mesh_M);
+    const TriangleSurfaceMesh<double>& mesh_M,
+    const FeatureNormalSet& normal_set_M);
 
 struct SignedDistanceToSurfaceMesh {
   double signed_distance;
   Vector3<double> nearest_point;
+  // The gradient is not continuous when the query point is at a vertex or in
+  // an edge shared by triangles with different face normals.
+  // The gradient is also discontinuous at the query point with multiple
+  // nearest points.
   Vector3<double> gradient;
 
   auto operator<=>(const SignedDistanceToSurfaceMesh&) const = default;
 };
 
-// Calculates the signed distance, the nearest point, and the signed-distance
-// gradient from the query point Q to the surface mesh. It accelerates the
-// computation using BVH. It determines the sign using the given
-// FeatureNormalSet of the mesh.
-//
-// @param p_MQ  position of the query point expressed in frame M of the
-//              surface mesh.
-// @param mesh_M   the surface mesh expressed in frame M.
-// @param bvh_M    the BVH of the surface mesh, expressed in frame M.
-// @param mesh_normal_M  provides angle-weighted average normals at vertices
-//                       and equal-weight average normals at edges of the
-//                       surface mesh, expressed in frame M.
-//
-// @pre  The surface mesh is watertight and a closed manifold. Otherwise, it
-// might return incorrect signs and gradients.
-//
-// @note If p_MQ is on the surface, the returned signed distance is zero,
-// the nearest point is p_MQ itself, and the gradient is the normal
-// at the triangle, edge, or vertex where p_MQ locates.
-//
-// @note If p_MQ has multiple nearest points, the returned nearest point
-// and the gradient are chosen and computed from one of them. The selection
-// is deterministic but arbitrary.
-//
-// The following table characterizes all possible cases.  The first part
-// considers typical cases of a query point Q with a unique nearest point N
-// in a triangle, an edge, or a vertex. The gradient depends on the sign of
-// the distance. The second part considers special cases when Q has multiple
-// nearest points, which exclude the cases of zero distances.
-//
-//  |   sign   | unique nearest|  location of  |      gradient       |
-//  |          | point N       | nearest point |                     |
-//  | :------: | :-----------: | :-----------: | :-----------------: |
-//  | positive |      yes      |   triangle    | (Q-N).normalized()  |
-//  | positive |      yes      |     edge      | (Q-N).normalized()  |
-//  | positive |      yes      |    vertex     | (Q-N).normalized()  |
-//  | negative |      yes      |   triangle    | (N-Q).normalized()  |
-//  | negative |      yes      |     edge      | (N-Q).normalized()  |
-//  | negative |      yes      |    vertex     | (N-Q).normalized()  |
-//  |   zero   |      yes      |   triangle    |     face normal     |
-//  |   zero   |      yes      |     edge      |     edge normal     |
-//  |   zero   |      yes      |    vertex     |    vertex normal    |
-//  | :------: | :-----------: | :-----------: | :-----------------: |
-//  | positive |      no       |   triangle    | (Q-N).normalized()ᵃ |
-//  | positive |      no       |     edge      | (Q-N).normalized()ᵃ |
-//  | positive |      no       |    vertex     | (Q-N).normalized()ᵃ |
-//  | negative |      no       |   triangle    | (N-Q).normalized()ᵃ |
-//  | negative |      no       |     edge      | (N-Q).normalized()ᵃ |
-//  | negative |      no       |    vertex     | (N-Q).normalized()ᵃ |
-//  __*Table 1*__: Possible cases of signed distances and gradients according
-//  to location and uniqueness of the nearest point.
-//
-//  - ᵃ The nearest point N is chosen arbitrarily when Q has multiple
-//      nearest points.
+/* Calculates the signed distance, the nearest point, and the signed-distance
+ gradient from the query point Q to the surface mesh. It accelerates the
+ computation using a BVH. It determines the sign using the given
+ FeatureNormalSet of the mesh.
+
+ @param p_MQ  position of the query point expressed in frame M of the
+              surface mesh.
+ @param mesh_M   the surface mesh expressed in frame M.
+ @param bvh_M    the BVH of the surface mesh, expressed in frame M.
+ @param feature_normals_M  provides angle-weighted average normals at
+                           vertices and equal-weight average normals at
+                           edges of the surface mesh, expressed in frame M.
+
+ @pre  The surface mesh is a closed manifold with neither coincident
+ vertices nor self-intersection, and every triangle has its face winding
+ that gives an outward face normal. If the mesh has coincident vertices, the
+ signs and gradients may flipped abruptly far from the surface, creating a
+ region of query points with negative distance outside the enclosed volume
+ or a region of query points with positive distance inside the enclosed volume.
+ If the mesh is not closed, the regions of query points with positive and
+ negative signed distances may appear arbitrarily, even though the non-closed
+ mesh has neither inside nor outside.
+
+ @note If p_MQ is on the surface, the returned signed distance is zero,
+ the nearest point is p_MQ itself, and the gradient is the normal
+ at the triangle, edge, or vertex on which p_MQ lies.
+
+ @note If p_MQ has multiple nearest points, the returned nearest point
+ and the gradient are chosen and computed from one of them. The selection
+ is deterministic but arbitrary because we use the given BVH to select the
+ first closest point. The search order depends on the structure of
+ the BVH.
+
+ The following table characterizes all possible cases. The sign is positive,
+ negative, or zero when the query point is outside, inside, or on the
+ surface respectively. The nearest point N could lie in a triangle, an edge,
+ or a vertex. The gradient depends on the sign of the distance.
+
+  |   sign   |  location of  |    gradient    |
+  |          | nearest point |                |
+  | :------: | :-----------: | :------------: |
+  | positive |   triangle    | (Q-N) / ‖Q-N‖  |
+  | positive |     edge      | (Q-N) / ‖Q-N‖  |
+  | positive |    vertex     | (Q-N) / ‖Q-N‖  |
+  | negative |   triangle    | (N-Q) / ‖N-Q‖  |
+  | negative |     edge      | (N-Q) / ‖N-Q‖  |
+  | negative |    vertex     | (N-Q) / ‖N-Q‖  |
+  |   zero   |   triangle    |  face normal   |
+  |   zero   |     edge      |  edge normal   |
+  |   zero   |    vertex     | vertex normal  |
+  __*Table 1*__: Possible cases of signed distances and gradients according
+  to the location of the nearest point.  */
 SignedDistanceToSurfaceMesh CalcSignedDistanceToSurfaceMesh(
     const Vector3<double>& p_MQ, const TriangleSurfaceMesh<double>& mesh_M,
     const Bvh<Obb, TriangleSurfaceMesh<double>>& bvh_M,
-    const FeatureNormalSet& mesh_normal_M);
+    const FeatureNormalSet& feature_normals_M);
 
 }  // namespace internal
 }  // namespace geometry
