8d083f9a280371a45681ca416599bec062058211
Rick Poyner <rick.poyner@tri.global>
2024-09-16 14:57:05 -0400

    [sensors] Add parameters to RgbdSensor
    Also, deprecate a variety of old accessor methods.
    
    Co-authored-by: Jeremy Nimmer <jeremy.nimmer@tri.global>

--- systems/sensors/rgbd_sensor.cc
+++ systems/sensors/rgbd_sensor.cc
@@ -15,7 +15,28 @@ using geometry::QueryObject;
 using geometry::SceneGraph;
 using geometry::render::ColorRenderCamera;
 using geometry::render::DepthRenderCamera;
+using internal::RgbdSensorParameters;
 using math::RigidTransformd;
+
+namespace {
+
+void SanityCheckDepthCamera(const DepthRenderCamera& depth_camera) {
+  // The depth_16U represents depth in *millimeters*. With 16 bits there is
+  // an absolute limit on the farthest distance it can register. This tests to
+  // see if the user has specified a maximum depth value that exceeds that
+  // value.
+  const float kMaxValidDepth16UInM =
+      (std::numeric_limits<uint16_t>::max() - 1) / 1000.;
+  const double max_depth = depth_camera.depth_range().max_depth();
+  if (max_depth > kMaxValidDepth16UInM) {
+    drake::log()->warn(
+        "Specified max depth is {} m > max valid depth for 16 bits {} m. "
+        "depth_image_16u might not be able to capture the full depth range.",
+        max_depth, kMaxValidDepth16UInM);
+  }
+}
+
+}  // namespace
 
 RgbdSensor::RgbdSensor(FrameId parent_id, const RigidTransformd& X_PB,
                        const DepthRenderCamera& depth_camera,
@@ -27,15 +48,17 @@ RgbdSensor::RgbdSensor(FrameId parent_id, const RigidTransformd& X_PB,
 RgbdSensor::RgbdSensor(FrameId parent_id, const RigidTransformd& X_PB,
                        ColorRenderCamera color_camera,
                        DepthRenderCamera depth_camera)
-    : parent_frame_id_(parent_id),
-      color_camera_(std::move(color_camera)),
-      depth_camera_(std::move(depth_camera)),
-      X_PB_(X_PB) {
-  const CameraInfo& color_intrinsics = color_camera_.core().intrinsics();
-  const CameraInfo& depth_intrinsics = depth_camera_.core().intrinsics();
+    : defaults_{.parent_frame_id = parent_id,
+                .color_camera = std::move(color_camera),
+                .depth_camera = std::move(depth_camera),
+                .X_PB = X_PB} {
+  const CameraInfo& color_intrinsics =
+      defaults_.color_camera.core().intrinsics();
+  const CameraInfo& depth_intrinsics =
+      defaults_.depth_camera.core().intrinsics();
 
   query_object_input_port_ = &this->DeclareAbstractInputPort(
-      "geometry_query", Value<geometry::QueryObject<double>>{});
+      "geometry_query", Value<QueryObject<double>>{});
 
   ImageRgba8U color_image(color_intrinsics.width(), color_intrinsics.height());
   color_image_port_ = &this->DeclareAbstractOutputPort(
@@ -60,19 +83,102 @@ RgbdSensor::RgbdSensor(FrameId parent_id, const RigidTransformd& X_PB,
   image_time_output_port_ = &this->DeclareVectorOutputPort(
       "image_time", 1, &RgbdSensor::CalcImageTime, {this->time_ticket()});
 
-  // The depth_16U represents depth in *millimeters*. With 16 bits there is
-  // an absolute limit on the farthest distance it can register. This tests to
-  // see if the user has specified a maximum depth value that exceeds that
-  // value.
-  const float kMaxValidDepth16UInM =
-      (std::numeric_limits<uint16_t>::max() - 1) / 1000.;
-  const double max_depth = depth_camera_.depth_range().max_depth();
-  if (max_depth > kMaxValidDepth16UInM) {
-    drake::log()->warn(
-        "Specified max depth is {} m > max valid depth for 16 bits {} m. "
-        "depth_image_16u might not be able to capture the full depth range.",
-        max_depth, kMaxValidDepth16UInM);
-  }
+  parameter_index_ = AbstractParameterIndex{
+      this->DeclareAbstractParameter(Value<RgbdSensorParameters>(defaults_))};
+
+  SanityCheckDepthCamera(defaults_.depth_camera);
+}
+
+RgbdSensor::~RgbdSensor() = default;
+
+const ColorRenderCamera& RgbdSensor::default_color_render_camera() const {
+  return defaults_.color_camera;
+}
+
+void RgbdSensor::set_default_color_render_camera(
+    const ColorRenderCamera& color_camera) {
+  defaults_.color_camera = color_camera;
+}
+
+const ColorRenderCamera& RgbdSensor::GetColorRenderCamera(
+    const Context<double>& context) const {
+  return context.get_abstract_parameter(parameter_index_)
+      .get_value<RgbdSensorParameters>()
+      .color_camera;
+}
+
+void RgbdSensor::SetColorRenderCamera(
+    Context<double>* context, const ColorRenderCamera& color_camera) const {
+  context->get_mutable_abstract_parameter(parameter_index_)
+      .template get_mutable_value<RgbdSensorParameters>()
+      .color_camera = color_camera;
+}
+
+const DepthRenderCamera& RgbdSensor::default_depth_render_camera() const {
+  return defaults_.depth_camera;
+}
+
+void RgbdSensor::set_default_depth_render_camera(
+    const DepthRenderCamera& depth_camera) {
+  SanityCheckDepthCamera(depth_camera);
+  defaults_.depth_camera = depth_camera;
+}
+
+const DepthRenderCamera& RgbdSensor::GetDepthRenderCamera(
+    const Context<double>& context) const {
+  return context.get_abstract_parameter(parameter_index_)
+      .get_value<RgbdSensorParameters>()
+      .depth_camera;
+}
+
+void RgbdSensor::SetDepthRenderCamera(
+    Context<double>* context, const DepthRenderCamera& depth_camera) const {
+  SanityCheckDepthCamera(depth_camera);
+  context->get_mutable_abstract_parameter(parameter_index_)
+      .template get_mutable_value<RgbdSensorParameters>()
+      .depth_camera = depth_camera;
+}
+
+const RigidTransformd& RgbdSensor::default_X_PB() const {
+  return defaults_.X_PB;
+}
+
+void RgbdSensor::set_default_X_PB(const RigidTransformd& sensor_pose) {
+  defaults_.X_PB = sensor_pose;
+}
+
+const RigidTransformd& RgbdSensor::GetX_PB(
+    const Context<double>& context) const {
+  return context.get_abstract_parameter(parameter_index_)
+      .get_value<RgbdSensorParameters>()
+      .X_PB;
+}
+
+void RgbdSensor::SetX_PB(Context<double>* context,
+                         const RigidTransformd& sensor_pose) const {
+  context->get_mutable_abstract_parameter(parameter_index_)
+      .template get_mutable_value<RgbdSensorParameters>()
+      .X_PB = sensor_pose;
+}
+
+FrameId RgbdSensor::default_parent_frame_id() const {
+  return defaults_.parent_frame_id;
+}
+
+void RgbdSensor::set_default_parent_frame_id(FrameId id) {
+  defaults_.parent_frame_id = id;
+}
+
+FrameId RgbdSensor::GetParentFrameId(const Context<double>& context) const {
+  return context.get_abstract_parameter(parameter_index_)
+      .get_value<RgbdSensorParameters>()
+      .parent_frame_id;
+}
+
+void RgbdSensor::SetParentFrameId(Context<double>* context, FrameId id) const {
+  context->get_mutable_abstract_parameter(parameter_index_)
+      .template get_mutable_value<RgbdSensorParameters>()
+      .parent_frame_id = id;
 }
 
 const InputPort<double>& RgbdSensor::query_object_input_port() const {
@@ -103,17 +209,51 @@ const OutputPort<double>& RgbdSensor::image_time_output_port() const {
   return *image_time_output_port_;
 }
 
+const CameraInfo& RgbdSensor::color_camera_info() const {
+  return defaults_.color_camera.core().intrinsics();
+}
+
+const CameraInfo& RgbdSensor::depth_camera_info() const {
+  return defaults_.depth_camera.core().intrinsics();
+}
+
+const ColorRenderCamera& RgbdSensor::color_render_camera() const {
+  return default_color_render_camera();
+}
+
+const DepthRenderCamera& RgbdSensor::depth_render_camera() const {
+  return default_depth_render_camera();
+}
+
+const RigidTransformd& RgbdSensor::X_PB() const {
+  return default_X_PB();
+}
+
+const RigidTransformd& RgbdSensor::X_BC() const {
+  return defaults_.color_camera.core().sensor_pose_in_camera_body();
+}
+
+const RigidTransformd& RgbdSensor::X_BD() const {
+  return defaults_.depth_camera.core().sensor_pose_in_camera_body();
+}
+
+FrameId RgbdSensor::parent_frame_id() const {
+  return default_parent_frame_id();
+}
+
 void RgbdSensor::CalcColorImage(const Context<double>& context,
                                 ImageRgba8U* color_image) const {
   const QueryObject<double>& query_object = get_query_object(context);
-  query_object.RenderColorImage(color_camera_, parent_frame_id_, X_PB_,
+  query_object.RenderColorImage(GetColorRenderCamera(context),
+                                GetParentFrameId(context), GetX_PB(context),
                                 color_image);
 }
 
 void RgbdSensor::CalcDepthImage32F(const Context<double>& context,
                                    ImageDepth32F* depth_image) const {
   const QueryObject<double>& query_object = get_query_object(context);
-  query_object.RenderDepthImage(depth_camera_, parent_frame_id_, X_PB_,
+  query_object.RenderDepthImage(GetDepthRenderCamera(context),
+                                GetParentFrameId(context), GetX_PB(context),
                                 depth_image);
 }
 
@@ -127,24 +267,39 @@ void RgbdSensor::CalcDepthImage16U(const Context<double>& context,
 void RgbdSensor::CalcLabelImage(const Context<double>& context,
                                 ImageLabel16I* label_image) const {
   const QueryObject<double>& query_object = get_query_object(context);
-  query_object.RenderLabelImage(color_camera_, parent_frame_id_, X_PB_,
+  query_object.RenderLabelImage(GetColorRenderCamera(context),
+                                GetParentFrameId(context), GetX_PB(context),
                                 label_image);
 }
 
 void RgbdSensor::CalcX_WB(const Context<double>& context,
                           RigidTransformd* X_WB) const {
   DRAKE_DEMAND(X_WB != nullptr);
-  if (parent_frame_id_ == SceneGraph<double>::world_frame_id()) {
-    *X_WB = X_PB_;
+  FrameId parent_frame_id = GetParentFrameId(context);
+  RigidTransformd X_PB = GetX_PB(context);
+  if (parent_frame_id == SceneGraph<double>::world_frame_id()) {
+    *X_WB = X_PB;
   } else {
     const QueryObject<double>& query_object = get_query_object(context);
-    *X_WB = query_object.GetPoseInWorld(parent_frame_id_) * X_PB_;
+    *X_WB = query_object.GetPoseInWorld(parent_frame_id) * X_PB;
   }
 }
 
 void RgbdSensor::CalcImageTime(const Context<double>& context,
                                BasicVector<double>* output) const {
   output->SetFromVector(Vector1d{context.get_time()});
+}
+
+const QueryObject<double>& RgbdSensor::get_query_object(
+    const Context<double>& context) const {
+  return query_object_input_port().Eval<QueryObject<double>>(context);
+}
+
+void RgbdSensor::SetDefaultParameters(const Context<double>& context,
+                                      Parameters<double>* parameters) const {
+  LeafSystem<double>::SetDefaultParameters(context, parameters);
+  parameters->get_mutable_abstract_parameter(parameter_index_)
+      .set_value(defaults_);
 }
 
 }  // namespace sensors
