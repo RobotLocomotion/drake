ae24ad72c8cfaaff0cbd34a1fc691c85d223011f
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-09-27 10:02:01 -0700

    [vtk] Add vtkProbeOpenGLVersion binary rule for debugging

--- tools/workspace/vtk_internal/vtk_opengl_init.cc
+++ tools/workspace/vtk_internal/vtk_opengl_init.cc
@@ -4,11 +4,11 @@
 #include <vtkOpenGLRenderer.h>  // vtkRenderingOpenGL2
 
 // There are some VTK operations that depend on VTK's OpenGL module being
-// initialized. In addition to the obvious RenderEngineVtk, the less obvious
-// MakeConvexHull() also depends (VTK parses glTF files directly into OpenGL
-// constructs).
+// initialized. In addition to the obvious RenderEngineVtk, there are less
+// obvious like parsing glTF files (VTK parses glTF files directly into
+// OpenGL constructs).
 //
 // If a translation unit uses VTK *and* requires OpenGL to be initialized, it
-// should declare //geometry:vtk_opengl_init as a dependency.
+// should declare deps on @drake//tools/workspace/vtk_internal:vtk_opengl_init.
 
 VTK_MODULE_INIT(vtkRenderingOpenGL2)
