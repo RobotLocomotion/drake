34db3a17131b09ae6b0e70cc93b4afe6dce372c6
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-10-01 12:17:42 -0700

    [solvers] Port solver back-ends to use SpecificOptions
    In the near future, we anticipate changing the SolverOptions API in
    support of loading and saving (i.e., serialization). That's especially
    troublesome for our solver back-ends that consume its information,
    given the low-level and hodge-podge ways in which they hunt for and
    apply their specific options.
    
    This commit introduces a higher-level intermediary between solver
    back-ends and the program's options. Now SolverOptions are solely a
    user-facing aspect of defining a program; the solver back-ends never
    touch SolverOptions anymore. The new SolverBase::DoSolve2 virtual
    provides the mechanism for back-ends to take advantage of the new API.
    All solvers have been ported to use it. The old API remains intact for
    any out-of-tree solvers.
    
    The new API is also designed to improve uniformity of common errors,
    such as unknown names or wrongly-typed values.
    
    The new API also lays the groundwork for more efficient processing, as
    future work. It removes the need for the "Merge" (copy) operation in
    the hot path -- since it only provides a *view* of the options, it can
    easily keep track of several dictionaries and query them in order,
    with no copying.

--- solvers/gurobi_solver.h
+++ solvers/gurobi_solver.h
@@ -210,8 +210,9 @@ class GurobiSolver final : public SolverBase {
   using SolverBase::Solve;
 
  private:
-  void DoSolve(const MathematicalProgram&, const Eigen::VectorXd&,
-               const SolverOptions&, MathematicalProgramResult*) const final;
+  void DoSolve2(const MathematicalProgram&, const Eigen::VectorXd&,
+                internal::SpecificOptions*,
+                MathematicalProgramResult*) const final;
 
   // Note that this is mutable to allow latching the allocation of env_
   // during the first call of Solve() (which avoids grabbing a Gurobi license
