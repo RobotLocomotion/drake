841eb1953a06dff0f0b78e9633d64ceb5ca42587
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2024-10-08 12:24:15 -0400

    Setup depth shaders on the clone mapper

--- geometry/render_vtk/internal_render_engine_vtk.h
+++ geometry/render_vtk/internal_render_engine_vtk.h
@@ -238,6 +238,12 @@ class DRAKE_NO_EXPORT RenderEngineVtk : public render::RenderEngine,
   // introduced that explicitly declares PBR materials.
   void SetPbrMaterials();
 
+  // Setup a custom shader on the polydata mapper for rendering depth as the
+  // distance from the background plane
+  //
+  // @pre actor is not null.
+  static void SetDepthShader(vtkActor* actor);
+
   // A geometry is modeled with one or more "parts". A part maps to the actor
   // representing it in VTK and an optional transform mapping the actor's frame
   // A to the Drake geometry frame G. This mapping can include scaling terms.
@@ -295,7 +301,7 @@ class DRAKE_NO_EXPORT RenderEngineVtk : public render::RenderEngine,
   // deformable and does *not* depend on the system's pose information.
   // (If there is deformable geometry, it will have to be handled differently.)
   // Having "shared geometry" means having shared vtkPolyDataAlgorithm and
-  // vtkOpenGLPolyDataMapper instances. The shader callback gets registered to
+  // vtkOpenGLShaderProperty instances. The shader callback gets registered to
   // the *mapper* instances, so they all, implicitly, share the same callback.
   // Making this member static facilitates that but it does preclude the
   // possibility of simultaneous renderings with different uniform parameters.
