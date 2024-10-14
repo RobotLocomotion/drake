a44078571f1a02ddc5549dded3e3869e3274681d
Matthew Woehlke <matthew.woehlke@kitware.com>
2024-10-14 14:35:50 -0400

    Use venv for Python dependencies (macOS)
    Create a series of scripts to set up and manage a Python virtual
    environment that can be used to build (and test) Drake. Remove Python
    packages from macOS setup. Add dependencies to the virtual environment
    as needed. Adjust install test wrapper to use the virtual environment.
    Adjust macOS wheel builder to set up its own virtual environment only
    for additional wheel-only packages.
    
    Co-authored-by: Jeremy Nimmer <jeremy.nimmer@tri.global>

--- tools/jupyter/test/jupyter_notebook_test.py
+++ tools/jupyter/test/jupyter_notebook_test.py
@@ -1,9 +1,18 @@
 import subprocess
+import sys
 import unittest
+
+from python import runfiles
 
 
 class TestJupyterNotebook(unittest.TestCase):
     def test_help(self):
         """Ensures that `jupyter notebook` is installed (#12042)."""
-        status = subprocess.call(args=["jupyter", "notebook", "--help"])
+        manifest = runfiles.Create()
+        if sys.platform == "darwin":
+            jupyter = manifest.Rlocation("python/bin/jupyter")
+        else:
+            jupyter = "jupyter"
+
+        status = subprocess.call(args=[jupyter, "notebook", "--help"])
         self.assertEqual(status, 0)
