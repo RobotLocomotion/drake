53b289e6c8fff4d021eb29ea44ec0a9876b26351
Calder Phillips-Grafflin <calder.phillips-grafflin@tri.global>
2024-10-22 00:18:06 -0400

    Flesh out use of encapsulated voxel grid types

--- planning/dev/voxel_self_filter.cc
+++ planning/dev/voxel_self_filter.cc
@@ -5,7 +5,9 @@
 #include <Eigen/Geometry>
 #include <common_robotics_utilities/voxel_grid.hpp>
 
+#include "drake/planning/dev/voxel_collision_map_internal.h"
 #include "drake/planning/dev/voxel_self_filter_internal.h"
+#include "drake/planning/dev/voxel_tagged_object_collision_map_internal.h"
 
 namespace drake {
 namespace planning {
@@ -17,13 +19,15 @@ using voxelized_geometry_tools::TaggedObjectCollisionCell;
 void SelfFilter(const SphereRobotModelCollisionChecker& collision_checker,
                 const Eigen::VectorXd& q, const double padding,
                 const BodyIndex grid_body_index,
-                voxelized_geometry_tools::CollisionMap* const collision_map,
+                VoxelCollisionMap* const collision_map,
                 const Parallelism parallelism,
                 const std::optional<int> context_number) {
   DRAKE_THROW_UNLESS(collision_map != nullptr);
+  auto& internal_collision_map =
+      internal::GetMutableInternalCollisionMap(*collision_map);
   const CollisionCell empty_cell(0.0f, 0u);
   return internal::SelfFilter(collision_checker, q, padding, grid_body_index,
-                              collision_map, empty_cell, parallelism,
+                              &internal_collision_map, empty_cell, parallelism,
                               context_number);
 }
 
@@ -31,12 +35,14 @@ void SelfFilter(
     const SphereRobotModelCollisionChecker& collision_checker,
     const Eigen::VectorXd& q, const double padding,
     const BodyIndex grid_body_index,
-    voxelized_geometry_tools::TaggedObjectCollisionMap* const collision_map,
+    VoxelTaggedObjectCollisionMap* const collision_map,
     const Parallelism parallelism, const std::optional<int> context_number) {
   DRAKE_THROW_UNLESS(collision_map != nullptr);
+  auto& internal_collision_map =
+      internal::GetMutableInternalTaggedObjectCollisionMap(*collision_map);
   const TaggedObjectCollisionCell empty_cell(0.0f, 0u, 0u, 0u);
   return internal::SelfFilter(collision_checker, q, padding, grid_body_index,
-                              collision_map, empty_cell, parallelism,
+                              &internal_collision_map, empty_cell, parallelism,
                               context_number);
 }
 
