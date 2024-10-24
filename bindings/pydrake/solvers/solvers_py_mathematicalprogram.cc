ec1896ddd035191dcf1541ae82094e907def93d9
Alexandre Amice <43739209+AlexandreAmice@users.noreply.github.com>
2024-10-24 18:35:50 -0400

    Solve mathematical programs in parallel (#21957)
    Adds convenience methods for solving batches of MathematicalProgram in parallel.
    
    Co-authored-by: Jeremy Nimmer <jeremy.nimmer@tri.global>

--- bindings/pydrake/solvers/solvers_py_mathematicalprogram.cc
+++ bindings/pydrake/solvers/solvers_py_mathematicalprogram.cc
@@ -1682,7 +1682,78 @@ void BindFreeFunctions(py::module m) {
               const std::optional<SolverOptions>&>(&solvers::Solve),
           py::arg("prog"), py::arg("initial_guess") = py::none(),
           py::arg("solver_options") = py::none(), doc.Solve.doc_3args)
-      .def("GetProgramType", &solvers::GetProgramType, doc.GetProgramType.doc);
+      .def("GetProgramType", &solvers::GetProgramType, doc.GetProgramType.doc)
+      .def(
+          "SolveInParallel",
+          // The pybind11 infrastructure cannot handle setting a vector to null,
+          // nor having nulls inside of a vector. We must use a lambda signature
+          // where pointers are never null by adding `optional<>` decorations.
+          // Inside of the lambda we'll demote nullopts back to nullptrs. Note
+          // that SolverOptions is not necessarily cheap to copy, so we still
+          // carefully accept it by-pointer. The VectorXd is always necessarily
+          // copied when going form numpy to Eigen so we still pass it by-value.
+          [](std::vector<const MathematicalProgram*> progs,
+              std::optional<std::vector<std::optional<Eigen::VectorXd>>>
+                  initial_guesses,
+              std::optional<std::vector<std::optional<SolverOptions*>>>
+                  solver_options,
+              std::optional<std::vector<std::optional<SolverId>>> solver_ids,
+              const Parallelism& parallelism, bool dynamic_schedule) {
+            std::vector<const Eigen::VectorXd*> initial_guesses_ptrs;
+            if (initial_guesses.has_value()) {
+              initial_guesses_ptrs.reserve(initial_guesses->size());
+              for (const auto& guess : *initial_guesses) {
+                initial_guesses_ptrs.push_back(guess ? &(*guess) : nullptr);
+              }
+            }
+            std::vector<const SolverOptions*> solver_options_ptrs;
+            if (solver_options.has_value()) {
+              solver_options_ptrs.reserve(solver_options->size());
+              for (const auto& option : *solver_options) {
+                solver_options_ptrs.push_back(option ? *option : nullptr);
+              }
+            }
+            return solvers::SolveInParallel(progs,
+                initial_guesses.has_value() ? &initial_guesses_ptrs : nullptr,
+                solver_options.has_value() ? &solver_options_ptrs : nullptr,
+                solver_ids.has_value() ? &(*solver_ids) : nullptr, parallelism,
+                dynamic_schedule);
+          },
+          py::arg("progs"), py::arg("initial_guesses") = std::nullopt,
+          py::arg("solver_options") = std::nullopt,
+          py::arg("solver_ids") = std::nullopt,
+          py::arg("parallelism") = Parallelism::Max(),
+          py::arg("dynamic_schedule") = false,
+          py::call_guard<py::gil_scoped_release>(),
+          doc.SolveInParallel
+              .doc_6args_progs_initial_guesses_solver_options_solver_ids_parallelism_dynamic_schedule)
+      .def(
+          "SolveInParallel",
+          [](std::vector<const MathematicalProgram*> progs,
+              std::optional<std::vector<std::optional<Eigen::VectorXd>>>
+                  initial_guesses,
+              const SolverOptions* solver_options,
+              const std::optional<SolverId>& solver_id,
+              const Parallelism& parallelism, bool dynamic_schedule) {
+            std::vector<const Eigen::VectorXd*> initial_guesses_ptrs;
+            if (initial_guesses.has_value()) {
+              initial_guesses_ptrs.reserve(initial_guesses->size());
+              for (const auto& guess : *initial_guesses) {
+                initial_guesses_ptrs.push_back(guess ? &(*guess) : nullptr);
+              }
+            }
+            return solvers::SolveInParallel(progs,
+                initial_guesses.has_value() ? &initial_guesses_ptrs : nullptr,
+                solver_options, solver_id, parallelism, dynamic_schedule);
+          },
+          py::arg("progs"), py::arg("initial_guesses") = std::nullopt,
+          py::arg("solver_options") = std::nullopt,
+          py::arg("solver_id") = std::nullopt,
+          py::arg("parallelism") = Parallelism::Max(),
+          py::arg("dynamic_schedule") = false,
+          py::call_guard<py::gil_scoped_release>(),
+          doc.SolveInParallel
+              .doc_6args_progs_initial_guesses_solver_options_solver_id_parallelism_dynamic_schedule);
 }
 
 }  // namespace
--- solvers/solve.h
+++ solvers/solve.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 
+#include "drake/common/parallelism.h"
 #include "drake/solvers/mathematical_program.h"
 #include "drake/solvers/mathematical_program_result.h"
 #include "drake/solvers/solver_base.h"
@@ -42,5 +43,66 @@ MathematicalProgramResult Solve(
     const Eigen::Ref<const Eigen::VectorXd>& initial_guess);
 
 MathematicalProgramResult Solve(const MathematicalProgram& prog);
+
+/**
+ * Solves progs[i] into result[i], optionally using initial_guess[i] and
+ * solver_options[i] if given, by invoking the solver at solver_ids[i] if
+ * provided.  If solver_ids[i] is nullopt then the best available solver is
+ * selected for progs[i] depending on the availability of
+ * the solver and the problem formulation. If solver_ids == nullptr then this is
+ * done for every progs[i].
+ *
+ * Uses at most parallelism cores, with static scheduling by default.
+ *
+ * @param dynamic_schedule If dynamic_schedule is false then static scheduling
+ * is used and so each core will solve approximately 1/parallelism of the
+ * programs. This is most efficient when all the programs take approximately the
+ * same amount of time to solve. If dynamic_schedule is true, then dynamic
+ * scheduling is used and all the programs are queued into a single pool and
+ * each core will take the next program off the queue when it becomes available.
+ * This is best when each program takes a dramatically different amount of time
+ * to solve.
+ *
+ * @note When using a proprietary solver (e.g. Mosek) your organization may have
+ * limited license seats. It is recommended that the number of parallel solves
+ * does not exceed the total number of license seats.
+ *
+ * @note Only programs which are thread safe are solved concurrently. Programs
+ * that are not thread safe will be solved sequentially in a thread safe manner.
+ *
+ * @throws std::exception if initial_guess and solver_options are provided and
+ * not the same size as progs.
+ *
+ * @throws std::exception if any of the progs are nullptr.
+ *
+ * @throws std::exception if any of the programs cannot be solved.
+ */
+std::vector<MathematicalProgramResult> SolveInParallel(
+    const std::vector<const MathematicalProgram*>& progs,
+    const std::vector<const Eigen::VectorXd*>* initial_guesses,
+    const std::vector<const SolverOptions*>* solver_options,
+    const std::vector<std::optional<SolverId>>* solver_ids,
+    Parallelism parallelism = Parallelism::Max(),
+    bool dynamic_schedule = false);
+
+/**
+ * Provides the same functionality as SolveInParallel, but allows for specifying
+ * a single solver id and solver option that is used when solving all programs.
+ *
+ * @throws std::exception if the provided solver cannot solve all of progs.
+ *
+ * @throws std::exception if initial_guesses are provided and not the same
+ * size as progs.
+ *
+ * @throws std::exception if any of the progs are nullptr.
+ */
+std::vector<MathematicalProgramResult> SolveInParallel(
+    const std::vector<const MathematicalProgram*>& progs,
+    const std::vector<const Eigen::VectorXd*>* initial_guesses = nullptr,
+    const SolverOptions* solver_options = nullptr,
+    const std::optional<SolverId>& solver_id = std::nullopt,
+    Parallelism parallelism = Parallelism::Max(),
+    bool dynamic_schedule = false);
+
 }  // namespace solvers
 }  // namespace drake
