e23794644ce89154fb9bb69ca5f5dc8723df1078
Rick Poyner <rick.poyner@tri.global>
2024-11-01 12:08:58 -0400

    wip: approximate annotations for geometry/

--- bindings/pydrake/geometry/geometry_py_visualizers.cc
+++ bindings/pydrake/geometry/geometry_py_visualizers.cc
@@ -3,6 +3,7 @@
 
 #include "drake/bindings/pydrake/common/default_scalars_pybind.h"
 #include "drake/bindings/pydrake/common/deprecation_pybind.h"
+#include "drake/bindings/pydrake/common/ref_cycle_pybind.h"
 #include "drake/bindings/pydrake/common/serialize_pybind.h"
 #include "drake/bindings/pydrake/common/type_pack.h"
 #include "drake/bindings/pydrake/common/type_safe_index_pybind.h"
@@ -54,9 +55,9 @@ void DefineDrakeVisualizer(py::module m, T) {
             py::arg("lcm") = nullptr,
             py::arg("params") = DrakeVisualizerParams{},
             // Keep alive, ownership: `return` keeps `builder` alive.
-            py::keep_alive<0, 1>(),
+            internal::ref_cycle<0, 1>(),
             // Keep alive, reference: `builder` keeps `lcm` alive.
-            py::keep_alive<1, 3>(), py_rvp::reference,
+            internal::ref_cycle<1, 3>(), py_rvp::reference,
             cls_doc.AddToBuilder.doc_4args_builder_scene_graph_lcm_params)
         .def_static("AddToBuilder",
             py::overload_cast<systems::DiagramBuilder<T>*,
@@ -66,9 +67,9 @@ void DefineDrakeVisualizer(py::module m, T) {
             py::arg("lcm") = nullptr,
             py::arg("params") = DrakeVisualizerParams{},
             // Keep alive, ownership: `return` keeps `builder` alive.
-            py::keep_alive<0, 1>(),
+            internal::ref_cycle<0, 1>(),
             // Keep alive, reference: `builder` keeps `lcm` alive.
-            py::keep_alive<1, 3>(), py_rvp::reference,
+            internal::ref_cycle<1, 3>(), py_rvp::reference,
             cls_doc.AddToBuilder.doc_4args_builder_query_object_port_lcm_params)
         .def_static("DispatchLoadMessage",
             &DrakeVisualizer<T>::DispatchLoadMessage, py::arg("scene_graph"),
@@ -136,7 +137,7 @@ void DefineMeshcatVisualizer(py::module m, T) {
             py::arg("builder"), py::arg("scene_graph"), py::arg("meshcat"),
             py::arg("params") = MeshcatVisualizerParams{},
             // Keep alive, ownership: `return` keeps `builder` alive.
-            py::keep_alive<0, 1>(),
+            internal::ref_cycle<0, 1>(),
             // `meshcat` is a shared_ptr, so does not need a keep_alive.
             py_rvp::reference,
             cls_doc.AddToBuilder.doc_4args_builder_scene_graph_meshcat_params)
@@ -147,7 +148,7 @@ void DefineMeshcatVisualizer(py::module m, T) {
             py::arg("builder"), py::arg("query_object_port"),
             py::arg("meshcat"), py::arg("params") = MeshcatVisualizerParams{},
             // Keep alive, ownership: `return` keeps `builder` alive.
-            py::keep_alive<0, 1>(),
+            internal::ref_cycle<0, 1>(),
             // `meshcat` is a shared_ptr, so does not need a keep_alive.
             py_rvp::reference,
             cls_doc.AddToBuilder
