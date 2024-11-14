3eeacf5d44810ee3135fd31587fc1709bdd8b848
Rick Poyner (rico) <rick.poyner@tri.global>
2024-11-14 12:06:23 -0500

    [common] Work around linker error with clang 16+ (#22172)
    This patch avoids linker errors seen with clang 16 through 18 seen on
    both macos and ubuntu noble. Somehow, without this patch, the compiler
    emits undefined symbols containing the Drake-only using-statement type
    `MatrixX`.

--- common/eigen_types.h
+++ common/eigen_types.h
@@ -19,6 +19,7 @@ static_assert(EIGEN_VERSION_AT_LEAST(3, 3, 5),
 #include "drake/common/constants.h"
 #include "drake/common/drake_assert.h"
 #include "drake/common/drake_copyable.h"
+#include "drake/common/fmt_eigen.h"  // Clang-16 workaround; see #22061.
 
 namespace drake {
 
