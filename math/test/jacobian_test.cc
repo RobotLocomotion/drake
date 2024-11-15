7225e8cce68bc11ed896c15c410d0f34c17c2fa3
Rick Poyner <rick.poyner@tri.global>
2024-11-14 23:18:37 -0500

    [math] Remove obsolete compiler workaround

--- math/test/jacobian_test.cc
+++ math/test/jacobian_test.cc
@@ -31,12 +31,7 @@ TEST_F(AutodiffJacobianTest, QuadraticForm) {
   Matrix3d A;
   FillWithNumbersIncreasingFromZero(A);
 
-  // Work around GCC 5.4 Wshadow bug; the bug is fixed as of GCC 6.1.
-  // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=67273.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wshadow"
   auto quadratic_form = [&](const auto& x) {
-#pragma GCC diagnostic pop
     using Scalar = typename std::remove_reference_t<decltype(x)>::Scalar;
     return (x.transpose() * A.cast<Scalar>().eval() * x).eval();
   };
@@ -102,12 +97,7 @@ TEST_F(AutoDiffHessianTest, QuadraticFunction) {
   FillWithNumbersIncreasingFromZero(D);
   FillWithNumbersIncreasingFromZero(e);
 
-  // Work around GCC 5.4 Wshadow bug; the bug is fixed as of GCC 6.1.
-  // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=67273.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wshadow"
   auto quadratic_function = [&](const auto& x) {
-#pragma GCC diagnostic pop
     using Scalar = typename std::remove_reference_t<decltype(x)>::Scalar;
     return ((A.cast<Scalar>() * x + b.cast<Scalar>()).transpose() *
             C.cast<Scalar>() * (D.cast<Scalar>() * x + e.cast<Scalar>()))
