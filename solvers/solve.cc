f3840757396973938d278769620cbeb91c80433d
amice <amice@mit.edu>
2024-11-21 14:42:34 -0500

    first draft

--- solvers/solve.cc
+++ solvers/solve.cc
@@ -61,121 +61,48 @@ std::vector<MathematicalProgramResult> SolveInParallel(
     DRAKE_THROW_UNLESS(solver_ids->size() == progs.size());
   }
 
-  // Pre-allocate the results (i.e., don't use push_back) so that we can safely
-  // write to the vector on multiple threads concurrently.
-  std::vector<MathematicalProgramResult> results{progs.size()};
+  // TODO(Alexandre.Amice) is there a way around this clone?
+  auto prog_generator = [&progs](const int thread_num, const int64_t i) {
+    unused(thread_num);
+    return progs[i]->Clone();
+  };
 
-  // Track which results are set during the par-for loop. Some programs are not
-  // thread safe so will be skipped inside the loop, and we'll need to circle
-  // back and solve them serially later. (N.B. we cannot use vector<bool> here
-  // because it's not thread safe.)
-  std::vector<uint8_t> result_is_populated(progs.size(), 0);
-
-  // As unique types of solvers are encountered by the threads, we will cache
-  // the solvers into this data structure so that we don't have to create and
-  // destroy them at every call to Solve.
-  std::vector<std::unordered_map<SolverId, std::unique_ptr<SolverInterface>>>
-      solvers(parallelism.num_threads());
-
-  // The worker lambda behaves slightly differently depending on whether we are
-  // in the par-for loop or in the single-threaded cleanup pass later on.
-  // This is the worker callback for the i'th program.
-  auto solve_ith = [&](const bool in_parallel, const int thread_num,
-                       const int64_t i) {
-    // If this program is not thread safe, then skip it and save it for later.
-    if (in_parallel && !progs[i]->IsThreadSafe()) {
-      return;
-    }
-
-    // Access (or choose) the required solver.
-    const SolverId solver_id =
-        ((solver_ids != nullptr) && (*solver_ids)[i].has_value())
-            ? *((*solver_ids)[i])
-            : ChooseBestSolver(*progs[i]);
-
-    // IPOPT's MUMPs linear solver is not thread-safe. Therefore, if we are
-    // operating in parallel we need to skip this program.
-    // TODO(#21476) Revisit this logic after we have SPRAL available.
-    if (in_parallel && solver_id == IpoptSolver::id()) {
-      static const logging::Warn log_once(
-          "IpoptSolver cannot currently solve programs in parallel, so will be "
-          "run serially. Consider specifying a different solver, e.g., Nlopt.");
-      return;
-    }
-
-    // Find (or create) the specified solver.
-    auto solver_iter = solvers[thread_num].find(solver_id);
-    if (solver_iter == solvers[thread_num].end()) {
-      // If this thread has not solved this type of program yet, save the solver
-      // for use later.
-      solver_iter = solvers[thread_num].emplace_hint(solver_iter, solver_id,
-                                                     MakeSolver(solver_id));
-    }
-    const SolverInterface& solver = *(solver_iter->second);
-
-    // Adjust the solver options to obey `parallelism`. If this solve is part of
-    // the par-for, then the solver is only allowed one thread; otherwise
-    // (during the serial cleanup pass) it is allowed `parallelism`. Note that
-    // if the user set a solver-specific options to use more threads, that could
-    // cause it to exceed its limit.
-    std::optional<SolverOptions> new_options;
-    if ((solver_options != nullptr) && ((*solver_options)[i] != nullptr)) {
-      // TODO(jwnimmer-tri) The SolverBase should offer an overload to take a
-      // list of options, so that we can avoid this copying.
-      new_options.emplace(*((*solver_options)[i]));
+  auto initial_guess_generator =
+      [&initial_guesses](int64_t thread_num,
+                         int64_t i) -> std::optional<Eigen::VectorXd> {
+    unused(thread_num);
+    if (initial_guesses != nullptr && initial_guesses->at(i) != nullptr) {
+      return *(initial_guesses->at(i));
     } else {
-      new_options.emplace();
+      return std::nullopt;
     }
-    new_options->SetOption(CommonSolverOption::kMaxThreads,
-                           in_parallel ? 1 : parallelism.num_threads());
-
-    // Convert the initial guess from nullable to optional.
-    // TODO(jwnimmer-tri) The SolverBase should offer a nullable overload so
-    // that we can avoid this copying.
-    std::optional<Eigen::VectorXd> initial_guess;
-    if ((initial_guesses != nullptr) && ((*initial_guesses)[i] != nullptr)) {
-      initial_guess = *((*initial_guesses)[i]);
-    }
-
-    // Solve the program.
-    solver.Solve(*(progs[i]), initial_guess, new_options, &(results[i]));
-    result_is_populated[i] = true;
-  };
-  const auto solve_ith_parallel = [&](const int thread_num, const int64_t i) {
-    solve_ith(/* in parallel */ true, thread_num, i);
   };
 
-  const auto solve_ith_serial = [&](const int64_t i) {
-    solve_ith(/* in parallel */ false, /* thread_num */ 0, i);
-  };
-
-  // Call solve_ith in parallel for all of the progs if more than one thread is
-  // available.
-  if (parallelism.num_threads() > 1) {
-    if (dynamic_schedule) {
-      DynamicParallelForIndexLoop(
-          DegreeOfParallelism(parallelism.num_threads()), 0, ssize(progs),
-          solve_ith_parallel, ParallelForBackend::BEST_AVAILABLE);
+  auto solver_options_generator =
+      [&solver_options](int64_t thread_num,
+                        int64_t i) -> std::optional<SolverOptions> {
+    unused(thread_num);
+    if (solver_options != nullptr) {
+      return *(solver_options->at(i));
     } else {
-      StaticParallelForIndexLoop(DegreeOfParallelism(parallelism.num_threads()),
-                                 0, ssize(progs), solve_ith_parallel,
-                                 ParallelForBackend::BEST_AVAILABLE);
+      return std::nullopt;
     }
-  }
+  };
 
-  // De-allocate the solvers cache except for the first worker. (We'll use the
-  // first worker's cache for the serial solves, below.) The clearing is
-  // important in case the solvers hold onto scarce resources (e.g., licenses).
-  solvers.resize(1);
-
-  // Finish solving the programs that couldn't be solved in parallel.
-  for (size_t i = 0; i < progs.size(); ++i) {
-    if (!result_is_populated[i]) {
-      solve_ith_serial(i);
+  auto solver_ids_generator =
+      [&solver_ids](int64_t thread_num, int64_t i) -> std::optional<SolverId> {
+    unused(thread_num);
+    if (solver_ids != nullptr && solver_ids->at(i).has_value()) {
+      return solver_ids->at(i);
+    } else {
+      return std::nullopt;
     }
-  }
+  };
 
-  return results;
+  return SolveInParallel(
+      prog_generator, initial_guess_generator, solver_options_generator,
+      solver_ids_generator, static_cast<int64_t>(0),
+      static_cast<int64_t>(progs.size()), parallelism, dynamic_schedule);
 }
 
 std::vector<MathematicalProgramResult> SolveInParallel(
@@ -199,6 +126,139 @@ std::vector<MathematicalProgramResult> SolveInParallel(
       broadcast_options.has_value() ? &(*broadcast_options) : nullptr,
       broadcast_ids.has_value() ? &(*broadcast_ids) : nullptr,  // BR
       parallelism, dynamic_schedule);
+}
+
+std::vector<MathematicalProgramResult> SolveInParallel(
+    const std::function<std::unique_ptr<MathematicalProgram>(int64_t, int64_t)>&
+        prog_generator,
+    const std::function<std::optional<Eigen::VectorXd>(int64_t, int64_t)>&
+        initial_guesses_generator,
+    const std::function<std::optional<SolverOptions>(int64_t, int64_t)>&
+        solver_options_generator,
+    const std::function<std::optional<SolverId>(int64_t, int64_t)>&
+        solver_ids_generator,
+    const int64_t range_start, const int64_t range_end, Parallelism parallelism,
+    bool dynamic_schedule) {
+  // Pre-allocate the results (i.e., don't use push_back) so that we can safely
+  // write to the vector on multiple threads concurrently.
+  std::vector<MathematicalProgramResult> results{
+      static_cast<std::size_t>(range_end)};
+
+  // Track which results are set during the par-for loop. Some programs are not
+  // thread safe so will be skipped inside the loop, and we'll need to circle
+  // back and solve them serially later. (N.B. we cannot use vector<bool> here
+  // because it's not thread safe.)
+  std::vector<uint8_t> result_is_populated(range_end, 0);
+
+  // As unique types of solvers are encountered by the threads, we will cache
+  // the solvers into this data structure so that we don't have to create and
+  // destroy them at every call to Solve.
+  std::vector<std::unordered_map<SolverId, std::unique_ptr<SolverInterface>>>
+      solvers(parallelism.num_threads());
+
+  // This caches programs which are skipped in the parallel for loop because
+  // they are not threadsafe. This avoids having to create the program twice.
+  std::vector<std::unique_ptr<MathematicalProgram>> cached_progs(
+      static_cast<std::size_t>(range_end));
+
+  // The worker lambda behaves slightly differently depending on whether we are
+  // in the par-for loop or in the single-threaded cleanup pass later on.
+  // This is the worker callback for the i'th program.
+  auto solve_ith = [&](const bool in_parallel, const int thread_num,
+                       const int64_t i) {
+    std::unique_ptr<MathematicalProgram> prog =
+        cached_progs[i] == nullptr ? prog_generator(thread_num, i)
+                                   : std::move(cached_progs[i]);
+    // If this program is not thread safe, then skip it and save it for
+    // later.
+    if (in_parallel && !prog->IsThreadSafe()) {
+      return;
+    }
+
+    // Access (or choose) the required solver.
+    std::optional<SolverId> solver_id = solver_ids_generator(thread_num, i);
+    if (!solver_id.has_value()) {
+      solver_id = ChooseBestSolver(*prog);
+    }
+
+    // IPOPT's MUMPs linear solver is not thread-safe. Therefore, if we are
+    // operating in parallel we need to skip this program.
+    // TODO(#21476) Revisit this logic after we have SPRAL available.
+    if (in_parallel && solver_id == IpoptSolver::id()) {
+      static const logging::Warn log_once(
+          "IpoptSolver cannot currently solve programs in parallel, so will be "
+          "run serially. Consider specifying a different solver, e.g., Nlopt.");
+      return;
+    }
+
+    // Find (or create) the specified solver.
+    auto solver_iter = solvers[thread_num].find(solver_id.value());
+    if (solver_iter == solvers[thread_num].end()) {
+      // If this thread has not solved this type of program yet, save the solver
+      // for use later.
+      solver_iter = solvers[thread_num].emplace_hint(
+          solver_iter, solver_id.value(), MakeSolver(solver_id.value()));
+    }
+    const SolverInterface& solver = *(solver_iter->second);
+
+    // Adjust the solver options to obey `parallelism`. If this solve is part of
+    // the par-for, then the solver is only allowed one thread; otherwise
+    // (during the serial cleanup pass) it is allowed `parallelism`. Note that
+    // if the user set a solver-specific options to use more threads, that could
+    // cause it to exceed its limit.
+    std::optional<SolverOptions> new_options =
+        solver_options_generator(thread_num, i);
+    if (!new_options.has_value()) {
+      new_options.emplace();
+    }
+    new_options->SetOption(CommonSolverOption::kMaxThreads,
+                           in_parallel ? 1 : parallelism.num_threads());
+
+    // Convert the initial guess from nullable to optional.
+    // TODO(jwnimmer-tri) The SolverBase should offer a nullable overload so
+    // that we can avoid this copying.
+    std::optional<Eigen::VectorXd> initial_guess =
+        initial_guesses_generator(thread_num, i);
+
+    // Solve the program.
+    solver.Solve(*prog, initial_guess, new_options, &(results[i]));
+    result_is_populated[i] = true;
+  };
+  const auto solve_ith_parallel = [&](const int thread_num, const int64_t i) {
+    solve_ith(/* in parallel */ true, thread_num, i);
+  };
+
+  const auto solve_ith_serial = [&](const int64_t i) {
+    solve_ith(/* in parallel */ false, /* thread_num */ 0, i);
+  };
+
+  // Call solve_ith in parallel for all of the progs if more than one thread is
+  // available.
+  if (parallelism.num_threads() > 1) {
+    if (dynamic_schedule) {
+      DynamicParallelForIndexLoop(
+          DegreeOfParallelism(parallelism.num_threads()), range_start,
+          range_end, solve_ith_parallel, ParallelForBackend::BEST_AVAILABLE);
+    } else {
+      StaticParallelForIndexLoop(DegreeOfParallelism(parallelism.num_threads()),
+                                 range_start, range_end, solve_ith_parallel,
+                                 ParallelForBackend::BEST_AVAILABLE);
+    }
+  }
+
+  // De-allocate the solvers cache except for the first worker. (We'll use the
+  // first worker's cache for the serial solves, below.) The clearing is
+  // important in case the solvers hold onto scarce resources (e.g., licenses).
+  solvers.resize(1);
+
+  // Finish solving the programs that couldn't be solved in parallel.
+  for (int64_t i = range_start; i < range_end; ++i) {
+    if (!result_is_populated[i]) {
+      solve_ith_serial(i);
+    }
+  }
+
+  return results;
 }
 
 }  // namespace solvers
