22d9f962987d89cea38e883f6e2fd76dbe8d31e8
Rick Poyner <rick.poyner@tri.global>
2024-11-21 17:08:20 -0500

    [setup] Add Xcode 16 specific build configuration
    Turn off a new optimization for dynamic_cast that is incompatible with
    loading multiple shared libraries into a python process. Limit the new
    option to clang versions that support it.

--- setup/mac/source_distribution/install_prereqs_user_environment.sh
+++ setup/mac/source_distribution/install_prereqs_user_environment.sh
@@ -16,12 +16,19 @@ fi
 workspace_dir="$(cd "$(dirname "${BASH_SOURCE}")/../../.." && pwd)"
 bazelrc="${workspace_dir}/gen/environment.bazelrc"
 arch="$(/usr/bin/arch)"
+clang_version="$(clang --version |head -n1 |sed 's#.*(clang-\([0-9]*\)\..*#\1#g')"
 
 mkdir -p "$(dirname "${bazelrc}")"
 cat > "${bazelrc}" <<EOF
 import %workspace%/tools/macos.bazelrc
 import %workspace%/tools/macos-arch-${arch}.bazelrc
 EOF
+
+if (( $clang_version >= 1600 )); then
+cat >> "${bazelrc}" <<EOF
+import %workspace%/tools/apple-clang-1600.bazelrc
+EOF
+fi
 
 # Prefetch the bazelisk download of bazel.
 # This is especially helpful for the "Provisioned" images in CI.
