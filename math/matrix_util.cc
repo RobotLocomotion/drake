e8b46d9ce50aa3618e6775514ccaeea16febf6e3
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-12-04 11:12:08 -0800

    [math] Fix GeneratePythonCsc to eschew C++ streams

--- math/matrix_util.cc
+++ math/matrix_util.cc
@@ -1,14 +1,17 @@
 #include "drake/math/matrix_util.h"
 
+#include <sstream>
+
 #include <fmt/format.h>
 
 namespace drake {
 namespace math {
-void GeneratePythonCsc(const Eigen::SparseMatrix<double>& mat,
-                       std::string_view name, std::ostream* python_stream) {
+std::string GeneratePythonCsc(const Eigen::SparseMatrix<double>& mat,
+                              std::string_view name) {
+  std::stringstream python_stream;
   if (mat.nonZeros() == 0) {
-    *python_stream << fmt::format("{} = sparse.csc_matrix(({}, {}))\n", name,
-                                  mat.rows(), mat.cols());
+    python_stream << fmt::format("{} = sparse.csc_matrix(({}, {}))\n", name,
+                                 mat.rows(), mat.cols());
   } else {
     std::vector<double> data;
     std::vector<int> rows;
@@ -24,12 +27,13 @@ void GeneratePythonCsc(const Eigen::SparseMatrix<double>& mat,
       }
     }
 
-    *python_stream << fmt::format(
+    python_stream << fmt::format(
         R"""({} = sparse.csc_matrix((np.array([{}], dtype=np.float64), ([{}], [{}])), shape=({}, {}))
 )""",
         name, fmt::join(data, ", "), fmt::join(rows, ", "),
         fmt::join(cols, ", "), mat.rows(), mat.cols());
   }
+  return python_stream.str();
 }
 }  // namespace math
 }  // namespace drake
