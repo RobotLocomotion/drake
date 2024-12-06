caf44054ee9e742a07648b66a8778920127de479
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-12-06 13:08:12 -0800

    PR [build] Support bzlmod for building and installing
    See https://bazel.build/external/migration for background.
    
    Note that this commit does not pass all tests when bzlmod is enabled;
    doing so requires enhancements to our runfiles handling, so for now
    bzlmod is still disabled by default (except for CMake installs).
    
    Details:
    - Add MODULE.bazel and WORKSPACE.bzlmod for bzlmod use.
      - Note that WORKSPACE.bzlmod is a copy of WORKSPACE with a tweaked
        overview comment and a single functional change to our bzlmod flag.
      - Fix latent bug in repository_excludes for rules_python.
    - Adjust install rule so installed paths are "drake" not "_main".
    - Disable drake_py add_test_rule pyc files (for newer rules_python).
    - Tweak local_config_cc spelling again for bzlmod.
    - Add linter for module<->workspace syncing.
    - Turn on bzlmod for CMake to achieve test coverage in CI.

---
+++ tools/workspace/workspace_bzlmod_sync_test.py
@@ -0,0 +1,138 @@
+import unittest
+from pathlib import Path
+
+from python import runfiles
+
+
+class TestWorkspaceBzlmodSync(unittest.TestCase):
+
+    def _read(self, respath):
+        """Returns the contents of the given resource path."""
+        manifest = runfiles.Create()
+        path = Path(manifest.Rlocation(respath))
+        return path.read_text(encoding="utf-8")
+
+    def _parse_modules(self, content):
+        """Given the contents of MODULE.bazel, returns a dictionary mapping
+        from module_name to module_version.
+        """
+        result = {}
+        for line in content.splitlines():
+            # Only match bazel_dep lines.
+            if not line.startswith("bazel_dep"):
+                continue
+            # Grab what's inside the parens.
+            _, line = line.split("(")
+            line, _ = line.split(")")
+            # Parse out the kwargs.
+            kwargs = {}
+            for item in line.split(","):
+                name, value = item.split(" = ")
+                kwargs[name.strip()] = value.strip().replace('"', '')
+            result[kwargs["name"]] = kwargs["version"]
+        return result
+
+    def _parse_repo_rule_version(self, content):
+        """Given the contents of a repository.bzl that calls 'github_archive',
+        returns the version number it pins to.
+        """
+        assert "github_archive" in content, content
+        for line in content.splitlines():
+            line = line.strip()
+            if not line.startswith("commit = "):
+                continue
+            _, version, _ = line.split('"')
+            return version
+        self.fail(f"No 'commit = ...' found in:\n{content}")
+
+    def test_version_sync(self):
+        """Some external version are independently listed in both MODULE.bazel
+        and WORKSPACE. This test ensures that the versions pinned in each file
+        are correctly synchronized.
+        """
+        modules = self._parse_modules(self._read(f"drake/MODULE.bazel"))
+
+        # Don't check modules that are known to be module-only.
+        del modules["rules_java"]
+
+        # Don't check module that are documented to purposefully skew versions.
+        del modules["rules_python"]
+
+        # Check that the module version matches the workspace version.
+        self.assertTrue(modules)
+        for name, module_version in modules.items():
+            workspace_version = self._parse_repo_rule_version(
+                self._read(f"drake/tools/workspace/{name}/repository.bzl"))
+            self.assertEqual(workspace_version, module_version)
+
+    def _parse_workspace_already_provided(self, content):
+        """Given the contents of default.bzl, returns the list of
+        REPOS_ALREADY_PROVIDED_BY_BAZEL_MODULES.
+        """
+        result = None
+        for line in content.splitlines():
+            line = line.strip()
+            if line == "REPOS_ALREADY_PROVIDED_BY_BAZEL_MODULES = [":
+                result = list()
+                continue
+            if result is None:
+                # We haven't seen the REPOS_ALREADY_... line yet.
+                continue
+            if line == "]":
+                break
+            assert line.startswith('"'), line
+            assert line.endswith('",'), line
+            result.append(line[1:-2])
+        assert result, content
+        return sorted(result)
+
+    def test_default_exclude_sync(self):
+        """Our default.bzl needs to know the list of modules that are already
+        provided by MODULE.bazel. This test ensures that the list is correctly
+        synchronized.
+        """
+        modules = self._parse_modules(self._read(f"drake/MODULE.bazel"))
+
+        # Don't check modules that are known to be module-only.
+        del modules["rules_java"]
+
+        # Check that default.bzl's constant matches the inventory of modules.
+        module_names = sorted(modules.keys())
+        self.assertEqual(module_names, self._parse_workspace_already_provided(
+            self._read("drake/tools/workspace/default.bzl")))
+
+    def _canonicalize_workspace(self, content):
+        """Given the contents of WORKSPACE or WORKSPACE.bzlmod, returns a
+        modified copy that:
+        - strips away comments and blank lines, and
+        - fuzzes out the `bzlmod = ...` attribute.
+        """
+        needle1 = "add_default_workspace(bzlmod = False)"
+        needle2 = "add_default_workspace(bzlmod = True)"
+        replacement = "add_default_workspace(bzlmod = ...)"
+        result = []
+        for line in content.splitlines():
+            if "#" in line:
+                line, _ = line.split("#", maxsplit=1)
+            line = line.strip()
+            if not line:
+                continue
+            if line in (needle1, needle2):
+                line = replacement
+            result.append(line)
+        return "\n".join(result) + "\n"
+
+    def test_workspace_copies(self):
+        """Checks that our WORKSPACE and WORKSPACE.bzlmod are identical,
+        modulo comments and the `bzlmod = ...` attribute.
+        """
+        workspace1 = self._canonicalize_workspace(
+            self._read(f"drake/WORKSPACE"))
+        workspace2 = self._canonicalize_workspace(
+            self._read(f"drake/WORKSPACE.bzlmod"))
+        self.maxDiff = None
+        self.assertMultiLineEqual(workspace1, workspace2)
+
+
+assert __name__ == '__main__'
+unittest.main()
