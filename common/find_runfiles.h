e176239e8014d26bc32c9f72bf5817cb95384c8d
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-12-19 10:22:13 -0800

    [build] Use bzlmod for development and testing
    In other words, Drake Developers will now be using bzlmod by default.
    (CMake installs were already using it.)
    
    Users who consume Drake as a bazel external must still use workspace
    mode (no bzlmod). Even though Drake now uses bzlmod for developers,
    it is not yet possible for downstream projects to consume Drake as a
    bzlmod module; that remains future work.
    
    The main change required for bzlmod here is that our module name in
    runfiles is "_main" now instead of "drake", which mostly affects some
    linters and tests that were hard-coded. More generally, there is now a
    concept of "repo mapping" where module names in a MODULE.bazel file are
    projected to unique names in runfiles, to allow for multiple copies of
    modules to co-exist. This means that a runfiles lookup must not only
    specify the path it's looking for, but from _whose point of view_ that
    path is coming from.
    
    * common: Adjust FindRunfile to know about source_repository. When
      none is given and we're seeking a drake runfile, supply the right
      "point of view" string automatically.
    
    * resource_tool_test: Loosen test condition to be only the file
      basename.
    
    * unittest_main: Adjust our source file scan to allow for either
      the bzlmod or workspace spelling of the test program. (The old
      workspace spelling would only be used by downstream projects.)
    
    * tools: Adjust find_all_sources scan for .bazelproject to check
      against the realpath instead of the runfiles path.

--- common/find_runfiles.h
+++ common/find_runfiles.h
@@ -4,7 +4,7 @@
 
 /** @file
 This file contains helpers to work with Bazel-declared runfiles -- declared
-data dependencies used by C++ code.  The functions in this file only succeed
+data dependencies used by C++ code. The functions in this file only succeed
 when used within a Bazel build.
 
 All source code within Drake should use FindResource() or FindResourceOrThrow()
@@ -34,10 +34,33 @@ struct RlocationOrError {
   std::string error;
 };
 
-/** (Advanced.) Returns the absolute path to the given resource_path from Bazel
-runfiles, or else an error message when not found.  When HasRunfiles() is
-false, returns an error. The `resource_path` looks like
-`workspace/pkg/subpkg/file.ext`, e.g., "drake/common/foo.txt". */
-RlocationOrError FindRunfile(const std::string& resource_path);
+/** (Advanced.) Returns the absolute path to the given `resource_path` from
+Bazel runfiles, or else an error message when not found. When HasRunfiles()
+is false, returns an error.
+
+@note For Drake Developers, note that in an installed copy of Drake (e.g., in a
+binary release) the HasRunfiles() check will return `false`, so this function
+will return an error. That means it's generally ill-advised to call this
+function from library code inside Drake, or if you do you'll need to have a
+fallback plan in case of an error. Typically our library code should be calling
+FindResource() not FindRunfile(). On the other hand, for our private code like
+unit tests and benchmarks, it's fine to call this function and in those programs
+it's also fine to leave the source_repository defaulted (empty).
+
+@param resource_path The path to find, formulated as the repository name
+followed by package and filename, e.g., "repository/topdir/subdir/file.ext".
+Drake resource paths look like "drake/common/foo.txt".
+
+@param source_repository When looking up a Drake runfile, this value is ignored
+(and therefore may be set to anything). Otherwise, it should be set to the value
+of the preprocessor definition `BAZEL_CURRENT_REPOSITORY` or else when running
+with bzlmod enabled you risk using an incorrect repo_mapping. That preprocessor
+definition is Bazel magic that takes on _different values_ depending on which
+translation unit is being compiled (and is undefined unless you add Bazel's
+runfiles library to your own library's `deps = ...` directly), so cannot be
+used as the default value here. */
+RlocationOrError FindRunfile(
+    const std::string& resource_path,
+    const std::string& source_repository = {});
 
 }  // namespace drake
