b9ed548492f6367ed05cc175436ac5315ae4d371
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-12-21 15:44:22 -0800

    [pydrake] Remove some gratuitous uses of python repo constants
    Configuring the python version in a repository rule goes against best
    practices for supporting toolchains (and thus, bzlmod). This commit
    takes a bite out of the problem; future commits that adjust pybind
    rules will finish the job.

--- bindings/pydrake/multibody/run_installed_mesh_to_model.py
+++ bindings/pydrake/multibody/run_installed_mesh_to_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Runs fix_inertia from an install tree.
+Runs mesh_to_model from an install tree.
 """
 
 from os.path import isdir, join, dirname, realpath
@@ -12,11 +12,12 @@ def main():
     # Ensure that we can import pydrake, accommodating symlinks.
     prefix_dir = dirname(dirname(realpath(__file__)))
     assert isdir(join(prefix_dir, "bin")), f"Bad location: {prefix_dir}"
-    site_dir = join(prefix_dir, "@PYTHON_SITE_PACKAGES_RELPATH@")
+    version = f"{sys.version_info.major}.{sys.version_info.minor}"
+    site_dir = join(prefix_dir, f"lib/python{version}/site-packages")
     sys.path.insert(0, site_dir)
 
     # Execute the imported main.
-    from pydrake.multibody.fix_inertia import _main
+    from pydrake.multibody.mesh_to_model import _main
     _main()
 
 
