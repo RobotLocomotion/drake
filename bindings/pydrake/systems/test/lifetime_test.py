75c535cd402b44850927c9aa07df80bea48e6d22
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-12-26 07:54:02 -0800

    [systems] Add DiagramBuilder overloads for shared_ptr
    AddSystem and AddNamedSystem functions are overloaded to allow passing
    the system via mutable shared_ptr (in addition to mutable unique_ptr).
    
    Architectually, the diagram still maintains exclusive ownership of its
    children: each child still has a back-pointer to the Diagram (via the
    SystemParentServiceInterface) so it's not possible to actually "share"
    a subsystem across multiple diagrams. However, unique_ptr is overly
    restrictive: it not only denotes exclusive ownership (which is fine),
    but also demands that resource cleanup is `delete MyClass` (which is
    insufficient for our Python bindings). We must use a smart pointer
    type that allows customizing cleanup with runtime data (i.e., uses
    type erasure). We could imagine using unqiue_ptr whose Deleter was a
    std::function, but it's simpler to just use shared_ptr.
    
    Relatedly, we also adjust the Diagram subclasses PidControlledSystem
    and RgbdSensorDiscrete to accept the more general shared_ptr (instead
    of unique_ptr) for their child systems and to use ref_cycle for their
    pydrake bindings.

--- bindings/pydrake/systems/test/lifetime_test.py
+++ bindings/pydrake/systems/test/lifetime_test.py
@@ -71,15 +71,18 @@ class TestLifetime(unittest.TestCase):
     def test_ownership_multiple_containers(self):
         info = Info()
         system = DeleteListenerSystem(info.record_deletion)
+
+        # Add the system to a built diagram.
         builder_1 = DiagramBuilder()
-        builder_2 = DiagramBuilder()
         builder_1.AddSystem(system)
-        # This is tested in our fork of `pybind11`, but echoed here for when
-        # we decide to switch to use `shared_ptr`.
-        with self.assertRaises(RuntimeError):
-            # This should throw an error from `pybind11`, since two containers
-            # are trying to own a unique_ptr-held object.
+        diagram_1 = builder_1.Build()
+
+        # Add it again to another diagram. We don't care if the Add fails or
+        # the Build fails, so long as one of them does.
+        builder_2 = DiagramBuilder()
+        with self.assertRaisesRegex(Exception, "already.*different Diagram"):
             builder_2.AddSystem(system)
+            builder_2.Build()
 
     def test_ownership_simulator(self):
         info = Info()
