3e04809fb6685f80da790cdfb8d1b88b7aa0b813
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-01-05 21:33:47 -0800

    PR [pydrake] Fix yaml_dump_typed for Union on str

--- bindings/pydrake/common/yaml.py
+++ bindings/pydrake/common/yaml.py
@@ -121,14 +121,20 @@ class _SchemaDumper(yaml.dumper.SafeDumper):
         scalars are not emitted by pyyaml.)
         """
         value: typing.Union[bool, int, float, str]
+        schema: type  # One of either bool, int, float, or str.
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._explicit_tag_prefix = "tag:drake.mit.edu/explicit:"
+        self.DEFAULT_TAG_PREFIXES[self._explicit_tag_prefix] = "!!"
 
     def _represent_explicit_scalar(self, explicit_scalar):
         assert isinstance(explicit_scalar, _SchemaDumper.ExplicitScalar)
         value = explicit_scalar.value
+        schema = explicit_scalar.schema
+        assert schema in _PRIMITIVE_YAML_TYPES
         node = self.yaml_representers[type(value)](self, value)
-        # Encourage pyyaml to emit the secondary tag, e.g., `!!int`.
-        # This does not work for strings.
-        node.style = "'"
+        node.tag = f"{self._explicit_tag_prefix}{schema.__name__}"
         return node
 
     def _represent_dict(self, data):
@@ -665,7 +671,8 @@ def _yaml_dump_typed_item(*, obj, schema):
         result = _yaml_dump_typed_item(obj=obj, schema=union_schema)
         if i != 0:
             if union_schema in _PRIMITIVE_YAML_TYPES:
-                result = _SchemaDumper.ExplicitScalar(result)
+                result = _SchemaDumper.ExplicitScalar(
+                    value=result, schema=union_schema)
             else:
                 class_name_with_args = pretty_class_name(union_schema)
                 class_name = class_name_with_args.split("[", 1)[0]
