c13c2cc58b5e29ff2a0b53dadc66b41e8b6e719a
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-01-08 14:49:44 -0800

    [py trajectories] Adjust Clone and MakeDerivative for unique_ptr (#22399)
    Because we allow implementations of Trajectory as Python subclasses,
    we cannot assume that the deleter associated with a call to Clone is
    `delete MyClass`, so we now adjust our PyTrajectory override logic to
    wrap its return value in a WrappedTrajectory. The only reason the
    unique_ptr used to work is Drake's custom fork or pybind11 with evil
    hacks, which will be going away soon.
    
    We also now warn Python subclasses to implement the canonical spelling
    of the __deepcopy__ method, instead of overriding the public Clone
    method. (Overriding Clone was already documented as deprecated in a
    prior commit; this just adds the warning.)

--- bindings/pydrake/_trajectories_extra.py
+++ bindings/pydrake/_trajectories_extra.py
@@ -1,4 +1,7 @@
-from pydrake.common import _MangledName
+from pydrake.common import (
+    _MangledName,
+    pretty_class_name as _pretty_class_name,
+)
 
 
 def __getattr__(name):
@@ -7,3 +10,17 @@ def __getattr__(name):
     """
     return _MangledName.module_getattr(
         module_name=__name__, module_globals=globals(), name=name)
+
+
+def _wrapped_trajectory_repr(wrapped_trajectory):
+    cls = type(wrapped_trajectory)
+    return f"{_pretty_class_name(cls)}({wrapped_trajectory.unwrap()!r})"
+
+
+def _add_repr_functions():
+    for param in _WrappedTrajectory_.param_list:
+        cls = _WrappedTrajectory_[param]
+        setattr(cls, "__repr__", _wrapped_trajectory_repr)
+
+
+_add_repr_functions()
