0a5f9c568ed77914a45b42688709e894df59b29a
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-01-20 08:57:14 -0800

    [inverse_kinematics] Fix plant lifetime in AddMultibodyPlantConstraints

--- multibody/inverse_kinematics/test/add_multibody_plant_constraints_test.cc
+++ multibody/inverse_kinematics/test/add_multibody_plant_constraints_test.cc
@@ -25,41 +25,41 @@ bool SnoptSolverUnavailable() {
 class AddMultibodyPlantConstraintsTest : public ::testing::Test {
  public:
   void SetUp() override {
-    plant_.set_discrete_contact_approximation(
+    plant_->set_discrete_contact_approximation(
         DiscreteContactApproximation::kSap);
-    plant_.SetUseSampledOutputPorts(false);
+    plant_->SetUseSampledOutputPorts(false);
 
-    body_A_ = &plant_.AddRigidBody(
+    body_A_ = &plant_->AddRigidBody(
         "body_A", SpatialInertia<double>::SolidBoxWithMass(1, 1, 1, 1));
-    body_B_ = &plant_.AddRigidBody(
+    body_B_ = &plant_->AddRigidBody(
         "body_B", SpatialInertia<double>::SolidBoxWithMass(1, 1, 1, 1));
-    world_A_ = &plant_.AddJoint<RevoluteJoint>(
-        "world_A", plant_.world_body(), RigidTransformd(Vector3d(-1, 0, 0)),
+    world_A_ = &plant_->AddJoint<RevoluteJoint>(
+        "world_A", plant_->world_body(), RigidTransformd(Vector3d(-1, 0, 0)),
         *body_A_, RigidTransformd(), Vector3d::UnitZ());
     // Add and then immediately remove a joint so that the joint indices do not
     // correspond to the position indices.
-    plant_.RemoveJoint(plant_.AddJoint<RevoluteJoint>(
+    plant_->RemoveJoint(plant_->AddJoint<RevoluteJoint>(
         "temp", *body_A_, RigidTransformd(Vector3d(1, 0, 0)), *body_B_,
         RigidTransformd(), Vector3d::UnitZ()));
-    A_B_ = &plant_.AddJoint<RevoluteJoint>(
+    A_B_ = &plant_->AddJoint<RevoluteJoint>(
         "A_B", *body_A_, RigidTransformd(Vector3d(1, 0, 0)), *body_B_,
         RigidTransformd(), Vector3d::UnitZ());
   }
 
   void CheckConstraints(int expected_num_constraints = 2, double tol = 1e-6) {
-    if (!plant_.is_finalized()) {
-      plant_.Finalize();
+    if (!plant_->is_finalized()) {
+      plant_->Finalize();
     }
-    plant_context_ = plant_.CreateDefaultContext();
+    plant_context_ = plant_->CreateDefaultContext();
 
     // First demonstrate that these constraints work with MathematicalProgram,
     // and use them to find a valid initial condition.
     solvers::MathematicalProgram prog;
-    auto q = prog.NewContinuousVariables(plant_.num_positions());
-    VectorXd q0 = VectorXd::LinSpaced(plant_.num_positions(), 0, 1);
+    auto q = prog.NewContinuousVariables(plant_->num_positions());
+    VectorXd q0 = VectorXd::LinSpaced(plant_->num_positions(), 0, 1);
     prog.AddQuadraticErrorCost(
-        MatrixXd::Identity(plant_.num_positions(), plant_.num_positions()), q0,
-        q);
+        MatrixXd::Identity(plant_->num_positions(), plant_->num_positions()),
+        q0, q);
 
     auto constraints =
         AddMultibodyPlantConstraints(plant_, q, &prog, plant_context_.get());
@@ -69,17 +69,18 @@ class AddMultibodyPlantConstraintsTest : public ::testing::Test {
 
     // Now step the dynamics forward and verify that the constraint stays
     // satisfied.
-    plant_.SetPositions(plant_context_.get(), result.GetSolution(q));
-    VectorXd qn = plant_.EvalUniquePeriodicDiscreteUpdate(*plant_context_)
+    plant_->SetPositions(plant_context_.get(), result.GetSolution(q));
+    VectorXd qn = plant_->EvalUniquePeriodicDiscreteUpdate(*plant_context_)
                       .value()
-                      .head(plant_.num_positions());
+                      .head(plant_->num_positions());
 
     prog.SetInitialGuessForAllVariables(qn);
     EXPECT_TRUE(prog.CheckSatisfiedAtInitialGuess(constraints, tol));
   }
 
  protected:
-  MultibodyPlant<double> plant_{0.1};
+  std::shared_ptr<MultibodyPlant<double>> plant_{
+      std::make_shared<MultibodyPlant<double>>(0.1)};
   std::unique_ptr<systems::Context<double>> plant_context_{};
   const RigidBody<double>* body_A_{nullptr};
   const RigidBody<double>* body_B_{nullptr};
@@ -88,24 +89,24 @@ class AddMultibodyPlantConstraintsTest : public ::testing::Test {
 };
 
 TEST_F(AddMultibodyPlantConstraintsTest, CouplerConstraint) {
-  plant_.AddCouplerConstraint(*world_A_, *A_B_, 2.3);
+  plant_->AddCouplerConstraint(*world_A_, *A_B_, 2.3);
   CheckConstraints();
 
   // Confirm that I also could have called the method with plant_context ==
   // nullptr.
   solvers::MathematicalProgram prog;
-  auto q = prog.NewContinuousVariables(plant_.num_positions());
+  auto q = prog.NewContinuousVariables(plant_->num_positions());
   EXPECT_NO_THROW(AddMultibodyPlantConstraints(plant_, q, &prog));
 }
 
 TEST_F(AddMultibodyPlantConstraintsTest, DistanceConstraint) {
-  plant_.AddDistanceConstraint(*body_A_, Vector3d(0.0, 1.0, 0.0), *body_B_,
-                               Vector3d(0.0, 1.0, 0.0), 1.5);
+  plant_->AddDistanceConstraint(*body_A_, Vector3d(0.0, 1.0, 0.0), *body_B_,
+                                Vector3d(0.0, 1.0, 0.0), 1.5);
   CheckConstraints();
 }
 
 TEST_F(AddMultibodyPlantConstraintsTest, BallConstraint) {
-  plant_.AddBallConstraint(*body_A_, Vector3d(0.0, 2.0, 0.0), *body_B_);
+  plant_->AddBallConstraint(*body_A_, Vector3d(0.0, 2.0, 0.0), *body_B_);
   if (SnoptSolverUnavailable()) {
     // IPOPT is flakey on this test.
     return;
@@ -114,10 +115,10 @@ TEST_F(AddMultibodyPlantConstraintsTest, BallConstraint) {
 }
 
 TEST_F(AddMultibodyPlantConstraintsTest, WeldConstraint) {
-  const RigidBody<double>& body_C = plant_.AddRigidBody(
+  const RigidBody<double>& body_C = plant_->AddRigidBody(
       "body_C", SpatialInertia<double>::SolidBoxWithMass(1, 1, 1, 1));
-  plant_.AddWeldConstraint(*body_A_, RigidTransformd(Vector3d(1, 2, 3)), body_C,
-                           RigidTransformd(Vector3d(4, 5, 6)));
+  plant_->AddWeldConstraint(*body_A_, RigidTransformd(Vector3d(1, 2, 3)),
+                            body_C, RigidTransformd(Vector3d(4, 5, 6)));
   if (SnoptSolverUnavailable()) {
     // IPOPT is flakey on this test.
     return;
@@ -128,15 +129,15 @@ TEST_F(AddMultibodyPlantConstraintsTest, WeldConstraint) {
 }
 
 GTEST_TEST(AdditionalTests, QuaternionsAndJointLimitsAndLocks) {
-  MultibodyPlant<double> plant(0);
+  auto plant = std::make_shared<MultibodyPlant<double>>(0);
 
   // Create a plant with four bodies.
-  const auto& world = plant.world_body();
+  const auto& world = plant->world_body();
   const auto M = SpatialInertia<double>::SolidCubeWithMass(1.0, 0.1);
-  const auto& body1 = plant.AddRigidBody("body1", M);
-  const auto& body2 = plant.AddRigidBody("body2", M);
-  const auto& body3 = plant.AddRigidBody("body3", M);
-  const auto& body4 = plant.AddRigidBody("body4", M);
+  const auto& body1 = plant->AddRigidBody("body1", M);
+  const auto& body2 = plant->AddRigidBody("body2", M);
+  const auto& body3 = plant->AddRigidBody("body3", M);
+  const auto& body4 = plant->AddRigidBody("body4", M);
 
   // Attach a specific joint to each body:
   // (1) A quaternion floating joint that will not be locked.
@@ -146,15 +147,15 @@ GTEST_TEST(AdditionalTests, QuaternionsAndJointLimitsAndLocks) {
   math::RigidTransform<double> I;
   Eigen::Vector3d X = Eigen::Vector3d::UnitX();
   const auto& joint1 =
-      plant.AddJoint<QuaternionFloatingJoint>("joint1", world, I, body1, I);
+      plant->AddJoint<QuaternionFloatingJoint>("joint1", world, I, body1, I);
   const auto& joint2 =
-      plant.AddJoint<QuaternionFloatingJoint>("joint2", world, I, body2, I);
+      plant->AddJoint<QuaternionFloatingJoint>("joint2", world, I, body2, I);
   const auto& joint3 =
-      plant.AddJoint<RevoluteJoint>("joint3", world, I, body3, I, X);
+      plant->AddJoint<RevoluteJoint>("joint3", world, I, body3, I, X);
   const auto& joint4 =
-      plant.AddJoint<RevoluteJoint>("joint4", world, I, body4, I, X);
-  plant.Finalize();
-  auto context = plant.CreateDefaultContext();
+      plant->AddJoint<RevoluteJoint>("joint4", world, I, body4, I, X);
+  plant->Finalize();
+  auto context = plant->CreateDefaultContext();
 
   // Leave joint1 unlocked.
 
@@ -163,17 +164,17 @@ GTEST_TEST(AdditionalTests, QuaternionsAndJointLimitsAndLocks) {
   joint2.Lock(&*context);
 
   // Set limits on joint3, but do not lock it.
-  dynamic_cast<RevoluteJoint<double>&>(plant.get_mutable_joint(joint3.index()))
+  dynamic_cast<RevoluteJoint<double>&>(plant->get_mutable_joint(joint3.index()))
       .set_position_limits(Vector1d{-0.5}, Vector1d{0.5});
 
   // Lock body4's revolute joint beyond its limit.
-  dynamic_cast<RevoluteJoint<double>&>(plant.get_mutable_joint(joint4.index()))
+  dynamic_cast<RevoluteJoint<double>&>(plant->get_mutable_joint(joint4.index()))
       .set_position_limits(Vector1d{-1}, Vector1d{1});
   joint4.set_angle(&*context, 1.1);
   joint4.Lock(&*context);
 
   solvers::MathematicalProgram prog;
-  auto q = prog.NewContinuousVariables(plant.num_positions());
+  auto q = prog.NewContinuousVariables(plant->num_positions());
   AddMultibodyPlantConstraints(plant, q, &prog, context.get());
 
   // The initial guess is set for the two quaternion floating joints.
@@ -226,7 +227,7 @@ GTEST_TEST(AdditionalTests, QuaternionsAndJointLimitsAndLocks) {
 
   // If we don't pass in the context, then we will not get the locked joints.
   solvers::MathematicalProgram prog2;
-  auto q2 = prog2.NewContinuousVariables(plant.num_positions());
+  auto q2 = prog2.NewContinuousVariables(plant->num_positions());
   AddMultibodyPlantConstraints(plant, q2, &prog2);
   EXPECT_EQ(prog2.linear_equality_constraints().size(), 0);
 }
