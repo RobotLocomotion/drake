410c28a48bb5d6fd091c1c62f958a4e2466a5065
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-01-20 18:40:37 -0800

    [wheel] Remove MUMPS, and other assorted repairs
    Remove MUMPS support in wheel builds of Ipopt (breaking change).
    This was already deprecated, but with some time still left on the
    fuse. However, we were not installing MUMPS's subordinate licenses
    for its suite of dynamic library dependencies, and since MUMPS is
    deprecated anyway we might as well just remove it immediately
    (in wheel builds), especially since SNOPT is always available.
    Enumerating the half-dozen licenses needs care and feeding, and
    they just add bloat to our wheels with no real upside.
    
    For the remaining shared library dependencies in drake.libs, add code
    to install their license notices as well as a new test that will fail
    in case more dynamic libraries unexpectedly show up inside the wheel.
    
    As part of this, notice that our dependency build of Reference-LAPACK
    is dead code; we've been dynamically linking Ubuntu's default BLAS and
    LAPACK into the wheel since forever, so remove it from dependencies.
    
    Also remove some unnecessary OS deps from the base image.

---
+++ tools/wheel/test/tests/libs-test.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+import re
+import sys
+from zipfile import ZipFile
+
+
+# Everything here must have its license installed as part of the wheel build.
+# This list must be kept in sync with the calls to copy_ubuntu_license in
+# drake/tools/wheel/image/build-wheel.sh.
+_ALLOWED_LIBS = [
+    "libGLX",
+    "libGLdispatch",
+    "libOpenGL",
+    "libblas",
+    "libgfortran",
+    "libgomp",
+    "liblapack",
+    "libquadmath",
+    # These are installed by a Drake's repository rule, so don't need copyright
+    # installed in build-wheel.sh.
+    "libmosek64",
+    "libtbb",
+]
+
+
+def all_files_in_wheel():
+    """Returns and iterator over all (relative) paths the wheel."""
+    wheel_path = Path(sys.argv[1])
+    with ZipFile(wheel_path) as whl:
+        for info in whl.infolist():
+            if info.is_dir():
+                continue
+            yield Path(info.filename)
+
+
+def is_good_file(path):
+    """Returns True if the file should not be in our wheel."""
+    # Check for top-level files.
+    if len(path.parents) == 1:
+        if path.name.startswith("drake.cpython"):
+            # Allow the weird little "drake.cpython....so" stub file.
+            return True
+        # Don't allow any other top-level files.
+        return False
+
+    # Branch on the top-level directory name.
+    top_dir = str(path.parents[-2])
+    if top_dir.endswith(".dist-info"):
+        # Wheel metadata is allowed.
+        return True
+    elif top_dir in ("drake", "pydrake"):
+        # All of our actual code and data is allowed.
+        return True
+    elif top_dir == "drake.libs":
+        # Only allow expected shared libraries to be shipped.
+        stem = path.name.split("-")[0]
+        return stem in _ALLOWED_LIBS
+    else:
+        return False
+
+
+def check_files():
+    """Reports whether any bad files are in the wheel."""
+    bad_files = [
+        path
+        for path in all_files_in_wheel()
+        if not is_good_file(path)
+    ]
+    if not bad_files:
+        return
+    print("ERROR: The following paths are not expected in the wheel:")
+    for path in bad_files:
+        print(f"  {path}")
+    sys.exit(1)
+
+
+# Only run this test on Linux (for now?).
+if 'darwin' not in sys.platform:
+    check_files()
