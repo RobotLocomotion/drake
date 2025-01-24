2b30aaa3da8060409ccacf815772f1f1ff0b484c
Xuchen Han <xuchen.han@tri.global>
2025-01-23 17:35:16 -0800

    Address platform comments

--- multibody/mpm/particle_sorter.h
+++ multibody/mpm/particle_sorter.h
@@ -10,6 +10,7 @@
 #include "drake/common/ssize.h"
 #include "drake/math/autodiff_gradient.h"
 #include "drake/multibody/mpm/bspline_weights.h"
+#include "drake/multibody/mpm/spgrid_flags.h"
 
 namespace drake {
 namespace multibody {
@@ -17,14 +18,25 @@ namespace mpm {
 namespace internal {
 
 /* A range of integers [start, end). */
-struct Range {
-  int start;
-  int end;
+class Range {
+ public:
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(Range);
+
+  Range(int start, int end) : start_(start), end_(end) {
+    DRAKE_ASSERT(start_ < end_);
+  }
+
+  int start() const { return start_; }
+  int end() const { return end_; }
+
+ private:
+  int start_{};
+  int end_{};
 };
 
 using RangeVector = std::vector<Range>;
 
-/* Given a sorted vector of integers key points, converts it to a vector of
+/* Given a sorted vector of integer key points, converts it to a vector of
  consecutive ranges. For example, given the input {1, 3, 6, 9}, the output
  should be {{1, 3}, {3, 6}, {6, 9}}.
  @pre data is sorted in ascending order (no repeats) and has at least two
@@ -64,19 +76,9 @@ class ParticleSorter {
   template <typename T, typename SpGrid>
   void Sort(const SpGrid& spgrid, double dx,
             const std::vector<Vector3<T>>& particle_positions) {
-    const auto& flags = spgrid.flags();
-    const int log2_max_grid_size = flags.log2_max_grid_size;
-    const int data_bits = flags.data_bits;
-    const int log2_page = flags.log2_page;
-
     const int num_particles = particle_positions.size();
-    data_indices_.resize(num_particles);
-    base_node_offsets_.resize(num_particles);
-    particle_sorters_.resize(num_particles);
-
-    // TODO(xuchenhan-tri): Switch to using radix sort to exploit the fact that
-    // our keys are 64 bit unsigned integers. It's proven to be much faster than
-    // std::sort in practice.
+    /* Resize data structures and clear old data. */
+    Initialize(num_particles);
 
     /* We sort particles first based on their base node offsets in the spgrid;
      if they are the same, then we sort the particles by their original indices.
@@ -90,125 +92,27 @@ class ParticleSorter {
          bits_for_base_node_offset | bits_for_particle_index.
 
      However, that's easier said than done, because the base node offset is
-     already taking 64 bits! Fortunately, we notice that all base node offsets
-     in the spgrid of the particle have the following structure
+     already taking 64 bits! Fortunately, there are some dead bits that we can
+     truncate to make some room. We now check how many bits we can truncate
+     based on information from the spgrid flags and how many particles we have.
+    */
+    const auto& flags = spgrid.flags();
+    ComputeBitInformation(flags, num_particles);
 
-         page_bits | block_bits | data_bits
+    /* Build 64-bit keys that encode (page_offset << shift) + particle_index. */
+    BuildKeys(spgrid, dx, particle_positions);
 
-     with all the data bits being zero (guaranteed by SPGrid). In addition, a
-     few bits (starting from the left) in the page bits are also zero (we
-     explain why below). Therefore, we can truncate the zero bits in the
-     base_node_offset and squeeze in the original particle index into the lower
-     bits of a single 64 bit unsigned integer.
-
-     Q: Why are the left most bits in page_bits zero?
-
-     A: Since we cap the total number of allowed grid nodes. There are at most
-     2^(3*log2_max_grid_size) number of grid nodes in the grid, which requires
-     3*log2_max_grid_size bits to represent. SPGrid uses page_bits and
-     block_bits to represent the grid nodes (and data_bits are used, well, to
-     store data). The page_bits and block_bits have (64 - data_bits) in total,
-     and only 3*log2_max_grid_size bits are used, so the left most
-     (64 - data_bits - 3*log2_max_grid_size) bits are zero.
-
-     Q: So how many zeros can we truncate in the bits for base_node_offsets to
-     make room for the particle indices? And is it enough room?
-
-     A: There are (64 - data_bits - 3*log2_max_grid_size) zeros on the left and
-     data_bits zeros on the right, so they add up to (64 - 3*log2_max_grid_size)
-     zeros in total. with a typical log2_max_grid_size == 10, we have
-     (64 - 3*10) = 34 bits to work with, which is more than enough to store the
-     32 bit particle indices.
-
-     Therefore, the overall strategy is to left shift the base node offset by
-     (64 - data_bits - 3*log2_max_grid_size), which leaves the lower
-     (64 - 3*log2_max_grid_size) bits to be zero. We then use the right most 32
-     bits to store the original index of the particles. */
-    const int index_bits = 64 - 3 * log2_max_grid_size;
-    // TODO(xuchenhan-tri): The following check (i.e. that we don't have too
-    // many particles) should be enforced when we register the particles to the
-    // MPM model.
-    /* Make sure that we have enough bits to store the index of the particles.
-     */
-    DRAKE_DEMAND(uint64_t(1) << index_bits >
-                 static_cast<uint64_t>(num_particles));
-    const int zero_page_bits = 64 - data_bits - 3 * log2_max_grid_size;
-
-    for (int p = 0; p < num_particles; ++p) {
-      /* We allow T == AutoDiffXd only for testing purpose. The positions in
-       those tests are guaranteed to have zero gradients. Here we cast
-       particle_x to double regardless of what type T is to simplify the code
-       path for computing base node immediately below. */
-      const auto& particle_x = [&]() -> const Vector3<double>& {
-        if constexpr (std::is_same_v<T, double>) {
-          return particle_positions[p];
-        } else if constexpr (std::is_same_v<T, float>) {
-          return particle_positions[p].template cast<double>();
-        } else {
-          return math::DiscardZeroGradient(particle_positions[p]);
-        }
-      }();
-      const Vector3<int> base_node = ComputeBaseNode<double>(particle_x / dx);
-      base_node_offsets_[p] =
-          spgrid.CoordinateToOffset(base_node[0], base_node[1], base_node[2]);
-      data_indices_[p] = p;
-      /* Confirm the data bits of the base node offset are all zero. */
-      DRAKE_ASSERT((base_node_offsets_[p] & ((uint64_t(1) << data_bits) - 1)) ==
-                   0);
-      /* Confirm the left most bits in the page bits are unused. */
-      DRAKE_ASSERT((base_node_offsets_[p] &
-                    ~((uint64_t(1) << (64 - zero_page_bits)) - 1)) == 0);
-      particle_sorters_[p] =
-          (base_node_offsets_[p] << zero_page_bits) + data_indices_[p];
-    }
+    // TODO(xuchenhan-tri): Switch to using radix sort to exploit the fact that
+    // our keys are 64 bit unsigned integers. It's proven to be much faster than
+    // std::sort in practice.
     std::sort(particle_sorters_.begin(), particle_sorters_.end());
 
-    /* Peel off the data indices and the base node offsets from
-     particle_sorters_. Meanwhile, reorder the data indices and the base node
-     offsets based on the sorting results. */
-    const uint64_t index_mask = ((uint64_t(1) << index_bits) - 1);
-    for (int p = 0; p < ssize(particle_sorters_); ++p) {
-      data_indices_[p] = particle_sorters_[p] & index_mask;
-      /* Right shift to erase the index bits, and then left shifts to recover
-       the zero data bits in base node offsets (as required by SPGrid). */
-      base_node_offsets_[p] = (particle_sorters_[p] >> index_bits) << data_bits;
-    }
+    /* Extract sorted data indices and base node offsets from the sorted keys.
+     */
+    UnpackResults();
 
-    /* Record the sentinel particles and the coloring of the blocks. */
-    sentinel_particles_.clear();
-    for (int b = 0; b < 8; ++b) {
-      colored_ranges_[b].clear();
-    }
-    uint64_t previous_page{};
-    int range_index = 0;
-    for (int p = 0; p < num_particles; ++p) {
-      /* The bits in the offset are ordered as follows:
-
-        page bits | block bits | data bits
-
-       block bits and data bits add up to log2_page bits.
-       We right shift to get the page bits. If the page bits of two offsets are
-       identical, it means that they belong to the same page.
-
-       We color the pages so that pages that are adjacent in 3D space all have
-       different colors, as described in [Hu et al. 2018]. */
-      const uint64_t page = base_node_offsets_[p] >> log2_page;
-      if (p == 0 || previous_page != page) {
-        previous_page = page;
-        sentinel_particles_.push_back(p);
-        const int color = spgrid.get_color(page);
-        ranges_indices_[color].push_back(range_index++);
-      }
-    }
-    sentinel_particles_.push_back(num_particles);
-
-    ConvertToRangeVector(sentinel_particles_, &ranges_);
-    for (int c = 0; c < 8; ++c) {
-      colored_ranges_[c].clear();
-      for (int r : ranges_indices_[c]) {
-        colored_ranges_[c].push_back(ranges_[r]);
-      }
-    }
+    /* Build the results for `colored_ranges()`. */
+    BuildColoredRanges(spgrid, num_particles);
   }
 
   /* Returns a vector of representative grid node offsets for all active SpGrid
@@ -258,7 +162,7 @@ class ParticleSorter {
      #pragma omp parallel for
      #endif
      for (const Range& range : particle_sorter.colored_ranges()[color]) {
-       for (int i = range.start; i < range.end; ++i) {
+       for (int i = range.start(); i < range.end(); ++i) {
          // do something with particle data at index
          // `particle_sorter.data_indices()[i]`.
        }
@@ -278,11 +182,99 @@ class ParticleSorter {
   }
 
  private:
-  /* All but last entry store (sorted) indices of particles marking the boundary
-   of a new block (aka the sentinel particles). Recall that a `block` is a
-   subset of all grid nodes that occupy a consecutive blob of memory in SpGrid
-   (see class documentation of SpGrid for more details) The last entry stores
-   the number of particles. */
+  /* Helper for Sort(). Resizes all containers and clear old data. */
+  void Initialize(int num_particles);
+
+  /* Helper for Sort(). Informs on to perform bit manipulation and squeeze base
+   node offsets and particle indices into a single 64 bit unsigned int. */
+  void ComputeBitInformation(const SpGridFlags& flags, int num_particles);
+
+  /* Helper for `Sort()` that builds 64-bit sort keys for each particle. */
+  template <typename T, typename SpGrid>
+  void BuildKeys(const SpGrid& spgrid, double dx,
+                 const std::vector<Vector3<T>>& particle_positions) {
+    const int num_particles = particle_positions.size();
+    for (int p = 0; p < num_particles; ++p) {
+      /* We allow T == AutoDiffXd only for testing purpose. The positions in
+       those tests are guaranteed to have zero gradients. Here we cast
+       particle_x to double regardless of what type T is to simplify the code
+       path for computing base node immediately below.
+       The computation is simple enough that converting from float to double
+       isn't going to trigger any noticeable overhead. (The main advantage for
+       using floats is its compact memory footprint anyway.) */
+      const auto& particle_x = [&]() -> const Vector3<double>& {
+        if constexpr (std::is_same_v<T, double>) {
+          return particle_positions[p];
+        } else if constexpr (std::is_same_v<T, float>) {
+          return particle_positions[p].template cast<double>();
+        } else {
+          return math::DiscardZeroGradient(particle_positions[p]);
+        }
+      }();
+      const Vector3<int> base_node = ComputeBaseNode<double>(particle_x / dx);
+      base_node_offsets_[p] =
+          spgrid.CoordinateToOffset(base_node[0], base_node[1], base_node[2]);
+      /* Confirm the data bits of the base node offset are all zero. */
+      DRAKE_ASSERT(
+          (base_node_offsets_[p] & ((uint64_t(1) << data_bits_) - 1)) == 0);
+      /* Confirm the left most bits in the page bits are unused. */
+      DRAKE_ASSERT((base_node_offsets_[p] &
+                    ~((uint64_t(1) << (64 - zero_page_bits_)) - 1)) == 0);
+      particle_sorters_[p] = (base_node_offsets_[p] << zero_page_bits_) + p;
+    }
+  }
+
+  /* Helper for `Sort()` that unpacks the key into base node offsets and data
+   indices. */
+  void UnpackResults();
+
+  /* Helper for `Sort`()` that builds `colored_ranges_`. */
+  template <typename SpGrid>
+  void BuildColoredRanges(const SpGrid& spgrid, int num_particles) {
+    /* Keep track of where each new "page" begins. */
+    uint64_t previous_page = 0;
+    int range_index = 0;
+
+    for (int p = 0; p < num_particles; ++p) {
+      /* The bits in the offset are ordered as follows:
+
+        page bits | block bits | data bits
+
+       block bits and data bits add up to log2_page bits.
+       We right shift to get the page bits. If the page bits of two offsets are
+       identical, it means that they belong to the same page.
+
+       We color the pages so that pages that are adjacent in 3D space all have
+       different colors, as described in [Hu et al. 2018]. */
+      const uint64_t page = base_node_offsets_[p] >> log2_page_;
+
+      if (p == 0 || page != previous_page) {
+        previous_page = page;
+        sentinel_particles_.push_back(p);
+        const int color = spgrid.get_color(page);
+        ranges_indices_[color].push_back(range_index++);
+      }
+    }
+    /* Final sentinel at the end (always the number of particles). */
+    sentinel_particles_.push_back(num_particles);
+
+    /* Convert sentinel indices -> (start, end) ranges. */
+    ConvertToRangeVector(sentinel_particles_, &ranges_);
+
+    /* For each color, pick out the relevant [start, end) pairs. */
+    for (int c = 0; c < 8; ++c) {
+      colored_ranges_[c].clear();
+      for (int r : ranges_indices_[c]) {
+        colored_ranges_[c].push_back(ranges_[r]);
+      }
+    }
+  }
+
+  /* All but last entry store (sorted) indices of particles marking the
+   boundary of a new block (aka the sentinel particles). Recall that a `block`
+   is a subset of all grid nodes that occupy a consecutive blob of memory in
+   SpGrid (see class documentation of SpGrid for more details) The last entry
+   stores the number of particles. */
   std::vector<int> sentinel_particles_;
   std::vector<int> data_indices_;            // see data_indices().
   std::vector<uint64_t> base_node_offsets_;  // see base_node_offsets().
@@ -295,6 +287,13 @@ class ParticleSorter {
   RangeVector ranges_;
   /* Helper data structure for building `colored_ranges_`. */
   std::array<std::vector<int>, 8> ranges_indices_;
+
+  /* Helper data for `Sort()`. */
+  int log2_max_grid_size_{};
+  int data_bits_{};
+  int log2_page_{};
+  int zero_page_bits_{};
+  int index_bits_{};
 };
 
 }  // namespace internal
