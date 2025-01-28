d7ec91ba7364105ea3f19265636dfdc1cba00608
Matthew Woehlke <matthew.woehlke@kitware.com>
2025-01-28 15:54:09 -0500

    Fix py_env_runner
    Modify py_env_runner to export the PYTHONPATH. This is needed to ensure
    that CPython can find everything the Python interpreter in use can find
    (particularly when using a virtual environment).

--- tools/skylark/py_env_runner.py
+++ tools/skylark/py_env_runner.py
@@ -6,8 +6,12 @@ Python libraries using an environment established by Bazel.
 # TODO(eric.cousineau): See if there is a way to do this in pure C++, such
 # that it is easier to debug.
 
+import os
 import subprocess
 import sys
 
+env = os.environ.copy()
+env['PYTHONPATH'] = ':'.join([p for p in sys.path if p])
+
 assert len(sys.argv) >= 2
-subprocess.check_call(sys.argv[1:])
+subprocess.check_call(sys.argv[1:], env=env)
