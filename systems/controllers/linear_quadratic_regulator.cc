88fc36ad50afae3e4ef4ca68eb638157f5363387
Wei-Chen Li <williamrob104@gmail.com>
2025-02-13 16:11:53 +0800

    Support `x'Nu` term in discrete-time LQR

--- systems/controllers/linear_quadratic_regulator.cc
+++ systems/controllers/linear_quadratic_regulator.cc
@@ -120,20 +120,37 @@ LinearQuadraticRegulatorResult DiscreteTimeLinearQuadraticRegulator(
     const Eigen::Ref<const Eigen::MatrixXd>& A,
     const Eigen::Ref<const Eigen::MatrixXd>& B,
     const Eigen::Ref<const Eigen::MatrixXd>& Q,
-    const Eigen::Ref<const Eigen::MatrixXd>& R) {
+    const Eigen::Ref<const Eigen::MatrixXd>& R,
+    const Eigen::Ref<const Eigen::MatrixXd>& N) {
   Eigen::Index n = A.rows(), m = B.cols();
   DRAKE_DEMAND(n > 0 && m > 0);
   DRAKE_DEMAND(B.rows() == n && A.cols() == n);
   DRAKE_DEMAND(Q.rows() == n && Q.cols() == n);
   DRAKE_DEMAND(R.rows() == m && R.cols() == m);
   DRAKE_DEMAND(is_approx_equal_abstol(R, R.transpose(), 1e-10));
+  if (N.rows() != 0) {
+    DRAKE_DEMAND(N.rows() == n && N.cols() == m);
+  }
+
+  Eigen::LLT<Eigen::MatrixXd> R_cholesky(R);
+  if (R_cholesky.info() != Eigen::Success)
+    throw std::runtime_error("R must be positive definite");
 
   LinearQuadraticRegulatorResult ret;
 
-  ret.S = math::DiscreteAlgebraicRiccatiEquation(A, B, Q, R);
+  if (N.rows() != 0) {
+    Eigen::MatrixXd A1 = A - B * R_cholesky.solve(N.transpose());
+    Eigen::MatrixXd Q1 = Q - N * R_cholesky.solve(N.transpose());
+    ret.S = math::DiscreteAlgebraicRiccatiEquation(A1, B, Q1, R);
 
-  Eigen::MatrixXd tmp = B.transpose() * ret.S * B + R;
-  ret.K = tmp.llt().solve(B.transpose() * ret.S * A);
+    Eigen::MatrixXd tmp = B.transpose() * ret.S * B + R;
+    ret.K = tmp.llt().solve(B.transpose() * ret.S * A + N.transpose());
+  } else {
+    ret.S = math::DiscreteAlgebraicRiccatiEquation(A, B, Q, R);
+
+    Eigen::MatrixXd tmp = B.transpose() * ret.S * B + R;
+    ret.K = tmp.llt().solve(B.transpose() * ret.S * A);
+  }
 
   return ret;
 }
@@ -143,14 +160,13 @@ std::unique_ptr<systems::LinearSystem<double>> LinearQuadraticRegulator(
     const Eigen::Ref<const Eigen::MatrixXd>& Q,
     const Eigen::Ref<const Eigen::MatrixXd>& R,
     const Eigen::Ref<const Eigen::MatrixXd>& N) {
-  // DiscreteTimeLinearQuadraticRegulator does not support N yet.
-  DRAKE_DEMAND(system.time_period() == 0.0 || N.rows() == 0);
-
   const int num_states = system.B().rows(), num_inputs = system.B().cols();
 
-  LinearQuadraticRegulatorResult lqr_result = (system.time_period() == 0.0) ?
-    LinearQuadraticRegulator(system.A(), system.B(), Q, R, N) :
-    DiscreteTimeLinearQuadraticRegulator(system.A(), system.B(), Q, R);
+  LinearQuadraticRegulatorResult lqr_result =
+      (system.time_period() == 0.0)
+          ? LinearQuadraticRegulator(system.A(), system.B(), Q, R, N)
+          : DiscreteTimeLinearQuadraticRegulator(system.A(), system.B(), Q, R,
+                                                 N);
 
   // Return the controller: u = -Kx.
   return std::make_unique<systems::LinearSystem<double>>(
@@ -165,8 +181,7 @@ std::unique_ptr<systems::AffineSystem<double>> LinearQuadraticRegulator(
     const System<double>& system, const Context<double>& context,
     const Eigen::Ref<const Eigen::MatrixXd>& Q,
     const Eigen::Ref<const Eigen::MatrixXd>& R,
-    const Eigen::Ref<const Eigen::MatrixXd>& N,
-    int input_port_index) {
+    const Eigen::Ref<const Eigen::MatrixXd>& N, int input_port_index) {
   // TODO(russt): accept optional additional argument to return the cost-to-go
   // but note that it will be a full quadratic form (x'S2x + s1'x + s0).
 
@@ -180,19 +195,16 @@ std::unique_ptr<systems::AffineSystem<double>> LinearQuadraticRegulator(
 
   // Use specified input and no outputs (the output dynamics are irrelevant for
   // LQR design).
-  auto linear_system = Linearize(
-      system, context, InputPortIndex{input_port_index},
-      OutputPortSelection::kNoOutput);
-
-  // DiscreteTimeLinearQuadraticRegulator does not support N yet.
-  DRAKE_DEMAND(linear_system->time_period() == 0.0 || N.rows() == 0);
+  auto linear_system =
+      Linearize(system, context, InputPortIndex{input_port_index},
+                OutputPortSelection::kNoOutput);
 
   LinearQuadraticRegulatorResult lqr_result =
       (linear_system->time_period() == 0.0)
           ? LinearQuadraticRegulator(linear_system->A(), linear_system->B(), Q,
                                      R, N)
           : DiscreteTimeLinearQuadraticRegulator(linear_system->A(),
-                                                 linear_system->B(), Q, R);
+                                                 linear_system->B(), Q, R, N);
 
   const Eigen::VectorXd& x0 =
       (linear_system->time_period() == 0.0)
