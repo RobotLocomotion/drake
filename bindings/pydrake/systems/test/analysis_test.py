bccbb16c412d305e3fddb84cae412fa127809a5d
Wei-Chen Li <williamrob104@gmail.com>
2025-02-17 11:58:29 +0800

    Add DiscreteTimeApproximation for converting general continuous-time systems

--- bindings/pydrake/systems/test/analysis_test.py
+++ bindings/pydrake/systems/test/analysis_test.py
@@ -8,9 +8,11 @@ from pydrake.math import isnan
 from pydrake.symbolic import Variable, Expression
 from pydrake.autodiffutils import AutoDiffXd
 from pydrake.systems.primitives import (
+    AffineSystem_,
     ConstantVectorSource,
     ConstantVectorSource_,
     FirstOrderLowPassFilter_,
+    Integrator_,
     LinearSystem_,
     SymbolicVectorSystem,
     SymbolicVectorSystem_,
@@ -20,6 +22,7 @@ from pydrake.systems.analysis import (
     ApplySimulatorConfig,
     BatchEvalUniquePeriodicDiscreteUpdate,
     BatchEvalTimeDerivatives,
+    DiscreteTimeApproximation,
     ExtractSimulatorConfig,
     InitializeParams,
     IntegratorBase_,
@@ -327,3 +330,62 @@ class TestAnalysis(unittest.TestCase):
         self.assertLess(status.return_time(), 1.1)
         simulator.clear_monitor()
         self.assertIsNone(simulator.get_monitor())
+
+    @numpy_compare.check_all_types
+    def test_discrete_time_approximation_affinesystem(self, T):
+        A = np.array([[0, 1], [0, 0]])
+        B = np.array([0, 1])
+        f0 = np.array([2, 1])
+        C = np.array([[1, 0]])
+        D = np.array([0])
+        y0 = np.array([0])
+
+        h = 0.031415926
+        Ad = np.array([[1, h], [0, 1]])
+        Bd = np.array([0.5*h**2, h])
+        f0d = np.array([2*h+0.5*h**2, h])
+        Cd = C
+        Dd = D
+        y0d = y0
+
+        def assert_array_close(x, y): np.testing.assert_allclose(
+            np.squeeze(x), np.squeeze(y), atol=1e-10)
+
+        continuous_system = LinearSystem_[T](A, B, C, D)
+        discrete_system = DiscreteTimeApproximation(
+            system=continuous_system, time_period=h)
+        assert_array_close(discrete_system.A(), Ad)
+        assert_array_close(discrete_system.B(), Bd)
+        assert_array_close(discrete_system.C(), Cd)
+        assert_array_close(discrete_system.D(), Dd)
+
+        continuous_system = AffineSystem_[T](A, B, f0, C, D, y0)
+        discrete_system = DiscreteTimeApproximation(
+            system=continuous_system, time_period=h)
+        assert_array_close(discrete_system.A(),  Ad)
+        assert_array_close(discrete_system.B(),  Bd)
+        assert_array_close(discrete_system.f0(), f0d)
+        assert_array_close(discrete_system.C(),  Cd)
+        assert_array_close(discrete_system.D(),  Dd)
+        assert_array_close(discrete_system.y0(), y0d)
+
+    @numpy_compare.check_nonsymbolic_types
+    def test_discrete_time_approximation_system(self, T):
+        size = 5
+        sys = DiscreteTimeApproximation(
+            system=Integrator_[T](size),
+            time_period=0.01, time_offset=0.0)
+        context = sys.CreateDefaultContext()
+
+        if T != AutoDiffXd:
+            sys.ToAutoDiffXd()
+        sys.Clone()
+        copy.copy(sys)
+        copy.deepcopy(sys)
+
+        self.assertEqual(sys.get_input_port().size(), size)
+        self.assertEqual(sys.get_output_port().size(), size)
+        self.assertEqual(context.num_discrete_state_groups(), 1)
+        self.assertEqual(
+            context.get_discrete_state_vector().size(), size)
+        self.assertEqual(context.get_continuous_state_vector().size(), 0)
