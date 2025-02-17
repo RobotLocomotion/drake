529f210888f5362854f6fea4007986fa71d00262
Wei-Chen Li <williamrob104@gmail.com>
2025-02-17 11:58:29 +0800

    Add DiscreteTimeApproximation for converting general continuous-time systems

---
+++ systems/analysis/discrete_time_approximation.cc
@@ -0,0 +1,282 @@
+#include "drake/systems/analysis/discrete_time_approximation.h"
+
+#include <memory>
+
+#include <unsupported/Eigen/MatrixFunctions>
+
+#include "drake/systems/analysis/simulator.h"
+#include "drake/systems/analysis/simulator_config_functions.h"
+#include "drake/systems/framework/diagram_output_port.h"
+#include "drake/systems/framework/leaf_system.h"
+
+namespace drake {
+namespace systems {
+
+template <typename T>
+std::unique_ptr<LinearSystem<T>> DiscreteTimeApproximation(
+    const LinearSystem<T>& system, double time_period) {
+  // Check that the original system is continuous.
+  DRAKE_THROW_UNLESS(system.IsDifferentialEquationSystem());
+  // Check that the discrete time_period is greater than zero.
+  DRAKE_THROW_UNLESS(time_period > 0);
+
+  const int ns = system.num_states();
+  const int ni = system.num_inputs();
+
+  Eigen::MatrixXd M(ns + ni, ns + ni);
+  M << system.A(), system.B(), Eigen::MatrixXd::Zero(ni, ns + ni);
+
+  Eigen::MatrixXd Md = (M * time_period).exp();
+
+  auto Ad = Md.block(0, 0, ns, ns);
+  auto Bd = Md.block(0, ns, ns, ni);
+  auto& Cd = system.C();
+  auto& Dd = system.D();
+
+  return std::make_unique<LinearSystem<T>>(Ad, Bd, Cd, Dd, time_period);
+}
+
+template <typename T>
+std::unique_ptr<AffineSystem<T>> DiscreteTimeApproximation(
+    const AffineSystem<T>& system, double time_period) {
+  // Check that the original system is continuous.
+  DRAKE_THROW_UNLESS(system.IsDifferentialEquationSystem());
+  // Check that the discrete time_period is greater than zero.
+  DRAKE_THROW_UNLESS(time_period > 0);
+
+  const int ns = system.num_states();
+  const int ni = system.num_inputs();
+
+  Eigen::MatrixXd M(ns + ni + 1, ns + ni + 1);
+  M << system.A(), system.B(), system.f0(),
+      Eigen::MatrixXd::Zero(ni + 1, ns + ni + 1);
+
+  Eigen::MatrixXd Md = (M * time_period).exp();
+
+  auto Ad = Md.block(0, 0, ns, ns);
+  auto Bd = Md.block(0, ns, ns, ni);
+  auto f0d = Md.block(0, ns + ni, ns, 1);
+  auto& Cd = system.C();
+  auto& Dd = system.D();
+  auto& y0d = system.y0();
+
+  return std::make_unique<AffineSystem<T>>(Ad, Bd, f0d, Cd, Dd, y0d,
+                                           time_period);
+}
+
+namespace {
+
+template <typename T>
+class DiscreteTimeSystem final : public LeafSystem<T> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(DiscreteTimeSystem);
+
+  DiscreteTimeSystem(std::unique_ptr<System<T>> system, double time_period,
+                     double time_offset,
+                     const SimulatorConfig& integrator_config)
+      : LeafSystem<T>(SystemTypeTag<DiscreteTimeSystem>{}),
+        continuous_system_(std::move(system)),
+        continuous_context_model_value_(
+            continuous_system_->CreateDefaultContext()),
+        time_period_(time_period),
+        time_offset_(time_offset),
+        integrator_config_(integrator_config) {
+    this->Initialize();
+  }
+
+  // Scalar-converting copy constructor. See @ref system_scalar_conversion.
+  template <typename U>
+  explicit DiscreteTimeSystem(const DiscreteTimeSystem<U>& other)
+      : DiscreteTimeSystem<T>(
+            other.continuous_system_->template ToScalarType<T>(),
+            other.time_period_, other.time_offset_, other.integrator_config_) {}
+
+ private:
+  template <typename U>
+  friend class DiscreteTimeSystem;
+
+  void Initialize() {
+    // Set name.
+    const std::string& name = continuous_system_->get_name();
+    this->set_name(name.empty() ? "discrete-time approximation"
+                                : "discrete-time approximated " + name);
+
+    // Create a cache entry for the continuous system context where we can
+    // safely modify it without a (non-thread-safe) mutable member.
+    continuous_context_cache_entry_ = &this->DeclareCacheEntry(
+        "continuous system context", *continuous_context_model_value_,
+        &DiscreteTimeSystem<T>::CopyAllContext,
+        {SystemBase::all_sources_ticket()});
+    // Another cache entry for the continuous system context, but this one does
+    // not copy the input port values.
+    continuous_context2_cache_entry_ = &this->DeclareCacheEntry(
+        "continuous system context2", *continuous_context_model_value_,
+        &DiscreteTimeSystem<T>::CopyAllContextExceptInput,
+        {SystemBase::all_sources_except_input_ports_ticket()});
+
+    // Declare input ports.
+    for (int i = 0; i < continuous_system_->num_input_ports(); ++i) {
+      const InputPort<T>& port = continuous_system_->get_input_port(i);
+      this->DeclareInputPort(port.get_name(), port.get_data_type(), port.size(),
+                             port.get_random_type());
+    }
+
+    // Declare output ports.
+    for (int i = 0; i < continuous_system_->num_output_ports(); ++i) {
+      const OutputPort<T>& port = continuous_system_->get_output_port(i);
+      bool input_dependent = continuous_system_->HasDirectFeedthrough(i);
+
+      DependencyTicket prerequisites_of_calc =
+          input_dependent ? SystemBase::all_sources_ticket()
+                          : SystemBase::all_sources_except_input_ports_ticket();
+
+      if (port.get_data_type() == PortDataType::kVectorValued) {
+        this->DeclareVectorOutputPort(
+            port.get_name(), port.size(),
+            [this, &port, input_dependent](const Context<T>& discrete_context,
+                                           BasicVector<T>* out) {
+              const Context<T>& continuous_context =
+                  (input_dependent ? continuous_context_cache_entry_
+                                   : continuous_context2_cache_entry_)
+                      ->template Eval<Context<T>>(discrete_context);
+              out->SetFromVector(port.Eval(continuous_context));
+            },
+            {prerequisites_of_calc});
+      } else if (port.get_data_type() == PortDataType::kAbstractValued) {
+        this->DeclareAbstractOutputPort(
+            port.get_name(),
+            [&port] {
+              return port.Allocate();
+            },
+            [this, &port, input_dependent](const Context<T>& discrete_context,
+                                           AbstractValue* out) {
+              const Context<T>& continuous_context =
+                  (input_dependent ? continuous_context_cache_entry_
+                                   : continuous_context2_cache_entry_)
+                      ->template Eval<Context<T>>(discrete_context);
+              port.Calc(continuous_context, out);
+            },
+            {prerequisites_of_calc});
+      }
+    }
+
+    // Declare parameters.
+    for (int i = 0;
+         i < continuous_context_model_value_->num_numeric_parameter_groups();
+         ++i) {
+      this->DeclareNumericParameter(
+          continuous_context_model_value_->get_numeric_parameter(i));
+    }
+    for (int i = 0;
+         i < continuous_context_model_value_->num_abstract_parameters(); ++i) {
+      this->DeclareAbstractParameter(
+          continuous_context_model_value_->get_abstract_parameter(i));
+    }
+
+    // Declare state.
+    this->DeclareDiscreteState(continuous_system_->num_continuous_states());
+    this->DeclarePeriodicDiscreteUpdateEvent(
+        time_period_, time_offset_, &DiscreteTimeSystem<T>::DiscreteUpdate);
+  }
+
+  void DiscreteUpdate(const Context<T>& discrete_context,
+                      DiscreteValues<T>* out) const {
+    // TODO(wei-chen): Make the simulator/integrator a cache variable.
+    Simulator<T> simulator(*continuous_system_);
+    ApplySimulatorConfig(integrator_config_, &simulator);
+    auto& integrator = simulator.get_mutable_integrator();
+
+    // Ensure that the continuous system context is up-to-date.
+    continuous_context_cache_entry_->template Eval<Context<T>>(
+        discrete_context);
+    auto& cache_entry_value =
+        continuous_context_cache_entry_->get_mutable_cache_entry_value(
+            discrete_context);
+    DRAKE_ASSERT(!cache_entry_value.is_out_of_date());
+
+    // We allow the integrator to modify the continuous system context in place.
+    // This is fine because (a) we mark the cache entry as out-of-date, and
+    cache_entry_value.mark_out_of_date();
+    auto& continuous_context =
+        cache_entry_value.template GetMutableValueOrThrow<Context<T>>();
+    integrator.reset_context(&continuous_context);
+    integrator.Initialize();
+    integrator.IntegrateWithMultipleStepsToTime(continuous_context.get_time() +
+                                                time_period_);
+
+    // (b) after updating `DiscreteValues<T>* out`, CopyAllContext() will be
+    // invoked next time continuous_context_cache_entry_->Eval() is called
+    // anyway.
+    out->get_mutable_vector().SetFromVector(
+        continuous_context.get_continuous_state_vector().CopyToVector());
+  }
+
+  void CopyAllContextExceptInput(const Context<T>& from_discrete_context,
+                                 Context<T>* to_continuous_context) const {
+    // Copy time.
+    to_continuous_context->SetTime(from_discrete_context.get_time());
+    // Copy state.
+    to_continuous_context->SetContinuousState(
+        from_discrete_context.get_discrete_state_vector().value());
+    // Copy parameters.
+    to_continuous_context->get_mutable_parameters().SetFrom(
+        from_discrete_context.get_parameters());
+    // Copy accuracy.
+    to_continuous_context->SetAccuracy(from_discrete_context.get_accuracy());
+  }
+
+  void CopyAllContext(const Context<T>& from_discrete_context,
+                      Context<T>* to_continuous_context) const {
+    CopyAllContextExceptInput(from_discrete_context, to_continuous_context);
+    // Copy fixed input port values.
+    for (int i = 0; i < continuous_system_->num_input_ports(); ++i) {
+      const AbstractValue* value_ptr =
+          this->EvalAbstractInput(from_discrete_context, i);
+      if (value_ptr != nullptr) {
+        to_continuous_context->FixInputPort(i, *value_ptr);
+      }
+    }
+  }
+
+  const std::unique_ptr<const System<T>> continuous_system_;
+  const std::unique_ptr<const Context<T>> continuous_context_model_value_;
+  const CacheEntry* continuous_context_cache_entry_;
+  const CacheEntry* continuous_context2_cache_entry_;
+  const double time_period_;
+  const double time_offset_;
+  const SimulatorConfig integrator_config_;
+};
+
+}  // namespace
+
+namespace scalar_conversion {
+template <>
+struct Traits<DiscreteTimeSystem> : public NonSymbolicTraits {};
+}  // namespace scalar_conversion
+
+template <typename T>
+std::unique_ptr<System<T>> DiscreteTimeApproximation(
+    const System<T>& system, double time_period, double time_offset,
+    const SimulatorConfig& integrator_config) {
+  // Check that the original system is continuous.
+  DRAKE_THROW_UNLESS(system.IsDifferentialEquationSystem());
+  // Check that the discrete time_period is greater than zero.
+  DRAKE_THROW_UNLESS(time_period > 0);
+
+  return std::make_unique<DiscreteTimeSystem<T>>(
+      system.Clone(), time_period, time_offset, integrator_config);
+}
+
+DRAKE_DEFINE_FUNCTION_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
+    (static_cast<std::unique_ptr<LinearSystem<T>> (*)(
+         const LinearSystem<T>&, double)>(&DiscreteTimeApproximation<T>),
+     static_cast<std::unique_ptr<AffineSystem<T>> (*)(
+         const AffineSystem<T>&, double)>(&DiscreteTimeApproximation<T>)));
+
+DRAKE_DEFINE_FUNCTION_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS((
+    static_cast<std::unique_ptr<System<T>> (*)(const System<T>&, double, double,
+                                               const SimulatorConfig&)>(
+        &DiscreteTimeApproximation<T>)));
+
+}  // namespace systems
+}  // namespace drake
---
+++ systems/analysis/test/discrete_time_approximation_test.cc
@@ -0,0 +1,276 @@
+#include "drake/systems/analysis/discrete_time_approximation.h"
+
+#include <memory>
+
+#include <gtest/gtest.h>
+
+#include "drake/common/test_utilities/eigen_matrix_compare.h"
+#include "drake/multibody/parsing/parser.h"
+#include "drake/multibody/plant/multibody_plant.h"
+#include "drake/systems/analysis/simulator.h"
+#include "drake/systems/controllers/linear_quadratic_regulator.h"
+#include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/primitives/constant_vector_source.h"
+#include "drake/systems/primitives/discrete_time_delay.h"
+#include "drake/systems/primitives/integrator.h"
+#include "drake/systems/primitives/vector_log_sink.h"
+#include "drake/systems/primitives/zero_order_hold.h"
+
+namespace drake {
+namespace systems {
+namespace {
+
+GTEST_TEST(DiscreteTimeApproximation, AffineSystemTest) {
+  Eigen::Matrix<double, 2, 2> A;
+  Eigen::Matrix<double, 2, 1> B;
+  Eigen::Vector<double, 2> f0;
+  Eigen::Matrix<double, 1, 2> C;
+  Eigen::Matrix<double, 1, 1> D;
+  Eigen::Vector<double, 1> y0;
+  A << 0, 1, 0, 0;
+  B << 0, 1;
+  f0 << 2, 1;
+  C << 1, 0;
+  D << 1;
+  y0 << 1;
+
+  // Reject discrete system as argument.
+  EXPECT_ANY_THROW(DiscreteTimeApproximation<double>(
+      AffineSystem<double>(A, B, f0, C, D, y0, 0.1), 0.1));
+
+  const double h = 0.031415926;
+
+  Eigen::Matrix<double, 2, 2> Ad;
+  Eigen::Matrix<double, 2, 1> Bd;
+  Eigen::Vector<double, 2> f0d;
+  Eigen::Matrix<double, 1, 2> Cd = C;
+  Eigen::Matrix<double, 1, 1> Dd = D;
+  Eigen::Vector<double, 1> y0d = y0;
+  Ad << 1, h, 0, 1;
+  Bd << 0.5 * h * h, h;
+  f0d << 2 * h + 0.5 * h * h, h;
+
+  AffineSystem<double> continuous_affine_sys(A, B, f0, C, D, y0);
+  auto discrete_affine_sys =
+      DiscreteTimeApproximation<double>(continuous_affine_sys, h);
+
+  const double kCompTolerance = 1e-14;
+  EXPECT_TRUE(CompareMatrices(discrete_affine_sys->A(), Ad, kCompTolerance));
+  EXPECT_TRUE(CompareMatrices(discrete_affine_sys->B(), Bd, kCompTolerance));
+  EXPECT_TRUE(CompareMatrices(discrete_affine_sys->f0(), f0d, kCompTolerance));
+  EXPECT_TRUE(CompareMatrices(discrete_affine_sys->C(), Cd));
+  EXPECT_TRUE(CompareMatrices(discrete_affine_sys->D(), Dd));
+  EXPECT_TRUE(CompareMatrices(discrete_affine_sys->y0(), y0d));
+
+  LinearSystem<double> continuous_linear_sys(A, B, C, D);
+  auto discrete_linear_sys =
+      DiscreteTimeApproximation<double>(continuous_affine_sys, h);
+
+  EXPECT_TRUE(CompareMatrices(discrete_affine_sys->A(), Ad, kCompTolerance));
+  EXPECT_TRUE(CompareMatrices(discrete_affine_sys->B(), Bd, kCompTolerance));
+  EXPECT_TRUE(CompareMatrices(discrete_affine_sys->C(), Cd));
+  EXPECT_TRUE(CompareMatrices(discrete_affine_sys->D(), Dd));
+}
+
+GTEST_TEST(DiscreteTimeApproximation, IntegrateConstantTest) {
+  DiagramBuilder<double> builder;
+  auto source = builder.AddSystem<ConstantVectorSource>(
+      Eigen::Matrix<double, 1, 1>::Ones());
+  auto logger = builder.AddSystem<VectorLogSink>(1);
+
+  const double time_period = 0.01;
+  auto sys = builder.AddSystem(
+      DiscreteTimeApproximation(Integrator<double>(1), time_period));
+  // Connect a constant 1.0 to the discrete-time approximation of the
+  // integrator
+  builder.Connect(source->get_output_port(), sys->get_input_port());
+  builder.Connect(sys->get_output_port(), logger->get_input_port());
+
+  auto diagram = builder.Build();
+
+  Simulator simulator(*diagram);
+  const double tf = 1.001;
+  simulator.AdvanceTo(tf);
+
+  auto& log = logger->FindLog(simulator.get_context());
+  for (int i = 0; i < log.num_samples(); ++i) {
+    const double rem = std::remainder(log.sample_times()[i], time_period);
+    const double area = (std::round(log.sample_times()[i] / time_period) +
+                         (std::abs(rem) > 1e-12 ? 1 : 0)) *
+                        time_period;
+    ASSERT_NEAR(area, log.data().coeff(i), 1e-12);
+  }
+}
+
+GTEST_TEST(DiscreteTimeApproximation, IntegrateRampTest) {
+  DiagramBuilder<double> builder;
+  auto source = builder.AddSystem<ConstantVectorSource>(
+      Eigen::Matrix<double, 1, 1>::Ones());
+  auto ramp = builder.AddSystem<Integrator>(1);
+  builder.Connect(source->get_output_port(), ramp->get_input_port());
+
+  const double time_period = 0.01;
+  auto sys = builder.AddSystem(
+      DiscreteTimeApproximation(Integrator<double>(1), time_period));
+  // Connect a ramp of slope 1 to the discrete-time approximation of the
+  // integrator. Since the discrete-time approximation has zero-order hold on
+  // the input, the output is the area under a staircase.
+  builder.Connect(ramp->get_output_port(), sys->get_input_port());
+  builder.ExportOutput(sys->get_output_port());
+
+  auto diagram = builder.Build();
+
+  Simulator simulator(*diagram, diagram->CreateDefaultContext());
+  const double tf = 1.001;
+  simulator.AdvanceTo(tf);
+
+  const int num_steps = std::ceil(tf / time_period);
+  const double area =
+      (num_steps - 1) * num_steps / 2 * (time_period * time_period);
+  EXPECT_NEAR(area, diagram->get_output_port().Eval(simulator.get_context())[0],
+              1e-12);
+}
+
+GTEST_TEST(DiscreteTimeApproximation, ResultSystemIsScalarConvertible) {
+  const double time_period = 0.01;
+  auto sys1 = DiscreteTimeApproximation(Integrator<double>(1), time_period);
+  EXPECT_NO_THROW(sys1->ToAutoDiffXd());
+  EXPECT_NO_THROW(sys1->Clone());
+  auto sys2 = DiscreteTimeApproximation(Integrator<AutoDiffXd>(1), time_period);
+  EXPECT_NO_THROW(sys2->ToScalarType<double>());
+  EXPECT_NO_THROW(sys2->Clone());
+}
+
+class DiscreteTimeApproximatedMultibodyPlantTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    DiagramBuilder<double> builder;
+    auto [plant, scene_graph] =
+        multibody::AddMultibodyPlantSceneGraph(&builder, 0.0);
+    const std::string sdf_url =
+        "package://drake/examples/multibody/cart_pole/cart_pole.sdf";
+    multibody::Parser(&plant, &scene_graph).AddModelsFromUrl(sdf_url);
+    plant.Finalize();
+
+    builder.ExportInput(plant.get_actuation_input_port(), "u");
+    builder.ExportOutput(plant.get_state_output_port(), "x");
+    builder.ExportOutput(scene_graph.get_query_output_port(), "query");
+
+    continuous_sys_ = builder.Build();
+    continuous_context_ = continuous_sys_->CreateDefaultContext();
+
+    discrete_sys_ = DiscreteTimeApproximation(*continuous_sys_, time_period_);
+    discrete_context_ = discrete_sys_->CreateDefaultContext();
+  }
+
+ protected:
+  std::unique_ptr<AffineSystem<double>> DiscreteStabilizingController() const {
+    const double theta_d = M_PI;
+    Eigen::VectorXd xd(4);
+    xd << 0, theta_d, 0, 0;
+    Eigen::VectorXd ud(1);
+    ud << 0;
+    discrete_context_->SetDiscreteState(xd);
+    discrete_sys_->get_input_port().FixValue(discrete_context_.get(), ud);
+
+    Eigen::DiagonalMatrix<double, 4> Q;
+    Q.diagonal() << 10, 10, 1, 1;
+    Eigen::DiagonalMatrix<double, 1> R;
+    R.diagonal() << 1;
+
+    return controllers::LinearQuadraticRegulator(
+        *discrete_sys_, *discrete_context_, Eigen::MatrixXd(Q),
+        Eigen::MatrixXd(R));
+  }
+
+  std::unique_ptr<System<double>> continuous_sys_;
+  std::unique_ptr<Context<double>> continuous_context_;
+  std::unique_ptr<System<double>> discrete_sys_;
+  std::unique_ptr<Context<double>> discrete_context_;
+  const double time_period_ = 0.01;
+};
+
+TEST_F(DiscreteTimeApproximatedMultibodyPlantTest, Construction) {
+  EXPECT_TRUE(continuous_sys_->IsDifferentialEquationSystem());
+  EXPECT_TRUE(discrete_sys_->IsDifferenceEquationSystem());
+
+  EXPECT_EQ(continuous_context_->num_continuous_states(),
+            discrete_context_->get_discrete_state_vector().size());
+}
+
+TEST_F(DiscreteTimeApproximatedMultibodyPlantTest, InputPortsMatch) {
+  EXPECT_EQ(continuous_sys_->num_input_ports(),
+            discrete_sys_->num_input_ports());
+  for (int i = 0; i < discrete_sys_->num_input_ports(); ++i) {
+    auto& port1 = continuous_sys_->get_input_port(i);
+    auto& port2 = discrete_sys_->get_input_port(i);
+    EXPECT_EQ(port1.get_name(), port2.get_name());
+    EXPECT_EQ(port1.get_data_type(), port2.get_data_type());
+    EXPECT_EQ(port1.size(), port2.size());
+    EXPECT_EQ(port1.get_random_type(), port2.get_random_type());
+  }
+}
+
+TEST_F(DiscreteTimeApproximatedMultibodyPlantTest, OutputPortsMatch) {
+  EXPECT_EQ(continuous_sys_->num_output_ports(),
+            discrete_sys_->num_output_ports());
+  for (int i = 0; i < discrete_sys_->num_output_ports(); ++i) {
+    auto& port1 = continuous_sys_->get_output_port(i);
+    auto& port2 = discrete_sys_->get_output_port(i);
+    EXPECT_EQ(port1.get_name(), port2.get_name());
+    EXPECT_EQ(port1.get_data_type(), port2.get_data_type());
+    EXPECT_EQ(port1.size(), port2.size());
+  }
+}
+
+TEST_F(DiscreteTimeApproximatedMultibodyPlantTest, SynthesizeDiscreteTimeLQR) {
+  DiagramBuilder<double> builder;
+  auto plant = builder.AddSystem(std::move(continuous_sys_));
+  auto controller = builder.AddSystem(DiscreteStabilizingController());
+  auto zoh = builder.AddSystem<ZeroOrderHold>(time_period_, 1);
+  builder.Connect(plant->get_output_port(0), controller->get_input_port());
+  builder.Connect(controller->get_output_port(), zoh->get_input_port());
+  builder.Connect(zoh->get_output_port(), plant->get_input_port());
+  auto diagram = builder.Build();
+  auto diagram_context = diagram->CreateDefaultContext();
+
+  const double theta_init = M_PI * 0.9;
+  Eigen::VectorXd x_init(4);
+  x_init << 0, theta_init, 0, 0;
+  diagram_context->SetContinuousState(x_init);
+
+  Simulator simulator(*diagram, std::move(diagram_context));
+  simulator.AdvanceTo(10);
+  const double theta_final =
+      simulator.get_context().get_continuous_state_vector()[1];
+  // Check discrete-time controller stabilizes continuous-time cart-pole.
+  const double theta_d = M_PI;
+  EXPECT_LT(std::abs(theta_final - theta_d), std::abs(theta_init - theta_d));
+}
+
+TEST_F(DiscreteTimeApproximatedMultibodyPlantTest,
+       DiscreteTimeSystemSimulatable) {
+  DiagramBuilder<double> builder;
+  auto controller = builder.AddSystem(DiscreteStabilizingController());
+  auto plant = builder.AddSystem(std::move(discrete_sys_));
+  builder.Connect(plant->get_output_port(0), controller->get_input_port());
+  builder.Connect(controller->get_output_port(), plant->get_input_port());
+  auto diagram = builder.Build();
+  auto diagram_context = diagram->CreateDefaultContext();
+
+  const double theta_init = M_PI * 0.9;
+  Eigen::VectorXd x_init(4);
+  x_init << 0, theta_init, 0, 0;
+  diagram_context->SetDiscreteState(x_init);
+
+  Simulator simulator(*diagram, std::move(diagram_context));
+  simulator.AdvanceTo(10);
+  const double theta_final = simulator.get_context().get_discrete_state()[1];
+  // Check discrete-time controller stabilizes discrete-time cart-pole.
+  const double theta_d = M_PI;
+  EXPECT_LT(std::abs(theta_final - theta_d), std::abs(theta_init - theta_d));
+}
+
+}  // namespace
+}  // namespace systems
+}  // namespace drake
