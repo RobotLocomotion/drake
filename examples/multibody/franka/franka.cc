0a72005fe3417b2cfef7058fd0ab350533e1ca4a
Vincent Kurtz <vjkurtz@gmail.com>
2025-02-20 17:12:43 -0600

    add optional manipuland to franka example

--- examples/multibody/franka/franka.cc
+++ examples/multibody/franka/franka.cc
@@ -49,13 +49,15 @@ DEFINE_double(
     "and no contact forces are displayed.  mbp_time_step must be >= 0.");
 
 // Physical parameters.
-DEFINE_bool(use_hydro, false, "If true, use hydro. Otherwise point contact.");
+DEFINE_bool(use_hydro, true, "If true, use hydro. Otherwise point contact.");
 DEFINE_double(stiction_tolerance, 1.0e-4, "Stiction tolerance [m/s].");
 
 // Visualization.
 DEFINE_bool(visualize, true, "Whether to visualize (true) or not (false).");
-DEFINE_bool(visualize_forces, false,
+DEFINE_bool(visualize_forces, true,
             "Whether to visualize forces (true) or not (false).");
+DEFINE_bool(visualize_proximity, false,
+            "Whether to visualize collision geometry (true) or not (false).");
 DEFINE_double(viz_period, 1.0 / 60.0, "Viz period.");
 
 // Discrete contact solver.
@@ -77,6 +79,9 @@ DEFINE_double(kp, 500, "Proportional gain for PID controller.");
 DEFINE_double(kd, 50, "Derivative gain for PID controller.");
 DEFINE_double(ki, 10, "Integral gain for PID controller.");
 
+// Whether to include a box to hold
+DEFINE_bool(manipuland, false, "Include a manipuland in the simulation.");
+
 using drake::geometry::CollisionFilterDeclaration;
 using drake::math::RigidTransform;
 using drake::math::RigidTransformd;
@@ -89,10 +94,12 @@ using drake::systems::ConvexIntegrator;
 using drake::systems::ImplicitEulerIntegrator;
 using drake::systems::IntegratorBase;
 using drake::systems::controllers::PidController;
+using Eigen::MatrixXd;
 using Eigen::Translation3d;
 using Eigen::Vector3d;
 using Eigen::VectorXd;
 using clock = std::chrono::steady_clock;
+using drake::geometry::SceneGraphConfig;
 using drake::multibody::contact_solvers::internal::SapSolverParameters;
 using drake::visualization::ApplyVisualizationConfig;
 using drake::visualization::VisualizationConfig;
@@ -111,12 +118,25 @@ int do_main() {
   auto [plant, scene_graph] =
       multibody::AddMultibodyPlant(plant_config, &builder);
 
+  // Enable hydroelastic contact
+  if (FLAGS_use_hydro) {
+    SceneGraphConfig sg_config;
+    sg_config.default_proximity_properties.compliance_type = "compliant";
+    sg_config.default_proximity_properties.resolution_hint = 0.01;
+    scene_graph.set_config(sg_config);
+  }
+
   // Add the franka arm model
-  multibody::Parser(&plant).AddModelsFromUrl(
+  multibody::Parser parser(&plant);
+  parser.AddModelsFromUrl(
       "package://drake_models/franka_description/urdf/panda_arm_hand.urdf");
   plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("panda_link0"));
 
-  // TODO(vincekurtz): Add a manipuland
+  // Add an object to hold, if requested
+  if (FLAGS_manipuland) {
+    parser.AddModelsFromUrl("package://drake_models/ycb/003_cracker_box.sdf");
+  }
+
   plant.Finalize();
 
   fmt::print("Num positions: {:d}\n", plant.num_positions());
@@ -128,15 +148,33 @@ int do_main() {
     VisualizationConfig vis_config;
     vis_config.publish_period = FLAGS_viz_period;
     vis_config.publish_contacts = FLAGS_visualize_forces;
+    vis_config.publish_proximity = FLAGS_visualize_proximity;
     ApplyVisualizationConfig(vis_config, &builder, nullptr, &plant,
                              &scene_graph, meshcat);
   }
 
+  // Define a target state
+  VectorXd q_nom(plant.num_positions());
+  VectorXd x_nom(plant.num_multibody_states());
+
+  if (FLAGS_manipuland) {
+    q_nom << 0, 0, 0, -M_PI_2, 0, M_PI_2, M_PI_4, 0.01, 0.01,  // robot
+        0.707, 0.707, 0, 0, 0.55, 0.0, 0.43;                   // box
+  } else {
+    q_nom << 0, 0, 0, -M_PI_2, 0, M_PI_2, 0, 0.01, 0.01;
+  }
+  x_nom << q_nom, VectorXd::Zero(plant.num_velocities());
+
   // Add a PID controller
-  VectorXd q_nom(9);
-  q_nom << 0, 0, 0, -M_PI_2, 0, M_PI_2, 0, 0.01, 0.01;
-  VectorXd x_nom(18);
-  x_nom << q_nom, VectorXd::Zero(9);
+  MatrixXd Px = MatrixXd::Identity(plant.num_multibody_states(),
+                                   plant.num_multibody_states());
+
+  if (FLAGS_manipuland) {
+    Px.resize(18, 31);
+    Px.setZero();
+    Px.block<9, 9>(0, 0) = MatrixXd::Identity(9, 9);
+    Px.block<9, 9>(9, 16) = MatrixXd::Identity(9, 9);
+  }
 
   VectorXd Kp(9), Kd(9), Ki(9);
   Kp << FLAGS_kp, FLAGS_kp, FLAGS_kp, FLAGS_kp, FLAGS_kp, FLAGS_kp, FLAGS_kp,
@@ -146,8 +184,9 @@ int do_main() {
   Ki << FLAGS_ki, FLAGS_ki, FLAGS_ki, FLAGS_ki, FLAGS_ki, FLAGS_ki, FLAGS_ki,
       0.1 * FLAGS_ki, 0.1 * FLAGS_ki;
 
-  auto pid_controller = builder.AddSystem<PidController>(Kp, Ki, Kd);
-  auto nominal_state_source = builder.AddSystem<ConstantVectorSource>(x_nom);
+  auto pid_controller = builder.AddSystem<PidController>(Px, Kp, Ki, Kd);
+  auto nominal_state_source =
+      builder.AddSystem<ConstantVectorSource>(Px * x_nom);
 
   builder.Connect(nominal_state_source->get_output_port(),
                   pid_controller->get_input_port_desired_state());
@@ -166,8 +205,11 @@ int do_main() {
       diagram->GetMutableSubsystemContext(plant, diagram_context.get());
   plant.SetDefaultContext(&plant_context);
 
-  // Set initial conditions
-  plant.SetPositions(&plant_context, q_nom);
+  // Set initial condition. The gripper is open a bit wider than in q_nom
+  VectorXd q_init = q_nom;
+  q_init(7) = 0.035;
+  q_init(8) = 0.035;
+  plant.SetPositions(&plant_context, q_init);
 
   // Set up the simulator with the specified integration scheme.
   auto simulator =
