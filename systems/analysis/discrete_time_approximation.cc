c4cba6787ac1f06ff70610608cfb223c02dc07d3
Wei-Chen Li <williamrob104@gmail.com>
2025-02-21 13:43:06 +0800

    Check input dependency

--- systems/analysis/discrete_time_approximation.cc
+++ systems/analysis/discrete_time_approximation.cc
@@ -6,6 +6,7 @@
 
 #include "drake/systems/analysis/simulator.h"
 #include "drake/systems/analysis/simulator_config_functions.h"
+#include "drake/systems/framework/diagram_output_port.h"
 #include "drake/systems/framework/leaf_system.h"
 
 namespace drake {
@@ -104,8 +105,14 @@ class DiscreteTimeSystem final : public LeafSystem<T> {
     // safely modify it without a (non-thread-safe) mutable member.
     continuous_context_cache_entry_ = &this->DeclareCacheEntry(
         "continuous system context", *continuous_context_model_value_,
-        &DiscreteTimeSystem<T>::CopyDiscreteContextToContinuousContext,
+        &DiscreteTimeSystem<T>::CopyAllSources,
         {SystemBase::all_sources_ticket()});
+    // Another cache entry for the continuous system context, but this one does
+    // not copy the input port values.
+    continuous_context2_cache_entry_ = &this->DeclareCacheEntry(
+        "continuous system context2", *continuous_context_model_value_,
+        &DiscreteTimeSystem<T>::CopyAllSourcesExceptInput,
+        {SystemBase::all_sources_except_input_ports_ticket()});
 
     // Declare input ports.
     for (int i = 0; i < continuous_system_->num_input_ports(); ++i) {
@@ -117,29 +124,40 @@ class DiscreteTimeSystem final : public LeafSystem<T> {
     // Declare output ports.
     for (int i = 0; i < continuous_system_->num_output_ports(); ++i) {
       const OutputPort<T>& port = continuous_system_->get_output_port(i);
+      bool input_dependent = continuous_system_->HasDirectFeedthrough(i);
+
+      std::set<DependencyTicket> prerequisites_of_calc{
+          input_dependent
+              ? SystemBase::all_sources_ticket()
+              : SystemBase::all_sources_except_input_ports_ticket()};
+
       if (port.get_data_type() == PortDataType::kVectorValued) {
         this->DeclareVectorOutputPort(
             port.get_name(), port.size(),
-            [this, &port](const Context<T>& discrete_context,
-                          BasicVector<T>* out) {
+            [this, &port, input_dependent](const Context<T>& discrete_context,
+                                           BasicVector<T>* out) {
               const Context<T>& continuous_context =
-                  continuous_context_cache_entry_->template Eval<Context<T>>(
-                      discrete_context);
+                  (input_dependent ? continuous_context_cache_entry_
+                                   : continuous_context2_cache_entry_)
+                      ->template Eval<Context<T>>(discrete_context);
               out->SetFromVector(port.Eval(continuous_context));
-            });
+            },
+            prerequisites_of_calc);
       } else if (port.get_data_type() == PortDataType::kAbstractValued) {
         this->DeclareAbstractOutputPort(
             port.get_name(),
             [&port] {
               return port.Allocate();
             },
-            [this, &port](const Context<T>& discrete_context,
-                          AbstractValue* out) {
+            [this, &port, input_dependent](const Context<T>& discrete_context,
+                                           AbstractValue* out) {
               const Context<T>& continuous_context =
-                  continuous_context_cache_entry_->template Eval<Context<T>>(
-                      discrete_context);
+                  (input_dependent ? continuous_context_cache_entry_
+                                   : continuous_context2_cache_entry_)
+                      ->template Eval<Context<T>>(discrete_context);
               port.Calc(continuous_context, out);
-            });
+            },
+            prerequisites_of_calc);
       }
     }
 
@@ -194,29 +212,34 @@ class DiscreteTimeSystem final : public LeafSystem<T> {
         continuous_context.get_continuous_state_vector().CopyToVector());
   }
 
-  void CopyDiscreteContextToContinuousContext(
-      const Context<T>& discrete_context,
-      Context<T>* continuous_context) const {
+  void CopyAllSourcesExceptInput(const Context<T>& from_discrete_context,
+                                 Context<T>* to_continuous_context) const {
     // Copy time.
-    continuous_context->SetTime(discrete_context.get_time());
+    to_continuous_context->SetTime(from_discrete_context.get_time());
     // Copy state.
-    continuous_context->SetContinuousState(
-        discrete_context.get_discrete_state_vector().value());
+    to_continuous_context->SetContinuousState(
+        from_discrete_context.get_discrete_state_vector().value());
     // Copy parameters.
-    continuous_context->get_mutable_parameters().SetFrom(
-        discrete_context.get_parameters());
+    to_continuous_context->get_mutable_parameters().SetFrom(
+        from_discrete_context.get_parameters());
     // Copy accuracy.
-    continuous_context->SetAccuracy(discrete_context.get_accuracy());
+    to_continuous_context->SetAccuracy(from_discrete_context.get_accuracy());
+  }
+
+  void CopyAllSources(const Context<T>& from_discrete_context,
+                      Context<T>* to_continuous_context) const {
+    CopyAllSourcesExceptInput(from_discrete_context, to_continuous_context);
     // Copy fixed input port values.
     for (int i = 0; i < continuous_system_->num_input_ports(); ++i) {
-      continuous_context->FixInputPort(
-          i, *(this->EvalAbstractInput(discrete_context, i)));
+      to_continuous_context->FixInputPort(
+          i, *this->EvalAbstractInput(from_discrete_context, i));
     }
   }
 
   const std::unique_ptr<const System<T>> continuous_system_;
   const std::unique_ptr<const Context<T>> continuous_context_model_value_;
   const CacheEntry* continuous_context_cache_entry_;
+  const CacheEntry* continuous_context2_cache_entry_;
   const double time_period_;
   const double time_offset_;
   const SimulatorConfig integrator_config_;
@@ -233,8 +256,11 @@ template <typename T>
 std::unique_ptr<System<T>> DiscreteTimeApproximation(
     const System<T>& system, double time_period, double time_offset,
     const SimulatorConfig& integrator_config) {
+  // Check that the original system is continuous.
   DRAKE_THROW_UNLESS(system.IsDifferentialEquationSystem());
+  // Check that the discrete time_period is greater than zero.
   DRAKE_THROW_UNLESS(time_period > 0);
+
   return std::make_unique<DiscreteTimeSystem<T>>(
       system.Clone(), time_period, time_offset, integrator_config);
 }
