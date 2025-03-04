14ac2d476b643386bd057eede2a80ee9abaa2530
Aiden2244 <github.quicken590@pasmail.net>
2025-03-04 15:29:17 -0500

    removed references to brew lockfiles

--- tools/wheel/wheel_builder/macos.py
+++ tools/wheel/wheel_builder/macos.py
@@ -72,11 +72,8 @@ def _provision(python_targets):
     Prepares wheel build environment.
     """
     packages_path = os.path.join(resource_root, 'image', 'packages-macos')
-
-    env=['HOMEBREW_BUNDLE_NO_LOCK'] = '1'
-
-    command = ['brew','bundle',f'--file={packages_path}',]
-    subprocess.check_call(command, env=env)
+    command = ['brew', 'bundle', f'--file={packages_path}',]
+    subprocess.check_call(command)
 
     for t in python_targets:
         subprocess.check_call(['brew', 'install', f'python@{t.version}'])
