7d87ce05fb8a71d2b5793465d04e2eaaf1858f3e
cohnt <cohnt@umich.edu>
2025-03-05 15:00:50 -0500

    Define IrisZo parameterization with a vector of Expressions.
    Squashed commits:
    
    Merge branch 'master' into iris-zo-expression-parametrization
    
    Merge branch 'iris-zo-subspace-bindings' into iris-zo-expression-parametrization
    
    Round of changes for platform reivew.
    
    Python bindings and bindings test.
    
    Remove unnecessary throws.
    
    Test failure modes for Expression parameterization.
    
    Run the algebraic parameterization test with an Expression.
    
    Run the linear subspace test with an Expression.
    
    Fix the issue where the environment was empty.
    
    Try using a shared_ptr to avoid copying the variables. The fact that this doesn't work suggests that there is some other issue.
    
    First test case. This doesn't work, likely because we're copying the variables?
    
    Allow using VectorX<Expression> to define the parameterization for IrisZo.
    
    This feature is mainly meant for people using Python. Functions defined in Python cannot be called concurrently in multiple threads in C++, but a VectorX<Expression> can be evaluated in parallel.
    
    Be even more explicit.
    
    Try to explicitly capture q_star_val.
    
    Fixes for platform review.
    
    This needs to be pass by value, or else the reference is lost.
    
    Switch to using RationalForwardKinematics when constructing the binding.
    
    Add python bindings for set_parameterization, but make it so threadsafe is always false.
    
    Merge branch 'master' into iris-zo-subspace-bindings
    
    Merge branch 'master' into iris-zo-subspace-bindings
    
    Merge branch 'iris-zo-subspace' into iris-zo-subspace-bindings
    
    Documentation tweaks and additional tests. Add DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN.
    
    Fix syntax error in iris_zo_test.
    
    Lint.
    
    Merge branch 'iris-zo-subspace' into iris-zo-subspace-bindings
    
    Update docs.
    
    Merge branch 'iris-zo-subspace' into iris-zo-subspace-bindings
    
    Update docs.
    
    Make the factory method a class method instead of just a lambda in the python bindings.
    
    Update bindings to reflect class nature of IrisZoOptions.
    
    Merge branch 'iris-zo-subspace' into iris-zo-subspace-bindings
    
    Rewrite to make IrisZoOptions a class.
    
    Update bindings per parameterization spelling.
    
    Merge branch 'iris-zo-subspace' into iris-zo-subspace-bindings
    
    Changes for platform review.
    
    Merge branch 'iris-zo-subspace' into iris-zo-subspace-bindings
    
    Fix complaint from leak sanitizer.
    
    Merge branch 'iris-zo-subspace' into iris-zo-subspace-bindings
    
    Test case for when the user gives a parametrization with the wrong dimension.
    
    Changes for feature review.
    
    Static member that creates a struct with the appropriate values set for the forbidden fields to generate regions in TC space.
    
    Test case for a reparametrization of the configuration space. (Specifically, the C-IRIS algebraic parametrization.)
    
    Test for a linear subspace in the convex configuration space.
    
    Implement the new feature.

--- planning/iris/iris_zo.cc
+++ planning/iris/iris_zo.cc
@@ -6,6 +6,9 @@
 #include <common_robotics_utilities/parallelism.hpp>
 
 #include "drake/common/fmt_eigen.h"
+#include "drake/common/symbolic/expression.h"
+#include "drake/common/symbolic/expression/environment.h"
+#include "drake/common/symbolic/expression/variable.h"
 #include "drake/common/text_logging.h"
 #include "drake/geometry/optimization/convex_set.h"
 #include "drake/geometry/optimization/hpolyhedron.h"
@@ -46,6 +49,47 @@ IrisZoOptions IrisZoOptions::CreateWithRationalKinematicParameterization(
                                 /* parameterization_is_threadsafe */ true,
                                 /* parameterization_dimension */ dimension);
   return instance;
+}
+
+void IrisZoOptions::SetParameterizationFromExpression(
+    const Eigen::VectorX<symbolic::Expression>& expression_parameterization,
+    const std::shared_ptr<Eigen::VectorX<symbolic::Variable>> variables) {
+  DRAKE_THROW_UNLESS(variables != nullptr);
+
+  // First, we check that the variables in expression_parameterization match the
+  // user-supplied variables.
+  symbolic::Variables expression_variables, user_supplied_variables;
+  for (const auto& expression : expression_parameterization) {
+    expression_variables.insert(expression.GetVariables());
+  }
+  user_supplied_variables.insert(variables->begin(), variables->end());
+  DRAKE_THROW_UNLESS(expression_variables == user_supplied_variables);
+
+  int dimension = ssize(expression_variables);
+
+  // Note that in this lambda, we copy the shared_ptr variables, ensuring that
+  // variables is kept alive without making a copy of the individual Variable
+  // objects (which would break the substitution machinery).
+  auto evaluate_expression =
+      [expression_parameterization_captured =
+           Eigen::VectorX<symbolic::Expression>(expression_parameterization),
+       variables](const Eigen::VectorXd& q) {
+        DRAKE_ASSERT(variables != nullptr);
+        DRAKE_ASSERT(q.size() == ssize(*variables));
+        symbolic::Environment env;
+        for (int i = 0; i < q.size(); ++i) {
+          env.insert((*variables)[i], q[i]);
+        }
+        Eigen::VectorXd out = expression_parameterization_captured.unaryExpr(
+            [&env](const symbolic::Expression& expression) {
+              return expression.Evaluate(env);
+            });
+        return out;
+      };
+
+  set_parameterization(evaluate_expression,
+                       /* parameterization_is_threadsafe */ true,
+                       /* parameterization_dimension */ dimension);
 }
 
 namespace {
