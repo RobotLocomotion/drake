86d97e856a78e69afee2a123aa175f95e14b7604
Wei-Chen Li <williamrob104@gmail.com>
2025-03-07 15:06:12 +0800

    Modify RungeKutta3Integrator

--- systems/analysis/runge_kutta3_integrator.h
+++ systems/analysis/runge_kutta3_integrator.h
@@ -76,13 +76,13 @@ class RungeKutta3Integrator final : public IntegratorBase<T> {
 
  private:
   void DoInitialize() override;
-  bool DoStep(const T& h) override;
+  bool DoStep(const T& h, Context<T>* context) const override;
 
   // Vector used in error estimate calculations.
-  VectorX<T> err_est_vec_;
+  mutable VectorX<T> err_est_vec_;
 
   // Vector used to save initial value of xc.
-  VectorX<T> save_xc0_;
+  mutable VectorX<T> save_xc0_;
 
   // These are pre-allocated temporaries for use by integration. They store
   // the derivatives computed at various points within the integration
