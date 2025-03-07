a513002354eee9d2e939d7581b612e15920c5f7d
Mitiguy <mitiguy@users.noreply.github.com>
2025-03-07 14:01:19 -0800

    Create MapQDDotToAcceleration() and MapAccelerationToQDDot() for simple mobilizers (#22698)
    Co-Authored-By: Paul Mitiguy <paul.mitiguy@tri.global>

--- multibody/tree/mobilizer.h
+++ multibody/tree/mobilizer.h
@@ -615,6 +615,26 @@ class Mobilizer : public MultibodyElement<T> {
   virtual void MapQDotToVelocity(const systems::Context<T>& context,
                                  const Eigen::Ref<const VectorX<T>>& qdot,
                                  EigenPtr<VectorX<T>> v) const = 0;
+
+  // Calculates q̈ from v̇, v, q using q̈ = N(q)⋅v̇ + Ṅ(q,v)⋅v.
+  // @param[in] context stores generalized positions q and velocities v.
+  // @param[in] vdot (v̇) 1ˢᵗ time derivatives of generalized velocities.
+  // @param[out] qddot (q̈) 2ⁿᵈ time derivatives of the generalized positions.
+  // TODO(Mitiguy) change this function to a pure virtual function when it has
+  //  been overridden in all subclasses.
+  virtual void MapAccelerationToQDDot(const systems::Context<T>& context,
+                                      const Eigen::Ref<const VectorX<T>>& vdot,
+                                      EigenPtr<VectorX<T>> qddot) const;
+
+  // Calculates v̇ from q̈, v, q using v̇ = N⁺(q)⋅q̈ + Ṅ⁺(q,v)⋅q̇ where q̇ = N(q)⋅v.
+  // @param[in] context stores generalized positions q and velocities v.
+  // @param[in] qddot (q̈) 2ⁿᵈ time derivatives of the generalized positions.
+  // @param[out] vdot (v̇) 1ˢᵗ time derivatives of generalized velocities.
+  // TODO(Mitiguy) change this function to a pure virtual function when it has
+  //  been overridden in all subclasses.
+  virtual void MapQDDotToAcceleration(const systems::Context<T>& context,
+                                      const Eigen::Ref<const VectorX<T>>& qddot,
+                                      EigenPtr<VectorX<T>> vdot) const;
   // @}
 
   // Returns a const Eigen expression of the vector of generalized positions
