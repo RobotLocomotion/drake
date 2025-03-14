301b18603958401222663107d2317dbd4f82de4d
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-03-14 11:30:12 -0700

    [build] Publish linux wheels for Python 3.13

--- tools/wheel/wheel_builder/linux.py
+++ tools/wheel/wheel_builder/linux.py
@@ -50,6 +50,12 @@ targets = (
         test_platform=Platform('ubuntu', '24.04', 'noble'),
         python_version_tuple=(3, 12, 8),
         python_sha='c909157bb25ec114e5869124cc2a9c4a4d4c1e957ca4ff553f1edc692101154e'),  # noqa
+    Target(
+        build_platform=Platform('ubuntu', '22.04', 'jammy'),
+        # TODO(jwnimmer-tri) Switch testing to 25.04 once it's been released.
+        test_platform=Platform('ubuntu', '24.10', 'oracular'),
+        python_version_tuple=(3, 13, 0),
+        python_sha='086de5882e3cb310d4dca48457522e2e48018ecd43da9cdf827f6a0759efb07d'),  # noqa
 )
 glibc_versions = {
     'jammy': '2_35',
