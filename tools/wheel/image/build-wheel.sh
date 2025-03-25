c946d357be5fd249126cf67dadd5af48567b1657
Matthew Woehlke <matthew.woehlke@kitware.com>
2025-03-25 19:15:12 -0400

    Build wheels on AlmaLinux instead of Ubuntu (#22779)
    Change wheel builds to use AlmaLinux instead of Ubuntu as the host
    platform. The motivating effect of this is to "downgrades" our required
    glibc version from 2.35 to 2.34.
    
    Note that AlmaLinux 9 is the base of manylinux_2_34_x86_64:2025.01.05-1.
    The reason we are not using manylinux is because a) it provides Python
    interpreters that do not have headers or libraries, and so are not
    useful for building Drake, and b) its compilers interfere with use of
    the underlying AlmaLinux compilers and attempts to use them result in
    obscure failures.
    
    Because packages derived from Fedora / Red Hat are missing what appears
    to be a required license notice, license information for redistributed
    libraries is instead copied from Debian's packages and manually added to
    the wheel.
    
    See also https://bugzilla.redhat.com/show_bug.cgi?id=2353932.

--- tools/wheel/image/build-wheel.sh
+++ tools/wheel/image/build-wheel.sh
@@ -36,16 +36,14 @@ chrpath()
     done
 }
 
-# Helper function to copy the copyright text from an Ubuntu package into the
-# wheel's documentation.
-copy_ubuntu_license()
+# Helper function to copy the imported copyright text into the wheel's
+# documentation.
+copy_license()
 {
     package_name=$1
     mkdir -p ${WHEEL_DIR}/pydrake/doc/${package_name}
-    # TODO(jwnimmer-tri) Is there a simple way to install something slightly
-    # more direct (e.g., LICENSE text itself) instead of the copyright file?
-    cp /usr/share/doc/${package_name}/copyright \
-        ${WHEEL_DIR}/pydrake/doc/${package_name}/copyright
+    cp -t ${WHEEL_DIR}/pydrake/doc/${package_name}/ \
+        /opt/drake-wheel-licenses/${package_name}/copyright
 }
 
 ###############################################################################
@@ -89,7 +87,7 @@ if [[ "$(uname)" == "Linux" ]]; then
     # The drake/tools/wheel/test/tests/libs-test.py must be kept in sync with
     # this list. To maintain that correspondence, the _ALLOWED_LIBS entry seen
     # in that test program is added as comment to the end of each line below.
-    copy_ubuntu_license libgfortran5   # libgfortran, libquadmath, libgomp
+    copy_license gcc  # libgfortran, libgomp, libquadmath
 fi
 
 cp -r -t ${WHEEL_SHARE_DIR}/drake \
