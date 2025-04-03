f3d2e44da01b2cd73bc2d166812a0fdd841db74a
Vincent Kurtz <vjkurtz@gmail.com>
2025-04-03 13:14:22 -0500

    parse command-line arguments

--- examples/integrators/suction_gripper.py
+++ examples/integrators/suction_gripper.py
@@ -1,3 +1,5 @@
+import time
+import argparse
 from pydrake.all import *
 import numpy as np
 
@@ -104,97 +106,147 @@ class SuctionGripper(LeafSystem):
 
         output.set_value(spatial_forces)
 
+def run_simulation(mbp_time_step, integrator, accuracy, use_error_control):
+    """Run the simulation with the given parameters."""
 
-# System setup
-meshcat = StartMeshcat()
-builder = DiagramBuilder()
+    # System setup
+    meshcat = StartMeshcat()
+    builder = DiagramBuilder()
 
-# Set up the plant model
-plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=1e-3)
-parser = Parser(plant)
-
-# The gripper itself is just a sphere floating in space (for now)
-radius = 0.02
-color = np.array([0.8, 0.5, 0.5, 1.0])
-pos = np.array([0.0, 0.0, 0.5])
-gripper_body = plant.AddRigidBody(
-    "gripper", SpatialInertia.SolidSphereWithDensity(1000, radius)
-)
-plant.RegisterVisualGeometry(
-    gripper_body, RigidTransform(), Sphere(radius), "gripper", color
-)
-plant.RegisterCollisionGeometry(
-    gripper_body,
-    RigidTransform(),
-    Sphere(radius),
-    "gripper",
-    CoulombFriction(1.0, 1.0),
-)
-plant.WeldFrames(
-    plant.world_frame(), gripper_body.body_frame(), RigidTransform(pos)
-)
-
-# Manipuland(s)
-box = parser.AddModelsFromUrl(
-    "package://drake_models/ycb/003_cracker_box.sdf"
-)[0]
-
-# Ground with friction
-plant.RegisterCollisionGeometry(
-    plant.world_body(),
-    RigidTransform([0.0, 0.0, -0.05]),
-    Box(10, 10, 0.1),
-    "ground",
-    CoulombFriction(1.0, 1.0),
-)
-
-plant.Finalize()
-
-# Connect the suction gripper model
-suction_model = builder.AddSystem(
-    SuctionGripper(
-        plant,
-        gripper_body.index(),
-        force_radius=0.2,
-        max_force=5.0,
+    # Set up the plant model
+    plant, scene_graph = AddMultibodyPlantSceneGraph(
+        builder, time_step=mbp_time_step
     )
-)
+    parser = Parser(plant)
 
-builder.Connect(
-    suction_model.GetOutputPort("force_output"),
-    plant.get_applied_spatial_force_input_port(),
-)
-builder.Connect(
-    plant.get_body_poses_output_port(),
-    suction_model.body_poses_input_port,
-)
-builder.Connect(
-    scene_graph.get_query_output_port(),
-    suction_model.query_object_input_port,
-)
+    # The gripper itself is just a sphere floating in space (for now)
+    radius = 0.02
+    color = np.array([0.8, 0.5, 0.5, 1.0])
+    pos = np.array([0.0, 0.0, 0.5])
+    gripper_body = plant.AddRigidBody(
+        "gripper", SpatialInertia.SolidSphereWithDensity(1000, radius)
+    )
+    plant.RegisterVisualGeometry(
+        gripper_body, RigidTransform(), Sphere(radius), "gripper", color
+    )
+    plant.RegisterCollisionGeometry(
+        gripper_body,
+        RigidTransform(),
+        Sphere(radius),
+        "gripper",
+        CoulombFriction(1.0, 1.0),
+    )
+    plant.WeldFrames(
+        plant.world_frame(), gripper_body.body_frame(), RigidTransform(pos)
+    )
+
+    # Manipuland(s)
+    box = parser.AddModelsFromUrl(
+        "package://drake_models/ycb/003_cracker_box.sdf"
+    )[0]
+
+    # Ground with friction
+    plant.RegisterCollisionGeometry(
+        plant.world_body(),
+        RigidTransform([0.0, 0.0, -0.05]),
+        Box(10, 10, 0.1),
+        "ground",
+        CoulombFriction(1.0, 1.0),
+    )
+
+    plant.Finalize()
+
+    # Connect the suction gripper model
+    suction_model = builder.AddSystem(
+        SuctionGripper(
+            plant,
+            gripper_body.index(),
+            force_radius=0.2,
+            max_force=5.0,
+        )
+    )
+
+    builder.Connect(
+        suction_model.GetOutputPort("force_output"),
+        plant.get_applied_spatial_force_input_port(),
+    )
+    builder.Connect(
+        plant.get_body_poses_output_port(),
+        suction_model.body_poses_input_port,
+    )
+    builder.Connect(
+        scene_graph.get_query_output_port(),
+        suction_model.query_object_input_port,
+    )
 
 
-# Connect to meshcat
-AddDefaultVisualization(builder, meshcat)
+    # Connect to meshcat
+    AddDefaultVisualization(builder, meshcat)
 
-# Build the diagram
-diagram = builder.Build()
-context = diagram.CreateDefaultContext()
+    # Build the diagram
+    diagram = builder.Build()
+    context = diagram.CreateDefaultContext()
 
-# Set the initial state
-q0_box = np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.01, 0.43])
-plant_context = plant.GetMyMutableContextFromRoot(context)
-plant.SetPositions(plant_context, box, q0_box)
+    # Set the initial state
+    q0_box = np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.01, 0.43])
+    plant_context = plant.GetMyMutableContextFromRoot(context)
+    plant.SetPositions(plant_context, box, q0_box)
 
-# Simulate
-simulator = Simulator(diagram, context)
-simulator.set_target_realtime_rate(1.0)
-simulator.Initialize()
+    # Set up the simulator
+    config = SimulatorConfig()
+    if mbp_time_step == 0.0:
+        config.integration_scheme = integrator
+    config.accuracy = accuracy
+    config.target_realtime_rate = 0.0
+    config.use_error_control = use_error_control
 
-print("Waiting for meshcat... press [ENTER] to continue.")
-input()
+    simulator = Simulator(diagram, context)
+    ApplySimulatorConfig(config, simulator)
+    simulator.Initialize()
 
-meshcat.StartRecording()
-simulator.AdvanceTo(1.0)
-meshcat.StopRecording()
-meshcat.PublishRecording()
+    print("Waiting for meshcat... press [ENTER] to continue.")
+    input()
+
+    # Simulate
+    meshcat.StartRecording()
+    st = time.time()
+    simulator.AdvanceTo(1.0)
+    wall_time = time.time() - st
+    meshcat.StopRecording()
+    meshcat.PublishRecording()
+
+    print(f"\nWall time: {wall_time}\n")
+
+    # Print a summary of solver statistics
+    PrintSimulatorStatistics(simulator)
+
+if __name__ == "__main__":
+    # Get command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--mbp_time_step",
+        type=float,
+        default=0.0,
+        help="The MultibodyPlant time step. default: 0.0",
+    )
+    parser.add_argument(
+        "--integrator",
+        type=str,
+        default="convex",
+        help="The integrator to use. default: convex",
+    )
+    parser.add_argument(
+        "--accuracy",
+        type=float,
+        default=0.1,
+        help="The accuracy to use. default: 0.1",
+    )
+    args = parser.parse_args()
+
+    # Run the simulation
+    run_simulation(
+        mbp_time_step=args.mbp_time_step,
+        integrator=args.integrator,
+        accuracy=args.accuracy,
+        use_error_control=True,
+    )
