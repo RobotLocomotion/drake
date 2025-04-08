defc113c82fa3f4c77d545936647b3a0f881b00a
Vincent Kurtz <vjkurtz@gmail.com>
2025-04-08 14:07:47 -0500

    add SDIRK option

--- systems/analysis/convex_integrator.cc
+++ systems/analysis/convex_integrator.cc
@@ -140,7 +140,8 @@ void ConvexIntegrator<T>::DoInitialize() {
 
 template <typename T>
 bool ConvexIntegrator<T>::DoStep(const T& h) {
-  return DoStepWithHalfStepErrorEstimate(h);
+  // return DoStepWithHalfStepErrorEstimate(h);
+  return DoStepWithSDIRK(h);
 }
 
 template <typename T>
@@ -202,6 +203,82 @@ bool ConvexIntegrator<T>::DoStepWithHalfStepErrorEstimate(const T& h) {
     ContinuousState<T>& err = *this->get_mutable_error_estimate();
     err.SetFrom(*x_next_full_);
     err.get_mutable_vector().PlusEqScaled(-1.0, x_next_half_2_->get_vector());
+  }
+
+  return true;  // step was successful
+}
+
+template <typename T>
+bool ConvexIntegrator<T>::DoStepWithSDIRK(const T& h) {
+  using std::sqrt;
+  // Get references to the overall diagram context and the plant context
+  const Context<T>& diagram_context = this->get_context();
+  const Context<T>& plant_context =
+      plant().GetMyContextFromRoot(diagram_context);
+
+  // Single Diagonally Implicit Runge Kutta (SDIRK) coefficients from Kennedy
+  // and Carpenter, "Diagonaly Implicit Runge-Kutta Method for Oridnary
+  // Differential Equations", 2016. pp 72, eq (221).
+  const double gamma = 1.0 - sqrt(2.0) / 2.0;
+  const double c1 = gamma;
+  const double c2 = 1.0;
+  const double a11 = gamma;
+  const double a21 = 1.0 - gamma;
+  const double a22 = gamma;
+  const double b1 = 1.0 - gamma;
+  const double b2 = gamma;
+
+  // Error estimation coefficients from Blom et al, "A comparison of Rosenbrock
+  // and ESDIRK methods combined with iterative solvers for unsteady
+  // compressible flows", 2016. pp 25, Table 4.
+  const double bhat2 = 2.0 - 5.0 / 4.0 * sqrt(2.0);
+  const double bhat1 = 1.0 - bhat2;
+
+  // Set time and time-step for debugging
+  const T t0 = diagram_context.get_time();
+  time_ = diagram_context.get_time();
+  time_step_ = h;
+
+  // Workspace allocations
+  VectorX<T>& v_guess = workspace_.v;
+
+  // Mutable context
+  Context<T>& mutable_context = *this->get_mutable_context();
+  ContinuousState<T>& x = mutable_context.get_mutable_continuous_state();
+
+  // TODO(vincekurtz): consider renaming these
+  const VectorX<T> x0 = diagram_context.get_continuous_state().CopyToVector();
+  ContinuousState<T>& x1 = *x_next_full_;
+  ContinuousState<T>& x2 = *x_next_half_1_;
+
+  // First phase: solve for x₁ = x₀ + h a₁₁f(t₁, x₁) using convex SAP.
+  // We'll do this by solving the SAP problem from x = x₀ with timestep h a₁₁.
+  solve_phase_ = 0;
+  mutable_context.SetTime(t0 + c1 * h);
+  v_guess = plant().GetVelocities(plant_context);
+  CalcNextContinuousState(h * a11, v_guess, &x1);
+  const VectorX<T> f1 = (x1.CopyToVector() - x0) / (h * a11);
+
+  // Second phase: solve for x₂ = x₀ + h a₂₁f(t₁, x₁) + h a₂₂f(t₂, x₂).
+  // We'll do this by first setting x = x₀ + h a₂₁f(t₁, x₁), then solving for
+  // the step from x to x₂ with timestep h a₂₂.
+  solve_phase_ = 1;
+  x.SetFromVector(x0 + h * a21 * f1);
+  mutable_context.SetTimeAndNoteContinuousStateChange(t0 + c2 * h);
+
+  v_guess = x.get_generalized_velocity().CopyToVector();
+  CalcNextContinuousState(h * a22, v_guess, &x2);
+  const VectorX<T> f2 = (x2.CopyToVector() - x.CopyToVector()) / (h * a22);
+
+  // Advance the state as x₀ + h b₁f(t₁, x₁) + h b₂f(t₂, x₂)
+  x.SetFromVector(x0 + h * (b1 * f1 + b2 * f2));
+  mutable_context.SetTimeAndNoteContinuousStateChange(t0 + h);
+
+  // Determine the error estimate using the embedded lower-order method
+  if (!this->get_fixed_step_mode()) {
+    const VectorX<T> x_hat = x0 + h * (bhat1 * f1 + bhat2 * f2);
+    ContinuousState<T>& err = *this->get_mutable_error_estimate();
+    err.SetFromVector(x.CopyToVector() - x_hat);
   }
 
   return true;  // step was successful
