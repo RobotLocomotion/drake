dec063cf8847d65b3a9d59ba6a7fbe6d37e30c6a
Wei-Chen Li <williamrob104@gmail.com>
2025-04-08 23:09:49 +0800

    Support scalar conversion for LuenbergerObserver (#22851)

--- systems/estimators/test/luenberger_observer_test.cc
+++ systems/estimators/test/luenberger_observer_test.cc
@@ -6,6 +6,7 @@
 #include <gtest/gtest.h>
 
 #include "drake/common/eigen_types.h"
+#include "drake/common/pointer_cast.h"
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
 #include "drake/common/test_utilities/is_dynamic_castable.h"
 #include "drake/examples/pendulum/pendulum_plant.h"
@@ -128,9 +129,8 @@ GTEST_TEST(LuenbergerObserverTest, ErrorDynamics) {
             x_est, output->GetMutableVectorData(0)->CopyToVector(), tol));
       }
 
-      // TODO(russt): Support scalar conversion.
-      EXPECT_FALSE(is_autodiffxd_convertible(*observer));
-      EXPECT_FALSE(is_symbolic_convertible(*observer));
+      EXPECT_TRUE(is_autodiffxd_convertible(*observer));
+      EXPECT_TRUE(is_symbolic_convertible(*observer));
     }
   }
 }
@@ -171,6 +171,112 @@ GTEST_TEST(LuenbergerObserverTest, InputOutputPorts) {
             observer.get_observed_system_output_input_port().get_index());
   EXPECT_EQ(observer.GetOutputPort("estimated_state").get_index(),
             observer.get_estimated_state_output_port().get_index());
+}
+
+template <typename T>
+class DummySystem final : public LeafSystem<T> {
+ public:
+  DummySystem() {
+    this->DeclareContinuousState(1);
+    this->DeclareVectorOutputPort(
+        kUseDefaultName, 1, [](const Context<T>& context, BasicVector<T>* out) {
+          out->SetZero();
+        });
+  }
+};
+
+GTEST_TEST(LuenbergerObserverTest, NotScalarConvertible) {
+  DummySystem<double> dummy_sys;
+  LuenbergerObserver observer(dummy_sys, *dummy_sys.CreateDefaultContext(),
+                              Eigen::MatrixXd::Ones(1, 1));
+
+  auto observer_ad = observer.ToAutoDiffXdMaybe();  // Should not throw.
+  EXPECT_EQ(observer_ad, nullptr);
+
+  auto observer_sym = observer.ToSymbolicMaybe();  // Should not throw.
+  EXPECT_EQ(observer_sym, nullptr);
+}
+
+template <typename T>
+class OutputParameterSystem final : public LeafSystem<T> {
+ public:
+  explicit OutputParameterSystem(int size, double time_period)
+      : LeafSystem<T>(SystemTypeTag<OutputParameterSystem>{}),
+        size_(size),
+        time_period_(time_period) {
+    DRAKE_THROW_UNLESS(size >= 0);
+    BasicVector<T> model_vector(size_);
+    this->DeclareNumericParameter(model_vector);
+    this->DeclareVectorOutputPort(
+        kUseDefaultName, model_vector,
+        [](const Context<T>& context, BasicVector<T>* out) {
+          out->set_value(context.get_numeric_parameter(0).value());
+        });
+
+    this->DeclareVectorInputPort(kUseDefaultName, size_);
+    this->DeclareDiscreteState(size_);
+    this->DeclarePeriodicDiscreteUpdateEvent(
+        time_period_, 0.0, &OutputParameterSystem<T>::DiscreteUpdate);
+  }
+
+  template <typename U>
+  explicit OutputParameterSystem(const OutputParameterSystem<U>& other)
+      : OutputParameterSystem(other.size_, other.time_period_) {}
+
+  void SetParameter(Context<T>* context,
+                    const Eigen::Ref<Eigen::VectorX<T>>& param) const {
+    context->get_mutable_numeric_parameter(0).set_value(param);
+  }
+
+ private:
+  template <typename U>
+  friend class OutputParameterSystem;
+
+  // Discrete-time dynamics: f(x̂[n],u[n]) = 0
+  void DiscreteUpdate(const Context<T>&, DiscreteValues<T>* next) const {
+    next->get_mutable_vector().SetZero();
+  }
+
+  const int size_;
+  const double time_period_;
+};
+
+GTEST_TEST(LuenbergerObserverTest, ParametersProperlyCopied) {
+  OutputParameterSystem<double> plant(1, 0.01);
+  auto plant_context = plant.CreateDefaultContext();
+
+  // Set the plant to output a constant.
+  Eigen::VectorXd p(1);
+  p << 12.3;
+  plant.SetParameter(plant_context.get(), p);
+
+  // Set y to a contant value.
+  Eigen::VectorXd y(1);
+  y << 4.56;
+
+  // x̂[n+1] = f(x̂[n],u[n],p) + L (y − g(x̂[n],u[n],p))
+  // x̂[n+1] =       0        + L (y - p)
+  Eigen::MatrixXd L = Eigen::MatrixXd::Identity(1, 1);
+  LuenbergerObserver<double> observer(plant, *plant_context, L);
+  auto context = observer.CreateDefaultContext();
+  observer.get_observed_system_output_input_port().FixValue(context.get(), y);
+  context->SetDiscreteState(
+      observer.EvalUniquePeriodicDiscreteUpdate(*context));
+  EXPECT_TRUE(CompareMatrices(
+      observer.get_estimated_state_output_port().Eval(*context), L * (y - p)));
+
+  // Cloned observer should give the same result.  This is true only if the
+  // plant parameters are properly copied to the new observer.
+  auto observer2 =
+      dynamic_pointer_cast<LuenbergerObserver<double>>(observer.Clone());
+  auto context2 = observer2->CreateDefaultContext();
+  observer2->get_observed_system_output_input_port().FixValue(context2.get(),
+                                                              y);
+  context2->SetDiscreteState(
+      observer2->EvalUniquePeriodicDiscreteUpdate(*context2));
+  EXPECT_TRUE(CompareMatrices(
+      observer2->get_estimated_state_output_port().Eval(*context2),
+      L * (y - p)));
 }
 
 }  // namespace
