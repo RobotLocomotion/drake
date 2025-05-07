62c43ffe317de7cf56b90603eb224bae602907a2
Matthew Woehlke <matthew.woehlke@kitware.com>
2025-05-07 13:31:12 -0400

    Use unique directory for macOS wheel tests
    f530173a71f4 refactored wheel builds to use a unique build root. This
    does something similar for tests on macOS. (See comment in common.py for
    why Linux is not also modified.)

--- tools/wheel/image/provision-build.sh
+++ tools/wheel/image/provision-build.sh
@@ -2,21 +2,23 @@
 
 # Internal script to prepare the build location.
 #
-# This procedure first creates the super root of the wheel build at
-# ``$HOME/.drake-wheel-build``. Then, the script creates a unique temporary
-# build directory for each wheel built as a subdirectory of the super root.
-# Finally, the temporary directory is symlinked to ``/tmp/drake-wheel-build``
-# for consistent reference in later steps of the wheel building process.
+# This creates a unique directory under ``$HOME/.drake-wheel-build`` which is
+# used to contain all artifacts produced and/or consumed during a wheel build.
+# This directory is symlinked as ``/tmp/drake-wheel-build`` in order to provide
+# a consistent location for the build process. Both the directory itself and
+# the symlink are normally removed after a successful build, unless the user
+# specified ``--keep-build``.
 #
-# The script takes an optional command-line argument denoting the python
-# version of the wheel being built. This adds the specified version as a
-# prefix to the random build directory name. This way, if the ``--keep-build``
-# flag is set for a macOS build, the user can differentiate between wheel
-# builds by python version (this is not applicable to Ubuntu builds since
-# Linux wheels are built in Docker containers).
+# An optional argument serves as a prefix for the created directory, which can
+# help disambiguate retained builds on macOS (which performs builds on the host
+# system rather than in a container).
+#
+# On macOS, this script is also used to create a unique space for running
+# tests. A second optional argument may be used to override the ``build``
+# portion of the ``/tmp/drake-wheel-build`` symlink.
 
 set -eu -o pipefail
 
 mkdir -p ~/.drake-wheel-build
 readonly d="$(mktemp -d ~/.drake-wheel-build/${1:-}XXXXXXXXXX)"
-ln -nsf "${d}" /tmp/drake-wheel-build
+ln -nsf "${d}" /tmp/drake-wheel-${2:-build}
