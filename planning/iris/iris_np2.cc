d8fe03a762ab45c3b7fa576bb0a80b62ba35e9e8
cohnt <cohnt@umich.edu>
2025-05-12 15:50:16 -0400

    Check unsupported features.

--- planning/iris/iris_np2.cc
+++ planning/iris/iris_np2.cc
@@ -1,23 +1,67 @@
 #include "drake/planning/iris/iris_np2.h"
 
+#include "drake/common/text_logging.h"
 #include "drake/geometry/optimization/hpolyhedron.h"
 #include "drake/geometry/optimization/hyperellipsoid.h"
 
 namespace drake {
 namespace planning {
 
+using Eigen::VectorXd;
 using geometry::optimization::HPolyhedron;
 using geometry::optimization::Hyperellipsoid;
 
 HPolyhedron IrisNp2(const SceneGraphCollisionChecker& checker,
                     const Hyperellipsoid& starting_ellipsoid,
                     const HPolyhedron& domain, const IrisNp2Options& options) {
-  unused(checker);
-  unused(starting_ellipsoid);
+  auto start = std::chrono::high_resolution_clock::now();
+
+  // Check for features which are currently unsupported.
+  if (options.sampled_iris_options.containment_points != std::nullopt) {
+    // TODO(cohnt): Support enforcing additional containment points.
+    throw std::runtime_error(
+        "IrisNp2 does not yet support enforcing additional containment "
+        "points.");
+  }
+  if (options.sampled_iris_options.prog_with_additional_constraints !=
+      nullptr) {
+    // TODO(cohnt): Support enforcing additional constraints.
+    throw std::runtime_error(
+        "IrisNp2 does not yet support specifying additional constriants.");
+  }
+  if (!(options.parameterization.get_parameterization()(
+            starting_ellipsoid.center()) == starting_ellipsoid.center())) {
+    // TODO(cohnt): Support growing regions along a parameterized subspace.
+    throw std::runtime_error(
+        "IrisNp2 does not yet support growing regions on a parameterized "
+        "subspace.");
+  }
+  if (!checker.GetAllAddedCollisionShapes().empty()) {
+    // TODO(cohnt): Support handling additional collision shapes.
+    throw std::runtime_error(
+        "IrisNp2 does not yet support collision checkers with added collision "
+        "shapes.");
+  }
+  if (checker.GetPaddingMatrix().cwiseAbs().maxCoeff() != 0.0) {
+    // A nonzero value implies nonzero padding. std::nullopt implies varying
+    // padding, so at least one such padding is nonzero.
+    // TODO(cohnt): Support nonzero padding.
+    throw std::runtime_error(
+        "IrisNp2 does not yet support collision checkers with nonzero "
+        "padding.");
+  }
+
   unused(domain);
-  unused(options);
-  throw std::runtime_error("IrisNp2 is not yet implemented.");
-  return HPolyhedron{};
+
+  auto stop = std::chrono::high_resolution_clock::now();
+  if (options.sampled_iris_options.verbose) {
+    log()->info(
+        "IrisNp2 execution time : {} ms",
+        std::chrono::duration_cast<std::chrono::milliseconds>(stop - start)
+            .count());
+  }
+  return HPolyhedron::MakeBox(checker.plant().GetPositionLowerLimits(),
+                              checker.plant().GetPositionUpperLimits());
 }
 
 }  // namespace planning
--- planning/iris/test/iris_np2_test.cc
+++ planning/iris/test/iris_np2_test.cc
@@ -2,8 +2,82 @@
 
 #include <gtest/gtest.h>
 
+#include "drake/common/test_utilities/expect_throws_message.h"
+#include "drake/geometry/optimization/hpolyhedron.h"
+#include "drake/planning/iris/iris_common.h"
+#include "drake/planning/iris/test/iris_test_utilities.h"
+
 namespace drake {
 namespace planning {
-namespace {}  // namespace
+namespace {
+
+using Eigen::VectorX;
+using geometry::Sphere;
+using geometry::optimization::HPolyhedron;
+using symbolic::Expression;
+using symbolic::Variable;
+
+// Reproduced from the IrisInConfigurationSpace unit tests.
+TEST_F(JointLimits1D, JointLimitsBasic) {
+  IrisNp2Options options;
+  auto sgcc_ptr = dynamic_cast<SceneGraphCollisionChecker*>(checker_.get());
+  ASSERT_TRUE(sgcc_ptr != nullptr);
+  HPolyhedron region =
+      IrisNp2(*sgcc_ptr, starting_ellipsoid_, domain_, options);
+  CheckRegion(region);
+}
+
+// Check unsupported features.
+TEST_F(JointLimits1D, UnsupportedOptions) {
+  IrisNp2Options options;
+  auto sgcc_ptr = dynamic_cast<SceneGraphCollisionChecker*>(checker_.get());
+  ASSERT_TRUE(sgcc_ptr != nullptr);
+
+  options.sampled_iris_options.containment_points = Eigen::MatrixXd::Zero(1, 3);
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      IrisNp2(*sgcc_ptr, starting_ellipsoid_, domain_, options),
+      ".*additional containment points.*");
+  options.sampled_iris_options.containment_points = std::nullopt;
+
+  solvers::MathematicalProgram prog;
+  options.sampled_iris_options.prog_with_additional_constraints = &prog;
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      IrisNp2(*sgcc_ptr, starting_ellipsoid_, domain_, options),
+      ".*additional constriants.*");
+  options.sampled_iris_options.prog_with_additional_constraints = nullptr;
+
+  VectorX<Variable> varable_vector(1);
+  VectorX<Expression> expression_vector(1);
+  expression_vector[0] = varable_vector[0] + 1;
+  options.parameterization =
+      IrisParameterizationFunction(expression_vector, varable_vector);
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      IrisNp2(*sgcc_ptr, starting_ellipsoid_, domain_, options),
+      ".*parameterized subspace.*");
+  options.parameterization = IrisParameterizationFunction();
+
+  const Sphere sphere(0.1);
+  const BodyShapeDescription body_shape{sphere, {}, "limits", "movable"};
+  sgcc_ptr->AddCollisionShape("test", body_shape);
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      IrisNp2(*sgcc_ptr, starting_ellipsoid_, domain_, options),
+      ".*added collision shapes.*");
+  sgcc_ptr->RemoveAllAddedCollisionShapes();
+}
+
+// Check padding as an unsupported feature. (We have to use a test environment
+// with multiple collision geometries.)
+TEST_F(DoublePendulum, PaddingUnsupported) {
+  IrisNp2Options options;
+  auto sgcc_ptr = dynamic_cast<SceneGraphCollisionChecker*>(checker_.get());
+  ASSERT_TRUE(sgcc_ptr != nullptr);
+
+  sgcc_ptr->SetPaddingAllRobotEnvironmentPairs(1.0);
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      IrisNp2(*sgcc_ptr, starting_ellipsoid_, domain_, options),
+      ".*nonzero padding.*");
+}
+
+}  // namespace
 }  // namespace planning
 }  // namespace drake
