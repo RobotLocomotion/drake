3c015039e90836026dec5d9916c6e946e3bc0695
Vincent Kurtz <vjkurtz@gmail.com>
2025-05-20 16:38:52 -0500

    set up integrator step test

--- systems/analysis/test/convex_integrator_test.cc
+++ systems/analysis/test/convex_integrator_test.cc
@@ -9,6 +9,7 @@
 namespace drake {
 namespace systems {
 
+using Eigen::VectorXd;
 using multibody::AddMultibodyPlantSceneGraph;
 using multibody::Parser;
 
@@ -51,6 +52,36 @@ GTEST_TEST(ConvexIntegratorTest, TestConstruction) {
 
   integrator.set_plant(&plant);
   integrator.Initialize();
+}
+
+GTEST_TEST(ConvexIntegratorTest, TestStep) {
+  // Create a simple system
+  DiagramBuilder<double> builder;
+  auto [plant, scene_graph] = AddMultibodyPlantSceneGraph(&builder, 0.0);
+  Parser(&plant, &scene_graph).AddModelsFromString(double_pendulum_xml, "xml");
+  plant.Finalize();
+  auto diagram = builder.Build();
+  auto diagram_context = diagram->CreateDefaultContext();
+  Context<double>& plant_context =
+      plant.GetMyMutableContextFromRoot(diagram_context.get());
+
+  // Set up the integrator
+  ConvexIntegrator<double> integrator(*diagram, &plant, diagram_context.get());
+  integrator.Initialize();
+
+  // Set initial conditions
+  VectorXd q0(2);
+  q0 << 0.1, 0.2;
+  plant.SetPositions(&plant_context, q0);
+
+  // Perform a step
+  const double dt = 0.01;
+  EXPECT_TRUE(integrator.IntegrateWithSingleFixedStepToTime(dt));
+  EXPECT_NEAR(plant_context.get_time(), dt,
+              std::numeric_limits<double>::epsilon());
+
+  const auto& q = plant.GetPositions(plant_context);
+  fmt::print("q after step: {}\n", fmt_eigen(q.transpose()));
 }
 
 }  // namespace systems
