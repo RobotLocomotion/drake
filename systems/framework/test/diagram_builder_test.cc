6bda1f71c1c62ed965aab58c0b349df0660804ba
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-05-27 08:07:09 -0700

    [systems] Add DiagramBuilder::Disconnect (#23035)

--- systems/framework/test/diagram_builder_test.cc
+++ systems/framework/test/diagram_builder_test.cc
@@ -736,6 +736,41 @@ TEST_F(DiagramBuilderSolePortsTest, ConnectionMap) {
     const InputPortLocator in1_input{in1_, InputPortIndex{0}};
     EXPECT_EQ(builder_.connection_map().at(in1_input), in1out1_output);
   }
+
+  // Disconnecting works correctly.
+  builder_.Disconnect(in1out1_->get_output_port(), in1_->get_input_port());
+  ASSERT_EQ(builder_.connection_map().size(), 1);
+  {
+    const OutputPortLocator out1_output{out1_, OutputPortIndex{0}};
+    const InputPortLocator in1out1_input{in1out1_, InputPortIndex{0}};
+    EXPECT_EQ(builder_.connection_map().at(in1out1_input), out1_output);
+  }
+
+  // Reconnect it again.
+  builder_.Connect(*in1out1_, *in1_);
+  ASSERT_EQ(builder_.connection_map().size(), 2);
+  {
+    const OutputPortLocator in1out1_output{in1out1_, OutputPortIndex{0}};
+    const InputPortLocator in1_input{in1_, InputPortIndex{0}};
+    EXPECT_EQ(builder_.connection_map().at(in1_input), in1out1_output);
+  }
+}
+
+// Disconnecting a non-existent connection will throw.
+TEST_F(DiagramBuilderSolePortsTest, DisconnectError) {
+  const OutputPort<double>& source = out1_->get_output_port();
+  const InputPort<double>& dest = in1_->get_input_port();
+  builder_.Connect(source, dest);
+
+  // Wrong `source`.
+  const OutputPort<double>& source_wrong = in1out1_->get_output_port();
+  DRAKE_EXPECT_THROWS_MESSAGE(builder_.Disconnect(source_wrong, dest),
+                              ".*source=y0, dest=in.*not.*connected.*");
+
+  // Wrong `dest`.
+  const InputPort<double>& dest_wrong = in1out1_->get_input_port();
+  DRAKE_EXPECT_THROWS_MESSAGE(builder_.Disconnect(source, dest_wrong),
+                              ".*source=y0, dest=u0.*not.*connected.*");
 }
 
 // The cascade synonym also works.
