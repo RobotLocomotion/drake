5f67528a0c2dc681e759786f2aea78f0acfb34e5
Vincent Kurtz <vincent.kurtz@tri.global>
2025-06-05 10:27:07 -0400

    implement inclined plane demo

---
+++ examples/multibody/inclined_plane_with_body/dynamic_friction_demo.cc
@@ -0,0 +1,211 @@
+#include <iostream>
+#include <memory>
+
+#include <gflags/gflags.h>
+
+#include "drake/geometry/scene_graph.h"
+#include "drake/lcm/drake_lcm.h"
+#include "drake/multibody/benchmarks/inclined_plane/inclined_plane_plant.h"
+#include "drake/multibody/plant/contact_results_to_lcm.h"
+#include "drake/multibody/plant/multibody_plant_config_functions.h"
+#include "drake/multibody/tree/revolute_joint.h"
+#include "drake/systems/analysis/convex_integrator.h"
+#include "drake/systems/analysis/simulator.h"
+#include "drake/systems/analysis/simulator_print_stats.h"
+#include "drake/systems/controllers/pid_controller.h"
+#include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/primitives/constant_vector_source.h"
+#include "drake/visualization/visualization_config_functions.h"
+
+namespace drake {
+namespace multibody {
+namespace examples {
+namespace dynamic_friction_demo {
+namespace {
+
+// Simulates a block on an inclined plane that is slowly raised until the block
+// slides off. Different choices of static and dynamic friction coefficients
+// give different results.
+
+DEFINE_double(mbp_time_step, 0.0, "Time step for plant (0 => continuous).");
+DEFINE_double(simulation_time, 10.0, "Simulation duration in seconds");
+DEFINE_double(static_friction, 1.0, "Coefficient of static friction.");
+DEFINE_double(dynamic_friction, 0.5, "Coefficient of dynamic friciton.");
+DEFINE_double(stiction_tolerance, 1e-4, "Stiction velocity (m/s).");
+DEFINE_bool(use_hydro, true, "Whether to use hydroelastic contact.");
+
+using math::RigidTransform;
+using geometry::SceneGraphConfig;
+using systems::controllers::PidController;
+using systems::ConstantVectorSource;
+
+int do_main() {
+  auto meshcat = std::make_shared<drake::geometry::Meshcat>();
+  systems::DiagramBuilder<double> builder;
+  
+  const drake::multibody::CoulombFriction<double> friction(
+      FLAGS_static_friction, FLAGS_dynamic_friction);
+
+  MultibodyPlantConfig plant_config;
+  plant_config.time_step = FLAGS_mbp_time_step;
+  plant_config.stiction_tolerance = FLAGS_stiction_tolerance;
+  auto [plant, scene_graph] =
+      multibody::AddMultibodyPlant(plant_config, &builder);
+
+  if (FLAGS_use_hydro) {
+    SceneGraphConfig sg_config;
+    sg_config.default_proximity_properties.compliance_type = "compliant";
+    scene_graph.set_config(sg_config);
+  }
+
+  // Add an inlined plane with actuation
+  const double LPx = 2.0;  // plane dimensions
+  const double LPy = 1.0;
+  const double LPz = 0.05;
+  
+  const SpatialInertia<double> M_PPcm_P =
+      SpatialInertia<double>::SolidBoxWithMass(10.0, LPx, LPy, LPz);
+  const RigidBody<double>& plane = plant.AddRigidBody("plane", M_PPcm_P);
+  const RigidTransform<double> X_PG;
+  const Vector4<double> lightGreen(0.5, 1.0, 0.7, 1.0);
+  plant.RegisterVisualGeometry(plane, X_PG, geometry::Box(LPx, LPy, LPz),
+                               "plane_visual", lightGreen);
+  plant.RegisterCollisionGeometry(plane, X_PG, geometry::Box(LPx, LPy, LPz),
+                                  "plane_collision", friction);
+  
+  // Add an actuated revolute joint on the plane
+  const double damping = 100.0;
+  const RevoluteJoint<double>& joint = plant.AddJoint<RevoluteJoint>(
+      "plane_joint", plant.world_body(), RigidTransform<double>(), plane,
+      std::optional<RigidTransform<double>>{}, Eigen::Vector3d::UnitY(),
+      damping);
+  plant.AddJointActuator("actuator", joint); 
+
+  // Add a box on the plane
+  const double LBx = 0.2;  // block dimensions
+  const double LBy = 0.2;
+  const double LBz = 0.1;
+  const SpatialInertia<double> M_BBcm_B =
+      SpatialInertia<double>::SolidBoxWithMass(1.0, LBx, LBy, LBz);
+  const RigidBody<double>& block = plant.AddRigidBody("block", M_BBcm_B);
+  const RigidTransform<double> X_BG;
+  const Vector4<double> lightBlue(0.5, 0.8, 1.0, 1.0);
+  plant.RegisterVisualGeometry(block, X_BG, geometry::Box(LBx, LBy, LBz),
+                               "block_visual", lightBlue);
+  plant.RegisterCollisionGeometry(block, X_BG, geometry::Box(LBx, LBy, LBz),
+                                  "block_collision", friction);
+
+  if (!FLAGS_use_hydro) {
+    // Add sphere collision geometries on the corners
+    const Vector4<double> red(1.0, 0.0, 0.0, 1.0);
+    const double radius = 0.01;
+    int i = 0;
+    for (double x_sign : {-1.0, 1.0}) {
+      for (double y_sign : {-1.0, 1.0}) {
+        const std::string name_spherei =
+            "Sphere" + std::to_string(++i) + "_Geometry";
+        const double x = x_sign * LBx / 2;
+        const double y = y_sign * LBy / 2;
+        const double z = -LBz / 2;
+        const Vector3<double> p_BoSpherei_B(x, y, z);
+        const RigidTransform<double> X_BSpherei(p_BoSpherei_B);
+        plant.RegisterCollisionGeometry(block, X_BSpherei,
+                                         geometry::Sphere(radius),
+                                         name_spherei,
+                                         friction);
+        plant.RegisterVisualGeometry(block, X_BSpherei,
+                                      geometry::Sphere(radius),
+                                      name_spherei, red);
+      }
+    }
+  }
+
+  plant.Finalize();
+
+  // Add a controller to set the plane angle
+  Vector1d kp(50.0);
+  Vector1d ki(0.0);
+  Vector1d kd(100.0);
+  Eigen::MatrixXd Px = Eigen::MatrixXd::Zero(2, 15);
+  Px(0, 0) = 1.0;
+  Px(1, 8) = 1.0;
+  auto ctrl = builder.AddSystem<PidController>(Px, kp, ki, kd);
+
+  const Eigen::Vector2d x_des(1.0, 0.0);
+  auto x_des_sender = builder.AddSystem<ConstantVectorSource>(x_des);
+
+  builder.Connect(
+    ctrl->get_output_port(),
+    plant.get_actuation_input_port()
+  );
+  builder.Connect(
+    plant.get_state_output_port(),
+    ctrl->get_input_port_estimated_state()
+  );
+  builder.Connect(
+    x_des_sender->get_output_port(),
+    ctrl->get_input_port_desired_state()
+  );
+ 
+  // Visualizer setup
+  visualization::VisualizationConfig vis_config;
+  vis_config.publish_period = std::numeric_limits<double>::infinity();
+  vis_config.publish_contacts = true;
+  visualization::ApplyVisualizationConfig(vis_config, &builder, nullptr, &plant,
+                                          &scene_graph, meshcat);
+
+  // Compile the diagram
+  auto diagram = builder.Build();
+  std::unique_ptr<systems::Context<double>> diagram_context =
+      diagram->CreateDefaultContext();
+  systems::Context<double>& plant_context =
+      diagram->GetMutableSubsystemContext(plant, diagram_context.get());
+
+  // Set initial conditions
+  const Vector3<double> p_WoBo_W(-0.8, 0.0, 0.1);
+  const math::RigidTransform<double> X_WB(p_WoBo_W);
+  plant.SetFreeBodyPoseInWorldFrame(&plant_context, block, X_WB);
+ 
+  // Set up the simulator with the convex integrator
+  systems::Simulator<double> simulator(*diagram, std::move(diagram_context));
+  if (FLAGS_mbp_time_step == 0.0) {
+    systems::ConvexIntegrator<double>& ci =
+        simulator.reset_integrator<systems::ConvexIntegrator<double>>();
+    ci.set_plant(&plant);
+    ci.set_maximum_step_size(0.001);   // fixed time step dt
+    ci.set_print_solver_stats(false);
+    ci.set_log_solver_stats(false);
+  }
+  simulator.set_publish_every_time_step(true);
+  simulator.set_target_realtime_rate(1.0);
+  simulator.Initialize();
+    
+  // Wait for meshcat to load
+  std::cout << "Press [ENTER] to continue ...\n";
+  getchar();
+
+  const double recording_frames_per_second = 32;
+  meshcat->StartRecording(recording_frames_per_second);
+  simulator.AdvanceTo(FLAGS_simulation_time);
+  meshcat->StopRecording();
+  meshcat->PublishRecording();
+
+  PrintSimulatorStatistics(simulator);
+  
+  // Wait for meshcat to load
+  std::cout << "Press [ENTER] to continue ...\n";
+  getchar();
+
+  return 0;
+}
+
+}  // namespace
+}  // namespace dynamic_friction_demo
+}  // namespace examples
+}  // namespace multibody
+}  // namespace drake
+
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  return drake::multibody::examples::dynamic_friction_demo::do_main();
+}
