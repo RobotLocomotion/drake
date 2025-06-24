0433c29b9f6c886223400e767d05754d60d33136
Vincent Kurtz <vjkurtz@gmail.com>
2025-06-24 16:52:00 -0500

    improve dynamic friciton demo

--- examples/multibody/inclined_plane_with_body/dynamic_friction_demo.cc
+++ examples/multibody/inclined_plane_with_body/dynamic_friction_demo.cc
@@ -28,22 +28,30 @@ namespace {
 // give different results.
 
 DEFINE_double(mbp_time_step, 0.0, "Time step for plant (0 => continuous).");
-DEFINE_double(simulation_time, 10.0, "Simulation duration in seconds");
-DEFINE_double(static_friction, 1.0, "Coefficient of static friction.");
-DEFINE_double(dynamic_friction, 0.5, "Coefficient of dynamic friciton.");
+DEFINE_double(simulation_time, 12.0, "Simulation duration in seconds");
+DEFINE_double(static_friction, 0.6, "Coefficient of static friction.");
+DEFINE_double(dynamic_friction, 0.2, "Coefficient of dynamic friciton.");
 DEFINE_double(stiction_tolerance, 1e-4, "Stiction velocity (m/s).");
-DEFINE_bool(use_hydro, true, "Whether to use hydroelastic contact.");
+DEFINE_bool(use_hydro, false, "Whether to use hydroelastic contact.");
+DEFINE_double(accuracy, 1e-3, "Convex integrator accuracy.");
+DEFINE_double(integrator_time_step, 0.01, "Integrator time step.");
+DEFINE_bool(use_error_control, true,
+            "Whether to use error control in the integrator.");
 
+using Eigen::Vector3d;
 using geometry::SceneGraphConfig;
 using math::RigidTransform;
+using multibody::CoulombFriction;
 using systems::ConstantVectorSource;
 using systems::controllers::PidController;
 
 int do_main() {
   auto meshcat = std::make_shared<drake::geometry::Meshcat>();
+  meshcat->SetCameraPose(Vector3d(-0.1, -0.4, 0.1), Vector3d(-0.2, 0.0, 0.1));
+
   systems::DiagramBuilder<double> builder;
 
-  const drake::multibody::CoulombFriction<double> friction(
+  const CoulombFriction<double> friction(
       FLAGS_static_friction, FLAGS_dynamic_friction);
 
   MultibodyPlantConfig plant_config;
@@ -59,17 +67,18 @@ int do_main() {
   }
 
   // Add an inlined plane with actuation
-  const double LPx = 2.0;  // plane dimensions
-  const double LPy = 1.0;
-  const double LPz = 0.05;
+  const double LPx = 0.4064;  // 16 inches
+  const double LPy = 0.3038;  // 12 inches
+  const double LPz = 0.02;
 
+  const Vector3d p0(-LPx / 2, 0.0, LPz / 2);
   const SpatialInertia<double> M_PPcm_P =
-      SpatialInertia<double>::SolidBoxWithMass(10.0, LPx, LPy, LPz);
+      SpatialInertia<double>::SolidBoxWithMass(1.0, LPx, LPy, LPz).Shift(-p0);
   const RigidBody<double>& plane = plant.AddRigidBody("plane", M_PPcm_P);
-  const RigidTransform<double> X_PG;
-  const Vector4<double> lightGreen(0.5, 1.0, 0.7, 1.0);
+  const RigidTransform<double> X_PG(p0);
+  const Vector4<double> planeColor(0.7, 0.5, 0.2, 1.0);
   plant.RegisterVisualGeometry(plane, X_PG, geometry::Box(LPx, LPy, LPz),
-                               "plane_visual", lightGreen);
+                               "plane_visual", planeColor);
   plant.RegisterCollisionGeometry(plane, X_PG, geometry::Box(LPx, LPy, LPz),
                                   "plane_collision", friction);
 
@@ -77,43 +86,50 @@ int do_main() {
   const double damping = 100.0;
   const RevoluteJoint<double>& joint = plant.AddJoint<RevoluteJoint>(
       "plane_joint", plant.world_body(), RigidTransform<double>(), plane,
-      std::optional<RigidTransform<double>>{}, Eigen::Vector3d::UnitY(),
+      std::optional<RigidTransform<double>>{}, Vector3d::UnitY(),
       damping);
   plant.AddJointActuator("actuator", joint);
 
   // Add a box on the plane
-  const double LBx = 0.2;  // block dimensions
-  const double LBy = 0.2;
-  const double LBz = 0.1;
+  const double LBx = 0.08;
+  const double LBy = 0.08;
+  const double LBz = 0.06;
   const SpatialInertia<double> M_BBcm_B =
-      SpatialInertia<double>::SolidBoxWithMass(1.0, LBx, LBy, LBz);
+      SpatialInertia<double>::SolidBoxWithMass(0.096, LBx, LBy, LBz);
   const RigidBody<double>& block = plant.AddRigidBody("block", M_BBcm_B);
   const RigidTransform<double> X_BG;
-  const Vector4<double> lightBlue(0.5, 0.8, 1.0, 1.0);
+  const Vector4<double> boxColor(0.9, 0.7, 0.4, 1.0);
   plant.RegisterVisualGeometry(block, X_BG, geometry::Box(LBx, LBy, LBz),
-                               "block_visual", lightBlue);
+                               "block_visual", boxColor);
   plant.RegisterCollisionGeometry(block, X_BG, geometry::Box(LBx, LBy, LBz),
                                   "block_collision", friction);
+
+  // Add ground collision
+  plant.RegisterCollisionGeometry(
+      plant.world_body(), RigidTransform<double>(Vector3d(0, 0, -5)),
+      geometry::Box(20, 20, 10.0), "ground_collision", friction);
 
   if (!FLAGS_use_hydro) {
     // Add sphere collision geometries on the corners
     const Vector4<double> red(1.0, 0.0, 0.0, 1.0);
-    const double radius = 0.01;
+    const double radius = 0.001;
     int i = 0;
     for (double x_sign : {-1.0, 1.0}) {
       for (double y_sign : {-1.0, 1.0}) {
-        const std::string name_spherei =
-            "Sphere" + std::to_string(++i) + "_Geometry";
-        const double x = x_sign * LBx / 2;
-        const double y = y_sign * LBy / 2;
-        const double z = -LBz / 2;
-        const Vector3<double> p_BoSpherei_B(x, y, z);
-        const RigidTransform<double> X_BSpherei(p_BoSpherei_B);
-        plant.RegisterCollisionGeometry(block, X_BSpherei,
-                                        geometry::Sphere(radius), name_spherei,
-                                        friction);
-        plant.RegisterVisualGeometry(
-            block, X_BSpherei, geometry::Sphere(radius), name_spherei, red);
+        for (double z_sign : {-1.0, 1.0}) {
+          const std::string name_spherei =
+              "Sphere" + std::to_string(++i) + "_Geometry";
+          const double x = x_sign * LBx / 2;
+          const double y = y_sign * LBy / 2;
+          const double z = z_sign * LBz / 2;
+          const Vector3<double> p_BoSpherei_B(x, y, z);
+          const RigidTransform<double> X_BSpherei(p_BoSpherei_B);
+          plant.RegisterCollisionGeometry(block, X_BSpherei,
+                                          geometry::Sphere(radius),
+                                          name_spherei, friction);
+          plant.RegisterVisualGeometry(
+              block, X_BSpherei, geometry::Sphere(radius), name_spherei, red);
+        }
       }
     }
   }
@@ -129,7 +145,7 @@ int do_main() {
   Px(1, 8) = 1.0;
   auto ctrl = builder.AddSystem<PidController>(Px, kp, ki, kd);
 
-  const Eigen::Vector2d x_des(1.0, 0.0);
+  const Eigen::Vector2d x_des(0.7, 0.0);
   auto x_des_sender = builder.AddSystem<ConstantVectorSource>(x_des);
 
   builder.Connect(ctrl->get_output_port(), plant.get_actuation_input_port());
@@ -153,7 +169,7 @@ int do_main() {
       diagram->GetMutableSubsystemContext(plant, diagram_context.get());
 
   // Set initial conditions
-  const Vector3<double> p_WoBo_W(-0.8, 0.0, 0.1);
+  const Vector3<double> p_WoBo_W(-0.31, 0.0, LBz / 2 + LPz + 0.002);
   const math::RigidTransform<double> X_WB(p_WoBo_W);
   plant.SetFreeBodyPoseInWorldFrame(&plant_context, block, X_WB);
 
@@ -163,7 +179,9 @@ int do_main() {
     systems::ConvexIntegrator<double>& ci =
         simulator.reset_integrator<systems::ConvexIntegrator<double>>();
     ci.set_plant(&plant);
-    ci.set_maximum_step_size(0.001);  // fixed time step dt
+    ci.set_maximum_step_size(FLAGS_integrator_time_step);
+    ci.set_fixed_step_mode(!FLAGS_use_error_control);
+    ci.set_target_accuracy(FLAGS_accuracy);
   }
   simulator.set_publish_every_time_step(true);
   simulator.set_target_realtime_rate(1.0);
