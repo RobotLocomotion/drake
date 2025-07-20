83c6d2076ba1ba4deff3816c1f731c8d20e2f999
juan.med <21977733+juanmed@users.noreply.github.com>
2025-07-21 02:13:04 +0900

    Add comments and reorganize a bit the code

--- geometry/shape_specification.cc
+++ geometry/shape_specification.cc
@@ -436,19 +436,31 @@ double CalcVolume(const Shape& shape) {
 template <typename T>
 std::optional<Eigen::Vector3<T>> GetNormalAtPointForBox(
     const Box& box, const Eigen::Vector3<T>& p) {
-    
   const T tol = 1e-5;
 
+  // Extract box half dimensions
+  const double h_w = box.width() / 2;   // half_width
+  const double h_d = box.depth() / 2;   // half_depth
+  const double h_h = box.height() / 2;  // half_height
+
+  // A container for all planes defined by the normals of a box
+  // and their distance to the origin. Each plane is defined by
+  // a 4-vector (n, d)^T where n is the normal vector and d is
+  // the distance to the plane along the normal direction. If a
+  // point p lies on a plane (n, d) then n*p - d = 0. This is
+  // used to verify if a point p lines on a plane; if so, the normal
+  // at point p is n.
   Eigen::Matrix<T, 4, 6> box_planes = Eigen::Matrix<T, 4, 6>::Zero();
-  const double h_w = box.width() / 2;  // half_width
-  const double h_d = box.depth() / 2;  // half_depth
-  const double h_h = box.height() / 2; // half_height
-  Eigen::Vector4<T> p_h(p.x(), p.y(), p.z(), 1.0);
+  // clang-format off
   box_planes << 1.0, -1.0, 0.0,  0.0, 0.0, 0.0,
                 0.0,  0.0, 1.0, -1.0, 0.0, 0.0,
                 0.0,  0.0, 0.0,  0.0, 1.0, -1.0,
                 -h_w, -h_w, -h_d, -h_d, -h_h, -h_h;
-  
+  // clang-format on
+
+  // Iterate through the planes. Return the normal vector of first
+  // plane that contains the point.
+  Eigen::Vector4<T> p_h(p.x(), p.y(), p.z(), 1.0);
   for (Eigen::Index c = 0; c < box_planes.cols(); ++c) {
     if (std::abs(box_planes.col(c).dot(p_h)) < tol) {
       return box_planes.block(0, c, 3, 1);
