7729dfe7b8d5fef9a23a5ba23f94197bc20189ac
juan.med <21977733+juanmed@users.noreply.github.com>
2025-07-23 13:57:38 +0900

    Working conveyor belt with continuous plant

--- examples/conveyor_belt/conveyor_belt.cc
+++ examples/conveyor_belt/conveyor_belt.cc
@@ -1,8 +1,9 @@
-#include <drake/geometry/meshcat.h>
-#include <drake/geometry/meshcat_visualizer.h>
 #include <gflags/gflags.h>
 
+#include "drake/geometry/meshcat.h"
+#include "drake/geometry/meshcat_visualizer.h"
 #include "drake/geometry/scene_graph.h"
+#include "drake/multibody/meshcat/contact_visualizer.h"
 #include "drake/multibody/parsing/parser.h"
 #include "drake/multibody/plant/multibody_plant.h"
 #include "drake/systems/analysis/simulator.h"
@@ -13,13 +14,12 @@ namespace examples {
 namespace conveyor_belt {
 namespace {
 
-DEFINE_double(max_time_step, 1.0e-4,
-              "Simulation time step used for integrator.");
+DEFINE_double(max_time_step, 1e-3, "Simulation time step used for integrator.");
 
-int do_main() {
+int do_main_continous_plant() {
   systems::DiagramBuilder<double> builder;
   auto [plant, scene_graph] =
-      multibody::AddMultibodyPlantSceneGraph(&builder, FLAGS_max_time_step);
+      multibody::AddMultibodyPlantSceneGraph(&builder, 0.0);
   std::string conveyor_belt_url =
       "package://drake/examples/conveyor_belt/conveyor_belt.sdf";
   multibody::Parser(&builder).AddModelsFromUrl(conveyor_belt_url);
@@ -29,6 +29,14 @@ int do_main() {
   auto meshcat = std::make_shared<geometry::Meshcat>();
   geometry::MeshcatVisualizer<double>::AddToBuilder(&builder, scene_graph,
                                                     meshcat);
+  geometry::MeshcatVisualizerParams meshcat_params;
+  meshcat_params.delete_on_initialization_event = false;
+  auto& visualizer = geometry::MeshcatVisualizerd::AddToBuilder(
+      &builder, scene_graph, meshcat, std::move(meshcat_params));
+  multibody::meshcat::ContactVisualizerParams cparams;
+  cparams.newtons_per_meter = 60.0;
+  multibody::meshcat::ContactVisualizerd::AddToBuilder(&builder, plant, meshcat,
+                                                       std::move(cparams));
 
   // Set up context
   std::unique_ptr<systems::Diagram<double>> diagram = builder.Build();
@@ -36,11 +44,16 @@ int do_main() {
       diagram->CreateDefaultContext();
   diagram->SetDefaultContext(context.get());
 
+  // Force visualization
+  diagram->ForcedPublish(*context);
+
   // Set up simulator
   systems::Simulator<double> simulator(*diagram);
   simulator.set_target_realtime_rate(1.0);
   simulator.Initialize();
-  simulator.AdvanceTo(10.0);
+  visualizer.StartRecording();
+  simulator.AdvanceTo(20.0);
+  visualizer.PublishRecording();
 
   return 0;
 }
@@ -53,6 +66,6 @@ int do_main() {
 int main(int argc, char* argv[]) {
   // Initialize gflags.
   gflags::ParseCommandLineFlags(&argc, &argv, true);
-  drake::examples::conveyor_belt::do_main();
+  drake::examples::conveyor_belt::do_main_continous_plant();
   return 0;
 }
