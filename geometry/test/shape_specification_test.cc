9dc2d8d82fc7fb635eb13cd2c2d8c2cfdffa190c
juan.med <21977733+juanmed@users.noreply.github.com>
2025-07-26 18:54:38 +0900

    Move point cloud sample gen to another file

--- geometry/test/shape_specification_test.cc
+++ geometry/test/shape_specification_test.cc
@@ -1,4 +1,5 @@
 #include "drake/geometry/shape_specification.h"
+#include "shape_to_point_cloud.h"
 
 #include <filesystem>
 #include <iostream>
@@ -9,14 +10,11 @@
 #include "drake/common/find_resource.h"
 #include "drake/common/fmt_eigen.h"
 #include "drake/common/overloaded.h"
-#include "drake/common/random.h"
-#include "drake/common/schema/stochastic.h"
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
 #include "drake/common/test_utilities/expect_no_throw.h"
 #include "drake/common/test_utilities/expect_throws_message.h"
 #include "drake/common/test_utilities/is_dynamic_castable.h"
 #include "drake/common/unused.h"
-#include "drake/math/random_rotation.h"
 
 namespace drake {
 namespace geometry {
@@ -1046,93 +1044,6 @@ GTEST_TEST(ShapeTest, MoveConstructor) {
   EXPECT_TRUE(orig.source().in_memory().mesh_file.contents().empty());
 }
 
-struct PointCloud {
-  std::vector<Eigen::Vector3d> points;
-  std::vector<Eigen::Vector3d> normals;
-};
-
-PointCloud SampleBoxSurface(const Box& box, const math::RigidTransformd& T,
-                            int n) {
-  PointCloud box_cloud;
-  box_cloud.points.reserve(n);
-  box_cloud.normals.reserve(n);
-
-  // Half dimensions
-  const double hw = box.width() / 2;
-  const double hd = box.depth() / 2;
-  const double hh = box.height() / 2;
-
-  // Create a random number generator.
-  drake::RandomGenerator generator(0);
-  drake::schema::Uniform uniform_x(-hw, hw);
-  drake::schema::Uniform uniform_y(-hd, hd);
-  drake::schema::Uniform uniform_z(-hh, hh);
-
-  int k = n / 6;
-
-  // Plane in +x direction
-  for (int i = 0; i < k; ++i) {
-    double y = uniform_y.Sample(&generator);
-    double z = uniform_z.Sample(&generator);
-    Eigen::Vector3d p(hw, y, z);
-    Eigen::Vector3d normal(1., 0., 0.);
-    box_cloud.points.push_back(T * p);
-    box_cloud.normals.push_back(T.rotation() * normal);
-  }
-
-  // Plane in -x direction
-  for (int i = 0; i < k; ++i) {
-    double y = uniform_y.Sample(&generator);
-    double z = uniform_z.Sample(&generator);
-    Eigen::Vector3d p(-hw, y, z);
-    Eigen::Vector3d normal(-1., 0., 0.);
-    box_cloud.points.push_back(T * p);
-    box_cloud.normals.push_back(T.rotation() * normal);
-  }
-
-  // Plane in +y direction
-  for (int i = 0; i < k; ++i) {
-    double x = uniform_x.Sample(&generator);
-    double z = uniform_z.Sample(&generator);
-    Eigen::Vector3d p(x, hd, z);
-    Eigen::Vector3d normal(0., 1., 0.);
-    box_cloud.points.push_back(T * p);
-    box_cloud.normals.push_back(T.rotation() * normal);
-  }
-
-  // Plane in -y direction
-  for (int i = 0; i < k; ++i) {
-    double x = uniform_x.Sample(&generator);
-    double z = uniform_z.Sample(&generator);
-    Eigen::Vector3d p(x, -hd, z);
-    Eigen::Vector3d normal(0., -1., 0.);
-    box_cloud.points.push_back(T * p);
-    box_cloud.normals.push_back(T.rotation() * normal);
-  }
-
-  // Plane in +z direction
-  for (int i = 0; i < k; ++i) {
-    double x = uniform_x.Sample(&generator);
-    double y = uniform_y.Sample(&generator);
-    Eigen::Vector3d p(x, y, hh);
-    Eigen::Vector3d normal(0., 0., 1.);
-    box_cloud.points.push_back(T * p);
-    box_cloud.normals.push_back(T.rotation() * normal);
-  }
-
-  // Plane in -z direction
-  for (int i = 0; i < k; ++i) {
-    double x = uniform_x.Sample(&generator);
-    double y = uniform_y.Sample(&generator);
-    Eigen::Vector3d p(x, y, -hh);
-    Eigen::Vector3d normal(0., 0., -1.);
-    box_cloud.points.push_back(T * p);
-    box_cloud.normals.push_back(T.rotation() * normal);
-  }
-
-  return box_cloud;
-}
-
 GTEST_TEST(ShapeTest, NormalAtBoxPoint) {
   const double tol = 1e-5;
 
@@ -1161,23 +1072,6 @@ GTEST_TEST(ShapeTest, NormalAtBoxPoint) {
         GetNormalAtPoint<double>(box, box_pc.points.at(i));
     EXPECT_LT((box_pc.normals.at(i) - n.value()).norm(), tol);
   }
-}
-
-PointCloud SampleSphereSurface(const Sphere& sphere, const RigidTransformd& T,
-                               int n) {
-  PointCloud cloud;
-  cloud.points.reserve(n);
-  cloud.normals.reserve(n);
-
-  drake::RandomGenerator rgn(0);
-  const double r = sphere.radius();
-  for (int i = 0; i < n; ++i) {
-    Eigen::Matrix3d m =
-        math::UniformlyRandomRotationMatrix<double>(&rgn).matrix();
-    cloud.points.emplace_back(r * m.col(0));
-    cloud.normals.emplace_back(m.col(0));
-  }
-  return cloud;
 }
 
 GTEST_TEST(ShapeTest, NormalAtSpherePoint) {
