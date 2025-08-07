99bfa8be32c7da75f63a5b1ab3e9cbef7002caff
Matthew Woehlke <matthew.woehlke@kitware.com>
2025-08-07 14:49:26 -0400

    Remove MOSEK from macOS wheel
    Modify macOS wheel build to remove the bundled MOSEK library and patch
    libdrake to point to the PyPI-supplied version instead.

--- tools/wheel/macos/change_lpath.py
+++ tools/wheel/macos/change_lpath.py
@@ -1,14 +1,50 @@
 """
 Given an input artifact (binary or library), apply a specified change to the
-path prefix of all libraries to which the artifact is linked.
+path prefix of all libraries to which the artifact is linked. The input can
+also be an artifact inside a wheel.
 """
 
 import argparse
+import os
 import re
+import shutil
 import subprocess
 import sys
+import tempfile
+import zipfile
 
 from tools.install import otool
+
+
+def _extract(wheel, path, dst):
+    """
+    Extracts a file from a wheel (zipfile) and returns it as a TemporaryFile.
+    """
+    with zipfile.ZipFile(wheel, 'r') as zip_file:
+        file_info = zip_file.getinfo(path)
+
+        with open(dst, 'wb') as out:
+            shutil.copyfileobj(zip_file.open(path, 'r'), out)
+
+
+def _replace(wheel, path, content):
+    """
+    Replaces a file in a wheel (zipfile) with the contents of a file-like
+    object.
+    """
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_path = os.path.join(tmp_dir, 'wheel')
+
+        with zipfile.ZipFile(tmp_path, 'w', zipfile.ZIP_DEFLATED) as zip_new:
+            with zipfile.ZipFile(wheel, 'r') as zip_old:
+                for item in zip_old.infolist():
+                    if item.filename == path:
+                        content.seek(0)
+                        zip_new.writestr(item, content.read())
+                    else:
+                        zip_new.writestr(item, zip_old.read(item.filename))
+
+        os.replace(tmp_path, wheel)
 
 
 def _chlpath(path, libs, old, new):
@@ -41,6 +77,9 @@ def main(args):
         '--new', required=True,
         help='Replacement prefix')
     parser.add_argument(
+        '--wheel',
+        help='Modify a file inside the specified wheel.')
+    parser.add_argument(
         'library', nargs='+',
         help='Dynamic library to modify')
 
@@ -49,8 +88,20 @@ def main(args):
 
     # Get list of dependent libraries and modify their prefixes.
     for path in options.library:
-        libs = otool.linked_libraries(path)
-        _chlpath(path, libs=libs, old=options.old, new=options.new)
+        if options.wheel:
+            with tempfile.TemporaryDirectory() as tmp_dir:
+                lib_path = os.path.join(tmp_dir, 'lib')
+                _extract(options.wheel, path, lib_path)
+
+                libs = otool.linked_libraries(lib_path)
+                _chlpath(lib_path, libs=libs, old=options.old, new=options.new)
+
+                with open(lib_path, 'rb') as lib_file:
+                    _replace(options.wheel, path, lib_file)
+
+        else:
+            libs = otool.linked_libraries(path)
+            _chlpath(path, libs=libs, old=options.old, new=options.new)
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
