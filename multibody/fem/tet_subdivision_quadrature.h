8dd93b3369028ed4801264591ed58b67c16c9be4
Kyle Cesare <kcesare@gmail.com>
2025-09-02 17:36:52 -0600

    Make TetSubdivisionQuadrature a closed template

--- multibody/fem/tet_subdivision_quadrature.h
+++ multibody/fem/tet_subdivision_quadrature.h
@@ -49,6 +49,9 @@ class TetSubdivisionQuadrature final
   using WeightsType = typename Base::WeightsType;  // array<double, 4^N>
   using VectorD = typename Base::VectorD;          // Vector3<double>
 
+  static_assert(0 <= subdivisions && subdivisions <= 4,
+                "Only 0, 1, 2, 3, and 4 subdivisions are supported.");
+
   /* Constructor builds the data (points + weights) and then forwards it to the
    base class constructor. */
   TetSubdivisionQuadrature() : Base(MakePointsAndWeights()) {}
@@ -63,65 +66,15 @@ class TetSubdivisionQuadrature final
   /* Returns (points[], weights[]) in a single std::pair, which the base class
    constructor expects.  The number of points is 4^N.  We store them in
    a std::array<Vector3<double>, 4^N> and std::array<double, 4^N>. */
-  static std::pair<LocationsType, WeightsType> MakePointsAndWeights() {
-    /* Recursively find centroids of sub-tets in standard reference domain.
-     We'll accumulate them in a std::vector first, then copy into std::array. */
-
-    /* The vertices of the *reference* tetrahedron. */
-    const Tetrahedron root{{Vector3<double>(0, 0, 0), Vector3<double>(1, 0, 0),
-                            Vector3<double>(0, 1, 0),
-                            Vector3<double>(0, 0, 1)}};
-
-    std::vector<Vector3<double>> centroids;
-    centroids.reserve(num_quadrature_points);
-    SubdivideAndCollectCentroids(root, subdivisions, &centroids);
-
-    /* The final arrays for Quadrature base class constructor. */
-    LocationsType points_array;
-    WeightsType weights_array;
-
-    /* Each sub-tet has volume = (1/6) / (4^N). Each centroid is assigned that
-     weight for integration in the reference tetrahedron. */
-    const double w = (1.0 / 6.0) / static_cast<double>(num_quadrature_points);
-
-    for (int i = 0; i < num_quadrature_points; ++i) {
-      points_array[i] = centroids[i];
-      weights_array[i] = w;
-    }
-
-    return std::make_pair(std::move(points_array), std::move(weights_array));
-  }
+  static std::pair<LocationsType, WeightsType> MakePointsAndWeights();
 
   /* Recursively subdivides a tetrahedron `tet` `level` times, collecting the
    centroid of each final sub-tet in the std::vector `result`. */
   static void SubdivideAndCollectCentroids(
-      const Tetrahedron& tet, int level, std::vector<Vector3<double>>* result) {
-    if (level <= 0) {
-      /* No further subdivision. Store the centroid of `tet`. */
-      result->push_back(ComputeCentroid(tet));
-      return;
-    }
-
-    /* Otherwise, subdivide this tetrahedron into 4 smaller ones by connecting
-     he centroid G to each face: */
-    const Vector3<double> G = ComputeCentroid(tet);
-
-    const Tetrahedron t0{{G, tet.v[0], tet.v[1], tet.v[2]}};
-    const Tetrahedron t1{{G, tet.v[0], tet.v[1], tet.v[3]}};
-    const Tetrahedron t2{{G, tet.v[0], tet.v[2], tet.v[3]}};
-    const Tetrahedron t3{{G, tet.v[1], tet.v[2], tet.v[3]}};
-
-    /* Recurse. */
-    SubdivideAndCollectCentroids(t0, level - 1, result);
-    SubdivideAndCollectCentroids(t1, level - 1, result);
-    SubdivideAndCollectCentroids(t2, level - 1, result);
-    SubdivideAndCollectCentroids(t3, level - 1, result);
-  }
+      const Tetrahedron& tet, int level, std::vector<Vector3<double>>* result);
 
   /* Computes the centroid of a tetrahedron with corners v0,v1,v2,v3. */
-  static Vector3<double> ComputeCentroid(const Tetrahedron& tet) {
-    return (tet.v[0] + tet.v[1] + tet.v[2] + tet.v[3]) / 4.0;
-  }
+  static Vector3<double> ComputeCentroid(const Tetrahedron& tet);
 };
 
 }  // namespace internal
