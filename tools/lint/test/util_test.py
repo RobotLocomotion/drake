7aa208bbe4b287313b3e8c1983bfb9e3309c13ee
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-09-22 12:57:57 -0700

    [ruff] Format tools/lint

--- tools/lint/test/util_test.py
+++ tools/lint/test/util_test.py
@@ -5,7 +5,6 @@ from tools.lint.util import find_all_sources
 
 
 class UtilTest(unittest.TestCase):
-
     def test_find(self):
         workspace_dir, relpaths = find_all_sources("drake")
 
@@ -17,8 +16,8 @@ class UtilTest(unittest.TestCase):
 
         # Sanity-check relpaths.
         self.assertGreater(len(relpaths), 1_000)
-        self.assertTrue('.bazelproject' in relpaths)
-        self.assertTrue('setup/ubuntu/install_prereqs.sh' in relpaths)
+        self.assertTrue(".bazelproject" in relpaths)
+        self.assertTrue("setup/ubuntu/install_prereqs.sh" in relpaths)
         THIRD_PARTY_SOURCES_ALLOWED_TO_BE_FOUND = [
             "third_party/BUILD.bazel",
             "third_party/README.md",
@@ -29,4 +28,5 @@ class UtilTest(unittest.TestCase):
                 self.assertTrue(
                     one_relpath in THIRD_PARTY_SOURCES_ALLOWED_TO_BE_FOUND
                     or one_relpath.startswith("."),
-                    one_relpath + " has been mis-identified as a source file")
+                    one_relpath + " has been mis-identified as a source file",
+                )
