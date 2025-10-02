7afda4c70e7881981339d76de591c1ec7ff163ce
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-10-02 07:01:20 -0700

    [tools] Add 80-column rule to drakelint
    When replacing pycodestyle with ruff, the only important lint check we
    will lose is the max line length of 80, so we need to add that check
    to drakelint.

--- doc/pydrake/pydrake_sphinx_extension.py
+++ doc/pydrake/pydrake_sphinx_extension.py
@@ -342,7 +342,7 @@ class PydrakeSystemDirective(Directive):
     See also:
     - https://www.sphinx-doc.org/en/1.6.7/extdev/tutorial.html#the-directive-classes
     - https://docutils.sourceforge.io/docs/howto/rst-directives.html#error-handling
-    - Example: https://github.com/sphinx-contrib/autoprogram/blob/0.1.5/sphinxcontrib/autoprogram.py
+    - https://github.com/sphinx-contrib/autoprogram/blob/0.1.5/sphinxcontrib/autoprogram.py
     """  # noqa
 
     has_content = True
