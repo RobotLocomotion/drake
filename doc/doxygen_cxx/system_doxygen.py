7570f118a425775d3cb25af8b0921a5656f86dad
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-10-06 13:15:06 -0700

    [ruff] Format doc (#23535)

--- doc/doxygen_cxx/system_doxygen.py
+++ doc/doxygen_cxx/system_doxygen.py
@@ -38,27 +38,30 @@ def system_yaml_to_html(system_yaml):
     if "input_ports" in y:
         for p in y["input_ports"]:
             input_port_html += (
-                f'<tr><td align=right style=\"padding:5px 0px 5px 0px\">{p}'
-                f'&rarr;</td></tr>')
+                f'<tr><td align=right style="padding:5px 0px 5px 0px">{p}'
+                f"&rarr;</td></tr>"
+            )
     output_port_html = ""
     if "output_ports" in y:
         for p in y["output_ports"]:
             output_port_html += (
-                '<tr><td align=left style=\"padding:5px 0px 5px 0px\">'
-                f'&rarr; {p}</td></tr>')
+                '<tr><td align=left style="padding:5px 0px 5px 0px">'
+                f"&rarr; {p}</td></tr>"
+            )
     # Note: keeping this on a single line avoids having to handle comment line
     # markers (e.g. * or ///)
     html = (
-        f'<table align=center cellpadding=0 cellspacing=0><tr align=center>'
-        f'<td style=\"vertical-align:middle\">'
-        f'<table cellspacing=0 cellpadding=0>{input_port_html}</table>'
-        f'</td>'
-        f'<td align=center style=\"border:solid;padding-left:20px;'
-        f'padding-right:20px;vertical-align:middle\" bgcolor=#F0F0F0>'
-        f'{y["name"]}</td>'
-        f'<td style=\"vertical-align:middle\">'
-        f'<table cellspacing=0 cellpadding=0>{output_port_html}</table>'
-        f'</td></tr></table>')
+        f"<table align=center cellpadding=0 cellspacing=0><tr align=center>"
+        f'<td style="vertical-align:middle">'
+        f"<table cellspacing=0 cellpadding=0>{input_port_html}</table>"
+        f"</td>"
+        f'<td align=center style="border:solid;padding-left:20px;'
+        f'padding-right:20px;vertical-align:middle" bgcolor=#F0F0F0>'
+        f"{y['name']}</td>"
+        f'<td style="vertical-align:middle">'
+        f"<table cellspacing=0 cellpadding=0>{output_port_html}</table>"
+        f"</td></tr></table>"
+    )
 
     return html
 
@@ -68,7 +71,7 @@ def system_yaml_to_pydrake_system_rst_directive(system_yaml):
     Converts a raw YAML description of a system into a corresponding reST
     directive.
     """
-    content = indent(system_yaml.strip(), '    ')
+    content = indent(system_yaml.strip(), "    ")
     return f".. pydrake_system::\n\n{content}"
 
 
@@ -82,9 +85,9 @@ def strip_cpp_comment_cruft(comment):
     tag to reside in a comment block.
     - // comments imply that the remainder of the line should be removed.
     """
-    comment = re.sub(r'///', '', comment)
-    comment = re.sub(r'\s*//.*', '', comment)
-    comment = re.sub(r'^\s*\*', '', comment, flags=re.MULTILINE)
+    comment = re.sub(r"///", "", comment)
+    comment = re.sub(r"\s*//.*", "", comment)
+    comment = re.sub(r"^\s*\*", "", comment, flags=re.MULTILINE)
     return comment
 
 
@@ -101,7 +104,8 @@ def _process_doxygen(s, transform_func):
     if s.find(obsolete_syntax) >= 0:
         raise RuntimeError(
             f'obsolete syntax "{obsolete_syntax}" found; use @system /'
-            f' @endsystem instead. Found in """\n{s}"""')
+            f' @endsystem instead. Found in """\n{s}"""'
+        )
     index = 0
     start_tag = "@system"
     end_tag = "@endsystem"
@@ -109,16 +113,16 @@ def _process_doxygen(s, transform_func):
         start = s.find(start_tag, index)
         end = s.find(end_tag, start + len(start_tag))
         if end < 0:
-            raise RuntimeError(
-                f'missing @endsystem tag in """\n{s[start:]}"""')
-        comment = s[start + len(start_tag):end]
+            raise RuntimeError(f'missing @endsystem tag in """\n{s[start:]}"""')
+        comment = s[start + len(start_tag) : end]
         nested = comment.find(start_tag)
         if nested >= 0:
             raise RuntimeError(
-                f'illegal nested @system tag in """\n{comment}"""')
+                f'illegal nested @system tag in """\n{comment}"""'
+            )
         comment_stripped = strip_cpp_comment_cruft(comment)
         transformed = transform_func(comment_stripped)
-        s = s[:start] + transformed + s[end + len(end_tag):]
+        s = s[:start] + transformed + s[end + len(end_tag) :]
         index = start + len(transformed)
     return s
 
@@ -149,7 +153,7 @@ if __name__ == "__main__":
         raise RuntimeError(f"Wrong number of arguments.\n{__doc__}")
 
     # Load the file in batch into a string.
-    with open(sys.argv[1], 'r', encoding='utf-8') as f:
+    with open(sys.argv[1], "r", encoding="utf-8") as f:
         s = f.read()
 
     try:
