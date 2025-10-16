cdbab4e23e9bebbfae76f93fe75325070afdee44
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-10-16 09:53:41 -0700

    [common] Reformat text_logging.h comments (#23602)
    In preparation for future surgery to this file.

--- common/text_logging.h
+++ common/text_logging.h
@@ -1,7 +1,6 @@
 #pragma once
 
-/**
-@file
+/** @file
 This is the entry point for all text logging within Drake.
 Once you've included this file, the suggested ways you
 should write log messages include:
@@ -95,15 +94,15 @@ namespace drake {
 namespace logging {
 
 // If we have spdlog, just alias logger into our namespace.
-/// The drake::logging::logger class provides text logging methods.
-/// See the text_logging.h documentation for a short tutorial.
+/** The drake::logging::logger class provides text logging methods.
+See the text_logging.h documentation for a short tutorial. */
 using logger = spdlog::logger;
 
-/// When spdlog is enabled in this build, drake::logging::sink is an alias for
-/// spdlog::sinks::sink.  When spdlog is disabled, it is an empty class.
+/** When spdlog is enabled in this build, drake::logging::sink is an alias for
+spdlog::sinks::sink.  When spdlog is disabled, it is an empty class. */
 using spdlog::sinks::sink;
 
-/// True only if spdlog is enabled in this build.
+/** True only if spdlog is enabled in this build. */
 constexpr bool kHaveSpdlog = true;
 
 }  // namespace logging
@@ -151,39 +150,39 @@ class sink {
 
 #endif  // HAVE_SPDLOG
 
-/// Retrieve an instance of a logger to use for logging; for example:
-/// <pre>
-///   drake::log()->info("potato!")
-/// </pre>
-///
-/// See the text_logging.h documentation for a short tutorial.
+/** Retrieve an instance of a logger to use for logging; for example:
+<pre>
+  drake::log()->info("potato!")
+</pre>
+
+See the text_logging.h documentation for a short tutorial. */
 logging::logger* log();
 
 namespace logging {
 
-/// (Advanced) Retrieves the default sink for all Drake logs.  When spdlog is
-/// enabled, the return value can be cast to spdlog::sinks::dist_sink_mt and
-/// thus allows consumers of Drake to redirect Drake's text logs to locations
-/// other than the default of stderr.  When spdlog is disabled, the return
-/// value is an empty class.
+/** (Advanced) Retrieves the default sink for all Drake logs.  When spdlog is
+enabled, the return value can be cast to spdlog::sinks::dist_sink_mt and thus
+allows consumers of Drake to redirect Drake's text logs to locations other than
+the default of stderr.  When spdlog is disabled, the return value is an empty
+class. */
 sink* get_dist_sink();
 
-/// When constructed, logs a message (at "warn" severity); the destructor is
-/// guaranteed to be trivial.  This is useful for declaring an instance of this
-/// class as a function-static global, so that a warning is logged the first
-/// time the program encounters some code, but does not repeat the warning on
-/// subsequent encounters within the same process.
-///
-/// For example:
-/// <pre>
-/// double* SanityCheck(double* data) {
-///   if (!data) {
-///     static const logging::Warn log_once("Bad data!");
-///     return alternative_data();
-///   }
-///   return data;
-/// }
-/// </pre>
+/** When constructed, logs a message (at "warn" severity); the destructor is
+guaranteed to be trivial.  This is useful for declaring an instance of this
+class as a function-static global, so that a warning is logged the first time
+the program encounters some code, but does not repeat the warning on subsequent
+encounters within the same process.
+
+For example:
+<pre>
+double* SanityCheck(double* data) {
+  if (!data) {
+    static const logging::Warn log_once("Bad data!");
+    return alternative_data();
+  }
+  return data;
+}
+</pre> */
 struct [[maybe_unused]] Warn {  // NOLINT(whitespace/braces)
   template <typename... Args>
   Warn(const char* a, const Args&... b) {
@@ -194,27 +193,29 @@ struct [[maybe_unused]] Warn {  // NOLINT(whitespace/braces)
   }
 };
 
-/// Sets the log threshold used by Drake's C++ code.
-/// @param level Must be a string from spdlog enumerations: `trace`, `debug`,
-/// `info`, `warn`, `err`, `critical`, `off`, or `unchanged` (not an enum, but
-/// useful for command-line).
-/// @return The string value of the previous log level. If SPDLOG is disabled,
-/// then this returns an empty string.
+/** Sets the log threshold used by Drake's C++ code.
+@param level Must be a string from spdlog enumerations: `trace`, `debug`,
+`info`, `warn`, `err`, `critical`, `off`, or `unchanged` (not an enum, but
+useful for command-line).
+@return The string value of the previous log level. If SPDLOG is disabled, then
+this returns an empty string. */
 std::string set_log_level(const std::string& level);
 
-/// The "unchanged" string to pass to set_log_level() so as to achieve a no-op.
+/** The "unchanged" string to pass to set_log_level() so as to achieve a no-op.
+ */
 extern const char* const kSetLogLevelUnchanged;
 
-/// An end-user help string suitable to describe the effects of set_log_level().
+/** An end-user help string suitable to describe the effects of set_log_level().
+ */
 extern const char* const kSetLogLevelHelpMessage;
 
-/// Invokes `drake::log()->set_pattern(pattern)`.
-/// @param pattern Formatting for message. For more information, see:
-/// https://github.com/gabime/spdlog/wiki/3.-Custom-formatting
+/** Invokes `drake::log()->set_pattern(pattern)`.
+@param pattern Formatting for message. For more information, see:
+https://github.com/gabime/spdlog/wiki/3.-Custom-formatting */
 void set_log_pattern(const std::string& pattern);
 
-/// An end-user help string suitable to describe the effects of
-/// set_log_pattern().
+/** An end-user help string suitable to describe the effects of
+set_log_pattern(). */
 extern const char* const kSetLogPatternHelpMessage;
 
 }  // namespace logging
