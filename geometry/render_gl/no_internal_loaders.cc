caf99ec14a6e8b070aeacef7c4f3e3928bc83a1c
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-10-29 10:05:58 -0700

    add internal_no_loaders_test

--- geometry/render_gl/no_internal_loaders.cc
+++ geometry/render_gl/no_internal_loaders.cc
@@ -8,12 +8,14 @@ namespace render_gl {
 namespace internal {
 
 void GladLoaderLoadEgl() {
-  // This is not reachable via Drake's public API.
+  // This is not reachable via Drake's public API. However, we use an exception
+  // instead of DRAKE_UNREACHABLE in support of our unit test.
   throw std::logic_error("Drake's GladLoaderLoadEgl() is unavailable.");
 }
 
 void* GladLoaderLoadGlx() {
-  // This is not reachable via Drake's public API.
+  // This is not reachable via Drake's public API. However, we use an exception
+  // instead of DRAKE_UNREACHABLE in support of our unit test.
   throw std::logic_error("Drake's GladLoaderLoadGlx() is unavailable.");
 }
 
