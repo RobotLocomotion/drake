eb93f7a9446549f33db7f7fb73cd94dfa36e4c84
Tyler Yankee <tyler.yankee@kitware.com>
2025-10-30 11:14:30 -0400

    properly removing the fluff
    first comment stays, second comment actually makes the change

--- systems/framework/cache_doxygen.h
+++ systems/framework/cache_doxygen.h
@@ -3,18 +3,12 @@
 
 #pragma once
 
+// Putting this document in drake::systems namespace makes the links work.
 namespace drake {
 namespace systems {
 
 /** @defgroup cache_design_notes System Cache Design and Implementation Notes
     @ingroup technical_notes
-
-@parblock                       <center>
-          \"There are only two hard things in computer science:<br>
-          <b>cache invalidation</b>, and naming things.\"<br>
-                            -- Phil Karlton
-                                </center>
-@endparblock
 
 <h2>Background</h2>
 Drake System objects are used to specify the computational _structure_ of a
