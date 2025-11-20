5aee0349d5722fb9bac6e1ff81100166e3b5ed52
Vince Kurtz <vjkurtz@gmail.com>
2025-11-20 15:16:50 -0600

    add doxygen-style comments to IcfSolverParameters

--- multibody/contact_solvers/icf/icf_solver_parameters.h
+++ multibody/contact_solvers/icf/icf_solver_parameters.h
@@ -7,10 +7,12 @@ namespace multibody {
 namespace contact_solvers {
 namespace icf {
 
-/* Parameters to configure the ICF convex solver. */
+/** Parameters to configure the ICF convex solver. */
 struct IcfSolverParameters {
-  /* Passes this object to an Archive.
-  Refer to @ref yaml_serialization "YAML Serialization" for background. */
+  /**
+   * Passes this object to an Archive.
+   * Refer to @ref yaml_serialization "YAML Serialization" for background.
+   */
   template <typename Archive>
   void Serialize(Archive* a) {
     a->Visit(DRAKE_NVP(max_iterations));
@@ -24,38 +26,50 @@ struct IcfSolverParameters {
     a->Visit(DRAKE_NVP(print_solver_stats));
   }
 
-  /* Outer solver iteration limit */
+  /** Outer solver iteration limit */
   int max_iterations{100};
 
-  /* Minimum tolerance ε for the convergence conditions
-       ‖D ∇ℓ‖ ≤ ε max(1, ‖D r‖),
-       η ‖D⁻¹ Δv‖ ≤ ε max(1, ‖D r‖).
-  This provides a lower bound on the actual tolerance used, which is specified
-  explicitly when calling the solver. CENIC uses this minimum tolerance in fixed
-  step mode, and relaxes it in error-controlled mode based on the desired
-  accuracy. */
+  /**
+   * Minimum tolerance ε for the convergence conditions
+   *
+   *    ‖D ∇ℓ‖ ≤ ε max(1, ‖D r‖),
+   *    η ‖D⁻¹ Δv‖ ≤ ε max(1, ‖D r‖).
+   *
+   * This provides a lower bound on the actual tolerance used, which is
+   * specified explicitly when calling the solver. CENIC uses this minimum
+   * tolerance in fixed step mode, and relaxes it in error-controlled mode based
+   * on the desired accuracy.
+   */
   double min_tolerance{1e-8};
 
-  /* Whether hessian reuse between iterations and time steps is enabled. */
+  /** Whether hessian reuse between iterations and time steps is enabled. */
   bool enable_hessian_reuse{false};
 
-  /* Target maximum number of iterations for Hessian reuse. The solver
-  effectively estimates the number of iterations to convergence. If the
-  estimate is larger than this target, the Hessian will be recomputed to
-  regain faster Newton-style convergence. See [Hairer, 1996], Ch. IV.8. */
+  /**
+   * Target maximum number of iterations for Hessian reuse. The solver
+   * effectively estimates the number of iterations to convergence. If the
+   * estimate is larger than this target, the Hessian will be recomputed to
+   * regain faster Newton-style convergence. See [Hairer, 1996], Ch. IV.8.
+   */
   int hessian_reuse_target_iterations{10};
 
-  /* Dense algebra (LDLT) for solving for the search direction H⁻¹ g.
-  This is primarily useful for debugging and testing: sparse algebra is
-  generally much faster. */
+  /**
+   * Dense algebra (LDLT) for solving for the search direction H⁻¹ g.
+   * This is primarily useful for debugging and testing: sparse algebra is
+   * generally much faster.
+   */
   bool use_dense_algebra{false};
 
-  /* Parameters for exact linesearch */
+  /** Maximum iterations for exact linesearch. */
   int max_ls_iterations{100};
-  double ls_tolerance{1e-8};
-  double alpha_max{1.0};  // maximum step length
 
-  /* Whether to print stats to console. */
+  /** Convergence tolerance for exact linesearch. */
+  double ls_tolerance{1e-8};
+
+  /** Maximum step length for exact linesearch. */
+  double alpha_max{1.0};
+
+  /** Whether to print stats to the console. */
   bool print_solver_stats{false};
 };
 
