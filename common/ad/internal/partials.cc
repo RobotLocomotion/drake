0ec4246a908678810b39d9a05a874c3c9123fab6
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-12-11 14:11:32 -0800

    [ad] Add a separate scaling factor to Partials
    This is a performance improvement.

--- common/ad/internal/partials.cc
+++ common/ad/internal/partials.cc
@@ -29,51 +29,73 @@ int IndexToInt(Eigen::Index index) {
 }  // namespace
 
 Partials::Partials(Eigen::Index size, Eigen::Index offset, double coeff)
-    : derivatives_{VectorXd::Zero(IndexToInt(size))} {
+    : coeff_{1.0}, storage_{VectorXd::Zero(IndexToInt(size))} {
   if (IndexToInt(offset) >= size) {
     throw std::out_of_range(fmt::format(
         "AutoDiff offset {} must be strictly less than size {}", offset, size));
   }
-  derivatives_[offset] = coeff;
+  storage_[offset] = coeff;
 }
 
 Partials::Partials(const Eigen::Ref<const VectorXd>& value)
-    : derivatives_{value} {
+    : coeff_{1.0}, storage_{value} {
   // Called for its side-effect of throwing on too-large sizes. (Unfortunately,
   // it's unrealistic to unit test this because it requires a >16 GiB value.)
   IndexToInt(value.size());
 }
 
 void Partials::MatchSizeOf(const Partials& other) {
+  // Partials with size() == 0 may be freely combined with any other size.
   if (other.size() == 0) {
     return;
   }
   if (size() == 0) {
-    derivatives_ = VectorXd::Zero(other.size());
+    storage_ = VectorXd::Zero(other.size());
+    coeff_ = 1.0;
     return;
   }
   ThrowIfDifferentSize(other);
 }
 
+void Partials::SetZero() {
+  storage_.setZero();
+  coeff_ = 1.0;
+}
+
 void Partials::Mul(double factor) {
   if (std::isfinite(factor)) [[likely]] {
-    derivatives_ *= factor;
+    // The `coeff_` will remain finite; multiplying two finite numbers always
+    // produces a finite result.
+    coeff_ *= factor;
   } else {
+    // Do a "sparse" multiply of only the non-zero values in storage. If the
+    // value is zero or NaN, then we'll leave it alone.
+    const double total_factor = coeff_ * factor;  // This will be non-finite.
     for (int i = 0; i < size(); ++i) {
-      const double x = derivatives_[i];
-      derivatives_[i] = (x == 0) ? 0 : x * factor;
+      const double x = storage_[i];
+      if ((x < 0) || (x > 0)) {
+        storage_[i] = x * total_factor;
+      }
     }
+    coeff_ = 1.0;
   }
 }
 
 void Partials::Div(double factor) {
-  if (!std::isnan(factor) && factor != 0) [[likely]] {
-    derivatives_ /= factor;
+  if (!std::isnan(factor) && factor != 0.0) [[likely]] {
+    // The `coeff_` will remain finite; dividing finite by ±∞ produces ±0.
+    coeff_ /= factor;
   } else {
+    // Do a "sparse" division of only the non-zero values in storage. If the
+    // value is zero or NaN, then we'll leave it alone.
+    const double total_factor = coeff_ / factor;
     for (int i = 0; i < size(); ++i) {
-      const double x = derivatives_[i];
-      derivatives_[i] = (x == 0) ? 0 : x / factor;
+      const double x = storage_[i];
+      if ((x < 0) || (x > 0)) {
+        storage_[i] = x * total_factor;
+      }
     }
+    coeff_ = 1.0;
   }
 }
 
@@ -82,30 +104,45 @@ void Partials::Add(const Partials& other) {
 }
 
 void Partials::AddScaled(double scale, const Partials& other) {
+  // Handle the case of this and/or other being empty (treated as all-zero).
+  // Partials with size() == 0 may be freely combined with any other size.
   if (other.size() == 0) {
     return;
   }
   if (size() == 0) {
-    if (std::isfinite(scale)) [[likely]] {
-      derivatives_ = other.derivatives_ * scale;
-    } else {
-      derivatives_ = VectorXd(other.size());
-      for (int i = 0; i < other.size(); ++i) {
-        const double x = other.derivatives_[i];
-        derivatives_[i] = (x == 0) ? 0 : x * scale;
-      }
-    }
+    *this = other;
+    Mul(scale);
     return;
   }
   ThrowIfDifferentSize(other);
+
+  // Common case: perform the linear combination.
   if (std::isfinite(scale)) [[likely]] {
-    derivatives_ += other.derivatives_ * scale;
-  } else {
-    for (int i = 0; i < size(); ++i) {
-      const double x = other.derivatives_[i];
-      derivatives_[i] += (x == 0) ? 0 : x * scale;
-    }
+    storage_.noalias() =
+        coeff_ * storage_ + (scale * other.coeff_) * other.storage_;
+    coeff_ = 1.0;
+    return;
   }
+
+  // Unlikely: non-finite scale; zero partials in `other` must not be scaled.
+  for (int i = 0; i < size(); ++i) {
+    const double this_datum = coeff_ * storage_[i];
+    const double other_datum = other.coeff_ * other.storage_[i];
+    storage_[i] = this_datum + ((other_datum == 0) ? 0 : other_datum * scale);
+  }
+  coeff_ = 1.0;
+}
+
+DerivativesConstXpr Partials::make_const_xpr() const {
+  return DerivativesConstXpr{coeff_, storage_.data(), size(), /* stride = */ 1};
+}
+
+VectorXd& Partials::GetRawStorageMutable() {
+  if (coeff_ != 1.0) {
+    storage_ *= coeff_;
+    coeff_ = 1.0;
+  }
+  return storage_;
 }
 
 void Partials::ThrowIfDifferentSize(const Partials& other) {
