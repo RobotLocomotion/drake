b34002f62512aeed4f345574a12d2e720bd28632
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-12-18 12:05:38 -0800

    [ad] Store partials in custom vector
    This is a non-functional refactoring to prepare for future changes
    that optimize storage layout, e.g., sparsity, COW, batching, etc.
    None of those optimizations are possible with VectorXd storage.
    
    In particular, note that the storage size is now represented by an int
    instead of an Eigen::Index. This gives us 4 more unused bytes in the
    struct that we'll be able to repurpose for new fields in the future
    (e.g., sparsity).

--- common/ad/test/partials_heap_test.cc
+++ common/ad/test/partials_heap_test.cc
@@ -14,31 +14,40 @@ using test::LimitMalloc;
 // An empty fixture for later expansion.
 class PartialsHeapTest : public ::testing::Test {};
 
-// Neither the constructor nor GetRawStorageMutable allocate when size() == 0.
-TEST_F(PartialsHeapTest, GetRawStorageMutableEmpty) {
+// Neither the constructor nor MakeMutableXpr allocate when size() == 0.
+TEST_F(PartialsHeapTest, MakeMutableXprEmpty) {
   LimitMalloc guard;
   Partials empty;
-  empty.GetRawStorageMutable();
-  empty.GetRawStorageMutable();
+  empty.MakeMutableXpr();
+  empty.MakeMutableXpr();
 }
 
-// Between the full-degree `value` constructor and GetRawStorageMutable, at
+// Between the full-degree `value` constructor and MakeMutableXpr, at
 // most one allocation (for the returned storage) is allowed.
-TEST_F(PartialsHeapTest, GetRawStorageMutableGeneric) {
+TEST_F(PartialsHeapTest, MakeMutableXprGeneric) {
   const Eigen::Vector3d value{1.0, 2.0, 3.0};
   LimitMalloc guard({1});
   Partials full(value);
-  full.GetRawStorageMutable();
-  full.GetRawStorageMutable();
+  full.MakeMutableXpr();
+  full.MakeMutableXpr();
 }
 
-// Between the unit-vector constructor and GetRawStorageMutable, at
+// Between the unit-vector constructor and MakeMutableXpr, at
 // most one allocation (for the returned storage) is allowed.
-TEST_F(PartialsHeapTest, GetRawStorageMutableUnit) {
+TEST_F(PartialsHeapTest, MakeMutableXprUnit) {
   LimitMalloc guard({1});
   Partials unit(10, 0);
-  unit.GetRawStorageMutable();
-  unit.GetRawStorageMutable();
+  unit.MakeMutableXpr();
+  unit.MakeMutableXpr();
+}
+
+// Copy-assignment of equal sizes doesn't allocate.
+// most one allocation (for the returned storage) is allowed.
+TEST_F(PartialsHeapTest, CopyAssign) {
+  Partials foo(Eigen::Vector3d{1.0, 2.0, 3.0});
+  const Partials bar(Eigen::Vector3d{4.0, 5.0, 6.0});
+  LimitMalloc guard;
+  foo = bar;
 }
 
 }  // namespace
