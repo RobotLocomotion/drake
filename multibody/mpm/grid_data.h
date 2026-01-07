2014bedf1037e1b52cfd62e6a1439a9cc4b2af83
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-01-07 11:15:31 -0800

    [common] Remove bit_cast.h (#23978)

--- multibody/mpm/grid_data.h
+++ multibody/mpm/grid_data.h
@@ -3,7 +3,6 @@
 #include <cstdint>
 
 #include "drake/common/autodiff.h"
-#include "drake/common/bit_cast.h"
 #include "drake/common/drake_assert.h"
 #include "drake/common/drake_copyable.h"
 #include "drake/common/eigen_types.h"
