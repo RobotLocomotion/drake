43d045b16b7a5165c496f5abdc1b62485621bfa9
Paul Mitiguy <paul.mitiguy@tri.global>
2026-02-02 19:49:15 -0800

    Respond to reviewer comments.

--- multibody/plant/multibody_plant.h
+++ multibody/plant/multibody_plant.h
@@ -3831,12 +3831,12 @@ class MultibodyPlant final : public internal::MultibodyTreeSystem<T> {
   ///   The frame B in which the positions `p_BQi` of a set of points `Qi` are
   ///   given.
   /// @param[in] p_BQi
-  ///   The input positions of each point `Qi` in frame B. `p_BQi ∈ ℝ³ˣⁿᵖ` with
-  ///   `np` the number of points in the set. Each column of `p_BQi` corresponds
+  ///   The input positions of each point `Qi` in frame B. `p_BQi ∈ ℝ³ˣⁿ` with
+  ///   `n` the number of points in the set. Each column of `p_BQi` corresponds
   ///   to a vector in ℝ³ holding the position of one of the points in the set
   ///   as measured and expressed in frame B. Each column of p_BQi is a position
   ///   vector associated with one point Qi, and the number of columns in p_BQi
-  ///   is the number np of points.
+  ///   is the number n of points.
   /// @param[in] frame_A
   ///   The frame A in which it is desired to compute the positions `p_AQi` of
   ///   each point `Qi` in the set.
@@ -3844,14 +3844,13 @@ class MultibodyPlant final : public internal::MultibodyTreeSystem<T> {
   ///   The output positions of each point `Qi` now computed as measured and
   ///   expressed in frame A. The output `p_AQi` **must** have the same size as
   ///   the input `p_BQi` or otherwise this method aborts. That is `p_AQi`
-  ///   **must** be in `ℝ³ˣⁿᵖ`. Each column of p_AQi is a position vector
+  ///   **must** be in `ℝ³ˣⁿ`. Each column of p_AQi is a position vector
   ///   associated with one point Qi, and the number of columns in p_BQi is the
-  ///   number np of points.
+  ///   number n of points.
   ///
   /// @note Both `p_BQi` and `p_AQi` must have three rows. Otherwise this
-  /// method will throw a std::exception. This method also throws
-  /// a std::exception if `p_BQi` and `p_AQi` differ in the number
-  /// of columns.
+  /// method will throw a std::exception. This method also throws a
+  /// std::exception if `p_BQi` and `p_AQi` differ in the number of columns.
   void CalcPointsPositions(const systems::Context<T>& context,
                            const Frame<T>& frame_B,
                            const Eigen::Ref<const MatrixX<T>>& p_BQi,
@@ -3886,13 +3885,13 @@ class MultibodyPlant final : public internal::MultibodyTreeSystem<T> {
   /// the generalized positions q and the generalized velocities v.
   /// @param[in] frame_B The frame B in which each point Qi is fixed and whose
   /// frame origin Bo is the starting point for position vectors in p_BQi.
-  /// frame_B is also the expressed-in-frame for position vectors P_BQi.
+  /// frame_B is also the expressed-in-frame for position vectors p_BQi.
   /// @param[in] p_BQi Position vectors from Bo (frame B's origin) to each
   /// point Qi (i = 0, ... n-1), expressed in frame B. Each column of p_BQi
   /// is a position vector associated with one point Qi, and the number of
   /// columns in p_BQi is the number n of points.
   /// @param[in] frame_A The frame in which the velocities are to be measured.
-  /// @param[in] frame_E The frame in which the velocities are expressed.
+  /// @param[in] frame_E The frame in which the velocities are to be expressed.
   /// @param[out] v_AQi_E The velocities of each point Qi (i = 0, ... n-1)
   /// measured in frame A and expressed in frame E. Each column of v_AQi_E is a
   /// translational velocity vector associated with one point Qi, and the
