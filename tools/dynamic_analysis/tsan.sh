1764335ccd84f1e87d5d6e0b3a91951ce86264fc
Tyler Yankee <tyler.yankee@kitware.com>
2026-02-20 09:50:19 -0500

    [tools] Disallow sanitizer options from calling environment

--- tools/dynamic_analysis/tsan.sh
+++ tools/dynamic_analysis/tsan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 me=$(python3 -c 'import os; print(os.path.realpath("'"$0"'"))')
 mydir=$(dirname "$me")
-export TSAN_OPTIONS="$TSAN_OPTIONS:detect_deadlocks=1:second_deadlock_stack=1:strip_path_prefix=/proc/self/cwd/:suppressions=$mydir/tsan.supp"
+export TSAN_OPTIONS="detect_deadlocks=1:second_deadlock_stack=1:strip_path_prefix=/proc/self/cwd/:suppressions=$mydir/tsan.supp"
 # Ensure executable named llvm-symbolizer is on the PATH.
 export PATH="$PATH:/usr/lib/llvm-19/bin:/usr/lib/llvm-15/bin:/usr/local/opt/llvm/bin"
 # Run with ASLR disabled.
