e1bc33076e745c80366333938e9dcef45b1d93ff
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-03-05 06:33:43 -0800

    [multibody] Change DesiredStateInput to better match call site needs

--- multibody/plant/desired_state_input.cc
+++ multibody/plant/desired_state_input.cc
@@ -9,36 +9,14 @@ namespace multibody {
 namespace internal {
 
 template <typename T>
-DesiredStateInput<T>::DesiredStateInput(int num_model_instances) {
-  DRAKE_DEMAND(num_model_instances > 0);
-  positions_.resize(num_model_instances);
-  velocities_.resize(num_model_instances);
-}
+DesiredStateInput<T>::DesiredStateInput() = default;
 
 template <typename T>
 DesiredStateInput<T>::~DesiredStateInput() = default;
-
-template <typename T>
-void DesiredStateInput<T>::SetModelInstanceDesiredStates(
-    ModelInstanceIndex model_instance, const Eigen::Ref<const VectorX<T>>& qd,
-    const Eigen::Ref<const VectorX<T>>& vd) {
-  DRAKE_DEMAND(model_instance < num_model_instances());
-  DRAKE_DEMAND(qd.size() == vd.size());
-  positions_[model_instance] = qd;
-  velocities_[model_instance] = vd;
-}
-
-template <typename T>
-void DesiredStateInput<T>::ClearModelInstanceDesiredStates(
-    ModelInstanceIndex model_instance) {
-  DRAKE_DEMAND(model_instance < num_model_instances());
-  positions_[model_instance] = std::nullopt;
-  velocities_[model_instance] = std::nullopt;
-}
 
 }  // namespace internal
 }  // namespace multibody
 }  // namespace drake
 
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
-    class ::drake::multibody::internal::DesiredStateInput);
+    struct ::drake::multibody::internal::DesiredStateInput);
