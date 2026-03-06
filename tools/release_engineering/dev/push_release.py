e90692494f49ac9eddf84b04c429f8e07e26ddc3
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-03-06 06:19:33 -0800

    [tools] Release manager now uploads the source archive
    The "download_release_candidate" now also creates the source archive,
    in addition to downloading the staging binaries.
    
    Remove the corresponding logic from push_release.py.
    
    Update the playbook and issue template to match.

--- tools/release_engineering/dev/push_release.py
+++ tools/release_engineering/dev/push_release.py
@@ -6,7 +6,6 @@ This program is only supported on Ubuntu Noble 24.04.
 """
 
 import argparse
-import hashlib
 import os
 import re
 import shutil
@@ -15,7 +14,6 @@ import sys
 import tempfile
 import textwrap
 from typing import Any, Dict, List, Optional
-import urllib.request
 
 import boto3
 import github3
@@ -25,11 +23,6 @@ from github3.repos.tag import RepoTag
 
 _GITHUB_REPO_OWNER = "RobotLocomotion"
 _GITHUB_REPO_NAME = "drake"
-_GITHUB_REPO_URI = (
-    f"https://github.com/{_GITHUB_REPO_OWNER}/{_GITHUB_REPO_NAME}"
-)
-
-_ARCHIVE_HASHES = {"sha256", "sha512"}
 
 _AWS_BUCKET = "drake-packages"
 _AWS_PREFIX = "drake/release"
@@ -92,46 +85,6 @@ class _State:
                 self._release.upload_asset(content_type, name, f)
             self._done()
 
-    def _compute_hash(self, path: str, algorithm: str) -> str:
-        """
-        Compute and return the specified hash for the specified file.
-        """
-        with open(path, "rb") as f:
-            return hashlib.file_digest(f, algorithm).hexdigest()
-
-    def _write_hashfile(
-        self, name: str, algorithm: str, local_path: str, hashfile_path: str
-    ) -> str:
-        """
-        Writes the hash of the specified file using the specified algorithm
-        to the specified path. Returns the computed hash.
-        """
-        self._begin(f"computing {algorithm} for", name)
-
-        digest = self._compute_hash(local_path, algorithm)
-        with open(hashfile_path, "wt") as f:
-            f.write(f"{digest} {name}\n")
-
-        self._done()
-        return digest
-
-    def _download_archive_github(self, name: str) -> str:
-        """
-        Downloads the source archive from the GitHub release. Returns the full
-        path to the downloaded file.
-        """
-        self._begin("downloading", name)
-
-        local_path = os.path.join(self._scratch.name, name)
-        archive_url = (
-            f"{_GITHUB_REPO_URI}/archive/refs/tags/"
-            f"v{self._options.source_version}.tar.gz"
-        )
-        assert urllib.request.urlretrieve(archive_url, local_path)
-
-        self._done()
-        return local_path
-
     def get_artifacts(self) -> List[Asset]:
         """
         Get artifacts associated with a GitHub release.
@@ -164,28 +117,6 @@ class _State:
             self._begin("pushing", artifact.name, dest)
             self._s3.upload_file(local_path, bucket, path)
             self._done()
-
-    def push_archive(self, name: str) -> None:
-        """
-        Pushes the release source archive to GitHub, along with computed
-        hashes.
-
-        If --dry-run was given, rather than actually pushing files, prints what
-        would be done.
-        """
-        local_path = self._download_archive_github(name)
-        self._upload_file_github(name, local_path)
-
-        # Calculate hashes and write hash files
-        for algorithm in _ARCHIVE_HASHES:
-            hashfile_name = f"{name}.{algorithm}"
-            hashfile_path = os.path.join(self._scratch.name, hashfile_name)
-
-            digest = self._write_hashfile(
-                name, algorithm, local_path, hashfile_path
-            )
-            print(f"{name!r} {algorithm}: {digest}")
-            self._upload_file_github(hashfile_name, hashfile_path)
 
 
 def _fatal(msg: str, result: int = 1) -> None:
@@ -246,15 +177,6 @@ def _find_tag(repo: Repository, tag: str) -> Optional[RepoTag]:
     return None
 
 
-def _push_source(state: _State) -> None:
-    """
-    Downloads the source .tar artifact and pushes it to GitHub.
-    """
-    version = state.source_version
-    dest_name = f"drake-{version}-src.tar.gz"
-    state.push_archive(dest_name)
-
-
 def _push_s3(state: _State) -> None:
     """
     Downloads GitHub release artifacts and pushes them to S3.
@@ -266,13 +188,6 @@ def _push_s3(state: _State) -> None:
 
 def main(args: List[str]) -> None:
     parser = argparse.ArgumentParser(prog="push_release", description=__doc__)
-    parser.add_argument(
-        "--source",
-        dest="push_source",
-        default=True,
-        action=argparse.BooleanOptionalAction,
-        help="Mirror source .tar archive to GitHub.",
-    )
     parser.add_argument(
         "--s3",
         dest="push_s3",
@@ -342,8 +257,6 @@ def main(args: List[str]) -> None:
     state = _State(options, release)
 
     # Push the requested release artifacts.
-    if options.push_source:
-        _push_source(state)
     if options.push_s3:
         _push_s3(state)
 
