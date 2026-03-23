5c46b0fcd7938e32030213a5ea4fd28be5ea33bc
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-03-23 06:46:09 -0700

    [wheel] Officially support Amazon Linux 2023
    Add wheel build regression tests.
    
    Add to support matrix documentation.
    
    Remove nearly-irrelevant JDK support details.

--- tools/wheel/test/provision.sh
+++ tools/wheel/test/provision.sh
@@ -6,14 +6,24 @@ set -eu -o pipefail
 
 readonly PYTHON=python${1:-3}
 
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get -y update
-
-apt-get -y install --no-install-recommends \
-    lib${PYTHON}-dev ${PYTHON}-venv \
-    python3-venv \
-    libx11-6 libsm6 libxt6 libglib2.0-0
+. /etc/os-release
+case "$ID" in
+  ubuntu)
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get -y update
+    apt-get -y install --no-install-recommends \
+        lib${PYTHON}-dev ${PYTHON}-venv \
+        python3-venv \
+        libx11-6 libsm6 libxt6 libglib2.0-0
+    ;;
+  amzn)
+    dnf install -y ${PYTHON}
+    ;;
+  *)
+    echo "Unknown distro '$ID'" >&2
+    exit 1
+    ;;
+esac
 
 ${PYTHON} -m venv /tmp/drake-wheel-test/python
 
