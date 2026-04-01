1ddd8bd25209a14d1b46c964066870da5a6eab28
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-04-01 14:39:41 -0700

    [common] Variable::Id is an object not a size_t
    It was already abstracted away as a typedef, but that left the door
    open to accidentally abusing its integer nature. We replace it with a
    real class now to prevent future mis-use, and to allow for changing
    its representation down the road.
    
    For the Python bindings, the original typedef was never bound, so for
    backwards compatibility we'll define a type_caster back to int rather
    than binding the Id class.

--- bindings/generated_docstrings/common_symbolic_expression.h
+++ bindings/generated_docstrings/common_symbolic_expression.h
@@ -1478,7 +1478,29 @@ Note:
         // Symbol: drake::symbolic::Variable::Id
         struct /* Id */ {
           // Source: drake/common/symbolic/expression/variable.h
-          const char* doc = R"""()""";
+          const char* doc = R"""(Identifier for a symbolic variable.)""";
+          // Symbol: drake::symbolic::Variable::Id::Id
+          struct /* ctor */ {
+            // Source: drake/common/symbolic/expression/variable.h
+            const char* doc =
+R"""(Constructs a "dummy" id, not associated with any variable.)""";
+          } ctor;
+          // Symbol: drake::symbolic::Variable::Id::operator<=>
+          struct /* operator_le_ */ {
+            // Source: drake/common/symbolic/expression/variable.h
+            const char* doc = R"""(Default comparison operators.)""";
+          } operator_le_;
+          // Symbol: drake::symbolic::Variable::Id::to_string
+          struct /* to_string */ {
+            // Source: drake/common/symbolic/expression/variable.h
+            const char* doc =
+R"""(Returns a string representation of this Id.)""";
+          } to_string;
+          // Symbol: drake::symbolic::Variable::Id::value
+          struct /* value */ {
+            // Source: drake/common/symbolic/expression/variable.h
+            const char* doc = R"""()""";
+          } value;
         } Id;
         // Symbol: drake::symbolic::Variable::Type
         struct /* Type */ {
