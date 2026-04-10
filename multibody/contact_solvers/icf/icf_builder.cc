3174069f38c2bf09df5ba8a1da327c0955688715
Rick Poyner <rick.poyner@tri.global>
2026-04-10 16:59:29 -0400

    wip: icf allows joint locking
    ... but doesn't implement it.

--- multibody/contact_solvers/icf/icf_builder.cc
+++ multibody/contact_solvers/icf/icf_builder.cc
@@ -296,17 +296,8 @@ void IcfBuilder<T>::ValidatePlant() {
 }
 
 template <typename T>
-void IcfBuilder<T>::ValidateContext(const systems::Context<T>& context) {
-  // Revisit this condition when joint locking support is implemented. See
-  // #23764.
-  for (const JointIndex& j : plant_.GetJointIndices()) {
-    if (plant_.get_joint(j).is_locked(context)) {
-      throw std::runtime_error(
-          fmt::format("The CENIC integrator does not yet support joint "
-                      "locking, but at least joint {} is locked",
-                      j));
-    }
-  }
+void IcfBuilder<T>::ValidateContext(const systems::Context<T>&) {
+  // Nothing to complain about.
 }
 
 template <typename T>
