RobotLocomotion/drake

commit 69e064f8cf851697eb821efafa6c7ee30fb6807a

[tools] Use opt_out_condition for excluding dynamic_analysis configurations (#24413)
The old e.g. `tags = ["no_asan"]` is now spelled as
`opt_out_conditions = ["//tools/asan:enabled"]`.

The old `disable_in_compilation_mode_dbg = True` is now spelled as
`opt_out_conditions = ["//tools:unoptimized"]`.

tools/skylark/drake_py.bzl
@@ -1,10 +1,12 @@
 load(
     "//tools/skylark:kwargs.bzl",
     "amend",
+    "combine_conditions",
     "incorporate_allow_network",
     "incorporate_display",
     "incorporate_num_threads",
     "incorporate_rendering",
+    "incorporate_test_weight_heuristics",
 )
 load("//tools/skylark:py.bzl", "py_binary", "py_library", "py_test")
 
@@ -164,8 +166,12 @@ def drake_py_binary(
             size = test_rule_size,
             timeout = test_rule_timeout,
             flaky = test_rule_flaky,
+            opt_out_conditions = [
+                # Smoke tests don't count as coverage.
+                "//tools/kcov:enabled",
+            ],
             rendering = test_rule_rendering,
-            tags = (test_rule_tags or []) + ["nolint", "no_kcov"],
+            tags = (test_rule_tags or []) + ["nolint"],
             # The added test rule isn't going to `import unittest`, but test
             # dependencies such as numpy(!!) do so unconditionally.  We should
             # allow that.
@@ -194,12 +200,6 @@ def drake_py_unittest(
         fail("Changing srcs= is not allowed by drake_py_unittest." +
              " Use drake_py_test instead, if you need something weird.")
     srcs = ["test/%s.py" % name, helper]
-
-    # kcov is only appropriate for small-sized unit tests. If a test needs a
-    # shard_count or a special timeout, we assume it is not small.
-    if "shard_count" in kwargs or "timeout" in kwargs:
-        amend(kwargs, "tags", append = ["no_kcov"])
-
     drake_py_test(
         name = name,
         srcs = srcs,
@@ -223,6 +223,7 @@ def drake_py_test(
         display = False,
         num_threads = None,
         opt_in_condition = None,
+        opt_out_conditions = None,
         rendering = False,
         **kwargs):
     """A wrapper to insert Drake-specific customizations.
@@ -252,6 +253,9 @@ def drake_py_test(
     @param opt_in_condition (optional, default is None)
         See drake/tools/skylark/README.md for details.
 
+    @param opt_out_conditions (optional, default is None)
+        See drake/tools/skylark/README.md for details.
+
     @param rendering (optional, default is False)
         See drake/tools/skylark/README.md for details.
 
@@ -272,15 +276,18 @@ def drake_py_test(
     kwargs = incorporate_display(kwargs, display = display)
     kwargs = incorporate_num_threads(kwargs, num_threads = num_threads)
     kwargs = incorporate_rendering(kwargs, rendering = rendering)
+    kwargs = incorporate_test_weight_heuristics(kwargs)
     kwargs = amend(kwargs, "tags", append = ["py"])
+    opt_out_conditions = (opt_out_conditions or []) + kwargs.pop("opt_out_conditions", [])
 
     deps = deps or []
     if not allow_import_unittest:
         deps = deps + ["//common/test_utilities:disable_python_unittest"]
-    target_compatible_with = select({
-        opt_in_condition: [],
-        "//conditions:default": ["@platforms//:incompatible"],
-    }) if opt_in_condition != None else None
+    target_compatible_with, _ = combine_conditions(
+        name = name,
+        opt_in_condition = opt_in_condition,
+        opt_out_conditions = opt_out_conditions,
+    )
     _py_target_isolated(
         name = name,
         py_target = py_test,

tools/skylark/kwargs.bzl
@@ -61,14 +61,16 @@ def incorporate_rendering(kwargs, *, rendering):
     if rendering not in (True, False):
         fail("The 'rendering = ...' attribute should be a boolean.")
     if rendering:
-        kwargs = amend(kwargs, "tags", append = [
+        kwargs = amend(kwargs, "opt_out_conditions", append = [
             # Disable under LeakSanitizer and Valgrind Memcheck due to
             # driver-related leaks. For more information, see #7520.
-            "no_lsan",
-            "no_memcheck",
+            "//tools/lsan:enabled",
+            "//tools/valgrind:enabled",
             # Similar to #7520, the GL vendor's libraries are not sufficiently
             # instrumented for compatibility with TSan.
-            "no_tsan",
+            "//tools/tsan:enabled",
+        ])
+        kwargs = amend(kwargs, "tags", append = [
             # Mitigates driver-related issues when running under `bazel test`.
             # For more information, see #7004.
             "no-sandbox",
@@ -117,3 +119,106 @@ def incorporate_allow_network(kwargs, *, allow_network):
         "DRAKE_ALLOW_NETWORK": allow_network,
     })
     return kwargs
+
+def incorporate_test_weight_heuristics(kwargs):
+    # kcov is only appropriate for small-sized unit tests. If a test needs a
+    # shard_count or a special timeout, we assume it is not small.
+    if "shard_count" in kwargs or "timeout" in kwargs:
+        kwargs = amend(kwargs, "opt_out_conditions", append = [
+            "//tools/kcov:enabled",
+        ])
+    return kwargs
+
+def _construct_compatibility_select_expressions(
+        *,
+        compatibles,
+        incompatibles):
+    incompatible = "@platforms//:incompatible"
+    positive_terms = dict()
+    positive_terms.update({x: [] for x in compatibles})
+    positive_terms.update({x: [incompatible] for x in incompatibles})
+    negative_terms = dict()
+    negative_terms.update({x: [incompatible] for x in compatibles})
+    negative_terms.update({x: [] for x in incompatibles})
+    return select(positive_terms), select(negative_terms)
+
+def combine_conditions(
+        *,
+        name,
+        opt_in_condition,
+        opt_out_conditions):
+    """Compiles opt_{in,out}_conditions into a pair of select() statements to be
+    used as a values for `target_compatible_with`. The return value is a tuple
+    of (positive, negative) compatibility; positive is compatible with the
+    conditions as given, and negative is the complement (i.e., opposite).
+    """
+
+    # Promote `None`s to default values.
+    default = "//conditions:default"
+    if opt_in_condition == None:
+        opt_in_condition = default
+    if opt_out_conditions == None:
+        opt_out_conditions = []
+
+    # Simple case with a default opt-in (i.e., only opting-out). This fits
+    # within what a single select() can denote.
+    if opt_in_condition == default:
+        return _construct_compatibility_select_expressions(
+            compatibles = [opt_in_condition],
+            incompatibles = opt_out_conditions,
+        )
+
+    # Simple case with a default opt-out (i.e., only opting-in). This fits
+    # within what a single select() can denote.
+    if len(opt_out_conditions) == 0:
+        return _construct_compatibility_select_expressions(
+            compatibles = [opt_in_condition],
+            incompatibles = [default],
+        )
+
+    # When both opt-in and opt-out are non-trivial, a single select() that mixed
+    # all conditions would be ambiguous (we could have two matching conditions
+    # with different compatibility answers). We need to express our priority that
+    # opt-in is checked first, followed by opt-out.
+    #
+    # 1. Declare string `alias()`es whose value is either "//tools:always" or
+    # "//tools:never":
+    #
+    #    a. Declare a string alias() for the opting-out filter: the value is
+    #    "never" when opted-out or else "always" as the default case.
+    #
+    #    b. Declare a string alias() for the opting-in filter: the value is
+    #    part (a) when opted-in or else "never" as the default case.
+    #
+    # 2. Use part (b) as the key in a select(), where when matched there is no
+    # incompatibility or else "incompatible" as the default case.
+    #
+    # These shenanigans are required because while Bazel in general allows
+    # nested selects statements, it doesn't allow it in target_compatible_with.
+    opted_out_alias_name = "_{}_opted_out".format(name)
+    native.alias(
+        name = opted_out_alias_name,
+        actual = select(
+            {
+                x: "//tools:never"
+                for x in opt_out_conditions
+            } | {default: "//tools:always"},
+        ),
+    )
+    opted_in_and_not_out_alias_name = "_{}_opted_in_and_not_out".format(name)
+    native.alias(
+        name = opted_in_and_not_out_alias_name,
+        actual = select({
+            opt_in_condition: ":{}".format(opted_out_alias_name),
+            default: "//tools:never",
+        }),
+    )
+    positive = select({
+        opted_in_and_not_out_alias_name: [],
+        default: ["@platforms//:incompatible"],
+    })
+    negative = select({
+        opted_in_and_not_out_alias_name: ["@platforms//:incompatible"],
+        default: [],
+    })
+    return positive, negative

BUILD.bazel
@@ -165,20 +165,20 @@ install(
 
 install_test(
     name = "install_test",
+    opt_out_conditions = [
+        # Running acceptance tests under coverage (kcov) probably burns more CI
+        # time and flakiness compared to any upside.
+        "//tools/kcov:enabled",
+        # Running acceptance tests under Valgrind tools is extremely slow and
+        # of limited value, so skip them.
+        "//tools/valgrind:enabled",
+    ],
     args = ["--install_tests_filename=$(location :{})".format(
         _INSTALL_TEST_COMMANDS,
     )],
     data = [
         ":install",
         _INSTALL_TEST_COMMANDS,
-    ],
-    tags = [
-        # Running acceptance tests under coverage (kcov) probably burns more CI
-        # time and flakiness compared to any upside.
-        "no_kcov",
-        # Running acceptance tests under Valgrind tools is extremely slow and
-        # of limited value, so skip them.
-        "no_valgrind_tools",
     ],
 )
 

bindings/pydrake/common/BUILD.bazel
@@ -475,7 +475,8 @@ drake_py_library(
 # API.
 drake_py_unittest(
     name = "deprecation_test",
-    tags = ["no_kcov"],  # kcov messes with module ref counts.
+    # kcov messes with module ref counts.
+    opt_out_conditions = ["//tools/kcov:enabled"],
     deps = [
         ":common",
         ":deprecation_example",

bindings/pydrake/systems/BUILD.bazel
@@ -443,7 +443,7 @@ drake_py_unittest(
     # Somehow kcov and pybind11 objects interact to spoil reference counting
     # heuristics. In any case, kcov can't really introspect mixed-language
     # programs.
-    tags = ["no_kcov"],
+    opt_out_conditions = ["//tools/kcov:enabled"],
     deps = [
         ":test_util_py",
         "//bindings/pydrake/common/test_utilities",

common/proto/BUILD.bazel
@@ -76,6 +76,8 @@ drake_cc_googletest(
 # backend on CI only.
 drake_py_unittest(
     name = "call_python_test",
+    # Fails when run under Valgrind.
+    opt_out_conditions = ["//tools/valgrind:enabled"],
     data = [
         ":call_python_client_cli",
         ":call_python_server_test",
@@ -84,8 +86,6 @@ drake_py_unittest(
     flaky = 1,
     # We wish to access neighboring files.
     isolate = 0,
-    # Fails when run under Valgrind tools.
-    tags = ["no_valgrind_tools"],
 )
 
 drake_cc_googletest(

common/test_utilities/BUILD.bazel
@@ -199,6 +199,8 @@ drake_cc_binary(
 # tests are therefore skipped under those configurations.
 drake_sh_test(
     name = "limit_malloc_death_test",
+    # Fails to terminate when run under Valgrind.
+    opt_out_conditions = ["//tools/valgrind:enabled"],
     srcs = [":limit_malloc_test"],
     args = select({
         "//tools/cc_toolchain:apple": [
@@ -213,8 +215,6 @@ drake_sh_test(
     data = [
         ":limit_malloc_test",
     ],
-    # Fails to terminate when run under Valgrind tools.
-    tags = ["no_valgrind_tools"],
 )
 
 drake_cc_googletest(

examples/kuka_iiwa_arm/BUILD.bazel
@@ -103,7 +103,7 @@ drake_cc_binary(
     test_rule_args = ["--simulation_sec=0.1 --target_realtime_rate=0.0"],
     # TODO(kyle.edwards@kitware.com): Re-enable this test when it no longer
     # causes timeouts in kcov.
-    test_rule_tags = ["no_kcov"],
+    test_rule_opt_out_conditions = ["//tools/kcov:enabled"],
 )
 
 drake_cc_binary(
@@ -168,6 +168,9 @@ alias(
 # Test that kuka_simulation can load the dual arm urdf
 drake_sh_test(
     name = "dual_kuka_simulation_test",
+    # TODO(kyle.edwards@kitware.com): Re-enable this test when it no longer
+    # causes timeouts in kcov.
+    opt_out_conditions = ["//tools/kcov:enabled"],
     srcs = ["kuka_simulation"],
     args = [
         "$(location :kuka_simulation)",
@@ -178,9 +181,6 @@ drake_sh_test(
     data = [
         ":dual_iiwa14_polytope_collision.urdf",
     ],
-    # TODO(kyle.edwards@kitware.com): Re-enable this test when it no longer
-    # causes timeouts in kcov.
-    tags = ["no_kcov"],
 )
 
 add_lint_tests()

commit 4f92b2965be66debb49df9442bb8c2a9ebde3ed5

[tools] Suppress kcov on sharded drake_py_unittest
This had been true since 8ae45299 but got broken in 69e064f8.

tools/skylark/drake_py.bzl
@@ -270,7 +270,7 @@ def drake_py_test(
         srcs = ["test/%s.py" % name]
     if kwargs.get("shard_count") != None:
         fail("Only drake_py_unittest can use sharding")
-    shard_count = kwargs.pop("_drake_py_unittest_shard_count", None)
+    kwargs["shard_count"] = kwargs.pop("_drake_py_unittest_shard_count", None)
 
     kwargs = incorporate_allow_network(kwargs, allow_network = allow_network)
     kwargs = incorporate_display(kwargs, display = display)
@@ -293,7 +293,6 @@ def drake_py_test(
         py_target = py_test,
         isolate = isolate,
         size = size,
-        shard_count = shard_count,
         srcs = srcs,
         deps = deps,
         target_compatible_with = target_compatible_with,

tools/skylark/kwargs.bzl
@@ -121,9 +121,20 @@ def incorporate_allow_network(kwargs, *, allow_network):
     return kwargs
 
 def incorporate_test_weight_heuristics(kwargs):
-    # kcov is only appropriate for small-sized unit tests. If a test needs a
-    # shard_count or a special timeout, we assume it is not small.
-    if "shard_count" in kwargs or "timeout" in kwargs:
+    # If a test needs a shard_count or a special timeout, we assume it is not
+    # small -- kcov is only appropriate for small-sized unit tests.
+    required = {
+        "shard_count": 1,
+        "size": "small",
+        "timeout": "short",
+    }
+    big = False
+    for arg_name, required_value in required.items():
+        value = kwargs.get(arg_name, None) or required_value
+        if value != required_value:
+            big = True
+            break
+    if big:
         kwargs = amend(kwargs, "opt_out_conditions", append = [
             "//tools/kcov:enabled",
         ])