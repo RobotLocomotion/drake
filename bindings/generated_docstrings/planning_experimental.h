1694050e226bb7063884df690ef16f8dcd7e7eec
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-05-11 12:46:12 -0700

    [doc] Add a policy to allow packaging experimental code
    Add a placeholder to planning to illustrate.

---
+++ bindings/generated_docstrings/planning_experimental.h
@@ -0,0 +1,44 @@
+#pragma once
+
+// GENERATED FILE DO NOT EDIT
+// This file contains docstrings for the Python bindings that were
+// automatically extracted by mkdoc.py.
+
+#include <array>
+#include <utility>
+
+#if defined(__GNUG__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#endif
+
+// #include "drake/planning/experimental/placeholder.h"
+
+// Symbol: pydrake_doc_planning_experimental
+constexpr struct /* pydrake_doc_planning_experimental */ {
+  // Symbol: drake
+  struct /* drake */ {
+    // Symbol: drake::planning
+    struct /* planning */ {
+      // Symbol: drake::planning::experimental
+      struct /* experimental */ {
+        // Symbol: drake::planning::experimental::Placeholder
+        struct /* Placeholder */ {
+          // Source: drake/planning/experimental/placeholder.h
+          const char* doc =
+R"""(This is a stub class to exercise the build system.)""";
+          // Symbol: drake::planning::experimental::Placeholder::Placeholder
+          struct /* ctor */ {
+            // Source: drake/planning/experimental/placeholder.h
+            const char* doc =
+R"""(This is a stub constructor to exercise the build system.)""";
+          } ctor;
+        } Placeholder;
+      } experimental;
+    } planning;
+  } drake;
+} pydrake_doc_planning_experimental;
+
+#if defined(__GNUG__)
+#pragma GCC diagnostic pop
+#endif
