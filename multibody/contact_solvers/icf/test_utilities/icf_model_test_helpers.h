ab24326ceaeab7882b0d1cd0fc2d578b3e36f307
Rick Poyner <rick.poyner@tri.global>
2026-05-13 15:35:38 -0400

    [multibody] Refile icf_model and constraint tests
    icf_model and all of the *_constraints_pool modules are in a link-time
    dependency cycle. Perhaps naturally, this has led to piling almost all of the
    relevant tests for the whole group into icf_model_test.cc.
    
    In order to make room for quite a few more tests on both icf_model core
    concerns, and on each of the constraint pool types, refile some large chunks of
    icf_model_test.cc. Move the test helper setup methods into the new
    icf_model_test_helpers, and move the constraint-focused tests into (some new,
    some existing) *_constraints_pool_test files.
    
    It should be reasonable to verify that aside from formalities, (like includes,
    namespaces, etc.) only whole functions or whole test cases have moved. The test
    case names haven't even changed, though that might get touched up in the
    future.

---
+++ multibody/contact_solvers/icf/test_utilities/icf_model_test_helpers.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <vector>
+
+#include "drake/multibody/contact_solvers/icf/icf_model.h"
+
+namespace drake {
+namespace multibody {
+namespace contact_solvers {
+namespace icf {
+namespace internal {
+
+/* Sets up a simple dummy model, without any constraints. This can create two
+versions of the same model: one with everything stuffed into a single clique,
+and one with three separate cliques. */
+template <typename T>
+void MakeUnconstrainedModel(IcfModel<T>* model, bool single_clique = false,
+                            double time_step = 0.01);
+
+/* Adds a coupler constraint to the given model. */
+template <typename T>
+void AddCouplerConstraint(IcfModel<T>* model);
+
+/* Adds some dummy gain constraints to the model.
+
+@returns The (K, u, e) vectors used to set the gain constraints on two cliques.
+*/
+template <typename T>
+std::vector<VectorX<T>> AddGainConstraints(IcfModel<T>* model);
+
+/* Adds limit constraints to the given model. The model can have 1 or 3 cliques.
+In either case, the same constraints are added to represent the same logical
+limits. */
+template <typename T>
+void AddLimitConstraints(IcfModel<T>* model);
+
+/* Adds some dummy contact constraints to the model. */
+template <typename T>
+void AddPatchConstraints(IcfModel<T>* model);
+
+/* Adds weld constraints to the given model. Two weld constraints are added:
+one between the world (body 0) and body 1, and one between body 2 and body 3
+(cross-clique in the multi-clique case). */
+template <typename T>
+void AddWeldConstraints(IcfModel<T>* model);
+
+}  // namespace internal
+}  // namespace icf
+}  // namespace contact_solvers
+}  // namespace multibody
+}  // namespace drake
