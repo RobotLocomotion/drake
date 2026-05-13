8f74f91a0397d62adc2187859d167180e7217c1d
Calder Phillips-Grafflin <calder.pg@gmail.com>
2026-05-13 14:25:54 -0700

    Revive use of voxel_grid_internal target

--- planning/experimental/voxel_grid_internal.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "drake/planning/experimental/voxel_occupancy_map_internal.h"
-#include "drake/planning/experimental/voxel_signed_distance_field_internal.h"
-#include "drake/planning/experimental/voxel_tagged_object_occupancy_map_internal.h"
