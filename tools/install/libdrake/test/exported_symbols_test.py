97711528090a27c32eab1f10305259043cf71d90
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-05-12 18:01:41 -0700

    [tools] Reject global constructors and destructors in libdrake
    Rephrase the "linux only" from unittest.skipIf to opt_in_condition.

--- tools/install/libdrake/test/exported_symbols_test.py
+++ tools/install/libdrake/test/exported_symbols_test.py
@@ -1,8 +1,9 @@
 from collections import namedtuple
 import re
 import subprocess
-import sys
 import unittest
+
+from python import runfiles
 
 # Any symbols whose name starts with one of these strings are okay.
 _GOOD_SYMBOLS_PREFIX = [
@@ -89,13 +90,49 @@ _KNOWN_BAD_SYMBOLS_SUBSTR = [
     "wrapper",
 ]
 
+LIBDRAKE = "tools/install/libdrake/libdrake.so"
+
+
+def _is_known_bad_ctor_or_dtor(*, filename, function_name):
+    is_snake_case = filename == filename.lower()
+    if filename.startswith("vtk") and not is_snake_case:
+        # TODO(#24447) Fix VTK to remove globals.
+        return True
+    if filename in ["Singletons.cxx", "Token.cxx", "vtk_opengl_init.cc"]:
+        # TODO(#24447) Fix VTK to remove globals.
+        return True
+    if function_name.startswith("drake_vendor::vtk"):
+        # TODO(#24447) Fix VTK to remove globals.
+        return True
+    if function_name.startswith("vtksys::"):
+        # TODO(#24447) Fix VTK to remove globals.
+        return True
+    if function_name.startswith("vtktoken::"):
+        # TODO(#24447) Fix VTK to remove globals.
+        return True
+    if filename == "Console.cc":
+        # TODO(#24446) Patch libsdformat to fix this.
+        return True
+    if filename == "CoinParamUtils.cpp":
+        # TODO(#24446) Patch libsdformat to fix this.
+        return True
+    if filename == "RoadError.cpp":
+        # TODO(#24446) Patch qhull to remove its global log.
+        return True
+    if filename == "meshcat.cc":
+        # TODO(#24446) Fix this somehow.
+        return True
+    if filename == "impl.cpp":
+        # TODO(#24446) Find and fix this somehow.
+        return True
+    return False
+
 
 class ExportedSymbolsTest(unittest.TestCase):
     def setUp(self):
         self._readelf_repair_pattern = None
 
-    @unittest.skipIf(sys.platform == "darwin", "Ubuntu only")
-    def test_exported_symbols(self):
+    def test_exported_symbols_and_global_constructors(self):
         """Confirms that the symbols exported by libdrake.so are only:
         - Drake API (`namespace drake { ... }`).
         - Vendored externals (`namespace drake_vendor { ... }`).
@@ -103,11 +140,10 @@ class ExportedSymbolsTest(unittest.TestCase):
         Note that many vendored externals are hidden (no exported symbols) but
         in some cases that's not possible and the symbols end up being public.
 
-        For simplicity, we only test on Ubuntu. Hopefully, macOS is similar.
+        Also checks that there are no global constructors or destructors.
         """
-        libdrake = "tools/install/libdrake/libdrake.so"
         command = ["readelf", "--wide", "--symbols"]
-        output = subprocess.check_output(command + [libdrake]).decode("utf8")
+        output = subprocess.check_output(command + [LIBDRAKE]).decode("utf8")
         lines = output.splitlines()
 
         # Now we get to parse the readelf output.  The lines look like this:
@@ -154,12 +190,36 @@ class ExportedSymbolsTest(unittest.TestCase):
         bad_rows = sorted(bad_rows, key=lambda x: (x.Type, x.Name))
 
         # Report the first few errors.
-        for row in bad_rows[:25]:
-            print(f"{row.Type} {row.Bind} {row.Vis}")
-            print(f" {self._demangle(row.Name)}")
-            print(f" {row.Name}")
+        if bad_rows:
+            print("======== Incorrectly namespaced symbol names ========")
             print()
+            for row in bad_rows[:25]:
+                print(f"{row.Type} {row.Bind} {row.Vis}")
+                print(f" {self._demangle(row.Name)}")
+                print(f" {row.Name}")
+                print()
         self.assertEqual(len(bad_rows), 0)
+
+        # Check for static initializers and/or destructors.
+        ctors, dtors = self._global_ctors_dtors(symbols)
+        if ctors:
+            print("======== Disallowed global constructors ========")
+            print()
+            for function_name in ctors:
+                print(f" {function_name}")
+                for filename in ctors[function_name]:
+                    print(f"  called by {filename}")
+                print()
+        if dtors:
+            print("======== Disallowed global destructors ========")
+            print()
+            for function_name in dtors:
+                print(f" {function_name}")
+                for filename in dtors[function_name]:
+                    print(f"  called by {filename}")
+                print()
+        self.assertEqual(len(ctors), 0)
+        self.assertEqual(len(dtors), 0)
 
     def _repair_readelf_output(self, line):
         # Mop up any "<OS specific>: %d" or "<processor specific>: # %d" output
@@ -210,3 +270,131 @@ class ExportedSymbolsTest(unittest.TestCase):
         # lists for symbol validation. (It's both too slow and too brittle
         # to use for the actual validation.)
         return subprocess.check_output(["c++filt"], input=x, encoding="utf-8")
+
+    @staticmethod
+    def _global_ctors_dtors(symbols):
+        """Returns (ctors, dtors) where each is a dict mapping the disallowed
+        constructor or destructor to the list of filename(s) that call it."""
+        ctors = {}
+        dtors = {}
+
+        # Find symbols (if any) where the compiler puts (1) calls to global
+        # constructors and (2) calls to `atexit` to register global destructors.
+        bad_symbols = [
+            row for row in symbols if row.Name.startswith("_GLOBAL__sub")
+        ]
+        if not bad_symbols:
+            return ctors, dtors
+
+        # Disassemble the sections (so we can look for calls). We need to use
+        # llvm objdump instead of Ubuntu objdump because Ubuntu is >100x slower.
+        bad_symbol_names = [symbol.Name for symbol in bad_symbols]
+        manifest = runfiles.Create()
+        objdump_all = subprocess.check_output(
+            [
+                manifest.Rlocation("llvm/bin/llvm-objdump"),
+                LIBDRAKE,
+                "-Mintel",
+                "--no-addresses",
+                "--no-show-raw-insn",
+                "--demangle",
+                f"--disassemble-symbols={','.join(bad_symbol_names)}",
+            ],
+            text=True,
+        ).splitlines()
+
+        # Loop over the bad symbols and find (1) functions that are directly
+        # called (which are therefore invalid constructor calls), and (2)
+        # function pointers that are passed to `atexit` (which are therefore
+        # invalid destructor calls).
+        for symbol in bad_symbols:
+            filename = symbol.Name.removeprefix("_GLOBAL__sub_I_")
+            ctor_calls, dtor_calls = ExportedSymbolsTest._ctor_dtor_calls(
+                symbol=symbol,
+                objdump_all=objdump_all,
+            )
+            if not ctor_calls and not dtor_calls:
+                # The constructor got inlined, so we couldn't identify any
+                # function names as part of a `call` instruction. Be sure to
+                # still report a non-specific error.
+                ctor_calls = ["unknown constructor"]
+            for function_name in ctor_calls:
+                is_known_bad = _is_known_bad_ctor_or_dtor(
+                    filename=filename,
+                    function_name=function_name,
+                )
+                if not is_known_bad:
+                    ctors.setdefault(function_name, []).append(filename)
+            for function_name in dtor_calls:
+                is_known_bad = _is_known_bad_ctor_or_dtor(
+                    filename=filename,
+                    function_name=function_name,
+                )
+                if not is_known_bad:
+                    dtors.setdefault(function_name, []).append(filename)
+        return ctors, dtors
+
+    @staticmethod
+    def _ctor_dtor_calls(*, symbol, objdump_all):
+        """Returns a pair of lists (ctor_calls, dtor_calls). Each list contains
+        function names that are called by the given "__GLOBAL_sub_..." `symbol`,
+        i.e., functions called during static initialization and destruction.
+        """
+        ctor_calls = []
+        dtor_calls = []
+
+        # Find the objdump output for our symbol.
+        i = objdump_all.index(f"<{symbol.Name}>:") + 1
+        instructions = []
+        while i < len(objdump_all):
+            line = objdump_all[i]
+            if not line.startswith(" "):
+                # The decompiled code is all indented by whitespace. A non-
+                # indented line indicates the start of the next function.
+                break
+            instructions.append(line.lstrip())
+            i += 1
+
+        # Remove useless padding at the end of the function.
+        while instructions[-1] == "int3":
+            del instructions[-1]
+
+        # Find calls to functions. These are the global constructor calls,
+        # unless the called function is `atexit` in which case it's a global
+        # destructor registration.
+        atexit_indices = []
+        for i, instruction in enumerate(instructions):
+            if instruction.startswith("call\t"):
+                function_name = instruction.split("<", maxsplit=1)[1][:-1]
+                if function_name == "__cxa_atexit@plt":
+                    atexit_indices.append(i)
+                else:
+                    ctor_calls.append(function_name)
+
+        # Also notice tail calls to `atexit`.
+        if instructions[-1].startswith("jmp\t"):
+            atexit_indices.append(len(instructions) - 1)
+
+        # For calls to `atexit`, figure out what the `rdi` argument is pointing
+        # to. These are the global destructor registrations. These look like:
+        #
+        # lea	rdi, [rip + 0x...] # 0x... <function_name>
+        # ...
+        # call	0x... <__cxa_atexit@plt>
+        max_lookback = 6  # At most this many instructions from lea-rdi to call.
+        for i in atexit_indices:
+            for lookback in range(1, max_lookback + 1):
+                prior = i - lookback
+                if prior < 0:
+                    continue
+                prior_instruction = instructions[prior]
+                if not prior_instruction.startswith("lea\trdi"):
+                    continue
+                function_name = prior_instruction.split("<", maxsplit=1)[1][:-1]
+                dtor_calls.append(function_name)
+                break
+            else:
+                # Could not find the `lea rdi, ...` instruction.
+                dtor_calls.append("unknown destructor")
+
+        return ctor_calls, dtor_calls
