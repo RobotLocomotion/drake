636b1729c3722af0497b49a940589d6cae2c14ec
Calder Phillips-Grafflin <calder.pg@gmail.com>
2026-05-13 10:36:08 -0700

    Inline internal representation getters to avoid symbol duplication issues

--- planning/experimental/voxel_occupancy_map_internal.h
+++ planning/experimental/voxel_occupancy_map_internal.h
@@ -10,15 +10,21 @@ namespace experimental {
 namespace internal {
 // Retrieve a const reference to the internal OccupancyMap of a
 // VoxelOccupancyMap instance.
-const voxelized_geometry_tools::OccupancyMap& GetInternalOccupancyMap(
-    const VoxelOccupancyMap& occupancy_map);
+inline const voxelized_geometry_tools::OccupancyMap& GetInternalOccupancyMap(
+    const VoxelOccupancyMap& occupancy_map) {
+  return *reinterpret_cast<const voxelized_geometry_tools::OccupancyMap*>(
+      occupancy_map.internal_representation());
+}
 
 // Retrieve a mutable reference to the internal OccupancyMap of a
 // VoxelOccupancyMap instance. API takes mutable reference instead of pointer
 // for consistency with non-mutable equivalent.
-voxelized_geometry_tools::OccupancyMap& GetMutableInternalOccupancyMap(
+inline voxelized_geometry_tools::OccupancyMap& GetMutableInternalOccupancyMap(
     // NOLINTNEXTLINE(runtime/references)
-    VoxelOccupancyMap& occupancy_map);
+    VoxelOccupancyMap& occupancy_map) {
+  return *reinterpret_cast<voxelized_geometry_tools::OccupancyMap*>(
+      occupancy_map.mutable_internal_representation());
+}
 
 }  // namespace internal
 }  // namespace experimental
--- planning/experimental/voxel_signed_distance_field_internal.h
+++ planning/experimental/voxel_signed_distance_field_internal.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <common_robotics_utilities/parallelism.hpp>
 #include <voxelized_geometry_tools/signed_distance_field.hpp>
 
 #include "drake/planning/experimental/voxel_signed_distance_field.h"
@@ -10,13 +11,24 @@ namespace experimental {
 namespace internal {
 // Retrieve the internal SignedDistanceField<float> from a
 // VoxelSignedDistanceField instance.
-const voxelized_geometry_tools::SignedDistanceField<float>&
-GetInternalSignedDistanceField(const VoxelSignedDistanceField& distance_field);
+inline const voxelized_geometry_tools::SignedDistanceField<float>&
+GetInternalSignedDistanceField(const VoxelSignedDistanceField& distance_field) {
+  using voxelized_geometry_tools::SignedDistanceField;
+  return *reinterpret_cast<const SignedDistanceField<float>*>(
+      distance_field.internal_representation());
+}
 
 // Convert the provided `parameters` into the paramter class used by
 // voxelized_geometry_tools for signed distance field generation.
-voxelized_geometry_tools::SignedDistanceFieldGenerationParameters<float> ToVGT(
-    const VoxelSignedDistanceField::GenerationParameters& parameters);
+inline voxelized_geometry_tools::SignedDistanceFieldGenerationParameters<float>
+ToVGT(const VoxelSignedDistanceField::GenerationParameters& parameters) {
+  using common_robotics_utilities::parallelism::DegreeOfParallelism;
+  using voxelized_geometry_tools::SignedDistanceFieldGenerationParameters;
+  return SignedDistanceFieldGenerationParameters<float>(
+      parameters.oob_value,
+      DegreeOfParallelism(parameters.parallelism.num_threads()),
+      parameters.unknown_is_filled, parameters.add_virtual_border);
+}
 
 }  // namespace internal
 }  // namespace experimental
