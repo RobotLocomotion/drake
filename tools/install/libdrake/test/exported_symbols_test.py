fd37535c5bbc3fca80807e00f2f4b0cc479f1077
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-05-14 07:49:25 -0700

    [workspace] Remove CoinUtils global variables
    The fix is simply dropping two unused files from the library.
    
    Also tweak a nearby comment after finding where impl.cpp is coming from.

--- tools/install/libdrake/test/exported_symbols_test.py
+++ tools/install/libdrake/test/exported_symbols_test.py
@@ -113,14 +113,11 @@ def _is_known_bad_ctor_or_dtor(*, filename, function_name):
     if filename == "Console.cc":
         # TODO(#24446) Patch libsdformat to fix this.
         return True
-    if filename == "CoinParamUtils.cpp":
-        # TODO(#24446) Patch libsdformat to fix this.
-        return True
     if filename == "meshcat.cc":
         # TODO(#24446) Fix this somehow.
         return True
     if filename == "impl.cpp":
-        # TODO(#24446) Find and fix this somehow.
+        # TODO(#24447) Fix VTK to remove globals.
         return True
     return False
 
