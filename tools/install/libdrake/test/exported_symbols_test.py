63f7309428fa6f94c5af57527d94c9773ce84381
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-05-14 10:12:22 -0700

    [workspace] Remove libsdformat global variables

--- tools/install/libdrake/test/exported_symbols_test.py
+++ tools/install/libdrake/test/exported_symbols_test.py
@@ -110,9 +110,6 @@ def _is_known_bad_ctor_or_dtor(*, filename, function_name):
     if function_name.startswith("vtktoken::"):
         # TODO(#24447) Fix VTK to remove globals.
         return True
-    if filename == "Console.cc":
-        # TODO(#24446) Patch libsdformat to fix this.
-        return True
     if filename == "impl.cpp":
         # TODO(#24447) Fix VTK to remove globals.
         return True
