2484939ac89add9932f8da86a83ee478c9d40a4c
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-06-03 14:11:38 -0700

    add missing nanobind/stl/tuple.h to fix some type casting

--- bindings/pydrake/pydrake_pybind.h
+++ bindings/pydrake/pydrake_pybind.h
@@ -48,6 +48,7 @@
 #include "nanobind/stl/shared_ptr.h"
 #include "nanobind/stl/string.h"
 #include "nanobind/stl/string_view.h"
+#include "nanobind/stl/tuple.h"
 #include "nanobind/stl/unique_ptr.h"
 #include "nanobind/stl/unordered_map.h"
 #include "nanobind/stl/variant.h"
