cca0d1d716890f19ea94caefb4b06a04410ea57c
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-07-10 07:21:56 -0700

    GetParentPlant ref internal

--- bindings/pydrake/multibody/tree_py.cc
+++ bindings/pydrake/multibody/tree_py.cc
@@ -84,10 +84,12 @@ void BindMultibodyElementMixin(PyClass* pcls) {
       .def("index", &Class::index)
       .def("model_instance", &Class::model_instance)
       .def("is_ephemeral", &Class::is_ephemeral)
-      .def("GetParentPlant",
+      .def(
+          "GetParentPlant",
           [](const Class& self) -> const multibody::MultibodyPlant<T>& {
             return self.GetParentPlant();
-          })
+          },
+          py_rvp::reference_internal)
       .def("__repr__", [](const Class& self) {
         py::str cls_name = py::cast<py::str>(
             internal::PrettyClassName(py::cast(&self).type()));
