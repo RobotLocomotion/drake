7328150c8ecb3ac86d924e48da51a07adf8c256e
Sean Curtis <sean.curtis@tri.global>
2026-07-14 11:29:58 -0700

    Upgrade RenderEngineGl's image readback
    Using glReadPixels is much faster than glTextTextureImage (the latter
    incurs a number of pieces of overhead that are unnecessary when all we
    want to do is read back from an uncompressed frame buffer).

--- geometry/render_gl/internal_render_engine_gl.h
+++ geometry/render_gl/internal_render_engine_gl.h
@@ -279,8 +279,8 @@ class RenderEngineGl final : public render::RenderEngine, private ShapeReifier {
   // Obtains the label image rendered from a specific object pose. This is
   // slower than it has to be because it does per-pixel processing on the CPU.
   // TODO(SeanCurtis-TRI): Figure out how to do all of this directly on the GPU.
-  void GetLabelImage(drake::systems::sensors::ImageLabel16I* label_image_out,
-                     const RenderTarget& target) const;
+  void GetLabelImage(
+      drake::systems::sensors::ImageLabel16I* label_image_out) const;
 
   // Acquires the render target for the given camera. "Acquiring" the render
   // target guarantees that the target will be ready for receiving OpenGL
