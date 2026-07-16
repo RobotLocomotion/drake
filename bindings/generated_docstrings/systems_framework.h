c8e2ee2f6f856d5f97b179b20f082734ec07725f
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-07-15 17:42:39 -0700

    docstrings

--- bindings/generated_docstrings/systems_framework.h
+++ bindings/generated_docstrings/systems_framework.h
@@ -11927,6 +11927,11 @@ Note:
 R"""(This method will be called by System<T>∷accept() if the System *is* a
 Diagram<T>.)""";
         } VisitDiagram;
+        // Symbol: drake::systems::SystemVisitor::VisitDiagramPointer
+        struct /* VisitDiagramPointer */ {
+          // Source: drake/systems/framework/system_visitor.h
+          const char* doc = R"""()""";
+        } VisitDiagramPointer;
         // Symbol: drake::systems::SystemVisitor::VisitSystem
         struct /* VisitSystem */ {
           // Source: drake/systems/framework/system_visitor.h
@@ -11934,6 +11939,11 @@ Diagram<T>.)""";
 R"""(This method will be called by System<T>∷accept() if the System *is
 not* a Diagram<T>.)""";
         } VisitSystem;
+        // Symbol: drake::systems::SystemVisitor::VisitSystemPointer
+        struct /* VisitSystemPointer */ {
+          // Source: drake/systems/framework/system_visitor.h
+          const char* doc = R"""()""";
+        } VisitSystemPointer;
       } SystemVisitor;
       // Symbol: drake::systems::TriggerType
       struct /* TriggerType */ {
