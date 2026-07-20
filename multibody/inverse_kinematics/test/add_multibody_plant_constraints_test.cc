7140a145e394762890e0ce2233c4d9fc2f9d35c7
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-07-20 07:30:25 -0700

    [multibody] AddMultibodyPlantConstraints rejects null plant_context
    When the plant contains distance constraints, the plant_context was
    documented to be a required parameter, but this was not enforced.
    Our contract says that we must throw in this case.

--- multibody/inverse_kinematics/test/add_multibody_plant_constraints_test.cc
+++ multibody/inverse_kinematics/test/add_multibody_plant_constraints_test.cc
@@ -82,6 +82,24 @@ class AddMultibodyPlantConstraintsTest : public ::testing::Test {
 
     prog.SetInitialGuessForAllVariables(qn);
     EXPECT_TRUE(prog.CheckSatisfiedAtInitialGuess(constraints, tol));
+
+    // Confirm whether that these constraint(s) required a context or not.
+    const int num_context_required_constraints =
+        plant_->num_constraints() - plant_->num_coupler_constraints();
+    solvers::MathematicalProgram no_context_prog;
+    auto no_context_q =
+        no_context_prog.NewContinuousVariables(plant_->num_positions());
+    auto add_constraints_no_context = [&]() {
+      return AddMultibodyPlantConstraints(plant_, no_context_q,
+                                          &no_context_prog,
+                                          /* plant_context = */ nullptr);
+    };
+    if (num_context_required_constraints > 0) {
+      EXPECT_THROW(add_constraints_no_context(), std::exception);
+    } else {
+      auto no_context_constraints = add_constraints_no_context();
+      EXPECT_EQ(no_context_constraints.size(), expected_num_constraints);
+    }
   }
 
  protected:
