359fff13a2128c4701c2ffa9afc42bd202374c86
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-07-21 15:21:57 -0700

    [workspace] Patch nanobind to allow list inputs for Eigens

--- bindings/temporary_scaffolding/sample_module.cc
+++ bindings/temporary_scaffolding/sample_module.cc
@@ -7,6 +7,22 @@ namespace py = nanobind;
 
 NB_MODULE(sample_module, m) {
   m  // BR
+      .def("copy_dense_mat",
+           [](const Eigen::MatrixXd& x) -> Eigen::MatrixXd {
+             return x;
+           })
+      .def("copy_dense_mat_ref",
+           [](const Eigen::Ref<const Eigen::MatrixXd>& x) -> Eigen::MatrixXd {
+             return x;
+           })
+      .def("copy_dense_vec",
+           [](const Eigen::VectorXd& x) -> Eigen::VectorXd {
+             return x;
+           })
+      .def("copy_dense_vec_ref",
+           [](const Eigen::Ref<const Eigen::VectorXd>& x) -> Eigen::VectorXd {
+             return x;
+           })
       .def("dense_to_sparse",
            [](const Eigen::MatrixXd& x) -> Eigen::SparseMatrix<double> {
              return x.sparseView();
