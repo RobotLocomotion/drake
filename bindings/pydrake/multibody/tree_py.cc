3e08e8424881952da00f32c888c363ab5d6f9ec7
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-07-23 07:24:15 -0700

    small cleanup

--- bindings/pydrake/multibody/tree_py.cc
+++ bindings/pydrake/multibody/tree_py.cc
@@ -89,8 +89,7 @@ void BindMultibodyElementMixin(PyClass* pcls) {
           },
           py_rvp::reference_internal)
       .def("__repr__", [](const Class& self) {
-        py::str cls_name = py::cast<py::str>(
-            internal::PrettyClassName(py::cast(&self).type()));
+        py::str cls_name(internal::PrettyClassName(py::cast(&self).type()));
         const int index = self.index();
         const int model_instance = self.model_instance();
         if constexpr (has_name_func<Class>::value) {
