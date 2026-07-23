5818e76e8716e71ec8527a1eff347946dee7afe1
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-07-23 11:51:21 -0700

    rm unnecessary diff

--- bindings/pydrake/common/module_py.cc
+++ bindings/pydrake/common/module_py.cc
@@ -13,7 +13,6 @@
 #include "drake/common/constants.h"
 #include "drake/common/drake_assert.h"
 #include "drake/common/drake_assertion_error.h"
-#include "drake/common/drake_copyable.h"
 #include "drake/common/drake_path.h"
 #include "drake/common/find_resource.h"
 #include "drake/common/memory_file.h"
