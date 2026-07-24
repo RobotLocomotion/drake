5c0a1f0c4080a5e67607ad01a2734f8f01696eab
Rick Poyner (rico) <rick.poyner@tri.global>
2026-07-24 13:23:23 -0400

    [multibody] fix a test typo (#24782)

--- multibody/contact_solvers/icf/test/icf_solver_test.cc
+++ multibody/contact_solvers/icf/test/icf_solver_test.cc
@@ -45,11 +45,10 @@ class IcfSolverTest : public ::testing::Test {
     params->body_to_clique = {0, 1};
     auto& reduction = params->reduction;
     reduction.unlocked_dofs = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-    reduction.per_clique_unlocked_dofs =
-        {
-            {0, 1, 2, 3, 4, 5},
-            {0, 1, 2, 3, 4, 5},
-        },
+    reduction.per_clique_unlocked_dofs = {
+        {0, 1, 2, 3, 4, 5},
+        {0, 1, 2, 3, 4, 5},
+    };
     model_.ResetParameters(std::move(params));
 
     // Add a basic contact (patch) constraint with one pair and one patch.
