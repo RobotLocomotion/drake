8c014756dd2d5c40d4867f9f32cadede8d616ad2
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-08-03 17:38:51 -0700

    lint (ruff auto-format)

--- bindings/pydrake/multibody/test/plant_test.py
+++ bindings/pydrake/multibody/test/plant_test.py
@@ -3277,24 +3277,40 @@ class TestPlant(unittest.TestCase):
     def test_tendon_constraint_api(self, T):
         plant = MultibodyPlant_[T](0.01)
         plant.set_discrete_contact_approximation(
-            DiscreteContactApproximation.kSap)
+            DiscreteContactApproximation.kSap
+        )
 
         # Add weld constraint.
         body_A = plant.AddRigidBody(name="A")
         body_B = plant.AddRigidBody(name="B")
-        revolute_joint = plant.AddJoint(RevoluteJoint_[T](
-                name="revolute_joint", frame_on_parent=plant.world_frame(),
-                frame_on_child=body_A.body_frame(), axis=[0, 0, 1],
-                damping=0.))
-        prismatic_joint = plant.AddJoint(PrismaticJoint_[T](
-                name="prismatic_joint", frame_on_parent=plant.world_frame(),
-                frame_on_child=body_B.body_frame(), axis=[0, 0, 1],
-                damping=0.))
+        revolute_joint = plant.AddJoint(
+            RevoluteJoint_[T](
+                name="revolute_joint",
+                frame_on_parent=plant.world_frame(),
+                frame_on_child=body_A.body_frame(),
+                axis=[0, 0, 1],
+                damping=0.0,
+            )
+        )
+        prismatic_joint = plant.AddJoint(
+            PrismaticJoint_[T](
+                name="prismatic_joint",
+                frame_on_parent=plant.world_frame(),
+                frame_on_child=body_B.body_frame(),
+                axis=[0, 0, 1],
+                damping=0.0,
+            )
+        )
 
         plant.AddTendonConstraint(
             joints=[revolute_joint.index(), prismatic_joint.index()],
-            a=[0.1, 0.2], offset=0.3, lower_limit=-1.2, upper_limit=3.4,
-            stiffness=5.6, damping=7.8)
+            a=[0.1, 0.2],
+            offset=0.3,
+            lower_limit=-1.2,
+            upper_limit=3.4,
+            stiffness=5.6,
+            damping=7.8,
+        )
 
         # We are done creating the model.
         plant.Finalize()
