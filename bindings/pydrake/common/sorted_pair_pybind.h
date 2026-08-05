87800f1061d25c9708c47225ec7163a2e954e8ee
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-08-05 06:57:52 -0700

    [pydrake] Port type_caster<SortedPair<T>> to nanobind
    Co-authored-by: Rick Poyner (rico) <rick.poyner@tri.global>

--- bindings/pydrake/common/sorted_pair_pybind.h
+++ bindings/pydrake/common/sorted_pair_pybind.h
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <utility>
+
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/common/sorted_pair.h"
 
-namespace pybind11 {
+namespace PYDRAKE_BINDER_NAMESPACE {
 namespace detail {
 
 // Casts `SortedPair<T>` as `Tuple[T]` comprised of `(first, second)`.
+#ifdef PYDRAKE_USE_PYBIND11
 template <typename T>
 struct type_caster<drake::SortedPair<T>> {
   using Type = drake::SortedPair<T>;
@@ -36,6 +39,28 @@ struct type_caster<drake::SortedPair<T>> {
     return out.release();
   }
 };
+#else   // PYDRAKE_USE_NANOBIND
+template <typename T>
+struct type_caster<drake::SortedPair<T>> : public type_caster<std::pair<T, T>> {
+  using Value = drake::SortedPair<T>;
+
+  template <typename U>
+  using Cast = Value;
+
+  template <typename U>
+  static handle from_cpp(
+      U&& value, rv_policy policy, cleanup_list* cleanup) noexcept {
+    using Pair = std::pair<T, T>;
+    return type_caster<Pair>::from_cpp(
+        Pair(value.first(), value.second()), policy, cleanup);
+  }
+
+  explicit operator Value() {
+    return Value(
+        this->caster1.operator cast_t<T>(), this->caster2.operator cast_t<T>());
+  }
+};
+#endif  // PYDRAKE_USE_PYBIND11
 
 }  // namespace detail
-}  // namespace pybind11
+}  // namespace PYDRAKE_BINDER_NAMESPACE
