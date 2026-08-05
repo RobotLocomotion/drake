08f2de25dae9382d27496e2b16f740d3881d0be2
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-08-05 06:53:50 -0700

    simplify the sorted_paid type caster

--- bindings/pydrake/common/sorted_pair_pybind.h
+++ bindings/pydrake/common/sorted_pair_pybind.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <tuple>
+#include <utility>
 
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/common/sorted_pair.h"
@@ -41,46 +41,24 @@ struct type_caster<drake::SortedPair<T>> {
 };
 #else   // PYDRAKE_USE_NANOBIND
 template <typename T>
-struct type_caster<drake::SortedPair<T>> {
+struct type_caster<drake::SortedPair<T>> : public type_caster<std::pair<T, T>> {
   using Value = drake::SortedPair<T>;
-  using InnerCaster = make_caster<T>;
 
   template <typename U>
   using Cast = Value;
 
-  static constexpr auto Name =
-      const_name("Tuple[") + type_caster<T>::Name + const_name("]");
-
-  bool from_python(handle src, uint8_t flags, cleanup_list* cleanup) noexcept {
-    nanobind::tuple t(src);
-    if (t.size() != 2) return false;
-    if (!first.from_python(t[0], flags, cleanup) ||
-        !second.from_python(t[1], flags, cleanup)) {
-      return false;
-    }
-    return true;
-  }
-
   template <typename U>
   static handle from_cpp(
       U&& value, rv_policy policy, cleanup_list* cleanup) noexcept {
-    object out =
-        make_tuple(steal(InnerCaster::from_cpp(value.first(), policy, cleanup)),
-            steal(InnerCaster::from_cpp(value.second(), policy, cleanup)));
-    return out.release();
-  }
-
-  template <typename U>
-  bool can_cast() const noexcept {
-    return first.template can_cast<U>() && second.template can_cast<U>();
+    using Pair = std::pair<T, T>;
+    return type_caster<Pair>::from_cpp(
+        Pair(value.first(), value.second()), policy, cleanup);
   }
 
   explicit operator Value() {
-    return Value(first.operator cast_t<T>(), second.operator cast_t<T>());
+    return Value(
+        this->caster1.operator cast_t<T>(), this->caster2.operator cast_t<T>());
   }
-
-  InnerCaster first;
-  InnerCaster second;
 };
 #endif  // PYDRAKE_USE_PYBIND11
 
