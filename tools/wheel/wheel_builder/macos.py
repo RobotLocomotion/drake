132cd8618b882681dfa55f6c60af7d3ae4af75a0
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-08-10 11:42:16 -0700

    further simplification

--- tools/wheel/wheel_builder/macos.py
+++ tools/wheel/wheel_builder/macos.py
@@ -15,7 +15,6 @@ from .common import (
     die,
     edit_wheel_version_for_binder,
     find_tests,
-    is_abi3_wheel,
     resource_root,
     test_root,
     wheel_name,
@@ -176,7 +175,7 @@ def build(options):
         )
         environment["DRAKE_PYTHON_BINDER"] = python_target.python_binder.value
         environment["DRAKE_IS_ABI3_WHEEL"] = (
-            "1" if is_abi3_wheel(version) else "0"
+            "1" if python_target.python_binder == PythonBinder.NANOBIND else "0"
         )
 
         build_script = os.path.join(resource_root, "macos", "build-wheel.sh")
