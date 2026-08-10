69c125f0fcf8d66c38a49f14168a5ce941ea5f87
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-08-10 13:53:25 -0700

    docs tweak

--- tools/wheel/wheel_builder/macos_types.py
+++ tools/wheel/wheel_builder/macos_types.py
@@ -6,8 +6,8 @@ from .common import PythonBinder
 
 class PythonTarget:
     """
-    A representation of a Python version target, constructed from the binder
-    and version number tuple.
+    A representation of a Python target, constructed from the binder and Python
+    version number tuple.
 
     Example:
         PythonTarget(PythonBinder.NANOBIND, 3, 2, 1)
@@ -18,6 +18,7 @@ class PythonTarget:
         version_full: Target full version as a string, e.g. '3.2.1'
         version: Target major/minor version as a string, e.g. '3.2'
         tag: Target major/minor version without separators, e.g. '32'
+
     """
 
     def __init__(self, python_binder: PythonBinder, *version_parts):
