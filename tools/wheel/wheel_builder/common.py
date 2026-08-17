5a88d76f8fea373748da8cbdf9fb4dc8c88394ed
Matthew Woehlke <matthew.woehlke@kitware.com>
2026-08-17 13:40:52 -0400

    [wheel] Clean up types in wheel builder
    Move `PythonTarget` from `macos_types.py` to `common.py` and use it also
    to describe the Python target in `linux_types.py`'s `Target`. The latter
    had essentially an inline copy of the type; this consolidates them so
    that there is less duplication. Having it as a separate type for Linux
    builds will also make it easier to implement testing on multiple Python
    versions. Add a new (but different from the Linux version) `Target` type
    for macOS to hold the remaining information not in the `PythonTarget`
    (currently, just the Python binder).

--- tools/wheel/wheel_builder/common.py
+++ tools/wheel/wheel_builder/common.py
@@ -42,6 +42,31 @@ class PythonBinder(Enum):
     PYBIND11 = "pybind11"
 
 
+class PythonTarget:
+    """
+    A representation of a Python target, constructed from the version number
+    tuple.
+
+    Example:
+        PythonTarget(3, 2, 1)
+
+    Members:
+        version_tuple: Target version as a tuple, e.g. (3, 2, 1)
+        version_full: Target full version as a string, e.g. '3.2.1'
+        version: Target major/minor version as a string, e.g. '3.2'
+        tag: Target major/minor version without separators, e.g. '32'
+    """
+
+    def __init__(self, *version_parts: int):
+        assert len(version_parts) == 3, version_parts
+
+        pv_parts = tuple(map(str, version_parts))
+        self.version_tuple = tuple(version_parts)
+        self.version_full = ".".join(pv_parts)
+        self.version = ".".join(pv_parts[:2])
+        self.tag = "".join(pv_parts[:2])
+
+
 def gripe(message):
     """
     Prints a message to stderr.
