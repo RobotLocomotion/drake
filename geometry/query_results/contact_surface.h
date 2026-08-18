25cc10bc233d0dec969a131da56a1c6c576373aa
Sean Curtis <sean.curtis@tri.global>
2026-08-18 09:18:17 -0700

    [doc] Correct doxygen groups in geometry (#24891)
    The pattern:
    ```
    /** @name Foo
    ...
    */
    //@{
    
    ...
    
    //@}
    ```
    
    Is defective. The "private" nature of the group brackets (`//@{` and
    `//@}`) are not respected by doxygen. The bounds of the group are not
    recognized leading to a couple of different artifacts:
    
    1. Run-on groups -- members after the closing //@} get included into the
       most recent `@name` group.
    2. Groups aren't properly recognized as groups and not rendered
       consistently.
    
    By promoting the group domain comments to the same "public" level as the
    group, the bounds are recognized.

--- geometry/query_results/contact_surface.h
+++ geometry/query_results/contact_surface.h
@@ -187,7 +187,7 @@ class ContactSurface {
          mesh reversed (to maintain the documented invariants). Comparing the
          input parameters with the members of the resulting %ContactSurface will
          reveal if such a swap has occurred. */
-  //@{
+  ///@{
 
   /** Constructs a %ContactSurface with a triangle mesh representation. */
   ContactSurface(GeometryId id_M, GeometryId id_N,
@@ -209,7 +209,7 @@ class ContactSurface {
 
   ~ContactSurface();
 
-  //@}
+  ///@}
 
   /** Returns the geometry id of Geometry M. */
   GeometryId id_M() const { return id_M_; }
@@ -225,28 +225,34 @@ class ContactSurface {
    without worrying about the representation. If necessary, the actual meshes
    and fields can be accessed directly via the representation-dependent APIs
    below. */
-  //@{
+  ///@{
 
+  /** Reports the number of faces -- triangles or polygons based on surface
+   representation. */
   int num_faces() const {
     return is_triangle() ? tri_mesh_W().num_elements()
                          : poly_mesh_W().num_elements();
   }
 
+  /** Reports the number of vertices. */
   int num_vertices() const {
     return is_triangle() ? tri_mesh_W().num_vertices()
                          : poly_mesh_W().num_vertices();
   }
 
+  /** Reports the area of indexed face. */
   const T& area(int face_index) const {
     return is_triangle() ? tri_mesh_W().area(face_index)
                          : poly_mesh_W().area(face_index);
   }
 
+  /** Reports the total area across all faces. */
   const T& total_area() const {
     return is_triangle() ? tri_mesh_W().total_area()
                          : poly_mesh_W().total_area();
   }
 
+  /** Reports the normal for the indexed face. */
   const Vector3<T>& face_normal(int face_index) const {
     return is_triangle() ? tri_mesh_W().face_normal(face_index)
                          : poly_mesh_W().face_normal(face_index);
@@ -264,14 +270,14 @@ class ContactSurface {
     return is_triangle() ? tri_mesh_W().centroid() : poly_mesh_W().centroid();
   }
 
-  //@}
+  ///@}
 
   /** @name Representation-dependent API
 
    These functions provide insight into what representation the %ContactSurface
    instance uses, and provide access to the representation-dependent quantities:
    mesh and field. */
-  //@{
+  ///@{
 
   /** Simply reports if this contact surface's mesh representation is triangle.
    Equivalent to:
@@ -324,7 +330,7 @@ class ContactSurface {
         e_MN_);
   }
 
-  //@}
+  ///@}
 
   /** @name  Evaluation of constituent pressure fields
 
@@ -344,7 +350,7 @@ class ContactSurface {
 
    The values ∇eₘ and ∇eₘ are piecewise constant over the %ContactSurface and
    can only be evaluate on a per-face basis.  */
-  //@{
+  ///@{
 
   /** @returns `true` if `this` contains values for ∇eₘ.  */
   bool HasGradE_M() const { return grad_eM_W_ != nullptr; }
@@ -362,7 +368,7 @@ class ContactSurface {
    @pre `index ∈ [0, mesh().num_faces())`.  */
   const Vector3<T>& EvaluateGradE_N_W(int index) const;
 
-  //@}
+  ///@}
 
   // TODO(#12173): Consider NaN==NaN to be true in equality tests.
   /** Checks to see whether the given ContactSurface object is equal via deep
