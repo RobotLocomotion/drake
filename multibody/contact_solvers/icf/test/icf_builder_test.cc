188364d4ddcdc3a304d04cf0c67331a37f513dc8
Rick Poyner <rick.poyner@tri.global>
2026-08-18 18:50:48 -0400

    [multibody] ICF (and hence CENIC) obey ConstraintActiveStatus

--- multibody/contact_solvers/icf/test/icf_builder_test.cc
+++ multibody/contact_solvers/icf/test/icf_builder_test.cc
@@ -16,6 +16,7 @@
 #include "drake/multibody/parsing/parser.h"
 #include "drake/multibody/plant/multibody_plant.h"
 #include "drake/multibody/plant/multibody_plant_config_functions.h"
+#include "drake/multibody/tree/revolute_joint.h"
 #include "drake/systems/framework/bus_value.h"
 #include "drake/systems/framework/diagram_builder.h"
 
@@ -606,6 +607,115 @@ GTEST_TEST(IcfBuilder, DistanceConstraintAnchoredBodyB) {
   EXPECT_EQ(pool.body_pairs()[0].second, dynamic_body.index());
   EXPECT_TRUE(CompareMatrices(pool.p_AP_W()[0], p_BQ));
   EXPECT_TRUE(CompareMatrices(pool.p_BQ_W()[0], p_AP));
+}
+
+// Unit test that the builder checks the active status of each constraint and
+// produces an IcfModel with only the active constraints.
+GTEST_TEST(IcfBuilder, ConstraintActiveStatus) {
+  MultibodyPlant<double> plant(0.0);
+
+  // Add two bodies and two joints in order to cover all constraint types.
+  // We aren't doing any physics in this test, just checking that constraint
+  // active status is forwarded through to the model. So bodies, joints, and
+  // constraints have arbitrary non-physical parameters for ease of setup.
+  const RigidBody<double>& body_A =
+      plant.AddRigidBody("body_A", SpatialInertia<double>::MakeUnitary());
+  const RigidBody<double>& body_B =
+      plant.AddRigidBody("body_B", SpatialInertia<double>::MakeUnitary());
+  const RevoluteJoint<double>& world_A = plant.AddJoint<RevoluteJoint>(
+      "world_A", plant.world_body(), RigidTransformd::Identity(), body_A,
+      RigidTransformd::Identity(), Vector3d::UnitZ());
+  const RevoluteJoint<double>& A_B = plant.AddJoint<RevoluteJoint>(
+      "A_B", body_A, RigidTransformd::Identity(), body_B,
+      RigidTransformd::Identity(), Vector3d::UnitZ());
+
+  // Add one ball constraint, one coupler constraint, one distance constraint
+  // and one weld constraint.
+  MultibodyConstraintId ball_constraint_id = plant.AddBallConstraint(
+      body_A, Vector3d::Zero(), body_B, Vector3d::Zero());
+  MultibodyConstraintId coupler_constraint_id =
+      plant.AddCouplerConstraint(world_A, A_B, 1.2);
+  MultibodyConstraintId distance_constraint_id = plant.AddDistanceConstraint(
+      body_A, Vector3d::Zero(), body_B, Vector3d(1.0, 2.0, 3.0), 1.0);
+  MultibodyConstraintId weld_constraint_id = plant.AddWeldConstraint(
+      body_A, RigidTransformd(), body_B, RigidTransformd());
+
+  plant.Finalize();
+
+  auto context = plant.CreateDefaultContext();
+
+  const double time_step = 0.01;
+  IcfBuilder<double> builder(&plant);
+  IcfModel<double> model;
+
+  // All constraints active.
+  {
+    builder.UpdateModel(*context, time_step, nullptr, nullptr, &model);
+
+    // Verify number of constraints when all are active. There is no contact
+    // so we expect one coupler constraint, one distance constraint one
+    // ball constraint and one weld constraint.
+    EXPECT_EQ(model.num_constraints(), 4);
+    EXPECT_EQ(model.ball_constraints_pool().num_constraints(), 1);
+    EXPECT_EQ(model.coupler_constraints_pool().num_constraints(), 1);
+    EXPECT_EQ(model.distance_constraints_pool().num_constraints(), 1);
+    EXPECT_EQ(model.weld_constraints_pool().num_constraints(), 1);
+  }
+
+  // Set the distance and ball constraints to inactive and verify that only the
+  // coupler and weld constraints show up in the model.
+  {
+    plant.SetConstraintActiveStatus(context.get(), distance_constraint_id,
+                                    false);
+    plant.SetConstraintActiveStatus(context.get(), ball_constraint_id, false);
+
+    builder.UpdateModel(*context, time_step, nullptr, nullptr, &model);
+
+    // Verify number of constraints when only the coupler and weld constraints
+    // are active.
+    EXPECT_EQ(model.num_constraints(), 2);
+    EXPECT_EQ(model.ball_constraints_pool().num_constraints(), 0);
+    EXPECT_EQ(model.coupler_constraints_pool().num_constraints(), 1);
+    EXPECT_EQ(model.distance_constraints_pool().num_constraints(), 0);
+    EXPECT_EQ(model.weld_constraints_pool().num_constraints(), 1);
+  }
+
+  // Now disable the coupler and weld constraints and verify that the model
+  // has 0 constraints.
+  {
+    plant.SetConstraintActiveStatus(context.get(), coupler_constraint_id,
+                                    false);
+    plant.SetConstraintActiveStatus(context.get(), weld_constraint_id, false);
+
+    builder.UpdateModel(*context, time_step, nullptr, nullptr, &model);
+
+    // Verify number of constraints when all are inactive.
+    EXPECT_EQ(model.num_constraints(), 0);
+    EXPECT_EQ(model.ball_constraints_pool().num_constraints(), 0);
+    EXPECT_EQ(model.coupler_constraints_pool().num_constraints(), 0);
+    EXPECT_EQ(model.distance_constraints_pool().num_constraints(), 0);
+    EXPECT_EQ(model.weld_constraints_pool().num_constraints(), 0);
+  }
+
+  // Reactivate all constraints and verify they show up in the model.
+  {
+    plant.SetConstraintActiveStatus(context.get(), distance_constraint_id,
+                                    true);
+    plant.SetConstraintActiveStatus(context.get(), ball_constraint_id, true);
+    plant.SetConstraintActiveStatus(context.get(), coupler_constraint_id, true);
+    plant.SetConstraintActiveStatus(context.get(), weld_constraint_id, true);
+
+    builder.UpdateModel(*context, time_step, nullptr, nullptr, &model);
+
+    // Verify number of constraints when all are active. There is no contact
+    // so we expect one coupler constraint, one distance constraint, one
+    // ball constraint and one weld constraint.
+    EXPECT_EQ(model.num_constraints(), 4);
+    EXPECT_EQ(model.ball_constraints_pool().num_constraints(), 1);
+    EXPECT_EQ(model.coupler_constraints_pool().num_constraints(), 1);
+    EXPECT_EQ(model.distance_constraints_pool().num_constraints(), 1);
+    EXPECT_EQ(model.weld_constraints_pool().num_constraints(), 1);
+  }
 }
 
 GTEST_TEST(IcfBuilder, TendonConstraintUnsupported) {
