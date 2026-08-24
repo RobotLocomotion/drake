51bb8b3ce623079905c25f69c447111a240fbd11
Aiden Woodruff <aiden.woodruff@kitware.com>
2026-08-24 17:34:40 -0400

    [workspace] Use libpng from BCR
    Loses drake-specific Intel msse4.1 SIMD instructions and addenda to
    pnglibconf.h to disable out-of-line functions and suppress exif
    warnings.

--- tools/workspace/libpng_internal/png_drake_vendor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/* The following are Drake's adjustments to libpng. */
-
-/* Always use the macros, never the slow out-of-line functions. */
-#undef PNG_READ_INT_FUNCTIONS_SUPPORTED
-
-/* Suppress benign eXIf warnings, as they produce unnecessary test output. */
-#undef PNG_READ_eXIf_SUPPORTED
