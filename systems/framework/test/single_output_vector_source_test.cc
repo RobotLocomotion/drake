5eec004e68e84672885959152deed8dc1e315b55
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-09-08 05:26:27 -0700

    move -Wno-deprecated-declarations to build file

--- systems/framework/test/single_output_vector_source_test.cc
+++ systems/framework/test/single_output_vector_source_test.cc
@@ -8,11 +8,6 @@
 #include "drake/common/drake_copyable.h"
 #include "drake/systems/framework/test_utilities/scalar_conversion.h"
 
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#endif
-
 namespace drake {
 namespace systems {
 namespace {
@@ -111,7 +106,3 @@ GTEST_TEST(SingleOutputVectorSourceConvertableTest, ScalarTypes) {
 }  // namespace
 }  // namespace systems
 }  // namespace drake
-
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic pop
-#endif
