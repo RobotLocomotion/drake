ec4c3912551eb63b0238ea5de8bd1bdc40782dfd
castor639 <316019788+castor639@users.noreply.github.com>
2026-09-09 15:28:17 -0500

    Apply clang-format to MuJoCo nested-include changes

--- multibody/parsing/detail_mujoco_parser.cc
+++ multibody/parsing/detail_mujoco_parser.cc
@@ -1,1 +1,57 @@
-file:///workspace/drake-24943-fmt/detail_mujoco_parser.cc
+#include "drake/multibody/parsing/detail_mujoco_parser.h"
+
+#include <algorithm>
+#include <filesystem>
+#include <limits>
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include <fmt/format.h>
+#include <tinyxml2.h>
+
+#include "drake/common/text_logging.h"
+#include "drake/geometry/proximity/obb.h"
+#include "drake/geometry/proximity/obj_to_surface_mesh.h"
+#include "drake/geometry/shape_specification.h"
+#include "drake/lcm/drake_lcm.h"
+#include "drake/math/rigid_transform.h"
+#include "drake/math/rotation_matrix.h"
+#include "drake/multibody/parsing/detail_make_model_name.h"
+#include "drake/multibody/parsing/detail_tinyxml.h"
+#include "drake/multibody/parsing/detail_tinyxml2_diagnostic.h"
+#include "drake/multibody/tree/ball_rpy_joint.h"
+#include "drake/multibody/tree/geometry_spatial_inertia.h"
+#include "drake/multibody/tree/prismatic_joint.h"
+#include "drake/multibody/tree/revolute_joint.h"
+#include "drake/multibody/tree/scoped_name.h"
+#include "drake/multibody/tree/weld_joint.h"
+#include "drake/systems/sensors/camera_config_functions.h"
+
+namespace drake {
+namespace multibody {
+namespace internal {
+
+using drake::internal::DiagnosticPolicy;
+using Eigen::AngleAxisd;
+using Eigen::Matrix3d;
+using Eigen::Vector2d;
+using Eigen::Vector3d;
+using Eigen::Vector4d;
+using geometry::GeometryId;
+using geometry::GeometryInstance;
+using math::RigidTransformd;
+using math::RotationMatrixd;
+using tinyxml2::XMLAttribute;
+using tinyxml2::XMLDocument;
+using tinyxml2::XMLElement;
+using tinyxml2::XMLNode;
+
+namespace {
+
+constexpr std::array kOrientationAttributes = {  // BR
+    "quat", "axisangle", "euler", "xyaxes", "zaxis"};
