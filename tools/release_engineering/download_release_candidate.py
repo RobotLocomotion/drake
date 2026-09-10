c177d49c6833ff579a8c04593ac7f3a7890c6ebe
Jeremy Nimmer <jeremy.nimmer@tri.global>
2026-09-10 08:56:59 -0700

    [wheel] Switch default binder to nanobind
    Nightly builds continue to publish pybind11 wheels as a fallback for
    users who can't use nanobind yet.
    
    This expands our macOS pip support matrix down to 3.12 as a minimum
    (instead of 3.13).

--- tools/release_engineering/download_release_candidate.py
+++ tools/release_engineering/download_release_candidate.py
@@ -134,14 +134,9 @@ def _download_binaries(*, version):
     binaries = {
         "https://drake-packages.csail.mit.edu/drake/staging": [
             # Wheel filenames.
-            f"drake-{version[1:]}-cp312-cp312-manylinux_2_34_x86_64.whl",
-            f"drake-{version[1:]}-cp312-cp312-manylinux_2_34_aarch64.whl",
-            f"drake-{version[1:]}-cp313-cp313-manylinux_2_34_x86_64.whl",
-            f"drake-{version[1:]}-cp313-cp313-manylinux_2_34_aarch64.whl",
-            f"drake-{version[1:]}-cp314-cp314-manylinux_2_34_x86_64.whl",
-            f"drake-{version[1:]}-cp314-cp314-manylinux_2_34_aarch64.whl",
-            f"drake-{version[1:]}-cp313-cp313-macosx_15_0_arm64.whl",
-            f"drake-{version[1:]}-cp314-cp314-macosx_15_0_arm64.whl",
+            f"drake-{version[1:]}-cp312-abi3-manylinux_2_34_x86_64.whl",
+            f"drake-{version[1:]}-cp312-abi3-manylinux_2_34_aarch64.whl",
+            f"drake-{version[1:]}-cp312-abi3-macosx_15_0_arm64.whl",
             # Deb filenames.
             f"drake-dev_{version[1:]}-1_amd64-noble.deb",
             f"drake-dev_{version[1:]}-1_arm64-noble.deb",
