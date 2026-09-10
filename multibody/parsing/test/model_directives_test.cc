58a930927225b8ee3b215221c374ca0488a6f8dc
castor639 <316019788+castor639@users.noreply.github.com>
2026-09-10 18:13:34 -0500

    [parsing] Cover remaining model_directives IsValid error branches

--- multibody/parsing/test/model_directives_test.cc
+++ multibody/parsing/test/model_directives_test.cc
@@ -4,6 +4,9 @@
 #include <gtest/gtest.h>
 
 #include "drake/common/test_utilities/diagnostic_policy_test_base.h"
+#include "drake/common/schema/rotation.h"
+#include "drake/common/schema/stochastic.h"
+#include "drake/common/schema/transform.h"
 #include "drake/common/yaml/yaml_io.h"
 
 using drake::yaml::LoadYamlString;
@@ -128,6 +131,92 @@ TEST_F(ModelDirectivesErrorTest, AddDirectivesEmptyFile) {
   EXPECT_FALSE(dut.IsValid(diagnostic_policy_));
   EXPECT_THAT(TakeError(),
               MatchesRegex(".*add_directives: `file` must be non-empty.*"));
+}
+
+
+TEST_F(ModelDirectivesErrorTest, AddWeldXpcBaseFrameRejected) {
+  AddWeld dut;
+  dut.parent = "parent_frame";
+  dut.child = "child_frame";
+  schema::Transform X_PC;
+  X_PC.base_frame = "world";
+  dut.X_PC = X_PC;
+  EXPECT_FALSE(dut.IsValid(diagnostic_policy_));
+  EXPECT_THAT(TakeError(),
+              MatchesRegex(".*add_weld: `X_PC` must not specify a `base_frame`.*"));
+}
+
+TEST_F(ModelDirectivesErrorTest, AddWeldXpcNonDeterministic) {
+  AddWeld dut;
+  dut.parent = "parent_frame";
+  dut.child = "child_frame";
+  schema::Transform X_PC;
+  X_PC.rotation.value = schema::Rotation::Uniform{};
+  dut.X_PC = X_PC;
+  EXPECT_FALSE(dut.IsValid(diagnostic_policy_));
+  EXPECT_THAT(TakeError(),
+              MatchesRegex(".*add_weld: `X_PC` must specify a deterministic.*"));
+}
+
+TEST_F(ModelDirectivesErrorTest, AddModelDefaultFreeBodyPoseNonDeterministic) {
+  AddModel dut;
+  dut.file = "base.sdf";
+  dut.name = "m";
+  schema::Transform pose;
+  pose.rotation.value = schema::Rotation::Uniform{};
+  dut.default_free_body_pose["body1"] = pose;
+  EXPECT_FALSE(dut.IsValid(diagnostic_policy_));
+  EXPECT_THAT(
+      TakeError(),
+      MatchesRegex(
+          ".*add_model: `default_free_body_pose` must specify a deterministic.*"));
+}
+
+TEST_F(ModelDirectivesErrorTest, AddFrameXpfNonDeterministic) {
+  AddFrame dut;
+  dut.name = "f";
+  dut.X_PF.base_frame = "world";
+  dut.X_PF.rotation.value = schema::Rotation::Uniform{};
+  EXPECT_FALSE(dut.IsValid(diagnostic_policy_));
+  EXPECT_THAT(TakeError(),
+              MatchesRegex(".*add_frame: `X_PF` must specify a deterministic.*"));
+}
+
+TEST_F(ModelDirectivesErrorTest, AddCollisionFilterGroupEmptyName) {
+  AddCollisionFilterGroup dut;
+  dut.members = {"link"};
+  EXPECT_FALSE(dut.IsValid(diagnostic_policy_));
+  EXPECT_THAT(
+      TakeError(),
+      MatchesRegex(".*add_collision_filter_group: `name` must be non-empty.*"));
+}
+
+TEST_F(ModelDirectivesErrorTest, AddCollisionFilterGroupEmptyMembers) {
+  AddCollisionFilterGroup dut;
+  dut.name = "group1";
+  EXPECT_FALSE(dut.IsValid(diagnostic_policy_));
+  EXPECT_THAT(
+      TakeError(),
+      MatchesRegex(
+          ".*add_collision_filter_group:.*at least one of `members` or "
+          "`member_groups` must be non-empty.*"));
+}
+
+TEST_F(ModelDirectivesErrorTest, ModelDirectiveNotUnique) {
+  ModelDirective dut;
+  // Zero alternatives set => not unique.
+  EXPECT_FALSE(dut.IsValid(diagnostic_policy_));
+  EXPECT_THAT(TakeError(), MatchesRegex(".*directive: Specify one of.*"));
+
+  AddModel add_model;
+  add_model.file = "base.sdf";
+  add_model.name = "m";
+  AddModelInstance add_model_instance;
+  add_model_instance.name = "mi";
+  dut.add_model = add_model;
+  dut.add_model_instance = add_model_instance;
+  EXPECT_FALSE(dut.IsValid(diagnostic_policy_));
+  EXPECT_THAT(TakeError(), MatchesRegex(".*directive: Specify one of.*"));
 }
 
 TEST_F(ModelDirectivesErrorTest, ModelDirectivesPropagatesErrors) {
